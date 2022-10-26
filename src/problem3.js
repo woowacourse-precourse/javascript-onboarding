88a3dd89b1ebdd1f5614384e433416f2e140a05b
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-27 06:25:51 +0900

    feat: [problem3] Add checkNumber

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function problem3(number) {}
+
+function checkNumber(number) {
+  const arrayNumber = number.toString().split("");
+  let result = arrayNumber.filter(
+    (item) => item == "3" || item == "6" || item == "9"
+  );
+  return result.length;
 }
 
 module.exports = problem3;
