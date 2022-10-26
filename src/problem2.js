b3163379271d1e3a1226ba414f655acd23a63bc6
minjaeKim <smallkdb@gmail.com>
2022-10-27 01:33:35 +0900

    feat: [Problem2] Add decrypting function
    암호문을 해독하는 decrypting 함수를 추가하였습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
+function decrypting(cryptogram) {
+  while(cryptogram !== cryptogram.replace(/(\w)\1/g, ""))
+    cryptogram = cryptogram.replace(/(\w)\1/g, "");
+  return cryptogram;
+}
+
 function problem2(cryptogram) {
   var answer;
+  answer = decrypting(cryptogram);
   return answer;
 }
 
