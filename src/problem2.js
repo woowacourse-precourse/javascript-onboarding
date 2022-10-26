3100cc4e931f3870235078f8092178bf1875e6cf
cys4585 <choizerowater@gmail.com>
2022-10-26 22:11:56 +0900

    feat: 문제2 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,37 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+
+  answer = decrypt(cryptogram);
   return answer;
+}
+
+function decrypt(cryptogram) {
+  while (isCryptogram(cryptogram)) {
+    cryptogram = deleteConsecutiveChar(cryptogram);
+  }
+  return cryptogram;
+}
+
+function isCryptogram(cryptogram) {
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (cryptogram[i - 1] === cryptogram[i]) return true;
+  }
+  return false;
+}
+
+function deleteConsecutiveChar(cryptogram) {
+  let result = "";
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (
+      cryptogram[i] === cryptogram[i + 1] ||
+      cryptogram[i] === cryptogram[i - 1]
+    ) {
+      continue;
+    }
+    result += cryptogram[i];
+  }
+  return result;
 }
 
 module.exports = problem2;
