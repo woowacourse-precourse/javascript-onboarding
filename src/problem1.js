bcb58a19c0916c0e85ba518a187a1d6a50123c99
catca <kdg96g@gmail.com>
2022-10-26 19:06:50 +0900

    feat: solved problem1
    각 자리 수 합, 곱 함수 생성
    입력값의 각 자리 수 합, 곱들 중 최댓값 구해서 비교

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,24 @@
+function calAdd(number) {
+  return number % 100 + (number / 10) % 10 + number % 10;
+}
+function calMul(number) {
+  return (number % 100) * ((number / 10) % 10) * (number % 10);
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(pobi[1] - pobi[0] !== 1 || pobi[0] % 2 !== 1 || pobi[0] < 2 || pobi[0] > 399) return -1
+  if(crong[1] - crong[0] !== 1 || crong[0] % 2 !== 1 || crong[0] < 2 || crong[0] > 399) return -1
+
+  const pobiMax = Math.max(calAdd(pobi[0]), calMul(pobi[0]), calAdd(pobi[1]), calMul(pobi[1]));
+  const crongMax = Math.max(calAdd(crong[0]), calMul(crong[0]), calAdd(crong[1]), calMul(crong[1]));
+
+  if(pobiMax > crongMax){
+    return 1;
+  } else if(pobiMax < crongMax){
+    return 2;
+  } else {
+    return 0;
+  }
 }
 
 module.exports = problem1;
