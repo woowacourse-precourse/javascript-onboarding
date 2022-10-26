2445bd580c264911761bb95041d9c4b0ae000936
donghhan <apensia914@gmail.com>
2022-10-26 17:06:59 +0900

    Problem 4 solution added: including exception case of numbers and special characters

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,25 @@
 function problem4(word) {
-  var answer;
+  const pattern = /[a-zA-Z\s]/g; // For checking if it's other than alphabets
+  let answer = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].match(pattern)) {
+      // Convert them if character is an alphabet
+      if (word[i] === word[i].toUpperCase() && word[i] !== " ") {
+        const upper = String.fromCharCode(155 - word[i].charCodeAt());
+        answer += upper;
+      } else if (word[i] === " ") {
+        answer += " ";
+      } else {
+        const lower = String.fromCharCode(219 - word[i].charCodeAt());
+        answer += lower;
+      }
+    } else {
+      // Disregarding converting characters if they are NOT alphabets
+      answer += word[i];
+    }
+  }
+
   return answer;
 }
 
