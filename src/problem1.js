57d6767de6a85d9f574b471662f7f1bc0e7fd48b
nlom0218 <nlom0218@naver.com>
2022-10-26 18:28:07 +0900

    feat: 문제 1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,38 @@
+const splitNumber = (num) => {
+  num = num + "";
+  return num.split("").map((item) => Number(item));
+};
+
+const getScore = (leftNums, rightNums) => {
+  const leftSum = leftNums.reduce((acc, cur) => acc + cur, 0);
+  const leftMultiply = leftNums.reduce((acc, cur) => acc * cur, 1);
+  const rightSum = rightNums.reduce((acc, cur) => acc + cur, 0);
+  const rightMultiply = rightNums.reduce((acc, cur) => acc * cur, 1);
+  return Math.max(leftSum, leftMultiply, rightSum, rightMultiply);
+};
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const [pobiLeftPage, pobiRightPage] = pobi;
+  const [crongLeftPage, crongRightPage] = crong;
+
+  if (
+    pobiRightPage - pobiLeftPage !== 1 ||
+    crongRightPage - crongLeftPage !== 1
+  )
+    return -1;
+
+  const pobiScore = getScore(
+    splitNumber(pobiLeftPage),
+    splitNumber(pobiRightPage)
+  );
+  const crongScore = getScore(
+    splitNumber(crongLeftPage),
+    splitNumber(crongRightPage)
+  );
+
+  if (pobiScore > crongScore) return 1;
+  if (pobiScore > crongScore) return 2;
+  return 0;
 }
 
 module.exports = problem1;
