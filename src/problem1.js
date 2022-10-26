7506c9d644fc19e6644dab22f1c19efddc42e084
BB-choi <choibobae0809@gmail.com>
2022-10-26 17:56:28 +0900

    feat: 문제1 - 페이지 수 이용하여 최대 숫자 추출하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,20 @@
+function isArrayIncludesZero(nums) {
+  return nums.includes(0);
+}
+
+function calcMaxNumber(num) {
+  const nums = num.toString().split("").map(Number);
+
+  if (isArrayIncludesZero(nums)) {
+    return nums.reduce((a, b) => a + b, 0);
+  }
+
+  return Math.max(
+    nums.reduce((a, b) => a * b, 1),
+    nums.reduce((a, b) => a + b, 0)
+  );
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
