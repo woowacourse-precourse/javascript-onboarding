cd78881a1c0f80c4aa5207753f54d22c3a36cdef
Leejin-Yang <ejin1996@gmail.com>
2022-10-26 19:00:14 +0900

    feat(problem4): 예외 사항 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,28 @@
+const isUpperCase = (letter) =>
+  letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90;
+const isLowerCase = (letter) =>
+  letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122;
+
+function isAlphabet(letter) {
+  if (isUpperCase(letter) || isLowerCase(letter)) {
+    return true;
+  }
+
+  return false;
+}
+
 function problem4(word) {
   var answer;
   return answer;
 }
 
 module.exports = problem4;
+
+function test() {
+  console.log(isAlphabet('A'));
+  console.log(isAlphabet('a'));
+  console.log(isAlphabet('/'));
+  console.log(isAlphabet(' '));
+}
+
+test();
