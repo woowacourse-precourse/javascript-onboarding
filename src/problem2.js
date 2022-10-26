71772c8f8fc81b32a72fc51307f66b7efb9c4a1d
SangDon <adoim@naver.com>
2022-10-27 03:04:32 +0900

    refactor : 주석 수정 / 코드정리

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,15 @@
-// function problem2(cryptogram) {
-//   var answer;
-//   return answer;
-// }
+function problem2(cryptogram) {
+  var answer;
+  answer = decipher(cryptogram);
+  return answer;
+}
 
-// module.exports = problem2;
+module.exports = problem2;
 
-let letter = "browoanoommnaon";
-
-function decipher(letter) {
-  letter.toLowerCase();
-  const arr = letter.split("");
+//문자를 배열로 변환후 중복제거 이후 다시 문자로 변경한다.
+function decipher(word) {
+  word.toLowerCase();
+  const arr = word.split(""); // 문자를 배열로 취급
 
   const charArray = decrypto(arr);
   const key = charArray.join("");
@@ -17,6 +17,7 @@ function decipher(letter) {
   return key;
 }
 
+// 중복 문자를 제거하고 다시 처음부터 루프를 도는 for loop
 function decrypto(arr) {
   for (let i = 0; i < arr.length; i++) {
     if (arr[i] === arr[i + 1]) {
@@ -27,5 +28,3 @@ function decrypto(arr) {
 
   return arr;
 }
-
-
