c34098df230d9364a6be3714c9702651ad059926
CoboCho <rlaalsrb1111@naver.com>
2022-10-27 04:29:37 +0900

    feat: [problem1] 예외 케이스 추가
    입력값 검증시 예외 케이스 추가
    - 좌측 페이지가 짝수일 경우
    - 첫페이지, 마지막 페이지를 열었을 경우

--- src/problem1.js
+++ src/problem1.js
@@ -1,10 +1,13 @@
 function problem1(pobi, crong) {
   if(pobi[1] !== pobi[0] + 1 || crong[1] !== crong[0] + 1) return -1;
+  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0) return -1;
+  if([1, 399].includes(pobi[0]) || [1, 399].includes(crong[0])) return -1;
   let pobiScore = getScore(pobi);
   let crongScore = getScore(crong);
   if(pobiScore > crongScore) return 1;
   if(pobiScore < crongScore) return 2;
   if(pobiScore === crongScore) return 0;
+  return -1;
 }
 
 function getScore(pages) {
