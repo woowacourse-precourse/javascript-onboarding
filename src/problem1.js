f5795bf7a2e1b6bb862bdf1fc223f0a2109aed99
gilpop8663 <wolfye@naver.com>
2022-10-26 23:48:16 +0900

    Add: [P1] 점수 계산 코드 작성 및 테스트 통과

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,48 @@
+function scoreCalculator(score) {
+  score = score.toString();
+
+  let ans;
+
+  let sumS = 0,
+    mulS = 1;
+
+  for (let i = 0; i < score.length; i++) {
+    let number = Number(score[i]);
+    sumS += number;
+    mulS *= number;
+  }
+
+  ans = Math.max(sumS, mulS);
+
+  return ans;
+}
+
 function problem1(pobi, crong) {
   var answer;
+
+  pobi.sort();
+  crong.sort();
+
+  if (pobi[0] <= 1 || crong[0] <= 1 || pobi[1] >= 400 || crong[1] >= 400)
+    return -1;
+  if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) return -1;
+
+  let pLeftScore, pRightScore, cLeftScore, cRightScore;
+
+  let pobiScore, crongScore;
+
+  pLeftScore = scoreCalculator(pobi[0]);
+  pRightScore = scoreCalculator(pobi[1]);
+  cLeftScore = scoreCalculator(crong[0]);
+  cRightScore = scoreCalculator(crong[1]);
+
+  pobiScore = Math.max(pLeftScore, pRightScore);
+  crongScore = Math.max(cLeftScore, cRightScore);
+
+  if (pobiScore > crongScore) answer = 1;
+  else if (pobiScore < crongScore) answer = 2;
+  else if (pobiScore === crongScore) answer = 0;
+
   return answer;
 }
 
