b014572ada241cc41b1b1c5097b2d514984f09aa
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-26 17:48:21 +0900

    problem4 분기문 설정

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
 function problem4(word) {
   var answer;
+  for (let i=0; i<word.length;i++){
+    if (word.charCodeAt(i)==32){
+      continue;
+    }
+    else if (65<=word.charCodeAt(i)<=90){
+
+    }
+    else if (97<=word.charCodeAt(i)<=122){
+
+    }
+  } 
   return answer;
 }
-
 module.exports = problem4;
