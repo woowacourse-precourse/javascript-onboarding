93459869d549e87511a592046b757069b197c851
froggy1014 <clcl6084@gmail.com>
2022-10-26 22:35:45 +0900

    feat[1]: 페이지를 입력받아 최댓값 반환 메소드 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+  let pobiMaxV = getMaxV(pobi);
+  let crongMaxV = getMaxV(crong);
 
-module.exports = problem1;
+  function getMaxV(pobi){
+    const values = pobi.map((pageNum) => {
+      let sum = String(pageNum).split('').reduce((a,b) => Number(a) + Number(b), 0)
+      let mul = String(pageNum).split('').reduce((a,b) => Number(a) * Number(b))
+      return Math.max(sum, mul)
+    })
+    return Math.max(...values);
+  }
+
+  console.log(pobiMaxV, crongMaxV);
+}
+problem1([97, 98], [199, 200]);
+
+// module.exports = problem1;
