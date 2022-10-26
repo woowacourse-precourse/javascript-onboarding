55d4dab0686154c046e5d8937a2ffadea17b4819
100Gyeon <bjy9966@naver.com>
2022-10-26 19:50:15 +0900

    feat: 큰 금액 단위부터 나눠서 화폐 변환

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const unitList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answerList = new Array(unitList.length).fill(0);
+  let index = 0;
+
+  while (money > 0) {
+    const count = Math.floor(money / unitList[index]);
+    answerList[index] = count;
+    if (count) money -= unitList[index] * count;
+    index++;
+  }
+
+  return answerList;
 }
 
 module.exports = problem5;
