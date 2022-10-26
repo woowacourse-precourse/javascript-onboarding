2f8818afad31c01ea462b6ac7d74b3b20d900cee
Suyoung <jsy8492@gmail.com>
2022-10-26 20:05:24 +0900

    feat(problem2): 문제 2 solution 함수 및 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let s = cryptogram;
+  for (let i = 0; i < s.length; i++) {
+    if (s[i] === s[i + 1]) {
+      s = s.slice(0, i) + s.slice(i + 2);
+
+      if (i > 1) {
+        i -= 2;
+      }
+    }
+  }
+
+  return s.length === 2 ? '' : s;
 }
 
 module.exports = problem2;
