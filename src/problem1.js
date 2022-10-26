4674811ffae96d4953d97d763c3bba89d4e9cdda
SangDon <adoim@naver.com>
2022-10-27 03:11:09 +0900

    refactor : 주석 정리 / problem1함수에 answer 를 전달하는 함수를 추가 하여 분리.

--- src/problem1.js
+++ src/problem1.js
@@ -1,17 +1,6 @@
 function problem1(pobi, crong) {
   var answer;
-
-  if (vaildate(pobi) || vaildate(crong)) {
-    return -1;
-  }
-
-  const pobiNumber = calcPage(pobi);
-  const crongNumber = calcPage(crong);
-
-  if (pobiNumber > crongNumber) answer = 1;
-  else if (crongNumber > pobiNumber) answer = 2;
-  else if (pobiNumber === crongNumber) answer = 0;
-
+  answer = resultCalc(pobi, crong);
   return answer;
 }
 
@@ -27,16 +16,6 @@ function isEven(num) {
   else return false;
 }
 
-// ****** 테스트용 난수 생성 ******** //
-// //1~ 399 페이지 난수 생성 함수  //범위를 가진 난수는 어떻게 만들지??
-// function createPage() {
-//   let page = Math.floor(Math.random() * (PAGE_END - PAGE_START)) + PAGE_START;
-
-//   // 난수가 짝수인지 판별하기 위해
-//   if (isEven(page)) return [page - 1, page];
-//   else return [page, page + 1];
-// }
-
 //숫자 리터럴을 배열로 변환하는 함수
 function numToArr(num) {
   origin = num.toString().split("");
@@ -45,16 +24,16 @@ function numToArr(num) {
 }
 
 //배열의 숫자를 계산해서 높은값을 리턴하는 함수
+// sum 값이 높은지 확인하여 sum과 multi 중 반환값을 결정.
 function pageResult(arr) {
   const sumResult = arr.reduce((a, b) => a + b);
   const multipleResult = arr.reduce((a, b) => a * b);
 
   if (sumResult > multipleResult) return sumResult;
-  else return multipleResult; // sum 값이 높은지 확인하여 sum과 multi 중 반환값을 결정.
+  else return multipleResult;
 }
 
-//배열의 숫자를 처리 하는 함수 // 숫자나 문자를 배열로 만들어아하는데?
-//리듀스 사용(누산값, 현재값)
+//배열의 숫자를 처리 하는 함수 //
 function calcPage(arr) {
   [start, next] = [...arr];
 
@@ -66,6 +45,20 @@ function calcPage(arr) {
 
   if (startResult > nextResult) return startResult; // 높은 숫자를 리턴
   else return nextResult; //
+}
+
+//메인 함수 answer 에 값을 반환하기 위한 함수
+function resultCalc(pobi, crong) {
+  if (vaildate(pobi) || vaildate(crong)) {
+    return -1;
+  }
+
+  const pobiNumber = calcPage(pobi);
+  const crongNumber = calcPage(crong);
+
+  if (pobiNumber > crongNumber) return 1;
+  else if (crongNumber > pobiNumber) return 2;
+  else if (pobiNumber === crongNumber) return 0;
 }
 
 //페이지의 길이검사, 페이지의 연속성검사 , 페이지 범위 검사
