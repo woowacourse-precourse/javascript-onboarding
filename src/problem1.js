99285018d278b28e64406d316fcee9cdbd3bafb9
su-lim <sujin.0xlim@gmail.com>
2022-10-26 19:31:20 +0900

    feat: prob1 - Add calcScore function

--- src/problem1.js
+++ src/problem1.js
@@ -8,6 +8,23 @@ function validCheck(left, right){
   return true
 }
 
+function calcScore(left, right){
+  leftSum = 0
+  leftMul = 1
+  rightSum = 0
+  rightMul = 1
+
+  for(let i = left; i > 0; i = parseInt(i / 10)){
+    leftSum += i%10
+    leftMul *= i%10
+  }
+  for(let i = right; i > 0; i = parseInt(i / 10)){
+    rightSum += i%10
+    rightMul *= i%10
+  }
+  
+  return Math.max(leftSum, leftMul, rightSum, rightMul);
+}
 
 function problem1(pobi, crong) {
   var answer;
@@ -16,8 +33,9 @@ function problem1(pobi, crong) {
     answer = -1
     return answer
   }
-
-  // 계산 후 answer 정하는 함수
+  pobiScore = calcScore(pobi[0], pobi[1])
+  crongScore = calcScore(crong[0], crong[1]) 
+  answer = pobiScore > crongScore ? 1 : pobiScore === crongScore ? 0 : 2
 
   return answer;
 }
