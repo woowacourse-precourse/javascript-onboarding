4e812443ce4e2197c76e8c430e7fe0480d48a665
minseok1109 <park485201@naver.com>
2022-10-26 17:20:47 +0900

    feat: 페이지가 올바른지 여러 조건 체크. #기능목록-5,6

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,4 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
-
+//책 페이지가 1~400인지 체크
 function isInPage(arr1, arr2) {
   if (
     arr1.every((item) => item >= 1 && item <= 400) &&
@@ -12,5 +8,21 @@ function isInPage(arr1, arr2) {
   }
   return false;
 }
+
+function isRightPage(arr1, arr2) {
+  //페이지 순서가 올바르지 않으면 false
+  if (arr1[1] - arr1[0] > 1 || arr2[1] - arr2[0] > 1) {
+    return false;
+    //오른쪽 페이지가 홀수이면 false
+  } else if (arr1[1] % 2 !== 0 || arr2[1] % 2 !== 0) {
+    return false;
+    //왼쪽 페이지가 짝수이면 false
+  } else if (arr1[0] % 2 !== 1 || arr2[0] % 2 !== 1) {
+    return false;
+  }
+  return true;
+}
+
+function problem1(pobi, crong) {}
 
 module.exports = problem1;
