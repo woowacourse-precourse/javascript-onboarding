b05a0867061009ea9ed22dcc7d24583d60f86562
bassyu <yrkdgus@gmail.com>
2022-10-26 19:10:34 +0900

    문제3 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let totalClap = 0;
+  for (let current = 1; current < number + 1; current++) {
+    totalClap += getClap(current);
+  }
+  return totalClap;
+}
+
+function getClap(current) {
+  let clap = 0;
+  current.toString().split('').forEach((char) => {
+    clap += '369'.includes(char);
+  })
+  return clap
 }
 
 module.exports = problem3;
