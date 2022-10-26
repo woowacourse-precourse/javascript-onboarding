0ea7fa12ebed93cfabf5713fca555585bbc382d3
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-10-26 23:54:19 +0900

    큰 수 구하기 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,22 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    var answer;
+
+    // 1. 큰 수 구하기
+    // - 오른쪽 페이지 번호의 덧셈값과 곱셉값 구하기
+    // - 둘 중 큰 수 구하기
+    function getBiggestNumber(arr) {
+        const rightPage = String(arr[1]).split("");
+        let add = 0;
+        let mul = 1;
+        for (let i = 0; i < rightPage.length; i++) {
+            add += Number(rightPage[i]);
+            mul *= Number(rightPage[i]);
+        }
+        const biggestNumber = add > mul ? add : mul;
+        return biggestNumber;
+    }
+
+    return answer;
 }
 
 module.exports = problem1;
