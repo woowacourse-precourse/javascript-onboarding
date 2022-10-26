aaaf4526cd62d006c5b9896ad78930c43e77a1d9
Jongco <narira0922@gmail.com>
2022-10-26 20:21:32 +0900

    problem1: 페이지 번호 게임 기능 구현 목록 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,10 @@
 function problem1(pobi, crong) {
-  var answer;
+  /**
+   * 1. 오른쪽 페이지 - 왼쪽 페이지의 값이 1이 아닐 때 예외처리 // -1
+   * 2. 2-3 과정을 통해 나오는 가장 큰 수 도출
+   * 3. 비교를 통해 result 반환
+   */
+  let answer;
   return answer;
 }
 
