fc2515ee2726874b9ea388e370b9d02dbc5f4d0b
yeong bin <2976622@naver.com>
2022-10-26 19:43:08 +0900

    chore: problem1 - skeleton code 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,17 @@
+function checkValidation() {
+  //입력된 페이지가 유효한지 검증
+}
+
+function calculateTotalScore() {
+  //펼친 페이지의 score 계산
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!checkValidation(pobi) || !checkValidation(crong))
+    return -1;
+  const score1 = calculateTotalScore(pobi);
+  const score2 = calculateTotalScore(crong);
+  return score1 > score2 ? 1 : (score1 < score2 ? 2 : 0);
 }
 
 module.exports = problem1;
