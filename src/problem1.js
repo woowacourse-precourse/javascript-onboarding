8a02cc21c32976af02a198e93232a861c1978321
yeonsuBeak <soothbugger@gmail.com>
2022-10-26 21:52:15 +0900

    feat: 각 자리 숫자 더하거나 곱하기 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,24 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const leftValues = calculatePageNumber(pobi[0]);
+  const rightValues = calculatePageNumber(pobi[1]);
+}
+
+function calculatePageNumber(page) {
+  const hundredNumber = Math.floor(page / 100);
+  const tenNumber = Math.floor((page / 10) % 10);
+  const oneNumber = Math.floor((page % 100) % 10);
+
+  if (hundredNumber === 0) {
+    if (tenNumber === 0) {
+      return { first: oneNumber, second: oneNumber };
+    }
+    return { first: tenNumber + oneNumber, second: tenNumber * oneNumber };
+  }
+
+  return {
+    first: hundredNumber + tenNumber + oneNumber,
+    second: hundredNumber * tenNumber * oneNumber,
+  };
 }
 
 module.exports = problem1;
