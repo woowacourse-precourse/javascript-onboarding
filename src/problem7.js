1b1ef6025f08c95de9890b0cca7073e04a767614
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-27 02:01:47 +0900

    feat: problem7 함께 아는 친구들의 점수 더하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,10 +1,18 @@
 function problem7(user, friends, visitors) {
   const relation = {};
+  const score = {};
   friends.forEach(([user1, user2]) => {
     if (!relation[user1]) relation[user1] = [];
     if (!relation[user2]) relation[user2] = [];
     relation[user1].push(user2);
     relation[user2].push(user1);
+  });
+  relation[user].forEach((friend) => {
+    relation[friend].forEach((knowTogether) => {
+      if (knowTogether === user) return;
+      if (!score[knowTogether]) score[knowTogether] = 0;
+      score[knowTogether] += 10;
+    });
   });
 }
 
