3f0eb06e2b192817729ab7cd80d1ab589bce6fbc
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 16:19:01 +0900

    feat: 예외처리 기능
    - 왼쪽 페이지가 홀수
    - 오른쪽 페이지가 왼쪽페이지보다 1 커야 한다

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,6 @@
 function problem1(pobi, crong) {
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
+  if (pobi[0] % 2 === 0 || crong[0] % 2 === 0) return -1;
   function getMaxNumber(num) {
     let arr = String(num).split("");
     let plus = arr.reduce((a, c) => {
@@ -11,6 +13,12 @@ function problem1(pobi, crong) {
     }, 1);
     return Math.max(plus, multiple);
   }
+  let pobiMaxNum = Math.max(getMaxNumber(pobi[0], pobi[1]));
+  let crongMaxNum = Math.max(getMaxNumber(crong[0], crong[1]));
+  if (pobiMaxNum > crongMaxNum) return 1;
+  if (pobiMaxNum < crongMaxNum) return 2;
+  if (pobiMaxNum === crongMaxNum) return 0;
+  return -1;
 }
 
 module.exports = problem1;
