10ce4a848e6390a576cf2e1cdc02ea00553bae80
foundaway98 <foundaway98@naver.com>
2022-10-27 01:37:08 +0900

    덧셈 및 곱셈 연산 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,17 @@
+function makeSum(n) {
+  let val = 0;
+  for (let i = 0; i < n.length; i++) {
+    val += parseInt(n[i]);
+  }
+}
+
+function makeMul(n) {
+  let val = 0;
+  for (let i = 0; i < n.length; i++) {
+    val *= parseInt(n[i]);
+  }
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
