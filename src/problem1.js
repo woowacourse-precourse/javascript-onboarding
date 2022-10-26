de308b162fc046457aec66f1677d35148f1ef4da
yeongi <cbg5255@gmail.com>
2022-10-26 21:17:18 +0900

    문제1  페이지 배열 계산 후 정렬 기능 분리 리펙토링 , 테스트 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,53 +1,50 @@
 function problem1(pobi, crong) {
-  //각 자리수 더하기
-  const sumDigit = (num) => {
-    let res = 0;
-    while (num > 0) {
-      res += Math.floor(num % 10);
-      num = Math.floor(num / 10);
-    }
-    return res;
-  };
-
-  //각 자리수 곱하기
-  const multiplyDigit = (num) => {
-    let res = 1;
-    while (num > 0) {
-      if (num < 0) break;
-      res *= Math.floor(num % 10);
-      num = Math.floor(num / 10);
-    }
-    return res;
-  };
-
   // 예외사항은 -1
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
   if (pobi[0] === 1 || crong[0] === 1 || pobi[1] === 400 || crong[1] === 400)
     return -1;
 
-  let pobiNum;
-  let crongNum;
-
-  pobi.map((num) =>
-    sumDigit(num) > multiplyDigit(num)
-      ? (pobiNum = sumDigit(num))
-      : (pobiNum = multiplyDigit(num))
-  );
-
-  crong.map((num) =>
-    sumDigit(num) > multiplyDigit(num)
-      ? (crongNum = sumDigit(num))
-      : (crongNum = multiplyDigit(num))
-  );
+  const pobiResult = CalAndSort(pobi, []);
+  const crongResult = CalAndSort(crong, []);
 
   //포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0,
-  if (pobiNum > crongNum) {
+  if (pobiResult[0] > crongResult[0]) {
     return 1;
-  } else if (pobiNum < crongNum) {
+  } else if (pobiResult[0] < crongResult[0]) {
     return 2;
   } else {
     return 0;
   }
 }
+
+//각 페이지 계산하고 정렬
+const CalAndSort = (req, res) => {
+  req.map((num) => {
+    res.push(sumDigit(num));
+    res.push(multiplyDigit(num));
+  });
+  return res.sort((a, b) => b - a);
+};
+
+//각 자리수 더하기
+const sumDigit = (num) => {
+  let res = 0;
+  while (num > 0) {
+    res += Math.floor(num % 10);
+    num = Math.floor(num / 10);
+  }
+  return res;
+};
+
+//각 자리수 곱하기
+const multiplyDigit = (num) => {
+  let res = 1;
+  while (num > 0) {
+    if (num < 0) break;
+    res *= Math.floor(num % 10);
+    num = Math.floor(num / 10);
+  }
+  return res;
+};
 
 module.exports = problem1;
--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,6 @@ function problem5(money) {
   return answer;
 }
 
+//오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+
 module.exports = problem5;
