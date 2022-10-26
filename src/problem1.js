0186d6bffb85ce9aff9a81529ef55da5caece891
jun <ggprgrkjh@naver.com>
2022-10-26 16:17:21 +0900

    docs: 기능 목록 업데이트 P1

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,38 @@
+//TODO list
+// - [x] 책을 임의로 펼친다. - 함수에 인자를 적용받는다. - 구현필요 x
+
+// findPageValue
+// - [] 페이지값의 length를 알아낸다
+// - [] 페이지값의 각 length값을 이용하여 자릿수의 값을 알아낸다
+
+// leftPageSum
+// - [] 왼쪽 페이지 번호의 각 자리 숫자를 모두 더한다.
+
+// leftPageMul
+// - [] 왼쪽 페이지 번호의 각 자리 숫자를 모두 곱한다.
+
+// rightPageSum
+// - [] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더한다.
+
+// rightPageMul
+// - [] 오른쪽 페이지 번호의 각 자리 숫자를 모두 곱한다.
+
+// findMax
+// - [] 가장 큰 수를 구한다 - pobi, crong
+
+// compareValues
+// - [] pobi와 crong의 최댓값을 비교한다.
+// - [] 가장 수가 큰 사람을 return 해준다.
+
+// 이미 입력값이 들어올 때 시작 면과 마지막 면이 들어오지 않으니 따로 구현 필요 x
+// - [x] 시작 면이 나오지 않게한다. - 1제외
+// - [x] 마지막 면이 나오지 않게한다. - 400제외
+
+// 로직
+// 1. pobi와 crong 할당
+// 2. 왼쪽, 오른쪽 페이지 합과 곱 구하기
+// 3. 가장 큰 수 return
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
