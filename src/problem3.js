1e267692f263a6eecd9f84444a76cf1a1b9486c3
pakxe <pigkill40@naver.com>
2022-10-27 04:48:33 +0900

    refactor: [P3] 메소드 다중사용이 가독성을 흐린다고 생각해 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let handsClap = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let num = i.toString().split(""); // 숫자를 순회할 수 있는 배열로 변경
+    num.map((n) => {
+      if (n === "3" || n === "6" || n === "9") handsClap++;
+    });
+  }
+
+  return handsClap;
 }
 
 module.exports = problem3;
