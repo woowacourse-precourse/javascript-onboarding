b79df08fe2951445bdb28e9293c48dabdb4db5a3
pakxe <pigkill40@naver.com>
2022-10-26 19:32:56 +0900

    feat: 페이지 유효성 검사 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,10 @@
+//페이지 유효성 검사
+const isValidPage = (book1, book2) => {
+  if (book1[1] - book1[0] !== 1) return false;
+  if (book2[1] - book2[0] !== 1) return false;
+  return true;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
