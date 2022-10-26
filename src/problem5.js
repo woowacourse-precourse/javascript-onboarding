b9726f77eb995870deb39fc393f248deca187994
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 03:23:27 +0900

    problem5/feat : 요구한 화폐 단위 별 변환 되는 개수 배열에 저장

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  if (String(money).length >= 1 && String(money).length <= 1000000) {
+    for (let i = 0; i < moneyArr.length; i++) {
+      answer.push(parseInt(money / moneyArr[i]));
+      money = money - parseInt(money / moneyArr[i]) * moneyArr[i];
+    }
+  }
+
   return answer;
 }
-
 module.exports = problem5;
