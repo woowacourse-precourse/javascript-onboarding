71961c046f08d2c3b5becf08250c026edad17289
박준서 <oponize@naver.com>
2022-10-26 23:14:54 +0900

    feat: problem1/유효페이지 검증 함수

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,9 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function isValidPage(numbers) {
+  if (numbers.indexOf(1) === -1 || numbers.indexOf(400) === -1) return true;
+  return false;
+}
+
 module.exports = problem1;
