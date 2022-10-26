0d6e0301c594942b3123cc04e81cd69d5852612f
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-26 21:19:58 +0900

    feat(Stack):스택을 활용한 문자열 제거 기능 추가(기능 목록 1번,2번 구현)

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const answer = [];
+  for (let x of cryptogram) {
+    if (answer[answer.length - 1] !== x) {
+      answer.push(x);
+    } else {
+      answer.pop();
+    }
+  }
+  return answer.length === 0 ? "" : answer.join("");
 }
 
 module.exports = problem2;
