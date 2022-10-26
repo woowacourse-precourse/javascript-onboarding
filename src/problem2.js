3a3ed9334297b96dfe1156383549249f8f3960ca
Hyun Su <tlsgustn93@naver.com>
2022-10-26 20:53:53 +0900

    problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = [...cryptogram];
+  let answer = ''
+  let i = 0;
+  while (i != arr.length) {
+      arr[i] == arr[i+1] ? (
+          arr.splice(i, 2),
+          i = 0
+      ) : i += 1
+  }
+  return arr.join('')
 }
 
 module.exports = problem2;
