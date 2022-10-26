dc5523e9de28492c86ae7bd202bc3aa158661bf3
bassyu <yrkdgus@gmail.com>
2022-10-26 19:44:17 +0900

    문제4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const wordMap = getWordMap();
+
+  let encoded = '';
+  word.split('').forEach((key) => {
+    encoded += wordMap[key];
+  });
+
+  return encoded;
+}
+
+function getWordMap() {
+  let wordMap = {' ': ' '};
+  for (let i = 65; i <= 90; i++) {
+    const key = String.fromCharCode(i);
+    const value = String.fromCharCode(90 + 65 - i);
+    wordMap[key] = value;
+  }
+  for (let i = 97; i <= 122; i++) {
+    const key = String.fromCharCode(i);
+    const value = String.fromCharCode(122 + 97 - i);
+    wordMap[key] = value;
+  }
+  return wordMap;
 }
 
 module.exports = problem4;
