b55b0e8d3d8388fcc2dbfa9d0ff803cb7b0b3245
imb96 <kimminje7267@naver.com>
2022-10-26 20:15:06 +0900

    Remove duplicate alphabets

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,25 @@
+/**
+ * | cryptogram        | result  |
+| ----------------- | ------- |
+| "browoanoommnaon" | "brown" |
+| "zyelleyz"        | ""      |
+ * @param {*} cryptogram
+ * @returns
+ */
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const map = new Map();
+  let result = "";
+  for (let i = 0; i < cryptogram.length; i++) {
+    map.set(
+      cryptogram.split("")[i],
+      (map.get(cryptogram.split("")[i]) || 0) + 1
+    );
+  }
+  for (const [key, value] of map) {
+    if (value % 2 == 1) {
+      result += key;
+    }
+  }
+  return result;
 }
-
 module.exports = problem2;
