c1ab7f7d5039e754edd1a3f5a760b985c0b68c49
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-26 16:34:13 +0900

    feat: 사용자 점수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,16 +1,23 @@
 const problem1 = (pobi, crong) => {
   if (checkException(pobi, crong)) return -1;
-}
+};
 
 const checkException = (player1, player2) => {
   return (player1[1] - player1[0] !== 1) || (player2[1] - player2[0] !== 1);
-}
+};
 
 const getPageScore = (pageNum) => {
   const pageNumArr = [...String(pageNum)].map((n) => Number(n));
   const plussedScore = pageNumArr.reduce((acc, cur) => acc + cur);
   const multipliedScore = pageNumArr.reduce((acc, cur) => acc * cur);
   return plussedScore > multipliedScore ? plussedScore : multipliedScore;
-}
+};
+
+const getPlayerScore = (player) => {
+  const [leftPageNum, rightPageNum] = player;
+  const leftPageScore = getPageScore(leftPageNum);
+  const rightPageScore = getPageScore(rightPageNum);
+  return leftPageScore >= rightPageScore ? leftPageScore : rightPageScore;
+};
 
 module.exports = problem1;
