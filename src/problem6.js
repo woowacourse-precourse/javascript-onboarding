eaaab6fb6c4b77a467a7ceea58e91db5f4bc5d8f
h2002y <h2002y@naver.com>
2022-10-26 20:52:37 +0900

    feat: Add checkNickname func

--- src/problem6.js
+++ src/problem6.js
@@ -2,6 +2,8 @@ function problem6(forms) {
   var answer;
   let map = new Map();
   let rejectIndex = [];
+  setMap(forms, map);
+  checkNickname(forms, map, rejectIndex);
   return answer;
 }
 
@@ -12,6 +14,18 @@ function setMap(forms, map){
     for(let j = 0; j < forms[i][1].length - 1; j++){
       let tmp = forms[i][1].substr(j,2);
       map.set(tmp, 1);
+    }
+  }
+}
+
+function checkNickname(forms, map, rejectIndex){
+  for(let i = 0; i < forms.length; i++){
+    for(let j = 0; j < forms[i][1].length - 1; j++){
+      let tmp = froms[i][1].substr(j,2);
+      if(map.has(tmp)){
+        rejectIndex.push(i);
+        break;
+      }
     }
   }
 }
