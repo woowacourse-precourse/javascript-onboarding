792f701bc41913e6d578b51c0dda8241c88a24c1
Yeom-JinHo <pwlsghq@naver.com>
2022-10-26 21:02:56 +0900

    feat : [P1] 가장 큰 수 리턴함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,19 @@
+// 각 숫자를 모두 더하거나, 모두 곱해서 가장 큰 수 리턴하는 함수 (2,3번 요구사항)
+const maxScore = (page) => {
+  let sumScore = 0;
+  let multipScore = 1;
+
+  do {
+    sumScore += page % 10;
+    multipScore *= page % 10;
+    page = parseInt(page / 10);
+  } while (page > 0);
+
+  return sumScore > multipScore ? sumScore : multipScore;
+};
+
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
   return answer;
 }
 
