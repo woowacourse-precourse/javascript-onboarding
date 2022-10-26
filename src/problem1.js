d625590a2609a86091d14ec719e51586018c10cc
oing <74910908+oing-yong@users.noreply.github.com>
2022-10-27 00:51:41 +0900

    feat: 문제1 제한사항을 어길 경우 return -1 하도록 함
    제한 사항은 아래와 같음
    -시작 면[1, 2]이나 마지막 면[399, 400]이 나오도록 책을 펼치지 않는다.
    -pobi와 crong의 길이는 2이다.
    -pobi와 crong에는 [왼쪽 페이지 번호(홀수), 오른쪽 페이지 번호(짝수)]가 순서대로 들어있다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,17 @@
 function problem1(pobi, crong) {
+  if (pobi[0], crong[0] === 1 || pobi[0], crong[0] === 399) {
+    //시작 면[1, 2]이나 마지막 면[399, 400]이 나오도록 책을 펼치지 않는다.
+    return -1;
+  } else if (pobi.length != 2 || crong.length != 2) {
+    //pobi와 crong의 길이는 2이다.
+    return -1;
+  } else if (pobi[0]%2, crong[0]%2 != 1 || pobi[1]%2, crong[1]%2 != 0) {
+    //pobi와 crong에는 [왼쪽 페이지 번호(홀수), 오른쪽 페이지 번호(짝수)]가 순서대로 들어있다.
+    return -1;
+  }
+
   var answer;
+
   return answer;
 }
 
