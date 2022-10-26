445dbc0b38cdb33ea8af56db06448431a669a560
sery <yunsel2@gmail.com>
2022-10-26 23:26:50 +0900

    feat: Add sumOfPage, multiplyOfPage function

--- src/problem1.js
+++ src/problem1.js
@@ -9,11 +9,29 @@ function problem1(pobi, crong) {
   const crongLeftPageArray = splitNumber(crongLeftPage);
   const crongRightPageArray = splitNumber(crongRightPage);
 
+  const pobiLeftSum = sumOfPage(pobiLeftPageArray);
+  const pobiRightSum = sumOfPage(pobiRightPageArray);
+  const crongLeftSum = sumOfPage(crongLeftPageArray);
+  const crongRightSum = sumOfPage(crongRightPageArray);
+
+  const pobiLeftMultiply = multiplyOfPage(pobiLeftPageArray);
+  const pobiRightMultiply = multiplyOfPage(pobiRightPageArray);
+  const crongLeftMultiply = multiplyOfPage(crongLeftPageArray);
+  const crongRightMultiply = multiplyOfPage(crongRightPageArray);
+
   return answer;
 }
 
 function splitNumber(page) {
   return page.toString().split('');
+}
+
+function sumOfPage(arr) {
+  return arr.reduce((prev, curr) => Number(prev) + Number(curr));
+}
+
+function multiplyOfPage(arr) {
+  return arr.reduce((prev, curr) => Number(prev) * Number(curr));
 }
 
 module.exports = problem1;
