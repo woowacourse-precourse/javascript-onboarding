804b2d0809bea737791256e6fad36f09ac3fecb3
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 18:01:07 +0900

    feat: problem2 문제 해결
    - 재귀를 이용한 기능구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (cryptogram.length === 0) return "";
+  function getNoContinuousWords(str) {
+    let obj = { 0: str[0] };
+    for (let i = 1; i < str.length; i++) {
+      if (str[i] !== str[i - 1]) {
+        obj[`${i}`] = str[i];
+      } else {
+        if (obj[`${i - 1}`]) {
+          delete obj[`${i - 1}`];
+        }
+      }
+    }
+    let answerStr = Object.values(obj).join("");
+    if (!answerStr) return "";
+    if (answerStr === str) return str;
+    return getNoContinuousWords(answerStr);
+  }
+  return getNoContinuousWords(cryptogram);
 }
 
 module.exports = problem2;
