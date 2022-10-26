ab6d54a0beb3c51bab4e4bf7055b7ad92d57ce6b
kangju2000 <kangju2000@naver.com>
2022-10-26 22:39:47 +0900

    feat: pageScore 함수 선언

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,20 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function pageScore(page) {
+  const result = page
+    .toString()
+    .split('')
+    .reduce(
+      (acc, cur) => {
+        acc[0] += Number(cur);
+        acc[1] *= Number(cur);
+        return acc;
+      },
+      [0, 1]
+    );
+
+  return Math.max(result[0], result[1]);
+}
+
 module.exports = problem1;
