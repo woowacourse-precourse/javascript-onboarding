938c77b1bcdc65b9fdad2700668a8d4b6a38bfad
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-26 23:11:57 +0900

    feat: check 3, 6, 9 number  in problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,12 @@
 function problem3(number) {
+  let clap = 0;
   for (let index = 0; index < number; index++) {
     const num = String(index + 1);
-    console.log(num);
+    clap += num
+      .split("")
+      .filter((x) => x === "3" || x === "6" || x === "9").length;
+  }
+  return clap;
 }
 
 module.exports = problem3;
