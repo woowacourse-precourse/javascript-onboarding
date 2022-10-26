4bb81ad15bfd141ae137c9bfef1b654b14836a27
김혜원 <dev.blair06@gmail.com>
2022-10-26 19:31:09 +0900

    feat#blair06: problem1 함수 작성 및 테스트 완료 - problem1 함수 작성 - calcPageNum 함수 작성 - test 실행

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,37 @@
 function problem1(pobi, crong) {
   var answer;
+  const pobiMAX = calcPageNum(pobi);
+  const crongMAX = calcPageNum(crong);
+
+  if (crongMAX === -1 || pobiMAX === -1) {
+    answer = -1;
+  } else if (crongMAX > pobiMAX) {
+    answer = 2;
+  } else if (crongMAX === pobiMAX) {
+    answer = 0;
+  } else if (pobiMAX > crongMAX) {
+    answer = 1;
+  }
   return answer;
+}
+function calcPageNum(arr) {
+  let maxNum = -1;
+  if (arr[0] + 1 !== arr[1]) {
+    return -1;
+  }
+
+  for (let page of arr) {
+    let sum = 0;
+    let mul = 1;
+    do {
+      sum += page % 10;
+      mul *= page % 10;
+      page = Math.floor(page / 10);
+    } while (page > 0);
+    maxNum = Math.max(maxNum, sum > mul ? sum : mul);
+
+    return maxNum;
+  }
 }
 
 module.exports = problem1;
