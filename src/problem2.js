0cb48099d5395fe9892c2c3b10558525c280331b
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-26 21:24:38 +0900

    feat: repeat until no repeated letters in problem 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,8 +1,15 @@
 function problem2(cryptogram) {
+  let cnt = 0;
   const arr = cryptogram.split("");
-  if (arr[cnt] === arr[cnt + 1]) {
-    arr.splice(cnt, 2);
+  while (cnt <= arr.length - 1) {
+    if (arr[cnt] === arr[cnt + 1]) {
+      arr.splice(cnt, 2);
+      cnt = cnt > 0 ? cnt - 3 : 0;
+    } else {
+      cnt += 1;
+    }
   }
+  return arr.join("");
 }
 
 module.exports = problem2;
