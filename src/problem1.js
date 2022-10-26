163a9ebd66a3b8c30bad0fbb1e45bdc451b3285b
CoboCho <rlaalsrb1111@naver.com>
2022-10-26 21:05:38 +0900

    feat: [problem1] 플레이어 점수 산정용 getScore() 구현 및 적용

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,19 @@
 function problem1(pobi, crong) {
   if(pobi[1] !== pobi[0] + 1 || crong[1] !== crong[0] + 1) return -1;
+  let pobiScore = getScore(pobi);
+  let crongScore = getScore(crong);
+}
+
+function getScore(pages) {
+  let score = 0;
+  pages.forEach(page => {
+    page = ('' + page).split('');
+    let multiplyAll = page.reduce((pre,cur) => +pre * +cur);
+    let plusAll = page.reduce((pre,cur) => +pre + +cur);
+    if(score < multiplyAll) score = multiplyAll;
+    if(score < plusAll) score = plusAll;
+  });
+  return score;
 }
 
 module.exports = problem1;
