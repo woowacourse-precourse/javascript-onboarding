77d1148fbdefef901ea6e5fe7540459e6df6152d
froggy1014 <clcl6084@gmail.com>
2022-10-26 22:49:06 +0900

    style[1]: 코드 별 주석 추가

--- src/problem1.js
+++ src/problem1.js
@@ -5,9 +5,12 @@ function problem1(pobi, crong) {
   const BOTHENDPAGES = [1, 2, 399, 400];
   const pobiMaxV = getMaxV(pobi);
   const crongMaxV = getMaxV(crong);
-
+  
+  // 입력값이 유효하지 않으면 -1 반환
+  // 두 최대값이 같으면 0 반환
+  // 두 최대값을 비교하여 1 or 2 반환
   if (isInvalidInput(pobi, crong)) return -1;
-  if(pobiMaxV === crongMaxV) return 0;
+  if (pobiMaxV === crongMaxV) return 0;
   switch (pobiMaxV > crongMaxV) {
     case true:
       return 1;
@@ -15,8 +18,9 @@ function problem1(pobi, crong) {
       return 2;
   }
 
-  function getMaxV(pobi){
-    const values = pobi.map((pageNum) => {
+  // 페이지값을 받아 최대값을 반환 메소드
+  function getMaxV(pages){
+    const values = pages.map((pageNum) => {
       let sum = String(pageNum).split('').reduce((a,b) => Number(a) + Number(b), 0)
       let mul = String(pageNum).split('').reduce((a,b) => Number(a) * Number(b))
       return Math.max(sum, mul)
@@ -24,6 +28,7 @@ function problem1(pobi, crong) {
     return Math.max(...values);
   }
 
+  // 입력값 검증 메소드 
   function isInvalidInput(pobi, crong) {
     const condition1 = hasBothEndsPage(pobi, crong);
     const condition2 = hasWrongLength(pobi, crong);
@@ -31,15 +36,18 @@ function problem1(pobi, crong) {
     return condition1 || condition2 || condition3;
   }
 
+  // 양끝 페이지가 들어있는지 확인 로직
   function hasBothEndsPage(pobiPage, crongPage) {
     return !!BOTHENDPAGES.filter((v) => (pobiPage.concat(crongPage).includes(v))).length;
   }
 
+  // 페이지 배열 길이가 2가 넘었는지 확인 로직
   function hasWrongLength(pobiPage, crongPage) {
     const TOTALPAGES = pobiPage.concat(crongPage);
     return TOTALPAGES.length !== LENGTHLIMIT * 2;
   }
 
+  // 홀, 짝수 및 페이지 순서 확인 로직
   function hasWrongPage(pobi, crong) {
     const rightOrder = 2 !== (pobi[RIGHTPAGE] - pobi[LEFTPAGE]) + (crong[RIGHTPAGE] - crong[LEFTPAGE]);
     const evenCheck = 0 !== pobi[RIGHTPAGE]%2 + crong[RIGHTPAGE]%2;
@@ -47,4 +55,4 @@ function problem1(pobi, crong) {
   }
 }
 
-// module.exports = problem1;
+module.exports = problem1;
