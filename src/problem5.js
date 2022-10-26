7488e83c63aef81beb954f02650ed4829e6fdf07
saejinpark <saejinpark951210@gmail.com>
2022-10-26 17:04:56 +0900

    문제5 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
+const MONEY_UNIT_ARY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
+  let answer = Array.from({ length: MONEY_UNIT_ARY.length }, () => 0);
+  let remain = money;
+  MONEY_UNIT_ARY.map((unit, i) => {
+    answer[i] = parseInt(remain / unit);
+    remain = remain % unit;
+  });
   return answer;
 }
 
