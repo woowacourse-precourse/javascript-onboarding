efba155224a6aa5f23264792c30ceffaf1c802cc
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 20:54:53 +0900

    feat: 1번 예외사항 처리 추가
    - 페이지 값이 존재 안하는 경우 (null or undefined)
    - 페이지가 처음이거나 마지막인 경우

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,10 @@
 function problem1(pobi, crong) {
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
   if (pobi[0] % 2 === 0 || crong[0] % 2 === 0) return -1;
+  if (pobi[0] === 399 || pobi[0] === 1 || crong[0] === 399 || crong[0] === 1) {
+    return -1;
+  }
+  if (!pobi[0] || !pobi[1] || !crong[0] || !crong[1]) return -1;
   function getMaxNumber(num) {
     let arr = String(num).split("");
     let plus = arr.reduce((a, c) => {
