5b7c41263b78c12a95078fe7dc93e7d4a2788a93
Circlewee <pnj1118@naver.com>
2022-10-26 17:41:16 +0900

    [#1]예외 case에서 -1을 반환

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,28 @@
+function addPageResult(numArr) {}
+
+function mulPageResult(numArr) {}
+
+function getScore(pageNumArr) {}
+
+function isExceptionCase(pageNumArr) {
+  if (pageNumArr[0] >= pageNumArr[1]) {
+    return true;
+  }
+
+  if (pageNumArr[0] + 1 !== pageNumArr[1]) {
+    return true;
+  }
+
+  return false;
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  let answer = -1;
+
+  if (isExceptionCase(pobi) || isExceptionCase(crong)) {
+    return answer;
+  }
+
   return answer;
 }
 
