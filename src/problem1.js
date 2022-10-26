0d89749c60feed4c57f52fdb9b7eb8489d78f853
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-10-26 17:12:37 +0900

    [FEAT] Solve prolbem1
    문제1 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,20 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if ((pobi[1] - pobi[0] > 1) || (crong[1] - crong[0] > 1)) return -1;
+
+  const maxPobi = Math.max.apply(null, pobi.map(v => getMax(v)));
+  const maxCrong = Math.max.apply(null, crong.map(v => getMax(v)));
+
+  if (maxPobi > maxCrong) return 1
+  else if (maxPobi < maxCrong) return 2
+  else return 0
+}
+
+function getMax(v) {
+  const add = String(v).split('').reduce((prev, curr) => +prev + +curr, 0);
+  const multiply = String(v).split('').reduce((prev, curr) => +prev * +curr, 1);
+  const max = Math.max(add, multiply);
+
+  return max;
 }
 
 module.exports = problem1;
