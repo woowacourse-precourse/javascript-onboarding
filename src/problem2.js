b382aefc8da0072d82eaa0bb829fd34a47953f09
bassyu <yrkdgus@gmail.com>
2022-10-26 17:46:11 +0900

    문제2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let encoded = cryptogram;
+  while (true) {
+    const newEncoded = encoding(encoded);
+    if (newEncoded === encoded) {
+      break;
+    }
+    encoded = newEncoded;
+  }
+  return encoded;
+}
+
+function encoding(encoded) {
+  let newEncoded = encoded;
+  for (let i = 1; i < encoded.length; i++) {
+    if (encoded[i-1] == encoded[i]) {
+      newEncoded = encoded.slice(0, i-1) + encoded.slice(i+1);
+      break;
+    }
+  }
+  return newEncoded
 }
 
 module.exports = problem2;
