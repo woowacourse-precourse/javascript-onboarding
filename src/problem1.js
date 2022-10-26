f9caaa30937ef3da39015a3e7d34c56f588cafed
sehwan <tpghks9245@gmail.com>
2022-10-26 22:28:37 +0900

    problem1 digitSum

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,25 @@
 function problem1(pobi, crong) {
   var answer;
+
   return answer;
 }
 
+function digitSum(...Arr) {
+  let firstArr = Arr[0][0].toString().split("").map(x => parseInt(x));
+  const firstNum = firstArr.reduce((a,b) => (a+b));
+
+  let secondArr = Arr[0][1].toString().split("").map(x => parseInt(x));
+  const secondNum = secondArr.reduce((a,b) => (a+b));
+
+  return Math.max(firstNum, secondNum);
+}
+
+
+problem1([97, 98], [111, 112]);
+
 module.exports = problem1;
+
+// 기능들
+// 1. 예외상황(첫장 끝장, 페이지수가 이상할때)
+// 2. 각 자리수 나눠서 더하기
+// 3. 각 자리수 나눠서 곱하기
