f7c5e42bbbe3ef8efd1c13be0f4ca6c9faa42e34
Jiwoo Park <jiwoo9446@naver.com>
2022-10-26 20:17:15 +0900

    test case solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram;
+  let duplicated = false;
+  while (true) {
+    duplicated = false;
+    let n = answer.length;
+    let res = "";
+    for (let i = 0; i < n; i++) {
+      if (answer[i] == answer[i + 1]) {
+        duplicated = true;
+        let cnt = 0;
+        while (answer[i] == answer[i + cnt]) cnt++;
+        i += cnt - 1;
+      } else res += answer[i];
+    }
+    answer = res;
+    if (!duplicated) break;
+  }
   return answer;
 }
-
+console.log(problem2("zyelleyz"));
 module.exports = problem2;
