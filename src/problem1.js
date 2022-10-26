45499dc2fb4faa32e6ba35f9bfff168ff96700fb
Suyeon <msjgr2801@gmail.com>
2022-10-26 17:08:54 +0900

    feat: Write Problem1's function lists

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,12 @@
+// 0. 예외 처리
+//  0.1 [ ] (2 ~ 398) 사이 숫자일 것
+
+// 1. 로직
+//  1.1 [ ] pobi와 crong이 가질 수 있는 가장 큰 수 구하기
+//  1.2 [ ] pobi와 crong의 숫자를 비교하기
 function problem1(pobi, crong) {
-  var answer;
+  let answer = null;
+
   return answer;
 }
 
