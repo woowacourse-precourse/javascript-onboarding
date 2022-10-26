d25bc97ed7cea8c09c11c29fdc192c9e266fc2a0
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-26 21:42:54 +0900

    bug(Remove While): while문 탈출 조건 미작성으로 인한 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,11 +1,8 @@
 function problem3(number) {
   let count = 0;
   for (let i = 2; i <= number; i++) {
-    let num = i;
-    while (num > 2) {
-      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
-        count++;
-      }
+    if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+      count++;
     }
   }
 
