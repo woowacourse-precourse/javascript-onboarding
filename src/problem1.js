31e4c6d91a3d7118a80ea4884792c00303bf2715
moonelysian <dandyblack@naver.com>
2022-10-26 16:50:12 +0900

    add: input 값이 조건에 맞게 들어왔는지 확인하는 함수 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,24 @@
+function isValid(pages) {
+  let [left, right] = pages;
+  // 왼쪽 페이지는 홀수
+  if (left % 2 !== 1) {
+    return false;
+  }
+  // 오른쪽 페이지는 짝수
+  if (right % 2 !== 0) {
+    return false;
+  }
+  if (Math.abs(right - left) > 1) {
+    return false;
+  }
+  return true;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if (!isValid(pobi) || !isValid(crong)) {
+    return -1;
+  }
   return answer;
 }
 
