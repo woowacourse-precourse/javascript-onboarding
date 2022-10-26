2ce8aa40868123522bd569c5f51d7e0c2c675418
froggy1014 <clcl6084@gmail.com>
2022-10-26 22:37:56 +0900

    feat[1]: 두 최댓값을 비교하는 스위치문과 if문 추가

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,14 @@ function problem1(pobi, crong) {
   let pobiMaxV = getMaxV(pobi);
   let crongMaxV = getMaxV(crong);
 
+  if(pobiMaxV === crongMaxV) return 0;
+  switch (pobiMaxV > crongMaxV) {
+    case true:
+      return 1;
+    case false:
+      return 2;
+  }
+
   function getMaxV(pobi){
     const values = pobi.map((pageNum) => {
       let sum = String(pageNum).split('').reduce((a,b) => Number(a) + Number(b), 0)
@@ -11,8 +19,7 @@ function problem1(pobi, crong) {
     return Math.max(...values);
   }
 
-  console.log(pobiMaxV, crongMaxV);
 }
-problem1([97, 98], [199, 200]);
+console.log(problem1([97, 98], [199, 200]));
 
 // module.exports = problem1;
