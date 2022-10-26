7025c2c5506fb658bd5660795bf3dba47065ab7d
lee-il-woo <empty001430@gmail.com>
2022-10-26 17:57:17 +0900

    페이지의 숫자를 더하고 곱하기 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,39 @@
 
+//페이지 숫자의 합
+function sumScore(score){
+  let sum_score = 0;
+  score.map((element)=>{
+  sum_score = sum_score + element
+  })
+  return sum_score;
+}
+
+function mulScore(score){
+  let mul_score = 1;
+  score.map((element)=>{
+    mul_score = mul_score * element
+  })
+  return mul_score;
+}
+
+//pobi나 crong을 받아 점수차이를 계산하는 함수 
+function scoreDiff(page){
+  let first_score = Array.from(String(page[0]));
+  first_score = first_score.map((element)=>Number(element))
+  let second_score = Array.from(String(page[1]));
+  second_score = second_score.map((element)=>Number(element))
+  //페이지 숫자를 배열로 변환 ex) 97 => [9,7]
+
+  first_sum = sumScore(first_score)
+  second_sum = sumScore(second_score)
+  first_mul = mulScore(first_score)
+  second_mul = mulScore(second_score)
+  return [first_mul,second_mul];
+}
+
+console.log(scoreDiff([197,198]))
+
+
 function problem1(pobi, crong) {
   var answer = -1;
   //예외사항 if문으로 제외하기
@@ -9,7 +44,5 @@ function problem1(pobi, crong) {
   }else if(pobi.length != 2 && crong.length != 2){
     return answer;
   }
-  ㅁㅁㅁ
 }
-
 module.exports = problem1;
