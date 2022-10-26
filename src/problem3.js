a18fae28e5117e7a5fb4e92e911c8bdc8b328145
yeong bin <2976622@naver.com>
2022-10-27 00:13:26 +0900

    chore: problem3 - checkValidation 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,15 +1,30 @@
-function countOf369(number) {
-  return number
+/**
+ * Check for valid value
+ * @param {number} num
+ * @returns {boolean}
+ */
+ function checkValidation(num) {
+  if (Number.isInteger(num) && num > 0 && num <= 10000)
+    return true;
+  return false;
+}
+
+/**
+ * 
+ * @param {number} num
+ * @returns {number} count of 3, 6, 9
+ */
+function countOf369(num) {
+  return num
     .toString()
     .split("")
     .reduce((acc, cur) => {
-      const num = parseInt(cur);
-      return acc + (num % 3 === 0 && num !== 0 ? 1 : 0);
+      const n = parseInt(cur);
+      return acc + (n % 3 === 0 && n !== 0 ? 1 : 0);
     }, 0);
 }
 
-function problem3(number) {
-  return number;
+function problem3(num) {
 }
 
 module.exports = problem3;
