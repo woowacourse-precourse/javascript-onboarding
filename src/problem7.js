d15eacdf96d60f91dc12b2faf7d91facdc219658
Jongco <narira0922@gmail.com>
2022-10-27 02:02:42 +0900

    problem7: 이미 친구인 사람 + 본인 배열 생성

--- src/problem7.js
+++ src/problem7.js
@@ -6,8 +6,22 @@ function problem7(user, friends, visitors) {
    * 4. visitor도 똑같이 세어서 객체로 생성 {name: 'jongco', friend: 2, visit: 1, score: 21}
    * 5. 점수 별로 나열 후 score가 0인 것 제외해서 배열에 이름 반환
    */
-  var answer;
-  return answer;
+  const alreadyFriendList = [...new Set(friends.filter((friend) => friend.includes(user)).flat())];
+  return alreadyFriendList;
 }
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 
 module.exports = problem7;
