ecf50ebb294619ff9d30740dc4c8cdd03399b39a
MyungAe <ladiescheong@naver.com>
2022-10-27 02:39:02 +0900

    feat : 기능 3번 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,36 @@
 function problem3(number) {
-  var answer;
+  let array = [];
+  let answer = 0;
+
+  for (let i = 1; i < number + 1; i++) {
+    let targetNumber = i;
+    while (targetNumber) {
+      if (targetNumber % 10 !== 0) array.push(targetNumber % 10);
+      targetNumber = Math.floor(targetNumber / 10);
+    }
+    console.log(array);
+    array.forEach((value) => {
+      if (value !== 0 && value % 3 === 0) answer++;
+    });
+    array = [];
+  }
+
+  console.log(answer);
   return answer;
 }
 
+problem3(13);
+problem3(33);
+
 module.exports = problem3;
+
+/*
+  describe("problem3", () => {
+    test("case1", () => {
+      expect(problem3(13)).toEqual(4);
+    });
+    test("case2", () => {
+      expect(problem3(33)).toEqual(14);
+    });
+  });
+*/
