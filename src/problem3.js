ee5a8543c9296dc07780dd516df099897a35c2d9
c0c0pang <coc1789@naver.com>
2022-10-26 19:22:46 +0900

    문제 3 구현.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  let num=0;
+    for(let i=1;i<=number;i++){
+        num=i;
+        while(num!==0){
+          if(num%10===3 || num%10===6 || num%10===9){
+              answer++;
+              num = Math.floor(num/10);
+          }
+          else{
+              num = Math.floor(num/10);
+          }
+      }
+}
   return answer;
 }
 
