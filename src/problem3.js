e09ec4bfa099c302fc406aef549ced03145195dc
h2002y <h2002y@naver.com>
2022-10-26 19:02:19 +0900

    feat: Add main func
    - problem3의 메인 함수 작성
    - for문을 한번 돌려 누적합을 구하고 정답 리턴

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
   var answer;
+  let prefixSum = [];
+  prefixSum[0] = 0;
+  for(let i = 1; i <= number; i++){
+    prefixSum[i] = prefixSum[i-1] + getClap(i);
+  }
+  answer = prefixSum[number];
   return answer;
 }
 
