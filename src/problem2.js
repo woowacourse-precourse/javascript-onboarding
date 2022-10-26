6f72ab2720ad343182e529630bcc0965342b1c03
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-27 04:30:29 +0900

    feat: [problem2] Add problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arrayCryptogram = [...cryptogram];
+  let letter = "";
+
+  for (let i = 0; i < arrayCryptogram.length; i++) {
+    if (letter == arrayCryptogram[i]) {
+      arrayCryptogram.splice(i - 1, 2);
+
+      i = 0;
+      letter = "";
+    }
+    letter = arrayCryptogram[i];
+  }
+  return arrayCryptogram.join("");
 }
 
 module.exports = problem2;
