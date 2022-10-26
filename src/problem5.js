e8141b6e5cd7e09b048ef2747050c6c3fb40d000
yeongi <cbg5255@gmail.com>
2022-10-26 21:34:55 +0900

    문제 5번 구현 완료 및 테스트 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,19 @@
 function problem5(money) {
-  var answer;
+  //오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+  const digitArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for (const i of digitArr) {
+    answer.push(digitMoney(money, i));
+    money %= i;
+  }
+
   return answer;
 }
 
-//오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+// 자리당 몫을 계산하는 함수
+const digitMoney = (money, digit) => {
+  return Math.floor(money / digit);
+};
 
 module.exports = problem5;
