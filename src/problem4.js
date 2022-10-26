3485948267632bf113b822a5a376e7623a576c4c
Suyeon <msjgr2801@gmail.com>
2022-10-26 17:10:31 +0900

    feat: Write Problem4's function lists

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,14 @@
+// 0. 예외처리
+//  0.1 [ ] word.length <= 0 : return answer
+
+// 1. 로직
+//  1.1 [ ] 알파벳에 해당하는 map을 모두 만들어 대응
+
+// 2. 대문자 / 소문자
+//  2.1 [ ] 각 문자마다 대소문자 판별하기 or 대문자 map, 소문자 map 따로 만들기 
 function problem4(word) {
-  var answer;
+  let answer = '';
+  
   return answer;
 }
 
