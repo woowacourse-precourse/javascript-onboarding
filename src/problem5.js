d0025ac8e07c9fe827df448fad95c5ed78165b36
Jongco <narira0922@gmail.com>
2022-10-26 21:17:48 +0900

    problem5: 화폐 계산기 기능 구현 목록 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,4 +1,9 @@
 function problem5(money) {
+  /**
+   * 1. [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]로 구성된 배열 생성
+   * 2. answer = money로 설정한 후 위에 생성한 monetaryUnit로 map 함수를 돌려 몫은 맵핑하고 answer = 나머지로 설정
+   * 3. 2번 과정 끝까지 반복
+   */
   var answer;
   return answer;
 }
