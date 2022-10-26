9da3a5d1b6b8f9d16a0da60657b4dadca9d95d90
Hwang Seong Taek <hst007@naver.com>
2022-10-26 18:31:00 +0900

    feat: problem2 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -6,8 +6,25 @@
 */
 
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+  // 문자열 분리
+  const cryptoArr = cryptogram.split('');
+  const result = [];
+
+  /* 초기 문자 push(), result 배열의 최상단 값과 입력 값 비교 같으면(연속된 중복 문자)
+     result.pop(), 중복이 아니면 해당 문자 push() */
+  for (let i=0; i<cryptoArr.length; i++) {
+    if (!result.length) {
+      result.push(cryptoArr[i]);
+    } else if (result[result.length-1] === cryptoArr[i]) {
+      result.pop()
+    } else if (result[result.length-1] !== cryptoArr[i]) {
+      result.push(cryptoArr[i]);
+    }
+  }
+
+  // 배열을 하나의 문자열로 return
+  return result.join('');
 }
 
 module.exports = problem2;
