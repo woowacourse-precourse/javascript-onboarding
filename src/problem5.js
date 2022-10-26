a21f7c5537cfbbe76e519d69bb7877cfe0ea43a0
nlom0218 <nlom0218@naver.com>
2022-10-26 21:35:42 +0900

    feat: 문제 5 - 화폐 단위에 따라 최대로 출금할 수 있는 금액과 화폐의 개수를 반환하는 함수 만들기

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,10 @@
+const UNIT_OF_MONEY = [50000, 10000, 5000, 1000, 500, 100, 10, 5, 1];
+
+const takeMoneyOut = (money, unit) => {
+  const count = Math.floor(money / unit);
+  return [count * unit, count];
+};
+
 function problem5(money) {
   var answer;
   return answer;
