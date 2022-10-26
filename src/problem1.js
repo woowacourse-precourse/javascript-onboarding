55df6348f45a3d1c7ed5e63f6aa30dcb2fed8744
sungjihyun <jhsung23@gmail.com>
2022-10-26 17:14:00 +0900

    Feat: problem 1: 예외 처리 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -19,9 +19,27 @@ const calcMul = (num) => {
   return result;
 };
 
+// 테스트 값의 예외사항 체크
+const checkExcep = (arr) => {
+  let left = arr[0];
+  let right = arr[1];
+
+  // 왼쪽 페이지 < 오른쪽 페이지 (두 값의 차이가 1인지)
+  if (right - left === 1) {
+    if (left % 2 !== 1) return false; // 왼쪽 페이지가 홀수값이 아니라면
+  } else return false; // 두 페이지 값의 차이가 1이 아니라면
+
+  // 페이지의 값이 범위 내에 있는지 검사
+  if (right >= 2 && right <= 400 && left >= 1 && left <= 399) return true;
+  return false; // 범위 밖이라면
+};
+
 function problem1(pobi, crong) {
   var answer;
   let scorePobi, scoreCrong;
+
+  // pobi와 crong의 페이지 값이 올바른지 검사
+  if (!checkExcep(pobi) || !checkExcep(crong)) return ERR;
 
   scorePobi = Math.max(
     calcSum(pobi[0]),
