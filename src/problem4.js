a58b0e2df9956dbe981b042b92877fa24aad50ae
CoboCho <rlaalsrb1111@naver.com>
2022-10-26 23:11:29 +0900

    feat: [problem4] 아스키코드 활용 결과값 산출 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answer;
+  answer = word.split('').map(letter => {
+    if(/[A-Z]/.test(letter)) return String.fromCharCode(90 - (letter.charCodeAt(0) - 65));
+    if(/[a-z]/.test(letter)) return String.fromCharCode(122 - (letter.charCodeAt(0) - 97));
+    return letter;
+  });
+  return answer.join('');
 }
 
 module.exports = problem4;
