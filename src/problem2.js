0c2775c34d939e4c7a96c27979841fe9409b1428
jun <ggprgrkjh@naver.com>
2022-10-27 00:51:42 +0900

    docs: 기능 목록 업데이트 P2

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,10 @@
+// 기능 요구 사항
+
+// - [] 2개 이상 잘라내는 정규식 patt 생성
+// - [] 정규식이 true일시 while문 유지
+// - [] while문이 유지될 때 2개 이상 연속 문자를 잘라내기
+// - [] 정규식이 false일시 answer값 return
+
 function problem2(cryptogram) {
   var answer;
   return answer;
