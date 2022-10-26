68a2d9395da25653134e0824cd45a865d020bd3e
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-26 15:38:44 +0900

    problem1 Multi function 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,8 @@
 function problem1(pobi, crong) {
   var pobiadd = Add(pobi[1]);
   var crongadd = Add(crong[1]);
+  var pobimulti = Multi(pobi[1]);
+  var crongmulti = Multi(crong[1]);
   var answer;
   return answer;
 }
@@ -13,5 +15,12 @@ function Add(n){
 
   return sum;
 }
-
+function Multi(n){
+  var sum = 1;
+  do{
+    sum *= n%10;
+    n = Math.floor(n/10);
+  }while(n > 0);
+  return sum;
+}
 module.exports = problem1;
