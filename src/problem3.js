8ba1ee07b658ece1e9e51fc402be7103c0fe92c6
Tory <88531407+1two13@users.noreply.github.com>
2022-10-26 21:46:36 +0900

    feat(problem3): 3, 6, 9가 들어가는 횟수 구하는 코드 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
+// 기능 목록
+// 1. 3, 6, 9가 들어가는 숫자는 손뼉 치기
+
 function problem3(number) {
-  var answer;
+  // 손벽 치는 횟수
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let arr = String(i).split("");
+
+    for (let j = 0; j < arr.length; j++) {
+      if (arr[j] === "3" || arr[j] === "6" || arr[j] === "9") answer++;
+    }
+  }
+
   return answer;
 }
 
