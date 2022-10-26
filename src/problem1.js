da869fa49f3167f7650c5e4bf456d29e4368ccbc
olo90632951 <olo90632951@gmail.com>
2022-10-26 20:49:53 +0900

    feat: 입력값 예외 처리 기능 추가 [Problem1-5]

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,8 @@
 function problem1(pobi, crong) {
-  return getWinner(pobi, crong);
+  if (validPages(pobi) && validPages(crong)) {
+    return getWinner(pobi, crong);
+  }
+  return -1;
 }
 
 const getNumberArray = (number) => {
@@ -34,6 +37,25 @@ const getWinner = (pobi, crong) => {
     winner = 2;
   }
   return winner;
+};
+
+const validPages = (pages) => {
+  if (typeof pages[0] !== 'number' || typeof pages[1] !== 'number') {
+    return false;
+  }
+  if (pages.length !== 2) {
+    return false;
+  }
+  if (pages[0] % 2 !== 1 || pages[1] % 2 !== 0) {
+    return false;
+  }
+  if (pages[0] + 1 !== pages[1]) {
+    return false;
+  }
+  if (pages[0] < 3 || pages[0] > 398 || pages[1] < 3 || pages[1] > 398) {
+    return false;
+  }
+  return true;
 };
 
 module.exports = problem1;
