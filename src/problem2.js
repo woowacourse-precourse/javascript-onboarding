a1d5db1f5efdb81970fbf11bd2e0981872ba740e
pakxe <pigkill40@naver.com>
2022-10-27 03:28:20 +0900

    feat: [P2] problem2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const str = cryptogram.toString().split("");
+  const stack = [];
+  let duplicatedChar = "";
+
+  str.forEach((cur, i) => {
+    if (duplicatedChar === cur) return;
+    else if (i === 0 || stack[stack.length - 1] !== cur) stack.push(cur);
+    else if (stack[stack.length - 1] === cur) {
+      duplicatedChar = stack.pop();
+    }
+  });
+
+  return stack.join("");
 }
 
 module.exports = problem2;
