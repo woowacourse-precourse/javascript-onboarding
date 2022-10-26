9dab8dbbf2b7f9225b48cdf14e8299dd9a243951
gilpop8663 <wolfye@naver.com>
2022-10-27 04:44:21 +0900

    Add:[P4]_문자를 청개구리 사전을 참고해 반대로 변환하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,41 @@
+function isUpperCase(string) {
+  return /^[A-Z]*$/.test(string);
+}
+
+function isLowerCase(string) {
+  return /^[a-z]*$/.test(string);
+}
+
 function problem4(word) {
-  var answer;
+  var answer = "";
+  let upperArr = [];
+  let lowerArr = [];
+
+  for (let i = 65; i <= 90; i++) {
+    upperArr.push(String.fromCharCode(i));
+  }
+
+  for (let i = 97; i <= 122; i++) {
+    lowerArr.push(String.fromCharCode(i));
+  }
+
+  for (let i = 0; i < word.length; i++) {
+    let origin = 0;
+
+    if (word[i] == " ") {
+      answer += " ";
+      continue;
+    }
+
+    if (isUpperCase(word[i])) {
+      origin = word[i].charCodeAt() - 65;
+      answer += upperArr[25 - origin];
+    } else if (isLowerCase(word[i])) {
+      origin = word[i].charCodeAt() - 97;
+      answer += lowerArr[25 - origin];
+    }
+  }
+
   return answer;
 }
 
