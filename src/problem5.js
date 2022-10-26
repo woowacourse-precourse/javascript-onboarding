bd48f231b097ac84bae812576b298afe6a2027d7
knh6269 <knh6269@naver.com>
2022-10-27 00:57:34 +0900

    미션5 완성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,23 @@
 function problem5(money) {
-  var answer;
+  //답 배열
+  let answer=[];
+  //주어진 돈을 금액 단위별로 계산해주는 함수
+  const sample = (count) =>{
+      answer.push(Math.floor(money/count));
+      money=money%count;
+  }
+  
+  //금액 단위별로 계산
+  sample(50000);
+  sample(10000);
+  sample(5000);
+  sample(1000);
+  sample(500);
+  sample(100);
+  sample(50);
+  sample(10);
+  sample(1);
+  
   return answer;
 }
 
