b46f5bfd8befbd680de39b7982241bc55df64750
olo90632951 <olo90632951@gmail.com>
2022-10-27 02:34:25 +0900

    feat: 주어진 문자열 한 단어씩 변환하는 기능 추가 [Problem4-1]

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  return replaceWord(word);
 }
+
+const replaceWord = (word) => {
+  const result = [];
+  for (let i = 0; i < word.length; i++) {
+    let asciiCode = word.charCodeAt(i);
+    if (65 <= asciiCode && asciiCode <= 90) {
+      asciiCode = 65 + 90 - asciiCode;
+    } else if (97 <= asciiCode && asciiCode <= 122) {
+      asciiCode = 97 + 122 - asciiCode;
+    }
+    result.push(String.fromCharCode(asciiCode));
+  }
+  return result.join('');
+};
 
 module.exports = problem4;
