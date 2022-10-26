0235f8b4b555daba67cb3ac12f39a50d516cf770
ReturnReason <reasonz@daum.net>
2022-10-26 19:28:22 +0900

    feat: problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  var answer = [];
+  const word = cryptogram.split('');
+
+  for (let i = 0; i < word.length; i++) {
+    if (i === 0) {
+      answer.push(word[i]);
+    } else {
+      if (answer[answer.length - 1] !== word[i]) {
+        answer.push(word[i]);
+      } else {
+        answer.pop();
+      }
+    }
+  }
+
+  return answer.join('');
 }
 
 module.exports = problem2;
