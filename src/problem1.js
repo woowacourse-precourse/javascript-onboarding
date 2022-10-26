6cf3ab4d8391962681af032f05b86715dfdc5e32
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-26 20:50:05 +0900

    feat(Function): 배열 속 값을 연산하는 함수 및 연산 결과 비교 기능추가(기능목록 2번 구현)

--- src/problem1.js
+++ src/problem1.js
@@ -2,12 +2,27 @@ function problem1(pobi, crong) {
   let answer = 0;
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
 
+  function check(num) {
+    let local = num.split("");
+    let sum = 0,
+      div = 1;
+    for (let x of local) {
+      sum += Number(x);
+      div *= Number(x);
+    }
+    return sum > div ? sum : div;
+  }
+
   for (let i of pobi) {
     if (i < 3 || i > 398) return -1;
+    let num = check(String(i));
+    num > pobimax ? (pobimax = num) : num;
   }
 
   for (let i of crong) {
     if (i < 3 || i > 398) return -1;
+    let num = check(String(i));
+    num > crongmax ? (crongmax = num) : num;
   }
 
   return answer;
