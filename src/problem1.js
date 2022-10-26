ff9861ed2ca9ef995a163817521f8c66ecb6b75f
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-10-27 00:07:08 +0900

    점수 확인하기 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -16,6 +16,31 @@ function problem1(pobi, crong) {
         return biggestNumber;
     }
 
+    // 2. 점수 구하기
+    // - pobi가 이기면 1, crong이 이기면 2, 무승부는 0
+    // - 왼쪽 페이지가 홀수가 아니거나, 오른쪽 페이지가 왼쪽 페이지 +1이 아니거나, 시작 면 또는 마지막 면이라면 -1
+    function checkPages(arr) {
+        let result;
+        if (
+            arr[0] % 2 === 1 &&
+            arr[1] === arr[0] + 1 &&
+            arr[0] !== 1 &&
+            arr[1] !== 400
+        ) {
+            result = true;
+        } else result = false;
+        return result;
+    }
+    if (checkPages(pobi) === false || checkPages(crong) === false)
+        return (answer = -1);
+    if (getBiggestNumber(pobi) > getBiggestNumber(crong)) {
+        answer = 1;
+    } else if (getBiggestNumber(pobi) < getBiggestNumber(crong)) {
+        answer = 2;
+    } else if (getBiggestNumber(pobi) === getBiggestNumber(crong)) {
+        answer = 0;
+    } else answer = -1;
+
     return answer;
 }
 
