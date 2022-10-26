cb9c72aa8b62a70ebc4814dc02828f60c69d9bcc
nlom0218 <nlom0218@naver.com>
2022-10-26 23:57:44 +0900

    feat: 문제 7 - 친구 추천 점수를 관리하는 를 만들고 점수 저장하기(키: user, 값: 점수), getKnowEachOtherSocre 함수 반환 값을 점수로 만들기

--- src/problem7.js
+++ src/problem7.js
@@ -3,20 +3,32 @@ const setUserMap = (userMap, [userA, userB]) => {
   userMap.set(userB, [...(userMap.get(userB) || []), userA]);
 };
 
-const getKnowEachOtherScore = (userMap, userA, userB) => {
-  const userAFriends = userMap.get(userA);
-  const userBFriends = userMap.get(userB);
-
+const getKnowEachOtherScore = (userAFriends, userBFriends) => {
   let count = 0;
   userAFriends.forEach((user) => {
     if (userBFriends.includes(user)) count++;
   });
+
   return count * 10;
+};
+
+const setUserScoreMap = (map, user, score) => {
+  map.set(user, (map.get(user) | 0) + score);
 };
 
 function problem7(user, friends, visitors) {
   const userMap = new Map();
   friends.forEach((item) => setUserMap(userMap, item));
+
+  const userScoreMap = new Map();
+  userMap.forEach((value, key) => {
+    if (key === user) return;
+    setUserScoreMap(
+      userScoreMap,
+      key,
+      getKnowEachOtherScore(userMap.get(user), value)
+    );
+  });
 }
 
 module.exports = problem7;
