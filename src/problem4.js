18830cd6cbe09b1a9e16b5013dac7dcdf95a9c4c
박준서 <oponize@naver.com>
2022-10-27 01:17:19 +0900

    feat: problem4/대소문자 판별 함수

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,16 @@ function problem4(word) {
   return answer;
 }
 
+function isUpperCase(char) {
+  const ascii = char.charCodeAt();
+  if (ascii >= 65 && ascii <= 90) return true;
+  return false;
+}
+
+function isLowerCase(char) {
+  const ascii = char.charCodeAt();
+  if (ascii >= 97 && ascii <= 122) return true;
+  return false;
+}
+
 module.exports = problem4;
