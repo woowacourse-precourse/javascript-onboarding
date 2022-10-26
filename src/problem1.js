9df28bc87a903cfc3d193273ac3da9078dfbeb0a
An Sol <apdlvmf486@gmail.com>
2022-10-26 22:59:04 +0900

    최댓값을 찾아주는 함수 findBiggest 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,18 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer = 0;
+  function findBiggest(arr) {
+    let biggest = 0;
+    const numToString = arr.map(x => x.toString().split(''))
+    for (let numbers of numToString) {
+      const stringToNum = numbers.map(x => parseInt(x))
+      const summation = stringToNum.reduce((prevValue, currValue) => prevValue + currValue);
+      const multiplication = stringToNum.reduce((prevValue, currValue) => prevValue * currValue);
+      biggest = Math.max(summation, multiplication);
+    }
+    return biggest;
+  }
+
+
   return answer;
 }
 
