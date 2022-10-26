8be05b87e938258353785328e74792942f14f646
100Gyeon <bjy9966@naver.com>
2022-10-26 17:30:02 +0900

    feat: 3, 6, 9 숫자 있을 때 손뼉 치는 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  const condition = /3|6|9/g;
+
+  for (let i = 1; i <= number; i++) {
+    answer += (String(i).match(condition) || []).length;
+  }
+
   return answer;
 }
 
