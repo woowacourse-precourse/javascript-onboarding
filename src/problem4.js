48bb742d286c7ce2c77edcc5de64d2be82b4f81c
pakxe <pigkill40@naver.com>
2022-10-27 05:26:15 +0900

    feat: [P4] 주어진 문자가 알파벳인지 확인하는 isAlphabet 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
+const ALPHABET = /^[a-z|A-Z]+$/;
+
 function problem4(word) {
   var answer;
   return answer;
+}
+
+function isAlphabet(char) {
+  if (ALPHABET.test(char)) return true;
+  else return false;
 }
 
 module.exports = problem4;
