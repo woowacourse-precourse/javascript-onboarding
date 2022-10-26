a5a4e6e684aa416f49c7324d7de18983cd6a67b4
nerdchanii <nerdchanii@gmail.com>
2022-10-27 04:27:31 +0900

    feat: [PROBLEM1] 승부결과 반환로직 추가
    - 가장 추상적인 로직
    
    linked #1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,32 @@
+/**
+ * 포비와 크롱 승자구하기
+ *
+ * @param {number[]} pobi
+ * @param {number[]} crong
+ * @returns {number} winner
+ */
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+
+  /** main logic */
+  let pobiPoints = getPoints(pobi);
+  let crongPoints = getPoints(crong);
+
+  if (pobiPoints === crongPoints) return 0;
+  if (pobiPoints > crongPoints) return 1;
+  else return 2;
+
+  /**
+   * - 유저 포인트 계산
+   * @param {[Number, Number]} numbers 
+   * @return {number} points
+   */
+  function getPoints(numbers) {
+    return 0;
+  }
+
+
+
 }
 
 module.exports = problem1;
