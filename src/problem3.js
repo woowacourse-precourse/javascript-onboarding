47349fa992446e20ed2b0749f53696bd97b4a485
Gyeongm1n <affirming_km@naver.com>
2022-10-26 22:29:55 +0900

    숫자의 3, 6, 9 개수 구하기 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
+function getCount(str) {
+  const arr = str.split('').map(Number);
+  const cnt = arr.filter((num) => num === 3 || num === 6 || num === 9).length;
+  return cnt;
+}
+
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let target = 1; target <= number; target++) {
+    answer += getCount(String(target));
+  }
+
   return answer;
 }
+
+console.log(problem3(33));
 
 module.exports = problem3;
