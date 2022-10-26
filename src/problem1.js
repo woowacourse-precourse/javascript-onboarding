48db8eb3edae91d9cc6c999f13824f9aeb6fa8d3
oseunghyun <ohsh1477@gmail.com>
2022-10-26 23:07:43 +0900

    feat(p1): 각 자릿수들의 곱을 구하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,6 +3,25 @@ const sumDigit = (num) => {
   return parseInt(num / 100) + parseInt((num % 100) / 10) + parseInt(num % 10);
 };
 
+// 각 자릿수들의 곱을 구하는 기능
+const multiplyDigit = (num) => {
+  if (parseInt(num / 100) == 0) {
+    if (parseInt((num % 100) / 10) == 0) {
+      return parseInt(num % 10);
+    } else {
+      return parseInt((num % 100) / 10) * parseInt(num % 10);
+    }
+  } else {
+    if (parseInt((num % 100) / 10) == 0) {
+      return parseInt(num / 100) * parseInt(num % 10);
+    } else {
+      return (
+        parseInt(num / 100) * parseInt((num % 100) / 10) * parseInt(num % 10)
+      );
+    }
+  }
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
