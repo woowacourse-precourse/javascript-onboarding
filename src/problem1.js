ec91b3db8a90aef4566e0a444d84d440f4231e1b
100Gyeon <bjy9966@naver.com>
2022-10-26 16:23:18 +0900

    feat: 페이지 번호 각 자리 숫자의 합과 곱 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,20 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const [pobiLeftPage, pobiRightPage] = pobi;
+  const [crongLeftPage, crongRightPage] = crong;
+
+  const getSum = (page) => {
+    return page
+      .toString()
+      .split('')
+      .reduce((acc, cur) => acc + Number(cur), 0);
+  };
+
+  const getMultiplication = (page) => {
+    return page
+      .toString()
+      .split('')
+      .reduce((acc, cur) => acc * Number(cur), 1);
+  };
 }
 
 module.exports = problem1;
