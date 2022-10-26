45062884e19e3b147d3978f7f5d7e50e9ebf7c3c
BB-choi <choibobae0809@gmail.com>
2022-10-26 23:07:36 +0900

    feat: 문제4 - 글자 변환 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,23 @@
+function isUpperCase(char) {
+  return char.toUpperCase() === char;
+}
+
+function reverseCharacter(char) {
+  let index = 0;
+  const A = 65;
+  const Z = 90;
+  const a = 97;
+  const z = 122;
+
+  const asciiChar = char.charCodeAt(index);
+
+  if (isUpperCase(char)) {
+    return String.fromCharCode(Z - asciiChar + A);
+  }
+
+  return String.fromCharCode(z - asciiChar + a);
+}
+
 function problem4(word) {
   var answer;
   return answer;
