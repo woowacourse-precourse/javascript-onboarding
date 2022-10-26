3a312a821b13bb2d217973bb71f010cc48d03994
Quartz <skyu.dev@gmail.com>
2022-10-26 22:23:32 +0900

    feat: Solve problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,26 @@
+/**
+ *
+ * @param {number} num
+ * @returns {number[]} numbers
+ */
+function getNumbersArr(num) {
+  let numbers = [];
+  let numString = num.toString();
+  for (let i = 0; i < numString.length; i++) {
+    numbers.push(parseInt(numString.charAt(i)));
+  }
+  return numbers;
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let numbersArr = getNumbersArr(i);
+    numbersArr = numbersArr.filter((v) => v === 3 || v === 6 || v === 9);
+    answer = answer + numbersArr.length;
+  }
+
   return answer;
 }
 
