37d5f0262873959439770f484ee7a07dae4b64c4
heesungjang <heesungj7@gmail.com>
2022-10-27 03:41:53 +1000

    우테코 1주차 과제 3번

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  const claps = Array(number)
+    .fill('')
+    .reduce((a, _, i) => (a += count369(i + 1).length), 0);
+
+  return claps;
 }
+
+const count369 = (num) =>
+  [...String(num)].filter((digit) => ['3', '6', '9'].includes(digit));
 
 module.exports = problem3;
