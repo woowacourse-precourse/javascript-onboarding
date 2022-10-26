afd630d060f496b151867d9c55f72c15c3a91a8f
loopy-lim <loopy@jnu.ac.kr>
2022-10-27 06:37:00 +0900

    feat: 각 입력은 2이다

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,19 @@
+const bookPage = {
+  left: 0,
+  right: 0,
+  inputPages: function (...pages) {
+    if (this.checkPages(pages)) {
+      return false;
+    }
+  },
+  checkPages: function (...pages) {
+    if (pages.length != 2) {
+      return false;
+    }
+    return true;
+  },
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
