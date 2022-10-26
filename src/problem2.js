8b6b83e1adecb80f224cf3bfbc7aa9861f21af88
An Sol <apdlvmf486@gmail.com>
2022-10-27 08:48:57 +0900

    길이 변화가 없을 때까지 반복하도록 반복문 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,18 @@
 function problem2(cryptogram) {
   let count = cryptogram.length;
   let newString = cryptogram
-  for(let num = 'a'.charCodeAt(0); num < 'z'.charCodeAt(0) + 1; num++){
-    const regex = new RegExp(`${String.fromCharCode(num)}{2}`, 'g');
-    newString = newString.replace(regex, '');
+  let flag = true;
+  while (flag){
+    for(let num = 'a'.charCodeAt(0); num < 'z'.charCodeAt(0) + 1; num++){
+      const regex = new RegExp(`${String.fromCharCode(num)}{2}`, 'g');
+      newString = newString.replace(regex, '');
+    }
+    if (newString.length < count){
+      count = newString.length;
+    }else{
+      flag = false;
+    }
   }
-
   return newString;
 
 }
