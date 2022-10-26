145acc1f7ce348e8bbc1d8d9c6de5ed1794e09f1
haesa <xdhaesa@gmail.com>
2022-10-26 22:58:53 +0900

    problem3 숫자에 있는 3, 6, 9 개수 카운트하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
+function count369(num) {
+  return [...num.toString()] //
+    .filter((n) => n === '3' || n === '6' || n === '9').length;
+}
+
 function problem3(number) {
   var answer;
   return answer;
 }
-
 module.exports = problem3;
