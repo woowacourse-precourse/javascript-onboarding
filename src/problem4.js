db446fb42d2149e2df80a666132eacd361b02762
yeongi <cbg5255@gmail.com>
2022-10-26 20:31:15 +0900

    문제4. 아스키 코드 KEY 객체 생성 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
   var answer;
+  const key = getFrogKey();
+  console.log(key);
   return answer;
 }
 
+problem4();
+
 module.exports = problem4;
+
+function getFrogKey() {
+  //65~90 : 90~65
+  const key = {};
+
+  //65: A ~ 90: Z : 90: Z ~ 65: A
+  for (let [i, j] = [65, 90]; i < 90; i++) {
+    key[i] = j--;
+  }
+
+  //65: A ~ 90: Z : 90: Z ~ 65: A
+  for (let [i, j] = [97, 122]; i < 122; i++) {
+    key[i] = j--;
+  }
+
+  return key;
+}
