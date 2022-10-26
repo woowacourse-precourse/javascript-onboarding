31ad29d4971d455d48100e19b047666c53563714
thewronghand <penfreak77@gmail.com>
2022-10-27 02:09:33 +0900

    feat: 예외사항 처리 로직 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,8 @@
 function problem1(pobi, crong) {
+  if (pobi[1]-pobi[0] !==1 || pobi[0] % 2 ==0 || crong[1]-crong[0] !==1 || crong[0] % 2 ==0){
+    return -1;
+  }
+  
   let pobiValue = Math.max(sumArrayNumber(stringToNumberArray(pobi[0])), sumArrayNumber(stringToNumberArray(pobi[1])), multiplyArrayNumber(stringToNumberArray(pobi[0])), multiplyArrayNumber(stringToNumberArray(pobi[1])))
   let crongValue = Math.max(sumArrayNumber(stringToNumberArray(crong[0])), sumArrayNumber(stringToNumberArray(crong[1])), multiplyArrayNumber(stringToNumberArray(crong[0])), multiplyArrayNumber(stringToNumberArray(crong[1])))
 
