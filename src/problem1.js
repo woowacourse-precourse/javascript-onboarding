927f3e3b35f2f21abccb78d6c199596d8549afbc
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-26 16:03:40 +0900

    feat: 예외사항 체크

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,9 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+const problem1 = (pobi, crong) => {
+  if (checkException(pobi, crong)) return -1;
+}
+
+const checkException = (player1, player2) => {
+  return (player1[1] - player1[0] !== 1) || (player2[1] - player2[0] !== 1);
 }
 
 module.exports = problem1;
