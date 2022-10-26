2a46d3ce3e21673a682a2e87138dd72638ac46b0
han0224 <dbn0224@gmail.com>
2022-10-26 20:27:48 +0900

    [문제4] 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
+const checkLowerCase = (char) => {
+  return char === char.toLowerCase();
+};
+
+const getReverseChar = (char) => {
+  const alphabet = "abcdefghijklmnopqrstuvwxyz".split("");
+  const index = alphabet.indexOf(char);
+  return alphabet[alphabet.length - 1 - index];
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const wordArray = word.split("");
+  for (let i = 0; i < wordArray.length; i++) {
+    if (wordArray[i] === " ") continue;
+    if (checkLowerCase(wordArray[i])) {
+      wordArray[i] = getReverseChar(wordArray[i]);
+    } else {
+      wordArray[i] = getReverseChar(wordArray[i].toLowerCase()).toUpperCase();
+    }
+  }
+  return wordArray.join("");
 }
 
 module.exports = problem4;
