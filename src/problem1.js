c583784a8e26949a7befacfc2b063e582741e63e
cham-min <hymn9613@gmail.com>
2022-10-27 01:19:41 +0900

    feat: 제한사항 처리 추가

--- src/problem1.js
+++ src/problem1.js
@@ -4,10 +4,20 @@ function problem1(pobi, crong) {
   const pobiScore = Math.max(...pobi.map((num) => getMaxScore(num)));
   const crongScore = Math.max(...crong.map((num) => getMaxScore(num)));
 
+  if (checkException(pobi) || checkException(crong)) return -1;
   if (pobiScore > crongScore) answer = 1;
   if (pobiScore < crongScore) answer = 2;
 
   return answer;
+}
+
+function checkException(num) {
+  if (num.length !== 2) return true;
+  if (num[1] - num[0] !== 1) return true;
+  if (num[0] === 1 || num[1] === 400) return true;
+  if (num[0] < 1 || num[1] > 400) return true;
+
+  return false;
 }
 
 function getMaxScore(num) {
