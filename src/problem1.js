76ea0c960ac283dd8a7800f4fd872f25a01a56d0
nuheajiohc <jaehun341012@gmail.com>
2022-10-26 22:42:55 +0900

    모든 경우의 점수 중 max 값 return

--- src/problem1.js
+++ src/problem1.js
@@ -1,12 +1,19 @@
 function problem1(pobi, crong) {
+    const maxScoreArr = [];
     let answer;
     return answer;
 }
 
 function maxNumber(people) {
+    const score = [];
     people.forEach(el => {
         const str = String(el).split("");
+        const sum = str.reduce((acc, cur) => (Number(acc) + Number(cur)));
+        const mult = str.reduce((acc, cur) => acc * cur);
+        score.push(sum);
+        score.push(mult);
     })
+    return Math.max(...score);
 }
 
 module.exports = problem1;
