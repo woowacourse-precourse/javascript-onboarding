7db5a9df722180e97ad7198f6535fe0ee340322a
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-27 02:12:15 +0900

    refactor: problem7 if문과 concat 을 스프레드 연산으로 교체

--- src/problem7.js
+++ src/problem7.js
@@ -2,10 +2,8 @@ function problem7(user, friends, visitors) {
   const relation = {};
   const score = {};
   friends.forEach(([user1, user2]) => {
-    if (!relation[user1]) relation[user1] = [];
-    if (!relation[user2]) relation[user2] = [];
-    relation[user1].push(user2);
-    relation[user2].push(user1);
+    relation[user1] = [...(relation[user1] || []), user2];
+    relation[user2] = [...(relation[user2] || []), user1];
   });
   relation[user].forEach((friend) => {
     relation[friend].forEach((knowTogether) => {
@@ -14,17 +12,17 @@ function problem7(user, friends, visitors) {
       score[knowTogether] += 10;
     });
   });
-  visitors.forEach((visit) => {
-    if (relation[user].includes(visit)) return;
-    if (!score[visit]) score[visit] = 0;
-    score[visit] += 1;
+  visitors.forEach((visitor) => {
+    if (relation[user].includes(visitor)) return;
+    if (!score[visitor]) score[visitor] = 0;
+    score[visitor] += 1;
   });
   return Object.entries(score)
     .sort(([user1Name, user1Score], [user2Name, user2Score]) => {
       if (user2Score === user1Score) return user2Name > user1Name ? -1 : 1;
       return user2Score - user1Score;
     })
-    .reduce((result, recommand) => result.concat(recommand[0]), [])
+    .reduce((result, recommand) => [...result, recommand[0]], [])
     .slice(0, 5);
 }
 
