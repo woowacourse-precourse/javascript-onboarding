33b2d1ce7ad51341d37dd64246fc82b1ec7b4da1
pangkyu <baesense@naver.com>
2022-10-27 00:53:26 +0900

    test : testCode 함수 생성
    ApplicationTest.js 에 있는 테스트코드 이외에 조건들 확인하기 위해서 testCode() 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,11 @@
 function problem1(pobi, crong) {
   const pageNumber = new Array(401);
   pageNumber[0] = 0;
+  console.log(pobi, crong);
 }
-problem1();
+
+function testCode() {
+  problem1([3, 0], [123, 124]);
+}
+testCode();
 module.exports = problem1;
