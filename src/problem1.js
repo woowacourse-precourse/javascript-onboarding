514fc773664a4efe7fb5ad7d9083fddae624b4dc
sojinjang <jsojin6119@gmail.com>
2022-10-27 01:22:56 +0900

    :sparkles: feat(prob1): input 유효성 검사 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,14 @@
+function checkInvalidity(pageList) {
+  const leftPage = pageList[0];
+  const rightPage = pageList[1];
+  if (leftPage < 3 || rightPage > 398) return true;
+  if (rightPage - leftPage !== 1) return true;
+  if (leftPage % 2 !== 1) return true;
+  return false;
+}
+
 function problem1(pobi, crong) {
+  if (checkInvalidity(pobi) | checkInvalidity(crong)) return -1;
   var answer;
   return answer;
 }
