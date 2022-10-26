b2f0c535fb636883c6d99e01d0b28c0dd3d0136b
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-27 00:16:26 +0900

    dosc<problem3>: Define features list

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
+/* 
+ * Features
+ * 1. 1 ~ 입력값 만큼 반복문 실행
+ * 2. [3, 6 ,9] 의 숫자가 들어가는지 검사하여 count
+ * 3. 두자릿수부터 개별적으로 세야함 e.g. 36 => cnt+=2 
+ * 4. 횟수 리턴
+ *  
+ * Exceptions
+ * 1. 1 ~ 10000 이하의 자연수
+ */
 function problem3(number) {
   var answer;
   return answer;
 }
 
 module.exports = problem3;
+
+
