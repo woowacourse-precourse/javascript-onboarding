d4b3d5c703cf132231037fbc679d1b0ad57fc39a
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-26 20:35:49 +0900

    feat(Exception):주어진 범위 외의 값일 경우 예외처리(1번 기능목록 구현)

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,15 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer = 0;
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
+
+  for (let i of pobi) {
+    if (i < 3 || i > 398) return -1;
+  }
+
+  for (let i of crong) {
+    if (i < 3 || i > 398) return -1;
+  }
+
   return answer;
 }
 
