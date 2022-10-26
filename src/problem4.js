26946d43d11e1b56dcf224723d69d0c3145d2c62
saejinpark <saejinpark951210@gmail.com>
2022-10-26 16:51:30 +0900

    문제4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
+const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWER_CASE = UPPER_CASE.toLowerCase();
+const BLUE_FLUG_OBJECT = {};
+const LIMIT = UPPER_CASE.length;
+
+for (let i = 0; i < LIMIT; i++) {
+  BLUE_FLUG_OBJECT[UPPER_CASE[i]] = UPPER_CASE[LIMIT - 1 - i];
+  BLUE_FLUG_OBJECT[LOWER_CASE[i]] = LOWER_CASE[LIMIT - 1 - i];
+}
+
+BLUE_FLUG_OBJECT[" "] = " ";
+Object.freeze(BLUE_FLUG_OBJECT);
+
 function problem4(word) {
-  var answer;
+  var answer = "";
+  for (let char of word) {
+    answer += BLUE_FLUG_OBJECT[char];
+  }
   return answer;
 }
 
