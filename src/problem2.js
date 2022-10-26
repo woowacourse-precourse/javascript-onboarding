a2c102d25892a231616d18c84d053d0009c01edc
Quartz <skyu.dev@gmail.com>
2022-10-26 21:25:55 +0900

    feat: Solve problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,30 @@
+function deleteDuplicate(string) {
+  let prevString = string;
+  let char = string[0];
+  let currString = char;
+  // Exception: 문자열 비었을 때
+  if (string.length === 0) {
+    return "";
+  }
+  for (i = 1; i < string.length; i++) {
+    if (char !== string[i]) {
+      currString = currString.concat(string[i]);
+    } else {
+      currString = currString.substring(0, currString.length - 1);
+    }
+    char = string[i];
+  }
+  if (prevString === currString) return currString;
+  return deleteDuplicate(currString);
+}
+
 function problem2(cryptogram) {
   var answer;
+  // Exception: 문자열 비었을 때
+  if (cryptogram.length === 0) {
+    return "";
+  }
+  answer = deleteDuplicate(cryptogram);
   return answer;
 }
 
