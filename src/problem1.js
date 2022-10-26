d736a7b3580ede6251cbc2ffd2c1b4c3529127d0
yeongi <cbg5255@gmail.com>
2022-10-26 16:05:29 +0900

    문제 1번 구현 완료 및 테스트 통과

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,53 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  //각 자리수 더하기
+  const sumDigit = (num) => {
+    let res = 0;
+    while (num > 0) {
+      res += Math.floor(num % 10);
+      num = Math.floor(num / 10);
+    }
+    return res;
+  };
+
+  //각 자리수 곱하기
+  const multiplyDigit = (num) => {
+    let res = 1;
+    while (num > 0) {
+      if (num < 0) break;
+      res *= Math.floor(num % 10);
+      num = Math.floor(num / 10);
+    }
+    return res;
+  };
+
+  // 예외사항은 -1
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
+  if (pobi[0] === 1 || crong[0] === 1 || pobi[1] === 400 || crong[1] === 400)
+    return -1;
+
+  let pobiNum;
+  let crongNum;
+
+  pobi.map((num) =>
+    sumDigit(num) > multiplyDigit(num)
+      ? (pobiNum = sumDigit(num))
+      : (pobiNum = multiplyDigit(num))
+  );
+
+  crong.map((num) =>
+    sumDigit(num) > multiplyDigit(num)
+      ? (crongNum = sumDigit(num))
+      : (crongNum = multiplyDigit(num))
+  );
+
+  //포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0,
+  if (pobiNum > crongNum) {
+    return 1;
+  } else if (pobiNum < crongNum) {
+    return 2;
+  } else {
+    return 0;
+  }
 }
 
 module.exports = problem1;
