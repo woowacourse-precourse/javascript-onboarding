156c66b4258d4819200a4d88d5da8586efad6ba1
sungjihyun <jhsung23@gmail.com>
2022-10-26 22:49:55 +0900

    Feat: problem 4: 사전 기반 단어 변환 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,13 @@
+const A_CHAR_CODE = "A".charCodeAt();
+const Z_CHAR_CODE = "Z".charCodeAt();
+
+const convert = (alphabet) => {
+  const result = String.fromCharCode(
+    A_CHAR_CODE + (Z_CHAR_CODE - alphabet.toUpperCase().charCodeAt())
+  );
+  return result;
+};
+
 function problem4(word) {
   var answer;
   return answer;
