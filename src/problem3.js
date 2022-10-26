1adc8435e23d5a52948e8bd96b3d052c56d845fb
ctd <qpflapffhs76@gmail.com>
2022-10-26 21:37:48 +0900

    feat: 숫자에 3, 6, 9가 포함되어 있는지 찾는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    i = i.toString();
+    if (i.includes(3) || i.includes(6) || i.includes(9)) {
+      answer++;
+    }
+  }
   return answer;
 }
 
