8a4bac1b1a7341088246af256e6720da5455fe67
CoboCho <rlaalsrb1111@naver.com>
2022-10-26 21:14:35 +0900

    feat: [problem2] 중복 제거 함수 removeDuplicateLetter() 구현 및 재귀 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,18 @@
 function problem2(cryptogram) {
-  let answer;  
+  let answer;
   if(cryptogram.length === 1) return cryptogram;
+  answer = removeDuplicateLetter(cryptogram);
   return answer;
+}
+
+function removeDuplicateLetter(string) {
+  string = string.split('');
+  let result = '';
+  string.forEach((letter, idx) => {
+    if(letter !== string[idx + 1] && letter !== string[idx - 1]) result += letter;
+  });
+  if(result !== string.join('')) return removeDuplicateLetter(result);
+  if(result === string.join('')) return result;
 }
 
 module.exports = problem2;
