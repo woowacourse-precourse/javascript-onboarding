ad31af8b76d7c58b5c52ea4e02eeed2531a5a4f0
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 07:15:13 +0900

    docs: 2일차 학습 체크리스트 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,25 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+  const decrypt = (cryptogram) => {
+    if (cryptogram.length === 0) {
+      return cryptogram;
+    }
 
-module.exports = problem2;
+    const 
+
+    const decrypted = [...cryptogram].reduce((acc, cur) => {
+      if (acc.slice(-1) === cur) {
+        return acc.slice(0, -1);
+      }
+      return acc + cur;
+    });
+
+    if (cryptogram === decrypted) {
+      return cryptogram;
+    }
+//?
+  };
+
+  decrypt(cryptogram); //?
+}
+problem2('browoanoommnaon'); //?
+// module.exports = problem2;
