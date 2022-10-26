b2c40e4c99d5b43607daa8ab6f53642ba202fb6b
hwnim5324 <hwnim5324@naver.com>
2022-10-27 00:56:30 +0900

    3번 기능 목록 작성.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
   var answer;
   return answer;
+
+  //1. 숫자를 배열로 변환해 값에 [3,6,9]가 몇 개 들어가는지 반환하는 함수 작성.
+  //2. 반복문을 사용해 3부터 number까지 기능1 함수 동작하도록 작성.
+  //3. 반환값들을 전부 더해 리턴.
 }
 
 module.exports = problem3;
