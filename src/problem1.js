d9e765b966b30abeb0f48dbe6d3983836bbec0ae
c0c0pang <coc1789@naver.com>
2022-10-26 16:51:34 +0900

    변수 이름 수정.(sum1->mul1,sum2->mul2

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,8 @@
 function problem1(pobi, crong) {
   //시작하는 면이나 끝나는 면은 예외처리 -1 반환
   //제한 길이가 2 이므로 99~102 범위는 4이므로 -1 반환
-  var PobiCheck = pobi[1]-pobi[0];
-  var CrongCheck = crong[1]-crong[0];
+  const PobiCheck = pobi[1]-pobi[0];
+  const CrongCheck = crong[1]-crong[0];
   if(PobiCheck>1 || CrongCheck>1){
     return -1;
   };
@@ -22,16 +22,18 @@ function problem1(pobi, crong) {
   }
 
   const MaxCrongiNum = ()=>{
-    const addpobi = addproblem1(crong[0],crong[1]);
-    const mulpobi = mulproblem1(crong[0],crong[1]);
-    if(addpobi>mulpobi){
-      return addpobi;
+    const addcrong = addproblem1(crong[0],crong[1]);
+    const mulcrong = mulproblem1(crong[0],crong[1]);
+    if(addcrong>mulcrong){
+      return addcrong;
     }
     else{
-      return mulpobi;
+      return mulcrong;
     }
   }
-  if(MaxPobiNum>MaxCrongiNum){
+  if(MaxPobiNum()>MaxCrongiNum()){
+      console.log(MaxPobiNum);
+      console.log(MaxCrongiNum);
     return 1;
   }
   else if(MaxPobiNum<MaxCrongiNum){
@@ -61,20 +63,20 @@ function addproblem1(num1,num2){
 }
 
 function mulproblem1(num1,num2){
-  var sum1=0,sum2=0;
-  while(sum1>0){
-    sum1 *= num1%10;
-    num1 = Math.floor(sum1/10);
+  var mul1=0,mul2=0;
+  while(mul1>0){
+    mul1 *= num1%10;
+    num1 = Math.floor(mul1/10);
   }
-  while(sum2>0){
-    sum2 *= num2%10;
-    num2 = Math.floor(sum2/10);
+  while(mul2>0){
+    mul2 *= num2%10;
+    num2 = Math.floor(mul2/10);
   }
-  if(sum1>sum2){
-    return sum1;
+  if(mul1>mul2){
+    return mul1;
   }
   else{
-    return sum2;
+    return mul2;
   }
 }
 
