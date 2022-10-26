be99f60343c33f60f11d5d72582e774d7f051c75
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 03:07:42 +0900

    problem4/feat : 배열로 저장 후 청개구리 사전대로 문자 변환 후 문자열로 처리

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  let wordArr = [...word];
+
+  for (let i = 0; i < wordArr.length; i++) {
+    if (wordArr[i] != " " && wordArr[i] == wordArr[i].toUpperCase()) {
+      wordArr[i] = String.fromCharCode(90 - (wordArr[i].charCodeAt() - 65));
+    }
+    if (wordArr[i] != " " && wordArr[i] == wordArr[i].toLowerCase()) {
+      wordArr[i] = String.fromCharCode(122 - (wordArr[i].charCodeAt() - 97));
+    }
+  }
+
+  answer = wordArr.join("");
   return answer;
 }
-
 module.exports = problem4;
