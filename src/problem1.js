0a5882f5e073a246026fcedd6951e18879945dcf
gyeongza <gyeongza@naver.com>
2022-10-27 00:16:33 +0900

    feat: 예외사항 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,16 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+// 예외사항 함수 생성
+const isValidInput = (leftPage, rightPage) => {
+  if (leftPage[0] <= 1 || leftPage[1] >= 400 || rightPage[0] <= 1 || rightPage[1] >= 400)
+    return false;
+  else if (leftPage[1] - leftPage[0] !== 1) return false;
+  else if (rightPage[1] - rightPage[0] !== 1) return false;
+  else return true;
+};
 
-module.exports = problem1;
+
+// function problem1(pobi, crong) {
+//   var answer;
+//   return answer;
+// }
+
+// module.exports = problem1;
