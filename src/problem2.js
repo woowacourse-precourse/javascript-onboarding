f441e61ab06679b1ad36d6059dbf61a7e334e852
Rki0 <pky00823@gmail.com>
2022-10-26 16:57:36 +0900

    feat: prob2.스택이 비어있을 경우 처리하는 함수
    스택이 비어있다면 암호문의 첫 글자를 스택에 저장하는 함수를 구현했습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
+function stackIsEmpty(stack, cryptogram, i) {
+  // 스택에 아무것도 없다면 연속인지 아닌지 모르므로
+  // 스택에 넣어야한다.
+  if (stack.length === 0) {
+    stack.push(cryptogram[i]);
+  }
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 스택 구조 활용?
+  // 스택에 하나씩 담음
+  // 스택 가장 위에 있는 값과 현재 담으려는 값이 같다면
+  // 스택에서 그 값을 빼고, 담으려는 값도 버려버린다
+  // 그게 아니라면 스택에 넣는다
+  let stack = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    stackIsEmpty(stack, cryptogram, i);
+  }
 }
 
 module.exports = problem2;
