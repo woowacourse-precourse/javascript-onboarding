b077636951aedf5648731b898db7236fc59f6199
연 <mogoo22@naver.com>
2022-10-26 23:12:46 +0900

    feat(문제1): 페이지의 각자리 숫자를 모두 더하거나 곱한 것중 큰 수를 구한다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+const calcMaxValue = (page) => {
+  const numbersArray = String(page).split("");
+  const sum = numbersArray.reduce((acc, cur) => acc + Number(cur), 0);
+  const multiple = numbersArray.reduce((acc, cur) => acc * Number(cur));
+
+  return Math.max(sum, multiple);
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
