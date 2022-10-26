065c93cce2388a9121913b1547862c671747b08f
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-26 23:30:15 +0900

    feat: 손뼉 치는 횟수 구하는 로직 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
-function problem3(number) {
-  var answer;
+const problem3 = (number) => {
+  const answerArr = [3, 6, 9];
+  let answer = 0;
+  for (let i = 1; i < number + 1; i += 1) {
+    const currentDigit = [...String(i)].map((num) => Number(num));
+    let cnt = 0;
+    currentDigit.forEach((cur) => {
+      if (answerArr.includes(cur)) {
+        cnt += 1;
+      }
+    })
+    answer += cnt;
+  }
   return answer;
 }
 
