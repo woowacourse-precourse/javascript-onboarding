7573e5477f5e3c5a81364aa53b34e58dfe2b34c3
vinivin153 <vinivin153@naver.com>
2022-10-27 01:37:50 +0900

    feat: 연속된 중복 문자 제거 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
+  const answer = removeDuplication(cryptogram);
   return answer;
+}
+
+function removeDuplication(cryptogram) {
+  const stack = [];
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (!stack.length || cryptogram[i] !== stack[stack.length - 1]) {
+      stack.push(cryptogram[i]);
+    } else if (stack.length && cryptogram[i] === stack[stack.length - 1]) {
+      stack.pop();
+    }
+  }
+  return stack.join('');
 }
 
 module.exports = problem2;
