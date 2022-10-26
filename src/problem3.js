8edf24e73a723b0f7886faf30bc49923476ef3ee
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-26 21:45:20 +0900

    feat(While): n번 이상의 카운트를 증가하기 위한 while문 추가(기능 목록 2번 구현)

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,12 @@
 function problem3(number) {
   let count = 0;
   for (let i = 2; i <= number; i++) {
-    if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
-      count++;
+    let num = i;
+    while (num > 2) {
+      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+        count++;
+      }
+      num = parseInt(num / 10);
     }
   }
 
