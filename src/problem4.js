1d43d9e874879694a342f8830aa5f9edbcc1a899
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-26 22:34:24 +0900

    feat(word Change): 공백 문자는 바로 문자열에 추가, 해당 문자를 대문자로 변경(.toUpperCase)하여객체속에서 대응되는 알파벳으로 변경하여 변수에 저장(기능 목록 2번 구현)

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,40 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  const wordsObject = {
+    A: "Z",
+    B: "Y",
+    C: "X",
+    D: "W",
+    E: "V",
+    F: "U",
+    G: "T",
+    H: "S",
+    I: "R",
+    J: "Q",
+    K: "P",
+    L: "O",
+    M: "N",
+    N: "M",
+    O: "L",
+    P: "K",
+    Q: "J",
+    R: "I",
+    S: "H",
+    T: "G",
+    U: "F",
+    V: "E",
+    W: "D",
+    X: "C",
+    Y: "B",
+    Z: "A",
+  };
+  for (let x of word) {
+    if (x === " ") {
+      answer += " ";
+      continue;
+    }
+    let char = wordsObject[x.toUpperCase()];
+  }
   return answer;
 }
 
