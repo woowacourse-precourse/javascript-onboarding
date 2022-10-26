b13054b68339bb76813ba21164224e2eaf7efbbc
sungjihyun <jhsung23@gmail.com>
2022-10-26 21:26:50 +0900

    Fix: problem 2: 암호 해독 기능 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,6 @@
 const decrypt = (code) => {
   return code.split("").reduce((acc, cur) => {
-    return acc[acc.length - 1] === cur ? acc : acc + cur;
+    return acc[acc.length - 1] === cur ? acc.slice(0, -1) : acc + cur;
   });
 };
 
