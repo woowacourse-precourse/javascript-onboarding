b900781a3f81e46c9b3b4726c6bc2c7ad07eb056
h2002y <h2002y@naver.com>
2022-10-26 21:27:25 +0900

    fix: setMap func

--- src/problem6.js
+++ src/problem6.js
@@ -14,7 +14,12 @@ function setMap(forms, map){
   for(let i = 0; i < forms.length; i++){
     for(let j = 0; j < forms[i][1].length - 1; j++){
       let tmp = forms[i][1].substr(j,2);
-      map.set(tmp, 1);
+      if(map.has(tmp)){
+        map.set(tmp, map.get(tmp) + 1);
+      }
+      else{
+        map.set(tmp, 1);
+      }
     }
   }
 }
