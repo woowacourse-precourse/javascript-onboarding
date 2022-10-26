92d61d2b4a9630a90f632c7aaba1b9783827c194
Leejin-Yang <ejin1996@gmail.com>
2022-10-26 22:48:03 +0900

    feat(problem5): 지갑 배열 구하기

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,18 @@
+const MONEY_UNITS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
+  const answer = MONEY_UNITS.map(() => 0);
+  let currentMoney = money;
+  let index = 0;
+
+  while (currentMoney > 0) {
+    const count = parseInt(currentMoney / MONEY_UNITS[index]);
+
+    answer[index] = count;
+    currentMoney %= MONEY_UNITS[index];
+    index += 1;
+  }
+
   return answer;
 }
 
