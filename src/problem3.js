f1954b9143d7d7ccd69bad3a0017c152ea28b850
yeong bin <2976622@naver.com>
2022-10-27 00:09:42 +0900

    chore: problem3 - countOf369 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
+function countOf369(number) {
+  return number
+    .toString()
+    .split("")
+    .reduce((acc, cur) => {
+      const num = parseInt(cur);
+      return acc + (num % 3 === 0 && num !== 0 ? 1 : 0);
+    }, 0);
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  return number;
 }
 
 module.exports = problem3;
