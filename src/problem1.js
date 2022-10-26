fa3e76eaf0ae534e5467435d08a6517ac3ae5032
lee-il-woo <empty001430@gmail.com>
2022-10-26 16:11:31 +0900

    예외사항 추가함

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  var answer = -1;
+  //예외사항 if문으로 제외하기
+  if(pobi[1]-pobi[0] == 1 && crong[1]-crong[0] == 1){
+    return answer;
+  }else if((pobi[1] <= 2 && pobi[0] >= 399)&&(crong[1] <= 2 && crong[0] >= 399)){
+    return answer;
+  }else if(pobi.length != 2 && crong.length != 2){
+    return answer;
+  }
+  ㅁㅁㅁ
 }
 
 module.exports = problem1;
