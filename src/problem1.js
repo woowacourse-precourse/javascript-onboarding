319c15b2e49eb5abc10441ead1846c4dfc15f4ce
Tory <88531407+1two13@users.noreply.github.com>
2022-10-26 20:21:38 +0900

    feat(problem1): pobi의 가장 큰 수 구하는 코드 추가

--- src/problem1.js
+++ src/problem1.js
@@ -5,12 +5,28 @@
 // 4. 승패 결과 처리하기
 
 function problem1(pobi, crong) {
+  // 포비의 가장 큰 수
+  let pobiMax;
+
   // 예외 사항 처리하기
   // 1. 포비나 크롱 배열이 1, 2, 399, 400의 값을 가지고 있을 때
   const concatArr = pobi.concat(crong);
   if (concatArr.filter((el) => el <= 2 || el >= 399).length > 0) return -1;
   // 2. 각 배열의 값의 차이가 1 초과일때 ex) [99, 102]
   if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) return -1;
+
+  // pobi의 가장 큰 수 구하기
+  pobi
+    // 배열안에 각 자리의 숫자를 담기
+    .map((el) => String(el).split(""))
+    .reduce((acc, cur) => {
+      pobiMax = Math.max(
+        acc.reduce((acc, cur) => Number(acc) + Number(cur)),
+        acc.reduce((acc, cur) => Number(acc) * Number(cur)),
+        cur.reduce((acc, cur) => Number(acc) + Number(cur)),
+        cur.reduce((acc, cur) => Number(acc) * Number(cur))
+      );
+    });
 }
 
 module.exports = problem1;
