fb195d2ae93dc91b1abd757d4f298deab8502ae3
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 02:16:17 +0900

    problem2/feat : 제한사항 조건 추가

--- src/problem2.js
+++ src/problem2.js
@@ -2,16 +2,25 @@ function problem2(cryptogram) {
   let cryptogramArr = [];
   let answer = "";
 
-  for (let i = 0; i < cryptogram.length; i++) {
-    cryptogramArr.push(cryptogram[i]);
+  if (
+    cryptogram.length >= 1 &&
+    cryptogram.length <= 1000 &&
+    cryptogram == cryptogram.toLowerCase()
+  ) {
+    for (let i = 0; i < cryptogram.length; i++) {
+      cryptogramArr.push(cryptogram[i]);
+    }
+
+    for (let i = 0; i < cryptogramArr.length; i++) {
+      if (cryptogramArr[i] == cryptogramArr[i + 1]) {
+        cryptogramArr.splice(i, 2);
+        i = -1;
+      }
+    }
+    answer = cryptogramArr.join("");
   }
 
-  for (let i = 0; i < cryptogramArr.length; i++) {
-    if (cryptogramArr[i] == cryptogramArr[i + 1]) {
-      cryptogramArr.splice(i, 2);
-      i = -1;
-    }
-  }
+  return answer;
 }
 
 module.exports = problem2;
