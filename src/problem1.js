9715da99d91df84e108d09d75873d162a4c91775
kangju2000 <kangju2000@naver.com>
2022-10-26 22:40:25 +0900

    feat: exceptionCheck 함수 선언

--- src/problem1.js
+++ src/problem1.js
@@ -19,4 +19,10 @@ function pageScore(page) {
   return Math.max(result[0], result[1]);
 }
 
+function exceptionCheck(pages) {
+  if (pages[0] <= 1 || pages[1] >= 400) return false;
+  else if (pages[1] - pages[0] !== 1) return false;
+  else return true;
+}
+
 module.exports = problem1;
