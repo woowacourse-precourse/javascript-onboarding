1d8460959d419e44963894c35276efaa87b195e2
Jiwoo Park <jiwoo9446@naver.com>
2022-10-26 22:31:34 +0900

    problem3 sloved

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clap = 0;
+  for (let i = 1; i <= number; i++) {
+    const splited = i.toString().split("");
+    const length = splited.length;
+    for (let j = 0; j < length; j++) {
+      if (parseInt(splited[j]) % 3 === 0 && splited[j] !== "0") clap++;
+    }
+  }
+  return clap;
 }
-
 module.exports = problem3;
