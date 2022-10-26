a6774db0e9d6d8bcacc1c0712b4127376b3192fa
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-27 02:00:18 +0900

    feat: 친구관계를 나타내는 그래프를 만들고 간선 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const relation = {};
+  friends.forEach(([user1, user2]) => {
+    if (!relation[user1]) relation[user1] = [];
+    if (!relation[user2]) relation[user2] = [];
+    relation[user1].push(user2);
+    relation[user2].push(user1);
+  });
 }
 
 module.exports = problem7;
