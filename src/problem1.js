ac633efb4cae6071b815d68b118b49bc080dfa6b
Suyoung <jsy8492@gmail.com>
2022-10-26 18:32:29 +0900

    feat(problem1): 각 페이지 번호의 각 자리 숫자 모두 더하기 구현

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,18 @@ function getMaxScore(pageNumbers) {
   return Math.max(getMaxSum(pageNumbers), getMaxMultiplication(pageNumbers));
 }
 
+function getMaxSum(pageNumbers) {
+  const sums = pageNumbers.map((pn) => getSum(pn));
+
+  return Math.max(...sums);
+}
+
+function getSum(pageNumber) {
+  const numbers = `${pageNumber}`.split('').map((pn) => parseInt(pn, 10));
+
+  return numbers.reduce((prevSum, number) => prevSum + number, 0);
+}
+
 function problem1(pobi, crong) {
   const pobiScore = getMaxScore(pobi);
   const crongScore = getMaxScore(crong);
