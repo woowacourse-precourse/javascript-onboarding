69a31108dcfb8027de57d8878cad49d2f27d5096
CoboCho <rlaalsrb1111@naver.com>
2022-10-27 01:13:21 +0900

    feat: [problem7] user의 친구 찾기 함수 isfriend() 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let userFriends = [];
+  friends.forEach(relationship => {
+    if(isfriend(user, relationship)) userFriends.push(isfriend(user, relationship));
+  });
+}
+function isfriend(user, relationship) {
+  if(relationship[0] === user) return relationship[1];
+  if(relationship[1] === user) return relationship[0];
+  return false;
 }
 
 module.exports = problem7;
