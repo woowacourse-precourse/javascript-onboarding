daa5fee9c87fbe43fae5ac95daee2484e280800a
nlom0218 <nlom0218@naver.com>
2022-10-26 23:30:19 +0900

    feat: 문제 7 - 친구 관계 Map 객체로 관리하기(키: user, 값: 친구 목록 배열)

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
+const setUserMap = (userMap, [userA, userB]) => {
+  userMap.set(userA, [...(userMap.get(userA) || []), userA]);
+  userMap.set(userB, [...(userMap.get(userB) || []), userB]);
+};
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userMap = new Map();
+  friends.forEach((item) => setUserMap(userMap, item));
 }
 
 module.exports = problem7;
