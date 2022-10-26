0a8582b0d527710df2f402ced4a4371786709400
Gyeongm1n <affirming_km@naver.com>
2022-10-26 20:50:12 +0900

    입력값 예외처리 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,28 @@
+function handleExceptions(pobi, crong) {
+  const [pobi_left, pobi_right] = pobi;
+  const [crong_left, crong_right] = crong;
+
+  if (
+    pobi_left % 2 !== 1 ||
+    crong_left % 2 !== 1 ||
+    pobi_right % 2 !== 0 ||
+    crong_right % 2 !== 0
+  ) {
+    return false;
+  }
+
+  if (pobi_right - pobi_left !== 1 || crong_right - crong_left !== 1) {
+    return false;
+  }
+
+  return true;
+}
+
 function problem1(pobi, crong) {
+  if (!handleExceptions(pobi, crong)) {
+    return -1;
+  }
+
   var answer;
   return answer;
 }
