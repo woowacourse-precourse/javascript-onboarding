3b6f3474f0fa9e4a4631115df36040abb10cdb13
sojinjang <jsojin6119@gmail.com>
2022-10-27 05:01:07 +0900

    :sparkles: feat(prob2): 배열 생성, 중복 문자 제거 기능 추가
    임시 배열 만든 뒤 배열 마지막 요소와 다음 문자 비교

--- src/problem2.js
+++ src/problem2.js
@@ -1,4 +1,14 @@
 function problem2(cryptogram) {
+  const [top, ...rest] = cryptogram.split("");
+  const temp = [top];
+  for (let char of rest) {
+    if (temp.length < 1) {
+      temp.push(char);
+    } else {
+      if (temp[temp.length - 1] === char) temp.pop();
+      else temp.push(char);
+    }
+  }
   var answer;
   return answer;
 }
