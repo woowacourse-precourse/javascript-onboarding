549ef27952cd892fdeda11042e472f59ff253cfa
jihun1233 <leejihun1233@gmail.com>
2022-10-26 19:15:11 +0900

    문제 2번 해결
    - stack으로 앞에 넣었던 글자와 다음 글자가 같은지 확인하며 push,pop 진행
    
    - 마지막에 stack을 join으로 합쳐서 결과 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
   var answer;
+
+  const stack = []
+  
+  for (let index = 0; index < cryptogram.length; index++) {
+    const char = cryptogram[index];
+    console.log(char, stack[stack.length-1])
+    if(char === stack[stack.length-1]){
+      stack.pop()
+    }else{
+      stack.push(char)
+    }
+  }
+
+  answer = stack.join('')
   return answer;
 }
 
