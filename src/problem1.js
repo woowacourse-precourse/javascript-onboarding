2831c39f7b1bdd43b3a84a7f40cc289c8c1e6715
parkjikuk <paxkk@naver.com>
2022-10-26 22:12:49 +0900

    페이지 예외 사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,14 @@ function problem1(pobi, crong) {
   const pobiScore = Math.max(parseInt(pobi.map(num => getScore(num))));
   const crongScore = Math.max(parseInt(crong.map(num => getScore(num))));
 
+  if(pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    return -1;
+  }
+  
+  if(pobi[0] === (1 || 399) || crong[0] === (1 || 399)) {
+    return false;
+  }
+
   if(pobiScore > crongScore) {
     return 1;
   } else if(crongScore > pobiScore) {
