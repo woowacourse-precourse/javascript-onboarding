e6323ee65c9b43fd30b038ab0bbfd431c5c56bee
jun <ggprgrkjh@naver.com>
2022-10-27 00:54:34 +0900

    feat: while문과 반환 #2

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,16 @@
 // 기능 요구 사항
 
 // - [x] 2개 이상 잘라내는 정규식 patt 생성
-// - [] 정규식이 true일시 while문 유지
-// - [] while문이 유지될 때 2개 이상 연속 문자를 잘라내기
-// - [] 정규식이 false일시 answer값 return
+// - [x] 정규식이 true일시 while문 유지
+// - [x] while문이 유지될 때 2개 이상 연속 문자를 잘라내기
+// - [x] 정규식이 false일시 answer값 return
 
 function problem2(cryptogram) {
   const patt = /(.)\1+/;
+
+  while (patt.test(cryptogram)) {
+    cryptogram = cryptogram.replace(patt, "");
+  }
 }
 
 module.exports = problem2;
