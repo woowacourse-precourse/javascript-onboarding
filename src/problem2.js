b57be2de0913b138b5ce1521342a936feaad3a7a
wontae chang <cwt629@naver.com>
2022-10-26 15:54:50 +0900

    completed problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 스택처럼 문자를 저장할 배열
+  const stack = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    // 스택의 맨 위 글자와 같은 글자가 들어오는 경우
+    if (stack.length > 0 && stack[stack.length - 1] === cryptogram.charAt(i)) {
+      // 계속해서 같은 글자는 모두 지워버린다
+      while (i < cryptogram.length && stack[stack.length - 1] === cryptogram.charAt(i))
+        i++;
+      // 중복되었으므로, 스택에서도 제거해준다
+      stack.pop();
+
+      // 이후 for문에 의해 i++이 자동으로 되므로, i 하나 떨어트리기
+      i--;
+    }
+
+    // 그 외에는 정상적으로 들어옴()
+    else stack.push(cryptogram.charAt(i));
+  }
+
+  // 모인 문자 모아서 반환
+  return stack.join("");
 }
 
 module.exports = problem2;
