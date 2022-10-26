16cb12453fbafc3848b22b56655a212fe92fea91
hyunjin <leehj0110@kakao.com>
2022-10-26 18:07:16 +0900

    refactor: 1번 함수 모듈화

--- src/problem1.js
+++ src/problem1.js
@@ -2,29 +2,19 @@ function problem1(pobi, crong) {
   if (!(pobi[1] - pobi[0] === 1) || !(crong[1] - crong[0] === 1)) return -1
 
   let answer = 0
-  const pobiScore = [],
-    crongScore = []
 
-  pobi.forEach((num) => {
-    const numArr = num.toString().split("")
-    const sum = numArr.reduce((acc, cur) => acc + Number(cur), 0)
-    const mul = numArr.reduce((acc, cur) => acc * Number(cur), 1)
-    pobiScore.push(Math.max(sum, mul))
-  })
-  crong.forEach((num) => {
-    const numArr = num.toString().split("")
-    const sum = numArr.reduce((acc, cur) => acc + Number(cur), 0)
-    const mul = numArr.reduce((acc, cur) => acc * Number(cur), 1)
-    crongScore.push(Math.max(sum, mul))
-  })
+  const pobiScore = Math.max(...pobi.map((num) => findMaxScore(num)))
+  const crongScore = Math.max(...crong.map((num) => findMaxScore(num)))
 
-  const pobiMax = Math.max(...pobiScore),
-    crongMax = Math.max(...crongScore)
-
-  if (pobiMax > crongMax) answer = 1
-  else answer = 2
-
+  if (pobiScore > crongScore) answer = 1
   return answer
+}
+
+function findMaxScore(num) {
+  const numArr = num.toString().split("")
+  const sum = numArr.reduce((acc, cur) => acc + Number(cur), 0)
+  const mul = numArr.reduce((acc, cur) => acc * Number(cur), 1)
+  return Math.max(sum, mul)
 }
 
 module.exports = problem1
