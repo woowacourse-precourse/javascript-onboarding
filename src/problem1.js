057d20693ec87a6d955b5f0eaebc5c012ba1dc7e
kyhyun <k29176@gmail.com>
2022-10-26 22:44:32 +0900

    feat : 문제 1번 - 가장 큰 수를 구하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -2,24 +2,17 @@
 구현 기능 목록
   [v] 각 자리 숫자를 모두 더하는 기능
   [v] 각 자리 숫자를 모두 곱하는 기능
-  [ ] 가장 큰 수를 구하는 기능
+  [v] 가장 큰 수를 구하는 기능
   [ ] 점수를 비교 및 판단하는 기능
 */
 
 function problem1(pobi, crong) {
   let answer;
-  const [pobi_left, pobi_right] = pobi;
-  const [crong_left, crong_right] = crong;
+  const [pobiLeft, pobiRight] = pobi;
+  const [crongLeft, crongRight] = crong;
 
-  console.log(sumByEachDigit(pobi_left)); // 16
-  console.log(sumByEachDigit(pobi_right)); // 17
-  console.log(sumByEachDigit(crong_left)); // 17
-  console.log(sumByEachDigit(crong_right)); // 18
-
-  console.log(multiplyNumbers(pobi_left)); // 63
-  console.log(multiplyNumbers(pobi_right)); // 72
-  console.log(multiplyNumbers(crong_left)); // 63
-  console.log(multiplyNumbers(crong_right)); // 72
+  console.log('pobi Result', findLargestNumber(pobiLeft, pobiRight));
+  console.log('crong Result', findLargestNumber(crongLeft, crongRight));
 
   return answer;
 }
@@ -38,6 +31,14 @@ function multiplyNumbers(pages) {
     .split('')
     .reduce((acc, cur) => acc * cur);
   return multiplyResult;
+}
+
+function findLargestNumber(firstNumber, secondNumber) {
+  const sumFirstResult = sumByEachDigit(firstNumber);
+  const sumSecondResult = sumByEachDigit(secondNumber);
+  const multiplyFirstResult = multiplyNumbers(firstNumber);
+  const multiplySecondResult = multiplyNumbers(secondNumber);
+  return Math.max(sumFirstResult, sumSecondResult, multiplyFirstResult, multiplySecondResult);
 }
 
 problem1([97, 98], [197, 198]); // 0
