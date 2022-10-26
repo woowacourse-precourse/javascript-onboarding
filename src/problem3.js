73b84163708aa61962cb6587d587b9b6fdca2bfc
jjo-mi <whdal2636@naver.com>
2022-10-26 22:31:07 +0900

    문제3 구현완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for(var i = 1; i <= number; i++) {
+    if(String(i).includes(3) || String(i).includes(6) || String(i).includes(9)) {
+      answer++;
+    }
+  }
   return answer;
 }
 
