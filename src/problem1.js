bd89a65852a0f0d37fe5c1d20fdcfc302f0f540b
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-26 23:29:29 +0900

    각자 점수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,29 @@
 function problem1(pobi, crong) {
   var answer;
+  let pobiPnt = 0,
+    crongPnt = 0;
+
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
+  pobi.forEach((e) => {
+    let arr = String(e)
+      .split("")
+      .map((str) => Number(str));
+    pobiPnt = Math.max(
+      pobiPnt,
+      arr.reduce((a, b) => (a *= b)),
+      arr.reduce((a, b) => (a += b))
+    );
+  });
+  crong.forEach((e) => {
+    let arr = String(e)
+      .split("")
+      .map((str) => Number(str));
+    crongPnt = Math.max(
+      crongPnt,
+      arr.reduce((a, b) => (a *= b)),
+      arr.reduce((a, b) => (a += b))
+    );
+  });
   return answer;
 }
 
