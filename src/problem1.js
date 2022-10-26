10c2a63127ee0ed4ea414f0741ee38a6f2a44b06
Sanggil Han <tkdrlf95@naver.com>
2022-10-26 16:03:17 +0900

    Add most calcualte logics to problem1.js
    페이지를 각 단위별로 나누어 배열에 저장하는 함수와
    숫자들을 담고 있는 배열을 인자로 받아 합을 반환하는 함수, 숫자들을 담고 있는 배여을 인자로 받아 곱을 반환하는 함수를 구현하였음
    또 prolem1 함수에서 승/패/무승부 여부를 판단하여 요구사항대로 answer에 저장하고 반환하도록 구현하였음

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,46 @@
 function problem1(pobi, crong) {
+  let pobiMaxNumber = Math.max(calFromPage(pobi[0]), calFromPage(pobi[1]))
+  let crongMaxNumber = Math.max(calFromPage(crong[0]), calFromPage(crong[1]))
   var answer;
+  if(pobiMaxNumber > crongMaxNumber) {
+    answer = 1
+  } else if(pobiMaxNumber == crongMaxNumber) {
+    answer = 0
+  } else if(pobiMaxNumber < crongMaxNumber) {
+    answer = 2
+  } else {
+    answer = -1
+  }
+  console.log(pobiMaxNumber, crongMaxNumber, answer)
   return answer;
+}
+
+function calFromPage(page) {
+  let numbers = []
+  while(page > 0) {
+    number = page % 10
+    numbers.push(number)
+    page /= 10
+    page = Math.floor(page)
+  }
+  return Math.max(sumWithNumbers(numbers), mulWithNumbers(numbers))
+
+}
+
+function sumWithNumbers(numbers) {
+  let sumation = 0
+  numbers.forEach(function (number) {
+    sumation += number
+  })
+  return sumation
+}
+
+function mulWithNumbers(numbers) {
+  let multiplication = 1
+  numbers.forEach(function (number) {
+    multiplication *= number
+  })
+  return multiplication
 }
 
 module.exports = problem1;
