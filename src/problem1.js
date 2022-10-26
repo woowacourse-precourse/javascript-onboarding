1567ed22dbdac534c0ebc43d2c1820a07f3492d5
guridaek <iue16@naver.com>
2022-10-26 18:25:33 +0900

    feat: 문제1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,34 @@
 function problem1(pobi, crong) {
-  var answer;
+  // 예외 사항
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    return -1;
+  }
+
+  const pobi_score = Math.max(getScore(pobi[0]), getScore([pobi[1]]));
+  const crong_score = Math.max(getScore(crong[0]), getScore([crong[1]]));
+  let answer = -1;
+
+  if (pobi_score > crong_score) {
+    answer = 1;
+  } else if (pobi_score < crong_score) {
+    answer = 2;
+  } else if (pobi_score === crong_score) {
+    answer = 0;
+  }
+  
   return answer;
+}
+
+function getScore(page) {
+  const str = page.toString();
+  let add_score = 0;
+  let mul_score = 1;
+  for (let e of str) {
+    add_score += Number(e);
+    mul_score *= Number(e);
+  }
+
+  return Math.max(add_score, mul_score);
 }
 
 module.exports = problem1;
