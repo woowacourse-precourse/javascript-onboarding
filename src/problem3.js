f8c9e1f57e2a17360876e3b63bf47761cfdbb548
moonelysian <dandyblack@naver.com>
2022-10-26 19:10:10 +0900

    add: 박수칠 숫자들 갯수세는 함수 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
+function countTargetNumber(targets, number) {
+  const numbers = [...number.toString()];
+  return numbers.filter(num => targets.includes(parseInt(num))).length;
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  const clapTargets = [3, 6, 9];
+  for (let i = 1; i <= number; i++) {
+  }
 }
 
 module.exports = problem3;
