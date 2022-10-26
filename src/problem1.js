512469816e60a46c1ec8e003779eff62fe5a2afd
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-26 21:21:47 +0900

    연속하는 페이지 판별 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,8 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function checkContinousPage(pages) {
+  if (pages[0] + 1 == pages[1]) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = problem1;
