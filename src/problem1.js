192445ce12e6ff6c5cb7261fef9ff252f930f8db
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-26 20:03:42 +0900

    feat: correct checkPages in problem 1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const checkPages = (arr) => {
+    return arr[1] - arr[0] !== 1 || arr[0] === 1 || arr[1] === 400
+      ? true
+      : false;
+  };
+  if (checkPages(pobi) || checkPages(crong)) {
+    return -1;
+  } else {
+  }
 }
 
 module.exports = problem1;
