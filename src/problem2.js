ec93cb34c03cb445dcb131ff84b3f9bfce31d2be
GuHyeonWoo <ghw9174@gmail.com>
2022-10-27 08:29:49 +0900

    문자열 분해 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -2,8 +2,16 @@
 // 1. 문자열 분해
 // 2. 중복 검사
 
+// 문자열 분해
+function splitString(cryptogram) {
+  // 문자를 전부 분해하여 리스트로 반환
+  return cryptogram.split('');
+}
+
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  // 분해한 문자열을 변수에 할당
+  let string_list = splitString(cryptogram);
   return answer;
 }
 
