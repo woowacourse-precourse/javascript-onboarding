533b197d77cb94aba7af1555335b0c8ad22eb834
yu-retel <dbdudals100@naver.com>
2022-10-26 19:20:19 +0900

    feat: problem1 더하기, 곱하기 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -5,3 +5,11 @@ function problem1(pobi, crong) {
 }
 
 module.exports = problem1;
+
+function add(number){
+  return number.toString().split('').reduce((a,b)=>Number(a)+Number(b));
+}
+
+function multiply(number){
+  return number.toString().split('').reduce((a,b)=>Number(a)*Number(b));
+}
