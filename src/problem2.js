fc8771456411c2a9cce13cac9f851e1b3b7e770b
kangju2000 <kangju2000@naver.com>
2022-10-26 23:22:10 +0900

    feat: decryptor 함수 선언

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,19 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function decryptor(str) {
+  let newStr = '';
+  let isDeleteWord = false;
+
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] !== str[i + 1]) {
+      isDeleteWord ? (isDeleteWord = false) : (newStr += str[i]);
+    } else {
+      isDeleteWord = true;
+    }
+  }
+
+  return newStr === str || newStr === '' ? newStr : decryptor(newStr);
+}
+
 module.exports = problem2;
