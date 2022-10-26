a36316f72071b93789ed471a74afea5e6ee4d068
BB-choi <choibobae0809@gmail.com>
2022-10-26 21:21:38 +0900

    feat: 문제3 - 손뼉 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,16 @@
+function countClap(numbers) {
+  return numbers
+    .map(
+      (number) =>
+        number
+          .toString()
+          .split("")
+          .map(Number)
+          .filter((el) => el === 3 || el === 6 || el === 9).length
+    )
+    .reduce((a, b) => a + b, 0);
+}
+
 function problem3(number) {
   var answer;
   return answer;
