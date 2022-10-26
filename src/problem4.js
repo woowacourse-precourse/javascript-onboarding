0cfaadb7915d6c18d90a1fa0dbf8858b0987a0f7
ctd <qpflapffhs76@gmail.com>
2022-10-26 23:17:30 +0900

    feat: 사전 추가, 문자열을 순회하면서 정답 반환하는 로직 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,38 @@
 function problem4(word) {
   var answer = '';
+  const dictionary = {
+    A: 'Z',
+    B: 'Y',
+    C: 'X',
+    D: 'W',
+    E: 'V',
+    F: 'U',
+    G: 'T',
+    H: 'S',
+    I: 'R',
+    J: 'Q',
+    K: 'P',
+    L: 'O',
+    M: 'N',
+    N: 'M',
+    O: 'L',
+    P: 'K',
+    Q: 'J',
+    R: 'I',
+    S: 'H',
+    T: 'G',
+    U: 'F',
+    V: 'E',
+    W: 'D',
+    X: 'C',
+    Y: 'B',
+    Z: 'A',
+  };
+
+  for (let letter of word) {
+    answer += convertLetter(letter, dictionary);
+  }
+
   return answer;
 }
 
