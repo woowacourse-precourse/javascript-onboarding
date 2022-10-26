45bc7d8fc44e5da53874ecff3ac8f5d5917c7868
h2002y <h2002y@naver.com>
2022-10-26 16:42:19 +0900

    problem1 solved

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,89 @@
 function problem1(pobi, crong) {
+  if(handleExcept(pobi, crong) == false){
+    return -1;
+  }
   var answer;
+  let pobiMaxNum, crongMaxNum;
+  pobiMaxNum = getMaxNum(pobi);
+  crongMaxNum = getMaxNum(crong);
+  if(pobiMaxNum > crongMaxNum){
+    answer = 1;
+  }
+  else if(pobiMaxNum < crongMaxNum){
+    answer = 2;
+  }
+  else{
+    answer = 0;
+  }
   return answer;
+}
+
+function handleExcept(pobi, crong){
+  if((pobi[0] % 2 == 0) || (crong[0] % 2 == 0) || (pobi[1] % 2 == 1) || (crong[1] % 2 == 1)){
+    return false;
+  }
+  if(((pobi[0] + 1 != pobi[1])) || ((crong[0] + 1) != crong[1])){
+    return false;
+  }
+}
+
+function getMaxNum(player){
+  let sum, mul;
+  sum = getMaxSum(player[0], player[1]);
+  mul = getMaxMul(player[0], player[1]);
+  if(sum < mul){
+    return mul;
+  }
+  return sum;
+}
+
+function getMaxSum(left, right){
+  let leftSum = 0, rightSum = 0;
+  leftSum = getSum(left);
+  rightSum = getSum(right);
+  if(leftSum > rightSum){
+    return leftSum;
+  }
+  else{
+    return rightSum;
+  }
+}
+
+function getMaxMul(left, right){
+  let leftMul = 1, rightMul = 1;
+  leftMul = getMul(left);
+  rightMul = getMul(right);
+  if(leftMul > rightMul){
+    return leftMul;
+  }
+  else{
+    return rightMul;
+  }
+}
+
+function getSum(num){
+  let number = num, divider = 100, sum = 0;
+  for(let i = 0; i < 3; i++){
+    sum += parseInt(number / divider);
+    number %= divider;
+    divider /= 10;
+  }
+  return sum;
+}
+
+function getMul(num){
+  let number = num, divider = 100, mul = 1;
+  for(let i = 0; i < 3; i++){
+    if(parseInt(number / divider) == 0){
+      mul *= 1;
+    }
+    else{
+      mul *= parseInt(number / divider);
+    }
+    number %= divider;
+    divider /= 10;
+  }
+  return mul;
 }
 
 module.exports = problem1;
