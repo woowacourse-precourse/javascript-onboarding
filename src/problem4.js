d4ec873c7855de615dcd24313e492c17ca3381bb
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-26 17:58:52 +0900

    feat: problem4 replace, regexp 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const result = word
+    .replace(/[a-z]/g, (letter) =>
+      String.fromCharCode(lowerFlog(letter.charCodeAt(0)))
+    )
+    .replace(/[A-Z]/g, (letter) =>
+      String.fromCharCode(upperFlog(letter.charCodeAt(0)))
+    );
+  return result;
 }
 
 module.exports = problem4;
