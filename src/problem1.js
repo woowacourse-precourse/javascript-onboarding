7c34754d96e229c549e4f0b552e97cd02ed2b64d
adultlee <seoungin1228@gmail.com>
2022-10-26 17:35:07 +0900

    feat: 예상 가능한 함수를 선언
    선언한 함수들에 대해서 필요한 기능들을 명시합니다

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  // 포비와 크롱의 가장 큰값을 찾는다. getMaxNumber
+  // pobiMaxNum  , crongMaxNum 비교
+  // 비교후 결과를 반환한다
+}
+
+function getMaxNumber(oddNumber, evenNumber) {
+  // 모두 곱해서 나온결과  와 모두 더해서 나온 결과를 비교하기
+  // 더 큰값을 반환한다
+}
+
+function sumNumber(number) {
+  // number의 모두 더한 값을 반환
+}
+function multiNumber(number) {
+  // number의 모두 곱한 값을 반환
 }
 
 module.exports = problem1;
