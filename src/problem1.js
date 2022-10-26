f23544990cfc248ac76be8b5ec915723729c817c
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-26 23:50:18 +0900

    problem1 - feat: 헬퍼함수 작성
    1. 배열복사 함수
    2. 곱하기 함수
    3. 덧셈 함수
    4. 커스텀 each 함수
    5. 커스텀 map 함수

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,32 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+const copyArray = targetArray => {
+  return [...targetArray];
+};
+
+const multiplicationFunction = (first, second) => {
+  return first * second;
+};
+
+const plusFunction = (first, second) => {
+  return first + second;
+};
+
+const each = (iterable, func) => {
+  for (const iterator of iterable) {
+    func(iterator);
+  }
+
+  return;
+};
+
+const map = (array, func) => {
+  const resultArray = [];
+
+  each(array, element => resultArray.push(func(element)));
+
+  return resultArray;
+};
+
 module.exports = problem1;
