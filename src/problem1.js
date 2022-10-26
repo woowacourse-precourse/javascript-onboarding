fba363f91870d5cd39b98be71ab3d1078bb02352
River <swimmingr@gmail.com>
2022-10-26 21:29:35 +0900

    problem1 비교 숫자 만들기

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,36 @@
+
+function makePlus(pNum){
+  if(pNum>=100){
+    (pNum/100)*((pNum%100)/10)+(pNum%10);
+  }else if(pNum>=10){
+    (pNum/10)+(pNum%10);
+  }else{
+    return pNum;
+  }
+  return pNum;
+};
+function makeMul(mNum){
+  if(mNum>=100){
+    (mNum/100)*((mNum%100)/10)*(mNum%10);
+  }else if(mNum>=10){
+    (mNum/10)*(mNum%10);
+  }else{
+    return mNum;
+  }
+  return mNum;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  let pPobi1 = makePlus(pobi[0]);
+  let pPobi2 = makePlus(pobi[1]);
+  let mPobi1 = makeMul(pobi[0]);
+  let mPobi2 = makeMul(pobi[1]); 
+  
+  let pCrong1 = makePlus(crong[0]);
+  let pCrong2 = makePlus(crong[1]);
+  let mCrong1 = makeMul(crong[0]);
+  let mCrong2 = makeMul(crong[1]);
   return answer;
 }
 
