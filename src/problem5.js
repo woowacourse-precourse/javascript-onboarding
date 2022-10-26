e354d1a982778d07b0537e98a4bf435c82773017
frants-jeon <jjh6963@naver.com>
2022-10-27 04:27:07 +0900

    Feat (problem5): 메인 함수 완성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const moneyTable = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+  let leftMoney = money;
+
+  for (const unitOfMoney of moneyTable) {
+    answer.push(countExchangeableMoneyPerUnit(leftMoney, unitOfMoney));
+    leftMoney = calculateMoneyChange(leftMoney, unitOfMoney);
+  }
   return answer;
 }
 
