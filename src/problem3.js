ae821fe1ba81faf0fea83716c6cc4c51f2ee9101
frants-jeon <jjh6963@naver.com>
2022-10-27 00:18:25 +0900

    [feat] add clapCounter function

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,19 @@ function problem3(number) {
   return answer;
 }
 
+const clapCounter = (num) => {
+  const clapNumber = "369";
+  let counter = 0;
+  const numString = num.toString();
+  for (let i = 0; i < numString.length; i++) {
+    for (let j = 0; j < clapNumber.length; j++) {
+      if (numString[i] === clapNumber[j]) {
+        counter += 1;
+        break;
+      }
+    }
+  }
+  return counter;
+};
+
 module.exports = problem3;
