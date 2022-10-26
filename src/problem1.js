e1f97715d9d2d1d106a43ca767671d467b6bdcea
An Sol <apdlvmf486@gmail.com>
2022-10-26 23:26:35 +0900

    예외 처리 함수 checkExceptions 구현 및 예외처리 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,11 @@
 function problem1(pobi, crong) {
+  //예외사항: 페이지가 연속되지 않을 경우 -> 1. 앞페이지 >= 뒷페이지, 뒷페이지 - 앞페이지 !== 1 | 숫자 값이 아닌 경우 | 배열 길이가 2가 아님
+  function checkExceptions(arr){
+    if ((arr[0] >= arr[1]) || (arr[1] - arr[0] !== 1) || (Number.isNaN(arr[0]) || Number.isNaN(arr[1])) || arr.length !== 2) return true;
+    return false;
+  }
+  if (checkExceptions(pobi) || checkExceptions(crong)) return -1;
+
   function findBiggest(arr) {
     let biggest = 0;
     const numToString = arr.map(x => x.toString().split(''))
