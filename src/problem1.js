ee32e6741e68f1e86fcc5921e0232e01d0dbc368
vinivin153 <vinivin153@naver.com>
2022-10-26 18:49:22 +0900

    init: add check exception function

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,20 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function isPageError({ leftPage, rightPage }) {
+  if (rightPage - leftPage !== 1) {
+    return true;
+  }
+
+  if (!(1 <= leftPage && leftPage <= 399)) {
+    return true;
+  }
+
+  if (!(2 <= rightPage && rightPage <= 400)) {
+    return true;
+  }
+
+  return false;
+}
+
 module.exports = problem1;
