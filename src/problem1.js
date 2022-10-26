2315b43d7393e7d6cf9604964f1cabbcbf538b20
minjaeKim <smallkdb@gmail.com>
2022-10-27 00:18:06 +0900

    feat: [Problem1] Add exceptionChecker function
    예외 처리를 담당하는 exceptionChecker 함수를 추가하였습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,21 @@
+function exceptionChecker(array) {
+  if(!Array.isArray(array)) return false;
+  if(array.length !== 2) return false;
+  if(array[0]%2 !== 1 || array[1]%2 !== 0) return false;
+  if(array[1]-array[0] !== 1) return false;
+  if(array[0] < 1 || array[1] > 400) return false;
+
+  array.forEach((index) => {
+    if(!Number.isInteger(index)) return false;
+  });
+
+  return true;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if(!exceptionChecker(pobi) || !exceptionChecker(crong)) return -1;
+
   return answer;
 }
 
