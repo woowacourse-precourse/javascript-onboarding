99a70ef1da1860ddd6785aca6604785ee9cacf9d
oseunghyun <ohsh1477@gmail.com>
2022-10-26 23:09:11 +0900

    feat(p1): 곱과 합들 중 가장 큰 수를 본인의 점수로 구하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -22,6 +22,16 @@ const multiplyDigit = (num) => {
   }
 };
 
+// 곱과 합들 중 가장 큰 수를 본인의 점수로 구하는 기능
+const maxScore = (leftPage, rightPage) => {
+  return Math.max(
+    sumDigit(leftPage),
+    sumDigit(rightPage),
+    multiplyDigit(leftPage),
+    multiplyDigit(rightPage)
+  );
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
