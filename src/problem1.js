968394620dbc658c29d73fa70b303f31f145ca71
Ryong-E <think8867@gmail.com>
2022-10-26 22:14:40 +0900

    feat: 최대값 구해주는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,11 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function getMaxNumber(value){
+  const sum = value.toString().split('').map(Number).reduce((total,current) => total + current , 0);
+  const multiply = value.toString().split('').map(Number).reduce((total,current) => total * current , 1);
+  const max = Math.max(sum,multiply);
+
+  return max
+}
 module.exports = problem1;
