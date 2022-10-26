c4432153a3033d7e3c78d72fd77309afa9168155
frants-jeon <jjh6963@naver.com>
2022-10-27 03:00:21 +0900

    [feat] 알파벳 배열 생성 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -7,4 +7,17 @@ const isAlphabet = (str) => {
   return /[a-zA-Z]/.test(str);
 };
 
+const getAlphabetArray = (size) => {
+  if (size !== "large" && size !== "small")
+    return new Error("사이즈는 'large'와 'small'만 전달할 수 있습니다.");
+
+  const startUnicode = size === "large" ? 65 : 97;
+
+  const upperCaseAlphabetArray = Array.from(Array(26), (_, idx) =>
+    String.fromCharCode(idx + startUnicode)
+  );
+
+  return upperCaseAlphabetArray;
+};
+
 module.exports = problem4;
