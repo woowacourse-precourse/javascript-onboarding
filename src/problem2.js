0438ec8d9e224769143c3ac5c75b53233d5e5ef5
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-27 01:35:45 +0900

    문제 2번

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
   var answer;
+  let check = true;
+  while (check) {
+    check = false;
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (cryptogram[i] === cryptogram[i + 1]) {
+        cryptogram = cryptogram.substr(0, i) + cryptogram.substr(i + 2, cryptogram.length - 1);
+        check = true;
+      }
+    }
+  }
+  answer = cryptogram;
   return answer;
 }
-
+console.log(problem2("browoanoommnaon"));
 module.exports = problem2;
