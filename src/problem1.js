d2de21733f93703b1b12b65ad90b43d43dbc25fd
parkjikuk <paxkk@naver.com>
2022-10-26 22:06:47 +0900

    feat: 게임 결과값을 반환하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,13 @@ function problem1(pobi, crong) {
   const pobiScore = Math.max(parseInt(pobi.map(num => getScore(num))));
   const crongScore = Math.max(parseInt(crong.map(num => getScore(num))));
 
+  if(pobiScore > crongScore) {
+    return 1;
+  } else if(crongScore > pobiScore) {
+    return 2;
+  } else
+    return 0;
+
   function getScore(num) {
     const add = String(num).split("").reduce((acc, cur) => acc + parseInt(cur), 0);
     const multiply = String(num).split("").reduce((acc, cur) => acc * parseInt(cur));
