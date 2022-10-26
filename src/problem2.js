24e459f0fc04d334b457ea35b2bc833ee96916f4
khj0426 <706shin1728@naver.com>
2022-10-26 19:14:34 +0900

    :feat:2번 문제 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,37 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram;
+
+  while (true) {
+    let is_not_valid = false;
+    for (let i = 0; i < answer.length - 1; i++) {
+      if (answer[i] === answer[i + 1]) {
+        let next_index = 0;
+        for (let j = i + 1; j < answer.length; j++) {
+          if (answer[i] !== answer[j]) {
+            break;
+          } else {
+            is_not_valid = true;
+            next_index = j;
+          }
+        }
+        if (is_not_valid) {
+          let substr_first = answer.substr(0, i);
+          let substr_second = answer.substr(next_index + 1, answer.length);
+
+          answer = '';
+          answer += substr_first;
+          answer += substr_second;
+        } else {
+          break;
+        }
+      }
+    }
+
+    if (is_not_valid == false) {
+      break;
+    }
+  }
+
   return answer;
 }
 
