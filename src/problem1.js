d6a1dbd7fec9583dc86553144a4f31816fa9caf8
mjmj0217 <mjmj0217@naver.com>
2022-10-26 19:46:14 +0900

    feat: add main function and exception handling

--- src/problem1.js
+++ src/problem1.js
@@ -6,7 +6,6 @@
 4. 승자 알아내서 알맞은 결과 반환하게 만들기
 5. 예외 처리하기
 */
-
 function maxFromNumber(number) {
   let plus = 0;
   let multiple = 1;
@@ -24,6 +23,11 @@ function maxFromArray(arr) {
   const odd = arr[0];
   const even = arr[1];
 
+  //예외 처리
+  if (odd % 2 === 0 || even % 2 !== 0 || odd + 1 !== even) {
+    return -1;
+  }
+
   const oddMax = maxFromNumber(odd);
   const evenMax = maxFromNumber(even);
 
@@ -32,6 +36,25 @@ function maxFromArray(arr) {
 
 function problem1(pobi, crong) {
   var answer;
+
+  const pobiMax = maxFromArray(pobi);
+  if (pobiMax == -1) {
+    return -1;
+  }
+  const crongMax = maxFromArray(crong);
+  if (crongMax == -1) {
+    return -1;
+  }
+
+  if (pobiMax === crongMax) {
+    answer = 0;
+  } else if (pobiMax > crongMax) {
+    answer = 1;
+  } else if (pobiMax < crongMax) {
+    answer = 2;
+  } else {
+    answer = -1;
+  }
 
   return answer;
 }
