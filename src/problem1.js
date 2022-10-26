1935fdaf5fb873505cefe508a962502df1ab1bb2
inthhh <glass005533@gmail.com>
2022-10-26 23:38:43 +0900

    p1_FindEachMax

--- src/problem1.js
+++ src/problem1.js
@@ -10,7 +10,41 @@ function problem1(pobi, crong) {
   } else if (pobi[1] != pobi[0] + 1 || crong[1] != crong[0] + 1) {
     answer = -1;
   } else {
+    let pobiNums = [0,0];
+    let crongNums = [0,0];
+    let i = 10;
 
+    for (let n = 0; n < 2; ++n) {
+      let pobiSum = 0;
+      let pobiMult = 1;
+
+      pDigit = getDigit(pobi[n]);
+      while (pDigit > 0) {
+        let p = pobi[n] % i;
+        pobiSum += p;
+        pobiMult *= p;
+        pobi[n] = (pobi[n] - p) / 10;
+        pDigit--;
+      }
+      pobiNums[n] = Math.max(pobiSum, pobiMult);
+    }
+
+    for (let n = 0; n < 2; ++n) {
+      let crongSum = 0;
+      let crongMult = 1;
+
+      cDigit = getDigit(crong[n]);
+      while (cDigit > 0) {
+        let c = crong[n] % i;
+        crongSum += c;
+        crongMult *= c;
+        crong[n] = (crong[n] - c) / 10;
+        cDigit--;
+      }
+      crongNums[n] = Math.max(crongSum, crongMult);
+    }
+
+    
   }
   return answer;
 }
