b77628625824f8912e9dee02d8e231af81113f85
minseok1109 <park485201@naver.com>
2022-10-26 16:10:10 +0900

    feat: 페이지가 1보다 작고 400보다 큰 지 확인하는 함수. #기능목록-4

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,14 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function isInPage(arr1, arr2) {
+  if (
+    arr1.every((item) => item >= 1 && item <= 400) &&
+    arr2.every((item) => item >= 1 && item <= 400)
+  ) {
+    return true;
+  }
+  return false;
+}
+
 module.exports = problem1;
