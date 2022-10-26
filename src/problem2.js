e00b39880d22ebe46bd4ea8012d172159aa9cb56
haesa <xdhaesa@gmail.com>
2022-10-26 22:37:15 +0900

    problem2 중복 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -4,6 +4,20 @@ function checkDuplication(cryptogram) {
   return check;
 }
 
+function removeDuplication(cryptogram) {
+  let answer = '';
+  let i;
+  for (i = 0; i < cryptogram.length - 1; i++) {
+    if (cryptogram[i] !== cryptogram[i + 1]) answer += cryptogram[i];
+    else {
+      while (i < cryptogram.length - 1 && cryptogram[i] === cryptogram[i + 1])
+        i++;
+    }
+  }
+  if (i === cryptogram.length - 1) answer += cryptogram[i];
+  return answer;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
