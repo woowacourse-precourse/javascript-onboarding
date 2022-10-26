d0f5e4738da6115e48cd0a04b97dfc96c0570cf9
Gyeongm1n <affirming_km@naver.com>
2022-10-26 23:12:41 +0900

    대, 소문자 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,47 @@
-function problem4(word) {
-  var answer;
-  return answer;
+const A_ASCII = 65,
+  a_ASCII = 97;
+const Z_ASCII = 90,
+  z_ASCII = 122;
+
+function getLowerCaseConvert(code) {
+  const newCode = z_ASCII - Math.abs(code - a_ASCII);
+  console.log(newCode);
+
+  return String.fromCharCode(newCode);
 }
+
+function getUpperCaseConvert(code) {
+  const newCode = Z_ASCII - Math.abs(code - A_ASCII);
+  console.log(newCode);
+
+  return String.fromCharCode(newCode);
+}
+
+function getConvert(ch) {
+  const ch_code = ch.charCodeAt();
+  let result = '';
+
+  //소문자;
+  if (ch_code >= a_ASCII) {
+    result = getLowerCaseConvert(ch_code);
+  } else {
+    result = getUpperCaseConvert(ch_code);
+  }
+
+  return result;
+}
+
+function problem4(word) {
+  const regex = /^[a-z|A-Z]$/;
+  let answer = '';
+
+  for (ch of word) {
+    if (regex.test(ch)) {
+      getConvert(ch);
+    }
+  }
+}
+
+problem4('I love you');
 
 module.exports = problem4;
