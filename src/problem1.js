4abb66ca8d42205f8632bda92bc73d039ea50428
Ryan-Dia <cjfdnjs158@naver.com>
2022-10-26 22:48:28 +0900

    [문제1] 예외 처리 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,10 @@
+const exception = (pobi, crong) => {
+  if (pobi[0] === 1 || pobi[0] === 399 || crong[0] === 1 || crong[0] === 399)
+    return -1;
+  if (pobi.reduce((a, b) => Math.abs(a) - b) !== -1) return -1;
+  if (crong.reduce((a, b) => Math.abs(a) - b) !== -1) return -1;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
