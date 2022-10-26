b1a754e7d08b2afc83db548f7eb8a1eade31db78
Tory <88531407+1two13@users.noreply.github.com>
2022-10-26 22:42:58 +0900

    feat(problem4): 공백 예외 처리 코드 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
+// 기능 목록
+// 1. 공백 예외 처리
+// 2. 대문자는 대문자로, 소문자는 소문자로
+// 3. 제한사항 처리(알파벳 외의 문자는 변환하지 않기)
+
 function problem4(word) {
-  var answer;
+  let answer = "";
+
+  for (let i = 0; i < word.length; i++) {
+    // 공백 예외 처리
+    if (word[i] === " ") answer += " ";
+  }
+
   return answer;
 }
 
