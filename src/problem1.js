a64fcfcbed1d4fbeabee504bae4946cd2bb1f035
wonkyung <mwk0725@gmail.com>
2022-10-26 23:30:36 +0900

    feat: [기능 0] 각 페이지 별 큰 값 구하기 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,37 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  // 페이지 숫자 쪼개기
+  const pageLeftSplit = page => (page[0] + "").split("");
+  const pageRightSplit = page => (page[1] + "").split("");
+
+  // 페이지 왼쪽, 오른쪽 쪼갠 값
+  let pobiLeftPageNum = pageLeftSplit(pobi);
+  let crongLeftPageNum = pageLeftSplit(crong);
+  let pobiRightPageNum = pageRightSplit(pobi);
+  let crongRightPageNum = pageRightSplit(crong);
+
+  const maxValueFn = nums => {
+    let multiAllValue = 1;
+    let addAllValue = 0;
+
+    // 더한 값
+    const addNums = nums => {
+      for (let i = 0; i < nums.length; i++) {
+        addAllValue += parseInt(nums[i]);
+      }
+      return addAllValue;
+    };
+
+    // 곱한 값
+    const multiNums = nums => {
+      for (let i = 0; i < nums.length; i++) {
+        multiAllValue *= parseInt(nums[i]);
+      }
+      return multiAllValue;
+    };
+
+    // 페이지 최대 값
+    return Math.max(multiNums(nums), addNums(nums));
+  };
 }
 
 module.exports = problem1;
