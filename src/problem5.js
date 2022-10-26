d8661b9603749f4e157f140406d609cab187773e
CoboCho <rlaalsrb1111@naver.com>
2022-10-26 23:19:28 +0900

    feat: [problem5] map()으로 지폐 종류 배열에 withdraw() 실행

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
   let answer = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  answer = answer.map(unit => {
+    const result = withdraw(unit, money);
+    money = result.change;
+    return result.count;
+    });
+  return answer;
 }
 
 function withdraw(unit, money) {
