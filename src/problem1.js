afec49a3f2b17ba4ebeb0cdbb26aca8b0f218f38
2yunseong <dbsdltjd3701@naver.com>
2022-10-27 06:06:47 +0900

    feat(문제 1번): 두 페이지의 차이가 1인지 판단하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,10 @@ function isFitLength(arr, length) {
   return arr.length === length;
 }
 
+function isRightOrder(arr) {
+  return arr[arr.length - 1] - arr[0] === 1;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
