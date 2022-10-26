b41a99f3acbf80ddda64fee92f037f7609a9eb33
Blackraven93 <reblackraven@gmail.com>
2022-10-26 21:22:00 +0900

    Problem1: 왼쪽 페이지, 오른쪽 페이지가 순서대로 들어있는지 예외사항 체크 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,6 +3,11 @@ function problem1(pobi, crong) {
 
   // 책의 길이가 2가 넘는 경우
   if (pobi.length !== 2 || crong.length !== 2) return -1;
+
+  // 왼쪽 페이지, 오른쪽 페이지가 순서대로 들어있다. ( 둘의 차가 1이다. )
+  const subtractBookPage = (array) =>
+    array.reduce((previousValue, currentValue) => currentValue - previousValue);
+  if (subtractBookPage(pobi) !== 1 || subtractBookPage(crong) !== 1) return -1;
 }
 
 module.exports = problem1;
