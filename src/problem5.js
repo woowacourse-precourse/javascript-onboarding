054c003ed9573c63667a0d92060f2aaff808506f
bassyu <yrkdgus@gmail.com>
2022-10-26 19:55:16 +0900

    문제5 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+  for (let i = 0; i < 9; i++) {
+    const unit = units[i];
+    answer.push(parseInt(money / unit));
+    money %= unit;
+  }
   return answer;
 }
 
