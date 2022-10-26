0d21ba51667edc8ace4de01a51cb98b198b5a46a
haesa <xdhaesa@gmail.com>
2022-10-26 19:33:58 +0900

    problem1 예외 검사 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,17 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function isException(pages) {
+  const [leftPage, rightPage] = pages;
+  if (leftPage === 1 || rightPage === 400) return 1;
+  if (rightPage - leftPage !== 1) return 1;
+  return 0;
 }
 
+function exceptionCheck(pages1, pages2) {
+  return isException(pages1) || isException(pages2);
+}
+
+function problem1(pobi, crong) {
+  if (exceptionCheck(pobi, crong)) return -1;
+  let answer;
+  return answer;
+}
 module.exports = problem1;
