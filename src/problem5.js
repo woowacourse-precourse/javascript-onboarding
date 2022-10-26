40047ebb3348b6ad6e04f74dcbd3a582d7d748e4
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 23:08:27 +0900

    feat: 5번 기능 구현
    - units를 배열로 만들어서 배열 메소드 map을 이용해서 구현
    - 각 unit에 대해서 반복적인 작업을 하므로 map을 쓰고자 함
    - map 메소드에 사용하는 함수 getUnitNumber를 통해 restAmount 값 업데이트

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let restAmount = money;
+  let units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  function getUnitNumber(unit) {
+    let N = Math.floor(restAmount / unit);
+    restAmount = restAmount - N * unit;
+    return N;
+  }
+  let getUnitNumberArr = units.map((v) => getUnitNumber(v));
+  return getUnitNumberArr;
 }
 
 module.exports = problem5;
