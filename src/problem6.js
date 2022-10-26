49116879192e0e0e18d3f6631678ded2309e783a
bassyu <yrkdgus@gmail.com>
2022-10-26 21:13:34 +0900

    문제6 해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,30 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let log = {}
+  for (let i = 0; i < forms.length; i++) {
+    const [, nickname] = forms[i];
+    for (let j = 0; j < nickname.length - 1; j++) {
+      const pattern = nickname[j] + nickname[j+1];
+      if (!log[pattern]) {
+        log[pattern] = 1;
+        continue;
+      }
+      log[pattern] += 1;
+    }
+  }
+  let result = [];
+  for (let i = 0; i < forms.length; i++) {
+    const [email, nickname] = forms[i];
+    for (let j = 0; j < nickname.length - 1; j++) {
+      const pattern = nickname[j] + nickname[j+1];
+      if (log[pattern] >= 2) {
+        result.push(email);
+        break;
+      }
+    }
+  }
+  result.sort();
+
+  return result;
 }
 
 module.exports = problem6;
