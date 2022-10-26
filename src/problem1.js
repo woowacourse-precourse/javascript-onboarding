f13fc7df1df768e4d3c86e55134818621a624be6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-26 16:23:53 +0900

    feat: 페이지 점수 가져오기

--- src/problem1.js
+++ src/problem1.js
@@ -6,4 +6,11 @@ const checkException = (player1, player2) => {
   return (player1[1] - player1[0] !== 1) || (player2[1] - player2[0] !== 1);
 }
 
+const getPageScore = (pageNum) => {
+  const pageNumArr = [...String(pageNum)].map((n) => Number(n));
+  const plussedScore = pageNumArr.reduce((acc, cur) => acc + cur);
+  const multipliedScore = pageNumArr.reduce((acc, cur) => acc * cur);
+  return plussedScore > multipliedScore ? plussedScore : multipliedScore;
+}
+
 module.exports = problem1;
