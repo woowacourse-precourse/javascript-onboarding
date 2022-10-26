d535cbf58ccd3e62956ec09c2a9dde049cc4924c
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-26 21:07:02 +0900

    feat: problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = units.map((unit) => {
+    const count = parseInt(money / unit);
+    money %= unit;
+    return count;
+  });
+  return result;
 }
 
 module.exports = problem5;
