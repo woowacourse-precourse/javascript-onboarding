848c57c0959d3f4f6498c88824ab39851cef4093
thewronghand <penfreak77@gmail.com>
2022-10-27 02:05:56 +0900

    feat: multiplyArrayNumber 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -5,3 +5,4 @@ module.exports = problem1;
 
 const stringToNumberArray = (n) => String(n).split('').map(char => Number(char));
 const sumArrayNumber = (array) => array.reduce((prev, cur) => {return prev + cur}, 0);
+const multiplyArrayNumber = (array) => array.reduce((prev, cur) => {return prev * cur}, 1);
