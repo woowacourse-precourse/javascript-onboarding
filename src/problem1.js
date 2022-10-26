75f7b3d70f4468845f8d0900668d1be6405337f4
ctd <qpflapffhs76@gmail.com>
2022-10-26 16:48:29 +0900

    feat: 각 자리 숫자를 모두 곱해주는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -12,4 +12,12 @@ function sum(num) {
     .reduce((acc, cur) => acc + cur, 0);
 }
 
+function multiply(num) {
+  return num
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((acc, cur) => acc * cur, 1);
+}
+
 module.exports = problem1;
