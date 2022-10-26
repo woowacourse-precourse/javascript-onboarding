0defc1648765c41d1451201b8e8d607512119bd6
adultlee <seoungin1228@gmail.com>
2022-10-27 00:55:08 +0900

    feat: [pro2] stack을 활용하여 문제 2번 풀기
    stack을 사용하여 입력과 동시에 출력할 문자열을 보관하는 배열에서 중복되는 문자를 제거합니다. 아닌 경우 출력될 stack에 추가합니다

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let stringStack = [cryptogram[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (stringStack[stringStack.length - 1] === cryptogram[i])
+      stringStack.pop();
+    else stringStack.push(cryptogram[i]);
+  }
+  return stringStack.join("");
 }
 
 module.exports = problem2;
