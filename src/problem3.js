49e500dfc665b42e85f8a1e58f6bc856dbaa413f
khj0426 <706shin1728@naver.com>
2022-10-26 19:44:05 +0900

    feat:문제 3번 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,20 @@
+function add_if_possible(str) {
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == '3' || str[i] == '6' || str[i] == '9') {
+      count++;
+    }
+  }
+  return count;
+}
+
 function problem3(number) {
   var answer;
+  let str = '';
+  for (let i = 1; i <= number; i++) {
+    str += i.toString();
+    answer = add_if_possible(str);
+  }
   return answer;
 }
 
