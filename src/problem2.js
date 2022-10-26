3cbd18cfaec13125fcb3bff345cdc1914bcc6afe
han0224 <dbn0224@gmail.com>
2022-10-26 16:50:59 +0900

    [문제2] 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const cryptogramArray = cryptogram.split("");
+  let index = 0;
+  while (true) {
+    if (index >= cryptogramArray.length - 1) break;
+
+    if (cryptogramArray[index] === cryptogramArray[index + 1]) {
+      cryptogramArray.splice(index, 2);
+      index = 0;
+    } else index += 1;
+  }
+
+  return cryptogramArray.join("");
 }
 
 module.exports = problem2;
