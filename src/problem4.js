79a4b8ca3777141a49cd2c622545839b68f14147
adultlee <seoungin1228@gmail.com>
2022-10-27 02:38:50 +0900

    faet: [pro4] 대문자 구별하는 함수 구현
    여기서 대문자만을 구별하는 함수를 선언하였다. 소문자만 구별하는 함수의 필요하지 않을것이라 생각했다

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,9 @@ const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
 function problem4(word) {
   var answer;
   return answer;
+function isUpperCase(letter) {
+  if (UPPERCASE.includes(letter)) return true;
+  return false;
 }
 
 module.exports = problem4;
