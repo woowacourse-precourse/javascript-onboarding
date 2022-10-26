0280520f187b3dd2d505e2fb8be3584c1c828df6
olo90632951 <olo90632951@gmail.com>
2022-10-26 22:18:04 +0900

    feat: 임의의 문자열 연속하는 중복 문자 제거하는 기능 추가 [Problem2-1]
    스택을 이용하여 해결하였음

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return removeDuplicateWord(cryptogram);
 }
+
+const removeDuplicateWord = (words) => {
+  const stack = [];
+  words.split('').forEach((word) => {
+    const stackLastIndex = stack.length - 1;
+    if (stack.length === 0) {
+      stack.push(word);
+    } else if (stack[stackLastIndex] !== word) {
+      stack.push(word);
+    } else if (stack[stackLastIndex] === word) {
+      stack.pop();
+    }
+  });
+  return stack.join('');
+};
 
 module.exports = problem2;
