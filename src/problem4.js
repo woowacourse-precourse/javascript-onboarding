1b10dc39a70861b9828dd87baba03476f52ee6de
kangju2000 <kangju2000@naver.com>
2022-10-27 01:55:18 +0900

    feat: frogDictionary 함수 선언

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,10 @@ function problem4(word) {
   return answer;
 }
 
+function frogDictionary(alphabet) {
+  return alphabet === alphabet.toUpperCase()
+    ? String.fromCharCode(155 - alphabet.charCodeAt(0))
+    : String.fromCharCode(219 - alphabet.charCodeAt(0));
+}
+
 module.exports = problem4;
