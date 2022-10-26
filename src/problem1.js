7607ae2438c4d148fd5d74684e138a872d715997
MinGyu-Kang <kagrin97@gmail.com>
2022-10-26 19:48:01 +0900

    problem1 : 실제 페이지 값들을 계산하는 calculation 함수구현
    calculation함수에서 값들을 더하거나, 곱하거나 최대 값을 return 해줌
    
    plush함수에서는 각 자릿수 값들을 더해줌
    
    multiplication함수에서는 각 자릿수 값들을 곱해줌

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,31 @@
+function multiplication(str) {
+  return str
+    .split("")
+    .map(Number)
+    .reduce((pre, cur) => pre * cur);
+}
+
+function plus(str) {
+  return str
+    .split("")
+    .map(Number)
+    .reduce((pre, cur) => pre + cur);
+}
+
+function calculation(left, right) {
+  const leftPlusValue = plus(left);
+  const leftMultiplicationValue = multiplication(left);
+
+  const maxLeft = Math.max(leftPlusValue, leftMultiplicationValue);
+
+  const rightPlusValue = plus(right);
+  const rightMultiplicationValue = multiplication(right);
+
+  const maxRight = Math.max(rightPlusValue, rightMultiplicationValue);
+
+  return Math.max(maxLeft, maxRight);
+}
+
 function oddOrEven(leftNum, rightNum) {
   if (leftNum % 2 !== 1 || rightNum % 2 !== 0) {
     return true;
@@ -25,10 +53,20 @@ function problem1(pobi, crong) {
   if (exceptions(pobiLeft, pobiRight)) {
     return -1;
   }
+  const maxPobiValue = calculation(pobiLeft, pobiRight);
 
   const [crongLeft, crongRight] = crong.map(String);
   if (exceptions(crongLeft, crongRight)) {
     return -1;
+  }
+  const maxCrongValue = calculation(crongLeft, crongRight);
+
+  if (maxPobiValue > maxCrongValue) {
+    return 1;
+  } else if (maxPobiValue < maxCrongValue) {
+    return 2;
+  } else {
+    return 0;
   }
 }
 
