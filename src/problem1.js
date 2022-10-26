c4d8a723368886c61bf44b914270cc7760763c1f
minjaeKim <smallkdb@gmail.com>
2022-10-27 00:21:49 +0900

    feat: [Problem1] Add compareScore function
    두 게임 참가자 점수를 비교하는 compareScore 함수를 추가하였습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -4,11 +4,9 @@ function exceptionChecker(array) {
   if(array[0]%2 !== 1 || array[1]%2 !== 0) return false;
   if(array[1]-array[0] !== 1) return false;
   if(array[0] < 1 || array[1] > 400) return false;
-
   array.forEach((index) => {
     if(!Number.isInteger(index)) return false;
   });
-
   return true;
 }
 
@@ -24,13 +22,16 @@ function getScore([leftPage, rightPage]) {
     );
 }
 
+function compareScore(pobiScore, crongScore) {
+  if(pobiScore > crongScore) return 1;
+  if(pobiScore < crongScore) return 2;
+  return 0;
+}
+
 function problem1(pobi, crong) {
   var answer;
   if(!exceptionChecker(pobi) || !exceptionChecker(crong)) return -1;
-
-  const pobiScore = getScore(pobi);
-  const crongScore = getScore(crong);
-
+  answer = compareScore(getScore(pobi), getScore(crong));
   return answer;
 }
 
