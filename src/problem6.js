844db7e580b393ac0e045dc0853cd3e07f7d51d1
Rki0 <pky00823@gmail.com>
2022-10-26 23:04:12 +0900

    feat: prob6. 닉네임 및 이메일 제한 사항 만족 여부 판별 함수
    제한 사항에 명시되어 있는 조건에 따라
    닉네임과 이메일이 알맞은 형식을 갖추고 있는지 판별하는 함수입니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,42 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+function checkEmailLength(forms) {
+  for (let i = 0; i < forms.length; i++) {
+    if (forms[i][0].length < 11 || forms[i][0].length >= 20) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
+function checkEmailForm(forms) {
+  for (let i = 0; i < forms.length; i++) {
+    const parsedEmail = forms[i][0].split("@");
+
+    if (parsedEmail[1] !== "email.com") {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function checkNickNameLength(forms) {
+  for (let i = 0; i < forms.length; i++) {
+    if (forms[i][1].length < 1 || forms[i][1].length >= 20) {
+      return false;
+    }
+  }
+
+  return true;
+}
+
+function problem6(forms) {
+  if (
+    !checkEmailLength(forms) ||
+    !checkEmailForm(forms) ||
+    !checkNickNameLength(forms)
+  ) {
+    return;
+  }
+}
 module.exports = problem6;
