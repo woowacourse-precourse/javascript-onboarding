e9b9b3d6356f5a6a1f14894d4f884995d594d849
Ryan-Dia <cjfdnjs158@naver.com>
2022-10-26 22:52:25 +0900

    [문제1] 덧셈과 뺄셈중 Max값을 새로운 배열에 pobi와 crong을  구분하여 push하는 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,21 @@
+let pobiMax = [];
+let crongMax = [];
+
 const exception = (pobi, crong) => {
   if (pobi[0] === 1 || pobi[0] === 399 || crong[0] === 1 || crong[0] === 399)
     return -1;
   if (pobi.reduce((a, b) => Math.abs(a) - b) !== -1) return -1;
   if (crong.reduce((a, b) => Math.abs(a) - b) !== -1) return -1;
+};
+
+const calulateMax = (array, plus, multiplication, pobi) => {
+  plus > multiplication
+    ? array === pobi
+      ? pobiMax.push(plus)
+      : crongMax.push(plus)
+    : array === pobi
+    ? pobiMax.push(multiplication)
+    : crongMax.push(multiplication);
 };
 
 const getMaxNumber = (array, pobi) => {
@@ -14,6 +27,7 @@ const getMaxNumber = (array, pobi) => {
 };
 
 function problem1(pobi, crong) {
+  pobiMax, (crongMax = []);
   if (exception(pobi, crong) === -1) return -1;
   getMaxNumber(pobi, pobi);
   getMaxNumber(crong, pobi);
