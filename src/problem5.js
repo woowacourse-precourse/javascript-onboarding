d42ddcec14042dc9a2d9ef7a7af0356b1eb83b49
Rki0 <pky00823@gmail.com>
2022-10-26 20:40:14 +0900

    feat: prob5. 원금을 기준이 되는 금액으로 나눈 몫을 저장하는 함수
    원금을 기준 금액으로 나눈 몫을 저장하고
    배열화하여 반환한다.
    가장 큰 금액부터 나눗셈 연산을 진행하므로,
    높은 금액부터 배열에 들어가게된다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,29 @@
+/**
+ * 기준 금액으로 원금을 나눈 몫을 연산하고, 정답 배열에 넣어주는 함수
+ * @param {number} remain 연산 후 남은 금액
+ * @param {array} cash 원금을 나누는 기준이 되는 금액들이 들어있는 배열
+ * @param {array} ans 원금을 기준이 되는 금액으로 나눈 몫이 들어있는 배열
+ * @returns 원금을 기준이 되는 금액으로 나눈 몫을 원소로 가지는 배열
+ */
+function calculator(remain, cash, ans) {
+  for (let i = 0; i < cash.length; i++) {
+    const quotient = Math.floor(remain / cash[i]);
+
+    remain -= cash[i] * quotient;
+
+    ans.push(quotient);
+  }
+
+  return ans;
+}
+
 function problem5(money) {
-  var answer;
-  return answer;
+  const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let ans = [];
+  let remain = money;
+
+  return calculator(remain, cash, ans);
 }
 
 module.exports = problem5;
