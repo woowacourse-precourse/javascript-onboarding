e4ebdda8f9c55dc3b50b6f06a43efa4379a44868
lee-il-woo <empty001430@gmail.com>
2022-10-26 18:31:14 +0900

    기능 구현 완료

--- src/problem1.js
+++ src/problem1.js
@@ -8,6 +8,7 @@ function sumScore(score){
   return sum_score;
 }
 
+//페이지 숫자의 곱
 function mulScore(score){
   let mul_score = 1;
   score.map((element)=>{
@@ -16,33 +17,43 @@ function mulScore(score){
   return mul_score;
 }
 
-//pobi나 crong을 받아 점수차이를 계산하는 함수 
-function scoreDiff(page){
-  let first_score = Array.from(String(page[0]));
-  first_score = first_score.map((element)=>Number(element))
-  let second_score = Array.from(String(page[1]));
-  second_score = second_score.map((element)=>Number(element))
+//player의 페이지를 받아 가장 큰 점수를 계산하는 함수 
+function maxScore(player){
   //페이지 숫자를 배열로 변환 ex) 97 => [9,7]
-
+  let first_score = Array.from(String(player[0]));
+  first_score = first_score.map((element)=>Number(element))
+  let second_score = Array.from(String(player[1]));
+  second_score = second_score.map((element)=>Number(element))
+  //숫자 계산 결과
   first_sum = sumScore(first_score)
   second_sum = sumScore(second_score)
   first_mul = mulScore(first_score)
   second_mul = mulScore(second_score)
-  return [first_mul,second_mul];
+  //가장 큰 숫자 리턴
+  return Math.max(first_sum,first_mul,second_sum,second_mul)
 }
-
-console.log(scoreDiff([197,198]))
 
 
 function problem1(pobi, crong) {
-  var answer = -1;
+  var answer;
   //예외사항 if문으로 제외하기
-  if(pobi[1]-pobi[0] == 1 && crong[1]-crong[0] == 1){
-    return answer;
+  if(pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1){
+    return answer = -1;
   }else if((pobi[1] <= 2 && pobi[0] >= 399)&&(crong[1] <= 2 && crong[0] >= 399)){
-    return answer;
+    return answer = -1;
   }else if(pobi.length != 2 && crong.length != 2){
-    return answer;
-  }
+    return answer = -1;
+  }else{
+    //점수 비교
+    let pobi_score = maxScore(pobi);
+    let crong_score = maxScore(crong);
+    if(pobi_score > crong_score){
+      return answer = 1;
+    }else if(pobi_score == crong_score){
+      return amswer = 0;
+    }else if(pobi_score < crong_score){
+      return answer = 2;
+    }
+}
 }
 module.exports = problem1;
