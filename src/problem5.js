ac192cb524aa1e575f431fe1d1b22f697247248f
jangsumin <jsm0726@naver.com>
2022-10-27 00:33:58 +0900

    문제5 : 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -3,7 +3,14 @@
 // 2. 인자 money를 위 배열을 순회하여 나눠가면서 값을 변경하기
 
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let moneyKind = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
+  moneyKind.map(el => {
+    answer.push(Math.floor(money / el));
+    money %= el;
+  });
+
   return answer;
 }
 
