13dcf335732fdc39d5e672ffc1674515c10502bd
nlom0218 <nlom0218@naver.com>
2022-10-26 20:36:48 +0900

    feat: 문제 4 - 청개구리 사전을 참고해 반대로 변환하는 함수 만들기

--- src/problem4.js
+++ src/problem4.js
@@ -1,9 +1,21 @@
+const CHAR_LNEGTH = 26;
+const CHAR_ARR = new Array(26)
+  .fill()
+  .map((_, i) => String.fromCharCode(i + 97))
+  .join("");
+
 const isAlphabet = (char) => {
   return char.match(/[a-zA-z]/) ? true : false;
 };
 
 const isUpperCase = (char) => {
   return char.match(/[A-Z]/) ? true : false;
+};
+
+const convertChar = (char) => {
+  char = char.toLowerCase();
+  const idx = CHAR_ARR.indexOf(char);
+  return CHAR_ARR[CHAR_LNEGTH - idx - 1];
 };
 
 function problem4(word) {
