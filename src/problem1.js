4e79b6fbea87fe17b5308d078daeb02f81116654
jangsumin <jsm0726@naver.com>
2022-10-26 18:32:49 +0900

    문제1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,45 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const check = (arr) => {
+    if (arr.length > 2) return false;
+    if (arr[0] < 1) return false;
+    if (arr[1] > 400) return false;
+    if (arr[0] + 1 !== arr[1]) return false;
+    if (arr[0] % 2 !== 1) return false;
+    if (arr[0] % 2 !== 0) return false;
+    return true;
+  };
+
+  if (!check(pobi) || !check(crong)) return -1;
+  else {
+    let pobiResultArr = [];
+    let crongResultArr = [];
+
+    pobi.forEach(element => {
+      let sum = 0;
+      let mul = 1;
+      while (element) {
+        sum += element % 10;
+        mul *= element % 10;
+        element = Math.floor(element / 10);
+      }
+      pobiResultArr.push(sum, mul);
+    });
+
+    crong.forEach(element => {
+      let sum = 0;
+      let mul = 1;
+      while (element) {
+        sum += element % 10;
+        mul *= element % 10;
+        element = Math.floor(element / 10);
+      }
+      crongResultArr.push(sum, mul);
+    });
+
+    if (Math.max(...pobiResultArr) > Math.max(...crongResultArr)) return 1;
+    else if (Math.max(...pobiResultArr) > Math.max(...crongResultArr)) return 2;
+    else return 0;
+  }
 }
 
 module.exports = problem1;
