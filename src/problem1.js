5dea18a3b97037d1067649e5c22d11c33977ad01
adultlee <seoungin1228@gmail.com>
2022-10-26 18:32:49 +0900

    chore: 불필요한 console 제거

--- src/problem1.js
+++ src/problem1.js
@@ -46,7 +46,4 @@ function multiNumber(number) {
   return multi;
 }
 
-console.log(problem1([97, 98], [197, 198]));
-console.log(problem1([131, 132], [211, 212]));
-console.log(problem1([99, 102], [211, 212]));
 module.exports = problem1;
