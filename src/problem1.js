56be097fb0991b3275c18a687f2fb5f2c9b5b163
jungsmins1 <jungsmins00@gmail.com>
2022-10-27 00:17:54 +0900

    feat: 예외사항 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+  //예외사항 기능
+  const exceptions = (page) => {
+    const minPage = 1;
+    const maxPage = 400;
+  
+    if (page[0] < minPage || page[1] > maxPage) return false;
+    if (page[0] !== page[1] - 1) return false;
+    if (page.length !== 2) return false;
+    return true;
+  }
 }
 
-module.exports = problem1;
+problem1([97, 98], [197, 198])
+problem1([131, 132], [211, 212])
+problem1([99, 102], [211, 212])
+// module.exports = problem1;
