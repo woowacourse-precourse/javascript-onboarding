654d0630b0b45d48ca3a03b35e11c91e8369fe77
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-27 01:39:52 +0900

    [문제2] 중복문자열 제거 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function deleteDuplicatedWord(wordsArray) {
+  let count = 1;
+  let index = 0;
+  let targetIndex = [];
+
+  while (true) {
+    if (wordsArray[index] == wordsArray[index + 1]) {
+      while (wordsArray[index] == wordsArray[index + count]) {
+        count++;
+      }
+      targetIndex.push(index, count);
+      break;
+    }
+    index++;
+  }
+  wordsArray.splice(targetIndex[0], targetIndex[1]);
+
+  return wordsArray;
 }
 
 module.exports = problem2;
