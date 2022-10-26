1e5d8e2cbc6af76fba3cd4d9a81bf3c1b9b74436
haesa <xdhaesa@gmail.com>
2022-10-27 01:29:33 +0900

    problem4 청개구리 글자 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -6,6 +6,14 @@ function isLowerrCase(code) {
   return code >= 97 && code <= 122;
 }
 
+function convertGreenFrog(code) {
+  const SUM_UPPER_A_TO_Z = 155;
+  const SUM_LOWER_A_TO_Z = 219;
+  if (isUpperCase(code)) return SUM_UPPER_A_TO_Z - code;
+  if (isLowerrCase(code)) return SUM_LOWER_A_TO_Z - code;
+  return code;
+}
+
 function problem4(word) {
   var answer;
   return answer;
