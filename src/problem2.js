6f6a7ee21b10c7fb3d28f8bdc3e007feb22aebc4
evencoding <evencoding@gmail.com>
2022-10-27 02:37:46 +0900

    p2-implement: 다음으로 확인해야 할 cryptogram의 index를 반환하는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -5,12 +5,26 @@ function shouldPush(stack, cryp, idx) {
   return false;
 }
 
+function getNextIdx(lastElement, cryp, idx) {
+  let j = 0;
+  while (lastElement === cryp[idx + j]) {
+    j += 1;
+  }
+  if (j > 0) {
+    idx += j - 1;
+  }
+  return idx;
+}
+
 function problem2(cryptogram) {
   const stack = [];
   for (let i = 0; i < cryptogram.length; i++) {
     if (shouldPush(stack, cryptogram, i)) {
       stack.push(cryptogram[i]);
     } else {
+      const lastElement = stack[stack.length - 1];
+      i = getNextIdx(lastElement, cryptogram, i);
+      stack.pop();
     }
   }
   return stack.join('');
