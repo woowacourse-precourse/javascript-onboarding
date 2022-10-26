ec2e8a1116878d1be717b7bf467c83417c6000da
foundaway98 <foundaway98@naver.com>
2022-10-27 02:58:32 +0900

    덧셈 및 곱셈 연산 함수 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,18 +1,21 @@
-function makeSum(n) {
-  let val = 0;
-  for (let i = 0; i < n.length; i++) {
-    val += parseInt(n[i]);
-  }
-}
-
-function makeMul(n) {
-  let val = 0;
-  for (let i = 0; i < n.length; i++) {
-    val *= parseInt(n[i]);
-  }
-}
-
 function problem1(pobi, crong) {
+  function makeSum(n) {
+    let sum = 0;
+    do {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+    return sum;
+  }
+
+  function makeMul(n) {
+    let mul = 1;
+    do {
+      mul *= n % 10;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+    return mul;
+  }
   var answer;
   return answer;
 }
