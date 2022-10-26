4cddd815594ad42ba20fc0374dfb5e61f6b39ac1
gilpop8663 <wolfye@naver.com>
2022-10-27 01:01:25 +0900

    Add:[P2]_연속하는 중복 문자 삭제 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,46 @@
+function isOverlap(cryptogram) {
+  for (let i = 0; i < cryptogram.length - 1; i++) {
+    if (cryptogram[i] == cryptogram[i + 1]) return false;
+  }
+
+  return true;
+}
+
+function deduplication(cryptogram) {
+  cryptogram += "!";
+
+  let str = "";
+
+  for (let i = 0; i < cryptogram.length - 1; i++) {
+    if (cryptogram[i] == cryptogram[i + 1]) {
+      let cnt = 0;
+      for (let j = i; j < cryptogram.length - 1; j++) {
+        if (cryptogram[j] == cryptogram[j + 1]) {
+          cnt++;
+        } else break;
+      }
+
+      i += cnt;
+
+      continue;
+    }
+    str += cryptogram[i];
+  }
+
+  return str;
+}
+
 function problem2(cryptogram) {
   var answer;
+
+  let str = cryptogram;
+
+  while (!isOverlap(str)) {
+    str = deduplication(str);
+  }
+
+  answer = str;
+
   return answer;
 }
 
