a02908494085a524a58e241bdfaf39ef321d5f87
DongRyeong Lee <dr11291129@gmail.com>
2022-10-26 20:11:32 +0900

    feat: P1 exceptionCheck 작성

--- src/problem1.js
+++ src/problem1.js
@@ -31,4 +31,18 @@ function maxNumber(number1, number2) {
   return result;
 }
 
+function exceptionCheck(array1, array2) {
+  let isRightInput = true;
+
+  if (array1.length != 2 || array2.length != 2) isRightInput = false;
+  else if (array1[0] + 1 != array1[1]) isRightInput = false;
+  else if (array2[0] + 1 != array2[1]) isRightInput = false;
+  else if (array1[0] < 1 || 400 < array1[0]) isRightInput = false;
+  else if (array1[1] < 1 || 400 < array1[1]) isRightInput = false;
+  else if (array2[0] < 1 || 400 < array2[0]) isRightInput = false;
+  else if (array2[1] < 1 || 400 < array2[1]) isRightInput = false;
+
+  return isRightInput;
+}
+
 module.exports = problem1;
