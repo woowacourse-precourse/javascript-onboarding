2994cd4f8028a7c2dfbfefc64607b69828dc8355
cham-min <hymn9613@gmail.com>
2022-10-27 01:14:23 +0900

    feat: pobi, crong 값 비교 값 반환

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,12 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer = 0;
+
+  const pobiScore = Math.max(...pobi.map((num) => getMaxScore(num)));
+  const crongScore = Math.max(...crong.map((num) => getMaxScore(num)));
+
+  if (pobiScore > crongScore) answer = 1;
+  if (pobiScore < crongScore) answer = 2;
+
   return answer;
 }
 
