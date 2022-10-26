a2ff5b1fb96f7c375b5be1f6af4666037cc59719
frants-jeon <jjh6963@naver.com>
2022-10-27 04:25:21 +0900

    Feat (problem5): 화폐단위당 교환 가능한 화폐 개수 구하는 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,8 @@ function problem5(money) {
   return answer;
 }
 
+const countExchangeableMoneyPerUnit = (money, unitOfMoney) => {
+  return Math.floor(money / unitOfMoney);
+};
+
 module.exports = problem5;
