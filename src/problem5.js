0d81e3b8f5ce3e95ddb518ad4d051d568f19d1f5
kangju2000 <kangju2000@naver.com>
2022-10-27 02:31:46 +0900

    feat: 문제 5 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  return arr.map((value) => {
+    let count = Math.floor(money / value);
+    if (count > 0) {
+      money -= value * count;
+    }
+    return count;
+  });
 }
 
 module.exports = problem5;
