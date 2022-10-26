fb38495579ef23ebfd0ff9d3fd3f23dda57c919e
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-26 17:59:33 +0900

    feat: problem4 lowerFlog 함수

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,15 @@
+function lowerFlog(letterCode) {
+  const mid = "m".charCodeAt(0);
+  if (letterCode <= mid) {
+    return letterCode + (mid - letterCode) * 2 + 1;
+  }
+  if (letterCode > mid) {
+    return letterCode - (letterCode - mid) * 2 + 1;
+  }
+}
+
+function upperFlog(letterCode) {}
+
 function problem4(word) {
   const result = word
     .replace(/[a-z]/g, (letter) =>
