c56bc8e46b54b4d3e9322ec76f0cc28de304d1cb
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-26 23:32:03 +0900

    feat: problem1.js에 페이지 유효성을 검사하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
+const isValidPages = (pages) => {
+  if ((pages[0] <= 1) || (pages[1] >= 400) || (pages[0] + 1 !== pages[1])) {
+    return false;
+  }
+
+  return true;
+};
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!(isValidPages(pobi) && isValidPages(crong))) {
+    return -1;
+  }
 }
 
 module.exports = problem1;
