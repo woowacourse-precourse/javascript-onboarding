196a65a9b176358af3fdae2bc8516e5720cb3005
saejinpark <saejinpark951210@gmail.com>
2022-10-26 17:53:28 +0900

    정답수정 문제6 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,9 +1,9 @@
-const MONEY_UNIT_ARY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+const MONEY_UNIT_ARR = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  let answer = Array.from({ length: MONEY_UNIT_ARY.length }, () => 0);
+  let answer = Array.from({ length: MONEY_UNIT_ARR.length }, () => 0);
   let remain = money;
-  MONEY_UNIT_ARY.map((unit, i) => {
+  MONEY_UNIT_ARR.map((unit, i) => {
     answer[i] = parseInt(remain / unit);
     remain = remain % unit;
   });
--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  let limitTextObj = {};
+  forms.map((form) => {
+    const [_, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substring(i, i + 2);
+      if (testText in limitTextObj) limitTextObj[testText] += 1;
+      else limitTextObj[testText] = 1;
+    }
+  });
+
+  forms.map((form) => {
+    const [email, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substring(i, i + 2);
+      if (limitTextObj[testText] > 1) answer.push(email);
+    }
+  });
   return answer;
 }
 
