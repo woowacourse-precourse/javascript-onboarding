ea5ef7dee015bad880f7d561eaac651a24e72238
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-26 17:03:19 +0900

    feat: problem2 stack 이용

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const cryptogramArr = Array.from(cryptogram);
+  const stack = [];
+  while (cryptogramArr.length) {
+    const cur = cryptogramArr.shift();
+    if (stack.length && stack[stack.length - 1] === cur) {
+      stack.pop();
+      continue;
+    }
+    stack.push(cur);
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
