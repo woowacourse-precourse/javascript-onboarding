03db085fda5966de8b36dc8af79a239ea6d91b38
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-26 22:08:00 +0900

    feat: problem6 중복확인 로직 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,32 @@
+function getKeys(nickname) {
+  const arr = [];
+  let num = 2;
+  while (num <= nickname.length) {
+    let cur = 0;
+    while (cur + num <= nickname.length) {
+      arr.push(nickname.substr(cur, num));
+      cur++;
+    }
+    num++;
+  }
+  return arr;
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  const list = {};
+  forms.map((form) => {
+    getKeys(form[1]).map((key) => {
+      if (key in list) list[key].push(form[0]);
+      else list[key] = [form[0]];
+    });
+  });
+  console.log(
+    Object.values(list).reduce((result, data) => {
+      if (data.length > 1) return result.concat(data);
+      return result;
+    }, [])
+  );
+  console.log(list);
 }
 
 module.exports = problem6;
