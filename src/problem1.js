d655780daf8eaa68392c6ce5423eff7fa29f2a19
KES <aprilsmaple@gmail.com>
2022-10-26 17:49:33 +0900

    feat: problem1. get max num of player

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,32 @@
 // 16:56 ~
 function problem1(pobi, crong) {
-  if (!isPagesCorrect(pobi) || isPagesCorrect(crong)) return -1;
-  let answer = 0;
-  return answer;
+  if (!isPagesCorrect(pobi) || !isPagesCorrect(crong)) return -1;
+  let pobiMaxNum = getMaxNum(pobi);
+  let crongMaxNum = getMaxNum(crong);
+  console.log(pobiMaxNum, crongMaxNum);
+}
+
+function getMaxNum(pages) {
+  return Math.max(
+    multiplyDigits(pages[0]),
+    addDigits(pages[0]),
+    multiplyDigits(pages[1]),
+    addDigits(pages[1])
+  );
+}
+
+function multiplyDigits(page) {
+  return page
+    .toString()
+    .split("")
+    .reduce((a, c) => a * Number(c), 1);
+}
+
+function addDigits(page) {
+  return page
+    .toString()
+    .split("")
+    .reduce((a, c) => a + Number(c), 0);
 }
 
 function isPagesCorrect(pages) {
@@ -11,4 +35,5 @@ function isPagesCorrect(pages) {
     ? false
     : true;
 }
+problem1([97, 98], [197, 198]);
 module.exports = problem1;
