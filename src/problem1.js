74d44baaaa3873ad16f58f09050e007c5c208e8c
pakxe <pigkill40@naver.com>
2022-10-26 20:02:47 +0900

    chore: 유효성 검사 함수 isValid 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,6 @@
 //페이지 유효성 검사
 const isValidPage = (book1, book2) => {
-  if (book1[0] <= 1 || book1[2] >= 400 || book2[0] <= 1 || book2[2] >= 400)
+  if (book1[0] <= 1 || book1[1] >= 400 || book2[0] <= 1 || book2[1] >= 400)
     return false;
   else if (book1[1] - book1[0] !== 1) return false;
   else if (book2[1] - book2[0] !== 1) return false;
@@ -44,10 +44,7 @@ const maxNum = (book) => {
   return max;
 };
 
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+function problem1(pobi, crong) {}
 
 // 여기에 테스트
 
