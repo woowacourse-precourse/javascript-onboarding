e9e4b1476f56e010bb481726b37653164605eb51
pakxe <pigkill40@naver.com>
2022-10-27 06:34:40 +0900

    refactor: [P4] 알파벳인지 확인하는 함수를 제거하고 청개구리 반환 함수의 else 문에서 수행하도록 함

--- src/problem4.js
+++ src/problem4.js
@@ -1,27 +1,15 @@
-const ALPHABET = /^[a-z|A-Z]+$/;
 const UPPER_ALPHABET = /^[A-Z]+$/;
-
+const LOWER_ALPHABET = /^[a-z]+$/;
 const ASCII_UPPER_A = 65;
 const ASCII_UPPER_Z = 90;
-
 const ASCII_LOWER_A = 97;
 const ASCII_LOWER_Z = 122;
 
 function problem4(word) {
-  let reverseStr = "";
-
-  for (let i = 0; i < word.length; i++) {
-    if (!isAlphabet(word[i])) reverseStr += word[i];
-    else reverseStr += reverseAlphabet(word[i]);
-  }
-
-  return reverseStr;
-}
-
-// 주어진 문자가 알파벳인지 확인하는 함수
-function isAlphabet(char) {
-  if (ALPHABET.test(char)) return true;
-  else return false;
+  return word
+    .split("")
+    .map((char) => reverseAlphabet(char))
+    .join("");
 }
 
 // 주어진 문자를 청개구리로 반환하는 함수
@@ -30,10 +18,12 @@ function reverseAlphabet(char) {
     return String.fromCharCode(
       ASCII_UPPER_Z - (char.charCodeAt() - ASCII_UPPER_A)
     );
-  } else {
+  } else if (LOWER_ALPHABET.test(char)) {
     return String.fromCharCode(
       ASCII_LOWER_Z - (char.charCodeAt() - ASCII_LOWER_A)
     );
+  } else {
+    return char;
   }
 }
 
