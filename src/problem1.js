c958a4113a5714a06190db90c655fa7454425cbf
NightlyHerb <82109216+nightlyherb@users.noreply.github.com>
2022-10-26 16:58:47 +0900

    p1:add: validation code

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,35 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+/**
+ * @typedef {number & { __brand: "PageNumber" }} PageNumber
+ */
+
+/**
+ * @param {unknown} x
+ * @returns {PageNumber}
+ */
+function parsePageNumberOrThrow(x) {
+  const isPageNumber =
+    typeof x === "number" && x % 1 === 0 && x >= 1 && x <= 400;
+  if (!isPageNumber) {
+    throw new TypeError(`Expected PageNumber, got ${x}`);
+  }
+  return /** @type {PageNumber} */ (x);
 }
 
-module.exports = problem1;
+/**
+ * @typedef {[PageNumber, PageNumber] & {__brand: "PagePair"}} PagePair
+ */
+
+/**
+ * @param {[PageNumber, PageNumber]} pagePair
+ * @returns {PagePair}
+ */
+function parsePagePairOrThrow(pagePair) {
+  const [leftPage, rightPage] = pagePair;
+  const isLeftPageOddNumber = leftPage % 2 === 1 || leftPage % 2 === -1;
+  const arePagesConsecutive = rightPage - leftPage === 1;
+  const isValidPagePair = isLeftPageOddNumber && arePagesConsecutive;
+  if (!isValidPagePair) {
+    throw new TypeError(`Expected Valid PagePair, got ${pagePair}`);
+  }
+  return /** @type {PagePair} */ (pagePair);
+}
