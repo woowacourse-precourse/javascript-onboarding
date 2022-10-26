d5f6012de6283665b9ed81c2f2c144511600f4b4
yuu <22yuu@naver.com>
2022-10-26 17:17:03 +0900

    feat: sovle problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
 function problem3(number) {
-  var answer;
+  let answer = null;
+  
+  
+  for(let i = 1; i <= number; i++) {
+    answer += countTargetNumbers(i);
+  }
   return answer;
 }
+
+function countTargetNumbers(num) {
+  let count = 0;
+  while(num > 0) {
+    let n = num % 10;
+    if(n == 3 || n == 6 || n == 9) count++;
+    num = Math.floor(num/10);
+  }
+
+  return count;
+}
+
+// console.log(problem3(13));
+// console.log(problem3(33));
 
 module.exports = problem3;
