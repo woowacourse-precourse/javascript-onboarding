3ad1cb457865c652881cb7c1575f4e4c84257adb
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-26 21:17:22 +0900

    fix: [문제4] 아스키코드 변수 수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,26 +1,26 @@
 function problem4(words) {
   const getOppositeAlphabet = (character) => {
-    if (!isUpperCase(character) && !isLowerCase(character)) {
+    const characterAscii = character.charCodeAt(0);
+
+    if (!isUpperCase(characterAscii) && !isLowerCase(characterAscii)) {
       return character;
     }
 
-    const asciiCharacter = character.char.CodeAt(0);
-
-    if (isUpperCase(character)) {
-      return String.fromCharCode(155 - asciiCharacter);
+    if (isUpperCase(characterAscii)) {
+      return String.fromCharCode(155 - characterAscii);
     }
 
-    if (isLowerCase(character)) {
-      return String.fromCharCode(219 - asciiCharacter);
+    if (isLowerCase(characterAscii)) {
+      return String.fromCharCode(219 - characterAscii);
     }
   };
 
-  const isUpperCase = (character) => {
-    return asciiCharacter >= 65 && asciiCharacter <= 90;
+  const isUpperCase = (characterAscii) => {
+    return characterAscii >= 65 && characterAscii <= 90;
   };
 
-  const isLowerCase = (character) => {
-    return asciiCharacter >= 97 && asciiCharacter <= 122;
+  const isLowerCase = (characterAscii) => {
+    return characterAscii >= 97 && characterAscii <= 122;
   };
 
   if (typeof words !== 'string' || words.length < 1 || words.length > 1000) {
