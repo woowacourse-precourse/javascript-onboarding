7649e8ef376be7704f763bc12672705834e3a0cc
h2002y <h2002y@naver.com>
2022-10-26 18:00:12 +0900

    problem2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,51 @@
+class Stack{
+  constructor(){
+    this.stack = [];
+  }
+  push(value){
+    this.stack.push(value);
+  }
+  pop(){
+    return this.stack.pop();
+  }
+  peek(){
+    return this.stack[this.stack.length - 1];
+  }
+  isEmpty(){
+    if(this.stack.length == 0) return true;
+    return false;
+  }
+  reverse(){
+    return this.stack.reverse();
+  }
+  join(){
+    return this.stack.join("");
+  }
+}
+
 function problem2(cryptogram) {
-  var answer;
+  var answer = "";
+  let inputLen = cryptogram.length;
+  const stack = new Stack();
+  for(let i = inputLen - 1; i >= 0; i--){
+    if(stack.isEmpty() || (cryptogram[i] != stack.peek())){
+      stack.push(cryptogram[i]);
+    }
+    else if(cryptogram[i] == stack.peek()){
+      decipher(cryptogram[i], stack);
+    }
+  }
+  if(!stack.isEmpty()){
+    stack.reverse();
+    answer = stack.join();
+  }
   return answer;
+}
+
+function decipher(crypto, stack){
+  while(!stack.isEmpty() && crypto == stack.peek()){
+    stack.pop();
+  }
 }
 
 module.exports = problem2;
