5d7c557c93452787a7ac9e790c49ac0f70b5f740
haesa <xdhaesa@gmail.com>
2022-10-26 22:36:35 +0900

    problem2 중복 체크 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
+function checkDuplication(cryptogram) {
+  const check = [...cryptogram] //
+    .some((x, i, cryptogram) => x === cryptogram[i + 1]);
+  return check;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
 }
-
 module.exports = problem2;
