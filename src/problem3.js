dc9a5857800295ae82ae138822468a3b76dd03df
nlom0218 <nlom0218@naver.com>
2022-10-26 19:07:14 +0900

    feat: 문제 3 - 특정 숫자의 3, 6, 9 개수 구하는 함수 만들기

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,14 @@
+const targetNums = [3, 6, 9];
+
+const countTargetNums = (num) => {
+  let count = 0;
+  num = num + "";
+  num.split("").forEach((item) => {
+    if (targetNums.includes(Number(item))) count++;
+  });
+  return count;
+};
+
 function problem3(number) {
   var answer;
   return answer;
