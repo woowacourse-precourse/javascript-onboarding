59c83c2e4c6faf504284df4c8084a7cfd093588e
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 21:17:45 +0900

    feat: 3번 문제 기능구현
    - 한 숫자의 자릿수랑 전체 수에 대해 for 문을 사용해 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  function get369Count(num) {
+    let str = String(num);
+    let answer = 0;
+    for (let i = 0; i < str.length; i++) {
+      let tempChar = str.charAt(i);
+      if (tempChar === "3" || tempChar === "6" || tempChar === "9") {
+        answer++;
+      }
+    }
+    return answer;
+  }
+  for (let i = 1; i <= number; i++) {
+    count += get369Count(i);
+  }
+  return count;
 }
 
 module.exports = problem3;
