ea5b9831ce05918df018e04b55c2eb0606365971
Rki0 <pky00823@gmail.com>
2022-10-26 16:23:37 +0900

    feat:prob1. 숫자를 배열로 변환하는 함수
    페이지 숫자를 배열로 변환하여 합, 곱을 구하기 쉽게 만들어주는 함수를 추가했습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,20 @@
 /**
+ * 한쪽 페이지의 숫자를 배열 형태로 변환하는 함수
+ * @param {number} eachSideNum 한쪽 면의 페이지 숫자
+ * @returns 숫자를 각 자릿수로 쪼깬 배열. 각 원소는 number 타입.
+ */
+function makeSplitedArr(eachSideNum) {
+  let eachSide = eachSideNum;
+
+  eachSide = eachSide
+    .toString()
+    .split("")
+    .map((item) => Number(item));
+
+  return eachSide;
+}
+
+/**
  * 예외 상황을 판별하는 함수
  * @param {array} who 펼친 페이지 번호가 들어있는 배열
  * @returns 예외상황이 맞다면 true, 아니라면 false 반환
