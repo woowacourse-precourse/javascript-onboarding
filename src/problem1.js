359add5127d2deb410a46034cbeae4e53bbd0bd3
b1y9mn <b1y9mn@gmail.com>
2022-10-27 03:49:50 +0900

    feat: 페이지 불연속 예외 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+  if (pobi[1] !== (pobi[0] + 1) || crong[1] !== (crong[0] + 1)) {
+    answer = -1;
+    return answer;
+  } 
+  
+  answer = 1;
   return answer;
 }
 
 module.exports = problem1;
+
+
+console.log(problem1([1, 2] , [6, 4]));
