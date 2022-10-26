0a0157dba4de633d22c4ee803ab40e0cfe306b2a
Minyoung Lee <mminy62@gmail.com>
2022-10-27 01:41:36 +0900

    [feat] 1-페이지 각 자리수의 합, 곱 구하기
    기능 구현 단계를 2단계로 나눔.

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,29 @@
+function userMax(userPages){
+  let resultUser = [];
+
+  // 기능 1: 페이지 각 자리수의 합, 곱 구하기
+  userPages.forEach(function(item){
+    let tempSum = 0;
+    let tempMul = 1;
+    let page = item.toString();
+    
+    for(let i = 0; i < page.length; i++){
+      tempSum += parseInt(page[i])
+      tempMul *= parseInt(page[i])
+    }
+
+    resultUser.push(tempSum);
+    resultUser.push(tempMul);
+  });
+
+  return Math.max(...resultUser);
+
+}
+
+
 function problem1(pobi, crong) {
   var answer;
+
   return answer;
 }
 
