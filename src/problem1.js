590c3d9ba8c727b13627eec27bb2bc0c23c9c093
ilgon0110 <moses011010@gmail.com>
2022-10-26 21:14:05 +0900

    feat: 예외처리 기능 구현(#problem1)

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,18 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  //예외처리
+  //기능목록 1-1
+  if (
+    pobi[0] % 2 !== 1 ||
+    pobi[1] % 2 !== 0 ||
+    crong[0] % 2 !== 1 ||
+    crong[1] % 2 !== 0
+  )
+    return -1;
+  //기능목록 1-2
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
+
   return answer;
 }
 
