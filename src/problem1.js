7aa4205fb1e347659062412fb5d58163b55a07e3
ctd <qpflapffhs76@gmail.com>
2022-10-26 16:48:09 +0900

    feat: 각 자리 숫자를 모두 더해주는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
   var answer;
+
   return answer;
+}
+
+function sum(num) {
+  return num
+    .toString()
+    .split('')
+    .map(Number)
+    .reduce((acc, cur) => acc + cur, 0);
 }
 
 module.exports = problem1;
