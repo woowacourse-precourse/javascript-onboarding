8089639102891ed021497abbc5d472ab282983d4
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-27 01:23:51 +0900

    feat: 입력값 예외처리 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
+const checkValid = (arr1, arr2) => {
+    let flag = true;
+    if(arr1[1] - arr1[0] !== 1 || arr2[1] - arr2[0] !== 1) {
+        flag = false;
+    } 
+    return flag;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let result;
+  if(!checkValid(pobi, crong)) result = -1;
+  return result;
 }
 
 module.exports = problem1;
