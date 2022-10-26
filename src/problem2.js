ff03f8644e74738e2c497930604e3ab1c24296ad
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-26 23:10:33 +0900

    enhance: problem2 add input error check logic

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,10 @@
-const { infinityDeleteDuplicateWord } = require("./problem2.util");
+const {
+  infinityDeleteDuplicateWord,
+  checkInputError,
+} = require("./problem2.util");
 
 function problem2(cryptogram) {
+  if (checkInputError(cryptogram)) return "error";
   return infinityDeleteDuplicateWord(cryptogram);
 }
 
