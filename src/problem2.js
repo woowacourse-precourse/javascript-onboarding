514b766d265546c6fa3f5c420fda7e30b4fb77fa
Tory <88531407+1two13@users.noreply.github.com>
2022-10-26 21:20:22 +0900

    feat(problem2): 연속하는 중복 문자열 제거하는 코드 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,16 @@
+// 기능 목록
+// 1. 연속하는 중복 문자열 모두 제거
+
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram[0];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    let top = answer[answer.length - 1];
+
+    if (top !== cryptogram[i]) answer += cryptogram[i];
+    else answer = answer.slice(0, -1);
+  }
+
   return answer;
 }
 
