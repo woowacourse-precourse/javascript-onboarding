e321506334d54c6f9e362ac20a47a481815b208c
yeong bin <2976622@naver.com>
2022-10-27 01:00:26 +0900

    fix: problem4 - getReversedChar 알파벳 외 문자 처리

--- src/problem4.js
+++ src/problem4.js
@@ -5,9 +5,12 @@
  */
 function getReversedChar(char) {
   const ascii = char.charCodeAt(0);
-  if (ascii >= 97)
+  if (ascii >= 97 && ascii <= 122)
     return String.fromCharCode(97 + 97 + 25 - ascii);
-  return String.fromCharCode(65 + 65 + 25 - ascii);
+  else if (ascii >= 65 && ascii <= 90)
+    return String.fromCharCode(65 + 65 + 25 - ascii);
+  else
+    return char;
 }
 
 function problem4(word) {
