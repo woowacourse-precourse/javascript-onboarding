773d4c22d08dc372504edccde0bfb89e6df3e21f
dkile <dkile532@naver.com>
2022-10-27 01:47:49 +0900

    refactor: change test case description format to Method_Scenario_ExpectedBehavior

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -7,115 +7,115 @@ const problem6 = require("../src/problem6");
 const problem7 = require("../src/problem7");
 
 describe("problem1", () => {
-  test("case1", () => {
+  test("Problem1_SameMultiplicationResult_Return0", () => {
     expect(problem1([97, 98], [197, 198])).toEqual(0);
   });
 
-  test("case2", () => {
+  test("Problem1_Param1SumBiggerThanParam2Sum_Return1", () => {
     expect(problem1([131, 132], [211, 212])).toEqual(1);
   });
 
-  test("case3", () => {
+  test("Problem1_Param1PageDiffNotOneException_Return-1", () => {
     expect(problem1([99, 102], [211, 212])).toEqual(-1);
   });
 
-  test("case4", () => {
+  test("Problem1_Param1FirstPageBoundaryException_Return-1", () => {
     expect(problem1([1, 2], [211, 212])).toEqual(-1);
   });
 
-  test("case5", () => {
+  test("Problem1_Param2FirstPageBoundaryException_Return-1", () => {
     expect(problem1([211, 212], [1, 2])).toEqual(-1);
   });
 
-  test("case6", () => {
+  test("Problem1_Param2LastPageBoundaryException_Return-1", () => {
     expect(problem1([211, 212], [399, 400])).toEqual(-1);
   });
 
-  test("case7", () => {
+  test("Problem1_Param1LastPageBoundaryException_Return-1", () => {
     expect(problem1([399, 400], [211, 212])).toEqual(-1);
   });
 
-  test("case8", () => {
+  test("Problem1_Param1OutOfPageBoundaryException_Return-1", () => {
     expect(problem1([401, 402], [211, 212])).toEqual(-1);
   });
 
-  test("case9", () => {
+  test("Problem1_Param2OutOfPageBoundaryException_Return-1", () => {
     expect(problem1([211, 212], [401, 402])).toEqual(-1);
   });
 
-  test("case10", () => {
+  test("Problem1_Param1OutOfPageBoundaryException_Return-1", () => {
     expect(problem1([-1, 0], [211, 212])).toEqual(-1);
   });
 
-  test("case11", () => {
+  test("Problem1_Param2OutOfPageBoundaryException_Return-1", () => {
     expect(problem1([211, 212], [-1, 0])).toEqual(-1);
   });
 
-  test("case12", () => {
+  test("Problem1_Param1LeftPageNumberNotEvenException_Return-1", () => {
     expect(problem1([2, 3], [211, 212])).toEqual(-1);
   });
 
-  test("case13", () => {
+  test("Problem1_Param2LeftPageNumberNotEvenException_Return-1", () => {
     expect(problem1([211, 212], [2, 3])).toEqual(-1);
   });
 
-  test("case14", () => {
+  test("Problem1_Param2PageDiffNotOneException_Return-1", () => {
     expect(problem1([211, 212], [99, 102])).toEqual(-1);
   });
 
-  test("case15", () => {
+  test("Problem1_Param1PageNumberSequenceException_Return-1", () => {
     expect(problem1([2, 1], [211, 212])).toEqual(-1);
   });
 
-  test("case16", () => {
+  test("Problem1_Param2PageNumberSequenceException_Return-1", () => {
     expect(problem1([211, 212], [2, 1])).toEqual(-1);
   });
 
-  test("case17", () => {
+  test("Problem1_Param2NotEnoughArraySizeException_Return-1", () => {
     expect(problem1([211, 212], [211])).toEqual(-1);
   });
 
-  test("case18", () => {
+  test("Problem1_Param1NotEnoughArraySizeException_Return-1", () => {
     expect(problem1([211], [211, 212])).toEqual(-1);
   });
 
-  test("case19", () => {
+  test("Problem1_NotEnoughNumberOfParamsException_Return-1", () => {
     expect(problem1([211, 212])).toEqual(-1);
   });
 
-  test("case20", () => {
+  test("Problem1_Param1PageDiffNotOneException_Return-1", () => {
     expect(problem1([1, 1], [211, 212])).toEqual(-1);
   });
 
-  test("case21", () => {
+  test("Problem1_Param2PageDiffNotOneException_Return-1", () => {
     expect(problem1([211, 212], [1, 1])).toEqual(-1);
   });
 
-  test("case22", () => {
+  test("Problem1_Param1MulBiggerThanParam2Mul_Return1", () => {
     expect(problem1([197, 198], [195, 196])).toEqual(1);
   });
 
-  test("case24", () => {
+  test("Problem1_Param1MulBiggerThanParam2Sum_Return1", () => {
     expect(problem1([197, 198], [201, 202])).toEqual(1);
   });
 
-  test("case25", () => {
+  test("Problem1_Param1MulSmallerThanParam2Mul_Return2", () => {
     expect(problem1([197, 198], [199, 200])).toEqual(2);
   });
 
-  test("case26", () => {
+  test("Problem1_Param1SumSmallerThanParam2Sum_Return2", () => {
     expect(problem1([111, 112], [211, 212])).toEqual(2);
   });
 
-  test("case27", () => {
+  test("Problem1_Param1SumSmallerThanParam2Mul_Return2", () => {
     expect(problem1([201, 202], [197, 198])).toEqual(2);
   });
 
-  test("case28", () => {
+  test("Problem1_SameParamValue_Return0", () => {
     expect(problem1([197, 198], [197, 198])).toEqual(0);
   });
 
-  test("case29", () => {
+  test("Problem1_SameMultiplicationResult_Return0", () => {
     expect(problem1([143, 144], [217, 218])).toEqual(0);
   });
 });
