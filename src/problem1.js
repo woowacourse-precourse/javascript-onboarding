af476b4dee89779e6b3d2e683efc0b9d4769a84a
sehwan <tpghks9245@gmail.com>
2022-10-26 23:26:38 +0900

    problem1 digitSum구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,21 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+}
+
+function digitSum(...arr) {
+  const firstArr = arr[0][0].toString().split("").map(x => parseInt(x));
+  const firstNum = firstArr.reduce((a,b) => (a+b));
+
+  const secondArr = arr[0][1].toString().split("").map(x => parseInt(x));
+  const secondNum = secondArr.reduce((a,b) => (a+b));
+
+  return Math.max(firstNum, secondNum);
 }
 
 module.exports = problem1;
+
+// 기능들
+// 1. 예외상황(첫장 끝장, 페이지수가 이상할때)
+// 2. 각 자리수 나눠서 더하기
+// 3. 각 자리수 나눠서 곱하기
+// 4. 비교해서 결과값 도출
