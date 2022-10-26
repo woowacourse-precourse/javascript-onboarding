c49fa39d55eba277a53c3634106b7c7c3c3782a7
adultlee <seoungin1228@gmail.com>
2022-10-27 01:27:31 +0900

    feat: [pro3] getClapCount 함수 구현
    각 시행별 박수를 count하는 함수를 구현합니다

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
   var answer;
-  return answer;
+function getClapCount(number) {
+  let count = 0;
+  number
+    .toString()
+    .split("")
+    .map((num) => {
+      if (["3", "6", "9"].includes(num)) count += 1;
+    });
+
+  return count;
 }
 
 module.exports = problem3;
