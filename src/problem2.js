3331f51042c17f23ca0f46e10adb3b946061a753
Suyeon <msjgr2801@gmail.com>
2022-10-26 17:09:40 +0900

    feat: Write Problem2's function lists

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
+// 0. 예외처리
+//  0.1 [ ] 문자열이 비었거나 하나의 문자만 있는 경우
+
+// 1. 문자열 제거 로직
+//  1.1 [ ] stack 사용
+//  1.2 [ ] 문자열을 앞에서부터 하나씩 answer에 더한다
+//    1.2.1. answer의 마지막 문자열과 cryptogram의 현재 문자가 같으면 answer에서 pop
+
+// 2. 1번 로직 반복 기준
+//  2.1 [ ] 연속으로 같은 문자열이 존재하지 않아야함 
+//  2.2 [ ] flag로 연속으로 같은 문자열이 있는지 판별 
+//  2.3 [ ] 연속되는 문자열이 있으면 true, 아니면 false
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = [];
+
+  return answer.join("");
 }
 
 module.exports = problem2;
