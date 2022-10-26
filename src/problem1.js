06ea537b5c7f84bc9f369b4ca416f34ed7c3df5a
김희진 <kimheejin@gimhuijins-Air.lan>
2022-10-26 20:19:44 +0900

    입력받은 배열을 개별의 숫자로 전환

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,6 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let pobi_1 = pobi[0];
+  let pobi_2 = pobi[1];
+  let crong_1 = crong[0];
+  let crong_2 = crong[1];
 }
-
-module.exports = problem1;
