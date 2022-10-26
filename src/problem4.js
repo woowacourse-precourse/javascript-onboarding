1166c5278e147226d8af43c09a40c8ab06de42f9
Rki0 <pky00823@gmail.com>
2022-10-26 18:40:43 +0900

    feat:전 기능 구현 완료
    최소한의 기능을 하도록 분리할 것. 분리마다 커밋.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,33 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let wordArr = word.split("");
+
+  let ans = [];
+
+  for (let i = 0; i < wordArr.length; i++) {
+    if (wordArr[i] === " ") {
+      ans.push(wordArr[i]);
+    }
+
+    const oneWordASCII = wordArr[i].charCodeAt();
+
+    if (oneWordASCII >= 65 && oneWordASCII <= 90) {
+      // 대문자는 대문자로 변경
+      const newWordASCII = 90 - (oneWordASCII - 65);
+
+      const newWord = String.fromCharCode(newWordASCII);
+
+      ans.push(newWord);
+    } else if (oneWordASCII >= 97 && oneWordASCII <= 122) {
+      // 소문자는 소문자로 변경
+      const newWordASCII = 122 - (oneWordASCII - 97);
+
+      const newWord = String.fromCharCode(newWordASCII);
+
+      ans.push(newWord);
+    }
+  }
+
+  return ans.join("");
 }
 
 module.exports = problem4;
