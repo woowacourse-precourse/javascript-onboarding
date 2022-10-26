45ad4967a377d6f45db93cb3eebba166c25767bf
sery <yunsel2@gmail.com>
2022-10-26 21:54:14 +0900

    feat: Split page number

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const [pobiLeftPage, pobiRightPage] = pobi;
+  const [crongLeftPage, crongRightPage] = crong;
+
+  const pobiLeftPageArray = splitNumber(pobiLeftPage);
+  const pobiRightPageArray = splitNumber(pobiRightPage);
+  const crongLeftPageArray = splitNumber(crongLeftPage);
+  const crongRightPageArray = splitNumber(crongRightPage);
+
   return answer;
+}
+
+function splitNumber(page) {
+  return page.toString().split('');
 }
 
 module.exports = problem1;
