3e9871230076c628b9551b023a62511dd97e4f33
100Gyeon <bjy9966@naver.com>
2022-10-26 18:35:53 +0900

    feat: unicode를 활용해 알파벳 반대로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const codeNumberList = [];
+  const answerList = [];
+
+  for (let i = 0; i < word.length; i++) {
+    codeNumberList.push(word.charCodeAt(i));
+  }
+
+  codeNumberList.forEach((number) => {
+    if (number === ' '.charCodeAt(0)) {
+      answerList.push(' ');
+    } else {
+      const opposite = number > 90 ? 219 - number : 155 - number;
+      answerList.push(String.fromCharCode(opposite));
+    }
+  });
+
+  return answerList.join('');
 }
 
 module.exports = problem4;
