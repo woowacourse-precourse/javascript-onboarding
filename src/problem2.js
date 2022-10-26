66b29dd6687a6ec365ed845bb5d4dcb248685489
ctd <qpflapffhs76@gmail.com>
2022-10-26 21:21:42 +0900

    feat: 중복 제거 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
   var answer;
+  answer = removeDuplicate(cryptogram);
   return answer;
+}
+
+function removeDuplicate(word) {
+  const stack = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] !== stack[stack.length - 1]) {
+      stack.push(word[i]);
+    } else {
+      stack.pop();
+    }
+  }
+  return stack.join('');
 }
 
 module.exports = problem2;
