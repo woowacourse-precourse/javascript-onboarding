a13c023c5d43a260538e3dfe96c6c50c58ddb96f
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-26 21:07:57 +0900

    feat: [문제3] 숫자 범위 예외사항 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,10 @@
 function problem3(number) {
+  if (typeof number !== 'number' || number < 1 || number > 1000) {
+    throw new Error(
+      'number는 숫자형만 가능하며, 1이상 10,000이하인 자연수만 가능합니다.'
+    );
+  }
+
   const container369 = ['3', '6', '9'];
   const count369 = (number, count) => {
     if (number === 0) {
