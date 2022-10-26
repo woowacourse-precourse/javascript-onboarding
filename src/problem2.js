eb81faa6c18f2677264b2f8e0fe8be4b51adb38e
Jongco <narira0922@gmail.com>
2022-10-26 20:41:20 +0900

    problem2: 중복 문자 암호 해독 기능 구현 목록 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
+  /**
+   * 1. 문자를 배열로 분해
+   * 2. 바로 다음에 오는 문자와 현재 문자를 비교하여 같으면 제거하고 함수 재실행(재귀함수)
+   * 3. 겹치는 문자가 없다면 함수 종료
+   */
+  let answer = cryptogram.split("");
   return answer;
 }
+
+console.log(problem2("browoanoommnaon"));
 
 module.exports = problem2;
