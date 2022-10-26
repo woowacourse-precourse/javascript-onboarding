657d46cf4aef028d861c1e051144f5bda002ebfc
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 21:33:20 +0900

    feat: 문제4 기능 구현
    - 문자열 메소드 charCodeAt과 fromCharCode를 이용해서 구현
    - 문자열 각 자리의 아스키 코드 값을 대문자일 때와 소문자일 때와 알파벳 외의 관련된 값일 경우로 나눔

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
-  var answer;
+  // A-Z 65-90
+  // a-z 97-122
+  let answer = "";
+  for (let i = 0; i < word.length; i++) {
+    let val = word.charCodeAt(i);
+    if (val >= 65 && val <= 90) {
+      answer += String.fromCharCode(155 - val);
+    } else if (val >= 97 && val <= 122) {
+      answer += String.fromCharCode(219 - val);
+    } else {
+      answer += word[i];
+    }
+  }
   return answer;
 }
 
