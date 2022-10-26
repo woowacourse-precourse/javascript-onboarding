d72885302d8ea8bd1a861022667e851749aea17f
kangju2000 <kangju2000@naver.com>
2022-10-26 23:54:19 +0900

    feat: count369 함수 선언

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,11 @@ function problem3(number) {
   return answer;
 }
 
+function count369(num) {
+  return (num + '').split('').reduce((acc, cur) => {
+    if (cur === '3' || cur === '6' || cur === '9') acc++;
+    return acc;
+  }, 0);
+}
+
 module.exports = problem3;
