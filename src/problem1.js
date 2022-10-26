6ebee1cf89df0f4ab76ee6ffcb5a0db8606324c9
foundaway98 <foundaway98@naver.com>
2022-10-27 03:00:29 +0900

    최대값 선택 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -16,6 +16,18 @@ function problem1(pobi, crong) {
     } while (n > 0);
     return mul;
   }
+
+  function choosePoint(arr) {
+    for (let i = 0; i < 2; i++) {
+      arr[i] = comparePoint(makeSum(arr[i]), makeMul(arr[i]));
+    }
+    return arr[0] > arr[1] ? arr[0] : arr[1];
+  }
+
+  function comparePoint(n1, n2) {
+    return n1 > n2 ? n1 : n2;
+  }
+
   var answer;
   return answer;
 }
