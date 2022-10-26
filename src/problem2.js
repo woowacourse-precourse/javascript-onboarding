5a58fc05a6de00212429f6c13903484bc6be1265
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-26 21:37:55 +0900

    feat: 정규식 이용하여 중복 문자 없을 때까지 중복 문자 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,11 @@
 const regex = /([a-z])\1+/g;
 
-const problem2 = (cryptogram) => {}
+const problem2 = (cryptogram) => {
+  let copiedCryptogram = cryptogram;
+  while (regex.test(copiedCryptogram)) {
+    copiedCryptogram = copiedCryptogram.replaceAll(regex, '');
+  }
+  return copiedCryptogram;
+}
 
 module.exports = problem2;
