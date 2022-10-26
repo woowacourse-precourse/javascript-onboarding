1def6662017b84fc1349545e0ac5f3bad41ae160
oyhoyhk <oyhoyhk@naver.com>
2022-10-26 17:16:43 +0900

    problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,47 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+	if (!pobi || !crong || !validationCheck(pobi) || !validationCheck(crong)) return -1;
+
+	pobi = getResults(pobi);
+	crong = getResults(crong);
+	return pobi > crong ? 1 : pobi < crong ? 2 : 0;
 }
+
+function validationCheck(info) {
+	if (info.length !== 2) return false;
+	if (info[1] - info[0] !== 1) return false;
+	if (info[0] <= 1 || info[1] >= 400) return false;
+
+	return true;
+}
+
+function getResults([left, right]) {
+	let arr = [];
+	arr.push(addDigit(left));
+	arr.push(mulDigit(left));
+	arr.push(addDigit(right));
+	arr.push(mulDigit(right));
+
+	return Math.max(...arr);
+}
+
+function addDigit(num) {
+	let result = 0;
+	while (num !== 0) {
+		result += num % 10;
+		num = Math.floor(num / 10);
+	}
+	return result;
+}
+
+function mulDigit(num) {
+	let result = 1;
+	while (num !== 0) {
+		result *= num % 10;
+		num = Math.floor(num / 10);
+	}
+	return result;
+}
+
+console.log(solution([99, 102], [211, 212]));
 
 module.exports = problem1;
