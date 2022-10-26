8555a66684d875d220ba70d56832b88e667707f1
cham-min <hymn9613@gmail.com>
2022-10-27 01:04:30 +0900

    feat: sumPage 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,11 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function sumPage(num) {
+  const numArr = num.toString().split("");
+  const result = numArr.reduce((prev, cur) => prev + Number(cur), 0);
+
+  return result;
+}
+
 module.exports = problem1;
