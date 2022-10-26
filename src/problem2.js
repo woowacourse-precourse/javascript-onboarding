ccfed8904fa51c33faa072df25b8c50cf0f63aba
parkjikuk <paxkk@naver.com>
2022-10-27 05:38:48 +0900

    feat : 문자 확인 및 중첩 제외 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = "";
+  const arr = [...cryptogram];
+
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] !== arr[i + 1]) {
+      answer += arr[i];
+    } else {
+      i ++;
+    }
+  }
+
   return answer;
 }
 
