80b14103cc9714d79a5fd818a36e1c2896bb0d08
turtle601 <turtle601@naver.com>
2022-10-26 20:51:36 +0900

    Feat: [problem2] 제한사항 처리 로직인 ErrorCase 클래스 구현
    <ErrorCase클래스>
    - checkLowerCase() : 입력값이 소문자인지 확인
    - checkLength(): 입력값의 길이가 1이상 1000이하 인지 확인
    - checkAllError() : 제한 사항 처리 함수를 모두 확인

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,34 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+class ErrorCase {
+  constructor(cryptogram) {
+    this.cryptogram = cryptogram;
+    this.checkAllError();
+  }
+
+  checkLowerCase() {
+    return this.cryptogram.match(/[a-z]/g).length === this.cryptogram.length;
+  }
+
+  checkLength() {
+    return 1 <= this.cryptogram.length && this.cryptogram.length <= 1000;
+  }
+
+  checkAllError() {
+    if (!(this.checkLength() && this.checkLowerCase())) {
+      throw new Error("input 양식에 에러가 발생했습니다!!");
+    }
+  }
 }
+
+function problem2(cryptogram) {
+  new ErrorCase(cryptogram);
+  return;
+}
+
+/*
+  console.log(problem2("Aasdfasdf")); // 알파벳 대문자가 있을 때
+  console.log(problem2("1dsakjfas")); // 문자열에 숫자가 있을 때
+  console.log(problem2("dsak akds")); // 문자열에 띄어쓰기가 있을 때
+  console.log(problem2("#$!aaa")); // 문자열에 소문자 외 문자가 있을 때
+*/
 
 module.exports = problem2;
