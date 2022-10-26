e980fed80802274d6fec176d127b3f0ae1c61cff
hyunjin <leehj0110@kakao.com>
2022-10-26 23:43:41 +0900

    feat: 3번

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let answer = 0
+
+  for (let i = 1; i <= number; i++) {
+    let count = 0
+    const num = i.toString()
+    for (let j = 0; j < num.length; j++) {
+      if (num[j] === "3" || num[j] === "6" || num[j] === "9") count++
+    }
+    answer += count
+  }
+
+  return answer
 }
 
-module.exports = problem3;
+problem3(13)
+
+module.exports = problem3
