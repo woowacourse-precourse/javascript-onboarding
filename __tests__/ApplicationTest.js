eaa683f84aafe5e0409c81d7eb5d4a838d518709
dkile <dkile532@naver.com>
2022-10-26 19:53:07 +0900

    test: add problem1 test case4(first page boundary exception)

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -18,6 +18,10 @@ describe("problem1", () => {
   test("case3", () => {
     expect(problem1([99, 102], [211, 212])).toEqual(-1);
   });
+
+  test("case4", () => {
+    expect(problem1([1, 2], [211, 212])).toEqual(-1);
+  });
 });
 
 describe("problem2", () => {
