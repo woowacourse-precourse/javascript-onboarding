295414d563f5309700a9c971fe9a6cb53610789f
han0224 <dbn0224@gmail.com>
2022-10-26 20:03:16 +0900

    [문제3] 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    for (let j = i; j > 0; j = Math.floor(j / 10)) {
+      const num = j % 10;
+      if (num === 3 || num === 6 || num === 9) answer += 1;
+    }
+  }
   return answer;
 }
 
