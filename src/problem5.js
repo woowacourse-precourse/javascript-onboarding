bc9b8386ed20aca538973c7883e3b27cdec5f267
Jongco <narira0922@gmail.com>
2022-10-26 21:51:39 +0900

    problem5: 화폐 단위 계산 함수 구현
    거스름돈을 만들고 미리 만들어둔 화폐 단위 배열로 각각의 거스름 돈을 나눈 후
    각 단위로 나눈 나머지를 거스름 돈으로 재할당하는 과정을 반복

--- src/problem5.js
+++ src/problem5.js
@@ -4,7 +4,13 @@ function problem5(money) {
    * 2. answer = money로 설정한 후 위에 생성한 monetaryUnit로 map 함수를 돌려 몫은 맵핑하고 answer = 나머지로 설정
    * 3. 2번 과정 끝까지 반복
    */
-  var answer;
+  const monetaryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let change = money;
+  let answer = monetaryUnit.map((unit) => {
+    const count = Math.floor(change / unit);
+    change = Math.floor(change % unit);
+    return count;
+  });
   return answer;
 }
 
