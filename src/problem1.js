235be6f09bb276f5a56b820438447ae8a9078468
seolyeongbae <peixueying@gmail.com>
2022-10-26 20:26:44 +0900

    feat: solve problem 1

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,49 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  const biggestNum = (score) => {
+    const scoreString = score.toString().split("");
+
+    let sumScore = 0;
+    let multiplyScore = 1;
+
+    scoreString.forEach((element) => {
+      sumScore += parseInt(element);
+      multiplyScore *= parseInt(element);
+    });
+
+    const maxValue = Math.max(sumScore, multiplyScore);
+
+    return maxValue;
+  };
+
+  const calcMaxValue = (bookPage) => {
+    if (bookPage[1] - bookPage[0] !== 1) {
+      return -1;
+    }
+    let maximum = -1;
+
+    bookPage.forEach((page) => {
+      const result = biggestNum(page);
+      if (result > maximum) {
+        maximum = result;
+      }
+    });
+
+    return maximum;
+  };
+
+  const pobiScore = calcMaxValue(pobi);
+  const crongScore = calcMaxValue(crong);
+
+  if (pobiScore === -1 || crongScore === -1) {
+    answer = -1;
+  } else if (pobiScore > crongScore) {
+    answer = 1;
+  } else if (pobiScore < crongScore) {
+    answer = 2;
+  } else [(answer = 0)];
+
   return answer;
 }
 
