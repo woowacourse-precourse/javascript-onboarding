712df33307e2c350a3b57a0aab7d7326a4f0e2dd
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 01:57:15 +0900

    problem1/feat : pobi와 crong 각각의 페이지 번호를 더하거나 곱한 값 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,26 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let pobiMax = 0;
+  let crongMax = 0;
+  let pobiArr = [];
+  let crongArr = [];
+
+  for (let i = 0; i < 2; i++) {
+    let pobiPlus = 0;
+    let pobiTimes = 1;
+    let crongPlus = 0;
+    let crongTimes = 1;
+
+    for (let z = 0; z < String(pobi[i]).length; z++) {
+      pobiPlus += parseInt(String(pobi[i])[z]);
+      pobiTimes *= parseInt(String(pobi[i])[z]);
+    }
+    for (let x = 0; x < String(crong[i]).length; x++) {
+      crongPlus += parseInt(String(crong[i])[x]);
+      crongTimes *= parseInt(String(crong[i])[x]);
+    }
+    pobiArr.push(pobiPlus, pobiTimes);
+    crongArr.push(crongPlus, crongTimes);
+  }
 }
 
 module.exports = problem1;
