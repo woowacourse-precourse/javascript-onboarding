a01726012527c02e48e14201883edb5087ea49d3
olo90632951 <olo90632951@gmail.com>
2022-10-27 00:13:47 +0900

    feat: 1부터 입력 받은 수까지 3,6,9 확인하여 카운팅하는 기능 [Problem3-1]

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  return playGame(number);
 }
+
+const playGame = (number) => {
+  let answer = 0;
+  for (let currentValue = 1; currentValue <= number; currentValue++) {
+    answer += search369(currentValue);
+  }
+  return answer;
+};
+
+const search369 = (currentValue) => {
+  let clap = 0;
+  String(currentValue)
+    .split('')
+    .forEach((number) => {
+      if (number === '3' || number === '6' || number === '9') {
+        clap += 1;
+      }
+    });
+  return clap;
+};
 
 module.exports = problem3;
