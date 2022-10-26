5d5463fc739618452d069d183906a4c984d74c28
CoboCho <rlaalsrb1111@naver.com>
2022-10-26 23:18:36 +0900

    feat: [problem5] 지폐 배열 및 withdraw() 인출 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let answer = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 }
 
+function withdraw(unit, money) {
+  let count = 0;
+  while(money >= unit) {
+    money -= unit;
+    count++;
+  }
+  return {change: money, count};
+}
 module.exports = problem5;
