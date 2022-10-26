c3a82fa2f729d986ade7bc554001324055fcb613
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-27 01:11:44 +0900

    feat(문제1): 각 자릿수 합을 계산하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,13 @@
+const calcSum = (num) => {
+  let sum = 0;
+  while (num > 0){
+    sum += Math.trunc(num % 10);
+    num = Math.trunc(num / 10);
+  }
+  return sum;
+}
+
 function problem1(pobi, crong) {
-  var answer;
   return answer;
 }
 
