7b9518f2bd4e69a9a77a49301aa05671d93fab7a
h2002y <h2002y@naver.com>
2022-10-26 19:54:31 +0900

    feat: Add main func

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  let upper = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+  let lower = upper.toLowerCase();
+  for(let i = 0; i < word.length; i++){
+    if((word[i] >= 'a') && (word[i] <= 'z')){
+      answer += lower[word[i].charCodeAt(0) - 'a'.charCodeAt(0)];
+    }
+    else if((word[i] >= 'A') && (word[i] <= 'Z')){
+      answer += upper[word[i].charCodeAt(0) - 'A'.charCodeAt(0)];
+    }
+    else{
+      answer += word[i];
+    }
+  }
   return answer;
 }
 
