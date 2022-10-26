082636f518e5ee551a016826d30cf6a17ba36462
saejinpark <saejinpark951210@gmail.com>
2022-10-26 15:45:55 +0900

    문제1 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,32 @@
+function inputCheck(pages) {
+  let [left, right] = pages;
+  if (1 <= left && left < 400 && left + 1 == right) {
+    return true;
+  }
+  return false;
+}
+
+function scoreGenerator(pages) {
+  const [left, right] = pages;
+  const leftNumArr = ("" + left).split("").map((v) => parseInt(v));
+  const rightNumArr = ("" + right).split("").map((v) => parseInt(v));
+  return Math.max(
+    leftNumArr.reduce((acc, cur) => (acc += cur), 0),
+    leftNumArr.reduce((acc, cur) => (acc *= cur), 0),
+    rightNumArr.reduce((acc, cur) => (acc += cur), 0),
+    rightNumArr.reduce((acc, cur) => (acc *= cur), 0)
+  );
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  if (!inputCheck(pobi) || !inputCheck(crong)) {
+    return -1;
+  }
+  let answer = 0;
+  const pobiScore = scoreGenerator(pobi);
+  const crongScore = scoreGenerator(crong);
+  if (pobiScore > crongScore) answer = 1;
+  if (pobiScore < crongScore) answer = 2;
   return answer;
 }
 
