685305aca948141fa5db28280326af12ddf41ebd
parkjikuk <paxkk@naver.com>
2022-10-26 22:05:06 +0900

    feat: 가장 높은 점수를 구하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const pobiScore = Math.max(parseInt(pobi.map(num => getScore(num))));
+  const crongScore = Math.max(parseInt(crong.map(num => getScore(num))));
+
+  function getScore(num) {
+    const add = String(num).split("").reduce((acc, cur) => acc + parseInt(cur), 0);
+    const multiply = String(num).split("").reduce((acc, cur) => acc * parseInt(cur));
+    const highScore = Math.max(add, multiply);
+
+    return highScore;
+  }
 }
 
 module.exports = problem1;
+
