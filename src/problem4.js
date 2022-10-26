a6675fb2b9dfa17e0a59481258af38279532e74d
nlom0218 <nlom0218@naver.com>
2022-10-26 20:26:55 +0900

    feat: 문제 4 - 대문자, 소문자 확인함수 만들기

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,10 @@ const isAlphabet = (char) => {
   return char.match(/[a-zA-z]/) ? true : false;
 };
 
+const isUpperCase = (char) => {
+  return char.match(/[A-Z]/) ? true : false;
+};
+
 function problem4(word) {
   var answer;
   return answer;
