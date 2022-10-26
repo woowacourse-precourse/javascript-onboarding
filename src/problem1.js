60149c304d26ab90242a342f27dbdb8133826892
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-26 15:33:03 +0900

    problem1 Add function 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,17 @@
 function problem1(pobi, crong) {
+  var pobiadd = Add(pobi[1]);
+  var crongadd = Add(crong[1]);
   var answer;
   return answer;
+}
+function Add(n){
+  var sum = 0;
+  do{
+    sum += n%10;
+    n = Math.floor(n/10);
+  }while(n > 0);
+
+  return sum;
 }
 
 module.exports = problem1;
