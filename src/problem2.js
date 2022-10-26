408d43c9bec7c38585ec5fcb40831d6eeb7832b5
guridaek <iue16@naver.com>
2022-10-26 21:22:06 +0900

    feat: 문제2 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,28 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let stack = [];
+  let flag = false;
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (stack[stack.length - 1] === cryptogram[i]) {
+      flag = true;
+      continue;
+    }
+
+    if (flag) {
+      stack.splice(-1, 1);
+      flag = false;
+      i--;
+      continue;
+    }
+
+    stack.push(cryptogram[i]);
+  }
+
+  if (flag) {
+    stack.splice(-1, 1);
+  }
+
+  return stack.join('');
 }
 
 module.exports = problem2;
