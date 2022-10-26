225f90a4fb8c09a027a0ad455b088d04478bd242
yeongi <cbg5255@gmail.com>
2022-10-26 18:17:46 +0900

    문제 2번 구현, 테스트 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,30 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  //중복 여부 확인
+  let isHave = true;
+  //문자 배열 생성
+  let str = [...cryptogram];
+
+  while (isHave) {
+    isHave = false;
+    str.forEach((c, i) => {
+      if (c === str[i + 1]) {
+        // 중복 문자열 제거
+        cryptogram = strEdit(cryptogram, c);
+        //이번 암호에 중복 문자열이 있다는 것
+        isHave = true;
+      }
+    });
+    //문자 배열 초기화
+    str = [...cryptogram];
+  }
+
+  return cryptogram;
 }
+
+// 중복 문자열 제거
+const strEdit = (str, char) => {
+  const arr = str.split(`${char}${char}`);
+  return arr[0].concat(arr[1]);
+};
 
 module.exports = problem2;
