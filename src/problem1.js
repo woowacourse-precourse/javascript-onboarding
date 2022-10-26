60bd2eab306c47a4d40870c38964f7110ee3a1d0
turtle601 <turtle601@naver.com>
2022-10-26 20:04:21 +0900

    Feat: [problem1] input 에러 케이스 로직인 PageError 클래스 구현
    <PageError클래스>
    - checkLimit() : 페이지가 1이상 400미만인지 확인
    - checkNext() : 왼쪽페이지 = 오른쪽페이지 + 1와 왼쪽 페이지가 홀수인지 확인
    - checkLength() : 책(입력값의 원소값)의 길이가 2인지 확인
    - checkAllError() : 모든 제한 사항과 에러사항을 확인

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,47 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+class PageError {
+  constructor(pobi, crong) {
+    this.pobi = pobi;
+    this.crong = crong;
+  }
+
+  checkLimit() {
+    return [...this.pobi, ...this.crong].every(
+      (page) => 1 < page && page < 400
+    );
+  }
+
+  checkNext() {
+    return [this.pobi, this.crong].every(
+      ([leftPage, rightPage]) =>
+        rightPage - leftPage === 1 && leftPage % 2 === 1
+    );
+  }
+
+  checkLength() {
+    return [this.pobi, this.crong].every((book) => book.length === 2);
+  }
+
+  checkAllError() {
+    if (!(this.checkLimit() && this.checkNext() && this.checkNext())) {
+      return false;
+    }
+
+    return true;
+  }
 }
+
+function problem1(pobi, crong) {
+  const pageError = new PageError(pobi, crong);
+  if (!pageError.checkAllError()) return -1;
+
+  return 0;
+}
+
+// 에러 테스트
+console.log(problem1([1, 2], [3, 4])); // 시작이 1일 때 에러
+console.log(problem1([3, 4], [399, 400])); // 끝이 400일 때
+console.log(problem1([-1, 0], [3, 4])); // 0미만의 값이 있을 때
+console.log(problem1([22, 23], [33, 34])); // 왼쪽 페이지가 짝수 일 때
+console.log(problem1([23, 28], [33, 34])); // 왼쪽 오른쪽 페이지가 2이상 차이날 때
 
 module.exports = problem1;
