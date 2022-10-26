8ae984e0ea78140bc2b4716a5f81cfa806b43a15
adultlee <seoungin1228@gmail.com>
2022-10-27 02:40:40 +0900

    feat: [pro4] 글자를 바꾸어주는 함수를 구현
    알파벳을 바꾸는 방식은 일반적인 알파벳 형식을 뒤집은 방식으로 25 -n 을 취해주면 바로 대칭되는 알파벳을 찾을 수 있습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,16 @@ const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
 function problem4(word) {
   var answer;
   return answer;
+function changeLetter(letter) {
+  if (isUpperCase(letter)) {
+    const index = UPPERCASE.split("").findIndex((upper) => upper === letter);
+    return UPPERCASE[25 - index];
+  } else {
+    const index = LOWERCASE.split("").findIndex((lower) => lower === letter);
+    return LOWERCASE[25 - index];
+  }
+}
+
 function isUpperCase(letter) {
   if (UPPERCASE.includes(letter)) return true;
   return false;
