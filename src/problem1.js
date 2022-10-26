950d05fb772f321502e303263aa67dd04e5acc88
kyhyun <k29176@gmail.com>
2022-10-26 20:49:30 +0900

    feat : 문제 1번 - 각 자리 숫자를 모두 곱하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,9 @@
 /* 
-구현에 예상되는 기능 목록
+구현 기능 목록
   [v] 각 자리 숫자를 모두 더하는 기능
-  [ ]각 자리 숫자를 모두 곱하는 기능
-  [ ]가장 큰 수를 구하는 기능
-  [ ]점수를 비교 및 판단하는 기능
+  [v] 각 자리 숫자를 모두 곱하는 기능
+  [ ] 가장 큰 수를 구하는 기능
+  [ ] 점수를 비교 및 판단하는 기능
 */
 
 function problem1(pobi, crong) {
@@ -15,6 +15,12 @@ function problem1(pobi, crong) {
   console.log(sumByEachDigit(pobi_right)); // 17
   console.log(sumByEachDigit(crong_left)); // 17
   console.log(sumByEachDigit(crong_right)); // 18
+
+  console.log(multiplyNumbers(pobi_left)); // 63
+  console.log(multiplyNumbers(pobi_right)); // 72
+  console.log(multiplyNumbers(crong_left)); // 63
+  console.log(multiplyNumbers(crong_right)); // 72
+
   return answer;
 }
 
@@ -25,6 +31,13 @@ function sumByEachDigit(pages) {
     pages = parseInt(pages / 10);
   }
   return sumResult;
+}
+
+function multiplyNumbers(pages) {
+  const multiplyResult = String(pages)
+    .split('')
+    .reduce((acc, cur) => acc * cur);
+  return multiplyResult;
 }
 
 problem1([97, 98], [197, 198]); // 0
