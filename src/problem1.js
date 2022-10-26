f36a82c916b0ec543983ba1ad1b0715693f8dd4d
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-27 00:11:51 +0900

    feat: checkNumber

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,49 @@
-function problem1(pobi, crong) {}
+function problem1(pobi, crong) {
+  const pobiOddNumber = pobi[0];
+  const pobiEvenNumber = pobi[1];
+  const crongOddNumber = crong[0];
+  const crongEvenNumber = crong[1];
+
+  const pobiScore = maxNumber(pobiOddNumber, pobiEvenNumber);
+  const crongScore = maxNumber(crongOddNumber, crongEvenNumber);
+
+  if (checkNumber(pobi) || checkNumber(crong)) {
+    return -1;
+  }
+
+  if (pobiScore > crongScore) {
+    return 1;
+  } else if (pobiScore < crongScore) {
+    return 2;
+  } else if (pobiScore == crongScore) {
+    return 0;
+  }
+}
+
+function checkNumber(numbers) {
+  if (numbers.length !== 2) {
+    return true;
+  }
+  if (numbers[0] % 2 !== 1) {
+    return true;
+  }
+  if (numbers[1] % 2 !== 0) {
+    return true;
+  }
+  if (numbers[0] + 1 !== numbers[1]) {
+    return true;
+  }
+  return false;
+}
+
+function maxNumber(oddNumber, evenNumber) {
+  return Math.max(
+    sumNumber(oddNumber),
+    mutiNumber(oddNumber),
+    sumNumber(evenNumber),
+    mutiNumber(evenNumber)
+  );
+}
 
 function sumNumber(number) {
   return number
@@ -13,5 +58,6 @@ function mutiNumber(number) {
     .split("")
     .reduce((muti, current) => parseInt(muti) * parseInt(current), 1);
 }
+problem1([1, 312], [1, 2312]);
 
 module.exports = problem1;
