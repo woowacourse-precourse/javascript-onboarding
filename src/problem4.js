ef1d71c0da2e10fde6a11d6337943206b31b2efd
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-27 01:22:04 +0900

    dosc<problem4>: Define fetures list

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
+/* 
+ * Features
+ * 1. 문자열을 입력받는다
+ * 2. 문자 단위로 값을 뒤집는다. (소문자 포함)
+ * 3. 뒤집은 값 반환
+ *  
+ * Exceptions
+ * 1. word 길이: 1~1000
+ * 2. 알파벳 외 문자는 x
+ */
 function problem4(word) {
   var answer;
   return answer;
 }
+
+console.log("A".charCodeAt())
+console.log("Z".charCodeAt())
+console.log("M".charCodeAt())
+console.log("N".charCodeAt())
+
 
 module.exports = problem4;
