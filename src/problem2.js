657eb10608945c876b28c2e70118d8d9d17d7ad7
An Sol <apdlvmf486@gmail.com>
2022-10-27 08:47:48 +0900

    중복되는 문자를 제거하는 기능  구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let count = cryptogram.length;
+  let newString = cryptogram
+  for(let num = 'a'.charCodeAt(0); num < 'z'.charCodeAt(0) + 1; num++){
+    const regex = new RegExp(`${String.fromCharCode(num)}{2}`, 'g');
+    newString = newString.replace(regex, '');
+  }
+
+  return newString;
+
 }
 
 module.exports = problem2;
