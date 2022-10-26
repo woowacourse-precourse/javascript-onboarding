2cfb9de37757caf3d0a791302948015208148c7e
h2002y <h2002y@naver.com>
2022-10-26 20:44:49 +0900

    feat: Add setMap func

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,19 @@
 function problem6(forms) {
   var answer;
+  let map = new Map();
+  let rejectIndex = [];
   return answer;
+}
+
+function handelException(){}
+
+function setMap(forms, map){
+  for(let i = 0; i < forms.length; i++){
+    for(let j = 0; j < forms[i][1].length - 1; j++){
+      let tmp = forms[i][1].substr(j,2);
+      map.set(tmp, 1);
+    }
+  }
 }
 
 module.exports = problem6;
