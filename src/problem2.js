515b3525c2bdda4a4d79e778a459403bf392c4d0
0dytpq0 <rkfnahs12e@gmail.com>
2022-10-26 23:22:39 +0900

    problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,13 +1,23 @@
 function problem2(cryptogram) {
-  var answer = "";
-  for (let i = 0; i < cryptogram.length; i++) {
-    if (cryptogram.indexOf(cryptogram[i]) === i) {
-      answer += cryptogram[i];
+  //길이 제한.
+  if (1 <= cryptogram.length <= 1000) {
+    //소문자 제한
+    cryptogram.toLowerCase();
+    //빈 배열
+    let stack = [];
+
+    for (let i of cryptogram) {
+      //현재값과 스택에있는 값이 일치하면
+      //pop으로 배열에서 제거 후 반복으로 이동.
+      if (i === stack[stack.length - 1]) {
+        stack.pop();
+        continue;
+      }
+      //모든 문자열 스택에 푸쉬.
+      stack.push(i);
     }
+    //배열 -> string
+    return stack.join("");;
   }
-  return answer;
 }
-
-console.log(problem2("browoanoommnaon"));
-
-// module.exports = problem2;
+module.exports = problem2;
