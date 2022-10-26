69af17f62fd688cb0d7f053566d1ddbcbd9351a9
CoboCho <rlaalsrb1111@naver.com>
2022-10-26 23:05:02 +0900

    feat: [problem3] 입력값 배열 변경 및 3,6,9 유무 카운트 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
   let answer = 0;
   if(number < 3) return 0;
+  for(let i = 3; i <= number; i++) {
+    let arr = (i + '').split('');
+    arr.forEach(number => {
+      if([3, 6, 9].includes(+number)) answer++;
+    });
+  }
+  return answer;
 }
 
 module.exports = problem3;
