72dd821900296f5f67cd522346bf08057438b373
Ryong-E <think8867@gmail.com>
2022-10-26 23:30:52 +0900

    feat: 중복 문자열 삭제 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,17 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function decodePassword(password){
+  let decoded = password.split('');
+  for (let i = 1; i < decoded.length + 1; i++){
+    if (decoded[i-1] === decoded[i]){
+      decoded.splice(i-1,2);
+      i = 0;
+    }
+  }
+  decoded = decoded.join('');
+
+  return decoded;
+}
+
 module.exports = problem2;
