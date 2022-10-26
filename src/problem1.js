47898e4c344df3c34645f2f2550bdd8b0a7804e5
eunsung <esk114477@gmail.com>
2022-10-26 18:16:37 +0900

    예외처리 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,15 @@
 function problem1(pobi, crong) {
+
+  //예외처리
+  const checkPage = (arr) => {
+    if(arr[0]+1 !== arr[1]) return false;
+    if(arr.length !== 2) return false;
+    if(arr[0] < 1 || arr[0] % 2 === 0) return false;
+    if(arr[1] > 400 || arr[1] % 2 === 1) return false;
+    // arr[0] 이 짝수이거나 arr[1] 이 홀수일때 조건
+    return true;
+  }
+
   var answer;
   return answer;
 }
