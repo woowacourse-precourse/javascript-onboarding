39d8b227b66bf30566b5421f8d0e15378bfc287d
sungjihyun <jhsung23@gmail.com>
2022-10-26 16:22:41 +0900

    Feat: problem 1: 연산 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,24 @@
+const POBIWIN = 1;
+const CRONGWIN = 2;
+const DRAW = 0;
+const ERR = -1;
+
+// 각 자리 숫자를 모두 더해 반환해주는 함수
+const calcSum = (num) => {
+  const result = (num + "").split("").reduce((acc, cur) => {
+    return (acc += parseInt(cur));
+  }, 0);
+  return result;
+};
+
+// 각 자리 숫자를 모두 곱해 반환해주는 함수
+const calcMul = (num) => {
+  const result = (num + "").split("").reduce((acc, cur) => {
+    return (acc *= parseInt(cur));
+  }, 1);
+  return result;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
