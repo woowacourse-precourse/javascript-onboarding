23b17f623783935e2aefc3866b3ccc44336d7ccf
CoboCho <rlaalsrb1111@naver.com>
2022-10-27 00:41:28 +0900

    feat: 중복문자 식별 isDuplicated() 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
 function problem6(forms) {
-  var answer;
+  var answer = new Set();
   return answer;
+}
+function isDuplicated(name1, name2) {
+  name1 = name1.split('');
+  name2 = name2.split('');
+  for(let i = 0; i < name1.length - 1; i++) {
+    for(let j = 0; j < name2.length - 1; j++) {
+      if(name1[i] === name2[j] && name1[i + 1] === name2[j + 1]) return true;
+    }
+  }
+  return false;
 }
 
 module.exports = problem6;
