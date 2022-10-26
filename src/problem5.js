e347d28b99f68bd1c96b354961d873ae05581276
pakxe <pigkill40@naver.com>
2022-10-27 07:00:41 +0900

    feat: [P5] problem5 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
+const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+  return MONETARY_UNIT.map((m) => {
+    let count = Math.trunc(money / m);
+    money = money % m;
+    return count;
+  });
 }
 
 module.exports = problem5;
