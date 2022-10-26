29b434669c7df4f9adef1915f5443f602ee91cf4
Hyun Su <tlsgustn93@naver.com>
2022-10-27 00:50:46 +0900

    Problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
+function solution(num) {
+  cnt = 0
+  for (let i = 0; i < String(num)[i]; i++) {
+      if(String(num)[i] === '3' || String(num)[i] === '6' || String(num)[i] === '9') cnt++
+  }
+  return cnt
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  const arr = Array.from({ length: number }, (v, i) => i + 1);
+  const answer = arr.map((num) => solution(num))
+  
+  return answer.reduce((acc, cur) => acc + cur, 0)
 }
 
 module.exports = problem3;
