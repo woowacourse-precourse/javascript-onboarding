3b0527f8c40b8aa435e395768658ccf986f8d678
Gyeongm1n <affirming_km@naver.com>
2022-10-26 21:56:59 +0900

    stack을 활용한 정답 구하기 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+
+  for (const ch of cryptogram) {
+    if (stack.length && stack[stack.length - 1] === ch) {
+      stack.pop();
+    } else {
+      stack.push(ch);
+    }
+  }
+
+  return stack.join('');
 }
 
 module.exports = problem2;
