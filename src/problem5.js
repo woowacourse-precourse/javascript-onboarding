3c87b322fbcc3f1ee0e4fcfddc98c3259d934573
Leejin-Yang <ejin1996@gmail.com>
2022-10-26 23:23:06 +0900

    refactor(problem5): 순회 횟수 감소

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,17 @@
 const MONEY_UNITS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  const answer = MONEY_UNITS.map(() => 0);
   let currentMoney = money;
-  let index = 0;
+  const answer = MONEY_UNITS.map((unit) => {
+    if (currentMoney === 0) {
+      return 0;
+    }
 
-  while (currentMoney > 0) {
-    const count = parseInt(currentMoney / MONEY_UNITS[index]);
+    const count = parseInt(currentMoney / unit);
+    currentMoney %= unit;
 
-    answer[index] = count;
-    currentMoney %= MONEY_UNITS[index];
-    index += 1;
-  }
+    return count;
+  });
 
   return answer;
 }
