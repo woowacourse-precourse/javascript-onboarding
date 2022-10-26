ebc4ccc735f7306ebd2b445f1e8145f8b4b7e76d
imb96 <kimminje7267@naver.com>
2022-10-26 22:24:06 +0900

    reverse word

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,51 @@
 function problem4(word) {
-  var answer;
-  return answer;
-}
+  const alphabet = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const bigAlphabet = alphabet.map((x) => x.toUpperCase());
+  let reverseAlphabet = [];
+  for (let i = alphabet.length - 1; 0 <= i; i--) {
+    reverseAlphabet.push(alphabet[i]);
+  }
+  const bigReverseAlphabet = reverseAlphabet.map((x) => x.toUpperCase());
 
+  let greenfrog = "";
+
+  const array = word.split("");
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== " " && array[i] === array[i].toUpperCase()) {
+      greenfrog += bigReverseAlphabet[bigAlphabet.indexOf(array[i])];
+    } else if (array[i] !== " " && array[i] === array[i].toLowerCase()) {
+      greenfrog += reverseAlphabet[alphabet.indexOf(array[i])];
+    } else if (array[i] === " ") {
+      greenfrog += " ";
+    } else greenfrog += array[i];
+  }
+  return greenfrog;
+}
 module.exports = problem4;
