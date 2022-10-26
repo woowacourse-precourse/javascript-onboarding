b8b8e32d7129772560eb7c13265e069ec360f69d
haesa <xdhaesa@gmail.com>
2022-10-26 23:00:48 +0900

    problem3 박수 횟수 카운트하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,8 +3,14 @@ function count369(num) {
     .filter((n) => n === '3' || n === '6' || n === '9').length;
 }
 
+function countClap(number) {
+  let count = 0;
+  for (let i = 1; i <= number; i++) count += count369(i);
+  return count;
+}
+
 function problem3(number) {
-  var answer;
+  const answer = countClap(number);
   return answer;
 }
 module.exports = problem3;
