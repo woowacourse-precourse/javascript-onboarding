d3775ea9ee1766db29527c804f27998dad67b2b8
Gyeongm1n <affirming_km@naver.com>
2022-10-26 21:38:58 +0900

    주석 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,4 @@
+// 1. 입력값 예외처리 함수
 function handleExceptions(pobi, crong) {
   const [pobi_left, pobi_right] = pobi;
   const [crong_left, crong_right] = crong;
@@ -18,18 +19,21 @@ function handleExceptions(pobi, crong) {
   return true;
 }
 
+// 2 - 1. 더하기 함수
 function getPlus(str) {
   const result = str.split('').reduce((acc, cur) => acc + parseInt(cur, 10), 0);
 
   return result;
 }
 
+// 2 - 2. 곱하기 함수
 function getMultiple(str) {
   const result = str.split('').reduce((acc, cur) => acc * parseInt(cur, 10), 1);
 
   return result;
 }
 
+// 2. 더하기, 곱하기 중 큰 수 찾기 함수
 function getLocalMax(arr) {
   const [left_Num, right_Num] = arr.map((el) => String(el));
 
@@ -39,6 +43,7 @@ function getLocalMax(arr) {
   return Math.max(left_Max, right_Max);
 }
 
+// 3. 최종 큰 수(정답) 구하기 함수
 function getAnswer(pobi_Max, crong_Max) {
   if (pobi_Max > crong_Max) {
     return 1;
