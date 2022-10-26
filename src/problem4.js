94e010bf175c28d68bd479f5c1f821780bc4520d
ctd <qpflapffhs76@gmail.com>
2022-10-26 23:10:55 +0900

    feat: 대문자, 소문자 판별해주는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,14 @@ function problem4(word) {
   return answer;
 }
 
+function isUpper(string) {
+  return string.match(new RegExp(/^[A-Z]/)) !== null;
+}
+
+function isLower(string) {
+  return string.match(new RegExp(/^[a-z]/)) !== null;
+}
+
+console.log(problem4('I love you'));
+
 module.exports = problem4;
