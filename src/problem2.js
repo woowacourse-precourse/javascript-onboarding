fea7c2fd28f7ddafd08af47562578b7e8d623542
sungjihyun <jhsung23@gmail.com>
2022-10-26 21:14:47 +0900

    Feat: problem 2: 암호 해독 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,9 @@
+const decrypt = (code) => {
+  return code.split("").reduce((acc, cur) => {
+    return acc[acc.length - 1] === cur ? acc : acc + cur;
+  });
+};
+
 function problem2(cryptogram) {
   var answer;
   return answer;
