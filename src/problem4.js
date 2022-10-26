2e3b428b19279de9c2e26ec10700df74ade8fda8
yeong bin <2976622@naver.com>
2022-10-27 00:39:46 +0900

    chore: problem4 - getReversedChar 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,15 @@
+/**
+ * 
+ * @param {char} char 
+ * @returns {char} reversed character
+ */
+function getReversedChar(char) {
+  const ascii = char.charCodeAt(0);
+  if (ascii >= 97)
+    return String.fromCharCode(97 + 97 + 25 - ascii);
+  return String.fromCharCode(65 + 65 + 25 - ascii);
+}
+
 function problem4(word) {
   var answer;
   return answer;
