5762409010b59a6764e2c049b036df322dc8dc81
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-26 16:20:50 +0900

    feat: problem1 input 유효성 검사

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+function isInvalidInput(pages) {
+  const [leftPage, rightPage] = pages;
+  if (leftPage + 1 !== rightPage) return true;
+  if (leftPage === 1 || rightPage === 400) return true;
+  if (leftPage % 2 === 0 || rightPage % 2) return true;
+  return false;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
