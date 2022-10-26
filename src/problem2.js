a1d2a5aa3d65c9d0fcd0c2116c634a09d8d75d29
Ryong-E <think8867@gmail.com>
2022-10-26 23:48:42 +0900

    refactor: 중복문자 삭제 함수 함수명/파라미터명 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,11 @@
 function problem2(cryptogram) {
   var answer;
-  answer = decodePassword(cryptogram)
+  answer = decodeCryptogram(cryptogram)
   return answer;
 }
 
-function decodePassword(password){
-  let decoded = password.split('');
+function decodeCryptogram(cryptogram){
+  let decoded = cryptogram.split('');
   for (let i = 1; i < decoded.length + 1; i++){
     if (decoded[i-1] === decoded[i]){
       decoded.splice(i-1,2);
