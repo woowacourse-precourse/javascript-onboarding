0bd79b06db9d49c66cf3325c2067c0709cecfec7
MinGyu-Kang <kagrin97@gmail.com>
2022-10-26 19:44:45 +0900

    problem1 : 예외처리 함수 구현
    1 or 400페이지가 나올경우 return -1
    
    오른쪽페이지가 왼쪽페이지보다 1만큼 더 크지 않을경우 return -1
    
    왼쪽페이지 != 홀수 or 오른페이지 != 짝수 일 경우 return -1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,35 @@
+function oddOrEven(leftNum, rightNum) {
+  if (leftNum % 2 !== 1 || rightNum % 2 !== 0) {
+    return true;
+  }
+}
+
+function exceptions(left, right) {
+  const leftNum = +left;
+  const rightNum = +right;
+
+  if (rightNum - leftNum !== 1) {
+    return true;
+  }
+  if (leftNum <= 1 || leftNum >= 400) {
+    return true;
+  }
+  if (oddOrEven(leftNum, rightNum)) {
+    return true;
+  }
+  return false;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const [pobiLeft, pobiRight] = pobi.map(String);
+  if (exceptions(pobiLeft, pobiRight)) {
+    return -1;
+  }
+
+  const [crongLeft, crongRight] = crong.map(String);
+  if (exceptions(crongLeft, crongRight)) {
+    return -1;
+  }
 }
 
 module.exports = problem1;
