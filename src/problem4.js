fa8ce523ff036cfa7091ab7483cc84eb6dd71c0f
Rki0 <pky00823@gmail.com>
2022-10-26 18:51:17 +0900

    feat: prob4. 대문자와 소문자를 구분하여 문자열을 변환하는 함수
    대문자는 대문자로, 소문자는 소문자로 변환한다.
    정반대의 순서로 알파벳이 진행되는 것은,
    수식으로 구현이 가능했다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,21 @@
+function checkUpperOrLower(oneWordASCII, ans) {
+  if (oneWordASCII >= 65 && oneWordASCII <= 90) {
+    // 대문자는 대문자로 변경
+    const newWordASCII = 90 - (oneWordASCII - 65);
+
+    const newWord = String.fromCharCode(newWordASCII);
+
+    ans.push(newWord);
+  } else if (oneWordASCII >= 97 && oneWordASCII <= 122) {
+    // 소문자는 소문자로 변경
+    const newWordASCII = 122 - (oneWordASCII - 97);
+
+    const newWord = String.fromCharCode(newWordASCII);
+
+    ans.push(newWord);
+  }
+}
+
 function checkSpaceBar(ans, word) {
   if (word === " ") {
     ans.push(word);
@@ -14,21 +32,7 @@ function problem4(word) {
 
     const oneWordASCII = wordArr[i].charCodeAt();
 
-    if (oneWordASCII >= 65 && oneWordASCII <= 90) {
-      // 대문자는 대문자로 변경
-      const newWordASCII = 90 - (oneWordASCII - 65);
-
-      const newWord = String.fromCharCode(newWordASCII);
-
-      ans.push(newWord);
-    } else if (oneWordASCII >= 97 && oneWordASCII <= 122) {
-      // 소문자는 소문자로 변경
-      const newWordASCII = 122 - (oneWordASCII - 97);
-
-      const newWord = String.fromCharCode(newWordASCII);
-
-      ans.push(newWord);
-    }
+    checkUpperOrLower(oneWordASCII, ans);
   }
 
   return ans.join("");
