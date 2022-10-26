ac64febbaf531df57cac27a05c22bc020a54459e
Hoon <si932174@gmail.com>
2022-10-26 17:40:56 +0900

    problem1 : 페이지숫자의 덧셈 곱셈 중 가장 큰 수 반환하는 함수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,47 @@
 function problem1(pobi, crong) {
   var answer;
+  
+  let pobiScore;
+  let crongScore;
+
+  const getNumberSumForPage = scoreNumbersArr => {
+
+    const numberSum = scoreNumbersArr.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0)
+
+    return numberSum;
+  };
+
+  const getNumberMultipleForPage = scoreNumbersArr => {
+    
+    const numberMultiple = scoreNumbersArr.reduce((acc, cur) => {
+      return acc * cur;
+    }, 1)
+
+    return numberMultiple;
+  };
+
+  const getBiggestNumber = (numberSum, numberMultiple) => {
+    return numberSum > numberMultiple ? numberSum : numberMultiple;
+  }
+
+  const getPageScore = (score) => {
+    console.log('score : ', score);
+
+    const scoreNumbersArr = (score + '').split('');
+    scoreNumbersArr.map((scoreNumber, i) => scoreNumbersArr[i] =  scoreNumber * 1);
+
+    const numbersSum = getNumberSumForPage(scoreNumbersArr);
+    const numbersMultiple = getNumberMultipleForPage(scoreNumbersArr);
+
+    const biggestNumber = getBiggestNumber(numbersSum, numbersMultiple);
+
+    return biggestNumber;
+  };
+
+  getPageScore(pobi[0]);
+  
   return answer;
 }
 
