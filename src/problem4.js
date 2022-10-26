3cbe7c956b7bddc7731e838d3cc2c99f49583a74
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-10-26 23:27:54 +0900

    [FEAT] Solve Probelm4
    문제4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  const lowObj = {
+    a: 'z', b: 'y', c: 'x', d: 'w', e: 'v', f: 'u', g: 't', h: 's', i: 'r', j: 'q', k: 'p', l: 'o',
+    m: 'n', n: 'm', o: 'l', p: 'k', q: 'j', r: 'i', s: 'h', t: 'g', u: 'f', v: 'e', w: 'd', x: 'c',
+    y: 'b', z: 'a',
+  };
+
+  const upperObj = {
+    A: 'Z', B: 'Y', C: 'X', D: 'W', E: 'V', F: 'U', G: 'T', H: 'S', I: 'R', J: 'Q', K: 'P', L: 'O',
+    M: 'N', N: 'M', O: 'L', P: 'K', Q: 'J', R: 'I', S: 'H', T: 'G', U: 'F', V: 'E', W: 'D', X: 'C',
+    Y: 'B', Z: 'A',
+  };
+
+  const answer = [...word].map(v => upperObj[v] || lowObj[v] || v).join('');
+
   return answer;
 }
 
