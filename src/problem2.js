b167e8d430abe379c3b13d33e80862a9e0fbc8d2
BB-choi <choibobae0809@gmail.com>
2022-10-26 20:49:37 +0900

    feat: 문제2 - 중복문자 제거 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let encryptedString = cryptogram;
+  let decryptedString = "";
+
+  for (let i = 0; i < encryptedString.length; i++) {
+    const currentChar = encryptedString[i];
+    const nextChar = encryptedString[i + 1];
+
+    if (currentChar === nextChar) {
+      i++;
+      continue;
+    }
+
+    decryptedString += currentChar;
+  }
+
+  return decryptedString;
 }
 
 module.exports = problem2;
