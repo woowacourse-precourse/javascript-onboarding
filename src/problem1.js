93903a838982df2f5559266d907c6acc365fc158
thewronghand <penfreak77@gmail.com>
2022-10-27 02:05:33 +0900

    feat: sumArrayNumber 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,18 +1,7 @@
 function problem1(pobi, crong) {
-  let pobiValue = Math.max(sumArrayNumber(stringToNumberArray(pobi[0])), sumArrayNumber(stringToNumberArray(pobi[1])), multiplyArrayNumber(stringToNumberArray(pobi[0])), multiplyArrayNumber(stringToNumberArray(pobi[1])))
-  let crongValue = Math.max(sumArrayNumber(stringToNumberArray(crong[0])), sumArrayNumber(stringToNumberArray(crong[1])), multiplyArrayNumber(stringToNumberArray(crong[0])), multiplyArrayNumber(stringToNumberArray(crong[1])))
-  if (pobiValue > crongValue){
-    return 1;
-  }
-  if (pobiValue == crongValue){
-    return 0;
-  }
-  if (pobiValue < crongValue){
-    return 2;
   }
 }
 module.exports = problem1;
 
 const stringToNumberArray = (n) => String(n).split('').map(char => Number(char));
 const sumArrayNumber = (array) => array.reduce((prev, cur) => {return prev + cur}, 0);
-const multiplyArrayNumber = (array) => array.reduce((prev, cur) => {return prev * cur}, 1);
