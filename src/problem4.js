e05e8c0164ce2c95998162f7410419d66e293671
Jongco <narira0922@gmail.com>
2022-10-26 21:11:06 +0900

    problem4: 아스키 코드의 반대 값을 적용하여 변환 후 출력
    - 아스키 코드로 변환하여 공백일 경우 공백(32) 그대로 출력
    - 대문자일 경우 (아스키 코드가 90이하일 경우) 청개구리 사전의 합이 155인 것을 이용해 반대 스펠링 반환
    - 소문자일 경우 합이 219인 것을 이용하여 반대 스펠링 반환(219 - ascii)
    - 문자로 변환 후 join을 이용해 결과 반환

--- src/problem4.js
+++ src/problem4.js
@@ -4,7 +4,20 @@ function problem4(word) {
    * 2. 대문자일 경우 (아스키 코드가 90이하일 경우) 청개구리 사전의 합이 155인 것을 이용해 반대 스펠링 반환
    * 3. 소문자일 경우 합이 219인 것을 이용하여 반대 스펠링 반환(219 - ascii)
    */
-  var answer;
+  let answer = word
+    .split("")
+    .map((string) => {
+      if (string.charCodeAt() === 32) {
+        return 32;
+      } else if (string.charCodeAt() <= 90) {
+        return 155 - string.charCodeAt();
+      } else {
+        return 219 - string.charCodeAt();
+      }
+    })
+    .map((ascii) => String.fromCharCode(ascii))
+    .join("");
+
   return answer;
 }
 
