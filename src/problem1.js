ac2d050ecfeb052b09fd429ee334f3218b1379e2
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 02:04:55 +0900

    problem1/feat : 제한사항 및 시작, 마지막 면에 해당하는 조건 추가

--- src/problem1.js
+++ src/problem1.js
@@ -4,43 +4,55 @@ function problem1(pobi, crong) {
   let pobiArr = [];
   let crongArr = [];
 
-  for (let i = 0; i < 2; i++) {
-    let pobiPlus = 0;
-    let pobiTimes = 1;
-    let crongPlus = 0;
-    let crongTimes = 1;
+  if (
+    pobi[1] - pobi[0] == 1 &&
+    crong[1] - crong[0] == 1 &&
+    pobi[0] != 1 &&
+    crong[0] != 1 &&
+    pobi[1] != 400 &&
+    crong[1] != 400 &&
+    pobi.length == 2 &&
+    crong.length == 2
+  ) {
+    for (let i = 0; i < 2; i++) {
+      let pobiPlus = 0;
+      let pobiTimes = 1;
+      let crongPlus = 0;
+      let crongTimes = 1;
 
-    for (let z = 0; z < String(pobi[i]).length; z++) {
-      pobiPlus += parseInt(String(pobi[i])[z]);
-      pobiTimes *= parseInt(String(pobi[i])[z]);
+      for (let z = 0; z < String(pobi[i]).length; z++) {
+        pobiPlus += parseInt(String(pobi[i])[z]);
+        pobiTimes *= parseInt(String(pobi[i])[z]);
+      }
+      for (let x = 0; x < String(crong[i]).length; x++) {
+        crongPlus += parseInt(String(crong[i])[x]);
+        crongTimes *= parseInt(String(crong[i])[x]);
+      }
+      pobiArr.push(pobiPlus, pobiTimes);
+      crongArr.push(crongPlus, crongTimes);
     }
-    for (let x = 0; x < String(crong[i]).length; x++) {
-      crongPlus += parseInt(String(crong[i])[x]);
-      crongTimes *= parseInt(String(crong[i])[x]);
-    }
-    pobiArr.push(pobiPlus, pobiTimes);
-    crongArr.push(crongPlus, crongTimes);
-  }
 
-  for (let i of pobiArr) {
-    if (pobiMax < i) {
-      pobiMax = i;
+    for (let i of pobiArr) {
+      if (pobiMax < i) {
+        pobiMax = i;
+      }
     }
-  }
 
-  for (let i of crongArr) {
-    if (crongMax < i) {
-      crongMax = i;
+    for (let i of crongArr) {
+      if (crongMax < i) {
+        crongMax = i;
+      }
     }
-  }
 
-  if (pobiMax > crongMax) {
-    return 1;
-  } else if (pobiMax < crongMax) {
-    return 2;
+    if (pobiMax > crongMax) {
+      return 1;
+    } else if (pobiMax < crongMax) {
+      return 2;
+    } else {
+      return 0;
+    }
   } else {
-    return 0;
+    return -1;
   }
 }
-
 module.exports = problem1;
