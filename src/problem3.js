8d2e49553097a9c5b50a6677f91cd19a3eb7a480
donghhan <apensia914@gmail.com>
2022-10-26 16:33:35 +0900

    Problem 3 solution added

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
-  var answer;
+  const arr = Array(number)
+    .fill()
+    .map((_, i) => String(i + 1));
+
+  const pattern = /[369]/g;
+  let answer = 0;
+
+  for (const i of arr) {
+    const count = i.match(pattern) !== null ? i.match(pattern).length : 0;
+    answer += count;
+  }
+
   return answer;
 }
 
