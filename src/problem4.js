526cfca31d5a3ba3d76b3a644a15da1471eb47e3
haesa <xdhaesa@gmail.com>
2022-10-27 01:28:10 +0900

    problem4 대소문자 판별 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
+function isUpperCase(code) {
+  return code >= 65 && code <= 90;
+}
+
+function isLowerrCase(code) {
+  return code >= 97 && code <= 122;
+}
+
 function problem4(word) {
   var answer;
   return answer;
 }
-
 module.exports = problem4;
