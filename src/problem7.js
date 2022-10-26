5200274ae56d72e5e8bc42c25a2d9f7763fb32ea
nlom0218 <nlom0218@naver.com>
2022-10-26 23:37:30 +0900

    feat: 문제 7 - 사용자와 함께 아는 친구의 수 점수 계산하는 함수 만들기

--- src/problem7.js
+++ src/problem7.js
@@ -3,9 +3,22 @@ const setUserMap = (userMap, [userA, userB]) => {
   userMap.set(userB, [...(userMap.get(userB) || []), userB]);
 };
 
+const getKnowEachOtherScore = (userMap, userA, userB) => {
+  const userAFriends = userMap.get(userA);
+  const userBFriends = userMap.get(userB);
+
+  let count = 0;
+  userAFriends.forEach((user) => {
+    if (userBFriends.includes(user)) count++;
+  });
+  return count * 10;
+};
+
 function problem7(user, friends, visitors) {
   const userMap = new Map();
   friends.forEach((item) => setUserMap(userMap, item));
+
+  console.log(getKnowEachOtherScore(userMap, "mrko", "jun"));
 }
 
 module.exports = problem7;
