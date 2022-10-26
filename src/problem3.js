732b6d288649dcc76fbf3d0027cfab4a1fac9c38
pakxe <pigkill40@naver.com>
2022-10-27 04:56:18 +0900

    refactor: [P3] 3, 6, 9 라는 숫자를 상수 CLAP_NUMBER로 묶음

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,12 @@
+const CLAP_NUMBER = [3, 6, 9];
+
 function problem3(number) {
   let handsClap = 0;
 
   for (let i = 1; i <= number; i++) {
     let num = i.toString().split(""); // 숫자를 순회할 수 있는 배열로 변경
     num.map((n) => {
-      if (n === "3" || n === "6" || n === "9") handsClap++;
+      if (CLAP_NUMBER.includes(Number(n))) handsClap++;
     });
   }
 
