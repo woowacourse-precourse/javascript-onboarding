ac8ebb11036de10d4be0ccf9bf220f0308ab1d90
ilgon0110 <moses011010@gmail.com>
2022-10-26 21:56:30 +0900

    feat: 연산 기능 구현(#problem2)

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  let stack = [];
+
+  //연산
+  //기능목록 1-1 구현
+  for (let text of cryptogram) {
+    //기능목록 1-2 구현
+    if (stack[stack.length - 1] === text) stack.pop();
+    else stack.push(text);
+  }
+
+  answer = stack.join("");
   return answer;
 }
 
