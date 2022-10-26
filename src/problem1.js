795c9494746c103b2d6b4f2982ce3108d234b4ee
hjy0951 <hjy0951@gmail.com>
2022-10-27 01:39:20 +0900

    problem1: 예외 처리 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,44 @@
+const LAST_PAGE = 400;
+
 function problem1(pobi, crong) {
   var answer;
+
+  if (hasException(pobi) || hasException(crong)) {
+    answer = -1;
+  } else {
+    // TODO
+  }
+
   return answer;
+}
+
+/* 예외 처리 */
+function hasException(pages) { 
+  return isOutOfRange(pages) || isInvalid(pages) || isNotContinuous(pages);
+}
+
+function isOutOfRange(pages) {
+  const [leftPage, rightPage] = pages;
+  if (leftPage < 1 || rightPage > LAST_PAGE) {
+    return true;
+  }
+  return false;
+}
+
+function isInvalid(pages) {
+  const [leftPage, rightPage] = pages;
+  if (leftPage % 2 !== 1 || rightPage % 2 === 1 || leftPage >= rightPage) {
+    return true;
+  }
+  return false;
+}
+
+function isNotContinuous(pages) {
+  const [leftPage, rightPage] = pages;
+  if (leftPage + 1 !== rightPage) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = problem1;
