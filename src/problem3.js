629f729de05ea854d5425a70df9030bad3102a1e
Suyoung <jsy8492@gmail.com>
2022-10-26 20:44:55 +0900

    feat(problem3): 문제 3 solution 함수 작성
    - 정규식을 활용하여 3, 6, 9 중 하나 이상을 포함하는지 검증

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  const regex369 = /[369]{1}/;
+
+  for (let i = 1; i <= number; i++) {
+    if (regex369.test(`${i}`)) {
+      answer += getClapCount(i);
+    }
+  }
+
   return answer;
 }
 
