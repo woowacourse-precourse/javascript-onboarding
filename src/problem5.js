afbb47bebbe0409f1620cc154d758d799ccff8cd
h2002y <h2002y@naver.com>
2022-10-26 19:59:09 +0900

    feat: Add main func

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let moneyLeft = money;
+  for(let i = 0; i < 9; i++){
+    answer.push(parseInt(moneyLeft / coins[i]));
+    moneyLeft %= coins[i];
+  }
   return answer;
 }
 
