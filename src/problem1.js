fca2558c3bd3ab70f7e5d80467bfb52951a201c5
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-26 16:07:38 +0900

    feat: add getMaxNumber function
    - 각 자리의 합과 곱을 비교하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,16 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  function getMaxNumber(num) {
+    let arr = String(num).split("");
+    let plus = arr.reduce((a, c) => {
+      a = a + parseInt(c);
+      return a;
+    }, 0);
+    let multiple = arr.reduce((a, c) => {
+      a = a * +c;
+      return a;
+    }, 1);
+    return Math.max(plus, multiple);
+  }
 }
 
 module.exports = problem1;
