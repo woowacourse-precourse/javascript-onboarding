0acf02adb2354b1a502de4c72e20bda4abf26f4a
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-26 17:27:10 +0900

    feat: 포비의 점수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,38 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const addAllNums = (num) => {
+    if (num < 10) {
+      return num;
+    }
+    return (num % 10) + addAllNums(parseInt(num / 10));
+  };
+
+  const multiplyAllNums = (num) => {
+    if (num < 10) {
+      return num;
+    }
+    return (num % 10) * multiplyAllNums(parseInt(num / 10));
+  };
+
+  const findMax = (num1, num2) => {
+    return Math.max(num1, num2);
+  };
+
+  const maxNumOfPobi = () => {
+    let leftMax_of_pobi = findMax(
+      addAllNums(pobi[0]),
+      multiplyAllNums(pobi[0])
+    );
+    let rightMax_of_pobi = findMax(
+      addAllNums(pobi[1]),
+      multiplyAllNums(pobi[1])
+    );
+    return findMax(leftMax_of_pobi, rightMax_of_pobi);
+  };
+
+  let pobi_score = maxNumOfPobi();
+
   return answer;
 }
 
