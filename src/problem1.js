5442aa96006f58b256aef46b9a76e92c6f007dbe
yeong bin <2976622@naver.com>
2022-10-26 19:59:17 +0900

    chore: problem1 - checkValidation 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,17 @@
-function checkValidation() {
-  //입력된 페이지가 유효한지 검증
+/**
+ * Check for valid values
+ * @param {Array} pages 
+ * @returns {boolean}
+ */
+function checkValidation(pages) {
+  if (pages.length === 2 
+      && (Number.isInteger(pages[0]) && Number.isInteger(pages[1]))
+      && (pages[0] % 2 === 1 && pages[0] + 1 === pages[1])
+      && pages[1] <= 400)
+    return true;
+  return false;
 }
+
 
 function calculateTotalScore() {
   //펼친 페이지의 score 계산
