9adc8bf6f0a6bc7abcf43612f74d46c79c020a5f
Hwang Seong Taek <hst007@naver.com>
2022-10-26 20:17:02 +0900

    feat: problem3 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -7,8 +7,18 @@
 */
 
 function problem3(number) {
-  var answer;
-  return answer;
+  let clap = 0;
+  const clapNum = /3|6|9/g;
+  
+  for (let i=1; i<=number; i++) {
+    const matchNum = i.toString().match(clapNum);
+
+    if(matchNum !== null) { // match된 3, 6, 9가 있을 경우
+      clap += matchNum.length;
+    } 
+  }
+
+  return clap;
 }
 
 module.exports = problem3;
