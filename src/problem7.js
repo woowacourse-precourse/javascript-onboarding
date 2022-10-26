dbbfe8ac69c583f4b95372e45cc0539dc909a988
h2002y <h2002y@naver.com>
2022-10-26 23:57:29 +0900

    feat: setMap func

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,16 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let map = new Map();
+  let mapIndex = 0;
+  setMap(map, mapIndex, user, friends, visitors);
   return answer;
+}
+
+function setMap(map, mapIndex, user, friends, visitors){
+  map.set(user);
+  mapIndex++;
+  setMapFriends(map, mapIndex, friends);
+  setMapVisitors(map, mapIndex, visitors);
 }
 
 module.exports = problem7;
