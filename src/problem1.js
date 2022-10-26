d2be555e6fde872ea6147c3ddde127e0ee52904d
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-26 23:44:13 +0900

    feat: problem1.js에 점수를 구하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -6,9 +6,35 @@ const isValidPages = (pages) => {
   return true;
 };
 
+const getScore = (pages) => {
+  const [leftPageSum, righPageSum] = pages.map((page) => 
+    String(page).split('').reduce((acc, cur) => Number(acc) + Number(cur), 0)
+  );
+  const [leftPageProduct, rightPageProduct] = pages.map((page) => 
+    String(page).split('').reduce((acc, cur) => Number(acc) * Number(cur), 1)
+  );
+
+  return Math.max(leftPageSum, righPageSum, leftPageProduct, rightPageProduct);
+};
+
 function problem1(pobi, crong) {
   if (!(isValidPages(pobi) && isValidPages(crong))) {
     return -1;
+  }
+
+  const pobiScore = getScore(pobi);
+  const crongScore = getScore(crong);
+  
+  if (pobiScore === crongScore) {
+    return 0;
+  }
+
+  if (pobiScore > crongScore) {
+    return 1;
+  }
+
+  if (pobiScore < crongScore) {
+    return 2;
   }
 }
 
