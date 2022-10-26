e0159f563bb467248dc7ffc04f575bdadb9ff458
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-27 00:19:51 +0900

    feat: check str is alphabet

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  for (let index = 0; index < word.length; index++) {
+    if (
+      (word.charCodeAt(index) >= 97 && word.charCodeAt(index) <= 122) ||
+      (word.charCodeAt(index) >= 65 && word.charCodeAt(index) <= 90)
+    ) {
+      console.log(word[index]);
+    } else {
+      console.log(word[index]);
+    }
+  }
 }
-
 module.exports = problem4;
