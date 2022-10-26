fada89b75dcc39997ac9e6c0c829d059aa2153df
jihun1233 <leejihun1233@gmail.com>
2022-10-26 19:04:44 +0900

    문제1 해결
    - 페이지의 번호를 단위별로 더하거나 곱하는 getMaxPageValue함수를 만들어 각 페이지마다 구하도록 함
    
    - 1번이나 400번이 나오는 경우, 두 페이지 번호 차이가 1이 아닐경우를 예외로 반환

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,33 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const [pobiLeft, pobiRight] = pobi
+  const [crongLeft, crongRight] = crong
+
+  if(Math.max(...pobi, ...crong) === 400 || Math.min(...pobi, ...crong)===0)return -1
+  if(pobiRight-pobiLeft !== 1 || crongRight - crongLeft !== 1)return -1
+
+  const pobiMax = math.Max(getMaxPageValue(pobiLeft), getMaxPageValue(pobiRight))
+  const crongMax = math.Max(getMaxPageValue(crongLeft), getMaxPageValue(crongRight))
+
+  if(pobiMax === crongMax)return 0
+  if(pobiMax > crongMax) return 1
+  if(pobiMax < crongMax) return 2
+  return -1;
+}
+
+function getMaxPageValue(pageNum){
+  const pageStr = `${pageNum}`
+  const pageArr = pageStr.split('')
+  
+  let sum = 0
+  let multi = 0
+
+  pageArr.forEach(num => {
+    const parsedNum = parseInt(num, 10)
+    sum += parsedNum
+    multi *= parsedNum
+  })
+  
+  return Math.max(sum, multi)
 }
 
 module.exports = problem1;
