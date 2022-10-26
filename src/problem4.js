a7e3bfafe178db805341b4f570226872c04e0b09
Jiwoo Park <jiwoo9446@naver.com>
2022-10-26 22:43:52 +0900

    problem4 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  for (let i = 0; i < word.length; i++) {
+    let ascii = word.charCodeAt(i);
+    if (ascii >= 65 && ascii <= 90) ascii = 65 + 90 - ascii;
+    else if (ascii >= 97 && ascii <= 122) ascii = 97 + 122 - ascii;
+    answer += String.fromCharCode(ascii);
+  }
   return answer;
 }
-
 module.exports = problem4;
