b0677c1a9d2c5367c7d3d5b37850ffe7572d5d2c
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-26 21:01:13 +0900

    feat: [문제3] 369 개수 세기 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  const count = 0;
+  const container369 = ['3', '6', '9'];
+  const count369 = (number, count) => {
+    if (number === 0) {
+      return count;
+    }
+
+    [...String(number)].forEach((character) => {
+      if (container369.includes(character)) {
+        count++;
+      }
+    });
+    return count369(number - 1, count);
+  };
+  return count369(number, 0);
 }
 
 module.exports = problem3;
