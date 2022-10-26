5be6510d0c0234d7b1d8696432a028ac0bda7b07
nlom0218 <nlom0218@naver.com>
2022-10-26 22:11:28 +0900

    feat: 문제 6 - 이메일의 도메인이 email.com인지 판별함수를 만들어 도메인이 다르다면 이메일 제외시키기

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,12 @@
+const isCorrectEmail = (email) => {
+  const [_, domain] = email.split("@");
+  return domain === "email.com";
+};
+
 function problem6(forms) {
+  forms = forms.filter(([email, nickname]) => {
+    return isCorrectEmail(email);
+  });
   var answer;
   return answer;
 }
