44699f6e8e9ae335140b0ae869d493f7d17792ce
evencoding <evencoding@gmail.com>
2022-10-26 22:54:53 +0900

    P1-Implement-페이지 번호의 각 자리 숫자를 모두 곱하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -8,6 +8,14 @@ function sumAllDigitNumber(page) {
   return result;
 }
 
+function mulAllDigitNumber(page) {
+  let result = +page[0];
+  for (let i = 1; i < page.length; i++) {
+    result *= +page[i];
+  }
+  return result;
+}
+
 function problem1(pobi, crong) {
   var answer;
 
