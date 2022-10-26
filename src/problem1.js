193925638a5db92e9ce4387f4b240ed718d0dd41
MyungAe <ladiescheong@naver.com>
2022-10-26 16:13:50 +0900

    style : 커밋 컨벤션 참고자료 추가

--- src/problem1.js
+++ src/problem1.js
@@ -93,3 +93,7 @@ module.exports = problem1;
     });
   });
 */
+
+/*
+커밋 컨벤션 참고 : https://velog.io/@shin6403/Git-git-%EC%BB%A4%EB%B0%8B-%EC%BB%A8%EB%B2%A4%EC%85%98-%EC%84%A4%EC%A0%95%ED%95%98%EA%B8%B0
+*/
