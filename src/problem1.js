74ffd1cb71714d41d44f0d9705bc180044b193b7
oseunghyun <ohsh1477@gmail.com>
2022-10-26 22:50:49 +0900

    feat(p1): 각 자릿수들의 합을 구하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,8 @@
+// 각 자릿수들의 합을 구하는 기능
+const sumDigit = (num) => {
+  return parseInt(num / 100) + parseInt((num % 100) / 10) + parseInt(num % 10);
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
