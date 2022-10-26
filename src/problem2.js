c9b55e55c17250f5a28d7fa1df54c17352889f2c
Jongco <narira0922@gmail.com>
2022-10-26 20:46:26 +0900

    problem2: 겹치는 문자가 홀수일 경우 문자 하나가 남는 상황 발생
    baaaoat를 입력했을 경우 boat가 아닌 baoat로 반환됨

--- src/problem2.js
+++ src/problem2.js
@@ -2,7 +2,8 @@ function problem2(cryptogram) {
   /**
    * 1. 문자를 배열로 분해
    * 2. 바로 다음에 오는 문자와 현재 문자를 비교하여 같으면 제거하고 함수 재실행(재귀함수)
-   * 3. 겹치는 문자가 없다면 함수 종료
+   * 3. 겹치는 문자가 홀수일 경우 문자 하나가 남는 상황 핸들링 // baaaoat -> boat가 아닌 baoat로 반환됨
+   * 4. 겹치는 문자가 없다면 함수 종료
    */
   let answer = cryptogram.split("");
   function removeString() {
@@ -17,6 +18,6 @@ function problem2(cryptogram) {
   return answer.join("");
 }
 
-console.log(problem2("browoanoommnaon"));
+console.log(problem2("baaaoat"));
 
 module.exports = problem2;
