5f88dfc220aa5a5456535704f6bc4e018bc48061
eunsung <esk114477@gmail.com>
2022-10-26 21:30:01 +0900

    최대값 구하기 작성

--- src/problem1.js
+++ src/problem1.js
@@ -2,16 +2,51 @@ function problem1(pobi, crong) {
 
   //예외처리
   const checkPage = (arr) => {
-    if(arr[0]+1 !== arr[1]) return false;
+    if(arr[0] + 1 !== arr[1]) return false;
     if(arr.length !== 2) return false;
-    if(arr[0] < 1 || arr[0] % 2 === 0) return false;
-    if(arr[1] > 400 || arr[1] % 2 === 1) return false;
-    // arr[0] 이 짝수이거나 arr[1] 이 홀수일때 조건
+    if(arr[0] <= 1 || arr[1] >= 400) return false;
+    if(arr[0] % 2 === 0 || arr[1] % 2 === 1) return false;
     return true;
   }
 
-  var answer;
-  return answer;
+  //최대값 구하기
+  const checkMax = (page) => {
+    let arrLeft = page[0];
+    let arrRight = page[1];
+    
+    let leftArr = arrLeft.toString().split("").map((i) => Number(i));
+    let rightArr = arrRight.toString().split("").map((i) => Number(i));
+
+    let sumLeft = leftArr.reduce((acc,cur,idx) => {return acc += cur}, 0);
+    let sumRight = rightArr.reduce((acc,cur) => {return acc += cur}, 0);
+
+    let mulLeft = leftArr.reduce((acc, cur) => {return acc *= cur}, 1);
+    let mulRight = rightArr.reduce((acc, cur) => {return acc *= cur}, 1);
+
+    let sumMax = Math.max(sumLeft, sumRight);
+    let mulMax = Math.max(mulLeft, mulRight);
+
+    let maxValue = Math.max(sumMax, mulMax);
+
+    return maxValue;
+  }
+
+  if(!checkPage(pobi) || !checkPage(crong)) {
+    return -1;
+  }
+
+  const pobiMax = checkMax(pobi);
+  const crongMax = checkMax(crong);
+
+  if(pobiMax > crongMax){
+    return 1;
+  } else if(pobiMax < crongMax){
+    return 2;
+  } else if(pobiMax === crongMax){
+    return 0;
+  } else {
+    return -1;
+  }
 }
 
 module.exports = problem1;
