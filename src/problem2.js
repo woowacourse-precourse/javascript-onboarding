52a77a11e5e6262c2cbbf0719fbc4dcbf2828dac
Simyumi <gkghgkgh0@naver.com>
2022-10-28 03:04:30 +0900

    problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
+// function problem2(cryptogram) {
+//   var answer;
+//   return answer;
+// }
+
+// module.exports = problem2;
+
 function problem2(cryptogram) {
-  var answer;
+  var answer = [];
+  let arr = cryptogram.split("");
+
+  for (let i = 0; i <= arr.length; i++) {
+    if (answer[answer.length - 1] == arr[i]) answer.pop();
+    else answer.push(arr[i]);
+  }
   return answer;
+
 }
 
-module.exports = problem2;
+const cryptogram = 'zyelleyz';
+console.log(problem2(cryptogram).join(" "));
