035d8cf5903d144d85feafbfd95e98095facaa89
moonelysian <dandyblack@naver.com>
2022-10-27 10:42:20 +0900

    add: 큰 금액 위주로 출금해 각 금액별로 몇개씩 필요한지 return

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = cash.map(c => {
+    const quo = parseInt(money / c);
+    money = money % c;
+    return quo
+  })
+  return result;
 }
 
 module.exports = problem5;
