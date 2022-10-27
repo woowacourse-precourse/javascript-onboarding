8cd391c3e03b97fa1e8f9b9073a07ed9914f41a1
KirschX <mk44879@gmail.com>
2022-10-27 19:36:03 +0900

    과제3 기능구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,29 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  const checkCount = (n) => {
+    let count = 0;
+    let stringN = n.toString();
+    for (let i = 0; i < stringN.length; i++) {
+      if (stringN[i] === "3" || stringN[i] === "6" || stringN[i] === "9") {
+        count = count + 1;
+        // console.log(n, i, count, stringN[i]);
+      }
+    }
+    return count;
+  };
+
+  for (let i = 1; i < number + 1; i++) {
+    const stringN = i.toString();
+
+    if (
+      stringN.includes("3") ||
+      stringN.includes("6") ||
+      stringN.includes("9")
+    ) {
+      answer = answer + checkCount(i);
+    }
+  }
   return answer;
 }
 
