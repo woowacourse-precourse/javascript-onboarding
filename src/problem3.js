b6c590ae83d3ecc3898b073ec0bde78d5c841afe
easyone <thinkanddoit@naver.com>
2022-10-27 18:12:22 +0900

    #3 feat: 문제3 `3, 6, 9` counting 함수 생성
    - count369(num)

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,15 @@
 function problem3(number) {
+  const count369 = (num) => {
+    const Candidates = [3, 6, 9];
+    let count = 0;
+    num
+      .toString()
+      .split("")
+      .forEach((char) => {
+        if (Candidates.includes(Number(char))) count++;
+      });
+  };
+
   var answer;
   return answer;
 }
