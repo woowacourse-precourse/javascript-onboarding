63c50a47c01c6a777d4a52cff04a8c776ef7a53f
seongminn <berry9901@gmail.com>
2022-10-28 01:03:22 +0900

    feat(problem1): 예외사항을 처리하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,14 @@
+function checkValidation(pageArr) {
+  const [left, right] = pageArr;
+
+  return left % 2 === 1 && right - left && left >= 3 && left <= 397;
+}
+
 function problem1(pobi, crong) {
+  if (!checkValidation(pobi) || !checkValidation(crong)) return -1;
+
   var answer;
+
   return answer;
 }
 
