de7d5a498850dba13b8734665c585cda51308935
GuHyeonWoo <ghw9174@gmail.com>
2022-10-27 16:31:05 +0900

    P3/수를 순회하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,8 +3,20 @@
 // 2. 수를 한 자 씩 나누는 기능
 // 3. 3, 6, 9를 카운트하는 기능
 
+// 수를 순회하는 함수
+function circuit(number) {
+  // 3, 6, 9의 총 개수
+  let count = 0;
+  // 1부터 number까지 순회
+  for(let i = 1; i <= number; i++){
+    // 수를 한 자 씩 나누어 카운트 하는 부분
+  }
+  return count;
+}
+
 function problem3(number) {
-  let answer;
+  // 순회하여 결과값을 반환 받아 할당
+  const answer = circuit(number);
   return answer;
 }
 
