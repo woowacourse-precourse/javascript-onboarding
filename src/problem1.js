4df2cbac43b9580a009e841cdd7fcaa82ea83155
Chris.c <cjs120dd@gmail.com>
2022-10-27 11:55:59 +0900

    fear:1번 문제를 해결한다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,51 @@
+const pageIndex = {
+    left: 0,
+    right: 1,
+}
+
+const winnerCount = {
+    pobi:1,
+    crong:2,
+    tie:0,
+    exception:-1
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    var answer = winnerCount.tie;
+    
+    function getPlusNum(targetPage) {
+        return [...String(targetPage)].reduce((acc, cur) => Number(acc) + Number(cur));
+    }
+    
+    function getMultipleNum(targetPage) {
+        return [...String(targetPage)].reduce((acc, cur) => Number(acc) * Number(cur));
+    }
+    
+    function getMaxScore(name) {
+        return Math.max(
+            getPlusNum(name[ pageIndex.left ]),
+            getMultipleNum(name[ pageIndex.left ]),
+            getPlusNum(name[ pageIndex.right ]),
+            getMultipleNum(name[ pageIndex.right ])
+        )
+    }
+    
+    const pobiScore = getMaxScore(pobi);
+    const crongScore = getMaxScore(crong);
+    if (pobiScore < crongScore) {
+        answer = winnerCount.crong;
+    }
+    if (pobiScore > crongScore) {
+        answer = winnerCount.pobi;
+    }
+    if (
+        pobi[ pageIndex.left ] + 1 !== pobi[ pageIndex.right ]
+        ||
+        crong[ pageIndex.left ] + 1 !== crong[ pageIndex.right ]
+    ) {
+        answer = winnerCount.exception;
+    }
+    return answer
 }
 
 module.exports = problem1;
