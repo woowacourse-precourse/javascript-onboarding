01629d714bd5db4dd963e617ddd88107ca7eff30
moonelysian <dandyblack@naver.com>
2022-10-27 10:34:33 +0900

    add: 청개구리 사전대로 return값 계산

--- src/problem4.js
+++ src/problem4.js
@@ -10,6 +10,15 @@ function problem4(word) {
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
   const upperAlphabet = alpha.map((x) => String.fromCharCode(x));
   const words = [...word]
+  const result = words.map(w => {
+    if (!isAlphabet(w)) {
+      return w;
+    }
+    const index = upperAlphabet.findIndex(alpha => alpha === w.toUpperCase());
+    const convertedIndex = upperAlphabet.length - index - 1;
+    return isUpperCase(w) ? upperAlphabet[convertedIndex] : upperAlphabet[convertedIndex].toLowerCase();
+  });
+  return result.join('');
 }
 
 module.exports = problem4;
