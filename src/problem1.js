1cf97f2747fbc2418cf32cfbeea3c23e4ede57c4
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-27 23:23:58 +0900

    feat: PROBLEM 1 각 자리 숫자 합 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
   let answer;
 
+  const [pobiLeftPage, pobiRightPage] = pobi;
+  const [crongLeftPage, crongRightPage] = crong;
+
+  const getSum = (page) => {
+    return page.toString().split('').reduce((a, b) => a + Number(b), 0);
+  };
+
   const check = (arr) => {
     if (arr.length !== 2) return false;
     if (arr[0] + 1 !== arr[1]) return false;
@@ -9,8 +16,10 @@ function problem1(pobi, crong) {
     if (arr[1] >= 400) return false;
     return true;
   }
-  if (!check(pobi) || !check(crong)) return answer = -1
-  
+  if (!check(pobi) || !check(crong)) return answer = -1;
+  else {
+
+  }
   return answer;
 }
 
