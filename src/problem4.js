72ebba8b26d9925efa3f3af3038ed7614d9a5f79
Suyoung <jsy8492@gmail.com>
2022-10-27 13:09:08 +0900

    feat(problem4): 청개구리 변환 함수 구현
    - charCode 기반으로 차를 이용하여 구현

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,26 @@ const [CHARCODE_A, CHARCODE_Z, CHARCODE_a, CHARCODE_z] = 'AZaz'
   .split('')
   .map((_, idx) => 'AZaz'.charCodeAt(idx));
 
+function frogTranslate(c) {
+  const charCode = c.charCodeAt(0);
+  let diffFromEnd;
+  let diffFromBegin;
+
+  if (charCode >= CHARCODE_a) {
+    [diffFromEnd, diffFromBegin] = [
+      CHARCODE_z - charCode,
+      charCode - CHARCODE_a,
+    ];
+  } else {
+    [diffFromEnd, diffFromBegin] = [
+      CHARCODE_Z - charCode,
+      charCode - CHARCODE_A,
+    ];
+  }
+
+  return String.fromCharCode(charCode + (diffFromEnd - diffFromBegin));
+}
+
 function problem4(word) {
   const alphabetRegex = /[a-zA-Z]/;
 
