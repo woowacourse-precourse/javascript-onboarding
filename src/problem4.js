eacaecfaf3be0f31b6eb508407bdb3911163e3ad
guridaek <iue16@naver.com>
2022-10-27 13:16:36 +0900

    feat: 문제4 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  let answer = '';
+
+  for (let e of word) {
+    if (e >= 'a' && e <= 'z') {
+      let ascii = 'z'.charCodeAt() - (e.charCodeAt() - 'a'.charCodeAt());
+      answer += String.fromCharCode(ascii);
+    } else if (e >= 'A' && e <= 'Z') {
+      let ascii = 'Z'.charCodeAt() - (e.charCodeAt() - 'A'.charCodeAt());
+      answer += String.fromCharCode(ascii);
+    } else {
+      answer += e;
+    }
+  }
+
   return answer;
 }
 
