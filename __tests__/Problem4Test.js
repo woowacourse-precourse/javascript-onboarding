756d61c56658296c97600fa3edb906dcd49d56bc
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-27 17:24:16 +0900

    feat: problem4 setting init , tdd test code

---
+++ __tests__/Problem4Test.js
@@ -0,0 +1,38 @@
+const {
+  checkInputError,
+  checkAlpha,
+  transAlpha,
+} = require("../src/problem4.util");
+
+describe("checkInputError", () => {
+  it("is return true by short", () => {
+    expect(checkInputError("")).toEqual(true);
+  });
+  it("is return true by long", () => {
+    let temp = "";
+    for (let i = 0; i <= 1000; i++) temp += "r";
+    expect(checkInputError(temp)).toEqual(true);
+  });
+
+  it("is return false", () => {
+    expect(checkInputError("abc")).toEqual(true);
+  });
+});
+
+describe("checkAlpha", () => {
+  it("is input alphabet check return true", () => {
+    expect(checkAlpha("a")).toEqual(true);
+  });
+  it("is input alphabet check return false", () => {
+    expect(checkAlpha(" ")).toEqual(false);
+  });
+});
+
+describe("transAlpha", () => {
+  it("is input trans big", () => {
+    expect(transAlpha("A")).toEqual("Z");
+  });
+  it("is input trans small", () => {
+    expect(transAlpha("a")).toEqual("z");
+  });
+});
---
+++ src/problem4.util.js
@@ -0,0 +1,10 @@
+// word 길이 체크
+const checkInputError = (word) => {};
+// 알파벳 체크
+const checkAlpha = (alpha) => {};
+// 알파벳 변환
+const transAlpha = (alpha) => {};
+
+exports.checkInputError = checkInputError;
+exports.checkAlpha = checkAlpha;
+exports.transAlpha = transAlpha;
