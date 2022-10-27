e5f612a65c426888a11a55f2f5cfa37ed85973ea
seohee0112 <ghdtj7@naver.com>
2022-10-27 09:45:40 +0900

    feat : problem1 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,37 @@
+function add(number) {
+  let sum = 0;
+  while (number > 0) {
+    sum += number % 10;
+    number = Math.floor(number / 10);
+  }
+  return sum;
+}
+function mul(number) {
+  let result = 1;
+  while (number > 0) {
+    result = result * (number % 10);
+    number = Math.floor(number / 10);
+  }
+  return result;
+}
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (Math.abs(pobi[0] - pobi[1]) != 1 || Math.abs(crong[0] - crong[1]) != 1)
+    return -1;
+
+  pobi[0] = add(pobi[0]) > mul(pobi[0]) ? add(pobi[0]) : mul(pobi[0]);
+  pobi[1] = add(pobi[1]) > mul(pobi[1]) ? add(pobi[1]) : mul(pobi[1]);
+
+  crong[0] = add(crong[0]) > mul(crong[0]) ? add(crong[0]) : mul(crong[0]);
+  crong[1] = add(crong[1]) > mul(crong[1]) ? add(crong[1]) : mul(crong[1]);
+
+  const pobiResult = pobi[0] > pobi[1] ? pobi[0] : pobi[1];
+  const crongResult = crong[0] > crong[1] ? crong[0] : crong[1];
+
+  if (pobiResult > crongResult) return 1;
+  else if (pobiResult < crongResult) return 2;
+  else return 0;
 }
 
 module.exports = problem1;
+
+console.log(problem1([97, 98], [197, 198]));
