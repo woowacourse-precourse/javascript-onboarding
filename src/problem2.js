9b069b6aed67d26b61fcfa59fec5f9263da9d1b7
Hungeun2 <bepoohun@gmail.com>
2022-10-27 16:59:22 +0900

    Fix: problem2 flow 수정 및 로직 수정 중복된 값이 2개 이상일때 제거 되지 않는 문제 fix

--- src/problem2.js
+++ src/problem2.js
@@ -1,30 +1,38 @@
 // 1. 주어진 문자열 배열로 변경
-// 2. for문을 통해 현재의 값과 다음 값이 동일하다면 현재 값과 다음 값 배열에서 splice
-// 3. 배열에 원소가 없거나 더이상 중복이 없을때까지 반복
+// 2. forEach문을 통해 현재의 값과 다음 값 및 이전 값 비교하여 다르면 임시 배열에 추가
+// 3. 임시 리스트 완성 후 원본 리스트와 길이가 같으면 더 이상 중복된 값이 없으므로 while문 break
+// 4. 다를 시 여전히 중복된 값이 존재 하므로 원본 리스트를 임시 리스트로 재할당
 // 4. 배열 문자열 화
 
 function problem2(cryptogram) {
   var answer;
   let flag = true;
   // List 화
-  let cryptoList = cryptogram.split("");
+  let cryptogramList = cryptogram.split("");
 
   // 값 비교 for문
   while (flag) {
-    // 원소가 없음
-    if (cryptoList.length === 0) flag = false;
+    //임시 리스트 생성
+    let tempList = [];
 
-    for (let i = 0; i < cryptoList.length; i++) {
-      if (cryptoList[i] === cryptoList[i + 1]) {
-        cryptoList.splice(i, 2);
-        break;
+    // 순회하며 임시 리스트에 값 추가
+    cryptogramList.forEach((data, index) => {
+      if (
+        data !== cryptogramList[index + 1] &&
+        data !== cryptogramList[index - 1]
+      ) {
+        tempList.push(data);
       }
-      // 중복이 없음
-      if (i === cryptoList.length - 1) flag = false;
-    }
+    });
+
+    // 길이 비교 같다면 더이상 중복된 문자열이 없으므로 while문 중단
+    if (cryptogramList.length === tempList.length) flag = false;
+
+    //재할당
+    cryptogramList = tempList;
   }
   //문자열 화
-  answer = cryptoList.join("");
+  answer = cryptogramList.join("");
   return answer;
 }
 
