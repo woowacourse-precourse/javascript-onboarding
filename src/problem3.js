bede831c0a5ab2d16d922174f1bfa126ab3a9020
jlee0505 <hj960322@gmail.com>
2022-10-27 15:18:25 +0900

    problem3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,26 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var count = 0;
+
+  const Numbers = Array.from({ length: number }, (v, i) => i + 1);
+  Numbers.forEach((eachNum) => {
+    let temp = eachNum.toString().split("");
+    temp.forEach((char) => {
+      switch (char) {
+        case "3":
+        case "6":
+        case "9":
+          count++;
+          break;
+      }
+    });
+  });
+
+  return count;
 }
 
 module.exports = problem3;
+
+// expected: 4
+console.log(problem3(13));
+// expected: 14
+console.log(problem3(33));
