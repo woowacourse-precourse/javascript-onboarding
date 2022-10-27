5c98b475927c560eb5401cba3f3349253e7d5de6
hsy <69149030+dev-redo@users.noreply.github.com>
2022-10-27 15:47:07 +0900

    [solved 1번] 구현 풀이

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,55 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const isPobiVaid = checkInputIsValid(pobi);
+  const isCrongValid = checkInputIsValid(crong);
+
+  if (!(isPobiVaid && isCrongValid)) {
+    return -1;
+  }
+
+  const pobiScroe = getScore(pobi);
+  const crongScroe = getScore(crong);
+
+  if (pobiScroe === crongScroe) return 0;
+  if (pobiScroe > crongScroe) return 1;
+  if (pobiScroe < crongScroe) return 2;
+}
+
+function checkInputIsValid(input) {
+  if (input.length !== 2) return false;
+
+  const [left, right] = input;
+  const [startPage, lastPage] = [1, 400];
+
+  if (left + 1 !== right) return false;
+  if (left % 2 !== 1 && right % 2 !== 0) return false;
+  if (left <= startPage || left >= lastPage) return false;
+  if (right <= startPage || right >= lastPage) return false;
+
+  return true;
+}
+
+function getScore([left, right]) {
+  return Math.max(
+    sumPageNum(left),
+    multiplyPageNum(left),
+    sumPageNum(right),
+    multiplyPageNum(right),
+  );
+}
+
+function sumPageNum(page) {
+  const pageNumbers = splitPageNumbers(page);
+  return pageNumbers.reduce((sum, num) => (sum += num), 0);
+}
+
+function multiplyPageNum(page) {
+  const pageNumbers = splitPageNumbers(page);
+  return pageNumbers.reduce((multi, num) => (multi *= num), 1);
+}
+
+function splitPageNumbers(page) {
+  const pageEl = page.toString().split('');
+  return pageEl.map(el => Number(el));
 }
 
 module.exports = problem1;
