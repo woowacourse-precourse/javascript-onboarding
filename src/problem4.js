6827dbb9f1ec1974058cd9bf2b37fbda3104e18f
cys4585 <choizerowater@gmail.com>
2022-10-27 20:13:02 +0900

    refactor: 코드 기능별로 함수화

--- src/problem4.js
+++ src/problem4.js
@@ -3,16 +3,34 @@ function problem4(word) {
 
   answer = "";
   for (let i = 0; i < word.length; i++) {
-    const character = word[i]
-    if ('A' <= character && character <= 'Z') {
-      answer += String.fromCharCode(65 + 90 - character.charCodeAt());
-    } else if ('a' <= character && character <= 'z') {
-      answer += String.fromCharCode(97 + 122 - character.charCodeAt());
+    const character = word[i];
+
+    if (isAlphabet(character)) {
+      answer += convertSymmetry(character);
     } else {
       answer += character;
     }
   }
+
   return answer;
+}
+
+function isAlphabet(character) {
+  if ("A" <= character && character <= "Z") return true;
+  if ("a" <= character && character <= "z") return true;
+  return false;
+}
+
+function convertSymmetry(character) {
+  let asciiCode;
+
+  if ("A" <= character && character <= "Z") {
+    asciiCode = "A".charCodeAt() + "Z".charCodeAt();
+  } else {
+    asciiCode = "a".charCodeAt() + "z".charCodeAt();
+  }
+  asciiCode -= character.charCodeAt();
+  return String.fromCharCode(asciiCode);
 }
 
 module.exports = problem4;
