92855fc44f3789a0ca48dfdc994482411aff695e
KES <aprilsmaple@gmail.com>
2022-10-27 11:19:23 +0900

    feat: problem4 created function for converting alphabets

--- src/problem4.js
+++ src/problem4.js
@@ -1,4 +1,22 @@
-function problem4(word) {}
+function problem4(word) {
+  return word
+    .split("")
+    .map((char) => (isAlphabet(char) ? convertAlpha(char) : char))
+    .join("");
+}
+
+function isLowerCase(char) {
+  return char.toUpperCase() !== char;
+}
+
+function convertAlpha(char) {
+  const average = ("A".charCodeAt(0) + "Z".charCodeAt(0)) / 2;
+
+  let converted = String.fromCharCode(
+    average - (char.toUpperCase().charCodeAt(0) - average)
+  );
+  return isLowerCase(char) ? converted.toLowerCase() : converted;
+}
 
 function isAlphabet(char) {
   return /[a-zA-Z]/.test(char);
