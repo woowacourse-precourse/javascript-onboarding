d5caf5a887789014d3015322812e58973048e7a2
jihun1233 <leejihun1233@gmail.com>
2022-10-27 20:04:34 +0900

    문자코드기준으로 반대로 바꿔주는 함수 구현
    - 이외 function키워드를 화살표함수와 섞어 사용한 오류 수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,13 +1,33 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+
+  for (let i = 0; i < word.length; i++) {
+    const code = word[i].charCodeAt()
+    answer += String.fromCharCode(revertCharCode(code))
+  }
 
   return answer;
 }
 
-function findCase = (code) => {
+const findCase = (code) => {
   if(65 <= code && code <= 90)return 'upperCase'
-  if(97 <= code && code <= 122)
+  if(97 <= code && code <= 122)return 'lowerCase'
   return 'notAlphabet'
+}
+
+const revertCharCode = (code) => {
+  const charCase = findCase(code)
+  if(charCase === 'notAlphabet')return code
+
+  let firstCode = 65
+  let lastCode = 90
+  
+  if(charCase === 'lowerCase'){
+    firstCode = 97
+    lastCode = 122
+  }
+
+  return firstCode + (lastCode - code)
 }
 
 module.exports = problem4;
