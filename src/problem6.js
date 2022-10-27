b2df19b3cc2152797180759ab1eb259d371f3449
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-27 15:22:44 +0900

    feat: 6번 문제 구현
    - 같은 이름에서 특정 단어가 반복될 수록 answer 배열에 중복되므로 Set을 이용
    - 객체를 이용해 해당 단어가 반복될 수록 value값을 1씩 증가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,25 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var answer = [];
+  let repeatTextObj = {};
+  forms.map((form) => {
+    const nickname = form[1];
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substr(i, 2);
+      if (testText in repeatTextObj) repeatTextObj[testText]++;
+      else {
+        repeatTextObj[testText] = 1;
+      }
+    }
+  });
+
+  forms.map((form) => {
+    const [email, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substr(i, 2);
+      if (repeatTextObj[testText] > 1) answer.push(email);
+    }
+  });
+  return [...new Set(answer)].sort();
 }
 
 module.exports = problem6;
