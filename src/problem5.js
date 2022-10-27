36a242030f2099437bf4d708a00fd72f87276d5b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-27 13:10:03 +0900

    feat: 각 화폐 단위별 필요 개수를 result에 담아 반환

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
-function problem5(money) {
-  var answer;
-  return answer;
+const problem5 = (money) => {
+  const result = [];
+  const moneyUnits = [50000, 10000, 5000, 1000, 5000, 100, 50, 10, 1];
+  moneyUnits.forEach((unit) => {
+    let cnt = 0;
+    if (money >= unit) {
+      cnt += Math.floor(money / unit);
+      money = money % unit;
+    }
+    result.push(cnt);
+  })
+  return result;
 }
 
 module.exports = problem5;
