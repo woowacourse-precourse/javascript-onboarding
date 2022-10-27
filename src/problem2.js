758aaa5509351980f836632638f2a50de8ee3068
vinivin153 <vinivin153@naver.com>
2022-10-27 19:14:55 +0900

    fix: 예외케이스 발견 후 새로 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,18 +1,26 @@
 function problem2(cryptogram) {
-  const answer = removeDuplication(cryptogram);
+  answer = removeDuplication([...cryptogram]);
   return answer;
 }
 
 function removeDuplication(cryptogram) {
-  const stack = [];
-  for (let i = 0; i < cryptogram.length; i++) {
-    if (!stack.length || cryptogram[i] !== stack[stack.length - 1]) {
-      stack.push(cryptogram[i]);
-    } else if (stack.length && cryptogram[i] === stack[stack.length - 1]) {
-      stack.pop();
+  let duplicatedCryptogram = getDuplicatedCryptogram(cryptogram);
+  if (cryptogram.join('') !== duplicatedCryptogram.join('')) {
+    return removeDuplication(duplicatedCryptogram);
+  }
+  return duplicatedCryptogram.join('');
+}
+
+function getDuplicatedCryptogram(cryptogram) {
+  let isDuplicateArray = [];
+  isDuplicateArray.fill(false);
+  for (let i = 0; i < cryptogram.length - 1; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      isDuplicateArray[i] = true;
+      isDuplicateArray[i + 1] = true;
     }
   }
-  return stack.join('');
+  return cryptogram.filter((value, idx) => value && !isDuplicateArray[idx]);
 }
 
 module.exports = problem2;
