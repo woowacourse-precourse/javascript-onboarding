83799916700526bed0517be6081249c35d1fa9e5
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 10:14:12 +0900

    feat: [문제1] pobi, crong의 최대 점수 구하기 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,26 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const getScores = ([firstPage, secondPage]) => {
+    return [
+      Number(
+        [...String(firstPage)].reduce((acc, cur) => Number(cur) * Number(acc))
+      ),
+      Number(
+        [...String(firstPage)].reduce((acc, cur) => Number(cur) + Number(acc))
+      ),
+      Number(
+        [...String(secondPage)].reduce((acc, cur) => Number(cur) * Number(acc))
+      ),
+      Number(
+        [...String(secondPage)].reduce((acc, cur) => Number(cur) + Number(acc))
+      ),
+    ];
+  };
+  const getMaxScore = (scores) => {
+    return scores.reduce((acc, cur) => Math.max(acc, cur));
+  };
+  getMaxScore(getScores(pobi)); //72
+  getMaxScore(getScores(crong)); //72
 }
 
-module.exports = problem1;
+problem1([97, 98], [197, 198]);
+// module.exports = problem1;
