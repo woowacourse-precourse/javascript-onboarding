c3115ab057621c74f3840e31e13c283699527224
Dean <yjj052800@gmail.com>
2022-10-27 13:10:36 +0900

    REFACTOR: [1번] 배열 생성, 정렬 기능 분리

--- src/problem1.js
+++ src/problem1.js
@@ -1,55 +1,38 @@
 function problem1(pobi, crong) {
+  if (!inputExecption(pobi, crong)) return -1;
   var answer;
   let pobiNumArr = [];
   let crongNumArr = [];
-  let pobiNumtoStr;
-  let crongNumtoStr;
-  let sum;
-  let multiply;
 
   for (let i = 0 ; i < 2 ; i++) {
-    pobiNumtoStr = pobi[i].toString();
-    crongNumtoStr = crong[i].toString();
-    sum = 0;
-    multiply =1;
-
-    for (let i = 0; i < pobiNumtoStr.length; i++) {
-      sum += parseInt(pobiNumtoStr[i]);
-      multiply *= parseInt(pobiNumtoStr[i]);
-      }
-    pobiNumArr.push(sum, multiply);
-    sum = 0;
-    multiply =1;
-
-    for (let i = 0; i < crongNumtoStr.length; i++) {
-      sum += parseInt(crongNumtoStr[i]);
-      multiply *= parseInt(crongNumtoStr[i]);
-      }
-    crongNumArr.push(sum, multiply);
+    let pobiNumtoStr = pobi[i].toString();
+    let crongNumtoStr = crong[i].toString();
+    pobiNumArr = pobiNumArr.concat(mkNumArr(pobiNumtoStr));
+    crongNumArr = crongNumArr.concat(mkNumArr(crongNumtoStr));
   }
-  //sort()를 이용해서 배열을 내림차순으로 정리
-  pobiNumArr.sort((a, b) => {
-    return b - a;
-  });
+  arrSort(pobiNumArr);
+  arrSort(crongNumArr);
 
-  crongNumArr.sort((a, b) => {
-    return b - a;
-  });
-
-  if (pobiNumArr[0] > crongNumArr[0]) {
-    answer = 1;
-  } else if (crongNumArr[0] > pobiNumArr[0]) {
-    answer = 2;
-  } else if (crongNumArr[0] === pobiNumArr[0]) {
-    answer = 0;
-  }
-
+  if (pobiNumArr[0] > crongNumArr[0]) answer = 1;
+  else if (crongNumArr[0] > pobiNumArr[0]) answer = 2; 
+  else if (crongNumArr[0] === pobiNumArr[0]) answer = 0;
   return answer;
 }
 
-module.exports = problem1;
+function mkNumArr(numtoStr) {
+  let sum = 0;
+  let multiply = 1;
+  let numArr = [];
+  
+  for (let i = 0; i < numtoStr.length; i++) {
+    sum += parseInt(numtoStr[i]);
+    multiply *= parseInt(numtoStr[i]);
+  }
+  numArr.push(sum, multiply);
+  return numArr;
+}
 
-const inputExecption = (arr1, arr2) => {
+function inputExecption(arr1, arr2) {
   let inputCheck = true;
   if (arr1[0] <= 1 || arr2[1] >= 400) inputCheck = false;
   else if (arr1[0] * arr2[0] % 2 !== 1) inputCheck = false;
@@ -57,3 +40,9 @@ const inputExecption = (arr1, arr2) => {
   else if (arr1.length != 2 || arr2.length != 2) inputCheck = false;
   return inputCheck;
 }
+
+function arrSort(fullNumArr) {
+  fullNumArr.sort((a, b) => {return b - a;});
+  return fullNumArr;
+}
+module.exports = problem1;
