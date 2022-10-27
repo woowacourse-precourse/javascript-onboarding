7a272bcea5c73049afa2870bf15c85391fea21ab
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-10-27 16:29:58 +0900

    요구조건 다시 이해한 후 problem2 재구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,4 +1,31 @@
 function problem2(cryptogram) {
+  var answer = cryptogram;
+  let is_deleted; // 중복된 문자를 삭제했는지 확인하는 변수
+
+  do {
+    // 중복된 문자를 '모두' 제거 후 다시 중복된 문자가 있는지 확인한다.
+    is_deleted = false;
+    let temp = '';
+
+    for (let i = 0; i < answer.length; i++) {
+      const cur = answer[i];
+      if (cur == answer[i + 1]) {
+        is_deleted = true;
+        while (cur == answer[i + 1]) {
+          i++;
+        }
+      } else temp += answer[i];
+    }
+    answer = temp;
+  } while (is_deleted);
+
+  return answer;
+}
+
+function problem2_wrong(cryptogram) {
+  // 앞에서부터 중복된 문자를 제거 후 바로 합친 뒤에 중복된 문자가 있는지 다시 확인하는 방법
+  // 문제의 요구 사항은 중복된 문자를 '모두' 제거한 뒤에 합친 뒤 다시 확인해야함
+  // 문제를 잘못 이해하여 푼 풀이지만 나중에 참고를 위해 남겨둠
   var answer = [];
 
   let cur = 0;
