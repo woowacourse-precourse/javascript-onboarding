d373a6a02391f7ff8fb25c2a2e28ce02cc75432c
GuHyeonWoo <ghw9174@gmail.com>
2022-10-27 19:04:51 +0900

    P4/소문자, 대문자 변환 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,19 @@
 // 3. 소문자를 반대로 변환하는 기능
 // 4. 대문자를 반대로 변환하는 기능
 
+// 알파벳 대문자를 반대로 변환
+function upperConversion(code){
+  const conversion_code = (122 - code) + 97;
+  return String.fromCharCode(conversion_code);
+}
+
+// 알파벳 소문자를 반대로 변환
+function lowerConversion(code){
+  // 코드를 반대의 문자 코드로 변환하여 문자로 변환 후 반환
+  const conversion_code = (90 - code) + 65;
+  return String.fromCharCode(conversion_code);
+}
+
 // 아키스 코드를 통해 문자 상태를 반환하는 함수
 function checkWordState(code){
   // 대문자, 소문자, 그 외 상태를 판단하여 반환
@@ -22,7 +35,18 @@ function circuitWord(word){
     const char_code = word.charCodeAt(i);
     // 아스키 코드로 어떤 문자인지 판단
     const word_state = checkWordState(char_code);
-    // 문자에 따라 반대로 변환하는 부분
+    // 변환할 문자를 담을 변수
+    let conversion_word;
+
+    // 문자에 따라 반대로 변환
+    if(word_state === 'capital'){
+      conversion_word = upperConversion(char_code);
+    }
+    else if(word_state === 'small'){
+      conversion_word = lowerConversion(char_code);
+    }
+
+    result_word += conversion_word;
   }
   return result_word;
 }
