71fe6d2fa770de7f713c120277294d2bbf5c1b3e
Suyoung <jsy8492@gmail.com>
2022-10-27 10:52:36 +0900

    refactor(problem2): 반례 고려하여 중복 제거 함수 로직 수정
    - 3개 이상의 중복된 문자 제거 가능하도록 로직 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,19 @@
 function removeDuplicates(str) {
   for (let i = 0; i < str.length - 1; i++) {
     if (str[i] === str[i + 1]) {
-      str = str.slice(0, i) + str.slice(i + 2);
+      let sliceIdx = i + 2;
+      for (let j = 2; i + j < str.length; j++) {
+        if (str[i] === str[i + j]) {
+          sliceIdx++;
+        } else {
+          break;
+        }
+      }
 
-      if (i > 1) {
-        i -= 2;
+      str = str.slice(0, i) + str.slice(sliceIdx);
+
+      if (i > 2) {
+        i -= sliceIdx - i;
       } else {
         i = -1;
       }
