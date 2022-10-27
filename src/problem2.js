ea2248a46cb496fdae4b5e7cda2b6a6dae7e2f43
wonkyum-kim <dodo12120@naver.com>
2022-10-27 21:22:05 +0900

    feat: add solve_cryptogram

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,27 @@
+function solve_cryptogram(str) {
+  let stack = [];
+  for (let i = 0; i < str.length; ++i) {
+    // if the stack is empty, push the charater.
+    if (stack.length === 0) {
+      stack.push(str[i]);
+      continue;
+    }
+    // if the character is same with the top of stack, delete the top of the stack.
+    if (stack[stack.length - 1] === str[i]) {
+      stack.pop();
+      continue;
+    }
+    stack.push(str[i]);
+  }
+  return stack;
+}
+
 function problem2(cryptogram) {
-  var answer;
+  const stack = solve_cryptogram(cryptogram);
+  let answer = "";
+  for (let char of stack) {
+    answer += char;
+  }
   return answer;
 }
 
