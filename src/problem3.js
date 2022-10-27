c55408c5bdffd83cd85f25c9ccb9f860d5a09c67
An Sol <apdlvmf486@gmail.com>
2022-10-27 09:16:58 +0900

    3, 6, 9의 개수 세기 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,7 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  for (let num = 1; num < number + 1; num++){
+    const numToString = num.toString().match(/[369]{1}/g);
+  }
 }
 
 module.exports = problem3;
