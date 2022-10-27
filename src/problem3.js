52a76d8530e2c2a63205c9a210f8550e32bc349f
1 <wrasf175@naver.com>
2022-10-27 19:17:11 +0900

    feat : 3번 과제 기능 구현 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCount = 0;
+
+  let getClapCount = function (number) {
+    const splitDigit = [...('' + number)];
+    let countedDigit = splitDigit.filter(
+      (num) => num === '3' || num === '6' || num === '9'
+    ).length;
+    clapCount = countedDigit;
+  };
+
+  for (let i = 1; i <= number; i++) {
+    getClapCount(i);
+  }
+
+  return clapCount;
 }
+
 
 module.exports = problem3;
