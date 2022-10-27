2bb47150dcf3ea3e5a425246f09762cf13931a44
c0c0pang <coc1789@naver.com>
2022-10-28 00:20:41 +0900

    문제 2번 3개이상 중복 문자열 제거 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,54 @@
 function problem2(cryptogram) {
-  let arr = [...cryptogram];
+  const arr = [...cryptogram];
   let i=0;
-  while(i!==arr.length){
-  if(arr[i]===arr[i+1]){
-    arr.splice(i,2);    
-      i=0;
-  }
-  else{
+  let cntbox  = [];
+  let check = false;
+  // let current = 0;
+  //순차적으로 탐색후 중복 발견하면 계속 탐색하고 끝나면 탐색한 부분까지 제거후 다시 원점으로 복귀를 반복
+  while(1){
+    if((arr[i]===arr[i+1]) && ((arr.length)!==i)){
+      cntbox.push(i);
+      check=true;
       i++;
+      continue;
+    }
+    else if((arr[i]!==arr[i+1]) && ((arr.length)!==i) && !check){
+      i++;
+      continue;
+    }
+    else if((arr[i]!==arr[i+1]) && ((arr.length)!==i) && check ){
+      arr.splice(cntbox[0],cntbox.length+1);
+      i=0;
+      cntbox = []
+      check = false;
+      continue;
+    }
+    
+    if(arr.length === i)
+      break;
   }
-}
-return arr.join('');
+  return arr.join('');
 }
 
 module.exports = problem2;
+/**
+    while(i!==arr.length){
+      if(arr[i]===arr[i+1]){
+        arr.splice(i,2);    
+          i=0;
+      }
+      else{
+          i++;
+      }
+    }
+
+        if(arr[i]===arr[i+1]){
+        cnt++;
+        i++;
+    }
+    else{
+        arr.splice(i-cnt,cnt);
+        i=0;
+        cnt = 1;
+    }
+ */
