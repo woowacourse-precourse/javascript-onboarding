8f83d192d0b9c6e3a516bcab9c335f2bbbd94c38
subin <newdeas100@gmail.com>
2022-10-28 00:44:53 +0900

    feat(problem2): checkInputErr 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,25 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  try{
+    if(checkInputErr(cryptogram)){
+      throw new Error("Invalid Input Error");
+    }
+
+    
+  }catch(err){
+    console.log(err);
+  }
+}
+
+function checkInputErr(cryptogram){
+  const vaildType = (input) => typeof input === 'string';
+  const validLength = (input) => 1 <= input.length && input.length <=1000;
+  const validInput = (input) => [...input].every(char => char === char.toLowerCase()); 
+
+  if(!vaildType(cryptogram)) return true;
+  else if(!validLength(cryptogram)) return true;
+  else if(!validInput(cryptogram)) return true;
+  else return false;
+
 }
 
 module.exports = problem2;
