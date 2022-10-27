43b7a62492f7e6014c07204ed3846187a35116a5
BB-choi <choibobae0809@gmail.com>
2022-10-27 18:18:39 +0900

    feat: 문제6 - 연속글자 저장시에 카운트 정보 저장

--- src/problem6.js
+++ src/problem6.js
@@ -13,19 +13,40 @@ function extractWord({ nickname, characterLength = 2 }) {
 function problem6(forms) {
   var answer;
 
-  /* {"단어" => {count: 0, users : [[email, 닉네임], [email,닉네임]]}} */
+  /* {"단어" => {count: 0, users : [{email, 닉네임}, {email,닉네임}]}} */
   const nicknameWordMap = new Map();
 
   for (const [email, nickname] of forms) {
     const wordSet = extractWord({ nickname });
-    wordSet.forEach((word) =>
-      nicknameWordMap.set(word, { count: 0, users: [] })
-    );
+
+    for (const word of wordSet) {
+      if (nicknameWordMap.has(word)) {
+        const { count, users } = nicknameWordMap.get(word);
+
+        nicknameWordMap.set(word, {
+          count: count + 1,
+          users: [...users, { email, nickname }],
+        });
+        continue;
+      }
+
+      nicknameWordMap.set(word, { count: 1, users: [{ email, nickname }] });
+    }
   }
 
   console.log(nicknameWordMap);
 
   return answer;
 }
+
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
 
 module.exports = problem6;
