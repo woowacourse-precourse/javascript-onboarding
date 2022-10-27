7eb9675b8fa96b8206b2b05a75097053d63b3290
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-28 04:09:42 +0900

    feat: P3 구현할 것

--- src/problem3.js
+++ src/problem3.js
@@ -4,3 +4,15 @@ function problem3(number) {
 }
 
 module.exports = problem3;
+
+/*
+ 1. number이 주어지면
+ 2. 해당 숫자까지
+ 3. 숫자가 3, 6, 9를 포함하는 개수 count.
+
+ 3, 6, 9를 몇 개 가지고 있는지 판별하는 함수.
+ count 추가해서 더하면 될 듯.
+ P : 숫자 커지면 for문 너무 많이 돌아서 비효율적.
+ S : 일단 구현하고 시간이 오래걸리면 공식으로 구현
+
+ */
