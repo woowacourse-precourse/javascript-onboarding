70c8f18ab6047310ba8e0e388d95a000ef053079
su-lim <sujin.0xlim@gmail.com>
2022-10-27 15:15:37 +0900

    feat: prob2 - Create removeDuplication func

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,43 @@
+function removeDuplication(cryptogram){
+  let decodingString = [];
+
+  for(let i=0; i<cryptogram.length; i++){
+
+    if(i === cryptogram.length-1){ // 맨 끝이면
+      decodingString.push(cryptogram[i]);
+      break;
+    }
+
+    if(cryptogram[i] !== cryptogram[i+1]){
+      decodingString.push(cryptogram[i]);
+      continue;
+    }
+
+    if(cryptogram[i] === cryptogram[i+1]){
+      next = i+1;
+      // 달라질 때 까지 반복
+      while(cryptogram[i] === cryptogram[next]){
+        next = next + 1;
+        if(next >= cryptogram.length){ // 끝을 넘기면 끝까지 반복되는 문자열이므로 종료
+          i = cryptogram.length;
+          break;
+        }
+      }
+      i = next-1;
+    }
+  }
+  return decodingString.join('');
+}
 function problem2(cryptogram) {
   var answer;
+  let afterString = cryptogram;
+
+  do{
+    beforeString = afterString;
+    afterString = removeDuplication(beforeString)
+  } while (beforeString !== afterString) // 달라지지 않을 때 까지 반복
+  
+  answer = afterString; 
   return answer;
 }
 
