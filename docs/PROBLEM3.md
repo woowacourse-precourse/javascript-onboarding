7f52bca21c6bfc63b05f65c179d5d118f5d8ee15
이예나 <yenalee@MacBook-Pro.local>
2022-10-27 22:10:41 +0900

    Feat: Change input to String

--- docs/PROBLEM3.md
+++ docs/PROBLEM3.md
@@ -4,6 +4,10 @@
 
 숫자 number가 매개변수로 주어질 때, 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 return 하도록 solution 메서드를 완성하라.
 
+## 기능 목록
+Feat: Change input num to string
+Feat: Count if string contains [369]
+
 ### 제한사항
 
 - number는 1 이상 10,000 이하인 자연수이다.
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer=0;
+
+  for(let i=1;i<=number;i++){
+    var num = String(i);
+    
+  }
+  console.log(answer);
   return answer;
 }
 
 module.exports = problem3;
+number = 33;
+problem3(number);
