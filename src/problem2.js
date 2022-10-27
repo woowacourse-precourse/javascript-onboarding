710506fcc75e6b9721812ee24b10095ca2bacf5a
froggy1014 <clcl6084@gmail.com>
2022-10-28 00:44:35 +0900

    feat[2]: 중복 문자 인덱스 추출 기능 메소드 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const cryptoArray = cryptogram.split('');
+    
+  const DetectedIndexs = getDupIndexs(cryptoArray)
+  
+  return DetectedIndexs;  
 }
 
-module.exports = problem2;
+function getDupIndexs(cryptoArray) {
+  const dupIndexs = [];
+  let isDuplicate = false;
+  for(let i = 0; i < cryptoArray.length; i++) {
+    if( cryptoArray[i] === cryptoArray[i+1]) {
+      isDuplicate = true;
+      dupIndexs.push(i);
+    } else if(isDuplicate) {
+      isDuplicate = false;
+      dupIndexs.push(i);
+    }
+  }
+  return dupIndexs;
+}
+
+console.log(problem2('browoanoommnaon'))
+
+// module.exports = problem2;
