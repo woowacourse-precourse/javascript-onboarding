71d435e352b5a175fa0fe2eb2a30cf40e41ac769
curomame <curomame@naver.com>
2022-10-27 23:28:42 +0900

    ✅ check : problem 3 해답 코드 작성 완료 1027.2328

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,19 @@
 function problem3(number) {
+
+  // 특이사항
+
+  //!1. 숫자는 1이상 10,000 이하인 자연수.
+
+  function CHECK_NUMBER_BETWEEN_ONE_TO_TENTHOUSAND (number){
+    if(number < 1 || number > 10000){
+      return false;
+    } return true;
+  }
+
+  if(CHECK_NUMBER_BETWEEN_ONE_TO_TENTHOUSAND(number) === false){
+    return "number의 숫자가 1미만 혹은 10,000을 초과합니다."
+  }
+
   // 필요함수
 
   //#1. 주어진 숫자에 3,6,9가 몇 번 포함되는지 반환하는 함수
@@ -14,18 +29,6 @@ function problem3(number) {
     }
     return count;
   }
-
-  // 특이사항
-
-  //!1. 숫자는 1이상 10,000 이하인 자연수.
-
-  function CHECK_NUMBER_BETWEEN_ONE_TO_TENTHOUSAND (number){
-    if(number < 1 || number > 10000){
-      return false;
-    } return true;
-  }
-
-  
 
   let result = 0;
   for (let j = 1; j <= number; j++) {
