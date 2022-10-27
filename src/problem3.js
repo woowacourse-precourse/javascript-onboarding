44872192cdf040fb656f3f8576784a912996c255
sangpok <48979587+sangpok@users.noreply.github.com>
2022-10-27 10:58:36 +0900

    Solve the problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    let includedNumbers = [...Array(number + 1).keys()]
+        .map((item) => item.toString())
+        .filter((item) => item.includes('3') || item.includes('6') || item.includes('9'));
+
+    if (!includedNumbers.length) return 0;
+
+    return includedNumbers
+        .map((item) => item.split('3').length + item.split('6').length + item.split('9').length - 3)
+        .reduce((a, b) => a + b);
 }
 
 module.exports = problem3;
