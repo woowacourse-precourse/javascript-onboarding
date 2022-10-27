6483d4a4023c95c78f1b27f07a6fe1247110f022
Subin Jo <sbjo1202@naver.com>
2022-10-28 02:25:30 +0900

    feat: 초기화(닉네임만 담은 배열 생성)

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
+  let nicknameAnswer = [];
+  let answer = [];
+
+  // deque처럼 동작할 nickname 배열
+  let nickname = [];
+
+  // 닉네임만 담기
+  for (form of forms) {
+    nickname.push(form[1]);
+  }
+  console.log(nickname); // [ '제이엠', '제이슨', '워니', '엠제이', '이제엠' ]
+
+  
+
+  
   return answer;
 }
 
-module.exports = problem6;
+forms = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"], ]
+problem6(forms);
+
+
+// module.exports = problem6;
