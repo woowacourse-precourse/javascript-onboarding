fac24cdd1ea40880164316e217d634e90963f792
hwnim5324 <hwnim5324@naver.com>
2022-10-27 14:00:43 +0900

    4번 기능2 완료.

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,6 @@
 function problem4(word) {
   var answer;
   return answer;
-  //2. 아스키코드 78~90일 때, 90과 차이를 i라고 할 때, 25-(i*2)만큼 더해 리턴.
   //3. 위 두 조건을 만족하는 함수 작성.
   //4. 문자열을 매핑하며 함수 결과에 따라 문자열 변환.
 }
@@ -10,7 +9,9 @@ function frog(alph){
   const alph = alph.charCodeAt(0);
   if(alph>=65 && alph<=77){
     return String.fromCharCode(65+(25-(alph-65)*2));
-  }
+  }else if(alph>=78 && alph<=90){
+    return String.fromCharCode(90-(25-(90-alph)*2));
+  }else return alph;
 }
 
 module.exports = problem4;
