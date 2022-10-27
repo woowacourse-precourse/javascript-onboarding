b7fe8011c09db25ab4d1e11cc3ff2ff2bd1f84e6
sery <yunsel2@gmail.com>
2022-10-27 18:21:47 +0900

    feat: Add exeption handling feature to PROBLEM3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
   let answer = 0;
 
+  if (number < 1 || number > 10000) {
+    alert('1 이상 10,000 이하의 숫자를 입력해주세요');
+    return;
+  }
+
   for (let i = 1; i < number + 1; i++) {
     const num = i.toString().split('');
 
