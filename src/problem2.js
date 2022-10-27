cf213e634bbe79a31c1d9fb476c131094093e23f
semnil5202 <semnil5202@gmail.com>
2022-10-27 23:30:40 +0900

    문제2번. 중복 문자 제거 및 기능 구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
   var answer;
+  const cryptogramArr = cryptogram.split("");
+  const onlyCrytogramArr = deleteNearSameChar(cryptogramArr);
+  answer = onlyCrytogramArr.join("");
   return answer;
+}
+
+function deleteNearSameChar(cryptogramArr) {
+  for (let i = 0; i < cryptogramArr.length; i++) {
+    if (cryptogramArr[i] === cryptogramArr[i + 1]) {
+      cryptogramArr.splice(i, 2);
+      deleteNearSameChar(cryptogramArr);
+      break;
+    }
+  }
+  return cryptogramArr;
 }
 
 module.exports = problem2;
