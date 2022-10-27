06676118b59252f3a2b07153c64a19958a6b6e57
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 14:09:21 +0900

    refactor: [문제2] 암호 해독함수 스택으로 재구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,25 +1,33 @@
 function problem2(cryptogram) {
   if (cryptogram.length === 0) return cryptogram;
-  const tempt = [...cryptogram];
-  let startIndex = 0;
-  let count = 0;
-  [...cryptogram].slice(1).forEach((char, index) => {
-    if (tempt[startIndex] === char) {
-      count = count + 1;
-    } else if (count > 0) {
-      tempt.splice(startIndex, count + 1);
-      count = 0;
-    } else {
-      startIndex = startIndex + 1;
-      count = 0;
+  const decrypted = [];
+  let pointer = '';
+  let isContinious = false;
+  [...cryptogram].forEach((character, index) => {
+    if (!isContinious) {
+      pointer = decrypted.pop() || '';
     }
-    if (index === cryptogram.length - 2 && count > 0) {
-      tempt.splice(startIndex);
+
+    if (pointer === character) {
+      console.log(pointer, character);
+      isContinious = true;
+    } else {
+      if (!isContinious) {
+        decrypted.push(pointer);
+      }
+      console.log(decrypted);
+      decrypted.push(character);
+      isContinious = false;
     }
   });
-  const temptString = tempt.join('');
-  if (temptString === cryptogram) return cryptogram;
-  return problem2(temptString);
-}
 
-module.exports = problem2;
+  const decryptogram = decrypted.join('');
+
+  if (decryptogram === cryptogram) {
+    return cryptogram;
+  }
+
+  return problem2(decryptogram);
+}
+problem2('good'); //?
+// module.exports = problem2;
