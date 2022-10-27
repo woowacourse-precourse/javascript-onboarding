05735211caba1e369e9195a6d03dfca3dc3be58d
xownsdl0 <xownsdl0@naver.com>
2022-10-27 10:52:22 +0900

    왼쪽 페이지 더하기/곱하기 값 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,27 @@
 function problem1(pobi, crong) {
+  pobiSum = 0;
+  pobiMultple = 1;
+  pobiMax = 0;
+
+  crongSum = 0;
+  crongMultple = 1;
+  crongMax = 0;
+
+  //left
+  pobiLeftNum = pobi[0];
+  pNum = String(pobiLeftNum);
+  for (let i = 0; i < pNum.length; i++) {
+    pobiSum += parseInt(pNum[i]);
+    pobiMultple *= parseInt(pNum[i]);
+  }
+
+  crongLeftNum = crong[0];
+  cNum = String(crongLeftNum);
+  for (let i = 0; i < cNum.length; i++) {
+    crongSum += parseInt(cNum[i]);
+    crongMultple *= parseInt(cNum[i]);
+  }
+
   var answer;
   return answer;
 }
