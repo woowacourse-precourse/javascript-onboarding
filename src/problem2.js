c0d8e9a7a43b6d8b6706459b09aa32223fd033bf
vjvl95 <vjvl95@naver.com>
2022-10-27 18:44:58 +0900

    solve problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+	cryptogram = [...cryptogram];
+	var i = 0;
+	while (cryptogram.length != 0) {
+		if (cryptogram[i] === cryptogram[i + 1]) {
+			cryptogram.splice(i, 2);
+			i = 0;
+			continue;
+		}
+		i++;
 
+		if (i === cryptogram.length) {
+			break;
+		}
+	}
+	cryptogram = cryptogram.join('');
+	return cryptogram;
+}
 module.exports = problem2;
