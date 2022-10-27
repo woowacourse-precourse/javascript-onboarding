6319d2a3d184a2f08eb1c7b9e98fc8e2468ac81e
yeongi <cbg5255@gmail.com>
2022-10-27 11:56:41 +0900

    문제 6 기능 목록

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,11 @@ function problem6(forms) {
   return answer;
 }
 
+// 1. 닉네임을 이메일과 분리하는 기능
+function nickSplit(id) {}
+// 2. 같은 글자가 연속적으로 포함하는 닉네임을 검사하는 기능
+function checkTwoChar(nickName) {}
+// 3. 이메일 붙이는 기능
+function emailConcat(id) {}
+
 module.exports = problem6;
