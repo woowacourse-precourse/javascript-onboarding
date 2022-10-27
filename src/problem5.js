269da071c7b8e43273c7d7e6ae467f9d3ae4e79c
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 22:49:31 +0900

    refactor(problem5): 함수 이름 수정

--- src/problem5.js
+++ src/problem5.js
@@ -2,19 +2,19 @@ function problem5(money) {
   let result = [];
   const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
-  const cnt_of_cash = (money, index) => {
+  const push_cnt_of_cash = (money, index) => {
     if (index == 8) {
       result.push(money);
       return;
     }
     if (money < cash[index]) {
       result.push(0);
-      return cnt_of_cash(money, index + 1);
+      return push_cnt_of_cash(money, index + 1);
     }
     result.push(parseInt(money / cash[index]));
-    return cnt_of_cash(money % cash[index], index + 1);
+    return push_cnt_of_cash(money % cash[index], index + 1);
   };
-  cnt_of_cash(money, 0);
+  push_cnt_of_cash(money, 0);
   return result;
 }
 
