0a83f8a60725e8d1ad55103cd4bbc15545f1a16e
hwangsungjae <edx2fire@likelion.org>
2022-10-27 17:58:27 +0900

    [온보딩 - 06] 돈 계산 문제 솔루션 함수 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]; 
+  answer = units.map(unit => {
+    const count = Math.floor(money / unit);
+    money %= unit;
+    return count;
+  });
   return answer;
 }
 
