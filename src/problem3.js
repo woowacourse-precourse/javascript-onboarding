ef018762e5b8fd9df58be422be7a52832bc75966
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-27 14:46:41 +0900

    feat: 각 자릿수의 박수 횟수를 return하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -2,4 +2,11 @@ function problem3(number) {}
 
 const canClap = numberChar => numberChar === '3' || numberChar === '6' || numberChar === '9'
 
+const getClapCount = number => {
+	const numberCharArr = String(number).split('')
+	let clapCount = numberCharArr.filter(canClap).length
+
+	return clapCount
+}
+
 module.exports = problem3
