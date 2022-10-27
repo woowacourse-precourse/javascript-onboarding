14e49d8c0ffa12b06f9fe5091b09c8e2c9e70e17
eunsung <esk114477@gmail.com>
2022-10-27 15:52:01 +0900

    문제 3번 완성

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+
+  let clap = 0;
+  let i = 1;
+  const reg365 = /3|6|9/g;
+
+  // (i.toString().match(reg365) || []) -> 값이 있거나 배일이 초기화되면 변수에 할당/ 그렇지 않으면 변수를 빈 배열로 초기화
+  for (i; i<=number; i++){
+    clap += (i.toString().match(reg365) || []).length;
+  }
+  return clap;
 }
 
 module.exports = problem3;
