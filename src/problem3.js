2d81adcb897b39a8649051ff4b18aa708309806d
HJeong1200 <hjeong1200@gmail.com>
2022-10-27 20:25:07 +0900

    03-pseudo-code

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
   return answer;
 }
 
 module.exports = problem3;
+
+/**
+ * 1) 1 ~ number까지 반복
+ * 2) 해당 숫자의 각 자릿수에 대해 3, 6, 9가 있으면 answer++
+ */
