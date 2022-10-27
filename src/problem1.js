f303792fe6ea92144b9e05f51e4b608feaaba242
jinseo <ghkdwlstj99@gmail.com>
2022-10-27 14:49:12 +0900

    기능 구현 목록 수정 및 페이지 번호의 각 자리 숫자를 모두 더하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,21 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+ 
+  return 
 }
+
+function pageSum(num){
+  if (num >= 100){
+  return num / 100 + ((num % 100) / 10) + num % 10 
+ }
+ return (num / 10) + (num % 10);
+}
+
+function pageMul(num){
+  if (num >= 100){
+      return (num/100) * ((num%100)/10) * (num % 10);
+  }
+  return (num / 10) * (num % 10);
+}
+
 
 module.exports = problem1;
