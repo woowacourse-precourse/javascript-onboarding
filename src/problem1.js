090446204cbb6b0ea641f434077aaaad3dfedbf8
hae-on <solbi2004@naver.com>
2022-10-27 22:30:39 +0900

    [문제1] 제한 사항 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,35 @@
+const checkPageRange = (page1, page2) => {
+  if (page1[0] >= 1 && page1[1] <= 400 && page2[0] >= 1 && page2[1] <= 400)
+    return true;
+  return false;
+};
+
+const checkPageGap = (page1, page2) => {
+  if (page1[1] - page1[0] === 1 && page2[1] - page2[0] === 1) return true;
+  return false;
+};
+
+const checkOddEven = (page1, page2) => {
+  if (
+    page1[0] % 2 === 1 &&
+    page2[0] % 2 === 1 &&
+    page1[1] % 2 === 0 &&
+    page2[1] % 2 === 0
+  )
+    return true;
+  return false;
+};
+
+const checkLength = (page1, page2) => {
+  if (page1.length === 2 && page2.length === 2) return true;
+  return false;
+};
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!checkPageRange(pobi, crong)) return -1;
+  if (!checkPageGap(pobi, crong)) return -1;
+  if (!checkOddEven(pobi, crong)) return -1;
+  if (!checkLength(pobi, crong)) return -1;
 }
 
 console.log(problem1([97, 98], [197, 198]));
