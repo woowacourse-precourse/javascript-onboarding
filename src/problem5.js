1b4290da6fc72edf69e0879267d47b3ebacf904b
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 21:33:21 +0900

    feat(Problem5): 예외 처리

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,12 @@
 function problem5(money) {
   let answer = new Array(9).fill(0);
-  
-  return answer;
+  if (money < 1 || money > 1000000){
+    return 'money 가능 범위를 벗어났습니다';
+  }
+  else{
+    
+    return answer;
+  }
 }
 
 module.exports = problem5;
