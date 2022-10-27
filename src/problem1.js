cb03401dec62b143d15d30ea1e12ef059b53aeeb
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-10-27 10:53:46 +0900

    pobi 점수 계산 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,27 @@
 function problem1(pobi, crong) {
   var answer;
+
+  //pobi 점수 계산
+  let pobiSum = 0;
+  let pobiMulti = 0;
+  for (var page of pobi){
+    let tmpSum = 0;
+    let tmpMulti = 1;
+    const newArray = Array.from(String(page));
+    for (var i of newArray){
+      tmpSum += Number(i);
+      pobiMulti *= Number(i);
+    }
+    if(pobiSum < tmpSum){
+      pobiSum = tmpSum;
+    }
+    if(pobiMulti < tmpMulti){
+      pobiMulti = tmpMulti;
+    }
+  }
+  
+  let pobiMax = (pobiSum > pobiMulti) ? pobiSum : pobiMulti;
+
   return answer;
 }
 
