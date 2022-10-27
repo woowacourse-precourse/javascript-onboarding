eac7350275b95dea23a83f1f25c9574352dbb5a2
JH <vvaa277@gmail.com>
2022-10-27 13:36:04 +0900

    Docs : 기능목록 추가 Feat : 4 ~ 6기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,18 @@
 function problem3(number) {
   let clap = 0;
+  let total = 0;
 
-  const num = number.toString().split("");
+  for (let i = 0; i < number + 1; i++) {
+    const inum = i.toString().split("");
 
-  for (el of num) {
-    if (el % 3 === 0 && el > 0) {
-      clap += 1;
+    for (iel of inum) {
+      if (iel % 3 === 0 && iel > 0) {
+        clap += 1;
+      }
     }
   }
-
+  total += clap;
+  return total;
 }
 
 module.exports = problem3;
