234d5e4b4c268a0cbbc1df0bf92a5fd0fc2ed2a9
Rki0 <pky00823@gmail.com>
2022-10-27 10:44:47 +0900

    style: prob1. 주석 전부 제거
    주석없이도 코드를 이해할 수 있다고 판단하여
    주석을 전부 삭제했습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -40,32 +40,24 @@ function eachSideCalc(eachSideNum) {
 }
 
 function pageNumCalc(who) {
-  // 왼쪽 페이지 연산
   const leftMax = eachSideCalc(who[0]);
 
-  // 오른쪽 페이지 연산
   const rightMax = eachSideCalc(who[1]);
 
-  // 왼쪽 가장 큰 수와 오른쪽 가장 큰 수 비교
   const finalMax = Math.max(leftMax, rightMax);
 
   return finalMax;
 }
 
 function exceptionCheck(who) {
-  // 페이지가 1 - 400 사이의 값이 아닌 경우.
   if (who[0] < 1 || who[0] > 400 || who[1] < 1 || who[1] > 400) {
     return true;
   }
 
-  // 페이지가 연속적이지 않은 경우.
-  // 왼쪽이 오른쪽보다 작으므로 오른쪽 - 왼쪽 === 1 이어야 올바른 페이지이다.
   if (who[1] - who[0] !== 1) {
     return true;
   }
 
-  // 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수이므로
-  // [1,2], [399,400]이 시작과 끝 면이다.
   if ((who[0] === 1 && who[1] === 2) || (who[0] === 399 && who[1] === 400)) {
     return true;
   }
@@ -88,7 +80,6 @@ function whoIsWinner(pobi, crong) {
 }
 
 function problem1(pobi, crong) {
-  // 한 명의 입력이라도 예외 상황이라면 -1 반환
   if (exceptionCheck(pobi) || exceptionCheck(crong)) {
     return -1;
   }
