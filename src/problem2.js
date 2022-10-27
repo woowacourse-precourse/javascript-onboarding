9c571a88683ca3b3bcabb20358df4d2bcdb1e8c8
MJ <mj632900@gmail.com>
2022-10-27 15:44:11 +0900

    feat : 중복이 끝나는 인덱스 반환하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,11 @@
+function getEndIndex(str, startIndex) {
+    for (let i = startIndex; i < str.length; i++) {
+        if (str[i] !== str[i + 1]) {
+            return i;
+        }
+    }
+}
+
 function getStartIndex(str) {
     for (let i = 0; i < str.length; i++) {
         if (str[i] === str[i + 1]) {
