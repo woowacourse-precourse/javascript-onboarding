2d87509538ed9245c0fe71529892dbdd9b6ea14f
GuHyeonWoo <ghw9174@gmail.com>
2022-10-27 22:11:09 +0900

    결과 값의 리스트를 만드는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,8 +2,21 @@
 // 1. 결과 값의 리스트를 만드는 기능
 // 2. 화폐 단위 별 몫과 나머지를 구하는 기능
 
+// 결과 값의 리스트를 만드는 함수
+function solution(money){
+  // 결과 값을 넣을 리스트
+  let result_list = [];
+  // 계산 후 나머지를 저장할 변수
+  let temp_money = money;
+
+  // 화폐 별 계산 할 부분
+
+  return result_list;
+}
+
 function problem5(money) {
-  var answer;
+  // 결과 값을 반환받아 할당
+  const answer = solution(money);
   return answer;
 }
 
