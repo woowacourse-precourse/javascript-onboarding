cb7ba9e85fdac22e03a9a4b79b4fad5be1fbba49
Suyeon <msjgr2801@gmail.com>
2022-10-27 11:25:43 +0900

    feat: solve problem1 & problem2

--- src/problem1.js
+++ src/problem1.js
@@ -1,13 +1,40 @@
 // 0. 예외 처리
-//  0.1 [ ] (2 ~ 398) 사이 숫자일 것
+//  0.1 [x] (2 ~ 398) 사이 숫자일 것
 
 // 1. 로직
-//  1.1 [ ] pobi와 crong이 가질 수 있는 가장 큰 수 구하기
-//  1.2 [ ] pobi와 crong의 숫자를 비교하기
-function problem1(pobi, crong) {
-  let answer = null;
+//  1.1 [x] pobi와 crong이 가질 수 있는 가장 큰 수 구하기
+//  1.2 [x] pobi와 crong의 숫자를 비교하기
 
-  return answer;
+const plus = (nums) => {
+  return nums.reduce((prev, curr) => {
+    Number(prev) + Number(curr);
+  }, 0);
+}
+
+const multiple = (nums) => {
+  return nums.reduce((prev, curr) => {
+    Number(prev) * Number(curr);
+  }, 1);
+}
+
+const calMax = (pobi, crong) => {
+  [pobi[0], pobi[1]] = [String(pobi[0]).split(""), String(pobi[1]).split("")];
+  [crong[0], crong[1]] = [String(crong[0]).split(""), String(crong[1]).split("")];
+
+  const pobiMax = Math.max(plus(pobi[0]), plus(pobi[1]), multiple(pobi[0]), multiple(pobi[1]));
+  const crongMax = Math.max(plus(crong[0]), plus(crong[1]), multiple(crong[0]), multiple(crong[1]));
+
+  return [pobiMax, crongMax];
+}
+
+function problem1(pobi, crong) {
+  // 예외 사항
+  if (pobi[1] <= 2 || pobi[0] >= 399) return -1;
+  if (crong[1] <= 2 || crong[0] >= 388) return -1;
+
+  const [maxPobi, maxCrong] = calMax(pobi, crong);
+
+  return Math.max(maxPobi, maxCrong);
 }
 
 module.exports = problem1;
--- src/problem2.js
+++ src/problem2.js
@@ -1,19 +1,51 @@
 // 0. 예외처리
-//  0.1 [ ] 문자열이 비었거나 하나의 문자만 있는 경우
+//  0.1 [x] 문자열이 비었거나 하나의 문자만 있는 경우
 
 // 1. 문자열 제거 로직
-//  1.1 [ ] stack 사용
-//  1.2 [ ] 문자열을 앞에서부터 하나씩 answer에 더한다
-//    1.2.1. answer의 마지막 문자열과 cryptogram의 현재 문자가 같으면 answer에서 pop
+//  1.1 [x] 문자열을 앞에서부터 (중복되지 않는 문자열만) 하나씩 answer에 더한다
 
 // 2. 1번 로직 반복 기준
-//  2.1 [ ] 연속으로 같은 문자열이 존재하지 않아야함 
-//  2.2 [ ] flag로 연속으로 같은 문자열이 있는지 판별 
-//  2.3 [ ] 연속되는 문자열이 있으면 true, 아니면 false
-function problem2(cryptogram) {
-  let answer = [];
+//  2.1 [x] flag로 연속으로 같은 문자열이 있는지 판별 
+//  2.2 [x] 연속되는 문자열이 있으면 true, 아니면 false
 
-  return answer.join("");
+const removeCharcater = (res, cryptogram) => {
+  for (let i=0; i<cryptogram.length; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      i += 2;
+    } else {
+      res += cryptogram[i];
+    }
+  }
+
+  return res;
+}
+
+const checkOverlap = (cryptogram) => {
+  if (cryptogram.length === 0 || cryptogram.length === 1) {
+    return false;
+  }
+
+  for (let i=0; i<cryptogram.length; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) return true;
+  }
+
+  return false;
+}
+
+function problem2(cryptogram) {
+  let answer = '';
+
+  if (cryptogram.length === 0) return answer;
+  if (cryptogram.length === 1) return cryptogram;
+
+  let flag = true;
+
+  while (flag) {
+    answer = removeCharcater(answer, cryptogram);
+    flag = checkOverlap(answer);
+  }
+
+  return answer;
 }
 
 module.exports = problem2;
