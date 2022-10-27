dae19503cfb14d2325b9ea27baf5f054cc8f8a88
jiyujin <yujin1679@naver.com>
2022-10-27 23:12:16 +0900

    docs(pro2): 기능 요구 사항 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,17 @@
+// 기능 요구 사항
+// - [] 연속하는 중복 문자들을 (연속하는 중복 문자가 없을때까지)삭제한다.
+// - ex1) "browoanoommnaon"
+//   - 1. "browoanoommnaon"
+//   - 2. "browoannaon"
+//   - 3. "browoaaon"
+//   - 4. "browoon"
+//   - 5. "brown"
+// - ex2) "zyelleyz"
+//   - 1. "zyeeyz"
+//   - 2. "zyyz"
+//   - 3. "zz"
+//   - 4. ""
+
 function problem2(cryptogram) {
   var answer;
   return answer;
