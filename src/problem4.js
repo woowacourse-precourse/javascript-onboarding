ea3093fd50035f83626ae43ec148ef04ccaad71c
Circlewee <pnj1118@naver.com>
2022-10-27 20:51:11 +0900

    [#4]feat: convertCharacter 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,32 @@
+/**
+ * @param {string} character
+ * @returns {string}
+ */
+function convertCharacter(character) {
+  const charCode = character.charCodeAt(0);
+
+  if (charCode >= 65 && charCode <= 90) {
+    return String.fromCharCode(65 - charCode + 90);
+  }
+
+  if (charCode >= 97 && charCode <= 122) {
+    return String.fromCharCode(97 - charCode + 122);
+  }
+
+  return character;
+}
+
+/**
+ * @param {string} word
+ * @returns {string}
+ */
 function problem4(word) {
-  var answer;
+  let answer = '';
+
+  for (let i = 0; i < word.length; i++) {
+    answer += convertCharacter(word[i]);
+  }
+
   return answer;
 }
 
