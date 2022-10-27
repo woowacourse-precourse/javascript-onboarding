d7f8fefae67f6a65ae1f82a51a679c86e0306eb6
wonkyung <mwk0725@gmail.com>
2022-10-27 22:59:01 +0900

    feat: [기능 1] 알파벳 변환 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,30 @@
 function problem4(word) {
   const alphabet = Array.from({length: 26}, (v, i) => String.fromCharCode(i + 65));
   let text = word.split("");
+  let transformText = [];
 
   // 반대 알파벳 인덱스 번호 찾기
   const findIdx = text => {
     return 25 - alphabet.indexOf(text);
   };
+
+  // 알파벳 변환
+  for (el of text) {
+    let upper = el.toUpperCase();
+    let num = findIdx(upper);
+
+    if (el === " ") {
+      el = " ";
+    } else if (el !== upper) {
+      el = ("" + alphabet[num]).toLowerCase();
+    } else if (el === upper) {
+      el = ("" + alphabet[num]).toUpperCase();
+    }
+    transformText.push(el);
+  }
+
+  let answer = transformText.join("");
+  return answer;
 }
 
 module.exports = problem4;
