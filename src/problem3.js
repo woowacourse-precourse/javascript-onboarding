2245682bf72272a3874d6b36eb917c7e7698129d
Leejin-Yang <ejin1996@gmail.com>
2022-10-27 14:33:26 +0900

    feat(problem3): 숫자별 손뼉 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,24 @@
+const isToClap = (digit) => digit === '3' || digit === '6' || digit === '9';
+
+function getClapNumber(number) {
+  const digits = number.toString().split('');
+  const clapNumbers = digits.filter((digit) => isToClap(digit));
+
+  return clapNumbers.length;
+}
+
 function problem3(number) {
   var answer;
   return answer;
 }
 
 module.exports = problem3;
+
+function test() {
+  console.log(getClapNumber(33));
+  console.log(getClapNumber(13));
+  console.log(getClapNumber(3319));
+  console.log(getClapNumber(4));
+}
+
+test();
