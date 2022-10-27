b162a65ed7e7d1281206d4e49ea2e6e404cdeb3e
seohee0112 <ghdtj7@naver.com>
2022-10-27 10:17:01 +0900

    feat : problem3 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
+function isClapping(number) {
+  let cnt = 0;
+  while (number > 0) {
+    const checkNum = number % 10;
+    if (checkNum == 3 || checkNum == 6 || checkNum == 9) cnt++;
+    number = Math.floor(number / 10);
+  }
+  return cnt;
+}
 function problem3(number) {
-  var answer;
-  return answer;
+  let result = 0;
+  for (let cnt = 1; cnt <= number; cnt++) result += isClapping(cnt);
+  return result;
 }
 
 module.exports = problem3;
