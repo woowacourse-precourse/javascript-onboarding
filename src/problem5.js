aa2f857d820769a36d8c5050d6e2c182741d19e4
kyw0716 <kyw0716@naver.com>
2022-10-27 13:50:35 +0900

    feat: problem5 구현 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let sortedMoneyArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < sortedMoneyArray.length; i++) {
+    let count = 0;
+    while (money - sortedMoneyArray[i] >= 0) {
+      money -= sortedMoneyArray[i];
+      count += 1;
+    }
+    answer.push(count);
+  }
+
   return answer;
 }
 
