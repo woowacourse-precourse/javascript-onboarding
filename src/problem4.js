13a971933b5fdc41d04b6beb581c9faa464e8cc9
dev-redo <mis05041@naver.com>
2022-10-27 15:06:04 +0900

    [solved 4번] 문자열 파싱 풀이

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,34 @@
+const UPPERCASE = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const LOWERCASE = 'abcdefghijklmnopqrstuvwxyz';
+
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = '';
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    result += convertLetter(letter);
+  }
+
+  return result;
+}
+
+function convertLetter(letter) {
+  if (letter === ' ') return letter;
+
+  const isLetterUpper = checkLetterIsUpperCase(letter);
+
+  if (isLetterUpper) {
+    const idx = UPPERCASE.split('').indexOf(letter);
+    return UPPERCASE[25 - idx];
+  }
+
+  const idx = LOWERCASE.split('').indexOf(letter);
+  return LOWERCASE[25 - idx];
+}
+
+function checkLetterIsUpperCase(letter) {
+  if (letter === letter.toUpperCase()) return true;
+  return false;
 }
 
 module.exports = problem4;
