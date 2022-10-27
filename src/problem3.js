ffada6441e83da149ec0f3e658978f004c6aff7f
yeonsuBeak <soothbugger@gmail.com>
2022-10-27 14:33:39 +0900

    feat(problem3): 3, 6, 9가 포함되었을 때 카운드 하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,30 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  const result = countClapNumber(number);
+
+  return result;
+}
+
+function countClapNumber(lastNumber) {
+  const FIRST_NUMBER = 1;
+  let countClap = 0;
+
+  for (
+    let currentNumber = FIRST_NUMBER;
+    currentNumber <= lastNumber;
+    currentNumber++
+  ) {
+    let currentString = currentNumber.toString();
+
+    if (
+      currentString.includes("3") ||
+      currentString.includes("6") ||
+      currentString.includes("9")
+    ) {
+      countClap++;
+    }
+  }
+
+  return countClap;
 }
 
 module.exports = problem3;
