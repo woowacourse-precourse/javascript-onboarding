7e8b07c76d8498694ddb73683d0d3abc6f4c6367
Siby1lA <psb4644@gmail.com>
2022-10-27 23:14:59 +0900

    feat: 더하고 곱해서 더 큰 값을 리턴하는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,12 @@
+function cal(page) {
+  const pageNum = [...page.toString()].map((str) => parseInt(str));
+  const add = pageNum.reduce((acc, cur) => acc + cur, 0);
+  const multi = pageNum.reduce((acc, cur) => acc * cur, 1);
+  return Math.max(add, multi);
+}
 function getMaxScore(pages) {
   const [leftPage, rightPage] = pages;
-  return Math.max();
+  return Math.max(cal(leftPage), cal(rightPage));
 }
 function problem1(pobi, crong) {
   const pobiMaxScore = getMaxScore(pobi);
