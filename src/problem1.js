af9e8148ae87d30a4929092b1c0cd3b640ac8dcd
jiyujin <yujin1679@naver.com>
2022-10-27 21:54:35 +0900

    feat(pro1): 예외사항 return 하는 isException 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -5,14 +5,33 @@
 // - [o] 점수를 비교해 높은 사람이 게임의 승자가 된다.
 //   - [o] 포비가 이기면 1, 크롱이 이기면 2, 무승부면 0, 그 외 -1을 return 한다.
 // 예외사항
-// - [] 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수이다.
-// - [] 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다. (덮개-0, 401-덮개)
-// - [] 오른쪽 페이지 수에서 왼쪽 페이지 수를 빼면 그 값은 1이다.
+// - [o] 오른쪽 페이지 수에서 왼쪽 페이지 수를 빼면 그 값은 1이다.
+// - [o] 왼쪽 페이지는 홀수, 오른쪽 페이지는 짝수이다.
+// - [o] 시작 면이나 마지막 면이 나오도록 책을 펼치지 않는다. (덮개-0, 401-덮개)
 
 function problem1(pobi, crong) {
-  const pobiMax = getMaxValue(pobi);
-  const crongMax = getMaxValue(crong);
-  console.log(whoWinner(pobiMax, crongMax));
+  if (isException(pobi) && isException(crong)) {
+    const pobiMax = getMaxValue(pobi);
+    const crongMax = getMaxValue(crong);
+    return whoWinner(pobiMax, crongMax);
+  } else {
+    return -1;
+  }
+}
+
+function isException(name) {
+  if (name[1] - name[0] != 1) {
+    return 0;
+  }
+  if ((name[0] % 2 == 0) || (name[1] % 2 != 0)) {
+    return 0;
+  }
+  if (name[0] < 1 || name[1] > 400) {
+    return 0;
+  }
+  else {
+    return 1;
+  }
 }
 
 function getMaxValue(name) {
@@ -50,5 +69,5 @@ function whoWinner(pobi, crong) {
 
 const pobi = [131, 132];
 const crong = [211, 212];
-problem1(pobi, crong);
+console.log(problem1(pobi, crong));
 module.exports = problem1;
