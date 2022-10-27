6ba8e1d4a26e8efd15cfe37194679dce38d69586
minseok1109 <park485201@naver.com>
2022-10-27 11:52:09 +0900

    feat: number까지 배열을 만드는 함수 #기능목록-1

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
+//number까지 배열을 만드는 함수 -기능목록 1
+const range = (from, to, step = 1) =>
+  [...Array(Math.floor((to - from) / step) + 1)].map((_, i) => from + i * step);
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let arrNumber = Array.from({ length: number + 1 }, (x, i) => i);
 }
 
+console.log(range(1, 13, 5));
 module.exports = problem3;
