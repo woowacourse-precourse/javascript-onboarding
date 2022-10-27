98b50d1b4b7693d81d49ff073bee8cd869ceebf5
moonelysian <dandyblack@naver.com>
2022-10-27 13:20:48 +0900

    add: 사용자의 친구관계 가져오기

--- src/problem7.js
+++ src/problem7.js
@@ -11,14 +11,15 @@ function getRelationships (friends) {
   let relationships = {}
   friends.map(friend => {
     const [f1, f2] = friend; 
-    mapRelations(f1);
-    mapRelations(f2);
+    mapRelations(f1, f2, relationships);
+    mapRelations(f2, f1, relationships);
   })
   return relationships;
 }
 
 function problem7(user, friends, visitors) {
   const relationships = getRelationships(friends);
+  const userFriends = relationships[user];
 }
 
 module.exports = problem7;
