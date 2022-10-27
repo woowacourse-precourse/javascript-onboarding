c9f7fd7aa16474272cea893de830ecca069e853e
wisdomin121@naver.com <wisdomin121@naver.com>
2022-10-27 14:45:37 +0900

    p1) 페이지 번호 split하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,16 @@
+// 기능 목록
+// 1. 페이지 번호 split
+// 2. 1번에서 나온 숫자 더하기 & 곱하기
+// 3. 2번에서 나온 숫자 중, 최대값 찾기
+// 4. 포비와 크롱 중, 승자 구하기
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  for(let i=0; i<2; i++) {
+    pobi[i] = pobi[i].split('').map(Number);
+    crong[i] = crong[i].split('').map(Number);
+  }
 }
+
+console.log(problem1([97, 98], [197, 198]))
 
 module.exports = problem1;
