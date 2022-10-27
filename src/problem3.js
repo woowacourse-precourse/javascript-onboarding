69bed5f03d177273337ba5030b3b898932cf8668
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-28 06:19:07 +0900

    [feat] problem3 - 기능(1) 숫자에 3,6,9가 몇 개 들어가는지 판단하는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,14 @@ function problem3(number) {
   return answer;
 }
 
+function check(num) {
+  let cnt = 0;
+  let num_string = num.toString();
+  for (let i = 0; i < num_string.length; i++) {
+    if (num_string[i] === "3" || num_string[i] === "6" || num_string[i] == "9") cnt++;
+  }
+
+  return cnt;
+}
+
 module.exports = problem3;
