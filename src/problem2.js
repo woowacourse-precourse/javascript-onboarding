730ed3f5fc57313b87f54b2db17fff2581ffc0c0
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-27 21:12:26 +0900

    feat(문제2): 연속된 중복 문자 제거하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const answerStack = [cryptogram[0]]; 
+  let duplicateChar = '';   
+  for (let i = 1; i < cryptogram.length; i += 1) {
+    if (duplicateChar === cryptogram[i]) continue;
+    if (answerStack[answerStack.length - 1] === cryptogram[i]) duplicateChar = answerStack.pop();
+    else if (answerStack[answerStack.length - 1] !== cryptogram[i]) answerStack.push(cryptogram[i]);
+  }
+  return answerStack.join('');
 }
 
 module.exports = problem2;
