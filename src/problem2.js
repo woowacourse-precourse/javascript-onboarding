420f3b4a5c8ceb582ddd61e06be15c428f01f66e
Dean <yjj052800@gmail.com>
2022-10-27 23:01:50 +0900

    FEAT: [2번] 중복되는 문자 제거 초안

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,19 @@
 function problem2(cryptogram) {
   var answer;
-  let resultofC = '';
+  let resultofC = ``;
   let storeC = cryptogram[0];
 
   for (let i of cryptogram) {
     console.log(i);
-    if (i !== storeC) {
-      resultofC += storeC;
-    }
+    if (i == storeC && resultofC.length >= 2) {
+      resultofC.slice(0, resultofC.length - 1);    
+      continue;     
+    } 
+      resultofC += i;
+      storeC = i;
+
+    
   }
-  resultofC += storeC;
-  console.log(resultofC);
    answer = resultofC
 
 
