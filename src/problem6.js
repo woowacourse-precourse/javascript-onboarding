aea667518a85516abfd0be00b9b6eddbd09359b2
Adult96 <djxor007@gmail.com>
2022-10-27 12:47:01 +0900

    feat: problem6 - 두글자의 닉네임을 닉네임 배열에 검색하여 2개 이상이 검색되면 findName배열 변수에 추가

--- src/problem6.js
+++ src/problem6.js
@@ -9,6 +9,15 @@ function problem6(forms) {
       parserName.push(nameArr[i][j] + nameArr[i][j + 1]);
     }
   }
+  //두글자의 닉네임을 닉네임 배열에 검색하여 2개 이상이 검색되면 findName배열에 추가한다
+  let findName = [];
+  const join = nameArr.join();
+  for (let i = 0; i < parserName.length; i++) {
+    const regexp = new RegExp(parserName[i], 'g');
+    if (join.match(regexp).length >= 2) {
+      findName.push(parserName[i]);
+    }
+  }
 }
 
 module.exports = problem6;
