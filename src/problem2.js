8097686438d44a13c572ee3340af3fcbcb47b0bf
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-28 03:11:31 +0900

    feat: checkLoop 추가 및 구현완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,32 @@
-function problem2(cryptogram) {}
+function problem2(cryptogram) {
+  let resultData = removeDuplicated(cryptogram);
+  while (checkLoop(resultData)) {
+    resultData = removeDuplicated(resultData);
+  }
+
+  return resultData;
+}
+
+module.exports = problem2;
+
+const checkLoop = (resultData) => {
+  let resultValue;
+  if (resultData.length === 0) {
+    return false;
+  } else if (resultData.length !== removeDuplicated(resultData).length) {
+    resultValue = true;
+  }
+
+  return resultValue;
+};
 
 const removeDuplicated = (text) => {
   const textArray = text.toString().split("");
   const parsedArray = parseDuplicate(textArray);
-  const resultData = parsedArray.join("");
+  const resultArray = parsedArray.join("");
 
-  return resultData;
+  return resultArray;
 };
-
-module.exports = problem2;
 
 const parseDuplicate = (textArray) => {
   const resultArray = [...textArray];
@@ -37,5 +55,8 @@ const parseDuplicate = (textArray) => {
   1. reduce용 배열 생성.
   2. prev === cur인 경우 해당 배열에 빈 스트링 넣어주기.
   3. join return
-  4. 중복 없을 때까지 돌려야함.
+  4. 중복 없을 때까지 돌려야함. 
+  reduce 내부에서 체크하거나, 중복단어 제거한 string의 길이 비교.
+
+
   */
