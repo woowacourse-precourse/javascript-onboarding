505f21af4919b620f84709c054c6bbb1b368bf90
moseung <tmdah900@naver.com>
2022-10-27 17:50:26 +0900

    ✨ problem1 기능목록 제작, 목록1 구현 problem1에서의 기능목록을 마크다운으로 구성했고 목록1을 구현했습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,24 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function compare(scoreArr) {
+  let big = 0;
+  for (i = 0; i < scoreArr.length; i++) {
+    String(scoreArr[i])
+      .split("")
+      .reduce((pv, cv) => Number(pv) + Number(cv)) > big
+      ? (big = String(scoreArr[i])
+          .split("")
+          .reduce((pv, cv) => Number(pv) + Number(cv)))
+      : (big = big);
+    String(scoreArr[i])
+      .split("")
+      .reduce((pv, cv) => Number(pv) * Number(cv)) > big
+      ? (big = String(scoreArr[i])
+          .split("")
+          .reduce((pv, cv) => Number(pv) * Number(cv)))
+      : (big = big);
+  }
+  return big;
 }
+
+function problem1(pobi, crong) {}
 
 module.exports = problem1;
