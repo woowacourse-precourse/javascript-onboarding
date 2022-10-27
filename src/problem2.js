3f7c0505212bf53d2a1853682e3fadc44a4902bc
yeonsuBeak <soothbugger@gmail.com>
2022-10-27 13:05:19 +0900

    feat: 연속하는 중복 문자를 삭제하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,30 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const removedCyptogram = removeDuplicates(cryptogram);
+  return removedCyptogram;
+
+  function removeDuplicates(word) {
+    const wordLength = word.length;
+    let removedResult = "";
+    let duplicateAlphabet = "";
+
+    for (let wordIndex = 0; wordIndex < wordLength; wordIndex++) {
+      if (word[wordIndex] === word[wordIndex + 1]) {
+        duplicateAlphabet = word[wordIndex];
+        wordIndex++;
+        continue;
+      }
+
+      if (duplicateAlphabet[wordIndex] === duplicateAlphabet) {
+        duplicateAlphabet = "";
+        wordIndex++;
+        continue;
+      }
+
+      removedResult += word[wordIndex];
+    }
+
+    return removedResult;
+  }
 }
 
 module.exports = problem2;
