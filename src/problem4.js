e05cab68fa5d40763cc941cd8ea92f9ff043eed1
moonelysian <dandyblack@naver.com>
2022-10-27 10:27:06 +0900

    add: 알파벳인지 확인, 알파벳 대문자인지 확인

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
+function isAlphabet(str) {
+  return /[A-Za-z]/.test(str);
+}
+
+function isUpperCase(str) {
+  return /[A-Z]/.test(str);
+}
+
 function problem4(word) {
   const alpha = Array.from(Array(26)).map((e, i) => i + 65);
-  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  const upperAlphabet = alpha.map((x) => String.fromCharCode(x));
+  const words = [...word]
 }
 
 module.exports = problem4;
