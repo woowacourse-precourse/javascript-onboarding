db33534a29cdc72c01e5effb5a176a3b3a634b0d
jungsmins1 <jungsmins00@gmail.com>
2022-10-28 01:38:34 +0900

    teat: 문자열을 반복하면서 연속하는 중복 문자를 찾고 삭제하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = [];
+
+  //문자열을 반복하면서 연속하는 중복 문자를 찾고 삭제하는 기능
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (arr[arr.length - 1] !== cryptogram[i]) {
+      arr.push(cryptogram[i])
+    } else if (arr[arr.length - 1] === cryptogram[i]) {
+      arr.pop()
+    }
+  }
+  return arr.join("")
 }
 
 module.exports = problem2;
