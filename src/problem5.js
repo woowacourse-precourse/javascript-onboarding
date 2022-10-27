0104e9415d03a6a8d9db7717751286a9538557b0
An Sol <apdlvmf486@gmail.com>
2022-10-27 14:23:55 +0900

    5번: 결과 배열, 화폐 단위 배열, money값을 담는 다른 변수 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let tempMoney = money;
+  let result = [];
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  return result;
 }
 
 module.exports = problem5;
