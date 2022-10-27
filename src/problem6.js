3431acd98bc03da6296abce527b815a93dc41923
olo90632951 <olo90632951@gmail.com>
2022-10-27 17:26:43 +0900

    feat: 사용자의 연속된 닉네임이 중복됐는지 검사하는 기능 추가 [Problem6-1]

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,36 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  return getEmail(forms);
 }
+
+const setUsers = (forms) => {
+  const usersHashMap = new Map();
+  forms.forEach((user) => {
+    usersHashMap.set(user[1], user[0]);
+  });
+  return usersHashMap;
+};
+
+const getName = (forms) => {
+  return forms.map((user) => user[1]);
+};
+
+const getEmail = (forms) => {
+  const usersHashMap = setUsers(forms);
+  return removeDuplicatedName(forms).map((name) => usersHashMap.get(name));
+};
+
+const removeDuplicatedName = (forms, result = []) => {
+  const usersNameHashMap = new Map();
+  getName(forms).forEach((userName) => {
+    for (let j = 0; j < userName.length - 1; j += 1) {
+      const dividedUserName = userName.slice(j, j + 2);
+      if (usersNameHashMap.has(dividedUserName)) {
+        result.push(userName, usersNameHashMap.get(dividedUserName));
+      }
+      usersNameHashMap.set(dividedUserName, userName);
+    }
+  });
+  return Array.from(new Set(result));
+};
 
 module.exports = problem6;
