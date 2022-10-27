528ac9ac6707aa6d0c5341280511387d41a3b499
lsy20140 <lsy20140@naver.com>
2022-10-27 16:57:58 +0900

    feat: [문제1] 페이지 번호 예외사항 조건 체크

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,38 @@
+function isValidInput(pageArr){
+  if(
+    checkArrLength(pageArr) 
+    && checkPageRange(pageArr) 
+    && checkPageNum(pageArr)
+    ){
+    return true;
+  }  
+}
+
+function checkArrLength(pageArr){
+  if(pageArr.length==2){
+    return true;
+  }
+}
+
+function checkPageRange(pageArr){
+  const [left, right] = pageArr;
+  if(left >= 1 && right <= 400){
+    return true;
+  }
+}
+
+function checkPageNum(pageArr){
+  const [left, right] = pageArr;
+  if(left + 1 == right && left%2 == 1 && right%2 == 0){
+    return true;
+  }
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if(!isValidInput(pobi) || !isValidInput(crong)){
+    answer = -1;
+  }
   return answer;
 }
 
