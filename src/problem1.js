4d74e73eaec780f165973751515164c7ce8844b5
dustmddus <dustmddus5@gmail.com>
2022-10-27 12:20:26 +0900

    Feat: [문제 1] 점수 구하는 calc 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function calc(page) {
+  const mapNum = (arg) => Number(arg);
+  let maxNum = 0;
+  for (let item of page) {
+    let numArr = Array.from(String(item), mapNum);
+    let arrSum = numArr.reduce((acc, cur) => {
+      return acc + cur;
+    }, 0);
+    let arrMulti = numArr.reduce((acc, cur) => {
+      return acc * cur;
+    }, 1);
+    maxNum = Math.max(arrMulti, arrSum, maxNum);
+  }
+  return maxNum;
 }
+
+function problem1(pobi, crong) {}
 
 module.exports = problem1;
