37342b269b181dd264fa83371967ee59bf38e572
js43o <js43o@naver.com>
2022-10-27 17:57:20 +0900

    Feat: Add problem2 code.

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,34 @@
+function deDuplication(str) {
+  let result = "";
+  let flag = false;
+
+  if (!str) return result;
+
+  for (let i = 0; i < str.length - 1; i++) {
+    if (str[i] != str[i + 1]) {
+      if (!flag) {
+        result += str[i];
+      }
+      flag = false;
+    } else {
+      flag = true;
+    }
+  }
+
+  if (!flag) result += str[str.length - 1];
+
+  return result;
+}
+
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram;
+
+  while (1) {
+    let deDuplicated = deDuplication(answer);
+    if (answer === deDuplicated) break;
+    answer = deDuplicated;
+  }
+
   return answer;
 }
 
