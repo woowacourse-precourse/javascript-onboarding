387966017b8d6ef99c2c0e5f31536024f895e383
Adult96 <djxor007@gmail.com>
2022-10-27 12:31:10 +0900

    feat: problem4 - [a-z],[A-Z]를 담은 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,62 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const arrLow = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+  const arrUp = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+
+  return words;
 }
 
 module.exports = problem4;
