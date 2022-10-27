7ad11b86f57fdbe92f9ed3d277eccb96ee2b0519
somin <smhs12@naver.com>
2022-10-27 12:59:22 +0900

    feat: problem2/ 중복 문자 처리

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = [];
+  let prev = "";
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (prev === cryptogram[i]) {
+      answer.pop();
+      prev = answer[answer.length - 1];
+    }
+  }
   return answer;
 }
 
