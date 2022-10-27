ebf42263421195537950daf067d153175848eb4e
sehwan <tpghks9245@gmail.com>
2022-10-28 08:11:11 +0900

    docs: 기능목록 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,5 @@
+
+
 //예외상황 판별(첫장 끝장, 페이지가 이상할때, pobi,crong의길이는 2, 왼쪽페이지는 홀수이다.)
 function exception(arr) {
   if (arr[0] == 1 || arr[1] == 400) return true;
@@ -9,7 +11,7 @@ function exception(arr) {
 }
 
 //각 자리수 나눠서 더하기
-function digitSum(...arr) {
+function addDigit(arr) {
   const firstArr = arr[0][0].toString().split("").map(x => parseInt(x));
   const firstNum = firstArr.reduce((a,b) => (a+b));
 
@@ -20,7 +22,7 @@ function digitSum(...arr) {
 }
 
 //각 자리수 나눠서 곱하기
-function digitMul(...arr) {
+function mulDigit(arr) {
   const firstArr = arr[0][0].toString().split("").map(x => parseInt(x));
   const firstNum = firstArr.reduce((a,b) => (a*b));
 
@@ -31,7 +33,7 @@ function digitMul(...arr) {
 }
 
 // 비교해서 결과값 도출
-function pointCompare(pobiPoint, crongPoint) {
+function comparePoint(pobiPoint, crongPoint) {
   if (pobiPoint == crongPoint) return 0;
   if (pobiPoint > crongPoint) return 1;
   if (pobiPoint < crongPoint) return 2;
@@ -40,16 +42,11 @@ function pointCompare(pobiPoint, crongPoint) {
 function problem1(pobi, crong) {
   if (exception(pobi) || exception(crong)) return -1;
 
-  const pobiPoint = Math.max(digitSum(pobi),digitMul(pobi));
-  const crongPoint = Math.max(digitSum(crong),digitMul(crong));
+  const pobiPoint = Math.max(addDigit(pobi),mulDigit(pobi));
+  const crongPoint = Math.max(addDigit(crong),mulDigit(crong));
 
-  return pointCompare(pobiPoint, crongPoint);
+  return comparePoint(pobiPoint, crongPoint);
 }
 
 module.exports = problem1;
 
-// 기능들
-// 1. 예외상황 판별(첫장 끝장, 페이지수가 이상할때)
-// 2. 각 자리수 나눠서 더하기
-// 3. 각 자리수 나눠서 곱하기
-// 4. 비교해서 결과값 도출
