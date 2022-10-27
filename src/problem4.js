5674e4e8d8959d47f093cea658c324cc4091bfa7
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-27 12:32:52 +0900

    feat: 청개구리가 반대로 말하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,7 +3,16 @@ const LOWER_CASE_END_CODE = 122;
 const UPPER_CASE_START_CODE = 65;
 const UPPER_CASE_END_CODE = 90;
 
-const badFrog = (word) => {}
+const badFrog = (word) => {
+  let charCode = word.charCodeAt(word);
+  if (charCode >= UPPER_CASE_START_CODE && charCode <= UPPER_CASE_END_CODE) {
+    charCode = UPPER_CASE_START_CODE + UPPER_CASE_END_CODE - charCode;
+  }
+  if (charCode >= LOWER_CASE_START_CODE && charCode <= LOWER_CASE_END_CODE) {
+    charCode = LOWER_CASE_START_CODE + LOWER_CASE_END_CODE - charCode;
+  }
+  return String.fromCharCode(charCode);
+}
 
 const problem4 = (word) => {}
 
