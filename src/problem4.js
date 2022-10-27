05ad4e55af7568906cbe23fd280dd1205469e603
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-10-28 08:57:21 +0900

    problem4 function pass

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
 function problem4(word) {
-  var answer;
+  var answer = solution(word);
   return answer;
+};
+function solution(word) {
+ let askiiNum = [], askiiStr = [];
+ for(let i = 0; i < word.length; i++) {
+  askiiNum.push(word.charCodeAt(i));
+ };
+ for(let i = 0; i < askiiNum.length; i++) {
+  if(askiiNum[i] === 32) {
+    askiiStr.push(String.fromCharCode(32));
+  }
+  if(askiiNum[i] <= 90 && askiiNum[i] !== 32) {
+    askiiStr.push(String.fromCharCode(90 - (askiiNum[i] - 65)));
+  } else if(askiiNum[i] <= 122 && askiiNum[i] !== 32) {
+    askiiStr.push(String.fromCharCode(122 - (askiiNum[i] - 97)));
+  }
+ };
+ return askiiStr.join("")
 }
-
 module.exports = problem4;
