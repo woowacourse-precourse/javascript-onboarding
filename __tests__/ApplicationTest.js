4378ff331b02d480865fc964047571c2dc3feb0f
heesungjang <heesungj7@gmail.com>
2022-10-27 16:58:36 +1000

    우테코 1주차 과제 6번

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -54,19 +54,19 @@ describe('problem5', () => {
   });
 });
 
-// describe("problem6", () => {
-//   test("case1", () => {
-//     expect(
-//       problem6([
-//         ["jm@email.com", "제이엠"],
-//         ["jason@email.com", "제이슨"],
-//         ["woniee@email.com", "워니"],
-//         ["mj@email.com", "엠제이"],
-//         ["nowm@email.com", "이제엠"],
-//       ])
-//     ).toEqual(["jason@email.com", "jm@email.com", "mj@email.com"]);
-//   });
-// });
+describe('problem6', () => {
+  test('case1', () => {
+    expect(
+      problem6([
+        ['jm@email.com', '제이엠'],
+        ['jason@email.com', '제이슨'],
+        ['woniee@email.com', '워니'],
+        ['mj@email.com', '엠제이'],
+        ['nowm@email.com', '이제엠'],
+      ])
+    ).toEqual(['jason@email.com', 'jm@email.com', 'mj@email.com']);
+  });
+});
 
 // describe("problem7", () => {
 //   test("case1", () => {
--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,39 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const result = new Set();
+
+  for (let i = 0; i < forms.length - 1; i++) {
+    const [currEmail, currNickname] = forms[i];
+    for (let j = i + 1; j < forms.length; j++) {
+      const [nextEmail, nextNickname] = forms[j];
+      const currSubStrings = getAllSubstrings(currNickname);
+      const nextSubStrings = getAllSubstrings(nextNickname);
+      const originalSubStringLen =
+        currSubStrings.length + nextSubStrings.length;
+
+      const set = new Set([...currSubStrings, ...nextSubStrings]);
+      const newSubStringLen = set.size;
+      if (originalSubStringLen !== newSubStringLen) {
+        result.add(currEmail);
+        result.add(nextEmail);
+      }
+    }
+  }
+
+  return [...result].sort((a, b) => a.localeCompare(b));
 }
+
+const getAllSubstrings = (str) => {
+  var subStrings = [];
+
+  for (let i = 0; i < str.length; i++) {
+    for (let j = i + 1; j < str.length + 1; j++) {
+      const subString = str.slice(i, j);
+      if (subString.length >= 2) {
+        subStrings.push(str.slice(i, j));
+      }
+    }
+  }
+  return subStrings;
+};
 
 module.exports = problem6;
