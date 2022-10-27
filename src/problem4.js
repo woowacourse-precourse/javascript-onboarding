dcd5432aeac1a26b8c2e9f0e42ef6bd55176265e
hyunjin <leehj0110@kakao.com>
2022-10-28 00:23:18 +0900

    feat: problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answer = ""
+
+  word.split("").forEach((char) => {
+    const asciiCode = char.charCodeAt(0)
+
+    if (asciiCode >= 97 && asciiCode <= 122) {
+      const index = asciiCode - 97
+      const newAsciiCode = 122 - index
+      answer += String.fromCharCode(newAsciiCode)
+    } else if (asciiCode >= 65 && asciiCode <= 90) {
+      const index = asciiCode - 65
+      const newAsciiCode = 90 - index
+      answer += String.fromCharCode(newAsciiCode)
+    } else {
+      answer += char
+    }
+  })
+
+  return answer
 }
 
-module.exports = problem4;
+module.exports = problem4
