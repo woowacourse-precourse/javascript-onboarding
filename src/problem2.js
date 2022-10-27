e7324d478f90743553ea3555dcf8e91658792aaa
moonhee0507 <sjmh0507@naver.com>
2022-10-27 21:14:36 +0900

    ✏️ 문제 2 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+    let str = cryptogram;
+    for (let i = 0; i < 1000; i++) {
+        for (let j = 0; j < str.length; j++) {
+            str = str.replace(/(.)\1+/g, '');
+        }
+    }
+    return str;
 }
 
 module.exports = problem2;
