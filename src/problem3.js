a345d3343d56566f4c7d9403067180d3bda34271
YoommY <seeun0792@gmail.com>
2022-10-27 20:10:50 +0900

    Fear : 3번 문제

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,24 @@
 function problem3(number) {
   var answer;
-  return answer;
+
+  var numberString = [];
+  var num = 0;
+  
+  for (var i = 1; i <= number; i++) {
+    numberString.push(i);
+  }
+
+  let nums = numberString.join("");
+  for (var i = 0; i < nums.length; i++) {
+    if (
+      nums[i] === "3" ||
+      nums[i] === '6' ||
+      nums[i] === '9'
+    ) {
+      num = num + 1;
+    }
+  }
+  return num;
 }
 
 module.exports = problem3;
