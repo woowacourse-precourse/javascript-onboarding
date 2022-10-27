d84ae87d4d21adfa8f3da4406c5ecaaaf5a91c1d
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-10-27 23:36:22 +0900

    problem3 function pass

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
+  var answer = solution(number);
   return answer;
-}
-
+};
+function solution(number) {
+  let changeArr = [];
+  let clap = [];
+  for(let i = 1; i <= number; i++) {
+    changeArr.push(i)
+  };
+  clap = changeArr.toString().split("").filter(item => item == 3 || item == 6 || item == 9);
+  let countClap = clap.length;
+  return countClap;
+};
 module.exports = problem3;
