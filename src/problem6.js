bce1551a8ef4587febcd649dd0ef3e2615c7557c
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-28 02:14:58 +0900

    [문제6] 같은 글자 반환 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+function getSameWord(crew1, crew2) {
+  let crew1Array = crew1.split("");
+  let crew2Array = crew2.split("");
+  let sameWord = "";
+
+  crew1Array.map((word1, index1) => {
+    crew2Array.map((word2, index2) => {
+      if (
+        word1 == word2 &&
+        crew1Array[index1 + 1] == crew2Array[index2 + 1] &&
+        crew1Array[index1 + 1] != undefined
+      ) {
+        sameWord = word1 + crew1Array[index1 + 1];
+      }
+    });
+  });
+
+  return sameWord;
 }
 
 module.exports = problem6;
