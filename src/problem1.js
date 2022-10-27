cefe1405c04cef16593ed9ba692f3af0c19b06a5
sulmeee <poorooria@naver.com>
2022-10-27 16:59:17 +0900

    feat: 각 자리 숫자 모두 곱하기

--- src/problem1.js
+++ src/problem1.js
@@ -11,6 +11,17 @@ function problem1(pobi, crong) {
     }
     return res;
   };
+
+  //각 자리 숫자 모두 곱하기
+  const multiplyAllPage = (num) => {
+    let res = 1;
+    while (num > 0) {
+      if (num < 0) break;
+      res *= Math.floor(num % 10);
+      num = Math.floor(num / 10);
+    }
+    return res;
+  };
 }
 
 module.exports = problem1;
