6ed18a9454bda2e46440bc0ac29246309ca1f602
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-27 14:59:22 +0900

    feat(Function): 현재 금액을 화폐 단위로 나눠 필요한 장수 반환, 0장의 화폐단위가 필요한 경우 건너뛰기 미구현(기능목록 1번 구현)

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,19 @@
 function problem5(money) {
-  var answer;
+  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let num = 0;
+  let count = 0;
+  let local = money;
+  for (let x of won) {
+    if (local % x === local) {
+      count++;
+      continue;
+    }
+    num = parseInt(local / x);
+    answer[count] = num;
+    local -= num * x;
+    count++;
+  }
   return answer;
 }
 
