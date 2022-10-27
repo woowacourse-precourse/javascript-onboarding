40a192c778bb9d384ef84d392ddfb8eb19567b34
chee9835 <huisu121@gmail.com>
2022-10-27 21:54:58 +0900

    feat: 영어 대소문자 구분해 규칙에 따라 문자 반환하기

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,20 @@
 function problem4(word) {
-  var answer;
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var lower = "abcdefghijklmnopqrstuvwxyz";
+  var answer = "";
+
+  for (var i = 0; i < word.length; i++) {
+    var text = word[i];
+    if (text === " ") {
+      answer += " ";
+      continue;
+    }
+
+    var textArr = upper.includes(text) ? upper : lower;
+    var index = 25 - textArr.indexOf(text);
+    answer += textArr[index];
+  }
+
   return answer;
 }
 
