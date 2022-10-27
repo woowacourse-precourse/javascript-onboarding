116ad733df66ae97997bb3321b1126c5f9ba442a
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-27 21:17:21 +0900

    refactor: problem5 매개변수의 값을 변경하는 안티패턴 제거

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,9 @@
 function problem5(money) {
+  let leftMoney = money;
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const result = units.map((unit) => {
-    const count = parseInt(money / unit);
-    money %= unit;
+    const count = parseInt(leftMoney / unit);
+    leftMoney %= unit;
     return count;
   });
   return result;
