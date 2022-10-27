562aae33caf97e9e5d566eba06e5a5cd683fdc49
imb96 <kimminje7267@naver.com>
2022-10-27 17:33:34 +0900

    Members except me and my friends are sorted in order of high scores.

--- src/problem7.js
+++ src/problem7.js
@@ -48,6 +48,18 @@ function problem7(user, friends, visitors) {
   for (const [name, rank] of rankMap) {
     answer.push([name, rank]);
   }
+
+  // 나와 내 친구들을 제외한 멤버들을 배열로 만들기
+  const meAndMyFriend = [...myFriend, user];
+  const result = [];
+  for (let i = 0; i < answer.length; i++) {
+    if (!meAndMyFriend.some((x) => x === answer[i][0])) {
+      result.push(answer[i]);
+    }
+  }
+
+  // 결과 점수가 큰 순으로 정렬하기
+  return result.sort((a, b) => b[1] - a[1]).map((x) => x[0]);
 }
 
 module.exports = problem7;
