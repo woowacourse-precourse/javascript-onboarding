b482f4349046e3096d0e3d2b5b98ffbf00f0c8ea
curomame <curomame@naver.com>
2022-10-27 23:22:41 +0900

    ➕ feature : 주어진 숫자에 3,6,9가 몇 번 포함되어있는지 반환하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,19 @@
 function problem3(number) {
-
   // 필요함수
 
   //#1. 주어진 숫자에 3,6,9가 몇 번 포함되는지 반환하는 함수
+
+  function RETURN_COUNT_INCLUDED_THREE_SIX_NINE(number) {
+    let count = 0;
+    const inputNumberString = String(number);
+    const numberList = inputNumberString.split("").map((i) => +i);
+    for (let i of numberList) {
+      if (i === 3 || i === 6 || i === 9) {
+        count += 1;
+      }
+    }
+    return count;
+  }
 
   // 특이사항
 
