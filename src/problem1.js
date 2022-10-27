8436b58d0e1f3f8390c53eb17f2061bd4e0afa3d
WonBeen Choi <zad1264@naver.com>
2022-10-27 21:44:36 +0900

    feat: 1번 문제 - 예외사항 처리 기능 추가
    두 페이지 쌍이 붙어있지 않은 경우 checkInvalidate에서 true를 반환, 프로그램을 종료

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,7 @@
  * -- 기능 목록 --
  * 1. 페이지의 배열인 유저를 받아 두 페이지 중 최고점을 반환하는 함수 - getUesrScore(user)
  * 2. 페이지를 받아 점수로 반환하는 함수 - calculatePageScore(page)
+ * 3. 두 페이지가 붙어있지 않은 예외사항을 처리하는 함수 - checkInvalidate(user[])
  */
 
 function getUserScore([leftPage, rightPage]) {
@@ -21,7 +22,13 @@ function getUserScore([leftPage, rightPage]) {
   return Math.max(leftPageScore, rightPageScore);
 }
 
+function checkInvalidate(userList) {
+  return userList.some(([left, right]) => left + 1 !== right);
+}
+
 function problem1(pobi, crong) {
+  if (checkInvalidate([pobi, crong])) return -1;
+
   let pobiScore = getUserScore(pobi);
   let crongScore = getUserScore(crong);
 
