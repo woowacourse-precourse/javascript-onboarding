a083e5a5a4bbc7dceb6d61b8b58dd99a0da0e376
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-10-27 12:31:49 +0900

    중복 문자 제거 기능 추가, 연속된 중복 문자 확인 기능 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,10 @@
 function problem2(cryptogram) {
     var answer;
+    let text = cryptogram.split("");
     // 1. 연속된 중복 문자가 있는 지 확인
     // - 중복된 문자가 있다면 true, 없다면 false 반환
     function checkText(text) {
-        let textArray = text.split("");
+        let textArray = text.slice();
         let result = false;
         let idx = 0;
         while (result === false && idx < textArray.length) {
@@ -17,6 +18,18 @@ function problem2(cryptogram) {
     }
 
     // 2. 있다면 모두 없어질 때까지 제거
+    while (checkText(text) === true) {
+        let textArray = [];
+        for (let i = 0; i < text.length; i++) {
+            if (i === 0) {
+                text[i] !== text[i + 1] ? textArray.push(text[i]) : null;
+            } else if (text[i] !== text[i + 1] && text[i] !== text[i - 1]) {
+                textArray.push(text[i]);
+            }
+        }
+        text = textArray;
+    }
+
     // 3. 없다면 반환
     return answer;
 }
