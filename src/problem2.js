4f54fd999a3d0784029b6a044415140f1aeaa3e0
Ryong-E <think8867@gmail.com>
2022-10-27 12:18:11 +0900

    refactor: 중복문자 삭제 함수 순차적으로 중복 문자 삭제/필요없는 구문 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -6,17 +6,22 @@ function problem2(cryptogram) {
 
 function decodeCryptogram(cryptogram){
   let decoded = cryptogram.split('');
-  for (let i = 1; i < decoded.length + 1; i++){
-    if (decoded[i-1] === decoded[i]){
-      decoded.splice(i-1,2);
-      i = 0;
+  const findDuplicated = /(.)\1+/;
+  while (findDuplicated.test(decoded.join(''))){
+    for (let i = 0; i < decoded.length + 1; i++){
+      if (decoded[i - 1] === decoded[i]){
+        let delCount = 2;
+        let addIndex = 1;
+        while(decoded[i - 1] === decoded[i + addIndex]){
+          delCount++;
+          addIndex++;
+        }
+        decoded.splice(i-1,delCount);
+        i--;
+      }
     }
   }
   decoded = decoded.join('');
-
-  if(decoded === []){
-    decoded = "";
-  }
 
   return decoded;
 }
