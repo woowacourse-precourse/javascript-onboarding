c9883426794e8cae9dab566ad42943dba5779243
khj0426 <706shin1728@naver.com>
2022-10-27 17:16:55 +0900

    :feat:문제 5번 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,23 @@
+function coin(won) {
+  this.won = parseInt(won);
+}
+
+function possible_coin(divide, coin) {
+  let result = parseInt(coin.won / divide);
+  coin.won = parseInt(coin.won % divide);
+  return result;
+}
+
 function problem5(money) {
-  var answer;
-  return answer;
+  var answer = 0;
+  let Coin = new coin(money);
+  let taget_ = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer_list = [];
+  for (let i = 0; i < taget_.length; i++) {
+    answer_list.push(possible_coin(taget_[i], Coin));
+  }
+
+  return answer_list;
 }
 
 module.exports = problem5;
