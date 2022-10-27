2912260ca5b7b0a09d75c7d698d4204a1d66dfd1
pangkyu <baesense@naver.com>
2022-10-27 17:05:33 +0900

    feat : 첫 배열 홀수, 두번째 배열 짝수 입력구현
    1. Math.floor(Math.random() * (398 - 2) + 2) 로 3부터 398까지만 출력된다
    2. checkOdd 함수로 짝수면 1을 더해 홀수로 만들어서 리턴하여 첫번째 배열에 입력한다. 단, 398이면 -1 처리를 해서 리턴하여 첫번째 배열에 입력한다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,7 @@
 function problem1(pobi, crong) {
-  const pageNumber = new Array(401);
-  pageNumber[0] = 0;
-  const pobiRecord = getRecord(pobi);
-  const crongRecord = getRecord(crong);
-
+  // if (checkArray(pobi) === true && checkArray(crong) === true) {
+  const pobiRecord = getMaxRecord(pobi);
+  const crongRecord = getMaxRecord(crong);
   if (pobiRecord > crongRecord) {
     return 1;
   } else if (pobiRecord == crongRecord) {
@@ -11,19 +9,41 @@ function problem1(pobi, crong) {
   } else if (pobiRecord < crongRecord) {
     return 2;
   }
+  // } else {
+  //   return -1;
+  // }
+}
+function getMaxRecord(user) {
+  console.log(user);
 }
 
-function getRecord(user) {}
+function checkOdd(user) {
+  if (user % 2 !== 0) {
+    return user;
+  } else {
+    if (user == 398) {
+      return user - 1;
+    }
+    return user + 1;
+  }
+}
 
-// function testCode() {
-//   let counter = 0;
-//   let pobiArray;
-//   for (let i = 0; i < 400; i++) {
-//     console.log(Math.floor(Math.random() * (399 - 2) + 2));
-//     counter++;
-//   }
-// }
+function testCode() {
+  let pobi = [0, 0];
+  let crong = [0, 0];
+
+  pobi[0] = Math.floor(Math.random() * (398 - 2) + 2);
+  crong[0] = Math.floor(Math.random() * (398 - 2) + 2);
+  pobi[0] = checkOdd(pobi[0]);
+  crong[0] = checkOdd(crong[0]);
+
+  pobi[1] = pobi[0] + 1;
+  crong[1] = crong[0] + 1;
+
+  problem1(pobi, crong);
+  // problem1([97, 98], [197, 198]);
+}
+
 testCode();
-problem1();
 
 module.exports = problem1;
