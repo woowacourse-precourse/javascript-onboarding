51bd6feaa258001a1a53659c5cd51c2c82cfb0ef
bae-sh <hyeon1293@gmail.com>
2022-10-27 22:39:27 +0900

    feat : problem1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -7,6 +7,12 @@ function calNumber(number) {
 }
 
 function problem1(pobi, crong) {
+  const wrongPage = pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1];
+
+  if (wrongPage) return -1;
+  const pobiHighestNumber = Math.max(calNumber(pobi[0]), calNumber(pobi[1]));
+  const crongHighestNumber = Math.max(calNumber(crong[0]), calNumber(crong[1]));
+  const answer = pobiHighestNumber > crongHighestNumber ? 1 : 0;
   return answer;
 }
 
