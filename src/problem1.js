73b9169aebd67567a0ab428a00cb35606262386d
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 17:10:08 +0900

    feat(Problem1): 각 자릿수를 배열에 담아 return하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+function returnNumber(page) {
+  const pg = [...String(page)]
+  const eachnum = []
+  pg.map((num) => eachnum.push(parseInt(num)))
+  return eachnum
+}
+
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
