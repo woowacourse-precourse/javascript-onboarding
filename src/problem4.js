04591d2b2406c954774d712e7d67e050152b480d
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-10-27 15:57:01 +0900

    problem4 - 일반 사전과 청개구리 사전을 만든다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
   var answer;
+  let dictionary = [];
+  let reversDictionary = [];
+  new Array(26).fill().map((_, i) => {
+    dictionary += String.fromCharCode(i + 97);
+  });
+  reversDictionary = dictionary.split("").reverse().join("");
+  console.log(dictionary, reversDictionary);
   return answer;
 }
 
 module.exports = problem4;
+
+// 기능 목록
+// 1. 일반 사전과 청개구리 사전을 만든다.
+// 2. 매개변수를 받았을 때, 그 문자가 일반 사전의 몇 번째 문자(index)인지 반환한다.
+// 3. index를 청개구리 사전에서 찾아서 반환한다.
+// 4. 예외 : 대문자는 대문자로 유지한다.
