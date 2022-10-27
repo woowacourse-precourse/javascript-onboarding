d373972234659dd3a59c6f80b5192ea46b6f404c
Sunghyun627 <kksscc4@gmail.com>
2022-10-27 19:55:44 +0900

    feat: 페이지 번호의 각 자리 숫자를 곱하는 기능 구현(getProductOfDigits)

--- src/problem1.js
+++ src/problem1.js
@@ -2,7 +2,7 @@
   구현할 기능 목록
   [O] 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인지 확인하는 기능 | return 값 : true/false | 함수명 : isConsecutiveNumbers    
   [O] 페이지 번호의 각 자리 숫자를 모두 더하는 기능 | return 값 : 각 자리 숫자의 합 
-  [ ] 페이지 번호의 각 자리 숫자를 모두 곱하는 기능 | return 값 : 각 자리 숫자의 곱
+  [O] 페이지 번호의 각 자리 숫자를 모두 곱하는 기능 | return 값 : 각 자리 숫자의 곱 | 함수명 : getProductOfDigits
   [ ] 인자로 주어진 값 중 가장 큰 수를 구하는 기능 | return 값 : 가장 큰 수
   [ ] 점수를 비교해 게임의 결과를 return 하는 기능 | return 값 : 0(무승부)/1(포비 승)/2(크롱 승) 
 */
@@ -20,6 +20,13 @@ function getSumOfDigits(number) {
     .toString()
     .split('')
     .reduce((accSum, curDigit) => accSum + parseInt(curDigit), 0);
+}
+
+function getProductOfDigits(number) {
+  return number
+    .toString()
+    .tosplit('')
+    .reduce((accProduct, curDigit) => accProduct * curDigit, 1);
 }
 
 module.exports = problem1;
