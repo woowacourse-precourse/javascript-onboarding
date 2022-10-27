f57af14ed4c37d6532cc2d81d97daf4ba2e1fe57
heesungjang <heesungj7@gmail.com>
2022-10-27 14:07:01 +1000

    우테코 1주차 과제 4번

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const dictionary = generateDic(word);
+
+  const reversedWord = word
+    .split('')
+    .reduce((a, c) => (a += dictionary[c]), '');
+
+  return reversedWord;
 }
+
+const generateDic = (word) => {
+  return word.split('').reduce((a, c) => {
+    if (c === ' ') {
+      a[c] = ' ';
+      return a;
+    }
+    if (c.charCodeAt() > 90) {
+      a[c] = String.fromCharCode(122 - (c.charCodeAt() - 97));
+      return a;
+    } else {
+      a[c] = String.fromCharCode(90 - (c.charCodeAt() - 65));
+      return a;
+    }
+  }, {});
+};
 
 module.exports = problem4;
