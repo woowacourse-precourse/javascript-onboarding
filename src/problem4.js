eb90e6ee9088d1b9e4876bcf9a2d8aa54b89827b
An Sol <apdlvmf486@gmail.com>
2022-10-27 13:52:05 +0900

    4번: 기능목록 수정 및 교체 문자열 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,9 @@
 function problem4(word) {
+  let result = '';
   function changeStr(letter) {
     const regex = /[a-zA-Z]/;
     if (!(regex.test(letter))) return letter;
-    
+
     let num = 0;
     if (letter !== letter.toUpperCase()){
       num = 25 - (letter.charCodeAt(0) - 'a'.charCodeAt(0)) * 2;
@@ -11,6 +12,10 @@ function problem4(word) {
     }
     return String.fromCharCode(letter.charCodeAt(0) + num); 
   }
+  for(let alphabet of word){
+    result += changeStr(alphabet);
+  }
+  return result;
 }
 
 module.exports = problem4;
