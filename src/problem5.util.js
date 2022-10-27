4b76321cdcc6a8658b50aa05c410597f03320828
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-27 22:07:32 +0900

    feat: problem5 checkInputError func

--- src/problem5.util.js
+++ src/problem5.util.js
@@ -1,6 +1,6 @@
 const MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
-const checkInputError = (n) => {};
+const checkInputError = (n) => n < 1 || n > 1000000;
 const countNumByMoney = (n) => {};
 
 exports.checkInputError = checkInputError;
