38892838e3d86262f53f150fdb5d5ed115974f80
Hypark2018 <hayoungpark2018@gmail.com>
2022-10-27 23:35:26 +0900

    🖊 feat: add main method & main logic structure.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isExceptional(pobi) || isExceptional(crong)) return -1
+  else if(getCalculate(pobi) > getCalculate(crong)) return 1
+  else if (getCalculate(crong) > getCalculate(pobi)) return 2
+  else if (getCalculate(crong) === getCalculate(pobi)) return 0
 }
+
+function isExceptional(pages){}
+
+function getCalculate(pages){}
+
 
 module.exports = problem1;
