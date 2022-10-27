45b3ebee04fc6e4567177e9a471646abc3a59245
Doyoung.Kim <dyflower@nate.com>
2022-10-27 20:17:26 +0900

    feat : problem5 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  const money_amount = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  //금액별로 필요한 갯수를 처리
+  let answer = money_amount.reduce((acc, v) => {
+    acc.push(Math.floor(money / v));
+    money %= v;
+    return acc;
+  }, []);
+
   return answer;
 }
 
