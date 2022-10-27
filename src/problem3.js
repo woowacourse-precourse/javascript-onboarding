03d4cebc93d6c67293d9fdf83669afaa77ccab3b
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-27 10:05:59 +0900

    feat: problem3.js에 손뼉을 쳐야하는 횟수를 세는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    let str = String(i).split('');
+    for (const char of str) {
+      if (char === '3' || char === '6' || char === '9') {
+        count += 1;
+      }
+    }
+  }
+
+  return count;
 }
 
 module.exports = problem3;
