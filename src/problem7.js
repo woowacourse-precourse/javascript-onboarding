fcaf4c1c2720b538d213cc80483dd3e3e97719b1
BB-choi <choibobae0809@gmail.com>
2022-10-27 21:43:24 +0900

    feat: 문제7 - 친구관계 Map에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,21 @@
+function createFriendsMap(friends) {
+  const frinedsMap = new Map();
+
+  friends.forEach(([userA, userB]) => {
+    frinedsMap.set(
+      userA,
+      frinedsMap.has(userA) ? [...frinedsMap.get(userA), userB] : [userB]
+    );
+
+    frinedsMap.set(
+      userB,
+      frinedsMap.has(userB) ? [...frinedsMap.get(userB), userA] : [userA]
+    );
+  });
+
+  return frinedsMap;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
