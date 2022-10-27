4066087cbc79a69ba7246a024d35d97e300b66cc
JaydenLee1116 <vv55adss@gmail.com>
2022-10-27 18:18:05 +0900

    feat: Solve problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
   var answer;
+
+  const CLAP_NUMBERS = ['3', '6', '9'];
+  let clapCount = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const stringNumber = String(i);
+    for (let j = 0; j < stringNumber.length; j++) {
+      if (CLAP_NUMBERS.includes(stringNumber.charAt(j))) clapCount++;
+    }
+  }
+
+  answer = clapCount;
   return answer;
 }
 
