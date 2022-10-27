7840494ef2e416e0ae51bf28a050d4b7e37d7674
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-10-28 01:21:33 +0900

    feat(problem4): 예외 처리 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,40 @@
+const INPUT = {
+  minLength: 1,
+  maxLength: 1000,
+};
+
+const RESULT = {
+  exception: -1,
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+  if (isWrongInput(word)) {
+    return RESULT.exception;
+  }
+}
+
+function isWrongInput(input) {
+  if (isWrongInputData(input)) {
+    return true;
+  }
+
+  if (isWrongLengthOfInput(input.length)) {
+    return true;
+  }
+
+  if (Number.isNaN(input)) {
+    return true;
+  }
+
+  return false;
+}
+
+function isWrongInputData(input) {
+  return !input;
+}
+
+function isWrongLengthOfInput(length) {
+  return length < INPUT.minLength || length > INPUT.maxLength;
 }
 
 module.exports = problem4;
