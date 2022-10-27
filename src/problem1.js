73db76316ce5b96dc054ffd92cc53dbb2f80a60f
kim-limit <rlawogks2468@naver.com>
2022-10-27 18:32:39 +0900

    feat : 'add addpages func(페이지를 받아 곱한것과 더한것중 더 큰걸 return)'

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,30 @@
+function addPages(number) {
+  let addSum = 0;
+  let multiSum = 1;
+  let str = parseInt(number).toString();
+
+  for (let i = 0; i < str.length; i++) {
+    addSum += parseInt(str[i]);
+    multiSum *= parseInt(str[i]);
+  }
+  return addSum > multiSum ? addSum : multiSum;
+}
+
 function problem1(pobi, crong) {
   var answer;
+
   return answer;
 }
+
+let pobi = [
+  [97, 98],
+  [131, 132],
+  [99, 102],
+];
+let crong = [
+  [197, 198],
+  [211, 212],
+  [211, 212],
+];
 
 module.exports = problem1;
