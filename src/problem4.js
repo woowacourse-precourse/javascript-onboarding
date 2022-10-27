d15d9869c5f91ed09375d8e2cbd3bb2038a69608
Chris.c <cjs120dd@gmail.com>
2022-10-27 11:56:34 +0900

    fear:4번 문제를 해결한다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
+const ASC_CODE = {
+    A:65,
+    Z:90,
+    a:97,
+    z:122
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+    const wordArr = [...word];
+    const ascCodeArr = wordArr.map(string => string.charCodeAt(0));
+    const convertedWordArr = ascCodeArr.map(num => {
+        if (ASC_CODE.A <= num && num <= ASC_CODE.Z) {
+            return String.fromCharCode(ASC_CODE.A+ASC_CODE.Z - num)
+        }
+        if (ASC_CODE.a <= num && num <= ASC_CODE.z) {
+            return String.fromCharCode(ASC_CODE.a+ASC_CODE.z - num)
+        }
+        return String.fromCharCode(num);
+    });
+    
+    return convertedWordArr.join("");
 }
 
 module.exports = problem4;
