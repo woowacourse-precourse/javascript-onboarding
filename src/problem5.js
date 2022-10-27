9053252e9fbf78ffffa45c4d7a002f8a33e02c82
nsyeon <lovepny@gmail.com>
2022-10-27 21:11:25 +0900

    feat: 금액에 따른 지폐 및 동전 개수 변환

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,19 @@
 function problem5(money) {
   var answer;
+
+  answer = [];
+
+  m = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+  for (var i = 0; i < m.length; i++){
+    //.push => 넣음
+    //Math.floor : 소수점 버림
+    //money / m[i] => money / 50000 ~ 1
+    answer.push(Math.floor(money/m[i]));
+    //money % m[i] => 나머지를 구하고 money에 대입
+    money = money % m[i];
+  }
+  
   return answer;
 }
 
