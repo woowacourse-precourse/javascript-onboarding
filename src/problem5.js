fde8c92064232cbd4b16799a7dc0c0a718e21b58
ctd <qpflapffhs76@gmail.com>
2022-10-27 11:08:49 +0900

    feat: problem5 거스름돈 Map을 순회하면서 결과를 출력하는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,30 @@
 function problem5(money) {
-  var answer;
+  var moneyMap = new Map([
+    [50000, 0],
+    [10000, 0],
+    [5000, 0],
+    [1000, 0],
+    [500, 0],
+    [100, 0],
+    [50, 0],
+    [10, 0],
+    [1, 0],
+  ]);
+  1;
+
+  for (let change of moneyMap.keys()) {
+    moneyMap.set(change, parseInt(money / change));
+    money %= change;
+  }
+
+  var answer = [];
+  for (let number of moneyMap.values()) {
+    answer.push(number);
+  }
+
   return answer;
 }
+
+console.log(problem5(50237));
 
 module.exports = problem5;
