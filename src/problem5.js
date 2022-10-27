aad1a3df0cc74a6f5b7ed03b20757ca5592b0e8d
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-27 14:33:09 +0900

    problem5 반환 count 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
   var answer;
+  const monetaryUnit = [50000,10000,5000,1000,500,100,50,10,1]
+  for (let i=0;i<monetaryUnit.length; i++){
+    let moneycount = 0
+    moneycount = parseInt(money/monetaryUnit[i])
+    money -= moneycount * monetaryUnit[i]
+  }
   return answer;
 }
-
 module.exports = problem5;
