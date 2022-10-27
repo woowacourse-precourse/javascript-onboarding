ab1458b51b7a989a2338da8eb2ba696cea39e57b
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-27 16:41:27 +0900

    feat: problem5.js에 화폐 단위 별 최소 개수로 변환하는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [];
+  let remaining = money;
+  
+  units.forEach((unit) => {
+    result.push(Math.floor(remaining / unit));
+    remaining %= unit;
+  });
+
+  return result;
 }
 
 module.exports = problem5;
