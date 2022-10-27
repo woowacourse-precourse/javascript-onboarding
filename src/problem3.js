77d5135f6396b270a4368821cdb1864cf509b809
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 15:19:53 +0900

    feat(problem3): 1부터 number까지 순회하며 각 숫자가 3,6,9 포함되는지 확인 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
   var answer;
+
+  const cntOfClap = (num) => {
+    // 포함되는 개수 더해주기
+  };
+
+  for (let num = 1; num <= number; num++) {
+    num = num.toString();
+    if (num.includes("3" || "6" || "9")) {
+      answer += cntOfClap(num);
+    }
+  }
   return answer;
 }
 
