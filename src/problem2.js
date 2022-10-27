19c7aa5eecd986befe8be9ec5dcb42553390b755
JongHun <minn4072@gmail.com>
2022-10-28 00:50:54 +0900

    우테코 프리코스 문제2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  let stack = [];
+
+  for (const i of cryptogram) {
+    if (stack.length === 0 || stack[stack.length - 1] !== i) stack.push(i);
+    else if (stack[stack.length - 1] === i) stack.pop();
+  }
+
+  answer = stack.join("");
   return answer;
 }
 
