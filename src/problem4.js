19bac19b64ebc108c7d1e77f33dabf5bef06fc44
c0c0pang <coc1789@naver.com>
2022-10-27 13:12:31 +0900

    문제 4 구현.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
   var answer;
-  return answer;
+  const newWord = [];
+  const ASCII_a = 65;
+  const ASCII_A = 97;
+  const ASCII_Space = ASCII_A-ASCII_a;
+  
+  let UpCheck,LowCheck;
+  for(let i=0;i<word.length;i++){
+    if(word[i]===word[i].toUpperCase()){
+      UpCheck = ASCII_a+((ASCII_a+25)-word[i].charCodeAt(0));
+      if(UpCheck===123){
+        newWord.push(String.fromCharCode(ASCII_Space));
+      }
+      else{
+        newWord.push(String.fromCharCode(UpCheck));
+      }
+    }
+    else{
+      LowCheck = ASCII_A+((ASCII_A+25)-word[i].charCodeAt(0));
+      newWord.push(String.fromCharCode(LowCheck));
+    }
+  }
+  return newWord.join("");
 }
 
 module.exports = problem4;
