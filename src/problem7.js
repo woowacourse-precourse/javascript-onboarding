9341fd9a0e27133c8057efe6f5a8a311c7002c6c
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 23:35:45 +0900

    feat(Problem7): visitor 각자의 방문횟수 객체로 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,29 @@
+function visitCount(visitors){
+  return visitors.reduce((prev, curr) => {
+    if (curr in prev){
+      prev[curr] += 1;
+    }
+    else{
+      prev[curr] = 1;
+    }
+    return prev;
+  }, {});
+};
+
+
 function problem7(user, friends, visitors) {
-  var answer;
+  
   return answer;
 }
 
 module.exports = problem7;
+
+console.log(problem7('mrko', [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ], ["bedi", "bedi", "donut", "bedi", "shakevan"]))
+
+/*
+user = "mrko";
+friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ];
+visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
+result = ["andole", "jun", "bedi"]
+
+*/
