1aa341813d1a4b0b93c8f109b96a6217f16b932c
WonBeen Choi <zad1264@naver.com>
2022-10-27 21:47:07 +0900

    refactor: 1번 문제 - 가독성 향상 및 함수 반복 정의 해결
    함수 내 함수 선언으로 인한 반복 선언 문제 해결

--- src/problem1.js
+++ src/problem1.js
@@ -2,39 +2,45 @@
  * -- 기능 목록 --
  * 1. 페이지의 배열인 유저를 받아 두 페이지 중 최고점을 반환하는 함수 - getUesrScore(user)
  * 2. 페이지를 받아 점수로 반환하는 함수 - calculatePageScore(page)
- * 3. 두 페이지가 붙어있지 않은 예외사항을 처리하는 함수 - checkInvalidate(user[])
+ * 3. 두 페이지가 붙어있지 않은 예외사항을 처리하는 함수 - isValid(user[])
  */
 
-function getUserScore([leftPage, rightPage]) {
-  const calculatePageScore = (page) =>
-    Math.max(
-      String(page)
-        .split("")
-        .reduce((acc, current) => acc + Number(current), 0),
-      String(page)
-        .split("")
-        .reduce((acc, current) => acc * Number(current), 1)
-    );
+function calculatePageScore(page) {
+  return Math.max(
+    String(page)
+      .split("")
+      .reduce((acc, current) => acc + Number(current), 0),
+    String(page)
+      .split("")
+      .reduce((acc, current) => acc * Number(current), 1)
+  );
+}
 
+function getUserScore([leftPage, rightPage]) {
   const leftPageScore = calculatePageScore(leftPage);
   const rightPageScore = calculatePageScore(rightPage);
 
   return Math.max(leftPageScore, rightPageScore);
 }
 
-function checkInvalidate(userList) {
-  return userList.some(([left, right]) => left + 1 !== right);
+function isValid(userList) {
+  return userList.every(([left, right]) => left + 1 === right);
 }
 
 function problem1(pobi, crong) {
-  if (checkInvalidate([pobi, crong])) return -1;
+  if (!isValid([pobi, crong])) return -1;
 
-  let pobiScore = getUserScore(pobi);
-  let crongScore = getUserScore(crong);
+  const pobiScore = getUserScore(pobi);
+  const crongScore = getUserScore(crong);
 
-  if (pobiScore > crongScore) return 1;
-  if (pobiScore < crongScore) return 2;
-  return 0;
+  switch (true) {
+    case pobiScore === crongScore:
+      return 0;
+    case pobiScore > crongScore:
+      return 1;
+    case pobiScore < crongScore:
+      return 2;
+  }
 }
 
 module.exports = problem1;
