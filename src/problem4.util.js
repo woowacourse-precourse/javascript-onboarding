ba135a899920e05b3784068eeb1ce8f646b52f61
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-27 17:26:05 +0900

    feat: problem4 checkinputerror func

--- src/problem4.util.js
+++ src/problem4.util.js
@@ -1,5 +1,5 @@
 // word 길이 체크
-const checkInputError = (word) => {};
+const checkInputError = (word) => word.length < 1 || word.length > 1000;
 // 알파벳 체크
 const checkAlpha = (alpha) => {};
 // 알파벳 변환
