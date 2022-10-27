d0fabd0ccca3113f2eaefefb12a862cc2d1b89f6
김희진 <kimheejin@gimhuijins-MacBook-Air.local>
2022-10-27 11:56:36 +0900

    크롱과 포비 배열 분리 및 숫자 배열 재 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,30 @@
 function problem1(pobi, crong) {
-  let pobi_1 = pobi[0];
-  let pobi_2 = pobi[1];
-  let crong_1 = crong[0];
-  let crong_2 = crong[1];
+  console.log("기본값",pobi, crong)
+  const N = 2;
+  let pobinums = []
+  let crongnums = []
+  for (i = 0 ; i < N ; i++) {
+    let _pobinums = [];
+    do {
+      _pobinums.push(pobi[i] % 10);  
+      pobi[i] = Math.floor(pobi[i] / 10);  
+    } while (pobi[i] > 0);
+    console.log("포비분해",_pobinums)
+    pobinums.push(_pobinums)
+
+    let _crongnums = [];
+    do {
+      _crongnums.push(crong[i] % 10);  
+      crong[i] = Math.floor(crong[i] / 10);  
+    } while (crong[i] > 0);
+    console.log("크롱분해",_crongnums)
+    crongnums.push(_crongnums)
+  }
+  console.log("포비확인",pobinums)
+  console.log("크로확인",crongnums)
 }
+
+
+
 
 module.exports = problem1;
