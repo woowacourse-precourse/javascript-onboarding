609734119d89a566bcc54f815429418028bcdbdf
dev-redo <mis05041@naver.com>
2022-10-27 13:31:13 +0900

    [solve 2번] stack 풀이

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [cryptogram[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    const currElem = cryptogram[i];
+
+    if (stack[stack.length - 1] === currElem) {
+      stack.pop();
+      continue;
+    }
+    stack.push(currElem);
+  }
+
+  return stack.join('');
 }
 
 module.exports = problem2;
