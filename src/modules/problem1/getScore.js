83093455eb04a863d9796d575d30368ffd8c2a64
cloudedpanther <rickeykoh@ajou.ac.kr>
2022-10-27 23:52:11 +0900

    all: 코드 컨벤션 조정

--- src/modules/problem1/getScore.js
+++ src/modules/problem1/getScore.js
@@ -2,12 +2,7 @@ const sumDigits = require("./sumDigits.js");
 const multiplyDigits = require("./multiplyDigits.js");
 
 function getScore(pages) {
-  return Math.max(
-    sumDigits(pages[0]),
-    sumDigits(pages[1]),
-    multiplyDigits(pages[0]),
-    multiplyDigits(pages[1])
-  );
+  return Math.max(sumDigits(pages[0]), sumDigits(pages[1]), multiplyDigits(pages[0]), multiplyDigits(pages[1]));
 }
 
 module.exports = getScore;
