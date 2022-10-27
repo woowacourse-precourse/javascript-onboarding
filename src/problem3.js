da4bbec5d5a44dfc28952957a471a69ad2a06563
minseok1109 <park485201@naver.com>
2022-10-27 11:57:24 +0900

    feat: 3,6,9를 가지고 있는지 체크하는 함수 #기능목록-2

--- src/problem3.js
+++ src/problem3.js
@@ -2,9 +2,20 @@
 const range = (from, to, step = 1) =>
   [...Array(Math.floor((to - from) / step) + 1)].map((_, i) => from + i * step);
 
-function problem3(number) {
-  let arrNumber = Array.from({ length: number + 1 }, (x, i) => i);
+//3,6,9를 가지고 있는지 체크하는 함수 - 기능목록 2
+function includes369(num) {
+  if (
+    num.toString().includes("3") ||
+    num.toString().includes("6") ||
+    num.toString().includes("9")
+  )
+    return true;
+  return false;
 }
 
-console.log(range(1, 13, 5));
+function problem3(number) {
+  let arrNumber = range(1, number);
+  return arrNumber.filter((item) => includes369(item));
+}
+
 module.exports = problem3;
