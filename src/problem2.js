61d8ec12aef0468e5750e3fde5e9db5a1f3b250f
easyone <thinkanddoit@naver.com>
2022-10-27 17:56:46 +0900

    #2 feat: 문제2 중복 문자 삭제 함수 생성 및 적용
    - test 2개 통과

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const deleteDuplication = (string) => {
+    const stringToArr = string.split("");
+
+    let i = 0;
+    while (i < stringToArr.length) {
+      if (stringToArr[i] === stringToArr[i + 1]) {
+        stringToArr.splice(i, 2);
+        i = 0;
+      } else i++;
+    }
+
+    return stringToArr.join("");
+  };
+
+  return deleteDuplication(cryptogram);
 }
 
 module.exports = problem2;
