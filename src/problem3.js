ba3057052c28c3090b1d1e7da5319f5c8f1e958c
Suyeon <msjgr2801@gmail.com>
2022-10-27 14:44:26 +0900

    comment: Write problem3's function lists

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,15 @@
+// 예외처리
+// 0. [x] number <= 2 -> return answer
+
+// 메인 로직
+// 1. [x] 1에서 number까지 순회
+// 2. [x] 3,6,9 를 포함하면 answer + 1
+
+// 369 판별 로직
+// 2.1 [x] String(number) 문자열로 바꾸기
+// 2.2 [ ] 문자열을 쪼개서 문자열을 순회하며 3,6,9의 개수를 count
+
+
 function problem3(number) {
   var answer;
   return answer;
