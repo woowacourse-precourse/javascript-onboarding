08ddcd9e2df002bac66bd81d2b366193cc3c24e0
SoMin <thals0107@naver.com>
2022-10-27 13:52:23 +0900

    feat: pobi, crong 의 left, right 변수 부여 각자 max 값 구하기

--- src/problem1.py
+++ src/problem1.py
@@ -14,4 +14,19 @@ def mult0(n):
       n//=10
   return mult_res
 
+# 문제 해결 함수
+def problem1(pobi, crong):
+  # 각 배열의 왼쪽과 오른쪽 구별
+  pobi_left = pobi[0]
+  pobi_right = pobi[1]
+  crong_left = crong[0]
+  crong_right = crong[1]
+
+  # 각자의 max값 구하기
+  pobi_max = max(sum0(pobi_left), sum0(pobi_right), mult0(pobi_left), mult0(pobi_right))
+  crong_max = max(sum0(crong_left), sum0(crong_right), mult0(crong_left), mult0(crong_right))
+
+
+
+
 
