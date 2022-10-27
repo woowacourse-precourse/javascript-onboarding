2d76497ced5193d76b4efb1c37deda407e604eb7
evencoding <evencoding@gmail.com>
2022-10-28 01:18:50 +0900

    [문제7] feat: 사용자끼리의 관계를 연결하는 Object를 구현하는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,29 @@
+const relations = {};
+const score = {};
+
+function makeRelations(friends, me) {
+  friends.forEach((friend) => {
+    if (friend[0] in relations) {
+      if (friend[1] !== me) {
+        relations[friend[0]].push(friend[1]);
+      }
+    } else {
+      relations[friend[0]] = [friend[1]];
+    }
+    if (friend[1] in relations && friend[0] !== me) {
+      if (friend[0] !== me) {
+        relations[friend[1]].push(friend[0]);
+      }
+    } else {
+      relations[friend[1]] = [friend[0]];
+    }
+  });
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const answer = [];
+  makeRelations(friends, user);
+
   return answer;
 }
 
