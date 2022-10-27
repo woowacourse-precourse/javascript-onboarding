da9e2174877dc52cca6cb2215dcbd3ef1aa10b99
hyeongdong <khd941018@gmail.com>
2022-10-27 19:56:18 +0900

    docs : 기능목록 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,10 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+/*
+--기능목록--
+
+1. 연속하는 중복 문자를 제거한다.
+    - 더 이상 제거하지 못할 때 까지 제거 후 최종 문자열을 리턴한다.
+*/
+
+function problem2(cryptogram) {}
 
 module.exports = problem2;
