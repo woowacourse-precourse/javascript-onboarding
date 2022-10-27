820103c6510c1e12c5cbf481b1bd2417aec6fa65
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-27 16:11:56 +0900

    feat: divide money until 0  in problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let idx = 0;
+  while (money > 0) {
+    money = money % unit[idx];
+    idx += 1;
+    console.log(money);
+  }
 }
 
 module.exports = problem5;
