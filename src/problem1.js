6ad8aa96be56c31e2225180c7d26c94a3e798569
catca <kdg96g@gmail.com>
2022-10-27 09:47:44 +0900

    fix: fixed problem1
    return -1 조건 수정

--- src/problem1.js
+++ src/problem1.js
@@ -6,8 +6,8 @@ function calMul(number) {
 }
 
 function problem1(pobi, crong) {
-  if(pobi[1] - pobi[0] !== 1 || pobi[0] % 2 !== 1 || pobi[0] < 2 || pobi[0] > 399) return -1
-  if(crong[1] - crong[0] !== 1 || crong[0] % 2 !== 1 || crong[0] < 2 || crong[0] > 399) return -1
+  if(pobi[1] - pobi[0] !== 1 || pobi[0] % 2 !== 1 || pobi[0] < 1 || pobi[0] > 399) return -1
+  if(crong[1] - crong[0] !== 1 || crong[0] % 2 !== 1 || crong[0] < 1 || crong[0] > 399) return -1
 
   const pobiMax = Math.max(calAdd(pobi[0]), calMul(pobi[0]), calAdd(pobi[1]), calMul(pobi[1]));
   const crongMax = Math.max(calAdd(crong[0]), calMul(crong[0]), calAdd(crong[1]), calMul(crong[1]));
