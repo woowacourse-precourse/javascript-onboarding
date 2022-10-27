2bee971d8b79ed81ce96ea59245ec1f17bdf195b
dim <dim@student.42seoul.kr>
2022-10-28 03:36:41 +0900

    [problem1 완성]Style: 주석 삭제

--- src/problem1.js
+++ src/problem1.js
@@ -42,16 +42,3 @@ function problem1(pobi, crong) {
 
 
 module.exports = problem1;
-
-/* 
-예외처리
-1. 시작면과 마지막면 포함하면 안됨 3 <= left <= 397
-2. left == 홀수
-3. left + 1 == right
-
-구현
-1. pobiMax와 crongMax값을 비교하고 리턴하는 메인 함수
-2. 예외처리 함수
-3. 페이지의 각 자리의 숫자를 모두 더하는 함수, 곱하는 함수
-4. 3번 값들을 비교해서 Max를 리턴하는 함수
- */
