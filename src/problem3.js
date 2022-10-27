bd9a26753847d3568d00acb84c36986f1152e733
minsoo <als494@naver.com>
2022-10-27 15:23:40 +0900

    3번 문제 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,21 @@
 function problem3(number) {
   var answer;
+
+  let i = 1;
+  let clap = 0;
+
+  while (i <= number) {
+    let arr = i.toString().split("");
+    arr.map((num) => {
+      if (num === "3" || num === "6" || num === "9") {
+        clap++;
+      }
+    });
+    i++;
+  }
+
+  answer = clap;
+
   return answer;
 }
 
