85d09ed3cb62928db009d1e3769263b0d76164f1
guridaek <iue16@naver.com>
2022-10-27 12:51:15 +0900

    feat: 문제3 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  
+  for (let i = 1; i <= number; i++) {
+    let num = i.toString();
+    for (let n of num) {
+      if ([3, 6, 9].includes(+n)) {
+        answer++;
+      }
+    }
+  }
+  
   return answer;
 }
 
