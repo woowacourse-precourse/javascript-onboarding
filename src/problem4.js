b7ac0b06c4c5a285001aaacea1d278737801cb69
ReturnReason <reasonz@daum.net>
2022-10-27 16:57:36 +0900

    feat: problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,27 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+
+  const alphabet = [];
+  for (let i = 97; i <= 122; i++) {
+    alphabet.push(String.fromCharCode(i));
+  }
+
+  const reverseAlphabet = [...alphabet].reverse();
+
+  for (let i = 0; i < word.length; i++) {
+    let ascii = word[i].charCodeAt();
+    let findIndex;
+    if (ascii >= 65 && ascii <= 90) {
+      findIndex = alphabet.indexOf(word[i].toLowerCase());
+      answer += reverseAlphabet[findIndex].toUpperCase();
+    } else if (ascii >= 97 && ascii <= 122) {
+      findIndex = alphabet.indexOf(word[i].toLowerCase());
+      answer += reverseAlphabet[findIndex];
+    } else {
+      answer += word[i];
+    }
+  }
+
   return answer;
 }
 
