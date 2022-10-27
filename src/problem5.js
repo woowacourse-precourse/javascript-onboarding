fd663621bcf788286ca90c44be50cecaa861efa8
chee9835 <huisu121@gmail.com>
2022-10-27 21:55:46 +0900

    feat: 몫과 나머지 이용해서 큰 금액 화폐 만들기

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  var units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (var i of units) {
+    var quotient = parseInt(money / i);
+    answer.push(quotient);
+    money = money % i;
+  }
   return answer;
 }
 
