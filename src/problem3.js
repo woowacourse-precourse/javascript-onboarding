0146ff569c7815459c5c895a0058ee7f2b0da4ba
violetwhenisawu <anashape710@gmail.com>
2022-10-27 21:51:50 +0900

    Feat: 3,6,9가 들어간 총 갯수를 반환하는 함수 완성

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,10 @@
 function problem3(number) {
-  // 1. 1 ~ number까지 도는 반복문
-  // 2. 369를 검사하는 함수를 호출하고, 반환값을 저장할 변수
-  // 3. 반환된 값을 모두 더할 변수 (리턴할 변수)
+  let result = 0;
+  for (let i = 1; i <= number; i++) {
+    let count = check369(i);
+    result += count;
+  }
+  return result;
 }
 
 // 3, 6, 9가 포함되어 있는지 검사하고, 포함된 갯수를 반환하는 함수
