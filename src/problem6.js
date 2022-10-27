129037630d6b3ea49e965cd2a8ccb759c4c7060c
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-27 22:56:03 +0900

    feat: problem6 solved

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,23 @@
+const {
+  makeNickNameCombination,
+  makeCountMapOfNickName,
+  filterValue2OverOfMapToList,
+  getNameIncludeIndex,
+  getTargetOfList,
+} = require("./problem6.util");
+
 function problem6(forms) {
-  var answer;
+  const emails = forms.map(([email, _]) => email);
+  const nickNames = forms.map(([_, nickName]) => nickName);
+  const combinations = nickNames.map(makeNickNameCombination).flat();
+  const map = makeCountMapOfNickName(combinations);
+  const filterCombinations = filterValue2OverOfMapToList(map);
+  const filterCombinationsCallback = (nickName) =>
+    getNameIncludeIndex(nickNames, nickName);
+  const indexs = Array.from(
+    new Set(filterCombinations.map(filterCombinationsCallback).flat())
+  );
+  const answer = getTargetOfList(emails, indexs).sort();
   return answer;
 }
 
