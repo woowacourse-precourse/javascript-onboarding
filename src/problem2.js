4195fd672a33521171091434870416afb1665e23
eunsung <esk114477@gmail.com>
2022-10-27 15:19:49 +0900

    문제2번 제한사항 및 중복 문자 제거 완성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+  //제한사항
+  if(cryptogram.length < 1 || cryptogram.length > 1000) return -1;
+  let cryptogramToLower = cryptogram.toLowerCase();
+
+  //중복 문자 제거하기
+  let arr = cryptogramToLower.split("");
+  let answer = [];
+
+  for(let str of arr){
+    if(answer[answer.length - 1] === str){
+      answer.pop();
+    } else {
+      answer.push(str);
+    }
+  }
+  
+  return answer.join("");
 }
 
 module.exports = problem2;
