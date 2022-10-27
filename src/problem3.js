58e2bc79d591f82af9a2ec8dea299bf73db00c42
ryuna <anottrx@gmail.com>
2022-10-28 03:00:53 +0900

    feat : 문자열에 3,6,9만 남도록 구현

--- src/problem3.js
+++ src/problem3.js
@@ -7,4 +7,8 @@ const makeNumberWord = (number) => {
   return Array.from({ length: number }, (_, index) => index + 1).join("");
 };
 
+const makeOnly369 = (word) => {
+  return word.replaceAll(/[0124578]/g, "");
+};
+
 module.exports = problem3;
