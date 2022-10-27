9beb40286f5cbfecd5f4a2947058e788b3257bcf
1 <wrasf175@naver.com>
2022-10-27 18:10:41 +0900

    feat : 2번 과제 기능 구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
-  var answer;
+  if (cryptogram.length === 1) return cryptogram;
+  let answer;
+  let stack = [cryptogram[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    let stackChar = stack[stack.length - 1];
+    let cryptogramChar = cryptogram[i];
+
+    if (stackChar === cryptogramChar) {
+      stack.pop();
+    }
+    if (stackChar !== cryptogramChar) {
+      stack.push(cryptogramChar);
+    }
+  }
+
+  answer = stack.join('');
   return answer;
 }
+
 
 module.exports = problem2;
