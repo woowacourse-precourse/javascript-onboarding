fe136e27f5bb6a8d7a822605fcbfb18f85a895c5
unknown <chn0202@gmail.com>
2022-10-27 17:58:59 +0900

    Feat: problem3 기능 개발
    1부터 number중에 3,6,9 몇번있는지 count 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCount = 0;
+  for (let i = 1; i <= number; i++) {
+    let strArr = String(i).split("");
+    for (let j = 0; j < strArr.length; j++) {
+      if (strArr[j] === "3") clapCount++;
+      else if (strArr[j] === "6") clapCount++;
+      else if (strArr[j] === "9") clapCount++;
+    }
+  }
+  return clapCount;
 }
 
 module.exports = problem3;
