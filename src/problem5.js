617c37738ffdea9fb299ce1e15bc82fd41be9d57
yeong bin <2976622@naver.com>
2022-10-27 18:47:16 +0900

    feat: problem5 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
+/**
+ * Returns the decrypt string
+ * @param {number} money
+ * @returns {Array<number>}
+ */
 function problem5(money) {
-  var answer;
-  return answer;
-}
+  let leftMoney = money;
+  return [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+      .map((m) => {
+        const count = parseInt(leftMoney / m);
+        leftMoney -= count * m;
+        return count;
+      })
+} //leftMoney 부수효과 없애야함
 
 module.exports = problem5;
