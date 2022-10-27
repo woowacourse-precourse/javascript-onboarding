415becd83ad865af856829b61287b6e966129b45
powercording <adoim@naver.com>
2022-10-27 19:02:43 +0900

    refactor : problem5 주석달기와 함수 분할

--- src/problem5.js
+++ src/problem5.js
@@ -6,27 +6,39 @@ function problem5(money) {
 
 module.exports = problem5;
 
+//머니 범위 상수 선언
 const MONEY_LOW = 1;
 const MONEY_HIGH = 1000000;
 
+//입력값이 숫자가 맞는지 범위가 맞는지 평가
 function isMoney(money) {
   if (money < MONEY_LOW || money > MONEY_HIGH) return fasle;
   if (typeof money === "number") return true;
   else return false;
 }
 
+//입력값에 대한 계산값을 problem5에 넘겨준다.
 function moneyChange(money) {
   if (!isMoney(money)) {
     alert("돈을 넣어야 합니다");
     return;
   }
+
+  let exchange = moneyLoop(money);
+  return exchange;
+}
+
+//실제 입력된 머니에 대한 계산 결과 배열을 리턴하는 함수
+function moneyLoop(money) {
   let tempMoney = money;
   const moneyStep = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let exchange = [];
+
   for (let i = 0; i < moneyStep.length; i++) {
     const result = Math.floor(tempMoney / moneyStep[i]);
     exchange[i] = result;
     tempMoney = tempMoney % moneyStep[i];
   }
+
   return exchange;
 }
