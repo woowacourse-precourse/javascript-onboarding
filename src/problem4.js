2c56ba2c2d12df5e83941bf736fe5fbf5de6b414
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 21:07:24 +0900

    feat(Problem4): 알파벳 변환 함수 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,17 @@
+function alphabet(letter){
+  if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90){
+    //대문자
+    return String.fromCharCode(155 - letter.charCodeAt(0));
+  }
+  else if (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122){
+    //소문자
+    return String.fromCharCode(219 - letter.charCodeAt(0));
+  }
+  else{
+    return letter
+  }
+}
+
 function problem4(word) {
   var answer;
   return answer;
