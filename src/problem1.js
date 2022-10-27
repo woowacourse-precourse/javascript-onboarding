aeb413c7675a23b8c6dafa489da0145a410df1b0
seongminn <berry9901@gmail.com>
2022-10-28 01:11:09 +0900

    feat(problem1): 페이지 번호의 곱을 반환하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -4,8 +4,14 @@ function checkValidation(pageArr) {
   return left % 2 === 1 && right - left && left >= 3 && left <= 397;
 }
 
+function getMulti(pageNum) {
+  return [...String(pageNum)].reduce((acc, cur) => (acc *= Number(cur)), 1);
+}
+
 function problem1(pobi, crong) {
   if (!checkValidation(pobi) || !checkValidation(crong)) return -1;
+
+  console.log(getMulti(pobi[0]));
 
   var answer;
 
