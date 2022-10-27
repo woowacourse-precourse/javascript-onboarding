2380799293a5bd9749fbd14ff7e78f87ef394057
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 20:57:54 +0900

    feat: [문제5] 돈의 액수에 따라 화폐별 변환 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = moneyList.map((moneyType) => {
+    const count = Math.floor(money / moneyType);
+    money = money % moneyType;
+    return count;
+  });
+
+  return result;
 }
+
+problem5(15000); //?
 
 module.exports = problem5;
