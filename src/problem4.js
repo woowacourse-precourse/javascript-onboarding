1c062d214a9e4a6ba75730a5ba0cbdb3aa59d41b
sery <yunsel2@gmail.com>
2022-10-27 23:25:05 +0900

    feat: Add exeption handling feature

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,20 @@
+const alphabet = 'abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+const reversedAlphabet = 'zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA';
+
 function problem4(word) {
-  var answer;
+  let answer;
+  const validWord = /^[a-zA-Z\s]*$/;
+
+  if (word.length < 1 || word.length > 1000) {
+    console.error('길이가 1 이상 1000 이하인 문자열을 입력해주세요');
+    return;
+  }
+
+  if (!validWord.test(word)) {
+    console.error('알파벳을 입력해주세요');
+    return;
+  }
+
   return answer;
 }
 
