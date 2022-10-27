1afd44982a473b9868089060bb2797c79b9fd282
h2002y <h2002y@naver.com>
2022-10-27 18:03:08 +0900

    feat: Add getNumOfCoins func in problem5.js

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,16 @@
 function problem5(money) {
   var answer = [];
   const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  getNumOfCoins(answer, coins, money);
+  return answer;
+}
+
+function getNumOfCoins(result, coins, money){
   let moneyLeft = money;
   for(let i = 0; i < 9; i++){
-    answer.push(parseInt(moneyLeft / coins[i]));
+    result.push(parseInt(moneyLeft / coins[i]));
     moneyLeft %= coins[i];
   }
-  return answer;
 }
 
 module.exports = problem5;
