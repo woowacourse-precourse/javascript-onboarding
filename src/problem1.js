d694cd51264195727f6e1fd9e54d19c1df002f80
hae-on <solbi2004@naver.com>
2022-10-28 00:11:57 +0900

    [문제1] 출력 예시 삭제

--- src/problem1.js
+++ src/problem1.js
@@ -67,8 +67,4 @@ function problem1(pobi, crong) {
   if (getMaxNumber(pobi) === getMaxNumber(crong)) return 0;
 }
 
-console.log(problem1([97, 98], [197, 198]));
-console.log(problem1([131, 132], [211, 212]));
-console.log(problem1([99, 102], [211, 212]));
-
 module.exports = problem1;
