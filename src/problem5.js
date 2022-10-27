f3f92d98a03c6552addf34b1b7faf33c721c33b1
lee-il-woo <empty001430@gmail.com>
2022-10-27 17:53:44 +0900

    문제5 반복문으로 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,23 +1,13 @@
 function problem5(money) {
   var answer = [];
-  answer.push(parseInt(money / 50000))
-  money = money % 50000
-  answer.push(parseInt(money / 10000))
-  money = money % 10000
-  answer.push(parseInt(money / 5000))
-  money = money % 5000
-  answer.push(parseInt(money / 1000))
-  money = money % 1000
-  answer.push(parseInt(money / 500))
-  money = money % 500
-  answer.push(parseInt(money / 100))
-  money = money % 100
-  answer.push(parseInt(money / 50))
-  money = money % 50
-  answer.push(parseInt(money / 10))
-  money = money % 10
+  let monetary_unit = 100000;
+  for(let i = 0;i<8;i++){
+    if(i % 2 == 0) monetary_unit = monetary_unit / 2;
+    if(i % 2 == 1) monetary_unit = monetary_unit / 5;
+    answer.push(parseInt(money / monetary_unit))
+    money = money % monetary_unit
+  }
   answer.push(parseInt(money / 1))
-  money = money % 1
   return answer;
 }
 module.exports = problem5;
