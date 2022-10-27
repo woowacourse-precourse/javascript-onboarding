e263b87613d12497148a3c2af668b8ff90e977e2
olo90632951 <olo90632951@gmail.com>
2022-10-27 11:26:27 +0900

    feat: 각 돈의 단위에 맞게 몇 개씩 반환되는지 계산하는 기능 추가 [Problem5-1]
    화폐의 단위가 큰 것부터 나누었음

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  return getCash(money);
 }
+
+const getCash = (money) => {
+  const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return cash.map((currentCash) => {
+    const result = Math.floor(money / currentCash);
+    money = money % currentCash;
+    return result;
+  });
+};
 
 module.exports = problem5;
