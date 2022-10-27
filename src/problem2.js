b743c5c04cff12e5d908bf82b54b76a2c5d9a540
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-10-27 14:41:47 +0900

    feat: 예외처리 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,33 @@
+const VALIDITY = {
+  minLength: 1,
+  maxLength: 1000,
+  string: 'string',
+};
+
+const RESULT = {
+  exception: -1,
+};
+
+function isValidInput(input) {
+  if (input.length < VALIDITY.minLength || input.length > VALIDITY.maxLength) {
+    return false;
+  }
+
+  if (typeof input !== VALIDITY.string) {
+    return false;
+  }
+
+  if (input === input.toUpperCase()) {
+    return false;
+  }
+
+  return true;
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (!isValidInput(cryptogram)) {
+    return RESULT.exception;
+  }
 }
 
 module.exports = problem2;
