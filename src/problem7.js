75320ec68ada27c7ab79e15b63a91b25a07f80e7
Miniling <bsm3925@naver.com>
2022-10-27 23:13:30 +0900

    [P7] Add/todo

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,11 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  // friends에서 user와 친구로 되어있는 사용자는 추천X
+  // friends에서 user와 친구로 되어있는 사용자와 친구인 사용자는 추천O (10점씩)
+  // visitors에서 추천 사용자의 중복횟수 * 3
+  // 추천 점수가 부여된 사용자의 목록을 점수-이름 순으로 정렬
+
   return answer;
 }
 
