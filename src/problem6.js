922275537048640486971bf2fae9744d6e912c6a
catca <kdg96g@gmail.com>
2022-10-27 21:11:47 +0900

    feat: solved problem6
    한글 닉네임 전체를 유니코드로 변경하여 codeArr에 저장
    한글 닉네임을 유니코드로 변경한 후, 연속된 두 글자씩 잘라서 searchArr에
    저장
    codeArr를 shift()로 searchArr에 포함된 자신 닉네임을 빼면서 검색하여
    일치하는 index 받아오기
    index에 해당하는 email 정렬하여 return

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,35 @@
 function problem6(forms) {
-  var answer;
+  const emailArr = forms.map(([email, _]) => email);
+  let codeArr = forms.map(([_, nickname]) => [...nickname].map(item => item.charCodeAt(0)).join('')); // 닉네임 유니코드로 변경
+  let searchArr = forms.map(([_, nickname]) => [...nickname].map(item => String(item.charCodeAt(0)))); // 닉네임을 한 글자씩 유니코드로 변경하여 배열에 저장
+  searchArr = searchArr.map(item => { // 닉네임을 연속된 두 글자로 자른 배열 형태로 저장 ex) '제이엠' => ['제이', '이엠']
+    if(item.length === 1) return [];
+
+    let array = [];
+    for(let i = 0; i < item.length - 1; i++){
+      array.push(item[i] + item[i + 1]);
+    }
+    return array;
+  })
+
+  let indexArr = [];
+  let shiftIndex = 0;
+  searchArr.forEach((item, index) => {
+    codeArr.shift();
+    shiftIndex++;
+    codeArr.forEach((element, codeIndex) => { 
+      item.forEach(str => {
+        if(element.includes(str)){
+          indexArr.push(index);
+          indexArr.push(codeIndex + shiftIndex);
+        }
+      })
+    })
+  })
+  
+  const uniqueArr = [...new Set(indexArr)];
+  let answer = emailArr.filter((_, index) => uniqueArr.includes(index));
+  answer = [...new Set(answer)].sort();
   return answer;
 }
 
