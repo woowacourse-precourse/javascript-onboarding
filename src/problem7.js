3faa340c0ec6e689356cff9747dd9a2fc9b07659
Leejin-Yang <ejin1996@gmail.com>
2022-10-27 16:15:50 +0900

    feat(problem7): user 친구 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,31 @@
+function findUserFriends(user, friends) {
+  const userRelations = friends.filter((friend) => friend.includes(user));
+  const userFriends = userRelations.map((relation) =>
+    relation.find((person) => person !== user)
+  );
+
+  return userFriends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
 
 module.exports = problem7;
+
+function test() {
+  const user = 'mrko';
+  const friends = [
+    ['donut', 'andole'],
+    ['donut', 'jun'],
+    ['donut', 'mrko'],
+    ['shakevan', 'andole'],
+    ['shakevan', 'jun'],
+    ['shakevan', 'mrko'],
+  ];
+
+  console.log(findUserFriends(user, friends));
+}
+
+test();
