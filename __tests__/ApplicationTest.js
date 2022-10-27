7c212aa8e4148c288563332d52a837e49e898e45
heesungjang <heesungj7@gmail.com>
2022-10-27 19:29:03 +1000

    우테코 1주차 과제 7번

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -68,21 +68,21 @@ describe('problem6', () => {
   });
 });
 
-// describe("problem7", () => {
-//   test("case1", () => {
-//     expect(
-//       problem7(
-//         "mrko",
-//         [
-//           ["donut", "andole"],
-//           ["donut", "jun"],
-//           ["donut", "mrko"],
-//           ["shakevan", "andole"],
-//           ["shakevan", "jun"],
-//           ["shakevan", "mrko"],
-//         ],
-//         ["bedi", "bedi", "donut", "bedi", "shakevan"]
-//       )
-//     ).toEqual(["andole", "jun", "bedi"]);
-//   });
-// });
+describe('problem7', () => {
+  test('case1', () => {
+    expect(
+      problem7(
+        'mrko',
+        [
+          ['donut', 'andole'],
+          ['donut', 'jun'],
+          ['donut', 'mrko'],
+          ['shakevan', 'andole'],
+          ['shakevan', 'jun'],
+          ['shakevan', 'mrko'],
+        ],
+        ['bedi', 'bedi', 'donut', 'bedi', 'shakevan']
+      )
+    ).toEqual(['andole', 'jun', 'bedi']);
+  });
+});
--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,47 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const scores = {};
+  const friendsOfUser = getFriends(user, friends);
+
+  friends
+    // 현재 유저 필터
+    .filter((link) => !link.includes(user))
+    // 유저 친구의 관계만 필터
+    .filter(
+      ([friend1, friend2]) =>
+        friend1 in friendsOfUser || friend2 in friendsOfUser
+    )
+    // 유저 친구의 친구 점수 카운트
+    .map(([friend1, friend2]) => (friend1 in friendsOfUser ? friend2 : friend1))
+    .forEach((friend) => {
+      if (friend in scores) {
+        scores[friend] += 10;
+      } else {
+        scores[friend] = 10;
+      }
+    });
+
+  // 타임 라인 방문자 점수 카운트
+  visitors.forEach((visitor) => {
+    if (visitor in friendsOfUser) return;
+    if (visitor in scores) {
+      scores[visitor] += 1;
+    } else {
+      scores[visitor] = 1;
+    }
+  });
+
+  return [...Object.keys(scores)];
 }
+
+const getFriends = (user, friends) =>
+  friends.reduce((friendList, friendLink) => {
+    if (friendLink.includes(user)) {
+      const friend = friendLink.filter((item) => item !== user)[0];
+
+      friendList[friend] = true;
+      return friendList;
+    }
+    return friendList;
+  }, {});
 
 module.exports = problem7;
