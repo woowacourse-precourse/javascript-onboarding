3b67b69f68453599387e3a05cce550fe907b67de
hwanfront <kim015jh@gmail.com>
2022-10-27 22:12:24 +0900

    feat: problem2 연속하는 중복 문자들을 제거하는 함수 구현
    - findIndexWithNotSame함수는 연속하는 중복 문자에서 다른 문자가 처음
      나왔을때 index를 return
    - getRemovedCryptogram 함수는 연속하는 중복 문자가 제거된 문자열을
      return

--- src/problem2.js
+++ src/problem2.js
@@ -9,7 +9,37 @@
  *     2-2-3. i-- 후 removedIndex를 i로, isRemoved를 true로 변경한다.
  * 3-1. isRemoved가 true이면 2번으로
  * 3-2. isRemoved가 false이면 문자열 결과를 return한다 (종료)
- *
+ */
+
+/**
+ * 연속하는 중복 문자에서 다른 문자가 처음나왔을때 index 찾는 함수
+ * @param {string} cryptogram
+ * @param {number} index
+ * @returns {number} charIndex
+ */
+function findIndexWithNotSame(cryptogram, index) {
+  let cnt = 2;
+  while (index + cnt < cryptogram.length) {
+    if (cryptogram[index] !== cryptogram[index + cnt]) {
+      break;
+    }
+    cnt++;
+  }
+  return index + cnt;
+}
+
+/**
+ * 연속하는 중복 문자가 제거된 문자열을 제공하는 함수
+ * @param {string} cryptogram
+ * @param {number} index
+ * @returns {string} removedCryptogram
+ */
+function getRemovedCryptogram(cryptogram, index) {
+  const charIdx = findIndexWithNotSame(cryptogram, index);
+  return cryptogram.substring(0, index) + cryptogram.substring(charIdx);
+}
+
+/**
  * @param {string} cryptogram
  * @returns {string} result
  */
@@ -17,5 +47,11 @@ function problem2(cryptogram) {
   var answer;
   return answer;
 }
+
+console.log(getRemovedCryptogram("browoanoommnaon", 7));
+console.log(getRemovedCryptogram("browoanoommnaon", 9));
+console.log(getRemovedCryptogram("zyelleyz", 3));
+console.log(getRemovedCryptogram("zyellleyz", 3));
+console.log(getRemovedCryptogram("zyellllll", 3));
 
 module.exports = problem2;
