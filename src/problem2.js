e773cdcae3a5c5e8936611b3b2049846263c9a27
sehwan <tpghks9245@gmail.com>
2022-10-27 21:09:17 +0900

    feat: 예외판별기능추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,23 @@
 function problem2(cryptogram) {
   if (exception(cryptogram)) return false;
-  var answer;
-  return answer;
+  
 }
 
-//암호문 길이 판별, 암호문이 소문자인지 판별
+// 암호문 길이 판별, 암호문이 소문자인지 판별
 function exception(cryptogram) {
   if (cryptogram < 1 || cryptogram> 1000 ) return true;
-  const compareCrypto = toLowerCase();
+  const compareCrypto = cryptogram.toLowerCase();
   if (cryptogram !== compareCrypto) return true;
   return false;
 }
+
+
+problem2("browoanoommnaon");
 
 module.exports = problem2;
 
 // 기능들
 // 1. 암호문 길이 판별(1~1000)
 // 2. 알파벳이 소문자인가?
-// 3. 연속하는 중복문자 제거
-// 4. 중복문자 있는지 판별(임시)
+// 3. 중복문자 있는지 판별
+// 4. 연속하는 중복문자 제거
