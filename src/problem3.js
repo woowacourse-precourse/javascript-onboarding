b6fcc1b2d60b844de7d60b5b2a6f19b4705ff945
GuHyeonWoo <ghw9174@gmail.com>
2022-10-27 16:54:24 +0900

    P3/수를 한 자 씩 나누는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,6 +3,12 @@
 // 2. 수를 한 자 씩 나누는 기능
 // 3. 3, 6, 9를 카운트하는 기능
 
+// 수를 한 자 씩 나누는 함수
+function listing(number){
+  // 문자열로 변환 하고 나누어 다시 정수로 변환한 리스트 반환
+  return String(number).split('').map(Number);
+}
+
 // 수를 순회하는 함수
 function circuit(number) {
   // 3, 6, 9의 총 개수
@@ -10,6 +16,8 @@ function circuit(number) {
   // 1부터 number까지 순회
   for(let i = 1; i <= number; i++){
     // 수를 한 자 씩 나누어 카운트 하는 부분
+    const listing_number = listing(i);
+    // 3, 6, 9를 카운트 할 부분
   }
   return count;
 }
