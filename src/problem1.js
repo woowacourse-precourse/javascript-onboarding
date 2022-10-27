864594cb187fd3812f413361f0b0cd084962685c
ashleysheo <ashleysyheo@gmail.com>
2022-10-28 00:08:26 +0900

    feat(problem1): add digits

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,15 @@
+function addDigits(number) {
+  let digits = number; 
+  let digitsSum = 0;
+
+  while (digits > 0) {
+    digitsSum += digits % 10; 
+    digits = Math.floor(digits / 10);
+  }
+
+  return digitsSum;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
