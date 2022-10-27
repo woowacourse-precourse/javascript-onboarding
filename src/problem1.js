da5bbaf5acc0b479604449f9ae0007e6761cc1d7
jinseo <ghkdwlstj99@gmail.com>
2022-10-27 23:19:53 +0900

    테스트 오류 기능 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,35 +1,36 @@
 function problem1(pobi, crong) {
   
-  if(exception == false){  
+  if(exception(pobi, crong) == false){  
     return -1;
   }
+  
   const pobiLeftScore = Math.max(pageSum(pobi[0]),pageMul(pobi[0]));
 
   const pobiRightScore = Math.max(pageSum(pobi[1]),pageMul(pobi[1]));
 
   const crongLeftScore = Math.max(pageSum(crong[0]),pageMul(crong[0]));
 
-  const crongRigthScore = Math.max(pageSum(crong[1]),pageMul(crong[1]));
+  const crongRightScore = Math.max(pageSum(crong[1]),pageMul(crong[1]));
 
   const pobiScore = Math.max(pobiLeftScore, pobiRightScore);
 
-  const crongScore = Math.max(crongLeftScore, crongRigthScore);
+  const crongScore = Math.max(crongLeftScore, crongRightScore);
 
   return ScoreResult(pobiScore,crongScore);
 }
 
 function pageSum(num){
   if (num >= 100){
-  return num / 100 + ((num % 100) / 10) + num % 10 
+    return parseInt(num / 100) + parseInt((num % 100) / 10) + parseInt(num % 10); 
  }
- return (num / 10) + (num % 10);
+ return (parseInt(num / 10) + (num % 10));
 }
 
 function pageMul(num){
   if (num >= 100){
-      return (num/100) * ((num%100)/10) * (num % 10);
+    return parseInt(num/100) * parseInt((num%100)/10) * (num % 10);
   }
-  return (num / 10) * (num % 10);
+  return (parseInt(num / 10) * (num % 10));
 }
 
 function exception(pobi,crong){
@@ -43,13 +44,13 @@ function exception(pobi,crong){
 }
 
 function ScoreResult(pobiScore, crongScore){
-  if (pobiScore > crongScore){
+  if(pobiScore > crongScore){
     return 1;
   }
-  if (pobiScore < crongScore){
+  if(pobiScore < crongScore){
     return 2;
   }
-  if (pobiScore == crongScore){
+  if(pobiScore == crongScore){
     return 0;
   }
 }
