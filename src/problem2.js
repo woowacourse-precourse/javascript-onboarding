95f9d09f76e3452dcc02101ff309a3f7d318d57c
yeonsuBeak <soothbugger@gmail.com>
2022-10-27 14:55:10 +0900

    refactor(problem2): problem2 함수 내에 모든 함수 밖으로 위치 변경

--- src/problem2.js
+++ src/problem2.js
@@ -2,62 +2,61 @@ function problem2(cryptogram) {
   const result = checkErrors(cryptogram);
 
   return result;
-
-  function checkErrors(word) {
-    const MINIMUM_LENGTH = 1;
-    const MAXIMUM_LENGTH = 1000;
-    const MINIMUM_ALPHABET = "a";
-    const MAXIMUM_ALPHABET = "z";
-    const wordLength = word.length;
-
-    if (wordLength < MINIMUM_LENGTH || wordLength > MAXIMUM_LENGTH) {
-      return "문자열의 길이는 최소 1, 최대 1000입니다.";
-    }
-
-    for (let wordIndex = 0; wordIndex < wordLength; wordIndex++) {
-      if (
-        word[wordIndex] < MINIMUM_ALPHABET ||
-        word[wordIndex] > MAXIMUM_ALPHABET
-      ) {
-        return "문자열은 소문자 알파벳으로만 이루어져야 합니다.";
-      }
-    }
-
-    return checkDuplicates(word);
-  }
-
-  function checkDuplicates(word) {
-    for (let wordIndex = 0; wordIndex < word.length; wordIndex++) {
-      if (word[wordIndex] === word[wordIndex + 1]) {
-        return removeDuplicates(word);
-      }
-    }
-    return word;
-  }
-
-  function removeDuplicates(word) {
-    const wordLength = word.length;
-    let removedResult = "";
-    let duplicateAlphabet = "";
-
-    for (let wordIndex = 0; wordIndex < wordLength; wordIndex++) {
-      if (word[wordIndex] === word[wordIndex + 1]) {
-        duplicateAlphabet = word[wordIndex];
-        wordIndex++;
-        continue;
-      }
-
-      if (duplicateAlphabet[wordIndex] === duplicateAlphabet) {
-        duplicateAlphabet = "";
-        wordIndex++;
-        continue;
-      }
-
-      removedResult += word[wordIndex];
-    }
-
-    return checkDuplicates(removedResult);
-  }
 }
 
+function checkErrors(word) {
+  const MINIMUM_LENGTH = 1;
+  const MAXIMUM_LENGTH = 1000;
+  const MINIMUM_ALPHABET = "a";
+  const MAXIMUM_ALPHABET = "z";
+  const wordLength = word.length;
+
+  if (wordLength < MINIMUM_LENGTH || wordLength > MAXIMUM_LENGTH) {
+    return "문자열의 길이는 최소 1, 최대 1000입니다.";
+  }
+
+  for (let wordIndex = 0; wordIndex < wordLength; wordIndex++) {
+    if (
+      word[wordIndex] < MINIMUM_ALPHABET ||
+      word[wordIndex] > MAXIMUM_ALPHABET
+    ) {
+      return "문자열은 소문자 알파벳으로만 이루어져야 합니다.";
+    }
+  }
+
+  return checkDuplicates(word);
+}
+
+function checkDuplicates(word) {
+  for (let wordIndex = 0; wordIndex < word.length; wordIndex++) {
+    if (word[wordIndex] === word[wordIndex + 1]) {
+      return removeDuplicates(word);
+    }
+  }
+  return word;
+}
+
+function removeDuplicates(word) {
+  const wordLength = word.length;
+  let removedResult = "";
+  let duplicateAlphabet = "";
+
+  for (let wordIndex = 0; wordIndex < wordLength; wordIndex++) {
+    if (word[wordIndex] === word[wordIndex + 1]) {
+      duplicateAlphabet = word[wordIndex];
+      wordIndex++;
+      continue;
+    }
+
+    if (duplicateAlphabet[wordIndex] === duplicateAlphabet) {
+      duplicateAlphabet = "";
+      wordIndex++;
+      continue;
+    }
+
+    removedResult += word[wordIndex];
+  }
+
+  return checkDuplicates(removedResult);
+}
 module.exports = problem2;
