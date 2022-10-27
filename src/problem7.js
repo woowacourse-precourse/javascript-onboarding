73b0d5832a60e056db3bf8e414343123d13264ed
Rki0 <pky00823@gmail.com>
2022-10-27 09:50:46 +0900

    feat: prob7. 친구 추천을 위한 점수를 산정하는 함수
    문제 조건에 따라, 친구의 친구는 10점, 방문자는 1점을 얻게됩니다.
    이름과 합산된 점수는 score라는 object에 저장됩니다.

--- src/problem7.js
+++ src/problem7.js
@@ -25,6 +25,25 @@ function getFriendOfFreinds(user, alreadyFriends, friendOfFreinds) {
   });
 }
 
+function getScore(friendOfFreinds, score, visitors) {
+  for (let i = 0; i < friendOfFreinds.length; i++) {
+    // 이미 score 객체에 존재하는 key라면 그 value에 10을 더한다
+    if (score[friendOfFreinds[i]]) {
+      score[friendOfFreinds[i]] += 10;
+    } else {
+      score[friendOfFreinds[i]] = 10;
+    }
+  }
+
+  visitors.forEach((item) => {
+    if (score[item]) {
+      score[item] += 1;
+    } else {
+      score[item] = 1;
+    }
+  });
+}
+
 function problem7(user, friends, visitors) {
   let alreadyFriends = [];
 
@@ -33,6 +52,10 @@ function problem7(user, friends, visitors) {
   let friendOfFreinds = [];
 
   getFriendOfFreinds(user, alreadyFriends, friendOfFreinds);
+
+  let score = {};
+
+  getScore(friendOfFreinds, score, visitors);
 }
 
 module.exports = problem7;
