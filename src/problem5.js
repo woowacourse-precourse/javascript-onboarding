3517cf8a6c57cc6d7e1689519d177e339e06578b
wonkyum-kim <dodo12120@naver.com>
2022-10-27 21:36:42 +0900

    feat: add problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let money_unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < money_unit.length; ++i) {
+    answer.push(Math.floor(money / money_unit[i]));
+    money %= money_unit[i];
+  }
   return answer;
 }
 
