c90056de1c8cbc75beb597c2b704d72e1dfad97a
Ryong-E <think8867@gmail.com>
2022-10-27 16:07:12 +0900

    feat: Currency을 돌며 사용량 만큼 amount배열에 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
+  let total = money;
   const CURRENCY = [50000, 10000, 5000, 1000, 500, 100, 10, 1];
-  return answer;
+  const amount = [];
+  for (let bill of CURRENCY) {
+    const useBillCount = Math.floor(total / bill);
+    if (useBillCount >= 1) {
+      amount.push(useBillCount);
+      total -= bill * useBillCount;
+    } else if (useBillCount < 1) {
+      amount.push(0);
+    }
+  }
+  return amount;
 }
 
 module.exports = problem5;
