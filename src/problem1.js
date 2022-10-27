44ff6533d4c6a35a58ed09f887d3b949859ccb39
jungsmins1 <jungsmins00@gmail.com>
2022-10-27 20:33:40 +0900

    feat: 2~3 과정을 통해 나온 숫자를 비교해 더 큰 숫자를 구하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -13,16 +13,22 @@ function problem1(pobi, crong) {
 
   //페이지 번호의 각 자리 숫자를 모두 더하는 기능
   const addNum = (num) => {
-     let numStr = num.toString().split("");
-     let add = numStr.reduce((prev, curr) => prev += Number(curr), 0);
-     return add
+    let numStr = num.toString().split("");
+    let add = numStr.reduce((prev, curr) => prev += Number(curr), 0);
+    return add;
   }
 
   //페이지 번호의 각 자리 숫자를 모두 곱하는 기능
   const mulNum = (num) => {
     let numStr = num.toString().split("");
-     let mul = numStr.reduce((prev, curr) => prev *= Number(curr), 1);
-     return mul
+    let mul = numStr.reduce((prev, curr) => prev *= Number(curr), 1);
+    return mul;
+  }
+
+  //2~3 과정을 통해 나온 숫자를 비교해 더 큰 숫자를 구하는 기능
+  const maxNum = (num) => {
+    let Score = Math.max(addNum(num[0]), mulNum(num[0]), addNum(num[1]), mulNum(num[1]));
+    return Score;
   }
 }
 
