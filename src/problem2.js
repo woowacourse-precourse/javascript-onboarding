eb7c1e478eb68db75624783741401c045a0abe70
sery <yunsel2@gmail.com>
2022-10-27 18:13:19 +0900

    feat: Add exeption handling feature to PROBLEM2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
   let answer = [];
 
+  if (cryptogram.length < 1 || cryptogram.length > 1001) {
+    alert('길이가 1 이상 1000 이하인 문자열을 입력해주세요');
+    return;
+  }
+
+  if (cryptogram !== cryptogram.toLowerCase()) {
+    alert('소문자를 입력해주세요');
+    return;
+  }
+
   for (const letter of cryptogram) {
     if (answer[answer.length - 1] === letter) {
       answer.pop();
