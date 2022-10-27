a686d85604238e5ac356877e4e44e2b68eecc1ec
Sijun <sionwer4@naver.com>
2022-10-27 20:50:20 +0900

    problem4 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
 function problem4(word) {
-  var answer;
+  let letters = []
+  
+  for(let i = 0; i < word.length; i++){
+    if(word[i] === " "){
+      letters[i] = " ";
+      continue;
+    }
+
+    if(word[i] === word[i].toUpperCase()){
+      letters[i] = String.fromCharCode(155 - word.charCodeAt(i))
+    }else{
+      letters[i] = String.fromCharCode(219 - word.charCodeAt(i))
+    }
+  }
+
+  const answer = letters.join('');
+
   return answer;
 }
 
