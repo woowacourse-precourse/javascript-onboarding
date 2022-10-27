a8f1254b88f3eda9e8312409670ea683d49f2958
seohee0112 <ghdtj7@naver.com>
2022-10-27 10:29:25 +0900

    feat : problem4 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let upperData = "ABCDEFGHIGKLMNOPQRSTUVWXYZ";
+  let lowerData = upperData.toLowerCase();
+
+  upperData = upperData.split("");
+  lowerData = lowerData.split("");
+
+  word = word.split("");
+
+  word = word.map((alpha) => {
+    if ("a" <= alpha && alpha <= "z")
+      return lowerData[lowerData.length - 1 - lowerData.indexOf(alpha)];
+    else if ("A" <= alpha && alpha <= "Z")
+      return upperData[upperData.length - 1 - upperData.indexOf(alpha)];
+
+    return alpha;
+  });
+  return word.join("");
 }
 
 module.exports = problem4;
