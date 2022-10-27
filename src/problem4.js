697b6cf6409bb00a198dcfe3f052f1e6b6396c4d
SangDon <adoim@naver.com>
2022-10-28 00:47:16 +0900

    feat : problem4.js 문자를 받으면 배열로 변환하는 함수

--- src/problem4.js
+++ src/problem4.js
@@ -4,3 +4,12 @@ function problem4(word) {
 }
 
 module.exports = problem4;
+
+//코드값 처리를 위한 상수 선언
+const ASCII_UPPER_MAX = 155;
+const ASCII_LOW_MAX = 219;
+
+//문자를 배열로 반환하는 함수
+function stringToArray(word) {
+  return word.split("");
+}
