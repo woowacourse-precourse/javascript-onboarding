a63c7b9fe0a7286dd638d4357176bf69580b0022
haesa <xdhaesa@gmail.com>
2022-10-27 13:35:22 +0900

    problem5 출금액의 화폐 개수 구하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,8 +2,17 @@ function getCurrencyNumber(money, type) {
   return Math.floor(money / type);
 }
 
+function withdrawMoney(money) {
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return currency.reduce((result, type) => {
+    result.push(getCurrencyNumber(money, type));
+    money %= type;
+    return result;
+  }, []);
+}
+
 function problem5(money) {
-  var answer;
+  const answer = withdrawMoney(money);
   return answer;
 }
 module.exports = problem5;
