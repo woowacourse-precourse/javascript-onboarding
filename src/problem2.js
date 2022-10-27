61d0c63f4fb333eabdf979c444d467ceddd3d250
kknyapple <kknyapple@gmail.com>
2022-10-28 04:01:49 +0900

    problem2 - 주석 및 코드정리

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,21 @@
 const removeDuplicate = (cryptogram) => {
   let strArr = [...cryptogram];
-  let blank = [];
+  let answer = [];
   for (let i = 0; i < cryptogram.length; i++) {
-    if (strArr[i] != strArr[i + 1]) {
-      if (strArr[i] != strArr[i - 1])
-        if (strArr[i] == blank[blank.length - 1]) blank = blank.slice(0, -1);
-        else {
-          blank.push(strArr[i]);
-        }
-    } else i = i + 1;
+    if (strArr[i] == strArr[i + 1]) {
+      i = i + 1; //현재 문자와 다음 문자가 같으면 다음 문자까지 answer 삽입 x
+    } else {
+      if (strArr[i] == strArr[i - 1])
+        //연속한 중복 문자가 3개 이상일 경우를 위해 이전 문자와 비교
+        continue;
+      else {
+        if (strArr[i] == answer[answer.length - 1])
+          answer = answer.slice(0, -1); //answer 마지막과 같으면 삭제
+        else answer.push(strArr[i]); //answer에 현재 문자 삽입
+      }
+    }
   }
-  return blank.join("");
+  return answer.join("");
 };
 
 function problem2(cryptogram) {
