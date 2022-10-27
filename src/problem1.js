8ab645a6ccab03a9b395eab0d7dc3bf2364272d4
wang-yurin <king_1994@naver.com>
2022-10-27 09:30:00 +0900

    Feat: 페이지 유효성 검사

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,9 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function isValidPage(pages) {
+  if (pages.length !== 2) return false;
+  if (pages[1] - pages[0] !== 1) return false;
+  return true;
 }
+
+function problem1(pobi, crong) {}
 
 module.exports = problem1;
