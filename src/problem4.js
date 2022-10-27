1bb38adb0751564de74036e1cbadcfec425e8a2d
JH <vvaa277@gmail.com>
2022-10-27 23:25:57 +0900

    Feat : 기능목록 4~5 구현
    #P4

--- src/problem4.js
+++ src/problem4.js
@@ -1,9 +1,16 @@
 function problem4(word) {
   let lesson = word.toUpperCase().split("");
+  let frog = [];
+
   const normArr = Array.from({ length: 26 }, (v, i) =>
     String.fromCharCode(i + 65)
   );
   const reverseArr = [...normArr].reverse();
+
+  for (let t = 0; t < lesson.length; t++) {
+    const index = normArr.indexOf(lesson[t]);
+    frog.push(index);
+  }
 }
 
 module.exports = problem4;
