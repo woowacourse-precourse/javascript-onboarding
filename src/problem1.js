639e088e9854cd58fe11d18c0470b58816f6563a
Sunghyun627 <kksscc4@gmail.com>
2022-10-27 21:07:11 +0900

    fix: getProductOfDigits 함수에서 잘못 표기한 tosplit 메서드를 올바른 배열 메서드인  split으로 변경

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,7 @@
 /*
   구현할 기능 목록
   [O] 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인지 확인하는 기능 | return 값 : true/false | 함수명 : isConsecutiveNumbers    
-  [O] 페이지 번호의 각 자리 숫자를 모두 더하는 기능 | return 값 : 각 자리 숫자의 합 
+  [O] 페이지 번호의 각 자리 숫자를 모두 더하는 기능 | return 값 : 각 자리 숫자의 합 | 함수명 :  
   [O] 페이지 번호의 각 자리 숫자를 모두 곱하는 기능 | return 값 : 각 자리 숫자의 곱 | 함수명 : getProductOfDigits
   [O] 계산된 값(인자로 주어진 값) 중 가장 큰 값을 구하는 기능 | return 값 : 가장 큰 수 | 함수명 : getMaxScore 
   [O] 점수를 비교해 게임의 결과를 return 하는 기능 | return 값 : 0(무승부)/1(포비 승)/2(크롱 승) | 함수명 : getGameResult
@@ -30,7 +30,7 @@ function getSumOfDigits(number) {
 function getProductOfDigits(number) {
   return number
     .toString()
-    .tosplit('')
+    .split('')
     .reduce((accProduct, curDigit) => accProduct * curDigit, 1);
 }
 
