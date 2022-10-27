03f0621a1c71ff583fc35da138ec497c63651518
An Sol <apdlvmf486@gmail.com>
2022-10-27 13:42:08 +0900

    4번: 문자 변환 함수 changeStr 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  function changeStr(letter) {
+    let num = 0;
+    if (letter !== letter.toUpperCase()){
+      num = 25 - (letter.charCodeAt(0) - 'a'.charCodeAt(0)) * 2;
+    }else{
+      num = 25 - (letter.charCodeAt(0) - 'A'.charCodeAt(0)) * 2;
+    }
+    return String.fromCharCode(letter.charCodeAt(0) + num); 
+  }
 }
 
 module.exports = problem4;
