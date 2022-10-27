1a1b13e61048af52c2d64a2f6e1ccd325c8aceac
heoputer <hjw1753@gmail.com>
2022-10-27 23:33:35 +0900

    feat : 알파벳 반전 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = [];
+  const alphabet = [...'abcdefghijklmnopqrstuvwxyz'];
+  const frog = [...'zyxwvutsrqponmlkjihgfedcba'];
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = 0; j < alphabet.length; j++) {
+      if (word[i].toLowerCase() === alphabet[j]) {
+        result.push(frog[j]);
+      }
+    }
+  }
+  return result.join('');
 }
 
 module.exports = problem4;
