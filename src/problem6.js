511548d24c8d6cdbe0f035f6ad88fb41856fedac
adultlee <seoungin1228@gmail.com>
2022-10-27 18:18:27 +0900

    feat: [pro6] 입력받는 forms의 중복에 따른 result 함수 구현
    result배열에서 삼항연산자를 사용하여 첫시행에서 서로 중복이 되는경우 두개의 form모두 중복을 처리하도록 예외처리를 진행하였습니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const enteredForms = [forms[0]];
+  const result = isDuplicatedNames(enteredForms, forms[1][1])
+    ? [forms[0][0]]
+    : [];
+
+  for (let i = 1; i < forms.length; i++) {
+    if (isDuplicatedNames(enteredForms, forms[i][1])) result.push(forms[i][0]);
+    enteredForms.push(forms[i]);
+  }
+
+  return makeResultArray(result);
+}
+
+function isDuplicatedNames(enteredForms, name) {
+  const twoLetterArray = generateTwoLetterArray(name);
+
+  for (let i = 0; i < enteredForms.length; i++) {
+    if (checkDuplication(enteredForms[i][1], twoLetterArray)) return true;
+  }
+  return false;
+}
+
 function checkDuplication(string, inputArray) {
   for (let i = 0; i < inputArray.length; i++) {
     if (string.includes(inputArray[i])) return true;
@@ -19,4 +39,5 @@ function generateTwoLetterArray(string) {
 function makeResultArray(array) {
   return Array.from(new Set(array)).sort();
 }
+
 module.exports = problem6;
