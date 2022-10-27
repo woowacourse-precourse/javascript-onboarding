a4796cce1c43b560aafc3aad9c93cabfd7713bf9
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-27 21:15:46 +0900

    [problem1] Add function 'pageGameApp' for function process management and Add validation condition to function 'validationArr'

--- src/problem1.js
+++ src/problem1.js
@@ -1,14 +1,21 @@
-console.log(problem1([95, 98],[197, 198]));
+console.log(problem1([97, 98],[197, 198]));
 
 function problem1(pobi, crong) {
-  if(validationArr(pobi) && validationArr(crong)){
-    return 0;
+  if(validationArr(pobi) && validationArr(crong)){  
+    return pageGameApp(pobi, crong);  // 두 값이 같으면 0, A가 크면 1, B가 크면 2
   }
   return -1;
 }
 
 function validationArr(arr){
-  return arr[1]-arr[0] == 1?true:false;
+  return arr.length!=2?false:arr[1]-arr[0] == 1?true:false;
+}
+
+function pageGameApp(numArrA,numArrB){
+  let maxNumA;
+  let maxNumB;
+  console.log(numArrA,numArrB);
+  return maxNumA==maxNumB?0:maxNumA>maxNumB?1:2; 
 }
 
 module.exports = problem1;
