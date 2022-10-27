5ca9831817e4d1aea8537ff5602f10b392eead56
Jongco <narira0922@gmail.com>
2022-10-27 13:21:47 +0900

    problem7: 합산한 점수 객체를 2중 배열로 변환 후 정렬
    이미 친구인 목록을 배열에서 제거해야 했기에 이미 친구인 목록으로 answer를 필터링 후
    sort를 이용해 점수별로 정렬하고, 점수가 같다면 이름순으로 정렬할 수 있는 로직 구현

--- src/problem7.js
+++ src/problem7.js
@@ -2,17 +2,23 @@ function problem7(user, friends, visitors) {
   /**
    * 1. 이미 친구인 사람 + 본인 목록 생성 및 중복 제거 : alreadyFriendList
    * 2. friends 이중 배열 분해 후 alreadyFriendList에 있는 값들 제거
-   * 3. 겹치는 갯수 세어서 객체로 생성 {name: 'jongco', friend: 2, visit: 1, score: 21}
-   * 4. visitor도 똑같이 세어서 객체로 생성 {name: 'jongco', friend: 2, visit: 1, score: 21}
+   * 3. 겹치는 갯수 세어서 객체로 생성 {'jongco':30, 'andole': 20}
+   * 4. visitor도 위의 객체와 점수 합산 {'jongco':33, 'andole': 22}
    * 5. 점수 별로 나열 후 score가 0인 것 제외해서 배열에 이름 반환
    */
   const alreadyFriendList = [...new Set(friends.filter((friend) => friend.includes(user)).flat())];
   let answer = {};
   friends.flat().forEach((name) => (answer[name] = (answer[name] || 0) + 10));
   visitors.forEach((name) => (answer[name] = (answer[name] || 0) + 1));
-  for (let person in answer) {
-    console.log(person);
-  }
+  answer = Object.entries(answer)
+    .filter(([person, _]) => !alreadyFriendList.includes(person))
+    .sort((a, b) => {
+      if (a[1] === b[1]) {
+        return a[0] < b[0] ? -1 : a[0] > b[0] ? 1 : 0;
+      }
+
+      return b[1] - a[1];
+    });
   return answer;
 }
 console.log(
@@ -21,8 +27,11 @@ console.log(
     [
       ["donut", "andole"],
       ["donut", "jun"],
+      ["donut", "bb"],
+      ["donut", "bb"],
       ["donut", "mrko"],
       ["shakevan", "andole"],
+      ["shakevan", "bedi"],
       ["shakevan", "jun"],
       ["shakevan", "mrko"],
     ],
