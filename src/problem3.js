70506b5f0ac24e6c9f430f918b9ca9ea1d738eea
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-28 03:00:29 +0900

    feat: (P3) 숫자 내 3,6,9 포함 개수 계산

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  // 1~number까지 순회 기능
+}
+
+// 3,6,9 포함 판별 기능
+function isIncludeThreeSixNine(number) {
+  const numberToString = number.toString();
+  let currentDigit = "";
+  let count = 0;
+
+  for (n of numberToString) {
+    currentDigit = n;
+    if (currentDigit === "3" || currentDigit === "6" || currentDigit === "9") {
+      count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = problem3;
