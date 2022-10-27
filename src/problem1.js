3202a9e83e142df61d6f0644c4d9ca129a5ee9af
김희진 <choha941108@gmail.com>
2022-10-28 00:10:19 +0900

    (1번) 반복문 사용하여 보기 좋게 코드 변경

--- src/problem1.js
+++ src/problem1.js
@@ -1,17 +1,19 @@
 function problem1(pobi, crong) {
   console.log("기본값",pobi, crong)
   const N = 2;
-  let pobiplus_1 = 0
-  let pobiplus_2 = 0
-  let pobimultiply_1 = 1
-  let pobimultiply_2 = 1
-  let crongplus_1 = 0
-  let crongplus_2 = 0
-  let crongmultiply_1 = 1
-  let crongmultiply_2 = 1
 
-  let pobinums = []
-  let crongnums = []
+  //실패 조건 검사
+  if(
+    (pobi[0] > 400 || pobi[1] > 400 || crong[0] > 400 || crong[1] > 400)//책은 총 400쪽 넘으면 True
+    || (1 == pobi[0]|| pobi[1]== 400 || 1 == crong[0] || crong[1] == 400)//시작/마지막면 이면 True
+    || (pobi[0]%2 != 1 || crong[0]%2 != 1)//왼쪽이 홀수 아니면 True 
+    || (pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1])//오른쪽은 왼쪽 + 1 아니면 True
+  ){
+    return -1
+  }
+
+  let pobinums = []  //포비에 받은 숫자 하나씩 분해
+  let crongnums = []  //크롱에 받은 숫자 하나씩 분해
   for (i = 0 ; i < N ; i++) {
     let _pobinums = [];
     
@@ -29,101 +31,74 @@ function problem1(pobi, crong) {
       _crongnums.push(crong[i] % 10);  
       crong[i] = Math.floor(crong[i] / 10);  
     } while (crong[i] > 0);
+
     console.log("크롱분해",_crongnums)
     crongnums.push(_crongnums)
   }
+  //포비/크롱 분해 완료
+  console.log("포비확인",pobinums)
+  console.log("크로확인",crongnums)
+
+
+  let pobi_plus = [0, 0];
+
   for ( i = 0 ; i < pobinums.length ; i ++) {
-    for ( j = 0 ; j <= pobinums[i].length ; j ++)
-      pobiplus_1 = pobiplus_1 + pobinums[j][i]
-      pobiplus_2 = pobiplus_2 + pobinums[j][i]
-      pobimultiply_1 = pobimultiply_1 * pobinums[j][i]
-      pobimultiply_2 = pobimultiply_2 * pobinums[j][i]
-  }
-  
-  for ( i = 0 ; i < crongnums.length ; i ++) {
-    for ( j = 0 ; j <= crongnums[i].length ; j ++){
-      crongplus_1 = crongplus_1 + crongnums[j][i]
-      crongplus_2 = crongplus_2 + crongnums[j][i]
-      crongmultiply_1 = crongmultiply_1 * crongnums[j][i]
-      crongmultiply_2 = crongmultiply_2 * crongnums[j][i]
+    for (j = 0; j < pobinums[i].length; j++) {
+      //i 입력 받은 두 숫자 중 몇번째 숫자를 선택할지
+      //j 선택한 숫자 중 몇번째 자리 숫자를 선택할지
+      pobi_plus[i] += pobinums[i][j];
     }
   }
 
-  if (pobiplus_1 < pobiplus_2){
-    pobiplus = pobiplus_2
-  } else if (pobiplus_1 > pobiplus_2) {
-    pobiplus = pobiplus_1
-  } else if (pobiplus_1 == pobiplus_2) { 
-    pobiplus = pobiplus_1
-  }
+  console.log("포비 더하기_1",pobi_plus[0])
+  console.log("포비 더하기_2",pobi_plus[1])
 
-  if (pobimultiply_1 < pobimultiply_2){
-    pobimultiply = pobimultiply_2
-  } else if (pobimultiply_1 > pobimultiply_2) {
-    pobimultiply = pobimultiply_1
-  } else if (pobimultiply_1 == pobimultiply_2) { 
-    pobimultiply = pobimultiply_1
-  }
+  let crong_plus = [0,0]
 
-  if (crongplus_1 < crongplus_2){
-    crongplus = crongplus_2
-  } else if (crongplus_1 > crongplus_2) {
-    crongplus = crongplus_1
-  } else if (crongplus_1 == crongplus_2) { 
-    crongplus = crongplus_1
+  for ( i = 0 ; i < crongnums.length ; i++) {
+    for ( j = 0 ; j < crongnums[i].length ; j++) {
+      crong_plus[i] += crongnums[i][j];
+    }
   }
+  console.log("크롱 더하기_1",crong_plus[0])
+  console.log("크롱 더하기_2",crong_plus[1])
 
-  if (crongmultiply_1 < crongmultiply_2){
-    crongmultiply = crongmultiply_2
-  } else if (pcrongmultiply_1 > crongmultiply_2) {
-    crongmultiply = crongmultiply_1
-  } else if (crongmultiply_1 == crongmultiply_2) { 
-    crongmultiply = crongmultiply_1
+  let pobi_multiply = [1,1]
+
+  for ( i = 0 ; i < pobinums.length ; i++) {
+    for ( j = 0 ; j < pobinums[i].length ; j++) {
+      pobi_multiply[i] *= pobinums[i][j];
+    }
   }
+  console.log("포비 곱하기_1",pobi_multiply[0])
+  console.log("포비 곱하기_2",pobi_multiply[1])
+  
+  let crong_multiply = [1,1]
 
-  if (pobiplus < pobimultiply) {
-    pobione = pobimultiply
-  } else if (pobiplus > pobimultiply) {
-    pobione = pobiplus
-  } else if (pobiplus == pobimultiply) {
-    pobione = pobiplus
+  for ( i = 0 ; i < crongnums.length ; i++) {
+    for ( j = 0 ; j < crongnums[i].length ; j++) {
+      crong_multiply[i] *= crongnums[i][j];
+    }
   }
+  console.log("크롱 곱하기_1",crong_multiply[0])
+  console.log("크롱 곱하기_2",crong_multiply[1])
 
-  if (crongplus < crongmultiply) {
-    crongone = crongmultiply
-  } else if (crongplus > crongmultiply) {
-    crongone = pobiplus
-  } else if (crongplus == crongmultiply) {
-    crongone = crongplus
+  let pobi_max = Math.max(Math.max(...pobi_plus), Math.max(...pobi_multiply))
+  let crong_max = Math.max(Math.max(...crong_plus), Math.max(...crong_multiply))
+
+  console.log(pobi_max, crong_max)
+
+  let res
+  if ( pobi_max < crong_max) {
+    res = 2
+  }else if ( pobi_max > crong_max) {
+    res = 1
+  } else if ( pobi_max == crong_max) {
+    res = 0
   }
+  
+  return res
 
-  if ( pobione < crongone) {
-    console.log(2)
-  }else if ( pobione > crongone) {
-    console.log(1)
-  } else if ( pobione == crongone) {
-    console.log(0)
-  } else {
-    console.log(-1)
-  }
-
-  console.log("중간확인",pobinums[1][0]);
-  console.log("포비확인",pobinums)
-  console.log("크로확인",crongnums)
-  console.log("포비 더하기_1",pobiplus_1)
-  console.log("포비 더하기_2",pobiplus_2)
-  console.log("크롱 더하기_1",crongplus_1)
-  console.log("크롱 더하기_2",crongplus_2)
-  console.log("포비 곱하기_1",pobimultiply_1)
-  console.log("포비 곱하기_2",pobimultiply_2)
-  console.log("크롱 곱하기_1",crongmultiply_1)
-  console.log("크롱 곱하기_2",crongmultiply_2)
-  console.log("포비더하기비교",pobiplus)
-  console.log("포비곱하기비교",pobimultiply)
-  console.log("크롱더하기비교",crongplus)  
-  console.log("크롱곱하기비교",crongmultiply)
-  console.log("포비 더하기/곱하기",pobione)
-  console.log("크롱 더하기/곱하기",crongone)
 }
 
 
