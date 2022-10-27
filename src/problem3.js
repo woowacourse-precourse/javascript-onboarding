64d8526080a5aa2fa4a24529db84dd332bb47f51
sangpyokim <rlatkdvy12@naver.com>
2022-10-27 19:25:28 +0900

    3 problem solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let answer = 0
+
+  for (let i = 1; i <= number; i++) {
+    i = i.toString()
+    for (let j = 0; j < i.length; j++) {
+      if (i[j] === '3' || i[j] === '6' || i[j] === '9') answer+=1
+    }
+  }
+  return answer
 }
 
 module.exports = problem3;
