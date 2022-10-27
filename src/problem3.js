277960820a54fa670050d5ceac3899e8e4ae1a45
saram <yujun1018@naver.com>
2022-10-28 02:17:34 +0900

    [문제3] feat: 해당 숫자들에서 3, 6, 9의 개수만큼 횟수를 증가하는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  for (let i = 1; i <= number; i++) {}
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    count +=
+      i.toString().split('3').length +
+      i.toString().split('6').length +
+      i.toString().split('9').length -
+      3;
+  }
+  return count;
 }
 
 module.exports = problem3;
