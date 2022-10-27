18991a6b600e84a03d8731e6636fa33412692319
Siby1lA <psb4644@gmail.com>
2022-10-27 23:44:20 +0900

    feat: 예외처리 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -8,7 +8,15 @@ function getMaxScore(pages) {
   const [leftPage, rightPage] = pages;
   return Math.max(cal(leftPage), cal(rightPage));
 }
+function exception(pages) {
+  const [leftPage, rightPage] = pages;
+  if (leftPage % 2 === 0 || rightPage % 2 === 1) return false;
+  if (leftPage <= 1 || rightPage >= 400) return false;
+  if (rightPage - leftPage !== 1) return false;
+  return true;
+}
 function problem1(pobi, crong) {
+  if (!exception(pobi) || !exception(crong)) return -1;
   const pobiMaxScore = getMaxScore(pobi);
   const crongMaxScore = getMaxScore(crong);
   if (pobiMaxScore > crongMaxScore) return 1;
