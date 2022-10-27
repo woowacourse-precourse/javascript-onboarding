c3170a51b4c7bf540e78bae22a11ba73ea29461d
eunha <dmsgk2323@naver.com>
2022-10-27 20:21:20 +0900

    3번 문제 조건문과 반복문으로 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
-  return answer;
-}
+  let count = 0;
+  for (let i = 1; i < number + 1; i++) {
+    let num = i.toString();
 
+    for (let j = 0; j < num.length; j++) {
+      if (num[j].includes("3")) {
+        count += 1;
+      }
+      if (num[j].includes("6")) {
+        count += 1;
+      }
+      if (num[j].includes("9")) {
+        count += 1;
+      }
+    }
+  }
+  return count;
+}
 module.exports = problem3;
