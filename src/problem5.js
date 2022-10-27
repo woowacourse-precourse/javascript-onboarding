991d67d2d68181a5da10c513775e9b06ebb7a570
Ryong-E <think8867@gmail.com>
2022-10-27 16:07:54 +0900

    fix: CURRENCY 누락값 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,6 @@
 function problem5(money) {
   let total = money;
-  const CURRENCY = [50000, 10000, 5000, 1000, 500, 100, 10, 1];
+  const CURRENCY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const amount = [];
   for (let bill of CURRENCY) {
     const useBillCount = Math.floor(total / bill);
