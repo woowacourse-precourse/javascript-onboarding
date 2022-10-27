1a526c32159eb121b3e313a2e166ddb54cde03ed
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-28 00:43:26 +0900

    feat(문제5): 돈을 단위 당 나눈 값을 반환하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,11 @@
 const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  var answer;
+  let answer = MONEY_UNIT.map(unit => { 
+    let num = Math.trunc(money / unit);
+    money %= unit;
+    return num;
+  });
   return answer;
 }
 
