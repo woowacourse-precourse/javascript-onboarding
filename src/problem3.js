d7abed8634f057d08d319cdaa86efd8c6c81ef52
inthhh <glass005533@gmail.com>
2022-10-27 16:44:48 +0900

    p3_count

--- src/problem3.js
+++ src/problem3.js
@@ -2,7 +2,10 @@ function problem3(number) {
   var answer;
   var clap = 0;
   for(let i=0; i<=number; ++i){
-    
+    let num = i.toString();
+    for(let j=0; j<num.length; ++j){
+      if(num[j] == 3 || num[j] == 6 || num[j] == 9) clap++;
+    }
   }
   answer = clap;
 
