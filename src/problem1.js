4f9b1dce793477c767a25694d19abd96b8577b8f
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-27 23:44:48 +0900

    feat: PROBLEM 1 점수 최댓값 설정 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -10,6 +10,20 @@ function problem1(pobi, crong) {
     return page.toString().split('').reduce((a, b) => a * Number(b), 1);
   };
 
+  const pobiMaxScore = Math.max(
+    getSum(pobiLeftPage),
+    getSum(pobiRightPage),
+    getMultiplication(pobiLeftPage),
+    getMultiplication(pobiRightPage)
+  );
+
+  const crongMaxScore = Math.max(
+    getSum(crongLeftPage),
+    getSum(crongRightPage),
+    getMultiplication(crongLeftPage),
+    getMultiplication(crongRightPage)
+  );
+
   const check = (arr) => {
     if (arr.length !== 2) return false;
     if (arr[0] + 1 !== arr[1]) return false;
