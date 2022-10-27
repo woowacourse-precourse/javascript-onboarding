d6ef0457f8d01714706b509e221f20a9cfe2c571
foundaway98 <foundaway98@naver.com>
2022-10-28 02:16:40 +0900

    출금 금액 연산 기능 구현 및 solution 작성 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const MONEY_ARRAY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < MONEY_ARRAY.length; i++) {
+    if (money == 0) {
+      break;
+    }
+    answer[i] = parseInt(money / MONEY_ARRAY[i]);
+    money = money % MONEY_ARRAY[i];
+  }
   return answer;
 }
 
