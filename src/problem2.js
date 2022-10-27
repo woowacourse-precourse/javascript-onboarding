f217468b4f54c6c98a409b9114a3c8cac4bcb91b
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-27 12:29:06 +0900

    feat: cryptogram의 중복 인덱스를 return하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function problem2(cryptogram) {}
+
+const getDuplicateIndex = cryptogram => {
+	const duplicateIndex = new Array(cryptogram.length).fill(false)
+
+	for (let i = 1; i < cryptogram.length; i++) {
+		if (cryptogram[i - 1] === cryptogram[i]) {
+			duplicateIndex[i - 1] = true
+			duplicateIndex[i] = true
+		}
+	}
+	return duplicateIndex
 }
 
-module.exports = problem2;
+module.exports = problem2
