78e2d2a27d15dbaa27160be512f03f1102874510
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 15:03:13 +0900

    Revert "feat: [문제2] cryptogram 예외사항 추가"
    This reverts commit 6b538bfe5a424ba755d19fce60a6c28651b801c1.

--- src/problem2.js
+++ src/problem2.js
@@ -1,47 +1,32 @@
 function problem2(cryptogram) {
-  const isLowerCase = (cryptogram) => {
-    const check = cryptogram.replace(/[^a-z]/, '');
-    return cryptogram === check;
-  };
+  if (cryptogram.length === 0) return cryptogram;
+  const decrypted = [];
+  let pointer = '';
+  let isContinious = false;
+  [...cryptogram].forEach((character) => {
+    if (!isContinious) {
+      pointer = decrypted.pop() || '';
+    }
 
-  if (
-    !(cryptogram.length >= 1 && cryptogram.length <= 1000) ||
-    !isLowerCase(cryptogram)
-  ) {
-    throw new Error(
-      'cryptogram은 길이가 1이상이고 1000이하인 소문자 알파벳 문자열이어야한다'
-    );
+    if (pointer === character) {
+      console.log(pointer, character);
+      isContinious = true;
+    } else {
+      if (!isContinious) {
+        decrypted.push(pointer);
+      }
+      decrypted.push(character);
+      isContinious = false;
+    }
+  });
+
+  const decryptogram = decrypted.join('');
+
+  if (decryptogram === cryptogram) {
+    return cryptogram;
   }
 
-  const decrypto = (cryptogram) => {
-    if (cryptogram.length === 0) return cryptogram;
-    const decrypted = [];
-    let pointer = '';
-    let isContinious = false;
-    [...cryptogram].forEach((character) => {
-      if (!isContinious) {
-        pointer = decrypted.pop() || '';
-      }
-
-      if (pointer === character) {
-        isContinious = true;
-      } else {
-        if (!isContinious) {
-          decrypted.push(pointer);
-        }
-        decrypted.push(character);
-        isContinious = false;
-      }
-    });
-    const decryptogram = decrypted.join('');
-
-    if (decryptogram === cryptogram) {
-      return cryptogram;
-    }
-    return decrypto(decryptogram);
-  };
-
-  return decrypto(cryptogram);
+  return problem2(decryptogram);
 }
-
-module.exports = problem2;
+problem2('good'); //?
+// module.exports = problem2;
