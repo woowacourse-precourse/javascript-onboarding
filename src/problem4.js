95a476d517d79cffe8dde33d251826be4d765981
wang-yurin <king_1994@naver.com>
2022-10-27 13:33:26 +0900

    Feat: problem4 문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const wordArr = [...word];
+  const newArr = [];
+  wordArr.forEach((item) => {
+    let ascii = item.charCodeAt();
+    if (ascii >= 65 && ascii <= 90) {
+      return newArr.push(String.fromCharCode(155 - ascii));
+    } else if (ascii >= 97 && ascii <= 122) {
+      return newArr.push(String.fromCharCode(219 - ascii));
+    } else {
+      return newArr.push(String.fromCharCode(ascii));
+    }
+  });
+  return newArr.join("");
 }
 
 module.exports = problem4;
