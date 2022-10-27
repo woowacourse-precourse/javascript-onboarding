10a79d66712c53a73a664a0bac2eb8b0138b639f
MJ <mj632900@gmail.com>
2022-10-27 15:57:05 +0900

    feat : 중복값 삭제후 문자열 반환하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -15,6 +15,17 @@ function getStartIndex(str) {
     return -1;
 }
 
+function deleteStr(str) {
+    const start = getStartIndex(str);
+    if (start === -1) {
+        return str;
+    }
+    const end = getEndIndex(str, start) + 1;
+
+    const dStr = str.split("").slice(start, end).join("");
+    return str.replace(dStr, "");
+}
+
 function problem2(cryptogram) {}
 
 module.exports = problem2;
