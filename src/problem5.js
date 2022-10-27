593f6d40c0f7f4c150c85f79030b8c5e23fe66a1
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-27 14:33:42 +0900

    problem5 반환 count answer에 저장

--- src/problem5.js
+++ src/problem5.js
@@ -1,10 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer = [0,0,0,0,0,0,0,0,0];
   const monetaryUnit = [50000,10000,5000,1000,500,100,50,10,1]
   for (let i=0;i<monetaryUnit.length; i++){
     let moneycount = 0
     moneycount = parseInt(money/monetaryUnit[i])
     money -= moneycount * monetaryUnit[i]
+    answer[i] = moneycount
   }
   return answer;
 }
