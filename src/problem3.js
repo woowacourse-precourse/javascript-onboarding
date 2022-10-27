8226655a586c1cbadc011eae8e3f74035e58006b
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 19:28:41 +0900

    fix(Problem3): 예외기능 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,7 @@
 function problem3(number) {
+  if (number < 1 || number > 10000){
+    return "number의 범위를 확인해주세요";
+  }
   const numberList = Array(number).fill().map((v,i)=>i+1); 
   numberList.map((num, idx)=> {
     numberList[idx] = String(num).replace(/[3,6,9]/g,"*").replace(/[0-9]/g,"");
