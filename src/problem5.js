0cc317f6447c8434e4fc4461179b82f84a02e120
seohee0112 <ghdtj7@naver.com>
2022-10-27 11:01:32 +0900

    feat : problem5 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
-}
+  const moneyData = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const resultMoney = new Array(moneyData.length).fill(0);
 
+  moneyData.forEach((data, idx) => {
+    if (Math.floor(money / data) != 0) {
+      resultMoney[idx] = Math.floor(money / data);
+      money %= data;
+    }
+  });
+
+  return resultMoney;
+}
 module.exports = problem5;
