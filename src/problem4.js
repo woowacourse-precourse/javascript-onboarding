7afc9d2d52359f0bbcc894a6b8e8e4c025e5ab3e
yiju_Kim <kls9939@naver.com>
2022-10-28 01:32:04 +0900

    Solve: Create problem4 business logic

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  // ASCII 코드표로 알파벳 배열을 만들자.
+  const upper = Array.from({length:26}, (_,i) => String.fromCharCode(65+i));
+  const lower = Array.from({length:26}, (_,i) => String.fromCharCode(97+i));
+  let result = ''
+  
+  for (let i = 0; i < word.length; i++) {
+    const codePosition = word[i].charCodeAt();
+    if (codePosition <= 90 && codePosition >= 65) {
+      const index = upper.indexOf(word[i]);
+      result += [...upper].reverse()[index];
+    }
+    else if (codePosition >= 97 && codePosition <= 122) {
+      const index = lower.indexOf(word[i]);
+      result += [...lower].reverse()[index];
+    } else {
+      result += word[i];
+    }
+  }
+  return result;
 }
-
 module.exports = problem4;
