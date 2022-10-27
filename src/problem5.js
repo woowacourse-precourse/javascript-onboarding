8b7549581dc55a4b1c926eeea1228dbe38e32c25
powercording <adoim@naver.com>
2022-10-27 18:58:09 +0900

    feat : problem5 테스트 통과

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,32 @@
 function problem5(money) {
   var answer;
+  answer = moneyChange(money);
   return answer;
 }
 
 module.exports = problem5;
+
+const MONEY_LOW = 1;
+const MONEY_HIGH = 1000000;
+
+function isMoney(money) {
+  if (money < MONEY_LOW || money > MONEY_HIGH) return fasle;
+  if (typeof money === "number") return true;
+  else return false;
+}
+
+function moneyChange(money) {
+  if (!isMoney(money)) {
+    alert("돈을 넣어야 합니다");
+    return;
+  }
+  let tempMoney = money;
+  const moneyStep = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let exchange = [];
+  for (let i = 0; i < moneyStep.length; i++) {
+    const result = Math.floor(tempMoney / moneyStep[i]);
+    exchange[i] = result;
+    tempMoney = tempMoney % moneyStep[i];
+  }
+  return exchange;
+}
