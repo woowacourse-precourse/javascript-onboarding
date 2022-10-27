51bd8ce23934024bec14e0f6427f745c5fdb64af
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-28 02:05:32 +0900

    refactor: 코드 예외사항 발견으로 코드 재구성

--- src/problem2.js
+++ src/problem2.js
@@ -1,19 +1,15 @@
 function problem2(cryptogram) {
-  const seperate_string = [...cryptogram];
-  for (let i = 1; i <= 1000; i++) {
-    remove_duplication(seperate_string);
-    if (seperate_string[0] === seperate_string[1]) return "";
-  }
-  return seperate_string.join("");
-}
+  let answer = [];
+  let seperate_string = [...cryptogram];
 
-function remove_duplication(arr) {
-  for (let i = 0; i < arr.length; i++) {
-    if (arr[i] === arr[i + 1]) {
-      arr.splice(i, 2);
-      return arr;
+  for (let i = 0; i < seperate_string.length; i++) {
+    if (answer[answer.length - 1] === seperate_string[i]) {
+      answer.pop();
+    } else {
+      answer.push(seperate_string[i]);
     }
   }
+  return answer.join("");
 }
 
 module.exports = problem2;
