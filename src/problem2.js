724844b157738409c4a1f5b971b2d3026d8fed16
h2002y <h2002y@naver.com>
2022-10-27 16:29:59 +0900

    feat: Add removeSequence func in problem2.js

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,21 @@
 function problem2(cryptogram) {
+  var answer = cryptogram;
+  answer = cryptogram;
   return answer;
+}
+
+function removeSequence(str){
+  for(let i = 0; i < str.length - 1; i++){
+    let index = i;
+    while(str[i]===str[index + 1]){
+      index++;
+    }
+    if(i!=index){
+      let len = str.length;
+      str = str.slice(0, i) + str.slice(index + 1, len);
+    }
+  }
+  return str;
 }
 
 module.exports = problem2;
