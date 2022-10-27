38412b4c1824f261121ae1e3089a11eff00e3d2a
Dean <yjj052800@gmail.com>
2022-10-27 21:44:04 +0900

    FEAT: [2번] 암호문의 중복되는 문자 찾기 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
   var answer;
+  let resultofC = '';
+  let storeC = cryptogram[0];
+
+  for (let i of cryptogram) {
+    console.log(i);
+    if (i !== storeC) {
+      resultofC += storeC;
+    }
+  }
+  resultofC += storeC;
+  console.log(resultofC);
+   answer = resultofC
+
+
   return answer;
 }
-
+console.log(problem2('browoanoommnaon'));
 module.exports = problem2;
