09872dd878b0ace99558db1e89a22d529dbb1d35
Hypark2018 <hayoungpark2018@gmail.com>
2022-10-28 02:29:03 +0900

    🍎 chore: add descriptions for functions.

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,4 @@
+//pro 1번의 메인 로직을 담당하는 메인 메소드 problem1. (각각 조건에 맞게 -1, 0, 1, 2 값을 반환 시켜준다)
 function problem1(pobi, crong) {
   if (isExceptional(pobi) || isExceptional(crong)) return -1
   else if(getCalculate(pobi) > getCalculate(crong)) return 1
@@ -5,6 +6,7 @@ function problem1(pobi, crong) {
   else if (getCalculate(crong) === getCalculate(pobi)) return 0
 }
 
+//올바르지 않은 인풋 값들에 대한 예외 처리를 해주는 함수.
 function isExceptional(pages){
   const isFirstOrLast = pages[0] <= 1 || pages[1] >= 400
   const isFitOddandEven = pages[0] % 2 !== 1 || pages[1] % 2 !== 0
@@ -14,6 +16,7 @@ function isExceptional(pages){
   if(isFirstOrLast || isFitOddandEven || isFitPageOrder || isRightPageForm) return true
 }
 
+//인풋 값의 각 자릿수를 모두 더한 값 vs 모두 곱한 값 중에서 가장 큰 값을 찾아주는 함수.
 function getCalculate(pages){
   let biggest = 0;
 
