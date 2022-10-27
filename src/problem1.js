c2cdd73215926cb1e03678de083b13c352537904
Sunghyun627 <kksscc4@gmail.com>
2022-10-27 14:17:09 +0900

    구현할 기능 목록 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,12 @@
+/*
+  구현할 기능 목록
+  [ ] 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인 지 확인 하는 기능 | return 값 : true/false    
+  [ ] 페이지 번호의 각 자리 숫자를 모두 더하는 기능 | return 값 : 각 자리 숫자의 합 
+  [ ] 페이지 번호의 각 자리 숫자를 모두 곱하는 기능 | return 값 : 각 자리 숫자의 곱
+  [ ] 인자로 주어진 값 중 가장 큰 수를 구하는 기능 | return 값 : 가장 큰 수
+  [ ] 점수를 비교해 게임의 결과를 return 하는 기능 | return 값 : 0(무승부)/1(포비 승)/2(크롱 승) 
+*/
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
