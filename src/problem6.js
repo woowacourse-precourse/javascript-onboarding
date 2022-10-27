2bcad5af3808b5668d57aa58b8432fdb8d0e2a6e
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-10-27 20:20:00 +0900

    [FEAT] Solve Problem6
    문제6 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
+// 중복 글자 체크
+function checkOverlap(nickname) {
+  const arr = [];
+
+  for (let i = 0; i < nickname.length; i++) {
+    const standard = nickname[i].slice(i, i + 2);
+    const expectSelf = nickname.filter(v => v !== nickname[i]);
+    if (expectSelf.some(v => v.includes(standard))) arr.push(standard);
+  }
+
+  return arr.filter(v => v !== '');
+}
+
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  const answer = [];
+  const nickname = forms.map(([_, id]) => id);
+
+  const OverlapWord = checkOverlap(nickname);
+
+  forms.forEach(([email, name]) => {
+    if (OverlapWord.some(v => name.includes(v))) answer.push(email);
+  })
+
+  return answer.sort();
 }
 
 module.exports = problem6;
