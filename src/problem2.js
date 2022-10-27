eaca0828a08b2a80249833c3170a4780acd14c02
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-28 01:16:36 +0900

    feat: removeDuplicate 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,27 +1,41 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+function problem2(cryptogram) {}
+
+const removeDuplicated = (text) => {
+  const textArray = text.toString().split("");
+  const parsedArray = parseDuplicate(textArray);
+  const resultData = parsedArray.join("");
+
+  return resultData;
+};
 
 module.exports = problem2;
 
+const parseDuplicate = (textArray) => {
+  const resultArray = [...textArray];
+  textArray.reduce((prev, cur, curIndex) => {
+    if (prev === cur) {
+      resultArray[curIndex] = "";
+      resultArray[curIndex - 1] = "";
+    }
+
+    return cur;
+  });
+
+  return resultArray;
+};
+
 /*
-연속된 문자 삭제 후, return
-reduce 사용한 뒤, 앞에서부터 동일한 data 삭제하면 될 듯.
-
-
-//"ababbbuqqk"
-
-구현할 것.
-### reduce 배운거 써먹어보기 
-Array.prototype.reduce()
-(prev, cur, curIndex)
-
-reduce 새로운 배열 생성해서 
-중복된 데이터 전부 삭제.(홀수 경우 생각)
-
-
-- 같을 경우 
-해당 index만 체크해서 
-
-*/
+  구현할 것.
+  ### reduce 배운거 써먹어보기 
+  Array.prototype.reduce()
+  
+  reduce 새로운 배열 생성해서
+  중복된 데이터 전부 삭제.(홀수 경우 생각)
+  
+  동일한 경우 prev, cur값 빈 스트링하고 join하면 될 듯?
+  
+  1. reduce용 배열 생성.
+  2. prev === cur인 경우 해당 배열에 빈 스트링 넣어주기.
+  3. join return
+  4. 중복 없을 때까지 돌려야함.
+  */
