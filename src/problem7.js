fdd4ccbc4e9c369d1a9751df5dadbebad2967768
BB-choi <choibobae0809@gmail.com>
2022-10-27 21:57:23 +0900

    feat: 문제7 - 친구 여부 확인 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -16,9 +16,40 @@ function createFriendsMap(friends) {
   return frinedsMap;
 }
 
+function isAlreadyFriend({ user, friend, friendsMap }) {
+  if (!friendsMap.has(user)) {
+    return false;
+  }
+
+  if (friendsMap.get(user).includes(friend)) {
+    return true;
+  }
+
+  return false;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+
+  const friendsMap = createFriendsMap(friends);
+  console.log(isAlreadyFriend({ user, friendsMap, friend: "jun" }));
+
   return answer;
 }
+
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 
 module.exports = problem7;
