50c818174252b0c3153ef3423e4efa4e5a2898aa
Kimyewon <yewon6282@gmail.com>
2022-10-28 01:45:43 +0900

    221028 문제1, 문제3 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,48 @@
 function problem1(pobi, crong) {
   var answer;
+
+  if(pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1]) return -1;
+
+  let pobiLeft = String(pobi[0]).split("");
+  let pobiRight = String(pobi[1]).split("");
+  let crongLeft = String(crong[0]).split("");
+  let crongRight = String(crong[1]).split("");
+
+  function pageSum(arr) {
+    let sum = 0;
+
+    for(let i=0; i<arr.length; i++) {
+      sum += Number(arr[i]);
+    }
+
+    return sum;
+  }
+
+  function pageMulti(arr) {
+    let multi = 1;
+
+    for(let i=0; i<arr.length; i++) {
+      multi *= arr[i];
+    }
+
+    return multi;
+  }
+
+  let pobiNum = [pageSum(pobiLeft), pageSum(pobiRight), pageMulti(pobiLeft), pageMulti(pobiRight)];
+  pobiNum.sort(function(a,b) {return b-a;});
+  let crongNum = [pageSum(crongLeft), pageSum(crongRight), pageMulti(crongLeft), pageMulti(crongRight)];
+  crongNum.sort(function(a,b) {return b-a;});
+
+  if(pobiNum[0] > crongNum[0]) {
+    answer = 1;
+  } else if(pobiNum[0] < crongNum[0]) {
+    answer = 2;
+  } else if(pobiNum[0] == crongNum[0]) {
+    answer = 0;
+  } else {
+    answer = -1;
+  }
+
   return answer;
 }
 
--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,12 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  const regex = /3|6|9/g;
+
+  for(let i = 1; i<=number; i++) {
+    //match : 정규식과 일치하는 문자열 찾음
+    answer += ((String(i).match(regex)||[]).length);
+  }
+
   return answer;
 }
 
