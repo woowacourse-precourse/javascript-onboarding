41290cfff1fbc99324a833205ecada26d60200f5
yiju_Kim <kls9939@naver.com>
2022-10-28 00:53:24 +0900

    solve: Create problem2 business logic

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+  let str = [...cryptogram];
+  let duplicatedStrPosition = [];
+  while(true) {
+    for (let i = 0; i < str.length - 1; i++){
+      if (str[i] === str[i+1]) {
+        duplicatedStrPosition.unshift(i);
+      }
+    }
+    if (!duplicatedStrPosition.length) break;
+    duplicatedStrPosition.forEach(elem => {
+      str.splice(elem,2)
+    });
+    duplicatedStrPosition = [];
+  }
+  return str.join('');
 }
 
 module.exports = problem2;
