a806a32eeab1de18e70799243377ee1a608f4fe6
GyuriKimm <gyuri.kim.2@stonybrook.edu>
2022-10-27 23:45:12 +0900

    Done with problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = checkDuplicate(cryptogram);
   return answer;
 }
-
+function checkDuplicate(cryptogram){
+  let stack = [];
+  stack.push(cryptogram[0]);
+  let nextchar;
+  
+  for(let i = 1; i < cryptogram.length;i++){
+    nextchar = cryptogram[i];
+    let topElement = stack.length-1;
+    if(stack[topElement]===nextchar){
+      stack.pop();
+    }
+    else{
+      stack.push(nextchar);
+    }
+  }
+  return stack.join('');
+}
 module.exports = problem2;
