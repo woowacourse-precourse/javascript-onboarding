a36b9fee927adee36b5a638c8a2cb470cd5765c7
useon <yoosk4986@naver.com>
2022-10-27 22:17:11 +0900

    feat: [problem1] crong의 점수를 구하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -7,6 +7,14 @@ function problem1(pobi, crong) {
   pobiCalculations.push(String(pobi[1]).split('').map((num) => +num).reduce((a, b) => a + b))
   pobiCalculations.push(String(pobi[1]).split('').map((num) => +num).reduce((a, b) => a * b))
   const pobiScore = pobiCalculations.sort((a, b) => b - a)[0];
+
+  // 2. crong의 각 자리 숫자를 모두 더하거나, 모두 곱해 그 중 가장 큰 수 고르기
+  const crongCalculations = [];
+  crongCalculations.push(String(crong[0]).split('').map((num) => +num).reduce((a, b) => a + b))
+  crongCalculations.push(String(crong[0]).split('').map((num) => +num).reduce((a, b) => a * b))
+  crongCalculations.push(String(crong[1]).split('').map((num) => +num).reduce((a, b) => a + b))
+  crongCalculations.push(String(crong[1]).split('').map((num) => +num).reduce((a, b) => a * b))
+  const crongScore = crongCalculations.sort((a, b) => b - a)[0];
   return answer;
 }
 
