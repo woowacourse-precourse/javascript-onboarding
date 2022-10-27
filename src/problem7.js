33456a57e6ddb778645adf23dd62ed8102a849fe
gilpop8663 <wolfye@naver.com>
2022-10-28 06:07:02 +0900

    Add:[P7]_사용자 아이디 user와 친구의 친구인 관계를 찾아 점수를 더하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -25,7 +25,43 @@ function problem7(user, friends, visitors) {
     }
   }
 
+  let userFriendship = userInfo.get(user).friendship;
+
+  for (let i = 0; i < userFriendship.length; i++) {
+    const realatedUsers = userInfo.get(userFriendship[i]).friendship;
+
+    userInfo.set(userFriendship[i], { friendship: realatedUsers, score: -100 });
+
+    for (let j = 0; j < realatedUsers.length; j++) {
+      if (realatedUsers[j] === user) continue;
+      const realatedUserFriendship = userInfo.get(realatedUsers[j]).friendship;
+      const realatedUserScore = userInfo.get(realatedUsers[j]).score;
+      if (realatedUserScore < 0) continue;
+      userInfo.set(realatedUsers[j], {
+        friendship: realatedUserFriendship,
+        score: realatedUserScore + 10,
+      });
+    }
+  }
+
+  userInfo.forEach((value, key, map) => {
+    console.log(`${key} : ${value.score}`);
+  });
+
   return answer;
 }
+
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 
 module.exports = problem7;
