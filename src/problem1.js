d1e4a4994192a3e54baea4d2818c1e80a14b8ab9
leeuihyun <meta212121@naver.com>
2022-10-28 00:15:33 +0900

    feat : problem1 예외처리

--- src/problem1.js
+++ src/problem1.js
@@ -11,6 +11,7 @@ function getBigNumber(number) {
 }
 
 function getFinalBigNumber(numberArray) {
+  if (!errorHandling(numberArray)) return false;
   const leftBigNumber = getBigNumber(numberArray[0]);
   const rightBigNumber = getBigNumber(numberArray[1]);
   return Math.max(leftBigNumber, rightBigNumber);
@@ -22,10 +23,24 @@ function getResult(pobiNumber, crongNumber) {
   if (pobiNumber === crongNumber) return 0;
 }
 
+function errorHandling(numberArray) {
+  const leftNumber = numberArray[0];
+  const rightNumber = numberArray[1];
+  if (numberArray.length !== 2) return false;
+  if (leftNumber % 2 === 0) return false;
+  if (rightNumber - leftNumber !== 1) return false;
+  if (leftNumber < 1 && leftNumber > 400) return false;
+  if (rightNumber < 1 && rightNumber > 400) return false;
+  if (leftNumber === 1) return false;
+  if (rightNumber === 400) return false;
+  return true;
+}
+
 function problem1(pobi, crong) {
   const pobiNumber = getFinalBigNumber(pobi);
   const crongNumber = getFinalBigNumber(crong);
-  return getResult(pobiNumber, crongNumber);
+  if (pobiNumber && crongNumber) return getResult(pobiNumber, crongNumber);
+  return -1;
 }
 
 module.exports = problem1;
