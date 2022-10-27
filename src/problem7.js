ebf660dec05c94b39b5b3831b5590fab818785db
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-10-28 00:02:10 +0900

    [FEAT] Solve Problem7
    문제7 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,37 @@
+// 정렬을 위한 Object 함수
+function ObjectSortFunction(obj) {
+  return Object.entries(obj)
+    .sort(([keyA, valueA], [keyB, valueB]) => valueA === valueB ? keyA.localeCompare(keyB) : valueB - valueA)
+    .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const obj = {};
+
+  const notFriends = friends.filter(([A, B]) => (A !== user && B !== user));
+  const alredyFriends = friends.filter(([A, B]) => !(A !== user && B !== user));
+
+  const friendList = alredyFriends.flat(Infinity).filter(v => v !== user);
+
+  // 이 부분 추후 수정
+  for (const friend of notFriends) {
+    const [A, B] = friend;
+    if (friendList.includes(A)) {
+      !obj[B] ? obj[B] = 10 : obj[B] += 10;
+    } else if (friendList.includes(B)) {
+      !obj[A] ? obj[A] = 10 : obj[A] += 10;
+    }
+  }
+
+  for (const visit of visitors) {
+    if (!friendList.includes(visit)) {
+      !obj[visit] ? obj[visit] = 1 : obj[visit] += 1;
+    }
+  }
+
+  const sortObj = ObjectSortFunction(obj);
+  const answer = Object.keys(sortObj);
+
   return answer;
 }
 
