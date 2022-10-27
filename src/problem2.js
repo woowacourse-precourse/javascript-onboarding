0619606c94845ca1e85339a7524dd1aedadbe222
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-10-27 14:55:34 +0900

    problem2 - 스택 배열에 임의의 문자열을 넣고, 중복 값이 들어올 경우 스택값을 삭제하면서 직전의 값으로 되돌아가 다음 문자열을 넣는다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
   var answer;
+  let stackArray = [];
+  let top = -1;
+  cryptogram.split("").map((x, index) => {
+    if (stackArray[top] === cryptogram[index]) {
+      top--;
+      console.log(stackArray[top], top, cryptogram[index]); // top이 가리키는 stackArray 값, top의 값, 삭제되어질 값
+      stackArray.pop(index);
+    } else {
+      top++;
+      stackArray.push(x);
+      console.log(stackArray, stackArray[top], top); // stackArray 상태, top이 가리키는 stackArray 값, top의 값
+    }
+    answer = stackArray.join("");
+  });
   return answer;
 }
 
 module.exports = problem2;
+
+// 기능 목록
+// 1. 스택 배열에 임의의 문자열을 넣고, 같은 값이 들어올경우 삭제한다.
