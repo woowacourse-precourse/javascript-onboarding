7d080b60b4a4f9e220ba2e8c5cafd3d69b1498b2
Leeseunghwan7305 <sun1238799@naver.com>
2022-10-27 09:33:33 +0900

    우테코 1번 문제 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,54 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+  if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) {
+    return -1;
+  }
+  //더할떄
+  let pobiPlus = Math.max(
+    ...pobi.map((page) => {
+      page = page + "";
+      return page.split("").reduce((pre, cur) => {
+        return Number(pre) + Number(cur);
+      }, 0);
+    })
+  );
 
+  let crongPlus = Math.max(
+    ...crong.map((page) => {
+      page = page + "";
+      return page.split("").reduce((pre, cur) => {
+        return Number(pre) + Number(cur);
+      }, 0);
+    })
+  );
+  //곱할떄
+  let pobiMul = Math.max(
+    ...pobi.map((page) => {
+      page = page + "";
+      return page.split("").reduce((pre, cur) => {
+        return Number(pre) * Number(cur);
+      }, 1);
+    })
+  );
+
+  let croungMul = Math.max(
+    ...crong.map((page) => {
+      page = page + "";
+      return page.split("").reduce((pre, cur) => {
+        return Number(pre) * Number(cur);
+      }, 1);
+    })
+  );
+
+  let pobiOfScore = Math.max(pobiPlus, pobiMul);
+  let crongOfScore = Math.max(crongPlus, croungMul);
+
+  if (pobiOfScore > crongOfScore) {
+    return 1;
+  } else if (pobiOfScore < crongOfScore) {
+    return 2;
+  } else if (pobiOfScore == crongOfScore) {
+    return 0;
+  }
+}
+console.log(problem1([131, 132], [211, 212]));
 module.exports = problem1;
