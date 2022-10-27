003eb869c3c8bf64b1b2d57fa02ee656d95c9d64
yuna Kim <yuna000804@gmail.com>
2022-10-27 12:26:39 +0900

    feat : 값 비교 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
   var answer;
+  
   return answer;
+}
+
+function compare(a, b) {
+  let maxValue = Math.max(a, b);
+
+  return maxValue;
 }
 
 module.exports = problem1;
