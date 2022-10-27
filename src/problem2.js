41e140d305b42425d450d3bcb2f034a12b0c0938
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 18:04:51 +0900

    feat(Problem2): 연속되는 중복문자를 찾아 삭제 후 문자열 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,17 @@
+function findDup(str){
+  const strArray = [...str];
+  const resultArray = [];
+  for (let i = 0; i < strArray.length; i++){
+    if (strArray[i] === resultArray[resultArray.length -1]){
+      resultArray.pop();
+    }
+    else{
+      resultArray.push(strArray[i]);
+    }
+  }
+  return resultArray.join('');
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
