4145d1d720d40f803c2edc1cd6f6d713260aff53
ShimSungBo <shimdb96@gmail.com>
2022-10-27 15:47:21 +0900

    Feat: [problem3] 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
+
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let countClap = 0;
+  for(let i = 1; i <= number; i++ ) {
+    let arr = Array.from(String(i));
+    arr.forEach(item => {
+      if(item === '3' || item === '6' || item === '9') countClap++
+    })
+  }
+  return countClap;
 }
 
+
+
+
 module.exports = problem3;
+
+
+
