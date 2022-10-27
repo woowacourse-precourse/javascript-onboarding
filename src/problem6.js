ea68e91f2cf901b516c31f71e575f164902f43eb
yeongi <cbg5255@gmail.com>
2022-10-27 16:09:36 +0900

    문제6. 2번 기능 수정, 2글자씩은 출력 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,13 +1,54 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const answer = [];
+  const wordArr = [];
+
+  forms.map((id) => {
+    const result = checkTwoChar(id[1]);
+    if (result) {
+      answer.push(id[0]);
+    }
+  });
+
+  return emailSortAndDupDel(answer);
 }
 
 // 1. 닉네임을 이메일과 분리하는 기능
-function nickSplit(id) {}
-// 2. 같은 글자가 연속적으로 포함하는 닉네임을 검사하는 기능
-function checkTwoChar(nickName) {}
-// 3. 이메일 붙이는 기능
-function emailConcat(id) {}
+function nickSplit(id) {
+  const idArr = id.split("@");
+  return idArr[0];
+}
+
+// 2. 같은 글자가 연속적으로 포함하는 닉네임목록을 생성
+function checkStraitWord(nickName) {
+  const arr = [];
+  let pos = 0;
+  for (let i = 1; i < nickName.length; i++) {
+    pos = 0;
+    while (pos < nickName.length - i) {
+      arr.push(nickName[pos].concat(nickName[pos + 1]));
+      pos++;
+    }
+  }
+  console.log(arr);
+
+  return arr;
+}
+
+checkStraitWord("제이엠");
+
+//3.이메일에서 영어 아이디 기준으로 오름 차순으로 정렬 후 중복은 제거
+function emailSortAndDupDel(arr) {
+  return arr.sort((a, b) => nickSplit(a) - nickSplit(b));
+}
+
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
 
 module.exports = problem6;
