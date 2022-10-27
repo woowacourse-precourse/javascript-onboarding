14e604bb51621e579cffa042324e11fc62b5fb61
sunny <huiseon37@gmail.com>
2022-10-28 02:06:53 +0900

    [problem2] 중복 문자 제거 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,22 @@
+function getDecodingArr(cryptogram) {
+  const cryptogramArr = Array.from(cryptogram);
+  const decodingArr = cryptogramArr.reduce((acc,cur)=>{
+    if(acc.length === 0 || acc[acc.length-1] !== cur){
+      return [...acc, cur];
+    };
+
+    acc.pop();
+    return acc;
+  },[]);
+
+  return decodingArr;
+}
+
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  
+  const decodingArr = getDecodingArr(cryptogram);
+
   return answer;
 }
 
