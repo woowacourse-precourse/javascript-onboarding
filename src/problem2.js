fd52a74348be047229de0e3a640d902c63221055
min <hororonoodle@gmail.com>
2022-10-27 23:57:19 +0900

    feat : add comment about array splice

--- src/problem2.js
+++ src/problem2.js
@@ -3,7 +3,7 @@ function problem2(cryptogram) {
   let cnt = 0;
   while (cnt !== cryptArr.length - 1 && cryptArr.length !== 0) {
     if (cryptArr[cnt] === cryptArr[cnt + 1]) {
-      cryptArr.splice(cnt, 2);
+      cryptArr.splice(cnt, 2); // 현재 문자와 다음 문자가 동일할 경우 배열에서 해당 두 문자를 제거해준다.
       cnt = 0;
     } else {
       cnt++;
