b7d21f122904aa8cc0bd026af977512ee349e4cd
xownsdl0 <xownsdl0@naver.com>
2022-10-27 11:51:56 +0900

    예외사항 구성 - 테스트 완료

--- src/problem1.js
+++ src/problem1.js
@@ -14,61 +14,63 @@ function problem1(pobi, crong) {
   var crongMax = 0;
 
   //예외처리
+  if (pobi[0] - pobi[1] === -1 && crong[0] - crong[1] === -1) {
+    //left
+    var pobiLeftNum = pobi[0];
+    pobiLeftNum = String(pobiLeftNum);
+    for (let i = 0; i < pobiLeftNum.length; i++) {
+      pobiLeftSum += parseInt(pobiLeftNum[i]);
+      pobiLeftMultple *= parseInt(pobiLeftNum[i]);
+    }
 
-  //left
-  var pobiLeftNum = pobi[0];
-  pobiLeftNum = String(pobiLeftNum);
-  for (let i = 0; i < pobiLeftNum.length; i++) {
-    pobiLeftSum += parseInt(pobiLeftNum[i]);
-    pobiLeftMultple *= parseInt(pobiLeftNum[i]);
+    var crongLeftNum = crong[0];
+    crongLeftNum = String(crongLeftNum);
+    for (let i = 0; i < crongLeftNum.length; i++) {
+      crongLeftSum += parseInt(crongLeftNum[i]);
+      crongLeftMultple *= parseInt(crongLeftNum[i]);
+    }
+
+    //right
+    var pobiRightNum = pobi[1];
+    pobiRightNum = String(pobiRightNum);
+    for (let i = 0; i < pobiRightNum.length; i++) {
+      pobiRightSum += parseInt(pobiRightNum[i]);
+      pobiRightMultple *= parseInt(pobiRightNum[i]);
+    }
+
+    var crongRightNum = crong[1];
+    crongRightNum = String(crongRightNum);
+    for (let i = 0; i < crongRightNum.length; i++) {
+      crongRightSum += parseInt(crongRightNum[i]);
+      crongRightMultple *= parseInt(crongRightNum[i]);
+    }
+
+    var pobiValue = [
+      pobiLeftSum,
+      pobiLeftMultple,
+      pobiRightSum,
+      pobiRightMultple,
+    ];
+    var crongValue = [
+      crongLeftSum,
+      crongLeftMultple,
+      crongRightSum,
+      crongRightMultple,
+    ];
+
+    pobiMax = Math.max(...pobiValue);
+    crongMax = Math.max(...crongValue);
+
+    if (pobiMax > crongMax) {
+      answer = 1;
+    } else if (pobiMax < crongMax) {
+      answer = 2;
+    } else if (pobiMax === crongMax) {
+      answer = 0;
+    }
+  } else {
+    answer = -1;
   }
-
-  var crongLeftNum = crong[0];
-  crongLeftNum = String(crongLeftNum);
-  for (let i = 0; i < crongLeftNum.length; i++) {
-    crongLeftSum += parseInt(crongLeftNum[i]);
-    crongLeftMultple *= parseInt(crongLeftNum[i]);
-  }
-
-  //right
-  var pobiRightNum = pobi[1];
-  pobiRightNum = String(pobiRightNum);
-  for (let i = 0; i < pobiRightNum.length; i++) {
-    pobiRightSum += parseInt(pobiRightNum[i]);
-    pobiRightMultple *= parseInt(pobiRightNum[i]);
-  }
-
-  var crongRightNum = crong[1];
-  crongRightNum = String(crongRightNum);
-  for (let i = 0; i < crongRightNum.length; i++) {
-    crongRightSum += parseInt(crongRightNum[i]);
-    crongRightMultple *= parseInt(crongRightNum[i]);
-  }
-
-  var pobiValue = [
-    pobiLeftSum,
-    pobiLeftMultple,
-    pobiRightSum,
-    pobiRightMultple,
-  ];
-  var crongValue = [
-    crongLeftSum,
-    crongLeftMultple,
-    crongRightSum,
-    crongRightMultple,
-  ];
-
-  pobiMax = Math.max(...pobiValue);
-  crongMax = Math.max(...crongValue);
-
-  if (pobiMax > crongMax) {
-    answer = 1;
-  } else if (pobiMax < crongMax) {
-    answer = 2;
-  } else if (pobiMax === crongMax) {
-    answer = 0;
-  }
-
   return answer;
 }
 
