1540e0498b349e6c932454a4a1ae8006c15d9e6b
sulmeee <poorooria@naver.com>
2022-10-27 16:51:25 +0900

    feat: 각 자리 숫자 모두 더하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,16 @@
 function problem1(pobi, crong) {
   // 예외사항으로 -1 리턴하기
-  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1
+
+  // 각 자리 숫자 모두 더하기
+  const sumAllPage = (num) => {
+    let res = 0;
+    while (num > 0) {
+      res += Math.floor(num % 10);
+      num = Math.floor(num / 10);
+    }
+    return res;
+  };
 }
 
 module.exports = problem1;
