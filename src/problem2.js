a719527801541f9287a9446b542b7d09895d3893
ryuna <anottrx@gmail.com>
2022-10-28 01:57:34 +0900

    feat : 배열이 스택처럼 작동하도록 하는 클래스 구현

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,22 @@ function problem2(cryptogram) {
   return answer;
 }
 
+class Stack {
+  constructor() {
+    this.__data__ = [];
+  }
+  push(item) {
+    this.__data__.splice(0, 0, item);
+  }
+  pop() {
+    return this.__data__.splice(0, 1)[0];
+  }
+  peek() {
+    return this.__data__[0];
+  }
+  size() {
+    return this.__data__.length;
+  }
+}
+
 module.exports = problem2;
