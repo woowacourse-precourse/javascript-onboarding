e39ed92479b5856e5de4987f12a4d1e6685edbaa
minseok1109 <park485201@naver.com>
2022-10-27 15:38:32 +0900

    feat: stack에 같은 문자 있으면 pop 아니면 push #기능목록 - 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,9 @@
 function problem2(cryptogram) {
   let stack = [];
+  for (let i of cryptogram) {
+    if (stack[stack.length - 1] === i) stack.pop();
+    else stack.push(i);
+  }
 }
 
 module.exports = problem2;
