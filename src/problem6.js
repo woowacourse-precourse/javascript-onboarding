0d03959d2786921de63a47da597f47730413b1d0
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 20:09:04 +0900

    problem6/feat : 같은 글자가 연속적으로 포함되는 닉네임 배열에 저장

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,17 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let arr = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let z = 0; z < forms[i][1].length - 1; z++) {
+      for (let h = 1 + i; h < forms.length; h++) {
+        if (forms[h][1].includes(forms[i][1][z] + forms[i][1][z + 1])) {
+          arr.push(forms[h]);
+          arr.push(forms[i]);
+        }
+      }
+    }
+  }
   return answer;
 }
 
