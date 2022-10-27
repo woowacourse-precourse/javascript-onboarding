e064e35683284be1bc4c28632f98c516048eda75
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-28 02:21:22 +0900

    feat: (P2) 연속되는 문자 제거 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,32 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = isContinuous(cryptogram);
   return answer;
+}
+
+//문자 연속 판별 기능
+function isContinuous(cryptogram) {
+  //연속문자 제거
+  let newCryptogram = continuousRemoval(cryptogram);
+  // 더이상 문자가 연속되지 않는 경우
+
+  // 아직 이웃하는 문자가 연속될 경우 (Recursion)
+}
+
+// 연속문자 제거 기능
+function continuousRemoval(code) {
+  let newCode = "";
+  let continuousAlphabet = "";
+  for (let i = 0; i < code.length; i++) {
+    // 현재 문자가 연속될 경우
+    if (code[i] === code[i + 1] || continuousAlphabet === code[i]) {
+      continuousAlphabet = code[i];
+      continue;
+    }
+
+    // 현재 문자가 연속되지 않는 경우
+  }
+
+  return newCode;
 }
 
 module.exports = problem2;
