b1ca3c5ce60a14ee6971b02a0725e650209b4c4f
WonBeen Choi <zad1264@naver.com>
2022-10-28 02:04:37 +0900

    feat: 6번 문제 - 이름을 두 글자 단위로 모아 검사를 실행하는 함수 추가

--- src/problem6.js
+++ src/problem6.js
@@ -4,7 +4,7 @@
  * 2. 겹치는 닉네임 조각을 소유한 이메일들을 저장하는 배열 - duplicateNicknameEmailList
  * 3. 이메일과 닉네임이 조건에 맞는지 확인하는 함수 - isValid([email, nickname])
  * 4. 겹치는 닉네임 조각이 있다면 false를 반환하고, 아니라면 true를 반환하며 dictionary를 업데이트 하는 함수 - isDictionaryContain(word)
- * 5. 이름을 두 글자 단위로 지나가며 isDictionaryContain을 실행하는 함수 - checkNickname(nickname)
+ * 5. 이름을 두 글자 단위로 모아 isDictionaryContain을 실행하는 함수 - checkNickname(nickname)
  */
 
 function isValid([email, nickname]) {
@@ -16,9 +16,28 @@ function isValid([email, nickname]) {
   return true;
 }
 
+function isDictionaryContain(word) {}
+
+function checkNickname(nickname, dictionary, duplicateNicknameEmailList) {
+  // 한 명의 닉네임 내에서 중복이 발생해도 허용하게끔 nickname을 자른 값들을 Set으로 저장
+  const words = new Set();
+  for (let i = 0; i < nickname.length - 1; i++) {
+    words.add(nickname.slice(i, i + 2));
+  }
+
+  words.forEach((word) =>
+    isDictionaryContain(word, dictionary, duplicateNicknameEmailList)
+  );
+}
+
 function problem6(forms) {
   const dictionary = {};
-  const duplicateNicknameEmailList = [];
+  const duplicateNicknameEmailList = new Set();
+
+  forms.forEach(([email, nickname]) => {
+    if (!isValid([email, nickname])) return;
+    checkNickname(nickname, dictionary, duplicateNicknameEmailList);
+  });
 
   var answer;
   return answer;
