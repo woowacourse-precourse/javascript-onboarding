a8d43d41f2aaf78ef056f81b4780b7be876d16e2
dev-redo <mis05041@naver.com>
2022-10-27 14:08:44 +0900

    [solved 3번] 구현 풀이

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,22 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCnt = 0;
+
+  let flag = 1;
+  while (flag <= number) {
+    clapCnt += getCurrentCnt(flag);
+    flag += 1;
+  }
+
+  return clapCnt;
+}
+
+function getCurrentCnt(number) {
+  const numberList = number.toString().split('');
+
+  return numberList.reduce((sum, curr) => {
+    if (['3', '6', '9'].includes(curr)) sum += 1;
+    return sum;
+  }, 0);
 }
 
 module.exports = problem3;
