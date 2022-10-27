0c5bca8a8cb853549b59d132fcc74a45f2735ae7
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-27 21:35:04 +0900

    feat: PROBLEM 1 예외사항 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,16 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  const check = (arr) => {
+    if (arr.length !== 2) return false;
+    if (arr[0] + 1 !== arr[1]) return false;
+    if (arr[0] % 2 === 0) return false;
+    if (arr[0] <= 1) return false;
+    if (arr[1] >= 400) return false;
+    return true;
+  }
+  if (!check(pobi) || !check(crong)) return answer = -1
+  
   return answer;
 }
 
