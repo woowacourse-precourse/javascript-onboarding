bd9175cdd074531db7af4d77d96732cc33c7a857
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-10-28 02:20:33 +0900

    feat(problem4): 개구리 언어로 변환하는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -7,10 +7,18 @@ const RESULT = {
   exception: -1,
 };
 
+const MAPPING = {
+  upperMapper: 155,
+  lowerMapper: 219,
+  alphabet: /[a-zA-Z]/,
+};
+
 function problem4(word) {
   if (isWrongInput(word)) {
     return RESULT.exception;
   }
+
+  return translateWord(word);
 }
 
 function isWrongInput(input) {
@@ -35,6 +43,43 @@ function isWrongInputData(input) {
 
 function isWrongLengthOfInput(length) {
   return length < INPUT.minLength || length > INPUT.maxLength;
+}
+
+function translateWord(word) {
+  return wordToArray(word).map(wordToFrogWord).join('');
+}
+
+function wordToArray(word) {
+  return Array.from(String(word));
+}
+
+function wordToFrogWord(v) {
+  if (isAlphabet(v)) {
+    const ascci = getAscciFromChar(v);
+    const frogChar = getCharFromAscci(ascci);
+    return frogChar;
+  }
+  return v;
+}
+
+function isAlphabet(v) {
+  return MAPPING.alphabet.test(v);
+}
+
+function getAscciFromChar(v) {
+  if (isLowerCase(v)) {
+    return MAPPING.lowerMapper - v.charCodeAt(0);
+  }
+
+  return MAPPING.upperMapper - v.charCodeAt(0);
+}
+
+function isLowerCase(alpha) {
+  return alpha === alpha.toLowerCase();
+}
+
+function getCharFromAscci(ascci) {
+  return String.fromCharCode(ascci);
 }
 
 module.exports = problem4;
