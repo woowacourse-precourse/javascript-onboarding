2c6935837e9a6faf22f14c7c2f61c3ba6e4f335d
Tory <88531407+1two13@users.noreply.github.com>
2022-10-27 10:44:42 +0900

    feat(problem5): 몫을 배열에 담는 코드 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,21 @@
+// 기능 목록
+// 1. 몫은 개수, 나머지는 money
+
 function problem5(money) {
-  var answer;
+  const arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const copyArr = [...arr];
+  // [오만 원권 개수, 만 원권 개수, 오천 원권 개수, 천 원권 개수, 오백원 동전 개수, 백원 동전 개수, 오십원 동전 개수, 십원 동전 개수, 일원 동전 개수]
+  let answer = arr.fill(0);
+
+  for (let i = 0; i < copyArr.length; i++) {
+    if (money >= copyArr[i]) {
+      // 몫
+      answer[i] = Math.floor(money / copyArr[i]);
+      // 나머지
+      money = money % copyArr[i];
+    } else continue;
+  }
+
   return answer;
 }
 
