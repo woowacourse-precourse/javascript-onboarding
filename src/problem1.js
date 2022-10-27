5d127151ef4678a00a50c468a3becd326cb0c258
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-27 16:11:25 +0900

    feat: 포비와 크롱이 펼친 페이지 더하기, 곱하기 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,24 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function plus(number) {
+  const string_number = String(number);
+  let plus_sum = 0;
+
+  for (let i = 0; i < string_number.length; i++) {
+    plus_sum += parseInt(string_number[i]);
+  }
+  return plus_sum;
+}
+
+function multiple(number) {
+  const string_number = String(number);
+  let multiple_sum = 1;
+
+  for (let i = 0; i < string_number.length; i++) {
+    multiple_sum *= parseInt(string_number[i]);
+  }
+  return multiple_sum;
+}
+
 module.exports = problem1;
