35d5a251b5d1e89621df6d525d884f48227ab220
evencoding <evencoding@gmail.com>
2022-10-27 15:01:34 +0900

    [문제3] implement: 1부터 number까지의 숫자에 포함된 3,6,9의 개수를 반환하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
+function getCount(num) {
+  let cnt = 0;
+  for (let i = 1; i <= num; i++) {
+    const curNum = i + '';
+    for (let j = 0; j < curNum.length; j++) {
+      if (curNum[j] === '3' || curNum[j] === '6' || curNum[j] === '9') {
+        cnt++;
+      }
+    }
+  }
+  return cnt;
+}
+
 function problem3(number) {
-  var answer;
+  const answer = getCount(number);
   return answer;
 }
 
