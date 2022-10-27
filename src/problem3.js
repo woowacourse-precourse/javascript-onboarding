a7bb39e288095a541467f6987102875320175f9b
JH <vvaa277@gmail.com>
2022-10-27 13:25:51 +0900

    Fix : 알아보기 쉽도록 변수명을 clap으로 변경 Docs : clap으로 용어 변경 #P3

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,14 @@
 function problem3(number) {
-  let count = 0;
+  let clap = 0;
+
   const num = number.toString().split("");
 
   for (el of num) {
     if (el % 3 === 0 && el > 0) {
-      count += 1;
+      clap += 1;
     }
   }
-  console.log(count);
+
 }
 
 module.exports = problem3;
