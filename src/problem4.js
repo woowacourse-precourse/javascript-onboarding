d96283b415be33e7e66b958f0310215b7e5dfe6d
Hwang Seong Taek <hst007@naver.com>
2022-10-27 13:05:46 +0900

    docs: problem4 구현목록 정리

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,11 @@
+/*
+구현목록
+1. 결과가 담길 배열 선언하기
+2. ASCII 코드로 변환된 문자를 통해 조건문 비교하기
+3. 조건에 맞는 문자로 변환한 후 result 배열에 저장하기
+4. result 배열을 하나의 문자열로 바꾼 후 return 하기
+*/
+
 function problem4(word) {
   var answer;
   return answer;
