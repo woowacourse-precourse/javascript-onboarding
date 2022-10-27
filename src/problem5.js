15b1ed2de9c421459065c85a745e4941a1eff7e3
soohyun <bmy1320@gmail.com>
2022-10-27 14:50:25 +0900

    docs: problem5 요구사항 및 기능목록 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,18 @@
-function problem5(money) {
-  var answer;
-  return answer;
-}
+/*
+*요구사항
+돈의 액수를 줄 수 있는 화폐로 나눈다.
+이때, 화폐 개수의 총합이 최소가 되게 하여라.
+1<= money <-1000000
+
+*해결전략
+50000부터 1원까지 차례로 나눠가면서 몫을 더해주고 나머지만 게속 남아가게 하자.
+중간에 나머지가 0이 되면 종료.
+
+*기술목록
+1. 각 화페에 맞게 금액을 나눠서 나머지를 도출해나아가는 함수 => divideCurrency
+
+*/
+
+function problem5(money) {}
 
 module.exports = problem5;
