9af13bc9e2d58873a18217ce2dd053d5885d961d
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-27 20:16:45 +0900

    feat: 배열의 인덱스를 사용하여 중복된 연속문자를 삭제하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,13 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function remove_duplication(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i, 2);
+      return arr;
+    }
+  }
+}
+
 module.exports = problem2;
