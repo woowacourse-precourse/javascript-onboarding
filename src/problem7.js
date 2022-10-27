c0e56b9719feca8e2dd2663fe7fdf6548753fd21
moonelysian <dandyblack@naver.com>
2022-10-27 13:13:25 +0900

    add: 친구관계 표현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,24 @@
+function mapRelations (key, value, relations) {
+  if (relations[key]) {
+    relations[key] = [...relations[key], value].sort();
+  } else {
+    relations[key] = [value];
+  }
+  return relations;
+}
+
+function getRelationships (friends) {
+  let relationships = {}
+  friends.map(friend => {
+    const [f1, f2] = friend; 
+    mapRelations(f1);
+    mapRelations(f2);
+  })
+  return relationships;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const relationships = getRelationships(friends);
 }
 
 module.exports = problem7;
