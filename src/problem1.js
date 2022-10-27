06f083ca3f70a048821df5151cf369bfbf4ab6bc
Sunghyun627 <kksscc4@gmail.com>
2022-10-27 16:14:21 +0900

    feat: 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인지 확인하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,6 @@
 /*
   구현할 기능 목록
-  [ ] 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인 지 확인 하는 기능 | return 값 : true/false    
+  [O] 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인지 확인하는 기능 | return 값 : true/false | 함수명 : isConsecutiveNumbers    
   [ ] 페이지 번호의 각 자리 숫자를 모두 더하는 기능 | return 값 : 각 자리 숫자의 합 
   [ ] 페이지 번호의 각 자리 숫자를 모두 곱하는 기능 | return 값 : 각 자리 숫자의 곱
   [ ] 인자로 주어진 값 중 가장 큰 수를 구하는 기능 | return 값 : 가장 큰 수
@@ -8,8 +8,11 @@
 */
 
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!(isConsecutiveNumbers(pobi) && isConsecutiveNumbers(crong))) return -1;
+}
+
+function isConsecutiveNumbers(numbers) {
+  return numbers[0] + 1 == numbers[1];
 }
 
 module.exports = problem1;
