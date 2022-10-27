1455d430fe2b9cf6695218b0d4ffdaef0c93509d
dkile <dkile532@naver.com>
2022-10-27 22:26:17 +0900

    feat: add function of replacing all repetitive char until there's nothing match

--- src/problem2.js
+++ src/problem2.js
@@ -2,8 +2,14 @@
  * @type {(cryptogram: string) => string}
  */
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const findingRepetitiveCharRegex = /([a-z])\1+/g;
+  let result = cryptogram;
+
+  while (result.match(findingRepetitiveCharRegex) !== null) {
+    result = result.replace(findingRepetitiveCharRegex, "");
+  }
+
+  return result;
 }
 
 module.exports = problem2;
