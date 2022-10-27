2f6cae935c36aadd0f5bdb893e55baa0dd566deb
userHWSEO <gmldnjseocnd@gmail.com>
2022-10-27 22:17:14 +0900

    feat: 배달이 369게임 기능 구현
    - 369게임 기능 구현.
    
    - 테스트케이스 확인 완료.
    
    - 간단한 주석 추가.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clap = 0;
+  // 1부터 number까지 숫자를 일의 자리의 수부터 쪼개어 배열로 만들어 준다.
+  for (let i = 1; i <= number; i++) {
+    arrNum = i.toString().split("");
+    // 숫자의 최대 자리수를 변수로 지정
+    placeValue = arrNum.length;
+    // 반복문을 만들어 1의 자리수부터 N의 자리수 까지 3 6 9를 찾는다.
+    for (let j = 0; j < placeValue; j++) {
+      if (arrNum[j] === "3" || arrNum[j] === "6" || arrNum[j] === "9") clap++;
+    }
+  }
+  return clap;
 }
 
 module.exports = problem3;
