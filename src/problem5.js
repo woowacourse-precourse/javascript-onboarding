1eef2245e590f2804136568e1998c9b2cf7bba20
Rki0 <pky00823@gmail.com>
2022-10-27 15:03:50 +0900

    feat: prob5. 입력 데이터 제한 조건 만족 여부 판단 기능
    입력 데이터에 관한 제한 사항을 만족하지 못하면
    return 되도록 만들었습니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,10 +1,3 @@
-/**
- * 기준 금액으로 원금을 나눈 몫을 연산하고, 정답 배열에 넣어주는 함수
- * @param {number} remain 연산 후 남은 금액
- * @param {array} cash 원금을 나누는 기준이 되는 금액들이 들어있는 배열
- * @param {array} ans 원금을 기준이 되는 금액으로 나눈 몫이 들어있는 배열
- * @returns 원금을 기준이 되는 금액으로 나눈 몫을 원소로 가지는 배열
- */
 function calculator(remain, cash, ans) {
   for (let i = 0; i < cash.length; i++) {
     const quotient = Math.floor(remain / cash[i]);
@@ -18,8 +11,10 @@ function calculator(remain, cash, ans) {
 }
 
 function problem5(money) {
-  // 큰 금액부터 나눗셈 연산을 진행하게 되므로
-  // 큰 금액이 정답 배열 앞쪽에 위치하게 된다.
+  if (money < 1 || money > 1000000) {
+    return;
+  }
+
   const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
   let ans = [];
