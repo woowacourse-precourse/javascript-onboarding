5adaac61552d2e9f986860f8cb43fd102172d53a
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-27 16:25:51 +0900

    feat: make arr and save count in problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,13 @@
 function problem5(money) {
   const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const arr = Array.from({ length: unit.length }, () => 0);
   let idx = 0;
   while (money > 0) {
+    arr[idx] = Math.floor(money / unit[idx]);
     money = money % unit[idx];
     idx += 1;
-    console.log(money);
   }
+  return arr;
 }
 
 module.exports = problem5;
