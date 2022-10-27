60e4be70b526deee365abbdac986f01f85018c05
seohee0112 <ghdtj7@naver.com>
2022-10-27 10:03:28 +0900

    feat : problem2 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  cryptogram = cryptogram.split("");
+
+  function deCryptogram(cryptogram) {
+    let isRemove = 0;
+    const newCryptogram = new Array(cryptogram.lengt);
+    const checkCryptogram = new Array(cryptogram.length).fill(0);
+    console.log(cryptogram.join(""));
+    for (let i = 1; i < cryptogram.length; i++) {
+      if (cryptogram[i] == cryptogram[i - 1]) {
+        checkCryptogram[i] = 1;
+        checkCryptogram[i - 1] = 1;
+        isRemove = 1;
+      }
+    }
+
+    checkCryptogram.forEach((check, idx) => {
+      if (!check) newCryptogram.push(cryptogram[idx]);
+    });
+    if (isRemove) return deCryptogram(newCryptogram);
+    else return newCryptogram;
+  }
+  return deCryptogram(cryptogram).join("");
 }
 
 module.exports = problem2;
