ec8ec2fe0432c4ae7bb60e5327fa0401767e27a4
jw <always1997@kakao.com>
2022-10-27 14:51:56 +0900

    problem1 기능2 구현 및 solved

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,7 @@
   기능 목록
   1. 각 자릿수의 합과 곱중 최댓값을 반환하는 findMax 함수 구현
     1-1. 예외값 우선처리 (첫페이지와 끝페이지는 불가능, 왼쪽값은 홀수만, 오른쪽값음 짝수만 가능)
+    1-2. 각 자릿수의 합과 곱중 더 큰값을 리턴
   2. 포비가 더 큰 수를 가지면 1 크롱이 더 큰수를 가지면 2 동일하면 0 예외시 -1 리턴
 */
 function findMax(nums) {
@@ -23,7 +24,14 @@ function findMax(nums) {
 function problem1(pobi, crong) {
   let answer;
 
+  let pobi_maxNum = findMax(pobi);
+  let crong_maxNum = findMax(crong);
+
+  if (pobi_maxNum === -1 || crong_maxNum === -1) answer = -1;
+  else if (pobi_maxNum > crong_maxNum) answer = 1;
+  else if (pobi_maxNum < crong_maxNum) answer = 2;
+  else answer = 0;
+
   return answer;
 }
-
 module.exports = problem1;
