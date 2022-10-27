6956f2fa5734f7d5e3fe48db31e52c530625901d
Hwang Seong Taek <hst007@naver.com>
2022-10-27 20:58:57 +0900

    feat: cash, wallet 배열 선언

--- src/problem5.js
+++ src/problem5.js
@@ -7,8 +7,8 @@
 */
 
 function problem5(money) {
-  var answer;
-  return answer;
+  const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const wallet = [];
 }
 
 module.exports = problem5;
