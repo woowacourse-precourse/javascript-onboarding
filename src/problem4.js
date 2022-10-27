ce9f73e4aeac2b3655aa2783758a69f2f40af648
violetwhenisawu <anashape710@gmail.com>
2022-10-28 02:42:57 +0900

    Feat: 아스키코드 연산을 활용해 반대 문자로 변환해주는 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -7,16 +7,22 @@ function problem4(word) {
     }
     let ascii = word.charCodeAt([i]);
     let checkedCase = checkStringCase(ascii);
+    result += reverseAscii(ascii, checkedCase);
   }
   return result;
 }
 
 function checkStringCase(ascii) {
   if (ascii >= 65 && ascii <= 90) {
-    return 65;
+    return 65;  
   } else if (ascii >= 97 && ascii <= 122) {
     return 97;
   }
+}
+
+function reverseAscii(ascii, checkedCase) {
+  let reversed = ascii + 25 - Math.abs(ascii - checkedCase) * 2;
+  return String.fromCharCode(reversed);
 }
 
 module.exports = problem4;
