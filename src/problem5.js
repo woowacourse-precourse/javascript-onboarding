939012fbc5773e7a964f67e8bcf8f47fe0e1f8a2
Gyeongm1n <affirming_km@naver.com>
2022-10-27 11:28:48 +0900

    화폐 단위에 따른 개수와 나머지 금액을 구하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,17 @@
+function calculate(money, unit) {
+  const count = Math.floor(money / unit);
+  const rest = money % unit;
+
+  return [count, rest];
+}
+
 function problem5(money) {
-  var answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for (const unit of units) {
+    const [count, rest] = calculate(money, unit);
+  }
   return answer;
 }
 
