f779c79d72b38b3c30a6519f2477192f9c1f3f67
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-27 12:32:12 +0900

    feat: 알파벳 아스키 코드 상수로 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,10 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+const LOWER_CASE_START_CODE = 97;
+const LOWER_CASE_END_CODE = 122;
+const UPPER_CASE_START_CODE = 65;
+const UPPER_CASE_END_CODE = 90;
+
+const badFrog = (word) => {}
+
+const problem4 = (word) => {}
 
 module.exports = problem4;
