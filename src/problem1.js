63c4ed1231b30d4f2508b0142e61995db42cf3b2
pangkyu <baesense@naver.com>
2022-10-27 17:27:50 +0900

    fix : 예외처리 수정
    1. checkArray 함수를 통해 0번인덱스++을 하여 1번인덱스와 붙어있는지 체크

--- src/problem1.js
+++ src/problem1.js
@@ -1,22 +1,37 @@
 function problem1(pobi, crong) {
-  // if (checkArray(pobi) === true && checkArray(crong) === true) {
-  const pobiRecord = getMaxRecord(pobi);
-  const crongRecord = getMaxRecord(crong);
-  if (pobiRecord > crongRecord) {
-    return 1;
-  } else if (pobiRecord == crongRecord) {
-    return 0;
-  } else if (pobiRecord < crongRecord) {
-    return 2;
+  if (checkArray(pobi) === true && checkArray(crong) === true) {
+    const pobiRecord = getMaxRecord(pobi);
+    const crongRecord = getMaxRecord(crong);
+    if (pobiRecord > crongRecord) {
+      return 1;
+    } else if (pobiRecord == crongRecord) {
+      return 0;
+    } else if (pobiRecord < crongRecord) {
+      return 2;
+    }
+  } else {
+    return -1;
   }
-  // } else {
-  //   return -1;
-  // }
 }
 function getMaxRecord(user) {
+  // const left = String(user[0]);
+  // const right = String(user[1]);
+  const left = user[0];
+  const right = user[1];
   console.log(user);
 }
 
+function sum() {}
+
+function multiply() {}
+
+function checkArray(user) {
+  if (user[0] + 1 == user[1]) {
+    return true;
+  } else {
+    return false;
+  }
+}
 function checkOdd(user) {
   if (user % 2 !== 0) {
     return user;
@@ -40,8 +55,9 @@ function testCode() {
   pobi[1] = pobi[0] + 1;
   crong[1] = crong[0] + 1;
 
-  problem1(pobi, crong);
-  // problem1([97, 98], [197, 198]);
+  //problem1(pobi, crong);
+  //  problem1([97, 98], [197, 198]);
+  problem1([99, 102], [211, 212]);
 }
 
 testCode();
