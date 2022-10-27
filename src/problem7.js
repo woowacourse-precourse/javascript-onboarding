668c05ca36bf0618704b51b5e90b5fb73ddd1f9c
catca <kdg96g@gmail.com>
2022-10-27 23:42:41 +0900

    feat: solved problem7
    user, friends, visitors을 사용하여 전체 유저와 user 친구들을 구함
    전체 유저에서 user 친구들 및 user를 뺀 추천 친구 목록을 구함
    추천 친구에게 친구 추천 규칙에 따라 점수를 줌
    추천 점수와 이름에 따라 정렬하고, 최대 5명 제한하여 return

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,57 @@
+const getUsers = (friends, visitors) => {
+  let arr = [];
+  friends.forEach(([frined1, frined2]) => {
+    if(!arr.includes(frined1)) arr.push(frined1);
+    if(!arr.includes(frined2)) arr.push(frined2);
+  })
+  visitors.forEach((user) => {
+    if(!arr.includes(user)) arr.push(user);
+  });
+  return arr;
+}
+
+const getUserFriends = (user, friends) => {
+  let arr = [];
+  friends.forEach(([frined1, frined2]) => {
+    if(user === frined1) arr.push(frined2);
+    if(user === frined2) arr.push(frined1);
+  });
+  return arr;
+}
+
+const getRecommendedFriends = (user, friends, visitors, users, userFriends) => {
+  let arr = [];
+  arr = users.filter((user) => !userFriends.includes(user));
+  arr = arr.filter((item) => item !== user);
+  return arr.map((user) => {
+    let score = 0;
+    friends.forEach(([friend1, friend2]) => {
+      if((user === friend1 && userFriends.includes(friend2)) || (user === friend2 && userFriends.includes(friend1))){
+        score += 10;
+      }
+    });
+    visitors.forEach(visitor => {
+      if(user === visitor) score++;
+    })
+    return {user: user, score: score}
+  });
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const users = getUsers(friends, visitors);
+  const userFriends = getUserFriends(user, friends);
+  const recommendedFriends = getRecommendedFriends(user, friends, visitors, users, userFriends);
+
+  recommendedFriends.sort((a, b) => {
+    if(a.score === b.score) {
+      const x = a.user.toLowerCase(), y = b.user.toLowerCase();
+      return x < y ? -1 : x > y ? 1 : 0;
+    }
+    return b.score - a.score;
+  });
+
+  let answer = recommendedFriends.map(({user, _}) => user);
+  answer = answer.filter((_, index) => index < 5);
   return answer;
 }
 
