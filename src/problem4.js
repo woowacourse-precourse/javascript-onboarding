49ac38cb92c85979a406d0ce46edb147dc6e0cd5
turtle601 <turtle601@naver.com>
2022-10-27 13:03:32 +0900

    Feat: [problem4] 제한사항 로직인 ErrorCase 클래스 구현
    <ErrorCase클래스>
    - checkLimit() : 주어진 단어 길이가 1이상 1000이하인지 확인

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
-function problem4(word) {
-  var answer;
-  return answer;
+class ErrorCase {
+  constructor(word) {
+    this.word = word;
+    this.checkLimit();
+  }
+
+  checkLimit() {
+    if (!(1 <= this.word.length && this.word.length <= 1000)) {
+      throw new Error("input 양식에 에러가 발생했습니다!!");
+    }
+  }
 }
+
+function problem4(word) {
+  console.log(word.length);
+  new ErrorCase(word);
+}
+
+/*
+  에러케이스 - 1000자 이상 테스트 성공 but 너무 길어 담지 않음
+  problem4("");
+*/
 
 module.exports = problem4;
