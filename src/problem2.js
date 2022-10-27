4c1f2dd0f5e589fb56df38ee27cd9964faab0b3b
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-27 14:17:54 +0900

    Add upload for first commit

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
+console.log(problem2("browoanoommnaon"));
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return removeDuplicatesLetters(cryptogram);
+}
+
+function removeDuplicatesLetters(word){
+  let answer=[word[0]];
+  for (let i = 1; i < word.length; i++) {
+    let top = answer[answer.length-1];
+    if (top != word[i]) {
+      answer.push(word[i]);
+    } else {
+      answer.pop();
+    }
+    console.log(answer.join(""));
+  }
+  return answer.join("");
 }
 
 module.exports = problem2;
