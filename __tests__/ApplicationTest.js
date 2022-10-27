37afaa712bfb7c7a3a513446676acb834e7ddd01
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-27 21:02:27 +0900

    fix problem2

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -20,7 +20,7 @@ describe("problem1", () => {
   });
 });
 
-describe.only("problem2", () => {
+describe("problem2", () => {
   test("case1", () => {
     expect(problem2("browoanoommnaon")).toEqual("brown");
   });
--- src/problem2.js
+++ src/problem2.js
@@ -1,36 +1,20 @@
 function problem2(cryptogram) {
   var answer;
   var cryptogramString = cryptogram;
-  var bundle = 1;
+  var bundle = 0;
   var flag = 1;
-
-  function DFS(nowPlace, checkingIndex) {
-    if (
-      cryptogramString[checkingIndex] != cryptogramString[checkingIndex + 1]
-    ) {
-      if (bundle > 1) {
-        nowPlace = checkingIndex;
-        bundle = 1;
-      } else {
-        answer += cryptogramString[checkingIndex];
-      }
-      if (checkingIndex == cryptogramString.length - 2)
-        answer += cryptogramString[checkingIndex + 1];
-      return nowPlace;
-    } else {
-      flag++;
-      bundle++;
-      if (checkingIndex !== cryptogramString.length - 2)
-        nowPlace = DFS(nowPlace, checkingIndex + 1);
-      return nowPlace;
-    }
-  }
-
-  while (cryptogramString.length >= 2 && flag > 0) {
+  while (cryptogramString.length > 1 && flag > 0) {
     answer = "";
     flag = 0;
-    for (var i = 0; i <= cryptogramString.length - 2; i++) {
-      i = DFS(i, i);
+    for (let i = 0; i < cryptogramString.length - 1; i++) {
+      if (cryptogramString[i] == cryptogramString[i + 1]) {
+        flag++;
+        bundle++;
+      } else {
+        if (bundle == 0) answer += cryptogramString[i];
+        else bundle = 0;
+        if (i == cryptogramString.length - 2) answer += cryptogramString[i + 1];
+      }
     }
     cryptogramString = answer;
   }
