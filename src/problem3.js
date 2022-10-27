5f246e8e2fd798a7c3ebeec9daab72d7515760d9
Hoon <si932174@gmail.com>
2022-10-27 15:13:18 +0900

    problem3 : number까지의 숫자 전체를 배열로 바꿔주는 함수 제작

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,19 @@
 function problem3(number) {
   var answer;
+
+  const getTotalNumberArray = number => {
+    let totalNumberString = '';
+
+    for(let i = 1; i<=number; i++){
+      totalNumberString = totalNumberString + String(i);
+    }
+
+    const totalNumberArray = totalNumberString.split('');
+
+    return totalNumberArray;
+  };
+  
+
   return answer;
 }
 
