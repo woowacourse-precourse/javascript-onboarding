7fc13010810baf1f4083cc8f8d29c87210cc020d
wonkyum-kim <dodo12120@naver.com>
2022-10-27 21:33:39 +0900

    feat: add problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,20 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  // Use the ASCII codes.
+  for (let i = 0; i < word.length; ++i) {
+    // The sum of two ascii code is constant
+    if ("A" <= word[i] && word[i] <= "Z") {
+      // A + Z = B + Y = ... =155
+      answer += String.fromCharCode(155 - word[i].charCodeAt(0));
+    } else if ("a" <= word[i] && word[i] <= "z") {
+      // a + z = b + y = ... = 219
+      answer += String.fromCharCode(219 - word[i].charCodeAt(0));
+    } else {
+      // This word is not an alphabet
+      answer += word[i];
+    }
+  }
+
   return answer;
 }
 
