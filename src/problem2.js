84939568250f24d313763661f9b8eb599e612808
lsy20140 <lsy20140@naver.com>
2022-10-28 00:58:15 +0900

    feat: [문제2] 중복 문자들 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+  let cryptogramArr = cryptogram.split('');
+
+  for(let i = 0 ; i < cryptogramArr.length ; ){
+    if(cryptogramArr[i] == cryptogramArr[i+1]){
+      cryptogramArr.splice(i,2)
+      i = 0;
+      continue;
+    } 
+    i += 1;
+  }
+
+  return cryptogramArr.join('');
+
 }
 
 module.exports = problem2;
