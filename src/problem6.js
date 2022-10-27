84986a882d2fca39d2fe85d0c996b097b678c1f5
pakxe <pigkill40@naver.com>
2022-10-27 20:23:18 +0900

    feat: [P6] problem6 해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let patternMap = new Map();
+  let resultSet = new Set();
+
+  for (let i = 0; i < forms.length; i++) {
+    let name = forms[i][1];
+    let email = forms[i][0];
+
+    for (let j = 0; j < name.length - 1; j++) {
+      let slicedName = name.substr(j, 2);
+      if (patternMap.has(slicedName)) {
+        resultSet.add(patternMap.get(slicedName));
+        resultSet.add(email);
+      } else {
+        patternMap.set(slicedName, email);
+      }
+    }
+  }
+
+  return [...resultSet].sort();
+  s;
 }
 
 module.exports = problem6;
