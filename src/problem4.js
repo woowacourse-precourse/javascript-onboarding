ec3308b70451ae6d7da03c8bd25fb844c630561c
ilgon0110 <moses011010@gmail.com>
2022-10-27 14:17:14 +0900

    feat: 예외처리,연산 기능 구현(#problem4)
    예외처리와 연산을 동시에 수행

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+
+  for (let x of word) {
+    //기능목록 2-2 구현
+    let text = x.charCodeAt();
+    //예외처리, 연산
+    //기능목록 1-1 구현, 2-1구현
+    if (text >= 65 && text <= 90) {
+      answer += String.fromCharCode(155 - text);
+    } else if (text >= 97 && text <= 122) {
+      answer += String.fromCharCode(219 - text);
+    } else if (text === 32) answer += " ";
+  }
   return answer;
 }
 
