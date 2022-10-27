fe94d19fc21e24cdcb00a6b7b5f719572e76dbf1
wonkyung <mwk0725@gmail.com>
2022-10-27 14:55:55 +0900

    feat: [기능 1] 연속 문자 확인 후 전부 삭제된 문자 출력

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,26 @@
 function problem2(cryptogram) {
-  let text = cryptogram.split("");
-  // 중복제거
-  for (let i = 0; i < text.length; i++) {
-    if (text[i] === text[i + 1]) {
-      delete text[i];
-      delete text[i + 1];
+  let answer = "";
+
+  const removeDup = str => {
+    let text = str.split("");
+    // 중복제거
+    for (let i = 0; i < text.length; i++) {
+      if (text[i] === text[i + 1]) {
+        delete text[i];
+        delete text[i + 1];
+      }
     }
-  }
+    // 반복 여부 확인
+    if (text.includes(undefined)) {
+      answer = text.join("");
+      removeDup(answer);
+    } else {
+      answer = text.join("");
+    }
+  };
+  removeDup(cryptogram);
+
+  return answer;
 }
 
 module.exports = problem2;
