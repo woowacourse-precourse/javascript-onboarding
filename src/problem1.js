e325d21c1ece73335e0b6a1f4d834b4e3683199b
Ryan-Dia <cjfdnjs158@naver.com>
2022-10-28 08:03:23 +0900

    [수정] 풀이 숨김

--- src/problem1.js
+++ src/problem1.js
@@ -1,38 +1,6 @@
-let pobiMax = [];
-let crongMax = [];
-
-const exception = (pobi, crong) => {
-  if (pobi[0] === 1 || pobi[0] === 399 || crong[0] === 1 || crong[0] === 399)
-    return -1;
-  if (pobi.reduce((a, b) => Math.abs(a) - b) !== -1) return -1;
-  if (crong.reduce((a, b) => Math.abs(a) - b) !== -1) return -1;
-};
-
-const calulateMax = (array, plus, multiplication, pobi) => {
-  plus > multiplication
-    ? array === pobi
-      ? pobiMax.push(plus)
-      : crongMax.push(plus)
-    : array === pobi
-    ? pobiMax.push(multiplication)
-    : crongMax.push(multiplication);
-};
-
-const getMaxNumber = (array, pobi) => {
-  array.forEach((page) => {
-    let plus = Object.values(String(page)).reduce((a, b) => +a + +b);
-    let multiplication = Object.values(String(page)).reduce((a, b) => +a * +b);
-    calulateMax(array, plus, multiplication, pobi);
-  });
-};
-
-function problem1(pobi, crong) {
-  pobiMax, (crongMax = []);
-  if (exception(pobi, crong) === -1) return -1;
-  getMaxNumber(pobi, pobi);
-  getMaxNumber(crong, pobi);
-  if (Math.max(...pobiMax) === Math.max(...crongMax)) return 0;
-  return Math.max(...pobiMax) > Math.max(...crongMax) ? 1 : 2;
+function problem1(money) {
+  var answer;
+  return answer;
 }
 
 module.exports = problem1;
--- src/problem4.js
+++ src/problem4.js
@@ -1,20 +1,6 @@
-function converFun(word) {
-  const upperCaseASCIINum = 155;
-  const lowerCaseASCIINUM = 219;
-  const classifyingASCIINum = 96;
-  if (word === " ") return " ";
-  else {
-    if (word.charCodeAt() > classifyingASCIINum) {
-      return String.fromCharCode(lowerCaseASCIINUM - word.charCodeAt());
-    } else {
-      return String.fromCharCode(upperCaseASCIINum - word.charCodeAt());
-    }
-  }
-}
-
 function problem4(word) {
-  let convertWord = word.split("").map((x) => converFun(x));
-  return convertWord.join("");
+  var answer;
+  return answer;
 }
 
 module.exports = problem4;
