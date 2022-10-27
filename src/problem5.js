13d483aff8e3a4a0e980a48ad8508371758c468c
heoputer <hjw1753@gmail.com>
2022-10-27 23:36:28 +0900

    [5번 문제] 최소 화폐 계산 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  const answer = [];
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < currency.length; i++) {
+    let count = 0;
+
+    count += Math.floor(Number(money / currency[i]));
+    money %= currency[i];
+    answer.push(count);
+  }
+
   return answer;
 }
 
