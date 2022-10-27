654ef1f2a211bc10cdcf282f844e53ad7ea8ed90
knh6269 <knh6269@gmail.com>
2022-10-27 23:45:48 +0900

    feat: 미션1) 유저 점수 분리 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,8 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+//유저의 점수를 분리해주는 함수
+function scoreSplit(user,amount,index,array){
+  array.push(Math.floor(user[index] / amount));
+  user[index] = user[index] % amount;
+}
 }
 
 module.exports = problem1;
