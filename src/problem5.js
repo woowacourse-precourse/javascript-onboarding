6b2825a695cecfa062ad45e84d606f79a3509361
hwnim5324 <hwnim5324@naver.com>
2022-10-27 14:59:20 +0900

    5번 리펙토링.

--- src/problem5.js
+++ src/problem5.js
@@ -1,31 +1,11 @@
 function problem5(money) {
-  var answer=[0,0,0,0,0,0,0,0,0];
-  
-  answer[0]=Math.floor(money/50000);
-  money%=50000;
+  var answer=[];
+  const won = [50000,10000,5000,1000,500,100,50,10,1];
 
-  answer[1]=Math.floor(money/10000);
-  money%=10000;
-
-  answer[2]=Math.floor(money/5000);
-  money%=5000;
-
-  answer[3]=Math.floor(money/1000);
-  money%=1000;
-
-  answer[4]=Math.floor(money/500);
-  money%=500;
-
-  answer[5]=Math.floor(money/100);
-  money%=100;
-
-  answer[6]=Math.floor(money/50);
-  money%=50;
-
-  answer[7]=Math.floor(money/10);
-  money%=10;
-
-  answer[8]=money;
+  won.forEach((el)=>{
+    answer.push(Math.floor(money/el));
+    money%=el;
+  });
 
   return answer;
 }
