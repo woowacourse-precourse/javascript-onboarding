d81d8182d9188bd2f272272e048e9dd82b642e81
jiyujin <yujin1679@naver.com>
2022-10-27 19:07:29 +0900

    feat(pro1): 본인의 가장 높은 점수를 구하는 getMaxValue 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,7 @@
 // 기능 요구 사항
-// - [] 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-// - [] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
-// - [] 위 과정에서 가장 큰 수를 본인의 점수로 한다.
+// - [o] 왼쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+// - [o] 오른쪽 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+// - [o] 위 과정에서 가장 큰 수를 본인의 점수로 한다.
 // - [] 점수를 비교해 높은 사람이 게임의 승자가 된다.
 //   - [] 포비가 이기면 1, 크롱이 이기면 2, 무승부면 0, 그 외 -1을 return 한다.
 // 예외사항
@@ -10,8 +10,33 @@
 // - [] 오른쪽 페이지 수에서 왼쪽 페이지 수를 빼면 그 값은 1이다.
 
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const pobiMax = getMaxValue(pobi);
+  const crongMax = getMaxValue(crong);
+  console.log(pobiMax, crongMax);
 }
 
+function getMaxValue(name) {
+  let maxValue = 0;
+  for (let i = 0; i < name.length; i++) {
+    let plus = 0;
+    let mult = 1;
+
+    let str = name[i].toString();
+    for (let j = 0; j < str.length; j++) {
+      plus += Number(str[j]);
+      mult *= Number(str[j]);
+    }
+    if (maxValue < plus) {
+      maxValue = plus;
+    }
+    if (maxValue < mult) {
+      maxValue = mult;
+    }
+  }
+  return maxValue;
+}
+
+const pobi = [131, 132];
+const crong = [211, 212];
+problem1(pobi, crong);
 module.exports = problem1;
