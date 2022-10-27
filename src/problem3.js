cd9b46208ac843fcb81c48ea993156bf2a9e594f
MinGyu-Kang <kagrin97@gmail.com>
2022-10-27 12:15:53 +0900

    problem3/feat : strArr을 순회하면서 3, 6, 9중 하나라도 일치하면 count+1 증가후 count값을 최종적으로 return

--- src/problem3.js
+++ src/problem3.js
@@ -1,11 +1,18 @@
 function problem3(number) {
+  const findNumber = ["3", "6", "9"];
+  let count = 0;
+
   for (let n = 1; n < number + 1; n++) {
-    let strArr = String(n).split("");
+    const strArr = String(n).split("");
+
+    strArr.forEach((str) => {
+      if (findNumber.includes(str)) {
+        count += 1;
+      }
+    });
   }
 
-  return;
+  return count;
 }
 
 module.exports = problem3;
-
-console.log(problem3(33));
