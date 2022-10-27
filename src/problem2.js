cc9d1ef35fe4c2df19c3bdc974ca995cd662de38
h2002y <h2002y@naver.com>
2022-10-27 17:41:46 +0900

    typo: Fix typo ; => {} in checkIsSequence func

--- src/problem2.js
+++ src/problem2.js
@@ -1,18 +1,31 @@
 function problem2(cryptogram) {
   var answer = cryptogram;
   answer = cryptogram;
+  while(checkIsSequence(answer)){
+    answer = removeSequence(answer);
+  }
   return answer;
+}
+
+function checkIsSequence(str){
+  for(let i = 0; i < str.length - 1; i++){
+    if(str[i] === str[i+1]){
+      return true;
+    }
+  }
+  return false;
 }
 
 function removeSequence(str){
   for(let i = 0; i < str.length - 1; i++){
     let index = i;
-    while(str[i]===str[index + 1]){
+    while(str[i] === str[index + 1]){
       index++;
     }
     if(i!=index){
       let len = str.length;
       str = str.slice(0, i) + str.slice(index + 1, len);
+      i--;
     }
   }
   return str;
