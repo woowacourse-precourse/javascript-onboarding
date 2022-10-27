fdcd4ad792671161f648554b5e71d19c7d8815d7
LEECHANHYUNG <lsj721538@gmail.com>
2022-10-28 00:00:41 +0900

    exception handling

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,15 @@
+/* 
+1. 페이지가 연속적이지 않은 예외에 대한 처리
+2. 각 자리수의 합, 곱을 계산
+3. 계산된 값을 비교해서 큰 값을 추출
+4. 포비와 크롱의 점수 비교
+*/
 function problem1(pobi, crong) {
+  const pobiSub = pobi.reduce((prev, next) => next - prev);
+  const crongSub = crong.reduce((prev, next) => next - prev);
+  if ((pobiSub !== 1) | (crongSub !== 1)) {
+    return -1;
+  }
   var answer;
   return answer;
 }
