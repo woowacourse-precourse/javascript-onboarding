c20965ee99d2f8ec3b9dcfbe4e899f178b18ca61
Rki0 <pky00823@gmail.com>
2022-10-27 09:42:46 +0900

    feat: prob7. 친구의 친구를 판별하는 함수
    친구 추천 점수 산정에 활용하기 위해
    친구의 친구를 판별하는 함수를 구현했습니다.
    추가적으로 친구를 구하는 함수는 모듈화하였습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,12 +1,32 @@
-function problem7(user, friends, visitors) {
-  let alreadyFriends = [];
-
+function getAlreadyFriends(user, friends, alreadyFriends) {
   friends.forEach((item) => {
     if (item.includes(user)) {
       // user가 1차원 배열의 0,1번 인덱스 중 어디있는지 모르기 때문
       item.indexOf(user) === 0
         ? alreadyFriends.push(item[1])
         : alreadyFriends.push(item[0]);
+    }
+  });
+}
+
+function problem7(user, friends, visitors) {
+  let alreadyFriends = [];
+
+  getAlreadyFriends(user, friends, alreadyFriends);
+
+  let friendOfFreinds = [];
+
+  friends.forEach((item) => {
+    for (let i = 0; i < alreadyFriends.length; i++) {
+      if (item.includes(alreadyFriends[i])) {
+        if (item.includes(user)) {
+          continue;
+        }
+
+        item.indexOf(alreadyFriends[i]) === 0
+          ? friendOfFreinds.push(item[1])
+          : friendOfFreinds.push(item[0]);
+      }
     }
   });
 }
