8c37b0972922c03a129b61031483f7b4b69788e2
Adult96 <djxor007@gmail.com>
2022-10-27 12:13:33 +0900

    feat: problem1 - 오른쪽 페이지 기준으로 +,*값 구하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -6,6 +6,22 @@ function problem1(pobi, crong) {
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
     return -1;
   }
+
+  for (let i = 0; i < 2; i++) {
+    const pobiSum = (pobi[1] + '')
+      .split('')
+      .map((v) => parseInt(v))
+      .reduce((s, n) => (i > 0 ? (s *= n) : (s += n)));
+    pobiArr.push(pobiSum);
+
+    const crongSum = (crong[1] + '')
+      .split('')
+      .map((v) => parseInt(v))
+      .reduce((s, n) => (i > 0 ? (s *= n) : (s += n)));
+    crongArr.push(crongSum);
+  }
+
+  return answer;
 }
 
 module.exports = problem1;
