5801ab251ee9eda2411957b8d52248b4d6db7660
curomame <curomame@naver.com>
2022-10-27 22:58:43 +0900

    ✅ check : problem 2 해답 코드 작성 완료 1027.2258

--- src/problem2.js
+++ src/problem2.js
@@ -1,26 +1,4 @@
 function problem2(cryptogram) {
-  //필요함수
-
-  //#1. 글자가 주어지면 중복된 글자를 제거한 새로운 글자를 반환하는 함수
-    // 1번 함수를 재귀적으로 적용할 예정
-      // 해당 함수의 break 포인트 필요
-
-  function RETURN_REMOVE_DUPLICATION_ALPHABET(word) {
-    const alphabetList = word.slice().split("");
-    const newAlphabetList = [];
-    let newWord;
-    for (let i = 0; i < alphabetList.length; i++) {
-      if (
-        alphabetList[i] !== alphabetList[i + 1] &&
-        alphabetList[i] !== alphabetList[i - 1]
-      ) { newAlphabetList.push(alphabetList[i]); }}
-    newWord = newAlphabetList.join("");
-    if (word === newWord) { return word; }
-    return RETURN_REMOVE_DUPLICATION_ALPHABET(newWord);
-  }
-
-
-  
 
   //특이사항
 
@@ -44,6 +22,34 @@ function problem2(cryptogram) {
     return true;
   }
 
+  
+  if(CHECK_WORD_LENGTH_BETWEEN_ONE_TO_THOUSAND(cryptogram) === false){
+    return "단어의 길이가 1미만, 1000초과입니다."
+  }
+
+  if(CHECK_WORD_IS_ALL_LOWERCASE(cryptogram) === false){
+    return "단어 중 알파벳 소문자가 아닌 문자가 존재합니다."
+  }
+
+  //필요함수
+
+  //#1. 글자가 주어지면 중복된 글자를 제거한 새로운 글자를 반환하는 함수
+    // 1번 함수를 재귀적으로 적용할 예정
+      // 해당 함수의 break 포인트 필요
+
+  function RETURN_REMOVE_DUPLICATION_ALPHABET(word) {
+    const alphabetList = word.slice().split("");
+    const newAlphabetList = [];
+    let newWord;
+    for (let i = 0; i < alphabetList.length; i++) {
+      if (
+        alphabetList[i] !== alphabetList[i + 1] &&
+        alphabetList[i] !== alphabetList[i - 1]
+      ) { newAlphabetList.push(alphabetList[i]); }}
+    newWord = newAlphabetList.join("");
+    if (word === newWord) { return word; }
+    return RETURN_REMOVE_DUPLICATION_ALPHABET(newWord);
+  }
 
   return RETURN_REMOVE_DUPLICATION_ALPHABET(cryptogram);
 }
