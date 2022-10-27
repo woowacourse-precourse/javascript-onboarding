8b9de79bd730318ba0218502bfc25075f637c848
jangsumin <jsm0726@naver.com>
2022-10-27 09:36:09 +0900

    문제6 : 의사 코드 수정 및 구조 분해 할당

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,24 @@
 // 의사 코드 작성
 // 1. 2차원 배열인 forms 인자의 원소들을 순회
+// 1-1. 구조 분해 할당 사용
 // 2. 이름 한 개를 기준으로 나머지 이름을 전부 탐색
+// 2-1. 이 때 2글자씩 뽑아내서 기준으로 삼음
 // 3. 같은 글자가 연속적으로 포함되면 결과 배열에 이메일 push
 // 4. Set 객체를 사용해서 중복 제거
 // 5. 결과 배열의 이메일들을 오름차순으로 정렬
 
 function problem6(forms) {
-  var answer;
+  let answer = [];
+
+  forms.map(form => {
+    let [email, nickname] = form;
+    // console.log(form);
+    for (let i=0;i<nickname.length-1;i++) {
+      let compareStr = nickname.substring(i, i+2);
+      // console.log(compareStr);
+    }
+  });
+
   return answer;
 }
 
