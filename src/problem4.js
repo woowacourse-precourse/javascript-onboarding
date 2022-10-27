794a66f65d0c4b7ae09c8b5eb5fb1c8b818fcc1a
violetwhenisawu <anashape710@gmail.com>
2022-10-28 02:57:47 +0900

    Docs: problem4 함수 주석 추가, 구현완료

--- src/problem4.js
+++ src/problem4.js
@@ -12,14 +12,16 @@ function problem4(word) {
   return result;
 }
 
+// 아스키코드 조건을 확인해서, 소/대문자를 검사하는 함수 (특수문자는 return하지 않음)
 function checkStringCase(ascii) {
   if (ascii >= 65 && ascii <= 90) {
-    return 65;  
+    return 65;
   } else if (ascii >= 97 && ascii <= 122) {
     return 97;
   }
 }
 
+// 아스키코드 연산을 활용해 반대 문자로 변환해주는 함수
 function reverseAscii(ascii, checkedCase) {
   let reversed = ascii + 25 - Math.abs(ascii - checkedCase) * 2;
   return String.fromCharCode(reversed);
