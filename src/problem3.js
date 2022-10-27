28173d40fc86a65323ea1fe7d0ce6f556f035363
yiju_Kim <kls9939@naver.com>
2022-10-28 01:31:39 +0900

    solve: Create problem3 business logic

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (let i = 1; i < number + 1; i++) {
+    const arr = String(i).split('');
+
+    arr.forEach(elem => {
+      if (elem === '3' || elem === '6' || elem === '9') {
+        count += 1;
+      }
+    })
+  }
+  
+  return count;
 }
 
 module.exports = problem3;
