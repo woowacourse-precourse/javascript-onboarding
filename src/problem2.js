57fae110faf87202062d9f9262e3cb2c43e45e5d
hyeongdong <khd941018@gmail.com>
2022-10-27 20:04:52 +0900

    feat(problem2) : 연속하는 중복 문자열 삭제하는 함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -5,6 +5,12 @@
     - 더 이상 제거하지 못할 때 까지 제거 후 최종 문자열을 리턴한다.
 */
 
-function problem2(cryptogram) {}
+function problem2(cryptogram) {
+  const regax = /([a-z])\1+/g;
+  const removedCryptogram = cryptogram.replace(regax, "");
+  if (cryptogram === removedCryptogram) return cryptogram;
+
+  return problem2(removedCryptogram);
+}
 
 module.exports = problem2;
