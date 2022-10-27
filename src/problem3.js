fa0b5473d67efc0355626680bc2a7fea3b05295b
Chris.c <cjs120dd@gmail.com>
2022-10-27 11:56:24 +0900

    fear:3번 문제를 해결한다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
+const clapNumber = {
+  three:3,
+  six:6,
+  nine:9
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  
+  for (let i = 1 ; i < number+1 ; i ++) {
+    let current = i;
+    while (current!==0){
+      if (current % 10 === clapNumber.three || current % 10 === clapNumber.six || current % 10 === clapNumber.nine){
+        answer += 1;
+      }
+      current =  Math.floor(current/10);
+    }
+  }
+  
   return answer;
 }
 
