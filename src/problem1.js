684c81fd837bbc2bd4184dd7feb8abdb0df76f18
semnil5202 <semnil5202@gmail.com>
2022-10-27 20:51:46 +0900

    문제1번. 예외처리 및 기능 구현 완료

--- src/problem1.js
+++ src/problem1.js
@@ -2,10 +2,16 @@ function problem1(pobi, crong) {
   var answer;
   const pobi2DArray = comparePages(separateDigit(pobi));
   const crong2DArray = comparePages(separateDigit(crong));
-  if (pobi2DArray > crong2DArray) answer = 1;
+  if (exceptions(pobi) || exceptions(crong)) answer = -1;
+  else if (pobi2DArray > crong2DArray) answer = 1;
   else if (pobi2DArray < crong2DArray) answer = 2;
   else if (pobi2DArray === crong2DArray) answer = 0;
   return answer;
+}
+
+function exceptions(pages) {
+  if (Math.abs(pages[0] - pages[1]) !== 1) return 1;
+  if (pages.length < 2) return 1;
 }
 
 function separateDigit(pages) {
