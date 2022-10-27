da580df3a0bc12c9de3ea3543d03376589ddbc0b
hwangsungjae <edx2fire@likelion.org>
2022-10-27 16:58:21 +0900

    [온보딩 - 04] 청개구리 문제 솔루션 함수 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabets = [];
+  
+  for (let i = 65; i <= 90; i++) alphabets.push(String.fromCharCode(i));
+  
+  return [...word].map(char => {
+    if (char === ' ') return char;
+    const reversed = [...alphabets].reverse()[alphabets.indexOf(char.toUpperCase())];
+    return char.toUpperCase() === char ? reversed : reversed.toLowerCase();
+  }).join('');
 }
 
 module.exports = problem4;
