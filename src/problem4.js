75a548ceb839d917db64b6cca3662969337673bf
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-27 12:55:19 +0900

    feat: problem4.js에 알파벳을 변환하는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,29 @@
+const encryptLetter = (letter) => {
+  const upperCaseLetters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const lowerCaseLetters = 'abcdefghijklmnopqrstuvwxyz'.split('');
+
+  if (upperCaseLetters.includes(letter)) {
+    const index = upperCaseLetters.indexOf(letter);
+    const encryptedIndex = (upperCaseLetters.length - 1) - index;
+    
+    return upperCaseLetters[encryptedIndex];
+  }
+
+  if (lowerCaseLetters.includes(letter)) {
+    const index = lowerCaseLetters.indexOf(letter);
+    const encryptedIndex = (lowerCaseLetters.length - 1) - index;
+    
+    return lowerCaseLetters[encryptedIndex];
+  }
+
+  return letter;
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const letters = word.split('');
+  const encryptedLetters = letters.map((letter) => encryptLetter(letter));
+
+  return encryptedLetters.join('');
 }
 
 module.exports = problem4;
