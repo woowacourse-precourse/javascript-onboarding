43d78d07c8b58e177749a1ede80776e7ad9ec082
연 <mogoo22@naver.com>
2022-10-27 23:48:33 +0900

    feat(문제4): 아스키코드번호로 알파벳유무와 알파벳 대소문자를 판별한다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
+const [MIN_UPPER_ASCII, MAX_UPPER_ASCII] = [65, 90];
+const [MIN_LOWER_ASCII, MAX_LOWER_ASCII] = [97, 122];
+const isUpperCase = (string) => string.charCodeAt(0) >= MIN_UPPER_ASCII && string.charCodeAt(0) <= MAX_UPPER_ASCII;
+const isLowerCase = (string) => string.charCodeAt(0) >= MIN_LOWER_ASCII && string.charCodeAt(0) <= MAX_LOWER_ASCII;
+
+const defineStringType = (string) => {
+  if (isUpperCase(string)) return "upperCase";
+  if (isLowerCase(string)) return "lowerCase";
+  return "noAlphabet";
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = "";
+
+  for (const index in word) {
+    const string = word[index];
+    const stringType = defineStringType(string);
+    }
+
+  return result;
 }
 
 module.exports = problem4;
