ea205c64070d2a803bdd5b060a10036c1207deab
jangsumin <jsm0726@naver.com>
2022-10-27 20:19:33 +0900

    문제2 : 기능 구현 및 함수 분할

--- src/problem2.js
+++ src/problem2.js
@@ -4,8 +4,32 @@
 // 3. 하지만 위 과정을 여러번 거쳐서 더 이상 연속되는 중복 문자가 없어야 함
 
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = [];
+  let cryptogramToArr = [...cryptogram];
+  let confirmDuplicate = cryptogramToArr[0];
+  
+  answer.push(cryptogramToArr[0]);
+  
+  for (let i=1;i<cryptogramToArr.length;i++) {
+    if (cryptogramToArr[i] === confirmDuplicate) {
+      answer.pop();
+    } else {
+      answer.push(cryptogramToArr[i]);
+      confirmDuplicate = cryptogramToArr[i];
+    }
+  }
+  
+  if (!check(answer)) return problem2(answer.join(""));
+  else return answer.join("");
+}
+
+function check(array) {
+  for(let i=0;i<array.length-1;i++) {
+    if (array[i] === array[i + 1]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 module.exports = problem2;
