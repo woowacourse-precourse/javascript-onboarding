b66cc09bf52abbc5cdbabd4029c5a4c52378a450
KES <aprilsmaple@gmail.com>
2022-10-27 11:53:01 +0900

    feat: problem5 diving money till answer

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let curMoney = money;
+
+  return moneyType.map((type) => {
+    let quotient = parseInt(curMoney / type);
+    curMoney = curMoney % type;
+    return quotient;
+  });
 }
 
 module.exports = problem5;
