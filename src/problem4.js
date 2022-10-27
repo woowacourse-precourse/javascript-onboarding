b1adf9da012ca4807df00de8f9b10ad75271e7c4
su-lim <sujin.0xlim@gmail.com>
2022-10-27 18:24:05 +0900

    feat: prob4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
 function problem4(word) {
-  var answer;
+  var answer = [];
+
+  for(let i=0;i<word.length;i++){
+    let originChar = word[i];
+    let charCode = originChar.charCodeAt();
+    let changeChar = "";
+    // upperCase
+    if(charCode >= 65 && charCode <= 90){
+      changeChar = String.fromCharCode(charCode + (25-((charCode-65)*2)));
+    }
+    // lowerCase
+    else if(charCode >= 97 && charCode <= 122){
+      changeChar = String.fromCharCode(charCode + (25-((charCode-97)*2)));
+    }
+    answer.push(changeChar || originChar);
+  }
+  answer = answer.join('');
   return answer;
 }
 
