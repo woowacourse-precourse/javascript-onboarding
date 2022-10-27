df38481410728338d1d4494400fd4b238b632235
hsy <69149030+dev-redo@users.noreply.github.com>
2022-10-27 15:50:28 +0900

    [solved 5번] 스택 풀이

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
+const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+  const result = [];
+
+  for (let i = 0; i < MONEY_UNIT.length; i++) {
+    const currUnit = MONEY_UNIT[i];
+    const unitCnt = Math.floor(money / currUnit);
+
+    result.push(unitCnt);
+    money -= currUnit * unitCnt;
+  }
+
+  return result;
 }
 
 module.exports = problem5;
