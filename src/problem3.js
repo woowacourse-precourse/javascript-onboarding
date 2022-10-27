2aa604e24b7e236bc5e1a77ac7e6008c56429cbd
sery <yunsel2@gmail.com>
2022-10-27 15:35:11 +0900

    feat: Solve PROBLEM3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i < number + 1; i++) {
+    const num = i.toString().split('');
+
+    num.forEach((n) => {
+      if (n.includes(3)) {
+        answer++;
+      }
+      if (n.includes(6)) {
+        answer++;
+      }
+      if (n.includes(9)) {
+        answer++;
+      }
+    });
+  }
+
   return answer;
 }
 
