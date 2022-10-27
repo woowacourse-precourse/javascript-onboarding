4e7f14d2f3a45fc250e0c205bd9273153b4fa86f
Dean <yjj052800@gmail.com>
2022-10-28 02:00:49 +0900

    FEAT: [2번] 예외사항 처리 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  let decodeArr = [];
-
-  decode(cryptogram, decodeArr);
-  answer = decodeArr.join("");
-  return answer;
+  if (inputExecption(cryptogram)) {
+    var answer;
+    let decodeArr = [];
+    decode(cryptogram, decodeArr);
+    answer = decodeArr.join("");
+    return answer;
+  }
 }
 module.exports = problem2;
 
@@ -14,4 +15,11 @@ function decode(arr1, arr2) {
     if (arr2[arr2.length - 1] == char) arr2.pop();
     else arr2.push(char);
   }
+}
+
+function inputExecption(string) {
+  let inputCheck = true;
+  if (string.length < 1 || string.length > 1000) inputCheck = false;
+  else if (string !== string.toLowerCase()) inputCheck = false;
+  return inputCheck;
 }
