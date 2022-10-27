731a2ee0c0904b85a306c1e770ad4eb178b66871
dlwl98 <ff981113@hanyang.ac.kr>
2022-10-27 20:51:32 +0900

    refactor: problem7 기능별로 분리 , score를 클로저로 관리

--- src/problem7.js
+++ src/problem7.js
@@ -1,29 +1,55 @@
-function problem7(user, friends, visitors) {
-  const relation = {};
+function Score() {
   const score = {};
+  this.getScore = () => score;
+  this.setScore = (recommandFriend, additionalScore) => {
+    if (!score[recommandFriend]) score[recommandFriend] = 0;
+    score[recommandFriend] += additionalScore;
+    return score;
+  };
+}
+
+function getRelation(friends) {
+  const relation = {};
   friends.forEach(([user1, user2]) => {
     relation[user1] = [...(relation[user1] || []), user2];
     relation[user2] = [...(relation[user2] || []), user1];
   });
-  relation[user].forEach((friend) => {
-    relation[friend].forEach((knowTogether) => {
-      if (knowTogether === user) return;
-      if (!score[knowTogether]) score[knowTogether] = 0;
-      score[knowTogether] += 10;
-    });
+  return relation;
+}
+
+function setKnowTogetherScore(user, score, knowTogetherFriends) {
+  knowTogetherFriends.forEach((knowTogether) => {
+    if (knowTogether === user) return;
+    score.setScore(knowTogether, 10);
   });
+}
+
+function problem7(user, friends, visitors) {
+  const score = new Score();
+  const relation = getRelation(friends);
+
+  relation[user].forEach((friend) => {
+    setKnowTogetherScore(user, score, relation[friend]);
+  });
+
   visitors.forEach((visitor) => {
     if (relation[user].includes(visitor)) return;
-    if (!score[visitor]) score[visitor] = 0;
-    score[visitor] += 1;
+    score.setScore(visitor, 1);
   });
-  return Object.entries(score)
-    .sort(([user1Name, user1Score], [user2Name, user2Score]) => {
-      if (user2Score === user1Score) return user2Name > user1Name ? -1 : 1;
+
+  const result = Object.entries(score.getScore())
+    .sort((user1, user2) => {
+      const [user1Name, user1Score] = user1;
+      const [user2Name, user2Score] = user2;
+      if (user2Score === user1Score) {
+        return user2Name > user1Name ? -1 : 1;
+      }
       return user2Score - user1Score;
     })
-    .reduce((result, recommand) => [...result, recommand[0]], [])
+    .reduce((recommands, [name]) => [...recommands, name], [])
     .slice(0, 5);
+
+  return result;
 }
 
 module.exports = problem7;
