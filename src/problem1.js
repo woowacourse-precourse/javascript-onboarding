bc00289899faa599c11b4246a46e70c2c4d94ffa
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-10-27 12:50:29 +0900

    Feat: 기본적인 예외 처리 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,12 +1,26 @@
 function problem1(pobi, crong) {
   let answer;
-
+  
   for (let i = 0; i < 1; i++) {
     let pobiScore, crongScore = 0;
     pobiScore = Math.max(sum(pobi[i]), sum(pobi[i+1]), multiply(pobi[i]), multiply(pobi[i+1]))
     crongScore = Math.max(sum(crong[i]), sum(crong[i+1]), multiply(crong[i]), multiply(crong[i+1]))
-
-  }
+    
+    if ((pobi[i] + 1 === pobi[i + 1]) && (crong[i] + 1 === crong[i + 1])) {
+        if (pobiScore > crongScore) {
+        answer = 1
+      } else if (pobiScore < crongScore) {
+        answer = 2
+      } else if (pobiScore == crongScore) {
+        answer = 0
+      } else {
+        answer = -1
+      }
+    } else {
+      answer = -1
+    }    
+  
+}
 
 
   return answer;
