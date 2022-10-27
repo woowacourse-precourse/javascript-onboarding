381fa4dd65d8005645454ce7a09b8497b4f80085
Deemou <4106243@naver.com>
2022-10-28 01:13:45 +0900

    feat<P1>: 페이지 유효성 확인
    연속된 숫자여야 한다.
    페이지 범위 내의 숫자여야 한다.
    시작 면(시작 페이지 포함)과 마지막 면은 포함하지 않는다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,20 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  if (!isValidPages(pobi) || !isValidPages(crong)) {
+    return (answer = -1);
+  }
+
   return answer;
 }
+const isValidPages = (pages) => {
+  if (pages[1] - pages[0] != 1) {
+    return false;
+  } else if (pages[0] <= 1 || pages[1] >= 400) {
+    return false;
+  } else {
+    return true;
+  }
+};
 
 module.exports = problem1;
