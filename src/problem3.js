185657406920c057b08f56390f87d6588e47f7af
parkjikuk <paxkk@naver.com>
2022-10-28 03:18:45 +0900

    feat : 3, 6, 9 의 개수 만큼 손뼉을 치는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
+function clap(num) {
+  let clap = 0;
+  let str = String(num);
+
+  for(let i = 0; i < str.length; i ++) {
+    
+    if(str[i] === "3" || str[i] === "6" || str[i] === "9") {
+      clap ++;
+    }
+  }
+
+  return clap;
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  
 }
 
 module.exports = problem3;
