59b05ec1c9f8a54d6d5825fc9d49b9e54911396e
Ryong-E <think8867@gmail.com>
2022-10-27 13:58:10 +0900

    feat: 해당 숫자에 3,6,9들어가있는 수량을 변수에 저장

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++){
+    if(String(i).match(/3/g)?.length >= 1){
+      answer += String(i).match(/3/g)?.length;
+    }
+    if(String(i).match(/6/g)?.length >= 1){
+      answer += String(i).match(/6/g)?.length;
+    }
+    if(String(i).match(/9/g)?.length >= 1){
+      answer += String(i).match(/9/g)?.length;
+    }
+  }
+
   return answer;
 }
 
