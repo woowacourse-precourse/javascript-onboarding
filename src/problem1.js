099682fefacdd87f694016e0a0e338dd22e96ec6
saram <yujun1018@naver.com>
2022-10-28 01:05:41 +0900

    [문제1] feat: 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,10 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+function problem1(pobi, crong) {}
+
+const getBiggerNumber = (pageList, index) => {
+  const digitList = [...pageList[index].toString()].map((v) => Number(v));
+  const plus = digitList.reduce((prev, cur) => prev + cur, 0);
+  const multiply = digitList.reduce((prev, cur) => prev * cur, 1);
+  return Math.max(plus, multiply);
+};
 
 module.exports = problem1;
