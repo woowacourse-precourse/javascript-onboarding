d327adc7430b09acc7efe0de47a6521e54714258
foundaway98 <foundaway98@naver.com>
2022-10-27 20:22:49 +0900

    문자 변환 기능 구현 및 실행 결과 출력 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  var answer = [];
+  word = [...word];
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].charCodeAt(0) > 64 && word[i].charCodeAt(0) < 91) {
+      answer.push(String.fromCharCode(155 - word[i].charCodeAt(0)));
+    } else if (word[i].charCodeAt(0) > 96 && word[i].charCodeAt(0) < 123) {
+      answer.push(String.fromCharCode(219 - word[i].charCodeAt(0)));
+    } else {
+      answer.push(word[i]);
+    }
+  }
+  answer = answer.join("");
   return answer;
 }
 
