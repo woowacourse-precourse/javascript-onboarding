91438d28d780044a81551e70867a69ad225d803d
하송희 <99heesong@naver.com>
2022-10-27 12:16:11 +0900

    [문제 1] 예외처리: return값이 -1인 영역 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,27 @@
+const isOutOfRange = (num) => {
+  if(num===1||num===399) return true;
+  return false;
+}
+
+const isNonSeriesArray = ([prev,next]) => {
+  if(next-prev===1) return false;
+  return true;
+}
+
+const isOdd= (num)=> num % 2
+
+function isException(arr) {
+  if(isOutOfRange(arr[0])||isOutOfRange(arr[1])) return true;
+  if(isNonSeriesArray(arr)) return true;
+  if(!isOdd(arr[0])) return true;
+  return false;
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  if(isException(pobi)||isException(crong)) return -1;
+  
   return answer;
 }
 
