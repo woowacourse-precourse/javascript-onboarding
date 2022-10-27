a127598db4b2f04e1efb7e54991349b999089677
feb-dain <feb.dain@gmail.com>
2022-10-28 05:35:21 +0900

    feat: [problem3] find 3, 6, 9

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,6 @@
 function problem3(number) {
   const makeArray = String([...Array(number+1).keys()]).split("");
+  const findThreeSixNine = makeArray.filter(num => ["3", "6", "9"].includes(num));
   
   return answer;
 }
