364f981a3b8a129586900df052f631e9b2bbaf12
sumin <csumin008255@gmail.com>
2022-10-27 13:36:04 +0900

    problem1_기능목록수

--- src/problem1.js
+++ src/problem1.js
@@ -2,7 +2,9 @@
 * 1. pobi와 crong의 페이지 번호 각 자리 숫자를 모두 곱한 값, 더한 값을 배열 pobi_num, crong_num에 저장
 * 2. pobi_num과 crong_num에서 큰 수 찾기
 * 3. pobi와 crong의 큰 수를 비교, 결과 출력
+* 4. 코드 정리
 */
+
 
 
 function problem1(pobi, crong) {
