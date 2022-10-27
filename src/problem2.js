893e254af62e96c9ebe1f2afbdc9e8432599d3ac
kyw0716 <kyw0716@naver.com>
2022-10-27 20:57:45 +0900

    refactor: problem2 변수명 변경
    가독성을 높이기 위해 직관적인 변수명으로 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,10 @@
 function problem2(cryptogram) {
   let answer = cryptogram;
-  let regexp =
+  let repeatCheckRegexp =
     /a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}/;
 
-  while (checkIsRepeatExist(answer, regexp)) {
-    answer = deleteRepeat(answer, regexp);
+  while (checkIsRepeatExist(answer, repeatCheckRegexp)) {
+    answer = deleteRepeatLetter(answer, repeatCheckRegexp);
   }
 
   return answer;
@@ -14,7 +14,7 @@ function checkIsRepeatExist(cryptogram, regexp) {
   return cryptogram.match(regexp) !== null;
 }
 
-function deleteRepeat(cryptogram, regexp) {
+function deleteRepeatLetter(cryptogram, regexp) {
   return cryptogram.replace(regexp, "");
 }
 
