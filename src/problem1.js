a601fcbcc95a8ea86e8a0f79a8a29596058dc696
semnil5202 <semnil5202@gmail.com>
2022-10-27 19:58:26 +0900

    문제1번. 페이지를 각 자릿수로 구분 후 2차원 배열로 반환

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
 function problem1(pobi, crong) {
   var answer;
+  const pobi2DArray = separateDigit(pobi);
+  const crong2DArray = separateDigit(crong);
   return answer;
+}
+
+function separateDigit(pages) {
+  const firstPage = String(pages[0])
+    .split("")
+    .map((i) => Number(i));
+  const secondPage = String(pages[1])
+    .split("")
+    .map((i) => Number(i));
+  return [firstPage, secondPage];
 }
 
 module.exports = problem1;
