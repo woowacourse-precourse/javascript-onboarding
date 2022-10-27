cb4c1f47a32e524c90166577ccc1b757c33ba35b
freemoon99 <soloist132@gmail.com>
2022-10-28 00:22:52 +0900

    fix[problem1]: 연산 기능 return값 오류 수정

--- src/problem1.js
+++ src/problem1.js
@@ -4,19 +4,21 @@ function problem1(pobi, crong) {
   const sumfn = (num) => {
     var sum = 0;
     for (var j = 0; j < String(num).length; j++) {
-      return sum += parseInt(String(num)[j]);
+      sum += parseInt(String(num)[j]);
     }
+    return sum;
   } 
 
   const mulfn = (num) => {
     var mul = 1;
     for (var k = 0; k < String(num).length; k++) {
-      return mul *= parseInt(String(num)[k]);
+       mul *= parseInt(String(num)[k]);
     }
+    return mul
   }
 
   const maxPoint = (arr) => {
-    Math.max(sumfn(arr[0]),sumfn(arr[1]), mulfn(arr[0]), mulfn(arr[1]));
+    return Math.max(sumfn(arr[0]),sumfn(arr[1]), mulfn(arr[0]), mulfn(arr[1]));
   }
   
   if(pobi[1]-pobi[0] != 1 || crong[1]-crong[0] !=1) answer = -1;
