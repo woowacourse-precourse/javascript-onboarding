f8ccbbe5f291a8a2eaa8e004310ac54fa111680c
ctd <qpflapffhs76@gmail.com>
2022-10-27 16:08:34 +0900

    feat: form을 순회하면서 닉네임을 2글자씩 분리하며, 갯수를 세는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,17 @@ function problem6(forms) {
   return answer;
 }
 
+function makeDuplicatedObj(forms) {
+  const duplicatedObj = {};
+  forms.forEach((form) => {
+    const [_, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substring(i, i + 2);
+      if (testText in duplicatedObj) duplicatedObj[testText] += 1;
+      else duplicatedObj[testText] = 1;
+    }
+  });
+  return duplicatedObj;
+}
+
 module.exports = problem6;
