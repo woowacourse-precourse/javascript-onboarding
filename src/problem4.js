7784380cd7b49e70324322306a16654796659ca5
Hwang Seong Taek <hst007@naver.com>
2022-10-27 13:13:24 +0900

    feat: problem4 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -7,8 +7,22 @@
 */
 
 function problem4(word) {
-  var answer;
-  return answer;
+  const result = [];
+
+  for (let i=0; i<word.length; i++) {
+    const ascii = word.charCodeAt(i);
+    if (ascii === 32) {
+      result.push(' ');
+    } else if (ascii >= 65 && ascii <= 90) {
+      result.push(String.fromCharCode(90 - (ascii - 65)));
+    } else if (ascii >= 97 && ascii <= 122) {
+      result.push(String.fromCharCode(122 - (ascii - 97)));
+    } else {
+      result.push(word[i]);
+    }
+  }
+
+  return result.join('');
 }
 
 module.exports = problem4;
