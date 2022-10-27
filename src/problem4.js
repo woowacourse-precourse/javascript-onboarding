2cb06ccb317d9083cd3e63068f769c7881b1c1a8
Ryong-E <think8867@gmail.com>
2022-10-27 14:45:19 +0900

    feat: 해당 단어 청개구리 사전대로 변환하여 저장

--- src/problem4.js
+++ src/problem4.js
@@ -2,7 +2,11 @@ function problem4(word) {
   let answer = word.split('');
   for (let i = 0; i < word.length; i++){
     if (answer[i] >= 'A' && answer[i] <= 'Z'){
+      const convertWord = 90 - (answer[i].charCodeAt(0) - 65);
+      answer[i] = String.fromCharCode(convertWord);
     } else if (answer[i] >= 'a' && answer[i] <= 'z'){
+      const convertWord = 122 - (answer[i].charCodeAt(0) - 97);
+      answer[i] = String.fromCharCode(convertWord);
     }
   }
   answer = answer.join('');
