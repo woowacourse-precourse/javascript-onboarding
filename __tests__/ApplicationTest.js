8c274bc4e7ad3f35b808908623965267833f8903
min <hororonoodle@gmail.com>
2022-10-27 23:55:09 +0900

    feat : solve problem2

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -7,24 +7,24 @@ const problem6 = require('../src/problem6');
 const problem7 = require('../src/problem7');
 
 describe('problem1', () => {
-  test.only('case1', () => {
+  test('case1', () => {
     expect(problem1([97, 98], [197, 198])).toEqual(0);
   });
 
-  test.only('case2', () => {
+  test('case2', () => {
     expect(problem1([131, 132], [211, 212])).toEqual(1);
   });
 
-  test.only('case3', () => {
+  test('case3', () => {
     expect(problem1([99, 102], [211, 212])).toEqual(-1);
   });
 });
 
 describe('problem2', () => {
-  test('case1', () => {
+  test.only('case1', () => {
     expect(problem2('browoanoommnaon')).toEqual('brown');
   });
-  test('case2', () => {
+  test.only('case2', () => {
     expect(problem2('zyelleyz')).toEqual('');
   });
 });
--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let cryptArr = [...cryptogram];
+  let cnt = 0;
+  while (cnt !== cryptArr.length - 1 && cryptArr.length !== 0) {
+    if (cryptArr[cnt] === cryptArr[cnt + 1]) {
+      cryptArr.splice(cnt, 2);
+      cnt = 0;
+    } else {
+      cnt++;
+    }
+  }
+  return cryptArr.join('');
 }
 
 module.exports = problem2;
