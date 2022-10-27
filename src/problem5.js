4860c60f56c6053771b877fd2a60b9d15e3b0721
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-27 22:38:40 +0900

    feat<problem5>: Resolve problem5 without exception

--- src/problem5.js
+++ src/problem5.js
@@ -7,7 +7,13 @@
  */
 
 function problem5(money) {
-  var answer;
+  var answer = [];
+  var units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  units.map((unit) => {
+    answer.push(parseInt(money / unit));
+    money = parseInt(money % unit);
+  })
   return answer;
 }
 
