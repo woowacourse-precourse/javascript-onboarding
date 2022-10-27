dd072f0ee60e3216198baddb45974ae6b2f0c0e0
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 10:55:14 +0900

    feat: 중복 문자 없을 때까지 삭제 반복하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,22 @@
 function problem2(cryptogram) {
   var answer;
+
+  const decryptString = (string) => {
+    for (let i = 0; i < string.length - 1; i++) {
+      for (let j = i + 1; j < string.length - 1; j++) {
+        if (string[i] != string[j]) {
+          break;
+        }
+        if (string[i] == string[j] && string[i] != string[j + 1]) {
+          string = string.slice(0, i) + string.slice(j + 1);
+          return decryptString(string);
+        }
+      }
+    }
+    return string;
+  };
+
+  answer = decryptString(string);
   return answer;
 }
 
