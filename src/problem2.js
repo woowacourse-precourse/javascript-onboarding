355719ad9876e5cda0d07bd51f02e55508f063bb
TaeHyung KIM <th1122kim@gmail.com>
2022-10-27 21:27:41 +0900

    problem2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let checked = true;
+  while (checked) {
+    checked = false;
+    for (let i = 0; i < cryptogram.length - 1; i++) {
+      if (cryptogram[i] === cryptogram[i + 1]) {
+        cryptogram = cryptogram.slice(0, i) + cryptogram.slice(i + 2);
+        i--;
+        checked = true;
+      }
+    }
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
