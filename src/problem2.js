e96c58b498b71f794e9ac8c8079bf74d944ba4ea
cham-min <hymn9613@gmail.com>
2022-10-28 02:00:12 +0900

    feat: 문제2 - 연이은 중복 제외 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const answer = [cryptogram[0]];
+
+  for (i = 1; i <= cryptogram.length; i++) {
+    if (answer[answer.length - 1] === cryptogram[i]) answer.pop();
+    else answer.push(cryptogram[i]);
+  }
+
+  return answer.join("");
 }
 
 module.exports = problem2;
