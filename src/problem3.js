f95b9b49d7be04f2fa0291621fdfec76da1afd55
kyhyun <k29176@gmail.com>
2022-10-27 15:49:34 +0900

    feat : 문제 3번 - 숫자를 카운트 해주는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,22 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let totalClap = 0;
+  for (let sequence = 1; sequence <= number; sequence++) {
+    totalClap += countClap(sequence.toString());
+  }
+  return totalClap;
+}
+
+function countClap(sequenceToString) {
+  const validNumbers = [3, 6, 9];
+  let clap = 0;
+  for (let i = 0; i < sequenceToString.length; i++) {
+    for (let j = 0; j < validNumbers.length; j++) {
+      if (parseInt(sequenceToString[i]) === validNumbers[j]) {
+        clap++;
+      }
+    }
+  }
+  return clap;
 }
 
 module.exports = problem3;
