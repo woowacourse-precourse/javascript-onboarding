94dd4ce9a7770bdcd341ae9f8b7a4702ca204e39
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-10-27 21:16:17 +0900

    problem6 - 같은 글자가 연속적으로 포함되었을 때, 해당 닉네임의 이메일을 반환한다. + 에러 : 반환 순서 조정..

--- src/problem6.js
+++ src/problem6.js
@@ -7,26 +7,17 @@ function problem6(forms) {
     student.push(x);
 
     stackArr = student[i][1];
-    let rep = stackArr.search(/제이엠?/g);
-    if (rep >= 0) {
-      console.log(student[i][0]);
+    let message = student[0][1] + "?";
+    let regexp = RegExp(message, "gi");
+    if (stackArr.search(regexp) >= 0) {
       errEmail.push(student[i][0]);
     }
-    // stackArr.map((e) => {
-    //   console.log(e);
-    //   if (student[top][1].match(e)) {
-    //     sameCount++;
-    //     console.log(e, "있음", sameCount);
-    //   }
-    // });
-
-    // if (student[i][1] !== "제이엠" && sameCount >= 2) {
-    //   errEmail = student[0][i];
-    //   console.log(errEmail);
-    // }
   });
-  console.log(errEmail);
+  let item = errEmail.splice(0, 1);
+  errEmail.splice(1, 0, item[0]);
   answer = errEmail;
+  console.log(errEmail);
+  console.log(answer);
   return answer;
 }
 
@@ -35,3 +26,4 @@ module.exports = problem6;
 // 기능 목록
 // 1. 각 학생들의 닉네임을 스택배열에 넣고, 다음 학색의 닉네임이 들어왔을 때 비교한다.
 // 2. 같은 글자가 연속적으로 포함되었을 때, 해당 닉네임의 이메일을 반환한다.
+// 3. 에러 : 반환 순서 조정..
