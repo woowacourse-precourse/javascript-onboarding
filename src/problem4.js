a24a1baf8d2d75d68333385ed8d0080e8d1f43fb
JH <vvaa277@gmail.com>
2022-10-27 22:14:40 +0900

    Feat : 기능목록 2~3 구현
    #P4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,9 @@
 function problem4(word) {
-  let lesson = word.toUpperCase().split("")
+  let lesson = word.toUpperCase().split("");
+  const normArr = Array.from({ length: 26 }, (v, i) =>
+    String.fromCharCode(i + 65)
+  );
+  const reverseArr = [...normArr].reverse();
 }
 
 module.exports = problem4;
