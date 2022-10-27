3c213f52c876d5ec9621d194861bfce25a04e9ea
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-27 23:48:02 +0900

    feat(문제4):  알파벳을 반대로 변환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,12 @@
+const getReverseAlphabet = (char) => {
+  const charASCII = char.charCodeAt();
+  if (charASCII >= 97 && charASCII <= 122)
+      return String.fromCharCode(97 + 122 - charASCII);
+  if (charASCII >= 65 && charASCII <= 90)
+      return String.fromCharCode(65 + 90 - charASCII);
+  return char;
+}
+
 function problem4(word) {
   let reverseWord = word.split('').map(c => getReverseAlphabet(c)).join('');
   return reverseWord;    
