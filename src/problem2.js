cdc317c3f40f35c2bbfd34da4872f3bb2ceee6af
TaeeunKim <xodms0309@naver.com>
2022-10-27 20:43:12 +0900

    [2번] feat: 스택이 비어있는 경우 처리

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,17 @@
 function problem2(cryptogram) {
   var answer;
+
+  const stack = [];
+  let top = '';
+
+  for (const alphabet of cryptogram) {
+    if (stack.length === 0) {
+      stack.push(alphabet);
+      top = alphabet;
+      continue;
+    }
+  }
+
   return answer;
 }
 
