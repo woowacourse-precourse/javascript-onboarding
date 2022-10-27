863d883b9a5edd8c34e4c115cd080e5cb4a6531e
TaeHyung KIM <th1122kim@gmail.com>
2022-10-27 22:48:02 +0900

    problem6 solved

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,27 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let result = [];
+  let obj = {};
+  forms.forEach((el) => {
+    let basename = el[1];
+    let email = el[0];
+    for (let i = 0; i < basename.length - 1; i++) {
+      let str = basename[i];
+      for (let k = i + 1; k < basename.length; k++) {
+        str += basename[k];
+        if (obj[str]) {
+          obj[str] = [email, ...obj[str]];
+        } else {
+          obj[str] = [email];
+        }
+      }
+    }
+  });
+  Object.values(obj).forEach((el) => {
+    el.length >= 2 ? result.push(...el) : null;
+  });
+  let res_set = new Set(result);
+  result = [...res_set].sort();
+  return result;
 }
 
 module.exports = problem6;
