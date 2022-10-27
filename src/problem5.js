616bb5a75525c76c17605a75b26092dc859f2914
BB-choi <choibobae0809@gmail.com>
2022-10-27 10:13:58 +0900

    feat: 문제5 - 지폐 종류에 따른 출금 함수 분리

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,33 @@
+function withrawCash({ wallet, money }) {
+  let remainingMoney = money;
+
+  const bills = Array.from(wallet.keys());
+
+  for (const bill of bills) {
+    console.log(bill);
+  }
+}
+
 function problem5(money) {
-  var answer;
+  let answer;
+
+  const wallet = new Map()
+    .set(50000, 0)
+    .set(10000, 0)
+    .set(5000, 0)
+    .set(1000, 0)
+    .set(500, 0)
+    .set(100, 0)
+    .set(50, 0)
+    .set(10, 0)
+    .set(1, 0);
+
+  withrawCash({ wallet, money });
+
   return answer;
 }
+
+console.log(problem5(50237));
+console.log(problem5(15000));
 
 module.exports = problem5;
