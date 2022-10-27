2bb0782b6025006aca119a5f779838af6cc72e61
eunha <dmsgk2323@naver.com>
2022-10-27 10:14:58 +0900

    2번 문제 풀이 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let str = cryptogram;
+
+  for (let i = 0; i < str.length * 2; i++) {
+    str = str.replace(/(.)\1+/, "");
+  }
+
+  return str;
 }
 
 module.exports = problem2;
