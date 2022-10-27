9394189d5980d7fce3651e0a6c36a886d7322047
JH <vvaa277@gmail.com>
2022-10-27 13:17:21 +0900

    Docs, Feat : 기능목록 작성, 기능목록 1~3 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,14 @@
 function problem2(cryptogram) {
+     
   let splited = cryptogram.split("");
   let noDup = [];
 
-  for (let el of splited ) {
-      (el === noDup[noDup.length-1]) ? noDup.pop() : noDup.push(el)
-    } 
+  for (let el of splited) {
+    el === noDup[noDup.length - 1] ? noDup.pop() : noDup.push(el);
+  }
 
-  
+  const answer = noDup.join("");
+  return answer;
 }
 
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  const num = number.toString().split("");
+
+  for (el of num) {
+    if (el % 3 === 0 && el > 0) {
+      count += 1;
+    }
+  }
+  console.log(count);
 }
 
 module.exports = problem3;
