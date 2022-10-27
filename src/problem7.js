34318441dbf9c53baf993980be1056bb2b581633
Jiwoo Park <jiwoo9446@naver.com>
2022-10-27 16:52:12 +0900

    problem7 solved

--- src/problem7.js
+++ src/problem7.js
@@ -1,19 +1,35 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let map = new Map();
+  let set = new Set();
+  let answer = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      if (friends[i][0] === user) set.add(friends[i][1]);
+      else set.add(friends[i][0]);
+    }
+  }
+  let direct = Array.from(set);
+  for (let friend of friends) {
+    const [A, B] = friend;
+    if (direct.includes(A) && B !== user) map.set(B, map.get(B) + 10 || 10);
+    else if (direct.includes(B) && A !== user)
+      map.set(A, map.get(A) + 10 || 10);
+  }
+
+  visitors.forEach((v) => {
+    if (!direct.includes(v) && v !== user) map.set(v, map.get(v) + 1 || 1);
+  });
+  let sorted = [...map.entries()]
+    .sort((a, b) => b[1] - a[1])
+    .sort((a, b) => {
+      if (a[1] == b[1]) return a[0].localeCompare(b[0]);
+    });
+
+  for (let [k, _] of sorted) {
+    if (answer.length >= 5) break;
+    answer.push(k);
+  }
   return answer;
 }
-console.log(
-  problem7(
-    "mrko",
-    [
-      ["donut", "andole"],
-      ["donut", "jun"],
-      ["donut", "mrko"],
-      ["shakevan", "andole"],
-      ["shakevan", "jun"],
-      ["shakevan", "mrko"],
-    ],
-    ["bedi", "bedi", "donut", "bedi", "shakevan"]
-  )
-);
+
 module.exports = problem7;
