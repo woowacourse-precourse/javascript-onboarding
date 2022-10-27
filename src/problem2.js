1e55a58f5b0cfc63c0c6ad41ddc970c0369a8b15
bassyu <yrkdgus@gmail.com>
2022-10-27 15:22:02 +0900

    문제2 오타 수정

--- src/problem2.js
+++ src/problem2.js
@@ -13,7 +13,7 @@ function problem2(cryptogram) {
 function encoding(encoded) {
   let newEncoded = encoded;
   for (let i = 1; i < encoded.length; i++) {
-    if (encoded[i-1] == encoded[i]) {
+    if (encoded[i-1] === encoded[i]) {
       newEncoded = encoded.slice(0, i-1) + encoded.slice(i+1);
       break;
     }
