9f6d99c1e7ef978b86da51daad55d36f0c47932a
jsh3418 <albert5428@gmail.com>
2022-10-27 22:03:54 +0900

    refactor: problem1 왼쪽, 오른쪽 각 자리 숫자를 더하고 곱하는 로직이 같아 배열로 리턴하도록 구현

--- src/problem1.js
+++ src/problem1.js
@@ -5,56 +5,42 @@ function problem1(pobi, crong) {
   const DRAW = 0;
   const EXCEPTIONS = -1;
 
-  // 1. 책의 페이지가 연속된 숫자이어야 한다. (예외사항)
+  // 책의 페이지가 연속된 숫자이어야 한다. (예외사항)
   if (pobi[0] !== pobi[1] - 1 || crong[0] !== crong[1] - 1) return EXCEPTIONS;
 
-  // 2. 포비와 크롱의 왼쪽 페이지 번호의 각 자리 숫자를 모두 더한 값을 구한다.
-  const sumLeftPageByPobi = String(pobi[0])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc + cur);
+  // 포비와 크롱의 왼쪽, 오른쪽 페이지 번호의 각 자리 숫자를 모두 더한 값을 구하여 배열로 할당한다.
+  const sumPagesByPobi = pobi.map((page) => {
+    const split = String(page).split("").map(Number);
+    const sum = split.reduce((acc, cur) => acc + cur);
 
-  const sumLeftPageByCrong = String(crong[0])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc + cur);
+    return sum;
+  });
 
-  // 3. 포비와 크롱의 왼쪽 페이지 번호의 각 자리 숫자를 모두 곱해 가장 큰 수를 구한다.
-  const multiplyLeftPageByPobi = String(pobi[0])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc * cur);
+  const sumPagesByCrong = crong.map((page) => {
+    const split = String(page).split("").map(Number);
+    const sum = split.reduce((acc, cur) => acc + cur);
 
-  const multiplyLeftPageByCrong = String(pobi[0])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc * cur);
+    return sum;
+  });
 
-  // 4. 포비와 크롱의 오른쪽 페이지 번호의 각 자리 숫자를 모두 더한 값을 구한다.
-  const sumRightPageByPobi = String(pobi[1])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc + cur);
+  // 포비와 크롱의 왼쪽, 오른쪽 페이지 번호의 각 자리 숫자를 모두 곱한 값을 구하여 배열로 할당한다.
+  const multiplyPagesByPobi = pobi.map((page) => {
+    const split = String(page).split("").map(Number);
+    const multiply = split.reduce((acc, cur) => acc * cur);
 
-  const sumRightPageByCrong = String(crong[1])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc + cur);
+    return multiply;
+  });
 
-  // 5. 포비와 크롱의 오른쪽 페이지 번호의 각 자리 숫자를 모두 곱한 값을 구한다.
-  const multiplyRightPageByPobi = String(pobi[1])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc * cur);
+  const multiplyPagesByCrong = crong.map((page) => {
+    const split = String(page).split("").map(Number);
+    const multiply = split.reduce((acc, cur) => acc * cur);
 
-  const multiplyRightPageByCrong = String(crong[1])
-    .split("")
-    .map(Number)
-    .reduce((acc, cur) => acc * cur);
+    return multiply;
+  });
 
-  // 6. 총 네가지의 값중 가장 큰 수를 구한다. 그리고 가장 큰 수를 본인의 점수로 한다.
-  const pobiScore = Math.max(...[sumLeftPageByPobi, multiplyLeftPageByPobi, sumRightPageByPobi, multiplyRightPageByPobi]);
-  const crongScore = Math.max(...[sumLeftPageByCrong, multiplyLeftPageByCrong, sumRightPageByCrong, multiplyRightPageByCrong]);
+  // 총 네가지의 값중 가장 큰 수를 구한다. 그리고 가장 큰 수를 본인의 점수로 한다.
+  const pobiScore = Math.max(...[...sumPagesByPobi, ...multiplyPagesByPobi]);
+  const crongScore = Math.max(...[...sumPagesByCrong, ...multiplyPagesByCrong]);
 
   if (pobiScore > crongScore) return POBI_WIN;
   if (pobiScore < crongScore) return CRONG_WIN;
