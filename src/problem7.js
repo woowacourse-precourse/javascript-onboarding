c67535ebf038ea78a0acab1bac307824abe65ad4
Miniling <bsm3925@naver.com>
2022-10-28 01:45:52 +0900

    [P7] Add/recommendFriends

--- src/problem7.js
+++ src/problem7.js
@@ -13,6 +13,19 @@ function problem7(user, friends, visitors) {
   }
 
   // friends에서 user와 친구로 되어있는 사용자와 친구인 사용자는 추천O (10점씩)
+  const users_reco = [];
+
+  const recommendFriends = () => {
+    for (let i = 0; i < friends.length; i++) {
+      for (let j = 0; j < users_friend.length; j++) {
+        if ((friends[i].includes(users_friend[j]) > 0)
+          && (friends[i].includes(user) == false)) {
+          users_reco.push(friends[i][1]);
+        }
+      }
+    }
+  }
+
   // visitors에서 추천 사용자의 중복횟수 * 3
   // 추천 점수가 부여된 사용자의 목록을 점수-이름 순으로 정렬
 
