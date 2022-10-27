4869171b96fce01a459f73e3f02ca26a58e27fd9
zktm9903 <zktm9903@naver.com>
2022-10-27 17:37:52 +0900

    problem2 solve

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,25 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const cryptoArr = cryptogram.split('');
+
+  for(let i=1;i<cryptoArr.length;i++){
+    // 전 문자와 일치하는지 검사
+    if(cryptoArr[i-1] === cryptoArr[i]){
+      let cnt = 2;
+      let nowIdx = i+1;
+
+      while(true){
+        // 현재 문자 뒤로 몇개가 일치하는지 체크
+        if(cryptoArr[nowIdx-1] === cryptoArr[nowIdx++]) cnt++;
+        else break;
+      }
+
+      //전 문자부터 일치하는 갯수만큼 배열 제거
+      cryptoArr.splice(--i, cnt);
+      i--;
+    }
+  }
+
+  return cryptoArr.join('');
 }
 
 module.exports = problem2;
