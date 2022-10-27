7254c500234ed017c5afd98175888c3d3d9459b8
pione3r <jhyong0626@gmail.com>
2022-10-27 12:30:08 +0900

    [온보딩] 용준현 문제 3번 기능 구현 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
 function problem3(number) {
   var answer;
+
+  let count = 0;
+
+  // 조건 1. 인수로 받은 숫자만큼 반복문 돌리기
+  for (let i = 1; i <= number; i++) {
+    // 조건 2. 해당 인덱스에서 각 자릿수에 
+    // 3, 6, 9가 들어가는지 확인하기
+    for (let char of String(i)) {
+      if (char == "3" || char == "6" || char == "9") count++;
+    }
+  }
+
+  answer = count;
   return answer;
 }
 
