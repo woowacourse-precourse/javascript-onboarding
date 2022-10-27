d6b73a76524c4935ac54e722d1320d73a5d17ca6
하송희 <99heesong@naver.com>
2022-10-27 18:28:41 +0900

    [문제 2] 기능추가: 중복 제거하며 새 배열로 문자열 이동

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,19 @@
+const isLowerCase = (string) => /^[a-z]*$/.test(string)
+
 function problem2(cryptogram) {
-  const isLowerCase = (string) => /^[a-z]*$/.test(string)
   if(!isLowerCase(cryptogram)) throw Error('문자열이 소문자만으로 이루어지지 않았습니다.')
   if(cryptogram.length<1||cryptogram.length>1000) throw Error('문자열의 길이가 맞지 않습니다.');
-}
+
+  const stack = [];
+  cryptogram.split("").forEach((char)=>{
+    if(stack[stack.length-1]===char) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }  
+  })
+
+  return stack.join("");
+  }
 
 module.exports = problem2;
