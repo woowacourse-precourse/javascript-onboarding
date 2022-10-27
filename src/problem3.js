338ac70dec3518b30a9b73ab37c8da7ecc86ffd8
su-lim <sujin.0xlim@gmail.com>
2022-10-27 16:07:56 +0900

    feat: prob3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,19 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for(let i=0; i<number; i++){
+    current = i+1;
+
+    while(current !== 0){
+      remainder = current % 10;
+      current = parseInt(current / 10);
+
+      if(remainder === 3 || remainder === 6 || remainder === 9){
+        answer++;
+      }
+    }
+  }
+
   return answer;
 }
 
