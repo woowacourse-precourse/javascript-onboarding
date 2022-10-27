853ce8e444f2b3b55ae46144af06fdf883774b09
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-27 23:55:37 +0900

    problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,14 @@
 function problem4(word) {
   var answer;
+  var originArr = word.split("").map((each) => each.charCodeAt());
+  var newArr = originArr.map((each) => {
+    if (each >= 65 && each <= 90) {
+      return 155 - each;
+    } else if (each >= 97 && each <= 122) {
+      return 219 - each;
+    } else return each;
+  });
+  answer = String.fromCharCode(...newArr);
   return answer;
 }
 
