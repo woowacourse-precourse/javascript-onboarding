3f74054123d4532c1a98d2403705a17617b8f159
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-27 21:32:40 +0900

    fix problem2

--- src/problem2.js
+++ src/problem2.js
@@ -3,19 +3,23 @@ function problem2(cryptogram) {
   var cryptogramString = cryptogram;
   var bundle = 0;
   var flag = 1;
-  while (cryptogramString.length > 1 && flag > 0) {
-    answer = "";
-    flag = 0;
-    for (let i = 0; i < cryptogramString.length - 1; i++) {
-      if (cryptogramString[i] == cryptogramString[i + 1]) {
-        flag++;
-        bundle++;
-      } else {
-        if (bundle == 0) answer += cryptogramString[i];
-        else bundle = 0;
-        if (i == cryptogramString.length - 2) answer += cryptogramString[i + 1];
-      }
+  function DFS(i) {
+    if (i == cryptogramString.length - 1) return;
+    if (cryptogramString[i] != cryptogramString[i + 1]) {
+      if (bundle == 0) {
+        answer += cryptogramString[i];
+      } else bundle = 0;
+      if (i == cryptogramString.length - 2) answer += cryptogramString[i + 1];
+    } else {
+      flag++;
+      bundle++;
     }
+    DFS(i + 1);
+  }
+  while (cryptogramString.length > 1 && flag > 0) {
+    flag = 0;
+    answer = "";
+    DFS(0);
     cryptogramString = answer;
   }
   return answer;
