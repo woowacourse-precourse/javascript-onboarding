b4d1b44108e694944a0115a39783044aadf6ff10
ilgon0110 <moses011010@gmail.com>
2022-10-27 17:04:01 +0900

    feat: 연산 기능 구현(#problem6)

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
+  let answer;
+  //연산
+  //기능목록 1-1 구현
+  const isRepeat = (str1, str2) => {
+    for (let rt = 0; rt < str2.length; rt++) {
+      let cnt = 0;
+      let tmp = rt;
+      for (let lt = 0; lt < str1.length; lt++) {
+        if (str2[tmp] === str1[lt]) {
+          cnt++;
+          tmp++;
+        } else cnt = 0;
+        if (cnt === 2) return true;
+      }
+    }
+    return false;
+  };
+
   return answer;
 }
-
 module.exports = problem6;
