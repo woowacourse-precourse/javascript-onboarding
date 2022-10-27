e648e89d56b5cb91b30367d2a244c21c201ba7d8
hxezin <leehxejin@gmail.com>
2022-10-27 16:55:26 +0900

    feat: 문제1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -15,6 +15,29 @@ function problem1(pobi, crong) {
     return -1;
   }
 
+  const getMax = (pages) => {
+    const max = pages.map((page) => {
+      const temp = String(page).split("");
+      const plus = temp.reduce((acc, cur) => acc + parseInt(cur), 0);
+      const multi = temp.reduce((acc, cur) => acc * parseInt(cur), 1);
+
+      return Math.max(plus, multi);
+    });
+
+    return Math.max(...max);
+  };
+
+  const maxPobi = getMax(pobi);
+  const maxCrong = getMax(crong);
+
+  if (maxPobi > maxCrong) {
+    answer = 1;
+  } else if (maxPobi < maxCrong) {
+    answer = 2;
+  } else {
+    answer = 0;
+  }
+
   return answer;
 }
 
