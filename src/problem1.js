f85c9873df63675018432a9dc5758ca57d14299f
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-27 20:09:56 +0900

    feat: [P1] 주어진 배열이 유효한 케이스인지 확인하기
    - 두 페이지가 연속되지 않은 경우
    - 왼쪽 페이지가 홀수가 아닌 경우
    - 오른쪽 페이지가 짝수가 아닌 경우

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,11 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isValidCase(pobi, crong) === false) return -1;
+}
+
+function isValidCase (case1, case2) {
+  if (case1[1] - case1[0] !== 1 || case2[1] - case2[0] !== 1) return false
+  if (case1[0] % 2 !== 1 || case2[0] % 2 !== 1) return false
+  if (case1[1] % 2 !== 0 || case1[1] % 2 !== 0) return false
 }
 
 module.exports = problem1;
