04277af5c4dc63a99887d4e4a8d195ab038ee2f2
gunyongbok <qhrrjsdyd123@gmail.com>
2022-10-27 10:55:02 +0900

    2 clear

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+    const array = cryptogram.split('');
+    let stack = [];
+    for (let i = 0; i < array.length; i++) {
+        if (stack.length === 0) {
+            stack.push(array[0]);
+        }
+        if (array[i] == stack[stack.length - 1]) {
+            stack.pop();
+            continue;
+        }
+        stack.push(array[i]);
+    }
+    var answer = stack.toString().replace(/,/g, '');
+    return answer;
 }
 
 module.exports = problem2;
