352e89baae785610a905f7cd45fb706931527402
최희원 <jeeen1@naver.com>
2022-10-28 01:03:40 +0900

    feat(problem1): add score calculation

--- src/problem1.js
+++ src/problem1.js
@@ -5,7 +5,7 @@ const PAGES_LENGTH = 2;
 /**
  * Checks if the input is number array of length 2.
  * @param {number[]} pages input given by function isPagesValid
- * @return
+ * @returns {boolean} whether the input is number array of length 2
  */
 function isPagesArrayValid(pages) {
   if (!Array.isArray(pages)) return false;
@@ -15,7 +15,8 @@ function isPagesArrayValid(pages) {
 
 /**
  * Checks if page number is between PAGE_MIN and PAGE_MAX.
- * @param {number} page a page number
+ * @param {number} page page number
+ * @returns {boolean} whether the page number is between PAGE_MIN and PAGE_MAX
  */
 function isInRange(page) {
   if (page < PAGE_MIN) return false;
@@ -26,6 +27,7 @@ function isInRange(page) {
 /**
  * Checks if the input is in valid format.
  * @param {any} pages input given by function problem 1 (pobi or crong)
+ * @returns {boolean} whether the input is in valid format
  */
 function isPagesValid(pages) {
   if (!isPagesArrayValid(pages)) return false;
@@ -34,6 +36,34 @@ function isPagesValid(pages) {
   if (leftPage % 2 === 0 || rightPage % 2 === 1) return false;
   if (leftPage + 1 !== rightPage) return false;
   return true;
+}
+
+/**
+ * Converts a number to array of digits
+ * @param {number} page page number
+ * @returns {number[]} converted digits in number array format
+ */
+function numberToDigits(page) {
+  let stringArray = page.toString().split("");
+  return stringArray.map((s) => +s);
+}
+
+/**
+ * Caculates score from the given page numbers.
+ * @param {number[]} pages number array of length 2 with page numbers in it
+ * @returns {number} maximum score produced by the given page numbers
+ */
+function getScore(pages) {
+  const pagesToDigitLeft = numberToDigits(pages[0]);
+  const pagesToDigitRight = numberToDigits(pages[1]);
+
+  const sumLeft = pagesToDigitLeft.reduce((a, b) => a + b, 0);
+  const productLeft = pagesToDigitLeft.reduce((a, b) => a * b, 1);
+
+  const sumRight = pagesToDigitRight.reduce((a, b) => a + b, 0);
+  const productRight = pagesToDigitRight.reduce((a, b) => a * b, 1);
+
+  return Math.max(sumLeft, productLeft, sumRight, productRight);
 }
 
 function problem1(pobi, crong) {
