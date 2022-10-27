4083d99828a732ab92a37e57c1875970e01895cf
yeonsuBeak <soothbugger@gmail.com>
2022-10-28 07:48:30 +0900

    feat(problem4): 알파벳 사전과 뒤집힌 알파벳 사전을 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,25 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const arrayedAlphabetLists = arrayAlphabet();
+}
+
+function arrayAlphabet() {
+  const TOTAL_ALPHABET = 26;
+  let alphabetList = new Array(TOTAL_ALPHABET);
+  let reversedAlphabetList = new Array(TOTAL_ALPHABET);
+
+  for (let alphabetIndex = 0; alphabetIndex < TOTAL_ALPHABET; alphabetIndex++) {
+    const START_ALPHABET_A = 65;
+    const LAST_ALPHABET_Z = 90;
+
+    alphabetList[alphabetIndex] = String.fromCharCode(
+      START_ALPHABET_A + alphabetIndex
+    );
+    reversedAlphabetList[alphabetIndex] = String.fromCharCode(
+      LAST_ALPHABET_Z - alphabetIndex
+    );
+  }
+
+  return [alphabetList, reversedAlphabetList];
 }
 
 module.exports = problem4;
