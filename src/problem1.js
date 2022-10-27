a2eb54ab143f7ca347b0f1562d33bd9efe5f69d0
lyssoi <114637463+lyssoi@users.noreply.github.com>
2022-10-27 23:25:27 +0900

    기능1 각자리수 더하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,25 @@
+
+var pobi = [235,236];
+var crong = [325,326]
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
 }
+
+function numberSum(a) {
+  var answer = 0;
+  var sumNumber = String(a);
+
+  for(var i=0; i<sumNumber.length; i++){
+    answer +=parseInt(sumNumber[i]);
+  }
+  
+  return answer;
+
+}
+
+
+console.log (numberSum(pobi[0]));
 
 module.exports = problem1;
