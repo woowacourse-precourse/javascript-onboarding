4e4e8c2c60670d4223ca71f77c96cade22dace03
Adult96 <djxor007@gmail.com>
2022-10-27 12:12:40 +0900

    feat: problem1 - if문을 사용하여 예외 사항 return -1 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,11 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let answer = 0;
+  let pobiArr = [];
+  let crongArr = [];
+
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    return -1;
+  }
 }
 
 module.exports = problem1;
