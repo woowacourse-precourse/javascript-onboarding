c9507e45a8dc4cc37c60baaced2b1cb0908a9e04
Leeseunghwan7305 <sun1238799@naver.com>
2022-10-27 13:06:48 +0900

    우테코 4번째 문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,57 @@
-function problem4(word) {
-  var answer;
-  return answer;
+function problem4(words) {
+  let result = "";
+  let wordMap = new Map([
+    ["A", "Z"],
+    ["B", "Y"],
+    ["C", "X"],
+    ["D", "W"],
+    ["E", "V"],
+    ["F", "U"],
+    ["G", "T"],
+    ["H", "S"],
+    ["I", "R"],
+    ["J", "Q"],
+    ["K", "P"],
+    ["L", "O"],
+    ["M", "N"],
+    ["N", "M"],
+    ["O", "L"],
+    ["P", "K"],
+    ["Q", "J"],
+    ["R", "I"],
+    ["S", "H"],
+    ["T", "G"],
+    ["U", "F"],
+    ["V", "E"],
+    ["W", "D"],
+    ["X", "C"],
+    ["Y", "B"],
+    ["Z", "A"],
+  ]);
+
+  for (let word of words) {
+    if (word == " " || word == null) {
+      result += " ";
+      continue;
+    }
+
+    if (word.charCodeAt(0) < 65 || word.charCodeAt(0) > 122) {
+      result += word;
+      continue;
+    }
+
+    if (word.toUpperCase() == word) {
+      //대문자면
+      result += wordMap.get(word);
+    } else if (word.toLowerCase() == word) {
+      //소문자면
+      word = word.toUpperCase();
+      result += wordMap.get(word).toLowerCase();
+    }
+  }
+  console.log(result);
 }
+
+problem4("I love you");
 
 module.exports = problem4;
