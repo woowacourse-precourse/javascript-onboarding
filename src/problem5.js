dc4739812bb9315c95ffe5028b3a639a80ac7d2a
Hwang Seong Taek <hst007@naver.com>
2022-10-27 21:02:47 +0900

    feat: 조건문에 따른 변환 값 wallet 배열에 저장, 결과 값 return

--- src/problem5.js
+++ src/problem5.js
@@ -9,6 +9,17 @@
 function problem5(money) {
   const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const wallet = [];
+
+  cash.forEach(item => {
+    if (money / item >= 1) {
+      wallet.push(Math.floor(money / item));
+      money = money % item;
+    } else if (money / item < 1) {
+      wallet.push(0);
+    }
+  });
+
+  return wallet;
 }
 
 module.exports = problem5;
