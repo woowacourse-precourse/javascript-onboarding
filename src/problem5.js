7eed0156b9db9433044faeb5c5112a142ae27321
Suyeon <msjgr2801@gmail.com>
2022-10-27 14:21:16 +0900

    feat: Write Problem5,6,7's function lists

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,9 @@
+// 1. [ ] 나눠지는 돈의 기준대로 배열 만들기
+// 2. [ ] answer엔 money를 각 standard로 나눈 몫을 저장하기
+//  2.1 [ ]거스름돈을 계산하며 1원단위까지 반복
 function problem5(money) {
-  var answer;
+  let answer = [];
+
   return answer;
 }
 
--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,9 @@
+// 1. [ ]친구관계를 연결한 graph를 map으로 생성
+// 2. [ ]친구점수를 저장한 map을 생성
+// 3. [ ]user의 친구를 찾아서, 친구의 친구는 + 10, vistior들은 + 1
+// 4. [ ]순회 -> 0점이상인 친구들을 저장하며 점수가 가장 낮은 친구 기록(이후 대체)
+// 5. [ ]순회가 끝난후 이름대로 정렬
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
