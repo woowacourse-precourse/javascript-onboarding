4fbf423e3e54b428b53cbd78d1f925252a071279
gyeongza <gyeongza@naver.com>
2022-10-28 00:03:04 +0900

    중복문자 제거 함수 생성

--- src/problem2.js
+++ src/problem2.js
@@ -3,6 +3,19 @@ const cryptogramArr = (string) => {
     return string.split("")
 }
 
+// 중복문자 제거 
+let i = 0;
+const compare = (word) => {
+    while (i !=word.length){
+    if (word[i] == word[i+1]){
+        word.splice(i,2);
+        i=0;
+    } else {
+        i +=1;
+    }
+} return word.join("");}
+
+
 // function problem2(cryptogram) {
 //   var answer;
 //   return answer;
