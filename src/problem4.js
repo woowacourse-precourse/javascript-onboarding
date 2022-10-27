34f84155250a56fc92a3171aa58e88fe33ddcc58
inthhh <glass005533@gmail.com>
2022-10-27 16:53:07 +0900

    p4_alphabet list

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,13 @@
 function problem4(word) {
   var answer;
+  var answer = '';
+  const alphaCap = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+  const alphaLow = 'abcdefghijklmnopqrstuvwxyz';
+
+  for(let i=0; i<word.length; ++i){
+    
+  }
+
   return answer;
 }
 
