db217826cef35265d7dad4b46e2d1e86099bd46f
adultlee <seoungin1228@gmail.com>
2022-10-27 18:09:39 +0900

    feat: [pro6] 연속하는 2글자인 배열을 생성하는 함수 구현
    연속하는 두 글자를 판별하기 위해 특정 문자열의 연속한 부분을 생성하는 함수를 구현합니다

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
   var answer;
   return answer;
+function generateTwoLetterArray(string) {
+  let twoLetterArray = [];
+  for (let i = 0; i < string.length - 1; i++) {
+    twoLetterArray.push(string[i] + string[i + 1]);
+  }
+  return twoLetterArray;
 }
 
 module.exports = problem6;
