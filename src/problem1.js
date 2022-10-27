9196fa2cefd9445a9f10d2815ad0c968e6dbd6a1
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-27 21:43:09 +0900

    ✨ 페이지의 숫자를 받아서 합과 곱중 큰수 반환

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,17 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+// 1) 페이지 숫자를 주면 합/곱중 큰 수를 리턴하는 함수
+function calculatePage(pageNum) {
+  let temp = [];
+  while (pageNum > 0) {
+    temp.unshift(pageNum % 10);
+    pageNum = Math.floor(pageNum / 10);
+  }
+  const sum = temp.reduce((acc, cur) => acc + cur, 0);
+  const multiply = temp.reduce((acc, cur) => acc * cur, 1);
+
+  return sum > multiply ? sum : multiply;
+}
+
 module.exports = problem1;
