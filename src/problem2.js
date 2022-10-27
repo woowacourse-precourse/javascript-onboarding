23fd75c4530e474f49883e308226996c8a067b2f
yu-retel <dbdudals100@naver.com>
2022-10-27 12:09:50 +0900

    feat: [p2] 중복여부판별 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -4,3 +4,17 @@ function problem2(cryptogram) {
 }
 
 module.exports = problem2;
+
+function overlap(cryptogram){
+  for(let i=0; i<cryptogram.length; i++){
+    if(cryptogram[i]===cryptogram[i+1]){
+      return true;
+    }
+  }
+  return false;
+}
+
+
+let cryptogram="browoanoommnaon"
+
+console.log(overlap(cryptogram))
