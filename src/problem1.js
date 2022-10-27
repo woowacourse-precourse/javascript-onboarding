b6207e68732324e6604ca7f040471ee5f0088f45
heoputer <hjw1753@gmail.com>
2022-10-27 23:27:50 +0900

    feat : 최대 숫자 계산 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,40 @@
 function problem1(pobi, crong) {
   var answer;
+
+  let pobiNum = getMaxNum(pobi);
+  let crongNum = getMaxNum(crong);
+
+  if (pobiNum > crongNum) answer = 1;
+  else if (pobiNum < crongNum) answer = 2;
+  else answer = 0;
   return answer;
+}
+
+function getMaxNum(who) {
+  const arr = [];
+  who.forEach((page) => {
+    if (page >= 100) {
+      let plusNum = 0;
+      let multiNum = 1;
+      for (let i = 0; i < 3; i++) {
+        plusNum += Number(String(page)[i]);
+        multiNum *= Number(String(page)[i]);
+      }
+      if (plusNum > multiNum) arr.push(plusNum);
+      else arr.push(multiNum);
+    }
+    if (page < 100) {
+      let plusNum = 0;
+      let multiNum = 1;
+      for (let i = 0; i < 2; i++) {
+        plusNum += Number(String(page)[i]);
+        multiNum *= Number(String(page)[i]);
+      }
+      if (plusNum > multiNum) arr.push(plusNum);
+      else arr.push(multiNum);
+    }
+  });
+  return Math.max(...arr);
 }
 
 module.exports = problem1;
