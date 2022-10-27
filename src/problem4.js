e69925d417e2746ec9785b54a7dbb2f3133b02da
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 17:57:36 +0900

    refactor(problem4): 특정 문자의 유니코드 의미하는 변수 제거하여 코드 간결화

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,4 @@
 function problem4(word) {
-  var answer = "";
-
   const isUpperCase = (word) => {
     return word.charCodeAt() >= 65 && word.charCodeAt() <= 90;
   };
@@ -14,13 +12,11 @@ function problem4(word) {
 
     for (let i = 0; i < word.length; i++) {
       if (isUpperCase(word[i])) {
-        let unicode = 155 - word[i].charCodeAt();
-        changedWord += String.fromCharCode(unicode);
+        changedWord += String.fromCharCode(155 - word[i].charCodeAt());
         continue;
       }
       if (isLowerCase(word[i])) {
-        let unicode = 219 - word[i].charCodeAt();
-        changedWord += String.fromCharCode(unicode);
+        changedWord += String.fromCharCode(219 - word[i].charCodeAt());
         continue;
       }
       changedWord += word[i];
@@ -28,8 +24,7 @@ function problem4(word) {
     return changedWord;
   };
 
-  answer = changeWord(word);
-  return answer;
+  return changeWord(word);
 }
 
 module.exports = problem4;
