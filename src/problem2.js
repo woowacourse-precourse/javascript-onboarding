9d11a367af91e1d6ccc6259b8681a0b3881230e0
Adult96 <djxor007@gmail.com>
2022-10-27 12:18:33 +0900

    feat: problem2 - whlie문 사용하여 중복 문자 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
   let answer = cryptogram.split('');
   let cnt = 0;
+  while (cnt !== answer.length) {
+    if (answer[cnt] === answer[cnt + 1]) {
+      answer.splice(cnt, 2);
+      cnt = 0;
+    } else {
+      cnt++;
+    }
+  }
 
   return answer;
 }
