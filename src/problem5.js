eb8c8c8c7e8f9a8f01d94f9540bf10ffadc68d17
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-27 18:55:06 +0900

    [문제5] 지폐 혹은 동전 반환 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,21 @@
 const unitsArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+function getMoneyNumber(money) {
+  let resultArray = [];
+  let rest = money;
+
+  unitsArray.map((unit) => {
+    let share = parseInt(rest / unit);
+    let remainder = rest % unit;
+    if (share != 0) {
+      resultArray.push(share);
+    } else {
+      resultArray.push(0);
+    }
+    rest = remainder;
+  });
+
+  return resultArray;
+}
+
 module.exports = problem5;
