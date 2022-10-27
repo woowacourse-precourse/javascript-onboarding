6289f7c77f6b07c195ade72511c32f9928b97fa2
freemoon99 <soloist132@gmail.com>
2022-10-27 22:28:57 +0900

    feat[problem1]: 각 자리수 덧셈 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,13 @@
 function problem1(pobi, crong) {
   var answer;
+  
+  const sumfn = (num) => {
+    var sum = 0;
+    for (var j = 0; j < String(num).length; j++) {
+      return sum += parseInt(String(num)[j]);
+    }
+  } 
+  
   return answer;
 }
 
