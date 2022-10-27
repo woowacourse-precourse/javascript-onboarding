251055ff6ae2b75a80763679b241916a5ffd6f2b
kyhyun <k29176@gmail.com>
2022-10-27 14:29:53 +0900

    feat : 문제 2번 - 중복 되는 문자를 찾아 제거하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return removeDuplication(cryptogram);
+}
+
+function removeDuplication(cryptogram) {
+  const cryptogramArr = cryptogram.split('');
+  let deduplicatedTemporary = '';
+  let containDuplicates = false;
+
+  for (let i = 0; i < cryptogramArr.length; i++) {
+    for (let j = i + 1; j <= cryptogramArr.length; j++) {
+      if (cryptogramArr[i] !== cryptogramArr[j]) {
+        deduplicatedTemporary += cryptogramArr[i];
+        break;
+      } else if (cryptogramArr[i++] === cryptogramArr[j]) {
+        containDuplicates = true;
+        break;
+      }
+    }
+  }
+  return containDuplicates ? removeDuplication(deduplicatedTemporary) : deduplicatedTemporary;
 }
 
 module.exports = problem2;
