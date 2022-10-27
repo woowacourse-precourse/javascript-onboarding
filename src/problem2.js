3e2e51c6aaa827563c38bb42eedaae9440a19ccd
sangpyokim <rlatkdvy12@naver.com>
2022-10-27 19:14:28 +0900

    1,2 problem solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+  return removeDeplitcation(cryptogram)
+}
+
+function removeDeplitcation(str) {
+  let s = ''
+  for (let char of str) {
+    if (char !== s[s.length-1]) {
+      s += char
+    } else {
+      s = s.slice(0, s.length-1)
+    }
+  }
+
+  return s
 }
 
 module.exports = problem2;
