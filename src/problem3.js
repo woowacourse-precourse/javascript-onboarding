d53ca166d59e020154ac9b4a31dc9a0ffbe632de
Circlewee <pnj1118@naver.com>
2022-10-27 17:12:42 +0900

    [#3]feat: caculateClapCount 함수 완성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,31 @@
+/**
+ * @param {number} num
+ * @returns {number}
+ */
+function calculateClapCount(num) {
+  const numArr = String(num)
+    .split('')
+    .map((value) => Number(value));
+  let result = 0;
+
+  for (let i = 0; i < numArr.length; i++) {
+    const num = numArr[i];
+
+    if (num !== 0 && num % 3 === 0) {
+      result++;
+    }
+  }
+
+  return result;
+}
+
+/**
+ * @param {number} number
+ * @returns {number}
+ */
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
   return answer;
 }
 
