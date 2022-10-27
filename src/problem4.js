52808fac6142115a9232517132589ca00dcfbdf2
biyamn <b1y9mn@gmail.com>
2022-10-27 17:02:15 +0900

    feat: 청개구리 사전을 딕셔너리로 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  var answer;
+  let frogDictUpper = {};
+  let frogDictLower = {};
+  for (let i = 0; i < 26; i++) {
+    frogDictUpper[String.fromCharCode(i + 65)] = String.fromCharCode(90 - i)
+    frogDictLower[String.fromCharCode(i + 97)] = String.fromCharCode(122 - i)
+  }
+
+  let answer;
   return answer;
 }
 
 module.exports = problem4;
+
+console.log(problem4("I love you"));
