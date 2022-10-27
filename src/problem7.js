2e3e2bae84a363491b2af5ed921dfd24e246a009
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-27 23:09:53 +0900

    feat: 7번 기능 1- 친구 수에 따른 점수 부여
    - friends 배열에서 나와 친구 관계가 아닐 경우 10점 부여
    - 사용자와 같은 요소 안 배열에 포함 되어있다면 0점 처리

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,38 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer = [];
+  let sortByScoreArr = [];
+  let recordScoreMap = new Map();
+
+  for (let friend of friends) {
+    if (!friend.includes(user)) {
+      for (let a of friend) {
+        recordScoreMap.has(a)
+          ? recordScoreMap.set(a, recordScoreMap.get(a) + 10)
+          : recordScoreMap.set(a, 10);
+      }
+    } else {
+      for (let a of friend) {
+        recordScoreMap.set(a, 0);
+      }
+    }
+  }
+
   return answer;
 }
+
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "jun"],
+      ["donut", "andole"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 
 module.exports = problem7;
