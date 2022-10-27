dff7292739bb17c0503539859c5f2817c98cc93a
hwangsungjae <edx2fire@likelion.org>
2022-10-27 12:18:28 +0900

    [온보딩- 01] 책 페이지 문제 솔루션 함수 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,22 @@
 function problem1(pobi, crong) {
-  var answer;
+  if (pobi[0] - pobi[1] !== -1 || crong[0] - crong[1] !== -1) return -1;
+  
+  let answer;
+  let max = 0;
+  let pobiMax = 0;
+  let crongMax = 0;
+
+  const getSum = num => Math.max(max, [...num + ''].map(el => +el).reduce((prev, curr) => prev + curr, 0));
+  const getMultiply = num => Math.max(max, [...num + ''].map(el => +el).reduce((prev, curr) => prev * curr, 1));
+
+  pobi.forEach(num => pobiMax = Math.max(getSum(num), getMultiply(num)));
+  crong.forEach(num => crongMax = Math.max(getSum(num), getMultiply(num)));
+
+  console.log(pobiMax, crongMax);
+
+  if (pobiMax === crongMax) answer = 0;
+  else answer = pobiMax > crongMax ? 1 : 2;
+
   return answer;
 }
 
