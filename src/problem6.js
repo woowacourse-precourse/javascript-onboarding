e5a5179418b6a184476ab0e1dd037b35e3be5e2c
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-28 07:02:34 +0900

    feat: [problem6] Add problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,20 @@
-function problem6(forms) {}
+function problem6(forms) {
+  const nameList = setName(forms);
+  const emailList = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      const equalNameList = nameList.filter(
+        (item) => item == forms[i][1].substr(j, 2)
+      );
+
+      if (equalNameList.length > 1) {
+        emailList.push(forms[i][0]);
+      }
+    }
+  }
+  return emailList.sort();
+}
 
 function setName(forms) {
   const array = [];
