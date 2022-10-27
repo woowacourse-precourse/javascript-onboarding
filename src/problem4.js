034e69b70155dde3feeacb4d3c8a2662f3fe0b2c
wonkyung <mwk0725@gmail.com>
2022-10-27 22:04:11 +0900

    feat: [기능 0] 반대 알파벳 인덱스 번호 찾기

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabet = Array.from({length: 26}, (v, i) => String.fromCharCode(i + 65));
+  let text = word.split("");
+
+  // 반대 알파벳 인덱스 번호 찾기
+  const findIdx = text => {
+    return 25 - alphabet.indexOf(text);
+  };
 }
 
 module.exports = problem4;
