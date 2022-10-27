b7ab8f2453d7d49e9de05f94b00e25229448008b
Rki0 <pky00823@gmail.com>
2022-10-28 00:09:32 +0900

    fix: prob7. 제한 사항 만족 조건문 연산자 수정
    모든 조건이 true인 경우 정상 작동을 하는 것이므로,
    && 연산자로 조건들을 묶어주는 것으로 수정했다.

--- src/problem7.js
+++ src/problem7.js
@@ -140,10 +140,10 @@ function getFriendsRecommendArray(alreadyFriends, sortedScore, ans) {
 
 function problem7(user, friends, visitors) {
   if (
-    checkUserLength(user) ||
-    checkFriendsLength(friends) ||
-    checkIdLength(friends) ||
-    checkVisitorsLength(visitors) ||
+    checkUserLength(user) &&
+    checkFriendsLength(friends) &&
+    checkIdLength(friends) &&
+    checkVisitorsLength(visitors) &&
     checkIdOnlyLowerCase(friends)
   ) {
     return;
