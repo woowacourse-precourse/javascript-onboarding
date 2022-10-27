55d5e8720649d56f5fb20e42a4e5dbcadfbf0a9f
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 20:14:21 +0900

    problem6/feat : 마지막으로 크루 수 제한

--- src/problem6.js
+++ src/problem6.js
@@ -25,23 +25,25 @@ function problem6(forms) {
     }
   }
 
-  for (let i = 0; i < forms.length; i++) {
-    for (let z = 0; z < forms[i][1].length - 1; z++) {
-      for (let h = 1 + i; h < forms.length; h++) {
-        if (forms[h][1].includes(forms[i][1][z] + forms[i][1][z + 1])) {
-          arr.push(forms[h]);
-          arr.push(forms[i]);
+  if (forms.length >= 1 && forms.length <= 10000) {
+    for (let i = 0; i < forms.length; i++) {
+      for (let z = 0; z < forms[i][1].length - 1; z++) {
+        for (let h = 1 + i; h < forms.length; h++) {
+          if (forms[h][1].includes(forms[i][1][z] + forms[i][1][z + 1])) {
+            arr.push(forms[h]);
+            arr.push(forms[i]);
+          }
         }
+      }
+    }
+    arr.sort();
+    for (let i = 0; i < arr.length; i++) {
+      if (answer.includes(arr[i][0]) == false) {
+        answer.push(arr[i][0]);
       }
     }
   }
 
-  arr.sort();
-  for (let i = 0; i < arr.length; i++) {
-    if (answer.includes(arr[i][0]) == false) {
-      answer.push(arr[i][0]);
-    }
-  }
   return answer;
 }
 
