f6cc1113fa7e287599b027bd925118048f7ab81d
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-27 22:40:00 +0900

    feat(문제3): 삼육구 손뼉 횟수 구하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (let i = 1; i <= number; i += 1) {
+    for (let n of i.toString())
+      if ([3, 6, 9].includes(Number(n))) count += 1;
+  }
+return count;
 }
 
 module.exports = problem3;
