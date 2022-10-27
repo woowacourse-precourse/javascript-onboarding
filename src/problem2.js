1f11dee0e9f7788f740d4531244b63a30aa977c6
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-10-27 14:48:57 +0900

    problem2 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,25 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = [];
+
+  let cur = 0;
+  while (cur < cryptogram.length) {
+    if (answer.length == 0) {
+      answer.push(cryptogram[cur]);
+      cur++;
+    } else if (answer[answer.length - 1] != cryptogram[cur]) {
+      answer.push(cryptogram[cur]);
+      cur++;
+    } else {
+      while (
+        answer[answer.length - 1] == cryptogram[cur] &&
+        cur < cryptogram.length
+      ) {
+        cur++;
+      }
+      answer.pop();
+    }
+  }
+  answer = answer.join('');
   return answer;
 }
 
