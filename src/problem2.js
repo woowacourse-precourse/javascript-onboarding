1d014d351762b0d698fe3fa0d2975e047218840d
foundaway98 <foundaway98@naver.com>
2022-10-27 17:58:50 +0900

    problem2 기능 요구사항 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
   var answer;
+  let stack = [];
+  for (let i = 0; i < cryptogram.length; i++) {
+    stack.push(cryptogram.slice(i, i + 1));
+    if (stack[stack.length - 1] == stack[stack.length - 2]) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  answer = stack.join("");
   return answer;
 }
-
 module.exports = problem2;
