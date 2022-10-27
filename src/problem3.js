ab24b5d3c193b1c2ba4ce8cf396fbb47c217a954
foundaway98 <foundaway98@naver.com>
2022-10-27 19:23:06 +0900

    박수 횟수 계산 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,14 @@
+function clap(number) {
+  let clapCnt = 0;
+  number = number.toString();
+  for (let i = 0; i < number.length; i++) {
+    if (number[i] == "3" || number[i] == "6" || number[i] == "9") {
+      clapCnt += 1;
+    }
+  }
+  return clapCnt;
+}
+
 function problem3(number) {
   var answer;
   return answer;
