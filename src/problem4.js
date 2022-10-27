45b75c4a51677820261b3f2fd80a8bf71db6a8f6
h2002y <h2002y@naver.com>
2022-10-27 17:52:21 +0900

    feat: Add decipher func in problem4.js

--- src/problem4.js
+++ src/problem4.js
@@ -2,18 +2,24 @@ function problem4(word) {
   var answer = "";
   let upper = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
   let lower = upper.toLowerCase();
+  answer = decipher(word, upper, lower);
+  return answer;
+}
+
+function decipher(word, upper, lower){
+  let decipherStr = "";
   for(let i = 0; i < word.length; i++){
     if((word[i] >= 'a') && (word[i] <= 'z')){
-      answer += lower[word[i].charCodeAt(0) - 'a'.charCodeAt(0)];
+      decipherStr += lower[word[i].charCodeAt(0) - 'a'.charCodeAt(0)];
     }
     else if((word[i] >= 'A') && (word[i] <= 'Z')){
-      answer += upper[word[i].charCodeAt(0) - 'A'.charCodeAt(0)];
+      decipherStr += upper[word[i].charCodeAt(0) - 'A'.charCodeAt(0)];
     }
     else{
-      answer += word[i];
+      decipherStr += word[i];
     }
   }
-  return answer;
+  return decipherStr;
 }
 
 module.exports = problem4;
