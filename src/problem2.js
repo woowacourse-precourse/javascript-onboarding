3e3cec116fe6ffc8aa25dd15844ba8fe2d18a79c
MinGyu-Kang <kagrin97@gmail.com>
2022-10-27 11:32:17 +0900

    problem2 : 기능목록 1번 문자열을 입력받아서 하나씩 나눈다음 배열에 넣는다. 코드 작성완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let strArr = cryptogram.split("");
+
+  return strArr;
 }
 
 module.exports = problem2;
+
+console.log(problem2("browoanoommnaon"));
