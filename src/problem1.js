7577810618a23f16d52c00d2727f0aa850fa2c84
wang-yurin <king_1994@naver.com>
2022-10-27 09:36:50 +0900

    Feat: 가장 큰 수 구하는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -4,6 +4,25 @@ function isValidPage(pages) {
   return true;
 }
 
+function maxNum(pages) {
+  const [leftPage, rightPage] = pages;
+  const leftPageNums = String(leftPage)
+    .split("")
+    .map((value) => parseInt(value));
+  const rightPageNums = String(rightPage)
+    .split("")
+    .map((value) => parseInt(value));
+
+  const maxNum = Math.max(
+    leftPageNums.reduce((acc, cur) => acc + cur, 0),
+    rightPageNums.reduce((acc, cur) => acc + cur, 0),
+    leftPageNums.reduce((acc, cur) => acc * cur, 1),
+    rightPageNums.reduce((acc, cur) => acc * cur, 1)
+  );
+
+  return maxNum;
+}
+
 function problem1(pobi, crong) {}
 
 module.exports = problem1;
