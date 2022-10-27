5a438853dfc3c7632755084a3a29fa9150000bb4
yeong bin <2976622@naver.com>
2022-10-27 22:12:58 +0900

    chore: [problem5] 불필요한 변수 제거

--- src/problem5.js
+++ src/problem5.js
@@ -6,11 +6,10 @@
  * @returns {Array<number>}
  */
 function problem5(money) {
-  let leftMoney = money;
   return [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
       .map((m) => {
-        const count = Math.floor(leftMoney / m);
-        leftMoney -= count * m;
+        const count = Math.floor(money / m);
+        money -= count * m;
         return count;
       })
 } //leftMoney 부수효과 없애야함
