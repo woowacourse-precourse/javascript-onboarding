8a487cdaf8179b6f949e277b4bd80c166d0d77be
Chris.c <cjs120dd@gmail.com>
2022-10-27 12:17:20 +0900

    fear:5번 문제를 해결한다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
+const currencyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+    const answerList = [];
+    let currentMoney = Number(money);
+    currencyList.forEach((currency)=>{
+        const number = Math.floor(currentMoney/currency);
+        answerList.push(number);
+        currentMoney -= number*currency;
+    })
+    
+    return answerList;
 }
 
 module.exports = problem5;
