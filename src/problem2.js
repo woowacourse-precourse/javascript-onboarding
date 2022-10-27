8b8946e93854611efdddb7dd4c42922586f9ac08
kitez <duswl339@ajou.ac.kr>
2022-10-27 17:31:06 +0900

    [ success ] problem2.js

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,25 @@
+function delOverlap(cryptogram) {
+  let sIdx = -1;
+  for (let i = 1; i <= cryptogram.length; i++) {
+    if (cryptogram[i - 1] === cryptogram[i] && sIdx < 0) {
+      sIdx = i - 1;
+    } else if (sIdx >= 0 && cryptogram[i - 1] !== cryptogram[i]) {
+      let tmp = cryptogram.slice(sIdx, i);
+      cryptogram = cryptogram.replace(tmp, '');
+      return delOverlap(cryptogram);
+    }
+  }
+
+  return cryptogram;
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  /**
+   * 연속하는 중복 문자들을 삭제한 결과를 return 하도록한다.
+   * */
+
+  return delOverlap(cryptogram);
 }
 
 module.exports = problem2;
+
