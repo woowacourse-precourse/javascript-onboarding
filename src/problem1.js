7ee7654af3f225468b9aacbdd487537fd93ab7cf
foundaway98 <foundaway98@naver.com>
2022-10-27 17:13:45 +0900

    코드 구조 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,33 +1,33 @@
+function makeSum(n) {
+  let sum = 0;
+  do {
+    sum += n % 10;
+    n = Math.floor(n / 10);
+  } while (n > 0);
+  return sum;
+}
+
+function makeMul(n) {
+  let mul = 1;
+  do {
+    mul *= n % 10;
+    n = Math.floor(n / 10);
+  } while (n > 0);
+  return mul;
+}
+
+function choosePoint(arr) {
+  for (let i = 0; i < 2; i++) {
+    arr[i] = comparePoint(makeSum(arr[i]), makeMul(arr[i]));
+  }
+  return comparePoint(arr[0], arr[1]);
+}
+
+function comparePoint(n1, n2) {
+  return n1 > n2 ? n1 : n2;
+}
+
 function problem1(pobi, crong) {
-  function makeSum(n) {
-    let sum = 0;
-    do {
-      sum += n % 10;
-      n = Math.floor(n / 10);
-    } while (n > 0);
-    return sum;
-  }
-
-  function makeMul(n) {
-    let mul = 1;
-    do {
-      mul *= n % 10;
-      n = Math.floor(n / 10);
-    } while (n > 0);
-    return mul;
-  }
-
-  function choosePoint(arr) {
-    for (let i = 0; i < 2; i++) {
-      arr[i] = comparePoint(makeSum(arr[i]), makeMul(arr[i]));
-    }
-    return arr[0] > arr[1] ? arr[0] : arr[1];
-  }
-
-  function comparePoint(n1, n2) {
-    return n1 > n2 ? n1 : n2;
-  }
-
   var answer;
   if (pobi[1] - pobi[0] == 1 && crong[1] - crong[0] == 1) {
     let pobiPoint = choosePoint(pobi);
