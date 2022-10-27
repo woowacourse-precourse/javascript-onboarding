14fb49232ba786ed2621392a5e187feea75355d9
bassyu <yrkdgus@gmail.com>
2022-10-27 14:49:58 +0900

    문제7 해결

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,50 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let relation = {};
+  let score = {};
+  for (let i = 0; i < friends.length; i++) {
+    const [a, b] = friends[i];
+    if (!relation[a]) {
+      relation[a] = [];
+      score[a] = 0;
+    }
+    if (!relation[b]) {
+      relation[b] = [];
+      score[b] = 0;
+    }
+    relation[a].push(b);
+    relation[b].push(a);
+  }
+
+  const userFriends = [...relation[user]];
+  for (let i = 0; i < userFriends.length; i++) {
+    const userFriend = userFriends[i];
+    const friendFriends = [...relation[userFriend]];
+    for (let j = 0; j < friendFriends.length; j++) {
+      const friendFriend = friendFriends[j];
+      if (friendFriend === user) {
+        continue;
+      }
+      score[friendFriend] += 10;
+    }
+  }
+
+  for (let i = 0; i < visitors.length; i++) {
+    const visitor = visitors[i];
+    if (!score[visitor]){
+      score[visitor] = 0;
+    }
+    score[visitor] += 1;
+  }
+
+  let result = [];
+  for (const id in score) {
+    if (userFriends.includes(id) || id === user) {
+      continue;
+    }
+    result.push([score[id], id]);
+  }
+
+  return result.sort().map(([,id]) => id);
 }
 
 module.exports = problem7;
