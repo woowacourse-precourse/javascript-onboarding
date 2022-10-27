7b4e6d4db70cd1d32807ca74864f134de539683e
jw <always1997@kakao.com>
2022-10-27 14:50:21 +0900

    problem1 기능 1 findMax 함수 구현 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,28 @@
+/*
+  기능 목록
+  1. 각 자릿수의 합과 곱중 최댓값을 반환하는 findMax 함수 구현
+    1-1. 예외값 우선처리 (첫페이지와 끝페이지는 불가능, 왼쪽값은 홀수만, 오른쪽값음 짝수만 가능)
+  2. 포비가 더 큰 수를 가지면 1 크롱이 더 큰수를 가지면 2 동일하면 0 예외시 -1 리턴
+*/
+function findMax(nums) {
+  if ( nums[0] === 1 || nums[0] % 2 === 0 || nums[1] === 400 || nums[1] % 2 !== 0 || nums[1] - nums[0] !== 1 ) return -1;
+  let maxAdd = 0, maxMulti = 0;
+  for (let i = 0; i < nums.length; i++) {
+    let add = 0, multi = 1;
+    do {
+      add += nums[i] % 10;
+      multi *= nums[i] % 10;
+      nums[i] = Math.floor(nums[i] / 10);
+    } while (nums[i] > 0);
+    if (maxAdd < add) maxAdd = add;
+    if (maxMulti < multi) maxMulti = multi;
+  }
+  return maxAdd > maxMulti ? maxAdd : maxMulti;
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
   return answer;
 }
 
