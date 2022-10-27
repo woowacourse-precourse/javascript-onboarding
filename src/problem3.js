88d92b78d5b5ebcb6ced8e9272512b1095f9c600
powercording <adoim@naver.com>
2022-10-27 17:03:20 +0900

    feat : 인자값이 숫자이며 제한사항 범위 내 인지 확인하는 함수 생성

--- src/problem3.js
+++ src/problem3.js
@@ -4,3 +4,12 @@ function problem3(number) {
 }
 
 module.exports = problem3;
+
+const NUMBER_RANGE_START = 1;
+const NUMBER_RANGE_END = 10000;
+
+function isNumber(number) {
+  if (number < NUMBER_RANGE_START || number > NUMBER_RANGE_END) return false;
+  if (typeof number === "number") return true;
+  else return false;
+}
