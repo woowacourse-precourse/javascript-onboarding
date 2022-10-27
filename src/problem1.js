2d90566753d1188ab2eaf4cb0f034ad20d6dda42
jungsmins1 <jungsmins00@gmail.com>
2022-10-27 19:50:11 +0900

    feat: 페이지 번호의 각 자리 숫자를 모두 더하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -10,6 +10,13 @@ function problem1(pobi, crong) {
     if (page.length !== 2) return false;
     return true;
   }
+
+  //페이지 번호의 각 자리 숫자를 모두 더하는 기능
+  const addNum = (num) => {
+     let numStr = num.toString().split("");
+     let add = numStr.reduce((prev, curr) => prev += Number(curr), 0);
+     return add
+  }
 }
 
 problem1([97, 98], [197, 198])
