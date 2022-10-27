25c03c09153fbfd82a201835c9806f1b69d2b71a
kknyapple <kknyapple@gmail.com>
2022-10-28 03:30:31 +0900

    problem2 - 연속하는 중복 문자들 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,23 @@
+const removeDuplicate = (cryptogram) => {
+  let strArr = [...cryptogram];
+  let blank = [];
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (strArr[i] != strArr[i + 1]) {
+      if (strArr[i] != strArr[i - 1])
+        if (strArr[i] == blank[blank.length - 1]) blank = blank.slice(0, -1);
+        else {
+          blank.push(strArr[i]);
+        }
+    } else i = i + 1;
+  }
+  return blank.join("");
+};
+
 function problem2(cryptogram) {
   var answer;
+
+  answer = removeDuplicate(cryptogram);
+
   return answer;
 }
 
