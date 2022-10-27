1710871cde506d90e3125aa74b59768a4f50b10d
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 11:03:51 +0900

    feat: [문제1] pobi, crong의 최대점수 비교 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -10,17 +10,27 @@ function problem1(pobi, crong) {
   const getScore = (page, calculateCallback) => {
     return getDigitNumberList(page).reduce(calculateCallback);
   };
-
   const getDigitNumberList = (page) => [...String(page)];
   const multiplyCallback = (acc, cur) => Number(cur) * Number(acc);
   const addCallback = (acc, cur) => Number(cur) + Number(acc);
   const getMaxScore = (scores) => {
     return scores.reduce((acc, cur) => Math.max(acc, cur));
   };
+  const compareScores = (oneScore, anotherScore) => {
+    if (oneScore > anotherScore) {
+      return 1;
+    }
+    if (oneScore < anotherScore) {
+      return -1;
+    }
+    return 0;
+  };
 
-  getMaxScore(getScoreList(pobi));
-  getMaxScore(getScoreList(crong));
+  const pobiMaxScore = getMaxScore(getScoreList(pobi));
+  const crongMaxScore = getMaxScore(getScoreList(crong));
+
+  return compareScores(pobiMaxScore, crongMaxScore);
 }
 
-problem1([131, 132], [211, 212]);
+problem1([99, 102], [211, 212]);
 // module.exports = problem1;
