2cfdb90de19d97a61bbe2e5c59d072682609f02d
ryuna <anottrx@gmail.com>
2022-10-28 00:09:59 +0900

    feat : 각 자리 더하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,11 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+const addPage = (page) => {
+  return page
+    .toString()
+    .split("")
+    .reduce((prev, cur) => prev + +cur, 0);
+};
+
 module.exports = problem1;
