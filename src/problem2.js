7694e5cedc2bac7c8cdc156c12e4f7a494d0d444
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-10-27 16:43:38 +0900

    feat: 중복되는 문자를 삭제하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -8,12 +8,72 @@ const RESULT = {
   exception: -1,
 };
 
+const DUPLICATION = true;
+
 function getStringFromArray(array) {
   return array.join('');
 }
 
 function getArrayFromString(string) {
   return Array.from(string);
+}
+
+function isDupWithNextElement(curElement, nextElement) {
+  return curElement === nextElement ? true : false;
+}
+
+function decode(cryptogramArr) {
+  let isDuplicate = false;
+
+  const res = cryptogramArr
+    .map((v, i) => {
+      if (isDupWithNextElement(v, cryptogramArr[i + 1])) {
+        isDuplicate = true;
+        return DUPLICATION;
+      }
+
+      if (isDuplicate && !isDupWithNextElement(v, cryptogramArr[i + 1])) {
+        isDuplicate = false;
+        return DUPLICATION;
+      }
+
+      return v;
+    })
+    .filter(v => v !== DUPLICATION);
+
+  return res;
+}
+
+function decodeInit(cryptogramArr) {
+  return {
+    res: decode(cryptogramArr),
+    prevLength: decode(cryptogramArr).length,
+  };
+}
+
+function isCycleDone(decodeRes) {
+  return decodeRes.res.length === decodeRes.prevLength ? true : false;
+}
+
+function decodeCycle(cryptogramArr) {
+  const decodeRes = decodeInit(cryptogramArr);
+
+  while (true) {
+    decodeRes.res = decode(decodeRes.res);
+    if (isCycleDone(decodeRes)) {
+      break;
+    }
+    decodeRes.prevLength = decodeRes.res.length;
+  }
+
+  return decodeRes.res;
+}
+
+function decodeCryptogram(cryptogram) {
+  const cryptogramArr = getArrayFromString(cryptogram);
+  const res = decodeCycle(cryptogramArr);
+
+  return getStringFromArray(res);
 }
 
 function isValidInput(input) {
@@ -36,6 +96,8 @@ function problem2(cryptogram) {
   if (!isValidInput(cryptogram)) {
     return RESULT.exception;
   }
+
+  return decodeCryptogram(cryptogram);
 }
 
-module.exports = problem2('browoanoommnaon');
+module.exports = problem2;
