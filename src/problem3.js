e8ac5cb3ad4b04e1afd19cf14ddf6bd8ea0d11fe
Adult96 <djxor007@gmail.com>
2022-10-27 12:29:44 +0900

    feat: problem3 - filter메서드 사용하여 3,6,9의 문자만 select후 배열의 길이를 return

--- src/problem3.js
+++ src/problem3.js
@@ -3,7 +3,8 @@ function problem3(number) {
     .fill(number)
     .map((_, i) => i + 1 + '')
     .join('')
-    .split('');
+    .split('')
+    .filter((v) => v === '3' || v === '6' || v === '9').length;
 }
 
 module.exports = problem3;
