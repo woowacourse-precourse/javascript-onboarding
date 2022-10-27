00ec7cc698bc25f59eadcab46d46b93de5854615
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-28 01:33:15 +0900

    feat: [problem5] Add problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const wallet = [];
+  const coinList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < coinList.length; i++) {
+    wallet.push(Math.trunc(money / coinList[i]));
+    money = money - coinList[i] * wallet[i];
+  }
+  return wallet;
 }
 
 module.exports = problem5;
