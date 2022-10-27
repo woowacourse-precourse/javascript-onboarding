cbad0ecdc8bf120a4c66d75d33e99813ce68ddd6
wonkyum-kim <dodo12120@naver.com>
2022-10-27 20:33:26 +0900

    feat: add get_digits

--- src/problem1.js
+++ src/problem1.js
@@ -11,19 +11,38 @@ function exception_handler(pages) {
   if (exception1 || exception2 || exception3) {
     return true;
   }
-
   return false;
 }
 
-function problem1(pobi, crong) {
-  // The page number is [1:400]
-  // left page(index 0) is an odd number.
-  // right page(index 1) is an even number
+function get_digits(num) {
+  let digits = []; // 1, 10, 100
+  for (let i = 0; i < 3; ++i) {
+    const temp = num % 10;
+    digits.push(temp);
+    num = (num - temp) / 10;
+    if (num === 0) {
+      break;
+    }
+  }
+  return digits;
+}
 
+function problem1(pobi, crong) {
   // execption
   if (exception_handler(pobi) || exception_handler(crong)) {
     return -1;
   }
+
+  // for pobi
+  let pobi_page_digits = [];
+  pobi_page_digits.push(get_digits(pobi[0])); // left page digits
+  pobi_page_digits.push(get_digits(pobi[1])); // right page digits
+
+  // for crong
+  let crong_page_digits = [];
+  crong_page_digits.push(get_digits(crong[0])); // left page digits
+  crong_page_digits.push(get_digits(crong[1])); // right page digits
+
   var answer;
   return answer;
 }
