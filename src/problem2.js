a0db2170854ffc5ab7784822d04d549a5160abbd
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-27 23:52:54 +0900

    feat: problem2 구상

--- src/problem2.js
+++ src/problem2.js
@@ -4,3 +4,24 @@ function problem2(cryptogram) {
 }
 
 module.exports = problem2;
+
+/*
+연속된 문자 삭제 후, return
+reduce 사용한 뒤, 앞에서부터 동일한 data 삭제하면 될 듯.
+
+
+//"ababbbuqqk"
+
+구현할 것.
+### reduce 배운거 써먹어보기 
+Array.prototype.reduce()
+(prev, cur, curIndex)
+
+reduce 새로운 배열 생성해서 
+중복된 데이터 전부 삭제.(홀수 경우 생각)
+
+
+- 같을 경우 
+해당 index만 체크해서 
+
+*/
