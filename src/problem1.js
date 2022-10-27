7e79b86454288f424aa4f9266b68db30d0e26e00
TaeeunKim <xodms0309@naver.com>
2022-10-27 18:27:08 +0900

    [1번] feat: 각 자리 수의 덧셈과 곱셈 중 최대값을 구하는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -5,10 +5,19 @@ function problem1(pobi, crong) {
     return book.map((pages) => pages.toString().split('').map(Number));
   };
 
+  const getMaxPage = (pages) => {
+    const sum = pages.reduce((acc, cur) => acc + cur);
+    const multiple = pages.reduce((acc, cur) => acc * cur);
+
+    return Math.max(sum, multiple);
+  };
+
   const pobiBook = splitPages(pobi);
   const crongBook = splitPages(crong);
 
   return answer;
 }
+
+problem1([97, 98], [197, 198]);
 
 module.exports = problem1;
