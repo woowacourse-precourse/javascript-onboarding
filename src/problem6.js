c70a6ec2b673b4965089af642dcd3625b42eb4f9
KES <aprilsmaple@gmail.com>
2022-10-27 16:02:27 +0900

    feat: problem6 repeated nickname check

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,29 @@
 function problem6(forms) {
-  var answer;
+  let answer = new Set();
+  const splittedNickname = {};
+
+  for (let i = 0; i < forms.length; i++) {
+    let [email, nickname] = forms[i];
+    for (let j = 0; j < nickname.length - 1; j++) {
+      let idx = splittedNickname[nickname.slice(j, j + 2)];
+      if (idx) {
+        answer.add(email);
+        answer.add(forms[idx - 1][0]);
+      } else {
+        splittedNickname[nickname.slice(j, j + 2)] = i + 1;
+      }
+    }
+  }
   return answer;
 }
 
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
 module.exports = problem6;
