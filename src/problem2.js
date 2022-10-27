8f8ee0ffe143c3070e6249d1bfea5979bac4d0a7
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-27 14:09:58 +0900

    problem2 status 적용, 바뀌는 문자 반복 검사

--- src/problem2.js
+++ src/problem2.js
@@ -2,19 +2,22 @@ function problem2(cryptogram) {
   var answer;
   let status = false;
   arrayCryptogram = [...cryptogram]
-  for (let i = 0; i<=arrayCryptogram.length-1; i++){
-    if(arrayCryptogram[i] == arrayCryptogram[i+1]){
-      let count=1;
-      let j=i;
-      while(arrayCryptogram[j]==arrayCryptogram[j+1]){
-          count++;
-          j++;
+  while(status == false){
+    status = true
+    for (let i = 0; i<=arrayCryptogram.length-1; i++){
+      if(arrayCryptogram[i] == arrayCryptogram[i+1]){
+        let count=1;
+        let j=i;
+        while(arrayCryptogram[j]==arrayCryptogram[j+1]){
+            count++;
+            j++;
+        }
+        arrayCryptogram.splice(i,count);
+        i=j-2;
+        status = false
       }
-      arrayCryptogram.splice(i,count);
-      i=j-2;
-      status = false
     }
-  }
+}
   answer = arrayCryptogram.join("")
   return answer;
 }
