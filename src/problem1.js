ed7666b115b52897ab328b6d923ef30bb23ab516
haesa <xdhaesa@gmail.com>
2022-10-27 13:32:58 +0900

    problem1 예외 검사 기능 수정
    주어진 입력의 길이가 2가 아닐 때 예외 처리 추가

--- src/problem1.js
+++ src/problem1.js
@@ -20,9 +20,10 @@ function getScore(pages) {
 
 function isException(pages) {
   const [leftPage, rightPage] = pages;
+  if (pages.length !== 2) return 1;
   if (leftPage === 1 || rightPage === 400) return 1;
-  if (rightPage - leftPage !== 1) return 1;
   if (leftPage % 2 === 0) return 1;
+  if (rightPage - leftPage !== 1) return 1;
   return 0;
 }
 
