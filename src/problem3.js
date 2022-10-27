63a1951b541c027edc1a31ab2935e7bf298e0cdd
연 <mogoo22@naver.com>
2022-10-27 23:39:33 +0900

    feat(문제3): 입력된 숫자 범위까지 지정된 숫자 3, 6, 9가 포함된 횟수를 반환한다.
    - 숫자를 문자열 배열로 변환한 뒤 '3', '6', '9'와 일치하는 원소만을 필터하고 총개수를 구한다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
+const is369 = (n) => n === "3" || n === "6" || n === "9";
+
+const game369 = (number) => {
+  return String(number)
+    .split("")
+    .filter((n) => is369(n)).length;
+};
+
 function problem3(number) {
-  var answer;
-  return answer;
+
+  let clap = 0;
+
+  for (let i = 1; i <= number; i++) {
+    clap += game369(i);
+  }
+
+  return clap;
 }
 
 module.exports = problem3;
