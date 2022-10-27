ea90adc139c325f365b7408f94f21d3e37b93977
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-27 18:31:44 +0900

    feat(Problem3): 정규표현식 사용 결과 도출

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
 function problem3(number) {
-  var answer;
+  const numberList = Array(number).fill().map((v,i)=>i+1); 
+  numberList.map((num, idx)=> {
+    numberList[idx] = String(num).replace(/[3,6,9]/g,"*").replace(/[0-9]/g,"");
+  })
+  const answer = (numberList.join('')).length;
   return answer;
 }
 
