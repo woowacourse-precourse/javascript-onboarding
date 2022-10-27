a48323f299701fd3b2822f42952eb740cbef1597
Chris.c <cjs120dd@gmail.com>
2022-10-27 11:56:13 +0900

    fear:2번 문제를 해결한다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let newCryptogram =[...cryptogram];
+  function removeDuplicateString() {
+    newCryptogram.forEach((string,idx)=>{
+      if (string===newCryptogram[idx+1]){
+        newCryptogram.splice(idx,2);
+      }
+    });
+  }
+  
+  for (const element of [...cryptogram]) {
+    removeDuplicateString();
+  }
+  
+  return newCryptogram.join("");
 }
 
 module.exports = problem2;
