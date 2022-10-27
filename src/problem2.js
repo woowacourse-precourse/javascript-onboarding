3204613eaf45ce594b539ad3e2c2b70352ed8b5c
MJ <mj632900@gmail.com>
2022-10-27 15:40:18 +0900

    feat : 중복값이 있다면 중복값의 시작 인덱스 반환하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function getStartIndex(str) {
+    for (let i = 0; i < str.length; i++) {
+        if (str[i] === str[i + 1]) {
+            return i;
+        }
+    }
+    return -1;
 }
+
+function problem2(cryptogram) {}
 
 module.exports = problem2;
