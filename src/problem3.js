e6a2a405796b9159e041f9054c54d06bc83742ad
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-27 17:53:36 +0900

    [문제3] 3의배수 총개수 도출 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function getMultipleThree(number) {
+  let totalNumber = 0;
+  let index = 3;
+
+  while (index <= number) {
+    let numberArray = index.toString().split("");
+    numberArray.map((number) => {
+      if (number == 3 || number == 6 || number == 9) {
+        totalNumber++;
+      }
+    });
+    index++;
+  }
+  return totalNumber;
 }
 
 module.exports = problem3;
