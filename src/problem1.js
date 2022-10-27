997f9b2c894de3717a1f6e53290e454cad547895
KINHYEONJI <hyeonji0718@gmail.com>
2022-10-27 15:26:54 +0900

    feat : compareSumAndMultiple 함수 구현
    - 각 자릿 수의 합과 곱 중에 더 큰 수를 봔환

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
   return answer;
+}
+
+function compareSumAndMultiple(num){
+  let numOfDigits = num.toString().split("").map(x=>parseInt(x));
+  let digitsSum = numOfDigits.reduce((acc, cur) => acc + cur);
+  let digitsMultiple = numOfDigits.reduce((acc, cur) => acc * cur);
+  return digitsSum >= digitsMultiple ? digitsSum : digitsMultiple;
 }
 
 module.exports = problem1;
