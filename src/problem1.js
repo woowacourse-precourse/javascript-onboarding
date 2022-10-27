16afb9c590db6f2276fd8d715c70ce60a85eda33
CoboCho <rlaalsrb1111@naver.com>
2022-10-27 14:23:31 +0900

    feat(problem1): 예외처리 추가 및 함수 분리

--- src/problem1.js
+++ src/problem1.js
@@ -1,13 +1,23 @@
 function problem1(pobi, crong) {
-  if(pobi[1] !== pobi[0] + 1 || crong[1] !== crong[0] + 1) return -1;
-  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0) return -1;
-  if([1, 399].includes(pobi[0]) || [1, 399].includes(crong[0])) return -1;
+  let answer;
+  if(isVaild(pobi, crong) === false) return -1;
   let pobiScore = getScore(pobi);
   let crongScore = getScore(crong);
+  answer = decideWinner(pobiScore, crongScore);
+  return answer;
+}
+
+function isVaild(pobi, crong) {  
+  if(pobi[1] !== pobi[0] + 1 || crong[1] !== crong[0] + 1) return false;
+  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0) return false;
+  if(pobi[0] > 398 || crong[0] > 398 || pobi[0] < 2 || crong[0] < 2) return false;
+  return true;
+}
+
+function decideWinner(pobiScore, crongScore) {  
   if(pobiScore > crongScore) return 1;
   if(pobiScore < crongScore) return 2;
   if(pobiScore === crongScore) return 0;
-  return -1;
 }
 
 function getScore(pages) {
