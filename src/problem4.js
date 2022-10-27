7ef1ddbdf55923da77743b0ab2e663eabc7537f2
sery <yunsel2@gmail.com>
2022-10-27 23:45:05 +0900

    feat: Push reversed word string into reversedWordArray

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,7 @@ const reversedAlphabet = 'zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA';
 
 function problem4(word) {
   let answer;
+  let reversedWordArray = [];
   const validWord = /^[a-zA-Z\s]*$/;
 
   if (word.length < 1 || word.length > 1000) {
@@ -24,6 +25,8 @@ function problem4(word) {
       const index = alphabet.indexOf(w);
       return reversedAlphabet.charAt(index);
     });
+
+    reversedWordArray.push(reversedWord.join(''));
   });
 
   return answer;
