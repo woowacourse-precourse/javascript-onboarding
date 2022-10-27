0fd1dfc4b3e5ff74a8b7789e48317247fcce0f96
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-28 07:02:16 +0900

    feat: [problem6] Add setName

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,14 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+function problem6(forms) {}
+
+function setName(forms) {
+  const array = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      array.push(forms[i][1].substr(j, 2));
+    }
+  }
+  return array;
 }
 
 module.exports = problem6;
