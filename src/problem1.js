5e7e2a588f0720d070fecf622aa5f7e0c5e3cd52
Hypark2018 <hayoungpark2018@gmail.com>
2022-10-27 23:47:51 +0900

    🖊 feat: add isExceptional function logic & dummy-data for test

--- src/problem1.js
+++ src/problem1.js
@@ -5,9 +5,17 @@ function problem1(pobi, crong) {
   else if (getCalculate(crong) === getCalculate(pobi)) return 0
 }
 
-function isExceptional(pages){}
+function isExceptional(pages){
+  const isFirstOrLast = pages[0] >= 399 || pages[1] <= 2
+  const isFitOddandEven = pages[0] % 2 !== 1 || pages[1] % 2 !== 0
+  const isFitPageOrder = pages[0] + 1 !== pages[1] 
+
+  if(isFirstOrLast || isFitOddandEven || isFitPageOrder) return true
+}
 
 function getCalculate(pages){}
 
+
+console.log(problem1([99, 102], [211, 212]))
 
 module.exports = problem1;
