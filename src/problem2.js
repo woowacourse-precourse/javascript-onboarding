5bc25f7d01804e50291944702c8b6c5f58f2cad1
oing <74910908+oing-yong@users.noreply.github.com>
2022-10-27 12:41:31 +0900

    feat. 문제2 연속되는 문자를 한 번이라도 삭제한 단어에 다른 연속되는 문자가 있는지 다시 확인하는 기능
    - 연속되는 문자를 지울 때 isRemoved 변수 값이 1씩 추가 됨
    - isRmoved 변수 값이 0이 아니면 연속되는 문자를 찾는 반복문을 계속 실행함

--- src/problem2.js
+++ src/problem2.js
@@ -2,23 +2,27 @@ function problem2(cryptogram) {
   let answer = cryptogram;
   let tracker = 0;
 
-  for(let i = 0; i < length(cryptogram); i++){
-    if(cryptogram[i] === cryptogram[i+1]){
-      tracker = tracker + 1;
-      continue;
-    }
+  do {
+    let isRemoved = 0;
+    for (let i = 0; i < length(cryptogram); i++) {
+      if (cryptogram[i] === cryptogram[i + 1]) {
+        tracker = tracker + 1;
+        continue;
+      }
 
-    if(tracker != 0){
-      if(i-tracker === 0){
-        answer = answer.slice(i+1, length(answer)); 
+      if (tracker != 0) {
+        if (i - tracker === 0) {
+          answer = answer.slice(i + 1, length(answer));
+        } else if (i - tracker != 0) {
+          answer = answer.slice(0, i - (tracker + 1)) + answer.slice(i + 1, length(answer) - 1);
+        }
+        isRemoved++;
+        i = i - (tracker + 1);
+        tracker = 0;
       }
-      else if(i-tracker != 0){
-        answer = answer.slice(0, i-(tracker+1)) + answer.slice(i+1, length(answer) -1);
-      }
-      i = i - (tracker + 1);
-      tracker = 0;
     }
-  }
+  } while (isRemoved != 0);
+
   return answer;
 }
 
