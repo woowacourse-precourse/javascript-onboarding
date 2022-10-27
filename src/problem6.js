55268c6857524eaaed025556675f7ab83049fcb9
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-27 20:12:01 +0900

    problem6/feat : 이메일 관련 제한사항 처리

--- src/problem6.js
+++ src/problem6.js
@@ -3,6 +3,17 @@ function problem6(forms) {
   let arr = [];
 
   for (let i = 0; i < forms.length; i++) {
+    if (
+      forms[i][0].length < 10 ||
+      forms[i][0].length >= 20 ||
+      forms[i][0].slice(-10) != "@email.com"
+    ) {
+      forms.splice(i, 1);
+      i -= 1;
+    }
+  }
+
+  for (let i = 0; i < forms.length; i++) {
     for (let z = 0; z < forms[i][1].length - 1; z++) {
       for (let h = 1 + i; h < forms.length; h++) {
         if (forms[h][1].includes(forms[i][1][z] + forms[i][1][z + 1])) {
