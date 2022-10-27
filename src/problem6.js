bfd00f0b2083fed30306e5d1026b210a335cbe57
Gyeongm1n <affirming_km@naver.com>
2022-10-27 12:20:14 +0900

    각 닉네임에서 2글자 이상이며 2개 이상 존재하는 단어를 찾는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,37 @@
+function getsubStrings(nickname) {
+  const arr = [];
+
+  for (let start = 0; start < nickname.length - 1; start++) {
+    for (let end = start + 2; end <= nickname.length; end++) {
+      const word = nickname.substring(start, end);
+      arr.push(word);
+    }
+  }
+
+  return [...new Set(arr)];
+}
+
+function getWords(forms) {
+  const map = {};
+  const words = [];
+  for (const [_, nickname] of forms) {
+    if (nickname.length < 2) continue;
+    const subStrs = getsubStrings(nickname);
+    for (const str of subStrs) {
+      if (!map[str]) map[str] = 0;
+      map[str] += 1;
+    }
+  }
+
+  for (const word in map) {
+    if (map[word] >= 2) words.push(word);
+  }
+
+  return words;
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  const words = {};
 }
 
 module.exports = problem6;
