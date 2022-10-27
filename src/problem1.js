fd0ab59ede6b3efab055389e665e09c9fa447aea
leeuihyun <meta212121@naver.com>
2022-10-27 22:47:50 +0900

    feat : problem1 각 페이지의 큰 수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,15 @@
+function getBigNumber(number) {
+  const strNumber = String(number);
+  const newArray = strNumber.split("").map((v) => Number(v));
+  const sumResult = newArray.reduce((acc, currentValue) => {
+    return acc + currentValue;
+  }, 0);
+  const mulResult = newArray.reduce((acc, currentValue) => {
+    return acc * currentValue;
+  }, 1);
+  return Math.max(sumResult, mulResult);
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
