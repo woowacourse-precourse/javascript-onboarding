e6c33582a822fc3e472bde1e39fc1ddd7a866d38
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-27 12:48:08 +0900

    feat: problem4(번역하기 구현)

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,12 @@
 function problem4(word) {
-  var answer;
+  let answer;
 
   const MINIMUM_NUMBER = 1;
   const MAXIMAL_NUMBER = 1000;
+
+  const UPPER_CASE_RANGE_UTF16_CODE = [65, 90];
+  const LOWER_CASE_RANGE_UTF16_CODE = [97, 122];
+  const EMPTY_STRING_UTF16_CODE = 32;
 
   // - [x] 사전 구현
   const dictionary = {
@@ -47,12 +51,45 @@ function problem4(word) {
     throw new Error('word는 1 이상 1,000 이하인 문자열이다.');
   }
 
-  // - [ ] 번역하기
+  // - [x] 번역하기
+  //  - [x] 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+  //  - [x] 알파벳 외의 문자는 변환하지 않는다.
+  const isUpperCase = (_code) =>
+    _code >= UPPER_CASE_RANGE_UTF16_CODE[0] &&
+    _code <= UPPER_CASE_RANGE_UTF16_CODE[1];
+  const isLowerCase = (_code) =>
+    _code >= LOWER_CASE_RANGE_UTF16_CODE[0] &&
+    _code <= LOWER_CASE_RANGE_UTF16_CODE[1];
+  const isEmptyString = (_code) => _code === EMPTY_STRING_UTF16_CODE;
 
-  // - [ ] 알파벳 외의 문자는 변환하지 않는다.
+  const translateWord = (_word) => {
+    let result = [];
+    for (const oneLetter of _word) {
+      const code = oneLetter.charCodeAt();
 
-  // - [ ] 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+      if (isUpperCase(code)) {
+        const translateLetter = dictionary[oneLetter];
+        result.push(translateLetter.toUpperCase());
+        continue;
+      }
 
+      if (isLowerCase(code)) {
+        const translateLetter = dictionary[oneLetter.toUpperCase()];
+        result.push(translateLetter.toLowerCase());
+        continue;
+      }
+
+      if (isEmptyString(code)) {
+        result.push(' ');
+        continue;
+      }
+
+      result.push(oneLetter);
+    }
+    return result.join('');
+  };
+
+  answer = translateWord(word);
   return answer;
 }
 
