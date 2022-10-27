078df62f818a8ceb197ce7ad5ed027ebbf8e91ff
BBIYAC <kiy7833@gmail.com>
2022-10-27 18:25:03 +0900

    feat(problem3): add clap feature
    - 1부터 number까지 쳐야하는 손뼉 횟수 구하기 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
+// 1부터 number까지 쳐야하는 손뼉 횟수 구하기
 function problem3(number) {
-  var answer;
-  return answer;
+  let clap = 0;
+  for (let num = 1; num <= number; num++) {
+    for (let n of num.toString()) {
+      if (n === '3' || n === '6' || n === '9') clap++;
+    }
+  }
+  return clap;
 }
 
 module.exports = problem3;
