a2bcdb70657a56e34f549b96a9a162f3c428bcc1
gilpop8663 <wolfye@naver.com>
2022-10-28 05:51:31 +0900

    Add:[P7]_아이디를 key 값 , 친구 관계에 있는 배열과 점수를 가진 오브젝트를 value로 하는 Map 으로 유저 정보 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,30 @@
 function problem7(user, friends, visitors) {
   var answer;
+  const userInfo = new Map();
+
+  for (let i = 0; i < friends.length; i++) {
+    let first = friends[i][0];
+    let second = friends[i][1];
+
+    if (userInfo.has(first)) {
+      userInfo.set(first, {
+        friendship: [...userInfo.get(first).friendship, second],
+        score: 0,
+      });
+    } else {
+      userInfo.set(first, { friendship: [second], score: 0 });
+    }
+
+    if (userInfo.has(second)) {
+      userInfo.set(second, {
+        friendship: [...userInfo.get(second).friendship, first],
+        score: 0,
+      });
+    } else {
+      userInfo.set(second, { friendship: [first], score: 0 });
+    }
+  }
+
   return answer;
 }
 
