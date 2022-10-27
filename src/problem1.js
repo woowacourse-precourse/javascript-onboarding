47b1fca72fc5f7ed198bb2bfec4b41888cbdaaac
saram <yujun1018@naver.com>
2022-10-28 01:06:35 +0900

    [문제1] feat: 1번 기능에 왼쪽과 오른쪽의 페이지 번호를 대입해 구한 수를 비교하여 가장 큰 수를 구하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,10 @@
 function problem1(pobi, crong) {}
 
+const getGreatestNumber = (pageList) => {
+  if (pageList[0] + 1 !== pageList[1]) return 'error';
+  return Math.max(getBiggerNumber(pageList, 0), getBiggerNumber(pageList, 1));
+};
+
 const getBiggerNumber = (pageList, index) => {
   const digitList = [...pageList[index].toString()].map((v) => Number(v));
   const plus = digitList.reduce((prev, cur) => prev + cur, 0);
