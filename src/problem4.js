d3baf865d2e4d5e664edad89fcabb88341cb065a
eunsung <esk114477@gmail.com>
2022-10-27 19:09:12 +0900

    문제4번 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,25 @@
 function problem4(word) {
-  var answer;
-  return answer;
+
+  let arr = [...word];
+  
+  const f = arr.map(c => {
+    const code = c.charCodeAt(0);
+
+    let upperAlpha;
+    let lowerAlpha;
+
+    if(code <= 0x5A){
+      upperAlpha = 0x5A - code;
+      lowerAlpha = 0x41;
+    } else {
+      upperAlpha = 0x7A - code;
+      lowerAlpha = 0x61;
+    }
+    return String.fromCharCode(upperAlpha + lowerAlpha);
+  }).join('');
+
+  return f.replace(/{/g," ");
+  
 }
 
 module.exports = problem4;
