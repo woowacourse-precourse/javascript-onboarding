3fa2468863f4c1d0e935514ddf63ca1427d80eeb
yeonsuBeak <soothbugger@gmail.com>
2022-10-27 13:42:53 +0900

    feat: 소문자 알파벳이 아닌 경우 오류 메세지를 보여주는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -6,10 +6,21 @@ function problem2(cryptogram) {
   function checkErrors(word) {
     const MINIMUM_LENGTH = 1;
     const MAXIMUM_LENGTH = 1000;
+    const MINIMUM_ALPHABET = "a";
+    const MAXIMUM_ALPHABET = "z";
     const wordLength = word.length;
 
     if (wordLength < MINIMUM_LENGTH || wordLength > MAXIMUM_LENGTH) {
       return "문자열의 길이는 최소 1, 최대 1000입니다.";
+    }
+
+    for (let wordIndex = 0; wordIndex < wordLength; wordIndex++) {
+      if (
+        word[wordIndex] < MINIMUM_ALPHABET ||
+        word[wordIndex] > MAXIMUM_ALPHABET
+      ) {
+        return "문자열은 소문자 알파벳으로만 이루어져야 합니다.";
+      }
     }
 
     return checkDuplicates(word);
