068d01da2d206b01467b4dec6d15dcc69e5e2150
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 17:30:13 +0900

    refactor(problem4): 조회하는 각 문자가 대문자 또는 소문자인지 확인하는 함수 따로 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,19 +1,34 @@
 function problem4(word) {
   var answer = "";
 
-  for (let i = 0; i < word.length; i++) {
-    if (word[i].charCodeAt() >= 65 && word[i].charCodeAt() <= 90) {
-      let unicode = 155 - word[i].charCodeAt();
-      answer += String.fromCharCode(unicode);
-      continue;
+  const isUpperCase = (word) => {
+    return word.charCodeAt() >= 65 && word.charCodeAt() <= 90;
+  };
+
+  const isLowerCase = (word) => {
+    return word.charCodeAt() >= 97 && word.charCodeAt() <= 122;
+  };
+
+  const changeWord = (word) => {
+    let changedWord = "";
+
+    for (let i = 0; i < word.length; i++) {
+      if (isUpperCase(word[i])) {
+        let unicode = 155 - word[i].charCodeAt();
+        changedWord += String.fromCharCode(unicode);
+        continue;
+      }
+      if (isLowerCase(word[i])) {
+        let unicode = 219 - word[i].charCodeAt();
+        changedWord += String.fromCharCode(unicode);
+        continue;
+      }
+      changedWord += word[i];
     }
-    if (word[i].charCodeAt() >= 97 && word[i].charCodeAt() <= 122) {
-      let unicode = 219 - word[i].charCodeAt();
-      answer += String.fromCharCode(unicode);
-      continue;
-    }
-    answer += word[i];
-  }
+    return changedWord;
+  };
+
+  answer = changeWord(word);
   return answer;
 }
 
