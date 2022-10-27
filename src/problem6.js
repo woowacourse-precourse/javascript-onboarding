cf1baae3d62f4f5bcd22bfe1312cace318f82959
CoboCho <rlaalsrb1111@naver.com>
2022-10-27 16:56:49 +0900

    refactor(problem6): 하드코딩 변수 선언 적용

--- src/problem6.js
+++ src/problem6.js
@@ -2,9 +2,13 @@ function problem6(forms) {
   let answer = new Set();
   for(let i = 0; i < forms.length; i++) {
     for(let j = i + 1; j < forms.length; j++) {
-      if(isDuplicated(forms[i][1], forms[j][1])) {
-        answer.add(forms[i][0]);
-        answer.add(forms[j][0]);
+      const userNickname = forms[i][1];
+      const userEmail = forms[i][0];
+      const otherUserNickname = forms[j][1];
+      const otherUserEmail = forms[j][0];
+      if(isDuplicated(userNickname, otherUserNickname)) {
+        answer.add(userEmail);
+        answer.add(otherUserEmail);
       }
     }
   }  
@@ -16,7 +20,9 @@ function isDuplicated(name1, name2) {
   name2 = name2.split('');
   for(let i = 0; i < name1.length - 1; i++) {
     for(let j = 0; j < name2.length - 1; j++) {
-      if(name1[i] === name2[j] && name1[i + 1] === name2[j + 1]) return true;
+      const partOfName1 = name1[i] + name1[i + 1];
+      const partOfName2 = name2[j] + name2[j + 1];
+      if(partOfName1 === partOfName2) return true;
     }
   }
   return false;
