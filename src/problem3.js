44481f4133802f6097b34c37afee7b350f71f8f9
vinivin153 <vinivin153@naver.com>
2022-10-27 22:02:13 +0900

    feat: 손뼉치는 횟수 세는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,15 @@ function problem3(number) {
   return answer;
 }
 
+function getCountClap(num) {
+  const clapNumber = new Set(['3', '6', '9']);
+  let countClap = 0;
+  for (let i = 0; i < num.length; i++) {
+    if (clapNumber.has(num[i])) {
+      countClap++;
+    }
+  }
+  return countClap;
+}
+
 module.exports = problem3;
