4479f59ecc693a9e6b909aae083a943d304558d1
RooT_Kim <bdd3452@gmail.com>
2022-10-27 22:45:03 +0900

    문제 1번 요구사항 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,43 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+	let pobiMax, crongMax;
+
+	try {
+		pobiMax = getMax(pobi);
+		crongMax = getMax(crong);
+
+		return pobiMax > crongMax ? 1 : pobiMax < crongMax ? 2 : 0;
+	} catch (error) {
+		// console.error(error);
+		return -1;
+	}
+}
+
+function getMax(arr) {
+	const left = arr[0];
+	const right = arr[1];
+	let leftMax = 0,
+		rightMax = 0;
+
+	if (left + 1 !== right) throw new Error('연속된 수가 아닙니다.');
+
+	leftMax = resultAdd(left) >= resultMul(left) ? resultAdd(left) : resultMul(left);
+	rightMax = resultAdd(right) >= resultMul(right) ? resultAdd(right) : resultMul(right);
+
+	return leftMax >= rightMax ? leftMax : rightMax;
+}
+
+function resultAdd(num) {
+	return num
+		.toString()
+		.split('')
+		.reduce((acc, cur) => (acc += Number(cur)), 0);
+}
+
+function resultMul(num) {
+	return num
+		.toString()
+		.split('')
+		.reduce((acc, cur) => (acc *= Number(cur)), 1);
 }
 
 module.exports = problem1;
