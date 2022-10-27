e2707e52708b24657f187643c8e17ea0c3059bf4
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-27 20:21:11 +0900

    [problem1] Add validationArr function for check input value

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,14 @@
+console.log(problem1([95, 98],[197, 198]));
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(validationArr(pobi) && validationArr(crong)){
+    return 0;
+  }
+  return -1;
+}
+
+function validationArr(arr){
+  return arr[1]-arr[0] == 1?true:false;
 }
 
 module.exports = problem1;
