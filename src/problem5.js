1b7493b5f8d9084d0a3f07a127ac2b2aee88f7de
jihun1233 <leejihun1233@gmail.com>
2022-10-27 20:28:00 +0900

    문제 5 해결
    - 남은 돈을 돈의 단위로 나누고, 해당 단위만큼의 금액을 남은 돈에서 빼는것을 마지막까지 반복
    
    - 남은 돈이 0이 되거나 마지막 단위까지 마치면 종료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,20 @@
 function problem5(money) {
   var answer;
+
+  const moneyUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  answer = new Array(moneyUnits.length).fill(0)
+
+  let lastMoney = money
+  for (let i = 0; i < moneyUnits.length; i++) {
+    const unit = moneyUnits[i];
+    const currentUnitCount = Math.floor(lastMoney / unit)
+
+    answer[i] = currentUnitCount
+    lastMoney -= currentUnitCount * unit
+    
+    if(lastMoney === 0)break
+  }
+
   return answer;
 }
 
