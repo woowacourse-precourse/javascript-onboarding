4012d165d6a22ac0ce86a841aadc0f87c61b8cad
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 17:24:34 +0900

    feat(problem4): 청개구리 사전 참고해 입력받은 word 문자열 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i].charCodeAt() >= 65 && word[i].charCodeAt() <= 90) {
+      let unicode = 155 - word[i].charCodeAt();
+      answer += String.fromCharCode(unicode);
+      continue;
+    }
+    if (word[i].charCodeAt() >= 97 && word[i].charCodeAt() <= 122) {
+      let unicode = 219 - word[i].charCodeAt();
+      answer += String.fromCharCode(unicode);
+      continue;
+    }
+    answer += word[i];
+  }
   return answer;
 }
 
