e977817f80d206fd5626ef37ca57e107e390906c
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-27 18:29:20 +0900

    [문제4] 알파벳배열  변수 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,30 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+const alphabetArray = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
 
 module.exports = problem4;
