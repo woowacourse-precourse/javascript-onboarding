0e74cdb1a1e640ab41fe38d38447c89272430fe1
ReturnReason <reasonz@daum.net>
2022-10-27 15:34:18 +0900

    feat: problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  const reg = /[3|6|9]/g;
+
+  for (let i = 1; i <= number; i++) {
+    let samYukGuNumber = String(i).match(reg);
+    if (samYukGuNumber) {
+      answer += samYukGuNumber.length;
+    }
+  }
+
   return answer;
 }
 
