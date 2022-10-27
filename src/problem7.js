b0d63290cce1921904d05d681a41c45b195cecbb
Adult96 <djxor007@gmail.com>
2022-10-27 12:52:13 +0900

    feat: problem7 - length가 1개인 배열을 find

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
   //friends 매개변수에서 user매개변수를 없앤다
   let removeMrko = friends.map((v) => v.filter((v) => v !== user));
+
+  //length가 1개인 배열을 찾는다
+  const findFriends = removeMrko
+    .filter((v) => v.length === 1)
+    .flatMap((v) => v);
 }
 
 module.exports = problem7;
