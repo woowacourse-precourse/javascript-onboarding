eb8396bb978c459fe43544746b0d786c86693587
이건우 <lifeflavor@igeon-uui-MacBookPro.local>
2022-10-27 13:53:01 +0900

    problem1 - 예외1: 페이지가 연속된 숫자가 아닐경우 , 예외2 : 시작 면이나 마지막 면이 나올경우 -> -1

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,5 @@
 function problem1(pobi, crong) {
+  var answer;
   let arr = [];
   let plus;
   let multiply;
@@ -35,9 +36,22 @@ function problem1(pobi, crong) {
       ? crongLeftRight[0]
       : crongLeftRight[1];
 
-  if (pobiMax > crongMax) return 1;
-  else if (pobiMax < crongMax) return 2;
-  else if (pobiMax === crongMax) return 0;
+  if (pobiMax > crongMax) answer = 1;
+  else if (pobiMax < crongMax) answer = 2;
+  else if (pobiMax === crongMax) answer = 0;
+  // 에외 상황
+  if (pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1]) {
+    return -1;
+  } else if (
+    pobi[0] === 1 ||
+    crong[0] === 1 ||
+    pobi[1] === 400 ||
+    crong[1] === 400
+  ) {
+    return -1;
+  }
+
+  return answer;
 }
 
 module.exports = problem1;
@@ -47,4 +61,5 @@ module.exports = problem1;
 // 2. pobi의 왼쪽페이지와 오른쪽페이지의 값중 큰 값을 본인의 점수로 한다.
 // 3. crong 도 pobi와 같은 과정을 거친다.
 // 4. 점수를 비교해 승자를 가린다.
-//  - 테스트케이스3 예외 상황 : 페이지가 연속된 숫자가 아닐 경우
+//  - 예외 상황 : 페이지가 연속된 숫자가 아닐 경우
+//  - 예외 상황 : 시작 면이나 마지막 면이 나올 경우
