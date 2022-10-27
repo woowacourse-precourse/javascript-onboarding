f054cd92201e827b4f21edd31e34881278681576
Gyeongm1n <affirming_km@naver.com>
2022-10-27 13:06:22 +0900

    각 유저의 친구 목록을 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,27 @@
+function getRelation(friends) {
+  const relation = {};
+
+  for (const [A, B] of friends) {
+    if (!relation[A]) relation[A] = [];
+    if (!relation[B]) relation[B] = [];
+    relation[A].push(B);
+    relation[B].push(A);
+  }
+
+  return relation;
+}
+function getInfos(user, friends) {
+  const relation = getRelation(friends);
+  console.log(relation);
+}
+getInfos('', [
+  ['donut', 'andole'],
+  ['donut', 'jun'],
+  ['donut', 'mrko'],
+  ['shakevan', 'andole'],
+  ['shakevan', 'jun'],
+  ['shakevan', 'mrko'],
+]);
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
