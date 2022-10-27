35d4763c178fd2589d4be6c939a44eef1ec511fc
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-27 18:47:46 +0900

    Remove all for reset

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,6 @@
-console.log(problem2("browoanoommnaon"));
-
 function problem2(cryptogram) {
-  return removeDuplicatesLetters(cryptogram);
-}
-
-function removeDuplicatesLetters(word){
-  let answer=[word[0]];
-  for (let i = 1; i < word.length; i++) {
-    let top = answer[answer.length-1];
-    if (top != word[i]) {
-      answer.push(word[i]);
-    } else {
-      answer.pop();
-    }
-    console.log(answer.join(""));
-  }
-  return answer.join("");
+  var answer;
+  return answer;
 }
 
 module.exports = problem2;
