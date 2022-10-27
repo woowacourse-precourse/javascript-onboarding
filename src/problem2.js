3cab7d8fecbe029cbb80f92142043fde0a3cf9db
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-27 22:29:30 +0900

    feat; [P2] 기능 구현
    1. 문자열 cryptogram의 첫번째 문자를 포함하는 stack 만들기
    
    2. cryptogram의 두번째 문자부터 마지막 문자까지 해당 문자가 앞 문자와 동일한지 확인하기
    - 이전 문자와 동일하면 해당 문자를 stack에서 제외하기
    - 이전 문자와 동일하지 않으면 해당 문자를 stack에 추가하기
    
    3. stack에 있는 문자를 공백없이 모두 합친 새로운 문자열 반환하기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let stack = new Array(cryptogram[0])
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (stack[stack.length - 1] === cryptogram[i]) stack.pop()
+    else stack.push(cryptogram[i])
+  }
+  
+  return stack.join("")
 }
 
 module.exports = problem2;
