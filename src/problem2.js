7b59835f9846cce578c187acf5c8af718d7c4901
KINHYEONJI <hyeonji0718@gmail.com>
2022-10-27 21:28:26 +0900

    feat : isDuplicate 구현
    - 똑같은 문자가 연속될 경우 1, 아닐 경우 0을 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  while(isDuplicate(cryptogram)){
+
+  }
   return answer;
 }
+function isDuplicate(cryptogram){
+  for(let i =0; i<cryptogram.length-1; i++){
+    if(cryptogram[i]===cryptogram[i+1])
+      return 1;
+  }
+  return 0;
+}
+
 
 module.exports = problem2;
