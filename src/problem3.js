a5c17c5e0ee30e1c5e20f795fb22d6ec7c63459d
An Sol <apdlvmf486@gmail.com>
2022-10-27 09:22:53 +0900

    3, 6, 9의 개수를 저장하고 리턴하는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,12 @@
 function problem3(number) {
+  let count = 0;
   for (let num = 1; num < number + 1; num++){
     const numToString = num.toString().match(/[369]{1}/g);
+    if (numToString){
+      count += numToString.length;
+    }
   }
+  return count;
 }
 
 module.exports = problem3;
