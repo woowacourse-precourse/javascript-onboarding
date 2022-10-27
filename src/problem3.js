0ce13fc86369f76dceb2bc258f1edd1683fec01a
rachel490 <ccori12@naver.com>
2022-10-28 00:32:32 +0900

    add problem3 solution

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,21 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function countClaps(i) {
+  const target = "369";
+  const claps = [...String(i)].filter(num => target.includes(num)).length 
+
+  return claps;
 }
+
+function problem3(number) {
+  let cnt = 0;
+
+  for (let i = 1; i <= number; i++) {
+    cnt += countClaps(i)
+  }
+
+  return cnt;
+}
+
+// console.log(problem3(13))
+// console.log(problem3(33))
 
 module.exports = problem3;
