fe7577b38e0bca120471c35bde6a28de5d7928fb
Adult96 <djxor007@gmail.com>
2022-10-27 12:45:44 +0900

    feat: problem6 - 배열에 담긴 닉네임을 2글자씩 나누는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,14 @@
 function problem6(forms) {
   //닉네임만 배열로 만든다
   const nameArr = forms.map((v) => v[1]);
+
+  //닉네임을 두글자씩 나눈다
+  const parserName = [];
+  for (let i = 0; i < nameArr.length; i++) {
+    for (let j = 0; j < nameArr[i].length - 1; j++) {
+      parserName.push(nameArr[i][j] + nameArr[i][j + 1]);
+    }
+  }
 }
 
 module.exports = problem6;
