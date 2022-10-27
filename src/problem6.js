f149a5da6d9439717ac70987db0153ff1c854c43
BB-choi <choibobae0809@gmail.com>
2022-10-27 18:10:24 +0900

    feat: 문제6 - 연속 글자 저장

--- src/problem6.js
+++ src/problem6.js
@@ -13,8 +13,17 @@ function extractWord({ nickname, characterLength = 2 }) {
 function problem6(forms) {
   var answer;
 
-  /* {"단어" => {count: 0, user : [[email, 닉네임], [email,닉네임]]}} */
-  const nicknameWordSet = new Map();
+  /* {"단어" => {count: 0, users : [[email, 닉네임], [email,닉네임]]}} */
+  const nicknameWordMap = new Map();
+
+  for (const [email, nickname] of forms) {
+    const wordSet = extractWord({ nickname });
+    wordSet.forEach((word) =>
+      nicknameWordMap.set(word, { count: 0, users: [] })
+    );
+  }
+
+  console.log(nicknameWordMap);
 
   return answer;
 }
