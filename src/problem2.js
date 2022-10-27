fed686c5ab6e57a9b01fea02c327cfb4840ae49c
jangsumin <jsm0726@naver.com>
2022-10-27 19:43:11 +0900

    문제2 : 주석으로 의사 코드 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,8 @@
+// 의사 코드 작성
+// 1. cryptogram의 첫 문자를 새로운 배열에 push
+// 2. 새로운 배열에 들어가있는 문자와 이제 들어올 문자를 비교해서 같으면 pop
+// 3. 하지만 위 과정을 여러번 거쳐서 더 이상 연속되는 중복 문자가 없어야 함
+
 function problem2(cryptogram) {
   var answer;
   return answer;
