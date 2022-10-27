a9e945159f5d1b7bfc9bc8fe51fdf5796e28dfbc
DongjoonShin <ehdwns1516@ajou.ac.kr>
2022-10-27 15:01:19 +0900

    problem2 중복 코드 제거 및 설명 추가

--- src/problem2.js
+++ src/problem2.js
@@ -3,13 +3,13 @@ function problem2(cryptogram) {
 
   let cur = 0;
   while (cur < cryptogram.length) {
-    if (answer.length == 0) {
-      answer.push(cryptogram[cur]);
-      cur++;
-    } else if (answer[answer.length - 1] != cryptogram[cur]) {
+    // 스택의 최상위 문자와 cryptogram[cur] 문자를 비교하며 연속된 중복 원소면 스택을 pop해주고 연속되지 않을 때까지 건너뛴다.
+    if (answer.length == 0 || answer[answer.length - 1] != cryptogram[cur]) {
+      // 문자가 연속되지 않을 때 문자를 push해준다
       answer.push(cryptogram[cur]);
       cur++;
     } else {
+      // 문자가 연속될 때
       while (
         answer[answer.length - 1] == cryptogram[cur] &&
         cur < cryptogram.length
@@ -19,6 +19,7 @@ function problem2(cryptogram) {
       answer.pop();
     }
   }
+
   answer = answer.join('');
   return answer;
 }
