784a3f0e194b37c2f66f1673b7f980c99851a9e2
turtle601 <turtle601@naver.com>
2022-10-27 12:36:17 +0900

    Feat: [problem3] 제한 사항 구현 로직인 ErrorCase 구현
    <ErrorCase클래스>
    - checkLimit() : input(number)길이가 1이상 10000이하인지 확인

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
-function problem3(number) {
-  var answer;
-  return answer;
+class ErrorCase {
+  constructor(number) {
+    this.number = number;
+    this.checkLimit();
+  }
+
+  checkLimit() {
+    if (!(1 <= this.number && this.number <= 10000)) {
+      throw new Error("input 양식에 에러가 발생했습니다!!");
+    }
+  }
 }
+
+function problem3(number) {
+  new ErrorCase(number);
+}
+
+/*
+  에러 케이스 테스트
+  
+  problem3(0);
+  problem3(10001);
+*/
 
 module.exports = problem3;
