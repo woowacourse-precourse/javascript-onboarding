80553b591fab3955910bf65d94dc301cd54dd353
하송희 <99heesong@naver.com>
2022-10-27 18:04:53 +0900

    [문제 2] 예외처리: 입력 문자열 검증

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,7 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const isLowerCase = (string) => /^[a-z]*$/.test(string)
+  if(!isLowerCase(cryptogram)) throw Error('문자열이 소문자만으로 이루어지지 않았습니다.')
+  if(cryptogram.length<1||cryptogram.length>1000) throw Error('문자열의 길이가 맞지 않습니다.');
 }
 
 module.exports = problem2;
