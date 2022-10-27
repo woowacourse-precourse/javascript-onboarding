de7202c80229a7ca27681a55dd5f92d2a13592cf
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-27 21:50:55 +0900

    문제4. 문자열 속 각 문자를 ASCII 범위를 이용해 대문자인지 소문자인지 구분

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,10 @@
 function problem4(word) {
   var answer;
+  for (x of word) {
+    if (65 <= x.charCodeAt(0) && x.charCodeAt(0) <= 90) {
+    } else if (97 <= x.charCodeAt(0) && x.charCodeAt(0) <= 122) {
+    } else continue;
+  }
   return answer;
 }
 
