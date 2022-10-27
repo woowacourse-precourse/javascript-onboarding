6557534c28122f4cda4dc8a09530a499ae4c5973
evencoding <evencoding@gmail.com>
2022-10-27 16:59:37 +0900

    [문제4] implement: 알파벳의 ASCII코드라면 대소문자를 판별해 reversed 된 문자를, 아니면 문자를 반환하는 함수

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,20 @@
+const LOWER_A_ASCII = 65;
+const LOWER_Z_ASCII = 90;
+const LOWER_A_PLUS_Z = 155;
+const UPPER_A_ASCII = 97;
+const UPPER_Z_ASCII = 122;
+const UPPER_A_PLUS_Z = 219;
+
+function asciiToReversedChar(ascii) {
+  if (ascii >= LOWER_A_ASCII && ascii <= LOWER_Z_ASCII) {
+    return String.fromCharCode(LOWER_A_PLUS_Z - ascii);
+  } else if (ascii >= UPPER_A_ASCII && ascii <= UPPER_Z_ASCII) {
+    return String.fromCharCode(UPPER_A_PLUS_Z - ascii);
+  } else {
+    return String.fromCharCode(ascii);
+  }
+}
+
 function charToAscii(char) {
   return char.charCodeAt();
 }
@@ -6,6 +23,7 @@ function problem4(word) {
   let reversedWord = '';
   for (let i = 0; i < word.length; i++) {
     const ascii = charToAscii(word[i]);
+    reversedWord += asciiToReversedChar(ascii);
   }
   return reversedWord;
 }
