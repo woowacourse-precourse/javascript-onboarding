06dfeab7017cb68bbd17124bd2b98f2e74647fd6
Suyeon <msjgr2801@gmail.com>
2022-10-27 14:37:50 +0900

    fix, comment: fix problem1's return value & 1,2's comment

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,9 @@
-// 0. 예외 처리
-//  0.1 [x] (2 ~ 398) 사이 숫자일 것
+// 예외 처리
+// 0. [x] (2 ~ 398) 사이 숫자일 것
 
-// 1. 로직
-//  1.1 [x] pobi와 crong이 가질 수 있는 가장 큰 수 구하기
-//  1.2 [x] pobi와 crong의 숫자를 비교하기
+// 로직
+//  1. [x] pobi와 crong이 가질 수 있는 가장 큰 수 구하기
+//  2. [x] pobi와 crong의 숫자를 비교하기
 
 const plus = (nums) => {
   return nums.reduce((prev, curr) => {
@@ -17,10 +17,11 @@ const multiple = (nums) => {
   }, 1);
 }
 
+// 1. pobi와 crong이 가질 수 있는 가장 큰 숫자 구하기
 const calMax = (pobi, crong) => {
   [pobi[0], pobi[1]] = [String(pobi[0]).split(""), String(pobi[1]).split("")];
   [crong[0], crong[1]] = [String(crong[0]).split(""), String(crong[1]).split("")];
-
+  // 더하거나 곱한 것 중 가장 큰 수 구하기
   const pobiMax = Math.max(plus(pobi[0]), plus(pobi[1]), multiple(pobi[0]), multiple(pobi[1]));
   const crongMax = Math.max(plus(crong[0]), plus(crong[1]), multiple(crong[0]), multiple(crong[1]));
 
@@ -28,13 +29,22 @@ const calMax = (pobi, crong) => {
 }
 
 function problem1(pobi, crong) {
-  // 예외 사항
-  if (pobi[1] <= 2 || pobi[0] >= 399) return -1;
-  if (crong[1] <= 2 || crong[0] >= 388) return -1;
-
+  let answer = -1;
+  // 0. 예외 처리
+  if (pobi[1] <= 2 || pobi[0] >= 399) return answer;
+  if (crong[1] <= 2 || crong[0] >= 388) return answer;
+  // 1번 로직 함수
   const [maxPobi, maxCrong] = calMax(pobi, crong);
+  // 2. pobi와 crong 중 더 큰 숫자를 가진 사람이 pobi면 1, crong이면 2, 무승부면 0
+  if (maxPobi > maxCrong) {
+    answer = 1;
+  } else if (maxPobi < maxCrong) {
+    answer = 2;
+  } else {
+    answer = 0;
+  }
 
-  return Math.max(maxPobi, maxCrong);
+  return answer;
 }
 
 module.exports = problem1;
