ee2e4bf12000d2b458a8c37532403c7f521638d0
pangkyu <baesense@naver.com>
2022-10-27 15:21:15 +0900

    feat : 포비와 크롱 승부가리기
    포비가 이기면 1
    크롱이 이기면 2
    무승부면 0을 리턴한다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,11 +1,29 @@
 function problem1(pobi, crong) {
   const pageNumber = new Array(401);
   pageNumber[0] = 0;
-  console.log(pobi, crong);
+  const pobiRecord = getRecord(pobi);
+  const crongRecord = getRecord(crong);
+
+  if (pobiRecord > crongRecord) {
+    return 1;
+  } else if (pobiRecord == crongRecord) {
+    return 0;
+  } else if (pobiRecord < crongRecord) {
+    return 2;
+  }
 }
 
-function testCode() {
-  problem1([3, 0], [123, 124]);
-}
+function getRecord(user) {}
+
+// function testCode() {
+//   let counter = 0;
+//   let pobiArray;
+//   for (let i = 0; i < 400; i++) {
+//     console.log(Math.floor(Math.random() * (399 - 2) + 2));
+//     counter++;
+//   }
+// }
 testCode();
+problem1();
+
 module.exports = problem1;
