33232815c6f8e7c214ee7f8bf17224f17e876485
moonelysian <dandyblack@naver.com>
2022-10-27 12:10:50 +0900

    add: 중복되는 닉네임을 가진 이메일 목록 배열로 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,14 @@
+function getDuplicatedEmails (index, target, forms) {
+  let duplicatedEmails = [];
+  forms.map((form, idx) => {
+    const [email, nickname] = form;
+    if (idx !== index && nickname.includes(target)) {
+      duplicatedEmails = [...duplicatedEmails, email];
+    }
+  });
+  return duplicatedEmails;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
