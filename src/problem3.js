0e03d561b1aa17b27aaf928f4ec68bb45f21ce45
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-27 15:54:41 +0900

    refactor: 숫자를 문자열로 변환하지 않고 각 자릿수가 3,6,9인지 확인하는 함수로 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,19 +1,22 @@
 function problem3(number) {
   var answer = 0;
 
-  const cntOfClap = (num) => {
-    let cnt = 0;
-    for (let i = 0; i < num.length; i++) {
-      if (num[i] == "3" || num[i] == "6" || num[i] == "9") {
-        cnt += 1;
-      }
+  const is369 = (num) => {
+    if (num == 3 || num == 6 || num == 9) {
+      return 1;
     }
-    return cnt;
+    return 0;
+  };
+
+  const clap_cnt_of_num = (num) => {
+    if (num < 10) {
+      return is369(num);
+    }
+    return is369(num % 10) + clap_cnt_of_num(parseInt(num / 10));
   };
 
   for (let num = 1; num <= number; num++) {
-    changedNum = num.toString();
-    answer += cntOfClap(changedNum);
+    answer += clap_cnt_of_num(num);
   }
   return answer;
 }
