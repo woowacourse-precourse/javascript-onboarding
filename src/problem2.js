bfaf91616ee0b47245dde68af9c018a72c3dea97
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-27 11:33:04 +0900

    feat: problem2(cryptogram 길이 제한 구현)

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,26 @@
 function problem2(cryptogram) {
   var answer;
+  const minimumLength = 1;
+  const maximalLength = 1000;
+
+  // - [x] cryptogram은 길이가 1 이상 1000 이하인 문자열이다.
+  const checkStringLength = (_cryptogram, min, max) => {
+    if (!typeof _cryptogram === 'string') {
+      return false;
+    }
+    const length = _cryptogram.length;
+    return length >= min && length <= max;
+  };
+
+  if (!checkStringLength(cryptogram, minimumLength, maximalLength)) {
+    throw new Error('cryptogram은 길이가 1 이상 1000 이하인 문자열입니다.');
+  }
+
+  // - [ ] cryptogram은 알파벳 소문자로만 이루어져 있다.
+
+  // - [ ] 연속하는 중복 문자 삭제하기
+  // - [ ] 배열 문자열로 바꾸기
+
   return answer;
 }
 
