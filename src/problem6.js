555ee1615d96c647ce0d6441af5963911b53d7b3
Jiwoo Park <jiwoo9446@naver.com>
2022-10-27 15:08:10 +0900

    기능목록 추가

--- src/problem6.js
+++ src/problem6.js
@@ -20,13 +20,5 @@ function problem6(forms) {
   });
   return new Set(answer.sort());
 }
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
+
 module.exports = problem6;
--- src/problem7.js
+++ src/problem7.js
@@ -2,5 +2,18 @@ function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
-
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 module.exports = problem7;
