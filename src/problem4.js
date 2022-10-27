eb419702d3cfe9cad020fabec813dbd6d725c847
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-27 22:01:43 +0900

    문제4. 각 문자들 규칙에 맞춰 치환 후 변수에 저장

--- src/problem4.js
+++ src/problem4.js
@@ -1,9 +1,12 @@
 function problem4(word) {
   var answer;
   for (x of word) {
+    let letter = x;
     if (65 <= x.charCodeAt(0) && x.charCodeAt(0) <= 90) {
+      letter = String.fromCharCode(155 - x.charCodeAt(0));
     } else if (97 <= x.charCodeAt(0) && x.charCodeAt(0) <= 122) {
-    } else continue;
+      letter = String.fromCharCode(219 - x.charCodeAt(0));
+    }
   }
   return answer;
 }
