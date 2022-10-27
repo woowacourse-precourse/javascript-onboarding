370813018aeebace7c706b157c4335d8dc0e7b84
han0224 <dbn0224@gmail.com>
2022-10-27 16:46:16 +0900

    [문제6] 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const emailSet = new Set();
+  for (let i = 0; i < forms.length; i++) {
+    for (let index = 1; i < forms[i][1].length; i++) {
+      const tmp = forms[i][1].slice(index - 1, index + 1);
+      for (let j = i + 1; j < forms.length; j++) {
+        if (forms[j][1].includes(tmp)) {
+          emailSet.add(forms[j][0]);
+          emailSet.add(forms[i][0]);
+        }
+      }
+    }
+  }
+  return Array.from(emailSet).sort((a, b) => a - b);
 }
 
 module.exports = problem6;
