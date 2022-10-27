7f69783974e16e89b4e15a10bc30ca0e8f1ad722
이예나 <yenalee@MacBook-Pro.local>
2022-10-27 23:02:57 +0900

    Feat: Return space

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
   var answer;
+  var newWord = '';
+  var newSen = '';
+  console.log(word);
+  // console.log(word[0]);
+  for(let i=0;i<word.length;i++){
+    if(word[i] === ' '){
+      newSen += ' ';
+    }
+  }
+  console.log(newSen);
+  answer = newSen;
   return answer;
 }
 
 module.exports = problem4;
+word = 'I love you';
+problem4(word);
