63565fe792667b5617996fba982d860c450264bb
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-27 12:16:27 +0900

    feat: problem3(손뼉 칠 숫자 세기 구현)

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,7 @@ function problem3(number) {
   let answer;
   const MINIMUM_NUMBER = 1;
   const MAXIMAL_NUMBER = 10000;
+  const CLAP_NUMBER_REGEX = /[3,6,9]/g;
 
   // - [x] number는 1 이상 10,000 이하인 자연수이다.
   const isAllowNumberRange = (number, min, max) => {
@@ -15,7 +16,15 @@ function problem3(number) {
     throw new Error('number는 1 이상 10,000 이하인 자연수이다.');
   }
 
-  // - [ ] 3, 6, 9 숫자 세기
+  // - [x] 3, 6, 9 숫자 세기
+  let spreadNumbers = [];
+  for (let i = 0; i < number; i++) {
+    spreadNumbers.push(i + 1);
+  }
+
+  const matchNumbers = spreadNumbers.join('').matchAll(CLAP_NUMBER_REGEX);
+  const matchNumbersToArray = [...matchNumbers];
+  answer = matchNumbersToArray.length;
 
   return answer;
 }
