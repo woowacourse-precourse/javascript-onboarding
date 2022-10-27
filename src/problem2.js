f4dc0e466e0c2f0db18929c5025c4222daef2905
biyamn <b1y9mn@gmail.com>
2022-10-27 13:06:40 +0900

    docs: README 목차 링크 이동 테스트

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+
+  for (let i = 0; i < cryptogram.length; i++ ) {
+    
+  }
+
   return answer;
 }
 
 module.exports = problem2;
+
+problem2("browoanoommnaon");
