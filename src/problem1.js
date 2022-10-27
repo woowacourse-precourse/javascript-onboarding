4a5beb89df9a18fdb9285bf23a5bb6eaaa76dcde
leeuihyun <meta212121@naver.com>
2022-10-28 00:44:35 +0900

    refactor : problem1 가독성을 올리기 위해서 페이지 자리수 곱과 합을 리턴하는 함수 분리

--- src/problem1.js
+++ src/problem1.js
@@ -1,12 +1,20 @@
+function sumNumOfDigit(array) {
+  return array.reduce((acc, currentValue) => {
+    return acc + currentValue;
+  }, 0);
+}
+
+function mulNumOfDigit(array) {
+  return array.reduce((acc, currentValue) => {
+    return acc * currentValue;
+  }, 1);
+}
+
 function getBigNumber(number) {
   const strNumber = String(number);
   const newArray = strNumber.split("").map((v) => Number(v));
-  const sumResult = newArray.reduce((acc, currentValue) => {
-    return acc + currentValue;
-  }, 0);
-  const mulResult = newArray.reduce((acc, currentValue) => {
-    return acc * currentValue;
-  }, 1);
+  const sumResult = sumNumOfDigit(newArray);
+  const mulResult = mulNumOfDigit(newArray);
   return Math.max(sumResult, mulResult);
 }
 
