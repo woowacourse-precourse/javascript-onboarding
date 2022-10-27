eddf64277023d544f832d26f69b34597498043bc
sumin <csumin008255@gmail.com>
2022-10-28 00:01:31 +0900

    problem2_기능목록작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,11 @@
+/* 기능 목록
+* 1. 문자열 cryptogram을 문자 배열로 변환
+* 2. 연속된 요소 비교 후 다른 경우에 새로운 배열에 추가
+* 3. 문자열로 변환 후 결과 출력
+* 4. 코드 정리
+*/
+
+
 function problem2(cryptogram) {
   var answer;
   return answer;
