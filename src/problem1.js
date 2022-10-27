82fa675d90ad67087e86cd9502f3596c86a3caa4
HANSANGJUN <hsj11962@naver.com>
2022-10-27 21:10:41 +0900

    1번 문제완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,62 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+  let pobiScore = [];
+  let crongScore = [];
+
+  if (
+    pobi[1] - pobi[0] != 1 ||
+    crong[1] - crong[0] != 1 ||
+    pobi[0] % 2 != 1 ||
+    pobi[1] % 2 != 0 ||
+    crong[0] % 2 != 1 ||
+    crong[1] % 2 != 0
+  ) {
+    return -1;
+  }
+
+  for (var i = 0; i < 2; i++) {
+    var sum = 0;
+    var mul = 1;
+    var n = pobi[i];
+
+    do {
+      var quot = n % 10;
+      sum += quot;
+      mul *= quot;
+
+      n = Math.floor(n / 10);
+    } while (n > 0);
+
+    pobiScore.push(sum);
+    pobiScore.push(mul);
+  }
+
+  for (var i = 0; i < 2; i++) {
+    var sum = 0;
+    var mul = 1;
+    var n = crong[i];
+    do {
+      var quot = n % 10;
+      sum += quot;
+      mul *= quot;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+
+    crongScore.push(sum);
+    crongScore.push(mul);
+  }
+
+  pobiScore.sort(function (a, b) {
+    return b - a;
+  });
+  crongScore.sort(function (a, b) {
+    return b - a;
+  });
+
+  if (pobiScore[0] > crongScore[0]) answer = 1;
+  else if (pobiScore[0] < crongScore[0]) answer = 2;
+  else answer = 0;
+
   return answer;
 }
 
