f312a39a37e0fa3a0746790b4b0d61def63f0288
haesa <xdhaesa@gmail.com>
2022-10-27 15:49:52 +0900

    problem6 중복 확인 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
+function checkDuplication(nickname, forms) {
+  const newForms = forms.filter(([email, nick]) => nick !== nickname);
+  for (let i = 0; i < nickname.length - 1; i++)
+    for (let j = i + 2; j <= nickname.length; j++) {
+      const check = newForms //
+        .some(([email, nick]) => nick.includes(nickname.substring(i, j)));
+      if (check) return true;
+    }
+  return false;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
 }
-
 module.exports = problem6;
