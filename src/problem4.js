5dbeebcdee8141490947d1c159d9ea52fa7c517a
nsyeon <lovepny@gmail.com>
2022-10-27 21:06:41 +0900

    feat: 문자 치환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,30 @@
 function problem4(word) {
   var answer;
+
+  answer = "";
+
+  var upper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ"  
+  var lower = "abcdefghijklmnopqrstuvwxyz"
+  
+  for(i = 0; i < word.length; i++) {  //i => I love you
+    for(j = 0; j < 26; j++){          //j => 대소문자 인덱스
+      //대문자 인덱스 확인
+      if(word[i] === upper[j]) {      //ex) word의 'I'에 인덱스 추출
+        answer += upper[25-j];
+        console.log("upper : " + answer);
+      }//소문자 인덱스 확인
+      if(word[i] === lower[j]) {
+        answer += lower[25-j];
+        console.log("lower : " + answer);
+      }
+    }
+    //공백 처리
+    if(word[i] == " "){
+      answer += " ";
+      console.log("spacing : " + answer);
+    }
+  }
+  
   return answer;
 }
 
