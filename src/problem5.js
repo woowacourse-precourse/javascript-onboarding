04901c3cc8f7f6c2eb0996d7c6c9c4d1e8196796
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 21:09:19 +0900

    feat: [문제5] money 범위 밖 예외처리 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,15 @@
 function problem5(money) {
+  if (money < 1 || money > 1000000) {
+    throw new Error('money는 1이상 1,000,000이하여야 합니다.');
+  }
+
   const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const result = moneyList.map((moneyType) => {
     const count = Math.floor(money / moneyType);
     money = money % moneyType;
     return count;
   });
-
   return result;
 }
-
-problem5(15000); //?
 
 module.exports = problem5;
