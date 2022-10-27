a6cf3ff8f6d706bb4718abd5f0d3cfc3d20d0ad5
h2002y <h2002y@naver.com>
2022-10-27 12:09:55 +0900

    feat: Add setUserScores func

--- src/problem7.js
+++ src/problem7.js
@@ -2,25 +2,31 @@ function problem7(user, friends, visitors) {
   var answer;
   let map = new Map();
   let mapIndex = 0;
-  mapIndex = setMap(map, mapIndex, user, friends, visitors);
+  let userList = [];
+  mapIndex = setMap(map, mapIndex, user, friends, visitors, userList);
   let relation = new Array(mapIndex+1);
+  let userScores = new Array(mapIndex+1);
   setRelation(relation, friends, map);
+  setUserScores(userScores, relation, visitors, userList);
+  console.log(userScores);
   return answer;
 }
 
-function setMap(map, mapIndex, user, friends, visitors){
+function setMap(map, mapIndex, user, friends, visitors, userList){
   map.set(user, mapIndex);
+  userList.push(user);
   mapIndex++;
-  mapIndex = setMapFriends(map, mapIndex, friends);
-  mapIndex = setMapVisitors(map, mapIndex, visitors);
+  mapIndex = setMapFriends(map, mapIndex, friends, userList);
+  mapIndex = setMapVisitors(map, mapIndex, visitors, userList);
   return mapIndex;
 }
 
-function setMapFriends(map, mapIndex, friends){
+function setMapFriends(map, mapIndex, friends, userList){
   for(let i = 0; i < friends.length; i++){
     for(let j = 0; j < 2; j++){
       if(!map.has(friends[i][j])){
         map.set(friends[i][j], mapIndex);
+        userList.push(friends[i][j]);
         mapIndex++;
       }
     }
@@ -28,10 +34,11 @@ function setMapFriends(map, mapIndex, friends){
   return mapIndex;
 }
 
-function setMapVisitors(map, mapIndex, visitors){
+function setMapVisitors(map, mapIndex, visitors, userList){
   for(let i = 0; i < visitors.length; i++){
     if(!map.has(visitors[i])){
       map.set(visitors[i], mapIndex);
+      userList.push(visitors[i]);
       mapIndex++;
     }
   }
@@ -48,7 +55,17 @@ function setRelation(relation, friends, map){
     relation[map.get(leftNode)].push(rightNode);
     relation[map.get(rightNode)].push(leftNode);
   }
-  console.log(relation);
+}
+
+function setUserScores(userScores, relation, visitors, userList){
+  for(let i = 0; i < userList.length; i++){
+    console.log(userList[i]);
+    userScores[i] = {
+      name: "",
+      score: 0,
+    };
+    userScores[i].name = userList[i];
+  }
 }
 
 module.exports = problem7;
