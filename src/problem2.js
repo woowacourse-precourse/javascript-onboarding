66c825378f633a37a32526a6e16535313ad31c97
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-28 04:14:21 +0900

    [feat] problem2 - 기능(1)중복된 문자열이 있는지 확인하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,16 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function check(word) {
+  const delete_list = [];
+  for (let i = 1; i < word.length; i++) {
+    if (word[i] === word[i - 1]) {
+      delete_list.push(i);
+      if (delete_list.includes(i - 1)) continue;
+      else delete_list.push(i - 1);
+    }
+  }
+  return delete_list.sort((a, b) => b - a);
+}
+
 module.exports = problem2;
