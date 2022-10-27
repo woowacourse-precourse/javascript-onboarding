ab7f23f28955d88469a97d7dcd740b06685058bb
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-28 03:26:25 +0900

    feat: 3, 6, 9가 몇번 포함 되어있는지 세는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
-  var answer;
-  return answer;
-}
+  let count_369 = 0;
 
+  for (let i = 1; i <= number; i++) {
+    let seperate_number_array = String(i).split("");
+
+    for (let j = 0; j < seperate_number_array.length; j++) {
+      if (
+        seperate_number_array[j] === "3" ||
+        seperate_number_array[j] === "6" ||
+        seperate_number_array[j] === "9"
+      )
+        count_369++;
+    }
+  }
+  return count_369;
+}
 module.exports = problem3;
