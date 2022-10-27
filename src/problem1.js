1606f8f5392c3cb290e0e8bfd54fb2f3e88e3429
nlom0218 <nlom0218@naver.com>
2022-10-27 18:07:56 +0900

    feat: 문제 1 - 구현 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,42 +1,3 @@
-// const splitNumber = (num) => {
-//   num = num + "";
-//   return num.split("").map((item) => Number(item));
-// };
-
-// const getScore = (leftNums, rightNums) => {
-//   const leftSum = leftNums.reduce((acc, cur) => acc + cur, 0);
-//   const leftMultiply = leftNums.reduce((acc, cur) => acc * cur, 1);
-//   const rightSum = rightNums.reduce((acc, cur) => acc + cur, 0);
-//   const rightMultiply = rightNums.reduce((acc, cur) => acc * cur, 1);
-//   return Math.max(leftSum, leftMultiply, rightSum, rightMultiply);
-// };
-
-// function problem1(pobi, crong) {
-//   const [pobiLeftPage, pobiRightPage] = pobi;
-//   const [crongLeftPage, crongRightPage] = crong;
-
-//   if (
-//     pobiRightPage - pobiLeftPage !== 1 ||
-//     crongRightPage - crongLeftPage !== 1
-//   )
-//     return -1;
-
-//   const pobiScore = getScore(
-//     splitNumber(pobiLeftPage),
-//     splitNumber(pobiRightPage)
-//   );
-//   const crongScore = getScore(
-//     splitNumber(crongLeftPage),
-//     splitNumber(crongRightPage)
-//   );
-
-//   if (pobiScore > crongScore) return 1;
-//   if (pobiScore > crongScore) return 2;
-//   return 0;
-// }
-
-// module.exports = problem1;
-
 const isCorrectPage = ([left, right]) => {
   if (left % 2 === 0 || right % 2 === 1) return false;
   if (left < 1 || right > 400) return false;
@@ -60,5 +21,12 @@ const getScore = ([left, right]) => {
 
 function problem1(pobi, crong) {
   if (!isCorrectPage(pobi) || !isCorrectPage(crong)) return -1;
+
+  const pobiScore = getScore(splitNumber(pobi));
+  const crongScore = getScore(splitNumber(crong));
+
+  if (pobiScore > crongScore) return 1;
+  if (pobiScore < crongScore) return 2;
+  return 0;
 }
 module.exports = problem1;
