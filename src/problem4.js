565e0eed85d2f75817b27b9ddbf696462e687705
biyamn <b1y9mn@gmail.com>
2022-10-27 19:53:03 +0900

    docs: 공통의 README를 각 문제 파일의 README에 분리

--- src/problem4.js
+++ src/problem4.js
@@ -1,12 +1,26 @@
 function problem4(word) {
   let frogDictUpper = {};
   let frogDictLower = {};
+
+  // 청개구리 사전 만들기
   for (let i = 0; i < 26; i++) {
     frogDictUpper[String.fromCharCode(i + 65)] = String.fromCharCode(90 - i)
     frogDictLower[String.fromCharCode(i + 97)] = String.fromCharCode(122 - i)
   }
-
-  let answer;
+  String.prototype.replaceAt = function(index, replacement) {
+    return this.substring(0, index) + replacement + this.substring(index + replacement.length);
+}
+  for (let i = 0; i < word.length; i++) {
+    if ((97 <= word.charCodeAt(i)) && (word.charCodeAt(i) <= 122)) {
+      word = word.replaceAt(i, frogDictLower[word[i]]);
+    } else if ((65 <= word.charCodeAt(i)) && (word.charCodeAt(i) <= 90)) {
+      word = word.replaceAt(i, frogDictUpper[word[i]]);
+    } else {
+      continue;
+    }
+  }
+  
+  let answer = word
   return answer;
 }
 
