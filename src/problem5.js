2c6bb3d349d97181b75f5cda0e831c2ac6604d97
lee-il-woo <empty001430@gmail.com>
2022-10-27 17:10:17 +0900

    문제5 기능 구현(수정중)

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,23 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  answer.push(parseInt(money / 50000))
+  money = money % 50000
+  answer.push(parseInt(money / 10000))
+  money = money % 10000
+  answer.push(parseInt(money / 5000))
+  money = money % 5000
+  answer.push(parseInt(money / 1000))
+  money = money % 1000
+  answer.push(parseInt(money / 500))
+  money = money % 500
+  answer.push(parseInt(money / 100))
+  money = money % 100
+  answer.push(parseInt(money / 50))
+  money = money % 50
+  answer.push(parseInt(money / 10))
+  money = money % 10
+  answer.push(parseInt(money / 1))
+  money = money % 1
   return answer;
 }
-
 module.exports = problem5;
