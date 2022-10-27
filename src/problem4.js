b233492f1fd156f29cb061ccf27194c5bede5d5a
sangpok <48979587+sangpok@users.noreply.github.com>
2022-10-27 11:20:05 +0900

    Solve the problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
 function problem4(word) {
-  var answer;
-  return answer;
+    const [
+        ALPHABET_COUNT,
+        LOWERCASE_ALPHABET_START,
+        LOWERCASE_ALPHABET_END,
+        UPPERCASE_ALPHABET_START,
+        UPPERCASE_ALPHABET_END,
+    ] = [26, 97, 122, 65, 90];
+
+    const REVERSE_CAESAR = {};
+
+    [...Array(ALPHABET_COUNT).keys()].map((codeStep) => {
+        REVERSE_CAESAR[String.fromCharCode(UPPERCASE_ALPHABET_START + codeStep)] =
+            String.fromCharCode(UPPERCASE_ALPHABET_END - codeStep);
+        REVERSE_CAESAR[String.fromCharCode(LOWERCASE_ALPHABET_START + codeStep)] =
+            String.fromCharCode(LOWERCASE_ALPHABET_END - codeStep);
+    });
+
+    return [...word].map((letter) => REVERSE_CAESAR[letter] || letter).join('');
 }
 
 module.exports = problem4;
