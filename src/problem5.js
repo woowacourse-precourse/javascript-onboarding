39a5d7a610c7d517a12f9ae0259097d00fd847a7
ilgon0110 <moses011010@gmail.com>
2022-10-27 15:01:08 +0900

    feat: 출력값 구하기 기능 구현(#problem5)

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,9 @@
 function problem5(money) {
-  var answer;
+  //출력값 구하기
+  let coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  //기능목록 2-1 구현
+  let answer = Array.from({ length: coins.length }, () => 0);
+
   return answer;
 }
 
