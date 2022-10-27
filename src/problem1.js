20d75bd502e850017d2e46e1902e327d2caca0fb
Sunghyun627 <kksscc4@gmail.com>
2022-10-27 20:22:56 +0900

    feat: 계산된 값(인자로 주어진 값) 중 가장 큰 값을 구하는 기능 구현(getMaxScore)

--- src/problem1.js
+++ src/problem1.js
@@ -3,12 +3,14 @@
   [O] 왼쪽 페이지 번호와 오른쪽 페이지 번호가 연속된 숫자인지 확인하는 기능 | return 값 : true/false | 함수명 : isConsecutiveNumbers    
   [O] 페이지 번호의 각 자리 숫자를 모두 더하는 기능 | return 값 : 각 자리 숫자의 합 
   [O] 페이지 번호의 각 자리 숫자를 모두 곱하는 기능 | return 값 : 각 자리 숫자의 곱 | 함수명 : getProductOfDigits
-  [ ] 인자로 주어진 값 중 가장 큰 수를 구하는 기능 | return 값 : 가장 큰 수
+  [O] 계산된 값(인자로 주어진 값) 중 가장 큰 값을 구하는 기능 | return 값 : 가장 큰 수 | 함수명 : getMaxScore 
   [ ] 점수를 비교해 게임의 결과를 return 하는 기능 | return 값 : 0(무승부)/1(포비 승)/2(크롱 승) 
 */
 
 function problem1(pobi, crong) {
   if (!(isConsecutiveNumbers(pobi) && isConsecutiveNumbers(crong))) return -1;
+  let maxScoreOfPobi = getMaxScore(getSumOfDigits(pobi), getProductOfDigits(pobi));
+  let maxScoreOfCrong = getMaxScore(getSumOfDigits(crong), getProductOfDigits(crong));
 }
 
 function isConsecutiveNumbers(numbers) {
@@ -27,6 +29,10 @@ function getProductOfDigits(number) {
     .toString()
     .tosplit('')
     .reduce((accProduct, curDigit) => accProduct * curDigit, 1);
+}
+
+function getMaxScore(scores) {
+  return Math.max(scores);
 }
 
 module.exports = problem1;
