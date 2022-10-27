19f84e797059eaaf24cb1b149a41565367d7bbf6
Jiwoo Park <jiwoo9446@naver.com>
2022-10-27 19:42:02 +0900

    problem1 페이지 최대값 구하기 함수 구현 리팩토링

--- src/problem1.js
+++ src/problem1.js
@@ -4,21 +4,22 @@ function problem1(pobi, crong) {
   if (pobi[0] <= 1 || crong[0] <= 1 || pobi[1] >= 400 || crong[1] >= 400)
     return -1;
 
-  const pobiScore = pobi.map((n) => {
-    const splited = n.toString().split("");
-    const sum = splited.reduce((acc, cur) => acc + Number(cur), 0);
-    const mul = splited.reduce((acc, cur) => acc * Number(cur), 1);
-    return Math.max(sum, mul);
-  });
-  const MaxPobi = Math.max(...pobiScore);
-  const crongScore = crong.map((n) => {
-    const splited = n.toString().split("");
-    const sum = splited.reduce((acc, cur) => acc + Number(cur), 0);
-    const mul = splited.reduce((acc, cur) => acc * Number(cur), 1);
-    return Math.max(sum, mul);
-  });
+  const getMaxScore = (user) => {
+    const score = user.map((n) => {
+      const splited = n.toString().split("");
+      const sum = splited.reduce((acc, cur) => acc + Number(cur), 0);
+      const mul = splited.reduce((acc, cur) => acc * Number(cur), 1);
+      return Math.max(sum, mul);
+    });
+    return score;
+  };
 
+  const pobiScore = getMaxScore(pobi);
+  const crongScore = getMaxScore(crong);
+
+  const MaxPobi = Math.max(...pobiScore);
   const MaxCrong = Math.max(...crongScore);
+
   if (MaxPobi > MaxCrong) return 1;
   else if (MaxPobi < MaxCrong) return 2;
   else return 0;
