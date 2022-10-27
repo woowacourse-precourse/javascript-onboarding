aea8dbf8cb95e74719a13e0ee231f75930500631
wonkyung <mwk0725@gmail.com>
2022-10-27 13:08:55 +0900

    feat: [기능 0] 연속적인 중복 문자 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let text = cryptogram.split("");
+  // 중복제거
+  for (let i = 0; i < text.length; i++) {
+    if (text[i] === text[i + 1]) {
+      delete text[i];
+      delete text[i + 1];
+    }
+  }
 }
 
 module.exports = problem2;
