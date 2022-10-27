2fbd9576c528b26786314aa707d90db2ff521acf
jiyujin <yujin1679@naver.com>
2022-10-28 00:29:34 +0900

    feat(pro2): 연속하는 중복 문자 제거하는 코드 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,5 @@
 // 기능 요구 사항
-// - [] 연속하는 중복 문자들을 (연속하는 중복 문자가 없을때까지)삭제한다.
+// - [o] 연속하는 중복 문자들을 (연속하는 중복 문자가 없을때까지)삭제한다.
 // - ex1) "browoanoommnaon"
 //   - 1. "browoanoommnaon"
 //   - 2. "browoannaon"
@@ -13,8 +13,21 @@
 //   - 4. ""
 
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while(true) {
+    let isEnd = false;
+    for(let i = 1; i <= cryptogram.length; i++) {
+      if (cryptogram[i-1] === cryptogram[i]) {
+        let left = cryptogram.substring(0, i-1);
+        let right = cryptogram.substring(i+1);
+        cryptogram = left + right;
+        isEnd = true;
+      }
+    }
+    if (isEnd == false) {
+      return cryptogram;
+    }
+  }
 }
 
+// console.log(problem2("browoanoommnaon"));
 module.exports = problem2;
