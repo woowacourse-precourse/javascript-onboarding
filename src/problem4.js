0d901a4537b95b82bc6d79d1876bc51ae80151cb
Ryan-Dia <cjfdnjs158@naver.com>
2022-10-27 23:50:18 +0900

    [문제4] map내부에 기존 문자열을 청개구리 사전을 참고하여 교체하는 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
+function converFun(word) {
+  const upperCaseASCIINum = 155;
+  const lowerCaseASCIINUM = 219;
+  const classifyingASCIINum = 96;
+  if (word === " ") return " ";
+  else {
+    if (word.charCodeAt() > classifyingASCIINum) {
+      return String.fromCharCode(lowerCaseASCIINUM - word.charCodeAt());
+    } else {
+      return String.fromCharCode(upperCaseASCIINum - word.charCodeAt());
+    }
+  }
+}
+
 function problem4(word) {
-  let convertWord = word.split("");
+  let convertWord = word.split("").map((x) => converFun(x));
 }
 
 module.exports = problem4;
