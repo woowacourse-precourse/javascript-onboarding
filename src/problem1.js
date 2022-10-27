e833d6031085d6d8e3618f1153c3165b6a438ad0
sumin <csumin008255@gmail.com>
2022-10-27 15:12:34 +0900

    problem1_기능목록수정 및 기능2

--- src/problem1.js
+++ src/problem1.js
@@ -1,15 +1,26 @@
 /* 기능 목록
 * 1. pobi와 crong의 페이지 번호 각 자리 숫자를 모두 곱한 값, 더한 값을 배열 pobi_num, crong_num에 저장
-* 2. pobi_num과 crong_num에서 큰 수 찾기
-* 3. pobi와 crong의 큰 수를 비교, 결과 출력
-* 4. 코드 정리
+* 2. 예외 사항 처리
+* 3. pobi_num과 crong_num에서 큰 수 찾기
+* 4. pobi와 crong의 큰 수를 비교, 결과 출력
+* 5. 코드 정리
 */
 
+
 function problem1(pobi, crong) {
-  var answer;
   var pobi_num = [];
   var crong_num = [];
 
+  if (pobi.length !=2 || crong.length != 2){
+    return -1;
+  }else if(pobi.includes(0) || crong.includes(0)){
+    return -1;
+  }else if(pobi[0]%2 != 1 || crong[0]%2 != 1 || pobi[1]%2 != 0 || crong[1]%2 != 0){
+    return -1;
+  }else if(pobi[1]-pobi[0] != 1|| crong[1]-crong[1] != 1){
+    return -1;
+  }
+  
   for (const item of pobi){
     var arr = item.toString().split("").map(x=> parseInt(x));
     pobi_num.push(arr.reduce((x,y)=> x+y));
@@ -21,8 +32,6 @@ function problem1(pobi, crong) {
     crong_num.push(arr.reduce((x,y)=> x+y));
     crong_num.push(arr.reduce((x,y)=> x*y));
   }
-
-  return answer;
 }
 
 module.exports = problem1;
