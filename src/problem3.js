90c225cf1cbd562fc71ca1c64d6baa564a057e00
Hungeun2 <bepoohun@gmail.com>
2022-10-27 17:02:37 +0900

    Feat: problem3 Add countClap function 손뼉 횟수 계산 function

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
 // 1. 손뼉 횟수 계산 function 생성
 // 2. 1~number 까지 손뼉 횟수를 각각 구해 더함
+function countClap(num) {
+  return String(num).match(/[3,6,9]/g)?.length || 0;
+}
+
 function problem3(number) {
   var answer;
   return answer;
