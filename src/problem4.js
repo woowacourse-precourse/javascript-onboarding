0f3c9976ee80f21b76fb2c1ea5095a67cc633e68
ShimSungBo <shimdb96@gmail.com>
2022-10-27 18:53:39 +0900

    [problem4] 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,29 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let lowerCase = [];
+  let upperCase = [];
+  let answer = [];
+  for(let i = 65; i <= 90; i++) upperCase.push(String.fromCharCode(i));
+  for(let i = 97; i <= 122; i++) lowerCase.push(String.fromCharCode(i));
+
+  for(const item of word) {
+    if(item === ' ') {
+      answer.push(' ');
+    }
+    if(upperCase.includes(item)) {
+      // 해당하는 i의 index를 값을 뒤집어서 찾아서 push해준다.
+      const UPPER = upperCase.indexOf(item);
+      const reverseWord = [...upperCase].reverse();
+      answer.push(reverseWord[UPPER]);
+    }
+    if(lowerCase.includes(item)) {
+      // 해당하는 i의 index값을 뒤집어서 찾아서 push해준다.
+      const LOWER = lowerCase.indexOf(item);
+      const reverseWord = [...lowerCase].reverse();
+      answer.push(reverseWord[LOWER]);
+    }
+    
+  }
+  return answer.join('');
 }
 
 module.exports = problem4;
