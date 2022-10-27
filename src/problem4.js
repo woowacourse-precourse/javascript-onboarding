dff12a1ec79918526886f43d2e0ba97463151bb0
GuHyeonWoo <ghw9174@gmail.com>
2022-10-27 18:38:41 +0900

    P4/문자열을 순회하여 아스키 코드로 변환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,8 +3,23 @@
 // 2. 소문자를 반대로 변환하는 기능
 // 3. 대문자를 반대로 변환하는 기능
 
+// 문자열을 순회하여 아스키 코드로 변환
+function circuitWord(word){
+  // 결과 문자열을 담을 변수
+  let result_word = '';
+
+  // 문자열을 순회하여 아스키 코드로 변환
+  for(let i = 0; i < word.length; i++){
+    const char_code = word.charCodeAt(i);
+    // 아스키 코드로 어떤 문자인지 판단하는 부분
+    // 문자에 따라 반대로 변환하는 부분
+  }
+  return result_word;
+}
+
 function problem4(word) {
-  var answer;
+  // 결과값을 answer에 저장
+  const answer = circuitWord(word);
   return answer;
 }
 
