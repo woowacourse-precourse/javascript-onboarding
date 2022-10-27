f8a3a402c15e34b297e80c6410d366f358b02783
Miniling <bsm3925@naver.com>
2022-10-27 21:07:33 +0900

    [P1] Add/exception

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,15 @@
 function problem1(pobi, crong) {
   var answer;
+
+  // 예외 확인
+  const check = (left, right) => {
+    // 입력 페이지가 정상이면 1, 비정상이면 -1
+    return (left + 1) == right ? 1 : -1;
+  }
+
+  let p = check(pobi[0], pobi[1]);
+  let c = check(crong[0], crong[1]);
+
   return answer;
 }
 
