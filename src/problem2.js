d2f46110602bf500c1fc6805602b31b3a1479b96
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-27 15:04:05 +0900

    feat: [문제2] cryptogram 예외사항 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,32 +1,47 @@
 function problem2(cryptogram) {
-  if (cryptogram.length === 0) return cryptogram;
-  const decrypted = [];
-  let pointer = '';
-  let isContinious = false;
-  [...cryptogram].forEach((character) => {
-    if (!isContinious) {
-      pointer = decrypted.pop() || '';
-    }
+  const isLowerCase = (cryptogram) => {
+    const check = cryptogram.replace(/[^a-z]/, '');
+    return cryptogram === check;
+  };
 
-    if (pointer === character) {
-      console.log(pointer, character);
-      isContinious = true;
-    } else {
-      if (!isContinious) {
-        decrypted.push(pointer);
-      }
-      decrypted.push(character);
-      isContinious = false;
-    }
-  });
-
-  const decryptogram = decrypted.join('');
-
-  if (decryptogram === cryptogram) {
-    return cryptogram;
+  if (
+    !(cryptogram.length >= 1 && cryptogram.length <= 1000) ||
+    !isLowerCase(cryptogram)
+  ) {
+    throw new Error(
+      'cryptogram은 길이가 1이상이고 1000이하인 소문자 알파벳 문자열이어야한다'
+    );
   }
 
-  return problem2(decryptogram);
+  const decrypto = (cryptogram) => {
+    if (cryptogram.length === 0) return cryptogram;
+    const decrypted = [];
+    let pointer = '';
+    let isContinious = false;
+    [...cryptogram].forEach((character) => {
+      if (!isContinious) {
+        pointer = decrypted.pop() || '';
+      }
+
+      if (pointer === character) {
+        isContinious = true;
+      } else {
+        if (!isContinious) {
+          decrypted.push(pointer);
+        }
+        decrypted.push(character);
+        isContinious = false;
+      }
+    });
+    const decryptogram = decrypted.join('');
+
+    if (decryptogram === cryptogram) {
+      return cryptogram;
+    }
+    return decrypto(decryptogram);
+  };
+
+  return decrypto(cryptogram);
 }
-problem2('good'); //?
-// module.exports = problem2;
+
+module.exports = problem2;
