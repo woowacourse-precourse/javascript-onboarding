b009d1fdb9fd843bbd6aef2d00769ea6c9370274
feb-dain <feb.dain@gmail.com>
2022-10-28 04:10:46 +0900

    feat: [problem1] find pobi's max number

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,15 @@
 function problem1(pobi, crong) {
   const checkExceptions = (pobi[1] - pobi[0]) - (crong[1] - crong[0]);
+
+  const pobiLeftPageArray = String(pobi[0]).split("");
+  const pobiRightPageArray = String(pobi[1]).split("");
+
+  const pobiMax = Math.max(...
+    [pobiLeftPageArray.reduce((acc, cur) => acc * Number(cur), 1),
+    pobiLeftPageArray.reduce((acc, cur) => acc + Number(cur), 0),
+    pobiRightPageArray.reduce((acc, cur) => acc * Number(cur), 1),
+    pobiRightPageArray.reduce((acc, cur) => acc + Number(cur), 0)]
+  );
   
   return answer;
 }
