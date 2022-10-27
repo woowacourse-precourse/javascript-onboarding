84aad1e10ae87ae0e74ac4d4db19c16e6c75c7b4
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-10-27 10:54:13 +0900

    crong 점수 계산 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -22,6 +22,37 @@ function problem1(pobi, crong) {
   
   let pobiMax = (pobiSum > pobiMulti) ? pobiSum : pobiMulti;
 
+  //crong 점수 계산
+  let crongSum = 0;
+  let crongMulti = 0;
+  for (var page of crong){
+    let tmpSum = 0;
+    let tmpMulti = 1;
+    const newArray = Array.from(String(page));
+    for (var i of newArray){
+      tmpSum += Number(i);
+      crongMulti *= Number(i);
+    }
+    if(crongSum < tmpSum){
+      crongSum = tmpSum;
+    }
+    if(crongMulti < tmpMulti){
+      crongMulti = tmpMulti;
+    }
+  }
+
+  let crongMax = (crongSum > crongMulti) ? crongSum : crongMulti;
+
+  if(pobiMax == crongMax){
+    answer = 0
+  }
+  else if(pobiMax > crongMax){
+    answer = 1
+  }
+  else{
+    answer = 2
+  }
+
   return answer;
 }
 
