be0d8b88b314412c59e24a5cc9a77434cbdf9af7
saram <yujun1018@naver.com>
2022-10-28 01:44:14 +0900

    [문제2] feat: 찾은 중복 문자들을 해당 문자열에서 삭제하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function problem2(cryptogram) {
   const duplicateCharacterList = cryptogram.match(/([a-z])\1+/g);
   if (duplicateCharacterList === null) return cryptogram;
+  duplicateCharacterList.forEach((duplicateCharacter) => {
+    cryptogram = cryptogram.replace(duplicateCharacter, '');
+  });
 }
 
 module.exports = problem2;
