19de5b8cd50a72a737ac7e3a872b6c5d3fb97424
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-27 10:23:52 +0900

    feat:[problem4] Add reverseLetter

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,44 @@
-function problem4(word) {
-  var answer;
-  return answer;
+function problem4(word) {}
+
+function reverseLetter(letter) {
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+
+  for (let i = 0; i < 26; i++) {
+    if (letter.toUpperCase() == alphabet[i]) {
+      if (letter.toUpperCase() == letter) {
+        return alphabet.reverse()[i];
+      } else {
+        return alphabet.reverse()[i].toLowerCase();
+      }
+    }
+  }
 }
 
 module.exports = problem4;
