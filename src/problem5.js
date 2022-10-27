e552c4318df0ab626934ac6a96835317d7131919
evencoding <evencoding@gmail.com>
2022-10-27 18:26:38 +0900

    [문제5] implement: 화폐 개수의 합이 최소가 되도록 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
+const KIND_OF_MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+  const result = new Array(KIND_OF_MONEY.length).fill(0);
+  for (let i = 0; i < KIND_OF_MONEY.length; i++) {
+    result[i] = Math.floor(money / KIND_OF_MONEY[i]);
+    money %= KIND_OF_MONEY[i];
+    if (money <= 0) {
+      break;
+    }
+  }
+  return result;
 }
 
 module.exports = problem5;
