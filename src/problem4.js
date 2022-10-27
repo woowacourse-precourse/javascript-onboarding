93ee9fca43683aa7e29cfeaa2c2064bf9dfb5637
moonelysian <dandyblack@naver.com>
2022-10-27 09:44:35 +0900

    add: 알파벳 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,6 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
 }
 
 module.exports = problem4;
