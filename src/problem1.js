f3401cb7ae6a62c88f5006496be932710700608f
TaeeunKim <xodms0309@naver.com>
2022-10-27 18:22:18 +0900

    [1번] feat: 각 페이지를 split하는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,13 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const splitPages = (book) => {
+    return book.map((pages) => pages.toString().split('').map(Number));
+  };
+
+  const pobiBook = splitPages(pobi);
+  const crongBook = splitPages(crong);
+
   return answer;
 }
 
