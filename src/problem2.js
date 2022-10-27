8a29c6779dc41a4e7f78d8c85a4a9c2e8148f197
oing <74910908+oing-yong@users.noreply.github.com>
2022-10-27 12:31:33 +0900

    feat: 문제2 입력받은 단어에 연속되는 문자가 있는지 확인
    - 연속되는 문자가 있을 경우 tracker 변수 값이 1씩 증가함

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
 function problem2(cryptogram) {
   var answer;
+  let tracker = 0;
+
+  for(let i = 0; i < length(cryptogram); i++){
+    if(cryptogram[i] === cryptogram[i+1]){
+      tracker = tracker + 1;
+      continue;
+    }
+  }
   return answer;
 }
 
