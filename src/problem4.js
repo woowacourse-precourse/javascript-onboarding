471d3c02ae77c817ede5368edb3078fcbc966538
minseok1109 <park485201@naver.com>
2022-10-27 17:33:32 +0900

    feat: 대문자인지 소문자인지 판별하는 함수 #기능목록 - 1

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,9 @@
+//대문자인지 체크하는 함수
+const isUpperCase = (string) => /^[A-Z]*$/.test(string);
+
+//소문자인지 체크하는 함수
+const isLowerCase = (string) => /^[a-z]*$/.test(string);
+
 function problem4(word) {
   var answer;
   return answer;
