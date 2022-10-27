15182890da94a65c220a465d597c8d998130a546
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-27 21:11:07 +0900

    문제3. 일의자리 숫자에 3,6,9가 있으면 박수+1

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,9 @@ function problem3(number) {
   var answer = 0;
   for (let i = 1; i <= number; i++) {
     let num = i;
+    while (num >= 1) {
+      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) answer++;
+    }
   }
   return answer;
 }
