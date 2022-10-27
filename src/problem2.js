0b5d425daec18f51c584cb31c640333bdea52b8a
easyone <thinkanddoit@naver.com>
2022-10-27 17:59:05 +0900

    #2 refactor: 함수 추출 및 변수명 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,19 +1,19 @@
 function problem2(cryptogram) {
-  const deleteDuplication = (string) => {
-    const stringToArr = string.split("");
-
-    let i = 0;
-    while (i < stringToArr.length) {
-      if (stringToArr[i] === stringToArr[i + 1]) {
-        stringToArr.splice(i, 2);
-        i = 0;
-      } else i++;
-    }
-
-    return stringToArr.join("");
-  };
-
-  return deleteDuplication(cryptogram);
+  return deleteDuplications(cryptogram);
 }
+
+const deleteDuplications = (string) => {
+  const chars = string.split("");
+
+  let i = 0;
+  while (i < chars.length) {
+    if (chars[i] === chars[i + 1]) {
+      chars.splice(i, 2);
+      i = 0;
+    } else i++;
+  }
+
+  return chars.join("");
+};
 
 module.exports = problem2;
