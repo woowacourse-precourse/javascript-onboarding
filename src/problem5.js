529e4506afaf9cab4e120932ba5117129ad4e1ee
Miniling <bsm3925@naver.com>
2022-10-27 23:00:22 +0900

    [P5] passed

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  // 나누기한 값을 정수화하여 단위화폐의 개수 계산
+  for (let i = 0; i < units.length; i++) {
+    answer.push(parseInt(money / units[i]));
+    money = money % units[i];
+  }
+
   return answer;
 }
 
