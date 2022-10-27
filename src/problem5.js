d13b0a92c9fa68b43a4d273c3e640add58096b08
Hwang Seong Taek <hst007@naver.com>
2022-10-27 20:53:20 +0900

    docs: problem5 구현목록 정리

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,11 @@
+/*
+구현목록
+1. 결과가 담길 배열과 현금의 종류가 저장된 배열 선언하기
+2. 금액이 큰 현금 순서로 money가 1 이상으로 나눠질 경우 해당 현금의 몫을 wallet 배열에 저장 후, 남은 금액을 money에 저장하여 초기화
+3. 몫이 1 미만일 경우 해당 현금으로 교환할 수 없으니 wallet 배열에 0을 저장하기
+4. 교환된 현금의 개수가 저장된 wallet 배열을 return 하기
+*/
+
 function problem5(money) {
   var answer;
   return answer;
