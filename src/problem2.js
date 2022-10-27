f19a914311ddd09040c8eb195615c368c65f9dc6
Subin Jo <sbjo1202@naver.com>
2022-10-27 15:47:42 +0900

    feat: 기능 1(연속하는 중복 문자열 체크) 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,56 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  let isRepeat; // 연속하는 중복 문자 유무
+
+  do {
+    isRepeat = false;
+  
+    console.log(isRepeat);
+
+    // 함수 1
+    cryptogram = checkRepeat(cryptogram);
+    console.log(isRepeat);
+    
+  } while (isRepeat)
+
+  answer = cryptogram;
+  console.log(answer);
   return answer;
 }
 
-module.exports = problem2;
+// 함수 1: 연속하는 중복 문자열 체크
+function checkRepeat(cryptogram, isRepeat) {
+  let yet = -1; // 임시 저장을 위한 변수
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    // 연속하는 중복 문자열 발견 시,
+    if (cryptogram[i] == cryptogram[i + 1]) {      
+      isRepeat = true;
+      console.log(isRepeat);
+      
+      // "앞"의 문자를 삭제
+      cryptogram = cryptogram.slice(0, i) + cryptogram.slice(i + 1);
+
+      // "뒤"의 문자를 yet 변수에 임시 저장 => 3개 이상 연속하는 중복 문자열에 대한 처리 위함
+      yet = i;
+      i -= 1;
+
+      continue
+    }
+
+    // "중복 되었던 "뒤" 문자와 그 다음 문자가 같지 않은 경우
+    if (yet != -1) {
+      // "뒤" 문자도 삭제
+      cryptogram = cryptogram.slice(0, yet) + cryptogram.slice(yet + 1);
+      yet = -1;
+      i -= 1;
+    }
+  }
+
+  return cryptogram;
+}
+
+
+problem2("browoanoommnaon");
+
+// module.exports = problem2;
