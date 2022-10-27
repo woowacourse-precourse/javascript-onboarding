988f8fb9806a9abad5262c80f5b4e747f3d86737
sying012 <syi369@naver.com>
2022-10-28 00:08:24 +0900

    problem1.js - 제한사항을 검사하는 violationChk 함수 작성

--- src/problem1.js
+++ src/problem1.js
@@ -6,6 +6,7 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+// 게임 결과를 리턴하는 함수
 function winner(player1, player2) {
   var result;
 
@@ -31,6 +32,22 @@ function winner(player1, player2) {
   }
 
   return result;
+}
+
+// 제한사항 위반을 검사하는 함수
+function violationChk(player1, player2) {
+  // 각 배열의 크기가 2인지 확인
+  var lenChk1 = player1.length == 2 ? true : false;
+  var lenChk2 = player2.length == 2 ? true : false;
+
+  // 왼쪽 페이지 번호가 홀수이고, 페이지 번호가 순서대로 들어있는지 확인
+  var pageChk1 =
+    player1[0] % 2 == 1 && player1[0] + 1 == player1[1] ? true : false;
+  var pageChk2 =
+    player2[0] % 2 == 1 && player2[0] + 1 == player2[1] ? true : false;
+
+  // 모든 결과가 true 일 때만 true 리턴
+  return lenChk1 && lenChk2 && pageChk1 && pageChk2;
 }
 
 module.exports = problem1;
