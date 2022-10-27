80fe598e5b9ab58ea2c7a8377946fca97a76d8a9
leeuihyun <meta212121@naver.com>
2022-10-28 01:36:50 +0900

    feat : problem2 중복 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,14 @@
+function deduplication(cryptogram) {
+  let array = [];
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (array[array.length - 1] === cryptogram[i]) {
+      array.pop();
+    } else array.push(cryptogram[i]);
+  }
+  return array;
+}
 function problem2(cryptogram) {
-  var answer;
+  var answer = deduplication(cryptogram);
   return answer;
 }
 
