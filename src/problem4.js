ec891542155d03a43293ba22832cbb17ce2bb27e
saram <yujun1018@naver.com>
2022-10-28 02:43:08 +0900

    [문제4] feat: UTF-16 코드를 이용해 알파벳을 청개구리 사전에 맞게 변환하여 반환하는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -3,13 +3,18 @@ function problem4(word) {
   const CHAR_CODE_Z = 90;
   const CHAR_CODE_a = 97;
   const CHAR_CODE_z = 122;
-
   const answer = [...word].map((v) => {
     if (v.charCodeAt() >= CHAR_CODE_A && v.charCodeAt() <= CHAR_CODE_Z) {
+      charCode = CHAR_CODE_A + CHAR_CODE_Z - v.charCodeAt();
+      return String.fromCharCode(charCode);
     }
     if (v.charCodeAt() >= CHAR_CODE_a && v.charCodeAt() <= CHAR_CODE_z) {
+      charCode = CHAR_CODE_a + CHAR_CODE_z - v.charCodeAt();
+      return String.fromCharCode(charCode);
     }
+    return v;
   });
+  return answer.join('');
 }
 
 module.exports = problem4;
