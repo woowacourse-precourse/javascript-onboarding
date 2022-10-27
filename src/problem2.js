b30fa858865ee60bea6bb5b4cbe4a4dfd0b66457
배병재 <39183208+BaeByungjae@users.noreply.github.com>
2022-10-28 02:07:22 +0900

    문제2 스택 이용해 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let target = cryptogram;
+  while (true) {
+    stack = [target[0]];
+    for (let i = 1; i < target.length; i++) {
+      const top = stack[stack.length - 1];
+      if (top !== target[i]) {
+        stack.push(target[i]);
+      } else {
+        stack.pop();
+      }
+    }
+    const newStr = stack.join("");
+    if (target == newStr) {
+      return target;
+    }
+    target = newStr;
+  }
 }
 
 module.exports = problem2;
