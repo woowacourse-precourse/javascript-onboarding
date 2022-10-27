f5a8d52e8ea3f44c30ba38503c801c50a7d995cf
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-10-27 12:48:55 +0900

    Feat: multiply 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -12,3 +12,8 @@ function sum(n) {
   let sumArr = (n+"").split("").map(x => parseInt(x))
   return sumArr.reduce((acc, cur) => acc + cur, 0)
 }
+
+function multiply(n) {
+  let multiplyArr = (n+"").split("").map(x => parseInt(x))
+  return multiplyArr.reduce((acc, cur) => acc * cur)
+}
