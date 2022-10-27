72c997588ae6bedec046644604f152ea7fe9bc04
unknown <chn0202@gmail.com>
2022-10-27 18:32:28 +0900

    Feat: problem5 기능 개발
    각 화폐단위가 몇번 쓰였는지

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const monetaryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+  let remainingAmount = money;
+  for (let won of monetaryUnit) {
+    answer.push(Math.floor(remainingAmount / won));
+    remainingAmount = remainingAmount % won;
+  }
+
   return answer;
 }
 
