efb30a4394174c1821c05cc0a318b94ec85e1801
hwangsungjae <edx2fire@likelion.org>
2022-10-27 17:18:18 +0900

    [온보딩 - 01] 1번 문제 불필요한 변수 제거 리팩토링

--- src/problem1.js
+++ src/problem1.js
@@ -2,15 +2,14 @@ function problem1(pobi, crong) {
   if (pobi[0] - pobi[1] !== -1 || crong[0] - crong[1] !== -1) return -1;
 
   let answer;
-  let max = 0;
   let pobiMax = 0;
   let crongMax = 0;
 
-  const getSum = num => Math.max(max, [...num + ''].map(el => +el).reduce((prev, curr) => prev + curr, 0));
-  const getMultiply = num => Math.max(max, [...num + ''].map(el => +el).reduce((prev, curr) => prev * curr, 1));
+  const getSum = num => [...num + ''].map(el => +el).reduce((prev, curr) => prev + curr, 0);
+  const getMultiply = num => [...num + ''].map(el => +el).reduce((prev, curr) => prev * curr, 1);
 
-  pobi.forEach(num => pobiMax = Math.max(getSum(num), getMultiply(num)));
-  crong.forEach(num => crongMax = Math.max(getSum(num), getMultiply(num)));
+  pobi.forEach(num => pobiMax = Math.max(pobiMax, getSum(num), getMultiply(num)));
+  crong.forEach(num => crongMax = Math.max(crongMax, getSum(num), getMultiply(num)));
 
   if (pobiMax === crongMax) answer = 0;
   else answer = pobiMax > crongMax ? 1 : 2;
