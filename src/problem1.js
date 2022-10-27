ff4a0f70ec94286d81a186efaa15e7967c8fceb8
Yoontaeyeon <okko8522@gmail.com>
2022-10-27 16:53:01 +0900

    Feat(problem1.js): 결과 처리 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,16 @@
 function problem1(pobi, crong) {
   var answer;
+
+  // 결과 처리
+  if (pobiPoint[0] > crongPoint[0]) {
+    answer = 1;
+  } else if (pobiPoint[0] < crongPoint[0]) {
+    answer = 2;
+  } else if (pobiPoint[0] === crongPoint[0]) {
+    answer = 0;
+  } else {
+    answer = -1;
+  }
   return answer;
 }
 
