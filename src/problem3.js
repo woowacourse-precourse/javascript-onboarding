279aba48de18478cda62acd83f282f3b5cafa28c
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-10-27 15:30:36 +0900

    손뼉 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    var answer = 0;
+    for (let i = 1; i <= number; i++) {
+        let arr = String(i).split("");
+        for (let j = 0; j < arr.length; j++) {
+            if (arr[j] === "3" || arr[j] === "6" || arr[j] === "9") {
+                answer++;
+            }
+        }
+    }
+    return answer;
 }
 
 module.exports = problem3;
