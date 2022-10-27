57b34bbd1b3b2f31d864c194be912985f742d842
hwnim5324 <hwnim5324@naver.com>
2022-10-27 14:23:47 +0900

    5번 기능 목록 작성.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
   var answer;
   return answer;
+  //1. 크기9, 0으로 이뤄진 배열 생성.
+  //2. money를 5만원, 1만원... 순서대로 나누어 값을 배열에 순서대로 저장, 나머지에 이어 계산.
+  //3. 배열 반환.
 }
 
 module.exports = problem5;
