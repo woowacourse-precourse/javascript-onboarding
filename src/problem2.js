eb9a9bcaed97a125ab14ff97cea533f328db4163
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-10-27 15:03:39 +0900

    Submit : problem 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,29 @@
 function problem2(cryptogram) {
   var answer;
+
   console.log('# this is problem 2')
 
+  let pastCryptogram = ''
+  let newCryptogram = ''
+  pastCryptogram = cryptogram
+  while ( cryptogram.length > 0 ) {
+    for ( let i = 0; i < (cryptogram.length-1); i++ ) {
+      if (cryptogram[i] === cryptogram[i+1]) {
+        removeWord = cryptogram[i] + cryptogram[i+1]
+        cryptogram = cryptogram.replace(removeWord, '')
+        newCryptogram = cryptogram
+        answer = cryptogram
+      }
+    }
+    if ( pastCryptogram === newCryptogram ) {
+      answer = cryptogram
+      break
+    } else {
+      pastCryptogram = cryptogram
+    }
+  }
+
+  answer = cryptogram
   return answer;
 }
 
