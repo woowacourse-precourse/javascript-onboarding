28ca624874ae18705922413a7b13ba10389234e4
JH <vvaa277@gmail.com>
2022-10-27 11:12:49 +0900

    Feat : 요소 중복 비교

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,12 @@
 function problem2(cryptogram) {
-    let splited = cryptogram.split("");
-    let noDup = [];
-    
+  let splited = cryptogram.split("");
+  let noDup = [];
+
+  for (let el of splited ) {
+      (el === noDup[noDup.length-1]) ? noDup.pop() : noDup.push(el)
+    } 
+
+  
 }
 
 module.exports = problem2;
