34a085a6cec9cb9f41511fe286c6b2f9ecb55348
hxezin <leehxejin@gmail.com>
2022-10-27 16:54:57 +0900

    feat: 문제1 예외사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,20 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const checkException = (pages) => {
+    if (pages[0] < 1 || pages[0] > 400 || pages[1] < 1 || pages[1] > 400) {
+      return true;
+    }
+    if (Math.abs(pages[0] - pages[1]) !== 1) {
+      return true;
+    }
+    return false;
+  };
+
+  if (checkException(pobi) || checkException(crong)) {
+    return -1;
+  }
+
   return answer;
 }
 
