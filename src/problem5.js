53cb009baa3a64efe02a893c4c564934e164abbb
turtle601 <turtle601@naver.com>
2022-10-27 14:57:49 +0900

    Feat: [problem5] 제한사항 로직인 ErrorCase 클래스 구현
    <ErrorCase클래스>
    - checkLimit() : 주어진 돈의 액수가 1이상 1000000이하인지 확인

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,25 @@
-function problem5(money) {
-  var answer;
-  return answer;
+class ErrorCase {
+  constructor(money) {
+    this.money = money;
+    this.checkLimit();
+  }
+
+  checkLimit() {
+    if (!(1 <= this.money && this.money <= 1000000)) {
+      throw new Error("input 양식에 에러가 발생했습니다!!");
+    }
+  }
 }
+
+function problem5(money) {
+  new ErrorCase(money);
+}
+
+/*
+  에러 케이스 테스트 예제
+  
+  problem5(0);
+  problem5(1000001);
+*/
 
 module.exports = problem5;
