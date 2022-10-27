1bf27c3b60e2d4ca14ed6a837275a4470059fa88
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-27 12:13:40 +0900

    feat: problem3(number 타입과 범위 제한 구현)

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
 function problem3(number) {
-  var answer;
+  let answer;
+  const MINIMUM_NUMBER = 1;
+  const MAXIMAL_NUMBER = 10000;
+
+  // - [x] number는 1 이상 10,000 이하인 자연수이다.
+  const isAllowNumberRange = (number, min, max) => {
+    if (!typeof number === 'number') {
+      return false;
+    }
+    return number >= min && number <= max;
+  };
+
+  if (!isAllowNumberRange(number, MINIMUM_NUMBER, MAXIMAL_NUMBER)) {
+    throw new Error('number는 1 이상 10,000 이하인 자연수이다.');
+  }
+
+  // - [ ] 3, 6, 9 숫자 세기
+
   return answer;
 }
 
