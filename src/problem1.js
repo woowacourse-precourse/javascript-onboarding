152819dcb467d599dec7c8504788639924a9003e
hae-on <solbi2004@naver.com>
2022-10-27 10:36:23 +0900

    [문제1] 출력 예시 값 입력

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,8 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+console.log(problem1([97, 98], [197, 198]));
+console.log(problem1([131, 132], [211, 212]));
+console.log(problem1([99, 102], [211, 212]));
+
 module.exports = problem1;
