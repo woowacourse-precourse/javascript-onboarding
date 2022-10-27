5b5634eb1d992e37185ca8b2cdb6c486fdd1eacf
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-27 18:55:40 +0900

    feat: problem4 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
+const { checkInputError, transAlpha, checkAlpha } = require("./problem4.util");
+
 function problem4(word) {
-  var answer;
-  return answer;
+  if (checkInputError(word)) return "error";
+  let words = "";
+  for (const str of word) {
+    let temp = checkAlpha(str) ? transAlpha(str) : " ";
+    words += temp;
+  }
+  return words;
 }
 
 module.exports = problem4;
