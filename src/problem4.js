e0ac21287b366cb9860fa450e29fe9447b6f51ad
YoommY <seeun0792@gmail.com>
2022-10-27 20:49:13 +0900

    Feat : 4번 문제

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  var answer = [];
+
+  //97~122(a-z) 아스키 코드
+  for (var i = 0; i < word.length; i++) {
+    {
+      if (word[i] !== ' ') {
+        //무조건 소문자
+        var words = String.fromCharCode(219 - word.toLowerCase().charCodeAt(i));
+
+        word[i] === word[i].toUpperCase() //만약 대문자라면
+          ? answer.push(words.toUpperCase())
+          : answer.push(words);
+      } else {
+        answer.push(' ');
+      }
+    }
+  }
+  return answer.join('');
 }
 
 module.exports = problem4;
