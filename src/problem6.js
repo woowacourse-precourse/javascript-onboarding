3a523ae38c58a0bf40a42d6cecb015e96f4915a9
BB-choi <choibobae0809@gmail.com>
2022-10-27 17:51:49 +0900

    feat: 문제6 - 연속 글자 추출하기

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,21 @@
+function extractWord({ nickname, characterLength = 2 }) {
+  const wordSet = new Set();
+
+  for (let i = 0; i <= nickname.length - characterLength; i++) {
+    const word = nickname.slice(i, characterLength + i);
+
+    wordSet.add(word);
+  }
+
+  return wordSet;
+}
+
 function problem6(forms) {
   var answer;
+
+  /* {"단어" => {count: 0, user : [[email, 닉네임], [email,닉네임]]}} */
+  const nicknameWordSet = new Map();
+
   return answer;
 }
 
