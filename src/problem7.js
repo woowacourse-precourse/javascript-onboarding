2ffa0f7dc7990b2992c8a0851ac95b2df6ac85da
Adult96 <djxor007@gmail.com>
2022-10-27 12:11:51 +0900

    docs: 기능 목록 작성

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,9 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+function problem7(user, friends, visitors) {
+  var answer;
+  return answer;
+}
+
+module.exports = problem7;
