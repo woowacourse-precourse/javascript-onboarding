31956aa54d016803b0e727895f6a50616546da2f
jihun1233 <leejihun1233@gmail.com>
2022-10-27 19:45:46 +0900

    문자 코드로 대소문자 구분함분하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
   var answer;
+
   return answer;
+}
+
+function findCase = (code) => {
+  if(65 <= code && code <= 90)return 'upperCase'
+  if(97 <= code && code <= 122)
+  return 'notAlphabet'
 }
 
 module.exports = problem4;
