56983926b7a15d8f601ffa09e05f270ca2132938
feb-dain <feb.dain@gmail.com>
2022-10-28 05:37:48 +0900

    feat: [problem3] add answer

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,8 @@
 function problem3(number) {
-  const makeArray = String([...Array(number+1).keys()]).split("");
+  const makeArray = String([...Array(number + 1).keys()]).split("");
   const findThreeSixNine = makeArray.filter(num => ["3", "6", "9"].includes(num));
-  
+  const answer = findThreeSixNine.length;
+
   return answer;
 }
 
