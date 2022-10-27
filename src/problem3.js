8e0caa516124cbaff9552fed4d41bdd7220d87ec
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-10-27 22:22:24 +0900

    Feat: 3번째 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,16 @@
 function problem3(number) {
-  let answer;
+  let answer = 0;
 
-  const arr = Array(number).fill().map((v, i) => i + 1);
-
+  const arr = Array(number).fill().map((v, i) => i + 1).join("").split("").map(v => +v)
+  for (let value of arr) {
+    if (value === 3 || value === 6 || value === 9) {
+      answer++;
+    }
+  }
+  
 
   return answer;
 }
 
 module.exports = problem3;
+
