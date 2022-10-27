34151a8cac0955c87533d009bd9c3531fd5a9cbf
cys4585 <choizerowater@gmail.com>
2022-10-27 13:42:19 +0900

    feat: 문제4 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
-  var answer;
+  let answer;
+
+  answer = "";
+  for (let i = 0; i < word.length; i++) {
+    const character = word[i]
+    if ('A' <= character && character <= 'Z') {
+      answer += String.fromCharCode(65 + 90 - character.charCodeAt());
+    } else if ('a' <= character && character <= 'z') {
+      answer += String.fromCharCode(97 + 122 - character.charCodeAt());
+    } else {
+      answer += character;
+    }
+  }
   return answer;
 }
 
