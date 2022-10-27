234356edb7e40fecbbe0cc7d1f54da7e5c609ab6
Miniling <bsm3925@naver.com>
2022-10-27 21:09:37 +0900

    [P1] Add/calculatePage

--- src/problem1.js
+++ src/problem1.js
@@ -7,8 +7,33 @@ function problem1(pobi, crong) {
     return (left + 1) == right ? 1 : -1;
   }
 
+  // 왼쪽 페이지 계산
+  const calLeft = (num) => {
+    let sumLeft = sum(num);
+    let productLeft = product(num);
+
+    return score(sumLeft, productLeft);
+  }
+
+  // 오른쪽 페이지 계산
+  const calRight = (num) => {
+    let sumRight = sum(num);
+    let productRight = product(num);
+
+    return score(sumRight, productRight);
+  }
+
   let p = check(pobi[0], pobi[1]);
   let c = check(crong[0], crong[1]);
+
+  // 잘못된 페이지 확인
+  if ((p === -1) || (c === -1)) {
+    answer = -1;
+  } else {
+    const p_score = score(calLeft(pobi[0]), calRight(pobi[1]));
+    const c_score = score(calLeft(crong[0]), calRight(crong[1]));
+    answer = gameResult(p_score, c_score);
+  }
 
   return answer;
 }
