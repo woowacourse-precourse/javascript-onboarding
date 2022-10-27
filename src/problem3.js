7f371d9f9693e82916e553697ede28dcfa1bf68c
이예나 <yenalee@MacBook-Pro.local>
2022-10-27 22:14:37 +0900

    Count if string contains '369'

--- src/problem3.js
+++ src/problem3.js
@@ -3,12 +3,12 @@ function problem3(number) {
 
   for(let i=1;i<=number;i++){
     var num = String(i);
-    
+    for(let j=0;j<num.length;j++){
+       if(num[j]=='3' || num[j]=='6' || num[j]=='9' ){
+         answer += 1;
+      }
+    }
   }
-  console.log(answer);
   return answer;
 }
-
 module.exports = problem3;
-number = 33;
-problem3(number);
