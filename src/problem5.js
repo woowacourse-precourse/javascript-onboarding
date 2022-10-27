f770b37763dac66a720f9e32d83932834e09b8b9
An Sol <apdlvmf486@gmail.com>
2022-10-27 14:33:15 +0900

    5번: 화폐단위로 나누기 구현 및 상수와 변수 위치 정렬

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,17 @@
 function problem5(money) {
-  let tempMoney = money;
-  let result = [];
   const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  let tempMoney = money;
+  let result = [];
+
+  for (let idx = 0; idx < currency.length; idx++){
+    if (tempMoney < currency[idx]){
+      result.push(0);
+      continue;
+    }
+    result.push(parseInt(tempMoney / currency[idx]));
+    tempMoney = tempMoney % currency[idx];
+  }
   return result;
 }
 
