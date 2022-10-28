d2b8e521f877dfce675f11a953a1eae4e0621d7f
coolIlive <galilee155@naver.com>
2022-10-29 00:17:52 +0900

    FEAT(problem3): 정규표현식을 통해 369의 개수를 센다
    각 숫자를 문자열로 바꾸고, 그 문자열에 있는 369의 개수를 정규표현식을 활용하여 세서 answer의 값에 더해준다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,12 @@
 function problem3(number) {
-  var answer;
+  let answer=0;
+  // 369의 유무 확인하는 정규표현식 패턴
+  const pattern=/[369]/g
+
+  // 각 수를 문자열로 바꿔 369가 있는 개수 만큼 answer를 더해준다.
+  for (let i=1;i<=number;i++) {
+    answer+=(i.toString().match(/[369]/g)||[]).length
+  }
   return answer;
 }
 
