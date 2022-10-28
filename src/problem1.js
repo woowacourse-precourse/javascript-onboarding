465a92666ab8f62946a0e220ca429764347c74ce
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 11:37:20 +0900

    feat: 입력값 검사 함수 추가
    전달된 인자의 길이 검사기능 추가
    홀수,짝수 순서 검사
    1페이지에서 400페이지 사이의 값인지 검사

--- src/problem1.js
+++ src/problem1.js
@@ -5,7 +5,46 @@
 // 4. 자리수 별로 모두 더하고 모두 곱한뒤 가장 큰 값 반환
 // 5. 포비, 크롱 점수 비교 후 승자 반환
 
+function validPageCheck(value) {
+  if (value[1] - value[0] !== 1) {
+    return -1;
+  }
+  if (value[0] % 2 === 0) {
+    return -1;
+  }
+  if (value[0] <= 1 || value[0] >= 399) {
+    return -1;
+  }
+  if (value[1] <= 2 || value[1] >= 400) {
+    return -1;
+  }
+  return 0;
+}
+
+function validPageInputCheck(firstValue, secondValue) {
+  if (firstValue.length !== 2 || secondValue.length !== 2) {
+    return -1;
+  }
+  return 0;
+}
+
+function validTest(firstValue, secondValue) {
+  if (validPageInputCheck(firstValue, secondValue) < 0) {
+    return -1;
+  }
+  if (validPageCheck(firstValue) < 0) {
+    return -1;
+  }
+  if (validPageCheck(secondValue) < 0) {
+    return -1;
+  }
+  return 0;
+}
+
 function problem1(pobi, crong) {
+  if (validTest(pobi, crong) < 0) {
+    return -1;
+  }
   var answer;
   return answer;
 }
