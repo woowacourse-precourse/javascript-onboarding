31f1913440f54149ee885adbfe3fb65b8ae73ea7
bae-sh <hyeon1293@gmail.com>
2022-10-28 23:53:44 +0900

    feat : problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  const strArr = word.split('');
+  let answer = '';
+  strArr.forEach(alpa => {
+    const code = alpa.charCodeAt(0);
+    if (65 <= code && code <= 90) {
+      const revChar = String.fromCharCode(155 - code);
+      answer += revChar;
+    } else if (97 <= code && code <= 122) {
+      const revChar = String.fromCharCode(219 - code);
+      answer += revChar;
+    } else answer += alpa;
+  });
   return answer;
 }
 
