4014c0c6b44864e645db23110527d0686514d8be
Deemou <4106243@naver.com>
2022-10-28 22:50:14 +0900

    refactor<P5>: countCurrency 함수 내 변수 타입 변환, while문 제거

--- src/problem5.js
+++ src/problem5.js
@@ -4,15 +4,12 @@ function problem5(money) {
 }
 const countCurrency = (money) => {
   const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let currencyCount = [];
-  while (money) {
-    currency.forEach((item) => {
-      let count = 0;
-      count += Math.floor(money / item);
-      money %= item;
-      currencyCount.push(count);
-    });
-  }
+  const currencyCount = [];
+  currency.forEach((item) => {
+    const count = Math.floor(money / item);
+    currencyCount.push(count);
+    money %= item;
+  });
   return currencyCount;
 };
 
