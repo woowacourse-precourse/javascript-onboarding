abd3c564bd28f11da05fd73e8e3bef1cbd759d7e
Deemou <4106243@naver.com>
2022-10-28 19:14:14 +0900

    feat<P3>: 주어진 숫자까지 3,6,9 개수 총합 카운트 함수 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,21 @@
 function problem3(number) {
-  let answer;
-
+  const answer = countTotalThreeSixNine(number);
   return answer;
 }
-
 const countThreeSixNine = (number) => {
   let numArr = number.toString().split("");
   let count = 0;
   numArr.forEach((n) => {
-    if (n % 3 == 0) count++;
+    if (n % 3 == 0 && n != 0) count++;
   });
   return count;
+};
+const countTotalThreeSixNine = (number) => {
+  let totalCount = 0;
+  for (let i = 1; i <= number; i++) {
+    totalCount += countThreeSixNine(i);
+  }
+  return totalCount;
 };
 
 module.exports = problem3;
