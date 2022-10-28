676b8a5c9d5b7e207eb03cf1bf2d829829384241
chee9835 <huisu121@gmail.com>
2022-10-28 21:45:31 +0900

    feat: 숫자의 각 자리수 이용해 비교하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,45 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  var [pLeft, pRight] = pobi;
+  var [cLeft, cRight] = crong;
+  if (pRight - pLeft !== 1 || cRight - cLeft !== 1) return -1;
+
+  function sliceNum(num) {
+    var numArr = [];
+    do {
+      var q = parseInt(num % 10);
+      numArr.push(q);
+      num = num / 10;
+    } while (num > 1);
+    return numArr;
+  }
+  pLeft = sliceNum(pLeft);
+  pRight = sliceNum(pRight);
+  cLeft = sliceNum(cLeft);
+  cRight = sliceNum(cRight);
+
+  function sumArr(arr) {
+    return arr.reduce((a, b) => a + b);
+  }
+  function mulArr(arr) {
+    return arr.reduce((a, b) => a * b);
+  }
+
+  var pobiMax = Math.max(
+    sumArr(pLeft),
+    sumArr(pRight),
+    mulArr(pLeft),
+    mulArr(pRight)
+  );
+  var crongMax = Math.max(
+    sumArr(cLeft),
+    sumArr(cRight),
+    mulArr(cLeft),
+    mulArr(cRight)
+  );
+
+  if (pobiMax === crongMax) return 0;
+  else if (pobiMax > crongMax) return 1;
+  else return 2;
 }
 
 module.exports = problem1;
