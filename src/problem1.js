8ded6f30fa673d3652903b90b4d5e539a68d54bd
unsnruu <unsnruu@gmail.com>
2022-10-28 13:05:14 +0900

    💡 만들어야 하는 기능을 명시한 주석 추가 (problem1)
    만들어야 하는 기능을 명시하는 주석을 추가하였습니다.
    몇몇 기능의 경우 재사용이 가능할 것으로 생각됩니다.
    이후 필요에 따라 주석을 추가하도록 하겠습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,11 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+/**
+ * 기능 정리
+ * 1. 페이지 번호의 각 자리 숫자를 모두 더하는 함수
+ * 2. 페이지 번호의 각 자리 숫자를 모두 더하는 함수
+ * 3. 어떤 숫자가 더 큰지 비교하는 함수
+ */
+
 module.exports = problem1;
