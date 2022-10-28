c475106bea9049be00f5377d4c02b445e23c9a49
d0422 <rlfehd2013@naver.com>
2022-10-28 12:17:04 +0900

    Feat:승자를 출력하는 메서드 추가 body : 각각의 경우에 대해 맞는 케이스 출력, 혹시나 예상치 못한 경우, -1을 리턴하도록 작성

--- src/problem1.js
+++ src/problem1.js
@@ -25,4 +25,12 @@ function compareNumber(num1, num2) {
     return num2;
   }
 }
+function getWinner(pobi, crong) {
+  const pobiMax = compareNumber(sumDigits(pobi[0], multiplyDigits(pobi[1])));
+  const crongMax = compareNumber(sumDigits(crong[0], multiplyDigits(crong[1])));
+  if (pobiMax == crongMax) return 0;
+  else if (pobiMax > crongMax) return 1;
+  else if (pobiMax < crongMax) return 2;
+  else return -1;
+}
 module.exports = problem1;
