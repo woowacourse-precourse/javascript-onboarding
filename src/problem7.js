4668a1b95d05aa7b2a2bc4312d86905fcc30b318
pinkishincoloragain <smb1103@gmail.com>
2022-10-28 23:04:43 +0900

    feat: solved problem7

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,43 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+function problem7(target, friends, targetVisitors) {
+  const userMap = initAdjList(friends);
+  const userList = [...userMap.keys()];
+
+  const usersScoreMap = new Map(userList.map((x) => [x, 0]));
+  const targetFriends = userMap.get(target) || new Set();
+
+  userMap.forEach((userFriends, user) => {
+    usersScoreMap.set(user, intersectNum(targetFriends, userFriends) * 10);
+  });
+  targetVisitors.forEach((visitor) => {
+    usersScoreMap.set(visitor, (usersScoreMap.get(visitor) || 0) + 1);
+  });
+
+  // filter out target, target's friends, and users with 0 score
+  const filterOption = ([name, score]) => name !== target && !targetFriends.has(name) && score > 0;
+
+  // entries: [[name,score], ...]
+  return ([...usersScoreMap.entries()]
+    .filter(filterOption)
+    .slice(0, 5)
+    .sort(sortByScoreAndName)
+    .map(([name, _]) => name));
 }
+
+const sortByScoreAndName = ([n1, s1], [n2, s2]) => s2 - s1 || n1.localeCompare(n2);
+
+// get intersection size of two sets
+const intersectNum = (a, b) => [...a].filter((x) => b.has(x)).length || 0;
+
+// userMap: { user : Set(friends) }
+const initAdjList = (friends) => {
+  const adjList = new Map();
+
+  friends.forEach(([a, b]) => {
+    adjList.set(a, (adjList.get(a) || new Set()).add(b));
+    adjList.set(b, (adjList.get(b) || new Set()).add(a));
+  });
+
+  return adjList;
+};
 
 module.exports = problem7;
