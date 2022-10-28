604482e5e3fce0af8de22ef19433bf1b320b9959
freemoon99 <soloist132@gmail.com>
2022-10-28 23:48:47 +0900

    fix[problem4]: 공백 조건 추가 및 기능

--- src/problem4.js
+++ src/problem4.js
@@ -5,12 +5,17 @@ function problem4(word) {
 
   for(let i=0;i<arr.length;i++){
     const ascii = arr[i].charCodeAt(0);
-
-    if(ascii >= 65 && ascii <= 90){
-      arr[i] = String.fromCharCode(155-ascii);
-    } else arr[i]=String.fromCharCode(219-ascii);
     
-    result.push(arr[i]);
+    if(arr[i] === ' '){
+      result.push(' ');
+    }
+    else {
+      if(ascii >= 65 && ascii <= 90){
+        arr[i] = String.fromCharCode(155-ascii);
+      } else arr[i]=String.fromCharCode(219-ascii);
+      
+      result.push(arr[i]);
+    }
   }
   return result.join('');
   return answer;
