4730d9b67a40567c69f09f8f4b23c55a2c47838f
yeong bin <2976622@naver.com>
2022-10-28 13:39:55 +0900

    chore: [problem6] getCombinationOfStr 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,21 @@
-/* 
- * 중복된 값이 있는 지 확인 
+/**
+ * 문자열의 연속 조합을 반환한다. ex) "수리남" -> ["수리", "리남"]
+ * @param {string} str 
+ * @returns {Array<string>} 조합된 배열
+ */
+function getCombinationOfStr(str) {
+  return str
+    .split("")
+    .map((cur, idx, arr) => `${cur}${arr[idx + 1] || ''}`)
+    .slice(0, str.length - 1);
+}
+
+/**
+ * 중복된 값이 있는 지 확인
  */
 function isRepeated(cur, idx, arr) {
+  // 문자열의 연속 조합 구하기 (getCombination)
+  // arr와 비교해서 있다면 true
 }
 
 /**
