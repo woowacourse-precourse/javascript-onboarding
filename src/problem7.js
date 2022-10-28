a447c223e66f19662a6895bc50bd5b9e02209b5c
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-28 12:59:15 +0900

    [문제7] user의 친구를 찾는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+function findFriendsOfUser(user, friends) {
+  let friendsArray = [];
+
+  friends.map((friend) => {
+    if (friend.includes(user)) {
+      let newArray = friend.filter((element) => element !== user);
+      friendsArray.push(newArray[0]);
+    }
+  });
+  return friendsArray;
 }
 
 module.exports = problem7;
