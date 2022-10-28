a26b4b3eca4d4f278da24721e384872211ce8cca
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 17:53:34 +0900

    feat: implement getFrogSpelling function

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,45 @@
+const FROG_DICTIONARY = {
+  a: 'z',
+  b: 'y',
+  c: 'x',
+  d: 'w',
+  e: 'v',
+  f: 'u',
+  g: 't',
+  h: 's',
+  i: 'r',
+  j: 'q',
+  k: 'p',
+  l: 'o',
+  m: 'n',
+  n: 'm',
+  o: 'l',
+  p: 'k',
+  q: 'j',
+  r: 'i',
+  s: 'h',
+  t: 'g',
+  u: 'f',
+  v: 'e',
+  w: 'd',
+  x: 'c',
+  y: 'b',
+  z: 'a',
+};
+
 function checkEnglish(spelling) {
   const regExp = /[a-zA-Z]/g;
   return regExp.test(spelling);
+}
+
+function getFrogSpelling(spelling) {
+  if (spelling === spelling.toUpperCase()) {
+    const lowerCaseSpelling = spelling.toLowerCase();
+    const newFrogWord = FROG_DICTIONARY[lowerCaseSpelling];
+    return newFrogWord.toUpperCase();
+  }
+
+  return FROG_DICTIONARY[spelling];
 }
 
 function problem4(word) {
