cfd77cc777002002084905ad4636ff8cc75f4e82
Hwang Juhee <julia0276@naver.com>
2022-10-29 02:39:53 +0900

    :sparkles: [feat] 금액별 화폐 개수 변환 기능 구현 #P5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  let moneyList = [50000,10000,5000,1000,500,100,50,10,1];
+  let answer = new Array(9).fill(0);
+
+  for (i of moneyList){
+    if(money===0){
+      break;
+    }
+    answer[moneyList.indexOf(i)] += parseInt(money/i);
+    money %= i;
+  }
+
   return answer;
 }
 
