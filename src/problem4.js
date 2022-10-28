f749b1c74d9019ddea3cdb76da015f9d06c470be
Woong <jiyko7@kyonggi.ac.kr>
2022-10-28 15:19:30 +0900

    4번 문제 해결
    4번 문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,22 @@
 function problem4(word) {
-  var answer;
+  var answer="";
+  var temp;
+  var s;
+  
+  for (var i = 0; i<word.length; i++){
+    s = word.charCodeAt(i);
+    if (s>=65 && s<=90){
+      temp = 155 - s;
+    }
+    else if(s>=97 && s<=122){
+      temp = 219 - s;
+    }
+    else{
+      temp = s;
+    }
+    answer += String.fromCharCode(temp);
+  }
+
   return answer;
 }
 
