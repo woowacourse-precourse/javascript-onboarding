c88052a1ac1576310f13acc68ff8e7f932ece86e
MJ <mj632900@gmail.com>
2022-10-28 17:23:19 +0900

    feat : 반대 알파벳을 반환해주는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,45 @@
-function problem4(word) {
-  var answer;
-  return answer;
+const alphabet = {
+    a: "z",
+    b: "y",
+    c: "x",
+    d: "w",
+    e: "v",
+    f: "u",
+    g: "t",
+    h: "s",
+    i: "r",
+    j: "q",
+    k: "p",
+    l: "o",
+    m: "n",
+    n: "m",
+    o: "l",
+    p: "k",
+    q: "j",
+    r: "i",
+    s: "h",
+    t: "g",
+    u: "f",
+    v: "e",
+    w: "d",
+    x: "c",
+    y: "b",
+    z: "a",
+};
+
+function getOppositeAlphabet(word) {
+    const codeNumber = word.charCodeAt();
+
+    if (codeNumber >= 65 && codeNumber <= 90) {
+        return alphabet[word.toLowerCase()].toUpperCase();
+    }
+    if (codeNumber >= 97 && codeNumber <= 121) {
+        return alphabet[word];
+    }
+
+    return word;
 }
+
+function problem4(word) {}
 
 module.exports = problem4;
