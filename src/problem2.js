7a6e7470987e681202635f9bc05f60959730fd98
Serin-Kim <serin9864@gmail.com>
2022-10-28 17:22:49 +0900

    feat: PROBLEM2 풀이 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+  let popped = "";
+  stack.push(cryptogram[0]);
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (stack.at(-1) === cryptogram[i]) popped = stack.pop();
+    else if (popped === cryptogram[i]) continue;
+    else stack.push(cryptogram[i]);
+  }
+
+  return stack.join("");
 }
+
+/* 
+// for test
+const cryptograms = ["browoanoommnaon", "zyelleyz"];
+
+for (cryptogram of cryptograms) {
+  console.log(problem2(cryptogram));
+}  
+*/
 
 module.exports = problem2;
