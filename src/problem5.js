f52296dc050548429a0377a31c3ee9ca7b99db9a
jiyujin <yujin1679@naver.com>
2022-10-28 17:54:03 +0900

    doce(pro5): 기능 요구 사항 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,10 @@
+// 기능 요구 사항
+// 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
+// 각 몇 개로 변환되는지 금액이 큰 순서대로 배열에 담아 return 한다.
+// - [] money / 50000 -> money %= 50000
+// - [] money / 10000 -> money %= 10000
+// - [] ... 과정 반복하면서 answer 배열에 값 push
+
 function problem5(money) {
   var answer;
   return answer;
