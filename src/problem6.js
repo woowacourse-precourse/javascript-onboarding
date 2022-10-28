eb69c71547be38be0fddc869f4d18a5780ab472a
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-29 00:32:26 +0900

    [problem6] feat: 닉네임을 추출하는 함수, 닉네임을 2자리씩 잘라 배열에 push하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  for (let i = 0; i < forms.length; i++) {
+    nickname.push(forms[i][1]);
+  }
+}
+
+function duplication(arr) {
+  let seperate_name_arr = [];
+
+  for (let i = 0; i < arr.length; i++) {
+    for (let j = 0; j < arr[i].length - 1; j++) {
+      seperate_name_arr.push(arr[i][j] + arr[i][j + 1]);
+    }
+  }
 }
 
 module.exports = problem6;
