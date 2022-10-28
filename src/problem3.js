f8d489999ff0d0b90dcd49612d91750c9dad9577
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-28 11:30:34 +0900

    feat: [P3] 한 숫자 속 3, 6, 9의 개수 세기
    - 숫자를 한자리씩 쪼갠 문자열 배열을 만들기
    - 배열 속 3, 6, 9의 개수를 세기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
-  var answer;
-  return answer;
+}
+
+function counter369(num) {
+  const splitNumber = (num) => {
+    return num.toString().split('')
+  } 
+  const check369 = (arr) => {
+    let count = 0;
+    arr.forEach((item) => {
+      if (item === "3" || item === "6" || item === "9") count++
+    })
+    return count
+  } 
+  return check369(splitNumber(num))
 }
 
 module.exports = problem3;
