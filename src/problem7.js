bc0891e0ac710164afcea27d691a964360a742e3
wonkyum-kim <dodo12120@naver.com>
2022-10-28 17:03:48 +0900

    feat: add make_adj

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,32 @@
+function make_adj(a, b, adj, map) {
+  if (!map.has(a)) {
+    map.set(a, [adj.length, 0]);
+    adj.push([b]); // a -> b
+  } else {
+    // map.has(a)
+    adj[map.get(a)[0]].push(b); // a -> b
+  }
+
+  if (!map.has(b)) {
+    map.set(b, [adj.length, 0]);
+    adj.push([a]); // b -> a
+  } else {
+    // map.has(b)
+    adj[map.get(b)[0]].push(a); // b -> a
+  }
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  // adjacency list
+  let adj = [];
+
+  // Informations of users
+  let user_info = new Map(); // name, [index, score]
+
+  // Make an adjacency list.
+  for (let [a, b] of friends) {
+    make_adj(a, b, adj, user_info);
+  }
 }
 
 module.exports = problem7;
