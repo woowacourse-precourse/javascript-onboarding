f3dfd61046e15e0c144637413ba923ddfd7699b8
Rki0 <pky00823@gmail.com>
2022-10-28 12:25:12 +0900

    refactor: prob2. 문제 해결 방법 변경에 따른 함수 수정
    3개 이상의 연속 문자까지 판별할 수 있도록 기능을 추가함에 따라
    스택을 사용하던 방법을 폐기하였습니다.
    따라서, 함수 내용 및 함수명이 전면 변경되었습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -16,16 +16,56 @@ function checkCryptogramOnlyLowerCase(cryptogram) {
   return true;
 }
 
-function compareStackWithCrypto(stack, cryptoArr) {
-  for (let crypto of cryptoArr) {
-    if (crypto === stack[stack.length - 1]) {
-      stack.pop();
-    } else {
-      stack.push(crypto);
+function runDecoding(cryptogram) {
+  let startIndex = 1000;
+  let endIndex = 1000;
+
+  while (true) {
+    let wantDeleteArr = [];
+
+    // 문자열을 순회하면서 반복되는 부분의 데이터를 배열화하여 저장한다.
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (cryptogram[i] !== cryptogram[i + 1]) {
+        if (startIndex !== 1000) {
+          endIndex = i;
+
+          wantDeleteArr.push([cryptogram[i], startIndex, endIndex]);
+
+          startIndex = 1000;
+          endIndex = 1000;
+
+          continue;
+        }
+
+        continue;
+      }
+
+      if (startIndex === 1000) {
+        startIndex = i;
+      }
+    }
+
+    // for문이 한번 끝나면 전체 문자열에 대한 순회가 한 번 끝난 것이다.
+    for (let i = 0; i < wantDeleteArr.length; i++) {
+      const str = wantDeleteArr[i][0];
+
+      const start = wantDeleteArr[i][1];
+
+      const end = wantDeleteArr[i][2];
+
+      const deleteStr = str.repeat(end - start + 1);
+
+      let deletedStr = cryptogram.split(deleteStr);
+
+      cryptogram = deletedStr.join("");
+    }
+
+    if (wantDeleteArr.length === 0) {
+      break;
     }
   }
 
-  return stack.join("");
+  return cryptogram;
 }
 
 function problem2(cryptogram) {
@@ -36,13 +76,7 @@ function problem2(cryptogram) {
     return;
   }
 
-  let stack = [];
-
-  let cryptoArr = cryptogram.split("");
-
-  const ans = compareStackWithCrypto(stack, cryptoArr);
-
-  return ans;
+  return runDecoding(cryptogram);
 }
 
 module.exports = problem2;
