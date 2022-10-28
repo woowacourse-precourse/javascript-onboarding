9c55a0c6bc9ea6ae934a13988af4b3bb74d86026
yong <tkddbs1644@gmail.com>
2022-10-28 16:58:56 +0900

    문제3: 숫자를 인수로 받아 손뼉치는 횟수를 구하는 기능을 구현한다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,18 @@
 /*
-[ ] 숫자를 인수로 받아 손뼉치는 횟수를 구하는 기능을 구현한다.
+[x] 숫자를 인수로 받아 손뼉치는 횟수를 구하는 기능을 구현한다.
 [ ] 1부터 number까지 손뼉치는 횟수를 구하는 기능을 구현하고 결과를 반환한다.
 */
+
+function countClap369(number) {
+  const targetNumbers = ["3", "6", "9"];
+  let clapCount = 0;
+  for (const charNumber of [...String(number)]) {
+    if (targetNumbers.includes(charNumber)) {
+      clapCount += 1;
+    }
+  }
+  return clapCount;
+}
 
 function problem3(number) {
   var answer;
