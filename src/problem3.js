b5ba07e53b2af9487d6ba0be9721c2e166745c2d
jongwonp <jw032876@gmail.com>
2022-10-28 09:22:05 +0900

    solve problem2

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (i = 1; i <= number; i++) {
+    count += count369(i);
+  }
+
+  return count;
 }
+
+const count369 = (num) => {
+  const regex = new RegExp(/[369]/g);
+  return num.toString().match(regex) ? num.toString().match(regex).length : 0;
+};
 
 module.exports = problem3;
