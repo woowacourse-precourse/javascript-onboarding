5466c28dfc1cd192e12ef640a0b2a58dea7683cb
evencoding <evencoding@gmail.com>
2022-10-28 23:56:05 +0900

    [문제5] refactor: for문을 reduce로 바꿔 구현 (slice, splice로 reduce에 break구현)

--- src/problem5.js
+++ src/problem5.js
@@ -1,15 +1,17 @@
 const KIND_OF_MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  const result = new Array(KIND_OF_MONEY.length).fill(0);
-  for (let i = 0; i < KIND_OF_MONEY.length; i++) {
-    result[i] = Math.floor(money / KIND_OF_MONEY[i]);
-    money %= KIND_OF_MONEY[i];
+  const answer = KIND_OF_MONEY.slice(0).reduce((acc, curMoney, i, arr) => {
+    acc[i] = Math.floor(money / curMoney);
+    money %= curMoney;
     if (money <= 0) {
-      break;
+      arr.splice(1);
     }
-  }
-  return result;
+
+    return acc;
+  }, new Array(KIND_OF_MONEY.length).fill(0));
+
+  return answer;
 }
 
 module.exports = problem5;
