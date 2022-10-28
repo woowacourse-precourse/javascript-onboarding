49159127033502cd5d7cfbe79087975d4544ce3f
feb-dain <feb.dain@gmail.com>
2022-10-28 16:30:54 +0900

    feat: [problem4] create array of lower case and upper case alphabet

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,7 @@
 function problem4(word) {
   const lowerCase = Array.from({ length: 26 }, (VOID, index) => String.fromCharCode(index + 97));
   const upperCase = Array.from({ length: 26 }, (VOID, index) => String.fromCharCode(index + 65));
+  const alphabet = lowerCase.concat(upperCase);
   
   return answer;
 }
