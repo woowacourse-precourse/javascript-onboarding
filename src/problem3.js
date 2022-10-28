27180f07267b4a318d9ecb419796d838c0ba1f27
pinkishincoloragain <smb1103@gmail.com>
2022-10-28 14:51:52 +0900

    feat: solved problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let targetArr = [
+    ...Array(number)
+      .fill()
+      .map((_, idx) => idx + 1)
+      .join(''),
+  ].filter((x) => ['3', '6', '9'].includes(x));
+
+  return targetArr.length;
 }
 
 module.exports = problem3;
