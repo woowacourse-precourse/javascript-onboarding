b5e4507ad17a666e7b1d987474d3c6ea1e15a3a1
audghks9245 <audghks9245@naver.com>
2022-10-28 16:44:16 +0900

    problem1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,43 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(exception(pobi)||exception(crong)) return -1;
+  return fight(pobi,crong);
+}
+
+function divideDigit(n){
+  let nums =[]
+  do{
+    nums.push(n%10);
+    n = Math.floor(n/10);
+  }
+  while(n>0);
+  return nums;
+}
+
+function addtimes(nums){
+
+  const addsum = divideDigit(nums).reduce((a,b)=>(a+b));
+  const timessum = divideDigit(nums).reduce((a,b)=>(a*b))
+  if(addsum>timessum)
+    return addsum;
+  if(timessum>addsum)
+    return timessum;
+}
+
+function fight(pobi,crong){
+  const pobiPoint=Math.max(addtimes(pobi[0]), addtimes(pobi[1]));
+  const crongPoint=Math.max(addtimes(crong[0]),addtimes(crong[1]));
+  if(pobiPoint>crongPoint) return 1;
+  if(pobiPoint<crongPoint) return 2;
+  if(pobiPoint==crongPoint) return 0;
+}
+
+
+function exception(array){
+  if(array[0]==1 || array[1]==400) return true;
+  if(array[0]%2==0) return true;
+  if(array.length!=2) return true;
+  if(array[1]-array[0]!=1) return true;
+  return false;
 }
 
 module.exports = problem1;
