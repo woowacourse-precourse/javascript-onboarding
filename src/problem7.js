f9d05fa1fd28a0a10acad674b2a97a25506e3494
kang <mdiu2203@naver.com>
2022-10-29 02:47:23 +0900

    feat(PROBLEM7): make score board using friend table and visitors

--- src/problem7.js
+++ src/problem7.js
@@ -22,4 +22,67 @@ function addFriendToTable(friendTable, friendA, friendB) {
       : (friendTable[friendB] = new Set([friendA]));
 }
 
+function makeScoreBoard(username, friendTable, visitors) {
+  const scoreBoardFromSameFriends = makeScoreBoardFromSameFriendsWith(
+      username,
+      friendTable,
+  );
+  const scoreBoardFromVisitors = makeScoreBoardFrom(visitors);
+
+  return combineScoreBoard(scoreBoardFromSameFriends, scoreBoardFromVisitors);
+}
+
+function makeScoreBoardFromSameFriendsWith(username, friendTable) {
+  const scoreBoard = {};
+  for (let candidateName of Object.keys(friendTable)) {
+    scoreBoard[candidateName] = calcScoreFromSameFriends(
+        friendTable[username],
+        friendTable[candidateName],
+    );
+  }
+
+  return scoreBoard;
+}
+
+function calcScoreFromSameFriends(usersFriends, candidatesFriends) {
+  const commonFriends = setIntersection(usersFriends, candidatesFriends);
+  return commonFriends.size * 10;
+}
+
+function makeScoreBoardFrom(visitors) {
+  const scoreBoard = {};
+  for (let visitor of visitors) {
+    if (scoreBoard[visitor]) {
+      scoreBoard[visitor] += 1;
+    } else {
+      scoreBoard[visitor] = 1;
+    }
+  }
+
+  return scoreBoard;
+}
+
+function combineScoreBoard(board1, board2) {
+  const scoreBoard = { ...board1 };
+
+  for (let name of Object.keys(board2)) {
+    scoreBoard[name]
+        ? (scoreBoard[name] += board2[name])
+        : (scoreBoard[name] = board2[name]);
+  }
+
+  return scoreBoard;
+}
+
+function setIntersection(setA, setB) {
+  const result = new Set();
+  for (const value of setA) {
+    if (setB.has(value)) {
+      result.add(value);
+    }
+  }
+
+  return result;
+}
+
 module.exports = problem7;
