d21b2f00f66876bebcd9bde9d953dcb13558c1ba
millifail <hwangbird99@gmail.com>
2022-10-28 23:35:13 +0900

    문제3 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,19 @@
 function problem3(number) {
   var answer;
+  var clap = 0;
+  var num;
+
+  for (var i = 1; i < number + 1; i++) {
+    num = i;
+    while (num !== 0) {
+      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+        clap += 1;
+      }
+      num = Math.floor(num / 10);
+    }
+  }
+  answer = clap;
+
   return answer;
 }
 
