e7eadf055e19b08a35e0044323050df3944311e7
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-28 13:04:17 +0900

    feat: problem7(점수 계산하고 결과 반환하기 구현)

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,9 @@
 function problem7(user, friends, visitors) {
   var answer;
+  const POINT = {
+    friendOfFriend: 10,
+    visit: 1,
+  };
 
   // - [x] 유저(mrko)의 친구 찾기
   const userFriends = friends
@@ -22,10 +26,32 @@ function problem7(user, friends, visitors) {
     (friend) => !userFriends.includes(friend)
   );
 
+  // - [x] 점수 계산하기
+  const friendsRecommandPoint = [];
+
+  const calculatePoint = (name, point) => {
+    const idx = friendsRecommandPoint.findIndex(
+      (friend) => friend.name === name
+    );
+    if (idx === -1) {
+      friendsRecommandPoint.push({ name, point });
+      return;
+    }
+    friendsRecommandPoint[idx].point = friendsRecommandPoint[idx].point + point;
+  };
+
+  friendsOfFriends.forEach((name) =>
+    calculatePoint(name, POINT.friendOfFriend)
+  );
+  visitorsNotFriend.forEach((name) => {
+    calculatePoint(name, POINT.visit);
+  });
+
   // - [ ] 결과 반환하기
   //    - 점수가 가장 높은 순으로 최대 5명 반환한다.
   //    - 추천 점수 0 -> 반환 안한다.
   //    - 추천 점수 같음 -> 이름순으로 정렬한다.
+  answer = friendsRecommandPoint.map((friend) => friend.name);
 
   return answer;
 }
