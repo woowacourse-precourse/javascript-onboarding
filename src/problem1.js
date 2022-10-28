ea36234e8edd052270c5d76fca64494ff7276064
coolIlive <galilee155@naver.com>
2022-10-29 00:13:57 +0900

    FEAT(problem1): 예외상황 입력시 -1 리턴 기능 추가합니다
    각 page의 차이가 1인 경우엔 잘못된 입력이므로 -1을 리턴한다

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,16 @@
 function problem1(pobi, crong) {
   var answer;
+  let pobiScore=0, crongScore=0;
+
+  // 잘못된 페이지인 경우 -1리턴
+  if (pobi[1]-pobi[0]!==1 || crong[1]-crong[0]!==1) {
+    return -1;
+  };
+
+  // pobi의 점수
+  
+  // crong의 점수
+  
   return answer;
 }
 
