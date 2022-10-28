98dbb6ac9add0d11ca52ef45e64c1ad14a540462
Raehyeon Jeong <raehy19@gmail.com>
2022-10-28 16:22:16 +0900

    feat(problem1) : 입력값 예외 처리 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,20 @@
+function numValueError(num) {
+  return isNaN(num) || !Number.isInteger(num) || num < 1 || num > 400;
+}
+
+function isException(arg) {
+  return (
+    !Array.isArray(arg) ||
+    arg.length !== 2 ||
+    numValueError(arg[0]) ||
+    numValueError(arg[1]) ||
+    arg[0] % 2 === 0 ||
+    arg[1] - arg[0] !== 1
+  );
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isException(pobi) || isException(crong)) return -1;
 }
 
 module.exports = problem1;
