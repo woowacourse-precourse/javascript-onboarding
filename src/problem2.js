e5adc2ca641edf90857945a84c450596480cac29
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-28 22:27:33 +0900

    feat: PROBLEM 2 연속하는 중복 알파벳 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -6,6 +6,31 @@ function problem2(cryptogram) {
     if (cryptogram !== lowerCaseStr) return false;
     return true;
   }
+
+  const removeDuplicate = (cryptogram) => {
+    let strArr = [...cryptogram];
+    let answer = [];
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (strArr[i] == strArr[i + 1]) {
+        i = i + 1;
+      } else {
+        if (strArr[i] == strArr[i - 1]) continue;
+        else {
+          if (strArr[i] == answer[answer.length - 1])
+            answer = answer.slice(0, -1);
+          else {
+            answer.push(strArr[i]);
+          }
+        }
+      }
+    }
+    return answer.join("");
+  };
+
+  let result = removeDuplicate(cryptogram);
+
+  if (!check(cryptogram)) { throw Error("1이상 1000이하 길이의 알파벳 소문자 문자열을 입력해주세요.") }
+  else return result;
 }
 
 module.exports = problem2;
