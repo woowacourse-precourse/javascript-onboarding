af6deafb03e7a406f6a501e4fb0e8a4927fdb8da
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-28 17:53:09 +0900

    feat: [P5] 주어진 금액에 대해 큰 액수부터 마지막 액수까지 나누기를 반복한 결과 반환
    - 각 액수로 나눈 몫을 해당 지폐 개수로 저장하기
    - 각 액수로 나눈 나머지를 다음 액수를 나누기 위한 금액으로 저장하기
    - 마지막 액수까지 해당 과정을 반복하기
    - 각 지폐 별 개수를 담은 Array 반환하기

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
   let result = new Array(9).fill(0)
+  
+  for (let i = 0; i < change.length; i++) {
+    result[i] = Math.floor(money / change[i])
+    money %= change[i]
+  } 
+  
+  return result
 }
 
 const change = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
