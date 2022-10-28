d68fe17e435d2dd5b4f5ab4f609f94606e1905fe
ghenmaru <rbgksqkr>
2022-10-28 23:46:09 +0900

    feat: reverse alphabet to use ASCII code

--- src/problem4.js
+++ src/problem4.js
@@ -3,8 +3,6 @@
 - 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
 1. 대문자인지, 소문자인지 확인
 2. 아스키코드값을 이용해 a-z를 뒤집어 변환
-
-
 */
 
 function isUpper(word) {
@@ -25,11 +23,35 @@ function isLower(word) {
 }
 
 
+function process(word){
+  result = "";
+  let first = 0;
+  let end = 0;
+  for(let i = 0; i < word.length; i++){
+    if(word[i] !== " "){
+      if(isUpper(word[i])){
+        first = 65;
+        end = 90;
+      } else if(isLower(word[i])){
+        first = 97;
+        end = 122;
+      }
+      let interval = word[i].charCodeAt() - first
+      result += String.fromCharCode(end-interval)
+    } else {
+      result += word[i];
+    }
+  }
+  return result;
+}
+
+
 function problem4(word) {
   let answer;
   answer = process(word);
   return answer;
 }
+
 
 module.exports = problem4;
 
