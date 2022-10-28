2e3de3d6b6a8595dc946f53cb936181ff181da93
freemoon99 <soloist132@gmail.com>
2022-10-28 23:44:32 +0900

    feat[problem4]: 대문자 리버스 기능

--- src/problem4.js
+++ src/problem4.js
@@ -5,6 +5,10 @@ function problem4(word) {
 
   for(let i=0;i<arr.length;i++){
     const ascii = arr[i].charCodeAt(0);
+
+    if(ascii >= 65 && ascii <= 90){
+      arr[i] = String.fromCharCode(155-ascii);
+    } 
   }
   return answer;
 }
