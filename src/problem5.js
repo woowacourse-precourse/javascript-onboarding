055b2f3379a1d8b08c4fc07173135c61b94048b9
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-10-29 00:30:08 +0900

    문제5 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
   var answer;
+  let price = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let number = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (var i=0; i<price.length; i++){
+    if(parseInt(money/price[i])){
+      number[i] = parseInt(money/price[i]);
+      money -= price[i]*number[i];
+    }
+  }
+  answer = number;
   return answer;
 }
 
