1a20a089540f7dabb56c37e189c36dd17746f897
givensik <kjs4072@icloud.com>
2022-10-29 04:17:56 +0900

    problem2 complete

--- src/problem2.js
+++ src/problem2.js
@@ -1,22 +1,25 @@
+/**중복있으면 지우는 함수 */
+function check(cryptogram){
+  for(i=0;i<cryptogram.length;i++){
+      cryptogram = cryptogram.replace(cryptogram[i]+cryptogram[i],'');
+  }
+  return cryptogram
+}
+
 function problem2(cryptogram) {
   var answer ='';
-  var i;
-  var flag = 0;
-  while(1){
-    flag = 0;
-    for(i=0;i<cryptogram.length - 1;i++){
-      console.log(cryptogram[i]);
-      if(cryptogram[i] == cryptogram[i+1]){
-        cryptogram.replace(cryptogram[i]+cryptogram[i], '');
-        flag = 1;
-      }
-    }
-    if(flag == 0){
+  
+  while(true){
+    tmp = check(cryptogram)
+    if(cryptogram === tmp || cryptogram === ''){//중복이 없으면 끝
       break;
+    }else{//중복이 있는 경우 다시 확인
+      cryptogram = tmp
     }
   }
   
   answer = cryptogram;
+  
   return answer;
 }
 
