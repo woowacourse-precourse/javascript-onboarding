7dc1c21508148a52198362dcfccc3062f7075203
yeonsuBeak <soothbugger@gmail.com>
2022-10-28 17:00:03 +0900

    feat(problem5): 화폐별로 변환되는 개수 구하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,23 @@
 function problem5(money) {
+  const result = countByBillUnit(money);
+}
+
+function countByBillUnit(currentMoney) {
   const BILL_UNIT_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const listLength = BILL_UNIT_LIST.length;
   let billCountList = new Array(listLength).fill(0);
+
+  for (
+    let currentUnitIndex = 0;
+    currentUnitIndex < listLength;
+    currentUnitIndex++
+  ) {
+    billCountList[currentUnitIndex] += Math.floor(
+      currentMoney / BILL_UNIT_LIST[currentUnitIndex]
+    );
+  }
+
+  return billCountList;
 }
 
 module.exports = problem5;
