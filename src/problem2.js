fcfed1c39fee234a6d68f7f1474b06458b4b112c
peacemaker474 <81507322+peacemaker474@users.noreply.github.com>
2022-10-28 23:07:21 +0900

    [FEAT] Add Overlap Word Variable
    중복 단어 체크하기 위한 변수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,17 @@
 function problem2(cryptogram) {
   const words = [...cryptogram];
   let answer = '';
+  let overword = null;
 
   for (let i = 0; i < words.length; i++) {
-    words[i] === words[i + 1] ? i++ : answer += words[i];
+    if (overword === words[i]) continue;
+
+    if (words[i] === words[i + 1]) {
+      overword = words[i];
+    } else {
+      overword = null;
+      answer += words[i];
+    }
   }
 
   return answer === cryptogram ? answer : problem2(answer);
