81d59be537d21bbf13b8a119178500a8025dd62e
minjaeKim <smallkdb@gmail.com>
2022-10-29 00:05:27 +0900

    feat: [Problem4] Add convertTextFroggy function
    문자열을 청개구리 사전에 맞게 변환하는 convertTextFroggy 함수를 추가하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -8,8 +8,31 @@ const ASCIICODE = {
 
 Object.freeze(ASCIICODE);
 
+function convertTextFroggy(word) {
+  const { UPPERCASE_A, UPPERCASE_Z, LOWERCASE_A, LOWERCASE_Z, SPACE } = ASCIICODE;
+  let asciiNumberList = [];
+
+  for(let i = 0; i < word.length; i++) {
+    const asciiNumber = word.charCodeAt(i);
+
+    if(asciiNumber === SPACE) {
+      asciiNumberList.push(asciiNumber);
+    }
+    if(asciiNumber >= UPPERCASE_A && asciiNumber <= UPPERCASE_Z) {
+      asciiNumberList.push(UPPERCASE_Z - asciiNumber%UPPERCASE_A);
+    }
+    if(asciiNumber >= LOWERCASE_A && asciiNumber <= LOWERCASE_Z) {
+      asciiNumberList.push(LOWERCASE_Z - asciiNumber%LOWERCASE_A);
+    }
+  }
+
+  return String.fromCharCode(...asciiNumberList);
+}
+
+
 function problem4(word) {
   var answer;
+  answer = convertTextFroggy(word);
   return answer;
 }
 
