9d4080b08232b2702d94bb0908a0dd36df6e6267
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-10-28 23:40:44 +0900

    문제3 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
   var answer;
+  let numberString = String(number)
+  let count = 0;
+  for (var i=1; i<=number; i++){
+    for(var j=0; j<String(i).length; j++){
+      if(String(i)[j] == '3' || String(i)[j] == '6' || String(i)[j] == '9'){
+        count++;
+      }
+    }
+  }
+  answer = count;
   return answer;
 }
 
