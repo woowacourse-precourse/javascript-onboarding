b31c4a8c2085d22ca36d8a2e8bafdead6871a071
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 12:03:09 +0900

    feat: 입력 문자열 검사함수 추가
    문자열의 길이와 소문자만 존재하는지 검사

--- src/problem2.js
+++ src/problem2.js
@@ -4,7 +4,29 @@
 // 3. 중복 문자열 제거 함수 구현
 // 4. 중복 문자열이 없을때까지 검사 후 반환값 리턴
 
+function isLowerCase(value) {
+  if (value < 97 || value > 122) {
+    return -1;
+  }
+  return 0;
+}
+
+function validInputCheck(inputValue) {
+  if (inputValue.length < 1 || inputValue.length > 1000) {
+    return -1;
+  }
+  for (index = 0; index < inputValue.length; index++) {
+    if (isLowerCase(inputValue.charCodeAt(index)) < 0) {
+      return -1;
+    }
+  }
+  return 0;
+}
+
 function problem2(cryptogram) {
+  if (validInputCheck(cryptogram) < 0) {
+    return "Argument Error";
+  }
   var answer;
   return answer;
 }
