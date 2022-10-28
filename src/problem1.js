7649dc7f5716a27e926a5d03901be66bef38373f
2wndrhs <2wndrhs@gmail.com>
2022-10-27 21:43:11 -0700

    feat(PROBLEM1): 페이지 번호가 연속적인지 확인하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,16 @@
+// 페이지 번호가 연속적인지 확인하는 기능
+function isPageContinuous(pages) {
+  if (pages[1] - pages[0] === 1) {
+    return true;
+  }
+  return false;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if (!isPageContinuous(pobi) || !isPageContinuous(crong)) {
+    return -1;
+  }
   return answer;
 }
 
