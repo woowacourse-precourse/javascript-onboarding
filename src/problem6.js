51968de66550099582754436eab2a76d80c4c862
GuHyeonWoo <ghw9174@gmail.com>
2022-10-28 23:50:57 +0900

    P6/이메일 예외처리 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -6,8 +6,22 @@
 // 5. 중복된 인덱스에 따라 이메일을 반환
 // 6. 중복되는 경우가 2가지 이상인 경우 -> 리스트를 개수에 맞게 반환
 
+// 정규식을 통해 이메일 예외처리
+function exceptionEmail(email) {
+  // 'RFC 5322 형식 + email.com'으로 이메일 확인
+  const regExp = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@email.com");
+  // 정규식에 충족하면 true, 불충족하면 false 반환
+  return regExp.test(email);
+}
+
 function problem6(forms) {
-  var answer;
+  let answer;
+
+  for([email, nick] of forms){
+    // 이메일 예외처리
+    let check_email = exceptionEmail(email);
+  }
+  
   return answer;
 }
 
