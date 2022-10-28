cb5ec3843bcac6a1120b1e5f507ea55f55603537
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-29 03:40:12 +0900

    feat(problem7): 사용자와 함께 아는 친구 있는 사람 추천친구목록 배열에 추가

--- src/problem7.js
+++ src/problem7.js
@@ -14,11 +14,12 @@ function problem7(user, friends, visitors) {
   // 추천 친구 목록 만들기
   let recommendations = [];
   for (let i = 0; i < current_friends.length; i++) {
+    // 2-1. 사용자 친구의 친구 찾기
     for (let j = 0; j < friends.length; j++) {
       for (let k = 0; k < 2; k++) {
-        // 사용자 친구의 친구 찾기
         if (current_friends[i] == friends[j][k]) {
-          // 추천 목록에 있는 사람인지 확인
+          // 추천친구목록에 추가
+          recommendations.push([friends[j][1 - k], 10]);
         }
       }
     }
