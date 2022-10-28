42398a8065743f1c6d7f607a360aa2923d76c65f
Deemou <4106243@naver.com>
2022-10-28 13:41:57 +0900

    feat<P2>: 주어진 문자열에서 연속적인 중복 문자들을 제거한 새 문자열을 받는다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,29 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+
   return answer;
 }
+const removeConsecutiveDuplicates = (text) => {
+  const textArr = text.split("");
+  let lastChar = textArr[0];
+  let dupeCount = 0,
+    i = 1;
+  for (i; i < textArr.length; i++) {
+    if (textArr[i] == lastChar) {
+      textArr[i - 1] = "";
+      dupeCount++;
+    } else {
+      if (dupeCount != 0) {
+        textArr[i - 1] = "";
+        dupeCount = 0;
+      }
+      lastChar = textArr[i];
+    }
+  }
+  if (dupeCount != 0) {
+    textArr[i - 1] = "";
+  }
+  return textArr.join("");
+};
 
 module.exports = problem2;
