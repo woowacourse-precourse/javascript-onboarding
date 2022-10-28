e921344dd5267c0cea9095ee081e0d938df08d4a
wonkyum-kim <dodo12120@naver.com>
2022-10-28 17:40:41 +0900

    feat: add recommend_friends

--- src/problem7.js
+++ src/problem7.js
@@ -1,30 +1,33 @@
-function make_adj(a, b, adj, map) {
-  if (!map.has(a)) {
-    map.set(a, [adj.length, 0]);
+function make_adj(a, b, adj, user_info) {
+  if (!user_info.has(a)) {
+    user_info.set(a, [adj.length, 0]);
     adj.push([b]); // a -> b
   } else {
     // map.has(a)
-    adj[map.get(a)[0]].push(b); // a -> b
+    adj[user_info.get(a)[0]].push(b); // a -> b
   }
 
-  if (!map.has(b)) {
-    map.set(b, [adj.length, 0]);
+  if (!user_info.has(b)) {
+    user_info.set(b, [adj.length, 0]);
     adj.push([a]); // b -> a
   } else {
     // map.has(b)
-    adj[map.get(b)[0]].push(a); // b -> a
+    adj[user_info.get(b)[0]].push(a); // b -> a
   }
 }
 
 function increase_score1(friends_of_user, user_info, adj, user) {
+  // If they have common friends, give 10 points.
   for (let curr_name of user_info.keys()) {
     // user don't have to increase the scores.
     if (curr_name === user) {
       continue;
     }
 
+    // `user_info.get(curr_name)[0]` is the index of curr_name in adj.
+    // Search curr_name's entire friends(next_name).
     for (let next_name of adj[user_info.get(curr_name)[0]]) {
-      // Check if they have common friends...
+      // Check if user and next_name are friends
       if (friends_of_user.includes(next_name)) {
         const [index, score] = user_info.get(curr_name);
         user_info.set(curr_name, [index, score + 10]);
@@ -34,19 +37,63 @@ function increase_score1(friends_of_user, user_info, adj, user) {
 }
 
 function increase_score2(friends_of_user, user_info, visitors) {
-  for (let a of visitors) {
+  for (let person of visitors) {
     // If he/she is already a friend with the user, don't recommend him/her.
-    if (friends_of_user.includes(a)) {
+    if (friends_of_user.includes(person)) {
       continue;
     }
-    if (!user_info.has(a)) {
-      user_info.set(a, [0, 1]);
+    // Give a point to a visitor
+    if (!user_info.has(person)) {
+      user_info.set(person, [0, 1]);
     } else {
       // user_info.has(a)
-      const [index, score] = user_info.get(a);
-      user_info.set(a, [index, score + 1]);
+      const [index, score] = user_info.get(person);
+      user_info.set(person, [index, score + 1]);
     }
   }
+}
+
+function select_friends(user_info, user) {
+  let recommendation = new Map();
+  let count = 0;
+  for (let [name, [, score]] of user_info) {
+    if (name === user || score === 0) {
+      continue;
+    }
+    if (count === 5) {
+      break;
+    }
+    if (!recommendation.has(score)) {
+      recommendation.set(score, [name]);
+    } else {
+      // recommendation.has(score)
+      let names = recommendation.get(score);
+      names.push(name);
+      recommendation.set(score, names);
+    }
+    count++;
+  }
+  return recommendation;
+}
+
+function recommend_friends(user_info, user) {
+  let recommendation = select_friends(user_info, user);
+  let answer = [];
+  let scores = [];
+  for (let score of recommendation.keys()) {
+    scores.push(score);
+  }
+  // Sort the scores with descending order
+  scores.sort((a, b) => b - a);
+
+  for (let score of scores) {
+    // If they have the same score, sort them by alphabetically.
+    let arr = [...recommendation.get(score)];
+    arr.sort();
+    answer.push(...arr);
+  }
+
+  return answer;
 }
 
 function problem7(user, friends, visitors) {
@@ -54,6 +101,7 @@ function problem7(user, friends, visitors) {
   let adj = [];
 
   // Informations of users
+  // Translate the name to index, and save the score.
   let user_info = new Map(); // name, [index, score]
 
   // Make an adjacency list.
@@ -67,6 +115,8 @@ function problem7(user, friends, visitors) {
   // If they have common friends, increse the score
   increase_score1(friends_of_user, user_info, adj, user);
   increase_score2(friends_of_user, user_info, visitors);
+
+  return recommend_friends(user_info, user);
 }
 
 module.exports = problem7;
