99790941a97a28b76962fb3af8a8fd540d52acf5
myung-j-p <jmpack97@gmail.com>
2022-10-29 01:23:05 +0900

    문제1_ 주어진 배열 pobi, crong에서 왼쪽과 오른쪽 각 자리숫자의 합, 곱의 배열에서 제일 큰 수를 찾는 기능추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,43 @@
 function problem1(pobi, crong) {
   var answer;
+  //랜덤 부분
+  // const PNum = [];
+  // for (let i = 0 ; i < 4 ; i++){
+  //   PNum.push(Math.floor(Math.random()*(400-1)*1));
+  // }
+  // console.log(pobi[1].slice())
+
+  function checker(...num){
+
+    function slicer(num){
+      let r = 0;
+      let r2 = 1;
+      for(let i = 0; i < num.length; i++){
+        let slicedNum = Number(num.slice(i,i+1));
+        r += slicedNum;
+        r2 *= slicedNum;
+      };
+      score.push(r,r2);
+    };
+
+    const score = [];
+    let index = 0;
+  
+    slicer(String(num[index]));
+    index++;
+    slicer(String(num[index]));
+
+    const finalScore = Math.max(...score);
+
+  }; 
+
+
+  const pobiScore = checker(pobi[0],pobi[1]);
+  const crongScore = checker(crong[0],crong[1]);
+
+
   return answer;
 }
+// problem1([333,222],[312,111]);
 
 module.exports = problem1;
---
+++ src/tempCodeRunnerFile.js
@@ -0,0 +1,4 @@
+
+  // const p2 = String(pobi[1]);
+  
+  // console.log(p1)
