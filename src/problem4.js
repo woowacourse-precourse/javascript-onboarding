0bd169e7535e345902940d571e7b1672a5a60cc4
minseok1109 <park485201@naver.com>
2022-10-28 14:05:49 +0900

    feat: 대소문자로 이루어진 사전 만드는 함수 #기능목록 - 4

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,39 @@ const isUpperCase = (string) => /^[A-Z]*$/.test(string);
 //소문자인지 체크하는 함수
 const isLowerCase = (string) => /^[a-z]*$/.test(string);
 
+//대문자는 대문자로, 소문자는 소문자로 사전 만들기
+function makeDict() {
+  //알파벳 대문자
+  let bigAlphabet = Array.from(Array(26))
+    .map((e, i) => i + 65)
+    .map((x) => String.fromCharCode(x));
+
+  //알파벳 소문자
+  let smallAlphabet = Array.from(Array(26))
+    .map((e, i) => i + 97)
+    .map((x) => String.fromCharCode(x));
+
+  //대문자 알파벳 뒤집기
+  let reverseBigAlphabet = [...bigAlphabet].reverse();
+
+  //소문자 알파벳 뒤집기
+  let reverseSmallAlphabet = [...smallAlphabet].reverse();
+
+  //대문자 사전 object 만들기
+  let bigDict = bigAlphabet.reduce(
+    (dict, alpha, index) => ({ ...dict, [alpha]: reverseBigAlphabet[index] }),
+    {}
+  );
+
+  //소문자 사전 object 만들기
+  let smallDict = smallAlphabet.reduce(
+    (dict, alpha, index) => ({ ...dict, [alpha]: reverseSmallAlphabet[index] }),
+    {}
+  );
+
+  return [smallDict, bigDict];
+}
+
 function problem4(word) {
   var answer;
   return answer;
