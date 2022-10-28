dd7d335ea11afba3acf28ccb4f3508ab154f8a67
Tory <88531407+1two13@users.noreply.github.com>
2022-10-28 14:09:29 +0900

    refactor(problem1): 예외 사항 체크하는 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,21 +1,23 @@
 // 기능 목록
-// 1. 예외 사항 처리 하기
+// 1. 예외 사항 validate 하기 (case1. pobi 또는 crong이 시작 면 또는 마지막 면이 나오도록 책을 펼치는 경우, case2. 각 배열의 값의 차이가 1 초과인 경우)
 // 2. pobi의 가장 큰 수 구하기
 // 3. crong의 가장 큰 수 구하기
 // 4. 승패 결과 처리하기
+
+// 예외 사항을 체크하는 함수
+function validate(user) {
+  // case1. user가 1, 2, 399, 400의 값을 가지고 있을 때
+  if (user.filter((el) => el <= 2 || el >= 399).length > 0) return false;
+  // case2. 배열의 값의 차이가 1 초과일때 ex) [99, 102]
+  if (user[1] - user[0] > 1) return false;
+  return true;
+}
 
 function problem1(pobi, crong) {
   // pobi 가장 큰 수
   let pobiMax;
   // 크롱의 가장 큰 수
   let crongMax;
-
-  // 예외 사항 처리하기
-  // 1. 포비나 크롱 배열이 1, 2, 399, 400의 값을 가지고 있을 때
-  const concatArr = pobi.concat(crong);
-  if (concatArr.filter((el) => el <= 2 || el >= 399).length > 0) return -1;
-  // 2. 각 배열의 값의 차이가 1 초과일때 ex) [99, 102]
-  if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) return -1;
 
   // pobi의 가장 큰 수 구하기
   pobi
@@ -44,7 +46,8 @@ function problem1(pobi, crong) {
     });
 
   // 승패 결과 처리하기 (포비 1, 크롱 2, 무승부 0, 예외사항 -1)
-  if (pobiMax > crongMax) return 1;
+  if (!validate(pobi) || !validate(crong)) return -1;
+  else if (pobiMax > crongMax) return 1;
   else if (pobiMax < crongMax) return 2;
   else return 0;
 }
