6d1ff2103851196883b2ead5405c59e43d59943f
JH <vvaa277@gmail.com>
2022-10-29 07:33:19 +0900

    Feat: 3~4 기능구현
    #P6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,6 @@
 function problem6(forms) {
   const twoChaArr = [];
-
+  const pushedEmail = [];
   for (let i = 0; i < forms.length; i++) {
     const nameSplited = forms[i][1].split("");
     for (let j = 0; j < nameSplited.length - 1; j++) {
@@ -8,6 +8,22 @@ function problem6(forms) {
       twoChaArr.push(dup);
     }
   }
+  const findDuplicates = (twoChaArr) =>
+    twoChaArr.filter(
+      (item, index) => twoChaArr[index] === twoChaArr[index + 2]
+    );
+  const duplicates = findDuplicates(twoChaArr);
+  const duplicatesJoined = duplicates.join("");
+
+  for (let h = 0; h < forms.length; h++) {
+    const findName = forms[h][1];
+    if (findName.includes(duplicatesJoined)) {
+      pushedEmail.push(forms[h][0]);
+    }
+  }
+  const noDupEmail = [...new Set(pushedEmail)];
+  
+  return noDupEmail.sort();
 }
 
 module.exports = problem6;
--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,5 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+
 }
 
 module.exports = problem7;
