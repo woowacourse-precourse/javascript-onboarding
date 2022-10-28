66dca8d70e9312488cdbe397a6378d46ae6ba8ee
cofla <cofla159@naver.com>
2022-10-29 07:06:28 +0900

    예외상황 추가 및 타입에러 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,47 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (
+    pobi[0] % 2 === 0 ||
+    pobi[0] + 1 !== pobi[1] ||
+    pobi[0] < 1 ||
+    pobi[1] > 400
+  )
+    return -1;
+  if (
+    crong[0] % 2 === 0 ||
+    crong[0] + 1 !== crong[1] ||
+    crong[0] < 1 ||
+    crong[1] > 400
+  )
+    return -1;
+  const pobiScore = getScore(pobi[0], pobi[1]);
+  const crongScore = getScore(crong[0], crong[1]);
+  if (pobiScore > crongScore) return 1;
+  else if (pobiScore < crongScore) return 2;
+  else return 0;
 }
 
-export default problem1;
+function getScore(left, right) {
+  return Math.max(plus(left), multiply(left), plus(right), multiply(right));
+}
+
+function plus(num) {
+  return num
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce((prev, cur) => prev + cur);
+}
+
+function multiply(num) {
+  return num
+    .toString()
+    .split("")
+    .map(Number)
+    .reduce((prev, cur) => prev * cur);
+}
+
+module.exports = problem1;
+
 /*
 0. pobi[0]+1 = pobi[1]이 아니거나 pobi%2==0이면 -1리턴
 1. Math.max(pobi[0].split('').reduce(더하기 or 곱하기))
