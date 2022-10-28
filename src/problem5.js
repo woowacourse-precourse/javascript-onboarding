d8a15929ed9b854887be29a7f6a80eb8d126e7fb
yu-retel <dbdudals100@naver.com>
2022-10-28 12:17:18 +0900

    feat: [P5] 변환배열 반환기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -4,3 +4,15 @@ function problem5(money) {
 }
 
 module.exports = problem5;
+
+function moneyTransform(money){
+  let moneyCount=0;
+  const resultArr=[];
+  const list = [50000,10000,5000,1000,500,100,50,10,1];
+  for(let item of list){
+    moneyCount = Math.floor(money/item); 
+    money = money - (item * Math.floor(money/item)); 
+    resultArr.push(moneyCount);
+  }
+  return resultArr;
+}
