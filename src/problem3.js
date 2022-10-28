0cf35b533364528949a34c7a8585757085c554c8
Inae Choi <inaechoi89@gmail.com>
2022-10-28 22:51:03 +0900

    feat(p3): count the numbers of 3, 6, 9

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,10 @@ function problem3(number) {
   var answer = 0;
   for (let i = 1; i <= number; i++) {
     let stringNum = String(i);
+    for (let j = 0; j < stringNum.length; j++) {
+      if (stringNum[j] === "3" || stringNum[j] === "6" || stringNum[j] === "9")
+        answer++;
+    }
   }
   return answer;
 }
