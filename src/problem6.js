2ee0e0cc866bac6e75ed6748f0681489b65c9abc
yong <tkddbs1644@gmail.com>
2022-10-28 19:57:47 +0900

    문제6: 단어를 포개서 자르는 기능을 구현한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,17 @@
 /*
-[ ] 단어를 포개서 자르는 기능을 구현한다.
+[x] 단어를 포개서 자르는 기능을 구현한다.
 [ ] 중복된 닉네임을 사용하는 유저를 찾는 기능을 구현한다.
 [ ] 중복된 닉네임을 사용하는 유저들을 반환한다.
 */
+
+function overlapWord(word) {
+  const slicedWords = [];
+  for (let index = 0; index < word.length - 1; index += 1) {
+    const slicedWord = word.slice(index, index + 2);
+    slicedWords.push(slicedWord);
+  }
+  return slicedWords;
+}
 
 function problem6(forms) {
   var answer;
