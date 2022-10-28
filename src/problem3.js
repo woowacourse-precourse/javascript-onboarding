31b9735cd97f294b24953d22f02a9485e4a5dfd2
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 13:57:24 +0900

    docs: 구현 기능 목록 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,17 @@
+// 1. 인자가 1이상 10000이하의 수인지 확인
+// 2. 인자에 따른 반환값 계산 함수 구현
+
+function validInputCheck(number) {
+  if (number < 1 || number > 10000) {
+    return -1;
+  }
+  return 0;
+}
+
 function problem3(number) {
+  if (validInputCheck(number) < 0) {
+    return "Argument Error";
+  }
   var answer;
   return answer;
 }
