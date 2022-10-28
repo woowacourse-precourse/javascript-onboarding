3246934e221fe9fc7031271486c380ab4efd9100
Hwang Juhee <julia0276@naver.com>
2022-10-29 02:27:07 +0900

    :sparkles: [feat] 알파벳 사전 반대순 구현 #P4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
 function problem4(word) {
-  var answer;
+  // 대문자 65-90 (155)
+  // 소문자 97-122 (219)
+  let splittedWord = word.split('')
+  let answer = '';
+  for (let i = 0; i<splittedWord.length; i++){
+    let ascii = splittedWord[i].charCodeAt()
+    if (ascii>=65 && ascii<=90){
+      answer += String.fromCharCode(155-ascii);
+    } else if (ascii>=97 && ascii<=122){
+      answer += String.fromCharCode(219-ascii);
+    } else {
+      answer += splittedWord[i]
+    }
+  }
+  
   return answer;
 }
 
