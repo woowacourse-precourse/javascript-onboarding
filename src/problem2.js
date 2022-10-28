566c24abc51b8ecec7c9b6da8f258778b9c689a1
wusi-hub <jws9595@naver.com>
2022-10-28 12:30:45 +0900

    problem2 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = [...cryptogram];
+  for(let i = 0; i < arr.length; i++) {
+    if(arr[i] === arr[i+1]) {
+      arr.splice(i,2);
+      i = -1;
+    } 
+  }
+  return arr.join('');
 }
 
 module.exports = problem2;
