6884f11d7ad27b3b64e9fbe1917c74cbd0e0d7f2
Rki0 <pky00823@gmail.com>
2022-10-28 15:26:18 +0900

    refactor: prob2. 기존 함수를 기능별로 쪼개서 모듈화
    하나의 함수에서 작동하던 기능들을 최대한 쪼갰습니다.
    indent를 더 이상 줄일 수 없는 부분이 있어,
    더 고민 중입니다.

--- src/problem2.js
+++ src/problem2.js
@@ -16,54 +16,82 @@ function checkCryptogramOnlyLowerCase(cryptogram) {
   return true;
 }
 
-function runDecoding(cryptogram) {
-  let startIndex = 1000;
-  let endIndex = 1000;
+function updateDeleteArray(cryptogram, startIndex, endIndex, wantDeleteArr, i) {
+  if (startIndex !== 1000) {
+    endIndex = i;
 
-  while (true) {
-    let wantDeleteArr = [];
+    wantDeleteArr.push([cryptogram[i], startIndex, endIndex]);
 
-    // 문자열을 순회하면서 반복되는 부분의 데이터를 배열화하여 저장한다.
-    for (let i = 0; i < cryptogram.length; i++) {
-      if (cryptogram[i] !== cryptogram[i + 1]) {
-        if (startIndex !== 1000) {
-          endIndex = i;
+    return true;
+  }
 
-          wantDeleteArr.push([cryptogram[i], startIndex, endIndex]);
+  return false;
+}
 
-          startIndex = 1000;
-          endIndex = 1000;
+function makeWantDeleteArray(cryptogram, startIndex, endIndex, wantDeleteArr) {
+  let isTrue = false;
 
-          continue;
-        }
+  // 문자열을 순회하면서 반복되는 부분의 데이터를 배열화하여 저장한다.
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (cryptogram[i] !== cryptogram[i + 1]) {
+      if (
+        updateDeleteArray(cryptogram, startIndex, endIndex, wantDeleteArr, i)
+      ) {
+        startIndex = 1000;
+        endIndex = 1000;
 
         continue;
       }
 
-      if (startIndex === 1000) {
-        startIndex = i;
-      }
+      continue;
     }
 
-    // for문이 한번 끝나면 전체 문자열에 대한 순회가 한 번 끝난 것이다.
-    for (let i = 0; i < wantDeleteArr.length; i++) {
-      const str = wantDeleteArr[i][0];
-
-      const start = wantDeleteArr[i][1];
-
-      const end = wantDeleteArr[i][2];
-
-      const deleteStr = str.repeat(end - start + 1);
-
-      let deletedStr = cryptogram.split(deleteStr);
-
-      cryptogram = deletedStr.join("");
+    if (startIndex === 1000) {
+      startIndex = i;
     }
+  }
+}
+
+function deleteRepeatedString(cryptogram, wantDeleteArr) {
+  // for문이 한번 끝나면 전체 문자열에 대한 순회가 한 번 끝난 것이다.
+  for (let i = 0; i < wantDeleteArr.length; i++) {
+    const str = wantDeleteArr[i][0];
+
+    const start = wantDeleteArr[i][1];
+
+    const end = wantDeleteArr[i][2];
+
+    const deleteStr = str.repeat(end - start + 1);
+
+    let deletedStr = cryptogram.split(deleteStr);
+
+    cryptogram = deletedStr.join("");
+  }
+
+  return cryptogram;
+}
+
+function roundsAllString(cryptogram, startIndex, endIndex) {
+  while (true) {
+    let wantDeleteArr = [];
+
+    makeWantDeleteArray(cryptogram, startIndex, endIndex, wantDeleteArr);
 
     if (wantDeleteArr.length === 0) {
       break;
     }
+
+    cryptogram = deleteRepeatedString(cryptogram, wantDeleteArr);
   }
+
+  return cryptogram;
+}
+
+function runDecoding(cryptogram) {
+  let startIndex = 1000;
+  let endIndex = 1000;
+
+  cryptogram = roundsAllString(cryptogram, startIndex, endIndex);
 
   return cryptogram;
 }
