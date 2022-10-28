2f4be87de476a038921cb51cc40a4a52e7fcb659
이석준 <sukjune96@gmail.com>
2022-10-28 22:57:37 +0900

    [문제4] fix: 알파벳 외의 문자 예외처리

--- src/problem4.js
+++ src/problem4.js
@@ -5,6 +5,7 @@ function problem4(word) {
     let newASCII = 32;
     if (ASCII >= 65 && ASCII <= 90) newASCII = 90 - (ASCII - 65);
     else if (ASCII >= 97 && ASCII <= 122) newASCII = 122 - (ASCII - 97);
+    else newASCII = ASCII;
     answer += String.fromCharCode(newASCII);
   }
   return answer;
@@ -16,7 +17,7 @@ module.exports = problem4;
 // 1. 문자열을 루프돌며 아스키 코드를 통해 대칭인 값 구하기, 대문자(65~90) 소문자(97~122)
 // 2. 대문자의 경우 90-(현재문자-65)
 //    소문자의 경우 122-(현재문자-97)
-//    공백문자는 그대로 번역
+//    알파벳 외의 글자는 그대로 번역
 // 3. 번역 결과 출력
 
-problem4("I love you");
+problem4("I love you1234");
