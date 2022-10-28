8407b78790f6494d633790a8a387554611d0df6e
yunhwan98 <79241793+yunhwan98@users.noreply.github.com>
2022-10-28 19:33:44 +0900

    문제5 풀이

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,18 @@
 function problem5(money) {
   var answer;
+
+  //나눌 금액
+  let div = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  //나눈 개수
+  let cnt = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  for (i = 0; i < div.length; i++) {
+    if (parseInt(money / div[i]) > 0) {
+      cnt[i] += parseInt(money / div[i]);
+      money %= div[i];
+    }
+  }
+  answer = cnt;
   return answer;
 }
 
