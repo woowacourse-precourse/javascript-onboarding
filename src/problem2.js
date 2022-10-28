cc45b8a951dfa1c8c93f804350d4c3e8c475e394
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-29 03:19:54 +0900

    problem 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
+  const answer = reduceWord(cryptogram);
+  // console.log(`prev : ${cryptogram}`)
+  // console.log(`next : ${answer}`)
   return answer;
+}
+
+const reduceWord = (word) => {
+  let charSet = Array.from(new Set(word.split('')));
+  let new_word = word
+  for(let i = 0 ; i<charSet.length; i++){
+    for(let j = 0 ; j<new_word.length; j++){
+      // console.log(new_word.match(/(\w)\1/))
+      new_word = new_word.replace(/(\w)\1/,'')
+      // new_word = new_word.replace(/([a-z])\1{1,}/,'')
+    }
+  }
+  return new_word
 }
 
 module.exports = problem2;
