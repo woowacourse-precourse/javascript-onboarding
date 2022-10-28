21fa35ca936220be508a66337469a298f370fd06
pp449 <mma7710@naver.com>
2022-10-28 22:39:38 +0900

    [문제 3] : 해당 숫자를 자리수 별로 나누는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,16 @@
+const divideByDigit = (number) => {
+  for(let i=number; i!=0; i=parseInt(i/10)) {
+    let n = i%10;
+  }
+}
+
 function problem3(number) {
   for (let i=1; i<=number; i++) {
-    
+    divideByDigit(i);
   }
   return answer;
 }
+
+problem3(13);
 
 module.exports = problem3;
