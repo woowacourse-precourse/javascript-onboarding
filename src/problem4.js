809b33cd8b9417a66c6225edcfdbf6aba7aaa304
easyone <thinkanddoit@naver.com>
2022-10-28 17:25:57 +0900

    #4 feat: 문제4 문자를 반대로 바꾸는 함수를 생성
    - 경우 3가지 적용 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
+  const reverseChar = (char) => {
+    const uniCode = char.charCodeAt();
+    if (/[A-Z]/.test(char)) return String.fromCharCode(155 - uniCode);
+    if (/[a-z]/.test(char)) return String.fromCharCode(219 - uniCode);
+    else return char;
+  };
   var answer;
   return answer;
 }
-
 module.exports = problem4;
