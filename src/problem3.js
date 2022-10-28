ae49c87e2a8c1159d459841b7e358888b825c871
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-28 19:59:13 +0900

    feat: 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,21 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    const value = scanTarget(i);
+    answer += value;
+  }
+
   return answer;
 }
+
+const scanTarget = (number) => {
+  let resultValue;
+  const rNum = /3|6|9/g;
+  const resultArray = [...String(number).matchAll(rNum)];
+  resultValue = resultArray.length;
+
+  return resultValue;
+};
 
 module.exports = problem3;
 
@@ -9,6 +23,9 @@ module.exports = problem3;
  1. number이 주어지면
  2. 해당 숫자까지
  3. 숫자가 3, 6, 9를 포함하는 개수 count.
+
+ - 3,6,9 숫자 찾는 함수
+
 
  3, 6, 9를 몇 개 가지고 있는지 판별하는 함수.
  count 추가해서 더하면 될 듯.
