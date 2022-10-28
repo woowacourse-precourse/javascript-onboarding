ea9d10443ed405e8c539aa392082083b36f03e36
saram <yujun1018@naver.com>
2022-10-28 23:12:58 +0900

    [문제5] feat: 나눈 값의 몫을 순서대로 배열에 담는 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,8 @@
 function problem5(money) {
   const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
   currency.forEach((v) => {
-    money / v;
+    answer.push(Math.floor(money / v));
     money %= v;
   });
 }
