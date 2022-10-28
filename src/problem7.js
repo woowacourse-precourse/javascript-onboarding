0af977a4eb2f7b983c84f18f5aa751b1407e632e
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-29 03:00:40 +0900

    feat(problem7): 사용자와 친구 관계인 사람들 찾아 새로 생성한 배열에 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,30 @@
 function problem7(user, friends, visitors) {
   var answer;
+  // 사용자와 친구관계인 사람 찾기
+  let current_friends = [];
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (user == friends[i][j]) {
+        // j가 0이면 1로, 1이면 0으로 바꾸기
+        current_friends.push(friends[i][1 - j]);
+      }
+    }
+  }
+  console.log(current_friends);
   return answer;
 }
+
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 
 module.exports = problem7;
