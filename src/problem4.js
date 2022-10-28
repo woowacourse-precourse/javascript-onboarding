0b40322909d7f34d875d3e0b706326c31ad0c9d2
min <hororonoodle@gmail.com>
2022-10-28 23:04:51 +0900

    feat : Solve problem 4 using regular expressions and ASCII code

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,27 @@
+const reverse = (letter) => {
+  // 예외상황 :: 알파벳 외에 문자는 변환하지 않는다.
+  if (letter.match(/[^a-z]/i)) return letter;
+
+  // 소문자일 때 알파벳을 반대로 변환
+  if (letter >= 'a' && letter <= 'z') {
+    return String.fromCharCode(
+      'z'.charCodeAt(0) - letter.charCodeAt(0) + 'a'.charCodeAt(0)
+    );
+  }
+
+  // 대문자일 때 알파벳을 반대로 변환
+  if (letter >= 'A' && letter <= 'Z') {
+    return String.fromCharCode(
+      'Z'.charCodeAt(0) - letter.charCodeAt(0) + 'A'.charCodeAt(0)
+    );
+  }
+};
+
 function problem4(word) {
-  var answer;
+  const answer = word.replace(/./g, (letter) => {
+    return reverse(letter);
+  });
+
   return answer;
 }
 
