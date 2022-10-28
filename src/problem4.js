01d46ef48ac455be6fde5c8a9860d188a8ad29b9
insu12021202 <insu12021202@gmail.com>
2022-10-28 12:38:23 +0900

    Feat:problem 4 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,30 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const bigWords = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const reverseBigWords = bigWords.split("").reverse().join("");
+  const smallWords = bigWords.toLowerCase();
+  const reverseSmallWords = smallWords.split("").reverse().join("");
+
+  let index;
+  let result = new Array();
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === " ") {
+      //공백일 때
+      result.push(" ");
+    } else if (word[i] === word[i].toUpperCase()) {
+      // 대문자일 때
+      index = bigWords.indexOf(word[i]);
+      result.push(reverseBigWords[index]);
+    } else {
+      //소문자일 때
+      index = smallWords.indexOf(word[i]);
+      result.push(reverseSmallWords[index]);
+    }
+  }
+
+  return result.join("");
 }
+
+problem4("I love you");
 
 module.exports = problem4;
