676c763a6b0df522516414bd5078cddad24093e9
sery <yunsel2@gmail.com>
2022-10-28 12:37:35 +0900

    feat(Problem5): Handle exeptional case

--- src/problem5.js
+++ src/problem5.js
@@ -4,6 +4,11 @@ function problem5(money) {
   let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   let price = money;
 
+  if (money < 1 || money > 1000000) {
+    console.error('money는 1 이상 1,000,000 이하입니다');
+    return;
+  }
+
   moneyUnit.forEach((unit, index) => {
     if (price >= unit) {
       const count = Math.floor(price / unit);
