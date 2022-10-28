3922d0cbaea279068c32e7be6b9482688a701910
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-28 13:56:16 +0900

    feat: 아스키 코드를 넣어 알파벳 배열의 인덱스를 추출하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,22 @@ function problem4(word) {
   return answer;
 }
 
+function upperCase(number) {
+  let alpabet = [];
+
+  for (let i = 65; i <= 90; i++) {
+    alpabet.push(i);
+  }
+  return alpabet.indexOf(number);
+}
+
+function lowerCase(number) {
+  let alpabet = [];
+
+  for (let i = 97; i <= 122; i++) {
+    alpabet.push(i);
+  }
+  return alpabet.indexOf(number);
+}
+
 module.exports = problem4;
