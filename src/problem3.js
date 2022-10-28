0ad927bd27dddcd15c4fe70faa05c77cfdc38bf3
김희진 <choha941108@gmail.com>
2022-10-28 16:07:02 +0900

    (3번) 숫자입력시 1부터 숫자까지 자리수별로 쪼개서 배열처리

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var answer ;
+  let number_add = [];
+
+  console.log(number)
+
+  for (i = 1 ; i <= number ; i++) {
+    let n = i
+    do {
+      number_add.push(n % 10);  
+      n = Math.floor(n / 10);  
+    } while (n > 0);  
+  }
+  console.log("숫자 확인",number_add)
+  //숫자가 입력됬을때 각 자리수 배열로 넣기
+
 }
 
 module.exports = problem3;
