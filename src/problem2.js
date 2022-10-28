743ac15d94e4edd10fbb95c957f5d680d8a87a09
thewronghand <penfreak77@gmail.com>
2022-10-28 14:31:41 +0900

    feat: 기능 목록 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
+  let array = cryptogram.split('');
+  let last = [];
+  let answer = [];
+  for (i=0; i <= array.length; i++){
+  if(array[i] !== last[0]){
+      answer.push(array[i]);
+      last.pop();
+      last.push(array[i]);
+  } 
+  answer.pop();
+  last.push(answer[answer.legth-1])
+  last.shift();
+}
   return answer;
 }
 
