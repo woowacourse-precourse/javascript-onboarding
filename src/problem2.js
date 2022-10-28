f6c910459b9ccece6fa09ed3f9d42eff2d04709b
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-28 22:10:39 +0900

    feat: PROBLEM 2 문자열 길이, 알파벳 대문자 예외 사항

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const check = (cryptogram) => {
+    const lowerCaseStr = cryptogram.toLowerCase();
+    if (cryptogram.length < 1 ) return false;
+    if (cryptogram.length > 1000) return false;
+    if (cryptogram !== lowerCaseStr) return false;
+    return true;
+  }
 }
 
 module.exports = problem2;
