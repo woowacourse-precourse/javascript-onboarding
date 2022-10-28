3c4ad62589c49e0cbe0fdaef810786ed69f9fd99
freemoon99 <soloist132@gmail.com>
2022-10-28 21:41:48 +0900

    feat[problem2]: 현재 글자와 이전 글자 비교 하여 해독 기능

--- src/problem2.js
+++ src/problem2.js
@@ -6,7 +6,12 @@ function problem2(cryptogram) {
   for (var i = 1; i < cryptogram.length; i++) {
     const curr = cryptogram[i];
     const prev = code.length - 1;
+
+    if (code[prev] === curr) {
+      code.pop();
+    } else code.push(curr);
   }
+  return code.join("");
   return answer;
 }
 
