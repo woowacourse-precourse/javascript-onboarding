df48667801877186c8322e1309f62c2863e0aa8c
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-10-28 17:45:41 +0900

    Submit : problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  console.log('this is problem 3')
+  for ( let i = 1 ; i < number+1 ; i ++ ) {
+    // 3, 6, 9의 개수만큼 손뼉을 쳐야 한다.
+    for ( let j = 0 ; j < String(i).length ; j ++ ) {
+      if ( String(i)[j].includes('3') || String(i)[j].includes('6') || String(i)[j].includes('9') ) {
+        answer += 1
+      }
+    }
+  }
+  console.log(answer) 
   return answer;
 }
 
