fd71e266bc956d0b9785b148b56ae926c8d7e056
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-29 05:23:23 +0900

    feat: Change variable name

--- src/problem5.js
+++ src/problem5.js
@@ -8,15 +8,16 @@ module.exports = problem5;
 
 const calcUnit = (money) => {
   let resultValue;
-  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  resultValue = unit.map((v, i) => {
+  const unitArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  resultValue = unitArray.map((v, i) => {
     let unitCount;
     if (i !== 0) {
-      const charge = money % unit[i - 1];
+      const charge = money % unitArray[i - 1];
       unitCount = parseInt(charge / v);
     } else {
       unitCount = parseInt(money / v);
     }
+
     return unitCount;
   });
 
@@ -24,8 +25,8 @@ const calcUnit = (money) => {
 };
 
 /*
- const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+ const unitArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
  1. 값을 나눈 몫이 0이 아닌 경우, 계산 후 저장 및 값 빼기.
- 2. unit.length만큼 반복
+ 2. unitArray.length만큼 반복
 
  */
