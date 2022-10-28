5f7acd2d3f7a5981d5f9ea36caa04bb411a08f92
JH <vvaa277@gmail.com>
2022-10-28 17:01:42 +0900

    Docs : 기능목록수정 Feat : 기능구현
    #P5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,31 @@
 function problem5(money) {
-  
+  const num = money;
+  const str = String(num);
+  const mapfn = (arg) => Number(arg);
+  const moneyArr = str.split("").map(mapfn);
+  const wallet = [];
+
+  moneyArr.forEach((el, i) => {
+    if (el === moneyArr[moneyArr.length - 1] && moneyArr[i - 1] !== 0) {
+      wallet.push(moneyArr[moneyArr.length - 1]);
+    }
+    if (el / 5 > 0 && el / 1 > 0 && el !== moneyArr[moneyArr.length - 1]) {
+      wallet.push(Math.floor(el / 5));
+    }
+    if (el / 5 < 1 && el / 1 > 0) {
+      wallet.push(Math.floor(el / 1));
+    }
+    if (moneyArr[i - 1] % 5 === 0 && el / 1 < 1 && moneyArr[i - 1] > 0) {
+      wallet.push(0);
+    }
+    if (moneyArr[i - 1] % 5 === 0 && el / 5 < 1 && el / 1 < 1) {
+      wallet.push(0);
+    }
+    if (el === 0 && moneyArr[i - 1] !== 0) {
+      wallet.push(0);
+    }
+  });
+  return wallet;
 }
 
 module.exports = problem5;
