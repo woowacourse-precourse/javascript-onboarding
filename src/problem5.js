70777ed95d0cd58303ca166a3cecac1f0acfb324
sunub <bsc5672@gmail.com>
2022-10-29 00:23:30 +0900

    money counts problem
    I solved this problem by dividing two sections, assigning empty arrays, and using indexes.

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,38 @@
 function problem5(money) {
-  var answer;
+  const fiveCounts = Array(4).fill(0);
+  const tenCounts = Array(5).fill(0);
+
+  const moneyCount = money.toString().split("");
+  const seperate = moneyCount.splice(0, moneyCount.length - 4);
+
+  let j = 1;
+  for (let i = 0; i < moneyCount.length; i++) {
+    if (moneyCount[i] % 5 === 0 && moneyCount[i] != 0 && i < 2) {
+      fiveCounts[j] += 1;
+    } else {
+      tenCounts[j] = moneyCount[i] * 1;
+    }
+    j++;
+  }
+
+  if (seperate.length > 1) {
+    fiveCounts[0] = Math.floor(seperate.join("") / 5);
+    tenCounts[0] = seperate.join("") % 5;
+  } else {
+    if (seperate.join("") % 5 === 0) {
+      fiveCounts[0] += 1;
+    } else {
+      tenCounts[0] = seperate[0] * 1;
+    }
+  }
+
+  const answer = [];
+  for (let i = 0; i < 5; i++) {
+    if (i < 4) {
+      answer.push(fiveCounts[i]);
+    }
+    answer.push(tenCounts[i]);
+  }
   return answer;
 }
 
