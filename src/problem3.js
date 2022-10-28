b928bbfd56ff36be986ee62acd8c9c050391f59c
lsy20140 <lsy20140@naver.com>
2022-10-28 10:07:41 +0900

    feat: [문제3] 각 숫자에 3,6,9 들어가는 횟수 출력

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
+function checkNum(number){
+  let count = 0;
+  
+  for(let i = 1 ; i < number + 1 ; i++){
+    const numArr = (i + '').split('');
+    for(let j = 0 ; j<numArr.length ; j++){
+      if(numArr[j]=="3" || numArr[j]=="6" || numArr[j]=="9") count++;
+    }
+  }
+  return count;
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+
+  return checkNum(number);
+
 }
 
 module.exports = problem3;
