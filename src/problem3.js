f94822c080827dfdbe80711807260de1dd37c4f6
woo-jk <201702042@o.cnu.ac.kr>
2022-10-29 00:22:43 +0900

    feat:[PROBLEM3] 3,6,9 개수 구하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,16 @@
+const isThreeSixNine = (number) => {
+  const isThreeSixNine = false;
+  if (number === "3" || number === "6" || number === "9") isThreeSixNine = true;
+  return isThreeSixNine;
+};
+const countThreeSixNine = (number) => {
+  let count = 0;
+  const numberToString = number.toString();
+  for (eachNumber of numberToString) {
+    if (isThreeSixNine(eachNumber)) count++;
+  }
+  return count;
+};
 function problem3(number) {
   var answer;
   return answer;
