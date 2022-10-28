749a7f5729f632c16790dcaaaef119e3d179753c
ashleysyheo <ashleysyheo@gmail.com>
2022-10-28 16:40:34 +0900

    feat(problem7): get friend recommendation

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,25 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const recommendationScore = new Map();
+  const friendsSet = new Set();
+
+  for (let i = 0; i < friends.length; i += 1) {
+    if (friendsSet.has(friends[i][1]) || friends[i][1] === user) continue;
+
+    friendsSet.add(friends[i][0]);
+    recommendationScore.set(friends[i][1], recommendationScore.get(friends[i][1]) + 10 || 10);
+  }
+
+  for (let i = 0; i < visitors.length; i += 1) {
+    if (friendsSet.has(visitors[i]) || visitors[i] === user) continue;
+
+    recommendationScore.set(visitors[i], recommendationScore.get(visitors[i]) + 1 || 1);
+  }
+
+  const recommendationArray = Array.from(recommendationScore.keys()).sort(
+    (a, b) => recommendationScore.get(b) - recommendationScore.get(a) || a.localeCompare(b)
+  );
+
+  return recommendationArray;
 }
 
 module.exports = problem7;
