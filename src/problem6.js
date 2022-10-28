0775a94675a19c59b91e757757c061e62438ea4a
Sijun <sionwer4@naver.com>
2022-10-28 17:37:02 +0900

    problem6 solved

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let word;
+  let result = [];
+  for(i = 0; i < forms.length; i++){
+    if(!forms[i]) continue;
+    for(j = 0; j < forms[i][1].length - 1; j++){
+      word = forms[i][1].substring(j, j + 2);
+      for(k = i + 1; k < forms.length; k++){
+        if(!forms[k]) continue;
+        if(forms[k][1].includes(word)){
+          result.push(forms[k][0]);
+          if(!result.includes(forms[i][0])){
+            result.push(forms[i][0]);
+          }
+          delete(forms[k]);
+        }
+      }
+    }
+    if(result.includes(forms[i][0])){
+      delete(forms[i]);
+    }
+  }
+  return result;
 }
 
 module.exports = problem6;
