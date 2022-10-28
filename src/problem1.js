ff0ded8b9500c75d4463ca8845bf152e0bc2c8b9
Yesong <to06109@naver.com>
2022-10-28 17:40:34 +0900

    [1주차 1번] FEAT: 각 자리숫자 더하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,17 @@
+function getSum(num) {
+  let result = 0;
+  for (let letter of num.toString()) {
+    result += parseInt(letter);
+  }
+  return result;
+}
+
 function problem1(pobi, crong) {
   const ERROR = -1;
+  const TIE = 0;
+  const POBI = 1;
+  const CRONG = 2;
+
   if (pobi[1] != pobi[0] + 1) return ERROR;
   else if (crong[1] != crong[0] + 1) return ERROR;
 
