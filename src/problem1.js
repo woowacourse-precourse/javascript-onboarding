6ce20d6978d96aa8a20f41da48ab6d7e85c6dc1c
chex <chex417@naver.com>
2022-10-29 01:59:55 +0900

    feat: 페이지 번호를 입력받아 가장 큰 수를 구하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -16,4 +16,18 @@ function isException(pages) {
   return false;
 }
 
+function getMaxNumByPage(page){
+  let candidate_plus = 0
+  let candidate_mult = 1
+  while (page!=0) {
+    candidate_plus += page % 10;
+    candidate_mult *= page % 10;
+    page = parseInt(page /= 10);
+  }
+  return Math.max(candidate_plus, candidate_mult);
+}
+
 module.exports = problem1;
+const arr1 = [3, 4];
+const arr2 = [3, 4];
+console.log(problem1(arr1, arr2));
