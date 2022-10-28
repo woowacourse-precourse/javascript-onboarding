9349e1d12bf1d3cae3e6558fe5ce8916073a6985
RooT_Kim <bdd3452@gmail.com>
2022-10-29 07:55:19 +0900

    문제4번) 기능목록 정리 및 함수 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  var answer;
-  return answer;
+	//알파벳인지 판별하는 함수 -> charFilter
+	//대문자, 소문자를 구별하는 함수 -> alphabetFilter
+	//대문자, 소문자 별로 알파벳을 반전하는 함수 -> reverseAlphabet
+
+	return charFilter(word);
 }
+
+function charFilter() {}
+
+function alphabetFilter() {}
+
+function reverseAlphabet() {}
 
 module.exports = problem4;
