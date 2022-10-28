808fc4ba7a8ef9c5752b2662f5c028c976f117c8
pp449 <mma7710@naver.com>
2022-10-29 02:29:35 +0900

    [문제 5] : 금액별로 나눈 몫으로 돈의 잔량을 구하는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,19 +1,17 @@
 const moneyChanger = (money) => {
-  let monetaryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const monetaryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const amountMoney = [];
+
   for(let i=0; i<monetaryUnit.length; i++) {
     let numberOfMoney = parseInt(money/monetaryUnit[i]);
     money = parseInt(money%monetaryUnit[i]);
-    
-    console.log(numberOfMoney);
+    amountMoney.push(numberOfMoney);
   }
-  console.log();
+  return amountMoney;
 }
 
 function problem5(money) {
-  moneyChanger(money);
+  return moneyChanger(money);
 }
-
-problem5(50237);
-problem5(15000);
 
 module.exports = problem5;
