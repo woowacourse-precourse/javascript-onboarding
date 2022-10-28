58d34acc38dc986bcc7154904306bfe9bd1f2a86
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-28 16:14:46 +0900

    refactor(problem6): answer배열에 이메일 추가하는 함수 따로 구현

--- src/problem6.js
+++ src/problem6.js
@@ -2,15 +2,21 @@ function problem6(forms) {
   var answer = [];
 
   const includeSameWord = (name1, name2) => {
-    for (let k = 0; k < name1.length - 1; k++) {
-      for (let l = 0; l < name2.length - 1; l++) {
-        if (name1[k] == name2[l] && name1[k + 1] == name2[l + 1]) {
+    for (let i = 0; i < name1.length - 1; i++) {
+      for (let j = 0; j < name2.length - 1; j++) {
+        if (name1[i] == name2[j] && name1[i + 1] == name2[j + 1]) {
           return true;
         }
       }
     }
     return false;
   };
+
+  const pushEmail = (email1, email2) => {
+    answer.push(email1);
+    answer.push(email2);
+  };
+
   // 두명씩 비교하기 위해 크루의 이메일, 닉네임 순회
   for (let i = 0; i < forms.length; i++) {
     let email_of_crew1 = forms[i][0];
@@ -22,8 +28,7 @@ function problem6(forms) {
       // 두 크루의 닉네임에 길이 2 이상의 중복 단어 있는지 확인
       if (includeSameWord(nickName_of_crew1, nickName_of_crew2)) {
         // answer에 해당 이메일 추가하기
-        answer.push(email_of_crew1);
-        answer.push(email_of_crew2);
+        pushEmail(email_of_crew1, email_of_crew2);
       }
     }
   }
