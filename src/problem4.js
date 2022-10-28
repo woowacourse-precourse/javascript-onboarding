bb9ab3ded36adbf0e0a8d4fca96bce139685ec52
coolIlive <galilee155@naver.com>
2022-10-29 00:18:24 +0900

    FEAT(problem4): 아스키코드 값을 통해 각 문자를 변환한다
    아스키값으로 대문자일경우, 소문자일경우, 그리고 문자가 아닐 경우를 나누고 문자를 하나씩 변환하여 answer에 추가함

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
 function problem4(word) {
-  var answer;
+  let answer='';
+
+  // 각 문자의 아스키값을 검사한다
+  for (let alphabet of word) {
+    // 알파벳의 아스키값을 통해 계산 _대소문자 따로
+    let asciiCode=alphabet.charCodeAt(0)
+    if (65<=asciiCode && asciiCode<=90) answer+=String.fromCharCode(90-(asciiCode-65));
+    else if (97<=asciiCode && asciiCode<=122) answer+=String.fromCharCode(122-(asciiCode-97));
+    else answer+=alphabet;
+  }
+
   return answer;
 }
 
