bfd46c278b389e0f6835b65e5edbe515b0800f9f
kangyeongmin <kym6050@hanyang.ac.kr>
2022-10-28 12:59:53 +0900

    [문제7] 친구의 친구를 찾는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -10,4 +10,20 @@ function findFriendsOfUser(user, friends) {
   return friendsArray;
 }
 
+function findFriendsOfFriend(user, friendsOfUser, friends) {
+  let friendsOfFriend = [];
+
+  friends.map((friend) => {
+    friendsOfUser.map((myFriend) => {
+      if (friend.includes(myFriend) && !friend.includes(user)) {
+        let newArray = friend.filter((element) => element !== myFriend);
+        if (!friendsOfFriend.includes(newArray[0])) {
+          friendsOfFriend.push(newArray[0]);
+        }
+      }
+    });
+  });
+  return friendsOfFriend;
+}
+
 module.exports = problem7;
