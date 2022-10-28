a779a851a57168f89011adfc30bb8b959e03ae0e
HJeong1200 <hjeong1200@gmail.com>
2022-10-28 16:44:52 +0900

    02-modification-pseudo-code

--- src/problem2.js
+++ src/problem2.js
@@ -28,4 +28,8 @@ module.exports = problem2;
  *  3) stack에서 pop된 경우 현재 문자도 stack에 추가하면 안됨 - duplicate 변수 추가
  *  4) duplicate === false인 경우에만 stack에 추가
  *  5) answer = stack.join("")
+ *    => 'zyellleyz'처럼 문자가 홀수 번 반복되면 제거되지 않는 케이스 발생
+ *
+ *  3) stack에서 pop된 문자를 deletedWord에 할당
+ *  4) deletedWord !== cryptogram[i]인 경우에만 stack에 추가하며, deletedWord = ''으로 재할당
  */
