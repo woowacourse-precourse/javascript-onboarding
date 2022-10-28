d3f2ae1a88111fca881e96907f2bd1ddee71ecfa
d0422 <rlfehd2013@naver.com>
2022-10-28 16:59:18 +0900

    Feat:아스키 코드 기반으로 대소문자를 뒤집는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,5 @@
+const UPPERCASE_REVERSE_NUMBER = 155;
+const LOWERCASE_REVERSE_NUMBER = 219;
 function problem4(word) {
   var answer;
   return answer;
@@ -8,5 +10,13 @@ function getAsciiFromString(char) {
 function getStringFromString(ascii) {
   return String.fromCharCode(ascii);
 }
-
+function reverseChar(char) {
+  const asciiNum = getAsciiFromString(char);
+  let resultAscii = asciiNum;
+  if (asciiNum >= 65 && asciiNum <= 90)
+    resultAscii = UPPERCASE_REVERSE_NUMBER - asciiNum;
+  if (asciiNum >= 97 && asciiNum <= 122)
+    resultAscii = LOWERCASE_REVERSE_NUMBER - asciiNum;
+  return getStringFromString(resultAscii);
+}
 module.exports = problem4;
