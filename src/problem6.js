2d0002f42e268c368f1ca2e480c6eff30ce6a927
wonkyung <mwk0725@gmail.com>
2022-10-28 18:06:18 +0900

    feat: [기능 0] 두 글자씩 자른 목록 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let members = [...forms];
+  let membersName = [];
+
+  for (let i = 0; i < members.length; i++) {
+    membersName.push(members[i][1]);
+  }
+
+  const splitName = name => {
+    let nameList = [];
+    for (let i = 0; i < name.length - 1; i++) {
+      nameList.push(name.substring(i, i + 2));
+    }
+    return nameList;
+  };
 }
 
 module.exports = problem6;
