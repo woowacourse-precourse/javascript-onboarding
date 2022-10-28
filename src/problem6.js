d78f437e27644c6ee2de3597da7d4b718d7b235d
sungjihyun <jhsung23@gmail.com>
2022-10-28 16:58:45 +0900

    Feat: problem 6: 중복된 요소 찾아주는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -7,12 +7,26 @@ const makeTokenArray = (nickname) => {
   return tokenArray;
 };
 
+const findDuplicate = (array) => {
+  let distinctSet = new Set(array);
+  const duplicates = array.filter((item) => {
+    if (distinctSet.has(item)) {
+      distinctSet.delete(item);
+    } else {
+      return item;
+    }
+  });
+  return [...new Set(duplicates)];
+};
+
 function problem6(forms) {
   var answer = [];
 
   const nicknameTokens = forms.reduce((acc, [email, nickname]) => {
     return acc.concat(makeTokenArray(nickname));
   }, []);
+
+  const duplicatedTokens = findDuplicate(nicknameTokens);
 
   return answer;
 }
