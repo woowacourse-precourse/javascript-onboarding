270d845d23ea2ac56edc891510c1a03fdd724ffe
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-29 00:10:30 +0900

    feat: implement createUserFriendObject function

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,22 @@
+function createUserFriendObject(user, friends) {
+  const TOTAL_INDEX = 1;
+  const initialFriendObject = {};
+  friends.forEach(friend => {
+    const userIndex = friend.indexOf(user);
+
+    if (userIndex !== -1) {
+      const friendIndex = TOTAL_INDEX - userIndex;
+      const friendName = friend[friendIndex];
+      initialFriendObject[friendName] = [];
+    }
+  });
+
+  return initialFriendObject;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  const userFriendObject = createUserFriendObject(user, friends);
   return answer;
 }
 
