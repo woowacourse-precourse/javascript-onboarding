cfd3d7160e81bdcfa4147b6033f1df4fe1785c19
rabBit64 <gojh47@gmail.com>
2022-10-28 15:17:47 +0900

    기능목록1: 알파벳 사전 저장

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
+function alphaDict(){
+  var alphaRev = {}
+  for(var i=0; i<26; i++){
+    var smallStart="abcdefghijklmnopqrstuvwxyz".charAt(i);
+    var smallEnd="abcdefghijklmnopqrstuvwxyz".charAt(25-i);
+    var bigStart="ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(i);
+    var bigEnd="ABCDEFGHIJKLMNOPQRSTUVWXYZ".charAt(25-i);
+    alphaRev[smallStart]=smallEnd;
+    alphaRev[bigStart]=bigEnd;
+  }
+  return alphaRev;
+}
 function problem4(word) {
-  var answer;
+  var answer="";
+  //기능목록1: 알파벳 저장
+  alphaRev=alphaDict();
+
   return answer;
 }
-
 module.exports = problem4;
