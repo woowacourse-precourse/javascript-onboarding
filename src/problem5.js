f163efcaf9c79de0d0da75c7c128052ba5bf3437
BBIYAC <kiy7833@gmail.com>
2022-10-28 14:51:39 +0900

    feat(problem5): add wallet feature
    - 돈을 변환하여 각 금액의 개수를 반환하는 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
+// 돈을 변환하여 각 금액의 개수를 반환하는 기능
 function problem5(money) {
-  var answer;
-  return answer;
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let wallet = [];
+
+  for (let coin of coins) {
+    wallet.push(parseInt(money / coin));
+    money %= coin;
+  }
+  
+  return wallet;
 }
 
 module.exports = problem5;
