5ebe4ed1e5fcdfba04e0bac4a762a41683021697
GuHyeonWoo <ghw9174@gmail.com>
2022-10-29 00:21:30 +0900

    P6/이메일 예외처리 추가 및 닉네임 예외처리 구현

--- src/problem6.js
+++ src/problem6.js
@@ -6,20 +6,29 @@
 // 5. 중복된 인덱스에 따라 이메일을 반환
 // 6. 중복되는 경우가 2가지 이상인 경우 -> 리스트를 개수에 맞게 반환
 
+// 정규식을 통해 닉네임 예외처리
+function exceptionNick(nick){
+  // 닉네임에 자음 or 모음만 들어갈 경우가 아닌 오직 한글로 이루어진 경우만 확인
+  const nickRegExp = new RegExp("[가-힣]+$");
+  // 닉네임 정규식 및 길이 확인
+  return nickRegExp.test(nick) && nick.length >= 1 && nick.length < 20?true:false;
+}
+
 // 정규식을 통해 이메일 예외처리
 function exceptionEmail(email) {
   // 'RFC 5322 형식 + email.com'으로 이메일 확인
-  const regExp = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@email.com");
-  // 정규식에 충족하면 true, 불충족하면 false 반환
-  return regExp.test(email);
+  const emailRegExp = new RegExp("([!#-'*+/-9=?A-Z^-~-]+(\.[!#-'*+/-9=?A-Z^-~-]+)*|\"\(\[\]!#-[^-~ \t]|(\\[\t -~]))+\")@email.com");
+  // 정규식에 및 길이 확인
+  return emailRegExp.test(email) && email.length >= 11 && email.length < 20?true:false;
 }
 
 function problem6(forms) {
   let answer;
 
   for([email, nick] of forms){
-    // 이메일 예외처리
-    let check_email = exceptionEmail(email);
+    // 이메일, 닉네임 예외처리
+    const check_email = exceptionEmail(email);
+    const check_nick = exceptionNick(nick);
   }
   
   return answer;
