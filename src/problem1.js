2d22b9270c30c1ccd0ee127534a9280f658419c6
chex <chex417@naver.com>
2022-10-29 01:14:35 +0900

    feat: 예외사항을 처리하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
   var answer;
+
+  // 예외사항 확인
+  if (isException(pobi) || isException(crong)){
+    return -1;
+  }
+
   return answer;
+}
+
+function isException(pages) {
+  if (pages[1]-pages[0] != 1) {
+    return true;
+  }
+  return false;
 }
 
 module.exports = problem1;
