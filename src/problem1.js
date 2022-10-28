50716fdef7c2e9ea52ef2e1971d49e40f5eb6b1a
DongWooKim97 <nanaukim@naver.com>
2022-10-28 16:27:43 +0900

    feat(P1): 입력값에 대한 예외 처리 및 검증 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,14 @@
 function problem1(pobi, crong) {
-  var answer;
+  const answer = makeMaxNumber(pobi, crong)
   return answer;
+}
+
+
+function makeMaxNumber(pobi, crong) {
+  // 입력값에 대한 각각의 예외처리 우선시!
+  if (!checkValid(pobi) || !checkValid(crong)) return -1
+  return 1
+  // 이후 메인 로직()
 }
 
 module.exports = problem1;
