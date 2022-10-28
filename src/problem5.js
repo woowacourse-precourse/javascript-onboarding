04e2a89bba37dbce7f4c52365debabc925261bf4
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-28 17:56:06 +0900

    feat: (P5) 화폐의 종류를 가진 배열 및 변수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,21 @@
 function problem5(money) {
-  var answer;
+  let answer = countMoneyType(money);
   return answer;
 }
+
+//화폐의 수를 구하는 기능
+function countMoneyType(money) {
+  //화폐의 종류를 가진 배열 선언
+  const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let currentMoney = money;
+  let countMoney = [];
+
+  //단위가 큰 화폐부터 순회
+
+  //현재 금액을 화폐로 나눠 몫을 구함
+
+  //나머지 값을 현재 금액으로 최신화
+}
+return countMoney;
 
 module.exports = problem5;
