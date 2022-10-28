a4a372eac6df94fc4ec72ac05aaa46986598ad2b
박정규 <srk99279@naver.com>
2022-10-28 16:04:20 +0900

    문제 4 구현. 정규식 이용하여 풀이.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  const lowerRegex = /[a-z]/;
+  const upperRegex = /[A-Z]/;
+
+  for (let spell of word) {
+    if (upperRegex.test(spell)) {
+      let inverseUpperSpell =
+        spell.charCodeAt() +
+        ("Z".charCodeAt() - spell.charCodeAt()) -
+        (spell.charCodeAt() - "A".charCodeAt());
+      answer += String.fromCharCode(inverseUpperSpell);
+    } else if (lowerRegex.test(spell)) {
+      let inverseLowerSpell =
+        spell.charCodeAt() +
+        ("z".charCodeAt() - spell.charCodeAt()) -
+        (spell.charCodeAt() - "a".charCodeAt());
+      answer += String.fromCharCode(inverseLowerSpell);
+    } else if (!upperRegex.test(spell) && !lowerRegex.test(spell))
+      answer += spell;
+  }
   return answer;
 }
+
+console.log(problem4("I love you"));
 
 module.exports = problem4;
