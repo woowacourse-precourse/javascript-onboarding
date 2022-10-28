a6cc23c7bba44ad13bfa41705773dac3da5932dc
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-28 12:35:08 +0900

    problem6 substr 함수 구조 변경 및 닉네임 추가 방식 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,25 +1,28 @@
 function problem6(forms) {
   var answer;
   let duplicatenickname = []
-  substr(forms[0])
+  for (let k = 0; k<forms.length; k++){
+    substr(forms[k])
+  }
   for (let k = 1; k<forms.length; k++){
     for (let p = 0; p<includenickname.length; p++){
       if (forms[k][1].includes(includenickname[p])){
         duplicatenickname.push(includenickname[p]);
       }
     }
-    substr(forms[k])
   }
   return answer;
 }
 function substr(user){
+  let temporarynickname = []
   for (let i = 0; i<user[1].length;i++){
     for (let j = 2; j<=user[1].length; j++){
       if (j+i <= user[1].length){
-        includenickname.push(user[1].substr(i,j))
+        temporarynickname.push(user[1].substr(i,j))
       }
     }
   }
+  includenickname.push(temporarynickname)
 }
 var includenickname = []
 console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
