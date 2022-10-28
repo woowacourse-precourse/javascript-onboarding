23c6820d028843e994054902dec5dd34fc471e1e
배병재 <39183208+BaeByungjae@users.noreply.github.com>
2022-10-28 22:08:37 +0900

    문제6 풀이

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const answer = [];
+  const wordDict = {};
+  forms.forEach((form) => {
+    const user = form[1];
+    for (let i = 2; i <= user.length; i++) {
+      for (let j = 0; j <= user.length - i; j++) {
+        const subStr = user.substr(j, i);
+        forms.forEach((data) => {
+          if (data[1] !== user && data[1].includes(subStr)) {
+            if (!wordDict[data[1]]) {
+              wordDict[data[1]] = true;
+              answer.push(data[0]);
+            }
+          }
+        });
+      }
+    }
+  });
+  return answer.sort();
 }
 
 module.exports = problem6;
