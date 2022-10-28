57185c1193754dffac4989593fc4a37a78d2cc38
Dean <yjj052800@gmail.com>
2022-10-28 16:01:15 +0900

    FEAT: [3번] 예외사항 처리 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,15 +1,24 @@
 function problem3(number) {
-  var answer;
-  let cnt = 0;
-  for (i = 1; i <= number; i++) {
-    let numberCompare = String(i).match(/[369]/g);
-    console.log(numberCompare);
-    if (numberCompare != null) cnt += numberCompare.length;
+  if (inputExecption(number)) {
+    var answer;
+    let cnt = 0;
+    for (i = 1; i <= number; i++) {
+      let numberCompare = String(i).match(/[369]/g);
+      console.log(numberCompare);
+      if (numberCompare != null) cnt += numberCompare.length;
+    }
+    answer = cnt;
+    
+    return answer;
   }
-  answer = cnt;
   
-  return answer;
 }
 
 console.log(problem3(33));
 module.exports = problem3;
+
+function inputExecption(num) {
+  let inputCheck = true;
+  if (num < 1 || num > 1000) inputCheck = false;
+  return inputCheck;
+}
