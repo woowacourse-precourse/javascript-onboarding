3fe6328210e49b9f068a7d9918c278cb1510d08a
dev-redo <mis05041@naver.com>
2022-10-28 14:49:36 +0900

    [Feat 6번] 중복 검사를 위해 2글자로 자른 닉네임의 개수를 구하는 Map 생성하는 util함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,14 +1,30 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const result = new Set();
+  const duplicateNickName = getSubStringNickNameMap(forms);
+
+  return duplicateNickName;
+}
+
+function getSubStringNickNameMap(forms) {
+  return forms.reduce((map, user) => {
+    const [_, nickname] = user;
+
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const substringNickName = nickname.substring(i, i + 2);
+      const currNicknameNum = map.get(substringNickName) || 0;
+      map.set(substringNickName, currNicknameNum + 1);
+    }
+
+    return map;
+  }, new Map());
 }
 
 // 문제 6번: Hash 풀이
-// 1. 자료구조 Map, Set 생성
+// 1. 자료구조 Map, Set 생성 ✅
 //   - Map: 2글자로 닉네임의 이름과 해당 이름의 개수를 저장
 //   - Set: 중복되는 Email 제거
 // 2. 닉네임 중복 여부 판별
-//    2.1. forms를 loop돌리면서 각 닉네임마다 2글자씩 substring 후 Map에 넣기
+//    2.1. forms를 loop돌리면서 각 닉네임마다 2글자씩 substring 후 Map에 넣기 ✅
 //    2.2. Map을 이용해 forms를 loop돌리면서 해당 유저의 닉네임의 substring이 Map에 2개 이상 있는지 판별
 //    2.3. 1개 이하 -> continue, 2개 이상 -> Set에 해당 유저의 이메일 add
 // 3. @return 중복된 유저의 email이 담긴 Set을 List로 변환
