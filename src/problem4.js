c048abce11048e5b7c0a199406de3f0a59e7eb92
easyone <thinkanddoit@naver.com>
2022-10-28 17:31:59 +0900

    #4 refactor: 함수 추출 및 코드 인라인, else 제거

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,12 @@
 function problem4(word) {
-  const reverseChar = (char) => {
-    const uniCode = char.charCodeAt();
-    if (/[A-Z]/.test(char)) return String.fromCharCode(155 - uniCode);
-    if (/[a-z]/.test(char)) return String.fromCharCode(219 - uniCode);
-    else return char;
-  };
   const answer = word.split("").map((char) => reverseChar(char));
   return answer.join("");
 }
+
+const reverseChar = (char) => {
+  if (/[A-Z]/.test(char)) return String.fromCharCode(155 - char.charCodeAt());
+  if (/[a-z]/.test(char)) return String.fromCharCode(219 - char.charCodeAt());
+  return char;
+};
+
 module.exports = problem4;
