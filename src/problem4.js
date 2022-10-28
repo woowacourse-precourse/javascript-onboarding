8dd775f3f0f329ad98abf072b83da244f4d84c39
xownsdl0 <xownsdl0@naver.com>
2022-10-28 15:05:55 +0900

    <P4> 대문자를 청개구리 사전의 대문자로 치환

--- src/problem4.js
+++ src/problem4.js
@@ -5,8 +5,13 @@ function problem4(word) {
   let replaceWord = "";
 
   for (let i of word) {
+    // i가 대문자인가.
     if (i === i.toUpperCase()) {
+      // 청개구리 사전의 알파벳으로 치환 후 replaceWord에 담는다.
+      let reChar = alphabet[reverseCase.indexOf(i)];
+      replaceWord += i.replace(i, reChar);
     }
+
     if (i === i.toLowerCase()) {
     }
   }
