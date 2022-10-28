0ba2e1d5fbe8a9d6046c2728de97716108e65a3d
ashleysheo <ashleysyheo@gmail.com>
2022-10-28 14:27:37 +0900

    feat(problem6): check common substring

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,19 @@
+function checkCommonSubstring(word1, word2) {
+  const dp = new Array(word1.length + 1).fill(0).map(() => new Array(word2.length + 1).fill(0));
+
+  for (let i = 1; i <= word1.length; i += 1) {
+    for (let j = 1; j <= word2.length; j += 1) {
+      if (word1[i - 1] === word2[j - 1]) {
+        dp[i][j] = dp[i - 1][j - 1] + 1; 
+        
+        if (dp[i][j] >= 2) return true; 
+      }
+    }
+  }
+
+  return false; 
+}
+
 function problem6(forms) {
   var answer;
   return answer;
