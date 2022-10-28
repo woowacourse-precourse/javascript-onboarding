5c24461eeb9ec05911121dc3e3ebcfbb7833bee2
woo-jk <201702042@o.cnu.ac.kr>
2022-10-28 23:19:22 +0900

    feat:PROBLEM2 반복문 만들기 및 테스트 통과

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,18 @@
+const duplicatedStringExist = (string) => {
+  const duplicateExist = string.search(/([a-zA-Z])\1/);
+  return duplicateExist !== -1;
+};
+
 const trimDuplicateString = (string) => {
   const trimedString = string.replace(/([a-zA-Z])\1+/g, "");
   return trimedString;
 };
 
 function problem2(cryptogram) {
-  var answer;
-  answer = trimDuplicateString(cryptogram);
-  return answer;
+  while (duplicatedStringExist(cryptogram)) {
+    cryptogram = trimDuplicateString(cryptogram);
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
