3377cb429cc5d97bfbba8a1e8fbb32b4d85d9290
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 17:12:14 +0900

    feat: implement get369Count function

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,15 @@
+function get369Count(num) {
+  let count = 0;
+  const stringNum = String(num);
+  for (i = 0; i < stringNum.length; i++) {
+    if (+stringNum[i] !== 0 && +stringNum[i] % 3 === 0) {
+      count++;
+    }
+  }
+
+  return count;
+}
+
 function problem3(number) {
   var answer;
   return answer;
