081fc7ef273f9dd7ca450bd184d955ac43000ee3
SeungWoo Kim <osabero@naver.com>
2022-10-28 16:25:15 +0900

    구현하기 위하여 일단 forEach를 이용하여 배열의 원소를 하나씩 호출하고 각 자리숫자를 곱하거나 더하기 위하여 전 for of 를 사용했습니다. for of를 사용하기 위하여 원소를 string으로 바꿔준후 다시 정수형으로 바꾸어 각자리 숫자를 더하거나 곱하는과정을 진행하였습니다.
    마지막에 각 값을 확인하여 answer값을 리턴하도록 하였습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -9,7 +9,40 @@ function problem1(pobi, crong) {
   if((pobi[1]-pobi[0])!==1) {return answer}
   if((crong[1]-crong[0])!==1) {return answer}
   //예외 처리
-  
+  var pobiValue = 0
+  var crongValue = 0
+
+  pobi.forEach((item)=>{
+    let addValue = 0
+    let mulValue = 1
+    let str = item.toString()
+    for (let i of str){
+      addValue += Number(i)
+    }
+    for (let j of str){
+      mulValue *= Number(j)
+    }
+    if (pobiValue < addValue){pobiValue = addValue}
+    if (pobiValue < mulValue){pobiValue = mulValue}
+    console.log(pobiValue)
+  })
+  crong.forEach((item)=>{
+    let addValue = 0
+    let mulValue = 1
+    let str = item.toString()
+    for (let i of str){
+      addValue += Number(i)
+    }
+    for (let j of str){
+      mulValue *= Number(j)
+    }
+    if (crongValue < addValue){crongValue = addValue}
+    if (crongValue < mulValue){crongValue = mulValue}
+    console.log(crongValue)
+  })
+  if (crongValue < pobiValue) {answer = 1}
+  if (crongValue == pobiValue) {answer = 0}
+  if (crongValue > pobiValue) {answer = 2}
 
   return answer;
 }
