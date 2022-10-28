7d240f5b8d5029688c746b4826a20058f4456fb3
Yesong <to06109@naver.com>
2022-10-28 17:25:01 +0900

    [1주차 1번] FEAT: 예외처리 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,8 @@
 function problem1(pobi, crong) {
+  const ERROR = -1;
+  if (pobi[1] != pobi[0] + 1) return ERROR;
+  else if (crong[1] != crong[0] + 1) return ERROR;
+
   var answer;
   return answer;
 }
