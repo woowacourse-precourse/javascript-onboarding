60879746e83243ea663e788390686aca0b94d186
Dean <yjj052800@gmail.com>
2022-10-28 23:59:34 +0900

    FEAT: [5번] 화폐 반환 기계 초안

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,18 @@
 function problem5(money) {
   var answer;
-
-  
+  let changeResult = [];
+  const changeMachine = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (i = 0; i <= changeMachine.length - 1; i++) {
+    let changeCnt = Math.floor(money / changeMachine[i]);
+    money -= (changeCnt * changeMachine[i]);
+    changeResult.push(changeCnt);
+  }
+  answer = changeResult;
   return answer;
 }
 
+
+
 module.exports = problem5;
+
+console.log(problem5(65000));
