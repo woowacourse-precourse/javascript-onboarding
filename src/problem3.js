31074691e4c956a4f6945a2b566a8d05541a58cc
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-29 03:04:53 +0900

    [problem3] Add createNumberArr function for make array

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,16 @@
+console.log(problem3(13));
+
 function problem3(number) {
   return game369App(number);
 }
 
 function game369App(number){
-  const arr = createNumberArr(number);
-  return getCount369(arr);
+  const eachNumbersArr = createNumberArr(number);
+  return getCount369(eachNumbersArr);
+}
+
+function createNumberArr(number){
+  return Array.from({length: number}, (_,idx)=>idx+1);
 }
 
 module.exports = problem3;
