fd9ccb0b7aa09b050689ed51532f75b7ae24ca89
DongWooKim97 <nanaukim@naver.com>
2022-10-28 16:43:21 +0900

    feat(P1): 예외처리를 위한 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -11,4 +11,39 @@ function makeMaxNumber(pobi, crong) {
   // 이후 메인 로직()
 }
 
+function checkValid(userName) {
+  if (!isDuplicate(userName)) return false
+  if (!isNotFitNumber(userName)) return false
+  if (!notInRange(userName)) return false
+  if (!betweenValue(userName)) return false
+  return true
+}
+
+function isDuplicate(userName) {
+  if (userName[0] === userName[1]) return false;
+  return true
+}
+
+function isNotFitNumber(userName) {
+  if (userName[0] % 2 !== 1 || userName[1] % 2 !== 0) return false
+  return true
+}
+
+function betweenValue(userName) {
+  if (userName[1] - userName[0] !== 1) return false;
+  return true
+}
+
+function notInRange(userName) {
+  if (!correctRange(userName[0]) || !correctRange(userName[1])) return false;
+  return true
+}
+
+function correctRange(pageNumber) {
+  const minPage = 1;
+  const maxPage = 400;
+  if (pageNumber >= minPage && pageNumber <= maxPage) return true
+  return false
+}
+
 module.exports = problem1;
