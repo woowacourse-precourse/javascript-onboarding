12b302cdfdaa2717e28534e4f8c755f77f5b489a
leeseooo <leeseoooo@naver.com>
2022-10-28 21:10:05 +0900

    feat: problem2 배열 순회하며 연속하는 문자 인덱스 찾고 배열에 저장

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while (1) {
+    let removeList = [];
+    cryptogram.split("").forEach((x, idx) => {
+      if (x === cryptogram[idx + 1]) {
+        removeList.push(idx);
+      }
+    });
+    // TODO: removeList에 담긴 index를 이용해서 배열을 조정
+  }
+  return cryptogram;
 }
+
+console.log(problem2("browoanoommnaon"));
+console.log(problem2('zyelleyz'))
 
 module.exports = problem2;
