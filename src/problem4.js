a09d3a69bacc3b212cbe1a48e453edb63cbef6cd
freemoon99 <soloist132@gmail.com>
2022-10-28 23:30:01 +0900

    feat[problem4]: 문자열을 아스키코드화

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,11 @@
 function problem4(word) {
   var answer;
   let arr = [...word];
-  
+  let result = [];
+
+  for(let i=0;i<arr.length;i++){
+    const ascii = arr[i].charCodeAt(0);
+  }
   return answer;
 }
 
