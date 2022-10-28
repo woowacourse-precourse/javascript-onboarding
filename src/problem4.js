44438c6496ded12c4cfb220a27d18f833153951a
woo-jk <201702042@o.cnu.ac.kr>
2022-10-29 02:18:17 +0900

    feat:[PROBLEM4]아스키 코드를 사용해 문자열 값을 바꾸는 로직

--- src/problem4.js
+++ src/problem4.js
@@ -6,7 +6,25 @@ const isUpperCase = (char) => {
 };
 
 function problem4(word) {
-  let answer;
+  const UPPER_ASCII_A_Z = 155;
+  const LOWER_ASCII_A_Z = 219;
+
+  let answer = "";
+  for (let currentChar = 0; currentChar < word.length; currentChar++) {
+    if (!isAlphabet(word[currentChar])) {
+      answer += word[currentChar];
+      continue;
+    }
+    if (isUpperCase(word[currentChar]))
+      answer += String.fromCharCode(
+        UPPER_ASCII_A_Z - word.charCodeAt(currentChar)
+      );
+
+    if (!isUpperCase(word[currentChar]))
+      answer += String.fromCharCode(
+        LOWER_ASCII_A_Z - word.charCodeAt(currentChar)
+      );
+  }
   return answer;
 }
 
