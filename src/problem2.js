6d7d033890c80231e8e14c8eab6b34667bbc4d0c
River <swimmingr@gmail.com>
2022-10-28 23:04:53 +0900

    problem2 암호문 조건 분별

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
 function problem2(cryptogram) {
   var answer;
+  if (crypto.length <= 100 && crypto.length > 1) {
+    if (crypto === crypto.toLowerCase()) {
+    } else {
+      return null;
+    }
+  } else {
+    return null;
+  }
   return answer;
 }
 
