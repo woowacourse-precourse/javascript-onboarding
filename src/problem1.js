063f6a3e4c7757f2ca92d69e9baa2f67f1bbfc36
Jiho1996 <jhp9270@naver.com>
2022-10-28 18:30:11 +0900

    feat : 각 자리수 합산 결과 리턴 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,6 +3,24 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+/**
+* 길이가 2인 배열을  넣으면 그 합을 리턴해주는 함수.
+* @param {array} arr 입력된 배열
+* @returns {number} 덧셈 결과 리턴
+*/
+const getMaxFromAddSum = arr => {
+  const result = arr.map( ele => 
+      String(ele).split('').reduce((acc,cur)=>
+          acc + parseInt(cur), 0)
+);
+  return  result[0] > result[1] ? result[0] : result[1];
+}
+
+/**
+ * 받은 배열이 유효한지 확인하는 함수.
+ * @param {array} arr 
+ * @returns {boolean}
+ */
 const isValidArray = (arr) => {
   const leftPage = arr[0];
   const rightPage = arr[1];
