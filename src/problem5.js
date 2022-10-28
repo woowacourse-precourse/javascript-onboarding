faf3eb2569e94dab7283e4de443c2ce52b074770
violetwhenisawu <anashape710@gmail.com>
2022-10-28 21:14:20 +0900

    Docs: problem5 함수 주석 추가, 구현완료

--- src/problem5.js
+++ src/problem5.js
@@ -2,9 +2,10 @@ function problem5(money) {
   let result = [];
   let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  // money를 화폐 단위별로 나눈 값을 반환하는 함수
   function takeOutMoney(unit) {
     let quotient = Math.floor(money / unit);
-    money = money - quotient * unit;
+    money = money - quotient * unit; // money 나머지 값을 변경하는 연산
     return quotient;
   }
 
