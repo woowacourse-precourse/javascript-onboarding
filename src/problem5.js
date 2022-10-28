f3eea94439cd2021ddceafc9a04d817afda2e5d5
ashleysheo <ashleysyheo@gmail.com>
2022-10-28 12:59:34 +0900

    feat(problem5): get minimum number of cash

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const cashList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const cashCount = new Array(9).fill(0); 
+  let currentMoney = money; 
+  let index = 0; 
+
+  while (currentMoney > 0) {
+    if (currentMoney >= cashList[index]) {
+      currentMoney -= cashList[index];
+      cashCount[index] += 1; 
+    } else {
+      index += 1;
+    }
+  }
+
+  return cashCount;
 }
 
 module.exports = problem5;
