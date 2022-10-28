3833ac695184e30fb625922c89a87c1e3d1d9975
dakyungoh <dakyung.work@gmail.com>
2022-10-29 04:39:15 +0900

    test: problem2의 테스트케이스 설계

---
+++ __tests__/problem2.test.js
@@ -0,0 +1,26 @@
+const problem2 = require("../src/problem2");
+
+describe("problem2", () => {
+  test("case1: 재귀로 일부 지워진다.", () => {
+    expect(problem2("browoanoommnaon")).toEqual("brown");
+  });
+  test("case2: 재귀로 전부 지워진다.", () => {
+    expect(problem2("zyelleyz")).toEqual("");
+  });
+  test("case3: 아무것도 지워지지 않는다.", () => {
+    expect(problem2("abcdefg")).toEqual("abcdefg");
+  });
+  test("case4: 맨 앞 또는 맨 뒤에서 한번 지워진다.", () => {
+    expect(problem2("aabcdefg")).toEqual("abcdefg");
+    expect(problem2("abcdefgg")).toEqual("abcdefg");
+  });
+  test("case5: 3개 이상이 연속되는 사례", () => {
+    expect(problem2("abcccdefg")).toEqual("abdefg");
+    expect(problem2("aaabcdef")).toEqual("bcdef");
+    expect(problem2("abcdeffff")).toEqual("abcde");
+  });
+  test("case5: 3개 이상이 연속되는 사례 + 재귀", () => {
+    expect(problem2("abecccebfg")).toEqual("afg");
+    expect(problem2("cbaaabcdef")).toEqual("def");
+  });
+});
