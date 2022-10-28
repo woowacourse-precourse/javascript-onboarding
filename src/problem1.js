aa2e8c98df957ed389bb334d4fa70c33c43ac9c6
Haram Baek <haram@Haramui-MacBookAir.local>
2022-10-28 15:10:35 +0900

    feat: 포비의 점수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,24 @@
+function eachSum(num) {
+  let sum = 0;
+
+  do {
+    sum += num % 10;
+    num = Math.floor(num / 10);
+  } while (num > 0)
+
+  return sum
+}
+
+function eachMulti(num) {
+  let mul = String(String(num).split('').reduce(function (a, b) { return a * b }));
+
+  return parseInt(mul)
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  
+  let pobiList = [eachSum(pobi[0]), eachMulti(pobi[0]), eachSum(pobi[1]), eachMulti(pobi[1])]
+  let pobiScore = Math.max(...pobiList);
+
   return answer;
 }
 
