7536b7a3519ac478b0334e6e8610af46ae02d833
BBIYAC <kiy7833@gmail.com>
2022-10-28 18:08:33 +0900

    feat(problem6): add restricted user feature
    - 같은 글자가 연속적으로 포함되는 지원자 추출 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
+// 같은 글자가 연속적으로 포함되는 지원자 추출 기능
 function problem6(forms) {
-  var answer;
-  return answer;
+  let emails = {},
+      names = [],
+      restricted_emails = [];
+  
+  for (let [email, name] of forms) {
+    for (let stored_name of names) {
+      for (let i = 0; i < name.length - 1; i++) {
+        if (stored_name.includes(name.substr(i, 2))) {
+          if (!restricted_emails.includes(emails[stored_name])) restricted_emails.push(emails[stored_name]);
+          if (!restricted_emails.includes(email)) restricted_emails.push(email);
+        }
+      }
+    }
+    
+    emails[name] = email;
+    names.push(name);
+  }
+
+  return restricted_emails;
 }
 
 module.exports = problem6;
