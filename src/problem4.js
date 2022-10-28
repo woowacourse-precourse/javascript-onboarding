ce599cbffb0660056bb2dcafb2e44d435b88f9ba
Rki0 <pky00823@gmail.com>
2022-10-28 15:58:07 +0900

    chore: prob4. 주석 처리했던 부분 제거
    커밋 전 테스트 결과 비교를 위해 주석 처리했던 부분을 지웠습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -24,32 +24,6 @@ function checkUpperOrLower(oneWordASCII, ans) {
   }
 }
 
-// function checkAlphabet(ans, oneWordASCII, word) {
-//   if (
-//     oneWordASCII < 65 ||
-//     (oneWordASCII > 90 && oneWordASCII < 97) ||
-//     oneWordASCII > 122
-//   ) {
-//     ans.push(word);
-
-//     return false;
-//   }
-
-//   return true;
-// }
-
-// function changeWord(word, ans) {
-//   for (let i = 0; i < word.length; i++) {
-//     const oneWordASCII = word[i].charCodeAt();
-
-//     if (!checkAlphabet(ans, oneWordASCII, word[i])) {
-//       continue;
-//     }
-
-//     checkUpperOrLower(oneWordASCII, ans);
-//   }
-// }
-
 function checkAlphabet(ans, word) {
   const regex = /^[a-z|A-Z]+$/g;
 
