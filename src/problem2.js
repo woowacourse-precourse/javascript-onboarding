20391d508c741cc2c93c4837f1ef8e8e27ab0733
Hun-Se <70246174+Hun-Se@users.noreply.github.com>
2022-10-28 23:36:46 +0900

    problem1, problem2 문제해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  cryptogram = cryptogram.split("");
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      cryptogram.splice(i, 2);
+      console.log(i, cryptogram, cryptogram.length);
+      i = -1;
+    }
+  }
+
+  return cryptogram.join("");
 }
 
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
   var answer;
-  return answer;
+  for (i = 1; i <= number; i++) {
+    if (
+      String(i).indexOf("3") !== -1 ||
+      String(i).indexOf("6") !== -1 ||
+      String(i).indexOf("9") !== -1
+    ) {
+      answer++;
+    }
+    return answer;
+  }
 }
-
 module.exports = problem3;
