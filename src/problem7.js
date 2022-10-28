d66ae72dd4fbc0261605c0cf41246727fac75afa
frants-jeon <jjh6963@naver.com>
2022-10-28 17:22:08 +0900

    feat (problem7): 유저의 친구 관계를 객체에 저장하는 함수 생성

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,17 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+// 기능 1번
+const setRelationshipEachOther = (eachOtherFriend, relationshipTable) => {
+  const [friendA, friendB] = eachOtherFriend;
+  relationshipTable[friendA] = [...(relationshipTable[friendA] || []), friendB];
+  relationshipTable[friendB] = [...(relationshipTable[friendB] || []), friendA];
+};
+
+const setRelationshipAll = (friends, relationshipTable) => {
+  for (const friend of friends) {
+    setRelationshipEachOther(friend, relationshipTable);
+  }
+};
+
 module.exports = problem7;
