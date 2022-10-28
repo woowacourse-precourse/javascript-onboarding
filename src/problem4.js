e48bbc358599916a62ab663f9ee61ff844498429
evencoding <evencoding@gmail.com>
2022-10-28 23:22:24 +0900

    [문제4] refactor: 기능함수들을 화살표 함수로 변경

--- src/problem4.js
+++ src/problem4.js
@@ -5,7 +5,7 @@ const UPPER_A_ASCII = 97;
 const UPPER_Z_ASCII = 122;
 const UPPER_A_PLUS_Z = 219;
 
-function asciiToReversedChar(ascii) {
+const asciiToReversedChar = (ascii) => {
   if (ascii >= LOWER_A_ASCII && ascii <= LOWER_Z_ASCII) {
     return String.fromCharCode(LOWER_A_PLUS_Z - ascii);
   } else if (ascii >= UPPER_A_ASCII && ascii <= UPPER_Z_ASCII) {
@@ -13,11 +13,9 @@ function asciiToReversedChar(ascii) {
   } else {
     return String.fromCharCode(ascii);
   }
-}
+};
 
-function charToAscii(char) {
-  return char.charCodeAt();
-}
+const charToAscii = (char) => char.charCodeAt();
 
 function problem4(word) {
   let reversedWord = [...word].reduce((acc, cur) => {
