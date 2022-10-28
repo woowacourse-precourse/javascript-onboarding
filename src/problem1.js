05dd5a8c42b9ad81379e00f0fc3aaa24adac9911
afds4567 <afds4567@gmail.com>
2022-10-28 23:45:00 +0900

    feat(problem1): 유저별 최댓값 계산 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -4,10 +4,32 @@ function problem1(pobi, crong) {
 
   if (pobiMax > crongMax) return 1;
   if (pobiMax === crongMax) return 0;
-  else return -1;
+  else return 2;
+
+  function numToDigit(pages) {
+    return pages.map((page) =>
+      page
+        .toString()
+        .split("")
+        .map((digit) => digit * 1)
+    );
+  }
 
   function getMax(nums) {
-    return 0;
+    let calcResults = numToDigit(nums)
+      .map((pageDigits) => [
+        pageDigits.reduce(sum),
+        pageDigits.reduce(multiply),
+      ])
+      .flat();
+    return Math.max(...calcResults);
+  }
+
+  function sum(a, b) {
+    return a + b;
+  }
+  function multiply(a, b) {
+    return a * b;
   }
 }
 
