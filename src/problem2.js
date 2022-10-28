dcf2e5ca72f10a215ee9548fe935ab6510f1e162
xilucks <siun0331@gmail.com>
2022-10-29 04:34:58 +0900

    feat: problem2 solve
    reduce의 사용에서 초기값을 할당하는 방법에 대해서 학습하고 적용하였습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = decode(cryptogram);
   return answer;
+}
+
+function decode(cryptogram) {
+  const spellings = [...cryptogram.toString()];
+  let result = spellings.reduce((acc, cur) => {
+    acc.at(-1) === cur ? acc.pop() : acc.push(cur);
+    return acc;
+  }, []);
+  return result.join("");
 }
 
 module.exports = problem2;
