b473bb6ed52795971561eca4ce78d33262da881a
jongwonp <jw032876@gmail.com>
2022-10-28 17:53:18 +0900

    solve problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let arr = Array.from(word.length);
+  for (let i = 0; i < word.length; i++) {
+    if (
+      (word[i].charCodeAt(0) >= 65 && word[i].charCodeAt(0) <= 90) ||
+      (word[i].charCodeAt(0) >= 97 && word[i].charCodeAt(0) <= 122)
+    ) {
+      arr[i] = converter(word[i]);
+    } else {
+      arr[i] = word[i];
+    }
+  }
+  return arr.join("");
 }
+
+const converter = (alphabet) => {
+  if (alphabet.charCodeAt(0) >= 65 && alphabet.charCodeAt(0) <= 90) {
+    return String.fromCharCode(
+      alphabet.charCodeAt(0) + (77 - alphabet.charCodeAt(0)) * 2 + 1
+    );
+  }
+  return String.fromCharCode(
+    alphabet.charCodeAt(0) + (109 - alphabet.charCodeAt(0)) * 2 + 1
+  );
+};
 
 module.exports = problem4;
