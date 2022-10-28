1cd4c1aa43e7d5bb211798a037124e893e48eadf
pp449 <mma7710@naver.com>
2022-10-28 22:45:51 +0900

    [문제 3] : 3. 해당 숫자가 3, 6, 9중 하나면 1을 더하는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,28 @@
+const checkNumber = (number) => {
+  switch (number){
+    case 3:
+    case 6:
+    case 9:
+      return 1;
+  }
+  return 0;
+}
+
 const divideByDigit = (number) => {
+  let count = 0;
   for(let i=number; i!=0; i=parseInt(i/10)) {
     let n = i%10;
+    count += checkNumber(n);
   }
+  return count;
 }
 
 function problem3(number) {
+  let answer = 0;
   for (let i=1; i<=number; i++) {
-    divideByDigit(i);
+    answer += divideByDigit(i);
   }
   return answer;
 }
-
-problem3(13);
 
 module.exports = problem3;
