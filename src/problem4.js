d4388471e1476ce45b5b8d97901055b2ba8a7e09
GC-Park <kuyt3122@naver.com>
2022-10-29 01:53:11 +0900

    (4번)Feat:각 값들을 주어진 조건에 따라 변환 후 리턴해주는 함수 wordChange 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,23 @@
+function wordChange(c){
+  let mn;
+  if(c===c.toUpperCase()){
+    mn= c-'A';
+    mn=25-mn;
+    mn=mn+'A';
+  }else{
+    mn= c-'a';
+    mn=25-mn
+    mn=mn+'a';
+  }
+  return mn;
+}
+
+function getStr(w){
+}
+
 function problem4(word) {
   var answer;
+  answer = getStr(word)
   return answer;
 }
 
