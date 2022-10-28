ce511686b8595283b087ee55c54a96db8d05272a
violetwhenisawu <anashape710@gmail.com>
2022-10-29 00:42:03 +0900

    Feat: 조건을 확인하여 중복 문자를 삭제하고, 반환하는 함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = cryptogram.split("");
+  let checkedArr = checkOverlap(arr);
+  return;
+}
+
+function checkOverlap(arr) {
+  let i = 0;
+  while (i !== arr.length) {
+    if (arr[i] === arr[i + 1]) {
+      arr.splice(i, 2);
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  return arr;
 }
 
 module.exports = problem2;
