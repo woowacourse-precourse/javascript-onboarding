96eee32534fd1f1fcaff7fde218964ab3060c932
leeseooo <leeseoooo@naver.com>
2022-10-28 21:03:05 +0900

    feat: problem1 페이지 번호 각 자릿수 합 반환하는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,21 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
   return answer;
 }
+
+function getAddedScore(pages) {
+  return Math.max(
+    String(pages[0])
+      .split("")
+      .reduce((acc, cur) => acc + +cur, 0),
+    String(pages[1])
+      .split("")
+      .reduce((acc, cur) => acc + +cur, 0)
+  );
+}
+
+console.log(problem1([97, 98], [197, 198]));
+console.log(problem1([131, 132], [211, 212]));
+console.log(problem1([99, 102], [211, 212]));
 
 module.exports = problem1;
