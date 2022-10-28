5a204e4e2b0712833075b368ea52f12bc066f191
leeuihyun <meta212121@naver.com>
2022-10-28 15:42:03 +0900

    feat : problem3 손뼉 횟수 구하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,15 @@
+function clap(number) {
+  let result = 0;
+  number.forEach((item) => {
+    for (v of item) {
+      if (v === "3") result += 1;
+      if (v === "6") result += 1;
+      if (v === "9") result += 1;
+    }
+  });
+  return answer;
+}
+
 function problem3(number) {
   var answer;
   return answer;
