f1fac14ccc930b8fe2c3642b5d19495489faf99e
leeseooo <leeseoooo@naver.com>
2022-10-28 22:42:29 +0900

    feat: problem6 닉네임에서 나올 수 있는 모든 문자를 저장하는 set 변수 생성 및 할당

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,31 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let nameList = [];
+
+  forms.forEach((form) => {
+    let splitedNameSet = new Set();
+    const [, nickname] = form;
+
+    for (let i = 2; i <= nickname.length; i++) {
+      for (let j = 0; j < nickname.length - 1; j++) {
+        splitedNameSet.add(nickname.slice(j, j + i));
+      }
+    }
+    nameList.push([form, [...splitedNameSet]]);
+    console.log(splitedNameSet);
+  });
+
   return answer;
 }
+
+console.log(
+  problem6([
+    ["jm@email.net", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
 
 module.exports = problem6;
