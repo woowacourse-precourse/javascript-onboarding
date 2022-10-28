8e3b3fd55e2b5579ce0f06eac64c4d5854a97a3e
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-28 20:37:02 +0900

    feat: P4 구현할 것

--- src/problem4.js
+++ src/problem4.js
@@ -4,3 +4,13 @@ function problem4(word) {
 }
 
 module.exports = problem4;
+
+/*
+ 1. word를 배열로 split
+ 2. map을 이용해 각 문자를 convert.
+    1) 아스키코드로 변환.
+    2) lowerCase, upperCase, exception(그대로 return) 분류.
+    3) 인덱스 반대로 치환.
+    4) return
+ 3. Array.join("") 후, return.
+*/
