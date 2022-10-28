b22daf1d9dbe16865609dd1d4c836d390c944b95
xownsdl0 <xownsdl0@naver.com>
2022-10-28 13:27:53 +0900

    <P2> 중복되는 값이 없는 경우

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,14 @@
 function problem2(cryptogram) {
   var answer;
+  let setArr = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    //중복 되는 값이 없는 경우
+    if (setArr[setArr.length - 1] !== cryptogram[i]) {
+      setArr.push(cryptogram[i]);
+    }
+  }
+
   return answer;
 }
 
