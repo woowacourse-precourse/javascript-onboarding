850abeec1776208d968f3ed6df85e3e068fdf818
dakyungoh <dakyung.work@gmail.com>
2022-10-29 05:25:50 +0900

    test: problem3 테스트 케이스 추가

--- __tests__/problem3.test.js
+++ __tests__/problem3.test.js
@@ -19,4 +19,14 @@ describe("problem3: 369게임", () => {
     expect(problem3(9)).toEqual(3);
     expect(problem3(10)).toEqual(3);
   });
+  test("case4: 다음 숫자랑의 차이 검사", () => {
+    expect(problem3(6392)).toEqual(problem3(6393) - 4);
+    expect(problem3(6392)).toEqual(problem3(6394) - 7);
+    expect(problem3(6392)).toEqual(problem3(6395) - 10);
+    expect(problem3(6392)).toEqual(problem3(6396) - 14);
+    expect(problem3(6392)).toEqual(problem3(6397) - 17);
+    expect(problem3(6392)).toEqual(problem3(6398) - 20);
+    expect(problem3(6392)).toEqual(problem3(6399) - 24);
+    expect(problem3(6392)).toEqual(problem3(6400) - 25);
+  });
 });
