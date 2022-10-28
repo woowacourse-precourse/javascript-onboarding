c91444e33dc0aa94c80343eadd8adf0b5cd0f5eb
waymokorea <taw0207@naver.com>
2022-10-28 23:24:53 +0900

    "problem3 first try"

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+
+  for (let i = 1; i <= number; i++) {
+    if (i % 10 === 3 || i % 10 === 6 || i % 10 === 9) {
+      count += 1;
+    }
+  }
+
+  return count;
 }
 
 module.exports = problem3;
