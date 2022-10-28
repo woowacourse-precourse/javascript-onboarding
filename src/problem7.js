66154d856249d843150be9cbc025ae73bf1b6361
coolIlive <galilee155@naver.com>
2022-10-29 00:23:08 +0900

    FEAT(problem7): 추천인 정렬기능 추가한다
    딕셔너리의 값을 리스트에 넣고, 점수 기준으로 정렬하고 점수가 같으면 이름순으로 정렬한다. 마지막으로 sortTable에 있는 이름을 answer에 순서대로 넣고 리턴해준다

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,5 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
 
   // 딕셔너리 만들기 지인 저장할것 하나, 점수 저장할 것 하나
   const userInfo={};
@@ -30,18 +30,42 @@ function problem7(user, friends, visitors) {
 
   // visitors중 친구목록에 없던것은 딕셔너리에 추가,점수 1 올리기
   // 친구목록에 있던건 그냥 점수 1 올리기
-  
+  visitors.forEach((visit) => {
+    if (!userScore[visit]) {
+      userScore[visit]=1
+    }
+    if (userScore[visit]) {
+      userScore[visit]+=1
+    }
+  })
 
   // user와 user의 지인 점수 제거
   delete userScore[user]
   for (let userFriend of userFriends) {
     delete userScore[userFriend]
   }
-  console.log(userScore)
+  
 
-
+  // 추천할 사람 정렬 기능
+  let sortTable=[];
+  for (let member of Object.keys(userScore)) {
+    if (userScore[member]!==0) {
+      sortTable.push([member,userScore[member]])
+    }
+  }
+  // sortTable=[  [ 'jun', 20 ], [ 'andole', 20 ],[ 'abndole', 20 ],[ 'cndole', 20 ],[ 'bedi', 4 ] ]
+  sortTable.sort((a,b)=> {
+    if (a[1]!==b[1]) {
+      return b[1]-a[1]
+    }
+    if (a[0]>b[0]) {
+      return 1
+    }
+    return -1
+  });
+  answer=sortTable.map((finalName)=>finalName[0])
 
   return answer;
 }
-console.log(problem7('mrko',[["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]],["bedi", "bedi", "donut", "bedi", "shakevan"]))
-// module.exports = problem7;
+// console.log(problem7('mrko',[["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]],["bedi", "bedi", "donut", "bedi", "shakevan"]))
+module.exports = problem7;
