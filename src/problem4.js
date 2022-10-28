4e4e0569fba59e34f5c5ee7d4e90f5ed3f2001e7
xownsdl0 <xownsdl0@naver.com>
2022-10-28 14:56:20 +0900

    <P4> 대소문자 판별식

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
   var answer;
+  let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let reverseCase = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+  let replaceWord = "";
+
+  for (let i of word) {
+    if (i === i.toUpperCase()) {
+    }
+    if (i === i.toLowerCase()) {
+    }
+  }
+
   return answer;
 }
 
