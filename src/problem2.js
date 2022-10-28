9b61faa43908dffd679e2059f9be014ac8d1bc86
freemoon99 <soloist132@gmail.com>
2022-10-28 21:39:50 +0900

    feat[problem2]: 빈배열을 생성하고 첫문자 입력

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,17 @@
 function problem2(cryptogram) {
   var answer;
+  var code = [];
+  code.push(cryptogram[0]);
+
+  for (var i = 1; i < cryptogram.length; i++) {
+    const curr = cryptogram[i];
+    const prev = code.length - 1;
+
+    if (code[prev] === curr) {
+      code.pop();
+    } else code.push(curr);
+  }
+  return code.join("");
   return answer;
 }
 
