58a79c05ab7ec051947fc984e13256d43acb98e0
minsoo <als494@naver.com>
2022-10-28 20:24:20 +0900

    4번 문제 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  // A ~ Z : 65 ~ 90,  a ~ z : 97 ~ 122
+  for (let i = 0; i < word.length; i++) {
+    let char = word.charCodeAt(i);
+    if (char >= 65 && char <= 90) {
+      char = 65 + 90 - char;
+    }
+    if (char >= 97 && char <= 122) {
+      char = 97 + 122 - char;
+    }
+    answer += String.fromCharCode(char);
+  }
+
   return answer;
 }
 
