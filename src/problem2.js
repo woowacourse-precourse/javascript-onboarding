21379c34410e3040fe3a5c9de074ee672bb2cc4d
nlom0218 <nlom0218@naver.com>
2022-10-28 15:56:39 +0900

    feat: 문제 2 - 스택을 반환하여 구현 완료하기

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,3 @@
-// const isOverlap = (answer, char) => {
-//   return answer[answer.length - 1] === char;
-// };
-
-// function problem2(cryptogram) {
-//   let answer = "";
-//   for (let i = 0; i < cryptogram.length; i++) {
-//     if (isOverlap(answer, cryptogram[i])) {
-//       answer = answer.slice(0, answer.length - 1);
-//       continue;
-//     }
-//     answer += cryptogram[i];
-//   }
-//   return answer;
-// }
-
-// module.exports = problem2;
-
 const isOverlap = (answer, char) => {
   return answer[answer.length - 1] === char;
 };
@@ -34,7 +16,8 @@ function problem2(cryptogram) {
     }
     stack += char;
   });
-  console.log(stack);
+
+  return stack;
 }
 
 module.exports = problem2;
