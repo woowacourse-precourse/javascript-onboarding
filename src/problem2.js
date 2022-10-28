060ad54604206620300e36a48f430d704b4e9c1c
SeongHo-C <leeseong010@naver.com>
2022-10-28 23:18:26 +0900

    feat(problem2): 연속하는 중복 문자 존재 유무 반환 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,19 @@
 // 연속하는 중복 문자 삭제 결과 반환 기능
 
-// 연속하는 중복 문자 존재 유무 반환 기능
+function isDuplicate(cryptogram) {
+  let check = false;
+
+  for (let i = 0; i < cryptogram.length - 1; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) return true;
+  }
+
+  return check;
+}
 
 function problem2(cryptogram) {
   let answer;
+
+  while (isDuplicate(crypto)) {}
 
   return answer;
 }
