0605eba43e68d6de86977217b5a6fccee3e93b3a
d0422 <rlfehd2013@naver.com>
2022-10-28 15:58:34 +0900

    Feat:3,6,9를 세어주는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -2,4 +2,14 @@ function problem3(number) {
   var answer;
   return answer;
 }
+function countNumber(number) {
+  str = string(number);
+  let count = 0;
+  str.forEach((char) => {
+    if (char == "3" || char == "6" || char == "9") {
+      count += 1;
+    }
+  });
+  return count;
+}
 module.exports = problem3;
