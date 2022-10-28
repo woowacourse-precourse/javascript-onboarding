abf2120fc791ae521b62fdf44d0cdced6ce15c94
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-10-29 00:17:23 +0900

    문제4 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,26 @@
 function problem4(word) {
   var answer;
+
+  let AtoZCapital = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z'];
+  let AtoZSmall = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  
+  let newWord = '';
+
+  for (var i=0; i<word.length; i++){
+    let indexInCapital = AtoZCapital.indexOf(word[i]);
+    let indexInSmall = AtoZSmall.indexOf(word[i]);
+    if(indexInCapital != -1){ //알파벳 대문자 변환
+      newWord += AtoZCapital[Math.abs(25 -indexInCapital)];
+    }
+    else if(indexInSmall != -1){ //알파벳 소문자 변환
+      newWord += AtoZSmall[Math.abs(25 -indexInSmall)];
+    }
+    else{ //변환X
+      newWord += word[i];
+    }
+  }
+
+  answer = newWord;
   return answer;
 }
 
