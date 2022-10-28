c0a3302b053c2a1bdc5f9a645d817dcd55a0a03b
saram <yujun1018@naver.com>
2022-10-29 00:22:53 +0900

    [문제6] feat: 1에서 추출한 두 글자가 타 크루의 닉네임에 포함되어 있다면, 해당 크루들의 이메일을 배열에 담는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,19 @@
 function problem6(forms) {
+  const answer = [];
+
   forms.forEach((crew) => {
     const nickname = crew[1];
+    const email = crew[0];
+
     for (let i = 0; i < nickname.length - 1; i++) {
       const twoLetters = nickname[i] + nickname[i + 1];
+
+      forms.forEach((crew2) => {
+        const nickname2 = crew2[1];
+        const email2 = crew2[0];
+        if (nickname2.includes(twoLetters) && nickname !== nickname2)
+          answer.push(email, email2);
+      });
     }
   });
 }
