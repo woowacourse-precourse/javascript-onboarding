d1e2aae7209e187d9952d9c41e20fd83b5a5d004
afds4567 <afds4567@gmail.com>
2022-10-28 17:01:09 +0900

    feat(problem1): 승부 결정 로직 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,14 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let pobiMax = getMax(pobi);
+  let crongMax = getMax(crong);
+
+  if (pobiMax > crongMax) return 1;
+  if (pobiMax === crongMax) return 0;
+  else return -1;
+
+  function getMax(nums) {
+    return 0;
+  }
 }
 
 module.exports = problem1;
