5591feb4189aba22c62b8482a49d91c20d457475
kang <mdiu2203@naver.com>
2022-10-29 02:32:29 +0900

    feat(PROBLEM3): find out how many certain numbers(3,6 or 9) it has

--- src/problem3.js
+++ src/problem3.js
@@ -3,6 +3,12 @@ function problem3(number) {
   return answer;
 }
 
+function howManyHas369Numbers(number) {
+  const numberArr = [...(number + '')];
+
+  return numberArr.filter(is369Number).length;
+}
+
 function is369Number(number) {
   return number === '3' || number === '6' || number === '9';
 }
