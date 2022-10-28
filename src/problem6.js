34f2be3cb33445d9a9354171e40c9efe35fee7ea
배병재 <39183208+BaeByungjae@users.noreply.github.com>
2022-10-28 22:35:06 +0900

    problem6 로직 수정

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,5 @@
 function problem6(forms) {
   const answer = [];
-  const wordDict = {};
   forms.forEach((form) => {
     const user = form[1];
     for (let i = 2; i <= user.length; i++) {
@@ -8,16 +7,13 @@ function problem6(forms) {
         const subStr = user.substr(j, i);
         forms.forEach((data) => {
           if (data[1] !== user && data[1].includes(subStr)) {
-            if (!wordDict[data[1]]) {
-              wordDict[data[1]] = true;
-              answer.push(data[0]);
-            }
+            answer.push(data[0]);
           }
         });
       }
     }
   });
-  return answer.sort();
+  return [...new Set(answer)].sort();
 }
 
 module.exports = problem6;
