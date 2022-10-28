6fbd0d4dcf90df1293b0bc35bd12ac26732197fe
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-28 12:17:31 +0900

    feat: [문제6] 선별 이메일 중 중복 제거하고 오름차순 정렬 추가

--- src/problem6.js
+++ src/problem6.js
@@ -2,23 +2,35 @@ function problem6(forms) {
   const result = [];
   forms.forEach((form, formIndex) => {
     const [_, name] = form;
-    for (let i = 0; i < form.length - 1; i++) {
-      const word = name.substring(i, 2);
+
+    for (let i = 0; i < form.length; i++) {
+      const word = name.substring(i, i + 2);
       for (let j = formIndex + 1; j < forms.length; j++) {
+        console.log(word);
         const compareName = forms[j][1];
         if (compareName.includes(word)) {
+          console.log(word);
           result.push(form[0]);
           result.push(forms[j][0]);
         }
       }
     }
   });
+  const removeEmailDuplication = (emailList) => {
+    return [...new Set(emailList)];
+  };
+
+  const sortListAscending = (array) => {
+    return array.sort();
+  };
+  return sortListAscending(removeEmailDuplication(result)); //?
 }
 
+//test
 problem6([
   ['jm@email.com', '제이엠'],
   ['jason@email.com', '제이슨'],
-  ['woniee@email.com', '워니'],
+  ['woniee@email.com', '이슨니'],
   ['mj@email.com', '엠제이'],
   ['nowm@email.com', '이제엠'],
 ]);
