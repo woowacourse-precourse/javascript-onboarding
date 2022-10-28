9c3bafe784a88c5a611dc29040bac741b5a70a95
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-28 15:12:07 +0900

    feat(PROBLEM4): 2. 소문자 'a ~ z' 반환하는 makeLowerAlphabet 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -10,3 +10,10 @@ const makeUpperAlphabet = () => {
     .map((_, i) => i + 65)
     .map((x) => String.fromCharCode(x));
 }
+
+const makeLowerAlphabet = () => {
+  return new Array(26)
+    .fill(null)
+    .map((_, i) => i + 97)
+    .map((x) => String.fromCharCode(x));
+}
