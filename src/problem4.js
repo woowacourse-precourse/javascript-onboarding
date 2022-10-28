88c2ad2cb228d913054dab5393ed6c386c404697
pp449 <mma7710@naver.com>
2022-10-28 23:31:21 +0900

    [문제 4] : 아스키코드를 이용해 대소문자와 알파벳 외의 문자를 찾아내는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
-function problem4(word) {
-  var answer;
-  return answer;
+const findAlpabat = (asciiNum) => {
+  if((65 <= asciiNum && asciiNum <= 90) || (97 <= asciiNum && asciiNum <= 122)) {
+    console.log("범위내 있음")
+  }
+  else {
+    console.log("범위내 없음");
+  }
 }
+
+function problem4(word) {
+  for(let i = 0; i<word.length; i++) {
+    findAlpabat(word.charCodeAt(i));
+  }
+}
+
+console.log(problem4("I love you"));
 
 module.exports = problem4;
