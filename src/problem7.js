976ed869d0801f0ee0e9962b11c478913a288f8b
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-28 22:03:18 +0900

    feat: 함께 아는 친구에 따른 점수 부여

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,23 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+const getFriendsShareScore = (user, friends, friendsMap) => {
+  friends.map((friend) => {
+    if (friend.includes(user)) {
+      for (let f of friend) {
+        friendsMap.set(f, 0);
+      }
+    }
+    else {
+      for (let a of friend) {
+        friendsMap.set(a, 10);
+      }
+    }
+  });
+
+  return friendsMap;
+};
+
+const problem7 = (user, friends, visitors) => {
+  let friendsMap = new Map();
+  friendsMap = getFriendsShareScore(user, friends, friendsMap);
+};
 
 module.exports = problem7;
