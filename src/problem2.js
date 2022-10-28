2a5cb0de1b21d929251e830e3643efe7f52e00e2
waymokorea <taw0207@naver.com>
2022-10-28 22:47:48 +0900

    problem2 test 1 pass

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = [];
+
+  for (let i = 0; i < cryptogram.length; ++i) {
+    arr.push(cryptogram[i]);
+    if (arr.length > 2) {
+      let num = arr.length;
+      if (arr[num - 1] === arr[num - 2]) {
+        arr.pop();
+        arr.pop();
+      }
+    }
+  }
+
+  return arr.join('');
 }
 
 module.exports = problem2;
