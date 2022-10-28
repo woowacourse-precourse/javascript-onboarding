c0e77b7f9a48e2b8fa41689a3df7ea38d63c5af6
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-29 01:04:40 +0900

    problem7 visitor 점수 계산 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,14 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let friendScore = {}
+  for (let i = 0; i<visitors.length; i++){
+    if(visitors[i] in friendScore == false){
+      friendScore[visitors[i]] = 1
+    }
+    else if (visitors[i] in friendScore == true){
+      friendScore[visitors[i]]+=1
+    }
+  }
   return answer;
 }
-
 module.exports = problem7;
