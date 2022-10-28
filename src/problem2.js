04f729a9965b15200afbe9b314d598d117466949
dustmddus <dustmddus5@gmail.com>
2022-10-28 20:24:56 +0900

    Feat: [문제 2] stack 사용 중복 문자 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,31 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = cryptogram.split("");
+  const stack = [];
+  let flag = false;
+
+  for (let i = 0; i < arr.length; i++) {
+    if (stack.length === 0) {
+      stack.push(arr[i]);
+      continue;
+    }
+    if (stack[stack.length - 1] === arr[i]) {
+      flag = true;
+    } else {
+      if (flag) {
+        stack.pop();
+        if (stack[stack.length - 1] !== arr[i]) {
+          flag = false;
+          stack.push(arr[i]);
+        }
+      } else {
+        stack.push(arr[i]);
+      }
+    }
+    if (i === arr.length - 1 && flag) {
+      stack.pop();
+    }
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
