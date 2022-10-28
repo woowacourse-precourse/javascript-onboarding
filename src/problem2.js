e82091b42295f8edddd2afb59ce123df5197fa20
sulmeee <poorooria@naver.com>
2022-10-28 12:34:43 +0900

    [problem2] feat: 반복문을 통해 중복된 단어 제거

--- src/problem2.js
+++ src/problem2.js
@@ -3,7 +3,19 @@ function problem2(cryptogram) {
   const result = [];
 
   // 주어진 단어를 단어 하나씩 나누기
-  const cryptoframWord = cryptogram.split('')
+  const cryptoframWord = cryptogram.split('');
+
+  // 반복문을 통해 배열을 순회하면서 중복된 단어가 있는 지 확인
+  // 중복되지 않았다면 배열에 담아주기
+  for (let i = 0; i <= cryptoframWord.length; i++) {
+    if (result[result.length - 1] === cryptoframWord[i]) {
+      result.pop();
+    } else {
+      result.push(cryptoframWord[i]);
+    }
+  }
+
+  return result;
 }
 
 module.exports = problem2;
