0feecea0bb35df24b671f5ca8c2f4bb888ec814f
pangkyu <baesense@naver.com>
2022-10-28 19:56:24 +0900

    feat : 알파벳 정보를 확인하여 리턴하는 기능
    1.만약 letter가 대문자라면 UPPERCASE를 스플릿하여 해당하는 문자의 인덱스를 찾아 저장하고 uppercase에서 반대로 리턴해준다.
    2.LOWERCASE도 마찬가지
    3. UPPER인지 LOWER인지는 입력받은 letter를 toUpperCase로 하여 일치한지 아닌지로 판별한다.

--- src/problem4.js
+++ src/problem4.js
@@ -6,8 +6,29 @@ function problem4(word) {
 
   for (let i = 0; i < word.length; i++) {
     const letter = word[i];
+    result += convert(letter);
   }
   console.log(result);
+  return result;
+}
+function convert(letter) {
+  if (letter === " ") {
+    return letter;
+  }
+  const isLetterUpper = checkUpper(letter);
+  if (isLetterUpper) {
+    const idx = UPPERCASE.split("").indexOf(letter);
+    return UPPERCASE[25 - idx];
+  }
+  const idx = LOWERCASE.split("").indexOf(letter);
+  return LOWERCASE[25 - idx];
+}
+
+function checkUpper(letter) {
+  if (letter === letter.toUpperCase()) {
+    return true;
+  }
+  return false;
 }
 
 function testCode() {
