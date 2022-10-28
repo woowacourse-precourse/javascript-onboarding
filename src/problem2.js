46a6dc667e93db6a7c58ea0abdc4fb17ea63170a
givensik <kjs4072@icloud.com>
2022-10-29 03:51:15 +0900

    problem1,problem2(not yet)

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer ='';
+  var i;
+  var flag = 0;
+  while(1){
+    flag = 0;
+    for(i=0;i<cryptogram.length - 1;i++){
+      console.log(cryptogram[i]);
+      if(cryptogram[i] == cryptogram[i+1]){
+        cryptogram.replace(cryptogram[i]+cryptogram[i], '');
+        flag = 1;
+      }
+    }
+    if(flag == 0){
+      break;
+    }
+  }
+  
+  answer = cryptogram;
   return answer;
 }
 
