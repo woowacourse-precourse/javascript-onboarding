18c71546e2b4f56ce80fd08b36ee2888fd0463ca
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-29 05:01:09 +0900

    feat: Add calcUnit Func

--- src/problem5.js
+++ src/problem5.js
@@ -1,19 +1,31 @@
 function problem5(money) {
-  var answer;
+  let answer;
+  answer = calcUnit(money);
   return answer;
 }
 
 module.exports = problem5;
 
-const calcCharge = (mopny) => {
+const calcUnit = (money) => {
+  let resultValue;
   const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  resultValue = unit.map((v, i) => {
+    let unitCount;
+    if (i !== 0) {
+      const charge = money % unit[i - 1];
+      unitCount = parseInt(charge / v);
+    } else {
+      unitCount = parseInt(money / v);
+    }
+    return unitCount;
+  });
+
+  return resultValue;
 };
 
-/**
- * [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
- * 1. 값을 나눈 몫이 0이 아닌 경우, 계산 후 저장 및 값 빼기.
- * 2. 반복
- *
- *
- *
+/*
+ const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+ 1. 값을 나눈 몫이 0이 아닌 경우, 계산 후 저장 및 값 빼기.
+ 2. unit.length만큼 반복
+
  */
