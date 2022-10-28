106a9e31235603989981004f8e112294305bb952
Dean <yjj052800@gmail.com>
2022-10-28 16:14:12 +0900

    REFACTOR: [3번] 기능 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,24 +1,25 @@
 function problem3(number) {
   if (inputExecption(number)) {
     var answer;
-    let cnt = 0;
-    for (i = 1; i <= number; i++) {
-      let numberCompare = String(i).match(/[369]/g);
-      console.log(numberCompare);
-      if (numberCompare != null) cnt += numberCompare.length;
-    }
-    answer = cnt;
-    
+    answer = clapCnt(number);
     return answer;
   }
-  
 }
-
-console.log(problem3(33));
-module.exports = problem3;
 
 function inputExecption(num) {
   let inputCheck = true;
   if (num < 1 || num > 1000) inputCheck = false;
   return inputCheck;
 }
+
+function clapCnt(num) {
+  let cnt = 0;
+  for (i = 1; i <= num; i++) {
+    let numberCompare = String(i).match(/[369]/g);
+    if (numberCompare != null) cnt += numberCompare.length;
+  }
+  return cnt;
+}
+
+console.log(problem3());
+module.exports = problem3;
