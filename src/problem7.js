fdce67af95b304a69eab4437d020a9ed747184a3
lee-il-woo <empty001430@gmail.com>
2022-10-28 20:04:02 +0900

    문제7 유저와 친구인 아이디 추출 함수 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,18 @@
+
+//유저와 친구인 배열을 리턴하는 함수
+function searchFriends(user, friends){
+  let user_friends = [];
+  friends.map((element)=>{
+    if(element[0] == user){
+      user_friends.push(element[1])
+    }
+    if(element[1] == user){
+      user_friends.push(element[0])
+    }
+  })
+  return user_friends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
