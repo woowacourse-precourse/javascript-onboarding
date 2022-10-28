1923f83a3daa8fcb716eaf7ccd2a4d789da6a638
nerdchanii <nerdchanii@gmail.com>
2022-10-28 17:27:42 +0900

    feat: [PROBLEM2] 복호화 함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -11,4 +11,16 @@ function problem2(cryptogram) {
   return regex.test(string);
 }
 
+
+/**
+ * 암호문을 복호화한다.
+ * @param {string} cryptogram
+ * @param {regex} REGEX
+ * @param {string | (match:string)=>string} replacer
+ * @returns
+ */
+ function decrypto(cryptogram, REGEX, replacer) {
+  return cryptogram.replace(REGEX, replacer);
+}
+
 module.exports = problem2;
