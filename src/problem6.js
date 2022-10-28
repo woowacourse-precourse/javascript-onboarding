5447a102d256409e04c13bf5fb97bea276f65e8b
jihun1233 <leejihun1233@gmail.com>
2022-10-28 21:19:56 +0900

    중복 확인 함수 구현
    - 주어진 문자열을 닉네임으로 가지고있다면 해당 이메일을 결과 set에 추가하는 함수 구현
    
    - 자기자신을 포함하기 때문에 결과가 하나라면 자기자신을 추가하지않고 끝내도록 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
+
 function problem6(forms) {
   var answer;
+  const results = new Set()
+  
+  const findDuplications = (word) => {
+    const matches = forms.filter(form => {
+      const [, nickName] = form
+
+      return nickName.match(word)
+    })
+    if(matches.length === 1)return
+    matches.forEach(match => {
+      const [email] = match
+
+      results.add(email)
+    })
+  }
+
   return answer;
 }
 
