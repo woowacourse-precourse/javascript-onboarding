1b3d36954decd6b5e2931a80f033c8d103aaac8e
leeuihyun <meta212121@naver.com>
2022-10-28 22:41:05 +0900

    feat : problem5 돈의 단위에 따라 변환 후 반환

--- src/problem5.js
+++ src/problem5.js
@@ -1,10 +1,25 @@
+function classfication(result, money) {
+  let start = 50000;
+  result = result.map((item, index) => {
+    item += parseInt(money / start);
+    money = parseInt(money % start);
+    if (start === 10) {
+      start /= 10;
+    } else if (index % 2 === 0) {
+      start /= 5;
+    } else {
+      start /= 2;
+    }
+    return item;
+  });
+  return result;
+}
 function makeArray(number) {
   return new Array(number).fill(0);
 }
 function problem5(money) {
   let answer = makeArray(9);
-
-  return answer;
+  return classfication(answer, money);
 }
 
 module.exports = problem5;
