a9d98183180e0009d40f06c9107384759c26aa8b
chee9835 <huisu121@gmail.com>
2022-10-28 21:47:17 +0900

    feat: 정규식 이용해서 포함된 숫자 갯수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  var regExp = /3|6|9/g;
+  var answer = 0;
+
+  for (var i = 1; i <= number; i++) {
+    if (regExp.test(i)) answer += String(i).match(regExp).length;
+  }
+
   return answer;
 }
 
