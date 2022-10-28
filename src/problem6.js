303b8d32d30574b4d6aefebf9ec7d77b9b3ff7e4
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-29 00:07:08 +0900

    feat: (P6) 크루 닉네임 두 글자씩 저장 & 닉네임 중복 확인을 위한 순회 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -2,12 +2,22 @@ function problem6(forms) {
   // Map(닉네임 두 글자씩 저장), Set(중복 이메일 저장) 선언
   let twoLetterMap = new Map();
   let duplicateEmailSet = new Set();
+
+  // 순회해가며 Map에 크루들의 닉네임 두 글자씩 저장
+  forms.map((form) => {
+    setMapTwoLetter(form, twoLetterMap);
+  });
+
+  // 순회해가며 Map에 크루 닉네임 중복 확인
+  forms.map((form) => {
+    isDuplicateNickname(form, duplicateEmailSet, twoLetterMap);
+  });
 }
 
 // 크루들의 닉네임 2글자씩 저장하는 기능 (map 사용)
-function setMapTwoLetter() {}
+function setMapTwoLetter(form, twoLetterMap) {}
 
 // 크루 닉네임 중복 확인 기능
-function isDuplicateNickname() {}
+function isDuplicateNickname(form, duplicateEmailSet, twoLetterMap) {}
 
 module.exports = problem6;
