b37a48533892ba9506bd289edecdbf9e84fd3935
JH <vvaa277@gmail.com>
2022-10-28 21:56:56 +0900

    Feat: 1~2 기능구현
    #P6

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,13 @@
 function problem6(forms) {
-  
+  const twoChaArr = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    const nameSplited = forms[i][1].split("");
+    for (let j = 0; j < nameSplited.length - 1; j++) {
+      const dup = nameSplited[j] + nameSplited[j + 1];
+      twoChaArr.push(dup);
+    }
+  }
 }
 
 module.exports = problem6;
