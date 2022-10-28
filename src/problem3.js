3ffebebf009a1219f49c8c1c6fc6546437f67b8d
hxezin <leehxejin@gmail.com>
2022-10-28 20:48:49 +0900

    feat: 문제3 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const temp = String(i);
+    const regex = /[3|6|9]/g;
+    if (regex.test(temp)) {
+      answer += temp.match(regex).length;
+    }
+  }
+
   return answer;
 }
 
