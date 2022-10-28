721b7274bc4c3d0f2ba2776c73f60221ec7cf9f7
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-28 16:28:30 +0900

    feat: problem7 getScoreTogetherFriend Func

--- src/problem7.util.js
+++ src/problem7.util.js
@@ -1,9 +1,20 @@
+const TOGETHER_FIREND_SCORE = 10;
+const VISTOR_SCORE = 1;
 // 아는 친구 list 만들기
 const getUserFriend = (user, friends) =>
   friends.filter((friend) => friend.includes(user));
 
 // 함께 아는 친구 중 user를 제외하고 score 계산하기
-const getScoreTogetherFriend = (user, friends) => {};
+const getScoreTogetherFriend = (user, friends) => {
+  const newFriends = friends.flat();
+  const obj = {};
+  for (const friend of newFriends) {
+    obj[friend] ??= 0;
+    obj[friend] += TOGETHER_FIREND_SCORE;
+  }
+  delete obj[user];
+  return obj;
+};
 
 // 방문 user 중 친구 제거하기
 const getVisitorDeleteFriend = (visitors, firends) => {};
