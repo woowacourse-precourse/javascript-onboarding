de125bce4207b8928bf65211049f6b05278890c8
knh6269 <knh6269@gmail.com>
2022-10-28 19:29:22 +0900

    feat: 미션1) 유저의 가장 높은 점수를 판별하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -3,6 +3,16 @@ function scoreSplit(user,amount,index,array){
   array.push(Math.floor(user[index] / amount));
   user[index] = user[index] % amount;
 }
+
+//유저의 가장 높은 점수를 판별하는 함수
+function getBestScore(array, best){
+  const plus = array.reduce((sum, cur) => sum + cur);
+  const mul = array.reduce((sum, cur) => sum * cur);
+  best = best < plus? plus : best;
+  best = best < mul? mul : best;
+  return best;
+}
+
 }
 
 module.exports = problem1;
