998c900214dce520b6bdcc75f9632642622c1634
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-28 20:51:42 +0900

    feat: problem7.js에 사용자의 친구를 구하는 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,18 @@
+const getUserFriends = (user, friends) => {
+  const userFriends = [];
+  
+  friends.forEach((friend) => {
+    if (friend.includes(user) && friend[0] === user) {
+      userFriends.push(friend[1]);
+    }
+    if (friend.includes(user) && friend[1] === user) {
+      userFriends.push(friend[0]);
+    }
+  });
+
+  return userFriends;
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
