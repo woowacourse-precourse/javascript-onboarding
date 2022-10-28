a6b704f6a7931bdb6a156f4dfde41ea4c9429360
sujungYu <su_moment@naver.com>
2022-10-28 17:30:31 +0900

    feat(PROBLEM):배열, Math.floor, 나머지 등을 이용해 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const changeMoneyArr = [];
+
+  for(let i=0; i<moneyArr.length; i++) {
+    let own = Math.floor(money/moneyArr[i]);
+    money = money%=moneyArr[i];
+    changeMoneyArr.push(own);
+  }
+  return changeMoneyArr;
 }
 
 module.exports = problem5;
