58380e2c4745d5e1e462400e021efe5429aa3886
saram <yujun1018@naver.com>
2022-10-28 23:11:56 +0900

    [문제5] feat: 화폐 금액이 큰 순서대로 돈을 화폐로 나누는 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  currency.forEach((v) => {
+    money / v;
+    money %= v;
+  });
 }
 
 module.exports = problem5;
