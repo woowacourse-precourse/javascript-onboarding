c40f4cc6ac4bf2299be01643c3783e1f11c8931b
Leejin-Yang <ejin1996@gmail.com>
2022-10-28 18:54:14 +0900

    feat(problem2): 연속되는 중복 문자 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,32 @@
+function deleteDuplicateCharacters(cryptogram) {
+  const deletedString = [...cryptogram].reduce((acc, cur, i, arr) => {
+    if (cur === arr[i + 1]) {
+      return acc;
+    }
+
+    if (cur === arr[i - 1]) {
+      return acc;
+    }
+
+    acc += cur;
+    return acc;
+  }, '');
+
+  return deletedString;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
 }
 
 module.exports = problem2;
+
+function test() {
+  console.log(deleteDuplicateCharacters('browoanoommnaon'));
+  console.log(deleteDuplicateCharacters('zyelleyz'));
+  console.log(deleteDuplicateCharacters('zyellleyz'));
+  console.log(deleteDuplicateCharacters('zyeleyz'));
+}
+
+test();
