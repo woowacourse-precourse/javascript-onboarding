a46d55679324654b3dbd6b653526ebbf17a20b37
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-28 10:45:59 +0900

    ✨ 순회하면서 대소문자에 맞게 변환하는 과정

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,15 @@ function problem4(word) {
   // 1) 모든철자를 대소문자 구분하여 만드는 과정
   const alphabet_L = "abcdefghijklmnopqrstuvwxyz";
   const alphabet_U = alphabet_L.toUpperCase();
+
+  // 3) 순회하면서 대소문자에 맞게 변환하는 과정
+  for (let spell of word) {
+    if (alphabet_L.includes(spell)) {
+      answer += convert(spell, alphabet_L);
+    } else if (alphabet_U.includes(spell)) {
+      answer += convert(spell, alphabet_U);
+    }
+  }
   return answer;
 }
 
