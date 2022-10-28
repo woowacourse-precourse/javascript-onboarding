8864cb78de05767c1303d6c7c051fe1f94994945
Hwang Seong Taek <hst007@naver.com>
2022-10-28 12:46:27 +0900

    docs: 구현사항 docs 경로 수정 problem(1-5).js > PROBLEM(1-5).md

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,3 @@
-/* 
-구현사항
-1. cryptogram 문자열을 spilt()으로 분리하기
-2. 연속되는 중복 문자를 제외한 문자를 result 배열에 저장하기
-3. result 배열을 join('')을 통해 문자열 return 하기
-*/
-
 function problem2(cryptogram) {
 
   // 문자열 분리
--- src/problem3.js
+++ src/problem3.js
@@ -1,11 +1,3 @@
-/*
-구현사항
-1. 3, 6, 9에 해당하는 정규 표현식과 박수 횟수가 저장 될 clap 변수 선언
-2. 1 ~ number까지 for문을 통해 i가 3, 6, 9에 해당하는지 확인
-3. match 될 경우 반환된 배열의 길이 만큼 clap 증가시키기 (ex. 33일 경우 3이 2개 이므로 clap 2번 증가!)
-4. 저장된 clap 값 return 하기
-*/
-
 function problem3(number) {
   let clap = 0;
   const clapNum = /3|6|9/g;
