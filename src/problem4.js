af0592ad7f6690b0e4e6adfb45bea7d0111028c9
xownsdl0 <xownsdl0@naver.com>
2022-10-28 15:27:47 +0900

    <P4> 소문자를 청개구리 사전의 문자로 치환

--- src/problem4.js
+++ src/problem4.js
@@ -12,6 +12,10 @@ function problem4(word) {
       // i가 대문자인가, 청개구리 사전의 알파벳으로 치환 후 replaceWord에 담는다.
       let reChar = alphabet[reverseCase.indexOf(i)];
       replaceWord += i.replace(i, reChar);
+    } else if (i === i.toLowerCase()) {
+      // i가 소문자인가, 청개구리 사전의 알파벳으로 치환 후 replaceWord에 담는다.
+      let reChar = alphabet[reverseCase.indexOf(i.toUpperCase())].toLowerCase();
+      replaceWord += i.replace(i, reChar);
     }
   }
 
