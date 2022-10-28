606f4cbdd69653cc9de0e847cf8c911421b29ae9
hyukzz <ri_chard@naver.com>
2022-10-28 21:32:28 +0900

    feat: [문제2]암호해독 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
+function overlapFilter(str) {
+	if (str === "") return str;
+	let overlapAlphabet = false;
+	const dubleAlphabet = str.match(
+		/(a{2,})|(b{2,})|(c{2,})|(d{2,})|(e{2,})|(f{2,})|(g{2,})|(h{2,})|(i{2,})|(j{2,})|(k{2,})|(l{2,})|(m{2,})|(n{2,})|(o{2,})|(p{2,})|(q{2,})|(r{2,})|(s{2,})|(t{2,})|(u{2,})|(v{2,})|(w{2,})|(x{2,})|(y{2,})|(z{2,})|(.)/g
+	);
+
+	let filteringAlphabet = dubleAlphabet.filter(el => {
+		if (el.length === 2) {
+			overlapAlphabet = true;
+			return false;
+		}
+		return true;
+	});
+
+	let answer = filteringAlphabet.join("");
+
+	return overlapAlphabet ? overlapFilter(answer) : answer;
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+	let answer = overlapFilter(cryptogram);
+	return answer;
 }
 
 module.exports = problem2;
