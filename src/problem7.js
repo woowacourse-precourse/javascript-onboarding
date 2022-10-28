0dcaf7070f0d95a134de5b2e6c845f6d955ea50a
cys4585 <choizerowater@gmail.com>
2022-10-28 20:57:36 +0900

    fix: 문제7 디버그
     매개변수를 잘못 설정해서 버그가 있었는데,
     정보를 전역변수로  선언해놓고 사용하고 있었다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,9 @@
-function problem7(user, fren, visitors) {
+function problem7(user, friends, visitors) {
   let answer;
+  let userInfoList;
 
-  makeUserInfoList(user, friends, visitors);
+  userInfoList = makeUserInfoList(user, friends, visitors);
+  console.log(userInfoList);
   return answer;
 }
 
@@ -9,15 +11,14 @@ function makeUserInfoList(user, friends, visitors) {
   let userInfoList;
   let friendsOfUser;
 
-  friendsOfUser = findFriendsOfUser(friends);
+  friendsOfUser = findFriendsOfUser(user, friends);
   userInfoList = makeUserList(user, friends, visitors).map((user) => {
     return { id: user, score: 0, isFriend: friendsOfUser.includes(user) };
   });
-  console.log(userInfoList);
   return userInfoList;
 }
 
-function findFriendsOfUser(friends) {
+function findFriendsOfUser(user, friends) {
   return friends
     .filter((friend) => friend.includes(user))
     .map(([userA, userB]) => (userA === user ? userB : userA));
@@ -36,18 +37,18 @@ function makeUserList(user, friends, visitors) {
   return Array.from(userSet);
 }
 
-let user = "mrko";
-let friends = [
-  ["donut", "andole"],
-  ["donut", "jun"],
-  ["donut", "mrko"],
-  ["shakevan", "andole"],
-  ["shakevan", "jun"],
-  ["shakevan", "mrko"],
-];
-let visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
-
-problem7(user, friends, visitors);
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 
 module.exports = problem7;
 
