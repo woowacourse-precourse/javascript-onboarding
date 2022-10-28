c9e9c99505b5d0da1e56b6aa782bb01cec1e7a51
박정규 <srk99279@naver.com>
2022-10-28 16:07:02 +0900

    문제 5 구현. README 문제 5 업데이트

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  const currentList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = new Array(currentList.length).fill(0);
+
+  currentList.forEach((val, idx) => {
+    let quotient = Math.floor(money / val);
+    money -= quotient * val;
+    answer[idx] += quotient;
+  });
+
   return answer;
 }
 
