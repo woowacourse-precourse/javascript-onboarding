6cb539269a132b9223593ad2bd128fc6bea9faed
pinkishincoloragain <smb1103@gmail.com>
2022-10-28 16:47:23 +0900

    feat: solved problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const splitted = [...word];
+  return splitted.map(frogConvert).join('');
 }
+
+const frogConvert = (x) => {
+  if (x >= 'a' && x <= 'z') return convertChar(x, true);
+  else if (x >= 'A' && x <= 'Z') return convertChar(x, false);
+  else return x;
+};
+
+const convertChar = (target, isLowerCase) => {
+  const INTERVAL = 25;
+  const base = isLowerCase ? 'a' : 'A';
+  const offset = target.charCodeAt(0) - base.charCodeAt(0);
+
+  return String.fromCharCode(base.charCodeAt(0) + (INTERVAL - offset));
+};
 
 module.exports = problem4;
