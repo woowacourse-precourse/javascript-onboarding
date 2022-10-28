074ffed651682b755378f8227c83629530b570b3
AnJaeSeong <dktmzh6@gmail.com>
2022-10-29 02:08:04 +0900

    feat: problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  const regex = /[369]/g;
+  for (let i=1;i<=number;i++) {
+    let res = [...i.toString().matchAll(regex)];
+    count += res.length;
+  }
+  return count;
 }
 
 module.exports = problem3;
