3f84db0ed3ecede4f876fe8fd9969f2df0969b99
Hwang Juhee <julia0276@naver.com>
2022-10-29 01:29:11 +0900

    :sparkles: [feat] 연속적으로 중복되는 문자 삭제 기능 구현 #P2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
+  let str = cryptogram.split("");
+  const recursion = (s) => {
+    for (let i = 0; i < s.length; i++) {
+      if (s[i] === s[i + 1]) {
+        s.splice(i, 2);
+        recursion(s);
+      }
+    }
+  };
+  recursion(str);
+  let answer = str.join('');
   return answer;
 }
 
