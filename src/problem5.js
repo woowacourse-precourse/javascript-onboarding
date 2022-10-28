089867079452927dbd5d17bcd5825cd17298b831
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-28 13:16:28 +0900

    feat<problem5>: Implement exception

--- src/problem5.js
+++ src/problem5.js
@@ -6,14 +6,23 @@
  * 1. 1이상 1000000
  */
 
+const isException = money => {
+  return !(money >= 1 && money <= 1000000)
+}
+
 function problem5(money) {
   var answer = [];
   var units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  if(isException(money)) {
+    throw 'Input range error!'
+  }
+
   units.map((unit) => {
     answer.push(parseInt(money / unit));
-    money = parseInt(money % unit);
+    money %= unit;
   })
+
   return answer;
 }
 
