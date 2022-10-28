cee31e3dbdef9d81d6594eac6e585cfea5534a1a
BaeDongWoo <ehddn5410@naver.com>
2022-10-28 23:21:31 +0900

    대문자 일때와 소문자 일때를 나누어 아스키 코드로 리버스 후 저장

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
   var answer;
+  let rev_word=[];
+  let str=[];
+  for(let i=0;i<word.length;i++){
+    if(65<=word.charCodeAt(i)&&word.charCodeAt(i)<=90){//대문자 일때
+      str[i]=String.fromCharCode(155-word.charCodeAt(i));//대문자의 리버스 숫자
+      rev_word[i]=str[i];
+    }
+    else if(97<=word.charCodeAt(i)&&word.charCodeAt(i)<=122){//소문자 일때
+      str[i]=String.fromCharCode(219-word.charCodeAt(i));//소문자의 리버스 숫자
+      rev_word[i]=str[i];
+    }
+  }
+  answer=rev_word.join("");
   return answer;
 }
 
