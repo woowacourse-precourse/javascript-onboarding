1f092b57467151f4b7b7cd368b12b05faf9a9d7b
sungjihyun <jhsung23@gmail.com>
2022-10-28 16:50:37 +0900

    Feat: problem 5: 거스름돈 개수 계산 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,17 @@
+const getEachChangeCount = (money) => {
+  let result = [];
+  const change = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < change.length; i++) {
+    const count = Math.floor(money / change[i]);
+    result.push(count);
+    money %= change[i];
+  }
+  return result;
+};
+
 function problem5(money) {
   var answer;
+  answer = getEachChangeCount(money);
   return answer;
 }
 
