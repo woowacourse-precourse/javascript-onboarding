a38d4662a0cc206880bd70c87e2ac1dd96f325b7
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-10-28 16:04:44 +0900

    feat(problem5): 예외 처리 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,45 @@
+const INPUT = {
+  numberType: 'number',
+  minRange: 1,
+  maxRange: 1000000,
+};
+
+const RESULT = {
+  exception: -1,
+};
+
 function problem5(money) {
-  var answer;
-  return answer;
+  if (isWrongInput(money)) {
+    return RESULT.exception;
+  }
+}
+
+function isWrongInput(input) {
+  if (isWrongValueOfInput(input)) {
+    return true;
+  }
+
+  if (isWrongTypeOfInput(typeof input)) {
+    return true;
+  }
+
+  if (isWrongRangeOfInput(input)) {
+    return true;
+  }
+
+  return false;
+}
+
+function isWrongValueOfInput(v) {
+  return !v;
+}
+
+function isWrongTypeOfInput(type) {
+  return type !== INPUT.numberType;
+}
+
+function isWrongRangeOfInput(range) {
+  return range < INPUT.minRange || range > INPUT.maxRange;
 }
 
 module.exports = problem5;
