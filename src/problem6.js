f9113275de8c17e167899001ae607a522c51ecfa
BBIYAC <kiy7833@gmail.com>
2022-10-28 23:07:26 +0900

    refactor(problem6): separate feature and add break
    - sort 함수 분리 및 불필요한 반복 중단을 위한 break 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,9 @@
+// 해당 지원자의 이메일 오름차순 정렬
+function getAscSortedArray(array) {
+  array.sort();
+  return array;
+}
+
 // 같은 글자가 연속적으로 포함되는 지원자 추출 기능
 function problem6(forms) {
   let emails = {},
@@ -10,6 +16,7 @@ function problem6(forms) {
         if (stored_name.includes(name.substr(i, 2))) {
           if (!restricted_emails.includes(emails[stored_name])) restricted_emails.push(emails[stored_name]);
           if (!restricted_emails.includes(email)) restricted_emails.push(email);
+          break;
         }
       }
     }
@@ -17,10 +24,8 @@ function problem6(forms) {
     emails[name] = email;
     names.push(name);
   }
-
-  // 해당 지원자의 이메일 오름차순 정렬
-  restricted_emails.sort();
-  return restricted_emails;
+  
+  return getAscSortedArray(restricted_emails);
 }
 
 module.exports = problem6;
