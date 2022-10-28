61fd35cf83eea7d3a2f5011676fd7331ac08cff0
vinivin153 <vinivin153@naver.com>
2022-10-28 16:18:33 +0900

    feat: 돈 변환하는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
+  const answer = exchangeMoney(money);
   return answer;
+}
+
+function exchangeMoney(money) {
+  const moneyUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (let i = 0; i < moneyUnits.length; i++) {
+    const share = Math.floor(money / moneyUnits[i]);
+    if (share) {
+      result[i] = share;
+      money -= share * moneyUnits[i];
+    }
+  }
+  return result;
 }
 
 module.exports = problem5;
