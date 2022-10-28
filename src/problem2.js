e38681374685fe63a7cf2af461986d9c8db5b807
heyheyley <jeonheyley@gmail.com>
2022-10-28 21:24:05 +0900

    문제3 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,5 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+  
+  }
 
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for(i=1; i <= number; i++){
+    //3,6,9를 골라내기 위해 문자로 변경한 후, 정규표현식을 사용하여 3,6,9가 들어간 수를 찾는다 
+    let current = i.toString().match(/[3+6+9]/g)
+    //들어간 갯수만큼 answer에 더해준다
+    if(current){
+      answer += current.length;
+    }
+  }
   return answer;
 }
 
