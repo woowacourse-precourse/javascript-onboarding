3efcd074c2bb7e3d057fbe6f540e911124186cd8
yu-retel <dbdudals100@naver.com>
2022-10-28 18:07:00 +0900

    feat: [P6] 연속되는 문자 반환기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -4,3 +4,17 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
+
+function continuousNicknames(forms){
+  let nicknameList=forms.map((el)=>el[1].split(''))
+  let continuousArr=[];
+  
+  nicknameList.map((nickname)=>{
+    for(let i=0; i<nickname.length; i++){
+      if((nickname[i]+nickname[i+1]).length===2){
+        continuousArr.push(nickname[i]+nickname[i+1]);
+      }
+    }
+  });
+  return continuousArr;
+}
