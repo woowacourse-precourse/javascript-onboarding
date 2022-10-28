37eff3c7459dda6a91a489163d3392e704d162bd
minjaeKim <smallkdb@gmail.com>
2022-10-29 00:02:32 +0900

    feat: [Problem4] Add ASCIICODE constant object
    문제 해결을 위해 필요한 아스키 코드 값을 담고있는 ASCIICODE 상수 객체를 추가하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,13 @@
+const ASCIICODE = {
+  UPPERCASE_A: 65,
+  UPPERCASE_Z: 90,
+  LOWERCASE_A: 97,
+  LOWERCASE_Z: 122,
+  SPACE: 32
+}
+
+Object.freeze(ASCIICODE);
+
 function problem4(word) {
   var answer;
   return answer;
