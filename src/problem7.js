ae3c03ebef06734e686a05aade06ee974f6e95d0
hwnim5324 <hwnim5324@naver.com>
2022-10-28 15:12:30 +0900

    7번 기능2 완료.

--- src/problem7.js
+++ src/problem7.js
@@ -10,6 +10,7 @@ function problem7(user, friends, visitors) {
 
   return sortedScore.slice(0,5).map((el)=>el.name);
 
+  //1. user와 친구인 목록 배열로 생성.
   //2. user와 친구인 목록의 친구인 목록 조회하여 점수 가산(+10).
   //3. 타임 라인에 방문한 사용자 조회하여 점수 가산(+1).
   //4. 조건에 맞게 정렬하여 반환.
@@ -21,6 +22,18 @@ function problem7(user, friends, visitors) {
       }
     });
   }
+
+  function addScoresFrinedsWith(){
+    friends.forEach((el)=>{
+      for(const friend of frinedsWithUser){
+        const knowFriend = el[1-el.indexOf(friend)];
+        if(el.includes(friend) && knowFriend!==user && !frinedsWithUser.includes(knowFriend)){
+          scores[knowFriend] = scores[knowFriend] ? scores[knowFriend]+10 : 10;
+        }
+      }
+    });
+  }
+
 }
 
 module.exports = problem7;
