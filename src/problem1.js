01b5c08f7ddc3c376b028f898d0095d03810b0ec
woo-jk <201702042@o.cnu.ac.kr>
2022-10-28 17:04:27 +0900

    feat: 각 자리 수 더하는 함수, 곱하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,27 @@
+const splitNumber = (number) => {
+  const numToString = number.toString();
+  const splittedNumber = numToString.split("");
+  return splittedNumber;
+};
+
+const addNumber = (number) => {
+  const splittedNumber = splitNumber(number);
+  const sum = splittedNumber.reduce(
+    (accumulator, currentNumer) => accumulator + currentNumer,
+    0
+  );
+  return sum;
+};
+
+const mulNumber = (number) => {
+  const splittedNumber = splitNumber(number);
+  const product = splittedNumber.reduce(
+    (accumulator, currentNumer) => accumulator * currentNumer,
+    0
+  );
+  return product;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
