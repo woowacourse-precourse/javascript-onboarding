cc9a14bb55fcb78fd4c8f507f937735726b2b23b
kang <mdiu2203@naver.com>
2022-10-29 02:11:32 +0900

    feat(PROBLEM1): check exception cases from page numbers

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,25 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function isException(leftPage, rightPage) {
+  return (
+      isNotPageDifference1(leftPage, rightPage) ||
+      isNotPageRange(leftPage) ||
+      isNotPageRange(rightPage) ||
+      isInvalidPage(leftPage, rightPage)
+  );
+}
+
+function isNotPageDifference1(leftPage, rightPage) {
+  return rightPage - leftPage !== 1;
+}
+
+function isNotPageRange(page) {
+  return page < 1 || page > 400;
+}
+
+function isInvalidPage(leftPage, rightPage) {
+  return leftPage % 2 === 0 || rightPage % 2 === 1;
+}
+
 module.exports = problem1;
