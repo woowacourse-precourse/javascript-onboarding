a290f128954979db825ec4f7e6081082af4c7ca8
Hoon <si932174@gmail.com>
2022-10-28 11:24:39 +0900

    problem4 : 대문자 변환 함수 제작

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
   var answer;
+
+  const capitalConverter = capitalLetter => {
+
+    const capitalLetterNumber = capitalLetter.charCodeAt();
+    const convertedNumber = 155 - capitalLetterNumber;
+    const convertedLetter = String.fromCharCode(convertedNumber);
+
+    return convertedLetter;
+  }
+
+  
+
   return answer;
 }
 
