89f129fcec5dc0024355e2167ec3a4384eb705b9
jihun1233 <leejihun1233@gmail.com>
2022-10-28 20:42:38 +0900

    같은 친구를 가진 친구가 아닌 대상에게 10점 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,8 @@
 function problem7(user, friends, visitors) {
   var answer;
 
+  const scores = {}
+
   const userFriends = friends.reduce((acc, friend)=>{
     const [userA, userB] = friend
     if(userA === user) acc.add(userB)
@@ -8,8 +10,27 @@ function problem7(user, friends, visitors) {
     return acc
   },new Set())
 
+  friends.forEach(friend=>{
+    const [userA, userB] = friend
+    if(userA === user || userB === user)return
+
+    if(userFriends.has(userA) && !userFriends.has(userB)){
+      addNumberToObject(10, scores, userB)
+    }
+    if(userFriends.has(userB) && !userFriends.has(userA)){
+      addNumberToObject(10, scores, userA)
+    }
+  })
 
   return answer;
+}
+
+const addNumberToObject = (number, object, key) => {
+  if(!object[key]){
+    object[key] = number
+  }else{
+    object[key] += number
+  }
 }
 
 module.exports = problem7;
