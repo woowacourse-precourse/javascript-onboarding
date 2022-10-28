7d633c1e4964a6cd2677629303aff2720e1e7a3e
sunub <bsc5672@gmail.com>
2022-10-29 00:07:57 +0900

    cryptogram interpretation
    I solved this problem using two different pointers.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,35 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (cryptogram.length < 1) {
+    return cryptogram;
+  } else if (cryptogram.length === 2) {
+    if (cryptogram[0] !== cryptogram[1]) {
+      return cryptogram;
+    } else {
+      return "";
+    }
+  }
+
+  const seperate = cryptogram.split("");
+  let ptr1 = 0;
+  let ptr2 = 1;
+  while (ptr2 < seperate.length) {
+    if (seperate[ptr1] !== seperate[ptr2]) {
+      ptr1++;
+      ptr2++;
+    } else if (seperate[ptr1] === seperate[ptr2]) {
+      const beforIndex = ptr1 - 1;
+      seperate.splice(ptr1, 2);
+      ptr1 = beforIndex;
+      ptr2 = beforIndex + 1;
+    }
+  }
+
+  if (seperate.length > 0) {
+    const answer = seperate.join("");
+    return answer;
+  } else {
+    return "";
+  }
 }
 
 module.exports = problem2;
