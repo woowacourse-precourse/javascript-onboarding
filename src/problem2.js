15269da4567e5d712a01107e328726ba672b4cfa
박정규 <srk99279@naver.com>
2022-10-28 16:01:25 +0900

    문제 2 완료. README 업데이트

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,18 @@
 function problem2(cryptogram) {
   var answer;
+  let spellList = cryptogram.split("");
+
+  let i = 0;
+
+  while (i < spellList.length) {
+    if (spellList[i] === spellList[i + 1]) {
+      spellList.splice(i, 2);
+      i = -1; // 문자열의 처음부터 다시 확인하기 위해 -1를 i에 할당
+    }
+    i++;
+  }
+
+  answer = spellList.join("");
   return answer;
 }
 
