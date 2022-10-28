1897d14c3f43a5c2067afb0bf3d794a5b1fd9693
kang <mdiu2203@naver.com>
2022-10-29 02:46:02 +0900

    feat(PROBLEM7): make friend table from friends

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,23 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function makeFriendsTable(friends) {
+  const friendTable = {};
+  for (let [friendA, friendB] of friends) {
+    addFriendToTable(friendTable, friendA, friendB);
+  }
+
+  return friendTable;
+}
+
+function addFriendToTable(friendTable, friendA, friendB) {
+  friendTable[friendA]
+      ? friendTable[friendA].add(friendB)
+      : (friendTable[friendA] = new Set([friendB]));
+
+  friendTable[friendB]
+      ? friendTable[friendB].add(friendA)
+      : (friendTable[friendB] = new Set([friendA]));
+}
+
 module.exports = problem7;
