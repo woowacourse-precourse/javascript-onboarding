6d2e1adc9f69dccfc910acd05f2974fde66b2922
seongminn <berry9901@gmail.com>
2022-10-28 20:42:56 +0900

    feat(problem3): 현재 숫자에서 3, 6, 9의 개수만큼 answer에 1을 더하는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,13 @@
 function problem3(number) {
   let answer = 0;
 
-  for (let i = 1; i <= number; i++) {}
+  for (let i = 1; i <= number; i++) {
+    String(i)
+      .split('')
+      .forEach((el) => {
+        if (el === '3' || el === '6' || el === '9') answer++;
+      });
+  }
 
   return answer;
 }
