6f76e4fefe4cdb778f59db9934559ff5a001b120
Sumin Kim <ksumin0730@hufs.ac.kr>
2022-10-28 11:39:16 +0900

    feat(Problem7): 예외처리

--- src/problem7.js
+++ src/problem7.js
@@ -56,31 +56,42 @@ function knowFriend(arr, user, relation, algorithm){
   return algorithm;
 }
 
-function problem7(user, friends, visitors) {
-  //const answer = friendRelation(friends)
-  const algorithm = visitCount(visitors, user, friends);
-  const relation = friendRelation(friends);
-  const friendScore = knowFriend(relation, user, relation, algorithm);
-  const sortByscore = Object.fromEntries(
-    Object.entries(friendScore).sort(([,a],[,b]) => a > b? -1: 1)
-  );
+function exception(user, friends, relation){
+  if (user.length < 1 || user.length > 30){
+    return false;
+  }
+  else if (user.toLowerCase() !== user){
+    return false;
+  }
+  else if (friends.length < 1 || friends.length > 10000){
+    return false;
+  }
+  Object.keys(relation).map(id => {
+    if (id.length < 1 || id.length > 30){
+      return false;
+    }
+  })
+  return true;
+}
 
-  if (Object.keys(sortByscore).length > 5){
-    return Object.keys(sortByscore).slice(0,5);
+function problem7(user, friends, visitors) {
+  
+  const relation = friendRelation(friends);
+  if (exception){
+    const algorithm = visitCount(visitors, user, friends);
+    const friendScore = knowFriend(relation, user, relation, algorithm);
+    const sortByscore = Object.fromEntries(
+      Object.entries(friendScore).sort(([,a],[,b]) => a > b? -1: 1)
+    );
+
+    if (Object.keys(sortByscore).length > 5){
+      return Object.keys(sortByscore).slice(0,5);
+    }
+    else{
+      return Object.keys(sortByscore);
+    }
   }
-  else{
-    return Object.keys(sortByscore);
-  }
+  
 }
 
 module.exports = problem7;
-
-console.log(problem7('mrko', [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ], ["bedi", "bedi", "donut", "bedi", "shakevan"]))
-
-/*
-user = "mrko";
-friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ];
-visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
-result = ["andole", "jun", "bedi"]
-
-*/
