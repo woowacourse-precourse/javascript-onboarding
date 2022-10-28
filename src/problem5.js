a981235ba443d0e1a586d9b0bb6f35d3c383ce62
Ryong-E <think8867@gmail.com>
2022-10-28 16:27:44 +0900

    refactor: PROBLEM5 사용화폐 수 변수명 변경

--- src/problem5.js
+++ src/problem5.js
@@ -3,11 +3,11 @@ function problem5(money) {
   const CURRENCY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const amount = [];
   for (let bill of CURRENCY) {
-    const useBillCount = Math.floor(total / bill);
-    if (useBillCount >= 1) {
-      amount.push(useBillCount);
-      total -= bill * useBillCount;
-    } else if (useBillCount < 1) {
+    const currencyCount = Math.floor(total / bill);
+    if (currencyCount >= 1) {
+      amount.push(currencyCount);
+      total -= bill * currencyCount;
+    } else if (currencyCount < 1) {
       amount.push(0);
     }
   }
