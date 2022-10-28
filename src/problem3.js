1ea8708354d04df51742aec70e92135539d72950
murpin <qw04011@gmail.com>
2022-10-28 14:41:26 +0900

    [문제 3번] docs: 기능 분석 주석 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,10 @@
+/*
+ * 기능 분석
+ * [] 3부터 number까지 있는 모든 숫자들을 지나가는 기능
+ * [] 숫자에서 3,6,9의 존재를 확인하는 정규표현식 작성
+ * [] 숫자에서 3,6,9의 갯수를 파악하고 더하는 기능 
+ */
+
 function problem3(number) {
   var answer;
   return answer;
