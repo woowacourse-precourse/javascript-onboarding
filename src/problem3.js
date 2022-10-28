0ca4b8b210db4d0b6a0888ce0247647199165f8e
Serin-Kim <serin9864@gmail.com>
2022-10-28 17:52:33 +0900

    feat: PROBLEM3 풀이 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
+const threeSixNine = [3, 6, 9];
+
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const nums = String(i).split("");
+    nums.forEach((num) => {
+      if (threeSixNine.includes(Number(num))) answer++;
+    });
+  }
+
   return answer;
 }
+
+/*
+// for test 
+const numbers = [13, 33];
+
+for (number of numbers) {
+  console.log(problem3(number));
+}
+*/
 
 module.exports = problem3;
