82c52be14cc9abf9d621613f6a28ebabef8f07b7
cys4585 <choizerowater@gmail.com>
2022-10-28 10:44:30 +0900

    comment: 문제7 함께아는 친구 찾는 법

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,14 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+/*
+** 내 친구의 친구다 -> +10점
+mrko: (donut, shakevan)
+
+donut: (andole, jun, mrko)
+shakevan: (andole, mrko)
+
+andole: (donut, shakevan)
+jun: (donut)
+*/
