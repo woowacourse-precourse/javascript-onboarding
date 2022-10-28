3748e5e1f1ae2df9b65d488bb5375393abd541a3
Chris.c <cjs120dd@gmail.com>
2022-10-28 11:37:58 +0900

    refactor: 3번 문제의 가독성을 위한 리펙토링을 한다

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,11 @@
-const clapNumber = {
-    three: 3,
-    six: 6,
-    nine: 9
-}
-
 function problem3(number) {
     let answer = 0;
+    const clapNumbers = [3,6,9]
     
     for (let i = 1 ; i < number + 1 ; i ++) {
         let current = i;
         while (current !== 0) {
-            if (current % 10 === clapNumber.three || current % 10 === clapNumber.six || current % 10 === clapNumber.nine) {
+            if (clapNumbers.includes(current%10)) {
                 answer += 1;
             }
             current = Math.floor(current / 10);
