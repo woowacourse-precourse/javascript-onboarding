4a17f8ccd61bfb3512a75d0466fb36b901b546a2
SNUPI <joohaem@daum.net>
2022-10-28 20:53:54 +0900

    [ feat ] complete problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
+  let answer;
+  answer = getChangeArray(money);
   return answer;
+}
+
+function getChangeArray(money) {
+  const changes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let currentMoney = money;
+
+  return changes.map((change) => {
+    const changeCnt = Math.floor(currentMoney / change);
+    currentMoney %= change;
+
+    return changeCnt;
+  });
 }
 
 module.exports = problem5;
