19d7e0acbdc7a8349b99301611463c22f1370744
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-28 14:21:54 +0900

    Feat: seperate String in problem 6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const nickname = forms.map((x) => x[1]);
+  const seperate = (str) => {
+    const arr = [];
+    let cnt = 0;
+    let long = 2;
+    while (cnt < str.length) {
+      while (long < str.length + 1 && cnt + long < str.length + 1) {
+        arr.push(str.substring(cnt, cnt + long));
+        if (cnt === str.length) {
+          break;
+        } else {
+          long += 1;
+        }
+      }
+      long = cnt + 2;
+      cnt += 1;
+    }
+    return arr;
+  };
+  console.log(seperate(nickname[1]));
 }
 
 module.exports = problem6;
