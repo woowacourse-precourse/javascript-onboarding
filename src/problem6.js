0bbe9b8f0e57c54bb9fd2642155f8ae3cc9c9816
ilgon0110 <moses011010@gmail.com>
2022-10-28 15:44:54 +0900

    feat:연산 기능 구현(#problem6)
    연산 1-3 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -18,17 +18,24 @@ function problem6(forms) {
 
   //출력값 구하기
   let answer = [];
+  let check = Array.from({ length: forms.length }, () => 0);
   //기능목록 2-1 구현
   let result = new Set();
   for (let i = 0; i < forms.length; i++) {
+    //기능목록 1-3 구현
+    if (check[i] !== 0) continue;
     //예외처리
     //기능목록 3-1 구현
     if (forms[i][0].match(/@email.com/) === null) continue;
     for (let j = 0; j < forms.length; j++) {
       if (i === j) continue;
-      if (isRepeat(forms[i][1], forms[j][1]))
+      if (isRepeat(forms[i][1], forms[j][1])) {
         //기능목록 1-2 구현
-        result.add(forms[i][0], forms[j][0]);
+        result.add(forms[i][0]).add(forms[j][0]);
+        //기능목록 1-3 구현
+        check[i] = 1;
+        check[j] = 1;
+      }
     }
   }
   //기능목록 2-2 구현
