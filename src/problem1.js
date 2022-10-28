391bc588666da2ff3c703cdba4067865c9e65437
Inae Choi <inaechoi89@gmail.com>
2022-10-28 18:06:06 +0900

    feat(p1): add exception handling function

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,16 @@
 function problem1(pobi, crong) {
   var answer;
+
+  function except(user) {
+    user.forEach((page) => {
+      if (page === 1 || page === 2 || page === 399 || page === 400) return -1;
+    });
+    if (user[0] % 2 === 0 || user[1] % 2 !== 0) return -1;
+    if (user[1] !== user[0] + 1) return -1;
+    return 0;
+  }
+  let exception = except(pobi) || except(crong);
+
   return answer;
 }
 
