4601a91b38f3240f2bb82d05a8b6e0c812cc42c5
minsoo <als494@naver.com>
2022-10-29 01:24:50 +0900

    6번 문제 solved

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,21 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = i + 1; j < forms.length; j++) {
+      for (let k = 0; k < forms[i][1].length - 1; k++) {
+        let compareWord = forms[i][1][k] + forms[i][1][k + 1];
+        if (forms[j][1].includes(compareWord)) {
+          answer.push(forms[i][0], forms[j][0]);
+        }
+      }
+    }
+  }
+
+  answer = new Set(answer);
+  answer = [...answer];
+  answer.sort();
+
   return answer;
 }
 
