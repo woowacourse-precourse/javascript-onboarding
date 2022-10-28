a3ad4fadb50252b7d211a5cfc22f914e9d6f06da
ashleysheo <ashleysyheo@gmail.com>
2022-10-28 12:03:41 +0900

    feat(problem3): count claps in 3, 6, 9 clap game

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCount = 0;
+
+  for (let i = 3; i <= number; i += 1) {
+    let currentNumber = i;
+
+    while (currentNumber > 0) {
+      if (currentNumber % 10 === 3 || currentNumber % 10 === 6 || currentNumber % 10 === 9) {
+        clapCount += 1;
+      }
+
+      currentNumber = Math.floor(currentNumber / 10);
+    }
+  }
+
+  return clapCount;
 }
 
 module.exports = problem3;
