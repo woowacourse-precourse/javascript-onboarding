ffcca8f57f6519b055d07715c113e47a46c64eef
Woong <jiyko7@kyonggi.ac.kr>
2022-10-28 15:53:03 +0900

    5번 문제 해결
    5번 문제 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  var answer = [0,0,0,0,0,0,0,0,0];
+  var coin = [50000,10000,5000,1000,500,100,50,10,1];
+  
+  for (var i=0; i<coin.length; i++){
+    if (money/coin[i]>=1){
+      var temp = Math.floor(money/coin[i]);
+      answer[i] += temp;
+      money -= coin[i] * temp;
+    }
+  }
   return answer;
 }
 
