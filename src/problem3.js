5e8c817c4f29fea314b891aa50b8c465b8d42c84
GC-Park <kuyt3122@naver.com>
2022-10-28 20:24:34 +0900

    (3번)Feat:숫자에 3, 6, 9가 들어가는지 체크하는 함수 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,29 @@
+/*
+ 기능 목록
+  1) 숫자에 3, 6, 9가 들어가는지 체크 기능
+  2) 3, 6, 9 가 들어간다면 몇번 들어가는지 횟수 체크 기능
+
+*/
+
+function numberCheck(n, c){
+
+}
+
+function numberInclude(n){
+  let sum=0;
+  let str=n.toString(10);
+  if(str.includes('3')){
+    sum+=numberCheck(n, '3');
+  }
+  if(str.includes('6')){
+    sum+=numberCheck(n, '6');
+  }
+  if(str.includes('9')){
+    sum+=numberCheck(n, '9');
+  }
+  return sum;
+}
+
 function problem3(number) {
   var answer;
   return answer;
