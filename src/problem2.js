7278128808de18a54a443a716cff47c29c294c2a
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 12:01:54 +0900

    docs: 구현 기능 목록 추가
    필요 기능 목록 정리

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,9 @@
+// 0. 문자열 길이 검사
+// 1. 문자열 알파벳 소문자인지 검사
+// 2. 중복 문자열 검사 함수 구현
+// 3. 중복 문자열 제거 함수 구현
+// 4. 중복 문자열이 없을때까지 검사 후 반환값 리턴
+
 function problem2(cryptogram) {
   var answer;
   return answer;
