815f7dbfb13983044a7dd69fefafaeaf67bb8f0c
cho seonghoon <chsida655@gmail.com>
2022-10-28 16:31:35 +0900

    Add(Prob1) : 합, 곱 구하는 함수 선언

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,19 @@
+function getPageSum(num) {
+  let result = 0;
+  for (let i = 0; i < num.length; i++) {
+    result += num[i];
+  }
+  return result;
+}
+
+function getPageMulti(num) {
+  let result = 1;
+  for (let i = 0; i < num.length; i++) {
+    result *= num[i];
+  }
+  return result;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
