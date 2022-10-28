767d3a94c8f3c9a421a9537edf729d1472f8403e
An Sol <apdlvmf486@gmail.com>
2022-10-28 10:17:46 +0900

    6번: 예외처리 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,31 @@
 function problem6(forms) {
+
+  function checkException(crew){
+    const email = crew[0];
+    const nickName = crew[1];
+    const regexForEmail = /.+@email.com/;
+    const regexForNickName = /[^ㄱ-ㅎ가-힣]+/;
+
+    //글자 수 제한에 걸린 경우
+    if (email.length < 11 || email.length >= 20) return 3;
+    if (nickName.length < 1 || nickName.length >= 20) return 4;
+
+    //이메일 형식이 틀린 경우
+    if (!(regexForEmail.test(email))) return 1;
+
+    //닉네임이 한글이 아닌 경우
+    if (regexForNickName.test(nickName)) return 2;
+    
+    return 0;
+  }
+
+  for (let idx = 0; idx < forms.length; idx++){
+    if (checkException(forms[idx]) === 1) return `이메일 형식이 틀립니다: ${forms[idx][0]}`;
+    if (checkException(forms[idx]) === 2) return `닉네임이 한글이 아닙니다: ${forms[idx][1]}`;
+    if (checkException(forms[idx]) === 3) return `이메일의 글자 수를 확인해주세요: ${forms[idx][0]}`;
+    if (checkException(forms[idx]) === 4) return `닉네임의 글자 수를 확인해주세요: ${forms[idx][1]}`;
+  }
+
   const result = [];
   for (let i = 0; i < forms.length; i++){
     const testCrew = forms.shift();
