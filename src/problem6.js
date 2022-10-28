32b0e78900efe58adb334241e50c9fea402463d5
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-28 19:20:03 +0900

    문제6. 추출한 문자를 포함한 닉네임이 있고, answer에 추가한 적이 없으면 추가

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,18 @@ function problem6(forms) {
   for (let i = 0; i < forms.length; i++) {
     for (let l = 0; l < forms[i][1].length - 1; l++) {
       let word = forms[i][1][l] + forms[i][1][l + 1];
+      for (let j = i + 1; j < forms.length; j++) {
+        if (forms[j][1].includes(word)) {
+          if (!forms[i][2]) {
+            answer.push(forms[i][0]);
+            forms[i][2] = 1;
+          }
+          if (!forms[j][2]) {
+            answer.push(forms[j][0]);
+            forms[j][2] = 1;
+          }
+        }
+      }
     }
   }
   return answer;
