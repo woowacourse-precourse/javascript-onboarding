298078458c15bac02bfb9ebffe72e936274cc235
Hwang Juhee <julia0276@naver.com>
2022-10-29 01:36:42 +0900

    :sparkles: [feat] 숫자에 3,6,9 포함 여부 확인 기능 구현 #P3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for (let i = 1; i<=number; i++){
+    let str = i.toString()
+    if (str.includes(3) || str.includes(6) || str.includes(9)){
+      answer += 1
+    }
+  }
   return answer;
 }
 
