f385ac59ed64f10ab66e21f05c0d2f12418e60bf
murpin <qw04011@gmail.com>
2022-10-28 20:05:17 +0900

    [문제 5번] feat: 화폐에 대한 기준을 적은 static 배열 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,10 @@
 /*
  * 기능 분석
- * [] 화폐에 대한 기준을 적은 static 배열 선언
+ * [x] 화폐에 대한 기준을 적은 static 배열 선언
  * [] 입력받은 money에 대한 화폐의 수가 가장 적게 하는 기능
  */
+
+const STATIC_MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
 
 function problem5(money) {
   var answer;
