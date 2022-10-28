43398e76ea81e3e9d790ccea3f65e348aa854314
kang <mdiu2203@naver.com>
2022-10-29 02:42:50 +0900

    feat(PROBLEM6): check duplicate string from str1 and str2

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,14 @@ function problem6(forms) {
   return answer;
 }
 
+function hasDuplicateStr(str1, str2) {
+  for (let i = 0; i < str1.length - 1; i++) {
+    if (str2.includes(str1.substring(i, i + 2))) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 module.exports = problem6;
