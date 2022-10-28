61e57d92a882295ccfadb1c8fa1862276293fa44
bae-sh <hyeon1293@gmail.com>
2022-10-29 00:01:19 +0900

    chore : 상수명 대문자 수정

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,6 @@
 function problem5(money) {
-  const denominations = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const answer = denominations.map(denomination => {
+  const DENOMINATION = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = DENOMINATION.map(denomination => {
     const count = Math.floor(money / denomination);
     money -= count * denomination;
     return count;
