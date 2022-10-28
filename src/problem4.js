6917b166e39b3b6324bb5e06efd1ddd20e935370
최희원 <jeeen1@naver.com>
2022-10-28 21:58:59 +0900

    feat(problem4): add reverse character

--- src/problem4.js
+++ src/problem4.js
@@ -7,6 +7,26 @@ function isAlphabet(character) {
   return character.match(/[a-z]/i);
 }
 
+/**
+ * reverse the given character (if it is an alphabet) according to given table
+ * @param {string} character given character or length 1
+ * @returns {string} reversed character
+ */
+function reverseCharacter(character) {
+  if (!isAlphabet(character)) return character;
+  if (character.toLowerCase() === character) {
+    return String.fromCharCode(
+      "z".charCodeAt(0) - character.charCodeAt(0) + "a".charCodeAt(0)
+    );
+  }
+  if (character.toUpperCase() === character) {
+    return String.fromCharCode(
+      "Z".charCodeAt(0) - character.charCodeAt(0) + "A".charCodeAt(0)
+    );
+  }
+  return "";
+}
+
 function problem4(word) {
   var answer;
   return answer;
