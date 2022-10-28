be591732586026e8cb66bea492e8ca1a1e55535d
yong <tkddbs1644@gmail.com>
2022-10-28 16:43:39 +0900

    문제2: 중복된 문자가 없을 때까지 중복된 문자를 제거하고 결과를 반환한다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,6 @@
 /*
 [x] 문자열을 한 번 순회하면서 중복된 문자를 제거하는 기능을 구현한다.
-[ ] 중복된 문자가 없을 때까지 중복된 문자를 제거하고 결과를 반환한다.
+[x] 중복된 문자가 없을 때까지 중복된 문자를 제거하고 결과를 반환한다.
 */
 
 function removeDuplicatedString(string) {
@@ -20,8 +20,14 @@ function removeDuplicatedString(string) {
 }
 
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while (true) {
+    const removedString = removeDuplicatedString(cryptogram);
+    if (removedString.length === cryptogram.length) {
+      break;
+    }
+    cryptogram = removedString;
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
