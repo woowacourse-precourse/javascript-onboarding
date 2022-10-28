baa48380c9d0ab2a89bfc4cb44f134d7f85b04f3
Sangwon Lee <tkdtm2010@ajou.ac.kr>
2022-10-28 19:22:55 +0900

    problem2 완성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,14 @@
 function problem2(cryptogram) {
   var answer;
+  let cryptoAry = cryptogram.split("");
+
+  for (let i = 0; i < cryptoAry.length; i++) {
+    if (cryptoAry[i] === cryptoAry[i - 1]) {
+      cryptoAry.splice(i - 1, 2);
+      i = 0;
+    }
+  }
+  answer = cryptoAry.join("");
   return answer;
 }
 
