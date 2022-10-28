fbdce211db6eb141a60e72a722b72f4ffe376558
HWI <dlwognl99@naver.com>
2022-10-28 10:58:14 +0900

    feat:예외처리 기능 구현현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 예외사항 1. 길이가 1보다 작거나 1000보다 클 수 없다.
+  if (cryptogram.length < 1 || cryptogram.length > 1000) {
+    return -1;
+  }
+  // 예외사항 2. 알파벳 소문자 이외에는 올 수 없다. (정규표현식 사용)
+  if (/[^a-z]/.test(cryptogram)) {
+    return -1;
+  }
 }
+
+// 중복된 문자가 있는 index 값을 얻는 함수
+function getDuplictatedindex(cryptogram) {}
+
+// 중복된 문자로부터 몇 개까지 중복인지 count 값을 얻는 함수
+function getCount(cryptogram, duplicatedindex) {}
 
 module.exports = problem2;
