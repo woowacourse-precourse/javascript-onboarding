9847c6754b821d4e5cc98ae6aea0c41839631877
ryuna <anottrx@gmail.com>
2022-10-28 17:16:32 +0900

    style : 코드 줄바꿈 수정

--- src/problem4.js
+++ src/problem4.js
@@ -7,16 +7,13 @@ function problem4(word) {
 
 const changeAlphabet = (alphabet) => {
   const asciiCode = alphabet.charCodeAt(0);
-
-  if (asciiCode >= 97 && asciiCode <= 122) {
+  if (asciiCode >= 97 && asciiCode <= 122)
     // 소문자 (97 ~ 122)
     return String.fromCharCode(219 - asciiCode);
-  } else if (asciiCode >= 65 && asciiCode <= 90) {
+  else if (asciiCode >= 65 && asciiCode <= 90)
     // 대문자 (65 ~ 90)
     return String.fromCharCode(155 - asciiCode);
-  } else {
-    return alphabet;
-  }
+  else return alphabet;
 };
 
 module.exports = problem4;
