856a31cdfd3be99c295aafac30c945f079b7d10c
Tory <88531407+1two13@users.noreply.github.com>
2022-10-28 16:30:16 +0900

    refactor(problem5): 주석 수정 및 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,19 @@
 // 기능 목록
-// 1. 몫은 개수, 나머지는 money
+// 1. 몫은 필요한 개수로 answer에 저장하고,
+// 2. 나머지는 다시 money에 할당하기
+// 3. 1, 2 과정을 일원 동전을 검사할 때까지 반복하기
 
 function problem5(money) {
   const arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const copyArr = [...arr];
-  // [오만 원권 개수, 만 원권 개수, 오천 원권 개수, 천 원권 개수, 오백원 동전 개수, 백원 동전 개수, 오십원 동전 개수, 십원 동전 개수, 일원 동전 개수]
+  // 필요한 개수
   let answer = arr.fill(0);
 
   for (let i = 0; i < copyArr.length; i++) {
     if (money >= copyArr[i]) {
-      // 몫
+      // 몫(개수)
       answer[i] = Math.floor(money / copyArr[i]);
-      // 나머지
+      // 나머지(money)
       money = money % copyArr[i];
     } else continue;
   }
