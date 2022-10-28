3ae00ecaa7851b1907595a23f0945a9ae64a21f8
lsy20140 <lsy20140@naver.com>
2022-10-29 02:31:01 +0900

    feat: [문제4] 대문자/소문자에 따라 문자 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,13 +1,19 @@
 function replaceItem(item, index, array){
   const checkEng = /[a-zA-Z]/;
   if(!checkEng.test(item)) return;
-  
+
+  if(item == item.toUpperCase()){
+    array[index] = String.fromCharCode((155 - item.charCodeAt(0)))
+  } else{
+    array[index] = String.fromCharCode((219 - item.charCodeAt(0)))
+  }
 }
 
 function problem4(word) {
   let wordArr = word.split('');
 
   wordArr.forEach(replaceItem);
+  return wordArr.join('');
   
 }
 
