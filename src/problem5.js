30e53b091d0218c2df0ec3b1ce6598bbb43f8efe
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-28 15:24:50 +0900

    feat(PROBLEM5): 2. units를 순회하면서 결과 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  
+
+  const res = [];
+
+  units.forEach(unit => {
+    const 몫 = Math.trunc(money / unit);
+
+    res.push(몫);
+    money -= unit * 몫; 
+  });
+ 
+  return res;
 }
 
 module.exports = problem5;
