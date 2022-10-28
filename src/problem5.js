f430093c9970f7f87cc1df6a3ab86294e7a378d6
easyone <thinkanddoit@naver.com>
2022-10-28 17:51:02 +0900

    #5 feat: 문제5 최대로 변환할 수 있는 count 함수 생성
    - 매개변수는 총금액, 변환 기준 금액(ex 5만원, 1만원)
    - 반환형식은 최대 count, 남은 금액 을 배열형태로

--- src/problem5.js
+++ src/problem5.js
@@ -1,4 +1,9 @@
 function problem5(money) {
+  const changeMaxCount = (total, base) => {
+    const count = parseInt(total / base);
+    const remain = total % base;
+    return [count, remain];
+  };
   var answer;
   return answer;
 }
