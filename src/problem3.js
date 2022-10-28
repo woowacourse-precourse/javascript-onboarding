8ea7398e30b568449e44a8890c06bd97e8bb16b9
Ryong-E <think8867@gmail.com>
2022-10-28 16:09:20 +0900

    refactor: PROBLEM3 IF조건문 삭제 후 1줄 코드로 변경

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,7 @@
 function problem3(number) {
   let answer = 0;
-
   for (let i = 1; i <= number; i++) {
-    if (String(i).match(/3/g)?.length >= 1) {
-      answer += String(i).match(/3/g)?.length;
-    }
-    if (String(i).match(/6/g)?.length >= 1) {
-      answer += String(i).match(/6/g)?.length;
-    }
-    if (String(i).match(/9/g)?.length >= 1) {
-      answer += String(i).match(/9/g)?.length;
-    }
+    answer += String(i).match(/[3|6|9]/g)?.length || 0;
   }
 
   return answer;
