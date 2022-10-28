1de666e0889d4ef23489c1eb1b3209eb7cba5256
mjmj0217 <mjmj0217@naver.com>
2022-10-28 22:25:12 +0900

    feat: problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,29 @@
+function removeSameWord(cryptogram) {
+  let result = cryptogram;
+  for (let i = 0; i < result.length - 1; i++) {
+    if (result[i] === result[i + 1]) {
+      result = result.slice(0, i) + result.slice(i + 2);
+      i -= 1;
+    }
+  }
+
+  return result;
+}
+
 function problem2(cryptogram) {
   var answer;
-  return answer;
+
+  answer = cryptogram;
+  let prevLength = answer.length;
+
+  while (true) {
+    answer = removeSameWord(answer);
+
+    if (answer.length === prevLength) {
+      return answer;
+    }
+    prevLength = answer.length;
+  }
 }
 
 module.exports = problem2;
