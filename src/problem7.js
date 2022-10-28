0b7f85c85b22c4a7fdee749e20ebb3289df3f4ac
olo90632951 <olo90632951@gmail.com>
2022-10-28 16:40:37 +0900

    feat: 사용자와 함께 아는 친구의 수 구하는 기능 추가 [Problem7-1]

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,39 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const { userFriends, userFriendsScore } = getUserFriendScroe(user, friends, visitors);
 }
+
+const getUserFriendScroe = (user, friends) => {
+  const userFriends = getUserFriends(user, friends);
+  const userFriendsScore = new Map();
+  friends.forEach((friend) => {
+    if (friend.includes(user)) {
+      return;
+    }
+    [friend, [...friend].reverse()].forEach((v) => {
+      const [friendA, friendB] = v;
+      if (userFriends.has(friendA) && !userFriends.has(friendB)) {
+        if (userFriendsScore.has(friendB)) {
+          userFriendsScore.set(friendB, userFriendsScore.get(friendB) + 10);
+        } else if (!userFriendsScore.has(friendB)) {
+          userFriendsScore.set(friendB, 10);
+        }
+      }
+    });
+  });
+  return { userFriends, userFriendsScore };
+};
+
+const getUserFriends = (user, friends) => {
+  const userFriends = new Map();
+  for (let i = 0; i < friends.length; i += 1) {
+    const [friendA, friendB] = friends[i];
+    if (friendA === user) {
+      userFriends.set(friendB, 0);
+    } else if (friendB === user) {
+      userFriends.set(friendA, 0);
+    }
+  }
+  return userFriends;
+};
 
 module.exports = problem7;
