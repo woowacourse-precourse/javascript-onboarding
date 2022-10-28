7c72e0c7278f3c21026fe99e639f2131a77fd0c6
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-29 00:49:52 +0900

    [problem6] feat: 변수 추가, 반복 문자 필터링, 필터링한 문자가 존재하는 닉네임 추출하는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
+  const nickname = [];
+  const answer = [];
+
   for (let i = 0; i < forms.length; i++) {
     nickname.push(forms[i][1]);
+  }
+
+  for (let j = 0; j < nickname.length; j++) {
+    if (nickname[j].includes(duplication(nickname).join())) answer.push(nickname[j]);
   }
 }
 
@@ -12,6 +19,13 @@ function duplication(arr) {
       seperate_name_arr.push(arr[i][j] + arr[i][j + 1]);
     }
   }
+
+  const findDuplicates = (seperate_name_arr) =>
+    seperate_name_arr.filter((item, index) => seperate_name_arr.indexOf(item) !== index);
+
+  const duplicates = findDuplicates(seperate_name_arr);
+
+  return [...new Set(duplicates)];
 }
 
 module.exports = problem6;
