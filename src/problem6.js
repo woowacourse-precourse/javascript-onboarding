911b76537cd4cb58f20622c7b178216d031192ad
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 22:18:55 +0900

    feat: implement removing crew whose name length is one

--- src/problem6.js
+++ src/problem6.js
@@ -1,14 +1,9 @@
 function problem6(forms) {
   var answer;
+  // 이름이 두 글자 이상인 크루 리스트
+  const nameLengthTwoList = forms.filter(form => form[1].length > 1);
+
   return answer;
 }
-
-problem6([
-  ['jm@email.com', '제이엠'],
-  ['jason@email.com', '제이슨'],
-  ['woniee@email.com', '워니'],
-  ['mj@email.com', '엠제이'],
-  ['nowm@email.com', '이제엠'],
-]);
 
 module.exports = problem6;
