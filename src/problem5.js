a3e02909723450e9beb0494fac6b383f781e7ad5
MJ <mj632900@gmail.com>
2022-10-28 22:53:38 +0900

    feat : 잔돈, 환전매수를 반환하는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
-function problem5(money) {
-  var answer;
-  return answer;
+function getExchange(haveMoney, exchangeMoney) {
+  const change = haveMoney % exchangeMoney;
+  const exchangeCount = Math.floor(haveMoney / exchangeMoney);
+  return {
+      haveMoney: change,
+      exchangeCount: exchangeCount,
+  };
 }
+
+function problem5(money) {}
 
 module.exports = problem5;
