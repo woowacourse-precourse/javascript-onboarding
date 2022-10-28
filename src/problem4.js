624341508087cc5a3f331ce2e77b937aa25184a4
RooT_Kim <bdd3452@gmail.com>
2022-10-29 08:59:45 +0900

    문제4번) reverseAlphabet 함수를 reverseLower, reverseUpper 함수로 분리

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,7 @@
 function problem4(word) {
 	//알파벳인지 판별하는 함수 -> charFilter
 	//대문자, 소문자를 구별하는 함수 -> alphabetFilter
-	//대문자, 소문자 별로 알파벳을 반전하는 함수 -> reverseAlphabet
+	//대문자, 소문자 별로 알파벳을 반전하는 함수 -> reverseSmall, reverseBig
 
 	return charFilter(word);
 }
@@ -10,14 +10,18 @@ function charFilter(word) {
 	let reverseWord = '';
 
 	for (let i = 0; i < word.length; i++) {
-		reverseWord += /[a-z][A-z]/g.test(word.charAt(i)) ? alphabetFilter(word.charAt(i)) : word.charAt(i);
+		reverseWord += /[A-z]/g.test(word.charAt(i)) ? alphabetFilter(word.charAt(i)) : word.charAt(i);
 	}
 
 	return reverseWord;
 }
 
-function alphabetFilter() {}
+function alphabetFilter(alphabet) {
+	return /[a-z]/g.test(alphabet) ? reverseLower(alphabet) : reverseUpper(alphabet);
+}
 
-function reverseAlphabet() {}
+function reverseLower() {}
+
+function reverseUpper() {}
 
 module.exports = problem4;
