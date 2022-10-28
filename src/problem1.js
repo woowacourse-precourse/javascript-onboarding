37e92f91afbfca44f24f6d245a5f89b78e8260b7
HAEROOL <gofn080776@gmail.com>
2022-10-29 00:45:36 +0900

    feat: [problem1] - 유저의 점수를 할당해주는 함수 작성

--- src/problem1.js
+++ src/problem1.js
@@ -7,6 +7,13 @@ function isValid(pageArray){
   return  pageArray[0] % 2 === 0 || pageArray[1] % 2 === 1
 }
 
+function getMaxScore(pageArray){
+  return Math.max(
+    sumAllElement(pageArray),
+    multiplyAllElement(pageArray)
+  )
+}
+
 function sumAllElement(pageArray){
   return Math.max(
     pageArray[0].split('').reduce((acc, current) => acc + Number(current),0),
