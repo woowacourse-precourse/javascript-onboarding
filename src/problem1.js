081ac68dd41729f1be2a408d42a654ca503f2710
Jiho1996 <jhp9270@naver.com>
2022-10-28 18:21:38 +0900

    feat :  0미만 400초과와 동시에 첫장 또는 마지막장 일시, 예외 처리

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,19 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+const isValidArray = (arr) => {
+  const leftPage = arr[0];
+  const rightPage = arr[1];
+  const firstPage = 1;
+  const finalPage = 400;
+  
+  const isValidRange = () => {
+      if (leftPage < finalPage - 1 && leftPage > firstPage) return true;
+      if (rightPage < finalPage && rightPage > firstPage + 1) return true;
+  }
+  
+  if (isValidRange()) return true
+  return false
+}
+
 module.exports = problem1;
