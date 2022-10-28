b5ac2cd868bb9976be153afaa4eb5d278ef7b66a
SeungWoo Kim <osabero@naver.com>
2022-10-28 15:54:57 +0900

    예외처리 기능구현하였습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,16 @@
 function problem1(pobi, crong) {
-  var answer;
+  var answer = -1;
+  pobi.forEach((item)=>{
+    if(item == 1 || item == 400) {return answer}
+  })
+  crong.forEach((item)=> {
+    if(item == 1 || item == 400) {return answer}
+  })
+  if((pobi[1]-pobi[0])!==1) {return answer}
+  if((crong[1]-crong[0])!==1) {return answer}
+  //예외 처리
   
+
   return answer;
 }
-
 module.exports = problem1;
