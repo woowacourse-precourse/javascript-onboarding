859a84284d15e7180dad839cb469be000da1cbc2
rabBit64 <gojh47@gmail.com>
2022-10-28 14:16:02 +0900

    기능목록1: 예외처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,20 @@
+function exception(pobi, crong){
+  var ret=false;
+  if(pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1){
+    ret=true;
+  }
+  return ret;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  //기능목록1: 예외처리
+  if(exception(pobi, crong)){
+    answer = -1;
+  }else{
+
+  }
+  
   return answer;
 }
 
