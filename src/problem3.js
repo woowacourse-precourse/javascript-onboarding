b1793e9911331e1db2261dd9214b6d92412cf481
cham-min <hymn9613@gmail.com>
2022-10-29 00:42:14 +0900

    feat: 문제3 - countClap 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,16 @@ function problem3(number) {
   return answer;
 }
 
+function countClap(number) {
+  let count = 0;
+  number
+    .toString()
+    .split("")
+    .map((num) => {
+      if (["3", "6", "9"].includes(num)) count += 1;
+    });
+
+  return count;
+}
+
 module.exports = problem3;
