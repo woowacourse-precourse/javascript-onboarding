c5dbf6f8eb0dce568ef8341bc9ea0d51df0aa138
DongWooKim97 <nanaukim@naver.com>
2022-10-28 22:18:14 +0900

    feat(P2): 유효성검사1 입력길이 1일때 체크 함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (!inputValidCheck(cryptogram) && !inputValidCheck(cryptogram)) {
+    return 'error'
+  }
+}
+
+function minLengthCase(inputValue) {
+  // 문자열의 길이가 1일땐 , 그 문자열 그대로 return
+  if (inputValue.length !== 1) return false;
+  return inputValue;
 }
 
 module.exports = problem2;
