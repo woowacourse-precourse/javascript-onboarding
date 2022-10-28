7aed5b641f21c7bcb798ab2026359ac72597daa8
yeonsuBeak <soothbugger@gmail.com>
2022-10-29 08:43:56 +0900

    feat(problem6): 이름을 두 글자로 자르는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,45 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const cutNames = cutNamesIntoTwoLetters(forms);
+}
+
+function cutNamesIntoTwoLetters(crews) {
+  const theNumberOfCrews = crews.length;
+
+  for (
+    let standardNameIndex = 0;
+    standardNameIndex < theNumberOfCrews;
+    standardNameIndex++
+  ) {
+    const standardName = crews[standardNameIndex][1];
+    const standardNameLength = standardName.length;
+
+    for (
+      let standardCutIndex = 0;
+      standardCutIndex < standardNameLength - 1;
+      standardCutIndex++
+    ) {
+      const standardCutName = standardName.substr(standardCutIndex, 2);
+
+      for (
+        let comparedNameIndex = standardNameIndex + 1;
+        comparedNameIndex < theNumberOfCrews;
+        comparedNameIndex++
+      ) {
+        const comparedName = crews[comparedNameIndex][1];
+        const comparedNameLength = comparedName.length;
+
+        for (
+          let comparedCutIndex = 0;
+          comparedCutIndex < comparedNameLength - 1;
+          comparedCutIndex++
+        ) {
+          const comparedCutName = comparedName.substr(comparedCutIndex, 2);
+
+          return [standardCutName, comparedCutName];
+        }
+      }
+    }
+  }
 }
 
 module.exports = problem6;
