b146a456c994f8c3479a02e61338ac823568677d
hanbeulYou <hanbeul.you@gmail.com>
2022-10-28 12:23:34 +0900

    feat(PROBLEM3): implement the features

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,33 @@
+const isClap = (number) => {
+  if(number%3 === 0 && number !== 0) return 1;
+  return 0;
+}
+
+const cntClap = (cnt, element) => {
+  cnt += isClap(element);
+  return cnt;
+}
+
+const countClaps = (numArr) => {
+  const cntOfClaps = numArr.reduce(cntClap, 0)
+  return cntOfClaps;
+}
+
+const strToNum = (str) => {
+  return Number(str);
+}
+
+const makeNumToArr = (number) => {
+  const numArr = number.toString().split('').map(strToNum);
+  return numArr;
+}
+
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for(let i=1; i<=number; i++){
+    const numArr = makeNumToArr(i);
+    answer += countClaps(numArr)
+  }
   return answer;
 }
 
