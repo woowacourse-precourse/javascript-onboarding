1eb38d42ad1491aae1828885d2f3972149b74cb0
An Sol <apdlvmf486@gmail.com>
2022-10-28 12:14:09 +0900

    7번: user와 동일한 친구를 가진 사용자의 점수 매기기 구현

--- src/problem7.js
+++ src/problem7.js
@@ -4,8 +4,23 @@ function problem7(user, friends, visitors) {
   for (let rel of friends){
     if (rel.includes(user)){
       userFriend.push(rel.filter(x => x !== user)[0]);
+    } else{
+      for (let id of rel){
+        scores[id] = 0;
+      }
     }
   }
+
+  for (let rel of friends.filter(x => !(x.includes(user)))) {
+    for (let friend of userFriend) {
+      if (rel.includes(friend)) {
+        scores[rel.filter(x => x !== friend)[0]] += 10;
+      }
+    }
+  }
+
 }
+
+
 
 module.exports = problem7;
