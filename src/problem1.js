d24f36050bed2a74c18b2c780dc6f773c53559a3
Yesong <to06109@naver.com>
2022-10-28 18:17:22 +0900

    [1주차 1번] FEAT: 점수 계산 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -20,11 +20,25 @@ function problem1(pobi, crong) {
   const POBI = 1;
   const CRONG = 2;
 
+  let pobi_score_arr = [];
+  let crong_score_arr = [];
+
   if (pobi[1] != pobi[0] + 1) return ERROR;
   else if (crong[1] != crong[0] + 1) return ERROR;
 
-  var answer;
-  return answer;
+  for (let idx = 0; idx < 2; idx++) {
+    pobi_score_arr.push(Math.max(getSum(pobi[idx]), getMul(pobi[idx])));
+  }
+  const pobi_score = Math.max(...pobi_score_arr);
+
+  for (let idx = 0; idx < 2; idx++) {
+    crong_score_arr.push(Math.max(getSum(crong[idx]), getMul(crong[idx])));
+  }
+  const crong_score = Math.max(...crong_score_arr);
+
+  if (pobi_score > crong_score) return POBI;
+  else if (pobi_score < crong_score) return CRONG;
+  else return TIE;
 }
 
 module.exports = problem1;
