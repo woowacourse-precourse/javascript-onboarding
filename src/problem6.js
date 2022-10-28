18ebe455a3f7604f0dc0665d2c5c7762c111f267
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-28 12:14:16 +0900

    problem6 첫 번쨰 인덱스의 닉네임 정보 배열에 넣기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
   var answer;
+  let includenickname = []
+  for (let i = 0; i<forms[0][1].length; i++){
+    for (let j = 2; j<=forms[0][1].length; j++){
+      if (j+i <= forms[0][1].length){
+        includenickname.push(forms[0][1].substr(i,j))
+      }
+    }
+  }
   return answer;
 }
-
 module.exports = problem6;
