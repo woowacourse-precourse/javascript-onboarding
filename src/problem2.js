9d0c92cc1fdf042d659210c0a4889018d8563a5b
oseunghyun <ohsh1477@gmail.com>
2022-10-29 06:30:51 +0900

    feat(p2): 문자에서 연속하는 중복 문자를 삭제하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -10,8 +10,31 @@ const isTextDuplicated = (text) => {
   return true;
 };
 
+// 연속하는 중복 문자를 삭제하는 기능
+const deleteDuplicatedText = (text) => {
+  const textArr = [...text];
+  var leftIndex = 0;
+  var rightIndex = 0;
+  maxLength = textArr.length;
+  for (leftIndex; leftIndex < maxLength - 1; leftIndex++) {
+    rightIndex = leftIndex + 1;
+    if (textArr[leftIndex] != textArr[rightIndex]) {
+      continue;
+    } else {
+      count = 1;
+      while (textArr[rightIndex] == textArr[leftIndex]) {
+        rightIndex += 1;
+        count++;
+      }
+      textArr.splice(leftIndex, count);
+      maxLength = textArr.length;
+    }
+  }
+  return textArr;
+};
 function problem2(cryptogram) {
   var answer;
+
   return answer;
 }
 
