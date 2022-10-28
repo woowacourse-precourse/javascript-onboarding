286b88da129a25b20f9f317453629ae2e097f3f6
Hwang Juhee <julia0276@naver.com>
2022-10-29 01:51:58 +0900

    :sparkles: [feat] 3,6,9 포함 개수 합 구현 #P3

--- src/problem3.js
+++ src/problem3.js
@@ -2,8 +2,10 @@ function problem3(number) {
   let answer = 0;
   for (let i = 1; i<=number; i++){
     let str = i.toString()
-    if (str.includes(3) || str.includes(6) || str.includes(9)){
+    for (let j = 0; j<str.length; j++){
+      if (str[j].includes(3) || str[j].includes(6) || str[j].includes(9)){
       answer += 1
+      }
     }
   }
   return answer;
