70b011888702f7320301b775c8b7c867ed4aed6a
Siby1lA <psb4644@gmail.com>
2022-10-28 20:11:58 +0900

    feat: 알파벳 외의 문자, word길이 관련 예외처리 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
+function isAlphabet(char) {
+  return char.match(/[a-zA-z]/) ? true : false;
+}
+function isWordValid(word) {
+  return word < 1 || word > 1000 ? true : false;
+}
+
 function problem4(word) {
-  var answer;
+  let answer = "";
+  if (isWordValid(word)) return -1;
+  [...word].forEach((char) => {
+    if (isAlphabet(char)) null;
+    else answer += char;
+  });
   return answer;
 }
 
