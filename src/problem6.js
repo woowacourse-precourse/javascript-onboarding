a304798631eab4c03c43e846677d97fc2a000d87
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-28 14:32:36 +0900

    feat: 중복된 닉네임 크루 체크용 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,7 @@
 function problem6(forms) {
 	const emails = [...new Map(forms).keys()]
 	const nickNames = [...new Map(forms).values()]
+	const duplicateCheckArr = new Array(forms.length).fill(false)
 }
 
 module.exports = problem6
