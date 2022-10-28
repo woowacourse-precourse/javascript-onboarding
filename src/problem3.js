2b0765d562d9ae77694a028008e8976a5da050f7
rabBit64 <gojh47@gmail.com>
2022-10-28 14:54:43 +0900

    기능목록1: 3,6,9 갯수 세기

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  var answer=0;
+  //기능목록1: 3,6,9 갯수 세기
+  for(var i=1; i<=number; i++){
+    var num=i.toString();
+    for(var j=0; j<num.length; j++){
+      if(num[j]==3||num[j]==6||num[j]==9){
+        answer += 1;
+      }
+    }
+  }
+  console.log(answer);
   return answer;
 }
 
