7dd1717990532a9549922b5b755c1af779e9d8b4
eunha <dmsgk2323@naver.com>
2022-10-28 16:13:19 +0900

    6번 문제 이름 두글자씩 나눈 배열을 값으로 갖는 오브젝트 완성

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,17 @@
 function problem6(forms) {
+  const name = [];
+  const obj = {};
+  for (let i = 0; i < forms.length; i++) {
+    name.push(forms[i][1]);
+    // console.log(name);
+    obj[name[i]] = [];
+    for (let j = 0; j < name[i].length - 1; j++) {
+      obj[name[i]] = obj[name[i]] + " " + [name[i].substr(j, 2)];
+    }
+    obj[name[i]] = obj[name[i]].split(" ");
+    obj[name[i]] = obj[name[i]].splice(1, 2);
+    // console.log(name);
+  }
   var answer;
   return answer;
 }
