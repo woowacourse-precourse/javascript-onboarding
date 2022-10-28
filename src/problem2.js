36726eb015a3d5ff385d2b204e9276929ca29b2b
loopy-lim <loopy@jnu.ac.kr>
2022-10-28 17:37:37 +0900

    feat: 연속된 문자열이 있으면 삭제한다

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,22 @@
+const Cyptogram = {
+  originText: "",
+  chars: [],
+  addOriginText: function (text) {
+    this.originText = text;
+    this.chars = this.originText.split("");
+  },
+  removeSameWord: function () {
+    let tmpChars = [...this.chars];
+    for (let i = 0; i < tmpChars.length - 1; i++) {
+      if (tmpChars[i] == tmpChars[i + 1]) {
+        tmpChars.slice(i, 2);
+        i--;
+      }
+    }
+    return tmpChars.join("");
+  },
+};
+
 function problem2(cryptogram) {
   var answer;
   return answer;
