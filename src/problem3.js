bcf0650fba6ab1878fc852ebb3dec98207f60098
freemoon99 <soloist132@gmail.com>
2022-10-28 22:51:47 +0900

    feat[problem3]: 3,6,9가 들어간 수 세는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
   var answer;
+  let clap = 0;
+
+  for (let i = 1; i <= number; i++) {
+    clap += (String(i).match(/3|6|9/g) || []).length;
+  }
+  return clap;
   return answer;
 }
 
