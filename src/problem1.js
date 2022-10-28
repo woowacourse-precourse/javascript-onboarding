f1474abcf30eb0ca219cd955271373c2794791ae
HBSPS <wiyeong79@naver.com>
2022-10-28 17:43:38 +0900

    :sparkles: [문제1] 기능2. 페이지 번호의 각 자리 숫자를 모두 곱하는 메소드 구현

--- src/problem1.js
+++ src/problem1.js
@@ -13,4 +13,14 @@ function sumDigits(num) {
   return sumResult;
 }
 
+function multiplyDigits(num) {
+  let multiplyResult = 1;
+  while (num > 0) {
+    multiplyResult *= num % 10;
+    num = parseInt(num / 10);
+  }
+
+  return multiplyResult;
+}
+
 module.exports = problem1;
