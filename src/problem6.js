19dec93fed5e11788acb4e8cd2c3b0e1660dc9c4
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 22:25:28 +0900

    feat: implement createNameDictionary function

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,13 @@
+function createNameDictionary(forms) {
+  const nameDictionary = {};
+
+  for (i = 0; i < forms.length; i++) {
+    nameDictionary[forms[i][1]] = 0;
+  }
+
+  return nameDictionary;
+}
+
 function problem6(forms) {
   var answer;
   // 이름이 두 글자 이상인 크루 리스트
