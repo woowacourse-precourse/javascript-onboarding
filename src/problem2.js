3ae589bbfb05cf8a094f11f441a51d54092d589b
dev-redo <mis05041@naver.com>
2022-10-29 02:04:23 +0900

    [Fix 2번] 여러번 중복되는 문자들을 삭제할 수 있도록 로직 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,18 @@
 function problem2(cryptogram) {
-  const stack = [cryptogram[0]];
+  const stack = [];
+  let prevDeletedElem = '';
 
-  for (let i = 1; i < cryptogram.length; i++) {
+  for (let i = 0; i < cryptogram.length; i++) {
     const currElem = cryptogram[i];
-
     if (stack[stack.length - 1] === currElem) {
       stack.pop();
-      continue;
+      prevDeletedElem = currElem;
     }
-    stack.push(currElem);
+
+    if (prevDeletedElem !== currElem) {
+      stack.push(currElem);
+      prevDeletedElem = '';
+    }
   }
 
   return stack.join('');
