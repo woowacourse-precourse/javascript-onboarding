b5af42c26b3a554132addd87563848feccda597b
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-28 12:57:48 +0900

    problem6 substr 구조 변경에 따른 duplicatenickname 추출 구조 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,16 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
   let duplicatenickname = []
   for (let k = 0; k<forms.length; k++){
     substr(forms[k])
   }
-  for (let k = 1; k<forms.length; k++){
+  for (let k = 0; k<forms.length; k++){
     for (let p = 0; p<includenickname.length; p++){
-      if (forms[k][1].includes(includenickname[p])){
-        duplicatenickname.push(includenickname[p]);
+      for (let q = 0; q<includenickname[p].length; q++){
+        if (p == k){
+          continue;
+        }
+        else if (forms[k][1].includes(includenickname[p][q])){
+          duplicatenickname.push(includenickname[p][q]);
+        }
       }
     }
   }
+  
   return answer;
 }
 function substr(user){
@@ -25,5 +31,4 @@ function substr(user){
   includenickname.push(temporarynickname)
 }
 var includenickname = []
-console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
 module.exports = problem6;
