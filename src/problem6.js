0825aa09626af1c32b9cdcbf3ab99ee672f5a94f
Ryong-E <think8867@gmail.com>
2022-10-28 15:18:18 +0900

    feat: 중복 닉네임 유저 검증 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  const duplicateWords = forms.map(user => nicknameCombination(user[1])).flat();
+  const duplicateWords = forms
+    .map(user => nicknameCombination(user[1]))
+    .flat();
   return answer;
+}
+
+function nicknameValidator(users, duplicateWords) {
+  const duplicateNicknameUsers = [];
+  for (let duplicateWord of duplicateWords) {
+    const duplicated = [];
+    users.forEach(
+      user => user[1].includes(duplicateWord) && duplicated.push(user[0])
+    );
+    if (duplicated.length > 2) {
+      duplicateNicknameUsers.push(duplicated);
+    }
+  }
+
+  return duplicateNicknameUsers;
 }
 
 function nicknameCombination(nickname) {
