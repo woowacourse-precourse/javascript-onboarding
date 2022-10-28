3bf67f491b4983cbe9146c983825b021e902c4b8
김아현 <dhflxhdxhd@gmail.com>
2022-10-28 21:58:26 +0900

    feat: [problem3] 문제 3 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function problem3(number){
+  var result;
+  var count = 0;
+  
+  for(let i = 1; i<number+1; i++){
+      numArr = (i + '').split('')
+      for(let j = 0; j<numArr.length; j++){
+          if(numArr[j] === "3" || numArr[j] === "6" || numArr[j] === "9"){
+              count += 1
+          }
+      }
+  }
+  result = count; 
+  return result;
 }
 
 module.exports = problem3;
