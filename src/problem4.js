9d1072dfec62221414fcacd7ff68595af3328ef4
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 14:43:51 +0900

    feat: 문자 변환 함수 추가
    getResult 함수로 문자열 차례대로 charToAscii 함수로 전달
    ASCII코드 변환 후 반환

--- src/problem4.js
+++ src/problem4.js
@@ -9,11 +9,39 @@ function validInputCheck(word) {
   return 0;
 }
 
+// ASCII CODE
+// A = 65
+// Z = 90
+// a = 97
+// z = 122
+function charToAscii(char) {
+  let ret;
+
+  if (char.charCodeAt(0) >= 65 && char.charCodeAt(0) <= 90) {
+    ret = 90 - (char.charCodeAt(0) - 65);
+  } else if (char.charCodeAt(0) >= 97 && char.charCodeAt(0) <= 122) {
+    ret = 122 - (char.charCodeAt(0) - 97);
+  } else {
+    ret = char.charCodeAt(0);
+  }
+  return ret;
+}
+
+function getResult(word) {
+  let ret = "";
+
+  for (let index = 0; index < word.length; index++) {
+    ret = ret + String.fromCharCode(charToAscii(word[index]));
+  }
+  return ret;
+}
+
 function problem4(word) {
   if (validInputCheck(word) < 0) {
     return "Argument Error";
   }
-  var answer;
+  var answer = getResult(word);
+
   return answer;
 }
 
