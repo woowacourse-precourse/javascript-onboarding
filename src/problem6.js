f5c7b2622b11cbdab27e274cadbb20ec532e00eb
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-28 17:28:37 +0900

    feat: 닉네임을 두 글자씩 나눠서 객체로 저장

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+const setNicknameObj = (form, nicknameObj) => {
+  const [email, nickname] = form;
+  for (let i = 0; i < nickname.length; i += 1) {
+    const subStr = nickname.substring(i, i + 2);
+    if (subStr in nicknameObj) {
+      nicknameObj[subStr] += 1;
+    } else nicknameObj[subStr] = 1;
+  }
 }
+
+const problem6 = (forms) => {
+  const result = [];
+  const nicknameObj = {};
+  forms.map((form) => {
+    setNicknameObj(form, nicknameObj);
+  })
+}
+
+problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]);
 
 module.exports = problem6;
