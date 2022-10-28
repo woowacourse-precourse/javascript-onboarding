c9db9a39b4a345527ed0976b9e17a82d73db94aa
thewronghand <penfreak77@gmail.com>
2022-10-28 14:50:39 +0900

    feat: answer 변수 설정 및 array 문자열화, answer 반환 로직 구현

--- src/problem2.js
+++ src/problem2.js
@@ -7,6 +7,8 @@ function problem2(cryptogram) {
       array.pop();
     }
   }
+  let answer = array.join('');
+  return answer;
 }
 
 module.exports = problem2;
@@ -16,4 +18,4 @@ module.exports = problem2;
 //[x]for 반복문을 이용하여 array에 cryptogram의 문자를 순서대로 담는다.
 //  [x]문자를 array에 저장할 때 마다 마지막에 추가된 문자가 이전 반복 시에 추가된 문자와 동일한지 확인한다.
 //    [x]동일한 문자로 확인될 경우 가장 최근에 저장된 두 개의 문자를 삭제한다.
-//[] join 메서드를 사용하여 array 배열을 하나의 문자열로 합친다.
+//[x] join 메서드를 사용하여 array 배열을 하나의 문자열로 합친다.
