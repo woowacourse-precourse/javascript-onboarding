8e1731fb096570ca64a00db583f5f76d43dd56c2
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-28 16:48:22 +0900

    feat: (P4) 알파벳 대&소문자 시작,끝 값 상수 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  // word를 배열에 저장 후 순환
+  // 문자 변환값들을 문자열로 반환
+}
+
+// 문자를 반대로 변환하는 기능
+function convertReverse(letter) {
+  const upperCaseStart = 65,
+    upperCaseEnd = 90;
+  const lowerCaseStart = 97,
+    lowerCaseEnd = 122;
+
+  // 알파벳일 경우 소문자 확인 후 변환
+
+  // 알파벳일 경우 대문자 확인 후 변환
+
+  // 알파벳이 아닌 경우
 }
 
 module.exports = problem4;
