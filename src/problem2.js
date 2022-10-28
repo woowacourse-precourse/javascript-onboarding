a30e557b3851a2ebfb760d07a96155c288a42e82
oseunghyun <ohsh1477@gmail.com>
2022-10-29 06:24:43 +0900

    feat(p2): 문자에서 연속하는 중복 문자의 여부를 판단하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,15 @@
+// 문자에서 연속하는 중복 문자의 여부를 판단하는 기능
+const isTextDuplicated = (text) => {
+  const textArr = [...text];
+  for (var i = 0; i < text.length - 1; i++) {
+    if (textArr[i] != textArr[i + 1]) {
+    } else {
+      return false;
+    }
+  }
+  return true;
+};
+
 function problem2(cryptogram) {
   var answer;
   return answer;
