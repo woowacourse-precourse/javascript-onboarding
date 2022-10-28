e43c6f389edcd46ab9bfc27fd85e48977dee2728
2wndrhs <2wndrhs@gmail.com>
2022-10-28 02:30:38 -0700

    feat(PROBLEM2): 중복되는 문자들을 반복적으로 제거하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,19 @@
+// 중복되는 문자들을 반복적으로 제거하는 기능
+function deduplication(str, regex) {
+  let result = str;
+  while (regex.test(result)) {
+    result = result.replace(regex, "");
+  }
+  return result;
+}
+
 // solution
 function solution(cryptogram) {
   const regex = /([a-z])\1+/g;
 }
 
 function problem2(cryptogram) {
-  var answer;
+  const answer = solution(cryptogram);
   return answer;
 }
 
