711863541a4dc3329e048043e722dba24a899c92
ZUITOPIA <korhwngml123@naver.com>
2022-10-28 15:27:56 +0900

    feat: 더하기 함수와 곱하기 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -14,6 +14,44 @@ function problem1(pobi, crong) {
     return true;
   };
 
+  const handleSum = (arr) => {
+    var left = arr[0];
+    var right = arr[1];
+    var sumLeft = 0;
+    var sumRight = 0;
+
+    do {
+      sumLeft += left % 10;
+      left = parseInt((left /= 10));
+    } while (left > 0);
+
+    do {
+      sumRight += right % 10;
+      right = parseInt((right /= 10));
+    } while (right > 0);
+
+    return sumLeft > sumRight ? sumLeft : sumRight;
+  };
+
+  const handleMultiply = (arr) => {
+    var left = arr[0];
+    var right = arr[1];
+    var multiLeft = 1;
+    var multiRight = 1;
+
+    do {
+      multiLeft *= left % 10;
+      left = parseInt((left /= 10));
+    } while (left > 0);
+
+    do {
+      multiRight *= right % 10;
+      right = parseInt((right /= 10));
+    } while (right > 0);
+
+    return multiLeft > multiRight ? multiLeft : multiRight;
+  };
+
   if (check(pobi) === true && check(crong) === true) {
     const pobiMax = compareScore(pobi);
     const crongMax = compareScore(crong);
