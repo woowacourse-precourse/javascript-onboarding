f8a2b129470c818b9520cb1871a93ff6d31817ad
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 14:38:24 +0900

    feat: 문자열 길이 검사 함수 추가
    문자열의 길이가 1보다 작거나 1000보다 클경우 Error메시지 반환

--- src/problem4.js
+++ src/problem4.js
@@ -2,7 +2,17 @@
 // 1. 문자를 조건에 맞게 변환하는 함수 구현
 // 2. 변환 후 반환
 
+function validInputCheck(word) {
+  if (word.length < 1 || word.length > 1000) {
+    return -1;
+  }
+  return 0;
+}
+
 function problem4(word) {
+  if (validInputCheck(word) < 0) {
+    return "Argument Error";
+  }
   var answer;
   return answer;
 }
