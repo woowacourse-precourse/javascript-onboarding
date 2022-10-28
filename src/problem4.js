0d6c76e8677087a829482725441ebec1f849e8c6
jiyujin <yujin1679@naver.com>
2022-10-28 15:41:25 +0900

    docs(pro4): 기능 요구 사항 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,11 @@
+// 기능 요구 사항
+// 청개구리! - 알파벳을 반대로 변환하여 return 한다.
+// A -> Z, B -> Y, ... Z -> A
+// ex) I love you -> R olev blf
+// - [] 대문자(A~Z(65~90))의 경우 65 + (90 - alpha)를 return
+// - [] 소문자(a~z(97~122))의 경우 97 + (122 - alpha)를 return
+// - [] 공백(띄워쓰기(32))의 경우 공백을 추가한다.
+
 function problem4(word) {
   var answer;
   return answer;
