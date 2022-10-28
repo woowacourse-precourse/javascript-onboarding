59844d06b4c1ab85deebe97c5bc3affe4f211afb
unknown <chn0202@gmail.com>
2022-10-28 15:10:23 +0900

    Feat: problem6 기능 개발
    중복닉네임을 찾고 중복닉네임 사용 유저 이메일을 오름차순으로 정렬

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,35 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let repetitionCheck = new Map();
+  let duplicateUserEmail = [];
+  for (let user of forms) {
+    let nickname = user[1];
+    for (let j = 0; j < nickname.length - 1; j++) {
+      let duplicateNickname = nickname.slice(j, j + 2);
+      if (repetitionCheck.has(duplicateNickname)) {
+        if (
+          !duplicateUserEmail.includes(repetitionCheck.get(duplicateNickname))
+        )
+          duplicateUserEmail.push(repetitionCheck.get(duplicateNickname));
+        duplicateUserEmail.push(user[0]);
+      } else repetitionCheck.set(duplicateNickname, user[0]);
+    }
+  }
+  for (let i = 0; i < duplicateUserEmail.length; i++) {
+    let lowCharacterIndex = i;
+
+    for (let j = i + 1; j < duplicateUserEmail.length; j++) {
+      if (
+        duplicateUserEmail[lowCharacterIndex].split("@")[0] >
+        duplicateUserEmail[j].split("@")[0]
+      )
+        lowCharacterIndex = j;
+    }
+    [duplicateUserEmail[i], duplicateUserEmail[lowCharacterIndex]] = [
+      duplicateUserEmail[lowCharacterIndex],
+      duplicateUserEmail[i],
+    ];
+  }
+  return duplicateUserEmail;
 }
 
 module.exports = problem6;
