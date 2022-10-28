0e6c3ad4fa3bd782422509da705d22a572d518c3
bae-sh <hyeon1293@gmail.com>
2022-10-29 00:00:09 +0900

    feat : problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  const denominations = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = denominations.map(denomination => {
+    const count = Math.floor(money / denomination);
+    money -= count * denomination;
+    return count;
+  });
   return answer;
 }
 
