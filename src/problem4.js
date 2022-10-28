d046179c0a16b5df8066ec73bb68473e73e8cd87
d0422 <rlfehd2013@naver.com>
2022-10-28 16:53:46 +0900

    Feat:문자를 아스키코드로 변환, 아스키코드를 문자로 변환하는 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -2,4 +2,11 @@ function problem4(word) {
   var answer;
   return answer;
 }
+function getAsciiFromString(char) {
+  return char.charCodeAt(0);
+}
+function getStringFromString(ascii) {
+  return String.fromCharCode(ascii);
+}
+
 module.exports = problem4;
