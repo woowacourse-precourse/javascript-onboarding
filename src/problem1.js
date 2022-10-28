62be5bc1bccf005a6768f8eddd8ce049e8a4b204
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-28 10:18:16 +0900

    feat(PROBLEM1): 1. 각 자릿수 합 구하는 함수 sum 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,16 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
 }
 
 module.exports = problem1;
+
+const sum = (num) => {
+  const string_num = String(num);
+  let res = 0;
+
+  for (let i = 0; i < string_num.length; i++) {
+    res += Number(string_num[i]);
+  }
+
+  return res;
+}
