4659473ae22a9c9456c326f4b239f27bc35d8b39
pengoosedev <73521518+pengoosedev@users.noreply.github.com>
2022-10-28 21:51:37 +0900

    feat: add checkAlpha, isLower

--- src/problem4.js
+++ src/problem4.js
@@ -14,3 +14,60 @@ module.exports = problem4;
     4) return
  3. Array.join("") 후, return.
 */
+
+const converter = (word) => {
+  const wordArray = word.split("");
+  wordArray.map((i) => {
+    let returnValue;
+    const isAlpha = checkAlpha(i);
+    if (isAlpha) {
+      returnValue = reverseAlpha(i); //인덱스 반대로
+    } else {
+      returnValue = i;
+    }
+
+    return returnValue;
+  });
+  //대소문자 체크 i;
+};
+
+const checkAlpha = (text) => {
+  let resultValue;
+  const rAlpha = /[a-zA-Z]/g;
+  if (text.match(rAlpha)) {
+    resultValue = true;
+  } else {
+    resultValue = false;
+  }
+
+  return resultValue;
+};
+
+const reverseAlpha = (text) => {
+  let resultValue;
+  const isLowerCase = isLower(text);
+  if (isLowerCase) {
+    //lowerCase
+  } else {
+    //upperCase
+  }
+};
+
+const isLower = (text) => {
+  let resultValue;
+  const charCode = text.charCodeAt(0);
+  if (97 <= charCode <= 122) {
+    resultValue = true;
+  } else {
+    resultValue = false;
+  }
+
+  return resultValue;
+};
+
+/*
+대문자 : 65 ~ 90 -> 0 ~ 25
+소문자 : 97 ~ 122 -> 0 ~ 25
+else : return
+
+*/
