8767fd84579ba92be799a93782a86373634103f5
HAEROOL <gofn080776@gmail.com>
2022-10-29 00:04:07 +0900

    feat: [problem1] - 각 자리 숫자를 모두 더하는 함수 작성

--- src/problem1.js
+++ src/problem1.js
@@ -7,4 +7,8 @@ function isValid(pageArray){
   return  pageArray[0] % 2 === 0 || pageArray[1] % 2 === 1
 }
 
+function sumAllElement(pageNumber){
+  return pageNumber.split('').reduce((acc, current) => acc + Number(current),0);
+}
+
 module.exports = problem1;
