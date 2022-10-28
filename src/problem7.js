8ba27427b1f39d854f6c5e00ebfba781d7dd3328
hwnim5324 <hwnim5324@naver.com>
2022-10-28 15:11:15 +0900

    7번 기능 목록 작성.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
+
+  //1. user와 친구인 목록 배열로 생성.
+  //2. user와 친구인 목록의 친구인 목록 조회하여 점수 가산(+10).
+  //3. 타임 라인에 방문한 사용자 조회하여 점수 가산(+1).
+  //4. 조건에 맞게 정렬하여 반환.
 }
 
 module.exports = problem7;
