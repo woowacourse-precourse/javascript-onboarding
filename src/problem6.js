474f1edc99d126420ccf2f861d1873fabe9203a6
eunha <dmsgk2323@naver.com>
2022-10-28 18:00:51 +0900

    6번문제 풀이 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,5 @@
 function problem6(forms) {
+  let result = [];
   const name = [];
   const obj = {};
   for (let i = 0; i < forms.length; i++) {
@@ -12,35 +13,28 @@ function problem6(forms) {
     obj[name[i]] = obj[name[i]].splice(1, 2);
   }
 
-  // for (let i = 0; i < forms.length - 1; i++) {
-  //   for (let j = i + 1; j < forms.length - 1; j++) {
-  //     if (obj[name[i]].filter((x) => obj[name[j]].includes(x))) {
-  //       console.log(name[i]);
-  //       console.log(obj[name[i]].filter((x) => obj[name[j]].includes(x)));
-  //     }
-  //   }
-  // }
   const newArr = [];
-  // console.log(obj["엠제이"].filter((x) => obj["제이엠"].includes(x)));
+
   for (let i = 0; i < forms.length; i++) {
     newArr.push(...obj[name[i]]);
   }
   let index;
+  let clue;
   console.log(newArr);
   for (let i = 0; i < newArr.length; i++) {
     if (newArr.indexOf(newArr[i]) !== newArr.lastIndexOf(newArr[i])) {
       index = newArr[i];
     }
-
-    // console.log(index);
   }
   for (let j = 0; j < name.length; j++) {
     if (obj[name[j]].indexOf(index) !== -1) {
-      console.log(name[j]);
+      clue = name[j];
+    }
+    if (forms[j][1] === clue) {
+      result.push(forms[j][0]);
     }
   }
-  var answer;
-  return answer;
+  return result.sort();
 }
 
 module.exports = problem6;
