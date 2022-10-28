ed9ff79cd759f81ea66f604575039a51ef572ff8
Deemou <4106243@naver.com>
2022-10-28 18:58:30 +0900

    feat<P3>: 주어진 숫자의 3,6,9 개수 카운트 함수 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
+  let answer;
+
   return answer;
 }
+
+const countThreeSixNine = (number) => {
+  let numArr = number.toString().split("");
+  let count = 0;
+  numArr.forEach((n) => {
+    if (n % 3 == 0) count++;
+  });
+  return count;
+};
 
 module.exports = problem3;
