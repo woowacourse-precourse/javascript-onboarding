1b11d6b37c3cbdb96b8e931969dcfde594ead468
Hoon <si932174@gmail.com>
2022-10-28 16:45:38 +0900

    problem5 : 이외 기타 돈 입력 시 반환하는 함수 제작

--- src/problem5.js
+++ src/problem5.js
@@ -3,10 +3,22 @@ function problem5(money) {
 
   const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  const moneyCountGetter = (money, moneys) => {
+    const moneyCount = [];
+
+    moneys.map( moneyUnit => {
+    
+      moneyCount.push(Math.floor(money / moneyUnit));
+      money = money % moneyUnit;
+    }) 
+
+    return moneyCount;
+  }
+
   if(money < 10) {
     answer = [0, 0, 0, 0, 0, 0, 0, 0, money];
   } else {
-
+    answer = moneyCountGetter(money, moneys);
   }
 
   return answer;
