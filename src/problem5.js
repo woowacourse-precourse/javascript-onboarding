fbb8d9fd945e73230074b7b79a9027493ddb0922
Hoon <si932174@gmail.com>
2022-10-28 16:39:31 +0900

    problem5 : 1자리수 입력 시 바로 반환

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
   var answer;
+
+  const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  if(money < 10) {
+    answer = [0, 0, 0, 0, 0, 0, 0, 0, money];
+  } else {
+
+  }
+
   return answer;
 }
 
