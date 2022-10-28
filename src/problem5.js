8124f56a83131648c051d15b31024b2f820f0f2a
minjaeKim <smallkdb@gmail.com>
2022-10-29 00:40:26 +0900

    feat: [Problem5] Add withDraw function
    원하는 금액을 큰 금액의 화폐 위주로 인출해주는 withDraw 함수를 추가하였습니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,18 @@
+const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+function withDraw(money) {
+  let wallet = [];
+  units.forEach((unit) => {
+    wallet.push(parseInt(money/unit));
+    money = money%unit;
+  });
+
+  return wallet;
+}
+
 function problem5(money) {
   var answer;
+  answer = withDraw(money);
   return answer;
 }
 
