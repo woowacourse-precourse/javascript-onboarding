d38315cc718d71bde1587cfb7676d00d80d01952
Hoon <si932174@gmail.com>
2022-10-28 11:37:42 +0900

    problem4 : 소문자 변환 함수 제작

--- src/problem4.js
+++ src/problem4.js
@@ -1,16 +1,24 @@
 function problem4(word) {
   var answer;
 
-  const capitalConverter = capitalLetter => {
+  const capitalLetterConverter = capitalLetter => {
 
     const capitalLetterNumber = capitalLetter.charCodeAt();
     const convertedNumber = 155 - capitalLetterNumber;
-    const convertedLetter = String.fromCharCode(convertedNumber);
+    const convertedCapitalLetter = String.fromCharCode(convertedNumber);
 
-    return convertedLetter;
-  }
+    return convertedCapitalLetter;
+  };
 
-  
+  const smallLetterConverter = smallLetter => {
+
+    const smallLetterNumber = smallLetter.charCodeAt();
+    const convertedNumber = 219 - smallLetterNumber;
+    const convertedSmallLetter = String.fromCharCode(convertedNumber);
+
+    return convertedSmallLetter;
+  };
+
 
   return answer;
 }
