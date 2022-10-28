64e6445a6d0154aac7de9654ff26838bb412acde
energy1-914 <dlawldnjs5@daum.net>
2022-10-28 13:54:58 +0900

    feat: 문제 1번 - 최대값 반환 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,24 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function maxNum(array){
+  let maxNum;
+  const leftPage = array[0].toString().split(''); 
+  const rightPage = array[1].toString().split(''); 
+  let addLeftNum = 0;
+  let multipleLeftNum = 1;
+  let addRightNum = 0;
+  let multipleRightNum = 1;
+  for (i=0; i<leftPage.length ; i++){  
+    addLeftNum += parseInt(leftPage[i]);
+    multipleLeftNum *= parseInt(leftPage[i]);
+  }
+  for (i=0; i<rightPage.length ; i++){  
+    addRightNum += parseInt(rightPage[i]);
+    multipleRightNum *= parseInt(rightPage[i]);
+  }
+  maxNum = Math.max(addLeftNum, addRightNum, multipleLeftNum, multipleRightNum)
+  return maxNum;
+}
+
 module.exports = problem1;
