5a3cd51a0b11c43d68239239e2eeb51aeadd28b4
d0422 <rlfehd2013@naver.com>
2022-10-28 15:19:49 +0900

    Feat: 같은 문자를 stack을 통해 제거하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -2,5 +2,19 @@ function problem2(cryptogram) {
   var answer;
   return answer;
 }
-
+function decryption(cryptogram) {
+  let index = 0;
+  const stack = [];
+  while (index < cryptogram.length) {
+    if (!stack) stack.push(cryptogram[index]);
+    else {
+      if (stack[stack.length - 1] == cryptogram[index]) {
+        stack.pop();
+      } else {
+        stack.push(cryptogram[index]);
+      }
+    }
+    index++;
+  }
+}
 module.exports = problem2;
