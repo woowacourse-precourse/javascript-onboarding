67df83ebcebfc3d39de3ea4ddc8fa0f402ee90b5
kaori-killer <sksbthwjd@daum.net>
2022-10-28 12:19:18 +0900

    feat:배열의 각 값을 계산 함수에 전달

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,11 @@
-function calPlusMinus(leftNum, rightNum){
-
+function calPlusMinus(page){
 }
 
 function problem1(pobi, crong) {
+    pobi.forEach(calPlusMinus);
+    crong.forEach(calPlusMinus);
+}
+
   var answer;
   return answer;
 }
