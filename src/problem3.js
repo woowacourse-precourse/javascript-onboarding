f30c33fed52bd43e23c495181daf8bd315b20c5a
nerdchanii <nerdchanii@gmail.com>
2022-10-29 05:24:57 +0900

    feat: [PROBLEM3] range, filter,map, reduce 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,39 @@ function problem3(number) {
   return answer;
 }
 
+let Lazy = {};
+
+Lazy.range = function* (start, end) {
+  let number = start;
+  while (number <= end) {
+    yield number;
+    number++;
+  }
+}
+
+Lazy.filter = function* (f, iter) {
+  for (const item of iter) {
+    if (f(item)) yield item;
+  }
+}
+
+Lazy.map = function* (f, iter) {
+  for (const item of iter) {
+    yield f(item);
+  }
+}
+
+function reduce(f, acc, iter) {
+  if (!iter) {
+    iter = acc[Symbol.iterator]();
+    acc = iter.next().value;
+  }
+  for (const item of iter) {
+    acc = f(acc, item);
+  }
+  return acc;
+}
+
+
+
 module.exports = problem3;
