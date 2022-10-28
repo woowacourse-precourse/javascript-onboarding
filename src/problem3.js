6078b7253144b7d0ef068b18c0b90841f9f4d629
xownsdl0 <xownsdl0@naver.com>
2022-10-28 14:01:48 +0900

    <P3> 각 숫자의 3, 6, 9 개수 확인

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
 function problem3(number) {
   var answer;
+  var isThree = 0;
+  var isSix = 0;
+  var isNine = 0;
+
+  for (let i = 0; i < number + 1; i++) {
+    let num = String(i);
+
+    // 각 숫자에 3, 6, 9의 개수를 확인하다.
+    isThree += num.split("3").length - 1;
+    isSix += num.split("6").length - 1;
+    isNine += num.split("9").length - 1;
+  }
+
   return answer;
 }
 
