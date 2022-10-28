109ba586078b5cf03c7a923f3919035b57836f9b
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-28 10:43:54 +0900

    ✨ 모든철자를 대소문자 구분하여 만드는 과정

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,8 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  // 1) 모든철자를 대소문자 구분하여 만드는 과정
+  const alphabet_L = "abcdefghijklmnopqrstuvwxyz";
+  const alphabet_U = alphabet_L.toUpperCase();
   return answer;
 }
 
