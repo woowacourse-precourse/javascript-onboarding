279386a3cd7c42b90d1d189e5773915436a7804c
cybersyn2 <quandonestpetit@outlook.com>
2022-10-28 20:06:25 +0900

    draft problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let s = decode(cryptogram);
+  while (cryptogram != s) {
+    cryptogram = s;
+    s = decode(cryptogram);
+  }
+  return cryptogram;
+}
+
+function decode(s) {
+  for (let i = 0; i < s.length - 1; i++) {
+    if (s[i] == s[i + 1]) return s.slice(0, i) + s.slice(i + 2);
+  }
+  return s;
 }
 
 module.exports = problem2;
