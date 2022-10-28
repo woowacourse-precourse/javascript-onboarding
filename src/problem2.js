3bf177d3bd4e6ca7596492632852dd84c288ad84
GC-Park <kuyt3122@naver.com>
2022-10-28 16:38:23 +0900

    (2번)Feat:중복된 문자를 없애주는 암호를 해독하고 새로운 string만드는 함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function decodeString(s) {
+    let newString = ''
+    for (let i = 0; i < s.length; i++) {
+        if (i === s.length - 1) {
+            if (s[i] !== s[i - 1]) newString += s[i]
+            continue
+        }
+        if (s[i] !== s[i + 1]) {
+            newString += s[i]
+        }
+    }
+    return newString
 }
 
-module.exports = problem2;
+function problem2(cryptogram) {
+    var answer
+    return answer
+}
+
+module.exports = problem2
