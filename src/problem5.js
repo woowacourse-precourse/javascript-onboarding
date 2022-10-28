a69d81442fb67eaaa04ff0d4c454d98bac62d238
pangkyu <baesense@naver.com>
2022-10-28 20:26:50 +0900

    feat : 오만원 갯수를 리턴하는 기능
    오만원의 개수를 리턴하고 잔액을 저장한다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,26 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let result = [];
+  let balance = money;
+  result.push(isOhmanwon(money));
+  balance = money - isOhmanwon(money) * 50000;
+  result.push(isManwon(balance));
+  console.log(isOhmanwon(money));
+  console.log(balance);
+  console.log(result);
 }
-
+function isOhmanwon(money) {
+  if (money >= 50000) {
+    let ohmanwon = money / 50000;
+    ohmanwon = Number.parseInt(ohmanwon);
+    return ohmanwon;
+  }
+  return 0;
+}
+function testCode() {
+  problem5(60237);
+  // problem5(10000);
+  // problem5(15000);
+  // problem5(1000000);
+}
+testCode();
 module.exports = problem5;
