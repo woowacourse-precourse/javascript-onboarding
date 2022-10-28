91fdaaccf05b2a2f3c372f616f03ca4c4e4ab044
GC-Park <kuyt3122@naver.com>
2022-10-28 19:33:21 +0900

    (2번)update:decodeString의 중복문자 체크하는 방법 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,28 +1,26 @@
 function decodeString(s) {
     let newString = ''
     for (let i = 0; i < s.length; i++) {
-        if (i === s.length - 1) {
-            if (s[i] !== s[i - 1]) newString += s[i]
-            continue
-        }
-        if (s[i] !== s[i + 1]) {
+        if (s[i] !== s[i + 1] && s[i] !== s[i - 1]) {
             newString += s[i]
         }
     }
     return newString
 }
 
-function getString(s) {
+function getStr(s) {
     let prev = ''
     while (1) {
         prev = s
         s = decodeString(s)
+        console.log(s)
         if (s === prev) return s
     }
 }
 
 function problem2(cryptogram) {
     var answer
+    answer = getStr(cryptogram)
     return answer
 }
 
