32a3e1ebbb40154aff49ffc4e5c5f0380c2d369c
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-28 20:56:31 +0900

    feat(PROBLEM6): 1. {<두글자>: <갯수>} 객체를 반환하는 함수twoStringHash 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,30 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  
 }
 
+
 module.exports = problem6;
+
+const twoStringHash = (forms) => {
+  const nicknames = forms.map(form => form[1]);
+  const hash = {};
+
+  nicknames.forEach((nickname, i) => {
+    let set = new Set();
+
+    for (let j = 0; j < nickname.length - 1; j++) {
+      const two = nickname[j] + nickname[j+1];
+      set.add(two);
+    }
+    
+    set.forEach(v => {
+      if (hash[v]) {
+        hash[v]++;
+      } 
+      else {
+        hash[v] = 1;
+      }
+    });
+  });
+  return hash;
+}
