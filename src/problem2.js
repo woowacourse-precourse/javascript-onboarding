6ed84f1e7e1293ccc357a9378677281c31df0ba4
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-28 12:05:54 +0900

    feat(PROBLEM2): 1. 중복을 1회 제거하는 함수 deduplication 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function problem2(cryptogram) { 
+
 }
 
 module.exports = problem2;
+
+const deduplication = (str) => {
+  let res = ''; 
+
+  for (let i = 0; i < str.length; i++) {
+    const pre = str[i-1];
+    const now = str[i];
+    const next = str[i+1];
+
+    if (now !== pre && now !== next) {
+      res += now;
+    }
+  }
+
+  return res;
+}
