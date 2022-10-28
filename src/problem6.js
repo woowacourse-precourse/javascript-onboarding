f6aa1c89f1d469142bf2de40bc714d66ed729f97
kang <mdiu2203@naver.com>
2022-10-29 02:44:03 +0900

    feat(PROBLEM6): find out duplicate crew's email addresses from forms

--- src/problem6.js
+++ src/problem6.js
@@ -3,6 +3,23 @@ function problem6(forms) {
   return answer;
 }
 
+function findDuplicateCrew(forms) {
+  const result = new Set();
+
+  for (let i = 0; i < forms.length; i++) {
+    const [emailA, nicknameA] = forms[i];
+    for (let j = i + 1; j < forms.length; j++) {
+      const [emailB, nicknameB] = forms[j];
+      if (hasDuplicateStr(nicknameA, nicknameB)) {
+        result.add(emailA);
+        result.add(emailB);
+      }
+    }
+  }
+
+  return result;
+}
+
 function hasDuplicateStr(str1, str2) {
   for (let i = 0; i < str1.length - 1; i++) {
     if (str2.includes(str1.substring(i, i + 2))) {
