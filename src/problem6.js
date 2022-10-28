e3fdb3fa51f317caad17d0737e294a6d28ccf7f3
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 22:11:44 +0900

    docs: update problem6 function list in FUNCTIONLIST.md

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,12 @@ function problem6(forms) {
   return answer;
 }
 
+problem6([
+  ['jm@email.com', '제이엠'],
+  ['jason@email.com', '제이슨'],
+  ['woniee@email.com', '워니'],
+  ['mj@email.com', '엠제이'],
+  ['nowm@email.com', '이제엠'],
+]);
+
 module.exports = problem6;
