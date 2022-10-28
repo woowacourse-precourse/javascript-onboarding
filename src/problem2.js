b3753d8c5f7378368706d583b85d790762d96d2b
Yeom-JinHo <pwlsghq@naver.com>
2022-10-28 20:18:52 +0900

    feat : [P2] 해독로직 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,29 @@
+const findDuplicatedIndex = (word, startIndex) => {
+  let duplicatedInd = -1;
+  for (let i = startIndex; i < word.length - 1; i++) {
+    if (word[i] === word[i + 1]) {
+      duplicatedInd = i;
+      break;
+    }
+  }
+  return duplicatedInd;
+};
+
 function problem2(cryptogram) {
-  var answer;
+  let answer = "";
+  let nowInd = 0;
+  let word = cryptogram.split("");
+  while (nowInd < word.length) {
+    const dupliIndex = findDuplicatedIndex(word, nowInd);
+    console.log(nowInd, word.join(""));
+    if (dupliIndex !== -1) {
+      word.splice(dupliIndex, 2);
+      nowInd = dupliIndex - 1;
+      continue;
+    }
+    break;
+  }
+  answer = word.join("");
   return answer;
 }
 
