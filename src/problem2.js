2140a8fa65077674140d6ee1f24b5a134a6174c3
hae-on <solbi2004@naver.com>
2022-10-28 13:15:28 +0900

    [문제2] stack을 이용한 중복 글자 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+  stack.push(cryptogram[0]);
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (stack[stack.length - 1] === cryptogram[i]) {
+      stack.pop();
+      continue;
+    }
+    stack.push(cryptogram[i]);
+  }
+  return stack.join('');
 }
 
 module.exports = problem2;
