9f6d03c84d8bb60deb9c020506aeec90dd8bcf40
jiyujin <yujin1679@naver.com>
2022-10-28 17:55:38 +0900

    feat(pro5): 지폐와 동전이 각각 몇개로 반환되는지 return하는 코드 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,31 @@
 // 기능 요구 사항
 // 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전
 // 각 몇 개로 변환되는지 금액이 큰 순서대로 배열에 담아 return 한다.
-// - [] money / 50000 -> money %= 50000
-// - [] money / 10000 -> money %= 10000
-// - [] ... 과정 반복하면서 answer 배열에 값 push
+// - [o] money / 50000 -> money %= 50000
+// - [o] money / 10000 -> money %= 10000
+// - [o] ... 과정 반복하면서 answer 배열에 값 push
 
 function problem5(money) {
-  var answer;
+  let answer = [];
+  answer.push(parseInt(money / 50000));
+  money = money % 50000;
+  answer.push(parseInt(money / 10000));
+  money = money % 10000;
+  answer.push(parseInt(money / 5000));
+  money = money % 5000;
+  answer.push(parseInt(money / 1000));
+  money = money % 1000;
+  answer.push(parseInt(money / 500));
+  money = money % 500;
+  answer.push(parseInt(money / 100));
+  money = money % 100;
+  answer.push(parseInt(money / 50));
+  money = money % 50;
+  answer.push(parseInt(money / 10));
+  money = money % 10;
+  answer.push(money);
   return answer;
 }
 
+console.log(problem5(50237));
 module.exports = problem5;
