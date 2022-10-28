3044e5f9fdd640a530fbd611674864314f19de1a
MAGHC <windowsapple95@gmail.com>
2022-10-28 17:00:43 +0900

    feat: problem6 nickName중복체크 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
+const checkDuPlicateNickName = (nickNameArray) => {
+  let prefixArray = [];
+  for (let i = 0; i < nickNameArray.length; i++) {
+    for (let j = 0; j < nickNameArray.length; j++) {
+      const prefix = nickNameArray[i].slice(0, 2);
+      if (nickNameArray[j] === nickNameArray[i]) continue;
+
+      if (nickNameArray[j].includes(prefix)) prefixArray.push(nickNameArray[i], nickNameArray[j]);
+    }
+  }
+  return Array.from(new Set([...prefixArray]));
+};
+
 function problem6(forms) {
-  var answer;
+  let answer;
+  const formsObj = Object.fromEntries(forms);
+  const nickNameArray = Object.values(formsObj);
+  checkDuPlicateNickName(nickNameArray);
+
   return answer;
 }
 
