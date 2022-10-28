a43626cef05611c92b4ef078dec7f8054015b317
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-29 04:29:33 +0900

    [feat] problem5 - 돈 액수별 변환 갯수 구하기 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
   var answer;
+  answer = cal_money(money);
   return answer;
+}
+
+function cal_money(money) {
+  const amount_list = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let money_list = [];
+  for (let i = 0; i < 9; i++) {
+    let amount = amount_list[i];
+    let n = parseInt(money / amount);
+    money = money % amount;
+    money_list = [...money_list, n];
+  }
+  return money_list;
 }
 
 module.exports = problem5;
