bf7184ed37e647acf629386ee2b2e5b0e8e2808c
yeongi <cbg5255@gmail.com>
2022-10-28 17:47:55 +0900

    문제 7. 기능 목록 구현

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,13 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+// 1. USER 의 친구를 거르는 기능
+function filteredFreinds(friends) {}
+// 2. 친구의 카운트를 세서 점수 계산하는  기능
+function friendsRecomandScore(id) {}
+// 3. VISITOR의 카운트를 세서 점수 계산하는 기능
+function visiterRecomanScore(visitors) {}
+// 4. 점수가 가장 높은 순으로 정렬,친구 추천 기능
+function scoreSortRecomandFreinds(score) {}
+
 module.exports = problem7;
