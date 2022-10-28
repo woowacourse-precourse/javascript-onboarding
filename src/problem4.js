49bd80497abb2ed36044f7624978dec447ca4dce
저니녁 <wjs_5025@naver.com>
2022-10-28 15:25:06 +0900

    solve : problem4 문제 해결 및 설명추가 필요

--- src/problem4.js
+++ src/problem4.js
@@ -1,12 +1,59 @@
 /* 문제 분석
+1. 청개구리 사전에 맞춰서 데이터를 변환하여 출력해야한다.
+2. 대소문자를 구분해서 반환한다.
  */
 /* 테스트 케이스 분석
+tc1. I Love you
+R olev blf 로 대응
  */
+
+const alpha = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+//
+const flogCase = [...alpha].reverse();
+
+function toFlogCase(char) {
+  let c = char.toUpperCase();
+  let index = alpha.indexOf(c);
+  let changed = flogCase[index] ?? " ";
+  if (char.charCodeAt(0) >= 97) return changed.toLowerCase();
+  return changed;
+}
 
 // main
 function problem4(word) {
-  var answer;
-  return answer;
+  var answer = [];
+  for (let i of [...word]) {
+    answer.push(toFlogCase(i));
+  }
+  return answer.join("");
 }
 
 module.exports = problem4;
--- test.js
+++ test.js
@@ -1,17 +1,57 @@
-function problem3(number) {
-  var answer = 0;
+/* 문제 분석
+1. 청개구리 사전에 맞춰서 데이터를 변환하여 출력해야한다.
+2. 대소문자를 구분해서 반환한다.
+ */
+/* 테스트 케이스 분석
 
-  for (let i = 1; i <= number; i++) {
-    let cnt = 0;
-    i.toString()
-      .split("")
-      .forEach((el) => {
-        if (el === "3" || el === "6" || el === "9") cnt += 1;
-      });
-    answer += cnt;
-  }
-  return answer;
+ */
+
+let alpha = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+let flogCase = [...alpha].reverse();
+
+function toFlogCase(char) {
+  let c = char.toUpperCase();
+  let index = alpha.indexOf(c);
+  let changed = flogCase[index] ?? " ";
+  if (char.charCodeAt(0) >= 97) return changed.toLowerCase();
+  return changed;
 }
 
-console.log(problem3(13));
-console.log(problem3(33));
+// main
+function problem4(word) {
+  var answer = [];
+  for (let i of [...word]) {
+    answer.push(toFlogCase(i));
+  }
+  return answer.join("");
+}
+
+console.log(problem4("I love You"));
