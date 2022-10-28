394f915133468487427fbb13171371a27589d90b
feb-dain <feb.dain@gmail.com>
2022-10-28 16:28:34 +0900

    feat: [problem4] create array of alphabet in upper case

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,6 @@
 function problem4(word) {
   const lowerCase = Array.from({ length: 26 }, (VOID, index) => String.fromCharCode(index + 97));
+  const upperCase = Array.from({ length: 26 }, (VOID, index) => String.fromCharCode(index + 65));
   
   return answer;
 }
