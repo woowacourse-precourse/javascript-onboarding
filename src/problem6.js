be1c90bf296dc6fa124757d5517f300a54d8ac72
An Sol <apdlvmf486@gmail.com>
2022-10-28 09:34:48 +0900

    6번: 닉네임 2개 단위로 자르기 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const result = [];
+  for (let i = 0; i < forms.length; i++){
+    const testCrew = forms.shift();
+    if (result.includes(testCrew[0])) continue;
+    for (let idx = 0; idx < testCrew[1].length - 1; idx++){
+      const testStr = testCrew[1].slice(idx, idx + 2);
+    }
+  }
+
 }
 
 module.exports = problem6;
