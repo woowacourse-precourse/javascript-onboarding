7101214fc0af9f9610cba27417bf08268b4a63b8
ho-ji <wkdd0205@gmail.com>
2022-10-28 23:54:12 +0900

    feat: 문제1 기능 구현
    pobi와 crong의 승부 결과를 반환하도록 기능을 구현함

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,13 @@
+function findMax(num) {
+  const ones = num % 10;
+  const tens = ((num % 100) - (num % 10)) / 10;
+  const hundreds = ((num % 1000) - (num % 100)) / 100;
+
+  const sum = ones + tens + hundreds;
+  const sumProduct =
+    hundreds == 0 ? (tens == 0 ? ones : tens * ones) : hundreds * tens * ones;
+  return sum > sumProduct ? sum : sumProduct;
+}
 function problem1(pobi, crong) {
   var answer;
   if (pobi[0] < 1 || pobi[0] > 399 || pobi[0] % 2 == 0) return -1;
@@ -5,6 +15,11 @@ function problem1(pobi, crong) {
   if (crong[0] < 1 || crong[0] > 399 || crong[0] % 2 == 0) return -1;
   if (crong[1] < 2 || crong[1] > 400 || crong[1] % 2 == 1) return -1;
   if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) return -1;
+  const maxPobi = Math.max(findMax(pobi[0], pobi[1]));
+  const maxCrong = Math.max(findMax(crong[0], crong[1]));
+  if (maxPobi > maxCrong) answer = 1;
+  else if (maxPobi < maxCrong) answer = 2;
+  else answer = 0;
   return answer;
 }
 
