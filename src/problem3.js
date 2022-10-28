cdf6cf93c3a3ba3e4dd08c73e864eb16a1ba5f8b
Hungeun2 <bepoohun@gmail.com>
2022-10-28 09:50:11 +0900

    Feat: problem3 Add comment

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,17 @@
 // 1. 손뼉 횟수 계산 function 생성
 // 2. 1~number 까지 손뼉 횟수를 각각 구해 더함
+// 손뼉 횟수 계산
 function countClap(num) {
   return String(num).match(/[3,6,9]/g)?.length || 0;
 }
 
 function problem3(number) {
   var answer = 0;
+  // 1 ~ number 까지 더하기
   for (let i = 1; i <= number; i++) {
     answer += countClap(i);
   }
   return answer;
 }
 
-console.log(problem3(14));
 module.exports = problem3;
