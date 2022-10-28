52c6ac5f0ec9a47ea4092b01c53613759da503c0
sery <yunsel2@gmail.com>
2022-10-28 12:32:08 +0900

    feat(Problem5): Set moneyUnit & Get count of each unit

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
+const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let price = money;
+
+  moneyUnit.forEach((unit, index) => {
+    if (price >= unit) {
+      const count = Math.floor(price / unit);
+    }
+  });
+
   return answer;
 }
 
