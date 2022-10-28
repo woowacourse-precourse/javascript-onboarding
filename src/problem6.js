ccf15dcc8944d3af323ab62f53cc81feb7cc1a79
Hwang Seong Taek <hst007@naver.com>
2022-10-28 14:18:09 +0900

    feat: 크루의 닉네임 중 중복되는 단어를 확인하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -2,6 +2,19 @@ function problem6(forms) {
   const splitCrewName = [];
   const duplicateName = [];
   const result = new Set();
+  
+  forms.forEach(crew => {
+    const crewNickName = crew[1];
+    for (let j=0; j<crewNickName.length-1; j++) {
+      const splitNickName = crewNickName.slice(j, j+2); // 크루의 닉네임을 2개씩 slice
+      if (splitCrewName.includes(splitNickName)) {
+        duplicateName.push(splitNickName); // 중복된 닉네임을 duplicateName 배열에 저장
+      } else if (!splitCrewName.includes(splitCrewName)) {
+        splitCrewName.push(splitNickName);
+      }
+    }  
+  });
+
 }
 
 module.exports = problem6;
