e47e89ced702be4fcc88044d1976ac0085739763
sujungYu <su_moment@naver.com>
2022-10-28 17:15:07 +0900

    feat(PROBLEM3):각 자리수를 분리하여 배열 생성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,6 @@
 function problem3(number) {
     const numArr = Array(number).fill().map((v,i)=> i+1);
+    const splitNumArr = numArr.join('').split('').map(Number);
 }
 
 module.exports = problem3;
