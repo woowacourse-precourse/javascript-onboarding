6085dd29cd7c68a68a200a70906c9f76d278fd4e
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-29 00:13:04 +0900

    feat: PROBLEM 3 자연수 범위 예외 사항

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,8 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  const check = (number) => {
+    if (number < 1 || 10000 < number) return false;
+    return true;
+  }
 }
 
 module.exports = problem3;
