564795e27cddc08929ea6497e8ba838a5b3063f7
최희원 <jeeen1@naver.com>
2022-10-28 21:23:12 +0900

    feat(problem2): add decode function

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,23 @@
+/**
+ * remove continuous duplicated haracters in given string.
+ * @param {string} cryptogram cryptogram(string to decode)
+ * @returns {string} decoded string(string with continuous duplicated characters removed)
+ */
+function removeCharacters(cryptogram) {
+  let decodedString = "";
+  const length = cryptogram.length;
+  for (let i = 0; i < length; i++) {
+    let isUnique = true;
+    if (i - 1 >= 0 && cryptogram[i] === cryptogram[i - 1]) isUnique = false;
+    if (i + 1 < length && cryptogram[i] === cryptogram[i + 1]) isUnique = false;
+    if (isUnique) decodedString += cryptogram[i];
+  }
+  return decodedString;
+}
+
 function problem2(cryptogram) {
   var answer;
+
   return answer;
 }
 
