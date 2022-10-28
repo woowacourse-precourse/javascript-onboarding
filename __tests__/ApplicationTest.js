a049733b5d6feca5173169da421bfd4d6e3bcc1a
sujungYu <su_moment@naver.com>
2022-10-28 17:09:41 +0900

    feat(PROBLEM3):1부터 입력된 숫자만큼 배열 생성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,23 +1,23 @@
-const problem1 = require("../src/problem1");
+// const problem1 = require("../src/problem1");
 // const problem2 = require("../src/problem2");
 // const problem3 = require("../src/problem3");
 // const problem4 = require("../src/problem4");
-// const problem5 = require("../src/problem5");
+const problem5 = require("../src/problem5");
 // const problem6 = require("../src/problem6");
 // const problem7 = require("../src/problem7");
-describe("problem1", () => {
-  test("case1", () => {
-    expect(problem1([97, 98], [197, 198])).toEqual(0);
-  });
+// describe("problem1", () => {
+//   test("case1", () => {
+//     expect(problem1([97, 98], [197, 198])).toEqual(0);
+//   });
 
-  test("case2", () => {
-    expect(problem1([131, 132], [211, 212])).toEqual(1);
-  });
+//   test("case2", () => {
+//     expect(problem1([131, 132], [211, 212])).toEqual(1);
+//   });
 
-  test("case3", () => {
-    expect(problem1([99, 102], [211, 212])).toEqual(-1);
-  });
-});
+//   test("case3", () => {
+//     expect(problem1([99, 102], [211, 212])).toEqual(-1);
+//   });
+// });
 
 // describe("problem2", () => {
 //   test("case1", () => {
@@ -43,15 +43,15 @@ describe("problem1", () => {
 //   });
 // });
 
-// describe("problem5", () => {
-//   test("case1", () => {
-//     expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
-//   });
+describe("problem5", () => {
+  test("case1", () => {
+    expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
+  });
 
-//   test("case2", () => {
-//     expect(problem5(15000)).toEqual([0, 1, 1, 0, 0, 0, 0, 0, 0]);
-//   });
-// });
+  test("case2", () => {
+    expect(problem5(15000)).toEqual([0, 1, 1, 0, 0, 0, 0, 0, 0]);
+  });
+});
 
 // describe("problem6", () => {
 //   test("case1", () => {
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,5 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    const numArr = Array(number).fill().map((v,i)=> i+1);
 }
 
 module.exports = problem3;
