f0503cea139f4dda2b2e3ecf06c098bc467a9626
nuheajiohc <jaehun341012@gmail.com>
2022-10-28 22:40:24 +0900

    문제2:(refactor) 변수 재설정

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,15 @@
 function problem2(cryptogram) {
     let beforeStr = cryptogram;
-    let afterStr = delDuplication(cryptogram);
+    let afterStr = overlapCheck(cryptogram);
     while (beforeStr.length !== afterStr.length) {
         beforeStr = afterStr;
-        afterStr = delDuplication(beforeStr);
+        afterStr = overlapCheck(beforeStr);
     }
     const answer = afterStr;
     return answer;
 }
 
-function delDuplication(crypto) {
+function overlapCheck(crypto) {
     let str = '';
     for (let i = 0; i < crypto.length; i++) {
         if (crypto[i] !== crypto[i + 1] && (crypto[i - 1] !== crypto[i])) {
