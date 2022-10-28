c96539b6d013fa3df15cd20848e8e2eb50a11dcf
dev-redo <mis05041@naver.com>
2022-10-29 05:59:40 +0900

    [Style 7번] 함수, 변수명 변경

--- src/problem7.js
+++ src/problem7.js
@@ -1,9 +1,9 @@
 function problem7(user, friends, visitors) {
   const friendRelation = getFriendsAdjacencyList(friends);
   const userFriends = friendRelation[user] ?? [];
-  const friendOfFriends = getFriendOfFriends({ user, userFriends, friendRelation });
+  const userFriendsOfFriends = getFriendOfFriends({ user, userFriends, friendRelation });
 
-  const initScore = initRecommendationScore({ userFriends, friendRelation, friendOfFriends });
+  const initScore = initRecommendationScore({ userFriends, friendRelation, userFriendsOfFriends });
   const score = scoreVisitor({ initScore, visitors, userFriends });
 
   const recommendedUserList = getRecommendationUserList(score);
@@ -11,62 +11,62 @@ function problem7(user, friends, visitors) {
 }
 
 function getFriendsAdjacencyList(friends) {
-  return friends.reduce((links, [user1, user2]) => {
-    if (!links[user1]) links[user1] = [];
-    if (!links[user2]) links[user2] = [];
+  return friends.reduce((friendsAdjList, [user1, user2]) => {
+    if (!friendsAdjList[user1]) friendsAdjList[user1] = [];
+    if (!friendsAdjList[user2]) friendsAdjList[user2] = [];
 
-    links[user1].push(user2);
-    links[user2].push(user1);
+    friendsAdjList[user1].push(user2);
+    friendsAdjList[user2].push(user1);
 
-    return links;
+    return friendsAdjList;
   }, {});
 }
 
 function getFriendOfFriends({ user, userFriends, friendRelation }) {
-  const frinedOfFriends = userFriends.reduce((set, friend) => {
+  const frinedOfFriends = userFriends.reduce((friendOfFriendsSet, friend) => {
     const friends = friendRelation[friend];
     const filteredFriends = friends.filter(
       friend => friend !== user && !userFriends.includes(friend),
     );
 
-    filteredFriends.forEach(friend => set.add(friend));
-    return set;
+    filteredFriends.forEach(friend => friendOfFriendsSet.add(friend));
+    return friendOfFriendsSet;
   }, new Set());
 
   return [...frinedOfFriends];
 }
 
-function initRecommendationScore({ userFriends, friendRelation, friendOfFriends }) {
-  return friendOfFriends.reduce((map, friend) => {
+function initRecommendationScore({ userFriends, friendRelation, userFriendsOfFriends }) {
+  return userFriendsOfFriends.reduce((recommendationScore, friend) => {
     const frineds = friendRelation[friend];
     const friendsNum = frineds.filter(friend => userFriends.includes(friend)).length;
 
-    map.set(friend, friendsNum * 10);
-    return map;
+    recommendationScore.set(friend, friendsNum * 10);
+    return recommendationScore;
   }, new Map());
 }
 
 function scoreVisitor({ initScore, visitors, userFriends }) {
-  return visitors.reduce((initScore, visitor) => {
-    if (userFriends.includes(visitor)) return initScore;
+  return visitors.reduce((score, visitor) => {
+    if (userFriends.includes(visitor)) return score;
 
-    const currVisitorScore = initScore.get(visitor) || 0;
-    initScore.set(visitor, currVisitorScore + 1);
+    const currVisitorScore = score.get(visitor) || 0;
+    score.set(visitor, currVisitorScore + 1);
 
-    return initScore;
+    return score;
   }, initScore);
 }
 
 function getRecommendationUserList(score) {
-  const sortedScore = [...score].sort(([prevKey, prevVal], [currKey, currVal]) => {
-    if (prevVal !== currVal) return currVal - prevVal;
+  const sortedScore = [...score].sort(([prevName, prevScore], [currName, currScore]) => {
+    if (prevScore !== currScore) return currScore - prevScore;
 
-    if (prevKey < currKey) return -1;
-    if (prevKey > currKey) return 1;
-    if (prevKey === currKey) return 0;
+    if (prevName < currName) return -1;
+    if (prevName > currName) return 1;
+    if (prevName === currName) return 0;
   });
 
-  const recommendedUserList = sortedScore.slice(0, 5).map(([name, score]) => name);
+  const recommendedUserList = sortedScore.slice(0, 5).map(([name]) => name);
   return recommendedUserList;
 }
 
