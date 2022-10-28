1902ccb333067f17c612f03f729baf8516cc5424
SNUPI <joohaem@daum.net>
2022-10-29 03:55:57 +0900

    [ feat ] complete problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
+  let answer;
+  answer = countClap(number);
   return answer;
+}
+
+function countClap(number) {
+  let cnt = 0;
+
+  for (let currentNumber = 1; currentNumber <= number; currentNumber++) {
+    [...("" + currentNumber)].forEach((digit) => {
+      if (digit === "3" || digit === "6" || digit === "9") cnt++;
+    });
+  }
+
+  return cnt;
 }
 
 module.exports = problem3;
