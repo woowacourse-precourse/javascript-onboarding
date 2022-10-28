1fe96f24b44da607fe39694544ab492c41a336bc
SNUPI <joohaem@daum.net>
2022-10-29 03:02:44 +0900

    [ feat ] complete problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,23 @@
 function problem6(forms) {
-  var answer;
+  let answer;
+
+  const formMap = new Map();
+  const duplicateMailSet = new Set();
+
+  forms.forEach((form) => {
+    for (let i = 0; i < form[1].length - 1; i++) {
+      const word = form[1].slice(i, i + 2);
+
+      if (formMap.has(word)) {
+        duplicateMailSet.add(form[0]);
+        duplicateMailSet.add(formMap.get(word));
+      }
+
+      formMap.set(word, form[0]);
+    }
+  });
+
+  answer = [...duplicateMailSet].sort();
   return answer;
 }
 
--- src/tempCodeRunnerFile.js
+++
@@ -1,1 +0,0 @@
-    console.log("friendSet", friendSet);
