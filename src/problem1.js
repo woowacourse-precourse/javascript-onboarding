0f0f1ad34436ecf824e32a145ef23d674199575a
evencoding <evencoding@gmail.com>
2022-10-29 02:02:10 +0900

    [문제1] feat: 입력받은 값이 유효한지 체크한지 확인하는 함수 (예외 처리)

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,28 @@
-const ARR_LENGTH = 2;
+const isValidValue = (pages) => {
+  // 페이지의 범위가 1~400인지 체크
+  for (let i in pages) {
+    if (pages[i] < 1 || pages[i] > 400) {
+      return 0;
+    }
+  }
+
+  // 공차가 1인 등차수열인지 체크
+  if (pages[1] - pages[0] !== 1 || pages[3] - pages[2] !== 1) {
+    return 0;
+  }
+
+  // 왼쪽 페이지 홀수, 오른쪽 페이지 짝수인지 체크
+  if (
+    pages[0] % 2 === 0 ||
+    pages[2] % 2 === 0 ||
+    pages[1] % 2 ||
+    pages[3] % 2
+  ) {
+    return 0;
+  }
+
+  return 1;
+};
 
 const getSumAllDigit = (pageNum) => {
   return [...pageNum.toString()].reduce(
@@ -26,19 +50,19 @@ const getMaxValue = (pages) => {
 };
 
 function problem1(pobi, crong) {
-  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+  let answer = 0;
+  if (!isValidValue([...pobi, ...crong])) {
     return -1;
-  }
-  for (let i = 0; i < ARR_LENGTH; i++) {
-    if (pobi[i] < 1 || pobi[i] > 400 || crong[i] < 1 || crong[i] > 400) {
-      return -1;
-    }
   }
   const pobiValue = getMaxValue(pobi);
   const crongValue = getMaxValue(crong);
-  if (pobiValue > crongValue) return 1;
-  if (pobiValue < crongValue) return 2;
-  if (pobiValue === crongValue) return 0;
+  if (pobiValue > crongValue) {
+    answer = 1;
+  } else if (pobiValue < crongValue) {
+    answer = 2;
+  }
+
+  return answer;
 }
 
 module.exports = problem1;
