163201eeb4d949afaaf034ebfffb55a3abd703d7
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-28 10:31:09 +0900

    refactor: 함수의 길이 조절을 위해 변수명 변경

--- src/problem3.js
+++ src/problem3.js
@@ -2,17 +2,14 @@ function problem3(number) {
   let count_369 = 0;
 
   for (let i = 1; i <= number; i++) {
-    let seperate_number_array = String(i).split("");
+    let number_array = String(i).split("");
 
-    for (let j = 0; j < seperate_number_array.length; j++) {
-      if (
-        seperate_number_array[j] === "3" ||
-        seperate_number_array[j] === "6" ||
-        seperate_number_array[j] === "9"
-      )
+    for (let j = 0; j < number_array.length; j++) {
+      if (number_array[j] === "3" || number_array[j] === "6" || number_array[j] === "9")
         count_369++;
     }
   }
   return count_369;
 }
+
 module.exports = problem3;
