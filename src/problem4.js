746ad355173b1cb6b6f82f00c30ce79571a3af94
Gabriel Ju Hyun, Yoon <69189073+gabrielyoon7@users.noreply.github.com>
2022-10-29 03:56:25 +0900

    problem 4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  var answer=[];
+  for(let i = 0 ; i < word.length; i++){
+    const ascii = word.charCodeAt(i);
+    if(ascii>=65 && ascii<=90){
+      answer.push(90-ascii+65)
+    }
+    if(ascii>=97 && ascii<=122){
+      answer.push(122-ascii+97)
+    }
+    if(ascii===32){
+      answer.push(32)
+    }
+  }
+  return answer.map((num)=>String.fromCharCode(num)).join('');
 }
 
 module.exports = problem4;
