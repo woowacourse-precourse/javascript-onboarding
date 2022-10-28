bbae1fb805e7a061ad86130d2d631aa7562d2621
ilgon0110 <moses011010@gmail.com>
2022-10-28 17:56:59 +0900

    feat: 출력값 구하기 기능 구현(#problem7)

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,4 @@
 function problem7(user, friends, visitors) {
-  let answer;
-
   let withFriends = new Map();
   let scores = new Map();
   //연산
@@ -28,11 +26,28 @@ function problem7(user, friends, visitors) {
       }
     });
   }
+
   //기능목록 1-2 구현
   for (let name of visitors) {
+    if (withFriends.get(user).includes(name)) continue;
     if (scores.has(name)) {
       scores.set(name, scores.get(name) + 1);
     } else scores.set(name, 1);
+  }
+
+  //출력값 구하기
+  let answer = [];
+  let scoreArray = [...scores];
+  //기능목록 2-2 구현
+  scoreArray.sort((a, b) => {
+    if (a[1] === b[1]) return a[0] - b[0];
+    else return b[1] - a[1];
+  });
+  //기능목록 2-1 구현
+  for (let i = 0; i < 5; i++) {
+    if (scoreArray[i] !== undefined && scoreArray[i][1] !== 0) {
+      answer.push(scoreArray[i][0]);
+    }
   }
 
   return answer;
