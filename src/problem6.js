55d9b379a964c87cda735070ed9cc09a3bba7b22
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-28 12:20:37 +0900

    problem6 중복되는 닉네임 확인 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,7 @@
 function problem6(forms) {
   var answer;
   let includenickname = []
+  let duplicatenickname = []
   for (let i = 0; i<forms[0][1].length; i++){
     for (let j = 2; j<=forms[0][1].length; j++){
       if (j+i <= forms[0][1].length){
@@ -8,6 +9,16 @@ function problem6(forms) {
       }
     }
   }
+  console.log(includenickname)
+  for (let k = 1; k<forms.length; k++){
+    for (let p = 0; p<includenickname.length; p++){
+      if (forms[k][1].includes(includenickname[p])){
+        duplicatenickname.push(includenickname[p]);
+      }
+    }
+
+  }
   return answer;
 }
+console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
 module.exports = problem6;
