efb5a12fcff5dd1b7b8c5e3e5cf946c9b7206018
pangkyu <baesense@naver.com>
2022-10-28 17:41:37 +0900

    feat : number 범위를 스플릿하여 담아놓을 공간
    문자열로 잘라 3, 6, 9가 들어있는지 볼 계획이므로 구현했음

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCount = 0;
+  let numberRange = [];
+  for (let i = 1; i <= number; i++) {
+    numberRange[i] = String(i);
+    let check = numberRange[i].split("");
+  }
 }
-
+function testCode() {
+  //problem3(13);
+  problem3(33);
+}
+testCode();
 module.exports = problem3;
