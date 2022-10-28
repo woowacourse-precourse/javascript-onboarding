ffe5ed8592cf654ef2fefe7bf5c821aa5414dacf
violetwhenisawu <anashape710@gmail.com>
2022-10-28 21:02:22 +0900

    Feat: money를 화폐 단위별로 나눈 값을 반환하는 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let result = [];
+  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  function takeOutMoney(unit) {
+    let quotient = Math.floor(money / unit);
+    money = money - quotient * unit;
+    return quotient;
+  }
+
+  return result;
 }
 
 module.exports = problem5;
