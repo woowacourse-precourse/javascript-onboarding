6718bfe8a34044192482e865004a965f2f6dee74
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-28 16:24:29 +0900

    feat: problem7 setting init && tdd setting

---
+++ __tests__/Problem7Test.js
@@ -0,0 +1,75 @@
+const {
+  getScoreVisitor,
+  getVisitorDeleteFriend,
+  getScoreTogetherFriend,
+  getUserFriend,
+} = require("../src/problem7.util");
+
+describe("getUserFriend", () => {
+  it("is get user's friends", () => {
+    expect(
+      getUserFriend("mrko", [
+        ["donut", "andole"],
+        ["donut", "jun"],
+        ["donut", "mrko"],
+        ["shakevan", "andole"],
+        ["shakevan", "jun"],
+        ["shakevan", "mrko"],
+      ]).toEqual([
+        ["donut", "mrko"],
+        ["shakevan", "mrko"],
+      ])
+    );
+    expect(
+      getUserFriend("donut", [
+        ["donut", "andole"],
+        ["donut", "jun"],
+        ["donut", "mrko"],
+        ["shakevan", "andole"],
+        ["shakevan", "jun"],
+        ["shakevan", "mrko"],
+      ]).toEqual([
+        ["donut", "andole"],
+        ["donut", "jun"],
+        ["donut", "mrko"],
+      ])
+    );
+  });
+});
+
+describe("getSetFriend", () => {
+  it("is get set array that input list delete user", () => {
+    expect(
+      getScoreTogetherFriend("mrko", [
+        ["donut", "mrko"],
+        ["shakevan", "mrko"],
+      ])
+    ).toEqual({
+      donut: 10,
+      shakevan: 10,
+    });
+    expect(
+      getScoreTogetherFriend("donut", [
+        ["donut", "andole"],
+        ["donut", "jun"],
+        ["donut", "mrko"],
+      ])
+    ).toEqual({ mrko: 10, jun: 10, andole: 10 });
+  });
+});
+
+describe("getVisitorDeleteFriend", () => {
+  it("is visitor delete friend", () => {
+    expect(getVisitorDeleteFriend(["bedi", "donut"], ["donut"])).toEqual([
+      "bedi",
+    ]);
+  });
+});
+
+describe("getScoreVisitor", () => {
+  it("is add visitor score", () => {
+    expect(
+      getScoreVisitor(["bedi"], { mrko: 10, jun: 10, andole: 10 })
+    ).toEqual({ mrko: 10, jun: 10, andole: 10, bedi: 1 });
+  });
+});
---
+++ src/problem7.util.js
@@ -0,0 +1,16 @@
+// 아는 친구 list 만들기
+const getUserFriend = (user, friends) => {};
+
+// 함께 아는 친구 중 user를 제외하고 score 계산하기
+const getScoreTogetherFriend = (user, friends) => {};
+
+// 방문 user 중 친구 제거하기
+const getVisitorDeleteFriend = (visitors, firends) => {};
+
+// 방문 user 점수 계산하기
+const getScoreVisitor = (visitors, score) => {};
+
+exports.getUserFriend = getUserFriend;
+exports.getScoreTogetherFriend = getScoreTogetherFriend;
+exports.getVisitorDeleteFriend = getVisitorDeleteFriend;
+exports.getScoreVisitor = getScoreVisitor;
