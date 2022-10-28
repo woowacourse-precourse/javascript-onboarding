0e2a7b8958ecbdee8adaa63117307514d4b01b3c
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-28 16:17:20 +0900

    feat: [P4] 청개구리 사전처럼 알파벳 문자를 바꿔주는 함수
    - 모든 알파벳 문자를 순서대로 요소로 하는 Array 만들기
    - Object를 생성하여 알파벳 순서대로 key를 저장하고 알파벳 역순으로 value를 저장하여 문자를 대응시키기
    - 입력한 알파벳 문자에 대응하는 문자를 반환하기

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,15 @@ function problem4(word) {
   return answer;
 }
 
+function reverseChar(char) {
+  let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
+  let frogDic = new Object()
+  
+  for (let i = 0; i < alphabet.length; i++) {
+    let key = alphabet[i]
+    frogDic[key] = alphabet[alphabet.length - 1 - i]
+  }
+  return frogDic[char]
+}
+
 module.exports = problem4;
