1379d51ebeb7c39154e909ae5d0b74827b39f869
moonthree <fpdhslr0725@gmail.com>
2022-10-28 17:56:10 +0900

    a

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,66 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+  let pobiArr = []
+  let crongArr = []
 
+  const plusFunc = ((data) => {
+    let result = 0
+    data.forEach((element) => {
+      result += parseInt(element)
+    })
+    return result
+  })
+
+  const multiplyFunc = ((data) => {
+    let result = 1
+    data.forEach((element) => {
+      result *= parseInt(element)
+    })
+    return result
+  })
+
+  pobi.forEach((element) => {
+    const strElement = String(element).split('')
+    const plus = plusFunc(strElement)
+    const multiply =  multiplyFunc(strElement)
+
+    if (plus > multiply) {
+      pobiArr.push(plus)
+    } else {
+      pobiArr.push(multiply)
+    }
+  })
+
+  crong.forEach((element) => {
+    const strElement = String(element).split('')
+    const plus = plusFunc(strElement)
+    const multiply =  multiplyFunc(strElement)
+
+    if (plus > multiply) {
+      crongArr.push(plus)
+    } else {
+      crongArr.push(multiply)
+    }  
+  })
+
+  pobiArr.sort((a, b) => b - a)
+  crongArr.sort((a, b) => b - a)
+
+  const pobiMax = pobiArr[0]
+  const crongMax = crongArr[0]
+  
+  let answer = 0
+  console.log(pobiMax)
+  console.log(crongMax)
+  if (pobiMax > crongMax) {
+    answer = 1
+  } else if (pobiMax < crongMax) {
+    answer = 2
+  } else if (pobiMax === crongMax) {
+    answer = 0
+  } else {
+    answer = -1
+  }
+
+  return answer
+}
 module.exports = problem1;
--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let arr = []
+  forms.forEach((element) => {
+    const nickArr = element[1].split('')
+    for (let i = 0; i < nickArr.length - 1; i++) {
+      let nickCheck = nickArr[0] + nickArr[1]
+      forms.forEach((data) => {
+        if (data[0] === element[0]) {
+          return
+        } else if (data[1].includes(nickCheck)) {
+          arr.push(data[0])
+        }
+      })
+    }
+  })
+
+  let arrRemoveOverlap = []
+  arr.forEach((element) => {
+    if (arrRemoveOverlap.includes(element) === false) {
+      arrRemoveOverlap.push(element)
+    }
+  })
+  return arrRemoveOverlap.sort()
 }
 
 module.exports = problem6;
