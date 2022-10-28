c59e8e5b479471cc6c9f6d9b1bb60503a8470381
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 16:01:30 +0900

    feat: implement checkStringDuplication function

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,15 @@
+function checkStringDuplication(str) {
+  let beforeWord = '';
+  for (i = 0; i < str.length; i++) {
+    if (beforeWord === str[i]) {
+      return true;
+    } else {
+      beforeWord = str[i];
+    }
+  }
+  return false;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
