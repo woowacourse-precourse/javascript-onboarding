f0e8b3acb9c620222d1da57e54f5df9495cd0359
sungjihyun <jhsung23@gmail.com>
2022-10-28 16:57:36 +0900

    Feat: problem 6: 두글자 단위 토큰으로 분리하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,19 @@
+const makeTokenArray = (nickname) => {
+  let tokenArray = [];
+  for (let i = 0; i < nickname.length / 2; i++) {
+    const token = nickname.slice(i, i + 2);
+    tokenArray.push(token);
+  }
+  return tokenArray;
+};
+
 function problem6(forms) {
-  var answer;
+  var answer = [];
+
+  const nicknameTokens = forms.reduce((acc, [email, nickname]) => {
+    return acc.concat(makeTokenArray(nickname));
+  }, []);
+
   return answer;
 }
 
