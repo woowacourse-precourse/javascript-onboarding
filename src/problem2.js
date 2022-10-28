90da15de174f48117b9ffc6fdef286fcb5f7f932
김희진 <choha941108@gmail.com>
2022-10-28 14:36:03 +0900

    (2번) 입력받은 단어 배열화 / 배열의 중복 알파벳 삭제 및 삭제완료 배열 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,23 @@
-function problem2(cryptogram) {
-  var answer;
+function problem2(cryptogram) { //단어하나를 받는다
+  var answer = [] ;
+  const cryptogram_add = [...cryptogram]
+  console.log("단어 배열변경",cryptogram_add)
+  //단어를 알파벳 별로 배열로 쪼갠다
+
+  answer.push(cryptogram_add[0])
+  for (i = 1 ; i < cryptogram_add.length ; i ++) { 
+    if (cryptogram_add[i] != answer.at(-1)) {
+      answer.push(cryptogram_add[i])
+    } else {
+      answer.pop()
+    }
+    console.log("단어 중복값 삭제",answer) 
+  }
+  //더이상 아무것도 삭제 안될때까지 반복  
+    //배열을 순회하면서, 중복시 삭제
+      //중복 삭제 배열 반환
+
+  //마지막에 만들어진 단어를 리턴한다
   return answer;
 }
 
