23e35b8f87b16570261e5d7f5662ab47f0c6ffe1
DongWooKim97 <nanaukim@naver.com>
2022-10-28 22:59:08 +0900

    feat(P3): 입력값에 대한 유효성 검사 및 예외처리 기능 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  if (!inputValidCheck(number)) {
+    return 'error'
+  }
+  // return 'checkValid Success'
+}
+
+function inputValidCheck(number) {
+  if(number < 1 || number > 1000) return false;
+  if(!Number.isInteger(number)) return false;
+  return true
 }
 
 module.exports = problem3;
