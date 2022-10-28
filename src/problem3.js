8a769013fdcbb48ed2b31fcf65c635b938e4cc18
yong <tkddbs1644@gmail.com>
2022-10-28 17:02:00 +0900

    문제3: 1부터 number까지 손뼉치는 횟수를 구하고 결과를 반환한다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,6 @@
 /*
 [x] 숫자를 인수로 받아 손뼉치는 횟수를 구하는 기능을 구현한다.
-[ ] 1부터 number까지 손뼉치는 횟수를 구하는 기능을 구현하고 결과를 반환한다.
+[x] 1부터 number까지 손뼉치는 횟수를 구하는 기능을 구현하고 결과를 반환한다.
 */
 
 function countClap369(number) {
@@ -15,8 +15,11 @@ function countClap369(number) {
 }
 
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCount = 0;
+  for (let num = 1; num <= number; num += 1) {
+    clapCount += countClap369(num);
+  }
+  return clapCount;
 }
 
 module.exports = problem3;
