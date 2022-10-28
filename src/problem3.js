448cb13fd94dd27f46fa0a44099d7842a6243ef8
gyeongza <gyeongza@naver.com>
2022-10-29 01:31:46 +0900

    feat: 1부터 입력값(num)까지 숫자 생성

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
-function problem3(number) {
-  var answer;
-  return answer;
+// 1부터 입력값(number)까지 숫자 생성
+const createNum = (num) => {
+  for (let i = 1; i <= num; i++){
+    const number = i.toString();
+  }
 }
+// function problem3(number) {
+//   var answer;
+//   return answer;
+// }
 
-module.exports = problem3;
+// module.exports = problem3;
