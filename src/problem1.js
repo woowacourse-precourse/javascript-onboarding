9ee4b0a10ee377d2c9aa04d11c7ad63869d197a5
HoberMin <sonhomin98@naver.com>
2022-10-29 01:16:53 +0900

    Update problem solving in problem2

--- src/problem1.js
+++ src/problem1.js
@@ -1,67 +1,63 @@
 function problem1(pobi, crong) {
-  let pobi_max = 0;
-  let crong_max = 0;
-  let multi = 1;
-  let total = 0;
+  let num;
 
-  if (except(pobi, crong)) {
+  function pagesum(num) {
+    let a, b, c;
+    if (num > 100) {
+      a = num / 100;
+      b = (num % 100) / 10;
+      c = num % 10;
+      return a + b + c;
+    } else {
+      b = (num % 100) / 10;
+      c = num % 10;
+      return b + c;
+    }
+  }
+
+  function pagemult(num) {
+    let a, b, c;
+    if (num > 100) {
+      a = parseInt(num / 100);
+      b = parseInt((num % 100) / 10);
+      c = num % 10;
+      return a * b * c;
+    } else {
+      b = parseInt((num % 100) / 10);
+      c = num % 10;
+      return b * c;
+    }
+  }
+
+  for (let i = 0; i < 2; i++) {
+    if (pobi[i] == 1 || crong[i] == 1 || pobi[i] == 400 || crong[i] == 400) {
+      return -1;
+    }
+  }
+
+  if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) {
     return -1;
   }
 
-  for (let pobiNumber of pobi) {
-    while (pobiNumber > 0) {
-      multi *= pobiNumber % 10;
-      total += pobiNumber % 10;
-      pobiNumber = Math.floor(pobiNumber / 10);
-    }
+  let pobimax = Math.max(
+    pagemult(pobi[0]),
+    pagemult(pobi[1]),
+    pagesum(pobi[0]),
+    pagesum(pobi[1])
+  );
+  let crongmax = Math.max(
+    pagemult(crong[0]),
+    pagemult(crong[1]),
+    pagesum(crong[0]),
+    pagesum(crong[1])
+  );
 
-    if (pobi_max < multi) pobi_max = multi;
-    if (pobi_max < total) pobi_max = total;
-    multi = 1;
-    total = 0;
-  }
-  console.log(pobi_max);
-
-  for (let crongNumber of crong) {
-    while (crongNumber > 0) {
-      multi *= crongNumber % 10;
-      total += crongNumber % 10;
-      crongNumber = Math.floor(crongNumber / 10);
-    }
-
-    if (crong_max < multi) crong_max = multi;
-    if (crong_max < total) crong_max = total;
-    multi = 1;
-    total = 0;
-  }
-
-  console.log(crong_max);
-
-  if (pobi_max > crong_max) {
-    return 1;
-  } else if (pobi_max < crong_max) {
-    return 2;
-  } else if (pobi_max === crong_max) {
+  if (pobimax == crongmax) {
     return 0;
-  } else {
-    return -1;
+  } else if (pobimax > crongmax) {
+    return 1;
+  } else if (pobimax < crongmax) {
+    return 2;
   }
 }
-
-function except(pobi, crong) {
-  if (pobi[0] + 1 !== pobi[1]) return true; //왼쪽페이지와 오른쪽 페이지가 연속된 숫자가 아닐때
-
-  if (crong[0] + 1 !== crong[1]) return true; //왼쪽페이지와 오른쪽 페이지가 연속된 숫자가 아닐때
-
-  if (pobi[0] % 2 === 0 || crong[0] % 2 === 0) return true; ////왼쪽페이지 짝수일때
-
-  if (pobi[0] === 1 || pobi[1] === 400 || crong[0] === 1 || crong[1] === 400)
-    return true;
-  //pobi,crong 왼쪽페이지가 1이거나 오른쪽페이지가 400일때
-}
-
 module.exports = problem1;
-const pobi = [399, 400];
-const crong = [159, 160];
-const answer = problem1(pobi, crong);
-console.log(answer);
--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let cryptogramArray = cryptogram.split("");
+  let cryptogramArray2 = "";
+  let waste = "";
+  cryptogramArray.forEach((code, index, cryptogramArray) => {
+    if (
+      code !== cryptogramArray[index - 1] &&
+      code !== cryptogramArray[index + 1]
+    ) {
+      cryptogramArray2 += code;
+    } else {
+      waste += code;
+    }
+  });
+
+  if (waste === "") {
+    return cryptogramArray2;
+  } else {
+    return problem2(cryptogramArray2);
+  }
 }
 
 module.exports = problem2;
+console.log(problem2("aasqwerwqsdseeesrr"));
