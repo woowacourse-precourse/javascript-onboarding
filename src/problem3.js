7e577b7db5f868c07394b747c2e0fac160030814
hailey-hy <lillyine.hy@gmail.com>
2022-10-28 12:16:36 +0900

    feat: problem3 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for(let i = number; i > 0; i--){
+    for(let j = 0; j < String(i).length; j++){
+      if(String(i)[j] == '3' || String(i)[j] == '6' || String(i)[j] == '9'){
+        answer += 1;
+      }
+    }
+  }
+
   return answer;
 }
 
