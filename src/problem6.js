c9e77cc6d4c58a199d52f250174df0bd60390373
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-28 23:49:41 +0900

    feat: (P6) 닉네임의 두 글자 & 중복 이메일 저장 할 Map, Set 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  // Map(닉네임 두 글자씩 저장), Set(중복 이메일 저장) 선언
+  let twoLetterMap = new Map();
+  let duplicateEmailSet = new Set();
 }
+
+// 크루들의 닉네임 2글자씩 저장하는 기능 (map 사용)
+function setMapTwoLetter() {}
+
+// 크루 닉네임 중복 확인 기능
+function isDuplicateNickname() {}
 
 module.exports = problem6;
