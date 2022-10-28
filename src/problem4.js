88fd63dc2981035c3587affb7babaf2afc9d7527
Dean <yjj052800@gmail.com>
2022-10-28 20:23:43 +0900

    FEAT: [4번] 청개구리 사전 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,28 @@
+const NORMAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const FROGDICTION = NORMAL.split('').reverse();
+
 function problem4(word) {
   var answer;
+
+  frogDiction(word);
+  console.log(frogDiction(word));
   return answer;
 }
+
+
+function frogDiction(word) {
+  let result = "";
+  for (i = 0; i <= word.length; i++) {
+    if (word[i] === ' ') result += word[i];
+    else {
+      const convertWord = FROGDICTION[NORMAL.split('').indexOf(word[i])];
+      result += convertWord;
+    }
+  } 
+  return result;
+}
+
+problem4("I LOVE YOU");
+
 
 module.exports = problem4;
