82ea15bc22e5e9a0545a75be2ac73570625f2081
inthhh <glass005533@gmail.com>
2022-10-28 16:19:11 +0900

    p6_FindDupl

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
   var answer;
+  let len = forms.length;
+  let duplMails = new Array(len).fill(0);
+  
+  for(let i=0; i<len; ++i){
+    for(let j=0; j<len; ++j){
+      let word = forms[i][1].substr(j,2);
+      
+      if(word.length<2) break;
+      console.log(word);
+      if(forms[j][1].indexOf(word) >= 0) {
+        duplMails[i] = 1;
+        duplMails[j] = 1;
+      }
+    }
+  }
+
+
   return answer;
 }
 
