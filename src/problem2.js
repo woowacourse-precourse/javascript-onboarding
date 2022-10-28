03a4c7bd9e898606567b971e16e3cc57393f4511
김희진 <choha941108@gmail.com>
2022-10-28 14:42:20 +0900

    (2번) 최종 단어 생성완료

--- src/problem2.js
+++ src/problem2.js
@@ -16,9 +16,11 @@ function problem2(cryptogram) { //단어하나를 받는다
   //더이상 아무것도 삭제 안될때까지 반복  
     //배열을 순회하면서, 중복시 삭제
       //중복 삭제 배열 반환
-
-  //마지막에 만들어진 단어를 리턴한다
+  answer = answer.join("")
+  console.log("단어확인",answer)
   return answer;
+  
+    //마지막에 만들어진 단어를 리턴한다
 }
 
 module.exports = problem2;
