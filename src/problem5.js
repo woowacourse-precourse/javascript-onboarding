ce8742dfae9e19356ff9ecb78c4ba8eebfdb85e2
최희원 <jeeen1@naver.com>
2022-10-28 22:06:24 +0900

    feat(problem5): solve problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
+const MONEY_COUNT = 9;
+const MONEYS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
   var answer;
+
+  var answer = new Array().fill(0);
+  for (let i = 0; i < MONEY_COUNT; i++) {
+    const count = Math.floor(money / MONEYS[i]);
+    answer[i] = count;
+    money -= count * MONEYS[i];
+  }
+
   return answer;
 }
 
