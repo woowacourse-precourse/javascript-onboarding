3b9ef39b72aa11b55555293585fe0730d80a3012
semnil5202 <semnil5202@gmail.com>
2022-10-29 01:37:12 +0900

    문제3번. 손뼉 친 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,19 @@ function problem3(number) {
   return answer;
 }
 
+function countClap(requestedNumber) {
+  let clapNumber = 0;
+  while (requestedNumber > 2) {
+    let check369 = requestedNumber;
+    while (check369 > 0) {
+      if (check369 % 10 === 3 || check369 % 10 === 6 || check369 % 10 === 9) {
+        clapNumber++;
+      }
+      check369 = parseInt(check369 / 10, 10);
+    }
+    requestedNumber--;
+  }
+  return clapNumber;
+}
+
 module.exports = problem3;
