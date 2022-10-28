c2b09659d8ac4682bf8ae5c74341fd358f687ac5
cofla <cofla159@naver.com>
2022-10-29 06:39:19 +0900

    풀이 방법 sudo

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,10 @@ function problem1(pobi, crong) {
   return answer;
 }
 
-module.exports = problem1;
+export default problem1;
+/*
+0. pobi[0]+1 = pobi[1]이 아니거나 pobi%2==0이면 -1리턴
+1. Math.max(pobi[0].split('').reduce(더하기 or 곱하기))
+2. pobi[1]에 대해서도 1번 하고 1,2중에 큰 값이 pobi 점수
+3. crong도 1~2번으로 점수 구해서 이긴 대로 결과
+*/
