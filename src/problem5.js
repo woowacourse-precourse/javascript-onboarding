11f6b98a0131f5aa858ad4e0942e5cf3738f5a25
Dean <yjj052800@gmail.com>
2022-10-29 00:07:00 +0900

    FEAT: [5번] 예외 사항 처리 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,9 @@
 function problem5(money) {
+  if (!inputExecption(money)) return money;
   var answer;
   let changeResult = [];
   const changeMachine = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
   for (i = 0; i <= changeMachine.length - 1; i++) {
     let changeCnt = Math.floor(money / changeMachine[i]);
     money -= (changeCnt * changeMachine[i]);
@@ -11,8 +13,14 @@ function problem5(money) {
   return answer;
 }
 
+function inputExecption(num) {
+  let inputCheck = true;
+  if (num < 1 || num > 1000000) inputCheck = false;
+  if (num == null) inputCheck = false;
+  return inputCheck;
+}
 
 
 module.exports = problem5;
 
-console.log(problem5(65000));
+console.log(problem5(232122));
