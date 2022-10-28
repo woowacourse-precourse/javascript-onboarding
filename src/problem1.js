8c4de904b47df508c9f47b6b59d12bb5042a93c5
ZUITOPIA <korhwngml123@naver.com>
2022-10-28 15:26:06 +0900

    feat: 요구, 제한사항 적용 및 출력할 결과 셋팅

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,24 @@
 function problem1(pobi, crong) {
-  var answer;
+  var answer = -1;
+  const check = (arr) => {
+    let leftPageNum = arr[0];
+    let rightPageNum = arr[1];
+
+    if (
+      arr.length !== 2 ||
+      !(0 < leftPageNum && leftPageNum < 400) ||
+      !(leftPageNum % 2 === 1) ||
+      !(rightPageNum - leftPageNum === 1)
+    )
+      return false;
+    return true;
+  };
+
+  if (check(pobi) === true && check(crong) === true) {
+    const pobiMax = compareScore(pobi);
+    const crongMax = compareScore(crong);
+    answer = pobiMax === crongMax ? 0 : pobiMax > crongMax ? 1 : 2;
+  } else answer = -1;
   return answer;
 }
 
