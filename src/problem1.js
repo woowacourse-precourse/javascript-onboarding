5f008154c2cdbcb446c30975ae79dca9d8f9fd39
coolIlive <galilee155@naver.com>
2022-10-29 00:26:42 +0900

    Refactor(problem1): 예외상황 처리 기능을 분리하다
    isRightPage 함수를 호출하여 잘못된 페이지인 경우 -1을 리턴한다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,43 +1,35 @@
 function problem1(pobi, crong) {
   var answer;
-  let pobiScore=0, crongScore=0;
+  // let pobiScore=0, crongScore=0;
 
   // 잘못된 페이지인 경우 -1리턴
-  if (pobi[1]-pobi[0]!==1 || crong[1]-crong[0]!==1) {
-    return -1;
-  };
-  if (pobi[0]===1 ||crong[0]===1 ||pobi[1]===400||crong[0]===400) {
-    return -1;
-  };
+  if (!isRightPage(pobi) || !isRightPage(crong)) {
+    return -1
+  }
 
-  // pobi의 점수
-  pobi.forEach((page)=> {
-    let addScore=0, mulScore=1;
-    do {
-      addScore+=page%10;
-      mulScore*=page%10;
-      page=Math.floor(page/10);
-    } while(page>0);
-    pobiScore=Math.max(addScore,mulScore,pobiScore);
-  });
-
-  // crong의 점수
-  crong.forEach((page)=> {
-    let addScore=0, mulScore=1;
-    do {
-      addScore+=page%10;
-      mulScore*=page%10;
-      page=Math.floor(page/10);
-    } while(page>0);
-    crongScore=Math.max(addScore,mulScore,crongScore);
-  });
+  // 각자의 점수 계산 함수
 
   // pobiScore와 crongScore를 비교하여 answer에 값을 넣고 리턴하기
-  if (pobiScore>crongScore) answer=1;
-  else if (pobiScore<crongScore) answer=2;
-  else answer=0;
+
+  // if (pobiScore>crongScore) answer=1;
+  // else if (pobiScore<crongScore) answer=2;
+  // else answer=0;
   
   return answer;
 }
 
-module.exports = problem1;
+function isRightPage(pages) {
+  if (pages[1]-pages[0]!==1) {
+    return 0;
+  };
+  if (pages[0]===1 || pages[1]===400) {
+    return 0;
+  };
+}
+
+
+console.log(problem1([97, 102],[197, 198]))
+
+
+
+// module.exports = problem1;
