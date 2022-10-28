0408f18b0da8fa07b61f4cf3ae4b6a0b31a3ff79
millifail <hwangbird99@gmail.com>
2022-10-28 23:08:46 +0900

    문제2 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
 function problem2(cryptogram) {
   var answer;
+
+  var flag = true;
+
+  while (flag) {
+    flag = false;
+    for (var i = 0; i < cryptogram.length - 1; i++) {
+      if (cryptogram[i] === cryptogram[i + 1]) {
+        cryptogram = cryptogram.replace(cryptogram[i] + cryptogram[i + 1], "");
+        flag = true;
+        break;
+      }
+    }
+  }
+
+  answer = cryptogram;
   return answer;
 }
 
