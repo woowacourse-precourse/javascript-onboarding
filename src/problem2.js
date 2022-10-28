a4902077d311d4e5e44dd7f3c6da6e518be0a60c
nerdchanii <nerdchanii@gmail.com>
2022-10-28 17:27:00 +0900

    feat: [PROBLEM2] 암호문 판별함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+}
+
+/**
+ * cryptoGram인지 판단
+ * @param {string} string
+ * @param {Regex} regex
+ * @returns {Boolean} boolean
+ */
+ function isCryptoGram(string, regex) {
+  return regex.test(string);
 }
 
 module.exports = problem2;
