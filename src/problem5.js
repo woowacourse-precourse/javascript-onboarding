cb60c6da953ef5b1e72905489370a2391670a03f
최희원 <jeeen1@naver.com>
2022-10-28 22:07:20 +0900

    fix(problem5): give array length

--- src/problem5.js
+++ src/problem5.js
@@ -4,7 +4,7 @@ const MONEYS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 function problem5(money) {
   var answer;
 
-  var answer = new Array().fill(0);
+  var answer = new Array(MONEY_COUNT).fill(0);
   for (let i = 0; i < MONEY_COUNT; i++) {
     const count = Math.floor(money / MONEYS[i]);
     answer[i] = count;
