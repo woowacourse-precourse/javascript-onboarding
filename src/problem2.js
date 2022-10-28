378cd98bf0adf770a23f59c4af2a97b902767f2d
Raehyeon Jeong <raehy19@gmail.com>
2022-10-28 18:51:12 +0900

    feat(problem2) : 입력값 예외 처리 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
+function hasNonLowercase(str) {
+  for (let i = 0; i < str.length; ++i) {
+    if (str.charCodeAt(i) < 97 || str.charCodeAt(i) > 122) return true;
+  }
+  return false;
+}
+
+function isException(str) {
+  return (
+    !(typeof str === 'string') ||
+    str.length > 1000 ||
+    str.length < 1 ||
+    hasNonLowercase(str)
+  );
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (isException(cryptogram)) return;
 }
 
 module.exports = problem2;
