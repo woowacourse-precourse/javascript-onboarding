9f6ed815c750af484201a368338efc57c31b06b7
ho-ji <wkdd0205@gmail.com>
2022-10-28 23:49:35 +0900

    feat: 예외사항 처리기능 추가
    각 페이지가 요구사항에 맞지 않는 값일때 -1을 리턴함
    왼쪽 페이지와 오른쪽 페이지의 차이가 1이 아닐 때도 -1을 리턴함

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,10 @@
 function problem1(pobi, crong) {
   var answer;
+  if (pobi[0] < 1 || pobi[0] > 399 || pobi[0] % 2 == 0) return -1;
+  if (pobi[1] < 2 || pobi[1] > 400 || pobi[1] % 2 == 1) return -1;
+  if (crong[0] < 1 || crong[0] > 399 || crong[0] % 2 == 0) return -1;
+  if (crong[1] < 2 || crong[1] > 400 || crong[1] % 2 == 1) return -1;
+  if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) return -1;
   return answer;
 }
 
