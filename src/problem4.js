7163cb352897c658fcf02a2c9d03d0a75b0b6ea4
pp449 <mma7710@naver.com>
2022-10-29 00:41:28 +0900

    [문제 4] : 2. 아스키코드를 통해 알파벳을 바꿔주는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,18 +1,25 @@
+const transformAlpabat = (asciiNum, a_ascii) => {
+  let interval = asciiNum - a_ascii;
+  return a_ascii - interval + 25;
+}
+
 const findAlpabat = (asciiNum) => {
-  if((65 <= asciiNum && asciiNum <= 90) || (97 <= asciiNum && asciiNum <= 122)) {
-    console.log("범위내 있음")
-  }
-  else {
-    console.log("범위내 없음");
-  }
+  if(65 <= asciiNum && asciiNum <= 90)        // 대문자
+    return String.fromCharCode(transformAlpabat(asciiNum, 65));
+  else if(97 <= asciiNum && asciiNum <= 122)  // 소문자
+    return String.fromCharCode(transformAlpabat(asciiNum, 97));
+  else if(asciiNum == 32) // 스페이스
+    return " ";
+  return "";
 }
 
 function problem4(word) {
+  let answer = "";
   for(let i = 0; i<word.length; i++) {
-    findAlpabat(word.charCodeAt(i));
+    answer += findAlpabat(word.charCodeAt(i));
   }
-}
 
-console.log(problem4("I love you"));
+  return answer;
+}
 
 module.exports = problem4;
