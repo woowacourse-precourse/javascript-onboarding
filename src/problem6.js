0d6a8df7e07eb90ec0b0c28db48810bf0dde101f
pinkishincoloragain <smb1103@gmail.com>
2022-10-28 20:43:58 +0900

    feat: solved problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,35 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  // nickSliceMap: { slice : Set(email) }
+  const nickSliceMap = new Map();
+
+  forms.forEach((form) => addFormToMap(form, nickSliceMap));
+  return getEmailTargets(nickSliceMap);
 }
+
+const addFormToMap = (form, nickSliceMap) => {
+  const [email, nickName] = form;
+
+  getSlices(nickName).forEach((slice) => {
+    if (!nickSliceMap.has(slice)) nickSliceMap.set(slice, new Set());
+    nickSliceMap.get(slice).add(email);
+  });
+
+  return nickSliceMap;
+};
+
+// nickname slice length: 2
+const getSlices = (str) => Array(str.length - 1)
+  .fill()
+  .map((_, i) => str[i] + str[i + 1]);
+
+const getEmailTargets = (nickSliceMap) => {
+  const emailTargets = new Set();
+
+  nickSliceMap.forEach((emails, key) => {
+    if (emails.size > 1) emails.forEach((email) => emailTargets.add(email));
+  });
+
+  return [...emailTargets].sort();
+};
 
 module.exports = problem6;
