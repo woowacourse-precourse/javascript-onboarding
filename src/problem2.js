1530358115ec0b6430bee0d0b43e0febe1bac64f
pangkyu <baesense@naver.com>
2022-10-28 16:09:48 +0900

    feat : 문자열 자르는 기능
    1. cryptogram의 i번째 데이터를 받는 것을로 문자열을 자름

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const crypto = cryptogram.split("");
+  console.log(crypto);
+  let cry;
+  for (let i = 0; i < cryptogram.length; i++) {
+    console.log(cryptogram[i]);
+    if (cryptogram[i] === cryptogram[i + 1]) {
+    }
+  }
 }
+
+function testCode() {
+  testcode = "browoanoommnaon";
+  // testcode = "zyelleyz";
+
+  problem2(testcode);
+}
+testCode();
 
 module.exports = problem2;
