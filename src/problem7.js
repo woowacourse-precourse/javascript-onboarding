6179c61fbf2a93e77531b3d4620d9267c492c25f
lee-il-woo <empty001430@gmail.com>
2022-10-28 21:25:20 +0900

    문제7 유저의 친구의 친구를 찾아주는 함수 작성

--- src/problem7.js
+++ src/problem7.js
@@ -2,15 +2,42 @@
 //유저와 친구인 배열을 리턴하는 함수
 function searchFriends(user, friends){
   let user_friends = [];
-  friends.map((element)=>{
+  let friends_index = [];
+  friends.map((element,index)=>{
     if(element[0] == user){
-      user_friends.push(element[1])
+      user_friends.push(element[1]);
+      friends_index.push(index)
+      return;
     }
     if(element[1] == user){
       user_friends.push(element[0])
+      friends_index.push(index)
+      return;
     }
   })
-  return user_friends;
+
+  friends_index.map((element,i)=>{
+
+    friends.splice(element-i,1)
+  })
+  return [user_friends,friends];
+}
+
+//유저의 친구의 친구를 리턴하는 함수
+function searchFriendsFriend(user_friends/**유저와 친구인 배열 */,friends/**유저와 친구를 거른 배열 */){
+  let recommand_10 = []
+  user_friends.map((user)=>{
+    friends.map((element)=>{
+      if(element[0] == user){
+        recommand_10.push(element[1]);
+        return;
+      }
+      if(element[1] == user){
+        recommand_10.push(element[0])
+        return;
+      }})
+  })
+  return recommand_10
 }
 
 function problem7(user, friends, visitors) {
