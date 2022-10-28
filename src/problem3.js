6dbd441b7a277e6f2d7671fa7e47b11657c49a9f
Lee-Young-Jae <yjlee1937@naver.com>
2022-10-28 22:12:02 +0900

    문제 3 - 기능 요구사항 정리

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,9 @@
+/** 기능 목록
+ * 1. 숫자에서 몇개의 3, 6, 9가 있는지를 반환하는 count369 함수 작성
+ * 2. 파라미터로 입력받은 숫자만큼 1부터 n까지 count369 함수를 호출하여
+ *    손뼉의 횟수를 반환한다.
+ */
+
 function problem3(number) {
   var answer;
   return answer;
