f3fea7bfb98b5b3b645f4dee2322b740ec4cb4bb
Tory <88531407+1two13@users.noreply.github.com>
2022-10-28 15:27:40 +0900

    refactor(problem2): answer 초기값 빈배열로 변경

--- src/problem2.js
+++ src/problem2.js
@@ -3,7 +3,7 @@
 // 2. 같으면 answer의 마지막 글자 제거하기
 
 function problem2(cryptogram) {
-  let answer = cryptogram[0];
+  let answer = "";
 
   for (let i = 1; i < cryptogram.length; i++) {
     if (answer[answer.length - 1] !== cryptogram[i]) answer += cryptogram[i];
