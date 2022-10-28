1a63387d324544c69f67f7dafc57a3a4f37bcf9a
SeungWoo Kim <osabero@naver.com>
2022-10-28 17:25:29 +0900

    코드 구현햇는데 아직 제대로 안돼서 일단 놔두었습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,20 @@
 function problem2(cryptogram) {
   var answer;
+  var i = 0
+  while (i < cryptogram.length){
+    if(cryptogram[i] !== cryptogram[i+1]) {
+      i += 1
+      continue
+    }
+    for(let j = i+1;j<cryptogram.length;j++){
+      if(cryptogram[j] == cryptogram[i]){continue}
+      cryptogram = cryptogram.slice(0,i) + cryptogram.slice(j,)
+      i = 0
+    }
+  }
+  answer = cryptogram
+
   return answer;
-  
 }
 
 module.exports = problem2;
