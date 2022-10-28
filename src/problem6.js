b29f96122e403deae32e8b575fc91db0ad1a5d0b
yong <tkddbs1644@gmail.com>
2022-10-28 20:01:44 +0900

    문제6: 중복된 닉네임을 사용하는 유저를 찾는 기능을 구현한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,6 @@
 /*
 [x] 단어를 포개서 자르는 기능을 구현한다.
-[ ] 중복된 닉네임을 사용하는 유저를 찾는 기능을 구현한다.
+[x] 중복된 닉네임을 사용하는 유저를 찾는 기능을 구현한다.
 [ ] 중복된 닉네임을 사용하는 유저들을 반환한다.
 */
 
@@ -11,6 +11,20 @@ function overlapWord(word) {
     slicedWords.push(slicedWord);
   }
   return slicedWords;
+}
+
+function makeDuplicatedUserTable(forms) {
+  const userTable = {};
+  for (const [email, nickname] of forms) {
+    const slicedNicknames = overlapWord(nickname);
+    for (const slicedNickname of slicedNicknames) {
+      if (!userTable[slicedNickname]) {
+        userTable[slicedNickname] = new Set();
+      }
+      userTable[slicedNickname].add(email);
+    }
+  }
+  return userTable;
 }
 
 function problem6(forms) {
