d03573a16f8292205f74339e80c7f8a9b1d4ad52
seongjin <77876601+ysjkof@users.noreply.github.com>
2022-10-28 12:38:49 +0900

    feat: problem7(유저의 친구 찾기 구현)

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,19 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  // - [x] 유저(mrko)의 친구 찾기
+  const userFriends = friends
+    .filter((friend) => friend.includes(user))
+    .map((friend) => friend.filter((_friend) => _friend !== user))
+    .flat(1);
+
+  // - [ ] 유저(mrko)의 친구의 친구 찾기
+
+  // - [ ] 결과 반환하기
+  //    - 점수가 가장 높은 순으로 최대 5명 반환한다.
+  //    - 추천 점수 0 -> 반환 안한다.
+  //    - 추천 점수 같음 -> 이름순으로 정렬한다.
+
   return answer;
 }
 
