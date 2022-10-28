cd896b07c4bad5ea2094b280bc0b89293dc59bd8
Siby1lA <psb4644@gmail.com>
2022-10-28 20:17:20 +0900

    feat: 청개구리식 리턴 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,15 +1,30 @@
+const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWER_CASE = "abcdefghijklmnopqrstuvwxyz";
+function isUpperCase(char) {
+  return UPPER_CASE.includes(char) ? true : false;
+}
 function isAlphabet(char) {
   return char.match(/[a-zA-z]/) ? true : false;
 }
 function isWordValid(word) {
   return word < 1 || word > 1000 ? true : false;
 }
-
+function translation(char) {
+  if (isUpperCase(char)) {
+    const index = UPPER_CASE.split("").findIndex(
+      (upperChar) => upperChar === char
+    );
+    return UPPER_CASE[25 - index];
+  } else {
+    const index = LOWER_CASE.split("").findIndex((lowChar) => lowChar === char);
+    return LOWER_CASE[25 - index];
+  }
+}
 function problem4(word) {
   let answer = "";
   if (isWordValid(word)) return -1;
   [...word].forEach((char) => {
-    if (isAlphabet(char)) null;
+    if (isAlphabet(char)) answer += translation(char);
     else answer += char;
   });
   return answer;
