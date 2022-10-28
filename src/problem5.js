eba966da046db9497fc3c1c7059c815560b2505e
Sukyeong Park <zztnrudzz13@naver.com>
2022-10-28 18:31:07 +0900

    feat: implement divideMoney function

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,9 @@
+const MONEY_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+function divideMoney(money, moneyDigit) {
+  return parseInt(money / moneyDigit);
+}
+
 function problem5(money) {
   var answer;
   return answer;
