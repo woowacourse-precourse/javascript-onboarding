4130ba22f6f9fd386cf51812404ffb66ed415977
2je0 <bulljy@naver.com>
2022-10-29 05:29:42 +0900

    ✨ feat(problem1): 페이지수 자리별로 더하거나 곱한 값 구하는 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,12 +1,28 @@
 function isError(arr) {
-  if (arr[0] < 1 || arr[1] > 400) return 1;
-  if (arr[0] % 2 == 0) return 1;
-  if (arr[0] + 1 !== arr[1]) return 1;
+  let [left, right] = arr;
+  if (left < 1 || right > 400) return 1;
+  if (left % 2 == 0) return 1;
+  if (left + 1 !== right) return 1;
   return 0;
+}
+function pageNumSum(num) {
+  let ret = 0;
+  while (num) {
+    ret = ret + (num % 10);
+    num = Math.trunc(num / 10);
+  }
+  return ret;
+}
+function pageNumMult(num) {
+  let ret = 1;
+  while (num) {
+    ret = ret * (num % 10);
+    num = Math.trunc(num / 10);
+  }
+  return ret;
 }
 function problem1(pobi, crong) {
   var answer;
   return answer;
 }
-
 module.exports = problem1;
