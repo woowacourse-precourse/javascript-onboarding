a27d7bc07172f3a5c6a48ad25d4417e7a1a27a0c
jihun1233 <leejihun1233@gmail.com>
2022-10-28 20:31:23 +0900

    user의 친구 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,14 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  const userFriends = friends.reduce((acc, friend)=>{
+    const [userA, userB] = friend
+    if(userA === user) acc.add(userB)
+    if(userB === user) acc.add(userA)
+    return acc
+  },new Set())
+
+
   return answer;
 }
 
