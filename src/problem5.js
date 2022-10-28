e5e22226a7f0fdc59461c70400b0d31b50912571
yong <tkddbs1644@gmail.com>
2022-10-28 18:44:56 +0900

    문제5: 큰 단위부터 돈을 나누고 결과를 반환한다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,6 @@
 /*
 [x] 돈을 단위로 나누었을 때 몫과 나머지를 반환하는 기능을 구현한다.
-[ ] 큰 단위부터 돈을 나누고 결과를 반환한다.
+[x] 큰 단위부터 돈을 나누고 결과를 반환한다.
 */
 
 function divideMoney(money, unit) {
@@ -8,8 +8,12 @@ function divideMoney(money, unit) {
 }
 
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return moneyUnits.map((moneyUnit) => {
+    const [quotient, remainder] = divideMoney(money, moneyUnit);
+    money = remainder;
+    return quotient;
+  });
 }
 
 module.exports = problem5;
