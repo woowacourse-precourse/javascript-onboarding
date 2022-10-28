b83f49c30ffaecfe47b4c724a31a98beb293f7d2
park junwoo <always1997@kakao.com>
2022-10-28 14:55:57 +0900

    problem4 대문자 판별 함수 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,20 @@
+/*
+  기능 목록
+  1. 대문자 판별하는 함수 is_upperCase() 구현
+  2. 문자를 반대로 변환하는 함수 reverseChar() 구현
+  3. 결과 출력
+*/
+
+// 대문자 1 소문자 0 공백 -1 리턴
+function is_UpperCase(char) {
+  if(char === ' ') return -1;
+  if(char === char.toUpperCase()) return 1;
+  return 0;
+}
+
 function problem4(word) {
-  var answer;
+  let answer = '';
+
   return answer;
 }
 
