3ae69a154e2feb810f694bf916a58a423f4b3c88
leeseooo <leeseoooo@naver.com>
2022-10-28 22:51:48 +0900

    feat: problem7 유저별 점수 저장하는 변수 생성, 방문한 사람들 1점씩 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,12 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let userScoreList = {};
+
+  visitors.forEach((visitor) => {
+    if (userScoreList[visitor] === undefined) userScoreList[visitor] = 0;
+    userScoreList[visitor] += 1;
+  });
+
   return answer;
 }
 
