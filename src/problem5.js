8b49f393ca39314650e9b70ad6ef0b6909b56d31
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-10-28 12:33:04 +0900

    problem5 function pass

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
+  var answer = solution(money);
   return answer;
-}
-
+};
+function solution(money) {
+  let currency = [50000,10000,5000,1000,500,100,50,10,1];
+  let count = [];
+  let withdraw = 1;
+  for(let i = 0; i < currency.length; i++) {
+    if(Math.floor(money / currency[i] !== 0)) {
+      count.push(Math.floor(money / currency[i]))
+      money -= withdraw * (currency[i] * Math.floor(money / currency[i]))
+    } else {
+      count.push(0)
+    }
+  };
+  return count
+};
 module.exports = problem5;
