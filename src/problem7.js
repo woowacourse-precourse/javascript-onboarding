a6f5a9d4f706ed4940cef294bc5d9ab1853eb1e4
cys4585 <choizerowater@gmail.com>
2022-10-28 20:51:32 +0900

    feat: 문제7 userInfo 목록 만들기

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,53 @@
-function problem7(user, friends, visitors) {
-  var answer;
+function problem7(user, fren, visitors) {
+  let answer;
+
+  makeUserInfoList(user, friends, visitors);
   return answer;
 }
+
+function makeUserInfoList(user, friends, visitors) {
+  let userInfoList;
+  let friendsOfUser;
+
+  friendsOfUser = findFriendsOfUser(friends);
+  userInfoList = makeUserList(user, friends, visitors).map((user) => {
+    return { id: user, score: 0, isFriend: friendsOfUser.includes(user) };
+  });
+  console.log(userInfoList);
+  return userInfoList;
+}
+
+function findFriendsOfUser(friends) {
+  return friends
+    .filter((friend) => friend.includes(user))
+    .map(([userA, userB]) => (userA === user ? userB : userA));
+}
+
+function makeUserList(user, friends, visitors) {
+  let userSet;
+
+  userSet = new Set();
+  friends.forEach(([userA, userB]) => {
+    userSet.add(userA);
+    userSet.add(userB);
+  });
+  visitors.forEach((visitor) => userSet.add(visitor));
+  userSet.delete(user);
+  return Array.from(userSet);
+}
+
+let user = "mrko";
+let friends = [
+  ["donut", "andole"],
+  ["donut", "jun"],
+  ["donut", "mrko"],
+  ["shakevan", "andole"],
+  ["shakevan", "jun"],
+  ["shakevan", "mrko"],
+];
+let visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
+
+problem7(user, friends, visitors);
 
 module.exports = problem7;
 
@@ -14,4 +60,19 @@ shakevan: (andole, mrko)
 
 andole: (donut, shakevan)
 jun: (donut)
+*/
+
+/*
+friends
+[
+  ["donut", "andole"],
+  ["donut", "jun"],
+  ["donut", "mrko"],
+  ["shakevan", "andole"],
+  ["shakevan", "jun"],
+  ["shakevan", "mrko"],
+]
+
+visitors
+["bedi", "bedi", "donut", "bedi", "shakevan"];
 */
