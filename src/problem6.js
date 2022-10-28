d122e11e570fd998f283601b87b19198d0295a37
easyone <thinkanddoit@naver.com>
2022-10-28 18:39:35 +0900

    #6 feat: 문제6 닉네임에서 나오는 모든 2글자 경우의 수를 Map에 저장하는 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,12 @@
 function problem6(forms) {
+  const nickCaseMap = new Map();
+  const getAllCase = (name) => {
+    for (let i = 0; i < name.length - 1; i++) {
+      const key = name.slice(i, i + 2);
+      if (nickCaseMap.get(key)) nickCaseMap.set(key, nickCaseMap.get(key) + 1);
+      else nickCaseMap.set(key, 1);
+    }
+  };
   var answer;
   return answer;
 }
