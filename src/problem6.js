da89882606d95e5049fed3f96f0bc6f71419664e
coolIlive <galilee155@naver.com>
2022-10-29 00:32:06 +0900

    Refactor(problem6): 공통 문자열의 자리가 일치하는지 확인하는 기능을 분리한다
    isPositionRight 함수를로 분리함. true인 경우에는 answer에 값을 추가하도록 함.

--- src/problem6.js
+++ src/problem6.js
@@ -8,13 +8,8 @@ function problem6(forms) {
       let sameWords=sameWordCheck(forms[i][1],forms[j][1])
 
       // 공통부분의 순서가 일치하는지 확인하는 기능
-      for (let c=0;c<sameWords.length-1;c++) {
-        // console.log(forms[j][1].includes(sameWords.substring(c, c+2)),forms[j][1])
-        if (forms[j][1].includes(sameWords.substring(c, c+2))) {
-          answer.add(forms[i][0])
-          answer.add(forms[j][0])
-          break
-        }
+      if (isPositionRight(sameWords,forms[j][1])) {
+        answer.add(forms[i][0]); answer.add(forms[j][0]);
       }
     }
   }
@@ -30,6 +25,13 @@ function sameWordCheck(firstName,secondName) {
   return [...firstName].filter(word => secondName.includes(word)).join('');
 };
 
+// 공통부분의 순서가 일치하는지 확인하는 기능
+function isPositionRight(sameWords,compareName) {
+  for (let index=0;index<sameWords.length-1;index++) {
+    if (compareName.includes(sameWords.substring(index, index+2))) return true;
+  }
+  return false
+}
 
-console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
-// module.exports = problem6;
+// console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
+module.exports = problem6;
