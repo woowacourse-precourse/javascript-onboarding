4b189609899048cddca297cc7faaff4f745da76f
SeongHo-C <leeseong010@naver.com>
2022-10-28 22:26:41 +0900

    feat(problem1): 유효페이지 검증 기능

--- src/problem1.js
+++ src/problem1.js
@@ -4,10 +4,15 @@
 
 // 연속페이지 검증 기능
 
-// 유효페이지 검증 기능
+function isValidPage(page) {
+  if (page[0] === 1 || page[1] === 400) return false;
+  else return true;
+}
 
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  if (!isValidPage(pobi) || !isValidPage(crong)) return -1;
 
   return answer;
 }
