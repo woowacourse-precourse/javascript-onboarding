343667de6912e8e3a95144e3116bcd496879eee3
Woong <jiyko7@kyonggi.ac.kr>
2022-10-28 20:00:12 +0900

    6번 문제 해결
    6번 문제 해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,25 @@
 function problem6(forms) {
   var answer;
+  var emailSet = new Set();
+
+  for (crew of forms){
+    for (var i=0; i<crew[1].length-1; i++){
+      var check = crew[1].substr(i,2);
+      for (c of forms){
+        if (c[1] == crew[1]){
+          continue;
+        }
+        else if (c[1].includes(check)){
+          emailSet.add(c[0]);
+        }
+        else{
+        }
+      }
+    }
+  }
+  answer = Array.from(emailSet);
+  answer.sort();
+
   return answer;
 }
 
