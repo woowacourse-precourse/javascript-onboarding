1a95211e8f76f6643798bd0b945d8553835fcfc4
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-28 10:41:24 +0900

    ✨ 반복되는 문자열을 빈 문자열로 바꾸는 과정

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,8 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram;
+  // 1) 반복되는 문자열을 빈 문자열로 바꾸는 과정
+  const REPEAT = /([a-z])\1+/g;
+  answer = answer.replace(REPEAT, "");
   return answer;
 }
 
