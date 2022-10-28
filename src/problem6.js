c462ff9e869c8fd07757798e6b7a2e6d07bfa738
yong <tkddbs1644@gmail.com>
2022-10-28 20:06:19 +0900

    문제6: 중복된 닉네임을 사용하는 유저를 반환한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,7 @@
 /*
 [x] 단어를 포개서 자르는 기능을 구현한다.
 [x] 중복된 닉네임을 사용하는 유저를 찾는 기능을 구현한다.
-[ ] 중복된 닉네임을 사용하는 유저들을 반환한다.
+[x] 중복된 닉네임을 사용하는 유저들을 반환한다.
 */
 
 function overlapWord(word) {
@@ -28,8 +28,17 @@ function makeDuplicatedUserTable(forms) {
 }
 
 function problem6(forms) {
-  var answer;
-  return answer;
+  const userTable = makeDuplicatedUserTable(forms);
+  const duplicatedUserEmails = new Set();
+  for (const emailSets of Object.values(userTable)) {
+    const emails = [...emailSets];
+    if (emails.length > 1) {
+      for (const email of emails) {
+        duplicatedUserEmails.add(email);
+      }
+    }
+  }
+  return [...duplicatedUserEmails].sort();
 }
 
 module.exports = problem6;
