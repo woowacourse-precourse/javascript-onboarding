9fedcd1a182aec2e6d59fb3c83c889cbe0879c75
KirschX <mk44879@gmail.com>
2022-10-28 18:33:02 +0900

    과제6 기능구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  let map = new Map();
+
+  forms.map(([email, nickname]) => {
+    for (let i = 1; i < nickname.length; i++) {
+      let cur = nickname[i - 1] + nickname[i];
+      map.set(cur, [...(map.get(cur) || []), email]);
+    }
+  });
+
+  for (item of map.values()) {
+    if (item.length > 1) {
+      answer = [...answer, ...item];
+    }
+  }
+
+  return answer.sort();
 }
 
 module.exports = problem6;
