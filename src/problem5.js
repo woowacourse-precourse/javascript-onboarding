f84f47a49b8f0ca6aa726c62e263d594263a7701
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 14:57:04 +0900

    feat: inputCheck 함수 추가
    1보다 작거나 1,000,000보다 클 경우 에러메시지 반환

--- src/problem5.js
+++ src/problem5.js
@@ -2,7 +2,20 @@
 // 1. 숫자 크기와 화폐 단위와 비교 함수 추가
 // 2. 단위에 맞게 반환값에 추가후 반환하는 함수 추가
 
+function validInputCheck(money) {
+  if (money.length < 1 || money.length > 1000000) {
+    return -1;
+  }
+  if (!Number.isInteger(money)) {
+    return -1;
+  }
+  return 0;
+}
+
 function problem5(money) {
+  if (validInputCheck(money) < 0) {
+    return "Argument Error";
+  }
   var answer;
   return answer;
 }
