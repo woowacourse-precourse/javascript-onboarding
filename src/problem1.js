98ba6c385bb43dcc21c896c6c48b0f276af6ff10
pp449 <mma7710@naver.com>
2022-10-28 18:17:59 +0900

    [문제 1] : 예외처리 기능추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,21 @@
+const checkErr = (pobi, crong) => {
+  let findErr = 0;
+  if(pobi.length !=2 || crong.length !=2)
+    findErr = -1;
+  if(pobi[0]<0 || pobi[0]>399 || crong[0]<0 || crong[0]>399)
+    findErr = -1;
+  if(pobi[1]!=pobi[0]+1 || crong[1]!=crong[0]+1)
+    findErr = -1;
+  if(pobi[0]%2==0 || crong[0]%2==0)
+    findErr = -1;
+
+  return findErr;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(checkErr === -1)
+    return -1;
+  
 }
 
 module.exports = problem1;
