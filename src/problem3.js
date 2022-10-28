944f75c53076276e0631649c5b264d78bec9f3b8
Sungjae.Kwak <sungjae8970@gmail.com>
2022-10-28 10:50:26 +0900

    ✨ 차례 숫자까지 반복하면서 문자열로 바꾸는 과정

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  // 1) 차례 숫자까지 반복하면서 문자열로 바꾸는 과정
+  for (let i = 1; i <= number; i++) {
+    let temp = i.toString();
+  }
   return answer;
 }
 
