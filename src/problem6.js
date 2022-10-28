7ec654bb760e898f37f0c6f5d9df0d317e50beb0
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-28 14:38:52 +0900

    feat: 각 닉네임의 부분 문자열을 포함하는 2차원 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -2,6 +2,17 @@ function problem6(forms) {
 	const emails = [...new Map(forms).keys()]
 	const nickNames = [...new Map(forms).values()]
 	const duplicateCheckArr = new Array(forms.length).fill(false)
+
+	const substrings = nickNames.map(nickName => {
+		let substrings = []
+		for (let i = 1; i < nickName.length; i++) {
+			const substring = nickName[i - 1] + nickName[i]
+			substrings.push(substring)
+		}
+		substrings = [...new Set(substrings)]
+
+		return substrings
+	})
 }
 
 module.exports = problem6
