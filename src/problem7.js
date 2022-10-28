daf69885188bcb51fcdd35e139855a1e7ebbb182
hwangsungjae <edx2fire@likelion.org>
2022-10-28 20:12:14 +0900

    [온보딩 - 07] 친구 추천 문제 솔루션 함수 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,24 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let answer = [];
+  const currFriends = friends.filter(el => el.includes(user)).flat().filter(name => name !== user);
+  answer = Array.from(new Set([...friends, ...visitors].flat())).filter(name => !currFriends.includes(name) && name !== user);
+  const friendPoint = {};
+  let friendEntry = [];
+
+  currFriends.forEach(friend => friendEntry = [...friendEntry, ...friends.filter(entry => !entry.includes(user) && entry.includes(friend))]);
+  
+  answer.forEach(name => {
+    friendPoint[name] = friendEntry.filter(friend => friend.includes(name)).length * 10;
+    friendPoint[name] = (friendPoint[name] || 0) + visitors.filter(visitor => visitor === name).length;
+  });
+  
+  answer = answer.filter(name => friendPoint[name] !== 0);
+  
+  return answer.sort((x, y) => {
+    if (friendPoint[x] > friendPoint[y]) return -1;
+    else if (friendPoint[x] < friendPoint[y]) return 1;
+    else return x > y ? 1 : -1;
+  });
 }
 
 module.exports = problem7;
