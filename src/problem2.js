41de1650c72664fcbaca5ed9440410c5b5b40f45
osdoonhyun <skw1996@naver.com>
2022-10-28 13:11:12 +0900

    [문제2] 기능 구현 목록 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
+/* 
+1. 주어진 문자열을 문자 별로 자르고 배열에 저장
+2. 문자들이 담긴 배열을 반복문을 통해 연속된 중복 문자들을 제거
+3. 삭제한 결과를 다시 하나의 문자열로 만듦
+4. 삭제 후 합친 문자열 return
+*/
+
 function problem2(cryptogram) {
   var answer;
+
   return answer;
 }
 
