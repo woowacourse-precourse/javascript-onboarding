c6b8b7d7598494b734c9b9c2e352486bbfdf1dc1
pp449 <mma7710@naver.com>
2022-10-29 02:26:43 +0900

    [문제 5] : 1. 입력받은 돈을 금액별로 나누고 나머지를 구하는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
-function problem5(money) {
-  var answer;
-  return answer;
+const moneyChanger = (money) => {
+  let monetaryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for(let i=0; i<monetaryUnit.length; i++) {
+    let numberOfMoney = parseInt(money/monetaryUnit[i]);
+    money = parseInt(money%monetaryUnit[i]);
+    
+    console.log(numberOfMoney);
+  }
+  console.log();
 }
+
+function problem5(money) {
+  moneyChanger(money);
+}
+
+problem5(50237);
+problem5(15000);
 
 module.exports = problem5;
