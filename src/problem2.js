2e05960ce124a1946ba37a8323492e3e39b2d954
kang <mdiu2203@naver.com>
2022-10-29 02:22:29 +0900

    feat(PROBLEM2): decipher a cryptogram

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,14 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function decipher(cryptogram) {
+  const re = new RegExp(/([a-z])\1+/g);
+
+  return (function internalDecipher(cryptogram) {
+    return re.test(cryptogram)
+        ? internalDecipher(cryptogram.replace(re, ''))
+        : cryptogram;
+  })(cryptogram);
+}
+
 module.exports = problem2;
