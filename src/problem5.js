37514862b4630e6e76c6b7a6586b03d4d34d9f28
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-28 12:10:13 +0900

    problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,26 @@
 function problem5(money) {
   var answer;
+  var mapper = new Map([
+    [50000, 0],
+    [10000, 0],
+    [5000, 0],
+    [1000, 0],
+    [500, 0],
+    [100, 0],
+    [50, 0],
+    [10, 0],
+    [1, 0],
+  ]);
+
+  mapper.forEach((_, key) => {
+    var quotient = parseInt(money / key);
+    if (quotient > 0) {
+      mapper.set(key, quotient);
+      money = money - key * quotient;
+    }
+  });
+
+  answer = Array.from(mapper.values());
   return answer;
 }
 
