c6a21e17dcc16b932a926c7c81bf49d2b67c7f41
lyssoi <114637463+lyssoi@users.noreply.github.com>
2022-10-28 18:15:17 +0900

    중복된 문자 삭제하기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
+
 function problem2(cryptogram) {
   var answer;
+  var cryptogramWord = cryptogram;
+  var cryptogramArray = cryptogramWord.split("");
+  answer = noCombination(cryptogramArray);
   return answer;
 }
+
+function noCombination(arr){
+  var answer = arr;
+  for(let i=0;i<arr.length;i++){
+    if(arr[i]==arr[i+1]){
+     answer.splice(i);
+    }
+}
+  answer = answer.join("");
+  return answer;
+
+}
+
 
 module.exports = problem2;
