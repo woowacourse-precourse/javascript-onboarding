e8e14feab4cb4d21d9d60146bd1c1afea9aa0479
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-28 23:35:08 +0900

    [problem2] Add removeDuplicatesChar function for remove duplicate letters

--- src/problem2.js
+++ src/problem2.js
@@ -3,7 +3,24 @@ function problem2(cryptogram) {
 }
 
 function cryptogramSolver(cryptogram) {
-  let cryptogramArr = [...cryptogram];
+  return cryptogram;
+}
+
+function removeDuplicatesChar(cryptogramArr) {
+  let progressChar;
+  for (let i = cryptogramArr.length - 1; i >= 0; i--) {
+    if (cryptogramArr[i] == cryptogramArr[i + 1]) {
+      progressChar = cryptogramArr[i];
+      cryptogramArr[i] = "";
+      cryptogramArr[i + 1] = "";
+    } else if (progressChar == cryptogramArr[i]) {
+      progressChar = cryptogramArr[i];
+      cryptogramArr[i] = "";
+    } else {
+      progressChar = "";
+    }
+  }
+  return cryptogramArr;
 }
 
 module.exports = problem2;
