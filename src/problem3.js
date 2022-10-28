d143b67e32ec4c85afd8ad6549825b6350bd1988
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-29 03:33:08 +0900

    [problem3] Add getCount369 function for count char '3','6','9'

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,21 @@
-console.log(problem3(13));
+console.log(problem3(33));
 
 function problem3(number) {
   return game369App(number);
 }
 
-function game369App(number){
+function game369App(number) {
   const eachNumbersArr = createNumberArr(number);
   return getCount369(eachNumbersArr);
 }
 
-function createNumberArr(number){
-  return Array.from({length: number}, (_,idx)=>idx+1);
+function createNumberArr(number) {
+  return Array.from({ length: number }, (_, idx) => idx + 1);
+}
+
+function getCount369(numberArr) {
+  let numberChaining = numberArr.join("");
+  return numberChaining.split("").filter((number) => number == 3 || number == 6 || number == 9).length;
 }
 
 module.exports = problem3;
