f852af28af43a00104d140456efbb9bee43789d8
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-28 13:22:31 +0900

    feat: problem6.js에 중복된 글자를 가진 크루의 닉네임을 구하는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,21 @@
+const getInvalidNames = (names) => {
+  const invalidNames = [];
+  const invalidNameCases = [];
+
+  names.forEach((name, index) => {
+    for (let i = 0; i < name.length - 1; i++) {
+      const invalidNameCase = name.slice(i, i + 2);
+      if (!invalidNames.includes(invalidNameCase) && invalidNameCases.includes(invalidNameCase)) {
+        invalidNames.push(invalidNameCase);
+      } else {
+        invalidNameCases.push(invalidNameCase);
+      }
+    }
+  });
+
+  return invalidNames;
+};
+
 function problem6(forms) {
   var answer;
   return answer;
