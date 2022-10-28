78ef243fb3189a150f4da64dc9c2fbeb915fa3de
JongHun <minn4072@gmail.com>
2022-10-28 12:36:47 +0900

    우테코 프리코스 문제3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,12 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  let reg = [];
+
+  for (let i = 1; i <= number; i++) {
+    reg = String(i).match(/[369]/g);
+    answer += reg !== null ? reg.length : 0;
+  }
+
   return answer;
 }
 
