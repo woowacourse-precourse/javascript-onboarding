34d2fac4e768f7d14d96811d374f0d6cd81e3c7a
woo-jk <201702042@o.cnu.ac.kr>
2022-10-28 21:54:39 +0900

    feat:PROBLEM2 중복 문자열 자르는 함수 제작

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,11 @@
+const trimDuplicateString = (string) => {
+  const trimedString = string.replace(/([a-zA-Z])\1+/g, "");
+  return trimedString;
+};
+
 function problem2(cryptogram) {
   var answer;
+  answer = trimDuplicateString(cryptogram);
   return answer;
 }
 
