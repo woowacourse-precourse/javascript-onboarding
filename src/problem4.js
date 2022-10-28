541de95b703f8da44c418785d02b9ff7b1f3e075
김희진 <choha941108@gmail.com>
2022-10-28 22:10:07 +0900

    (4번) 아스키/반대알파벳 변환 성공

--- src/problem4.js
+++ src/problem4.js
@@ -1,39 +1,50 @@
 function problem4(word) {
   var answer;
+  
 
   console.log("워드입력",word)
 
-  
-  //워드 단어 변수화
-  let dictionary_big = {}
-  let dictionary_small = {}
+  let dictionary = {}
 
   
   for ( i = 65 ; i < 91 ; i ++ ) {
-    dictionary_big[i] = 90 + 65 - i 
+    dictionary[i] = 90 + 65 - i 
   }
   //대문자 변환 테이블 생성
-  console.log(dictionary_big)
   
   for ( j = 97 ; j < 123 ; j ++ ) {
-    dictionary_small[j] = 122 + 97 - j 
+    dictionary[j] = 122 + 97 - j 
   }
-  console.log(dictionary_small)
+  console.log("테이블 확인",dictionary)
   //소문자 변환 테이블 생성
 
-  for (i = 0 ; i <= word.length  ; i++) { 
-    let word = word.charCodeAt(i)
+  let new_word =[]
+  for ( k = 0 ; k < word.length ; k ++) {
+    new_word.push(word.charCodeAt(k))
+    
   }
-  console.log("아스키 워드 테스트",word)
+  console.log("아스키 변환확인",new_word) 
   //입력 단어 아스키 코드 변환
 
-  //변환 단어 알파벳으로 변환
 
-  for (i = 0 ; i <= word.length  ; i++) { 
-    let word = word.charCodeAt(i)
+
+
+  let last_word = []
+  for ( l = 0 ; l < new_word.length ; l ++) {
+    let chr = new_word[l]
+    if ( chr in  dictionary){
+      last_word.push(String.fromCharCode(dictionary[chr]))
+    }
+    else{
+      last_word.push(String.fromCharCode(chr))
+    }
   }
-  console.log(word)
-  return answer;
+  console.log("알파벳 변환확인",last_word) 
+  //변환 단어 알파벳으로 변환
+  
+  answer = last_word.join("")
+
+  return answer
 }
 
 module.exports = problem4;
