ce8126f162ea4cb327cd82f240ebf8f450cd4186
BaeDongWoo <ehddn5410@naver.com>
2022-10-28 22:21:52 +0900

    받은 숫자에 3,6,9가 들어가는지 10으로 나누어 확인

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,25 @@
 function problem3(number) {
-  var answer;
+  var answer=0;
+  for(let i=1;i<=number;i++){
+    let num=i;//1부터 number입력
+    while(num!=0){//박수칠 횟수
+      if(num%10==3){//나머지가 3일때 
+        answer=answer+1;
+        num=parseInt(num/10);
+      }
+      else if(num%10==6){//나머지가 6일때
+        answer=answer+1;
+        num=parseInt(num/10);
+      }
+      else if(num%10==9){//나머지가 9일때
+        answer=answer+1;
+        num=parseInt(num/10);
+      }
+      else{
+        num=parseInt(num/10);
+      }
+    }
+  }
   return answer;
 }
 
