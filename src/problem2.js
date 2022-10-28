90a9bd5d2ade6856661188f27b4643bc6c1310dd
coolIlive <galilee155@naver.com>
2022-10-29 00:29:16 +0900

    Style(problem2): 주석과 필요없는 줄을 삭제하다
    불필요한 주석과 줄을 삭제했다. 그리고 코드 마지막에 ';'를 썼는지 확인했다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,9 @@
 function problem2(cryptogram) {
-  // 스택으로 풀기
   let stack=[];
-
   for (let word of cryptogram) {
     if (stack[stack.length-1] != word) stack.push(word);
     else stack.pop();
   };
-
   return stack.join('');
 }
 
