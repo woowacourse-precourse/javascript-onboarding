3b5ade7a25951afe69a89dc350047a61fc155629
yeongi <cbg5255@gmail.com>
2022-10-28 19:11:56 +0900

    문제 7. 기능 5: 점수계산 추가 및 구현 , 테스트 통과

--- src/problem7.js
+++ src/problem7.js
@@ -1,16 +1,13 @@
 function problem7(user, friends, visitors) {
-  const recommandFreind = {};
-
   const [myFriends, friendFriends] = filteredFreinds(user, friends);
-  console.log(friendFriends);
 
   const friendObj = friendsRecomandList(user, friendFriends, friends);
 
   const visitObj = visiterRecomandList(visitors);
 
-  scoreSortRecomandFreinds(friendObj, visitObj, myFriends);
+  const result = scoreSortRecomandFreinds(friendObj, visitObj, myFriends);
 
-  return friendObj;
+  return resultSorting(result);
 }
 
 // 1. USER 의 친구를 거르는 기능
@@ -72,20 +69,27 @@ function scoreSortRecomandFreinds(friends, visiters, myF) {
       result[visiter] = visiters[visiter] * 1;
     }
   }
-  console.log(result);
+  return result;
 }
 
-problem7(
-  "mrko",
-  [
-    ["donut", "andole"],
-    ["donut", "jun"],
-    ["donut", "mrko"],
-    ["shakevan", "andole"],
-    ["shakevan", "jun"],
-    ["shakevan", "mrko"],
-  ],
-  ["bedi", "bedi", "donut", "bedi", "shakevan"]
-);
+//기능 5.결과 객체를 점수 순서로 정렬 후 5명 까지만 만들기
+function resultSorting(result) {
+  const test = Object.entries(result).sort(([a, b], [c, d]) => {
+    if (b === d) {
+      if (a > c) return 1;
+      else if (a < c) return -1;
+      else return 0;
+    } else {
+      return d - b;
+    }
+  });
+  const list = [];
+
+  for (let i = 0; i < (test.length <= 5 ? test.length : 5); i++) {
+    list.push(test[i][0]);
+  }
+
+  return list;
+}
 
 module.exports = problem7;
