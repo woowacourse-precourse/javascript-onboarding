cdfc60e49cdd99df945beb517fb6f4c93fbfa041
zktm9903 <zktm9903@naver.com>
2022-10-29 01:37:14 +0900

    problem3 solve

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+
+  for(let i=1;i<=number;i++){
+
+    //문자열로 변환후 각 자리 수가 3, 6, 9인지 검사 
+    (i+'').split('').map(digit=>{
+      if(digit === '3' || digit === '6' || digit === '9')
+        count++;
+    })
+  }
+
+  return count;
 }
 
 module.exports = problem3;
