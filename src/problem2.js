4390f3d16b4a5c77817764f390df8667ff8ab0c6
Tory <88531407+1two13@users.noreply.github.com>
2022-10-28 15:25:33 +0900

    refactor(problem2): top 변수 제거

--- src/problem2.js
+++ src/problem2.js
@@ -6,9 +6,7 @@ function problem2(cryptogram) {
   let answer = cryptogram[0];
 
   for (let i = 1; i < cryptogram.length; i++) {
-    let top = answer[answer.length - 1];
-
-    if (top !== cryptogram[i]) answer += cryptogram[i];
+    if (answer[answer.length - 1] !== cryptogram[i]) answer += cryptogram[i];
     else answer = answer.slice(0, -1);
   }
 
