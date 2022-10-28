cac528ec052ce54050f34aca15341d546a8cb229
sujungYu <su_moment@naver.com>
2022-10-28 17:21:37 +0900

    feat(PROBLEM4):알파벳 배열 생성 및 입력 받은 문자 분리하여 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+    const newWord = [];
+    const alphabetArr = [
+        'A', 'B', 'C', 'D', 'E', 'F',
+        'G', 'H', 'I', 'J', 'K', 'L',
+        'M', 'N', 'O', 'P', 'Q', 'R',
+        'S', 'T', 'U', 'V', 'W', 'X',
+        'Y', 'Z'
+    ];
+    const wordArr = word.split('');
 }
 
 module.exports = problem4;
