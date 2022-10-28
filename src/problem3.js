bd3782e400edfee2784a6477ce0249afa7d316c4
osdoonhyun <skw1996@naver.com>
2022-10-28 17:15:06 +0900

    [문제3] 기능 구현 목록 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,11 @@
 /*
-1. number까지 for문을 돌며 각 자리 숫자에 3,6,9를 돈다.
-2. 1번의 요구 사항이 true이면 result에 1씩 더해준다.
+1. number까지 for문을 돌며 각 자리수를 문자열로 나눠 배열에 저장하는 기능
+2. 배열을 ['3','6','9']에 해당하는지 체크하는 기능
 */
 
 function problem3(number) {
   var answer;
+
   return answer;
 }
 
