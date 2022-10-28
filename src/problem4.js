cc0090edd307c495536c32a3fecb19fd00542294
김아현 <dhflxhdxhd@gmail.com>
2022-10-29 00:50:08 +0900

    문제 4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,27 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  var upperDict = {};
+  var lowerDict = {};
+
+  // 청개구리사전
+  for (let i = 97; i<123;  i++){
+      upperDict[String.fromCharCode(i)] = String.fromCharCode(219 - i);
+      lowerDict[String.fromCharCode(i-32)] = String.fromCharCode(219-i-32);
+  }
+  console.log(upperDict["A"])
+
+  for (let j = 0; j < word.length; j++) {
+    if(upperDict[word[j]]){
+        answer += upperDict[word[j]] 
+    }else if(lowerDict[word[j]]){
+        answer += lowerDict[word[j]]
+    }else{
+        answer += word[j]
+    }
+
+  
+  }
+
   return answer;
 }
 
