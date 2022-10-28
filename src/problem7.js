786d7f5ed41b61618ff05b3621b19662d96fc0b0
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-28 23:02:52 +0900

    feat: 각 사용자의 친구 관계를 Map에 정리하는 로직 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,19 @@
 function problem7(user, friends, visitors) {
 	const friendLists = new Map()
+
+	friends.map(([userA, userB]) => {
+		if (friendLists.has(userA)) {
+			friendLists.get(userA).push(userB)
+		} else {
+			friendLists.set(userA, [userB])
+		}
+
+		if (friendLists.has(userB)) {
+			friendLists.get(userB).push(userA)
+		} else {
+			friendLists.set(userB, [userA])
+		}
+	})
 }
 
 module.exports = problem7
