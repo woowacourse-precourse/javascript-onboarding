3544439e863c22ef18d40e4b9d0f6e8f984dc3e5
JSH99 <tjgus9966@naver.com>
2022-10-28 19:51:05 +0900

    feat: 청개구리 사전 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,20 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+
+  for(var i = 0; i < word.length; i++) {
+    const char = word.charCodeAt(i);
+    var num;
+    
+    if ( char >= 65 && char <= 90 ) { // 대문자
+      num = word.charCodeAt(i) - 65;
+      answer += String.fromCharCode(90 - num);
+    } else if ( char >= 97 && char <= 122 ) { // 소문자
+      num = word.charCodeAt(i) - 97;
+      answer += String.fromCharCode(122 - num);
+    } else { // 공백 및 다른 문자
+      answer += word[i];
+    } 
+  }
   return answer;
 }
 
