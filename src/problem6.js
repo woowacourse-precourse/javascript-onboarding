72c1dc851f3701b538634edf2dfd5d94c2411fc1
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-28 19:16:02 +0900

    문제6. 각 이름에서 비교할 두글자 이름 추출

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,11 @@
 function problem6(forms) {
   var answer = [];
   forms.forEach((e) => (e[2] = 0));
+  for (let i = 0; i < forms.length; i++) {
+    for (let l = 0; l < forms[i][1].length - 1; l++) {
+      let word = forms[i][1][l] + forms[i][1][l + 1];
+    }
+  }
   return answer;
 }
 module.exports = problem6;
