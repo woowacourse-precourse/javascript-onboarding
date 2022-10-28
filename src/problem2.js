5c6e456024b1a318c528d4b03694043e4f768983
Tory <88531407+1two13@users.noreply.github.com>
2022-10-28 15:23:05 +0900

    refactor(problem2): 기능 목록 주석 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,6 @@
 // 기능 목록
-// 1. 연속하는 중복 문자열 모두 제거
+// 1. answer 마지막 글자랑 cryptogram의 n번째 글자랑 비교했을 때 다르면 answer에 cryptogram의 n번째 글자 추가하기
+// 2. 같으면 answer의 마지막 글자 제거하기
 
 function problem2(cryptogram) {
   let answer = cryptogram[0];
