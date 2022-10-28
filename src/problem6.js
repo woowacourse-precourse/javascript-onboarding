d07889653478dcda1565c86d566ee7ae8c6b87a5
leeseooo <leeseoooo@naver.com>
2022-10-28 22:44:49 +0900

    feat: problem6 forms 순회하면서 split된 문자를 포함하는 크루원 이메일 모두 저장

--- src/problem6.js
+++ src/problem6.js
@@ -12,8 +12,21 @@ function problem6(forms) {
       }
     }
     nameList.push([form, [...splitedNameSet]]);
-    console.log(splitedNameSet);
   });
+
+  for (let i = 0; i < nameList.length; i++) {
+    const [aEmail] = nameList[i][0];
+    const a = nameList[i][1];
+    for (let j = i + 1; j < nameList.length; j++) {
+      const [bEmail] = nameList[j][0];
+      const b = nameList[j][1];
+      a.forEach((x) => {
+        if (b.includes(x)) {
+          answer.push(aEmail, bEmail);
+        }
+      });
+    }
+  }
 
   return answer;
 }
