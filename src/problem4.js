c3e80480f4887cde2954067016e7c75016637e4f
feb-dain <feb.dain@gmail.com>
2022-10-28 16:33:36 +0900

    feat: [problem4] sort array in descending order

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,8 @@ function problem4(word) {
   const lowerCase = Array.from({ length: 26 }, (VOID, index) => String.fromCharCode(index + 97));
   const upperCase = Array.from({ length: 26 }, (VOID, index) => String.fromCharCode(index + 65));
   const alphabet = lowerCase.concat(upperCase);
+
+  const oppositeDictionary = alphabet.reverse(); // z = oppositeDictionary[26]
   
   return answer;
 }
