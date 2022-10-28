fef9ea0692d13ff953ee362e23b7297c8339c91c
yeonsuBeak <soothbugger@gmail.com>
2022-10-28 16:49:37 +0900

    feat(problem5): 화폐 단위와 그의 개수에 대한 배열 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const BILL_UNIT_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const listLength = BILL_UNIT_LIST.length;
+  let billCountList = new Array(listLength).fill(0);
 }
 
 module.exports = problem5;
