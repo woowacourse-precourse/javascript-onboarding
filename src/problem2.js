a98c1c6bbba3b3e158946042889cf083d0fdae1d
SNUPI <joohaem@daum.net>
2022-10-28 19:22:06 +0900

    [ feat ] complete problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  answer = decode(cryptogram);
   return answer;
+}
+
+function decode(cryptogram) {
+  const cryptoStack = [];
+
+  [...cryptogram].forEach((char) => {
+    switch (cryptoStack[cryptoStack.length - 1] === char) {
+      case true:
+        cryptoStack.pop();
+        break;
+      case false:
+        cryptoStack.push(char);
+        break;
+    }
+  });
+
+  return cryptoStack.join("");
 }
 
 module.exports = problem2;
