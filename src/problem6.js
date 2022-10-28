07a9d664a2bf157ba57cc14027d060010b680258
MAGHC <windowsapple95@gmail.com>
2022-10-28 17:49:16 +0900

    feat: problem6 중복닉네임 이메일 매치 기능구현완료

--- src/problem6.js
+++ src/problem6.js
@@ -11,11 +11,24 @@ const checkDuPlicateNickName = (nickNameArray) => {
   return Array.from(new Set([...prefixArray]));
 };
 
+const matchDuplicateNickNameToEmail = (emailArray, duplicateNickNameArray, formsobj) => {
+  let result = [];
+  for (let email of emailArray) {
+    for (let i = 0; i < duplicateNickNameArray.length; i++) {
+      if (formsobj[email] === duplicateNickNameArray[i]) result.push(email);
+    }
+  }
+  return result;
+};
+
 function problem6(forms) {
   let answer;
   const formsObj = Object.fromEntries(forms);
   const nickNameArray = Object.values(formsObj);
-  checkDuPlicateNickName(nickNameArray);
+  const emailArray = Object.keys(formsObj);
+  const duplicateNickNameArray = checkDuPlicateNickName(nickNameArray);
+
+  answer = matchDuplicateNickNameToEmail(emailArray, duplicateNickNameArray, formsObj).sort();
 
   return answer;
 }
