0dc11d664c427db67962cbb6eaa004c7c5eced2c
loopy-lim <loopy@jnu.ac.kr>
2022-10-28 22:05:53 +0900

    feat: 들어온 숫자가 1~10000이하인 자연수인지 확인한다

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,17 @@
+const game369 = {
+  endNumber: 0,
+  count: 0,
+  inputEndNumber: function (number) {
+    if (!this.checkNumber(number)) {
+      return false;
+    }
+    this.endNumber = number;
+  },
+  checkNumber: function (number) {
+    return number >= 1 && number <= 100000;
+  },
+};
+
 function problem3(number) {
   var answer;
   return answer;
