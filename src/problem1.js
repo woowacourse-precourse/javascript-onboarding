f8372c0161db23b1c5cf3b1c54e97c73f5bcceaa
rabBit64 <gojh47@gmail.com>
2022-10-28 14:19:13 +0900

    기능목록2: 포비,크롱 점수 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -5,13 +5,32 @@ function exception(pobi, crong){
   }
   return ret;
 }
-
+function score(person){
+  var ret;
+  var pL = person[0].toString();
+  var pLAdd = 0; var pLMul = 1;
+  var pR = person[1].toString();
+  var pRAdd = 0; var pRMul = 1;
+  for(var i=0; i<pL.length; i++){
+    pLAdd += Number(pL[i]);
+    pLMul *= Number(pL[i]);
+  }
+  for(var i=0; i<pR.length; i++){
+    pRAdd += Number(pR[i]);
+    pRMul *= Number(pR[i]);
+  }
+  ret= Math.max(Math.max(pLAdd,pLMul),Math.max(pRAdd,pRMul));
+  return ret;
+}
 function problem1(pobi, crong) {
   var answer;
   //기능목록1: 예외처리
   if(exception(pobi, crong)){
     answer = -1;
   }else{
+  //기능목록2: 포비, 크롱 점수 구하기
+  var pobiScore = score(pobi);
+  var crongScore = score(crong);
 
   }
   
