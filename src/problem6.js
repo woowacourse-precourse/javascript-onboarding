b449ecf049615286d6d16fbaba65929d8278af86
ShimSungBo <shimdb96@gmail.com>
2022-10-28 23:03:07 +0900

    Fix: [problem6] 기능 구현 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,24 @@
 function problem6(forms) {
-  let a = forms.filter(v => v[1].includes('제이')).sort();
-  let b = a.reduce((a,c) => a.concat(c));
-  return b.filter(v => !v.includes('제이'));
+
+  const map = new Map();
+  for(let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      const consistentWord = forms[i][1].substring(j, j+2);
+      if(map.has(consistentWord)) {
+        map.set(consistentWord, map.get(consistentWord) + 1);
+      } else {
+        map.set(consistentWord, 1);
+      }
+    }
+  }
+  const consistentWord = [...map].filter(name => name[1] > 1).reduce((a,c) => a.concat(c));
+  let answer = [];
+  for(const nickName of forms) {
+    if(nickName[1].includes(consistentWord[0])) {
+      answer.push(nickName[0]);
+    }
+  }
+  return answer.sort();
 }
 
 module.exports = problem6;
