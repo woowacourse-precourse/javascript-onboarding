57da54200ea0c52f0d6331ce1354924882715043
park junwoo <always1997@kakao.com>
2022-10-28 17:27:20 +0900

    problem6 기능목록 2-1 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,13 @@
+/*
+  기능목록
+  1.닉네임만 따로 저장하는 배열 구현
+  2.연속적으로 포함된 닉네임 판별 및 인덱스 리턴
+  2-1. 중복되는 인덱스는 받지않도록 set자료구조 활용
+  3. 해당 인덱스의 이메일만 따로 저장하는 배열 구현
+  4. 이메일 배열 정렬 후 result 출력 
+*/
 function problem6(forms) {
-  var answer;
+  let answer;
   return answer;
 }
 
