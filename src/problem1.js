e57e2ecae75f51487b4a5b9ca99ecb9f89d071a6
coolIlive <galilee155@naver.com>
2022-10-29 00:28:51 +0900

    Style(problem1): 불필요한 줄을 삭제하고 세미콜론을 통일하다
    주석과 불필요한 줄을 삭제. 코드 마지막에 ';'붙이는 것을 통일시킴

--- src/problem1.js
+++ src/problem1.js
@@ -1,27 +1,19 @@
 function problem1(pobi, crong) {
-  // 잘못된 페이지인 경우 -1리턴
-  if (!isRightPage(pobi) || !isRightPage(crong)) {
-    return -1
-  }
-
-  // 각자의 점수 계산 함수
-  let pobiScore=scoreCount(pobi)
-  let crongScore=scoreCount(crong)
-
-  // pobiScore와 crongScore를 비교하여 answer에 값을 넣고 리턴하기
-  let answer = (pobiScore>crongScore) ? 1 : ((pobiScore<crongScore) ? 2 : 0);
-  
+  if (!isRightPage(pobi) || !isRightPage(crong)) return -1;
+  let pobiScore=scoreCount(pobi);
+  let crongScore=scoreCount(crong);
+  let answer=(pobiScore>crongScore) ? 1 : ((pobiScore<crongScore) ? 2 : 0);
   return answer;
-}
+};
 
 function isRightPage(pages) {
   if (pages[1]-pages[0]!==1) return 0;
   if (pages[0]===1 || pages[1]===400) return 0;
-  return 1
-}
+  return 1;
+};
 
 function scoreCount(pages) {
-  let finalScore=0
+  let finalScore=0;
   pages.forEach((page)=> {
     let addScore=0, mulScore=1;
     do {
@@ -31,7 +23,7 @@ function scoreCount(pages) {
     } while(page>0);
     finalScore=Math.max(addScore,mulScore,finalScore);
   });
-  return finalScore
+  return finalScore;
 }
 
 module.exports = problem1;
