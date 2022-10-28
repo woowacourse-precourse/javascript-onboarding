102fbe0a8f479dc511db2c31306e1f05273b319f
Inae Choi <inaechoi89@gmail.com>
2022-10-28 21:41:58 +0900

    feat(p2): add decode functon

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,18 @@
 function problem2(cryptogram) {
   var answer;
+
+  let decode = [];
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (decode[decode.length - 1] !== cryptogram[i]) {
+      decode.push(cryptogram[i]);
+    } else {
+      if (cryptogram[i + 1] !== decode[decode.length - 1]) {
+        decode.pop();
+      }
+    }
+  }
+  answer = decode.join("");
+
   return answer;
 }
 
