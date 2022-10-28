e0d450eeae7481489d628583b43019bb3a7c4597
wusi-hub <jws9595@naver.com>
2022-10-28 17:52:29 +0900

    problem3 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let result = 0;
+  let arr = Array(number).fill().map((v, i) => i + 1).join('').split('');
+  
+  for(const el of arr) {
+    if(el === '3' || el === '6'|| el === '9') result++;
+  }
+  return result;
 }
 
 module.exports = problem3;
