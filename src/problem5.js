84045b0650c664284d4afe7d8ad6c98d820f56f8
jlee0505 <hj960322@gmail.com>
2022-10-29 08:41:56 +0900

    problem5 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,20 @@
 function problem5(money) {
-  var answer;
+  const answer = [];
+
+  const won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let quo = money;
+
+  for (let i = 0; i < won.length; i++) {
+    answer.push(Math.floor(quo / won[i]));
+    quo = Math.floor(money % won[i]);
+  }
+
   return answer;
 }
 
 module.exports = problem5;
+
+//expected: [1, 0, 0, 0, 0, 2, 0, 3, 7]
+console.log(problem5(50237));
+//expected: [0, 1, 1, 0, 0, 0, 0, 0, 0];
+console.log(problem5(15000));
