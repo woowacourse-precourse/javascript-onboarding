f5582c0a5f86f1e225e89e54be64ed5dd7d42737
loopy-lim <loopy@jnu.ac.kr>
2022-10-28 23:09:43 +0900

    feat: word는 길이가 1 이상, 1000 이하인 문자열이다

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,17 @@
+const treeFrog = {
+  monSay: "",
+  chars: [],
+  inputMonSay: function (text) {
+    if (!this.checkTextLength(text)) {
+      return;
+    }
+    this.monSay = text;
+  },
+  checkTextLength: function (text) {
+    return text.length >= 1 && text.length <= 1000;
+  },
+};
+
 function problem4(word) {
   var answer;
   return answer;
