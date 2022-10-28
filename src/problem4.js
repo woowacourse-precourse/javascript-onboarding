a2c716cb0e8844bd9d6ea4369012123c5dafbe07
Lee-Young-Jae <yjlee1937@naver.com>
2022-10-28 22:48:31 +0900

    문제 4 - 기능 요구사항 정리

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,10 @@
+/** 기능 목록
+ * 1. 청개구리 사전을 만든다.
+ * 2. 입력된 단어를 알파벳 별로 청개구리 사전에서 찾아 result에 추가한다.
+ * 3. 청개구리 사전에 없는 알파벳은 그대로 result에 추가한다. (ex. 공백)
+ * 4. 완성된 result를 반환한다.
+ */
+
 function problem4(word) {
   var answer;
   return answer;
