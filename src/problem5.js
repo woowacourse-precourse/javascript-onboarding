b13a0741be45708ab2c0d1cb9360d3d4acaf6e85
inthhh <glass005533@gmail.com>
2022-10-28 14:14:12 +0900

    p5_ResultIntoList

--- src/problem5.js
+++ src/problem5.js
@@ -5,35 +5,34 @@ function problem5(money) {
   while(money>0){
     if(money >= 50000){
       money -= 50000;
-      
+      coins[0]++;
     }else if(money >= 10000){
       money -= 10000;
-
+      coins[1]++;
     }else if(money >= 5000){
       money -= 5000;
-
+      coins[2]++;
     }else if(money >= 1000){
       money -= 1000;
-
+      coins[3]++;
     }else if(money >= 500){
       money -= 500;
-
+      coins[4]++;
     }else if(money >= 100){
       money -= 100;
-
+      coins[5]++;
     }else if(money >= 50){
       money -= 50;
-
+      coins[6]++;
     }else if(money >= 10){
       money -= 10;
-
+      coins[7]++;
     }else{
       money --;
-
+      coins[8]++;
     }
-
   }
-
+  answer = coins;
   return answer;
 }
 
