0a77a9993da4833f9a0e6097af8a88c1315792da
jsh3418 <albert5428@gmail.com>
2022-10-28 23:53:44 +0900

    chore: problem2 요구사항 정리

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,7 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 연속하는 중복 문자들을 삭제한 결과를 반환하는 로직을 만든다.
+  // 반환한 결과의 문자가 연속하는 중복 문자가 없도록 반복한다.
+  // 더 이상 중복 문자가 없다면 결과를 반환한다.
 }
 
 module.exports = problem2;
