e8123194a77c563423373b7d98dda419e74686a9
dakyungoh <dakyung.work@gmail.com>
2022-10-29 05:52:47 +0900

    test: problem4 테스트케이스 추가

--- __tests__/problem4.test.js
+++ __tests__/problem4.test.js
@@ -1,7 +1,10 @@
-const problem4 = require("../src/problem3");
+const problem4 = require("../src/problem4");
 
 describe("problem4: ", () => {
-  test("case1", () => {
+  test("case1: 우테코 예제 테스트 케이스", () => {
     expect(problem4("I love you")).toEqual("R olev blf");
+  });
+  test("case1: 중간에 영어 말고 다른걸 넣어보자.", () => {
+    expect(problem4("I, 정말로 love ! you~.")).toEqual("R, 정말로 olev ! blf~.");
   });
 });
