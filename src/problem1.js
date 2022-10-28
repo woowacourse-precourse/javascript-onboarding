7554a428ac9d4158b83a56dd245b7cd3a54c893c
GC-Park <kuyt3122@naver.com>
2022-10-28 14:14:32 +0900

    feat:값 비교를 위한 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,17 @@
+function compareValue(v1, v2){
+  if(v1>v2) return v1;
+  else if(v2>v1) return v2;
+  else return v1;
+}
+
+function getValue(a){
+  
+}
+
 function problem1(pobi, crong) {
   var answer;
+  let pobiValue=getValue(pobi);
+  let crongValue=getValue(crong);
   return answer;
 }
 
