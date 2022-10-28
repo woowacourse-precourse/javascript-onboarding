4857d8a10d2e687689cce52716e57560695d3212
RooT_Kim <bdd3452@gmail.com>
2022-10-29 03:27:42 +0900

    문제3번) countClap 함수 구현(checkNum에서 countClap으로 함수명 변경)

--- src/problem3.js
+++ src/problem3.js
@@ -1,18 +1,30 @@
 function problem3(number) {
 	//1부터 number까지 루프를 돌리는 함수 -> clap
-	//숫자에 3,6,9가 속해있는지 체크하는 함수 -> checkNum
+	//숫자에 3,6,9가 속해있는지 체크하여 박수의 횟수를 반환하는 함수 -> countClap
 	return clap(number);
 }
 
 function clap(number) {
 	let sum = 0;
+
 	for (let i = 1; i <= number; i++) {
-		if (checkNum(i)) sum++;
+		sum += countClap(i);
 	}
 
 	return sum;
 }
 
-function checkNum(number) {}
+function countClap(number) {
+	let count = 0;
+
+	number
+		.toString()
+		.split('')
+		.forEach((el) => {
+			count += [3, 6, 9].includes(Number(el)) ? 1 : 0;
+		});
+
+	return count;
+}
 
 module.exports = problem3;
