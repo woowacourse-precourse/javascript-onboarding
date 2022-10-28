9b6e86f3813f22ac41939e69c32caf86855cb9bc
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-28 18:02:42 +0900

    Feat: confirm str includes other str in problem 6

--- src/problem6.js
+++ src/problem6.js
@@ -18,7 +18,25 @@ function problem6(forms) {
     }
     return arr;
   };
-  console.log(seperate(nickname[1]));
+  let idx = 0;
+  // nickname에 있는 모든 문자열을 돌면서 문자열 분리
+  const arr = [];
+  while (idx < nickname.length) {
+    let i = idx + 1;
+    let same = 0;
+    // 분리된 문자열이 다른 문자열에 있는지
+    while (i < nickname.length) {
+      seperate(nickname[idx]).map((x) => {
+        if (nickname[i].includes(x)) {
+          arr.push(...nickname.splice(i, 1));
+          same += 1;
+          console.log(x, idx, nickname);
+        }
+      });
+      i += 1;
+    }
+    same ? nickname.splice(idx, 1) : (idx += 1);
+  }
 }
 
 module.exports = problem6;
