c7f1ce88f75d5a181b68c645ba70ed7c2fc88de4
leeuihyun <meta212121@naver.com>
2022-10-28 17:30:45 +0900

    feat : problem4 청개구리 사전에 따른 문자 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,22 @@
+function change(word) {
+  let result = "";
+  for (let i = 0; i < word.length; i++) {
+    let tmp = word.charCodeAt(i);
+    let number = 0;
+    if (tmp >= 65 && tmp <= 90) {
+      number = tmp - 65;
+      tmp = 90 - number;
+      result += String.fromCharCode(tmp);
+    } else if (tmp >= 97 && tmp <= 122) {
+      number = tmp - 97;
+      tmp = 122 - number;
+      result += String.fromCharCode(tmp);
+    } else result += word[i];
+  }
+  return result;
+}
 function problem4(word) {
-  var answer;
+  let answer = change(word);
   return answer;
 }
 
