2b4485eed7eecc9f92dc135322a265a1ddd39dd6
Shin Wonse <62709718+shinwonse@users.noreply.github.com>
2022-10-28 22:05:03 +0900

    feat: 타임라인에 방문한 횟수에 따른 점수 부여

--- src/problem7.js
+++ src/problem7.js
@@ -15,9 +15,23 @@ const getFriendsShareScore = (user, friends, friendsMap) => {
   return friendsMap;
 };
 
+const getTimelineVisitedScore = (visitors, friendsMap) => {
+  visitors.map((visitor) => {
+    if (!friendsMap.has(visitor)) {
+      friendsMap.set(visitor,1);
+    } else if (friendsMap.get(visitor) !== 0) {
+      // friendsMap이 0점이라면 이미 친구이므로 추천 x, 0점이 아니라면 추가 점수
+      friendsMap.set(visitor, friendsMap.get(visitor) + 1);
+    }
+  });
+
+  return friendsMap;
+};
+
 const problem7 = (user, friends, visitors) => {
   let friendsMap = new Map();
   friendsMap = getFriendsShareScore(user, friends, friendsMap);
+  friendsMap = getTimelineVisitedScore(visitors, friendsMap);
 };
 
 module.exports = problem7;
