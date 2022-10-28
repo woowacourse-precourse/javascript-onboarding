f6f4f0bd443a99853094e6663661114139bc36e0
Junghwan Kim <junghwki@c2r6s6.42seoul.kr>
2022-10-28 16:34:14 +0900

    docs: 기능 구현 목록 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,10 @@
+// 0. 인자의 갯수가 1이상 10000이하의 수인지 확인
+// 1. email 형식 검사 및 도메인 검사
+// 2. 닉네임이 한글인지 길이가 1이상 20이하인지 확인
+// 3. 중복 검사 함수 구현
+// 4. 중복 제거 함수 구현
+// 5. email 정렬후 result 반환
+
 function problem6(forms) {
   var answer;
   return answer;
