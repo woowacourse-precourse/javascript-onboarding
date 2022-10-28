059d13af2047268df272d4963970bac65fb9a5fe
yunho <Yunho7058@github.com>
2022-10-28 19:13:26 +0900

    problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let rep = /(\w)\1+/g;
+  if (rep.test(cryptogram)) {
+    const result = cryptogram.replace(rep, '');
+    return problem2(result);
+  } else {
+    return cryptogram;
+  }
 }
 
 module.exports = problem2;
