5f1074131b92897d4f8cab91964c894021fc5f98
sujungYu <su_moment@naver.com>
2022-10-28 17:17:30 +0900

    feat(PROBLEM3):3,6,9 갯수만큼 clapNum 증가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
+    let clapNum = 0;
     const numArr = Array(number).fill().map((v,i)=> i+1);
     const splitNumArr = numArr.join('').split('').map(Number);
+    for(let i=0; i<splitNumArr.length; i++) {
+        if(splitNumArr[i]==3|splitNumArr[i]==6|splitNumArr[i]==9) {
+            clapNum +=1;
+        }
+    }
+    return clapNum;
 }
 
 module.exports = problem3;
