701aac8fb44536224989e39320fb9009f86195c9
nuheajiohc <jaehun341012@gmail.com>
2022-10-28 16:00:44 +0900

    문제3 : 3,6,9 숫자 체크 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    let answer;
+    return answer;
 }
 
+function numCheck(num) {
+    const str = String(num).split("");
+    const threeSixNineArr = str.filter(element => element === '3' || element === '6' || element === '9');
+    return threeSixNineArr.length;
+}
 module.exports = problem3;
