cbb3a28dc60c0fbb3397d1ca20bbad82d3f5b893
oing <74910908+oing-yong@users.noreply.github.com>
2022-10-28 22:19:46 +0900

    feat: 문제4 word 내의 문자가 알파벳인지 아닌지 구분
    - test 함수과 for문을 이용하여 word[i] 문자가 알파벳인지 아닌지, 알파벳이라면 대문자인지 소문자인지 구분

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
 function problem4(word) {
-  var answer;
+  let answer = word;
+  let length = word.length;
+
+  for (let index = 0; index < length; index++) {
+    const isCapital = /^[A-Z]+$/;
+    const isSmall = /^[a-z]+$/;
+
+    if (isCapital.test(word[index])) {
+      answer[index] = frogDictionary(word[index], "capital");
+    } else if (isSmall.test(word[index])) {
+      answer[index] = frogDictionary(word[index], "small");
+    }
+  }
   return answer;
 }
+
+function frogDictionary(char, capitalOrSmall) {}
 
 module.exports = problem4;
