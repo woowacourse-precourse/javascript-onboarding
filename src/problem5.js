9491416e1288eb57fd54f7d00af0f8273a54c267
BeeMOre32 <windowssart01@gmail.com>
2022-10-28 10:30:56 +0900

    problem5 solved!!

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
   var answer;
+  answer = chargeChange(money);
   return answer;
+}
+
+function chargeChange(money) {
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  const arr = [];
+
+  unit.map((value) => {
+    arr.push(Math.floor(money / value));
+    money = money - Math.floor(money / value) * value;
+  });
+  return arr;
 }
 
 module.exports = problem5;
