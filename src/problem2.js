74bd0aa1528dcca5e278ee362640835cc42fdf3a
Rki0 <pky00823@gmail.com>
2022-10-28 12:22:40 +0900

    feat: prob2. 제한 사항 만족 여부 판단 함수
    입력데이터의 제한 사항 만족여부를 판단하는 함수를 추가했습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,21 @@
-/**
- * 암호문에서 연속으로 이어지는 문자들을 지운 결과를 반환하는 함수
- * @param {array} stack 스택 구조를 활용하기 위한 빈 배열
- * @param {array} cryptoArr 암호를 쪼개서 배열로 변환한 것
- * @returns 최종적으로 스택에 남아있는 문자열들을 합쳐서 반환
- */
+function checkCryptogramLength(cryptogram) {
+  if (cryptogram.length < 1 || cryptogram.length > 1000) {
+    return false;
+  }
+
+  return true;
+}
+
+function checkCryptogramOnlyLowerCase(cryptogram) {
+  const regex = /^[a-z]+$/g;
+
+  if (!regex.test(cryptogram)) {
+    return false;
+  }
+
+  return true;
+}
+
 function compareStackWithCrypto(stack, cryptoArr) {
   for (let crypto of cryptoArr) {
     if (crypto === stack[stack.length - 1]) {
@@ -17,12 +29,13 @@ function compareStackWithCrypto(stack, cryptoArr) {
 }
 
 function problem2(cryptogram) {
-  // 스택 구조 활용?
-  // 스택에 하나씩 담음
-  // 스택 가장 위에 있는 값과 현재 담으려는 값이 같다면
-  // 스택에서 그 값을 뺀다.
-  // 그게 아니라면 스택에 넣는다
-  // 그런데 원본 배열로 연산하면 길이가 변하므로 웬만하면 건드리지말자.
+  if (
+    !checkCryptogramLength(cryptogram) ||
+    !checkCryptogramOnlyLowerCase(cryptogram)
+  ) {
+    return;
+  }
+
   let stack = [];
 
   let cryptoArr = cryptogram.split("");
