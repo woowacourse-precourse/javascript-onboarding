d88a6222f056eb320a661784ddce7e0c5591774b
pp449 <mma7710@naver.com>
2022-10-28 22:22:59 +0900

    [문제 2] : 1. 입력받은 문자열 중 연속하는 문자열을 제외하고 리턴하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,34 @@
+const removeConsecutiveString = (cryptogram) => {
+  let change = false;
+  let array = "";
+  let lastWord = '';
+
+  for(let i=0;i<cryptogram.length-1;i++) {
+    if(cryptogram[i] === cryptogram[i+1]) {
+      lastWord = cryptogram[i];
+      change = true;
+      while(cryptogram[i] === cryptogram[i+1]) {
+        i++;
+        if(i === cryptogram.length-1) {
+          break;
+        }
+      }
+    }
+    else {
+      lastWord = cryptogram;
+      array+=cryptogram[i];
+    }
+  }
+  if(lastWord !== cryptogram[cryptogram.length-1])
+    array += cryptogram[cryptogram.length-1];
+
+  // console.log(array);
+  if(change === false)
+    return array;
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return removeConsecutiveString(cryptogram);
 }
 
 module.exports = problem2;
