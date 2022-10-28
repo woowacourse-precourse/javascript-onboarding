2a647cb65db511c8e4dccd8778cb8c12e68b5859
wusi-hub <jws9595@naver.com>
2022-10-28 09:49:34 +0900

    problem1 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,31 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
 
+  let result = 0;
+
+  if(pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) {
+    return -1;
+  }
+
+  const pobiMax = Math.max(
+    ...pobi.map((n) => {
+      const sum = n.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
+      const mul = n.toString().split('').reduce((acc, cur) => acc * Number(cur), 1);
+      return Math.max(sum, mul);
+    })
+  )
+
+  const crongMax = Math.max(
+    ...crong.map((n) => {
+      const sum = n.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
+      const mul = n.toString().split('').reduce((acc, cur) => acc * Number(cur), 1);
+      return Math.max(sum, mul);
+    })
+  )
+
+  if (pobiMax > crongMax) result = 1;
+  if (pobiMax < crongMax) result = 2;
+
+  return result;
+
+}
 module.exports = problem1;
