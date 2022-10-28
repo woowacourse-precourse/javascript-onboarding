3d39eb8c5a98336c8a8f58da4362ad12070460d2
jin-Pro <dnjun2@ajou.ac.kr>
2022-10-28 16:54:00 +0900

    feat: problem7 solved

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,23 @@
+const {
+  getUserFriend,
+  getScoreTogetherFriend,
+  getVisitorDeleteFriend,
+  getScoreVisitor,
+} = require("./problem7.util");
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const getFriend = (user) => getUserFriend(user, friends);
+  const userFriends = getFriend(user).flat();
+  const weKnowFriend = userFriends.map(getFriend).flat();
+  const score = getScoreTogetherFriend(userFriends, weKnowFriend);
+  const newVisitors = getVisitorDeleteFriend(visitors, userFriends);
+  const newScore = getScoreVisitor(newVisitors, score);
+  return Object.entries(newScore)
+    .sort(([k, v], [_k, _v]) => {
+      if (v !== _v) return _v - v;
+      return _k - k;
+    })
+    .map(([k, v]) => k);
 }
 
 module.exports = problem7;
