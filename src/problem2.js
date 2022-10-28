d860c30323004936870918c8ed7bc9e19a48dd9f
nuheajiohc <jaehun341012@gmail.com>
2022-10-28 15:16:00 +0900

    문제2 : 한사이클을 돌며 중복 문자를 삭제하는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+    var answer;
+    return answer;
+}
+
+function delDuplication(crypto) {
+    let str = '';
+    for (let i = 0; i < crypto.length; i++) {
+        if (crypto[i] !== crypto[i + 1] && (crypto[i - 1] !== crypto[i])) {
+            str += crypto[i];
+        }
+    }
+    return str;
 }
 
 module.exports = problem2;
