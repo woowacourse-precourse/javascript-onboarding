24e382d9ca4f05fe72b653bcafea7580770e6c65
Yuncheol Kwak <lvbhkwak@gmail.com>
2022-10-28 19:59:22 +0900

    Mission Complete

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  while(money != 0) {
+    for(let unit of moneyUnit) {
+        let num = Math.floor(money / unit);
+        if(num != 0) {
+          answer.push(num);
+          money -= unit * num;
+        } else answer.push(0);
+    }
+  }
   return answer;
 }
 
