03d1ca1bc67e61c1ef239faa80f76e6ed0c418fd
seongminn <berry9901@gmail.com>
2022-10-28 21:47:02 +0900

    feat(problem4): 문자를 변환하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,22 @@
+function getOpposite(letter) {
+  const ascNum = letter.charCodeAt();
+
+  // 대문자일 경우 A와 Z의 아스키 코드를 더한 값에서 현재 문자의 아스키 코드 빼기
+  if (ascNum >= 65 && ascNum <= 90) {
+    return String.fromCharCode(155 - ascNum);
+  }
+
+  // 소문자일 경우 a와 z의 아스키 코드를 더한 값에서 현재 문자의 아스키 코드 빼기
+  if (ascNum >= 97 && ascNum <= 122) {
+    return String.fromCharCode(219 - ascNum);
+  }
+
+  // 알파벳이 아닌 경우 변환하지 않는다.
+  return letter;
+}
+
 function problem4(word) {
-  var answer;
+  let answer = '';
   return answer;
 }
 
