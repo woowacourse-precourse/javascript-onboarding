d05977aff9c8b153c641e95b40845c59cc8f3280
hwanfront <kim015jh@gmail.com>
2022-10-28 18:00:24 +0900

    feat: problem3 369 개수 count 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -5,7 +5,27 @@
  * 3. 배열을 순회한다.
  *   3-1. 배열 요소마다 3, 6, 9의 개수를 count해서 결과에 추가한다.
  * 4. 결과값을 return한다.
- *
+ */
+
+/**
+ * @param {string} char
+ */
+const check369Includes = (char) => ["3", "6", "9"].includes(char);
+
+/**
+ * @param {string} number
+ */
+const count369InNumber = (number) => {
+  let cnt = 0;
+  for (const char of number) {
+    if (check369Includes(char)) {
+      cnt++;
+    }
+  }
+  return cnt;
+};
+
+/**
  * @param {number} number
  * @returns {number} result
  */
