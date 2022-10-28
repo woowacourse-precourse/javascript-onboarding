0676227e273355bb4382b8ec72f7dc08eb9c77ba
GC-Park <kuyt3122@naver.com>
2022-10-29 01:58:38 +0900

    (4번)Feat:변환된 값 가져오는 함수 getStr 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,10 @@
+/*
+ 기능 목록
+  1) 변환된 값 가져오는 기능
+  2) 각 값들을 주어진 조건에 따라 값 변환 후 리턴
+  3) 소문자와 대문자만 변환하도록 예외 처리
+
+*/
 function wordChange(c){
   let mn;
   if(c===c.toUpperCase()){
@@ -12,7 +19,19 @@ function wordChange(c){
   return mn;
 }
 
+function exceptHandling(c){
+}
+
 function getStr(w){
+  let str=''
+  for(let c of w){
+    if(exceptHandling(c)){
+      str+=wordChange(c);
+    }else{
+      str+=c;
+    }
+  }
+  return str;
 }
 
 function problem4(word) {
