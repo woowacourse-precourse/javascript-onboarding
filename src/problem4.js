59fee4521350143c72eca0bf51207889ae5315fe
mjmj0217 <mjmj0217@naver.com>
2022-10-28 23:08:25 +0900

    feat: problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,32 @@
+function frogResult(alphabet) {
+  let alphabetString = "abcdefjhijklmnopqrstuvwxyz";
+  let index = alphabetString.indexOf(alphabet.toLowerCase());
+
+  //알파벳이 아닌 경우
+  if (index === -1) {
+    return alphabet;
+  }
+
+  //대문자인 경우
+  if (alphabet === alphabet.toUpperCase()) {
+    return alphabetString
+      .charAt(alphabetString.length - index - 1)
+      .toUpperCase();
+  }
+  //소문자인 경우
+  if (alphabet === alphabet.toLowerCase()) {
+    return alphabetString.charAt(alphabetString.length - index - 1);
+  }
+}
+
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  for (alphabet of word) {
+    console.log(frogResult(alphabet));
+    answer = answer + frogResult(alphabet);
+  }
+
   return answer;
 }
 
