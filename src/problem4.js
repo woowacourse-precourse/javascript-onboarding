cdfd580837594d105f284454969e88ab27f95619
loopy-lim <loopy@jnu.ac.kr>
2022-10-28 23:30:29 +0900

    feat: 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,5 @@
 const treeFrog = {
   monSay: "",
-  chars: [],
   inputMonSay: function (text) {
     if (!this.checkTextLength(text)) {
       return;
@@ -10,7 +9,27 @@ const treeFrog = {
   checkTextLength: function (text) {
     return text.length >= 1 && text.length <= 1000;
   },
+  convertTreeFlog: function () {
+    let convertedBigChars = convertWordSwitch(this.monSay, "A", "Z");
+    let convertedSmallChars = convertWordSwitch(convertedBigChars, "a", "z");
+    return convertedSmallChars;
+  },
 };
+
+function convertWordSwitch(text, startChar, endChar) {
+  let chars = text.split("");
+  let start = String.charCodeAt(startChar);
+  let end = String.charCodeAt(endChar);
+  chars = chars.map((char) => {
+    let charCode = String.charCodeAt(char);
+    if (charCode > start && charCode < end) {
+      let gap = charCode - start;
+      return String.charAt(end - gap);
+    }
+    return char;
+  });
+  return chars.join("");
+}
 
 function problem4(word) {
   var answer;
