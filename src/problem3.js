93ec6b893c997db72a6c1034e55e165c3dd0b340
jungsmins1 <jungsmins00@gmail.com>
2022-10-28 13:07:43 +0900

    feat: 3, 6, 9번에 맞춰 손뼉을 치게하는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
 function problem3(number) {
-  var answer;
-  return answer;
+
+  const clapNum = [3, 6, 9];
+  let clap = 0;
+  
+  //3, 6, 9번에 맞춰 손뼉을 치게하는 기능
+  const clapCheck = (num) => {
+    let str = String(num).split("");
+    str.forEach((s) => {
+      if (clapNum.includes(Number(s))) clap++;
+    })
+  }
 }
 
-module.exports = problem3;
+problem3(13)
+problem3(33)
+// module.exports = problem3;
