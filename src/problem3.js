92d2c5152ef88f85c5d2e45b5bb6ff08ece1dcd6
mjmj0217 <mjmj0217@naver.com>
2022-10-28 22:33:45 +0900

    feat: problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,24 @@
+function count(num) {
+  let cnt = 0;
+  while (num > 0) {
+    let target = num % 10;
+    if (target % 10 === 3 || target % 10 === 6 || target % 10 === 9) {
+      cnt += 1;
+    }
+    num = Math.floor(num / 10);
+  }
+  return cnt;
+}
+
 function problem3(number) {
   var answer;
+  let sum = 0;
+
+  for (let n = 0; n <= number; n++) {
+    sum += count(n);
+  }
+  answer = sum;
+
   return answer;
 }
 
