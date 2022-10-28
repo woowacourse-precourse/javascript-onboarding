c0db7dc280e825db93cac91dfc0d145e8b72ff4f
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-28 14:41:37 +0900

    feat(PROBLEM3): 1. n에 대해 3,6,9를 포함하는 갯수 구하는 clapCount 생성

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
-  var answer;
-  return answer;
+
 }
 
 module.exports = problem3;
+
+const clapCount = n => {
+  let count = 0;
+  const arr = [...String(n)]; 
+
+  const threeCount = arr.filter(a => a == '3').length;
+  const sixCount = arr.filter(a => a == '6').length;
+  const nineCount = arr.filter(a => a == '9').length;
+  
+  count += threeCount + sixCount + nineCount;
+
+  return count;
+}
