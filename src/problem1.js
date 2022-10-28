ec95dfbe2c2aeaf39db7ebbf82a25e857b9ecb7a
Hwang Juhee <julia0276@naver.com>
2022-10-28 23:50:47 +0900

    :sparkles: [feat] 숫자의 각 자리 더하거나 곱하여 큰 수 찾기 기능 구현 #P1

--- src/problem1.js
+++ src/problem1.js
@@ -14,7 +14,39 @@ function problem1(pobi, crong) {
     return -1;
   } 
   else {
+    function checkBigNum (num) {
+      let str = String(num);
+      let sum = 0;
+      let multiply = 1;
+      for(let i = 0; i < str.length; i++){
+        sum += Number(str[i]);
+        multiply *= Number(str[i]);
+      }
+      return sum,multiply;
+    }
     
+    function getBiggerNum (arr) {
+      let bigNum = [];
+      bigNum.push(checkBigNum(arr[0]));
+      bigNum.push(checkBigNum(arr[1]));
+      bigNum.sort((a,b)=>b-a);
+      console.log(bigNum)
+      return bigNum[0];
+    }
+    
+    let answer;
+    const [pobiNum, crongNum] = [getBiggerNum(pobi),getBiggerNum(crong)];
+    
+    if (pobiNum===crongNum){
+      answer = 0;
+    } else if (pobiNum>crongNum) {
+      answer = 1;
+    } else if (pobiNum<crongNum) {
+      answer = 2;
+    } else {
+      answer = -1;
+    }
+    return answer;
   }
 }
 
