8d92560886ec5aa9e06c5ff686d4738ccfe06b81
2wndrhs <2wndrhs@gmail.com>
2022-10-27 21:46:28 -0700

    feat(PROBLEM1): 페이지 번호를 각 자리 숫자로 분리하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -6,12 +6,28 @@ function isPageContinuous(pages) {
   return false;
 }
 
+// 페이지 번호를 각 자리 숫자로 분리하는 기능
+function splitPageNumber(pages) {
+  const splitedNumbers = pages.map((number) =>
+    number
+      .toString()
+      .split("")
+      .map((number) => +number)
+  );
+  return splitedNumbers;
+}
+
 function problem1(pobi, crong) {
   var answer;
   if (!isPageContinuous(pobi) || !isPageContinuous(crong)) {
     return -1;
   }
+  const splitedPobi = splitPageNumber(pobi);
+  const splitedCrong = splitPageNumber(crong);
+  console.log(splitedPobi, splitedCrong);
   return answer;
 }
+
+problem1([97, 98], [101, 102]);
 
 module.exports = problem1;
