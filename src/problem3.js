bda04aa6618964afae1d53db671e8db631d5c567
RooT_Kim <bdd3452@gmail.com>
2022-10-29 00:29:44 +0900

    문제3번) 기능목록 정리 및 함수 선언

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
-  return answer;
+	//1부터 number까지 루프를 돌리는 함수 -> clap
+	//숫자에 3,6,9가 속해있는지 체크하는 함수 -> checkNum
+	return clap(number);
 }
+
+function clap() {}
+
+function checkNum() {}
 
 module.exports = problem3;
