a6f2309b6ca67a494913f0c96cec01d3b0e102d9
MAGHC <windowsapple95@gmail.com>
2022-10-28 14:23:16 +0900

    feat: problem5 기능구현 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let item of unit) {
+    answer.push(Math.floor(money / item));
+    money = money % item;
+  }
+
   return answer;
 }
 
