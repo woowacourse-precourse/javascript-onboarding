cec85781f5b81bfe3bf8070590a1718cc3d5ae61
최희원 <jeeen1@naver.com>
2022-10-28 21:37:49 +0900

    feat(problem3): add digit checker

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,15 @@
+/**
+ * checks if the digit is 3 or 6 or 9.
+ * @param {string} digit given digit
+ * @returns {boolean} whether the digit is (3, 6, 9) or not.
+ */
+function is369(digit) {
+  if (digit === "3") return true;
+  if (digit === "6") return true;
+  if (digit === "9") return true;
+  return false;
+}
+
 function problem3(number) {
   var answer;
   return answer;
