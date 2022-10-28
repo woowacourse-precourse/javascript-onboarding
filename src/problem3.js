a9faafd22be56bb3aa280a27cc6babec0b202e56
hyeongdong <khd941018@gmail.com>
2022-10-28 16:06:13 +0900

    docs : 기능 목록 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
-function problem3(number) {
-  var answer;
-  return answer;
-}
+/*
+--기능 목록--
+1. 숫자의 각 자리수들 중 3,6,9의 총 개수를 리턴한다.
+    - 3,6,9가 없을 시 0을 리턴
+
+2. 1부터 number까지 숫자를 하나씩 대면서, 각 숫자의 3,6,9 개수의 총합을 구한다.
+
+*/
+
+function problem3(number) {}
 
 module.exports = problem3;
