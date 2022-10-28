04f99c94ad75cd95692ea1ccc14cc4821f584384
pinkishincoloragain <smb1103@gmail.com>
2022-10-28 14:27:14 +0900

    feat: solved problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+  let stack = [];
+  let splitted = [...cryptogram];
 
+  splitted.forEach((spl) => {
+    if (stack.length < 1) stack.push(spl);
+    else if (spl === stack[stack.length - 1]) stack.pop();
+    else stack.push(spl);
+  });
+
+  return stack.join('');
+}
 module.exports = problem2;
