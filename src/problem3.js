d6eae7ec0740c746b144fd8626619934051b28ab
Moyujin <scab12341@gmail.com>
2022-10-29 04:01:55 +0900

    3번 문제 3, 6, 9인 경우 횟수 세기 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
+  var answer = countClap(number);
+
   return answer;
 }
+
+const countClap = (number) => {
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    let num = i;
+    while (num > 0) {
+      let n = Math.floor(num % 10);
+      if (n === 3 || n === 6 || n === 9) count++;
+      num = Math.floor(num / 10);
+    }
+  }
+  return count;
+};
 
 module.exports = problem3;
