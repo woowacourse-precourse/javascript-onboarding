b5e8809dff6a0baacda64d359d9e7b634476a7a1
leeseooo <leeseoooo@naver.com>
2022-10-28 22:53:13 +0900

    feat: problem7 user와 친구인 사용자들 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,12 +1,26 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let friendsList = {};
   let userScoreList = {};
+  let myFriends = [];
 
   visitors.forEach((visitor) => {
     if (userScoreList[visitor] === undefined) userScoreList[visitor] = 0;
     userScoreList[visitor] += 1;
   });
 
+  friends.forEach((friend) => {
+    const [a, b] = friend;
+    userScoreList[a] = 0;
+    userScoreList[b] = 0;
+    if (friendsList[a] === undefined) friendsList[a] = [];
+    if (friendsList[b] === undefined) friendsList[b] = [];
+    friendsList[a].push(b);
+    friendsList[b].push(a);
+    if (friend.includes(user)) {
+      myFriends.push(...friend.filter((x) => x !== user));
+    }
+  });
   return answer;
 }
 
