635dd19c51510b1ab3d98c11f0d4f24a36d1a473
turtle601 <turtle601@naver.com>
2022-10-28 14:02:07 +0900

    Feat: [problem7] input 제한 사항 및 예외처리하는 ErrorCase 클래스 구현
    <ErrorCase클래스>
    - checkUserLimit() : user 길이 체크 함수
    - checkUserLower() : user가 모두 소문자로 이루어있는지 확인하는 함수
    - checkUser() : user 관련 체크 함수를 모두 확인하는 함수
    - checkFriendsLimit() : friends 배열 길이 체크 함수
    - checkFriendsIDLimit() : friends의 ID값 길이 체크 함수
    - checkFriendsIDListLimit() : friends 배열 원소의 길이 체크 함수
    - checkFriends() : friends 관련 체크 함수를 모두 확인하는 함수
    - checkVisitorsLimit() : visitors 배열 길이 체크 함수
    - checkAllError() : user, friends, visitors 값에 대한 에러 발생 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,129 @@
+class ErrorCase {
+  constructor(user, friends, visitors) {
+    this.user = user;
+    this.friends = friends;
+    this.visitors = visitors;
+    this.checkAllError();
+  }
+
+  checkUserLimit() {
+    return 1 <= this.user.length && this.user.length <= 30;
+  }
+
+  checkUserLower() {
+    return this.user.match(/[a-z]/g).length === this.user.length;
+  }
+
+  checkUser() {
+    return this.checkUserLimit() && this.checkUserLower();
+  }
+
+  checkFriendsLimit() {
+    return 1 <= this.friends.length && this.friends.length <= 10000;
+  }
+
+  checkFriendsIDLimit() {
+    return this.friends
+      .flatMap((friend) => friend)
+      .every((friend) => 1 <= friend.length && friend.length <= 30);
+  }
+
+  checkFriendIDListLimit() {
+    return this.friends.every((friend) => friend.length === 2);
+  }
+
+  checkFriends() {
+    return (
+      this.checkFriendsLimit() &&
+      this.checkFriendsIDLimit() &&
+      this.checkFriendIDListLimit()
+    );
+  }
+
+  checkVisitorsLimit() {
+    return 0 <= this.visitors.length && this.visitors.length <= 10000;
+  }
+
+  checkAllError() {
+    if (!this.checkUser()) {
+      throw new Error("input 양식 중 user값에 오류가 발생하였습니다.");
+    }
+
+    if (!this.checkFriends()) {
+      throw new Error("input 양식 중 friends값에 오류가 발생하였습니다.");
+    }
+
+    if (!this.checkVisitorsLimit()) {
+      throw new Error("input 양식 중 visitor값에 오류가 발생하였습니다.");
+    }
+  }
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  new ErrorCase(user, friends, visitors);
+
+  return;
 }
 
 module.exports = problem7;
+
+// 에러 케이스
+
+// 사용자 아이디가 소문자로 이루어지지 않았을 때
+// problem7(
+//   "Arko",
+//   [
+//     ["Donut", "andole"],
+//     ["donut", "jun"],
+//     ["donut", "mrko"],
+//     ["shakevan", "andole"],
+//     ["shakevan", "jun"],
+//     ["shakevan", "mrko"],
+//   ],
+//   ["bedi", "bedi", "donut", "bedi", "shakevan"]
+// );
+
+// user 길이가 30자가 넘을 때
+// problem7(
+//   "mrkodasljkfkljsadfkjlsajdflkjsadlkfjalsjdflasjfljasljflasjldfjlsa;jflkjasljfdldajslkdfjalsj",
+//   [
+//     ["donut", "andole"],
+//     ["donut", "jun"],
+//     ["donut", "mrko"],
+//     ["shakevan", "andole"],
+//     ["shakevan", "jun"],
+//     ["shakevan", "mrko"],
+//   ],
+//   ["bedi", "bedi", "donut", "bedi", "shakevan"]
+// );
+
+// friends 길이 1이상 10000이하
+// problem7("mrko", [], [("bedi", "bedi", "donut", "bedi", "shakevan")]);
+
+// friends 원소 길이가 2인지
+// problem7(
+//   "mrko",
+//   [
+//     ["donut"],
+//     ["donut", "jun"],
+//     ["donut", "mrko"],
+//     ["shakevan", "andole"],
+//     ["shakevan", "jun"],
+//     ["shakevan", "mrko"],
+//   ],
+//   ["bedi", "bedi", "donut", "bedi", "shakevan"]
+// );
+
+// friends 아이디 길이가 1이상 30이하 인지
+// problem7(
+//   "mrko",
+//   [
+//     ["", "andole"],
+//     ["donut", "jun"],
+//     ["donut", "mrko"],
+//     ["shakevan", "andole"],
+//     ["shakevan", "jun"],
+//     ["shakevan", "mrko"],
+//   ],
+//   ["bedi", "bedi", "donut", "bedi", "shakevan"]
+// );
