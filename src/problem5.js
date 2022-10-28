d467fb034f711bfeea3651b4397ba2f843fa0b09
yong <tkddbs1644@gmail.com>
2022-10-28 18:34:47 +0900

    문제5: 돈을 단위로 나누었을 때 몫과 나머지를 반환하는 기능을 구현한다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,11 @@
 /*
-[ ] 돈을 단위로 나누었을 때 몫과 나머지를 반환하는 기능을 구현한다.
+[x] 돈을 단위로 나누었을 때 몫과 나머지를 반환하는 기능을 구현한다.
 [ ] 큰 단위부터 돈을 나누고 결과를 반환한다.
 */
+
+function divideMoney(money, unit) {
+  return [Math.floor(money / unit), money % unit];
+}
 
 function problem5(money) {
   var answer;
