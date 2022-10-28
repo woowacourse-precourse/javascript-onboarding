da33a5bc380066e9d02d248070651154545a2368
murpin <qw04011@gmail.com>
2022-10-28 12:49:05 +0900

    [문제 1번] docs: 기능 분석 주석 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+/*
+ * 기능 분석
+ * [] pobi와 crong이 펼친 왼쪽, 오른쪽 페이지 계산 기능 
+ * [] pobi와 crong이 펼친 페이지 중 계산 값이 가장 큰 값 반환 기능
+ * [] 입력된 값이 연속된 페이지가 아닌 경우 -1을 반환 기능
+ * [] pobi와 crong의 점수 비교 후 결과에 대한 결과 반환 기능
+ */
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
