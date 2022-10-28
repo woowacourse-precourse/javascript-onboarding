df4187dfe323f395b29264951389da619d8e9bf4
Gyu <tmdrb0912@naver.com>
2022-10-29 01:25:57 +0900

    문제 2번 풀이 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
 function problem2(cryptogram) {
   var answer;
-  return answer;
+  let i = 0;
+  let tmpary = cryptogram.split("");
+  let checking = true;
+
+  while (checking) {
+    let count = 0,
+      indexCount = 0;
+    for (chr of tmpary) {
+      if (chr == tmpary[indexCount + 1]) {
+        tmpary.splice(indexCount, 2);
+        count++;
+        break;
+      }
+      indexCount++;
+    }
+    if (count == 0) checking = false;
+  }
+
+  console.log(tmpary);
+
+  return tmpary.join("");
 }
 
 module.exports = problem2;
