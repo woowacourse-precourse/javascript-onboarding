36538489649a8f545c444776e143406f910ed731
sulmeee <poorooria@naver.com>
2022-10-28 12:33:32 +0900

    [problem2] feat: 단어를 하나씩 나누기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function problem2(cryptogram) {
-  // 중복된 단어를 담을 배열 선언
+  // 결과를 담아줄 배열 선언
   const result = [];
+
+  // 주어진 단어를 단어 하나씩 나누기
+  const cryptoframWord = cryptogram.split('')
 }
 
 module.exports = problem2;
