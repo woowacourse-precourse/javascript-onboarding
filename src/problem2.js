2a49de751d39051aa7f0d4f4b3660a8eb5d42ace
HWI <dlwognl99@naver.com>
2022-10-28 11:07:19 +0900

    feat: 중복 시작되는 문자의 index 값을 얻는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -11,14 +11,23 @@ function problem2(cryptogram) {
   while (1) {
     // 중복되는 index 값을 얻는다.
     const duplicatedindex = getDuplictatedindex(cryptogram);
-    if (duplicatedindex === 0) break;
+    if (duplicatedindex === -1) break;
     const count = getCount(cryptogram, duplicatedindex);
     cryptogram.splice(duplicatedindex, count);
   }
+
+  return cryptogram;
 }
 
 // 중복된 문자가 있는 index 값을 얻는 함수
-function getDuplictatedindex(cryptogram) {}
+function getDuplictatedindex(cryptogram) {
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      return i;
+    }
+    return -1;
+  }
+}
 
 // 중복된 문자로부터 몇 개까지 중복인지 count 값을 얻는 함수
 function getCount(cryptogram, duplicatedindex) {}
