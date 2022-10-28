14c39489cef959ad739e66ff816ba1de84bfa3e6
min <hororonoodle@gmail.com>
2022-10-28 23:03:41 +0900

    refactor : Reduce the coupling of functions that find values contained in numbers

--- src/problem3.js
+++ src/problem3.js
@@ -3,9 +3,10 @@ function problem3(number) {
   // 1부터 number 까지 1개씩 숫자를 말한다.
   for (let i = 1; i <= number; i++) {
     // 숫자에서 3,6,9 의 갯수를 센다. (33 일경우 2)
-    answer += [...i.toString()].filter(
+    const filteredNumberArr = [...i.toString()].filter(
       (val) => val === '3' || val === '6' || val === '9'
-    ).length;
+    );
+    answer += filteredNumberArr.length;
   }
   return answer;
 }
