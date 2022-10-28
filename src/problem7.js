541bc371fdfc5f35125f0dc5a9908416ba8d46b7
chocoro99 <76997927+chocoro99@users.noreply.github.com>
2022-10-28 21:14:15 +0900

    problem7/feat : user와 친구인 사람들은 따로 배열에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let userFriend = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      for (let z = 0; z < friends[i].length; z++) {
+        if (friends[i][z] != user) {
+          userFriend.push(friends[i][z]);
+        }
+      }
+    }
+  }
 }
-
 module.exports = problem7;
