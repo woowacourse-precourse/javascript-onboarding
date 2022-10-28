b2e88db3c20a97ed7e170035e5d29256dbee938f
vinivin153 <vinivin153@naver.com>
2022-10-28 20:04:35 +0900

    feat: 유저의 친구를 찾는 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,17 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function findUserFrieds(user, friends) {
+  const userFriends = new Set();
+  friends.forEach((friend) => {
+    const [friend1, friend2] = friend;
+    if (friend1 === user) {
+      userFriends.add(friend2);
+    } else if (friend2 === user) {
+      userFriends.add(friend1);
+    }
+  });
+  return userFriends;
+}
+
 module.exports = problem7;
