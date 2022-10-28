13f7af8e500a7965259bf8a5c2b77f9b5d60331e
2je0 <bulljy@naver.com>
2022-10-29 05:15:05 +0900

    ✨ feat(problem1): 페이지 에러 체크 함수 isError 추가
    1~400 페이지가 아니거나, 왼쪽페이지가 홀수가 아니거나, 왼쪽 페이지와 오른쪽 페이지가 연속되지 않았을 때 1반환, 그 외 0 반환

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,9 @@
+function isError(arr) {
+  if (arr[0] < 1 || arr[1] > 400) return 1;
+  if (arr[0] % 2 == 0) return 1;
+  if (arr[0] + 1 !== arr[1]) return 1;
+  return 0;
+}
 function problem1(pobi, crong) {
   var answer;
   return answer;
