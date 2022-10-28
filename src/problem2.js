3ea872986f01403a452231ae38c6fe7ca8ec3182
김희진 <choha941108@gmail.com>
2022-10-28 15:03:36 +0900

    (2번)다음문제를 위해 로그 주석처리

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,7 @@
 function problem2(cryptogram) { //단어하나를 받는다
   var answer = [] ;
   const cryptogram_add = [...cryptogram]
-  console.log("단어 배열변경",cryptogram_add)
+  //console.log("단어 배열변경",cryptogram_add)
   //단어를 알파벳 별로 배열로 쪼갠다
 
   answer.push(cryptogram_add[0])
@@ -11,13 +11,13 @@ function problem2(cryptogram) { //단어하나를 받는다
     } else {
       answer.pop()
     }
-    console.log("단어 중복값 삭제",answer) 
+   // console.log("단어 중복값 삭제",answer) 
   }
   //더이상 아무것도 삭제 안될때까지 반복  
     //배열을 순회하면서, 중복시 삭제
       //중복 삭제 배열 반환
   answer = answer.join("")
-  console.log("단어확인",answer)
+  //console.log("단어확인",answer)
   return answer;
   
     //마지막에 만들어진 단어를 리턴한다
