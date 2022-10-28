004a267f1106d35539fcf76de0fb3be39f331453
eunsung <esk114477@gmail.com>
2022-10-29 01:38:20 +0900

    문제6번 완성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,28 @@
 function problem6(forms) {
-  var answer;
+  
+  let user = [];
+  let answer = [];
+  
+  forms.map((form) => {
+    const [useremail, username] = form;
+    for(let i = 0; i<username.length - 1; i++){
+      let word = username.slice(i, i + 2);
+      word in user ? user[word]++ : user[word] = 1;
+    }    
+  });
+
+  forms.map((form) => {
+    const [useremail, username] = form;
+    for(let i = 0; i < username.length - 1; i++){
+      let word = username.slice(i, i + 2);
+      if(user[word] > 1){
+        answer.push(useremail);
+      }
+    }
+  });
+
+  answer.sort();
+  
   return answer;
 }
 
