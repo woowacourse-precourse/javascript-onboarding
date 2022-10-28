b395b809333930d946881ee4e31fc89d91c1c9b7
pp449 <mma7710@naver.com>
2022-10-28 20:44:15 +0900

    [문제 1] : pobi와 crong중 누가 이겼는지 출력하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -12,10 +12,33 @@ const checkErr = (pobi, crong) => {
   return findErr;
 }
 
+const findMax = (num) => {
+  let sum_max = 0;
+  let mul_max = 1;
+  for(let i = num;i!=0;i=parseInt(i/10)) {
+    let tmp = i%10;
+    sum_max += tmp;
+    mul_max *= tmp;
+  }
+  return Math.max(sum_max, mul_max);
+}
+
+const winner = (pobi_max, crong_max) => {
+  if(pobi_max > crong_max)
+    return 1;
+  else if(pobi_max < crong_max)
+    return 2;
+  else
+    return 0;
+}
+
 function problem1(pobi, crong) {
-  if(checkErr === -1)
+  if(checkErr(pobi, crong) === -1)
     return -1;
-  
+  pobi_max = Math.max(findMax(pobi[0]),findMax(pobi[1]));
+  crong_max = Math.max(findMax(crong[0]),findMax(crong[1]));
+
+  return winner(pobi_max, crong_max);
 }
 
 module.exports = problem1;
