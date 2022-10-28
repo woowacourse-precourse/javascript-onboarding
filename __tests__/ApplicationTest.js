cc2d0bf1960eb8b12c1b41fce73776abce73d987
HWI <dlwognl99@naver.com>
2022-10-29 02:09:39 +0900

    문제3_테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,6 +1,6 @@
 const problem1 = require("../src/problem1");
 const problem2 = require("../src/problem2");
-// const problem3 = require("../src/problem3");
+const problem3 = require("../src/problem3");
 // const problem4 = require("../src/problem4");
 // const problem5 = require("../src/problem5");
 // const problem6 = require("../src/problem6");
@@ -29,14 +29,14 @@ describe("problem2", () => {
   });
 });
 
-// describe("problem3", () => {
-//   test("case1", () => {
-//     expect(problem3(13)).toEqual(4);
-//   });
-//   test("case2", () => {
-//     expect(problem3(33)).toEqual(14);
-//   });
-// });
+describe("problem3", () => {
+  test("case1", () => {
+    expect(problem3(13)).toEqual(4);
+  });
+  test("case2", () => {
+    expect(problem3(33)).toEqual(14);
+  });
+});
 
 // describe("problem4", () => {
 //   test("case1", () => {
