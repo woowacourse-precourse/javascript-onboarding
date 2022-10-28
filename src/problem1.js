5b0b9af529154e29802e21a63773e1a28a515101
knh6269 <knh6269@gmail.com>
2022-10-28 19:33:02 +0900

    feat: 미션1) 유저의 점수를 계산하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -13,6 +13,23 @@ function getBestScore(array, best){
   return best;
 }
 
+//유저의 점수를 계산하는 함수
+function calculateScore(user){
+  let best = 0; //최고 점수 
+  for(let i = 0; i < user.length; i++){
+    let array = [];
+    //100점보다 큰경우 
+    if (user[i] >= 100) {
+      scoreSplit(user, 100, i, array);
+    }
+    scoreSplit(user, 10, i, array);
+    scoreSplit(user, 1, i, array);
+    //최고점수 판독 
+    best = getBestScore(array, best);
+  }
+  return best;
+}
+
 }
 
 module.exports = problem1;
