17b89cc67003734c6ad878470d019dfb1dbf4687
MinGyu-Kang <kagrin97@gmail.com>
2022-10-28 11:55:12 +0900

    problem4/feat : 기능목록 1번인 A~Z를 키 Z~A를 값에 객체에 저장

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const dict = {};
+
+  let reverse = 90;
+
+  for (let i = 65; i < 91; i++) {
+    dict[String.fromCharCode(i)] = String.fromCharCode(reverse);
+    reverse -= 1;
+  }
+  return dict;
 }
 
 module.exports = problem4;
+
+console.log(problem4("I love you"));
