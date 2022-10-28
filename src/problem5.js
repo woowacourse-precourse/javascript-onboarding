dd005c0bfd379da86fc30b48af67bf37dced771a
MJ <mj632900@gmail.com>
2022-10-28 22:57:56 +0900

    feat : 결과 출력

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,18 @@
+const exchange = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function getExchange(haveMoney, exchangeMoney) {
-  const change = haveMoney % exchangeMoney;
-  const exchangeCount = Math.floor(haveMoney / exchangeMoney);
-  return {
-      haveMoney: change,
-      exchangeCount: exchangeCount,
-  };
+    const change = haveMoney % exchangeMoney;
+    const exchangeCount = Math.floor(haveMoney / exchangeMoney);
+    return {
+        haveMoney: change,
+        exchangeCount: exchangeCount,
+    };
 }
-
-function problem5(money) {}
-
+function problem5(money) {
+    return exchange.map((toChange) => {
+        const moneyInfo = getExchange(money, toChange);
+        money = moneyInfo.haveMoney;
+        return moneyInfo.exchangeCount;
+    });
+}
 module.exports = problem5;
