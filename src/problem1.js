2abf5b3c040d73360dfbf1e07ee22a88d376b651
d0422 <rlfehd2013@naver.com>
2022-10-28 11:58:14 +0900

    Feat:숫자 자리수를 더하는 메서드 추가

--- src/problem1.js
+++ src/problem1.js
@@ -2,4 +2,13 @@ function problem1(pobi, crong) {
   var answer;
   return answer;
 }
+function sumDigits(number) {
+  let sum = 0;
+  while (number > 0) {
+    sum += number % 10;
+    number = Math.floor(number / 10);
+  }
+  return sum;
+}
+
 module.exports = problem1;
