0dd89d0ebf76ac14928f23a4bbb958a652d64f4d
coolIlive <galilee155@naver.com>
2022-10-29 00:24:47 +0900

    FEAT(problem6): 공통된 문자열 추출 기능을 구현하다
    for문을 돌며 각 문자열 비교. 한 문자씩 비교할때 filter와includes함수를 사용함. 그리고 리스트로 출력되는 것을 문자열로 변환해서 변수에 저장했다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
+  let answer=new Set();
+
+  // 각 닉네임에서 공통된 부분 추출하는 기능, 공통부분의 순서가 일치하는지 확인
+  for (let i=0;i<forms.length-1;i++) {
+    for (let j=i+1;j<forms.length;j++) {
+      // 각 닉네임에서 공통된 부분 추출하는 기능
+      let sameWords = [...forms[i][1]].filter(x => forms[j][1].includes(x)).join('');
+      
+      // 공통부분의 순서가 일치하는지 확인하는 기능
+    }
+  }
+
+
   return answer;
 }
-
-module.exports = problem6;
+console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
+// module.exports = problem6;
