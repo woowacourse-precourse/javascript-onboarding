0cc0417668a2c93acc76fae844ad1bfda2d0b9b0
kang <mdiu2203@naver.com>
2022-10-29 02:37:38 +0900

    feat(PROBLEM4): make a blue frog dictionary

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,22 @@ function problem4(word) {
   return answer;
 }
 
+function makeBlueFrogDictionary() {
+  const [charUpperA, charUpperZ, charLowerA, charLowerZ] = ['A', 'Z', 'a', 'z',].map(c => c.charCodeAt(0));
+
+  const result = new Map();
+  for (let i = 0; i < 26; i++) {
+    result.set(
+        String.fromCharCode(charUpperA + i),
+        String.fromCharCode(charUpperZ - i),
+    );
+    result.set(
+        String.fromCharCode(charLowerA + i),
+        String.fromCharCode(charLowerZ - i),
+    );
+  }
+
+  return result;
+}
+
 module.exports = problem4;
