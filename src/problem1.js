f60ebd99247c7858efbfd59ebff8775a6c2f8a63
HAEROOL <gofn080776@gmail.com>
2022-10-29 00:25:23 +0900

    feat: [problem1] - 숫자를 모두 더하는 함수와 모두 곱하는 함수에서 최댓값을 구할 수 있도록 변경
    숫자를 연산하는 함수의 인자를 페이지 숫자 배열로 변경하여 최댓값을 구하는 함수로 기능을 수정

--- src/problem1.js
+++ src/problem1.js
@@ -7,12 +7,18 @@ function isValid(pageArray){
   return  pageArray[0] % 2 === 0 || pageArray[1] % 2 === 1
 }
 
-function sumAllElement(pageNumber){
-  return pageNumber.split('').reduce((acc, current) => acc + Number(current),0);
+function sumAllElement(pageArray){
+  return Math.max(
+    pageArray[0].split('').reduce((acc, current) => acc + Number(current),0),
+    pageArray[1].split('').reduce((acc, current) => acc + Number(current),0)
+    );
 }
 
-function multiplyAllElement(pageNumber){
-  return pageNumber.split('').reduce((acc, current) => acc * Number(current),1);
+function multiplyAllElement(pageArray){
+  return Math.max(
+    pageArray[0].split('').reduce((acc, current) => acc * Number(current),1),
+    pageArray[1].split('').reduce((acc, current) => acc * Number(current),1)
+  );
 }
 
 module.exports = problem1;
