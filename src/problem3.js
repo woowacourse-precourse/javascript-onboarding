899f8b83f2e0ab6231d2c8a270194e57a530a696
Dean <yjj052800@gmail.com>
2022-10-28 14:26:38 +0900

    FEAT: [3번] 1부터 number까지  배열에 나열

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
   var answer;
+  let numberArr = [];
+  let sum = 0;
+  for (i = 1; i <= number; i++) {
+    numberArr.push(i)
+  }
+  console.log(numberArr);
+
+  
   return answer;
 }
 
 module.exports = problem3;
+problem3(13);
