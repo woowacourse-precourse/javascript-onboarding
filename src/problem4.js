38c7995bac14e3e114acd9e132d0be100b8b85a8
jungsmins1 <jungsmins00@gmail.com>
2022-10-28 20:46:35 +0900

    feat: 청개구리 엄마의 말을 사전을 참고해 반대로 변환하는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  
+  let result;
+  wordArr = word.split("");
+
+  //청개구리 엄마의 말을 사전을 참고해 반대로 변환하는 기능
+  const conversion = (str) => {
+    let asciiNum = str.charCodeAt(0);
+    
+    if (asciiNum > 64 && asciiNum < 91) {
+      return String.fromCodePoint(155 - asciiNum);
+    }
+    if (asciiNum > 96 && asciiNum < 123) {
+      return String.fromCodePoint(219 - asciiNum);
+    }
+
+    return String.fromCodePoint(asciiNum);
+  }
+
+  return result;
 }
 
-module.exports = problem4;
+problem4("I love you")
+// module.exports = problem4;
