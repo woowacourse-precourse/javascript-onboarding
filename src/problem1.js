a3016b26c88d045afea26bcbe6662f12d5cd2573
Lee-Young-Jae <yjlee1937@naver.com>
2022-10-28 19:56:53 +0900

    문제 1 - 예외처리: 연속되지 않은 페이지, 왼쪽페이지(홀수) 오른쪽페이지(짝수)가 아님

--- src/problem1.js
+++ src/problem1.js
@@ -11,6 +11,22 @@
 
 function problem1(pobi, crong) {
   var answer;
+
+  /** 예외처리 */
+  // pobi, crong이 펼친 페이지 번호가 연속되지 않은 경우
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    return -1;
+  }
+  // pobi, crong의 왼쪽 페이지가 홀수가 아니고 오른쪽 페이지가 짝수가 아닌 경우
+  if (
+    pobi[0] % 2 === 0 ||
+    crong[0] % 2 === 0 ||
+    pobi[1] % 2 !== 0 ||
+    crong[1] % 2 !== 0
+  ) {
+    return -1;
+  }
+
   return answer;
 }
 
