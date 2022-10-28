f19297d622947713c74a53adace1b9a8cfd56b72
evencoding <evencoding@gmail.com>
2022-10-28 22:34:30 +0900

    [문제6] feat: 중복이 있는 연속 된 두 글자를 반환하는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -2,8 +2,19 @@ const getDuplicateChars = (charsCase) => [
   ...new Set(charsCase.filter((char, i) => charsCase.indexOf(char) !== i)),
 ];
 
+const getTwoCharsArr = (forms) => {
+  return forms.reduce((acc, crew, i) => {
+    const nickname = [...crew[1]];
+    for (let j = 0; j < nickname.length - 1; j++) {
+      acc.push(nickname[j] + nickname[j + 1]);
+    }
+    return acc;
+  }, []);
+};
+
 function problem6(forms) {
   const charsCase = getTwoCharsArr(forms);
+  const duplicateChars = getDuplicateChars(charsCase);
 
   return;
 }
