dfa2db8bbe19f78988cec0ee31801f8821b4fc03
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-28 12:30:16 +0900

    problem6 nickname 자르는 기능 substr 함수로 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,24 +1,26 @@
 function problem6(forms) {
   var answer;
-  let includenickname = []
   let duplicatenickname = []
-  for (let i = 0; i<forms[0][1].length; i++){
-    for (let j = 2; j<=forms[0][1].length; j++){
-      if (j+i <= forms[0][1].length){
-        includenickname.push(forms[0][1].substr(i,j))
-      }
-    }
-  }
-  console.log(includenickname)
+  substr(forms[0])
   for (let k = 1; k<forms.length; k++){
     for (let p = 0; p<includenickname.length; p++){
       if (forms[k][1].includes(includenickname[p])){
         duplicatenickname.push(includenickname[p]);
       }
     }
-
+    substr(forms[k])
   }
   return answer;
 }
+function substr(user){
+  for (let i = 0; i<user[1].length;i++){
+    for (let j = 2; j<=user[1].length; j++){
+      if (j+i <= user[1].length){
+        includenickname.push(user[1].substr(i,j))
+      }
+    }
+  }
+}
+var includenickname = []
 console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
 module.exports = problem6;
