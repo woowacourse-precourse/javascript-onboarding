c70b2417a535510c0005792f345e66061788e304
kyw0716 <kyw0716@naver.com>
2022-10-28 15:09:45 +0900

    feat: [problem2] 새로운 구현 방식 적용 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,8 @@
 function problem2(cryptogram) {
   let answer = cryptogram;
-  let repeatCheckRegexp =
-    /a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}/;
 
-  while (checkIsRepeatExist(answer, repeatCheckRegexp)) {
-    answer = deleteRepeatLetter(answer, repeatCheckRegexp);
+  while (getIsRepeatExist(answer)) {
+    answer = getModifiedCryptogram(answer);
   }
 
   return answer;
