e63244f6055386280e596b959ec63e0b30e60144
An Sol <apdlvmf486@gmail.com>
2022-10-28 12:02:31 +0900

    7번: user의 친구목록 분리 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const scores = {};
+  const userFriend = [];
+  for (let rel of friends){
+    if (rel.includes(user)){
+      userFriend.push(rel.filter(x => x !== user)[0]);
+    }
+  }
 }
 
 module.exports = problem7;
