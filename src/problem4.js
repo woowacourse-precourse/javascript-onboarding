0d3d2231cdeea48ed6e5ddc52a15b7c4a4e0d28d
ryuna <anottrx@gmail.com>
2022-10-28 17:05:45 +0900

    feat : 대문자와 소문자에 맞게 바꾸도록 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,12 @@ function problem4(word) {
   return answer;
 }
 
+const changeAlphabet = (alphabet) => {
+  if (alphabet.charCodeAt(0) > 96) {
+    // 소문자
+  } else {
+    // 대문자
+  }
+};
+
 module.exports = problem4;
