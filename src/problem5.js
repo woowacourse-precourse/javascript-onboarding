d83d2eb0830671badc259781d7dc744b1d1d4993
Deemou <4106243@naver.com>
2022-10-28 21:30:49 +0900

    feat<P5>: 각 화폐 단위의 개수를 계산하는 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
+  const answer = countCurrency(money);
   return answer;
 }
+const countCurrency = (money) => {
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let currencyCount = [];
+  while (money) {
+    currency.forEach((item) => {
+      let count = 0;
+      count += Math.floor(money / item);
+      money %= item;
+      currencyCount.push(count);
+    });
+  }
+  return currencyCount;
+};
 
 module.exports = problem5;
