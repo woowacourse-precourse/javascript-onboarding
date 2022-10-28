f60f7b7e4621c76b497a6d8763844ec46b16f820
coolIlive <galilee155@naver.com>
2022-10-29 00:31:09 +0900

    Style(problem5): 주석과 필요없는 줄을 삭제하고 세미콜론을 통일하다
    불필요한 주석과 줄을 삭제하고 코드 마지막에 ';'를 통일했다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,11 @@
 function problem5(money) {
   let answer=new Array(9);
   const moneyType=[50000,10000,5000,1000,500,100,50,10,1];
-
-  // 그리디 알고리즘_ 큰 단위의 화폐부터 차례대로 확인하기
   for (let i=0;i<9;i++) {
     answer[i]=parseInt(money/moneyType[i]);
     money%=moneyType[i];
-  }
-
+  };
   return answer;
-}
+};
 
 module.exports = problem5;
