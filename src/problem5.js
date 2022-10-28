c92298be8e57d404e876a328bf4212d23934f0fe
leeseooo <leeseoooo@naver.com>
2022-10-28 22:39:27 +0900

    feat: problem5 금액 변환 함수 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
   let answer = [];
   const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  convertMoney(money, answer);
+  return answer;
+}
+
+function convertMoney(cur, answer) {
+  moneyList.forEach((money) => {
+    answer.push(Math.floor(cur / money));
+    cur %= money;
+  });
   return answer;
 }
 
