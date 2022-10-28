8290ba9809fd339576b74b5d282e309da36f6da3
nlom0218 <nlom0218@naver.com>
2022-10-28 15:48:09 +0900

    feat: 문제 2 - 연속하는 중복 문자인지 구분하는 함수 만들기

--- src/problem2.js
+++ src/problem2.js
@@ -1,17 +1,25 @@
+// const isOverlap = (answer, char) => {
+//   return answer[answer.length - 1] === char;
+// };
+
+// function problem2(cryptogram) {
+//   let answer = "";
+//   for (let i = 0; i < cryptogram.length; i++) {
+//     if (isOverlap(answer, cryptogram[i])) {
+//       answer = answer.slice(0, answer.length - 1);
+//       continue;
+//     }
+//     answer += cryptogram[i];
+//   }
+//   return answer;
+// }
+
+// module.exports = problem2;
+
 const isOverlap = (answer, char) => {
   return answer[answer.length - 1] === char;
 };
 
-function problem2(cryptogram) {
-  let answer = "";
-  for (let i = 0; i < cryptogram.length; i++) {
-    if (isOverlap(answer, cryptogram[i])) {
-      answer = answer.slice(0, answer.length - 1);
-      continue;
-    }
-    answer += cryptogram[i];
-  }
-  return answer;
-}
+function problem2(cryptogram) {}
 
 module.exports = problem2;
