9ea91440f79acd9cc00fb7bf8a88c3b485d8c42d
Hwang Juhee <julia0276@naver.com>
2022-10-28 23:49:04 +0900

    :sparkles: [feat] 제한사항 필터링 구현 #P1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,21 @@
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let isRestrictions = false;
+  
+  if(pobi.length!==2 || crong.length!==2){
+    isRestrictions = true;
+  } else if(pobi.includes(1)||pobi.includes(400)||crong.includes(1)||crong.includes(400)){
+    isRestrictions = true;
+  } else if(pobi[1]-pobi[0]!==1 || crong[1]-crong[0]!==1){
+    isRestrictions = true;
+  }
+
+  if (isRestrictions){
+    return -1;
+  } 
+  else {
+    
+  }
 }
 
 module.exports = problem1;
