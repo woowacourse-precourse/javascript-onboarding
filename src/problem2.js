972fc35eb4bf248800911652de4018eeca6a25ae
ekkim <eklikeable@kakao.com>
2022-10-28 12:56:01 +0900

    ✨ first implementation

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,10 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let pattern = /(\w)\1+/g;
+  let decode = cryptogram;
+  while (pattern.test(decode)) {
+    decode = decode.replace(pattern, '');
+    console.log(decode);
+  }
+  return decode ? decode : '';
 }
-
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let numbers = [];
+  for (let i = 1; i <= number; i++) {
+    numbers.push(String(i).split(''));
+  }
+  return numbers.flat().filter((e) => e === '3' || e === '6' || e === '9')
+    .length;
 }
 
 module.exports = problem3;
