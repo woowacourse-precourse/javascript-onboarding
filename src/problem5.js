ef81840242e362556da0798ff07027ce10e04559
GuHyeonWoo <ghw9174@gmail.com>
2022-10-28 16:24:59 +0900

    P5/화폐 단위 별 목과 나머지를 구하는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,14 +2,32 @@
 // 1. 결과 값의 리스트를 만드는 기능
 // 2. 화폐 단위 별 몫과 나머지를 구하는 기능
 
+// 몫과 나머지를 구하는 함수
+function divide(unit, money){
+  // 몫과 나머지를 저장
+  const share = parseInt(money / unit);
+  const remainder = money % unit;
+
+  return [share, remainder];
+}
+
 // 결과 값의 리스트를 만드는 함수
 function solution(money){
+  // 화폐 단위 리스트
+  const unit_list = [50000, 10000, 5000, 1000, 500, 100, 50, 10];
   // 결과 값을 넣을 리스트
   let result_list = [];
-  // 계산 후 나머지를 저장할 변수
+  // 값을 나눌때마다 저장할 변수
   let temp_money = money;
 
-  // 화폐 별 계산 할 부분
+  // 몫은 리스트에, 나머지는 temp_money에 저장하여 순회
+  for(unit of unit_list){
+    [share, remainder] = divide(unit, temp_money);
+    result_list.push(share);
+    temp_money = remainder;
+  }
+  // 마지막 1원 단위는 그대로 저장
+  result_list.push(temp_money);
 
   return result_list;
 }
