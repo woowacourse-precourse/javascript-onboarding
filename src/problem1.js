0a36c604af0124ff824084e6732849a0a66725cd
Raehyeon Jeong <raehy19@gmail.com>
2022-10-28 16:45:00 +0900

    feat(problem1) : 점수 계산 기능

--- src/problem1.js
+++ src/problem1.js
@@ -13,6 +13,33 @@ function isException(arg) {
   );
 }
 
+function calSum(num) {
+  const stringNum = num.toString();
+  let sum = 0;
+
+  for (let i = 0; i < stringNum.length; ++i) {
+    sum += parseInt(stringNum.charAt(i));
+  }
+  return sum;
+}
+
+function calProduct(num) {
+  const stringNum = num.toString();
+  let product = 1;
+
+  for (let i = 0; i < stringNum.length; ++i) {
+    product *= parseInt(stringNum.charAt(i));
+  }
+  return product;
+}
+
+function calScore(arg) {
+  return Math.max(
+    Math.max(calSum(arg[0]), calProduct(arg[0])),
+    Math.max(calSum(arg[1]), calProduct(arg[1])),
+  );
+}
+
 function problem1(pobi, crong) {
   if (isException(pobi) || isException(crong)) return -1;
 }
