f4e5a1347b7405067227338a2801def7cefab538
minsoo <als494@naver.com>
2022-10-28 23:09:02 +0900

    5번 문제 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,19 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+
+  let unitOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  unitOfMoney.map((unit) => {
+    if (money >= unit) {
+      let cnt = 0;
+      cnt = Math.floor(money / unit);
+      answer.push(cnt);
+      money -= cnt * unit;
+    } else {
+      answer.push(0);
+    }
+  });
+
   return answer;
 }
 
