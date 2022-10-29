d15123a9c6f8dd2321290d857c200d5fadf72eca
js43o <js43o@naver.com>
2022-10-29 16:27:41 +0900

    Feat: Add problem5 code.

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < unit.length; i++) {
+    if (money >= unit[i]) {
+      answer[i] = parseInt(money / unit[i]);
+      money %= unit[i];
+    }
+  }
+
   return answer;
 }
 
