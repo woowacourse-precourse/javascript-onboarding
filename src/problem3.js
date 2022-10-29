231f699df4f098c745f721e7234d4f9424a76bd5
jeonbyeongmin <qudals7613@gmail.com>
2022-10-29 18:16:56 +0900

    Clear `problem3`

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,24 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const eachNumbers = separateNumber(i);
+
+    eachNumbers.forEach((n) => {
+      if (n === 3 || n === 6 || n === 9) {
+        count = count + 1;
+      }
+    });
+  }
+
+  return count;
+}
+
+function separateNumber(number) {
+  const strNumber = String(number);
+  const eachNumbers = [...strNumber].map((value) => Number(value));
+
+  return eachNumbers;
 }
 
 module.exports = problem3;
