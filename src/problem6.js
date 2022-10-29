20cf8aaee5323778e48ce8346de7161ce4e860b3
박승훈 <tmdgns1126@naver.com>
2022-10-29 22:16:48 +0900

    [P6] Feat: 2글자씩 key로 하여 포함하는 유저의 이메일을 모은 Object를 만드는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  // 닉네임의 2글자를 key로 하여 포함하는 모든 유저의 이메일을 set으로 저장
+  // 이를 모아 Object로 반환
+  const createDuplicationObject = (forms) => {
+    const duplications = {};
+    forms.forEach(([email, name]) => {
+      const nameLength = name.length;
+      const nameCharList = name.split("");
+      for (let i = 0; i < nameLength - 1; i++) {
+        const part = nameCharList.slice(i, i + 2).join("");
+        if (!duplications[part]) {
+          duplications[part] = new Set([email]);
+        } else {
+          duplications[part].add(email);
+        }
+      }
+    });
+    return duplications;
+  };
 }
 
 module.exports = problem6;
