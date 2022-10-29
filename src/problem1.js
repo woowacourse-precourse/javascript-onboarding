93a117f0d87b43c8bbcd382f81dd5062a7ff2d0c
unknown <sjjoseph106@gmail.com>
2022-10-29 16:01:40 +0900

    :sparkles: problem1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,12 +1,31 @@
 // @ts-check
 
 /**
+ * @param {number} page
+ * @returns {number}
+ */
+const findMax = (page) => {
+  return Math.max(
+    `${page}`.split("").reduce((acc, cur) => acc + Number(cur), 0),
+    `${page}`.split("").reduce((acc, cur) => acc * Number(cur), 1)
+  );
+};
+
+/**
  * @param {number[]} pobi
  * @param {number[]} crong
  * @returns {number}
  */
 function problem1(pobi, crong) {
-  return 0;
+  if (pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1]) return -1;
+
+  const pobiMax = Math.max(findMax(pobi[0]), findMax(pobi[1]));
+  const crongMax = Math.max(findMax(crong[0]), findMax(crong[1]));
+
+  if (pobiMax > crongMax) return 1;
+  else if (pobiMax < crongMax) return 2;
+  else if (pobiMax === crongMax) return 0;
+  else return -1;
 }
 
 console.log(problem1([97, 98], [197, 198])); // 0
