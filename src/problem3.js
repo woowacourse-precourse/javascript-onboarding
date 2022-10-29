d762e84c1a0cfa3b87782f227d225ed970daf2df
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-29 23:07:52 +0900

    fix: [문제3] 재귀 스택오버플로우 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,24 +1,25 @@
 function problem3(number) {
-  if (typeof number !== 'number' || number < 1 || number > 1000) {
+  const threeSixNine = ['3', '6', '9'];
+  const getThreeSixNineCountUntilZero = (number) => {
+    let count = 0;
+    do {
+      [...String(number)].forEach((digitNumber) => {
+        if (threeSixNine.includes(digitNumber)) {
+          count = count + 1;
+        }
+      });
+      number = number - 1;
+    } while (number > 0);
+    return count;
+  };
+
+  if (typeof number !== 'number' || number < 1 || number > 10000) {
     throw new Error(
       'number는 숫자형만 가능하며, 1이상 10,000이하인 자연수만 가능합니다.'
     );
   }
 
-  const container369 = ['3', '6', '9'];
-  const count369 = (number, count) => {
-    if (number === 0) {
-      return count;
-    }
-
-    [...String(number)].forEach((character) => {
-      if (container369.includes(character)) {
-        count++;
-      }
-    });
-    return count369(number - 1, count);
-  };
-  return count369(number, 0);
+  return getThreeSixNineCountUntilZero(number);
 }
 
 module.exports = problem3;
