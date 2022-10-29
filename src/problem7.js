d1791105f106ac8c059fee24831e2d42f1d37610
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-29 22:07:16 +0900

    feat: (P7) 임시 친구추천 배열에서 사용자 중복제거 및 점수 10점 부여

--- src/problem7.js
+++ src/problem7.js
@@ -23,12 +23,27 @@ function problem7(user, friends, visitors) {
     .filter((friend) => !alreadyFriends.includes(friend));
 
   // 임시 친구추천 배열에서 사용자 중복제거 & 점수 +10점 부여
+  let recommendationFriends = new Map();
+  tempRecommendationFriends.map((tempRecommendationFriend) => {
+    // 친구추천에 등록돼 있는 경우 +10점
+    if (recommendationFriends.has(tempRecommendationFriend)) {
+      let recommendationFriendScore = recommendationFriends.get(
+        tempRecommendationFriend
+      );
+      return recommendationFriends.set(
+        tempRecommendationFriend,
+        recommendationFriendScore + 10
+      );
+    }
+    // 친구추천에 등록돼 있지 않은 경우 신규 등록
+    return recommendationFriends.set(tempRecommendationFriend, 10);
+  });
 
   // visitor중 유저나 이미 친구인 사용자 제외
 
-  // 임시 친구추천 배열에 등록된 visitor는 점수 +1점 부여
+  // 친구추천에 등록된 visitor는 점수 +1점 부여
 
-  // 임시 친구추천 배열에 등록되지 않은 visitor는 신규 등록 (점수 1점 부여)
+  // 친구추천에 등록되지 않은 visitor는 신규 등록 (점수 1점 부여)
 }
 
 module.exports = problem7;
