c41692f5c91df298b4cd7cbdba0d5e2fd9568718
SeungMiL <tmskqj@naver.com>
2022-10-29 20:01:27 +0900

    problem5 테스트 통과

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < currency.length; i++) {
+    answer.push(Math.floor(money / currency[i]));
+    money = money % currency[i];
+  }
+
   return answer;
 }
 
