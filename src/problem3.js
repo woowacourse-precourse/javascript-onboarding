fe769cdfb5c268190405bfe6c08ca7cb6d9b5dad
kaori-killer <sksbthwjd@daum.net>
2022-10-29 16:38:44 +0900

    숫자에 3또는 6또는 9가 들어있는지 확인

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for(let nowNumber = 1; nowNumber < number + 1; nowNumber++){
+    nowNumberArray = nowNumber.toString().split("");
+    if(nowNumberArray.includes("3") || nowNumberArray.includes("6") || nowNumberArray.includes("9")){
+      count += 1;
+    }
+  }
+  return count;
 }
+
+console.log(problem3(13));
 
 module.exports = problem3;
