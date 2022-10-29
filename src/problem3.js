95b8f472916f3959096bbf4e41f1ea8c1133b081
kknyapple <kknyapple@gmail.com>
2022-10-30 04:56:34 +0900

    problem3 - 손뼉 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -2,9 +2,24 @@ const isDiviedThree = (num) => {
   if (num != 0 && num % 3 == 0) return true;
 };
 
+const countClap = (num) => {
+  let clap = 0;
+
+  for (let i = 1; i < num + 1; i++) {
+    let currentNum = i;
+
+    while (currentNum > 0) {
+      let digitNum = currentNum % 10;
+      if (isDiviedThree(digitNum) == true) clap++;
+      currentNum = parseInt(currentNum / 10);
+    }
+  }
+  return clap;
+};
+
 function problem3(number) {
   var answer;
-
+  answer = countClap(number);
   return answer;
 }
 
