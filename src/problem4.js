585a0360a150b034df15e8e4b8363faef26ad202
parkjikuk <paxkk@naver.com>
2022-10-30 05:27:36 +0900

    feat : 알파벳이 담긴 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
+function reverseWord(char) {
+  const upperArr = Array.from({ length : 26 }, (v, i) => String.fromCharCode(i + 65));
+  const lowerArr  = Array.from({ length : 26 }, (v, i) => String.fromCharCode(i + 97));
+  const reverseUpper = [...upperArr].reverse();
+  const reverseLower = [...lowerArr].reverse();
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const strArr = [...word];
+  const reverse = strArr.map((char) => reverseWord(char));
+
+  return reverse.join("");
 }
 
 module.exports = problem4;
