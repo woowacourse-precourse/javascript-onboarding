b55af6c22a420351776695f02b60b01fae37d7a1
cofla <cofla159@naver.com>
2022-10-30 04:20:04 +0900

    p3 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for (let i = 1; i < number + 1; i++) {
+    if (i.toString().match(/[369]/g)) {
+      answer += i.toString().match(/[369]/g).length;
+    }
+  }
   return answer;
 }
 
 module.exports = problem3;
+/*
+1. answer = 0에서 시작
+2. number.includes(3) 이면 ++
+*/
