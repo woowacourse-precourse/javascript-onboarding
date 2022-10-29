7d2cb4e065c20c5d0dbf29b1a580365667e652ae
TaeeunKim <xodms0309@naver.com>
2022-10-29 21:03:27 +0900

    [6번] feat: 자른 닉네임이 몇명씩 겹치는지 객체에 넣음

--- src/problem6.js
+++ src/problem6.js
@@ -1,14 +1,25 @@
 function problem6(forms) {
   var answer;
   const crew = [];
-  const splittedNicknames = new Set();
+  const duplicateNicknames = {};
 
   for (const [email, nickname] of forms) {
     crew.push({ email: email, nickname: nickname });
     //2글자씩 자른 닉네임을 set에 넣음
+    const splittedNicknames = new Set();
     for (let i = 0; i < nickname.length - 1; i++) {
       const slicedNickname = nickname.slice(i, i + 2);
       splittedNicknames.add(slicedNickname);
+    }
+
+    //set을 순회하면서 자른 닉네임이 몇 명이 겹치는지 카운트
+    for (const splittedNickname of splittedNicknames) {
+      if (!nickname.includes(splittedNickname)) continue;
+      if (duplicateNicknames[splittedNickname]) {
+        duplicateNicknames[splittedNickname] += 1;
+      } else {
+        duplicateNicknames[splittedNickname] = 1;
+      }
     }
   }
 
