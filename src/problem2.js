33740c96b770d1531f6ea49f64cd9c4ed0e1e7aa
confidential-nt <youyoy689@gmail.com>
2022-10-30 05:11:19 +0900

    요구사항 2번 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,45 @@
+function findDuplicated(splited) {
+  const duplicated = [];
+
+  let pre = splited[0];
+
+  for (let i = 1; i < splited.length; i++) {
+    if (splited[i] === pre) {
+      if (!duplicated[i - 1]) {
+        duplicated[i] = 1;
+        duplicated[i - 1] = 1;
+      } else {
+        duplicated[i] = 1;
+      }
+    }
+
+    pre = splited[i];
+  }
+
+  return duplicated;
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (cryptogram.length > 1000 || cryptogram.length < 1) return;
+  if (cryptogram.match(/([^A-Za-z])+/g)) return;
+
+  let string = cryptogram.toLowerCase();
+  let splited = string.split("");
+
+  while (true) {
+    const duplicated = findDuplicated(splited);
+
+    if (!duplicated.length) break;
+    for (let i = 0; i < duplicated.length; i++) {
+      if (duplicated[i]) {
+        splited.splice(i, 1, null);
+      }
+    }
+    string = splited.join("");
+    splited = string.split("");
+  }
+
+  return string;
 }
 
 module.exports = problem2;
