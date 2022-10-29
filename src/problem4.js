73cd10c29d6aa6b5755b5c5c31682f327a172d4e
jsh3418 <albert5428@gmail.com>
2022-10-29 11:53:30 +0900

    feat: problem4 대문자와 소문자 배열 할당, 대문자와 소문자를 뒤집은 배열 할당

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,36 @@
 function problem4(word) {
   // 대문자, 소문자 알파벳 배열을 만든다.
+  const upperCaseArr = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const lowerCaseArr = upperCaseArr.join("").toLowerCase().split("");
+  const reversedUpperCaseArr = upperCaseArr.slice().reverse();
+  const reversedLowerCaseArr = lowerCaseArr.slice().reverse();
   // word를 순회하며 알파벳의 경우 배열의 인덱스를 이용해 청개구리의 말로 바꾸어 반환한다.
 }
 
