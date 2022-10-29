9872387789ebcbed036f236f2582d0702f23dbe5
oxhwixo <hwi_030@naver.com>
2022-10-29 16:36:34 +0900

    Problem1: 각 자리수 곱하는 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -8,6 +8,16 @@ function add(page) {
   return number;
 }
 
+/** 입력받은 숫자의 각 자리수를 곱한 값을 반환하는 함수 **/
+function multiply(page) {
+  var number;
+  while (page > 0) {
+    number = number * (page % 10);
+    page = page / 10;
+  }
+  return number;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
