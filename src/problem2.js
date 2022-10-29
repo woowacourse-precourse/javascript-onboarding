0e4bed836c19ea910a4ba4438bfef8d93e724836
Choejeonghun <saa00123@naver.com>
2022-10-29 10:45:56 +0900

    feat: 문제 2번 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,16 @@
 function problem2(cryptogram) {
   var answer;
+  var stack = [];
+
+  for (var i = 0; i < cryptogram.length; i++) {
+    stack.push(cryptogram[i]);
+    if (stack[stack.length - 1] === stack[stack.length - 2]) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  answer = stack.join('');
+
   return answer;
 }
 
