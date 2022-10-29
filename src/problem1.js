ea760c3dac061b426e9e8227d197039e066560eb
oxhwixo <hwi_030@naver.com>
2022-10-29 17:34:31 +0900

    Proble1: 예외처리 추가 & multiply 함수 반환값이 0으로 고정되는 오류 수정

--- src/problem1.js
+++ src/problem1.js
@@ -10,7 +10,7 @@ function add(page) {
 
 /** 입력받은 숫자의 각 자리수를 곱한 값을 반환하는 함수 **/
 function multiply(page) {
-  var number = 0;
+  var number = 1;
   while (page > 0) {
     number = number * (page % 10);
     page = Math.floor(page / 10);
@@ -29,6 +29,20 @@ function findLargeNumber(name) {
 
 function problem1(pobi, crong) {
   var answer;
+
+  if (pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1]) return -1;
+  for (var i = 0; i < 2; i++) {
+    if (typeof pobi[i] != "number" || typeof crong[i] != "number") return -1;
+  }
+
+  scorePobi = findLargeNumber(pobi);
+  scoreCrong = findLargeNumber(crong);
+
+  if (scorePobi > scoreCrong) answer = 1;
+  else if (scorePobi < scoreCrong) answer = 2;
+  else if (scorePobi === scoreCrong) answer = 0;
+  else answer = -1;
+
   return answer;
 }
 
