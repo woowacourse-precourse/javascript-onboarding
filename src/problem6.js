69ad122a5191e88d43e83c241d70d86fe31a9a7f
TaeeunKim <xodms0309@naver.com>
2022-10-29 19:35:33 +0900

    [6번] feat: 크루들의 이메일과 닉네임을 묶은 객체 선언

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,10 @@
 function problem6(forms) {
   var answer;
+  const crew = [];
+  for (const [email, nickname] of forms) {
+    crew.push({ email: email, nickname: nickname });
+  }
+
   return answer;
 }
 
