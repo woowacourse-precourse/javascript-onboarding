c9c0875b1aaff63974f8a8a7f7791bef2d1dfdde
woo-jk <201702042@o.cnu.ac.kr>
2022-10-29 21:05:32 +0900

    feat:[PROBLEM6] 중복된 크루 명단 구하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,44 @@
+const splitName = (crewName) => {
+  const splittedLetter = [];
+  for (
+    let firstLetterIdx = 0;
+    firstLetterIdx < crewName.length - 1;
+    firstLetterIdx++
+  ) {
+    splittedLetter.push(crewName.substr(firstLetterIdx, 2));
+  }
+  return splittedLetter;
+};
+
+const isDuplicated = (forms, currentIdx) => {
+  let duplicate = false;
+  const currentCrew = forms[currentIdx];
+  const currentCrewName = currentCrew[1];
+  const splittedName = splitName(currentCrewName);
+
+  forms.forEach((otherCrew, idx) => {
+    if (duplicate) return;
+    if (idx === currentIdx) return;
+
+    const otherCrewName = otherCrew[1];
+    splittedName.forEach((word) => {
+      if (otherCrewName.includes(word)) duplicate = true;
+    });
+  });
+  return duplicate;
+};
+
+const getDuplicateNames = (forms) => {
+  const duplicatedCrew = [];
+  forms.forEach((crew, idx) => {
+    if (isDuplicated(forms, idx)) duplicatedCrew.push(crew);
+  });
+  return duplicatedCrew;
+};
+
 function problem6(forms) {
   var answer;
+  console.log(getDuplicateNames(forms));
   return answer;
 }
 
