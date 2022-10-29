fa3e9305042096602cedd54690f53065a09419cc
daal2quf <daal2quf@gmail.com>
2022-10-30 06:25:51 +0900

    문자열 돌면서 중복제거 (할것: 예외처리)

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
   var answer;
+  let word = [];
+  let trash = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (word[word.length - 1] === cryptogram[i]) {
+      word.pop()
+      trash.push(cryptogram[i])
+    } else {
+      word.push(cryptogram[i])
+      console.log(word)
+    }
+    
+  }
+  answer = word.join('');
   return answer;
 }
 
