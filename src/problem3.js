b5424bbd663ce2a2365f37fb5018a40014a95bfa
2wndrhs <2wndrhs@gmail.com>
2022-10-29 00:02:33 -0700

    feat(PROBLEM3): 3, 6, 9가 포함되어 있는 숫자의 개수를 찾는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -9,10 +9,18 @@ function splitNumbers(numbers) {
   return splitedNumbers;
 }
 
+// 분리한 숫자들 중에서 3, 6, 9가 포함되어 있는 숫자의 개수를 찾는 기능
+function findNumber(source, number) {
+  const result = source.reduce(
+    (acc, cur) => (cur.includes(number) ? ++acc : acc),
+    0
+  );
+  return result;
+}
 // solution
 function solution(number) {
   const sequences = Array.from({ length: number }, (_, i) => i);
-  console.log(splitNumbers(sequences));
+  const splitedNumbers = splitNumbers(sequences);
 }
 
 function problem3(number) {
