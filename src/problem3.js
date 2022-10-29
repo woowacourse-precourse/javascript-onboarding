e2b8ba933a76dba8627544ad970f443f1459a80f
sunny <huiseon37@gmail.com>
2022-10-30 02:46:31 +0900

    [problem3] 각 숫자별 3,6,9 포함여부 함수 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,28 @@
+function getClapNum(number){
+  const clapArr = Array.from(number.toString(),(num) => Number(num)).reduce((acc,cur)=>{
+    if(cur === 3 || cur === 6 || cur === 9){
+      return [...acc, cur];
+    }
+    return acc;
+  },[]);
+
+  return clapArr.length;
+}
+
+function getEntireClapNum(number){
+  let cnt = 0;
+
+  for(let i=1; i<=number; i++){
+    const clapNum = getClapNum(i);
+    cnt = cnt + clapNum;
+  }
+  
+  return cnt;
+}
+
 function problem3(number) {
-  var answer;
+  const answer = getEntireClapNum(number);
+  
   return answer;
 }
 
