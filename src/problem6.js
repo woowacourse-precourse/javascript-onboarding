81c4d6c95333d29bd2ab52a1598d2df78a7a65f7
sery <yunsel2@gmail.com>
2022-10-29 20:20:03 +0900

    feat(Problem6): Get email array, nickname array

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,9 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+
+  const emailList = forms.map((form) => form[0]);
+  const nicknameList = forms.map((form) => form[1]);
+
   return answer;
 }
 
