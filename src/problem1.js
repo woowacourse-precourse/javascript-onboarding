112ff643d62a274c26b4f8e83490eea73f2c9ca8
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-29 20:15:27 +0900

    [문제1] <6단계 기능요구사항> 예외사항이 존재하면 -1을 반환하고 종료

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,19 @@
 function problem1(pobi, crong) {
-    // 1단계) 입력받은 배열의 요소를 알기 쉬운 변수명으로 저장
+    // 1단계) 입력받은 길이 2배열의 요소들을 각각의 변수에 저장
     [pobiLeftPage, pobiRightPage, crongLeftPage, crongRightPage] = [undefined, undefined, undefined, undefined];
     if (Array.isArray(pobi)) {
         [pobiLeftPage, pobiRightPage, crongLeftPage, crongRightPage] = [pobi[0], pobi[1], crong[0], crong[1]];
+    }
+
+    // 6단계) 게임시작 전에 예외사항이 발생하면 -1을 반환하고 게임을 종료
+    if (pobiLeftPage <= 1 || pobiRightPage <= 1 || crongLeftPage <= 1 || crongRightPage <= 1) {
+        return -1;
+    }
+    if (pobiLeftPage >= 400 || pobiRightPage >= 400 || crongLeftPage >= 400 || crongRightPage >= 400) {
+        return -1;
+    }
+    if (pobiRightPage - pobiLeftPage !== 1 || crongRightPage - crongLeftPage !== 1) {
+        return -1;
     }
 
     /** 2~3단계) 각 자리 숫자를 모두 더하는 함수 */
@@ -27,12 +38,26 @@ function problem1(pobi, crong) {
 
     /**  4단계) 가장 큰 수를 구하는 함수 */
     const getBigScore = (nameLeftPage, nameRightPage) => {
-        return Math.max(sum(nameLeftPage), sum(nameRightPage), multiply(nameLelftPage), multiply(nameRightPage));
+        return Math.max(sum(nameLeftPage), sum(nameRightPage), multiply(nameLeftPage), multiply(nameRightPage));
     };
 
-    // 4단계) 본인의 점수를 구합니다.
+    // 4단계) 본인의 점수를 구함
     const pobiScore = getBigScore(pobiLeftPage, pobiRightPage);
     const crongScore = getBigScore(crongLeftPage, crongRightPage);
+
+    // 5단계) 요구사항에 따라서 게임의 승자를 구함. 1:pobi승리 2:crong승리 0:무승부
+    if (pobiScore > crongScore) {
+        console.log(1);
+        return 1;
+    }
+    if (pobiScore < crongScore) {
+        console.log(2);
+        return 2;
+    }
+    if (pobiScore === crongScore) {
+        console.log(0);
+        return 0;
+    }
 }
 
 module.exports = problem1;
