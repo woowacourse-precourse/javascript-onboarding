dcd63a88280bd58b1869c74c4fa8a776b1d70c9c
2yunseong <dbsdltjd3701@naver.com>
2022-10-29 12:18:07 +0900

    feat(문제 2번): 문자열 내 중복이 있는 부분을 탐색하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,21 @@
+function findOverlapStringIndex(str, base) {
+  let offset = base + 1;
+  if (offset > str.length) return base;
+  if (str[base] !== str[offset]) return base;
+  return findOverlapStringIndex(str, offset);
+}
 function problem2(cryptogram) {
   var answer;
   let base = 0; // 중복검사 때 사용하는 base index
   let target = cryptogram;
 
-  while (true) {
+  while (base < target.length - 1) {
     // 중복 검사로직 작성
+    let offset = findOverlapStringIndex(target, base);
+    if (offset === base) {
+      base++;
+      continue;
+    }
     // 새 문자열 로직 작성
   }
   return answer;
