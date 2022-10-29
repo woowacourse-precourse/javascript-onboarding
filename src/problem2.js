d509aa3b31943d35f335529c43b8cc805c364b24
kaori-killer <sksbthwjd@daum.net>
2022-10-29 16:07:57 +0900

    feat: 중복 문자열이 발생하지 않을 때까지 제거를 반복한다.

--- src/problem2.js
+++ src/problem2.js
@@ -5,8 +5,9 @@ function deleteString(cryptogram){
   let count = 0;
 
   for (let i = 0; i < stringLength; i++){
+    if(check[i] == 1) continue;
     
-    if((cryptogram[i] == cryptogram[i + 1]) && (i != stringLength - 1)){
+    if((cryptogram[i] == cryptogram[i + 1])){
       count += 1;
       check[i] = 1;
       check[i+1] = 1;
@@ -16,7 +17,8 @@ function deleteString(cryptogram){
     }
   }
 
-  return newCryptogram;
+  if(count == 0){ return newCryptogram; }
+  else { return deleteString(newCryptogram); }
 }
 
 function problem2(cryptogram) {
