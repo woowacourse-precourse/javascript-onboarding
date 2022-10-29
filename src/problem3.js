ca1d09bb34b14052eb019467ed55fcbecfe2f60a
kaori-killer <sksbthwjd@daum.net>
2022-10-29 16:57:47 +0900

    feat: 숫자에 3 또는 6 또는 9가 몇 개 들어있는지 확인

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,14 @@
 function problem3(number) {
-  let count = 0;
+  let resultCount = 0;
   for(let nowNumber = 1; nowNumber < number + 1; nowNumber++){
     nowNumberArray = nowNumber.toString().split("");
-    if(nowNumberArray.includes("3") || nowNumberArray.includes("6") || nowNumberArray.includes("9")){
-      count += 1;
-    }
+    nowNumberArray.forEach(element => {
+      if(element === '3' || element == '6' || element == '9') { resultCount += 1 }
+    });
   }
-  return count;
+  return resultCount;
 }
 
-console.log(problem3(13));
+console.log(problem3(33));
 
 module.exports = problem3;
