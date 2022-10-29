7badd55f4a48bf5faf71df714df77560dd03af6e
haesa <xdhaesa@gmail.com>
2022-10-29 16:34:29 +0900

    problem7 친구 추천 점수 계산 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -16,6 +16,25 @@ function getFriendsOfFriends(user, userFriends, friends) {
   return friensdOfUserFriends;
 }
 
+function getRecomendScore(user, friends, visitors) {
+  const userFriends = getUserFriends(user, friends);
+  const friendsOfUserFriends = getFriendsOfFriends(user, userFriends, friends);
+  const list = new Set([...friends.flat(), ...visitors]);
+  const notUserFriends = [...list].filter(
+    (x) => x !== user && !userFriends.includes(x)
+  );
+  const recommendFriends = new Map();
+  notUserFriends.forEach((x) => recommendFriends.set(x, 0));
+
+  friendsOfUserFriends.forEach((x) =>
+    recommendFriends.set(x, recommendFriends.get(x) + 10)
+  );
+  visitors = visitors.filter((x) => !userFriends.includes(x));
+  visitors.forEach((x) => recommendFriends.set(x, recommendFriends.get(x) + 1));
+
+  return recommendFriends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
