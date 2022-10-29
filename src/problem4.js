936d7b53c266e6d643f9210fbc5123d4773f602e
semnil5202 <semnil5202@gmail.com>
2022-10-30 01:07:35 +0900

    [문제 4번] feat: 엄마 말씀(알파벳) 사전을 배열로 만들기

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,11 @@ function problem4(word) {
   return answer;
 }
 
+function makeAlphabetArray() {
+  const alphabet = new Array(26)
+    .fill()
+    .map((_, i) => String.fromCharCode(i + 65));
+  return alphabet;
+}
+
 module.exports = problem4;
