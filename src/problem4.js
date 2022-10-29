fe82f47241365ba10ee4bed0f2f20fae3a932229
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-29 22:56:56 +0900

    feat: PROBLEM 4 문자열 길이 예외 사항

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,8 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const check = (word) => {
+    if (word.length < 1 || 1000 < word.length) return false;
+    return true;
+  }
 }
 
 module.exports = problem4;
