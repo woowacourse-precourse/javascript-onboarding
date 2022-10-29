601868bd7ef7053d0b4f03a52c5f8c5ba5245caa
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-30 03:22:31 +0900

    [problem7] feat: user의 친구를 찾는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,16 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function user_friends(user, friends) {
+  const known_friends = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      known_friends.push(friends[i].filter((el) => el !== user));
+    }
+  }
+
+  return known_friends;
+}
+
 module.exports = problem7;
