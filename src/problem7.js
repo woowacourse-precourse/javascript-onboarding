0c6f215475ed6c3268302ac0a0cd05d400644bd7
BBIYAC <kiy7833@gmail.com>
2022-10-29 15:33:59 +0900

    feat(problem7): add acquaintance feature
    - 사용자와 함께 아는 친구의 수 카운트하여 추천 점수에 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,26 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let recommend_friends = {},
+      acquaintance = {};
+
+  friends.forEach(([friend1, friend2]) => {
+    if (friend2 !== user) {
+      if (acquaintance[friend1]) acquaintance[friend1].push(friend2);
+      else acquaintance[friend1] = [friend2];
+    }
+    if (friend1 !== user) {
+      if (acquaintance[friend2]) acquaintance[friend2].push(friend1);
+      else acquaintance[friend2] = [friend1];
+    }
+  });
+  
+  acquaintance[user].forEach(friend => {
+    acquaintance[friend].forEach(recommand => {
+      if (recommend_friends[recommand]) recommend_friends[recommand] += 10;
+      else recommend_friends[recommand] = 10;
+    })
+  })
+  
+  return recommend_friends;
 }
 
 module.exports = problem7;
