bc9e03e016e5c054dab3c873bbc04f9da529b1b0
Yunjae <nuguri206@gmail.com>
2022-10-29 15:08:55 +0900

    feat: [prob1] 포비, 크롱 각각 최대 값

--- src/problem1.js
+++ src/problem1.js
@@ -9,25 +9,42 @@ function problem1(pobi, crong) {
     else return true;
   };
 
-//책 페이지 합산 값
-const getSumNum  = (book) => {
-  const bookString  = book.toString().split("");
-  let sumNum = 0;
-  for(let i=0; i<bookString.length; i++){
-    sumNum += Number(bookString[i])
-  }
-  return sumNum;
-};
+  //책 페이지 합산 값
+  const getSumNum  = (book) => {
+    const bookString  = book.toString().split("");
+    let sumNum = 0;
+    for(let i=0; i<bookString.length; i++){
+      sumNum += Number(bookString[i])
+    }
+    return sumNum;
+  };
 
-//책 페이지 곱한 값
-const getmultNum  = (book)  => {
-  const bookString  = book.toString().split("");
-  let mulNum = 1;
-  for(let i=0; i<bookString.length; i++){
-    mulNum *= Number(bookString[i])
-  }
-  return mulNum;
-};
+  //책 페이지 곱한 값
+  const getmultNum  = (book)  => {
+    const bookString  = book.toString().split("");
+    let mulNum = 1;
+    for(let i=0; i<bookString.length; i++){
+      mulNum *= Number(bookString[i])
+    }
+    return mulNum;
+  };
+
+  //포비, 크롱의 가장 큰 값
+  const maxNum = (book) => {
+    let max = 0;
+
+    book.forEach((page) => {
+      let sum = getSumNum (page);
+      let mul = getmultNum(page);
+      if (sum >= mul){
+        max = sum;
+      }
+      if (sum < mul){
+        max = mul;
+      }
+    });
+    return max;
+  };
     
 }
 
