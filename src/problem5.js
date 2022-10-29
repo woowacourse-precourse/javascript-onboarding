eac38cad39b4d905e24b5a0513834e46301716ac
ryuna <anottrx@gmail.com>
2022-10-29 22:23:22 +0900

    feat : 만든 배열을 이용해 money를 나누는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,21 @@
-const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+const billCoinList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  var answer;
+  var answer = billCoinList.map((billCoin) => {
+    const result = countMoney(money, billCoin);
+    money = result[0];
+    return result[1];
+  });
   return answer;
 }
+
+const countMoney = (money, billCoin) => {
+  return money >= billCoin
+    ? [
+        money - Math.floor(money / billCoin) * billCoin,
+        Math.floor(money / billCoin),
+      ]
+    : [money, 0];
+};
 
 module.exports = problem5;
