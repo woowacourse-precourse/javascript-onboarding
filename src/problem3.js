14dccd8445d40ab2079b05c73993e6d1181f11ea
yesolyo <ahnyelim77@gmail.com>
2022-10-29 11:48:58 +0900

    369갯수만큼 숫자세기

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
   var answer;
+  answer = 0;
+
+  //369 갯수만큼 숫자세기
+  for (let i = 1; i < number + 1; i++) {
+    var set_number = "" + i;
+    for (var j = 0; j < set_number.length; j++) {
+      if (set_number[j] == 3 || set_number[j] == 6 || set_number[j] == 9) {
+        answer += 1;
+      }
+    }
+  }
   return answer;
 }
 
