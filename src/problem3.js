6670bc538bac2a005fc5b70171c1e544b3eeecca
2je0 <bulljy@naver.com>
2022-10-29 19:19:02 +0900

    ✨ feat(problem3): 어떠한 숫자가 들어왔을 때 쳐야하는 박수 횟수를 반환하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -2,9 +2,16 @@ function is369(number) {
   if (number !== 0 && number % 3 === 0) return 1;
   else return 0;
 }
+function countClap(number) {
+  let ret = 0;
+  while (number) {
+    if (is369(number % 10)) ret++;
+    number = Math.trunc(number / 10);
+  }
+  return ret;
+}
 function problem3(number) {
   var answer;
   return answer;
 }
-
 module.exports = problem3;
