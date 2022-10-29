619aee24ed5f6a4ceea3bcdb77e5035533f41b69
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-29 23:10:19 +0900

    Feat(문제3-기능1): 3,6,9 갯수를 반환하는 함수(getClapNumber) 구현

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,19 @@
 // 1. 각 숫자에대하여 3,6,9의 개수를 반환하는 함수
 // 2. 1부터 number까지 3,6,9의 개수를 누적하여 더함
 
+function getClapNumber(number) {
+  const arr_369 = ["3", "6", "9"];
+  const str_number = number.toString();
+  const arr_number = str_number.split("");
+  let count = 0;
+  arr_number.forEach((ele) => {
+    if (arr_369.includes(ele)) {
+      count++;
+    }
+  });
+  return count;
+}
+
 function problem3(number) {
   var answer;
   return answer;
