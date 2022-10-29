a71e8b5533ca63f213baf9b458c519c0f8a9514e
su-lim <sujin.0xlim@gmail.com>
2022-10-29 16:00:40 +0900

    feat: implement prob5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let unitOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1] ;
+
+  let change = money;
+  for(let i=0; i<unitOfMoney.length; i++){
+    let quotient = parseInt(change/unitOfMoney[i]);
+    change = change%unitOfMoney[i];
+    answer.push(quotient);
+  }
+
   return answer;
 }
 
