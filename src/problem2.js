d0c450c549d5246eb447443310e5df17b71b7192
100Gyeon <bjy9966@naver.com>
2022-10-29 22:12:47 +0900

    feat: push와 pop을 반복하며 연속하는 중복 문자들을 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const uniqueList = [];
+  const characterList = cryptogram.split('');
+
+  characterList.forEach((character) => {
+    character === uniqueList[uniqueList.length - 1] ? uniqueList.pop() : uniqueList.push(character);
+  });
+
+  return uniqueList.join('');
 }
 
 module.exports = problem2;
