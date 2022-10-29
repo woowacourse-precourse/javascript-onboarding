96bc9b8f8242793f79ebce5153bd368aeee5ff42
curomame <curomame@naver.com>
2022-10-29 19:00:52 +0900

    🛠 fix : 변경된 알파벳을 반환하는 함수 수정

--- src/problem4.js
+++ src/problem4.js
@@ -29,7 +29,7 @@ function problem4(word) {
       outputASCII = ASCII_OBJ.z - (inputASCII - ASCII_OBJ.a);
     }
 
-    if (inputASCII > ASCII_OBJ.m && inputASCII < ASCII_OBJ.n) {
+    if (inputASCII >= ASCII_OBJ.m && inputASCII <= ASCII_OBJ.z) {
       outputASCII = ASCII_OBJ.a + (ASCII_OBJ.z - inputASCII);
     }
 
@@ -40,8 +40,8 @@ function problem4(word) {
   //!1. word의 길이는 1이상 1000 이하인 문자열
   //!2. 알파벳 외의 문자는 변환하지 않음
   //!3. 알파벳 대문자는 대문자, 소문자는 소문자로 변환
-  var answer;
-  return answer;
+
+
 }
 
 module.exports = problem4;
