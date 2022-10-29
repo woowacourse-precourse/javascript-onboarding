4367040ca23dd605f68fa5403f261dc8047db134
hyukzz <ri_chard@naver.com>
2022-10-29 23:30:44 +0900

    feat: [문제5]화폐 단위 구분 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
+const UnitMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+	let answer = [];
+
+	for (let i = 0; i < UnitMoney.length; i++) {
+		answer.push(Math.floor(money / UnitMoney[i]));
+		money = money % UnitMoney[i];
+	}
+
+	return answer;
 }
 
 module.exports = problem5;
