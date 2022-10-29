47dbd32faf0809ee751fdd41a161f8d1b2cd44a5
GC-Park <kuyt3122@naver.com>
2022-10-29 17:06:13 +0900

    (7번)Feat:user의 친구들 배열 삽입 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,8 @@
 /* 
  기능 목록
   1) Object를 생성해 친구 사이인 사람끼리 각자의 배열에 추가
-  2) Object안에 key값 생성 및 Set으로 초기화
+  2) Object안에 key값 생성 및 Value는 Set으로 초기화
+  3) user의 친구들 배열로 만들기
 */
 
 function connectFreind(f, r){
@@ -18,12 +19,20 @@ function initializationObj(f, r){
   }
 }
 
+function createUserFreind(u, r, fou){
+  r[u].forEach((a)=>{
+    fou.push(a);
+  })
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   let rel={};
+  let fou=[];
   initializationObj(friends, rel);
   connectFreind(friends, rel);
-  console.log(rel)
+  createUserFreind(user, rel, fou)
+  console.log(fou)
   return answer;
 }
 
