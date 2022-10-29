cb36ebd408746f2becff5d2687c459bc7c4b290b
ksg2388 <ksg2388@naver.com>
2022-10-29 15:50:40 +0900

    problem 해결 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = [];
+  let cur = 0;
+
+  cryptogram = [...cryptogram];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    cur = cryptogram[i];
+    if (answer[answer.length - 1] === cur) {
+      answer.pop();
+    } else {
+      answer.push(cur);
+    }
+  }
+
+  return answer.join("");
 }
 
 module.exports = problem2;
--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,46 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer = [];
+  let bestFriends = [];
+  let recommend = {};
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][1] === user) {
+      bestFriends.push(friends[i][0]);
+    }
+  }
+
+  //서로 아는 친구의 경우 확인
+  for (let i = 0; i < friends.length; i++) {
+    if (bestFriends.indexOf(friends[i][0]) !== -1 && friends[i][1] !== user) {
+      if (friends[i][1] in recommend) {
+        recommend[friends[i][1]] += 10;
+      } else {
+        recommend[friends[i][1]] = 10;
+      }
+    }
+  }
+
+  for (let i = 0; i < visitors.length; i++) {
+    if (bestFriends.indexOf(visitors[i]) === -1) {
+      if (visitors[i] in recommend) {
+        recommend[visitors[i]] += 1;
+      } else {
+        recommend[visitors[i]] = 1;
+      }
+    }
+  }
+
+  for (let name in recommend) {
+    answer.push([name, recommend[name]]);
+  }
+
+  answer.sort((a, b) => b[1] - a[1]);
+
+  answer = answer.map((item) => item[0]);
+  if (answer.length > 5) {
+    answer = answer.slice(0, 6);
+  }
+
   return answer;
 }
 
