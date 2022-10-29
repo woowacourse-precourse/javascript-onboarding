b7d7f58200248c95cb65f6d7eba5b1ebf951d1c1
2wndrhs <2wndrhs@gmail.com>
2022-10-29 08:49:46 -0700

    feat(PROBLEM4): 정규식에 매칭되는 문자만 유니코드로 변환하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,16 @@
+// 정규식에 매칭되는 문자만 유니코드로 변환하는 기능
+function convertUnicode(regex, splitedWord) {
+  const result = splitedWord.map((char) =>
+    regex.test(char) ? char.charCodeAt() : char
+  );
+  return result;
+}
+
 // solution
 function solution(word) {
   const regex = /[a-zA-z]/;
   const splitedWord = word.split("");
+  const unicodeWord = convertUnicode(regex, splitedWord);
 }
 
 function problem4(word) {
