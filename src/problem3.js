a002983abcada0e6130858e2f2baf23237b13d82
daal2quf <daal2quf@gmail.com>
2022-10-30 05:30:20 +0900

    369 나오면 카운트 올리기구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  let num;
+  for (let i = 0; i < number; i++) { 
+    num = '' + (i + 1) 
+    for(let j = 0; j < num.length; j++) {
+      if(num[j] == 3 || num[j] == 6 || num[j] == 9) {
+        answer++;
+      }
+    }
+  }
+  console.log(answer)
   return answer;
 }
 
