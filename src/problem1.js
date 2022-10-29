86f8752a0207b021474a5eca6f62824115e22825
sh981013s <sh981013s@gmail.com>
2022-10-29 16:05:52 +0900

    feat: [1] 배열 길이 확인 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,14 @@
+/**
+ * 배열의 길이가 2인지 체크한다.
+ * @param {number[]} arr - 한명의 페이지 번호가 담긴 배열
+ * @return {boolean}
+ */
+const isArrayLengthTwo = (arr) => {
+  const DESIGNATED_ARR_LENGTH = 2;
+
+  return arr.length === DESIGNATED_ARR_LENGTH;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
