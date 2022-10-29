c37fa47dd24b4a22921d29a04ade3faa72a07f0e
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 13:21:12 +0900

    problem2. 주석 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,9 @@
 function problem2(cryptogram) {
+  // 매개변수 배열화 및 stack 배열 선언
   let array = cryptogram.split("");
   let stack = [];
 
+  // 연속된 중복 문자를 제외한 문자를 stack 배열에 담는 for of 문 작성
   for (const el of array) {
     if (el === stack[stack.length - 1]) {
       stack.pop();
@@ -10,6 +12,7 @@ function problem2(cryptogram) {
     }
   }
 
+  // 문자열로 변환하여 결과값 산출
   return stack.join("");
 }
 
