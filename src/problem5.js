fd085997983572844b81c65af6776c91191ef575
HBSPS <wiyeong79@naver.com>
2022-10-29 18:02:09 +0900

    :sparkles: [문제5] 기능1. 금액별로 몇 개로 변환되는지 return 하는 메소드

--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,18 @@ function problem5(money) {
   return answer;
 }
 
+function changeMoney(money) {
+  const changedMoney = []
+  const moneyArray = [50000, 10000, 5000, 1000, 100, 50, 10, 1];
+
+  while (money > 0) {
+    for (let i = 0; i < moneyArray.length; i++) {
+      changedMoney.push(parseInt(money / moneyArray[i]));
+      money = money % moneyArray[i];
+    }
+  }
+
+  return changedMoney;
+}
+
 module.exports = problem5;
