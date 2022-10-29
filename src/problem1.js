8758de8323d7fa897702a8ae3dbc9739c8cb0c52
wisdomin121@naver.com <wisdomin121@naver.com>
2022-10-29 15:01:44 +0900

    p1) 예외 처리

--- src/problem1.js
+++ src/problem1.js
@@ -3,6 +3,7 @@
 // 2. 1번에서 나온 숫자 더하기 & 곱하기
 // 3. 2번에서 나온 숫자 중, 최대값 찾기
 // 4. 포비와 크롱 중, 승자 구하기
+// + 예외 처리 (페이지 수)
 
 const sum = (arr) => {
   return result = arr.reduce((pv, cv) => {
@@ -16,7 +17,28 @@ const mul = (arr) => {
   });
 };
 
+const exceptionCheck = (pobi, crong) => {
+  // 페이지 수가 이어지는지 판단
+  if(pobi[1]-pobi[0] !== 1 || crong[1]-crong[0] !== 1) {
+    return false;
+  }
+  
+  // 왼쪽 홀수, 오른쪽 짝수 판단
+  if(pobi[0]%2 !== 1 || crong[0]%2 !== 1) {
+    return false;
+  }
+  if(pobi[1]%2 !== 0 || crong[1]%2 !== 0) {
+    return false;
+  }
+
+  return true;
+}
+
 function problem1(pobi, crong) {
+  // 예외 처리
+  if(!exceptionCheck(pobi, crong)) return -1;
+
+  // 페이지 번호 split
   const pobiSplit = [];
   const crongSplit = [];
   for(let i=0; i<2; i++) {
@@ -24,6 +46,7 @@ function problem1(pobi, crong) {
     crongSplit.push(crong[i].toString().split('').map(Number));
   }
 
+  // 숫자 더하기 & 곱하기
   const pobiSumMul = [];
   const crongSumMul = [];
   for(let i=0; i<2; i++) {
@@ -31,9 +54,11 @@ function problem1(pobi, crong) {
     crongSumMul.push(...[sum(crongSplit[i]), mul(crongSplit[i])]);
   }
 
+  // 최대값 찾기
   const pobiMax = Math.max(...pobiSumMul);
   const crongMax = Math.max(...crongSumMul);
 
+  // 승자 구하기
   if(pobiMax > crongMax) {
     return 1;
   }else if(pobiMax < crongMax) {
