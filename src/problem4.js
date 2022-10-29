72cf9deef018a264e9222eab64b91d5f9614476e
hyukzz <ri_chard@naver.com>
2022-10-29 22:31:43 +0900

    feat: [문제4]청개구리 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,33 @@
 function problem4(word) {
-  var answer;
-  return answer;
+	let answer = [];
+	let lowerAlphabet = [];
+	let upperAlphabet = [];
+
+	for (let i = 97; i <= 122; i++) {
+		lowerAlphabet.push(String.fromCharCode(i));
+	}
+	for (let i = 65; i <= 90; i++) {
+		upperAlphabet.push(String.fromCharCode(i));
+	}
+
+	for (let i = 0; i < word.length; i++) {
+		if (word[i] === " ") {
+			answer.push(" ");
+		}
+
+		if (lowerAlphabet.includes(word[i])) {
+			answer.push(
+				lowerAlphabet[lowerAlphabet.length - lowerAlphabet.indexOf(word[i]) - 1]
+			);
+		}
+
+		if (upperAlphabet.includes(word[i])) {
+			answer.push(
+				upperAlphabet[upperAlphabet.length - upperAlphabet.indexOf(word[i]) - 1]
+			);
+		}
+	}
+	return answer.join("");
 }
 
 module.exports = problem4;
