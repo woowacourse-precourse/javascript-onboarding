d1b803233db3e4aea1128d59b8f58a2c06454d88
sojinjang <jsojin6119@gmail.com>
2022-10-30 04:14:14 +0900

    :sparkles: feat(prob5): 화폐 개수 구하는 기능 추가
    1. 화폐 단위 배열에 담아두기
    2. 가진 돈을 화폐 단위로 나누어 몫만큼 변환 개수 배열에 넣기
    3. 나머지를 화폐 단위로 나누기 반복

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,20 @@
+function exchageMoney(money, unit) {
+  return [parseInt(money / unit), money % unit];
+}
+
 function problem5(money) {
-  var answer;
-  return answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10];
+  const result = [];
+  let change = money;
+  let count;
+
+  units.map((unit) => {
+    [count, change] = exchageMoney(change, unit);
+    result.push(count);
+  });
+  result.push(change); // 1원 동전 개수 담기
+
+  return result;
 }
 
 module.exports = problem5;
