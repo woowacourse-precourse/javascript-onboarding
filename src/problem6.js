ace12b912c68c8c92b1d3deeba11570c610a68e8
Hoon <si932174@gmail.com>
2022-10-29 13:32:47 +0900

    problem6 : 닉네임 1자인 인원 제거

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,18 @@
 function problem6(forms) {
   var answer;
+
+  const singleNickNameDeleter = forms => {
+
+    const noSingleNickNameForms = [];
+
+    forms.map(form => {
+      if(form[1].length !== 1){
+        noSingleNickNameForms.push(form)
+      }
+    })
+    return noSingleNickNameForms;
+  };
+
   return answer;
 }
 
