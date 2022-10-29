7359d3b45062bb11064fe4800010541fffefe2fc
2je0 <bulljy@naver.com>
2022-10-30 00:58:29 +0900

    ✨ feat(problem7): 받은 score를 기반으로 정렬된 배열을 반환받는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -11,6 +11,7 @@ function getNetwork(friends, network) {
   }
 }
 function getNetworkScore(user, network, scoreMap) {
+  if (!network.has(user)) return;
   for (const friend of network.get(user)) {
     for (const other of network.get(friend)) {
       if (other === user) continue;
@@ -26,6 +27,13 @@ function getVisitScore(visitors, scoreMap) {
     const score = scoreMap.get(visitor);
     scoreMap.set(visitor, score + 1);
   }
+}
+function getSortedScoreArr(scoreMap) {
+  const sortedArr = [...scoreMap.entries()].sort((a, b) => {
+    if (a[1] !== b[1]) return b[1] - a[1];
+    return a[0].localeCompare(b[0]);
+  });
+  return sortedArr;
 }
 function problem7(user, friends, visitors) {
   var answer;
