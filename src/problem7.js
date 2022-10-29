c362cf097534a9a9e7efc4ed618054b0f3d72358
murpin <qw04011@gmail.com>
2022-10-29 23:39:00 +0900

    [문제 7번] feat:  user의 친구의 친구 중 user와 친구를 맺지 않은 친구들에 대한 선택 및 10점씩 추가 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,7 @@
 /*
  * 기능 분석
  * [x] friends에 대한 친구 내용과 관계를 friendList에 저장하는 기능
- * [] user의 친구의 친구 중 user와 친구를 맺지 않은 친구들에 대한 선택 및 10점씩 추가 기능
+ * [x] user의 친구의 친구 중 user와 친구를 맺지 않은 친구들에 대한 선택 및 10점씩 추가 기능
  * [] user의 친구의 친구 중 user와 친구를 맺지 않은 친구들이 visitor에 존재하는 갯수만큼 1점씩 추가 기능
  * [] 점수가 결정된 친구에 대한 recommendList에 저장하기
  * [] user의 친구의 친구는 아니지만 visitor에 있는 인원에 대한 점수 계산 및 recommendList에 저장
@@ -17,8 +17,57 @@ function updateFriendsList([friend, otherFriend], friendsList) {
   }
 }
 
+// 추천친구리스트에 중복을 확인하는 함수
+function isDuplicateRecommendFriendList(name, list) {
+  for(let f =0;f<list.length;f++){
+    if(list[f][0] === name){
+      return true;
+    }
+  }
+  return false;
+}
+
+/*
+ * 추천 친구 리스트를 반환해주는 기능의 함수
+ * 이 함수는 user 친구의 친구 중 user와 친구가 아닌 이들에 대해 추천해주어야한다. 
+ * 위에 조건에 부합하는 친구를 추천친구라고 한다면, 
+ * 추천친구가 만약 user의 친구들 중 n명과 친구라면 추천친구의 point는 n * 10이 된다.
+ * 그리고 추천친구가 user의 타임라인에 방문한 횟수만큼 point를 +1을 한다. 
+ * 최종점수와 이름을 setRecommendList를 통해 추천친구리스트를 저장하고 반환한다.
+ */
+function getRecommendFriendList(user, visitors, friendsList) {
+  const recommendFriendList = [];
+
+  // user의 친구 수 만큼 반복
+  for(let friend = 0; friend < friendsList[user].length; friend++){
+
+    // user의 친구의 친구수 만큼 반복
+    for(let otherFriend = 0; otherFriend < friendsList[friendsList[user][friend]].length; otherFriend++){
+      let point = 0;
+
+      //만약 친구의 친구가 user의 친구 중에 포함되지 않은 인원이며, user본인이 아니며, 추천리스트에 아직 없는 경우
+      if (!friendsList[user].includes(friendsList[friendsList[user][friend]][otherFriend]) && (
+          friendsList[friendsList[user][friend]][otherFriend] !== user && (
+          !isDuplicateRecommendFriendList(friendsList[friendsList[user][friend]][otherFriend], recommendFriendList)
+        ))) {
+
+        // 친구의 친구의 친구 수만큼 반복
+        for (let k = 0; k < friendsList[friendsList[friendsList[user][friend]][otherFriend]].length; k++) {
+
+          // 친구의 친구의 친구 중에 user의 친구가 있다면 10점씩 추가
+          if (friendsList[user].includes(friendsList[friendsList[friendsList[user][friend]][otherFriend]][k])) {
+            point += 10;
+          }
+        }
+      }
+    }
+  }
+
+  return recommendFriendList;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
   const friendsList = {
     [user] : [],
   }
@@ -29,6 +78,8 @@ function problem7(user, friends, visitors) {
     updateFriendsList([otherFriend, friend], friendsList);
   });
 
+  let recommendList = getRecommendFriendList(user, visitors, friendsList);
+  
   return answer;
 }
 
