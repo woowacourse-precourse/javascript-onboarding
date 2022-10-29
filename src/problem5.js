e0a4c43cef75fad5155e9b0876a563ea6ba9a1ba
jsh3418 <albert5428@gmail.com>
2022-10-29 13:34:54 +0900

    feat: problem5 인자로 받은 돈을 최대한 큰 단위의 화폐로 바꾼 횟수를 배열로 리턴하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -3,7 +3,19 @@ function problem5(money) {
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
   // 단위를 순회하며 money를 최대한 뺄 수 있는 만큼 빼고 뺀만큼 카운트 하여 배열에 푸쉬한다.
-  // 카운트 한 배열을 리턴한다.
+  const unitsCount = [];
+
+  units.forEach((unit) => {
+    let count = 0;
+    while (money - unit >= 0) {
+      money = money - unit;
+      count++;
+    }
+    unitsCount.push(count);
+  });
+
+  // 배열을 리턴한다.
+  return unitsCount;
 }
 
 module.exports = problem5;
