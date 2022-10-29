786b24e33a08959c0be29b5090da7338d7aa4e5a
js43o <js43o@naver.com>
2022-10-29 16:22:25 +0900

    Feat: Add problem4 code.

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+
+  for (let c of word) {
+    if (c === " ") answer += c;
+    else if (c === c.toUpperCase()) {
+      answer += String.fromCodePoint(
+        "A".charCodeAt(0) + ("Z".charCodeAt(0) - c.charCodeAt(0))
+      );
+    } else {
+      answer += String.fromCodePoint(
+        "a".charCodeAt(0) + ("z".charCodeAt(0) - c.charCodeAt(0))
+      );
+    }
+  }
+
   return answer;
 }
 
