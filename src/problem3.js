084a2292244c29dc89ae54b0cfb3a83fcd5f395c
kaori-killer <sksbthwjd@daum.net>
2022-10-29 17:58:25 +0900

    refactor: 기능 오타 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,7 @@
 function problem3(number) {
   let resultCount = 0;
   for(let nowNumber = 1; nowNumber < number + 1; nowNumber++){
-    const nowNumberArray = nowNumber.toString()
+    const nowNumberArray = nowNumber.toString().split("");
     nowNumberArray.forEach(element => {
       if(element === '3' || element == '6' || element == '9') { resultCount += 1 }
     });
