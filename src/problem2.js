d667be9bb51f9c1aecebe150fbf7e7540b130a04
Buyoung Chung <sunarol@naver.com>
2022-10-30 00:19:33 +0900

    feat: eliminate characters

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
+function eliminating(arr) {
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === arr[i - 1]) {
+      arr.splice(i - 1, 2);
+      break;
+    }
+  }
+  return arr;
+}
 function problem2(cryptogram) {
   let cryptoArray = cryptogram.split("");
   let arrayLength = cryptoArray.length;
+
+  while (cryptoArray.length > 0) {
+    cryptoArray = eliminating(cryptoArray);
+    if (arrayLength === cryptoArray.length) break;
+    arrayLength = cryptoArray.length;
+  }
 }
 
 module.exports = problem2;
