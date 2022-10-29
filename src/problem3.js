5aa1c50aa927fc5fb564d248510243c126165a28
AhnGeunTaek <dksrmsxor@gmail.com>
2022-10-29 21:06:51 +0900

    Refactor: 주석, console.log 제거

--- src/problem3.js
+++ src/problem3.js
@@ -4,20 +4,14 @@ function problem3(number) {
   for (let i = 1; i <= number; i++) {
     let count = 0;
     let numToStr = i.toString();
-    console.log("체크", numToStr);
 
     for (let j = 0; j < numToStr.length; j++) {
       if (numToStr[j] === "3" || numToStr[j] === "6" || numToStr[j] === "9")
         count++;
-      console.log("체크2", count);
     }
     answer += count;
   }
   return answer;
 }
 
-problem3(13);
 module.exports = problem3;
-// number	result
-// 13	     4
-// 33	     14
