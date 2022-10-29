14122653be64c08b57d366c01422683cde5c95c3
su-lim <sujin.0xlim@gmail.com>
2022-10-30 02:44:55 +0900

    feat: [prob6] 기능 개선, 유사 닉네임을 가진 이메일 구하기

--- src/problem6.js
+++ src/problem6.js
@@ -1,14 +1,26 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  let words = {};
+  let dupWords = {};
 
-  // 1. 닉네임 리스트를 돌면서 두 글자씩 뽑아서 리스트를 만든 후 중복 제거
-
-  // 2. 두 글자 리스트를 돌면서 해당하는 이메일들을 얻음
-
-  // 3. 같은 글자가 연속적으로 포함된 닉네임을 가진 이메일을 뽑음
+  // 1. 닉네임 리스트를 돌면서 두 글자씩 뽑아서 등장 빈도수 확인, 두 번이상 등장한 단어만 남김
+  for(let i=0;i<forms.length;i++){
+    if(forms[i][1].length === 1){
+      continue;
+    }
+    let nickname = forms[i][1];
+    for(let j=0;j<nickname.length-1;j++){
+      let twoWord = nickname[j] + nickname[j+1];
+      if(words[twoWord]){
+        words[twoWord].push(forms[i][0]);
+        dupWords[twoWord] = words[twoWord];
+      }else{
+        words[twoWord] = [forms[i][0]];
+      }
+    }
+  }
   
-  // 4. 오름차순 정렬 후 중복 제거
-  
+  // 2. 오름차순 정렬 후 중복 제거
   return answer;
 }
 
