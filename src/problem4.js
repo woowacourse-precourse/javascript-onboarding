2f973563a83b41f112c4a016f4f24eabd268f787
HBSPS <wiyeong79@naver.com>
2022-10-29 16:51:25 +0900

    :sparkles: [문제4] 기능1.2. 소문자를 반대로 변환하는 메소드

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,10 @@ function problem4(word) {
   return answer;
 }
 
+function reverseLower(letter) {
+  // 영어 소문자 ASCII : 97 ~ 122
+  const index = 97 - letter.charCodeAt();
+  return String.fromCharCode(122 + index);
+}
+
 module.exports = problem4;
