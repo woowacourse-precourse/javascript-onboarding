6828d94a89fe732a94fbd178a5109ce258afbff9
dakyungoh <dakyung.work@gmail.com>
2022-10-29 22:19:27 +0900

    feat: problem5 화폐 단위 정의

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,7 @@
 function problem5(money) {
-  const banknotes = getTypesOfBanknotes()
+  const banknotes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
   const counts = countsBanknotes(money, banknotes)
-  return counts;
-}
-
-// TODO: PROBLEM5.md의 설명에 맞춰 화폐 종류를 반환한다.
-function getTypesOfBanknotes() {
-  return []
+  return counts
 }
 
 // TODO: 금액과 화폐의 종류를 입력받아 화폐 종류별로 필요한 갯수를 구한다.
