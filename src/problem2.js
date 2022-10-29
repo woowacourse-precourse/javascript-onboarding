0c73cd63149f177f6e3bc9c43109c5b8cec2b181
b-hyoung <youqlrqod@naver.com>
2022-10-30 04:31:31 +0900

    add Problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+ function problem2(cryptogram) {
+   var answer = "";
 
-module.exports = problem2;
+   for (let i = 0; i < cryptogram.length; i++) {
+     if (cryptogram[i] !== cryptogram[i + 1]) {
+       answer += cryptogram[i]
+     } else if (cryptogram[i] === cryptogram[i + 1]) {
+       answer = cryptogram.replace(cryptogram.substr(i, 2), '')
+       return problem2(answer)
+     }
+   }
+   return answer
+ }
+ // browoan
+ // brow oan oo mm n
+
+ module.exports = problem2;
