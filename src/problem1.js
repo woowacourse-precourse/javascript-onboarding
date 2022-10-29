4e2e1a50b354d66962fe3f96a872c50880393846
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-29 14:06:33 +0900

    Feat(문제1-기능1): 예외사항 체크 함수 구현, try/catch문으로 예외사항 -1 반환 처리

--- src/problem1.js
+++ src/problem1.js
@@ -5,8 +5,37 @@
 // 3. 길이가 2인 숫자 배열을 받고 큰수(본인 점수)를 반환하는 함수 구현
 // 3. 두 사람의 점수를 비교하여 알맞는 반환값 지정하기
 
+// 예외 사항 체크 함수
+function isException(pobi, crong) {
+  // 정수형인지 확인 및 연속된 숫자인지 확인
+  const intPobi = pobi.filter((pobi_ele) => Number.isInteger(pobi_ele));
+  const intcrong = crong.filter((crong_ele) => Number.isInteger(crong_ele));
+  const pobi_diff = intPobi[1] - intPobi[0];
+  const crong_diff = intcrong[1] - intcrong[0];
+
+  // 조건 만족하면 예외사항으로 간주 true 반환
+  if (
+    pobi_diff !== 0 ||
+    crong_diff !== 0 ||
+    intPobi.length !== 2 ||
+    intcrong.length !== 2
+  ) {
+    return true;
+  }
+}
+
 function problem1(pobi, crong) {
   var answer;
+
+  try {
+    // 예외 사항 확인후 true이면 Error 날리기
+    if (isException(pobi, crong)) {
+      throw new Error("예외사항");
+    }
+  } catch (error) {
+    // try문에서의 오류로 예외사항인 경우 answer = -1;
+    answer = -1;
+  }
   return answer;
 }
 
