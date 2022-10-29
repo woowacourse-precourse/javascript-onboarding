4731ccb94a53c069b6ec6acaad77f54967b64452
unknown <sjjoseph106@gmail.com>
2022-10-29 16:22:18 +0900

    :sparkles: problem2 구현

--- src/problem2.js
+++ src/problem2.js
@@ -5,7 +5,19 @@
  * @returns {string}
  */
 function problem2(cryptogram) {
-  return "";
+  let loop = true;
+
+  while (loop) {
+    loop = false;
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (cryptogram[i] === cryptogram[i + 1]) {
+        loop = true;
+        cryptogram = cryptogram.replace(cryptogram[i].repeat(2), "");
+        i--;
+      }
+    }
+  }
+  return cryptogram;
 }
 
 console.log(problem2("browoanoommnaon")); // "brown"
