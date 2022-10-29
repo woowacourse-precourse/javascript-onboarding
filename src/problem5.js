a2a2ed88adb93c4510e06d6f2fc2745557761587
lsy20140 <lsy20140@naver.com>
2022-10-29 20:02:49 +0900

    feat: [문제5] 화폐 종류별 개수 반환하는 함수 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
+function getCount(money){
+  const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const countArr = [];
+
+  for(let i = 0 ; i<moneyType.length ; i++){
+    countArr.push(parseInt(money / moneyType[i]));
+    money %= moneyType[i];
+  }
+
+  return countArr;
+}
+
 function problem5(money) {
-  var answer;
-  return answer;
+  return getCount(money);
 }
 
 module.exports = problem5;
