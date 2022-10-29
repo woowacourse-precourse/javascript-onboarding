769d7b0d828bbdda7c6025f14d3ffa1f065c5113
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-30 01:14:58 +0900

    [문제3] <1~2단계 기능요구사항> 1의자리에 369가 포함되어 있다면 count에 +1

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    /** 입력받은 숫자 n의 각 자릿수가 3으로 나누어 떨어질 때마다 count++ */
+    const clapCount = (n) => {
+        while (n > 0) {
+            // 1단계) 1의 자릿수가 3으로 나누어 떨어질 경우 count +1, 0은 항상 나누어떨어지므로 제외
+            if ((n % 10) % 3 === 0 && n % 10 !== 0) {
+                count++;
+            }
+            // 2단계) 이전 자리의 숫자를 확인하기 위해 n을 10으로 나누기
+            n = Math.floor(n / 10);
+        }
+    };
 }
 
 module.exports = problem3;
