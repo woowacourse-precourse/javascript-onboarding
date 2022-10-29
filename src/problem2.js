f0e344968480a844f0f06fcf20802642d36a21e8
cofla <cofla159@naver.com>
2022-10-30 02:54:57 +0900

    p2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while (/([a-z])\1{1,}/.test(cryptogram)) {
+    cryptogram = cryptogram.replace(/([a-z])\1{1,}/, "");
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
+
+/*
+1. 같은 글자가 2글자 이상 연속되면 삭제
+  /([a-z])\1{2,}/.test(cryptogram) === true이면 replace("")
+2. 없을 때까지 반복
+*/
