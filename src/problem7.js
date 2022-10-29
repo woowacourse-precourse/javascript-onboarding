f5700aa22176f3cefd9574f109fe75e4bcb03c41
GuHyeonWoo <ghw9174@gmail.com>
2022-10-29 18:43:24 +0900

    P7/기능 목록 추가 & 친구인 경우 예외처리 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -2,14 +2,26 @@
 // 1. 아는 친구를 찾아 저장하는 기능
 // 2. 아는 친구 리스트와 방문한 사람 리스트를 스코어 매기는 기능
 // 3. 스코어에 따라 친구 정렬
+// 4. 친구인 경우 예외처리 기능
+
+// 친구인 경우 예외처리하는 함수
+function filterList(person_score, firstDegree){
+  let temp_person_score = person_score.filter(x=>{
+    if(!firstDegree.includes(x['user_name'])) return x;
+  })
+  return temp_person_score;
+}
 
 // 점수와 이름에 따라 정렬하는 함수
-function sortList(person_score){
+function sortList(person_score, firstDegree){
+  // 이미 친구이 경우는 예외처리
+  person_score = filterList(person_score, firstDegree);
   person_score.sort((a, b)=>{
     // 점수를 내림차순, 점수가 같다면 이름을 오름차순하여 정렬
     if(a['score'] !== b['score']) return b['score'] - a['score'];
     else return a['user_name'] - b['user_name'];
   })
+  return person_score;
 }
 
 // 점수를 계산하는 함수
@@ -49,29 +61,31 @@ function checkAcquaintance(user, friends){
     if(acquaintance_object[user_2]) acquaintance_object[user_2].push(user_1);
     else acquaintance_object[user_2] = [user_1];
   }
+  // 친구 리스트 저장
+  let firstDegree = acquaintance_object[user];
   // 사용자의 친구의 친구를 구하기 위해 user의 친구 리스트를 순회
-  for(user_name of acquaintance_object[user]){
+  for(user_name of firstDegree){
     // user 본인인 경우를 제외하고 저장
     acquaintance_object[user_name].map(x=>{
       if(x !== user) acquaintance_score_user.push(x);
     })
   }
-  return acquaintance_score_user;
+  return [acquaintance_score_user, firstDegree];
 }
 
 function problem7(user, friends, visitors) {
   let answer = [];
   // score을 저장할 리스트
   let person_score = [];
-  // 아는 사람을 체크한 리스트
-  let acquaintance_user = checkAcquaintance(user, friends);
+  // 아는 사람과 친구를 체크한 리스트
+  let [acquaintance_user, firstDegree] = checkAcquaintance(user, friends);
   
   // 아는 사람과 방문한 사람의 점수를 계산하여 리스트에 저장
   person_score = scoreSave(person_score, acquaintance_user, 10).slice();
   person_score = scoreSave(person_score, visitors, 1).slice();
 
   // person_score을 점수와 이름으로 정렬
-  sortList(person_score);
+  person_score = sortList(person_score, firstDegree);
 
   // 최대 다섯명만 결과 리스트에 추가
   for(let i = 0; i < person_score.length; i++){
