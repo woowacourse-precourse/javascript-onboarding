dcbfd39804fd3e8ef81fa298041132fef19989bb
SoMin <thals0107@naver.com>
2022-10-29 10:35:21 +0900

    feat: 문제 1번 - 각 자리 더하는 함수 정의

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,14 @@
+// 각 자리 더하는 함수 선언
+const sumDigit = (n) => {
+  let sum_res = 0;
+  while (n > 0) {
+    sum_res += n % 10;
+    n = parseInt(n / 10);
+  }
+  return sum_res;
+};
+
+// 문제 해결 함수
 function problem1(pobi, crong) {
   var answer;
   return answer;
