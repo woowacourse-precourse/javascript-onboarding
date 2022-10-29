00c1b9303bed3492a981742a74fe96d0f97fc335
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-30 05:25:16 +0900

    [feat] problem7- 친구의 친구  명단 구하기 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -14,3 +14,16 @@ function find_friend_list(user, friends) {
   });
   return Array.from(friend_list);
 }
+
+//2. 친구의 친구 명단 구하기
+function common_friend(user, friends) {
+  let friend_list = find_friend_list(user, friends);
+  let common_friend_list = new Set();
+  friends.map((friend) => {
+    if (friend_list.includes(friend[0]) && !friend_list.includes(friend[1]) && friend[1] != user)
+      common_friend_list.add(friend[1]);
+    if (friend_list.includes(friend[1]) && !friend_list.includes(friend[1]))
+      common_friend_list.add(friend[0]);
+  });
+  return Array.from(common_friend_list);
+}
