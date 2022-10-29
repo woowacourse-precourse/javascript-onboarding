9eaa0897e7ef854d8697964454466217fa5477a2
adultlee <seoungin1228@gmail.com>
2022-10-29 16:27:09 +0900

    feat: [pro7] 모든 값들을 제한사항에 맞추어 정렬

--- src/problem7.js
+++ src/problem7.js
@@ -20,6 +20,17 @@ function problem7(user, friends, visitors) {
     if (recommendUserMap[visitors[i]] !== undefined)
       recommendUserMap[visitors[i]] += 1;
   }
+
+  const recommendUserArray = objectToArray(recommendUserMap);
+
+  recommendUserArray.sort((a, b) => sortMorePoint(a, b));
+  recommendUserArray.sort((a, b) => sortNamePoint(a, b));
+
+  return recommendUserArray.map((user) => {
+    return user[0];
+  });
+}
+
 function getNotUserFriendsArray(user, friends, visitors, USER_FRIENDS) {
   const notUserFriends = [];
   for (let i = 0; i < friends.length; i++) {
@@ -57,6 +68,25 @@ function initObj(array) {
   array.map((el) => (obj[el] = 0));
 
   return obj;
+}
+
+function objectToArray(object) {
+  const array = [];
+
+  for (key in object) {
+    array.push([key, object[key]]);
+  }
+  return array;
+}
+
+function sortMorePoint(A, B) {
+  return A[1] > B[1] ? -1 : 1;
+}
+
+function sortNamePoint(A, B) {
+  if (A[1] === B[1]) return A[0] > B[0] ? 1 : -1;
+
+  return 0;
 }
 
 module.exports = problem7;
