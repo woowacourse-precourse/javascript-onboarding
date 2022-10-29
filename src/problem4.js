4f706f907e87f95fdacb69aa96cc7f9cbd4f7ed6
kyhyun <k29176@gmail.com>
2022-10-29 19:12:47 +0900

    feat : 문제 4번 - 문자가 대문자인지 판별하는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,7 @@
 /*
 기능 구현 목록
 [v] 문자가 소문자인지 판별하는 기능
-[ ] 문자가 대문자인지 판별하는 기능
+[v] 문자가 대문자인지 판별하는 기능
 [ ] 대문자 알파벳 변환 기능
 [ ] 소문자 알파벳 변환 기능
 */
@@ -15,6 +15,12 @@ function isLower(char) {
   const LOWER_CASE_START_POINT = 97;
   const LOWER_CASE_END_POINT = 122;
   return LOWER_CASE_START_POINT <= char && char <= LOWER_CASE_END_POINT ? true : false;
+}
+
+function isUpper(char) {
+  const UPPER_CASE_START_POINT = 65;
+  const UPPER_CASE_END_POINT = 90;
+  return UPPER_CASE_START_POINT <= char && char <= UPPER_CASE_END_POINT ? true : false;
 }
 
 problem4('I love you'); // 'R olev blf'
