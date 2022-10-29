d5a82123d0413fcb0872543e487b17fe6a69d4a1
dladncks1217 <dlaxodud1217@gmail.com>
2022-10-29 20:35:30 +0900

    feat: problem5 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 결과
+  const cost = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]; // 단위
+
+  for (let i = 0; i < cost.length; i++) {
+    if (money / cost[i] >= 1) {
+      let count = parseInt(money / cost[i]);
+      answer[i] += count;
+      money -= cost[i] * count;
+    }
+  }
+
   return answer;
 }
 
