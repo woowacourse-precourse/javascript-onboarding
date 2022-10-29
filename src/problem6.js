11b61427f98db5319a03f9c8477f9d96eda79938
TaeeunKim <xodms0309@naver.com>
2022-10-29 21:20:42 +0900

    [6번] feat: 다른 크루와 닉네임이 겹친 크루의 이메일을 저장

--- src/problem6.js
+++ src/problem6.js
@@ -6,6 +6,7 @@ function problem6(forms) {
   for (const [email, nickname] of forms) {
     crew.push({ email: email, nickname: nickname });
     //2글자씩 자른 닉네임을 set에 넣음
+    //제이제이엠과 같이 자른 닉네임이 중복될 수 있으므로 set 사용
     const splittedNicknames = new Set();
     for (let i = 0; i < nickname.length - 1; i++) {
       const slicedNickname = nickname.slice(i, i + 2);
@@ -23,9 +24,22 @@ function problem6(forms) {
     }
   }
 
+  //2명 이상 겹친 닉네임들
   const duplicates = Object.entries(duplicateNicknames).filter(
     ([key, value]) => value >= 2,
   );
+
+  const result = new Set();
+
+  //다른 크루와 닉네임이 겹친 크루의 닉네임 추가
+  for (const person of crew) {
+    for (const [duplicate, _] of duplicates) {
+      if (!person.nickname.includes(duplicate)) continue;
+      result.add(person.email);
+    }
+  }
+
+  answer = Array.from(result).sort();
 
   return answer;
 }
