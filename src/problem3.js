a29f0f62bd232dc9f742dadbbaaabbde414bb297
위성노 <92wsn1111@gamilc.com>
2022-10-29 20:48:58 +0900

    feat: Add [문제3] 제한 사항 체크 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  
+  // 제한 사항 만족하는지 체크
+  // - number는 1 이상 10,000 이하인 자연수이다.
+  if (number < 1 || number > 10000) return false;
+  if (!(/^[0-9]+$/.test(number))) return false;
+  
 }
 
 module.exports = problem3;
