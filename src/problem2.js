80c34f3cb9f6003dddd51f24b6f1155eb8481d75
박겸영 <yong_drg@naver.com>
2022-10-30 05:43:22 +0900

    feat: 문제 2 연속하는 중복 문자들 1회 찾기 - 기능 1: 연속하는 중복 문자들을 암호문의 처음부터 끝까지 1회 찾아 배열에 담기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const duplicateLetterList = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    const [prev, curr, next] = [cryptogram[i - 1], cryptogram[i], cryptogram[i + 1]];
+    let duplicateLetter = '';
+
+    if (curr === next) {
+      if (curr === prev) {
+        duplicateLetterList[duplicateLetterList.length - 1] += curr;
+      } else {
+        duplicateLetterList.push((duplicateLetter += curr + next));
+      }
+    }
+  }
 }
 
 module.exports = problem2;
