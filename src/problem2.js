72f6b5c6320198483db8a42e5f039776c62bfa7d
cruelladevil <dev.timetravel@gmail.com>
2022-10-30 01:31:31 +0900

    feat(PROBLEM2): add removeAlladjacent

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,20 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function removeAllAdjacent(string) {
+  let result = '';
+
+  for (let i = 0; i < string.length; i += 1) {
+    if (string.charAt(i) === string.charAt(i + 1)) {
+      while (string.charAt(i) === string.charAt(i + 1)) {
+        i += 1;
+      }
+    } else {
+      result += string.charAt(i);
+    }
+  }
+
+  return result;
+}
+
 module.exports = problem2;
