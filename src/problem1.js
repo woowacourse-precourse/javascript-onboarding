e3456b6cf298f16b7d82103df271bcb450b91919
unqeye <salyuan@naver.com>
2022-10-29 22:32:27 +0900

    docs: 문제1 기능 목록

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+function checkPages(pages) {
+  const [left, right] = pages;
+  if (1 <= left && left < 400 && left % 2 === 1 && left + 1 === right) {
+    return true;
+  }
+  return false;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
