8d8df3d8c4d1a974196509a60491b927bb5f44ed
SoMin <thals0107@naver.com>
2022-10-29 10:36:57 +0900

    feat: 문제 1번 - 각 배열의 왼쪽 페이지와 오른쪽 페이지 변수 선언

--- src/problem1.js
+++ src/problem1.js
@@ -20,6 +20,16 @@ const multDigit = (n) => {
 
 // 문제 해결 함수
 function problem1(pobi, crong) {
+  // 각 배열의 왼쪽과 오른쪽 구별
+  const pobi_left = pobi[0];
+  // console.log("pobi_left", pobi_left);
+  const pobi_right = pobi[1];
+  // console.log("p-r", pobi_right);
+  const crong_left = crong[0];
+  // console.log("c-l", crong_left);
+  const crong_right = crong[1];
+  // console.log("c-r", crong_right);
+
   var answer;
   return answer;
 }
