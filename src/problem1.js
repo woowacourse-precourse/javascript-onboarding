373f96e98d98fc15fd06c11af3f85d09a3d74d48
LEEJW1953 <jiwonlee1953@naver.com>
2022-10-29 22:14:20 +0900

    1번 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,36 +1,27 @@
-function multiple(num) {
-  let result = 1;
+function cal_max(num) {
+  let mul = 1;
+  let sum = 0;
   while (num !== 0) {
-    result *= num % 10;
+    mul *= num % 10;
+    sum += num % 10;
     num = parseInt(num / 10);
   }
-  return result
-}
-
-function plus(num) {
-  let result = 0;
-  while (num !== 0) {
-    result += num % 10;
-    num = parseInt(num / 10)
-  }
-  return result
+  return Math.max(mul, sum)
 }
 
 function problem1(pobi, crong) {
   var answer;
   if ((pobi[0] % 2 == 1) && (pobi[1] == (pobi[0] + 1)) && (crong[0] % 2 == 1) && (crong[1] = crong[0] + 1)) {
-    let pobires = [], crongres = [];
+    let pobi_res = [], crong_res = [];
     for (let i = 0; i < 2; i++) {
-      pobires.push(multiple(pobi[i]));
-      pobires.push(plus(pobi[i]));
-      crongres.push(multiple(crong[i]));
-      crongres.push(plus(crong[i]));
+      pobi_res.push(cal_max(pobi[i]));
+      crong_res.push(cal_max(crong[i]));
     }
-    if (Math.max(...pobires) > Math.max(...crongres)) {
+    if (Math.max(...pobi_res) > Math.max(...crong_res)) {
       answer = 1;
-    } else if (Math.max(...pobires) < Math.max(...crongres)) {
+    } else if (Math.max(...pobi_res) < Math.max(...crong_res)) {
       answer = 2;
-    } else if (Math.max(...pobires) === Math.max(...crongres)) {
+    } else if (Math.max(...pobi_res) === Math.max(...crong_res)) {
       answer = 0;
     }
   } else {
