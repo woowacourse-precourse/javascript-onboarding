48f0f712047478ddb4c8cd8e015a26b04a3aa281
김미진 <kimmijin@gimmijin-ui-MacBookAir.local>
2022-10-29 20:55:04 +0900

    problem1 해결
    C언어만 쓰다 보니까 JS가 실수 정수 신경 안 쓴다는 걸 잊어버림...내가 원하던 알고리즘을 위해서는 int형으로 값을 변환해야 했다!

--- src/problem1.js
+++ src/problem1.js
@@ -8,42 +8,64 @@
 
 function problem1(pobi, crong) {
     var answer;
-    let pobiPlus; pobiMul; pobiMax;
-    let crongPlus; crongMul; crongMax;
+    let pobiPlus = [,];
+    let pobiMul = [,];
+    let pobiMax;
+    let crongPlus = [,];
+    let crongMul = [,];
+    let crongMax;
 
-    if (pobi[0] != pobi[1] - 1 || crong[0] != crong[1] - 1 || pobi[1] - pobi[0] != 1 || cobi[1] - cobi[0] != 1) {
+    if (pobi[0] != pobi[1] - 1 || crong[0] != crong[1] - 1 || pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) {
         answer = -1;
     }
 
     else {
-        //cobi 값
-        for (i = 0; i < pobi.length; i++) {
-            var p100 = pobi[i] / 100;
-            var p10 = pobi[i] / 10 - p100;
-            var p1 = pobi[i] % 10;
+        //pobi 값
+        for (let i = 0; i < 2; i++) {
+            let p100 = [,];
+            let p10 = [,];
+            let p1 = [,];
+            p100[i] = pobi[i] / 100;
+            p10[i] = pobi[i] / 10 - p100;
+            p1[i] = pobi[i] % 10;
 
-            pobiPlus[i] = p100 + p10 + p1;
-            pobiMul[i] = p100 * p10 * p1;
+            pobiPlus[i] = p100[i] + p10[i] + p1[i];
+            pobiMul[i] = p100[i] * p10[i] * p1[i];
             const pobiPM = (pobiPlus[i] > pobiPlus[i + 1]) ? pobiPlus[i] : pobiPlus[i + 1];
             const pobiMM = (pobiMul[i] > pobiMul[i + 1]) ? pobiMul[i] : pobiMul[i + 1];
             pobiMax = (pobiPM > pobiMul) ? pobiPM : pobiMM;
         }
 
         //crong 값
-        for (i = 0; i < crong.length; i++) {
-            var c100 = crong[i] / 100;
-            var c10 = crong[i] / 10 - c100;
-            var c1 = crong[i] % 10;
+        for (let i = 0; i < 2; i++) {
+            let c100 = [,];
+            let c10 = [,];
+            let c1 = [,];
+            c100[i] = crong[i] / 100;
+            c10[i] = crong[i] / 10 - c100;
+            c1[i] = crong[i] % 10;
 
-            crongPlus[i] = c100 + c10 + c1;
-            crongMul[i] = c100 * c10 * c1;
-            let crongPM = (crongPlus[i] > crongPlus[i + 1]) ? crongPlus[i] : crongPlus[i + 1];
-            let crongMM = (crongMul[i] > crongMul[i + 1]) ? crongMul[i] : crongMul[i + 1];
+            crongPlus[i] = c100[i] + c10[i] + c1[i];
+            crongMul[i] = c100[i] * c10[i] * c1[i];
+            const crongPM = (crongPlus[i] > crongPlus[i + 1]) ? crongPlus[i] : crongPlus[i + 1];
+            const crongMM = (crongMul[i] > crongMul[i + 1]) ? crongMul[i] : crongMul[i + 1];
             crongMax = (crongPM > crongMul) ? crongPM : crongMM;
+        }
+
+        //pobi와 crong 값 비교//
+        if (pobiMax > crongMax) {
+            answer = 1;
+        }
+        if (pobiMax < crongMax) {
+            answer = 2;
+        }
+        if (pobiMax = crongMax) {
+            answer = 0;
         }
     }
 
-  return answer;
+    return answer;
 }
 
 module.exports = problem1;
+
