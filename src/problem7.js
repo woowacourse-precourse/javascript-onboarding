d0505f14851c37f8e642f37672600a2b349a40ff
adultlee <seoungin1228@gmail.com>
2022-10-29 16:22:09 +0900

    feat: [pro7] user와 친구인지 아닌지 분리
    각 친구를 만들어주는 함수를 사용하면서 중복을 제거 합니다

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,39 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const USER_FRIENDS = getUserFriendesArray(user, friends);
+  const NOT_USER_FRIENDS = getNotUserFriendsArray(
+    user,
+    friends,
+    visitors,
+    USER_FRIENDS
+  );
+function getNotUserFriendsArray(user, friends, visitors, USER_FRIENDS) {
+  const notUserFriends = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] !== user && !USER_FRIENDS.includes(friends[i][0]))
+      notUserFriends.push(friends[i][0]);
+    if (friends[i][1] !== user && !USER_FRIENDS.includes(friends[i][1]))
+      notUserFriends.push(friends[i][1]);
+  }
+  for (let i = 0; i < visitors.length; i++)
+    if (
+      !notUserFriends.includes(visitors[i]) &&
+      !USER_FRIENDS.includes(visitors[i])
+    )
+      notUserFriends.push(visitors[i]);
+  return deleteDuplication(notUserFriends);
 }
 
+function getUserFriendesArray(user, friends) {
+  let userFriendsArray = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] === user) userFriendsArray.push(friends[i][1]);
+    else if (friends[i][1] === user) userFriendsArray.push(friends[i][0]);
+  }
+
+  return deleteDuplication(userFriendsArray);
+}
+
+function deleteDuplication(array) {
+  return Array.from(new Set(array));
+}
 module.exports = problem7;
