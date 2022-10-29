0c4dc629298585017f91a0728f5db9270b2db3b3
loco9939 <kls9939@naver.com>
2022-10-29 17:09:03 +0900

    feat: Create func
    - 중복된 닉네임을 가진 유저를 반환하는 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -17,9 +17,34 @@ const getPartialStr = (str) => {
   return res;
 };
 
+const getDuplicatedUser = (arr) => {
+  let index = 0;
+  let position = 1;
+  const res = [];
+  while (index !== arr.length - 1) {
+    const set = new Set([...arr[index], ...arr[position]]);
+    if (set.size !== arr[index].length + arr[position].length) {
+      res.push(...arr[index].slice(-1), ...arr[position].slice(-1));
+    }
+    position += 1;
+    // if (position > arr.length) break;
+    if (position === arr.length) {
+      index += 1;
+      position = index + 1;
+    }
+  }
+  return [...new Set(res)];
+};
+
 function problem6(forms) {
+  const hash = {};
+  forms.forEach((form) => {
+    hash[form[1]] = form[0];
+  });
   const nicknames = forms.map((nickname) => nickname[1]);
   const partialStrs = nicknames.map((nickname) => getPartialStr(nickname));
+  const selectedUsers = getDuplicatedUser(partialStrs);
+
 }
 
 const input = [
