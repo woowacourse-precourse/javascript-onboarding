a3bfbe014d4ea9858c7db29219f5f3659475e7cf
2yunseong <dbsdltjd3701@naver.com>
2022-10-29 12:11:33 +0900

    chore(문제 2번): 문제 풀이에 필요한 스켈레톤 코드 작성 - 사용할 변수 선언 - 반복문 블록만 구성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
 function problem2(cryptogram) {
   var answer;
+  let base = 0; // 중복검사 때 사용하는 base index
+  let target = cryptogram;
+
+  while (true) {
+    // 중복 검사로직 작성
+    // 새 문자열 로직 작성
+  }
   return answer;
 }
 
