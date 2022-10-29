d522f7ede46c54f047e1038ef50c2ccb421d5a0d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-29 15:41:23 +0900

    [problem4]

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  for (var i = 0; i < word.length; i++) {
+    if (word.charCodeAt(i) >= 65 && word.charCodeAt(i) <= 90) {
+      // 대문자인 경우
+      answer += String.fromCharCode(155 - word.charCodeAt(i));
+    } else if (word.charCodeAt(i) >= 97 && word.charCodeAt(i) <= 122) {
+      // 소문자인 경우
+      answer += String.fromCharCode(219 - word.charCodeAt(i));
+    } else {
+      answer += word.charAt(i);
+    }
+  }
+
   return answer;
 }
 
