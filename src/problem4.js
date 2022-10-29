f426de79369bb8d347bc003081eda1a2d4cefb60
curomame <curomame@naver.com>
2022-10-29 19:23:03 +0900

    ✅ check : problem 4 해답 코드 작성 완료 1029.1922

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,39 @@
 function problem4(word) {
+
+  // 특이사항
+  //!1. word의 길이는 1이상 1000 이하인 문자열
+
+  function CHECK_WORD_LENGTH_BETWEEN_ONE_TO_THOUSAND (word){
+    if(word.length < 1 || word > 1000){
+      return false;
+    } return true;
+  }
+
+  //!2. 알파벳 외의 문자는 변환하지 않음
+
+  function CHECK_IS_ALPHABET(letter){
+    
+    if(typeof letter !== "string"){
+      return false;
+    }
+    let inputASCII = letter.charCodeAt();
+    if(inputASCII >= ASCII_OBJ.A && inputASCII <= ASCII_OBJ.Z){
+      return true
+    }
+    if(inputASCII >= ASCII_OBJ.a && inputASCII <= ASCII_OBJ.z){
+      return true
+    }
+    return false;
+  }
+
+  
+  if(CHECK_WORD_LENGTH_BETWEEN_ONE_TO_THOUSAND(word) === false){
+    return "word의 길이가 1미만 혹은 1,000초과 입니다."
+  }
+
   // 필요함수
   //#1. 알파벳이 들어오면 반대의 알파벳으로 변경해주는 함수
+    //!3. 알파벳 대문자는 대문자, 소문자는 소문자로 변환
 
   const ASCII_OBJ = {
     A: 65,
@@ -14,6 +47,7 @@ function problem4(word) {
   };
 
   function RETURN_REVERSED_ALPHABET(letter) {
+
     let inputASCII = letter.charCodeAt();
     let outputASCII;
 
@@ -36,45 +70,13 @@ function problem4(word) {
     return String.fromCharCode(outputASCII);
   }
 
-  // 특이사항
-  //!1. word의 길이는 1이상 1000 이하인 문자열
-
-  function CHECK_WORD_LENGTH_BETWEEN_ONE_TO_THOUSAND (word){
-    if(word.length < 1 || word > 1000){
-      return false;
-    } return true;
-  }
-
-  //!2. 알파벳 외의 문자는 변환하지 않음
-
-  function CHECK_IS_ALPHABET(letter){
-    
-    if(typeof letter !== "string"){
-      return false;
-    }
-    
-    let inputASCII = letter.charCodeAt();
-
-    if(inputASCII >= ASCII_OBJ.A && inputASCII <= ASCII_OBJ.Z){
-      return true
-    }
-
-    if(inputASCII >= ASCII_OBJ.a && inputASCII <= ASCII_OBJ.z){
-      return true
-    }
-
-    return false;
-
-  }
-
-
-  //!3. 알파벳 대문자는 대문자, 소문자는 소문자로 변환
 
   const result = [];
 
   for (i of word) {
-    if (i === " ") {
-      result.push(" ");
+
+    if(CHECK_IS_ALPHABET(i) === false){
+      result.push(i)
     } else {
       result.push(RETURN_REVERSED_ALPHABET(i));
     }
