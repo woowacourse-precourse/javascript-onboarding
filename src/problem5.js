ca722c2b0707036c2b87989f92dadaa2e50a03e5
subin <newdeas100@gmail.com>
2022-10-29 22:50:32 +0900

    feat(problem5): calUnits함수 기능추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,23 @@
 function problem5(money) {
-  var answer;
-  return answer;
+
 }
+
+function calUnits(money) {
+  const moneyUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  let totalLeft = money;
+
+  moneyUnits.forEach(unit => {
+    const share = Math.floor(totalLeft / unit);
+    answer.push(share);
+    totalLeft -= share * unit;
+  })
+
+  console.log(answer);
+}
+
+calUnits(15000);
+
 
 module.exports = problem5;
