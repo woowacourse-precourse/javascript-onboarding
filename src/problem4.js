0ab6d054a9ea8a9e9d129608c78122d5f964642c
kaori-killer <sksbthwjd@daum.net>
2022-10-29 17:53:34 +0900

    feat: (대문자) 155 - 현재 값의 아스키 코드, (소문자) 218 - 현재 값의 아스키 코드로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
 function problem4(word) {
-  
+  const wordArray = word.split("");
+  let resultString = '';
+
+  wordArray.forEach(char => { 
+    console.log(String.fromCharCode(155 - char.charCodeAt()));
+    console.log(String.fromCharCode(219 - char.charCodeAt()));
+    console.log(char);
+
+  });
 }
 
 module.exports = problem4;
+
+console.log(problem4("I love you"));
