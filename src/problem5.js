10f38b2caa63841ff117be1300c070a95779e1a6
woo-jk <201702042@o.cnu.ac.kr>
2022-10-29 13:26:46 +0900

    feat: [PROBLEM5] 가장 큰 화폐 크기를 찾는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,14 @@
+const findLargestMoneyUnit = (money) => {
+  if (money >= 50000) return 50000;
+  if (money >= 10000) return 10000;
+  if (money >= 5000) return 5000;
+  if (money >= 1000) return 1000;
+  if (money >= 500) return 500;
+  if (moeny >= 100) return 100;
+  if (money >= 50) return 50;
+  if (money >= 10) return 10;
+  if (money >= 1) return 1;
+};
 function problem5(money) {
   var answer;
   return answer;
