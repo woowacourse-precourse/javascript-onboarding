3f3368460f227026fa5b83a429e0c22694f4c835
RooT_Kim <bdd3452@gmail.com>
2022-10-29 22:35:47 +0900

    문제6번) checkDuplicate 함수 수정 (compareWords 함수 선언 후 로직 분리)

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,5 @@
 function problem6(forms) {
+	//서로 중복된 단어가 있는지 비교하는 함수 -> compareWords
 	//닉네임 중복 체크를 하는 함수 -> checkDuplicate
 	//중복된 닉네임의 인덱스를 체크하는 함수 -> getIndex
 	//중복 확인된 닉네임의 인덱스 값을 이용해 결과를 리턴하는 함수 -> checkNickname
@@ -32,18 +33,20 @@ function checkDuplicate(selected, nicknames) {
 	const words = selected.split('');
 	let result = [];
 
-	result = nicknames.filter((nickname) => {
-		let count = 0;
-
-		for (let i = 0; i < words.length - 1; i++) {
-			count += nickname.includes(words[i] + words[i + 1]) ? 1 : 0;
-		}
-
-		return count > 0 ? true : false;
-	});
+	result = nicknames.filter((nickname) => compareWords(words, nickname));
 
 	console.log('결과:', result);
 	return result.length > 1 ? true : false;
+}
+
+function compareWords(words, nickname) {
+	let count = 0;
+
+	for (let i = 0; i < words.length - 1; i++) {
+		count += nickname.includes(words[i] + words[i + 1]) ? 1 : 0;
+	}
+
+	return count > 0 ? true : false;
 }
 
 module.exports = problem6;
