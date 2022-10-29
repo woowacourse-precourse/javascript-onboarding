174385bb0ad10778d50bcdc5f9cb1ed716565812
Yunjae <nuguri206@gmail.com>
2022-10-29 15:06:09 +0900

    feat: [prob1]  필요 함수 이름 정의

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,25 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+  //책의 범위 설정 getBookNum
+  const getBookNum = () => {
+
+  }
+  
+  //포비, 크롱의 가장 큰 값
+  const getMaxNum = () => {
+
+  };
+
+  //책 페이지 합산 값
+  const getSumNum = () => {
+
+  };
+
+  //책 페이지 곱한 값
+  const getmultNum = () => {
+
+  };
+    
 }
 
 module.exports = problem1;
