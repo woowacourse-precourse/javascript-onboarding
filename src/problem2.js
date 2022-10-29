2cd4afb19c52631d9694a43a09ad89b5d05079e3
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-29 19:52:26 +0900

    Docs(문제2): 기능목록 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,8 @@
+// 기능 목록
+// 1. 문자열의 해당하는 배열에대하여 연속하는 중복문자열을 제거하는 함수 구현
+// 2. while문으로 1번 함수의 input/output이 같아질때까지 반복
+// 3. 배열을 문자열로 변환후 반환
+
 function problem2(cryptogram) {
   var answer;
   return answer;
