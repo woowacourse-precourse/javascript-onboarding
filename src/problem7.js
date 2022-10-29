9a1c87ae99ba70776383b04f47573b50b55bf1f3
GC-Park <kuyt3122@naver.com>
2022-10-29 16:52:38 +0900

    (7번)Feat:Object안 key값에 맞게 배열 초기화 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,7 @@
 /* 
  기능 목록
   1) Object를 생성해 친구 사이인 사람끼리 각자의 배열에 추가
-
+  2) object안에 key값에 맞게 배열 초기화
 */
 
 function connectFreind(f, r){
@@ -11,9 +11,17 @@ function connectFreind(f, r){
   }
 }
 
+function initializationObj(f, r){
+  for(let i=0; i<f.length; i++){
+    r[f[i][0]]=[]
+    r[f[i][1]]=[]
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   let rel={};
+  initializationObj(friends, rel);
   connectFreind(friends, rel);
   console.log(rel)
   return answer;
