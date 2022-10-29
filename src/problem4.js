ded5230b3e6b865f13ccdfbd57bcfb7ab78c876b
subin <newdeas100@gmail.com>
2022-10-29 17:43:50 +0900

    feat(problem4): checkInputErr 함수 기능추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,30 +1,50 @@
 function problem4(word) {
-  const dict = new Map();
-  makeDictionary(dict);
-  const answer = handleString(word,dict);
 
-  return answer;
+  try {
+    if (checkInputErr(word)) {
+      throw new Error("Invalid Input Error");
+    }
+
+    const dict = new Map();
+    makeDictionary(dict);
+    const answer = handleString(word, dict);
+
+    return answer;
+
+  } catch (err) {
+    console.log(err.message);
+  }
 }
 
-function makeDictionary(dict){
-  const alpha = new Array(26).fill(null).map((_,i) => String.fromCharCode(i+65));
+function checkInputErr(word) {
+
+  const vaildType = (input) => typeof input === 'string'; //TypeError
+  const validLength = (input) => 1 <= input.length && input.length <= 1000; // LengthError
+
+  if (!vaildType(word)) return true;
+  else if (!validLength(word)) return true;
+  else return false;
+}
+
+function makeDictionary(dict) {
+  const alpha = new Array(26).fill(null).map((_, i) => String.fromCharCode(i + 65));
   const reverseAlpha = [...alpha].reverse();
-  for(let i=0; i<alpha.length;i++){
-    const [key,value] = [alpha[i], reverseAlpha[i]];
+  for (let i = 0; i < alpha.length; i++) {
+    const [key, value] = [alpha[i], reverseAlpha[i]];
     dict.set(key, value);
     dict.set(key.toLowerCase(), value.toLowerCase());
   }
 }
 
-function handleString(word,dict){
+function handleString(word, dict) {
   const temp = [];
 
-  for(let i=0; i<word.length;i++){
+  for (let i = 0; i < word.length; i++) {
     const char = word[i];
-    if(!dict.has(char)){
+    if (!dict.has(char)) {
       temp.push(char);
     }
-    else{
+    else {
       temp.push(dict.get(char));
     }
   }
