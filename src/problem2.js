4efbbc608b042f5af861c29ddc7dfaff103dec4a
Chaeduk <kkj2551@naver.com>
2022-10-29 21:57:49 +0900

    feat(problem2) : 문제 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while(true){
+    const cryptogramLength = cryptogram.length
+    cryptogram = cryptogram.replace(/([a-z])\1+/g,'')
+    if(cryptogramLength === cryptogram.length){
+      break
+    }
+  }
+  return cryptogram
 }
 
 module.exports = problem2;
