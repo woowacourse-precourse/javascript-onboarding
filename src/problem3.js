c8211192c55af087c33ff23cd34249e860aeec92
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-30 01:43:00 +0900

    문제3) feat: 숫자의 각 자릿수에 3, 6, 9가 들어있는 횟수 반환

--- src/problem3.js
+++ src/problem3.js
@@ -8,4 +8,14 @@ const is369 = (number) => {
   return false;
 };
 
+const count369 = (number) => {
+  const numString = number + "";
+  let count = 0;
+  for (let i = 0; i < numString.length; i++) {
+    if (is369(+numString[i])) count += 1;
+  }
+
+  return count;
+};
+
 module.exports = problem3;
