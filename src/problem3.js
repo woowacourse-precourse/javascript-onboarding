7d4338b6c60259146f776e0568248bda5f4b85ce
박승훈 <tmdgns1126@naver.com>
2022-10-29 21:35:49 +0900

    [P3] Feat: 369 개수 계산 함수 가정해서 결과 반환 로직 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  // 369 체크를 위한 set
+  const num369 = new Set(["3", "6", "9"]);
+  let ans = 0;
+
+  // 1부터 number까지의 각 숫자마다 369의 개수를 ans에 더함
+  for (let n = 1; n <= number; n++) {
+    ans += count369(n);
+  }
+  return ans;
 }
 
 module.exports = problem3;
