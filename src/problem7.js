6ac50ca7b6ba402a47c543e651cfd2cfbc1fd328
jeonjeunghoon <jeonjeunghoon@gmail.com>
2022-10-30 03:29:53 +0900

    feat(problem7): solution 메서드 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -15,11 +15,154 @@ const VALIDITY = {
   maxLengthOfVisitor: 10000,
 };
 
+const ARRAY = {
+  vertex1: 0,
+  vertex2: 1,
+};
+
+const Score = {
+  friendOfFriend: 10,
+  visit: 1,
+};
+
 function problem7(user, friends, visitors) {
   if (!isValidInput(user, friends, visitors)) {
     console.log('무효한 인풋!');
     return RETURN.invalidInput;
   }
+
+  return solution(user, friends, visitors);
+}
+
+function solution(user, friends, visitors) {
+  const graph = getGraph(user, friends);
+  const scoreBoard = getScoreBoard(friends, visitors);
+  gradeFriendsScore(user, graph, scoreBoard);
+  gradeVisitorsScore(visitors, scoreBoard);
+  const refinedBoard = refindBoard(scoreBoard, user, graph);
+  return getNewFriendsList(refinedBoard);
+}
+
+function addVertex(graph, vertex) {
+  if (graph[vertex]) {
+    return;
+  }
+  graph[vertex] = [];
+}
+
+function addEdge(graph, vertex1, vertex2) {
+  if (!graph[vertex1]) {
+    addVertex(graph, vertex1);
+  }
+
+  if (!graph[vertex2]) {
+    addVertex(graph, vertex2);
+  }
+
+  graph[vertex1].push(vertex2);
+  graph[vertex2].push(vertex1);
+}
+
+function getGraph(user, friends) {
+  const graph = {};
+
+  addVertex(graph, user);
+  friends.forEach(arr => bulidRelationship(graph, arr));
+
+  return graph;
+}
+
+function bulidRelationship(graph, arr) {
+  arr.forEach(v => addVertex(graph, v));
+  addEdge(graph, arr[ARRAY.vertex1], arr[ARRAY.vertex2]);
+}
+
+function getScoreBoard(friends, visitors) {
+  const scoreBoard = [];
+
+  addFriendsInBoard(scoreBoard, friends);
+  addVisitorsInBoard(scoreBoard, visitors);
+
+  return scoreBoard;
+}
+
+function addFriendsInBoard(scoreBoard, friends) {
+  friends.forEach(arr => arr.forEach(v => pushObejct(scoreBoard, v)));
+}
+
+function addVisitorsInBoard(scoreBoard, visitors) {
+  visitors.forEach(v => pushObejct(scoreBoard, v));
+}
+
+function pushObejct(scoreBoard, v) {
+  if (checkValueInScoreBoard(scoreBoard, v)) {
+    return;
+  }
+
+  scoreBoard.push({ id: v, score: 0 });
+}
+
+function checkValueInScoreBoard(scoreBoard, v) {
+  return scoreBoard.some(prop => prop.id === v);
+}
+
+function gradeFriendsScore(user, graph, scoreBoard) {
+  graph[user].forEach(v =>
+    graph[v].forEach(v =>
+      addScore(scoreBoard[findId(scoreBoard, v)], Score.friendOfFriend),
+    ),
+  );
+}
+
+function addScore(friend, score) {
+  friend.score += score;
+}
+
+function findId(scoreBoard, v) {
+  return scoreBoard.findIndex(element => element.id === v);
+}
+
+function gradeVisitorsScore(visitors, scoreBoard) {
+  visitors.forEach(v =>
+    addScore(scoreBoard[findId(scoreBoard, v)], Score.visit),
+  );
+}
+
+function refindBoard(scoreBoard, user, graph) {
+  const refinedBoard = removeUserAndFriendId(scoreBoard, user, graph);
+  return sortScoreBoard(refinedBoard);
+}
+
+function removeUserAndFriendId(scoreBoard, user, graph) {
+  return scoreBoard.filter(
+    v => !isUser(v.id, user) && !isFriend(v.id, graph[user]),
+  );
+}
+
+function isUser(id, user) {
+  return id === user;
+}
+
+function isFriend(id, friends) {
+  return friends.includes(id);
+}
+
+function sortScoreBoard(scoreBoard) {
+  return scoreBoard.sort((a, b) => b.score - a.score);
+}
+
+function getNewFriendsList(sortedBoard) {
+  return sortedBoard
+    .filter((v, i) => checkFiveFriends(i) && checkScore(v.score))
+    .map(v => v.id);
+}
+
+function checkFiveFriends(i) {
+  return i < 5;
+}
+
+function checkScore(score) {
+  return score > 0;
 }
 
 function isValidInput(user, friends, visitors) {
@@ -196,14 +339,14 @@ function checkVisitorsElement(visitors) {
 }
 
 problem7(
-  'mrko',
+  'jeunghoon',
   [
     ['donut', 'andole'],
-    ['donut', 'jun'],
-    ['donut', 'mrko'],
+    ['donut', 'cun'],
+    ['donut', 'jeunghoon'],
     ['shakevan', 'andole'],
-    ['shakevan', 'jun'],
-    ['shakevan', 'mrko'],
+    ['shakevan', 'cun'],
+    ['shakevan', 'jeunghoon'],
   ],
   ['bedi', 'bedi', 'donut', 'bedi', 'shakevan'],
 );
