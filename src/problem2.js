c0d78705691f56d1e75ab403f4e2adbd60200372
nlom0218 <nlom0218@naver.com>
2022-10-29 20:23:57 +0900

    feat: 문제 2 - 연속하여 중복되는 문자열을 제거하는 함수 만들기

--- src/problem2.js
+++ src/problem2.js
@@ -1,22 +1,12 @@
-const isOverlap = (answer, char) => {
-  return answer[answer.length - 1] === char;
-};
-
-function problem2(cryptogram) {
-  let stack = "";
-  let recentDeletedCahr = "";
-  [...cryptogram].forEach((char) => {
-    if (isOverlap(stack, char)) {
-      stack = stack.slice(0, stack.length - 1);
-      recentDeletedCahr = char;
-    }
-    if (recentDeletedCahr !== char) {
-      stack += char;
-      recentDeletedCahr = "";
-    }
+const removeOverlapChar = (string) => {
+  let removeIdx = [];
+  [...string].forEach((char, idx) => {
+    if (char === string[idx - 1]) removeIdx.push(idx, idx - 1);
   });
 
-  return stack;
-}
+  return [...string].filter((_, idx) => !removeIdx.includes(idx)).join("");
+};
+
+function problem2(cryptogram) {}
 
 module.exports = problem2;
