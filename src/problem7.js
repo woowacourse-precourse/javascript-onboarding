cfef925db5e71e6514cc5aa57a2284c4326438db
evencoding <evencoding@gmail.com>
2022-10-29 18:48:21 +0900

    [문제7] refactor: 최상단 변수 메인함수 안으로 넣기, makeRelations 함수 for문 -> reduce문

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,4 @@
-const relations = {};
-const score = {};
-
-function getSortedScoreArr() {
+function getSortedScoreArr(score) {
   const scoreArr = Object.entries(score);
   scoreArr.sort((a, b) => {
     if (a[1] < b[1]) return 1;
@@ -11,55 +8,65 @@ function getSortedScoreArr() {
     if (a[0] > b[0]) return 1;
     return 0;
   });
+
   return scoreArr;
 }
 
-function calculateScore(user, visitors) {
-  relations[user].forEach((friend) => {
-    if (friend in relations) {
-      relations[friend].forEach((f) => {
-        if (f in score) {
-          score[f] += 10;
-        } else {
-          score[f] = 10;
-        }
-      });
-    }
-  });
+function calculateScore(user, visitors, relations) {
+  const score = {};
+  if (user in relations) {
+    relations[user].forEach((friend) => {
+      if (friend in relations) {
+        relations[friend].forEach((f) => {
+          if (f in score) {
+            score[f] += 10;
+          } else {
+            score[f] = 10;
+          }
+        });
+      }
+    });
+  }
   visitors.some((visitor) => {
-    if (relations[user].includes(visitor)) return 0;
+    if (user in relations && relations[user].includes(visitor)) return 0;
     if (visitor in score) {
       score[visitor] += 1;
     } else {
       score[visitor] = 1;
     }
   });
+
+  return score;
 }
 
-function makeRelations(friends, me) {
-  friends.forEach((friend) => {
-    if (friend[0] in relations) {
-      if (friend[1] !== me) {
-        relations[friend[0]].push(friend[1]);
+function makeRelations(me, friends) {
+  const relations = friends.reduce((acc, relation, i) => {
+    if (relation[1] !== me) {
+      if (relation[0] in acc) {
+        acc[relation[0]].push(relation[1]);
+      } else {
+        acc[relation[0]] = [relation[1]];
       }
-    } else {
-      relations[friend[0]] = [friend[1]];
     }
-    if (friend[1] in relations && friend[0] !== me) {
-      if (friend[0] !== me) {
-        relations[friend[1]].push(friend[0]);
+    if (relation[0] !== me) {
+      if (relation[1] in acc) {
+        acc[relation[1]].push(relation[0]);
+      } else {
+        acc[relation[1]] = [relation[0]];
       }
-    } else {
-      relations[friend[1]] = [friend[0]];
     }
-  });
+
+    return acc;
+  }, {});
+
+  return relations;
 }
 
 function problem7(user, friends, visitors) {
   const answer = [];
-  makeRelations(friends, user);
-  calculateScore(user, visitors);
-  const sortedScore = getSortedScoreArr();
+  const relations = makeRelations(user, friends);
+  const score = calculateScore(user, visitors, relations);
+  const sortedScore = getSortedScoreArr(score);
   sortedScore.some((each, i) => {
     if (i > 4) return true;
     answer.push(each[0]);
