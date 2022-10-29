75d2b981259a9c9f0ff142a131b03cfb0e61f1fb
gyeongza <gyeongza@naver.com>
2022-10-29 18:11:49 +0900

    feat: [A-Z], [Z-A], [a-z], [z-a] 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+// [A-Z], [Z-A], [a-z], [z-a] 배열 생성
+const upcArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+const upcArrRe = [...upcArr].reverse();
+const lowercArr = [...upcArr].map(n => n.toLowerCase());
+const lowercArrRe = [...lowercArr].reverse();
 
-module.exports = problem4;
+
+
+
+
+// function problem4(word) {
+//   var answer;
+//   return answer;
+// }
+
+// module.exports = problem4;
