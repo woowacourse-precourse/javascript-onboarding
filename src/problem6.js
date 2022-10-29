ad8ddc69e8a420907db7c360ade6bb797a15b515
TaeeunKim <xodms0309@naver.com>
2022-10-29 20:01:53 +0900

    [6번] feat: 두 글자씩 자른 닉네임을 set에 넣음

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,15 @@
 function problem6(forms) {
   var answer;
   const crew = [];
+  const splittedNicknames = new Set();
+
   for (const [email, nickname] of forms) {
     crew.push({ email: email, nickname: nickname });
+    //2글자씩 자른 닉네임을 set에 넣음
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const slicedNickname = nickname.slice(i, i + 2);
+      splittedNicknames.add(slicedNickname);
+    }
   }
 
   return answer;
