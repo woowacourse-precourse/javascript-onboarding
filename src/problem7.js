16432a98997e04490a2d38b5cca471f6ae705858
imb96 <kimminje7267@naver.com>
2022-10-29 15:15:49 +0900

    [problem7] style : 주석 제거

--- src/problem7.js
+++ src/problem7.js
@@ -1,16 +1,3 @@
-/**
- *
- * @param {*} user
- * @param {*} friends
- * @param {*} visitors
- * @returns
- *
- * 기능 구현
- * 1. 점수를 기록할 장소만들기
- * 2. 나와 친구인 사람들의 목록 만들기
- * 3. 나와 친구인 사람과 친구인 사람 찾아서 10점씩 주기
- * 4. 방문한 사람(visitors)들에게 1점씩 주기
- */
 function problem7(user, friends, visitors) {
   // 점수 기록할 맵 생성
   const answer = [];
