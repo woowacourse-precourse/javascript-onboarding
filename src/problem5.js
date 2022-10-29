e7feed6f99d753424102c74fc6fa1d4de22b16c0
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 16:03:40 +0900

    problem5. 주어진 매개변수에 따라 각 화폐의 수를 산정하는 for 문 작성

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,13 @@ function problem5(money) {
   let moneyArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+  for (let i = 0; i < answer.length; i++) {
+    if (money) {
+      answer[i] = parseInt(money / moneyArray[i]);
+      money = money % moneyArray[i];
+    }
+  }
+
   return;
 }
 
