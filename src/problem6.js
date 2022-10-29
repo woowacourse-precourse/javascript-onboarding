e9209b8fe42e85db560c397f06820562bbf22a52
chex <chex417@naver.com>
2022-10-29 23:45:22 +0900

    feat: 중복된 닉네임을 신청한 크루의 이메일 목록을 만드는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -2,11 +2,28 @@ function problem6(forms) {
   let answer = [];
   let email_set= new Set(); // 중복허용X
   let crew_num = forms.length;
-
+  
   if (crew_num < 2){ // crew가 1명인 경우 종료
     return answer;
   }
   
+  let overlaps = Array.from({length: crew_num}, () => 0); // 닉네임 중복여부 저장
+
+  for (let i=0; i<crew_num; i++){ // i번 크루는 i+1~n번 크루와 비교
+    for (let j=i+1; j<crew_num; j++){
+      if (overlaps[i]==1 && overlaps[j]==1){ // 이미 목록에 있다면 다음으로 넘어가기
+        continue;
+      }
+      if(isOverlapNickname(i, j, forms)){
+        overlaps[i] = 1; // 닉네임 중복 체크
+        overlaps[j] = 1;
+        email_set.add(forms[i][0]); // 이메일목록에 추가
+        email_set.add(forms[j][0]);
+      }
+    }
+  }
+
+  answer = sortEmailArray(email_set); // 이메일 목록 오름차순 정렬
   return answer;
 }
 
