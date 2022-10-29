5fa60d9b152cbfaf425597916ce4288e3cfed978
Jiho1996 <jhp9270@naver.com>
2022-10-29 17:05:15 +0900

    feat : 1부터 입력받은 수까지 탐색하는 로직 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  [...Array(1,number + 1).keys()].forEach(
+    (num) => {
+    }
+    );
+  
   return answer;
+  
 }
 
 module.exports = problem3;
+
