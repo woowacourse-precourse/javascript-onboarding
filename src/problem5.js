04beb3ec77e143d341f676563953566b71edf4a1
woo-jk <201702042@o.cnu.ac.kr>
2022-10-29 13:34:08 +0900

    feat: [PROBLEM5] 배열의 몇 번째 인덱스인지 구하는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -4,11 +4,24 @@ const findLargestMoneyUnit = (money) => {
   if (money >= 5000) return 5000;
   if (money >= 1000) return 1000;
   if (money >= 500) return 500;
-  if (moeny >= 100) return 100;
+  if (money >= 100) return 100;
   if (money >= 50) return 50;
   if (money >= 10) return 10;
   if (money >= 1) return 1;
 };
+
+const findResultArrayIndex = (money) => {
+  if (money === 50000) return 0;
+  if (money === 10000) return 1;
+  if (money === 5000) return 2;
+  if (money === 1000) return 3;
+  if (money === 500) return 4;
+  if (money === 100) return 5;
+  if (money === 50) return 6;
+  if (money === 10) return 7;
+  if (money === 1) return 8;
+};
+
 function problem5(money) {
   var answer;
   return answer;
