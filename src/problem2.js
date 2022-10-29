6782efda544c9542863de1a89e1fd0c926395279
inthhh <glass005533@gmail.com>
2022-10-30 00:46:28 +0900

    p2_noDuplicate count

--- src/problem2.js
+++ src/problem2.js
@@ -3,17 +3,19 @@ function problem2(cryptogram) {
   var cr = cryptogram;
 
   while(1){
-
+    let cnt=0;
     for(let i=0; i<cr.length; ++i){
       if(cr[i] == cr[i+1]){
-        
+        cnt++;
         let front = cr.substring(0,i);
         let back = cr.substring(i+2, cr.length);
         cr = front + back;
         i--;
       }
     }
+    if(cnt==0) break;
   }
+  answer = cr;
   return answer;
 }
 
