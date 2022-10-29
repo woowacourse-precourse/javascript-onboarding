61e30444821d8bfb942a686c342051f0c7d02eaf
semnil5202 <semnil5202@gmail.com>
2022-10-30 03:17:53 +0900

    [문제 4번] feat: 엄마 말씀을 청개구리 사전으로 변환하기

--- src/problem4.js
+++ src/problem4.js
@@ -27,4 +27,30 @@ function makeCapitalAlphabetArray(word) {
   return capitalWordArray;
 }
 
+function checkIndexOfAlphabet(wordArr, forwardArr) {
+  const indexOfWord = [];
+  for (let i = 0; i < wordArr.length; i++) {
+    for (let j = 0; j < forwardArr.length; j++) {
+      if (!(wordArr[i].charCodeAt(0) > 64 && wordArr[i].charCodeAt(0) < 91)) {
+        indexOfWord.push(wordArr[i]);
+        break;
+      }
+      if (wordArr[i] === forwardArr[j]) {
+        indexOfWord.push(forwardArr.indexOf(forwardArr[j]));
+        break;
+      }
+    }
+  }
+  return indexOfWord;
+}
+
+function convertReverseAlphabet(indexArr, reverseArr) {
+  const result = [];
+  for (let i = 0; i < indexArr.length; i++) {
+    if (isNaN(indexArr[i]) || indexArr[i] === " ") result.push(indexArr[i]);
+    else result.push(reverseArr[indexArr[i]]);
+  }
+  return result;
+}
+
 module.exports = problem4;
