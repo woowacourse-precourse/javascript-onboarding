ceef476e15a9c48bddd75fc6baf49cd18799ca30
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-30 02:23:09 +0900

    문제4) feat: 알파벳 여부 체크하는 함수

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,10 @@ function problem4(word) {
   return answer;
 }
 
+const isAlphabet = (char) => {
+  const regex = /^[a-z|A-Z]+$/;
+  if (regex.test(char)) return true;
+  return false;
+};
+
 module.exports = problem4;
