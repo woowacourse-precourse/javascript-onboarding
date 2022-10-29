cf829971293994ccc0463cde2280adb0c6003d63
Ryong-E <think8867@gmail.com>
2022-10-30 01:44:09 +0900

    feat: PROBLEM7 추천 친구 점수 매기는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -3,39 +3,56 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
-function getMyFriend(user,friends){
+function getMyFriend(user, friends) {
   const foundFriends = [];
-  friends.forEach(friend => {
-    if(friend.includes(user)){
-      const friendName = friend.filter(friend => friend !== user);
+  friends.forEach((friend) => {
+    if (friend.includes(user)) {
+      const friendName = friend.filter((friend) => friend !== user);
       foundFriends.push(...friendName);
     }
-  })
+  });
 
   return foundFriends;
 }
 
-function getRecomendFriendList(user,myFriends,friends){
-  const recomendFriends = []
-  myFriends.forEach(myFriend=>{
-    for (let friend of friends){
-      if (friend.includes(myFriend) && friend.includes(user) === false){
-        const recomendFriendName = friend.filter(friend=> friend !== myFriend);
-        recomendFriends.push(...recomendFriendName)
+function getRecomendFriendList(user, myFriends, friends) {
+  const recomendFriends = [];
+  myFriends.forEach((myFriend) => {
+    for (let friend of friends) {
+      if (friend.includes(myFriend) && friend.includes(user) === false) {
+        const recomendFriendName = friend.filter(
+          (friend) => friend !== myFriend
+        );
+        recomendFriends.push(...recomendFriendName);
       }
     }
-  })
+  });
 
-  return recomendFriends
+  return recomendFriends;
 }
 
-function getStrangeVisitorList(myFriends,visitors){
+function getStrangeVisitorList(myFriends, visitors) {
   let totalStrangeVisitorsList = visitors;
-  for (let name of myFriends){
-      totalStrangeVisitorsList = totalStrangeVisitorsList.filter(visitor => visitor !== name);
+  for (let name of myFriends) {
+    totalStrangeVisitorsList = totalStrangeVisitorsList.filter(
+      (visitor) => visitor !== name
+    );
   }
 
   return totalStrangeVisitorsList;
+}
+
+function scoreConversion(recomendFriends, plusScore) {
+  const score = recomendFriends.reduce((total, current) => {
+    if (!total[current]) {
+      total[current] = plusScore;
+    } else if (current in total) {
+      total[current] += plusScore;
+    }
+    return score;
+  }, {});
+
+  return score;
 }
 
 module.exports = problem7;
