1622d1fd3fd58309c1fb87ba5efedfd850917371
Dohyeon Lee <dlehgus1110@naver.com>
2022-10-29 19:46:13 +0900

    refactor: 문제1 예외사항 처리 로직 가독성 개선
    pobi[0]의 형식으로 숫자를 표현하여 의미가 명확하지 않았음.
    pobiLeftNubmer 형식의 변수로 변경하여 보다 명확한 숫자의 의미를 가지도록 하였음.
    
    pobi와 clong의 예외처리 로직이 합쳐져 있어 가독성이 좋지 않았음.
    각 로직을 함수로 분리하여 가독성을 개선하였음.

--- src/problem1.js
+++ src/problem1.js
@@ -1,27 +1,41 @@
 function problem1(pobi, crong) {
-  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1
-  if (pobi[0] === 1 || crong[0] === 1 || pobi[0] === 399 || crong[0] === 399) return -1
+	if (!isValidPageNumbers(pobi, crong)) return -1
 
-  const pobiScore = Math.max(
-    ...pobi.map(pageNum => Math.max(sumEachDigit(pageNum), multiplyEachDigit(pageNum)))
-  )
-  const crongScore = Math.max(
-    ...crong.map(pageNum => Math.max(sumEachDigit(pageNum), multiplyEachDigit(pageNum)))
-  )
-  return pobiScore > crongScore ? 1 : pobiScore < crongScore ? 2 : 0
+	const pobiScore = Math.max(...pobi.map(pageNum => Math.max(sumEachDigit(pageNum), multiplyEachDigit(pageNum))))
+	const crongScore = Math.max(...crong.map(pageNum => Math.max(sumEachDigit(pageNum), multiplyEachDigit(pageNum))))
+	return pobiScore > crongScore ? 1 : pobiScore < crongScore ? 2 : 0
+}
+const isValidPageNumbers = (pobi, crong) => {
+	const [pobiLeftNumber, pobiRightNumber] = pobi
+	const [crongLeftNumber, crongRightNumber] = crong
+
+	if (!isValidPageOrder(pobiLeftNumber, pobiRightNumber)) return false
+	if (!isValidPageOrder(crongLeftNumber, crongRightNumber)) return false
+	if (isFirstPage(pobiLeftNumber, crongLeftNumber)) return false
+	if (isLastPage(pobiLeftNumber, crongLeftNumber)) return false
+	return true
+}
+const isValidPageOrder = (leftNumber, rightNumber) => {
+	return rightNumber - leftNumber === 1
+}
+const isFirstPage = (leftNumber1, leftNumber2) => {
+	return leftNumber1 === 1 || leftNumber2 === 1
+}
+const isLastPage = (leftNumber1, leftNumber2) => {
+	return leftNumber1 === 399 || leftNumber2 === 399
 }
 
 const sumEachDigit = pageNum => {
-  const sumNum = String(pageNum)
-    .split('')
-    .reduce((sum, current) => sum + parseInt(current, 10), 0)
-  return sumNum
+	const sumNum = String(pageNum)
+		.split('')
+		.reduce((sum, current) => sum + parseInt(current, 10), 0)
+	return sumNum
 }
 const multiplyEachDigit = pageNum => {
-  const mulNum = String(pageNum)
-    .split('')
-    .reduce((mul, current) => mul * parseInt(current, 10), 1)
-  return mulNum
+	const mulNum = String(pageNum)
+		.split('')
+		.reduce((mul, current) => mul * parseInt(current, 10), 1)
+	return mulNum
 }
 
 module.exports = problem1
