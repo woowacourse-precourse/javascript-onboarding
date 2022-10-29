eadcd981648a280f3fcf9f2bf6f662bbae0907fa
leeuihyun <meta212121@naver.com>
2022-10-29 20:07:42 +0900

    feat : problem6 사람의 닉네임을 새로운 해시맵에 저장

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,21 @@
+function nicknameWordMap(forms) {
+  let map = new Map();
+  forms.forEach((item) => {
+    let nickname = item[1];
+    for (let i = 0; i < length - 1; i++) {
+      let subStr = nickname.substring(i, i + 2);
+      if (map.has(subStr)) {
+        map.set(subStr, "duplication");
+      } else {
+        map.set(subStr, "one");
+      }
+    }
+  });
+  return map;
+}
 function problem6(forms) {
   var answer;
+  let map = nicknameWordMap(forms);
   return answer;
 }
 
