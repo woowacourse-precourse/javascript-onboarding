147ee30b1fa5bc0bb0573c8fe8c3d84ec0786462
d0422 <rlfehd2013@naver.com>
2022-10-29 16:15:00 +0900

    Feat:문자열을 두글자씩 끊어서 검증을 위한 비교 패턴을 만드는 기능추가

--- src/problem6.js
+++ src/problem6.js
@@ -2,5 +2,13 @@ function problem6(forms) {
   var answer;
   return answer;
 }
-
+function makePatterns(nickname) {
+  const resultArray = [];
+  let x = 0;
+  while (x < nickname.length - 1) {
+    resultArray.push(nickname.substr(x, 2));
+    x++;
+  }
+  return resultArray;
+}
 module.exports = problem6;
