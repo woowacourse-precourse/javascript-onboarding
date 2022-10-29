ef214fb6aa72f88488b31794466b2b540e23583d
LEEJW1953 <jiwonlee1953@naver.com>
2022-10-29 23:51:59 +0900

    5번 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let changes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < 9; i++) {
+    change = parseInt(money / changes[i])
+    answer.push(change);
+    money -= (change * changes[i]);
+  }
   return answer;
 }
 
