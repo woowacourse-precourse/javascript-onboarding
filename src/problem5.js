cace2ec07859967036d8ff3c3cc8b479e80320ca
loco9939 <kls9939@naver.com>
2022-10-29 15:04:40 +0900

    solve: Create problem5 logic

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let moneyInPulse = money;
+  const papers = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return papers.map((paper) => {
+    const count = parseInt(moneyInPulse / paper);
+    moneyInPulse = moneyInPulse % paper;
+    return count;
+  });
 }
 
 module.exports = problem5;
