d06883e3ed9830b3c73df43c642f26306aa81065
cloudedpanther <76900250+cloudedpanther@users.noreply.github.com>
2022-10-30 03:45:30 +0900

    Problem5 (#5)
    * problem5: 기능 목록 작성
    
    * problem5: getPapersUsed 기능 추가
    
    * problem5: 기능 요구사항 해결
    
    * problem5: getPapersUsed 오타 수정

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
+const getPapersUsed = require("./modules/problem5/getPapersUsed.js");
+
 function problem5(money) {
-  var answer;
-  return answer;
+  const papers = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return getPapersUsed(money, papers, 0);
 }
 
 module.exports = problem5;
