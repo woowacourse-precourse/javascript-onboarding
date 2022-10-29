886832e5a3ea7f0052ceb6def5cc81313b21b303
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-29 22:04:13 +0900

    feat(problem1): 게임의 규칙에 따라 각각의 점수 계산 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,11 +1,29 @@
 function isValidPages(pages) {
-  let leftPage = pages[0];
-  let rightPage = pages[1];
+  var leftPage = pages[0];
+  var rightPage = pages[1];
 
   if (1 <= leftPage && leftPage <= 399 && leftPage + 1 == rightPage) {
     return (true);
   }
   return (false);
+}
+
+function getScore(pages) {
+  var score = 0;
+
+  pages.forEach(page => {
+    var pageDigits = String(page).split('');
+    var digitSum = 0;
+    var digitProduct = 1;
+
+    pageDigits.forEach(digit => {
+      digit = Number(digit);
+      digitSum += digit;
+      digitProduct *= digit;
+    });
+    score = Math.max(score, digitSum, digitProduct);
+  });
+  return score;
 }
 
 function problem1(pobi, crong) {
