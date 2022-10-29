12fe132b57f3ee7468e735e70a3a7d678a40a7e3
BB-choi <choibobae0809@gmail.com>
2022-10-29 22:51:44 +0900

    refactor: 문제2 - 재귀함수로 리팩토링

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,14 @@
-function decrypt(cryptogram) {
-  let result = "";
+function problem2(cryptogram) {
+  let decryptedString = "";
 
   for (let i = 0; i < cryptogram.length; i++) {
-    const leftChar = cryptogram[i];
+    const currentChar = cryptogram[i];
 
     let pointer = i + 1;
-    let rightChar = cryptogram[pointer];
+    let comparedChar = cryptogram[pointer];
 
-    while (leftChar === rightChar) {
-      rightChar = cryptogram[++pointer];
+    while (currentChar === comparedChar) {
+      comparedChar = cryptogram[++pointer];
     }
 
     if (pointer !== i + 1) {
@@ -16,32 +16,14 @@ function decrypt(cryptogram) {
       continue;
     }
 
-    result += leftChar;
+    decryptedString += currentChar;
   }
 
-  return result;
-}
-
-function problem2(cryptogram) {
-  let encryptedString = cryptogram;
-  let decryptedString = "";
-  let isNeedDecryption = true;
-
-  while (isNeedDecryption) {
-    decryptedString = decrypt(encryptedString);
-
-    if (decryptedString === encryptedString) {
-      return decryptedString;
-    }
-
-    encryptedString = decryptedString;
-    decryptedString = "";
+  if (decryptedString !== cryptogram) {
+    decryptedString = problem2(decryptedString);
   }
 
   return decryptedString;
 }
-
-console.log(problem2("browoanoommnaon"));
-console.log(problem2("zyelleyz"));
 
 module.exports = problem2;
