6f2918be94253b359c776d7e839716caf0d22d59
subin <newdeas100@gmail.com>
2022-10-29 16:22:12 +0900

    feat(problem2) : deleteSame 함수 기능추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,31 +1,48 @@
 function problem2(cryptogram) {
-  try{
-    if(checkInputErr(cryptogram)){
+  try {
+    if (checkInputErr(cryptogram)) {
       throw new Error("Invalid Input Error");
     }
 
-    
-  }catch(err){
+    const answer = deleteSame(cryptogram);
+    return answer;
+
+  } catch (err) {
     console.log(err);
   }
 }
 
-function checkInputErr(cryptogram){
+function checkInputErr(cryptogram) {
   const vaildType = (input) => typeof input === 'string';
-  const validLength = (input) => 1 <= input.length && input.length <=1000;
-  const validInput = (input) => [...input].every(char => char === char.toLowerCase()); 
+  const validLength = (input) => 1 <= input.length && input.length <= 1000;
+  const validInput = (input) => [...input].every(char => char === char.toLowerCase());
 
-  if(!vaildType(cryptogram)) return true;
-  else if(!validLength(cryptogram)) return true;
-  else if(!validInput(cryptogram)) return true;
+  if (!vaildType(cryptogram)) return true;
+  else if (!validLength(cryptogram)) return true;
+  else if (!validInput(cryptogram)) return true;
   else return false;
 }
 
-function deleteSame(input){
- 
-}
+function deleteSame(input) {
+  const map = new Map();
+  let copyString = input;
 
-console.log(deleteSame("browoanoommnaon"));
+  while (true) {
+    for (let i = 0; i < copyString.length-1; i++) {
+      const [cur, next] = [copyString[i], copyString[i + 1]];
+      if (cur === next) {
+        map.set(cur, 1);
+      }
+    }
+      const keyArr = [...map.keys()];
+      if(keyArr.length ===0) break;
+      const repeatChar = keyArr.join('|');
+      const regex = new RegExp(`(${repeatChar}){2,}`, 'g');
+      copyString = copyString.replace(regex, "");
+      map.clear();
+    }
 
+    return copyString;
+  }
 
-module.exports = problem2;
+  module.exports = problem2;
