9253279f6ca6e63e119a27740cfc2a116868223a
gyeongza <gyeongza@naver.com>
2022-10-29 18:12:47 +0900

    feat: 입력된 값을 나누어 배열로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,9 +1,14 @@
-// [A-Z], [Z-A], [a-z], [z-a] 배열 생성
+//[A-Z], [Z-A], [a-z], [z-a] 배열 생성
 const upcArr = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
 const upcArrRe = [...upcArr].reverse();
 const lowercArr = [...upcArr].map(n => n.toLowerCase());
 const lowercArrRe = [...lowercArr].reverse();
 
+
+//입력된 값을 나누어 배열로 변환
+const inputString = (sentence) => {
+  return sentence.split("")
+}
 
 
 
