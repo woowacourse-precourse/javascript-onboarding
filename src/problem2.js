ed3deefcb4847d2d9085f83ee37bea2e90607e79
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 13:17:01 +0900

    problem2. 연속된 중복 문자를 제외한 문자를 stack 배열에 담는 for of 문 작성

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,14 @@ function problem2(cryptogram) {
   let array = cryptogram.split("");
   let stack = [];
 
+  for (const el of array) {
+    if (el === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(el);
+    }
+  }
+
   return;
 }
 
