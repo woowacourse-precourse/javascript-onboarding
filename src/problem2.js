390354c92b7abe98a2f57079f5f263281c03483b
OrangeVinyl <hap_hazard@naver.com>
2022-10-30 01:26:31 +0900

    Problem2_Stack top

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,7 @@
 function problem2(cryptogram) {
   var answer = [cryptogram[0]]; // cryptogram을 배열로 입력 받아 answer에 대입
+  var top = answer[answer.length - 1]; //stack의 top을 지정 및 변경
+  return answer;
 }
 
 
