d9364bb63cfb3413864ba88d901abb0b39d85670
js43o <js43o@naver.com>
2022-10-29 13:46:35 +0900

    Fead: Add problem3 code.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    for (let c of i.toString()) {
+      if (c > 2 && c % 3 === 0) {
+        answer++;
+      }
+    }
+  }
+
   return answer;
 }
 
