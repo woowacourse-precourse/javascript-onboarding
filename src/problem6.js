1fd6fd994b1335559ee3cd724ce33ffb3991a4fd
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-29 18:29:20 +0900

    [feat] problem6 - 같은 글자 연속적으로 포함되는 닉네임 파악 및 오름차순 정렬 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,44 @@
 function problem6(forms) {
-  var answer;
+  var answer = new Set();
+  let dict = used_pair(forms);
+  answer = check(forms, dict).sort();
   return answer;
 }
 
+//연속된 2개의 문자열 탐색
+function used_pair(forms) {
+  let dict = {};
+  forms.map((form) => {
+    let email = form[0];
+    let name = form[1];
+    for (let i = 0; i < name.length - 1; i++) {
+      let divided_name = name.substring(i, i + 2);
+      if (!(divided_name in dict)) dict[divided_name] = 1;
+      else dict[divided_name] += 1;
+    }
+  });
+  dict = overlapped_word(dict);
+  return dict;
+}
+
+//2개 이상 나온 2글자 연속값 저장하는 배열 만들기
+function overlapped_word(dict) {
+  for (let key in dict) {
+    if (dict[key] < 2) delete dict[key];
+  }
+  return dict;
+}
+
+//해당 닉네임이 겹치는 닉네임인지 확인하고, 이메일 출력하기
+function check(forms, dict) {
+  let ans = [];
+  forms.map((form) => {
+    let email = form[0];
+    let name = form[1];
+    for (let i = 0; i < Object.keys(dict).length; i++) {
+      if (name.includes(Object.keys(dict)[i])) ans.push(email);
+    }
+  });
+  return ans;
+}
 module.exports = problem6;
