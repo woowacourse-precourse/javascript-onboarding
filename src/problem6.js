590f230123804db71c44a6184afc2181a30f2009
An Sol <apdlvmf486@gmail.com>
2022-10-29 15:20:03 +0900

    feat(problem6.js): 중복 닉네임 판별 기능 분리
    중복 닉네임을 판별하는 함수findDuplicate을 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,6 @@
 function problem6(forms) {
 
-  function checkException(crew){
+  function checkException(crew) {
     const email = crew[0];
     const nickName = crew[1];
     const regexForEmail = /.+@email.com/;
@@ -15,33 +15,42 @@ function problem6(forms) {
 
     //닉네임이 한글이 아닌 경우
     if (regexForNickName.test(nickName)) return 2;
-    
+
     return 0;
   }
 
-  for (let idx = 0; idx < forms.length; idx++){
+  for (let idx = 0; idx < forms.length; idx++) {
     if (checkException(forms[idx]) === 1) return `이메일 형식이 틀립니다: ${forms[idx][0]}`;
     if (checkException(forms[idx]) === 2) return `닉네임이 한글이 아닙니다: ${forms[idx][1]}`;
     if (checkException(forms[idx]) === 3) return `이메일의 글자 수를 확인해주세요: ${forms[idx][0]}`;
     if (checkException(forms[idx]) === 4) return `닉네임의 글자 수를 확인해주세요: ${forms[idx][1]}`;
   }
 
-  const result = [];
-  for (let i = 0; i < forms.length; i++){
-    const testCrew = forms.shift();
-    if (result.includes(testCrew[0])) continue;
-    for (let idx = 0; idx < testCrew[1].length - 1; idx++){
-      const testStr = testCrew[1].slice(idx, idx + 2);
-      const repeatedNames = forms.filter(x => x[1].indexOf(testStr) !== -1);
-      if (repeatedNames.length > 0){
-        result.push(testCrew[0]);
-        for (let crew of repeatedNames){
-          if (!(result.includes(crew[0]))) result.push(crew[0]);
+  function findDuplicate(arr, target) {
+    const result = [];
+    for (let idx = 0; idx < target[1].length - 1; idx++) {
+      const testStr = target[1].slice(idx, idx + 2);
+      const repeatedNames = arr.filter(x => x[1].indexOf(testStr) !== -1);
+      if (repeatedNames.length > 0) {
+        result.push(target[0]);
+        for (let crew of repeatedNames) {
+          result.push(crew[0]);
         }
+        return result;
       }
     }
+    return result;
   }
 
+  const result = [];
+  for (let i = 0; i < forms.length; i++) {
+    const testCrew = forms.shift();
+    if (result.includes(testCrew[0])) continue;
+    const duplicates = findDuplicate(forms, testCrew);
+    for (let crew of duplicates) {
+      if (!(result.includes(crew))) result.push(crew);
+    }
+  }
   return result.sort();
 }
 
