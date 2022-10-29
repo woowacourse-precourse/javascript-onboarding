90dcb365c6f218ff6691d23babf60a7373951b1d
foundaway98 <foundaway98@naver.com>
2022-10-29 18:08:49 +0900

    문제 6번 makeNameSliceTable 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -2,7 +2,17 @@
 //key : 두 글자로 나눈 문자열
 //value : 해당 문자열을 가진 닉네임이 포함된 forms의 index
 //value의 길이가 2 이상이라면 해당 value의 key를 가진 인원이 2명 이상 <- 핵심 아이디어
-function makeNameSliceTable(name, index, nameSliceTable) {}
+function makeNameSliceTable(name, index, nameSliceTable) {
+  let nameSlice = "";
+  for (let j = 0; j < name.length - 1; j++) {
+    nameSlice = name.substr(j, 2);
+    if (!(nameSlice in nameSliceTable)) {
+      nameSliceTable[nameSlice] = new Set();
+    }
+    nameSliceTable[nameSlice].add(index);
+  }
+  return nameSliceTable;
+}
 
 //nameSliceTable을 순회하면서 중복되는 nameSlice가 있는 form의 index를 찾는다.
 function findDupForm(nameSliceTable) {}
@@ -15,7 +25,14 @@ function makeAnswerArr(forms, dupForm) {}
 
 function problem6(forms) {
   var answer = [];
-
+  var answer = [];
+  let nameSliceTable = {};
+  for (let i = 0; i < forms.length; i++) {
+    if (forms[i][1].length == 1) {
+      continue;
+    }
+    nameSliceTable = makeNameSliceTable(forms[i][1], i, nameSliceTable);
+  }
   return answer;
 }
 
