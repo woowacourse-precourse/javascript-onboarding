2ffaf3722072437139f97a8acd2c943befade8ae
박승훈 <tmdgns1126@naver.com>
2022-10-29 21:46:46 +0900

    [P5] Feat: 문제 풀이 구현 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,10 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return moneyArr.map((unit) => {
+    const ret = Math.floor(money / unit);
+    money %= unit;
+    return ret;
+  });
 }
 
 module.exports = problem5;
