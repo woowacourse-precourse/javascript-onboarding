75ceba0c3f30476a6ac48043e80326dc281535b3
Sang-Mini <sangmin3062@gmail.com>
2022-10-29 21:39:36 +0900

    [problem1] 페이지의 각 자릿수를 곱하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,17 +1,28 @@
 function problem1(pobi, crong) {
   let answer;
 
-  // 페이지의 숫자를 더하는 함수
+  // 페이지의 각 자릿수를 더하는 함수
   function plusValue(arr) {
-    let plusResult = 0;
     let result = 0;
     let a = String(arr);
 
     for (let i = 0; i < a.length; i++) {
-      plusResult += parseInt(a[i]);
+      result += parseInt(a[i]);
     };
 
-    return result
+    return result;
+  };
+
+  // 페이지의 각 자릿수를 곱하는 함수
+  function multiplyValue(arr) {
+    let result = 1;
+    let a = String(arr);
+
+    for (let i = 0; i < a.length; i++) {
+      result *= parseInt(a[i]);
+    };
+
+    return result;
   };
 
   return answer;
