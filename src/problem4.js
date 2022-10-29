53a260a295b41c0d62845b698f6537542373ee93
subin <newdeas100@gmail.com>
2022-10-29 17:29:40 +0900

    feat(problem4) : makeDictionary 함수 기능추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const dict = new Map();
+  makeDictionary(dict);
+
+  console.log(dict);
 }
+
+function makeDictionary(dict){
+  const alpha = new Array(26).fill(null).map((_,i) => String.fromCharCode(i+65));
+  const reverseAlpha = [...alpha].reverse();
+  for(let i=0; i<alpha.length;i++){
+    const [key,value] = [alpha[i], reverseAlpha[i]];
+    dict.set(key, value);
+  }
+}
+
+
+
+
+problem4("I love you");
+
 
 module.exports = problem4;
