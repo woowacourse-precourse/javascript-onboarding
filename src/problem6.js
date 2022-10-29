b988efc8eb806355c3fcc72bfd46a766b4d6771d
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-10-29 22:25:49 +0900

    feat: 문제 6 같은 글자가 연속적으로 포함된 닉네임 선별 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,34 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let nameSet = [];
+  let emailSet = [];
+  let sequenceCharIndex = [];
+
+  for (let nameNum = 0; nameNum < forms.length; nameNum++) {
+    emailSet = forms[nameNum][0];
+    nameSet = forms[nameNum][1];
+  }
+
+  for (let index = 0; index < nameSet.length; index++) {
+    if (sequenceChar(nameSet[index], nameSet)) {
+      sequenceCharIndex += index;
+      sequenceCharIndex = [...new Set(sequenceCharIndex)];
+    }
+  }
+
   return answer;
 }
 
+function sequenceChar(name, set) {
+  let indexArr = [];
+  for (let i = 0; i < name.length - 1; i++) {
+    let chars = name[i] + name[i + 1];
+    for (let loop = 0; loop < set.length; loop++) {
+      if(!(set[loop].indexOf(chars))){
+        indexArr += loop;
+      }
+    }
+  }
+  return indexArr;
+}
 module.exports = problem6;
