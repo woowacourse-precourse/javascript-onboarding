b4da46433520cffd3592a5d82b2ea5d1dda2ff1b
unqeye <salyuan@naver.com>
2022-10-29 22:56:28 +0900

    feat: 문제3 수의 각 자리 숫자에 3, 6, 9가 들어가는 횟수

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,9 @@
+function countClap(number) {
+  return [...number.toString()].filter((digit) =>
+    ["3", "6", "9"].includes(digit)
+  ).length;
+}
+
 function problem3(number) {
   var answer;
   return answer;
