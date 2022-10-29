a74b8d0c08b98b27976a8113d7fdca46a7e78662
위성노 <92wsn1111@gamilc.com>
2022-10-29 20:49:11 +0900

    feat: Add [문제3] 3,6,9 게임 손뼉치는 횟수 구하는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -5,6 +5,26 @@ function problem3(number) {
   if (number < 1 || number > 10000) return false;
   if (!(/^[0-9]+$/.test(number))) return false;
   
+  /**
+   * 1 부터 number 까지 자연수 중 3,6,9 숫자가 나오는 횟수를 세는 기능
+   * @param {number} number 
+   * @returns 1부터 number 까지 3,6,9 개수 
+   */
+  function checkThreeSixNine(number){
+    // 1 부터 number 까지 숫자를 배열에 저장
+    let arr = [];
+    for (let i = 1; i <= number; i++ ){
+      arr.push(i);
+    };
+    // 1 부터 number 까지 3,6,9 개수 체크
+    const num = arr.join('');
+    const regex = /[3,6,9]/g;
+    const numThreeSixNine = num.match(regex).length;
+
+    return numThreeSixNine;
+  }
+
+  return checkThreeSixNine(number);
 }
 
 module.exports = problem3;
