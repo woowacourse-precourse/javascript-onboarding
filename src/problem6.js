45dd41172233c00a8d321baf77e180902f87721a
pp449 <mma7710@naver.com>
2022-10-30 00:26:29 +0900

    [문제 6] : 오름차순으로 정렬해주는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -8,7 +8,6 @@ const duplicateNicknamePushToEmailList = (emailList) => {
       }
     }
   }
-  
 }
 
 const pushToNickname = (splitedWord, email) => {
@@ -32,9 +31,9 @@ function problem6(forms) {
   }
   duplicateNicknamePushToEmailList(emailList);
   emailList = Array.from(new Set(emailList));
+  emailList.sort();
   console.log(emailList);
+  return emailList;
 }
-
-problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]);
 
 module.exports = problem6;
