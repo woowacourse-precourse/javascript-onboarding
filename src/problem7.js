75415e9b91ea056a49633efc7f891c7185060d5b
SaeWooKKang <87258182+SaeWooKKang@users.noreply.github.com>
2022-10-29 19:57:20 +0900

    feat(PROBLEM7): 1. 모든 유저 이름 구하는 allUserNames 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const peoples = allUserNames(friends, visitors);
+
 }
 
 module.exports = problem7;
+
+const allUserNames = (friends, visitors) => {
+  const peoples = {}; 
+
+  friends.forEach(friend => {
+    if (!peoples[friend[0]]) peoples[friend[0]] = 0;
+    if (!peoples[friend[1]]) peoples[friend[1]] = 0;
+  });
+
+  visitors.forEach(visitor => {
+    if (!peoples[visitor]) peoples[visitor] = 0;
+  })
+  return peoples;
+}
