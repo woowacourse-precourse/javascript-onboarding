cc95536b71efdf366480e4e2a3e8ecd9ea0bf3ee
Jiho1996 <jhp9270@naver.com>
2022-10-29 17:37:42 +0900

    refactor : 인자까지의 숫자배열 만드는 로직  분리

--- src/problem3.js
+++ src/problem3.js
@@ -8,8 +8,12 @@ function problem3(number) {
   const isThreeSixNine = (digit) =>{
     return digit === '3' || digit === '6' || digit === '9'
   }
-
-  [...Array(number + 1).keys()].forEach(
+  /**
+   * 0부터 받은 인자 까지의 수가 담긴 배열
+   */
+  const makeArrFromZeroToNumber = [...Array(number + 1).keys()];
+  
+  makeArrFromZeroToNumber.forEach(
     (num) =>  [...String(num)].forEach(
       (digit) => {
         if (isThreeSixNine(digit))
@@ -17,6 +21,7 @@ function problem3(number) {
       }
     )
   );
+
   return answer;
   
 }
