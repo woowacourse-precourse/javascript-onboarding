7995688e082f9e3099df7f6a7228b1fcb8008e00
elasjay <elasjay@LAPTOP-5ETGJV08.localdomain>
2022-10-30 03:51:29 +0900

    [프리코스 1주차/Web Front] problem3 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
+  var answer = solution(number);
   return answer;
+}
+
+function solution(number){
+  let count = 0;
+  for(let i = 1; i <= number; i++){
+    const numArr = String(i);
+    for(let n of numArr){
+      if(n === '3' || n === '6' || n === '9'){
+        count += 1;
+      }
+    }
+  }
+
+  return count;
 }
 
 module.exports = problem3;
