c60a513e37694cf3623cac32408b14628055484a
pp449 <mma7710@naver.com>
2022-10-30 02:08:44 +0900

    [문제 7] : 1. friendsList에 friends를 Key, value로 매핑하는 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+const inputFriendsList = (friendsList, friends) => {
+  for(let i=0; i<friends.length; i++) {
+    for(let j=0; j<2; j++) {
+      let k = 0;
+      if(j==0) 
+        k = 1;
+      if(friendsList[friends[i][j]] === undefined)
+        friendsList[friends[i][j]] = [friends[i][k]];
+    }
+  }
+  console.log(friendsList);
 }
+
+function problem7(user, friends, visitors) {
+  const friendsList = new Object();
+  inputFriendsList(friendsList, friends);
+}
+
+problem7("mrko", [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ], ["bedi", "bedi", "donut", "bedi", "shakevan"]);
 
 module.exports = problem7;
