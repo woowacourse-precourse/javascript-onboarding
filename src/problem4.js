f1bfa26d7c46c45b4a9cf35c2ba4cf47b26f3322
gyeongza <gyeongza@naver.com>
2022-10-29 18:44:03 +0900

    feat: 2번에서 받은 값을 1번의 배열에서 반대 문자의 인덱스 값을 참조

--- src/problem4.js
+++ src/problem4.js
@@ -7,9 +7,24 @@ const lowercArrRe = [...lowercArr].reverse();
 
 //입력된 값을 나누어 배열로 변환
 const inputString = (sentence) => {
-  return sentence.split("")
+  const inputArr = sentence.split("")
+  
+  //2번에서 받은 값을 1번의 배열에서 반대 문자의 인덱스 값을 참조
+  const answer = []
+  for (let i = 0; i < inputArr.length; i++){
+    if (inputArr[i].charCodeAt() >= 65 && inputArr[i].charCodeAt() <= 90 ) {
+      const numIndex1 = upcArr.indexOf(inputArr[i]);
+      answer.push(upcArrRe[numIndex1]);
+    }
+    else if (inputArr[i].charCodeAt() >= 97 && inputArr[i].charCodeAt() <= 122) {
+      const numIndex2 = lowercArr.indexOf(inputArr[i]);
+      answer.push(lowercArrRe[numIndex2]);
+    }
+    else if (inputArr[i] === " ") {
+      answer.push(" ");
+    }
+  } return answer.join('');
 }
-
 
 
 
