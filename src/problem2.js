74a2c75c48f8dddcc867179a6792fbf1ae063aa7
2je0 <bulljy@naver.com>
2022-10-30 03:20:24 +0900

    ✨ feat(problem2): 문자열 특정 인덱스와 그 다음 인덱스가 같은지 검사하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function lastChar(str, idx) {
   return idx === str.length - 1;
 }
+function sameNext(str, idx) {
+  return str[idx] === str[idx + 1];
+}
 function problem2(cryptogram) {
   var answer;
   return answer;
