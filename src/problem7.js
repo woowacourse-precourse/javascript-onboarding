4799439992ad3e45da779fb7ee410406483205c5
dakyungoh <dakyung.work@gmail.com>
2022-10-30 04:21:52 +0900

    docs: problem7 문제 구조화 및 작업 할 내역 정의

--- src/problem7.js
+++ src/problem7.js
@@ -1,4 +1,9 @@
 function problem7(user, friends, visitors) {
+  // 함께 아는 친구 수 구하기
+  // 함께 아는 친구 수에 따른 점수 더하기
+  // 방문 횟수에 따른 점수 더하기
+  // 이미 친구인 애 빼기
+  // 추천 점수 순으로 정렬해서 최대 5명만 반환하기 
   var answer;
   return answer;
 }
