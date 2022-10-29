cd407657bf7a026a4dba96965a4b0d572d780b37
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-29 16:42:38 +0900

    refactor: 3번 리팩토링
    - 3,6,9 개수 찾는 함수에서 for문이 아닌 정규 표현식 이용하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,20 +1,15 @@
 function problem3(number) {
-  let count = 0;
   function get369Count(num) {
     let str = String(num);
-    let answer = 0;
-    for (let i = 0; i < str.length; i++) {
-      let tempChar = str.charAt(i);
-      if (tempChar === "3" || tempChar === "6" || tempChar === "9") {
-        answer++;
-      }
-    }
-    return answer;
+    return (str.match(/[3,6,9]/g) || []).length;
   }
-  for (let i = 1; i <= number; i++) {
-    count += get369Count(i);
-  }
-  return count;
+
+  let answer = new Array(number)
+    .fill("")
+    .map((_, index) => index + 1)
+    .reduce((acc, curr) => acc + get369Count(curr), 0);
+
+  return answer;
 }
 
 module.exports = problem3;
