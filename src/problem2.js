d847623c6d005c1a15beaa5cdaf18c5924fa7632
hae-on <solbi2004@naver.com>
2022-10-29 22:48:46 +0900

    [문제2] 제한 사항 검사 오류 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,7 @@
 const checkValid = (cryptogram) => {
-  if (typeof cryptogram === 'string') return true;
-  if (cryptogram.length >= 1 && cryptogram.length <= 1000) return true;
-  if (cryptogram === cryptogram.toLowerCase()) return true;
+  if (typeof cryptogram !== 'string') return true;
+  if (cryptogram.length < 1 || cryptogram.length > 1000) return true;
+  if (cryptogram !== cryptogram.toLowerCase()) return true;
   return false;
 };
 
