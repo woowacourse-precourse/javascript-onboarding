9abcf3994f48bc846a94e66b7bed74b35a113d1a
elasjay <elasjay@LAPTOP-5ETGJV08.localdomain>
2022-10-30 03:24:45 +0900

    [프리코스 1주차/Web Front] problem4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,22 @@ function problem4(word) {
   return answer;
 }
 
+function solution(word){
+  new_word = '';
+  for(let i = 0; i<word.length;i++){
+    ascii = word.charCodeAt(i);
+    if(ascii>=65 && ascii <= 90){ // Lower Case
+      new_ch = String.fromCharCode((90-ascii) + 65);
+    }
+    else if(ascii >=97 && ascii <= 122){ // Upper Case
+      new_ch = String.fromCharCode((122-ascii) + 97);
+    }
+    else{
+      new_ch = word[i];
+    }
+    new_word += new_ch;
+  }
+  return new_word;
+}
+
 module.exports = problem4;
