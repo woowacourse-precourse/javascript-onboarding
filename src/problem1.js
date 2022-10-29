dc5130ea6be8dd34b132b57960f20f464ae12827
SorinKwon <ksl020700@gmail.com>
2022-10-29 14:39:57 +0900

    feat(problem1): 최종 점수를 비교해 결과를 반환하는 기능 추가
    포비와 크롱의 점수를 비교하여 결과를 반환하는 기능을 추가함

--- src/problem1.js
+++ src/problem1.js
@@ -19,6 +19,17 @@ function mutiplyPageNumber(n){
   }
   return result;
 }
+function compareScore(a,b){
+  if(a>b){
+    return 1;
+  }
+  if(a<b){
+    return 2;
+  }
+  if(a==b){
+    return 0;
+  }
+}
 function problem1(pobi, crong) {
     let answer;
     return answer;
