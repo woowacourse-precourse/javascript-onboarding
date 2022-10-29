7cba0837bb1b5664e63f91ab74947e7c74dfec49
bae-sh <hyeon1293@gmail.com>
2022-10-29 23:10:50 +0900

    feat : connectFriends 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,4 +1,15 @@
+function connectFriends({ friendsNetwork, idA, idB }) {
+  if (friendsNetwork[idA]) friendsNetwork[idA][friends].push(idB);
+  else friendsNetwork[idA] = { score: 0, friends: [idB] };
+}
 function problem7(user, friends, visitors) {
+  const friendsNetwork = {};
+
+  friends.forEach((idA, idB) => {
+    connectFriends({ friendsNetwork, idA, idB });
+    connectFriends({ friendsNetwork, idB, idA });
+  });
+
   var answer;
   return answer;
 }
