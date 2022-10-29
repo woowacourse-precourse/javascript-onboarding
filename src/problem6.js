e30ae3d27854561f0186296efdbf9e34d50f5af3
jinseo <ghkdwlstj99@gmail.com>
2022-10-29 19:01:07 +0900

    닉네임이 한글인지 확인하는 기능과 닉네임의 길이가 1자 이상 20자 미만인지 확인하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -2,5 +2,22 @@ function problem6(forms) {
   var answer;
   return answer;
 }
+function checkNickLength(forms){
+    let checkNick = forms[1];
+    if(checkNick < 1 || checkNick > 20){
+      return false;
+    }
+    return true;
+  }
+
+function checkNickKorean(forms){
+  let checkNick = forms[1];
+  let koreanNick =  /^[ㄱ-ㅎ|가-힣]+$/
+
+  if(checkNick !== koreanNick){
+    return false;
+  }
+  return true;
+}
 
 module.exports = problem6;
