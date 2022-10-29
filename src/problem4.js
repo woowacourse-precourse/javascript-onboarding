d7f7b6cd07c16ac324d6dceecba13c8b56157f26
chex <chex417@naver.com>
2022-10-29 18:13:52 +0900

    feat: 청개구리 사전 만드는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
 function problem4(word) {
-  var answer;
+  let answer;
   return answer;
+}
+
+function makeFrogDictionary(){
+  let frog_dict = {};
+  const a_ascii = 'a'.charCodeAt(0);
+  const z_ascii = 'z'.charCodeAt(0);
+  const A_ascii = 'A'.charCodeAt(0);
+  const Z_ascii = 'Z'.charCodeAt(0);
+  
+  for (let i = 0; i<=25; i++) {
+    frog_dict[String.fromCharCode(a_ascii+i)] = String.fromCharCode(z_ascii-i); // 소문자
+    frog_dict[String.fromCharCode(A_ascii+i)] = String.fromCharCode(Z_ascii-i); // 대문자
+  }
+  
+  return frog_dict;
 }
 
 module.exports = problem4;
