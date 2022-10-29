035bacdf7f67a5b428a93f0f41ca59aa1942fba3
unsnruu <unsnruu@gmail.com>
2022-10-29 17:20:13 +0900

    ✨ getThree 함수 구현 (problem3)
    만약 숫자가 "3"을 포함하고 있을 시에 해당하는 만큼
    카운팅을 하여서 반환하는 함수를 생성하였습니다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,29 @@
 function problem3(number) {
   var answer;
+
   return answer;
 }
 
 module.exports = problem3;
+
+/**
+ * 1. 3, 6, 9가 있는 숫자들을 찾는다.
+ * 2. 3의 배수 찾기와 헷갈려서는 안된다. includes되느냐의 문제
+ * 3. 숫자 배열을 순회하면서 count를 추가한다.
+ */
+
+/**
+ * @param {number} num
+ * @returns {number}
+ */
+function getThree(num) {
+  const split = num.toString().split("");
+  let count = 0;
+
+  split.forEach((ch) => {
+    ch === "3";
+    count++;
+  });
+
+  return count;
+}
