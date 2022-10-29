3c1999c4d9553146680884c492e2ac2ef494c681
freemoon99 <soloist132@gmail.com>
2022-10-29 15:27:58 +0900

    feat[problem6]: 닉네임 2글자 뽑아내는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
 function problem6(forms) {
   var answer;
+  let data = new Map(); 
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      const test = forms[i][1].substr(j, 2);
+      data.set(test);
+    }    
+  }
+  
   return answer;
 }
 
