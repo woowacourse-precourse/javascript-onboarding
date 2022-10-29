a1b0345bb47daebd064e3e5a4c657584517de2ce
Quartz <skyu.dev@gmail.com>
2022-10-29 21:09:46 +0900

    feat: Solve problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,34 @@
+const FIFTY_THOUS = 50000;
+const TEN_THOUS = 10000;
+const FIVE_THOUS = 5000;
+const ONE_THOUS = 1000;
+const FIVE_HUND = 500;
+const ONE_HUND = 100;
+const FIFTY = 50;
+const TEN = 10;
+const ONE = 1;
+
 function problem5(money) {
-  var answer;
+  var answer = [];
+  const currency = [
+    FIFTY_THOUS,
+    TEN_THOUS,
+    FIVE_THOUS,
+    ONE_THOUS,
+    FIVE_HUND,
+    ONE_HUND,
+    FIFTY,
+    TEN,
+    ONE,
+  ];
+  currency.map((curr, i) => {
+    let count = 0;
+    while (money >= curr) {
+      money = money - curr;
+      count++;
+    }
+    answer.push(count);
+  });
   return answer;
 }
 
