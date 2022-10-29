3603f74fbf3586e2a6ed35b79d303d001cf24457
unknown <sjjoseph106@gmail.com>
2022-10-29 16:34:14 +0900

    :sparkles: problem3 구현

--- src/problem3.js
+++ src/problem3.js
@@ -5,7 +5,14 @@
  * @returns {number}
  */
 function problem3(number) {
-  return 0;
+  let count = 0;
+
+  for (let i = 1; i <= number; i++) {
+    `${i}`
+      .split("")
+      .forEach((num) => (num === "3" || num === "6" || num === "9") && count++);
+  }
+  return count;
 }
 
 console.log(problem3(13)); // 4
