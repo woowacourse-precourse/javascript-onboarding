f985860fed541365794b31c9315d7182733436ac
unqeye <salyuan@naver.com>
2022-10-29 22:45:18 +0900

    feat: 문제1 페이지 번호가 올바른지 확인

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,4 @@
-function checkPages(pages) {
+function isPagesValid(pages) {
   const [left, right] = pages;
   if (1 <= left && left < 400 && left % 2 === 1 && left + 1 === right) {
     return true;
@@ -7,6 +7,10 @@ function checkPages(pages) {
 }
 
 function problem1(pobi, crong) {
+  if (!isPagesValid(pobi) || !isPagesValid(crong)) {
+    return -1;
+  }
+
   var answer;
   return answer;
 }
