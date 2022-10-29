73307050d00e2827247e3de3c02078ac89cd751b
jeonbyeongmin <qudals7613@gmail.com>
2022-10-29 18:53:43 +0900

    Clear `problem4`

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,58 @@
+const alphabets = [
+  "a",
+  "b",
+  "c",
+  "d",
+  "e",
+  "f",
+  "g",
+  "h",
+  "i",
+  "j",
+  "k",
+  "l",
+  "m",
+  "n",
+  "o",
+  "p",
+  "q",
+  "r",
+  "s",
+  "t",
+  "u",
+  "v",
+  "w",
+  "x",
+  "y",
+  "z",
+];
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const characters = [...word];
+
+  const convertedCharacters = characters.map((character) => {
+    const index = alphabets.findIndex((alphabet) => {
+      return alphabet === character.toLowerCase();
+    });
+
+    if (index === -1) {
+      return character;
+    }
+
+    if (isUpperCase(character)) {
+      return alphabets[alphabets.length - 1 - index].toUpperCase();
+    } else {
+      return alphabets[alphabets.length - 1 - index];
+    }
+  });
+
+  return convertedCharacters.join("");
+}
+
+function isUpperCase(alpha) {
+  const upperRegex = /[A-Z]/g;
+
+  return alpha.match(upperRegex) ? true : false;
 }
 
 module.exports = problem4;
