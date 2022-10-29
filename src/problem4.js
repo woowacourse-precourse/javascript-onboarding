b51aeac6fa1beff4ceee33426aa00592bc6f6646
sunny <huiseon37@gmail.com>
2022-10-30 03:12:50 +0900

    [problem4] 대문자  변경 함수 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
+function upperChange(ascii) {
+  const changeAscii = ascii < 78 ? 2 * (77 - ascii) +1 : 2 * (78 - ascii) + 1;
+  
+  return ascii + changeAscii;
+}
+
+function getChangedWord(word) {
+  const wordArr = Array.from(word,(el) => el.charCodeAt());
+
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  // var answer;
+  getChangedWord(word)
+  // return answer;
 }
 
 module.exports = problem4;
