3d73a4d2f44dda7cc9c8a9268ecfc0411b15434e
sujungYu <su_moment@naver.com>
2022-10-29 16:02:16 +0900

    feat(PROBLEM1~PROBLEM5):테스트 통과

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,47 +1,47 @@
-// const problem1 = require("../src/problem1");
-// const problem2 = require("../src/problem2");
-// const problem3 = require("../src/problem3");
-// const problem4 = require("../src/problem4");
+const problem1 = require("../src/problem1");
+const problem2 = require("../src/problem2");
+const problem3 = require("../src/problem3");
+const problem4 = require("../src/problem4");
 const problem5 = require("../src/problem5");
 // const problem6 = require("../src/problem6");
 // const problem7 = require("../src/problem7");
-// describe("problem1", () => {
-//   test("case1", () => {
-//     expect(problem1([97, 98], [197, 198])).toEqual(0);
-//   });
+describe("problem1", () => {
+  test("case1", () => {
+    expect(problem1([97, 98], [197, 198])).toEqual(0);
+  });
 
-//   test("case2", () => {
-//     expect(problem1([131, 132], [211, 212])).toEqual(1);
-//   });
+  test("case2", () => {
+    expect(problem1([131, 132], [211, 212])).toEqual(1);
+  });
 
-//   test("case3", () => {
-//     expect(problem1([99, 102], [211, 212])).toEqual(-1);
-//   });
-// });
+  test("case3", () => {
+    expect(problem1([99, 102], [211, 212])).toEqual(-1);
+  });
+});
 
-// describe("problem2", () => {
-//   test("case1", () => {
-//     expect(problem2("browoanoommnaon")).toEqual("brown");
-//   });
-//   test("case2", () => {
-//     expect(problem2("zyelleyz")).toEqual("");
-//   });
-// });
+describe("problem2", () => {
+  test("case1", () => {
+    expect(problem2("browoanoommnaon")).toEqual("brown");
+  });
+  test("case2", () => {
+    expect(problem2("zyelleyz")).toEqual("");
+  });
+});
 
-// describe("problem3", () => {
-//   test("case1", () => {
-//     expect(problem3(13)).toEqual(4);
-//   });
-//   test("case2", () => {
-//     expect(problem3(33)).toEqual(14);
-//   });
-// });
+describe("problem3", () => {
+  test("case1", () => {
+    expect(problem3(13)).toEqual(4);
+  });
+  test("case2", () => {
+    expect(problem3(33)).toEqual(14);
+  });
+});
 
-// describe("problem4", () => {
-//   test("case1", () => {
-//     expect(problem4("I love you")).toEqual("R olev blf");
-//   });
-// });
+describe("problem4", () => {
+  test("case1", () => {
+    expect(problem4("I love you")).toEqual("R olev blf");
+  });
+});
 
 describe("problem5", () => {
   test("case1", () => {
