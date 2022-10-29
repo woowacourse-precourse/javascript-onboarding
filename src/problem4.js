a808cacfa269bf392ca096ca98493f1fac14c938
Monte <monte6198@gmail.com>
2022-10-29 23:53:01 +0900

    refactor(problem4.js): isUpperCase() 주석 수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,7 @@
 function isUpperCase(ascii) {
   //ascii 코드가 대문자인지 판별하는 함수
   if (ascii >= 65 && ascii <= 90)
-    //ascii 코드가 65 이상 90 이하(대문자 알파벳이라면)라면
+    //ascii 코드가 65 이상 90 이하(대문자 알파벳)라면
     return true; //true를 반환한다.
   return false; //대문자 알파벳이 아니라면 false를 반환한다.
 }
