d8fdb10688f41ee5b9bafa7508fc74bc059e3c1f
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-30 00:03:00 +0900

    feat(문제6): 중복된 닉네임의 이메일을 저장하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,9 +1,23 @@
 function problem6(forms) {
+  const duplicateCrewEmail = [];
   const duplicateNickname = new Set();
 
   for (let i = 0; i < forms.length; i += 1){
     for(let j = 0; j < forms[i][1].length - 1; j += 1){
       duplicateNickname.add(forms[i][1].substr(j, 2));
+    }
+  }
+  
+  for (let nick of duplicateNickname){
+    let count = 0;
+    for(let i = 0; i < forms.length; i += 1){
+      if (forms[i][1].includes(nick)){
+        duplicateCrewEmail.push(forms[i][0]);
+        count += 1;
+      }    
+    }
+    if (count < 2){
+      duplicateCrewEmail.pop();
     }
   }
 }
