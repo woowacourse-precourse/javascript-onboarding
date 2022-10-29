77ab14953429989e47b57313f0f82f79d604b147
박승훈 <tmdgns1126@naver.com>
2022-10-29 16:48:40 +0900

    [P1] Feat: 예외처리 로직 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,16 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const [p1, p2] = pobi;
+  const [c1, c2] = crong;
+
+  // 1. 예외처리로 -1 반환
+  const checkValidation = (pages) => {
+    const [p1, p2] = pages;
+    const check1 = Math.abs(p1 - p2) === 1;
+    const check2 = p1 % 2 && !(p2 % 2);
+    return check1 && check2;
+  };
+
+  if (!checkValidation(pobi) || !checkValidation(crong)) return -1;
 }
 
 module.exports = problem1;
