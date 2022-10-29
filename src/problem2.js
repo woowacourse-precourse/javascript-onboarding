90c151d7a3eeabfa8e89022426c22f5feab90069
jsh3418 <albert5428@gmail.com>
2022-10-29 10:08:32 +0900

    feat: problem2 암호문의 중복 문자가 없을 때까지 반복한 후 결과를 리턴하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,23 +1,33 @@
 function problem2(cryptogram) {
   // 연속하는 중복 문자들을 삭제한 결과를 반환하는 로직을 만든다.
-  let cryptogramArray = cryptogram.split("");
-
-  let prevChr = cryptogramArray[0];
-
-  for (let i = 1; i < cryptogramArray.length; i++) {
-    if (prevChr === cryptogramArray[i]) {
-      delete cryptogramArray[i - 1];
-      delete cryptogramArray[i];
-    } else {
-      prevChr = cryptogramArray[i];
-    }
-  }
-
-  cryptogramArray = cryptogramArray.filter((chr) => chr !== undefined);
-
-  return cryptogramArray.join("");
   // 반환한 결과의 문자가 연속하는 중복 문자가 없도록 반복한다.
-  // 더 이상 중복 문자가 없다면 결과를 반환한다.
+  const cryptogramArray = cryptogram.split("");
+
+  const decrypt = (cryptogram) => {
+    if (typeof cryptogram === "string") return cryptogram;
+
+    let prevChr = cryptogram[0];
+
+    for (let i = 1; i < cryptogram.length; i++) {
+      if (prevChr === cryptogram[i]) {
+        delete cryptogram[i - 1];
+        delete cryptogram[i];
+      } else {
+        prevChr = cryptogram[i];
+      }
+    }
+
+    const decryptedArray = cryptogram.filter((chr) => chr !== undefined);
+
+    // 더 이상 중복 문자가 없다면 결과를 반환한다.
+    if (decryptedArray.length === cryptogram.length) {
+      return decryptedArray.join("");
+    }
+
+    return decrypt(decryptedArray);
+  };
+
+  return decrypt(cryptogramArray);
 }
 
 module.exports = problem2;
