eb296fd9a5fdeffa1ca79b8994529d83bbfeecc4
Hoon <si932174@gmail.com>
2022-10-29 14:27:35 +0900

    problem6 : 첫번째 form 부터 닉네임 배열로 뽑기

--- src/problem6.js
+++ src/problem6.js
@@ -13,7 +13,26 @@ function problem6(forms) {
     return noSingleNickNameForms;
   };
 
+  const duplicateCollector = forms => {
+
+    const duplicateFormsBasket = {};
+
+    for(let i=0 ; i < forms.length ; i++){
+
+      const stringArr = forms[i][1].split("");
+
+    }
+
+    return duplicateFormsBasket;
+  };
+
+  const filteredForms = singleNickNameDeleter(forms);
+  const duplicateForms = duplicateCollector(filteredForms);
+
+  answer = Object.keys(duplicateForms).length === 1 ? duplicateForms[Object.keys(duplicateForms)[0]] : duplicateForms;
+
   return answer;
 }
 
 module.exports = problem6;
+
