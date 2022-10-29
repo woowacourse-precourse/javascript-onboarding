cf36c5375932c6ffc946b10109ac2aef5bd6ae15
letsjo <hyunoh.jo@gmail.com>
2022-10-30 04:39:46 +0900

    [problem6] Add validationArr function for check input value

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,39 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  return duplicateCheckApp(validationArr(forms));
+}
+
+function validationArr(forms) {
+  const validationFilterArr = [];
+  const emailDataArr = [];
+  forms.map((data) => {
+    if (
+      data[0].length >= 11 &&
+      data[0].length < 20 &&
+      data[1].length >= 1 &&
+      data[1].length < 20 &&
+      data[0].includes("@email") &&
+      !/[^가-힣]/.test(data[1]) &&
+      !emailDataArr.includes(data[0])
+    ) {
+      validationFilterArr.push(data);
+      emailDataArr.push(data[0]);
+    }
+  });
+  return validationFilterArr;
+}
+
+function duplicateCheckApp(CrewDataArr) {
+  const duplicateDataArr = [];
+  for (let i = 0; i < CrewDataArr.length; i++) {
+    let TwoLetterNameArr = getNameByTwoLetterArr(CrewDataArr[i]);
+    for(let j = 0; j < TwoLetterNameArr.length; j++) {
+      if(checkNickNameFromCrewData(CrewDataArr[i],TwoLetterNameArr[j])){
+        duplicateDataArr.push(CrewDataArr[i]);
+        CrewDataArr.splice(i,1);
+        i--;
+      }
+    }
+  }
 }
 
 module.exports = problem6;
