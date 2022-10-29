5909eb7b5e89a8982622628f34a8b6f99118bf01
WaiNaat <qoli0703@gmail.com>
2022-10-29 15:48:47 +0900

    feat(src/problem3.js): Add solution for problem 3 \n\n Just count claps for each number.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,29 @@
+/*
+방법 1
+  N자리 숫자의 손뼉치는 횟수를 opt(N)이라 하면
+  opt(N) = opt(N-1)*10 + 3*10^(N-1)
+  opt(1) = 3
+방법 2
+  1부터 하나씩 세는 방법
+
+방법 1은 빠른데 주어지는 수가 너무 무작위라 자릿수 처리때문에 코드가 지저분해짐
+방법 2는 느린데 직관적임
+max(number)=10000이므로 방법 2 사용
+*/
+
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let num=1; num<=number; num++)
+  {
+    num = String(num);
+    for (digit of num)
+    {
+      if (digit == '3' || digit == '6' || digit == '9')
+        answer++;
+    }
+  }
+
   return answer;
 }
 
