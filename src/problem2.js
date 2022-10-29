974f419cad1f65e7d23f5995f81927c7afc4877f
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-30 04:40:16 +0900

    feat: [problem2] - stack 구현

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,24 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function Stack() {
+  this.dataStore = [];
+  this.top = 0;
+  this.push = push;
+  this.pop = pop;
+  this.peek = peek;
+}
+
+function push(element) {
+  this.dataStore[this.top++] = element;
+}
+
+function pop() {
+  return this.dataStore[--this.top];
+}
+
+function peek() {
+  return this.dataStore[this.top - 1];
+}
+
 module.exports = problem2;
