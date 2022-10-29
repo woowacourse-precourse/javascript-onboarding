d17366bab0a36edf0bdd050ebc525483eff0ee54
jeonbyeongmin <qudals7613@gmail.com>
2022-10-29 19:15:20 +0900

    Clear `problem5`

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const monetaryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const wallet = new Array(monetaryUnit.length).fill(0);
+
+  monetaryUnit.forEach((unit, index) => {
+    wallet[index] = Math.floor(money / unit);
+    money = money % unit;
+  });
+
+  return wallet;
 }
 
 module.exports = problem5;
