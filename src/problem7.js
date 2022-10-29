a093b96bbbb8e8e0881f6f38901bec0c5dc4bfd3
biyamn <b1y9mn@gmail.com>
2022-10-29 14:09:25 +0900

    feat(PROBLEM7): user와 이미 친구인 사람의 목록을 alreadyFriendList에 담음

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,32 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  // [ 'donut', 'shakevan' ]
+  let alreadyFriendList = [];
+  for (let i of friends) {
+    if (i[0] === user) {
+      alreadyFriendList.push(i[1]);
+    } else if (i[1] === user) {
+      alreadyFriendList.push(i[0]);
+    }
+  }
+
+
+
+
+
+  let answer = alreadyFriendList;
   return answer;
 }
 
 module.exports = problem7;
+
+console.log(problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]));
