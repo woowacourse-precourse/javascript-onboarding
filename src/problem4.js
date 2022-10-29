d8422377ce1e3dac8e70b048568196fbc75cc2e4
unqeye <salyuan@naver.com>
2022-10-29 23:58:16 +0900

    feat: 문제4 문자가 알파벳 대문자이거나 소문자인지 확인

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,22 @@
+const ASCII_UPPERCASE_A = 65;
+const ASCII_UPPERCASE_Z = 90;
+const ASCII_LOWERCASE_A = 97;
+const ASCII_LOWERCASE_Z = 122;
+
+function isUpperCase(char) {
+  return (
+    ASCII_UPPERCASE_A <= char.charCodeAt(0) &&
+    char.charCodeAt(0) <= ASCII_UPPERCASE_Z
+  );
+}
+
+function isLowerCase(char) {
+  return (
+    ASCII_LOWERCASE_A <= char.charCodeAt(0) &&
+    char.charCodeAt(0) <= ASCII_LOWERCASE_Z
+  );
+}
+
 function problem4(word) {
   var answer;
   return answer;
