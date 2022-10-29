281cf378765ff2b4e8d2a4b5e747f51e68450ee8
yjh1008 <exesea12@gmail.com>
2022-10-29 23:03:04 +0900

    [problem] problem2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  var answer="";
+  let isClash = false;
+  const cryptoArr = Array.from(cryptogram);
+  for(let i= 0; i < cryptoArr.length ; i++) {
+    if(cryptoArr[i] !== cryptoArr[i+1]) {
+      answer += cryptoArr[i];
+    }else {
+      isClash = true;
+      i++;
+    }
+  }
+  const result = isClash ? problem2(answer) : answer;
+  return result;
 }
-
 module.exports = problem2;
