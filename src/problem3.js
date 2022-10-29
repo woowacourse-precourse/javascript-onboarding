41cbced6057c0b542e448008036cae18b8e6516b
박승훈 <tmdgns1126@naver.com>
2022-10-29 21:37:13 +0900

    [P3] Feat: 특정 숫자의 369 개수 계산 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,15 @@
 function problem3(number) {
+  // 인자로 전달된 num에 포함된 369 개수 카운트
+  const count369 = (num) => {
+    let ret = 0;
+    String(num)
+      .split("")
+      .forEach((n) => {
+        if (num369.has(n)) ret++;
+      });
+    return ret;
+  };
+
   // 369 체크를 위한 set
   const num369 = new Set(["3", "6", "9"]);
   let ans = 0;
