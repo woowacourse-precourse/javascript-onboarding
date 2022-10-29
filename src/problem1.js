3ca3b8454e3040671f81d9a5afa5c6a41c2a0697
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-29 22:51:11 +0900

    문제1) feat: 각 자리 숫자 더하기

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,15 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+const sumNumbers = (pageNumber) => {
+  const pageString = pageNumber + "";
+  let score = 0;
+
+  for (let i = 0; i < pageString.length; i++) {
+    score += +pageString[i];
+  }
+
+  return score;
+};
+
 module.exports = problem1;
