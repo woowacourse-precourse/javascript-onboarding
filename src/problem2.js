fe71818b21036693a4d4e8bd4b589d8f9e35bddb
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-29 19:53:18 +0900

    Feat(문제2-기능1): 배열에서 중복문자 제거 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -3,6 +3,14 @@
 // 2. while문으로 1번 함수의 input/output이 같아질때까지 반복
 // 3. 배열을 문자열로 변환후 반환
 
+// 연속하는 중복 배열 필터링
+function removeOverlap(cryptogram) {
+  const newArr = cryptogram.filter(
+    (ele, idx, arr) => !(ele === arr[idx + 1] || ele === arr[idx - 1])
+  );
+  return newArr;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
