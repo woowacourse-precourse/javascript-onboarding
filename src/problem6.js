a217c969e981d00fa859ce17be3afddcadbee034
chex <chex417@naver.com>
2022-10-29 23:38:58 +0900

    feat: 예외사항을 처리하는 기능 추가(크루가 1명인 경우)

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,11 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let crew_num = forms.length;
+
+  if (crew_num < 2){ // crew가 1명인 경우 종료
+    return answer;
+  }
+
   return answer;
 }
 
