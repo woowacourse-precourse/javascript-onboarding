f64c85100b301e6820601615534ab8dea2fe4432
marugy <gyulife7301@naver.com>
2022-10-29 23:23:00 +0900

    solve4

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,19 @@
+function check3(a) {
+  var cnt = 0;
+  while (a > 0) {
+    if (a % 10 == 3 || a % 10 == 6 || a % 10 == 9) {
+      cnt++;
+    }
+    a = parseInt(a / 10);
+  }
+  return cnt;
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (var i = 1; i <= number; i++) {
+    answer += check3(i);
+  }
   return answer;
 }
 
--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  var answer = [];
+  for (var i = 0; i < word.length; i++) {
+    if ("a" <= word[i] && word[i] <= "z") {
+      answer.push(
+        String.fromCharCode(
+          "z".charCodeAt() + ("a".charCodeAt() - word[i].charCodeAt())
+        )
+      );
+    } else if ("A" <= word[i] && word[i] <= "Z") {
+      answer.push(
+        String.fromCharCode(
+          "Z".charCodeAt() + ("A".charCodeAt() - word[i].charCodeAt())
+        )
+      );
+    } else {
+      answer.push(word[i]);
+    }
+  }
+
+  return answer.join("");
 }
 
 module.exports = problem4;
