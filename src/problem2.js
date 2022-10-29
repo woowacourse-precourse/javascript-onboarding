56aba8c21c85451299d7f4cf40698d0fea08cd6c
inthhh <glass005533@gmail.com>
2022-10-30 01:36:00 +0900

    p2_fix and makeFunc

--- src/problem2.js
+++ src/problem2.js
@@ -5,11 +5,10 @@ function problem2(cryptogram) {
   while(1){
     let cnt=0;
     for(let i=0; i<cr.length; ++i){
+      let notDup=0;
       if(cr[i] == cr[i+1]){
         cnt++;
-        let front = cr.substring(0,i);
-        let back = cr.substring(i+2, cr.length);
-        cr = front + back;
+        cr = deleteDup(cr, i);
         i--;
       }
     }
@@ -18,5 +17,13 @@ function problem2(cryptogram) {
   answer = cr;
   return answer;
 }
-
+function deleteDup(crypto, i) {
+    for(let j=i+1; j<crypto.length; ++j){
+      if(crypto[i]!=crypto[j]) { notDup=j; break; }
+    }
+    let front = crypto.substring(0,i);
+    let back = crypto.substring(notDup, crypto.length);
+    crypto = front + back;
+  return crypto;
+}
 module.exports = problem2;
