69e12106aa91b72981cecc2d60f20d529e2fd79d
nuheajiohc <jaehun341012@gmail.com>
2022-10-29 14:14:34 +0900

    문제7 : 사용자와 친구인 사람을 찾는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    var answer;
+    return answer;
+}
+
+
+function friendCheck(user, friends) {
+    let userFriends = [];
+    friends.forEach(friendship => {
+        if (friendship.includes(user)) {
+            const friend = friendship.filter(people => people != user).join();
+            userFriends.push(friend);
+        }
+    })
+    return userFriends;
 }
 
 module.exports = problem7;
