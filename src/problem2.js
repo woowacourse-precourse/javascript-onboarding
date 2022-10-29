0655c89ab7fa82bb3af394b3e4361a15d2793f8e
OrangeVinyl <hap_hazard@naver.com>
2022-10-30 01:27:47 +0900

    Problem2_Push, Pop

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
   var answer = [cryptogram[0]]; // cryptogram을 배열로 입력 받아 answer에 대입
   var top = answer[answer.length - 1]; //stack의 top을 지정 및 변경
+  if (top !== cryptogram[i]) { // stack내의 top에 위치한 내역과 
+    answer.push(cryptogram[i]);
+  }
+  else if (top == cryptogram[i]) {
+    answer.pop(cryptogram[i]);
+  }
   return answer;
 }
 
