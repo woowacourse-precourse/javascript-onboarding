0a1df11f121690d8ce5d48a6b9f7066a8b0a5bf5
subin <newdeas100@gmail.com>
2022-10-30 02:24:22 +0900

    feat(problem7) : initMap 함수 기능추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,29 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendObj = {}
+  const scoreMap = new Map()
+  initMap(friendObj, friends,scoreMap);
+
+  console.log(scoreMap, friendObj);
+
+  
 }
 
+function initMap(obj, friends,scoreMap) {
+  friends.forEach((val) => {
+    const [friendA, friendB] = val;
+    if (!obj[friendA]) {
+      obj[friendA] = [];
+    }
+    if (!obj[friendB]) {
+      obj[friendB] = [];
+    }
+    scoreMap.set(friendB,0);
+    scoreMap.set(friendA,0);
+    obj[friendA].push(friendB);
+    obj[friendB].push(friendA);
+  })
+}
 module.exports = problem7;
+
+
+problem7("mrko",[ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ],["bedi", "bedi", "donut", "bedi", "shakevan"])
