26abe13e7ce11d98332d5093ccc612fc01e1d647
xilucks <siun0331@gmail.com>
2022-10-29 15:17:37 +0900

    feat: problem3 solve
    시간 복잡도를 줄이기 위해 Dynamic Programming을 활용하여 풀고자 했으나,
    문제를 푸는 과정에서 문자열로 결국 자리수 대로 나눠서 dp를 탐색하면 시간복잡도가 동일할 것
    이라고 생각하여 학습했던 Filter를 활용하였음.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
 function problem3(number) {
-  var answer;
+  var answer = countClap(number);
   return answer;
+}
+
+function countClap(maxNumber) {
+  let clap = 0;
+  for (let nowNumber = 1; nowNumber <= maxNumber; nowNumber++) {
+    const nums = [...nowNumber.toString()];
+    clap += nums.filter(
+      (num) => parseInt(num) !== 0 && parseInt(num) % 3 === 0
+    ).length;
+  }
+  return clap;
 }
 
 module.exports = problem3;
