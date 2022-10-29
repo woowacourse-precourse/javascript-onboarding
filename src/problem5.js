2c2e6357137982da2ca0ea9f78f29bc302934fb2
froggy1014 <clcl6084@gmail.com>
2022-10-30 01:47:39 +0900

    feat[5]: 몫, 나머지 계산 출력 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,7 +2,14 @@ function problem5(money) {
   const MONEYUNIT = [10000, 1000, 100, 10, 1]
   const WALLET = [];
   
-
+  MONEYUNIT.map((unit, i) => {
+      const Q = money / unit;
+      const R = money % unit
+      money = R;
+      console.log(Q,R);
+  })
 }
 
-module.exports = problem5;
+console.log(problem5(50237));
+
+// module.exports = problem5;
