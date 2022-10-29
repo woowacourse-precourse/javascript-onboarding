820d885c9849786a3d4a9058aac2f67410550b4c
SeongHo-C <leeseong010@naver.com>
2022-10-29 16:34:26 +0900

    feat(problem4): 알파벳 대문자 반대로 변환 후 반환 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,9 +1,20 @@
 // 알파벳 소문자 반대로 변환하여 반환하는 기능
 
-// 알파벳 대문자 반대로 변환하여 반환하는 기능
+function uppercase(num) {
+  return String.fromCharCode(65 + (90 - num));
+}
 
 function problem4(word) {
-  let answer;
+  let answer = '';
+
+  for (let x of word) {
+    const asciiX = x.charCodeAt();
+
+    if (asciiX >= 65 && asciiX <= 90) {
+      const reverseUpper = uppercase(asciiX);
+      answer += reverseUpper;
+    }
+  }
 
   return answer;
 }
