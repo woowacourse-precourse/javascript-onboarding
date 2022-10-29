84f7b740b2d5915a9712082634f8fc957222b3fd
Yunjae <nuguri206@gmail.com>
2022-10-29 16:51:36 +0900

    feat: [prob2] array에 같은 문자가 있을 때 제거 함수 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,14 @@
 function problem2(cryptogram) {
   const array = [];
+
+  //중복 문자 제거 함수
+  for(let i = 0; i < cryptogram.length; i++){
+    if(array[array.length - 1 ] === cryptogram[i] ){
+      array.pop();
+    }else{
+      array.push(cryptogram[i]);
+    }
+  }
 }
 
 module.exports = problem2;
