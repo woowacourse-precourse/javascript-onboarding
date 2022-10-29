f496262b1f4c6df3e39cd5f439a16a3e4f45cb29
Jiho1996 <jhp9270@naver.com>
2022-10-29 17:19:29 +0900

    refactor : 3 6 9 검증 함수로 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,22 @@
 function problem3(number) {
   let answer = 0;
+  /**
+   * 3 또는 6 또는 9 인지 검사하는 함수.
+   * @param {number} digit 
+   * @returns {boolean}
+   */
+  const isThreeSixNine = (digit) =>{
+    return digit === '3' || digit === '6' || digit === '9'
+  }
+
   [...Array(number + 1).keys()].forEach(
     (num) =>  [...String(num)].forEach(
       (digit) => {
-        if (digit === '3' || digit === '6' || digit === '9')
+        if (isThreeSixNine(digit))
          answer ++;
       }
     )
   );
-  
   return answer;
   
 }
