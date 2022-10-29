75e71519d7a1c93e2aa25cb42829923700c63c1d
kimyou1102 <yoosk5485@naver.com>
2022-10-29 16:29:33 +0900

    feat: 문제 1번 - 각 자리 숫자를 모두 더하고 곱하는 함수를 구현한다.

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,14 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function calc(name) {
+  const arr = [];
+  for (let i = 0; i < name.length; i++) {
+    const nums = String(name[i]).split('').map(x => +x);
+    arr.push(nums.reduce((a, b) => a + b));
+    arr.push(nums.reduce((a, b) => a * b));
+  }
+  return arr;
+}
+
 module.exports = problem1;
