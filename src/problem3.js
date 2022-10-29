6db0793f3fafe9089356bea83212fc4be37b6db3
jsh3418 <albert5428@gmail.com>
2022-10-29 10:39:09 +0900

    feat: problem3 1부터 number까지의 숫자를 하나의 문자열로 합 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
   // 1부터 number까지의 숫자를 하나의 문자열로 합한다.
+  let str = "";
+
+  let i = 1;
+  while (i <= number) {
+    str += i;
+    i++;
+  }
+
   // 문자열에 3,6,9의 개수를 구한다.
 }
 
