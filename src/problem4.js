16ae6e32a9c9f3d543dc3877fe6d4ecf46fabf67
Buyoung Chung <sunarol@naver.com>
2022-10-30 00:23:36 +0900

    feat: convert the character's ASCII code

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,10 @@
+function convertCode(code) {
+  if (code >= 65 && code <= 90) {
+    return code + parseInt((77.5 - code) * 2);
+  } else {
+    return code + parseInt((109.5 - code) * 2);
+  }
+}
 function problem4(word) {
   var answer;
   return answer;
