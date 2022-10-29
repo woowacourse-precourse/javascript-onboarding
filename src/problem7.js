23c522d43bc1ced74f13b1a37ee0278551431d58
Buyoung Chung <sunarol@naver.com>
2022-10-30 00:34:12 +0900

    feat: make friends list

--- src/problem7.js
+++ src/problem7.js
@@ -6,6 +6,12 @@ function initializing(recommendScore, friendsList, friends) {
     }
   }
 }
+function makeFriendsList(friendsList, friends) {
+  for (let friend of friends) {
+    friendsList.get(friend[0]).push(friend[1]);
+    friendsList.get(friend[1]).push(friend[0]);
+  }
+}
 function problem7(user, friends, visitors) {
   var answer = [];
 
@@ -13,6 +19,7 @@ function problem7(user, friends, visitors) {
   let friendsList = new Map();
 
   initializing(recommendScore, friendsList, friends);
+  makeFriendsList(friendsList, friends);
 }
 
 module.exports = problem7;
