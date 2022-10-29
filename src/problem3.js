6902f002f8f38b1c56bc34357114c501c48dfc9c
subin <newdeas100@gmail.com>
2022-10-29 17:06:51 +0900

    feat(problem3) : checkInputErr 함수 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,30 @@
 function problem3(number) {
   let answer = 0;
 
+  try{
+    if(checkInputErr(number)){
+      throw new Error("Invalid input Error");
+    }
+
   for(let i=1; i<=number; i++){
     const cur = [...i.toString()].map(Number);
     answer += findNumberSet(cur).reduce((acc,cur)=> acc+=cur,0);
   }
 
   return answer;
+  }catch(err){
+    console.log(err);
+  }
 }
 
+function checkInputErr(number) {
+  const vaildType = (input) => typeof input === 'number'; //TypeError
+  const validRange = (input) => 1 <= input && input <= 10000; // LengthError
+
+  if (!vaildType(number)) return true;
+  else if (!validRange(number)) return true;
+  else return false;
+}
 
 
 function findNumberSet(inputArray){
