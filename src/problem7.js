7651a2cdd3a30dcbfc564d6d84558a1846a64c91
Buyoung Chung <sunarol@naver.com>
2022-10-30 00:33:40 +0900

    feat: initialize scoreboard and friendslist

--- src/problem7.js
+++ src/problem7.js
@@ -1,8 +1,18 @@
+function initializing(recommendScore, friendsList, friends) {
+  for (let friend of friends) {
+    for (let x of friend) {
+      recommendScore.set(x, 0);
+      friendsList.set(x, []);
+    }
+  }
+}
 function problem7(user, friends, visitors) {
   var answer = [];
 
   let recommendScore = new Map();
   let friendsList = new Map();
+
+  initializing(recommendScore, friendsList, friends);
 }
 
 module.exports = problem7;
