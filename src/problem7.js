e344d647d394a0505ae81764bf1ae76a5dc6f3d1
2je0 <bulljy@naver.com>
2022-10-30 00:22:44 +0900

    ✨ feat(problem7): 방문으로 인해 획득하는 점수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,5 @@
 function getNetwork(friends, network) {
-  for (let friend of friends) {
+  for (const friend of friends) {
     if (!network.has(friend[0])) {
       network.set(friend[0], []);
     }
@@ -11,13 +11,20 @@ function getNetwork(friends, network) {
   }
 }
 function getNetworkScore(user, network, scoreMap) {
-  for (let friend of network.get(user)) {
-    for (let other of network.get(friend)) {
+  for (const friend of network.get(user)) {
+    for (const other of network.get(friend)) {
       if (other === user) continue;
       if (!scoreMap.has(other)) scoreMap.set(other, 0);
       const score = scoreMap.get(other);
       scoreMap.set(other, score + 10);
     }
+  }
+}
+function getVisitScore(visitors, scoreMap) {
+  for (const visitor of visitors) {
+    if (!scoreMap.has(visitor)) scoreMap.set(visitor, 0);
+    const score = scoreMap.get(visitor);
+    scoreMap.set(visitor, score + 1);
   }
 }
 function problem7(user, friends, visitors) {
