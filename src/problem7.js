f3d7873d0389a9340a9708b23eceaf5e28eb25ac
GuHyeonWoo <ghw9174@gmail.com>
2022-10-29 18:17:52 +0900

    P7/점수를 계산하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -3,6 +3,29 @@
 // 2. 아는 친구 리스트와 방문한 사람 리스트를 스코어 매기는 기능
 // 3. 스코어에 따라 친구 정렬
 
+// 점수를 계산하는 함수
+function scoreSave(person_score, user_list, score){
+  // 깊은 복사를 통해 독립적인 리스트 생성
+  let temp_person_score = person_score.slice();
+  // user_list를 순회하며 기존의 리스트에 이미 있는지 확인
+  for(let user_name of user_list){
+    let existence = false;
+
+    for(let i = 0; i < temp_person_score.length; i++){
+      // 이미 있다면 기존의 점수에 추가
+      if(temp_person_score[i]['user_name'] ===  user_name){
+        existence = true;
+        temp_person_score[i]['score'] += score; 
+        break;
+      }
+    }
+    // 없다면 새로 객체 생성하여 저장
+    if(!existence) temp_person_score.push({user_name: user_name, score: score});
+  }
+
+  return temp_person_score;
+}
+
 // 아는사람을 체크하는 함수
 function checkAcquaintance(user, friends){
   // 친구 관계를 저장할 객체
@@ -33,6 +56,10 @@ function problem7(user, friends, visitors) {
   let person_score = [];
   // 아는 사람을 체크한 리스트
   let acquaintance_user = checkAcquaintance(user, friends);
+  
+  // 아는 사람과 방문한 사람의 점수를 계산하여 리스트에 저장
+  person_score = scoreSave(person_score, acquaintance_user, 10).slice();
+  person_score = scoreSave(person_score, visitors, 1).slice();
 
   return answer;
 }
