28c4ae62da27afde8c8db04d2ab10f1b2740817a
wusi-hub <jws9595@naver.com>
2022-10-29 15:26:53 +0900

    problem5 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  let result = []; 
+  for(let i = 0; i < moneyArr.length; i++) {
+    result.push(Math.floor(money / moneyArr[i]));
+    money =  money % moneyArr[i];
+  }
+  return result;
 }
 
 module.exports = problem5;
