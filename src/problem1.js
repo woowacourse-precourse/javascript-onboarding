4c7e99f5da68738e0773353135ca7c787d2e16ea
Azamman327 <wjsdbdud327@naver.com>
2022-10-29 11:36:13 +0900

    Docs: [P1]기능 목록 정리

--- src/problem1.js
+++ src/problem1.js
@@ -4,3 +4,13 @@ function problem1(pobi, crong) {
 }
 
 module.exports = problem1;
+
+
+/*
+1. 더한 값을 리턴할 함수
+2. 곱한 값을 리턴할 함수
+3. 1, 2의 결과를 pobiRslt, crongRslt 배열에 저장
+4. 더한값, 곱한값을 비교하여 가장 큰 수를 변수에 저장
+5. 포비와 크롱의 max값 비교
+6. 결과 출력 1/2/0/-1
+*/
