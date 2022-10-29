5ae7f8b8c178fde1332ed647302251e8f58fcebe
violetwhenisawu <anashape710@gmail.com>
2022-10-29 22:30:10 +0900

    Feat: 각 숫자를 받아, 곱하거나 더했을 때 가장 큰 숫자를 반환하는 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,27 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let pobiLargeNum = getLargeNum(pobi);
+  let crongLargeNum = getLargeNum(crong);
+  console.log(pobiLargeNum, crongLargeNum);
+}
+
+function getLargeNum(arr) {
+  const sumCallback = (accumulator, current) => {
+    return accumulator + current;
+  };
+
+  const multiCallback = (accumulator, current) => {
+    return accumulator * current;
+  };
+
+  let leftPage = arr[0].toString().split("").map(Number);
+  let rightPage = arr[1].toString().split("").map(Number);
+  let max = Math.max(
+    leftPage.reduce(sumCallback),
+    leftPage.reduce(multiCallback),
+    rightPage.reduce(sumCallback),
+    rightPage.reduce(multiCallback)
+  );
+  return max;
 }
 
 module.exports = problem1;
