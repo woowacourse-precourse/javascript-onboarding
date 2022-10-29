dece7835c4e7a42ee70eba18264effc53e04345f
solo5star <solo_5@naver.com>
2022-10-29 20:02:33 +0900

    feat(문제1): 점수 계산에 사용될 전략 함수들 구현
    * `addStrategy(number)`: 책 페이지의 자리수를 모두 더하여 점수로 계산
    * `multiplyStrategy(number)`: 책 페이지의 자리수를 모두 곱하여 점수로
      계산

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,21 @@
+/**
+ * 책 페이지의 각 자리 숫자를 더하여 점수를 계산합니다.
+ * @param {number} page 책 페이지 숫자
+ * @returns 계산된 점수
+ */
+function addStrategy(page) {
+  return String(page).split('').reduce((a, b) => +a + +b);
+}
+
+/**
+ * 책 페이지의 각 자리 숫자를 곱하여 점수를 계산합니다.
+ * @param {number} page 책 페이지 숫자
+ * @returns 계산된 점수
+ */
+function multiplyStrategy(page) {
+  return String(page).split('').reduce((a, b) => +a * +b);
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
