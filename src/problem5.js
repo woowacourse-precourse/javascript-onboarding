57c959bc21d500053d9a1aa33ada95cc5339cbdc
hae-on <solbi2004@naver.com>
2022-10-29 21:35:00 +0900

    [문제5] 동전 종류 배열 담기 & 답 변수명 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const kindOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const countMoney = [];
+
+  return countMoney;
 }
 
 module.exports = problem5;
