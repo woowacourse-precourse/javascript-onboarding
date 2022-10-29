d8c7b2bd0ad9612e5022df650f5b30dffa302584
RooT_Kim <bdd3452@gmail.com>
2022-10-30 03:27:57 +0900

    문제7번) 기능목록 정리 및 함수 선언

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,23 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+	//추천 친구 목록을 반환할 함수 -> solution
+	//전체 사용자 아이디 목록을 구할 함수 -> getUserList
+	//사용자의 점수를 구할 함수 -> getUserPoint
+	//사용자와 함께 아는 친구 수를 구할 함수 -> friendWith
+	//사용자의 현재 친구 목록을 구하는 함수 -> friendNow
+	//타임라인 방문 횟수를 구할 함수 -> countVisit
+	return solution();
 }
+
+function solution() {}
+
+function getUserList() {}
+
+function getUserPoint() {}
+
+function friendWith() {}
+
+function friendNow() {}
+
+function countVisit() {}
 
 module.exports = problem7;
