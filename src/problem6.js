c6d74b89d93a2fbee0e18c4ef5e4a71822ec1af8
박정규 <srk99279@naver.com>
2022-10-29 12:57:25 +0900

    문제 6 테스트용 콘솔로그 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -41,14 +41,4 @@ function problem6(forms) {
   });
 }
 
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
-
 module.exports = problem6;
