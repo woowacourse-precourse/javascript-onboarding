5e47e9e1f4def4d52e226b4720b46009175b7873
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 15:42:28 +0900

    problem4. 알파벳 변환 for 문 작성

--- src/problem4.js
+++ src/problem4.js
@@ -6,6 +6,16 @@ function problem4(word) {
   const lowerAlphabet = upperAlphabet.toLowerCase();
   const lowerReversed = lowerAlphabet.split("").reverse();
 
+  for (let i = 0; i < word.length; i++) {
+    if (upperAlphabet.includes(word[i])) {
+      answer += upperReversed[upperAlphabet.indexOf(word[i])];
+    } else if (lowerAlphabet.includes(word[i])) {
+      answer += lowerReversed[lowerAlphabet.indexOf(word[i])];
+    } else {
+      answer += " ";
+    }
+  }
+
   return;
 }
 
