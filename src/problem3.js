fab38069b9518532133ad4921a996fbe48b0659e
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-29 17:04:12 +0900

    problem3 - feat: 3, 6, 9 확인 함수
    해당 문자열이 3, 6, 9 와 일치하는지 파악

--- src/problem3.js
+++ src/problem3.js
@@ -8,6 +8,26 @@ const each = (iterable, func) => {
 
 const convertToString = element => `${element}`;
 
+const findThreeSixNine = eachString => {
+  const find = true;
+  const THREE = "3";
+  const SIX = "6";
+  const NINE = "9";
+
+  switch (eachString) {
+    case THREE:
+      break;
+    case SIX:
+      break;
+    case NINE:
+      break;
+    default:
+      return false;
+  }
+
+  return find;
+};
+
 function problem3(number) {
   var answer;
   return answer;
