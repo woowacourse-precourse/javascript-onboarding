047543c9aba1e11ffe44c955c2bd2a3e25263a4e
DongWooKim97 <nanaukim@naver.com>
2022-10-29 18:56:43 +0900

    feat(P4): 입력값에 대한 예외처리 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  if(!checkInputValid(word)) {
+    return 'error';
+  }
+
+  //return true
+}
+
+function checkInputValid(word) {
+  if (word.length < 1 || word.length > 1000) return false;
+  return true;
 }
 
 module.exports = problem4;
