30d7fc3bb5360ac695b5357245d7a62d9a51d88a
subin <newdeas100@gmail.com>
2022-10-30 00:37:03 +0900

    fix(problem6): checkInputErr 함수 input > input.length로 수정

--- src/problem6.js
+++ src/problem6.js
@@ -1,16 +1,20 @@
 function problem6(forms) {
   const userInfo = new Map();
   const nickMap = new Map();
+
   makeMaps(userInfo, nickMap, forms);
+
+  console.log(userInfo,nickMap);
+
 }
 
 function checkInputErr(email, nickName){
   const validType = (input) => typeof input === 'string'; //TypeError
-  const validlength = (input,a,b) => a <= input.length && input < b; // RangeError
+  const validlength = (input,a,b) => a <= input.length && input.length < b; // RangeError
   const validEmail = (input) =>{
-    const regex = /^[a-zA-Z0-9]+@email.com/;
+    const regex = /^[a-zA-Z0-9]+@email.com/; 
     return regex.test(input);
-  };
+  }; 
   const validName = (input) =>{
     const regex = /^[ㄱ-ㅎ|가-힣]+$/;
     return regex.test(input);
@@ -28,6 +32,7 @@ function makeMaps(userInfo, nickMap, forms){
   for(let i=0; i<forms.length; i++){
     const [email, nickName] = forms[i];
     if(checkInputErr(email,nickName)) continue;
+
     userInfo.set(nickName, email);
 
     for(let i=0; i<nickName.length-1; i++){
@@ -35,6 +40,9 @@ function makeMaps(userInfo, nickMap, forms){
       nickMap.has(key)? nickMap.set(key,nickMap.get(key)+1) : nickMap.set(key,1) 
     }
   }
+  console.log(userInfo,nickMap);
 }
+
+problem6([["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]]);
 
 module.exports = problem6;
