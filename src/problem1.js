6be15af0193dfb0ecf200273e9ce9d5febc62047
Steadystudy <sgmin206@naver.com>
2022-10-29 22:52:14 +0900

    [problem1] Feat: 책 페이지 룰 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,24 @@
+function isEven(num) {
+  return num%2 === 0;
+}
+
+function isOdd(num) {
+  return num%2 === 1;
+}
+
+function pageRule(pages) {
+  const left = pages[0];
+  const right = pages[1];
+
+  if(right - left !== 1) return false; // '오른쪽 페이지 번호 - 왼쪽 페이지 번호'가 1이 아닐 경우 'false' 리턴
+  if(!isOdd(left)) return false; // 왼쪽페이지가 홀수가 아닐 경우 'false' 리턴
+  if(!isEven(right)) return false; // 오른쪽페이지가 짝수가 아닐 경우 'false' 리턴
+
+  return true;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(!pageRule(pobi) || !pageRule(crong)) return -1;
 }
 
 module.exports = problem1;
