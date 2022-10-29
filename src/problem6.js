5b9d690d114d900436529da4f0f83f9d6230424c
pp449 <mma7710@naver.com>
2022-10-30 00:10:53 +0900

    [문제 6] : 4. 배열의 key를 모두 반복하며 value에 email주소가 2개 이상인 경우 새로운 배열에 넣어주는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,14 +1,21 @@
 const nickname = new Object();
 
+const duplicateNicknamePushToEmailList = (emailList) => {
+  for (const [nick, ema] of Object.entries(nickname)) {
+    if(ema.length > 1) {
+      for(let i=0; i<ema.length; i++) {
+        emailList.push(ema[i]);
+      }
+    }
+  }
+  console.log(emailList);
+}
+
 const pushToNickname = (splitedWord, email) => {
-  if(nickname[splitedWord] === undefined){
+  if(nickname[splitedWord] === undefined)
     nickname[splitedWord] = [email];
-  }
-  else {
-    console.log("hgih");
+  else 
     nickname[splitedWord].push(email);
-  }
-  console.log(nickname);
 }
 
 const split_words = (word, email) => {
@@ -19,9 +26,11 @@ const split_words = (word, email) => {
 }
 
 function problem6(forms) {
+  const emailList = [];
   for(let i=0; i<forms.length; i++) {
     split_words(forms[i][1], forms[i][0]);
   }
+  duplicateNicknamePushToEmailList(emailList);
 }
 
 problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]);
