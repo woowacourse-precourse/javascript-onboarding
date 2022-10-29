87b0ee97d19fb065bc23fbc5ef43682f35d975e6
GC-Park <kuyt3122@naver.com>
2022-10-29 17:00:52 +0900

    (7번)Refactor:Object안에 value값을 array가 아닌 Set으로 초기화 수정

--- src/problem7.js
+++ src/problem7.js
@@ -1,20 +1,20 @@
 /* 
  기능 목록
   1) Object를 생성해 친구 사이인 사람끼리 각자의 배열에 추가
-  2) object안에 key값에 맞게 배열 초기화
+  2) Object안에 key값 생성 및 Set으로 초기화
 */
 
 function connectFreind(f, r){
   for(let i=0; i<f.length; i++){
-    r[f[i][0]].push(f[i][1]);
-    r[f[i][1]].push(f[i][0]);
+    r[f[i][0]].add(f[i][1]);
+    r[f[i][1]].add(f[i][0]);
   }
 }
 
 function initializationObj(f, r){
   for(let i=0; i<f.length; i++){
-    r[f[i][0]]=[]
-    r[f[i][1]]=[]
+    r[f[i][0]]=new Set()
+    r[f[i][1]]=new Set()
   }
 }
 
