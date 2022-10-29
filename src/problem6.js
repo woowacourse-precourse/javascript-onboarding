7a8850fe47c12faac84da6f7490f7f1771d58b7c
unknown <sjjoseph106@gmail.com>
2022-10-29 19:24:29 +0900

    :sparkles: problem6 구현

--- src/problem6.js
+++ src/problem6.js
@@ -5,7 +5,33 @@
  * @returns {string[]}
  */
 function problem6(forms) {
-  return [];
+  const res = [];
+  const seqArr = [];
+  const hashMap = new Map();
+  const mailObj = {};
+
+  forms.forEach((form) => {
+    const seq = [
+      ...form[1]
+        .split("")
+        .map((_, i) => form[1][i] + form[1][i + 1])
+        .slice(0, -1),
+    ];
+    seqArr.push(...seq);
+    mailObj[form[0]] = seq;
+  });
+  seqArr.forEach((seq) =>
+    hashMap.set(seq, hashMap.has(seq) ? hashMap.get(seq) + 1 : 1)
+  );
+  for (const mail in mailObj) {
+    for (let i = 0; i < mailObj[mail].length; i++) {
+      if (hashMap.get(mailObj[mail][i]) > 1) {
+        res.push(mail);
+        break;
+      }
+    }
+  }
+  return res.sort();
 }
 
 console.log(
