fdafbe2c25848d5292d1db0b7fce583c8f714404
Buyoung Chung <sunarol@naver.com>
2022-10-30 00:21:35 +0900

    feat: calculate the number of "3", "6," "9"

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i < number + 1; i++) {
+    let strNum = String(i);
+    for (let x of strNum) {
+      if (x === "3" || x === "6" || x === "9") {
+        answer++;
+      }
+    }
+  }
   return answer;
 }
 
