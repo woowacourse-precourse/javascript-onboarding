8956fe8cd204ac07fd26dcd493e193c14fd43966
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-10-29 18:51:57 +0900

    problem7 친구 추천 후보 목록 만들기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,25 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    var answer;
+
+    // 1. 친구 추천 후보 목록 만들기
+    // - 전체 친구 목록에서 user와 이미 친구인 사람 제외
+    // - 친구의 친구 목록에서 user 제외
+
+    let userFriends = new Set();
+    let otherFriends = new Set();
+    friends.forEach((friend) => {
+        userFriends.add(friend[0]);
+        if (friend[1] !== user) {
+            otherFriends.add(friend[1]);
+        }
+    });
+    visitors.forEach((friend) => {
+        if (userFriends.has(friend) === false) {
+            otherFriends.add(friend);
+        }
+    });
+
+    return answer;
 }
 
 module.exports = problem7;
