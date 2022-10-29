30629d39a55324f5ec9807a0804528319d262c61
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-29 23:33:16 +0900

    [문제2] <1~2단계 기능요구사항> 연속되는 문자열을 제외하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-    var answer;
-    return answer;
+    let wordArr = cryptogram.split("");
+
+    // 1단계~2단계) 연속되는 문자열을 발견하면 해당 문자열과 다음 문자열을 제외하는 함수
+    const findRepetition = (word) => {
+        for (i = 0; i < word.length; i++) {
+            if (word[i] == word[i + 1]) {
+                wordArr.splice(i, 2);
+                break;
+            }
+        }
+    };
 }
 
 module.exports = problem2;
