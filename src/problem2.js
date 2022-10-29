462b8b73d34c8fcea6cad62af3e8d325c7d4cc71
Jiho1996 <jhp9270@naver.com>
2022-10-29 15:56:07 +0900

    refactor : 포인터 움직임pointerMove 함수로 분리

--- src/problem2.js
+++ src/problem2.js
@@ -2,19 +2,28 @@ function problem2(cryptogram) {
   let leftPointer = 0;
   let rightPoiner = 1;
   const cryptogramList = [...cryptogram];
-
+  const pointerMoveBackward = () => {
+    leftPointer -= 1;
+    rightPoiner -= 1;
+  }
+  const pointerMoveForward = () => {
+    leftPointer += 1;
+    rightPoiner += 1;
+  }
+ 
   while (leftPointer >= 0 && rightPoiner < cryptogramList.length){
     if (cryptogramList[leftPointer] === cryptogramList[rightPoiner]){
       cryptogramList.splice(leftPointer, 2);
-      leftPointer -= 1;
-      rightPoiner -= 1;
+      pointerMoveBackward();
       continue;
     }
     if (cryptogramList[leftPointer] !== cryptogramList[rightPoiner]){
-      leftPointer += 1;
-      rightPoiner += 1;
+      pointerMoveForward();
     }
   }
   return cryptogramList.join("");
+
+
+  
 }
 module.exports = problem2;
