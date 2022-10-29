a3a512aa13e839535f04508b32b175d68a9a81f8
imb96 <kimminje7267@naver.com>
2022-10-29 15:03:48 +0900

    [problem3] style: 주석 제거

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,3 @@
-/**
- *
- * @param {*} money
- * @returns
- *
- * 기능 구현
- * 돈의 액수를 큰 금액의 화폐로 나누는 함수 정의
- * 화폐로 나눈 몫을 세어서 결과 배열에 넣기
- * 돈이 일의 단위가 될 때까지 반복하는 반복문 정의
- */
-
 function problem5(money) {
   const coinBox = new Array(9).fill(0);
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
