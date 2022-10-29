268ce16284cce89c1295c1ce492a6bcf922e7e54
loopy-lim <loopy@jnu.ac.kr>
2022-10-30 05:54:10 +0900

    docs: rename from checkFormEmail to checkEmailEmail

--- src/problem6.js
+++ src/problem6.js
@@ -6,14 +6,14 @@ const NickNameChecker = {
     if (!this.checkFormsLength(forms)) {
       return false;
     }
-    forms = forms.filter((form) => this.checkFormEmail(form[0]));
+    forms = forms.filter((form) => this.checkEmailForm(form[0]));
     forms = forms.filter((form) => this.checkNickname(form[1]));
     return true;
   },
   checkFormsLength: function (forms) {
     return forms.length >= 1 && forms.length <= 10000;
   },
-  checkFormEmail: function (emailString) {
+  checkEmailForm: function (emailString) {
     let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
     if (!emailString.test(mailformat)) return false;
     return emailString.test(/@email.com/);
