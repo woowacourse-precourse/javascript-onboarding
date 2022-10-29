d79ad8bee5d9353b4d576b8e0cb053c0593e21cf
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-29 23:37:46 +0900

    feat(문제6): 닉네임에서 두 글자씩 추출하여 저장하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,11 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const duplicateNickname = new Set();
+
+  for (let i = 0; i < forms.length; i += 1){
+    for(let j = 0; j < forms[i][1].length - 1; j += 1){
+      duplicateNickname.add(forms[i][1].substr(j, 2));
+    }
+  }
 }
 
 module.exports = problem6;
