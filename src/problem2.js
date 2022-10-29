32d786db26b35ce1fdb2a6e6add20797db289e7d
Jiho1996 <jhp9270@naver.com>
2022-10-29 15:36:38 +0900

    feat : 포인터가 이동 가능한 유효 범위 지정

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,10 @@ function problem2(cryptogram) {
   let leftPointer = 0;
   let rightPoiner = 1;
   const cryptogramList = [...cryptogram];
+
+  while (leftPointer >= 0 && rightPoiner < cryptogramList.length){
+
+  }
   
 }
 module.exports = problem2;
