cf690f096265fa0a2418ec1c9ec203031e36a9bc
Siby1lA <psb4644@gmail.com>
2022-10-29 17:19:38 +0900

    feat: 큰 금액의 화폐 위주로 지불하는 로직 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  const CASH = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  CASH.forEach((won) => {
+    answer.push(parseInt(money / won));
+    money = money % won;
+  });
   return answer;
 }
 
