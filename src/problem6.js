c0bd57bc7e6e8ce68a340947db21eb453a891092
Gaeun-Jang <110662057+Gaeun-Jang@users.noreply.github.com>
2022-10-29 14:57:49 +0900

    problem6 연속된 중복 글자가 있는지 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    var answer = [];
+    let nicknames = [];
+
+    // 1. 연속된 중복 글자가 있는지 확인
+    for (let i = 0; i < forms.length; i++) {
+        nicknames.push(forms[i][1]);
+    }
+    function checkNickname(nickname, nicknames) {
+        let isUnique;
+        for (let i = 0; i < nickname.length; i++) {
+            let letter = nickname.charAt(i) + nickname.charAt(i + 1);
+            if (letter.length < 2) return;
+            for (let j = 0; j < nicknames.length; j++) {
+                isUnique = nicknames[j].includes(letter) ? false : true;
+                if (isUnique === false && j !== nicknames.indexOf(nickname))
+                    return true;
+            }
+        }
+        return !isUnique;
+    }
+
+    return answer;
 }
 
 module.exports = problem6;
