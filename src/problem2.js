2cf58967a181a6767ab360e55f799f7dabbbaea0
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 00:25:08 +0900

    feat(problem2): 연속하는 중복 문자 삭제 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,16 @@
 function problem2(cryptogram) {
   var answer;
+  var cryptoPop;
+  var cryptoArray = cryptogram.split('');
+  var answerArray = [];
+
+  while (cryptoPop = cryptoArray.pop()) {
+    if (cryptoPop == answerArray[answerArray.length - 1]) {
+      answerArray.pop();
+    } else {
+      answerArray.push(cryptoPop);
+    }
+  }
   return answer;
 }
 
