94138f83e70860c0f71a35b73b18b803656ab8e8
Monte <monte6198@gmail.com>
2022-10-29 21:04:19 +0900

    feat(problem3.js): has369() 및 숫자 내 3/6/9 개수 구하기 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,16 @@ function getDigits(number) {
   return number.toString().split("");
 }
 
+function has369(digits) {
+  let count = 0;
+  for (let i = 0; i < digits.length; i++) {
+    if (digits[i] === "3") count++;
+    if (digits[i] === "6") count++;
+    if (digits[i] === "9") count++;
+  }
+  return count;
+}
+
 function problem3(number) {
   var answer;
   return answer;
