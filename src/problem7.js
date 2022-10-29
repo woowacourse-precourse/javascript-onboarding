18fe4d04ddc6ed84d7f5aa43c226125e76c9e767
solo5star <solo_5@naver.com>
2022-10-30 00:53:42 +0900

    feat(문제7): `User` 클래스 추가와 친구 추천 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,4 +1,22 @@
 /**
+ * 사용자 이름과 사용자가 방문한 사람들의 이름을 저장하는
+ * 간단한 용도의 클래스
+ */
+class User {
+
+  /** @type {string} 사용자의 이름 */
+  name
+
+  /** @type {string[]} 사용자가 방문한 사람들의 이름 */
+  visitors = []
+
+  constructor(name, visitors) {
+    this.name = name;
+    this.visitors = visitors;
+  }
+}
+
+/**
  * SNS 시스템을 나타낸 클래스이며 친구 관계 상태 유지,
  * 친구 관계로부터 점수를 계산하여 친구 추천 계산을 수행한다.
  */
@@ -37,6 +55,43 @@ class SocialNetwork {
    */
   getFriends(username) {
     return this.#friendRelations[username] ?? [];
+  }
+
+  /**
+   * 주어진 유저의 친구 관계, 타임 라인 방문에 따른 친구 추천을 계산한다.
+   * 높은 점수를 획득한 순서로 정렬되어 반환된다.
+   *
+   * 다른 사용자의 타임 라인에 방문한 경우 +1점
+   * 사용자와 함께 아는 친구는 +10점
+   * @param {User} user 친구 추천을 생성할 사용자
+   * @returns {string[]} 친구 추천 목록
+   */
+  getFriendRecommendations(user) {
+    const scores = {};
+    const addScore = (username, score) => {
+      scores[username] = scores[username] ?? 0;
+      scores[username] += score;
+    };
+
+    // 사용자의 타임 라인에 방문은 +1점
+    user.visitors.forEach(username => addScore(username, 1));
+
+    // 사용자와 함께 아는 친구는 +10점
+    const friends = this.getFriends(user.name);
+    for (const friend of friends) {
+      this.getFriends(friend).forEach(_friend => addScore(_friend, 10));
+    }
+
+    // 본인과 친구들은 제외
+    delete scores[user.name];
+    friends.map(friend => delete scores[friend]);
+
+    // 점수 순으로 정렬
+    const friendRecommendations = Object.entries(scores)
+      .sort(([usernameA, scoreA], [usernameB, scoreB]) => scoreB - scoreA)
+      .map(([username, score]) => username);
+
+    return friendRecommendations;
   }
 }
 
