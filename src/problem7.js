5cd06f92f86cddd0ee0526a49d9bf1129a9e71ba
WonBeen Choi <zad1264@naver.com>
2022-10-29 14:59:50 +0900

    refactor: 7번 문제 - 생성자 함수와 메소드 구조로 변경해 사용성 및 가독성 개선

--- src/problem7.js
+++ src/problem7.js
@@ -1,73 +1,79 @@
 /*
  * -- 기능 목록 --
- * 1. { 이름: [친구 목록] } 구조로 모든 친구 목록을 저장한 객체 - friendDictionary
- * 2. friends 배열의 요소를 통해 사전을 갱신할 함수 - updateDictionary(relation, friendDictionary)
- * 3. 방문자 목록과 친구 목록 사전을 통해 추천 결과를 계산하는 함수 - getRecommendList(user, visitors, friendDictionary);
- * 3.1 추천 대상인 추천점수를 획득한 인원들을 { 이름: 점수 } 구조로 모아둔 객체 - recommendUsers
+ * 1. 친구 목록과 관련된 모든 정보를 다루는 객체의 생성자 함수 FriendDictionary
+ * 2. FriendDictionary의 내부 메소드
+ *   2.1 friends와 visitors 정보로 친구 관계, 방문 점수를 추가해 사전을 초기 세팅하는 메소드 - init
+ *   2.2 두 명을 서로 친구 관계로 추가하는 메소드 - addLink
+ *   2.3 유저 이름을 받아, 해당 유저의 사전 데이터를 활용해 5명의 추천 친구 목록을 반환하는 메소드 - getRecommendList(user)
+ *   2.4 정렬 조건에 맞게 비교해주는 내부 비교 함수 - compare
  */
 
-// 점수는 내림차순, 이름은 오름차순으로 정렬하는 비교 함수
-function compareFriend([nameA, scoreA], [nameB, scoreB]) {
-  if (scoreA !== scoreB) {
-    return scoreB - scoreA;
-  }
+function FriendDictionary() {
+  this.friendList = {};
+  this.recommendUsers = {};
 
-  return nameA > nameB ? 1 : -1;
-}
-
-function getRecommendList(user, visitors, friendDictionary) {
-  const recommendUsers = {};
-  const friendList = friendDictionary[user];
-
-  // user의 친구 목록을 돌며 recommendUser에 이미 있다면 점수를 올리고, 없다면 초기화
-  for (let userFriend of friendList) {
-    friendDictionary[userFriend].forEach((friend) => {
-      if (recommendUsers[friend]) {
-        recommendUsers[friend] += 10;
-      } else {
-        recommendUsers[friend] = 10;
-      }
-    });
-  }
-
-  // visitors를 반복하며 같은 과정을 수행한다.
-  for (let visitor of visitors) {
-    if (recommendUsers[visitor]) {
-      recommendUsers[visitor] += 1;
-    } else {
-      recommendUsers[visitor] = 1;
+  // 점수는 내림차순, 이름은 오름차순으로 정렬하는 비교 함수
+  this.compare = ([nameA, scoreA], [nameB, scoreB]) => {
+    if (scoreA !== scoreB) {
+      return scoreB - scoreA;
     }
-  }
+    return nameA > nameB ? 1 : -1;
+  };
 
-  // 이미 친구인 인원들과 자신을 제외하고, 정렬해서 이름만 남겨 길이 5만큼만 남긴 결과
-  const recommendUserList = Object.entries(recommendUsers)
-    .filter(([name]) => !(friendList.includes(name) || name == user))
-    .sort(compareFriend)
-    .map(([name]) => name)
-    .slice(0, 5);
+  this.addLink = ([targetName, friendName]) => {
+    if (this.friendList[targetName]) {
+      this.friendList[targetName].push(friendName);
+    } else {
+      this.friendList[targetName] = [friendName];
+    }
+  };
 
-  return recommendUserList;
-}
+  this.init = (friends, visitors) => {
+    friends.forEach(([firstUser, secondUser]) => {
+      this.addLink([firstUser, secondUser]);
+      this.addLink([secondUser, firstUser]);
+    });
 
-function updateDictionary([targetName, friendName], friendDictionary) {
-  if (friendDictionary[targetName]) {
-    friendDictionary[targetName].push(friendName);
-  } else {
-    friendDictionary[targetName] = [friendName];
-  }
+    for (let visitor of visitors) {
+      if (this.recommendUsers[visitor]) {
+        this.recommendUsers[visitor] += 1;
+      } else {
+        this.recommendUsers[visitor] = 1;
+      }
+    }
+  };
+
+  this.getRecommendList = (user) => {
+    const userFriendList = this.friendList[user];
+
+    // user의 친구 목록을 돌며 recommendUsers에 이미 있다면 점수를 올리고, 없다면 초기화
+    for (let userFriend of userFriendList) {
+      this.friendList[userFriend].forEach((friend) => {
+        if (this.recommendUsers[friend]) {
+          this.recommendUsers[friend] += 10;
+        } else {
+          this.recommendUsers[friend] = 10;
+        }
+      });
+    }
+
+    const recommendUserList = Object.entries(this.recommendUsers)
+      .filter(([name]) => !(userFriendList.includes(name) || name == user))
+      .sort(this.compare)
+      .map(([name]) => name)
+      .slice(0, 5);
+
+    return recommendUserList;
+  };
 }
 
 function problem7(user, friends, visitors) {
-  const friendDictionary = {
-    [user]: [],
-  };
+  const friendDictionary = new FriendDictionary();
+  friendDictionary.init(friends, visitors);
 
-  friends.forEach(([firsetUser, secondUser]) => {
-    updateDictionary([firsetUser, secondUser], friendDictionary);
-    updateDictionary([secondUser, firsetUser], friendDictionary);
-  });
+  const recommendUserList = friendDictionary.getRecommendList(user);
 
-  return getRecommendList(user, visitors, friendDictionary);
+  return recommendUserList;
 }
 
 module.exports = problem7;
