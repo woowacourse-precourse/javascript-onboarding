f0b01a23d856b511b3c9025a5c1122d1470dc86f
chex <chex417@naver.com>
2022-10-29 16:55:05 +0900

    feat: 알파벳 소문자를 배열에 담아 반환하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,7 @@
 function problem2(cryptogram) {
   let answer;
   let check = true;
+  let alpha_arr = getAlphaArray();
 
   while (check){
     check = false;
@@ -9,6 +10,15 @@ function problem2(cryptogram) {
     }
   }
   return answer
+}
+
+function getAlphaArray(){
+  let alpha_arr = [];
+  for (let i=97; i<=122; i++){
+    // 아스키코드를 문자(a~z)로 변환 후 배열에 저장 
+    alpha_arr.push(String.fromCodePoint(i));
+  }
+  return alpha_arr;
 }
 
 module.exports = problem2;
