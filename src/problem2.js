ff5412cc7a39629137172f024c56263ed79136cb
HBSPS <wiyeong79@naver.com>
2022-10-29 14:57:00 +0900

    :sparkles: [문제2] 기능1. 연속하는 중복 문자들을 제거하는 메소드

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,22 @@ function problem2(cryptogram) {
   return answer;
 }
 
+function deleteOverlap(cryptogram) {
+  const stack = [cryptogram[0]];
+  let index = 1;
+  
+  while (index < cryptogram.length) {  
+    if (stack[stack.length-1] != cryptogram[index]) {
+      stack.push(cryptogram[index]);
+    }
+    else {
+      while (stack[stack.length - 1] == cryptogram[index]) {
+        stack.pop();
+      }
+    }
+
+    index++;
+  }
+}
+
 module.exports = problem2;
