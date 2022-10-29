3cf0d0b9778bd064d042779b1e74c1f3907c0294
wisdomin121@naver.com <wisdomin121@naver.com>
2022-10-29 16:56:08 +0900

    p5) 돈의 단위 배열인 moneys와 개수를 담을 배열인 cnts 생성하기

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
+// 기능 목록
+// 1. 돈의 단위 배열인 moneys와 개수를 담을 배열인 cnts 생성하기
+// 2. money를 각각의 moneys로 나눠주고, 몫을 cnts에 저장하기
+
 function problem5(money) {
-  var answer;
-  return answer;
+  let moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let cnts = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 }
 
 module.exports = problem5;
