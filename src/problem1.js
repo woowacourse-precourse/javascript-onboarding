dc21f0cba061c883d2c3b472e2edaedadede00ac
cruelladevil <dev.timetravel@gmail.com>
2022-10-30 01:13:20 +0900

    feat(PROBLEM1): add some constant

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,10 @@
+const FIRST_PAGE = 1;
+const LAST_PAGE = 400;
+const EXCEPTION = -1;
+const POBI = 1;
+const CRONG = 2;
+const DRAW = 0;
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
