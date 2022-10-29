7e0affbabacc235372369f9d7ed059a00dd862ef
2je0 <bulljy@naver.com>
2022-10-29 20:11:18 +0900

    ✨ feat(problem5): problem5 함수 구현
    화폐단위 current 배열을 순회하며 몫을 answer에 저장

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
+const current = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 function problem5(money) {
-  var answer;
+  var answer = [];
+  answer = current.map((cur) => {
+    const ret = Math.trunc(money / cur);
+    money %= cur;
+    return ret;
+  });
   return answer;
 }
-
 module.exports = problem5;
