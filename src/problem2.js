8bd6f7fc18eb248be9c1f012b8f21b0c29edbdec
afds4567 <afds4567@gmail.com>
2022-10-30 08:07:54 +0900

    feat(problem2): (pattern=중복되는 문자열 여부)성립확인  함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const pattern = /(.)\1{1,}/g;
+  let result = cryptogram;
+  while (isRepeat(result, pattern)) {
+    return;
+  }
+  function isRepeat(string, pattern) {
+    return pattern.test(string);
+  }
 }
 
 module.exports = problem2;
