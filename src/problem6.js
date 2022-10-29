685c0dd227db8f39e1338a07fc9d555a5c9340d2
DESKTOP-PDMTIQO\hansu <fathlic5@gmail.com>
2022-10-29 11:54:35 +0900

    problem6 function pass

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,25 @@
 function problem6(forms) {
-  var answer;
+  var answer = solution(forms);
   return answer;
-}
-
+};
+function solution(forms) {
+  let answer = [];
+  let memberList = {};
+  forms.map((form) => {
+    let [_, nickName] = form;
+    for(let i = 0; i < nickName.length -1; i++) {
+      let overlap = nickName.substring(i, i + 2);
+      if(overlap in memberList) memberList[overlap] += 1;
+      else memberList[overlap] = 1;
+    }
+  });
+  forms.map((form) => {
+  let [email, nickName] = form;
+  for(let i = 0; i < nickName.length -1; i++) {
+    let overlap = nickName.substring(i, i + 2);
+    if(memberList[overlap] > 1) answer.push(email);
+  }
+  });
+  return answer.sort();
+};
 module.exports = problem6;
