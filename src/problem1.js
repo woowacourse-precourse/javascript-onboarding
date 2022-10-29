cdd1e7c1612cf1c879669b000a0bcdc6ae09a7c1
cruelladevil <dev.timetravel@gmail.com>
2022-10-30 01:18:57 +0900

    feat(PROBLEM1): add input validation

--- src/problem1.js
+++ src/problem1.js
@@ -6,8 +6,17 @@ const CRONG = 2;
 const DRAW = 0;
 
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!validate(pobi) || !validate(crong)) return EXCEPTION;
+}
+
+function validate(pages) {
+  const [left, right] = pages;
+  const isOutOfBook = left < FIRST_PAGE || right > LAST_PAGE;
+  const isSequence = left + 1 === right;
+
+  if (isOutOfBook || !isSequence) return false;
+
+  return true;
 }
 
 module.exports = problem1;
