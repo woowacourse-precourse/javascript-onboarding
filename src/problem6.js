4dedbd646154bebe12c41d2225efcf94035a4cbb
Chaeduk <kkj2551@naver.com>
2022-10-29 23:50:17 +0900

    feat(problem6) : 문제 해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,24 @@
 function problem6(forms) {
-  var answer;
+  const answer = []
+  const splitNames = forms.map((element) => (
+    element[1]
+          .split('')
+          .map((element, index, origin) => {
+                return index !== origin.length-1 ? element + origin[index+1] : null
+              })
+          .slice(undefined, -1)
+  ))
+
+  forms.forEach((element, eIndex) => {
+    splitNames.forEach((pieces, pIndex) => {
+      if(eIndex !== pIndex && pieces.some((piece)=>element[1].includes(piece)) && answer.at(-1) !== element[0]){
+          answer.push(element[0])
+      }
+    })
+  })
+
+  answer.sort()
+
   return answer;
 }
 
