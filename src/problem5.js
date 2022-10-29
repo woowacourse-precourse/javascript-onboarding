230aa3f9ee18411be2358721e9992c66a2f3f4dc
inthhh <glass005533@gmail.com>
2022-10-30 02:24:55 +0900

    p5_makingForRepeat...

--- src/problem5.js
+++ src/problem5.js
@@ -1,36 +1,17 @@
 function problem5(money) {
   var answer;
   let coins = [0,0,0,0,0,0,0,0,0];
+  let unit = [50000,10000,5000,1000,500,100,50,10,1];
 
-  while(money>0){
-    if(money >= 50000){
-      money -= 50000;
-      coins[0]++;
-    }else if(money >= 10000){
-      money -= 10000;
-      coins[1]++;
-    }else if(money >= 5000){
-      money -= 5000;
-      coins[2]++;
-    }else if(money >= 1000){
-      money -= 1000;
-      coins[3]++;
-    }else if(money >= 500){
-      money -= 500;
-      coins[4]++;
-    }else if(money >= 100){
-      money -= 100;
-      coins[5]++;
-    }else if(money >= 50){
-      money -= 50;
-      coins[6]++;
-    }else if(money >= 10){
-      money -= 10;
-      coins[7]++;
-    }else{
-      money --;
-      coins[8]++;
+  for(let i=0; i<9; ++i){
+    if(money >= unit[i]){
+      for(let n=1; money > unit[i]*n; ++n){
+        money -= unit[i];
+        coins[i]++;
+        if(money <= 0) break;
+      }
     }
+  if(money <= 0) break;
   }
   answer = coins;
   return answer;
