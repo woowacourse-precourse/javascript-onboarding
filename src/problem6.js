cf080583b9bc7aa72c964c1f41dbffc1bd6e1d60
sujungYu <su_moment@naver.com>
2022-10-29 18:34:06 +0900

    feat(PROBLEM6):객체로 닉네임 중복 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    let checkObj = {};
+    forms.forEach(v=> {
+        const [, name] = v;
+        for(let i=0; i<name.length-1; i++) {
+            const checkName = name.substr(i, 2);
+            if(checkObj[checkName]) checkObj[checkName] +=1;
+            else checkObj[checkName] = 1;
+        }
+    })
 }
 
 module.exports = problem6;
