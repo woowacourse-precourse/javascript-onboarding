0517adafb827a314bfd8498d4991fc491d70cde9
2je0 <bulljy@naver.com>
2022-10-29 23:58:25 +0900

    ✨ feat(problem7): 친구관계를 표현한 network 반환하는 함수 구현
    Map 형태로 구현되어 있음

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,19 @@
+function getNetwork(friends) {
+  let network = new Map();
+  for (let friend of friends) {
+    if (!network.has(friend[0])) {
+      network.set(friend[0], []);
+    }
+    if (!network.has(friend[1])) {
+      network.set(friend[1], []);
+    }
+    network.get(friend[0]).push(friend[1]);
+    network.get(friend[1]).push(friend[0]);
+  }
+  return network;
+}
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
-
 module.exports = problem7;
