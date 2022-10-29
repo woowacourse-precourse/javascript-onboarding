3f0d45df70b1810fff039563dd22e6a8c316eded
Inae Choi <inaechoi89@gmail.com>
2022-10-29 10:00:35 +0900

    feat(p4): distinguish upper or lower case

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,10 @@ function problem4(word) {
   var answer = "";
   for (let i = 0; i < word.length; i++) {
     let point = word.charCodeAt(i);
+    if (point >= 65 && point <= 90) {
+    } else if (point >= 97 && point <= 122) {
+    } else {
+    }
   }
   return answer;
 }
