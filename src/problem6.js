a823a00d3d5b579229736f1ccb86772d10a028c4
jsh3418 <albert5428@gmail.com>
2022-10-29 15:26:51 +0900

    feat: problem6 중복된 값의 닉네임을 포함하고 있는 크루들의 이메일을 배열에 담아 리턴하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -18,7 +18,22 @@ function problem6(forms) {
   });
 
   // 중복된 값을 닉네임으로 포함하고 있는 크루들을 찾아 이메일을 한 배열에 넣는다.
+  const duplicateCrewEmails = forms
+    .map((form) => {
+      const email = form[0];
+      const nickName = form[1].split("");
+
+      for (let i = 0; i < nickName.length - 1; i++) {
+        const twoLetterNickName = nickName[i] + nickName[i + 1];
+        if (duplicateNickNames.includes(twoLetterNickName)) {
+          return email;
+        }
+      }
+    })
+    .filter((email) => email !== undefined);
+
   // 배열을 리턴한다.
+  return duplicateCrewEmails;
 }
 
 module.exports = problem6;
