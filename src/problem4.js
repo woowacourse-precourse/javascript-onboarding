a47c8c0b8957fd79543224e77548a1cf08051c5f
wisdomin121@naver.com <wisdomin121@naver.com>
2022-10-29 16:30:08 +0900

    p4) 객체 dictionary 만들기

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
+// 기능 목록
+// 1. 객체 dictionary 만들기
+// 2. word 문자 하나씩 dictionary에 넣어서 변환하기
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const dictionary = {'A':'Z', 'B':'Y', 'C':'X', 'D':'W', 'E':'V', 'F':'U', 'G':'T', 'H':'S', 'I':'R', 'J':'Q', 'K':'P', 'L':'O', 'M':'N', 'N':'M', 'O':'L', 'P':'K', 'Q':'J', 'R':'I', 'S':'H', 'T':'G', 'U':'F', 'V':'E', 'W':'D', 'X':'C', 'Y':'B', 'Z':'A'};
+
+  return dictionary;
 }
 
 module.exports = problem4;
