14f5bffc81a2eb11bcdb5feb1599708bb25902bd
sunny <huiseon37@gmail.com>
2022-10-30 03:45:08 +0900

    [problem4]  문자 변경 함수 구조 변경

--- src/problem4.js
+++ src/problem4.js
@@ -1,23 +1,39 @@
-function LowerChange(ascii) {
-  const changeAscii = ascii < 110 ? 2 * (109 - ascii) +1 : 2 * (110 - ascii) + 1;
-  
-  return ascii + changeAscii;
+function changeAscii(ascii, middleAscii) {
+  const asciiGap = ascii > middleAscii ? 2 * (middleAscii + 1 - ascii) - 1 : 2 * (middleAscii - ascii) +1;
+ 
+  return ascii + asciiGap;
 }
 
-function upperChange(ascii) {
-  const changeAscii = ascii < 78 ? 2 * (77 - ascii) +1 : 2 * (78 - ascii) + 1;
-  
-  return ascii + changeAscii;
+function changeWord(ascii){
+  const ASCII_A = 65;
+  const ASCII_Z = 90;
+  const ASCII_a = 97;
+  const ASCII_z = 122;
+
+  if(ascii>=ASCII_A && ascii<=ASCII_Z){
+      return changeAscii(ascii, parseInt((ASCII_A+ASCII_Z)/2));
+  }
+  else if(ascii>=ASCII_a && ascii<=ASCII_z){
+      return changeAscii(ascii, parseInt((ASCII_a+ASCII_z)/2));
+  }
+  else{
+    return ascii;
+  }
 }
 
 function getChangedWord(word) {
-  const wordArr = Array.from(word,(el) => el.charCodeAt());
+  const wordArr = Array.from(word,(el) => el.charCodeAt()).reduce((acc,cur)=>{
+    const newWord = String.fromCharCode(changeWord(cur));
+    return [...acc, newWord];
+  },[]);
+
+  return wordArr.join('');
 }
 
 function problem4(word) {
-  // var answer;
-  getChangedWord(word)
-  // return answer;
+  const answer = getChangedWord(word)
+  return answer;
 }
+
 
 module.exports = problem4;
