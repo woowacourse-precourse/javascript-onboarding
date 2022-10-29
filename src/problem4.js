ee525b14cb652e9dea463417d76f0d7d0725b459
unsnruu <unsnruu@gmail.com>
2022-10-29 19:14:39 +0900

    ♻️ 불필요한 함수 제거 (problem4)
    problem4에서 곧바로 사용하고 있어 불필요한 층위를 생성하는,
    mapAlphabet 함수를 삭제하였습니다.
    또한 가독성을 위해서 함수의 위치를 조정하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,36 +1,19 @@
-function problem4(word) {
-  return mapAlphabet(word);
-}
-
 /**
  * A -Z를 맵핑할 함수를 생성
  * 해당 함수를 사용해서 문자열을 순회하기
  */
-
-/**
- *
- * @param {string} word
- * @returns {string}
- */
-function mapAlphabet(word) {
+function problem4(word) {
   const split = word.split("");
   const reversed = split.map((ch) => reverseCharacter(ch));
   return reversed.join("");
 }
 
-function getMid(chCode) {
-  let mid = 0;
+module.exports = problem4;
 
-  if (chCode >= "A".charCodeAt() && chCode <= "Z".charCodeAt()) {
-    mid = ("Z".charCodeAt() + "A".charCodeAt()) / 2;
-  } else if (chCode >= "a".charCodeAt() && chCode <= "z".charCodeAt()) {
-    mid = ("z".charCodeAt() + "a".charCodeAt()) / 2;
-  } else {
-    throw new Error("알파벳이 아닙니다");
-  }
-  return mid;
-}
-
+/**
+ * @param {ch} ch 알파벳 값
+ * @returns {string} 반전된 알파벳 값
+ */
 function reverseCharacter(ch) {
   try {
     const chCode = ch.charCodeAt();
@@ -47,4 +30,20 @@ function reverseCharacter(ch) {
   }
 }
 
-module.exports = problem4;
+/**
+ *
+ * @param {number} chCode String.charCodeAt을 사용해 변환된 숫자값
+ * @returns {number} UTF-16 코드를 나타내진 알파벳들의 중간 값
+ */
+function getMid(chCode) {
+  let mid = 0;
+
+  if (chCode >= "A".charCodeAt() && chCode <= "Z".charCodeAt()) {
+    mid = ("Z".charCodeAt() + "A".charCodeAt()) / 2;
+  } else if (chCode >= "a".charCodeAt() && chCode <= "z".charCodeAt()) {
+    mid = ("z".charCodeAt() + "a".charCodeAt()) / 2;
+  } else {
+    throw new Error("알파벳이 아닙니다");
+  }
+  return mid;
+}
