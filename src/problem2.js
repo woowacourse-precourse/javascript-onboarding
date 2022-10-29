8f9c8bc86eca35cdd8d883524c18fc5ba443f70b
paduck-96 <peterrabitt1234@naver.com>
2022-10-29 22:49:08 +0900

    problem2-1

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
   var answer;
-  return answer;
+  const text = [...cryptogram];
+  function checkText(arr) {
+    let i = 0;
+    while (i < 500) {
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] === arr[j + 1]) {
+          arr.splice(j, 2);
+          break;
+        }
+      }
+      i++;
+    }
+    return text;
+  }
+  return checkText(text).join("");
 }
 
 module.exports = problem2;
