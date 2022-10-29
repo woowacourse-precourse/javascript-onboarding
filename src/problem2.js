dbc859e32a908ce1c256dea6872d3c57099af01e
An Sol <apdlvmf486@gmail.com>
2022-10-29 14:24:13 +0900

    fix(problem2.js): 정규표현식 구문 수정
    오타인 {2}를 {2,}로 수정. 2개가 아니라 2개 이상의 중복되는 알파벳을 의미하도록 변경함

--- src/problem2.js
+++ src/problem2.js
@@ -4,7 +4,7 @@ function problem2(cryptogram) {
   let flag = true;
   while (flag){
     for(let num = 'a'.charCodeAt(0); num < 'z'.charCodeAt(0) + 1; num++){
-      const regex = new RegExp(`${String.fromCharCode(num)}{2}`, 'g');
+      const regex = new RegExp(`${String.fromCharCode(num)}{2,}`, 'g');
       newString = newString.replace(regex, '');
     }
     if (newString.length < count){
