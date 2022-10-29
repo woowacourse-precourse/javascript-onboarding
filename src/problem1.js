dd58d96f8f50c9459ac23acf8550f8980e100792
SorinKwon <ksl020700@gmail.com>
2022-10-29 14:29:10 +0900

    feat(problem1):  페이지번호의 각 자리를 더하는 기능 추가
    페이지번호의 각 자리를 더하고 문자열을 다시 숫자로 바꾸어 반환하는 기능을 추가함

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,16 @@ function changeNumberToString(n){
   n=n.toString();
   return n;
 }
+
+function plus(n){
+  let result=0;
+  n = changeNumberToString(n);
+  for(let i=0;i<n.length;i++){
+      result+=parseInt(n[i]);
+  }
+  return result;
+}
+
 function problem1(pobi, crong) {
     let answer;
     return answer;
