d928f2ce3df1dd2522179b78b1a3488252770e5b
confidential-nt <youyoy689@gmail.com>
2022-10-30 05:17:01 +0900

    요구사항 4번 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,58 @@
+const alphabet = [
+  "A",
+  "B",
+  "C",
+  "D",
+  "E",
+  "F",
+  "G",
+  "H",
+  "I",
+  "J",
+  "K",
+  "L",
+  "M",
+  "N",
+  "O",
+  "P",
+  "Q",
+  "R",
+  "S",
+  "T",
+  "U",
+  "V",
+  "W",
+  "X",
+  "Y",
+  "Z",
+];
+
+function createDictionary(array) {
+  const dictionary = {};
+  for (let i = 0; i < array.length; i++) {
+    dictionary[array[i]] = array[array.length - i - 1];
+  }
+
+  return dictionary;
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const dictionary = createDictionary(alphabet);
+
+  if (word.match(/[^a-zA-Z\s+]/g)) return;
+  if (word.length < 1 || word.length > 1000) return;
+
+  const splited = word.split("");
+
+  for (let i = 0; i < splited.length; i++) {
+    if (splited[i] === " ") continue;
+    const replace = dictionary[splited[i].toUpperCase()];
+    if (!splited[i].match(/[A-Z]/g)) {
+      splited[i] = replace.toLowerCase();
+    } else splited[i] = replace;
+  }
+
+  return splited.join("");
 }
 
 module.exports = problem4;
