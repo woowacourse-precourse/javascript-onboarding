fcea838fb039a23f9a73eaad0de3f62bb6683338
dim <dim@student.42seoul.kr>
2022-10-30 06:05:27 +0900

    [problem2]Feat: main structure and exception

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
+  if (cryptogram.length <= 1) return cryptogram;
+  var answer = removeDup(cryptogram.split(''));
   return answer;
 }
 
 module.exports = problem2;
+
+
+/* 
+예외처리
+cryptogram 길이가 1일때 빠른리턴
+
+구현
+1. 길이가 1인 문자열 예외처리
+2. 배열 이용해서 cur인덱스 i와 i+1한 인덱스의 요소 비교해서 같은값인지 확인
+3. 같은 값이 있다면 splice로 지움(기존배열은 지운값으로 재할당)
+3. 중복값이 없을때까지 2, 3 반복 후 리턴
+*/
