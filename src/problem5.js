52bfe9b50c325b17c33432b13c6c8a62edb0dd4c
loopy-lim <loopy@jnu.ac.kr>
2022-10-29 12:57:59 +0900

    feat:  money 는 1 이상 1,000,000 이하인 자연수이다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,18 @@
+const Bank = {
+  amount: 0,
+  result: Array.from({ length: 9 }),
+  inputMoney: function (money) {
+    if (!this.checkMoney(money)) {
+      return false;
+    }
+    this.amount = money;
+    return true;
+  },
+  checkMoney: function (money) {
+    return money >= 1 && money <= 1000000;
+  },
+};
+
 function problem5(money) {
   var answer;
   return answer;
