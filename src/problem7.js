36cdca8391ac44287f674fcfd27ccdf6944e901d
d0422 <rlfehd2013@naver.com>
2022-10-29 19:04:33 +0900

    Feat:사용자와 친구인 사람들의 목록을 통해 함께 아는 친구의 점수 더하기

--- src/problem7.js
+++ src/problem7.js
@@ -10,4 +10,20 @@ function getFriendsList(user, friends) {
   }
   return friendList;
 }
+function getAcquaintanceScore(user, friends, friendList) {
+  const acquaintance = {};
+  for (let x = 0; x < friends.length; x++) {
+    if (!friends[x].includes(user)) {
+      if (friendList.includes(friends[x][0]))
+        applyScore(acquaintance, friends[x][1]);
+      if (friendList.includes(friends[x][1]))
+        applyScore(acquaintance, friends[x][0]);
+    }
+  }
+  return acquaintance;
+}
+function applyScore(acquaintance, target) {
+  if (acquaintance[target]) acquaintance[target] += 10;
+  else acquaintance[target] = 10;
+}
 module.exports = problem7;
