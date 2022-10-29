6c57d7ea5fb1e0b35cc48b5ecd524603b810403a
김희진 <choha941108@gmail.com>
2022-10-29 17:03:55 +0900

    (6번) 폼입력/ 배열쪼개기 완료

--- src/problem6.js
+++ src/problem6.js
@@ -6,13 +6,27 @@ function problem6(forms) {
 
   for ( i = 0 ; i < forms.length ; i++) {
     forms[i][1] = [...forms[i][1]]
-    
-    }console.log("배열 쪼갠것 확인",forms)
-  return answer;
-
-//배열의 두번째 이름 배열로 쪼개기
+    //배열의 두번째 이름 배열로 쪼개기
+  } console.log("배열 쪼갠것 확인",forms)
+  
+  
+  for ( i = 0 ; i < forms.length ; i++) {
+    for ( j = 0 ; j <forms[i][1].length ; j++) {
+      forms[i][1].sort()
+      console.log("정렬확인",forms)
+        if(forms[i][1][j] != forms[i][1][j+1]) {
+          forms.push(forms[i][1][j])
+        }else {
+          forms.pop()
+        }
+      }
+      console.log("단어삭제확인",forms)
+    }
+  }
+  
 
 //스택으로 비교하기
 //비교해서 같은 값이 2개일 경우 삭제
-}
+return answer;
+
 module.exports = problem6;
