1979e58afface4c5ef5a8f7a768c899f1344f704
subin <newdeas100@gmail.com>
2022-10-29 16:57:26 +0900

    feat(problem3): findNumberSet 기능추가 > 배열에서 3,6,9 갯수세기

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,17 @@ function problem3(number) {
   return answer;
 }
 
+function findNumberSet(inputArray){
+  const filterNum = (numArray) =>{
+    const temp=[];
+    numArray.forEach(inputNum){
+      const count = inputArray.filter(cur => cur === inputNum).length;
+      temp.push(count);
+    }
+    return temp;
+  }
+  
+  return filterNum([3,6,9]);
+}
+
 module.exports = problem3;
