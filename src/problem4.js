dc94f26a7495797d746b9c3934f2be1f0ea13aea
subin <newdeas100@gmail.com>
2022-10-29 17:35:28 +0900

    feat(problem4) : makeDictionary 함수 > 소문자변환기능추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,9 @@
 function problem4(word) {
   const dict = new Map();
   makeDictionary(dict);
-
   console.log(dict);
+
+
 }
 
 function makeDictionary(dict){
@@ -11,13 +12,15 @@ function makeDictionary(dict){
   for(let i=0; i<alpha.length;i++){
     const [key,value] = [alpha[i], reverseAlpha[i]];
     dict.set(key, value);
+    dict.set(key.toLowerCase(), value.toLowerCase());
   }
 }
 
+function changeString(word, dict){
+
+}
 
 
-
-problem4("I love you");
 
 
 module.exports = problem4;
