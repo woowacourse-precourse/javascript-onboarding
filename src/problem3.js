cc0a5a41cd7fa1b6fe7610d420a933faed9abbcb
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-10-29 19:51:38 +0900

    problem3 기능 구현 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let str = 0;
+  for (let i = 1; i <= number; i++) {
+    str += String(i);
+  }
+
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if ((str[i] === "3") | (str[i] === "6") | (str[i] === "9")) {
+      count++;
+    }
+  }
+  return count;
 }
 
 module.exports = problem3;
