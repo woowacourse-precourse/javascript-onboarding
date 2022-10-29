3851f0b93e9ede2fed4070677a96ce790f9c5679
loopy-lim <loopy@jnu.ac.kr>
2022-10-29 15:47:58 +0900

    이메일은 이메일 형식에 부합하며, 전체 길이는 11자 이상 20이 미만이다

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,8 @@
+const isEmail = function (text) {
+  let mailformat = /^\w+([\.-]?\w+)*@\w+([\.-]?\w+)*(\.\w{2,3})+$/;
+  return text.match(mailformat);
+};
+
 function problem6(forms) {
   var answer;
   return answer;
