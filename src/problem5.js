57d09c0f1ffec34a82194d27f2f667d80c95a905
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-29 13:29:43 +0900

    refactor: 5번 함수 이름 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,13 @@
 function problem5(money) {
   let restAmount = money;
   let units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  function getUnitNumber(unit) {
+  function getDigit(unit) {
     let N = Math.floor(restAmount / unit);
     restAmount = restAmount - N * unit;
     return N;
   }
-  let getUnitNumberArr = units.map((v) => getUnitNumber(v));
-  return getUnitNumberArr;
+  let getDigitArr = units.map((v) => getDigit(v));
+  return getDigitArr;
 }
 
 module.exports = problem5;
