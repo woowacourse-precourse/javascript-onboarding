d389ebff7cb6dc9ea4116c9dd0c8de17cb223415
jongwonp <jw032876@gmail.com>
2022-10-29 17:36:35 +0900

    solve problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = new Array(unit.length).fill(0);
+
+  for (let i = 0; i < unit.length; i++) {
+    if (money / unit[i] >= 1) {
+      result[i] = Math.floor(money / unit[i]);
+      money -= result[i] * unit[i];
+    }
+  }
+
+  return result;
 }
 
 module.exports = problem5;
