bc17185a97494d4a41630f7b126b22b773d30486
dakyungoh <dakyung.work@gmail.com>
2022-10-29 22:28:08 +0900

    feat: problem5 화폐 세는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,17 @@
 function problem5(money) {
   const banknotes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
-  const counts = countsBanknotes(money, banknotes)
-  return counts
+  return countsBanknotes(money, banknotes)
 }
 
 // TODO: 금액과 화폐의 종류를 입력받아 화폐 종류별로 필요한 갯수를 구한다.
 function countsBanknotes(money, banknotes) {
-  return []
+  const counts = []
+  let remain = money
+  for (let i = 0; i < banknotes.length; i++) {
+    counts[i] = (parseInt(remain / banknotes[i]))
+    remain = remain % banknotes[i]
+  }
+  return counts
 }
 
 module.exports = problem5;
