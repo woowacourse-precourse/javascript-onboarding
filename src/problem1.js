e5ed9537879d2d4291e1a770635cbead33c2a10c
sh981013s <sh981013s@gmail.com>
2022-10-29 16:11:04 +0900

    feat: [1] 배열 원소 범위 확인 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -9,6 +9,25 @@ const isArrayLengthTwo = (arr) => {
   return arr.length === DESIGNATED_ARR_LENGTH;
 };
 
+/**
+ * 배열의 원소들이 range 에서 벗어나지 않는지 체크한다.
+ * @param {number[]} arr - 한명의 페이지 번호가 담긴 배열
+ * @return {boolean}
+ */
+const isValidRange = (arr) => {
+  const MIN_PAGE_NUM = 1;
+  const MAX_PAGE_NUM = 400;
+  const firstPage = arr[0];
+  const secondPage = arr[1];
+
+  const isFirstPageInValidRange =
+    firstPage >= MIN_PAGE_NUM && firstPage <= MAX_PAGE_NUM;
+  const isSecondPageInValidRange =
+    secondPage >= MIN_PAGE_NUM && secondPage <= MAX_PAGE_NUM;
+
+  return isFirstPageInValidRange && isSecondPageInValidRange;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
