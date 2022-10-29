5f7695b42b4112aa5a1fdd9b95ab18096e4dd137
Yesong <to06109@naver.com>
2022-10-29 18:04:47 +0900

    [1주차 3번] FEAT: 3, 6, 9 포함한 숫자 배열 추출 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
+function find369(number) {
+  const regexp = /3|6|9/;
+  let found369 = [];
+
+  for (let n = 1; n < number + 1; n++) {
+    if (regexp.test(n)) {
+      found369.push(n);
+    }
+  }
+
+  return found369;
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let clap = 0;
+  return clap;
 }
 
 module.exports = problem3;
