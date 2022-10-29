7638afb1b426c2f0523982728862bfbfe7964252
Yesong <to06109@naver.com>
2022-10-29 16:57:45 +0900

    [1주차 2번] FEAT: 중복 문자 제거 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram;
+
+  function delDuplication(string) {
+    let result = string;
+    const alphabet = "abcdefghijklmnopqrstuvwxyz";
+
+    for (let letter of alphabet) {
+      const regex = new RegExp(`${letter}{2,}`, "g");
+      if (result.match(regex) !== null) {
+        result = result.replace(regex, "");
+        countDuplication += 1;
+      }
+    }
+    return result;
+  }
+
   return answer;
 }
 
