b6a321c29c492f0bb6dc54389f21a5bdcadb8cc6
chex <chex417@naver.com>
2022-10-29 23:41:49 +0900

    feat: 두 크루의 닉네임이 중복인지 판단하는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,28 @@
 function problem6(forms) {
   let answer = [];
   let crew_num = forms.length;
-
+  
   if (crew_num < 2){ // crew가 1명인 경우 종료
     return answer;
   }
-
+  
   return answer;
+}
+
+function isOverlapNickname(crew1, crew2, forms){
+  let str_to_search = '';
+  nick1 = forms[crew1][1];
+  nick2 = forms[crew2][1];
+
+  for (let i=0; i<nick1.length-1; i++){
+    str_to_search = nick1.substring(i, i+2);
+
+    // nick1의 연속된 두 문자열이 nick2에 존재하는지 확인
+    if(nick2.includes(str_to_search)){ 
+      return true;
+    } 
+  }
+  return false;
 }
 
 module.exports = problem6;
