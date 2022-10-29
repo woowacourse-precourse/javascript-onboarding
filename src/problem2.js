b138584466e159508432af467de39f6913a35825
박승훈 <tmdgns1126@naver.com>
2022-10-29 17:02:10 +0900

    [P2] Feat: 문제 풀이 구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const arr = cryptogram.split("");
+  const ret = [""];
+  let l = 0;
+  arr.forEach((c) => {
+    if (c === ret[l]) {
+      ret.pop();
+      l--;
+    } else {
+      ret.push(c);
+      l++;
+    }
+  });
+  return ret.join("");
 }
 
 module.exports = problem2;
