cc2626e44e38168a9ae3f6094a5d4cf46fdea798
ksg2388 <ksg2388@naver.com>
2022-10-29 23:38:14 +0900

    problem7(fix): fix

--- src/problem7.js
+++ src/problem7.js
@@ -3,15 +3,32 @@ function problem7(user, friends, visitors) {
   let bestFriends = [];
   let recommend = {};
 
+  //user와 현재 친구인 사람 찾기
   for (let i = 0; i < friends.length; i++) {
     if (friends[i][1] === user) {
       bestFriends.push(friends[i][0]);
+    } else if (friends[i][0] === user) {
+      bestFriends.push(friends[i][1]);
     }
   }
 
   //서로 아는 친구의 경우 확인
   for (let i = 0; i < friends.length; i++) {
-    if (bestFriends.indexOf(friends[i][0]) !== -1 && friends[i][1] !== user) {
+    if (
+      bestFriends.indexOf(friends[i][0]) === -1 &&
+      friends[i][0] !== user &&
+      bestFriends.indexOf(friends[i][1]) !== -1
+    ) {
+      if (friends[i][0] in recommend) {
+        recommend[friends[i][0]] += 10;
+      } else {
+        recommend[friends[i][0]] = 10;
+      }
+    } else if (
+      bestFriends.indexOf(friends[i][1]) === -1 &&
+      friends[i][1] !== user &&
+      bestFriends.indexOf(friends[i][0]) !== -1
+    ) {
       if (friends[i][1] in recommend) {
         recommend[friends[i][1]] += 10;
       } else {
@@ -34,11 +51,25 @@ function problem7(user, friends, visitors) {
     answer.push([name, recommend[name]]);
   }
 
-  answer.sort((a, b) => b[1] - a[1]);
-
+  answer.sort(function (a, b) {
+    if (b[1] > a[1]) {
+      return 1;
+    }
+    if (a[1] > b[1]) {
+      return -1;
+    }
+    if (a[0] > b[0]) {
+      return 1;
+    }
+    if (b[0] > a[0]) {
+      return -1;
+    }
+    return 0;
+  });
   answer = answer.map((item) => item[0]);
+
   if (answer.length > 5) {
-    answer = answer.slice(0, 6);
+    answer = answer.slice(0, 5);
   }
 
   return answer;
