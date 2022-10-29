3e33e61fd5ff07c23d402605f0e7dd81e5dcacde
Ryong-E <think8867@gmail.com>
2022-10-30 03:32:47 +0900

    fix: PROBLEM7 sort 조건 수정

--- src/problem7.js
+++ src/problem7.js
@@ -96,7 +96,7 @@ function sorting(recomendFriends) {
       } else if (a < b) {
         return 1;
       } else if (a === b) {
-        return a < b ? 1 : -1;
+        return a < b ? -1 : 1;
       }
     })
   );
