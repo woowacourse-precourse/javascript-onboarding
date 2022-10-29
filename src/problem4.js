e9af406573a3147eb35bb85f04aa3e93a2f20b35
박승훈 <tmdgns1126@naver.com>
2022-10-29 17:59:03 +0900

    [P4] Feat: 문자-아스키 변환 및 대문자 여부 확인 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  // 1. 기본 함수 정의
+  // 1.1. 문자와 아스키코드의 양방 변형
+  const cToAsc = (c) => c.charCodeAt(0);
+  const ascToC = (asc) => String.fromCharCode(asc);
+
+  // 1.2. 대문자 여부 확인 함수
+  const checkIsUpper = (c) => {
+    return cToAsc(c) <= cToAsc("Z");
+  };
 }
 
 module.exports = problem4;
