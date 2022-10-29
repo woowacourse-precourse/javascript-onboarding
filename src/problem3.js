04879c9fb1ff2b79e7b5dd7f81160dd5a0967339
sulmeee <poorooria@naver.com>
2022-10-29 18:01:05 +0900

    [problem3] feat: 반복문 순회 및 예외사항

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
   // 결과를 담을 배열 선언
   const result = [];
+
+  // 반복문을 통해 숫자를 순회
+  for (let i = 1; i <= number; i++) {
+    // 예외사항 - 두자리수 이상일 때
+    if (String(i).length >= 2) {
+      String(i).split("").map(e => {
+        if (e.match(/[3|6|9]/g)) {
+          result.push(i);
+        }
+      })
+    }
+  }
 }
 
 module.exports = problem3;
