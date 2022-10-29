18223d4580dafbb2c73d7d652958a7123e86bcb7
Monte <monte6198@gmail.com>
2022-10-30 02:59:34 +0900

    feat(problem4.js): chgLetters() 및 문자 배열 청개구리식 변환 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -24,6 +24,23 @@ function chgLowerCase(ascii) {
   return String.fromCharCode(109.5 + (109.5 - ascii));
 }
 
+function chgLetters(letters) {
+  //참개구리 사전식 변환 실행
+  for (let i = 0; i < letters.length; i++) {
+    //각 문자마다
+    ascii = letters[i].charCodeAt(0); //ascii 코드를 받아서
+    if (isUpperCase(ascii)) {
+      //알파벳 대문자라면
+      letters[i] = chgUpperCase(ascii); //청개구리 사전식 대문자로 변환한다.
+    } else if (isLowerCase(ascii)) {
+      //알파벳 소문자라면
+      letters[i] = chgLowerCase(ascii); //청개구리 사전식 소문자로 변환한다.
+    } else {
+      continue; //그 외에는 다음 반복문을 수행한다.
+    }
+  }
+}
+
 function problem4(word) {
   var answer;
   let letters = word.split("");
