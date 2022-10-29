7ba9121a03702cebe0254b5997250ca472f4f883
yesolyo <ahnyelim77@gmail.com>
2022-10-29 20:44:07 +0900

    배열 내 앞, 뒤 문자가 중복되었다면 앞, 뒤 문자를 삭제한다

--- src/problem2.js
+++ src/problem2.js
@@ -7,6 +7,17 @@ function problem2(cryptogram) {
   for (let i = 0; i < cryptogram.length; i++) {
     answer_array[i] = cryptogram[i];
   }
+  //배열 내 앞, 뒤 문자가 중복되었다면 앞, 뒤 문자를 삭제한다
+  for (let i = 0; i < answer_array.length; i++) {
+    if (answer_array[i] == answer_array[i + 1]) {
+      answer_array.splice(i, 2);
+      i = 0;
+    }
+  }
+
+  if (answer_array[0] == answer_array[1]) {
+    answer_array.splice(0, 2);
+  }
 
   return answer;
 }
