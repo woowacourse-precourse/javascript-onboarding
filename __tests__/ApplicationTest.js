9bef729f15559af4761cc1fc5394865ae289df7e
River <swimmingr@gmail.com>
2022-10-30 00:05:19 +0900

    problem2 암호해독 완성

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -6,7 +6,7 @@ const problem5 = require("../src/problem5");
 const problem6 = require("../src/problem6");
 const problem7 = require("../src/problem7");
 
-describe.only("problem1", () => {
+describe("problem1", () => {
   test("case1", () => {
     expect(problem1([97, 98], [197, 198])).toEqual(0);
   });
@@ -20,7 +20,7 @@ describe.only("problem1", () => {
   });
 });
 
-describe("problem2", () => {
+describe.only("problem2", () => {
   test("case1", () => {
     expect(problem2("browoanoommnaon")).toEqual("brown");
   });
