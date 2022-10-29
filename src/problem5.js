9eab4e0b5688e242b492fcf459614c27224ea973
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 16:01:58 +0900

    problem5. 화폐 금액 배열 및 각 화폐의 수 배열 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let moneyArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  return;
 }
 
 module.exports = problem5;
