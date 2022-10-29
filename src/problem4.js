3bad3149149d3ee444a9d30204d186f8b49aa8b7
TaeeunKim <xodms0309@naver.com>
2022-10-29 17:21:44 +0900

    [4번] refactor: 아스키 코드 변환 로직 함수화

--- src/problem4.js
+++ src/problem4.js
@@ -1,23 +1,25 @@
 function problem4(word) {
   var answer = '';
+
+  const getOppositeCode = (alphabet) => {
+    let oppositeCode = 219 - alphabet.charCodeAt();
+    if (alphabet === alphabet.toUpperCase()) {
+      oppositeCode -= 64;
+    }
+    return oppositeCode;
+  };
+
   for (alphabet of word) {
     //1. 알파벳 외의 문자가 들어오는 경우
     if (!/[a-zA-Z]/g.test(alphabet)) {
       answer += alphabet;
       continue;
     }
-    //2. 소문자가 들어오는 경우
-    if (alphabet === alphabet.toLowerCase()) {
-      const oppositeCode = 219 - alphabet.charCodeAt();
-      answer += String.fromCharCode(oppositeCode);
-    } else {
-      const oppositeCode = 155 - alphabet.charCodeAt();
-      answer += String.fromCharCode(oppositeCode);
-    }
+    const oppositeCode = getOppositeCode(alphabet);
+    answer += String.fromCharCode(oppositeCode);
   }
+
   return answer;
 }
-
-problem4('I love you');
 
 module.exports = problem4;
