b89a137f4b340d9eb3a29ca6cad825201602e433
heesungjang <heesungj7@gmail.com>
2022-10-29 16:27:02 +1000

    우테코 1주차 과제 6번: refactor

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,13 @@
 function problem6(forms) {
   const result = new Set();
 
-  for (let i = 0; i < forms.length - 1; i++) {
-    const [currEmail, currNickname] = forms[i];
-    for (let j = i + 1; j < forms.length; j++) {
-      const [nextEmail, nextNickname] = forms[j];
-      const currSubStrings = getAllSubstrings(currNickname);
-      const nextSubStrings = getAllSubstrings(nextNickname);
+  forms.forEach((curr, i) => {
+    const [currEmail, currNickname] = curr;
+    forms.slice(i + 1).forEach((next, j) => {
+      const [nextEmail, nextNickname] = next;
+      const currSubStrings = subStringsLongerThan(2)(currNickname);
+      const nextSubStrings = subStringsLongerThan(2)(nextNickname);
+
       const originalSubStringLen =
         currSubStrings.length + nextSubStrings.length;
 
@@ -16,24 +17,21 @@ function problem6(forms) {
         result.add(currEmail);
         result.add(nextEmail);
       }
-    }
-  }
+    });
+  });
 
   return [...result].sort((a, b) => a.localeCompare(b));
 }
 
-const getAllSubstrings = (str) => {
-  var subStrings = [];
-
-  for (let i = 0; i < str.length; i++) {
-    for (let j = i + 1; j < str.length + 1; j++) {
-      const subString = str.slice(i, j);
-      if (subString.length >= 2) {
-        subStrings.push(str.slice(i, j));
+const subStringsLongerThan = (predicate) => (str) =>
+  [...str].reduce((a, c, i) => {
+    [...str].map((_, j) => {
+      const subString = str.slice(i, j + 1);
+      if (subString.length >= predicate) {
+        a.push(subString);
       }
-    }
-  }
-  return subStrings;
-};
+    });
+    return a;
+  }, []);
 
 module.exports = problem6;
