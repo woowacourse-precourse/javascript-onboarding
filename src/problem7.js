7488fc4a06cd5c1325c1ef95982a18cf0c14bc69
xilucks <siun0331@gmail.com>
2022-10-29 18:46:07 +0900

    feat: problem7 solve

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,77 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = recommendFriends(user, friends, visitors);
   return answer;
+}
+
+function recommendFriends(user, friends, visitors) {
+  const resultPointMap = createResultMap(user, friends, visitors);
+  return Object.keys(resultPointMap).slice(0, 3);
+}
+
+function createResultMap(user, friends, visitors) {
+  let relationPointMap = friendsPointCounter(user, friends);
+  return visitorsPointCounter(relationPointMap, visitors);
+}
+function visitorsPointCounter(pointMap, visitors) {
+  const prevPointMap = pointMap;
+  visitors.forEach((visitor) => {
+    !prevPointMap[visitor]
+      ? (prevPointMap[visitor] = 1)
+      : (prevPointMap[visitor] += 1);
+  });
+
+  return prevPointMap;
+}
+
+function friendsPointCounter(user, friends) {
+  const friendsMap = makeFriendsMap(friends);
+  let pointMap = {};
+
+  const userFriends = friendsMap[user];
+
+  const friendsRelation = userFriends.reduce((acc, cur) => {
+    acc.push(...searchFriends(user, friendsMap[cur]));
+    return acc;
+  }, []);
+
+  friendsRelation.forEach((friend) => {
+    !pointMap[friend] ? (pointMap[friend] = 10) : (pointMap[friend] += 10);
+  });
+
+  return pointMap;
+}
+
+function searchFriends(user, friends) {
+  return friends.reduce((acc, cur) => {
+    if (cur !== user) {
+      acc.push(cur);
+    }
+    return acc;
+  }, []);
+}
+
+function makeFriendsMap(friends) {
+  let relations = {};
+  friends.forEach((relation) => {
+    let [friend1, friend2] = relation;
+    makeRelation(friend1, friend2, relations);
+  });
+  return relations;
+}
+
+function makeRelation(friend1, friend2, relations) {
+  let prevRelations = relations;
+  prevRelations = makeFriends(friend1, friend2, prevRelations);
+  prevRelations = makeFriends(friend2, friend1, prevRelations);
+  return prevRelations;
+}
+
+function makeFriends(friend1, friend2, relations) {
+  let prevRelation = relations;
+  relations.hasOwnProperty(friend1)
+    ? prevRelation[friend1].push(friend2)
+    : (prevRelation[friend1] = [friend2]);
+  return prevRelation;
 }
 
 module.exports = problem7;
