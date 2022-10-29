029044a07d1c143644bfe9fb5578c7cf37b74fb5
Monte <monte6198@gmail.com>
2022-10-29 23:50:37 +0900

    feat(problem4.js): isUpperCase() 및 대문자 판별 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,11 @@
+function isUpperCase(ascii) {
+  //ascii 코드가 대문자인지 판별하는 함수
+  if (ascii >= 65 && ascii <= 90)
+    //ascii 코드가 65 이상 90 이하(대문자 알파벳이라면)라면
+    return true; //true를 반환한다.
+  return false; //대문자 알파벳이 아니라면 false를 반환한다.
+}
+
 function problem4(word) {
   var answer;
   let letters = word.split("");
