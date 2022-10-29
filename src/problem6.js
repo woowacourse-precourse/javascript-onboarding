c70e026d1da03d635c8ca0ce503e07fdcadd8dec
daal2quf <daal2quf@gmail.com>
2022-10-30 07:11:16 +0900

    이메일과 이름 나누기

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,19 @@
 function problem6(forms) {
   var answer;
+  let userEmail = [];
+  let userName = [];
+  let idx = new Set()
+
+  const seperate = (array) => {
+    userEmail.push(array[0])
+    userName.push(array[1])
+  }
+  
+  forms.forEach(element => {
+    seperate(element)
+  });
+  
+
   return answer;
 }
 
