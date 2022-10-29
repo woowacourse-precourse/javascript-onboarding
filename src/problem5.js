266e6764daa9f3e3adc48623ec6a82f582066040
chex <chex417@naver.com>
2022-10-29 20:30:40 +0900

    feat: 돈의 액수와 화폐종류가 주어졌을 때 변환될 수 있는 화폐 최대 개수를 구하는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let money_kinds = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
   return answer;
+}
+
+function calculateMoneyKindCount(money, money_kind){
+  let cnt = parseInt(money/money_kind);
+  return cnt;
 }
 
 module.exports = problem5;
