c46fea0039f07cc81fee32d2b3d28fe1eb21f6d5
confidential-nt <youyoy689@gmail.com>
2022-10-30 05:46:53 +0900

    요구사항 6번 주석달기

--- src/problem6.js
+++ src/problem6.js
@@ -3,6 +3,7 @@ function problem6(forms) {
 
   const result = [];
 
+  // 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어있는지 확인. 맞다면 result 배열에 푸쉬.
   forms.forEach((element, startPoint) => {
     for (let i = startPoint + 1; i < forms.length; i++) {
       for (let j = 0; j < element[1].length - 1; j++) {
@@ -13,11 +14,13 @@ function problem6(forms) {
     }
   });
 
+  // result의 결과로 나온 중복된 것들 제거 위해 오브젝트로 변형
   const reduced = result.reduce((pre, cur) => {
     pre[cur[0]] = null;
     return pre;
   }, {});
 
+  // 정렬해서 배열로 리턴.
   return Object.keys(reduced).sort();
 }
 
