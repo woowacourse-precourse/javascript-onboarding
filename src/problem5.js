3bc741354994935f450199507ef446bb42ff0fee
kyhyun <k29176@gmail.com>
2022-10-30 01:36:00 +0900

    refactor : 문제 5번 - 돈을 큰 금액의 화폐 단위로 나누는 기능과 정렬하는 기능으로 분리

--- src/problem5.js
+++ src/problem5.js
@@ -1,24 +1,22 @@
-/*
-기능 구현 목록
-- [v] 돈을 큰 금액의 화폐 순으로 나누는 기능
-- [v] 화폐 별로 나눈 개수를 세는 기능 
-*/
-
 function problem5(money) {
-  return divideMoney(money);
+  return sortMoney(money);
 }
 
-function divideMoney(money) {
+function sortMoney(money) {
   const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const wallet = [];
   let _money = money;
 
   for (let i = 0; i < MONETARY_UNIT.length; i++) {
     wallet.push(countMoney(_money, MONETARY_UNIT[i]));
-    _money %= MONETARY_UNIT[i];
+    _money = divideMoney(_money, MONETARY_UNIT[i]);
   }
 
   return wallet;
+}
+
+function divideMoney(money, currentMonetaryUnit) {
+  return money % currentMonetaryUnit;
 }
 
 function countMoney(money, currentMonetaryUnit) {
