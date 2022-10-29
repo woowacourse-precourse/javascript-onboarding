63330d17d33dbf8e7ed53aaa5b0afb6197c1dbfd
banma1234 <banma1234@gmail.com>
2022-10-29 20:35:07 +0900

    problem_2 : 암호 쪼개기

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,23 @@
+// main
 function problem2(cryptogram) {
+  // 예외처리
   if (errorHandler(cryptogram)) {
     console.log("Error : 글자수 제한");
     return null;
   }
-  
+
   var answer;
   return answer;
 }
 
 function errorHandler(cryptogram) {
   if (cryptogram.length > 1000) return true;
+}
+
+// 암호해독 함수
+function splitWord(word) {
+  // 암호를 쪼개서 arr에 저장
+  var arr = (word + '').split('');
 }
 
 module.exports = problem2;
