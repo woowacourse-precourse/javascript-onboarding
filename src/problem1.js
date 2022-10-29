968dd5e4cb57126f8b766ecfd4780ef5abf8b386
marugy <gyulife7301@naver.com>
2022-10-29 21:57:35 +0900

    1,2번 clear

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,35 @@
+function calPlus(a) {
+  var sum = 0;
+  while (a > 0) {
+    sum = sum + (a % 10);
+    a = parseInt(a / 10);
+  }
+  return sum;
+}
+function calMultiply(a) {
+  var mul = 1;
+  while (a > 0) {
+    mul = mul * (a % 10);
+    a = parseInt(a / 10);
+  }
+  return mul;
+}
+
+function compareBig(a, b) {
+  var left_big = calPlus(a) > calMultiply(a) ? calPlus(a) : calMultiply(a);
+  var right_big = calPlus(b) > calMultiply(b) ? calPlus(b) : calMultiply(b);
+  return left_big > right_big ? left_big : right_big;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1]) return -1;
+
+  let pobiBig = compareBig(pobi[0], pobi[1]);
+  let crongBig = compareBig(crong[0], crong[1]);
+
+  if (pobiBig > crongBig) return 1;
+  else if (pobiBig == crongBig) return 0;
+  else if (pobiBig < crongBig) return 2;
 }
 
 module.exports = problem1;
--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,31 @@
+function checkOverlap(str) {
+  for (var i = 0; i < str.length - 1; i++) {
+    if (str[i] == str[i + 1]) return 1;
+  }
+}
+
+function deleteOverlap(str) {
+  var test = str;
+  while (checkOverlap(test)) {
+    var test2 = [];
+    if (test[0] != test[1]) test2.push(test[0]);
+    for (var i = 1; i < test.length - 1; i++) {
+      if (test[i - 1] != test[i] && test[i] != test[i + 1]) {
+        test2.push(test[i]);
+      }
+    }
+    if (test[test.length - 1] != test[test.length - 2]) {
+      test2.push(test[test.length - 1]);
+    }
+    test = test2;
+  }
+  return test;
+}
+
 function problem2(cryptogram) {
-  var answer;
+  var answer = deleteOverlap(cryptogram);
+  answer = answer.join("");
+
   return answer;
 }
 
