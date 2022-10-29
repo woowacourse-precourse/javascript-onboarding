d9e3b49dda252ef584ad9aa04deb4007333d0f3a
daal2quf <daal2quf@gmail.com>
2022-10-30 05:18:07 +0900

    예외처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,8 @@
 function problem1(pobi, crong) {
   var answer;
 
-  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0 || pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1 || pobi[0] === 1 || crong[0] === 1 || pobi[1] === 400 || crong[1] === 400) {
+  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0 || pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1 ||
+     pobi[0] === 1 || crong[0] === 1 || pobi[1] === 400 || crong[1] === 400) {
     return -1
   }
   
