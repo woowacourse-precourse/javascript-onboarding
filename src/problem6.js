645aa42eccf83c5b7ef8370fb0fa90e7b669d261
김희진 <choha941108@gmail.com>
2022-10-29 16:01:32 +0900

    (6번)이름 배열로 변환

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
   var answer;
-  return answer;
-}
+  forms_add = []
 
+  console.log("폼 입력 확인",forms)
+
+  for ( i = 0 ; i < forms.length ; i++) {
+    forms[i][1] = [...forms[i][1]]
+    
+    }console.log("배열 쪼갠것 확인",forms)
+  return answer;
+
+//배열의 두번째 이름 배열로 쪼개기
+
+//스택으로 비교하기
+//비교해서 같은 값이 2개일 경우 삭제
+}
 module.exports = problem6;
