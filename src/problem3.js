a2108e602d3f7523a0f46e574f9b01c662959ad4
Choejeonghun <saa00123@naver.com>
2022-10-29 10:02:55 +0900

    feat:문제 3번 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (var i = 1; i < number; i++) {
+    var str = '' + (i + 1);
+    for (var j = 0; j < str.length; j++) {
+      if (str[j] == 3 || str[j] == 6 || str[j] == 9) {
+        answer++;
+      }
+    }
+  }
   return answer;
 }
 
