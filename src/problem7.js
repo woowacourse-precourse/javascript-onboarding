6c76349b4198e90efe1daecbe4c7db5e31cf81b7
yong <tkddbs1644@gmail.com>
2022-10-29 23:54:48 +0900

    문제7: [refactor] 전체 로직에서 친구추천기를 분리하고 추상화한다.

--- src/problem7.js
+++ src/problem7.js
@@ -4,6 +4,56 @@
 [x] 정렬하여 결과를 반환한다.
 */
 
+class FriendRecommender {
+  userRelation = {};
+  constructor(user, friends, visitors) {
+    this._initUserRelation(friends);
+    this.recommendedFriends = this._recommendFriend(user, visitors);
+  }
+
+  _initUserRelation(friends) {
+    for (const [personA, personB] of friends) {
+      if (!this.userRelation[personA]) {
+        this.userRelation[personA] = new Set();
+      }
+      if (!this.userRelation[personB]) {
+        this.userRelation[personB] = new Set();
+      }
+      this.userRelation[personA].add(personB);
+      this.userRelation[personB].add(personA);
+    }
+  }
+
+  _recommendFriend(user, visitors) {
+    const recommendedFriends = {};
+    const myFriendSet = this.userRelation[user];
+    for (const [username, userFriendSet] of Object.entries(this.userRelation)) {
+      if (user === username || myFriendSet.has(username)) {
+        continue;
+      }
+      if (!recommendedFriends[username]) {
+        recommendedFriends[username] = 0;
+      }
+      const friendIntersection = intersect(myFriendSet, userFriendSet);
+      recommendedFriends[username] += 10 * [...friendIntersection].length;
+    }
+
+    for (const username of visitors) {
+      if (myFriendSet.has(username)) {
+        continue;
+      }
+      if (!recommendedFriends[username]) {
+        recommendedFriends[username] = 0;
+      }
+      recommendedFriends[username] += 1;
+    }
+
+    return Object.entries(recommendedFriends).filter(
+      ([_, recommendedNumber]) => recommendedNumber !== 0
+    );
+  }
+}
+
 function intersect(setA, setB) {
   if (!setA instanceof Set || !setB instanceof Set) {
     throw new Error("params is not Set instance.");
@@ -11,62 +61,17 @@ function intersect(setA, setB) {
   return new Set([...setA].filter((x) => setB.has(x)));
 }
 
-function makeUserRelation(friends) {
-  const userRelation = {};
-  for (const [personA, personB] of friends) {
-    if (!userRelation[personA]) {
-      userRelation[personA] = new Set();
-    }
-    if (!userRelation[personB]) {
-      userRelation[personB] = new Set();
-    }
-    userRelation[personA].add(personB);
-    userRelation[personB].add(personA);
+function compare(userA, userB) {
+  const [usernameA, userScoreA] = userA;
+  const [usernameB, userScoreB] = userB;
+  if (userScoreA === userScoreB) {
+    return usernameA < usernameB ? -1 : 1;
   }
-  return userRelation;
-}
-
-function recommendFriend(user, friends, visitors) {
-  const recommendedFriends = {};
-  const userRelation = makeUserRelation(friends);
-  const myFriendSet = userRelation[user];
-  for (const [username, userFriendSet] of Object.entries(userRelation)) {
-    if (user === username || myFriendSet.has(username)) {
-      continue;
-    }
-    if (!recommendedFriends[username]) {
-      recommendedFriends[username] = 0;
-    }
-    const friendIntersection = intersect(myFriendSet, userFriendSet);
-    recommendedFriends[username] += 10 * [...friendIntersection].length;
-  }
-
-  for (const username of visitors) {
-    if (myFriendSet.has(username)) {
-      continue;
-    }
-    if (!recommendedFriends[username]) {
-      recommendedFriends[username] = 0;
-    }
-    recommendedFriends[username] += 1;
-  }
-
-  return Object.entries(recommendedFriends).filter(
-    ([_, recommendedNumber]) => recommendedNumber !== 0
-  );
+  return userScoreB - userScoreA;
 }
 
 function problem7(user, friends, visitors) {
-  function compare(userA, userB) {
-    const [usernameA, userScoreA] = userA;
-    const [usernameB, userScoreB] = userB;
-    if (userScoreA === userScoreB) {
-      return usernameA < usernameB ? -1 : 1;
-    }
-    return userScoreB - userScoreA;
-  }
-
-  const recommendedFriends = recommendFriend(user, friends, visitors);
+  const { recommendedFriends } = new FriendRecommender(user, friends, visitors);
   return recommendedFriends.sort(compare).map(([username]) => username);
 }
 
