d3e4b1a31b6f1167cbd5514391065f6d46f09ba5
haesa <xdhaesa@gmail.com>
2022-10-29 16:31:23 +0900

    problem7 user의 친구, 친구의 친구 구하기 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,23 @@
+function getUserFriends(user, friends) {
+  return friends
+    .filter((friends) => friends.includes(user))
+    .map((friends) => (friends[0] !== user ? friends[0] : friends[1]));
+}
+
+function getFriendsOfFriends(user, userFriends, friends) {
+  const friensdOfUserFriends = friends
+    .filter(
+      (friends) =>
+        !friends.includes(user) && friends.some((x) => userFriends.includes(x))
+    )
+    .map((friends) =>
+      userFriends.includes(friends[0]) ? friends[1] : friends[0]
+    );
+  return friensdOfUserFriends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
-
 module.exports = problem7;
