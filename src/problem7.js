f5f450a50e2b37eacbaec9b2e81e0c5d9d3c3652
BBIYAC <kiy7833@gmail.com>
2022-10-29 16:04:16 +0900

    feat(problem7): add recommend friends feature
    - 추천 친구를 정렬하여 반환한다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,7 @@
 function problem7(user, friends, visitors) {
   let recommend_friends = {},
-      acquaintance = {};
+      acquaintance = {},
+      sorted_friends = [];
 
   friends.forEach(([friend1, friend2]) => {
     if (friend2 !== user) {
@@ -27,7 +28,15 @@ function problem7(user, friends, visitors) {
     }
   })
 
-  return recommend_friends;
+  sorted_friends = Object.keys(recommend_friends)
+    .sort((a, b) => {
+      if (recommend_friends[a] > recommend_friends[b]) return -1;
+      if (recommend_friends[a] < recommend_friends[b]) return 1;
+      if (a > b) return 1;
+      if (a < b) return -1;
+    });
+
+  return sorted_friends.slice(0, 5);
 }
 
 module.exports = problem7;
