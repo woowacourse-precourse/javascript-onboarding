41e9e0cb4626a9a5c35e4e137736a9302c95e14d
marugy <gyulife7301@naver.com>
2022-10-29 23:39:37 +0900

    solve5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
+const arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  for (var i = 0; i < 9; i++) {
+    if (money <= 0) break;
+    var cnt = parseInt(money / arr[i]);
+    answer[i] = cnt;
+    money = parseInt(money % arr[i]);
+  }
   return answer;
 }
 
