0301a38f7cc58f55b366efd396832abab13f09c2
김희진 <choha941108@gmail.com>
2022-10-29 14:46:44 +0900

    (5번)반복되는 코딩으로 인한 정답도출(수정예정)

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,72 @@
 function problem5(money) {
-  var answer;
+  var answer =[];
+
+
+  console.log("머니 유형확인", money)
+  
+  price = 50000
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //50000원 지폐 필요 갯수확인  
+
+  price = 10000
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //50000원 필요갯수 만큼 곱한 수 제외 10000원 필요갯수확인
+
+  price = 5000
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //10000원 필요갯수 만큼 곱한 수 제외 5000원 필요갯수확인
+
+  price = 1000
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //5000원 필요갯수 만큼 곱한 수 제외 1000원 필요갯수확인
+
+  price = 500
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //1000원 필요갯수 만큼 곱한 수 제외 500원 필요갯수확인
+
+  price = 100
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //500원 필요갯수 만큼 곱한 수 제외 100원 필요갯수확인
+
+  price = 50
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //100원 필요갯수 만큼 곱한 수 제외 50원 필요갯수확인
+
+  price = 10
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //10원 필요개수 배열 등록
+
+  price = 1
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+  
+  //1원 필요개수 배열 등록 
+
   return answer;
 }
 
