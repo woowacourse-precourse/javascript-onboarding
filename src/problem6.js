ff03a11b6327f8d344fa4ce65ee30cee0d14e394
Dean <yjj052800@gmail.com>
2022-10-29 22:35:04 +0900

    FEAT: [6번] 닉네임 조합

--- src/problem6.js
+++ src/problem6.js
@@ -1,15 +1,29 @@
 function problem6(forms) {
   var answer;
-  
-  for (let i = 1; i < forms.length -1 ; i++) {
-    let comparisonArr = [...forms[0][1]];
+  let resultArr = [];
+  for (let i = 1; i < forms.length ; i++) {
+    let comparisonArr = [];
+    let comapredArr = [];
+    let firstNick = forms[0][1];
+
+    makePossibleArr(firstNick);
+    console.log(makePossibleArr(firstNick));
+    
+    
+    
     let temp = forms[0];
+
     let length = forms.length - i;
 
-    for (let j = 1; j <= length; j++) {
-      let comparedArr = [...forms[j][1]];
-      let intersection = comparisonArr.filter(x => comparedArr.includes(x));
-      
+
+
+    for (let p = 1; p <= length; p++) {
+
+      // let comparedArr = [...forms[p][1]];
+      // let intersection = afterFilter.filter(x => comparedArr.includes(x));
+      // let overlap = intersection.join('');
+      // console.log(overlap);
+      // console.log('ㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡㅡ')
     }
     
     forms[0] = forms[length];
@@ -18,8 +32,18 @@ function problem6(forms) {
     
     
   }
-  console.log(Boolean(-1));
+  console.log(resultArr);
   return answer;
+}
+
+function makePossibleArr(string) {
+  let possibleArr =[];
+  for (let k = 0; k < string.length; k++) {
+    for (let j = 0; j < string.length + 1; j++) {
+    if (j - k >= 0) possibleArr.push(string.substring(k, j));
+  }
+}
+return possibleArr.filter(x => x.length > 1);
 }
 
 problem6([
