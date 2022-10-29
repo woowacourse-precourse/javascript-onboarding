f02dfbe2baeaddeab82951975088ff19215a2d53
cofla <cofla159@naver.com>
2022-10-30 04:58:46 +0900

    p4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  for (w of word) {
+    const uniCode = w.charCodeAt(0);
+    if (65 <= uniCode && uniCode <= 90) {
+      answer += String.fromCharCode(155 - uniCode);
+    } else if (97 <= uniCode && uniCode <= 122) {
+      answer += String.fromCharCode(219 - uniCode);
+    } else if (w === " ") {
+      answer += " ";
+    }
+  }
   return answer;
 }
-
 module.exports = problem4;
+
+/*
+for 루프 돌면서
+  65<=word[i].charCodeAt(0)<=90 이면
+    변환 = fromCharCode(155-word[i].charCodeAt(0))
+  97<=word[i].charCodeAt(0)<=122이면
+    변환 = fromCharCode(219-word[i].charCodeAt(0))
+*/
