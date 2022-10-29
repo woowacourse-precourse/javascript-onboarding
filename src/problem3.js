39ae4363dfcdccfaaa71279cd8763c8fa383ac1d
yan9vvoojin <yan9vvoojin@naver.com>
2022-10-29 23:55:38 +0900

    refactor: problem3.js에 손뼉을 쳐야하는 횟수를 세는 기능 개선

--- src/problem3.js
+++ src/problem3.js
@@ -2,11 +2,9 @@ function problem3(number) {
   let count = 0;
   for (let i = 1; i <= number; i++) {
     let digits = String(i).split('');
-    for (const digit of digits) {
-      if (digit === '3' || digit === '6' || digit === '9') {
-        count += 1;
-      }
-    }
+    digits.forEach((digit) => {
+      if (digit === '3' || digit === '6' || digit === '9') count += 1;
+    });
   }
 
   return count;
