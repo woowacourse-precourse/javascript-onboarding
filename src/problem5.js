0a7bbd469aa28cdcadc8c27d7b23130e0b74a258
dakyungoh <dakyung.work@gmail.com>
2022-10-29 22:13:50 +0900

    feat: problem5 문제 구조화하기

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const banknotes = getTypesOfBanknotes()
+  const counts = countsBanknotes(money, banknotes)
+  return counts;
+}
+
+// TODO: PROBLEM5.md의 설명에 맞춰 화폐 종류를 반환한다.
+function getTypesOfBanknotes() {
+  return []
+}
+
+// TODO: 금액과 화폐의 종류를 입력받아 화폐 종류별로 필요한 갯수를 구한다.
+function countsBanknotes(money, banknotes) {
+  return []
 }
 
 module.exports = problem5;
