e2dfa3a8ba1f1411b87f6c54dfa04891e8290438
위성노 <92wsn1111@gamilc.com>
2022-10-29 14:59:08 +0900

    feat: Add [문제1] 예외사항 체크 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,32 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+
+  // 예외사항 체크
+  const isValidPobi = isValidInput(pobi);
+  const isValidCrong = isValidInput(crong);
+  
+  if(!(isValidPobi && isValidCrong)) return -1;
+
+  /**
+   * 예외사항 체크 함수
+   * - input 의 길이가 2가 아닌 경우
+   * - 시작 면 또는 마지막 면이 포함된 경우
+   * - [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 [홀수, 짝수]가 아닌 경우
+   * - [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어있지 않은 경우
+   * @param {number[]} input [왼쪽 페이지 번호, 오른쪽 페이지 번호]
+   * @returns {boolean} 예외사항에 해당되면 false 반환
+   */
+  function isValidInput(input){
+    if (
+      input.length !== 2 | 
+      input[1] === 0 | 
+      input[0] === 399 | 
+      input[0]%2 !== 1 | 
+      input[1]%2 !== 0 | 
+      Math.abs(input[1] - input[0]) !== 1
+      ) return false;
+    return true;
+  };
+
+};
 
 module.exports = problem1;
