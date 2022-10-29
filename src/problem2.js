934830eff38d6c0f20ff85fbc375f84906bca974
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-30 07:11:11 +0900

    feat: [problem2]

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,26 @@ function problem2(cryptogram) {
   var stack = [];
   stack.push(cryptogram.charAt(0));
 
+  var cryptogramLastIndex = cryptogram.length - 1;
+
+  for (var i = 1; i < cryptogram.length; i++) {
+    var stackTop = stack.length - 1;
+
+    if (cryptogram.charAt(i) === stack[stackTop]) {
+      while (
+        i <= cryptogramLastIndex &&
+        cryptogram.charAt(i + 1) === stack[stackTop]
+      ) {
+        i++;
+      }
+
+      stack.pop();
+      continue;
+    }
+
+    stack.push(cryptogram.charAt(i));
+  }
+
   return stack.join("");
 }
 
