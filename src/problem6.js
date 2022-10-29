ef2cfc06d224be94b0449c15c350620d583346b6
jeonbyeongmin <qudals7613@gmail.com>
2022-10-29 21:29:05 +0900

    Clear `problem6`

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,50 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const targetEmails = new Set();
+
+  for (const form of forms) {
+    const [email, nickname] = form;
+
+    const filteredForms = forms.filter((f) => email !== f[0]);
+    const emails = findDuplicatedNickname(nickname, filteredForms);
+
+    emails.forEach((e) => targetEmails.add(e));
+  }
+
+  return [...targetEmails].sort();
+}
+
+function findDuplicatedNickname(targetNickname, forms) {
+  const wordSet = getWordSet(targetNickname);
+
+  const emails = [];
+
+  forms.forEach((form) => {
+    const [email, nickname] = form;
+
+    if (isDuplicate(wordSet, nickname)) {
+      emails.push(email);
+    }
+  });
+
+  return emails;
+}
+
+function getWordSet(str) {
+  const wordSet = new Set();
+
+  for (let i = 0; i <= str.length; i++) {
+    for (let j = i + 2; j <= str.length; j++) {
+      wordSet.add(str.substring(i, j));
+    }
+  }
+
+  return [...wordSet];
+}
+
+function isDuplicate(wordSet, nickname) {
+  const index = wordSet.findIndex((word) => nickname.includes(word));
+
+  return index !== -1;
 }
 
 module.exports = problem6;
