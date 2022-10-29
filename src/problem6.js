4dcf16b6f21390887bfbd127ca2da55d36147024
0hyeon <djdjdjk2006@naver.com>
2022-10-29 17:11:00 +0900

    problem 6 -ing

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  for(let i =0;i< forms.length;i++){
+    console.log(forms[i]);
+    
+    for(let j = 0;j< forms[i][1].length;j++){//이름의 갯수만큼 포문
+        console.log(forms[i][1][j]);
+    }
+  }
+
+  // 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함된 단어 찾기
+}
+
+input =[
+  [ 
+    ["jm@email.com", "제이엠"], 
+    ["jason@email.com", "제이슨"], 
+    ["woniee@email.com", "워니"], 
+    ["mj@email.com", "엠제이"], 
+    ["nowm@email.com", "이제엠"]  
+  ] 
+]
+for (let i = 0; i < input.length; i++) {
+  console.log(`#${i + 1} ${problem6(input[i])}`);
 }
 
 module.exports = problem6;
