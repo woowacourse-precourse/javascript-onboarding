3f0be5d331c997adb272d0527dd4f00fb4468b8a
RooT_Kim <bdd3452@gmail.com>
2022-10-29 20:29:28 +0900

    문제6번) checkNickname 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -2,10 +2,19 @@ function problem6(forms) {
 	//닉네임 중복 체크를 하는 함수 -> checkDuplicate
 	//중복된 닉네임의 인덱스를 체크하는 함수 -> getIndex
 	//중복 확인된 닉네임의 인덱스 값을 이용해 결과를 리턴하는 함수 -> checkNickname
-	checkNickname(forms);
+	return checkNickname(forms);
 }
 
-function checkNickname(forms) {}
+function checkNickname(forms) {
+	const nicknameOnly = forms.map((el) => el[1]);
+	const duplicateIndex = getIndex(nicknameOnly);
+
+	return forms
+		.map((el, idx) => {
+			if (duplicateIndex.includes(idx)) return el[0];
+		})
+		.sort((a, b) => (a > b ? 1 : -1));
+}
 
 function getIndex() {}
 
