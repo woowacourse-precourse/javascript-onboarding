2b4416f1ac79cfe21659b9ed56b88d3512605cfe
hae-on <solbi2004@naver.com>
2022-10-29 21:40:32 +0900

    [문제5] 제한 사항 검사

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
+const checkValid = (money) => {
+  if (Number.isInteger(money)) return true;
+  if (money >= 1 && money <= 1000000) return true;
+  return false;
+};
+
 function problem5(money) {
   const kindOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const countMoney = [];
+
+  if (checkValid(money)) return false;
 
   for (let i of kindOfMoney) {
     countMoney.push(Math.floor(money / i));
