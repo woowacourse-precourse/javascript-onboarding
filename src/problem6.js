e078d95dff599880b9487280a46221ec64e1dc35
jsh3418 <albert5428@gmail.com>
2022-10-29 14:52:26 +0900

    feat: problem6 두 글자씩 딴 배열에서 중복된 값을 배열로 만드는 로직 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,14 +1,22 @@
 function problem6(forms) {
-  // 모든 크루들의 닉네임의 두글자씩 딴 배열을 만든다.
-  const splittedCrewsNickName = forms.map((form) => {
-    const crewNickName = form[1].split("");
-    const arr = [];
-    for (let i = 0; i < crewNickName.length - 1; i++) {
-      arr.push(crewNickName[i] + crewNickName[i + 1]);
+  // 모든 크루들의 닉네임의 두글자씩 딴 배열을 만들고 두 글자씩 딴 배열에서 중복된 값을 추려낸다.
+  const splittedNickNames = [];
+  const duplicateNickNames = [];
+
+  forms.forEach((form) => {
+    const nickName = form[1].split("");
+
+    for (let i = 0; i < nickName.length - 1; i++) {
+      const twoLetterNickName = nickName[i] + nickName[i + 1];
+
+      if (splittedNickNames.includes(twoLetterNickName) && !duplicateNickNames.includes(twoLetterNickName)) {
+        duplicateNickNames.push(twoLetterNickName);
+      } else {
+        splittedNickNames.push(twoLetterNickName);
+      }
     }
-    return arr;
   });
-  // 두 글자씩 딴 배열에서 중복된 값을 추려낸다.
+
   // 중복된 값을 닉네임으로 포함하고 있는 크루들을 찾아 이메일을 한 배열에 넣는다.
   // 배열을 리턴한다.
 }
