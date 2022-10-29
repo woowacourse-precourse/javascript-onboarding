4830139b9db01b89336f826db8813f38ceec3328
kyhyun <k29176@gmail.com>
2022-10-30 01:23:06 +0900

    feat : 문제 5번 - 화폐 별로 나눈 개수를 세는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,26 +1,28 @@
 /*
 기능 구현 목록
 - [v] 돈을 큰 금액의 화폐 순으로 나누는 기능
-- [ ] 화폐 별로 나눈 개수의 결과를 담는 기능
+- [v] 화폐 별로 나눈 개수를 세는 기능 
 */
 
 function problem5(money) {
-  let answer = 0;
-  return answer;
+  return divideMoney(money);
 }
 
 function divideMoney(money) {
   const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const _money = money;
+  const wallet = [];
+  let _money = money;
 
   for (let i = 0; i < MONETARY_UNIT.length; i++) {
+    wallet.push(countMoney(_money, MONETARY_UNIT[i]));
     _money %= MONETARY_UNIT[i];
   }
 
-  return;
+  return wallet;
 }
 
-problem5(50237); // [1, 0, 0, 0, 0, 2, 0, 3, 7]
-problem5(15000); // [0, 1, 1, 0, 0, 0, 0, 0, 0]
+function countMoney(money, currentMonetaryUnit) {
+  return Math.floor(money / currentMonetaryUnit);
+}
 
 module.exports = problem5;
