61dac2ca6c33a8bd742f5f5fba6def2d2a72b14f
sery <yunsel2@gmail.com>
2022-10-29 20:39:48 +0900

    feat(Problem6): Get divided nickname array

--- src/problem6.js
+++ src/problem6.js
@@ -4,7 +4,20 @@ function problem6(forms) {
   const emailList = forms.map((form) => form[0]);
   const nicknameList = forms.map((form) => form[1]);
 
+  const dividedNicknameList = [].concat(...getBrokenNicknameList(nicknameList));
+
   return answer;
+}
+
+function getBrokenNicknameList(arr) {
+  return arr.map((nickname) => {
+    let brokenNickname = [];
+    for (let i = 1; i < nickname.length; i++) {
+      const substring = nickname[i - 1] + nickname[i];
+      brokenNickname.push(substring);
+    }
+    return brokenNickname;
+  });
 }
 
 module.exports = problem6;
