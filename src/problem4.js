6515fa12c4caec2ba11fcd97f89d457502513185
mauserne <mauzerne@gmail.com>
2022-10-30 00:42:08 +0900

    [문제4] 통과

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  for (var i in word) {
+    if (word[i] == " ") {
+      answer += " ";
+    } else if ("A" <= word[i] && word[i] <= "Z") {
+      answer += check_upper(word[i]);
+    } else {
+      answer += check_lower(word[i]);
+    }
+  }
   return answer;
 }
 
+function check_upper(cha) {
+  distance = cha.charCodeAt() - 65;
+  return String.fromCharCode(90 - distance);
+}
+function check_lower(cha) {
+  distance = cha.charCodeAt() - 97;
+  return String.fromCharCode(122 - distance);
+}
 module.exports = problem4;
