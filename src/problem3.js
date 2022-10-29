7f3278464a5640bf7a796c3c233aff5f9f4e4ca5
2yunseong <dbsdltjd3701@naver.com>
2022-10-29 12:50:55 +0900

    feat(3번 문제): 이번 턴의 숫자가 박수를 몇 번 치는지 확인하는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,20 @@
+function makeArrayBySeparateOrder(n) {
+  return (n + "").split("");
+}
+
+function calcClapCount(n) {
+  let count = 0;
+  const digits = makeArrayBySeparateOrder(n);
+  digits.forEach((digit) => {
+    if (digit % 3 === 0) count++;
+  });
+
+  return count;
+}
+
 function problem3(number) {
   var answer;
+
   return answer;
 }
 
