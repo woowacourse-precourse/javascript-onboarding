5c249faf9eca69875b26a9ce647e31046dbc0cb3
Minhye Kang <alsgp9898@naver.com>
2022-10-29 22:37:32 +0900

    feat(PROBLEM1): 입력된 배열이 연속하는 페이지인지 판별하는 함수 구현
    - 오른쪽 페이지와 왼쪽 페이지의 차이가 1이 아닐 경우 true 반환(isValid)
    - pobi와 crong의 배열 중 하나만 참이여도 -1을 리턴(problem1)

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,9 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(isValid(pobi) || isValid(crong)) return -1;
 }
+
+const isValid = (pageArr) => {
+  return (pageArr[1] - pageArr[0] !== 1);
+};
 
 module.exports = problem1;
