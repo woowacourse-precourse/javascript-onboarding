5f09fd88efbbf7b1d0a81ac9201a5c52fc71fbc5
Steadystudy <sgmin206@naver.com>
2022-10-30 00:13:22 +0900

    [problem5] Feat: 재귀를 통해 큰 금액의 화폐 위주로 받기

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,47 @@
 function problem5(money) {
-  var answer;
+  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  function recursion(change) {
+    if(change >= 50000) {
+      answer[0] += 1;
+      return recursion(change-50000)
+    }
+    if(change >= 10000) {
+      answer[1] += 1;
+      return recursion(change-10000)
+    }
+    if(change >= 5000) {
+      answer[2] += 1;
+      return recursion(change-5000)
+    }
+    if(change >= 1000) {
+      answer[3] += 1;
+      return recursion(change-1000)
+    }
+    if(change >= 500) {
+      answer[4] = answer[4] + 1;
+      return recursion(change-500)
+    }
+    if(change >= 100) {
+      answer[5] = answer[5] + 1;
+      return recursion(change-100)
+    }
+    if(change >= 50) {
+      answer[6] = answer[6] + 1;
+      return recursion(change-50)
+    }
+    if(change >= 10) {
+      answer[7] = answer[7] + 1;
+      return recursion(change-10)
+    }
+    if(change >= 1) {
+      answer[8] = answer[8] + 1;
+      return recursion(change-1)
+    }
+  }
+
+  recursion(money);
+  
   return answer;
 }
 
