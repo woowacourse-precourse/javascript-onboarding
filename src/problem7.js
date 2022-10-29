a23c373205f36d40b20818260ef8841585e79564
js43o <js43o@naver.com>
2022-10-29 20:56:27 +0900

    Feat: Add problem7 code.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,51 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let answer = [];
+  let friendMap = {};
+  let friendScore = {};
+  let userFriend = [];
+
+  for (let [a, b] of friends) {
+    if (a === user || b === user) {
+      userFriend.push(a === user ? b : a);
+      continue;
+    }
+
+    if (!friendMap[a]) friendMap[a] = [b];
+    else friendMap[a].push(b);
+
+    if (!friendMap[b]) friendMap[b] = [a];
+    else friendMap[b].push(b);
+
+    friendScore[a] = 0;
+    friendScore[b] = 0;
+  }
+
+  for (let u in friendMap) {
+    for (let f of friendMap[u]) {
+      if (!userFriend.includes(f) && f !== user) {
+        friendScore[f] += 10;
+      }
+    }
+  }
+
+  visitors.forEach(
+    (v) => (friendScore[v] = friendScore[v] ? friendScore[v] + 1 : 1)
+  );
+
+  for (let name in friendScore) {
+    if (!userFriend.includes(name)) {
+      answer.push([name, friendScore[name]]);
+    }
+  }
+  answer.sort((a, b) => {
+    if (a[1] < b[1]) return 1;
+    if (a[1] > b[1]) return -1;
+    if (a[0] > b[0]) return 1;
+    if (a[0] < b[0]) return -1;
+    return 0;
+  });
+
+  return answer.slice(0, 5).map((a) => a[0]);
 }
 
 module.exports = problem7;
