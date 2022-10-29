46312c7d0703811fde2d75113317a7a2e720c7bf
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-10-30 02:46:55 +0900

    미션 5 성공

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+
+  // 금액을 담은 배열
+  const payMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let answer = [];
+
+  for (let i = 0; i < payMoney.length; i++){
+    answer.push(parseInt(money / payMoney[i]));
+    money %= payMoney[i];
+  };
+  
   return answer;
 }
 
