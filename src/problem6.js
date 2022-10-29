de30a42a6174296fcd96011b15592d7a84c7ac72
HBSPS <wiyeong79@naver.com>
2022-10-29 19:42:08 +0900

    :sparkles: [문제6] 기능2. 1의 집합에 해당하는 글자가 있는지 확인하는 메소드

--- src/problem6.js
+++ src/problem6.js
@@ -12,4 +12,12 @@ function sliceNickname(nickname) {
   return nicknameSet;
 }
 
+function checkOverlap(nickname, set) {
+  for (let i = 2; i <= nickname.length; i++) {
+    if (set.has(nickname.slice(i - 2, i))) return true; // 중복되는 문자열을 갖는다면 true 반환
+  }
+
+  return false;
+}
+
 module.exports = problem6;
