8c6f5c016dc9931b95fd8264828b5ab67e15667e
d0422 <rlfehd2013@naver.com>
2022-10-29 16:51:04 +0900

    Feat:해당 인덱스 다음 인덱스부터 끝까지 포함하는지 검사하는 기능추가

--- src/problem6.js
+++ src/problem6.js
@@ -4,11 +4,27 @@ function problem6(forms) {
 }
 function makePatterns(nickname) {
   const resultArray = [];
-  let x = 0;
-  while (x < nickname.length - 1) {
+  for (let x = 0; x < nickname.length - 1; x++)
     resultArray.push(nickname.substr(x, 2));
-    x++;
-  }
   return resultArray;
+}
+function process(forms) {
+  let x = 0;
+  const result = [];
+  for (x = 0; x < forms.length; x++) {
+    const patterns = makePatterns(forms[x][1]);
+    duplicateCheck(forms, result, x, patterns);
+  }
+  return result;
+}
+function duplicateCheck(forms, result, start, patterns) {
+  let index = start + 1;
+  while (index < forms.length) {
+    patterns.forEach((pattern) => {
+      if (forms[index][1].includes(pattern)) {
+      }
+    });
+    index++;
+  }
 }
 module.exports = problem6;
