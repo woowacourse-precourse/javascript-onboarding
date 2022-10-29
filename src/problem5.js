85fdf1dfafd56189a9f9777be64d2cfbd8f10b68
DongWooKim97 <nanaukim@naver.com>
2022-10-29 21:06:35 +0900

    feat(P5): 화폐 단위로 몫을 카운트하고 출력하는 기능 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,23 @@
 function problem5(money) {
-  if(!checkInputValid(money)) {
+  if (!checkInputValid(money)) {
     return 'error';
   }
 
-  // return true;
-  
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = currency.map((value) => {
+    let quotient = Math.floor(money / value)
+    if (quotient) {
+      money -= (quotient * value)
+      return quotient
+    }
+    return 0
+  })
+
+  return answer;
 }
 
 function checkInputValid(money) {
-  if(money < 1 || money > 1000000) return false;
+  if (money < 1 || money > 1000000) return false;
   return true;
 }
 
