937a1d39e1bf344e69a7b7db974c981d03b98148
jsh3418 <albert5428@gmail.com>
2022-10-29 09:15:49 +0900

    feat: problem2 연속하는 중복 문자들을 삭제한 결과를 반환하는 로직 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,21 @@
 function problem2(cryptogram) {
   // 연속하는 중복 문자들을 삭제한 결과를 반환하는 로직을 만든다.
+  let cryptogramArray = cryptogram.split("");
+
+  let prevChr = cryptogramArray[0];
+
+  for (let i = 1; i < cryptogramArray.length; i++) {
+    if (prevChr === cryptogramArray[i]) {
+      delete cryptogramArray[i - 1];
+      delete cryptogramArray[i];
+    } else {
+      prevChr = cryptogramArray[i];
+    }
+  }
+
+  cryptogramArray = cryptogramArray.filter((chr) => chr !== undefined);
+
+  return cryptogramArray.join("");
   // 반환한 결과의 문자가 연속하는 중복 문자가 없도록 반복한다.
   // 더 이상 중복 문자가 없다면 결과를 반환한다.
 }
