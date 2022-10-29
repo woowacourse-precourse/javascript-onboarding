3581434291eaf325501f88d203dfa903404cab64
Inae Choi <inaechoi89@gmail.com>
2022-10-29 10:03:57 +0900

    feat(p4): convert word to dictionary value

--- src/problem4.js
+++ src/problem4.js
@@ -3,8 +3,13 @@ function problem4(word) {
   for (let i = 0; i < word.length; i++) {
     let point = word.charCodeAt(i);
     if (point >= 65 && point <= 90) {
+      point = 155 - point;
+      answer += String.fromCharCode(point);
     } else if (point >= 97 && point <= 122) {
+      point = 219 - point;
+      answer += String.fromCharCode(point);
     } else {
+      answer += word[i];
     }
   }
   return answer;
