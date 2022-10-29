1d0936c3745aa31e5bf691b82563a2596c6c0822
jsh3418 <albert5428@gmail.com>
2022-10-29 14:11:42 +0900

    chore: problem6 요구사항, 로직 정리

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,8 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  // 모든 크루들의 닉네임의 두글자씩 딴 배열을 만든다.
+  // 두 글자씩 딴 배열에서 중복된 값을 추려낸다.
+  // 중복된 값을 닉네임으로 포함하고 있는 크루들을 찾아 이메일을 한 배열에 넣는다.
+  // 배열을 리턴한다.
 }
 
 module.exports = problem6;
