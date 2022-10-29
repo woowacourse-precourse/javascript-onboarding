449d99995be1bd31f4fbf0610a17ffb44e583581
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-10-29 23:33:11 +0900

    fix: 문제6 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 목록 return 기능 수정
    - 배열에 새로운 값을 추가하기 위해 기존에 사용한 문법이 잘못되어 수정함 : += => push()함수
    - sequenceChar(name, nameSet) 함수 오류 수정:  name 매개변수와 동일한 연속된 문자를  nameSet 매개변수에서 찾는 기능인데, for 문 범위 오류로 name이 자기 자신과도 비교하여 잘못된 결과가 나오게 되어있었음. 그래서 let i의 초기값으로 index(nameSet[index] = name) 매개변수를 받아 추가함.
    - sequenceChar(name, nameSet) 함수에서 반환된 배열의 모음이 다중배열이어서 reduce 함수를 이용해 일차배열로 변경하는 과정 추가함

--- src/problem6.js
+++ src/problem6.js
@@ -2,43 +2,49 @@ function problem6(forms) {
   let answer = [];
   let nameSet = [];
   let emailSet = [];
-  let sequenceCharIndex = [];
+  let sCharIndex = [];
 
   for (let nameNum = 0; nameNum < forms.length; nameNum++) {
-    emailSet = forms[nameNum][0];
-    nameSet = forms[nameNum][1];
+    emailSet.push(forms[nameNum][0]);
+    nameSet.push(forms[nameNum][1]);
   }
 
   for (let index = 0; index < nameSet.length; index++) {
-    if (sequenceChar(nameSet[index], nameSet)) {
-      sequenceCharIndex += index;
+    let inst = sequenceChar(nameSet[index], nameSet, index);
+    if (inst != -1) {
+      sCharIndex.push(inst);
     }
   }
 
-  sequenceCharIndex = [...new Set(sequenceCharIndex)];
-  answer = returnEmail(sequenceCharIndex, emailSet);
+  sCharIndex = sCharIndex.reduce(function (acc, cur) {
+    return acc.concat(cur);
+  })
+  sCharIndex = [...new Set(sCharIndex)];
+  answer = returnEmail(sCharIndex, emailSet);
   return answer;
 }
 
-function sequenceChar(name, nameSet) {
+function sequenceChar(name, nameSet ,index) {
   let indexArr = [];
-  for (let i = 0; i < name.length - 1; i++) {
+  for (let i = index; i < name.length - 1; i++) {
     let chars = name[i] + name[i + 1];
-    for (let loop = 0; loop < set.length; loop++) {
-      if(!(set[loop].indexOf(chars))){
-        indexArr += loop;
+    for (let loop = 0; loop < nameSet.length; loop++) {
+      if (nameSet[loop].indexOf(chars)>-1) {
+        indexArr.push(loop);
       }
     }
   }
-  return indexArr;
+  if (indexArr.length != 0) {
+    return indexArr;
+  }
+  return -1;
 }
 
-function returnEmail(indexSet, emailSet){
+function returnEmail(indexSet, emailSet) {
   let emailArr = [];
-  for(let i =0; i< indexSet.length; i++){
-    emailArr = emailSet[indexSet[i]];
+  for (let i = 0; i < indexSet.length; i++) {
+    emailArr.push(emailSet[indexSet[i]]);
   }
-
   return emailArr.sort();
 }
 module.exports = problem6;
