47466ad46d8b29d2f4ee51358f231b4a5931356a
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-10-29 16:16:50 +0900

    feat: Add Exception handling

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
 function problem1(pobi, crong) {
   var answer;
-  return answer;
+  if (checkValue(pobi)&&checkValue(crong)){
+
+    return answer;
+  }
+  return -1;
 }
+
+function checkValue(arr){
+  let result = arr[1]-arr[0]==1?true:false;
+  return result;
+}
+
+
+console.log(problem1([97, 98], [197, 198]));
 
 module.exports = problem1;
