c863169f185fe564f6643fc42d53537a9106aeb1
Sungwk <73461032+Sungwk@users.noreply.github.com>
2022-10-29 20:29:21 +0900

    문제7. 나와 친구인 유저 배열에 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,25 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let friendWithMe = [];
+  friends.map((u) => {
+    if (u[0] === user) friendWithMe.push(u[1]);
+    else if (u[1] === user) friendWithMe.push(u[0]);
+  });
   return answer;
 }
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 
 module.exports = problem7;
