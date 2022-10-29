7378d81f586ea69f2d978f50e8a6e9afb542988f
kyhyun <k29176@gmail.com>
2022-10-29 19:44:48 +0900

    feat: 문제 4번 - 문자를 유니코드로 변환하는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,6 @@
 /*
 기능 구현 목록
+[v] 문자를 유니코드로 변환하는 기능
 [v] 문자가 소문자인지 판별하는 기능
 [v] 문자가 대문자인지 판별하는 기능
 [ ] 대문자 알파벳 변환 기능
@@ -8,19 +9,30 @@
 
 function problem4(word) {
   let answer = '';
+  for (const char of word) {
+    console.log(convertUnicode(char));
+  }
   return answer;
 }
 
-function isLower(char) {
-  const LOWER_CASE_START_POINT = 97;
-  const LOWER_CASE_END_POINT = 122;
-  return LOWER_CASE_START_POINT <= char && char <= LOWER_CASE_END_POINT ? true : false;
+function convertUnicode(char) {
+  return char.charCodeAt();
 }
 
-function isUpper(char) {
+function isLower(unicodeChar) {
+  const LOWER_CASE_START_POINT = 97;
+  const LOWER_CASE_END_POINT = 122;
+  return LOWER_CASE_START_POINT <= unicodeChar && unicodeChar <= LOWER_CASE_END_POINT
+    ? true
+    : false;
+}
+
+function isUpper(unicodeChar) {
   const UPPER_CASE_START_POINT = 65;
   const UPPER_CASE_END_POINT = 90;
-  return UPPER_CASE_START_POINT <= char && char <= UPPER_CASE_END_POINT ? true : false;
+  return UPPER_CASE_START_POINT <= unicodeChar && unicodeChar <= UPPER_CASE_END_POINT
+    ? true
+    : false;
 }
 
 problem4('I love you'); // 'R olev blf'
