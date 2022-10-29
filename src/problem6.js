c432f03ceefb3dbb0bc80a197639a44475551549
leeuihyun <meta212121@naver.com>
2022-10-29 20:26:55 +0900

    refactor : problem6 for문 안의 범위 오타 수정

--- src/problem6.js
+++ src/problem6.js
@@ -2,7 +2,7 @@ function nicknameWordMap(forms) {
   let map = new Map();
   forms.forEach((item) => {
     let nickname = item[1];
-    for (let i = 0; i < length - 1; i++) {
+    for (let i = 0; i < nickname.length - 1; i++) {
       let subStr = nickname.substring(i, i + 2);
       if (map.has(subStr)) {
         map.set(subStr, "duplication");
