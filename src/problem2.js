b7043bbf06bee3a79d6be2cad59790494e7cce48
Haram Baek <haram@Haramui-MacBookAir.local>
2022-10-29 15:20:15 +0900

    feat: [PROBLEM2] 매개변수 cryptogram에서 중복된 문자가 있는지 확인한다

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
+function checkOverlap(word) {
+  // 문자열(word)내에 중복된 문자가 있으면 true, 없으면 false를 반환한다.
+  let result = false;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === word[i + 1]) {
+      result = true;
+    }
+  }
+  return result
+}
+
 function problem2(cryptogram) {
-  var answer;
+  var answer = cryptogram;
+
+  console.log(checkOverlap(answer));
+  
   return answer;
 }
 
