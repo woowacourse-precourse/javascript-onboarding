42d90e0ce08f8f03bde89a52bda8eae3aac8415d
TaeeunKim <xodms0309@naver.com>
2022-10-29 19:17:11 +0900

    [5번] feat: 각 화폐의 개수 변환

--- src/problem5.js
+++ src/problem5.js
@@ -3,6 +3,12 @@ function problem5(money) {
 
   const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  MONETARY_UNIT.forEach((unit) => {
+    const count = parseInt(money / unit);
+    answer.push(count);
+    money %= unit;
+  });
+
   return answer;
 }
 
