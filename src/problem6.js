7a63ef6a770b6570cbc3f167f873afe212706992
sujungYu <su_moment@naver.com>
2022-10-29 18:37:24 +0900

    feat(PROBLEM6):닉네임별 연속 글자 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,13 +1,25 @@
 function problem6(forms) {
     let checkObj = {};
+    let checkArr = [];
+    let mailArr = [];
     forms.forEach(v=> {
         const [, name] = v;
+        checkArr.push(duplicateTest(name));
         for(let i=0; i<name.length-1; i++) {
             const checkName = name.substr(i, 2);
             if(checkObj[checkName]) checkObj[checkName] +=1;
             else checkObj[checkName] = 1;
         }
     })
+}
+
+function duplicateTest(name) {
+    let testArr = [];
+    for(let i=0; i<name.length-1; i++) {
+        const text = name.substr(i, 2);
+        testArr.push(text)
+    }
+    return testArr;
 }
 
 module.exports = problem6;
