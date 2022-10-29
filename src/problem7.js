fc346d95b670479427dcfbd424ee54d0d469194b
yu-retel <dbdudals100@naver.com>
2022-10-29 12:49:50 +0900

    feat: [P7] 사용자의 친구찾는기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,18 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+function userFriends(user,friends){
+  let userFriends=[];
+  for(let i=0; i<friends.length; i++){
+    if(friends[i].includes(user)){
+      userFriends.push(friends[i].find((el)=>el!==user))
+    }
+  }
+  return userFriends;
+}
+
+
+let user='mrko'
+let friends=[ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ];
+console.log(userFriends(user,friends))
