f34fde20bc04cefbf2a3c1bdb4fc05bf13eed5e8
hae-on <solbi2004@naver.com>
2022-10-29 21:39:01 +0900

    [문제5] 동전 변환 갯수 구한 후 배열에 입력

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,11 @@ function problem5(money) {
   const kindOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const countMoney = [];
 
+  for (let i of kindOfMoney) {
+    countMoney.push(Math.floor(money / i));
+    money %= i;
+  }
+
   return countMoney;
 }
 
