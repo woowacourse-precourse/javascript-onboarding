731552e9e7bd07189e345afd28767803d4a0df31
unsnruu <unsnruu@gmail.com>
2022-10-29 16:45:57 +0900

    ✨ getRepeatedRange 구현 (problem2)
    getRepeatedRange를 생성하여 복잡했던 로직의 가독성을 향상시켰습니다.
    또한 getRepeated 함수의 이름을 getRepeatedRanges로 변경하였습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -12,23 +12,52 @@ module.exports = problem2;
 
 /**
  * @param {array} str
- * @returns
+ * @returns {array}
  */
-function getRepeated(str) {
+function getRepeatedRanges(str) {
   const results = [];
 
   for (let i = 0; i < str.length; i++) {
-    return results;
+    const range = getRepeatedRange(i, str);
+    if (range.length) results.push(range);
   }
+  return results;
+}
+/**
+ * @param {number} start 비교의 기준이 되는 색인 값
+ * @param {string} str 모든 문자를 담고 있는 문자열
+ * @returns {array} 같은 문자열의 시작과 끝의 인덱스가 담긴 배열
+ */
+function getRepeatedRange(start, str) {
+  const result = [];
+  const pivot = str[start];
+  let continued = 0;
+
+  for (let i = start + 1; i < str.length; i++) {
+    const ch = str[i];
+
+    //만약 pivot과 ch의 문자가 같다면
+    if (ch === pivot) {
+      continued++;
+      continue;
+    }
+    //만약 다르다면
+    //연속된 문자열이 존재하지 않는다면 빈배열 반환
+    if (continued === 0) return [];
+    result.push(start, start + continued);
+    return result;
+  }
+  //만약에 문자열의 마지막까지 탐색하고도 함수가 return되지 않았더라면
+  if (continued > 0) result.push(start, start + continued);
+  return result;
 }
 
 /**
- * @param {array} slice 중복된 문자의 시작과 끝 정보를 가진 배열
+ * @param {number} start
+ * @param {number} end
  * @param {array} str 원래 문자열
  */
-function changeCharacter(slice, str) {
-  const [start, end] = slice;
-
+function changeCharacter(start, end, str) {
   for (let i = start; i <= end; i++) {
     str[i] = null;
   }
