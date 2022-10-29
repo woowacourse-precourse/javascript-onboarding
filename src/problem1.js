cacedeae9834bca6d830c199e76fde691e0bf8bf
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-29 19:23:03 +0900

    [문제1] <4단계 기능요구사항 추가> 가장 큰 수를 구하여 본인의 점수를 구함

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,5 @@
 function problem1(pobi, crong) {
+    // 1단계) 입력받은 배열의 요소를 알기 쉬운 변수명으로 저장
     [pobiLeftPage, pobiRightPage, crongLeftPage, crongRightPage] = [undefined, undefined, undefined, undefined];
     if (Array.isArray(pobi)) {
         [pobiLeftPage, pobiRightPage, crongLeftPage, crongRightPage] = [pobi[0], pobi[1], crong[0], crong[1]];
@@ -23,6 +24,15 @@ function problem1(pobi, crong) {
         }
         return multiplyIs;
     };
+
+    /**  4단계) 가장 큰 수를 구하는 함수 */
+    const getBigScore = (nameLeftPage, nameRightPage) => {
+        return Math.max(sum(nameLeftPage), sum(nameRightPage), multiply(nameLelftPage), multiply(nameRightPage));
+    };
+
+    // 4단계) 본인의 점수를 구합니다.
+    const pobiScore = getBigScore(pobiLeftPage, pobiRightPage);
+    const crongScore = getBigScore(crongLeftPage, crongRightPage);
 }
 
 module.exports = problem1;
