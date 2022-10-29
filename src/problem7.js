a29936fe8b363afbcf51a4022c2ead7c9b712e75
jsh3418 <albert5428@gmail.com>
2022-10-29 18:04:38 +0900

    chore: problem7 요구사항, 로직 정리

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,9 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  // friends를 순회하여 user의 친구를 배열에 담는다.
+  // friends를 순회하여 scores 객체에 친구의 이름과 점수를 담는다. 이미 있다면 +10점, 없다면 새로 만들어 10점을 준다.
+  // visitors를 순회하여 scores 객체에 친구의 이름과 점수를 담는다. 이미 있다면 +1점, 없다면 새로 만들어 1점을 준다.
+  // scores를 배열로 만들어 점수 기준 오름차순으로 정렬한다.
+  // 정렬한 scores를 순회하며 반환할 배열에 조건에 맞게 푸쉬한다. (user와 이미 친구이거나, result의 길이가 5에 도달했을 때, user 본인일 때 푸쉬하지 않는다.)
 }
 
 module.exports = problem7;
