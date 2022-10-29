20a7666aaa90078a7a1ba71b2a5eeae49c251a0b
박승훈 <tmdgns1126@naver.com>
2022-10-29 23:48:15 +0900

    [P7] Feat: 관계 설정 함수 로직 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  // 관계 설정 함수
+  const createRelations = (friends) => {
+    const addRelation = (friendA, friendB) => {
+      if (!relations[friendA]) {
+        relations[friendA] = new Set([friendB]);
+      } else {
+        relations[friendA].add(friendB);
+      }
+    };
+
+    const relations = {};
+    friends.forEach(([friendA, friendB]) => {
+      addRelation(friendA, friendB);
+      addRelation(friendB, friendA);
+    });
+    return relations;
+  };
 }
 
 module.exports = problem7;
