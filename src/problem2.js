c6ba0525ea688bb826c94e08103d97f0d5e3aed8
GuHyeonWoo <ghw9174@gmail.com>
2022-10-29 19:19:38 +0900

    P2/중복인 문자가 3개 이상일 경우를 생각하여 로직 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,17 @@
 // 기능 목록
 // 1. 문자열 분해
 // 2. 분해한 문자열 순회
-// 3. 중복 검사
-// + 연속된 문자가 2개 이상일 경우라면? -> 코드 수정 요망
+// 3. 중복 검사 -> 중복된 문자가 3개 이상일 경우도 예외처리
 
 // 중복 검사
-function checkDuplicate(val_1, val_2) {
-  // 두 값을 검사하여 결과 반환
-  if (val_1 === val_2) return true;
-  return false;
+function checkDuplicate(string_list, check_index) {
+  let count = 1;
+  let check_char = string_list[check_index];
+  for(let i = check_index + 1; i < string_list.length; i++){
+    if(string_list[i] !== check_char) break;
+    count++;
+  }
+  return count > 1?count:false;
 }
 
 // 분해한 문자열 순회
@@ -21,13 +24,11 @@ function stringTraversal(string_list) {
   // check_index와 그 다음 값을 확인하므로 리스트 길이의 -1보다 작을때까지 ㅏ
   while (check_index < string_list.length - 1) {
     // 문자열 중복 검사
-    if (checkDuplicate(string_list[check_index], string_list[check_index + 1])) {
-      // 중복된다면 해당 인덱스부터 2개의 값을 삭제
-      string_list.splice(check_index, 2);
-      // 인덱스를 1 뺀다. 단 0 이상임을 검사 -> 예외처리
+    let counted = checkDuplicate(string_list, check_index);
+    if(counted){
+      string_list.splice(check_index, counted);
       check_index = check_index - 1 < 0 ? 0 : check_index - 1;
     }
-    // 중복되지 않는다면 다음 인덱스로 이동
     else check_index++;
   }
   // 남은 리스트 원소들을 합하여 반환
