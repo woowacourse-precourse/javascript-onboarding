31fb7ef33760d38295dbfa750c9bfcb2ad493c68
givensik <kjs4072@icloud.com>
2022-10-30 00:57:38 +0900

    problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,18 @@
 function problem5(money) {
-  var answer;
+  var answer = [0,0,0,0,0,0,0,0,0];
+  var unit =[50000,10000,5000,1000,500,100,50,10,1];
+
+  for(i=0;i<9;i++){
+    if(money == 0){//money가 0이면 종료
+      break;
+    }
+    while(money>=unit[i]){//해당 지폐값보다 크면 빼기
+      money = money - unit[i];
+      answer[i]++;
+    }
+  }
+
+  
   return answer;
 }
 
