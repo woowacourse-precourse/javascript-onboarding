1619068577b307c6d8717d05c647715a600caa8f
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-29 19:10:53 +0900

    [문제1] <2~3단계 기능요구사항> 각 자리의 숫자를 모두 더하는 함수, 곱하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,23 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    /** 2~3단계) 각 자리 숫자를 모두 더하는 함수 */
+    const sum = (n) => {
+        let sumIs = 0;
+        while (n > 0) {
+            sumIs += n % 10;
+            n = parseInt(n / 10);
+        }
+        return sumIs;
+    };
+
+    /** 2~3단계) 각 자리 숫자를 모두 곱하는 함수 */
+    const multiply = (n) => {
+        let multiplyIs = 1;
+        while (n > 0) {
+            multiplyIs *= n % 10;
+            n = parseInt(n / 10);
+        }
+        return multiplyIs;
+    };
 }
 
 module.exports = problem1;
