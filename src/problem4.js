a887a06f4dff142fb346def6669722c794dabbeb
TaeeunKim <xodms0309@naver.com>
2022-10-29 17:14:39 +0900

    [4번] feat: 알파벳 외의 문자가 들어오는 경우 처리

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+  for (alphabet of word) {
+    //1. 알파벳 외의 문자가 들어오는 경우
+    if (!/[a-zA-Z]/g.test(alphabet)) {
+      answer += alphabet;
+      continue;
+    }
+  }
   return answer;
 }
+
+problem4('I love you');
 
 module.exports = problem4;
