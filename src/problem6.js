33340d356e02d2a510ef9938752b520b0394ae0b
minjaeKim <smallkdb@gmail.com>
2022-10-29 22:41:13 +0900

    feat: [Problem6] Add makeWordSet function
    닉네임으로 만들 수 있는 단어 조합으로 집합을 만드는 makeWordSet 함수를 추가하였습니다

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,19 @@
+function makeWordSet(nickName) {
+  const wordSet = new Set();
+  for(let i = 0; i < nickName.length - 1; i++)
+    wordSet.add(nickName.substr(i, 2));
+
+  return wordSet;
+}
+
 function problem6(forms) {
   var answer;
+
+  forms.forEach(([email, nickName]) => {
+    const wordSet = makeWordSet(nickName);
+
+  });
+
   return answer;
 }
 
