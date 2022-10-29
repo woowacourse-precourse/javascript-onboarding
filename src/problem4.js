53b857282bf795d6e93ff326011b99539d870a9a
solo5star <solo_5@naver.com>
2022-10-29 22:36:46 +0900

    feat(문제4): 청개구리 사전 규칙에 따라 문자가 매핑되는 테이블 생성
    A -> Z, B -> Y, C -> X 와 같이 문자가 거꾸로 매핑이 되어야 함

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,23 @@
+const ASCII_UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const ASCII_LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+
+/**
+ * A -> Z, B -> Y 처럼 거꾸로 매핑되는 청개구리 사전
+ * @type {Object.<string, string>}
+ */
+const TRANSLATE_TABLE = Object.freeze(
+  Object.fromEntries(
+    // 각 코드(uppercase, lowercase)에 대해 거꾸로 mapping되는
+    // 코드 테이블 생성
+    [ASCII_UPPERCASE, ASCII_LOWERCASE]
+      .map(code => code
+        .split('')
+        .map((ch, index) => [ch, code[code.length - index - 1]]
+        ))
+      .flat()
+  )
+);
+
 function problem4(word) {
   var answer;
   return answer;
