2c01115db80fffc8f24810b72ec2dfd3f0c317e9
unsnruu <unsnruu@gmail.com>
2022-10-29 18:41:05 +0900

    ✨ 입력된 소문자와 대문자를 반대로 변환하는 함수를 생성 (problem4)
    convertCapital, convertLower 함수를 생성하였습니다.
    입력된 소문자와 대문자를 각각 반대로 변환하는 함수를 생성하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,49 @@ function problem4(word) {
   return answer;
 }
 
+/**
+ * A -Z를 맵핑할 함수를 생성
+ * 해당 함수를 사용해서 문자열을 순회하기
+ */
+
+/**
+ *
+ * @param {string} ch
+ */
+function mapAlphabet(ch) {
+  const chCode = ch.charCodeAt();
+  let pivot = 0;
+  if (chCode >= "A".charCodeAt() && chCode <= "Z".charCodeAt()) {
+    //대문자일떄
+  } else if (chCode >= "a".charCodeAt() && chCode <= "z".charCodeAt()) {
+    //소문자일떄
+  } else {
+    throw new Error("알파벳이 아닙니다");
+  }
+}
+
+function convertCapital(ch) {
+  const mid = ("Z".charCodeAt() + "A".charCodeAt()) / 2;
+
+  if (ch < mid) {
+    const diff = mid - ch.charCodeAt();
+    return String.fromCharCode(mid + diff);
+  } else {
+    const diff = ch.charCodeAt() - mid;
+    return String.fromCharCode(mid - diff);
+  }
+}
+
+function convertLower(ch) {
+  const mid = ("z".charCodeAt() + "a".charCodeAt()) / 2;
+
+  if (ch < mid) {
+    const diff = mid - ch.charCodeAt();
+    return String.fromCharCode(mid + diff);
+  } else {
+    const diff = ch.charCodeAt() - mid;
+    return String.fromCharCode(mid - diff);
+  }
+}
+
 module.exports = problem4;
