a04fcb4344715c24c58f0072e2f87881001992fc
kimyou1102 <yoosk5485@naver.com>
2022-10-29 19:07:10 +0900

    feat: 문제 2번 - 중복되는 문자열을 제거한다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  cryptogram = cryptogram.split('');
+  const stack = [];
+
+  while (cryptogram.length !== 0) {
+    stack.push(cryptogram.shift());
+    if (stack[stack.length - 1] === stack[stack.length - 2]) {
+      stack.pop();
+      if (stack[stack.length - 1] !== cryptogram[0]) stack.pop();
+    }
+
+  }
+
+  return stack.join('');
 }
 
 module.exports = problem2;
