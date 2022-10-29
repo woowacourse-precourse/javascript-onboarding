e11e33c64ad4d4a7a47e23f5d72f099c9cc2f7fc
dakyungoh <dakyung.work@gmail.com>
2022-10-30 05:26:08 +0900

    feat: problem7 추천 리스트 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -4,11 +4,8 @@ function problem7(user, friends, visitors) {
   const scoresForCommonFriends = getScoresForCommonFriends(countsOfCommonFriends)
   const scoresForVisting = getScoresForVisting(visitors)
   const totalScore = sumScores(scoresForCommonFriends, scoresForVisting)
-  // const recommendedList = getRecommendedList(totalScore)
-  // 본인과 이미 친구인 애 빼기
-  // 추천 점수 순으로 정렬해서 최대 5명만 반환하기 
-  var answer;
-  return answer;
+  const recommendedList = getRecommendedList(totalScore, friendsOfUser)
+  return recommendedList
 }
 
 /**
@@ -34,7 +31,7 @@ function getFriendsOfUser(user, friends) {
 /**
  * 함께 아는 친구의 수를 표시하는 객체를 반환한다.
  * @param {*} user 
- * @param {string[]} friendsOfUser 
+ * @param {Set<string>} friendsOfUser 
  * @param {set} friends 
  * @returns {{ [key: string]: number }} { user_id : number_of_common_freinds }
  */
@@ -55,7 +52,7 @@ function getCountsOfCommonFriends(user, friendsOfUser, friends) {
 /**
  * 함께 아는 친구 수에 따른 점수를 구한다.
  * @param {{ [key: string]: number }} common { user_id : number_of_common_freinds }
- * @returns {{ [key: string]: number }} { user_id: score }
+ * @returns {{ [key: string]: number }} { user: score }
  */
 function getScoresForCommonFriends(common) {
   const scores = {}
@@ -68,7 +65,7 @@ function getScoresForCommonFriends(common) {
 /**
  * 방문 횟수에 따른 점수를 구한다.
  * @param {string[]} visitors 방문자 아이디 목록을 담고 있는 배열
- * @returns {{ [key: string]: number }} { user_id: score }
+ * @returns {{ [key: string]: number }} { user: score }
  */
 function getScoresForVisting(visitors) {
   const scores = {}
@@ -81,8 +78,8 @@ function getScoresForVisting(visitors) {
 
 /**
  * 두 scores를 합친 결과를 반환한다.
- * @param {{ [key: string]: number }} scores1 { user_id: score }
- * @param {{ [key: string]: number }} scores2 { user_id: score }
+ * @param {{ [key: string]: number }} scores1 { user: score }
+ * @param {{ [key: string]: number }} scores2 { user: score }
  * @return {{ [key: string]: number }} 
  */
 function sumScores(scores1, scores2) {
@@ -94,12 +91,64 @@ function sumScores(scores1, scores2) {
     total[user] = total[user] ? total[user] + scores2[user] : scores2[user]
   }
   return total
+}
 
+/**
+ * 점수를 바탕으로 친구 추천 목록을 만든다. 
+ *   - 이미 친구인 유저는 추천 목록에 포함하지 않는다.
+ *   - 입력된 추천 목록을 점수가 높은 순으로 정렬하여 반환한다. 점수가 같으면 알파벳 순서로 정렬한다.
+ *   - 최대 5명까지만 추천 목록에 포함한다.
+ * @param {{ [key: string]: number }} scores { user: score }
+ * @param {Set<string>} friendsOfUser 
+ * @returns {string[]}
+ */
+function getRecommendedList(scores, friendsOfUser) {
+  const recommendedList = getTotalRecommendedList(scores, friendsOfUser)
+  const sortedList = sortRecommendedList(recommendedList)
+  const slicedList = sortedList.slice(0, 5)
+  return slicedList.map(item => item.user)
+}
+
+/**
+ * 점수를 바탕으로 친구 추천 목록을 만든다. 이미 친구인 유저는 추천 목록에 포함하지 않는다.
+ * @param {{ [key: string]: number }} scores { user: score }
+ * @param {Set<string>} friendsOfUser 
+ * @returns {Array<{ user: string, score: number }>}
+ */
+function getTotalRecommendedList(scores, friendsOfUser) {
+  const recommendedList = []
+  for (let user in scores) {
+    if (!friendsOfUser.has(user)) {
+      recommendedList.push({ user: user, score: scores[user] })
+    }
+  }
+  return recommendedList
+}
+
+/**
+ * 입력된 추천 목록을 점수가 높은 순으로 정렬하여 반환한다. 점수가 같으면 알파벳 순서로 정렬한다.
+ * @param {Array<{ user: string, score: number }>} recommendedList 
+ * @returns {Array<{ user: string, score: number }>}
+ */
+function sortRecommendedList(recommendedList) {
+  return recommendedList.sort((a, b) => {
+    if (a.score === b.score) {
+      return a.user.localeCompare(b.user)
+    } else {
+      return a.score > b.score
+    }
+  })
 }
 
 console.log(problem7(
   "mrko",
   [["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+))
+
+console.log(problem7(
+  "mrko",
+  [["donut", "jun"], ["shakevan", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["donut", "andole"], ["shakevan", "mrko"]],
   ["bedi", "bedi", "donut", "bedi", "shakevan"]
 ))
 
