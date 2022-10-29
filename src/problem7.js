d18ff876b4af40ca9f3cb01de6d62b2437f30d15
MinGyu-Kang <kagrin97@gmail.com>
2022-10-29 17:51:32 +0900

    problem7/feat : friends배열을 순회하면서 user와 친구인 아이디를 dude배열에 저장하는 코드 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,31 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const dude = [];
+
+  friends.forEach((arr, idx) => {
+    const [A, B] = arr;
+    if (A === user) {
+      dude.push(B);
+    } else if (B === user) {
+      dude.push(A);
+    }
+  });
+
+  return dude;
 }
 
 module.exports = problem7;
+
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
