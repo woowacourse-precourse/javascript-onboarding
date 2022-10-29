4e307d0ff86ccafcc4c99d2c6d55154cf142d8fc
RooT_Kim <bdd3452@gmail.com>
2022-10-29 10:29:32 +0900

    문제5번) 기능목록 정리 및 함수 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,34 @@
 function problem5(money) {
-  var answer;
-  return answer;
+	//결과 값을 구하고 반환할 함수 -> solution
+	//오만 원권 갯수를 구할 함수 -> fiftyThousand
+	//만 원권 갯수를 구할 함수 -> tenThousand
+	//오천 원권 갯수를 구할 함수 -> fiveThousand
+	//천 원권 갯수를 구할 함수 -> thousand
+	//오백원 동전 갯수를 구할 함수 -> fiveHundred
+	//백원 동전 갯수를 구할 함수 -> hundred
+	//오십원 동전 갯수를 구할 함수 -> fifty
+	//십원 동전 갯수를 구할 함수 -> ten
+	//일원 동전 갯수를 구할 함수 -> one
 }
+
+function solution() {}
+
+function fiftyThousand() {}
+
+function tenThousand() {}
+
+function fiveThousand() {}
+
+function thousand() {}
+
+function fiveHundred() {}
+
+function hundred() {}
+
+function fifty() {}
+
+function ten() {}
+
+function one() {}
 
 module.exports = problem5;
