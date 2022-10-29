fd1a50c6e48299d46bd857758ed8d75f54bc0a6d
wonkyung <mwk0725@gmail.com>
2022-10-29 13:05:40 +0900

    feat: [기능 0] 사용자별 추천 점수 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userName = user;
+  const allUserList = friends.flat().filter(arr => arr !== userName);
+  const delDupUserList = [...new Set([...new Set(allUserList), ...new Set(visitors)])].filter(
+    arr => arr !== userName
+  );
+
+  // 점수 리스트
+  const userPointList = Object.assign({}, ...delDupUserList.map((key, i) => ({[key]: 0})));
+  // friends 점수 +10
+  for (let friends of allUserList) {
+    userPointList[friends] += 10;
+  }
+  // visitors 점수 +1
+  for (let visitor of visitors) {
+    userPointList[visitor] += 1;
+  }
 }
 
 module.exports = problem7;
