12bd0e1a2d19afde583421c60a403be80050fc19
2je0 <bulljy@naver.com>
2022-10-29 20:28:46 +0900

    ✨ feat(problem6): 중복된 문자열이 있는지 검사하는 함수 구현
    두 문자열을 hasOverlap 함수에 넣었을때 두글자 이상 문자가 연속적으로 겹치면 1을 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
+function hasOverlap(a, b) {
+  for (let i = 0; i < a.length - 1; i++) {
+    const word = a.slice(i, i + 2);
+    if (b.indexOf(word) !== -1) return 1;
+  }
+  return 0;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
 }
-
 module.exports = problem6;
