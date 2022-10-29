a5e600e1ee3b0c127a95c7a55878aa94470281d3
jsh3418 <albert5428@gmail.com>
2022-10-29 14:49:04 +0900

    feat: problem6 모든 크루 닉네임의 두 글자씩 딴 배열을 만드는 로직 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,13 @@
 function problem6(forms) {
   // 모든 크루들의 닉네임의 두글자씩 딴 배열을 만든다.
+  const splittedCrewsNickName = forms.map((form) => {
+    const crewNickName = form[1].split("");
+    const arr = [];
+    for (let i = 0; i < crewNickName.length - 1; i++) {
+      arr.push(crewNickName[i] + crewNickName[i + 1]);
+    }
+    return arr;
+  });
   // 두 글자씩 딴 배열에서 중복된 값을 추려낸다.
   // 중복된 값을 닉네임으로 포함하고 있는 크루들을 찾아 이메일을 한 배열에 넣는다.
   // 배열을 리턴한다.
