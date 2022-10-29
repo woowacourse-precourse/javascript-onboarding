79e54ab970e6964933778b6e2a37c8406324a471
Azamman327 <wjsdbdud327@naver.com>
2022-10-29 16:04:21 +0900

    Docs: [P2] 구현 기능 목록 정리

--- src/problem2.js
+++ src/problem2.js
@@ -2,5 +2,11 @@ function problem2(cryptogram) {
   var answer;
   return answer;
 }
-
 module.exports = problem2;
+
+/*
+1. 연속하는 문자열 체크하는 함수 생성
+2. 1에서 나온 연속하는 부분을 제거하는 함수 생성
+3. 체크 함수에 문자열이 끝났는지 여부 확인
+4. 문자열 체크 함수를 실행하는 반복문 생성, 문자열이 끝난 경우 반복문 탈출
+*/
