da63d925bf3e6d3f837abd01fea84b34a193be23
yeeun426 <thsudkcla7@naver.com>
2022-10-30 02:48:33 +0900

    feat: 문제 3번 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,23 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  
+  for(var i = 1; i < number+1; i++) {
+    let str_num = String(i);
+
+    // 두자리 수 이상일 때
+    if(str_num.length > 1) {
+      for(var j = 0; j < str_num ; j ++) {
+        if(str_num.charAt(j) === '3' || str_num.charAt(j) === '6' || str_num.charAt(j) === '9') {
+          answer = answer + 1;
+        }
+      }
+    } else{ // 한자리 수 일 때
+      if(str_num.charAt(str_num.length-1) === '3' || str_num.charAt(str_num.length-1) === '6' || str_num.charAt(str_num.length-1) === '9') {
+        answer = answer + 1;  
+      }
+    }
+  }
+  
   return answer;
 }
 
