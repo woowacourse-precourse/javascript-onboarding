9d48d260b65f8ff6f47a3585b68ba80602ed4741
dladncks1217 <dlaxodud1217@gmail.com>
2022-10-29 20:35:02 +0900

    feat: problem3 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,23 @@
+// 특정 숫자에서 박수를 몇번 칠지 세어주는 함수
+function getClapCount(num) {
+  let count = 0;
+  while (num) {
+    if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+      count++;
+    }
+
+    num = parseInt(num / 10);
+  }
+  return count;
+}
+
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  // 0번부터 number까지 getClapCount()함수 결과 계속 호출
+  for (let i = 1; i <= number; i++) {
+    answer += getClapCount(i);
+  }
   return answer;
 }
 
