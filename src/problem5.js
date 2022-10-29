308fa6b8a15ef39cf5666cd682e320d15db34c45
김희진 <choha941108@gmail.com>
2022-10-29 15:14:03 +0900

    (5번) 반복코드 함수로 전환

--- src/problem5.js
+++ src/problem5.js
@@ -1,69 +1,58 @@
+function calc(money,price,answer){
+  curr = parseInt(money / price)
+  money = money % price
+  answer.push(curr)
+
+  console.log ("함수" ,money,answer)
+
+  return money
+} 
+
 function problem5(money) {
   var answer =[];
 
-
-  console.log("머니 유형확인", money)
-  
   price = 50000
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
-  
+  money = calc(money,price,answer)
+
   //50000원 지폐 필요 갯수확인  
 
   price = 10000
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //50000원 필요갯수 만큼 곱한 수 제외 10000원 필요갯수확인
 
   price = 5000
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //10000원 필요갯수 만큼 곱한 수 제외 5000원 필요갯수확인
 
   price = 1000
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //5000원 필요갯수 만큼 곱한 수 제외 1000원 필요갯수확인
 
   price = 500
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //1000원 필요갯수 만큼 곱한 수 제외 500원 필요갯수확인
 
   price = 100
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //500원 필요갯수 만큼 곱한 수 제외 100원 필요갯수확인
 
   price = 50
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //100원 필요갯수 만큼 곱한 수 제외 50원 필요갯수확인
 
   price = 10
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //10원 필요개수 배열 등록
 
   price = 1
-  curr = parseInt(money / price)
-  money = money % price
-  answer.push(curr)
+  money = calc(money,price,answer)
   
   //1원 필요개수 배열 등록 
 
