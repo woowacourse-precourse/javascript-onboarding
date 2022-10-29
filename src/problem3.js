1693953b8fdccfb19eae738e4b58e50f578d25d2
sojinjang <jsojin6119@gmail.com>
2022-10-29 17:27:46 +0900

    :sparkles: feat(prob3): 369 포함 횟수 카운트 기능, 총 박수 횟수 누적 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
+function countClap(strNum) {
+  return (strNum.match(/[369]/g) || []).length;
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let totalClapCount = 0;
+  for (let n = 3; n < number + 1; n++) {
+    totalClapCount += countClap(String(n));
+  }
+  return totalClapCount;
 }
 
 module.exports = problem3;
