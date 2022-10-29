0a89f055c1645dba31230e2037453c3872154ce0
wusi-hub <jws9595@naver.com>
2022-10-30 00:08:23 +0900

    problem6 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+
+  let checkMemo = [];
+  let repeatedName = [forms[0]];
+
+  for(let el of forms) {
+    let nickname = el[1];
+    for(let i = 0; i < nickname.length-1; i++) {
+      let checkName = nickname[i] + nickname[i+1];
+      if(checkMemo.includes(checkName)) repeatedName.push(el);
+      else checkMemo.push(checkName);
+    } 
+  }
+
+  let dedup = [...new Set(repeatedName)];
+  let result = [];
+  
+  for(let el of dedup) {
+    result.push(el[0]);
+    }
+  return result.sort();
 }
 
 module.exports = problem6;
