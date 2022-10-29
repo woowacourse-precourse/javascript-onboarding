6bc7b04e6fcb9cdcd0a110c9d9c661c02f7fdf1f
d0422 <rlfehd2013@naver.com>
2022-10-29 15:18:57 +0900

    Feat:반환된 글자를 붙여서 반환하는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,12 +1,13 @@
 const UPPERCASE_REVERSE_NUMBER = 155;
 const LOWERCASE_REVERSE_NUMBER = 219;
 function problem4(word) {
+  var answer;
   const wordArray = [...word];
   let index = 0;
   for (index = 0; index < wordArray.length; index++) {
-    wordArray[index] = reverseChar(wordArray);
+    wordArray[index] = reverseChar(wordArray[index]);
   }
-  var answer;
+  answer = getString(wordArray);
   return answer;
 }
 function getAsciiFromString(char) {
@@ -23,5 +24,8 @@ function reverseChar(char) {
   if (asciiNum >= 97 && asciiNum <= 122)
     resultAscii = LOWERCASE_REVERSE_NUMBER - asciiNum;
   return getStringFromString(resultAscii);
+}
+function getString(array) {
+  return array.join("");
 }
 module.exports = problem4;
