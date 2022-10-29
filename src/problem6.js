3f868a7ffd5742ec9cc3865b9e61bdd8573501bc
pp449 <mma7710@naver.com>
2022-10-29 23:44:54 +0900

    [문제 6] : 1. 신청받은 닉네임 2단어씩 나눠주는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+const split_words = (word) => {
+  for(let i=0; i<word.length-1; i++) {
+    const splitedWord = word.substr(i, i+2);
+    console.log(splitedWord);
+  }
 }
+
+function problem6(forms) {
+  for(let i=0; i<forms.length; i++) {
+    split_words(forms[i][1]);
+  }
+}
+
+problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]);
 
 module.exports = problem6;
