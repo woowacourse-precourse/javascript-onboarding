31dce729afb7f9103bc7250163d53fc89da3b1bb
Chaeduk <kkj2551@naver.com>
2022-10-30 01:56:08 +0900

    feat(problem7) : 문제 해결

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,39 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userFriends = []
+  const friendOfFriends = []
+  const result = {}
+  friends.forEach((element) => element.forEach((person, index, origin) => {
+    if(person === user){
+      if(index === 0) userFriends.push(origin[1])
+      else if(index === 1) userFriends.push(origin[0])
+    }
+  }))
+  friends.forEach((element) => element.forEach((person, index, origin) => {
+    if(userFriends.includes(person)){
+      if(index === 0 && origin[1] !== user && !friendOfFriends.includes(origin[1])) friendOfFriends.push(origin[1])
+      else if(index === 1 && origin[0] !== user && !friendOfFriends.includes(origin[0])) friendOfFriends.push(origin[0])
+    }
+  }))
+  
+  for(const i of friendOfFriends){
+    result[i] = 10
+  }
+  visitors.forEach((element) => {
+    if(Object.keys(result).includes(element)){
+      result[element] = result[element]+1
+    } else if(!userFriends.includes(element)){
+      result[element] = 1
+    }
+  })
+  const answer = Object.keys(result)
+  answer.sort((a,b)=>{
+    if(result[a] < result[b]) return 1
+    else if(result[a] === result[b]){
+      if(a>b) return 1
+      else if(a===b) return 0
+      else return -1
+    } else return -1
+  })
+  return answer
 }
-
 module.exports = problem7;
