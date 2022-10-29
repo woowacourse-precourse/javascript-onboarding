602b1420a584849241687ec434eb99dacc149f68
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-29 17:54:30 +0900

    feat: find user friends in problem 7

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,16 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  // 친구는 모조건 1이상 있음, 방문자는 없을 수도 있다.
+  //   점수가 0초과 최대 5명까지만 추천
+  //   사용자와 함께 아는 친구 구하기
+
+  //   사용자의 친구 구하기
+  const userFrineds = (user, relation) => {
+    return relation
+      .filter((x) => x[0] === user || x[1] === user)
+      .map((x) => (x[0] === user ? x[1] : x[0]));
+  };
+
+  console.log(userFrineds(user, friends));
 }
 
 module.exports = problem7;
