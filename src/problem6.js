834fa47ab731eb092f15138636311275a23c5929
Inae Choi <inaechoi89@gmail.com>
2022-10-29 20:59:31 +0900

    feat(p6): find duplicate nickname users

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,23 @@
 function problem6(forms) {
   var answer;
+  let sameNickIdx = new Set();
+
+  for (let i = 0; i < forms.length; i++) {
+    let twoChar = new Set();
+    let name = forms[i][1];
+    for (let j = 0; j < name.length - 1; j++) {
+      twoChar.add(name.substr(j, j + 2));
+    }
+    for (let j = i + 1; j < forms.length; j++) {
+      for (let item of twoChar.values()) {
+        if (forms[j][1].includes(item)) {
+          sameNickIdx.add(i);
+          sameNickIdx.add(j);
+        }
+      }
+    }
+  }
+
   return answer;
 }
 
