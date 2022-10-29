a7901ee313f6d4800c5a73b0a3e9ca86fc8a513a
pakxe <pigkill40@naver.com>
2022-10-29 23:53:21 +0900

    refactor: [P7] scoreVisitor의 인덴트를 1 줄임

--- src/problem7.js
+++ src/problem7.js
@@ -4,15 +4,19 @@ function problem7(user, friends, visitors) {
 
   let scoreList = new Map();
   friendOfFriend(user, relationGraph, scoreList);
-  scoreVisitor(user, relationGraph, visitors, scoreList);
+  visitors.forEach((visitor) =>
+    scoreVisitor(user, relationGraph, visitor, scoreList)
+  );
 
-  return sortResult(scoreList);
+  return recommendFriend(scoreList);
 }
 
+// 주어진 친구쌍을 그래프로 만드는 함수
 function makeGraph(graph, pair) {
   [leftName, rightName] = pair;
   let tempSet = new Set();
   let value;
+
   if (graph.has(leftName)) {
     value = graph.get(leftName);
     graph.set(leftName, value.add(rightName));
@@ -34,30 +38,31 @@ function friendOfFriend(user, graph, scoreList) {
   userFriends.forEach((friend) => {
     let fof = graph.get(friend);
     fof.forEach((name) => {
-      // user 자신과 user와 겹친구는 제외
+      // user 자신, user와 겹친구는 제외
       if (name !== user && !graph.get(user).has(name)) scoreList.set(name, 10);
     });
   });
 }
 
-function scoreVisitor(user, graph, visitors, scoreList) {
-  visitors.forEach((visitor) => {
-    if (scoreList.has(visitor)) {
-      let score = scoreList.get(visitor);
-      scoreList.set(visitor, score + 1);
-    } else if (!scoreList.has(visitor) && !graph.get(user).has(visitor)) {
-      scoreList.set(visitor, 1);
-    }
-  });
+// 방문자의 점수를 계산하는 함수
+function scoreVisitor(user, graph, visitor, scoreList) {
+  if (scoreList.has(visitor)) {
+    let score = scoreList.get(visitor);
+    scoreList.set(visitor, score + 1);
+  } else if (!scoreList.has(visitor) && !graph.get(user).has(visitor)) {
+    scoreList.set(visitor, 1);
+  }
 }
 
-// 조건대로 정렬하는 함수
-function sortResult(scoreList) {
+// 추천 친구의 이름만 반환하는 함수
+function recommendFriend(scoreList) {
   let ArrScoreList = [...scoreList]; // 배열로 만들기
   ArrScoreList.sort(ascendingSort);
 
   return ArrScoreList.map((score) => score[0]);
 }
+
+// 주어진 조건대로 정렬하는 함수
 function ascendingSort(a, b) {
   [name1, score1] = a;
   [name2, score2] = b;
