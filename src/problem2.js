020d7507a385f4c72cf2afd8b3d54b3ca9b87842
yesolyo <ahnyelim77@gmail.com>
2022-10-29 20:42:39 +0900

    문자열 cryptogram을 배열로 저장한다

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
 function problem2(cryptogram) {
   var answer;
+  answer = "";
+  var answer_array = [];
+
+  //문자열 cryptogram을 배열로 저장한다
+  for (let i = 0; i < cryptogram.length; i++) {
+    answer_array[i] = cryptogram[i];
+  }
+
   return answer;
 }
 
