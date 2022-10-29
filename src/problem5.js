92f191041cc562e37763695774547b56e3383ea3
kimeuns <dmstn2006@gmail.com>
2022-10-30 03:47:05 +0900

    문제5 풀이

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,20 @@
 function problem5(money) {
-  var answer;
+  var answer = Array.from({length:9},()=>0);
+  let x= 5,
+    digit = 50000,
+    idx = 0;
+
+  while (idx < 8){
+    answer[idx]=parseInt(money/digit)
+    money = money%digit
+    digit = digit/x
+    x==2?x=5:x==5?x=2:null
+    idx++
+  }
+  answer[8]=money
+
   return answer;
+
 }
 
 module.exports = problem5;
