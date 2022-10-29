26fee6e16500dbb19aed1598bd90e59187e26ff0
HBSPS <wiyeong79@naver.com>
2022-10-29 19:35:11 +0900

    :sparkles: [문제6] 기능1. 닉네임을 연속된 두 글자씩 나누어 집합으로 저장하는 메소드

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,13 @@ function problem6(forms) {
   return answer;
 }
 
+function sliceNickname(nickname) {
+  const nicknameSet = new Set();
+  for (let i = 2; i <= nickname.length; i++) {
+    nicknameSet.add(nickname.slice(i-2, i));
+  }
+
+  return nicknameSet;
+}
+
 module.exports = problem6;
