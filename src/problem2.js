7787f675132cbf562056f5b912175e0d22db4727
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-29 12:35:04 +0900

    problem2 - feat: 유틸함수 작성
    1. 배열복사 함수
    2. each 함수
    3. map 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,23 @@
+const copyArray = targetArray => {
+  return [...targetArray];
+};
+
+const each = (iterable, func) => {
+  for (const iterator of iterable) {
+    func(iterator);
+  }
+
+  return;
+};
+
+const map = (array, func) => {
+  const resultArray = [];
+
+  each(array, element => resultArray.push(func(element)));
+
+  return resultArray;
+};
+
 function problem2(cryptogram) {
   var answer;
   return answer;
