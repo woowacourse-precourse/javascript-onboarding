4d48d32ec4a99a36cb51e5c534e4264809f925af
위성노 <92wsn1111@gamilc.com>
2022-10-29 23:13:44 +0900

    feat: Add [문제5] 제한사항 체크 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,6 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  // 제한사항 만족하는지 체크
+  if(/[^0-9]/g.test(money) || money < 1 || money > 1000000 || money % 1 !== 0) return false;
 }
 
 module.exports = problem5;
