8feb1905fa93be640b559463792871b7e20277c2
jinlee <vito9270@naver.com>
2022-10-30 02:35:04 +0900

    problem3 자리수 가중치 부여 클래스 생성

--- docs/week1_회고.md
+++ docs/week1_회고.md
@@ -1,1 +1,6 @@
 # [[우테코] clean code 를 위한 check list](https://velog.io/@jinlee/%EC%9A%B0%ED%85%8C%EC%BD%94-clean-code-%EB%A5%BC-%EC%9C%84%ED%95%9C-check-list)
+
+알고리즘 문제들은 그렇게 어렵진 않았지만
+알고리즘을 해결할 때 class를 생성해서 사용해본적이 없어 초반에는 문법에서 어려움을 겪었고
+
+clean code를 위한 pr_checklist를 유의하면서 코드를 짜려고 하니 생각했던 방법을 사용할 수 없어서 어려움을 겪었음
--- src/problem3.js
+++ src/problem3.js
@@ -8,13 +8,36 @@ function problem3(number) {
       const digits = [];
       while (digit > 0) {
         digits.push(digit % 10);
-        digit = parseInt(temp / 10);
+        digit = parseInt(digit / 10);
       }
       return digits;
     }
   }
 
   const sepatedDigits = new Digits(number).digitMaker();
+
+  class ValueTransfer {
+    constructor(sepatedDigits) {
+      this.sepatedDigits = sepatedDigits;
+    }
+    transfer() {
+      const values = [];
+      for (let i = 0; i < this.sepatedDigits.length; i++) {
+        if (this.sepatedDigits[i] < 3) {
+          values.push(0);
+        } else if (this.sepatedDigits[i] >= 3 && this.sepatedDigits[i] < 6) {
+          values.push(1);
+        } else if (this.sepatedDigits[i] >= 6 && this.sepatedDigits[i] < 9) {
+          values.push(2);
+        } else {
+          values.push(3);
+        }
+      }
+      return values;
+    }
+  }
+
+  const transferedValues = new ValueTransfer(sepatedDigits).transfer();
 
   return answer;
 }
