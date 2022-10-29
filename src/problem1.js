51df74ddeee8fe3d65f8d994b9103e98444bc1c9
NYeonK <skdus728@gmail.com>
2022-10-30 01:46:16 +0900

    [ feat ] 덧셈, 곱셈을 통한 가장 큰 수 찾기 기능 구현 #PROBLEM1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,25 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+  const addSum = ([a, b, c=0]) => Number(a)+Number(b)+Number(c);
+  const multiplySum = ([a, b, c=1]) => Number(a)*Number(b)*Number(c);
+
+
+  const largestNumber = (left, right) => {
+    left = String(left).split('');
+    right = String(right).split('');
+
+    addSum(left) >= multiplySum(left) ? left = addSum(left) : left = multiplySum(left);
+    addSum(right) >= multiplySum(right) ? right = addSum(right) : right = multiplySum(right);
+
+    return left>=right ? left : right;
+  };
+
+  pobi = largestNumber(pobi[0], pobi[1])
+  crong = largestNumber(crong[0], crong[1])
+
+  return pobi > crong ? 1 : pobi = crong ? 0 : 2;
 }
+
+problem1([97, 98], [197, 198]) //0
 
 module.exports = problem1;
