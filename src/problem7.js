76e72d20deddc5369f328d08528117d63b82074a
easyone <thinkanddoit@naver.com>
2022-10-29 17:02:12 +0900

    #7 feat: 문제7 Map에 친구관계 정보 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,16 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  const friendRelation = new Map();
+  const pushToMap = (key, value) => {
+    if (friendRelation.has(key))
+      friendRelation.set(key, [...friendRelation.get(key), value]);
+    else friendRelation.set(key, [value]);
+  };
+  friends.forEach(([one, theOther]) => {
+    pushToMap(one, theOther);
+    pushToMap(theOther, one);
+  });
+
+  const answer = [];
   return answer;
 }
 
