99f1c3a833279e611908385ae1f854090fb602e5
confidential-nt <youyoy689@gmail.com>
2022-10-30 05:19:11 +0900

    요구사항 6번 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  if (forms.length < 1 || forms.length > 10000) return;
+
+  const result = [];
+
+  forms.forEach((element, startPoint) => {
+    for (let i = startPoint + 1; i < forms.length; i++) {
+      for (let j = 0; j < element[1].length - 1; j++) {
+        if (forms[i][1].search(element[1].substr(j, 2)) !== -1) {
+          result.push(element, forms[i]);
+        }
+      }
+    }
+  });
+
+  const reduced = result.reduce((pre, cur) => {
+    pre[cur[0]] = null;
+    return pre;
+  }, {});
+
+  return Object.keys(reduced).sort();
 }
 
 module.exports = problem6;
