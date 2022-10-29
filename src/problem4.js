8992d7926d5bd23aba0f72ee2246fe74662bca7d
wusi-hub <jws9595@naver.com>
2022-10-29 13:53:16 +0900

    problem4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let upperCaseArr = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 65));
+  let lowerCaseArr = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 97));
+  let result = [];
+
+  for(let i = 0; i < word.length; i++) {
+    if(word[i] === ' ') result.push(' ');
+    if(upperCaseArr.includes(word[i])) result.push(upperCaseArr[26-upperCaseArr.indexOf(word[i])-1]);
+    if(lowerCaseArr.includes(word[i])) result.push(lowerCaseArr[26-lowerCaseArr.indexOf(word[i])-1]);
+  }
+
+  return result.join('');
 }
 
 module.exports = problem4;
