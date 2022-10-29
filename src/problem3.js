f5cead62a738cd3d9ff8102cd6362008f708b58a
TaeeunKim <xodms0309@naver.com>
2022-10-29 16:51:12 +0900

    [3번] feat: 3, 6, 9를 찾아주는 정규표현식 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
 function problem3(number) {
   var answer;
+
+  for (let i = 1; i <= number; i++) {
+    const numberToString = i.toString();
+    const matchArr = numberToString.match(/[3|6|9]/g);
+  }
   return answer;
 }
 
