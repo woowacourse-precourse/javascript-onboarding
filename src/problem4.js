77f7a2097719c62695527548caebd0e00ef17c31
unknown <sjjoseph106@gmail.com>
2022-10-29 17:23:03 +0900

    :sparkles: problem4 구현

--- src/problem4.js
+++ src/problem4.js
@@ -5,7 +5,15 @@
  * @returns {string}
  */
 function problem4(word) {
-  return "";
+  let res = "";
+
+  for (let i = 0; i < word.length; i++) {
+    let ascii = word.charCodeAt(i);
+    if (65 <= ascii && ascii <= 90) ascii = 155 - ascii;
+    else if (97 <= ascii && ascii <= 122) ascii = 219 - ascii;
+    res += String.fromCharCode(ascii);
+  }
+  return res;
 }
 
 console.log(problem4("I love you")); // "R olev blf"
