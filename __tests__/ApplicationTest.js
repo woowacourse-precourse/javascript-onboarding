52d8b7028e33b786d61de1374bd068f926a1e89f
sujungYu <su_moment@naver.com>
2022-10-29 18:43:53 +0900

    feat(PROBLEM6):테스트 통과

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -3,7 +3,7 @@ const problem2 = require("../src/problem2");
 const problem3 = require("../src/problem3");
 const problem4 = require("../src/problem4");
 const problem5 = require("../src/problem5");
-// const problem6 = require("../src/problem6");
+const problem6 = require("../src/problem6");
 // const problem7 = require("../src/problem7");
 describe("problem1", () => {
   test("case1", () => {
@@ -53,19 +53,19 @@ describe("problem5", () => {
   });
 });
 
-// describe("problem6", () => {
-//   test("case1", () => {
-//     expect(
-//       problem6([
-//         ["jm@email.com", "제이엠"],
-//         ["jason@email.com", "제이슨"],
-//         ["woniee@email.com", "워니"],
-//         ["mj@email.com", "엠제이"],
-//         ["nowm@email.com", "이제엠"],
-//       ])
-//     ).toEqual(["jason@email.com", "jm@email.com", "mj@email.com"]);
-//   });
-// });
+describe("problem6", () => {
+  test("case1", () => {
+    expect(
+      problem6([
+        ["jm@email.com", "제이엠"],
+        ["jason@email.com", "제이슨"],
+        ["woniee@email.com", "워니"],
+        ["mj@email.com", "엠제이"],
+        ["nowm@email.com", "이제엠"],
+      ])
+    ).toEqual(["jason@email.com", "jm@email.com", "mj@email.com"]);
+  });
+});
 
 // describe("problem7", () => {
 //   test("case1", () => {
