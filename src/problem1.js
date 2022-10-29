9045b01813a917ea231164388ab60b76748ee2af
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-29 16:09:48 +0900

    refactor: 1번 리팩토링
    - 변수 이름명 알아보기 쉽게 변경
    - reduce 메소드 작성 부분 간소화

--- src/problem1.js
+++ src/problem1.js
@@ -5,20 +5,17 @@ function problem1(pobi, crong) {
     return -1;
   }
   if (!pobi[0] || !pobi[1] || !crong[0] || !crong[1]) return -1;
+
   function getMaxNumber(num) {
     let arr = String(num).split("");
-    let plus = arr.reduce((a, c) => {
-      a = a + parseInt(c);
-      return a;
-    }, 0);
-    let multiple = arr.reduce((a, c) => {
-      a = a * +c;
-      return a;
-    }, 1);
-    return Math.max(plus, multiple);
+    let plusDigits = arr.reduce((acc, curr) => acc + +curr, 0);
+    let multipleDigits = arr.reduce((acc, curr) => acc * +curr, 1);
+    return Math.max(plusDigits, multipleDigits);
   }
+
   let pobiMaxNum = Math.max(getMaxNumber(pobi[0], pobi[1]));
   let crongMaxNum = Math.max(getMaxNumber(crong[0], crong[1]));
+
   if (pobiMaxNum > crongMaxNum) return 1;
   if (pobiMaxNum < crongMaxNum) return 2;
   if (pobiMaxNum === crongMaxNum) return 0;
