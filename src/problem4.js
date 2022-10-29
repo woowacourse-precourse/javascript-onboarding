603bed49412d4ff1e015b6229a2b055bffb92817
2je0 <bulljy@naver.com>
2022-10-29 19:40:56 +0900

    ✨ feat(problem4): 문자 반대로 변환하는 함수 구현
    요구사항에 따라 반대로 변환해서 반환, 알파벳이 아닐경우 빈 문자열 반환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
+function transformAlpha(alpha) {
+  let retAlpha = "";
+  const alphaAscii = alpha.charCodeAt(0);
+  const aAscii = "a".charCodeAt(0);
+  const zAscii = "z".charCodeAt(0);
+  const AAscii = "A".charCodeAt(0);
+  const ZAscii = "Z".charCodeAt(0);
+  if (aAscii <= alphaAscii && alphaAscii <= zAscii) {
+    const retAscii = aAscii + zAscii - alphaAscii;
+    retAlpha = String.fromCharCode(retAscii);
+  } else if (AAscii <= alphaAscii && alphaAscii <= ZAscii) {
+    const retAscii = AAscii + ZAscii - alphaAscii;
+    retAlpha = String.fromCharCode(retAscii);
+  }
+  return retAlpha;
+}
 function problem4(word) {
   var answer;
   return answer;
 }
-
 module.exports = problem4;
