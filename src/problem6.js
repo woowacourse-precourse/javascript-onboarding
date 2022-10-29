5b901a864e80f63034753c47ca7c0b3a1f88784a
HJeong1200 <hjeong1200@gmail.com>
2022-10-29 22:17:18 +0900

    06-pseudo-code

--- src/problem6.js
+++ src/problem6.js
@@ -4,3 +4,25 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
+
+/**
+ * 이메일 제한(forms[0])
+ *  1) 이메일 형식에 부합해야 함
+ *  2) email.com 도메인
+ *      => regexr = /^[0-9a-zA-Z]([-_\.]?[0-9a-zA-Z])*@email.com$/
+ *  3) 11 <= 이메일.length < 20
+ *      => forms[0].length >= 11 && forms[0].length < 20
+ *
+ *
+ * 닉네임 제한(forms[1])
+ *  1) 한글만 허용(영문, 숫자, 특수문자 x)
+ *    =>  /^[가-힣]+$/
+ *  2) 1 <= 닉네임.length < 20
+ *    => forms[1].length >= 1 && forms[1].length < 20
+ *  3) 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되면 중복
+ *
+ * result 제한
+ *  1) 이메일만 포함
+ *  2) 문자열을 오름차순으로 정렬
+ *  3) 중복되는 이메일 제거
+ */
