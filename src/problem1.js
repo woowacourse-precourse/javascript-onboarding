c0f7ca55bdc9ec260f7b7b457c074fb7b1d12103
daal2quf <daal2quf@gmail.com>
2022-10-30 05:16:37 +0900

    예외처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,9 @@
 function problem1(pobi, crong) {
   var answer;
 
-  let test = 'abc'
+  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0 || pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1 || pobi[0] === 1 || crong[0] === 1 || pobi[1] === 400 || crong[1] === 400) {
+    return -1
+  }
   
   return answer;
 }
