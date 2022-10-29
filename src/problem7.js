43dc06abaaf6df1b4e1f25805e28e2a9e610d1bf
ramda <91455804+gnfnfnr@users.noreply.github.com>
2022-10-29 19:33:51 +0900

    feat: socre list in problem 7

--- src/problem7.js
+++ src/problem7.js
@@ -1,16 +1,10 @@
 function problem7(user, friends, visitors) {
-  // 친구는 모조건 1이상 있음, 방문자는 없을 수도 있다.
-  //   점수가 0초과 최대 5명까지만 추천
-  //   사용자와 함께 아는 친구 구하기
-
-  //   사용자의 친구 구하기
   const userFrineds = (user, relation) => {
     return relation
       .filter((x) => x[0] === user || x[1] === user)
       .map((x) => (x[0] === user ? x[1] : x[0]));
   };
 
-  // 친구와 함께 아는 친구 숫자 구하기
   const foafCnt = (relation, friends, user) => {
     const foafRel = relation.filter(
       (x) =>
@@ -20,29 +14,32 @@ function problem7(user, friends, visitors) {
     const foafPerson = foafRel.map((x) =>
       friends.includes(x[0]) ? x[1] : x[0]
     );
-    // const arr = [];
-    // foafPerson.forEach((element) => {
-    //   arr.push([element, 10]);
-    // });
-    console.log(foafPerson.map((x) => [x, 10]));
-    // console.log(arr);
     return foafPerson.map((x) => [x, 10]);
   };
 
-  // 방문 횟수에 따른 점수 구하기
   const visitedCnt = (visitors, alreadyFrineds) => {
     visitors = visitors.filter((x) => !alreadyFrineds.includes(x));
-    // const arr = [];
-    // visitors.forEach((element) => {
-    //   arr.push([element, 1]);
-    // });
     return visitors.map((x) => [x, 1]);
+  };
+
+  const highScore = (arr) => {
+    const recommendList = [];
+    const recommendListKey = [...new Set(arr.map((x) => x[0]))];
+    recommendListKey.forEach((x) => recommendList.push([x, 0]));
+    arr.forEach(
+      (x) => (recommendList[recommendListKey.indexOf(x[0])][1] += x[1])
+    );
+    return recommendList
+      .sort((a, b) => b[1] - a[1])
+      .map((x) => x[0])
+      .slice(0, 5);
   };
 
   const alreadyFrineds = userFrineds(user, friends);
   const foaf = foafCnt(friends, alreadyFrineds, user);
-  console.log(userFrineds(user, friends));
-  console.log(foaf);
+  const visit = visitedCnt(visitors, alreadyFrineds);
+
+  return highScore([...visit, ...foaf]);
 }
 
 module.exports = problem7;
