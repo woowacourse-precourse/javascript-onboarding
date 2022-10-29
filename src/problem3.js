dbf7f5dd41aac2bb182021251968b6e51620c3d5
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-29 23:17:26 +0900

    Feat(문제3-기능2): 숫자 배열의 3,6,9 갯수 누적

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,7 @@
 // 1. 각 숫자에대하여 3,6,9의 개수를 반환하는 함수
 // 2. 1부터 number까지 3,6,9의 개수를 누적하여 더함
 
+// 숫자를 입력받아 박수 횟수 반환 함수
 function getClapNumber(number) {
   const arr_369 = ["3", "6", "9"];
   const str_number = number.toString();
@@ -17,6 +18,13 @@ function getClapNumber(number) {
 
 function problem3(number) {
   var answer;
+
+  // 1 ~ number의 숫자 배열
+  let range = [...Array(number)].map((v, idx) => idx + 1);
+
+  // 숫자 배열에 대해서 누적 박수 횟수 구하기
+  answer = range.reduce((acc, num) => acc + getClapNumber(num), 0);
+
   return answer;
 }
 
