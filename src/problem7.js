c33acb4f652518beb81ab2bad2bf655f7eeda44b
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-10-30 03:33:10 +0900

    [problem7] feat: 함께 아는 친구 추출, 방문자 중 이미 친구인 사람 제외하고 추출하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,22 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const known_friends = user_friends(user, friends);
+  const unknown_friends = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < known_friends.length; j++) {
+      if (friends[i].includes(known_friends[j][0])) {
+        unknown_friends.push(
+          ...friends[i].filter((el) => el !== known_friends[j][0] && el !== user)
+        );
+      }
+    }
+  }
+
+  for (let k = 0; k < visitors.length; k++) {
+    for (let l = 0; l < known_friends.length; l++) {
+      if (visitors[k] === known_friends[l][0]) visitors.splice(k, 1);
+    }
+  }
 }
 
 function user_friends(user, friends) {
