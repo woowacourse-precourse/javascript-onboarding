3aa5a1100830b95b1f12c6b3d4b86baa5dc0b618
kimyou1102 <yoosk5485@naver.com>
2022-10-29 19:35:19 +0900

    feat: 문제 3번 - 3,6,9가 들어가는 횟수를 구한다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
   let answer = 0;
   const number_arr = Array.from({ length: number }, (v, i) => i + 1).map(x => String(x));
+  const set = new Set(['3', '6', '9']);
+
+  for (let i = 0; i < number_arr.length; i++) {
+    const num_str = number_arr[i].split('');
+    for (let j = 0; j < num_str.length; j++) {
+      if (set.has(num_str[j])) {
+        answer++;
+      }
+    }
+  }
 
   return answer;
 }
