ee57cfa870ce16d752594dd4e1e4315011f6fab9
chex <chex417@naver.com>
2022-10-29 16:59:00 +0900

    feat: 연속하는 중복 문자열이 있는지 확인하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -8,7 +8,17 @@ function problem2(cryptogram) {
     if (cryptogram.length <= 1) {
       return cryptogram;
     }
+
+    for (let i=0; i<=25; i++){
+      str_to_remove = new RegExp(`[${alpha_arr[i]}]{2,}`, 'g'); // 2번 이상 반복되는 문자열을 매칭하기 위한 정규 표현식 
+      if (str_to_remove.test(cryptogram)===true){ // 연속하는 중복 문자열이 있다면 계속 진행 
+        check = true;
+      }
+    }
+
   }
+  
+  answer = cryptogram;
   return answer
 }
 
