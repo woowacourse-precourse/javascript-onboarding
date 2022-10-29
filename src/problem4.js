9144ce76dd71675ec22415565311a70ab5d3274b
2yunseong <dbsdltjd3701@naver.com>
2022-10-29 14:22:38 +0900

    feat(문제 4번): 청개구리 사전 규칙을 통해 변환하는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,30 @@
+function isUpperCase(char) {
+  const UPPER_CODE_A = "A".charCodeAt(0);
+  const UPPER_CODE_Z = "Z".charCodeAt(0);
+
+  return UPPER_CODE_A <= char && char <= UPPER_CODE_Z;
+}
+
+function isLowerCase(char) {
+  const LOWER_CODE_A = "a".charCodeAt(0);
+  const LOWER_CODE_Z = "z".charCodeAt(0);
+
+  return LOWER_CODE_A <= char && char <= LOWER_CODE_Z;
+}
 function problem4(word) {
   var answer = "";
   for (let i = 0; i < word.length; i++) {
-    if (word[i] === word[i].toUpperCase()) {
-    } else if (word[i] === word[i].toLowerCase()) {
+    const charCode = word.charCodeAt(i);
+    if (isUpperCase(charCode)) {
+      const offset = charCode - 65;
+      const encryptWord = 90 - offset;
+      answer += String.fromCharCode(encryptWord);
+    } else if (isLowerCase(charCode)) {
+      const offset = charCode - 97;
+      const encryptWord = 122 - offset;
+      answer += String.fromCharCode(encryptWord);
+    } else {
+      answer += word[i];
     }
   }
   return answer;
