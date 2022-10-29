ca0d18707191040fc5bab0f0875540b5bec24189
SangDon <adoim@naver.com>
2022-10-29 18:46:11 +0900

    update : problem2.js 2자리이상 연속되는 중복에도 전부 필터링 하도록 함수 변경

--- src/problem2.js
+++ src/problem2.js
@@ -17,15 +17,31 @@ function decipher(word) {
   return key;
 }
 
-// 중복 문자를 제거하고 다시 처음부터 루프를 도는 for loop
+//중복문자 제거 함수
 function decrypto(arr) {
-  const strArr = [...arr];
-  for (let i = 0; i < strArr.length; i++) {
-    if (strArr[i] === strArr[i + 1]) {
-      strArr.splice(i, 2);
-      i = -1;
-    }
-  }
+  let newArr = []; //새로운 배열을 담는다.
+  let deletedWord = "";
+  let index = 0; //배열삭제추가등으로 인덱스가 동적이기때문에 직접 조절해줌.
 
-  return strArr;
+  arr.map((word) => {
+    if (newArr.length === 0) {
+      newArr.push(word);
+      index += 1;
+      return;
+    } //첫글자 또는 배열이 비었을시 반드시 추가
+    if (word === newArr[index - 1]) {
+      newArr.pop();
+      deletedWord = word;
+      index -= 1;
+      return;
+    } //새로 들어올 문자가 기존문자의 마지막과 같으면 기존문자 삭제후 인덱스 감소
+    if (word === deletedWord) {
+      return; //이전에 삭제된 문자와 새로 들어올 문자가 같으면 무시
+    } else {
+      newArr.push(word);
+      deletedWord = "";
+      index += 1;
+    } //새로운 글자는 추가하고 인덱스 증가.
+  });
+  return newArr;
 }
