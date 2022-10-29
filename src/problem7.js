766222e7488de0be699fefa3d6d53efaf859d9a7
murpin <qw04011@gmail.com>
2022-10-29 22:39:42 +0900

    [문제 7번] docs: 기능 분석 주석 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,13 @@
+/*
+ * 기능 분석
+ * [] friends에 대한 친구 관계 friendList에 저장하는 기능
+ * [] user의 친구의 친구 중 user와 친구를 맺지 않은 친구들에 대한 선택 및 10점씩 추가 기능
+ * [] user의 친구의 친구 중 user와 친구를 맺지 않은 친구들이 visitor에 존재하는 갯수만큼 1점씩 추가 기능
+ * [] 점수가 결정된 친구에 대한 recommendList에 저장하기
+ * [] user의 친구의 친구는 아니지만 visitor에 있는 인원에 대한 점수 계산 및 recommendList에 저장
+ * [] recommendList에 저장될 때 점수가 높고, 같을 경우 초성이 사전순으로 빠른 5개만 가지고 있도록 유지하는 기능 
+ */
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
