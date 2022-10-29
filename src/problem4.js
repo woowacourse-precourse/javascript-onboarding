e4825a93fc8ccb7d05c6c508b4b4e29348d59dd3
yaena1223 <yaena1223@dgu.ac.kr>
2022-10-29 12:50:46 +0900

    [feat] problem4 - 문자열 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,35 @@
 function problem4(word) {
   var answer;
+  answer = flip_word(word);
   return answer;
 }
 
+function flip_big_alp(alp) {
+  let flip;
+  alp = alp.charCodeAt();
+  flip = "A".charCodeAt(0) + "Z".charCodeAt(0) - alp;
+  flip = String.fromCharCode(flip);
+  return flip;
+}
+
+function flip_small_alp(alp) {
+  let flip;
+  alp = alp.charCodeAt();
+  flip = "a".charCodeAt(0) + "z".charCodeAt(0) - alp;
+  flip = String.fromCharCode(flip);
+  return flip;
+}
+
+function flip_word(word) {
+  let ans = "";
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == " ") ans += " ";
+    else if (word[i] > "a") ans += flip_small_alp(word[i]);
+    else ans += flip_big_alp(word[i]);
+  }
+  return ans;
+}
 module.exports = problem4;
+
+//A와 Z의 아스키코드의 합은 155. 모든 flip되는 쌍은 합이 155임.
+console.log("a".charCodeAt(0));
