7addcb8e2f54fe4204fdaf6e7181a631320eac02
lineJeong <line.jeong32@gmail.com>
2022-10-29 17:18:41 +0900

    problem1 완료

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,44 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const maxNumOfPobi = findMaxNum(pobi);
+  const maxNumOfCrong = findMaxNum(crong);
+
+  if (maxNumOfPobi === -1 || maxNumOfCrong === -1) return -1;
+
+  if (maxNumOfPobi === maxNumOfCrong) {
+    answer = 0;
+  } else if (maxNumOfPobi > maxNumOfCrong) {
+    answer = 1;
+  } else {
+    answer = 2;
+  }
+
   return answer;
+}
+
+// 왼쪽, 오른쪽 페이지 중 더 큰 값 return
+function findMaxNum([left, right]) {
+  if (left > 0 && right < 401 && left % 2 && right === left + 1) {
+    const leftResult = calculateNums(left);
+    const rightResult = calculateNums(right);
+
+    return leftResult > rightResult ? leftResult : rightResult;
+  }
+
+  return -1;
+}
+
+// 더하기, 곱하기 중 더 큰 값 return
+function calculateNums(num) {
+  const sum = String(num)
+    .split("")
+    .reduce((acc, cur) => acc + Number(cur), 0);
+  const multi = String(num)
+    .split("")
+    .reduce((acc, cur) => acc * Number(cur), 1);
+
+  return sum > multi ? sum : multi;
 }
 
 module.exports = problem1;
