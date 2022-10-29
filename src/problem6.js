ed60642c0b420520ec0d2bd71b71fdf67c574bd1
ghenmaru <rbgks1937@naver.com>
2022-10-29 21:16:21 +0900

    feat: [problem6] add function list

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,19 @@
+/*
+[기능 목록]
+1. 이메일 형식 체크
+2. 연속된 중복문자 체크
+3. 닉네임 한글 체크
+4. 연속된 중복문자가 없는 이메일 중 중복된 이메일 제거하고 오름차순 정렬한 결과 반환
+*/
+
+
 function problem6(forms) {
-  var answer;
+  let answer;
   return answer;
 }
+
+problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], 
+["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ])
+// ["jason@email.com", "jm@email.com", "mj@email.com"]
 
 module.exports = problem6;
