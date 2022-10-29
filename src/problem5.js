d91eebaa7cacc0a6faf591ca3c4510089b344455
jsh3418 <albert5428@gmail.com>
2022-10-29 13:30:21 +0900

    chore: problem5 요구사항, 로직 정리

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  // 돈의 단위를 배열로 정렬
+  // 단위를 순회하며 money를 최대한 뺄 수 있는 만큼 빼고 뺀만큼 카운트 하여 배열에 푸쉬한다.
+  // 카운트 한 배열을 리턴한다.
 }
 
 module.exports = problem5;
