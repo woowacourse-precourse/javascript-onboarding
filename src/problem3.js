ab8d9b28a35c4f85d6948dec2c0787e002a72fed
hyukzz <ri_chard@naver.com>
2022-10-29 15:49:09 +0900

    feat: [문제3]3,6,9마다 카운트 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,21 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function countClapNums(num) {
+	const clapNums = [3, 6, 9];
+	let count = 0;
+	num = num + "";
+
+	num.split("").forEach(el => {
+		if (clapNums.includes(Number(el))) return count++;
+	});
+	return count;
+}
+
+function problem3(num) {
+	let answer = 0;
+
+	for (let i = 0; i <= num; i++) {
+		answer += countClapNums(i);
+	}
+	return answer;
 }
 
 module.exports = problem3;
