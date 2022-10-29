0bd146a80659dc99a02db00b8efdae5364f27493
HBSPS <wiyeong79@naver.com>
2022-10-29 19:04:15 +0900

    :ambulance: [문제5] 기능1. changeMoney 메소드에 500원 케이스 추가
    changeMoney : 금액별로 몇 개로 변환되는지 return 하는 메소드

--- src/problem5.js
+++ src/problem5.js
@@ -8,14 +8,12 @@ function problem5(money) {
 }
 
 function changeMoney(money) {
-  const changedMoney = []
-  const moneyArray = [50000, 10000, 5000, 1000, 100, 50, 10, 1];
+  const changedMoney = [];
+  const moneyArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
-  while (money > 0) {
-    for (let i = 0; i < moneyArray.length; i++) {
-      changedMoney.push(parseInt(money / moneyArray[i]));
-      money = money % moneyArray[i];
-    }
+  for (let i = 0; i < moneyArray.length; i++) {
+    changedMoney.push(parseInt(money / moneyArray[i]));
+    money = money % moneyArray[i];
   }
 
   return changedMoney;
