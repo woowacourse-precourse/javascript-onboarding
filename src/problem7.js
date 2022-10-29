a95c79a7d20522171f8127add0abeae49be82c04
confidential-nt <youyoy689@gmail.com>
2022-10-30 05:22:27 +0900

    요구사항 7번 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,53 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const score = {};
+
+  const usersFriends = friends
+    .filter((element) => element.includes(user))
+    .flatMap((element) =>
+      element.splice(
+        element.findIndex((innerEl) => innerEl !== user),
+        1
+      )
+    );
+
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < usersFriends.length; j++) {
+      if (!friends[i].includes(user) && friends[i].includes(usersFriends[j])) {
+        const index = friends[i].findIndex(
+          (element) => element !== usersFriends[j]
+        );
+
+        if (friends[i][index] in score) {
+          score[friends[i][index]] += 10;
+        } else {
+          score[friends[i][index]] = 10;
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < visitors.length; i++) {
+    if (usersFriends.includes(visitors[i])) continue;
+    if (visitors[i] in score) {
+      score[visitors[i]] += 1;
+    } else {
+      score[visitors[i]] = 1;
+    }
+  }
+
+  const result = Object.entries(score)
+    .sort((a, b) => {
+      if (a[1] > b[1]) return -1;
+      if (a[1] < b[1]) return 1;
+      if (a[1] === b[1]) {
+        if (a[0] > b[0]) return 1;
+        if (a[0] < b[0]) return -1;
+        else return 0;
+      }
+    })
+    .flatMap((element) => element[0]);
+
+  return result.splice(0, 5);
 }
 
 module.exports = problem7;
