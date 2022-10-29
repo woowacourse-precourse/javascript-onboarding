f1b01944d62ebe9d33bacac9ad64c58dc3e9d422
freemoon99 <soloist132@gmail.com>
2022-10-29 18:12:19 +0900

    feat[problem7]: 이미 친구혹은 본인인 경우 0점을 부여하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,14 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let score = new Map();
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      friends[i].map((ele) => {
+        return score.set(ele, 0);
+      });
+    } 
+  }
   return answer;
 }
 
