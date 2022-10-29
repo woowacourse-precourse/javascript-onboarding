9da1aaeca10b02a709405d564b947f5a77509252
MinGyu-Kang <kagrin97@gmail.com>
2022-10-29 16:44:57 +0900

    problem6/feat : forms 배열을 돌면서 닉네임이 2글자 이상일 경우 문자열을 모든 연속된 2글자씩 문자로 구하는 코드 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const nickName = new Set();
+
+  forms.forEach((arr, idx) => {
+    const [email, name] = arr;
+
+    if (name.length >= 2) {
+      for (let i = 0; i < name.length - 1; i++) {
+        const str = name[i] + name[i + 1];
+        console.log(str);
+      }
+    }
+  });
+
+  return;
 }
 
 module.exports = problem6;
+
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
