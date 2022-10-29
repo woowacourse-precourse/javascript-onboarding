eee08ff08f1be24764aebaca0486acd420122c62
unknown <chn0202@gmail.com>
2022-10-29 17:01:09 +0900

    Refactor: proplem6 리팩토링
    depth줄이기

--- src/problem6.js
+++ src/problem6.js
@@ -1,35 +1,27 @@
 function problem6(forms) {
-  let repetitionCheck = new Map();
-  let duplicateUserEmail = [];
-  for (let user of forms) {
-    let nickname = user[1];
-    for (let j = 0; j < nickname.length - 1; j++) {
-      let duplicateNickname = nickname.slice(j, j + 2);
-      if (repetitionCheck.has(duplicateNickname)) {
-        if (
-          !duplicateUserEmail.includes(repetitionCheck.get(duplicateNickname))
-        )
-          duplicateUserEmail.push(repetitionCheck.get(duplicateNickname));
-        duplicateUserEmail.push(user[0]);
-      } else repetitionCheck.set(duplicateNickname, user[0]);
-    }
-  }
-  for (let i = 0; i < duplicateUserEmail.length; i++) {
-    let lowCharacterIndex = i;
+  let duplicateCount = new Map();
 
-    for (let j = i + 1; j < duplicateUserEmail.length; j++) {
-      if (
-        duplicateUserEmail[lowCharacterIndex].split("@")[0] >
-        duplicateUserEmail[j].split("@")[0]
-      )
-        lowCharacterIndex = j;
+  forms.forEach((crew) => {
+    for (let i = 0; i < crew[1].length - 1; i++) {
+      let duplicate = crew[1].slice(i, i + 2);
+      if (duplicateCount.has(duplicate))
+        duplicateCount.set(duplicate, duplicateCount.get(duplicate) + 1);
+      else duplicateCount.set(duplicate, 1);
     }
-    [duplicateUserEmail[i], duplicateUserEmail[lowCharacterIndex]] = [
-      duplicateUserEmail[lowCharacterIndex],
-      duplicateUserEmail[i],
-    ];
-  }
-  return duplicateUserEmail;
+  });
+  let duplicateStrArr = [];
+  duplicateCount.forEach((value, key) => {
+    if (value > 1) duplicateStrArr.push(key);
+  });
+
+  let emails = [];
+
+  forms.forEach((crew) => {
+    duplicateStrArr.forEach((y) => {
+      if (crew[1].includes(y)) emails.push(crew[0]);
+    });
+  });
+  return emails.sort();
 }
 
 module.exports = problem6;
