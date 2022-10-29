34f1f02cc991969fbe5eb2fe98fe1e60b11e698d
River <swimmingr@gmail.com>
2022-10-30 03:45:07 +0900

    problem3 1부터 number까지 숫자 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,11 @@
+function toNum(num) {
+  let nArr = [];
+  for (let i = 1; i <= num; i++) {
+    nArr.push(i);
+  }
+  return nArr;
+}
+
 function problem3(number) {
   var answer;
   return answer;
