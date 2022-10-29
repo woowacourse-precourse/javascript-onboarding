71dc6b55ce2ab0cfe1aaa5423473a9aa808df6d6
easyone <thinkanddoit@naver.com>
2022-10-29 17:31:28 +0900

    #7 feat: 문제7 타임라인 방문기록을 점수에 반영
    - 최종 score을 [userId, score] 형태로 final_score에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -14,11 +14,19 @@ function problem7(user, friends, visitors) {
   friendRelation.delete(user);
 
   const FRIEND_SCORE = 10;
+  const VISIT_SCORE = 1;
   const scores = Array.from(friendRelation).map(([userId, friends]) => {
     const score =
       Number(friends.filter((userId) => userFriends.includes(userId)).length) *
       FRIEND_SCORE;
     return [userId, score];
+  });
+
+  const final_scores = scores.map(([userId, score]) => {
+    const final_score =
+      score +
+      Number(visitors.filter((id) => userId === id).length) * VISIT_SCORE;
+    return [(userId, final_score)];
   });
 
   const answer = [];
