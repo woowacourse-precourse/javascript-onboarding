95fbd13f1ff7583d76f6880dd949ac1009374083
hae-on <solbi2004@naver.com>
2022-10-29 21:54:53 +0900

    [문제4] 제한 사항 검사

--- src/problem4.js
+++ src/problem4.js
@@ -3,7 +3,15 @@ const asciiUpperZ = 90;
 const asciiLowerA = 97;
 const asciiLowerZ = 122;
 
+const checkValid = (word) => {
+  if (typeof word === 'string') return true;
+  if (word.length >= 1 && word.length <= 1000) return true;
+  return false;
+};
+
 function problem4(word) {
+  if (checkValid(word)) return false;
+
   let frogWord = '';
 
   for (let i = 0; i < word.length; i++) {
