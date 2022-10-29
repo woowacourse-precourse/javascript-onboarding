51127f4b24b02e76dce72735bdefb727bd9383da
Jaehyeon <jaehyeonp74@gmail.com>
2022-10-29 15:30:08 +0900

    refactor: 6번 예외처리 및 리팩토링
    - 한 사람의 이름에서 특정 단어가 중복해서 나오는 경우 예외처리
    - 변수명 다시 짓기 + 구조 할당 분해

--- src/problem6.js
+++ src/problem6.js
@@ -1,26 +1,41 @@
 function problem6(forms) {
-  var answer = [];
+  let overLappingWords = [];
   let repeatTextObj = {};
-  forms.map((form) => {
-    const nickname = form[1];
-    for (let i = 0; i < nickname.length - 1; i++) {
-      const testText = nickname.substr(i, 2);
-      if (testText in repeatTextObj) {
+
+  forms.map((form, index) => {
+    let firstRecordObj = {};
+    const [_, nickname] = form;
+    for (let j = 0; j < nickname.length - 1; j++) {
+      const testText = nickname[j] + nickname[j + 1];
+      if (testText in repeatTextObj && firstRecordObj[testText] !== index) {
         repeatTextObj[testText]++;
       } else {
         repeatTextObj[testText] = 1;
+        firstRecordObj[testText] = index;
       }
     }
   });
 
   forms.map((form) => {
     const [email, nickname] = form;
-    for (let i = 0; i < nickname.length - 1; i++) {
-      const testText = nickname.substr(i, 2);
-      if (repeatTextObj[testText] > 1) answer.push(email);
+    for (let j = 0; j < nickname.length - 1; j++) {
+      const testText = nickname[j] + nickname[j + 1];
+      if (repeatTextObj[testText] > 1) overLappingWords.push(email);
     }
   });
-  return [...new Set(answer)].sort();
+  return [...new Set(overLappingWords)].sort();
 }
 
+console.log(
+  problem6([
+    ["jm@email.com", "이불이불엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
+
+// 같은 글자가 연속적으로 반복하되 같은 이름에서 반복될 경우는
+// 제외해주어야 한다
 module.exports = problem6;
