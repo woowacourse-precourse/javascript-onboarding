a1dc29a0129137f48a257092443854be0cdb9eb0
heyheyley <jeonheyley@gmail.com>
2022-10-30 02:13:42 +0900

    문제4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  for (let i = 0; i < word.length; i++) {
+    let char = word.charAt(i);
+    let ascii = char.charCodeAt(0);
+    //대문자 소문자 별로 케이스 분류 후,
+    //문자별 아스키코드를 전부 더하여 현재 탐색하고 있는 알파벳의 아스키코드만큼 빼주면 거꾸로 된 아스키코드가 나오게 된다.
+    //그걸 다시 문자로 변환해 answer에 넣어줌
+    if (ascii >= 97 && ascii <= 122) {
+      answer += String.fromCharCode(122 + 97 - ascii);
+    } else if (ascii >= 65 && ascii <= 90) {
+      answer += String.fromCharCode(65 + 90 - ascii);
+    } else {
+      answer += char;
+    }
+  }
   return answer;
 }
 
