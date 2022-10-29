a97a58982bd4ead453ac2b56c8d4b7f5c0840332
d0422 <rlfehd2013@naver.com>
2022-10-29 18:37:28 +0900

    Feat:이미 친구인 사람들의 목록을 구하는 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -2,5 +2,12 @@ function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
-
+function getFriendsList(user, friends) {
+  const friendList = [];
+  for (let x = 0; x < friends.length; x++) {
+    if (friends[x][0] == user) friendList.push(friends[x][1]);
+    if (friends[x][1] == user) friendList.push(friends[x][0]);
+  }
+  return friendList;
+}
 module.exports = problem7;
