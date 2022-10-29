3c9a9d6e7892380ea8df76a3fd75ba73974773dc
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 14:47:05 +0900

    problem3. 특정 숫자 확인을 위해 for 문 내 매개변수 배열화

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let nums = String(i).split("");
+  }
+
+  return;
 }
 
 module.exports = problem3;
