dca0bda5c8b89959af1913b279c0de21646a9cbe
kangju2000 <kangju2000@naver.com>
2022-10-29 20:14:59 +0900

    feat: setTwoLetters 함수 선언

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,15 @@ function problem6(forms) {
   return answer;
 }
 
+function setTwoLetters(nickname) {
+  let twoLettersArr = nickname.split('').reduce((acc, cur, idx) => {
+    if (idx !== nickname.length - 1) {
+      acc.push(cur + nickname[idx + 1]);
+    }
+    return acc;
+  }, []);
+
+  return [...new Set(twoLettersArr)];
+}
+
 module.exports = problem6;
