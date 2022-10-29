483dad254f2e2fbfd2d3f4d6002905dcaf97f2c8
Ryong-E <think8867@gmail.com>
2022-10-29 21:38:01 +0900

    feat: PROBLEM7 유저이름으로 내 친구들 찾는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,15 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function getMyFriend(user,friends){
+  const foundFriends = [];
+  friends.forEach(friend => {
+    if(friend.includes(user)){
+      const friendName = friend.slice(0,friend.indexOf(user));
+      foundFriends.push(...friendName);
+    }
+  })
+
+  return foundFriends;
+}
 module.exports = problem7;
