37a7d8bb4cadec306d2bea7b3f0df94e7be7fcf5
xilucks <siun0331@gmail.com>
2022-10-29 17:41:48 +0900

    feat: problem6 solve
    17번째 라인의 결과값을 도출하기 위해 배열을 합치는 과정에서
    concat을 사용하면 합쳐지지 않는데, 해당 부분에 대한 공부를 진행해야함.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,46 @@
+/***
+ * 두글자 단위로 싹 파싱하여 자료구조에 저장 ObjectType {key: value [parsingNickname] : e-mail}
+ * 중복되면 이메일 배열에 추
+ * @param forms {Array}
+ * @returns {*}
+ */
 function problem6(forms) {
-  var answer;
+  var answer = makeDuplicateList(forms);
   return answer;
+}
+
+function makeDuplicateList(forms) {
+  const userList = makeNicknameList(forms);
+  let result = [];
+  for (let nickname in userList) {
+    if (userList[nickname].length > 1) {
+      result.push(...userList[nickname]);
+    }
+  }
+  return sortUserList(result);
+}
+
+function sortUserList(userList) {
+  return userList.sort();
+}
+
+function makeNicknameList(forms) {
+  let userList = {};
+  return forms.reduce((acc, cur) => {
+    let [email, nickname] = cur;
+    return (acc = userNicknameParser(acc, email, nickname));
+  }, userList);
+}
+
+function userNicknameParser(userList, email, nickname) {
+  for (let spellingIdx = 0; spellingIdx < nickname.length - 1; spellingIdx++) {
+    const parsingNickname = nickname.substring(spellingIdx, spellingIdx + 2);
+    !userList.hasOwnProperty(parsingNickname)
+      ? (userList[parsingNickname] = [email])
+      : userList[parsingNickname].push(email);
+  }
+
+  return userList;
 }
 
 module.exports = problem6;
