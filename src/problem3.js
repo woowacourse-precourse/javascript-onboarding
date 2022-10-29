1e5cb950e9cd626584f51fc61b6abf2225ef6b49
oseunghyun <ohsh1477@gmail.com>
2022-10-29 12:59:31 +0900

    feat(p3): 각 자리 숫자를 구해 문자열 배열로 저장하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,9 @@
+// number의 각 자리 숫자를 구해 문자열 배열로 저장하는 기능
+const convertToDigitArr = (number) => {
+  number = number.toString();
+  return number;
+};
+
 function problem3(number) {
   var answer;
   return answer;
