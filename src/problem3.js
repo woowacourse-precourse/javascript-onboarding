bfb35305128eed2c554b457b28632ae98fbef552
confidential-nt <youyoy689@gmail.com>
2022-10-30 05:46:32 +0900

    요구사항 3번 주석달기

--- src/problem3.js
+++ src/problem3.js
@@ -11,8 +11,10 @@ function problem3(number) {
   if (number < 1 || number > 10000) return;
   let count = 0;
   for (let i = 1; i <= number; i++) {
+    // number의 각 자릿수 얻기
     const digits = getDigits(i);
 
+    // 각 자릿수가 몇 번 나왔는지 계산
     const digitsObj = digits.reduce((pre, cur) => {
       if (cur in pre) pre[cur]++;
       else pre[cur] = 1;
@@ -20,6 +22,7 @@ function problem3(number) {
       return pre;
     }, {});
 
+    // 박수 몇번 쳐야하는지 계산
     if (digitsObj[3]) {
       count += digitsObj[3];
     }
