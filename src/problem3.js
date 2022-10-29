14d62b5e0241466b698103d73369aa9b60263126
2wndrhs <2wndrhs@gmail.com>
2022-10-28 23:38:37 -0700

    feat(PROBLEM3): 숫자를 각 자리 숫자로 분리하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
+// 숫자를 각 자리 숫자로 분리하는 기능
+function splitNumbers(numbers) {
+  const splitedNumbers = numbers.map((number) =>
+    number
+      .toString()
+      .split("")
+      .map((number) => +number)
+  );
+  return splitedNumbers;
+}
+
+// solution
+function solution(number) {
+  const sequences = Array.from({ length: number }, (_, i) => i);
+  console.log(splitNumbers(sequences));
+}
+
 function problem3(number) {
   var answer;
   return answer;
 }
+
+solution(15);
 
 module.exports = problem3;
