d32c612e8b22310228a019164d5a354f01bdca61
TeusLee <xodn1311@gmail.com>
2022-10-30 08:10:04 +0900

    prob1 sol complete

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,31 @@
+const fin_max = (page) => {
+  temp_sum = 0
+  temp_mul = 1  
+  while (page >=1){
+    page = page * 0.1
+    temp_digit = Math.round(10*(page - Math.floor(page)), 0)
+    temp_sum += temp_digit
+    temp_mul *= temp_digit 
+    page = Math.floor(page)
+  }  
+  return Math.max(temp_sum, temp_mul)
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if (pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1){
+    return -1
+  }
+  pobi_max = Math.max(fin_max(pobi[0]), fin_max(pobi[1]))
+  crong_max = Math.max(fin_max(crong[0]), fin_max(crong[1]))
+  if (pobi_max > crong_max){
+    answer = 1
+  } else if (pobi_max < crong_max){
+    answer = 2
+  } else if (pobi_max === crong_max){
+    answer = 0
+  }
+
   return answer;
 }
 
