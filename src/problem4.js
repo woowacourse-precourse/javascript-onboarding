a319a3d77f972a27419cbff6a28b5f491340a6cd
sunny <huiseon37@gmail.com>
2022-10-30 03:16:20 +0900

    [problem4] 소문자 변경 함수 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,9 @@
+function LowerChange(ascii) {
+  const changeAscii = ascii < 110 ? 2 * (109 - ascii) +1 : 2 * (110 - ascii) + 1;
+  
+  return ascii + changeAscii;
+}
+
 function upperChange(ascii) {
   const changeAscii = ascii < 78 ? 2 * (77 - ascii) +1 : 2 * (78 - ascii) + 1;
   
@@ -6,7 +12,6 @@ function upperChange(ascii) {
 
 function getChangedWord(word) {
   const wordArr = Array.from(word,(el) => el.charCodeAt());
-
 }
 
 function problem4(word) {
