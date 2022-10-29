bece89e5c994de17713c656c928dd82048807887
saram <yujun1018@naver.com>
2022-10-29 16:42:05 +0900

    [문제7] feat: 친구 관계 배열을 이용해 친구 목록을 생성하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,22 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendList = createFriendList(friends, user);
+  const friendsFriendList = [];
+  for (let i = 0; i < friendList.length; i++) {
+    const friend = friendList[i];
+    friendsFriendList.push(...createFriendList(friends, friend, friendList));
+  }
 }
+
+const createFriendList = (relationshipList, who, friendList = []) => {
+  const list = [];
+  relationshipList.forEach((relationship) => {
+    if (relationship.includes(who)) {
+      const friendName =
+        relationship[relationship.findIndex((name) => name !== who)];
+      if (!friendList.includes(friendName)) list.push(friendName);
+    }
+  });
+  return list;
+};
 
 module.exports = problem7;
