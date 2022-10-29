70ffedebc38dd0ea034f3d27990bdbdf82fe1a6c
kyhyun <k29176@gmail.com>
2022-10-29 19:07:40 +0900

    feat : 문제 4번 - 문자가 소문자인지 판별하는 기능 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
+/*
+기능 구현 목록
+[v] 문자가 소문자인지 판별하는 기능
+[ ] 문자가 대문자인지 판별하는 기능
+[ ] 대문자 알파벳 변환 기능
+[ ] 소문자 알파벳 변환 기능
+*/
+
 function problem4(word) {
-  var answer;
+  let answer = '';
   return answer;
 }
 
+function isLower(char) {
+  const LOWER_CASE_START_POINT = 97;
+  const LOWER_CASE_END_POINT = 122;
+  return LOWER_CASE_START_POINT <= char && char <= LOWER_CASE_END_POINT ? true : false;
+}
+
+problem4('I love you'); // 'R olev blf'
+problem4('Hello Everyone!'); // 'Svool Veviblmv!'
+problem4('Grrr... Wang!'); // 'Tiii... Dzmt!'
 module.exports = problem4;
