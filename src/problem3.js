c454894ab6e8278e4a50bdb0ca6b6b4abe15a616
Steadystudy <sgmin206@naver.com>
2022-10-29 23:32:43 +0900

    [problem3] Feat: 박수갯수 세는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
-function problem3(number) {
-  var answer;
-  return answer;
+let clap = 0;
+
+function countClap(num) {
+  const character = num % 10;
+  if(character !== 0 && character % 3 === 0) {
+    clap++;
+  }
+
+  return num / 10 < 1 ? clap : countClap(Math.floor(num / 10));
 }
 
+function problem3(number) {}
+
 module.exports = problem3;
+
+
