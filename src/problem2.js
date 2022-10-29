c888c9f190da1bee2434c21a842cdab480e03259
unsnruu <unsnruu@gmail.com>
2022-10-29 12:46:45 +0900

    ✨ 문자열에서 연속으로 중복된 부분을 찾는 함수 구현 (problem2)
    문자열에서 연속으로 중복되는 부분을 찾는 함수를 구현했습니다.
    해당 함수는 중복된 문자열의 인덱스 값을 반환합니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,7 @@
 function problem2(cryptogram) {
   var answer;
+  console.log(cryptogram);
+  getRepeated(cryptogram);
   return answer;
 }
 
@@ -10,3 +12,26 @@ module.exports = problem2;
  * 2. 문자열에서 중복되는 지를 판별하는 함수가 필요로 하다.
  * 3. 문자열을 제거하는 함수가 필요로 하다.
  */
+
+function getRepeated(str) {
+  const results = [];
+
+  for (let i = 0; i < str.length; i++) {
+    let ch = str[i];
+    let continued = 0;
+
+    for (let j = i + 1; j < str.length; j++) {
+      let nextCh = str[j];
+
+      if (ch !== nextCh) {
+        if (continued !== 0) {
+          results.push([i, i + continued]);
+        }
+        break;
+      } else {
+        continued++;
+      }
+    }
+  }
+  console.log(results);
+}
