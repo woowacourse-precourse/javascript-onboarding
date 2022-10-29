0408e6c2485a486e96eb619d0a1c24e3347a5b48
yu-retel <dbdudals100@naver.com>
2022-10-29 14:13:28 +0900

    feat: [P7] 점수배점기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -9,7 +9,7 @@ function userFriends(user,friends){
   let userFriends=[];
   for(let i=0; i<friends.length; i++){
     if(friends[i].includes(user)){
-      userFriends.push(friends[i].find((el)=>el!==user))
+      userFriends.push(friends[i].find((el)=>el!==user));
     }
   }
   return userFriends;
@@ -21,9 +21,22 @@ function friendOfFriends(user,friends){
   for(let i=0; i<userFriendArr.length; i++){
     for(let j=0; j<friends.length; j++){
       if(friends[j].includes(userFriendArr[i])){
-        friendOfFriends.push(friends[j].find(el=>el!==userFriendArr[i]))
+        friendOfFriends.push(friends[j].find(el=>el!==userFriendArr[i]));
       }
     }
   }
-  return friendOfFriends.filter(el=>el!==user)
+  return friendOfFriends.filter(el=>el!==user);
+}
+
+function giveScore(user,friends,visitors){
+  let scoreList=new Map();
+  let tenPoint=friendOfFriends(user,friends);
+  let onePoint=visitors;
+  for(let i=0; i<tenPoint.length; i++){
+    scoreList.set(tenPoint[i],(scoreList.get(tenPoint[i]) || 0)+10);
+  }
+  for(let i=0; i<onePoint.length; i++){
+    scoreList.set(onePoint[i],(scoreList.get(onePoint[i]) || 0)+1);
+  }
+  return [...scoreList];
 }
