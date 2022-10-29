c68ea68daf17853362f5bd2301e4162cce2bd1fe
Sijun <sionwer4@naver.com>
2022-10-29 18:00:39 +0900

    problem7 sorted alphabet problem solved

--- src/problem7.js
+++ src/problem7.js
@@ -39,7 +39,26 @@ function problem7(user, friends, visitors) {
   // 4. sort in descending order.
   const sortedRec = [...recommended.entries()].sort((a, b) => (b[1] - a[1]));
 
-  // 5. withdraw the result from sorted array
+  // 5. sort in the case of same point.
+  if(sortedRec.length > 1){
+    let samepoint = new Set();
+    for(i = 1; i < sortedRec.length; i++){
+      if(sortedRec[i][1] == sortedRec[i - 1][1]){
+        samepoint.add(sortedRec[i - 1][0]);
+        samepoint.add(sortedRec[i][0]);
+      }else{
+        var arr = Array.from(samepoint);
+        arr.sort();
+        var start = i - arr.length;
+        for(j = 0; j < arr.length; j++){
+          sortedRec[j + start][0] = arr[j];
+        }
+      }
+    }
+  }
+
+
+  // 6. withdraw the result from sorted array
   for(i = 0; i < sortedRec.length; i++){
     result.push(sortedRec[i][0]);
   }
