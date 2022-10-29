c4f851d1a44dc53c26290c8f22fcc2b497d8f5ea
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-10-30 02:46:34 +0900

    미션 2 성공

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+  let cryptogramArr = cryptogram.split('');
+
+  let i = 0;
+
+  while (i < cryptogramArr.length) {
+    cryptogramArr[i] == cryptogramArr[i + 1] ? (cryptogramArr.splice(i, 2), (i = 0)) : (i += 1); //false i 1증가
+  }
+
+  return cryptogramArr.join('');
 }
 
 module.exports = problem2;
