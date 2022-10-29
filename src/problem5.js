897267b031b291c496360668c7af32fe6cffa998
mochang2 <qjsrodksro@gmail.com>
2022-10-29 23:18:16 +0900

    [PROBLEM5] 기능 요구 사항 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,47 @@
-function problem5(money) {
-  var answer;
-  return answer;
+/**
+ * 올바른 입력인지 확인한다.
+ * @param {number} number
+ * @returns {boolean}
+ */
+function isInvalidInputs(number) {
+  if (!number) {
+    return true
+  }
+
+  if (typeof number !== 'number') {
+    return true
+  }
+
+  if (number < 1 || number > 1000000) {
+    return true
+  }
+
+  return false
 }
 
-module.exports = problem5;
+/**
+ * 은행에서 출금할 화폐를 반환한다.
+ * @param {number} money
+ * @returns {number[] | string}
+ */
+function problem5(money) {
+  if (isInvalidInputs(money)) {
+    return '입력값이 잘못되었습니다.'
+  }
+
+  let currencyCount
+  let changes = money
+  const currencies = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  const currencyCounts = []
+
+  currencies.forEach((currency) => {
+    currencyCount = Math.floor(changes / currency)
+    changes %= currency
+
+    currencyCounts.push(currencyCount)
+  })
+
+  return currencyCounts
+}
+
+module.exports = problem5
