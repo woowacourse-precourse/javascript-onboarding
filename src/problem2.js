96fe14a4e25e7cc1282219fd9f432552afb50cbf
inthhh <glass005533@gmail.com>
2022-10-30 00:44:38 +0900

    p2_getUnduplicate

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
   var answer;
+  var cr = cryptogram;
+
+  while(1){
+
+    for(let i=0; i<cr.length; ++i){
+      if(cr[i] == cr[i+1]){
+        
+        let front = cr.substring(0,i);
+        let back = cr.substring(i+2, cr.length);
+        cr = front + back;
+        i--;
+      }
+    }
+  }
   return answer;
 }
 
