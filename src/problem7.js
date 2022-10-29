f04107b4deac8149b60843b16aacd2be2144f5ae
su-lim <sujin.0xlim@gmail.com>
2022-10-29 18:56:30 +0900

    feat: [prob7] 친구 관계 객체 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,13 @@
 function problem7(user, friends, visitors) {
   var answer;
+  
+  let friendsList = {}
 
   // 1. 친구 관계 배열 생성
+  for (let i=0; i<friends.length; i++){
+    friendsList[friends[i][0]] ? friendsList[friends[i][0]].push(friends[i][1]) : friendsList[friends[i][0]] = [friends[i][1]];
+    friendsList[friends[i][1]] ? friendsList[friends[i][1]].push(friends[i][0]) : friendsList[friends[i][1]] = [friends[i][0]];
+  }
 
   // 2. 유저 배열 생성
 
