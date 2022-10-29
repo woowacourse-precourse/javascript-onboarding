6e7d7b69ef3fae0dc8afb2eacbf5182a6c667e05
Steadystudy <sgmin206@naver.com>
2022-10-29 23:35:36 +0900

    [problem3] Feat: 총 박수 세기

--- src/problem3.js
+++ src/problem3.js
@@ -9,7 +9,17 @@ function countClap(num) {
   return num / 10 < 1 ? clap : countClap(Math.floor(num / 10));
 }
 
-function problem3(number) {}
+function problem3(number) {
+  let totalClap = 0;
+  
+  for(let i = 1; i <= number; i++) {
+    clap = 0;
+    countClap(i);
+    totalClap += clap;
+  }
+
+  return totalClap;
+}
 
 module.exports = problem3;
 
