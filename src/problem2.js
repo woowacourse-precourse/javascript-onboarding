3a1a182afd3950c31dbe98deba7f469de636e9cc
OrangeVinyl <hap_hazard@naver.com>
2022-10-30 01:29:08 +0900

    Problem2_Repeat

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,13 @@
 function problem2(cryptogram) {
   var answer = [cryptogram[0]]; // cryptogram을 배열로 입력 받아 answer에 대입
-  var top = answer[answer.length - 1]; //stack의 top을 지정 및 변경
-  if (top !== cryptogram[i]) { // stack내의 top에 위치한 내역과 
-    answer.push(cryptogram[i]);
-  }
-  else if (top == cryptogram[i]) {
-    answer.pop(cryptogram[i]);
+  for (var i = 1; i < cryptogram.length; i++) { // 배열의 인덱스를 변경하며 비교
+    var top = answer[answer.length - 1]; //stack의 top을 지정 및 변경
+    if (top !== cryptogram[i]) { // stack내의 top에 위치한 내역과 
+      answer.push(cryptogram[i]);
+    }
+    else if (top == cryptogram[i]) {
+      answer.pop(cryptogram[i]);
+    }
   }
   return answer;
 }
