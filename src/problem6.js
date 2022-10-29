0cc7a2625bace8aca57023c729ba963ddcbcb029
RooT_Kim <bdd3452@gmail.com>
2022-10-29 20:04:45 +0900

    문제6번) 기능목록 정리 및 함수 선언

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,14 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+	//닉네임 중복 체크를 하는 함수 -> checkDuplicate
+	//중복된 닉네임의 인덱스를 체크하는 함수 -> getIndex
+	//중복 확인된 닉네임의 인덱스 값을 이용해 결과를 리턴하는 함수 -> checkNickname
+	checkNickname(forms);
 }
+
+function checkNickname(forms) {}
+
+function getIndex() {}
+
+function checkDuplicate() {}
 
 module.exports = problem6;
