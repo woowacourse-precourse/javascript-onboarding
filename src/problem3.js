a83bec5040d951174d0ce82044593468eabfb0bc
su2333066 <su2333066@naver.com>
2022-10-29 17:27:15 +0900

    problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i < number + 1; i++) {
+    const arrStr = [...String(i)];
+    const arrInt = arrStr.map((i) => Number(i));
+
+    for (let j = 0; j < arrInt.length; j++) {
+      if (arrInt[j] === 3 || arrInt[j] === 6 || arrInt[j] === 9) {
+        answer += 1;
+      }
+    }
+  }
+
   return answer;
 }
 
