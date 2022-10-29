7e4832fd026a1211788efa69030035cacbcabbf0
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-29 21:13:54 +0900

    feat(problem1): 입력으로 주어진 배열의 유효성 검사 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,19 @@
+function isValidPages(pages) {
+  let leftPage = pages[0];
+  let rightPage = pages[1];
+
+  if (1 <= leftPage && leftPage <= 399 && leftPage + 1 == rightPage) {
+    return (true);
+  }
+  return (false);
+}
+
 function problem1(pobi, crong) {
   var answer;
+
+  if (!isValidPages(pobi) || !isValidPages(crong)) {
+    return -1;
+  }
   return answer;
 }
 
