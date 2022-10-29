79005c3903e35a0ab77c2b894544b7d2c302284d
GC-Park <kuyt3122@naver.com>
2022-10-29 13:31:51 +0900

    (문제5)Feat:지폐와 동전을 지갑이 최대한 가볍도록 수를 정해주고 반환해주는 함수

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,22 @@
+/*
+ 기능 목록
+  1) 오만 원권, 만 원권, 오천 원권, 천 원권, 오백원 동전, 백원 동전, 오십원 동전, 십원 동전, 일원 동전 각 몇 개로 변환되는지 구하는 기능
+*/
+
+function cashChange(c, m){
+  let cash, t=[];
+  for(let i=0; i<c.length; i++){
+    cash=parseInt(m/c[i]);
+    t[i]=cash;
+    m=m-c[i]*cash;
+  }
+  return t;
+}
+
 function problem5(money) {
   var answer;
+  let currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  answer = cashChange(currency, money);
   return answer;
 }
 
