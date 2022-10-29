03aac74f82e01b924334e9b0f5acdcf5924e403c
osdoonhyun <skw1996@naver.com>
2022-10-29 13:49:08 +0900

    [문제3] 기능 목록 수정, 기능1: 각 자리수를 문자로 나눠 배열에 저장하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,12 +1,19 @@
-/*
-1. number까지 for문을 돌며 각 자리수를 문자열로 나눠 배열에 저장하는 기능
-2. 배열을 ['3','6','9']에 해당하는지 체크하는 기능
-*/
+// 기능1
+function numToCharArr(num) {
+  const numToChar = String(num);
+  const charArr = Array.from(numToChar);
+
+  return charArr;
+}
 
 function problem3(number) {
-  var answer;
-
+  var answer = 0;
   return answer;
 }
 
 module.exports = problem3;
+
+/*
+1. 각 자리수를 문자로 나눠 배열에 저장하는 기능
+2. 배열을 ['3','6','9']에 해당하는지 체크하는 기능
+*/
