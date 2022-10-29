2cd4a40cf62c3fc446f49314ec59f7dfe80422a3
2yunseong <dbsdltjd3701@naver.com>
2022-10-29 12:56:44 +0900

    fix(문제 3번): 잘못된 박수 조건 수정 - 자료형이 일치하지 않던 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -6,15 +6,17 @@ function calcClapCount(n) {
   let count = 0;
   const digits = makeArrayBySeparateOrder(n);
   digits.forEach((digit) => {
-    if (digit % 3 === 0) count++;
+    if (digit == "3" || digit == "6" || digit == "9") count++;
   });
 
   return count;
 }
 
 function problem3(number) {
-  var answer;
-
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    answer += calcClapCount(i);
+  }
   return answer;
 }
 
