d57d71824cd2843146512827fd70e59a8811329f
kaori-killer <sksbthwjd@daum.net>
2022-10-29 18:16:54 +0900

    feat: 단위가 큰 순으로 돈을 나눈다

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const bankArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let moneyCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  bankArray.forEach((bankMoney, index) => {
+    moneyCount[index] = parseInt(money / bankArray[index]);
+    money = money % bankArray[index];
+  });
+  return moneyCount;
 }
 
+console.log(problem5(15000));
 module.exports = problem5;
