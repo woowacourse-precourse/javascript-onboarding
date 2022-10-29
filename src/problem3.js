203394ccc8bd348923a7a8afd458582349885a13
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-10-30 02:46:41 +0900

    미션 3 성공

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+
+  let answer = 0;
+
+  //정규표현식
+  let regex = /3|6|9/g;
+
+  for (let i = 1; i <= number; i++) {
+    answer += (String(i).match(regex) || []).length;
+  }
+
   return answer;
 }
 
