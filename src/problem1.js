cc3d9085fdc1f8af00d087a537630b1fe51e63ce
Azamman327 <wjsdbdud327@naver.com>
2022-10-29 14:59:03 +0900

    Feat: [P1]예외처리 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,16 +1,29 @@
 function problem1(pobi, crong) {
   var answer;
 
+  //예외case
+  if (isPageInvalid(pobi, crong)) return -1;
+
   // **수정사항 : sum, multiple값을 저장할 pobiRslt, crongRslt 배열 필요없음(삭제함), Math.max시 바로 값 대입
   //총 4개의 값 중 최댓값을 구하고 저장
   const pobiMax = Math.max(sumNum(pobi[0]), multipleNum(pobi[0]), sumNum(pobi[1]), multipleNum(pobi[1]))
   const crongMax = Math.max(sumNum(crong[0]), multipleNum(crong[0]), sumNum(crong[1]), multipleNum(crong[1]))
 
+  //pobiMax, crongMax 비교
   if (pobiMax > crongMax) answer = 1        //포비 승
   else if (crongMax > pobiMax) answer = 2   //크롱 승
   else if (pobiMax == crongMax) answer = 0  //무승부
 
   return answer;
+}
+
+//예외처리
+function isPageInvalid(pobi, crong) {
+  if (pobi[0] <= 1 || crong[0] <= 1) return true;                         //맨 첫 페이지 또는 0 이하의 값 입력시
+  if (pobi[1] >= 400 || crong[1] >= 400) return true;                     //맨 끝 페이지 또는 400 이상의 값 입력시
+  if (pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1) return true;    //펼친 페이지 값이 연속되지 않을 때, EX) 5, 9
+  
+  return false;
 }
 
 //자릿수의 합을 구하는 함수
