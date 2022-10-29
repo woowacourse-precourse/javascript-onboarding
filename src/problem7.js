734f435e9e0f8f5c4b142d42644f1e26a13bacd1
pp449 <mma7710@naver.com>
2022-10-30 02:18:13 +0900

    [문제 7] : 3. friendsScore변수를 만들어 이름별 점수를 0점으로 초기화

--- src/problem7.js
+++ src/problem7.js
@@ -1,21 +1,24 @@
-const inputFriendsList = (friendsList, friends) => {
+const inputFriendsList = (friendsList, friends, friendsScore) => {
   for(let i=0; i<friends.length; i++) {
     for(let j=0; j<2; j++) {
       let k = 0;
       if(j==0) 
         k = 1;
-      if(friendsList[friends[i][j]] === undefined)
+      if(friendsList[friends[i][j]] === undefined){
         friendsList[friends[i][j]] = [friends[i][k]];
+        friendsScore[friends[i][j]] = 0;
+      }
       else
         friendsList[friends[i][j]].push(friends[i][k]);
     }
   }
-  console.log(friendsList);
+  console.log(friendsScore);
 }
 
 function problem7(user, friends, visitors) {
   const friendsList = new Object();
-  inputFriendsList(friendsList, friends);
+  const friendsScore = new Object();
+  inputFriendsList(friendsList, friends, friendsScore);
 }
 
 problem7("mrko", [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ], ["bedi", "bedi", "donut", "bedi", "shakevan"]);
