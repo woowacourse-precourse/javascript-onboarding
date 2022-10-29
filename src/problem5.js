299f2371dede86c090e9716f173de22455a0eb8e
kyeongwxx <kyeongwxx@gmail.com>
2022-10-29 16:05:03 +0900

    problem5. 주석 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,9 @@
 function problem5(money) {
+  // 화폐 금액 배열 및 각 화폐의 수 배열 선언
   let moneyArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
+  // 주어진 매개변수에 따라 각 화폐의 수를 산정하는 for 문 작성
   for (let i = 0; i < answer.length; i++) {
     if (money) {
       answer[i] = parseInt(money / moneyArray[i]);
@@ -9,6 +11,7 @@ function problem5(money) {
     }
   }
 
+  // 결과값 산출
   return answer;
 }
 
