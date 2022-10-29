808b49c870b64fbe5ff1b23c6820e6723d135c6f
Gyu <tmdrb0912@naver.com>
2022-10-29 14:58:42 +0900

    문제 3번 풀이 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
   var answer;
-  return answer;
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    let nums = i.toString().split("");
+    for (num of nums) {
+      if (num == "3") {
+        count++;
+      } else if (num == "6") {
+        count++;
+      } else if (num == "9") {
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = problem3;
