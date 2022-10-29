a6c7c22665bc2522c255ac945aa64eee05327c14
imb96 <kimminje7267@naver.com>
2022-10-29 15:05:04 +0900

    [problem6] style: 주석 제거

--- src/problem6.js
+++ src/problem6.js
@@ -1,10 +1,3 @@
-/**
- * [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
- * ["jason@email.com", "jm@email.com", "mj@email.com"]
- * 닉네임의 글자중 같은글자가 연속적으로 포함될 경우
- * 닉네임의 이메일을 목록에 추가하여 반환
- * @param {*} forms
- */
 function problem6(forms) {
   const set = new Set();
   const map = new Map();
