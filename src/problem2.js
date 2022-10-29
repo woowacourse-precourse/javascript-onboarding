ccd65a09cf65b24686491d9bc4b3df9adf6134ea
NYeonK <skdus728@gmail.com>
2022-10-30 02:44:05 +0900

    [ feat ] 연속하는 중복 문자 삭제 기능 구현 #PROBLEM2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer=[];
+
+  cryptogram.split('').map(code => {
+    answer[answer.length - 1] === code ? answer.pop() : answer.push(code);
+  });
+
+  return `${answer.join('')}`;
 }
 
 module.exports = problem2;
