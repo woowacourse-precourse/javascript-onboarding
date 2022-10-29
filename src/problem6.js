bb5dae4dc13d4c0ae898154324a9bfa63d8d5434
dakyungoh <dakyung.work@gmail.com>
2022-10-30 04:13:11 +0900

    feat: problem6 set to arr 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -8,8 +8,7 @@
 function problem6(forms) {
   const substrings = getDuplicatedSubstring(forms)
   const emails = getEmailsWithStringsInNicknames(forms, substrings)
-  // TODO: emails의 중복 제거 및 오름차순 정렬
-  return emails
+  return getSortedArrayFromStringSet(emails)
 }
 
 /**
@@ -21,17 +20,16 @@ function getDuplicatedSubstring(forms) {
   const subTotal = new Set()
   const subDuplicated = new Set()
 
-  console.log('hello', forms)
-  for (let i = 0; i < forms.length - 1; i++) {
+  for (let i = 0; i < forms.length; i++) {
     const nickname = forms[i][1]
     const subUser = new Set()
 
     for (let j = 0; j < nickname.length - 1; j++) {
       const sub = nickname.slice(j, j + 2)
       if (!subUser.has(sub) && subTotal.has(sub)) {
-        subUser.add(sub)
         subDuplicated.add(sub)
       }
+      subUser.add(sub)
       subTotal.add(sub)
     }
   }
@@ -44,11 +42,11 @@ function getDuplicatedSubstring(forms) {
  * strings로 입력받은 문자열을 포함하는 닉네임을 가진 크루들의 이메일을 반환한다.
  * @param {string[][]} forms
  * @param {Set} strings 
- * @return {string[]} 
+ * @return {Set} 
  */
 function getEmailsWithStringsInNicknames(forms, strings) {
   const ret = new Set()
-  for (let i = 0; i < forms.length - 1; i++) {
+  for (let i = 0; i < forms.length; i++) {
     const [email, nickname] = forms[i]
     for (let j = 0; j < nickname.length - 1; j++) {
       const sub = nickname.slice(j, j + 2)
@@ -58,6 +56,16 @@ function getEmailsWithStringsInNicknames(forms, strings) {
     }
   }
   return ret
+}
+
+/**
+ * 스트링으로 구성된 Set을 입력받아 compareFucntion의 기준대로 정렬된 배열 형태로 반환한다.
+ * @param {Set} strings
+ * @param {Function} compareFunction 
+ * @returns {string[]}
+ */
+function getSortedArrayFromStringSet(strings, compareFunction) {
+  return ([...strings]).sort(compareFunction)
 }
 
 module.exports = problem6;
