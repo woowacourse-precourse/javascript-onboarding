8f273db933cb8ede2770a839076fe36f714e6bc9
mauserne <mauzerne@gmail.com>
2022-10-29 22:52:59 +0900

    [문제2] 통과

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = cryptogram;
+  for (var i = 0; i < answer.length; i++) {
+    if (answer[i] == answer[i + 1]) {
+      answer = answer.replace(answer[i] + answer[i], "");
+      i = -1;
+      continue;
+    }
+  }
   return answer;
 }
 
