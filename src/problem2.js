cd5a747733298bda0a45ad7d4e79352fd08c41be
banma1234 <banma1234@gmail.com>
2022-10-29 20:29:22 +0900

    problem_2 : 예외처리 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
+  if (errorHandler(cryptogram)) {
+    console.log("Error : 글자수 제한");
+    return null;
+  }
+  
   var answer;
   return answer;
+}
+
+function errorHandler(cryptogram) {
+  if (cryptogram.length > 1000) return true;
 }
 
 module.exports = problem2;
