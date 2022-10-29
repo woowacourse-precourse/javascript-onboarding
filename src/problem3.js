0237e7a016e7dac02d93dc349a0cb703469a5602
chex <chex417@naver.com>
2022-10-29 17:30:04 +0900

    feat: 현재 숫자에서 손뼉을 몇 번 쳐야 하는지 구하는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,16 @@ function problem3(number) {
   return answer;
 }
 
+function calculateClapHands(cur_num){
+  let cnt = 0 // 3,6,9 개수
+  while (cur_num!=0) {
+    let tmp = cur_num%10;
+    if (tmp == 3 || tmp == 6 || tmp == 9) {
+      cnt += 1;
+    }
+    cur_num = parseInt(cur_num/10);
+  }
+  return cnt;
+}
+
 module.exports = problem3;
