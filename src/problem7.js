ebff0f1a8cd49ba8ae1eb9119cde1be65bfcfe37
TaeeunKim <xodms0309@naver.com>
2022-10-29 21:41:44 +0900

    [7번] feat: 친구 관계를 연결한 friendList 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,15 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  const friendList = {};
+
+  for (const [a, b] of friends) {
+    if (friendList[a]) friendList[a].push(b);
+    else friendList[a] = [b];
+    if (friendList[b]) friendList[b].push(a);
+    else friendList[b] = [a];
+  }
+
   return answer;
 }
 
