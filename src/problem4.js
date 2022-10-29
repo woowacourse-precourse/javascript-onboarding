da2ddf6e306d82ae0b317ddd732c72e85d2542c0
GC-Park <kuyt3122@naver.com>
2022-10-29 12:20:23 +0900

    (4번)Fix:아스키 코드를 쓰기 위해 특정 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -5,42 +5,50 @@
   3) 소문자와 대문자만 변환하도록 예외 처리
 
 */
-function wordChange(c){
-  let mn;
-  if(c===c.toUpperCase()){
-    mn= c-'A';
-    mn=25-mn;
-    mn=mn+'A';
-  }else{
-    mn= c-'a';
-    mn=25-mn
-    mn=mn+'a';
-  }
-  return mn;
-}
-
-function exceptHandling(c){
-  if(c>='A'&&c<='Z') return 1;
-  else if(c>='a'&&c<='z') return 1;
-  else return 0;
-}
-
-function getStr(w){
-  let str=''
-  for(let c of w){
-    if(exceptHandling(c)){
-      str+=wordChange(c);
-    }else{
-      str+=c;
+function wordChange(c) {
+    let mn
+    if (c === c.toUpperCase()) {
+        mn = c.charCodeAt() - 'A'.charCodeAt()
+        mn = 25 - mn
+        mn = mn + 'A'.charCodeAt()
+    } else {
+        mn = c.charCodeAt() - 'a'.charCodeAt()
+        mn = 25 - mn
+        mn = mn + 'a'.charCodeAt()
     }
-  }
-  return str;
+    return String.fromCharCode(mn)
+}
+
+function exceptHandling(c) {
+    if (
+        c.charCodeAt() >= 'A'.charCodeAt() &&
+        c.charCodeAt() <= 'Z'.charCodeAt()
+    )
+        return 1
+    else if (
+        c.charCodeAt() >= 'a'.charCodeAt() &&
+        c.charCodeAt() <= 'z'.charCodeAt()
+    )
+        return 1
+    else return 0
+}
+
+function getStr(w) {
+    let str = ''
+    for (let c of w) {
+        if (exceptHandling(c)) {
+            str += wordChange(c)
+        } else {
+            str += c
+        }
+    }
+    return str
 }
 
 function problem4(word) {
-  var answer;
-  answer = getStr(word)
-  return answer;
+    var answer
+    answer = getStr(word)
+    return answer
 }
 
-module.exports = problem4;
+module.exports = problem4
