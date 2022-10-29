72e89033decfc8ef6ec9d3337aabb7c3091b9fd0
inthhh <glass005533@gmail.com>
2022-10-30 01:40:56 +0900

    p6_정리

--- src/problem6.js
+++ src/problem6.js
@@ -8,7 +8,6 @@ function problem6(forms) {
       let word = forms[i][1].substr(j,2);
       
       if(word.length<2) break;
-      console.log(word);
       if(forms[j][1].indexOf(word) >= 0) {
         duplMails[i] = 1;
         duplMails[j] = 1;
