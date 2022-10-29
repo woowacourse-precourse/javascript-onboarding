3e5e4d41e22788bd1268a3ff6e3cbf1c518d02f3
chee9835 <huisu121@gmail.com>
2022-10-29 10:44:21 +0900

    feat: Map 이용해 같은 글자가 연속적으로 포함된 크루들 찾기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var hashMap = new Map();
+  for (var form of forms) {
+    for (var i = 0; i < form[1].length - 1; i++) {
+      var nickname = form[1].slice(i, i + 2);
+      if (hashMap.has(nickname)) {
+        var emails = hashMap.get(nickname);
+        hashMap.set(nickname, [...emails, form[0]]);
+      } else hashMap.set(nickname, [form[0]]);
+    }
+  }
+
+  var answer = "";
+
+  hashMap.forEach((v) => {
+    if (answer.length < v.length) answer = v;
+  });
+
+  return answer.sort();
 }
 
 module.exports = problem6;
