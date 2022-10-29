bc5cf2ec8f2155d8f307d15f35036c828a17ce8c
Hoon <si932174@gmail.com>
2022-10-29 22:38:26 +0900

    problem7 : user와 친구인 사람 목록 뽑기

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,23 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  const userFriendsListGetter = (user, friends) => {
+
+    const userFriendsList = [];
+
+    friends.map(friend => {
+      if(friend.indexOf(user) !== -1) {
+        userFriendsList.push(friend[1-friend.indexOf(user)]);
+      }
+    })
+
+    console.log(userFriendsList);
+
+    return userFriendsList;
+  }
+
+  userFriendsListGetter(user, friends);
+
   return answer;
 }
 
