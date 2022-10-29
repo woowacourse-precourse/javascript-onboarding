268a7b745a417d5d7753c0f0c635ab3c0d95835d
kaori-killer <sksbthwjd@daum.net>
2022-10-29 16:06:44 +0900

    현재 문자열을 기준으로 오른쪽 문자열이 같은면 check 배열을 1로 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,10 +1,28 @@
-function problem2(cryptogram) {
+function deleteString(cryptogram){
   stringLength = cryptogram.length;
   let check = new Array(stringLength).fill(0);
-  
-  return answer;
+  let newCryptogram = '';
+  let count = 0;
+
+  for (let i = 0; i < stringLength; i++){
+    
+    if((cryptogram[i] == cryptogram[i + 1]) && (i != stringLength - 1)){
+      count += 1;
+      check[i] = 1;
+      check[i+1] = 1;
+    }
+    else{
+      newCryptogram += cryptogram[i]
+    }
+  }
+
+  return newCryptogram;
 }
 
-problem2("browoanoommnaon");
+function problem2(cryptogram) {
+    return deleteString(cryptogram);
+}
+
+console.log(problem2("browoanoommnaon"));
 
 module.exports = problem2;
