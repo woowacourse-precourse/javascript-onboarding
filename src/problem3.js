fb8e974fa9e1a05e6d7895a243f92ee3d263e64c
jsh3418 <albert5428@gmail.com>
2022-10-29 10:41:44 +0900

    feat: problem3 문자열에 3,6,9의 개수를 구하여 리턴 구현

--- src/problem3.js
+++ src/problem3.js
@@ -9,6 +9,14 @@ function problem3(number) {
   }
 
   // 문자열에 3,6,9의 개수를 구한다.
+  let count = 0;
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] === "3" || str[i] === "6" || str[i] === "9") {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = problem3;
