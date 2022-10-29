119605641cc23303228fd3266af4e3fd00e6b260
semnil5202 <semnil5202@gmail.com>
2022-10-30 05:30:34 +0900

    [문제 5번] feat: 돈의 액수에 대해 필요한 각 지폐 장수 구하고 배열에 담기

--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,14 @@ function problem5(money) {
   return answer;
 }
 
+function withdraw(money) {
+  const billsType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const billsNumber = [];
+  for (let i = 0; i < billsType.length; i++) {
+    billsNumber.push(parseInt(money / billsType[i], 10));
+    money %= billsType[i];
+  }
+  return billsNumber;
+}
+
 module.exports = problem5;
