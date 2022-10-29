6fe0619fca203a0c6b2c8920f0362adb527f9ea9
mochang2 <qjsrodksro@gmail.com>
2022-10-29 22:27:13 +0900

    [PROBLEM3] 기능 요구 사항 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,40 @@
-function problem3(number) {
-  var answer;
-  return answer;
+/**
+ * 올바른 입력인지 확인한다.
+ * @param {number} number
+ * @returns {boolean}
+ */
+function isInvalidInputs(number) {
+  if (!number) {
+    return true
+  }
+
+  if (typeof number !== 'number') {
+    return true
+  }
+
+  if (number < 1 || number > 10000) {
+    return true
+  }
+
+  return false
 }
 
-module.exports = problem3;
+/**
+ * 369 게임에서 박수치는 횟수를 반환한다.
+ * @param {number} number
+ * @returns {number | string}
+ */
+function problem3(number) {
+  if (isInvalidInputs(number)) {
+    return '입력값이 잘못되었습니다.'
+  }
+
+  const numbers = Array.from({ length: number }, (_, index) =>
+    (index + 1).toString()
+  ).join('')
+  const clapCount = numbers.match(/[369]/g)?.length || 0
+
+  return clapCount
+}
+
+module.exports = problem3
