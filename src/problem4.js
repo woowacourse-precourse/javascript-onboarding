3732098ef6ebfab4c25af715cb7c5a0db227209d
Hyunoh Jo <hyunoh.jo@gmail.com>
2022-10-29 12:01:53 +0900

    [problem4] Add treeFrogApp function for total process management

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  return treeFrogApp(word);
+}
+
+function treeFrogApp(word){
+  const eachCharArr = createStringArr(word);
+  for(let i = 0; i < eachCharArr.length; i++){
+    eachCharArr[i] != " " && (eachCharArr[i] = getTreeFrogChar(getUTF16Code(eachCharArr[i])));
+  }
+  return getFromCharsToString(eachCharArr);
 }
 
 module.exports = problem4;
