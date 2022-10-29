383c447ef04a2920f41f2fc40784dd4c43b65e8d
loco9939 <kls9939@naver.com>
2022-10-29 16:09:44 +0900

    feat: Create func
    - 연속된 2글자 이상의 문자열을 반환하는 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,34 @@
+const getPartialStr = (str) => {
+  const res = [];
+  let limit = 2;
+  while (limit <= str.length) {
+    const output = [];
+    let index = 0;
+    while (true) {
+      const seperated = [...str].slice(index, index + limit).join("");
+      index += 1;
+      if (limit > seperated.length) break;
+      output.push(seperated);
+    }
+    limit += 1;
+    res.push(...output);
+  }
+
+  return res;
+};
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  const nicknames = forms.map((nickname) => nickname[1]);
+  const partialStrs = nicknames.map((nickname) => getPartialStr(nickname));
 }
+
+const input = [
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+];
+problem6(input);
 
 module.exports = problem6;
