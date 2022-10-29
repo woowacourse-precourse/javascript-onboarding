20ed84e2e7c8b2ff154ca1e8ee66fd852fae632f
seonjae choi <tjswo2292@naver.com>
2022-10-29 14:56:33 +0900

    완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,25 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  let arr = [];
+
+  for (let i = 1; i <= number; i++) {
+    if (i < 10) {
+      if (i === 3 || i === 6 || i === 9) {
+        count += 1;
+      }
+    } else {
+      arr = i.toString().split("");
+      for (let j = 0; j < arr.length; j++) {
+        if (arr[j] === "3" || arr[j] === "6" || arr[j] === "9") {
+          count += 1;
+        }
+      }
+    }
+  }
+
+  return count;
 }
+
+console.log(problem3(33));
 
 module.exports = problem3;
