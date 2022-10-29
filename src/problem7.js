f602e205ef3114b20abb0c762c5f3b364cae2671
dakyungoh <dakyung.work@gmail.com>
2022-10-30 04:30:15 +0900

    feat: problem7 유저와 직접 친구인 사용자 목록 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,4 +1,6 @@
 function problem7(user, friends, visitors) {
+  const friendsOfUser = getFriendsOfUser(user, friends)
+  console.log(friendsOfUser)
   // 함께 아는 친구 수 구하기
   // 함께 아는 친구 수에 따른 점수 더하기
   // 방문 횟수에 따른 점수 더하기
@@ -7,5 +9,30 @@ function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
+
+/**
+ * user와 친구인 사용자 목록을 반환한다.
+ * @param {string} user 
+ * @param {string} friends 
+ * @returns {Set<string>}
+ */
+function getFriendsOfUser(user, friends) {
+  const firstDegree = new Set()
+  for (let i = 0; i < friends.length; i++) {
+    const [from, to] = friends[i]
+    if (from === user) {
+      firstDegree.add(to)
+    }
+    if (to === user) {
+      firstDegree.add(from)
+    }
+  }
+  return firstDegree
+}
+
+console.log(getFriendsOfUser(
+  "mrko",
+  [["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]]
+))
 
 module.exports = problem7;
