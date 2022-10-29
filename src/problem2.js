307a5d49cba42679eadd2a67ef6793b44bd2b2af
yeeun426 <thsudkcla7@naver.com>
2022-10-30 02:15:26 +0900

    문제 2번 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+
+    var cry_arr = cryptogram.split("");
+
+    var i = 0;
+    while(i != cry_arr.length) {
+      if(cry_arr[i] == cry_arr[i+1]) {
+        cry_arr.splice(i,2);
+        console.log(cry_arr)
+        i = 0;
+      } else {
+        i += 1;
+      }
+    }      
+    return cry_arr.join("");
 }
 
 module.exports = problem2;
