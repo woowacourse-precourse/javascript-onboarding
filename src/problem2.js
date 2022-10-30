f928d6d8efe05f13ff07298e1f33dadd45c3b4c9
nalsae <nalsae0207@naver.com>
2022-10-30 22:16:28 +0900

    feat: Add a logic for early return
    - 전달 받은 문자열에 중복된 부분이 없는 경우를 위한 조건문 로직 추가

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,9 @@ const findDuplicate = (string) => string.match(/([a-z])\1{1,}/g);
 
 const problem2 = (cryptogram) => {
   let answer;
+  const cipherText = cryptogram;
+
+  if (!findDuplicate(cipherText)) return cipherText;
 
   return answer;
 };
