c04a18ad4233484fd99dfed4b30e0929987181d7
하지훈 <jnh03336@gmail.com>
2022-10-30 15:51:26 +0900

    feat: pro6, logic design

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,29 @@
+let subnamesWithMails = {};
+
+const saveMailForEverySubname = (mail, name) => {
+  // do something
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  subnamesWithMails = {};
+
+  for(let i = 0; i < forms.length; i++){
+    const [mail, name] = forms[i];
+    saveMailForEverySubname(mail, name);
+  }
+
+  const mailSet = new Set();
+
+  for(const subname in subnamesWithMails){
+    if(subnamesWithMails[subname].length < 2) continue;
+    subnamesWithMails[subname].forEach((mail) => {
+      mailSet.add(mail);
+    })
+  }
+  const result = [...mailSet];
+  result.sort();
+  
+  return result;
 }
 
 module.exports = problem6;
