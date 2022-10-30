7006f9667698a4768763f31d954187943b21376d
Ryan-Dia <cjfdnjs158@naver.com>
2022-10-31 01:39:57 +0900

    feat: [문제3] 3,6,9을 포함하면 추출하는 기능추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,8 @@
-function problem3(word) {
-  var answer;
+function problem3(number) {
+  let numberOf369 = "";
+  for (i = 0; i <= number; i++) {
+    let matchNumberOf369 = i.toString().match(/[369]+/g);
+  }
   return answer;
 }
 
