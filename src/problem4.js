d1af8c66472f7ac95bda9e3718853fb5c1b32622
sery <yunsel2@gmail.com>
2022-10-30 16:46:58 +0900

    refactor(Problem5): Seperate exeption check function

--- src/problem4.js
+++ src/problem4.js
@@ -4,17 +4,9 @@ const reversedAlphabet = 'zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA';
 function problem4(word) {
   let answer;
   const reversedWordArray = [];
-  const validWord = /^[a-zA-Z\s]*$/;
 
-  if (word.length < 1 || word.length > 1000) {
-    console.error('길이가 1 이상 1000 이하인 문자열을 입력해주세요');
-    return;
-  }
-
-  if (!validWord.test(word)) {
-    console.error('알파벳을 입력해주세요');
-    return;
-  }
+  if (!isAlphabet(word)) return;
+  if (!isValidLength(word, 1, 1000)) return;
 
   const wordArray = word.split(' ');
 
@@ -31,6 +23,26 @@ function problem4(word) {
   });
 
   return answer;
+}
+
+function isValidLength(word, min, max) {
+  if (word.length < min || word.length > max) {
+    console.error(
+      `길이가 ${min} 이상 ${max.toLocaleString()} 이하인 문자열을 입력해주세요`
+    );
+    return false;
+  }
+  return true;
+}
+
+function isAlphabet(word) {
+  const validWord = /^[a-zA-Z\s]*$/;
+
+  if (!validWord.test(word)) {
+    console.error('알파벳을 입력해주세요');
+    return false;
+  }
+  return true;
 }
 
 module.exports = problem4;
