6d7d01d55d684879b4cbbd002288ca707de78076
Hungeun2 <bepoohun@gmail.com>
2022-10-30 18:24:00 +0900

    Feat: problem5 Add moneyUnit List

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,9 @@
 // 2. 지폐 배열을 순회하며 큰 지폐부터 갯수 추가
 // 3. 원래 금액에서 뺀 지폐 만큼 차감 & 금액 최신화
 
+//지폐 배열 생성
+const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
   var answer;
   return answer;
