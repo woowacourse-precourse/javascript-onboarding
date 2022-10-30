fd701205bac8094e3698ab8667f7ad1cd34e7cc4
Jiho1996 <jhp9270@naver.com>
2022-10-30 17:05:05 +0900

    feat : money를 나누기 및 정답 배열 저장 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
   const kindOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = Array(kindOfMoney.length).fill(0);
+
+  kindOfMoney.forEach((cur, idx) => {
+    answer[idx] = parseInt(money / cur);
+    money %= cur;
+  });
+
   return answer;
 }
 
