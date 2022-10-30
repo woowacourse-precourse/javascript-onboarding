1672af6e23deaee1fb551ce0121f8deaada731b8
한수지 <hansuji@hansujiui-MacBookAir.local>
2022-10-30 16:50:57 +0900

    problem4 리팩토링 if문 수정을 통한  의미개선

--- src/problem4.js
+++ src/problem4.js
@@ -1,22 +1,21 @@
 function problem4(word) {
   var answer = solution(word);
   return answer;
-};
+}
 function solution(word) {
- let askiiNum = [], askiiStr = [];
- for(let i = 0; i < word.length; i++) {
-  askiiNum.push(word.charCodeAt(i));
- };
- for(let i = 0; i < askiiNum.length; i++) {
-  if(askiiNum[i] === 32) {
-    askiiStr.push(String.fromCharCode(32));
+  let askiiNum = [], askiiStr = [];
+  for (let i = 0; i < word.length; i++) {
+    askiiNum.push(word.charCodeAt(i));
   }
-  if(askiiNum[i] <= 90 && askiiNum[i] !== 32) {
-    askiiStr.push(String.fromCharCode(90 - (askiiNum[i] - 65)));
-  } else if(askiiNum[i] <= 122 && askiiNum[i] !== 32) {
-    askiiStr.push(String.fromCharCode(122 - (askiiNum[i] - 97)));
+  for (let i = 0; i < askiiNum.length; i++) {
+    if (askiiNum[i] >= 65 && askiiNum[i] <= 90) {
+      askiiStr.push(String.fromCharCode(155 - askiiNum[i]));
+    } else if (askiiNum[i] >= 97 && askiiNum[i] <= 122) {
+      askiiStr.push(String.fromCharCode(219 - askiiNum[i]));
+    } else {
+      askiiStr.push(String.fromCharCode(askiiNum[i]));
+    }
   }
- };
- return askiiStr.join("")
+  return askiiStr.join("");
 }
 module.exports = problem4;
