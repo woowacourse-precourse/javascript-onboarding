3009e9e12f7c6079076fce0a1e7507ed25e8deed
hae-on <solbi2004@naver.com>
2022-10-31 02:23:12 +0900

    [문제6] 연속 중복 문자 찾기 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,23 @@
+const partOfWord = [];
+const overLappingWord = [];
+
+const findOverLappingWord = (nickname) => {
+  for (let i = 0; i < nickname.length - 1; i++) {
+    const tmp = nickname.slice(i, i + 2);
+    if (!partOfWord.includes(tmp)) partOfWord.push(tmp);
+    else if (!overLappingWord.includes(tmp)) overLappingWord.push(tmp);
+  }
+  return overLappingWord;
+};
+
 function problem6(forms) {
-  var answer;
+  const answer = [];
+
+  forms.map((form) => {
+    const [, nickname] = form;
+    findOverLappingWord(nickname);
+  });
+
   return answer;
 }
 
