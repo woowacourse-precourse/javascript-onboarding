2d0d48a8bec4d72daff4caacbbc0347f0586f7de
pangkyu <baesense@naver.com>
2022-10-31 00:03:09 +0900

    feat : 스플릿하여 닉네임에만 따로 접근가능한 데이터를 만드는 기능
    오브젝트를 스트링으로 변환하여 잘라 저장했음
    -> i + 2 하면 닉네임 단위로 접근가능할 것 같다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
-  return answer;
-}
+  let user = [];
 
+  user = String(forms).split(",");
+
+  console.log(user);
+}
+function testCode() {
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ]);
+}
+testCode();
 module.exports = problem6;
