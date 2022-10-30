42c6f925b9e3642784f69a6380a805e0bcfaa060
Yunjae <nuguri206@gmail.com>
2022-10-30 15:31:54 +0900

    feat: [prob5] 돈 범위 설정

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  //돈 범위
+  const moneyRange = (money) => {
+    if(money < 1 || money > 1000000 ) return false;
+    else return true;
+  };
 }
 
 module.exports = problem5;
