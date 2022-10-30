fcc8ac8c106a338cfb187e92587444619926961b
foundaway98 <foundaway98@naver.com>
2022-10-30 10:37:16 +0900

    문제 7번 기능 목록

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,18 @@
+//friends에 있는 모든 사람을 대상으로 하는 FriendObj를 생성한다.
+function makeFriendsObj() {}
+//추천 점수를 저장하는 pointObj를 생성한다.
+function makePointObj() {}
+//user와 같은 친구를 가진 사람들의 점수를 계산한다.
+function addSameFriendPoint() {}
+//user의 timeline에 방문한 사람들의 점수를 계산한다.
+function addVisitorPoint() {}
+//점수에 따라 정렬하고, 추천 점수가 같은 경우 이름 순으로 정렬한다.
+function sortWithPoint() {}
+//정답 배열을 생성한다.
+function makeAnswerArr() {}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
   return answer;
 }
 
