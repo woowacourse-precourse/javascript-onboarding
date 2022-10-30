76f23509e399d892a32d2975c9062db8236d7e52
Minyoung Lee <mminy62@gmail.com>
2022-10-30 15:41:28 +0900

    feat: [PROBLEM5] 단위가 큰 순으로 배열 만들어 몫 반환

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+
+  //1. 단위가 큰 순으로 배열 만들어 몫 반환
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for(let i = 0; i < unit.length; i++){
+    quo = Math.floor(money/unit[i]);
+  }
+ 
+  
   return answer;
 }
 
