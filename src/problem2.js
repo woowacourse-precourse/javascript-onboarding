068e31eb6b212e0e8f2aada23158f07a28cb721f
useon <yoosk4986@naver.com>
2022-10-30 18:08:06 +0900

    feat: [problem2] 연속 중복 문자의 시작과 끝 index를 구하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let cryptogramArr = cryptogram.split('');
+  let sameStringIndex = 0;
+
+  // 1. 연속된 중복 문자의 시작 index와 끝 index를 구하는 기능
+  for (let i = 0; i < cryptogramArr.length; i++) {
+    if (cryptogramArr[i] === cryptogramArr[i + 1]) {
+      sameStringIndex = i + 1;
+      while (true) {
+        if (cryptogramArr[i] === cryptogramArr[sameStringIndex]) break;
+        sameStringIndex++;
+      }
+    }
+  }
+
+  return
 }
 
 module.exports = problem2;
