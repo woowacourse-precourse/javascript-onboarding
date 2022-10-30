50342b6cc2ad88621637715f338db2b66c897784
moseung <tmdah900@naver.com>
2022-10-30 18:32:11 +0900

    ✨ 기능목록 2,3 구현 기능목록 2,3: 주어진 아스키코드값으로 알파벳인지 체크후 아니라면 그대로 추가 해당값이라면 변환후 추가, 모든값을 추가후 return

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,20 @@
+function checkAlphabet(ascii) {
+  if (ascii >= 65 && ascii <= 90) return true;
+  else if (ascii >= 97 && ascii <= 122) return true;
+  else return false;
+}
+
+function convertFrog(ascii) {
+  let convertedAscii;
+  if (ascii >= 65 && ascii <= 90) {
+    convertedAscii = 90 - (ascii - 65);
+  } else if (ascii >= 97 && ascii <= 122) {
+    convertedAscii = 122 - (ascii - 97);
+  }
+  console.log(convertedAscii);
+  return convertedAscii;
+}
+
 function problem4(word) {
   let answer = "";
   for (let x of word) {
