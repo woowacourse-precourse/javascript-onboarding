492f238c7cc91af0eb9b46101c121a0e3baf569d
금가경 <gageum0@gmail.com>
2022-10-31 00:24:08 +0900

    feat : [problem7] 사용자의 친구 찾기 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let friendsOfUser = [];
+
+  friends.map(friend => {
+    for(let i = 0; i < friend.length; i++){
+      if(friend[i] === user){
+        i === 0? friendsOfUser.push(friend[1]) : friendsOfUser.push(friend[0]);
+      }
+    }
+  })
 }
 
 module.exports = problem7;
