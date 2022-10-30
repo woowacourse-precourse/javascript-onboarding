efe13b5b8e6b364be15b30304b9a2d951000aa25
thewronghand <penfreak77@gmail.com>
2022-10-31 03:21:37 +0900

    feat: 연속 문자열 케이스 계산 반복문 생성

--- src/problem6.js
+++ src/problem6.js
@@ -15,14 +15,21 @@ for (const item of nicknameArray){
   dismantled.push(components);
 }
 
+const serialString = []; 
+for (const components of dismantled){
+  for(i = 0; i < components.length-1; i++){
+    serialString.push(components[i]+components[i+1]);
+    }
+}
+
 module.exports = problem6;
 
 //problem6 기능 목록
 //[x] 유저들의 닉네임, 사용 제한 유저, 사용 제한 유저 이메일을 저장하기 위한 배열을 생성한다.
 //[x] forms에서 유저들의 닉네임만을 추출하여 nicknameArray에 저장한다.
-//[]모든 닉네임에 대하여 2글자의 연속 문자열이 발생할 수 있는 모든 경우의 수를 계산한다.
+//[x]모든 닉네임에 대하여 2글자의 연속 문자열이 발생할 수 있는 모든 경우의 수를 계산한다.
 //  [x] 모든 닉네임을 문자 단위로 분해한다.
-//  [] 발생할 수 있는 2글자의 연속 문자열들을 모두 계산해 배열에 저장한다.
+//  [x] 발생할 수 있는 2글자의 연속 문자열들을 모두 계산해 배열에 저장한다.
 //[]계산된 경우의 수를 토대로 하여 현재 유저들 사이에서 발생할 수 있는 중복값을 구한다.
 //  [] 배열에서 중복된 값을 구하는 함수를 선언한다.
 //    [] 연속문자열 배열에서 중복값을 구하고, 중복되는 중복값은 삭제한다.
