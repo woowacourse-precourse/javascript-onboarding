da6b772b552b329c34e08da4ac9111200dbb9f83
Ryong-E <think8867@gmail.com>
2022-10-30 14:21:46 +0900

    fix: PROBLEM6 전개연산자 미적용으로 중복 제거 안된 점 수정

--- src/problem6.js
+++ src/problem6.js
@@ -14,7 +14,7 @@ function nicknameValidator(users, duplicateWords) {
       user => user[1].includes(duplicateWord) && duplicated.push(user[0])
     );
     if (duplicated.length >= 2) {
-      duplicateNicknameUsers.push(duplicated);
+      duplicateNicknameUsers.push(...duplicated);
     }
   }
 
