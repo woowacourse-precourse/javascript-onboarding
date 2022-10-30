8da3e40d12699314a2cf8213cfa1fc9e276a293e
kimyou1102 <yoosk5485@naver.com>
2022-10-30 23:45:57 +0900

    feat: 문제 5번 - 큰 금액의 화폐 위주로 돈을 반환한다.

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,13 @@ function problem5(money) {
   const unit_arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let answer = new Array(unit_arr.length).fill(0);
 
+  for (let i = 0; i < unit_arr.length; i++) {
+    if (money / unit_arr[i]) {
+      answer[i] = Math.floor(money / unit_arr[i]);
+      money = money % unit_arr[i];
+    }
+  }
+
   return answer;
 }
 
