f40d3921563b5de84c08d70f5087bbb8efe6a1b2
devMingu <rooster100@naver.com>
2022-10-30 15:33:05 +0900

    feat: count369함수는 원소의 3,6,9의 개수를 찾아 반환해줍니다.

--- src/problem2.js
+++ src/problem2.js
@@ -4,33 +4,33 @@ const deleteOverride = (left, cryptogram) => {
   var cnt = 1;
   var tmp = left + 1;
 
-  while (tmp < cryptogram.length){
-    if (cryptogram[left] === cryptogram[tmp]){
+  while (tmp < cryptogram.length) {
+    if (cryptogram[left] === cryptogram[tmp]) {
       cnt += 1;
       tmp += 1;
-    }
-    else{
-      break
+    } else {
+      break;
     }
   }
   cryptogram.splice(left, cnt);
 
   return cryptogram;
-
-}
+};
 
 function problem2(cryptogram) {
   var answer = "";
-  cryptogram = stringToArray(cryptogram);
+  var left = 0;
+  var right = 1;
+  var check = 0; //chekc 변수를 사용하는 이유는 문자열에 중복이 없을때를 확인해주기 위함힙니다.
 
+  cryptogram = stringToArray(cryptogram);
 
   // 길이가 1일때는 중복이 없기때문에 바로 반환시켜줍니다.
   if (cryptogram.length == 1) return cryptogram[0];
 
-
-  while (true){
+  while (true) {
     // 중복이 2개 이상인 경우도 고려해줍니다.
-    if (cryptogram[left] === cryptogram[right]){
+    if (cryptogram[left] === cryptogram[right]) {
       cryptogram = deleteOverride(left, cryptogram);
       check = 1;
       // 중복을 제거하고 제거된 위치
@@ -41,20 +41,22 @@ function problem2(cryptogram) {
     left += 1;
     right += 1;
 
-    if ((check == 0 && right >= cryptogram.length) || cryptogram.length == 0){
+    if ((check == 0 && right >= cryptogram.length) || cryptogram.length == 0) {
       break;
     }
-    if (right >= cryptogram.length){
+    if (right >= cryptogram.length) {
       left = 0;
       right = 1;
       check = 0;
     }
   }
 
+  //남은 cryptogram에서 각 문자들을 뽑아내어 답을 만들어냅니다.
+  for (var el of cryptogram) {
+    answer += el;
+  }
+
   return answer;
 }
 
-console.log(problem2("a"));
 module.exports = problem2;
-
-
--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,12 @@
+const count369 = (arr) => {
+  var cnt = 0;
+  arr.forEach(element => {
+    if (element === '3' || element === '6' || element === '9') cnt += 1;
+  });
+
+  return cnt;
+}
+
 function problem3(number) {
   var answer;
 
