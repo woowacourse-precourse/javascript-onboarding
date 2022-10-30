c20511850a38443bc35fe04ffab9187e91675a47
R-jisu <79021544+R-jisu@users.noreply.github.com>
2022-10-30 23:05:07 +0900

    [problem2]passed

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,18 @@
 function problem2(cryptogram) {
+  let stackcryptogram = []
+  for(let i=0; i<cryptogram.length;i++){
+    stackcryptogram.push(cryptogram[i]);
+  }
+
+  for(i=0; i<stackcryptogram.length;i++){
+    if (stackcryptogram[i] === stackcryptogram[i+1]) {
+      stackcryptogram.splice(i,2);
+      i=-1;
+    }
+  }
+
   var answer;
+  answer = stackcryptogram.join('');
   return answer;
 }
 
