c61b0afcdd892ceea49af4537e2406b9067d9176
y00eunji <aine0227@naver.com>
2022-10-30 23:32:09 +0900

    feat: [problem1] check pages is not first page or last page

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,15 @@
+const isInvalidPageRange = (left, right) => {
+  return left < 2 || right < 2 || left > 399 || right > 399;
+};
+
+const isValidPage = (page) => {
+  const [left, right] = page;
+
+  if (isInvalidPageRange(left, right)) return false;
+
+  return true;
+};
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
