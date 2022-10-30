40b8324db693000bdba758506067235383231237
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-31 01:03:27 +0900

    feat: 3번- 숫자의 각 자리를 순회하며 3,6,9가 있을 경우 count하는 메서드 구현

--- src/problem3.js
+++ src/problem3.js
@@ -2,7 +2,9 @@ function problem3(number) {
   let answer = 0;
   //number까지의 숫자를 반환하는 메서드
   for(let i=1; i < number+1; i++) {
-    const eachNum = i.toString().split("").map(x => parseInt(x))
+    const eachNum = i.toString().split("").map(x => parseInt(x));
+    const count = eachNum.filter((element) => 3 === element || 6 === element || 9 === element).length;
+    answer += count;
   }
   return answer;
 }
