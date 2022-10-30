4f2b55495cbff5bbd1b24ae604736240f69f69d3
최희원 <jeeen1@naver.com>
2022-10-30 15:54:41 +0900

    feat(problem6): add check similar function

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,24 @@
+/**
+ * check if the two given strings have same consequtive characters.
+ * @param {string} name1 first string to check
+ * @param {string} name2 second string to check
+ * @returns {boolean} whether the two given strings have same consequtive characters or not
+ */
+function isSimilar(name1, name2) {
+  const name1Length = name1.length;
+  const name2Length = name2.length;
+
+  for (let i = 0; i < name1Length - 1; i++) {
+    for (let j = 0; j < name2Length - 1; j++) {
+      if (name1[i] === name2[j] && name1[i + 1] === name2[j + 1]) {
+        return true;
+      }
+    }
+  }
+
+  return false;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
