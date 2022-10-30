3f7833ffefb49b8eb3c9fa4ef50ad5ff6f230aca
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 00:00:38 +0900

    정규식 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
+/*
+### 기능 목록
+1. 중복된 문자를 탐지할 정규식 작성한다.
+2. 중복된 문자가 없을때까지('regax.test(answer)->false'), 중복된 문자열을 찾아 제거한다. 
+*/
+
 function problem2(cryptogram) {
-  var answer;
+  const regex = new RegExp("([a-z])\\1+", "g");
+  let answer = cryptogram;
   return answer;
 }
 
