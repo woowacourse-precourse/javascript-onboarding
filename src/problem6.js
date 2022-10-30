af7adf0f8ad7e3461b631a669eb30e8bf37d791b
zktm9903 <zktm9903@naver.com>
2022-10-30 18:43:51 +0900

    problem6 주석 추가

--- src/problem6.js
+++ src/problem6.js
@@ -6,20 +6,20 @@ function problem6(forms) {
     const [email, nick] = el;
 
     for(let i=0;i<nick.length-1;i++){
-      const nick2 = nick.slice(i, i+2);
-      console.log(nick2)
-      if(map.has(nick2)){
-        if(map.get(nick2).includes(email) === false)
-          map.get(nick2).push(email);
+      const nick2 = nick.slice(i, i+2); // 두 글자씩 자르기
+
+      if(map.has(nick2)){ // 두 글자 닉의 키가 이미 존재할 경우
+        if(map.get(nick2).includes(email) === false) // 배열에 자신의 이메일이 없을 경우
+          map.get(nick2).push(email); // 배열에 이메일 삽입
       }
-      else
-        map.set(nick2, [email]);
+      else // 두 글자 닉의 키가 존재하지 않을 경우
+        map.set(nick2, [email]); // 배열 생성 및 이메일 삽입
     }
   })
 
   map.forEach((emails)=>{
-    if(emails.length > 1)
-      emails.map(el=>set.add(el));
+    if(emails.length > 1) // 닉네임에 해당되는 이메일이 2개 이상인 경우
+      emails.map(el=>set.add(el)); //집합에 이메일 삽입
   })
 
   return Array.from(set).sort();
