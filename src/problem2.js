bc3b9033a061975a1cb8a671f1f160f2c5d63aaf
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-30 20:23:57 +0900

    feat: problem 2 스택을 이용한 문장 중복제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -5,5 +5,17 @@ function checkException(str) {
   const y = str.toLowerCase() === str;
   return x && y;
 }
-
+function isDup(stack, c) {
+  if (c === stack[stack.length - 1]) return true;
+  return false;
+}
+function delDup(str) {
+  let stack = [];
+  for (let i = 0; i < str.length; i++) {
+    if (!isDup(stack, str[i]) || i === 0) stack.push(str[i]);
+    else stack.pop();
+  }
+  console.log(stack);
+  return stack.join("");
+}
 module.exports = problem2;
