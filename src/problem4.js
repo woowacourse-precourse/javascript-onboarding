664f4c8be2f5acbdc37f1668849d9229cf6a9527
LEEJW1953 <jiwonlee1953@naver.com>
2022-10-30 16:20:02 +0900

    4번 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,11 @@
 function problem4(word) {
-  var answer=[];
-  for (let i=0; i<word.length; i++) {
-    let temp=word.charCodeAt(i);
-    if ((65<=temp) && (temp<=90)) {
-      answer.push(String.fromCharCode(155-temp));
-    } else if ((97<=temp) && (temp<=122)){
-      answer.push(String.fromCharCode(219-temp));
+  var answer = [];
+  for (let i = 0; i < word.length; i++) {
+    let temp = word.charCodeAt(i);
+    if ((65 <= temp) && (temp <= 90)) {
+      answer.push(String.fromCharCode(155 - temp));
+    } else if ((97 <= temp) && (temp <= 122)) {
+      answer.push(String.fromCharCode(219 - temp));
     } else {
       answer.push(word[i]);
     }
