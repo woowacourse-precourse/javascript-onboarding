fe7e2d7bdb0b7f4ab1134ea2d8faac18ce74d7e7
runkey <csi9509@naver.com>
2022-10-31 00:39:49 +0900

    Feat: a~z가 두 번 이상 반복되는 문자열을 전역 검색

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram;
+  
+  while(true) {
+      // a~z 가 두번 이상 반복되는 문자열을 전역 검색한다.
+      let regExp = answer.match(/a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}/g);
+
+  }
+  
   return answer;
 }
 
