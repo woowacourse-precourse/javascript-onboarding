c66a33e30e75db471a7a788e76fc08629e791c10
Minhye Kang <alsgp9898@naver.com>
2022-10-30 13:40:00 +0900

    feat(PROBLEM1): 페이지 번호의 각 자리 숫자를 더한 후 더  큰 수를 반환하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,23 @@
 function problem1(pobi, crong) {
-  if(isValid(pobi) || isValid(crong)) return -1;
+  if (isValid(pobi) || isValid(crong)) return -1;
+  console.log(getMaxSum(pobi));
 }
 
 const isValid = (pageArr) => {
-  return (pageArr[1] - pageArr[0] !== 1);
+  return pageArr[1] - pageArr[0] !== 1;
 };
 
+const getMaxSum = (pageArr) => {
+  const sumArr = [];
+  pageArr.forEach((pageNum) =>
+    sumArr.push(
+      String(pageNum)
+        .split("")
+        .reduce((acc, curr) => acc + Number(curr), 0)
+    )
+  );
+  return Math.max(...sumArr);
+};
+
+problem1([377, 378], [133, 134]);
 module.exports = problem1;
