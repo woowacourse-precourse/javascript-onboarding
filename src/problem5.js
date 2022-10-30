6579864718238c2009c71b00b55f24c3a22b86b7
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-30 20:37:48 +0900

    문제5) feat: 각 화폐개수를 담을 배열과 화폐금액이 내림차순으로 있는 배열 초기화

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,9 @@
 function problem5(money) {
   var answer;
+
+  const moneyCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
   return answer;
 }
 
