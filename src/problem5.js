6f1d6de8dbab1a044a133770be362fcf4c30eb8b
Circlewee <pnj1118@naver.com>
2022-10-30 15:44:08 +0900

    [#5]feat: BILL_LIST 정의

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
+const BILL_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+/**
+ * @param {number} money
+ * @returns {number[]}
+ */
 function problem5(money) {
-  var answer;
+  const answer = [];
+
   return answer;
 }
 
