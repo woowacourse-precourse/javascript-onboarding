b0565394bfe91786fbc88bc798da230568d15300
paduck-96 <peterrabitt1234@naver.com>
2022-10-30 18:52:37 +0900

    problem3-1

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
   var answer;
+  let arr = [];
+  for (let i = number; i > 0; i--) {
+    arr.push(i);
+  }
+  arr = [...arr.join("")];
+  answer = arr.filter((e) => e === "3" || e === "6" || e === "9").length;
   return answer;
 }
 
