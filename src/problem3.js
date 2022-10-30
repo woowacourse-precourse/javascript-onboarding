9c333827910e6bfbce3594e83f43cb27c8f00785
SoobinJ <soobin08011812@gmail.com>
2022-10-30 15:54:19 +0900

    feat : problem 3 요구 사항 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
 function problem3(number) {
   var answer;
+  var count = 0; //3,6,9의 개수(손뼉을 쳐야할 수)
+
+  for (var i = 0; i < number + 1; i++) {
+    var numberArr = Array.from(String(i)); //number의 각 자리 수를 배열로 저장
+    for (var j = 0; j < arr.length; j++) {
+      if (
+        //3,6,9가 포함되는지 검사, 포함되지 않을 경우 -1을 반환
+        numberArr[j].indexOf(3) !== -1 ||
+        numberArr[j].indexOf(6) !== -1 ||
+        numberArr[j].indexOf(9) !== -1
+      ) {
+        count++;
+        answer = count;
+      }
+    }
+  }
+
   return answer;
 }
 
