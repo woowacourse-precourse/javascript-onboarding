d40ee859398cba6521bc5b10be13fea4a5ef5236
박겸영 <yong_drg@naver.com>
2022-10-30 14:52:08 +0900

    feat: 문제 3) 1부터 number까지 3, 6, 9 들어가는 수 모두 구하기 - 기능 1: 반복문으로 3, 6, 9가 들어가는 수를 모두 구해서 하나의 문자열에 담기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let StringOfClapNumbers = '';
+
+  for (let currNum = 1; currNum <= number; currNum++) {
+    const currStr = currNum.toString();
+
+    if (currStr.includes('3') || currStr.includes('6') || currStr.includes('9'))
+      StringOfClapNumbers += currStr;
+  }
 }
 
 module.exports = problem3;
