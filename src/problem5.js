c2ce74cc00aff66d1094e3354fb8a8d284f8100c
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 20:11:04 +0900

    feat(problem5): 주어진 금액을 기준 금액으로 나누되 금액이 큰 순서대로 배열에 담아 return

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,12 @@
-const Amounts = Object.freeze({
-  BILL50: 0,
-  BILL10: 1,
-  BILL5: 2,
-  BILL1: 3,
-  COIN500: 4,
-  COIN100: 5,
-  COIN50: 6,
-  COIN10: 7,
-  COIN1: 8
-})
-
 function problem5(money) {
-  var answer;
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const currencyCount = currency.length;
+  var answer = new Array(currencyCount);
+
+  for (let i = 0; i < currencyCount; ++i) {
+    answer[i] = parseInt(money / currency[i]);
+    money %= currency[i];
+  }
   return answer;
 }
 
