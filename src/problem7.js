bd9324c0b0f5128e3b9edb76fe4704ea32be28e8
sujungYu <su_moment@naver.com>
2022-10-30 18:03:36 +0900

    feat(PROBLEM7):친구 추천 점수

--- src/problem7.js
+++ src/problem7.js
@@ -8,6 +8,8 @@ function problem7(user, friends, visitors) {
         if(A==user) userFriends.push(B);
         if(B==user) userFriends.push(A);
     });
+
+    friendsObj = setScore(user, friends, visitors, userFriends, friendsObj);
 }
 
 function setList(friends, visitors) {
@@ -19,6 +21,20 @@ function setList(friends, visitors) {
     });
     visitors.forEach(v=> {
         if(!friendsObj[v]) friendsObj[v] = 0;
+    });
+    return friendsObj;
+}
+
+function setScore(user, friends, visitors, userFriends, friendsObj) {
+    friends.forEach(v=> {
+        const [A, B] = v;
+        userFriends.forEach(d=> {
+            if(A==d && B!=user) friendsObj[B] +=10;
+            if(B==d && A!=user) friendsObj[A] +=10;
+        });
+    })
+    visitors.forEach(v=> {
+        friendsObj[v] +=1;
     });
     return friendsObj;
 }
