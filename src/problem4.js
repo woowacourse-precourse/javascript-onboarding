7471370ca0f61cdcbce2538ae5ef07fe5de98e7b
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-30 19:51:23 +0900

    problem4 - feat: 암호 문자열 반환 함수
    특정 키를 넣으면 특정 값을 반환한다.

--- src/problem4.js
+++ src/problem4.js
@@ -16,6 +16,39 @@ const map = (array, func) => {
 
 const getCharCode = eachString => `${eachString}`.charCodeAt(0);
 
+const getGreenFrogDictionary = upperCase => {
+  const greenFrogDictionary = {
+    A: "Z",
+    B: "Y",
+    C: "X",
+    D: "W",
+    E: "V",
+    F: "U",
+    G: "T",
+    H: "S",
+    I: "R",
+    J: "Q",
+    K: "P",
+    L: "O",
+    M: "N",
+    N: "M",
+    O: "L",
+    P: "K",
+    Q: "J",
+    R: "I",
+    S: "H",
+    T: "G",
+    U: "F",
+    V: "E",
+    W: "D",
+    X: "C",
+    Y: "B",
+    Z: "A",
+  };
+
+  return greenFrogDictionary[upperCase];
+};
+
 function problem4(word) {
   var answer;
   return answer;
