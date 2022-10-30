510efbfbfdedd3ae5084b8c7b352321c96f37917
HWI <dlwognl99@naver.com>
2022-10-30 20:09:15 +0900

    문제3_feat: number를 검사하고 count를 얻는 함수 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -12,4 +12,15 @@ function problem3(number) {
   return clapCount;
 }
 
+function getClapCount(number) {
+  // 주어진 매개변수를 문자열로 반환하고 요소를 하나씩 쪼갠다.
+  const list = number.toString().split("");
+
+  // filter로 문자 '3' '6' '9' 가 있는 요소만 모아 새로운 clap 배열을 만들고 그 clap 배열의 길이를 반환한다.
+  const clap = list.filter(
+    (list) => list === "3" || list === "6" || list === "9"
+  ).length;
+  return clap;
+}
+
 module.exports = problem3;
