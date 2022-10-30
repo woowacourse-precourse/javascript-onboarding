0bf40d993247cf576ce18327a36c5ff885c5ba1f
useon <yoosk4986@naver.com>
2022-10-31 02:39:39 +0900

    feat: [problem4] word를 반대로 변환하는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -15,6 +15,15 @@ function problem4(word) {
     lowerCaseMap.set(String.fromCharCode(ASCII), String.fromCharCode(lowerCaseCode));
     lowerCaseCode--;
   }
+  // 3. 각 word를 반대로 변환하는 기능
+  for (let index = 0; index < word.length; index++) {
+    if (upperCaseMap.has(word[index]) || lowerCaseMap.has(word[index])) {
+      if (upperCaseMap.has(word[index])) answer += upperCaseMap.get(word[index]);
+      if (lowerCaseMap.has(word[index])) answer += lowerCaseMap.get(word[index]);
+    } else {
+      answer += word[index];
+    }
+  }
   return answer;
 }
 
