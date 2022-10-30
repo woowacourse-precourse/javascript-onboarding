7b8e0dc35e3543f2b398880c55837595fae55821
Blackraven93 <reblackraven@gmail.com>
2022-10-31 02:20:39 +0900

    Problem4[Feat] 유니코드 상수화 및 문자 유니코드의  대칭성을 파악하고 해당 유니코드값을 디코딩하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,30 @@ function problem4(word) {
     .map((alphabet) => {
       // 알파벳 외의 문자는 변환하지 않는다. (특수문자 및 공백 체크)
       if (!/[a-zA-Z]/g.test(alphabet)) return alphabet;
+
+      const UNICODE = {
+        A: "A".charCodeAt(),
+        Z: "Z".charCodeAt(),
+        N: "N".charCodeAt(),
+        a: "a".charCodeAt(),
+        z: "z".charCodeAt(),
+        n: "n".charCodeAt(),
+      };
+      const targetUnicode = alphabet.charCodeAt();
+
+      // 소문자일 경우
+      if (targetUnicode >= UNICODE.a)
+        // 단어 n이 넘어간 경우 (절반 이상인 경우)
+        return targetUnicode >= UNICODE.n
+          ? String.fromCharCode(UNICODE.a + (UNICODE.z - targetUnicode))
+          : String.fromCharCode(UNICODE.z - (targetUnicode - UNICODE.a));
+
+      // 대문자일 경우
+      if (targetUnicode <= UNICODE.Z)
+        // 단어 N이 넘어간 경우 (절반 이상인 경우)
+        return targetUnicode >= UNICODE.N
+          ? String.fromCharCode(UNICODE.A + (UNICODE.Z - targetUnicode))
+          : String.fromCharCode(UNICODE.Z - (targetUnicode - UNICODE.A));
     })
     .join("");
 }
