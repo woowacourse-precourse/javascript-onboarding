24eb706dc74dcc3f5b178af39f2d3d9302b881fd
thewronghand <penfreak77@gmail.com>
2022-10-31 02:38:17 +0900

    feat: answer 변수 선언 및 반환

--- src/problem4.js
+++ src/problem4.js
@@ -6,6 +6,8 @@ function problem4(word) {
     return frogDict[findIndex(x)]
   } return x;
 }
+let answer = word.split('').map(translate).join('');
+return answer;
 }
 
 module.exports = problem4;
@@ -16,5 +18,5 @@ module.exports = problem4;
 //[x] 입력된 문자 x가 input의 몇 번째 문자에 해당하는지 확인하는 findIndex 기능을 구현한다.
 //[x] findIndex기능으로 찾아낸 x의 순서를 frogDict에 대입하여 x를 청개구리어로 바꿔주는 기능을 구현한다.
 //  [x] translate 기능 안에 if 조건문을 삽입하여 입력된 x가 알파벳이 아닐 경우 x를 그대로 반환하도록 한다.
-//[] 주어진 문자열 word를 배열화하고, 배열에 translate를 적용한 후 join하여 다시 문자열로 변환하는 기능을 구현한다.
-//[] 이를 answer로 두고 반환한다.
+//[x] 주어진 문자열 word를 배열화하고, 배열에 translate를 적용한 후 join하여 다시 문자열로 변환하는 기능을 구현한다.
+//[x] 이를 answer로 두고 반환한다.
