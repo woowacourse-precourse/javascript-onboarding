c65bf1f2a46c96a025297e49f73e5bbcf05616a9
Raehyeon Jeong <raehy19@gmail.com>
2022-10-31 02:08:10 +0900

    feat(problem3) : 입력값 예외 처리 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
+function isException(number) {
+  return (
+    isNaN(number) || !Number.isInteger(number) || number < 1 || number > 10000
+  );
+}
+
 function problem3(number) {
-  var answer;
-  return answer;
+  if (isException(number)) return -1;
 }
 
 module.exports = problem3;
