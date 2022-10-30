acd48ffbb7ba7dd52662834d72a09e0861a14adc
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-10-30 21:44:34 +0900

    feat: solve problem2 1st

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+const checkCyrp=(cyrp)=>{
+  for(let i=0; i<cyrp.length-1; i++){
+      if (cyrp[i]===cyrp[i+1]){
+          return cyrp[i];
+      }
+  }
+  return false;
+}
+
+function problem2(cryptogram){
+  let regex;
+  let word = checkCyrp(cryptogram);
+  while(word){
+      regex = new RegExp(`${word}${word}+`, 'g');
+      cryptogram = cryptogram.replace(regex, '')
+      word = checkCyrp(cryptogram)
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
