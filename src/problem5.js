4ddacc79d2a16a70ad7f08777086f0be5c79490d
random-olive <104327907+random-olive@users.noreply.github.com>
2022-10-30 21:21:06 +0900

    refactoring

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,12 @@
 function problem5(money) {
-  var answer=new Array(9).fill(0)
-  //ex
-  //[160,237]// 50000,10000,5000,1000,500,100,50,10,1// [1,1,0,0,0,2,0,3,7] 
- 
-  test[0]=Math.floor(n/50000);
-  n=n%50000;
+  var answer = new Array(9).fill(0);
+  let s = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
-  test[1]=Math.floor(n/10000)
-  n=n%10000;
- 
+  for (let i = 0; i < s.length; i++) {
+    answer[i] = Math.floor(n / s[i]);
+    n = n % s[i];
+  }
+
   return answer;
 }
 
