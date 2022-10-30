236eea797c280b65cdd7f75f5040de6a0d694c50
Ji Seungha <jish0210@naver.com>
2022-10-30 18:59:27 +0900

    Problem 3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
+function counter(num){
+  var answer = 0;
+  while(num){
+    if(num % 10 == 3 || num % 10 == 6 || num % 10 == 9) answer++;
+    num = parseInt(num/10);
+  }
+  return answer;
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for(var i = 1; i <= number; i++){
+    answer += counter(i);
+  }
   return answer;
 }
 
