6e3ccd142cdc7ac5f847674477ef22811299a880
sunny <huiseon37@gmail.com>
2022-10-31 06:21:58 +0900

    [problem6] 닉네임별 정규식 구현 함수 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
+function getRegexStr(nickname){
+  let regexPatternList = [];
+  for(let i = 0; i < nickname.length-1 ; i++){
+    regexPatternList = [...regexPatternList, `${nickname[i]}${nickname[i+1]}`]
+  }
+  
+  return regexPatternList.join('|');
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
 }
 
 module.exports = problem6;
