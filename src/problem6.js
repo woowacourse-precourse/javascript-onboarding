731ffaf7568d90bbe7aa20d84f898e7198537b92
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 21:11:31 +0900

    feat(problem6): 각 크루의 닉네임을 2글자 단위로 토큰화 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,17 @@
 function problem6(forms) {
   var answer;
+  const crewCount = forms.length;
+  let nicknames = new Array(crewCount);
+
+  for (let crewIndex = 0; crewIndex < crewCount; ++crewIndex) {
+    const nickname = forms[crewIndex][1];
+    const nicknameLength = nickname.length;
+
+    nicknames[crewIndex] = [];
+    for (let charIndex = 0; charIndex < nicknameLength - 1; ++charIndex) {
+      nicknames[crewIndex].push(nickname.substr(charIndex, 2));
+    }
+  }
   return answer;
 }
 
