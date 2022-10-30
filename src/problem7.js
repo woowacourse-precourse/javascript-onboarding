717ef5a84507030f6ab193582851ca8487f6d0da
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-31 05:12:49 +0900

    refactor: [problem7] - 사용자의 친구 목록을 반환하는 함수 수정 feat: 사용자와 친구 관계인 배열을 구하는 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,5 @@
+const { val } = require("jshint/src/options");
+
 function problem7(user, friends, visitors) {
   var answer;
 
@@ -5,6 +7,20 @@ function problem7(user, friends, visitors) {
 }
 
 function getUserFriends(user, friends) {
+  var userFriends = [user];
+  var userFriendRelationship = getUserFriendRelationship(user, friends);
+
+  for (var i = 0; i < userFriendRelationship.length; i++) {
+    var userFriens = userFriendRelationship[i].filter(
+      (element) => element !== user
+    );
+    userFriends.push(userFriens.join());
+  }
+
+  return userFriends;
+}
+
+function getUserFriendRelationship(user, friends) {
   return friends.filter((element) => element.includes(user));
 }
 
