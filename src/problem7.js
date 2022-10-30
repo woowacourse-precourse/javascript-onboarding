586b2cc7f2bbb8bf59c047ee6afd023c57ebf611
jun <ggprgrkjh@naver.com>
2022-10-31 00:27:35 +0900

    docs: 기능 요구 사항 P7

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,9 @@
+// 기능 요구 사항
+
+// -[] 친구 배열에 유저가 포함되어 있으면 나머지 한 친구를 배열에 저장한다.
+// -[] 배열에 저장된 친구의 친구가 유저가 아니면 그 친구에게 10점을 부과한다.
+// -[] 방문자 점수를 부과한다.
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
