c5d688a1460cdf76437ab3a28c1427176f347d8a
hjy0951 <hjy0951@gmail.com>
2022-10-30 19:43:50 +0900

    problem3: 구현 설계

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
   var answer;
+  /* 1~number까지 순회하며 3,6,9 숫자 counting */
   return answer;
 }
+
+/* 파라미터로 받은 숫자에 3,6,9 숫자가 몇개 들어있는지 확인하는 함수
+    각 자릿수가 3,6,9 중 하나인지 확인
+      -> 5자리라면 5번의 비교가 발생, 1~10000까지 비교시 50000이 안되는 비교가 발생
+*/
 
 module.exports = problem3;
