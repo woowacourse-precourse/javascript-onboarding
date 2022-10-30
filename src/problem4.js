809ff955423ebfe01285bbd26266e132dfffae85
waymokorea <taw0207@naver.com>
2022-10-30 16:14:08 +0900

    problem4 test

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,53 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabet = [
+    'a',
+    'b',
+    'c',
+    'd',
+    'e',
+    'f',
+    'g',
+    'h',
+    'i',
+    'j',
+    'k',
+    'l',
+    'm',
+    'n',
+    'o',
+    'p',
+    'q',
+    'r',
+    's',
+    't',
+    'u',
+    'v',
+    'w',
+    'x',
+    'y',
+    'z',
+  ];
+
+  const upperAlphabet = alphabet.map((x) => x.toUpperCase());
+  let reverseAlphabet = [];
+  for (let i = 0; i < alphabet.length; i++) {
+    reverseAlphabet.unshift(alphabet[i]);
+  }
+  const upperReverseAlphabet = reverseAlphabet.map((x) => x.toUpperCase());
+
+  let result = '';
+
+  const array = word.split('');
+  for (let i = 0; i < array.length; i++) {
+    if (array[i] !== ' ' && array[i] === array[i].toUpperCase()) {
+      result += upperReverseAlphabet[upperAlphabet.indexOf(array[i])];
+    } else if (array[i] !== ' ' && array[i] === array[i].toLowerCase()) {
+      result += reverseAlphabet[alphabet.indexOf(array[i])];
+    } else if (array[i] === ' ') {
+      result += ' ';
+    } else result += array[i];
+  }
+  return result;
 }
 
 module.exports = problem4;
