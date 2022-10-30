b906b236de277a7fa9e73a96725fb97287aae8c2
Minhye Kang <alsgp9898@naver.com>
2022-10-30 14:53:58 +0900

    feat(PROBLEM2): 연속으로 중복하는 문자열이 없는지 확인하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,13 @@
 function problem2(cryptogram) {
-  console.log(removeDuplicate(cryptogram))
+
 }
 
 const removeDuplicate = (cryptogram) => {
   return cryptogram.replace(/([a-z])\1+/g, "")
+}
+
+const isNotDuplicate = (cryptogram) => {
+  return !(/([a-z])\1+/g.test(cryptogram))
 }
 
 module.exports = problem2;
