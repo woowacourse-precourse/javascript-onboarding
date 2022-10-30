bcf7a494fbfe6b2c2f41902d93a544df19dfa9c9
Sim <zh025700@naver.com>
2022-10-31 03:08:45 +0900

    feat: problem3 num to arr

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
   var answer;
+  numArr = makeNumArr(number);
   return answer;
 }
+
+const makeNumArr = (num) =>
+  Array.from(Array(num).keys())
+    .map((v) => v + 1)
+    .map((v) => v);
 
 module.exports = problem3;
