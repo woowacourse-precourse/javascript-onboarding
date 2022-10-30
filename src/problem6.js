857b1fe79217d60e909110247763be30d5517c6a
Chaeduk <kkj2551@naver.com>
2022-10-30 12:28:14 +0900

    refactor(problem6) : 기존 array로 했던 로직 수행을 Map&Set을 활용하도록 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,25 +1,49 @@
 function problem6(forms) {
   const answer = []
-  const splitNames = forms.map((element) => (
-    element[1]
-          .split('')
-          .map((element, index, origin) => {
-                return index !== origin.length-1 ? element + origin[index+1] : null
-              })
-          .slice(undefined, -1)
-  ))
+  const duplicatedArry = getDuplicatedArray(forms.map((element) => element[1]))
 
-  forms.forEach((element, eIndex) => {
-    splitNames.forEach((pieces, pIndex) => {
-      if(eIndex !== pIndex && pieces.some((piece)=>element[1].includes(piece)) && answer.at(-1) !== element[0]){
-          answer.push(element[0])
-      }
-    })
+  forms.forEach((element) => {
+    if(duplicatedArry.some((duplicated) => element[1].includes(duplicated) )){
+      answer.push(element[0])
+    }
   })
 
   answer.sort()
 
   return answer;
+}
+
+function splitWord(word){
+  const result = []
+  for(let i=0; i<word.length-1; i++){
+    result.push(word.substring(i, i+2))
+  }
+  return result
+}
+
+function addWordsToMapObj(words, mapObj){
+  for(const word of new Set(words)){
+    if(mapObj.has(word)){
+      mapObj.set(word, mapObj.get(word) + 1)
+    } else {
+      mapObj.set(word, 1)
+    }
+  }
+}
+
+function getDuplicatedArray(words){
+  const dict = words.reduce((acc, cur) => {
+    addWordsToMapObj(splitWord(cur), acc)
+    return acc
+  }, new Map())
+
+  for(const word of dict.keys()){
+    if(dict.get(word) < 2){
+      dict.delete(word)
+    }
+  }
+
+  return Array.from(dict, ([word, _]) => (word))
 }
 
 module.exports = problem6;
