66c111e1b34934ccf7574f0d9ba05f68e556a979
ZUITOPIA <korhwngml123@naver.com>
2022-10-30 22:29:51 +0900

    feat: (#4) 아스키코드를 이용하여 문자바꾸기 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,28 @@
 function problem4(word) {
-  var answer;
+  let wordArr = word.split("");
+  let answer = "";
+
+  const getWord = (changedIndex) => {
+    let changedChar = String.fromCharCode(changedIndex);
+    answer += changedChar;
+  };
+
+  for (let i = 0; i < wordArr.length; i++) {
+    let tempAscii = wordArr[i].charCodeAt();
+    const isUpper = 67 <= tempAscii && tempAscii <= 90;
+    const isLower = 97 <= tempAscii && tempAscii <= 122;
+    const isBlank = tempAscii === 32;
+
+    if (isUpper) {
+      let changedIndex = 155 - tempAscii;
+      getWord(changedIndex);
+    } else if (isLower) {
+      let changedIndex = 219 - tempAscii;
+      getWord(changedIndex);
+    } else if (isBlank) {
+      answer += wordArr[i];
+    }
+  }
   return answer;
 }
 
