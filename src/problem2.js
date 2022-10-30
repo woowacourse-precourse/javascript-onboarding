3d7cb5a8f603b130e76d6d7a67c93a5860bc41a6
Sunghyun627 <kksscc4@gmail.com>
2022-10-30 16:01:09 +0900

    feat: 배열의 마지막 요소와 문자가 같지 않은 경우에 대한 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -5,8 +5,8 @@
       * 암호문의 모든 문자에 대해 순서대로 아래의 case별로 처리
       - [O] 1. 배열이 비여있는 경우 => 배열의 마지막에 삽입 
       - [O] 2. 배열의 마지막 요소와 문자가 같고, 문자가 암호문의 마지막 요소일 경우 =>  배열에서 마지막 요소 제거
-      - [ ] 3. 배열의 마지막 요소와 문자가 같고, 문자가 중복되지 않은 상태인 경우 => 문자의 중복 상태 여부 True로 변경
-      - [ ] 4. 배열의 마지막 요소와 문자가 다르고, 문자의 중복 상태 여부가 True인 경우 => 배열의 마지막 요소를 제거하고, 문자의 중복 상태 여부 False로 변경 후, 문자를 배열 끝에 삽입
+      - [O] 3. 배열의 마지막 요소와 문자가 같고, 문자가 중복되지 않은 상태인 경우 => 문자의 중복 상태 여부 True로 변경
+      - [] 4. 배열의 마지막 요소와 문자가 다르고, 문자의 중복 상태 여부가 True인 경우 => 배열의 마지막 요소를 제거하고, 문자의 중복 상태 여부 False로 변경 후, 문자를 배열 끝에 삽입
       - [ ] 5. 배열의 마지막 요소와 문자가 다르고, 문자의 중복 상태 여부가 False인 경우 => 문자를 배열의 끝에 삽입
 */
 
@@ -39,6 +39,11 @@ function problem2(cryptogram) {
         isDuplicated = true;
         continue;
       }
+      if (isDuplicated) {
+        decryptedCryptogramStack.pop();
+        isDuplicated = false;
+      }
+      decryptedCryptogramStack.push(beforeDecryptedCryptogram[i]);
     }
   }
 }
