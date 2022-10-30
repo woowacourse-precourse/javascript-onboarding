836fb02885ae4292c77f916fb3a100cfe1e2aeec
DongRyeong Lee <dr11291129@gmail.com>
2022-10-30 20:39:07 +0900

    feat: P2 deleteOverlap 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
+let isChanged = false;
+
 function problem2(cryptogram) {
   var answer;
   return answer;
+}
+
+function deleteOverlap(string) {
+  let result = "";
+
+  for (let i = 0; i < string.length - 1; i++) {
+    if (string[i] == string[i + 1]) {
+      i++;
+      isChanged = true;
+    } else {
+      result += string[i];
+    }
+  }
+
+  return result;
 }
 
 module.exports = problem2;
