d130a21224493a9c8f569f627706cb52cfc1df23
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-30 19:36:05 +0900

    feat: problem 1 parameter exception handling 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,12 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (checkException(pobi) === -1 || checkException(crong) === -1) return -1;
+}
+
+function checkException(arr) {
+  if (arr.length !== 2) return -1;
+  if (arr[1] - arr[0] !== 1) return -1;
+  if (arr.some((item) => item <= 1 || item >= 400)) return -1;
+  return 1;
 }
 
 module.exports = problem1;
