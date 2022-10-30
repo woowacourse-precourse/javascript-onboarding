542947d7ae8b7ab7ebccace71fa9c1959dde0680
wnw <fkaus65@naver.com>
2022-10-30 18:35:53 +0900

    feat(problem4): add isUpper isUpper: Check for capitalization and return

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
+const isUpper = (char) => /[A-Z]/.test(char);
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const UPPER = 65;
+  const LOWER = 97;
+
+  const upperAlphas = getAlphasTypeOf(UPPER);
+  const lowerAlphas = getAlphasTypeOf(LOWER);
+
+  return [...word]
+    .map((char) => {
+      if (char === ' ') return char;
+
+      const frogIndex = getFrogIndex(char);
+
+      if (isUpper(char)) return upperAlphas[frogIndex];
+      return lowerAlphas[frogIndex];
+    })
+    .join('');
 }
 
 module.exports = problem4;
