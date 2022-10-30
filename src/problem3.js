fd72d342af62288ac69b784b3081be97ab48871d
Sunghyun627 <kksscc4@gmail.com>
2022-10-30 19:10:23 +0900

    구현할 기능 목록 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
+/*
+  구현할 기능 목록
+  [] 해당 숫자가 3, 6, 9 중 하나인지 확인하는 기능 | return 값 : 1(3, 6, 9 중 하나인 경우)/0(3, 6, 9 중 하나가 아닌 경우)  
+  [] 해당 숫자를 각 자리수로 쪼개어, 각 자리수마다 3, 6, 9인지 확인하는 기능
+*/
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let result = 0;
+  return result;
 }
 
 module.exports = problem3;
