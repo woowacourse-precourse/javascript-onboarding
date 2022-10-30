47d3e211dad6912082ee0def22c2420dd13fe3eb
kimyou1102 <yoosk5485@naver.com>
2022-10-31 02:04:18 +0900

    feat: 문제 6번 - 연속하는 문자가 될 수 있는 경우의 수들을 구한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,16 @@
 function problem6(forms) {
   var answer;
+
+  for(let i=0; i < forms.length; i++) {
+    const [email, nickname] = forms[i];
+    for(let x=0; x < nickname.length; x++) {
+      for(let y=x+1; y < nickname.length; y++) {
+        const str = nickname.slice(x, y+1);
+        
+      }
+    }
+  }
+  
   return answer;
 }
 
