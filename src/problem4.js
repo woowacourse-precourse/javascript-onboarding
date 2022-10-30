6b3ac9208eeb7a0289764e693b4bbc8032035943
하송희 <99heesong@naver.com>
2022-10-30 20:54:28 +0900

    [문제 4] 기능추가: 입력 문자열 첫 글자의 문자 판단 후 반대로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let newStr = "";
+  let char = word[0];
+  if(char >= 'A' && char <= 'Z') {
+    newStr += String.fromCharCode('Z'.charCodeAt(0) - (char.charCodeAt(0) - 'A'.charCodeAt(0)));
+  } else if(char >= 'a' && char <= 'z') {
+    newStr += String.fromCharCode('z'.charCodeAt(0) - (char.charCodeAt(0) - 'a'.charCodeAt(0)));
+  } else {
+    newStr += char;
+  }
 }
 
 module.exports = problem4;
