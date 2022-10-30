5acf14906754974842a07ba8ef80c1d87333c976
동동이 <drdragon777@naver.com>
2022-10-30 21:01:38 +0900

    PROBLEM4 - initial commit(solved)

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabets = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(65 + i)
+  );
+  const reversedAlphabets = [...alphabets].reverse();
+
+  return [...word]
+    .map((ch) => {
+      if (ch === " ") return " ";
+      const reversed = reversedAlphabets[alphabets.indexOf(ch.toUpperCase())];
+      return ch.toUpperCase() === ch ? reversed : reversed.toLowerCase();
+    })
+    .join("");
 }
 
 module.exports = problem4;
