3d08c087f3e9b5d88eb9b7a04fc726c966d5dc85
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-31 00:21:48 +0900

    refactor(문제2): 중복 문자를 대체하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,12 @@
 const DUPLICATE_REGEX = /(\w)\1+/g;
+const REPLACE_CHAR = "";
 
 const checkDuplicate = (cryptogram) => {
   return DUPLICATE_REGEX.test(cryptogram);
+}
+
+const replaceDuplicate = (cryptogram, replaceChar) => {
+  return cryptogram.replace(DUPLICATE_REGEX, replaceChar);
 }
 
 function problem2(cryptogram) {
