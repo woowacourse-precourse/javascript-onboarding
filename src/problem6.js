9d95c3bc12266b4828d0b117897d4d5adbfb93bf
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-30 20:16:02 +0900

    feat: PROBLEM 6 닉네임 형식 검사 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -7,10 +7,14 @@ const isCrewForm = (forms) => {
   let realCrewArray = [];
   forms.filter((element) => {
     let email = element[0];
+    let nickname = element[1];
     let domain = "@email.com"
+    let checkKor = /[a-z0-9]|[ \[\]{}()<>?|`~!@#$%^&*-_+=,.;:\"'\\]/g;
 
     if (email.length < 11 || 20 < email.length) return false;
     if (!email.includes(domain)) return false;
+    if (nickname.length < 1 || 20 < nickname.length) return false;
+    if (checkKor.test(nickname)) return false;
 
     realCrewArray.push(element);
   })
