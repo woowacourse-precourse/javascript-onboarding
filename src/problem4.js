a14b71ed86694c74a70242b135cfa58800c3d39f
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-30 20:42:29 +0900

    feat: problem 4 기능 완성

--- src/problem4.js
+++ src/problem4.js
@@ -1,4 +1,28 @@
-function problem4(word) {}
+function problem4(word) {
+  if (!checkException(word)) return;
+  const a = word.split("");
+  let answer = "";
+  a.map((item) => {
+    if (
+      item.charCodeAt() >= "A".charCodeAt() &&
+      item.charCodeAt() <= "Z".charCodeAt()
+    ) {
+      answer += String.fromCharCode(
+        "A".charCodeAt() + "Z".charCodeAt() - item.charCodeAt()
+      );
+    } else if (
+      item.charCodeAt() >= "a".charCodeAt() &&
+      item.charCodeAt() <= "z".charCodeAt()
+    ) {
+      answer += String.fromCharCode(
+        "a".charCodeAt() + "z".charCodeAt() - item.charCodeAt()
+      );
+    } else {
+      answer += item;
+    }
+  });
+  return answer;
+}
 
 function checkException(word) {
   return word.length >= 1 && word.length <= 1000;
