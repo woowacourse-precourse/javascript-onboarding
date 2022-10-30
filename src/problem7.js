6c2c9b3230b28c50f7023a4a24ab6b3c7d49d53d
kimyou1102 <yoosk5485@naver.com>
2022-10-31 02:43:37 +0900

    feat: 문제 7번 - 친구 중 사용자와 함께 아는 친구가 있으면 명당 10점을 더한다.

--- src/problem7.js
+++ src/problem7.js
@@ -30,6 +30,15 @@ function problem7(user, friends, visitors) {
   user_friends.forEach((friend) => {
     users.delete(friend);
   })
+
+  for (let i = 0; i < friends.length; i++) {
+    const [id_a, id_b] = friends[i];
+    if (user_friends.includes(id_a)) {
+      if (users.has(id_b)) users.set(id_b, users.get(id_b) + 10);
+    } else if (user_friends.includes(id_b)) {
+      if (users.has(id_a)) users.set(id_a, users.get(id_a) + 10);
+    }
+  }
   
   return answer;
 }
