23f293933f3e1de77dd18d244a84185d538805ee
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-10-30 20:00:08 +0900

    feat: [P2] 배열의 중복 검사 및 제거 함수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,18 @@
 function stringToArray(array) {
   return [...array];
 }
+function problem2(cryptogram) {
+  let cryptogramArray = stringToArray(cryptogram);
+  let compareArray = [];
+  let i = 0;
+
+  for (; i < cryptogramArray.length; i++) {
+    if (cryptogramArray[i] === compareArray[compareArray.length - 1]) {
+      compareArray.pop();
+    } else if (cryptogramArray[i] !== compareArray[compareArray.length - 1]) {
+      compareArray.push(cryptogramArray[i]);
+    }
+  }
+
+  return compareArray.join("");
+}
