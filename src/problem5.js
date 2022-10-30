079d06f2608866f9c411cc701fcf95c14e756786
xownsdl0 <xownsdl0@naver.com>
2022-10-30 21:07:11 +0900

    <P5> 인출 금액의 화폐 단위별 장수 계산

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
   var answer;
+  let moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let count = 0;
+
+  for (let i = 0; i < moneyArr.length; i++) {
+    count = money % moneyArr[i];
+    moneyArr[i] = Math.floor(money / moneyArr[i]);
+    money = count;
+  }
+
   return answer;
 }
 
