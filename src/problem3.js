768f2f52c0e5ef2a48ac44e409b39face67f1c9a
박준서 <oponize@naver.com>
2022-10-30 13:59:50 +0900

    refactor: problem3/함수 네이밍

--- src/problem3.js
+++ src/problem3.js
@@ -16,13 +16,13 @@
 function problem3(number) {
   let count = 0;
   for (let i = 1; i <= number; i++) {
-    const includes = getInclude369(i);
+    const includes = getIncluded369(i);
     if (includes) count += includes.length;
   }
   return count;
 }
 
-function getInclude369(number) {
+function getIncluded369(number) {
   return number.toString().match(/3|6|9/gi);
 }
 
