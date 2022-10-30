93e14b612a59bf32f42d83fd7c65011b3b5b6b83
kim-limit <rlawogks2468@naver.com>
2022-10-30 13:35:39 +0900

    feat: 'add check alpa case func'

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
+const LOWER = 219;
+const UPPER = 155;
+
+function checkIsLower(alpa) {
+  if (alpa > 64 && alpa < 91) {
+    return 1;
+  } else if (alpa > 96 && alpa < 123) {
+    return 2;
+  } else {
+    return 0;
+  }
+}
+
 function problem4(word) {
-  var answer;
+  var answer = "";
   return answer;
 }
 
