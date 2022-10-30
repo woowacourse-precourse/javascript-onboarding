8b0c0baff0862c5efd4149e4a321b78615a33bd4
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-30 19:47:29 +0900

    problem4 - feat: each, map 헬퍼 함수

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,19 @@
+const each = (iterable, func) => {
+  for (const iterator of iterable) {
+    func(iterator);
+  }
+
+  return;
+};
+
+const map = (array, func) => {
+  const resultArray = [];
+
+  each(array, element => resultArray.push(func(element)));
+
+  return resultArray;
+};
+
 function problem4(word) {
   var answer;
   return answer;
