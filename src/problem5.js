ba2172f36622c3778b5412ae9555997bee758f88
parkjikuk <paxkk@naver.com>
2022-10-31 00:36:43 +0900

    feat : 높은 금액부터 지급할 수 있는 개수를 구하는 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let result = [];
+  let bill = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < bill.length; i++) {
+    result.push(Math.floor(money / bill[i]));
+  } 
+
+  return result;
 }
 
 module.exports = problem5;
