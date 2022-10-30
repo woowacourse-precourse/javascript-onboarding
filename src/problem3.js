f973ae62dce8705b28ae6049f377cc7087646cbf
Deemou <4106243@naver.com>
2022-10-31 01:45:20 +0900

    refactor<P3>: countThreeSixNine () 내 변수 타입 변경

--- src/problem3.js
+++ src/problem3.js
@@ -3,7 +3,7 @@ function problem3(number) {
   return answer;
 }
 const countThreeSixNine = (number) => {
-  let numArr = number.toString().split("");
+  const numArr = number.toString().split("");
   let count = 0;
   numArr.forEach((n) => {
     if (n % 3 == 0 && n != 0) count++;
