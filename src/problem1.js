2d2d1aa5091ed5d90877acb252996a6217b91848
하지훈 <jnh03336@gmail.com>
2022-10-30 13:17:37 +0900

    feat: pro1 argument validation

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,11 @@
 const isValidPages = (pages) => {
+  if(!Array.isArray(pages)) return false;
+  if(pages.length !== 2) return false;
+
+  const [left, right] = pages;
+  if(typeof left !== 'number' || typeof right !== 'number') return false;
+  if(left+1 !== right) return false;
+  if(!(right >= 2 && right <= 400)) return false;
   return true;
 }
 
@@ -9,7 +16,7 @@ const getGameScore = (pages) => {
 function problem1(pobi, crong) {
   if(!isValidPages(pobi)) return -1;
   if(!isValidPages(crong)) return -1;
-  
+
   var answer;
 
   const pobiScore = getGameScore(pobi);
