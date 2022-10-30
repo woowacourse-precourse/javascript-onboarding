c50af218a46e0c99cd34d5806ea477f307b1a738
heejorika <y20001206@gmail.com>
2022-10-31 05:36:55 +0900

    money가 제한사항을 만족하는 지 검사하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
+/*
 function problem5(money) {
   var answer;
   return answer;
 }
 
 module.exports = problem5;
+*/
+
+// money가 제한사항을 만족하는지 
+function checkMoney(m){
+  if (m<1 || m>1000000){return true;}
+  return false;
+}
