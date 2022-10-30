84a965fb7eb1cb2870731c68295585f9fae1841e
Sang-Mini <sangmin3062@gmail.com>
2022-10-30 19:06:15 +0900

    [problem2] 연속으로 중복되는 문자를 제거해주는 코드에서 이슈를 발견하여 임시 방어 코드 작성

--- src/problem2.js
+++ src/problem2.js
@@ -14,6 +14,14 @@ function problem2(cryptogram) {
     a += 1;
   }
 
+  // 연속으로 중복되는 문자를 제거해 주는 반복문에서 현재 마지막 반복이 돌지 않는 이슈를 발견하여 임시 방어 코드 작성
+  // 원인을 찾아 이슈를 해결하려 하였으나. 시간 관계상 강제로 반복문을 한 번 더 돌도록 작성하였음 ㅠㅠ
+  for (let i = 0; i <= answer.length; i++) {
+    if (answer[i] == answer[i + 1]) {
+      answer.splice(i, 2);
+    }
+  }
+
   return result;
 }
 
