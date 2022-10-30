15311be363decec285545f8e9ddb6d76900bf511
kimyou1102 <yoosk5485@naver.com>
2022-10-31 02:34:06 +0900

    feat: 문제 7번 - 추천할 친구 후보와 사용자의 친구를 구한다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,15 +1,31 @@
 function problem7(user, friends, visitors) {
   var answer;
   const users = new Map();
+  const user_friends = [];
 
   visitors.forEach(visitor => {
-    if(users.has(visitor)) {
-      users.set(visitor, users.get(visitor)+1);
+    if (users.has(visitor)) {
+      users.set(visitor, users.get(visitor) + 1);
     } else {
       users.set(visitor, 1);
     }
   });
-  
+
+  for (let i = 0; i < friends.length; i++) {
+    const [id_a, id_b] = friends[i];
+    if (!users.has(id_a)) {
+      users.set(id_a, 0);
+    } else if (!users.has(id_b)) {
+      users.set(id_b, 0);
+    }
+
+    if (id_a === user) {
+      user_friends.push(id_b);
+    } else if (id_b === user) {
+      user_friends.push(id_a);
+    }
+  }
+
   return answer;
 }
 
