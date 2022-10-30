bb22fb6a03c3dca60a32e731a63e71172e78c3bc
hwnim5324 <hwnim5324@naver.com>
2022-10-31 02:08:11 +0900

    2번 기능 목록 작성.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,8 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer=[];
   return answer;
+  //1. 문자열을 배열로 변환해 반복문으로 중복 문자를 판별하고, 중복이 아닌 문자들을 배열 answer에 담는다.
+  //2. answer를 문자열로 변환해 반환한다.
 }
 
 module.exports = problem2;
