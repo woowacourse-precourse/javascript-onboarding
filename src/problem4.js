fe611763085a41036b8be6af413522d77471d23c
jinlee <vito9270@naver.com>
2022-10-30 15:53:06 +0900

    problem4 기능목록 및 문자열 변환 클래스 메소드 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,28 @@
 function problem4(word) {
-  var answer;
+  class WordTransfer {
+    constructor(word) {
+      this.word = word;
+    }
+    transfer() {
+      let newWord = ''
+      for (let i = 0; i < this.word.length; i++) {
+        let temp = this.word.charCodeAt(i)
+
+        if(temp >= 65 && temp <= 90){
+          temp = String.fromCharCode(90 - (temp + 25) % 90)
+        }
+        if(temp >= 97 && temp <= 122){
+          temp = String.fromCharCode(122 - (temp + 25) % 122)
+        }
+        if(temp < 65 || temp > 122 || (temp > 90 && temp < 97)) {
+          temp = String.fromCharCode(temp)
+        }
+        newWord += temp
+      }
+      return newWord
+    }
+  }
+  const answer = new WordTransfer(word).transfer()
   return answer;
 }
 
