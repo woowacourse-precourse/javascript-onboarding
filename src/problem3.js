c6d834042254529d3820453643fa00832c0df8fd
ywoo12321 <ywoo12321@gmail.com>
2022-10-30 18:24:49 +0900

    problem 3 해결방안 구상

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,7 @@
-function problem3(number) {
-  var answer;
+const problem3 = (number) => {
+  let answer;
   return answer;
-}
-
+};
+/*배달이가 좋아하는 369게임을 하고자 한다. 놀이법은 1부터 숫자를 하나씩 대면서, 3, 6, 9가 들어가는 숫자는 숫자를 말하는 대신 3, 6, 9의 개수만큼 손뼉을 쳐야 한다.
+숫자 number가 매개변수로 주어질 때, 1부터 number까지 손뼉을 몇 번 쳐야 하는지 횟수를 */
 module.exports = problem3;
