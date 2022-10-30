c998a87a0c6345bf994f7b0162d331bd7c86e5a3
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-30 16:00:51 +0900

    dosc<problem2>: Define features list

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,12 @@
+/* 
+ * Features
+ * 1. 연속하는 중복 문자들을 삭제한 결과를 return
+ *  
+ * Exceptions
+ * 1. cryptogram은 길이가 1 이상 1000 이하인 문자열이다
+ * 2. cryptogram은 알파벳 소문자로만 이루어져 있다.
+ */
+
 function problem2(cryptogram) {
   let answer;
   return answer;
