5ebebdf78481c5976b7c5b91dc804ccb50f47cea
nerdchanii <nerdchanii@gmail.com>
2022-10-30 13:56:10 +0900

    feat: [PROBLEM5] 메인로직 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
   const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return map((unit) => {
+    let [res, quantity] = caculate(money, unit);
+    money = res;
+    return quantity;
+  }, MONETARY_UNIT)
   function caculate(money, unit) {
     return [money % unit, Math.floor(money / unit)];
   }
