98f1865fdc25a4f614176f51f985b2e55fbf84dd
Hwang Seong Taek <hst007@naver.com>
2022-10-30 22:54:46 +0900

    feat: [problem7] user의 친구 목록 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,10 @@
 function problem7(user, friends, visitors) {
   const userFriends = [];
   const recommendFriends = new Map();
-  
+
+  friends.forEach(item => {
+    if(item.includes(user)) userFriends.push(item.join('').replace(user, ''))
+  });
 }
 
 module.exports = problem7;
