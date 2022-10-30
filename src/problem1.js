e24d9b13f0ef6bade5abd6a01bb47e6d6e5802c8
random-olive <104327907+random-olive@users.noreply.github.com>
2022-10-30 12:06:09 +0900

    prob4: init

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,11 @@
 function problem1(pobi, crong) {
   var answer;
-  if (pobi[1] !== pobi[0] + 1 || crong[1] !== crong[0] + 1) {
+  if (
+    pobi[1] !== pobi[0] + 1 ||
+    crong[1] !== crong[0] + 1 ||
+    pobi[0] % 2 === 0 ||
+    crong[0] % 2 === 0
+  ) {
     return -1;
   }
 
--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,10 @@
 function problem4(word) {
   var answer;
+  // A:65~Z:90 // a:97~z:122 // +25
+  let s='a'
+  let ss=s.charCodeAt(0) //str -> uni
+  let sss=String.fromCharCode(ss+25) //uni -> str
+
   return answer;
 }
 
