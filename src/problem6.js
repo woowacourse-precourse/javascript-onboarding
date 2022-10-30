794ba45d4d3fe6a21f3bed98964fcc8a9e8788f8
semnil5202 <semnil5202@gmail.com>
2022-10-31 01:03:37 +0900

    [문제 6번] feat: 닉네임을 연속된 두 글자로 나누고 중복된 글자만 추출하기
    입력 받은 폼에서 닉네임 부분을 반복문으로 순회하며 두 글자 단위로 나누어 배열에 담는다.
    배열에 있는 기존의 두 글자와 새롭게 나눠진 두 글자를 비교하며 중복 글자를 찾아 또다른 배열에 담는다.
    중복 글자를 담은 배열만 반환한다.

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,18 @@ function problem6(forms) {
   return answer;
 }
 
+function separateTwoWord(array2D) {
+  const nicknames = array2D;
+  const TwoWords = [];
+  const duplicateIndex = [];
+  for (let i = 0; i < nicknames.length; i++) {
+    for (let j = 0; j < nicknames[i][1].length - 1; j++) {
+      if (TwoWords.includes(nicknames[i][1].substr(j, 2))) {
+        duplicateIndex.push(nicknames[i][1].substr(j, 2));
+      } else TwoWords.push(nicknames[i][1].substr(j, 2));
+    }
+  }
+  return Array.from(new Set(duplicateIndex));
+}
+
 module.exports = problem6;
