5fa8999191577f7dbeb58653af5bf863dce5c616
yeonsuBeak <soothbugger@gmail.com>
2022-10-31 06:32:39 +0900

    feat(problem7): 사용자 점수표 생성 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,39 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const usersList = getListOfUserAndFriends(friends);
+}
+
+function getListOfUserAndFriends(userFriends) {
+  let member = new Array().fill(0);
+  let scoreTable = [member];
+
+  userFriends.forEach((twoFriends) => {
+    twoFriends.forEach((friend) => {
+      member = {};
+
+      let declaredMember = false;
+      scoreTable.forEach((scoreList) => {
+        if (Object.values(scoreList).includes(friend)) {
+          declaredMember = true;
+        }
+      });
+
+      if (declaredMember === false) {
+        member.name = friend;
+        member.score = 0;
+        scoreTable.push(member);
+      }
+    });
+  });
+
+  scoreTable.shift();
+
+  return scoreTable;
 }
 
 module.exports = problem7;
+
+andole = 10 + 10;
+jun = 10 + 10;
+donut = 1;
+shakevan = 1;
+bedi = 1 + 1 + 1;
