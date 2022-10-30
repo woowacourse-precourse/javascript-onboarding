600e935a13a0fd569131bbef35bc5e1701b5ff9b
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-10-30 21:40:41 +0900

    feat: [P6] 전체배열에서 중복 배열과 겹치는 크루의 email을 리턴하는 함수 추가

--- src/problem6.js
+++ src/problem6.js
@@ -35,3 +35,20 @@ function pushOverlapArray(array) {
   });
   return overlapArray;
 }
+function problem6(forms) {
+  const twoCharNameArr = cutTwoCharArray(forms);
+  const overlapArray = pushOverlapArray(twoCharNameArr);
+  let answer = [];
+
+  forms.forEach((nameAndEmail) => {
+    let i = 0;
+
+    for (; i < nameAndEmail.length; i++) {
+      if (overlapArray.includes(nameAndEmail[1].slice(i, i + 2))) {
+        answer.push(nameAndEmail[0]); //전체 배열에서 중복된 이름이 있는 지 확인 후 있다면 그 이메일을 반환하는 함수
+      }
+    }
+  });
+
+  return answer.sort();
+}
