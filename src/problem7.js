88d5273a57b61c4e7adade4c9c760b63e503b878
wang-yurin <king_1994@naver.com>
2022-10-31 01:40:37 +0900

    Feat: problem7 user와 친구인 요소 찾는 코드 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const scoreObj = {};
+
+  const userFriendList = friends
+    .filter((item) => item.includes(user))
+    .map((value) => {
+      if (value[0] === user) {
+        return value[1];
+      } else return value[0];
+    });
 }
 
 module.exports = problem7;
