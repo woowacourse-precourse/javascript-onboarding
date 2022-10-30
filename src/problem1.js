20f0228b79c93006fa45b26c77f729affdca1c02
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-30 20:20:30 +0900

    feat: 1번 - 최대값 구하는 메서드 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,16 @@
 function problem1(pobi, crong) {
 
 }
+
+// 최대값 구하는 함수
+function largeNumber(n) {
+  let arr = []
+  for (i of n) {
+    const num = i.toString().split('').map(x => parseInt(x));
+    arr.push(num.reduce((prev, cur) => prev + cur, 0));
+    arr.push(num.reduce((prev, cur) => prev * cur, 1));
+  }
+  return Math.max(...arr);
+}
+
 module.exports = problem1;
