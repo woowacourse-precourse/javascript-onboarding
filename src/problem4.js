fb39abb26b9b98ddb95ce178c8f53e87b85c8fd6
cham-min <hymn9613@gmail.com>
2022-10-30 18:27:10 +0900

    feat: 문제4 - 알파벳을 반대로 변환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,11 @@ function problem4(word) {
   return answer;
 }
 
+function inverseAlphabet(ascii) {
+  if (ascii >= 65 && ascii <= 90) return String.fromCharCode(65 + 90 - ascii);
+  else if (ascii >= 97 && ascii <= 122)
+    return String.fromCharCode(97 + 122 - ascii);
+  else return String.fromCharCode(ascii);
+}
+
 module.exports = problem4;
