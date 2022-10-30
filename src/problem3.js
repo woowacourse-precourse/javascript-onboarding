70fbb3389ef96ff732482bd463dc8a3de2a24c2d
myung-j-p <jmpack97@gmail.com>
2022-10-30 17:32:00 +0900

    문제4_인덱싱에 필요한 리스트 작성, 입력된 word를 한글자씩 분해하여 글자마다 리스트에서 인덱싱하여 대응하는 값 찾는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,17 @@
 function problem3(number) {
   var answer = 0;
   let arr = Array.from({length: number}, (v, i) => i+1);
+  let ind = 0;
 
+  for(i in arr){
+    let v = Array.from(String(arr[ind]));
+    for (i in v){
+      if (v[i] == '3' || v[i] == '6' || v[i] == '9'){
+        answer++;
+      };
+    }
+    ind++;
+  };
 
   return answer;
 }
--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
   var answer;
+  const arr = 'abcdefghijklmnopqrstuvwxyz';
+  const alphabetArr = [...arr];
+  let words = [...word];
+
+  let ind = 0;
+
+  alphabetArr[ind],
+  alphabetArr[alphabetArr.length - ind -1]
+
   return answer;
 }
+
+problem4('I love you')
 
 module.exports = problem4;
