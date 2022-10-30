47848b7f2b766fd738a26bdc6398661cbe857754
afds4567 <afds4567@gmail.com>
2022-10-30 23:46:56 +0900

    feat(problem3): 전체 로직 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  const pattern = /[3|6|9]/g;
+  for (let i = 1; i <= number; i++) {
+    const numCount = ("" + i).match(pattern);
+    if (numCount !== null) answer += numCount.length;
+  }
   return answer;
 }
 
