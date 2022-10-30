5d1362fd60d3d2b0cfe8e3ea78d98d91760a6a3c
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-30 20:14:34 +0900

    feat: [P3] 1부터 number까지 손뼉을 치는 횟수 합산

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
+// 포함되는 수를 지정해주기 위한 상수
+const CLAP_INCLUDE_NUMBERS = [3, 6, 9];
+
+// 1부터 number까지 손뼉을 치는 횟수 구하기
 function problem3(number) {
-  var answer;
-  return answer;
+  let clapCnt = 0;
+  let numArr;
+  for(let i = 1; i<=number; i++) {
+    numArr = String(i).split('').map(x=>+x);
+    numArr.map(x => {
+      if(CLAP_INCLUDE_NUMBERS.includes(x)) clapCnt++;
+    })
+  }
+  return clapCnt;
 }
 
 module.exports = problem3;
