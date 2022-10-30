2c10475ecc4cbfa61ac2c14332b3bd2c8900ff95
banma1234 <banma1234@gmail.com>
2022-10-30 12:18:53 +0900

    problem_3 : 예외처리 함수

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,24 @@ function problem3(number) {
   return answer;
 }
 
+// 예외처리 및 숫자 쪼개는 함수
+function errorHandler(num) {
+  // 숫자들을 저장할 배열
+  var arr;
+
+  if (num > 10000) {
+    // 10000보다 클 경우 10000까지의 숫자 저장
+    for (i=1; i<10001; i++){
+      arr.append(i);
+    }
+  }
+  else{
+    for (i=1; i<num+1; i++){
+      arr.append(i);
+    }
+  }
+
+  return arr;
+}
+
 module.exports = problem3;
