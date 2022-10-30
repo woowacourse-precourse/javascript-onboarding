71891370ffeb51fa0d9400d953e2fa63a4af9d30
KPGY <yuchw8@korea.ac.kr>
2022-10-30 20:48:54 +0900

    가장큰값을 리턴하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,17 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+//왼쪽 오른쪽중에 가장큰값을 리턴하는 함수
+function Maxnum(page) {
+  let max=[];
+  for(i=0; i<2; i++)
+  {
+    max.push(String(page[i]).split('').map(Number).reduce((a,b)=> (a*b)));
+    max.push(String(page[i]).split('').map(Number).reduce((a,b)=> (a+b)));
+  };
+  const maxnum = Math.max.apply(null, max);
+  return maxnum;
+  
+}
+
 module.exports = problem1;
