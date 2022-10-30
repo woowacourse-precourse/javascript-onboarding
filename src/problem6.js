28f778ca2c493066594cfde73a2f134e87823859
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-30 21:40:01 +0900

    feat: [problem6] - 크루들의 닉네임을 두 글자씩 끊어 저장한 배열을 반환하는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,19 @@
 function problem6(forms) {
   var answer;
+  var nicknameSplit = new Map();
+
+  for (var i = 0; i < forms.length; i++) {}
   return answer;
+}
+
+function getNameSplitArr(nickname) {
+  var arr = [];
+
+  for (var i = 0; i < nickname.length - 1; i++) {
+    arr.push(nickname.substr(i, 2));
+  }
+
+  return arr;
 }
 
 module.exports = problem6;
