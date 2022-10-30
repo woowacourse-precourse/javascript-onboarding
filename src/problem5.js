7d2fc2d48614251fe421fdab128f332cdfdd5fe7
nailen1 <nietsnie1@gmail.com>
2022-10-31 00:15:20 +0900

    Solve problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,35 @@
+class Cash {
+  constructor(money) {
+    this.input = money;
+    const cashes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+    let pocket = cashes.map(x => {
+      let n = parseInt(money / x)
+      money = money - x * n;
+      return n
+    })
+    this.result = pocket;
+  }
+  get valid() {
+    if (this.input >= 1 && this.input <= 1000000) {
+      return true
+    }
+    else {
+      return false
+    }
+  }
+}
+
 function problem5(money) {
   var answer;
+  let cashInfo = new Cash(money);
+  // console.log(cashInfo)
+
+  if (cashInfo.valid) {
+    answer = cashInfo.result;
+  }
+  else {
+    answer = 'invalid input';
+  }
   return answer;
 }
 
