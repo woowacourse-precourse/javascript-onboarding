8cc2613f9dd723b2b9637a0a95f1cf67424d4c3f
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-10-30 21:25:11 +0900

    Feat: 1번 예외처리를 제외한 기능 구현 완료(case1,case2 통과) & Feat: 구현 전 기능 목록 리드미 파일 작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,43 @@
 function problem1(pobi, crong) {
   var answer;
+  let pobi_max=Math.max(
+    Math.max(sumDigits(pobi[0]),mulDigits(pobi[0])),
+    Math.max(sumDigits(pobi[1]),mulDigits(pobi[1])));
+
+  let crong_max=Math.max(
+    Math.max(sumDigits(crong[0]),mulDigits(crong[0])),
+    Math.max(sumDigits(crong[1]),mulDigits(crong[1])));
+
+  if (pobi_max>crong_max){
+    answer=1;
+  }
+  else if (pobi_max===crong_max){
+    answer=0;
+  }
+  else{
+    answer=-1
+  }
+
   return answer;
+}
+
+function sumDigits(num){
+  var sum=0;
+  while(num>0){
+    sum+=num%10;
+    num=parseInt(num/10);
+  }
+  return sum
+}
+
+
+function mulDigits(num){
+  var mul=1
+  while(num>0){
+    mul*=num%10;
+    num=parseInt(num/10);
+  }
+  return mul
 }
 
 module.exports = problem1;
