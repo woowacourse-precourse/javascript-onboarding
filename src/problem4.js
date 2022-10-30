b45d871a5275d1d1d34642bcc927b96501c3a658
박겸영 <yong_drg@naver.com>
2022-10-30 21:41:00 +0900

    feat: 문제 4 반대로 변환한 문자열 구하기 - 기능 3: 엄마 말씀의 각 문자가 대문자 배열이나 소문자 배열에 포함되면 반대 순서 문자로 변환해서 결과 문자열에 추가 - 둘 중 어떤 배열에도 포함되지 않으면 변환하지 않고 결과 문자열에 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,19 @@
 function problem4(word) {
   const uppercases = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
   const lowercases = uppercases.map((letter) => letter.toLowerCase());
+  let result = '';
 
   word.split('').forEach((letter) => {
     const indexOfUppercase = uppercases.indexOf(letter);
     const indexOfLowercase = lowercases.indexOf(letter);
+    const length = uppercases.length - 1;
+
+    if (indexOfUppercase !== -1) result += uppercases[length - indexOfUppercase];
+    else if (indexOfLowercase !== -1) result += lowercases[length - indexOfLowercase];
+    else result += letter;
   });
+
+  return result;
 }
 
 module.exports = problem4;
