57e43478afde63402dd2be0ca0220aa708f6c01e
DongWooKim97 <nanaukim@naver.com>
2022-10-31 00:21:30 +0900

    feat(P6): 배열을 순회하며 중복값 기준 설정 후 제거 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,11 +1,25 @@
 function problem6(forms) {
   const emailList = forms.sort((a, b) => a[1].length - b[1].length).map(i => i[0]) // 닉네임의 길이로 sort한 후 이에 대해 이메일 리스트 따로 저장
   const nicknameList = forms.sort((a, b) => a[1].length - b[1].length).map(i => i[1]); // 닉네임의 길이로 sort한 후 이에 대해 닉네임 리스트 따로 저장
-
+  
   if(!inputValidCheck(emailList, nicknameList)) {
     return 'error'
   }
-  return true
+  
+  const duplicateCrewEmail = [];
+  const leastDuplicateWord = 2;
+
+  for (let i = 0; i < nicknameList.length; i++) { // 모듈화 가능
+    for (let j = 0; j <= nicknameList[i].length - 2; j++) { // 모듈화 가능
+      const headSliceWord = nicknameList[i].slice(0 + j, leastDuplicateWord + j);
+      for (let k = 0; k < nicknameList.length; k++) { // 모듈화 가능
+        // i !==k를 걸어준 이유는 자기 자신을 자른 문장은 검사할 필요 X -> 오히려 검사하게 되면 
+        // 푸시 할 수도 있기 때문에!!
+        if ((i !== k) && (nicknameList[k].includes(headSliceWord))) duplicateCrewEmail.push(emailList[k]) // 모듈화 가능
+      }
+    }
+  }
+  return [...new Set(duplicateCrewEmail)].sort();
 
 }
 
