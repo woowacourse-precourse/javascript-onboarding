469b7ca07a060b73a60adf103b5043be3b03d8f0
kaori-killer <sksbthwjd@daum.net>
2022-10-30 16:38:34 +0900

    feat: 친구 관계를 이용해 양방향 그래프 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let friendGraph = {};
+
+  friends.forEach(friendArray => {
+    const A = friendArray[0];
+    const B = friendArray[1];
+
+    if(!(A in friendGraph)) { friendGraph[A] = []; }
+    if(!(B in friendGraph)) { friendGraph[B] = []; }
+    friendGraph[A].push(B);
+    friendGraph[B].push(A);
+    
+  });
 }
+
+problem7("mrko", 	[["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]], ["bedi", "bedi", "donut", "bedi", "shakevan"], ["andole", "jun", "bedi"])
 
 module.exports = problem7;
