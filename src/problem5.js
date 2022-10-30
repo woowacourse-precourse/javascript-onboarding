f7024aa4ff9ea8100fbf55be5ba6a0d68f4189fe
sumin <csumin008255@gmail.com>
2022-10-30 21:51:03 +0900

    problem5_기능목록작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,11 @@
+/* 기능목록
+* 1. 금액 배열 만들기
+* 2. money를 배열 요소대로 나누기
+* 3. 몫은 result 배열에 추가, 나머지는 money에 대입하기
+* 4. 결과 배열 출력
+* 5. 코드 정리
+*/
+
 function problem5(money) {
   var answer;
   return answer;
