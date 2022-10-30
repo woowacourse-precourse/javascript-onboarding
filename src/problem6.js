d860bee1740cbc7c5c83945c7010f314b1ce2a1c
froggy1014 <clcl6084@gmail.com>
2022-10-30 17:01:31 +0900

    feat[6]: 중첩단어 감지하는 기능
    1. map 객체에서 2개 이상 존재하는 단어만 골라서
    2. 모든 닉네임과 대조하면서 중첩된 닉네임의 메일을 Answer 배열에 쏴준다.
    3. checkArray에 해당 인덱스 체크해주면서 중첩되는 일없게함

--- src/problem6.js
+++ src/problem6.js
@@ -2,6 +2,7 @@ function problem6(forms) {
   
   const checkArray = Array(forms.length).fill(false);
   const map = new Map();
+  const Answer = [];  
 
   for ( let i = 0 ; i < checkArray.length ; i++) {
     for( let j = 1 ; j <= forms[i][1].length - 1; j++ ) {
@@ -11,7 +12,19 @@ function problem6(forms) {
       else map.set(forms[i][1][j - 1]+forms[i][1][j], (map.get(forms[i][1][j - 1]+forms[i][1][j])||0)+1);  
     }
   }
-  console.log(map);
+
+  for(let [key,val] of map){
+    if(1 < val){
+      for( let i = 0 ; i < forms.length; i++){
+        if(forms[i][1].includes(key) && !checkArray[i]) {
+          Answer.push(forms[i][0]);
+          checkArray[i] = true;
+        } 
+      }
+    }  
+  }
+  
+  return Answer;
 }
 
 console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
