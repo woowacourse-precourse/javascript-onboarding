c8c7d19b2ede90f4b385614913b394c509949bc3
sunub <bsc5672@gmail.com>
2022-10-31 02:32:24 +0900

    I tried to refectorize the last code.

--- src/problem7.js
+++ src/problem7.js
@@ -1,46 +1,3 @@
-function problem7(user, friends, visitors) {
-  this.adjacent = new AdjacentList();
-  for (let i = 0; i < friends.length; i++) {
-    this.adjacent.add(friends[i][0], friends[i][1]);
-    this.adjacent.add(friends[i][1], friends[i][0]);
-  }
-  this.adjacent.sort();
-
-  const friendMap = this.adjacent.adjacentList;
-  const notInvolved = [user, ...friendMap.get(user)];
-
-  const recommendList = {
-    10: [],
-    1: [],
-  };
-
-  for (let i = 0; i < notInvolved.length; i++) {
-    const connected = friendMap.get(notInvolved[i]);
-    for (let j = 0; j < connected.length; j++) {
-      if (!notInvolved.includes(connected[j])) {
-        notInvolved.push(connected[j]);
-        if (!recommendList[10].includes(connected[j])) {
-          recommendList[10].push(connected[j]);
-        }
-      }
-    }
-  }
-
-  for (let i = 0; i < visitors.length; i++) {
-    if (!notInvolved.includes(visitors[i])) {
-      notInvolved.push(visitors[i]);
-      if (!recommendList[1].includes(visitors[i])) {
-        recommendList[1].push(visitors[i]);
-      }
-    }
-  }
-  recommendList[10].sort((a, b) => a[0].charCodeAt() - b[0].charCodeAt());
-  recommendList[1].sort((a, b) => a[0].charCodeAt() - b[0].charCodeAt());
-
-  const answer = [...recommendList[10], ...recommendList[1]];
-  return answer;
-}
-
 class AdjacentList {
   list = [];
   constructor() {}
@@ -70,6 +27,68 @@ class AdjacentList {
       this.adjacentList.set(key, value);
     }
   }
+}
+
+function Sort(input) {
+  input.sort((a, b) => a[0].charCodeAt() - b[0].charCodeAt());
+  for (let i = 0; i < input.length - 1; i++) {
+    if (input[i][0] === input[i + 1][0]) {
+      const length = input[i].length > input[i + 1].length ? input[i + 1].length : input[i].length;
+      for (let j = 1; j < length; j++) {
+        if (input[i][j] > input[i + 1][j]) {
+          [input[i], input[i + 1]] = [input[i + 1], input[i]];
+          break;
+        }
+      }
+    }
+  }
+  return input;
+}
+
+// 문제의 조건 중 오름차순으로 정렬한다는 조건을 빼먹어서 정리하였고
+// 기존에 this를 사용하여 adjacent를 사용한 문제를 수정하였습니다.
+
+function problem7(user, friends, visitors) {
+  const adjacent = new AdjacentList();
+  for (let i = 0; i < friends.length; i++) {
+    adjacent.add(friends[i][0], friends[i][1]);
+    adjacent.add(friends[i][1], friends[i][0]);
+  }
+  adjacent.sort();
+
+  const friendMap = adjacent.adjacentList;
+  const notInvolved = [user, ...friendMap.get(user)];
+
+  const recommendList = {
+    10: [],
+    1: [],
+  };
+
+  for (let i = 0; i < notInvolved.length; i++) {
+    const connected = friendMap.get(notInvolved[i]);
+    for (let j = 0; j < connected.length; j++) {
+      if (!notInvolved.includes(connected[j])) {
+        notInvolved.push(connected[j]);
+        if (!recommendList[10].includes(connected[j])) {
+          recommendList[10].push(connected[j]);
+        }
+      }
+    }
+  }
+
+  for (let i = 0; i < visitors.length; i++) {
+    if (!notInvolved.includes(visitors[i])) {
+      notInvolved.push(visitors[i]);
+      if (!recommendList[1].includes(visitors[i])) {
+        recommendList[1].push(visitors[i]);
+      }
+    }
+  }
+  const tenScore = Sort(recommendList[10]);
+  const oneScore = Sort(recommendList[1]);
+
+  const answer = [...tenScore, ...oneScore];
+  return answer;
 }
 
 module.exports = problem7;
