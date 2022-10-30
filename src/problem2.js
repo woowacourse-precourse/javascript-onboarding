e826a8b38e396b82517e890b4fcb07c3c68c5857
Minhye Kang <alsgp9898@naver.com>
2022-10-30 14:58:02 +0900

    feat(PROBLEM2): 연속하는 중복 문자가 없을 때까지 중복 문자들을 삭제하고 결과 문자열을 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,13 +1,17 @@
 function problem2(cryptogram) {
-
+  while (1) {
+    cryptogram = removeDuplicate(cryptogram);
+    if (isNotDuplicate(cryptogram)) break;
+  }
+  return cryptogram;
 }
 
 const removeDuplicate = (cryptogram) => {
-  return cryptogram.replace(/([a-z])\1+/g, "")
-}
+  return cryptogram.replace(/([a-z])\1+/g, "");
+};
 
 const isNotDuplicate = (cryptogram) => {
-  return !(/([a-z])\1+/g.test(cryptogram))
-}
+  return !/([a-z])\1+/g.test(cryptogram);
+};
 
 module.exports = problem2;
