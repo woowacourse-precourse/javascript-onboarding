d043cb94b4d1cbaa60c8f1274e474e134650534f
ryuna <anottrx@gmail.com>
2022-10-30 15:30:24 +0900

    feat(7번): 전체 아이디 목록을 만드는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,23 @@
 function problem7(user, friends, visitors) {
+  const idList = getIdSet(user, friends, visitors);
   var answer;
   return answer;
 }
+
+/**
+ * user, friends, visitors에 등장하는 모든 id를 중복없이 저장한다
+ * @param {string} user - 사용자 아이디
+ * @param {Array<Array<string, string>>} friends - 친구 관계를 담은 이차원 배열
+ * @param {Array<string>} visitors - 사용자 타임 라인 방문 기록
+ * @returns {Array<string>} 모든 id 리스트
+ */
+const getIdSet = (user, friends, visitors) => {
+  const idSet = new Set(visitors);
+  idSet.add(user);
+  friends.forEach((friend) => {
+    idSet.add(friend[0]).add(friend[1]);
+  });
+  return [...idSet];
+};
 
 module.exports = problem7;
