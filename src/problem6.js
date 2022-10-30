e232a492a8e06159069d5a722e459c3d8a8b47ca
seongminn <berry9901@gmail.com>
2022-10-30 23:40:48 +0900

    feat(problem6): 유저 닉네임을 2글자씩 나누는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,23 @@
+function getSlicedName(arr) {
+  const slicedName = [];
+
+  arr.forEach((name) => {
+    let alphaArr = [...name[1]];
+
+    alphaArr.forEach((alpha, idx) => {
+      if (idx === alphaArr.length - 1) return;
+
+      slicedName.push(alpha + alphaArr[idx + 1]);
+    });
+  });
+
+  return slicedName;
+}
+
 function problem6(forms) {
-  var answer;
+  const answer = [];
+  const slicedName = getSlicedName(forms);
+
   return answer;
 }
 
