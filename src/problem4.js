8dbd7831eab048585acb3be2ac1f86a653c74a98
2wndrhs <2wndrhs@gmail.com>
2022-10-29 23:26:59 -0700

    feat(PROBLEM4): 유니코드만 반대로 변환하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -6,16 +6,27 @@ function convertUnicode(regex, splitedWord) {
   return result;
 }
 
+// 유니코드만 반대로 변환하는 기능
+function reverseUnicode(word) {
+  const result = word.map((char) =>
+    typeof char === "number" ? 155 - char : char
+  );
+  return result;
+}
 // solution
 function solution(word) {
   const regex = /[a-zA-z]/;
   const splitedWord = word.split("");
   const unicodeWord = convertUnicode(regex, splitedWord);
+  const reversedWord = reverseUnicode(unicodeWord);
+  console.log(reversedWord);
 }
 
 function problem4(word) {
   var answer;
   return answer;
 }
+
+solution("ab cd");
 
 module.exports = problem4;
