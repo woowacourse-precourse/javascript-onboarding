ec01707f742ad2671774c2a6bc04692a2a96a5bc
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-30 20:05:57 +0900

    feat: [P2] 연속하는 중복 문자들을 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
+// 연속하는 중복 문자들을 삭제
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const cryptoStr = cryptogram.split('');
+  const stack = [];
+  let duplicatedChar = '';
+
+  for(let i=0; i<cryptoStr.length; i++) {
+    if(duplicatedChar === cryptoStr[i]) {
+      continue;
+    }
+    else if(stack[stack.length - 1] !== cryptoStr[i]) {
+      stack.push(cryptoStr[i]);
+    }
+    else if(stack[stack.length - 1] === cryptoStr[i]) {
+      duplicatedChar = stack.pop();
+    }
+  }
+
+  return stack.join('');
 }
 
 module.exports = problem2;
