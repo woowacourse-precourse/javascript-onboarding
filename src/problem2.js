dcc32845051fa404d78f799b0c93eedeb305e72f
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-10-30 23:51:54 +0900

    feat: 문제 2 -  연속하는 중복 문자가 3개 이상일 때 삭제 기능 및 테스트 추가

--- src/problem2.js
+++ src/problem2.js
@@ -6,10 +6,17 @@ function problem2(cryptogram) {
   for (let i = 1; i < cryptogram.length; i++) {
     let prev = result[result.length - 1];
     let cur = cryptogram[i];
+    let next = cryptogram[i + 1];
 
     // 이전 값과 현재 값이 같으면 이전 값을 제거하고, 현재 값을 추가
     if (prev === cur) {
       result.pop();
+      result.push(cur);
+
+      // 추가한 현재 값과 다음 값이 다르면 현재 값을 제거 (연속한 중복값이 2개일 경우)
+      if (prev !== next) {
+        result.pop();
+      }
     }
     // 이전 값과 현재 값이 다를 경우, 현재 값을 추가
     else {
