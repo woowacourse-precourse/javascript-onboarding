09177e56492582525feb73d42f87433558ad96ac
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-10-30 22:48:26 +0900

    Feat: 2번 기능 구현 완료 & Fix: 실 기능 구현에 따른 기능 목록 리드미 파일 수정 및 개선

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 암호 문자열의 문자들을 분리하여 배열로 전환
+  var cry = [...cryptogram];
+  var answer=[cry[0]];
+  for (let i=1;i<cry.length;i++){
+    var top = answer[answer.length-1];
+    if (top===cry[i]){
+        answer.splice(answer.length-1,1);
+    }
+    else{
+      answer.push(cry[i]);
+    }
+  }
+  const answer_str = answer.join('')
+  return answer_str;
 }
 
 module.exports = problem2;
