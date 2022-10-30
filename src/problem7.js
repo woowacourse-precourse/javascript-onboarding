41eeafded7adf12d5e0fa779724bcdbf9ba4016d
minjaeKim <smallkdb@gmail.com>
2022-10-31 02:08:32 +0900

    feat: [Problem7] Add getUserFriends function
    유저의 친구들을 구하는 getUserFriends 함수를 추가하였습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
+function getUserFriends(user, friends) {
+  const userFriends = [];
+
+  friends.forEach(([firstPerson, secondPerson]) => {
+    if(firstPerson === user) userFriends.push(secondPerson);
+    if(secondPerson === user) userFriends.push(firstPerson);
+  });
+
+  return userFriends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  const userFriends = getUserFriends(user, friends);
   return answer;
 }
 
