9d549828d74d33935274d7bba7bb308596410e42
HWI <dlwognl99@naver.com>
2022-10-30 20:08:24 +0900

    문제3_feat: 예외처리 및 메인 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  // number는 1이상 10000이하인 자연수이다.
+  if (number < 1 || number > 10000 || Number.isInteger(number) !== true) {
+    return -1;
+  }
+
+  let clapCount = 0;
+
+  for (let i = 0; i <= number; i++) {
+    clapCount += getClapCount(i);
+  }
+  return clapCount;
 }
 
 module.exports = problem3;
