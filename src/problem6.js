a8efa6005fe123892dc1a665a682dccd6917b6fc
kim-limit <rlawogks2468@naver.com>
2022-10-30 16:07:30 +0900

    refact: '중복피하기 위해 break추가'

--- src/problem6.js
+++ src/problem6.js
@@ -7,6 +7,7 @@ function problem6(forms) {
       for (let j = 0; j < forms[i][1].length - 1; j++) {
         if (forms[z][1].includes(forms[i][1].substr(j, j + 2))) {
           result.push(forms[z][0]);
+          break;
         }
       }
     }
