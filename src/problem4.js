7c26623cf5e28cc4b8da1bdfdbf20820f11d6628
random-olive <104327907+random-olive@users.noreply.github.com>
2022-10-30 19:23:15 +0900

    word to uni

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,18 @@
 function problem4(word) {
-  var answer;
-  // A:65~Z:90 // a:97~z:122 // +25
-  let s='a'
-  let ss=s.charCodeAt(0) //str -> uni
-  let sss=String.fromCharCode(ss+25) //uni -> str
+  let s = "";
+  let ss = [];
+  let sss = [];
 
-  return answer;
+  for (let i = 0; i < word.length; i++) {
+    s+= word[i];
+    ss.push(s.charCodeAt(i)); //[65, 98, 32, ..]
+   
+  }
+  // let s='a' //word[i]
+  // let ss = s.charCodeAt(0); //str -> uni
+  // let sss = String.fromCharCode(ss + 25); //uni -> str
+
+  return sss.join('')
 }
 
 module.exports = problem4;
