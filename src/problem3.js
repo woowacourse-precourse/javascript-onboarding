886c17d34ed49a43a976eab04858b7bb54b2bfc4
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 19:47:13 +0900

    refactor(problem3): 변수 선언 시 `let`과 `const` 키워드 사용

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,9 @@
 function problem3(number) {
   var answer = 0;
-  var regExp369 = /[369]/g;
-  var array369;
+  const regExp369 = /[369]/g;
+  let array369;
 
-  for (var i = 1; i <= number; ++i) {
+  for (let i = 1; i <= number; ++i) {
     if (array369 = String(i).match(regExp369)) {
       answer += array369.length;
     }
