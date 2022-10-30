9839c107e205f8b32dfa4e83c40107d33c0a3943
kimeuns <dmstn2006@gmail.com>
2022-10-30 18:48:42 +0900

    문제7 추천친구 객체 생성(함께아는 친구 기준)

--- src/problem7.js
+++ src/problem7.js
@@ -1,23 +1,36 @@
 function problem7(user, friends, visitors) {
   var answer;
-  let result = {}
   const friendList = makeFriendList(user,friends)
-  let userFriend = friendList[user]
-  
+  const userFriend = friendList[user]
+  const recommendObj= recommendFriend(userFriend,friendList)
+
+
   return answer;
 }
 
 function makeFriendList(friends){
   return friends.reduce(function(obj,item){
     for(let i in item){
-        if(!obj[item[i]]){
-            obj[item[i]]=[item[1-i]]
-        }else{
-            obj[item[i]].push(item[1-i])
-        }
+      if(!obj[item[i]]){
+          obj[item[i]]=[item[1-i]]
+      }else{
+          obj[item[i]].push(item[1-i])
+      }
     }
     return obj
   },{})
+}
+
+function recommendFriend(userFriend, friendList){
+  let recommendObj = {}
+  for(let name of Object.keys(friendList)){
+    //user 제외
+    if(name!==user){
+      let sharedFriend =userFriend.filter(friend=>friendList[name].includes(friend))
+      sharedFriend.length!==0?result[name]=sharedFriend.length*10:null
+    }
+  }
+  return recommendObj
 }
 
 
