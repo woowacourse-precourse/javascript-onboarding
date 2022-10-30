b4c2d234b963a74b8ce964e2bf4228f0d413cf29
lsy20140 <lsy20140@naver.com>
2022-10-30 20:19:34 +0900

    feat: [문제6] 'email.com' 도메인인 크루만 forms에 저장

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,11 @@
+function checkEmail(crew, idx, forms){
+  if(!crew[0].includes("email.com")){
+    forms.splice(idx, 1);
+  }
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  forms.forEach(checkEmail); 
 }
 
 module.exports = problem6;
