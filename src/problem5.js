4482261cda0d5b334ab6b0c5ced828fb106f00cb
Azamman327 <wjsdbdud327@naver.com>
2022-10-30 20:20:14 +0900

    Docs: [P5] 기능 목록 정리

--- src/problem5.js
+++ src/problem5.js
@@ -4,3 +4,9 @@ function problem5(money) {
 }
 
 module.exports = problem5;
+
+/*
+1. 매개변수로 들어온 돈이 몇 개 변환될 수 있는지 count하는 함수
+2. 오만 원권 ~ 일원 동전 값이 들어있는 배열을 값이 큰 순서대로 1의 함수에 대입
+3. 결과 배열(withdraw)에 반환값 저장 후 return
+*/
