9134155201ecb057748e4a2dba4c35629f8acdbb
wonkyung <mwk0725@gmail.com>
2022-10-30 21:45:32 +0900

    style: {} 삭제, a -> name 변경

--- src/problem7.js
+++ src/problem7.js
@@ -1,9 +1,14 @@
 function problem7(user, friends, visitors) {
   const userName = user;
-  const allUserList = friends.flat().filter(arr => arr !== userName);
-  const delDupUserList = [...new Set([...new Set(allUserList), ...new Set(visitors)])].filter(
-    arr => arr !== userName
-  );
+  const friendsList = friends;
+  const visitorsList = visitors.filter(arr => arr !== userName);
+
+  // 내(user) 친구 구하기
+  let myFriends = [];
+  for (let a of friends) {
+    if (a[0] === userName) myFriends.push(a[1]);
+    if (a[1] === userName) myFriends.push(a[0]);
+  }
 
   // 점수 리스트
   const userPointList = Object.assign({}, ...delDupUserList.map((key, i) => ({[key]: 0})));
