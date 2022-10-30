ec70e2cd1bf908526d430443ecd8e1f3e6d43a58
Serin-Kim <serin9864@gmail.com>
2022-10-30 15:50:31 +0900

    refactor: PROBLEM5 변수명, 메서드 구조 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,11 @@
-const coinList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  let answer = [];
-  let leftMoney = money;
-
-  for (coin of coinList) {
-    if (leftMoney >= coin) {
-      const usedCoinCount = Math.floor(leftMoney / coin);
-      answer.push(usedCoinCount);
-      leftMoney = leftMoney - coin * usedCoinCount;
-    } else answer.push(0);
-  }
-
+  const answer = MONETARY_UNIT.map((monetaryUnit) => {
+    const count = Math.floor(money / monetaryUnit); // TODO: 변수명 변경
+    money = money % monetaryUnit;
+    return count;
+  });
   return answer;
 }
 
