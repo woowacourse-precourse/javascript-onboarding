84547fd96e827d217e939c986d3adcba6188fdf7
mjmj0217 <mjmj0217@naver.com>
2022-10-30 20:43:00 +0900

    feat: problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,23 @@
+function returnCount(money, coin) {
+  console.log(money, coin, Math.floor(money / coin));
+  return Math.floor(money / coin);
+}
+
 function problem5(money) {
-  var answer;
+  var answer = [];
+
+  const coinArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (coin of coinArray) {
+    if (money <= 0) {
+      answer.push(0);
+      continue;
+    }
+
+    answer.push(returnCount(money, coin));
+    money = money - coin * answer[answer.length - 1];
+  }
+
   return answer;
 }
 
