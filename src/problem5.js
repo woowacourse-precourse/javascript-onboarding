61b512df5aa273df16242fa7ffcbe4a958e524e4
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-30 20:39:45 +0900

    문제5) feat: 화폐금액 배열을 forEach로 순회하면서 현재 money에서 몇 개로 변환되는지 계산

--- src/problem5.js
+++ src/problem5.js
@@ -1,10 +1,14 @@
 function problem5(money) {
-  var answer;
-
   const moneyCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
-  return answer;
+  moneyType.forEach((type, index) => {
+    const typeCount = Math.floor(money / type);
+    money -= type * typeCount;
+    moneyCount[index] += typeCount;
+  });
+
+  return moneyCount;
 }
 
 module.exports = problem5;
