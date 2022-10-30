6833983b3c2da9dedf0ab37b9fbefb232e4d8357
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-30 20:18:18 +0900

    problem5 - feat: 금액 분류 함수
    재귀적으로 돌면서 값을 분류하고 결과를 반환한다.

--- src/problem5.js
+++ src/problem5.js
@@ -14,6 +14,21 @@ const updateClassificationResults = (resultArray, currentIndex, wonCount) => {
   return copiedArray;
 };
 
+const recursiveClassifiers = (currentMoney, currentIndex, resultArray) => {
+  if (currentIndex === 9) {
+    return copyArray(resultArray);
+  }
+
+  const classification = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const target = classification[currentIndex];
+  const moneyCount = classifyMoney(currentMoney, target);
+  const remainingMoney = getRemainingMoney(currentMoney, moneyCount, target);
+  const newResultArray = updateClassificationResults(resultArray, currentIndex, moneyCount);
+  const nextIndex = currentIndex + 1;
+
+  return recursiveClassifiers(remainingMoney, nextIndex, newResultArray);
+};
+
 function problem5(money) {
   var answer;
   return answer;
