93b70b733e02a2f32a6cb45eab33114eaf30d27d
runkey <csi9509@naver.com>
2022-10-31 00:41:23 +0900

    Feat: 3,6,9 가 들어가는지 검색해서 횟수를 return

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+    
+  for(let i = 1; i < number + 1; i++) {
+      let temp = i + ""; // 문자열로 변환
+      let matched_txt = temp.match(/3|6|9/g);
+      
+      if(matched_txt !== null) answer += matched_txt.length;
+  }
+  
   return answer;
 }
 
