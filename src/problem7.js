7665c74ee209785369c57b6e513866b1f59866b9
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-31 02:40:00 +0900

    feat: [problem7] - 사용자의 친구 목록을 반환하는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
   var answer;
+
   return answer;
+}
+
+function getUserFriends(user, friends) {
+  return friends.filter((element) => element.includes(user));
 }
 
 module.exports = problem7;
