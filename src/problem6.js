3d9c51dfe73273f6d8c5dbcf4e993de9fb2e137e
Lee-Young-Jae <yjlee1937@naver.com>
2022-10-31 02:54:27 +0900

    문제 6 - 기능 요구사항 정리

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,9 @@
+/** 기능 목록
+ *  1. 유저의 닉네임을 2글자씩 잘라 중복 횟수를 표시하는 객체 Dictionary를 생성
+ *  2. 객체 Dictionary를 순회하며 forms의 각 요소의 닉네임에 Dictionary의 요소가 포함되어 있는지 확인
+ *  3. 포함되어 있다면, 해당 유저의 Email을 반환 (오름차순 중복 제거)
+ */
+
 function problem6(forms) {
   var answer;
   return answer;
