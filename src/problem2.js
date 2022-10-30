1827b2e0ae24612d92675bd5bd6373e07f403015
kimjiwon <hallu0317@gmail.com>
2022-10-30 17:17:04 +0900

    problem2 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
   var answer;
-  return answer;
+    let temp = [];
+    temp.push(cryptogram[0]);
+
+    for(let i=1; i<cryptogram.length; i++) {
+      if(temp.length === 0) {
+        temp.push(cryptogram[i]);
+      }
+      else {
+        if(temp[temp.length-1] === cryptogram[i]) {
+          temp.pop();
+        }
+        else{
+          temp.push(cryptogram[i])
+        }
+      }
+    }
+  return temp.join("");
 }
+
 
 module.exports = problem2;
