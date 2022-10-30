3459f413636865b625d5634569033293b1964bc3
yb8350 <yb8350@naver.com>
2022-10-30 19:20:28 +0900

    Feat: problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,13 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  for (let i = 0; i < word.length; i++) {
+    let txt = word[i].charCodeAt();
+    if (txt >= 65 && txt <= 90)
+      answer += String.fromCharCode(txt - (txt - 65) - (txt - 90));
+    else if (txt >= 97 && txt <= 122)
+      answer += String.fromCharCode(txt - (txt - 97) - (txt - 122));
+    else answer += word[i];
+  }
   return answer;
 }
 
