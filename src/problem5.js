148c47041ed5183afb57f1d05bbde6d589bc8fb4
moseung <tmdah900@naver.com>
2022-10-30 19:16:56 +0900

    ✨ problem5 기능1 구현 입력받은 돈을 큰값부터 나눌 수 있는지 검사하고 원하는 값을 return하는 for문 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  let copiedMoney = money;
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (i = 0; i < answer.length; i++) {
+    const canDivideValue = canDivide(copiedMoney, i);
+    if (canDivideValue !== false) {
+      answer[i] = canDivideValue.quotient;
+      copiedMoney = canDivideValue.remainder;
+    }
+  }
   return answer;
 }
 
