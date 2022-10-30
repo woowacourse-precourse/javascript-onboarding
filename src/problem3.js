714a886c6972c9a9af3de1fb3e5ff7a7c87ef01d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-30 15:18:12 +0900

    feat: [problem3] - 해당 숫자에 특정 숫자가 몇 개 있는지 반환하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
   var answer;
+
   return answer;
+}
+
+function getCount(num, target) {
+  var arr = Array.from(String(num));
+
+  return arr.filter((e) => String(target) === e).length;
 }
 
 module.exports = problem3;
