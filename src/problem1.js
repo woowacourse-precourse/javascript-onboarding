f8b4366fb10701282ffcdc5176152a3c9f7979c0
y00eunji <aine0227@naver.com>
2022-10-30 23:39:00 +0900

    feat: [problem1] check left page is odd and right page is even

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+const isLeftOdd = (num) => {
+  return num % 2 === 1;
+};
+
+const isRightEven = (num) => {
+  return num % 2 === 0;
+};
+
 const isInvalidPageRange = (left, right) => {
   return left < 2 || right < 2 || left > 399 || right > 399;
 };
@@ -5,6 +13,8 @@ const isInvalidPageRange = (left, right) => {
 const isValidPage = (page) => {
   const [left, right] = page;
 
+  if (!isLeftOdd(left)) return false;
+  if (!isRightEven(right)) return false;
   if (isInvalidPageRange(left, right)) return false;
 
   return true;
