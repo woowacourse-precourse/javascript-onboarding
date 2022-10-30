76406885ecd0ff763682cf33c9928a00172001e1
ming0830 <minsun0830@naver.com>
2022-10-31 03:49:38 +0900

    [PROBLEM2] feat : 문자들을 비교하여 중복을 제거하는 함수 구현
    '스택' 알고리즘 개념을 이용

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let result = [];
+  for(let i =0;i<cryptogram.length;i++){
+    const char = cryptogram[i];
+    if(result[result.length-1] === char){
+      result.pop()
+    } else{
+      result.push(char)
+    }
+  } return result
 }
+
 
 module.exports = problem2;
