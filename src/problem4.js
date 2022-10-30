83d92ffc3ec3e00681a0543219515095344f7197
hjy0951 <hjy0951@gmail.com>
2022-10-30 20:40:26 +0900

    problem4: 구현 설계

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
+
 function problem4(word) {
   var answer;
   return answer;
 }
+
+/* 알파벳을 담고 있을 배열 생성 */
+/* 알파벳을 역순으로 담고 있을 배열 생성 */
+/* 대소문자 판단 함수 */
+/* 청개구리 사전을 참고하여 문자열을 변환해주는 함수 */
 
 module.exports = problem4;
