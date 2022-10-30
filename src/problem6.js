839760cca595fe2cecee5f3a05fe69c5e2e0db2b
kimyou1102 <yoosk5485@naver.com>
2022-10-31 02:06:52 +0900

    feat: 문제 6번 - 경우의 수들 중 해당하는 경우의 수가 있는 email을 구한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,16 +1,23 @@
 function problem6(forms) {
   var answer;
+  const map = new Map();
+  const mail_arr = [];
 
-  for(let i=0; i < forms.length; i++) {
+  for (let i = 0; i < forms.length; i++) {
     const [email, nickname] = forms[i];
-    for(let x=0; x < nickname.length; x++) {
-      for(let y=x+1; y < nickname.length; y++) {
-        const str = nickname.slice(x, y+1);
-        
+    for (let x = 0; x < nickname.length; x++) {
+      for (let y = x + 1; y < nickname.length; y++) {
+        const str = nickname.slice(x, y + 1);
+        if (map.has(str)) {
+          mail_arr.push(email);
+          mail_arr.push(map.get(str));
+        } else {
+          map.set(str, email);
+        }
       }
     }
   }
-  
+
   return answer;
 }
 
