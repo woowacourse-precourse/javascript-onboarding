0c668a473c5d8e605340e954a523bd75abd592b2
Dean <yjj052800@gmail.com>
2022-10-30 16:08:04 +0900

    FEAT: [7번] friends와 visitor 통합 목록

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,38 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  
+  let allUser = friendestoArr(friends).concat(visitors);
+  
+  const set = new Set(allUser);
+  const userArr = [...set];
+  console.log(userArr);
+
+  
+
   return answer;
 }
 
+function friendestoArr(arr) {
+  let result = arr.reduce((acc, cur) => {
+    return acc.concat(cur);
+  });
+  return result;
+}
+
 module.exports = problem7;
+
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
