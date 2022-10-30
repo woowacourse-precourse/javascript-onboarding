514dc1f3238e3082bcb2b36e5cc1ee44e522dc5d
Jin1won <wlsdnjs16521@gmail.com>
2022-10-30 23:31:45 +0900

    [문제1] feat: 페이지 번호 각 자리 숫자 더하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -2,4 +2,11 @@ function problem1(pobi, crong) {
   if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) return -1;
 }
 
+function calculateAdd(num) {
+  let result = 0,
+    str = num + "";
+  [...str].forEach((s) => (result += Number(s)));
+  return result;
+}
+
 module.exports = problem1;
