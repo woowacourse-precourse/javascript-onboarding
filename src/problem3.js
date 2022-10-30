dd638718f9156ed50502fccdc412161cfd204fd1
hozzijeong <ghwls0905@naver.com>
2022-10-30 19:21:19 +0900

    Feat: 3번 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  // 1 ~ number 사이에 3과 6과 9의 개수를 반환하는 문제.
+  for (let i = 1; i <= number; i++) {
+    const numMatch = ("" + i).match(/[3|6|9]/g);
+    if (numMatch) answer += numMatch.length;
+  }
   return answer;
 }
 
