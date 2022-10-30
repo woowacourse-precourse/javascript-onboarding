7f0abcfdf1c5688a00b23502a4dc1c10f4cd82ed
lsy20140 <lsy20140@naver.com>
2022-10-31 00:30:17 +0900

    feat: [문제7] user의 친구 목록 userFriend 배열에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,15 @@
+function getUserFriend(user, friends){
+  const userInclArr = friends.filter(friend => friend.includes(user));
+  const userInclList = userInclArr.reduce(function (acc, cur) {
+    return [...acc, ...cur];
+  });
+  const userFriend = userInclList.filter(v => !v.includes(user));
+
+  return userFriend;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  console.log(getUserFriend(user, friends));
 }
 
 module.exports = problem7;
