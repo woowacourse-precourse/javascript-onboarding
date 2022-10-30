0df7ec3f0e32b9524fd2cd93f6db0c90c77ef630
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-10-31 04:33:18 +0900

    feat: Add sum function for find sum

--- src/problem1.js
+++ src/problem1.js
@@ -1,17 +1,29 @@
 function problem1(pobi, crong) {
   var answer;
-  if (checkValue(pobi)&&checkValue(crong)){
-
+  if (checkValue(pobi) && checkValue(crong)) {
     return answer;
   }
   return -1;
 }
 
-function checkValue(arr){
-  let result = arr[0]%2==1&&arr[1]%2==0&&arr[1]-arr[0]==1?true:false;
+function checkValue(arr) {
+  let result =
+    arr[0] % 2 == 1 && arr[1] % 2 == 0 && arr[1] - arr[0] == 1 ? true : false;
   return result;
 }
 
+// function validationArr(arr){
+//   return arr[0]>400||arr[1]>400||arr[0]<1||arr[1]<1?false:arr[1]-arr[0] == 1?true:false;
+// }
+
+function sumValue(arr) {
+  let sum = 0;
+  do {
+    sum += arr[0] % 10;
+    arr[0] = Math.floor(arr[0] / 10);
+  } while (arr[0] > 0);
+  return sum;
+}
 
 console.log(problem1([99, 100], [197, 198]));
 
