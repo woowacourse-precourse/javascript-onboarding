26736d2a710ee64d35c195b0a4b1b44d2928ea41
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-30 15:44:00 +0900

    [문제4] <1단계 기능요구사항> 알파벳의 인덱스를 구하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
 function problem4(word) {
-  var answer;
-  return answer;
+    // 입력받은 문자열을 아스키코드로 변환한 배열
+    const asciiAlphabetArray = word.split("").map((el) => el.charCodeAt());
+
+    /** 1단계) 알파벳의 아스키코드를 입력받으면 해당 알파벳의 인덱스를 반환하는 함수
+     * @returns => 알파벳의 인덱스 a/A:1, b/B:1, ... , z/Z:26
+     */
+    const getAlphabetIndex = (asciiAlphabet) => {
+        if (asciiAlphabet < 97) {
+            return asciiAlphabet % 64;
+        }
+        if (asciiAlphabet >= 97) {
+            return asciiAlphabet % 96;
+        }
+    };
 }
 
 module.exports = problem4;
