6cb9c06e4b97e4b864fae3b62613ff732e4b886b
하송희 <99heesong@naver.com>
2022-10-30 22:28:22 +0900

    [문제 5] 기능추가: 금액 단위 배열, 초기화된 배열 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,6 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let wallet = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 }
 
-module.exports = problem5;
+  module.exports = problem5;
