101b84588e92eb54b46b60008f4fb7e5a6604adc
doheeMac <thsehgml37@korea.ac.kr>
2022-10-30 15:40:34 +0900

    feat: problem4 알파벳인 경우만 사전순 반대로 변경 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  for (i = 0; i < word.length; i++) {
+    const ascii = word.charCodeAt(i);
+    if (65 <= ascii && ascii <= 90) {
+      // 대문자
+      answer += String.fromCharCode(90 - (ascii - 65));
+    } else if (97 <= ascii && ascii <= 122) {
+      // 소문자
+      answer += String.fromCharCode(122 - (ascii - 97));
+    } else {
+      // 알파벳이 아닌 경우
+      answer += word[i];
+    }
+  }
+
   return answer;
 }
 
