d6fe09f43d635106981386eae3d1da8834f46988
HelloHailie <alsltjsals@gmail.com>
2022-10-31 01:23:21 +0900

    problem5 test 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  var moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < moneyArr.length; i++) {
+    if (parseInt(money / moneyArr[i])) {
+      answer.push(parseInt(money / moneyArr[i]));
+    } else {
+      answer.push(0);
+    }
+    money %= moneyArr[i];
+  }
   return answer;
 }
 
