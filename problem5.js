0ead139dcd64466cdb4d94e6f386efe2e0368c21
백서영 <101791501+seoye0ng@users.noreply.github.com>
2022-10-30 23:37:14 +0900

    feat : problem5 돈의 액수를 화폐단위별로 변환 문제 해결

---
+++ problem5.js
@@ -0,0 +1,21 @@
+function problem5(money) {
+  chageMoney(money);
+  return answer;
+}
+
+module.exports = problem5;
+
+const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+var answer = [];
+
+function chageMoney(money) {
+  for (var i = 0; i < moneyArr.length; i++) {
+    let resultNum = Math.floor(money / moneyArr[i]);
+
+    if (resultNum !== 0) {
+      answer.push(resultNum);
+    } else {
+      answer.push(0);
+    }
+  }
+}
