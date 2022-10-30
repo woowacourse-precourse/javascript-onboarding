d07be1fbd8b57ec515b55f617e891a1f0b3d763f
yunju <yom8926@naver.com>
2022-10-30 22:22:51 +0900

    문제4번 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,21 @@
 function problem4(word) {
-  var answer;
+  var original = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let reversed = "zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA";
+  var answer = [];
+  let result;
 
-  return answer;
+  for (let i = 0; i < word.length; i++) {
+
+    var index = original.indexOf(word[i])
+    if (index == -1) {
+      answer.push(" ")
+    }
+    let char = reversed[index]
+    answer.push(char)
+
+  }
+  result = answer.join("");
+  return result
 }
 
 module.exports = problem4;
