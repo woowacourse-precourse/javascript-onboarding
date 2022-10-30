2a39efd50b4943e10f65b37988b0b50bb1997a27
HAEROOL <gofn080776@gmail.com>
2022-10-30 22:37:53 +0900

    feat: [problem4] - 알파벳을 반대로 변경하는 함수 작성

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,22 @@ function problem4(word) {
   return answer;
 }
 
+function changeAlphabet(alphabet){
+  const frogDiction = {
+    'A':'Z','B':'Y','C':'X','D':'W',
+    'E':'V','F':'U','G':'T','H':'S',
+    'I':'R','J':'P','K':'O','L':'N',
+    'M':'M','N':'L','O':'K','P':'J',
+    'R':'I','S':'H','T':'G','U':'F',
+    'V':'E','W':'D','X':'C','Y':'B',
+    'Z':'A',}
+  let chagedAlphabet = alphabet
+  if(alphabet.charCodeAt(0) > 96 && alphabet.charCodeAt(0) < 123){
+    chagedAlphabet = frogDiction[chagedAlphabet.toUpperCase()];
+    return chagedAlphabet.toLowerCase();
+  }else{
+    return frogDiction[chagedAlphabet];
+  }
+}
+
 module.exports = problem4;
