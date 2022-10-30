e4e6f2edd8b31ff58149c20f7365290a9184e718
R-jisu <79021544+R-jisu@users.noreply.github.com>
2022-10-30 20:55:09 +0900

    [problem3]passed

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
+  let clap = 0; //clap count
+  let stringNumber = ''; 
+  for (let j=1; j<number+1; j++){
+    stringNumber = String(j); // number to string
+    for(let i=0; i<stringNumber.length;i++) { // repeat len stringnumber
+      if(stringNumber[i] === '3' || stringNumber[i]==='6' || stringNumber[i]==='9') clap++;
+    }
+  }
   var answer;
+  answer = clap;
   return answer;
 }
 
