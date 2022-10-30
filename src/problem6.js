1af761e2cfd9c1bec336dfba5a8a2e0d7b615391
oio337a <oio337a@gmail.com>
2022-10-30 22:16:27 +0900

    딕셔너리 저장 알고리즘 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let result = [];
+  let filter = {};
+  let len;
+  let data;
+
+  for (i = 0; i < forms.length; i++) {
+    len = forms[i][1].length;
+
+    for (j = 2; j < len; j++) {
+      for (k = 0; k + j <= len; k++) {
+        data = forms[i][1].slice(k, k + j);
+        if (data in filter) {
+          filter[data] += 1;
+        } else {
+          filter[data] = 1;
+        }
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = problem6;
