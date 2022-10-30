39c40ef4fd7a62b4206291a62364a07ba6200672
wonkyung <mwk0725@gmail.com>
2022-10-30 21:48:31 +0900

    refactor: allUserList-> friendsList 변수명 변경 및 값 수정

--- src/problem7.js
+++ src/problem7.js
@@ -1,13 +1,15 @@
 function problem7(user, friends, visitors) {
   const userName = user;
   const friendsList = friends;
-  const visitorsList = visitors.filter(arr => arr !== userName);
+  const delDupUserList = [...new Set([...new Set(allUserList), ...new Set(visitors)])].filter(
+    arr => arr !== userName
+  );
 
   // 내(user) 친구 구하기
   let myFriends = [];
-  for (let a of friends) {
-    if (a[0] === userName) myFriends.push(a[1]);
-    if (a[1] === userName) myFriends.push(a[0]);
+  for (let name of friends) {
+    if (name[0] === userName) myFriends.push(a[1]);
+    if (name[1] === userName) myFriends.push(a[0]);
   }
 
   // 점수 리스트
@@ -19,17 +21,6 @@ function problem7(user, friends, visitors) {
   // visitors 점수 +1
   for (let visitor of visitors) {
     userPointList[visitor] += 1;
-  }
-
-  // user의 친구 구하기
-  let myFriends = [];
-  for (let a of friends) {
-    if (a[0] === userName) {
-      myFriends.push(a[1]);
-    }
-    if (a[1] === userName) {
-      myFriends.push(a[0]);
-    }
   }
 
   // 점수별로 정렬
