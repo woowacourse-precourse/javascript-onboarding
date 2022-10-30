87b91a7689d9dfaaa7732459bc02c80af0f33213
Deemou <4106243@naver.com>
2022-10-30 13:31:28 +0900

    feat<P6>: 중복 닉네임의 이메일 목록을 반환하는 함수 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,24 @@
 function problem6(forms) {
-  var answer;
+  const answer = findEmailForDuplicateNickname(forms);
   return answer;
 }
+//input: [["abc@email.com","제이제이엠"], ["tell@email.com", "에이다"], ["coco@email.com", "타이어"], ["sson@email.com", "이어폰"]]
+//expected output: ["coco@email.com", "sson@email.com"]
+const findEmailForDuplicateNickname = (forms) => {
+  const nicknameMap = getSeparatedNicknameMap(forms),
+    emails = [],
+    size = 2;
+  forms.forEach(([email, nickname]) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const piece = nickname.substring(i, i + size);
+      if (nicknameMap.get(piece) > 1) {
+        emails.push(email);
+        break;
+      }
+    }
+  });
+  return emails.sort();
+};
 const getSeparatedNicknameMap = (forms) => {
   const map = new Map(),
     size = 2;
