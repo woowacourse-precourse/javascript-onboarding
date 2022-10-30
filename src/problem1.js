f6ec5ce06221a96ab275d298369b592bb5c26d94
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-10-30 23:26:19 +0900

    feat: 문제1 각 페이지에서 가장 큰 수 계산 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,17 @@ function validateInput(input) {
   return input[0] >= 1 && input[0] < 400 && input[1] - input[0] === 1;
 }
 
+function calculatePageNumber(page) {
+  let add = 0;
+  let multiply = 1;
+  while (page > 0) {
+    add += page % 10;
+    multiply *= page % 10;
+    page = parseInt(page / 10);
+  }
+  return add > multiply ? add : multiply;
+}
+
 function problem1(pobi, crong) {
   let answer;
   if (!validateInput(pobi) || !validateInput(crong)) return -1;
