ec0af5f39118124c350065168bb8e6a0576b6b8e
Yunjae <nuguri206@gmail.com>
2022-10-30 15:32:43 +0900

    feat: [prob5] 전체 돈 종류 배열 및 최종 지폐 계수 배열 정의

--- src/problem5.js
+++ src/problem5.js
@@ -4,6 +4,12 @@ function problem5(money) {
     if(money < 1 || money > 1000000 ) return false;
     else return true;
   };
+
+  //전체 돈 종류 배열
+  const moneyArr = [50000,10000,5000,1000,500,100,50,10,1];
+
+  //지폐 계수 배열
+  const answer = [];
 }
 
 module.exports = problem5;
