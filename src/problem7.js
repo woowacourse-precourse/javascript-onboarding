82047d684ec1873f4d572b0865ad5a212ff9450b
Jin1won <wlsdnjs16521@gmail.com>
2022-10-31 00:05:44 +0900

    [문제7] feat: user와 친구가 아닌 사용자의 목록을 초기화한다

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,7 @@
 function problem7(user, friends, visitors) {
   let answer = [];
   const relationships = {};
+  const notFriends = {};
 
   friends.forEach((friend) => {
     //사용자간의 관계를 초기화한다.
@@ -10,6 +11,12 @@ function problem7(user, friends, visitors) {
       relationships[friend[1]] = [];
     relationships[friend[0]].push(friend[1]);
     relationships[friend[1]].push(friend[0]);
+  });
+
+  Object.keys(relationships).forEach((person) => {
+    //user와 친구가 아닌 사용자의 목록을 초기화한다.
+    if (!relationships[user].includes(person) && person !== user)
+      notFriends[person] = 0;
   });
 
   return answer;
