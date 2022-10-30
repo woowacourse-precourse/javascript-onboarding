61a1811171557867fbad188d741825997310e2cb
LEECHANHYUNG <lsj721538@gmail.com>
2022-10-30 22:50:30 +0900

    change number to string

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
+/* 
+1. 숫자을 배열로 변환
+2. 숫자 크기 만큼 반복문을 돌면서 3, 6, 9 여부 확인
+*/
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    const numArr = [...String(i)];
+  }
+  return count;
 }
 
 module.exports = problem3;
