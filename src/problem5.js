aaf534f3ad42ebee2ad2b30d1343d8e608d8cee0
thewronghand <penfreak77@gmail.com>
2022-10-31 02:56:37 +0900

    feat: 현재 금액 최신화 로직 구현

--- src/problem5.js
+++ src/problem5.js
@@ -7,15 +7,17 @@ function problem5(money) {
 
   let result = (currentMoney[0] -(currentMoney[0] % item))/item;
   answer.push(result);
+  currentMoney.push(currentMoney[0] - item * result)
+  currentMoney.shift();
 }
 
 module.exports = problem5;
 
 //problem5 기능 목록
 //[x] 사용할 화폐 단위를 모두 저장한 배열 currency를 생성한다.
-//[] 빈 배열 answer와 currentMoney를 생성한다.
-//[] currentMoney에 출금할 금액인 money의 값을 저장한다.
-//[] 큰 화폐단위부터 시작하여 필요한 최대 매수를 계산한다.
-//[] 계산을 마친 화폐단위*장수만큼의 금액을 차감한 값을 현재 금액으로 저장한다.
+//[x] 빈 배열 answer와 currentMoney를 생성한다.
+//[x] currentMoney에 출금할 금액인 money의 값을 저장한다.
+//[x] 큰 화폐단위부터 시작하여 필요한 최대 매수를 계산한다.
+//[x] 계산을 마친 화폐단위*장수만큼의 금액을 차감한 값을 현재 금액으로 저장한다.
 //[] 이를 반복하여 각 화폐의 필요 매수를 구한 후, 결과를 answer에 저장한다.
 //[] answer를 반환한다.
