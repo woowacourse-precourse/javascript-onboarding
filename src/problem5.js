b01852d916775ec019c613095c655b9f8e10200c
nalsae <nalsae0207@naver.com>
2022-10-30 23:38:19 +0900

    feat: Add a initial list of target money
    - 로직에서 사용할 금액 목록을 배열에 담아 변수에 할당

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
-function problem5(money) {
-  var answer;
+const targetMoneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+const problem5 = (money) => {
+  const answer = [];
   return answer;
-}
+};
 
 module.exports = problem5;
