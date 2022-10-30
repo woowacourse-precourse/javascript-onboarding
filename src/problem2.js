24dd6db4126f234d41ecdb3fe67d548e9f050734
고동현 <dhjk35@naver.com>
2022-10-31 04:41:55 +0900

    feat: 스택을 이용해서 연속된 문자열을 제거하는 기능 구현 - 문제 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,31 @@
+/**
+ * 기능 정의
+ *
+ * - [x] 스택을 이용해서 주어진 문자열을 비교해나가면서 연속된 문자 제거하는 기능
+ */
+
+/**
+ * 암호화된 문자열 cryptogram을 입력받고, 연속된 문자열이 없을 때까지
+ * 제거한 후에 남은 문자열을 반환하는 함수
+ *
+ * @param {string} cryptogram 길이가 1이상 1000이하인 문자열
+ * @returns {string} 연속된 문자열을 제거하고 남은 문자열
+ */
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [cryptogram[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    const current = cryptogram[i];
+
+    if (stack.length > 0 && stack[stack.length - 1] === current) {
+      stack.pop();
+      continue;
+    }
+
+    stack.push(current);
+  }
+
+  return stack.join("");
 }
 
 module.exports = problem2;
