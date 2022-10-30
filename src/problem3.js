27e7ac396b5a01f932a5dac1bc5dac269d16bfcf
DongRyeong Lee <dr11291129@gmail.com>
2022-10-30 21:12:45 +0900

    feat: P3 getClapNumber 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,16 @@ function problem3(number) {
   return answer;
 }
 
+function getClapNumber(number) {
+  let counter = 0;
+
+  for (let i = 1; i <= number; i++) {
+    counter += i.toString().split("3").length - 1;
+    counter += i.toString().split("6").length - 1;
+    counter += i.toString().split("9").length - 1;
+  }
+
+  return counter;
+}
+
 module.exports = problem3;
