109d3ec9ec2d3a7d9a15528814b8f015611eb2f7
devMingu <rooster100@naver.com>
2022-10-30 15:44:21 +0900

    feat: forms으로부터 닉네임만을 뽑아냅니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,17 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  var nickName = [];
+  var myObj = new Object();
+  
+
+
+  // 닉네임만 따로 뽑아냅니다.
+  forms.forEach(element => {
+    nickName.push(element[1]);
+    myObj[`${element[1]}`] = element[0];
+  });
+
+
   return answer;
 }
 
