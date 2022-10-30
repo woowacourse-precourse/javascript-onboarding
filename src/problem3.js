fb894c5a27f7b0de6e1967e84ae69cb23fb6e595
장경은 <jang_kyung_eun@gyeong-eun-ui-MacBookAir.local>
2022-10-30 23:57:04 +0900

    문제 5번까지 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  let numString = 0;
+
+  for (let i = 1; i <= number; i++) {
+    numString = i.toString();
+    for (let j = 0; j < numString.length; j++) {
+      if (numString.charCodeAt(j) == 51 || numString.charCodeAt(j) == 54 || numString.charCodeAt(j) == 57) {
+        answer++;
+      }
+    }
+  }
+
   return answer;
 }
 
--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,54 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let n50000 = 0;
+  let n10000 = 0;
+  let n5000 = 0;
+  let n1000 = 0;
+  let n500 = 0;
+  let n100 = 0;
+  let n50 = 0;
+  let n10 = 0;
+  let n1 = 0;
+
+  while (money >= 50000) {
+    money = money - 50000;
+    n50000++;
+  }
+  while (money >= 10000) {
+    money = money - 10000;
+    n10000++;
+  }
+  while (money >= 5000) {
+    money = money - 5000;
+    n5000++;
+  }
+  while (money >= 1000) {
+    money = money - 1000;
+    n1000++;
+  }
+  while (money >= 500) {
+    money = money - 500;
+    n500++;
+  }
+  while (money >= 100) {
+    money = money - 100;
+    n100++;
+  }
+  while (money >= 50) {
+    money = money - 50;
+    n50++;
+  }
+  while (money >= 10) {
+    money = money - 10;
+    n10++;
+  }
+  while (money >= 1) {
+    money = money - 1;
+    n1++;
+  }
+
+  answer.push(n50000, n10000, n5000, n1000, n500, n100, n50, n10, n1);
+
   return answer;
 }
 
