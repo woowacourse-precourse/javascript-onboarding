8c7d7ecfb20ba33cad9c08c97e004cac9fe3bb2c
seoyoung-dev <seoyoung.dev@gmail.com>
2022-10-31 01:59:47 +0900

    Add problem4 solution

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
-function problem4(word) {
-  var answer;
-  return answer;
+function problem4(cryptogram) {
+    let splitted = cryptogram.split("");
+    let charCode;
+
+    function cvtCharacter(target) {
+        let charCode = target.charCodeAt(0);
+        if (charCode >= 65 && charCode <= 90) {
+            return String.fromCharCode(155 - charCode);
+        }
+        if (charCode >= 97 && charCode <= 122) {
+            return String.fromCharCode(219 - charCode);
+        }
+        return target;
+    }
+
+    const answer = splitted.map((ele) => cvtCharacter(ele));
+
+    return answer.join("");
 }
 
 module.exports = problem4;
