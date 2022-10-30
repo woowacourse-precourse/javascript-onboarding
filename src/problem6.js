cdd23181a980f97337e78bd9bc45ee45959e38e0
sehwan <tpghks9245@gmail.com>
2022-10-31 00:06:50 +0900

    feat: 닉네임 두글자씩 나누는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -15,7 +15,23 @@ function divideNickname(forms) {
   return nickArr;
 }
 
+// 닉네임당 두 글자씩 나누기
+function divideTwoWord(nickname) {
+  const twoWordArr = nickname.map(nickname => {
+    let twoWordArr = [];
+    for(let i = 1; i < nickname.length; i++) {
+      const twoWord = nickname[i- 1] + nickname[i];
+      twoWordArr.push(twoWord);
+    }
+  
+    twoWordArr = [...new Set(twoWordArr)]
+    console.log(twoWordArr);
+    return twoWordArr;
+  })
+}
+
 // problem6([["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]]);
+divideTwoWord(divideNickname([["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]]));
 
 module.exports = problem6;
 
