d7192a9a4e4de20a1f468b0e33702effbf55c12c
xownsdl0 <xownsdl0@naver.com>
2022-10-30 21:56:45 +0900

    <P6> 중복되는 이름 찾기

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  let standard = "";
+  let compare = [];
+  let isSame = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    standard = forms[i][1];
+    for (let j = 0; j < standard.length - 1; j++) {
+      let word = standard[j] + standard[j + 1];
+      if (compare.includes(word)) {
+        isSame.push(word);
+        let set = new Set(isSame);
+        isSame = [...set];
+      }
+      compare.push(word);
+    }
+  }
+
   return answer;
 }
 
