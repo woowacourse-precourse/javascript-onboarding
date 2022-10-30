d35a585d8f154fbf8bc459e4c8024b4476abdeba
2yunseong <dbsdltjd3701@naver.com>
2022-10-30 17:05:07 +0900

    feat(문제 7번): 주어진 이차원 배열을 그래프에 추가하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,31 @@
+const socialGraph = {};
+const socialScore = {};
+function addVertex(node) {
+  if (!socialGraph[node]) {
+    socialGraph[node] = [];
+  }
+  return;
+}
+
+function addEdge(nodeA, nodeB) {
+  addVertex(nodeA);
+  addVertex(nodeB);
+
+  if (!socialGraph[nodeA].includes(nodeB)) {
+    socialGraph[nodeA].push(nodeB);
+  }
+  if (!socialGraph[nodeB].includes(nodeA)) {
+    socialGraph[nodeB].push(nodeA);
+  }
+  return;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  friends.forEach((friend) => {
+    addEdge(friend[0], friend[1]);
+  });
+
   return answer;
 }
 
