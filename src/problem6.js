4e8f91cb00b6386fd757389a4dec62e4caf5a266
Deemou <4106243@naver.com>
2022-10-31 01:42:51 +0900

    refactor<P6>: getSeparatedNicknameMap() 구조 변경
    pieces
    array -> set 변경

--- src/problem6.js
+++ src/problem6.js
@@ -23,17 +23,14 @@ const getSeparatedNicknameMap = (forms) => {
   const map = new Map(),
     size = 2;
   forms.forEach(([_, nickname]) => {
-    const pieces = [];
+    const pieces = new Set();
     for (let i = 0; i < nickname.length - 1; i++) {
       const piece = nickname.substring(i, i + size);
-      pieces.push(piece);
+      pieces.add(piece);
     }
-    pieces.sort();
     pieces.forEach((piece, i) => {
-      if (piece != pieces[i - 1]) {
-        const count = map.has(piece) ? map.get(piece) + 1 : 1;
-        map.set(piece, count);
-      }
+      const count = map.has(piece) ? map.get(piece) + 1 : 1;
+      map.set(piece, count);
     });
   });
   return map;
