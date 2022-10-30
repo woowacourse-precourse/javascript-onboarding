1ab617bb2f5e5a1017d7a871fddfb74b9072b0d2
Jisu00 <jisudev00@gmail.com>
2022-10-30 18:58:09 +0900

    Add: Problem 5 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
 function problem5(money) {
-  var answer;
+  const isInvalid = money < 1 || money > 1000000;
+  if (isInvalid) return;
+
+  let currMoney = money;
+  const divArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for (let n of divArr) {
+    answer.push(parseInt(currMoney / n));
+    currMoney %= n;
+  }
+
   return answer;
 }
 
