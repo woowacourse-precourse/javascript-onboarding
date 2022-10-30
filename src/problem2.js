53dcc61e352c230f070fa9e90b5dc9677c1bb9eb
jhyns <tockiib@gmail.com>
2022-10-30 17:06:49 +0900

    Add problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = '';
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    cryptogram[i] === cryptogram[i + 1] ? i++ : answer += cryptogram[i];
+  }
+  if (answer === cryptogram) {
+    return answer;
+  }
+  return problem2(answer);
 }
 
 module.exports = problem2;
