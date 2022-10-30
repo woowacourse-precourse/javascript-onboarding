96092a9a1b43c1690a13030e1f5d32f824b8bfe8
kim-limit <rlawogks2468@naver.com>
2022-10-30 16:06:41 +0900

    feat: '같은 연속단어 있는 문자열의 이메일 결과값 추가 로직'

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
+const result = [];
+
 function problem6(forms) {
   var answer;
+  for (let i = 0; i < forms.length; i++) {
+    for (let z = i + 1; z < forms.length; z++) {
+      for (let j = 0; j < forms[i][1].length - 1; j++) {
+        if (forms[z][1].includes(forms[i][1].substr(j, j + 2))) {
+          result.push(forms[z][0]);
+        }
+      }
+    }
+  }
+
   return answer;
 }
-
 module.exports = problem6;
