b8ecbccb346279bad6187955e3c4b5fd1fb7dfac
Blackraven93 <reblackraven@gmail.com>
2022-10-31 01:17:07 +0900

    Problem4[Feat] 알파벳 외의 문자를 체크하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,10 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  return [...word]
+    .map((alphabet) => {
+      // 알파벳 외의 문자는 변환하지 않는다. (특수문자 및 공백 체크)
+      if (!/[a-zA-Z]/g.test(alphabet)) return alphabet;
+    })
+    .join("");
 }
 
 module.exports = problem4;
