5456c70ea5cbf940432e1fa8e910abb6b1e549d3
userHWSEO <gmldnjseocnd@gmail.com>
2022-10-30 11:00:06 +0900

    feat: 청개구리 알파벳 게임 기능 구현
    - 알파벳을 반대 순서로 변환 시키는 기능 구현.
    
    - 테스트케이스 확인 완료.
    
    - 간단한 주석 추가.

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,27 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  // 알파벳을 A부터 Z까지 생성
+  const arr = Array.from({ length: 26 }, (v, i) => String.fromCharCode(i + 65));
+  // 참조 타입의 완전 복사를 위해 스프레드 문법 사용
+  const reverseArr = [...arr].reverse();
+  let obj = {};
+  // Key에는 알파벳 Value에는 반대 알파벳을 객체에 넣음
+  arr.forEach((el, i) => {
+    obj[el] = reverseArr[i];
+    obj[el.toLowerCase()] = reverseArr[i].toLowerCase();
+  });
+  // for in 문을 사용하여 word의 알파벳을 확인하여 반대 알파벳을 answer에 넣어줌
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === " ") {
+      answer += " ";
+    } else {
+      for (let key in obj) {
+        if (word[i] === key) {
+          answer += obj[key];
+        }
+      }
+    }
+  }
   return answer;
 }
 
