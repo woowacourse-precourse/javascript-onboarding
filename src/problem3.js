58327b75e61fa261736048aba3e491c79abc6b14
Jyeo <dhkfkr134@naver.com>
2022-10-31 06:01:00 +0900

    문제4 솔루션 추가(청개구리 문제) 문제4. 주어진 문자열에 대하여 a~z의 문자를 거꾸로 대치하는 문제이다. a->z, b->y, c->x의 방식으로 변환한다. 정규식과 아스키코드를 이용하여 문제를 해결하였다.

--- src/problem3.js
+++ src/problem3.js
@@ -7,11 +7,12 @@ function problem3(number) {
     return -1;
   }
   //변수 regexp369는 '3','6','9'를 판별하는 정규표현식이다.
+  //전체 문자열을 탐색하고 반환하기 위해서 'g'플래그를 사용
   let answer;
   let count = 0;
   const regexp369 = /3|6|9/g;
 
-  //match()의 결과 일치하는 것이 없으면 null을 반환하므로 '|| []'을 추가했다.
+  //match()의 결과, 일치하는 것이 없으면 null을 반환하므로 '|| []'을 추가했다.
   for (let i = 1; i < number + 1; i++) {
     count += (String(i).match(regexp369) || []).length;
   }
--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,30 @@
+// 2022 Kimjungwon 우아한테크코스 문제풀이 4
+
 function problem4(word) {
-  var answer;
+  //제한사항1. word는 길이가 1이상 1,000이하인 문자열이다.
+  if (word.length < 1 || word.length > 1000) {
+    return -1;
+  }
+  let answer = "";
+  let tmpChr = "";
+
+  //replace()사용해도 되지만 가독성을 위해서 if문을 사용
+  //아스키코드를 사용하여 26개의 문자를 정수화시키고, -27과 절댓값을 통해서 문자를 거꾸로 만든다.
+  //제한사항3. 알파벳 대문자는 알파벳 대문자로, 알파벳 소문자는 알파벳 소문자로 변환한다.
+  for (let i = 0; i < word.length; i++) {
+    if (String(word[i]).match(/[A-Z]/)) {
+      tmpChr = String.fromCharCode(Math.abs(word.charCodeAt(i) - 64 - 27) + 64);
+    }
+    else if (String(word[i]).match(/[a-z]/)) {
+      tmpChr = String.fromCharCode(Math.abs(word.charCodeAt(i) - 96 - 27) + 96);
+    }
+    //제한사항2. 알파벳 외의 문자는 변환하지 않는다.
+    else {
+      tmpChr = word[i];
+    }
+    answer += tmpChr;
+
+  }
   return answer;
 }
-
 module.exports = problem4;
