72214dc986cfd54383ba52f17198b36e50b00d48
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-31 00:56:30 +0900

    feat(Function):관계와 방문자에 따른 점수 추가 및 각 점수 저장(기능 목록 2번 구현)

--- src/problem7.js
+++ src/problem7.js
@@ -1,8 +1,6 @@
 function problem7(user, friends, visitors) {
-  const answer = [];
   const scoremap = new Map();
   const arr = new Set();
-  const scoreSet = new Set();
 
   for (let x of friends) {
     if (x.includes(user)) {
@@ -12,7 +10,30 @@ function problem7(user, friends, visitors) {
     scoremap.set(x[0], 0);
     scoremap.set(x[1], 0);
   }
-  console.log(scoremap);
+  let num = 0;
+  for (let x of friends) {
+    if (x.includes(user)) continue;
+
+    if (arr.has(x[0])) {
+      num = scoremap.has(x[1]) ? scoremap.get(x[1]) + 10 : 10;
+      scoremap.set(x[1], num);
+      scoreSet.add(num);
+      continue;
+    }
+    if (arr.has(x[1])) {
+      num = scoremap.has(x[0]) ? scoremap.get(x[0]) + 10 : 10;
+      scoremap.set(x[0], num);
+      scoreSet.add(num);
+      continue;
+    }
+  }
+
+  for (let x of visitors) {
+    if (arr.has(x)) continue;
+    num = scoremap.has(x) ? scoremap.get(x) + 1 : 1;
+    scoremap.set(x, num);
+    scoreSet.add(num);
+  }
 }
 
 problem7(
