5bcb6e1be8ae801fff7aec66073c6cc68a5ae876
2jaebbang <72256238+2jaebbang@users.noreply.github.com>
2022-10-30 21:17:13 +0900

    comment: (P2) 주석 제거

--- src/problem2.js
+++ src/problem2.js
@@ -3,31 +3,24 @@ function problem2(cryptogram) {
   return answer;
 }
 
-//문자 연속 판별 기능
 function isContinuous(cryptogram) {
-  //연속문자 제거
   let newCryptogram = continuousRemoval(cryptogram);
 
-  // 더이상 문자가 연속되지 않는 경우
   if (newCryptogram === cryptogram) {
     return newCryptogram;
   }
-  // 아직 이웃하는 문자가 연속될 경우 (Recursion)
   return isContinuous(newCryptogram);
 }
 
-// 연속문자 제거 기능
 function continuousRemoval(code) {
   let newCode = "";
   let continuousAlphabet = "";
   for (let i = 0; i < code.length; i++) {
-    // 현재 문자가 연속될 경우
     if (code[i] === code[i + 1] || continuousAlphabet === code[i]) {
       continuousAlphabet = code[i];
       continue;
     }
 
-    // 현재 문자가 연속되지 않는 경우
     continuousAlphabet = "";
     newCode += code[i];
   }
