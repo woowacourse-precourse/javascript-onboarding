e1874b883db3a3330788f0857f25d27d53bbfd94
동동이 <drdragon777@naver.com>
2022-10-30 18:51:48 +0900

    PROBLEM3 - initial commit(solved)

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  for (let i = 1; i <= number; i++) {
+    const matchedWith369 = i.toString().match(/[369]/g);
+    count += matchedWith369?.length ?? 0;
+  }
+  return count;
 }
 
 module.exports = problem3;
