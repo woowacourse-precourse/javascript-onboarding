50fa17334de1f4cc93ac5b8dab86e2a5da589c38
banma1234 <banma1234@gmail.com>
2022-10-30 12:25:11 +0900

    problem_3 : 게임진행 함수

--- src/problem3.js
+++ src/problem3.js
@@ -10,17 +10,26 @@ function errorHandler(num) {
 
   if (num > 10000) {
     // 10000보다 클 경우 10000까지의 숫자 저장
-    for (i=1; i<10001; i++){
-      arr.append(i);
-    }
+    for (i=1; i<10001; i++) arr.append(i);
   }
   else{
-    for (i=1; i<num+1; i++){
-      arr.append(i);
-    }
+    for (i=1; i<num+1; i++) arr.append(i);
   }
 
   return arr;
+}
+
+function playGame(arr) {
+  // 손뼉 횟수를 저장할 변수
+  var count;
+  var result;
+
+  // 문자로 변환한 후 자리수별로 쪼개 3, 6, 9가 포함되있을 경우 count를 1 증가
+  result = arr.toString().split('').filter(function(n) {
+    if (['3', '6', '9'].includes(n))  count++;
+  })
+
+  return count;
 }
 
 module.exports = problem3;
