9a13f9462d8d426dd8b415c9bf0e9cb3cf89b13f
oxhwixo <hwi_030@naver.com>
2022-10-30 19:55:46 +0900

    Problem2: 중복된 문자열 삭제하는 기능 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,27 @@
+function removeOverlap(cryptogram) {
+  cryptogram = [...cryptogram];
+
+  for (var i = 0; i < cryptogram.length - 1; i++) {
+    console.log(i);
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      var startIndex = i;
+      var overlapLength = 2;
+      checkOverlap = true;
+      i++;
+      while (cryptogram[i] === cryptogram[i + 1]) {
+        overlapLength++;
+        i++;
+      }
+      cryptogram.splice(startIndex, overlapLength);
+      i = i - overlapLength;
+    }
+  }
+
+  cryptogram = cryptogram.join("");
+
+  return cryptogram;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
