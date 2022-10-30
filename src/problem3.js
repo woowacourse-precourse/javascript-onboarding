c555f7b28334f5633207accba89d41a114c3a863
Minyoung Lee <mminy62@gmail.com>
2022-10-30 14:03:09 +0900

    feat: [PROBLEM3] - 숫자를 string으로 변환

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  //1. 숫자를 string으로 변환
+   for(let i = 1; i < number + 1; i++){
+     let tempNum = i.toString().split('');
+     
+     console.log(tempNum);
+
+  }
+
   return answer;
+  
 }
+
+
+problem3(10);
 
 module.exports = problem3;
