270ca90f39f995877bcb9f30c0f01c6eabeb76ef
Minyoung Lee <mminy62@gmail.com>
2022-10-30 14:29:30 +0900

    feat: [PROBLEM4] word string 을 ascii 코드로 전환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
   var answer;
+
+  //1. word string 을 ascii 코드로 전환 
+
+  let wordArray = word.split('');
+
+  wordArray.forEach(function(char){
+    console.log(char + ": " + char.charCodeAt(0));
+  })
+
+
   return answer;
 }
+
+
+problem4("I love you");
 
 module.exports = problem4;
