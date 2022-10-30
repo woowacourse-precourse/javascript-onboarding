41e18485ff416202128f6b3d3f911913bfd3c6a1
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 18:21:02 +0900

    feat(problem4): 문자를 변환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,12 +3,28 @@ function isAlpha(char) {
   var lowerRegExp = /[a-z]/;
 
   if (upperRegExp.test(char)) {
-    return 2;
-  }
-  if (lowerRegExp.teset(char)) {
     return 1;
   }
+  if (lowerRegExp.teset(char)) {
+    return 2;
+  }
   return 0;
+}
+
+function convertChar(char) {
+  var newCharCode;
+
+  switch (isAlpha(char)) {
+    case 1:
+      newCharCode = 'A'.charCodeAt() + 'Z'.charCodeAt() - char.charCodeAt();
+      break;
+    case 2:
+      newCharCode = 'a'.charCodeAt() + 'z'.charCodeAt() - char.charCodeAt();
+      break;
+    default:
+      return char;
+  }
+  return String.fromCharCode(newCharCode);
 }
 
 function problem4(word) {
