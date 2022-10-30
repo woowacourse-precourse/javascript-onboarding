975176da754e2ae0fd28273bb990d7b368d85688
2wndrhs <2wndrhs@gmail.com>
2022-10-29 23:39:58 -0700

    fix(PROBLEM4): 모든 문자를 유니코드로 변환하도록 수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,25 +1,14 @@
-// 정규식에 매칭되는 문자만 유니코드로 변환하는 기능
-function convertUnicode(regex, splitedWord) {
-  const result = splitedWord.map((char) =>
-    regex.test(char) ? char.charCodeAt() : char
-  );
+// 문자를 유니코드로 변환하는 기능
+function convertUnicode(splitedWord) {
+  const result = splitedWord.map((char) => char.charCodeAt());
   return result;
 }
 
-// 유니코드만 반대로 변환하는 기능
-function reverseUnicode(word) {
-  const result = word.map((char) =>
-    typeof char === "number" ? 155 - char : char
-  );
-  return result;
-}
 // solution
 function solution(word) {
-  const regex = /[a-zA-z]/;
   const splitedWord = word.split("");
-  const unicodeWord = convertUnicode(regex, splitedWord);
-  const reversedWord = reverseUnicode(unicodeWord);
-  console.log(reversedWord);
+  const unicodeWord = convertUnicode(splitedWord);
+  console.log(unicodeWord);
 }
 
 function problem4(word) {
