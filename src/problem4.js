b1823156729cd93f8070b51347e5c642fa6d18dc
Yunjae <nuguri206@gmail.com>
2022-10-30 15:27:48 +0900

    feat: [prob4] 아스키 코드 범위

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
 function problem4(word) {
   //단어를 배열로 변환
   let words = word.split("")
+
+  //ascii_A = 65;
+  //ascii_Z = 90;
+  //ascii_a = 97;
+  //ascii_z = 122;
+  //ascii_spacebar = 32;   
 }
 
 module.exports = problem4;
