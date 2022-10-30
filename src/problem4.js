011ca9476699bfcc263a54964b3fa0001d1ebd0d
devMingu <rooster100@naver.com>
2022-10-30 15:40:53 +0900

    feat: 반복문을 통해 문자에 대한 암호를 찾아 답을 도출.

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,30 @@
-const problem4 = (element) => {
-  var answer;
+const findCrypto = (element) => {
+  var ans = "";
+  // 대문자인 경우.
+  if (element >= 'A' && element <= 'Z'){
+    asciiNum = 'Z'.charCodeAt() - (element.charCodeAt() - 'A'.charCodeAt()); //아스키코드를 사용하여 풀 수 있다.
+    ans = String.fromCharCode(asciiNum);
+  }
+  // 소문자인 경우.
+  else if (element >= 'a' && element <= 'z'){
+    asciiNum = 'z'.charCodeAt() - (element.charCodeAt() - 'a'.charCodeAt());
+    ans = String.fromCharCode(asciiNum);
+  }
+  // 그 외 나머지 빈칸.
+  else{
+    ans = element;
+  }
+
+  return ans;
+}
+
+const problem4 = (word) => {
+  var answer = "";
+  word = [...word];
+  
+  word.forEach(element => {
+    answer += findCrypto(element);
+  });
 
   return answer;
 }
--- src/problem5.js
+++ src/problem5.js
@@ -1,25 +1,4 @@
-const findCrypto = (element) => {
-  var ans = "";
-  // 대문자인 경우.
-  if (element >= 'A' && element <= 'Z'){
-    asciiNum = 'Z'.charCodeAt() - (element.charCodeAt() - 'A'.charCodeAt()); //아스키코드를 사용하여 풀 수 있다.
-    ans = String.fromCharCode(asciiNum);
-  }
-  // 소문자인 경우.
-  else if (element >= 'a' && element <= 'z'){
-    asciiNum = 'z'.charCodeAt() - (element.charCodeAt() - 'a'.charCodeAt());
-    ans = String.fromCharCode(asciiNum);
-  }
-  // 그 외 나머지 빈칸.
-  else{
-    ans = element;
-  }
-
-  return ans;
-}
-
 function problem5(money) {
-  var answer;
   
   return answer;
 }
