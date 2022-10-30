22a7ddea8d206aa152ca5713f16493ba9d6052b5
Siby1lA <psb4644@gmail.com>
2022-10-30 12:19:45 +0900

    feat: [문제 6] 중복된 단어의 개수를 세서 객체 형태로 저장하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -12,11 +12,19 @@ function nicknameSplitTwoArray(forms) {
   });
   return nickNameArray.split(",").slice(0, -1);
 }
-
+function nicknameDupleCountMap(nickname) {
+  const nickNameMap = nickname.reduce((accu, curr) => {
+    accu.set(curr, (accu.get(curr) || 0) + 1);
+    return accu;
+  }, new Map());
+  return nickNameMap;
+}
 function problem6(forms) {
   let answer = [];
   let nicknameArray = [];
+  let dupleCountMap = new Map();
   nicknameArray = nicknameSplitTwoArray(forms);
+  dupleCountMap = nicknameDupleCountMap(nicknameArray);
   return answer;
 }
 
