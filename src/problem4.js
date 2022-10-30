4aab58cd1e00c3cdb08b8cf035294fc4702a4296
Yeom-JinHo <pwlsghq@naver.com>
2022-10-30 21:25:16 +0900

    feat : [P4] 알파벳 판별하는 로직

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,19 @@
+const AlphaCase = {
+  UPPER: "upper",
+  LOWER: "lower",
+  NOT: "not",
+};
+
+function getWordCase(word) {
+  if (word >= "A" && word <= "Z") {
+    return AlphaCase.UPPER;
+  }
+  if (word >= "a" && word <= "z") {
+    return AlphaCase.LOWER;
+  }
+  return AlphaCase.NOT;
+}
+
 function problem4(word) {
   var answer;
   return answer;
