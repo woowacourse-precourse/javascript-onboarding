8210b03fc63c5df00e56ccf68f7c5a092a9eefed
minsoo <als494@naver.com>
2022-10-30 20:56:54 +0900

    7번 문제 해결 순서 정리

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,11 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  // 1. friends 안에서 user랑 같은 배열에 있는 친구를 찾고 userFriends 배열로 넣기
+  // 2. 그 친구와 친구인 사람을 찾아서 새 이차원 배열 recommand에 [10, '이름'] 이렇게 넣고 이미 있으면 +10
+  // 3. visitors 배열을 돌면서 recommand에 [1, '이름'] 이렇게 넣고 이미 있으면 +1
+  // 4. 내림차순으로 정렬하고 5개까지 자르고 이름만 뽑아서 새 배열 result에 넣기
+
+
   return answer;
 }
 
