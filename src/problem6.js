e3e431bcc05e0ac5a4b7a3ce619ee8cd3eadfbcc
ryuna <anottrx@gmail.com>
2022-10-30 13:36:30 +0900

    feat(6번): 크루 한 명마다 모든 크루의 닉네임과 비교하고, 결과인 set을 배열로 변경하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,8 @@
 function problem6(forms) {
-  var answer;
-  forms = forms.filter((form) => form[1].length > 1);
+  forms = forms
+    .filter((form) => form[1].length > 1)
+    .map((form) => form.concat(splitNickname(form[1])));
+  var answer = compareAllCrews(forms);
   return answer;
 }
 
@@ -15,6 +17,49 @@ const splitNickname = (nickname) => {
     splitedSet.add(nickname.slice(i, i + 2));
   }
   return splitedSet;
+};
+
+/**
+ * 크루 한 명마다 모든 크루의 닉네임과 비교한다
+ * @param {Array<string, string, Set<string>>} forms - [크루 이메일, 닉네임, 닉네임 Set]
+ * @returns {Array<string>} 겹치는 크루의 이메일
+ */
+const compareAllCrews = (forms) => {
+  const emailSet = new Set();
+  forms.forEach((form, index) => {
+    compareNicknames(form[0], form[2], index, forms).forEach((email) => {
+      emailSet.add(email);
+    });
+  });
+  console.log("emailSet", emailSet);
+  return [...emailSet];
+};
+
+/**
+ * 크루 한 명마다 자신을 제외한 모든 크루의 닉네임과 비교하고, 겹치는 닉네임 문자가 있다면 해당 이메일을 모아 리턴한다
+ * @param {string} myEmail - 크루의 이메일
+ * @param {Set<string>} myNicknameSet - 크루 닉네임 Set
+ * @param {string} myIndex - 해당 크루 인덱스
+ * @param {Array<string, string, Set<string>>} forms - [크루 이메일, 닉네임, 닉네임 Set]
+ * @returns {Array<string>} 겹치는 크루의 이메일
+ */
+const compareNicknames = (myEmail, myNicknameSet, myIndex, forms) => {
+  const duplicatedEmailList = [];
+  forms.forEach((form, index) => {
+    if (index !== myIndex) {
+      const copyedNicknameSet = new Set([...form[2]]);
+      const crewNicknameSetSize = copyedNicknameSet.size;
+      myNicknameSet.forEach((splitedNickname) => {
+        if (copyedNicknameSet.has(splitedNickname))
+          copyedNicknameSet.delete(splitedNickname);
+      });
+      if (copyedNicknameSet.size !== crewNicknameSetSize) {
+        duplicatedEmailList.push(form[0]);
+        duplicatedEmailList.push(myEmail);
+      }
+    }
+  });
+  return duplicatedEmailList;
 };
 
 module.exports = problem6;
