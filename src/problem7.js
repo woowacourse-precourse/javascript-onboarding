e3177c8a022c77089620c1f29a819fd36ea7614d
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-31 00:54:44 +0900

    feat(Setting): 사용자들의 점수 테이블 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,31 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const answer = [];
+  const scoremap = new Map();
+  const arr = new Set();
+  const scoreSet = new Set();
+
+  for (let x of friends) {
+    if (x.includes(user)) {
+      arr.add(x[0] === user ? x[1] : x[0]);
+      continue;
+    }
+    scoremap.set(x[0], 0);
+    scoremap.set(x[1], 0);
+  }
+  console.log(scoremap);
 }
+
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 
 module.exports = problem7;
