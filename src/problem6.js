0f163176ae1e4d3aa1af1d26085f24c323b27f67
Hwang Juhee <julia0276@naver.com>
2022-10-31 00:18:44 +0900

    :sparkles: [feat] 중복 닉네임 확인 함수 구현 #P6

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,3 @@
-function popCrew (forms,checkNickname) {
-  const result = forms.filter(crew => crew[1] !== checkNickname);
-  return result;
-}
-
 function getDuplicateNicknames(forms){
   const duplNicknames = forms.filter(crew => crew[2])
   let result = [];
@@ -13,9 +8,27 @@ function getDuplicateNicknames(forms){
 
 function problem6(forms) {
   let answer = [];
-  // 중복 여부 확인을 위한 값 추가
+  // 중복 제거 확인용 변수 추가
   forms.forEach(crew => crew[2] = false);
-  
+
+  forms.forEach(function(crew,idx){
+    let nickname = crew[1];
+    // 배열에 대한 반복
+    for (let i = idx+1; i<forms.length; i++){
+      let checkNick = forms[i][1]
+      // 중복확인문자에 대한 반복
+      for (let j = 0; j<nickname.length-1; j++){
+        let checkIdx = checkNick.indexOf(nickname[j])
+        if(checkIdx >= 0 && checkIdx < checkNick.length-1){
+          if(checkNick[checkIdx+1]===nickname[j+1]){
+            forms[i][2] = true;
+            crew[2] = true;
+            break;
+          } 
+        } break;
+      }
+    }
+  })
 
   answer = getDuplicateNicknames(forms);
   return answer;
