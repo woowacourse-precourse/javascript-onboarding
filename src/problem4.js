2ed0c13b83961bb0ea80df7d6e62868e356f2f96
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 08:36:22 +0900

    구현할 기능 목록 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
+/*
+  구현할 기능 목록
+  [] 1. 청개구리 사전을 생성하는 기능
+  [] 2. 해당 문자가 알파벳인지 확인하는 기능
+  [] 3. 해당 문자가 대문자인지 확인하는 기능
+  [] 4. 1~3의 기능을 이용하여 주어진 word를 이용하여 변환된 문자열을 반환하는 기능
+*/
+
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = '';
+
+  return result;
 }
 
 module.exports = problem4;
