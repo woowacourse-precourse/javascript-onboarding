a349bc7900557545d34bea63f035fca5d7b68083
feb-dain <feb.dain@gmail.com>
2022-10-31 00:33:53 +0900

    feat: [problem2] find duplicates and remove them

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,10 @@
 function problem2(cryptogram) {
-  var answer;
+  for (let index = 0; index < cryptogram.length; index++) {
+    if (cryptogram[index] === cryptogram[index + 1]) {
+      cryptogram = cryptogram.slice(0, index) + cryptogram.slice(index + 2);
+    }
+  }
+
   return answer;
 }
 
