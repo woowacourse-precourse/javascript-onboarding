a9d3aa045ef86330942f9c03899ab4d0c4da19ef
금가경 <gageum0@gmail.com>
2022-10-30 23:50:35 +0900

    feat : [problem1] 페이지 게임 예외사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,12 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1){
+      return -1;
+  }
+  else if(pobi[0] % 2 === 0 || crong[0] % 2 === 0){
+      return -1;
+  }
+  else if(pobi[1] % 2 === 1 || crong[1] % 2 === 1){
+      return -1;
+  }
 }
-
 module.exports = problem1;
