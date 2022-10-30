bff4e7708b60b91216756fb377a96d87061e4cb3
금가경 <gageum0@gmail.com>
2022-10-31 00:06:13 +0900

    feat : [problem3] 369 등장 횟수 계산 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let sum = 0;
+  const r369 = /[369]/g;
+
+  for(let i = 1; i <= number; i++){
+    if((i+'').match(r369)){
+      sum += ((i+'').match(r369)).length;
+    }
+  }
+  return sum;
 }
 
 module.exports = problem3;
