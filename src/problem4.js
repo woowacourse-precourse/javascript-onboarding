cae06d3a7a48c2b7458bbc26d1d130bb0dab1df5
moseung <tmdah900@naver.com>
2022-10-30 18:30:52 +0900

    ✨ problem4 기능1 추가 problem4 기능 1 : 주어진 문자열에서 아스키코드로 변환하고 해당값을 추가하는 반복문

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  for (let x of word) {
+    let ascii = x.charCodeAt(0);
+    const checkedAlphabet = checkAlphabet(ascii);
+    if (ascii === 32) {
+      answer += " ";
+      continue;
+    }
+    if (!checkedAlphabet) {
+      answer += String.fromCodePoint(x);
+      continue;
+    }
+    const frogValue = convertFrog(ascii);
+    answer += String.fromCodePoint(frogValue);
+  }
   return answer;
 }
 
