9c2914e22f586473b18c6ca44c405374a5ca6e90
Jerry <110958704+Jerryjerry0102@users.noreply.github.com>
2022-10-30 19:20:11 +0900

    problem4 기능 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const words = word.split("");
+  let a = "abcdefghijklmnop";
+  let z = "zyxwvutsrqponmlk";
+  let result = "";
+
+  for (let i = 0; i < words.length; i++) {
+    if (a.includes(word[i])) {
+      result += z[a.indexOf(words[i])];
+    } else if (z.includes(word[i])) {
+      result += a[z.indexOf(words[i])];
+    } else if (a.toUpperCase().includes(word[i])) {
+      result += z[a.toUpperCase().indexOf(words[i])].toUpperCase();
+    } else if (z.toUpperCase().includes(word[i])) {
+      result += a[z.toUpperCase().indexOf(words[i])].toUpperCase();
+    } else {
+      result += " ";
+    }
+  }
+  return result;
 }
 
 module.exports = problem4;
