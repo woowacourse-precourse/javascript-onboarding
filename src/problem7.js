98815dcae18eef4467990ae6e78530938213e784
kimeuns <dmstn2006@gmail.com>
2022-10-30 18:41:23 +0900

    문제7 친구 리스트 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,26 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let result = {}
+  const friendList = makeFriendList(user,friends)
+  let userFriend = friendList[user]
   
   return answer;
 }
+
+function makeFriendList(friends){
+  return friends.reduce(function(obj,item){
+    for(let i in item){
+        if(!obj[item[i]]){
+            obj[item[i]]=[item[1-i]]
+        }else{
+            obj[item[i]].push(item[1-i])
+        }
+    }
+    return obj
+  },{})
+}
+
+
+
 
 module.exports = problem7;
