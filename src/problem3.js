c7e13ffe7dca71ecc79915429e779df3d1c4808a
yjh1008 <exesea12@gmail.com>
2022-10-30 18:07:15 +0900

    [docs] PROBLEM3.md 기능사항 작성

--- src/problem3.js
+++ src/problem3.js
@@ -2,8 +2,8 @@ function problem3(number) {
   var answer = 0;
   for(let i = 1; i <= number ; i++){
     let numberArr = [...String(i)];
-    numberArr.forEach((d) => {
-      if(d === '3' || d === '6' || d==='9'){
+    numberArr.forEach((num) => {
+      if(num === '3' || num === '6' || num ==='9') {
         answer++;
       }
     })
