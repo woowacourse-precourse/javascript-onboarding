a6f913cb3ba5325bf7132508ac970e03170280f0
marugy <gyulife7301@naver.com>
2022-10-30 17:12:10 +0900

    done

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var point = [];
+  //forms 훑기
+  for (var i = 0; i < forms.length; i++) {
+    //forms에서 i번째 닉네임의 substring 뽑기
+    for (var j = 0; j < forms[i][1].length - 1; j++) {
+      for (var k = j + 2; k < forms[i][1].length; k++) {
+        // substring을 포함하고 있는게 있는지 확인하기
+        for (var l = i + 1; l < forms.length; l++) {
+          if (forms[l][1].includes(forms[i][1].substring(j, k))) {
+            point.push(forms[l][0]);
+            point.push(forms[i][0]);
+          }
+        }
+      }
+    }
+  }
+  var set = new Set(point);
+  const answer = Array.from(set);
   return answer;
 }
 
--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,50 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let userSet = new Set();
+  friends.map((it) => userSet.add(it[0]).add(it[1]));
+  visitors.map((it) => userSet.add(it));
+  const userList = [...userSet];
+
+  let adjList = {};
+
+  for (let i = 0; i < userList.length; i++) {
+    adjList[userList[i]] = [[], 0];
+  }
+
+  for (let i = 0; i < friends.length; i++) {
+    adjList[friends[i][0]][0].push(friends[i][1]);
+    adjList[friends[i][1]][0].push(friends[i][0]);
+  }
+
+  adjList["mrko"][0].map((it) => {
+    adjList[it][0].map((it) => {
+      if (it !== "mrko" && !adjList["mrko"][0].includes(it))
+        adjList[it][1] += 10;
+    });
+  });
+
+  visitors.map((it) => {
+    if (!adjList["mrko"][0].includes(it)) adjList[it][1] += 1;
+  });
+
+  var sortList = [];
+  for (var username in adjList) {
+    sortList.push([username, adjList[username][1]]);
+  }
+  sortList.sort((a, b) => {
+    if (a[1] > b[1]) {
+      return -1;
+    } else {
+      return 1;
+    }
+  });
+
+  let result = [];
+  for (let i = 0; i < sortList.length && i < 5; i++) {
+    if (sortList[i][1] != 0) {
+      result.push(sortList[i][0]);
+    }
+  }
+  return result;
 }
 
 module.exports = problem7;
