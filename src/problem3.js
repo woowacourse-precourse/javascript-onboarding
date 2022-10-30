a9baef4bd3ac5d9e7756e40ca5c9ce91c0970985
yb8350 <yb8350@naver.com>
2022-10-30 18:52:02 +0900

    Feat: problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    let arr = String(i).match(/[369]/g);
+    if (arr) answer += arr.length;
+  }
   return answer;
 }
 
