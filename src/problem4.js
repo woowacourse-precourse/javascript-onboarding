663c1d6f969ce3f208c129f06ec958ce495d6656
nalsae <nalsae0207@naver.com>
2022-10-30 23:27:54 +0900

    feat: Create numericVars for solution
    - 문제 해결을 위해 사용할 변수를 객체에 프로퍼티로 담아서 저장

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
-function problem4(word) {
-  var answer;
+const numericVars = {
+  UPPER_START: 65,
+  UPPER_END: 90,
+  LOWER_START: 97,
+  LOWER_END: 122,
+  LETTER_COUNT: 26,
+};
+
+const problem4 = (word) => {
+  const answer = '';
+
   return answer;
-}
+};
 
 module.exports = problem4;
