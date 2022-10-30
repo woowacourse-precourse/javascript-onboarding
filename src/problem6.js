9f35bc31868fb4028cbe1c746d4c4e24ef77cb47
이종현 <ijonghyeon@ijonghyeon-ui-MacBookAir.local>
2022-10-31 00:30:40 +0900

    Submit : problem 6

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,36 @@
 function problem6(forms) {
   var answer;
+
+
+  console.log("this is problem 6")
+
+  function check(name, target) {
+    let rlt = []
+    for ( let a = 0 ; a < name[1].length; a++){
+      for (let b = 0; b < target[1].length; b++){
+        if ( name[1][a] === target[1][b] ){
+          if ( name[1][a+1] === target[1][b+1] && name[1][a+1] !== undefined && target[1][b+1] !== undefined ){
+            rlt.push(name[0])
+            rlt.push(target[0])
+            break
+          }
+        }
+      }
+    }
+    return rlt
+  }
+  let result = []
+  for ( let i = 0 ; i < forms.length; i ++) {
+    for ( let j = i+1 ; j < forms.length; j ++) {
+      rlt = check(forms[i],forms[j])
+      for ( let k = 0 ; k < rlt.length ; k ++) {
+        if ( result.includes(rlt[k]) === false ) {
+          result.push(rlt[k])
+        }
+      }
+    }
+  }
+  answer = result.sort()
   return answer;
 }
 
