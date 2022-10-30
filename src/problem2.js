21ac352f950f99b8f501c7f8fa56a400a5cddd08
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-30 22:43:02 +0900

    refactor: [문제2] decrypt 해독함수를 스택에서 정규표현식으로 로직 변경

--- src/problem2.js
+++ src/problem2.js
@@ -4,37 +4,28 @@ function problem2(cryptogram) {
     return CRYPTOGRAM_REGEX.test(cryptogram);
   };
 
+  const decryptCryptogram = (cryptogram) => {
+    const removedRepeat = removeRepeat(cryptogram);
+
+    if (isDecryptoRecursiveBaseCase(cryptogram, removedRepeat)) {
+      return removedRepeat;
+    }
+
+    return decryptCryptogram(removedRepeat);
+  };
+
+  const isDecryptoRecursiveBaseCase = (cryptogram, removedRepeat) => {
+    return removedRepeat === cryptogram || removedRepeat.length === 0;
+  };
+
+  const removeRepeat = (cryptogram) => {
+    const repeatRegex = /([a-z])\1{1,}/g;
+    return cryptogram.replace(repeatRegex, '');
+  };
+
   if (isValidCryptogram(cryptogram)) {
     throw new Error('1이상 1000이하의 영어 소문자만 입력 가능합니다.');
   }
-
-  const decryptCryptogram = (cryptogram) => {
-    if (cryptogram.length === 0) return cryptogram;
-    const decrypted = [];
-    let currentCharacter = '';
-    let isRepeated = false;
-    [...cryptogram].forEach((character) => {
-      if (!isRepeated) {
-        currentCharacter = decrypted.pop() || '';
-      }
-
-      if (currentCharacter === character) {
-        isRepeated = true;
-      } else {
-        if (!isRepeated) {
-          decrypted.push(currentCharacter);
-        }
-        decrypted.push(character);
-        isRepeated = false;
-      }
-    });
-    const decryptogram = decrypted.join('');
-
-    if (decryptogram === cryptogram) {
-      return cryptogram;
-    }
-    return decryptCryptogram(decryptogram);
-  };
 
   return decryptCryptogram(cryptogram);
 }
