b35630eac20adabc3e21d2a47d7e0d116824c0fb
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-30 19:02:25 +0900

    Feat(문제4-기능1): 알파벳 체크 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,10 @@
 // 3. 1,2의 함수로 청개구리 사전의 반대쪽 알파벳의 반환하는 함수 구현
 // 4. 입력받은 word에 맞는 문자열 출력하도록 구현
 
+function checkAlpha(char) {
+  return (char >= "a" && char <= "z") || (char >= "A" && char <= "Z");
+}
+
 function problem4(word) {
   var answer;
   return answer;
