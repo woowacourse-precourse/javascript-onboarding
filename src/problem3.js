138f0ffe9824043e1b185beb5e48b8048279aceb
박겸영 <yong_drg@naver.com>
2022-10-30 15:02:54 +0900

    feat: 문제 3) 손뼉 치는 숫자들에 들어가는 3, 6, 9의 개수 구하기 - 기능 2: 1부터 number까지 중에서 3, 6, 9가 들어가는 수들을 모두 한 자리 수로 쪼개어 그 중 3, 6, 9의 개수 세기 - refactor: 변수명 소문자로 변경 (stringOfClapNumbers)

--- src/problem3.js
+++ src/problem3.js
@@ -1,12 +1,15 @@
 function problem3(number) {
-  let StringOfClapNumbers = '';
+  let stringOfClapNumbers = '';
 
   for (let currNum = 1; currNum <= number; currNum++) {
     const currStr = currNum.toString();
 
     if (currStr.includes('3') || currStr.includes('6') || currStr.includes('9'))
-      StringOfClapNumbers += currStr;
+      stringOfClapNumbers += currStr;
   }
+
+  return stringOfClapNumbers.split('').filter((str) => str === '3' || str === '6' || str === '9')
+    .length;
 }
 
 module.exports = problem3;
