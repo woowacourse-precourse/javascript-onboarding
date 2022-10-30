0862c38ab25b0218ebb4d2691f07fb7e76036407
nerdchanii <nerdchanii@gmail.com>
2022-10-30 19:48:57 +0900

    feat : [PROBLEM6] 부분문자열 함수 추가

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,12 @@ function problem6(forms) {
   return answer;
 }
 
+function subNickname(nickname, length) {
+  let res = [];
+  for (let i = 0; i <= nickname.length - length; i++) {
+    res.push(nickname.slice(i, length + i));
+  }
+  return res;
+}
+
 module.exports = problem6;
