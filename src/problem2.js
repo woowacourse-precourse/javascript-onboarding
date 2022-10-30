bbc6a72e3b81e7dfc7b098c762d82a9b67f8bae2
afds4567 <afds4567@gmail.com>
2022-10-30 21:46:39 +0900

    feat(problem2): 암호문 해독 로직 구현

--- src/problem2.js
+++ src/problem2.js
@@ -2,10 +2,14 @@ function problem2(cryptogram) {
   const pattern = /(.)\1{1,}/g;
   let result = cryptogram;
   while (isRepeat(result, pattern)) {
-    return;
+    result = decode(result, pattern);
   }
+  return result;
   function isRepeat(string, pattern) {
     return pattern.test(string);
+  }
+  function decode(crpytogram, pattern) {
+    return crpytogram.replace(pattern, "");
   }
 }
 
