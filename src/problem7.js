de4009c3612b5a7e44a5467d2213a6e5ffb3f476
Hwang Seong Taek <hst007@naver.com>
2022-10-30 23:00:04 +0900

    feat: [problem7] user와 함께 아는 친구의 지인과 추천 점수를 객체에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -3,7 +3,18 @@ function problem7(user, friends, visitors) {
   const recommendFriends = new Map();
 
   friends.forEach(item => {
-    if(item.includes(user)) userFriends.push(item.join('').replace(user, ''))
+    if(item.includes(user)) userFriends.push(item.join('').replace(user, ''));
+  });
+
+  userFriends.forEach(item => {
+    for (let i=0; i<friends.length; i++) {
+      if (friends[i].includes(item) && !friends[i].includes(user)) {
+        const newUser = friends[i].filter(x => x !== item);
+        newUser.forEach(user => {
+          if (!userFriends.includes(user)) recommendFriends.has(user) ? recommendFriends.set(user, recommendFriends.get(user) + 10) : recommendFriends.set(user, 10);
+        });
+      }
+    }
   });
 }
 
