2d07d11492a879df6e32c4e8c8956fa4cbbd8c87
sh981013s <sh981013s@gmail.com>
2022-10-31 01:30:31 +0900

    feat: [3] 자릿수대로 쪼갠 후 반환하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,12 @@
+/**
+ * 숫자를 자릿수로 쪼갠 문자열의 배열을 반환한다.
+ * @param {number} num - 확인해야 할 숫자
+ * @return {string[]}
+ */
+const getDigitSplittedArr = (num) => {
+  return num.toString().split("");
+};
+
 function problem3(number) {
   var answer;
   return answer;
