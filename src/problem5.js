6eb02ffc6ac012df6471130019e0058df2a99a8a
myung-j-p <jmpack97@gmail.com>
2022-10-31 00:22:29 +0900

    문제5_주어진 숫자에서 큰 금액 단위 부터 나눠 몫을 카운팅하고, 나머지에 차례대로 반복 연산 후 단위의 몫 마다 리스트에 담아 리턴

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  arr = [50000,10000,5000,1000,500,100,50,10,1];
+
+  for (i in arr){
+    answer.push(parseInt(money/arr[i]));
+    money %= arr[i];
+  }
   return answer;
 }
-
 module.exports = problem5;
