88ecd5e2356912fbe6e22719ead7aa1897c0dad0
wnw <fkaus65@naver.com>
2022-10-30 19:05:46 +0900

    feat(problem6): complete problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const namePieceObj = {};
+  const formObj = {};
+
+  for (const [email, nickname] of forms) {
+    formObj[nickname] = email;
+
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const namePiece = nickname.slice(i, i + 2);
+
+      namePieceObj[namePiece] = (namePieceObj[namePiece] || new Set()).add(
+        nickname
+      );
+    }
+  }
+
+  const emailsToNotice = Object.values(namePieceObj)
+    .reduce(
+      (names, nameSet) => (nameSet.size >= 2 ? [...names, ...nameSet] : names),
+      []
+    )
+    .map((nickname) => formObj[nickname])
+    .sort((a, b) => a.localeCompare(b));
+
+  return [...new Set(emailsToNotice)];
 }
 
 module.exports = problem6;
