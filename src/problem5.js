b59643fd9c3aa6947e9505167358314857b3e986
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-10-30 21:45:04 +0900

    feat: solve problem5 1st

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,17 @@
-function problem5(money) {
+function problem5(money){
   var answer;
+  let units = [50000,10000,5000,1000,500,100,50,10,1];
+  let num;
+  answer = units.map((unit)=>{
+      if(money>=unit){
+          num = parseInt(money/unit);
+          money  %= unit;
+          return num;
+      }
+      else{
+          return 0;
+      }
+  })
   return answer;
 }
 
