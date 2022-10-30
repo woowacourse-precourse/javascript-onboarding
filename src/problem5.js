0c27ba9ef6fda391def6486766180af59ecb9697
연 <mogoo22@naver.com>
2022-10-30 21:31:54 +0900

    feat(문제5): 입력된 숫자가 1이상 1,000,000 이하의 자연수임을 검증한다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,4 +1,7 @@
 const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+const isValidMoney = (money) => money >= 1 && money <= 1000000;
+
 const changeUnit = (money) =>
   moneyUnit.map((unit) => {
     const count = parseInt(money / unit);
@@ -7,6 +10,8 @@ const changeUnit = (money) =>
   });
 
 function problem5(money) {
+  if (!isValidMoney(money)) return;
+
   return changeUnit(money);
 }
 
