910071b555a170614d67d42ccd3e4dc85ad5254b
하송희 <99heesong@naver.com>
2022-10-30 21:19:32 +0900

    [문제 4] 기능추가: 문자열 처음부터 끝까지 반대로 변환 후 저장

--- src/problem4.js
+++ src/problem4.js
@@ -1,13 +1,16 @@
 function problem4(word) {
   let newStr = "";
-  let char = word[0];
-  if(char >= 'A' && char <= 'Z') {
-    newStr += String.fromCharCode('Z'.charCodeAt(0) - (char.charCodeAt(0) - 'A'.charCodeAt(0)));
-  } else if(char >= 'a' && char <= 'z') {
-    newStr += String.fromCharCode('z'.charCodeAt(0) - (char.charCodeAt(0) - 'a'.charCodeAt(0)));
-  } else {
-    newStr += char;
+  for(let i=0; i<word.length; i++) {
+    let char = word[i];
+    if(char >= 'A' && char <= 'Z') {
+      newStr += String.fromCharCode('Z'.charCodeAt(0) - (char.charCodeAt(0) - 'A'.charCodeAt(0)));
+    } else if(char >= 'a' && char <= 'z') {
+      newStr += String.fromCharCode('z'.charCodeAt(0) - (char.charCodeAt(0) - 'a'.charCodeAt(0)));
+    } else {
+      newStr += char;
+    }
   }
+  return newStr;
 }
 
 module.exports = problem4;
