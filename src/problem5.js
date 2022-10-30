c682c15efa570704231a999dfbf0f5e227dab7a2
jaewoogwak <jaewoo010207@gmail.com>
2022-10-30 22:07:27 +0900

    feat: 5번 문제 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,10 +1,16 @@
+const BILL = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  let changes = [0,0,0,0,0,0,0,0,0]
+  let changes = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
- 
+  for (let i = 0; i < BILL.length; i++) {
+    if (money < BILL[i]) continue;
 
-  var answer;
-  return answer;
+    changes[i] = Math.floor(money / BILL[i]);
+    money = money % BILL[i];
+  }
+
+  return changes;
 }
 
 module.exports = problem5;
