051978926fa26687467bfe824f446f5f3daea1ae
Circlewee <pnj1118@naver.com>
2022-10-30 15:44:48 +0900

    [#5]feat: 화폐 갯수를 배열에 담은 후 반환

--- src/problem5.js
+++ src/problem5.js
@@ -7,6 +7,13 @@ const BILL_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 function problem5(money) {
   const answer = [];
 
+  BILL_LIST.reduce((leftMoney, bill) => {
+    const count = parseInt(leftMoney / bill, 10);
+    answer.push(count);
+
+    return leftMoney % bill;
+  }, money);
+
   return answer;
 }
 
