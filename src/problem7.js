c549ec8f1ba434d53a693243b5e2adf16cefaada
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-30 21:59:49 +0900

    refactor(problem7): 추천친구목록에 있던 사람 별 최종 점수 구하는 함수 따로 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,7 @@
 function problem7(user, friends, visitors) {
   let current_friends = [];
   let recommendations = [];
+  let recommendations2 = [];
 
   const push_current_friend = (person) => {
     current_friends.push(person);
@@ -50,31 +51,36 @@ function problem7(user, friends, visitors) {
     }
   };
 
+  const push_recommendations2 = (person, score) => {
+    recommendations2.push({ name: person, score: score });
+  };
+
+  const find_sum_of_score = () => {
+    for (let i = 0; i < recommendations.length - 1; i++) {
+      let score_sum = recommendations[i][1];
+      for (let j = i + 1; j < recommendations.length; j++) {
+        // 같은 사람인 경우
+        if (recommendations[i][0] == recommendations[j][0]) {
+          score_sum += recommendations[j][1];
+          recommendations.splice(j, 1);
+          j--;
+        }
+      }
+      push_recommendations2(recommendations[i][0], score_sum);
+    }
+  };
+
   const solution = () => {
     find_current_friends();
 
     find_friends_of_friends();
 
     find_visitors();
+
+    find_sum_of_score();
   };
 
   solution();
-
-  // 3. 최종 추천친구 목록 만들기
-  let recommendations2 = [];
-  for (let i = 0; i < recommendations.length - 1; i++) {
-    let score_sum = recommendations[i][1];
-    for (let j = i + 1; j < recommendations.length; j++) {
-      // 같은 사람인 경우
-      if (recommendations[i][0] == recommendations[j][0]) {
-        score_sum += recommendations[j][1];
-        recommendations.splice(j, 1);
-        j--;
-      }
-    }
-    // 새 배열에 최종 [이름, 점수] 값 추가
-    recommendations2.push({ name: recommendations[i][0], score: score_sum });
-  }
 
   // 4. 최종 추천친구 목록 정렬하기
   recommendations2.sort(function (a, b) {
