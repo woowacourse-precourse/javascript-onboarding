61d4bdcbf21895454a136ce7f4c6cfcbfc9c01fe
zktm9903 <zktm9903@naver.com>
2022-10-30 15:48:37 +0900

    problem5 solve

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const result = [];
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50 , 10, 1]; // 화폐 단위
+
+  unit.map(el=>{
+    num = Math.floor(money / el); // 화폐 갯수
+    money %= el;ㄴ
+
+    result.push(num);
+  })
+
+  return result;
 }
 
 module.exports = problem5;
