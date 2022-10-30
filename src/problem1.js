8f5ed255e28b4750111faeedf31ebd32e6be4ccb
KUD <kimud6003@gmail.com>
2022-10-30 19:15:20 +0900

    ✨ Feature: problem1.js errorCheck - problem1.js : errorCheck  (페이지수, 인자값 유효 판단 )

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,29 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(errorCheck(pobi,crong)) 
+    return -1;
+  let pobiScore = getMaxScore(pobi);
+  let crongScore = getMaxScore(crong);
+}
+
+function sumOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((acc, curr) => {
+      return acc + Number(curr);
+    }, 0);
+}
+
+function mulOfDigits(num) {
+  return String(num)
+    .split('')
+    .reduce((acc, curr) => {
+      return acc * Number(curr);
+    },1);
+}
+
+function errorCheck(pobi,crong) {
+  if(pobi.length !== 2 || crong.length !== 2) return true;
+  else if(pobi[0]+1 !== pobi[1] || crong[0]+1 !== crong[1]) return true;
 }
 
 module.exports = problem1;
