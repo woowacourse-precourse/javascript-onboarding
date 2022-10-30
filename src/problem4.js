da9a4c4006b708bd910d7ceb31fa9a0a4317e147
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-10-31 00:05:38 +0900

    feat: 문제 4 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,28 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = "";
+  let ascii;
+
+  // 소문자 알파벳 a, z ascii code
+  const asciia = "a".charCodeAt(0);
+  const asciiz = asciia + 25;
+
+  // 대문자 알파벳 A, Z ascii code
+  const asciiA = "A".charCodeAt(0);
+  const asciiZ = asciiA + 25;
+
+  // 각 문자를 ascii code로 변환
+  for (let i = 0; i < word.length; i++) {
+    let ch = word[i];
+    if ("a" <= ch && ch <= "z") {
+      ascii = ch.charCodeAt(0);
+      ch = String.fromCharCode(asciia + asciiz - ascii);
+    } else if ("A" <= ch && ch <= "Z") {
+      ascii = ch.charCodeAt(0);
+      ch = String.fromCharCode(asciiA + asciiZ - ascii);
+    }
+    result += ch;
+  }
+  return result;
 }
 
 module.exports = problem4;
