df2801d622dcc94ee4e3bcc7f93418a66fb771ad
KingJiwon <wldnjs0401@naver.com>
2022-10-31 01:51:24 +0900

    cash_out

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,33 @@
 function problem5(money) {
   var answer;
+  let coins = 0;
+  function calculate(pre, current) {
+    coins = Math.floor((money % pre) / current);
+    return coins;
+  }
+  const change_money = {
+    ohmanwon: Math.floor(money / 50000),
+    manwon: calculate(50000, 10000),
+    ohcheonwon: calculate(10000, 5000),
+    cheonwon: calculate(5000, 1000),
+    obegwon: calculate(1000, 500),
+    begwon: calculate(500, 100),
+    oshipwon: calculate(100, 50),
+    shipwon: calculate(50, 10),
+    illwon: calculate(10, 1),
+  };
+  const money_arr = [
+    change_money.ohmanwon,
+    change_money.manwon,
+    change_money.ohcheonwon,
+    change_money.cheonwon,
+    change_money.obegwon,
+    change_money.begwon,
+    change_money.oshipwon,
+    change_money.shipwon,
+    change_money.illwon,
+  ];
+  answer = money_arr;
   return answer;
 }
 
