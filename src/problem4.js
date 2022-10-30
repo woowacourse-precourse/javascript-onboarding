8ef148f0be741a5dcf5776e3fe0f8a91f9f0a62e
minjaeKim <smallkdb@gmail.com>
2022-10-31 03:31:28 +0900

    fix: [Problem4] Correct convertTextForggy function
    convertTextForggy 함수에서 공백 문자 이외의 다른 특수 문자도 아스키 숫자 리스트에 담길 수 있도록 수정하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -2,27 +2,26 @@ const ASCIICODE = {
   UPPERCASE_A: 65,
   UPPERCASE_Z: 90,
   LOWERCASE_A: 97,
-  LOWERCASE_Z: 122,
-  SPACE: 32
+  LOWERCASE_Z: 122
 }
 
 Object.freeze(ASCIICODE);
 
 function convertTextFroggy(word) {
-  const { UPPERCASE_A, UPPERCASE_Z, LOWERCASE_A, LOWERCASE_Z, SPACE } = ASCIICODE;
+  const { UPPERCASE_A, UPPERCASE_Z, LOWERCASE_A, LOWERCASE_Z } = ASCIICODE;
   let asciiNumberList = [];
 
   for(let i = 0; i < word.length; i++) {
     const asciiNumber = word.charCodeAt(i);
 
-    if(asciiNumber === SPACE) {
-      asciiNumberList.push(asciiNumber);
-    }
     if(asciiNumber >= UPPERCASE_A && asciiNumber <= UPPERCASE_Z) {
       asciiNumberList.push(UPPERCASE_Z - asciiNumber%UPPERCASE_A);
     }
-    if(asciiNumber >= LOWERCASE_A && asciiNumber <= LOWERCASE_Z) {
+    else if(asciiNumber >= LOWERCASE_A && asciiNumber <= LOWERCASE_Z) {
       asciiNumberList.push(LOWERCASE_Z - asciiNumber%LOWERCASE_A);
+    }
+    else {
+      asciiNumberList.push(asciiNumber);
     }
   }
 
