a10cd8906d802d28f61466513ab59e47da3e54e4
LEECHANHYUNG <lsj721538@gmail.com>
2022-10-30 22:57:37 +0900

    add money unit array

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
+/* 
+  1. 화폐 단위 배열 생성
+  2. 생성한 화폐 단위 배열을 순회하면서 입력된 money를 나눈다.
+  3. 2에서 나눈 몫만큼 money를 뺀다.
+*/
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var moneyCount = [];
+  let idx = 0;
 }
 
 module.exports = problem5;
