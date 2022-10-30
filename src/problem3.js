71dd483cd10b802961505c0031fae6f926e6526c
seungki <sysh9498@gmail.com>
2022-10-30 22:55:52 +0900

    problem 3 sovle

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (var i = 1; i < number + 1; i++) {
+    let str_i = i.toString();
+    for (var j = 0; j < str_i.length; j++) {
+      if (str_i[j] == 3 || str_i[j] == 6 || str_i[j] == 9) {
+        answer++;
+      }
+    }
+  }
+
   return answer;
 }
 
