0510dc291c07035ae83556c35c1479423d867790
kimyou1102 <yoosk5485@naver.com>
2022-10-31 02:31:52 +0900

    feat: 문제 7번 - 사용자의 타임 라인에 방문한 사람에게 1회 당 1점을 부여한다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,15 @@
 function problem7(user, friends, visitors) {
   var answer;
+  const users = new Map();
+
+  visitors.forEach(visitor => {
+    if(users.has(visitor)) {
+      users.set(visitor, users.get(visitor)+1);
+    } else {
+      users.set(visitor, 1);
+    }
+  });
+  
   return answer;
 }
 
