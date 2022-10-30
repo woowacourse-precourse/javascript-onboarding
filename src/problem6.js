2a983faafe7537e7b839fdbea344b25927a9b127
hjy0951 <hjy0951@gmail.com>
2022-10-31 00:32:03 +0900

    problem6: map re-naming

--- src/problem6.js
+++ src/problem6.js
@@ -5,17 +5,17 @@ function problem6(forms) {
 
 /* 크루는 1~10000명 */
 /*  닉네임 두글자씩 끊어서
-      1. map1에 (key: 자른 닉네임, value: email)로 set
-      2. 또 다른 map2에 (key: email, value: 0(count))로 set
-    만약 substring이 이미 map1에 존재한다면 해당 substring의 email의 map2 count를 올려줌
+      1. subNicknameMap에 (key: 자른 닉네임, value: email)로 set
+      2. 또 다른 emailMap에 (key: email, value: 0(count))로 set
+    만약 substring이 이미 subNicknameMap에 존재한다면 해당 substring의 email의 emailMap count를 올려줌
       그리고 현재 email은 answer에 포함
-    모든 과정이 완료되면 map2를 순회하며 answer에 포함되지 않은 email을 answer에 추가한다.
+    모든 과정이 완료되면 emailMap를 순회하며 answer에 포함되지 않은 email을 answer에 추가한다.
     중복 삭제 후 종료.
 */
 function checkForms(forms) {
   const crewList = [];
-  const map1 = new Map();
-  const map2 = new Map();
+  const subNicknameMap = new Map();
+  const emailMap = new Map();
 
   forms.forEach(function(form) {
     if (isProperForm(form) === false) return;
@@ -23,18 +23,18 @@ function checkForms(forms) {
     const [email, nickname] = form;
     for(let i = 0 ; i <= nickname.length - 2 ; i++) {
       const subNickname = getSubNickname(nickname, i);
-      const existEmail = map1.get(subNickname);
+      const existEmail = subNicknameMap.get(subNickname);
       if (existEmail === undefined) {
-        map1.set(subNickname, email);
-        map2.set(email, 0);
+        subNicknameMap.set(subNickname, email);
+        emailMap.set(email, 0);
       } else {
-        map2.set(existEmail, 1);
+        emailMap.set(existEmail, 1);
         if (crewList.includes(email) === false) crewList.push(email);
       }
     }
   });
 
-  map2.forEach(function(value, key) {
+  emailMap.forEach(function(value, key) {
     if (value === 1) crewList.push(key);
   });
 
