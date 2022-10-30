a9159145b89d9cff3541cd97d8cf0ebc7dad0d62
하지훈 <jnh03336@gmail.com>
2022-10-30 23:27:24 +0900

    feat: pro7, logic design

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,37 @@
+let friendsScore = {};
+let friendGraph = {};
+const shareFriendScore = 10;
+const visitScore = 1;
+ 
+const makeFriendGraph = (friends) => {
+
+}
+
+const calculateShareFriendsScore = (user) => {
+  
+};
+
+const calculateVisitScore = (visitors) => {
+
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  friendsScore = {};
+  friendGraph = {};
+
+  makeFriendGraph(friends);
+  calculateShareFriendsScore(user);
+  calculateVisitScore(visitors);
+
+  const friendNames = friendsScore.keys();
+  friendNames.sort();
+  friendNames.sort((a, b) => friendsScore[b] - friendsScore[a])
+
+  const answer = [];
+  for(let i = 0; (answer.length <= 5 && i < friendNames.length); i++){
+    if(friendsScore(friendNames[i]) === 0) break;
+    if("not friend of a user") answer.push(friendNames[i]);
+  }
   return answer;
 }
 
