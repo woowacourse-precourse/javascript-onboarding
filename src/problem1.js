dacaffa469e6e1c129618e97cf191685552a98a4
kwh97 <zzzang0512@naver.com>
2022-10-31 01:36:19 +0900

    feat(problem1): 각 자리 숫자를 더하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+}
+
+function getAddResult(num) {
+  let result = 0;
+  while(num !== 0) {
+    result += num % 10;
+    num = Math.floor(num / 10);
+  }
+
+  return result;
 }
 
 module.exports = problem1;
