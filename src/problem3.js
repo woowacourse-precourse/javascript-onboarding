5a9abb37e8584dbd6caea296d55b4d54fc73458f
sery <yunsel2@gmail.com>
2022-10-30 16:52:42 +0900

    refactor(Problem3): Seperate exeption check function

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,7 @@
 function problem3(number) {
   let answer = 0;
 
-  if (number < 1 || number > 10000) {
-    alert('1 이상 10,000 이하의 숫자를 입력해주세요');
-    return;
-  }
+  if (!isValidNumber(number, 1, 10000)) return;
 
   for (let i = 1; i < number + 1; i++) {
     const num = i.toString().split('');
@@ -23,6 +20,16 @@ function problem3(number) {
   }
 
   return answer;
+}
+
+function isValidNumber(num, min, max) {
+  if (num < min || num > max) {
+    console.error(
+      `${min} 이상 ${max.toLocaleString()} 이하의 숫자를 입력해주세요`
+    );
+    return false;
+  }
+  return true;
 }
 
 module.exports = problem3;
