c05269195eda9566dbcf1005f839cbbf36b15f84
JAESEOK PARK <jaesukpark77@naver.com>
2022-10-30 16:28:48 +0900

    기능 요구 사항 문제4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,27 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+  let upper = {};
+  let lower = {};
+  // 아스키코드 대문자, 소문자 표현
+  const upperStart = 65, upperEnd = 90, lowerStart = 97, lowerEnd = 122;
+  
+  for(let i = upperStart;i <= upperEnd; i++){
+    upper[String.fromCharCode(i)] = String.fromCharCode(155 - i)
+  }
+
+  for(let i = lowerStart; i <= lowerEnd; i++){
+    lower[String.fromCharCode(i)] = String.fromCharCode(219 - i)
+  }
+
+  for(let i = 0; i < word.length; i++){
+    if(upper[word[i]]){
+      answer += upper[word[i]];
+    }else if(lower[word[i]]){
+      answer += lower[word[i]];
+    }else{
+      answer += word[i];
+    }
+  }
   return answer;
 }
 
