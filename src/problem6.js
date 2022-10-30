4fd472e44cc4b26231d897d8d89f2663d28b5d33
froggy1014 <clcl6084@gmail.com>
2022-10-30 16:46:49 +0900

    feat[6]: 닉네임마다 모든 경우의 수 저장
    1. Map을 사용해서 모든 2글자 단어들을 키값으로 두었다.
    2. 중첩되는 키값이 있다면 value값을 늘려주는 방식으로 저장했습니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,19 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  
+  const checkArray = Array(forms.length).fill(false);
+  const map = new Map();
+
+  for ( let i = 0 ; i < checkArray.length ; i++) {
+    for( let j = 1 ; j <= forms[i][1].length - 1; j++ ) {
+      if(map.has(forms[i][1][j - 1]+forms[i][1][j])){
+        map.set(forms[i][1][j - 1]+forms[i][1][j], map.get(forms[i][1][j - 1]+forms[i][1][j])+1);
+      }
+      else map.set(forms[i][1][j - 1]+forms[i][1][j], (map.get(forms[i][1][j - 1]+forms[i][1][j])||0)+1);  
+    }
+  }
+  console.log(map);
 }
 
-module.exports = problem6;
+console.log(problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]))
+
+// module.exports = problem6;
