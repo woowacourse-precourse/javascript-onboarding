0bdb1ab95402dbe93ae469ec2ec2f4aec1610eef
jhyns <tockiib@gmail.com>
2022-10-30 22:02:03 +0900

    Add problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,19 @@
 function problem3(number) {
-  var answer;
+  function get369ofNum(num) {
+    let result = 0;
+
+    while (num > 0) {
+      result += [3, 6, 9].includes(num % 10);
+      num = Math.floor(num / 10);
+    }
+    return result;
+  }
+
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    answer += get369ofNum(i);
+  }
   return answer;
 }
 
