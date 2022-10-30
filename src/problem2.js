0831491d15970a98a878f61dae80146bee7abdc4
KPGY <yuchw8@korea.ac.kr>
2022-10-30 22:29:53 +0900

    스택을 이용한 중복제거

--- src/problem2.js
+++ src/problem2.js
@@ -4,7 +4,7 @@ let ans = Array.from(cryptogram); //문자열을 배열형식으로 변환
 let ansarr = []; //빈 스택을 하나 생성
 for(i=0; i<ans.length; i++)
 {
-  if(!ansarr.length || ansarr[ansarr.length-1] != ans[i]) //스택이 빌경우 혹은 전값과 다른경우 푸쉬
+  if(ansarr.length===0 || ansarr[ansarr.length-1] != ans[i]) //스택이 빌경우 혹은 전값과 다른경우 푸쉬
   {
     ansarr.push(ans[i]);
   }
