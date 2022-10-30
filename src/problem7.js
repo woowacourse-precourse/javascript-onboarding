bb9197df0f4144b7dbdd488f1a42f90f4e4afab6
이예나 <yenalee@MacBook-Pro.local>
2022-10-31 00:59:43 +0900

    Feat: Make object for friendInfo

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,8 @@
 function problem7(user, friends, visitors) {
   var answer = [];
+  var result = [];
   var alreadyFriend = [];
+  let friendInfo = {};
 
   for(let friend in friends){
     if(friends[friend].includes(user)){
@@ -11,7 +13,14 @@ function problem7(user, friends, visitors) {
       }
     }
   }
-
+  for(let i=0;i<friends.length;i++){
+    for(let j=0;j<2;j++){     
+        if(alreadyFriend.includes(friends[i][j]) == false && friends[i][j] != user){
+          friendInfo[friends[i][j]] = 0;
+        }
+    }
+  }
+ 
   return answer;
 }
 
