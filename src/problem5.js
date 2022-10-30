f280cdff1352ee350bf677d267db0779d500599a
nerdchanii <nerdchanii@gmail.com>
2022-10-30 13:54:45 +0900

    feat: [PROBLEM5] map 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -2,4 +2,12 @@ function problem5(money) {
   const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 }
 
+function map(f, iter) {
+  let res = [];
+  for (const a of iter) {
+    res.push(f(a));
+  }
+  return res;
+}
+
 module.exports = problem5;
