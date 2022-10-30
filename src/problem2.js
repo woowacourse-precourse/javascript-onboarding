c8ad9a0e781b3eb8858eda95d9811166f82a7436
hwnim5324 <hwnim5324@naver.com>
2022-10-31 02:08:26 +0900

    2번 기능1 완료.

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,15 @@
 function problem2(cryptogram) {
   var answer=[];
+
+  cryptogram.split('').forEach((el)=>{
+    if(answer[answer.length-1]===el){
+      answer.pop();
+    }else{
+      answer.push(el);
+    }
+  });
+
   return answer;
-  //1. 문자열을 배열로 변환해 반복문으로 중복 문자를 판별하고, 중복이 아닌 문자들을 배열 answer에 담는다.
   //2. answer를 문자열로 변환해 반환한다.
 }
 
