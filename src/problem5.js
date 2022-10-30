9cf8676a99dc780c18edbb02cfc2eeb307c9c6a5
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-30 20:50:30 +0900

    feat: problem 5 parameter 예외처리 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
-function problem5(money) {
-  var answer;
-  return answer;
+function problem5(money) {}
+function checkException(money) {
+  x = money >= 1 && money <= 1000000;
+  y = money === parseInt(money);
+  return x && y;
 }
-
 module.exports = problem5;
