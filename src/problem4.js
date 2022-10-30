9ecbcbe48a648cec4694f6723fa82ec5d87c72c8
yjh1008 <exesea12@gmail.com>
2022-10-30 18:10:46 +0900

    [problem] 문제 4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  const wordArr = Array.from(word);
+  wordArr.forEach((c) => {
+    if(c.charCodeAt(0) >= 97) answer += String.fromCharCode(122-(c.charCodeAt(0)-97));
+    else if(c.charCodeAt(0) < 90  && c.charCodeAt(0) >= 65){
+      answer += String.fromCharCode(65-(c.charCodeAt(0)-90));
+    }
+    else answer += c;
+  })
   return answer;
 }
-
 module.exports = problem4;
