28c2008eb4ab0377acf96f92a551f6adfcc24cb9
moseung <tmdah900@naver.com>
2022-10-30 17:32:08 +0900

    ✨ problem3 기능목록에 따른 기능 구현 369의 몇번의 박수를 쳐야하는지 함수 생성 및 해당값에 따른 값을 return

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
+function check369(gameNumber) {
+  let count = 0;
+  for (i = 1; i <= gameNumber; i++) {
+    console.log((i % 10) % 3);
+    if ((i % 10) % 3 === 0 && i % 10 !== 0) count++;
+    if (i > 10 && Math.floor(i / 10) % 3 === 0) count++;
+  }
+  return count;
+}
+
 function problem3(number) {
-  var answer;
+  const answer = check369(number);
   return answer;
 }
 
