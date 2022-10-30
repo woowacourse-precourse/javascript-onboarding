0da6b53dca5008d1052861b98b50d44f9427680c
devMingu <rooster100@naver.com>
2022-10-30 15:11:17 +0900

    feat: 예외사항을 확인하는 checkException함수의 기능을 구현했습니다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,25 +1,11 @@
-const count369 = (arr) => {
-  var cnt = 0;
-  arr.forEach(element => {
-    if (element === '3' || element === '6' || element === '9') cnt += 1;
-  });
-
-  return cnt;
-}
-
 function problem3(number) {
-  var answer = 0;
-  var tmp = "";
-  var arr = [];
-  for(let i=1; i<=number ; i++){
-    tmp = i.toString();
-    arr = [...tmp];
-    answer += count369(arr);
-    arr = [];
-  }
+  var answer;
+
 
   return answer;
 }
+
+
 
 module.exports = problem3;
 
