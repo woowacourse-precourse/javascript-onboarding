b734819e547d8a1454189ce58db7f816b2c722a0
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-30 20:49:43 +0900

    feat(문제7): 친구 관계를 만드는 함수 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,19 @@
+const findFriends = (friends, friendsMap, person) => {
+  let tempFriends = [];
+  for (let i = 0; i < friends.length; i += 1){
+    if (friends[i].find(name => name === person)){
+      tempFriends.push(friends[i][0] === person ? friends[i][1] : friends[i][0]);
+    }
+  }
+  friendsMap.set(person, tempFriends);
+}
+
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendsMap = new Map();
+  
+  // friends에서 내 친구 찾아 friendsMap에 넣기, 내 친구의 친구 관계 friendsmap에 넣기
+  findFriends(friends, friendsMap, user);
 }
 
 module.exports = problem7;
