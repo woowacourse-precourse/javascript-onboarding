2e879780cc48c37d1a9fbdbab8601b2980b91a69
HelloHailie <alsltjsals@gmail.com>
2022-10-30 15:17:02 +0900

    problem1 test 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,54 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let leftP = pobi[0].toString();
+  let rightP = pobi[1].toString();
+  let leftC = crong[0].toString();
+  let rightC = crong[1].toString();
+
+  if (
+    parseInt(leftP) + 1 !== parseInt(rightP) ||
+    parseInt(leftC) + 1 !== parseInt(rightC)
+  ) {
+    return -1;
+  } else {
+    let leftsumP = 0;
+    let leftmultiP = 1;
+    let rightsumP = 0;
+    let rightmultiP = 1;
+
+    let leftsumC = 0;
+    let leftmultiC = 1;
+    let rightsumC = 0;
+    let rightmultiC = 1;
+
+    for (let j = 0; j < leftP.length; j++) {
+      leftsumP += parseInt(leftP[j]);
+      leftmultiP *= parseInt(leftP[j]);
+    }
+    for (let i = 0; i < rightP.length; i++) {
+      rightsumP += parseInt(rightP[i]);
+      rightmultiP *= parseInt(rightP[i]);
+    }
+
+    for (let k = 0; k < leftC.length; k++) {
+      leftsumC += parseInt(leftC[k]);
+      leftmultiC *= parseInt(leftC[k]);
+    }
+    for (let p = 0; p < rightC.length; p++) {
+      rightsumC += parseInt(rightC[p]);
+      rightmultiC *= parseInt(rightC[p]);
+    }
+
+    Ppresult = Math.max(leftsumP, leftmultiP, rightsumP, rightmultiP);
+    Cpresult = Math.max(leftsumC, leftmultiC, rightsumC, rightmultiC);
+
+    if (Ppresult > Cpresult) {
+      return 1;
+    } else if (Ppresult < Cpresult) {
+      return 2;
+    } else if ((Ppresult = Cpresult)) {
+      return 0;
+    }
+  }
 }
 
 module.exports = problem1;
