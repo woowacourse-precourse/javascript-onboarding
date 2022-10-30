fe8d558bfcd000d79991667a1f24441c1e9b34f6
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-31 05:40:23 +0900

    feat: [problem7] - 타임라인에 방문한 친구의 추천 점수를 구하는 방식 구현 - map에 이미 점수가 등록되어 있는 경우 - map에 점수가 등록되어 있지 않은 경우

--- src/problem7.js
+++ src/problem7.js
@@ -4,17 +4,20 @@ function problem7(user, friends, visitors) {
   var answer;
   var resultMap = new Map();
   var userFriends = getUserFriends(user, friends);
+  var recommededFriend, score;
 
   for (var i = 0; i < friends.length; i++) {
+    /* 사용자 친구 목록과의 교집합 */
     var interWithUserFreinds = friends[i].filter((element) =>
       userFriends.includes(element)
-    ); // 사용자 친구 목록과의 교집합
+    );
 
-    // 추천 점수 10점, 둘 중의 한명은 사용자 친구인 경우
+    /* 추천 점수 10점 */
+
     if (interWithUserFreinds.length === 1) {
+      // 둘 중의 한명은 사용자 친구인 경우
       var friend = friends[i][0];
-      var recommendedFriend;
-      var score = 10;
+      score = 10;
 
       if (userFriends.includes(friend)) {
         recommendedFriend = friends[i][1];
@@ -34,9 +37,23 @@ function problem7(user, friends, visitors) {
     }
   }
 
+  /* 추천 점수 1점 */
+
   var notUserFriends = visitors.filter(
     (element) => !userFriends.includes(element)
   );
+
+  for (var i = 0; i < notUserFriends.length; i++) {
+    recommededFriend = notUserFriends[i];
+    score = 1;
+
+    if (resultMap.has(recommededFriend)) {
+      resultMap.set(recommededFriend, resultMap.get(recommededFriend) + 1);
+      continue;
+    }
+
+    resultMap.set(recommededFriend, 1);
+  }
 
   return answer;
 }
