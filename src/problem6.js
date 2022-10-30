73359f546812c3038f08c90f60dcd07d5c4441ae
cruelladevil <dev.timetravel@gmail.com>
2022-10-30 23:24:23 +0900

    feat(PROBLEM6): add getAdjacentStrings

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,15 @@ function problem6(forms) {
   return answer;
 }
 
+function getAdjacentStrings(string) {
+  const adjacentStrings = [];
+
+  for (let i = 0; i < string.length - 1; i += 1) {
+    const adjacentString = string.slice(i, i + 2);
+    adjacentStrings.push(adjacentString);
+  }
+
+  return adjacentStrings;
+}
+
 module.exports = problem6;
