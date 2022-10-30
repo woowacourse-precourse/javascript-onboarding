6984892d48423fb9bbeb60984b363fc1192192d1
DongRyeong Lee <dr11291129@gmail.com>
2022-10-30 21:49:50 +0900

    feat: P4 getTreeFrogWord 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,19 @@ function problem4(word) {
   return answer;
 }
 
+function getTreeFrogWord(word) {
+  let result = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if ("A" <= word[i] && word[i] <= "Z") {
+      result += String.fromCharCode(65 + 26 - (word.charCodeAt(i) - 65 + 1));
+    } else if ("a" <= word[i] && word[i] <= "z") {
+      result += String.fromCharCode(97 + 26 - (word.charCodeAt(i) - 97 + 1));
+    } else {
+      result += word[i];
+    }
+  }
+  return result;
+}
+
 module.exports = problem4;
