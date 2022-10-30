75163174f1e03522521c3189cefe882dade538fc
BBIYAC <kiy7833@gmail.com>
2022-10-30 14:57:06 +0900

    refactor(problem1): update variable and function name
    - 변수명과 함수명 가독성 좋게 리팩터링
    - boolean으로 리턴되는 값들 리팩터링

--- src/problem1.js
+++ src/problem1.js
@@ -1,11 +1,11 @@
 // 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구하는 함수
-function pageMax(page_num) {
+function getMaxPageNum(page) {
   let sum = 0, 
       multi = 1,
       mod;
 
-  for (let num = page_num; num > 0; num = parseInt(num / 10)) {
-    mod = num % 10;
+  for (let page_num = page; page_num > 0; page_num = parseInt(page_num / 10)) {
+    mod = page_num % 10;
     sum += mod;
     multi *= mod;
   }
@@ -13,14 +13,14 @@ function pageMax(page_num) {
 }
 
 // 왼쪽 페이지 번호의 결과와 오른쪽 페이지 번호의 결과 중 큰 수를 구하는 함수
-function pageScore(page) {
-  return Math.max(pageMax(page[0]), pageMax(page[1]));
+function getUserScore(pages) {
+  return Math.max(getMaxPageNum(pages[0]), getMaxPageNum(pages[1]));
 }
 
 // 포비와 크롱의 점수를 비교해 더 큰 사람을 구하는 함수
-function winner(pobi, crong) {
-  const pobi_score = pageScore(pobi),
-        crong_score = pageScore(crong);
+function getWinner(pobi, crong) {
+  const pobi_score = getUserScore(pobi),
+        crong_score = getUserScore(crong);
 
   if (pobi_score > crong_score) return 1;
   if (pobi_score < crong_score) return 2;
@@ -28,18 +28,18 @@ function winner(pobi, crong) {
 }
 
 // 왼쪽 페이지와 오른쪽 페이지가 연속되는지 확인하는 함수
-function isSerialPage(page){
-  return (Math.abs(page[0] - page[1]) === 1)? true: false;
+function isContinuousPage(pages){
+  return Math.abs(pages[0] - pages[1]) === 1;
 }
 
 // 포비와 크롱의 페이지가 둘 다 정상적인지 확인하는 함수
-function isSerialBoth(pobi, crong) {
-  return (isSerialPage(pobi) && isSerialPage(crong))? true: false;
+function isCorrectBoth(pobi, crong) {
+  return isContinuousPage(pobi) && isContinuousPage(crong);
 }
 
 function problem1(pobi, crong) {
-  if (!isSerialBoth(pobi, crong)) return -1;
-  return winner(pobi, crong);
+  if (!isCorrectBoth(pobi, crong)) return -1;
+  return getWinner(pobi, crong);
 }
 
 module.exports = problem1;
