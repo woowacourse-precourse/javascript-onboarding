bff6a953986834575e5979e943931066f6f354a3
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-30 15:45:56 +0900

    [문제4] <2단계 기능요구사항> 기능요구사항에 따라 알파벳의 아스키코드를  변환하는 함수

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,4 @@
 function problem4(word) {
-    // 입력받은 문자열을 아스키코드로 변환한 배열
-    const asciiAlphabetArray = word.split("").map((el) => el.charCodeAt());
-
     /** 1단계) 알파벳의 아스키코드를 입력받으면 해당 알파벳의 인덱스를 반환하는 함수
      * @returns => 알파벳의 인덱스 a/A:1, b/B:1, ... , z/Z:26
      */
@@ -11,6 +8,18 @@ function problem4(word) {
         }
         if (asciiAlphabet >= 97) {
             return asciiAlphabet % 96;
+        }
+    };
+
+    /** 2단계) 중앙(m/n)을 기준으로 정반대 위치의 알파벳에 해당하는 아스키코드로 변환하는 함수
+     * 예시) a=>z, m=>n, Z=>A, N=>M
+     */
+    const changeAlphabet = (asciiAlphabet, alphabetIndex) => {
+        if (alphabetIndex < 14) {
+            return asciiAlphabet + (14 - alphabetIndex) * 2 - 1;
+        }
+        if (alphabetIndex >= 14) {
+            return asciiAlphabet - (alphabetIndex - 13) * 2 + 1;
         }
     };
 }
