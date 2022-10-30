4a9a5c4413447a23d5d87f3545d63ad2bc5f5b84
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-10-30 21:44:44 +0900

    feat: solve problem3 1st

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
-function problem3(number) {
-  var answer;
+function problem3(number){
+  var answer=0;
+  let numStr;
+  const regex = new RegExp('[369]','g');
+  for (let i=1; i<=number; i++){
+      numStr = String(i).match(regex);
+      if(numStr){
+          answer+=numStr.length;
+      }
+  }
   return answer;
 }
 
