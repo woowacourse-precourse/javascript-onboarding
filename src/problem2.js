0ff9f6871cd08ea62b2345695db5228350f10e1a
hjy0951 <hjy0951@gmail.com>
2022-10-30 18:21:28 +0900

    problem2: 구현할 내용 정리

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
   return answer;
 }
 
+/* 암호 해독 함수 */
+/* 스택을 이용하여 중복된 문자 제거 */
+/*  루프를 통해 모든 문자 순회
+      스택이 비어있다면 문자 push
+      비어있지 않다면 top과 현재 문자 비교
+        같다면 top 문자 pop 
+        다르다면 현재 문자 push
+*/
+
 module.exports = problem2;
+
+/*
+browoanoommnaon
+*/
