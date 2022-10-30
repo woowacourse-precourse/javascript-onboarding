ed2f5145d2ca1a10c048bdb0f3df753e60ddeb45
김나현 <nahyeon.dev@gmail.com>
2022-10-31 00:01:39 +0900

    problem2 수정, 기능구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,9 @@
 function problem2(cryptogram) {
   //var answer;
-  let arr;
   let cnt = 0;
-  arr = cryptogram.split(""); // 문자열->배열
-  for(let i=0; i <= arr.length; i++){
+  let arr = cryptogram.split("");
+
+  for(let i=0; i < arr.length; i++){
     if(arr[i] == arr[i+1]){
       cnt = 1;
       let j = i+1;
@@ -13,7 +13,7 @@ function problem2(cryptogram) {
       }
       arr.splice(i, cnt+1);
       cnt = 0;
-      i = 0;
+      i = -1;
     }
   }
   arr = arr.join(""); //배열->문자열
