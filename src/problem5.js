424dbe1e87fa8b7511ad02048064406aec0b0ed6
b-hyoung <youqlrqod@naver.com>
2022-10-30 16:12:37 +0900

    add problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+
+  let array = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+  for (let i = 0; i < array.length; i++){
+    let temp = money / array[i]
+    let integer = Math.floor(temp)
+    answer.push(integer);
+    money %= array[i];
+}
   return answer;
 }
 
