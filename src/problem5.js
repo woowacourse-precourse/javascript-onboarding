26fe667f986a3b0b56f8345ea502aa342ce0ee72
Serin-Kim <serin9864@gmail.com>
2022-10-30 15:40:44 +0900

    feat: PROBLEM5 풀이 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,23 @@
+const coinList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let leftMoney = money;
+
+  for (coin of coinList) {
+    if (leftMoney >= coin) {
+      const usedCoinCount = Math.floor(leftMoney / coin);
+      answer.push(usedCoinCount);
+      leftMoney = leftMoney - coin * usedCoinCount;
+    } else answer.push(0);
+  }
+
   return answer;
+}
+
+const wallet = [50237, 15000];
+for (money of wallet) {
+  console.log(problem5(money));
 }
 
 module.exports = problem5;
