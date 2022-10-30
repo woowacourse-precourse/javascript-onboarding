9430621536da019561fcb15c3b37dfe4f6279c97
jinlee <vito9270@naver.com>
2022-10-31 01:26:06 +0900

    problem5 기능 요구사항 작성 및 기능 요구 사항 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,23 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  class moneyChanger {
+    constructor(money) {
+      this.money = money;
+    }
+    makeChange() {
+      const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+      const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+      let balance = this.money;
+      for (let i = 0; i < moneys.length; i++) {
+        while (balance >= moneys[i]) {
+          answer[i] = parseInt(balance / moneys[i]);
+          balance = parseInt(balance % moneys[i]);
+        }
+      }
+      return answer;
+    }
+  }
+
+  return new moneyChanger(money).makeChange();
 }
 
 module.exports = problem5;
