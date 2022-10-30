795b8ee749e89b9cf5f6863a8b863b4cbcffaa10
2yunseong <dbsdltjd3701@naver.com>
2022-10-30 15:11:08 +0900

    feat(문제 6번): 닉네임이 한글자라면 문제의 중복조건에 상관없이 동일한 닉네임이 있는지 검사하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,9 +1,25 @@
+const NICK_NAME = 1;
+
 function isLengthOne(nickname) {
   return nickname.length === 1;
 }
 
 function problem6(forms) {
   var answer;
+  const isDuplicate = new Array(forms.length).fill(0);
+
+  for (let i = 0; i < forms.length; i++) {
+    if (isLengthOne(forms[i][NICK_NAME])) {
+      for (let j = 0; j < forms.length; j++) {
+        if (i == j) continue;
+        if (isDuplicate[j] === 1) continue;
+        if (forms[i][NICK_NAME] === forms[j][NICK_NAME]) {
+          isDuplicate[i] = 1;
+          isDuplicate[j] = 1;
+        }
+      }
+    }
+  }
   return answer;
 }
 
