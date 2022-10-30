cfaaccbec0fe04a7b1f52013d1a26c3db0b54f60
paduck-96 <peterrabitt1234@naver.com>
2022-10-30 18:52:57 +0900

    problem4-1

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,30 @@
 function problem4(word) {
   var answer;
+  let upper = [];
+  let lower = [];
+  let upperReverse = [];
+  let lowerReverse = [];
+  let result = [];
+  for (let i = 0; i < 26; i++) {
+    upper.push(String.fromCharCode(65 + i));
+  }
+  for (let i = 0; i < 26; i++) {
+    lower.push(String.fromCharCode(97 + i));
+  }
+  upperReverse = [...upper].reverse();
+  lowerReverse = [...lower].reverse();
+  const text = word.split("");
+  for (let i = 0; i < text.length; i++) {
+    if (upper.includes(text[i])) {
+      result.push(upperReverse[upper.indexOf(text[i])]);
+      continue;
+    } else if (lower.includes(text[i])) {
+      result.push(lowerReverse[lower.indexOf(text[i])]);
+      continue;
+    }
+    result.push(text[i]);
+  }
+  answer = result.join("");
   return answer;
 }
 
