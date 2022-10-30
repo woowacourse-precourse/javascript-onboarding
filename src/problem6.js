11791e994d6eab30476cfaae113a703f8e716b8f
hyukzz <ri_chard@naver.com>
2022-10-31 00:27:59 +0900

    feat: [문제6]중복닉네임확인 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+	let answer = [];
+	let arr = [];
+
+	forms.map(el => {
+		const [_, nickName] = el;
+
+		for (let i = 0; i < nickName.length - 1; i++) {
+			let temp = nickName.slice(i, i + 2);
+			temp in arr ? arr[temp]++ : (arr[temp] = 1);
+		}
+	});
+
+	forms.map(el => {
+		const [email, nickName] = el;
+
+		for (let i = 0; i < nickName.length - 1; i++) {
+			let temp = nickName.slice(i, i + 2);
+
+			if (arr[temp] > 1) answer.push(email);
+		}
+	});
+	answer.sort();
+
+	return answer;
 }
 
 module.exports = problem6;
