e5dba022c7b64a006b7e455e8245dbf421aaf89e
하지훈 <jnh03336@gmail.com>
2022-10-30 15:19:54 +0900

    feat: pro4, change to frog char

--- src/problem4.js
+++ src/problem4.js
@@ -1,4 +1,7 @@
 const chageToFrogChar = (char) => {
+  const ascii = char.charCodeAt(0);
+  if(ascii >= 65 && ascii <= 90) return String.fromCharCode(155 - ascii);
+  if(ascii >= 97 && ascii <= 122) return String.fromCharCode(219 - ascii); 
   return char;
 }
 
