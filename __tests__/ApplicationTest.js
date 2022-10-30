f6b74c7d9f8f5bc1e9814b1472a17bf09489fed8
Blackraven93 <reblackraven@gmail.com>
2022-10-31 02:21:52 +0900

    Problem5[Feat] 돈의 가장 큰 단위부터 매개변수로 주어진 금액을 나누어 몫을 배열에 추가하는 기능 구현

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -2,7 +2,7 @@ const problem1 = require("../src/problem1");
 const problem2 = require("../src/problem2");
 const problem3 = require("../src/problem3");
 const problem4 = require("../src/problem4");
-// const problem5 = require("../src/problem5");
+const problem5 = require("../src/problem5");
 // const problem6 = require("../src/problem6");
 // const problem7 = require("../src/problem7");
 
@@ -44,15 +44,15 @@ describe("problem4", () => {
   });
 });
 
-// describe("problem5", () => {
-//   test("case1", () => {
-//     expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
-//   });
+describe("problem5", () => {
+  test("case1", () => {
+    expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
+  });
 
-//   test("case2", () => {
-//     expect(problem5(15000)).toEqual([0, 1, 1, 0, 0, 0, 0, 0, 0]);
-//   });
-// });
+  test("case2", () => {
+    expect(problem5(15000)).toEqual([0, 1, 1, 0, 0, 0, 0, 0, 0]);
+  });
+});
 
 // describe("problem6", () => {
 //   test("case1", () => {
--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let current = money;
+
+  [50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1].forEach((unit) => {
+    answer.push(Math.floor(current / unit));
+    current = current % unit;
+  });
+
   return answer;
 }
 
