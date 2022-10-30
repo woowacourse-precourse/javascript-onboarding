b7fff8cf2156ebdd1300a350c6811f73dceed3b9
kim-limit <rlawogks2468@naver.com>
2022-10-30 16:09:52 +0900

    feat: '중복 체크 방지 로직'

--- src/problem6.js
+++ src/problem6.js
@@ -6,6 +6,9 @@ function problem6(forms) {
   for (let i = 0; i < forms.length; i++) {
     length = result.length;
     for (let z = i + 1; z < forms.length; z++) {
+      if (result.includes(forms[z][0])) {
+        continue;
+      }
       for (let j = 0; j < forms[i][1].length - 1; j++) {
         if (forms[z][1].includes(forms[i][1].substr(j, j + 2))) {
           result.push(forms[z][0]);
