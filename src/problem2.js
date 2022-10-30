d3275e34e3ce89f7850119ba0f7a2a497b6fa433
sh981013s <sh981013s@gmail.com>
2022-10-30 21:51:44 +0900

    feat: [2] 중복문자 유무 확인 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,16 @@
+/**
+ * 더이상 중복문자를 제거할 필요가 없는지 확인한다.
+ * @param cryptogram
+ * @return {boolean}
+ */
+const isTaskFinished = (cryptogram) => {
+  for (let i = 1; i <= cryptogram.length; i++) {
+    if (cryptogram[i] === cryptogram[i - 1]) return false;
+  }
+
+  return true;
+};
+
 function problem2(cryptogram) {
   var answer;
   return answer;
