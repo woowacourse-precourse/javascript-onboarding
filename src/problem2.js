ec33aa645260737e39fbc150f4b36db5f07df538
runkey <csi9509@naver.com>
2022-10-31 00:40:47 +0900

    Feat: 반복되는 문자열이 없으면 종료, 아니면 빈문자열 바꿈

--- src/problem2.js
+++ src/problem2.js
@@ -4,7 +4,13 @@ function problem2(cryptogram) {
   while(true) {
       // a~z 가 두번 이상 반복되는 문자열을 전역 검색한다.
       let regExp = answer.match(/a{2,}|b{2,}|c{2,}|d{2,}|e{2,}|f{2,}|g{2,}|h{2,}|i{2,}|j{2,}|k{2,}|l{2,}|m{2,}|n{2,}|o{2,}|p{2,}|q{2,}|r{2,}|s{2,}|t{2,}|u{2,}|v{2,}|w{2,}|x{2,}|y{2,}|z{2,}/g);
-
+      
+      // 반복되는 문자열이 없으면 
+      if(regExp === null) break;
+      
+      for(let i = 0; i < regExp.length; i++) {
+          answer = answer.replace(regExp[i], "");
+      }
   }
   
   return answer;
