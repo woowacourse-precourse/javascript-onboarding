8a20a71d5ef5661b88b7454f090d4c472017c7ec
ekkim <eklikeable@kakao.com>
2022-10-30 21:09:49 +0900

    ✨ first implementation

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const currencies = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let result = [];
+  for (let currency of currencies) {
+    let numberOfTheCurrency = Math.floor(money / currency);
+    result.push(numberOfTheCurrency);
+    money = money - currency * numberOfTheCurrency;
+  }
+  return result;
 }
 
 module.exports = problem5;
--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,38 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  if (forms.length < 1 || forms.length > 10000) return 'Please check the data';
+
+  const duplicated = [];
+
+  forms.forEach((target, targetIndex) => {
+    const [targetEmail, targetNickName] = target;
+    if (isValid(targetEmail, targetNickName)) {
+      for (let i = targetIndex + 1; i < forms.length; i++) {
+        for (let j = 0; j < targetNickName.length - 1; j++) {
+          if (forms[i][1].search(targetNickName.substr(j, 2)) !== -1) {
+            duplicated.push(targetEmail, forms[i][0]);
+          }
+        }
+      }
+    }
+  });
+
+  const result = [...new Set(duplicated)].sort();
+  return result;
+}
+
+function isValid(targetEmail, targetNickName) {
+  const valPattern_nickName_number = /^[가-힣]{1,20}$/;
+  const valPattern_email = /^[a-zA-Z0-9]{1,9}(?=\@email\.com)/;
+
+  if (
+    valPattern_nickName_number.test(targetNickName) &&
+    valPattern_email.test(targetEmail)
+  ) {
+    return true;
+  } else {
+    console.log('Please check your information');
+    return false;
+  }
 }
 
 module.exports = problem6;
