acd169a8848c9e85db8037429c63a7bb1d620086
SeungWoo Kim <osabero@naver.com>
2022-10-30 17:17:35 +0900

    반복문을 이용하여 문자열을 앞에서부터 하나씩탐색하여 알파벳일경우 문자열변환을 아닐경우 그대로 문자열로 들어가도록 하였습니다. 마지막 기능구현은 전부 대문자인 배열을 만들어 findIndex을 활용해 index를 파악하고 그에 맞는 알파벳으로 문자열에 넣도록 하였습니다. 마지막 if문을 통해 아스키코드가 대문자이면 대문자로 아니면 소문자로 넣도록 구현하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,15 +1,20 @@
 function problem4(word) {
   var answer = "";
-  var alpha = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
-  var alphaReverse = alpha.sort()
+  var alpha = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"]
   for (let x of word) {
     if (x.charCodeAt()<65){answer += x; continue}
     if (x.charCodeAt()>122){answer += x; continue}
     if (x.charCodeAt()>90 && x.charCodeAt()<97){answer += x; continue}
     //알파벳 이외문자 예외처리
-    
-    let check = x.toUpperCase()
+    let check = x.toUpperCase();
+    let checkidx = (element) => element == check;
+    let index = alpha.findIndex(checkidx)
+    console.log(index)
+    if(x.charCodeAt()>=65 && x.charCodeAt()<=90) {
+      answer += alpha[25 - index]} else {
+      answer += alpha[25 - index].toLowerCase();}
   }
+  //알파벳 변환해서 새로운 문자열에 넣기
   return answer;
 
 }
