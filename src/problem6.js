d459532025df4fccd5f82ea4374cfbfdb52bc67b
Sangwon Lee <tkdtm2010@ajou.ac.kr>
2022-10-30 15:14:38 +0900

    problem6 완성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length-1; j++) {
+      let strFind = forms[i][1].substring(j, j + 2);
+      for (let k = i + 1; k < forms.length; k++) {
+        if (forms[k][1].includes(strFind)) {
+          answer.push(forms[i][0]);
+          answer.push(forms[k][0]);
+        }
+      }
+    }
+  }
+  
+  set =  new Set(answer);
+  answer = [...set];
+  answer.sort();
+
   return answer;
 }
 
