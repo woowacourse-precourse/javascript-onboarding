2f82c842f9c80f7cf11ded31a2e9f1963320e1d7
runkey <csi9509@naver.com>
2022-10-31 01:21:13 +0900

    Feat: 예외사항 재정의

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,18 @@
 function problem5(money) {
+  
   let result = [0, 0, 0, 0, 0, 0, 0, 0, 0]
-  let temp = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
-  for (let i = 0 ; i < 9 ; i++){
-    result[i] = parseInt(money/temp[i])
-    money -= result[i] * temp[i]
-  }
   
   // 예외사항
   if (money < 1 || money > 1000000) result = "예외사항";
+
+  else{
+    let temp = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+    for (let i = 0 ; i < 9 ; i++){
+      result[i] = parseInt(money/temp[i])
+      money -= result[i] * temp[i]
+    }
+  }
+
   return result;
 }
 
