c2f36e087ea5a1cb96a0a0536a18520b0cf21b35
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-10-30 20:23:06 +0900

    feat: [P4] 알파벳을 불러올 수 있는 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,59 @@
-function problem4(word) {
-  var answer;
-  return answer;
+function alphabetGather() {
+  const upperWord = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+  ];
+  const lowerWord = [
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  return [lowerWord, upperWord];
 }
-
-module.exports = problem4;
