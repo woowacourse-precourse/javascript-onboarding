f9111bbf42008694b1a20d4cc3f597d3f7fea99e
hyukzz <ri_chard@naver.com>
2022-10-30 23:17:51 +0900

    refactor: [문제5]반복문 대신 map 사용

--- src/problem5.js
+++ src/problem5.js
@@ -3,10 +3,10 @@ const UnitMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 function problem5(money) {
 	let answer = [];
 
-	for (let i = 0; i < UnitMoney.length; i++) {
-		answer.push(Math.floor(money / UnitMoney[i]));
-		money = money % UnitMoney[i];
-	}
+	UnitMoney.map(UnitMoney => {
+		answer.push(Math.floor(money / UnitMoney));
+		money = money % UnitMoney;
+	});
 
 	return answer;
 }
