9ba44053390c87a26f537e0763977320655a4ed2
sumin <csumin008255@gmail.com>
2022-10-30 18:43:31 +0900

    problem4_기능목록수정 및 기능1

--- src/problem4.js
+++ src/problem4.js
@@ -1,13 +1,13 @@
 /* 기능목록
-* 1. word를 문자 배열로 변환
-* 2. 공백이 아닐 경우 문자 배열의 각 요소를 아스키 코드로 변환
-* 3. 대문자, 소문자 구분 후 청개구리 사전에 맞게 변환
-* 4. 결과 출력
-* 5. 코드 정리
+* 1. word를 아스키 코드 배열로 변환
+* 2. 대문자, 소문자, 공백 구분 후 청개구리 사전에 맞게 변환
+* 3. 결과 출력
+* 4. 코드 정리
 */
 
 function problem4(word) {
-  var wordArr = word.toString().split("");
+  var result = "";
+  var wordArr = word.toString().split("").map(x=> x.charCodeAt([0]));
 }
 
 module.exports = problem4;
