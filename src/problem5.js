70c9b8f7eb1b073391b60268993fac4fcf37c98d
jhyns <tockiib@gmail.com>
2022-10-30 22:32:08 +0900

    Add problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+
+  [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1].forEach((unit) => {
+    answer.push(Math.floor(money / unit));
+    money = money % unit;
+  });
   return answer;
 }
 
