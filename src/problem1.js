e8e4d06cd7a14a6456d2242fdb618cfab3ae41c9
ming0830 <minsun0830@naver.com>
2022-10-30 14:46:37 +0900

    [PROBLEM1] feat : user의 점수를 구하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,41 @@
+const pobi = [97,98];
+const crong = [197,198];
+
+
+
+function score(userName){
+  let scoreList =[];
+  let leftsum = 0;
+  let rightsum = 0;
+  let leftmultiple = 1;
+  let rightmultiple = 1;
+  
+
+  const strNum = userName.map(v=>String(v));
+  const leftpageNums = strNum[0].split(''); //9,7
+  const rightpageNums = strNum[1].split(''); // 9,8
+
+  for(let i =0;i<leftpageNums.length;i++){
+    leftsum += parseInt(leftpageNums[i]);
+    rightsum += parseInt(rightpageNums[i]);
+    leftmultiple *= parseInt(leftpageNums[i]);
+    rightmultiple *= parseInt(rightpageNums[i]);
+  }
+
+  scoreList.push(leftsum);
+  scoreList.push(rightsum);
+  scoreList.push(leftmultiple);
+  scoreList.push(rightmultiple);
+  
+  return Math.max(...scoreList)
+}
+
+
+
+
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
 }
 
 module.exports = problem1;
