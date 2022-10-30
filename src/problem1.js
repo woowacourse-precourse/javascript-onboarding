98434e0c82836b6f879235d49d1cedf06fdabf16
nalsae <nalsae0207@naver.com>
2022-10-30 22:01:52 +0900

    feat: Add a function for creating result array
    - 각 페이지 배열의 합과 곱을 배열로 반환하는 함수를 분리하여 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,17 @@
-function problem1(pobi, crong) {
-  var answer;
+const createResults = (page) => {
+  const splitPage = page.toString().split('');
+
+  return [
+    splitPage.reduce((total, currentNum) => +total + +currentNum),
+    splitPage.reduce((total, currentNum) => +total * +currentNum),
+  ];
+};
+
+const problem1 = (pobi, crong) => {
+  let answer;
+  const pobiPages = pobi;
+  const crongPages = crong;
   return answer;
-}
+};
 
 module.exports = problem1;
