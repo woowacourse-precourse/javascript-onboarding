8a2185e7c3689215a3044a9fc57041ad0aed690a
oio337a <oio337a@gmail.com>
2022-10-30 20:14:20 +0900

    문제 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,15 @@
 function problem5(money) {
-  let answer;
+  let answer = [];
+  let tmp;
   const chage = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  for (i = 0; i < chage.length; i++) {
+    tmp = parseInt(money / chage[i]);
+    answer.push(tmp);
+    if (tmp != 0) {
+      money = money - tmp * chage[i];
+    }
+  }
   return answer;
 }
 
