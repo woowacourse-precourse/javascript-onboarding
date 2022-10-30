4e3ed99f2bae4a5fd107a3981e5e18407a636d42
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-31 01:01:30 +0900

    feat: 3번- 숫자의 각 자리를 반환하는 메서드 구현

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,7 @@ function problem3(number) {
   let answer = 0;
   //number까지의 숫자를 반환하는 메서드
   for(let i=1; i < number+1; i++) {
+    const eachNum = i.toString().split("").map(x => parseInt(x))
   }
   return answer;
 }
