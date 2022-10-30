21d7132b55a8cdfcf9f5fef18473d350985b7a50
Minyoung Lee <mminy62@gmail.com>
2022-10-30 14:39:45 +0900

    feat: [PROBLEM4] ascii 값 이용 문자 변환
    ascii로 문자가 아닌 값 확인, 대소문자 구분, ascii 값 이용해서 문자 변환하기

--- src/problem4.js
+++ src/problem4.js
@@ -1,19 +1,32 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
 
   //1. word string 을 ascii 코드로 전환 
-
   let wordArray = word.split('');
+  let convertAscii;
+
 
   wordArray.forEach(function(char){
-    console.log(char + ": " + char.charCodeAt(0));
-  })
+    charAscii = char.charCodeAt(0);
 
+    //2. ascii로 문자가 아닌 값 확인, 대소문자 구분, ascii 값 이용해서 문자 변환하기
+    if (charAscii < 65 || (charAscii > 90 && charAscii < 97) || charAscii > 122 ){
+      answer += char;
+    }
+    else{
+      if (charAscii > 90){ //A~Z : 65~90 // ASCII > 90: lowercase
+        convertAscii = 122 - (charAscii - 97);
+      }
+      else{
+        convertAscii = 90 - (charAscii - 65);
+      }
+      answer += String.fromCharCode(convertAscii);
+
+    }
+
+  })
 
   return answer;
 }
-
-
-problem4("I love you");
 
 module.exports = problem4;
