0ea3b57f025e591f4c94cf6b051e8e0d8f8d75e0
thewronghand <penfreak77@gmail.com>
2022-10-31 02:21:11 +0900

    feat: countClapTimes 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -6,10 +6,11 @@ function problem3(number) {
 module.exports = problem3;
 
 const splitToNumberArray = (n) => String(n).split('').map(char => Number(char));
+const countClapTimes = (n) => splitToNumberArray(n).filter(element => 3 === element || 6 === element || 9 === element).length
 
 //problem 3 기능 목록
-//[] 주어진 숫자를 자릿수별로 분리된 배열로 변환하는 기능을 구현한다.
-//[] 주어진 배열에 포함된 3, 6, 9 의 갯수를 세는 기능을 구현한다.
+//[x] 주어진 숫자를 자릿수별로 분리된 배열로 변환하는 기능을 구현한다.
+//[x] 주어진 배열에 포함된 3, 6, 9 의 갯수를 세는 기능을 구현한다.
 //[] 배열의 요소를 모두 합하는 기능을 구현한다.
 //[] for 반복문을 이용해 1부터 number까지 박수를 한 번이라도 쳐야 하는 숫자들의 박수 횟수를 array에 저장한다.
 //[] array에 저장된 값을 모두 더하여 answer로 하고, answer를 반환한다.
