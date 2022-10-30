25f736e52ead36a753de1e71737e871b1d757b84
kimyou1102 <yoosk5485@naver.com>
2022-10-30 22:19:47 +0900

    feat: 문제 4번 - 청개구리 사전 배열을 만든다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,8 @@
 function problem4(word) {
-  var answer;
+  let answer = '';
+  const upper_arr = ['Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
+  const lower_arr = upper_arr.map(x => x.toLowerCase());
+
   return answer;
 }
 
