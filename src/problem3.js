0aa582fc9cf4cad2a6c9e27d1f87335c31961528
Ryan-Dia <cjfdnjs158@naver.com>
2022-10-31 01:43:24 +0900

    feat: [문제3] 3,6,9의 개수에 따라 알맞은 result 출력

--- src/problem3.js
+++ src/problem3.js
@@ -2,8 +2,9 @@ function problem3(number) {
   let numberOf369 = "";
   for (i = 0; i <= number; i++) {
     let matchNumberOf369 = i.toString().match(/[369]+/g);
+    matchNumberOf369 && (numberOf369 += String(matchNumberOf369));
   }
-  return answer;
+  return numberOf369.length;
 }
 
 module.exports = problem3;
