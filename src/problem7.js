477dd0dd1d98d1146e8ba9c8eee18e123a52341a
Chaeduk <kkj2551@naver.com>
2022-10-30 11:43:56 +0900

    chore(problem7) : 가독성을 위해 if-else문 줄바꿈 처리

--- src/problem7.js
+++ src/problem7.js
@@ -47,7 +47,8 @@ function changeObjToArray(friendOfFriendsObj){
       if(a>b) return 1
       else if(a===b) return 0
       else return -1
-    } else return -1
+    } 
+    else return -1
   })
   return result
 }
