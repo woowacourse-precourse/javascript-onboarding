62f310603e2b6f7c314838c0f25df9dce2da25c1
thewronghand <penfreak77@gmail.com>
2022-10-31 02:19:39 +0900

    feat: splitToNumberArray 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -5,6 +5,8 @@ function problem3(number) {
 
 module.exports = problem3;
 
+const splitToNumberArray = (n) => String(n).split('').map(char => Number(char));
+
 //problem 3 기능 목록
 //[] 주어진 숫자를 자릿수별로 분리된 배열로 변환하는 기능을 구현한다.
 //[] 주어진 배열에 포함된 3, 6, 9 의 갯수를 세는 기능을 구현한다.
