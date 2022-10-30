2296cc14e3dcb03a16190461b2c6b7a9a35b2791
moonhee0507 <sjmh0507@naver.com>
2022-10-30 22:17:37 +0900

    ✏️ 문제 6 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,59 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    const email = forms.map((el) => el[0]);
+    const nickName = forms.map((el) => el[1]);
+    const array = new Array();
+
+    for (let i = 0; i < forms.length; i++) {
+        const collection = new Array();
+        for (let j = 0; j <= nickName[i].split('').length - 2; j++) {
+            for (let k = j + 2; k <= nickName[i].split('').length; k++) {
+                collection.push(nickName[i].split('').slice(j, k));
+            }
+        }
+        array.push(collection);
+    }
+
+    const compare = new Array();
+    for (let i = 0; i < forms.length; i++) {
+        for (let j = 0; j < array[i].length; j++) {
+            for (let k = i + 1; k < forms.length; k++) {
+                for (let l = 0; l < array[k].length; l++) {
+                    if (
+                        JSON.stringify(array[i][j]) ===
+                        JSON.stringify(array[k][l])
+                    ) {
+                        compare.push(array[i][j]);
+                    }
+                }
+            }
+        }
+    }
+
+    const duplicate = compare.filter((element, index) => {
+        return (
+            JSON.stringify(compare).indexOf(JSON.stringify(element)) === index
+        );
+    });
+
+    const index = new Array();
+    for (let i = 0; i < array.length; i++) {
+        for (let j = 0; j < duplicate.length; j++) {
+            if (
+                JSON.stringify(array[i]).includes(JSON.stringify(duplicate[j]))
+            ) {
+                index.push(i);
+            }
+        }
+    }
+
+    const duplicateEmail = new Array();
+    for (let i = 0; i < index.length; i++) {
+        duplicateEmail.push(email[index[i]]);
+    }
+
+    const answer = duplicateEmail.sort();
+
+    return answer;
 }
 
 module.exports = problem6;
