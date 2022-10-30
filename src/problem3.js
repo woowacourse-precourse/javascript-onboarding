5377dffb635e5ce7a43370a752db333b95b4ac27
yiju_Kim <kls9939@naver.com>
2022-10-31 01:07:52 +0900

    refactor: Modify problem 3 business logic
    - 제한사항에 대한 에러처리
    - 변수명 변경
    - if문을 삼항연산자로 변경

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,14 @@
 function problem3(number) {
-  let count = 0;
-  for (let i = 1; i < number + 1; i++) {
-    const arr = String(i).split('');
+  if (number <= 0 || number > 10000 || number !== parseInt(number)) throw new Error('매개변수는 1이상 10,000 이하의 자연수만 입력 가능합니다.');
 
-    arr.forEach(elem => {
-      if (elem === '3' || elem === '6' || elem === '9') {
-        count += 1;
-      }
-    })
+  let clapCount = 0;
+  for (let i = 1; i < number + 1; i++) {
+    const splitStr = String(i).split('');
+
+    splitStr.forEach(elem => clapCount = elem === '3' || elem === '6' || elem === '9' ? clapCount + 1 : clapCount);
   }
   
-  return count;
+  return clapCount;
 }
 
 module.exports = problem3;
