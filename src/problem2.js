cfb2f043e5fe0b67d29e306347da8a4479a68216
동동이 <drdragon777@naver.com>
2022-10-30 18:32:22 +0900

    PROBLEM2 - initial commit(solved)

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const result = [];
+  const chList = cryptogram.split("");
+  chList.forEach((ch) => {
+    if (ch === result[result.length - 1]) result.pop();
+    else result.push(ch);
+  });
+  return result.join("");
 }
 
 module.exports = problem2;
