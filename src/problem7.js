30658a335026a898831423e2509d808c5bf0c1f5
이예나 <yenalee@MacBook-Pro.local>
2022-10-31 00:58:40 +0900

    Feat: Make alreadyFriend List

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,24 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  var alreadyFriend = [];
+
+  for(let friend in friends){
+    if(friends[friend].includes(user)){
+      for(let i in friend){
+        if(friends[friend][i]!==user){
+          alreadyFriend.push(friends[friend][i]);
+        }
+      }
+    }
+  }
+
   return answer;
 }
 
 module.exports = problem7;
+
+user = "mrko";
+friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ] ;
+visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"] ;
+//result =  ["andole", "jun", "bedi"] 
+problem7(user,friends,visitors);
