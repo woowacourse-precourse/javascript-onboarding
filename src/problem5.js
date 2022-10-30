d700e3a720705b1eb214e418c13c2e95e1dbcb61
updownpark2 <101778169+updownpark2@users.noreply.github.com>
2022-10-30 20:37:58 +0900

    feat: [P5] 돈을 화폐단위로 나누고 나머지를 이어 계속 나누는 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
-function problem5(money) {
-  var answer;
+function solution(money) {
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let userMoney = money;
+  let answer = [];
+
+  coins.forEach((item) => {
+    answer.push(Math.floor(userMoney / item));
+    userMoney = userMoney % item;
+  });
+
   return answer;
 }
-
-module.exports = problem5;
