61764b92507ac025165db019cfd9a93d8debccc8
wnw <fkaus65@naver.com>
2022-10-30 18:37:44 +0900

    feat(problem4): add getFrogIndex, getAlphasTypeOf function getFrogIndex: Returns the index depending on whether it is uppercase or not getAlphasTypeOf: Generate alphabetical array corresponding to CODE

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
 const isUpper = (char) => /[A-Z]/.test(char);
 
+const getFrogIndex = (char) => {
+  const ALPHA_SIZE = 25;
+  const UPPER = 65;
+  const LOWER = 97;
+
+  return ALPHA_SIZE - (char.charCodeAt() - (isUpper(char) ? UPPER : LOWER));
+};
+
+const getAlphasTypeOf = (CODE) => {
+  const ALPHA_SIZE = 25;
+
+  return Array.from({ length: ALPHA_SIZE + 1 }, (_, index) =>
+    String.fromCharCode(index + CODE)
+  );
+};
+
 function problem4(word) {
   const UPPER = 65;
   const LOWER = 97;
