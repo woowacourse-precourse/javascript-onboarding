8daa3b7cbd1033fb19c4c24e6bac9efb89b97b20
Jyeo <dhkfkr134@naver.com>
2022-10-31 04:38:07 +0900

    문제3 솔루션 추가(369게임) ㅇ문제3. 369게임에서 number의 숫자까지 몇번의 박수가 나오는지 찾는 것이다. 정규표현식을 사용하여 3|6|9에 해당하는 문자열을 match()시켜서 찾아냈다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
+// 2022 Kimjungwon 우아한테크코스 문제풀이 3
+
 function problem3(number) {
-  var answer;
+
+  //제한사항 : number는 1이상 10,000 이하인 자연수이다.
+  if (number < 1 || number > 10000) {
+    return -1;
+  }
+  //변수 regexp369는 '3','6','9'를 판별하는 정규표현식이다.
+  let answer;
+  let count = 0;
+  const regexp369 = /3|6|9/g;
+
+  //match()의 결과 일치하는 것이 없으면 null을 반환하므로 '|| []'을 추가했다.
+  for (let i = 1; i < number + 1; i++) {
+    count += (String(i).match(regexp369) || []).length;
+  }
+
+  answer = count;
   return answer;
 }
 
