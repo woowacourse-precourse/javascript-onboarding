ca80b94684da9b9c266fe4fbda95ba36e0fa1e52
Hwang Juhee <julia0276@naver.com>
2022-10-30 16:08:44 +0900

    :sparkles: [feat] 결과값 오름차순 정렬 구현 #P6

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,12 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  // 중복 여부 확인을 위한 값 추가
+  forms.forEach(crew => crew[2] = false);
+  
+
+  // 출력값 중복 제거 훅 오름차순 정렬
+  answer = [...new Set(answer)];
+  answer.sort();
   return answer;
 }
 
