f28968759435a44f30d6b5ce8dac2876ace8692c
sujungYu <su_moment@naver.com>
2022-10-30 18:00:13 +0900

    feat(PROBLEM7):친구 리스트 객체 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    let userFriends = [];
+    let answer = [];
+    let friendsObj = setList(friends, visitors);
+}
+
+function setList(friends, visitors) {
+    let friendsObj = {};
+    friends.forEach(v=> {
+        const [A, B] = v;
+        if(!friendsObj[A]) friendsObj[A] = 0;
+        if(!friendsObj[B]) friendsObj[B] = 0;
+    });
+    visitors.forEach(v=> {
+        if(!friendsObj[v]) friendsObj[v] = 0;
+    });
+    return friendsObj;
 }
 
 module.exports = problem7;
