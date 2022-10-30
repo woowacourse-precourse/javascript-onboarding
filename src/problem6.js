585b71de3fa2b6730867c4279b0dbb8c0f1d01eb
kimeuns <dmstn2006@gmail.com>
2022-10-30 18:02:15 +0900

    문제6 이메일 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
+  var answer=[];
+  let overlappingObj = makeOverlappingObj(forms)
+  
   return answer;
 }
+
+function checkEmail(email){
+  if(email.length<11 || email.length>=20){
+    return true
+  }
+  let emailArr = email.split("@")
+  if(emailArr.includes("email.com")){
+    return true
+  }
+  return false
+}
+
+
 
 module.exports = problem6;
