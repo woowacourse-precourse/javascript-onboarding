3269b7aad52ee3cf8b75ab93983e121613f6e01e
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 16:56:07 +0900

    feat(problem3): 숫자의 각 자릿수마다 3, 6, 9의 존재 확인 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  var regExp369 = /[369]/g;
+  var array369;
+
+  for (var i = 1; i <= number; ++i) {
+    if (array369 = String(i).match(regExp369)) {
+      answer += array369.length;
+    }
+  }
   return answer;
 }
 
