def3db130fffe1eb7633970ed95a91f27cfe0ca8
oio337a <oio337a@gmail.com>
2022-10-30 18:29:00 +0900

    문자가 대문자일 경우 변환

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,9 @@ function problem4(word) {
 
   for (i = 0; i < word.length; i++) {
     ascii = word.charCodeAt(i);
+    if (65 <= ascii && ascii <= 90) {
+      answer += String.fromCharCode(65 + (90 - ascii));
+    }
   }
   return answer;
 }
