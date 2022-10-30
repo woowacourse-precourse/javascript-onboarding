2e4cbade33c12aadd9c34421c6fb78d6f26e5e11
kim-limit <rlawogks2468@naver.com>
2022-10-30 14:45:27 +0900

    feat: 'add devide logic'

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
+const arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+const result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
 function problem5(money) {
   var answer;
+  for (let i = 0; i < 9; i++) {
+    result[i] = Math.floor(money / arr[i]);
+  }
   return answer;
 }
 
