a3837501f6c37776ea65f5c40b3829f2ff03c7ba
Azamman327 <wjsdbdud327@naver.com>
2022-10-30 19:57:02 +0900

    Refactor: [P4] 소문자/대문자 청개구리 알파벳 변환 함수 삭제

--- src/problem4.js
+++ src/problem4.js
@@ -14,22 +14,10 @@ function isAlpha(word) {
   ch = word.charCodeAt(0);
 
   if (ch >= 65 && ch <= 90)   //대문자
-    ch = upperAlphaChange(ch);
+    return 155 - ch;          //(65, 90), (66, 89), (67, 88)...  각 순서쌍의 합은 155, 즉 155-ch는 청개구리 알파벳 값과 동일
   else if (ch >= 97 && ch <= 122)   //소문자
-    ch = lowerAlphaChange(ch);
-  
+    return 219 - ch;                //(97, 122) -> 97 + 122 = 219
   return ch;   //결과값 ch return
-}
-
-function upperAlphaChange(ch) {
-  //(65, 90), (66, 89), (67, 88)...이렇게 대응
-  //각 순서쌍의 합은 155, 즉 155-ch return
-  return 155 - ch;
-}
-
-function lowerAlphaChange(ch) {
-  //(97, 122) -> 97 + 122 = 219
-  return 219 - ch;
 }
 
 module.exports = problem4;
