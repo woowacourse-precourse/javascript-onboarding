b3a3830c8b3435d307c55bd56311f9d36ab8f599
Sim <zh025700@naver.com>
2022-10-31 02:36:55 +0900

    feat: problem1 validChecker

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,14 @@
 function problem1(pobi, crong) {
   var answer;
+  validChecker(pobi, crong);
+
   return answer;
+}
+
+function validChecker(pobi, crong) {
+  return (
+    Math.abs(pobi[0] - pobi[1]) !== 1 || Math.abs(crong[0] - crong[1]) !== 1
+  );
 }
 
 module.exports = problem1;
