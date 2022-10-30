ce4fd1759704417368bf92c39c5df6104177038b
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-30 20:35:25 +0900

    feat: [P5] 돈의 단위 별 갯수 계산

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
+// 돈의 단위를 위한 상수
+const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+// 돈의 단위 별 갯수 계산
 function problem5(money) {
-  var answer;
-  return answer;
+  const result = Array.from({length: 9}, () => 0);
+
+  MONEY_UNIT.map((unit, idx)=> {
+    if(parseInt(money / unit) > 0) {
+      result[idx] = parseInt(money / unit);
+      money = money % unit;
+    }
+  })
+  return result;
 }
 
 module.exports = problem5;
