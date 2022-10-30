cacb9c844baca3d154f0e4eec8c29402077421f0
useon <yoosk4986@naver.com>
2022-10-31 02:30:24 +0900

    feat: [problem4] 서로 반대로 대응되는 대문자를 담은 Map 객체 만드는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,13 @@
 function problem4(word) {
-  var answer;
+  let answer = '';
+  const upperCaseMap = new Map();
+  let upperCaseCode = 90;
+
+  // 1. 서로 반대로 대응되는 대문자를 담은 Map 객체 만드는 기능
+  for (let ASCII = 65; ASCII <= 90; ASCII++) {
+    upperCaseMap.set(String.fromCharCode(ASCII), String.fromCharCode(upperCaseCode));
+    upperCaseCode--;
+  }
   return answer;
 }
 
