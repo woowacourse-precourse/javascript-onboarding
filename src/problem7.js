ebad15deb68a53a22efde6d8aea8b625904e17b3
BaeDongWoo <ehddn5410@naver.com>
2022-10-31 00:02:55 +0900

    친구의 친구 찾기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,34 @@
 function problem7(user, friends, visitors) {
   var answer;
-  return answer;
+  let FR=[];
+  for(let i=0;i<friends.length;i++){//user와 친구인 friends 찾기
+    if(friends[i][0]===user){
+      FR.push(frineds[i][1]);
+    }
+    if(friends[i][1]===user){
+      FR.push(friends[i][0]);
+    }
+  }
+  let FR_FR=[];
+  for(let i=0;i<FR.length;i++){//친구의 친구 저장
+    for(let j=0;j<friends.length;j++){
+      if(FR[i]===friends[j][1]){
+        FR_FR.push(friends[j][0]);
+      }
+      if(FR[i]===friends[j][0]){
+        FR_FR.push(friends[j][1]);
+      }
+    }
+  }
+  FR_FR= new Set(FR_FR);//중복제거
+ /* for(let i=0;i<visitors.length;i++){
+    for(let j=0;j<FR_Score.length;j++){
+      if(visitors===FR_Score[j][1]){
+
+      }
+    }
+  }
+  return answer;*/
 }
 
 module.exports = problem7;
