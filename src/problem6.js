0e3c86f26ff2d1e3bfbc13d430270e8dc35f248b
강인영 <kiy@gang-in-yeong-ui-MacBookAir.local>
2022-10-31 00:00:40 +0900

    feat: problem6 function2. Add Email at Set and Sort Array in Ascending Order

--- src/problem6.js
+++ src/problem6.js
@@ -3,12 +3,12 @@
 //      두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어 있는지 체크
 //2. 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거한 다음 result 배열 반환
 
-function checkNickname(forms, index){
+function checkNickname(forms, index){ //'닉네임'에서 중복 체크
   var targetName = forms[index][1]; //기준 문자열
   var compName; //기준 문자열과 같은지 비교할 문자열, 
   var tmpString = "";
   var flag = 1; //중복 여부 판단
-  var duplicatedIndex = new Set(); //중복 닉네임을 가진 크루의 인덱스 저장
+  var duplicatedIndex = new Set(); //중복 닉네임을 가진 크루의 인덱스를 집합에 저장
 
   for (i=index+1; i<forms.length; i++){
       compName = forms[i][1]; //비교할 크루의 닉네임
@@ -26,6 +26,15 @@ function checkNickname(forms, index){
   return Array.from(duplicatedIndex); //중복 닉네임을 가진 크루의 인덱스 반환
 }
 
+function sortArray(set){
+  const array = Array.from(set);
+  var resultArray; //result 배열
+  //console.log("array is..",array);
+  resultArray = array.sort(); //이메일에 해당하는 부분의 문자열을 오름차순으로 정렬
+
+  return resultArray
+}
+
 function problem6(forms) {
   var answer, duplicatedName;
   var duplicatedArray = [];
@@ -36,7 +45,12 @@ function problem6(forms) {
     duplicatedArray = checkNickname(forms, i); //중복 닉네임이 있는지 확인
     console.log(duplicatedArray); //중복되는 모든 크루의 인덱스 출력
   }
-
+  for(j=0; j<duplicatedArray.length; j++){
+    var duplicatedIndex = duplicatedArray[j];
+    emailSet.add(forms[duplicatedIndex][0]);
+  }
+  //console.log(emailSet);
+  answer = sortArray(emailSet);
   
   return answer;
 }
