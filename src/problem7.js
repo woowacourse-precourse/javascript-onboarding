351c62f3249099a7320cfdf3b8f2fe8001c1f40d
foundaway98 <foundaway98@naver.com>
2022-10-30 10:43:18 +0900

    문제 7번 makeFriendsObj 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,16 @@
 //friends에 있는 모든 사람을 대상으로 하는 FriendObj를 생성한다.
-function makeFriendsObj() {}
+function makeFriendsObj(friends) {
+  let friendsObj = {};
+  for (let j = 0; j < 2; j++) {
+    for (let i = 0; i < friends.length; i++) {
+      if (!(friends[i][j] in friendsObj)) {
+        friendsObj[friends[i][j]] = new Array();
+      }
+      friendsObj[friends[i][j]].push(friends[i][j == 1 ? 0 : 1]);
+    }
+  }
+  return friendsObj;
+}
 //추천 점수를 저장하는 pointObj를 생성한다.
 function makePointObj() {}
 //user와 같은 친구를 가진 사람들의 점수를 계산한다.
@@ -13,6 +24,8 @@ function makeAnswerArr() {}
 
 function problem7(user, friends, visitors) {
   var answer = [];
+  let friendObj = makeFriendsObj(friends);
+
   return answer;
 }
 
