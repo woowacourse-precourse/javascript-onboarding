7259db0b3f78ee6eba1d32bf26296bed3ef16a5d
이여진 <jinspace3710@gmail.com>
2022-10-31 06:32:01 +0900

    feat: problem1 기능 1 - 배열 값 유효성 체크 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,25 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+  if (isValid(pobi) && isValid(crong)) {
+  } else {
+    answer = -1;
+  }
   return answer;
 }
 
+/*
+배열값이 유효한지 확인하는 함수
+1. 왼쪽 페이지 번호가 홀수인지
+2. 오른쪽 페이지 번호가 짝수인지
+3. 왼쪽, 오른쪽 페이지 번호 차이가 1인지
+*/
+function isValid(bookArray) {
+  if (
+    bookArray[0] % 2 === 1 &&
+    bookArray[1] % 2 === 0 &&
+    bookArray[1] - bookArray[0] === 1
+  )
+    return true;
+  else return false;
+}
 module.exports = problem1;
