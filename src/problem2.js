8e66416ad799b314f9af8a01bc3e66610dc2d09a
heejorika <y20001206@gmail.com>
2022-10-31 03:07:06 +0900

    cryptogram이 유효한 문자열인지 확인하는 기능구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
+/*
 function problem2(cryptogram) {
   var answer;
   return answer;
 }
 
 module.exports = problem2;
+*/
+
+let cryptogram="Browoanoommnaon";
+let c = ""
+console.log(c)
+
+// cryptogram이 유효한 문자열인지 확인
+function checkValid(c){
+  if (c.length<1) {return false;}
+  return c.toLowerCase();
+}
+
+console.log(checkValid(c));
