c2af145305bb41d604ed07ae3154e778d1d7763b
하지훈 <jnh03336@gmail.com>
2022-10-30 14:03:27 +0900

    feat: pro2, remove duplication

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,22 @@
-const removeDuplication = (crypto) => {
-  return crypto
+const removeDuplication = (string) => {
+  const chars = [];
+
+  for(let i = 0; i < string.length; i++){
+    if(i+1 < string.length && string[i] === string[i+1]) {
+      i++
+      continue;
+    } 
+    chars.push(string[i]);
+  }
+
+  return chars.join("");
 }
 
 function problem2(cryptogram) {
   var answer = cryptogram;
   while(true){
     const processedCrypto = removeDuplication(answer);
+    
     if (processedCrypto === answer) break;
     answer = processedCrypto;
   }
