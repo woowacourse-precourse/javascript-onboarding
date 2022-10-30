051cba68fffa64ea8c3528eef2b4bdf56ea481e4
하지훈 <jnh03336@gmail.com>
2022-10-30 15:13:12 +0900

    feat: pro4, logic design

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
+const chageToFrogChar = (char) => {
+  return char;
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const chars = [];
+  for(let i = 0; i < word.length; i++){
+    chars.push(chageToFrogChar(word[i]));
+  }
+  
+  return chars.join("");
 }
 
 module.exports = problem4;
