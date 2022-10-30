aca20b116c0ba1c456a16ca53a65ef36dbf3caa6
ywoo12321 <ywoo12321@gmail.com>
2022-10-30 17:38:39 +0900

    problem 2 해결방안 구상

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,33 +1,33 @@
 const problem1 = require("../src/problem1");
-// const problem2 = require("../src/problem2");
-// const problem3 = require("../src/problem3");
-// const problem4 = require("../src/problem4");
-// const problem5 = require("../src/problem5");
-// const problem6 = require("../src/problem6");
-// const problem7 = require("../src/problem7");
+const problem2 = require("../src/problem2");
+const problem3 = require("../src/problem3");
+const problem4 = require("../src/problem4");
+const problem5 = require("../src/problem5");
+const problem6 = require("../src/problem6");
+const problem7 = require("../src/problem7");
 
-describe("problem1", () => {
-  test("case1", () => {
-    expect(problem1([97, 98], [197, 198])).toEqual(0);
-  });
-
-  test("case2", () => {
-    expect(problem1([131, 132], [211, 212])).toEqual(1);
-  });
-
-  test("case3", () => {
-    expect(problem1([99, 102], [211, 212])).toEqual(-1);
-  });
-});
-
-// describe("problem2", () => {
+// describe("problem1", () => {
 //   test("case1", () => {
-//     expect(problem2("browoanoommnaon")).toEqual("brown");
+//     expect(problem1([97, 98], [197, 198])).toEqual(0);
 //   });
+
 //   test("case2", () => {
-//     expect(problem2("zyelleyz")).toEqual("");
+//     expect(problem1([131, 132], [211, 212])).toEqual(1);
+//   });
+
+//   test("case3", () => {
+//     expect(problem1([99, 102], [211, 212])).toEqual(-1);
 //   });
 // });
+
+describe("problem2", () => {
+  test("case1", () => {
+    expect(problem2("browoanoommnaon")).toEqual("brown");
+  });
+  test("case2", () => {
+    expect(problem2("zyelleyz")).toEqual("");
+  });
+});
 
 // describe("problem3", () => {
 //   test("case1", () => {
