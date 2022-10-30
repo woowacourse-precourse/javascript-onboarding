a1242fcb527d6b4501cfa0f81bac4682f6fe5a03
paduck-96 <peterrabitt1234@naver.com>
2022-10-30 19:04:26 +0900

    problem5-1

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
   var answer;
+  let result = [];
+  const arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < arr.length; i++) {
+    result.push(Math.floor(money / arr[i]));
+    money = Math.floor(money % arr[i]);
+  }
+  answer = result;
   return answer;
 }
 
