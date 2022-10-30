230f252d46260e33e76ffa0945dac26fef0fb098
seoyoung-dev <seoyoung.dev@gmail.com>
2022-10-31 01:07:57 +0900

    Add problem5 solution

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
-  var answer;
-  return answer;
-}
+    let answer = Array(9).fill(0);
+    let balance = money;
+    let quotient;
+    let bills = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+    for (let i = 0; i < 9; i++) {
+        quotient = Math.floor(balance / bills[i]);
+        if (quotient > 0) {
+            answer[i] = quotient;
+            balance = balance % bills[i];
+        }
+    }
+    return answer;
+}
 module.exports = problem5;
