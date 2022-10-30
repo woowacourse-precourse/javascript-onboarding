8833fdc72d748a8dca68cf742529274daf8635a5
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-30 23:36:20 +0900

    feat(Function): 비교할 이름과 그 외의 이름들 출력(기능목록 1번 구현)

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const arr = new Set();
+  let index = 0;
+  let state = Boolean;
+  for (let [email, nickname] of forms) {
+    state = false;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      let twoChar = nickname[i] + nickname[i + 1];
+      for (let j = index + 1; j < forms.length; j++) {
+        console.log(twoChar, forms[j]);
+      }
+    }
+    index++;
+  }
 }
 
 module.exports = problem6;
