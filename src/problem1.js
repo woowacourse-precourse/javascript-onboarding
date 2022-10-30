370e5f9252c839032bceb27a66b0cfd69d44491a
d-charlie-kim <dokkim@student.42seoul.kr>
2022-10-31 05:53:40 +0900

    feat: <problem1> problem1 메소드  구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,10 +1,35 @@
+function exceptionCheck(array) {
+
+}
+
+function getMultiplyOrSum(num) {
+
+}
+
 function getBiggestNum(array) {
-  
+
+}
+
+function compareTwoNum(first, second) {
+  if (first > second)
+    return (1);
+  else if (first < second)
+    return (2);
+  else
+    return (0);
 }
 
 function problem1(pobi, crong) {
-  var answer;
-  
+  try {
+    exceptionCheck(pobi);
+    exceptionCheck(crong);
+  } catch {
+    return (-1);
+  }
+
+  let pobiNum = getBiggestNum(pobi);
+  let crongNum = getBiggestNum(crong);
+  let answer = compareTwoNum(pobiNum, crongNum);
   return answer;
 }
 
