9136f677201658fdf1ae89d701c43cc56b4966d9
BaeDongWoo <ehddn5410@naver.com>
2022-10-30 18:54:52 +0900

    각 동전마다 갯수를 카운트 후 배열에 저장

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,34 @@
 function problem5(money) {
   var answer;
+  let money_cnt=[];//각 동전의 갯수를 카운트할 배열
+ 
+  money_cnt[0]=parseInt(money/50000);
+  money=parseInt(money%50000);
+
+  money_cnt[1]=parseInt(money/10000);
+  money=parseInt(money%10000);
+
+  money_cnt[2]=parseInt(money/5000);
+  money=parseInt(money%5000);
+ 
+  money_cnt[3]=parseInt(money/1000);
+  money=parseInt(money%1000);
+
+  money_cnt[4]=parseInt(money/500);
+  money=parseInt(money%500);
+
+  money_cnt[5]=parseInt(money/100);
+  money=parseInt(money%100);
+ 
+  money_cnt[6]=parseInt(money/50);
+  money=parseInt(money%50);
+
+  money_cnt[7]=parseInt(money/10);
+
+  money_cnt[8]=parseInt(money%10);
+
+  
+  answer=money_cnt;
   return answer;
 }
 
