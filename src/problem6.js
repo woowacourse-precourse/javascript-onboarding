58749239aab35229992f0ce8c56b582f0932e690
Jiho1996 <jhp9270@naver.com>
2022-10-30 18:53:54 +0900

    feat : 처음 obj에 추가 될 시 value를 배열로 저장 로직 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,17 @@
 function problem6(forms) {
+  const dictionary = {};
 
   forms.forEach((formList) => {
-      stack = [];
+      const stack = [];
       for (let letter of formList[1]){
           if (stack.length < 2)
               stack.push(letter);
           if (stack.length === 2){
               x = stack.join('')
+              if (!dictionary.hasOwnProperty(x)){
+                dictionary[x] = [formList[0]]
+              }
+              
           }
       } 
   })
