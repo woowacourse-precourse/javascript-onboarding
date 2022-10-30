3e9131ba6c67e1567a573a69e63eba8be5465dc3
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-30 15:58:01 +0900

    feat: [P6] 닉네임을 연속적인 두 글자의 단어로 나누고, 각 단어를 해당 크루의 이메일과 매칭한 객체를 반환하는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,18 @@ function problem6(forms) {
   return answer;
 }
 
+function formAnalyzer(form) {
+  const getSubstring = (nickname) => {
+      let substring = []
+      for (let i = 0; i < nickname.length - 1; i++) {
+          substring.push(nickname.substring(i, i + 2))
+      }
+      return new Set(substring)
+  }
+  let email = form[0]
+  let matchingEmail = new Map()
+  getSubstring(form[1]).forEach((word) => matchingEmail.set(word, email))
+  return matchingEmail
+}
+
 module.exports = problem6;
