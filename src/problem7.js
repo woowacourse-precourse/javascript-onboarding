3406e0770ac9077b7eb6f59641b7fc69707afe68
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-30 18:24:02 +0900

    [Modified] problem7.js

--- src/problem7.js
+++ src/problem7.js
@@ -1,80 +1,64 @@
-const findBfs = (user) => (couples) => {
-  return couples.reduce((reduced, couple) => {
-    const copy = reduced.slice();
-    if (couple.indexOf(user) > -1) {
-      const bf = couple.indexOf(user) === 1 ? couple[0] : couple[1];
-      copy.push(bf);
-      return copy;
-    }
-    return reduced;
-  }, []);
-};
+const compose =
+  (...fns) =>
+  (initialValue) =>
+    fns.reduce((composed, fn) => fn(composed), initialValue);
 
-const findFriendsOfBfs = (couples) => (bfs) => {
-  const ret = bfs.reduce((reduced, bf) => {
+const isNotExisting = (array, item) => array.indexOf(item) === -1;
+const getUserBfs = (user) => (friends) => {
+  return friends.reduce((reduced, couple) => {
     const copy = reduced.slice();
-    copy.push(...findBfs(bf)(couples));
+    const [_, bf] = couple.indexOf(user) === 0 ? couple : [...couple].reverse();
+    if (isNotExisting(copy, bf)) copy.push(bf);
     return copy;
   }, []);
-
-  return ret;
-};
-const exceptUser = (user) => (bfs) => {
-  return bfs.filter((bf) => bf !== user);
 };
 
-const countRate = (friends) => {
-  const ret = friends.reduce((reduced, friend) => {
+const getBfsOfBfs = (friends) => (users) => {
+  return users.reduce((reduced, user) => getUserBfs(user)(friends), []);
+};
+
+const removeUserAndUserBfs = (user, friends) => (bfs) => {
+  return bfs.filter(
+    (bf) => bf !== user && getUserBfs(user)(friends).indexOf(bf) === -1
+  );
+};
+
+const excludeUsers =
+  (...users) =>
+  () => {};
+
+const recordScore = (friends) => {
+  return friends.reduce((reduced, friend) => {
     const copy = { ...reduced };
     if (copy[friend]) copy[friend] += 10;
     else copy[friend] = 10;
     return copy;
   }, {});
-  return ret;
 };
 
-const addVisitorsToFriends = (visitors) => (friends) =>
-  visitors.reduce((reduced, visitor) => {
+const addVisitScore = (visitors) => (scores) => {
+  const copiedScores = { ...scores };
+  return visitors.reduce((reduced, visitor) => {
     const copy = { ...reduced };
     if (copy[visitor]) copy[visitor]++;
     else copy[visitor] = 1;
     return copy;
-  }, friends);
+  }, copiedScores);
+};
 
-const compose =
-  (...fns) =>
-  (arg) =>
-    fns.reduce((reduced, fn) => fn(reduced), arg);
+const getNames = (users) => Object.keys(users);
 
 function problem7(user, friends, visitors) {
   const result = compose(
-    findBfs(user),
-    findFriendsOfBfs(friends),
-    exceptUser(user),
-    countRate,
-    addVisitorsToFriends(
-      visitors.filter(
-        (visitor) => findBfs(user)(friends).indexOf(visitor) === -1
-      )
-    )
+    getUserBfs(user),
+    getBfsOfBfs(friends),
+    removeUserAndUserBfs(user, friends),
+    recordScore,
+    addVisitScore(removeUserAndUserBfs(user, friends)(visitors)),
+    getNames
   )(friends);
 
-  return Object.keys(result);
+  return result;
 }
-
-console.log(
-  problem7(
-    "mrko",
-    [
-      ["donut", "andole"],
-      ["donut", "jun"],
-      ["donut", "mrko"],
-      ["shakevan", "andole"],
-      ["shakevan", "jun"],
-      ["shakevan", "mrko"],
-    ],
-    ["bedi", "bedi", "donut", "bedi", "shakevan"]
-  )
-);
 
 module.exports = problem7;
