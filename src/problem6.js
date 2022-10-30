021fddb6f855dbc58f59aaa03eba683134ff17eb
jhyns <tockiib@gmail.com>
2022-10-30 22:51:02 +0900

    Add problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,32 @@
 function problem6(forms) {
-  var answer;
+  function setDict(crew, ind, dict) {
+    for (let i = 0; i < crew[1].length - 1; i++) {
+      dict = checkDup(crew[1].slice(i, i + 2), ind, dict);
+    }
+    return dict;
+  }
+
+  function checkDup(string, ind, dict) {
+    if (dict[string] === undefined) {
+      dict[string] = ind;
+    } else {
+      dict[dict[string]] = true;
+      dict[ind] = true;
+    }
+    return dict;
+  }
+
+  let answer = [], dict = [];
+
+  forms.forEach((crew, ind) => {
+    dict = setDict(crew, ind, dict);
+  });
+  dict.map((dup, index) => {
+    if (dup === true) {
+      answer.push(forms[index][0]);
+    }
+  });
+  answer.sort();
   return answer;
 }
 
