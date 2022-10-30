50a50f5646dfeb3eb7b8e8729d84087b85e9969a
Ji Seungha <jish0210@naver.com>
2022-10-30 19:23:23 +0900

    Problem 5 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  var answer = [0,0,0,0,0,0,0,0,0];
+  var unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for(var i = 0; i < 9; i++){
+    answer[i] = parseInt(money / unit[i]);
+    money = money % unit[i];
+  }
   return answer;
 }
 
