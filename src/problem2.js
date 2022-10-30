4f0d51c181f4355abdd764a0842c7666046bdf43
Ji Seungha <jish0210@naver.com>
2022-10-30 18:38:54 +0900

    Problem 2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,28 @@
 function problem2(cryptogram) {
   var answer;
-  return answer;
+  var bool = true;
+  var old_string = cryptogram;
+  var before_length = old_string.length;
+
+
+  while(bool){
+    var new_string= "";
+    for(var i = 0; i < before_length - 1; i++){
+      if(old_string[i] == old_string[i+1]) i++;
+      else new_string += old_string[i];
+    }
+    if(old_string[old_string.length-1] != old_string[old_string.length-2]) new_string += old_string[old_string.length-1];
+    
+    if(before_length == new_string.length){
+      answer = new_string;
+      bool = false;
+    }
+    else {
+      old_string = new_string;
+      before_length = new_string.length;
+    }
+  }
+  return answer;  
 }
 
 module.exports = problem2;
