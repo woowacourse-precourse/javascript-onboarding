c87680f02cd28b232e4a1420c0d78b7f608b0f5b
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-30 20:14:33 +0900

    feat: PROBLEM 6 이메일 형식 검사 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -3,6 +3,22 @@ const checkCrewNumber = (forms) => {
   return true;
 }
 
+const isCrewForm = (forms) => {
+  let realCrewArray = [];
+  forms.filter((element) => {
+    let email = element[0];
+    let domain = "@email.com"
+
+    if (email.length < 11 || 20 < email.length) return false;
+    if (!email.includes(domain)) return false;
+
+    realCrewArray.push(element);
+  })
+
+  if (realCrewArray.length !== forms.length) return false;
+  return true;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
