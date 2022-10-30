c4254fb7252de3df1e1b6bfc502d893a213e6ac1
박겸영 <yong_drg@naver.com>
2022-10-30 21:33:05 +0900

    feat: 문제 4 알파벳 대문자, 소문자 배열 생성 - 기능 1: 알파벳의 모든 대문자와 모든 소문자를 포함한 배열을 각각 생성하기

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,6 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const uppercases = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
+  const lowercases = uppercases.map((letter) => letter.toLowerCase());
 }
 
 module.exports = problem4;
