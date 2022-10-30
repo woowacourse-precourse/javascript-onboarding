e22b1bc87d3119d1b10732f4d0132875986251c0
HAEROOL <gofn080776@gmail.com>
2022-10-30 23:01:07 +0900

    feat: [problem4] - 주어진 문자열의 알파벳을 반대로 변경하는 함수 작성
    작성하면서 chageAlphabet의 대/소문자 변경 조건문을 changeString 함수로 위임함

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer = chageString(word)
   return answer;
+}
+
+function chageString(word){
+  let chagedString = word.split('').map(c => {
+    if(c === ' '){return ' '}
+    if(c.charCodeAt(0) > 96 && c.charCodeAt(0) < 123){
+      return changeAlphabet(c.toUpperCase()).toLowerCase();
+    }else{
+      return changeAlphabet(c);
+    }
+  })
+  return chagedString.join('');
 }
 
 function changeAlphabet(alphabet){
@@ -12,13 +24,7 @@ function changeAlphabet(alphabet){
     'R':'I','S':'H','T':'G','U':'F',
     'V':'E','W':'D','X':'C','Y':'B',
     'Z':'A',}
-  let chagedAlphabet = alphabet
-  if(alphabet.charCodeAt(0) > 96 && alphabet.charCodeAt(0) < 123){
-    chagedAlphabet = frogDiction[chagedAlphabet.toUpperCase()];
-    return chagedAlphabet.toLowerCase();
-  }else{
-    return frogDiction[chagedAlphabet];
-  }
+  return frogDiction[alphabet]
 }
 
 module.exports = problem4;
