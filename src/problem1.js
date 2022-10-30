18021cdf2da58976fab2369f918fd262ce74bc8e
Sim <zh025700@naver.com>
2022-10-31 02:37:50 +0900

    feat: problem1 maxValChecker

--- src/problem1.js
+++ src/problem1.js
@@ -11,4 +11,25 @@ function validChecker(pobi, crong) {
   );
 }
 
+function valueChecker(pages) {
+  const num1 = String(pages[0]),
+    num2 = String(pages[1]);
+
+  let num2P = parseInt(num2[0]),
+    num2M = parseInt(num2[0]),
+    num1P = parseInt(num1[0]),
+    num1M = parseInt(num1[0]);
+
+  for (let i of num1.slice(1)) {
+    num1P = num1P + parseInt(i);
+    num1M = num1M * parseInt(i);
+  }
+
+  for (let i of num2.slice(1)) {
+    num2P = num2P + parseInt(i);
+    num2M = num2M * parseInt(i);
+  }
+  return Math.max(Math.max(num1P, num1M), Math.max(num2P, num2M));
+}
+
 module.exports = problem1;
