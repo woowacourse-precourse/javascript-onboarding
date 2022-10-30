adcf5bab490e43b3d6f6946ab0aa5f8e78439a90
hozzijeong <ghwls0905@naver.com>
2022-10-30 19:15:46 +0900

    Feat: 2번문제 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 연속하는 중복 문자 삭제
+  var answer = "";
+  const len = cryptogram.length;
+
+  for (let i = 0; i < len; i++) {
+    const curChar = cryptogram[i];
+    if (curChar === cryptogram[i + 1] || curChar === cryptogram[i - 1])
+      continue;
+    else answer += curChar;
+  }
+  if (answer === cryptogram) return answer;
+  else return problem2(answer);
 }
 
 module.exports = problem2;
