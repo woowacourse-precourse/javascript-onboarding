de5568e1c8d522ed51cf3f710c7cb289902091e8
HelloHailie <alsltjsals@gmail.com>
2022-10-30 20:06:32 +0900

    problem4 test 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  let pos = number.toString().indexOf("3");
+  while (pos !== -1) {
+    answer++;
+    pos = number.toString().indexOf("3", pos + 1);
+  }
   return answer;
 }
 
--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,126 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabet = [
+    "A",
+    "B",
+    "C",
+    "D",
+    "E",
+    "F",
+    "G",
+    "H",
+    "I",
+    "J",
+    "K",
+    "L",
+    "M",
+    "N",
+    "O",
+    "P",
+    "Q",
+    "R",
+    "S",
+    "T",
+    "U",
+    "V",
+    "W",
+    "X",
+    "Y",
+    "Z",
+    "a",
+    "b",
+    "c",
+    "d",
+    "e",
+    "f",
+    "g",
+    "h",
+    "i",
+    "j",
+    "k",
+    "l",
+    "m",
+    "n",
+    "o",
+    "p",
+    "q",
+    "r",
+    "s",
+    "t",
+    "u",
+    "v",
+    "w",
+    "x",
+    "y",
+    "z",
+  ];
+  const reverseAlphabet = [
+    "Z",
+    "Y",
+    "X",
+    "W",
+    "V",
+    "U",
+    "T",
+    "S",
+    "R",
+    "Q",
+    "P",
+    "O",
+    "N",
+    "M",
+    "L",
+    "K",
+    "J",
+    "I",
+    "H",
+    "G",
+    "F",
+    "E",
+    "D",
+    "C",
+    "B",
+    "A",
+    "z",
+    "y",
+    "x",
+    "w",
+    "v",
+    "u",
+    "t",
+    "s",
+    "r",
+    "q",
+    "p",
+    "o",
+    "n",
+    "m",
+    "l",
+    "k",
+    "j",
+    "i",
+    "h",
+    "g",
+    "f",
+    "e",
+    "d",
+    "c",
+    "b",
+    "a",
+  ];
+  let answer = [];
+  for (let i = 0; i < word.length; i++) {
+    answer.push(alphabet.indexOf(word[i]));
+  }
+  // return answer
+  let result = "";
+  for (let i = 0; i < answer.length; i++) {
+    if (answer[i] === -1) {
+      result += " ";
+    } else {
+      result += reverseAlphabet[answer[i]];
+    }
+  }
+  return result;
 }
 
 module.exports = problem4;
