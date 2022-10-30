5399dd6551b440f550bf8ba607d7486c1b617f0e
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-10-30 21:36:14 +0900

    feat: [P6] 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자 목록 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
+// 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자 목록 반환
 function problem6(forms) {
-  var answer;
-  return answer;
+  const result = [];
+  const nickname = [];
+  forms.map(x => {nickname.push(x[1]);})
+  
+  nickname.map((name) => {
+    let standard = '';
+    for(let i=0; i<name.length-1; i++) {
+      standard = name.substr(i, 2);
+      
+      nickname.map((x, idx) => {
+        if(x !== name && x.includes(standard) && !result.includes(forms[idx][0])) {
+          result.push(forms[idx][0]);
+        }
+      })
+    }
+  })
+
+  return result.sort();
 }
 
 module.exports = problem6;
