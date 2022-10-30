8765cda3e3b56fa23f7f28b2261964ba4290d516
myung-j-p <jmpack97@gmail.com>
2022-10-30 16:32:26 +0900

    문제3_ 1~ 주어진 숫자만큼 세는 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  let arr = Array.from({length: number}, (v, i) => i+1);
+
+
   return answer;
 }
+
 
 module.exports = problem3;
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,4 +1,1 @@
-
-  // const p2 = String(pobi[1]);
-  
-  // console.log(p1)
+9
