a97a3aa17936f79f09a4cb750772e475cb0ecd51
yuna Kim <yuna000804@gmail.com>
2022-10-31 08:00:05 +0900

    problem2 [feat] : 반복문으로 연속된 문자를 제거하되, count/flag 변수로 연속된 문자가 있는지 파악하여 반복문을 벗어남

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,25 @@
 function problem2(cryptogram) {
   var answer;
+  let count = 0;
+  cryptogram = [...cryptogram];
+
+  while(count === 0) {
+    count = 1;
+    for(let i = cryptogram.length-1; i > 0; i--) {
+      let k = i;
+      let flag = 0;
+      while(cryptogram[k] === cryptogram[k-1]){
+        k--;
+        flag = 1;
+      }
+      if(flag === 1) {
+        cryptogram.splice(k, i-k+1);
+        count = 0;
+        i = k;
+      }
+    }
+  }
+  answer = cryptogram.join("");
   return answer;
 }
 
