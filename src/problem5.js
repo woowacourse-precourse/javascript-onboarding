9ebd84f6d5c5dec9e6312a3e93ce60d96200893b
nalsae <nalsae0207@naver.com>
2022-10-30 23:42:47 +0900

    feat: Create a function about push counts in answer array
    - 총액이 주어지면 나눠야 할 금액으로 나눈 몫을 배열에 담는 함수 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,17 @@
 const targetMoneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 const problem5 = (money) => {
-  const answer = [];
+  let answer = [];
+
+  const pushMoneyCount = (targetMoney) => {
+    if (money >= targetMoney) {
+      const quotient = Math.floor(money / targetMoney);
+
+      answer = [...answer, quotient];
+      money -= targetMoney * quotient;
+    } else answer = [...answer, 0];
+  };
+
   return answer;
 };
 
