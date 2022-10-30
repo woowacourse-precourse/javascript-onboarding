6e545fd474d18a6acef83a3501fa2e62f7e47b5b
동동이 <drdragon777@naver.com>
2022-10-30 22:29:19 +0900

    PROBLEM5 - initial commit(solved)

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let copyMoney = money;
+  const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  return moneyUnit.map((unit) => {
+    const count = Math.floor(copyMoney / unit);
+    copyMoney %= unit;
+    return count;
+  });
 }
 
 module.exports = problem5;
