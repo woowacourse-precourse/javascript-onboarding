d7501085c6f09bc64f37bafb4de023bf78ae5a01
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-31 00:38:22 +0900

    refactor(문제2): 중복이 없을 때까지 반복하도록 수정

--- src/problem2.js
+++ src/problem2.js
@@ -10,14 +10,11 @@ const replaceDuplicate = (cryptogram, replaceChar) => {
 }
 
 function problem2(cryptogram) {
-  const answerStack = [cryptogram[0]]; 
-  let duplicateChar = '';   
-  for (let i = 1; i < cryptogram.length; i += 1) {
-    if (duplicateChar === cryptogram[i]) continue;
-    if (answerStack[answerStack.length - 1] === cryptogram[i]) duplicateChar = answerStack.pop();
-    else if (answerStack[answerStack.length - 1] !== cryptogram[i]) answerStack.push(cryptogram[i]);
+  let resultCryptogram = cryptogram;
+  while (checkDuplicate(resultCryptogram)) {
+    resultCryptogram = replaceDuplicate(resultCryptogram, REPLACE_CHAR);
   }
-  return answerStack.join('');
+  return resultCryptogram;
 }
 
 module.exports = problem2;
