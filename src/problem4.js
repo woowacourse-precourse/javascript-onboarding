7b2610e4c67beb29ac06062d70583482bda8f1d1
Serin-Kim <serin9864@gmail.com>
2022-10-30 17:00:53 +0900

    feat: PROBLEM4 풀이 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,38 @@
+const UPPER_ALPHABET = /^[A-Z]/;
+const LOWER_ALPHABET = /^[a-z]/;
+
+const ASCII_UPPER_A = 65;
+const ASCII_UPPER_Z = 90;
+const ASCII_LOWER_A = 97;
+const ASCII_LOWER_Z = 122;
+
+const reverseUpper = (letter) => {
+  return String.fromCharCode(ASCII_UPPER_A + (ASCII_UPPER_Z - letter.charCodeAt(0)));
+};
+
+const reverseLower = (letter) => {
+  return String.fromCharCode(ASCII_LOWER_A + (ASCII_LOWER_Z - letter.charCodeAt(0)));
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const answer = [];
+
+  for (let i = 0; i < word.length; i++) {
+    const letter = word[i];
+    if (UPPER_ALPHABET.test(letter)) answer.push(reverseUpper(letter));
+    else if (LOWER_ALPHABET.test(letter)) answer.push(reverseLower(letter));
+    else answer.push(letter);
+  }
+
+  return answer.join("");
 }
+
+/*
+// for test
+const words = ["I love you", "This is me being REASONABLE."];
+for (word of words) {
+  console.log(problem4(word));
+}
+*/
 
 module.exports = problem4;
