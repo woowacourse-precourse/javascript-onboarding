6fa92f67774ddf2e8455267e866037dea971e182
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-30 21:51:33 +0900

    문제6) feat: 두 글자씩 자른 현재 닉네임이 다음 닉네임에 들어있으면 현재 이메일과 다음 이메일을 집합에 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,18 @@
 function problem6(forms) {
   var answer;
+  const result = new Set([]);
+
+  forms.forEach((form, index) => {
+    for (let i = index + 1; i < forms.length; i++) {
+      for (let j = 0; j < form[1].length - 1; j++) {
+        if (forms[i][1].includes(form[1].substr(j, 2))) {
+          result.add(form[0]);
+          result.add(forms[i][0]);
+        }
+      }
+    }
+  });
+
   return answer;
 }
 
