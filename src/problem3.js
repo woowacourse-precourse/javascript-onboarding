b9997cf74cd361ae61d1a59f98da1e2daa8f9e79
sumin <csumin008255@gmail.com>
2022-10-30 15:36:01 +0900

    problem3_기능목록수정 및 기능2

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,15 @@
 /* 기능 목록
 * 1. 1부터 number까지 반복하여 숫자를 배열로 변환
-* 2. 배열에 3,6,9가 있으면 count +1
+* 2. 배열에 3,6,9가 있으면 count
 * 3. 결과 출력
 * 4. 코드 정리
 */
 
 function problem3(number) {
+  var count = 0;
   for(var i = 1; i<= number; i++){
     var numArr = i.toString().split("");
+    count += numArr.filter(num => '3' === num || '6' === num || '9'===num).length;
   }
 }
 
