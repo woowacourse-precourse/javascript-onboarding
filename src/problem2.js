75f7f4c747241bec8e085f08d3df218b1c8ddf89
heejorika <y20001206@gmail.com>
2022-10-31 03:58:37 +0900

    기능목록 수정 및 중복되는 문자열 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -7,9 +7,7 @@ function problem2(cryptogram) {
 module.exports = problem2;
 */
 
-let cryptogram="Browoanoommnaon";
-let c = ""
-console.log(c)
+let cryptogram="browoanoommnaon";
 
 // cryptogram이 유효한 문자열인지 확인
 function checkValid(c){
@@ -17,4 +15,24 @@ function checkValid(c){
   return c.toLowerCase();
 }
 
-console.log(checkValid(c));
+// 이웃하는 문자열이 동일한 모든 구간 체크
+function deleteOverlap(c){
+  let newC="";
+  let i;
+  for (i=0; i<(c.length-1); i++){
+      if (c[i]==c[i+1]){
+          i+=1;
+      }else{
+          console.log(c[i]);
+          newC+=c[i];}
+  }
+  if(newC[newC.length-1]!=c[c.length-1]){newC+=c[c.length-1];}
+  return newC;
+}
+
+console.log(deleteOverlap(cryptogram));
+
+// 
+function solution(c){
+  c = checkValid(c); if (c==false){return "";}
+}
