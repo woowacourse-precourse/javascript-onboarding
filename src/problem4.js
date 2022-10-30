920920802349e0dff0a246aaaab7654702630c15
kknyapple <kknyapple@gmail.com>
2022-10-30 15:50:05 +0900

    problem4 - 문자가 알파벳인지 판별

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
+const isAlphabet = (str) => {
+  if (
+    ("A".charCodeAt() <= str.charCodeAt() &&
+      str.charCodeAt() <= "Z".charCodeAt()) ||
+    ("a".charCodeAt() <= str.charCodeAt() &&
+      str.charCodeAt() <= "z".charCodeAt())
+  )
+    return true;
+};
+
 function problem4(word) {
   var answer;
+
   return answer;
 }
 
