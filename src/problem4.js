69259878b646fe6a29250d31a5a787073513d3ba
Choejeonghun <saa00123@naver.com>
2022-10-30 23:59:58 +0900

    refactor: 코드 전체 수정 및 간소화

--- src/problem4.js
+++ src/problem4.js
@@ -1,55 +1,14 @@
 function problem4(word) {
-  var answer;
-  var lower = [
-    'a',
-    'b',
-    'c',
-    'd',
-    'e',
-    'f',
-    'g',
-    'h',
-    'i',
-    'j',
-    'k',
-    'l',
-    'm',
-    'n',
-    'o',
-    'p',
-    'q',
-    'r',
-    's',
-    't',
-    'u',
-    'v',
-    'w',
-    'x',
-    'y',
-    'z',
-  ];
-  var upper = [];
-  for (var i = 0; i < lower.length; i++) {
-    var up = lower[i].toUpperCase();
-    upper.push(up);
-  }
-  var reverse_lower = [...lower].reverse();
-  var reverse_upper = [...upper].reverse();
-
-  var check = word.split('');
-
+  var answer = '';
   for (var i = 0; i < word.length; i++) {
-    check[i] = check[i].replace(
-      lower[lower.indexOf(check[i])],
-      reverse_lower[lower.indexOf(check[i])]
-    );
-    check[i] = check[i].replace(
-      upper[upper.indexOf(check[i])],
-      reverse_upper[upper.indexOf(check[i])]
-    );
-  }
-  answer = check.join('');
+    var str = word.charCodeAt(i);
 
+    if (66 < str && str < 91)
+      answer = answer + String.fromCharCode(90 - str + 65);
+    else if (96 < str && str < 123)
+      answer = answer + String.fromCharCode(122 - str + 97);
+    else if (str == 32) answer = answer + String.fromCharCode(32);
+  }
   return answer;
 }
 
