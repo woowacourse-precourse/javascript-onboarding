fd7f3148330710aa2fe3d63c617e9579abd9c099
하지훈 <jnh03336@gmail.com>
2022-10-30 14:14:05 +0900

    feat: pro3, clap count

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
+const getDigits = (num) => {
+  const digits = [];
+  while(num !== 0){
+    digits.push(num%10);
+    num = Math.floor(num / 10);
+  }
+
+  return digits;
+}
+
 const clapCount = (num) => {
-  return num;
+  const clapDigits = [3,6,9];
+  const digits = getDigits(num);
+  let count = 0;
+  for(let i = 0; i < digits.length; i++){
+    if(clapDigits.includes(digits[i])) count++;
+  }
+
+  return count;
 }
 
 function problem3(number) {
