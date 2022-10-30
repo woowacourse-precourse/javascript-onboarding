d9354ec52eb41b8c25409c1afc77ae72300616da
feb-dain <feb.dain@gmail.com>
2022-10-31 00:19:10 +0900

    feat: [problem5] find minimum and add answer

--- src/problem5.js
+++ src/problem5.js
@@ -2,7 +2,14 @@ function problem5(money) {
   const denominationsArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
   let answer = Array(denominationsArray.length);
-  
+
+  denominationsArray.map((denomination, index) => {
+
+    answer[index] = ~~(money / denomination); // ~~ : 소수점 아래 버림
+    money %= denomination;
+
+  });
+
   return answer;
 }
 
