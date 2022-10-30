e88b23261f85e06139ee13e588d81375647d231c
Sunghyun627 <kksscc4@gmail.com>
2022-10-30 19:14:11 +0900

    feat: 해당 숫자가 3, 6, 9 중 하나인지 확인하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,12 +1,17 @@
 /*
   구현할 기능 목록
-  [] 해당 숫자가 3, 6, 9 중 하나인지 확인하는 기능 | return 값 : 1(3, 6, 9 중 하나인 경우)/0(3, 6, 9 중 하나가 아닌 경우)  
+  [O] 해당 숫자가 3, 6, 9 중 하나인지 확인하는 기능 | return 값 : 1(3, 6, 9 중 하나인 경우) / 0(3, 6, 9 중 하나가 아닌 경우)
   [] 해당 숫자를 각 자리수로 쪼개어, 각 자리수마다 3, 6, 9인지 확인하는 기능
 */
 
 function problem3(number) {
   let result = 0;
   return result;
+}
+
+function isThreeSixNine(digit) {
+  const threeSixNine = [3, 6, 9];
+  return threeSixNine.includes(digit) ? 1 : 0;
 }
 
 module.exports = problem3;
