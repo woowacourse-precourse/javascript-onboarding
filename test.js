0773f3e25cb035949b66f2e3a3a4080227f697e7
저니녁 <wjs_5025@naver.com>
2022-10-31 00:56:42 +0900

    solve : problem5

--- test.js
+++ test.js
@@ -1,57 +1,13 @@
-/* 문제 분석
-1. 청개구리 사전에 맞춰서 데이터를 변환하여 출력해야한다.
-2. 대소문자를 구분해서 반환한다.
- */
-/* 테스트 케이스 분석
-
- */
-
-let alpha = [
-  "A",
-  "B",
-  "C",
-  "D",
-  "E",
-  "F",
-  "G",
-  "H",
-  "I",
-  "J",
-  "K",
-  "L",
-  "M",
-  "N",
-  "O",
-  "P",
-  "Q",
-  "R",
-  "S",
-  "T",
-  "U",
-  "V",
-  "W",
-  "X",
-  "Y",
-  "Z",
-];
-
-let flogCase = [...alpha].reverse();
-
-function toFlogCase(char) {
-  let c = char.toUpperCase();
-  let index = alpha.indexOf(c);
-  let changed = flogCase[index] ?? " ";
-  if (char.charCodeAt(0) >= 97) return changed.toLowerCase();
-  return changed;
-}
-
-// main
-function problem4(word) {
+function problem5(money) {
   var answer = [];
-  for (let i of [...word]) {
-    answer.push(toFlogCase(i));
+  const deviders = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let devider of deviders) {
+    answer.push(parseInt(money / devider));
+
+    money = money % devider;
   }
-  return answer.join("");
+  return answer;
 }
 
-console.log(problem4("I love You"));
+console.log(problem5(15000));
