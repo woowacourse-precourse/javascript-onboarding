be599ae81785c8981e257b13c3e5222f1b8a2acb
Yunjae <nuguri206@gmail.com>
2022-10-30 15:25:28 +0900

    feat: [prob3] 박수 쳤을 때 count가 증가하게 만든 함수 생성

--- src/problem3.js
+++ src/problem3.js
@@ -3,6 +3,17 @@ function problem3(number) {
   const clap = [3,6,9];
   //박수 친 숫자
   let count = 0;
+
+  //박수 쳤을 때 count가 증가하는 함수
+  for( let i=0; i<=number; i++){
+    let numbers = i.toString().split("");
+    for(let j=0; j<numbers.length; j++){
+      if(clap.includes((Number(numbers[j])))){
+        count++;
+      }
+    }
+  };
+  return count;
 }
 
 module.exports = problem3;
