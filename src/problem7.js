d96223f505c5ddb4560f361a685ce1d6fdfe4c1a
woo-jk <201702042@o.cnu.ac.kr>
2022-10-30 23:35:55 +0900

    feat:[PROBLEM7] 친구 배열 만들어주는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,13 @@
+const getFriends = (user, friends) => {
+  let userFriends = [];
+  friends.forEach((friendPair) => {
+    if (!friendPair.includes(user)) return;
+    if (friendPair[0] === user) userFriends.push(friendPair[1]);
+    if (friendPair[1] === user) userFriends.push(friendPair[0]);
+  });
+  return userFriends;
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
