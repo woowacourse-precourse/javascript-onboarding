161d61927ddd44172e7b510c62ccd7433c9b2f40
gyeongza <gyeongza@naver.com>
2022-10-30 22:11:27 +0900

    fix: problem2 수정 후 테스트 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,25 +1,17 @@
-// 입력 받은 문자열을 배열로 변환
-const cryptogramArr = (string) => {
-    return string.split("")
-}
-
 // 중복문자 제거 
-
 const compare = (word) => {
-    let i = 0;
-    while (i !=word.length){
-    if (word[i] === word[i+1]){
-        word.splice(i,2);
-        i=0;
-    } else {
-        i +=1;
-    }
-} return word.join("");}
+    while(true){
+        const wordLength = word.length
+        word = word.replace(/([a-z])\1+/g,'')
+        if(wordLength === word.length){
+            break
+        }
+    } return word
+}
 
 // 결과값 출력
 function problem2(cryptogram) {
-    let arr = cryptogramArr(cryptogram);
-    return compare(arr);
+    return compare(cryptogram);
 }
 
 module.exports = problem2;
