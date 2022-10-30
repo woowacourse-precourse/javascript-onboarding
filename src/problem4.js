4c4e571db698714a3ce7b12f1c1c6e856b974259
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 18:12:11 +0900

    feat(problem4): 문자를 검사하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,19 @@
+function isAlpha(char) {
+  var upperRegExp = /[A-Z]/;
+  var lowerRegExp = /[a-z]/;
+
+  if (upperRegExp.test(char)) {
+    return 2;
+  }
+  if (lowerRegExp.teset(char)) {
+    return 1;
+  }
+  return 0;
+}
+
 function problem4(word) {
   var answer;
   return answer;
 }
-
-console.log('b' - 'a')
 
 module.exports = problem4;
