52c97541046ce6e75441e46377732ae47171d32d
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-10-30 12:15:07 +0900

    feat: 문제 7 입력받은 배열을 [UserName, 알고리즘 점수]로 변경 및 중복 UserName 삭제
    [] 친구의 친구와 방문자 배열을 [UserName, 알고리즘 점수]으로 바꾸는 기능
    [] [UserName, 알고리즘 점수] 배열에서 UserName에 사용자 본인의 이름이나 친구 이름이 있으면 제외함

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,28 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+  let friendCopy = [];
+  let algorithmArr = [];
+
+  for (let index = 0; index < friends.length; index++) {
+    friendCopy.push(friends[index][0]);
+
+    let overlapIdx = algorithmArr.indexOf(friends[index][1]);
+    if (overlapIdx != -1 && friends[index][1].length === algorithmArr[overlapIdx][0]) {
+      algorithmArr[overlapIdx][1] += 10;
+      continue;
+    }
+    algorithmArr.push([friends[index][1], 10]);
+  }
+
+  for (index = 0; index < visitors.length; index++) {
+    let overlapIdx = algorithmArr.indexOf(visitors[index]);
+    if (overlapIdx != -1 && visitors[index].length === algorithmArr[overlapIdx][0]) {
+      algorithmArr[overlapIdx][1] += 1;
+      continue;
+    }
+    algorithmArr.push([visitors[index], 1]);
+  }
+
   return answer;
 }
 
