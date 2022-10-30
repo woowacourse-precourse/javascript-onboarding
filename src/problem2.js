8d2e2a57c5422038c930f8e58486c4efbb5e6ba2
nalsae <nalsae0207@naver.com>
2022-10-30 22:09:57 +0900

    feat: Add a function for finding duplicate
    - 연속된 알파벳 소문자를 찾는 함수 분리하여 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
-function problem2(cryptogram) {
-  var answer;
+const findDuplicate = (string) => string.match(/([a-z])\1{1,}/g);
+
+const problem2 = (cryptogram) => {
+  let answer;
+
   return answer;
-}
+};
 
 module.exports = problem2;
