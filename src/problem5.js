4227cdec3d829cf7655149aceae4bb87adf3a6a9
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-30 15:41:58 +0900

    [Modified] problem5.js

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,12 @@
 function problem5(money) {
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const result = Array.from({ length: units });
+  const result = [];
 
   let temp = money;
-  for (let i = 0; i < units.length; i++) {
-    result[i] = Math.floor(temp / units[i]);
-    temp %= units[i];
+  for (let i in units) {
+    const unit = units[i];
+    result[i] = Math.floor(temp / unit);
+    temp %= unit;
   }
 
   return result;
