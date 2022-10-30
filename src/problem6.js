cf450c9022827af9706f6699c15a9333dd3b6dd2
vinivin153 <vinivin153@naver.com>
2022-10-30 22:42:21 +0900

    feat: 중복된 닉네임의 이메일 찾는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const allConsecutiveLetters = {};
+  const duplicateEamil = new Set();
+
+  forms.forEach((user) => {
+    const [email, nickname] = user;
+    const userConsecutiveLetters = getConsecutiveLetters(nickname);
+
+    userConsecutiveLetters.forEach((letter) => {
+      if (letter in allConsecutiveLetters) {
+        duplicateEamil.add(allConsecutiveLetters[letter]);
+        duplicateEamil.add(email);
+      } else if (!(letter in allConsecutiveLetters)) {
+        allConsecutiveLetters[letter] = email;
+      }
+    });
+  });
+
+  return duplicateEamil;
 }
 
 function getConsecutiveLetters(nickname) {
