0372f09c21d54630a8aefcf42c25f977e2d034bd
박겸영 <yong_drg@naver.com>
2022-10-30 21:37:42 +0900

    feat: 문제 4 엄마 말씀 각 문자의 알파벳 배열 순서 찾기 - 기능 2: 엄마 말씀 문자열을 한 글자씩 쪼개고, 각각 대문자 배열과 소문자 배열에서 몇 번째 순서에 해당하는지 찾기

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
   const uppercases = [...Array(26)].map((_, i) => String.fromCharCode(i + 65));
   const lowercases = uppercases.map((letter) => letter.toLowerCase());
+
+  word.split('').forEach((letter) => {
+    const indexOfUppercase = uppercases.indexOf(letter);
+    const indexOfLowercase = lowercases.indexOf(letter);
+  });
 }
 
 module.exports = problem4;
