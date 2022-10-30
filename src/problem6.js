ffda78270ecbe81960db5e549f2d27367ef8d7cd
100Gyeon <bjy9966@naver.com>
2022-10-30 15:32:51 +0900

    feat: 모든 닉네임을 두 글자씩 잘라서 총 몇 번 나타났는지 카운트

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const twoLettersMap = new Map();
+
+  forms.forEach(([_, nickname]) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const twoLetters = nickname.substring(i, i + 2);
+      if (twoLettersMap.has(twoLetters)) {
+        twoLettersMap.set(twoLetters, twoLettersMap.get(twoLetters) + 1);
+      } else {
+        twoLettersMap.set(twoLetters, 1);
+      }
+    }
+  });
 }
 
 module.exports = problem6;
