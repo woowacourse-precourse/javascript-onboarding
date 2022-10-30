02be74ca129627663a712a23da0e16efc5d94c6b
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-30 15:20:27 +0900

    [Add & Modified] problem3.js

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,13 @@
 const makeArr = (number) => String(number).split("");
-const filterArrayBy = (arr, ...args) => arr.filter((v) => args.includes(+v));
+const filterArrayBy = (arr, ...by) => arr.filter((v) => by.includes(+v));
 
 function problem3(number) {
   let cnt = 0;
-  for (let i = 0; i < number + 1; i++) {
+  for (let i = 0; i <= number; i++) {
     const temp = filterArrayBy(makeArr(i), 3, 6, 9);
     cnt += temp.length;
   }
   return cnt;
 }
-console.log(problem3(33));
+
 module.exports = problem3;
