41090435f2271ff37f27dce2e788475a5e09b395
Sim <zh025700@naver.com>
2022-10-31 03:17:53 +0900

    feat: problem4 find char position & update requirement

--- src/problem4.js
+++ src/problem4.js
@@ -5,7 +5,16 @@ function problem4(word) {
 
 const encode = (word) => {
   let strArr = [];
-  [...word].map((e) => {});
+  [...word].map((e) => {
+    const code = findAscii(e);
+    if (65 <= code && code <= 90) {
+    } else if (97 <= code && code <= 122) {
+    } else {
+    }
+  });
+};
+const findAscii = (word) => {
+  return word.charCodeAt(0);
 };
 
 module.exports = problem4;
