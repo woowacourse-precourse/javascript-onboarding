1d460df455a6d0200ff9f847a1546cde918a00f1
TeusLee <xodn1311@gmail.com>
2022-10-30 15:31:08 +0900

    prob3 sol complete

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,26 @@
+const is_tsn = (digit) => {
+  if (digit === 3 || digit === 6 || digit === 9){
+    return 1
+  } else {
+    return 0
+  }
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  var cnt = 1
+  while (cnt <= number){
+    temp_cnt = cnt
+    while (temp_cnt >= 1){    
+      temp_cnt = temp_cnt * 0.1
+      temp_digit = Math.round(10*(temp_cnt - Math.floor(temp_cnt)), 0)
+      console.log(temp_digit)
+      answer += is_tsn(temp_digit) 
+      temp_cnt = Math.floor(temp_cnt)
+    }
+    cnt += 1;
+  }
+    
   return answer;
 }
 
