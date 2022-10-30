535a13adadebe5c2f97a212f0fbf8ab90fe0b0c7
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-10-30 15:02:30 +0900

    Feat: 중간 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,131 @@
+// function problem7(user, friends, visitors) {
+//   let answer;
+//   return answer;
+// }
+
+
+
+// const user = "mrko";
+// const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
+// const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
+
+
+
+// problem7(user, friends, visitors)
+
+
+
+
+
+
+
+// module.exports = problem7;
+
+//step 1
+// 친구 추출하기
+
+// function problem7(user, friends, visitors) {
+//   let answer;
+//   let userFriends = [];
+//   for (let i = 0; i < friends.length; i++) {
+//     let tmp = "";
+//     for (let j = 0; j < 2; j++) {
+//       tmp = friends[i][j];
+//       if (tmp === user) {
+//         userFriends.push(friends[i])
+//       }
+//     }
+//   }
+
+//   console.log(userFriends)
+//   return answer;
+// }
+
+
+
+// const user = "mrko";
+// const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
+// const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
+
+
+
+// problem7(user, friends, visitors)
+
+// //setp 2 친구의 친구 추출하기
+
+// function problem7(user, friends, visitors) {
+//   let answer;
+//   let userFriends = [];
+//   for (let i = 0; i < friends.length; i++) {
+//     let tmp = "";
+//     for (let j = 0; j < 2; j++) {
+//       tmp = friends[i][j];
+//       if (tmp === user) {
+//         userFriends.push(friends[i])
+//       }
+//     }
+//   }
+//   let friendsList = [];
+
+//   userFriends.map(item => {
+//     friendsList = [...friendsList, ...item]
+//   })
+
+//   console.log(friendsList)
+//   return answer;
+// }
+
+
+
+// const user = "mrko";
+// const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
+// const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
+
+
+
+// problem7(user, friends, visitors)
+
+//setp 3 친구의 친구를 활용하여 나에게 추천할 점수 부여 하기
+
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+  let userFriends = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    let tmp = "";
+    for (let j = 0; j < 2; j++) {
+      tmp = friends[i][j];
+      if (tmp === user) {
+        userFriends.push(friends[i])
+      }
+    }
+  }
+  let friendsList = [];
+  userFriends.map(item => {
+    friendsList = [...friendsList, ...item]
+  })
+
+  let recommendationList = [];
+  for (let i = 0; i < friends.length; i++) {
+      let tmp = "";
+      for (let j = 0; j < 2; j++) {
+        tmp = friends[i][j];
+        for (let x of friendsList) {
+          if (x === user) {
+            continue;
+          } else if (x === tmp && friends[i].indexOf(user) === -1) {
+            recommendationList.push(friends[i])
+          }
+        }
+      }
+    }
+  let newRecommendationList = recommendationList.filter(value => {
+
+  })
+  console.log(friendsList)
   return answer;
 }
-
-module.exports = problem7;
+const user = "mrko";
+const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
+const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
+problem7(user, friends, visitors)
