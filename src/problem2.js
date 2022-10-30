a56bc0958a88bec3bbb0947ab5f1e2d8f3f7b4b9
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-30 20:22:51 +0900

    feat: problem 2 인자 예외처리 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function problem2(cryptogram) {}
+
+function checkException(str) {
+  const x = str.length >= 1 && str.length <= 1000;
+  const y = str.toLowerCase() === str;
+  return x && y;
 }
 
 module.exports = problem2;
