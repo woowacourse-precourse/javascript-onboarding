bfa9b7e47d24618eb8225f5385191c066b6c868e
zktm9903 <zktm9903@naver.com>
2022-10-30 18:31:14 +0900

    problem6 solve

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const map = new Map();
+  const set = new Set();
+
+  forms.map(el=>{
+    const [email, nick] = el;
+
+    for(let i=0;i<nick.length-1;i++){
+      const nick2 = nick.slice(i, i+2);
+      console.log(nick2)
+      if(map.has(nick2)){
+        if(map.get(nick2).includes(email) === false)
+          map.get(nick2).push(email);
+      }
+      else
+        map.set(nick2, [email]);
+    }
+  })
+
+  map.forEach((emails)=>{
+    if(emails.length > 1)
+      emails.map(el=>set.add(el));
+  })
+
+  return Array.from(set).sort();
 }
 
 module.exports = problem6;
