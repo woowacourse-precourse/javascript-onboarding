422e146d28820f9a8d99a0e5465b85a5b60fdec5
yeeun426 <thsudkcla7@naver.com>
2022-10-30 21:09:26 +0900

    feat: 문제 5번 주석 추가

--- src/problem5.js
+++ src/problem5.js
@@ -3,11 +3,13 @@ function problem5(money) {
   var copy_money = money
   var answer = new Array();
 
+  // for문으로 오만원권, 만원권, 오천원권, 천원권, 오백원, 백원, 오십원, 십원, 일원을 돌림
   for(var i = 0; i < arrNumber.length; i++) {
+    // 1개 이상 변환 가능한 경우 그 값을 배열에 저장
     if(parseInt(copy_money / arrNumber[i]) > 0) {
       answer[i] = parseInt(copy_money / arrNumber[i]);
       copy_money = copy_money - arrNumber[i] * parseInt(copy_money / arrNumber[i]);
-    } else if(parseInt(copy_money / arrNumber[i]) == 0) {
+    } else if(parseInt(copy_money / arrNumber[i]) == 0) { // 0개인 경우 배열에 0을 저장
       answer[i] = 0;
     }
   } 
