5d554b4f627d6889a927d514075390bee28e3058
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 00:17:36 +0900

    String to Array

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,14 @@
+/*
+  ### 기능 목록
+
+  1. 전달받은 단어를 배열로 변환한다.(String to Array)
+  2. 각 알파벳을 아스키코드로 변환하여, 대문자와 소문자를 구별한다.
+  [ ASCII CODE - 'A'=65, 'Z'=90 / 'a'=97, 'z'=122] 
+  3. [ 65 + 90 =155 / 97 + 122 = 219 ] <- 이를 고려하여, 반대되는 알파벳을 찾는다.
+  4. 이후 배열을 문자열로 변환한다, (Array to String)
+*/
 function problem4(word) {
-  var answer;
+  const Array = word.split("");
   return answer;
 }
 
