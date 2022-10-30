ddb25c02b1ebbc7d385fa5ff693b7014065b7d21
Deemou <4106243@naver.com>
2022-10-30 22:33:20 +0900

    feat<P7>: 각 유저의 친구 리스트를 만드는 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,15 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+const getFriendRelationList = (friends) => {
+  const friendRelation = {};
+  friends.forEach(([user1, user2]) => {
+    if (!friendRelation[user1]) friendRelation[user1] = [];
+    if (!friendRelation[user2]) friendRelation[user2] = [];
+    friendRelation[user1].push(user2);
+    friendRelation[user2].push(user1);
+  });
+  return friendRelation;
+};
+
 module.exports = problem7;
