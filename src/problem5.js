8103949f40145fd11bdc8707c206953fa00e2404
yunju <yom8926@naver.com>
2022-10-30 22:52:47 +0900

    문제5번 구현완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,10 @@
 function problem5(money) {
-  let coin = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  let total = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
   var result = [];
 
-  for(let i=0; i < coin.length; i++) {
-   var oman = money / coin[i];
-   result[i] = 0;
-   result[0] = oman;
-    money %= coin[i];
+  for (let i = 0; i < total.length; i++) {
+    result.push(Math.floor(money / total[i]))
+    money %= total[i];
   }
   return result;
 }
