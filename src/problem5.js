e8a719af5438f545d841ae0f77ad63f0dac6f97e
하송희 <99heesong@naver.com>
2022-10-30 22:43:54 +0900

    [문제 5] 기능추가: 값 비교 후 계산하고 배열 증가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
   const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let wallet = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  while(unit[i] <= money) {
+    money -= unit[i];
+    wallet[i]++;
+  }
 }
 
   module.exports = problem5;
