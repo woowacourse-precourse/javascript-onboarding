1fabffae99ab47db88cc0dbe6c3a50dedb1e5b8f
HWI <dlwognl99@naver.com>
2022-10-30 22:19:42 +0900

    문제4_feat: 메인함수 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,25 @@
+// 청개구리 사전
+const CAPITAL = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const SMALL = "abcdefghijklmnopqrstuvwxyz";
+
 function problem4(word) {
-  var answer;
-  return answer;
+  if (word.length < 1 || word.length > 1000) {
+    return -1;
+  }
+  // 청개구리 사전에서 반환된 문자로 채울 빈 문자열을 만든다.
+  let result = "";
+
+  for (let i = 0; i < word.length; i++) {
+    const type = word[i];
+    if (/[A-Z]/.test(type)) {
+      result += converCapital(type);
+    } else if (/[a-z]/.test(type)) {
+      result += converSmall(type);
+    } else {
+      result += word[i];
+    }
+  }
+  return result;
 }
 
 module.exports = problem4;
