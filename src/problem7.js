bda92a69e10566b1eb9692e164b58888a4f83f2a
kaori-killer <sksbthwjd@daum.net>
2022-10-30 16:51:14 +0900

    feat: 사용자의 친구를 기록한다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,16 +1,25 @@
 function problem7(user, friends, visitors) {
   let friendGraph = {};
+  let notRecommendationFriends = {};
+  let recommendationScore = {};
 
   friends.forEach(friendArray => {
     const A = friendArray[0];
     const B = friendArray[1];
 
-    if(!(A in friendGraph)) { friendGraph[A] = []; }
-    if(!(B in friendGraph)) { friendGraph[B] = []; }
+    if(!(A in friendGraph)) { friendGraph[A] = []; recommendationScore[A] = []; }
+    if(!(B in friendGraph)) { friendGraph[B] = []; recommendationScore[B] = []; }
     friendGraph[A].push(B);
     friendGraph[B].push(A);
-    
   });
+
+  friendGraph[user].forEach(alreadyFriend => {
+    notRecommendationFriends[user] = 1; 
+    notRecommendationFriends[alreadyFriend] = 1;
+
+  })
+
+
 }
 
 problem7("mrko", 	[["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]], ["bedi", "bedi", "donut", "bedi", "shakevan"], ["andole", "jun", "bedi"])
