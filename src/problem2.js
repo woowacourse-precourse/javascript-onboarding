404b08c40babd80840f9201c07833f54c8b6dd2a
Sim <zh025700@naver.com>
2022-10-31 02:52:10 +0900

    feat: problem2 checkArrNull & repeat str

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
   var answer;
+  let duplicateCnt = [];
+  let strArr = [...cryptogram];
+  while (true) {
+    if (checkArrIsNull(strArr)) {
+      return "";
+    }
+
+    return (answer = arrToStr(strArr));
+  }
+
   return answer;
 }
+
+const checkArrIsNull = (arr) => {
+  return arr.length === 0 ? true : false;
+};
 
 module.exports = problem2;
