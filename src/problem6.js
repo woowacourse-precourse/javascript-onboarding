26a40fd38aa466f7b01e051e41d2433615c6199b
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-30 23:05:56 +0900

    feat: [problem6] - 각 크루의 닉네임을 두글자씩 연속으로 잘라 반환받은 배열을 순회하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,17 @@
 function problem6(forms) {
-  var answer;
-  var nicknameSplit = new Map();
+  var resultEmailList = [];
+  var nicknameSplitMap = new Map();
 
-  for (var i = 0; i < forms.length; i++) {}
-  return answer;
+  for (var i = 0; i < forms.length; i++) {
+    var nameSplitStrings = getSplitStrings(forms[i][1]);
+
+    for (var j = 0; j < nameSplitArr.length; j++) {}
+
+    return resultEmailList;
+  }
 }
 
-function getNameSplitArr(nickname) {
+function getSplitStrings(nickname) {
   var arr = [];
 
   for (var i = 0; i < nickname.length - 1; i++) {
