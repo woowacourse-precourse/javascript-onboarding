49e6fb5c76289bd020e0de558ebe5a2fb2705cb4
한수지 <hansuji@hansujiui-MacBookAir.local>
2022-10-30 15:39:33 +0900

    problem1 리팩토링 실행속도 개선

--- src/problem1.js
+++ src/problem1.js
@@ -2,38 +2,34 @@ function problem1(pobi, crong) {
   var answer = solution(pobi, crong);
   return answer;
 };
-function exception(pobi, crong) {
-  if( 
-    pobi[0] % 2 == 0 || crong[0] % 2 == 0 ||
-    Math.min(...pobi) <= 2 || Math.max(...pobi) >= 400 ||
-    Math.min(...crong) <= 2 || Math.max(...crong) >= 400 ||
-    pobi[0]+1 !== pobi[1] || crong[0]+1 !== crong[1]
-  )   answer = false;
-  else answer = true;
-  return answer;
+function hasException(person) {
+  let leftNum = person[0];
+  let rightNum = person[1];
+  if (leftNum % 2 === 0) return true;
+  if (Math.min(leftNum, rightNum) <= 2) return true;
+  if (Math.max(leftNum, rightNum) >= 400) return true;
+  if (leftNum + 1 !== rightNum) return true;
+  return false;
 };
-function makeMaxScore(arr) {
+function getMaxScore(arr) {
   oddNum = arr[0].toString().split("").map(Number);
   evenNum = arr[1].toString().split("").map(Number);
-  let oddNumPlus = oddNum.reduce((a, b) => a + b, 0);
-  let oddNumMulty = oddNum.reduce((a, b) => a * b, 1);
-  let evenNumPlus = evenNum.reduce((a, b) => a + b, 0);
-  let evenNumMulty = evenNum.reduce((a, b) => a * b, 1);
-  let score = [oddNumPlus, oddNumMulty, evenNumPlus, evenNumMulty]
-  return Math.max(...score)
+  let oddNumPlus = oddNum.reduce((acc, curr) => acc + curr, 0);
+  let oddNumMulty = oddNum.reduce((acc, curr) => acc * curr, 1);
+  let evenNumPlus = evenNum.reduce((acc, curr) => acc + curr, 0);
+  let evenNumMulty = evenNum.reduce((acc, curr) => acc * curr, 1);
+  return Math.max(oddNumPlus, oddNumMulty, evenNumPlus, evenNumMulty)
 };
 function solution(pobi, crong) {
-  pobiScore = makeMaxScore(pobi);
-  crongScore = makeMaxScore(crong);
-  checkException = exception(pobi,crong);
-  if(checkException === false) {
-      answer = -1;}
-  if(pobiScore > crongScore && checkException !== false) {
-      answer = 1;
-  } else if(pobiScore < crongScore && checkException !== false) {
-      answer = 2;
-  } else if(pobiScore === crongScore && checkException !== false) {
-      answer = 0;
+  pobiScore = getMaxScore(pobi);
+  crongScore = getMaxScore(crong);
+  if(hasException(pobi) || hasException(crong)) return -1;
+  if(pobiScore > crongScore) {
+    answer = 1;
+  } else if(pobiScore < crongScore) {
+    answer = 2;
+  } else if(pobiScore === crongScore) {
+    answer = 0;
   } return answer;
 };
 module.exports = problem1;
