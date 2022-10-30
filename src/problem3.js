08cb78ebb9cabdd87e369c54169c697ec7db0ba3
b-hyoung <youqlrqod@naver.com>
2022-10-30 12:41:15 +0900

    add problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let x = i % 10;
+    let y = i / 10;
+    if (((x === 3 || x === 6 || x === 9)) || ((y === 3 || y === 6 || y === 9))) {
+      answer++
+    }
+    if (i % 10 == 0) {
+      answer++
+    }
+  }
   return answer;
 }
+
 
 module.exports = problem3;
