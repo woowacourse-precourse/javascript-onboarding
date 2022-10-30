0c92563693ab99eace17875925a6d3882288e0cf
kaori-killer <sksbthwjd@daum.net>
2022-10-30 17:47:40 +0900

    feat: 우선순위를 점수, 이름 순으로 정렬한다.

--- src/problem7.js
+++ src/problem7.js
@@ -36,11 +36,26 @@ function countVisitFriend(visitors, notRecommendationFriends, recommendationScor
 function problem7(user, friends, visitors) {
   let notRecommendationFriends = {};
   let recommendationScore = {};
+  let resultName = [];
+  let recommendationScoreArray = [];
 
   [notRecommendationFriends, recommendationScore] = countYourFriend(user, friends, notRecommendationFriends);
   recommendationScore = countVisitFriend(visitors, notRecommendationFriends, recommendationScore);
-  
-  return recommendationScore;
+
+  for(const name in recommendationScore){ 
+    recommendationScoreArray.push([recommendationScore[name], name]);
+  }
+
+  recommendationScoreArray.sort((a, b) => {
+    if(a[0] == b[0]) { 
+      return a[1] < b[1] ? -1 : a[1] > b[1] ? 1 : 0;
+    }
+    return b[0] - a[0];
+  });
+
+  console.log(recommendationScoreArray);
+
+  return resultName;
 }
 
 console.log(problem7("mrko", 	[["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"]], ["bedi", "bedi", "donut", "bedi", "shakevan"]));
