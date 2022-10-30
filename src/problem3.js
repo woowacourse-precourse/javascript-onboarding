c4148be1759ad0e0d3d1bb3f189db3bcdd2921f8
Minyoung Lee <mminy62@gmail.com>
2022-10-30 14:06:46 +0900

    feat: [PROBLEM3] 숫자 array에 있는 3, 6, 9 count

--- src/problem3.js
+++ src/problem3.js
@@ -4,16 +4,21 @@ function problem3(number) {
   //1. 숫자를 string으로 변환
    for(let i = 1; i < number + 1; i++){
      let tempNum = i.toString().split('');
-     
-     console.log(tempNum);
 
+     //2. 숫자 array에 있는 3, 6, 9 count
+     tempNum.forEach(function(c){
+       
+      if(c === '3' || c === '6' || c === '9'){
+        answer += 1;
+
+      }
+    })
+     
   }
 
   return answer;
   
 }
 
-
-problem3(10);
 
 module.exports = problem3;
