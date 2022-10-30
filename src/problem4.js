eccdda5582cf5f4fbe158483ba7d4d51b42a6051
R-jisu <79021544+R-jisu@users.noreply.github.com>
2022-10-30 21:24:52 +0900

    [problem4]passed

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,22 @@
 function problem4(word) {
+  let Word = word;
+  let frogword = '';
+  const bigAlpa = ['Z','Y','X','W','V','U','T','S','R','Q','P','O','N','M','L','K','J','I','H','G','F','E','D','C','B','A'];
+  const smallAlpa = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'];
+  for(let i=0; i<Word.length; i++){
+    //space
+    if(Word[i] === ' ') frogword+=' ';
+    //bigalpa condition
+    if(Word[i].charCodeAt() > 64 && Word[i].charCodeAt() < 91){
+      frogword+=bigAlpa[Word[i].charCodeAt()-65];
+    }
+    //smallalpa condition
+    if(Word[i].charCodeAt() > 96 && Word[i].charCodeAt() < 123){
+      frogword+=smallAlpa[Word[i].charCodeAt()-97];
+    }
+  }
   var answer;
+  answer = frogword;
   return answer;
 }
 
