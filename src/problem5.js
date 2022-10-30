8971b8cfbbec81921cac94941396dde8d7ca3199
wnw <fkaus65@naver.com>
2022-10-30 18:39:06 +0900

    feat(problem5): complete problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let restMoney = money;
+
+  return moneyUnits.map((unit) => {
+    const value = Math.floor(restMoney / unit);
+    restMoney %= unit;
+
+    return value;
+  });
 }
 
 module.exports = problem5;
