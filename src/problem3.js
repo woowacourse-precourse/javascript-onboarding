26662f0d2f3982489c289113714e04cd5760261f
Y2IN <dpdls0228@daum.net>
2022-10-30 19:19:29 +0900

    feat : solve problem

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,29 @@
+function findThree(c) {
+  let count = 0;
+  if (c == 3 || c == 6 || c == 9) {
+    return 1;
+  }
+  return 0;
+}
+
+function splitArr(num) {
+  let count = 0;
+  const numStr = num.toString();
+  for (let i = 0; i < numStr.length; i++) {
+    count += findThree(numStr[i]);
+  }
+  return count;
+}
+
 function problem3(number) {
-  var answer;
+  const numArr = [];
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    numArr.push(i);
+  }
+  for (i = 0; i <= numArr[i]; i++) {
+    answer += splitArr(numArr[i]);
+  }
   return answer;
 }
 
