8330ffb347dbef87bd20add87852803cc6a8857c
HAEROOL <gofn080776@gmail.com>
2022-10-30 20:55:58 +0900

    feat: [problem3] - 수에 포함되어 있는 3,6,9의 수를 구하는 함수 작성

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,13 @@ function problem3(number) {
   return answer;
 }
 
+function countClap(number){
+  const countRegEx = /3|6|9/g;
+  const numberArr = String(number).match(countRegEx);
+  if(numberArr){
+    return numberArr.length;
+  }
+  return 0;
+}
+
 module.exports = problem3;
