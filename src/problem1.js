797bbe3f302937d5fd9156dbd946be07a551697e
하지훈 <jnh03336@gmail.com>
2022-10-30 13:09:18 +0900

    feat: pro1 logic design

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,26 @@
+const isValidPages = (pages) => {
+  return true;
+}
+
+const getGameScore = (pages) => {
+  return 100;
+}
+
 function problem1(pobi, crong) {
+  if(!isValidPages(pobi)) return -1;
+  if(!isValidPages(crong)) return -1;
+  
   var answer;
+
+  const pobiScore = getGameScore(pobi);
+  const crongScore = getGameScore(crong);
+
+  if(pobiScore === crongScore) {
+    answer = 0;
+  } else {
+    answer = pobiScore > crongScore ? 1 : 2;
+  }
+
   return answer;
 }
 
