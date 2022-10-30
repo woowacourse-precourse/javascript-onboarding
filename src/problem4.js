5ab51318dc99b2b7a34b4fad136c090bcee0ce93
kimyou1102 <yoosk5485@naver.com>
2022-10-30 22:22:16 +0900

    feat: 문제 4번 - 문자를 변환한다.
    대문자라면 대문자 청개구리 사전을 참고해서 변환한다.
    소문자라면 소문자 청개구리 사전을 참고해서 변환한다.
    알파벳 외의 문자는 변환하지 않는다.

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,20 @@ function problem4(word) {
   let answer = '';
   const upper_arr = ['Z', 'Y', 'X', 'W', 'V', 'U', 'T', 'S', 'R', 'Q', 'P', 'O', 'N', 'M', 'L', 'K', 'J', 'I', 'H', 'G', 'F', 'E', 'D', 'C', 'B', 'A'];
   const lower_arr = upper_arr.map(x => x.toLowerCase());
+  const UPPER_CASE_NUM = 65;
+  const LOWER_CASE_NUM = 97;
+
+  const words = word.split('');
+  for (let i = 0; i < words.length; i++) {
+    const ascii = words[i].charCodeAt(0);
+    if (ascii >= 65 && ascii <= 90) {
+      answer += upper_arr[ascii - UPPER_CASE_NUM];
+    } else if (ascii >= 97 && ascii <= 122) {
+      answer += lower_arr[ascii - LOWER_CASE_NUM];
+    } else {
+      answer += words[i];
+    }
+  }
 
   return answer;
 }
