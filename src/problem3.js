72706baf676944548cbc059e95171ffbd7546e7b
하송희 <99heesong@naver.com>
2022-10-30 16:09:57 +0900

    [문제 3] 기능추가: 주어진 값까지 반복문 돌며 특정 값 찾기

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,15 @@
 function problem3(number) {
-  var answer;
-  
   if(number<1||number>10000) throw Error('숫자가 범위를 벗어났습니다.');
 
-  return answer;
+  let clap = 0;
+  for(let currentNum=1; currentNum<=number; currentNum++) {
+    currentNum.toString().split("").forEach((item) => {
+      if(item==3||item==6||item==9) {
+        clap++;
+      }
+    })
+  }
+  return clap;
 }
 
 module.exports = problem3;
