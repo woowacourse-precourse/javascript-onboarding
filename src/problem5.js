7ffeeed9544c36cc5a883bb28e8c893a18606a4c
SeungWoo Kim <osabero@naver.com>
2022-10-30 18:06:23 +0900

    if else문을 활용하여 기능구현하였습니다. 남은 시간동안 더 깔끔하게 할 수 있는지 생각해보려 합니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,41 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  if(money/50000 >= 1) {
+    answer.push(Math.floor(money/50000));
+    money -= 50000*Math.floor(money/50000);
+  } else {answer.push(0)}
+  if(money/10000 >= 1) {
+    answer.push(Math.floor(money/10000));
+    money -= 10000*Math.floor(money/10000);
+  } else {answer.push(0)}
+  if(money/5000 >= 1) {
+    answer.push(Math.floor(money/5000));
+    money -= 5000*Math.floor(money/5000);
+  } else {answer.push(0)}
+  if(money/1000 >= 1) {
+    answer.push(Math.floor(money/1000));
+    money -= 1000*Math.floor(money/1000);
+  } else {answer.push(0)}
+  if(money/500 >= 1) {
+    answer.push(Math.floor(money/500));
+    money -= 500*Math.floor(money/500);
+  } else {answer.push(0)}
+  if(money/100 >= 1) {
+    answer.push(Math.floor(money/100));
+    money -= 100*Math.floor(money/100);
+  } else {answer.push(0)}
+  if(money/50 >= 1) {
+    answer.push(Math.floor(money/50));
+    money -= 50*Math.floor(money/50);
+  } else {answer.push(0)}
+  if(money/10 >= 1) {
+    answer.push(Math.floor(money/10));
+    money -= 10*Math.floor(money/10);
+  } else {answer.push(0)}
+  if(money >= 1) {
+    answer.push(money);
+  } else {answer.push(0)}
   return answer;
 }
-
 
 module.exports = problem5;
