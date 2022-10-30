d332873e54ed52103025702d02a73ac4d10c5c2f
Deemou <4106243@naver.com>
2022-10-30 13:27:22 +0900

    feat<P6>: 닉네임을 분리해 카운트와 함께 저장하는 함수 추가

--- src/problem6.js
+++ src/problem6.js
@@ -2,5 +2,24 @@ function problem6(forms) {
   var answer;
   return answer;
 }
+const getSeparatedNicknameMap = (forms) => {
+  const map = new Map(),
+    size = 2;
+  forms.forEach(([_, nickname]) => {
+    const pieces = [];
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const piece = nickname.substring(i, i + size);
+      pieces.push(piece);
+    }
+    pieces.sort();
+    pieces.forEach((piece, i) => {
+      if (piece != pieces[i - 1]) {
+        const count = map.has(piece) ? map.get(piece) + 1 : 1;
+        map.set(piece, count);
+      }
+    });
+  });
+  return map;
+};
 
 module.exports = problem6;
