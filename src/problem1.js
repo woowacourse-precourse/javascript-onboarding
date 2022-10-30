65198c97d8a8806348d550e8c1e56c59dc845a75
wnw <fkaus65@naver.com>
2022-10-30 18:27:10 +0900

    feat(problem1): add isExcept, getMaxScore
    isExcept: Exception handling
    getMaxScore: Get max score of pages

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,29 @@
+const isExcept = (pageL, pageR) => {
+  if (pageL >= pageR) return true;
+  if (Math.abs(pageL - pageR) > 1) return true;
+  if (!(pageL % 2)) return true;
+  if (pageR % 2) return true;
+
+  return false;
+};
+
+const getMaxScore = ([pageL, pageR]) => {
+  if (isExcept(pageL, pageR)) return -1;
+
+  const leftDigits = getDigits(pageL);
+  const rightDigits = getDigits(pageR);
+
+  return Math.max(calScore(leftDigits), calScore(rightDigits));
+};
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const pobiScore = getMaxScore(pobi);
+  const crongScore = getMaxScore(crong);
+
+  if (pobiScore === -1) return -1;
+  if (crongScore === -1) return -1;
+  if (pobiScore === crongScore) return 0;
+  return pobiScore > crongScore ? 1 : 2;
 }
 
 module.exports = problem1;
