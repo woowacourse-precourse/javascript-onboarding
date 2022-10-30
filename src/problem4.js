9679de5fcc41fc758f7ab3e87bc572b6dcefb488
ekkim <eklikeable@kakao.com>
2022-10-31 01:14:23 +0900

    🎨 함수 분리, 입력값이 알파벳이 아닌경우 그대로 출력

--- src/problem4.js
+++ src/problem4.js
@@ -1,27 +1,24 @@
+function convertToFrogDictionary(targetChar) {
+  if (targetChar >= 65 && targetChar <= 90) {
+    return String.fromCharCode(90 - (targetChar - 65));
+  } else if (targetChar >= 97 && targetChar <= 122) {
+    return String.fromCharCode(122 - (targetChar - 97));
+  }
+}
+
 function problem4(word) {
   const words = word.split('');
   const result = [];
-  for (e of words) {
-    if (e === ' ') {
-      // 문자열이 공백이면 result배열에 공백을 담아줌
-      result.push(' ');
+  for (char of words) {
+    const pattern = /[a-zA-Z]/;
+    if (pattern.test(char)) {
+      const targetASCII = char.charCodeAt();
+      result.push(convertToFrogDictionary(targetASCII));
     } else {
-      // 문자열이 공백이 아니라면
-      const targetWord = e.charCodeAt();
-      let replaceWord = '';
-      //문자열이 대문자인 경우 대체문자 찾기
-      if (targetWord >= 65 && targetWord <= 90) {
-        replaceWord = String.fromCharCode(90 - (targetWord - 65));
-        //문자열이 소문자인 경우 대체문자 찾기
-      } else if (targetWord >= 97 && targetWord <= 122) {
-        replaceWord = String.fromCharCode(122 - (targetWord - 97));
-      }
-      // 찾은 대체문자를 result 배열에 담아줌
-      result.push(replaceWord);
+      result.push(char);
     }
   }
-  // result 배열을 문자열로 리턴
-  return result.reduce((sum, cur) => sum + cur, '');
+  return result.join('');
 }
 
 module.exports = problem4;
