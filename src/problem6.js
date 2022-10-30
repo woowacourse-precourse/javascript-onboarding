65a3b46e13f1699c5c563802b29d6f3f8fb5323b
100Gyeon <bjy9966@naver.com>
2022-10-30 16:03:15 +0900

    feat: 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록을 오름차순으로 정렬해서 리턴

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,6 @@
 function problem6(forms) {
   const twoLettersMap = new Map();
+  const emailList = [];
 
   forms.forEach(([_, nickname]) => {
     for (let i = 0; i < nickname.length - 1; i++) {
@@ -15,6 +16,17 @@ function problem6(forms) {
   for (let [key, value] of twoLettersMap.entries()) {
     if (value < 2) twoLettersMap.delete(key);
   }
+
+  forms.forEach(([email, nickname]) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const twoLetters = nickname.substring(i, i + 2);
+      if (twoLettersMap.has(twoLetters)) {
+        emailList.push(email);
+      }
+    }
+  });
+
+  return emailList.sort();
 }
 
 module.exports = problem6;
