3e8da64b073a6cb88c4fafb280dc1a16f455714d
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-30 22:08:08 +0900

    [문제5] <1단계 기능요구사항> 화폐단위를 저장한 객체, 지불할 화폐 수를 저장할 배열 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
-  return answer;
+    // 1단계) 0~8까지의 인덱스별로 화폐의 단위를 각각 저장한 객체 생성
+    const values = {
+        0: 50000,
+        1: 10000,
+        2: 5000,
+        3: 1000,
+        4: 500,
+        5: 100,
+        6: 50,
+        7: 10,
+        8: 1,
+    };
+
+    // 1단계) 요소를 0으로 채운 길이가 9인 배열 생성
+    const arr = Array.from({ length: 9 }, (el) => 0);
 }
 
 module.exports = problem5;
