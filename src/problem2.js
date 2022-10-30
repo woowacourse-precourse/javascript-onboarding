aef5863bee0d38ca35efbfd53840a2bffa4101cb
yb8350 <yb8350@naver.com>
2022-10-30 18:31:01 +0900

    Feat: problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
   var answer;
+  while (true) {
+    let tf = true;
+    let tmp = "";
+    let cryptogramArr = cryptogram.split("");
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (cryptogramArr[i] === cryptogramArr[i + 1]) {
+        tf = false;
+        i++;
+      } else tmp += cryptogramArr[i];
+    }
+    cryptogram = tmp;
+    if (tf) break;
+  }
+  answer = cryptogram;
   return answer;
 }
 
