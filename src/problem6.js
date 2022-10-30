595ecff9e8f9d8b938f212b48140a0e04a922079
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-31 00:17:43 +0900

    dosc<problem6>: Define features list

--- src/problem6.js
+++ src/problem6.js
@@ -1,40 +1,19 @@
-// function problem6(forms) {
-//   let answer;
-//   return setArrToJSON(forms);
-// }
-// module.exports = problem6;
+/* 
+ * Features
+ * 1. 닉네임 중 같은 글자가 연속적으로 중복되는 지원자 검사
+ * 2. 이메일 목록 리턴
+ *  
+ * Exceptions
+ * 1. 크루는 1명 이상 10,000명 이하
+ * 2. 이메일은 이메일 형식에 부합하며, 전체 길이는 11자 이상 20자 미만이다.
+ * 3. 신청할 수 있는 이메일은 email.com 도메인으로만 제한
+ * 4. 닉네임은 한글만 가능하고 전체 길이는 1자 이상 20자 미만
+ * 5. result는 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거
+ */
 
-
-const setArrToJSON = arr => {
-  let result = [];
-
-  arr.map(item => {
-    result.push({
-      email: item[0], 
-      nickname: item[1]
-    })
-  })
-
-  return result;
+function problem6(forms) {
+  let answer;
+  return answer;
 }
-
-let forms = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ]
-
-let nform = setArrToJSON(forms)
-
-nform.map((user)=>{
-  let charCombination = [];
-
-  let nameParsing = user.nickname.split("");
-  // console.log(user.nickname.length)
-  for (let i = 0; i < nameParsing.length; i++) {
-    for (let j = i+1; j < nameParsing.length; j++){
-      charCombination.push(nameParsing[i] + nameParsing[j])
-    }
-  }
-
-  user.parsingName = charCombination
-})
-
-console.log(nform)
+module.exports = problem6;
 
