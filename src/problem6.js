62d2efff216bf56fac83aa0a94b4cabd4c1ccf42
lsy20140 <lsy20140@naver.com>
2022-10-30 20:29:57 +0900

    feat: [문제6] 각 닉네임 두 글자씩 잘라 배열에 저장

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,19 @@ function checkEmail(crew, idx, forms){
   }
 }
 
+function splitNickname(forms){
+  const twoWordArr = [];
+
+  for(let i = 0 ; i<forms.length ; i++){
+    const add = [];
+    for(let j = 0; j<forms[i][1].length - 1 ; j++){
+      add.push(forms[i][1].substr(j,2));
+    }
+    twoWordArr.push(add);
+  }
+  return twoWordArr;
+}
+
 function problem6(forms) {
   forms.forEach(checkEmail); 
 }
