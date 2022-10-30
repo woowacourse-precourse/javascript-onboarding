4cf0b8d6ebe95ad2d59fb22f604d47768b51f7f9
YejinHwang-D <hyj3463@naver.com>
2022-10-30 18:15:33 +0900

    feat: [problem3-4] 솔루션 작성 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  var tempForCheck;
+  var result = 0;
+
+  for (var i=1; i<=number; i++) {
+    tempForCheck = i.toString();
+    result = tempForCheck.match(/[3|6|9]/g);
+    if (result != undefined)
+      answer += result.length;
+  }
+
   return answer;
 }
 
