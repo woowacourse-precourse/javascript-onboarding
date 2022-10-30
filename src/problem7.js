bbbc43069454d1f5944f58fcc3bf9964f9b356d9
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-10-30 09:10:07 +0900

    problem7 friends의 배열에서 user의 친구 추출

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,7 @@
 function problem7(user, friends, visitors) {
   var answer;
   let friendScore = {}
+  let userFriend = []
   for (let i = 0; i<visitors.length; i++){
     if(visitors[i] in friendScore == false){
       friendScore[visitors[i]] = 1
@@ -9,6 +10,15 @@ function problem7(user, friends, visitors) {
       friendScore[visitors[i]]+=1
     }
   }
+  for (let j = 0; j<friends.length; j++){
+    for (let k = 0;k<friends[k].length; k++){
+      if (friends[j][k] == user){
+        userFriend.push(friends[j][(k+1)%2])
+      }
+    }
+  }
+
   return answer;
 }
 module.exports = problem7;
+
