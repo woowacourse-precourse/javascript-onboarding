784c1b677ae85bfbbec6b6f5dc4126a19f9d2214
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-10-30 13:12:36 +0900

    fix: 문제7 알고리즘 점수 부여 오류, 친구 추천에 본인 및 현재 친구 포함되는 오류 수정
    [] (수정 전) 기존에 이름이 있는 데도 이름 및 알고리즘 점수가 배열에 새로 추가됨 => (수정 후) 기존에 이름이 있는 경우, 해당 이름의 알고리즘 점수만 누적 추가됨
    [] (수정 전) 친구 추천 목록에 본인 및 현재 친구 포함 됨 => (수정 후) indexOf 함수를 이용해 본인 및 현재 친구는 포함하지 않게 수정함

--- src/problem7.js
+++ src/problem7.js
@@ -1,26 +1,31 @@
 function problem7(user, friends, visitors) {
-  let answer;
-  let friendCopy = [];
+  let answer = [];
+  let friendAndMe = [user];
   let algorithmArr = [];
 
   for (let index = 0; index < friends.length; index++) {
-    friendCopy.push(friends[index][0]);
+    friendAndMe.push(friends[index][0]);
 
     let overlapIdx = algorithmArr.indexOf(friends[index][1]);
-    if (overlapIdx != -1 && friends[index][1].length === algorithmArr[overlapIdx][0]) {
-      algorithmArr[overlapIdx][1] += 10;
+    if (friendAndMe.indexOf(friends[index][1]) != -1) {
+      continue;
+    } else if (overlapIdx != -1 && friends[index][1].length === algorithmArr[overlapIdx].length) {
+      algorithmArr[overlapIdx + 1] += 10;
       continue;
     }
-    algorithmArr.push([friends[index][1], 10]);
+    algorithmArr.push(friends[index][1], 10);
   }
 
   for (index = 0; index < visitors.length; index++) {
     let overlapIdx = algorithmArr.indexOf(visitors[index]);
-    if (overlapIdx != -1 && visitors[index].length === algorithmArr[overlapIdx][0]) {
-      algorithmArr[overlapIdx][1] += 1;
+
+    if (friendAndMe.indexOf(visitors[index]) != -1) {
+      continue;
+    } else if (overlapIdx != -1 && visitors[index].length === algorithmArr[overlapIdx].length) {
+      algorithmArr[overlapIdx + 1] += 1;
       continue;
     }
-    algorithmArr.push([visitors[index], 1]);
+    algorithmArr.push(visitors[index], 1);
   }
 
   algorithmArr.sort(function (a, b) {
@@ -29,13 +34,15 @@ function problem7(user, friends, visitors) {
     if (a < b) return -1;
   });
 
-  for(index=0;index<5; index++){
-    if(algorithmArr[index]){
+  for (index = 0; index < 5; index++) {
+    if (algorithmArr[index]) {
       break;
     }
     answer.push(algorithmArr[index][0]);
   }
-  return answer.sort();
+  answer = answer.sort();
+  console.log(answer);
+  return answer;
 }
 
 module.exports = problem7;
