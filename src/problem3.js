c3e7b87987880ac1503e105dbf32f0cee3ef205d
seoyoung-dev <seoyoung.dev@gmail.com>
2022-10-31 00:43:09 +0900

    Add problem3 solution

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    let answer = 0;
+    let digits = [];
+    for (let i = 1; i <= number; i++) {
+        digits = i
+            .toString()
+            .split("")
+            .map((ele) => parseInt(ele));
+
+        digits.forEach((num) => {
+            if (num === 3 || num === 6 || num === 9) {
+                answer += 1;
+            }
+        });
+    }
+    return answer;
 }
 
 module.exports = problem3;
