c0fe964780368a43112f26f6e20ce49596c738da
MinGyu-Kang <kagrin97@gmail.com>
2022-10-30 11:25:20 +0900

    problem3/refactor : 문자열 배열을 순회하면서 3,6,9의 개수를 체크하는 코드를 findThreeSixNine함수로 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,15 +1,20 @@
-function problem3(number) {
+function findThreeSixNine(array, count) {
   const findNumber = ["3", "6", "9"];
+  array.forEach((str) => {
+    if (findNumber.includes(str)) {
+      count += 1;
+    }
+  });
+  return count;
+}
+
+function problem3(number) {
   let count = 0;
 
   for (let n = 1; n < number + 1; n++) {
     const strArr = String(n).split("");
 
-    strArr.forEach((str) => {
-      if (findNumber.includes(str)) {
-        count += 1;
-      }
-    });
+    count = findThreeSixNine(strArr, count);
   }
 
   return count;
