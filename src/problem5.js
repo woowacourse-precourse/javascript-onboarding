033bd2f98ec3038f8ec2bb86f6a97139a47ff970
TeusLee <xodn1311@gmail.com>
2022-10-30 15:49:36 +0900

    prob5 sol complete

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,18 @@
 function problem5(money) {
-  var answer;
+  var unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  unit_index = 0
+  while (unit_index <= answer.length){
+    while (true){
+      if (money >= unit[unit_index]){
+        money = money - unit[unit_index]
+        answer[unit_index] = answer[unit_index] + 1
+      } else {
+        break
+      }
+    }
+    unit_index = unit_index + 1
+  }  
   return answer;
 }
 
