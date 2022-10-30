2f799dfdd64b5029ed629833c233c02188aa280d
myung-j-p <jmpack97@gmail.com>
2022-10-30 23:41:22 +0900

    문제4_대소문자 판별하고 그에 맞춰 변환과 함께 출력

--- src/problem4.js
+++ src/problem4.js
@@ -1,19 +1,25 @@
 function problem4(word) {
   var answer = [];
-  const arr = 'abcdefghijklmnopqrstuvwxyz';
-  const alphabetArr = [...arr];
+  let arr = 'abcdefghijklmnopqrstuvwxyz';
+  let alphabetArr = [...arr];
   let words = [...word];
 
-  for (l in words){
+  function indexer (l){
     for (let i = 0 ; i < alphabetArr.length ; i++){
-      if (words[l].toLowerCase() == alphabetArr[i]){
+      if (words[l] == alphabetArr[i]){
         answer.push(alphabetArr[alphabetArr.length -i -1]);
+      } else if (words[l] == alphabetArr[i].toUpperCase()){
+        answer.push(alphabetArr[alphabetArr.length -i -1].toUpperCase());
       }
     }
   }
+  for (l in words){
+    console.log(l);
+    if (words[l] == ' '){answer.push(words[l])}
+    else  {indexer(l);}
+  }
 
-  return answer;
+  return answer.join('');
 }
-
 
 module.exports = problem4;
--- src/tempCodeRunnerFile.js
+++ src/tempCodeRunnerFile.js
@@ -1,3 +1,4 @@
-
-  //   if (a == )
-  // }
+        // let UpperArr = alphabetArr.toUpperCase;
+        // for (let i = 0 ; i < alphabetArr.length ; i++){
+        //   if (words[l] == UpperArr[i]){
+        //     answer.push(UpperArr[UpperArr.length -i -1]);
