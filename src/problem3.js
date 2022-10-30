eae803918125fa85c866dc9785c83a3a0b46e227
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 00:08:12 +0900

    정규식 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
+/*
+ ### 기능 목록
+ 
+  1. number가 3,6,9 박수를 칠 수 있는지 확인하기 위해, 정규식을 작성한다.
+  2. number에서 3,6,9 박수를 친다면,
+    2-1. 3부터 number 까지의 박수의 개수를 센다.
+    number에서 3,6,9 박수를 안친다면, 0을 반환한다.
+  3. 각 자리수의 3,6,9 가 들어갔을때, 누적해서 더한다.
+  */
+
 function problem3(number) {
   var answer;
+  const regex = /[3,6,9]/g;
+
   return answer;
 }
 
