5c6a63a586c09c49cd70850f810f49cd2f5c99ee
kaori-killer <sksbthwjd@daum.net>
2022-10-30 13:32:57 +0900

    feat: 이름을 2개 단위로 저장할 딕셔너리 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let nameCheckBox = {};
+
 }
 
+problem6([["jm@email.com", "제이엠"],
+["jason@email.com", "제이슨"], 
+["woniee@email.com", "워니"], 
+["mj@email.com", "엠제이"], 
+["nowm@email.com", "이제엠"]]);
+
 module.exports = problem6;
+
