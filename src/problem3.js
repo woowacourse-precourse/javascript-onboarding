1b6297a87a9390406668726f8c129186f01065bd
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-30 20:35:01 +0900

    feat: problem 3 기능 완성

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,16 @@
-function problem3(number) {}
+function problem3(number) {
+  let count = 0;
+  let j = 0;
+  if (!checkException(number)) return;
+  for (let i = 0; i <= number; i++) {
+    j = i;
+    while (j > 0) {
+      if (j % 10 === 3 || j % 10 === 6 || j % 10 === 9) count++;
+      j = parseInt(j / 10);
+    }
+  }
+  return count;
+}
 
 function checkException(number) {
   return number <= 10000 && number >= 1;
