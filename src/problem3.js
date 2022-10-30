f16947542376268d794cab533ab5916f8e0a811f
nalsae <nalsae0207@naver.com>
2022-10-30 23:19:02 +0900

    feat: Add a logic for solution about find char '3'
    - 3을 포함한 개수만큼 answer에 더하는 로직으로 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
-function problem3(number) {
-  var answer;
+const problem3 = (number) => {
+  let answer = 0;
+
+  Array.from({ length: number }).forEach((_, i) => {
+    const matchResult = `${i + 1}`.match(/3|6|9/g);
+
+    if (matchResult) answer += matchResult.length;
+  });
+
   return answer;
-}
+};
 
 module.exports = problem3;
