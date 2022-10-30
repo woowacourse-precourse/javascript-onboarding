fc69063ff65796c7b503951f0d3331f839365c38
HWI <dlwognl99@naver.com>
2022-10-30 21:05:06 +0900

    문제4_테스트

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -1,7 +1,7 @@
 const problem1 = require("../src/problem1");
 const problem2 = require("../src/problem2");
 const problem3 = require("../src/problem3");
-// const problem4 = require("../src/problem4");
+const problem4 = require("../src/problem4");
 // const problem5 = require("../src/problem5");
 // const problem6 = require("../src/problem6");
 // const problem7 = require("../src/problem7");
@@ -38,11 +38,11 @@ describe("problem3", () => {
   });
 });
 
-// describe("problem4", () => {
-//   test("case1", () => {
-//     expect(problem4("I love you")).toEqual("R olev blf");
-//   });
-// });
+describe("problem4", () => {
+  test("case1", () => {
+    expect(problem4("I love you")).toEqual("R olev blf");
+  });
+});
 
 // describe("problem5", () => {
 //   test("case1", () => {
