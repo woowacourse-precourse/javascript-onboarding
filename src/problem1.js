ccdc42d72e326617a5f03b48ce03269dc0ecb58b
Adult96 <djxor007@gmail.com>
2022-10-30 19:07:19 +0900

    refactoring: problem1 함수 단위로 리펙토링

--- src/problem1.js
+++ src/problem1.js
@@ -1,28 +1,12 @@
 function problem1(pobi, crong) {
   let answer = 0;
-  let pobiArr = [];
-  let crongArr = [];
 
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
     return -1;
   }
 
-  for (let i = 0; i < 2; i++) {
-    const pobiSum = (pobi[1] + '')
-      .split('')
-      .map((v) => parseInt(v))
-      .reduce((s, n) => (i > 0 ? (s *= n) : (s += n)));
-    pobiArr.push(pobiSum);
-
-    const crongSum = (crong[1] + '')
-      .split('')
-      .map((v) => parseInt(v))
-      .reduce((s, n) => (i > 0 ? (s *= n) : (s += n)));
-    crongArr.push(crongSum);
-  }
-
-  const pobiMax = Math.max(...pobiArr);
-  const crongMax = Math.max(...crongArr);
+  const pobiMax = Math.max(...arrSum(pobi));
+  const crongMax = Math.max(...arrSum(crong));
 
   if (pobiMax > crongMax) {
     answer = 1;
@@ -35,3 +19,15 @@ function problem1(pobi, crong) {
 }
 
 module.exports = problem1;
+
+const arrSum = (arr) => {
+  let result = [];
+  for (let i = 0; i < 2; i++) {
+    const sum = (arr[1] + '')
+      .split('')
+      .map((v) => parseInt(v))
+      .reduce((s, n) => (i > 0 ? (s *= n) : (s += n)));
+    result.push(sum);
+  }
+  return result;
+};
