c120062a9e98751e0d61cad7b9fbd40785bf08f6
runkey <csi9509@naver.com>
2022-10-31 00:42:10 +0900

    Feat: 아스키코드 값 이용해서 대소문자 판단해서 반댓값 출력

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,29 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let result = "";
+  let ascii = 0;
+  let reverse = 0;
+  let abstemp = 0;
+  
+  // 대문자, 소문자 변환
+  for(let i = 0; i < word.length; i++) {
+      // 대문자인지 소문자인지 판단
+      if(word[i] === " ") {
+          result += " ";
+          continue;
+      }
+      else if(word[i] === word[i].toUpperCase()) {
+          ascii = 65;
+      }
+      else if(word[i] === word[i].toLowerCase()) {
+          ascii = 97;
+      }
+      abstemp = Math.abs(ascii - word.charCodeAt(i));
+      reverse = (ascii + 25) - abstemp;
+      result += String.fromCharCode(reverse);
+  }
+  
+  return result;
+
 }
 
 module.exports = problem4;
