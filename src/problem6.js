723ba084b5c8efe1ab53eccad563a59ee68e20af
SeongHo-C <leeseong010@naver.com>
2022-10-30 18:20:41 +0900

    comment(problem6): 기능 목록 주석 변경

--- src/problem6.js
+++ src/problem6.js
@@ -3,7 +3,11 @@
 // 닉네임 검증 기능
 
 function problem6(forms) {
-  let answer;
+  let answer = new Set();
+
+  for (let i = 0; i < forms.length; i++) {
+    // 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 추가 기능
+  }
 
   return answer;
 }
