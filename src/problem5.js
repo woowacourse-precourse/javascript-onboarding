abef1149da27c173f91be2de3e572d3a3f6368a3
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-30 19:04:57 +0900

    feat(problem5): 각 기준 금액과 배열의 인덱스를 매핑하는 열거형 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,15 @@
+const Amounts = Object.freeze({
+  BILL50: 0,
+  BILL10: 1,
+  BILL5: 2,
+  BILL1: 3,
+  COIN500: 4,
+  COIN100: 5,
+  COIN50: 6,
+  COIN10: 7,
+  COIN1: 8
+})
+
 function problem5(money) {
   var answer;
   return answer;
