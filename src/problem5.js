83471de237ed656578d22a74db9dd02f666ed9c0
waymokorea <taw0207@naver.com>
2022-10-30 16:43:30 +0900

    problem5 test

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyunits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [];
+
+  for (let i = 0; i < moneyunits.length; i++) {
+    const currUnit = moneyunits[i];
+    const needMoney = Number.parseInt(money / currUnit);
+
+    result.push(needMoney);
+    money -= currUnit * needMoney;
+  }
+
+  return result;
 }
 
 module.exports = problem5;
