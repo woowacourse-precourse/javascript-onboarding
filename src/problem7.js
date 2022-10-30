a82baed6ccc243152de8e724162037fe50aab796
leeuihyun <meta212121@naver.com>
2022-10-30 19:49:41 +0900

    feat : problem7 user의 친구의 친구라면 +10점 추가

--- src/problem7.js
+++ src/problem7.js
@@ -7,6 +7,21 @@ function friendOfFriend(user, friends, set) {
   });
   return { friends, set };
 }
+
+function friendAddTen(friends, set, obj) {
+  friends.forEach((item) => {
+    const [A, B] = item;
+    if (set.has(A)) {
+      if (!obj[B]) obj[B] = 10;
+      else obj[B] += 10;
+    }
+    if (set.has(B)) {
+      if (!obj[A]) obj[A] = 10;
+      else obj[A] += 10;
+    }
+  });
+  return obj;
+}
 function problem7(user, friends, visitors) {
   var answer = [];
   var obj = {};
@@ -14,6 +29,7 @@ function problem7(user, friends, visitors) {
   var firstData = friendOfFriend(user, friends, set);
   set = firstData.set;
   friends = firstData.friends;
+  obj = friendAddTen(friends, set, obj);
   return answer;
 }
 
