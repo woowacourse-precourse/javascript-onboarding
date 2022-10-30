94fa84890777fb63c296334c627fd84036d93632
이여진 <jinspace3710@gmail.com>
2022-10-31 07:12:28 +0900

    feat: problem1 기능 2 - 점수 계산하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,9 @@
 function problem1(pobi, crong) {
   let answer;
+  let pobiScore, crongScore;
   if (isValid(pobi) && isValid(crong)) {
+    pobiScore = calculateScore(pobi);
+    crongScore = calculateScore(crong);
   } else {
     answer = -1;
   }
@@ -21,5 +24,31 @@ function isValid(bookArray) {
   )
     return true;
   else return false;
+}
+
+/*
+본인의 점수를 구하는 함수
+1. 왼쪽, 오른쪽 페이지 번호의 각 자리수의 곱과 합 구하기
+2. 1의 결과값 중 가장 큰 값 구하기
+*/
+function calculateScore(bookArray) {
+  let score = 0;
+  
+  for (int i = 0; i < bookArray.length; i++) {
+    let plusScore = 0;
+    let multiplyScore = 1;
+
+    while (bookArray[i] > 0) {
+      plusScore += bookArray % 10;
+      multiplyScore *= bookArray % 10;
+      bookArray = parseInt(bookArray / 10);
+    }
+
+    if (plusScore > score || multiplyScore > score) {
+      if (plusScore >= multiplyScore) score = plusScore;
+      else score = multiplyScore;
+    }
+  }
+  return score;
 }
 module.exports = problem1;
