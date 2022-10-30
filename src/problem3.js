d13b1051656909beacd2770d9cec0024b94e8df7
wnw <fkaus65@naver.com>
2022-10-30 18:32:36 +0900

    feat(problem3): add has369, get369Count
    has369: Check for inclusion of 369
    get369Count: if num has 369, return the length of 369

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
+const has369 = (num) => /[369]/.test(`${num}`);
+const get369Count = (num) => `${num}`.match(/[369]+/g).join('').length;
+
 function problem3(number) {
   var answer;
+
   return answer;
 }
 
