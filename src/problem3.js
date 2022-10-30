88e93086f031cca8f968736dea0edc94fdf7a953
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-10-30 22:37:30 +0900

    style: 변수 선언 위치 변경 및 변수선언은 모두 let으로 선언

--- src/problem3.js
+++ src/problem3.js
@@ -1,12 +1,11 @@
 function problem3(number) {
-  var answer = 0;
-  let numString;
-  let howMany = 0;
-
   if (number < 1 || number > 10000 || Number.isInteger(number) === false) {
     return -1;
   }
 
+  let answer = 0;
+  let numString;
+  let howMany = 0;
   for (let i = 1; i <= number; i++) {
     numString = i.toString();
     howMany = numString.match(/[3,6,9]/g);
