6e1ee8487d4bb3d6adf1bb87b30380d1e20fe54e
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-10-30 21:44:54 +0900

    feat: solve problem4 1st

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,22 @@
-function problem4(word) {
+function problem4(word){
   var answer;
+  const [code_A, code_Z, code_a, code_z] =
+        ['A'.charCodeAt(0), 'Z'.charCodeAt(0),'a'.charCodeAt(0), 'z'.charCodeAt(0)];
+  const [high, low] = [code_A+code_Z, code_a+code_z];
+  word = [...word].map((i)=>{
+      if(/[a-zA-Z]/.test(i)){
+          if(i==i.toUpperCase()){
+              return String.fromCharCode(high-i.charCodeAt(0));
+          }
+          else{
+              return String.fromCharCode(low-i.charCodeAt(0));
+          }
+      }
+      else{
+          return i;
+      }
+  })
+  answer = word.join('');
   return answer;
 }
 
