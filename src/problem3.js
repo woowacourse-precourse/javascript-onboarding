779e164016484582845a8992eef83c716293266e
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-10-31 00:04:33 +0900

    feat: 문제 3 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let result = 0;
+  for (let i = 1; i <= number; i++) {
+    let num = i.toString();
+    // 숫자에 3, 6, 9가 있다면 해당 개수를 result에 추가
+    for (let j = 0; j < num.length; j++) {
+      if (num[j] === "3" || num[j] === "6" || num[j] === "9") {
+        result++;
+      }
+    }
+  }
+  return result;
 }
 
 module.exports = problem3;
