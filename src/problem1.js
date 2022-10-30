3c901456b13b4de0e6d6a92761515b165807813d
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-10-30 23:26:24 +0900

    feat: 문제 1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,27 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  // 각 자리 숫자를 더하거나 곱한 값 중 가장 큰 값 찾기
+  function calculate(num) {
+    numArr = num.toString().split("");
+
+    const add = numArr.reduce((acc, cur) => acc + parseInt(cur), 0);
+    const mul = numArr.reduce((acc, cur) => acc * parseInt(cur), 1);
+    return Math.max(add, mul);
+  }
+
+  let result;
+  const pobiScore = Math.max(...pobi.map((num, index) => calculate(num)));
+  const crongScore = Math.max(...crong.map((num, index) => calculate(num)));
+
+  // pobi와 crong의 점수 비교하여 최종 승부 점수 구하기
+  if (pobiScore > crongScore) {
+    result = 1;
+  } else if (pobiScore < crongScore) {
+    result = 2;
+  } else {
+    result = 0;
+  }
+
+  return result;
 }
 
 module.exports = problem1;
