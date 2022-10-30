cf5ee049ff22b8ceb78feea9afd3be61f650a04f
nerdchanii <nerdchanii@gmail.com>
2022-10-30 13:55:28 +0900

    feat: [PROBLEM5] 계산을 위한 함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,8 @@
 function problem5(money) {
   const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  function caculate(money, unit) {
+    return [money % unit, Math.floor(money / unit)];
+  }
 }
 
 function map(f, iter) {
