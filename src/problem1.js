6de33cb6221a12545440fd6d6553ba21306244c2
kwh97 <zzzang0512@naver.com>
2022-10-31 01:38:16 +0900

    feat(problem1): 덧셈결과와 곱셈결과 중에서 더 큰 수를 반환하는 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,24 @@ function problem1(pobi, crong) {
 
 }
 
+function getMax(num) {
+  const sum = getAddResult(num);
+  const mul = getMulResult(num);
+  let max = 0;
+
+  if (sum > mul) {
+    max = sum;
+  } 
+  if (sum < mul) {
+    max = mul;
+  }
+  if (sum === mul) {
+    max = sum;
+  }
+
+  return max;
+}
+
 function getAddResult(num) {
   let result = 0;
   while(num !== 0) {
