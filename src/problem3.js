b28084a7e7226541d4b248734e04b43959ab6952
ZUITOPIA <korhwngml123@naver.com>
2022-10-30 15:52:16 +0900

    feat: (#3) 1부터 number 까지의 수를 모두 split하고, split 된 각 자릿수마다 count 하기

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,13 @@ function problem3(number) {
   let countSum;
   let eachCountArr = new Array(10).fill(0);
 
+  for (let i = 1; i <= number; i++) {
+    let splitNumArr = String(i).split("");
+    for (n = 0; n < splitNumArr.length; n++) {
+      eachCountArr[splitNumArr[n] - "0"]++;
+    }
+  }
+
   countSum = eachCountArr[3] + eachCountArr[6] + eachCountArr[9];
   return countSum;
 }
