6c22cd63e99e29e6dcc34a0b758e042e724c8129
SeungWoo Kim <osabero@naver.com>
2022-10-30 16:52:32 +0900

    알파벳 이외문자가 왔을때 변환하지 않도록 예외처리를 구현하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,17 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  var alpha = [A,B,C,D,E,F,G,H,I,J,K,L,M,N,O,P,Q,R,S,T,U,V,W,X,Y,Z]
+  var alphaReverse = alpha.sort()
+  for (let x of word) {
+    if (x.charCodeAt()<65){answer += x; continue}
+    if (x.charCodeAt()>122){answer += x; continue}
+    if (x.charCodeAt()>90 && x.charCodeAt()<97){answer += x; continue}
+    //알파벳 이외문자 예외처리
+    
+    let check = x.toUpperCase()
+  }
   return answer;
-  
+
 }
 
 module.exports = problem4;
