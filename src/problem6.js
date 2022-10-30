9b4c9489df4bee5eb86579c6b1c967815e76e321
이예나 <yenalee@MacBook-Pro.local>
2022-10-30 19:47:17 +0900

    Feat: Make nickname array

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
-  var answer;
+  var answer=[];
+  var form = [];
+  for(let i=0; i<forms.length;i++){
+    form.push(forms[i][1]);
+  }
   return answer;
 }
 
 module.exports = problem6;
+// forms = [["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]];
+// problem6(forms);
