e827bb0ab9b9576b21f42ffb6b59db0037e4028e
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-30 20:16:01 +0900

    problem5 - feat: 분류값 업데이트 함수
    새로운 결과가 나오면 기존의 값과 함께 새로운 배열을 반환한다.

--- src/problem5.js
+++ src/problem5.js
@@ -6,6 +6,14 @@ const classifyMoney = (currentMoney, won) => Math.floor(currentMoney / won);
 
 const getRemainingMoney = (currentMoney, wonCount, won) => currentMoney - wonCount * won;
 
+const updateClassificationResults = (resultArray, currentIndex, wonCount) => {
+  const copiedArray = copyArray(resultArray);
+
+  copiedArray[currentIndex] = wonCount;
+
+  return copiedArray;
+};
+
 function problem5(money) {
   var answer;
   return answer;
