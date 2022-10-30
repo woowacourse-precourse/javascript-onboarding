6d00a76ba806f1a58c157054a3a65d746e96ffd9
lyssoi <114637463+lyssoi@users.noreply.github.com>
2022-10-31 02:03:34 +0900

    problem2 오류수정

--- src/problem2.js
+++ src/problem2.js
@@ -3,21 +3,32 @@ function problem2(cryptogram) {
   var answer;
   var cryptogramWord = cryptogram;
   var cryptogramArray = cryptogramWord.split("");
+  var prev ="";
   answer = noCombination(cryptogramArray);
+  while(prev!==answer){
+    prev=answer;
+    answer=noCombination(answer);
+  }
   return answer;
 }
 
 function noCombination(arr){
-  var answer = arr;
+  var answer = [];
+  var newCrypto = [];
   for(let i=0;i<arr.length;i++){
-    if(arr[i]==arr[i+1]){
-     answer.splice(i);
+    if(arr[i]!==arr[i-1]){
+      newCrypto.push(arr[i])
+    }
+    else{
+      newCrypto.pop()
     }
 }
-  answer = answer.join("");
+  answer = newCrypto.join("");
   return answer;
 
 }
+
+console.log(problem2("zyelleyz"))
 
 
 module.exports = problem2;
