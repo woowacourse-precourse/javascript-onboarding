827b67532d166fd743f4a678a60f4ed83e8f13d7
Jin1won <wlsdnjs16521@gmail.com>
2022-10-31 00:04:57 +0900

    [문제7] feat: 사용자간의 관계를 초기화한다

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer = [];
+  const relationships = {};
+
+  friends.forEach((friend) => {
+    //사용자간의 관계를 초기화한다.
+    if (!Object.keys(relationships).includes(friend[0]))
+      relationships[friend[0]] = [];
+    if (!Object.keys(relationships).includes(friend[1]))
+      relationships[friend[1]] = [];
+    relationships[friend[0]].push(friend[1]);
+    relationships[friend[1]].push(friend[0]);
+  });
+
   return answer;
 }
 
