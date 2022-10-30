a0ff1d9156dc1ead47754e03f5e7f1c75db7a5d8
runkey <csi9509@naver.com>
2022-10-31 00:42:47 +0900

    Feat: 각 가격별로 몇 장씩 필요한지 몫을 통해 입력

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let result = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  let temp = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  for (let i = 0 ; i < 9 ; i++){
+    result[i] = parseInt(money/temp[i])
+    money -= result[i] * temp[i]
+  }
+  return result;
 }
 
 module.exports = problem5;
