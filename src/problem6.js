c4da29fb6c061636d18aa66d8d8d8ab2d30e637e
MJ <mj632900@gmail.com>
2022-10-31 01:03:36 +0900

    feat : 이름 사용자의 수를 객체로 반환하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -5,6 +5,17 @@ function getNickName(list) {
     });
 }
 
+function checkNameCount(name) {
+    let nameList = {};
+    name.forEach((na) => {
+        for (let i = 0; i < na.length - 1; i++) {
+            const name = na.slice(i, i + 2);
+            nameList[`${name}`] = (nameList[`${name}`] += 1) || 1;
+        }
+    });
+    return nameList;
+}
+
 function problem6(forms) {}
 
 module.exports = problem6;
