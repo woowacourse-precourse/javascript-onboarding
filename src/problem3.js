d95b0dd782117b4ad32805d486e2010515cfccc3
LEEJW1953 <jiwonlee1953@naver.com>
2022-10-30 16:43:13 +0900

    3번 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,19 +1,19 @@
 function game(num) {
-  let count = 0
+  let count = 0;
   while (num !== 0) {
     let temp = num % 10;
     if ((temp === 3) || (temp === 6) || (temp === 9)) {
-      count += 1
+      count += 1;
     }
     num = parseInt(num / 10);
   }
-  return count
+  return count;
 }
 
 function problem3(number) {
   var answer = 0;
   for (let i = 0; i <= number; i++) {
-    answer += game(i)
+    answer += game(i);
   }
   return answer;
 }
