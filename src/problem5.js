fd42abc38269bb02341489fb608e687ab1bdcd09
하송희 <99heesong@naver.com>
2022-10-30 22:52:28 +0900

    [문제 5] 기능추가: 금액 단위 배열 끝까지 돌아서 최종 배열 저장

--- src/problem5.js
+++ src/problem5.js
@@ -2,10 +2,13 @@ function problem5(money) {
   const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let wallet = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
-  while(unit[i] <= money) {
-    money -= unit[i];
-    wallet[i]++;
-  }
+  for(let i=0; i<unit.length; i++) {
+    while(unit[i] <= money) {
+      money -= unit[i];
+      wallet[i]++;
+    }
+  } 
+  return wallet;
 }
 
-  module.exports = problem5;
+module.exports = problem5;
