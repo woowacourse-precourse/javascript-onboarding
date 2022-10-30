118daecfe447aa6a7803fad22db1360a0e9c460d
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-31 05:23:58 +0900

    feat: [problem7] - 추천 친구를 구하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -8,6 +8,18 @@ function problem7(user, friends, visitors) {
     var interWithUserFreinds = friends[i].filter((element) =>
       userFriends.includes(element)
     ); // 사용자 친구 목록과의 교집합
+
+    // 추천 점수 10점, 둘 중의 한명은 사용자 친구인 경우
+    if (interWithUserFreinds.length === 1) {
+      var friend = friends[i][0];
+      var recommendedFriend;
+
+      if (userFriends.includes(friend)) {
+        recommendedFriend = friends[i][1];
+      } else {
+        recommendedFriend = friends[i][0];
+      }
+    }
   }
 
   return answer;
