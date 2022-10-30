40267f28fe18cea50e3506ef7ebe47880c97d919
donghhan <apensia914@gmail.com>
2022-10-30 14:26:36 +0900

    Comment added

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,9 @@
+/*
+  ** Let's Think... **
+  Sample size < 10,000 => Should use algorithm with time complexity of O(n^2)?
+  Algorithm: Trie?
+*/
+
 function problem6(forms) {
   var answer;
   return answer;
