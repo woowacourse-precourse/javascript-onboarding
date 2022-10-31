30ada8d6592410c294c43537c2d3d7562820ff1c
Haram Baek <haram@Haramui-MacBookAir.local>
2022-10-31 23:19:11 +0900

    refactor: [PROBLEM4] object의 key들만 for문 도는 것이 아니라 entries 프로퍼티를 사용하여 key와 value 모두 추출

--- src/problem4.js
+++ src/problem4.js
@@ -1,49 +1,48 @@
 function problem4(word) {
   var answer;
   let frogDict = {
-    "A": "Z",
-    "B": "Y",
-    "C": "X",
-    "D": "W",
-    "E": "V",
-    "F": "U",
-    "G": "T",
-    "H": "S",
-    "I": "R",
-    "J": "Q",
-    "K": "P",
-    "L": "O",
-    "M": "N",
-    "N": "M",
-    "O": "L",
-    "P": "K",
-    "Q": "J",
-    "R": "I",
-    "S": "H",
-    "T": "G",
-    "U": "F",
-    "V": "E",
-    "W": "D",
-    "X": "C",
-    "Y": "B",
-    "Z": "A"
+    A: "Z",
+    B: "Y",
+    C: "X",
+    D: "W",
+    E: "V",
+    F: "U",
+    G: "T",
+    H: "S",
+    I: "R",
+    J: "Q",
+    K: "P",
+    L: "O",
+    M: "N",
+    N: "M",
+    O: "L",
+    P: "K",
+    Q: "J",
+    R: "I",
+    S: "H",
+    T: "G",
+    U: "F",
+    V: "E",
+    W: "D",
+    X: "C",
+    Y: "B",
+    Z: "A"
   }
 
   let i = 0;
   do {
-    for (let j = 0; j < Object.keys(frogDict).length; j++) {
-      let key = Object.keys(frogDict)[j];
+    for (const [key, value] of Object.entries(frogDict)) {
       if (word[i] === key) {
-        // console.log(word[i], '대문자');
-        console.log(key, 'key');
+        // 대문자
+        console.log(`${key}: ${value}`);
       } else if (word[i] === key.toLowerCase()) {
-        // console.log(word[i], '소문자'); 
-        console.log(key, 'key');
+        // 소문자
+        console.log(`${key}: ${value}`);
       }
     }
-
     i++;
   } while (i < word.length)
+
 
   return answer;
 }
