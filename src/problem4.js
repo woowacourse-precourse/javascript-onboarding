d9dfe8a24f570aaa885537dd9f04e65d4121d058
SoMin <thals0107@naver.com>
2022-10-31 11:10:44 +0900

    feat: 문제 4 - 띄어쓰기 처리

--- src/problem4.js
+++ src/problem4.js
@@ -3,14 +3,19 @@ function problem4(word) {
   let answer = [];
 
   for (let i = 0; i < string.length; i++) {
-    const ascii = string[i].charCodeAt(0);
-
-    if (ascii >= 65 && ascii <= 90) {
-      convert = String.fromCharCode(155 - ascii); //대문자 155
+    if (string[i] === " ") {
+      answer.push(string[i]);
+      continue;
     } else {
-      convert = String.fromCharCode(219 - ascii); //소문자 219
+      const ascii = string[i].charCodeAt(0);
+
+      if (ascii >= 65 && ascii <= 90) {
+        convert = String.fromCharCode(155 - ascii); //대문자 155
+      } else {
+        convert = String.fromCharCode(219 - ascii); //소문자 219
+      }
+      answer.push(convert);
     }
-    answer.push(convert);
   }
   return answer.join("");
 }
