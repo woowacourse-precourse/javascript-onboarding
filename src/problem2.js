756ff3e82d1a7cbe6d7aca117865b0d1bdd20317
d0422 <rlfehd2013@naver.com>
2022-10-31 15:53:27 +0900

    Docs:2번 기능설계 변경

--- src/problem2.js
+++ src/problem2.js
@@ -6,30 +6,30 @@ function problem2(cryptogram) {
   answer = decryption(cryptogram);
   return answer;
 }
-function decryption(cryptogram) {
-  let index = 0;
-  const stack = [];
-  while (index < cryptogram.length) {
-    if (!stack) stack.push(cryptogram[index]);
-    else {
-      if (stack[stack.length - 1] == cryptogram[index]) {
-        index = progressIndex(cryptogram, index, cryptogram[index]) - 1;
-        stack.pop();
-      } else {
-        stack.push(cryptogram[index]);
-      }
-    }
-    index++;
-  }
-  return getString(stack);
-}
-function progressIndex(cryptogram, index, targetChar) {
-  while (cryptogram[index] == targetChar) index++;
-  return index;
-}
-function getString(Array) {
-  return Array.join("");
-}
+// function decryption(cryptogram) {
+//   let index = 0;
+//   const stack = [];
+//   while (index < cryptogram.length) {
+//     if (!stack) stack.push(cryptogram[index]);
+//     else {
+//       if (stack[stack.length - 1] == cryptogram[index]) {
+//         index = progressIndex(cryptogram, index, cryptogram[index]) - 1;
+//         stack.pop();
+//       } else {
+//         stack.push(cryptogram[index]);
+//       }
+//     }
+//     index++;
+//   }
+//   return getString(stack);
+// }
+// function progressIndex(cryptogram, index, targetChar) {
+//   while (cryptogram[index] == targetChar) index++;
+//   return index;
+// }
+// function getString(Array) {
+//   return Array.join("");
+// }
 
 function checkValidation(cryptogram) {
   return checkLength(cryptogram) && checkLowercase(cryptogram);
