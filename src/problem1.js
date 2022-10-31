e44fe180328aff0a5088081dc53c42830e545c78
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 13:53:21 +0900

    feat : problem1
    함수의 기능을 유효성을 확인하는 check함수와
    최대값을 찾아주는 findMaxNumber로 나눠서 작성했습니다.
    
    예외사항의 경우 첫페이지나 마지막페이지를 펼쳤거나, 페이지 수가 연속되지
    않을 경우를 false로 설정했습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,39 @@
+const findMaxNumber = (arr) => {
+    let leftNumberArr = arr[0].toString().split('').map(Number);
+    let rightNumberArr = arr[1].toString().split('').map(Number);
+
+    const newArr = [
+        leftNumberArr.reduce((a, c) => a + c, 0),
+        leftNumberArr.reduce((a, c) => a * c, 1),
+        rightNumberArr.reduce((a, c) => a + c, 0),
+        rightNumberArr.reduce((a, c) => a * c, 1),
+    ];
+
+    return Math.max(...newArr);
+};
+const check = (arr) => {
+    const leftNumber = arr[0];
+    const rightNumber = arr[1];
+
+    if (leftNumber === 1 || rightNumber === 400) {
+        return false;
+    }
+
+    if (rightNumber - 1 === leftNumber) {
+        return true;
+    }
+
+    return false;
+};
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    if (!check(pobi) || !check(crong)) {
+        return -1;
+    }
+    const arr1Max = findMaxNumber(pobi);
+    const arr2Max = findMaxNumber(crong);
+
+    return arr1Max > arr2Max ? 1 : 0;
 }
 
 module.exports = problem1;
