5b6f1c1ede5089863083f5fa0d5e9646818f6d25
daal2quf <daal2quf@gmail.com>
2022-11-01 02:19:35 +0900

    불필요한 변수 제거

--- src/problem4.js
+++ src/problem4.js
@@ -1,27 +1,25 @@
 function problem4(word) {
   let answer;
+  let motherWords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let frogWords = [...motherWords].sort((a, b) => b.localeCompare(a))
+  let lowerMotehrWords = motherWords.map((e) => e.toLowerCase())
+  let lowerFrogWords = frogWords.map((e) => e.toLowerCase())
+  let frogSay = '';
+  
 
   const frogConvertor = (str) => {
     if (str === ' ') return str
 
     if (motherWords.includes(str)) {
       let idx = motherWords.indexOf(str)
-      let newWord = frogWords[idx]
-      return newWord
+      return frogWords[idx]
     } else if (lowerMotehrWords.includes(str)) {
       let idx = lowerMotehrWords.indexOf(str)
-      let newWord = lowerFrogWords[idx]
-      return newWord
+      return lowerFrogWords[idx]
+    } else if(str === ' ') {
+      return str
     }
-
   }
-
-  let motherWords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  let frogWords = [...motherWords].sort((a, b) => b.localeCompare(a))
-  let lowerMotehrWords = lowerCase(motherWords)
-  let lowerFrogWords = lowerCase(frogWords) 
-  let frogSay = '';
-  
   
   for (let i = 0; i < word.length; i++) {
     frogSay += frogConvertor(word[i])
