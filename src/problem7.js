6fbf972cfab027515203cf304ea845fa61f8c10a
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-01 07:20:55 +0900

    Feat: <problem7> 항목별 점수 부여 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,8 +1,24 @@
-function setFriendPoints(people) {
+function getTopFivePeople(people) {
+  let sortedPeople = [...people].sort((a, b) => (b[1] - a[1]));
+  let topFive = [];
 
+  for (let i = 0; i < 5; i++) {
+    if (sortedPeople[i][1] > 0)
+      topFive.push(sortedPeople[i][0]);
+  }
+  return topFive;
 }
 
-function setVisitorPoints(people, visitors) {
+function giveFriendPoints(people, friends) {
+  for (let i = 0; i < friends.length; i++) {
+    if (people.get(friends[i][0]) == -1 && people.get(friends[i][1]) >= 0)
+      people.set(friends[i][1], people.get(friends[i][1]) + 10);
+    if (people.get(friends[i][1]) == -1 && people.get(friends[i][0]) >= 0)
+      people.set(friends[i][0], people.get(friends[i][0]) + 10);
+  }
+}
+
+function giveVisitorPoints(people, visitors) {
   let newbie = 0;
 
   for (let i = 0; i < visitors.length; i++) {
@@ -13,30 +29,27 @@ function setVisitorPoints(people, visitors) {
   }
 }
 
-function makeUsersList(user, friends) {
+function makePeopleList(user, friends) {
   let people = new Map();
   let alreadyFriend = -1;
   let newbie = 0;
 
-  people.set(user, -2);
   for (let i = 0; i < friends.length; i++) {
-    if (friends[i][0] != user)
-      people.set(friends[i][0], newbie);
-    if (friends[i][1] != user)
-      people.set(friends[i][1], newbie);
+    people.set(friends[i][0], newbie);
+    people.set(friends[i][1], newbie);
     if (friends[i][0] == user)
-      people.set(friends[i][1], alreadyFriend);
-    if (friends[i][1] != user)
-      people.set(friends[i][0], alreadyFriend);
+    people.set(friends[i][1], alreadyFriend);
+    if (friends[i][1] == user)
+    people.set(friends[i][0], alreadyFriend);
   }
+  people.set(user, -2);
   return people;
 }
 
 function problem7(user, friends, visitors) {
-  let people = makePeopleList(friends, visitors);
-
-  setFriendPoints(people);
-  setVisitorPoints(people, visitors);
+  let people = makePeopleList(user, friends);
+  giveFriendPoints(people, friends);
+  giveVisitorPoints(people, visitors);
   let answer = getTopFivePeople(people);
   return answer;
 }
