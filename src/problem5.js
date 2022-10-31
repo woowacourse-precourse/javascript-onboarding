d50bf432a85e4743ffadeb340b2eac8bab6470e5
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 05:38:40 +0900

    FEAT : problem5 - 사용한 화폐 금액만큼 제외 및 결과 반환

--- src/problem5.js
+++ src/problem5.js
@@ -3,6 +3,13 @@ function problem5(money) {
   const MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   // 반환할 배열 생성
   let answer = [];
+
+  MONEY.forEach((price, index) => {
+    answer.push(Math.floor(money / price));
+    money -= price * answer[index];
+  })
+
+  return answer;
 }
 
 module.exports = problem5;
