00d4e44696dc7cad9cad05fc761661d0b796770b
parkjikuk <paxkk@naver.com>
2022-11-01 06:43:10 +0900

    feat : forms의 배열 요소를 구조 분해 할당

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
 function problem6(forms) {
+  const emailArr = new Set();
   
+  for (let i = 0; i < forms.length - 1; i++) {
+    const [email1, nickName1] = forms[i];
+    for(let j = i + 1; j < forms.length; j++) {
+      const [email2, nickName2] = forms[j];
+    }
+  }
+
+  return emailArr;
 }
 
 function duplicateNick(nickName1, nickName2) {
