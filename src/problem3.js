097306fdd6cd53ec4a4dcb82cfe42107dd095921
pitang <wpeffect@gmail.com>
2022-10-31 15:30:50 +0900

    3번 기능 요구사항.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  // 1. [3, 6, 9] 포함된 정규식 변수 생성.
+  let numberOfGame = /[3|6|9]/g;
+
+  for(let i = 1; i <= number; i++) {
+    // 2. 체크해서 match 되는 숫자 저장.
+    let numberOfMatch = (""+i).match(numberOfGame);
+    // 3. count 
+    if(numberOfMatch != undefined) answer += numberOfMatch.length;
+  }
+
   return answer;
 }
 
