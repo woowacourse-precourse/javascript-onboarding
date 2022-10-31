0cb93b4c3b0e532320ab9a8c0ad4537b4fd2e770
ming0830 <minsun0830@naver.com>
2022-11-01 01:24:16 +0900

    [PROBLEM5] feat : money를 가장 큰 금액의 화폐로 나눈 몫을 저장하고 나머지를 구하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
-  var answer;
-  return answer;
-}
+  const kindOfMoney = [50000,10000,5000,1000,500,100,50,10,1]
+  let result = [];
+  
+    for(let i =0;i<kindOfMoney.length;i++){
+      let quo = Math.floor(money/kindOfMoney[i]);
+      money = money % kindOfMoney[i]
+      result.push(quo)
+      
+    }
+
+
+  return result
+} 
+
 
 module.exports = problem5;
