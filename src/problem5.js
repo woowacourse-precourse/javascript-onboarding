cba97b1e25fe27bec21a345ed83cd77e1d15fc27
HAEROOL <gofn080776@gmail.com>
2022-10-31 21:48:59 +0900

    feat: [problem5] - 주어진 수를 지정된 수들로 나누는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
+  var answer = withdrawMoney(money)
   return answer;
 }
 
 module.exports = problem5;
+
+
+function withdrawMoney(money){
+  let leftMoney = money;
+  let withdrawList = [];
+  const CURRENCY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  CURRENCY.forEach(curr => {
+    let devidedMoney = parseInt(leftMoney / curr);
+    withdrawList.push(devidedMoney)
+    leftMoney  = leftMoney % curr
+  })
+  return withdrawList
+}
