af27730c1149a3683bb10902f100c1a7d7729b2b
KUD <kimud6003@gmail.com>
2022-10-31 13:21:43 +0900

    ✨ Feature: problem3.js gameMatcher - problem3.js : 문자열에 369가 있을시 체크해주는 함수 gameMatcher

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
+  let answer = gameMatcher(number);
   return answer;
+}
+
+function gameMatcher(number){
+  let result = 0;
+  const condition = /[3|6|9]/;
+  for (let i =1; i<=number; i ++)
+    result+= [...i.toString().matchAll(condition)].length;
+  return result;
 }
 
 module.exports = problem3;
