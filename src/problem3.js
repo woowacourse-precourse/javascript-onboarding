aa09355aeb2a2ec58151f0f75895078332d553cf
sohyun <park.sohyun0430@gmail.com>
2022-10-31 16:57:18 +0900

    cal_clap 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -3,9 +3,22 @@ function number_to_str(number){
   return number_arr
 }
 
+function cal_clap(answer,check_num,number_arr){
+  for(let j=0;j<3;j++){
+    number_arr.forEach((ele)=>{ele==check_num[j] ? answer+=1 : answer});
+  }
+  return answer;
+}
+
 function problem3(number) {
   let answer = 0;
-  
+  const check_num = [3,6,9]
+  for(let num=1;num<=number;num++)
+  {
+    let number_arr = number_to_str(num)
+    // 각 숫자에 3,6,9 몇개인지 확인
+    answer = cal_clap(answer,check_num,number_arr);
+  }
   return answer;
 }
 
