cbabd52dce88d3583b442a5155d71d740b6cbbee
pakxe <pigkill40@naver.com>
2022-11-01 01:48:47 +0900

    fix: [P7] 6번 테스트케이스를 통과하도록 수정

--- src/problem7.js
+++ src/problem7.js
@@ -1,61 +1,18 @@
 const LENGTH_RECOMMEND_FRIEND = 5;
 
 function problem7(user, friends, visitors) {
-  let relationGraph = new Map();
-  friends.forEach((pair) => makeGraph(relationGraph, pair));
-
-  let scoreList = new Map();
-  friendOfFriend(user, relationGraph, scoreList);
-  visitors.forEach((visitor) =>
-    scoreVisitor(user, relationGraph, visitor, scoreList)
-  );
+  const relationList = makeFriendsList(friends);
+  const usersFriends = getUsersFriends(user, relationList);
+  const friendOfFriendList = getFriendOfFriend({
+    user,
+    relationList,
+    usersFriends,
+  });
+  const scoreList = scoreFriends(friendOfFriendList);
+  scoreVisitors(scoreList, visitors, usersFriends);
 
   return recommendFriends(scoreList, LENGTH_RECOMMEND_FRIEND);
 }
-
-// 주어진 친구쌍을 그래프로 만드는 함수
-function makeGraph(graph, pair) {
-  [leftName, rightName] = pair;
-  let tempSet = new Set();
-  let value;
-
-  if (graph.has(leftName)) {
-    value = graph.get(leftName);
-    graph.set(leftName, value.add(rightName));
-  } else {
-    graph.set(leftName, tempSet.add(rightName));
-  }
-  if (graph.has(rightName)) {
-    value = graph.get(rightName);
-    graph.set(rightName, value.add(leftName));
-  } else {
-    tempSet.clear();
-    graph.set(rightName, tempSet.add(leftName));
-  }
-}
-
-// 친구의 친구에게 10점씩 주는 함수
-function friendOfFriend(user, graph, scoreList) {
-  let userFriends = graph.get(user);
-  userFriends.forEach((friend) => {
-    let fof = graph.get(friend);
-    fof.forEach((name) => {
-      // user 자신, user와 겹친구는 제외
-      if (name !== user && !graph.get(user).has(name)) scoreList.set(name, 10);
-    });
-  });
-}
-
-// 방문자의 점수를 계산하는 함수
-function scoreVisitor(user, graph, visitor, scoreList) {
-  if (scoreList.has(visitor)) {
-    let score = scoreList.get(visitor);
-    scoreList.set(visitor, score + 1);
-  } else if (!scoreList.has(visitor) && !graph.get(user).has(visitor)) {
-    scoreList.set(visitor, 1);
-  }
-}
-
 // 추천 친구의 이름만 반환하는 함수
 function recommendFriends(scoreList, length) {
   let ArrScoreList = [...scoreList]; // 배열로 만들기
@@ -65,6 +22,10 @@ function recommendFriends(scoreList, length) {
   return NameList.splice(0, length);
 }
 
+function getUsersFriends(user, relationList) {
+  if (!relationList.has(user)) return [];
+  else return [...relationList.get(user)];
+}
 // 주어진 조건대로 정렬하는 함수
 function ascendingSort(a, b) {
   [name1, score1] = a;
@@ -76,5 +37,76 @@ function ascendingSort(a, b) {
     if (name1 <= name2) return -1;
   }
 }
+
+function scoreVisitors(scoreList, visitors, usersFriends) {
+  visitors.forEach((visitor) => {
+    if (scoreList.has(visitor)) {
+      const score = scoreList.get(visitor);
+      scoreList.set(visitor, score + 1);
+    } else if (!usersFriends.includes(visitor)) {
+      scoreList.set(visitor, 1);
+    }
+  });
+}
+
+function scoreFriends(friendOfFriendList) {
+  const scoreMap = new Map();
+  friendOfFriendList.forEach((fof) => {
+    if (scoreMap.has(fof)) {
+      const score = scoreMap.get(fof);
+      scoreMap.set(fof, score + 10);
+    } else {
+      scoreMap.set(fof, 10);
+    }
+  });
+
+  return scoreMap;
+}
+
+function makeFriendsList(friends) {
+  const friendsList = new Map();
+
+  friends.forEach(([leftName, rightName]) => {
+    if (friendsList.has(leftName))
+      friendsList.set(leftName, friendsList.get(leftName).add(rightName));
+    else friendsList.set(leftName, new Set([rightName]));
+
+    if (friendsList.has(rightName))
+      friendsList.set(rightName, friendsList.get(rightName).add(leftName));
+    else friendsList.set(rightName, new Set([leftName]));
+  });
+
+  return friendsList;
+}
+
+// 친구의 친구가 있는 경우 배열을 반환, 없는 경우 빈 배열을 반환
+function getFriendOfFriend({ user, relationList, usersFriends }) {
+  const friendsOfFriends = [];
+
+  for (let i = 0; i < usersFriends.length; i++) {
+    const friendsSet = relationList.get(usersFriends[i]);
+
+    friendsSet.forEach((friendOfFriend) => {
+      if (friendOfFriend !== user && !usersFriends.includes(friendOfFriend)) {
+        friendsOfFriends.push(friendOfFriend);
+      }
+    });
+  }
+
+  return friendsOfFriends;
+}
+
+// let user = "mrko";
+// let friends = [
+//   ["donut", "andole"],
+//   ["donut", "jun"],
+//   ["donut", "mrko"],
+//   ["shakevan", "andole"],
+//   ["shakevan", "jun"],
+//   ["shakevan", "mrko"],
+// ];
+// let visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
+
+// console.log(problem7(user, friends, visitors));
 
 module.exports = problem7;
