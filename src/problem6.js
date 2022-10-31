6247c5fd757847b95f624fa6e58d912216fa7394
heyheyley <jeonheyley@gmail.com>
2022-11-01 01:24:50 +0900

    문제6:닉네임 배열생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let nicknameArr = [];
+
+  //for문을 돌면서 닉네임 2글자씩 넣은(1부터 시작하여 그 앞문자열까지 문자열로 만든)배열 생성
+  forms.map(([email, nickname]) => {
+    for (let i = 1; i < nickname.length; i++) {
+      nicknameArr.push(nickname[i - 1] + nickname[i]);
+    }
+  });
+  
   return answer;
 }
 
