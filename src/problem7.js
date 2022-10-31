82012a063c31cf57cb3c681b12f42e162b529d78
dladncks1217 <dlaxodud1217@gmail.com>
2022-10-31 13:41:09 +0900

    refac: problem7 getSortedResult기능 분리

--- src/problem7.js
+++ src/problem7.js
@@ -4,13 +4,14 @@ function makeFriendsList(mainUser, friends, visitors) {
   const hash = {};
   // 친구 목록들 정리.
   for (const [A, B] of friends) {
+    // 특청 친구 이름 아직 안들어갔으면
     if (hash[A] === undefined) {
       userList.push(A);
       hash[A] = [B];
     } else {
       hash[A].push(B);
     }
-
+    // 특청 친구 이름 아직 안들어갔으면
     if (hash[B] === undefined) {
       userList.push(B);
       hash[B] = [A];
@@ -29,6 +30,28 @@ function makeFriendsList(mainUser, friends, visitors) {
   return [hash, userList];
 }
 
+// 점수가 동일한 추천친구가 있을 경우 이름순 정렬
+function getSortedResult(score, userList) {
+  const result = [];
+  let maxScore = Math.max(...score); // 최대 점수
+  // 결과 추려내기
+  while (maxScore > 0) {
+    const temp = []; // 같은 점수 시 사전순으로 정리할 배열
+
+    let index = score.indexOf(maxScore);
+    while (index > -1) {
+      temp.push(userList[index]);
+      score[index] = 0;
+      index = score.indexOf(maxScore);
+    }
+    temp.sort(); // 사전순 정렬
+    result.push(...temp); // answer에 결과 삽입
+    maxScore = Math.max(...score); // 최대 점수
+  }
+  return result;
+}
+
+// 문제 풀이
 function problem7(user, friends, visitors) {
   let answer = [];
   const [friendList, userList] = makeFriendsList(user, friends, visitors); // 친구 목록들 담은 해시, 점수기록할 배열이랑 같이 쓸 유저이름배열
@@ -53,23 +76,10 @@ function problem7(user, friends, visitors) {
     }
   });
 
-  let maxScore = Math.max(...score); // 최대 점수
-  // 결과 추려내기
-  while (maxScore > 0) {
-    const temp = []; // 같은 점수 시 사전순으로 정리할 배열
+  // 점수가 같은 사람들이 있다면 이름순 정렬 결과 도출
+  answer = getSortedResult(score, userList);
 
-    let index = score.indexOf(maxScore);
-    while (index > -1) {
-      temp.push(userList[index]);
-      score[index] = 0;
-      index = score.indexOf(maxScore);
-    }
-    temp.sort(); // 사전순 정렬
-    answer.push(...temp); // answer에 결과 삽입
-    maxScore = Math.max(...score); // 최대 점수
-  }
-
-  // 결과가 5개이상 들어가있을경우 (중복 결과가 많을 경우)
+  // 결과가 5개이상 들어가있을경우 결과 5개로 추리기(중복 결과가 많아 결과가 5개 이상인 경우)
   if (answer.length > 5) {
     for (let i = answer.length; i > 5; i--) {
       answer.pop();
