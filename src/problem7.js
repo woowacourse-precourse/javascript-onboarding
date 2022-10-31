17bbdbb90de8d6d696a46002ecde800aea5be585
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 22:18:28 +0900

    [Feat:Problem7] 배열 내 해당 값 존재 여부 확인하기

--- src/problem7.js
+++ src/problem7.js
@@ -2,3 +2,5 @@ const compose =
   (...fns) =>
     (initialValue) =>
       fns.reduce((composed, fn) => fn(composed), initialValue);
+
+const isNotExisting = (array, item) => array.indexOf(item) === -1;
