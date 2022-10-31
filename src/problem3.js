d1d3fbd14f323f24d2f92fdb4313527376a1a622
Suyeon <msjgr2801@gmail.com>
2022-10-31 17:05:43 +0900

    Fix: add For loop
    서브 기능 구현만 하고 메인 루프를 돌지 않은 문제 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,26 @@
 /* 
   메인 로직
-  다음 과정을 number가 0이 될 때까지 반복
-  1. [x] number를 10으로 나눈 나머지가 3,6,9 중 하나면 박수 + 1
-  2. [x] number <- number / 10
+  1.[x] 1부터 n까지 숫자 순회
+  2. 3,6,9 판별
+    2.1. [x] number를 10으로 나눈 나머지가 3,6,9 중 하나면 박수 + 1
+    2.2. [x] number <- number / 10
 */
+
+/**
+ * 
+ */
+const check369 = (number) => {
+  let clap = 0;
+
+  while (number) {
+    if (number % 10 === 3 || number % 10 === 6 || number % 10 === 9) {
+      clap++;
+    }
+    number = parseInt(number / 10);
+  }
+
+  return clap;
+}
 
 /**
  * Main function - count 3,6,9 claps
@@ -13,11 +30,8 @@
 function problem3(number) {
   let clap = 0;
 
-  while (number) {
-    if (number % 10 === 3 || number % 10 === 6 || number % 10 === 9) {
-      clap++;
-    }
-    number = parseInt(number / 10);
+  for (let i = 1; i <= number; i++) {
+    clap += check369(number);
   }
 
   return clap;
