c87017ca63b9426367e046c0ca98d1b031bb2c31
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-01 02:00:15 +0900

    ✨ 친구추천 리스트를 만들어 친구의 친구에게 10점을 더해줌

--- src/problem7.js
+++ src/problem7.js
@@ -12,6 +12,20 @@ function problem7(user, friends, visitors) {
   // 문제발생) 유저도 결국 그들의 친구여서 중복되는 현상 발생
   // 문제해결 1) 다음단계로 가기 전에 _지금단계에서_ 유저를 배열에서 지우는거 [o]
   relation = relation.filter((nickname) => nickname !== user);
+
+  // 3) 친구추천리스트를 객체롤 만듭니다. 점수를 저장하기 위해서
+  // 닉네임 : 점수
+  const recommendList = {};
+
+  // 4) 친구의 친구에겐 10점을 더해줍니다.
+  for (const may_know of relation) {
+    if (recommendList[may_know] === undefined) {
+      recommendList[may_know] = 10;
+    } else if (recommendList[may_know]) {
+      recommendList[may_know] += 10;
+    }
+  }
+
   return answer;
 }
 
