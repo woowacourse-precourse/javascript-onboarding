2643e8cb5b38aa9e34cf33a4efe7c954e4058ba8
cruelladevil <dev.timetravel@gmail.com>
2022-10-31 18:55:09 +0900

    feat(PROBLEM7): make friends graph

--- src/problem7.js
+++ src/problem7.js
@@ -2,8 +2,20 @@ const FRIEND_POINT = 10;
 const VISITOR_POINT = 1;
 
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendsGraph = makeFriendsGraph(friends);
+}
+
+function makeFriendsGraph(friends) {
+  const graph = {};
+
+  friends.forEach(([a, b]) => {
+    if (!graph[a]) graph[a] = [];
+    graph[a].push(b);
+    if (!graph[b]) graph[b] = [];
+    graph[b].push(a);
+  });
+
+  return graph;
 }
 
 module.exports = problem7;
