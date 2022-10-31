68bd6466f8cf8d90f93c60029b1b7c34a91d41d1
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-01 00:32:59 +0900

    refactor: problem4 불필요한 변수 삭제, 조건문 간결화
    isBig(letter) => 대소문자 판별해주는 함수
    조건문 가독성을 위해 함수로 따로 생성함

--- src/problem4.js
+++ src/problem4.js
@@ -1,25 +1,19 @@
-//1. A-Z, a-z 아스키코드값 활용해서 뒤집기
-//2. 대문자 <-> 소문자 바꾸기
+function isBig(letter){
+    return letter===letter.toUpperCase();
+}
+
 function problem4(word){
-  var answer;
-  const [code_A, code_Z, code_a, code_z] =
-        ['A'.charCodeAt(0), 'Z'.charCodeAt(0),'a'.charCodeAt(0), 'z'.charCodeAt(0)];
-  const [high, low] = [code_A+code_Z, code_a+code_z];
-  word = [...word].map((i)=>{
-      if(/[a-zA-Z]/.test(i)){
-          if(i==i.toUpperCase()){
-              return String.fromCharCode(high-i.charCodeAt(0));
-          }
-          else{
-              return String.fromCharCode(low-i.charCodeAt(0));
-          }
-      }
-      else{
-          return i;
-      }
-  })
-  answer = word.join('');
-  return answer;
+    const [BIG_RANGE, SMALL_RANGE] =
+          ['A'.charCodeAt(0)+'Z'.charCodeAt(0), 'a'.charCodeAt(0)+'z'.charCodeAt(0)];
+          
+    word = [...word].map((letter)=>{
+        if(!/[a-zA-Z]/.test(letter)) { return letter; }
+        letter = isBig(letter)
+        ? String.fromCharCode(BIG_RANGE-letter.charCodeAt(0))
+        : String.fromCharCode(SMALL_RANGE-letter.charCodeAt(0));
+        return letter;
+    })
+    return word.join('');
 }
 
 module.exports = problem4;
