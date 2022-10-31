368bf588d35d3b3926a0ecdb8448f1fa84143d29
YeoInsu <insu12021202@gmail.com>
2022-10-31 17:17:30 +0900

    Feat:problem6 구현 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  var answer;
-  return answer;
-}
+  let arr = new Array();
+  let resultEmail = new Array();
+  let duplicateWord = "";
 
+  forms.map(([email, nickname]) => {
+    for (let i = 1; i < nickname.length; i++) {
+      arr.push(nickname[i - 1] + nickname[i]);
+    }
+  });
+  arr.map((v, i) => {
+    if (arr.indexOf(v) !== i) {
+      duplicateWord = v;
+    }
+  });
+  forms.map(([email, nickname]) => {
+    if (nickname.includes(duplicateWord)) {
+      resultEmail.push(email);
+    }
+  });
+  return resultEmail.sort();
+}
 module.exports = problem6;
