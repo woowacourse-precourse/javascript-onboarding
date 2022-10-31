dfb3767fd021722dc3fe2c73c27b55e6a36db1e0
김상두 <puki4416@gmail.com>
2022-10-31 23:35:44 +0900

    problem3 : problem3 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,20 @@
 // 1부터 입력숫자까지 수들중 3,6,9를 찾는 problem3 함수 구현
 
 function problem3(number) {
-  var answer;
-  return answer;
+  let result = 0;
+  for (let i = 2; i <= number; i++) {
+    result += String(i)
+      .split("")
+      .map(Number)
+      .reduce((prev, cur) => {
+        if (cur !== 0 && cur % 3 === 0) {
+          return prev + 1;
+        } else {
+          return prev;
+        }
+      }, 0);
+  }
+  return result;
 }
 
 module.exports = problem3;
