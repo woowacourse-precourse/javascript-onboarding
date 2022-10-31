113e4b1cd2aee010fad8f52f8efc336e0cf932e7
Monte <monte6198@gmail.com>
2022-10-31 20:56:20 +0900

    feat(problem6.js): firstSlice() 및 첫 중복 문자열 allSlices에 반영 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,9 @@
+function firstSlice(nameSlice, allSlices) {
+  if (!allSlices.has(nameSlice))
+    //이전에 없던 첫 중복 문자열이라면
+    allSlices.set(nameSlice, 0); //중복 문자열 목록(allSlices)에 새로 추가한다.
+}
+
 function problem6(forms) {
   var answer;
   let result = new Set(); //같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록
