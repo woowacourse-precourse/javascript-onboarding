1e8a3daa89bb1b5784216a701e98e9e489dec908
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-01 00:02:50 +0900

    feat:[문제4] 알파벳 거꾸로 구현

--- src/problem4.js
+++ src/problem4.js
@@ -5,7 +5,17 @@ function problem4(word) {
 
   for(let i = 65; i<=90; i++){ upper.push(String.fromCharCode(i)); }
   for(let i = 97; i<=122; i++){ lower.push(String.fromCharCode(i)); }
-
+  
+  for(let i = 0; i<word.length; i++){
+    if(word[i] === ' ') { answer.push(' '); }
+    if(lower.includes(word[i])){
+      answer.push(lower[lower.length - lower.indexOf(word[i]) - 1]);
+    }
+    if(upper.includes(word[i])){
+      answer.push(upper[upper.length - upper.indexOf(word[i]) - 1]);
+    }
+  }
+  answer = answer.join('');
   return answer;
 }
 
