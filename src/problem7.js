8dc65c1336736072412cf919ff1bb06357b1e156
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 22:12:28 +0900

    '친구의 친구' 추천 점수 매기기

--- src/problem7.js
+++ src/problem7.js
@@ -3,8 +3,7 @@
 
   1. user와 친구인 사람을 찾는다.
   1-1. friend에서 '친구'는 추천받지 않음으로, friends에서 user의 친구 관계를 삭제한다.
-  2. user와 친구인 사람인 '친구의 친구
-  를 찾는다. 
+  2. user와 친구인 사람인 '친구의 친구'를 찾는다. 
   3. '친구의 친구' 사람(key)을 Map에 넣고 value = 10으로 설정한다.
   4. visitors를 참고해 방문객을 Map에 넣고, value+=1 점씩 매긴다.
   5. 점수순(내림차순), 이름순(오름차순)으로 정렬한다.  
@@ -24,6 +23,16 @@ function problem7(user, friends, visitors) {
       friends.splice(i, 1);
     }
   }
+
+  friends.forEach((them) => {
+    myFriend.forEach((one) => {
+      them.indexOf(one) == 0
+        ? answer.set(them[1], 10)
+        : them.indexOf(one) == 1
+        ? answer.set(them[0], 10)
+        : "";
+    });
+  });
 }
 
 module.exports = problem7;
