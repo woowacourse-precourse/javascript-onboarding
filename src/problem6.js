9a2b70e541ac0f59d0e40a2b847545cf0856c0b0
oxhwixo <hwi_030@naver.com>
2022-10-31 22:10:49 +0900

    Problem6: 같은 글자가 연속적으로 포함되는 닉네임을 찾는 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,31 @@
+function findOverlap(forms) {
+  var names = [];
+  var overlapNames = [];
+  var nowName;
+  var temp;
+
+  forms.forEach((element) => {
+    names = names.concat(element[1]);
+  });
+
+  for (let i = 0; i < names.length; i++) {
+    nowName = String(names.splice(i, 1));
+    for (let j = 0; j < nowName.length - 1; j++) {
+      temp = nowName[j] + nowName[j + 1];
+      names.forEach((element) => {
+        if (element.includes(temp) === true) overlapNames.push(element);
+      });
+    }
+    names.splice(i, 0, String(nowName));
+  }
+
+  overlapNames = [...new Set(overlapNames)];
+  console.log(overlapNames);
+}
+
 function problem6(forms) {
   var answer;
+  findOverlap(forms);
   return answer;
 }
 
