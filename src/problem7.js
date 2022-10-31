b431ea8f3b853abb764828feb8af33bc9e4144ad
ming0830 <minsun0830@naver.com>
2022-11-01 08:53:52 +0900

    [PROBLEM7] feat : user의 친구 목록 구현

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,26 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+//user의 친구 목록
+function getUserFriends(user,friends){
+  let userFriendSet = [];
+  for(let friendSet of friends){
+    if(friendSet.includes(user)){
+      userFriendSet.push(friendSet);
+    }
+  } 
+  // 중복 제거 및 user 제외 목록 구현
+  const userFriend = [...new Set(userFriendSet.flat())];
+  let userFriendList = userFriend.filter((ele) => ele !== user);
+
+
+  return userFriendList
+
+}
+
+const user = "mrko";
+const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]  ;
+const visitors =  ["bedi", "bedi", "donut", "bedi", "shakevan"];
+getUserFriends(user,friends)
 module.exports = problem7;
+
