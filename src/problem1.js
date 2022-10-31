cbd0a2d102be9330adad4d963d648f9996ac48e4
dhkham <khamdong@gmail.com>
2022-10-31 15:38:00 +0900

    feat: (prob_1) get scores for both and compare to return the result

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,26 @@
 function problem1(pobi, crong) {
   var answer;
+  pobiLeft = pobi[0];
+  pobiRight = pobi[1];
+  crongLeft = crong[0];
+  crongRight = crong[1];
+
+  pobiMaxSum = largerNumber(digitSum(pobiLeft), digitSum(pobiRight));
+  pobiMaxMultiply = largerNumber(digitMultiply(pobiLeft), digitMultiply(pobiRight));
+  pobiMax = largerNumber(pobiMaxSum, pobiMaxMultiply); // max score for pobi
+  crongMaxSum = largerNumber(digitSum(crong[0]), digitSum(crong[1]));
+  crongMaxMultiply = largerNumber(digitMultiply(crong[0]), digitMultiply(crong[1]));
+  crongMax = largerNumber(crongMaxSum, crongMaxMultiply); // max score for crong
+
+  if (pobiRight - pobiLeft !== 1 || crongRight - crongLeft !== 1) {
+      answer = -1; // wrong page number: return -1
+  } else if (pobiMax > crongMax) {
+      answer = 1; // pobi wins: return 1
+  } else if (pobiMax < crongMax) {
+      answer = 2; // crong wins: return 2
+  } else if (pobiMax === crongMax) {
+      answer = 0; // draw: return 0
+  }
   return answer;
 }
 
