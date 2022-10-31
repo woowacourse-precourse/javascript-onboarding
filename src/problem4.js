8295050f4b70008d211d834d34ab230fec534df4
ryuna <anottrx@gmail.com>
2022-10-31 11:07:20 +0900

    comment(4번): JSDoc 추가

--- src/problem4.js
+++ src/problem4.js
@@ -5,6 +5,11 @@ function problem4(word) {
   return answer;
 }
 
+/**
+ * 알파벳 소문자와 대문자만 뒤집어서 리턴한다
+ * @param {string} alphabet - 알파벳 또는 다른 문자 하나
+ * @returns {string} 규칙에 맞게 뒤집거나 그대로인 문자
+ */
 const changeAlphabet = (alphabet) => {
   const asciiCode = alphabet.charCodeAt(0);
   if (asciiCode >= 97 && asciiCode <= 122)
