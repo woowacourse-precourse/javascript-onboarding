c6e2429f742ddf97e71de674f53f8015a50f37ec
Junghwan Kim <junghwki@c2r5s6.42seoul.kr>
2022-10-31 16:57:49 +0900

    feat: [problem6] 중복 검사 및 제거 함수 추가
    Map을 이용해 닉네임을 잘라서 저장후
    중복 처리후 array에 추가
    반환 하기 전 중복값 제거 및 정렬 후 반환

--- src/problem6.js
+++ src/problem6.js
@@ -5,6 +5,41 @@
 // 4. 중복 제거 함수 구현
 // 5. email 정렬후 result 반환
 
+function setOverlapArray(keyValue, userEmail, table, unsortedArray) {
+  const alreadyExist = table.get(keyValue);
+
+  if (alreadyExist) {
+    unsortedArray.push(alreadyExist);
+    unsortedArray.push(userEmail);
+    return;
+  }
+  table.set(keyValue, userEmail);
+}
+
+function overlapCheck(userData, table, unsortedArray) {
+  let keyValue;
+  const userEmail = userData[0];
+  const userNickname = userData[1];
+
+  for (let index = 0; index < userNickname.length - 1; index++) {
+    keyValue = userNickname[index] + userNickname[index + 1];
+    setOverlapArray(keyValue, userEmail, table, unsortedArray);
+  }
+}
+
+function getResult(forms) {
+  let nicknameTable = new Map();
+  let unsortedArray = [];
+
+  for (let index = 0; index < forms.length; index++) {
+    overlapCheck(forms[index], nicknameTable, unsortedArray);
+  }
+  const ret = unsortedArray.filter(
+    (element, index) => unsortedArray.indexOf(element) === index
+  );
+  return ret.sort();
+}
+
 function validInputCheck(forms) {
   if (forms.length < 1 || forms.length > 10000) {
     return -1;
@@ -37,16 +72,16 @@ function checkNickname(nickname) {
 }
 
 function checkEmail(email) {
-  const emailDomainRegex = /^\w{1,10}@email.com$/;
+  const emailRegex = /^\w{1,10}@email.com$/;
 
-  return emailDomainRegex.test(email);
+  return emailRegex.test(email);
 }
 
 function problem6(forms) {
   if (validInputCheck(forms) < 0) {
     return "Argument Error";
   }
-  var answer;
+  var answer = getResult(forms);
   return answer;
 }
 
