036d1cc13dcf39576d1f25bbfac6bc35f4dd6180
KPGY <yuchw8@korea.ac.kr>
2022-10-31 22:48:41 +0900

    정규식을 이용한 포함값 계산

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var answer =0;
+for(i=0; i<=number; i++)
+{
+  answer += (String(i).match(/[3|6|9]/g)||[]).length;
+}
+return answer;
 }
 
 module.exports = problem3;
--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,20 @@ function problem4(word) {
   return answer;
 }
 
+let word ="I LOVE YOU"
+let alpa ="ABCDEFGHIJKLMNOPQRSATUVWXYZ";
+let alpaArr= Array.from(alpa);
+let beta = Array.from(alpa).reverse();
+let wordArr = Array.from(word);
+let ans =[];
+let answer =[];
+for(i=0; i<=wordArr.length; i++)
+{
+  ans.push(alpaArr.indexOf(wordArr[i]))
+}
+console.log(ans)
+for(i=0; i<=wordArr.length; i++)
+{
+  answer.push(alpaArr.find(alpaArr.indexOf ===3))
+}
 module.exports = problem4;
