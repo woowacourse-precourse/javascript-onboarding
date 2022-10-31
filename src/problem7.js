8006e51009bd8e63d9364f8658c6dc1127776ead
Siby1lA <psb4644@gmail.com>
2022-10-31 17:11:05 +0900

    feat: [문제 7] 함께 아는친구 10점, 방문 횟수 1점을 주는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -7,12 +7,19 @@ function getNewMembers(followFriends, list) {
   });
   return result;
 }
+const recommendScore = (list, score) => {
+  let result = {};
+  list.map((member) => (result[member] = (result[member] || 0) + score));
+  return result;
+};
 function problem7(user, friends, visitors) {
   const followFriends = [
     ...new Set(friends.filter((list) => list.includes(user)).flat()),
   ];
   const newFriends = getNewMembers(followFriends, friends.flat());
   const newVisitors = getNewMembers(followFriends, visitors);
+  const friendScore = recommendScore(newFriends, 10);
+  const visitorScore = recommendScore(newVisitors, 1);
 }
 
 module.exports = problem7;
