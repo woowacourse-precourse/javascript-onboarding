beb1c9813cf962b5f157b5c28da4749231dbe864
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:20:31 +0900

    [ feat ] problem4_알파벳 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
+// 기능 구현 목록
+// - 알파벳 배열 생성
+// - 거꾸로 된 알파벳 배열 생성
+// - 알파벳&거꾸로 된 알파벳 객체 생성
+// - 청개구리 사전에 따른 문자열
+
 function problem4(word) {
-  var answer;
+  let originAlphabet = [];
+
+  for (let i = 97; i <= 122; i++) {
+    originAlphabet.push(String.fromCharCode(i));
+  }
+
   return answer;
 }
 
