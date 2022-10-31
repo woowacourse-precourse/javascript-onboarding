5a877fce28a3592426fdbe158d03b29297657b27
spsp8426 <spsp8426@naver.com>
2022-10-31 14:26:10 +0900

    주석 log 제거

--- src/problem1.js
+++ src/problem1.js
@@ -11,7 +11,7 @@ function problem1(pobi, crong) {
   if (pobi[0] % 2 === 0) return -1;
   if (pobi[0] === 1) return -1;
   if (pobi[1] === 400) return -1;
-  // pobi 더하기,곱하기
+
   const pobisumleft = pobi[0]
     .toString()
     .split("")
@@ -29,14 +29,10 @@ function problem1(pobi, crong) {
     .split("")
     .reduce((acc, cur) => (acc *= Number(cur)), 1);
   const pobisumMax = Math.max(pobisumleft, pobisumright);
-  console.log("pobi더하기=", pobisumleft, pobisumright);
-  console.log("제일 큰숫자 = ", pobisumMax);
   const pobimulMax = Math.max(pobimulleft, pobimulright);
-  console.log("pobi곱하기=", pobimulleft, pobimulright);
-  console.log("제일 큰 숫자 = ", pobimulMax);
+
   let pobiMax = Math.max(pobisumMax, pobimulMax);
-  console.log("둘중 더큰 숫자 = ", pobiMax);
-  // crong 더하기 곱하기
+
   const crongsumleft = crong[0]
     .toString()
     .split("")
@@ -45,9 +41,9 @@ function problem1(pobi, crong) {
     .toString()
     .split("")
     .reduce((acc, cur) => (acc += Number(cur)), 0);
-  console.log("crong더하기=", crongsumleft, crongsumright);
+
   const crongsumMax = Math.max(crongsumleft, crongsumright);
-  console.log("제일 큰 숫자=", crongsumMax);
+
   const crongmulleft = crong[0]
     .toString()
     .split("")
@@ -56,11 +52,11 @@ function problem1(pobi, crong) {
     .toString()
     .split("")
     .reduce((acc, cur) => (acc *= Number(cur)), 1);
-  console.log("crong곱하기 = ", crongmulleft, crongmulright);
+
   const crongmulMax = Math.max(crongmulleft, crongmulright);
-  console.log("제일 큰 숫자 = ", crongmulMax);
+
   let crongMax = Math.max(crongsumMax, crongmulMax);
-  console.log("둘중 더큰 숫자 =", crongMax);
+
   if (pobiMax > crongMax) {
     answer = 1;
   } else if (pobiMax < crongMax) {
