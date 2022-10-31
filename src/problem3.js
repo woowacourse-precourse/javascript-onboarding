aea50607f730fbed54a6159d5d9c4cc8fb21271f
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:11:43 +0900

    [ feat ] problem3_number를 string배열로 변환하는 함숫 생성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
+//기능 구현 목록
+// - number를 string배열로 변환하는 함수 생성
+// - 3,6,9 포함 여부를 판단하는 함수 생성
+// - 손뼉 회수 count
+
 function problem3(number) {
-  var answer;
+  function createStringArr(number) {
+    const changedNum = String(number);
+    const resultArr = changedNum.split("");
+    return resultArr;
+  }
+
   return answer;
 }
 
