2cd621a4ba74e621aeeb4b9b602deb946dff7d99
chiman <rlaclghks123@naver.com>
2022-10-31 19:30:54 +0900

    feat: problem6- getUserName 함수 생성
    username을 가져오는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,13 @@
+function getUserName(userInfo) {
+  const userNameArr = [];
+
+  userInfo.map((user) => userNameArr.push(user[1]));
+
+  return userNameArr;
+}
 function problem6(forms) {
   var answer;
+  const userNameArr = getUserName(forms);
   return answer;
 }
 
