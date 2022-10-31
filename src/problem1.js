834101fecda5778b3b0cf1a5dcc1d8aee051f2f6
gsong <rmsdlf1206@gmail.com>
2022-10-31 19:10:35 +0900

    docs: README.md 과제별 기능 목록 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,25 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
   return answer;
 }
 
 module.exports = problem1;
+
+function checkBigger(num) {
+  let num_sum = num
+    .toString()
+    .split("")
+    .map((x) => parseInt(x))
+    .reduce((sum, curr) => sum + curr, 0);
+  let num_times = num
+    .toString()
+    .split("")
+    .map((x) => parseInt(x))
+    .reduce((sum, curr) => sum * curr, 1);
+
+  if (num_sum > num_times) {
+    return num_sum;
+  } else {
+    return num_times;
+  }
+}
