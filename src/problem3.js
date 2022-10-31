25b5310f2b923732fbf2495d2734cdc5779703bc
Minhye Kang <alsgp9898@naver.com>
2022-10-31 20:57:53 +0900

    feat(PROBLEM3): 3,6,9 게임 구현 완료
    - 숫자 인덱스를 문자열로 치환하고 정규표현식과 일치하는 배열을 반환
    - 단축평가를 이용하여 반환된 배열이 있으면 배열의 길이만큼 손뼉 카운트를 증가시킴
    - for문 순회를 마친 후 clapCount를 반환

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,12 @@
 function problem3(number) {
+  let clapCount = 0;
+  const clapRegExp = /[369]/g;
 
-  for(let index=0; index <= number; index++){
-    String(index)
+  for (let index = 0; index <= number; index++) {
+    clapCount = clapCount + (String(index).match(clapRegExp) || []).length;
   }
   
+  return clapCount;
 }
 
 module.exports = problem3;
