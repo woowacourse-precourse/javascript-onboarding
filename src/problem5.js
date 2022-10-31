11592b574233aa7c9dfe4be16be5e93b0f8329b2
박겸영 <yong_drg@naver.com>
2022-11-01 02:05:52 +0900

    feat: 문제 5 변환 과정 반복하여 모든 변환 개수 도출 - 기능 3: 오만 원권부터 일원 동전까지 변환한 개수를 구하고 남은 금액으로 다시 변환하는 과정 반복

--- src/problem5.js
+++ src/problem5.js
@@ -2,8 +2,12 @@ function problem5(money) {
   const monetaryUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const result = [];
 
-  result.push(Math.floor(money / monetaryUnits[0]));
-  money %= monetaryUnits[0];
+  monetaryUnits.forEach((unit) => {
+    result.push(Math.floor(money / unit));
+    money %= unit;
+  });
+
+  return result;
 }
 
 module.exports = problem5;
