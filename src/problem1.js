0a710f65a3632b0d0455edbe34eb5b80e9fd9d8f
rabbitmjh <rabbitmjh@naver.com>
2022-10-31 13:57:42 +0900

    [p1] 페이지의 합

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,15 @@
 function problem1(pobi, crong) {
   var answer;
+
+  function plusSum(num){
+    var sum = 0;
+    while(num){
+      sum += num % 10;
+      sum /= 10;
+    }
+    return sum;
+  }
+
   return answer;
 }
 
