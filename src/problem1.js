6d9d1a75bbf892a2e3820a8a0e9783682e5362d6
dhkham <khamdong@gmail.com>
2022-10-31 14:08:50 +0900

    feat: (prob_1) add function that takes in a number and returns the sum of digits

--- src/problem1.js
+++ src/problem1.js
@@ -4,3 +4,13 @@ function problem1(pobi, crong) {
 }
 
 module.exports = problem1;
+
+// function that returns the sum of digits
+function digitSum(number) {
+  let sum = 0;
+  while (number > 0) {
+      sum += number % 10;
+      number = Math.floor(number / 10);
+  }
+  return sum;
+}
