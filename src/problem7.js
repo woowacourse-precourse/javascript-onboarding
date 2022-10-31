e8170ce39559fd842b28c8d94032ab1bcc3b975b
gsong <rmsdlf1206@gmail.com>
2022-10-31 22:35:51 +0900

    feat: problem 7 change function

--- src/problem7.js
+++ src/problem7.js
@@ -2,11 +2,12 @@ function problem7(user, friends, visitors) {
   let userFriends = [];
   let point = {};
   let result = [];
-  checkUserFriends(user, friends, userFriends);
+  checkUserFriends(user, friends, userFriends); // user의 친구 확인
   addFriends(user, friends, userFriends, point);
   addTenPoint(user, friends, userFriends, point);
   addOnePoint(userFriends, point, visitors);
   return sortByValue(point);
+  // console.log(userFriends, point);
 }
 function checkUserFriends(user, friends, userFriends) {
   for (let i = 0; i < friends.length; i++) {
@@ -15,7 +16,6 @@ function checkUserFriends(user, friends, userFriends) {
     }
   }
 } // user의 친구를 확인하는 함수
-
 function addFriends(user, friends, userFriends, point) {
   for (let i = 0; i < friends.length; i++) {
     for (let k = 0; k < 2; k++) {
@@ -24,17 +24,8 @@ function addFriends(user, friends, userFriends, point) {
       }
     }
   }
-}
+} // 친구관계가 아닌 사람들 point의 key 값으로 추가하는 함수
 
-function addFriends(user, friends, userFriends, point) {
-  for (let i = 0; i < friends.length; i++) {
-    for (let k = 0; k < 2; k++) {
-      if (!userFriends.includes(friends[i][k]) && friends[i][k] !== user) {
-        point[friends[i][k]] = 0;
-      }
-    }
-  }
-}
 function addTenPoint(user, friends, userFriends, point) {
   for (let j = 0; j < friends.length; j++) {
     for (let i = 0; i < Object.keys(point).length; i++) {
@@ -49,7 +40,7 @@ function addTenPoint(user, friends, userFriends, point) {
       }
     }
   }
-}
+} // point의 key로 있는 사람들의 친구들중 user의 친구로 있다면 value +10 하는 함수
 
 function addOnePoint(userFriends, point, visitors) {
   for (let i = 0; i < visitors.length; i++) {
@@ -59,7 +50,8 @@ function addOnePoint(userFriends, point, visitors) {
       point[visitors[i]] = 1;
     }
   }
-}
+} // visitor는 돌면서 userFriends 에 없고 point의 키값으로 없으면 point의 키로 등록하면서 동시에 value+1 userFriends 에 없고 point의 키값으로 있으면 value + 1 userFriends 에 있으면 무시 하는 함수 구현
+
 function sortByValue(point) {
   let sortable = [];
   let result = [];
@@ -71,5 +63,17 @@ function sortByValue(point) {
     result.push(sortable[i][0]);
   }
   return result;
-}
+} // point 객체 value를 기반으로 정렬 , 같다면 사전순 정렬 하여 상위 5개의 값 result 에 추가하는 함수
+// problem7(
+//   "mrko",
+//   [
+//     ["donut", "andole"],
+//     ["donut", "jun"],
+//     ["donut", "mrko"],
+//     ["shakevan", "andole"],
+//     ["shakevan", "jun"],
+//     ["shakevan", "mrko"],
+//   ],
+//   ["bedi", "bedi", "donut", "bedi", "shakevan"]
+// );
 module.exports = problem7;
