8e64324d27bd8718542f8ff19ec4c81d7414fd50
SeongHo-C <leeseong010@naver.com>
2022-10-31 22:12:05 +0900

    refactor(problem7): 정렬 기능

--- src/problem7.js
+++ src/problem7.js
@@ -50,8 +50,11 @@ function problem7(user, friends, visitors) {
   }
 
   let recommendSort = [...recommend].sort((a, b) => {
-    if (b[0] > b[1]) return 1;
-    else if (b[0] === b[1]) return a[0] - a[1];
+    if (a[1] > b[1]) return 1;
+    else if (a[1] === b[1]) {
+      if (a[0] > b[0]) return 1;
+      else return -1;
+    } else return 0;
   });
 
   if (recommendSort.length > 5) recommendSort = recommendSort.slice(0, 5);
