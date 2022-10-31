9379f64b82dde7b7439a316d2d3d5f8ac6a60bb5
chiman <rlaclghks123@naver.com>
2022-10-31 18:34:30 +0900

    feat: problem5- isValid함수 생성
    예외 처리를 위한 함수 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
+function isValid(money) {
+  const regex = /^[1-9][0-9]*$/;
+
+  if (money < 1 || money > 1000000 || !regex.test(money)) {
+    throw "1 이상 1,000,000 이하로 입력해주세요";
+  }
+}
+
 function problem5(money) {
-  var answer;
+  let answer = [];
+  isValid(money);
   return answer;
 }
 
