3555bb4f641771b3d8c48c4934a7c8d8e81fb2d2
odhdhfjf <odhdhfjf@gmail.com>
2022-10-31 12:44:01 +0900

    코딩테스트?

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,33 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  function search(s) {
+    let first = s[0];
+    let result = "";
+    
+    for(let i = 1; i < s.length; i++) {
+      
+      if (first === s[i]) {
+         
+         result = s.slice(0, i-1) + s.slice(i+1, s.length);
+         
+
+         return [true, result];
+      }else {
+        first = s[i];
+      }
+    }
+    return [false, s];
+  }
+  let li = search(cryptogram);
+  while (true) {
+    if (li[0] === false) {
+      break 
+    }else{
+      li = search(li[1]);
+    }
+  }
+  return li[1];
+  
+
 }
 
 module.exports = problem2;
