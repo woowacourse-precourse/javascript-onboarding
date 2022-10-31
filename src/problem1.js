5af89c5a2fb1725593d419d916085bb9625ab16a
Jin-uu <78341764+Jin-uu@users.noreply.github.com>
2022-10-31 22:39:30 +0900

    (prob 1) 기능 1 구현

--- src/problem1.js
+++ src/problem1.js
@@ -13,4 +13,15 @@ function problem1(pobi, crong) {
 	return answer;
 }
 
+// 1. 전달받은 pobi, crong 내부 값 범위 검사 (각 1~400)
+// return => valid : true, not valid : false
+function isValid(book) {
+	var left, right;
+	left = book[0]; right = book[1];
+	if((right - left) != 1) return false;				// 한 페이지 차이인지 검사
+	if(left % 2 != 1 || right % 2 != 0) return false;	// 왼쪽이 홀수, 오른쪽이 짝수인지 검사
+
+	return true;
+}
+
 module.exports = problem1;
