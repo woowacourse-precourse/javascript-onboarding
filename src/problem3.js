97e3eb7fbf4cc09755f6500a15bdfdb916444467
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 14:36:12 +0900

    feat : problem3 풀이 및 test
    3,6,9가 포함되는지 단순히 비교를 통해서도 확인 할 수 있지만
    최근에  회원가입 페이지의 비밀번호 검증을 구현하는 기능을 공부했었는데,
    거기서 사용한 정규식을 이용해봤습니다. match는 문자열이 정규식과
    매치되는 부분을 검색하는 메서드입니다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-    var answer;
-    return answer;
+    let count = 0;
+    const regex365 = /3|6|9/g;
+
+    for (let i = 1; i <= number; i++) {
+        for (let x of i.toString()) {
+            if (x.match(regex365)) {
+                count++;
+            }
+        }
+    }
+
+    return count;
 }
 
 module.exports = problem3;
