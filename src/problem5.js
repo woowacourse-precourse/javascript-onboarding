0e84fcae871a65d16ef014f5a5a040739c8bb3ec
wusi-hub <jws9595@naver.com>
2022-11-01 07:43:22 +0900

    problem5) 코드 설명을 위한 주석 추가 후 제출

--- src/problem5.js
+++ src/problem5.js
@@ -1,10 +1,18 @@
 function problem5(money) {
+
+  // 화폐 단위를 배열로 생성
   let moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+  // 결괏값을 담아줄 빈 배열 생성
   let result = []; 
+
+  // 화폐 단위 별로 몇 개가 변환되는 지 금액이 큰 순서대로 변환
   for(let i = 0; i < moneyArr.length; i++) {
     result.push(Math.floor(money / moneyArr[i]));
     money =  money % moneyArr[i];
   }
+
+  // 최종 결괏값 리턴
   return result;
 }
 
