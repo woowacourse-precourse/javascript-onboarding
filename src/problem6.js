e57da9c219ae837c1ebccc82a23f48fe11bebfa3
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:49:55 +0900

    6번 문제 닉네임 중복이면 이메일 주소 반환하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,13 +1,24 @@
 function problem6(forms) {
   var answer;
-
+  let overname;
   for(let i=0;i<forms.length;i++){
     let name=forms[i][1];
     for(let j=0;j<name.length-1;j++){
       if(name.length<3){
         break;
       }
-      let overname=name.slice(i,i+2);
+      overname=name.slice(i,i+2);
+    }
+  }
+  for(let i=0;i<overname.length;i++){
+    for(let j=0; j<overname.length;j++){
+      if((overname[i]==overname[j]&&!(i==k)){
+        for(let k=0; k<forms.length;k++){
+          if(forms[k][1]==overname[i]){
+            answer=forms[k][0];
+          }
+        }
+      }
     }
   }
   return answer;
