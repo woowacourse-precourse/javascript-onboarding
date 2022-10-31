c7354c0eb3e6a6047bf5e77682eda84877962c97
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-31 17:38:15 +0900

    Feat: 4번- 알파벳인지 확인하는 메서드 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
+// 알파벳인지 확인하는 함수
+function checkAlphabet(str) {
+  const regExp = /[a-zA-Z]/g;
+  if(regExp.test(str)){
+    return true;
+  } else{
+    return false;
+  }
+}
+
+// main 함수
 function problem4(word) {
-  var answer;
+  let answer;
   return answer;
 }
 
