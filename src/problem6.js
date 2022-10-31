66277afaae54dc6cae58149f8a298532d2d1a618
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:47:02 +0900

    6번 문제 닉네임 두글자 단위로 자르는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,15 @@
 function problem6(forms) {
   var answer;
+
+  for(let i=0;i<forms.length;i++){
+    let name=forms[i][1];
+    for(let j=0;j<name.length-1;j++){
+      if(name.length<3){
+        break;
+      }
+      let overname=name.slice(i,i+2);
+    }
+  }
   return answer;
 }
 
