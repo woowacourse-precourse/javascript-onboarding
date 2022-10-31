13307b64cdf36fd5f1f84911d394fcfcf8d3ca1c
Dahye <06robin11@gmail.com>
2022-10-31 15:29:35 +0900

    feat: frog's dictionary is made for capital letters

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,33 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const upperCaseDic = {
+    A: "Z",
+    B: "Y",
+    C: "X",
+    D: "W",
+    E: "V",
+    F: "U",
+    G: "T",
+    H: "S",
+    I: "R",
+    J: "Q",
+    k: "P",
+    L: "O",
+    M: "N",
+    N: "M",
+    O: "L",
+    P: "K",
+    Q: "J",
+    R: "I",
+    S: "H",
+    T: "G",
+    U: "F",
+    V: "E",
+    W: "D",
+    X: "C",
+    Y: "B",
+    Z: "A",
+    a: "z",
+  };
 }
 
 module.exports = problem4;
