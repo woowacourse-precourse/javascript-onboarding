ed853cb3ba619c5d914d9db043bd27890b6e2b0f
Yesong <to06109@naver.com>
2022-10-31 18:44:28 +0900

    [1주차 6번] FEAT: 가능한 연속 문자열 추출 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+
+  for (let form of forms) {
+    const cases = words(form[1]);
+  }
+
   return answer;
+}
+
+// 가능한 연속된 문자열 추출 함수
+function words(name) {
+  const wordOfCases = [];
+  let end = name.length;
+
+  for (let index = 0; index < name.length; index++) {
+    for (let count = 2; count <= end; count++) {
+      wordOfCases.push(name.substr(index, count));
+    }
+    end -= 1;
+  }
+
+  return wordOfCases;
 }
 
 module.exports = problem6;
