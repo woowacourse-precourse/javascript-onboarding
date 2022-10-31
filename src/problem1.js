59579dda169993d0304fbf0dbcb2e879980797ca
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:14:20 +0900

    1번 문제 페이지 곱과 합을 구해 answer 반환

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,28 @@
 function problem1(pobi, crong) {
   var answer;
+  let pobilist,cronglist;
   
+  for(let i=0; i<pobi;i++){
+    let value1=pobi[i];
+    let value2=crong[i];
+    pobilist.push(add(value1));
+    cronglist.push(add(value2));
+    pobilist.push(mul(value1));
+    cronglist.push(mul(value2));
+
+    if((Math.min(cronglist)==0)|(Math.min(pobilist)==0)){
+      answer=-1;
+    }
+    else{
+      if(Math.max(cronglist)>Math.max(pobilist)){
+        answer=2;
+      }
+      else if(Math.max(cronglist)<Math.max(pobilist)){
+        answer=1;
+      }
+      else answer=0;
+    }
+    
     const add= (num) =>{
       answer= num/100 +num%100/10 +num%10;
       return answer;
@@ -15,8 +37,9 @@ function problem1(pobi, crong) {
     
   }
   return answer;
-
-
-
+}
 
 module.exports =problem1;
+
+
+
