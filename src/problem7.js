c670ac3a848767da98f2ffaf373b52c5b42eca2b
gyeongza <gyeongza@naver.com>
2022-10-31 22:34:28 +0900

    feat: 친구 관계 배열을 이용해 친구 목록을 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,14 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+  //친구 관계 배열을 이용해 친구 목록을 생성
+  const friendArr = createfriendArr(friends, user);
+  const mutualFriendArr = [];
+  for (let i = 0; i < friendArr.length; i++) {
+    const friend = friendArr[i];
+    mutualFriendArr.push(...createfriendArr(friends, friend, friendArr));
+    visitors = visitors.filter((visit) => visit !== friend);
+  }
+
+  
+};
 
 module.exports = problem7;
