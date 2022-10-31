9665d4780b3c006b2ed6c65b59b601aa57f1de71
김나현 <nahyeon.dev@gmail.com>
2022-10-31 14:05:12 +0900

    problem6 중복 확인을 위한 변수,배열 선언 및 사용

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  //var answer;
+  let check = [];
+  let answer = [];
+
+  //비교를 위해 닉네임만 check 배열에 push
+  for(let i = 0; i<forms.length; i++){
+    check.push(forms[i][1]);
+  }
+
+  for(let i = 0; i<forms.length; i++){
+    for(let j = 0; j<forms[i][1].length-1; j++){
+      let str = forms[i][1][j] + forms[i][1][j+1]; //닉네임 두 글자로 잘라서 저장
+      checkString(i, str, check, answer); //index, string, check배열, 저장할 answer배열
+    }
+  }
+
+  
 }
 
 module.exports = problem6;
