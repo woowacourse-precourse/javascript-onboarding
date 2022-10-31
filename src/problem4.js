821d9dc98fca994ca28f6b7a971ecaa953664607
Jeonjunwoo <tira618@naver.com>
2022-10-31 16:39:11 +0900

    정상적인 알파벳의 순서와 청개구리의 알파벳 순서

--- src/problem4.js
+++ src/problem4.js
@@ -2,5 +2,6 @@ function problem4(word) {
   var answer;
   return answer;
 }
-
+const alphabetArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
+const strangeAlphabetArr = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'];
 module.exports = problem4;
