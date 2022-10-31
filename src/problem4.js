9fdd8f510075384695f7ec35fae799131e2e9145
Haram Baek <haram@Haramui-MacBookAir.local>
2022-10-31 16:32:23 +0900

    feat: 엄마 말씀과 청개구리 사전을 mapping 시키는 object 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,34 @@
 function problem4(word) {
   var answer;
+  let map = {
+    A: Z,
+    B: Y,
+    C: X,
+    D: W,
+    E: V,
+    F: U,
+    G: T,
+    H: S,
+    I: R,
+    J: Q,
+    K: P,
+    L: O,
+    M: N,
+    N: M,
+    O: L,
+    P: K,
+    Q: J,
+    R: I,
+    S: H,
+    T: G,
+    U: F,
+    V: E,
+    W: D,
+    X: C,
+    Y: B,
+    Z: A
+  }
+
   return answer;
 }
 
