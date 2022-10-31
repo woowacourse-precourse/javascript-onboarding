1196147af5469f809d2a55271bbd745253a6250f
hae-on <solbi2004@naver.com>
2022-11-01 01:43:14 +0900

    [문제7] 함께 아는 친구 목록 구현

--- src/problem7.js
+++ src/problem7.js
@@ -9,6 +9,17 @@ const findFriend = (user, friends) => {
   return friendList;
 };
 
+const findFriendOfFriend = (user, friends, friendList) => {
+  friends.map((friend) => {
+    const [A, B] = friend;
+    friendList.map((friendName) => {
+      if (A == friendName && B != user) friendOfFriendList.push(B);
+      if (B == friendName && A != user) friendOfFriendList.push(A);
+    });
+  });
+  return friendOfFriendList;
+};
+
 function problem7(user, friends, visitors) {
   let answer;
   return answer;
