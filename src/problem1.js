c6795906f3cab5dc2d9e9f52fe7da40f86c6eeae
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 03:14:30 +0900

    feat: [problem1] 각 자리 수를 모두 더해 반환하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,11 @@
+// 페이지 번호 각 자리 숫자를 모두 더해 반환하는 기능
+function getSum(n) {
+  let sum = 0;
+  let number = n.toString();
+  [...number].forEach(i => sum += parseInt(i));
+  return sum;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
