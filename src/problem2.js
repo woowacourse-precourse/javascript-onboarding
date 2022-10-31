04c910a82de5020150adeae307f22dbc00236470
Jeonjunwoo <tira618@naver.com>
2022-10-31 16:23:55 +0900

    중복반복문을 사용하여 연속하여 중복되는 요소를 배열에서 제거

--- src/problem2.js
+++ src/problem2.js
@@ -7,6 +7,13 @@ function cutString(cryptogram) {
   for(i = 0; i < cryptogram.length; i++) {
     alphaItem.push(cryptogram.substring(i, i+1));
   } 
+  for(j = 0; j < cryptogram.length; j++){
+    for(i = 0; i < cryptogram.length; i++) { 
+      if (alphaItem[i] === alphaItem[i+1]) {
+        alphaItem.splice(i, 2);
+      }
+    }
+  }
 }
 
 module.exports = problem2;
