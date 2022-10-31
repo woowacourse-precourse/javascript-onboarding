40d3872b02c9fae2ff17b98b4e9b10789f5a156e
jiyun1006 <jiyun4251@gmail.com>
2022-11-01 01:41:02 +0900

    🎉 problem2 푸는 중..... (feat. 처음에 모든 알파벳으로 연속되는 문자열만 삭제하면 된다고 생각함 --> 새로생기는 문자열에서 생기는 문제 발생)

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+
+  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
+    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+
+  alphabet.map((el) => {
+    let arrCrypto = cryptogram.split("");
+    let idx = arrCrypto.indexOf(el);
+    if (idx !== -1) {
+      cryptogram = cryptogram.split(`${arrCrypto[idx]}${arrCrypto[idx]}`).join('');
+    }
+    console.log(cryptogram);
+  })
+  
   return answer;
 }
 
