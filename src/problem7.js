0b73e6b23ede7f10fb9f504eced5840f4ba702fc
HBSPS <wiyeong79@naver.com>
2022-10-31 17:10:54 +0900

    :sparkles: [문제7] problem7 함수 구현 (예외처리 O)

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,10 @@
 function problem7(user, friends, visitors) {
   var answer;
 
+  if (!validUser(user)) throw new Error("잘못된 user입니다.");
+  if (!validFriends(friends)) throw new Error("잘못된 friends입니다.");
+  if (!validVisitors(visitors)) throw new Error("잘못된 visitors입니다.");
+
   const friendsTotalScore = calScore(user, friends, visitors);
   const userFriendSet = getConnection(friends)[user];
   answer = sortFriends(userFriendSet, friendsTotalScore);
@@ -23,6 +27,9 @@ function getConnection(friends) {
   for (let i = 0; i < friends.length; i++) {
     const friendA = friends[i][0];
     const friendB = friends[i][1];
+
+    if (!validID(friendA)) throw new Error("잘못된 ID가 포함되어 있습니다.");
+    if (!validID(friendB)) throw new Error("잘못된 ID가 포함되어 있습니다.");
 
     if (!Object.keys(friendsConnection).includes(friendA)) friendsConnection[friendA] = new Set();
     if (!Object.keys(friendsConnection).includes(friendB)) friendsConnection[friendB] = new Set();
