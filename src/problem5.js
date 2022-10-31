5b7aebb532d1955fe53c1d4fa95ecd5fd2c6a9d0
JeongSabibi <jsbb528@naver.com>
2022-11-01 02:26:01 +0900

    feat(PROBLEM5): 전체 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,17 @@
 function problem5(money) {
-  var answer;
+  var answer = new Array(9);
+
+  var moneySort = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i=0; i<moneySort.length; i++) {
+    if (i === 0) {
+      answer[i] = parseInt(money / moneySort[i]);
+    }
+    else {
+      answer[i] = parseInt((money % moneySort[i-1]) / moneySort[i]);
+    }
+  }
+
   return answer;
 }
 
