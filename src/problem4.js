cacabfd7dcb36a46d76b94be6c711016a820262d
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 21:57:36 +0900

    [Feat:Problem4] 문자열 ascii charcode 배열로 변환하기

--- src/problem4.js
+++ src/problem4.js
@@ -6,3 +6,6 @@ const tail_l = "z".charCodeAt(0);
 const isAlpha = (charcode) =>
   (charcode >= head_u && charcode <= tail_u) ||
   (charcode >= head_l && charcode <= tail_l);
+
+const stringToCharcodeArr = (word) =>
+  word.split("").map((char) => char.charCodeAt(0));
