3c94820891eb63047f1283728cf060770ec3b3a8
sein <hyuntpdls@gmail.com>
2022-10-31 16:09:17 +0900

    [문제3] feat: 3, 6, 9 숫자 개수 확인을 통해 손뼉 치는 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,32 @@
 function problem3(number) {
   var answer;
+  // 제한사항
+  if (number < 1 || number > 10000) return;
+
+  answer = checkNum(number);
 
   return answer;
+}
+
+// 숫자 자리수에 3, 6, 9 개수 확인
+function checkNum(number) {
+  let arr = [];
+  let answer = [];
+
+  for (let i = 1; i <= number; i++) {
+    arr.push(makeNumDigits(i));
+  }
+  for (let i = 0; i < arr.length; i++) {
+    answer.push(
+      arr[i].filter(
+        (element) => "3" === element || "6" === element || "9" === element
+      ).length
+    );
+  }
+
+  let result = answer.reduce((sum, currValue) => sum + currValue);
+
+  return result;
 }
 
 // 숫자 자리수로 나누기
