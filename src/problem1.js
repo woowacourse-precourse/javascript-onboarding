3a7205dabe473d3a76a3e5458136b6ee3d80d2da
durumi99 <kms990615@naver.com>
2022-10-31 17:16:25 +0900

    [problem 1] 점수를 비교하여 결과 return 구현

--- src/problem1.js
+++ src/problem1.js
@@ -29,6 +29,7 @@ function returnMult(pageNum){
   } 
   return tmp;
 }
+// 2, 3번 과정에서 가장 큰 수를 구하는 함수
 function returnMaxScore(player){
   return Math.max(returnSum(player[0]), returnMult(player[0]), returnSum(player[1]), returnMult(player[1]))
 }
@@ -40,6 +41,12 @@ function problem1(pobi, crong) {
   let pobiScore = returnMaxScore(pobi);
   let crongScore = returnMaxScore(crong)
   
+  if(pobiScore > crongScore) // 포비가 이기는 경우
+    answer = 1
+  else if(pobiScore < crongScore) // 크롱이 이기는 경우
+    answer = 2
+  else // 무승부
+    answer = 0
   return answer;
 }
 
