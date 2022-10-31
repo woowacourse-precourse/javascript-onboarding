582cd42b3b6596cefa1db111475caea82cd99c49
OrangeVinyl <hap_hazard@naver.com>
2022-10-31 22:50:58 +0900

    정규식 이용 알파벳을 제외한 값 정상 변환 및 테스트 실패

--- src/problem4.js
+++ src/problem4.js
@@ -1,19 +1,24 @@
 function problem4(word) {
   var answer = "";
-  let reverse, abstemp, ascii = 0;
-  for (var i = 0; i<word.length; i++) {
-    if(word[i] === word[i].toUpperCase()){
+  let reverse,
+    abstemp,
+    ascii = 0;
+  const pattern = /[a-zA-Z]/g;
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === word[i].toUpperCase()) {
       ascii = 65;
-    }
-    else if (word[i]=== word[i].toLowerCase()){
+    } else if (word[i] === word[i].toLowerCase()) {
       ascii = 97;
+    } else if (!pattern.test(word[i]) && !pattern.test(word[i])) {
+      answer += word[i];
+      continue;
     }
     abstemp = Math.abs(ascii - word.charCodeAt(i));
-    reverse = (ascii + 25) - abstemp;
+    reverse = ascii + 25 - abstemp;
     answer = answer + String.fromCharCode(reverse);
   }
+
   return answer;
 }
-
 
 module.exports = problem4;
