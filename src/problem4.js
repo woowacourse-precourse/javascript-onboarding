28ab7eec469ade65cf1939438724e5880313cf02
SoMin <thals0107@naver.com>
2022-10-31 11:09:22 +0900

    feat: 문제 4 - string의 각 문자열 돌면서 아스키코드로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
   let string = [...word];
   let answer = [];
+
+  for (let i = 0; i < string.length; i++) {
+    const ascii = string[i].charCodeAt(0);
+  }
+  return answer.join("");
 }
 
 module.exports = problem4;
