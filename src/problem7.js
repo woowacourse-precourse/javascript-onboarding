86fbddcc842089104d4b2dd9e9f3f0764bfadaff
yesolyo <ahnyelim77@gmail.com>
2022-10-31 18:37:26 +0900

    user친구의 친구아이디를 찾는다

--- src/problem7.js
+++ src/problem7.js
@@ -6,18 +6,29 @@ function problem7(user, friends, visitors) {
   var visitors_number = [];
   var n = 0;
 
-  //friends에서 사용자인 mrko를 찾는다
+  //friends에서 user인 mrko를 찾는다
   for (let i = 0; i < friends.length; i++) {
     if (user == friends[i][0]) {
-      //mrko친구 아이디B를 찾는다
+      //user친구 아이디B를 찾는다
       user_friends[n] = friends[i][1];
       n += 1;
     } else if (user == friends[i][1]) {
-      //mrko친구 아이디A를 찾는다
+      //user친구 아이디A를 찾는다
       user_friends[n] = friends[i][0];
       n += 1;
     } else {
       continue;
+    }
+  }
+
+  //user친구의 친구아이디를 찾는다
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < user_friends.length; j++) {
+      if (user_friends[j] == friends[i][0] && user != friends[i][1]) {
+      } else if (user_friends[j] == friends[i][1] && user != friends[i][0]) {
+      } else {
+        continue;
+      }
     }
   }
 
