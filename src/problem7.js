a08daae042711affd2d6cb91d7b8abb7617b7d17
SeongHo-C <leeseong010@naver.com>
2022-10-31 19:52:19 +0900

    feat(problem7): 사용자의 친구 반환 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,9 +1,22 @@
 // 사용자와 함께 아는 친구 반환 기능
 
-// 사용자의 친구 반환 기능
+function findUserFriends(user, friends) {
+  const findFriends = [];
+
+  for (let x of friends) {
+    if (x.includes(user)) {
+      if (x[0] === user) findFriends.push(x[1]);
+      else findFriends.push(x[0]);
+    }
+  }
+
+  return findFriends;
+}
 
 function problem7(user, friends, visitors) {
   let answer;
+
+  const userFriends = findUserFriends(user, friends);
 
   // 사용자와 함께 아는 친구에게 추천 점수 부여 기능
 
