4cbd670d10a3db4f798ed20a564724f8d52193f4
areumsheep <areumsheep@gmail.com>
2022-11-01 06:54:50 +0900

    add: 문제7 답안 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,46 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+const SCORE = {
+  ACQUAINTANCE: 10,
+  VISITED_FRIEND: 1,
+};
+const MAX_FRIENDS_TO_SHOW = 5;
+
+const sortByAlphabetically = (a = '', b = '') => {
+  const upperCaseA = a.toUpperCase();
+  const upperCaseB = b.toUpperCase();
+
+  if (upperCaseA > upperCaseB) return 1;
+  if (upperCaseA < upperCaseB) return -1;
+  if (upperCaseA === upperCaseB) return 0;
+};
+
+function problem7(user = '', friends = [], visitors = []) {
+  const beFriends = {};
+  const alreadyFriends = new Set();
+
+  for (const [alreadyFriend, beFriend] of friends) {
+    if (beFriend === user) continue;
+
+    alreadyFriends.add(alreadyFriend);
+    if (beFriends[beFriend] !== undefined) {
+      beFriends[beFriend] = beFriends[beFriend] + SCORE.ACQUAINTANCE;
+    } else beFriends[beFriend] = SCORE.ACQUAINTANCE;
+  }
+
+  for (const visitor of visitors) {
+    if (alreadyFriends.has(visitor)) continue;
+
+    if (beFriends[visitor]) beFriends[visitor] += SCORE.VISITED_FRIEND;
+    else beFriends[visitor] = SCORE.VISITED_FRIEND;
+  }
+
+  const sortedFriends = Object.entries(beFriends).sort(
+    ([nameA, scoreA], [nameB, scoreB]) => {
+      if (scoreA === scoreB) return sortByAlphabetically(nameA, nameB);
+      return scoreB - scoreA;
+    }
+  );
+
+  return sortedFriends.map(([name, score]) => name);
 }
 
 module.exports = problem7;
