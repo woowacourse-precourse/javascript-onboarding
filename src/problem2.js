f1c41b5f3dde2b28193058f0cf36457e2238935e
Hwang Juhee <julia0276@naver.com>
2022-10-31 23:06:04 +0900

    :hammer: [refactor] 재귀함수 수정 #P2

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,15 @@
+function decoding (s) {
+  s.forEach((x,i)=>{
+    if (s[i] === s[i + 1]) {
+      s.splice(i, 2);
+      decoding(s);
+    }
+  })
+};
+
 function problem2(cryptogram) {
   let str = cryptogram.split("");
-  const recursion = (s) => {
-    for (let i = 0; i < s.length; i++) {
-      if (s[i] === s[i + 1]) {
-        s.splice(i, 2);
-        recursion(s);
-      }
-    }
-  };
-  recursion(str);
+  decoding(str);
   let answer = str.join('');
   return answer;
 }
