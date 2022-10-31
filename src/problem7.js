7a93d59db4925b8156936c32ef00446ae486b1d9
sein <hyuntpdls@gmail.com>
2022-10-31 16:29:51 +0900

    [문제7] feat: 내 친구 확인하기

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,15 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+// 내 친구 체크
+function findFriends(user, friendsList) {
+  let result = [];
+  for (let i = 0; i < friendsList.length; i++) {
+    if (friendsList[i].includes(user)) {
+      result.push(friendsList[i].filter((name) => name !== user).join(""));
+    }
+  }
+  return result;
+}
+
 module.exports = problem7;
