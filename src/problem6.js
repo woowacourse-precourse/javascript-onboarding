9737c40517b89cb5c213cabbac689148dc9c0b72
Jeonjunwoo <tira618@naver.com>
2022-10-31 17:28:44 +0900

    입력되는 크루들의 이메일과 이름을 분리하여 배열에 저장

--- src/problem6.js
+++ src/problem6.js
@@ -2,5 +2,12 @@ function problem6(forms) {
   var answer;
   return answer;
 }
-
+const crews = [];
+const emails = [];
+function devidePeople(forms) {
+  for (i = 0; i < forms.length; i++) {
+    crews.push(forms[i][1]);
+    emails.push(forms[i][0]);
+  }
+}
 module.exports = problem6;
