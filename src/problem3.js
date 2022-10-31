bf2de4d0b2e617123099f1587c3d5e37f23628ea
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-31 17:14:31 +0900

    problem3: count() done

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,18 @@
 function problem3(number) {
   var answer;
 
-  function count(number) {}
+  function count(number) {
+    var result = 0;
+    for (let num = 1; num <= number; num++) {
+      num
+        .toString()
+        .split("")
+        .forEach((element) => {
+          if ((element == "3") | (element == "6") | (element == "9")) result++;
+        });
+    }
+    return result;
+  }
 
   answer = count(number);
 
