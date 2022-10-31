468d06b93129cdc470cb7bfb4922516d2a4f1d01
KangYunJi <dbswl701@ajou.ac.kr>
2022-10-31 22:03:03 +0900

    feat:[문제2] 구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = [...cryptogram];
+  let i=0;
+
+  while(i != answer.length){
+    if(answer[i] == answer[i+1]){
+      answer.splice(i, 2);
+      i=0;
+    } else { i++; }
+  }
+  answer = answer.join('');
   return answer;
 }
 
