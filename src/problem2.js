a98a263c8072d60b7416367cdf688c35383c467a
leesoyuun <leesoyun4206@naver.com>
2022-10-31 23:45:31 +0900

    [Problem 2] 예외처리 추가함

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,14 @@ function problem2(cryptogram) {
   let arr = cryptogram.split("");
   let answer = [];
 
+  //예외처리
+  let firstStrValue = cryptogram.charAt(0);
+    if(firstStrValue === firstStrValue.toLowerCase()) {
+        return answer.join("");
+    }
+    if(cryptogram.length<1 || cryptogram.length>1000)
+        return answer.join("");
+        
   for(let str of arr){
       if(answer[answer.length - 1] === str){
           answer.pop();
