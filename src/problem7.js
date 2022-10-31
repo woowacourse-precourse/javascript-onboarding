6da47c23118c005c758a0c8d74b3f02a9b80b451
nalsae <nalsae0207@naver.com>
2022-10-31 20:44:41 +0900

    feat: Add a logic to add scores
    - friends 배열을 순회하면서 서로 아는 친구인 경우 생성한 점수 리스트에
      10점을 추가하는 로직 작성

--- src/problem7.js
+++ src/problem7.js
@@ -16,6 +16,17 @@ const problem7 = (user, friends, visitors) => {
     scoreList.set(person, 0)
   );
 
+  friends.forEach((friend) =>
+    friend.forEach((person) => {
+      if (friendsOfUser.includes(person)) {
+        const targetPerson =
+          friend.indexOf(person) === 0 ? friend[1] : friend[0];
+        const score = scoreList.get(targetPerson) + 10;
+        scoreList.set(targetPerson, score);
+      }
+    })
+  );
+
   return answer;
 };
 
