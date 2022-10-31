bd011982dd37c414d0e9553bb50789871d9bfe72
JiSu Kim <85912592+jsk3342@users.noreply.github.com>
2022-11-01 02:32:08 +0900

    Add: 7번 문제 해결

--- src/problem7.js
+++ src/problem7.js
@@ -1,25 +1,45 @@
-// function problem7(user, friends, visitors) {
-//   let answer;
-//   return answer;
-// }
+function problem7(user, friends, visitors) {
+  let answer = [];
+  const userFriends = friends.map(i => i.filter(j => !user.includes(j))).filter(j => j.length < 2).flat();
+  const recommendationList = [...new Set(friends.map(i => i.filter(j => !userFriends.includes(j))).filter(j => !user.includes(j)).filter(j => j.length < 2).flat())]
+  //배열과 이름과 점수를 담아보자
+  const scoreFriendsList = recommendationList.map(i => 10)
+  //두 배열의 길이는 항상 같아야 함.
+  //방문자 확인해서 점수 올려주기
+  visitors.forEach(i => {
+    if (recommendationList.indexOf(i) !== -1) {
+      scoreFriendsList[recommendationList.indexOf(i)]++
+    } else if (recommendationList.indexOf(i) === -1 && userFriends.indexOf(i) === -1) {
+      recommendationList.push(i);
+      scoreFriendsList.push(1);
+    }
+  })
 
+  //이름과 점수 추출함
+  // console.log(recommendationList)
+  // console.log(scoreFriendsList)
+  
+  //이름과 점수 가지고 점수를 비교해서 등수로 정렬하기
+  //두 개의 배열로 관리하면 힘들다. 하나로 묶어서 점수 기준으로 정렬하기
+  const countArr = [];
+  for (let i = 0; i < recommendationList.length; i++) {
+    countArr.push([recommendationList[i], scoreFriendsList[i]])
+  }
 
+  countArr.sort((a, b) => {
+    if (a[1] === b[1]) {
+      return (a[0] - b[0])
+    } else {
+      (b[1] - a[1])
+    }
+  })
+  
+  //정렬된 기준으로 정답에 푸시
+  countArr.forEach(i => answer.push(i[0]))
+  return answer;
+}
 
-// const user = "mrko";
-// const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
-// const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
-
-
-
-// problem7(user, friends, visitors)
-
-
-
-
-
-
-
-// module.exports = problem7;
+module.exports = problem7;
 
 //step 1
 // 친구 추출하기
@@ -132,32 +152,3 @@
 
 //다시 돌아와서 처음부터 친구 리스트를 추출할 때 내 이름을 빼고 추출해야한다.
 
-function problem7(user, friends, visitors) {
-  let answer;
-  const userFriends = friends.map(i => i.filter(j => !user.includes(j))).filter(j => j.length < 2).flat();
-  const recommendationList = [...new Set(friends.map(i => i.filter(j => !userFriends.includes(j))).filter(j => !user.includes(j)).filter(j => j.length < 2).flat())]
-  //배열과 이름과 점수를 담아보자
-  const scoreFriendsList = recommendationList.map(i => 10)
-  //두 배열의 길이는 항상 같아야 함.
-  //방문자 확인해서 점수 올려주기
-  visitors.forEach(i => {
-    if (recommendationList.indexOf(i) !== -1) {
-      scoreFriendsList[recommendationList.indexOf(i)]++
-    } else if (recommendationList.indexOf(i) === -1 && userFriends.indexOf(i) === -1) {
-      recommendationList.push(i);
-      scoreFriendsList.push(1);
-    }
-  })
-
-  //이름과 점수 추출함
-  console.log(recommendationList)
-  console.log(scoreFriendsList)
-
-  //이름과 점수 가지고 점수를 비교해서 등수로 정렬하기
-  return answer;
-}
-const user = "mrko";
-const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]
-const visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
-
-problem7(user, friends, visitors)
