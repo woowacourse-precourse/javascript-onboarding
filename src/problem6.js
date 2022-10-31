b5647998f18473d84223414e97817298daa98af2
Yeom-JinHo <pwlsghq@naver.com>
2022-10-31 22:12:09 +0900

    feat : [P6] 닉네임에서 나올 수 있는 연속적인 단어 색출

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,14 @@
+function consecutiveWord(word) {
+  let arr = [];
+  const wordArr = word.split("");
+  for (let i = 0; i < wordArr.length; i++) {
+    for (let j = i + 2; j <= wordArr.length; j++) {
+      arr.push(wordArr.slice(i, j).join(""));
+    }
+  }
+  return arr;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
