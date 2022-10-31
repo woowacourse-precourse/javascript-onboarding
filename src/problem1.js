0e9ba258250dc655bca1827c28ffc74ffd93032b
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 19:57:08 +0900

    feat: [1번 문제] 예외 사항 같은 페이지 검사

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (!passExceptions(pobi, crong)) return -1;
+}
+
+function passExceptions(pobi, crong) {
+  return isSameChapter(pobi[0], pobi[1]) && isSameChapter(crong[0], crong[1]);
+}
+
+function isSameChapter(leftPage, rightPage) {
+  return rightPage - leftPage === 1;
 }
 
 module.exports = problem1;
