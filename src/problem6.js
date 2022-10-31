0439f8d418491b49207d74da26700ef8fca1c2d4
HBSPS <wiyeong79@naver.com>
2022-10-31 17:30:49 +0900

    :recycle: [문제6] 각 메소드 구분용 주석 추가

--- src/problem6.js
+++ src/problem6.js
@@ -2,10 +2,12 @@ function problem6(forms) {
   var answer;
   if (!validRange(forms)) throw new Error("크루의 범위를 벗어났습니다.");
   
-  answer = changeSetToArray(checkNickname(forms));
+  answer = changeSetToArray(checkNickname(forms)); // checkNickname 메소드의 return은 Set이므로 Array로 변환
   return answer;
 }
 
+
+// 닉네임을 연속된 두 글자로 나누어 집합으로 만든 뒤, 중복 여부를 검사하는 메소드
 function checkNickname(arr) {
   const overlapedNickname = new Set();
   let nicknameSet = new Set();
@@ -20,11 +22,11 @@ function checkNickname(arr) {
   }
 
   firstNickname = sliceNickname(arr[0][1], firstNickname);
-  if (checkFirst) overlapedNickname.add(arr[0][0]);
+  if (checkFirst) overlapedNickname.add(arr[0][0]); // 첫번째 닉네임에 중복되는 부분이 있다면 정답 집합에 추가
 
   return overlapedNickname;
 }
-
+// 닉네임을 두 글자씩 잘라 중복 검사용 집합에 저장하는 메소드
 function sliceNickname(nickname, overlapedSet) {
   for (let i = 2; i <= nickname.length; i++) {
     overlapedSet.add(nickname.slice(i-2, i));
@@ -32,7 +34,7 @@ function sliceNickname(nickname, overlapedSet) {
 
   return overlapedSet;
 }
-
+// 중복 검사용 집합과 비교하여 중복 닉네임이 있는지 검사하는 메소드
 function checkOverlap(nickname, set) {
   for (let i = 2; i <= nickname.length; i++) {
     if (set.has(nickname.slice(i - 2, i))) return true; // 중복되는 문자열을 갖는다면 true 반환
@@ -41,6 +43,8 @@ function checkOverlap(nickname, set) {
   return false;
 }
 
+
+// 첫번째 닉네임을 확인하기 위한 메소드
 function checkFirst(arr, set) {
   for (let i = 1; i < arr.length; i++) {
     if (checkOverlap(arr[i][1], set)) return true;
@@ -49,12 +53,16 @@ function checkFirst(arr, set) {
   return false;
 }
 
+
+// 중복 제거용 집합을 배열로 변환하는 메소드
 function changeSetToArray(set) {
   const arr = Array.from(set);
 
   return arr.sort();
 }
 
+
+// 예외 처리
 function validRange(arr) {
   return 1 <= arr.length && arr.length <= 10000;
 }
