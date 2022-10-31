629cf34b45b5b73e6e0a3ae209362011ddbdf290
고동현 <dhjk35@naver.com>
2022-10-31 15:58:47 +0900

    fix: 연속된 문자열이 3개일 경우 제대로 해독하지 못하는 오류 해결 - 문제 2

--- src/problem2.js
+++ src/problem2.js
@@ -2,7 +2,6 @@
  * 기능 정의
  *
  * - [x] 길이가 1이라면 그대로 반환하는 기능
- * - [x] 길이가 2라면 서로 같은지 확인하고 같다면 공백, 아니면 문자 그대로 반환하는 기능
  * - [x] 스택을 이용해서 주어진 문자열을 비교해나가면서 연속된 문자 제거하는 기능
  */
 
@@ -14,28 +13,25 @@
  * @returns {string} 연속된 문자열을 제거하고 남은 문자열
  */
 function problem2(cryptogram) {
-  if (cryptogram.length < 2) {
+  if (cryptogram.length <= 1) {
     return cryptogram;
   }
 
-  if (cryptogram.length === 2) {
-    return cryptogram[0] === cryptogram[1] ? "" : cryptogram;
-  }
+  const stack = [cryptogram];
 
-  const stack = [cryptogram[0]];
+  while (stack) {
+    const current = stack.pop();
+    const decrypted = current
+      .split("")
+      .filter((char, i) => char !== current[i - 1] && char !== current[i + 1]);
+    const answer = decrypted.join("");
 
-  for (let i = 1; i < cryptogram.length; i++) {
-    const current = cryptogram[i];
-
-    if (stack.length > 0 && stack[stack.length - 1] === current) {
-      stack.pop();
-      continue;
+    if (current === answer) {
+      return answer;
     }
 
-    stack.push(current);
+    stack.push(answer);
   }
-
-  return stack.join("");
 }
 
 module.exports = problem2;
