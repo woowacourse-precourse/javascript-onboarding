0935bff5760272f8956aaebf385025870585fb50
dahlia <wlstnrdahlia@gmail.com>
2022-10-31 23:54:03 +0900

    Add : problem2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,24 @@
 function problem2(cryptogram) {
   var answer;
+  let len = cryptogram.length
+  while (true) {
+    for (i = 0; i < len - 1; i++) {
+      if (cryptogram[i] === cryptogram[i+1]) {
+        word1 = cryptogram.slice(0, i);
+        word2 = cryptogram.slice(i+2, len);
+        newCrypto = word1 + word2;
+      }
+    }
+    if (newCrypto === cryptogram) {
+      break
+    } else {
+      cryptogram = newCrypto;
+      len = cryptogram.length
+    }
+  }
+
+  answer = cryptogram
+
   return answer;
 }
 
