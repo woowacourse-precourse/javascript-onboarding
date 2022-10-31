328b34b6de677bc38b63929730efef8792b9f1b1
woong <inwoong100@gmail.com>
2022-10-31 22:09:05 +0900

    refactor: problem1 코드 리팩토링
    1. 기능 단위 함수로 분리
    2. 각 기능에 맞는 함수명 지정
    3. 각 역할에 맞는 변수명 지정

--- src/problem1.js
+++ src/problem1.js
@@ -1,38 +1,68 @@
+const multiplyElements = (elements) => {
+  let results = 1;
+
+  elements.forEach((element) => {
+    results *= Number(element);
+  });
+
+  return results;
+};
+
+const addElements = (elements) => {
+  let results = 0;
+
+  elements.forEach((element) => {
+    results += Number(element);
+  });
+
+  return results;
+};
+
+const makeAddMultipleArray = (page) => {
+  const results = [];
+
+  const pageElements = page.toString().split('');
+
+  results.push(multiplyElements(pageElements));
+  results.push(addElements(pageElements));
+
+  return results;
+};
+
+const MaxOfSumMultiple = (pages) => {
+  let answerArray = [];
+
+  //예외사항3.
+  if (pages[1] - pages[0] !== 1) return -1;
+  //예외사항4.
+  if (pages[1] % 2 !== 0) return -1;
+  if (pages[0] % 2 !== 1) return -1;
+  //예외사항5.
+  if (pages[0] >= pages[1]) return -1;
+
+  for (const page of pages) {
+    //예외사항2.
+    if (page === (1 || 400)) return -1;
+
+    answerArray = [...answerArray, ...makeAddMultipleArray(page)];
+  }
+
+  return Math.max(...answerArray);
+};
+
 function problem1(pobi, crong) {
-  const plusMultiple = (array) => {
-    const answerArray = [];
+  //예외사항1.
+  if (pobi.length !== 2 || crong.length !== 2) return -1;
 
-    //예외사항
-    if (array[1] - array[0] !== 1) return -1;
-    if (array[1] % 2 !== 0) return -1;
-    if (array[0] % 2 !== 1) return -1;
+  const [pobiScore, crongScore] = [
+    MaxOfSumMultiple(pobi),
+    MaxOfSumMultiple(crong),
+  ];
 
-    for (const element of array) {
-      //예외사항
-      if (element === (1 || 400)) return -1;
+  //예외에 속하는지 유무 판단
+  if (pobiScore === -1 || crongScore === -1) return -1;
 
-      const array = element.toString().split('');
-      let sum = 0;
-      array.forEach((el) => {
-        sum += Number(el);
-      });
-      answerArray.push(sum);
-
-      let multiple = 1;
-      array.forEach((el) => {
-        multiple *= Number(el);
-      });
-      answerArray.push(multiple);
-    }
-    return Math.max(...answerArray);
-  };
-
-  const pobiAnswer = plusMultiple(pobi);
-  const crongAnswer = plusMultiple(crong);
-
-  if (pobiAnswer === -1 || crongAnswer === -1) return -1;
-
-  return pobiAnswer > crongAnswer ? 1 : pobiAnswer === crongAnswer ? 0 : 2;
+  return pobiScore > crongScore ? 1 : pobiScore === crongScore ? 0 : 2;
 }
 
 module.exports = problem1;
