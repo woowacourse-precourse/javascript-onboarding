b3f2d3bc0edcb87c1411579853337825f88c40f2
dkile <dkile532@naver.com>
2022-10-31 16:51:26 +0900

    feat: add function of calculating bill counts when withdrawing money as bill in large order

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  const bills = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for (const bill of bills) {
+    const billCnt = Math.trunc(money / bill);
+    const remains = money % bill;
+    answer.push(billCnt);
+    money = remains;
+  }
   return answer;
 }
 
