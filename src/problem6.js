ba046accc31055e056560b27c25eb2027e8eec04
sein <hyuntpdls@gmail.com>
2022-10-31 16:15:30 +0900

    [문제6] feat: 중복 검사를 위해 문자열을 2글자씩 나누기

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,17 @@ function problem6(forms) {
   return answer;
 }
 
+// 문자열을 2글자씩 나누기
+function separateString(string) {
+  let array = [];
+
+  for (let i = 0; i < string[1].length - 1; i++) {
+    array.push(string[1].substr(i, 2));
+  }
+
+  console.log(array);
+
+  return array;
+}
+
 module.exports = problem6;
