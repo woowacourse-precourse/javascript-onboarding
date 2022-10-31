03112bd892c6d6df2a60814d48ea16901a38357a
하송희 <99heesong@naver.com>
2022-10-31 23:17:51 +0900

    [문제 6] 기능추가: 닉네임 중복 검사하며 겹치는 배열의 email 새 배열로 이동

--- src/problem6.js
+++ src/problem6.js
@@ -1,9 +1,18 @@
 function problem6(forms) {
   const newForms = forms.map((arr) => ({ email: arr[0], nickname: arr[1] }));
 
-  for(let i=0; i<obj.nickname.length-1; i++) {
-    const str = obj.nickname[i] + obj.nickname[i+1];
-  }
+  const duplStrArr = [];
+  newForms.forEach((obj, idx) => {
+    for(let i=0; i<obj.nickname.length-1; i++) {
+      const str = obj.nickname[i] + obj.nickname[i+1];
+      for(let j=0; j<newForms.length; j++) {
+        if(j===idx) continue;
+        if(newForms[j].nickname.includes(str)) {
+          duplStrArr.push(newForms[j].email);
+        }
+      }
+    }
+  });
 }
 
 module.exports = problem6;
