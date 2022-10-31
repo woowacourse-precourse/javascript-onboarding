68e64f936357dc29fcfcc31e6168e3321ec1b590
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 22:19:12 +0900

    [Feat:Problem7] 유저의 친구 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,12 @@ const compose =
       fns.reduce((composed, fn) => fn(composed), initialValue);
 
 const isNotExisting = (array, item) => array.indexOf(item) === -1;
+
+const getUserBfs = (user) => (friends) => {
+  return friends.reduce((reduced, couple) => {
+    const copy = reduced.slice();
+    const [_, bf] = couple.indexOf(user) === 0 ? couple : [...couple].reverse();
+    if (isNotExisting(copy, bf)) copy.push(bf);
+    return copy;
+  }, []);
+};
