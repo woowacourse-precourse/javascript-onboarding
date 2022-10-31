6e310fbc40a5b81d535c642fccfd53c2146eaff4
Azamman327 <wjsdbdud327@naver.com>
2022-11-01 06:31:36 +0900

    Docs: [P6] 기능 목록 정리

--- src/problem6.js
+++ src/problem6.js
@@ -4,3 +4,11 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
+
+/*
+1. 첫 번째 닉네임부터 2글자씩 substring으로 만들기
+2. substring이 닉네임에 포함되는지 검사
+3. 만약 substring을 포함한다면 중복 닉네임의 이메일을 배열 duplicate에 저장
+
+4. duplicate 정렬 및 출력
+*/
