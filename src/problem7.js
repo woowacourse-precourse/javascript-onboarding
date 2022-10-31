b4232910c0658de5b034b623fd5cf1342fb0f287
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 06:26:19 +0900

    FEAT : problem7 - 유저의 친구 목록을 가져오는 함수 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userFriends = findUserFriends(user, friends);
+}
+
+const findUserFriends = (user, friends) => {
+  return friends
+          .filter(([name1, name2]) => name1 === user || name2 === user)
+          .map(([name1, name2]) => name1 === user ? name2 : name1);
 }
 
 module.exports = problem7;
