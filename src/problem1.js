725145320625ea14f1b2927e396d2ec1524a3bee
su-lim <sujin.0xlim@gmail.com>
2022-11-01 02:15:17 +0900

    style: [prob1] 세미콜론 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,26 +1,26 @@
 function validCheck(left, right){
   if(left % 2 !== 1 || right % 2 !== 0){
-    return false
+    return false;
   }
   if(right - left !== 1){
-    return false
+    return false;
   }
-  return true
+  return true;
 }
 
 function calcScore(left, right){
-  leftSum = 0
-  leftMul = 1
-  rightSum = 0
-  rightMul = 1
+  leftSum = 0;
+  leftMul = 1;
+  rightSum = 0;
+  rightMul = 1;
 
   for(let i = left; i > 0; i = parseInt(i / 10)){
-    leftSum += i%10
-    leftMul *= i%10
+    leftSum += i%10;
+    leftMul *= i%10;
   }
   for(let i = right; i > 0; i = parseInt(i / 10)){
-    rightSum += i%10
-    rightMul *= i%10
+    rightSum += i%10;
+    rightMul *= i%10;
   }
   
   return Math.max(leftSum, leftMul, rightSum, rightMul);
@@ -30,12 +30,12 @@ function problem1(pobi, crong) {
   var answer;
   
   if(!(validCheck(pobi[0], pobi[1]) && validCheck(crong[0], crong[1]))){
-    answer = -1
-    return answer
+    answer = -1;
+    return answer;
   }
-  pobiScore = calcScore(pobi[0], pobi[1])
-  crongScore = calcScore(crong[0], crong[1]) 
-  answer = pobiScore > crongScore ? 1 : pobiScore === crongScore ? 0 : 2
+  pobiScore = calcScore(pobi[0], pobi[1]);
+  crongScore = calcScore(crong[0], crong[1]); 
+  answer = pobiScore > crongScore ? 1 : pobiScore === crongScore ? 0 : 2;
 
   return answer;
 }
