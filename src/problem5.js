2f89a63aa892ef14eaa9bfee06ce3b01b96684e9
100Gyeon <bjy9966@naver.com>
2022-10-31 17:35:43 +0900

    refactor: 선언형 프로그래밍 방식으로 리팩토링

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,10 @@
 function problem5(money) {
   const unitList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const answerList = new Array(unitList.length).fill(0);
-  let index = 0;
-
-  while (money > 0) {
-    const count = Math.floor(money / unitList[index]);
-    answerList[index] = count;
-    if (count) money -= unitList[index] * count;
-    index++;
-  }
+  const answerList = unitList.map((unit) => {
+    const count = Math.floor(money / unit);
+    money -= count * unit;
+    return count;
+  });
 
   return answerList;
 }
