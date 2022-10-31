74ff9b09e79320f256465a2d64d6130f000e1be7
sein <hyuntpdls@gmail.com>
2022-10-31 16:25:35 +0900

    [문제6] console.log() 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -17,11 +17,9 @@ function checkDuplication(forms) {
 
   // 중복 검사
   let result = checkDuplicationNickName(test_slice, forms, tmp);
-  console.log(result);
 
   // set을 배열로 변환
   let duplicationResult = setToArray(result);
-  console.log(setToArray(result));
 
   // 오름 차순 정렬
   return duplicationResult.sort();
@@ -39,7 +37,6 @@ function checkDuplicationNickName(test_slice, forms, tmp) {
     for (let j = 0; j < test_slice[i].length; j++) {
       obj.name = forms[i][0];
       obj.dup = checkArray(i, test_slice[i][j], tmp);
-      console.log(obj);
       // dup에 값이 있으면 이름 저장해라
       if (obj["dup"] !== undefined) {
         result.add(obj.name);
@@ -53,7 +50,6 @@ function checkDuplicationNickName(test_slice, forms, tmp) {
 function checkArray(i, forms, array) {
   for (let j = 0; j < array[i].length; j++) {
     if (forms[1].includes(array[i][j]) === true) {
-      console.log(forms[1]);
       return forms[0];
     }
   }
@@ -65,8 +61,6 @@ function separateString(string) {
   for (let i = 0; i < string[1].length - 1; i++) {
     array.push(string[1].substr(i, 2));
   }
-
-  console.log(array);
 
   return array;
 }
