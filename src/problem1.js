ea722e11ece415905ec938abbc09eccf18352825
Sangjun-man <molung@icloud.com>
2022-10-31 14:36:46 +0900

    refactor : problem 1 리팩토링

--- src/problem1.js
+++ src/problem1.js
@@ -5,24 +5,32 @@
 
 // #2
 // 테스트코드를 작성하는 이유가 있구나 여러 예외에 대해 생각해보고 빠르게 확인하여 수정할 수 있구나
-function problem1(pobi, crong) {
-  var answer;
 
+function problem1(pobi, crong) {
   //포비 크롱 중 예외사항 발생할 경우 -1 리턴
   if (isExceptCase(pobi) || isExceptCase(crong)) return -1;
 
   //최대값 확인
   const pobiMax = Math.max(splitAndSum(pobi), splitAndMultiply(pobi));
   const crongMax = Math.max(splitAndSum(crong), splitAndMultiply(crong));
-  answer = compare({ pobi: pobiMax, crong: crongMax });
 
-  return answer;
+  //포비와 크롱의 최대값을 비교한 뒤 리턴
+  if (pobiMax > crongMax) return 1;
+  else if (pobiMax < crongMax) return 2;
+  else return 0;
 }
-const isExceptCase = (arr) => {
-  // array에 제대로 된 값이 아닐 경우 false 리턴
-  if (!arr.length || arr.length !== 2) return true;
 
-  const [a, b] = arr.map(Number);
+/**
+ * 첵페이지 배열이 예외상황인지 아닌지 체크하는 함수,
+ *
+ * @param {[number,number]} bookarr 책의 [앞페이지, 뒷페이지] 정보를 파라미터로 받음
+ * @return {boolean}
+ */
+function isExceptCase(bookarr) {
+  // array에 제대로 된 값이 아닐 경우 false 리턴
+  if (!bookarr.length || bookarr.length !== 2) return true;
+
+  const [a, b] = bookarr.map(Number);
   //앞페이지가 짝수거나 뒷페이지가 홀수일때
   if (a % 2 === 0 || b % 2 === 1) return true;
   //앞페이지, 뒷페이지 모두 0보다 작을때
@@ -36,32 +44,45 @@ const isExceptCase = (arr) => {
   // 입력값이 잘못되었을때
   // 모두 통과하면 예외사항 아니므로 false 반환\
   return false;
-};
+}
 
-const splitToNumArray = (int) => {
-  const strFromInt = int.toString();
+/**
+ * 숫자를 받아서 자리수를 기준으로 나눠서 배열로 만들어주는 함수.
+ * ex) 100 => [1,0,0] 1234 => [1,2,3,4]
+ *
+ * @param {number} num
+ * @returns {number[]}
+ */
+function splitToNumArray(num) {
+  const strFromInt = num.toString();
   return strFromInt.split("").map(Number);
-};
+}
 
-const splitAndSum = (arr) => {
-  const [a, b] = arr;
+/**
+ * 앞페이지와 뒷 페이지의 각 자리의 숫자값을 더했을 때 더 큰 값을 리턴하는 함수.
+ *
+ * @param {[number,number]} bookArr
+ * @returns {number} max
+ */
+function splitAndSum(bookArr) {
+  const [a, b] = bookArr;
   let sumA = splitToNumArray(a).reduce((prev, cur) => (prev += cur));
   let sumB = splitToNumArray(b).reduce((prev, cur) => (prev += cur));
 
   return Math.max(sumA, sumB);
-};
+}
 
-const splitAndMultiply = (arr) => {
-  const [a, b] = arr;
+/**
+ * 앞페이지와 뒷 페이지의 각 자리의 숫자값을 곱했을 때 더 큰 값을 리턴하는 함수.
+ *
+ * @param {[number,number]} bookArr
+ * @returns {number} max
+ */
+function splitAndMultiply(bookArr) {
+  const [a, b] = bookArr;
   let mulA = splitToNumArray(a).reduce((prev, cur) => (prev *= cur));
   let mulB = splitToNumArray(b).reduce((prev, cur) => (prev *= cur));
   return Math.max(mulA, mulB);
-};
-
-const compare = ({ pobi, crong }) => {
-  if (pobi > crong) return 1;
-  else if (pobi < crong) return 2;
-  else return 0;
-};
+}
 
 module.exports = problem1;
