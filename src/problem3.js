a7fbcb10c416e287df10718666ff8cd155167831
Jiwon <jiwony933@naver.com>
2022-10-31 22:53:26 +0900

    문제 3번 해결 : 삼육구 게임

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    var answer;
+    let allNumArr = [];
+    for (let i = 1; i <= number; i++) {
+        allNumArr.push(i);
+    }
+
+    const clapCondition = (el) =>
+        el.includes("3") || el.includes("6") || el.includes("9");
+
+    answer = allNumArr
+        .toString()
+        .split("")
+        .filter((el) => clapCondition(el)).length;
+
+    return answer;
 }
 
 module.exports = problem3;
