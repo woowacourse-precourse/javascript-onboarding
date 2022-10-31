5225c75dadaab778365ea7bcfd3b9dcd6d0b3d80
River <swimmingr@gmail.com>
2022-10-31 09:35:28 +0900

    해당 숫자의 3,6,9 개수를 확인

--- src/problem3.js
+++ src/problem3.js
@@ -5,9 +5,28 @@ function toNum(num) {
   }
   return nArr;
 }
+function counting(num) {
+  let count = 0;
+  let arr = [];
+ 
+  while (num > 1) {
+    arr.push(parseInt(num % 10, 10));
+    num /= 10;
+  }
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] === 3 || arr[i] === 6 || arr[i] === 9) {
+      count++;
+    }
+  }
+  return count;
+}
 
 function problem3(number) {
   var answer;
+  if(number>=1 || number<=10){
+
+  }
+  else{return null;}
   return answer;
 }
 
