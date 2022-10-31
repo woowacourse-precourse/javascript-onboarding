6daeb17858e7036ba04c92f77f82c631d70bec60
frants-jeon <jjh6963@naver.com>
2022-11-01 03:40:11 +0900

    feat (problem6): 연속된 글자의 갯수를 세어주는 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -10,6 +10,14 @@ class RenamingTargetListMaker {
   constructor(forms) {
     this.forms = forms;
   }
+
+  setDuplicationNickNameCount = (nickName) => {
+    for (let i = 0; i < nickName.length - 1; i++) {
+      const partOfNickName = nickName[i] + nickName[i + 1];
+      this.nickNameChecker[partOfNickName] =
+        (this.nickNameChecker[partOfNickName] || 0) + 1;
+    }
+  };
 }
 
 module.exports = problem6;
