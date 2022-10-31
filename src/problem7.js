176e067e7a2b58a6f3e896f93ca11b1f11506258
Yesong <to06109@naver.com>
2022-10-31 18:54:02 +0900

    [1주차 7번] FEAT: 유저목록 정리 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,29 @@
+let hash = {};
+
 function problem7(user, friends, visitors) {
   var answer;
+
+  arrangeFriends();
+
   return answer;
+
+  // 유저목록 정리
+  function arrangeFriends() {
+    // friends 배열
+    friends.forEach((friend) => {
+      friend.forEach((person) => {
+        if (person !== user && hash[person] === undefined) {
+          hash[person] = 0;
+        }
+      });
+    });
+    // visitors 배열
+    visitors.forEach((visitors) => {
+      if (hash[visitors] === undefined) {
+        hash[visitors] = 0;
+      }
+    });
+  }
 }
 
 module.exports = problem7;
