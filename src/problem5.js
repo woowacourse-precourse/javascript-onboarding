d9ee15913c616da7b5e7a73203b1b838eccbf3af
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-01 02:08:41 +0900

    Feat: 4번- solution 메서드 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,11 +2,17 @@
 function divideNum(num, mon) {
   let quo = parseInt(num / mon);
   let left = num % mon;
+  return [quo, left];
 }
 
 function problem5(money) {
-  let answer;
-  
+  let answer = [];
+  const arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for(i of arr) {
+    let [resultQuo, resultLeft] = divideNum(money, i)
+    answer.push(resultQuo);
+    money = resultLeft;
+  }
 
   return answer;
 }
