2c03beb739cb0758184b0fb0e7dc50f1bc89159f
daal2quf <daal2quf@gmail.com>
2022-11-01 01:18:33 +0900

    리팩토링: 예외처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,17 +1,21 @@
 function problem1(pobi, crong) {
   var answer;
 
-  if(pobi[0] % 2 === 0 || crong[0] % 2 === 0 || pobi[1] - pobi[0] != 1 || crong[1] - crong[0] != 1 ||
-     pobi[0] === 1 || crong[0] === 1 || pobi[1] === 400 || crong[1] === 400) {
-    return -1
-  }
+  if(pobi[0] < 1 || pobi[1] > 400) return -1;
+  if(crong[0] < 1 || crong[1] > 400) return -1;
+
+  if(pobi[1] - pobi[0] !== 1) return -1;
+  if(crong[1] - crong[0] !== 1) return -1;
+
+  if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return -1
+  if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return -1
 
 const bigNum = (arr) => {
   let big;
   let newArr = [];
   for(let i = 0; i < arr.length; i++) {
     const changeNum = (arg) => Number(arg)
-    let num = String(arr[i]).split('').map(changeNum).reduce((acc, cur) => acc + cur)
+    let num = String(arr[i]).split('').map(changeNum).reduce((prev, curv) => prev + curv)
     newArr.push(num)
     num = String(arr[i]).split('').map(changeNum).reduce((prev, curv) => prev * curv)
     newArr.push(num)
