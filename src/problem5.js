dec1f5d50efd74139ba96f0d0ba209da3b7c7873
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 23:32:50 +0900

    feat: [5번 문제] 지폐 종류 담긴 배열

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
+const MONEY_TYPE_ARR = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
-  return answer;
+  return MONEY_TYPE_ARR;
 }
 
 module.exports = problem5;
