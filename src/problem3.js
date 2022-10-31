db6c9f1c8577a42dcad9798a9cbae31ee5fcf92a
망망 <mang@mangmang-ui-MacBookPro.local>
2022-11-01 06:53:25 +0900

    문제3

--- src/problem3.js
+++ src/problem3.js
@@ -4,3 +4,38 @@ function problem3(number) {
 }
 
 module.exports = problem3;
+
+/*
+메서드 : 객체를 정의할 때 프로퍼티 값으로 함수를 선언하는 것으로 만들 수 있음
+const bigtop = {
+  satHi: function() {
+    console.log('Hi');
+  }
+};
+
+const bigtop = {
+  satHi: () => {
+    console.log('Hi');
+  }
+};
+
+const bigtop = {
+  satHi () {
+    console.log('Hi');
+  }
+};
+
+const bigtop = {};
+bigtop.sayHi = function () {
+  console.log('Hi');
+};
+
+메서드 호출
+bittop.sayHi();
+bigtop.satHi;
+bigtop["sayHi"]();
+bigtop["sayHi"];
+bigtop["sayHi"()];
+bigtop[sayHi()];
+
+*/
