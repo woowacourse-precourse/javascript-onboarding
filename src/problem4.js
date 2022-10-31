9d543bb20aad8e50b82c541a992e958d2a629806
afds4567 <afds4567@gmail.com>
2022-10-31 20:10:38 +0900

    feat(problem4): 청개구리 사전 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const BLUE_FROG_DICT = {};
+  const ALPHABET_LENGTH = UPPER_CASE.length;
+
+  for (let i = 0; i < ALPHABET_LENGTH; i++) {
+    BLUE_FROG_DICT[UPPER_CASE[i]] = UPPER_CASE[ALPHABET_LENGTH - (i + 1)];
+  }
+
+  for (let char of word) {
+    answer += BLUE_FROG_DICT[char];
+  }
   return answer;
 }
 
