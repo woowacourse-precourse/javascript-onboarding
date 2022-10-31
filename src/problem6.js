29db1f6fe257830e023eb45566ae78207a350b6f
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:40:08 +0900

    [ feat ] problem6_닉네임 부분 문자열 객체를 만드는 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,6 @@
 // 기능 추가 목록
 // - nickname&email 담은 객체 생성
-// - 닉네임 부분 문자열을 담은 객체 생성
+// - 닉네임 부분 문자열 객체를 만드는 함수 생성
 // - 부분문자열의 길이를 결정하는 함수 생성
 // - 부분문자열 객체 생성하는 함수 실행
 // - 중복되는 닉네임 판별
@@ -8,10 +8,28 @@
 
 function problem6(forms) {
   const nickNameEmail = {};
+  const strPart = {};
 
   forms.forEach((element) => {
     nickNameEmail[element[1]] = element[0];
   });
+
+  function saveString(strLength, nickName) {
+    let start = 0;
+    let end = 0 + strLength;
+    while (end <= nickName.length) {
+      const checkStr = nickName.slice(start, end);
+      if (checkStr in strPart) {
+        strPart[checkStr][0].push(nickName);
+        strPart[checkStr][1] += 1;
+      } else {
+        strPart[checkStr] = [[], 1];
+        strPart[checkStr][0].push(nickName);
+      }
+      start += 1;
+      end += 1;
+    }
+  }
   return answer;
 }
 
