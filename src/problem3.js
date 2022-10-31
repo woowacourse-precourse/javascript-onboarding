fc76ac8d9a236aef1f96960f27b079eb3dbf6d5d
durumi99 <kms990615@naver.com>
2022-10-31 22:39:24 +0900

    [problem 3] 숫자를 입력으로 받아 들어간 3, 6, 9 의 개수를 반환하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,14 @@
+// 해당 num에서 손뼉을 쳐야 하는 횟수
+function count369(num){
+  let cnt = 0;
+  while(num){
+    if(num % 10 == 3 || num % 10 == 6 || num % 10 == 9){
+      cnt++;
+    }
+    num = ~~(num / 10)
+  }
+  return cnt
+}
 function problem3(number) {
   var answer;
   return answer;
