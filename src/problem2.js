bb9c512d374f122eceba0b4511ede38147ea4da2
Sangwon Lee <tkdtm2010@ajou.ac.kr>
2022-11-01 07:46:14 +0900

    2번 초과 연속 값도 제거하도록 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,13 +1,22 @@
 function problem2(cryptogram) {
   var answer;
   let cryptoAry = cryptogram.split("");
+  let count;
 
   for (let i = 0; i < cryptoAry.length; i++) {
-    if (cryptoAry[i] === cryptoAry[i - 1]) {
-      cryptoAry.splice(i - 1, 2);
+    if (cryptoAry[i] == cryptoAry[i + 1]) {
+      count = i + 1;
+      while (cryptoAry[count] === cryptoAry[count + 1]) {
+        count++;
+      }
+      cryptoAry.splice(i, count - i + 1);
       i = 0;
     }
+    if (cryptoAry.length == 2 && cryptoAry[0] === cryptoAry[1]) {
+      cryptoAry = [];
+    }
   }
+  
   answer = cryptoAry.join("");
   return answer;
 }
