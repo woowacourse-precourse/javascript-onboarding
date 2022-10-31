ed770a5d7c2406770a49ac9fd34fb57b39b7926d
sujungYu <su_moment@naver.com>
2022-10-31 15:17:48 +0900

    refactor(PROBLEM3):연산자 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,10 @@
 function problem3(number) {
     let clapNum = 0;
-    const numArr = Array(number).fill().map((v,i)=> i+1);
+    const numArr = Array(number).fill().map((v,i)=> i + 1);
     const splitNumArr = numArr.join('').split('').map(Number);
     for(let i=0; i<splitNumArr.length; i++) {
-        if(splitNumArr[i]==3|splitNumArr[i]==6|splitNumArr[i]==9) {
-            clapNum +=1;
+        if(splitNumArr[i] === 3 | splitNumArr[i] === 6 | splitNumArr[i] === 9) {
+            clapNum += 1;
         }
     }
     return clapNum;
