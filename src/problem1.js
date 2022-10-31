3ba81234afe296ec839afada2bae90d322e04c20
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 08:44:37 +0900

    feat: 필요한 함수 기능 check

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,23 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  // maxNumber 비교 후 점수 return
+  return score;
+}
+
+// 페이지 번호 각 자리 숫자 더하는 함수
+function sumPage(pages) {}
+
+// 페이지 번호 각 자리 숫자 곱하는 함수
+function multiPage(pages) {}
+
+// 각 과정 중 가장 큰 수 return 함수
+function maxNumber(leftPage, rightPage) {}
+
+// 예외처리 함수
+function checkProblem(pages) {
+  // leftPage 홀수, rightPage 짝수
+  // leftPage + 1 = rightPage
+  // leftPage || rightPage > 0
+  // legtPage || rightPage < 401
 }
 
 module.exports = problem1;
