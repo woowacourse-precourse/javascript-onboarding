a927e115caed55bed23ba58ecf67864d68e0339a
kwh97 <zzzang0512@naver.com>
2022-10-31 15:47:44 +0900

    feat(problem3): 숫자를 문자열로 바꾸는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for(let i=1; i<=number; i++) {
+    let str = changeToString(i);
+  }
   return answer;
+}
+
+function changeToString(i) {
+  const strI = String(i);
+  return strI;
 }
 
 module.exports = problem3;
