2600071e4dd5184d3e665e53cfb1e7db4149e2c0
letsjo <hyunoh.jo@gmail.com>
2022-11-01 01:20:39 +0900

    [problem4] Simplify code for readability

--- src/problem4.js
+++ src/problem4.js
@@ -5,37 +5,41 @@ function problem4(word) {
 function treeFrogApp(word){
   const eachCharArr = createStringArr(word);
   for(let i = 0; i < eachCharArr.length; i++){
-    eachCharArr[i] = getTreeFrogChar(eachCharArr[i]);
+    eachCharArr[i] = convertTreeFrogChar(convertUTF16(eachCharArr[i]));
   }
-  return getFromCharArrToString(eachCharArr);
+  return convertString(eachCharArr);
 }
 
 function createStringArr(word){
   return word.split("");
 }
 
-function getFromCharToUTF16Code(char){
-  return char.charCodeAt(0);
-}
-
-function getFromUTF16CodeToChar(utf16Code){
-  return String.fromCharCode(utf16Code);
-}
-
-function getFromCharArrToString(charArr){
-  return charArr.join('');
+function convertTreeFrogChar(utf16Code){
+  return checkAlphabet(utf16Code)?convertChar(convertReverseCode(utf16Code)):convertChar(utf16Code);
 }
 
 function checkAlphabet(utf16Code){
-  return (utf16Code >= 65 && utf16Code <= 90) || (utf16Code <= 122 && utf16Code >= 97);
+  return ( 65 <= utf16Code && utf16Code <= 90) || ( 97 <= utf16Code && utf16Code <= 122);
 }
 
-function getTreeFrogChar(char){
-  let utf16Code = getFromCharToUTF16Code(char);
-  if (checkAlphabet(utf16Code)){
-    utf16Code>=97?utf16Code=219-utf16Code:utf16Code=155-utf16Code;
-  }
-  return getFromUTF16CodeToChar(utf16Code);
+function convertChar(utf16Code){
+  return String.fromCharCode(utf16Code);
+}
+
+function convertReverseCode(utf16Code){
+  return checkLowerCase(utf16Code)?219-utf16Code:155-utf16Code;
+}
+
+function convertUTF16(char){
+  return char.charCodeAt(0);
+}
+
+function convertString(charArr){
+  return charArr.join('');
+}
+
+function checkLowerCase(utf16Code){
+  return utf16Code>=97;
 }
 
 module.exports = problem4;
