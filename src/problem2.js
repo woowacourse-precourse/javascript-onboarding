9d60696a3d603e88f5ada99eda3c05af388fac63
okorion <ok.or.orion@gmail.com>
2022-10-31 23:50:29 +0900

    feat: 중복 문자 삭제 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
+// **기능 요구 사항**
+// 1. 중복 문자 삭제하는 기능
+// 2. 1의 과정을 가능할 때까지 반복할 수 있는 기능
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let crypt = cryptogram;
+  console.log(String(crypt))
+  console.log(crypt.replace(/([a-z])\1{1,}/g, ""));
+// .toString().split('')
+  return crypt;
 }
 
 module.exports = problem2;
+
+problem2("browoanoommnaon");
