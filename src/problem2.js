637500252ed1af73d77718ca7522b8d8cfe0235e
jinlee <vito9270@naver.com>
2022-11-01 05:47:21 +0900

    problem 2 기능 목록 수정 및 예제 통과

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+  let prev = "";
+
+  for (const char of cryptogram) {
+    const lastChar = stack[stack.length - 1];
+
+    if (char !== lastChar && char !== prev) {
+      stack.push(char);
+      prev = char;
+    } else if (char === lastCshar) {
+      stack.pop();
+    }
+  }
+
+  return stack.join("");
 }
 
 module.exports = problem2;
