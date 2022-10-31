d84cef278924cd813004e51601c8abf8597aab84
Siby1lA <psb4644@gmail.com>
2022-10-31 17:05:19 +0900

    feat: [문제 7] 친구인 사람들을 구하고 이를 제외한 친구, 방문자 목록을 배열로 리턴하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
+function getNewMembers(followFriends, list) {
+  let result = [];
+  list.map((member) => {
+    if (!followFriends.includes(member)) {
+      result.push(member);
+    }
+  });
+  return result;
+}
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const followFriends = [
+    ...new Set(friends.filter((list) => list.includes(user)).flat()),
+  ];
+  const newFriends = getNewMembers(followFriends, friends.flat());
+  const newVisitors = getNewMembers(followFriends, visitors);
 }
 
 module.exports = problem7;
