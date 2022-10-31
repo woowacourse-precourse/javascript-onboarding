efdba15df8f693cb9820a8e76cc07694564f287a
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 20:06:47 +0900

    feat: user의 타임라인에 방문한 사람인 경우, 방문 횟수 당 score를 1씩 증가시키는 기능(addScoreForVisiting)

--- src/problem7.js
+++ src/problem7.js
@@ -3,8 +3,8 @@
   [O] friends 배열에서 주어지는 사람들의 친구 리스트를 Map 객체(key: 이름, value: friends 배열)로 생성하는 기능 | 함수명 : initFriendsList
   [O] frineds 배열어서 주이지는 사람들의 score 리스트를 Map 객체(key: 이름, value: score)로 생성하는 기능 | 함수명 : initScoreList
   [O] 해당 사람이 user의 friend인지 확인하는 기능 | 함수명 : isFriendWithUser
-  [O] user와 함께 아는 친구인 경우가 있는 사람인 경우, 아는 친구 한명 당 score를 10씩 증가시키는 기능 | 함수명 : addScoreForMutualFrien
-  [ ] user의 타임라인에 방문한 사람인 경우, 방문 횟수 당 score를 1씩 증가시키는 기능
+  [O] user와 함께 아는 친구인 경우가 있는 사람인 경우, 아는 친구 한명 당 score를 10씩 증가시키는 기능 | 함수명 : addScoreForMutualFriend
+  [O] user의 타임라인에 방문한 사람인 경우, 방문 횟수 당 score를 1씩 증가시키는 기능 | 함수명 : addScoreForVisiting
   [ ] Map객체를 배열로 convert하는 기능
   [ ] score가 0일 때 리스트에서 제거하는 기능
   [ ] 추천 친구를 점수가 가장 높은 순, 점수가 같은 경우 이름 순으로 정렬하는 기능
@@ -30,6 +30,10 @@ function problem7(user, friends, visitors) {
   for (const person of scoreList.keys()) {
     addScoreForMutualFriend(friendsList, scoreList, userFriendsList, person);
   }
+
+  visitors.forEach((visitor) => {
+    addScoreForVisiting(scoreList, userFriendsList, visitor);
+  });
 }
 
 function initFriendsList(friendsList, person1, person2) {
@@ -57,6 +61,15 @@ function addScoreForMutualFriend(friendsList, scoreList, userFriendsList, person
       scoreList.set(person, scoreList.get(person) + 10);
     }
   });
+}
+
+function addScoreForVisiting(scoreList, userFriendsList, visitor) {
+  if (isFriendWithUser(userFriendsList, visitor)) return;
+  if (!scoreList.has(visitor)) {
+    scoreList.set(visitor, 1);
+  } else {
+    scoreList.set(visitor, scoreList.get(visitor) + 1);
+  }
 }
 
 module.exports = problem7;
