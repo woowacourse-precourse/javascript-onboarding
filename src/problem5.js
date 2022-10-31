c6102acf176314121a29e11ae3622f2a44e31c0b
sein <hyuntpdls@gmail.com>
2022-10-31 16:12:22 +0900

    [문제5] feat: 돈을 각 단위로 나누어 계산하기

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
   return answer;
+}
+
+// 돈을 각 단위로 나누어 몫과 나머지 구함 
+function calculationMoney(money, unit) {
+  let quotient = parseInt(money / unit);
+  money = money % unit;
+  return { money, quotient };
 }
 
 module.exports = problem5;
