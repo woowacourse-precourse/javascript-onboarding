e7ee664bb04d7cde45eb57c2c07faf01b02a319e
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 06:51:28 +0900

    problem1 compare numbers

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,30 @@
 function problem1(pobi, crong) {
-  var answer;
+  var answer = solution(pobi, crong);
   return answer;
+}
+
+function solution(pobi, crong) {
+  if (!checkValid(pobi) || !checkValid(crong)) {
+    return -1;
+  }
+  
+
+
+  return null;
+}
+
+function checkValid(array) {
+  return array[0]+1 == array[1];
+}
+
+
+
+function compareNum(a, b) {
+  if (a >= b) {
+    return a;
+  } else {
+    return b;
+  }
 }
 
 module.exports = problem1;
