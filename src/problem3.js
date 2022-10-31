78fd067cd1d536c5770926e1d2ba4b92908f57e7
leesukkyeong <104298444+leesukkyeong@users.noreply.github.com>
2022-10-31 10:54:38 +0900

    2-7

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
+  var answer = clap(number);
   return answer;
 }
 
-module.exports = problem3;
+function clap(number){
+  let count = 0;
+  let num;
+  for(let i = 1;i<=number;i++){
+    num = i;
+    while(num!=0){
+      if(num%10==3||num%10==6||num%10==9){
+        count++;
+      }
+      num = Math.floor(num/10);
+    }
+  }
+  return count;
+}
--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,49 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var arr = new Map();
+  var answer = []
+  let closeFreind = findCloseFriends(user,friends);
+  arr = scoring(friends,user,visitors,closeFreind);
+  arr = [...arr];
+  arr.sort((a,b)=>b[0]-a[0]);
+  for(let i = 0;i<arr.length;i++){
+    answer.push(arr[i][0]);
+  }
   return answer;
 }
 
-module.exports = problem7;
+function findCloseFriends(user,friends){
+  let result = [];
+  for(let i = 0;i<friends.length;i++){
+    if(friends[i].includes(user)){
+      result.push(friends[i].filter((element)=>element!==user));
+    }
+  }
+  return result;
+}
+function scoring(friends,user,visitors,array){
+  let result = new Map();
+  for(let friend of array){
+    for(let i=0;i<friends.length;i++){
+      if(friends[i].includes(friend[0])&& !friends[i].includes(user)){
+        let name = friends[i].filter((element)=>element!==friend[0]);
+        if(!result.has(name)){
+          result.set(name[0],10);
+        }
+      }
+    }
+  }
+  for(let visit of visitors){
+    if(result.has(visit)){
+      result.set(visit,result.get(visit)+1);
+    }
+    else{
+      result.set(visit,1);
+    }
+  }
+  for(let closeFreind of array){
+    if(result.has(closeFreind[0])){
+      result.delete(closeFreind[0]);
+    }
+  }
+  return result;
+}
