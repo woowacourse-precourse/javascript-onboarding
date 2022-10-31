e7d499d9791d79c1c0762394e905f0e77ea5c92f
soomin <qkrtnals970804@gmail.com>
2022-11-01 04:24:33 +0900

    [problem4] 문자가 영어인지 판별하는 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,25 @@
+//영어인지 판별하는 함수
+const checkEng = (w, isEng) => {
+  return isEng.test(w);
+}
+
 function problem4(word) {
-  var answer;
+  let answer = "";
+  const isEng = /[a-zA-Z]/;
+  const alphabet = [
+    'A', 'B', 'C', 'D', 'E',
+    'F', 'G', 'H', 'I', 'J',
+    'K', 'L', 'M', 'N', 'O',
+    'P', 'Q', 'R', 'S', 'T',
+    'U', 'V', 'W', 'X', 'Y',
+    'Z'
+  ];
+  for (let w of word) {
+    //영어인지 판별하기
+    if (!checkEng(w)) {
+      answer += w
+    }
+  }
   return answer;
 }
 
