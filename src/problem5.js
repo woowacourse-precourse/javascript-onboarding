9bbbbe6b58fb981391de5a1e04cdc6e8b05066b4
“somm12” <sominan8744@gmail.com>
2022-10-31 18:07:20 +0900

    각 화폐 및 동전이 몇 개로 변환되는지 구하기 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < coins.length; i++) {
+    answer.push(Math.floor(money / coins[i]));
+    money = money % coins[i];
+  }
+  console.log(answer);
   return answer;
 }
-
+problem5(15000);
 module.exports = problem5;
