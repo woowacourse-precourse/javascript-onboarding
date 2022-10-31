c782420874e2db532ed321d604cb9ab584477eaa
JongHun <minn4072@gmail.com>
2022-10-31 23:41:02 +0900

    user, user 친구는 제외한다.

--- src/problem7.js
+++ src/problem7.js
@@ -3,7 +3,8 @@
 // 2. 친구 관계를 map 형태로 저장한다.
 // 3. user 와 친구인 친구들에게 10점씩 부여한다.
 // 4. 방문자에게 1점씩 부여한다
-// 5. 점수순 최대 5명 리턴 -> 점수 0 제외 -> 동일한 점수 이름순 정렬
+// 5. user, user 친구는 제외한다.
+// 6. 점수순 최대 5명 리턴 -> 점수 0 제외 -> 동일한 점수 이름순 정렬
 
 function problem7(user, friends, visitors) {
   let answer;
@@ -34,6 +35,16 @@ function problem7(user, friends, visitors) {
     for (const j of fridens_relation.get(i)) {
       recommend_points.set(j, recommend_points.get(j) + 10);
     }
+  }
+
+  // 방문자에게 1점씩 부여한다.
+  for (const i of visitors) {
+    recommend_points.set(i, recommend_points.get(i) + 1);
+  }
+
+  // user, user 친구는 제외한다.
+  for (const i of [...fridens_relation.get(user), user]) {
+    recommend_points.delete(i);
   }
 
   console.log(recommend_points);
