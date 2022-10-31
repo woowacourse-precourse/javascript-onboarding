eb7a3fa60c4d805775f0d428c0e063ca96486a54
afds4567 <afds4567@gmail.com>
2022-10-31 21:21:14 +0900

    feat(problem5): 전체 로직 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = Array.from({ length: MONEY_UNIT.length }, () => 0);
+  let rest = money;
+
+  MONEY_UNIT.map((unit, idx) => {
+    answer[idx] = Math.floor(rest / unit);
+    rest %= unit;
+  });
   return answer;
 }
 
