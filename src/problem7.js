75fa074d5dfff0e21820a80bd99e32cc1ad6e84e
insu12021202 <insu12021202@gmail.com>
2022-10-31 23:53:50 +0900

    Feat:problem7 구현 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,6 @@
 function problem7(user, friends, visitors) {
-  userFriend = new Array();
+  let userFriend = [];
+  let friendScore = {};
   //friends 배열에서 사용자와 친구인 닉네임 찾기
   friends.map((item) => {
     if (item.includes(user)) {
@@ -10,19 +11,36 @@ function problem7(user, friends, visitors) {
       }
     }
   });
+  friends.map((item) => {
+    userFriend.map((friend) => {
+      if (item.includes(friend) && !item.includes(user)) {
+        if (item[0] === friend && !userFriend.includes(item[1])) {
+          if (!friendScore[item[1]]) {
+            friendScore[item[1]] = 10;
+          } else {
+            friendScore[item[1]] += 10;
+          }
+        } else if (item[1] === friend && !userFriend.includes(item[0])) {
+          if (!friendScore[item[0]]) {
+            friendScore[item[0]] = 10;
+          } else {
+            friendScore[item[0]] += 10;
+          }
+        }
+      }
+    });
+  });
 
-  console.log(userFriend);
+  visitors.map((name) => {
+    if (!userFriend.includes(name)) {
+      if (!friendScore[name]) {
+        friendScore[name] = 1;
+      } else {
+        friendScore[name] += 1;
+      }
+    }
+  });
+  return Object.keys(friendScore);
 }
-problem7(
-  "mrko",
-  [
-    ["donut", "andole"],
-    ["donut", "jun"],
-    ["donut", "mrko"],
-    ["shakevan", "andole"],
-    ["shakevan", "jun"],
-    ["shakevan", "mrko"],
-  ],
-  ["bedi", "bedi", "donut", "bedi", "shakevan"]
-);
+
 module.exports = problem7;
