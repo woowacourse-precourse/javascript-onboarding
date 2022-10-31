636d44c6f8cc40891d8e77250bff5629dc5bf9bc
Yoo Seung Guk <dbtmd878@naver.com>
2022-10-31 22:33:20 +0900

    refactor: 코드 리펙토링

--- src/problem3.js
+++ src/problem3.js
@@ -1,15 +1,25 @@
+function checkThreeNumber(number) {
+  return number % 10 === 3 || number % 10 === 6 || number % 10 === 9
+    ? true
+    : false;
+}
+
+function clapCount(number) {
+  let clapCount = 0;
+  while (number > 2) {
+    if (checkThreeNumber(number)) {
+      clapCount++;
+    }
+    number = parseInt(number / 10);
+  }
+  return clapCount;
+}
+
 function problem3(number) {
   let count = 0;
-  for (let i = 2; i <= number; i++) {
-    let num = i;
-    while (num > 2) {
-      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
-        count++;
-      }
-      num = parseInt(num / 10);
-    }
+  for (let countNumber = 2; countNumber <= number; countNumber++) {
+    count += clapCount(countNumber);
   }
-
   return count;
 }
 
