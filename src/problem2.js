fb3026d2a5bc7e0b8272c9e96e1168e66e427a3c
kwh97 <zzzang0512@naver.com>
2022-10-31 15:37:29 +0900

    feat(problem2): 문제2 연속하는 중복문자를 삭제하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let cryptogramArr = [...cryptogram];
+  let str;
+
+  for(let i=0; i<cryptogramArr.length; i++) {
+    if (str === cryptogramArr[i]) {
+      cryptogramArr.splice(i - 1, 2);
+      i = 0;
+      str = "";
+    }
+    str = cryptogramArr[i];
+  }
+
+  return cryptogramArr.join("");
 }
 
 module.exports = problem2;
