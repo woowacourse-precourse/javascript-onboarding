a1afb9fb868a4fdb392feb209eb7df7cbbe65c98
boyon <bo_yon_99@naver.com>
2022-10-31 22:25:37 +0900

    problem5

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -6,8 +6,12 @@ const problem5 = require("../src/problem5");
 const problem6 = require("../src/problem6");
 const problem7 = require("../src/problem7");
 
-describe("problem4", () => {
+describe("problem5", () => {
   test("case1", () => {
-    expect(problem4("I love you")).toEqual("R olev blf");
+    expect(problem5(50237)).toEqual([1, 0, 0, 0, 0, 2, 0, 3, 7]);
+  });
+
+  test("case2", () => {
+    expect(problem5(15000)).toEqual([0, 1, 1, 0, 0, 0, 0, 0, 0]);
   });
 });
