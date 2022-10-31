82a50a9e24443a2752a240113d7d85c1b7493382
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 04:30:10 +0900

    FEAT : problem2 - 연속된 값을 제거하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -3,7 +3,16 @@ function problem2(cryptogram) {
   let cryptogramArray = Array.from(cryptogram);
   // index + 1의 값을 환인할 변수 / 연속된 값이 있는지 없는지 확인할 수 있는 키로 적용
   let tmpIndex = null;
-  
+
+  // 배열을 하나씩 돌면서 연속된 값을 제거한다.
+  cryptogramArray = cryptogramArray.map((ele, index) => {
+    if(tmpIndex === index) return false;
+    if(ele === cryptogramArray[index+1]) {
+      tmpIndex = index+1;
+      return false;
+    }
+    else return ele;
+  }).filter((ele) => ele);
 }
 
 module.exports = problem2;
