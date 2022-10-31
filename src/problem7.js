c6e83139f5a6fb241a28eb7a7ebf6e3c44734203
semnil5202 <semnil5202@gmail.com>
2022-11-01 00:35:22 +0900

    [문제 7번] feat: 사용자와 친구 관계인 아이디 확인하여 배열로 반환

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,13 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function getUsersFriend(user, friends) {
+  const userFriend = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] === user) userFriend.push(friends[i][1]);
+    if (friends[i][1] === user) userFriend.push(friends[i][0]);
+  }
+  return userFriend;
+}
+
 module.exports = problem7;
