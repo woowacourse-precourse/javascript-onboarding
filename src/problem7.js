12f3800f37f5edcafb539f5c0ca5516f00ebd1fd
Jiwoo Park <jiwoo9446@naver.com>
2022-10-31 21:35:55 +0900

    refactor:delete console problem7

--- src/problem7.js
+++ src/problem7.js
@@ -2,6 +2,7 @@ function problem7(user, friends, visitors) {
   let map = new Map();
   let set = new Set();
   let answer = [];
+
   for (let i = 0; i < friends.length; i++) {
     if (friends[i].includes(user)) {
       if (friends[i][0] === user) set.add(friends[i][1]);
@@ -10,7 +11,6 @@ function problem7(user, friends, visitors) {
   }
 
   let direct = Array.from(set);
-  console.log(direct);
   for (let friend of friends) {
     const [A, B] = friend;
     if (direct.includes(A) && B !== user && !direct.includes(B))
@@ -18,7 +18,6 @@ function problem7(user, friends, visitors) {
     else if (direct.includes(B) && A !== user && !direct.includes(A))
       map.set(A, map.get(A) + 10 || 10);
   }
-  console.log(map);
 
   visitors.forEach((v) => {
     if (!direct.includes(v) && v !== user) map.set(v, map.get(v) + 1 || 1);
