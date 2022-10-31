8e37a421b06dc0c6e6e8a0a32cfd7fb23b8269e0
dahlia <wlstnrdahlia@gmail.com>
2022-11-01 00:25:29 +0900

    Add : problem4 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,20 @@
 function problem4(word) {
   var answer;
+  answer = "";
+
+  function lowerUpper(idx) {
+    let char = word.charCodeAt(idx);
+    if ((char >= 65 && char <= 90)) {
+      char = 155 - char;
+    } else if (char >= 97 && char <= 122) {
+        char = 219 - char;
+      }
+      return char
+    }
+
+  for (let idx = 0; idx < word.length; idx++) {
+      answer += String.fromCharCode(lowerUpper(idx))
+  }
   return answer;
 }
 
