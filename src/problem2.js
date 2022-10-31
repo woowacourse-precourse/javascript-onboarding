c56c8a1d6028fa0b917620ab8b72fd72b1cd516c
j <yejinkwon0928@gmail.com>
2022-10-31 16:29:34 +0900

    problem2 update

--- src/problem2.js
+++ src/problem2.js
@@ -1,17 +1,17 @@
 function problem2(cryptogram) {
   var answer=[];
-  for(var i = 0;i<str.length;i++){
+  for(var i = 0;i<cryptogram.length;i++){
     if(answer.length === 0) {
-      answer.push(str[i]);
+      answer.push(cryptogram[i]);
     }else{
-      if(answer[answer.length-1] != str[i]) {
-        answer.push(str[i]);
+      if(answer[answer.length-1] != cryptogram[i]) {
+        answer.push(cryptogram[i]);
       }else{
         answer.pop();
       }
     }
   }
-  return answer;
+  return answer.join('');
 }
 
 module.exports = problem2;
