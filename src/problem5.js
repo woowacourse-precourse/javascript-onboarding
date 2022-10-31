980dd5de46dab15792bf9d8916271e74544323c5
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-01 02:47:01 +0900

    feat: Add money unit array

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
-  var answer;
+  let answer;
   return answer;
 }
+
+function quotientMoney(money) {
+  let moneyUnitArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  console.log(moneyUnitArray);
+}
+
+console.log(quotientMoney(50327));
+
+// console.log(problem5(50237));
 
 module.exports = problem5;
