c4041323b6475dc59ff57e16fa75146aa60cde3b
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 02:59:24 +0900

    Feat: problem2 -  중복 체크 반복문 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
 function problem2(cryptogram) {
-  const words = [...cryptogram];
+  const duplicationCheck = [...cryptogram];
   let answer = "";
+
+  for (let i = 0; i < duplicationCheck.length; i++) {
+    duplicationCheck[i] === duplicationCheck[i + 1]
+      ? i++
+      : (answer += duplicationCheck[i]);
+  }
 }
+
 module.exports = problem2;
