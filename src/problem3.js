123cbccb3c154cdbe72ade5d788828c4d51cbde9
MINJUN PARK <mj9457@naer,com>
2022-10-31 22:52:05 +0900

    Feat. week1 problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for(var i=1; i<=number; i++) {
+      var str = String(i);
+      for(var j=0; j<str.length; j++) {
+          if(str[j] == 3 || str[j] == 6 || str[j] == 9) {
+              answer += 1;
+          }
+      }
+  }
   return answer;
 }
 
