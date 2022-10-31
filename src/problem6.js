203dd8506466803f8fdc1492320dee8113682a17
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-31 16:14:41 +0900

    refactor: [P6] 이메일을 담을 Array와 가능한 케이스를 담을 Object 생성
    - 중복된 단어가 닉네임에 있는 크루의 이메일을 담을 Array 만들기
    - 다른 크루 닉네임 속 단어와의 중복 여부를 판단하기 위한 Object 만들기

--- src/problem6.js
+++ src/problem6.js
@@ -1,42 +1,6 @@
 function problem6(forms) {
-  let email = []
-  let repeatedWords = getRepeatedWords(forms)
-  for (let word of repeatedWords) {
-      forms.forEach((form) => {
-          if (formAnalyzer(form).has(word)) email.push(formAnalyzer(form).get(word))
-      })
-  }
-  let removeRepeatedEmail = new Set([...email])
-  return [...removeRepeatedEmail].sort()
-}
-
-function getRepeatedWords(forms) {
-  let wordFrequencyCount = {}
-  let repeatedWords = []
-  forms.forEach((form) => {
-      let words = formAnalyzer(form).keys()
-      for (let word of words) {
-          wordFrequencyCount[word] ? wordFrequencyCount[word] += 1 : wordFrequencyCount[word] = 1
-      }
-  })
-  for (let word in wordFrequencyCount) {
-      if (wordFrequencyCount[word] > 1) repeatedWords.push(word)
-  }
-  return repeatedWords
-}
-
-function formAnalyzer(form) {
-  const getSubstring = (nickname) => {
-      let substring = []
-      for (let i = 0; i < nickname.length - 1; i++) {
-          substring.push(nickname.substring(i, i + 2))
-      }
-      return new Set(substring)
-  }
-  let email = form[0]
-  let matchingEmail = new Map()
-  getSubstring(form[1]).forEach((word) => matchingEmail.set(word, email))
-  return matchingEmail
-}
+  let targetEmail = []
+  let wordCase = {}
+} 
 
 module.exports = problem6;
