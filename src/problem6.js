17c8ad21f86709b28d30126a157cd37d1f415357
gyeongza <gyeongza@naver.com>
2022-10-31 22:00:56 +0900

    feat: 이메일(key), 닉네임(value)를 각각의 배열에 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+//이메일(key), 닉네임(value)를 각각의 배열에 반환
+const createArr = (data) => {
+  const emailArr = [...new Map(data).keys()]
+  const nicknameArr = [...new Map(data).values()]
+  const checkArr = new Array(data.length)
 }
+ 
+// function problem6(forms) {
+//   return createArr(forms)
+// }
 
-module.exports = problem6;
+
+// // module.exports = problem6;
