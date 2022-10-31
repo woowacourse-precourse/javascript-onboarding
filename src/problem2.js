9e6108cae1891aeabcf5829e4a45197986f53eaf
OrangeVinyl <hap_hazard@naver.com>
2022-11-01 02:57:01 +0900

    Problem2) 문제 해결 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,21 @@
 function problem2(cryptogram) {
-  var answer = [cryptogram[0]]; // cryptogram을 배열로 입력 받아 answer에 대입
-  for (var i = 1; i < cryptogram.length; i++) { // 배열의 인덱스를 변경하며 비교
-    var top = answer[answer.length - 1]; //stack의 top을 지정 및 변경
-    if (top !== cryptogram[i]) { // stack내의 top에 위치한 내역과 
+  var answer = [cryptogram[0]]; 
+
+  // 배열의 인덱스 순회
+  for (var i = 1; i < cryptogram.length; i++) { 
+    // top 지정 
+    var top = answer[answer.length - 1]; 
+    
+    //비교하며 삽입,제거
+    if (top !== cryptogram[i]) { 
       answer.push(cryptogram[i]);
     }
     else if (top == cryptogram[i]) {
       answer.pop(cryptogram[i]);
     }
   }
-  answer = answer.join(''); // 배열을 다시 문자열로 변경
+  // 문자열로 변경
+  answer = answer.join(''); 
   return answer;
 }
 
