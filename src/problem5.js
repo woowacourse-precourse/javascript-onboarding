137370f6c56f8ed43ff994b4e5f7824fa38554ac
Sang-Mini <sangmin3062@gmail.com>
2022-10-31 22:45:37 +0900

    [problem5] 필요한 화폐 갯수를 리턴해주는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -4,6 +4,12 @@ function problem5(money) {
   // 각 금액 단위별 화폐 배열을 가진 변수 선언
   const won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  // 필요한 화폐 갯수를 리턴해주는 기능 구현
+  for (let i = 0; i < won.length; i++) {
+    answer.push(Math.floor(money / won[i]));
+    money = money % won[i];
+  }
+
   return answer;
 }
 
