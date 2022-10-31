d34e13888c58d845f7d17c0a6575cf3fcaf8593a
Yebin Kim <kyb4312@gmail.com>
2022-10-31 22:52:28 +0900

    (problem 4): List feature list as annotation

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,9 @@
+/*
+ * <기능 목록>
+ * 1. 청개구리 사전대로 알파벳 하나를 변환하는 changeAlphabetGreenFrog() 함수.
+ * 2. 청개구리 사전대로 입력받은 문자열 전체를 변환하는 changeWordGreenFrog() 함수.
+ */
+
 function problem4(word) {
   var answer;
   return answer;
