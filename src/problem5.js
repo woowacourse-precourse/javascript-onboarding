c4697f32f106da0384a2830b6ad9ae4fac9c982c
이석준 <sukjune96@gmail.com>
2022-10-31 18:47:39 +0900

    [문제5] refactor: 가독성을 위해 for문을 forEach문으로 교체

--- src/problem5.js
+++ src/problem5.js
@@ -2,12 +2,13 @@ function problem5(money) {
   let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
-  for (let i = 0; i < moneyList.length; i++) {
-    if (money >= moneyList[i]) {
-      answer[i] = parseInt(money / moneyList[i]);
-      money = money - moneyList[i] * answer[i];
+  moneyList.forEach((nowMoney, idx) => {
+    if (money >= nowMoney) {
+      answer[idx] = parseInt(money / nowMoney);
+      money = money - nowMoney * answer[idx];
     }
-  }
+  });
+
   return answer;
 }
 
@@ -16,4 +17,4 @@ module.exports = problem5;
 // 기능구현
 // 1. [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1] 배열 생성
 // 2. 배열을 돌며 각 원소와 잔액의 크기 비교 후 잔액이 더 큰 경우 몫을 저장
-// 3. 잔액 = 원금 - moneyList[i] * 몫
+// 3. 잔액 = 원금 - nowMoney * 몫
