286c95c6b1afd68a35b354287842e51edb771170
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 16:17:44 +0900

    feat : problem7 함께 아는 친구 처리, visitor 처리
    앞서 만든 친구리스트를 bfs(너비우선탐색) 알고리즘을 이용하여, 친구리스트 요소를 하나씩 탐색하며 입력받은 user의 친구 리스트에 포함되는지  확인하고 점수를 부여했습니다.
    부여된 점수의 저장은 map 자료구조를 사용했습니다.(메서드명이 직관적)
    (함께 아는 친구 처리)
    
    bfs를 사용한 이유는 모든 노드(친구 리스트)를 탐색해야하기 때문에
    사용하기 좋을 것이라 판단했습니다.
    
    visitor의 처리는 반복문을 사용해 처리했습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -20,7 +20,50 @@ function problem7(user, friends, visitors) {
     const answer = [];
 
     const compare = friendsList[user]; // user와 친구 사이
-    return answer;
+    const queue = [];
+    const myMap = new Map();
+
+    //bfs, 함께 아는 친구 처리
+    for (let key of Object.keys(friendsList)) {
+        if (key === user) continue;
+        if (!myMap.has(key)) myMap.set(key, 0);
+        queue.push(key);
+        while (queue.length) {
+            let current = queue.shift();
+            for (let x of friendsList[current]) {
+                if (compare.includes(x)) {
+                    myMap.set(key, myMap.get(key) + 10);
+                }
+            }
+        }
+    }
+
+    // visitor 처리
+    for (let x of visitors) {
+        if (!myMap.has(x)) {
+            myMap.set(x, 1);
+        } else {
+            myMap.set(x, myMap.get(x) + 1);
+        }
+    }
+
+    for (let [key, value] of myMap.entries()) {
+        answer.push([key, value]);
+    }
+
+    // 배열 정렬
+    answer.sort((a, b) => {
+        if (a[1] === b[1]) {
+            return a[0] - b[0];
+        } else {
+            return b[1] - a[1];
+        }
+    });
+
+    return answer
+        .filter((e) => e[1] !== 0)
+        .map((e) => e[0])
+        .splice(0, 3);
 }
 
 module.exports = problem7;
