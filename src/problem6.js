8b1899d7238a58bd2a93a1e4df3e671d9754d92e
Junghwan Kim <junghwki@c2r5s6.42seoul.kr>
2022-10-31 11:42:56 +0900

    feat: [problem6] input 닉네임 및 이메일 값 검사
    정규표현식을 사용해 입력값을 검사해 에러처리

--- src/problem6.js
+++ src/problem6.js
@@ -5,7 +5,47 @@
 // 4. 중복 제거 함수 구현
 // 5. email 정렬후 result 반환
 
+function validInputCheck(forms) {
+  if (forms.length < 1 || forms.length > 10000) {
+    return -1;
+  }
+  for (let index = 0; index < forms.length; index++) {
+    if (checkForms(forms[index]) < 0) {
+      return -1;
+    }
+  }
+  return 0;
+}
+
+function checkForms(form) {
+  const userEmail = form[0];
+  const userNickname = form[1];
+
+  if (checkEmail(userEmail) === false) {
+    return -1;
+  }
+  if (checkNickname(userNickname) === false) {
+    return -1;
+  }
+  return 0;
+}
+
+function checkNickname(nickname) {
+  const nicknameRegex = /^[가-힣]{1,20}$/;
+
+  return nicknameRegex.test(nickname);
+}
+
+function checkEmail(email) {
+  const emailDomainRegex = /^\w{1,10}@email.com$/;
+
+  return emailDomainRegex.test(email);
+}
+
 function problem6(forms) {
+  if (validInputCheck(forms) < 0) {
+    return "Argument Error";
+  }
   var answer;
   return answer;
 }
