abe37a8ffddba9978a5700f869a9d8de60c19411
daal2quf <daal2quf@gmail.com>
2022-11-01 03:49:07 +0900

    Map의 value를 순회하며 중복단어 찾고 해당 key 배열push 및 정렬

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,8 @@
 function problem6(forms) {
-  var answer;
-  let map = new Map()
+  var answer = [];
+  const map2 = new Map(forms)
+
+  const map = new Map()
 
   for (let e of forms) {
     const userNameParts = e[1].split('')
@@ -14,9 +16,30 @@ function problem6(forms) {
       }
     }
   }
+  
+  let duplicatedWord;
+  for (let key of map.keys()) {
+    if(map.get(key) > 1) {
+      duplicatedWord = key;
+    }
+  }
 
-console.log(map)
+  for(let key of map2.keys()) {
+    if(map2.get(key).includes(duplicatedWord)) {
+      answer.push(key)
+    }
+  }
+  
+  answer.sort((a, b) => a.localeCompare(b))
+  console.log(answer)
   return answer;
 }
 
+problem6([
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+])
 module.exports = problem6;
