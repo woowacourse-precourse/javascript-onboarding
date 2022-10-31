2895e78ed9636da41a9c233e878eb53c0c875cb1
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-31 17:31:54 +0900

    feat: PROBLEM 7 user 친구의 친구 찾는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,11 +1,22 @@
 function problem7(user, friends, visitors) {
   let realUserFriend = [];
+  let newFriend = [];
 
   for (let i = 0; i < friends.length; i++) {
     if (friends[i][0] == user) {
       realUserFriend.push(friends[i][1]);
     } else if (friends[i][1] == user) {
       realUserFriend.push(friends[i][0]);
+    }
+  }
+
+  for (let i = 0; i < realUserFriend.length; i++) {
+    for (let j = 0; j < friends.length; j++) {
+      if (friends[j][0] == realUserFriend[i] && friends[j][1] != user) {
+        newFriend.push(friends[j][1]);
+      } else if (friends[j][1] == realUserFriend[i] && friends[j][0] != user) {
+        newFriend.push(friends[j][0]);
+      }
     }
   }
 }
