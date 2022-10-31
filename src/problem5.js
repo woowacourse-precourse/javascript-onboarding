8caa4e05a6d1013acb4a828f27e0005d7256318c
yaena1223 <yaena1223@dgu.ac.kr>
2022-11-01 05:54:31 +0900

    [refactor] problem5- 변수명 변경

--- src/problem5.js
+++ src/problem5.js
@@ -6,14 +6,14 @@ function problem5(money) {
 
 function cal_money(money) {
   const amount_list = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let money_list = [];
+  let count_converted_money = [];
   for (let i = 0; i < 9; i++) {
     let amount = amount_list[i];
     let n = parseInt(money / amount);
     money = money % amount;
-    money_list = [...money_list, n];
+    count_converted_money = [...count_converted_money, n];
   }
-  return money_list;
+  return count_converted_money;
 }
 
 module.exports = problem5;
