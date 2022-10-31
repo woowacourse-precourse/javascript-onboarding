5b1b5173d33375ebd7aecbc403d5244ae29b6619
sying012 <syi369@naver.com>
2022-10-31 16:11:27 +0900

    problem6 - 기능목록,  제한사항 검사 함수 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,38 @@
 function problem6(forms) {
   var answer;
+
+  if (violationChk) {
+  }
+
   return answer;
+}
+
+function violationChk(forms) {
+  var result = false;
+
+  // 크루 수가 1 ~ 10000명 사이인지 검사
+  if (forms.length >= 1 && forms.length <= 10000) {
+    for (var i = 0; i < forms.length; i++) {
+      var emailAddr = forms[i][0];
+      var nickName = forms[i][1];
+
+      // 이메일 주소 길이 제한 검사
+      if (emailAddr.length >= 11 && emailAddr.length < 20) {
+        // 도메인 제한 검사
+        if (domainChk(emailAddr)) {
+          // 닉네임 길이 제한 검사
+          if (nickName.length >= 1 && nickName.length < 20) {
+            // 닉네임이 한글인지 검사
+            if (korChk(nickName)) {
+              result = true;
+            }
+          }
+        }
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = problem6;
