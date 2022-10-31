c39796b6ce4fab2cdbe1fa500729ce79fbea47e9
Sang-Mini <sangmin3062@gmail.com>
2022-10-31 22:20:46 +0900

    [problem4] 기본사전, 청개구리사전 데이터 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,13 @@
 function problem4(word) {
-  const dictionary = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
-  const oppositeDictionary = "ZYXWVUTSRQPONMLKJIHGFEDCBA"
+  let answer = "";
 
-  let answer;
+  let dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ';
+  let oppositeDictionary = 'ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba ';
+
+  for (let i = 0; i < word.length; i++) {
+    let b = dictionary.indexOf(word[i]);
+    answer = answer.concat(oppositeDictionary[b]);
+  }
 
   return answer;
 }
