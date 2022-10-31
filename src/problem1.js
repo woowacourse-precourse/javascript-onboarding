48d65eab00cdb7badb1db08da564fc8e6fdc815b
Hwang Seong Taek <hst007@naver.com>
2022-10-31 17:16:57 +0900

    refactor: [problem1] 페이지 점수 계산 기능 분리

--- src/problem1.js
+++ src/problem1.js
@@ -4,25 +4,11 @@ function problem1(pobi, crong) {
 
   // 포비 점수 배열
   const pobiScore = [];
-
-  pobi.forEach(page => {
-    const pageSplit = page.toString().split('');
-    const add = pageSplit.reduce((fir, idx) => fir + idx/1, 0);
-    const mul = pageSplit.reduce((fir, idx) => fir * idx/1, 1);
-
-    pobiScore.push(Math.max(add, mul));
-  });
+  pobi.forEach(page => pobiScore.push(pageScore(page)));
 
   // 크롱 점수 배열
   const crongScore = [];
-  
-  crong.forEach(page => {
-    const pageSplit = page.toString().split('');
-    const add = pageSplit.reduce((fir, idx) => fir + idx/1, 0);
-    const mul = pageSplit.reduce((fir, idx) => fir * idx/1, 1);
-
-    crongScore.push(Math.max(add, mul));
-  });
+  crong.forEach(page => crongScore.push(pageScore(page)));
 
   // 포비와 크롱의 점수를 비교하여 결과 값 return
   if (Math.max(...pobiScore) > Math.max(...crongScore)) {
@@ -38,6 +24,15 @@ function problem1(pobi, crong) {
 function exceptionPage(pobi, crong) {
   if (pobi[1] === 1 || crong[1] === 1 || pobi[0] === 400 || crong[0] === 400) return true; // 시작면으로 끝나거나 끝면으로 시작 할 경우
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return true; // 페이지가 순서대로 없을 경우
+}
+
+// 페이지 점수 계산
+function pageScore(page) {
+  const pageSplit = page.toString().split('');
+  const add = pageSplit.reduce((fir, idx) => fir + idx/1, 0);
+  const mul = pageSplit.reduce((fir, idx) => fir * idx/1, 1);
+
+  return Math.max(add, mul);
 }
 
 module.exports = problem1;
