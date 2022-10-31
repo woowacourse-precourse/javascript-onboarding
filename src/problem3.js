8e8e515cf8078df2b99679f36a66819c8c6fcb46
jaehak26 <jack10829@naver.com>
2022-11-01 01:48:17 +0900

    problem3.js 기능구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,40 @@
-function problem3(number) {
+
+
+
+//예외사항
+//1<= number <= 10000
+//number은 정수
+
+function except(number=0){
+  if(Number.isInteger(number)!=true){return -1}
+  if(1>number || 10000<number){return -1}
+  return 0
+}
+
+function action(number=0){
+  let count = 0;
+
+  for(let i=0; i<=number; i++){
+    let n = i;
+    for(let j=0; j<(""+n).length;j++){
+      if(n%3==0||n%6==0||n%9==0){count++;}
+      n = n/10
+    }
+  }
+
+  return count;
+}
+
+function problem3(number=0) {
   var answer;
+
+  let e = except(number)
+  if(e == -1){return -1}
+
+  answer = action(number)
+
   return answer;
 }
 
 module.exports = problem3;
+
