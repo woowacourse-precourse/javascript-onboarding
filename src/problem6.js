3e03e98478963cf66472e1528f3457e6c2211907
SoMin <thals0107@naver.com>
2022-10-31 12:30:03 +0900

    feat: 문제 6 - 닉네임 길이 2 단위로 잘라서 map에 저장

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,12 @@
 function problem6(forms) {
-  var answer;
+  let map = new Map();
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      const str = forms[i][1].substr(j, 2);
+      // console.log(str);
+    }
+  }
   return answer;
 }
 
