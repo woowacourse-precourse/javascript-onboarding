1d5c313a5b0f69782ed0f421f7ec90e8207ce9c6
Monte <monte6198@gmail.com>
2022-11-01 04:39:05 +0900

    feat(problem7.js): getUserFriend() 및 user 친구 추출

--- src/problem7.js
+++ src/problem7.js
@@ -1,9 +1,22 @@
+function getUserFriend(pair, user, userFriends) {
+  // 친구 목록에서 user의 친구를 추출한다.
+  if (pair.includes(user)) {
+    // user의 이름을 포함하고 있는 목록에서
+    userFriends.push(pair.filter((name) => name !== user)[0]); // user의 이름을 제외한 친구의 이름을 배열에 추가한다.
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   answer = [];
   let userFriends = [];
   let theirFriends = new Set();
   let scores = new Map();
+
+  for (let pair of friends) {
+    // user의 친구 목록 전체에 대해 반복문을 수행하며
+    getUserFriend(pair, user, userFriends); // user의 친구 추출
+  }
 
   return answer;
 }
