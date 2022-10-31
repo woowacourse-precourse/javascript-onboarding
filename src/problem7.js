184261ec51a186b32a1beb73c56c22b65f811a8d
Azamman327 <wjsdbdud327@naver.com>
2022-10-31 20:54:24 +0900

    Docs: [P7] 기능 목록 정리

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,17 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+/*
+1. 닉네임, 점수를 가진 객체배열 생성 score
+2. 배열을 닉네임 기준으로 정렬
+3. 사용자 친구를 찾아 배열 friendList에 저장
+4. friendList에 있는 유저를 친구로 갖는 유저, 횟수를 담은 객체배열 linkedFriends
+5. 횟수에 따라 객체배열 score 업데이트
+
+6. visitors 배열에 따라 객체배열 score 업데이트
+
+7. value기준으로 score 객체배열 정렬
+8. value가 0인 부분부터 잘라내기
+9. score의 닉네임 부분 차례로 출력
+*/
