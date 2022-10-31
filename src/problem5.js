7aaa155d603085fd4ecc65f1e8d8dd313a2894e4
sh981013s <sh981013s@gmail.com>
2022-10-31 23:26:34 +0900

    feat: [5] 처리해야 할 액수 0원 확인 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,12 @@
+/**
+ * 처리해야 할 액수가 0원인지 확인한다.
+ * @param {number} moneyLeft - 처리해야 할 액수
+ * @return {boolean}
+ */
+const isMoneyLeftZero = (moneyLeft) => {
+  return !moneyLeft;
+};
+
 function problem5(money) {
   var answer;
   return answer;
