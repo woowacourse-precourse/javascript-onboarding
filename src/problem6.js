9314b2e1b54e64904fce22bf82ca5e12a03d28d2
jinseo <ghkdwlstj99@gmail.com>
2022-10-31 12:41:11 +0900

    중복 문자 확인 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const twoLetterName = checkDuplicateChar(forms);
+}
+
+function checkDuplicateChar(forms){
+  let twoLetterNick = [];
+  for(let i = 0; i <forms.length; i++){
+    let checkDuplicate = forms[i][1].split("");
+    for(let j = 0 ; j < checkDuplicate.length-1 ; j ++ ){
+      twoLetterNick.push(checkDuplicate[j] + checkDuplicate[j+1]);
+    }
+  }
+  return twoLetterNick; 
 }
 
 module.exports = problem6;
