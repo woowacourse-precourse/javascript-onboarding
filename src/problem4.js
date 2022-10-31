48b26a7f8b51cf37b46530f28a6fae1e1a7cd3e6
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-10-31 12:46:11 +0900

    feat: 문제4 청개구리 사전 규칙에 맞게 변환

--- src/problem4.js
+++ src/problem4.js
@@ -7,6 +7,18 @@ function isUpperCase(letter) {
   return letter === letter.toUpperCase();
 }
 
+function convertAlphabet(letter) {
+  if (isUpperCase(letter)) {
+    min = 65;
+    max = 90;
+  } else {
+    min = 97;
+    max = 122;
+  }
+  letter = max - (letter.charCodeAt() - min);
+  return String.fromCharCode(letter);
+}
+
 function problem4(word) {
   let answer = '';
   for (let i = 0; i < word.length; i++) {
@@ -14,6 +26,7 @@ function problem4(word) {
       answer += word[i];
       continue;
     }
+    answer += convertAlphabet(word[i]);
   }
   return answer;
 }
