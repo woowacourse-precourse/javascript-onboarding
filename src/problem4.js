e1987bb4c31d2985577c524e6cbd630c3d4bdc64
imseoyoung <lsy783@sookmyung.ac.kr>
2022-11-01 00:36:02 +0900

    feat: problem4 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const pattern = /[a-zA-Z\s]/g;
+  let answer = [];
+
+for (i=0; i<word.length; i++) {
+  if (word[i].match(pattern)) {
+    if (word[i] === " ") {
+      answer.push(word[i]);
+    }
+    else if (word[i] === word[i].toUpperCase()) {
+      answer.push(String.fromCharCode(155-word[i].charCodeAt()));
+    }
+    else {
+      answer.push(String.fromCharCode(219-word[i].charCodeAt()));
+    }
+  }
+  else {
+    answer.push(word[i]);
+  }
+}
+  return answer.join('');
 }
 
 module.exports = problem4;
