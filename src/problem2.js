e6cf0ffee8790f0543974648a403f09febfa89c2
Vo9Ming <kimminj2017@gmail.com>
2022-10-31 23:24:33 +0900

    매개변수로 받은 cryptogram을 문자를 공백을 기준으로 나누기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,10 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = "";
+  var ans = cryptogram.split("");
+
   return answer;
 }
 
 module.exports = problem2;
+console.log(problem2("browoanoommnaon"));
+console.log(problem2("zyelleyz"));
