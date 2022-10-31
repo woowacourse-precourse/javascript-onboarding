8477ca8a484eaa417c5e38677ec02ae0dc3353a7
이여진 <jinspace3710@gmail.com>
2022-10-31 16:45:49 +0900

    feat: problem2 기능 1 - 연속하는 중복 문제 삭제 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+
   return answer;
 }
 
+function decrypt(cryptogram) {
+  if (typeof cryptogram === 'string') {
+    let cryptogramArray = [...cryptogram];
+
+    for (i = 0; i < cryptogramArray.length - 1; i++) {
+      if (cryptogramArray[i] === cryptogramArray[i + 1]) {
+        cryptogramArray[i] = '';
+        cryptogramArray[i + 1] = '';
+        cryptogram = cryptogramArray.join('');
+        return cryptogram;
+      }
+    }
+    cryptogram = cryptogramArray.join('');
+    return cryptogram;
+  }
+}
 module.exports = problem2;
