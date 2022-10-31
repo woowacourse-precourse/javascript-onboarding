7d196a98fb0b305d582482549e49d39e37bcaf16
고동현 <dhjk35@naver.com>
2022-10-31 21:03:19 +0900

    feat: 알파벳인지 판별하는 기능 구현 - 문제 4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,33 @@
+/**
+ * 기능 정의
+ *
+ * - [x] 알파벳인지 판별하는 기능
+ * - [ ] 대소문자를 구분하는 기능
+ * - [ ] 알파벳 기준 반대의 알파벳을 반환하는 기능
+ */
+
+/**
+ * word가 주어졌을 때, 각 문자를 알파벳 기준으로 반대로 변환해주는 함수.
+ * 알파벳 외의 문자는 변환하지 않으며, 알파벳 대문자는 대문자로, 소문자는 소문자로 변환
+ *
+ * @param {string} word 길이가 1 이상 1,000 이하인 문자열
+ * @returns {string} 조건에 맞게 변경된 문자열
+ */
 function problem4(word) {
   var answer;
   return answer;
+}
+
+/**
+ * 정규식을 통해 알파벳인지 판별하는 함수
+ *
+ * @param {string} char 알파벳인지 확인할 문자열
+ * @returns {boolean} 알파벳이라면 true, 아니면 false
+ */
+function isAlphabet(char) {
+  const regExp = /^[a-zA-Z]*$/;
+
+  return regExp.test(char);
 }
 
 module.exports = problem4;
