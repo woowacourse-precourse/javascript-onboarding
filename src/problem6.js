ef91be883a64816580def616dc0679cdd68dc265
chiman <rlaclghks123@naver.com>
2022-10-31 19:34:52 +0900

    feat: problem6- findDuplicateUser 함수 생성
    중복된 단어를 가진 username을 찾아준다.

--- src/problem6.js
+++ src/problem6.js
@@ -31,11 +31,25 @@ function findDuplicateUserWord(cutUserNameArr) {
 
   return [...new Set(duplicateUserWord)];
 }
+
+function findDuplicateUser(duplicateUserWordArr, forms) {
+  const duplicatedUser = [];
+
+  for (let user of forms) {
+    let username = user[1];
+    duplicateUserWordArr.map((word) => {
+      username.includes(word) ? duplicatedUser.push(user) : "";
+    });
+  }
+  return [...new Set(duplicatedUser)];
+}
+
 function problem6(forms) {
   var answer;
   const userNameArr = getUserName(forms);
   const cutUserNameArr = cutUserName(userNameArr);
   const duplicateUserWordArr = findDuplicateUserWord(cutUserNameArr);
+  const duplicateUserArr = findDuplicateUser(duplicateUserWordArr, forms);
   return answer;
 }
 
