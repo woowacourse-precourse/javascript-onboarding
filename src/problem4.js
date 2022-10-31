a215d889235b0b4189042d0e2e151ee48806ea30
Lucy Ma <55519907+p3bble123@users.noreply.github.com>
2022-10-31 15:46:18 +0900

    우아한테크코스 1주차 과제

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,42 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const upperAlpha = getAlphabets(65);
+  const lowerAlpha = getAlphabets(97);
+  
+  return invertWord(word, upperAlpha, lowerAlpha);
+}
+
+function invertWord(word, upperAlpha, lowerAlpha) {
+  var inverted = '';
+  const range = word.length;
+  const alphaRange = upperAlpha.length;
+
+  for (var idx=0; idx < range; idx++) {
+    let currentChar = word.charAt(idx)
+    
+    if (upperAlpha.includes(currentChar)) {
+      originalIdx = upperAlpha.indexOf(currentChar);
+      invertedIdx = alphaRange - originalIdx -1;
+      inverted += upperAlpha[invertedIdx];
+    }
+    else if (lowerAlpha.includes(currentChar)) {
+      originalIdx = lowerAlpha.indexOf(word.charAt(idx));
+      invertedIdx = alphaRange - originalIdx -1;
+      inverted += lowerAlpha[invertedIdx];
+    }
+    else {
+      inverted += currentChar;
+    }
+  }
+
+  return inverted;
+}
+
+function getAlphabets(ascii) {
+  const alpha = Array.from(Array(26)).map((e, idx) => idx + ascii);
+  const alphabet = alpha.map((n) => String.fromCharCode(n));
+  
+  return alphabet;
 }
 
 module.exports = problem4;
+
--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,32 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  return getWallet(money)
+}
+
+function getWallet(money) {
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const wallet = {
+    money : money,
+    billsAndCoins: []
+  }
+
+  units.forEach(unit => {
+    getChangeForUnit(wallet, unit);
+  });
+  
+  return wallet.billsAndCoins;
+}
+
+function getChangeForUnit(wallet, unit) {
+  var money = wallet.money;
+  var bills = 0;
+  
+  if(money >= unit) {
+    bills = Math.floor(money/unit)
+    money = money - (unit*bills)
+  }
+
+  wallet.money = money
+  wallet.billsAndCoins.push(bills)
 }
 
 module.exports = problem5;
