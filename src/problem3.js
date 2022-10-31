2d1adae611f4ac71dea0d7bd36d539f0eaf5d56a
ilgon0110 <moses011010@gmail.com>
2022-10-31 20:10:42 +0900

    refactor: 연산 기능 수정(#problem3)

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,14 @@
 function problem3(number) {
   let answer = 0;
-  let check = ["3", "6", "9"];
+  let check = ['3', '6', '9'];
 
   //연산
-  for (let i = 1; i <= number; i++) {
+  for (let i = 1; i <= number; i += 1) {
     //기능목록 1-1 구현
-    let num = i.toString();
-    for (let x of num) {
-      //기능목록 1-2 구현
-      if (check.includes(x)) answer++;
-    }
+    let num = String(i);
+    answer += num.split('').filter(v => check.includes(v)).length;
   }
+
   return answer;
 }
 
