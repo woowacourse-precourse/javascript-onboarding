9b782f6fe9a4160526ada6767052148ef99b7869
woong <inwoong100@gmail.com>
2022-10-31 23:01:48 +0900

    refactor: problem5 코드 리팩토링
    1. 역할에 맞는 변수명 지정
    2. 컨벤션에 맞도록 줄간격 조정

--- src/problem5.js
+++ src/problem5.js
@@ -1,16 +1,21 @@
 function problem5(money) {
+  //예외사항1
   if (money < 1 || money > 1000000) return 'ERROR';
+  //예외사항2
   if (money % 1) return 'ERROR';
 
-  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const answer = [];
-  for (let i = 0; i < currency.length; i++) {
-    const coin = Math.floor(money / currency[i]);
-    answer.push(coin);
-    money -= currency[i] * coin;
+  const currencies = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  const results = [];
+
+  for (const currency of currencies) {
+    const coin = Math.floor(money / currency);
+    results.push(coin);
+
+    money -= currency * coin;
   }
 
-  return answer;
+  return results;
 }
 
 module.exports = problem5;
