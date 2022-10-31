96236c812c1485b5b66f54a26c4f31023ebe66d6
“somm12” <sominan8744@gmail.com>
2022-10-31 18:16:10 +0900

    problem7: user의 친구를 찾는 함수 filterRealFriends함수정의

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,23 @@
+function filterRealFriends(user, friends) {
+  let array = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] === user) {
+      array.push(friends[i][1]);
+    } else if (friends[i][1] === user) {
+      array.push(friends[i][0]);
+    }
+  }
+  return array;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  let realFriends;
+  let dict;
+  let limit; // 최대 5명 까지 추천이므로, 제한 변수.
+
+  // user의 친구를 담은 배열 realFriends 만들기.
+  realFriends = filterRealFriends(user, friends);
   return answer;
 }
 
