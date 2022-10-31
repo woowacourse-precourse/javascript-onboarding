c648afb7ec1a8f71020ff1ceed8572621814b895
HeoJiye <heoze1031@gmail.com>
2022-10-31 21:32:34 +0900

    (problem7) feat: 추천 친구 후보 초기화 및 추천 점수 초기화

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,7 @@
 // < 기능 목록 >
 // 1. 친구 추천 점수 매기기
 //    - 사용자와 함께 아는 친구의 수 카운트
+//        + 사용자의 친구 목록
 //    - 사용자의 타임 라인에 방문한 횟수 카운트
 //    [예외처리] 이미 친구인 경우
 // 2. 추천 점수가 높은 순으로 정렬하여 5명만 뽑는다
@@ -8,8 +9,52 @@
 //    [예외처리] 추천 점수가 0점 이상인 사용자가 4명 이하인 경우
 
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  var score = []; // 친구 추천 점수
+  var user_firends = []; // user의 친구 목록
+  
+  // user_firends 설정 (사용자의 친구 목록)
+  for(f of friends) {
+    if(f[0] == user) user_firends.push(f[1]);
+    else if(f[1] == user) user_firends.push(f[0]);
+  }
+
+  // answer 초기화 (추천 친구 후보들)
+  for(f of friends) {
+    answer.push(f[0]);
+    answer.push(f[1]);
+  }
+  for(v of visitors) {
+    answer.push(v);
+  }
+
+  answer = answer.sort().filter(function(item, idx, array) {
+      if(item == user) return false; // user 제외
+      for(f of user_firends) {// user 친구들 제외
+        if(f == item) return false;
+      }
+      return !idx || item != array[idx - 1]; // 중복값 제외
+    });
+  
+  // score 초기화
+  for(var i = 0; i < answer.length; i++) score[i] = 0;
+
   return answer;
 }
+
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 
 module.exports = problem7;
