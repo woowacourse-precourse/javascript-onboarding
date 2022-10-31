a4b23d298701e946a84fa2419d620deca0954014
Jeonjunwoo <tira618@naver.com>
2022-10-31 17:26:12 +0900

    각 금액단위들을 생산자 함수로 정리

--- src/problem5.js
+++ src/problem5.js
@@ -16,6 +16,16 @@ function intoArr (money){
     moneyArr.push(remains[0])
     remains = [];
   }
+  const fiftyThousand = new switches(50000);
+  const tenThousand = new switches(10000);
+  const fiveThousand = new switches(5000);
+  const thousand = new switches(1000);
+  const fiveHundred = new switches(500);
+  const hundred = new switches(100);
+  const fifty = new switches(50);
+  const ten = new switches(10);
+  const one = new switches(1);
+  return resultArr;
 }
 
 module.exports = problem5;
