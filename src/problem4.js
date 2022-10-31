b874e4d235d1c9d0505dfdcad8a10dc89dc32831
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 13:07:29 +0900

    Feat: [문제번호4] 대문자와 소문자 상수 설정 후 대문자 판별

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,12 @@ function problem4(word) {
   return answer;
 }
 
+const UPPERCASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+const LOWERCASE = "abcdefghijklmnopqrstuvwxyz";
+
+function checkUpperCase(element) {
+  if (element === element.toUpperCase()) return true; // 대문자인지 아닌지 확인
+  return false;
+}
+
 module.exports = problem4;
