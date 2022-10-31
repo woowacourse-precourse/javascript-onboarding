ffc8acfaf43f01c33cdc7ea5c0412611a2afda1c
jungsmins1 <jungsmins00@gmail.com>
2022-10-31 22:30:02 +0900

    feat: 사용자의 친구를 구하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+
+  let userFriends = new Array;
+
+  //사용자의 친구를 구하는 기능
+  for (let i = 0; i < friends.length; i++){
+    if (friends[i].includes(user)) {
+      userFriends.push(friends[i][0]);
+    }
+  }
 }
 
-module.exports = problem7;
+problem7(
+  "mrko", 
+  [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+)
+// module.exports = problem7;
