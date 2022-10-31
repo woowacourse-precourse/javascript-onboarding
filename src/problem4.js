018ee7e140a4ba1c81c33ed491dc5e6e2a254038
kmin-jeong <desk535353@gmail.com>
2022-10-31 20:36:11 +0900

    feat:4,5번 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,19 @@
+//정렬뿐만 아니라 청개구리라서 역렬도 고려, 테스트 문장에 대소문자 섞여있음
 function problem4(word) {
-  var answer;
+  let answer;
+  let result = [];
+  let astring = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz";
+  let zstring = "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba";
+
+  for (let value = 0; value <= word.length; value++) {
+    let mark = astring.indexOf(word[value]);
+    if (mark === -1) {
+      result.push(word[value]);
+    } else {
+      result.push(zstring[mark]);
+    }
+  }
+  answer = result.join("");
   return answer;
 }
 
