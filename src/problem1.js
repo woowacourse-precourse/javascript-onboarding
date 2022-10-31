784a3d86b34ac267c6befcb66304eebf5eca4ce7
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 02:03:31 +0900

    Feat: problem1 - 예외 사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -2,20 +2,6 @@ function problem1(pobi, crong) {
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
     return -1;
   }
-
-  const pobiMaxNum = Math.max(...pobi.map((num) => calculateMaxNum(num)));
-  const crongMaxNum = Math.max(...crong.map((num) => calculateMaxNum(num)));
-
-  if (pobiMaxNum > crongMaxNum) return 1;
-  else if (pobiMaxNum < crongMaxNum) return 2;
-  else return 0;
 }
-
-const calculateMaxNum = (num) => {
-  const numArr = num.toString().split("");
-  const plusNum = numArr.reduce((acc, cur) => acc + Number(cur), 0);
-  const multiplyNum = numArr.reduce((acc, cur) => acc * Number(cur), 1);
-  return Math.max(plusNum, multiplyNum);
-};
 
 module.exports = problem1;
