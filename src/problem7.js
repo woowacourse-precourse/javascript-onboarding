91d4e01fa789337bfec9d50fbd4e9e258160f584
jinseo <ghkdwlstj99@gmail.com>
2022-10-31 13:16:31 +0900

    유저의 친구리스트를 확인하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const checkUserFreind = userFriend(user,friends)
+}
+
+function userFriend(user,friends){
+  let userFriendList = [];
+  for ( let i = 0 ; i < friends.length; i++){
+    if (friends[i][0].includes(user)){
+    userFriendList.push(friends[i][1]);
+    }
+    if(friends[i][1].includes(user)){
+    userFriendList.push(friends[i][0]);
+    }
+  }
+  return userFriendList;
 }
 
 module.exports = problem7;
