c2a01c0d2875a26f7f93409c8638b288081b813f
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-31 21:07:33 +0900

    refactor: PROBLEM 3 리팩토링

--- src/problem3.js
+++ src/problem3.js
@@ -1,23 +1,23 @@
-function problem3(number) {
-  const check = (number) => {
-    if (number < 1 || 10000 < number) return false;
-    return true;
-  }
+const check = (number) => {
+  if (number < 1 || 10000 < number) return false;
+  return true;
+}
 
-  function countClap(number) {
-    let answer = 0
+function countClap(number) {
+  let answer = 0
 
-    for (let i = 1; i <= number; i++) {
-      let count = 0
-      const num = i.toString()
-      for (let j = 0; j < num.length; j++) {
-        if (num[j] === "3" || num[j] === "6" || num[j] === "9") count++
-      }
-      answer += count
+  for (let i = 1; i <= number; i++) {
+    let count = 0
+    const num = i.toString()
+    for (let j = 0; j < num.length; j++) {
+      if (num[j] === "3" || num[j] === "6" || num[j] === "9") count++
     }
-    return answer;
+    answer += count
   }
-  
+  return answer;
+}
+
+function problem3(number) {
   let result = countClap(number)
 
   if (!check(number)) { throw Error("1이상 10,000이하 자연수를 입력해주세요.") }
