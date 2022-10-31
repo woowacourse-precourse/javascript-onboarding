43d730d780ea54526e301131635373af8200ef66
kwh97 <zzzang0512@naver.com>
2022-10-31 16:18:16 +0900

    feat(problem4): 입력 문자열의 요소가 대문자인지 소문자인지 공백인지 확인하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answerArr = [];
+
+  for(let i=0; i<word.length; i++) {
+    let char = word[i];
+
+    if (char === char.toUpperCase()) {
+      answerArr.push(handleUpperCase(char));
+    }
+
+    if (char === char.toLowerCase()) {
+      answerArr.push(handleSmallCase(char));
+    }
+
+    if (char === ' ') {
+      answerArr.push(' ');
+    }
+  }
+
+  return answerArr.join("");
 }
 
 module.exports = problem4;
