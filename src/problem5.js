24fb155302505226aaaa7b5263aa5c63039dc4fd
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 05:18:35 +0900

    [p5] 금액이 큰 순서대로 변환

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,15 @@
+function exchange(num){
+  const wallet = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const ans = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for(i = 0; i < 9; i++) {
+    if(num - wallet[i] >= 0){
+      ans[i] = num / wallet[i];
+      num %= wallet[i];
+    }
+  }
+  return ans;
+}
+
 function problem5(money) {
   var answer;
   return answer;
