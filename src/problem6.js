26adce5f9d86772d60c7efd56cf553d0e5aeee96
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 14:44:15 +0900

    구현할 기능 목록 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
+/*
+  구현할 기능 목록
+  [ ] 닉네임이 다른 닉네임과 중복처리 됐을 때, 이메일 목록에 해당 닉네임의 이메일을 넣는 기능
+  [ ] 특정 닉네임을 2글자씩 분해하여, 해당 닉네임을 제외한 다른 닉네임들과 중복을 확인하는 기능 | return 값: true/false
+  [ ] 해당 문자열을 닉네임 배열이 포함하는 지 확인하는 기능 | return 값 : true/false
+*/
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  let result;
+  return result;
 }
 
 module.exports = problem6;
