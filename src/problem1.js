eacf735ad72c5aeda7b37a71b8100cc417c7446d
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-31 16:00:45 +0900

    problem1: exception() done

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,21 @@
 function problem1(pobi, crong) {
   var answer;
 
-  function exception(player1, player2) {}
+  function exception(player1, player2) {
+    var players = [player1, player2];
+    if (
+      players.filter(
+        (player) =>
+          (player[1] - player[0] != 1) |
+          player.includes(1) |
+          player.includes(2) |
+          player.includes(399) |
+          player.includes(400)
+      ).length > 0
+    )
+      return true;
+    else return false;
+  }
 
   function score(player) {}
 
