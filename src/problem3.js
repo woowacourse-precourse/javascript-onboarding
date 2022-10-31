a28b70217a0d0af7c9312e39b30287138a275e69
chiman <rlaclghks123@naver.com>
2022-10-31 18:26:26 +0900

    feat: problem-3 isValid 함수 생성
    예외처리를 하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
+function isValid(number) {
+  const regex = /^[1-9][0-9]*$/;
+
+  if (number.length < 1 || number.length > 10000 || !regex.test(number)) {
+    throw "1 이상 10000 이하인 자연수를 입력해주세요";
+  }
+}
+
 function problem3(number) {
   var answer;
+
+  isValid(number);
   return answer;
 }
 
