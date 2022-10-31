b42a8dd23216320896055535264e8fe08c55e9d7
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 03:16:19 +0900

    refactor:[문제4] input값이 알파벳 외의 문자(공백포함)이면 바로 return 시켜 불필요한 동작을 줄인다

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
+function isNotWord(word) {
+  const WORD_ASCII_CODE_FIRST = 65;
+  const WORD_ASCII_CODE_SECOND = 90;
+  const WORD_ASCII_CODE_THIRD = 97;
+  const WORD_ASCII_CODE_FOURTH = 122;
+  if (word.charCodeAt() < WORD_ASCII_CODE_FIRST) return true;
+  if (
+    word.charCodeAt() > WORD_ASCII_CODE_SECOND &&
+    word.charCodeAt() < WORD_ASCII_CODE_THIRD
+  )
+    return true;
+  if (word.charCodeAt() > WORD_ASCII_CODE_FOURTH) return true;
+
+  return false;
+}
+
+function convertWord(word) {
+  if (isNotWord(word)) return word;
+  return;
+}
+
 function problem4(word) {
   const convertWords = word.split("").map((alphabet) => convertWord(alphabet));
-  return;
+  return convertWords;
 }
 
 module.exports = problem4;
