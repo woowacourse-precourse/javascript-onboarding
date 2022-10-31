402c78a65a9fbdb52dfc3de670c4bbc3fa8cefb9
saetakki <lime.lemonade03@gmail.com>
2022-10-31 11:54:42 +0900

    4번 문제

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,24 @@
 function problem4(word) {
-  var answer;
+  const alpha = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  
+  let strArr = String(word).split("")
+
+  let answer = strArr.reduce((acc,cur) => {
+    if(cur !== " "){
+      if(alpha.includes(cur)){
+        let newIdx = 25 - alpha.indexOf(cur)
+        acc += alpha[newIdx]
+      } else {
+        let newIdx = 25 - alpha.toLowerCase().indexOf(cur)
+        acc += alpha[newIdx].toLowerCase()
+      }
+    } else {
+      acc += cur
+    }
+    return acc
+  },"")
+
+
   return answer;
 }
 
