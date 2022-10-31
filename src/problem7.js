73498e69e8efeeeaef64301307f1dfda09917f31
박겸영 <yong_drg@naver.com>
2022-11-01 00:55:39 +0900

    feat: 문제 7 사용자 친구의 친구 목록 구하기 - 기능 2: 사용자 친구 각각과 친구 관계인 아이디 목록을 구하고, 사용자와 사용자 친구 아이디는 목록에서 제외하기

--- src/problem7.js
+++ src/problem7.js
@@ -2,6 +2,14 @@ function problem7(user, friends, visitors) {
   const userFriends = friends
     .filter((friend) => friend.includes(user))
     .flatMap((friend) => friend.filter((id) => id !== user));
+
+  const friendsOfUserFriends = userFriends
+    .flatMap((userFriend) =>
+      friends
+        .filter((friend) => friend.includes(userFriend))
+        .flatMap((friend) => friend.filter((id) => id !== userFriend && id !== user)),
+    )
+    .filter((id) => !userFriends.includes(id));
 }
 
 module.exports = problem7;
