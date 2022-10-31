78de68f263ae1d443465f5f973862c7cd76e72a1
froggy1014 <clcl6084@gmail.com>
2022-10-31 17:14:57 +0900

    feat[7]: 서로 아는 친구 확인 및 점수 추가 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,9 +1,26 @@
 function problem7(user, friends, visitors) {
   const userFriends = getUserFreinds(user, friends);
   const othersFriends = getOthersFriends(user, friends);
+  const rank = new Map();
 
-  console.log(userFriends);
-  console.log(othersFriends);
+  for ( let friend of userFriends ) {
+    for( let [A, B] of othersFriends ) {
+      if( A === friend ){
+        rank.has(B) 
+        ? rank.set(B, rank.get(B)+10)
+        : rank.set(B, (rank.get(B)||0)+10);  
+      } else if ( B === friend) {
+        rank.has(A) 
+        ? rank.set(A, rank.get(A)+10)
+        : rank.set(A, (rank.get(A)||0)+10);
+      }
+    }
+  }
+
+  console.log(userFriends)
+  console.log(othersFriends)
+  console.log(rank)
+
 }
 
 function getUserFreinds(user, relationship) {
