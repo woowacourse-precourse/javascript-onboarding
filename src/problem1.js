eb50c08f766996aecb74853e3c908e0ec79c5664
sujungYu <su_moment@naver.com>
2022-10-31 13:23:31 +0900

    refactor(PROBLEM1):페이지 확인

--- src/problem1.js
+++ src/problem1.js
@@ -1,21 +1,27 @@
 function problem1(pobi, crong) {
-   if(pobi[0]+1!=pobi[1] || crong[0]+1 != crong[1]) return -1;
+   if(checkPage(pobi) == -1 || checkPage(crong) == -1) return -1;
    else {
-    const pobiArr = pobi.map(v=> String(v).split('').map(Number));
-    const crongArr = crong.map(v=> String(v).split('').map(Number));
-    const pobiValue = compareValue(pobiArr);
-    const crongValue = compareValue(crongArr);
-    if(pobiValue>crongValue) return 1;
-    else if(pobiValue<crongValue) return 2;
+    const pobiPage = pobi.map(v=> String(v).split('').map(Number));
+    const crongPage = crong.map(v=> String(v).split('').map(Number));
+    const pobiValue = compareValue(pobiPage);
+    const crongValue = compareValue(crongPage);
+    if(pobiValue > crongValue) return 1;
+    else if(pobiValue < crongValue) return 2;
     else return 0;
    }
+}
+function checkPage(page) {
+    if(page.some(v=> v <= 1 || v >= 400)) return -1;
+    if(page[0]+1 != page[1]) return -1;
+    if(page.length !== 2) return -1;
+    return 1;
 }
 function compareValue(arr) {
     const [lPage] = arr.slice(0,1);
     const [rPage] = arr.slice(1,2);
     const left = calculation(lPage);
     const right = calculation(rPage);
-    return left>right ? left : right
+    return left > right ? left : right
 }
 function calculation(value) {
     let sum = 0;
@@ -27,4 +33,5 @@ function calculation(value) {
     }
     return sum>multiply ? sum : multiply;
 }
+
 module.exports = problem1;
