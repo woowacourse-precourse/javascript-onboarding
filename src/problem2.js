c880b39f25f681360d4da671cd54cb00279188e1
NaveOWO <pianowell96@gmail.com>
2022-10-31 16:57:08 +0900

    [ feat ] problem2_새로운 배열 원소를 담을 stack 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,12 @@
+//기능 구현 목록
+// - 새로운 배열 원소를 담을 stack 생성
+// - 배열 중복 검사
+// - 스택 문자열로 변환
+
 function problem2(cryptogram) {
-  var answer;
+  let codeStack = [];
+
+  codeStack.push(cryptogram[0]);
   return answer;
 }
 
