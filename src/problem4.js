aa501506ad18cca2c41749536e1e62c153a08298
OrangeVinyl <hap_hazard@naver.com>
2022-10-31 23:15:35 +0900

    Problem4) 구현사항 변경 및 초기화

--- src/problem4.js
+++ src/problem4.js
@@ -1,24 +1,7 @@
 function problem4(word) {
-  var answer = "";
-  let reverse,
-    abstemp,
-    ascii = 0;
-  const pattern = /[a-zA-Z]/g;
-  for (var i = 0; i < word.length; i++) {
-    if (word[i] === word[i].toUpperCase()) {
-      ascii = 65;
-    } else if (word[i] === word[i].toLowerCase()) {
-      ascii = 97;
-    } else if (!pattern.test(word[i]) && !pattern.test(word[i])) {
-      answer += word[i];
-      continue;
-    }
-    abstemp = Math.abs(ascii - word.charCodeAt(i));
-    reverse = ascii + 25 - abstemp;
-    answer = answer + String.fromCharCode(reverse);
-  }
-
+  var answer;
   return answer;
 }
+
 
 module.exports = problem4;
