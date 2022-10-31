06f78f4348e890c81abbe77ef90b852aa302802a
nalsae <nalsae0207@naver.com>
2022-10-31 20:41:39 +0900

    feat: Create an array to find out friends of user
    - user의 친구 정보를 담은 배열 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,17 @@
-function problem7(user, friends, visitors) {
-  var answer;
+const problem7 = (user, friends, visitors) => {
+  let answer = [];
+  let friendsOfUser = [];
+
+  friends.forEach((friend) => {
+    if (friend.includes(user)) {
+      friendsOfUser = [
+        ...friendsOfUser,
+        friend.indexOf(user) === 0 ? friend[1] : friend[0],
+      ];
+    }
+  });
+
   return answer;
-}
+};
 
 module.exports = problem7;
