a42903f614752e7f6725bad0b59d259f1ef4007f
kangju2000 <kangju2000@naver.com>
2022-10-31 15:30:55 +0900

    feat: getUserFriends 함수 선언

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,15 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function getUserFriends(user, friends) {
+  let friendArr = [user];
+
+  friends.forEach((el) => {
+    if (el[0] === user) friendArr.push(el[1]);
+    else if (el[1] === user) friendArr.push(el[0]);
+  });
+
+  return friendArr;
+}
+
 module.exports = problem7;
