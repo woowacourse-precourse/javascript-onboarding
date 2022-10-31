bb8bb1fdc545c9d89bd94082f8a3fe4f36d83f7f
sohyun <park.sohyun0430@gmail.com>
2022-10-31 19:09:14 +0900

    findEmail 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,18 @@
+function findEmail(temp_str,idx,nickname,email,result){
+  for(let j=idx+1;j<nickname.length;j++)
+  {
+    if(nickname[j].match(temp_str)){
+      if(!result.includes(email[j])){
+          result.push(email[j])
+      }
+      if(!result.includes(email[idx])){
+          result.push(email[idx])
+        }
+      }
+  }
+  return result;
+}
+
 function problem6(forms) {
   let result=[];
   let nickname =[]
@@ -6,6 +21,14 @@ function problem6(forms) {
     nickname.push(ele[1])
     email.push(ele[0])
   });
+
+  nickname.forEach(function(ele,idx){
+    for(let i=0;i<ele.length-1;i++)
+      {
+        let temp_str = ele[i]+ele[i+1]
+        result = findEmail(temp_str,idx,nickname,email,result);
+      }
+  })
 }
 
 module.exports = problem6;
