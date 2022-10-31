55afe21ee111050f4470a4b032bf4da8c560704c
yoon052 <ynj3126@hanmail.net>
2022-10-31 14:16:48 +0900

    feat: 2번 문제 해결
    [1주차] 2번 문제 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer =[];
+  const arr = Array.isArray(cryptogram);
+
+  answer += arr[0];
+
+  for(let i=1; i<arr.length; i++){
+    if(arr[i-1] !== arr[i]){
+      answer += arr[i];
+    }
+  }
+
   return answer;
 }
+
+problem2();
 
 module.exports = problem2;
