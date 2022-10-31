0350b47c5be1c29bdd76483688b11164637674d9
Suyeon <msjgr2801@gmail.com>
2022-10-31 16:06:24 +0900

    Fix: fix lowercase
    소문자 판별 로직의 범위가 잘못 돼 있던 것을  수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,37 +1,41 @@
-// 예외처리
-//  0 [x] word.length <= 0 : return answer
+/*
+  예외처리
+  0. [x] word.length <= 0 : return answer
 
-// 로직
-// 1 알파벳에 해당하는 map을 모두 만들어 대응
-// or 2 Ascii 코드 특성 사용 A+Z: 155 a+z: 219
+  로직
+  Ascii 코드 특성 사용 A+Z: 155 a+z: 219
 
-// ! 2번 로직을 선택하여 작성
-// 1. [ ] A-Z, a-z 는 합이 각각 155, 219가 되도록 치환할 수 있음
-// 2. 대문자의 경우 155에서 해당 문자의 아스키값을 뺀 값을 치환하여 추가
-// 3. 소문자의 경우 219에서 해당 문자의 아스키값을 뺀 값을 치환하여 추가
-// 4. 알파벳 외의 문자는 그대로 answer에 추가
- 
+  1. [x] A-Z, a-z 는 합이 각각 155, 219가 되도록 치환할 수 있음 - 각 변수 설정 
+  2. [x] 대문자의 경우 155에서 해당 문자의 아스키값을 뺀 값을 치환하여 추가
+  3. [x] 소문자의 경우 219에서 해당 문자의 아스키값을 뺀 값을 치환하여 추가
+  4. [x] 알파벳 외의 문자는 그대로 answer에 추가
+*/
+
 function problem4(word) {
   // 0. 예외처리
   if (word.length <= 0) return word;
-
+  const upperA = "A".charCodeAt(0);
+  const upperZ = "Z".charCodeAt(0);
+  const lowerA = "a".charCodeAt(0);
+  const lowerZ = "z".charCodeAt(0);
   let answer = '';
+
   // 1. 각 합 155, 219
   // A + Z : 155
-  const upper = "A".charCodeAt(0) + "Z".charCodeAt(0);
+  const upperSum = upperA + upperZ;
   // a + z : 219
-  const lower = "a".charCodeAt(0) + "z".charCodeAt(0);
+  const lowerSum = lowerA + lowerZ;
 
   for (let i=0; i<word.length; i++) {
     const currCode = word.charCodeAt(i);
-    if (currCode >= "A".charCodeAt(0) && currCode <= "Z".charCodeAt(0)) {
-    // 2. 대문자의 경우
-      answer += String.fromCharCode(upper - currCode);
-    } else if (currCode >= "a".charCodeAt(0) && currCode >= "z".charCodeAt(0)) {
+    if (currCode >= upperA && currCode <= upperZ) {
+      // 2. 대문자의 경우
+      answer += String.fromCharCode(upperSum - currCode);
+    } else if (currCode >= lowerA && currCode <= lowerZ) {
       // 3. 소문자의 경우
-      answer += String.fromCharCode(lower - currCode);
+      answer += String.fromCharCode(lowerSum - currCode);
     } else {
-      // 4. 그 외 값은 그대로 추가ㄴ
+      // 4. 그 외 값은 그대로 추가
       answer += word[i];
     }
   }
