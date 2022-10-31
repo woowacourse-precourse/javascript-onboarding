b99f12bed209b7ed5fad8483807f2795d7537027
Minju Park <project.mizzu@gmail.com>
2022-10-31 18:40:14 +0900

    Feat : change 변수 생성
    - 잔액을 의미하는 change 변수를 생성함
    - 특정 단위로 나누어진 후의 잔액이 담기게 됨

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,21 @@
+// [] 잔돈을 의미하는 change 변수 생성
+// [] result 배열 생성
+// [] money를 5만원권으로 나누고 몫은 result[0]에 저장, 나머지는 change에 저장
+// [] money를 1만원권으로 나누고 몫은 result[1]에 저장, 나머지는 change에 저장
+// [] money를 5천원권으로 나누고 몫은 result[2]에 저장, 나머지는 change에 저장
+// [] money를 1천원권으로 나누고 몫은 result[3]에 저장, 나머지는 change에 저장
+// [] money를 5백원으로 나누고 몫은 result[4]에 저장, 나머지는 change에 저장
+// [] money를 1백원으로 나누고 몫은 result[5]에 저장, 나머지는 change에 저장
+// [] money를 50원으로 나누고 몫은 result[6]에 저장, 나머지는 change에 저장
+// [] money를 10원으로 나누고 몫은 result[7]에 저장, 나머지는 change에 저장
+// [] money를 1원으로 나누고 몫은 result[8]에 저장, 나머지는 change에 저장
+// [] result 배열 반환
+
 function problem5(money) {
-  var answer;
-  return answer;
+  let change = 0;
+  return result;
 }
+
+console.log(problem5(50237));
 
 module.exports = problem5;
