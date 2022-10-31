8f4ffae861522f9ec4175e5d5cc7d9513217407a
yeonkr <1714882@sookmyung.ac.kr>
2022-11-01 03:32:08 +0900

    feat: problem7 solved

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,23 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer = [];
+  let relation = new Map();
+
+  friends.forEach((friend) => {
+    if (friend.indexOf(user) === -1) {
+      friend.forEach((score) => relation.set(score, 10));
+    } else friend.forEach((score) => relation.set(score, 0));
+  });
+
+  visitors.forEach((visitor) => {
+    if (!relation.has(visitor)) relation.set(visitor, 1);
+  });
+
+  let scoreArr = [...relation].sort((a, b) => b - a);
+
+  scoreArr.forEach((score) => {
+    if (score[1] > 0) answer.push(score[0]);
+    answer.slice(0, 5);
+  });
   return answer;
 }
 
