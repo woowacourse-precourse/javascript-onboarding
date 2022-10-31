bda55fa256edd0b4e391a1750960aa2dbd7dc232
Moyujin <scab12341@gmail.com>
2022-11-01 01:00:37 +0900

    5번 큰 금액부터 차례대로 나눠서 카운트 하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,18 @@
 function problem5(money) {
-  var answer;
+  var answer = exchangeMoney(money);
   return answer;
 }
+
+const exchangeMoney = (money) => {
+  const changes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [changes.length];
+
+  changes.forEach((change, idx) => {
+    result[idx] = Math.floor(money / change);
+    money = money % change;
+  });
+
+  return result;
+};
 
 module.exports = problem5;
