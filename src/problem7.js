9092a95a0ccab502fba10fb0ceff20c53f6185e4
unqeye <salyuan@naver.com>
2022-11-01 00:19:58 +0900

    feat: 문제7 각 사용자의 친구 목록 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,14 @@
+function getFriendList(friends) {
+  const friendList = {};
+  friends.forEach(([friend1, friend2]) => {
+    friendList[friend1] = friendList[friend1] || new Set();
+    friendList[friend1].add(friend2);
+    friendList[friend2] = friendList[friend2] || new Set();
+    friendList[friend2].add(friend1);
+  });
+  return friendList;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
