ba68aed3b60712d8aa5e1a156f3dd4550e5f752c
kwh97 <zzzang0512@naver.com>
2022-10-31 16:21:23 +0900

    feat(problem4): 요소가 소문자인 경우를 처리하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -25,4 +25,9 @@ function handleUpperCase(char) {
   return changeChar(upperArr, char);
 }
 
+function handleSmallCase(char) {
+  const lowerArr = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm','n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
+  return changeChar(lowerArr, char);
+}
+
 module.exports = problem4;
