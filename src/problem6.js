0788802b59f853bbe17264b14da7b6537c76f8c4
somin <smhs12@naver.com>
2022-10-31 16:16:21 +0900

    feat: problem6 / 닉네임 배열 만드는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,11 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+function problem6(forms) {}
+
+function getAllNickName(forms) {
+  const nickNames = [];
+  for (let i = 0; i < forms.length; i++) {
+    nickNames.push(forms[i][1]);
+  }
+  return nickNames;
 }
 
 module.exports = problem6;
