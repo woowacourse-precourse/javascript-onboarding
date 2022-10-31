ea1ac964944c9034619af7f1044da6d1d360cbba
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 04:54:28 +0900

    [p4] ASCII 코드를 활용한 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
+function ascii(num){
+  if(65 <= num && num <= 90){
+    if(num <= 77)
+      num = (78 - num) + 77;
+    else
+      num = 78 - (num - 77);
+    return num;
+  }
+  else return num;
+}
+
 function problem4(word) {
-  var answer;
+  var answer = "";
   return answer;
 }
 
