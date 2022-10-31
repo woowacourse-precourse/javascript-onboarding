f124c772018385af9c70d2920d30c1c8348527e3
jincde <asdzxc@kakao.com>
2022-11-01 00:58:27 +0900

    feat : problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = [];
+
+  for (let temp of moneys) {
+    answer.push(Math.floor(money / temp));
+    money %= temp;
+  }
   return answer;
 }
 
