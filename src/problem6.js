d4f2192b4ab1239ce0bca822eef7ed18c4c079f8
Jin1won <wlsdnjs16521@gmail.com>
2022-10-31 20:13:36 +0900

    [문제6] feat: 이메일 형식 체크를 위한 기능을 구현한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  const regex = /^[A-Za-z0-9]{2,10}@email.com/; // 이메일 형식 체크를 위한 정규표현식
+
+  return answer.sort(); // 오름차순으로 정렬한다.
+}
+
+// 이메일 형식인지 체크한다.
+function checkEmail(regex, email) {
+  return regex.test(email);
 }
 
 module.exports = problem6;
