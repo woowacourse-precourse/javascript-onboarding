e83ebec885a4ea21afdc2d7a8830afe18eb8da40
greeneeya <suzythetravelerr@gmail.com>
2022-11-01 00:28:11 +0900

    problem 6: 중복된 닉네임

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,23 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let tmp = new Set();
+  let names = new Map();
+
+  for (let user of forms) {
+    for (let i = 0; i < user[1].length - 1; i++) {
+      let word = user[1][i] + user[1][i + 1];
+      if (names.has(word)) {
+        tmp.add(names.get(word));
+        tmp.add(user[0]);
+        break;
+      } else {
+        names.set(word, user[0]);
+      }
+    }
+  }
+
+  answer = Array.from(tmp).sort();
+
   return answer;
 }
 
