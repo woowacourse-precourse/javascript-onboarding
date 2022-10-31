0e06ba857899a7cf8c59f3239dcafd2ab65b2291
Dongkeun <hongdongk@naver.com>
2022-10-31 20:17:01 +0900

    problem1: 페이지의 합,곱구하기

--- src/problem1.js
+++ src/problem1.js
@@ -5,6 +5,15 @@ function problem1(pobi, crong) {
 
   if(pobileft-pobiright !== -1 || crongleft-crongright !== -1 ) return -1;
 
+  const sum = (page) => {
+    const Page = String(page).split('').reduce((a,b) => Number(a) + Number(b), 0 );
+    return Page;
+  };
+  const multiply = (page) => {
+    const Page = String(page).split('').reduce((a,b) => Number(a) * Number(b), 1 );
+    return Page;
+  };
+
 }
 
 module.exports = problem1;
