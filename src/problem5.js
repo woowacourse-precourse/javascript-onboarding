f1f8f852f8637eb47b464933895d9bef4b1aa847
spsp8426 <spsp8426@naver.com>
2022-10-31 15:15:40 +0900

    5번 예외처리

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,7 @@ function problem5(money) {
   let answer = [];
 
   const array = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  if(money < 1 || money > 1000000) return false;
 
   for (let coin of array) {
     answer.push(Math.floor(money / coin));
@@ -9,5 +10,4 @@ function problem5(money) {
   }
   return answer;
 }
-
 module.exports = problem5;
