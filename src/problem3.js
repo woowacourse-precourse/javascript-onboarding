3d07a927fb15221297d0fc352b9ad6b7d5cb09fb
SorinKwon <ksl020700@gmail.com>
2022-10-31 11:02:30 +0900

    feat(problem3): 문자열에서 3,6,9가 들어가는 숫자 찾는 기능 추가
    문자열에서 3,6,9가 들어가는 숫자를 찾고 횟수를 세는 기능을 추가함

--- src/problem3.js
+++ src/problem3.js
@@ -5,8 +5,26 @@ function createString(n){
   }
   return s;
 }
+function findNumber(n){
+  let result=0;
+  for(let i=0;i<n.length;i++){
+      if(n.charAt([i])==='3'){
+          ++result;
+      }
+      if(n.charAt([i])==='6'){
+          ++result;
+      }
+      if(n.charAt([i])==='9'){
+          ++result;
+      }
+  }
+  return result;
+}
+
+
 function problem3(number) {
   let answer;
+  answer=findNumber(createString(number));
 
   return answer;
 }
