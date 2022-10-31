e7107a6b34c3798a72d6dc92fe67dec62a5e74d9
userHWSEO <gmldnjseocnd@gmail.com>
2022-10-31 14:45:56 +0900

    feat: SNS의 친구 추천 알고리즘 기능 구현
    - 친구 추천 알고리즘 기능 구현.
    
    - 테스트케이스 확인 완료.
    
    - 간단한 주석 추가.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,43 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let result = [];
+  let overlapFriends = [];
+  let friendsObj = {};
+
+  // 사용자와 함께 아는 친구의 점수를 객체에 넣음
+  for (let i = 0; i < friends.length; i++) {
+    let friendsName = friends[i][1];
+    if (friendsName !== user) {
+      friendsObj[friendsName] === 10
+        ? (friendsObj[friendsName] += 10)
+        : (friendsObj[friendsName] = 10);
+    }
+  }
+  // 중복되는 친구들을 배열에 넣음
+  for (let i = 0; i < friends.length; i++) {
+    overlapFriends.push(friends[i][0]);
+  }
+
+  // 중복되는 친구들의 중복을 제거
+  let set = new Set(overlapFriends);
+  let friendsArr = [...set];
+
+  // 방문자의 점수를 객체에 넣음
+  for (let i = 0; i < visitors.length; i++) {
+    for (let j = 0; j < friendsArr.length; j++) {
+      if (visitors[i] !== friends[i][0]) {
+        friendsObj[visitors[i]] === 1
+          ? (friendsObj[visitors[i]] += 1)
+          : (friendsObj[visitors[i]] = 1);
+      }
+    }
+  }
+
+  // 객체를 배열로 반환
+  for (key in friendsObj) {
+    result.push(key);
+  }
+
+  return result;
 }
 
 module.exports = problem7;
