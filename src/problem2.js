6502efec085c1b652806c1c1e1a535f92645b137
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 04:29:11 +0900

    FEAT : problem2 - 문자열 배열로 변경 및 필요 변수 선언

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 문자열을 배열로 바꾼다.
+  let cryptogramArray = Array.from(cryptogram);
+  // index + 1의 값을 환인할 변수 / 연속된 값이 있는지 없는지 확인할 수 있는 키로 적용
+  let tmpIndex = null;
+  
 }
 
 module.exports = problem2;
