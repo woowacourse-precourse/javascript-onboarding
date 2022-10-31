8cc5ef9dfea5ce0c98013b25537e2d8fa124b947
JangHoonSeok <orirori512@naver.com>
2022-11-01 01:52:59 +0900

    feat: problem4. 문자열의 Case 판단하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,19 +3,31 @@
 // 소문자 : 97 ~ 122
 // 32 : Space
 function problem4(word) {
-  var answer;
-  let list = [];
-  list = wordToASCII(word);
-  return answer;
+  const beforeChange = wordToASCII(word);
+  let afterChange = "";
+
+  for (item of beforeChange) {
+    const item_status = charCase(item);
+    if (item_status === 0) afterChange += upperChange(item);
+    else if (item_status === 1) afterChange += lowerChange(item);
+    else afterChange += " ";
+  }
+
+  return afterChange;
 }
 
 function wordToASCII(word) {
-  // word를 ASCII 코드로 변환하는 함수.
+  // 문자열을 ASCII 코드로 변환하는 함수.
   let arrayASCII = [];
-  for (item of word) {
-    arrayASCII.push(item.charCodeAt(0));
-  }
+  for (item of word) arrayASCII.push(item.charCodeAt(0));
   return arrayASCII;
+}
+
+function charCase(item) {
+  // 해당하는 문자가 대문자면 0, 소문자면 1, Space면 2를 return.
+  if (item >= 65 || item <= 90) return 0; // 대문자면 0을 return
+  else if (item >= 97 || item <= 122) return 1; // 소문자면 1을 return
+  else return 2; // space면 2를 return
 }
 
 module.exports = problem4;
