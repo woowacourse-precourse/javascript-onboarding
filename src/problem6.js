bc1d0be07b0a79c6d5678147efb6c42c61b8e799
heejorika <y20001206@gmail.com>
2022-11-01 06:02:35 +0900

    저장 형식 리스트=>객체

--- src/problem6.js
+++ src/problem6.js
@@ -10,11 +10,6 @@ module.exports = problem6;
 forms= [ ["제이엠","jm@email.com"], ["jason@email.com", "제이슨ss"], ["woniee@emil.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠",4] ] 
 //| ["jason@email.com", "jm@email.com", "mj@email.com"] |
 
-/*
-
-4. 두 글자 이상의 문자가 연속적으로 중복되는 경우 찾기
-5. result 리스트 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거함 
-*/
 
 // 한글 아스키 코드 검사
 function checkHangle(c){
@@ -30,24 +25,24 @@ function checkValid(forms){
     forms = forms.slice(0,10001);
   }
 
-  let validForms = []
+  let validForms = {}
   for(f of forms){
     // 길이가 2보다 크거나 작을 수 없음 
     if (f.length!=2) continue;
 
     // f[0]이 이메일이 아닐 경우
     if (f[0].includes("a")){
-      if (f[0].length<11 || f[0].length>20) continue; // 이메일 길이 제한
-      email = f[0].split("@"); 
+      email = f[0]
       nickname = f[1];
     }else{
-      if (f[1].length<11 || f[1].length>20) continue; // 이메일 길이 제한
-      email = f[1].split("@"); 
+      email = f[1] 
       nickname = f[0];
     }
 
     // 이메일은 도메인-email.com
-    if (email[1] != "email.com") continue; 
+    if (email.length<11 || email.length>20) continue; // 이메일 길이 제한
+    e = email.split('@')
+    if (e[1] != "email.com") continue; 
 
     // 닉네임은 한글만, 1<=length<20
     if (nickname.length<1 || nickname.length>20) continue; // 닉네임 길이 제한
@@ -57,14 +52,19 @@ function checkValid(forms){
       if (!checkHangle(code)) continue;
     }
 
-    validForms.push(f);
+    validForms.email=nickname;
   }
 
   return validForms;
 }
 
 function solution(forms){
-  let f = checkValid(forms);
+  let validF = checkValid(forms); // 객체로 반환 받음
+
+  // 
+  for(form of validF){
+    for(f of form)
+  }
 
 
 }
@@ -73,3 +73,8 @@ function solution(forms){
 //신청받은 닉네임 중 **같은 글자가 연속적으로 포함** 되는 닉네임을 작성한 지원자의 이메일 목록을 return 하도록 solution 메서드를 완성하라.
 
 
+/*
+
+4. 두 글자 이상의 문자가 연속적으로 중복되는 경우 찾기
+5. result 리스트 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거함 
+*/
