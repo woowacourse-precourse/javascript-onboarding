a109feae3b27bede465d922259fcd47eb76e8257
hyeongdong <khd941018@gmail.com>
2022-10-31 22:14:54 +0900

    [문제5] docs : 기능 목록 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
-function problem5(money) {
-  var answer;
-  return answer;
-}
+/*
+--기능 목록--
+1. 주어진 money를 가장 적은 개수의 지폐와 동전으로 변환한다.
+
+*/
+
+function problem5(money) {}
 
 module.exports = problem5;
