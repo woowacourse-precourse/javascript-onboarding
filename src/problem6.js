cbce0298c4ef050e62b51fbb07535fee52efdedc
gsong <rmsdlf1206@gmail.com>
2022-10-31 21:49:01 +0900

    feat: problem6 배열메서드 배열메서드 search(~) !== -1 를 통해 두개의 문자가 forms에 존재하는지 단어들에 포함되는지를 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,19 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let wordArray = checkDuplicateWord(forms);
+  removeDuplicateArray(wordArray);
+}
+function checkDuplicateWord(forms) {
+  const result = [];
+  forms.forEach((element, startPoint) => {
+    for (let i = startPoint + 1; i < forms.length; i++) {
+      for (let j = 0; j < element[1].length - 1; j++) {
+        if (forms[i][1].search(element[1].substr(j, 2)) !== -1) {
+          result.push(element, forms[i]);
+        }
+      }
+    }
+  });
+  return result;
 }
 
 module.exports = problem6;
