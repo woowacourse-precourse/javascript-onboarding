a41bf5c2b63f7e6589a52da2bbba7bae678cca07
lsy20140 <lsy20140@naver.com>
2022-11-01 01:06:35 +0900

    feat: [문제7] 합산된 점수 객체의 key 이름순으로 정렬 후 return

--- src/problem7.js
+++ src/problem7.js
@@ -11,61 +11,49 @@ function getInterFriendScore(user, friends){
   const userFriendList = getUserFriend(user, friends);
   const friendInclArr = [];
   let interFriendList = [];
+  
   for(let i = 0 ; i<friends.length ; i++){
     if(userFriendList.filter(v => friends[i].includes(v)).length == 1){
       friendInclArr.push(friends[i]);
     }
   }
-
   interFriendList = friendInclArr.reduce(function (acc, cur) {
     return [...acc, ...cur];
   });
-
   interFriendList = interFriendList.filter(v => !userFriendList.includes(v));
   interFriendList = interFriendList.filter(v => !v.includes(user));
+
   const friendCount = (arr) => arr.reduce((ac, v) => ({ ...ac, [v]: (ac[v] || 0) + 10 }), {});
   return friendCount(interFriendList);
 }
 
-// function getVisitorScore(user, friends, visitors){
-//   const userFriendList = getUserFriend(user, friends);
-//   const visitList = visitList.filter(v => !userFriendList.includes(v));
-//   const visitCount = (arr) => arr.reduce((ac, v) => ({ ...ac, [v]: (ac[v] || 0) + 1 }), {});
-//   return visitCount(visitors);
-// }
-
 function getVisitList(user, friends, visitors){
    const userFriendList = getUserFriend(user, friends);
    const visitList = visitors.filter(v => !userFriendList.includes(v));
-
    return visitList;
 }
 
 function getScore(user, friends, visitors){
   const interFriendScore = getInterFriendScore(user, friends);
   const visitList = getVisitList(user, friends, visitors);
-
   
   for(let i = 0; i<visitList.length ; i++){
     if(interFriendScore.hasOwnProperty(visitList[i])){
-      console.log(visitList[i]+"있음!");
       interFriendScore[visitList[i]]+=1;
     }
     else{
-      console.log(visitList[i]+"없음!")
       interFriendScore[visitList[i]] = 0;
       interFriendScore[visitList[i]] +=1;
-      console.log(interFriendScore[visitList[i]]);
-
     }
   }
-  console.log(interFriendScore);
-
+  return interFriendScore;
 }
 
 function problem7(user, friends, visitors) {
-  
-  return getScore(user, friends, visitors);
+  const scoreObj = getScore(user, friends, visitors);
+  const obj = Object.keys(scoreObj);
+
+  return obj;
 }
 
 module.exports = problem7;
