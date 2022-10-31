9b3e3f9f86784a64ccbd3f7a721b8b08b0e3f924
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-10-31 13:31:17 +0900

    feat: [문제 5] 기능 요구 사항 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const price = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = Array.from(Array(9), () => 0);
+
+  for (let i = 0; i < 9; i++) {
+    answer[i] += Math.floor(money / price[i]);
+    money = money % price[i];
+  }
+
   return answer;
 }
 
