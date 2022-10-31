7abf57a0d70a748590525cf1ef004cbd26e32dc7
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 14:53:03 +0900

    feat : problem4 객체를 생성하는 함수 추가
    알파벳 문자열과 뒤집어진 알파벳 문자열을 받아서 키-값
    형태로 만들어 리턴하는 함수를 생성했고, 이 함수를 사용해
    대문자, 소문자 객체를 생성했습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
+const makeObject = (str, frogStr) => {
+    const obj = {};
+
+    for (let i = 0; i < 26; i++) {
+        obj[str[i]] = frogStr[i];
+    }
+
+    return obj;
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+    let answer = '';
+    const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    const frog = alphabet.split('').reverse().join('');
+
+    const uppercase = makeObject(alphabet, frog);
+    const lowercase = makeObject(alphabet.toLowerCase(), frog.toLowerCase());
+    return answer;
 }
 
 module.exports = problem4;
