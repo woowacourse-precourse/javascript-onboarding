9accacf9fece8ddd60d24ce7de9fa2ae7bbe1093
chiman <rlaclghks123@naver.com>
2022-10-31 18:08:26 +0900

    feat: problem-1 isValid 함수 생성
    예외처리를 위한 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,28 @@
+function isValid(pageInfo) {
+  const leftPage = pageInfo[0];
+  const rightPage = pageInfo[1];
+  const myRegExp = /^[0-9]+$/;
+  if (leftPage <= 1 || rightPage >= 400) {
+    return false;
+  }
+  if (leftPage % 2 === 0 || rightPage % 2 !== 0) {
+    return false;
+  }
+  if (rightPage - leftPage !== 1) {
+    return false;
+  }
+  if (!myRegExp.test(leftPage) || !myRegExp.test(rightPage)) {
+    return false;
+  }
+  if (pageInfo.length !== 2) {
+    return false;
+  }
+  return true;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if (!isValid(pobi) || !isValid(crong)) return -1;
   return answer;
 }
 
