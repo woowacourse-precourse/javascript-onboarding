c94d8bb2476a10c12279b08dca9b083f12907aa9
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-31 14:46:06 +0900

    fix problem7

--- src/problem7.js
+++ src/problem7.js
@@ -4,10 +4,10 @@ function problem7(user, friends, visitors) {
   var friendsCandidate = [];
   var scoreMap = new Map();
 
-  friends.forEach((two) => {
-    if (two.includes(user)) {
-      userFriends.push(two[1 - two.indexOf(user)]);
-    } else friendsCandidate.push(two);
+  friends.forEach((pair) => {
+    if (pair.includes(user)) {
+      userFriends.push(pair[1 - pair.indexOf(user)]);
+    } else friendsCandidate.push(pair);
   });
 
   for (var i = friendsCandidate.length - 1; i >= 0; i--) {
@@ -18,9 +18,9 @@ function problem7(user, friends, visitors) {
   }
 
   userFriends.forEach((userFriend) => {
-    friendsCandidate.forEach((two) => {
-      if (two.includes(userFriend)) {
-        var newFriend = two[1 - two.indexOf(userFriend)];
+    friendsCandidate.forEach((pair) => {
+      if (pair.includes(userFriend)) {
+        var newFriend = pair[1 - pair.indexOf(userFriend)];
         if (!scoreMap.has(newFriend)) scoreMap.set(newFriend, 10);
         else scoreMap.set(newFriend, scoreMap.get(newFriend) + 10);
       }
