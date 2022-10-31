a8d8379f7e33405014d2017a1517079a6d623c8e
김세환 <sehwan@gimsehwan-ui-MacBookPro.local>
2022-11-01 01:36:14 +0900

    chore: 주석정리

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,11 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+// ### 기능들
+// 1. 사용자들의 친구관계 정리하기
+// 2. user의 친구를 함께 아는 사람들 찾기
+// 3. 함께 아는 친구의 수 점수 계산
+// 4. 타임라인에 방문한 횟수 점수 계산
+// 5. 이미 user와 친구인 관계 제외하기
+// 6. 친구목록을 점수별 정렬하기
