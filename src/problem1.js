03ccce6da9cefd68251276fc3a064c211cad512d
letsjo <hyunoh.jo@gmail.com>
2022-10-31 22:45:13 +0900

    [problem1] Simplify code for readability

--- src/problem1.js
+++ src/problem1.js
@@ -1,47 +1,37 @@
+console.log(problem1([97, 98], [197, 198]));
+// console.log(splitNumber(97));
+
 function problem1(pobi, crong) {
   if (validationArr(pobi) && validationArr(crong)) {
-    return pageGameApp(pobi, crong);
+    return pageGameApp([pobi, crong]);
   }
   return -1;
 }
 
-function validationArr([leftPage,rightPage]) {
-  return ![leftPage,rightPage].some((page) => 1 > page || page > 400) && rightPage - leftPage == 1 ? true : false;
+function validationArr([leftPage, rightPage]) {
+  return [leftPage, rightPage].every((page) => 1 <= page && page <= 400) && rightPage - leftPage == 1 ? true : false;
 }
 
-function pageGameApp(numArrA, numArrB) {
-  let maxNumA = maxValueCalculator([splitNumber(numArrA[0]), splitNumber(numArrA[1])]);
-  let maxNumB = maxValueCalculator([splitNumber(numArrB[0]), splitNumber(numArrB[1])]);
-  return maxNumA == maxNumB ? 0 : maxNumA > maxNumB ? 1 : 2;
+function pageGameApp(PageListArr) {
+  const [pobiMaxScore, crongMaxScore] = PageListArr.map((pageArr) => maxValueCalculator(pageArr));
+  if (pobiMaxScore == crongMaxScore) return 0;
+  return pobiMaxScore > crongMaxScore ? 1 : 2;
 }
 
-function maxValueCalculator(arr) {
-  return Math.max(sumArr(arr[0]), mulArr(arr[0]), sumArr(arr[1]), mulArr(arr[1]));
+function maxValueCalculator(pageArr) {
+  return Math.max(...sumArr(pageArr), ...mulArr(pageArr));
 }
 
-function splitNumber(n) {
-  const splitArr = [];
-  while (n > 0) {
-    splitArr.unshift(n % 10);
-    n = parseInt(n / 10);
-  }
-  return splitArr;
+function splitNumber(number) {
+  return (number+"").split("").map(Number);
 }
 
-function sumArr(arr) {
-  let totalSum = 0;
-  for (let i = 0; i < arr.length; i++) {
-    totalSum += arr[i];
-  }
-  return totalSum;
+function sumArr(pageArr) {
+  return pageArr.map((page)=> splitNumber(page).reduce((totalSum, number) => totalSum += number));
 }
 
-function mulArr(arr) {
-  let totalMul = 1;
-  for (let i = 0; i < arr.length; i++) {
-    totalMul *= arr[i];
-  }
-  return totalMul;
+function mulArr(pageArr) {
+  return pageArr.map((page)=> splitNumber(page).reduce((totalMul, number) => totalMul *= number,1));
 }
 
 module.exports = problem1;
