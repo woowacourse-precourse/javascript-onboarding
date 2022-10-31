524e0d99d08ff87c9f945c7f25ac59cba4ec3c40
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 04:37:01 +0900

    [p3] 숫자에 있는 369세는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,14 @@
+function count(num){
+  var cnt = 0;
+  while (num) {
+    var tmp = num % 10;
+    if(tmp == 3 || tmp == 6 || tmp == 9)
+      cnt++;
+    num = parseInt(num / 10);
+  }
+  return cnt;
+}
+
 function problem3(number) {
   var answer;
   return answer;
