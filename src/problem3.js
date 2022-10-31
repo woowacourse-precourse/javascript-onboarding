fc967f738495f46df17b2aa60efdae8122cd4149
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 21:54:53 +0900

    [Feat:Problem3] 최종

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,16 @@
 const makeArr = (number) => String(number).split("");
 
 const filterArrayBy = (arr, ...by) => arr.filter((v) => by.includes(+v));
+
+function problem3(number) {
+  let cnt = 0;
+  for (let i = 0; i <= number; i++) {
+    const temp = filterArrayBy(makeArr(i), 3, 6, 9);
+    cnt += temp.length;
+  }
+
+  return cnt;
+}
+
+module.exports = problem3;
+
