1b1564a3867291e75e162e49eab5c0d910f163b1
OrangeVinyl <hap_hazard@naver.com>
2022-11-01 02:13:54 +0900

    Problem1) 예외사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,12 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+//예외사항 : 첫, 마지막 페이지일때 예외. 다음 페이지가 이전 페이지보다 +1이 아닐 때
+function exceptions(arr) {
+  if (arr[0] + 1 !== arr[1]) return false;
+  if (arr[0] === 1 || arr[1] === 400) return false;
+  return true;
+}
+
+
 module.exports = problem1;
