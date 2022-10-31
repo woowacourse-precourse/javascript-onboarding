16e096200170782c933b782965986676638bf908
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-10-31 17:37:04 +0900

    refactor: Fix function of total count and split array

--- src/problem3.js
+++ src/problem3.js
@@ -3,22 +3,17 @@ function problem3(number) {
 
   let arr = numberArr(number);
 
-  answer = findNumber(arr, "3") + findNumber(arr, "6") + findNumber(arr, "9");
-  
+  answer = arr.filter(
+    (element) => "3" === element || "6" === element || "9" === element
+  ).length;
+
   return answer;
 }
 
 function numberArr(number) {
-  let array = [];
-  let newArray = "";
-  for (let i = 1; i <= number; i++) {
-    array.push(i.toString().split(""));
-  }
-  return newArray.concat(array).split(",");
-}
-
-function findNumber(arr, findNum) {
-  return arr.filter((element) => findNum === element).length;
+  return Array.from({ length: number }, (_, idx) => idx + 1)
+    .join("")
+    .split("");
 }
 
 console.log(problem3(33));
