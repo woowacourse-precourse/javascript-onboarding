b4f1eb8ff39dead9c88286549bd139ed11a58ee5
daal2quf <daal2quf@gmail.com>
2022-11-01 02:09:43 +0900

    map()메서드를 이용해 필요없는 함수 제거

--- src/problem4.js
+++ src/problem4.js
@@ -1,63 +1,35 @@
-function problem6(forms) {
-  var answer;
-  let userEmail = [];
-  let userName = [];
-  let idx = new Set()
+function problem4(word) {
+  let answer;
 
-  const seperate = (array) => {
-    userEmail.push(array[0])
-    userName.push(array[1])
-  }
-  
-  forms.forEach(element => {
-    seperate(element)
-  });
+  const frogConvertor = (str) => {
+    if (str === ' ') return str
 
-  for (let i = 0; i < userName.length; i++) {
-    let ref1 = userName[i].slice(0, 2)
-    let ref2 = userName[i].slice(1)
-    for(let j = i + 1; j <userName.length; j++ ) {
-      let comp1 = userName[j].slice(0, 2)
-      let comp2 = userName[j].slice(1)
-      if(ref1 === comp1 || ref1 === comp2 ||ref2 === comp1 || ref2 === comp2 ) {
-        idx.add(i)
-        idx.add(j)
-      }
+    if (motherWords.includes(str)) {
+      let idx = motherWords.indexOf(str)
+      let newWord = frogWords[idx]
+      return newWord
+    } else if (lowerMotehrWords.includes(str)) {
+      let idx = lowerMotehrWords.indexOf(str)
+      let newWord = lowerFrogWords[idx]
+      return newWord
     }
+
   }
 
-  let ArrIdx = [...idx]
-  let duplication = [];
-  for(let i = 0; i < ArrIdx.length; i++) {
-    duplication.push(userEmail[ArrIdx[i]])
-  }
-  duplication.sort((a, b) => a.localeCompare(b))
-  answer = duplication;
-  console.log(answer)
-  return answer;
-}
+  let motherWords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let frogWords = [...motherWords].sort((a, b) => b.localeCompare(a))
+  let lowerMotehrWords = lowerCase(motherWords)
+  let lowerFrogWords = lowerCase(frogWords) 
+  let frogSay = '';
   
+  
+  for (let i = 0; i < word.length; i++) {
+    frogSay += frogConvertor(word[i])
+  }
+  console.log(frogSay)
+  return frogSay;
+}
 
+problem4("I love you")
 
-
-
-let forms = [["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]]
-
-problem6(forms)
-
-
-
-module.exports = problem6;
-
-
-
-
-// for(let i = 0; i < forms.length; i++) {
-//   if(!(i % 2)) {
-//     userEmail.push(forms[i][0])
-//     userName.push(forms[i][1])
-//   } else if (i % 2) {
-//     userEmail.push(forms[i][0])
-//     userName.push(forms[i][1])
-//   }
-// }
+module.exports = problem4;
