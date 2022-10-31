023cd096fd2eb8fc3e2372a064ce77ab3e9b3e04
confidential-nt <youyoy689@gmail.com>
2022-10-31 14:16:44 +0900

    요구사항 6번 리팩토링

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,4 @@
-function problem6(forms) {
-  if (forms.length < 1 || forms.length > 10000) return;
-
+function findDuplicatedNickname(forms) {
   const result = [];
 
   // 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어있는지 확인. 맞다면 result 배열에 푸쉬.
@@ -13,6 +11,14 @@ function problem6(forms) {
       }
     }
   });
+
+  return result;
+}
+
+function problem6(forms) {
+  if (forms.length < 1 || forms.length > 10000) return;
+
+  const result = findDuplicatedNickname(forms);
 
   // result의 결과로 나온 중복된 것들 제거 위해 오브젝트로 변형
   const reduced = result.reduce((pre, cur) => {
