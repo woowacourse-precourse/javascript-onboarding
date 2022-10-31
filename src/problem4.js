047d00fae0da307f19005b4e351b27c11d074a3f
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-31 17:30:11 +0900

    problem4: function list

--- src/problem4.js
+++ src/problem4.js
@@ -1,14 +1,23 @@
 function problem4(word) {
   var answer;
-  var originArr = word.split("").map((each) => each.charCodeAt());
-  var newArr = originArr.map((each) => {
-    if (each >= 65 && each <= 90) {
-      return 155 - each;
-    } else if (each >= 97 && each <= 122) {
-      return 219 - each;
-    } else return each;
-  });
-  answer = String.fromCharCode(...newArr);
+
+  function toAscii(wordString) {
+    var originArray;
+    return originArray;
+  }
+
+  function convert(originArray) {
+    var newArray;
+    return newArray;
+  }
+
+  function toChar(newArray) {
+    var newWord;
+    return newWord;
+  }
+
+  answer = toChar(convert(toAscii(word)));
+
   return answer;
 }
 
