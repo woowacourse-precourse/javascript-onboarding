6efa49a96eb0d097007013e56ae00de0821ef435
somin <smhs12@naver.com>
2022-10-31 15:52:01 +0900

    feat: problem3 / 각각의 자릿수에 3 또는 6 또는 9를 포함하는지 확인하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
 function problem3(number) {
-  var answer;
+  const answer = countClap(number);
   return answer;
+}
+
+function countClap(number) {
+  let clap = 0;
+  for (let i = 1; i <= number; i++) {
+    let current = i;
+    while (current !== 0) {
+      if (current % 10 === 3 || current % 10 === 6 || current % 10 === 9) {
+        clap++;
+      }
+      current = Math.floor(current / 10);
+    }
+  }
+  return clap;
 }
 
 module.exports = problem3;
