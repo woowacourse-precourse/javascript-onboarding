763321d704be2edd6cbd2a214147ffa9a4885bc1
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-31 15:36:31 +0900

    problem1: function list

--- src/problem1.js
+++ src/problem1.js
@@ -1,42 +1,14 @@
 function problem1(pobi, crong) {
   var answer;
-  var players = [pobi, crong];
-  if (
-    players.filter(
-      (player) =>
-        (player[1] - player[0] != 1) |
-        player.includes(1) |
-        player.includes(2) |
-        player.includes(399) |
-        player.includes(400)
-    ).length > 0
-  )
-    return (answer = -1);
 
-  var [pobiScore, crongScore] = players.map((player) => {
-    var playerMax = Number.MIN_SAFE_INTEGER;
-    player.forEach((number) => {
-      var num = number;
-      var result = 0;
-      while (num > 0) {
-        result += num % 10;
-        num = parseInt(num / 10);
-      }
-      if (result > playerMax) playerMax = result;
-      num = number;
-      result = 1;
-      while (num > 0) {
-        result *= num % 10;
-        num = parseInt(num / 10);
-      }
-      if (result > playerMax) playerMax = result;
-    });
-    return playerMax;
-  });
+  function exception(player1, player2) {}
 
-  if (pobiScore > crongScore) answer = 1;
-  else if (pobiScore < crongScore) answer = 2;
-  else if (pobiScore == crongScore) answer = 0;
+  function score(player) {}
+
+  function result(player1, player2) {}
+
+  if (exception(pobi, crong)) answer = -1;
+  else answer = result(score(pobi), score(crong));
 
   return answer;
 }
