fa9de5602341055aafe82e52831d3947c20bc99e
nara04040 <nara04040@naver.com>
2022-10-31 14:40:19 +0900

    feat : 각 함수를 가지고 게임의 승자를 구하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,23 @@
 function problem1(pobi, crong) {
   let answer;
+  // 예외사항
+  if (checkingExceptions(pobi) === -1 || checkingExceptions(crong) === -1) {
+    return -1;
+  }
+  //  최대숫자
+  let pobiMaxNum = getCrewMaxNum(getCrewMaxNum(multiPageNum(pobi[0]), sumPageNum(pobi[0])), getCrewMaxNum(multiPageNum(pobi[1]), sumPageNum(pobi[1])));
+  let crongMaxNum = getCrewMaxNum(getCrewMaxNum(multiPageNum(crong[0]), sumPageNum(crong[0])), getCrewMaxNum(multiPageNum(crong[1]), sumPageNum(crong[1])));
+  //승자구하기
+  if (pobiMaxNum > crongMaxNum) {
+    answer = 1;
+  } else if (pobiMaxNum < crongMaxNum) {
+    answer = 2;
+  } else {
+    answer = 0;
+  }
   return answer;
 }
-// 예외사항을 판별하는 함수
+
 const checkingExceptions = (pages) => {
   if (pages.length !== 2) return -1;
   if (pages[1] - pages[0] !== 1) return -1;
