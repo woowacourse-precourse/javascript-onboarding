1f3cfa06fb94569148e91159a2997a7af716c67f
Jeong <jeong@HwiJeongs-MacBook-Pro.local>
2022-10-31 13:51:22 +0900

    우테코 프리코스 1주차 온보딩 미션

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    number_arr = String(i).split("");
+
+    const divison_arr = number_arr.map((x) => Number(x) > 0 && Number(x) % 3);
+
+    const clap = divison_arr.filter((x) => x === 0).length;
+    answer += clap;
+  }
   return answer;
 }
-
 module.exports = problem3;
