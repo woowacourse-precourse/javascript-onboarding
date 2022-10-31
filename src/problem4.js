c84f1e89d1cab0c4c1f6eaa0f7601b9f04582f09
zzinao <kjn0265@gmail.com>
2022-10-31 22:47:31 +0900

    feat: problem4 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
-function problem4(word) {
-  var answer;
-  return answer;
+function convertStr(code) {
+  const upperSumCode = 155;
+  const lowerSumCode = 219;
+
+  if (code >= 65 && code <= 90) return upperSumCode - code;
+  if (code >= 97 && code <= 122) return lowerSumCode - code;
+
+  return code;
 }
 
+function problem4(word) {
+  const uniCodeArr = [...word]
+    .map((char, idx) => word.charCodeAt(idx))
+    .map(convertStr);
+  return String.fromCharCode(...uniCodeArr);
+}
 module.exports = problem4;
