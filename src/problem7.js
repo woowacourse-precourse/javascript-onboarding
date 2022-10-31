741e118ae48ee73801a360c7a19a8c2ceba1e86f
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-10-31 19:29:24 +0900

    문제7 user의 친구의 친구(FOAF) 점수 +10점

--- src/problem7.js
+++ src/problem7.js
@@ -27,6 +27,37 @@ function problem7(user, friends, visitors) {
       friendsList[newIndexB] = [idA];
     }
   }
+
+  //출력
+  for (var i=0; i<nameList.length; i++){
+    console.log(nameList[i],"의 친구 목록 : ",friendsList[i]);
+  }
+
+  let recommendedUserList = []; //점수가 있는 사용자 리스트
+  let recommendedUserScore = []; //각 사용자의 추천 점수
+  const indexUser = nameList.indexOf(user);
+  if(indexUser!=-1){
+    let userFriendsList = friendsList[indexUser]; //user의 친구목록
+    for (var userFriend of userFriendsList){
+      const index = nameList.indexOf(userFriend);
+      if(index!=-1){
+        for(var FOAF of friendsList[index]){ //user 친구의 친구(FOAF) 목록 보기 (단, FOAF가 user와 친구인지는 판단X)
+          const indexFOAF = recommendedUserList.indexOf(FOAF);
+          if(FOAF == user){ //user는 PASS
+            continue; 
+          }
+          if(indexFOAF != -1){ //FOAF가 점수 유저 목록에 있다면 점수 +10점
+            recommendedUserScore[indexFOAF] += 10;
+          }
+          else{ //점수 목록 생성 후 점수 초기화(10점)
+            recommendedUserList.push(FOAF);
+            let newIndexFOAF = recommendedUserList.length-1;
+            recommendedUserScore[newIndexFOAF] = 10; //push와 동일
+          }
+        }
+      }   
+    }
+  }  
   
   return answer;
 }
