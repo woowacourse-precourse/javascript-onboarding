cae3ce3456f5cd5c262d0ca495a9c6f6e472fafc
Minhye Kang <alsgp9898@naver.com>
2022-11-01 00:33:29 +0900

    feat(PROBLEM4): 정렬된 알파벳 목록을 만드는 함수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+function problem4(word) {}
+
+const getOrderAlphabet = () => {
+  const orderAlphabetArr = [];
+
+  for (let i = 65; i <= 90; i++) {
+    orderAlphabetArr.push(String.fromCharCode(i));
+    orderAlphabetArr.push(String.fromCharCode(i).toLowerCase());
+  }
+
+  return orderAlphabetArr;
+};
 
 module.exports = problem4;
