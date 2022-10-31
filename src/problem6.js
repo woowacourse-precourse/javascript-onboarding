007b449c6cbd94ac6a56b8a7b32f47865ed06889
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 18:30:04 +0900

    단어 Slicing

--- src/problem6.js
+++ src/problem6.js
@@ -3,9 +3,11 @@
 
   1. 정상적인 이메일인지 확인하기
   2. 정상적인 닉네임인지 확인하기
-  3. 닉네임에 접근하여 두자리씩 비교해, 중복되는 사람을 이메일 보낼 배열에 넣는다.
-  2-1. 이때 이미 배열 넣은 사람은 제외하고 비교한다.
-
+  3. 단어 자르기
+  4. 닉네임에 접근하여 두자리씩 비교해, 중복되는 사람을 이메일 보낼 배열에 넣는다.
+  4-1. 이때 이미 배열 넣은 사람은 제외하고 비교한다.
+  5. 중복되는 닉네임을 가진 이메일을 배열에 추가하기
+  6. 배열에서 중복되는 요소 제거하기
 
   */
 
@@ -14,12 +16,21 @@ function problem6(forms) {
   const email_regex = new RegExp(/^(\w{2,10})(@email\.com)$/);
   const ninkname_regex = new RegExp(/^([ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{1,19})$/);
 
-  const vaild_Array = forms.filter((e) => {
-    return email_regex.test(e[0]) && ninkname_regex.test(e[1]);
+  const vaild_Array = forms.filter((element) => {
+    return email_regex.test(element[0]) && ninkname_regex.test(element[1]);
   });
 
-  console.log(vaild_Array);
-  return answer;
+  const SlicingWord = (fullname) => {
+    let words_Array = [];
+    for (let i = 0; i < fullname.length - 1; i++) {
+      words_Array.push(fullname.slice(i, i + 2));
+    }
+    return words_Array;
+  };
+
+  vaild_Array.forEach((element, index) => {
+    let words = SlicingWord(element[1]);
+  });
 }
 
 module.exports = problem6;
