0660a8bd965c776ac05e8bbf3919e891bd189c22
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 06:06:00 +0900

    FEAT : problem6 - 이메일, 닉네임을 사용한 유효성 검사후 유저 반환

--- src/problem6.js
+++ src/problem6.js
@@ -12,6 +12,14 @@ const getValidUser = (users) => {
   // 형식 검사를 위한 상수 선언
   const isEmailValid = /[a-z0-9]+@+email.com$/;
   const isNickNameKorean = /^[ㄱ-ㅎ|가-힣|]+$/;
+
+
+  // 조건에 맞는 유저들만 반환
+  return users.filter(([email, nickname]) => 
+    (email.length >= EMAIL_MIN_LENGTH || email.length < EMAIL_MAX_LENGTH) &&
+    (nickname.length >= NICKNAME_MIN_LENGTH || nickname.length < NICKNAME_MAX_LENGTH) &&
+    (isEmailValid.test(email)) && (isNickNameKorean.test(nickname))
+  );
 }
 
 module.exports = problem6;
