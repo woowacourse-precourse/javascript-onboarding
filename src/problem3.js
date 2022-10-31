ebf52efa10f86c354f0263f6bd5f9c83ee86aacf
d-charlie-kim <dokkim@student.42seoul.kr>
2022-10-31 22:29:49 +0900

    Feat: <problem3> 자릿수 구분해서 박수 수 계산 및 합산 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,50 @@
+function units(num) {
+  let unitClap = (Math.floor(num / 3));
+  return unitClap;
+}
+
+function tens(num) {
+  let tensClap = (Math.floor(num / 3) * 10);
+  tensClap += (num * 3);
+  return tensClap;
+}
+
+function hundreds(num) {
+  let hundredsClap = (Math.floor(num / 3) * 100);
+  hundredsClap += (num * 57);
+  return hundredsClap;
+}
+
+function thousands(num) {
+  let thousandsClap = (Math.floor(num / 3) * 1000);
+  thousandsClap += (num * 813);
+  return thousandsClap;
+}
+
+function switching(digit, num) {
+  switch(digit) {
+    case 1:
+      return units(num);
+    case 2:
+      return tens(num);
+    case 3:
+      return hundreds(num);
+    case 4:
+      return thousands(num);
+  }
+}
+
+function countClaps(num) {
+  let clapCount = 0;
+  for (let digit = 1; num > 0; num = Math.floor(num / 10) && digit++) {
+    clapCount += switching(digit, num % 10);
+  }
+  return clapCount;
+}
+
 function problem3(number) {
-  var answer;
+  let answer = countClaps(number);
   return answer;
 }
 
-module.exports = problem3;
+// module.exports = problem3;
