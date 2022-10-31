1642cc5f3a0677f20615d25c7deae480f8ea2ef0
JSH99 <tjgus9966@naver.com>
2022-11-01 01:23:48 +0900

    feat: 닉네임 부분 집합 구하기

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
   var answer;
+  var result = [];
+
+  
+  for(var n = 0; n < forms.length; n++) { 
+    // 1. 크루 별 닉네임 부분 집합 구하기
+    const [email, nickname] = forms[n]; // 기준 크루 : n
+    var partion = new Set(); 
+      
+    for(var i = 0; i < nickname.length; i++) { // n의 부분 닉네임 구하기
+      var temp = '';
+      for(var j = i + 1; j <= nickname.length; j++) {
+        temp = nickname.substring(i, j);
+        if(temp.length >= 2) partion.add(temp); 
+      }
+    }
+  }
+
   return answer;
 }
 
