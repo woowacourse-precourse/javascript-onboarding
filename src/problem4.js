f9b46659e53f7c48e2a3ea0209ec41d30d7ad202
KINHYEONJI <hyeonji0718@gmail.com>
2022-10-31 19:25:18 +0900

    feat : changeLowerCase 함수 구현
    - 소문자를 변환하여 반환

--- src/problem4.js
+++ src/problem4.js
@@ -2,5 +2,7 @@ function problem4(word) {
   var answer;
   return answer;
 }
-
+function changeLowerCase(alphabet) {
+  return String.fromCharCode(219 - alphabet.charCodeAt(0));
+}
 module.exports = problem4;
