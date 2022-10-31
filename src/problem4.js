299f2e47554f0f3d77438260ed17a03179ee1e9d
sein <hyuntpdls@gmail.com>
2022-10-31 16:10:02 +0900

    [문제4] feat: 문자열 배열로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
 function problem4(word) {
   var answer;
+
   return answer;
 }
 
+// 문자열을 배열로 변환
+function separateString(string) {
+  let array = string.split("");
+  return array;
+}
 module.exports = problem4;
