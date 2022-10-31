10ad42f708c34c8a558d0d43be3c44a1911573f8
foundaway98 <foundaway98@naver.com>
2022-10-31 18:55:33 +0900

    문제 5번 answer 키워드 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,5 @@
 function problem5(money) {
-  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const MONEY_ARRAY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   for (let i = 0; i < MONEY_ARRAY.length; i++) {
     if (money == 0) {
