a560fbced843b859a681c542aa11a3a1a0d7fdbe
c0c0pang <coc1789@naver.com>
2022-10-31 18:03:54 +0900

    문제 2 코드 정렬

--- src/problem2.js
+++ src/problem2.js
@@ -9,30 +9,30 @@
 */
 function problem2(cryptogram) {
   const arr = [...cryptogram];
-  let i=0;
-  let cntbox  = [];
+  let i = 0;
+  let cntbox = [];
   let check = false;
   //순차적으로 탐색후 중복 발견하면 계속 탐색하고 끝나면 탐색한 부분까지 제거후 다시 원점으로 복귀를 반복
-  while(1){
-    if((arr[i]===arr[i+1]) && ((arr.length)!==i)){
+  while (1) {
+    if ((arr[i] === arr[i + 1]) && ((arr.length) !== i)) {
       cntbox.push(i);
-      check=true;
-      i++;
-      continue;
-    } 
-    else if((arr[i]!==arr[i+1]) && ((arr.length)!==i) && !check){
+      check = true;
       i++;
       continue;
     }
-    else if((arr[i]!==arr[i+1]) && ((arr.length)!==i) && check ){
-      arr.splice(cntbox[0],cntbox.length+1);
-      i=0;
+    else if ((arr[i] !== arr[i + 1]) && ((arr.length) !== i) && !check) {
+      i++;
+      continue;
+    }
+    else if ((arr[i] !== arr[i + 1]) && ((arr.length) !== i) && check) {
+      arr.splice(cntbox[0], cntbox.length + 1);
+      i = 0;
       cntbox = []
       check = false;
       continue;
     }
-    
-    if(arr.length === i)
+
+    if (arr.length === i)
       break;
   }
   return arr.join('');
