03dddd169ca38b74187738c15cc8628df8fa1699
SorinKwon <ksl020700@gmail.com>
2022-11-01 00:09:09 +0900

    feat(problem6): 중복된 문자열을 포함한 닉네임의 이메일을 찾는 기능 추가
    중복된 문자열을 포함한 닉네임을 찾아 이메일을 반환하는 기능을 추가함

--- src/problem6.js
+++ src/problem6.js
@@ -17,6 +17,19 @@ function findDuplicate(n){
     }
     return result;
 }
+function includeDuplicate(n){
+    let answer=[];
+    let result=findDuplicate(divideString(n));
+    for(let i=0;i<n.length;i++){
+        const a = n[i][1];
+        for(let j=0;j<result.length;j++){
+            if(a.includes(result[j])){
+            answer.push(n[i][0]);
+        }
+        }
+    }
+    return answer;
+}
 
 function problem6(forms) {
     let answer;
