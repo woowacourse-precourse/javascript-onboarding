54a074bdf1c10547a3aa3763c3bcf1e72b59a0e7
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 23:03:28 +0900

    feat: [3번 문제] 3, 6, 9 들어가는 match 정규식

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
+const threeSixNineReg = /[3|6|9]/g;
+
 function problem3(number) {
-  var answer;
-  return answer;
+  return threeSixNineMatch(number);
+}
+
+function threeSixNineMatch(number) {
+  return number.toString().match(threeSixNineReg);
 }
 
 module.exports = problem3;
