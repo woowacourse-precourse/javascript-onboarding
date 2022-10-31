84f91182f4931b0d25d2afa48414867d36a4c5dc
taeyoung <rlaxodud911@gmail.com>
2022-10-31 20:22:54 +0900

    Feat: problem 7 friends에서 각 유저의 친구를 찾는 함수 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,19 @@
+/**
+ * user의 친구 목록을 반환합니다.
+ * @param {string} user 친구 관계를 알고 싶은 사용자 아이디입니다.
+ * @param {string[]} friends 친구 관계가 담긴 배열입니다.
+ * @returns {string[]} user와 친구인 사람들의 배열입니다.
+ */
+function getFriends(user, friends) {
+  return friends
+    .map(([f1, f2]) => {
+      if ([f1, f2].includes(user)) {
+        return f1 === user ? f2 : f1;
+      }
+    })
+    .filter((f) => f);
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
