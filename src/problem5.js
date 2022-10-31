75d82de091e21bb916a46f2d6407a8ab310eeaa4
durumi99 <kms990615@naver.com>
2022-10-31 23:02:46 +0900

    [problem 5] 입력값을 화폐 액수 배열을 이용해 금액이 큰 순서대로 배열에 담아 return

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,11 @@
 function problem5(money) {
   var answer;
   let arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = [];
+  for(let i = 0 ; i < arr.length; i++){
+    answer.push(~~(money / arr[i]));
+    money %= arr[i];
+  }
   return answer;
 }
-
 module.exports = problem5;
