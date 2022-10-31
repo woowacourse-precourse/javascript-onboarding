29b52a257e13463ba3f35ea1c80631d5d6cb53a8
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-01 01:55:11 +0900

    주석 추가

--- src/problem3.js
+++ src/problem3.js
@@ -2,9 +2,10 @@ function problem3(number) {
 
   let answer = 0;
 
-  //정규표현식
+  // 정규표현식
   let regex = /3|6|9/g;
 
+  // 문자열이 정규식과 매치되는 부분을 검색
   for (let i = 1; i <= number; i++) {
     answer += (String(i).match(regex) || []).length;
   }
