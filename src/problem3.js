4e069c9f50d951d71065fef5a243c0dbe1568673
gsong <rmsdlf1206@gmail.com>
2022-10-31 19:57:18 +0900

    feat: problem 3 1~number까지의 수에 박수를 각 각 몇 번 쳐야하는지 배열로 반환하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function problem3(number) {}
+function checkClapNum(number) {
+  clapNumList = [];
+  for (let num = 0; num < number.length; num++) {
+    let clapNum = 0;
+    num
+      .toString()
+      .split("")
+      .map((x) => {
+        if (x === "3" || x === "6" || x === "9") clapNum += 1;
+      });
+  }
+  clapNumList.push(clapNum);
 }
 
 module.exports = problem3;
