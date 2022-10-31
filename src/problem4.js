a8be6f62e27d51f7b1e0054194308eec99ec352e
sh981013s <sh981013s@gmail.com>
2022-10-31 18:59:17 +0900

    feat: [4] 문자 알파벳 확인 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,12 @@
+/**
+ * param으로 들어온 문자가 알파벳인지 확인한다.
+ * @param {string} char - 변환해야할 문자
+ * @return {boolean}
+ */
+const isAlphabet = (char) => {
+  return /^[A-Z]$/i.test(char);
+};
+
 function problem4(word) {
   var answer;
   return answer;
