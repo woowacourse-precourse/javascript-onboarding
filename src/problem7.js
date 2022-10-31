474eb57181b01910619893059c787f47610cc074
Circlewee <pnj1118@naver.com>
2022-10-31 17:19:21 +0900

    [#7]feat: 친구 목록으로 추천 목록 생성

--- src/problem7.js
+++ src/problem7.js
@@ -12,6 +12,21 @@ function problem7(user, friends, visitors) {
       bestFriends.push(user === friend[1] ? friend[0] : friend[1]);
     }
   });
+
+  const recommandList = bestFriends.reduce((acc, friend) => {
+    if (!acc[friend]) acc[friend] = [];
+
+    for (let i = 0; i < friends.length; i++) {
+      if (
+        (friends[i][0] === friend && friends[i][1] !== user) ||
+        (friends[i][1] === friend && friends[i][0] !== user)
+      ) {
+        acc[friend].push(friend === friends[i][1] ? friends[i][0] : friends[i][1]);
+      }
+    }
+
+    return acc;
+  }, {});
 }
 
 module.exports = problem7;
