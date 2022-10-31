82ab37f7a445474b751875858e156e533afbddb9
김상두 <puki4416@gmail.com>
2022-10-31 23:37:51 +0900

    problem5 : problem5 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,14 @@
 // 주어진 단위에 대해 순차적으로 나누어 결과를 구하는 problem5 함수 구현
 
 function problem5(money) {
-  var answer;
-  return answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let leftMoney = money;
+  return units.map((unit) => {
+    const remainder = leftMoney % unit;
+    const quotient = Math.floor(leftMoney / unit);
+    leftMoney = remainder;
+    return quotient;
+  });
 }
 
 module.exports = problem5;
