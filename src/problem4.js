fc719983ed99a21b6e8dddb05f9dcb2bda21e4fe
woong <inwoong100@gmail.com>
2022-10-31 22:44:20 +0900

    feature: problem4 풀이 완성
    1. 구현방식에 따른 순차적인 풀이 완성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabet = 'AaBbCcDdEeFfGgHhIiJjKkLlMmNnOoPpQqRrSsTtUuVvWwXxYyZz';
+  const reverseAlphabet =
+    'ZzYyXxWwVvUuTtSsRrQqPpOoNnMmLlKkJjIiHhGgFfEeDdCcBbAa';
+
+  const answer = [];
+  for (let i = 0; i < word.length; i++) {
+    const el = word[i];
+    const idx = alphabet.indexOf(el);
+    if (idx < 0) answer.push(' ');
+    else answer.push(reverseAlphabet[idx]);
+  }
+
+  return answer.join('');
 }
 
 module.exports = problem4;
