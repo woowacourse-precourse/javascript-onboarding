6f29d3e2d9f13e3feca1cbb5a7d6f0b4af0145cf
jihyun12 <kkjjhhbb@naver.com>
2022-11-01 00:28:45 +0900

    [solve]problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  const A = "A".charCodeAt();
+  const Z = "Z".charCodeAt();
+  const a = "a".charCodeAt();
+  const z = "z".charCodeAt();
+  const Alphas = Z - A;
+  const alphas = z - a;
+  console.log(A, Z, Alphas, Alphas - "I".charCodeAt() + A + A);
+  [...word].forEach((character) => {
+    if (character !== " ") {
+      if (character == character.toLowerCase()) {
+        answer += String.fromCharCode(alphas - character.charCodeAt() + a + a);
+      } else {
+        answer += String.fromCharCode(Alphas - character.charCodeAt() + A + A);
+      }
+    } else {
+      answer += " ";
+    }
+  });
   return answer;
 }
-
 module.exports = problem4;
