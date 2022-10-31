f54f6a5cf098f434626a4596b31f5f3aebc4595a
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 14:57:38 +0900

    feat: Problem 4 - 전체 기능 구현하기

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
-function problem4(word) {
-  var answer;
+const convertToFrogLetter = letter => {
+  let convertedLetter = letter;
+  const ACode = 65, ZCode = 90, aCode = 97, zCode = 122;
+
+  if (/[A-Z]/.test(letter))
+    convertedLetter = String.fromCharCode(ACode + ZCode - letter.charCodeAt());
+  else if (/[a-z]/.test(letter))
+    convertedLetter = String.fromCharCode(aCode + zCode - letter.charCodeAt());
+
+  return convertedLetter;
+}
+
+const problem4 = word => {
+  const answer = word.split('').map(x => convertToFrogLetter(x)).join('');
   return answer;
 }
 
