e5bb908dcbd2515ffd475eccdd258a42f73702fe
김지수 <jisudev00@gmail.com>
2022-10-31 12:13:12 +0900

    Add: Problem 6 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,32 @@
 function problem6(forms) {
-  var answer;
+  const dupList = {};
+  const subStrList = {};
+
+  // 두 글자만 중복돼도 중복
+  for (let [, nickname] of forms) {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const subStr = nickname.substring(i, i + 2);
+      const prev = subStrList[nickname] || [];
+      dupList[subStr] = ++dupList[subStr] || 1;
+      subStrList[nickname] = [...prev, subStr];
+    }
+  }
+
+  const answer = forms
+    .map(([email, nickname]) => {
+      let isDup = false;
+      for (let subStr in dupList) {
+        const cnt = dupList[subStr];
+        if (nickname.includes(subStr) && cnt > 1) {
+          isDup = true;
+          break;
+        }
+      }
+
+      if (isDup) return email;
+    })
+    .filter((v) => v);
+
   return answer;
 }
 
