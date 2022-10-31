21e88c5108fafbdd10f77716399e131eec92a088
Ssoco <dlscjs8646@gmail.com>
2022-11-01 03:26:27 +0900

    p2 : 반복문을 통해 글자 처리

--- src/problem2.js
+++ src/problem2.js
@@ -1,4 +1,26 @@
 function problem2(cryptogram) {
+  // 배열로 나누어서 확인하기
+  while (1) {
+    let tmp = "";
+    let firstChk = true;
+    const arr = cryptogram.split("");
+    if (arr.length) tmp += arr[0];
+    for (let i = 1; i < arr.length; ++i) {
+      if (arr[i] === arr[i - 1]) {
+        if (firstChk) {
+          tmp = tmp.slice(0, -1);
+          firstChk = false;
+        }
+        continue;
+      } else {
+        tmp += arr[i];
+        firstChk = true;
+      }
+    }
+    if (tmp === cryptogram) break;
+    else cryptogram = tmp;
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
