585ccc8aad4c35b582dadcd9b19ad0d37acc2fed
SoMin <thals0107@naver.com>
2022-10-31 12:05:54 +0900

    feat: 문제 7 - 함께 아는 친구 10점, 직접 아는 친구 0점 부여

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,24 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer = [];
+  let result = [];
+  let map = new Map();
+
+  // friens 확인
+  for (let friend of friends) {
+    if (!friend.includes(user)) {
+      for (let a of friend) {
+        // 함께 아는 친구 10점
+        map.set(a, 10);
+      }
+    } else {
+      for (let a of friend) {
+        // 원래 친구 0점
+        map.set(a, 0);
+      }
+    }
+  }
+  // console.log(map);
+
   return answer;
 }
 
