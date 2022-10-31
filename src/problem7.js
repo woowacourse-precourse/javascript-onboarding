1c25171af71d3f76258589ba6bdfbf94a706f2eb
kyeongwxx <kyeongwxx@gmail.com>
2022-10-31 21:37:30 +0900

    problem7. user의 friends를 찾는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let answer;
+
+  const findFriendsOfUser = (user, friends) => {
+    let friendsOfUser = [];
+
+    for (let i = 0; i < friends.length; i++) {
+      if (friends[i][0] === user) {
+        arr.push(friends[i][1]);
+      } else if (friends[i][1] === user) {
+        arr.push(friends[i][0]);
+      }
+    }
+
+    return friendsOfUser;
+  };
+
+  return;
 }
 
 module.exports = problem7;
