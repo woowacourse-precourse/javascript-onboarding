c244663eca2ac67f01045d4e7702e0af032dfb7b
MJ <mj632900@gmail.com>
2022-11-01 03:08:20 +0900

    feat : user의 친구를 객체로 반환하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+function getUserFriend(user, friendList) {
+    return friendList
+        .filter((item) => item.includes(user))
+        .map(([user1, user2]) => (user1 === user ? user2 : user1))
+        .reduce((acc, cur) => {
+            acc[cur] = [];
+            return acc;
+        }, {});
 }
+
+function problem7(user, friends, visitors) {}
 
 module.exports = problem7;
