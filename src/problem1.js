1351b575127b206871199c068bc0b38185ebcf86
Saans <92422813+saans808@users.noreply.github.com>
2022-10-31 15:06:31 +0900

    [p1] 함수 생성, 페이지 조건 설정

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,9 @@
 function problem1(pobi, crong) {
   var answer;
+  function plusOrMultiply(name) {
+    let condition = (name[0] + 1 == name[1]) && (name[0] % 2 == 1 && name[1] % 2 == 0);
+    
+  }
   return answer;
 }
 
