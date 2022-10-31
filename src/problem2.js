9060fd7b14a299a058759136b020bc2d58ef4d57
JangHoonSeok <orirori512@naver.com>
2022-11-01 00:38:21 +0900

    feat: problem2. 중복문자 제거 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  answer = deleteString(cryptogram);
   return answer;
+}
+
+function deleteString(cryptogram) {
+  // 중복된 문자 삭제하는 메서드
+  let stackS = [];
+
+  for (const item of cryptogram) {
+    // stack으로 암호문 축약시키기.
+    if (stackS && stackS[stackS.length - 1] === item) stackS.pop();
+    else stackS.push(item);
+  }
+
+  return stackS;
 }
 
 module.exports = problem2;
