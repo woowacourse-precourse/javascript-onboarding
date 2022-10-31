1be65107d43baf9d3199f1a278d8c27e9e6ac25f
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 19:52:52 +0900

    구현할 기능 목록 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,17 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+/*
+  구현할 기능 목록
+  [ ] friends 배열에서 주어지는 사람들의 friends 리스트를 Map 객체(key: 이름, value: friends 배열)로 생성하는 기능
+  [ ] frineds 배열어서 주이지는 사람들의 score 리스트를 Map 객체(key: 이름, value: score)로 생성하는 기능
+  [ ] 해당 사람이 user의 friend인지 확인하는 기능
+  [ ] user와 함께 아는 친구인 경우가 있는 사람인 경우, 아는 친구 한명 당 score를 10씩 증가시키는 기능
+  [ ] user의 타임라인에 방문한 사람인 경우, 방문 횟수 당 score를 1씩 증가시키는 기능
+  [ ] Map객체를 배열로 convert하는 기능
+  [ ] score가 0일 때 리스트에서 제거하는 기능
+  [ ] 추천 친구를 점수가 가장 높은 순, 점수가 같은 경우 이름 순으로 정렬하는 기능
+  [ ] 리스트에서 사람 이름만 추출하여 배열로 반환하는 기능
+  [ ] 리스트를 앞에서부터 최대 5개로 자르는 기능
+*/
+
+function problem7(user, friends, visitors) {}
 
 module.exports = problem7;
