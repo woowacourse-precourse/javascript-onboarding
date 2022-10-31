f39dd640fd637e4b167c921284f528b4e31dcaf0
somin <smhs12@naver.com>
2022-10-31 15:57:24 +0900

    feat: problem4 / 아스키코드를 이용하여 대문자, 소문자를 판단하여 변환하고  이외 문자들은 그대로 출력한다

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,11 @@
 function problem4(word) {
   const answer = [];
+  for (let char of word) {
+    const ascii = char.charCodeAt(0);
+    if (isUpper(ascii)) answer.push(String.fromCharCode(155 - ascii));
+    else if (isLower(ascii)) answer.push(String.fromCharCode(219 - ascii));
+    else answer.push(char);
+  }
   return answer;
 }
 
