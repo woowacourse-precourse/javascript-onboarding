11858f8cc07f35c162ae253e9ac615070f43b01f
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-10-31 19:14:33 +0900

    [1번 문제] feat: sum 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,12 @@
+function sum(num){
+  var sumRes = 0;
+  while(num > 0){
+    sumRes += (num%10);
+    num = Math.floor(num/10);
+  }
+  return sumRes;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
