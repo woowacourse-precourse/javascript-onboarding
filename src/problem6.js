1deb7612dd9e17235dc26a3a9b906c6f76c47601
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 15:17:00 +0900

    feat : problem6 풀이 및 테스트
    닉네임을 두글자씩 뽑아서 개수를 셀 때, map 자료구조를 사용했습니다.
    object를 사용해도 되지만 map 자료구조의 has, get, set이 직관적으로
    가독성이 좋다고 생각되어서 사용했습니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    const answer = [];
+
+    let myMap = new Map();
+    forms.forEach((form) => {
+        const [email, nickname] = form;
+        for (let i = 0; i < nickname.length - 1; i++) {
+            const str = nickname.substring(i, i + 2);
+            if (myMap.has(str)) myMap.set(str, myMap.get(str) + 1);
+            else myMap.set(str, 1);
+        }
+    });
+
+    forms.forEach((form) => {
+        const [email, nickname] = form;
+        for (let i = 0; i < nickname.length - 1; i++) {
+            const str = nickname.substring(i, i + 2);
+            if (myMap.get(str) > 1) answer.push(email);
+        }
+    });
+    return answer.sort();
 }
 
 module.exports = problem6;
