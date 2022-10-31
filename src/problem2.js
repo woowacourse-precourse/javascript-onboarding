7f9b895bc7d62ec9368d6933650be669440e4238
kaori-killer <sksbthwjd@daum.net>
2022-10-31 16:02:10 +0900

    docs: 알고리즘 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,25 +1,7 @@
 function deleteString(cryptogram){
-  stringLength = cryptogram.length;
-  let check = new Array(stringLength).fill(0);
-  let newCryptogram = '';
-  let count = 0;
 
-  for (let i = 0; i < stringLength; i++){
-    if(check[i] == 1) continue;
-    
-    if((cryptogram[i] == cryptogram[i + 1]) && (i != stringLength - 1)){
-      count += 1;
-      check[i] = 1;
-      check[i+1] = 1;
-    }
-    else{
-      newCryptogram += cryptogram[i]
-    }
-  }
-
-  if(count == 0){ return newCryptogram; }
-  else { return deleteString(newCryptogram); }
 }
+
 
 function problem2(cryptogram) {
     return deleteString(cryptogram);
