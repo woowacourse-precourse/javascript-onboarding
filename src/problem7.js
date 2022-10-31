fefb259dd026a17fa06c0363b257ecff7b90439f
nara04040 <nara04040@naver.com>
2022-10-31 19:59:07 +0900

    [problem7] feat : user와 친구리스트 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,14 @@
 function problem7(user, friends, visitors) {
-  let answer;
+  let answer = [];
+
+  let friendsList = [];
+  for (let i = 0; i < friends.length; i++) {
+    let sliceFriends = friends[i].slice();
+    sliceFriends.forEach((ele) => {
+      friendsList.push(ele);
+    });
+  }
+
   return answer;
 }
 
