ca016d407995e870f0b9e8245672dfdcff06ab40
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 22:04:13 +0900

    [Feat:Problem6] compose fn

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,4 @@
-function problem6(forms) {
-  var answer;
-  return answer;
-}
-
-module.exports = problem6;
+const compose =
+  (...fns) =>
+    (initialValue) =>
+      fns.reduce((composed, fn) => fn(composed), initialValue);
