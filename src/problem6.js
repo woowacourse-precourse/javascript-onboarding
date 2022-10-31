cef019103cff8f72eee537036059c2108139c6bb
unqeye <salyuan@naver.com>
2022-11-01 02:38:16 +0900

    feat: 문제6 닉네임의 substring 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,11 @@
+function getSubnicks(nickname) {
+  const subnicks = new Set();
+  for (let i = 0; i < nickname.length - 1; i++) {
+    subnicks.add(nickname.substr(i, 2));
+  }
+  return subnicks;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
