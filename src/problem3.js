1d8c6efc0d5ad535a8a79028c14a263c3e841ab2
semnil5202 <semnil5202@gmail.com>
2022-11-01 05:45:50 +0900

    [문제 3번] refactor: 가독성을 위해 중첩 반복문 함수로 분리하기

--- src/problem3.js
+++ src/problem3.js
@@ -7,18 +7,18 @@ function problem3(number) {
 function countClap(number) {
   let clap = 0;
   while (number > 2) {
-    let threeSixNine = number;
-    while (threeSixNine > 0) {
-      if (
-        threeSixNine % 10 === 3 ||
-        threeSixNine % 10 === 6 ||
-        threeSixNine % 10 === 9
-      ) {
-        clap++;
-      }
-      threeSixNine = parseInt(threeSixNine / 10, 10);
-    }
+    clap = isThreeSixNine(number, clap);
     number--;
+  }
+  return clap;
+}
+
+function isThreeSixNine(number, clap) {
+  while (number > 0) {
+    if (number % 10 === 3 || number % 10 === 6 || number % 10 === 9) {
+      clap++;
+    }
+    number = parseInt(number / 10, 10);
   }
   return clap;
 }
