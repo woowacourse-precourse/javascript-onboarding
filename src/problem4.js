dbc2ea6806701c7f0e20d781f935104a9fbe3f71
taeyoung <rlaxodud911@gmail.com>
2022-10-31 19:29:26 +0900

    Feat: problem 4 조건에 따라 알파벳을 변환하는 반복문 작성

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,17 @@ function problem4(word) {
   const UPPER = 'A'.charCodeAt(0) + 'Z'.charCodeAt(0);
   const isLower = /[a-z]/;
   const isUpper = /[A-Z]/;
+  const result = [];
+
+  for (const w of word) {
+    if (isLower.test(w)) {
+      result.push(LOWER - w.charCodeAt(0));
+    } else if (isUpper.test(w)) {
+      result.push(UPPER - w.charCodeAt(0));
+    } else {
+      result.push(w.charCodeAt(0));
+    }
+  }
 
   return answer;
 }
