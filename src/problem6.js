51d1f510e9268c18fac126ae439e16716efeec5e
youngentry <90388461+youngentry@users.noreply.github.com>
2022-10-31 18:08:12 +0900

    [문제6] <1단계 기능요구사항> 모든 연속되는 패턴 찾기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
-    var answer;
-    return answer;
+    let nicknameSet = new Set([]);
+    // 1단계) 모든 연속되는 패턴을 찾아 집합으로 만들기
+    for (let i = 0; i < forms.length; i++) {
+        let nickname = forms[i][1];
+        for (let j = 0; j < nickname.length - 1; j++) {
+            nicknameSet.add(nickname[j] + nickname[j + 1]);
+        }
+    }
 }
 
 module.exports = problem6;
