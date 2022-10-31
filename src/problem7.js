7ce36ab2efea027e05b009a67fd9587f218a762c
yesolyo <ahnyelim77@gmail.com>
2022-10-31 18:33:33 +0900

    freinds에서 사용자인 mrko를 찾는다

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,20 @@
 function problem7(user, friends, visitors) {
   var answer;
+  answer = [];
+  var user_friends = [];
+  var friends_number = [];
+  var visitors_number = [];
+  var n = 0;
+
+  //friends에서 사용자인 mrko를 찾는다
+  for (let i = 0; i < friends.length; i++) {
+    if (user == friends[i][0]) {
+    } else if (user == friends[i][1]) {
+    } else {
+      continue;
+    }
+  }
+
   return answer;
 }
 
