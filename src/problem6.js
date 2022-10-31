207a3949e8609d30f1e916df3cfb8a29a00dfc9b
osdoonhyun <skw1996@naver.com>
2022-11-01 04:16:01 +0900

    [문제6] 기능 목록 추가

--- src/problem6.js
+++ src/problem6.js
@@ -10,7 +10,7 @@ function makeContinueNick(forms) {
   let nickForms = [];
   for (let i = 0; i < forms.length; i++) {
     let tmp = "";
-    nickForms.push(forms[i][0]);
+    nickForms.push([forms[i][0], []]);
     for (let j = 0; j < forms[i][1].length - 1; j++) {
       tmp = forms[i][1][j] + forms[i][1][j + 1];
       nickForms[i][1].push(tmp);
@@ -18,11 +18,33 @@ function makeContinueNick(forms) {
   }
   return nickForms;
 }
-
+// 기능2
+function checkContinueNick(continueArr) {
+  let continueNickArr = [];
+  for (let i = 0; i < continueArr.length - 1; i++) {
+    for (let j = i + 1; j < continueArr.length; j++) {
+      for (let k = 0; k < continueArr[i][1].length; k++) {
+        if (continueArr[i][1].includes(continueArr[j][1][k])) {
+          continueNickArr.push(i, j);
+        }
+      }
+    }
+  }
+  return continueNickArr;
+}
 function problem6(forms) {
-  var answer;
+  let answer = [];
   let continueArr = makeForms(forms);
-  makeContinueNick(continueArr);
+  continueArr = makeContinueNick(continueArr);
+
+  //기능3
+  let nickArr = [...new Set(checkContinueNick(continueArr))];
+  //기능4
+  for (let i = 0; i < nickArr.length; i++) {
+    answer.push(forms[nickArr[i]][0]);
+  }
+  answer.sort();
+
   return answer;
 }
 
@@ -35,5 +57,6 @@ ex) "제이엠" -> ["제이","이엠"]
   1.1 닉네임을 한 글자씩 나누는 기능 ex)'제이엠'->'제','이','엠'
   1.2 한 글자씩 나눈 것을 연속 되게 하여 배열 생성하는 기능 ex) '제','이','엠'->'제이','이엠'
 2. 신청 받은 닉네임 중 같은 글자가 연속적으로 포함 되는 닉네임을 체크하는 기능
-3. 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거하는 기능 
+3. 중복 되는 닉네임을 삭제하는 기능 (Set활용)
+4. 닉네임에 해당하는 이메일을 찾고 오름차순 정렬하는 기능
 */
