61f92f7eaf98fcc46e253a8ea751bbc7d21e6958
froggy1014 <clcl6084@gmail.com>
2022-10-31 18:09:22 +0900

    style[2]: JS Doc 추가 및 변수 이름 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,20 +1,24 @@
 function problem2(cryptogram) {
-  const stringArray = cryptogram.split('');
+  const crpyStrArray = cryptogram.split('');
 
   while(1){
-    const DetectedIndexs = getDupIndexs(stringArray)
+    const DetectedIndexs = getDupIndexs(crpyStrArray)
     if(DetectedIndexs.length === 0) break;
-    DetectedIndexs.map((index) => stringArray.splice(index, 1))
+    DetectedIndexs.map((index) => crpyStrArray.splice(index, 1))
   }
   
-  return stringArray.join('');
+  return crpyStrArray.join('');
 }
 
-function getDupIndexs(stringArray) {
+/**
+ * @param {string[]} strArray  문자열배열
+ * @returns 중복되어 문자들의 인덱스를 숫자배열에 담아 반환합니다.
+ */
+function getDupIndexs(strArray) {
   const dupIndexs = [];
   let isDuplicate = false;
-  for(let i = 0; i < stringArray.length; i++) {
-    if( stringArray[i] === stringArray[i+1]) {
+  for(let i = 0; i < strArray.length; i++) {
+    if( strArray[i] === strArray[i+1]) {
       isDuplicate = true;
       dupIndexs.unshift(i);
     } else if(isDuplicate) {
