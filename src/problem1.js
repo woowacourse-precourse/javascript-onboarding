7b5717599412b6ffa3a2dad0ebc73bd1b2d0a9d8
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 03:21:39 +0900

    feat: [problem1] 예외 처리 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,19 @@
+// 예외 처리 기능
+function isExcept(page) {
+  if (page.length != 2)
+    return true
+
+  let left = page[0];
+  let right = page[1];
+  if (left % 2 != 1 || left <= 1 || left >= 400)
+    return true
+  if (right % 2 != 0 || right <= 1 || right >= 400)
+    return true
+  if (right - left != 1)
+    return true
+  return false
+}
+
 // 페이지 번호 각 자리 숫자를 모두 더해 반환하는 기능
 function getSum(n) {
   let sum = 0;
@@ -22,16 +38,23 @@ function getScore(page) {
 function problem1(pobi, crong) {
   var answer;
 
-  let pobiScroe = getScore(pobi);
-  let crongScroe = getScore(crong);
-
-  // pobi와 crong의 점수를 비교해 답 구하기
-  if (pobiScroe > crongScroe)
-    answer = 1;
-  else if (pobiScroe < crongScroe)
-    answer = 2;
-  else if (pobiScroe == crongScroe)
-    answer = 0;
+  // 예외 처리
+  if ((isExcept(pobi) || isExcept(crong)))
+    answer = -1;
+  else {
+    let pobiScroe = getScore(pobi);
+    let crongScroe = getScore(crong);
+  
+    // pobi와 crong의 점수를 비교해 답 구하기
+    if (pobiScroe > crongScroe)
+      answer = 1;
+    else if (pobiScroe < crongScroe)
+      answer = 2;
+    else if (pobiScroe == crongScroe)
+      answer = 0;
+    else
+      answer = -1;
+  }
 
   return answer;
 }
