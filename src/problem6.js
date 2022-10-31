b9cdd0e95e97e9ae50f21793637a48699c6c9097
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 06:05:39 +0900

    FEAT : problem6 - 이메일, 닉네임을 사용한 유효성 검사를 위한 상수 선언

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+}
+
+// 이메일, 닉네임 유효성 검사
+const getValidUser = (users) => {
+  // 길이 유효성 검사를 위한 상수 선언
+  const EMAIL_MIN_LENGTH = 11;
+  const EMAIL_MAX_LENGTH = 20;
+  const NICKNAME_MIN_LENGTH = 1;
+  const NICKNAME_MAX_LENGTH = 20;
+
+  // 형식 검사를 위한 상수 선언
+  const isEmailValid = /[a-z0-9]+@+email.com$/;
+  const isNickNameKorean = /^[ㄱ-ㅎ|가-힣|]+$/;
 }
 
 module.exports = problem6;
