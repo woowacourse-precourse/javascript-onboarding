4dd7e8656554b48ea88d165f2f93bb4c43e76ded
sein <hyuntpdls@gmail.com>
2022-10-31 15:16:11 +0900

    [문제1] feat: 페이지수를 각 자리수로 나누어 array배열에 삽입

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
 function problem1(pobi, crong) {
   var answer;
+ 
   return answer;
+}
+
+
+// number를 각 자리 수로 나누어 array배열에 삽입
+function makeNumDigits(num) {
+  num = num.toString();
+  let array = [];
+  for (let i = 0; i < num.length; i++) {
+    array.push(Number(num[i]));
+  }
+  return array;
 }
 
 module.exports = problem1;
