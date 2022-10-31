22ac9602a321f0073dddfc373307bd146c01a14c
Jeonjunwoo <tira618@naver.com>
2022-10-31 16:39:55 +0900

    정상적인 대문자의 순서와 청개구리의 대문자의 순서

--- src/problem4.js
+++ src/problem4.js
@@ -4,4 +4,13 @@ function problem4(word) {
 }
 const alphabetArr = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 const strangeAlphabetArr = ['z','y','x','w','v','u','t','s','r','q','p','o','n','m','l','k','j','i','h','g','f','e','d','c','b','a'];
+const upperAlphabetArr = [];
+const upperStrangeAlphabetArr = [];
+alphabetArr.forEach(element => {
+  upperAlphabetArr.push(element.toUpperCase());
+});
+strangeAlphabetArr.forEach(element => {
+  upperStrangeAlphabetArr.push(element.toUpperCase());
+});
+
 module.exports = problem4;
