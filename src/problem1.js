7f3657817a81c2f8d655cd65cb7b339f88a81099
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 03:28:14 +0900

    FEAT : problem1 - 페이지 번호 유효성 검사 기능 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if(!isValidPageNumber(pobi) || !isValidPageNumber(crong)) {
+    return -1;
+  }
+
+  
+}
+
+// 페이지 번호의 유효성 검사
+const isValidPageNumber = ([left, right]) => {
+  // 시작 면이나 마지막 면이 나오게 된다면 false 반환
+  if(left === 1 || right === 400) return false;
+  // 연속되지 않은 숫자라면 false 반환
+  if(left + 1 != right) return false;
+  // 이외의 경우 true 반환
+  return true;
 }
 
 module.exports = problem1;
