542eef8767b052d6da7411029e470b6994098fec
durumi99 <kms990615@naver.com>
2022-10-31 17:06:23 +0900

    [problem 1] 예외 처리 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,20 @@
+// 예외처리 함수
+function checkException(player){
+  if(player.some(elem => elem <= 2 || elem >= 399)) 
+    return true;
+  if(player.length != 2)
+    return true; 
+  if(player[1] - player[0] != 1)
+    return true;
+  if(player[0] % 2 == 0)
+    return true;
+  return false;
+}
 function problem1(pobi, crong) {
   var answer;
+  // 예외처리
+  if(checkException(pobi) || checkException(crong))
+    return -1;
   return answer;
 }
 
