2d7fc2224d523a1a25d27681274d71b491ea5a26
knh6269 <knh6269@gmail.com>
2022-10-31 22:48:24 +0900

    feat : 미션5) 메인 함수

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,8 @@
+//메인 함수
 function problem5(money) {
-  var answer;
+  const amounts = [50000,10000,5000,1000,500,100,50,10,1]; //금액의 단위
+  let answer = []; 
+
   //단위 별 갯수를 푸쉬해주고 남은 돈을 처리해주는 함수
   const disposeMoney = (many, count) =>{
     answer.push(many); 
