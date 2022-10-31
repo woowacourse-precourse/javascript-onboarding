6cc36a65bf9d609349281bd705d2cdd7bbccfa17
sungjihyun <jhsung23@gmail.com>
2022-10-31 23:41:25 +0900

    Fix: problem 6: 닉네임 토큰 획득 과정 로직 오류 수정

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,6 @@
 const makeTokenArray = (nickname) => {
   const tokenArray = [];
-  for (let i = 0; i < nickname.length / 2; i++) {
+  for (let i = 0; i < nickname.length - 1; i++) {
     const token = nickname.slice(i, i + 2);
     tokenArray.push(token);
   }
