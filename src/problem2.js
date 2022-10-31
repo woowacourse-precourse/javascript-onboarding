cc2257fbf8f3fdb04510bb2d7f7ce5b88197f038
Jiho1996 <jhp9270@naver.com>
2022-10-31 22:43:44 +0900

    fix : 문자가 다르다면, 포인터 각각 한칸 전진

--- src/problem2.js
+++ src/problem2.js
@@ -2,16 +2,10 @@ function problem2(cryptogram) {
   let leftPointer = 0;
   let rightPoiner = 1;
   const cryptogramList = [...cryptogram];
-  const pointerMoveBackward = () => {
-    leftPointer -= 1;
-    rightPoiner -= 1;
-  }
+
   const pointerMoveForward = () => {
     leftPointer += 1;
     rightPoiner += 1;
-  }
-  const removeDuplicated = () => {
-    cryptogramList.splice(leftPointer, 2);
   }
   /**
    * 유효한 범위를 한정해 주는 함수.
@@ -20,20 +14,18 @@ function problem2(cryptogram) {
   const isValidRange = () => {
     return leftPointer >= 0 && rightPoiner < cryptogramList.length;
   }
-
+ 
   while (isValidRange()){
-    if (cryptogramList[leftPointer] === cryptogramList[rightPoiner]){
-      removeDuplicated();
-      pointerMoveBackward();
-      continue;
-    }
     if (cryptogramList[leftPointer] !== cryptogramList[rightPoiner]){
       pointerMoveForward();
+      continue;
     }
-  }
-  return cryptogramList.join("");
-
-
+    
+   
+    } 
+    return cryptogramList.join("");
+  }   
   
-}
+
+
 module.exports = problem2;
