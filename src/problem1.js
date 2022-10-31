05b564fd851c06e8720dd890ced258642290e2e6
leesoyuun <leesoyun4206@naver.com>
2022-10-31 21:59:43 +0900

    [1번문제] 예외처리 함수인 exceptionPage 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,18 @@
+function exceptionPage(arr) {
+  const diff = arr[1] - arr[0] === 1 ? true : false;
+  var range = true;
+
+  for(let i = 0;i < arr.length;i++){
+    if(arr[i]>=0 && arr[i]<=400){
+      range = true;
+    }
+    else{
+      range = false;
+    }
+  }
+  return diff && range;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
