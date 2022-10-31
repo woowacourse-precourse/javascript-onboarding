3ee7996138b7669c728f85fd36d5a45a0bcfe40b
HojinAn <asdv94@naver.com>
2022-11-01 08:28:59 +0900

    docs: problem 2 feature list

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,11 @@
+/**
+ * Problem 2
+ * 1. 중복 문자에 해당하는 정규표현식 작성
+ * 2. 중복 문자가 존재할 경우 제거하고 제거한 암호문 다시 재귀
+ * 3. 중복 문자가 존재하지 않을 경우 해당 문자열 반환
+ * @param {string} cryptogram 1 이상 1000 이하
+ * @returns {string}
+ */
 function problem2(cryptogram) {
   var answer;
   return answer;
