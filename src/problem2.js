81c27937b8738bcdb3e9aff525f2856e7d05eaec
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 11:15:19 +0900

    Feat: [문제번호2] 스택을 활용하여 중복되는 값들 remove

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = removeDuplicates(cryptogram);
   return answer;
+}
+
+function removeDuplicates(str) {
+  const stack = [str[0]];
+
+  for (let i = 1; i < str.length; i++) {
+    const prev = str[i];
+
+    if (stack[stack.length - 1] === prev) {
+      stack.pop();
+      continue;
+    }
+    stack.push(prev);
+  }
+
+  return stack.join("");
 }
 
 module.exports = problem2;
