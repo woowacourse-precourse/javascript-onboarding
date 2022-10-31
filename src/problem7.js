ebc8fcfdaf035d61efe1faf30abc323db996f1f2
hyeongdong <khd941018@gmail.com>
2022-11-01 02:32:17 +0900

    [문제7] docs : 기능 목록 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+/* 
+--기능목록--
+1. user와 친구인 사람들의 아이디를 찾는다.
+2. user와 친구인 사람들의 친구를 찾아 점수 10점을 부여한다.
+3. user의 타임라인에 방문한 사람들에게 방문횟수마다 1점씩 부여한다.
+4. 점수가 높은 순으로, 같은 경우 이름순으로 아이디를 정렬한다.  
 
+*/
+
+function problem7(user, friends, visitors) {}
 module.exports = problem7;
