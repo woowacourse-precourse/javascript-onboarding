c508e131dd0fd214680fd685463fff5d4f27c5d0
heyheyley <jeonheyley@gmail.com>
2022-11-01 00:23:19 +0900

    문제1:에러핸들링

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,20 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    //에러핸들링
+    function checkError(arr) {
+      //오른쪽 페이지는 왼쪽 페이지 + 1 이여야 함
+      if (arr[0] + 1 !== arr[1]) return false;
+  
+      //왼쪽은 홀수,오른쪽 페이지 짝수이여 함
+      if (arr[0] % 2 !== 1 || arr[1] % 2 !== 0) return false;
+  
+      //왼쪽페이지 첫페이지나 오른쪽 마지막페이지가 나오면 안됨
+      if (arr[0] === 1 || arr[1] === 400) return false;
+  
+      //모두 통과해야 true
+      return true;
+    }
+  
+    if (!checkError(pobi) || !checkError(crong)) return false;
 }
 
 module.exports = problem1;
