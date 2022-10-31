518794249caa4891773426a2e08e8506cc6dfd1f
sungjihyun <jhsung23@gmail.com>
2022-10-31 21:16:24 +0900

    Fix: problem 2: 암호 해독 기능 에러 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,33 @@
 const decrypt = (code) => {
-  return code.split("").reduce((acc, cur) => {
-    return acc[acc.length - 1] === cur ? acc.slice(0, -1) : acc + cur;
-  });
+  const result = [];
+  let word = code[0];
+  let count = 1;
+  for (let i = 1; i < code.length; i++) {
+    if (code[i] === word) {
+      count++;
+    } else {
+      if (count === 1) {
+        result.push(word);
+      }
+      word = code[i];
+      count = 1;
+    }
+  }
+  if (count === 1) {
+    result.push(word);
+  }
+  return result.join("");
 };
 
 function problem2(cryptogram) {
-  var answer = "";
+  var answer;
   let code = cryptogram;
+
   while (answer !== decrypt(code)) {
-    answer = decrypt(code);
-    code = answer;
+    code = decrypt(code);
+    answer = code;
   }
+
   return answer;
 }
 
