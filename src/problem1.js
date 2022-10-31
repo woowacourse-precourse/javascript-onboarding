524fb9375ebd75956651065b29b48f74df01d943
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-31 10:06:12 +0900

    숫자를 입력받아 각 자리수의 합을 반환하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -2,7 +2,9 @@ function problem1(pobi, crong) {
   const MIN = 1;
   const MAX = 400;
 
-
+  function getSum(page) {
+    return String(page).split("").reduce((acc, curr) => acc += Number(curr), 0);
+  }
 }
 
 module.exports = problem1;
