0017f3ba18b127a58143c34dbadd92e04f090b5b
gyeongza <gyeongza@naver.com>
2022-10-31 22:01:38 +0900

    feat: 닉네임을 두글자씩 나누어 새로운 배열에 반환

--- src/problem6.js
+++ src/problem6.js
@@ -3,6 +3,18 @@ const createArr = (data) => {
   const emailArr = [...new Map(data).keys()]
   const nicknameArr = [...new Map(data).values()]
   const checkArr = new Array(data.length)
+
+  //닉네임을 두글자씩 나누어 새로운 배열에 반환
+  const splicing = nicknameArr.map(nickname => {
+    let splicing = []
+    for (let i =0; i <nickname.length-1; i++){
+      const substring = nickname[i] + nickname[i+1]
+      splicing.push(substring)
+    }
+    splicing = [...new Set(splicing)]
+
+    return splicing
+  })
 }
  
 // function problem6(forms) {
