3e05d1fd545cd7e98e1a81eedbee33c29a11844a
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-10-31 10:47:33 +0900

    feat: 문제2 연속하는 중복 문자가 없을 때까지 반복적으로 해독하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,21 @@
 function problem2(cryptogram) {
   let answer;
   let overlap = 1;
-  const letters = cryptogram.split('');
-  for (let i = 0; i < letters.length; i++) {
-    if (letters[i] === letters[i + 1]) {
-      overlap++;
-    } else if (overlap >= 2) {
-      cryptogram = cryptogram.replace(letters[i].repeat(overlap), '');
-      overlap = 1;
+  let count;
+  do {
+    count = 0;
+    const letters = cryptogram.split('');
+    for (let i = 0; i < letters.length; i++) {
+      if (letters[i] === letters[i + 1]) {
+        overlap++;
+      } else if (overlap >= 2) {
+        cryptogram = cryptogram.replace(letters[i].repeat(overlap), '');
+        overlap = 1;
+        count += 1;
+      }
     }
-  }
+  } while (count > 0);
+
   return answer;
 }
 
