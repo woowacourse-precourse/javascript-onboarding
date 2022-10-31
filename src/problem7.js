f34ddbcdcb381b31dca1592448df67ad5294ab71
Jiho1996 <jhp9270@naver.com>
2022-10-31 20:13:01 +0900

    fix : 친구관계가 없을 경우 수정

--- src/problem7.js
+++ src/problem7.js
@@ -46,7 +46,8 @@ function problem7(user, friends, visitors) {
 
   for (let key in friendsRelaion){
     if (key === user) continue;
-    if (!friendsRelaion[key].includes(user)){
+    if (inputedUsersFriends && !friendsRelaion[key].includes(user)){
+      
       inputedUsersFriends.forEach((friend) => {
         if (friendsRelaion[key].includes(friend)){
           scoreBoard.hasOwnProperty(key) ? scoreBoard[key] += relationScore : scoreBoard[key] = relationScore;
@@ -55,7 +56,7 @@ function problem7(user, friends, visitors) {
     }
   }
   visitors.forEach((visitor) => {
-    if (!inputedUsersFriends.includes(visitor)){
+    if (!inputedUsersFriends || !inputedUsersFriends.includes(visitor)){
       scoreBoard.hasOwnProperty(visitor) ? scoreBoard[visitor] += visitScore : scoreBoard[visitor] = visitScore;
       }
     }
@@ -63,10 +64,11 @@ function problem7(user, friends, visitors) {
 
   const sortedScoreBoard = getSortScoreBoard(scoreBoard);
   const name = 0;
-
+    
   sortedScoreBoard.forEach((personalScore) => {
     if (result.length < 5) result.push(personalScore[name])
   })
+  
   return result
 
 }
