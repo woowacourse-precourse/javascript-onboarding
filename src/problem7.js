fbe8cdcd47726d3489e4142ee4be332d958bb481
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-31 11:36:36 +0900

    feat: problem 7 기능목록 1: 사용자와 함께 아는 친구 점수 map에 set

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,33 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let user_friends = [];
+  let map = new Map();
+  let answer = [];
+  user_friends = getUserFriends(user, friends);
+
+  // 건너서 아는 친구 점수 map에 set
+  for (uf of user_friends) {
+    for (friend of friends) {
+      if (friend.includes(uf)) {
+        const f = friend[0] === uf ? friend[1] : friend[0];
+        if (f !== user) {
+          map.set(f, 10);
+        }
+      } else {
+        map.set(friend[0], 0);
+        map.set(friend[1], 0);
+      }
+    }
+  }
+}
+
+function getUserFriends(user, friends) {
+  let u_friends = [];
+  for (friend of friends) {
+    if (friend.includes(user)) {
+      u_friends.push(friend[0] === user ? friend[1] : friend[0]);
+    }
+  }
+  return u_friends;
 }
 
 module.exports = problem7;
