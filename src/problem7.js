06eb98cc5ba888b307afbd0fa172d341fe5281f3
Jiho1996 <jhp9270@naver.com>
2022-10-31 18:50:40 +0900

    feat : 친구 관계 그래프 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,24 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+function makeRelationship(friends) {
+  const relation = new Object();
+
+  friends.forEach((friendSet) => {
+    relation[friendSet[0]] = [];
+    relation[friendSet[1]] = [];
+  })
+  friends.forEach((friendSet) => {
+    relation[friendSet[0]].push(friendSet[1]);
+    relation[friendSet[1]].push(friendSet[0]);
+  })
+  return relation;
 }
+
+function problem7(user, friends, visitors) {
+  const result = [];
+
+  const friendsRelaion = makeRelationship(friends);
+  console.log(friendsRelaion)
+}
+
+console.log(problem7("mrko", [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ], ["bedi", "bedi", "donut", "bedi", "shakevan"]))
 
 module.exports = problem7;
