ad47bff1d142824a70b329e071f72215cbb7ecb6
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-31 10:37:01 +0900

    p4: 문자를 순회하면서 ASCII코드 조회

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,12 @@ function problem4(word) {
   const CAPITAL_SUM = 155;
   const SMALL_SUM = 219;
 
+  for (let i = 0; i < word.length; i++) {
+    const ascii = word.charCodeAt(i);
+
+
+  }
+
   return answer;
 }
 
