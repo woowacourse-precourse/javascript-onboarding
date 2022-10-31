875fe7ccb0f5ed503bc3fb95820d5aa64bb8cb0a
woong <inwoong100@gmail.com>
2022-11-01 00:31:06 +0900

    refactor: problem7 코드 리팩토링
    1. 분리가능한 부분 함수로 분리
    2. 기능에 맞는 함수명 지정
    3. 역할에 맞는 변수명 지정
    4. 예외사항 처리

--- src/problem7.js
+++ src/problem7.js
@@ -1,39 +1,43 @@
-function problem7(user, friends, visitors) {
-  const answer = [];
+const getFriendsMap = (friends) => {
   const friendsMap = new Map();
-  for (let i = 0; i < friends.length; i++) {
-    const one = friendsMap.get(friends[i][0]) || [];
-    const two = friendsMap.get(friends[i][1]) || [];
-    if (!one.includes(friends[i][1])) {
-      friendsMap.set(friends[i][0], [...one, friends[i][1]]);
+  for (const friendShip of friends) {
+    const firstFriendsArray = friendsMap.get(friendShip[0]) || [];
+    const secondFriendsArray = friendsMap.get(friendShip[1]) || [];
+
+    if (!firstFriendsArray.includes(friendShip[1])) {
+      friendsMap.set(friendShip[0], [...firstFriendsArray, friendShip[1]]);
     }
-    if (!two.includes(friends[i][0])) {
-      friendsMap.set(friends[i][1], [...two, friends[i][0]]);
+    if (!secondFriendsArray.includes(friendShip[0])) {
+      friendsMap.set(friendShip[1], [...secondFriendsArray, friendShip[0]]);
     }
   }
+  return friendsMap;
+};
 
-  const userFriends = friendsMap.get(user);
-  const keys = Array.from(friendsMap.keys());
-  const scoreObj = {};
-  const countFriends = (key) => {
-    if (key === user) return 0;
-    let score = 0;
-    const keyFriends = friendsMap.get(key);
-    for (const friend of userFriends) {
-      if (keyFriends.includes(friend)) score += 10;
-    }
-    scoreObj[key] = score;
-  };
+const scoreCommonFriends = (user, friend, friendsMap, scoreObj) => {
+  if (friend === user) return;
+  const friendsArray = friendsMap.get(friend);
+  const userFriendsArray = friendsMap.get(user);
+  let score = 0;
 
-  for (const key of keys) {
-    countFriends(key);
+  for (const friend of userFriendsArray) {
+    if (friendsArray.includes(friend)) score += 10;
   }
 
+  scoreObj[friend] = score;
+
+  return scoreObj;
+};
+
+const scoreVisitor = (visitors, scoreObj) => {
   for (const visitor of visitors) {
     let score = scoreObj[visitor] || 0;
     scoreObj[visitor] = score + 1;
   }
-  const scoreArray = Object.entries(scoreObj).sort((a, b) => {
+};
+
+const getSortedArrayFromObject = (obj) => {
+  return Object.entries(obj).sort((a, b) => {
     if (b[1] > a[1]) return 1;
     else if (b[1] < a[1]) return -1;
     else {
@@ -42,36 +46,83 @@ function problem7(user, friends, visitors) {
       else return 0;
     }
   });
+};
 
-  scoreArray.forEach((friendScore) => {
+const getHighScoredFriend = (obj, userFriendsArray) => {
+  const results = [];
+
+  const friendScoreArray = getSortedArrayFromObject(obj);
+
+  friendScoreArray.forEach((friendScore) => {
     if (
       friendScore[1] !== 0 &&
-      !userFriends.includes(friendScore[0]) &&
-      answer.length < 5
+      !userFriendsArray.includes(friendScore[0]) &&
+      results.length < 5
     )
-      answer.push(friendScore[0]);
+      results.push(friendScore[0]);
   });
-  return answer;
+  //예외사항 7
+  if (results.length == 0) return 'ERROR';
+
+  return results;
+};
+
+const checkFriendLength = (friends) => {
+  for (const friend of friends) {
+    if (friend.length < 1 || friend.length > 30) return false;
+  }
+
+  return true;
+};
+
+const checkUserID = (user) => {
+  const lowerCase = /[a-z]/;
+  for (const element of user.split('')) {
+    if (!lowerCase.test(element)) return false;
+  }
+  return true;
+};
+
+const getUniqueArray = (array) =>
+  [...new Set(array.join('|').split('|'))]
+    .map((v) => v.split(','))
+    .map((v) => v.map((a) => +a));
+
+const checkCommonFriendsShip = (friends) => {
+  const uniqueFriends = getUniqueArray(friends);
+
+  if (uniqueFriends.length !== friends.length) return false;
+  return true;
+};
+
+function problem7(user, friends, visitors) {
+  //예외사항1
+  if (user.length < 1 || user.length > 30) return 'ERROR';
+  //예외사항2
+  if (friends.length < 1 || friends.length > 10000) return 'ERROR';
+  //예외사항3
+  if (!checkFriendLength(friends)) return 'ERROR';
+  //예외사항4
+  if (visitors.length > 1000) return 'ERROR';
+  //예외사항5
+  if (!checkUserID(user)) return 'ERROR';
+  //예외사항6
+  if (!checkCommonFriendsShip(friends)) return 'ERROR';
+
+  const scoreObj = {};
+
+  const friendsMap = getFriendsMap(friends);
+
+  const userFriendsArray = friendsMap.get(user);
+  const friendList = Array.from(friendsMap.keys());
+
+  for (const friend of friendList) {
+    scoreCommonFriends(user, friend, friendsMap, scoreObj);
+  }
+
+  scoreVisitor(visitors, scoreObj);
+
+  return getHighScoredFriend(scoreObj, userFriendsArray);
 }
 
-const testcase = [
-  [
-    'mrko',
-    [
-      ['donut', 'jun'],
-      ['donut', 'andole'],
-      ['donut', 'mrko'],
-      ['shakevan', 'andole'],
-      ['shakevan', 'jun'],
-      ['shakevan', 'mrko'],
-    ],
-    ['bedi', 'bedi', 'donut', 'bedi', 'shakevan'],
-  ],
-];
-
-for (let i = 0; i < testcase.length; i++) {
-  console.log(
-    `${i + 1} ${problem7(testcase[i][0], testcase[i][1], testcase[i][2])}`
-  );
-}
 module.exports = problem7;
