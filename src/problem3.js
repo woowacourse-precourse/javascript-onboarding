65649d1fbf1d4515d5eef4f5f5b19c8dd85ead77
oxhwixo <hwi_030@naver.com>
2022-10-31 16:18:05 +0900

    Problem3: 1부터 number까지 손뼉을 몇 번 쳐야하는지 횟수를 세는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,21 @@
+// 인자가 3,6,9 인지 확인하는 기능
 function find369(number) {
   if (number === "3" || number === "6" || number === "9") return true;
   else return false;
 }
 
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  var numberToStringArray;
 
-  // number를 문자열 배열로 변환하는 기능
-  number = String(number).split("");
+  // 1부터 number까지 손뼉을 몇 번 쳐야하는지 횟수를 세는 기능
+  for (let i = 1; i <= number; i++) {
+    // 숫자를 문자열 배열로 변환하는 기능
+    numberToStringArray = String(i).split("");
+    for (let j = 0; j < numberToStringArray.length; j++) {
+      if (find369(numberToStringArray[j]) === true) answer++;
+    }
+  }
 
   return answer;
 }
