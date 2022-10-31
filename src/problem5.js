dda5481d0b1eb64e676b952b4a901fbb9e595daf
Dongkeun <hongdongk@naver.com>
2022-10-31 20:27:21 +0900

    problem5: 나눈몫을 배열에 담기

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for( let i =0; i<coins.length; i++){
+    answer[i] = parseInt(money/coins[i], 10);
+    money %= coins[i]; 
+  };
+
   return answer;
 }
 
