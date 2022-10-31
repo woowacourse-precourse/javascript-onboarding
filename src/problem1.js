3d30b908f483b090f005aefbed950d9ab04e8a6d
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 21:45:51 +0900

    [Feat:Problem1] 예외 처리하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,14 @@
+const errorHandler = (pobi, crong) => {
+  let error = false;
+  if ([pobi, crong].some((user) => user.length !== 2)) error = true;
+  else {
+    if ([...pobi, ...crong].some((value) => value <= 1 || value >= 400))
+      error = true;
+    if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) error = true;
+  }
+  return error;
+};
+
 const getSum = (number) => {
   return String(number)
     .split("")
