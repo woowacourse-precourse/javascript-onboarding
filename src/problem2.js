ebbe631b6d261a1fbc3890fbbdf472a417373106
KES <aprilsmaple@gmail.com>
2022-10-31 18:53:03 +0900

    refactor: problem2 function splitted

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,22 @@
 function problem2(cryptogram) {
+  return repeatDeleting(cryptogram);
+}
+
+function repeatDeleting(cryptogram) {
   let currentCryptogram = cryptogram;
+
   while (currentCryptogram) {
-    let deletedCryptogram = deleteRepeatedChars(currentCryptogram);
+    let deletedCryptogram = deleteDuplicatedChars(currentCryptogram);
+
     if (deletedCryptogram === currentCryptogram) break;
+
     currentCryptogram = deletedCryptogram;
   }
+
   return currentCryptogram;
 }
 
-function deleteRepeatedChars(cryptogram) {
+function deleteDuplicatedChars(cryptogram) {
   return cryptogram
     .split("")
     .filter(
