853418872090955efe499b878f6f4fed003733ce
webdreamer12 <dreamerdev12@gmail.com>
2022-11-01 01:48:34 +0900

    feat: problem4.js - 주어진 매개변수를 활용하기 위해 배열로 변환후 wordArray 변수에 저장

--- src/problem4.js
+++ src/problem4.js
@@ -4,7 +4,8 @@ function problem4(word) {
   let lowercaseOrdered = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
   let lowercaseReverseOrdered = ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"];
   var referenceIndex = 0;
-  
+
+  var wordArray = Array.from(word);
   var answer;
   return answer;
 }
