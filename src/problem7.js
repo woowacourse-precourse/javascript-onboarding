d0c7ceaae72bf642757cb90de84fb2a2d689ad81
jiyujin <yujin1679@naver.com>
2022-10-31 11:07:44 +0900

    docs(pro7): 기능 요구 사항 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,14 @@
+// 기능 요구 사항
+// 친구 추천 알고리즘!
+// 사용자와 함께 아는 친구의 친구 = 10점 (friends)
+// 사용자의 타임라인에 방문한 횟수 = 1점 (visitors)
+// - [o] user의 친구를 담는 배열을 생성한다.
+// - [o] user의 친구의 친구를 count += 10 해준다.
+// - [o] user의 친구가 아닌 사람 중 visitors에 있는 사람을 count += 1 해준다.
+// - [o] count가 큰 순서대로 정렬한다.
+//   - [o] 단, 점수가 같은 경우 이름순으로 정렬한다.
+//   - [o] 정렬된 배열을 5개로 잘라 이름만 return 하도록 한다.
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
