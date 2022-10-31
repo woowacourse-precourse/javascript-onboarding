4595edb715ddfabb340f84745e5527549ff02742
SeungMiL <tmskqj@naver.com>
2022-11-01 01:43:41 +0900

    problem6 테스트통과(오타로 인한 commit reset 후 재 commit)

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,36 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  let compareArr = [];
+
+  let strMap = new Map();
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      let str = forms[i][1].substr(j, 2);
+      if (strMap.has(str)) {
+        strMap.set(str, strMap.get(str) + 1);
+      } else {
+        strMap.set(str, 1);
+      }
+    }
+  }
+
+  strMap.forEach((x, y) => {
+    if (x >= 2) {
+      compareArr.push(y);
+    }
+  });
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < compareArr.length; j++) {
+      if (forms[i][1].includes(compareArr[j])) {
+        answer.push(forms[i][0]);
+      }
+    }
+  }
+
+  answer.sort();
+
   return answer;
 }
 
