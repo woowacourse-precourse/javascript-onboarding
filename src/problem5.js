eff8cc7eddbaa93c3568ed576eb8f185ff4417fb
su2333066 <su2333066@naver.com>
2022-11-01 00:20:51 +0900

    problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  const extraMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < extraMoney.length; i++) {
+    if (parseInt(money / extraMoney[i]) !== 0) {
+      answer[i] = parseInt(money / extraMoney[i]);
+      money = money % extraMoney[i];
+    }
+  }
+
   return answer;
 }
 
