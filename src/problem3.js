b38ce02263c37b20bf0d484be80d770fe75c015b
DAIN KIM <dnkim419@gmail.com>
2022-10-31 20:55:06 +0900

    feat: solved problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (var i = 1; i <= number; ++i) {
+    while (i > 0) {
+      if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) answer += 1;
+      i /= 10;
+    }
+  }
+
   return answer;
 }
 
