63f4cb15ce11f9466fee7d83cd14e9f633e6ac34
Vo9Ming <kimminj2017@gmail.com>
2022-10-31 23:27:36 +0900

    반복문 안에서 요소 하나하나에 접근하며 각 문자를 비교한 후에 같다면 빼내는 처리

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
   var answer = "";
   var ans = cryptogram.split("");
+  for (var i = 0; i < ans.length; i++) {
+    if (ans[i] === ans[i + 1]) {
+      ans.splice(i + 1, 1);
+      ans.splice(i, 1);
+      i = -1;
+      console.log(ans);
+    }
+  }
 
   return answer;
 }
