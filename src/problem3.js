6c48dd786cce1ba1db2713248c377ac2e3835686
deli-ght <minjju0660@gmail.com>
2022-10-31 22:22:15 +0900

    ✨ 문제 3 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let answer = 0
+  const regex = /3|6|9/g
+  for (let i = 1; i <= number; i++) {
+    answer += (String(i).match(regex) || []).length
+  }
+  return answer
 }
 
-module.exports = problem3;
+module.exports = problem3
