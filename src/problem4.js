6bc5d34419b56e5175e14977a19bdb32052cfec3
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 05:13:09 +0900

    feat: [problem4] 알파벳 대소문자 판별 기능

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,16 @@
+function isUpperCase(c) {
+  if (c.charCodeAt(0) >= 97 && c.charCodeAt(0) <= 122)
+    return true;
+  else
+    return false;
+}
+function isLowerCase(c) {
+  if (c.charCodeAt(0) >= 65 && c.charCodeAt(0) <= 90)
+    return true;
+  else
+    return false;
+}
+
 function problem4(word) {
   var answer;
   return answer;
