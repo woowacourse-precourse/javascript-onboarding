9de64f2847d48d7f187634839b8433e7e4271852
Steadystudy <sgmin206@naver.com>
2022-10-31 15:09:47 +0900

    [problem7] Feat: 친구관계 맵 그리기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,12 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendsMap = {};
+
+  friends.forEach(([a, b]) => {
+    if(friendsMap.hasOwnProperty(a)) friendsMap[a].push(b);
+    else friendsMap[a] = [b];
+    if(friendsMap.hasOwnProperty(b)) friendsMap[b].push(a);
+    else friendsMap[b] = [a];
+  })
 }
 
 module.exports = problem7;
