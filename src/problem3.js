aa11eb89bc5ac5cbc6cd1503bb1ba7388194c8fe
zzinao <kjn0265@gmail.com>
2022-10-31 17:21:38 +0900

    feat: problem3 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let n = 0;
+  let threeSixNine = ['3', '6', '9'];
+  for (i = 0; i < number + 1; i++) {
+    const num = [...String(i).split('')]
+    n += getElNum(num, threeSixNine)
+  }
+  return n
 }
+
+const getElNum = (arr, el) => arr.reduce((acc, v) => acc + (v == el[0] || v == el[1] || v == el[2]), 0);
 
 module.exports = problem3;
