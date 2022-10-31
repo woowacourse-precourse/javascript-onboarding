0248a6cc7983e6aa1c0ebb90240be7a6ab27587a
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-31 21:10:24 +0900

    refactor: PROBLEM 5 리팩토링

--- src/problem5.js
+++ src/problem5.js
@@ -1,21 +1,21 @@
+const check = (money) => {
+  if (money < 1 || 1000000 < money) return false;
+  return true;
+}
+
+function changeMoney(money) {
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+
+  for (let i = 0; i < coins.length; i++) {
+    let count = Math.floor(money / coins[i]);
+    answer.push(count)
+    money %= coins[i];
+  }
+  return answer;
+}
+
 function problem5(money) {
-  const check = (money) => {
-    if (money < 1 || 1000000 < money) return false;
-    return true;
-  }
-
-  function changeMoney(money) {
-    const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-    let answer = [];
-
-    for (let i = 0; i < coins.length; i++) {
-      let count = Math.floor(money / coins[i]);
-      answer.push(count)
-      money %= coins[i];
-    }
-    return answer;
-  }
-
   let result = changeMoney(money)
 
   if (!check(money)) { throw Error("1이상 1,000,000이하 자연수를 입력해주세요.") }
