3619101ad2311de2d736549d63097ce373860241
Circlewee <pnj1118@naver.com>
2022-10-31 21:06:36 +0900

    [#2]feat: solved problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,40 @@
+/**
+ * @param {string} cryptogram
+ * @returns {string}
+ */
 function problem2(cryptogram) {
-  var answer;
+  let flag = true;
+  let temp = cryptogram;
+  let answer;
+  let cache = '';
+
+  while (flag) {
+    for (let i = 0; i < temp.length; i++) {
+      const cur = temp[i];
+      for (let j = i + 1; j < temp.length; j++) {
+        const next = temp[j];
+
+        if (cur === next) {
+          if (!cache) {
+            cache += cur + next;
+          } else {
+            cache += next;
+          }
+        } else {
+          break;
+        }
+      }
+
+      if (cache) break;
+    }
+
+    answer = temp.replace(cache, '');
+    cache = '';
+
+    if (temp !== answer) temp = answer;
+    else flag = false;
+  }
+
   return answer;
 }
 
