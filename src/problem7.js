45db25debdb2d8a12ad1fa4a8a756e38f6256360
Minyoung Lee <mminy62@gmail.com>
2022-10-31 23:43:50 +0900

    feat: [PROBLEM7] 친구 후보 목록 생성
    friends, visitors을 통해 user, 중복을 없앤 것으로 친구 후보 목록을 생성한다.
    친구 목록은 object로 (key, value) = (이름, 점수)

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,38 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let flist = {};
+
+  //1. friends, visitors 합쳐 중복 없앤 후 친구 후보 목록 만들기
+  let fkeys = friends.reduce(function (acc, cur){
+    return acc.concat(cur);
+  })
+  visitors.map(item => fkeys.push(item));
+
+  const set = new Set(fkeys);
+  set.delete(user);
+
+  //이름 : 0점 
+  set.forEach(element => {
+    flist[element] = 0;
+  });
+
+
+  
+
   return answer;
 }
+
+problem7(
+  "mrko",
+        [
+          ["donut", "andole"],
+          ["donut", "jun"],
+          ["donut", "mrko"],
+          ["shakevan", "andole"],
+          ["shakevan", "jun"],
+          ["shakevan", "mrko"],
+        ],
+        ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 
 module.exports = problem7;
