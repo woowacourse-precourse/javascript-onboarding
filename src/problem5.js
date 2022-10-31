f4bad1cc931ca0a170f662329354a3b5742ceca6
loco9939 <kls9939@naver.com>
2022-10-31 10:36:34 +0900

    fix: add a line space
    - 가독성을 위해 한줄 띄어쓰기 해줬습니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,9 @@
 function problem5(money) {
   if (!money || money > 1000000) throw new RangeError('매개변수는 1 이상 1,000,000 이하의 자연수여야 합니다.');
-  
+
   let moneyInPulse = money;
   const papers = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
   return papers.map((paper) => {
     const count = parseInt(moneyInPulse / paper);
     moneyInPulse = moneyInPulse % paper;
