14e31269d5b947599d50e79cfbe39ffc837e7d40
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-11-01 05:04:21 +0900

    [5번 문제] feat: solution 메소드 구현

--- src/problem5.js
+++ src/problem5.js
@@ -3,7 +3,12 @@ function countBill(money,bill){
 }
 
 function problem5(money) {
-  var answer;
+  var answer = [0,0,0,0,0,0,0,0,0];
+  var bill = [50000,10000,5000,1000,500,100,50,10,1];
+  for(let i = 0;i < 9;i++){
+    answer[i] += countBill(money,bill[i]);
+    money %= bill[i];
+  }
   return answer;
 }
 
