30a9482c4e4577b6d8740f1bcf1d4580ee0f8e2d
Vo9Ming <kimminj2017@gmail.com>
2022-10-31 12:56:50 +0900

    전달받은 매개변수가 10보다 작을 조건에 대한 처리

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (var i = 1; i < number + 1; i++) {
+    if (number < 10) {
+      if (i % 1 === 3 || i % 10 === 3 || i / 10 === 9) {
+        answer++;
+      } // 일의 자리에 3, 6, 9가 들어갈 경우
+    }
+  }
+
   return answer;
 }
 
