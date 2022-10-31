678b34a87e0c366277956a1a615b63ce065d99ef
YMS <yms1789@naver.com>
2022-10-31 20:22:58 +0900

    fix: problem6 정렬 및 중복 제거 기능 수정

--- src/problem6.js
+++ src/problem6.js
@@ -19,14 +19,43 @@ function problem6(forms) {
         same.push(forms[j][0]);
       }
     }
-    group.push(same.sort());
+    group.push(
+      same.sort((a, b) => {
+        if (a > b) return 1;
+        else if (a < b) return -1;
+        else return 0;
+      })
+    );
   }
   for (let i = 0; i < group.length; i++) {
     if (group[i].length > 1) {
       answer.push(...group[i]);
     }
   }
-  return answer;
+  answer = answer.sort((a, b) => {
+    if (a > b) return 1;
+    else if (a < b) return -1;
+    else return 0;
+  });
+  return [...new Set(answer)];
 }
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+    ["jasp@email.com", "박이김"],
+    ["jp@email.com", "김이박"],
+    ["qpw@email.com", "김이순"],
+    ["teemo@email.com", "티모"],
+    ["banana@email.com", "대위티모"],
+    ["secondJm@email.com", "제이엠"],
+    ["apple@email.com", "땷횱봵"],
+    ["stran@email.com", "횱봵으"],
+    ["zebra@email.com", "두글자이상의문자가연속적으로이제맞티모"],
+  ])
+);
 
 module.exports = problem6;
