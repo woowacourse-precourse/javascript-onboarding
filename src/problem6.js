b1933f2df003a87ecee74a9c614aa987e5da98a7
violetwhenisawu <anashape710@gmail.com>
2022-10-31 22:51:45 +0900

    Feat: forms의 이름 요소들을 도는 반복문 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms.length - 1; j++) {
+      let name1 = forms[i][1];
+      let name2 = forms[j][1];
+      if (name1 === name2) {
+        console.log(name1, name2);
+        break;
+      }
+    }
+  }
 }
 
 module.exports = problem6;
+problem6([
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+]);
