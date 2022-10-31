6fb913770984152bb45b51f5794186d4013ef129
useon <yoosk4986@naver.com>
2022-10-31 17:47:32 +0900

    feat: [problem5] 구한 몫과 index의 단위를 곱한 값을 주어진 돈에서 뺀다.

--- src/problem5.js
+++ src/problem5.js
@@ -2,9 +2,11 @@ function problem5(money) {
   let answer = [];
   // 1. 화폐 단위를 담은 배열을 만든다.
   const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  for (let index = 0; i < unit.length; index++) {
+  for (let index = 0; index < unit.length; index++) {
     // 2. 화폐 단위 배열을 순회하며 주어진 돈과 해당 index의 화폐 단위를 나눈 몫을 answer 배열에 넣는다.
     answer.push(Math.floor(money / unit[index]));
+    // 3. 주어진 돈에서 해당 index의 단위로 나눈 몫과 화폐 단위를 곱한 값을 빼준다.
+    money -= unit[index] * Math.floor(money / unit[index]);
   }
   return answer;
 }
