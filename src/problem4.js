9d9d77cef6e28f928e5ea0e91bf94d507b0f2952
somin <smhs12@naver.com>
2022-10-31 15:54:47 +0900

    feat: problem4 / 대문자 판단 함수

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,10 @@
 function problem4(word) {
-  var answer;
+  const answer = [];
   return answer;
 }
 
+function isUpper(ascii) {
+  if (ascii >= 65 && ascii <= 90) return true;
+  return false;
+}
 module.exports = problem4;
