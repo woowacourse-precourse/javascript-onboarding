1cc06688137435a4f251074e154c384bda842cdc
ilgon0110 <moses011010@gmail.com>
2022-10-31 17:25:31 +0900

    docs:기능 구현 목록 수정(#problem1)

--- src/problem1.js
+++ src/problem1.js
@@ -1,24 +1,28 @@
 function problem1(pobi, crong) {
-  let answer;
+  //기능목록 1-1, 1-2, 1-3 구현
+  const isWrong = () => {
+    return (
+      pobi[0] % 2 !== 1 ||
+      pobi[1] % 2 !== 0 ||
+      pobi[0] === 1 ||
+      pobi[1] === 400 ||
+      pobi[1] - pobi[0] !== 1 ||
+      crong[0] % 2 !== 1 ||
+      crong[1] % 2 !== 0 ||
+      crong[0] === 1 ||
+      crong[1] === 400 ||
+      crong[1] - crong[0] !== 1
+    );
+  };
 
   //예외처리
-  //기능목록 1-1 구현
-  if (
-    pobi[0] % 2 !== 1 ||
-    pobi[1] % 2 !== 0 ||
-    crong[0] % 2 !== 1 ||
-    crong[1] % 2 !== 0
-  )
-    return -1;
-  //기능목록 1-2 구현
-  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1;
-
+  if (isWrong()) return -1;
   //연산
   let pScore = 0;
   let cScore = 0;
 
   //기능목록 2-1,2-2 구현
-  const makeNum = (num) => {
+  const makeNum = num => {
     let sum = 0;
     let times = 1;
     while (num) {
@@ -37,6 +41,7 @@ function problem1(pobi, crong) {
 
   //출력값 구하기
   //기능목록 3-1 구현
+  let answer;
   if (pScore > cScore) answer = 1;
   else if (pScore < cScore) answer = 2;
   else answer = 0;
