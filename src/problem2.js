a68cb87d408fa6d9ada266a178ab91ee92097386
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 04:20:19 +0900

    feat: [problem2] 연속하는 중복 문자가 없을 때까지 반복하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,8 @@
 function removeConsecutive(cryptogram) {
   filteredCrypto = cryptogram.filter((char, i, list) => list[i-1] != char && list[i+1] != char);
+  if (cryptogram.toString() == filteredCrypto.toString())
+    return filteredCrypto;
+  return removeConsecutive(filteredCrypto);
 }
 
 function problem2(cryptogram) {
