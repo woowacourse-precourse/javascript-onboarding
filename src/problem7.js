caaec88d0f30b328a8bec847eac513bd48491c84
Sangwon Lee <tkdtm2010@ajou.ac.kr>
2022-10-31 11:11:01 +0900

    name과 계산된 point를 한 쌍으로 하는 배열을 원소로 가지는 배열 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,6 @@
 function problem7(user, friends, visitors) {
   var answer;
+  const initPoint = parseInt(0);
   let list = [];
   let recommendList = [];
   let tempVisitor = visitors;
@@ -41,6 +42,35 @@ function problem7(user, friends, visitors) {
     }
     return allNames;
   }, {});
+
+  let countedArray = Object.keys(countedNames);
+  let visitorsArray = Object.keys(countedVisitor);
+
+  let finalList = [...countedArray, ...visitorsArray];
+
+  finalList = finalList.filter((element, index) => {
+    return finalList.indexOf(element) === index;
+  });
+
+  for (let i = 0; i < finalList.length; i++) {
+    finalList[i] = [finalList[i], initPoint];
+  }
+
+  for (let nameInCounted in countedNames) {
+    for (let nameInList of finalList) {
+      if (nameInCounted === nameInList[0]) {
+        nameInList[1] += countedNames[nameInCounted] * 10;
+      }
+    }
+  }
+
+  for (let visitorInCounted in countedVisitor) {
+    for (let nameInList of finalList) {
+      if (visitorInCounted === nameInList[0]) {
+        nameInList[1] += countedVisitor[visitorInCounted];
+      }
+    }
+  }
 
   return answer;
 }
