ae6269d65c455a7fa1ff1289e0da091c6d3bf76f
하송희 <99heesong@naver.com>
2022-10-31 23:03:18 +0900

    [문제 6] 기능추가: 닉네임 중복 비교 기준 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,9 @@
 function problem6(forms) {
   const newForms = forms.map((arr) => ({ email: arr[0], nickname: arr[1] }));
+
+  for(let i=0; i<obj.nickname.length-1; i++) {
+    const str = obj.nickname[i] + obj.nickname[i+1];
+  }
 }
 
 module.exports = problem6;
