ed0089dde69e46be20cc0e833023393337d62788
sungjihyun <jhsung23@gmail.com>
2022-10-31 14:45:56 +0900

    Feat: problem 7: user의 친구들 목록 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,21 @@
+const getUsersFriends = (user, relationship) => {
+  let friends = relationship.reduce((acc, [personA, personB]) => {
+    if (personA === user) {
+      acc.push(personB);
+    } else if (personB === user) {
+      acc.push(personA);
+    }
+    return acc;
+  }, []);
+  return [...new Set(friends)];
+};
+
 function problem7(user, friends, visitors) {
   var answer;
+
+  // 유저의 친구들(내친구들)
+  const usersFriends = getUsersFriends(user, friends);
+
   return answer;
 }
 
