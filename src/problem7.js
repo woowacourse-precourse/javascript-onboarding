480cfb1fdd95e77b95baa9bc5ad7995a17fd738b
knh6269 <knh6269@gmail.com>
2022-10-31 22:50:38 +0900

    feat : 미션7) 내 친구 목록 생성 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,15 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+//내 친구 목록 생성 함수
+const makeMyFriends = (friends, user, myFriends) =>{
+    
+  for(let friend of friends){
+    if (friend.includes(user)) {
+      myFriends.push(
+        friend[Math.abs(friend.indexOf(user)-1)]
+        );
+    }
+  }
+};
+
 }
 
 module.exports = problem7;
