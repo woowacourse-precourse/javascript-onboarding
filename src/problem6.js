f25c7e6f750397f4e2a644b94b3e13800f81759c
Buyoung Chung <sunarol@naver.com>
2022-10-31 18:22:35 +0900

    [문제6] 닉네임을 한글자씩 비교
    연속으로 같은 문자를 가졌다면 유사한 닉네임

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,21 @@
 function problem6(forms) {
   var answer = [];
   for (let i = 0; i < forms.length; i++) {
-    for (let j = i + 1; j < forms.length; j++) {}
+    for (let j = i + 1; j < forms.length; j++) {
+      let currentNick = forms[i][1];
+      let compareNick = forms[j][1];
+      let lt = (rt = cnt = 0);
+      while (rt < currentNick.length) {
+        if (currentNick[lt] !== compareNick[rt]) {
+          rt++;
+          cnt = 0;
+        } else {
+          lt++;
+          rt++;
+          cnt++;
+        }
+      }
+    }
   }
   return answer;
 }
