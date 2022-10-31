6e0633f52b5678583b8936c2e0ec66faeee474c5
JangHoonSeok <orirori512@naver.com>
2022-11-01 01:32:18 +0900

    feat: problem4. 주어진 문자열을 아스키 코드로 변환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
+// ASCII 코드
+// 대문자 : 65 ~ 90
+// 소문자 : 97 ~ 122
+// 32 : Space
 function problem4(word) {
   var answer;
+  let list = [];
+  list = wordToASCII(word);
   return answer;
+}
+
+function wordToASCII(word) {
+  // word를 ASCII 코드로 변환하는 함수.
+  let arrayASCII = [];
+  for (item of word) {
+    arrayASCII.push(item.charCodeAt(0));
+  }
+  return arrayASCII;
 }
 
 module.exports = problem4;
