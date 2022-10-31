f57c43ad396ec4ed66001a431ce322a80cf31edf
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-10-31 11:31:08 +0900

    feat: Add get maximum of Pobi and Crong

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,19 @@
 function problem1(pobi, crong) {
-  var answer;
   if (checkValue(pobi) && checkValue(crong)) {
-    return answer;
+    let maximumPobi = maximum([
+      leftSumValue(pobi),
+      rightSumValue(pobi),
+      leftMultValue(pobi),
+      rightMultValue(pobi),
+    ]);
+    let maximumCrong = maximum([
+      leftSumValue(crong),
+      rightSumValue(crong),
+      leftMultValue(crong),
+      rightMultValue(crong),
+    ]);
+
+    return Math.max(maximumPobi, maximumCrong);
   }
   return -1;
 }
@@ -16,16 +28,25 @@ function checkValue(arr) {
 //   return arr[0]>400||arr[1]>400||arr[0]<1||arr[1]<1?false:arr[1]-arr[0] == 1?true:false;
 // }
 
-function sumValue(arr) {
+function leftSumValue(arr) {
   let sum = 0;
-  do {
-    sum += arr[0] % 10;
-    arr[0] = Math.floor(arr[0] / 10);
-  } while (arr[0] > 0);
+  arr[0] = arr[0].toString();
+  for (let i = 0; i < arr[0].length; i++) {
+    sum += parseInt(arr[0][i]);
+  }
   return sum;
 }
 
-function multValue(arr){
+function rightSumValue(arr) {
+  let sum = 0;
+  arr[1] = arr[1].toString();
+  for (let i = 0; i < arr[1].length; i++) {
+    sum += parseInt(arr[1][i]);
+  }
+  return sum;
+}
+
+function leftMultValue(arr) {
   let mult = 1;
   do {
     mult *= arr[0] % 10;
@@ -34,11 +55,19 @@ function multValue(arr){
   return mult;
 }
 
-function maximun([arr]){
-  return Math.max(arr);
+function rightMultValue(arr) {
+  let mult = 1;
+  do {
+    mult *= arr[1] % 10;
+    arr[1] = Math.floor(arr[1] / 10);
+  } while (arr[1] > 0);
+  return mult;
 }
 
-// console.log(problem1([99, 100], [197, 198]));
+function maximum(arr) {
+  return Math.max(...arr);
+}
 
+console.log(problem1([187, 188], [197, 198]));
 
 module.exports = problem1;
