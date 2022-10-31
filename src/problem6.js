9fbe8d99d7365f4d1b787cf459917b1743c33172
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-31 16:20:48 +0900

    refactor: [P6] 한 크루의 닉네임을 연속적인 두 글자로 나눈 모든 단어를 담은 Array 만들기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,13 @@
 function problem6(forms) {
   let targetEmail = []
   let wordCase = {}
+  for (let form of forms) {
+    let nickname = form[1]
+    let wordList = []
+    for (let i = 0; i < nickname.length - 1; i++) {
+      wordList.push(nickname.substring(i, i + 2))
+    }
+  }
 } 
 
 module.exports = problem6;
