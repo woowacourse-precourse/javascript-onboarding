ca815a931c5b6509387fafee57f447979c04c03c
dahlia <wlstnrdahlia@gmail.com>
2022-11-01 00:25:48 +0900

    Add : problem5 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,33 @@
 function problem5(money) {
   var answer;
+  answer = [];
+  let bill = 50000;
+  for (let i = 0; i < 9; i++) {
+    let num;
+    if (i === 0) {
+      num = Math.floor(money / bill);
+      money %= bill;
+      answer.push(num);
+    } 
+    else if (i !== 8) {
+      {
+        if (i % 2 === 1) {
+          bill /= 5;
+          num = Math.floor(money / bill);
+        }
+        else if (i % 2 === 0) {
+          bill /= 2;
+          num = Math.floor(money / bill);
+        }
+        money %= bill;
+        answer.push(num);
+      }
+    }
+    else {
+      answer.push(money);
+    }
+  }
+  
   return answer;
 }
 
