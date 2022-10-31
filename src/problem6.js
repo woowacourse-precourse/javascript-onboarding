00787bc1550c9e24e29132b3fc4be4bc9fe1eb57
KUD <kimud6003@gmail.com>
2022-10-31 16:53:34 +0900

    ✨ Feature: problem6.js duplicateChekcer - problem6.js 문자 2개가 중복시 이메일 저장 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  var answer;
+  var answer= duplicateChecker(forms);
   return answer;
 }
 
+function duplicateChecker(forms) {
+  let result = []
+  let store = new Set();
+  forms.forEach(([email,nickName])=>{
+    for(let i = 0; i <nickName.length-1; i++ ){
+      const wordOfNickName = nickName.slice(i,i+2);
+      if(!store[wordOfNickName])
+        store[wordOfNickName] = new Set([email]);
+      else
+        store[wordOfNickName].add(email);
+    }
+    for(let key of Object.keys(store))
+      if(store[key].size> 1 ) 
+        result.push(...store[key]);
+  })
+  return [...new Set(result)].sort();
+}
 module.exports = problem6;
