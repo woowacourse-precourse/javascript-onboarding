e78e1463735adf0d1b62774ef02157646c9b15c1
7ngenious <zxvs1218@gmail.com>
2022-11-01 05:17:45 +0900

    feat: [Problem3] 1st Soltion

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    let cnt = 0;
+    for (let j of String(i)) {
+      if (j === '3' || j === '6' || j === '9') cnt++;
+    }
+    answer += cnt;
+  }
+
   return answer;
 }
 
