4348cece51a02e6dfdc2fb1c8cc84f759abcab71
Adult96 <djxor007@gmail.com>
2022-11-01 04:45:20 +0900

    refactor: problem7 -  주석 삭제

--- src/problem7.js
+++ src/problem7.js
@@ -1,13 +1,10 @@
 function problem7(user, friends, visitors) {
-  //friends 매개변수에서 user매개변수를 없앤다
   let removeMrko = friends.map((v) => v.filter((v) => v !== user));
 
-  //length가 1개인 배열을 찾는다
   const findFriends = removeMrko
     .filter((v) => v.length === 1)
     .flatMap((v) => v);
 
-  //user를 없앤 배열 변수(removeMrko)에서 length가 1개인 (findFriends)같은 값을 삭제한다
   for (const value of findFriends) {
     removeMrko = removeMrko
       .flatMap((v) => v)
@@ -19,11 +16,9 @@ function problem7(user, friends, visitors) {
       .sort();
   }
 
-  //찾은 목록들에서 중복을 제거한다
   const selectFriends = [...new Set(removeMrko)];
   const selectVisitors = [...new Set(visitors)];
 
-  //찾은 friends와 visitors에게 점수를 부여한다
   const object = {};
   for (const value of selectFriends) {
     object[value] = removeMrko.filter((v) => v === value).length * 10;
@@ -34,12 +29,10 @@ function problem7(user, friends, visitors) {
       : (object[value] = visitors.filter((v) => v === value).length);
   }
 
-  //높은 점수순으로 이름을 나열한다
   const sortableObject = Object.entries(object)
     .sort(([, a], [, b]) => b - a)
     .reduce((r, [k, v]) => ({ ...r, [k]: v }), {});
 
-  //최대 5명까지만 return한다
   const answer = Object.keys(sortableObject).slice(0, 5);
 
   return answer;
