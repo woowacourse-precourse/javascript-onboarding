25116be3dbb680a732486ad36792e640cca087de
MINJUN PARK <mj9457@naer,com>
2022-10-31 22:39:04 +0900

    Feat. week1 problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = '';
+  var arr = cryptogram.split('');
+  
+  while(true) {
+    var t = true;
+    for(var i=0; i<arr.length-1; i++) {
+      if(arr[i] === arr[i+1]) {
+        arr.splice(i,2);
+        t = false;
+      }
+    }
+    if(t) {
+      break;
+    }
+  }
+  answer = arr.join('');  
   return answer;
 }
 
