5281015104e50b62e3e9e4016e11b8545a8c6d2c
knh6269 <knh6269@gmail.com>
2022-10-31 22:49:28 +0900

    feat : 미션6) Map을 순회하며 중복되는 유저의 이메일을 찾는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -10,6 +10,21 @@ const checkMap = (index,string,maps) => {
   }
 };
 
+ //map을 순회하며 중복되는 유저의 이메일을 찾는 함수
+ const mapValueMoreThanOne = (maps,same,forms) =>{
+  //map의 value를 순회하며 배열의 크기 1보다 크면 
+  for(let map of maps.values()){
+    if (map.length > 1) {
+      for(let i =0 ; i<map.length; i++){
+        //해당 인덱스를 가진 forms의 이메일을 푸쉬
+          same.push(forms[map[i]][0]);
+        }
+      
+    }
+  }
+};
+const removeSame = (same) => {
+  return [...new Set(same)]
 }
 
 module.exports = problem6;
