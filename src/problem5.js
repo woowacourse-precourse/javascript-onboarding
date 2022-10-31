f3581c4fb395c2c86988bf06d9f5157bf2c37e74
gsong <rmsdlf1206@gmail.com>
2022-10-31 20:39:54 +0900

    반복문을 통해 moneyBack함수 실행 및 result 배열 반환

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,10 @@
 function problem5(money) {
   let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < result.length; i++) {
+    money = moneyBack(money, moneyUnit[i], i, result);
+  }
+  return result;
 }
 function moneyBack(money, unit, index, result) {
   while (money >= unit) {
