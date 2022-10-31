5bca82794da2231c41879d79adca7890f6291922
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:29:12 +0900

    3번 문제 3,6,9일시 1더하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
   var answer;
+  
+
+
+  if(number==3){
+    answer= answer+1;
+  } else if(number==6){
+    answer=answer+1;
+  } else if(number==9){
+    answer=answer+1;
+  }
+
+  
   return answer;
 }
 
