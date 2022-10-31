5256bafd0914c79ed90b21056b54d87948a70d17
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2022-10-31 21:45:39 +0900

    :memo: update to do list
    add function list to solve problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,13 @@
 function problem1(pobi, crong) {
   var answer;
+
   return answer;
 }
+
+// 1-1. 각 자리 숫자 모두 더하는 함수
+
+// 1-2. 각 자리 숫자 모두 곱하는 함수
+
+// 1-3. 가장 큰 수 구하는 함수
 
 module.exports = problem1;
