e8651cd5d367576892185950126ef7ba4b9f45ca
yongjaechoi <88219703+yjc2021@users.noreply.github.com>
2022-10-31 11:32:19 +0900

    feat: problem 6 기능목록 1: map 가공

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  let map = new Map();
+  // 닉네임을 2글자씩 map에 key로 입력, 개수를 value로 입력
+  for (let i = 0; i < forms.length; i++) {
+    let nname = forms[i][1];
+    for (let j = 0; j < nname.length - 1; j++) {
+      const token = nname.substr(j, 2);
+      if (!map.has(token)) {
+        map.set(token, 1);
+      } else {
+        map.set(token, map.get(token) + 1);
+      }
+    }
+  }
 }
 
 module.exports = problem6;
