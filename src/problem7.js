16e9a052f8f8a1fe54874b5439061ea40511d744
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-31 19:31:31 +0900

    feat<problem7>: Implement exceptions

--- src/problem7.js
+++ src/problem7.js
@@ -9,7 +9,7 @@
  * 1. 아이디는 길이가 1 이상 30 이하인 문자열이다.
  * 2. friends는 길이가 1 이상 10,000 이하인 배열이다.
  * 3. visitors는 길이가 0 이상 10,000 이하인 배열이다.
- * 4. 사용자 아이디는 알파벳 소문자로만 이루어져 있다.
+ * 4. 아이디는 알파벳 소문자로만 이루어져 있다.
  * 5. 동일한 친구 관계가 중복해서 주어지지 않는다
  * 6. 추천할 친구가 없는 경우는 주어지지 않는다.
  */
@@ -19,7 +19,6 @@ const getScoreByVisitors = (visitors, score) => {
     score[visitor] = visitors.reduce((cnt, element) => cnt + (visitor === element), 0)
   })
 }
-
 
 const getMyFriends = (user, friends) => {
   let myFriends = []
@@ -50,27 +49,75 @@ const getScoreByFriends = (friends, myFriends, score) => {
   })
 }
 
-const sortScore = score => Object.fromEntries(Object.entries(score).sort(([,a],[,b]) => a > b? -1: 1))
+const sortScore = score => Object.fromEntries(Object.entries(score).sort(([,a], [,b]) => a > b? -1: 1))
 
+const filterKey = (conditions, me, score) => {
+  conditions.push(me)
+  return Object.keys(score).filter(userId => !(conditions.includes(userId)));
+}
 
+const isIDLengthError = id => !(id.length >= 1 && id.length <= 30)
+const isLowerCase = id => id === id.toLowerCase()
+
+const validationsOfUser = user => {
+  if (isIDLengthError(user)) {
+    throw "user ID length error"
+  }
+  if (!isLowerCase(user)) {
+    throw "ID must be small letter"
+  }
+}
+const validationsOfFriends = friends => {
+  if (!(friends.length >= 1 && friends.length <= 10000)) {
+    throw "friends length error"
+  }
+  friends.map(friend => {
+    if (friend.length === 2) {
+      if (isIDLengthError(friend[0]) || isIDLengthError(friend[1])) {
+        throw "friend ID length error"
+      }
+      if (!isLowerCase(friend[0]) || !isLowerCase(friend[1])) {
+        throw "ID must be small letter"
+      }
+    } else {
+      throw "friend length error"
+    }
+  })
+}
+const validationsOfVisitors = visitors => {
+  if (!(visitors.length >= 0 && visitors.length <= 10000)) {
+    throw "visitors length error"
+  }
+  visitors.map(visitor => {
+    if (isIDLengthError(visitor)) {
+      throw "visitor ID length error"
+    }
+    if (!isLowerCase(visitor)) {
+      throw "ID must be small letter"
+    }
+  })
+}
+
+const validations = (user, friends, visitors) => {
+  validationsOfUser(user);
+  validationsOfFriends(friends);
+  validationsOfVisitors(visitors);  
+}
 
 function problem7(user, friends, visitors) {
+  let answer;
   let score = {};
   let myFriends = getMyFriends(user, friends);
 
-  getScoreByVisitors(visitors ,score);
+  validations(user, friends, visitors);
+
+  getScoreByVisitors(visitors, score);
   getScoreByFriends(friends, myFriends, score);
   score = sortScore(score)
-  
-  myFriends.push(user)
+  answer = filterKey(myFriends, user, score)
 
-  let filteredKeys = Object.keys(score).filter(userId => {
-    return !(myFriends.includes(userId))
-  });
-
-  return filteredKeys;
+  return answer;
 }
-
 
 module.exports = problem7;
 
