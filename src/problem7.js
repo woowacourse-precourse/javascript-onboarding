f35447013f888d97f7cb6d5a4505c2355963283b
HWI <dlwognl99@naver.com>
2022-10-31 22:44:01 +0900

    문제7_feat: 친구일 때 점수 얻는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,8 +1,34 @@
 function problem7(user, friends, visitors) {
   // user와 친구인 사용자 아이디 배열
   const alreadyFriend = [];
+  // 아이디를 키 점수를 값으로 가지고 있는 score객체
+  const scoreObj = {};
 
   friendCheck(user, friends, alreadyFriend);
+
+  // friends 점수 얻기
+  alreadyFriend.forEach((element) => {
+    friends.forEach((targetElement) => {
+      // targetElement 첫번째 요소의 사용자 아이디가 user와 이미 친구인 사용자의 아이디라면
+      if (targetElement[0] === element) {
+        // score객체에서 키 값이 비어있고 그 키 값이 user가 아니라면 채워주고 10점을 부여한다.
+        if (!scoreObj[targetElement[1]] && targetElement[1] !== user) {
+          scoreObj[targetElement[1]] = 10;
+          // score 객체에서 키 값이 비어있지 않고 그 키 값이 user가 아니라면 이미 있는 값에 10점을 더한다.
+        } else if (targetElement[1] !== user) {
+          scoreObj[targetElement[1]] += 10;
+        }
+      }
+      // targetElement 두번째 요소의 사용자 아이디가 user와 이미 친구인 사용자의 아이디라면
+      else if (targetElement[1] === element) {
+        if (!scoreObj[targetElement[0]] && targetElement[0] !== user) {
+          scoreObj[targetElement[0]] = 10;
+        } else if (targetElement[0] !== user) {
+          scoreObj[targetElement[0]] += 10;
+        }
+      }
+    });
+  });
 }
 
 function friendCheck(user, friends, alreadyFriend) {
@@ -14,6 +40,8 @@ function friendCheck(user, friends, alreadyFriend) {
       alreadyFriend.push(element[0]);
     }
   });
+
+  return alreadyFriend;
 }
 
 module.exports = problem7;
