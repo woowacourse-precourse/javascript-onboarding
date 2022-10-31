64e5656a3cde7c3c647651ff4bd71e09be4445db
김희진 <choha941108@gmail.com>
2022-10-31 19:42:50 +0900

    (7번) 부족한 부분 수정

--- src/problem7.js
+++ src/problem7.js
@@ -5,12 +5,20 @@ function problem7(user, friends, visitors) {
   console.log("값확인",friends)
   console.log("값확인",user)
 
-
+  const peple = []
+  for (p = 0; p < friends.length ; p++) { 
+    for (q = 0 ; q < friends[p].length; q++){
+      peple.push(friends[p][q])
+    }
+  }
+  console.log("사람수 확인",peple)
+  //추천친구 배열 만들기
+  
   const arr = []
   let nums = 0
 
-  for (i = 0;  i < friends.length ; i ++) {
-    arr.push([friends[i][1],nums])
+  for (i = 0;  i < peple.length ; i ++) {
+    arr.push([peple[i],nums])
   }
   console.log("점수배열 이름",arr)
   //[[추천친구],[점수]]배열 만들기
@@ -22,7 +30,18 @@ function problem7(user, friends, visitors) {
   } console.log("유저이름 제외",arr)
   //친구리스트 유저이름 제외
 
-  for( i = 0 ; i < arr.length ; i++) {
+  for ( l = 0 ; l < arr.length ; l++){
+    const arr_2 = arr[l][0]
+    for(m = l+1; m < arr.length; m++) {
+      if(arr_2 == arr[m][0]) {
+        arr.splice(m,1)
+      }
+    } 
+  } console.log("중복이름 제외_1",arr)
+}
+  //중복 이름 삭제
+
+  /*for( i = 0 ; i < arr.length ; i++) {
     const arr_1 = arr[i][0];
     
     for(j = i+1; j < arr.length; j++) {
@@ -33,21 +52,22 @@ function problem7(user, friends, visitors) {
   }console.log("중복친구 점수 + 10",arr)
   //중복 인자 갯수 * 10 점수에 더하기
 
-  for ( l = 0 ; l < arr.length ; l++){
-    const arr_2 = arr[l][0]
-    for(m = l+1; m < arr.length; m++) {
-      if(arr_2 == arr[m][0]) {
-        arr.splice(m,1)
+
+  
+  for( n = 0 ; n < arr.length ; n++) {
+    const arr_1 = arr[n][0];
+    
+    for(o = n+1; o < arr.length; o++) {
+      if(arr_1 == visitors[o]) {
+        arr[i][1] += 1
       }
-    } 
-  } console.log("중복이름 제외_1",arr)
-  //중복 이름 삭제
+    } console.log("한자리점수 확인",arr)
   
-  
+
   //중복 이름 + visitors 내 중복 인자 확인
   //중복인자 확인 된 숫자 * 1 점수에 더하기
   //[점수]배열 기준 오름차순 배열 만들기
   //[추천친구] 이름 도출 
-  return answer;
-}
+  return answer;*/
+
 module.exports = problem7;
