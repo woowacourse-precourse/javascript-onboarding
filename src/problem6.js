91d1a7c085a6cf24f691034658e99d1994d06f79
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-31 11:11:24 +0900

    p6: 각 단어의 순열 테이블을 만드는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,26 @@
 function problem6(forms) {
   const MIN_WORD_LENGTH = 2;
+
+  function createSegmentsTable(forms) {
+    const table = {};
+
+    for (let form of forms) {
+      const [email, name] = form;
+      const segments = getSegments(name);
+
+      for (let segment of segments) {
+        if (table[segment]) {
+          table[segment].push(email);
+        }
+        else {
+          table[segment] = [email];
+        }
+      }
+    }
+
+    return table;
+  }
+
 }
 
 module.exports = problem6;
