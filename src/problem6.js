67fb1ee807241c48bbbbe3d84bfd76ef94fc5ffa
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-31 19:37:39 +0900

    problem6 - feat: 이메일 예외 함수
    이메일은 이메일 형식에 부합하며, 전체 길이는 11자 이상 20자 미만이다.

--- src/problem6.js
+++ src/problem6.js
@@ -18,6 +18,20 @@ const getLength = element => {
   return element.length;
 };
 
+const isEmailException = email => {
+  const emailLength = getLength(email);
+
+  if (emailLength < 11 || 20 <= emailLength) {
+    throw new RangeError("The total length is between 11 characters and less than 20 characters.");
+  }
+
+  if (!email.match(/[a-z0-9]+@email.com/)) {
+    throw new Error("Email applications are limited to the email.com domain only.");
+  }
+
+  return false;
+};
+
 function problem6(forms) {
   var answer;
   return answer;
