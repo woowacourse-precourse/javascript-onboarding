5ce0066f96e05190442ca47ac91dc7717a6b2484
Monte <monte6198@gmail.com>
2022-10-31 21:31:05 +0900

    feat(problem6.js): problem6()에서 getAllSlices() 호출 추가

--- src/problem6.js
+++ src/problem6.js
@@ -31,6 +31,7 @@ function problem6(forms) {
   var answer;
   let result = new Set(); //같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록
   let allSlices = new Map(); //크루들의 닉네임 내에 존재하는 연속적인 문자열 목록(문자열, 출현 빈도 수)
+  getAllSlices(forms, allSlices);
 
   return answer;
 }
