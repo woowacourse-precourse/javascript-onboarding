3ac9a6612633b9e0c7915b759dcde4796f13e5f0
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-31 20:11:22 +0900

    problem7 - feat: each, map 순회함수

--- src/problem7.js
+++ src/problem7.js
@@ -2,6 +2,22 @@ const copyArray = targetArray => {
   return [...targetArray];
 };
 
+const each = (iterable, func) => {
+  for (const iterator of iterable) {
+    func(iterator);
+  }
+
+  return;
+};
+
+const map = (array, func) => {
+  const resultArray = [];
+
+  each(array, element => resultArray.push(func(element)));
+
+  return resultArray;
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
