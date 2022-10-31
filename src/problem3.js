848166ce5ee9ca4a11684186b350aba95b11f75d
areumsheep <areumsheep@gmail.com>
2022-11-01 01:19:58 +0900

    add: 문제3 답안 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
-function problem3(number) {
-  var answer;
-  return answer;
+const includes369 = (number) => {
+  const splitNumber = `${number}`.split('');
+  return splitNumber.filter((splited) => {
+    if (splited.includes(3) || splited.includes(6) || splited.includes(9))
+      return splited;
+  }).length;
+};
+
+function problem3(number = 0) {
+  let count = 0;
+  for (let i = 0; i <= number; i++) {
+    count += includes369(i);
+  }
+  return count;
 }
 
 module.exports = problem3;
