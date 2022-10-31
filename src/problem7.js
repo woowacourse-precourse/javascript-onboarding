e7b21bf5114ea1d8b5c5e699f0fe2532a0666c74
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 16:15:42 +0900

    feat : problem7, 모든 user의 친구리스트를 만드는 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,26 @@
+const makeFriendsList = (friends) => {
+    let obj = {};
+
+    for (let [x, y] of friends) {
+        if (!(x in obj)) {
+            obj[x] = [];
+        }
+        if (!(y in obj)) {
+            obj[y] = [];
+        }
+        obj[x].push(y);
+        obj[y].push(x);
+    }
+
+    return obj;
+};
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    let friendsList = makeFriendsList(friends);
+    const answer = [];
+
+    const compare = friendsList[user]; // user와 친구 사이
+    return answer;
 }
 
 module.exports = problem7;
