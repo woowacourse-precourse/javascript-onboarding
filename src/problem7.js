b6269e074f4aa9051e2f7760954463b6577b27ba
loopy-lim <loopy@jnu.ac.kr>
2022-10-31 15:23:30 +0900

    feat: user는 길이가 1 이상 30 이하인 문자열이다

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,24 @@
+const SnsFreindrRecomander = {
+  user: "",
+  freinds: [],
+  visitors: [],
+  inputDatas: function (user, friends, visitors) {
+    if (this.checkDatas(user, friends, visitors)) {
+      return false;
+    }
+    this.user = user;
+    this.friends = friends;
+    this.visitors = visitors;
+    return true;
+  },
+  checkDatas: function (user, friends, visitors) {
+    return this.checkUserLength(user);
+  },
+  checkUserLength: function (user) {
+    return user >= 1 && user <= 30;
+  },
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
