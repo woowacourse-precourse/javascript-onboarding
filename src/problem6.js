8860f1358a9d054f4275ccb8b84ce08bb7adce67
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-01 04:20:50 +0900

    Feat: <problem6> 마지막 중복 제거 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,26 @@
+function checkNickname(name, list) {
+
+}
+
+function checkEmail(mail, list) {
+  mail.pop();
+}
+
+function checkException(forms, list) {
+
+}
+
+function deleteDuplicate(list) {
+  let set = new Set(list);
+  let answer = Array.from(set);
+  return answer;
+}
+
 function problem6(forms) {
-  var answer;
+  let list = [];
+  checkException(forms, problems);
+
+  const answer = deleteDuplicate(list);
   return answer;
 }
 
