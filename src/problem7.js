c961db1ede8675d4a4c3019a87ffc653e50c97d1
Circlewee <pnj1118@naver.com>
2022-10-31 17:18:27 +0900

    [#7]feat: user와 친구 관계인 id 배열 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,17 @@
+/**
+ * @param {string} user
+ * @param {string[][]} friends
+ * @param {string[]} visitors
+ * @returns {string[]}
+ */
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const bestFriends = [];
+
+  friends.forEach((friend) => {
+    if (friend.find((id) => id === user)) {
+      bestFriends.push(user === friend[1] ? friend[0] : friend[1]);
+    }
+  });
 }
 
 module.exports = problem7;
