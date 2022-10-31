b795ba85321af73032af355ee4ff1a54b619da4e
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-01 00:53:58 +0900

    문제1 refactoring

--- src/problem1.js
+++ src/problem1.js
@@ -1,73 +1,32 @@
 function problem1(pobi, crong) {
-  var answer;
+  const pobiLeftPage = pobi[0];
+  const pobiRightPage = pobi[1];
+  const crongLeftPage = crong[0];
+  const crongRightPage = crong[1];
 
-  //예외사항
-  if(pobi[0] >= pobi[1] || crong[0] >= crong[1]){ //왼쪽 >= 오른쪽인 경우
-    answer = -1;
-    return answer;
-  }
-  else if(pobi[1]-pobi[0] != 1 || crong[1]-crong[0] != 1){ //페이지의 차이가 1이 아닌 경우
-    answer = -1;
-    return answer;
-  }
-  else if(pobi[0]==1 || pobi[0]==399 || crong[0]==1 || crong[0] == 399){ //시작 또는 마지막 면이 나온 경우
-    answer = -1;
-    return answer;
-  }
+  if(pobiLeftPage >= pobiRightPage || crongLeftPage >= crongRightPage) return -1;
+  if(pobiRightPage-pobiLeftPage != 1 || crongRightPage-crongLeftPage != 1) return -1;
+  if(pobiLeftPage==1 || pobiLeftPage==399 || crongLeftPage==1 || crongLeftPage == 399) return -1;
 
-  //pobi 점수 계산
-  let pobiSum = 0;
-  let pobiMulti = 0;
-  for (var page of pobi){
-    let tmpSum = 0;
-    let tmpMulti = 1;
-    const newArray = Array.from(String(page));
-    for (var i of newArray){
-      tmpSum += Number(i);
-      pobiMulti *= Number(i);
-    }
-    if(pobiSum < tmpSum){
-      pobiSum = tmpSum;
-    }
-    if(pobiMulti < tmpMulti){
-      pobiMulti = tmpMulti;
-    }
+  let pobiScore = getScore(pobiLeftPage, pobiRightPage);
+  let crongScore = getScore(crongLeftPage, crongRightPage);
+
+  if(pobiScore == crongScore) return 0;
+  else if(pobiScore > crongScore) return 1;
+  else return 2;
+}
+
+function getScore(leftPage, rightPage){
+  let sum = 0;
+  let mul = 1;
+
+  const eachDigitArray = Array.from(String(leftPage)+String(rightPage));
+  for (var eachDigit of eachDigitArray){
+    sum += Number(eachDigit);
+    mul *= Number(eachDigit);
   }
   
-  let pobiMax = (pobiSum > pobiMulti) ? pobiSum : pobiMulti;
-
-  //crong 점수 계산
-  let crongSum = 0;
-  let crongMulti = 0;
-  for (var page of crong){
-    let tmpSum = 0;
-    let tmpMulti = 1;
-    const newArray = Array.from(String(page));
-    for (var i of newArray){
-      tmpSum += Number(i);
-      crongMulti *= Number(i);
-    }
-    if(crongSum < tmpSum){
-      crongSum = tmpSum;
-    }
-    if(crongMulti < tmpMulti){
-      crongMulti = tmpMulti;
-    }
-  }
-
-  let crongMax = (crongSum > crongMulti) ? crongSum : crongMulti;
-
-  if(pobiMax == crongMax){
-    answer = 0
-  }
-  else if(pobiMax > crongMax){
-    answer = 1
-  }
-  else{
-    answer = 2
-  }
-
-  return answer;
+  return sum > mul ? sum : mul;
 }
 
 module.exports = problem1;
