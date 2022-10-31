d206ed60d19990e4fbbad88a90b429a8bb18a6a0
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-10-31 14:26:52 +0900

    problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let num = 1; num <= number; num++) {
+    num
+      .toString()
+      .split("")
+      .forEach((element) => {
+        if ((element == "3") | (element == "6") | (element == "9")) answer++;
+      });
+  }
+
   return answer;
 }
 
