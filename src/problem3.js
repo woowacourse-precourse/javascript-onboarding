d07e06b05b78858827a9c87ac639ce131c3afaa0
oxhwixo <hwi_030@naver.com>
2022-10-31 15:59:21 +0900

    Problem3: 입력 인자가 3,6,9 인지 확인하는 기능 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,14 @@
+function find369(number) {
+  if (number === "3" || number === "6" || number === "9") return true;
+  else return false;
+}
+
 function problem3(number) {
   var answer;
 
   // number를 문자열 배열로 변환하는 기능
   number = String(number).split("");
-  console.log(number);
+
   return answer;
 }
 
