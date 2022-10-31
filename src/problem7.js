745ee68abe9d7216d6c35b4e069bd2ec9b518b5f
동동이 <drdragon777@naver.com>
2022-11-01 07:30:12 +0900

    PROBLEM7 - getFriends function

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,29 @@
+const judgeFriend = (me, pair) => (pair[0] === me ? pair[1] : pair[0]);
+
+const getFriends = (me, friends) => {
+  return friends.reduce((acc, pair) => {
+    if (pair.includes(me)) return [...acc, judgeFriend(me, pair)];
+    return acc;
+  }, []);
+};
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const myFriends = getFriends(user, friends);
+  return 0;
 }
 
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 module.exports = problem7;
