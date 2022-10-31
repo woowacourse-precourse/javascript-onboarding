81196bd1a27bb4b6cbbda7eae79ab5498772906a
Siby1lA <psb4644@gmail.com>
2022-10-31 17:20:06 +0900

    feat: [문제 7] 추천 점수가 가장 높은 5명을 리턴하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -7,11 +7,28 @@ function getNewMembers(followFriends, list) {
   });
   return result;
 }
-const recommendScore = (list, score) => {
+function recommendScore(list, score) {
   let result = {};
   list.map((member) => (result[member] = (result[member] || 0) + score));
   return result;
-};
+}
+function recommentFriends(friendScore, visitorScore) {
+  let result = [];
+  const totalScore = Object.assign({}, friendScore, visitorScore);
+  let totalResult = Object.entries(totalScore)
+    .sort((a, b) => {
+      if (a[1] === b[1]) {
+        return a[0] < b[0] ? -1 : 1;
+      }
+      return b[1] - a[1];
+    })
+    .map((member) => member[0]);
+  for (i = 0; i < totalResult.length; i++) {
+    if (i === 5) break;
+    result.push(totalResult[i]);
+  }
+  return result;
+}
 function problem7(user, friends, visitors) {
   const followFriends = [
     ...new Set(friends.filter((list) => list.includes(user)).flat()),
@@ -20,6 +37,7 @@ function problem7(user, friends, visitors) {
   const newVisitors = getNewMembers(followFriends, visitors);
   const friendScore = recommendScore(newFriends, 10);
   const visitorScore = recommendScore(newVisitors, 1);
+  return recommentFriends(friendScore, visitorScore);
 }
 
 module.exports = problem7;
