16a2e0818310e3caa1810269024e5a34a09ec745
JeongSabibi <jsbb528@naver.com>
2022-11-01 02:25:19 +0900

    feat(PROBLEM2): 전체 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,24 @@
 function problem2(cryptogram) {
   var answer;
+  var check=true;
+  var str;
+  var list = cryptogram;
+
+  while(check) {
+    check = false;
+    str = [...list];
+    for (let i=0; list.length-1>i; i++) {
+      if (list[i] === list[i+1]) {
+        str.splice(i, 2);
+        list = [...str];
+        check = true;
+        continue;
+      }
+    }
+    list = [...str];
+  }
+  answer = list.toString().replace(/,/g, "");
+
   return answer;
 }
 
