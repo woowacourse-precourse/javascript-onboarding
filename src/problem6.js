4ed3c8eb77f27cc3b5c5f8cdfcffba8e07301e94
saram <yujun1018@naver.com>
2022-10-31 21:22:08 +0900

    [문제6] refactor: 매개변수 구조 분해 할당

--- src/problem6.js
+++ src/problem6.js
@@ -1,16 +1,11 @@
 function problem6(forms) {
   const answer = [];
 
-  forms.forEach((crew) => {
-    const nickname = crew[1];
-    const email = crew[0];
-
+  forms.forEach(([email, nickname]) => {
     for (let i = 0; i < nickname.length - 1; i++) {
       const twoLetters = nickname[i] + nickname[i + 1];
 
-      forms.forEach((crew2) => {
-        const nickname2 = crew2[1];
-        const email2 = crew2[0];
+      forms.forEach(([email2, nickname2]) => {
         if (nickname2.includes(twoLetters) && nickname !== nickname2)
           answer.push(email, email2);
       });
