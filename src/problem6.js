bbc1c4cd682494ee85c3fef353e5c3b334f15aa6
daal2quf <daal2quf@gmail.com>
2022-11-01 07:36:40 +0900

    중복단어찾기 와 이메일찾기 기능 나누기

--- src/problem6.js
+++ src/problem6.js
@@ -3,31 +3,37 @@ function problem6(forms) {
   const crewName = new Map()
   const formsMap = new Map(forms)
 
-  for (let e of forms) {
+  forms.forEach((e) => {
     const oneOfNames = e[1].split('')
-
+    console.log(oneOfNames)
     for (let i = 0; i < oneOfNames.length - 1; i++) {
       const twoWordOFName = oneOfNames[i] + oneOfNames[i + 1]
-      if(crewName.has(twoWordOFName)) {
+      if (crewName.has(twoWordOFName)) {
         crewName.set(twoWordOFName, crewName.get(twoWordOFName) + 1)
       } else {
         crewName.set(twoWordOFName, 1)
       }
     }
   }
-  
-  for (let key of crewName.keys()) {
-    if(crewName.get(key) > 1) {
-      for(let email of formsMap.keys()) {
-        if(formsMap.get(email).includes(key)) {
+  )
+
+  let asd = Array.from(crewName.keys())
+
+  asd.forEach((key) => {
+    if (crewName.get(key) > 1) {
+      for (let email of formsMap.keys()) {
+        if (formsMap.get(email).includes(key)) {
           answer.push(email)
         }
       }
     }
-  }
+  })
 
   answer.sort((a, b) => a.localeCompare(b))
+  console.log(answer)
   return answer;
 }
 
+
+problem6([["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]])
 module.exports = problem6;
