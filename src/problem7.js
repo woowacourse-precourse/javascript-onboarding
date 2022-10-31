c3c85507cfd2516b66557adab2051c0bacb3281e
MAGHC <windowsapple95@gmail.com>
2022-10-31 17:44:09 +0900

    feat: problem7 scoreObject 만드는 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,37 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+  const scoreObject = {};
+  const scoreListKeyArray = new Set();
+
+  for (let friend of friends.flat()) {
+    scoreListKeyArray.add(friend);
+  }
+
+  console.log(scoreListKeyArray);
+  console.log(createScoreObj(scoreListKeyArray, scoreObject));
   return answer;
 }
+
+const createScoreObj = (scoreKeySet, scoreObj) => {
+  for (let item of scoreKeySet) {
+    scoreObj[item] = 0;
+  }
+  return scoreObj;
+};
+
+console.log(
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  )
+);
 
 module.exports = problem7;
