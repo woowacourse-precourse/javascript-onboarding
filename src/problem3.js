21750025ecb53bf0436c5e349b60ce8aea1b6d6a
ming0830 <minsun0830@naver.com>
2022-10-31 16:34:32 +0900

    [PROBLEM3] feat : 1부터 number까지의 숫자를 문자열로 바꾸어 조건에 맞으면 카운팅하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let cnt = 0;
+  for(let i =1;i<=number;i++){
+    let strNum = String(i).split('');
+    for(let num of strNum){
+      if(num ==='3' || num === '6' || num === '9'){
+        cnt++
+      }
+    }
+
+  }
+  return cnt
 }
 
 module.exports = problem3;
