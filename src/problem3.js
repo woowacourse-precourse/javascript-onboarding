4fd861f936b6f83b40a33d348e13a989979c9baf
hhhminme <hororonoodle@gmail.com>
2022-11-01 00:39:04 +0900

    refactor : Clearly change the variable name used in question 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,12 +1,11 @@
 function problem3(number) {
   let answer = 0;
-  // 1부터 number 까지 1개씩 숫자를 말한다.
   for (let i = 1; i <= number; i++) {
     // 숫자에서 3,6,9 의 갯수를 센다. (33 일경우 2)
-    const filteredNumberArr = [...i.toString()].filter(
-      (val) => val === '3' || val === '6' || val === '9'
+    const filteredNumberArray = [...i.toString()].filter(
+      (digit) => digit === '3' || digit === '6' || digit === '9',
     );
-    answer += filteredNumberArr.length;
+    answer += filteredNumberArray.length;
   }
   return answer;
 }
