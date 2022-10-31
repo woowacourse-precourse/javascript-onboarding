9cd2f9f0931483ec00776f21b06c6fb81a5c8514
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-10-31 13:58:26 +0900

    feat: [문제 6] 중복이 될 수있는 두글자 목록을 Map 으로 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,24 @@
+const createConsecutiveLetterMap = (args) => {
+  const { forms } = args;
+  const consecutiveLetterMap = new Map();
+  forms.forEach((userData) => {
+    const userNickName = userData[1];
+    const userNicknameLength = userNickName.length;
+
+    for (let i = 0; i < userNicknameLength; i += 1) {
+      if (i === userNicknameLength - 1) break;
+
+      let consecutiveLetter = userNickName.substring(i, i + 2);
+      consecutiveLetterMap.set(
+        consecutiveLetter,
+        consecutiveLetterMap.get(consecutiveLetter) + 1 || 1
+      );
+    }
+  });
+
+  return { ...args, consecutiveLetterMap };
+};
+
 function problem6(forms) {
   var answer;
   return answer;
