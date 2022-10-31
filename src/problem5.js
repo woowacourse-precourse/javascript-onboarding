5ca889f04529878e4ad7d2e1ecd3258ee2e4333f
ekkim <eklikeable@kakao.com>
2022-10-31 21:56:36 +0900

    🎨 남은금액 계산식 수정, 변수명 변경

--- src/problem5.js
+++ src/problem5.js
@@ -2,9 +2,9 @@ function problem5(money) {
   const currencies = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let result = [];
   for (let currency of currencies) {
-    let numberOfTheCurrency = Math.floor(money / currency);
-    result.push(numberOfTheCurrency);
-    money = money - currency * numberOfTheCurrency;
+    let numberOfTheBills = Math.floor(money / currency);
+    result.push(numberOfTheBills);
+    money = money % currency;
   }
   return result;
 }
