a153d30f0a6a93dad1212f47c3d6741d2a4cb7b2
ShimSungBo <shimdb96@gmail.com>
2022-10-31 13:34:11 +0900

    Fix [problem7] 기능목록 전면 수정

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,58 @@
+
+function findUserFriends(user, friends) {
+  return friends.filter(el => el.includes(user)).flat().filter(el => el !== user);
+}
+
+function relationFriends(user, friends) {
+  const relations = [];
+  for (const relation of friends) {
+      if(!relation.includes(user)) {
+          relations.push(relation);
+      }
+  }
+    return relations.flat()
+}
+
+function checkVisitorScore(visitors) {
+  const map = new Map();
+  for (let visitor of visitors) {
+    if (!map.has(visitor)) {
+      map.set(visitor, 1);
+    } else if (map.get(visitor) !== 0) {
+      map.set(visitor, map.get(visitor) + 1);
+    }
+  }
+  return map;
+}
+
+
+function friendsScore(user, friends, visitor) {
+  const friendsOfUser = findUserFriends(user, friends);
+  const relation = relationFriends(user, friends);
+  const visitorScore = checkVisitorScore(visitor);
+  const score = new Map(visitorScore);
+
+  for (const friend of relation) {
+      if(friend !== friendsOfUser[0] && friend !== friendsOfUser[1]) {
+        if(!score.has(friend)) {
+          score.set(friend, 10);
+        } else {
+          score.set(friend, score.get(friend) + 10);
+        }
+      }
+  }
+    return score;
+}
+
+
+
+
+
+
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  
 }
 
 module.exports = problem7;
+
