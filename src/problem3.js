535288e2ddd0659e25eafa3b38e8fead4ceac186
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-31 10:33:53 +0900

    p3: 0부터 number까지 각 자리수 순회

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
   let answer = 0;
-  const rule = "369";
+  const RULE = "369";
+
+  let counter = 0;
+  while (counter <= number) {
+    String(counter).split("").forEach(x => {
+
+    })
+  }
 }
 
 module.exports = problem3;
