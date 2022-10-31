4e87ccbd11e5c3b480c7521d74203aee87d02684
nalsae <nalsae0207@naver.com>
2022-10-31 20:28:11 +0900

    feat: Add an array of results about merging two letters each
    - 분할한 문자를 두 글자씩 병합한 결과를 담은 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,16 @@
 const problem6 = (forms) => {
   let answer = [];
+  let namePieces = [];
 
   const nameList = forms.map((form) => form[1].split(""));
+
+  // prettier-ignore
+  nameList.forEach((name) =>
+    (namePieces = [...namePieces, name.map((_, i) => {
+      if (i !== name.length - 1) return name[i] + name[i + 1];
+    }).filter((namePiece) =>
+      namePiece)])
+  );
 
   return answer;
 };
