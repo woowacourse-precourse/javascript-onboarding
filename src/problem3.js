b09fe641d1c7caefb69ad4449581febb5ee0dcb8
woong <inwoong100@gmail.com>
2022-10-31 22:39:50 +0900

    refactor: problem3 코드 리팩토링
    1. 분리가능한 부분 함수로 분리
    2. 기능에 맞는 함수 명 지정
    3. 역할에 맞는 변수명 지정

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,30 @@
-function problem3(number) {
-  let answer = 0;
-  const checkNumber = ['3', '6', '9'];
-  for (let i = 1; i <= number; i++) {
-    const iArray = i.toString().split('');
-    for (const el of iArray) {
-      if (checkNumber.includes(el)) answer++;
-    }
+const getIncludedNumber = (number, checkNumbers) => {
+  let results = 0;
+
+  const numberArray = number.toString().split('');
+
+  for (const element of numberArray) {
+    if (checkNumbers.includes(element)) results++;
   }
-  return answer;
+
+  return results;
+};
+
+function problem3(number) {
+  //예외사항1
+  if (number < 1 || number > 10000) return 'ERROR';
+  //예외사항2
+  if (number % 1) return 'ERROR';
+
+  let results = 0;
+
+  const checkNumbers = ['3', '6', '9'];
+
+  for (let i = 1; i <= number; i++) {
+    results += getIncludedNumber(i, checkNumbers);
+  }
+
+  return results;
 }
 
 module.exports = problem3;
