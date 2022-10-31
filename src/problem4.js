ab52f10cc74ab858287406416ae84f01720d5a57
KangYunJi <dbswl701@ajou.ac.kr>
2022-10-31 23:59:26 +0900

    feat:[문제4] 아스키코드를 이용해 대문자, 소문자 배열 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,11 @@
 function problem4(word) {
-  var answer;
+  let answer=[];
+  let lower=[];
+  let upper=[];
+
+  for(let i = 65; i<=90; i++){ upper.push(String.fromCharCode(i)); }
+  for(let i = 97; i<=122; i++){ lower.push(String.fromCharCode(i)); }
+
   return answer;
 }
 
