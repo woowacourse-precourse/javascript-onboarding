d5c119d2707a8ffc49052e082f5ee8e76a06a235
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 14:54:18 +0900

    feat: Problem 1 - 점수 계산 기능 구현하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,22 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+const getMaxScoreUsingPage = page => {
+  const pageDigit = page.toString().split('').map(x => parseInt(x));
+  const sumOfDigit = pageDigit.reduce((x, y) => x + y)
+  const multiplyOfDigit = pageDigit.reduce((x, y) => x * y);
+
+  const maxScore = Math.max(sumOfDigit, multiplyOfDigit);
+  return maxScore;
+}
+
+const problem1 = (pobi, crong) => {
+  const pobiScore = Math.max(getMaxScoreUsingPage(pobi[0]), getMaxScoreUsingPage(pobi[1]));
+  const crongScore = Math.max(getMaxScoreUsingPage(crong[0]), getMaxScoreUsingPage(crong[1]));
+
+  if (pobiScore > crongScore)
+    return 1;
+  else if (pobiScore < crongScore)
+    return 2;
+  else
+    return 0;
 }
 
 module.exports = problem1;
