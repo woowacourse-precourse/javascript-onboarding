4380149e80c60b7e6f19b6d491cbaef5366c4d21
Y2IN <dpdls0228@daum.net>
2022-11-01 02:51:37 +0900

    feat : solve problem

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,32 @@
+function changeSmall(alpha) {
+  const alphaAscii = alpha.charCodeAt(0);
+  const changeChar = 219 - alphaAscii;
+  String.fromCharCode(changeChar);
+  return String.fromCharCode(changeChar);
+}
+
+function changeBig(alpha) {
+  const alphaAscii = alpha.charCodeAt(0);
+  const changeChar = 155 - alphaAscii;
+  String.fromCharCode(changeChar);
+  return String.fromCharCode(changeChar);
+}
+
+function checkAlpha(alpha) {
+  if (alpha >= "a" && alpha <= "z") {
+    return changeSmall(alpha);
+  } else if (alpha >= "A" && alpha <= "Z") {
+    return (letter = changeBig(alpha));
+  }
+  return alpha;
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  let str = [];
+  for (let i = 0; i < word.length; i++) {
+    str += checkAlpha(word[i]);
+  }
+  return str;
 }
 
 module.exports = problem4;
