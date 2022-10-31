802acb5fdab2b8e18516ce20ddad53f85725e5a2
sulmeee <poorooria@naver.com>
2022-10-31 20:36:49 +0900

    [problem5] feat: 반복문을 통한 돈 카운트

--- src/problem5.js
+++ src/problem5.js
@@ -4,6 +4,15 @@ function problem5(money) {
 
   // 돈을 카운트해줄 배열 선언
   let moneyCount = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+
+  // 반복문을 순회하면서 돈 카운트
+  for (let i = 0; i < moneyCount.length; i++) {
+    answer.push(Math.floor(money / moneyCount[i]));
+    money = money % moneyCount[i];
+  }
+
+  return answer;
 }
 
 module.exports = problem5;
