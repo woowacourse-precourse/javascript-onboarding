745d87c17101f939792768a78c23d6f38cb9d3bb
froggy1014 <clcl6084@gmail.com>
2022-10-31 19:57:37 +0900

    style[7]: 함수에 대한 JS Doc 작성

--- src/problem7.js
+++ src/problem7.js
@@ -22,19 +22,39 @@ function problem7(user, friends, visitors) {
     else if (!userFriends.includes(name)) rank.set(name, (rank.get(name)||0)+1);  
   }   
 
+  /* Map 객체인 rank를 배열로 바꿔주고, sort를 통해 내가 원하는 순서대로 정렬해준다음에 
+     .flat.filter() 메소드를 통해서 점수를 제거한 뒤 반환합니다. */
   return [...rank].sort(sortAlgorithm).flat().filter((v) => isNaN(v));
 }
 
+/**
+ * @description user와 친구관계인 이름만 반환합니다.
+ * @param {string} user 
+ * @param {string[][]} relationship 
+ * @returns {string[][]} user's relationship
+ */
 function getUserFreinds(user, relationship) {
   return relationship.filter((rel) => rel.includes(user))
                      .flat()
                      .filter(name => name !== user);
 }
 
+/**
+ * @description user와 연결되지않은 인간관계를 반홥합니다. 문자열 2차원배열로 반환합니다.
+ * @param {string} user 
+ * @param {string[][]} relationship 
+ * @returns {string[][]} others relationship
+ */
 function getOthersFriends(user, relationship) {
   return relationship.filter((v) => !(v.includes(user)));
 } 
 
+/**
+ * @description 1.점수 2.알파벳 기준으로 정렬해주는 함수
+ * @param {[string, number]} a
+ * @param {[string, number]} b
+ * @returns {number} priorty
+ */
 function sortAlgorithm(a, b) {
   if ( a[1] < b[1] ) return 1;
   if ( a[1] > b[1] ) return -1;
