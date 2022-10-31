1769c728061dd94e1f53814dc38e5d409891add2
SeongHo-C <leeseong010@naver.com>
2022-10-31 19:48:23 +0900

    comment(problem7): 기능 목록 주석 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,14 @@
+// 사용자와 함께 아는 친구 반환 기능
+
+// 사용자의 친구 반환 기능
+
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+
+  // 사용자와 함께 아는 친구에게 추천 점수 부여 기능
+
+  // 사용자의 타임 라인에 방문한 사용자와 함께 아는 친구에게 추천 점수 부여 기능
+
   return answer;
 }
 
