ac57cbe46d361a5bb0006a54875130bc3d8d5788
Monte <monte6198@gmail.com>
2022-11-01 05:06:06 +0900

    feat(problem7.js): getVisitorScore() 및 방문자 점수 연산 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -16,6 +16,20 @@ function getTheirFriend(pair, user, userFriends, theirFriends) {
   }
 }
 
+function getVisitorScore(visitor, userFriends, scores) {
+  if (!userFriends.includes(visitor)) {
+    // user의 친구가 아닌 방문자이고
+    if (!scores.has(visitor)) {
+      // scores에 해당 방문자명(visitor)이 존재하지 않는다면
+      scores.set(visitor, 1); // scores에 방문자명을 추가하고 추천 점수 1로 시작한다.
+    } else if (scores.has(visitor)) {
+      // scores에 해당 방문자 데이터가 존재한다면
+      let score = scores.get(visitor); // 그 방문자의 추천 점수를
+      scores.set(visitor, score + 1); // 1만큼 증가시키도 저장한다.
+    }
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   answer = [];
@@ -37,6 +51,11 @@ function problem7(user, friends, visitors) {
   theirFriends.forEach((friend) => {
     scores.set(friend, 10);
   });
+
+  for (let visitor of visitors) {
+    // 방문자 목록에 대해 반복문을 수행하며
+    getVisitorScore(visitor, userFriends, scores); // user의 친구가 아닌 방문자에 대해 추천 점수 부여
+  }
 
   return answer;
 }
