2a0f379c95a68c4128c8cae11234feffb97e37cb
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 22:05:23 +0900

    [Feat:Problem6] 배열에서 특정 값(target: email | nickname) 가져오기

--- src/problem6.js
+++ src/problem6.js
@@ -2,3 +2,10 @@ const compose =
   (...fns) =>
     (initialValue) =>
       fns.reduce((composed, fn) => fn(composed), initialValue);
+
+const getValuesOf = (target) => (forms) =>
+  forms.map(([email, nickname]) => {
+    if (target === "email") return email;
+    else if (target === "nickname") return nickname;
+    else throw new Error("해당 데이터는 존재하지 않습니다.");
+  });
