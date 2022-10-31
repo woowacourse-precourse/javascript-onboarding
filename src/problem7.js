4fc955e617f7c2d9b9745a4e517a657c0ace6385
Buyoung Chung <sunarol@naver.com>
2022-10-31 18:32:06 +0900

    [문제7] 친구 리스트 작성

--- src/problem7.js
+++ src/problem7.js
@@ -6,12 +6,19 @@ function initializing(recommendScore, friendsList, friends) {
     }
   }
 }
+function makeFriendsList(friendsList, friends) {
+  for (let friend of friends) {
+    friendsList.get(friend[0]).push(friend[1]);
+    friendsList.get(friend[1]).push(friend[0]);
+  }
+}
 function problem7(user, friends, visitors) {
   var answer = [];
 
   let recommendScore = new Map();
   let friendsList = new Map();
   initializing(recommendScore, friendsList, friends);
+  makeFriendsList(friendsList, friends);
   return answer;
 }
 
