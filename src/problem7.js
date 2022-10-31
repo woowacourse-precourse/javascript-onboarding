5da5cbd46398b770f6d20439defadcef18a4424c
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-01 01:53:38 +0900

    ✨ 유저의 친구목록을 반환하는 함수 생성

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,16 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+// 친구관계를 담은 2차원배열과, 유저닉네임을 넘기면 유저의 친구목록을 배열로 반환
+function findUsersFriend(friends, user) {
+  const result = [];
+  friends.forEach((relation) => {
+    const IDX_USER = relation.indexOf(user);
+    if (IDX_USER !== -1) {
+      result.push(relation[1 - IDX_USER]);
+    }
+  });
+  return result;
+}
+
 module.exports = problem7;
