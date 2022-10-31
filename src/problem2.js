a69f31e2b5dcabc3881b5229d2cd1dd8b99a1cc0
leesoyuun <leesoyun4206@naver.com>
2022-10-31 23:27:31 +0900

    [Problem 2] 연속되는 중복 문자 제거하여 출력

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let arr = cryptogram.split("");
+  let answer = [];
+
+  for(let str of arr){
+      if(answer[answer.length - 1] === str){
+          answer.pop();
+      } else {
+          answer.push(str);
+      }
+  }
+
+  return answer.join("");
 }
 
 module.exports = problem2;
