0c9471dafc3ae0da1ef7a4fdc9722bfdbf1c19c9
imseoyoung <lsy783@sookmyung.ac.kr>
2022-11-01 00:35:51 +0900

    feat: problem3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  
+  let number_arr = [];
+
+  for (i=0; i<number+1; i++) {
+    number_arr.push(i);
+  }
+
+return [...number_arr.toString().matchAll(/[3|6|9]/g)].length;
+
 }
+
 
 module.exports = problem3;
