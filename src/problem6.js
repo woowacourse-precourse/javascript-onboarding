1e2d761d8388ab0081efdc91edea0f0408859b77
Narcoker <jykim9868@gmail.com>
2022-10-31 19:09:11 +0900

    fix: console.log() 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,8 @@
-function problem6(forms) {  
+function problem6(forms) {
   function verify(nickName, index) {
     for (let k = 0; k < nickName.length - 1; k++) {
       let str = nickName.slice(k, k + 2);
-      
+
       for (let targetIndex = index + 1; targetIndex < nickNames.length; targetIndex++) {
         if (nickNames[targetIndex].includes(str)) {
           answer.add(forms.get(nickName));
@@ -12,7 +12,7 @@ function problem6(forms) {
       }
     }
   }
-  
+
   forms = forms.reduce((res, v) => {
     res.set(v[1], v[0]);
     return res;
@@ -22,7 +22,7 @@ function problem6(forms) {
 
   for (let i = 0; i < nickNames.length; i++)
     verify(nickNames[i], i);
-  
+
   return [...answer].sort((a, b) => {
     if (a > b) return 1;
     else if (a < b) return -1;
@@ -30,5 +30,4 @@ function problem6(forms) {
   });
 }
 
-// console.log(problem6([["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]]))
 module.exports = problem6;
