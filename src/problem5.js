b3f4416551ca7ea3690a84b6ffaf3b0900b13c02
YMS <yms1789@naver.com>
2022-10-31 19:07:37 +0900

    new: problem5 미션 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let list = Array(cash.length).fill(0);
+  for (let i = 0; i < cash.length; i++) {
+    if (money >= cash[i]) {
+      while (money >= cash[i]) {
+        money -= cash[i];
+        list[i]++;
+      }
+    }
+  }
+  return list;
 }
 
 module.exports = problem5;
