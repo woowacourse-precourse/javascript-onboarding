0f112db16cc09c503ba4ae176b530b798a16a642
spsp8426 <spsp8426@naver.com>
2022-10-31 15:08:05 +0900

    3번 예외처리

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,6 @@
 function problem3(number) {
   let answer = 0;
-
+  if (number < 1 || number > 10000) return false;
   for (let i = 1; i <= number; i++) {
     let results = i.toString().match(/[3,6,9]/g);
     if (results)
