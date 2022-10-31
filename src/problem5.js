4e29d750f3d11d7eb28ea6d8e7441edd213a13e3
ming0830 <minsun0830@naver.com>
2022-11-01 01:33:48 +0900

    [PROBLEM5] feat : moeny가 0이 될때까지 앞선 과정을 반복하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,7 @@
 function problem5(money) {
   const kindOfMoney = [50000,10000,5000,1000,500,100,50,10,1]
   let result = [];
-  
+  while(money!=0){
     for(let i =0;i<kindOfMoney.length;i++){
       let quo = Math.floor(money/kindOfMoney[i]);
       money = money % kindOfMoney[i]
@@ -9,7 +9,7 @@ function problem5(money) {
       
     }
 
-
+  }
   return result
 } 
 
