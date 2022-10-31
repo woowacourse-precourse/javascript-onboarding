dfececc2b0f34083b1daa57b950f30d46030bd26
soomin <qkrtnals970804@gmail.com>
2022-11-01 04:36:07 +0900

    [problem4] 대문자 판별 후 반대되는 알파벳 추가

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,11 @@ const checkEng = (w, isEng) => {
   return isEng.test(w);
 }
 
+//대문자인지 판별하는 함수
+const checkBig = (w) => {
+  return w === w.toUpperCase();
+}
+
 function problem4(word) {
   let answer = "";
   const isEng = /[a-zA-Z]/;
@@ -16,8 +21,15 @@ function problem4(word) {
   ];
   for (let w of word) {
     //영어인지 판별하기
-    if (!checkEng(w)) {
-      answer += w
+    if (!checkEng(w, isEng)) {
+      answer += w;
+    } else {
+      //대문자인지 소문자인지에 따라서 변경하여 answer에 붙이기
+      if (checkBig(w)) {
+        answer += alphabet[25 - alphabet.indexOf(w)];
+      } else {
+        answer += alphabet[25 - alphabet.indexOf(w.toUpperCase())].toLowerCase();
+      }
     }
   }
   return answer;
