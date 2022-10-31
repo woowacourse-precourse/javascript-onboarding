f97250ea4d85646f96f60a1a73dce41b8e3d98e5
daal2quf <daal2quf@gmail.com>
2022-11-01 02:01:54 +0900

    숫자를 배열로만들어 비교하지않고 숫자자체를 비교할수 있게

--- src/problem1.js
+++ src/problem1.js
@@ -9,31 +9,31 @@ function problem1(pobi, crong) {
 
   if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return -1
   if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return -1
+   
 
-const bigNum = (arr) => {
-  let big;
-  let newArr = [];
-  for(let i = 0; i < arr.length; i++) {
-    const changeNum = (arg) => Number(arg)
-    let num = String(arr[i]).split('').map(changeNum).reduce((prev, curv) => prev + curv)
-    newArr.push(num)
-    num = String(arr[i]).split('').map(changeNum).reduce((prev, curv) => prev * curv)
-    newArr.push(num)
-    num;
+const bigNum = (number) => {
+  let sum = 0;
+  let multiply = 1;
+  while(number > 1) {
+    sum += Math.floor(number % 10)
+    multiply *= Math.floor(number % 10)
+    number /= 10
   }
-  newArr.sort((a, b) => b - a)
-  big = newArr[0]
-  return big
+  return Math.max(sum, multiply)
 }
 
-if(bigNum(pobi) > bigNum(crong)) {
-  return 1
-} else if (bigNum(pobi) < bigNum(crong)) {
-  return 2
-} else if (bigNum(pobi) === bigNum(crong)) {
-  return 0
-}
+const pobiBigNum = Math.max(bigNum(pobi[0]), bigNum(pobi[1]))
+const crongBigNum = Math.max(bigNum(crong[0]), bigNum(crong[1]))
+console.log(pobiBigNum, crongBigNum)
 
+if(pobiBigNum > crongBigNum) {
+  answer = 1
+} else if (pobiBigNum < crongBigNum) {
+  answer = 2
+} else if (pobiBigNum === crongBigNum) {
+  answer = 0
+}
+console.log(answer)
 return answer;
 }
 
