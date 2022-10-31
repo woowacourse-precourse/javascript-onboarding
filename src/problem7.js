37f2f603a750db53457d230a8e524e1081c3f300
Quartz <skyu.dev@gmail.com>
2022-10-31 14:13:43 +0900

    refactor: create functions in problem7

--- src/problem7.js
+++ src/problem7.js
@@ -1,40 +1,53 @@
+function separateFriends(user, friend, friendNames, notFriends) {
+  if (friend[0] === user) {
+    friendNames[friend[1]] = friend[1]; // user의 친구
+  } else if (friend[1] === user) {
+    friendNames[friend[0]] = friend[0]; // user의 친구
+  } else {
+    notFriends.push(friend);
+  }
+}
+
+function isIndirectFriend(friendNames, f, scores) {
+  if (friendNames[f[0]]) {
+    if (scores[f[1]]) scores[f[1]] = scores[f[1]] + 10;
+    else scores[f[1]] = 10;
+  } else if (friendNames[f[1]]) {
+    if (scores[f[0]]) scores[f[0]] = scores[f[0]] + 10;
+    else scores[f[0]] = 10;
+  }
+}
+
+function scoreVisitor(scores, visitor) {
+  if (scores[visitor]) {
+    scores[visitor]++;
+  } else {
+    scores[visitor] = 1;
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer = [];
-  const friendNames = {};
-  const indirectFriends = [];
+  const friendNames = {}; // user의 친구
+  const notFriends = [];
   const scores = {};
-  friends.forEach((friend, i) => {
-    if (friend[0] === user) {
-      friendNames[friend[1]] = friend[1];
-    } else if (friend[1] === user) {
-      friendNames[friend[0]] = friend[0];
-    } else {
-      indirectFriends.push(friend);
-    }
+  friends.forEach((friend) => {
+    separateFriends(user, friend, friendNames, notFriends);
   });
-  indirectFriends.forEach((f) => {
-    if (friendNames[f[0]]) {
-      if (scores[f[1]]) scores[f[1]] = scores[f[1]] + 10;
-      else scores[f[1]] = 10;
-    } else if (friendNames[f[1]]) {
-      if (scores[f[0]]) scores[f[0]] = scores[f[0]] + 10;
-      else scores[f[0]] = 10;
-    }
+  notFriends.forEach((f) => {
+    isIndirectFriend(friendNames, f, scores);
   });
   visitors.forEach((visitor) => {
     if (!friendNames[visitor]) {
-      if (scores[visitor]) {
-        scores[visitor]++;
-      } else {
-        scores[visitor] = 1;
-      }
+      scoreVisitor(scores, visitor);
     }
   });
+
   const sortedScores = Object.fromEntries(
-    Object.entries(scores).sort(([, a], [, b]) => b - a)
+    Object.entries(scores).sort(([, a], [, b]) => b - a) // 내림차순 정렬
   );
   for (const score in sortedScores) {
-    if (answer.length < 5) answer.push(score);
+    if (answer.length < 5) answer.push(score); // 최대 5 명 출력
     else return;
   }
 
