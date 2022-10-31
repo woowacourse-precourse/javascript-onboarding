ebb31554ad27eec8e9d1b5dbc34240186832026f
pangkyu <baesense@naver.com>
2022-10-31 17:17:04 +0900

    feat : visitor & count기능
    visitor명단과 횟수를 카운트하여 리턴하는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,20 +1,42 @@
 function problem7(user, friends, visitors) {
-  getFriendsAdjacencyList(friends);
   const userFriend = getFriendUser(user, friends);
+  const uni = getVisitorUser(user, visitors);
 
+  getGrade(userFriend);
   // console.log(userFriend);
+}
+
+function getGrade(userFriend) {}
+
+function getVisitorUser(user, visitors) {
+  const unique = visitors.filter((element, index) => {
+    return visitors.indexOf(element) === index;
+  });
+  let uniqueGrade = [];
+  for (let i = 0; i < unique.length; i++) {
+    const countVisitor = visitors.reduce(
+      (count, data) => (data === unique[i] ? count + 1 : count),
+      0
+    );
+    uniqueGrade[i] = countVisitor;
+    // console.log(countVisitor);
+  }
+
+  // console.log(unique);
+  // console.log(uniqueGrade);
+  return [unique, uniqueGrade];
 }
 
 function getFriendUser(user, friends) {
   const friend = [];
+
   for (let i = 0; i < friends.length; i++) {
     const includeUser = friends[i].includes(user);
     if (includeUser) {
-      const temp = String(friends[i]).replace(user, "");
-      friend.push(String(temp).replace(",", ""));
+      const temp = String(friends[i]).replace(",", "");
+      friend.push(String(temp).replace(user, ""));
     }
   }
-  console.log(friend);
 
   return friend;
 }
