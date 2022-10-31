ddfa87af7723a6ae10e5517e00d41105833276fa
yb8350 <yb8350@naver.com>
2022-11-01 00:46:43 +0900

    Feat: problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let mnyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < 9; i++) {
+    answer[i] += parseInt(money / mnyArr[i]);
+    money = money % mnyArr[i];
+  }
   return answer;
 }
 
