fbcdf5ab68b726d56b67b60029cb71d586506145
Sangmin Lee <gink.ss00@gmail.com>
2022-10-31 19:35:40 +0900

    [problem2] feat: decoder 함수 구현
    암호문을 넣으면 주어진 조건에 따라 해독하여 반환해준다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,31 @@
+function decoder(decodeCryptogram) {
+  let decodeHelper = "";
+  let duplicatePoint = false;
+  let reDecodeCheck = false;
+
+  do {
+    if (decodeCryptogram.length === 1) return decodeCryptogram;
+    let i;
+    duplicatePoint = false;
+    reDecodeCheck = false;
+
+    for (i = 0; i < decodeCryptogram.length - 1; i++) {
+      if (decodeCryptogram[i] === decodeCryptogram[i + 1]) {
+        duplicatePoint = true;
+        reDecodeCheck = true;
+      } else if (duplicatePoint === true) {
+        duplicatePoint = false;
+      } else decodeHelper = decodeHelper.concat(decodeCryptogram[i]);
+    }
+    if (decodeCryptogram[i] !== decodeCryptogram[i - 1]) {
+      decodeHelper = decodeHelper.concat(decodeCryptogram[i]);
+    }
+    decodeCryptogram = decodeHelper;
+    decodeHelper = "";
+  } while (reDecodeCheck === true && decodeCryptogram.length > 1);
+  return decodeCryptogram;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
