62711dee594d391d65acb19484bc6e0087bdb103
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:57:32 +0900

    [ feat ] problem7_함께 아는 친구의 수 판별 & 점수부여

--- src/problem7.js
+++ src/problem7.js
@@ -2,8 +2,8 @@
 // - 서로 친구인 사용자 객체를 만드는 함수 생성
 // - 친구 관계 객체 생성
 // - user의 친구를 담은 Set 생성
-// - 함께 아는 친구의 수 판별
-// - 타임라인에 방문한 횟수 count
+// - 함께 아는 친구의 수 판별 & 점수부여
+// - 타임라인에 방문한 횟수 count & 점수부여
 // - 점수가 0을 초과하는 사용자를 담은 배열 생성 & 내림차순 정렬
 // - 최대 5명의 친구 추천
 
@@ -25,6 +25,15 @@ function problem7(user, friends, visitors) {
   });
 
   const userFriendsSet = new Set(mateObj[user][0]);
+
+  for (const key in mateObj) {
+    mateObj[key][0].forEach((element) => {
+      if (key === user) return;
+      if (userFriendsSet.has(element)) {
+        mateObj[key][1] += 10;
+      }
+    });
+  }
 
   return answer;
 }
