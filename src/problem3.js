70a151223b5b327de852ce9a977f633cbd2e1361
Buyoung Chung <sunarol@naver.com>
2022-10-31 17:45:21 +0900

    [문제3] 문자열 순회하면서 3 ,6, 9 갯수 확인

--- src/problem3.js
+++ src/problem3.js
@@ -3,6 +3,11 @@ function problem3(number) {
   var answer = 0;
   for (let i = 3; i < number + 1; i++) {
     let strNum = String(i);
+    for (let x of strNum) {
+      if (x === "3" || x === "6" || x === "9") {
+        answer++;
+      }
+    }
   }
   return answer;
 }
