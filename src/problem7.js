b22f012efe1fdb20601ebcc7fde65d4c94901e28
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 19:59:28 +0900

    feat: 해당 사람이 user의 friend 인지 확인하는 기능(isFriendWithUser)

--- src/problem7.js
+++ src/problem7.js
@@ -2,7 +2,7 @@
   구현할 기능 목록
   [O] friends 배열에서 주어지는 사람들의 친구 리스트를 Map 객체(key: 이름, value: friends 배열)로 생성하는 기능 | 함수명 : initFriendsList
   [O] frineds 배열어서 주이지는 사람들의 score 리스트를 Map 객체(key: 이름, value: score)로 생성하는 기능 | 함수명 : initScoreList
-  [ ] 해당 사람이 user의 friend인지 확인하는 기능
+  [O] 해당 사람이 user의 friend인지 확인하는 기능 | 함수명 : isFriendWithUser
   [ ] user와 함께 아는 친구인 경우가 있는 사람인 경우, 아는 친구 한명 당 score를 10씩 증가시키는 기능
   [ ] user의 타임라인에 방문한 사람인 경우, 방문 횟수 당 score를 1씩 증가시키는 기능
   [ ] Map객체를 배열로 convert하는 기능
@@ -24,6 +24,8 @@ function problem7(user, friends, visitors) {
     initScoreList(scoreList, person1, user);
     initScoreList(scoreList, person2, user);
   });
+
+  let userFriendsList = friendsList.get(user);
 }
 
 function initFriendsList(friendsList, person1, person2) {
@@ -38,6 +40,10 @@ function initScoreList(scoreList, person, user) {
   if (user !== person && !scoreList.has(person)) {
     scoreList.set(person, 0);
   }
+}
+
+function isFriendWithUser(userFriendsList, person) {
+  return userFriendsList.includes(person);
 }
 
 module.exports = problem7;
