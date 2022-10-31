2a14e3d0b6a5e1543b9749f1f726fe7a1e9c9bcd
이여진 <jinspace3710@gmail.com>
2022-10-31 21:55:25 +0900

    feat: problem4 기능 1 - 아스키 코드를 활용한 문자 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
 function problem4(word) {
-  var answer;
+  let answer;
+
+  if (typeof word === 'string') {
+    let wordArray = [...word];
+
+    for (i = 0; i < wordArray.length; i++) {
+      if (wordArray[i] !== ' ') {
+        let asciiCode = wordArray[i].charCodeAt(0);
+
+        if (asciiCode >= 65 && asciiCode <= 90)
+          wordArray[i] = String.fromCharCode(155 - asciiCode);
+        else wordArray[i] = String.fromCharCode(219 - asciiCode);
+      }
+    }
+    answer = wordArray.join('');
+  }
+
   return answer;
 }
 
