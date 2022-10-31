63d1ee9cd429427409fae744c6e928b25daf46a7
ashleysyheo <ashleysyheo@gmail.com>
2022-10-31 15:55:35 +0900

    feat(problem7): calculate friends score

--- src/problem7.js
+++ src/problem7.js
@@ -9,6 +9,22 @@ function getFriends(user, friends) {
   return friendsSet;
 }
 
+function calcFriendScore(friends, userFriends) {
+  const recommendationScore = {};
+
+  for (let i = 0; i < friends.length; i += 1) {
+    if (userFriends.has(friends[i][0]) && !userFriends.has(friends[i][1])) {
+      recommendationScore[friends[i][1]] = recommendationScore[friends[i][1]] + 10 || 10;
+    }
+
+    if (userFriends.has(friends[i][1]) && !userFriends.has(friends[i][0])) {
+      recommendationScore[friends[i][0]] = recommendationScore[friends[i][0]] + 10 || 10;
+    }
+  }
+
+  return recommendationScore;
+}
+
 function problem7(user, friends, visitors) {
   let recommendationResult; 
 
