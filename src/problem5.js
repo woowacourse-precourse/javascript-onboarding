b20c267de1e252f1204d552b50021d01fdbab3c3
energy1-914 <dlawldnjs5@daum.net>
2022-11-01 01:29:02 +0900

    feat:문제 5번 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+  for (i=0;i<9;i++){
+    answer.push(parseInt(money/unit[i]));
+    money=money%unit[i];
+  }
   return answer;
 }
 
