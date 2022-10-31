35aedd81f7a629ec84f8495b0a9a521a789eb0c1
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 03:51:55 +0900

    FEAT : problem1 - 페이지 번호 중 가장 큰 수를 구하여 자신의 점수로 설정하는 기능

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,26 @@
 function problem1(pobi, crong) {
-  if(!isValidPageNumber(pobi) || !isValidPageNumber(crong)) {
-    return -1;
+  // 점수 선언
+  const SCORE = {
+    POBI_WIN: 1,
+    CRONG_WIN: 2,
+    TIE: 0,
+    ERROR: -1
   }
 
-  
+  // 유효성 검사 후 예외사항은 -1로 return
+  if(!isValidPageNumber(pobi) || !isValidPageNumber(crong)) {
+    return SCORE.ERROR;
+  }
+
+  pobi = pobi.map((el) => getMaxNumberPlusOrMutiplication(String(el)))
+  crong = crong.map((el) => getMaxNumberPlusOrMutiplication(String(el)))
+}
+
+// 번호의 각 자리 숫자를 모두 더하거나 곱해서 가장 큰 수를 구한다.
+const getMaxNumberPlusOrMutiplication = (numberStr) => {
+  const plus = [...numberStr].reduce((prev, current) => Number(prev) + Number(current), 0);
+  const multiplication = [...numberStr].reduce((prev, current) => Number(prev) * Number(current), 1);
+  return plus > multiplication ? plus : multiplication;
 }
 
 // 페이지 번호의 유효성 검사
