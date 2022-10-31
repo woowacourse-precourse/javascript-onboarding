c83adedd1d06c82cd9788bb3f24f39bfab25214c
energy1-914 <dlawldnjs5@daum.net>
2022-11-01 00:16:14 +0900

    feat: 문제4번 기능구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,13 @@
 function problem4(word) {
-  var answer;
+  let splitWord = word.split('');
+  const askiArr = splitWord.map(x=> x.charCodeAt())  
+  let reverseArr = [];
+  for (let x of askiArr){
+    if (65<=x && x<=90) reverseArr.push(155-x);
+    else if (97<=x && x<=122) reverseArr.push(219-x);
+    else  reverseArr.push(x);
+  }
+  const answer= reverseArr.map(x=>String.fromCharCode(x)).join('')
   return answer;
 }
 
