ff28b55c04538a6c3356418eda1acf73697ba218
HBSPS <wiyeong79@naver.com>
2022-10-31 13:04:59 +0900

    :sparkles: [문제7] 기능1. friends 속의 각 친구 관계를 딕셔너리로 나타내는 메소드

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,21 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function getConnection(friends) {
+  const friendsConnection = {};
+
+  for (let i = 0; i < friends.length; i++) {
+    const friendA = friends[i][0];
+    const friendB = friends[i][1];
+
+    if (!Object.keys(friendsConnection).includes(friendA)) friendsConnection[friendA] = new Set();
+    if (!Object.keys(friendsConnection).includes(friendB)) friendsConnection[friendB] = new Set();
+
+    friendsConnection[friendA].add(friendB);
+    friendsConnection[friendB].add(friendA);
+  }
+
+  return friendsConnection;
+}
+
 module.exports = problem7;
