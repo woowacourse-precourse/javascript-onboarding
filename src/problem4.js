72c9b6453e195fdc55da2a55369f8214c9f35d40
HojinAn <asdv94@naver.com>
2022-11-01 08:34:05 +0900

    docs: problem 4 feature list

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,11 @@
+/**
+ * Problem 4
+ * 1. a~z, A~Z까지의 청개구리 사전 작성
+ * 2. word를 배열로 변환
+ * 3. 사전을 참고하여 문자열 반환
+ * @param {string} word 1 이상 1,000 이하
+ * @returns {string}
+ */
 function problem4(word) {
   var answer;
   return answer;
