1bdef6e595a2ab208d98ca4848c5be491a1f3529
HelloHailie <alsltjsals@gmail.com>
2022-10-31 17:10:23 +0900

    problem7 test 해결

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,36 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  let friendReco = [];
+  let friend = [];
+  let visit = [...visitors];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0].includes(user)) {
+      friendReco.push(friends[i][0]);
+      friend.push(friends[i][1]);
+    } else {
+      friendReco.push(friends[i][1]);
+      friend.push(friends[i][0]);
+    }
+  }
+  const realFriend = friend.filter((e, idx) => friend.indexOf(e) === idx); // 이미 친구인 이름
+  friendReco = friendReco.filter((e) => e !== user); // 추천 친구 10점이상
+
+  for (let i = 0; i < realFriend.length; i++) {
+    visit = visit.filter((e) => e !== `${realFriend[i]}`); // 방문자 친구 1점이상
+  }
+
+  const realFriendReco = friendReco.filter(
+    (e, idx) => friendReco.indexOf(e) === idx
+  );
+  for (let i = 0; i < realFriendReco.length; i++) {
+    answer.push(realFriendReco[i]);
+  }
+  const realVisit = visit.filter((e, idx) => visit.indexOf(e) === idx);
+
+  for (let i = 0; i < realVisit.length; i++) {
+    answer.push(realVisit[i]);
+  }
+
   return answer;
 }
 
