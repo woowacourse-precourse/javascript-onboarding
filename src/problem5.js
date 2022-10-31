6624ad4c60289822cdf646c44c604aa29437217c
Dean <yjj052800@gmail.com>
2022-10-31 20:22:56 +0900

    REFACTOR: [5번] 돈 교환 결과 배열 생성 기능 분리

--- src/problem5.js
+++ src/problem5.js
@@ -1,16 +1,22 @@
 function problem5(money) {
   if (!inputExecption(money)) return money;
   var answer;
-  let changeResult = [];
+
   const changeMachine = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  
-  for (i = 0; i <= changeMachine.length - 1; i++) {
-    let changeCnt = Math.floor(money / changeMachine[i]);
-    money -= (changeCnt * changeMachine[i]);
-    changeResult.push(changeCnt);
-  }
+  let changeResult = runChangeMachine(changeMachine, money);
   answer = changeResult;
+  
   return answer;
+}
+
+function runChangeMachine(arr, int) {
+  let result = [];
+  for (i = 0; i <= arr.length - 1; i++) {
+    let changeCnt = Math.floor(int / arr[i]);
+    int -= (changeCnt * arr[i]);
+    result.push(changeCnt);
+  }
+  return result;
 }
 
 function inputExecption(num) {
@@ -22,5 +28,3 @@ function inputExecption(num) {
 
 
 module.exports = problem5;
-
-console.log(problem5(232122));
