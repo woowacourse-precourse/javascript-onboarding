3ac1cf81ed0de7dfb093aff0edfed5f43a5758af
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-01 02:52:34 +0900

    problem7: function list

--- src/problem7.js
+++ src/problem7.js
@@ -1,49 +1,35 @@
 function problem7(user, friends, visitors) {
-  var answer = [];
-  var userFriends = [];
-  var friendsCandidate = [];
-  var scoreMap = new Map();
+  var answer;
 
-  friends.forEach((pair) => {
-    if (pair.includes(user)) {
-      userFriends.push(pair[1 - pair.indexOf(user)]);
-    } else friendsCandidate.push(pair);
-  });
-
-  for (var i = friendsCandidate.length - 1; i >= 0; i--) {
-    if (
-      friendsCandidate[i].filter((one) => userFriends.includes(one)).length == 2
-    )
-      friendsCandidate.splice(i, 1);
+  function createRelation(friends) {
+    var relationMap = new Map();
+    return relationMap;
   }
 
-  userFriends.forEach((userFriend) => {
-    friendsCandidate.forEach((pair) => {
-      if (pair.includes(userFriend)) {
-        var newFriend = pair[1 - pair.indexOf(userFriend)];
-        if (!scoreMap.has(newFriend)) scoreMap.set(newFriend, 10);
-        else scoreMap.set(newFriend, scoreMap.get(newFriend) + 10);
-      }
-    });
-  });
+  function createScore(relationMap, visitors, user) {
+    var scoreMap = new Map();
+    return scoreMap;
+  }
 
-  visitors.forEach((one) => {
-    if (!userFriends.includes(one)) {
-      if (!scoreMap.has(one)) scoreMap.set(one, 1);
-      else {
-        scoreMap.set(one, scoreMap.get(one) + 1);
-      }
-    }
-  });
+  function relationScore(scoreMap, relationMap, user) {
+    return scoreMap;
+  }
 
-  answer = Array.from(scoreMap)
-    .sort((a, b) => {
-      if (a[1] == b[1]) a[0] - b[0];
-      else a[1] - b[1];
-    })
-    .slice(0, 5)
-    .map((map) => map[0]);
+  function visitScore(scoreMap, visitors) {
+    return scoreMap;
+  }
 
+  function sorting(scoreMap) {
+    var resultArray;
+
+    return resultArray;
+  }
+
+  var relation = createRelation(friends);
+  var scoreMap = createScore(relation, visitors, user);
+  scoreMap = relationScore(scoreMap, relation, user);
+  scoreMap = visitScore(scoreMap, visitors);
+  answer = sorting(scoreMap);
   return answer;
 }
 
