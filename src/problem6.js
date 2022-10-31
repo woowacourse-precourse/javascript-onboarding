96fb7ccba7fae998c99b3849aeb62e6168e786be
nara04040 <nara04040@naver.com>
2022-10-31 19:41:56 +0900

    [problem6] feat : 배열을 순회하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,17 @@
 function problem6(forms) {
   let answer = [];
+  forms.forEach((ele, nameIdx) => {
+    console.log(ele, nameIdx);
+  });
   return answer;
 }
+let forms = [
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+];
+console.log(problem6(forms));
 
 module.exports = problem6;
