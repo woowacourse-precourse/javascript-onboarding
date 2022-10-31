2f2d8505eb168da834c3909cec87f0f21fc33f33
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 20:01:52 +0900

    feat: [1번 문제] 각 자리 숫자 곱하기

--- src/problem1.js
+++ src/problem1.js
@@ -9,6 +9,13 @@ function addDigits(number) {
     .reduce((acc, cur) => acc + Number(cur), 0);
 }
 
+function multiplyDigits(number) {
+  return number
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc * Number(cur), 1);
+}
+
 function passExceptions(pobi, crong) {
   return isSameChapter(pobi[0], pobi[1]) && isSameChapter(crong[0], crong[1]);
 }
