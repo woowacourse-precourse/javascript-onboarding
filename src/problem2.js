e6464c99792913990036280444b0bb1c02df3c37
knh6269 <knh6269@gmail.com>
2022-10-31 22:39:22 +0900

    feat: 미션2) 메인 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,6 @@
+function problem2(cryptogram) {
+  var answer;
+  return answer;
 //같은 문자들을 지워주는 함수
 const eraseArray = (cryptogram, index) => {
   let count = 1; //자르는 갯수
@@ -13,6 +16,26 @@ const eraseArray = (cryptogram, index) => {
   cryptogram.splice(index, count);
 }
 
+//메인 함수
+const problem2 = (cryptogram) => {
+  //자르기 편하게 배열로 변환
+  cryptogram = cryptogram.split("");
+  
+  while(1){
+    let status = 1;
+    for(let i = 0; i < cryptogram.length; i++){
+        //이전 인덱스와 현재 인덱스의 값이 같다면
+        if (cryptogram[i] === cryptogram[i+1]) {
+        eraseArray(cryptogram, i);
+        status = 0;
+        i--;
+        }
+    }
+    if(status) break;
+  }
+  
+  //다시 문자열로 변환후 return
+  return (cryptogram.join(""))
 }
 
 module.exports = problem2;
