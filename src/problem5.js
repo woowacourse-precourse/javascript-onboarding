e8f1daa1fd3caecc4ba9a375a1ac05172dd18ff9
ryukyunghye <ryukyung123@naver.com>
2022-11-01 01:40:05 +0900

    add: problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  let moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let calc of moneyList) {
+    let cut = parseInt(money / calc);
+    let rest = money % calc;
+    answer.push(cut);
+    money = rest;
+  }
   return answer;
 }
-
 module.exports = problem5;
