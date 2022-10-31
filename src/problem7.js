8a33c19aca7af17afed64a9d7e53024e9280f911
김상두 <puki4416@gmail.com>
2022-10-31 23:41:01 +0900

    problem7 : 기능 정의

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,9 @@
+// set의 교집합을 구하는 함수 구현
+// 친구관계 객체를 만드는 함수 구현
+// 친구별 추천점수를 계산하는 함수 구현
+// 이미 친구인 유저를 목록에서 삭제하는 함수 구현
+// 결과 객체를 정렬하고, 5개까지만 나오도록 잘라주는 함수 구현
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
