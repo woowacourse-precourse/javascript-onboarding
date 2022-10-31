a66dd6bcbec3c30d6c3c3f6d5f8669c76e80b672
jinseo <ghkdwlstj99@gmail.com>
2022-11-01 01:18:27 +0900

    유저와 함께 아는 친구가 있는지 확인하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,9 @@
 function problem7(user, friends, visitors) {
-  const checkUserFreind = userFriend(user,friends)  
-  const checkVisitList = visitorList(visitors)
-  console.log(checkUserFreind, checkVisitList)
+  const checkUserFreind = userFriend(user,friends);  
+  const checkVisitList = visitorList(visitors);
+  const acquaintance_List = acquaintance(checkUserFreind, friends, user);
+  const acquaintance_List_Number = acquaintanceListNumber(acquaintance_List);
+  console.log(checkUserFreind, checkVisitList,acquaintance_List,acquaintance_List_Number);
 }
 
 function userFriend(user,friends){
@@ -23,5 +25,26 @@ function visitorList(visitors){
     visitor_List[visitor] = (visitor_List[visitor] || 0)+1});
     return visitor_List;
   };
+
+function acquaintance(userFriend, friends, user){
+  let acquaintance_List = [];
+  for (let i = 0; i < userFriend.length; i++) {
+    for (let j = 0; j < friends.length; j++) {
+      if (friends[j][0] == userFriend[i] && friends[j][1] != user) {
+        acquaintance_List.push(friends[j][1]);
+      } else if (friends[j][1] == userFriend[i] && friends[j][0] != user) {
+        acquaintance_List.push(friends[j][0]);
+      }
+    }
+  }
+  return acquaintance_List;
+}
+
+function acquaintanceListNumber(acquaintance_List){
+  const acquaintanceListNumber = [];
+  acquaintance_List.filter((acquaintance) => { acquaintanceListNumber[acquaintance]
+     = (acquaintanceListNumber[acquaintance] || 0)+1});
+     return acquaintanceListNumber;
+}
 
 module.exports = problem7;
