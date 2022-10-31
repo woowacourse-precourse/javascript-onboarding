c5ffd7c809b72101a9963fbef0135a930848033b
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-10-31 10:41:24 +0900

    feat: 문제2 연속하는 중복 문자 탐색하고 삭제하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  let overlap = 1;
+  const letters = cryptogram.split('');
+  for (let i = 0; i < letters.length; i++) {
+    if (letters[i] === letters[i + 1]) {
+      overlap++;
+    } else if (overlap >= 2) {
+      cryptogram = cryptogram.replace(letters[i].repeat(overlap), '');
+      overlap = 1;
+    }
+  }
   return answer;
 }
 
