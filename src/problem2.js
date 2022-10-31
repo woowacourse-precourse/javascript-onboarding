7c8ab631e1d61c5dee836051d0a0aa7e2b2300a9
somin <smhs12@naver.com>
2022-10-31 14:56:30 +0900

    feat: problem2/ 문자열 중복 포함 여부 검사 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,12 @@
 function problem2(cryptogram) {
-  let answer = [];
-  let prev = "";
-  for (let i = 0; i < cryptogram.length; i++) {
-    if (prev === cryptogram[i]) {
-      answer.pop();
-      prev = answer[answer.length - 1];
-    } else {
-      answer.push(cryptogram[i]);
-      prev = cryptogram[i];
-    }
-  }
-  return answer.join("");
+  return answer;
 }
 
+function isDuplicate(cryptogram) {
+  const setCryptogram = new Set(cryptogram);
+  if (cryptogram.length !== setCryptogram.size) {
+    return true;
+  }
+  return false;
+}
 module.exports = problem2;
