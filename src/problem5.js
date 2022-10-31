356810d9a6c090bd1065992452fcbe2da2dc8b41
김나현 <nahyeon.dev@gmail.com>
2022-10-31 14:02:56 +0900

    problem 5 기능구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  //var answer;
+  const answer = [0,0,0,0,0,0,0,0,0];
+  const m_check = [50000,10000,5000,1000,500,100,50,10,1];
+
+  for(let i = 0; i < answer.length; i++){
+    if(parseInt(money / m_check[i]) === 0) continue;
+    else { 
+      answer[i] = parseInt(money / m_check[i]);
+      money = money % m_check[i];
+    }
+  }
   return answer;
 }
 
