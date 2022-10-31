91a5216acd6bc9ac5923b1c20ea6816fb5f7cfae
Koeunseooooo <rhdmstj17@cau.ac.kr>
2022-10-31 21:52:43 +0900

    Feat: 4번 기능 구현 완료 & Feat: 4번 기능 목록 리드미 파일 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,39 @@
 function problem4(word) {
-  var answer;
+  var answer="";
+  // var word = convertUL(word); -> 나중에 나오는거임
+  console.log(word);
+  for (let i=0;i<word.length;i++){
+    let ascii = word.charCodeAt(i);
+    let cov_ascii;
+    let cov_str;
+    if ((ascii>=65 && ascii<=90)){ //대문자라면
+      if (ascii<78){
+        cov_ascii=ascii+25-((ascii-65)*2);
+      }else{
+        cov_ascii=ascii-((ascii-78)*2+1);
+      }
+      cov_str=String.fromCharCode(cov_ascii);
+      answer+=cov_str;
+    }
+    else if ((ascii>=97 && ascii<=122)){ //소문자라면
+      if (ascii<110){
+        cov_ascii=ascii+25-((ascii-97)*2);
+      }else{
+        cov_ascii=ascii-((ascii-110)*2+1);
+      }
+      cov_str=String.fromCharCode(cov_ascii);
+      answer+=cov_str;
+    }
+    else{
+      answer+=" ";
+    }
+    console.log(cov_ascii);
+  }
+  
+  console.log(answer);
   return answer;
 }
 
+
+problem4("I love you")
 module.exports = problem4;
