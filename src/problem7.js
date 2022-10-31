3888e1272482e1e796bfadea78be71f4a056fb63
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-31 13:45:25 +0900

    feat: [P7] 사용자와 함께 아는 친구가 있는 사람에게 함께 아는 친구 수에 따라 친구 점수 부여하기
    - 사용자를 제외한 친구 관계 배열 만들기
    - 사용자와의 친구 관계를 확인하여 사용자의 친구를 담은 배열 만들기
    - 사용자를 제외한 친구 관계 배열에서 사용자의 친구 별로 친구 관계를 확인하기
    - 사용자의 친구와 친구인 사람에게 함께 아는 친구 수마다 10점씩 추가하기
    - 그 사람이 이미 사용자의 친구인 경우 제외하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,29 @@
 function problem7(user, friends, visitors) {
 }
 
+function getFriendPoint(user, friends) {
+  const userFriend = checkFriendship(user, friends)
+  const friendsExceptUser = exceptUser(user, friends)
+  let friendPoint = {}
+  userFriend.forEach((friend) => {
+      checkFriendship(friend, friendsExceptUser).forEach((person) => {
+          friendPoint[person] ? friendPoint[person] += 10 : friendPoint[person] = 10
+      })
+  })
+  userFriend.forEach((friend) => {
+      if (friendPoint[friend]) delete friendPoint[friend]
+  })
+  return friendPoint
+}
+
+function exceptUser(user, friends) {
+  let friendsExceptUser = []
+  friends.forEach((friendship) => {
+      if (friendship[0] !== user && friendship[1] !== user) friendsExceptUser.push(friendship)
+  })
+  return friendsExceptUser
+}
+
 function checkFriendship(person, friends) {
   let friendOfPerson = []
   friends.forEach((friendship) => {
