8a50a3a8aa9a3ba6dbafb47993b9893fb76e8b98
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:32:14 +0900

    3번 문제 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,17 +1,18 @@
 function problem3(number) {
   var answer;
   
-
-
-  if(number==3){
-    answer= answer+1;
-  } else if(number==6){
-    answer=answer+1;
-  } else if(number==9){
-    answer=answer+1;
+  for(let i=1;i<=number;i++){
+    const str=number.charAt(i);
+    for(let j=0;j<str.length;j++){
+      if(str[i]==3){
+        answer= answer+1;
+      } else if(str[i]==6){
+        answer=answer+1;
+      } else if(str[i]==9){
+        answer=answer+1;
+      }
+    }
   }
-
-  
   return answer;
 }
 
