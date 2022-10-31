bcc0be7d9c436a42a5031b75993eda4994098a22
Sang-Mini <sangmin3062@gmail.com>
2022-10-31 22:22:02 +0900

    [problem4] 매개변수로 전달된 문자열을 청개구리 사전을 참고하여 반대로 변환하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,7 @@ function problem4(word) {
   let dictionary = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ';
   let oppositeDictionary = 'ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba ';
 
+  // 매개변수로 전달된 문자열을 청개구리 사전을 참고하여 반대로 변환하는 기능 구현
   for (let i = 0; i < word.length; i++) {
     let b = dictionary.indexOf(word[i]);
     answer = answer.concat(oppositeDictionary[b]);
