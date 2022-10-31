16c5ceb75048de709a86ea02d442e5f0ebf69f2b
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-11-01 00:53:12 +0900

    refactor: problem5 변수 선언 키워드 수정, 변수명 수정

--- src/problem5.js
+++ src/problem5.js
@@ -1,20 +1,15 @@
-//화폐 단위 리스트 units
-//map(..) - money를 단위로 나눈값 또는  0
 function problem5(money){
-  var answer;
-  let units = [50000,10000,5000,1000,500,100,50,10,1];
-  let num;
-  answer = units.map((unit)=>{
-      if(money>=unit){
-          num = parseInt(money/unit);
-          money  %= unit;
-          return num;
-      }
-      else{
-          return 0;
-      }
-  })
-  return answer;
-}
+    const units = [50000,10000,5000,1000,500,100,50,10,1];
+    let num_coin;
+    const answer = units.map((unit)=>{
+        if(money>=unit){
+            num_coin = parseInt(money/unit);
+            money  %= unit;
+            return num_coin;
+        }
+        return 0;
+    })
+    return answer;
+  }
 
 module.exports = problem5;
