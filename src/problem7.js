6f0aad430ef62d2f136c278448956dd73abc0736
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 22:09:37 +0900

    user 친구찾기

--- src/problem7.js
+++ src/problem7.js
@@ -1,24 +1,29 @@
 /*
   ### 기능 목록
 
-  1. friends 에서 user를 찾는다.
-  2. 해당 배열의 다른 사람(key)을 Set에 넣고 value = 10으로 설정한다.
-  2. visitors를 참고하여 value+=1 점씩 매긴다.
-  3. 점수가 0이하인 경우는 제외한다.
-  4. 점수순, 이름순으로 정렬한다.  
+  1. user와 친구인 사람을 찾는다.
+  1-1. friend에서 '친구'는 추천받지 않음으로, friends에서 user의 친구 관계를 삭제한다.
+  2. user와 친구인 사람인 '친구의 친구
+  를 찾는다. 
+  3. '친구의 친구' 사람(key)을 Map에 넣고 value = 10으로 설정한다.
+  4. visitors를 참고해 방문객을 Map에 넣고, value+=1 점씩 매긴다.
+  5. 점수순(내림차순), 이름순(오름차순)으로 정렬한다.  
 */
 
 function problem7(user, friends, visitors) {
-  const answer = new Map();
-  friends.forEach((element) => {
-    if (element.indexOf(user) != -1) {
-      element.indexOf(user) == 0
-        ? answer.set(element[1], 10)
-        : answer.set(element[0], 10);
+  let answer = new Map();
+  const myFriend = [];
+
+  let i = friends.length;
+  while (i--) {
+    if (friends[i].indexOf(user) == 0) {
+      myFriend.push(friends[i][1]);
+      friends.splice(i, 1);
+    } else if (friends[i].indexOf(user) == 1) {
+      myFriend.push(friends[i][0]);
+      friends.splice(i, 1);
     }
-  });
-  console.log(answer);
-  return answer;
+  }
 }
 
 module.exports = problem7;
