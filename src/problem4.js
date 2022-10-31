e3cf85b3869eedc7d0c4a02a854d456b234dce41
김상두 <puki4416@gmail.com>
2022-10-31 23:36:28 +0900

    problem4 : checkChar 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,17 @@
 // 문자를 변환하는 함수 구현
 // 구현된 함수를 사용하여 problem4 함수 구현
 
+function checkChar(char) {
+  const asciCode = char.charCodeAt();
+  if (asciCode >= 65 && asciCode <= 90) {
+    return 1;
+  } else if (asciCode >= 97 && asciCode <= 122) {
+    return 2;
+  } else {
+    return 0;
+  }
+}
+
 function problem4(word) {
   var answer;
   return answer;
