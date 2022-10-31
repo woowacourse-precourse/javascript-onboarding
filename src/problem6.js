3d2363ab42d9819dc4ac7895f50582396accccae
ming0830 <minsun0830@naver.com>
2022-11-01 07:19:03 +0900

    [PROBLEM6] style : 세미콜론 누락, console.log 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -8,15 +8,16 @@ function problem6(forms) {
   });
 
   // 조건에 부합하는 닉네임의 이메일을 추출
-  let targetEmails = []
+  let targetEmails = [];
   for(let targetNickname of findNickname(nicknames)){
     let idx = nicknames.indexOf(targetNickname);
-    targetEmails.push(emails[idx])
+    targetEmails.push(emails[idx]);
   }
   // 오름차순 정렬
   targetEmails.sort();
-
-  console.log(targetEmails)
+  
+  return targetEmails;
+  
 }
 
 // 연속으로 중복된 문자들을 모은 배열 생성
@@ -37,7 +38,7 @@ function findNickname(nicknames){
   let temp = [];
   for(let nickname of nicknames){
     if(getConsecutiveStr(nickname).length != 1){
-      temp.push((getConsecutiveStr(nickname)))
+      temp.push((getConsecutiveStr(nickname)));
     } 
   } 
   let checkWord = [...new Set(temp.flat())];
@@ -47,18 +48,16 @@ function findNickname(nicknames){
   for(let word of checkWord){
     for(let nickname of nicknames){
       if(nickname.includes(word)){
-        duplicatedNikname.push(nickname)
+        duplicatedNikname.push(nickname);
       } 
     } break
-  } return duplicatedNikname
+  } return duplicatedNikname;
     
   
   
 }
 
-const forms = [ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ];
 
-problem6(forms)
 
 
 module.exports = problem6;
