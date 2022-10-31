7d7a52478478dfed88490e2b01ef548485b15bfc
Moyujin <scab12341@gmail.com>
2022-11-01 05:27:00 +0900

    6번 닉네임이 정상인지 판별하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -8,12 +8,32 @@ function problem6(forms) {
   return answer;
 }
 
+const checkValue = (forms) => {
+  for (let i = 0, formsLen = forms.length; i < formsLen; i++) {
+    const [email, nickname] = forms[i];
+
+    if (!isEmailTypeValid(email)) return false;
+    if (!isNicknameTypeValid(nickname)) return false;
+  }
+  return true;
+};
+
 const isEmailTypeValid = (email) => {
   const re = /^[A-Za-z0-9]+@email.com/;
   if (email.length < 11 || 20 <= email.length || !re.test(email)) {
     console.log("이메일 형식이 잘못되었습니다");
     return false;
   }
+
+  const isNicknameTypeValid = (nickname) => {
+    const re = /[ㄱ-ㅎ|가-힣]/;
+    if (nickname.length < 1 || 20 <= nickname.length || !re.test(nickname)) {
+      console.log("닉네임 형식이 잘못되었습니다");
+      return false;
+    }
+
+    return true;
+  };
 
   return true;
 };
