5b71c849ad65ea8d9aadcfa2bff755b21a758036
yeongi <cbg5255@gmail.com>
2022-10-31 11:22:17 +0900

    문제2. 기능 목록 재 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,30 +1,10 @@
 function problem2(cryptogram) {
-  //중복 여부 확인
-  let isHave = true;
-  //문자 배열 생성
-  let str = [...cryptogram];
-
-  while (isHave) {
-    isHave = false;
-    str.forEach((c, i) => {
-      if (c === str[i + 1]) {
-        // 중복 문자열 제거
-        cryptogram = strEdit(cryptogram, c);
-        //이번 암호에 중복 문자열이 있다는 것
-        isHave = true;
-      }
-    });
-    //문자 배열 초기화
-    str = [...cryptogram];
-  }
-
   return cryptogram;
 }
 
-// 중복 문자열 제거
-const strEdit = (str, char) => {
-  const arr = str.split(`${char}${char}`);
-  return arr[0].concat(arr[1]);
-};
+//기능 1. 중복 문자열 찾기
+function findDupStr(str) {}
+//기능 2. 중복 문자열 제거
+const dupStrDelete = (str, char) => {};
 
 module.exports = problem2;
