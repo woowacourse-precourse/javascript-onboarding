a245624966495881d303329e6ab77563b9961938
ming0830 <minsun0830@naver.com>
2022-11-01 03:52:33 +0900

    [PROBLEM6] feat : 연속으로 중복된 문자들을 모은 배열을 생성하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,15 @@
 function problem6(forms) {
-  var answer;
-  return answer;
 }
+// 연속으로 중복된 문자들을 모은 배열 생성
+function getConsecutiveStr(nickname){
+  let wordList = new Set();
+  for(let i =0;i<nickname.length;i++){
+    for(let j = i+2;j<=nickname.length;j++){
+      wordList.add(nickname.substring(i,j))
+    }
+
+  } console.log([...wordList])
+}
+
 
 module.exports = problem6;
