596156e9aaedfe6240ec61164cb8b220a0a6497c
Buyoung Chung <sunarol@naver.com>
2022-10-31 17:29:46 +0900

    [문제1] 페이지를 입력받아 최대값 계산

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,20 @@
+function calculator(pages) {
+  let pageResult = [];
+  for (let page of pages) {
+    let sum = 0;
+    let time = 1;
+    while (page > 0) {
+      sum += page % 10;
+      time *= page % 10;
+      page = Math.floor(page / 10);
+    }
+    sum > time ? pageResult.push(sum) : pageResult.push(time);
+  }
+  return Math.max(...pageResult);
+}
 function problem1(pobi, crong) {
-  var answer;
+  var answer = [];
+  answer.push(calculator(pobi), calculator(crong));
   return answer;
 }
 
