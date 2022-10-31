5b6a347c04792da6900de2b86ba1713b3b1beb11
gsong <rmsdlf1206@gmail.com>
2022-10-31 22:17:32 +0900

    user의 친구를 확인하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  checkUserFriends(user, friends, userFriends);
 }
-
+function checkUserFriends(user, friends, userFriends) {
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      userFriends.push(String(friends[i].filter((e) => e !== user)));
+    }
+  }
+} // user의 친구를 확인하는 함수
 module.exports = problem7;
