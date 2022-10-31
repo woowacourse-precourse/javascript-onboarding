4564d3e334ef40e1216999b3412688301818dc14
gsong <rmsdlf1206@gmail.com>
2022-10-31 20:27:45 +0900

    feat: problem 4 ascii코드를 통해 소문자와 대문자로 나누어 청개구리 사전에 따라 변환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answerList = convertByAscii(word);
+  console.log(answerList);
 }
 
+function convertByAscii(word) {
+  answerList = [];
+  word.split("").map((letter) => {
+    if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
+      answerList.push(String.fromCharCode(155 - x.charCodeAt(0))); // 155 -> ascii A(65) + Z(90)
+    } else if (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122) {
+      answerList.push(String.fromCharCode(219 - letter.charCodeAt(0)));
+    } else {
+      answerList.push(letter);
+    }
+  });
+  return answerList;
+}
+problem4("abc");
 module.exports = problem4;
