7d216d7ead7f6a8d117f3b67dfe33d81915a341f
Haram Baek <haram@Haramui-MacBookAir.local>
2022-10-31 14:32:32 +0900

    feat: [PROBLEM3] 정규 표현식으로 3,6,9 찾아서 배열에 넣기

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,20 @@
 function problem3(number) {
   var answer = 0;
+  const reg = /[369]/g;
+  let clap = [];
 
-  for (let i = 1; i < number; i++) {
+  for (let i = 1; i < number + 1; i++) {
     let num = String(i);
-    console.log(num);
+
+    if (num.match(reg) !== null) {
+      for (let c = 0; c < num.match(reg).length; c++) {
+        clap.push(num.match(reg)[c]);
+      }
+    }
   }
+
+  console.log(clap);
+
 
   return answer;
 }
