b5a4c313a00f70a29f7432077dd3f77456c86d6c
Jiho1996 <jhp9270@naver.com>
2022-10-31 11:42:40 +0900

    refactor : dictionary(obj) 관련 로직 분리

--- src/problem6.js
+++ src/problem6.js
@@ -3,33 +3,35 @@ function problem6(forms) {
   const result = [];
   const userMail = 0;
   const userId = 1;
-  const maxLetterLength = 2;
+  const maxStackLength = 2;
   
   const StoreDuplicatedIdFromObj = (result, dictionary) => {
-    const isDuplicated = dictionary => dictionary[key].length > 1;
+    const isDuplicatedFromObj = key => dictionary[key].length > 1;
 
     for (let key in dictionary){
-      if (isDuplicated(dictionary)){
+      if (isDuplicatedFromObj(key)){
           result.push(...dictionary[key]);
         }
     }
   }
-
+  const removeDuplicated = result => result.filter((v, i) => result.indexOf(v) === i);
+  const pushEmailToExistedProperty = (x, info) => dictionary[x].push(info[userMail])
+  const createNewProperty = (x, info) => dictionary[x] = [info[userMail]]
   /**
    * forms들을 순회하며 dictionary에 중복 포함 저장하는 로직
    */
   forms.forEach((personalInfo) => {
       const stack = [];
       for (let info of personalInfo[userId]){
-          if (stack.length < maxLetterLength)
+          if (stack.length < maxStackLength)
               stack.push(info);
-          if (stack.length === maxLetterLength){
+          if (stack.length === maxStackLength){
               x = stack.join('')
               if (dictionary.hasOwnProperty(x)){
-                dictionary[x].push(personalInfo[userMail])
+                pushEmailToExistedProperty(x, personalInfo);
               }
               if (!dictionary.hasOwnProperty(x)){
-                dictionary[x] = [personalInfo[userMail]]
+                createNewProperty(x, personalInfo);
               }
               stack.shift();
               
@@ -37,8 +39,8 @@ function problem6(forms) {
       } 
   });
 
-  StoreDuplicatedIdFromObj();
-  return result.filter((v, i) => result.indexOf(v) === i).sort(); 
+  StoreDuplicatedIdFromObj(result, dictionary);
+  return removeDuplicated(result).sort(); 
 }
 
 module.exports = problem6;
