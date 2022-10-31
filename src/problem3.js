b9f2fcee6a6d46534f19c94e56e6654aa5fe09bb
okorion <ok.or.orion@gmail.com>
2022-11-01 00:33:38 +0900

    feat: 왼쪽/오륜 number까지 수를 구하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
+// **기능 요구 사항**
+// 1. 1에서 number 까지의 수를 구한다.
+// 2. 1에서 구한 수들 중에 3,6,9의 숫자를 카운트한다.
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let last_number = number;
+  let count = 0;
+
+  for (let i = 0; i < last_number; i++) {
+    console.log(i)
+  }
 }
 
 module.exports = problem3;
+
+problem3(13)
