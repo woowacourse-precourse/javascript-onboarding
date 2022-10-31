0a9cc37a9fc209969e60ab78263e015736c8b46b
pangkyu <baesense@naver.com>
2022-10-31 13:44:56 +0900

    feat : 유저와 친구를 찾는 기능
    includes로 user와 친구사이를 찾은 뒤, replace로 친구인 사람만 push하여 리턴하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,34 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const result = [];
+
+  const userFriend = getFriendUser(user, friends);
 }
 
+function getFriendUser(user, friends) {
+  const friend = [];
+  for (let i = 0; i < friends.length; i++) {
+    const includeUser = friends[i].includes(user);
+    if (includeUser) {
+      console.log(String(friends[i]).replace(user && ",", ""));
+      friend.push(String(friends[i]).replace(user && ",", ""));
+    }
+  }
+  return friend;
+}
+
+function testCode() {
+  problem7(
+    "mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"],
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]
+  );
+}
+testCode();
 module.exports = problem7;
