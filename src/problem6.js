f5e1b31c46c8f3bad4655c088df0d13363403210
jeongbaebang_dev <jeongbaebang_dev@naver.com>
2022-10-31 19:34:04 +0900

    problem6 - feat: each 함수
    요소를 순회한다.

--- src/problem6.js
+++ src/problem6.js
@@ -2,6 +2,14 @@ const copyArray = targetArray => {
   return [...targetArray];
 };
 
+const each = (iterable, func) => {
+  for (const iterator of iterable) {
+    func(iterator);
+  }
+
+  return;
+};
+
 function problem6(forms) {
   var answer;
   return answer;
