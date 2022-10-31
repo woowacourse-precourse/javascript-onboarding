1c6f2f411fcc4d6f06b2a1a1a3b25e2e7d6e10dc
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 05:10:00 +0900

    FEAT : problem4 - 문자열을 하나씩 비교하여 알파벳이 아니라면 바로 반환하고 알파벳이라면 주어진 조건에 따라 반환

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,16 @@ function problem4(word) {
   const ALPHABET_REVERSE = ALPHABET.slice().reverse();
   // 알파벳 정규식 선언
   const reg = /^[a-zA-Z]*$/;
+
+  return [...word].map((text) => {
+    // 알파벳이 아니라면 반환
+    if(!reg.test(text)) return text;
+
+    // 문자의 인덱스를 찾는다.
+    const findIndex = ALPHABET.findIndex((alphabet) => text.toUpperCase() === alphabet);
+    if(text === ALPHABET[findIndex]) return ALPHABET_REVERSE[findIndex];
+    else return ALPHABET_REVERSE[findIndex].toLowerCase();
+  }).join("");
 }
 
 module.exports = problem4;
