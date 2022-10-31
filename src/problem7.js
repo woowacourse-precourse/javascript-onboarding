3f8df44d59453a6a1c6de526332aa113f7f2db7a
BBIYAC <kiy7833@gmail.com>
2022-10-31 22:52:35 +0900

    refactor(problem7): separation of function
    - 함께 아는 친구 점수, 방문자 점수, 정렬 함수 분리

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,5 @@
-function problem7(user, friends, visitors) {
-  let recommend_friends = {},
-      acquaintance = {},
-      sorted_friends = [];
+function getAcquaintance(friends, user) {
+  let acquaintance = {};
 
   friends.forEach(([friend1, friend2]) => {
     if (friend2 !== user) {
@@ -13,28 +11,62 @@ function problem7(user, friends, visitors) {
       else acquaintance[friend2] = [friend1];
     }
   });
-  
+
+  return acquaintance;
+}
+
+function getAcquaintanceScore(acquaintance, user) {
+  let score = {};
+
   acquaintance[user].forEach(friend => {
-    acquaintance[friend].forEach(recommand => {
-      if (recommend_friends[recommand]) recommend_friends[recommand] += 10;
-      else recommend_friends[recommand] = 10;
+    acquaintance[friend].forEach(recommend => {
+      if (score[recommend]) score[recommend] += 10;
+      else score[recommend] = 10;
     })
-  })
+  });
+
+  return score;
+}
+
+function getVisitScore(acquaintance, visitors, user) {
+  let score = {};
 
   visitors.forEach(visitor => {
     if (!acquaintance[user].includes(visitor)) {
-      if (recommend_friends[visitor]) recommend_friends[visitor] += 1;
-      else recommend_friends[visitor] = 1;
+      if (score[visitor]) score[visitor] += 1;
+      else score[visitor] = 1;
     }
   })
 
-  sorted_friends = Object.keys(recommend_friends)
+  return score;
+}
+
+function getRecommandScore(acquaintance, visitors, user) {
+  const acquaintance_score = getAcquaintanceScore(acquaintance, user),
+        visit_score = getVisitScore(acquaintance, visitors, user),
+        recommend_friends = {...acquaintance_score, ...visit_score};
+
+  return recommend_friends;
+}
+
+function sortFriends(friends) {
+  let sorted_friends = [];
+  // 점수 내림차순, 이름 오름차순
+  sorted_friends = Object.keys(friends)
     .sort((a, b) => {
-      if (recommend_friends[a] > recommend_friends[b]) return -1;
-      if (recommend_friends[a] < recommend_friends[b]) return 1;
+      if (friends[a] > friends[b]) return -1;
+      if (friends[a] < friends[b]) return 1;
       if (a > b) return 1;
       if (a < b) return -1;
     });
+
+  return sorted_friends;
+}
+
+function problem7(user, friends, visitors) {
+  const acquaintance = getAcquaintance(friends, user),
+        recommend_friends = getRecommandScore(acquaintance, visitors, user),
+        sorted_friends = sortFriends(recommend_friends);
 
   return sorted_friends.slice(0, 5);
 }
