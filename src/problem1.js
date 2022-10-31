aa08a12aa582f6411c5592445868da1a083df3c7
foundaway98 <foundaway98@naver.com>
2022-10-31 18:53:21 +0900

    문제 1번 함수 위치 변경 및 answer 키워드 변경

--- src/problem1.js
+++ src/problem1.js
@@ -1,34 +1,33 @@
-function makeSum(n) {
-  let sum = 0;
-  do {
-    sum += n % 10;
-    n = Math.floor(n / 10);
-  } while (n > 0);
-  return sum;
-}
-
-function makeMul(n) {
-  let mul = 1;
-  do {
-    mul *= n % 10;
-    n = Math.floor(n / 10);
-  } while (n > 0);
-  return mul;
-}
-
-function choosePoint(arr) {
-  for (let i = 0; i < 2; i++) {
-    arr[i] = comparePoint(makeSum(arr[i]), makeMul(arr[i]));
-  }
-  return comparePoint(arr[0], arr[1]);
-}
-
-function comparePoint(n1, n2) {
-  return n1 > n2 ? n1 : n2;
-}
-
 function problem1(pobi, crong) {
-  var answer;
+  function makeSum(n) {
+    let sum = 0;
+    do {
+      sum += n % 10;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+    return sum;
+  }
+
+  function makeMul(n) {
+    let mul = 1;
+    do {
+      mul *= n % 10;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+    return mul;
+  }
+
+  function choosePoint(arr) {
+    for (let i = 0; i < 2; i++) {
+      arr[i] = comparePoint(makeSum(arr[i]), makeMul(arr[i]));
+    }
+    return comparePoint(arr[0], arr[1]);
+  }
+
+  function comparePoint(n1, n2) {
+    return n1 > n2 ? n1 : n2;
+  }
+  let answer;
   if (pobi[1] - pobi[0] == 1 && crong[1] - crong[0] == 1) {
     let pobiPoint = choosePoint(pobi);
     let crongPoint = choosePoint(crong);
