1aab1daaa361ed42a3270cfa55c93af7c0cfb1b7
webdreamer12 <dreamerdev12@gmail.com>
2022-11-01 00:33:27 +0900

    feat: problem3.js - 369 게임에서 각 숫자의 자릿수 별 3, 6, 9가 얼마나 존재하는지 확인하고, 이를 threeSixNineCount 변수에 누적시키는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
   var threeSixNineCount = 0;
+  for (currentNumber = 0; currentNumber < number; currentNumber++) {
+    var arrOfDigits = Array.from(String(currentNumber+1), Number);
+    for (const element of arrOfDigits) {
+        if ((element == 3) || (element == 6) || (element == 9)) {
+            threeSixNineCount++;
+        }
+    }
+  }
   var answer;
   return answer;
 }
