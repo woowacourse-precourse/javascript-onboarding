215912c5e66b4f8e1a416731559a8a1bbf699aa3
KimJerry <k90966277@gmail.com>
2022-11-01 00:11:24 +0900

    6번 ~ 7번 문제풀이
    6번 ~ 7번 문제풀이

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    const answer = [];
+
+    for (let i = 0; i < forms.length; i++) {
+        for (let j = 0; j < forms[i][1].length - 1; j++) {
+            if (forms[i][1].length > 1) {
+                let token = forms[i][1].substr(j, 2);
+                for (let k = 0; k < forms.length; k++) {
+                    if (k === i) continue;
+                    if (
+                        forms[k][1].includes(token) &&
+                        !answer.includes(forms[k][0])
+                    ) {
+                        answer.push(forms[k][0]);
+                    }
+                }
+            }
+        }
+    }
+    answer.sort();
+    return answer;
 }
 
 module.exports = problem6;
