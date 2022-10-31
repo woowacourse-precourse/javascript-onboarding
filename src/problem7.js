2235c68bc51c69b23ae122a2eabd6136b884c684
Azamman327 <wjsdbdud327@naver.com>
2022-11-01 05:50:37 +0900

    Style: [P7] score 배열명 recomm으로 변경

--- src/problem7.js
+++ src/problem7.js
@@ -1,62 +1,62 @@
 function problem7(user, friends, visitors) {
   var answer = [];
 
-  let score = {};
+  let recomm = {};
   let friendList = [];
   let linkedFriends = {};
 
-  //friends를 통해 닉네임 리스트(score)를 생성함
+  //friends를 통해 닉네임 리스트(recomm)를 생성함
   for (i = 0; i < friends.length; i++) 
-    score = updateScoreList(user, friends[i], score);
-  delete (score[user]);
+    recomm = updateRecommList(user, friends[i], recomm);
+  delete (recomm[user]);
 
   //user와 친구인 사람들을 저장하는 friendList 생성
   for (i = 0; i < friends.length; i++) 
     friendList = findUserFriend(user, friends[i], friendList);
 
   //friendList와 친구인 사람들, count 저장
-  linkedFriends = Object.assign({}, score);  //score의 초기 setting 복사하여 사용
+  linkedFriends = Object.assign({}, recomm);  //recomm의 초기 setting 복사하여 사용
   for (i = 0; i < friends.length; i++) 
     linkedFriends = ch_linkedFriends(friends[i], friendList, linkedFriends); //친구관계리스트 검사
   delete (linkedFriends[user]);
 
-  //linkedFriends를 기반으로 score 합산
+  //linkedFriends를 기반으로 recomm 합산
   const arrLen = Object.keys(linkedFriends).length;
   const tmpList = Object.keys(linkedFriends);
   for (i = 0; i < arrLen; i++)  
-    score[tmpList[i]] += linkedFriends[tmpList[i]] * 10;
+    recomm[tmpList[i]] += linkedFriends[tmpList[i]] * 10;
   
-  score = updateScoreList(user, visitors, score);   //score에 없는 유저 업데이트
+  recomm = updateRecommList(user, visitors, recomm);   //recomm에 없는 유저 업데이트
 
-  for (i = 0; i < visitors.length; i++)   //visitors 배열에 따라 score점수 계산
-    score[visitors[i]] += 1;
+  for (i = 0; i < visitors.length; i++)   //visitors 배열에 따라 recomm점수 계산
+  recomm[visitors[i]] += 1;
 
   for (i = 0; i < friendList.length; i++) //이미 친구인 유저는 추천 리스트에서 삭제
-    delete(score[friendList[i]]);
+    delete(recomm[friendList[i]]);
 
   //정렬
-  let sortedScore = [];
-  for (let value in score) 
-    sortedScore.push([value, score[value]]);
+  let sortedRecomm = [];
+  for (let value in recomm) 
+    sortedRecomm.push([value, recomm[value]]);
   
-  sortedScore.sort(function(a, b) {
+  sortedRecomm.sort(function(a, b) {
     return b[1] - a[1];
   });
 
   //출력
-  for (i = 0; i < sortedScore.length; i++) 
-    answer.push(sortedScore[i][0]);
+  for (i = 0; i < sortedRecomm.length; i++) 
+    answer.push(sortedRecomm[i][0]);
     
   return answer;
 }
 
-function updateScoreList(user, friends, score) {
-  if (!Object.keys(score).some(value => value === friends[0]))   //score에 없다면
-    score[friends[0]] = 0;
-  if (!Object.keys(score).some(value => value === friends[1]))   //score에 없다면
-    score[friends[1]] = 0;
+function updateRecommList(user, friends, recomm) {
+  if (!Object.keys(recomm).some(value => value === friends[0]))   //recomm에 없다면
+    recomm[friends[0]] = 0;
+  if (!Object.keys(recomm).some(value => value === friends[1]))   //recomm에 없다면
+    recomm[friends[1]] = 0;
 
-  return score;
+  return recomm;
 }
 
 //friendList를 생성하는 함수
@@ -82,15 +82,15 @@ function ch_linkedFriends(friend, userFriends, linkedFriends) {
 module.exports = problem7;
 
 /*
-1. 닉네임, 점수를 가진 객체배열 생성 score
+1. 닉네임, 점수를 가진 객체배열 생성 recomm
 2. 배열을 닉네임 기준으로 정렬
 3. 사용자 친구를 찾아 배열 friendList에 저장
 4. friendList에 있는 유저를 친구로 갖는 유저, 횟수를 담은 객체배열 linkedFriends
-5. 횟수에 따라 객체배열 score 업데이트
+5. 횟수에 따라 객체배열 recomm 업데이트
 
-6. visitors 배열에 따라 객체배열 score 업데이트
+6. visitors 배열에 따라 객체배열 recomm 업데이트
 
-7. value기준으로 score 객체배열 정렬
+7. value기준으로 recomm 객체배열 정렬
 8. value가 0인 부분부터 잘라내기
-9. score의 닉네임 부분 차례로 출력
+9. recomm의 닉네임 부분 차례로 출력
 */
