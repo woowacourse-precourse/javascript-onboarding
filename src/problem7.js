1205407fc44159cae10b2ff64c207db65ea2af69
yb8350 <yb8350@naver.com>
2022-11-01 02:40:32 +0900

    Feat: problem7-친구 관계를 map으로 표현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,19 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  let friendList = new Map();
+  for (let i = 0; i < friends.length; i++) {
+    addFriend(friendList, friends[i][0], friends[i][1]);
+    addFriend(friendList, friends[i][1], friends[i][0]);
+  }
+
   return answer;
+}
+
+function addFriend(friendList, friend1, friend2) {
+  if (friendList.get(friend1) !== undefined) {
+    friendList.set(friend1, [...friendList.get(friend1), friend2]);
+  } else friendList.set(friend1, [friend2]);
 }
 
 module.exports = problem7;
