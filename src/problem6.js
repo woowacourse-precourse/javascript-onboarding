ffb753875f6b3a8d6f6c48880daf9046bd003bd3
DAIN KIM <dnkim419@gmail.com>
2022-11-01 01:20:32 +0900

    feat: solved problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+
+  for (var i = 0; i < forms.length; ++i) {
+    const [emailI, nicknameI] = forms[i];
+
+    for (var j = 0; j < nicknameI.length - 1; ++j) {
+      const testCase = nicknameI.slice(j, j + 1);
+
+      for (var k = i + 1; k < forms.length; ++k) {
+        const [emailK, nicknameK] = forms[k];
+        if (nicknameK.includes(testCase)) answer.push(emailI, emailK);
+      }
+    }
+  }
+
+  answer = [...new Set(answer)];
+  answer.sort();
+
   return answer;
 }
 
