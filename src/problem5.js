a515f70a44ced5ddbfc3cde0e3979ff21fe57341
Minju Park <project.mizzu@gmail.com>
2022-10-31 18:50:46 +0900

    Feat : 화폐 단위 배열 listAmounts 생성
    - 사용할 화폐의 단위를 담은 배열 listAmounts 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,6 @@
 // [✅] 잔돈을 의미하는 change 변수 생성
-// [] result 배열 생성
+// [✅] result 배열 생성
+// [✅] 사용할 화폐의 단위를 담은 배열 생성 -> listAmounts
 // [] money를 5만원권으로 나누고 몫은 result[0]에 저장, 나머지는 change에 저장
 // [] money를 1만원권으로 나누고 몫은 result[1]에 저장, 나머지는 change에 저장
 // [] money를 5천원권으로 나누고 몫은 result[2]에 저장, 나머지는 change에 저장
@@ -14,6 +15,8 @@
 function problem5(money) {
   let change = 0;
   const result = [];
+  const listAmounts = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  console.log(Math.floor(money / listAmounts[0]), money % listAmounts[0]);
   return result;
 }
 
