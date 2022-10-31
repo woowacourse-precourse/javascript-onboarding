6f3a0ebf66c098a80a73dc902c9c90b646cae2f6
sying012 <syi369@naver.com>
2022-10-31 12:59:14 +0900

    problem4 - 기능 목록 작성, 제한사항 검사 함수 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,30 @@
 function problem4(word) {
   var answer;
+
   return answer;
+}
+
+function violationChk(word) {
+  // 문자열 길이가 1~1000자 사이인지 검사
+  var lengthChk = word.length > 0 && word.length <= 1000;
+
+  var isAlphabet = false;
+  // 문자열 길이 만큼 하나씩 검사
+  for (var i = 0; i < word.length; i++) {
+    // 문자가 알파벳일 경우
+    if (
+      (word[i].charCodeAt() >= 65 && word[i].charCodeAt() <= 90) ||
+      (word[i].charCodeAt() >= 97 && word[i].charCodeAt() <= 122)
+    ) {
+      isAlphabet = true;
+      //문자가 알파벳이 아닐 경우
+    } else {
+      isAlphabet = false;
+      break;
+    }
+  }
+
+  return lengthChk && isAlphabet;
 }
 
 module.exports = problem4;
