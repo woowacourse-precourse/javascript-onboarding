69ab46e8510bdd8bff547f2fcb376844322160f9
HojinAn <asdv94@naver.com>
2022-11-01 08:33:03 +0900

    docs: problem 3 feature list

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,12 @@
+/**
+ * Problem 3
+ * 1. 1부터 number까지 숫자를 element로 갖는 배열 생성
+ * 2. join을 통해 하나의 문자열 변환
+ * 3. 문자열에서 3, 6, 9를 제거
+ * 4. 처음 문자열과 길이 비교 => 해당 값이 박수 횟수
+ * @param {number} number 1 이상 10,000 이하
+ * @return {number}
+ */
 function problem3(number) {
   var answer;
   return answer;
