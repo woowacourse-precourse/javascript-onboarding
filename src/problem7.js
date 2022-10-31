74afde15ba709723c5f9cba711676c0ac22240af
somin <smhs12@naver.com>
2022-10-31 17:42:11 +0900

    feat: problem7 / 이미 친구인 사람 찾는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,15 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userFriend = findFriend(user, friends);
 }
 
+function findFriend(user, friends) {
+  const userFriend = [];
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < friends[i].length; j++) {
+      if (friends[i].includes(user) && friends[i][j] !== user)
+        userFriend.push(friends[i][j]);
+    }
+  }
+  return userFriend;
+}
 module.exports = problem7;
