351bee7c8cddca2bbb3bb793665f453e3b29635d
oing-yong <74910908+oing-yong@users.noreply.github.com>
2022-10-31 20:18:09 +0900

    fix: 문제7 알고리즘 함수 수정
    - friendsAl(user, friends)함수 추가: {친구추천 배열 길이, 사용자와 현재 친구 배열, 친구추천 배열, 알고리즘 점수 배열}로 이루어진 객체 반환하는 함수
    - visitorAl(algorithm, visitors) 함수 추가: friendsAl에서 생성한 객체의 친구추천 배열 및 알고리즘 점수 배열에 visitor 정보를 추가하는 함수
    - pickFive(obj) 함수 수정 : 알고리즘 상위 5명을 반환함(알고리즘 점수가 같을 때는 오름차순으로 정렬하여 자름)

--- src/problem7.js
+++ src/problem7.js
@@ -1,48 +1,87 @@
 function problem7(user, friends, visitors) {
   let answer = [];
+  let algorithm = {};
+
+  algorithm = friendsAl(user, friends);
+  algorithm = visitorsAl(algorithm, visitors);
+
+  answer = pickFive(algorithm);
+
+  return answer;
+}
+
+
+function friendsAl(user, friends) {
   let friendAndMe = [user];
-  let algorithmArr = [];
+  let algorithmName = [];
+  let algorithmNum = [];
 
   for (let index = 0; index < friends.length; index++) {
     friendAndMe.push(friends[index][0]);
 
-    let overlapIdx = algorithmArr.indexOf(friends[index][1]);
+    let overlapIdx = algorithmName.indexOf(friends[index][1]);
     if (friendAndMe.indexOf(friends[index][1]) != -1) {
       continue;
-    } else if (overlapIdx != -1 && friends[index][1].length === algorithmArr[overlapIdx].length) {
-      algorithmArr[overlapIdx + 1] += 10;
+    } else if (overlapIdx != -1 && friends[index][1].length === algorithmName[overlapIdx].length) {
+      algorithmNum[overlapIdx] += 10;
       continue;
     }
-    algorithmArr.push(friends[index][1], 10);
+    algorithmName.push(friends[index][1]);
+    algorithmNum.push(10);
   }
 
+  return {
+    length: algorithmName.length,
+    curFriend: friendAndMe,
+    nameArr: algorithmName,
+    scoreArr: algorithmNum,
+  };
+}
+
+function visitorsAl(obj, visitors) {
   for (index = 0; index < visitors.length; index++) {
-    let overlapIdx = algorithmArr.indexOf(visitors[index]);
+    let overlapIdx = obj.nameArr.indexOf(visitors[index]);
 
-    if (friendAndMe.indexOf(visitors[index]) != -1) {
+    if (obj.curFriend.indexOf(visitors[index]) != -1) {
       continue;
-    } else if (overlapIdx != -1 && visitors[index].length === algorithmArr[overlapIdx].length) {
-      algorithmArr[overlapIdx + 1] += 1;
+    } else if (overlapIdx != -1 && visitors[index].length === obj.nameArr[overlapIdx].length) {
+      obj.scoreArr[overlapIdx] += 1;
       continue;
     }
-    algorithmArr.push(visitors[index], 1);
+    obj.nameArr.push(visitors[index]);
+    obj.scoreArr.push(1);
   }
 
-  algorithmArr.sort(function (a, b) {
-    if (a > b) return 1;
-    if (a === b) return 0;
-    if (a < b) return -1;
-  });
+  return obj;
+}
 
-  for (index = 0; index < 5; index++) {
-    if (algorithmArr[index]) {
-      break;
+function pickFive(obj) {
+  const name = obj.nameArr;
+  const score = obj.scoreArr;
+  const set = new Set(score);
+  let tmpScore = [...set];
+  let returnName = [];
+
+  while (tmpScore.length != 0) {
+    let tmpName = [];
+    let curMax = Math.max(...tmpScore);
+    tmpScore = tmpScore.filter(function (item) {
+      return item !== curMax;
+    });
+    for (let m = 0; m < score.length; m++) {
+      if (score[m] === curMax) {
+        tmpName.push(name[m]);
+      }
     }
-    answer.push(algorithmArr[index][0]);
+    //동일한 알고리즘 점수를 가진 사람들끼리 오름차순 정렬된 배열 만들어짐
+    returnName.push(tmpName.sort());
   }
-  answer = answer.sort();
-  console.log(answer);
-  return answer;
+
+  returnName = returnName.flat();
+  if (returnName.length > 5) {
+    return returnName.slice(0, 6);
+  }
+  return returnName;
 }
 
 module.exports = problem7;
