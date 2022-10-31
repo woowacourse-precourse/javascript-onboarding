285e0c79e122980384e555826402a4de8605d97c
Bori-github <qhflrnfl4324@gmail.com>
2022-10-31 22:05:37 +0900

    ✨ problem2 > 연속하는 중복 문자 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,25 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (
+    cryptogram.length < 1 ||
+    cryptogram.length > 1000 ||
+    typeof cryptogram !== 'string'
+  ) {
+    return false;
+  }
+
+  var answer = [cryptogram[0]];
+
+  for (var i = 1; i < cryptogram.length; i++) {
+    var checkReduplication = answer[answer.length - 1];
+
+    if (checkReduplication === cryptogram[i]) {
+      answer.pop();
+      continue;
+    }
+    answer.push(cryptogram[i]);
+  }
+
+  return answer.join('');
 }
 
 module.exports = problem2;
