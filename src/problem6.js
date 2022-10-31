45ed29dde30076eac6b5f2c71a239569f3798ba2
HBSPS <wiyeong79@naver.com>
2022-10-31 18:56:17 +0900

    :technologist: [문제6] 메소드 기능 및 형식 설명용 주석 추가

--- src/problem6.js
+++ src/problem6.js
@@ -7,26 +7,36 @@ function problem6(forms) {
 }
 
 
-// 닉네임을 연속된 두 글자로 나누어 집합으로 만든 뒤, 중복 여부를 검사하는 메소드
-function checkNickname(arr) {
+/**
+ * 중복된 닉네임을 갖는 이메일로 구성된 집합을 구하는 메소드
+ * @param {string[]} forms 크루 정보를 담고있는 배열
+ * @returns {Set} 중복된 닉네임을 갖는 이메일로 구성된 집합
+ */
+function checkNickname(forms) {
   const overlapedNickname = new Set();
   let nicknameSet = new Set();
   let firstNickname = new Set(); // 첫 번째 닉네임을 확인하기 위해 첫 번째 닉네임을 두 글자씩 나누어 집합으로 저장
 
-  for (let i = 0; i < arr.length; i++) {
-    if (!validEmail(arr[i][0])) throw new Error("잘못된 이메일이 포함되어 있습니다.");
-    if (!validNickname(arr[i][1])) throw new Error("잘못된 닉네임이 포함되어 있습니다.");
-    if (checkOverlap(arr[i][1], nicknameSet)) overlapedNickname.add(arr[i][0]);
+  for (let i = 0; i < forms.length; i++) {
+    if (!validEmail(forms[i][0])) throw new Error("잘못된 이메일이 포함되어 있습니다.");
+    if (!validNickname(forms[i][1])) throw new Error("잘못된 닉네임이 포함되어 있습니다.");
+    if (checkOverlap(forms[i][1], nicknameSet)) overlapedNickname.add(forms[i][0]);
     
-    nicknameSet = sliceNickname(arr[i][1], nicknameSet);
+    nicknameSet = sliceNickname(forms[i][1], nicknameSet);
   }
 
-  firstNickname = sliceNickname(arr[0][1], firstNickname);
-  if (checkFirst) overlapedNickname.add(arr[0][0]); // 첫번째 닉네임에 중복되는 부분이 있다면 정답 집합에 추가
+  firstNickname = sliceNickname(forms[0][1], firstNickname);
+  if (checkFirst) overlapedNickname.add(forms[0][0]); // 첫번째 닉네임에 중복되는 부분이 있다면 정답 집합에 추가
 
   return overlapedNickname;
 }
-// 닉네임을 두 글자씩 잘라 중복 검사용 집합에 저장하는 메소드
+
+/**
+ * 닉네임을 두 글자씩 잘라 중복 검사용 집합에 저장하는 메소드
+ * @param {string} nickname 닉네임
+ * @param {Set} overlapedSet 중복 검사용 집합 (해당 닉네임 추가 전)
+ * @returns {Set} 중복 검사용 집합 (해당 닉네임 추가 후)
+ */
 function sliceNickname(nickname, overlapedSet) {
   for (let i = 2; i <= nickname.length; i++) {
     overlapedSet.add(nickname.slice(i-2, i));
@@ -34,7 +44,13 @@ function sliceNickname(nickname, overlapedSet) {
 
   return overlapedSet;
 }
-// 중복 검사용 집합과 비교하여 중복 닉네임이 있는지 검사하는 메소드
+
+/**
+ * 중복 닉네임이 있는지 검사하는 메소드
+ * @param {string} nickname 검사 대상 닉네임
+ * @param {Set} set 중복 검사용 집합
+ * @returns {boolean} boolean
+ */
 function checkOverlap(nickname, set) {
   for (let i = 2; i <= nickname.length; i++) {
     if (set.has(nickname.slice(i - 2, i))) return true; // 중복되는 문자열을 갖는다면 true 반환
@@ -44,17 +60,26 @@ function checkOverlap(nickname, set) {
 }
 
 
-// 첫번째 닉네임을 확인하기 위한 메소드
+/**
+ * 첫번째 닉네임을 확인하기 위한 메소드
+ * @param {string[]} arr 전체 닉네임 배열
+ * @param {*} set 첫번째 닉네임을 두 글자씩 자른 것으로 구성된 집합
+ * @returns {boolean} boolean
+ */
 function checkFirst(arr, set) {
   for (let i = 1; i < arr.length; i++) {
-    if (checkOverlap(arr[i][1], set)) return true;
+    if (checkOverlap(arr[i][1], set)) return true; // 첫번째 닉네임을 제외한 닉네임들 중, 첫번째 닉네임과 중복된 부분이 있다면 true 반환
   }
 
   return false;
 }
 
 
-// 중복 제거용 집합을 배열로 변환하는 메소드
+/**
+ * 집합을 배열로 변환 후 정렬하는 메소드
+ * @param {Set} set 집합
+ * @returns {Array} 정렬된 배열
+ */
 function changeSetToArray(set) {
   const arr = Array.from(set);
 
