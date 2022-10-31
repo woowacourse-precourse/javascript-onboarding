44c1eac93423fe12b338fb45041d4b46ccd1edd1
2wndrhs <2wndrhs@gmail.com>
2022-10-31 03:58:11 -0700

    feat(PROBLEM6): 크루들의 닉네임을 두 글자로 분리하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,33 @@
+// 크루들의 닉네임을 두 글자로 분리하는 기능
+function splitNickname(forms) {
+  const splittedName = forms
+    .map((crew) => crew[1])
+    .reduce((acc, nickname) => {
+      for (let i = 0; i < nickname.length - 1; i++) {
+        acc.push(nickname.slice(i, i + 2));
+      }
+      return acc;
+    }, []);
+  return splittedName;
+}
+
+// solution
+function solution(forms) {
+  const splittedName = splitNickname(forms);
+  console.log(splittedName);
+}
+
 function problem6(forms) {
   var answer;
   return answer;
 }
+
+solution([
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+]);
 
 module.exports = problem6;
