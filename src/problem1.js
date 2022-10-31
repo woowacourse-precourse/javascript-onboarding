403b4f574733f340a44d43e3b04865a5a42416ae
Jeonjunwoo <tira618@naver.com>
2022-10-31 15:53:58 +0900

    각 페이지를 더하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -2,5 +2,14 @@ function problem1(pobi, crong) {
   var answer;
   return answer;
 }
+function sum(n){    //배열의 값을 각각 더하는 함수
+  let answer = 0
+  n = n.toString()
+  for (let i = 0; i < n.length; i++){
+    answer += parseInt(n[i])
+  }
+  return answer; 
+}
+
 
 module.exports = problem1;
