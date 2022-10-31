1c1861b052c9e760008b0be45dd49297559283c5
Kim Yeri <98sena@naver.com>
2022-11-01 01:35:27 +0900

    Solved Prob3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let cnt = 0;
+  for (let n = 1; n < number + 1; n++) {
+    let now = n;
+    while (now != 0) {
+      if (now % 10 === 3 || now % 10 === 6 || now % 10 === 9) cnt += 1;
+      now = Math.floor(now / 10);
+    }
+  }
+  return cnt;
 }
-
 module.exports = problem3;
