13d998220e9e8c0bec7bd3679de4684d56f96020
sunny <huiseon37@gmail.com>
2022-11-01 02:18:42 +0900

    [problem7] 친구 관계 map 생성 함수 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
+function getFriendMap(friends){
+  const friendMap = new Map()
+  
+  friends.forEach(([firstName,secondName])=>{
+    const firstPrevValue = friendMap.get(firstName);
+    const secondPrevValue = friendMap.get(secondName);
+    
+    firstPrevValue? friendMap.set(firstName, [...firstPrevValue, secondName]) : friendMap.set(firstName, [secondName]);
+    secondPrevValue? friendMap.set(secondName, [...secondPrevValue, firstName]) : friendMap.set(secondName, [firstName]);
+  });
+
+  return friendMap;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+
+  // var answer;
+  // return answer;
 }
 
 module.exports = problem7;
