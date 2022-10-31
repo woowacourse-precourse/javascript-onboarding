fcebd2e31d72a39e3a0d8cfdc17fc5759fe89053
oseunghyun <ohsh1477@gmail.com>
2022-10-31 18:56:11 +0900

    fix(p7): 방문자나 친구가 0인 경우에 대한 예외 처리 추가함

--- src/problem7.js
+++ src/problem7.js
@@ -3,32 +3,38 @@ const scoreFromMutual = (user, friendsArr) => {
   let friendsWithUser = friendsArr.filter((friends) => {
     return friends[0] == user || friends[1] == user;
   });
-  friendsWithUser = friendsWithUser.reduce((prev, curr) => prev.concat(curr));
-  friendsWithUser = friendsWithUser.filter((friends) => {
-    return friends != user;
-  });
+  if (friendsWithUser.length != 0) {
+    friendsWithUser = friendsWithUser.reduce((prev, curr) => prev.concat(curr));
+    friendsWithUser = friendsWithUser.filter((friends) => {
+      return friends != user;
+    });
+  }
 
   let friendsNotWithUser = friendsArr.filter((friends) => {
     return friends[0] != user && friends[1] != user;
   });
 
   let mutualObj = {};
-  for (i = 0; i < friendsWithUser.length; i++) {
-    for (j = 0; j < friendsNotWithUser.length; j++) {
-      if (friendsWithUser[i] == friendsNotWithUser[j][0]) {
-        if (Object.keys(mutualObj).includes(friendsNotWithUser[j][1])) {
-          mutualObj[friendsNotWithUser[j][1]] += 10;
-        } else {
-          mutualObj[friendsNotWithUser[j][1]] = 10;
-        }
-      } else if (friendsWithUser[i] == friendsNotWithUser[j][1]) {
-        if (Object.keys(mutualObj).includes(friendsNotWithUser[j][0])) {
-          mutualObj[friendsNotWithUser[j][0]] += 10;
-        } else {
-          mutualObj[friendsNotWithUser[j][0]] = 10;
+  if (friendsWithUser.length != 0) {
+    for (i = 0; i < friendsWithUser.length; i++) {
+      for (j = 0; j < friendsNotWithUser.length; j++) {
+        if (friendsWithUser[i] == friendsNotWithUser[j][0]) {
+          if (Object.keys(mutualObj).includes(friendsNotWithUser[j][1])) {
+            mutualObj[friendsNotWithUser[j][1]] += 10;
+          } else {
+            mutualObj[friendsNotWithUser[j][1]] = 10;
+          }
+        } else if (friendsWithUser[i] == friendsNotWithUser[j][1]) {
+          if (Object.keys(mutualObj).includes(friendsNotWithUser[j][0])) {
+            mutualObj[friendsNotWithUser[j][0]] += 10;
+          } else {
+            mutualObj[friendsNotWithUser[j][0]] = 10;
+          }
         }
       }
     }
+  } else {
+    mutualObj = {};
   }
   return [friendsWithUser, mutualObj];
 };
@@ -53,26 +59,35 @@ function problem7(user, friends, visitors) {
   let userFriends;
   let scoreFromMutualObj = scoreFromMutual(user, friends)[1];
   let scoreFromVisitObj = scoreFromVisit(visitors);
-  if (Object.keys(scoreFromVisitObj).length != 0) {
-    for (i = 0; i < Object.keys(scoreFromVisitObj).length; i++) {
-      for (j = 0; j < Object.keys(scoreFromMutualObj).length; j++) {
-        if (
-          Object.keys(scoreFromVisitObj)[i] ==
-          Object.keys(scoreFromMutualObj)[j]
-        ) {
-          candidateFriends[Object.keys(scoreFromVisitObj)[i]] =
-            Object.values(scoreFromVisitObj)[i] +
-            Object.values(scoreFromMutualObj)[j];
-        } else {
-          candidateFriends[Object.keys(scoreFromVisitObj)[i]] =
-            Object.values(scoreFromVisitObj)[i];
-          candidateFriends[Object.keys(scoreFromMutualObj)[j]] =
-            Object.values(scoreFromMutualObj)[j];
+
+  if (Object.keys(scoreFromVisitObj).length == 0) {
+    if (Object.keys(scoreFromMutualObj).length == 0) {
+      candidateFriends = {};
+    } else {
+      candidateFriends = scoreFromMutualObj;
+    }
+  } else {
+    if (Object.keys(scoreFromMutualObj).length == 0) {
+      candidateFriends = scoreFromVisitObj;
+    } else {
+      for (i = 0; i < Object.keys(scoreFromVisitObj).length; i++) {
+        for (j = 0; j < Object.keys(scoreFromMutualObj).length; j++) {
+          if (
+            Object.keys(scoreFromVisitObj)[i] ==
+            Object.keys(scoreFromMutualObj)[j]
+          ) {
+            candidateFriends[Object.keys(scoreFromVisitObj)[i]] =
+              Object.values(scoreFromVisitObj)[i] +
+              Object.values(scoreFromMutualObj)[j];
+          } else {
+            candidateFriends[Object.keys(scoreFromVisitObj)[i]] =
+              Object.values(scoreFromVisitObj)[i];
+            candidateFriends[Object.keys(scoreFromMutualObj)[j]] =
+              Object.values(scoreFromMutualObj)[j];
+          }
         }
       }
     }
-  } else {
-    candidateFriends = scoreFromMutualObj;
   }
 
   userFriends = scoreFromMutual(user, friends)[0];
@@ -112,3 +127,19 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+problem7(
+  "hello",
+  [
+    ["andole", "jun"],
+    ["andole", "bedi"],
+    ["jun", "shakevan"],
+    ["jun", "kane"],
+    ["jun", "sam"],
+    ["bedi", "shakevan"],
+    ["bedi", "anne"],
+    ["bedi", "sam"],
+    ["anne", "mrko"],
+  ],
+  ["donut", "anne", "mrko", "mrko", "sam"]
+);
