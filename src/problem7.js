b0c16062047b8dc7ed5c7a26a67b766dfa89161b
seongminn <berry9901@gmail.com>
2022-10-31 23:14:17 +0900

    feat(problem7): 유저의 친구 목록을 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,14 @@
+function getUserFriends(user, friends) {
+  const userFriends = friends
+    .filter((friends) => friends.includes(user))
+    .map((friend) => (friend[0] === user ? friend[1] : friend[0]));
+
+  return userFriends;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const userFriends = getUserFriends(user, friends);
+
   return answer;
 }
 
