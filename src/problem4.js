41fcea6807242d259bf6008fc239ff32dd1359f3
nara04040 <nara04040@naver.com>
2022-10-31 17:01:32 +0900

    [problem4] feat : 엄마의단어와 청개구리 단어를 구한다

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,13 @@
 function problem4(word) {
   var answer = "";
+  for (let i = 0; i < word.length; i++) {
+    const momChar = word[i];
+    const momCharIdx = word[i].charCodeAt(0);
+    const startLowerCharIdx = "a".charCodeAt(0);
+    const endLowerCharIdx = "z".charCodeAt(0);
+    const startUpperCharIdx = "A".charCodeAt(0); // 65
+    const endUpperCharIdx = "Z".charCodeAt(0); // 90
+  }
   return answer;
 }
 
