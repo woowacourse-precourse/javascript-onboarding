783cc68d92e3ae16fa84db85419898746f75def7
taeyoung <rlaxodud911@gmail.com>
2022-10-31 19:03:19 +0900

    Comment: problem2 함수 주석 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,8 @@
+/**
+ * 암호문을 해독하여 반환합니다.
+ * @param {string} cryptogram 해독 할 암호문입니다.
+ * @returns {string} 중복 문자가 제거 된 문자열을 반환합니다.
+ */
 function problem2(cryptogram) {
   let answer = cryptogram;
   const regex = /(.+)\1/;
