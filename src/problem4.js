f5c2be1a88dee32be2a16dc253f2e4d7f8fb4b4a
d-charlie-kim <dokkim@student.42seoul.kr>
2022-10-31 23:11:01 +0900

    Feat: <problem3> 배열 이용 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,23 @@
+function changeWords(word) {
+  let upperCase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  let upperCaseReverse = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+  let lowerCase = "abcdefghijklmnopqrstuvwxyz";
+  let lowerCaseReverse = "zyxwvutsrqponmlkjihgfedcba";
+  let reverseWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] == word[i].toUpperCase())
+      reverseWord += upperCaseReverse[upperCase.indexOf(word[i])];
+    else if (word[i] == word[i].toLowerCase())
+      reverseWord += lowerCaseReverse[lowerCase.indexOf(word[i])];
+    else
+      reverseWord += word[i];
+  }
+  return reverseWord;
+}
+
 function problem4(word) {
-  var answer;
+  let answer = changeWords(word);
   return answer;
 }
 
