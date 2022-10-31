75816b101bdfc2560cdf8e8fcf27e544d0a94a95
kwh97 <zzzang0512@naver.com>
2022-10-31 15:50:49 +0900

    feat(problem3): 문자열의 인덱스를 확인하여 3, 6, 9가 포함될 경우 박수 카운트를 증가시키는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,29 @@
 function problem3(number) {
   let answer = 0;
+
   for(let i=1; i<=number; i++) {
     let str = changeToString(i);
+    answer = answer + getClapCount(str);
   }
+
   return answer;
 }
 
 function changeToString(i) {
   const strI = String(i);
   return strI;
+}
+
+function getClapCount(str) {
+  let clapCount = 0;
+
+  for(let i=0; i<str.length; i++) {
+    if(str[i] % 10 === 3 || str[i] % 10 === 6 || str[i] % 10 === 9) {
+      clapCount++;
+    }
+  }
+
+  return clapCount;
 }
 
 module.exports = problem3;
