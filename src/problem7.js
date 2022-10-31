15d2d7df96263bee2934ebff955aa06d636b6801
Dongkeun <hongdongk@naver.com>
2022-10-31 20:33:48 +0900

    problem7: 유저의 친구찾기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  
+  const userfriends = [];
+
+  friends.filter((a) => a.includes(user)).filter((a) => {
+    for (let i = 0; i < a.length; i++) {
+      if (a[i] !== user) userfriends.push(a[i]);
+    }
+  });
+
 }
 
 module.exports = problem7;
