dc8fe6008b25e59e7b9d1d45c4f68f21e9835bef
Saans <92422813+saans808@users.noreply.github.com>
2022-10-31 16:08:59 +0900

    [p1] 숫자 하나씩 분리 후 합과 곱중 더 큰 수 리턴

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,19 @@
 function problem1(pobi, crong) {
+  
   var answer;
+
   function plusOrMultiply(name) {
     let condition = (name[0] + 1 == name[1]) && (name[0] % 2 == 1 && name[1] % 2 == 0);
-    
+    if (condition == true) {
+      let tmpArr = name.join('');
+      const toNumArr = (a) => Number(a);
+      tmpArr = Array.from(tmpArr, toNumArr);
+      const sum = tmpArr.reduce((a,b) => (a+b));
+      const multiply = tmpArr.reduce((a, b) => (a*b));
+      let num = sum > multiply ? sum : multiply;
+      return num;
+    } else {
+      return false;
   }
   return answer;
 }
