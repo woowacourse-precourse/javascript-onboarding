5dcb7c3a830a3fa97d612e55449f43d6dd2ce9a5
knh6269 <knh6269@gmail.com>
2022-10-31 22:48:09 +0900

    feat : 미션5) 주어진 돈을 금액 단위별로 계산해주는 함수

--- src/problem5.js
+++ src/problem5.js
@@ -6,6 +6,18 @@ function problem5(money) {
     money=money % count;
   }
 
+  //주어진 돈을 금액 단위별로 계산해주는 함수
+  const calculateMoney = (count) =>{
+      const many = Math.floor(money / count); //단위 별 필요한 갯수
+      disposeMoney(many, count);
+     
+  }
+  
+  //금액 단위별로 계산
+  for(let amount of amounts){
+    calculateMoney(amount);
+  }
+ 
   return answer;
 }
 
