b9086d741216068b51e6c8a4002fad00b3835766
spsp8426 <spsp8426@naver.com>
2022-10-31 14:23:40 +0900

    2번 문제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer = cryptogram.split("");
+  for (let i = 0; i < answer.length; i++) {
+    //console.log(answer[i]);
+    if (answer[i] === answer[i - 1]) {
+      let stock = 1;
+      let j = i;
+      // console.log(j)
+      while (answer[j] === answer[j - 1]) {
+        stock++;
+        j++;
+        //console.log(j)
+      }
+      answer.splice(i - 1, stock);
+      i = 0;
+      console.log(answer);
+    }
+  }
+  answer = answer.join("");
   return answer;
 }
-
+console.log(problem2("browoanoommnaon"));
 module.exports = problem2;
