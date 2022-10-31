83fa3ddfa6f1b85f29a9f3bc2b26aeabbfeecf06
Bori-github <qhflrnfl4324@gmail.com>
2022-10-31 20:47:22 +0900

    ✨ problem1 > 최대값 비교 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,37 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const maxNum = (pageArr) => {
+    const resultArr = [];
+
+    for (let i = 0; i < pageArr.length; i++) {
+      const sum = pageArr[i]
+        .toString()
+        .split('')
+        .reduce((prev, curr) => prev + parseInt(curr), 0);
+      const multiply = pageArr[i]
+        .toString()
+        .split('')
+        .reduce((prev, curr) => prev * parseInt(curr), 1);
+
+      resultArr.push(sum);
+      resultArr.push(multiply);
+    }
+
+    return Math.max(...resultArr);
+  };
+
+  if (maxNum(pobi) === maxNum(crong)) {
+    return 0;
+  }
+
+  if (maxNum(pobi) > maxNum(crong)) {
+    return 1;
+  }
+
+  if (maxNum(pobi) < maxNum(crong)) {
+    return 2;
+  }
+
+  return -1;
 }
 
 module.exports = problem1;
