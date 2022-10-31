d46b4f6e735513cb048df7c940ab360cae65c993
frants-jeon <jjh6963@naver.com>
2022-11-01 03:41:22 +0900

    feat (problem6): 중복된 닉네임을 객체에 저장하는 함수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -18,6 +18,17 @@ class RenamingTargetListMaker {
         (this.nickNameChecker[partOfNickName] || 0) + 1;
     }
   };
+
+  setTargetEmailList = (email, nickName) => {
+    if (!this.didDuplicationCheck)
+      return new Error("닉네임 중복체크를 먼저 진행해야 합니다.");
+
+    for (let i = 0; i < nickName.length - 1; i++) {
+      const partOfNickName = nickName[i] + nickName[i + 1];
+      if (this.nickNameChecker[partOfNickName] > 1)
+        this.targetEmailList.add(email);
+    }
+  };
 }
 
 module.exports = problem6;
