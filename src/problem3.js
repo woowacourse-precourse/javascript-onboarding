ed10f4c3d4c2c5b42aea890989b3ef5bc8c04871
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-31 23:36:46 +0900

    refactor(문제3): 박수 원시데이터 상수화

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,10 @@
+const CLAP_NUMBER = [3, 6, 9];
+
 function problem3(number) {
   let count = 0;
   for (let i = 1; i <= number; i += 1) {
     for (let n of i.toString())
-      if ([3, 6, 9].includes(Number(n))) count += 1;
+      if (CLAP_NUMBER.includes(Number(n))) count += 1;
   }
 return count;
 }
