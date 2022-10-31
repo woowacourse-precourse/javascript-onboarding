a7558847a47b503d7f7f52a21b59985e45750c99
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-31 23:46:12 +0900

    refactor(문제3): 특정 숫자의 박수를 구하는 함수 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,16 @@
 const CLAP_NUMBER = [3, 6, 9];
 
+const countClap = (number) => {
+  let count = 0;
+  for (let n of number.toString())
+    if (CLAP_NUMBER.includes(Number(n))) count += 1;
+  return count;
+}
+
 function problem3(number) {
   let count = 0;
   for (let i = 1; i <= number; i += 1) {
-    for (let n of i.toString())
-      if (CLAP_NUMBER.includes(Number(n))) count += 1;
+    count += countClap(i);
   }
 return count;
 }
