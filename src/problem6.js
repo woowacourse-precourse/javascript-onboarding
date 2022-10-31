d3de9b916859c7b5b45566ca1dd8ffba03dd3e8a
박겸영 <yong_drg@naver.com>
2022-10-31 19:11:55 +0900

    feat: 문제 6 모든 닉네임을 연속하는 2글자로 쪼개기 - 기능 1: 닉네임을 두 글자씩 연속하는 문자열로 쪼개어 배열에 담기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const nicknames = forms.map((form) => form[1]);
+  const twoLetters = [];
+
+  nicknames.forEach((nickname) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      twoLetters.push(nickname[i] + nickname[i + 1]);
+    }
+  });
 }
 
 module.exports = problem6;
