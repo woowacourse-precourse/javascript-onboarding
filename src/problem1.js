1efb210c2ee8593afd7383a8619297437c3abdbb
dkile <dkile532@naver.com>
2022-11-01 02:54:37 +0900

    refactor: extract result state

--- src/problem1.js
+++ src/problem1.js
@@ -47,25 +47,36 @@ function problem1(pobi, crong) {
   const MIN_PAGE = 2;
   const MAX_PAGE = 399;
 
-  if (pobi === undefined || crong === undefined) return -1;
-  if (pobi.length !== ARR_LENGTH || crong.length !== ARR_LENGTH) return -1;
+  /**
+   * @type {{POBI_WIN: 1, CRONG_WIN: 2, DRAW: 0, EXCEPTION: -1}}
+   */
+  const resultState = {
+    POBI_WIN: 1,
+    CRONG_WIN: 2,
+    DRAW: 0,
+    EXCEPTION: -1,
+  };
+
+  if (pobi === undefined || crong === undefined) return resultState.EXCEPTION;
+  if (pobi.length !== ARR_LENGTH || crong.length !== ARR_LENGTH)
+    return resultState.EXCEPTION;
   if (minus(pobi[0], pobi[1]) !== -1 || minus(crong[0], crong[1]) !== -1)
-    return -1;
+    return resultState.EXCEPTION;
   if (
     pobi[0] < MIN_PAGE ||
     pobi[1] > MAX_PAGE ||
     crong[0] < MIN_PAGE ||
     crong[1] > MAX_PAGE
   )
-    return -1;
-  if (isEven(pobi[0]) || isEven(crong[0])) return -1;
+    return resultState.EXCEPTION;
+  if (isEven(pobi[0]) || isEven(crong[0])) return resultState.EXCEPTION;
 
   const pobiScore = calculateMyScore(pobi);
   const crongScore = calculateMyScore(crong);
 
-  if (pobiScore > crongScore) return 1;
-  else if (pobiScore < crongScore) return 2;
-  else return 0;
+  if (pobiScore > crongScore) return resultState.POBI_WIN;
+  else if (pobiScore < crongScore) return resultState.CRONG_WIN;
+  else return resultState.DRAW;
 }
 
 module.exports = problem1;
