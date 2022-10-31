f2b147d2bbcb2b77ec9d6c552e77aa70b6769e44
choieastsea <choieastsea@gmail.com>
2022-11-01 07:30:55 +0900

    7) Making Basic Relation Object
    친구관계 그래프를 표현하기 위한 Object 초기화
    {이름 : [친구리스트], ... }의 Object 이용

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,35 @@
 function problem7(user, friends, visitors) {
   var answer;
+  var answer = [];
+  let relation_obj = {}; // {이름 : [친구들 이름] , ...}의 객체 만들기
+  for (let relationship of friends) {
+    let person0 = relationship[0];
+    let person1 = relationship[1];
+    let relation_of_person0 = relation_obj[person0];
+    let relation_of_person1 = relation_obj[person1];
+
+    relation_obj[person0] = relation_of_person0
+      ? [...relation_obj[person0], person1]
+      : [person1];
+    relation_obj[person1] = relation_of_person1
+      ? [...relation_obj[person1], person0]
+      : [person0];
+  }
+  console.log(relation_obj);
   return answer;
 }
 
 module.exports = problem7;
+
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
