313704545d29d370d4ddb48835977d17e9a34688
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 14:58:07 +0900

    feat: Problem 5 - 전체 기능 구현하기

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
-function problem5(money) {
-  var answer;
+const problem5 = money => {
+  const price = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = new Array(9).fill(0);
+
+  for (let i = 0; i < 9; i++) {
+    answer[i] += Math.floor(money / price[i]);
+    money = money % price[i];
+  }
+
   return answer;
 }
 
