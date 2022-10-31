119c6fdbd1bc3321e454ab80eac705ac95a31463
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 23:21:04 +0900

    feat: [4번 문제] 기준이 되는 알파벳 charCode 구하기

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
+const aCharCodeAt = "a".charCodeAt(0);
+const zCharCodeAt = "z".charCodeAt(0);
+const ACharCodeAt = "A".charCodeAt(0);
+const ZCharCodeAt = "Z".charCodeAt(0);
+const lowercase = aCharCodeAt + zCharCodeAt;
+const uppercase = ACharCodeAt + ZCharCodeAt;
+
 function problem4(word) {
-  var answer;
-  return answer;
+  return word;
 }
 
 module.exports = problem4;
