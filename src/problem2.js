4f6a3718a174f9efbd987acbd86b5bdf3250318d
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-10-31 21:13:10 +0900

    [2번 문제] feat: solution 메소드 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = "";
+  let count = 0;
+  while(count < cryptogram.length){
+    if(answer[answer.length-1] === cryptogram[count]){
+      answer = answer.substring(0,answer.length-1);
+    }
+    else{
+      answer += cryptogram[count];
+    }
+    count++;
+  }
   return answer;
 }
 
