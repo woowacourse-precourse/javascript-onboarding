4cb28b23c86b6f490234b2e101e9d5ecf2e9a7a3
bkdragon0228 <rlaqjarb2067@naver.com>
2022-10-31 14:05:52 +0900

    feat : problem2
    중복 문자를 제거하기 위해 배열을 stack처럼 사용했습니다.
    stack에 문자를 넣으면서 마지막 요소와 비교를 해서 같으면stack에서  빼주었습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+    const stack = [];
+
+    for (let x of cryptogram) {
+        if (stack[stack.length - 1] !== x) {
+            stack.push(x);
+        } else {
+            stack.pop();
+        }
+    }
+    return stack.join('');
 }
 
 module.exports = problem2;
