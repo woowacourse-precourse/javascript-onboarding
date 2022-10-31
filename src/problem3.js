ff8a95bae4c6d843e2580703a4b8cccf533a5eb4
Dongkeun <hongdongk@naver.com>
2022-10-31 20:23:36 +0900

    problem3: 3,6,9가 들어가는 횟수구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  
+  let count = 0;
+  
+  for (let i=1; i<=number; i++){
+    let arr = String(i).split('');   
+
+    for(let v= 0; v<arr.length; v++){
+      if(arr[v]=="3" || arr[v]=="6" || arr[v]=="9") count+=1;
+    };
+  };
+
+  return count;
 }
 
 module.exports = problem3;
