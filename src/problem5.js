9862cdeba1b883dbe4c8f5702c1ba053ec73bd7b
Sang-Mini <sangmin3062@gmail.com>
2022-10-31 22:44:24 +0900

    [problem5] 금액 단위별 화폐 배열을 가진 변수 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,9 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+
+  // 각 금액 단위별 화폐 배열을 가진 변수 선언
+  const won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
   return answer;
 }
 
