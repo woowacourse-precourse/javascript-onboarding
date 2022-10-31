6724ae80ee8a8c75e504de69e2b78c91f5a9c1f8
YMS <yms1789@naver.com>
2022-10-31 19:06:51 +0900

    new: problem2 미션 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  function sameSlice(text) {
+    let isDouble = false;
+    for (let i = 0; i < text.length - 1; i++) {
+      if (text[i] === text[i + 1]) {
+        text[i] = "";
+        text[i + 1] = "";
+        isDouble = true;
+      }
+    }
+    text = text.filter((ele) => ele !== "");
+    if (isDouble === false) {
+      return text.join("");
+    } else {
+      return sameSlice(text);
+    }
+  }
+  answer = sameSlice(Array.from(cryptogram));
   return answer;
 }
-
 module.exports = problem2;
