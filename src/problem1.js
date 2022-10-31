d6cb1229c7d8be4cb31159f818a1694da39c66b4
Jin-uu <78341764+Jin-uu@users.noreply.github.com>
2022-10-31 22:33:31 +0900

    (prob 1) 기능 목록 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,8 +1,16 @@
+/*
+기능 목록
+1. 전달받은 pobi, crong 내부 값 범위 검사 (각 1~400)
+2. 페이지 값에 따른 점수 계산
+3. pobi, crong의 승패 판정
+*/
+
+
 // pobi, crong => length : 2, type : int
 // return => pobi win : 1, crong win : 2, draw : 0, excep : -1
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+	var answer;
+	return answer;
 }
 
 module.exports = problem1;
