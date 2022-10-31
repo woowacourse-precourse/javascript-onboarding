b89e431655534edf67106e2f40624571abccf8ee
SorinKwon <ksl020700@gmail.com>
2022-10-31 10:51:23 +0900

    feat(problem2): 문자열 중 연속되는 중복 문자열을 삭제하는 기능 추가
    이전 인덱스를 반환하여 연속되는 중복 문자열을 삭제하는 기능을 추가함

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function sol(n){
+  let result=[];
+  let point=0;
+  result[0]=n[0];
+
+  for(let i=1;i<n.length;i++){
+    if(n[i]==result[point]){
+      point=point-1;
+    }
+  }
+  return result.slice(0,point+1);
 }
+function problem2(cryptogram) {
+  let answer;
+  answer=sol(cryptogram);
+
+  return answer.join('');
+}
+
 
 module.exports = problem2;
