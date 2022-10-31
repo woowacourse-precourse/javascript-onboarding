251afbc9ab5e54b87f99f1818345bdd29d7b5579
nara04040 <nara04040@naver.com>
2022-10-31 19:27:50 +0900

    [problem5] feat : 금액따라 나누고 거기에대한 몫을 출력한다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
+  const newArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const answer = [];
+  for (let i = 0; i < newArr.length; i++) {
+    answer.push(parseInt(money / newArr[i]));
+    money = money % newArr[i];
+  }
   return answer;
 }
 let money = 50237;
