568bd7ff48eac1b70c7eedb9d8e7f1c84c04eb7a
JangHoonSeok <orirori512@naver.com>
2022-11-01 02:27:33 +0900

    feat: problem5. problem5 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  const money_list = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (item of money_list) {
+    answer.push(Math.floor(money / item));
+    money %= item;
+  }
   return answer;
 }
 
