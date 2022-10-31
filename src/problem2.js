83ae2dc6a6a488da30a6257e3aa04eadbf58b493
chiman <rlaclghks123@naver.com>
2022-10-31 18:20:31 +0900

    feat: problem-2 isValid 함수 생성
    예외처리 함수 생성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
+function isValid(cryptogram) {
+  const regex = /^[a-z]+$/;
+
+  let str = cryptogram;
+
+  if (cryptogram.length < 1 || cryptogram.length > 1000) {
+    throw "1 이상 1000 이하인 글자수를 입력해주세요.";
+  }
+  if (!regex.test(str)) {
+    throw "알파벳 소문자로 입력해주세요";
+  }
+}
+
 function problem2(cryptogram) {
   var answer;
+  isValid(cryptogram);
+
   return answer;
 }
 
