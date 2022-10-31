369c3cc264ef54f385c29521166e45b61c31b75a
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-10-31 13:11:17 +0900

    problem 1 코드 설명 추가

--- src/problem1.js
+++ src/problem1.js
@@ -49,8 +49,10 @@ const checkVerified = (pages) => {
  * @returns {0 | 1 | 2 | -1}
  */
 const init = (pobi, crong) => {
-  const bestValueOfPobi = Math.max(...pobi.map((page) => findMaxOfPageGame(page))); // 포비의 베스트 숫자 찾기
-  const bestValueOfCrong = Math.max(...crong.map((page) => findMaxOfPageGame(page))); // 크롱의 베스트 숫자 찾기
+  const resultsOfPobi = pobi.map((page) => findMaxOfPageGame(page)); // 포비가 가진 페이지들의 게임 결과 값
+  const resultsOfCrong = crong.map((page) => findMaxOfPageGame(page)) // 크롱이 가진 페이지들의 게임 결과 값
+  const bestValueOfPobi = Math.max(...resultsOfPobi); // 포비의 베스트 숫자 찾기
+  const bestValueOfCrong = Math.max(...resultsOfCrong); // 크롱의 베스트 숫자 찾기
 
   //  포비가 이긴다면 1, 크롱이 이긴다면 2, 무승부는 0
   if (bestValueOfPobi === bestValueOfCrong) {
@@ -66,13 +68,14 @@ const init = (pobi, crong) => {
 }
 
 /**
- * 들어온 페이지 번호의 각 자리 숫자를 모두 더하거나, 모두 곱해 가장 큰 수를 구한다.
+ * 들어온 페이지 번호의 각 자리 숫자를 모두 더하거나 모두 곱한 후, 가장 큰 수를 구한다.
  * @param {number} page 계산하고 싶은 한 쪽 페이지 번호
  * @returns {number}
  */
 const findMaxOfPageGame = (page) => {
   // 받은 페이지의 각 자리 숫자를 리스트로 변환해주는 작업
-  const pageNumberList = page.toString().split('').map((num) => parseInt(num));
+  const pageStringList = page.toString().split('');
+  const pageNumberList = pageStringList.map((num) => parseInt(num));
   // 각 자리 숫자를 모두 더했을 때
   const addOfPage = pageNumberList.reduce((a, b) => a + b, 0);
   // 각 자리 숫자를 모두 곱했을 때
