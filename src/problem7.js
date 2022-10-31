eecfa614da8678028ee7c84d18eb7ba006316f45
Jeonjunwoo <tira618@naver.com>
2022-10-31 20:05:04 +0900

    입력값들을 배열에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -2,5 +2,18 @@ function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
+const userArr = [];
+const friendsArr = [];
+const visitorsArr = [];
 
+function sns(user, friends, visitors){
+  userArr.push(user);
+  function setPeople(people,peopleArr) {
+    for(i = 0; i < people.length; i++) {
+      peopleArr.push(people[i]);
+    }
+  }
+  const setFriends = new setPeople(friends,friendsArr);
+  const setVisitors = new setPeople(visitors,visitorsArr);
+}
 module.exports = problem7;
