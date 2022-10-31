042cc3c12c718dd2fc6cbac63b23d12cd47931f4
c0c0pang <coc1789@naver.com>
2022-10-31 18:04:16 +0900

    문제 3 코드 정렬

--- src/problem3.js
+++ src/problem3.js
@@ -8,19 +8,19 @@
 
 function problem3(number) {
   let answer = 0;
-  let num=0;
-    for(let i=1;i<=number;i++){
-        num=i;
-        while(num!==0){
-          if(num%10===3 || num%10===6 || num%10===9){
-              answer++;
-              num = Math.floor(num/10);
-          }
-          else{
-              num = Math.floor(num/10);
-          }
+  let num = 0;
+  for (let i = 1; i <= number; i++) {
+    num = i;
+    while (num !== 0) {
+      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+        answer++;
+        num = Math.floor(num / 10);
       }
-}
+      else {
+        num = Math.floor(num / 10);
+      }
+    }
+  }
   return answer;
 }
 
