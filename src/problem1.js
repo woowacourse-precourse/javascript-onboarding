cc8178300817ce56d88f8d7ed37b0dc4c835b422
rabbitmjh <rabbitmjh@naver.com>
2022-10-31 14:23:01 +0900

    [p1] 페이지 체크

--- src/problem1.js
+++ src/problem1.js
@@ -1,14 +1,32 @@
-function problem1(pobi, crong) {
-  var answer;
-
-  function plusSum(num){
-    var sum = 0;
-    while(num){
-      sum += num % 10;
-      sum /= 10;
-    }
-    return sum;
+function plusSum(num){
+  var sum = 0;
+  while(num){
+    sum += num % 10;
+    sum /= 10;
   }
+  return sum;
+}
+
+function multiSum(num){
+  var sum = 1;
+  while(num){
+    sum *= num % 10;
+    sum /= 10;
+  }
+  return sum;
+}
+
+function pageCheck(page){
+  const [left, right] = page;
+  if(left % 2 == 1 && 1 <= left && left <= 399 && right % 2 == 0 && 2 <= right && right <= 400)
+    return 1;
+  else
+    return 0;
+}
+
+function problem1(pobi, crong) {
+  var answer; 
+
 
   return answer;
 }
