2b08c52968d741f1b2f20c9f8c7aea51ea5121cf
ryukyunghye <ryukyung123@naver.com>
2022-10-31 22:28:55 +0900

    add: problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  const upperChar = 65 + 90;
+  const lowerChar = 97 + 122;
+  for (let i = 0; i < word.length; i++) {
+    if (word[i] === " ") {
+      answer += word[i];
+    } else {
+      var changChar = word[i].charCodeAt(0);
+      if (changChar < 97) {
+        answer += String.fromCharCode(upperChar - changChar);
+      } else {
+        answer += String.fromCharCode(lowerChar - changChar);
+      }
+    }
+  }
   return answer;
 }
-
 module.exports = problem4;
