75c5af4d0b806b559dd9f2b9a3bfc87fdd6e4aea
KINHYEONJI <hyeonji0718@gmail.com>
2022-10-31 13:09:19 +0900

    feat : countClap 함수 구현
    - 3,6,9 게임 규칙에 맞춰서 박수 횟수를 반환

--- src/problem3.js
+++ src/problem3.js
@@ -6,4 +6,11 @@ function splitNum(num) {
   let splitNumArr = num.toString().split("");
   return splitNumArr;
 }
+function countClap(splitNumArr) {
+  let clapCnt = 0;
+  for (let i = 0; i < splitNumArr.length; i++) {
+    if (splitNumArr[i] % 3 === 0 && splitNumArr[i] !== "0") clapCnt++;
+  }
+  return clapCnt;
+}
 module.exports = problem3;
