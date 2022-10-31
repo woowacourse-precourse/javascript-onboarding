e49c73634019b071f45a7181bc593e46ef998823
seoyoung-dev <seoyoung.dev@gmail.com>
2022-10-31 22:32:51 +0900

    refactor: problem 1 연산 기능 함수 통합

--- src/problem1.js
+++ src/problem1.js
@@ -1,35 +1,47 @@
 function problem1(pobi, crong) {
-    function chkException([p1, p2]) {
+    function checkException([p1, p2]) {
         if (p1 !== p2 - 1 || p1 === 1 || p2 === 400) return true;
         return false;
     }
 
-    function calPages([p1, p2]) {
+    const calculateDigits = {
+        add(arr) {
+            return arr.reduce((sum, cur) => sum + cur);
+        },
+        multiply(arr) {
+            return arr.reduce((sum, cur) => sum * cur);
+        }
+    };
+
+    function convertPageIntoArray(num) {
+        return num
+            .toString()
+            .split("")
+            .map((ele) => parseInt(ele));
+    }
+
+    function calculatePages([p1, p2]) {
         let results = [];
-        let p1Digits = p1
-            .toString()
-            .split("")
-            .map((ele) => parseInt(ele));
-        let p2Digits = p2
-            .toString()
-            .split("")
-            .map((ele) => parseInt(ele));
-        results.push(p1Digits.reduce((sum, cur) => sum + cur));
-        results.push(p2Digits.reduce((sum, cur) => sum + cur));
-        results.push(p1Digits.reduce((sum, cur) => sum * cur));
-        results.push(p2Digits.reduce((sum, cur) => sum * cur));
+        let p1Digits = convertPageIntoArray(p1);
+        let p2Digits = convertPageIntoArray(p2);
+
+        results.push(calculateDigits.add(p1Digits));
+        results.push(calculateDigits.add(p2Digits));
+        results.push(calculateDigits.multiply(p1Digits));
+        results.push(calculateDigits.multiply(p2Digits));
+
         return Math.max(...results);
     }
 
-    if (chkException(pobi)) return -1;
-    if (chkException(crong)) return -1;
+    if (checkException(pobi)) return -1;
+    if (checkException(crong)) return -1;
 
-    let pobiMaxNum = calPages(pobi);
-    let crongMaxNum = calPages(crong);
+    const pobiMaxNumber = calculatePages(pobi);
+    const crongMaxNumber = calculatePages(crong);
 
-    if (pobiMaxNum === crongMaxNum) {
+    if (pobiMaxNumber === crongMaxNumber) {
         return 0;
-    } else if (pobiMaxNum > crongMaxNum) {
+    } else if (pobiMaxNumber > crongMaxNumber) {
         return 1;
     } else {
         return 2;
