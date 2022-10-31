8d216d924dcfa019a843307c73635bf323bbb07b
Inae Choi <inaechoi89@gmail.com>
2022-10-31 14:40:03 +0900

    feat(p7): add a function to get user's friend list

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,21 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  let scores = {};
+
+  function getUserFriend(user, friends) {
+    let _userFriends = [];
+    friends.forEach((friendship) => {
+      if (friendship.includes(user)) {
+        friendship.forEach((friendship) => {
+          if (friendship != user) _userFriends.push(friendship);
+        });
+      }
+    });
+    return _userFriends;
+  }
+  let userFriends = getUserFriend(user, friends);
+
   return answer;
 }
 
