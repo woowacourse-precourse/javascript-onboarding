f088b0a0957e28a784991ad9caa20d57833f7b91
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:27:29 +0900

    [ feat ] problem5_화폐종류 & answer을 담을 배열 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
+// 기능 구현 목록
+// - 화폐종류 & answer 을 담을 배열 생성
+// - 남은돈을 화폐로 변환하는 함수 생성
+// - 큰 금액부터 화폐로 변환
+// - 1원 화폐 개수 추가
+
 function problem5(money) {
-  var answer;
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10];
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
   return answer;
 }
 
