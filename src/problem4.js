ee0476d724269bea91c6d3153ccc48c7a493dd24
seunghoria <102285042+seunghoria@users.noreply.github.com>
2022-10-31 21:08:58 +0900

    refactor: PROBLEM 4 리팩토링

--- src/problem4.js
+++ src/problem4.js
@@ -1,27 +1,27 @@
+const check = (word) => {
+  if (word.length < 1 || 1000 < word.length) return false;
+  return true;
+}
+
+const reverseWord = (word) => {
+  let answer;
+  let wordArray = Array.from(word)
+  let reverseArray = []
+  wordArray.map((element) => {
+    let charCode = element.charCodeAt(0)
+    if (charCode >= 97 && charCode <= 122) {
+      charCode = 219 - charCode
+    }
+    if (charCode >= 65 && charCode <= 90) {
+      charCode = 155 - charCode
+    }
+    reverseArray.push(String.fromCharCode(charCode));
+  })
+  answer = reverseArray.join("")
+  return answer;
+}
+
 function problem4(word) {
-  const check = (word) => {
-    if (word.length < 1 || 1000 < word.length) return false;
-    return true;
-  }
-
-  const reverseWord = (word) => {
-    let answer;
-    let wordArray = Array.from(word)
-    let reverseArray = []
-    wordArray.map((element) => {
-      let charCode = element.charCodeAt(0)
-      if (charCode >= 97 && charCode <= 122) {
-        charCode = 219 - charCode
-      }
-      if (charCode >= 65 && charCode <= 90) {
-        charCode = 155 - charCode
-      }
-      reverseArray.push(String.fromCharCode(charCode));
-    })
-    answer = reverseArray.join("")
-    return answer;
-  }
-
   let result = reverseWord(word);
 
   if (!check(word)) { throw Error("1이상 1000이하의 문자열을 입력해주세요.") }
