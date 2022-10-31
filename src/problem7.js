54e9180cc8089c9ea01e04876d169be1b6235273
연 <mogoo22@naver.com>
2022-10-31 15:25:32 +0900

    feat(문제7): 사용자와 함께 아는 친구는 10점 방문자는 1점을 부여한다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,18 @@
+const getScore = () => {
+  const scoreList = [];
+
+  const calcScore = (user, score) => {
+    const userScore = scoreList.find((list) => list.user === user);
+    if (userScore) {
+      userScore.score += score;
+      return;
+    }
+    scoreList.push({ user, score });
+  };
+
+  return { scoreList, calcScore };
+};
+
 const getUserFriends = (friendList, user) => {
   const userFriends = [];
 
@@ -10,7 +25,24 @@ const getUserFriends = (friendList, user) => {
 };
 
 function problem7(user, friends, visitors) {
+  const { scoreList, calcScore } = getScore();
   const userFriends = getUserFriends(friends, user);
+  const isUserFriends = (nickname) => userFriends.includes(nickname);
+
+  friends.forEach(([userA, userB]) => {
+    if (userA === user || userB === user) return;
+    if (isUserFriends(userA) && !isUserFriends(userB)) {
+      calcScore(userB, 10);
+    }
+    if (isUserFriends(userB) && !isUserFriends(userA)) {
+      calcScore(userA, 10);
+    }
+  });
+
+  visitors.forEach((visitor) => {
+    if (!isUserFriends(visitor)) calcScore(visitor, 1);
+  });
+
 }
 
 module.exports = problem7;
