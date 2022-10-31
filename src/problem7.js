d93fb610c6be559d2832d90d99246fd628dd5d93
Yeom-JinHo <pwlsghq@naver.com>
2022-10-31 23:56:12 +0900

    feat : [P7] 함께 아는 친구 유저리스트 판별

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const recommendUser = new Map();
+  const friendMap = new Map();
+  friends.forEach((friend) => {
+    if (friendMap.has(friend[0])) {
+      friendMap.set(friend[0], [...friendMap.get(friend[0]), friend[1]]);
+    } else {
+      friendMap.set(friend[0], [friend[1]]);
+    }
+
+    if (friendMap.has(friend[1])) {
+      friendMap.set(friend[1], [...friendMap.get(friend[1]), friend[0]]);
+    } else {
+      friendMap.set(friend[1], [friend[0]]);
+    }
+  });
+
+  return recommendUser;
 }
 
 module.exports = problem7;
