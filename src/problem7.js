d57afd5af35ed93ea04fb726ad8872baf05b030f
yesolyo <ahnyelim77@gmail.com>
2022-10-31 18:35:08 +0900

    user의 친구 아이디를 찾는다

--- src/problem7.js
+++ src/problem7.js
@@ -9,7 +9,13 @@ function problem7(user, friends, visitors) {
   //friends에서 사용자인 mrko를 찾는다
   for (let i = 0; i < friends.length; i++) {
     if (user == friends[i][0]) {
+      //mrko친구 아이디B를 찾는다
+      user_friends[n] = friends[i][1];
+      n += 1;
     } else if (user == friends[i][1]) {
+      //mrko친구 아이디A를 찾는다
+      user_friends[n] = friends[i][0];
+      n += 1;
     } else {
       continue;
     }
