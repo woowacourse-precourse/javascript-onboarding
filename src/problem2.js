5d6d717ba33dda617ad7ce947f30d666a8991745
Hwang Seong Taek <hst007@naver.com>
2022-10-31 19:27:04 +0900

    fix: [problem2] 전체 알고리즘과 구현목록 docs 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,23 +1,9 @@
 function problem2(cryptogram) {
+  const duplicateWord = /([a-z])\1+/g;
 
-  // 문자열 분리
-  const cryptoArr = cryptogram.split('');
-  const result = [];
+  while (duplicateWord.test(cryptogram)) cryptogram = cryptogram.replace(duplicateWord, '');
 
-  /* 초기 문자 push(), result 배열의 최상단 값과 입력 값 비교 같으면(연속된 중복 문자)
-     result.pop(), 중복이 아니면 해당 문자 push() */
-  for (let i=0; i<cryptoArr.length; i++) {
-    if (!result.length) {
-      result.push(cryptoArr[i]);
-    } else if (result[result.length-1] === cryptoArr[i]) {
-      result.pop()
-    } else if (result[result.length-1] !== cryptoArr[i]) {
-      result.push(cryptoArr[i]);
-    }
-  }
-
-  // 배열을 하나의 문자열로 return
-  return result.join('');
+  return cryptogram;
 }
 
 module.exports = problem2;
