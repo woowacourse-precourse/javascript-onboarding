4a43cb68fe0f0ef4940ee21f8d208ae803a7955c
dkile <dkile532@naver.com>
2022-10-31 16:30:41 +0900

    feat: add function of replacing lowercase alphabet by tree frog dictionary

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
+function replaceLowerCaseByTreeFrogDict(str) {
+  let res = str;
+  const rgx = /[a-z]/g;
+  const endsAsciiValSum = "a".charCodeAt(0) + "z".charCodeAt(0);
+  const replacer = (match) => {
+    return String.fromCharCode(endsAsciiValSum - match.charCodeAt(0));
+  };
+  res = str.replace(rgx, replacer);
+  return res;
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const res = replaceLowerCaseByTreeFrogDict(word);
+
+  return res;
 }
 
 module.exports = problem4;
