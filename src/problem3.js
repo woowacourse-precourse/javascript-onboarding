5f07be40861e46c3ba7855e52cb9729a57e636d3
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-10-31 21:28:39 +0900

    [3번 문제] feat: checkNum 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
+function checkNum(stringNum){
+  var sum = 0;
+  for(let i = 0;i < stringNum.length;i++){
+    if(stringNum[i] == 3 || stringNum[i] == 6 || stringNum[i] == 9){
+      sum++;
+    }
+  }
+  return sum;
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
   return answer;
 }
 
