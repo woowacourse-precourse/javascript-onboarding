d9b553d19f10f5534afe1f2aeb29e47664222a61
afds4567 <afds4567@gmail.com>
2022-10-31 20:14:55 +0900

    feat(problem4): 소문자, 공백 예외처리

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,14 @@
 function problem4(word) {
   var answer = "";
   const UPPER_CASE = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const LOWER_CASE = UPPER_CASE.toLowerCase();
   const BLUE_FROG_DICT = {};
   const ALPHABET_LENGTH = UPPER_CASE.length;
 
   for (let i = 0; i < ALPHABET_LENGTH; i++) {
     BLUE_FROG_DICT[UPPER_CASE[i]] = UPPER_CASE[ALPHABET_LENGTH - (i + 1)];
+    BLUE_FROG_DICT[LOWER_CASE[i]] = LOWER_CASE[ALPHABET_LENGTH - (i + 1)];
+    BLUE_FROG_DICT[" "] = " ";
   }
 
   for (let char of word) {
