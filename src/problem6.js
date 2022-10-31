0f40bf7c0535d1323d4fc19a1dd726e9a3ae7811
nalsae <nalsae0207@naver.com>
2022-10-31 20:25:33 +0900

    feat: Add an array of splited names
    - split 메서드를 사용하여 전달 받은 form의 이름 분할

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,9 @@
-function problem6(forms) {
-  var answer;
+const problem6 = (forms) => {
+  let answer = [];
+
+  const nameList = forms.map((form) => form[1].split(""));
+
   return answer;
-}
+};
 
 module.exports = problem6;
