495f0b1595f3e1a89454063bc0ce90793103cca0
pitang <wpeffect@gmail.com>
2022-10-31 17:04:56 +0900

    5번 기능 요구사항.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
+  for(let i = 0; i < moneyArr.length; i++) {
+    answer[i] = Math.floor(money / moneyArr[i]);
+    money = (money % moneyArr[i]);
+  }
   return answer;
 }
+
+// 1. 개미일꾼이랑 비슷 ! 
+// 2. 배열을 Return 해줘야함.
+// 1 : 50000, 2 : 10000, 3 : 5000, 4 : 1000, 
+// 5 : 500, 6 : 100, 7 : 50, 8 : 10, 9 : 1
 
 module.exports = problem5;
