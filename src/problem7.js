c73f39cb874a99ad5fac75930c043eaaa49774c6
JSH99 <tjgus9966@naver.com>
2022-11-01 04:12:20 +0900

    feat: 사용자 친구 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  var score = {};
+  const userFriends = [];
+
+  // 1. 사용자의 친구 구하기
+  for(var i = 0; i < friends.length; i++) {
+    score[friends[i][0]] = 0;
+    score[friends[i][1]] = 0;  
+
+    if(friends[i].includes(user))
+      userFriends.push(friends[i][0] === user ? friends[i][1] : friends[i][0]);
+  }
+
   return answer;
 }
 
