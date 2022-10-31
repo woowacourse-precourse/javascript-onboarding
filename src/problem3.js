cfc12769b270c7a176aced906b92d782e7c53c08
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-10-31 21:32:34 +0900

    ✨ problem3 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,20 @@
-function problem3(number) {
-  var answer;
-  return answer;
-}
+const COUNT_CONDITION = [3, 6, 9];
+
+const getSplitedNumbersArray = (number) =>
+  Array.from({ length: number }, (_, i) => ('' + (i + 1)).split(''));
+
+const getCount = (array) =>
+  array.reduce(
+    (count, currentNumber) => count + COUNT_CONDITION.includes(+currentNumber),
+    0
+  );
+
+const problem3 = (number) => {
+  const splitedArray = getSplitedNumbersArray(number);
+  return splitedArray.reduce(
+    (totalCount, currentNumbers) => totalCount + getCount(currentNumbers),
+    0
+  );
+};
 
 module.exports = problem3;
