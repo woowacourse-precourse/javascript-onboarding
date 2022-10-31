1890d7c51c1eae124a6a982e0775e3cc08e83abd
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-10-31 14:29:56 +0900

    problem 6 주석 추가 및 변수명 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,10 +1,20 @@
+/**
+ * problem 6
+ * @param {*} forms 
+ * @returns 
+ */
 function problem6(forms) {
-  const formsJSON = convertJSON(forms);
-  const uniqueWords = findUniqueWordsOfNickname(formsJSON);
-  return findEmailByDuplicatedName(formsJSON, uniqueWords).sort();
+  const formsObjectArray = convertToObjectArray(forms);
+  const uniqueWords = findUniqueWordsOfNickname(formsObjectArray);
+  return findEmailByDuplicatedName(formsObjectArray, uniqueWords).sort();
 }
 
-const convertJSON = (forms) => {
+/**
+ * 들어온 리스트를 JavaScript Object로 변환해주는 함수
+ * @param {*} forms 
+ * @returns 
+ */
+const convertToObjectArray = (forms) => {
   const array = [];
   forms.map(element => {
     array.push({
@@ -15,26 +25,45 @@ const convertJSON = (forms) => {
   return array;
 }
 
-const findUniqueWordsOfNickname = (formsJSON) => {
+/**
+ * 사용자들의 이름을 2글자 이상으로 잘개 쪼개어 중복이 없는 리스트로 반환해주는 함수
+ * @param {*} formsObjectArray 
+ * @returns 
+ */
+const findUniqueWordsOfNickname = (formsObjectArray) => {
   let uniqueWords = new Set();
-  formsJSON.forEach(element => {
+  formsObjectArray.forEach(element => {
     const nickname = element.name;
+    // 검사할 글자 갯수를 제어하기
     for (let i = 0; i < nickname.length; i++) {
+      // 글자를 처음부터 끝까지 검사하기
       for (let j = 0; j < nickname.length - i; j++) {
+        // 글자를 잘라보기 (for문이 돌면서 1글자, 2글자, 3글자, ... 순으로 잘리게 됨)
         const word = nickname.slice(j, j + i + 1);
+        // 자른 글자의 갯수가 1 초과인 경우에만 Set 함수에 추가를 시도함
         if (word.length > 1) {
           uniqueWords.add(word);
         }
       }
     }
   });
+  // Set함수를 배열로 반환
   return Array.from(uniqueWords);
 }
 
-const findEmailByDuplicatedName = (formsJSON, uniqueWords) => {
+/**
+ * 단어 리스트를 중복 사용 하고 있는 사람을 찾아 이메일을 반환하는 함수
+ * @param {*} formsObjectArray 
+ * @param {*} uniqueWords 
+ * @returns 
+ */
+const findEmailByDuplicatedName = (formsObjectArray, uniqueWords) => {
   let emails = new Set();
   uniqueWords.forEach((word) => {
-    const list = formsJSON.filter((form) => form.name.includes(word))
+    // 특정 단어가 포함된 이름을 찾아 객체를 배열로 만들기
+    const list = formsObjectArray.filter((form) => form.name.includes(word))
+    // 찾은 결과가 1보다 크다면 중복 사용 중인 사람들로 간주하고
+    // emails 집합에 그사람들의 email을 추가해줌
     if (list.length > 1) {
       list.map(
         (el) => {
@@ -43,6 +72,7 @@ const findEmailByDuplicatedName = (formsJSON, uniqueWords) => {
       )
     }
   })
+  // 배열로 반환
   return Array.from(emails);
 }
 
