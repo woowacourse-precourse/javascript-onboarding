b8d0bcd61d0403eab82a5d2c2ed6c64cf1509e26
Monte <monte6198@gmail.com>
2022-10-31 21:04:52 +0900

    feat(problem6.js): addCount() 및 특정 중복 문자열 출현 빈도 수 증가 기능 추가

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,11 @@ function firstSlice(nameSlice, allSlices) {
     allSlices.set(nameSlice, 0); //중복 문자열 목록(allSlices)에 새로 추가한다.
 }
 
+function addCount(nameSlice, allSlices) {
+  let count = allSlices.get(nameSlice); //특정 중복 문자열(nameSlice)에 대한 출현 빈도 수를
+  allSlices.set(nameSlice, count + 1); //1만큼 증가시킨다.
+}
+
 function problem6(forms) {
   var answer;
   let result = new Set(); //같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자의 이메일 목록
