c2d7e24a19f624010b6fb91482f916007b93b989
ho-ji <wkdd0205@gmail.com>
2022-10-31 17:59:41 +0900

    feat: 문제5 기능 구현
    큰 금액의 화폐 위주로 받는 잔돈의 개수를 구하는 기능을 구현함

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
 function problem5(money) {
-  var answer;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const listOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  if (money >= 100000) {
+    answer[0] += (money / 100000) * 2;
+    money -= parseInt(money / 100000) * 100000;
+  }
+  for (let i = 0; i < 10; i++) {
+    while (money >= listOfMoney[i]) {
+      money -= listOfMoney[i];
+      answer[i]++;
+    }
+  }
   return answer;
 }
 
