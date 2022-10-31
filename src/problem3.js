1d2602941bac933dc7a749f8e73e7dbd4386bdf1
woong <inwoong100@gmail.com>
2022-10-31 22:30:09 +0900

    feature: problem3 풀이 완성
    1. 구현방식에 따른 순차적인 풀이 완성
    2. 예외방식 예외 처리 완성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,12 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  const checkNumber = ['3', '6', '9'];
+  for (let i = 1; i <= number; i++) {
+    const iArray = i.toString().split('');
+    for (const el of iArray) {
+      if (checkNumber.includes(el)) answer++;
+    }
+  }
   return answer;
 }
 
