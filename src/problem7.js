c46450def5ab0c1574626dbe17477c53d9a7ecb1
hae-on <solbi2004@naver.com>
2022-11-01 01:29:52 +0900

    [문제7] 친구 목록 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,16 @@
+const friendList = [];
+
+const findFriendList = (user, friends) => {
+  friends.map((friend) => {
+    const [A, B] = friend;
+    if (A == user) friendList.push(B);
+    if (B == user) friendList.push(A);
+  });
+  return friendList;
+};
+
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
   return answer;
 }
 
