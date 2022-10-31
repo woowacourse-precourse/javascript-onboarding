22c4673e306cdacad8ba5bb77ec7ec9bfa773765
ilgon0110 <moses011010@gmail.com>
2022-10-31 20:13:22 +0900

    refactor: 변수 할당 수정(#problem3)
    const , let 변수 할당 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,11 +1,11 @@
 function problem3(number) {
+  const check = ['3', '6', '9'];
   let answer = 0;
-  let check = ['3', '6', '9'];
 
   //연산
   for (let i = 1; i <= number; i += 1) {
     //기능목록 1-1 구현
-    let num = String(i);
+    const num = String(i);
     answer += num.split('').filter(v => check.includes(v)).length;
   }
 
