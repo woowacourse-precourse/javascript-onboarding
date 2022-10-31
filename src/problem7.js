b1c6d05e1dd629202b830cd4953f177a2e776d77
Buyoung Chung <sunarol@naver.com>
2022-10-31 18:31:32 +0900

    [문제7] 추천 점수판과 친구 리스트 초기화

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
+function initializing(recommendScore, friendsList, friends) {
+  for (let friend of friends) {
+    for (let x of friend) {
+      recommendScore.set(x, 0);
+      friendsList.set(x, []);
+    }
+  }
+}
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+
+  let recommendScore = new Map();
+  let friendsList = new Map();
+  initializing(recommendScore, friendsList, friends);
   return answer;
 }
 
