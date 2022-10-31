ea1cb899cce08b5535afb6641143c9a8c5cb1816
gunyongbok <qhrrjsdyd123@gmail.com>
2022-10-31 17:53:34 +0900

    2 final fix

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,30 @@
-function problem2(cryptogram) {
-    const array = cryptogram.split('');
-    let stack = [];
-    for (let i = 0; i < array.length; i++) {
-        if (stack.length === 0) {
-            stack.push(array[0]);
-        }
-        if (array[i] == stack[stack.length - 1]) {
-            while (array[i] === array[i + 1]) {
-                i++;
-            }
-            stack.pop();
-            continue;
-        }
-        stack.push(array[i]);
-    }
-    var answer = stack.toString().replace(/,/g, '');
-    return answer;
-}
+function problem2(s) {
+    const chars = s.split('');
+    const n = s.length;
 
+    let k = 0;
+    let i = 1;
+
+    while (i < n) {
+        if (chars[i - 1] !== chars[i]) {
+            chars[k] = chars[i - 1];
+            k = k + 1;
+        } else {
+            while (i < chars.length && chars[i - 1] === chars[i]) {
+                i = i + 1;
+            }
+        }
+        i = i + 1;
+    }
+    chars[k] = chars[i - 1];
+    k = k + 1;
+
+    s = chars.slice(0, k).join('');
+
+    if (k !== n && n > 0) {
+        return problem2(s);
+    }
+
+    return s;
+}
 module.exports = problem2;
