cd0c07dff4133513b8f9ea58d2b8f3b0889988de
YeoInsu <insu12021202@gmail.com>
2022-10-31 17:38:59 +0900

    Feat:problem6 사용자와 친구인 닉네임 찾기 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,28 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+  userFriend = new Array();
+  //friends 배열에서 사용자와 친구인 닉네임 찾기
+  friends.map((item) => {
+    if (item.includes(user)) {
+      if (item[0] === user) {
+        userFriend.push(item[1]);
+      } else {
+        userFriend.push(item[0]);
+      }
+    }
+  });
 
+  console.log(userFriend);
+}
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 module.exports = problem7;
