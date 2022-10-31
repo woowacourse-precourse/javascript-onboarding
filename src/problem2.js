02d7c540b709902762296e351dce2fa1140e6736
“somm12” <sominan8744@gmail.com>
2022-10-31 17:53:02 +0900

    problem2 스택을 이용한 연속적인 문자 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = "";
+  // 첫 문자를 stack에 저장.
+  let stack = [cryptogram[0]];
+  // 입력된 문자를 하니씩 읽고 해당 문자와 stack의 마지막 문자와 같은지 확인. 같으면 pop, 다르면 push
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (cryptogram[i] === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(cryptogram[i]);
+    }
+  }
+
   return answer;
 }
-
 module.exports = problem2;
