2537ba7944f81920c31f476b925ed4d7661dcde5
SorinKwon <ksl020700@gmail.com>
2022-10-31 11:25:57 +0900

    feat(problem5): 각각 화폐 단위로 나누는 기능 추가
    money를 각가 화폐 단위로 나누어 새로운 배열에 추가하는 기능 추가함

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
-function problem5(money) {
-  var answer;
-  return answer;
-}
+function transformMoney(n){
+  let money_unit = [50000,10000,5000,1000,500,100,50,10,1];
+  let result=[];
 
+  for(let i=0;i<money_unit.length;i++){
+      result[i]=parseInt((n/money_unit[i]));
+  }
+  return result;
+}
+function problem5(money) {
+  let answer;
+  answer=transformMoney(money);
+  return answer;
+
+}
 module.exports = problem5;
