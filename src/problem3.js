a9f30a48568d6ad035e9fed6a00c838539b96ecf
SoMin <thals0107@naver.com>
2022-10-31 11:06:48 +0900

    feat: 문제 3 - 변환한 문자열 각 자리 수에 3,6,9가 포함 확인 후 cnt+=1

--- src/problem3.js
+++ src/problem3.js
@@ -3,8 +3,12 @@ function problem3(number) {
   for (let i = 0; i < number; i++) {
     let str = "" + (i + 1);
     // console.log(str);
+    for (let j = 0; j < str.length; j++) {
+      if (str[j] == 3 || str[j] == 6 || str[j] == 9) {
+        cnt += 1;
+      }
+    }
   }
-  return cnt;
 }
 // console.log(problem3(13));
 module.exports = problem3;
