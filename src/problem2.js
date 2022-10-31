b4b02359f6c9619da8fbea631872de9420baac8e
박정규 <srk99279@naver.com>
2022-10-31 18:24:57 +0900

    feat: (문제 2) 기존 2개의 중복문자만을 제거하던 것을 2개 이상의 중복문자도 제거하도록 while문 추가

--- src/problem2.js
+++ src/problem2.js
@@ -5,8 +5,12 @@ function problem2(cryptogram) {
   let i = 0;
 
   while (i < spellList.length) {
+    let countReduplicated = i;
+    while (spellList[countReduplicated] === spellList[countReduplicated + 1]) {
+      countReduplicated++;
+    }
     if (spellList[i] === spellList[i + 1]) {
-      spellList.splice(i, 2);
+      spellList.splice(i, countReduplicated - i + 1);
       i = -1; // 문자열의 처음부터 다시 확인하기 위해 -1를 i에 할당
     }
     i++;
