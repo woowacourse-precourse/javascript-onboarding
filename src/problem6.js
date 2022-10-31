b7b78c7ac3f589c3218251722a2e20ec8ea94176
ekkim <eklikeable@kakao.com>
2022-11-01 00:51:34 +0900

    🩹 메소드 변경(search, substr), 제한사항 검증 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -1,17 +1,13 @@
 function problem6(forms) {
-  if (forms.length < 1 || forms.length > 10000) return 'Please check the data';
-
   const duplicated = [];
 
   forms.forEach((target, targetIndex) => {
     const [targetEmail, targetNickName] = target;
-    if (isValid(targetEmail, targetNickName)) {
-      for (let i = targetIndex + 1; i < forms.length; i++) {
-        for (let j = 0; j < targetNickName.length - 1; j++) {
-          if (forms[i][1].search(targetNickName.substr(j, 2)) !== -1) {
-            duplicated.push(targetEmail, forms[i][0]);
-            break;
-          }
+    for (let i = targetIndex + 1; i < forms.length; i++) {
+      for (let j = 0; j < targetNickName.length - 1; j++) {
+        if (forms[i][1].includes(targetNickName.slice(j, j + 2))) {
+          duplicated.push(targetEmail, forms[i][0]);
+          break;
         }
       }
     }
@@ -19,21 +15,6 @@ function problem6(forms) {
 
   const result = [...new Set(duplicated)].sort();
   return result;
-}
-
-function isValid(targetEmail, targetNickName) {
-  const valPattern_nickName_number = /^[가-힣]{1,19}$/;
-  const valPattern_email = /^[a-zA-Z0-9]{1,9}(?=\@email\.com)/;
-
-  if (
-    valPattern_nickName_number.test(targetNickName) &&
-    valPattern_email.test(targetEmail)
-  ) {
-    return true;
-  } else {
-    console.log('Please check your information');
-    return false;
-  }
 }
 
 module.exports = problem6;
