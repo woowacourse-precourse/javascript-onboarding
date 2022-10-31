847f5a8200e1a2e838d5ff6f2a8d9f0d519effb1
wusi-hub <jws9595@naver.com>
2022-11-01 07:21:22 +0900

    problem2) 코드 설명을 위한 주석 추가 후 제출

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,17 @@
 function problem2(cryptogram) {
+
+  // 문자열 cryptogram을 배열로 변환하여 담아줌
   let arr = [...cryptogram];
+
+  // 배열을 탐색하여 연속하는 중복 문자들이 있을 경우 해당 문자들을 삭제
   for(let i = 0; i < arr.length; i++) {
     if(arr[i] === arr[i+1]) {
       arr.splice(i,2);
       i = -1;
     } 
   }
+
+  // 삭제한 결과를 다시 문자열로 변환하여 리턴
   return arr.join('');
 }
 
