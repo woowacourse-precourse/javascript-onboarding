1d11b30a1aee35a8a0cd1de8986c77e3cf077523
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 21:17:34 +0900

    [Feat:Problem1] 예외 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,38 +1,11 @@
-const getSum = (number) => {
-  return String(number)
-    .split("")
-    .reduce((sum, str) => (sum += +str), 0);
+const errorHandler = (pobi, crong) => {
+  let error = false;
+  if ([pobi, crong].some((user) => user.length !== 2)) error = true;
+  else {
+    if ([...pobi, ...crong].some((value) => value <= 1 || value >= 400))
+      error = true;
+    if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) error = true;
+  }
+
+  return error;
 };
-
-const getMultiply = (number) => {
-  return String(number)
-    .split("")
-    .reduce((total, str) => (total *= +str), 1);
-};
-
-const getMax = (...args) => Math.max(...args);
-
-function problem1(pobi, crong) {
-  const [pLeft, pRight] = pobi;
-  const [cLeft, cRight] = crong;
-  if (pRight - pLeft !== 1 || cRight - cLeft !== 1) return -1;
-
-  const pMax = getMax(
-    getSum(pLeft),
-    getMultiply(pLeft),
-    getSum(pRight),
-    getMultiply(pRight)
-  );
-  const cMax = getMax(
-    getSum(cLeft),
-    getMultiply(cLeft),
-    getSum(cRight),
-    getMultiply(cRight)
-  );
-
-  if (pMax > cMax) return 1;
-  else if (pMax < cMax) return 2;
-  else return 0;
-}
-
-module.exports = problem1;
