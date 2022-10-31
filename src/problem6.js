57a25e45fdef948109eb3d4293ea7bd2fff0bbb9
daal2quf <daal2quf@gmail.com>
2022-11-01 04:15:58 +0900

    중복된 email 찾는 코드 합치기

--- src/problem6.js
+++ src/problem6.js
@@ -1,45 +1,33 @@
 function problem6(forms) {
   var answer = [];
-  const map2 = new Map(forms)
-
-  const map = new Map()
+  const crewName = new Map()
+  const formsMap = new Map(forms)
 
   for (let e of forms) {
-    const userNameParts = e[1].split('')
+    const oneOfNames = e[1].split('')
 
-    for (let i = 0; i < userNameParts.length - 1; i++) {
-      const twoWordOFName = userNameParts[i] + userNameParts[i + 1]
-      if(map.has(twoWordOFName)) {
-        map.set(twoWordOFName, map.get(twoWordOFName) + 1)
+    for (let i = 0; i < oneOfNames.length - 1; i++) {
+      const twoWordOFName = oneOfNames[i] + oneOfNames[i + 1]
+      if(crewName.has(twoWordOFName)) {
+        crewName.set(twoWordOFName, crewName.get(twoWordOFName) + 1)
       } else {
-        map.set(twoWordOFName, 1)
+        crewName.set(twoWordOFName, 1)
       }
     }
   }
   
-  let duplicatedWord;
-  for (let key of map.keys()) {
-    if(map.get(key) > 1) {
-      duplicatedWord = key;
+  for (let key of crewName.keys()) {
+    if(crewName.get(key) > 1) {
+      for(let email of formsMap.keys()) {
+        if(formsMap.get(email).includes(key)) {
+          answer.push(email)
+        }
+      }
     }
   }
 
-  for(let key of map2.keys()) {
-    if(map2.get(key).includes(duplicatedWord)) {
-      answer.push(key)
-    }
-  }
-  
   answer.sort((a, b) => a.localeCompare(b))
-  console.log(answer)
   return answer;
 }
 
-problem6([
-  ["jm@email.com", "제이엠"],
-  ["jason@email.com", "제이슨"],
-  ["woniee@email.com", "워니"],
-  ["mj@email.com", "엠제이"],
-  ["nowm@email.com", "이제엠"],
-])
 module.exports = problem6;
