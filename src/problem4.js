c82b832cb1399d038c16ce78171404a0655019e5
webdreamer12 <dreamerdev12@gmail.com>
2022-11-01 01:47:29 +0900

    feat: problem4.js - 1.주어진 매개변수로부터의 참조와 변경을 위한 알파벳 소문자, 대문자 배열과 각 배열의 역순 배열 생성 2.참조 배열 인덱스 변수 referenceIndex 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,4 +1,10 @@
 function problem4(word) {
+  let uppercaseOrdered = ["A","B","C","D","E","F","G","H","I","J","K","L","M","N","O","P","Q","R","S","T","U","V","W","X","Y","Z"];
+  let uppercaseReverseOrdered = ["Z", "Y", "X", "W", "V", "U", "T", "S", "R", "Q", "P", "O", "N", "M", "L", "K", "J", "I", "H", "G", "F", "E", "D", "C", "B", "A"];
+  let lowercaseOrdered = ["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
+  let lowercaseReverseOrdered = ["z", "y", "x", "w", "v", "u", "t", "s", "r", "q", "p", "o", "n", "m", "l", "k", "j", "i", "h", "g", "f", "e", "d", "c", "b", "a"];
+  var referenceIndex = 0;
+  
   var answer;
   return answer;
 }
