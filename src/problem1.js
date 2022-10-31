9e015bc64b721fcdf165ed64faeab45cc4ca819d
nara04040 <nara04040@naver.com>
2022-10-31 14:25:03 +0900

    feat : 예외사항 판별하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
   return answer;
 }
+// 예외사항을 판별하는 함수
+const checkingExceptions = (pages) => {
+  if (pages.length !== 2) return -1;
+  if (pages[1] - pages[0] !== 1) return -1;
+  if (
+    pages.some((ele) => {
+      ele <= 1 || ele >= 400;
+    })
+  )
+    return -1;
+
+  return 1;
+};
 
 module.exports = problem1;
