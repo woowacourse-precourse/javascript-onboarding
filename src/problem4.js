661c3d1ceb734d9575cb5a47dcc6753a92d7c4c5
taeyoung <rlaxodud911@gmail.com>
2022-10-31 19:28:05 +0900

    Feat: problem 4 소문자인지 대문자인지 판별하는 정규식 작성

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,8 @@ function problem4(word) {
   var answer;
   const LOWER = 'a'.charCodeAt(0) + 'z'.charCodeAt(0);
   const UPPER = 'A'.charCodeAt(0) + 'Z'.charCodeAt(0);
+  const isLower = /[a-z]/;
+  const isUpper = /[A-Z]/;
 
   return answer;
 }
