b6299081dec961b1cdae3d209290b786f49b0dfd
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-10-31 15:28:15 +0900

    refactor: Fix numberArray function to edit return

--- src/problem3.js
+++ src/problem3.js
@@ -1,19 +1,21 @@
 function problem3(number) {
   let answer;
 
+  let arr = numberArr(number);
+
   return answer;
 }
 
 function numberArr(number) {
   let array = [];
-  let newArray = '';
+  let newArray = "";
   for (let i = 1; i <= number; i++) {
     array.push(i.toString().split(""));
   }
-  console.log(newArray.concat(array));
+  return newArray.concat(array).split(",");
 }
 
-// console.log(problem3(13));
-console.log(numberArr(13));
+console.log(problem3(13));
+
 
 module.exports = problem3;
