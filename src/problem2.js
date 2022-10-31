d99ccd64147cc613c0f193e01ee80b0821be5238
somin <smhs12@naver.com>
2022-10-31 14:58:56 +0900

    feat: problem2 / 입력 문자열 연속된 문자 포함 여부 확인 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
+  if (!isInitialContinuous(cryptogram)) {
+    return cryptogram;
+  }
   return answer;
+}
+
+function isInitialContinuous(cryptogram) {
+  let continuityCount = 0;
+  for (let i = 0; i < cryptogram.length - 1; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      continuityCount++;
+    }
+  }
+  if (continuityCount > 0) return true;
+  return false;
 }
 
 function isDuplicate(cryptogram) {
