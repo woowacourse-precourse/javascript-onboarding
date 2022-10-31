f7f4cb6c7a8ada3b80ef73d5712a65273d7dcf2a
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-31 10:08:53 +0900

    숫자의 자릿수의 합과 곱중 최대값을 반환하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -9,6 +9,11 @@ function problem1(pobi, crong) {
   function getMul(page) {
     return String(page).split("").reduce((acc, curr) => acc *= Number(curr), 1);
   }
+
+  function getMaxScore(pages) {
+    const [left, right] = pages;
+    return Math.max(getSum(left), getSum(right), getMul(left), getMul(right));
+  }
 }
 
 module.exports = problem1;
