88a1878ee92e6a789c5d25699113bd6e20316989
woong <inwoong100@gmail.com>
2022-10-31 22:56:17 +0900

    feature: problem5 풀이 완성
    1. 구현방식에 따른 순차적인 풀이 완성
    2. 예외방식 예외 처리 완성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
 function problem5(money) {
-  var answer;
+  if (money < 1 || money > 1000000) return 'ERROR';
+  if (money % 1) return 'ERROR';
+
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+  for (let i = 0; i < currency.length; i++) {
+    const coin = Math.floor(money / currency[i]);
+    answer.push(coin);
+    money -= currency[i] * coin;
+  }
+
   return answer;
 }
 
