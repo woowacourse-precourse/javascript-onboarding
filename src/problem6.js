00ddccc67aeccee62b16a8aceee5bee6723ca0b2
jiheon788 <jiheon788@ajou.ac.kr>
2022-10-31 14:44:03 +0900

    refactor<problem6>: Clean code

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,8 @@
 /* 
  * Features
- * 1. 닉네임 중 같은 글자가 연속적으로 중복되는 지원자 검사
- * 2. 이메일 목록 리턴
+ * 1. JSON 폼으로 변경하는 기능
+ * 2. 문자의 조합을 찾아주는 기능
+ * 3. 문자의 조합 중복 검사
  *  
  * Exceptions
  * 1. 크루는 1명 이상 10,000명 이하
@@ -10,23 +11,21 @@
  * 4. 닉네임은 한글만 가능하고 전체 길이는 1자 이상 20자 미만
  * 5. result는 이메일에 해당하는 부분의 문자열을 오름차순으로 정렬하고 중복은 제거
  */
-const setArrToJSON = arr => {
-  let result = [];
-
-  arr.map(item => {
-    result.push({
+const changeToJSON = originForm => {
+  let newForm = [];
+  originForm.map(item => {
+    newForm.push({
       email: item[0], 
       nickname: item[1],
-      isDuplicate: false
+      isDuplicated: false
     })
   })
 
-  return result;
+  return newForm;
 }
 
 const getCombinations = str =>{
   let combinations = [];
-
   for (let i = 0 ;i < str.length; i++) {
     for(let j = i + 1; j< str.length + 1; j++) {
       if (str.slice(i, j).length > 1) {
@@ -38,34 +37,33 @@ const getCombinations = str =>{
   return combinations;
 }
 
-function problem6(forms) {
-  let answer = setArrToJSON(forms);  
-  
-  answer.map((user)=>{
-    var c = getCombinations(user.nickname);
-    user.nameCombinations = c;
-  })
-  
-  for(let i = 0; i < answer.length; i++) {
-    answer[i].nameCombinations.map(char=>{
-      for(let j = i + 1; j < answer.length; j++) {
-        if(answer[j].nameCombinations.includes(char)){
-          answer[i].isDuplicate = true;
-          answer[j].isDuplicate = true;
+const setIsDuplicated = forms => {
+  for (let i = 0; i < forms.length; i++) {
+    forms[i].nameCombinations.map(char => {
+      for (let j = i + 1; j < forms.length; j++) {
+        if (forms[j].nameCombinations.includes(char)) {
+          forms[i].isDuplicated = true;
+          forms[j].isDuplicated = true;
         }
       }
     })
   }
+}
 
-  let newArr = answer.filter((item)=>item.isDuplicate)
-
-  let newArr2 = [];
-  
-  newArr.map((item)=>{
-    newArr2.push(item.email)
+function problem6(forms) {
+  let answer = [];
+  forms = changeToJSON(forms);  
+  forms.map(user => {
+    user.nameCombinations = getCombinations(user.nickname);
+  })
+  setIsDuplicated(forms);
+  forms.map(user => {
+    if (user.isDuplicated) {
+      answer.push(user.email);
+    }
   })
 
-  return newArr2.sort();
+  return answer.sort();
 }
 
 module.exports = problem6;
