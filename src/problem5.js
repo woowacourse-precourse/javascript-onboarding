56f079543f24a5fbbb697bc1e381601203ff3c69
sulmeee <poorooria@naver.com>
2022-10-31 20:36:03 +0900

    [problem5] feat: 돈을 카운트 해줄 배열 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
   // 결과를 담을 변수 선언
   let result = [];
+
+  // 돈을 카운트해줄 배열 선언
+  let moneyCount = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 }
 
 module.exports = problem5;
