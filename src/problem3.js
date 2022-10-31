470f4a61de0d2e7f7a9a7cec10262c11001bdc6e
이여진 <jinspace3710@gmail.com>
2022-10-31 17:21:30 +0900

    feat: problem3 기능 1 - 반복문을 통한 자리수 판단

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (i = 1; i <= number; i++) {
+    let temp;
+    temp = i;
+
+    let digit;
+
+    while (temp > 0) {
+      digit = temp % 10;
+      if (digit === 3 || digit === 6 || digit === 9) answer++;
+      temp = parseInt(temp / 10);
+    }
+  }
   return answer;
 }
 
