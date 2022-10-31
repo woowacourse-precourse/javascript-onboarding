8165ca9058abe307cafc8bb3f3ddcb0c66fbc82c
letsjo <hyunoh.jo@gmail.com>
2022-10-31 23:40:55 +0900

    [problem5] Simplify code for readability

--- src/problem5.js
+++ src/problem5.js
@@ -1,16 +1,20 @@
+console.log(problem5(50237));
+
 function problem5(money) {
   return withdrawApp(money);
 }
 
 function withdrawApp(money){
-  const billType = [50000,10000,5000,1000,500,100,50,10,1];
+  const billTypeArr = createBillTypeArr();
   const currBillCount = [];
-  for(let i = 0; i < billType.length; i++){
-    let [currAccount,billCount] = calcWithdraw(money,billType[i]);
-    money = currAccount;
-    currBillCount[i] = billCount;
+  for(let i = 0; i < billTypeArr.length; i++){
+    [money,currBillCount[i]] = calcWithdraw(money,billTypeArr[i]);
   }
   return currBillCount;
+}
+
+function createBillTypeArr(){
+  return [50000,10000,5000,1000,500,100,50,10,1];
 }
 
 function calcWithdraw(money, billType){
