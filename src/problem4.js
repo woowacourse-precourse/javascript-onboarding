e1a33ac87829cbc4e54fbb42cdb24524eec53784
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:22:34 +0900

    [ feat ] problem4_알파벳&거꾸로 된 알파벳 객체 생성

--- src/problem4.js
+++ src/problem4.js
@@ -7,6 +7,8 @@
 function problem4(word) {
   let originAlphabet = [];
   let reversedAlphabet = [];
+  const alphabetObj = {};
+  const ALPHABET_LENGTH = 26;
 
   for (let i = 97; i <= 122; i++) {
     originAlphabet.push(String.fromCharCode(i));
@@ -14,6 +16,10 @@ function problem4(word) {
 
   for (let i = 122; i >= 97; i--) {
     reversedAlphabet.push(String.fromCharCode(i));
+  }
+
+  for (let i = 0; i < ALPHABET_LENGTH; i++) {
+    alphabetObj[originAlphabet[i]] = reversedAlphabet[i];
   }
 
   return answer;
