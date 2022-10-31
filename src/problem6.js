70faf991181873ea87792c584e223f8db7f2b51d
yb8350 <yb8350@naver.com>
2022-11-01 01:30:57 +0900

    Feat: problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var answer = [];
+
+  let wordList = new Map();
+  let overlapSet = new Set();
+
+  for (let i = 0; i < forms.length; i++) {
+    if (forms[i][1].length === 1) continue;
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      let word = forms[i][1].slice(j, j + 2);
+      if (wordList.get(word) !== undefined)
+        overlapSet.add(wordList.get(word)).add(i);
+      else wordList.set(word, i);
+    }
+  }
+
+  for (let item of overlapSet) {
+    answer.push(forms[item][0]);
+  }
+
+  return answer.sort();
 }
 
 module.exports = problem6;
