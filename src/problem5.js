4e7c4126288efa27eb43eedbaedd583da9da78a3
knh6269 <knh6269@gmail.com>
2022-10-31 22:47:31 +0900

    feat : 미션5) 단위 별 갯수를 푸쉬해주고 남은 돈을 처리해주는 함수

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
   var answer;
+  //단위 별 갯수를 푸쉬해주고 남은 돈을 처리해주는 함수
+  const disposeMoney = (many, count) =>{
+    answer.push(many); 
+    money=money % count;
+  }
+
   return answer;
 }
 
