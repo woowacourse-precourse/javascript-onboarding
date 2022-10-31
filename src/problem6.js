c0c38ff0b74dbf91c65bf52ca2700b39ce3a2ae7
Sungjae.Kwak <sungjae8970@gmail.com>
2022-11-01 02:16:14 +0900

    ⚡️ 닉네임이 한글자면 비교의 필요가 없으니 건너뛰는 과정 추가

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,10 @@ function problem6(forms) {
   // 비교는 항상 0번째의 유저와 하며, shift로 0번째 유저가 교체되어 나간다
   while (forms.length > 1) {
     const [email, nickname] = forms[0];
+    if (nickname.length === 1) {
+      forms.shift();
+      continue;
+    }
     // 임시공간에 0번째의 유저의 닉네임을 2글자로 쪼갠 문자열들을 넣는다
     const temp = [];
     for (let i = 0; i < nickname.length - 1; i++) {
