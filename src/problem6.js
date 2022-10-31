cd27b345e1c290568ca03116a54e5b332e6682b5
daal2quf <daal2quf@gmail.com>
2022-11-01 07:37:53 +0900

    중복단어찾기 와 이메일찾기 기능 나누기

--- src/problem6.js
+++ src/problem6.js
@@ -1,11 +1,12 @@
 function problem6(forms) {
   var answer = [];
+  let duplicateWord;
   const crewName = new Map()
   const formsMap = new Map(forms)
 
-  forms.forEach((e) => {
+  for (let e of forms) {
     const oneOfNames = e[1].split('')
-    console.log(oneOfNames)
+
     for (let i = 0; i < oneOfNames.length - 1; i++) {
       const twoWordOFName = oneOfNames[i] + oneOfNames[i + 1]
       if (crewName.has(twoWordOFName)) {
@@ -15,25 +16,21 @@ function problem6(forms) {
       }
     }
   }
-  )
 
-  let asd = Array.from(crewName.keys())
-
-  asd.forEach((key) => {
+  for (let key of crewName.keys()) {
     if (crewName.get(key) > 1) {
-      for (let email of formsMap.keys()) {
-        if (formsMap.get(email).includes(key)) {
-          answer.push(email)
-        }
-      }
+      duplicateWord = key;
     }
-  })
+  }
 
-  answer.sort((a, b) => a.localeCompare(b))
-  console.log(answer)
+  for (let email of formsMap.keys()) {
+    if (formsMap.get(email).includes(duplicateWord)) {
+      answer.push(email);
+    }
+  }
+
+  answer.sort((a, b) => a.localeCompare(b));
   return answer;
 }
 
-
-problem6([["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]])
 module.exports = problem6;
