034e6f76b94a4768ee814e35b770cb93ecf00ab0
yb8350 <yb8350@naver.com>
2022-11-01 02:44:05 +0900

    Feat: problem7-사용자와 함께 아는 친구의 수 계산

--- src/problem7.js
+++ src/problem7.js
@@ -2,9 +2,17 @@ function problem7(user, friends, visitors) {
   var answer;
 
   let friendList = new Map();
+  let recommend = new Map();
+
   for (let i = 0; i < friends.length; i++) {
     addFriend(friendList, friends[i][0], friends[i][1]);
     addFriend(friendList, friends[i][1], friends[i][0]);
+  }
+
+  for (let friendId of friendList.get(user)) {
+    for (let snsId of friendList.get(friendId)) {
+      if (user !== snsId) friendPoint(recommend, snsId);
+    }
   }
 
   return answer;
@@ -14,6 +22,12 @@ function addFriend(friendList, friend1, friend2) {
   if (friendList.get(friend1) !== undefined) {
     friendList.set(friend1, [...friendList.get(friend1), friend2]);
   } else friendList.set(friend1, [friend2]);
+}
+
+function friendPoint(recommend, snsId) {
+  if (recommend.get(snsId) !== undefined) {
+    recommend.set(snsId, recommend.get(snsId) + 10);
+  } else recommend.set(snsId, 10);
 }
 
 module.exports = problem7;
