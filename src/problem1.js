b35338f34387e8b8c3d93d1951cc08e36f5c13c2
jellikim <jellikiim@gmail.com>
2022-10-31 17:10:31 +0900

    123

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,34 @@
+// function problem1(pobi, crong) {
+//   var answer;
+//   return answer;
+// }
+
+// module.exports = problem1;
+
 function problem1(pobi, crong) {
+  function sum(n) {
+    return String(n)
+      .split("")
+      .reduce((tot, e) => tot + parseInt(e), 0);
+  }
+
+  function multiple(n) {
+    return String(n)
+      .split("")
+      .reduce((acc, cur) => acc * cur);
+  }
+
+  function pobiOdd(pobi) {
+    return sum(pobi[0]) >= multiple(pobi[0]) ? sum(pobi[0]) : multible(pobi[0]);
+  }
+
+  function pobiEven(pobi) {
+    return sum(pobi[1]) >= multiple(pobi[1]) ? sum(pobi[1]) : multible(pobi[1]);
+  }
   var answer;
   return answer;
+  console.log(pobi[1]);
 }
 
+console.log(problem1([97, 98], [197, 198]));
 module.exports = problem1;
--- src/problem2.js
+++ src/problem2.js
@@ -4,3 +4,15 @@ function problem2(cryptogram) {
 }
 
 module.exports = problem2;
+
+function problem2(cryptogram) {
+  var answer = "";
+  let s = cryptogram;
+  for (let i = 0; i < s.length; i++) {
+    console.log(s[i], i, s.indexOf([i]));
+    if (s.indexOf(s[i]) === s.indexOf(s[i + 1])) answer = s -= s[i] + s[i + 1];
+  }
+  return answer;
+}
+
+console.log(problem2("browoanoommnaon"));
