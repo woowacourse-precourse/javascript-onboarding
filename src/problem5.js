045d4f4a415d03d6ae69905155e64aed815855c6
DongRyeong Lee <dr11291129@gmail.com>
2022-10-31 16:54:34 +0900

    feat: P5 getCoinNumber 구현

--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,14 @@ function problem5(money) {
   return answer;
 }
 
+function getCoinNumber(money, coin) {
+  let result = {
+    curMoney: money,
+    counter: 0,
+  };
+  result.curMoney = Math.trunc(money % coin);
+  result.counter = Math.trunc(money / coin);
+  return result;
+}
+
 module.exports = problem5;
