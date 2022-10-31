8618a5370c3a6b06f53167d5a667a4e9c18eba9e
khj0426 <706shin1728@naver.com>
2022-10-31 12:13:15 +0900

    refactor:문제 7번 리팩토링

--- src/problem7.js
+++ src/problem7.js
@@ -41,11 +41,6 @@ function insert_map(first, second) {
   second.map.push(first);
 }
 
-//타임라인 방문시 1 추가
-function visite_timeline(objarray, name) {
-  let person = find_person_inobj(objarray, name);
-  person.score += 1;
-}
 //모든 친구 관계 추가
 function handle_friends(person_obj_list, friends) {
   friends.forEach((person) => {
@@ -55,13 +50,20 @@ function handle_friends(person_obj_list, friends) {
   });
 }
 
-//타임 라인 방문자 점수 추가
+//visitors를 받아서 visite_timeline함수를 실행한다.
 function handle_visitors(person_obj_list, visitors) {
   visitors.forEach((person) => {
     visite_timeline(person_obj_list, person);
   });
 }
 
+//타임 라인 방문시 점수 1점 증가
+function visite_timeline(objarray, name) {
+  let person = find_person_inobj(objarray, name);
+  person.score += 1;
+}
+
+//user객체의 map을 돌면서, 사용자와 이미 친구인 사용자의 점수를 0점처리한다.
 function is_friend(person_obj_list, user) {
   let userobj = find_person_inobj(person_obj_list, user);
   userobj.map.forEach((person) => {
@@ -79,11 +81,11 @@ function is_user(person, user) {
 
 function handle_common_friend(person_obj_list, user) {
   person_obj_list.forEach((person) => {
-    if (is_user(person, user) === false) {
-      has_common_friend(person, user.map);
-    }
+    has_common_friend(person, user.map);
   });
 }
+
+//이 함수는 사람객체의 map을 순회하면서, 사용자와 함께 아는 친구가 있다면, 사람객체의 점수를 10점씩 올려줌
 function has_common_friend(person, user_map) {
   let update_score = person.score;
 
@@ -96,36 +98,58 @@ function has_common_friend(person, user_map) {
   person.score += update_score;
 }
 
+function is_valid_person(person_obj_list, answer, user) {
+  person_obj_list.forEach((person) => {
+    if (person.score != 0 && answer.length <= 5 && person.name !== user) {
+      answer.push(person);
+    }
+  });
+  return answer;
+}
+
+//점수 순으로 정렬
+function sort_by_score(answer) {
+  answer.sort(function (a, b) {
+    return b.score - a.score;
+  });
+
+  return answer;
+}
+
+//Person객체의 이름만 뽑아서 반환
+function get_name(answer) {
+  let final_answer = [];
+  answer.forEach((person) => {
+    final_answer.push(person.name);
+  });
+  return final_answer;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
 
   let all_person = find_all_person(friends, visitors);
   let person_obj_list = create_person_obj(all_person);
 
+  //친구 관계 추가
   handle_friends(person_obj_list, friends);
 
+  //모든 사람의 객체 배열을 돌면서 모든 사람마다 has_common_friend함수를 실행함
   handle_common_friend(
     person_obj_list,
     find_person_inobj(person_obj_list, user)
   );
+  //타임 라인 방문자 처리
   handle_visitors(person_obj_list, visitors);
+
+  //사용자와 이미 친구인 경우 0점처리
   is_friend(person_obj_list, user);
   answer = [];
 
-  person_obj_list.forEach((person) => {
-    if (person.name !== user && person.score !== 0 && answer.length <= 5) {
-      answer.push(person);
-    }
-  });
-
-  answer.sort(function (a, b) {
-    return b.score - a.score;
-  });
-  const final_result = [];
-  answer.forEach((person) => {
-    final_result.push(person.name);
-  });
-  return final_result;
+  answer = is_valid_person(person_obj_list, answer, user);
+  answer = sort_by_score(answer);
+  answer = get_name(answer);
+  return answer;
 }
 
 module.exports = problem7;
