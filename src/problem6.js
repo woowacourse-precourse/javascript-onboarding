ce0c776ecbb2115e1847c286c8e0b76cf2922ea3
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 15:07:57 +0900

    feat: Problem 6 - 크루의 이름을 두 글자 단위로 저장하는 기능 구현하기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+const addName = (crewName, nameMap) => {
+  let crewTwoLetterSet = new Set();
+
+  for (let i = 0; i < crewName.length - 1; i++) {
+    const twoLetterOfCrewName = crewName[i] + crewName[i + 1];
+    crewTwoLetterSet.add(twoLetterOfCrewName);
+  }
+
+  crewTwoLetterSet = Array.from(crewTwoLetterSet);
+
+  for (let i = 0; i < crewTwoLetterSet.length; i++) {
+    const currentTwoLetter = crewTwoLetterSet[i];
+
+    if (nameMap.has(currentTwoLetter))
+      nameMap.set(currentTwoLetter, nameMap.get(currentTwoLetter) + 1);
+    else
+      nameMap.set(currentTwoLetter, 1);
+  }
+}
+
+const problem6 = forms => {
+  let nameMap = new Map();
+  let emailSet = new Set();
 }
 
 module.exports = problem6;
