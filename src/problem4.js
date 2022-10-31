6702d4b502dc2e001e8b37135140004f818d39f1
JangHoonSeok <orirori512@naver.com>
2022-11-01 02:12:10 +0900

    refactor: charCase함수 수정

--- src/problem4.js
+++ src/problem4.js
@@ -25,8 +25,8 @@ function wordToASCII(word) {
 
 function charCase(item) {
   // 해당하는 문자가 대문자면 0, 소문자면 1, Space면 2를 return.
-  if (item >= 65 || item <= 90) return 0; // 대문자면 0을 return
-  else if (item >= 97 || item <= 122) return 1; // 소문자면 1을 return
+  if (item >= 65 && item <= 90) return 0; // 대문자면 0을 return
+  else if (item >= 97 && item <= 122) return 1; // 소문자면 1을 return
   else return 2; // space면 2를 return
 }
 
