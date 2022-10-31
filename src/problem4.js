f574089698a5d79159a2ca1fe22cd3e9891aaee4
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-01 01:46:16 +0900

    feat: Add uppercase function

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,26 @@
 function problem4(word) {
-  let answer ;
+  let answer;
 
   return answer;
 }
 
+function findUpperArr(word) {
+  let alphabetUpperArr = Array.from({ length: 26 }, (v, i) =>
+    String.fromCharCode(i + 65)
+  );
+
+  let UpperArr = [];
+  for (let i = 0; i < word.split("").length; i++) {
+    let changeUpperCase = alphabetUpperArr.indexOf(word.split("")[i]);
+    if (alphabetUpperArr[26 - (changeUpperCase + 1)]) {
+      UpperArr.push(alphabetUpperArr[26 - (changeUpperCase + 1)]);
+    } else {
+      UpperArr.push(word[i]);
+    }
+  }
+  return UpperArr;
+}
+
+console.log(problem4("I love you"));
 
 module.exports = problem4;
