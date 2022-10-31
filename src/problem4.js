ea25b8e21ec945b26f05e2f4aceffbcf8b059dfe
Minju Park <project.mizzu@gmail.com>
2022-10-31 18:04:50 +0900

    Feat : 알파벳 문자 변환
    - word의 특정 값이 대문자인 경우 소문자로 변경
    - word의 특정 값이 소문자인 경우 대문자로 변경
    - word의 특정 값이 알파벳 이외인 경우 변경하지 않음
    - wordArr.join('')하여 반환

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,12 @@
 // [✅] 청개구리 사전의 원본(대문자) 입력하기, 소문자 사전 만들기
 // [✅] 청개구리 사전의 reverse 사전 만들기 -> 스프레드
-// [] word -> arr로 변환 후 각 index가 아스키 코드 기준 범위에 있는 값이면 변경 / 아스키 코드 65 ~ 90 (대문자), 97 ~ 122 (소문자)
-// [] word[i]에 해당하는 알파벳이 대문자일 경우, dictionaryUpper.indexOf(word[i])를 통해 index 찾기
-// [] wordArr[i]의 값을 dictionaryUpperReverse[idx]의 값으로 변경
-// [] word[i]에 해당하는 알파벳이 소문자일 경우, dictionaryLower.indexOf(word[i])를 통해 index 찾기
-// [] wordArr[i]의 값을 dictionaryLowerReverse[idx]의 값으로 변경
-// [] 알파벳을 제외한 이외의 문자는 변환 X (띄어쓰기, 한글 등)
+// [✅] word -> arr로 변환 후 각 index가 아스키 코드 기준 범위에 있는 값이면 변경 / 아스키 코드 65 ~ 90 (대문자), 97 ~ 122 (소문자)
+// [✅] word[i]에 해당하는 알파벳이 대문자일 경우, dictionaryUpper.indexOf(word[i])를 통해 index 찾기
+// [✅] wordArr[i]의 값을 dictionaryUpperReverse[idx]의 값으로 변경
+// [✅] word[i]에 해당하는 알파벳이 소문자일 경우, dictionaryLower.indexOf(word[i])를 통해 index 찾기
+// [✅] wordArr[i]의 값을 dictionaryLowerReverse[idx]의 값으로 변경
+// [✅] 알파벳을 제외한 이외의 문자는 변환 X (띄어쓰기, 한글 등)
+// [✅] wordArr.join('') 하여 반환
 
 function problem4(word) {
   const wordArr = word.split('');
@@ -42,8 +43,19 @@ function problem4(word) {
     alphabet.toLowerCase()
   );
   const dictionaryLowerReverse = [...dictionaryLower].reverse();
-
-  return wordArr;
+  for (let i = 0; i < word.length; i++) {
+    // word의 특정 값이 대문자인 경우
+    if (word[i].charCodeAt(0) >= 65 && word[i].charCodeAt(0) <= 90) {
+      const idx = dictionaryUpper.indexOf(word[i]);
+      wordArr[i] = dictionaryUpperReverse[idx];
+    }
+    // word의 특정 값이 소문자인 경우
+    if (word[i].charCodeAt(0) >= 97 && word[i].charCodeAt(0) <= 122) {
+      const idx = dictionaryLower.indexOf(word[i]);
+      wordArr[i] = dictionaryLowerReverse[idx];
+    }
+  }
+  return wordArr.join('');
 }
 
 console.log(problem4('I love you'));
