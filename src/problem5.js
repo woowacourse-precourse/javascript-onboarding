406c1507819b45deb187cd0fc4374ae29d50bc9a
HeoJiye <heoze1031@gmail.com>
2022-10-31 19:42:18 +0900

    (problem5) feat: 출금 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -2,8 +2,22 @@
 // 1. 최대한 큰 금액 위주로 돈을 출금한다.    
 
 function problem5(money) {
-  var answer;
+  var answer = [];
+  var moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  // 배열 초기화
+  for(var i = 0; i < 9; i++)
+    answer[i] = 0;
+
+  var i = 0;
+  while(money > 0) {
+    answer[i] = Math.floor(money/moneys[i]);
+    money -= moneys[i] * answer[i];
+    i++;
+  }
   return answer;
 }
+
+console.log(problem5(15000));
 
 module.exports = problem5;
