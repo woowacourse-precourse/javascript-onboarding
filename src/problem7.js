90e3893384f2d3b08f196701e8de10c94b0229a9
dkile <dkile532@naver.com>
2022-11-01 02:23:20 +0900

    feat: add function of extracting two degree friends

--- src/problem7.js
+++ src/problem7.js
@@ -4,10 +4,29 @@ const extractUserFriends = (user, friends) =>
     .flat()
     .filter((friend) => friend !== user);
 
+const extractTwoDegreeFriends = (user, friends) => {
+  const twoDegreeFriends = [];
+  const userExcludedFriends = friends.filter(
+    (friend) => !friend.includes(user)
+  );
+  for (const userFriend of extractUserFriends(user, friends)) {
+    twoDegreeFriends.push(
+      userExcludedFriends
+        .filter((friend) => friend.includes(userFriend))
+        .flat()
+        .filter((friend) => friend !== userFriend)
+    );
+
+    return twoDegreeFriends.flat();
+  }
+};
+
 function problem7(user, friends, visitors) {
   const userFriends = extractUserFriends(user, friends);
 
-  return userFriends;
+  const twoDegreeFriends = extractTwoDegreeFriends(user, friends);
+
+  return twoDegreeFriends;
 }
 
 module.exports = problem7;
