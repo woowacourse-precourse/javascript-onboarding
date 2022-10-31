cc4dfc8649c8a728928d85415e25621a2cfc5c95
choieastsea <choieastsea@gmail.com>
2022-11-01 07:25:23 +0900

    6) Making Object which count index of whose two sequential characters of nickname
    두글자씩 겹치면 닉네임이 비슷하다고 판단되므로, {두글자 : 해당 두글자를 갖는 이용자의 인덱스, ...}의 객체를 생성하였다

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,25 @@
 function problem6(forms) {
   var answer;
+  let nickname = [];
+  let pair_obj = {}; //두글자씩 나눠서 {"제이" : [0,1, ...]} <=> 닉네임에 "제이"를 포함한 사람의 인덱스 로 구성될 객체
+  for (let i = 0; i < forms.length; i++) {
+    nickname.push(forms[i][1]);
+    for (let j = 0; j < nickname[i].length - 1; j++) {
+      let pair_char = nickname[i].slice(j, j + 2); //두글자씩 자르기
+      let tmp = pair_obj[pair_char];
+      pair_obj[pair_char] = tmp ? [...pair_obj[pair_char], i] : [i];
+    }
+  }
+  console.log(pair_obj);
   return answer;
 }
 
 module.exports = problem6;
+
+problem6([
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+]);
