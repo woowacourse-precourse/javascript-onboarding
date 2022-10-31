108d3d68e9e4f2a1dec25236e2ac14c2e5386635
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-11-01 03:16:49 +0900

    feat : problem4 solve

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,23 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+  let arr = word.split('')
+  var temp;
+  var change;
+
+  arr.forEach(element => {
+    temp = element.charCodeAt(0);
+    if (65 <= temp && temp <= 90) {
+      change = 155 - temp;
+      answer += String.fromCharCode(change);
+    }
+    else if (97 <= temp && temp <= 122) {
+      change = 219 - temp;
+      answer += String.fromCharCode(change);
+    }
+    else {
+      answer += element;
+    }
+  });
   return answer;
 }
 
