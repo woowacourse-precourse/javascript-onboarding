b245a805109bb983ec1fca584e02ad78cd760ff3
HeoJiye <heoze1031@gmail.com>
2022-10-31 19:10:24 +0900

    (problem4) docs: 기능 목록 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,10 @@
+// < 기능 목록 >
+// 1. word를 청개구리 사전대로 변환한다.
+//    [예외처리] 알파벳 외의 문자가 있을 수 있다.
+//    [예외처리] 소문자/대문자를 구분해야한다.
+
 function problem4(word) {
-  var answer;
+  var answer
   return answer;
 }
 
