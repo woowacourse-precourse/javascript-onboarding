a419055181fb5a2f44a0885503d3e2c99d2cfe7d
100Gyeon <bjy9966@naver.com>
2022-10-31 17:25:47 +0900

    refactor: 선언형 프로그래밍 방식으로 리팩토링

--- src/problem4.js
+++ src/problem4.js
@@ -1,25 +1,23 @@
 function problem4(word) {
-  const codeNumberList = [];
-  const answerList = [];
+  const firstUpperCase = 'A'.charCodeAt(0);
   const lastUpperCase = 'Z'.charCodeAt(0);
-  const upperCaseSum = 'A'.charCodeAt(0) + 'Z'.charCodeAt(0);
-  const lowerCaseSum = 'a'.charCodeAt(0) + 'z'.charCodeAt(0);
+  const firstLowerCase = 'a'.charCodeAt(0);
+  const lastLowerCase = 'z'.charCodeAt(0);
+  const upperCaseSum = firstUpperCase + lastUpperCase;
+  const lowerCaseSum = firstLowerCase + lastLowerCase;
 
-  for (let i = 0; i < word.length; i++) {
-    codeNumberList.push(word.charCodeAt(i));
-  }
-
-  codeNumberList.forEach((number) => {
+  const codeNumberList = Array.from(word).map((character) => character.charCodeAt(0));
+  const reverseList = codeNumberList.reduce((acc, number) => {
     if (number === ' '.charCodeAt(0)) {
-      answerList.push(' ');
+      return [...acc, ' '];
     } else {
       const isUpperCase = number < lastUpperCase;
       const opposite = isUpperCase ? upperCaseSum - number : lowerCaseSum - number;
-      answerList.push(String.fromCharCode(opposite));
+      return [...acc, String.fromCharCode(opposite)];
     }
-  });
+  }, []);
 
-  return answerList.join('');
+  return reverseList.join('');
 }
 
 module.exports = problem4;
