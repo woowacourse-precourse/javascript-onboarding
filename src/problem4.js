4a5143d63b57804a143735bc5db5a043d97986f4
김미진 <kimmijin@gimmijin-ui-MacBookAir.local>
2022-10-31 22:42:17 +0900

    문제 4 기능 요구사항 4번 해결 및 코딩 완료
    answer을 return한다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,16 +1,42 @@
 //기능 목록 작성
 //1. 알파벳을 한 글자마다 배열로 저장한다.
-//1. 알파벳을 숫자로 치환해서 word값을 0붜 25까지의 숫자로 바꾼다.
-//2. 25에 바뀐 숫자들을 빼서 저장한다.
-//3. 바뀐 값을 알파벳으로 다시 바꿔서 result에 저장한다.
-//4. result를 return한다.
+//2. 아스키코드 숫자로 변환한 후 청개구리가 말한 아스키코드 값으로 변환한다.
+//3. 청개구리가 말한 아스키코드값을 다시 영어로 변환한다.
+//4. answer을 return한다.
 
 function problem4(word) {
     var answer;
     let tmpword = word;
     let tmpArray = tmpword.split("");
 
+    if (word.length < 1 || word.length > 10000) {
+        answer = "1 이상 10000이하의 문자열만을 입력하시오";
+    }
+
+    else {
+        for (let i = 0; i < tmpArray.length; i++) {
+            if (tmpArray[i] == ' ') {
+                tmpArray[i] = ' ';
+            }
+            else {
+                tmpArray[i] = tmpword.charCodeAt(i);
+                let tmpNum = tmpArray[i];
+
+                //소문자 변환
+                if (tmpNum >= 97 && tmpNum <= 122) {
+                    let tmpCount2 = 219 - tmpNum;
+                    tmpArray[i] = String.fromCharCode(tmpCount2);
+                }
+
+                //대문자 변환
+                if (tmpNum >= 65 && tmpNum <= 90) {
+                    let tmpCount = 155 - tmpNum;
+                    tmpArray[i] = String.fromCharCode(tmpCount);
+                }
+            }
+        }
+        answer = tmpArray.join('');
+    }
     return answer;
 }
-
 module.exports = problem4;
