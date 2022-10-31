363162408a1421ba321fb271108f34a31d95bf1d
hhhminme <hororonoodle@gmail.com>
2022-11-01 01:11:20 +0900

    fix : edit valuable name typo

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,5 @@
 // 출금 가능한 금액 Enum
-const WithdrawList = {
+const withDrawList = {
   fiftyThousand: 50000,
   tenThousand: 10000,
   fiveThousand: 5000,
@@ -13,13 +13,13 @@ const WithdrawList = {
 
 function problem5(money) {
   let balance = money;
-  const widthDrawArray = Object.keys(WithdrawList); // 출금 가능한 금액의 목록을 가져온다.
-  const answer = Array.from({ length: widthDrawArray.length }, () => 0);
+  const withDrawArray = Object.keys(withDrawList); // 출금 가능한 금액의 목록을 가져온다.
+  const answer = Array.from({ length: withDrawArray.length }, () => 0);
 
   // 계좌에서 가장 큰 금액 순서대로 출금을 시도한다.
-  widthDrawArray.map((v, i) => {
-    answer[i] = Math.floor(balance / widthDrawArray[v]); // 출금 가능한 지폐 갯수를 구한다.
-    balance -= widthDrawArray[v] * answer[i]; // 출금된 금액만큼 계좌에서 빼준다.
+  withDrawArray.map((v, i) => {
+    answer[i] = Math.floor(balance / withDrawList[v]); // 출금 가능한 지폐 갯수를 구한다.
+    balance -= withDrawList[v] * answer[i]; // 출금된 금액만큼 계좌에서 빼준다.
   });
 
   return answer;
