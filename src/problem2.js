82007117876978ee1f610dfa37635b77f04ceae8
Saans <92422813+saans808@users.noreply.github.com>
2022-10-31 16:11:54 +0900

    [p2] 배열의 마지막 값과 char을 비교하여 같으면 마지막 값을 없애고 아니면 문자를 배열에 넣은 후 문자열 하나로 묶기

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,10 @@
 function problem2(cryptogram) {
   var answer;
+  let arr = [];
+  for (let char of cryptogram) {
+    arr[arr.length - 1] === char ? arr.pop() : arr.push(char);
+  }
+  answer = arr.join('');
   return answer;
 }
 
