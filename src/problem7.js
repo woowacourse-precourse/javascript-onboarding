5a6117216deea53dedec8c57c0bba4be331b8249
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-10-31 21:32:57 +0900

    ✨ problem7 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,33 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+const problem7 = (user, friends, visitors) => {
+  const relationShip = {};
+  const relationScore = {};
+
+  friends.forEach(([friendA, friendB]) => {
+    relationShip[friendA] = relationShip[friendA]
+      ? [...relationShip[friendA], friendB]
+      : [friendB];
+    relationShip[friendB] = relationShip[friendB]
+      ? [...relationShip[friendB], friendA]
+      : [friendA];
+  });
+
+  Object.keys(relationShip).forEach((person) => {
+    if (person !== user) {
+      relationScore[person] +=
+        relationShip[person].filter((someone) =>
+          relationShip[user].includes(someone)
+        ).length * 10;
+    }
+  });
+
+  visitors.forEach((visitor) => {
+    relationScore[visitor] = relationScore[visitor] + 1 ?? 1;
+  });
+
+  return Object.keys(relationScore)
+    .filter((existed) => !relationShip[user].includes(existed))
+    .sort((a, b) => relationScore[b] - relationScore[a])
+    .slice(0, 5);
+};
 
 module.exports = problem7;
