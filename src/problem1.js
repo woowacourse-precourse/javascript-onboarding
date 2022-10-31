736b217ddb0ea3ddec5d60e81620910a790f7dd4
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-10-31 13:00:01 +0900

    feat: [문제 1] 예외처리 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,36 @@
+const FIRSTPAGE = 1;
+const LASTPAGE = 400;
+const NEERPAGE = 1;
+
+const isWrongPageRange = (bookPage) => {
+  const [leftPage, _] = bookPage;
+  return leftPage < 1 || leftPage > 400;
+};
+const isLeftPageHasEvenPage = (bookPage) => {
+  const [leftPage, _] = bookPage;
+  return leftPage % 2 === 0;
+};
+const isBookPageToneApart = (bookPage) => {
+  const [leftPage, rightPage] = bookPage;
+  return rightPage - leftPage !== NEERPAGE;
+};
+
+const isFirstPage = (bookPage) => bookPage.includes(FIRSTPAGE);
+
+const isLastPage = (bookPage) => bookPage.includes(LASTPAGE);
+
+const isExceptionSituationOccurred = (bookPage) => {
+  return (
+    isWrongPageRange(bookPage) ||
+    isLeftPageHasEvenPage(bookPage) ||
+    isBookPageToneApart(bookPage) ||
+    isFirstPage(bookPage) ||
+    isLastPage(bookPage)
+  );
+};
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isExceptionSituationOccurred(pobi) || isExceptionSituationOccurred(crong))
+    return -1;
 }
 
 module.exports = problem1;
