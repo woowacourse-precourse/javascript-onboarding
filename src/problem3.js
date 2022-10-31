ca15ddb403514d28b1a5e4853ae7e64006aeec4b
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-11-01 02:48:32 +0900

    feat : problem3 solve

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,22 @@
+function count(n) {
+  var result = 0;
+  var str = String(n);
+  const mapfn = (arg) => Number(arg);
+  let arr = str.split('').map(mapfn);
+
+  for(let i = 0; i < arr.length; i++ ) {
+    if (arr[i] === 3 || arr[i] === 6 || arr[i] === 9) {
+      result += 1;
+    } 
+  }
+  return result
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for(let i = 1; i <= number; i++ ) {
+    answer += count(i);
+  }
   return answer;
 }
 
