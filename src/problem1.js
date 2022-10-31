40237f478350d65f289437d63256c7b9e5bbf079
Jeonjunwoo <tira618@naver.com>
2022-10-31 15:18:46 +0900

    책 페이지의 각 자리수를 더하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,14 @@
 function problem1(pobi, crong) {
-  var answer;
+  var answer = page(pobi,crong);
   return answer;
 }
+function sum(n){    //배열의 값을 각각 더하는 함수
+  let answer = 0
+  n = n.toString()
+  for (let i = 0; i < n.length; i++){
+    answer += parseInt(n[i])
+  }
+  return answer; 
 
+  
 module.exports = problem1;
