cbe94557e4821d105056aa07e71d80fdcb8b9a14
Buyoung Chung <sunarol@naver.com>
2022-10-31 17:31:50 +0900

    [문제1] 예외 사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -12,7 +12,23 @@ function calculator(pages) {
   }
   return Math.max(...pageResult);
 }
+function exceptionHandler(pobi, crong) {
+  if (
+    pobi[1] - pobi[0] !== 1 ||
+    crong[1] - crong[0] !== 1 ||
+    pobi[0] === 1 ||
+    crong[0] === 1 ||
+    pobi[1] === 400 ||
+    crong[1] === 400 ||
+    pobi[0] % 2 === 0 ||
+    crong[0] % 2 === 0 ||
+    pobi[1] % 2 === 1 ||
+    crong[1] % 2 === 1
+  )
+    return true;
+}
 function problem1(pobi, crong) {
+  if (exceptionHandler(pobi, crong)) return -1;
   var answer = [];
   answer.push(calculator(pobi), calculator(crong));
   if (answer[0] > answer[1]) {
