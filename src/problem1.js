129ccf12cf89e93136486d7d3a5ac7b07a6ddce2
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 21:19:47 +0900

    [Feat:Problem2] 곱하기

--- src/problem1.js
+++ src/problem1.js
@@ -15,3 +15,9 @@ const getSum = (number) => {
     .split("")
     .reduce((sum, str) => (sum += +str), 0);
 };
+
+const getMultiply = (number) => {
+  return String(number)
+    .split("")
+    .reduce((total, str) => (total *= +str), 1);
+};
