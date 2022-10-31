b3a1e35fc60ec0b53a69c1bfc42d6908d91ab3a7
oseunghyun <ohsh1477@gmail.com>
2022-10-31 14:12:49 +0900

    feat(p7): 함께 아는 친구를 기준으로 추천 점수를 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,39 @@
+// 함께 아는 친구를 기준으로 추천 점수를 구하는 기능
+const scoreFromMutual = (user, friendsArr) => {
+  let friendsWithUser = friendsArr.filter((friends) => {
+    return friends[0] == user || friends[1] == user;
+  });
+  friendsWithUser = friendsWithUser.reduce((prev, curr) => prev.concat(curr));
+  friendsWithUser = friendsWithUser.filter((friends) => {
+    return friends != user;
+  });
+
+  let friendsNotWithUser = friendsArr.filter((friends) => {
+    return friends[0] != user && friends[1] != user;
+  });
+
+  let mutualObj = {};
+  for (i = 0; i < friendsWithUser.length; i++) {
+    for (j = 0; j < friendsNotWithUser.length; j++) {
+      if (friendsWithUser[i] == friendsNotWithUser[j][0]) {
+        if (Object.keys(mutualObj).includes(friendsNotWithUser[j][1])) {
+          mutualObj[friendsNotWithUser[j][1]] += 10;
+        } else {
+          mutualObj[friendsNotWithUser[j][1]] = 10;
+        }
+      } else if (friendsWithUser[i] == friendsNotWithUser[j][1]) {
+        if (Object.keys(mutualObj).includes(friendsNotWithUser[j][0])) {
+          mutualObj[friendsNotWithUser[j][0]] += 10;
+        } else {
+          mutualObj[friendsNotWithUser[j][0]] = 10;
+        }
+      }
+    }
+  }
+
+  return mutualObj;
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
