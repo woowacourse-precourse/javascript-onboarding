c18c82f6dddb4d3ad84d0d05e6e2b8bca7e0b5b8
Narcoker <jykim9868@gmail.com>
2022-10-31 17:19:16 +0900

    feat: 1주차 4번 문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,26 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  let upperAlpha = [];
+  let lowerAlpha = [];
+
+  for (let i = 0; i < 26; i++){
+    upperAlpha.push(String.fromCharCode(65 + i));
+    lowerAlpha.push(String.fromCharCode(97 + i));
+  }
+  
+  for (let i = 0; i < word.length; i++){
+    let ascii = word[i].charCodeAt(0);
+    
+    if (ascii >= 97 && ascii <= 122) 
+      answer += lowerAlpha.at(-(lowerAlpha.indexOf(word[i]) + 1));
+    else if (ascii >= 65 && ascii <= 90)
+      answer += upperAlpha.at(-(upperAlpha.indexOf(word[i]) + 1));
+    else
+      answer += word[i];
+  }
+
   return answer;
 }
+
 
 module.exports = problem4;
