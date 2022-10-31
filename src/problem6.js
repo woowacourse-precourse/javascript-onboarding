97cd3521f3fd05db2670c739069e65145b66c236
Buyoung Chung <sunarol@naver.com>
2022-10-31 18:21:25 +0900

    [문제6] 모든 크루원을 서로 비교할 수 있게 반복문
    을 추가한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,8 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = i + 1; j < forms.length; j++) {}
+  }
   return answer;
 }
 
