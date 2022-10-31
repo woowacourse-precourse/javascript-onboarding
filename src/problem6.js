d21ad5b234406ceb7bf68db3be9a9467e0a552d1
Narcoker <jykim9868@gmail.com>
2022-11-01 00:27:10 +0900

    fix: 예외 케이스 발견 & 재풀이

--- src/problem6.js
+++ src/problem6.js
@@ -1,33 +1,33 @@
 function problem6(forms) {
-  function verify(nickName, index) {
-    for (let k = 0; k < nickName.length - 1; k++) {
-      let str = nickName.slice(k, k + 2);
-
-      for (let targetIndex = index + 1; targetIndex < nickNames.length; targetIndex++) {
-        if (nickNames[targetIndex].includes(str)) {
-          answer.add(forms.get(nickName));
-          answer.add(forms.get(nickNames[targetIndex]));
-          return;
-        }
+  const group = new Map();
+  const answer = new Set();
+  
+  for(let form of forms){
+    for (let i = 0; i < form[1].length - 1; i++){
+      let str = form[1].slice(i, i + 2);
+      if (group.has(str)) {
+        let newUsers = group.get(str);
+        newUsers.push(form[0]);
+        group.set(str, newUsers);
+        break;
+      } else {
+        group.set(str, [form[0]]);
       }
     }
   }
 
-  forms = forms.reduce((res, v) => {
-    res.set(v[1], v[0]);
-    return res;
-  }, new Map());
-  const answer = new Set();
-  let nickNames = [...forms.keys()];
-
-  for (let i = 0; i < nickNames.length; i++)
-    verify(nickNames[i], i);
+  for (let users of group) {
+    if (users[1].length >= 2) {
+      for (let user of users[1]) 
+        answer.add(user);
+    }
+  }
 
   return [...answer].sort((a, b) => {
     if (a > b) return 1;
     else if (a < b) return -1;
     else return 0;
-  });
+  })
 }
 
 module.exports = problem6;
