d0a458e686570e80d905f504804c2fbbccd22359
Jungkyu Yang <67031524+jungkyuYang@users.noreply.github.com>
2022-10-31 16:06:04 +0900

    feat: [problem2] Add problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    stack.push(cryptogram[i]);
+
+    if (stack[stack.length - 1] == stack[stack.length - 2]) {
+      stack.pop();
+      stack.pop();
+    }
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
