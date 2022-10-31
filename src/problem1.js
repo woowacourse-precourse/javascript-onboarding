3be85451460050398737dda26168c3b9b5096fae
hwangsungjae <edx2fire@likelion.org>
2022-10-31 13:48:10 +0900

    [온보딩 - 01] 테스트 케이스 고려한 로직 추가 및 조건문 삼항연산자로 리팩토링

--- src/problem1.js
+++ src/problem1.js
@@ -1,7 +1,8 @@
 function problem1(pobi, crong) {
   if (pobi[0] - pobi[1] !== -1 || crong[0] - crong[1] !== -1) return -1;
+  if (pobi.some(num => num >= 400 || num <= 1) || crong.some(num => num >= 400 || num <= 1)) return -1;
 
-  let answer;
+  let answer = 0;
   let pobiMax = 0;
   let crongMax = 0;
 
@@ -11,8 +12,7 @@ function problem1(pobi, crong) {
   pobi.forEach(num => pobiMax = Math.max(pobiMax, getSum(num), getMultiply(num)));
   crong.forEach(num => crongMax = Math.max(crongMax, getSum(num), getMultiply(num)));
 
-  if (pobiMax === crongMax) answer = 0;
-  else answer = pobiMax > crongMax ? 1 : 2;
+  answer = pobiMax === crongMax ? 0 : pobiMax > crongMax ? 1 : 2;
 
   return answer;
 }
