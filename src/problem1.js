5b24c9359a9d03985bef26485c486aa68dad354c
망망 <mang@mangmang-ui-MacBookPro.local>
2022-11-01 06:52:28 +0900

    문제1

--- src/problem1.js
+++ src/problem1.js
@@ -4,3 +4,23 @@ function problem1(pobi, crong) {
 }
 
 module.exports = problem1;
+
+
+/*
+ 변수 키워드 : let
+ let message = 'hello';
+ var도 존재하나 오래된 방식
+
+ 상수 키퉈드 : const - 변화하지 않는 변수
+ const mtBirthday = '01.01.2022'
+
+ */
+
+ /*
+ 자료형
+ 1. 숫자형 : 정수 및 부동소수점 숫자
+ let n = 123;
+ 연산 : +, -, *, /
+ 특수 숫자값 : Infinity, -Infinity - 어떤 숫자든 0으로 나누면 무한대 값, NaN - 에러 값
+ 
+ */
