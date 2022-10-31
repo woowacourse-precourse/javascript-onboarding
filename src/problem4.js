b7dedd79f7045db8098d74d84ff961aa6b6afde0
OrangeVinyl <hap_hazard@naver.com>
2022-10-31 23:41:32 +0900

    Problem4) Map, 정규식 사용 알파벳제외 문자 정상반환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,12 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const aplha = word.split("");
+
+  let answer = aplha.map((r) => {
+    const regex = /^[a-z|A-Z]/;
+    if (!regex.test(r)) return r;
+  });
+
+  return answer.join("");
 }
 
 module.exports = problem4;
