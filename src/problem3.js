72495267ddd0bd58c7b2dd4f0905ef3d9f6920b4
nara04040 <nara04040@naver.com>
2022-10-31 15:45:04 +0900

    [problem3] feat : number수만큼 반복될 때 3,6,9가 존재한다면 answer에 추가한다.

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,9 @@ function problem3(number) {
   let answer = 0;
   for (let i = 0; i <= number; i++) {
     let gameArr = String(i);
+    if (gameArr.match(/[3,6,9]/g)) {
+      answer += gameArr.match(/[3,6,9]/g).length;
+    }
   }
   return answer;
 }
