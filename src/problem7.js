c6af6c3dd0f41d8bfc0d4af21e499cf4c01df2a8
JSH99 <tjgus9966@naver.com>
2022-11-01 04:35:53 +0900

    feat: 함께 아는 친구 수 점수 계산

--- src/problem7.js
+++ src/problem7.js
@@ -12,6 +12,15 @@ function problem7(user, friends, visitors) {
       userFriends.push(friends[i][0] === user ? friends[i][1] : friends[i][0]);
   }
 
+  // 2. +(사용자와 함께 아는 친구의 수 * 10)
+  for(var i = 0; i < friends.length; i++) {
+    for(var j = 0; j < userFriends.length; j++) {
+      if(friends[i].includes(userFriends[j])) {
+        score[friends[i][0] === userFriends[j] ? friends[i][1] : friends[i][0]] += 10;
+      }
+    }
+  }
+
   return answer;
 }
 
