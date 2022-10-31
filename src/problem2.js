f573f07debc5bae226ab47aac6b1301311f9006e
HANSANGJUN <hsj11962@naver.com>
2022-10-31 20:01:08 +0900

    2번문제 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,22 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [cryptogram[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    const curr = cryptogram[i];
+
+    if (stack.length === 0) {
+      stack.push(curr);
+    } else {
+      const prev = stack[stack.length - 1];
+
+      if (prev === curr) {
+        stack.pop();
+      } else {
+        stack.push(curr);
+      }
+    }
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
