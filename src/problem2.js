5e59ab541720187d4d7677048523e62bdc5a4911
sein <hyuntpdls@gmail.com>
2022-10-31 16:04:47 +0900

    [문제2] feat: 문자열 배열로 변환

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
   var answer;
+  
   return answer;
+}
+
+// 문자열을 배열로 변환
+function separateString(string) {
+  let array = string.split("");
+  return array;
 }
 
 module.exports = problem2;
