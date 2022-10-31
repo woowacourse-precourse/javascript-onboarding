f0d23c609d2fc92ad0ed6ec5d1bcb9c814358737
leeuihyun <meta212121@naver.com>
2022-10-31 21:05:39 +0900

    refactor : problem5 가독성 향상을 위한 코드 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,25 +1,16 @@
-function classfication(result, money) {
-  let start = 50000;
-  result = result.map((item, index) => {
-    item += parseInt(money / start);
-    money = parseInt(money % start);
-    if (start === 10) {
-      start /= 10;
-    } else if (index % 2 === 0) {
-      start /= 5;
-    } else {
-      start /= 2;
-    }
-    return item;
+function classfication(moneyUnit, money) {
+  moneyUnit = moneyUnit.map((item) => {
+    const result = parseInt(money / item);
+    money %= item;
+    return result;
   });
-  return result;
+  return moneyUnit;
 }
-function makeArray(number) {
-  return new Array(number).fill(0);
+function moneyUnit() {
+  return [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 }
 function problem5(money) {
-  let answer = makeArray(9);
-  return classfication(answer, money);
+  return classfication(moneyUnit(), money);
 }
 
 module.exports = problem5;
