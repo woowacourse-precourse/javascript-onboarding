b3d87688921995ef9002736d303e50423f1d8ced
SorinKwon <ksl020700@gmail.com>
2022-11-01 00:05:49 +0900

    feat(problem6): 닉네임을 두 글자로 나누는 기능
    닉네임을 두 글자로 나누는 기능을 추가함

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,15 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+function divideString(n) {
+    let result = [];
+    for (let i = 0; i < n.length; i++) {
+        const a = n[i][1];
+        for(let j=0;j<a.length-1;j++){
+            result.push(a.substr(j,2));
+        }
+    }
+    return result;
 }
-
+function problem6(forms) {
+    let answer;
+    return answer;
+}
 module.exports = problem6;
