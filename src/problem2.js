6e752e47d191625e7574e5308aae78c4a83d82ae
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:26:28 +0900

    2번 문제 앞뒤로 같은 알파벳이면 제거하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
   var answer;
   return answer;
+  let check=true;
+  while(check){
+    check=false;
+    for(let i=0; i<cryptogram.length;i++){
+      if(cryptogram.charAt(i)==cryptogram.charAt(i+1)){
+        cryptogram= cryptogram.slice(0,i) + cryptogram.slice(i+2);
+        check=true;
+      }
+    }
+  }
 }
-
+answer=cryptogram;
 module.exports = problem2;
