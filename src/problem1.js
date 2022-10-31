5da4626e0fc26ef33985a14baf3ad16d8eae8fa5
“somm12” <sominan8744@gmail.com>
2022-10-31 17:46:48 +0900

    problem1 : 예외처리 함수 checkException 정의

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,25 @@
+function checkException(arr) {
+  const diff = arr[1] - arr[0] === 1 ? true : false;
+  var range = true;
+  for (let i = 0; i < arr.length; i++) {
+    if (arr[i] >= 0 && arr[i] <= 400) {
+      range = range && true;
+    } else {
+      range = range && false;
+    }
+  }
+  return diff && range;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  //예외처리
+  const checkP = checkException(pobi);
+  const checkC = checkException(crong);
+  if (!checkP || !checkC) {
+    answer = -1;
+    return answer;
+  }
   return answer;
 }
 
