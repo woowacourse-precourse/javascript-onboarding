b3b8f9f53311fcac0604f1a8b90817e5c7ac45e4
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 02:06:07 +0900

    fix[P1]: prettier 코드스타일 적용

--- src/problem1.js
+++ src/problem1.js
@@ -1,55 +1,65 @@
 function problem1(pobi, crong) {
-  let answer;
-  if(checkValid(pobi,crong)){
-    let pobiMax = getMaxValue(pobi);
-    let crongMax = getMaxValue(crong);
-    answer = returnAnswer(pobiMax,crongMax);
-  } else{
-    answer = -1;
-  }
+	let answer;
+	if (checkValid(pobi, crong)) {
+		let pobiMax = getMaxValue(pobi);
+		let crongMax = getMaxValue(crong);
+		answer = returnAnswer(pobiMax, crongMax);
+	} else {
+		answer = -1;
+	}
 
-  function checkValid(arr1, arr2){
-    //예외 check
-    let checkRange = (page) => page<1 || page>400;
+	function checkValid(arr1, arr2) {
+		//예외 check
+		let checkRange = (page) => page < 1 || page > 400;
 
-    if(arr1.some(checkRange) || arr2.some(checkRange)) return 0;
-    if(arr1[0] % 2 !== 1 || arr2[0] % 2 !== 1) return 0;
-    if(arr1[1]-arr1[0] !== 1 || arr2[1]-arr2[0] !== 1) return 0;
-    if(arr1[0] == 1 || arr1[0] == 399 || arr2[0] == 1 || arr1[1] == 399) return 0;
-    
-    return 1;
-  }
+		if (arr1.some(checkRange) || arr2.some(checkRange)) return 0;
+		if (arr1[0] % 2 !== 1 || arr2[0] % 2 !== 1) return 0;
+		if (arr1[1] - arr1[0] !== 1 || arr2[1] - arr2[0] !== 1) return 0;
+		if (arr1[0] == 1 || arr1[0] == 399 || arr2[0] == 1 || arr1[1] == 399)
+			return 0;
 
-  function getMaxValue(arr){
-    let leftDigitArray = arr[0].toString().split("").map(str => Number(str));
-    let rightDigitArray = arr[1].toString().split("").map(str => Number(str));
-    let leftArrPlus = 0;
-    let rightArrPlus = 0;
-    let leftArrMultiple = 1;
-    let rightArrMultiple = 1;
-    leftDigitArray.forEach(element => {
-      leftArrPlus+=element;
-      leftArrMultiple*=element;
-    });
-    rightDigitArray.forEach(element => {
-      rightArrPlus+=element;
-      rightArrMultiple*=element;
-    });
-    return compareInt(compareInt(leftArrPlus,leftArrMultiple),compareInt(rightArrPlus,rightArrMultiple))
-  }
+		return 1;
+	}
 
-  function compareInt(a,b){
-    if(a>b) return a;
-    else return b;
-  }
+	function getMaxValue(arr) {
+		let leftDigitArray = arr[0]
+			.toString()
+			.split("")
+			.map((str) => Number(str));
+		let rightDigitArray = arr[1]
+			.toString()
+			.split("")
+			.map((str) => Number(str));
+		let leftArrPlus = 0;
+		let rightArrPlus = 0;
+		let leftArrMultiple = 1;
+		let rightArrMultiple = 1;
+		leftDigitArray.forEach((element) => {
+			leftArrPlus += element;
+			leftArrMultiple *= element;
+		});
+		rightDigitArray.forEach((element) => {
+			rightArrPlus += element;
+			rightArrMultiple *= element;
+		});
+		return compareInt(
+			compareInt(leftArrPlus, leftArrMultiple),
+			compareInt(rightArrPlus, rightArrMultiple)
+		);
+	}
 
-  function returnAnswer(pobi,crong){
-    if(pobi>crong) return 1;
-    else if(pobi<crong) return 2;
-    else return 0;
-  }
-  
-  return answer;
+	function compareInt(a, b) {
+		if (a > b) return a;
+		else return b;
+	}
+
+	function returnAnswer(pobi, crong) {
+		if (pobi > crong) return 1;
+		else if (pobi < crong) return 2;
+		else return 0;
+	}
+
+	return answer;
 }
 
 module.exports = problem1;
