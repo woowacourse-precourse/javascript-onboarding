c2cfc5a23a73df2ae17567030e8b99f04740d672
knh6269 <knh6269@gmail.com>
2022-10-31 22:36:59 +0900

    feat: 미션2) 같은 문자들을 지워주는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -6,6 +6,16 @@ const eraseArray = (cryptogram, index) => {
   let count = 1; //자르는 갯수
   let j = index;
 
+  //같지 않을때 까지 인덱스를 증가시킨다.
+  while (cryptogram[j] === cryptogram[j+1]) {
+      count++;
+      j++;
+  }
+
+  //같은 갯수만큼 잘라준다.
+  cryptogram.splice(index, count);
+}
+
 }
 
 module.exports = problem2;
