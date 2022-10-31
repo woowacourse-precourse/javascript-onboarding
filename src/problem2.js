2754674b271dd5abc54cfe65e85808192f18682a
DAIN KIM <dnkim419@gmail.com>
2022-10-31 20:42:24 +0900

    feat: solved problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,29 @@
+function removeDuplicate(cryptogram) {
+  var tmp = cryptogram[0];
+  var answer = "";
+
+  for (i = 1; i < cryptogram.length; ++i) {
+    if (tmp[0] === cryptogram[i]) tmp += cryptogram[i];
+    else {
+      if (tmp.length === 1) answer += tmp;
+      tmp = cryptogram[i];
+    }
+  }
+
+  if (tmp.length === 1) answer += tmp;
+
+  return answer;
+}
+
 function problem2(cryptogram) {
   var answer;
+
+  while (cryptogram !== removeDuplicate(cryptogram)) {
+    cryptogram = removeDuplicate(cryptogram);
+  }
+
+  var answer = cryptogram;
+
   return answer;
 }
 
