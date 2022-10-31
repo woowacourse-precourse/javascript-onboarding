6e2b074a96918511a3a79b324378d999439082cd
OrangeVinyl <hap_hazard@naver.com>
2022-11-01 01:04:50 +0900

    Problem5) for문 순회 및 문제구현 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
+  var answer = 0;
   const regex = /3|6|9/g;
-  answer += (String(i).match(regex) || []).length;
+  for (let i = 1; i <= number; i++) {
+      answer += (String(i).match(regex) || []).length;
+  }
+  return answer;
 }
 
 module.exports = problem3;
