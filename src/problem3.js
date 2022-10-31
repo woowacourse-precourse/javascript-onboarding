871f840ced071dcabb2e83cba5288a8d5d3283b4
knh6269 <knh6269@gmail.com>
2022-10-31 22:41:24 +0900

    feat : 미션3) 메인 함수

--- src/problem3.js
+++ src/problem3.js
@@ -5,6 +5,15 @@ const getClap = (num) => {
   return clap ? clap.length : 0;
 }
 
+//메인함수
+function problem3(number){
+  let answer = 0;
+
+  //숫자를 1부터 증가시키며 확인한다.
+  for(let i = 1; i <= number; i++){
+    answer += getClap(i);
+  }
+
   return answer;
 }
 
