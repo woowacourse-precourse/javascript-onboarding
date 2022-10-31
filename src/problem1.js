64116ba6c0a476fd58029eb8cfbfebc89f5bd89a
SeongOh Park <qkrtjddh1212@naver.com>
2022-10-31 23:28:40 +0900

    fix[P1]: 예외 홀수페이지 체크 수정

--- src/problem1.js
+++ src/problem1.js
@@ -11,8 +11,8 @@ function problem1(pobi, crong) {
     let checkRange = (page) => page<1 || page>400;
 
     if(arr1.some(checkRange) || arr2.some(checkRange)) return 0;
-    if(arr1[0] % 2 !== 0 || arr2[0] % 2 !== 0) return 0;
-    if(arr1[1]-arr1[0] !== 1 && arr2[1]-arr2[0] !== 1) return 0;
+    if(arr1[0] % 2 !== 1 || arr2[0] % 2 !== 1) return 0;
+    if(arr1[1]-arr1[0] !== 1 || arr2[1]-arr2[0] !== 1) return 0;
     if(arr1[0] == 1 || arr1[0] == 399 || arr2[0] == 1 || arr1[1] == 399) return 0;
     
     return 1;
