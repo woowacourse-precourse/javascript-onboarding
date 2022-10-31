2d22fd6222f7b80906be67123e79c7d9e7d27ef5
terry <kdjtiger@naver.com>
2022-10-31 17:59:05 +0900

    받은 숫자를 배열에 넣기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
+  numToArr(number);
+
   var answer;
   return answer;
 }
 
+function numToArr(num){
+  let numArr = new Array;
+  for (let i = 1; i < num + 1; i++){
+    numArr.push(i);
+  }
+  console.log(numArr);
+}
+
 module.exports = problem3;
+
+problem3(13);
