15a1815037a6c2048ac50957097af74521882837
연 <mogoo22@naver.com>
2022-10-31 17:01:30 +0900

    refactor(문제4): 아스키코드번호 범위값을 상수에서 A, Z의 아스키코드 변환 결과값으로 수정함

--- src/problem4.js
+++ src/problem4.js
@@ -1,16 +1,14 @@
 const defineASCIIRange = (string) => {
-  const [MIN_UPPER_ASCII, MAX_UPPER_ASCII] = [65, 90];
-  const [MIN_LOWER_ASCII, MAX_LOWER_ASCII] = [97, 122];
-  const MIN_ASCII = string.toUpperCase() === string ? MIN_UPPER_ASCII : MIN_LOWER_ASCII;
-  const MAX_ASCII = string.toUpperCase() === string ? MAX_UPPER_ASCII : MAX_LOWER_ASCII;
-  return [MIN_ASCII, MAX_ASCII];
+  if (string.toUpperCase() === string) {
+    return ["A".charCodeAt(), "Z".charCodeAt()];
+  }
+  return ["a".charCodeAt(), "z".charCodeAt()];
 };
 
 const reverseWord = (string) => {
-  const [MIN_ASCII, MAX_ASCII] = defineASCIIRange(string);
-  const WORD_ASCII = string.charCodeAt(0);
-  const REVERSE_ASCII = MAX_ASCII - WORD_ASCII + MIN_ASCII;
-  return String.fromCharCode(REVERSE_ASCII);
+  const [minASCII, maxASCII] = defineASCIIRange(string);
+  const reverseASCII = maxASCII - string.charCodeAt() + minASCII;
+  return String.fromCharCode(reverseASCII);
 };
 
 function problem4(word) {
