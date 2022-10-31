05e401b631a38517744a6931068ca8ceda1d0c74
OrangeChick <retww@naver.com>
2022-10-31 09:30:56 +0900

    feat: ⛳️ Create solution of problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  let numArr = [];
+
+  for (let i = 1; i <= number; i++) {
+    numArr.push(i.toString().split(""));
+  }
+
+  numArr = numArr.flat();
+
+  for (let i = 0; i < numArr.length; i++) {
+    if (numArr[i] === "3" || numArr[i] === "6" || numArr[i] === "9") answer++;
+  }
+
   return answer;
 }
 
