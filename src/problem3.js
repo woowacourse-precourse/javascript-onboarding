a6054f221636c1a1588a312791689e1e4c8c9bf2
FRANZ <franzcho@SUNGui-MacBookPro.local>
2022-11-01 02:05:09 +0900

    레쓰고우테코

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
+  let answer = "";
+
+  for (let i = number.length - 1; i >= 0; i--){
+    answer = answer + number[i];
+  }
   return answer;
 }
+
+console.log(problem3("I love you"));
 
 module.exports = problem3;
--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,10 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+
+  for (let i = word.length - 1; i >= 0; i--){
+    answer = answer + word[i];
+  }
+  
   return answer;
 }
 
