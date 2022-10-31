3f59ead07ec7f05451ffe44902230f5945c4bb05
afds4567 <afds4567@gmail.com>
2022-11-01 00:01:34 +0900

    feat(problem6): ; 전체 로직 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const warningList = new Set();
+  const map = new Map();
+  for (let i = 0; i < forms.length; i++) {
+    const [email, nickname] = forms[i];
+    if (nickname.length < 2) {
+      break;
+    }
+    for (let j = 0; j < nickname.length - 1; j++) {
+      let twoWord = nickname.substring(j, j + 2);
+      if (map.has(twoWord)) {
+        let savedEmail = map.get(twoWord);
+        if (savedEmail !== email) {
+          warningList.add(savedEmail);
+          warningList.add(email);
+        }
+      }
+      map.set(twoWord, email);
+    }
+  }
+  return [...warningList].sort();
 }
-
 module.exports = problem6;
