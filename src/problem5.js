4dc15d0f12f3dfb1dd1b3ce7b23748377977414f
areumsheep <areumsheep@gmail.com>
2022-11-01 03:00:14 +0900

    add: 문제5 답안 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
-function problem5(money) {
-  var answer;
-  return answer;
+const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+function problem5(money = 0) {
+  const result = [];
+  MONEY_UNIT.forEach((unit) => {
+    result.push(Math.trunc(money / unit));
+    money = money % unit;
+  });
+  return result;
 }
 
 module.exports = problem5;
