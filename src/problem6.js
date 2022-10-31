353bb6df29f9e1249999890ad23d45584807d5df
nara04040 <nara04040@naver.com>
2022-10-31 19:53:59 +0900

    [problem6] feat : 같은 글자 구분후 값이 null인 값만 출력

--- src/problem6.js
+++ src/problem6.js
@@ -1,9 +1,21 @@
 function problem6(forms) {
   let answer = [];
   forms.forEach((ele, nameIdx) => {
-    console.log(ele, nameIdx);
+    for (let i = nameIdx + 1; i < forms.length; i++) {
+      for (let j = 0; j < ele[1].length; j++) {
+        if (forms[i][1].search(ele[1].substring(j, 2)) !== -1) {
+          answer.push(ele, forms[1]);
+        }
+      }
+    }
   });
-  return answer;
+
+  const reduceAnswer = answer.reduce((pre, cur) => {
+    pre[cur[0]] = null;
+    return pre;
+  }, {});
+
+  return reduceAnswer;
 }
 let forms = [
   ["jm@email.com", "제이엠"],
