cbce742ad72735e6f556a0fff6a75cf08aff8bc8
KUD <kimud6003@gmail.com>
2022-11-01 01:14:36 +0900

    ✨ Feature: problem4.js frogChanger - problem4.js 문자 변환 함수 frogChanger 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,25 @@
 function problem4(word) {
-  var answer;
+  let answer = frogChanger(word);
   return answer;
+}
+
+function frogChanger(word) {
+  let wordList = word.split("");
+  let result ="";
+
+  for(let char of wordList){
+    const charCode= char.charCodeAt(0);
+    if(charCode  >= 65 && charCode <= 90) {
+      result += String.fromCharCode(155-charCode);
+    } else if(charCode>= 97 && charCode <= 122) {
+      result += String.fromCharCode(219-charCode);
+    }else {
+      result += char;
+    }
+  }
+
+  return result;
+
 }
 
 module.exports = problem4;
