48d862a2d6a5fb0b192f807dfec93af18588f237
HBSPS <wiyeong79@naver.com>
2022-10-31 18:42:59 +0900

    :technologist: [문제4] 메소드 기능 및 형식 설명용 주석 추가

--- src/problem4.js
+++ src/problem4.js
@@ -2,14 +2,19 @@ function problem4(word) {
   var answer;
   if (!validType) throw new Error("잘못된 타입입니다.");
   if (!validRange) throw new Error("잘못된 범위입니다.");
-  if (!validLetters) throw new Error("잘못된 형식입니다.");
+  if (!validWord) throw new Error("잘못된 형식입니다.");
 
   answer = checkLetter(word);
   return answer;
 }
 
 
-// 정답을 return 하는 메소드 (공백인 경우, 그대로 정답 배열에 추가)
+// 정답과 관련된 메소드
+/**
+ * 문자열을 변환하는 메소드
+ * @param {string} word 변환하고자 하는 word
+ * @returns {string} 변환된 문자열
+ */
 function checkLetter(word) {
   const reversedArray = []
   for (let i = 0; i < word.length; i++) {
@@ -24,20 +29,33 @@ function checkLetter(word) {
 }
 
 
-// 알파벳을 반대로 변환하는 메소드 (알파벳이 아닌 경우 변환하지 않는다)
+// 문자 변환과 관련된 메소드
+/**
+ * 알파벳을 반대로 변환하는 메소드
+ * @param {string} letter 공백이 아닌 문자
+ * @returns {string} 변환된 문자
+ */
 function reverseLetter(letter) {
   const letterASCII = letter.charCodeAt();
   if (97 <= letterASCII && letterASCII <= 122) return reverseLower(letter);
   if (65 <= letterASCII && letterASCII <= 90) return reverseUpper(letter);
   return letter;
 }
-
+/**
+ * 소문자를 변환하는 메소드
+ * @param {string} letter 변환 대상 소문자
+ * @returns {string} 변환된 소문자
+ */
 function reverseLower(letter) {
   // 영어 소문자 ASCII : 97 ~ 122
   const index = 97 - letter.charCodeAt();
   return String.fromCharCode(122 + index);
 }
-
+/**
+ * 대문자를 변환하는 메소드
+ * @param {string} letter 변환 대상 대문자
+ * @returns {string} 변환된 대문자
+ */
 function reverseUpper(letter) {
   // 영어 대문자 ASCII 65 ~ 90
   const index = 65 - letter.charCodeAt();
@@ -50,7 +68,7 @@ function validRange(word) {
   return 1 <= word.length && word.length <= 1000;
 }
 
-function validLetters(word) {
+function validWord(word) {
   const reg = /^[a-zA-Z\s]*$/
 
   return reg.test(word);
