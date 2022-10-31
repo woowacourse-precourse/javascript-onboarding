d20f81e9ca822939000e905d349a9140c47c4759
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 23:33:57 +0900

    feat: [5번 문제] 변환 결과 반환

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,11 @@ const MONEY_TYPE_ARR = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
   let answer = Array.from({ length: MONEY_TYPE_ARR.length }, () => 0);
+  let remainMoney = money;
+  MONEY_TYPE_ARR.map((referenceMoney, i) => {
+    answer[i] = Math.floor(remainMoney / referenceMoney);
+    remainMoney = remainMoney % referenceMoney;
+  });
   return answer;
 }
 
