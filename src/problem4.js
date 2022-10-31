1cd037cbd816e80793bce3435443af91961d5ce0
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 21:56:43 +0900

    [Feat:Problem4] 알파벳 여부 확인하기

--- src/problem4.js
+++ src/problem4.js
@@ -2,3 +2,7 @@ const head_u = "A".charCodeAt(0);
 const tail_u = "Z".charCodeAt(0);
 const head_l = "a".charCodeAt(0);
 const tail_l = "z".charCodeAt(0);
+
+const isAlpha = (charcode) =>
+  (charcode >= head_u && charcode <= tail_u) ||
+  (charcode >= head_l && charcode <= tail_l);
