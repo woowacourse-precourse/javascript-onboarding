d53fcb39b2a4c30f51f19f84cf373c3ce73507cd
unqeye <salyuan@naver.com>
2022-11-01 02:02:50 +0900

    feat: 문제7 추천할 친구 정렬 후 반환

--- src/problem7.js
+++ src/problem7.js
@@ -14,7 +14,6 @@ function getMutualFriendScore(user, friendList) {
   friendList[user].forEach((friend) => {
     friendList[friend].forEach((mutualFriend) => {
       if (mutualFriend === user || friendList[user].has(mutualFriend)) {
-        console.log(friend, mutualFriend);
         return;
       }
       score[mutualFriend] = score[mutualFriend] || 0;
@@ -37,7 +36,25 @@ function getVisitorScore(user, friendList, visitors) {
 }
 
 function problem7(user, friends, visitors) {
-  var answer;
+  const friendList = getFriendList(user, friends);
+  const mutualFriendScore = getMutualFriendScore(user, friendList);
+  const visitorScore = getVisitorScore(user, friendList, visitors);
+
+  const scores = {};
+  Object.entries(mutualFriendScore).forEach(([name, score]) => {
+    scores[name] = scores[name] || 0;
+    scores[name] += score;
+  });
+  Object.entries(visitorScore).forEach(([name, score]) => {
+    scores[name] = scores[name] || 0;
+    scores[name] += score;
+  });
+
+  const sortedScores = Object.entries(scores).sort((a, b) =>
+    a[1] === b[1] ? a[0].localeCompare(b[0]) : b[1] - a[1]
+  );
+  const answer = sortedScores.slice(0, 5).map(([name]) => name);
+
   return answer;
 }
 
