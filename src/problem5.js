0dd62bd9b7d3f7efe0831cbfb378eb042384025a
taeyoung <rlaxodud911@gmail.com>
2022-10-31 19:39:02 +0900

    Feat: problem 5 화폐 단위로 money를 나눈 몫을 저장하는 반복문 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
+  const answer = [];
   const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (const c of currency) {
+    let [quotient, remainder] = [parseInt(money / c), money % c];
+
+    answer.push(quotient);
+    money = remainder;
+  }
 
   return answer;
 }
