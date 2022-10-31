f00cb899582e064fbd6596994d55dc72922be0db
KINHYEONJI <hyeonji0718@gmail.com>
2022-10-31 19:31:16 +0900

    feat : 결과를 출력해 내는 최종 함수 problem4 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answer = [];
+  for (let i = 0; i < word.length; i++) {
+    if (word.charCodeAt(i) > 54 && word.charCodeAt(i) < 91) {
+      answer.push(changeUpperCase(word[i]));
+      continue;
+    }
+    if (word.charCodeAt(i) > 96 && word.charCodeAt(i) < 123) {
+      answer.push(changeLowerCase(word[i]));
+      continue;
+    }
+    answer.push(word[i]);
+  }
+  return answer.join("");
 }
 function changeLowerCase(alphabet) {
   return String.fromCharCode(219 - alphabet.charCodeAt(0));
