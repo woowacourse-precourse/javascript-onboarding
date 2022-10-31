19aab59aa66b70feae983b21c6caf53d0283341d
“somm12” <sominan8744@gmail.com>
2022-10-31 17:57:23 +0900

    숫자에 3/6/9 가 몇개 있는지 확인하는 함수 정의

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,16 @@
+function isInclude369(n) {
+  let count = 0;
+  let digit;
+  while (n > 0) {
+    digit = n % 10;
+    if (digit === 3 || digit === 6 || digit === 9) {
+      count += 1;
+    }
+    n = Math.floor(n / 10);
+  }
+  return count;
+}
+
 function problem3(number) {
   var answer;
   return answer;
