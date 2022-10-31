79af65803bd783c8e996afb02e10b23c166de53a
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 22:54:37 +0900

    feat: [2번 문제] 연속되는 중복 문자가 없을 때까지 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,23 +1,36 @@
 const FIRST_INDEX = 0;
 
 function problem2(cryptogram) {
-  let referenceCharacter = cryptogram[FIRST_INDEX];
-  let dededuplicatedArr = [referenceCharacter];
+  let beforeDeduplicationLength = 0;
+  let afterDeduplicationLength = 0;
+  let deduplicatedCryptogram = cryptogram;
+  let referenceCharacter = deduplicatedCryptogram[FIRST_INDEX];
+  let dededuplicatedArr = [];
 
-  for (let i = 1; i < cryptogram.length; i++) {
-    const targetCharacter = cryptogram[i];
+  do {
+    beforeDeduplicationLength = deduplicatedCryptogram.length;
+    dededuplicatedArr.push(referenceCharacter);
 
-    if (referenceCharacter !== targetCharacter) {
-      dededuplicatedArr.push(targetCharacter);
-      referenceCharacter = targetCharacter;
-    } else if (
-      referenceCharacter === dededuplicatedArr[dededuplicatedArr.length - 1]
-    ) {
-      dededuplicatedArr.pop();
+    for (let i = FIRST_INDEX + 1; i < beforeDeduplicationLength; i++) {
+      const targetCharacter = deduplicatedCryptogram[i];
+
+      if (referenceCharacter !== targetCharacter) {
+        dededuplicatedArr.push(targetCharacter);
+        referenceCharacter = targetCharacter;
+      } else if (
+        referenceCharacter === dededuplicatedArr[dededuplicatedArr.length - 1]
+      ) {
+        dededuplicatedArr.pop();
+      }
     }
-  }
 
-  return dededuplicatedArr.join("");
+    deduplicatedCryptogram = dededuplicatedArr.join("");
+    afterDeduplicationLength = deduplicatedCryptogram.length;
+    referenceCharacter = deduplicatedCryptogram[FIRST_INDEX];
+    dededuplicatedArr = [];
+  } while (beforeDeduplicationLength !== afterDeduplicationLength);
+
+  return deduplicatedCryptogram;
 }
 
 module.exports = problem2;
