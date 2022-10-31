023354e6144b7f0d515a9a10f2dec21931779709
dahlia <wlstnrdahlia@gmail.com>
2022-11-01 00:25:09 +0900

    Add : problem3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
   var answer;
+  answer = 0;
+  
+  for (let i = 1; i <= number; i++) {
+    let num = i
+    while (num !== 0) {
+      if (num % 10 === 3 || num % 10 === 6 || num % 10 === 9) {
+        answer += 1;
+      }
+      num = Math.floor(num / 10);
+    }
+  }
   return answer;
 }
 
