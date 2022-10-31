1ca677011db07d8f6c383b4540ddecdf08fd589d
wusi-hub <jws9595@naver.com>
2022-11-01 07:16:24 +0900

    problem1) 코드 설명을 위한 주석 추가 후 제출

--- src/problem1.js
+++ src/problem1.js
@@ -1,11 +1,14 @@
 function problem1(pobi, crong) {
 
+  // 결괏값을 담아줄 변수 선언
   let result = 0;
 
+  // 예외사항 
   if(pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) {
     return -1;
   }
 
+  // 포비 최댓값
   const pobiMax = Math.max(
     ...pobi.map((n) => {
       const sum = n.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
@@ -13,7 +16,7 @@ function problem1(pobi, crong) {
       return Math.max(sum, mul);
     })
   )
-
+  // 크롱 최댓값
   const crongMax = Math.max(
     ...crong.map((n) => {
       const sum = n.toString().split('').reduce((acc, cur) => acc + Number(cur), 0);
@@ -22,9 +25,11 @@ function problem1(pobi, crong) {
     })
   )
 
+  // 포비와 크롱의 최댓값을 비교 후 포비가 이긴다면 1, 크롱이 이긴다면 2를 리턴
   if (pobiMax > crongMax) result = 1;
   if (pobiMax < crongMax) result = 2;
 
+  // 최종 결괏값 리턴 
   return result;
 
 }
