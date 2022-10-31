1b8656dd637677988c4480b369472d07ce82bc1f
hhhminme <hororonoodle@gmail.com>
2022-11-01 00:25:11 +0900

    refactor : Remove the abbreviation of the array variable name in problem 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,16 @@
 function problem2(cryptogram) {
-  let cryptArr = [...cryptogram];
+  let cryptoArray = [...cryptogram];
   let cnt = 0;
-  while (cnt !== cryptArr.length - 1 && cryptArr.length !== 0) {
-    if (cryptArr[cnt] === cryptArr[cnt + 1]) {
-      cryptArr.splice(cnt, 2); // 현재 문자와 다음 문자가 동일할 경우 배열에서 해당 두 문자를 제거해준다.
+  while (cnt !== cryptoArray.length - 1 && cryptoArray.length !== 0) {
+    if (cryptoArray[cnt] === cryptoArray[cnt + 1]) {
+      cryptoArray.splice(cnt, 2); // 현재 문자와 다음 문자가 동일할 경우 배열에서 해당 두 문자를 제거해준다.
       cnt = 0;
     } else {
       cnt++;
     }
   }
-  return cryptArr.join('');
+  const answer = cryptArr.join('');
+  return answer;
 }
 
 module.exports = problem2;
