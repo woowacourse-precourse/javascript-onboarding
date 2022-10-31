33f6832643cda10660e64b3caf27f4dcc67a9a15
choieastsea <choieastsea@gmail.com>
2022-11-01 07:14:53 +0900

    4) Refactoring
    Refactoring by adding functions

--- src/problem4.js
+++ src/problem4.js
@@ -1,17 +1,14 @@
 function problem4(word) {
   var answer = "";
-  const A_code = "A".charCodeAt(0);
-  const Z_code = "Z".charCodeAt(0);
-  const a_code = "a".charCodeAt(0);
-  const z_code = "z".charCodeAt(0);
+  const A_code = charCode("A");
+  const Z_code = charCode("Z");
+  const a_code = charCode("a");
+  const z_code = charCode("z");
   for (let i in word) {
-    var char_code = word[i].charCodeAt(0);
-    if (
-      (char_code >= a_code && char_code <= z_code) ||
-      (char_code >= A_code && char_code <= Z_code)
-    ) {
+    var char_code = charCode(word[i]);
+    if (isAlpha(char_code)) {
       //alphabet인 경우
-      if (char_code >= a_code) {
+      if (isDown(char_code)) {
         //소문자인 경우
         char_code = z_code - char_code + a_code;
       } else {
@@ -23,4 +20,18 @@ function problem4(word) {
   }
   return answer;
 }
+function charCode(char) {
+  return char.charCodeAt(0);
+}
+function isAlpha(char_code) {
+  return (
+    (char_code >= charCode("a") && char_code <= charCode("z")) ||
+    (char_code >= charCode("A") && char_code <= charCode("Z"))
+  );
+}
+function isDown(char_code) {
+  if (char_code >= charCode("a")) return true;
+  return false;
+}
+
 module.exports = problem4;
