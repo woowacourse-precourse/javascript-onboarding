95cf3f3a544339b09c215a393c73748e5f7a09b8
Jaehyeon Park <sawogus29@gmail.com>
2022-10-31 15:05:57 +0900

    problem 4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const trans = {};
+  const [A, Z, a, z] = ["A", "Z", "a", "z"].map((x) => x.charCodeAt());
+  for (let i = 0; i < 26; i++) {
+    trans[String.fromCharCode(A + i)] = String.fromCharCode(Z - i);
+    trans[String.fromCharCode(a + i)] = String.fromCharCode(z - i);
+  }
+
+  return Array.from(word)
+    .map((c) => trans[c] || c)
+    .join("");
 }
+
+// console.log(problem4("I love you"));
 
 module.exports = problem4;
