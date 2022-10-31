42e8da92c19157b1e9cee0f982b65749bbc8f6f7
spsp8426 <spsp8426@naver.com>
2022-10-31 14:27:01 +0900

    2번 주석처리 log 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,12 @@
 function problem2(cryptogram) {
   let answer = cryptogram.split("");
   for (let i = 0; i < answer.length; i++) {
-    //console.log(answer[i]);
     if (answer[i] === answer[i - 1]) {
       let stock = 1;
       let j = i;
-      // console.log(j)
       while (answer[j] === answer[j - 1]) {
         stock++;
         j++;
-        //console.log(j)
       }
       answer.splice(i - 1, stock);
       i = 0;
@@ -19,5 +16,4 @@ function problem2(cryptogram) {
   answer = answer.join("");
   return answer;
 }
-console.log(problem2("browoanoommnaon"));
 module.exports = problem2;
