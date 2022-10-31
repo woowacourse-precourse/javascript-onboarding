8b59f0d7c932e3adfccfd1e06281013628c0a66e
박승훈 <tmdgns1126@naver.com>
2022-11-01 00:07:52 +0900

    [P2] Fix: 3개 이상의 연속문자 예외 처리

--- src/problem2.js
+++ src/problem2.js
@@ -2,12 +2,15 @@ function problem2(cryptogram) {
   const arr = cryptogram.split("");
   const ret = [""];
   let l = 0;
+  let lastChar = "";
   arr.forEach((c) => {
     if (c === ret[l]) {
       ret.pop();
       l--;
+    } else if (c === lastChar) {
     } else {
       ret.push(c);
+      lastChar = c;
       l++;
     }
   });
