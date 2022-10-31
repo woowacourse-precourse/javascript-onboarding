c4423cd9ff5658c18ecca9145c62a5c186e3fa2e
wish0ne <wish@khu.ac.kr>
2022-10-31 18:09:34 +0900

    feat: Add 369 game for problem3
    Add 369 game logic for problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,24 @@
 function problem3(number) {
   var answer;
-  return answer;
-}
+  try{
+    //예외사항 처리
+    //입력의 데이터 타입이 숫자가 아닌 경우
+    if(typeof number !== "number") throw new TypeError("입력의 데이터 타입이 숫자가 아닙니다.");
 
+    //입력의 범위가 1 ~ 10,000이 아닌 경우
+    if(number < 1 || number > 10000) throw new RangeError("입력의 범위가 1 ~ 10,000을 벗어났습니다.");
+
+    //369 게임 시작
+    answer = 0;
+    for(let i = 1; i <= number; i++){
+      i.toString().split('').forEach((value) => {
+        if(value === '3' || value === '6' || value ==='9') answer += 1;
+      })
+    }
+    return answer;
+  }
+  catch(e){
+    return e;
+  }
+}
 module.exports = problem3;
