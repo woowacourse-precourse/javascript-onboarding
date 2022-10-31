cbbdd54bd01249048d89ac731d5a65e9d56d7707
daal2quf <daal2quf@gmail.com>
2022-11-01 03:16:17 +0900

    변수명 알기쉽게 변경

--- src/problem4.js
+++ src/problem4.js
@@ -1,33 +1,29 @@
 function problem4(word) {
-  let answer;
-  let motherWords = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
-  let frogWords = [...motherWords].sort((a, b) => b.localeCompare(a))
-  let lowerMotehrWords = motherWords.map((e) => e.toLowerCase())
-  let lowerFrogWords = frogWords.map((e) => e.toLowerCase())
+  
+  let mother = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
+  let frog = [...mother].sort((a, b) => b.localeCompare(a))
+  let lowerMother = mother.map((e) => e.toLowerCase())
+  let lowerFrog = frog.map((e) => e.toLowerCase())
   let frogSay = '';
-  
 
-  const frogConvertor = (str) => {
-    if (str === ' ') return str
-
-    if (motherWords.includes(str)) {
-      let idx = motherWords.indexOf(str)
-      return frogWords[idx]
-    } else if (lowerMotehrWords.includes(str)) {
-      let idx = lowerMotehrWords.indexOf(str)
-      return lowerFrogWords[idx]
-    } else if(str === ' ') {
-      return str
-    }
-  }
-  
-  for (let i = 0; i < word.length; i++) {
-    frogSay += frogConvertor(word[i])
-  }
-  console.log(frogSay)
-  return frogSay;
+const frogConvertor = (str) => {
+  if (mother.includes(str)) {
+    let idx = mother.indexOf(str)
+    return frog[idx]
+  } else if (lowerMother.includes(str)) {
+    let idx = lowerMother.indexOf(str)
+    return lowerFrog[idx]
+  } else if (str === ' ')
+    return str
 }
 
-problem4("I love you")
+for (let i = 0; i < word.length; i++) {
+  frogSay += frogConvertor(word[i])
+}
 
+console.log(frogSay)
+return frogSay;
+}
+
+problem4('I love you')
 module.exports = problem4;
