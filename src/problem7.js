45261e91ce658e43944c6d5def75ac9ee30e7329
MJ <mj632900@gmail.com>
2022-11-01 03:12:31 +0900

    feat : 함께아는 친구 리스트 반환하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -12,6 +12,16 @@ function exceptUserList(user, friendList) {
     return friendList.filter((name) => !name.includes(user));
 }
 
+function getFriendOfFriend(friendObj, notUserFriendList) {
+    return notUserFriendList
+        .filter(
+            ([user1, user2]) =>
+                !(user1 in friendObj && user2 in friendObj) &&
+                (user1 in friendObj || user2 in friendObj)
+        )
+        .map(([user1, user2]) => (user1 in friendObj ? user2 : user1));
+}
+
 function problem7(user, friends, visitors) {}
 
 module.exports = problem7;
