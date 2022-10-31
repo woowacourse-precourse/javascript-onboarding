13a5ba9ac17902537c3da3506a1dc7e063daf69a
kim-limit <rlawogks2468@naver.com>
2022-10-31 15:17:23 +0900

    feat: 'add getFreinlist func(사람별로 친구 리스트를 가진 dict)'

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,21 @@
+function getFriendList(friends) {
+  let friendDict = {};
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (!friendDict[friends[i][j]]) {
+        friendDict[friends[i][j]] = [friends[i][1 - j]];
+      } else {
+        friendDict[friends[i][j]].push(friends[i][1 - j]);
+      }
+    }
+  }
+  return friendDict;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  let friendDict = getFriendList(friends);
+
   return answer;
 }
 
