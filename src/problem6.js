c96166e99a9339bf55ba20486ae567bb647954d8
shlee0820 <72565083+shlee0820@users.noreply.github.com>
2022-11-01 03:10:39 +0900

    style: [P6] 이메일 저장 객체 및 조회하는 문자열 변수명 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,23 +1,23 @@
 // 같은 글자가 연속적으로 포함되는 닉네임을 작성한 지원자 목록 반환
 function problem6(forms) {
-  const result = new Set(); 
+  const emailSet = new Set(); 
   const nickname = [];
   forms.map(x => {nickname.push(x[1])});
   
   nickname.map((name) => {
-    let standard = '';
+    let lookupWord = '';
     for(let i=0; i<name.length-1; i++) {
-      standard = name.substr(i, 2);
+      lookupWord = name.substr(i, 2);
       
       nickname.map((x, idx) => {
-        if(x !== name && x.includes(standard)) {
-          result.add(forms[idx][0]);
+        if(x !== name && x.includes(lookupWord)) {
+          emailSet.add(forms[idx][0]);
         }
       })
     }
   })
 
-  return Array.from(result).sort();
+  return Array.from(emailSet).sort();
 }
 
 module.exports = problem6;
