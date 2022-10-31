9c8386448d57766901346b11d78843b5c12ed92d
DongRyeong Lee <dr11291129@gmail.com>
2022-10-31 17:30:39 +0900

    feat: P6 isKoreanName 구현

--- src/problem6.js
+++ src/problem6.js
@@ -7,4 +7,13 @@ function isRightDomain(email) {
   return email.indexOf("email.com");
 }
 
+function isKoreanName(name) {
+  for (let i = 0; i < name.length; i++) {
+    if (!(0x1100 <= c) && !(c <= 0x11ff)) return -1;
+    if (!(0x3130 <= c) && !(c <= 0x318f)) return -1;
+    if (!(0xac00 <= c) && !(c <= 0xd7a3)) return -1;
+  }
+  return 1;
+}
+
 module.exports = problem6;
