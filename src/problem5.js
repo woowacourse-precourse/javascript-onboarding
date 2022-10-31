a01967f6c58c212e6b0badbc87c29d1edb440ef8
Suyeon <msjgr2801@gmail.com>
2022-10-31 15:31:40 +0900

    Comment: Add JS DOC to prolbem5
    문제 5번에 JS DOC으로 함수 설명 추가

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,11 @@
 // 2. [x] answer엔 money를 각 standard로 나눈 몫을 저장하기
 //  2.1 [x]거스름돈을 계산하며 1원단위까지 반복
 
+/**
+ * Main Function - return change function
+ * @param {number} money 
+ * @returns {number[]} answer - number of each change standards
+ */
 function problem5(money) {
   // 1. 나눠지는 돈대로 배열 생성
   const standard = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
