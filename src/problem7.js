fd02c0176f736a527db37b2474379b49a1b0831f
Hwang Juhee <julia0276@naver.com>
2022-10-31 22:50:24 +0900

    :sparkles: [feat] 추천 친구 리스트 추출 함수 구현 #P7

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,34 @@
+function getRecommendedFriend(user, friends, visitors) {
+  let recommendedArr = [];
+  // user 친구 리스트 추출
+  let friendsOfUser = friends.filter((element)=> element[0] === user || element[1] === user);
+  friendsOfUser = friendsOfUser.reduce(function(acc,cur) {
+    return [...new Set(acc.concat(cur))].filter((element)=>!element.includes(user))
+  });
+
+  // user와 친구가 아닌 사람의 리스트 추출
+  let friendsOfOthers= friends.reduce(function(acc,cur){
+    let arr = [...new Set(acc.concat(cur))];
+    arr = arr.filter(x => !friendsOfUser.includes(x));
+    arr = arr.filter((element)=>!element.includes(user))
+    return arr;
+  });
+
+  // 방문자 중복제거 리스트 추출
+  let visitorsOfUser = [...new Set(visitors)].filter(x => !friendsOfUser.includes(x));
+
+  friendsOfOthers.forEach(people => recommendedArr.push([people,0]));
+  visitorsOfUser.forEach(people => recommendedArr.push([people,0]));
+  
+  return [recommendedArr, friendsOfUser];
+}
+
+
 function problem7(user, friends, visitors) {
-  var answer;
+  
+  let answer = [];
+  let [recommendedArr,friendsOfUser] = getRecommendedFriend(user, friends, visitors)
+  
   return answer;
 }
 
