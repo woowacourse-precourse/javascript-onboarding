f43f48bd7207043cd32e94f0a261b292c7bedd8f
froggy1014 <clcl6084@gmail.com>
2022-10-31 16:50:57 +0900

    feat[7]: 유저의 친구 목록을 구하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,27 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userFriends = getUserFreinds(user, friends);
+
+  
+  return userFriends;
 }
 
-module.exports = problem7;
+function getUserFreinds(user, relationship) {
+  return relationship.filter((rel) => rel.includes(user))
+                     .flat()
+                     .filter(name => name !== user);
+}
+
+const user = "mrko";
+const friends =  [
+  ["donut", "andole"], 
+  ["donut", "jun"], 
+  ["donut", "mrko"], 
+  ["shakevan", "andole"], 
+  ["shakevan", "jun"], 
+  ["shakevan", "mrko"]
+  ];
+const visitors =  ["bedi", "bedi", "donut", "bedi", "shakevan"];
+
+console.log(problem7(user, friends, visitors));
+
+// module.exports = problem7;
