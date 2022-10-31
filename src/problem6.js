680470ab1df30b7a922a880d3a4b4334cf31aecc
yjh1008 <exesea12@gmail.com>
2022-10-31 13:22:13 +0900

    [problem] 문제 6 해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  const isLimitNickname = new Map();
+  forms.forEach((student) => {
+    const [email, nickname] = student;
+    for(let i =0;  i< nickname.length-1;i++) {
+      const tempStr = nickname.substring(i,i+2);
+      if(isLimitNickname.has(tempStr)) isLimitNickname.set(tempStr, isLimitNickname.get(tempStr)+1);
+      else isLimitNickname.set(tempStr, 1);
+    }
+  });
+  forms.forEach((student) => {
+    const [email, nickname] = student;
+    for(let i =0;  i< nickname.length-1 ;i++) {
+      const tempStr = nickname.substring(i,i+2);
+      if(isLimitNickname.get(tempStr) > 1) answer.push(email);
+    }
+  });
+  answer = [...new Set(answer.sort())];
   return answer;
 }
-
 module.exports = problem6;
