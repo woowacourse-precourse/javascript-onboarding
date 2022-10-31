f2455283d1564b3f49d7412323b2c057ca32d245
jjongs2 <jjongs@hanyang.ac.kr>
2022-10-31 22:04:13 +0900

    refactor(problem6): 토큰화 기능을 더욱 작은 단위로 나누어 각각 함수로 분리

--- src/problem6.js
+++ src/problem6.js
@@ -1,17 +1,31 @@
-function problem6(forms) {
-  var answer;
-  const crewCount = forms.length;
-  let nicknames = new Array(crewCount);
+function getTokens(crew) {
+  const crewNickname = crew.nickname;
+  const nicknameLength = crewNickname.length;
+  let crewTokens = crew.tokens;
+
+  for (let charIndex = 0; charIndex < nicknameLength - 1; ++charIndex) {
+    crewTokens.push(crewNickname.substr(charIndex, 2));
+  }
+}
+
+function getCrewsInfo(forms, crews) {
+  const crewCount = crews.length;
 
   for (let crewIndex = 0; crewIndex < crewCount; ++crewIndex) {
-    const nickname = forms[crewIndex][1];
-    const nicknameLength = nickname.length;
-
-    nicknames[crewIndex] = [];
-    for (let charIndex = 0; charIndex < nicknameLength - 1; ++charIndex) {
-      nicknames[crewIndex].push(nickname.substr(charIndex, 2));
-    }
+    crews[crewIndex] = {
+      email: forms[crewIndex][0],
+      nickname: forms[crewIndex][1],
+      tokens: []
+    };
+    getTokens(crews[crewIndex]);
   }
+}
+
+function problem6(forms) {
+  var answer;
+  let crews = new Array(forms.length);
+
+  getCrewsInfo(forms, crews);
   return answer;
 }
 
