13c3a196b0e20a820eaa28b270dffe449d54d9ae
gsong <rmsdlf1206@gmail.com>
2022-10-31 22:18:34 +0900

    feat: problem 7 친구관계가 아닌 사람들 point의 key 값으로 추가하는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,9 @@
 function problem7(user, friends, visitors) {
+  let userFriends = [];
+  let point = {};
+  let result = [];
   checkUserFriends(user, friends, userFriends);
+  addFriends(user, friends, userFriends, point);
 }
 function checkUserFriends(user, friends, userFriends) {
   for (let i = 0; i < friends.length; i++) {
@@ -8,4 +12,14 @@ function checkUserFriends(user, friends, userFriends) {
     }
   }
 } // user의 친구를 확인하는 함수
+
+function addFriends(user, friends, userFriends, point) {
+  for (let i = 0; i < friends.length; i++) {
+    for (let k = 0; k < 2; k++) {
+      if (!userFriends.includes(friends[i][k]) && friends[i][k] !== user) {
+        point[friends[i][k]] = 0;
+      }
+    }
+  }
+}
 module.exports = problem7;
