316d02cef3236ec1ad46eb9224dec539017c444a
gabrielyoon7 <gabrielyoon7@gmail.com>
2022-10-31 13:37:17 +0900

    problem 4 주석 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,18 +1,23 @@
 function problem4(word) {
-  var answer=[];
-  for(let i = 0 ; i < word.length; i++){
-    const ascii = word.charCodeAt(i);
-    if(ascii>=65 && ascii<=90){
-      answer.push(90-ascii+65)
+  let answer = [];
+  // 모든 글자에 대해
+  for (let i = 0; i < word.length; i++) {
+    const ascii = word.charCodeAt(i); // 아스키 코드로 변환 후
+    // 소문자는 대문자로
+    if (ascii >= 65 && ascii <= 90) {
+      answer.push(90 - ascii + 65)
     }
-    if(ascii>=97 && ascii<=122){
-      answer.push(122-ascii+97)
+    // 대문자는 소문자로
+    if (ascii >= 97 && ascii <= 122) {
+      answer.push(122 - ascii + 97)
     }
-    if(ascii===32){
+    // 띄어쓰기는 그대로
+    if (ascii === 32) {
       answer.push(32)
     }
   }
-  return answer.map((num)=>String.fromCharCode(num)).join('');
+  // 모든 아스키 코드를 문자로 변환 후 합치기
+  return answer.map((num) => String.fromCharCode(num)).join('');
 }
 
 module.exports = problem4;
