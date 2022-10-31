cedce77d07f2dfc87ecd0638c8a2a619f699b8a5
yunho <Yunho7058@github.com>
2022-10-31 20:37:11 +0900

    problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,50 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+  let list = [
+    'A',
+    'B',
+    'C',
+    'D',
+    'E',
+    'F',
+    'G',
+    'H',
+    'I',
+    'J',
+    'K',
+    'L',
+    'M',
+    'N',
+    'O',
+    'P',
+    'Q',
+    'R',
+    'S',
+    'T',
+    'U',
+    'V',
+    'W',
+    'X',
+    'Y',
+    'Z',
+  ];
+  for (let i = 0; i < word.length; i++) {
+    let regexpStr = /([a-z]|[A-Z])/;
+    let regexpUpper = /[A-Z]/g;
+    if (regexpStr.test(word[i])) {
+      if (regexpUpper.test(word[i])) {
+        answer = answer + list.reverse()[25 - list.indexOf(word[i])];
+      } else {
+        answer =
+          answer +
+          list
+            .reverse()
+            [25 - list.indexOf(word[i].toUpperCase())].toLowerCase();
+      }
+    } else {
+      answer = answer + word[i];
+    }
+  }
   return answer;
 }
 
