64de9f7e2002c069b338ba9d9b7bed732e7826e7
taeyoung <rlaxodud911@gmail.com>
2022-10-31 19:59:25 +0900

    Feat: problem 6 비슷한 닉네임을 판별하는 함수 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,21 @@
+/**
+ * target과 nickname이 2글자가 겹치는지 검사합니다.
+ * @param {string} nickname 겹치는지 확인 할 닉네임입니다.
+ * @param {string} target 겹치는지 확인 할 닉네임입니다. 검사의 기준이 됩니다.
+ * @returns {boolean} 겹치면 true, 안겹치면 false를 반환합니다.
+ */
+function isRepeatedNickname(nickname, target) {
+  for (let i = 0; i < target.length; i++) {
+    const targetString = target.substring(i, 2);
+
+    if (targetString.length === 2 && nickname.includes(targetString)) {
+      return true;
+    }
+  }
+
+  return false;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
