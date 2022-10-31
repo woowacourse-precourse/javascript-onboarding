bb7c09adbfaec75590a3e73381358111c15d19fc
sying012 <syi369@naver.com>
2022-10-31 13:59:46 +0900

    problem5 - 계수기 함수 작성

--- src/problem5.js
+++ src/problem5.js
@@ -9,7 +9,24 @@ function problem5(money) {
 }
 
 function violationChk(money) {
+  // 입력된 금액이 1 ~ 1000000원 사이인지 검사
   return money >= 1 && money <= 1000000;
+}
+
+// 화폐 계수기 함수
+function counter(money) {
+  // 화폐 단위
+  var currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var result = [];
+  var currentMoney = money;
+
+  // 화폐 단위 개수만큼 반복
+  for (let i = 0; i < currency.length; i++) {
+    result[i] = parseInt(currentMoney / currency[i]);
+    currentMoney = parseInt(currentMoney % currency[i]);
+  }
+
+  return result;
 }
 
 module.exports = problem5;
