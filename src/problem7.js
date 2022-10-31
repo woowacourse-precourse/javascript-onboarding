51f7c2c967b597e1b7b25a6a118f61ca68cbe5ed
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-31 13:43:43 +0900

    feat: [P7] 친구 관계를 확인하는 함수 만들기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+}
+
+function checkFriendship(person, friends) {
+  let friendOfPerson = []
+  friends.forEach((friendship) => {
+    if (friendship[0] === person) friendOfPerson.push(friendship[1])
+    if (friendship[1] === person) friendOfPerson.push(friendship[0])
+  })
+  return friendOfPerson
 }
 
 module.exports = problem7;
