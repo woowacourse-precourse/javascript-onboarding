43cba474885912b2ebdc9b5995f54d0d41a16b81
100Gyeon <bjy9966@naver.com>
2022-10-31 17:46:45 +0900

    refactor: 불필요한 for문 제거

--- src/problem6.js
+++ src/problem6.js
@@ -13,14 +13,10 @@ function problem6(forms) {
     }
   });
 
-  for (let [key, value] of twoLettersMap.entries()) {
-    if (value < 2) twoLettersMap.delete(key);
-  }
-
   forms.forEach(([email, nickname]) => {
     for (let i = 0; i < nickname.length - 1; i++) {
       const twoLetters = nickname.substring(i, i + 2);
-      if (twoLettersMap.has(twoLetters)) {
+      if (twoLettersMap.get(twoLetters) > 1) {
         emailList.push(email);
       }
     }
