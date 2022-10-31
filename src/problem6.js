d2ef9fe795baa0e3216c9260b63927f0a3567081
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-10-31 21:32:51 +0900

    ✨ problem6 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,27 @@
-function problem6(forms) {
-  var answer;
-  return answer;
-}
+const isDuplicated = (nick1, nick2) => {
+  let result = false;
+  for (let i = 0; i < nick1.length - 1; i++) {
+    for (let j = 0; j < nick2.length - 1; j++) {
+      if (nick1[i] === nick2[j] && nick1[i + 1] === nick2[j + 1]) return true;
+    }
+  }
+  return result;
+};
+
+const problem6 = (forms) => {
+  const duplicated = new Set();
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = i + 1; j < forms.length; j++) {
+      if (isDuplicated(forms[i][1], forms[j][1])) {
+        duplicated.add(forms[i][0]);
+        duplicated.add(forms[j][0]);
+        break;
+      }
+    }
+  }
+
+  return [...duplicated].sort();
+};
 
 module.exports = problem6;
