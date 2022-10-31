f9ab862d0c439f028a550fbf6cc414d6d428e352
JeongSabibi <jsbb528@naver.com>
2022-11-01 02:25:38 +0900

    feat(PROBLEM3): 전체 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let count=1; count<=number; count++) {
+    var str = count.toString();
+
+    for (let i=0; i<str.length; i++) {
+      var clap = str.charAt(i);
+      if (clap === "3" || clap === "6" || clap === "9") {
+        answer = answer + 1;
+      }
+    }
+  }
+
   return answer;
 }
 
