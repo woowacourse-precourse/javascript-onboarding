67875f0646deef362933f9cd1e305b13554b3f56
jonghyunlee95 <108605838+jonghyunlee95@users.noreply.github.com>
2022-11-01 00:14:04 +0900

    [problem1] refactor: 예외사항 추가 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,16 +1,33 @@
 function problem1(pobi, crong) {
+  const pobi_score_arr = [];
+  const crong_score_arr = [];
   let pobi_score = 0;
   let crong_score = 0;
 
   for (let i = 0; i < pobi.length; i++) {
-    pobi_score = Math.max(plus(pobi[i]), multiple(pobi[i]));
+    pobi_score_arr.push(plus(pobi[i]), multiple(pobi[i]));
   }
 
   for (let i = 0; i < crong.length; i++) {
-    crong_score = Math.max(plus(crong[i]), multiple(crong[i]));
+    crong_score_arr.push(plus(crong[i]), multiple(crong[i]));
   }
 
-  if (pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1]) return -1;
+  pobi_score = Math.max(...pobi_score_arr);
+  crong_score = Math.max(...crong_score_arr);
+
+  if (
+    pobi[0] + 1 !== pobi[1] ||
+    crong[0] + 1 !== crong[1] ||
+    pobi[0] < 1 ||
+    crong[0] < 1 ||
+    pobi[1] > 400 ||
+    crong[1] > 400 ||
+    pobi[0] % 2 === 0 ||
+    crong[0] % 2 === 0 ||
+    pobi[1] % 2 !== 0 ||
+    crong[1] % 2 !== 0
+  )
+    return -1;
   else if (pobi_score > crong_score) return 1;
   else if (pobi_score < crong_score) return 2;
   else return 0;
