97cf2199f6c46b9e14c8f3c40215eba30f0da773
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 07:21:27 +0900

    problem1 add and multiply

--- src/problem1.js
+++ src/problem1.js
@@ -17,7 +17,26 @@ function checkValid(array) {
   return array[0]+1 == array[1];
 }
 
+function biggerNum(array) {
+  num1 = ones(array[0]);
+  num2 = tens(array[0]);
+  num3 = hundreds(array[0]);
+  num4 = ones(array[1]);
+  num5 = tens(array[2]);
+  num6 = hundreds(array[3]);
+  addition = compareNum(num1+num2+num3, num4+num5+num6);
+  multiplication = compareNum(num1*num2*num3, num4*num5*num6);
+}
 
+function ones(num) {
+  return num % 10;
+}
+function tens(num) {
+  return num / 10 % 10;
+}
+function hundreds(num) {
+  return num / 100;
+}
 
 function compareNum(a, b) {
   if (a >= b) {
