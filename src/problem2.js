e7935c4369aa94da567fcd9abf73494a0a1dcc0a
woong <inwoong100@gmail.com>
2022-10-31 22:14:26 +0900

    feature: problem2 풀이 완성
    1. 구현방식에 따른 순차적인 풀이 완성
    2. 예외방식 예외 처리 완성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,30 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const check = (word) => {
+    let before = '';
+    for (const el of word) {
+      if (el == before) return true;
+      else before = el;
+    }
+  };
+
+  let checkWord = cryptogram;
+
+  while (check(checkWord)) {
+    let answer = [];
+    let before = '';
+    for (const el of checkWord.split('')) {
+      if (el == before) {
+        answer.pop();
+        continue;
+      } else {
+        answer.push(el);
+        before = el;
+      }
+    }
+    checkWord = answer.join('');
+  }
+
+  return checkWord;
 }
 
 module.exports = problem2;
