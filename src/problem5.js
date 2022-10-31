0e6d1f21d239b42ac99415cf3bec2ac28cf81308
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 23:33:28 +0900

    feat: [5번 문제] 변환 정보가 담긴 배열 초기화

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,8 @@
 const MONEY_TYPE_ARR = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  return MONEY_TYPE_ARR;
+  let answer = Array.from({ length: MONEY_TYPE_ARR.length }, () => 0);
+  return answer;
 }
 
 module.exports = problem5;
