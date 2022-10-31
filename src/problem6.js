d2206a5d031e6dcc124050fa5fba0496fba5d7bf
Dahye <06robin11@gmail.com>
2022-10-31 15:11:24 +0900

    feat: find repeated fractions from nickname

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const fractionMap = new Map();
+  forms.forEach((form) => {
+    const [, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const fraction = nickname[i] + nickname[i + 1];
+      fractionMap.set(fraction, fractionMap.get(fraction) + 1 || 1);
+    }
+  });
+  const repeatedFractions = [];
+  for (const [fraction, number] of fractionMap) {
+    if (number > 1) repeatedFractions.push(fraction);
+  }
 }
 
 module.exports = problem6;
