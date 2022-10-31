288a6d707a42e4b9f4cc33a7d96e5aa9a8666eb7
foundaway98 <foundaway98@naver.com>
2022-10-31 18:54:36 +0900

    문제 3번 함수 위치 변경 및 answer 키워드 변경

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,15 @@
-function clap(number) {
-  let clapCnt = 0;
-  number = number.toString();
-  for (let i = 0; i < number.length; i++) {
-    if (number[i] == "3" || number[i] == "6" || number[i] == "9") {
-      clapCnt += 1;
-    }
-  }
-  return clapCnt;
-}
-
 function problem3(number) {
-  var answer = 0;
+  function clap(number) {
+    let clapCnt = 0;
+    number = number.toString();
+    for (let i = 0; i < number.length; i++) {
+      if (number[i] == "3" || number[i] == "6" || number[i] == "9") {
+        clapCnt += 1;
+      }
+    }
+    return clapCnt;
+  }
+  let answer = 0;
   for (let i = 1; i <= number; i++) {
     answer += clap(i);
   }
