5c481ecc1047cd37c8a2a69812941f6e7b326b64
youzysu <111998760+youzysu@users.noreply.github.com>
2022-10-31 16:21:53 +0900

    refactor: [P6] 각 단어를 해당 크루의 이메일과 매칭하여 Object에 추가하기
    - Object에 해당 단어가 없는 경우 Object에 추가하기
    - Object에 해당 단어가 있는 경우 이전에 저장해둔 크루의 email과 이번 크루의 email을 Array에 추가하기

--- src/problem6.js
+++ src/problem6.js
@@ -2,10 +2,19 @@ function problem6(forms) {
   let targetEmail = []
   let wordCase = {}
   for (let form of forms) {
+    let email = form[0]
     let nickname = form[1]
     let wordList = []
     for (let i = 0; i < nickname.length - 1; i++) {
       wordList.push(nickname.substring(i, i + 2))
+    }
+    for (let word of wordList) {
+      if (!wordCase[word]) wordCase[word] = email
+      else {
+        let presavedEmail = wordCase[word]
+        targetEmail.push(presavedEmail)
+        targetEmail.push(email)
+      }
     }
   }
 } 
