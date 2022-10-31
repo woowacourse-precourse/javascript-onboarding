ae2034af67e03e43479e647f311fb6dcb0c033ff
Sallyvely <tpdus4159@naver.com>
2022-10-31 17:38:55 +0900

    4번 문제 아스키코드를 통해 문자 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,14 @@
 function problem4(word) {
   var answer;
+
+  if(word.charCodeAt[i]>=97 && word.charCodeAt[i]<=122 ){
+    answer= answer+ (219-word.charCodeAt[i]);
+  }
+  else if(word.charCodeAt[i]>=65 && word.charCodeAt[i]<=97){
+    answer= answer+ 155-word.charCodeAt[i];
+  }
+  else answer= answer+word.charCodeAt[i];
+
   return answer;
 }
 
