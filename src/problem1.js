3a7dbae6c67bee442eea9338e0cac82e9bea709c
dhkham <khamdong@gmail.com>
2022-10-31 23:40:09 +0900

    style: (prob_1) change minor styling according to eslint/prettier suggestions

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,4 @@
+/* eslint-disable no-use-before-define */
 function problem1(pobi, crong) {
   var answer;
   const pobiLeft = pobi[0];
@@ -6,20 +7,27 @@ function problem1(pobi, crong) {
   const crongRight = crong[1];
 
   const pobiMaxSum = largerNumber(digitSum(pobiLeft), digitSum(pobiRight));
-  const pobiMaxMultiply = largerNumber(digitMultiply(pobiLeft), digitMultiply(pobiRight));
+  const pobiMaxMultiply = largerNumber(
+    digitMultiply(pobiLeft),
+    digitMultiply(pobiRight)
+  );
   const pobiMax = largerNumber(pobiMaxSum, pobiMaxMultiply); // max score for pobi
+
   const crongMaxSum = largerNumber(digitSum(crong[0]), digitSum(crong[1]));
-  const crongMaxMultiply = largerNumber(digitMultiply(crong[0]), digitMultiply(crong[1]));
+  const crongMaxMultiply = largerNumber(
+    digitMultiply(crong[0]),
+    digitMultiply(crong[1])
+  );
   const crongMax = largerNumber(crongMaxSum, crongMaxMultiply); // max score for crong
 
   if (pobiRight - pobiLeft !== 1 || crongRight - crongLeft !== 1) {
-      answer = -1; // wrong page number: return -1
+    answer = -1; // wrong page number: return -1
   } else if (pobiMax > crongMax) {
-      answer = 1; // pobi wins: return 1
+    answer = 1; // pobi wins: return 1
   } else if (pobiMax < crongMax) {
-      answer = 2; // crong wins: return 2
+    answer = 2; // crong wins: return 2
   } else if (pobiMax === crongMax) {
-      answer = 0; // draw: return 0
+    answer = 0; // draw: return 0
   }
   return answer;
 }
@@ -29,9 +37,10 @@ module.exports = problem1;
 // function that returns the sum of digits
 function digitSum(number) {
   let sum = 0;
-  while (number > 0) {
-      sum += number % 10;
-      number = Math.floor(number / 10);
+  let temp = number;
+  while (temp > 0) {
+    sum += temp % 10;
+    temp = Math.floor(temp / 10);
   }
   return sum;
 }
@@ -39,9 +48,10 @@ function digitSum(number) {
 // function that returns multiplied digits
 function digitMultiply(number) {
   let multiply = 1;
+  let temp = number;
   while (number > 0) {
-      multiply *= number % 10;
-      number = Math.floor(number / 10);
+    multiply *= temp % 10;
+    temp = Math.floor(temp / 10);
   }
   return multiply;
 }
@@ -49,8 +59,7 @@ function digitMultiply(number) {
 // function that compares two numbers and returns the larger one
 function largerNumber(number1, number2) {
   if (number1 > number2) {
-      return number1;
-  } else {
-      return number2;
+    return number1;
   }
+  return number2;
 }
