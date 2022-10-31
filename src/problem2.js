175ac8ef0e37cdbbbd5e0a7113709abf5f75fb78
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 14:57:00 +0900

    feat: Problem 2 - 전체 기능 구현하기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,15 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+const problem2 = cryptogram => {
+  let isMatched = true;
+
+  while (isMatched) {
+    isMatched = false;
+    cryptogram = cryptogram.replace(/(.)\1+/g, () => {
+      isMatched = true;
+      return '';
+    });
+  }
+
+  return cryptogram;
 }
 
 module.exports = problem2;
