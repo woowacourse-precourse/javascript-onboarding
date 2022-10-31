03e725ce10d4ca215a7d0b7a60c30760cbb27c76
Jeong seulho <wjdtmfgh@yu.ac.kr>
2022-10-31 13:49:46 +0900

    Feat(문제5-기능2): 몫 결과들 배열에 담는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -8,6 +8,15 @@ function problem5(money) {
 
   // 화폐 단위
   const kor_won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  // 루프를 돌며 화폐 단위로 나눈 money의 몫들을 배열에 저장
+  kor_won.reduce((acc, ele) => {
+    const 몫 = parseInt(acc / ele);
+    const 나머지 = acc % ele;
+    answer.push(몫);
+    return 나머지;
+  }, money);
+
   return answer;
 }
 
