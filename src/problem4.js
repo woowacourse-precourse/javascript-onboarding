065b6213a2185b1809d4158081f159007b654861
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 13:08:31 +0900

    Feat: [문제번호4] 대소문자 각각 반대로 전환

--- src/problem4.js
+++ src/problem4.js
@@ -11,4 +11,20 @@ function checkUpperCase(element) {
   return false;
 }
 
+function convertWord(element) {
+  if (element === " ") return element; // 아무것도 입력되지 않았을 때 예외처리
+
+  const isUpperLetter = checkUpperCase(element);
+
+  if (isUpperLetter) {
+    // 대문자에서 찾기
+    const idx = UPPERCASE.split("").indexOf(element); // word를 나눈 요소 하나하나 비교하여 해당 idx 찾기.
+    return UPPERCASE[25 - idx]; // 0~25까지
+  } else {
+    // 소문자에서 찾기
+    const idx = LOWERCASE.split("").indexOf(element);
+    return LOWERCASE[25 - idx];
+  }
+}
+
 module.exports = problem4;
