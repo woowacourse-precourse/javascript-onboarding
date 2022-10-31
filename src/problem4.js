c627061012d4fb301087ba9f5ce685ab772ce6bb
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-11-01 04:39:17 +0900

    [4번 문제] feat: changeCapital 함수 및 changeSmall 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 var capitalSum = 'A'.charCodeAt(0) + 'Z'.charCodeAt(0);
 var smallSum = 'a'.charCodeAt(0) + 'z'.charCodeAt(0);
 
+function changeCapital(capital){
+  return String.fromCharCode(capitalSum-capital.charCodeAt(0));
+}
+
+function changeSmall(small){
+  return String.fromCharCode(smallSum-small.charCodeAt(0));
+}
+
+
 function problem4(word) {
   var answer;
   return answer;
