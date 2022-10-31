b464478fdc12c21dbd248c882ab22b09d1795307
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 15:12:00 +0900

    feat: Problem 7 - 친구 관계 저장 기능 구현하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,22 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+const makeFriendList = (friends, friendList) => {
+  for (let i = 0; i < friends.length; i++) {
+    const friendA = friends[i][0], friendB = friends[i][1];
+
+    if (!friendList.hasOwnProperty(friendA))
+      friendList[friendA] = new Set();
+    if (!friendList.hasOwnProperty(friendB))
+      friendList[friendB] = new Set();
+
+    friendList[friendA].add(friendB);
+    friendList[friendB].add(friendA);
+  }
+}
+
+const problem7 = (user, friends, visitors) => {
+  let friendList = {};
+  let score = {};
+
+  makeFriendList(friends, friendList);
 }
 
 module.exports = problem7;
