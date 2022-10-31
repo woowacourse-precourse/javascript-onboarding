07a4fa842ba311769d97a7463bcc7a81b9736192
woo-jk <201702042@o.cnu.ac.kr>
2022-10-31 20:06:13 +0900

    refactor:[PROBLEM2] 함수 사이 공백

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,9 @@
 const duplicateStringExists = (string) => {
   return string.search(/([a-zA-Z])\1/) !== -1;
 };
-
 const cutDuplicateString = (string) => {
   return string.replace(/([a-zA-Z])\1+/g, "");
 };
-
 function problem2(cryptogram) {
   while (duplicateStringExists(cryptogram)) {
     cryptogram = cutDuplicateString(cryptogram);
