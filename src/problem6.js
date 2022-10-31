84998711e72ae5fd73bd36c30f825ab94dfd3e1f
useon <yoosk4986@naver.com>
2022-10-31 23:04:29 +0900

    feat: [problem6] 닉네임을 중복의 가장 작은 단위로 잘라 중복없이 담는다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
 function problem6(forms) {
-  var answer;
+  const nicknameSet = new Set();
+  let answer = [];
+
+  // 1. 모든 닉네임을 중복의 가장 작은 단위로 자른 결과를 중복없이 담는다.
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j + 1 < forms[i][1].length; j++) {
+      nicknameSet.add(forms[i][1].slice(j, j + 2));
+    }
+  }
+
   return answer;
 }
 
