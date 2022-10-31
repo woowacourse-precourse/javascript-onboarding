bfb84b9118e45491f3e0e8735f56d7fc6bd28fee
HojinAn <asdv94@naver.com>
2022-11-01 08:35:03 +0900

    docs: problem 5 feature list

--- src/problem5.js
+++ src/problem5.js
@@ -1,3 +1,11 @@
+/**
+ * Problem 5
+ * 1. 50000, 10000, 5000, 1000, 500, 100, 50, 10, 1을 key로 갖는 Map 자료구조 작성
+ * 2. 입력된 money에 대해 Map.keys()로 순회하며 각 금액의 몫을 구하고 money에서 차감
+ * 3. Map.values()를 반환
+ * @param {number} money 1 이상 1,000,000 이하
+ * @returns {number[]}
+ */
 function problem5(money) {
   var answer;
   return answer;
