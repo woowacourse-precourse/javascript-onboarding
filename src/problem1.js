3a312900c7df52f8dc4b1b17b3ecb28b281407f1
김상두 <puki4416@gmail.com>
2022-10-31 23:28:30 +0900

    problem1 : checkException 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,6 +3,22 @@
 // 승부결과를 판단하는 함수 구현
 // 구현된 함수를 사용하여 problem1 함수 구현
 
+function checkException(pages) {
+  const [left, right] = pages;
+  if (
+    left >= 1 &&
+    left <= 399 &&
+    right >= 2 &&
+    right <= 400 &&
+    left % 2 === 1 &&
+    right % 2 === 0 &&
+    right - left === 1
+  ) {
+    return false;
+  }
+  return true;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
