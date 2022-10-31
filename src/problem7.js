16ba58a22f6cf42f8c3700c5176acdfcf9584591
nerdchanii <nerdchanii@gmail.com>
2022-10-31 22:37:41 +0900

    feat:[PROBLEM7] 친구목록 해쉬맵 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let friendHash = setFriends(friends, new Map());
+function setFriends(friend, hash) {
+  for (const [a, b] of friend) {
+    hash = set(hash, a, b);
+    hash = set(hash, b, a);
+  }
+  return hash;
+
+  function set(hash, a, b) {
+    if (hash.get(a) !== undefined) hash.set(a, [...hash.get(a), b]);
+    else hash.set(a, [b]);
+    return hash;
+  }
+}
 }
 
 module.exports = problem7;
