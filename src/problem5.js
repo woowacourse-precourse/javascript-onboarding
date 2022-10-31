7f5d668b3766889701267db4e2b6b998783adb69
okorion <ok.or.orion@gmail.com>
2022-11-01 01:57:41 +0900

    feat: result 배열 구현 후 매핑 준비

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,17 @@
 // **기능 요구 사항**
-// 1.
-// 2.
-
+// 1. result의 배열을 만든다.
+// 2. result 배열을 인덱스에 따라 최대 나눗셈 횟수를 카운트한다.
+// 3. 결과를 반환한다.
 
 function problem5(money) {
-  var answer;
-  return answer;
+  let array = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let money_table = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let temp_money = money;
+  let temp_index = 0;
+
+
 }
 
 module.exports = problem5;
+
+problem5(50237)
