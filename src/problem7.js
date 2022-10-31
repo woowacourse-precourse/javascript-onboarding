fb12de72524535cb0d182f23d35aed8d12696b5d
김지수 <jisudev00@gmail.com>
2022-10-31 15:56:33 +0900

    Add: Problem 7 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,35 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  const scoreList = {};
+  const friendsList = {};
+
+  for (let [A, B] of friends) {
+    friendsList[A] = friendsList[A] ? [...friendsList[A], B] : [B];
+    friendsList[B] = friendsList[B] ? [...friendsList[B], A] : [A];
+  }
+
+  for (let id of visitors) {
+    scoreList[id] = ++scoreList[id] || 1;
+  }
+
+  // 이미 친구
+  for (let friend of friendsList[user]) {
+    friendsList[friend].forEach((id) => {
+      scoreList[id] = scoreList[id] + 10 || 10;
+    });
+    delete scoreList[friend];
+  }
+
+  delete scoreList[user];
+
+  const sortedList = Object.entries(scoreList).sort((a, b) => {
+    if (a[1] === b[1]) return -1;
+    else return b[1] - a[1];
+  });
+
+  const answer = [...sortedList].map(([name, score]) => {
+    if (score > 0) return name;
+  });
+
   return answer;
 }
-
 module.exports = problem7;
