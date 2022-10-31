36fc60cea2311cbd1e53a04765b8d7bafb2e8ea3
mjmj0217 <mjmj0217@naver.com>
2022-10-31 13:25:21 +0900

    refactor:problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,10 +1,8 @@
 /*
-기능목록
 1. 숫자의 각자릿수의 합과 곱 중 최댓값 구하기
 2. 홀수 짝수의 합,곱의 최댓값 중 최댓값 구하기
 3. 포비와 크롱의 최댓값 중 최댓값 구하기
-4. 승자 알아내서 알맞은 결과 반환하게 만들기
-5. 예외 처리하기
+4. 예외 처리하기
 */
 function maxFromNumber(number) {
   let plus = 0;
@@ -23,7 +21,6 @@ function maxFromArray(arr) {
   const odd = arr[0];
   const even = arr[1];
 
-  //예외 처리
   if (odd % 2 === 0 || even % 2 !== 0 || odd + 1 !== even) {
     return -1;
   }
@@ -35,7 +32,7 @@ function maxFromArray(arr) {
 }
 
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
 
   const pobiMax = maxFromArray(pobi);
   if (pobiMax == -1) {
