56e0fc768c318763f07f468fed07f6668f3c7027
Buyoung Chung <sunarol@naver.com>
2022-10-31 18:11:54 +0900

    [문제5] 배열 answer, bills 변수 pointer 선언
    및 초기화

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,7 @@
 function problem5(money) {
-  var answer;
+  var answer = new Array(9).fill(0);
+  let bills = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let pointer = 0;
   return answer;
 }
 
