5f2d4e52804d15aa36f29dec278ef1b2cf5716ea
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-01 00:06:49 +0900

    problem6 중복 이메일 push 에러 수정

--- src/problem6.js
+++ src/problem6.js
@@ -21,7 +21,9 @@ function problem6(forms) {
   for (let r = 0; r<forms.length; r++){
     for (let s = 0; s<setnickname.length; s++){
       if(forms[r][1].includes(setnickname[s])){
-        answer.push(forms[r][0])
+        if(answer.includes(forms[r][0])==false){
+          answer.push(forms[r][0])
+        }
       }
     }
   }
@@ -40,4 +42,20 @@ function substr(user){
   includenickname.push(temporarynickname)
 }
 var includenickname = []
+// console.log(problem6([
+//   ["jm@email.com", "제이엠"],
+//   ["jason@email.com", "제이슨"],
+//   ["woniee@email.com", "워니"],
+//   ["mj@email.com", "엠제이"],
+//   ["nowm@email.com", "이제엠"],
+//   ["jasp@email.com", "박이김"],
+//   ["jp@email.com", "김이박"],
+//   ["qpw@email.com", "김이순"],
+//   ["teemo@email.com", "티모"],
+//   ["banana@email.com", "대위티모"],
+//   ["secondJm@email.com", "제이엠"],
+//   ["apple@email.com", "땷횱봵"],
+//   ["stran@email.com", "횱봵으"],
+//   ["zebra@email.com", "두글자이상의문자가연속적으로이제맞티모"]
+// ]))
 module.exports = problem6;
