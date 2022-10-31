39f58852dae4a5317de4cc839d14012e2903dc08
sh981013s <sh981013s@gmail.com>
2022-10-31 19:02:41 +0900

    feat: [4] 아스키코드 => 문자 변환 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -7,6 +7,15 @@ const isAlphabet = (char) => {
   return /^[A-Z]$/i.test(char);
 };
 
+/**
+ * 아스키코드로부터 변환한 문자를 반환한다.
+ * @param {number} charCode - 아스키코드
+ * @return {string}
+ */
+const getCharFromAsciiCode = (charCode) => {
+  return String.fromCharCode(charCode);
+};
+
 function problem4(word) {
   var answer;
   return answer;
