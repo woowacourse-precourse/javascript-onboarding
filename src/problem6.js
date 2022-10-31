5362cac4cfe4935fbdcd175506f28d220351bde6
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 14:58:50 +0900

    feat: 해당 문자열을 닉네임 배열이 포함하는 지 확인하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -2,7 +2,7 @@
   구현할 기능 목록
   [O] 닉네임이 다른 닉네임과 중복처리 됐을 때, 이메일 목록에 해당 닉네임의 이메일을 넣는 기능
   [O] 해당 닉네임이 자기 자신을 제외한 다른 닉네임들과 중복인지 확인하는 기능 | return 값: true/false
-  [ ] 해당 문자열을 닉네임 배열이 포함하는 지 확인하는 기능 | return 값 : true/false
+  [O] 해당 문자열을 닉네임 배열이 포함하는 지 확인하는 기능 | return 값 : true/false
 */
 
 function problem6(forms) {
@@ -24,6 +24,10 @@ function isNicknameDuplicate(targetNickname, nicknames, index) {
     if (isNicknameIncludesSubstr(nicknames, subStr, index)) return true;
   }
   return false;
+}
+
+function isNicknameIncludesSubstr(nicknames, subStr, index) {
+  return nicknames.some((nickname, idx) => index != idx && nickname.includes(subStr));
 }
 
 module.exports = problem6;
