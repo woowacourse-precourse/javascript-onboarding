f69ebf710676c7a2a0021266f0af0c22a5b9fa8a
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 04:02:22 +0900

    [p2] 연속된 글자 세기

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,11 @@
+function count(str, i) {
+  while(i < str.length() - 1){
+    if(str[i] === str[i + 1])
+      i++;
+  }
+  return i;
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
