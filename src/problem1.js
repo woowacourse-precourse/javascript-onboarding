10b9622ae5cfc0040420507fc929743107c4df0e
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 20:00:06 +0900

    feat: [1번 문제] 각 자리 숫자 더하기

--- src/problem1.js
+++ src/problem1.js
@@ -2,6 +2,13 @@ function problem1(pobi, crong) {
   if (!passExceptions(pobi, crong)) return -1;
 }
 
+function addDigits(number) {
+  return number
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc + Number(cur), 0);
+}
+
 function passExceptions(pobi, crong) {
   return isSameChapter(pobi[0], pobi[1]) && isSameChapter(crong[0], crong[1]);
 }
