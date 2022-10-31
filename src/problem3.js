0e58b37dd546fa7cc9a3d71b23b40287b6fb6b06
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 11:19:23 +0900

    Feat: [문제번호3] 369일때 count

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,13 @@ function problem3(number) {
   return answer;
 }
 
+function getCurrentCnt(number) {
+  const numberList = number.toString().split("");
+
+  return numberList.reduce((sum, curr) => {
+    if (["3", "6", "9"].includes(curr)) sum += 1;
+    return sum;
+  }, 0);
+}
+
 module.exports = problem3;
