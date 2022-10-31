a3a32afc119d698818b6d699c5588ec155542fb4
deli-ght <minjju0660@gmail.com>
2022-10-31 20:10:38 +0900

    ✨ 문제 4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answer = []
+
+  for (let a of word) {
+    const codeNum = a.charCodeAt(0)
+
+    // 해당 문자가 대문자인 경우
+    if (codeNum >= 65 && codeNum <= 90) {
+      answer.push(155 - codeNum)
+    } else if (codeNum >= 97 && codeNum <= 122) {
+      answer.push(219 - codeNum)
+    } else {
+      answer.push(codeNum)
+    }
+  }
+
+  return String.fromCharCode(...answer)
 }
 
-module.exports = problem4;
+module.exports = problem4
