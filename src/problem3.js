a0e98ecdfd42bf0e8523caf78488d0bc53a1bc3f
Jeonjunwoo <tira618@naver.com>
2022-10-31 16:28:03 +0900

    1부터 입력되는 number까지 배열에 저장

--- src/problem3.js
+++ src/problem3.js
@@ -2,5 +2,10 @@ function problem3(number) {
   var answer;
   return answer;
 }
-
+function countNumber(number) {
+  const arr = [];
+  for(i = 1; i <= number; i ++){
+    arr.push(i);
+  }
+}
 module.exports = problem3;
