3bc4746bf90a323d420e43cd67b138a417487332
minseok1109 <park485201@naver.com>
2022-11-01 01:16:22 +0900

    feat: [problem7] 친구관계 만들기 #기능목록-1

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,22 @@
+//친구관계 object 만드는 함수
+function makeEdge(friends) {
+  let edge = {};
+  for (let i of friends) {
+    let [friendA, friendB] = i;
+    if (friendA in edge) {
+      edge[friendA].push(friendB);
+    } else {
+      edge[friendA] = [friendB];
+    }
+    if (friendB in edge) {
+      edge[friendB].push(friendA);
+    } else {
+      edge[friendB] = [friendA];
+    }
+  }
+  return edge;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
