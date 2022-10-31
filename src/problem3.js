6b038cd2bdd139f1f8127a77922eab8b3b930e7b
KangYunJi <dbswl701@ajou.ac.kr>
2022-10-31 22:31:26 +0900

    feat:[문제3] 요구사항 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
+function is369(number){
+  if(number == '3' || number == '6' || number == '9') return true;
+  return false;
+}
+
 function problem3(number) {
-  var answer;
+  let answer=0;
+  for(let i = 1; i<=number; i++){
+    for(let j = 0; j<i.toString().length; j++){
+      if(is369(i.toString()[j])) answer++;
+    }
+  }
   return answer;
 }
 
