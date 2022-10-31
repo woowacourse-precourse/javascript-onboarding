c71900483e19bf9df3abdfe49e2587625e6a3ca8
seongminn <berry9901@gmail.com>
2022-10-31 23:15:36 +0900

    feat(problem7): 유저와 함께 아는 친구 목록을 구하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -6,8 +6,21 @@ function getUserFriends(user, friends) {
   return userFriends;
 }
 
+function getFriendsOfFriends(user, friends, userFriends) {
+  const friendsOfFriends = friends
+    .filter(
+      (friend) =>
+        !friend.includes(user) &&
+        friend.some((friend) => userFriends.includes(friend))
+    )
+    .map((friend) => (userFriends.includes(friend[0]) ? friend[1] : friend[0]));
+
+  return [...new Set(friendsOfFriends)];
+}
+
 function problem7(user, friends, visitors) {
   const userFriends = getUserFriends(user, friends);
+  const friendsOfFriends = getFriendsOfFriends(user, friends, userFriends);
 
   return answer;
 }
