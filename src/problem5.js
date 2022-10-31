8165566c07d18a006a9574db511beebf62ecf516
hhhminme <hororonoodle@gmail.com>
2022-11-01 00:53:22 +0900

    refactor : Clearly change the variable name used in solution 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,5 @@
 // 출금 가능한 금액 Enum
-const exchangeList = {
+const WithdrawList = {
   fiftyThousand: 50000,
   tenThousand: 10000,
   fiveThousand: 5000,
@@ -12,17 +12,14 @@ const exchangeList = {
 };
 
 function problem5(money) {
-  // 제한사항 :: 금액은 1 이상 1,000,000 이하의 자연수 이어야 한다.
-  if (money < 1 || money > 1000000 || money % 1 !== 0) return;
-
   let balance = money;
-  const exchangeArr = Object.keys(exchangeList); // 출금 가능한 금액의 목록을 가져온다.
-  const answer = Array.from({ length: exchangeArr.length }, () => 0);
+  const widthDrawArray = Object.keys(WithdrawList); // 출금 가능한 금액의 목록을 가져온다.
+  const answer = Array.from({ length: widthDrawArray.length }, () => 0);
 
   // 계좌에서 가장 큰 금액 순서대로 출금을 시도한다.
-  exchangeArr.map((v, i) => {
-    answer[i] = Math.floor(balance / exchangeList[v]); // 출금 가능한 지폐 갯수를 구한다.
-    balance -= exchangeList[v] * answer[i]; // 출금된 금액만큼 계좌에서 빼준다.
+  widthDrawArray.map((v, i) => {
+    answer[i] = Math.floor(balance / widthDrawArray[v]); // 출금 가능한 지폐 갯수를 구한다.
+    balance -= widthDrawArray[v] * answer[i]; // 출금된 금액만큼 계좌에서 빼준다.
   });
 
   return answer;
