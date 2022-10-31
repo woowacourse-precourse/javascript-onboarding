faa2aca33ca6f2a843d0613e5287561e43742a4c
NaveOWO <pianowell96@gmail.com>
2022-10-31 17:54:19 +0900

    [ feat ] problem7_서로 친구인 사용자 객체를 만드는 함수 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,24 @@
+// 기능 구현 목록
+// - 서로 친구인 사용자 객체를 만드는 함수 생성
+// - 친구 관게 객체 생성
+// - user의 친구를 담은 Set 생성
+// - 함께 아는 친구의 수 판별
+// - 타임라인에 방문한 횟수 count
+// - 점수가 0을 초과하는 사용자를 담은 배열 생성 & 내림차순 정렬
+// - 최대 5명의 친구 추천
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const mateObj = {};
+
+  function saveFriend(one, two) {
+    if (one in mateObj) {
+      mateObj[one][0].push(two);
+    } else {
+      mateObj[one] = [[], 0];
+      mateObj[one][0].push(two);
+    }
+  }
+
   return answer;
 }
 
