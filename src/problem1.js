63fb6d12b12e14c5283f4f5ec03e9dd96869bb1b
guridaek <iue16@naver.com>
2022-10-31 13:39:29 +0900

    refactor: 문제1 리턴 방식 변경

--- src/problem1.js
+++ src/problem1.js
@@ -7,17 +7,10 @@ function problem1(pobi, crong) {
 
   const pobi_score = Math.max(getScore(pobi[0]), getScore([pobi[1]]));
   const crong_score = Math.max(getScore(crong[0]), getScore([crong[1]]));
-  let answer = -1;
 
-  if (pobi_score > crong_score) {
-    answer = 1;
-  } else if (pobi_score < crong_score) {
-    answer = 2;
-  } else if (pobi_score === crong_score) {
-    answer = 0;
-  }
-  
-  return answer;
+  if (pobi_score > crong_score) return 1;
+  if (pobi_score < crong_score) return 2;
+  return 0;
 }
 
 function getScore(page) {
@@ -28,7 +21,7 @@ function getScore(page) {
     add_score += Number(e);
     mul_score *= Number(e);
   }
-  
+
   return Math.max(add_score, mul_score);
 }
 
