0965c84fe717e7e8733480b7964411e2a1b39cd2
MINJUN PARK <mj9457@naer,com>
2022-10-31 22:17:16 +0900

    Feat. week1 problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
-function problem1(pobi, crong) {
+function problem1(pobi,crong) {    
   var answer;
-  return answer;
+    
+  var pobiArr1 = pobi.map(x => x.toString().split('').map(x => parseInt(x)).reduce((acc,cur) => acc + cur,0));
+  var pobiArr2 = pobi.map(x => x.toString().split('').map(x => parseInt(x)).reduce((acc,cur) => acc * cur));
+  var pobiMax1 = Math.max(pobiArr1[0],pobiArr1[1]);
+  var pobiMax2 = Math.max(pobiArr2[0],pobiArr2[1]);
+
+  var crongArr1 = crong.map(x => x.toString().split('').map(x => parseInt(x)).reduce((acc,cur) => acc + cur,0));
+  var crongArr2 = crong.map(x => x.toString().split('').map(x => parseInt(x)).reduce((acc,cur) => acc * cur));
+  var crongMax1 = Math.max(crongArr1[0],crongArr1[1]);
+  var crongMax2 = Math.max(crongArr2[0],crongArr2[1])
+  
+  answer = pobi[1] != pobi[0]+1 || crong[1] != crong[0]+1 ? -1 : (Math.max(pobiMax1,pobiMax2) > Math.max(crongMax1,crongMax2) ? 1 : (Math.max(pobiMax1,pobiMax2) == Math.max(crongMax1,crongMax2) ? 0 : 2 )); 
+  return answer
 }
 
 module.exports = problem1;
