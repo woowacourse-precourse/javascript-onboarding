8a2dc572adfccbfafe1137f87359a942df7d67a9
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-10-31 21:32:21 +0900

    ✨ problem1 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,36 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+const MAX_PAGE = 400;
+
+const getSplitedArray = (argument) => ('' + argument).split('');
+
+const getSum = (numbers) =>
+  numbers.reduce((result, currentNumber) => +result + +currentNumber, 0);
+
+const getMultiple = (numbers) =>
+  numbers.reduce((result, currentNumber) => +result * +currentNumber, 1);
+
+const getMaxScore = (left, right) =>
+  Math.max(
+    getSum(getSplitedArray(left)),
+    getMultiple(getSplitedArray(left)),
+    getSum(getSplitedArray(right)),
+    getMultiple(getSplitedArray(right))
+  );
+
+const isValid = (left, right) => {
+  if (+left <= 0 || +right >= MAX_PAGE) return false;
+  if (+left % 2 === 0 || +right % 2 === 1) return false;
+  if (+left + 1 !== +right) return false;
+
+  return true;
+};
+
+const problem1 = (pobi, crong) => {
+  if (!isValid(...pobi) || !isValid(...crong)) return -1;
+
+  const pobiScore = getMaxScore(...pobi);
+  const crongScore = getMaxScore(...crong);
+
+  return pobiScore > crongScore ? 1 : crongScore > pobiScore ? 2 : 0;
+};
 
 module.exports = problem1;
