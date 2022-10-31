0fc6d2ab5e3d9f135e6c62b9d20a817e0441f965
Narcoker <jykim9868@gmail.com>
2022-10-31 17:19:32 +0900

    feat: 1주차 5번 문제 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = new Array(moneyType.length).fill(0);
+  for (let i = 0; i < moneyType.length; i++){
+    answer[i] += Math.floor(money / moneyType[i]);
+    money = money % moneyType[i];
+  }
   return answer;
 }
 
