442d4ea440eade7d2ef510a7d1e23edf1b2a62a8
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 13:28:28 +0900

    Feat: [문제번호7] 각 user 별 map 생성

--- src/problem7.js
+++ src/problem7.js
@@ -6,4 +6,9 @@ function problem7(user, friends, visitors) {
 const ADD_FRIEND_SCORE = 10;
 const ADD_VISITED_SCORE = 1;
 
+const setUserMap = (userMap, [userA, userB]) => {
+  userMap.set(userA, [...(userMap.get(userA) || []), userB]);
+  userMap.set(userB, [...(userMap.get(userB) || []), userA]);
+};
+
 module.exports = problem7;
