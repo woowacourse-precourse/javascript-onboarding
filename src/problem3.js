945659da4f57fbd1dd352817ce02053787725db1
knh6269 <knh6269@gmail.com>
2022-10-31 22:40:37 +0900

    feat : 미션3) 박수 갯수를 구하는 함수

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
-function problem3(number) {
-  var answer;
+//박수 갯수를 구하는 함수
+const getClap = (num) => {
+  //숫자를 문자로 변환후 정규식을 이용해 3,6,9의 개수를 구한다.
+  const clap = String(num).match(/[3,6,9]/g);
+  return clap ? clap.length : 0;
+}
+
   return answer;
 }
 
