c8afb9fc3df8e63bdf5aa2509bb481dd86d636cd
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-11-01 08:41:18 +0900

    [7번 문제] feat: findFriends 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,15 @@
+function findFriends(user, friend){
+  if(friend[0] === user){
+    return friend[1];
+  }
+  else if(friend[1] === user){
+    return friend[0];
+  }
+  else{
+    return 0;
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
