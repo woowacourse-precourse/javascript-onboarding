30f31d2f0a8f9d5381b128a3b6cbc73e19c26f02
deli-ght <minjju0660@gmail.com>
2022-10-31 16:16:33 +0900

    ✨ 문제 2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,32 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let origin = cryptogram
+  let answer = cryptogram
+
+  while (true) {
+    origin = cryptogramCheck(origin)
+
+    if (answer === origin) break
+    else {
+      answer = origin
+    }
+  }
+
+  return answer
 }
 
-module.exports = problem2;
+module.exports = problem2
+
+function cryptogramCheck(value) {
+  let arr = []
+  let prev
+  for (let a of value) {
+    if (prev === a) {
+      arr.pop()
+    } else {
+      arr.push(a)
+      prev = a
+    }
+  }
+
+  return arr.join("")
+}
