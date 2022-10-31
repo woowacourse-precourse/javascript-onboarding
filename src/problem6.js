52c021388bb2759df0c0e3bea5a470defa34dd4d
Jin1won <wlsdnjs16521@gmail.com>
2022-10-31 21:15:41 +0900

    [문제6] feat: 모든 닉네임의 경우의 수에 대해 체크하며 부합하는 이메일을 저장한다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,20 @@
 function problem6(forms) {
   let answer = [];
+  const nickNames = {};
   const regex = /^[A-Za-z0-9]{2,10}@email.com/; // 이메일 형식 체크를 위한 정규표현식
+  forms.forEach((form) => {
+    const [email, nickName] = form;
+    if (!checkEmail(regex, email)) return; // 이메일 형식에 부합하지 않으면 해당 이메일을 스킵한다.
+    if (nickName.length === 1) return; // 닉네임이 한 글자면 연속되는 글자가 없으므로 제외한다.
+    splitWord(nickName).forEach((split) => {
+      if (!Object.keys(nickNames).includes(split)) {
+        nickNames[split] = email;
+      } else {
+        if (!answer.includes(nickNames[split])) answer.push(nickNames[split]);
+        answer.push(email);
+      }
+    });
+  });
 
   return answer;
 }
