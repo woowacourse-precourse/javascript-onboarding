93968cc3028f35a7af0f06b04fe081a6e4e8771f
daal2quf <daal2quf@gmail.com>
2022-11-01 02:04:29 +0900

    승자와 무승부, 예외상황을 알기쉽게 변수에 담음

--- src/problem1.js
+++ src/problem1.js
@@ -1,14 +1,18 @@
 function problem1(pobi, crong) {
   var answer;
+  const pobiWin = 1;
+  const crongWin = 2;
+  const draw = 0;
+  const exception = -1
 
-  if(pobi[0] < 1 || pobi[1] > 400) return -1;
-  if(crong[0] < 1 || crong[1] > 400) return -1;
+  if(pobi[0] < 1 || pobi[1] > 400) return exception;
+  if(crong[0] < 1 || crong[1] > 400) return exception;
 
-  if(pobi[1] - pobi[0] !== 1) return -1;
-  if(crong[1] - crong[0] !== 1) return -1;
+  if(pobi[1] - pobi[0] !== 1) return exception;
+  if(crong[1] - crong[0] !== 1) return exception;
 
-  if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return -1
-  if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return -1
+  if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return exception;
+  if(pobi[0] % 2 !== 1 || pobi[1] % 2 !== 0) return exception;
    
 
 const bigNum = (number) => {
@@ -17,23 +21,21 @@ const bigNum = (number) => {
   while(number > 1) {
     sum += Math.floor(number % 10)
     multiply *= Math.floor(number % 10)
-    number /= 10
+    number /= 10;
   }
   return Math.max(sum, multiply)
 }
 
 const pobiBigNum = Math.max(bigNum(pobi[0]), bigNum(pobi[1]))
 const crongBigNum = Math.max(bigNum(crong[0]), bigNum(crong[1]))
-console.log(pobiBigNum, crongBigNum)
 
 if(pobiBigNum > crongBigNum) {
-  answer = 1
+  answer = pobiWin;
 } else if (pobiBigNum < crongBigNum) {
-  answer = 2
+  answer = crongWin;
 } else if (pobiBigNum === crongBigNum) {
-  answer = 0
+  answer = draw;
 }
-console.log(answer)
 return answer;
 }
 
