c9cfdfec6e10203c3214e9562b73348e610ae495
Jin1won <wlsdnjs16521@gmail.com>
2022-10-31 21:13:52 +0900

    [문제6] feat: 단어를 나눌수 있는 모든 경우의 수를 구하는 기능을 구현한다.

--- src/problem6.js
+++ src/problem6.js
@@ -2,7 +2,21 @@ function problem6(forms) {
   let answer = [];
   const regex = /^[A-Za-z0-9]{2,10}@email.com/; // 이메일 형식 체크를 위한 정규표현식
 
-  return answer.sort(); // 오름차순으로 정렬한다.
+  return answer;
+}
+
+//단어를 나눌수 있는 모든 경우의 수를 구한다.
+function splitWord(word) {
+  let wordList = [];
+
+  for (let i = 0; i < word.length; i++) {
+    for (let j = i + 2; j <= word.length; j++) {
+      let split = word.slice(i, j);
+      wordList.push(split);
+    }
+  }
+
+  return wordList;
 }
 
 // 이메일 형식인지 체크한다.
