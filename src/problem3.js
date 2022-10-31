5fe3b6675d8755f814ed32e2bcb67cc809149d7d
spsp8426 <spsp8426@naver.com>
2022-10-31 14:28:13 +0900

    3번문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let results = i.toString().match(/[3,6,9]/g);
+    if (results)
+      for (let j = 0; j < results.length; j++) {
+        answer++;
+      }
+  }
+
   return answer;
 }
-
 module.exports = problem3;
