b489f248133fef4aec77ff7b66a732c4bbce7e5d
1wooseok <74036731+1wooseok@users.noreply.github.com>
2022-10-31 11:08:11 +0900

    p5: 단위를 순회하면서 몫을 정답 배열에 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
   const answer = Array(9).fill(0);
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < units.length; i++) {
+    const unit = units[i];
+    const 몫 = parseInt(money / unit);
+    if (몫 > 0) {
+      money = money % unit;
+    }
+    answer[i] += 몫;
+  }
+
+  return answer;
 }
 
 module.exports = problem5;
