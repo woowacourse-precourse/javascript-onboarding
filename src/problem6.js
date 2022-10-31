1b46a979894f94a178b9315eef0abdcd509073d2
sujungYu <su_moment@naver.com>
2022-10-31 15:20:08 +0900

    refactor(PROBLEM6):중복 제거

--- src/problem6.js
+++ src/problem6.js
@@ -7,18 +7,20 @@ function problem6(forms) {
         checkArr.push(duplicateTest(name));
         for(let i=0; i<name.length-1; i++) {
             const checkName = name.substr(i, 2);
-            if(checkObj[checkName]) checkObj[checkName] +=1;
+            if(checkObj[checkName]) checkObj[checkName] += 1;
             else checkObj[checkName] = 1;
         }
     })
     checkArr.forEach((v,i)=> {
         v.forEach(d=> {
             if(checkObj[d]>1) {
-                const [mail] = forms[i].slice(0,1)
-                mailArr.push(mail)
+                const [mail] = forms[i].slice(0,1);
+                mailArr.push(mail);
             }
         })
     })
+    mailArr = new Set(mailArr);
+    mailArr = [...mailArr];
     return mailArr.sort();
 }
 
@@ -26,8 +28,8 @@ function duplicateTest(name) {
     let testArr = [];
     for(let i=0; i<name.length-1; i++) {
         const text = name.substr(i, 2);
-        testArr.push(text)
-    }
+        testArr.push(text);
+    };
     return testArr;
 }
 
