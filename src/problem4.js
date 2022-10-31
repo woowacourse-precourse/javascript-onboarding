e01760ce7126c806b9bae4d006fa67289033fb6b
Moyujin <scab12341@gmail.com>
2022-11-01 00:48:58 +0900

    4번 word 반대로 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,21 @@
 function problem4(word) {
-  var answer;
+  var answer = changeWord(word);
   return answer;
 }
+
+const changeWord = (word) => {
+  let result = [];
+  for (let i = 0, wordLen = word.length; i < wordLen; i++) {
+    let unicode = word.charCodeAt(i);
+
+    if (65 <= unicode && unicode <= 90) {
+      unicode = 65 + 90 - unicode;
+    } else if (97 <= unicode && unicode <= 122) {
+      unicode = 97 + 122 - unicode;
+    }
+    result[i] = String.fromCharCode(unicode);
+  }
+  return result.join("");
+};
 
 module.exports = problem4;
