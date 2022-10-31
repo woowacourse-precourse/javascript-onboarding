bb3f7acb1bf2ad876afea41fe1ff367009ac4da5
김지수 <jisudev00@gmail.com>
2022-10-31 12:21:48 +0900

    Add: Problem 6 오름차순 및 중복 제거 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,7 @@
 function problem6(forms) {
   const dupList = {};
   const subStrList = {};
+  const emailSet = new Set();
 
   // 두 글자만 중복돼도 중복
   for (let [, nickname] of forms) {
@@ -12,22 +13,26 @@ function problem6(forms) {
     }
   }
 
-  const answer = forms
-    .map(([email, nickname]) => {
-      let isDup = false;
-      for (let subStr in dupList) {
-        const cnt = dupList[subStr];
-        if (nickname.includes(subStr) && cnt > 1) {
-          isDup = true;
-          break;
-        }
+  forms.map(([email, nickname]) => {
+    for (let subStr in dupList) {
+      const cnt = dupList[subStr];
+      if (nickname.includes(subStr) && cnt > 1) {
+        emailSet.add(email);
+        break;
       }
+    }
+  });
 
-      if (isDup) return email;
-    })
-    .filter((v) => v);
-
-  return answer;
+  return Array.from(emailSet).sort();
 }
+
+problem6([
+  ["jm@email.com", "제이엠"],
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+]);
 
 module.exports = problem6;
