89864254e9ed44b13048cca7ae53ebdc3f201580
JSH99 <tjgus9966@naver.com>
2022-10-31 20:29:22 +0900

    feat: 중복 문자 체크

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = cryptogram;
+
+    // 1. 중복 문자 체크
+    var temp = answer[0]; 
+    var bool = [];
+    
+    for(var i = 1; i < answer.length; i++) {
+      if(temp === answer[i]) {
+        bool.push(true);
+      } else {
+        bool.push(false);
+        temp = answer[i];
+      }
+    }
+
   return answer;
 }
 
