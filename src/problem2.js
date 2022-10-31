6a7e1dc802cd372554cf64eb8dd2744c03051602
imseoyoung <lsy783@sookmyung.ac.kr>
2022-11-01 00:35:40 +0900

    feat: problem2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
+
+  let answer = "";
+
+  while (true) {
+    for (i=0; i<cryptogram.length; i++) {
+      if (cryptogram[i] !== cryptogram[i+1] && cryptogram[i] != cryptogram[i-1]) {
+          answer += cryptogram[i];
+      }
+    }
+    if (cryptogram === answer) {
+      break;
+    }
+
+    cryptogram = answer;
+    answer = ""
+  }
   return answer;
 }
 
