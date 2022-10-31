809de242738cd4dfda17f5429935f4543096c766
Mcdonalds Trump <whdmschd2@gmail.com>
2022-10-31 23:04:57 +0900

    feat: [3번 문제] 총 손뼉 횟수

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,13 @@
 const threeSixNineReg = /[3|6|9]/g;
 
 function problem3(number) {
-  return threeSixNineMatch(number);
+  let clapsNumber = 0;
+  for (let i = 1; i <= number; i++) {
+    if (threeSixNineMatch(i) != null) {
+      clapsNumber += threeSixNineMatch(i).length;
+    }
+  }
+  return clapsNumber;
 }
 
 function threeSixNineMatch(number) {
