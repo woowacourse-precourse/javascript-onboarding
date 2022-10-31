fa406eeb3769242bff9abae997157aa8eab0add5
Monte <monte6198@gmail.com>
2022-10-31 20:45:46 +0900

    Revert "feat(problem6.js): firstSlice() 및 첫 중복 문자열 allSlices에 반영 기능 추가"
    This reverts commit bcf181e3896479ff04e9bf5ce5060b5c52ba47f2.

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,3 @@
-function firstSlice(nameSlice, allSlices) {
-  let count = allSlices.get(nameSlice);
-  allSlices.set(nameSlice, count + 1);
-}
-
 function problem6(forms) {
   var answer;
   return answer;
