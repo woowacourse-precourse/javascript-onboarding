fa399b091da56722703292c315f5d2c0fde06a4a
knh6269 <knh6269@gmail.com>
2022-10-31 22:34:17 +0900

    feat : 미션1) 같은 문자들을 지워주는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
   var answer;
   return answer;
+//같은 문자들을 지워주는 함수
+const eraseArray = (cryptogram, index) => {
+  let count = 1; //자르는 갯수
+  let j = index;
+
 }
 
 module.exports = problem2;
