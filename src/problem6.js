f51b07a790ea3f48a9330926a5dd42b4dd71c3bd
DongRyeong Lee <dr11291129@gmail.com>
2022-10-31 17:36:00 +0900

    fix: P6 isKoreanName 수정

--- src/problem6.js
+++ src/problem6.js
@@ -9,9 +9,8 @@ function isRightDomain(email) {
 
 function isKoreanName(name) {
   for (let i = 0; i < name.length; i++) {
-    if (!(0x1100 <= c) && !(c <= 0x11ff)) return -1;
-    if (!(0x3130 <= c) && !(c <= 0x318f)) return -1;
-    if (!(0xac00 <= c) && !(c <= 0xd7a3)) return -1;
+    check = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    if (!check.test(name[i])) return -1;
   }
   return 1;
 }
