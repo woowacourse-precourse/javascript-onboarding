8bc1cbb809b512be6ae915f24edb3175ac21c3f8
hyeongdong <khd941018@gmail.com>
2022-10-31 22:32:00 +0900

    [문제5] feat(problem5) : getNumberOfEachMoneyUnits를 리턴하는 코드 추가

--- src/problem5.js
+++ src/problem5.js
@@ -6,6 +6,7 @@
 
 function problem5(money) {
   const moneyUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return getNumberOfEachMoneyUnits(money, moneyUnits);
 }
 
 const getNumberOfEachMoneyUnits = (money, moneyUnits) => {
