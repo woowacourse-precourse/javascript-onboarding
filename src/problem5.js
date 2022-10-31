439137510f15b7ee09894546db5ed81a37189966
saetakki <lime.lemonade03@gmail.com>
2022-10-31 12:13:38 +0900

    5번 문제

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  var answer = new Array(9).fill(0)
+  const wallet = [50000,10000,5000,1000,500,100,50,10,1]
+
+  for (let currency of wallet){
+    let idx = wallet.indexOf(currency)
+    answer[idx] = Math.floor(money/currency)
+    money -= currency * Math.floor(money/currency)
+  }
+  return answer
 }
 
 module.exports = problem5;
