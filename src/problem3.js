a7d485f27cadca89dc2e9dd25ff54f92bcca534b
JangHoonSeok <orirori512@naver.com>
2022-11-01 01:08:30 +0900

    feat: problem3. 3, 6, 9 개수 만큼 손뼉치는 횟수 Count하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,27 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  return cntClap(number);
+}
+
+function cntClap(number) {
+  // number까지 박수 몇번 치는지에 대한 함수.
+  let cnt = 0;
+
+  for (let i = 0; i <= number; i++) {
+    let current_num = i;
+
+    while (current_num > 0) {
+      if (
+        current_num % 10 == 3 ||
+        current_num % 10 == 6 ||
+        current_num % 10 == 9
+      ) {
+        cnt += 1;
+      }
+      current_num = Math.floor(current_num / 10);
+    }
+  }
+
+  return cnt;
 }
 
 module.exports = problem3;
