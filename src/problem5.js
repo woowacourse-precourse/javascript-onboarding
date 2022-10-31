e40138d312205335212ad5cc61a0ef1ae5f5d548
yiso22 <88380737+yiso22@users.noreply.github.com>
2022-11-01 03:28:32 +0900

    feat : problem5 solve

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  var change = money;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  var coin = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < coin.length; i++) {
+    answer[i] = parseInt(change / coin[i]);
+    change = change % coin[i];
+  }
+  console.log(answer);
   return answer;
 }
 
