008a06dd502d2a7e26968e1fc09d24e70d005984
KimDongYoung <ehddud1006@pusan.ac.kr>
2022-10-31 14:26:42 +0900

    feat: [문제 7] 친구목록을 Set으로 만드는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,15 @@
+const makeFriendSet = (args) => {
+  const { user, friends } = args;
+  const friendSet = new Set();
+  friends.forEach((friendRelationship) => {
+    const [A, B] = friendRelationship;
+    if (A === user) return friendSet.add(B);
+    if (B === user) return friendSet.add(A);
+  });
+
+  return { ...args, friendSet };
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
