b8ca9980c9a2c87deb437ebc2130aed73f42f468
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-10-31 18:15:35 +0900

    refactor: problem1 main함수 코드 수정
    index 사용하는 for문 삭제 후 map() 으로 대체+변수명 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,5 @@
 //각 자리 합, 곱 중 최댓값을 반환하는 함수
-const makeMax = (n) => {
+function makeMax(n){
   let [add,mul]=[0,1];
   str_num = String(n);
   str_num.split('').forEach((num)=>{
@@ -10,16 +10,15 @@ const makeMax = (n) => {
 }
 
 function problem1(pobi, crong){
-  let answer;
   if (pobi[1]-pobi[0]!==1||crong[1]-crong[0]!==1){
       return -1;
   }
-  let max_arr = [...pobi,...crong]
-  for(let i=0; i<4; i++){
-      max_arr[i] = makeMax(max_arr[i]);
-  }
-  const [p, c] = [Math.max(max_arr[0],max_arr[1]),Math.max(max_arr[2],max_arr[3])];
-  answer = p<c?2:(p>c?1:0);
+  const pobi_ans = Math.max.apply(null,
+                   pobi.map((num)=>makeMax(num)));
+  const crong_ans = Math.max.apply(null,
+                   crong.map((num)=>makeMax(num)));
+  const answer = pobi_ans<crong_ans? 2
+                :(pobi_ans>crong_ans? 1 : 0);
   return answer;
 }
 
