5f3d9fdbcb7dd9325b5e4a79bc21f553380115b0
dudgp128 <20181048@sungshin.ac.kr>
2022-10-31 12:15:17 +0900

    이메일 정규 표현식 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,18 @@
+/*
+  ### 기능 목록
+
+  1. 신청할 수 있는 이메일인지 확인하기
+
+  */
+
 function problem6(forms) {
-  var answer;
+  const regex = /[\w]{2,9}(@email\.com)$/g;
+  const email_Array = forms.filter((e) => {
+    return e[0].search(regex) != null;
+  });
+
+  console.log(email_Array);
+
   return answer;
 }
 
