f390b422bef2f0ca489ea886987095b710af5fb3
Minju Park <project.mizzu@gmail.com>
2022-10-31 16:06:56 +0900

    Feat : currentNumber를 이용한 결과도출
    - 기존의 digit 대신, 현재의 숫자를 10으로 나누는 과정을 반복문을 통해 해결
    - 10으로 나누는 과정에서 소수가 발생하므로 내림인 Math.floor(currentNumber/10)로 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,23 +1,29 @@
 // 13인 경우 4(3,6,9,13인 경우 박수)회, 33인 경우 14(3,6,9,13,16,19,23,26,29,30,31,32,33(2))회
-// 1의 자리에서 10000의 자리까지 3,6,9가 들어가는 경우 count += 1
-// [✅] number의 자릿수 파악하기 -> const digit = number.toString().split('').length;
+// 1의 자리에서 10000의 자리까지 3,6,9가 들어가는 경우 모두 count += 1
 // [✅] let count = 0; 생성
-// [✅] 1에서 number까지 전체의 수 검사
-// [] digit이 1인 경우(9이하) -> number % 10 === 3 || number % 10 === 6 || number % 10 === 9 인 경우일 때 마다 count += 1;
-// [] digit이 2인 경우(99이하) -> (number % 10) 동일 과정 2회 반복 -> 2자리 검사
-// [] digit이 3인 경우(999이하) -> (number % 10) 동일 과정 3회 반복 -> 3자리 검사
-// [] digit이 4인 경우(9999이하) -> (number % 10) 동일 과정 4회 반복 -> 4자리 검사
+// [✅] 1에서 number까지 전체의 수 검사 (for)
+// [✅] 각 자리가 number % 10 === 3 || number % 10 === 6 || number % 10 === 9 인 경우일 때 마다 count += 1;
+// [✅] currentNumber를 10으로 나눈 수가 0이 될 때 까지 반복 (while) -> currentNumber = Math.floor(currentNumber / 10);
 // [✅] count 반환
 
 function problem3(number) {
-  const digit = number.toString().split('').length;
   let count = 0;
   for (let i = 1; i <= number; i++) {
-    console.log(i);
+    let currentNumber = i;
+    while (currentNumber > 0) {
+      if (
+        currentNumber % 10 === 3 ||
+        currentNumber % 10 === 6 ||
+        currentNumber % 10 === 9
+      ) {
+        count += 1;
+      }
+      currentNumber = Math.floor(currentNumber / 10);
+    }
   }
   return count;
 }
 
-console.log(problem3(133));
+console.log(problem3(10000));
 
 module.exports = problem3;
