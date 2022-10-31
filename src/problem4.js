48ad250c0ea8049b138221e4b522404b5f333bb6
DAIN KIM <dnkim419@gmail.com>
2022-10-31 21:17:39 +0900

    feat: solved problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  for (var i = 1; i <= word.length; ++i) {
+    if (word.charCodeAt(i) >= 65 && word.charCodeAt(i) <= 90) {
+      answer += String.fromCharCode(155 - word.charCodeAt(i));
+    } else if (word.charCodeAt(i) >= 97 && word.charCodeAt(i) <= 122) {
+      answer += String.fromCharCode(219 - word.charCodeAt(i));
+    } else {
+      answer += word[i];
+    }
+  }
+
   return answer;
 }
 
