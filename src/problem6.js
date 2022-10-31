40f1d06711d39068a971649245647171fbd65438
durumi99 <kms990615@naver.com>
2022-10-31 23:29:53 +0900

    [problem 6] 두 닉네임을 입력받아 같은 글자가 연속적으로 포함하는지 확인하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,12 @@
+// nick1 과 nick2가 같은 글자를 연속적으로 포함하면 true를 return
+function includeSameChar(nick1, nick2){
+  for(let i = 0 ; i < nick1.length-1 ; i++){
+    subStr = nick1.slice(i, i+2);
+    if(nick2.includes(subStr))
+      return true;
+  }
+  return false;
+}
 function problem6(forms) {
   var answer;
   return answer;
