4622cd921313a1fd413ed778084253a4cdd824ca
Y2IN <dpdls0228@daum.net>
2022-11-01 02:52:21 +0900

    feat : solve problem

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
+function getMoney(money) {
+  let res = [];
+  const won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i < won.length; i++) {
+    res[i] = Math.floor(money / won[i]);
+    money %= won[i];
+  }
+  // console.log(res);
+  return res;
+}
+
 function problem5(money) {
-  var answer;
-  return answer;
+  return getMoney(money);
 }
 
 module.exports = problem5;
