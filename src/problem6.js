968ca8b48f4f095f67039939a173de934669507f
hanbeulYou <hanbeul.you@gmail.com>
2022-10-31 12:30:54 +0900

    feat(PROBLEM6): implement the features 4-6

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,12 @@
 const isValidEmail = (email) => {
   const regExp = /@email.com$/;
-  if(email.length < 11 || email.length.length >= 20) return false;
+  if(email.length < 11 || email.length >= 20) return false;
   return regExp.test(email);
 }
 
 const isValidNickname = (nickname) => {
   const regExp = /^[ㄱ-ㅎ|가-힣]+$/;
-  if(nickname.length < 1 || email.length.length >= 20) return false;
+  if(nickname.length < 1 || nickname.length >= 20) return false;
   return regExp.test(nickname);
 }
 
@@ -14,31 +14,60 @@ const isValid = (form) => {
   return isValidEmail(form[0]) && isValidNickname(form[1]);
 }
 
-const putInDict = (dictObject, subStr) => {
+const cntDefaultDict = (subStr, dictObject) => {
   if(subStr in dictObject) dictObject[subStr] += 1;
   else dictObject[subStr] = 1;
 }
 
-const makeDictObject = (dictObject, nickname) => {
-  const nicknameArr = nickname.split('');
-  const arrLength = nicknameArr.length;
-  for(let i=0; i<arrLength-1; i++) {
-    const subStr = nicknameArr.slice(i, i+2).join('');
-    putInDict(dictObject, subStr);
-  }
+const putInDict = (subStrArr, dictObject) => {
+  subStrArr.forEach((element => {
+    cntDefaultDict(element, dictObject);
+  }));
 }
 
-const makeSubstr = (forms) => {
-  let dictObject = {}
+const makeSubstr = (nickname) => {
+  const nicknameArr = nickname.split('');
+  const arrLength = nicknameArr.length;
+  const subStrArr = [];
+  for(let i=0; i<arrLength-1; i++) {
+    const subStr = nicknameArr.slice(i, i+2).join('');
+    subStrArr.push(subStr);
+  }
+  return subStrArr;
+}
+
+const makeDictObject = (forms, dictObject) => {
   forms.forEach((element) => {
-    makeDictObject(dictObject, element[1])
+    const subStrArr = makeSubstr(element[1])
+    putInDict(subStrArr, dictObject);
   })
+}
+
+const checkDict = (subStr, dictObject) => {
+  return dictObject[subStr] != 1;
+}
+
+const isSameExist = (form, dictObject) => {
+  const formSubStr = makeSubstr(form[1]);
+  return formSubStr.some(element => checkDict(element, dictObject));
+}
+
+const onlyNicknames = (returnForms) => {
+  const returnArr = []
+  returnForms.forEach(element => {
+    returnArr.push(element[0]);
+  })
+  return returnArr;
 }
 
 function problem6(forms) {
   const filteredForms = forms.filter(isValid);
-  var answer;
-  return answer;
+  let dictObject = {}
+  makeDictObject(filteredForms, dictObject);
+
+  const returnForms = forms.filter((element) => isSameExist(element, dictObject))
+  const returnEmails = onlyNicknames(returnForms);
+  return returnEmails.sort();
 }
 
 module.exports = problem6;
