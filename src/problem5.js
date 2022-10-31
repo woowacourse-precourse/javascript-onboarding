521693b6c62976f31b79506896bc5096e2635870
7ngenious <zxvs1218@gmail.com>
2022-11-01 05:07:05 +0900

    feat: [Problem5] 1st Soltion

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
 function problem5(money) {
-  var answer;
+  const count = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+  let total = money;
+
+  for (let i = 0; i < count.length; i++) {
+    const cnt = Math.floor(total / count[i]);
+
+    total = total % count[i];
+
+    answer.push(cnt);
+  }
+
   return answer;
 }
 
