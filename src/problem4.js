33cf10bc54d194a3cfc968ae8b62c6b66e47b127
Buyoung Chung <sunarol@naver.com>
2022-10-31 17:54:26 +0900

    [문제4] 아스키 코드 값 바꿔주는 함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,10 @@
+function convertCode(code) {
+  if (code >= 65 && code <= 90) {
+    return code + (77.5 - code) * 2;
+  } else if (code >= 97 && code <= 122) {
+    return code + (109.5 - code) * 2;
+  }
+}
 function problem4(word) {
   var answer;
   return answer;
