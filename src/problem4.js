cdec73671247b4088e08151078a809df99e4338d
OrangeVinyl <hap_hazard@naver.com>
2022-10-31 23:43:24 +0900

    Problem4) 대문자,소문자 구분 아스키코드 반환 및 함수 완성

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,23 @@
 function problem4(word) {
+  //문자열 쪼개기
   const aplha = word.split("");
 
   let answer = aplha.map((r) => {
     const regex = /^[a-z|A-Z]/;
-    if (!regex.test(r)) return r;
-  });
 
+    //정규식으로 알파벳이 아닌 문자 확인
+    if (!regex.test(r)) return r;
+    
+    // 문자가 대문자, 소문자일때 구분
+    if (r === r.toUpperCase()) {
+      let ascii = r.toLowerCase().charCodeAt(0);
+      return String.fromCharCode(122 - ascii + 97).toUpperCase();
+    } else {
+      let ascii = r.charCodeAt(0);
+      return String.fromCharCode(122 - ascii + 97);
+    }
+  });
+  //쪼개진 문자열 합치기
   return answer.join("");
 }
 
