548c26651fd268dcb93b5590a55a05d0e01bb355
DAIN KIM <dnkim419@gmail.com>
2022-10-31 16:14:22 +0900

    feat: solved problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,73 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const [pobiLeft, pobiRight] = pobi;
+  const [crongLeft, crongRight] = crong;
+
+  if (
+    pobiLeft + 1 !== pobiRight ||
+    pobiLeft % 2 !== 1 ||
+    pobiRight % 2 !== 0 ||
+    crongLeft + 1 !== crongRight ||
+    crongLeft % 2 !== 1 ||
+    crongRight % 2 !== 0 ||
+    pobiLeft < 1 ||
+    pobiLeft > 400 ||
+    pobiRight < 1 ||
+    pobiRight > 400 ||
+    crongLeft < 1 ||
+    crongRight > 400 ||
+    crongRight < 1 ||
+    crongRight > 400
+  )
+    return -1;
+
+  const [pobiLeftSum, pobiRightSum, crongLeftSum, crongRightSum] = [
+    pobiLeft,
+    pobiRight,
+    crongLeft,
+    crongRight,
+  ].map((num) => {
+    sum = 0;
+    while (num > 0) {
+      sum += num % 10;
+      num /= 10;
+    }
+  });
+
+  const [pobiLeftMul, pobiRightMul, crongLeftMul, crongRightMul] = [
+    pobiLeft,
+    pobiRight,
+    crongLeft,
+    crongRight,
+  ].map((num) => {
+    mul = 1;
+    while (num > 0) {
+      mul *= num % 10;
+      num /= 10;
+    }
+  });
+
+  const pobiMax = Math.max(
+    pobiLeftSum,
+    pobiRightSum,
+    pobiLeftMul,
+    pobiRightMul
+  );
+
+  const crongMax = Math.max(
+    crongLeftSum,
+    crongRightSum,
+    crongLeftMul,
+    crongRightMul
+  );
+
+  pobiMax > crongMax
+    ? (answer = 1)
+    : pobiMax < crongMax
+    ? (answer = 2)
+    : (answer = 0);
+
   return answer;
 }
 
