80ac430733986573d5d7c4ab3901fd340015815e
0hyeon <djdjdjk2006@naver.com>
2022-10-31 19:35:03 +0900

    problem7

--- src/problem6.js
+++ src/problem6.js
@@ -1,22 +1,68 @@
 function problem6(forms) {
-  for(let i =0;i< forms.length;i++){
-    console.log(forms[i]);
-    
-    for(let j = 0;j< forms[i][1].length;j++){//이름의 갯수만큼 포문
-        console.log(forms[i][1][j]);
+  
+  let formArray = [];
+  formArray = searchName(forms);
+  let formArrayAfter = []
+  formArrayAfter = formArrayWord(formArray,forms).split(",");
+  let duplicate = duplicatefc(formArrayAfter);
+  // console.log("duplicate : " ,duplicate);
+  //index반환;
+  let result= [[]];
+  let count=0;
+
+  if(duplicate == "") return;
+
+  for(let i = 0;i<forms.length;i++ ){//이안에 제이 있으면 index반환 
+    // console.log(forms[i][1]);
+    if(forms[i][1].includes(duplicate) == true){
+      result[count++] = forms[i][0];
+      result = result.sort();
+      // console.log("result : ",result);
     }
   }
+  return result;
 
-  // 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함된 단어 찾기
 }
 
+function searchName (forms){
+  let result = [];
+  for(let i =0;i< forms.length;i++){
+    result += forms[i][1]+ ",";
+  }
+  return result;
+}
+function formArrayWord(formArray,forms){
+  let result = [];
+  for(let i = 0; i < formArray.length;i++){
+    for(let j = forms[1][1].length ; j < formArray.length;j++){//제이슨부터 시작 
+      if( formArray[i]== formArray[j]){//첫글자가 같고
+        if( formArray[i+1]== formArray[j+1]){//두번째 글자도 같으면 
+          let afterWord = formArray[i]+formArray[i+1];//두단어를 합쳐서
+          if(afterWord.includes(",")) continue;// 콤마를빼고
+          result += afterWord+",";
+        }
+      }
+    }
+  }
+  return result;
+}
+function duplicatefc(formArrayAfter){
+  let duplicate = [];
+  for(let i = 0;i < formArrayAfter.length; i++){
+    if(duplicate.includes(formArrayAfter[i])==true){
+      console.log("중복",formArrayAfter[i]);
+      return formArrayAfter[i];
+    }
+    duplicate += formArrayAfter[i]+" ";
+  }
+}
 input =[
   [ 
-    ["jm@email.com", "제이엠"], 
-    ["jason@email.com", "제이슨"], 
-    ["woniee@email.com", "워니"], 
-    ["mj@email.com", "엠제이"], 
-    ["nowm@email.com", "이제엠"]  
+      ["jm@email.com", "제이엠"],
+      ["jason@email.com", "제이슨"],
+      ["woniee@email.com", "워니"],
+      ["mj@email.com", "엠제이"],
+      ["nowm@email.com", "이제엠"],
   ] 
 ]
 for (let i = 0; i < input.length; i++) {
--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,159 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+  function problem7(user, friends, visitors) {
+    //   사용자와 함께 아는 친구의 수 = 10점
+    // 사용자의 타임 라인에 방문한 횟수 = 1점
+    // 미스터코의 친구 추천 규칙에 따라 점수가 가장 높은 순으로 정렬하여 
+    // 최대 5명을 return 하도록 solution 메서드를 완성하라. 
+    // 이때 추천 점수가 0점인 경우 추천하지 않으며, 추천 점수가 같은 경우는 이름순으로 정렬한다.
+    let userFriends= findIndex(user,friends);////mrko의 친구를 찾고,
+    let withoutObj = userFriends.concat(user);//본인과 친구들제외시켜서
+    let allFrinds = friendsOfFriends(withoutObj,friends);//본인과 친구들을 제외한 새로운 친구들을 
+    let knowFriends = withoutObjfx(withoutObj,allFrinds);//중복제거된 값// andole', 'jun'
+    let visitor = withoutObjfx(withoutObj,visitors);
+    return scoreFx(knowFriends,visitor);
+  }
+  function scorePlus(ScoreObj,knowFriends,visitor){
+    result = [];
+    count=0;
+    console.log("ScoreObj : ",ScoreObj);
+    console.log("knowFriends : ",knowFriends);
+    console.log("visitor : ",visitor);
 
-module.exports = problem7;
+    for(let l= 0; l < ScoreObj.length;l++){
+      for(let i= 0; i < knowFriends.length;i++){
+        if(ScoreObj[l].name == knowFriends[i]){
+          ScoreObj[l].score += 10;
+        }
+      }
+    }
+    for(let l= 0; l < ScoreObj.length;l++){
+      for(let i= 0; i < visitor.length;i++){
+        if(ScoreObj[l].name == visitor[i]){
+          ScoreObj[l].score += 1;
+        }
+      }
+    }
+    console.log("ScoreObj : ",ScoreObj);
+    for(let j = 0;j<ScoreObj.length;j++){
+      result[count++] = ScoreObj[j].name;
+    }
+    console.log("result : ",result);
+    return result;
+  }
+  function scoreFx(knowFriend,visitor){
+
+    knowFriends = [];
+    let duplicateVisit = [];
+    for(let o = 0; o< knowFriend.length;o++){//아는친구 중복제거 
+      if(!knowFriends.includes(knowFriend[o])) knowFriends.push(knowFriend[o]);
+    }
+    for(let t = 0; t< visitor.length;t++){//아는친구 중복제거 
+      if(!duplicateVisit.includes(visitor[t])) duplicateVisit.push(visitor[t]);
+    }
+
+    // console.log(knowFriends);
+    // console.log(duplicateVisit);//한개의 visit
+    //중복제거
+    let allObj = [];
+    let ScoreObj = [];
+    for (let i = 0;i < knowFriends.length;i++){
+      allObj.push(knowFriends[i]); 
+    };
+    
+    for (let j = 0;j < duplicateVisit.length;j++){
+      allObj.push(duplicateVisit[j]); 
+    };
+    for(let t = 0; t< visitor.length;t++){// 중복제거 
+      if(!duplicateVisit.includes(visitor[t])) duplicateVisit.push(visitor[t]);
+    }
+
+
+
+    for(let l = 0;l< allObj.length;l++){//객체생성
+      if(!ScoreObj.includes(allObj[l])){
+        ScoreObj.push(
+          new Object({
+            name : `${allObj[l]}`,
+            score : 0
+          })
+        )
+      } 
+    };
+
+
+    let news = [];
+    let newsCount = 0;
+    for(s=0; s < knowFriends.length ;s++){
+      news[newsCount++] = knowFriends[s];
+    }
+    for(u=0; u < visitor.length ;u++){
+      news[newsCount++] = visitor[u];
+    }
+
+    
+    return scorePlus(ScoreObj,knowFriends,visitor);
+    
+  };
+  function withoutObjfx(withoutObj,allFrinds){
+    let thirdResult = [];
+    let count = 0;
+    let newadult = allFrinds.toString().split(',');
+    for(let i = 0; i< allFrinds.length;i++){
+      if(withoutObj.includes(newadult[i]) == false ){
+        thirdResult[count++] = newadult[i];
+      }
+    }
+    
+    return thirdResult;
+  }
+  function friendsOfFriends(user,friends){
+    let arrayResult= [];
+    let count = 0;
+    for(let j = 0; j<user.length;j++){ //1 2 
+      for(let i = 0; i<friends.length;i++){// 8
+        if(friends[i].includes(user[j]) == true){//user와 함께한 친구리스트
+          arrayResult[count++] = friends[i];
+        }
+      }
+    }
+    return arrayResult;
+    // return secondResult;
+  }
+  function findIndex(user,friends) {
+    let arrayResult= [];
+    let count = 0;
+    for(let i = 0; i<friends.length;i++){
+      if(friends[i].includes(user) == true){//user와 함께한 친구리스트
+        arrayResult[count++] = friends[i];
+        // user를 제외하고 리스트 담아반환하기 
+        let userWithFrinds = arrayResult.toString().split(",");
+        secondResult = [];
+        secondCount = 0;
+        for(let j = 0; j < userWithFrinds.length;j++){
+          if(userWithFrinds[j].includes(user)== false){
+            secondResult[secondCount++] = userWithFrinds[j];
+          }
+        }
+      }
+    }
+    return secondResult;
+  }
+  input = [
+    {
+    user :  "mrko",
+    friends : [ 
+      ["donut", "andole"], 
+      ["donut", "jun"], 
+      ["donut", "mrko"], 
+      ["shakevan", "andole"], 
+      ["shakevan", "jun"], 
+      ["shakevan", "mrko"] 
+    ],
+    visitors : ["bedi", "bedi", "donut", "bedi", "shakevan"]
+    // result 기대값 = ["andole", "jun", "bedi"]
+    }
+  ]
+  for(let i = 0; i< input.length;i++){
+    console.log( `# ${problem7(input[i].user,input[i].friends,input[i].visitors)}`);
+  }
+  
+  module.exports = problem7;
