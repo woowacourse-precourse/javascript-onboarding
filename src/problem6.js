1973fe338e4823f2f530213b7f536ea105e2c37b
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 13:15:13 +0900

    Feat: [문제번호6] 이메일 형식과 닉네임이 한글인지 체크하는 로직 생성

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,16 @@ function problem6(forms) {
   return answer;
 }
 
+const POSSIBLE_EMAIL = "email.com";
+
+const checkEmail = (email) => {
+  const [_, domain] = email.split("@");
+
+  return domain === POSSIBLE_EMAIL;
+};
+
+const isKorea = (nickname) => {
+  return Boolean(nickname.match(/^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/));
+};
+
 module.exports = problem6;
