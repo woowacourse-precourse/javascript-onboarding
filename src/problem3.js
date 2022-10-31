ba35cfa16c5c0664b368c7bfed7690df577d530b
Buyoung Chung <sunarol@naver.com>
2022-10-31 17:44:20 +0900

    [문제3] 숫자들을 문자열로 변환

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,9 @@
 function problem3(number) {
   if (number === 1 || number === 2) return 0;
-  var answer;
+  var answer = 0;
+  for (let i = 3; i < number + 1; i++) {
+    let strNum = String(i);
+  }
   return answer;
 }
 
