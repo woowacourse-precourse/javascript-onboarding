7acab940bc9e1113e3bd7e85f4ca9d746e30d873
yunho <Yunho7058@github.com>
2022-10-31 18:39:23 +0900

    problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    let num = `${i}`;
+    if (num.includes('3') || num.includes('6') || num.includes('9')) {
+      while (num.length > 0) {
+        let last = num.length - 1;
+        if (num[last] === '3' || num[last] === '6' || num[last] === '9') {
+          answer++;
+        }
+        num = num.slice(0, num.length - 1);
+      }
+    }
+  }
   return answer;
 }
 
