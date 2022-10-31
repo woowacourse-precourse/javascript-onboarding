9fe38627a382a658d0b70406e6139af89de15638
rovxxmjxx <rovxxmjxx@gmail.com>
2022-10-31 22:07:02 +0900

    [Feat:Problem6] 에러 처리

--- src/problem6.js
+++ src/problem6.js
@@ -3,6 +3,16 @@ const compose =
     (initialValue) =>
       fns.reduce((composed, fn) => fn(composed), initialValue);
 
+const handleError = (forms) => {
+  const emails = getValuesOf("email")(forms);
+
+  for (let email of emails) {
+    const e = email.split("@")[1];
+    if (e !== "email.com") throw new Error("지원하지 않는 이메일 형식입니다.");
+  }
+  return forms;
+};
+
 const getValuesOf = (target) => (forms) =>
   forms.map(([email, nickname]) => {
     if (target === "email") return email;
