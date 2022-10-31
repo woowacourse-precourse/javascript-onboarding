f357dc3c13462c3ad899a04ae217bd1024640766
kyeongwxx <kyeongwxx@gmail.com>
2022-10-31 19:34:45 +0900

    problem6. 중복 문자를 key로, 중복 횟수를 value로 갖는 duplicatedList 객체 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = []
+  let nickname;
+
+  let duplicatedList = forms
+    .map((el) => el[1])
+    .reduce((acc, cur) => {
+      for (let i = 0; i < cur.length - 1; i++) {
+        nickname = cur.substr(i, 2);
+        acc[nickname] ? (acc[nickname] += 1) : (acc[nickname] = 1);
+      }
+      return acc;
+    }, {});
+
+  return;
 }
 
 module.exports = problem6;
