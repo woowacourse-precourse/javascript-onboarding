30d18009775da9cbc77c7944d7bf5f1fd67681cd
YMS <yms1789@naver.com>
2022-10-31 20:51:26 +0900

    fix: problem2 중복 문자 삭제 기능 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,20 @@
 function problem2(cryptogram) {
   let answer;
   function sameSlice(text) {
-    let isDouble = false;
+    let isSame = false;
     for (let i = 0; i < text.length - 1; i++) {
       if (text[i] === text[i + 1]) {
+        let j = i + 1;
+        while (text[j] === text[i]) {
+          text[j] = "";
+          j++;
+        }
         text[i] = "";
-        text[i + 1] = "";
-        isDouble = true;
+        isSame = true;
       }
     }
     text = text.filter((ele) => ele !== "");
-    if (isDouble === false) {
+    if (isSame === false) {
       return text.join("");
     } else {
       return sameSlice(text);
@@ -19,4 +23,5 @@ function problem2(cryptogram) {
   answer = sameSlice(Array.from(cryptogram));
   return answer;
 }
+
 module.exports = problem2;
