83117fdac87b16484fa1cab361eef2f8e8c24273
sein <hyuntpdls@gmail.com>
2022-10-31 16:08:05 +0900

    [문제3] feat: 숫자 각 자리수로 나누기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
 function problem3(number) {
   var answer;
+
   return answer;
+}
+
+// 숫자 자리수로 나누기
+function makeNumDigits(num) {
+  num = num.toString();
+  let array = [];
+  for (let i = 0; i < num.length; i++) {
+    array.push(num[i]);
+  }
+  return array;
 }
 
 module.exports = problem3;
