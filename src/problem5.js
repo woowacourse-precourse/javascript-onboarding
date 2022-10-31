d15e03de22edd3a11cf7fc87d167907c433533cf
sying012 <syi369@naver.com>
2022-10-31 13:48:17 +0900

    problem5 - 기능 목록 작성, 제한사항 검사 함수 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
   var answer;
+
+  if (violationChk(money)) {
+    answer = counter(money);
+  }
+
   return answer;
+}
+
+function violationChk(money) {
+  return money >= 1 && money <= 1000000;
 }
 
 module.exports = problem5;
