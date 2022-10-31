80aeba19867defdf95dd58d83e45835596cc79a2
Ryong-E <think8867@gmail.com>
2022-10-31 16:06:51 +0900

    feat: PROBLEM1 첫 왼쪽페이지 짝수일때 에러처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,14 @@
 function problem1(pobi, crong) {
   let answer;
+  if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) {
+    return -1;
+  }
+  if (pobi[0] === 1 || pobi[1] === 400 || crong[0] === 1 || crong[1] === 400) {
+    return -1;
+  }
+  if (pobi[0] % 2 === 0 || crong[0] % 2 === 0){
+    return -1;
+  }
   const pobiScore = Math.max.apply(
     null,
     pobi.map((pageNumber) => getMaxNumber(pageNumber))
@@ -15,13 +24,6 @@ function problem1(pobi, crong) {
     answer = 2;
   } else if (pobiScore === crongScore) {
     answer = 0;
-  }
-
-  if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) {
-    answer = -1;
-  }
-  if (pobi[0] === 1 || pobi[1] === 400 || crong[0] === 1 || crong[1] === 400) {
-    answer = -1;
   }
 
   return answer;
