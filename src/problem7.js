d668063b4a577e141ae02fe889018150a41a7ae4
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-11-01 08:43:20 +0900

    [7번 문제] feat: arrFriends 구현

--- src/problem7.js
+++ src/problem7.js
@@ -22,6 +22,14 @@ function problem7(user, friends, visitors) {
       nameMap.set(friends[i][1],0);
     }
   }
+
+  
+  var arrFriends = [];
+  for(let i = 0;i < friends.length;i++){
+    if(findFriends(user,friends[i]) !== 0){
+      arrFriends.push(findFriends(user,friends[i]));
+    }
+  }
   return answer;
 }
 
