d807c53b9965ff2558b2f61c013f1d1861eb3de4
durumi99 <kms990615@naver.com>
2022-10-31 19:47:17 +0900

    [problem 2] 더 이상 연속하는 중복 문자가 없을 때까지 연속하는 중복 문자 제거 반복하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -2,15 +2,20 @@ function problem2(cryptogram) {
   var answer;
   if(cryptogram.length == 1)
     return answer;
-  
-  for(let i = 0 ; i < cryptogram.length-1; i++){
-    if(cryptogram[i] == cryptogram[i+1]){ // 중복 문자가 연속되는 경우      
-      tmp = i+1;
-      while(cryptogram[i] == cryptogram[tmp++]);
-      // 문자열의 인덱스 i~tmp 까지 제거    
-      cryptogram = cryptogram.slice(0,i) + cryptogram.slice(tmp-1);
-      i--;
+  while(true){
+    let check = false;
+    for(let i = 0 ; i < cryptogram.length-1; i++){
+      if(cryptogram[i] == cryptogram[i+1]){ // 중복 문자가 연속되는 경우
+        check = true;
+        tmp = i+1;
+        while(cryptogram[i] == cryptogram[tmp++]);
+        // 문자열의 인덱스 i~tmp 까지 제거    
+        cryptogram = cryptogram.slice(0,i) + cryptogram.slice(tmp-1);
+        i--;
+      }
     }
+    if(check == false) // 연속된 중복 문자가 없는 경우 while문 종료
+      break;   
   }
   answer = cryptogram
   return answer;
