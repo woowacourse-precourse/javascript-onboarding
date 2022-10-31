c7b412d2de84387256c072748246ba44354acf3c
nara04040 <nara04040@naver.com>
2022-10-31 20:12:49 +0900

    [problem7] feat : 리스트에서 중복 제거

--- src/problem7.js
+++ src/problem7.js
@@ -9,7 +9,21 @@ function problem7(user, friends, visitors) {
     });
   }
   const visitorFriends = friendsList.concat(visitors);
+  let justFriends = new Set(visitorFriends);
+  console.log(justFriends);
   return answer;
 }
+user = "mrko";
+friends = [
+  ["mrko", "jun"],
+  ["donut", "jun"],
+  ["donut", "mrko"],
+  ["shakevan", "andole"],
+  ["jun", "andole"],
+  ["shakevan", "jun"],
+  ["shakevan", "mrko"],
+];
+visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
 
+console.log(problem7(user, friends, visitors));
 module.exports = problem7;
