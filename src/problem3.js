0e4fc21293c1b1ba94cce68d4959a07f93d035f9
nangkyeonglim <nangkyeonglim@gmail.com>
2022-10-31 23:49:24 +0900

    refactor(문제3): 특정 숫자를 한 자리씩 쪼개는 기능 분리

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,22 @@
 const CLAP_NUMBER = [3, 6, 9];
 
+const splitSingleDigit = (number) => {
+  return number.toString().split("");
+}
+
 const countClap = (number) => {
   let count = 0;
-  for (let n of number.toString())
+  number.map((n) =>{
     if (CLAP_NUMBER.includes(Number(n))) count += 1;
+  })
   return count;
 }
 
 function problem3(number) {
   let count = 0;
   for (let i = 1; i <= number; i += 1) {
-    count += countClap(i);
+    let num = splitSingleDigit(i);
+    count += countClap(num);
   }
 return count;
 }
