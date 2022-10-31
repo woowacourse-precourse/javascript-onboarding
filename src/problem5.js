6b40ea135bee556be88a1bbcb5c02f751d801fcf
pakxe <pigkill40@naver.com>
2022-10-31 17:12:03 +0900

    chore: [P5] 가독성 향상을 위한 주석 작성

--- src/problem5.js
+++ src/problem5.js
@@ -3,7 +3,7 @@ const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 function problem5(money) {
   return MONETARY_UNIT.map((m) => {
     let count = Math.trunc(money / m);
-    money = money % m;
+    money = money % m; // 단위 지폐를 뺀 나머지 잔돈
     return count;
   });
 }
