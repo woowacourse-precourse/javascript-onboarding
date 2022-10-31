78ad1eaaff4980449adfba2408df7c11466ddfb4
jiyun1006 <jiyun4251@gmail.com>
2022-11-01 01:55:20 +0900

    ✅  problem2 case1, 2 완성 (feat. 앞뒤 연속되는 문자를 모든 문자열에서 제거하고, 문자열을 재구성한 뒤, 다시 연속되는 문자를 발견하는 방식으로 해결)

--- src/problem2.js
+++ src/problem2.js
@@ -1,18 +1,22 @@
 function problem2(cryptogram) {
   let answer;
-
-  const alphabet = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p',
-    'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z'];
-
-  alphabet.map((el) => {
-    let arrCrypto = cryptogram.split("");
-    let idx = arrCrypto.indexOf(el);
-    if (idx !== -1) {
-      cryptogram = cryptogram.split(`${arrCrypto[idx]}${arrCrypto[idx]}`).join('');
+  let len = cryptogram.length;
+  let idx = 0;
+  let cryptoArr = cryptogram.split(""); // 탐색하기 편하게 배열로 변환.
+  while (idx < len - 1) { // 마지막 전 문자까지 탐색할 것 이므로.
+    
+    if (cryptoArr[idx] === cryptoArr[idx + 1]) {
+      cryptogram = cryptogram.split(`${cryptoArr[idx]}${cryptoArr[idx]}`).join(''); // 문자를 갱신
+      cryptoArr = cryptogram.split(""); // 갱신된 문자를 다시 배열로 변환.
+      len = cryptogram.length; // 계속해서 길이를 갱신.
+      if (idx === 0) idx = 0;
+      else idx -= 1;
     }
-    console.log(cryptogram);
-  })
-  
+    else { // 현재 idx의 문자와 뒤의 문자가 같지않다면 다음 문자로 넘어간다.
+      idx++;
+    }
+  }
+  answer = cryptogram;
   return answer;
 }
 
