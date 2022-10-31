8c3e2af211cccdb82697517b6ed185f5218d1cfc
jinlee <vito9270@naver.com>
2022-11-01 01:23:16 +0900

    problem7  friends와 visitors 배열을 하나로 합쳐 set으로 중복을 제거한 형태 제작

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  
+  class PersonName {
+    constructor(friends, visitors) {
+      this.friends = friends
+      this.visitors = visitors
+    }
+    seeAllNames() {
+      let temp = friends.flat()
+      const allNames = [].concat(temp, visitors)
+      console.log('allNames', allNames)
+      return new Set(allNames)
+    }
+  }
+  const relatedUsers = new PersonName(friends, visitors).seeAllNames()
+  console.log(relatedUsers)
+  // return answer;
 }
 
 module.exports = problem7;
