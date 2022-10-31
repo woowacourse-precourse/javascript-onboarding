1c79f93ada225cdfd64763f0187cf1584c8258fe
Heeseung <gmltmd0816@naver.com>
2022-10-31 23:07:29 +0900

    Solved: Problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
+function change(alphabet) {
+  var code = alphabet.charCodeAt(0);
+  
+  if (65 <= code && code <= 90) {
+    return String.fromCharCode(155 - code);
+  } else if (97 <= code && code <= 122) {
+    return String.fromCharCode(219 - code);
+  } else {
+    return String.fromCharCode(code);
+  }
+}
+
 function problem4(word) {
-  var answer;
-  return answer;
+  var answer = [];
+  var arr = [...word]
+  
+  for (let i = 0; i < arr.length; i++) {
+    answer.push(change(arr[i]));
+  }
+
+  return answer.join("");
 }
 
 module.exports = problem4;
