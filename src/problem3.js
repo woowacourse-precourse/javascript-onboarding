bd34ea348b46a7fd0748a3526047895cae209215
Chanwoong1 <jcw7468@gmail.com>
2022-10-31 20:46:28 +0900

    refactor: 코드 간결화 및 최종 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,26 +1,23 @@
 function isValid(number) {
-  if (typeof number !== 'number') {
-    return false;
-  }
-  if (number < 1 || number > 10000) {
-    return false;
-  }
-  return true;
+  return (
+    typeof number !== 'number' ||
+    (number < 1 || number > 10000)
+  ) ? false : true;
 }
 
 function strToArr(strIter) {
-  const arrIter = strIter.split('');
-  return arrIter;
+  return strIter.split('');
 }
 
 function findThreeSixNineElement(element, index, array) {
-  // if (["3", "6", "9"].includes(element)) array[index] = 1;
-  // else array[index] = 0;
   array[index] = ["3", "6", "9"].includes(element) ? 1 : 0;
 }
 
 function getSumArrIter(arrIter) {
-  return arrIter.reduce(function add(sum, currValue) {return sum + currValue}, 0);
+  return arrIter.reduce(
+    function add(sum, currValue) {
+      return sum + currValue
+    }, 0);
 }
 
 function threeSixNine(strIter) {
@@ -46,10 +43,3 @@ function problem3(number) {
 }
 
 module.exports = problem3;
-
-console.log(problem3(13));
-console.log(problem3(33));
-console.log(problem3(10000));
-console.log(problem3(1));
-console.log(problem3(5000));
-console.log(problem3(7500));
