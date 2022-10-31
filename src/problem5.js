efdd40d77c5de24d1533957e069a69d1c9c94de9
spsp8426 <spsp8426@naver.com>
2022-10-31 14:32:36 +0900

    5번문제 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+
+  const array = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let coin of array) {
+    answer.push(Math.floor(money / coin)); //동전의 개수
+    money -= coin * Math.floor(money / coin); // 남은 돈 계산
+  }
   return answer;
 }
+
 
 module.exports = problem5;
