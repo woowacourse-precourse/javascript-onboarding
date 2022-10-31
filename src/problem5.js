9eae4800ed523663decf3f271079d7b3f7395f14
somin <smhs12@naver.com>
2022-10-31 16:06:57 +0900

    feat: problem5 / 화폐 금액 전환 함수

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,26 @@
 function problem5(money) {
+  const answer = calculateChange(money);
   return answer;
+}
+
+function calculateChange(money) {
+  let current = money;
+  let divisor = 50000;
+  const change = [];
+  while (divisor >= 10) {
+    change.push(Math.floor(current / divisor));
+    current = current % divisor;
+    divisor = changeDivisor(divisor);
+  }
+  change.push(current);
+  return change;
+}
+
+function changeDivisor(divisor) {
+  const divisortoStr = divisor.toString();
+  if (divisortoStr[0] === "5") divisor /= 5;
+  else if (divisortoStr[0] === "1") divisor /= 2;
+  return divisor;
 }
 
 module.exports = problem5;
