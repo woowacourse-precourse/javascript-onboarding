092b6f135f501bf502cf1ce15974f37c4b5ac0eb
Narcoker <jykim9868@gmail.com>
2022-10-31 17:18:19 +0900

    feat: 1주차 1번 문제 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,30 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  function isCorrectPage(pages) {
+    return pages[0] + 1 === pages[1] ? true : false; 
+  }
+
+  function getScore(pages) {
+    let leftScore = pages[0].toString().split("").map(Number).reduce((sum, v) => {
+      sum[0] += v;
+      sum[1] *= v;
+      return sum;
+    }, [0, 1]);
+  
+    let rightScore = pages[0].toString().split("").map(Number).reduce((sum, v) => {
+      sum[0] += v;
+      sum[1] *= v;
+      return sum;
+    }, [0, 1]);
+    
+    return Math.max(...leftScore,...rightScore);
+  }
+
+  if (!isCorrectPage(pobi) || !isCorrectPage(crong)) return -1; 
+
+  let pobiScore = getScore(pobi);
+  let crongScore = getScore(crong);
+
+  return pobiScore > crongScore? 1 : (pobiScore < crongScore ? -1 : 0);
 }
 
 module.exports = problem1;
