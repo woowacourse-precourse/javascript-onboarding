79fd322fc2d39c590ab0faf0d244576fdd395343
zzinao <kjn0265@gmail.com>
2022-11-01 01:23:55 +0900

    feat: problem6 기능구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,32 @@
+function getPartsOfNickname(nickname) {
+  const parts = [...nickname]
+    .reduce((acc, _, idx) => [...acc, nickname.substr(idx, 2)], [])
+    .slice(0, -1);
+
+  return parts;
+}
+
+function getDuplication(forms) {
+  return forms.reduce((duplicates, form) => {
+    const [email, nickname] = form;
+
+    getPartsOfNickname(nickname).forEach((part) => {
+      duplicates[part] = [...(duplicates[part] || []), email];
+    });
+
+    return duplicates;
+  }, {});
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  const duplication = getDuplication(forms);
+
+  const result = Object.values(duplication)
+    .filter((duplicate) => duplicate.length > 1)
+    .flat()
+    .sort();
+
+  return [...new Set(result)];
 }
 
 module.exports = problem6;
