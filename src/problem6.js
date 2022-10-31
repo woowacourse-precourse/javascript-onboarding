b0f56db181f4ca9a5b8d8e98cd00619b2828551b
김상두 <puki4416@gmail.com>
2022-10-31 23:38:14 +0900

    problem6 : 기능 정의

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,8 @@
+// 연속되는 2자리 문자열 모두를 구하는 함수를 구현
+// 타겟 대상의 중복 여부를 체크하는 함수 구현
+// 결과 set을 정렬하는 함수 구현
+// 구현된 함수를 이용하여 forms 각각의 중복을 체크하는 problem6 함수 구현
+
 function problem6(forms) {
   var answer;
   return answer;
