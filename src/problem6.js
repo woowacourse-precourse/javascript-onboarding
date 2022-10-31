a2e612910fa792bbde6f8d93f02d766cece39c82
daal2quf <daal2quf@gmail.com>
2022-11-01 01:52:21 +0900

    reset:3글자이상 처리불가, map을 이용해 연속된문자 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,39 +1,21 @@
 function problem6(forms) {
   var answer;
-  let userEmail = [];
-  let userName = [];
-  let idx = new Set()
+  let map = new Map()
 
-  const seperate = (array) => {
-    userEmail.push(array[0])
-    userName.push(array[1])
-  }
+  for (let e of forms) {
+    const userNameParts = e[1].split('')
 
-  forms.forEach(element => {
-    seperate(element)
-  });
-
-  for (let i = 0; i < userName.length; i++) {
-    let ref1 = userName[i].slice(0, 2)
-    let ref2 = userName[i].slice(1)
-    for (let j = i + 1; j < userName.length; j++) {
-      let comp1 = userName[j].slice(0, 2)
-      let comp2 = userName[j].slice(1)
-      if (ref1 === comp1 || ref1 === comp2 || ref2 === comp1 || ref2 === comp2) {
-        idx.add(i)
-        idx.add(j)
+    for (let i = 0; i < userNameParts.length - 1; i++) {
+      const twoWordOFName = userNameParts[i] + userNameParts[i + 1]
+      if(map.has(twoWordOFName)) {
+        map.set(twoWordOFName, map.get(twoWordOFName) + 1)
+      } else {
+        map.set(twoWordOFName, 1)
       }
     }
   }
-  
-  let ArrIdx = [...idx]
-  let duplication = [];
-  for(let i = 0; i < ArrIdx.length; i++) {
-    duplication.push(userEmail[ArrIdx[i]])
-  }
-  duplication.sort((a, b) => a.localeCompare(b))
-  answer = duplication;
-  console.log(answer)
+
+console.log(map)
   return answer;
 }
 
