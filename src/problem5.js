1f74ea75dd5d8e125923092269420c3482b3b369
Buyoung Chung <sunarol@naver.com>
2022-10-31 18:12:21 +0900

    [문제5] 각 금액이 사용될 수 있는 수 계산

--- src/problem5.js
+++ src/problem5.js
@@ -2,6 +2,17 @@ function problem5(money) {
   var answer = new Array(9).fill(0);
   let bills = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let pointer = 0;
+
+  while (money > 0) {
+    if (money < bills[pointer]) {
+      pointer++;
+      continue;
+    }
+    let count = Math.floor(money / bills[pointer]);
+    money = money % bills[pointer];
+    answer[pointer] = count;
+    pointer++;
+  }
   return answer;
 }
 
