6242ae2da5fc3b2303a8d9d22d60cca606e4df2c
박겸영 <yong_drg@naver.com>
2022-11-01 01:59:46 +0900

    feat: 문제 5 오만 원권으로 변환되는 개수 구하기 - 기능 1: 변환할 금액 money가 오만 원권 몇 개로 변환되는지 구하여 배열에 담기

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const monetaryUnits = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [];
+
+  result.push(Math.floor(money / monetaryUnits[0]));
 }
 
 module.exports = problem5;
