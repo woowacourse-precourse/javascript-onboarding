939859871f7b8a9174a4d0549a5d9086c2d24395
kmin-jeong <desk535353@gmail.com>
2022-10-31 16:37:21 +0900

    feat: 문제 2번, 3번 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
+// "browoanoommnaon" -> "brown", "zyelleyz" -> "" /(\w+)\1/g
+// 중복이 되더라도 홀수일때는 남아있다 -> 2개가 만나면 사라진다
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+  let stack = [];
+
+  for (let value = 0; value <= cryptogram.length; value++) {
+    let current_value = cryptogram[value];
+    if (current_value === stack[stack.length - 1]) {
+      stack.pop();
+    } else {
+      stack.push(current_value);
+    }
+  }
+  answer = stack.join("");
   return answer;
 }
-
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
+// 3,6,9만 나오게 하고, -로 가면 안된다(자연수여야 해서) -> 정규식 사용
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let clap = 0; clap <= number; clap++) {
+    let game = ("" + clap).match(/[3|6|9]/g);
+    if (game !== null) {
+      answer += game.length;
+    }
+  }
   return answer;
 }
 
