abe0d8a0bc345dc47bb1250f36dbebf52a595dc8
NaveOWO <pianowell96@gmail.com>
2022-10-31 16:47:08 +0900

    [ feat ] problem1_페이지 번호를 더하는 함수 생성

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,22 @@
+//기능 구현 목록
+// - 페이지 번호를 더하는 함수 생성
+// - 페이지 번호를 곱하는 함수 생성
+// - 포비와 크롱의 최댓값을 구하는 함수 생성
+// - 최댓값을 비교해 승자를 판별하는 함수 생성
+// - 예외사항을 처리하는 Error 함수 생성
+
 function problem1(pobi, crong) {
   var answer;
+
+  function getAddResult(pageNum) {
+    pageNum = String(pageNum);
+    let result = 0;
+    for (let i = 0; i < pageNum.length; i++) {
+      result += Number(pageNum[i]);
+    }
+    return result;
+  }
+
   return answer;
 }
 
