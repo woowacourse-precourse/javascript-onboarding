9eb253b2fa2dc33f6253da827c2e2c2e5b216769
Jiho1996 <jhp9270@naver.com>
2022-10-31 19:19:02 +0900

    refactor : 계산의 if문로직 삼항연산자로 축약

--- src/problem7.js
+++ src/problem7.js
@@ -36,23 +36,15 @@ function problem7(user, friends, visitors) {
     if (key === user) continue;
     if (!friendsRelaion[key].includes(user)){
       inputedUsersFriends.forEach((friend) => {
-        if (friendsRelaion[key].includes(friend) && scoreBoard.hasOwnProperty(key)){
-          scoreBoard[key] += 10;
-        }
-        if (friendsRelaion[key].includes(friend) && !scoreBoard.hasOwnProperty(key)){
-          scoreBoard[key] = 10;
+        if (friendsRelaion[key].includes(friend)){
+          scoreBoard.hasOwnProperty(key) ? scoreBoard[key] += 10 : scoreBoard[key] = 10;
         }
       })
     }
   }
   visitors.forEach((visitor) => {
     if (!inputedUsersFriends.includes(visitor)){
-      if (scoreBoard.hasOwnProperty(visitor)){
-        scoreBoard[visitor] += 1;
-        }
-      if (!scoreBoard.hasOwnProperty(visitor)){
-        scoreBoard[visitor] = 1;
-        }
+      scoreBoard.hasOwnProperty(visitor) ? scoreBoard[visitor] += 1 : scoreBoard[visitor] = 1;
       }
     }
   )
