83181bfe22085706e68a3e5fbed8075f7e67f5cb
useon <yoosk4986@naver.com>
2022-10-31 23:30:34 +0900

    feat: [problem6] 쪼갠 글자별로 해당하는 닉네임의 메일을 담는다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,6 @@
 function problem6(forms) {
   const nicknameSet = new Set();
+  const duplicationCountMap = new Map();
   let answer = [];
 
   // 1. 모든 닉네임을 중복의 가장 작은 단위로 자른 결과를 중복없이 담는다.
@@ -8,6 +9,19 @@ function problem6(forms) {
       nicknameSet.add(forms[i][1].slice(j, j + 2));
     }
   }
+
+  // 2. 쪼갠 글자들(key)과 쪼갠 글자가 포함된 닉네임의 메일(value)로 이루어진 Map 객체를 만든다.
+  nicknameSet.forEach((item) => {
+    forms.map((e) => {
+      if (e[1].includes(item)) {
+        if (duplicationCountMap.has(item)) {
+          duplicationCountMap.set(item, [...duplicationCountMap.get(item), e[0]]);
+        } else {
+          duplicationCountMap.set(item, [e[0]]);
+        }
+      }
+    })
+  })
 
   return answer;
 }
