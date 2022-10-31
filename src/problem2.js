596291532dd003e8539b7b547da7abbb9a113a44
Suyeon <msjgr2801@gmail.com>
2022-10-31 19:04:49 +0900

    Init: Init problem2.js
    새로운 알고리즘 기능목록 구현 & 초기화

--- src/problem2.js
+++ src/problem2.js
@@ -1,50 +1,21 @@
-// 예외처리
-// 0. [x] 문자열이 비었거나 하나의 문자만 있는 경우
+/* 예외처리
+ 0. [x] 문자열이 비었거나 하나의 문자만 있는 경우
 
-// 로직
-// 1. 문자열 제거 로직
-//  1.1 [x] 문자열을 앞에서부터 (중복되지 않는 문자열만) 하나씩 answer에 더한다
+  로직
+  1. [ ] stack에 문자를 하나씩 넣는다
+  2. [ ] cryptogram의 글자를 하나씩 stack에 넣는다
+  3. [ ] stack의 마지막 요소가 cryptogram과 일치하면 stack의 요소를 빼낸다
+*/
 
-// 2. 1번 로직 반복 기준
-//  2.1 [x] flag로 연속으로 같은 문자열이 있는지 판별 
-//  2.2 [x] 연속되는 문자열이 있으면 true, 아니면 false
-
-const removeCharcater = (res, cryptogram) => {
-  for (let i=0; i<cryptogram.length; i++) {
-    if (cryptogram[i] === cryptogram[i + 1]) {
-      i += 2;
-    } else {
-      res += cryptogram[i];
-    }
-  }
-
-  return res;
-}
-
-const checkOverlap = (cryptogram) => {
-  if (cryptogram.length === 0 || cryptogram.length === 1) {
-    return false;
-  }
-
-  for (let i=0; i<cryptogram.length; i++) {
-    if (cryptogram[i] === cryptogram[i + 1]) return true;
-  }
-
-  return false;
-}
-
+/**
+ * 
+ * @param {*} cryptogram 
+ * @returns 
+ */
 function problem2(cryptogram) {
-  let answer = '';
+  let answer = [];
 
-  if (cryptogram.length === 0) return answer;
-  if (cryptogram.length === 1) return cryptogram;
-
-  let flag = true;
-
-  while (flag) {
-    answer = removeCharcater(answer, cryptogram);
-    flag = checkOverlap(answer);
-  }
+  if (cryptogram.length === 0 || cryptogram.length === 1) return cryptogram;
 
   return answer;
 }
