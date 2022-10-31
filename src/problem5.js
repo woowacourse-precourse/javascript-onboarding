f911420a803722e0988e2b5c2c46ddf7f5762cea
chaeeun-seo <seochaeeun961126@gmail.com>
2022-11-01 03:06:57 +0900

    [problem5] solution for problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,18 @@
 function problem5(money) {
-  var answer;
+  trans = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  for (i=0; i < trans.length; i++){
+    if (money >= trans[i]){
+      answer[i] += parseInt(money / trans[i]);
+      money -= (trans[i] * answer[i]);
+    }
+    
+  }        
   return answer;
 }
+
+console.log(problem5(50237));
+console.log(problem5(15000));
 
 module.exports = problem5;
