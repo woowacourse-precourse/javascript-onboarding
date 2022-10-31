59b058d8411cbc4750e0a6a45bb8216f8bb1de1f
areumsheep <areumsheep@gmail.com>
2022-11-01 07:25:14 +0900

    add: 문제2 답안 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function problem2(cryptogram = '') {
+  const result = [];
+  const letterCounts = {};
+  const splitedLetters = cryptogram.split('');
+  splitedLetters.forEach((letter) => {
+    if (letterCounts[letter]) letterCounts[letter]++;
+    else letterCounts[letter] = 1;
+  });
+
+  for (const [letter, count] of Object.entries(letterCounts)) {
+    if (count % 2 === 1) result.push(letter);
+  }
+
+  return result.join('');
 }
 
 module.exports = problem2;
