dea015e3432d6cf2f1ef93f4b8a20125f2301f38
choieastsea <choieastsea@gmail.com>
2022-11-01 07:34:08 +0900

    7) Making Score map
    친구가 아닌 사람 들 중 추천이 될만한 사람들의 점수를 저장하도록 함
    ex : { 'andole' => 20, 'jun' => 20, 'bedi' => 3 }

--- src/problem7.js
+++ src/problem7.js
@@ -15,7 +15,34 @@ function problem7(user, friends, visitors) {
       ? [...relation_obj[person1], person0]
       : [person0];
   }
-  console.log(relation_obj);
+  let user_friends = relation_obj[user]; // user의 친구들 list
+  let score = new Map(); // {이름 : 점수, ...}로 구성될 map 객체
+
+  for (user_friend of user_friends) {
+    // (친구의 친구들 - 나의 친구들)에게 10점씩
+    for (friends_friend of relation_obj[user_friend]) {
+      if (friends_friend !== user && !user_friends.includes(friends_friend)) {
+        //본인이거나 친구인 경우 제외
+        if (score.has(friends_friend)) {
+          score.set(friends_friend, score.get(friends_friend) + 10);
+        } else {
+          score.set(friends_friend, 10);
+        }
+      }
+    }
+  }
+  // visitors들에게 1점씩
+  for (visitor of visitors) {
+    if (!user_friends.includes(visitor)) {
+      //친구 제외
+      if (score.has(visitor)) {
+        score.set(visitor, score.get(visitor) + 1);
+      } else {
+        score.set(visitor, 1);
+      }
+    }
+  }
+  console.log(score);
   return answer;
 }
 
