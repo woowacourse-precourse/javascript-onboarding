1f32ddcc5b03c37fe4d31e55f274968554f39e73
knh6269 <knh6269@gmail.com>
2022-10-31 22:49:00 +0900

    feat : 미션6) Map에 string이 이미 있는지 확인해주는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,15 @@
-function problem6(forms) {
-  var answer;
-  return answer;
+//맵에 string이 이미 있는지 없는지 확인하는 함수
+const checkMap = (index,string,maps) => {
+  //맵에 이미 string이 있다면 index를 추가해준다.
+  if (maps.has(string)) {
+    const valueOfMap = maps.get(string);
+    maps.set(string, [...valueOfMap, index]);
+  }
+  if (!maps.has(string)) {
+    maps.set(string, [index]);
+  }
+};
+
 }
 
 module.exports = problem6;
