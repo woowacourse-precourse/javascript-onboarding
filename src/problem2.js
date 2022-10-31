730f1384bf468d15eeff1b9238aef16e2e5aeb92
d-charlie-kim <dokkim@student.42seoul.kr>
2022-10-31 21:08:53 +0900

    feat: <problem2> 연속하는 문자 중복 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,37 @@
+function howManyDuplicate(index, array) {
+  let base = array[index];
+  let count = 0;
+
+  for (i = index; array[i] == base && i < array.length; i++)
+    count++;
+  return count;
+}
+
+function deleteDuplicateAlpha(array) {
+  if (array.length == 0)
+    return array;
+  
+  let newArray = "";
+  for (let i = 0; i < array.length;) {
+    let count = howManyDuplicate(i, array);
+    if (count == 1)
+      newArray += array[i];
+    i += count;
+  }
+  return newArray;
+}
+
 function problem2(cryptogram) {
-  var answer;
+  let modifiedCryptogram = cryptogram;
+  let check;
+
+  while (1) {
+    check = modifiedCryptogram;
+    modifiedCryptogram = deleteDuplicateAlpha(modifiedCryptogram);
+    if (check == modifiedCryptogram)
+      break;
+  }
+  let answer = modifiedCryptogram;
   return answer;
 }
 
