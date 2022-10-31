6ba1f707192008adc0f3f16583d8b398e58dd392
Wang HoEun <hoeunwang@gmail.com>
2022-10-31 13:17:24 +0900

    Feat: [문제번호6] 닉네임을 두글자씩 나눈뒤 map을 형성

--- src/problem6.js
+++ src/problem6.js
@@ -15,4 +15,21 @@ const isKorea = (nickname) => {
   return Boolean(nickname.match(/^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]*$/));
 };
 
+const subNickNameTwoElement = (nickname) => {
+  const lettersArr = [];
+  [...nickname].forEach((char, idx) => {
+    if (idx === nickname.length - 1) return;
+    const letter = char + nickname[idx + 1];
+    lettersArr.push(letter);
+  });
+
+  return lettersArr;
+};
+
+const setNickNameMap = (nickNameMap, lettersArr) => {
+  lettersArr.forEach((letter) =>
+    nickNameMap.set(letter, (nickNameMap.get(letter) || 0) + 1)
+  );
+};
+
 module.exports = problem6;
