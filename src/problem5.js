ce984b24cb514a54ab897176ab61d55bee3cb4a8
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 09:27:05 +0900

    구현할 기능 목록 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
-function problem5(money) {
-  var answer;
-  return answer;
-}
+/*
+  구현할 기능 목록
+  [ ] 큰 금액의 화폐단위부터 차례대로 확인하여, 각 화폐가 필요한 개수를 반환하는 기능
+*/
+function problem5(money) {}
 
 module.exports = problem5;
