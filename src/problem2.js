41c3efa0600b385bdd00b7154a23a57ae5539330
unqeye <salyuan@naver.com>
2022-11-01 00:18:31 +0900

    feat: 문제2 연속하는 중복 문자들을 삭제한 결과 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,16 @@
+function decrypt(cryptogram) {
+  const decrypted = [];
+
+  [...cryptogram].forEach((char, index) => {
+    if (cryptogram[index - 1] === char || cryptogram[index + 1] === char) {
+      return;
+    }
+    decrypted.push(char);
+  });
+
+  return decrypted.join("");
+}
+
 function problem2(cryptogram) {
   var answer;
   return answer;
