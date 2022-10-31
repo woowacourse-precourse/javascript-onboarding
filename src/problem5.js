138d70afaf0d8d2633b674d224ddeaca38f96c4c
sh981013s <sh981013s@gmail.com>
2022-10-31 23:30:32 +0900

    feat: [5] 화폐 사용 가능 확인 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -7,6 +7,16 @@ const isMoneyLeftZero = (moneyLeft) => {
   return !moneyLeft;
 };
 
+/**
+ * 처리해야 할 액수가 현제 포인터로 지정된 화폐단위로 해결할 수 있는지 확인한다.
+ * @param {number} moneyLeft - 처리해야 할 액수
+ * @param {number} moneyKind - 사용 할 화폐단위
+ * @return {boolean}
+ */
+const isPossibleToUseSelectedMoneyKind = (moneyLeft, moneyKind) => {
+  return moneyLeft >= moneyKind;
+};
+
 function problem5(money) {
   var answer;
   return answer;
