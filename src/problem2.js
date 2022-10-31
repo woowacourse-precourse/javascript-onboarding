0201f5ba80f31623f4d64a3d1cc4c8023f2787d6
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-31 20:18:29 +0900

    문제2) feat: 잘라낼 구간의 인덱스를 받아서 문자열을 자르는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
   var answer;
+
   return answer;
 }
+
+const sliceDuplication = (curCryptogram, duplicationIdx) => {
+  const firstIndex = duplicationIdx[0];
+  const lastIndex = duplicationIdx[1];
+
+  return (
+    curCryptogram.substring(0, firstIndex) + curCryptogram.substring(lastIndex)
+  );
+};
 
 module.exports = problem2;
