8315962595bba67237e625ec41f829ad0edbf242
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-10-31 20:30:02 +0900

    문제2) feat: 중복되는 구간의 인덱스를 반환하는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -4,13 +4,35 @@ function problem2(cryptogram) {
   return answer;
 }
 
-const sliceDuplication = (curCryptogram, duplicationIdx) => {
-  const firstIndex = duplicationIdx[0];
-  const lastIndex = duplicationIdx[1];
+const sliceDuplication = (curCryptogram, duplicationArr) => {
+  const firstIndex = duplicationArr[0];
+  const lastIndex = duplicationArr[1];
 
   return (
     curCryptogram.substring(0, firstIndex) + curCryptogram.substring(lastIndex)
   );
+};
+
+const duplicationSection = (curCryptogram) => {
+  const duplicationArr = [];
+  let duplicationCount = 1;
+  let curChar = curCryptogram[0];
+
+  for (let i = 1; i < curCryptogram.length; i++) {
+    if (curCryptogram[i] === curChar) {
+      duplicationCount += 1;
+      continue;
+    }
+
+    if (duplicationCount > 1) {
+      duplicationArr.push([i - duplicationCount, i]);
+      duplicationCount = 1;
+    }
+
+    curChar = curCryptogram[i];
+  }
+
+  return duplicationArr;
 };
 
 module.exports = problem2;
