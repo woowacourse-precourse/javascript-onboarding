983c7ee76e3a5057d2e733106eb92bb006ba5a95
Heeseung <gmltmd0816@naver.com>
2022-10-31 22:00:43 +0900

    Solved: Problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    if (String(i).match(/3|6|9/g)) {
+      console.log(String(i));
+      answer += String(i).match(/3|6|9/g).length;
+    }
+  }
   return answer;
 }
 
