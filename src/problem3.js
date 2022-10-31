710cdb11abf961e2468ea12eaec9668081d53a59
Halim Lim <81246338+gkfla668@users.noreply.github.com>
2022-10-31 15:25:38 +0900

    refactor: [problem3] - 문자열을 배열로 변환하는 기능 분리

--- src/problem3.js
+++ src/problem3.js
@@ -2,16 +2,20 @@ function problem3(number) {
   var answer = 0;
 
   for (var i = 1; i <= number; i++) {
-    answer += getCount(i, 3) + getCount(i, 9) + getCount(i, 9);
+    answer += getCount(i, 3) + getCount(i, 6) + getCount(i, 9);
   }
 
   return answer;
 }
 
 function getCount(num, target) {
-  var arr = Array.from(String(num));
+  var arr = convertStringToArray(String(num));
 
   return arr.filter((e) => String(target) === e).length;
+}
+
+function convertStringToArray(str) {
+  return Array.from(str);
 }
 
 module.exports = problem3;
