c47127357e5923bd08a0a8b6d649124b0cf6550d
DongWooKim97 <nanaukim@naver.com>
2022-10-31 13:47:08 +0900

    feat(P7): 예외처리 함수 초안 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,31 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+
+  if(!checkInputValid()) {
+    return 'error'
+  }
+  
+}
+
+function checkInputValid() {
+  if(!userLengthInRange()) return false;
+  if(!friendsLengthInRange()) return false;
+  if(!idLengthInRange()) return false;
+  if(!visitorsLengthInRange()) return false;
+  if(!userIdValueCase()) return false;
+  if(!duplicateFriendship()) return false;
+  return true;
 }
 
 module.exports = problem7;
+
+console.log(
+  problem7("mrko",
+    [
+      ["donut", "andole"],
+      ["donut", "jun"],
+      ["donut", "mrko"],
+      ["shakevan", "andole"],
+      ["shakevan", "jun"],
+      ["shakevan", "mrko"]
+    ],
+    ["bedi", "bedi", "donut", "bedi", "shakevan"]));
