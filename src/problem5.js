7e3d30633d04d04b61917a22d5ce7a38695eaf7a
Serin-Kim <serin9864@gmail.com>
2022-11-01 01:15:10 +0900

    refactor: [PROBLEM5] 변수명 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,9 +1,9 @@
 const MONETARY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  const answer = MONETARY_UNIT.map((monetaryUnit) => {
-    const count = Math.floor(money / monetaryUnit); // TODO: 변수명 변경
-    money = money % monetaryUnit;
+  const answer = MONETARY_UNIT.map((unit) => {
+    const count = Math.floor(money / unit);
+    money = money % unit;
     return count;
   });
   return answer;
