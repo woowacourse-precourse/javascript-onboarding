178a0e0be759107ba1468da33f7a3ed722528644
SoMin <thals0107@naver.com>
2022-10-31 12:30:33 +0900

    feat: 문제 6 - map 항목 중 중복되는 문자열은 점수 + 1

--- src/problem6.js
+++ src/problem6.js
@@ -5,6 +5,11 @@ function problem6(forms) {
     for (let j = 0; j < forms[i][1].length - 1; j++) {
       const str = forms[i][1].substr(j, 2);
       // console.log(str);
+      if (!map.has(str)) {
+        map.set(str, 1);
+      } else {
+        map.set(str, map.get(str) + 1);
+      }
     }
   }
   return answer;
