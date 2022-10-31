119e43b8887520387493a17b58d0b3db65b71709
OrangeVinyl <hap_hazard@naver.com>
2022-10-31 22:47:02 +0900

    Problem4) 알파벳 변환공식 및 대소문자 구분

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  let reverse, abstemp, ascii = 0;
+  for (var i = 0; i<word.length; i++) {
+    if(word[i] === word[i].toUpperCase()){
+      ascii = 65;
+    }
+    else if (word[i]=== word[i].toLowerCase()){
+      ascii = 97;
+    }
+    abstemp = Math.abs(ascii - word.charCodeAt(i));
+    reverse = (ascii + 25) - abstemp;
+    answer = answer + String.fromCharCode(reverse);
+  }
   return answer;
 }
 
