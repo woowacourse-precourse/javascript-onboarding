cb2d468c487bffd49474b2e0b1a88a0d23adba20
kk2522 <kk2522@naver.com>
2022-11-01 00:22:37 +0900

    1주차

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  var stack = []
+
+
+  for(let i of cryptogram){
+    if (stack.length == 0)
+    {stack.push(i)}
+    else if (stack.length != 0)
+    {
+      
+      if(stack[stack.length-1] == i){
+        stack.pop()
+      }
+      else if(stack[stack.length-1]!= i){
+        stack.push(i)
+      }
+    }
+  }
+
+  return stack.join("")
 }
 
 module.exports = problem2;
+console.log(problem2("browoanoommnaon"))
