9322e8e58c3f448f0e1331a9bceb53fadfcaa9e8
durumi99 <kms990615@naver.com>
2022-10-31 19:45:16 +0900

    [problem 2] 연속하는 중복 문자 제거 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,18 @@
 function problem2(cryptogram) {
   var answer;
   if(cryptogram.length == 1)
-    return cryptogram;
+    return answer;
+  
+  for(let i = 0 ; i < cryptogram.length-1; i++){
+    if(cryptogram[i] == cryptogram[i+1]){ // 중복 문자가 연속되는 경우      
+      tmp = i+1;
+      while(cryptogram[i] == cryptogram[tmp++]);
+      // 문자열의 인덱스 i~tmp 까지 제거    
+      cryptogram = cryptogram.slice(0,i) + cryptogram.slice(tmp-1);
+      i--;
+    }
+  }
+  answer = cryptogram
   return answer;
 }
 
