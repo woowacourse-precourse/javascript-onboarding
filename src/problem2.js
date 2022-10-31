97145c1cf20d17a4672af3b85c58c193df443e25
Jeonjunwoo <tira618@naver.com>
2022-10-31 16:22:17 +0900

    cryptogram을 알파벳으로 분리하여 alphaItem 배열에 저장

--- src/problem2.js
+++ src/problem2.js
@@ -2,5 +2,11 @@ function problem2(cryptogram) {
   var answer;
   return answer;
 }
+function cutString(cryptogram) { 
+  const alphaItem = [];
+  for(i = 0; i < cryptogram.length; i++) {
+    alphaItem.push(cryptogram.substring(i, i+1));
+  } 
+}
 
 module.exports = problem2;
