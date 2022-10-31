0b9a268c05629a106cf8570a953fa2ee3a2bd2f9
chiman <rlaclghks123@naver.com>
2022-10-31 19:48:48 +0900

    feat: problem7- findFriends 함수 생성
    친구를 찾아주는 함수

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,18 @@
+function findFriends(friends, user) {
+  const friendsArr = [];
+  for (const friend of friends) {
+    if (friend[0] === user) {
+      friendsArr.push(friend[1]);
+    } else if (friend[1] === user) {
+      friendsArr.push(friend[0]);
+    }
+  }
+  return friendsArr;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  const friendsArr = findFriends(friends, user);
   return answer;
 }
 
