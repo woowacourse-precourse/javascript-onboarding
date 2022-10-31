bdded746dda697acde469820fcf29c9ffc18c4e1
wzrabbit <curious.wzrabbit@gmail.com>
2022-10-31 14:57:21 +0900

    feat: Problem 3 - 전체 기능 구현하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
-function problem3(number) {
-  var answer;
-  return answer;
+const problem3 = number => {
+  let clapCount = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const currentNumber = i.toString();
+    clapCount += (currentNumber.match(/3|6|9/g) || []).length;
+  }
+
+  return clapCount;
 }
 
 module.exports = problem3;
