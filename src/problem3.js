76f3a33e350b0fe395ede7e5e2fad7f7a8e38354
chasj0326@naver.com <62418379+chasj0326@users.noreply.github.com>
2022-10-31 21:32:32 +0900

    refactor: problem3 조건문 간결화

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,12 @@
 //정규식 사용, 문자열로 형변환 후 3,6,9 갯수만큼 +1
 //위 task 1~number 까지 반복
 function problem3(number){
-  var answer=0;
+  let answer=0;
   let numStr;
   const regex = new RegExp('[369]','g');
   for (let i=1; i<=number; i++){
-      numStr = String(i).match(regex);
-      if(numStr){
-          answer+=numStr.length;
-      }
+    numStr = String(i).match(regex);
+    answer += (numStr && numStr.length)
   }
   return answer;
 }
