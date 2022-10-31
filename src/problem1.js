45fd7e622756febd9cdfb1f46da7d64d749bdfcf
“somm12” <sominan8744@gmail.com>
2022-10-31 17:48:15 +0900

    page의 점수를 알아내는 함수 eachPageScore 정의

--- src/problem1.js
+++ src/problem1.js
@@ -11,6 +11,19 @@ function checkException(arr) {
   return diff && range;
 }
 
+function eachPageScore(page) {
+  let plus = 0;
+  let multiply = 1;
+  while (page > 0) {
+    digit = page % 10;
+    plus += digit;
+    multiply *= digit;
+    page = Math.floor(page / 10);
+  }
+  score = Math.max(plus, multiply);
+  return score;
+}
+
 function problem1(pobi, crong) {
   var answer;
   //예외처리
@@ -20,6 +33,16 @@ function problem1(pobi, crong) {
     answer = -1;
     return answer;
   }
+
+  //pobi의 가장 큰 수
+  const pLeft = eachPageScore(pobi[0]);
+  const pRight = eachPageScore(pobi[1]);
+  const pScore = Math.max(pLeft, pRight);
+  //crong의 가장 큰 수
+  const cLeft = eachPageScore(crong[0]);
+  const cRight = eachPageScore(crong[1]);
+  const cScore = Math.max(cLeft, cRight);
+
   return answer;
 }
 
