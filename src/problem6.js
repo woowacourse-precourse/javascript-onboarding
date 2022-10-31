f65e093c59561029ca45d57afca1975842f995c9
parkjikuk <paxkk@naver.com>
2022-11-01 06:37:15 +0900

    feat : 두개의 닉네임을 두 글자씩 비교하여 중복을 찾는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  
 }
 
+function duplicateNick(nickName1, nickName2) {
+  for (let i = 0; i < nickName1.length - 1; i++) {
+    if(nickName2.includes(nickName1.slice(i, i + 2))) {
+      return true;
+    }
+  }
+  return false;
+}
+
+
+
 module.exports = problem6;
+
+
