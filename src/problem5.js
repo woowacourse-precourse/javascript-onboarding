097ec529d37198a6d9b18b8bb4879d54ac4b7aab
sujinjwa <91577550+sujinjwa@users.noreply.github.com>
2022-10-31 15:30:37 +0900

    refactor(problem5): 기능별 함수 분리하여 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,16 +2,24 @@ function problem5(money) {
   let result = [];
   const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+  const push_result = (cnt) => {
+    result.push(cnt);
+  };
+
+  const isLastIndex = (index) => {
+    return index == 8;
+  };
+
   const find_cnt_of_cash = (money, index) => {
-    if (index == 8) {
-      result.push(money);
+    if (isLastIndex(index)) {
+      push_result(money);
       return;
     }
     if (money < cash[index]) {
-      result.push(0);
+      push_result(0);
       return find_cnt_of_cash(money, index + 1);
     }
-    result.push(parseInt(money / cash[index]));
+    push_result(parseInt(money / cash[index]));
     return find_cnt_of_cash(money % cash[index], index + 1);
   };
 
