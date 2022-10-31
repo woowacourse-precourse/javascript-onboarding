9cc400c4a3c121ed922fa02635a364547217f343
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-10-31 18:15:14 +0900

    Feat: 4번- 알파벳을 청개구리 사전대로 변환하는 메서드 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,28 @@
 // 알파벳인지 확인하는 함수
 function checkAlphabet(str) {
   const regExp = /[a-zA-Z]/g;
-  if(regExp.test(str)){
+  if (regExp.test(str)) {
     return true;
-  } else{
+  } else {
     return false;
   }
+}
+
+// 청개구리 사전대로 변환하는 함수
+function reverseEng(n) {
+  // 알파벳 a-z까지 출력
+  const arrEng = Array.from({ length: 26 }, (value, index) =>
+    String.fromCharCode(index + 97)
+  );
+  const arrEngRev = [...arrEng].reverse();
+
+  const arrIndex = arrEng.forEach((value, index) => {
+    if (n === value) {
+      return index;
+    }
+  });
+
+  return arrEng[index];
 }
 
 // main 함수
