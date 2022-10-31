11801d00e882b537a62b662de5ca4ce349ed9d20
ctd <qpflapffhs76@gmail.com>
2022-10-31 11:59:16 +0900

    feat: problem7 유저의 친구를 찾는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,18 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function findFriendsByUser(user, friends) {
+  return friends
+    .map((friend) => {
+      const [A, B] = friend;
+      if (A === user) {
+        return B;
+      }
+      if (B === user) {
+        return A;
+      }
+    })
+    .filter((element) => element);
+}
+
 module.exports = problem7;
