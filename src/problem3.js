d0b5e617f9a110afb75eab40d6ac054192d243e2
Yebin Kim <kyb4312@gmail.com>
2022-10-31 22:50:00 +0900

    (problem 3): List feature list as annotation

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,9 @@
+/*
+ * <기능 목록>
+ * 1. 숫자를 문자열로 변환하고 문자열에 3,6,9가 들어가는 (손뼉치는) 횟수를 반환하는 countClap() 함수.
+ * 2. 1부터 주어진 숫자 number까지 손뼉치는 총 횟수를 반환하는 countTotalClap() 함수.
+ */
+
 function problem3(number) {
   var answer;
   return answer;
