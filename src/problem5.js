853dffa0cf5f398749bc3bd67b1b4f5d87edb14d
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 05:38:10 +0900

    FEAT : problem5 - 필요한 상수 선언

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  // 화폐를 상수로 선언
+  const MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  // 반환할 배열 생성
+  let answer = [];
 }
 
 module.exports = problem5;
