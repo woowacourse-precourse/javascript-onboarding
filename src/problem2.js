ea077ba4cfc65cc58a0004784cc5da0fc0043ba4
loco9939 <kls9939@naver.com>
2022-10-31 10:37:43 +0900

    fix: add ; & a line space
    - 문의 마침을 의미하는 세미콜론 추가
    - 가독성을 위해 한줄 띄어쓰기 했습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,7 @@
 function problem2(cryptogram) {
-  if (cryptogram !== cryptogram.toLowerCase()) throw new TypeError('매개변수는 소문자만 입력할 수 있습니다.')
-  if (!cryptogram.length || cryptogram.length > 1000) throw new RangeError('매개변수는 1자 이상 1000자 이하만 입력할 수 있습니다.')
+  if (cryptogram !== cryptogram.toLowerCase()) throw new TypeError('매개변수는 소문자만 입력할 수 있습니다.');
+  if (!cryptogram.length || cryptogram.length > 1000) throw new RangeError('매개변수는 1자 이상 1000자 이하만 입력할 수 있습니다.');
+
   let strArr = [...cryptogram];
   let duplicatedStrPosition = [];
   let hasDuplicatedStr = true;
