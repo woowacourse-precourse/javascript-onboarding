9ddd2fce30c8fe85073d99dc01cb01cc6a257f38
c0c0pang <coc1789@naver.com>
2022-10-31 18:07:50 +0900

    문제 7 기능목록 작성 및 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,10 @@
+/*
+  ["andole", "jun", "bedi"]
+  사용자와 친구 dount, shakevan
+  사용자의 친구와 함께 아는 친구 andole - 20, jun - 20
+  사용자의 타임라인 방문한 사람 bedi - 3
+  사용자와 친구인 사람은 목록에서 제외 
+*/
 function problem7(user, friends, visitors) {
   const FRIENDS_LENGTH = 2;
   const UFV_LIST = {
@@ -9,78 +16,71 @@ function problem7(user, friends, visitors) {
   for (let i = 0; i < UFV_LIST.friends.length; i++) {
     for (let j = 0; j < FRIENDS_LENGTH; j++) {
       if (UFV_LIST.user === UFV_LIST.friends[i][j]) {
-        FRIENDS_LIST.push(UFV_LIST.friends[i][1-j]);
-        UFV_LIST.friends.splice(i,1);
+        FRIENDS_LIST.push(UFV_LIST.friends[i][1 - j]);
+        UFV_LIST.friends.splice(i, 1);
       }
     }
   }
-
   const RECOMMEND_LIST = [];
-  const USER_FRIENDS = arrconvert(FRIENDS_LIST);
-  for(let i=0;i<UFV_LIST.friends.length;i++){
+  for (let i = 0; i < UFV_LIST.friends.length; i++) {
+
+    if (((FRIENDS_LIST.indexOf(UFV_LIST.friends[i][0]) >= 0) && (FRIENDS_LIST.indexOf(UFV_LIST.friends[i][1]) >= 0))) {
+      continue;
+    }
     for (let j = 0; j < FRIENDS_LENGTH; j++) {
-      if(USER_FRIENDS.indexOf(UFV_LIST.friends[i][j])>=0){
-        // RECOMMEND_LIST.push(UFV_LIST.friends[i][1-j]);
-        if(UFV_LIST.friends[i][1-j] in RECOMMEND_LIST){
-          RECOMMEND_LIST[UFV_LIST.friends[i][1-j]]+=10;
+      if (FRIENDS_LIST.indexOf(UFV_LIST.friends[i][j]) >= 0) {
+        if (UFV_LIST.friends[i][1 - j] in RECOMMEND_LIST) {
+          RECOMMEND_LIST[UFV_LIST.friends[i][1 - j]] += 10;
         }
-        else{
-          RECOMMEND_LIST[UFV_LIST.friends[i][1-j]]=10;
+        else {
+          RECOMMEND_LIST[UFV_LIST.friends[i][1 - j]] = 10;
         }
       };
-    };
-  };
-  // let LAST_LIST = Object.entries(arrtoobj(RECOMMEND_LIST));
-
-  for(let i=0;i<UFV_LIST.visitors.length;i++){
-    // VISITORS_LIST.push(UFV_LIST.visitors[i]);
-    if(FRIENDS_LIST.indexOf(UFV_LIST.visitors[i])>-1){
-      UFV_LIST.visitors.splice(i,1);
     }
   };
 
-  for(let i=0;i<UFV_LIST.visitors.length;i++){
-    // VISITORS_LIST.push(UFV_LIST.visitors[i]);
-    if(UFV_LIST.visitors[i] in RECOMMEND_LIST){
-      RECOMMEND_LIST[UFV_LIST.visitors[i]]+=1;
+  for (let i = 0; i < UFV_LIST.visitors.length; i++) {
+    if (!(FRIENDS_LIST.indexOf(UFV_LIST.visitors[i]) === -1))
+      continue;
+    if (UFV_LIST.visitors[i] in RECOMMEND_LIST) {
+      RECOMMEND_LIST[UFV_LIST.visitors[i]] += 1;
     }
-    else{
-      RECOMMEND_LIST[UFV_LIST.visitors[i]]=1;
+    else {
+      RECOMMEND_LIST[UFV_LIST.visitors[i]] = 1;
     }
   };
-
-  // const a = Object.entries(RECOMMEND_LIST).map()
-  let LAST_LIST = Object.entries(RECOMMEND_LIST).sort((a,b)=>{
-    if(a[1]===b[1]){
-      return a[0]-b[0];
+  let LAST_LIST = Object.entries(RECOMMEND_LIST).sort((a, b) => {
+    if (a[1] === b[1]) {
+      if (a[0] > b[0]) {
+        return 1;
+      }
+      if (a[0] < b[0]) {
+        return -1;
+      }
     }
-    else{
-      return b[1] - a[1];
+    if (a[1] > b[1]) {
+      return -1;
+    }
+    if (a[1] < b[1]) {
+      return 1;
     }
   });
-
-  return LAST_LIST.map((e)=>{
-    return e[0]
-  });
+  Arr_Length_Five(LAST_LIST);
+  return Arr_Length_Five(LAST_LIST);
 }
 
-function arrconvert(list) {
-  return list.reduce((acc, row) => {
-      return acc.concat(row);
+function Arr_Length_Five(arr) {
+  let New_Arr = arr.map((e) => {
+    return e[0];
   });
-};
-
-function arrtoobj(list){
-  return list.reduce((acc,row)=>{
-    const [user,score] = row;
-    acc[user]  = [...(acc[user]||[]),score];
-    return acc
-  },[])
+  let Last_Arr = [];
+  for (let i = 0; i < New_Arr.length; i++) {
+    if (i === 5) {
+      break;
+    }
+    Last_Arr.push(New_Arr[i]);
+  }
+  return Last_Arr;
 }
-//["andole", "jun", "bedi"]
-// 사용자와 친구 dount, shakevan
-// 사용자의 친구와 함께 아는 친구 andole - 20, jun - 20
-// 사용자의 타임라인 방문한 사람 bedi - 3
-// 사용자와 친구인 사람은 목록에서 제외 
 
 module.exports = problem7;
