55670834eb9a422a5aedc09a7953aeb000cbeed9
choieastsea <choieastsea@gmail.com>
2022-11-01 07:18:03 +0900

    5) Refactoring

--- src/problem5.js
+++ src/problem5.js
@@ -3,18 +3,24 @@ function problem5(money) {
   let iter = 1;
   let bill = 50000; //화폐 단위
   while (bill >= 1) {
-    answer.push(parseInt(money / bill));    // bill*몫 만큼 담을 수 있음
-    money %= bill;  // money에 남은 돈 저장
-    if (bill == 10) {   //화폐단위 : 50000,10000,5000,1000,500,100,50,10,1 으로 가도록 루프 유도
-      bill /= 10;
-    } else if (iter % 2 == 1) {
-      bill /= 5;
-    } else {
-      bill /= 2;
-    }
+    answer.push(parseInt(money / bill)); // bill*몫 만큼 담을 수 있음
+    money %= bill; // money에 남은 돈 저장
+    bill = setNextBill(bill, iter);
     iter += 1;
   }
   return answer;
+}
+
+function setNextBill(bill, iter) {
+  if (bill == 10) {
+    //화폐단위 : 50000,10000,5000,1000,500,100,50,10,1 으로 가도록 루프 유도
+    bill /= 10;
+  } else if (iter % 2 == 1) {
+    bill /= 5;
+  } else {
+    bill /= 2;
+  }
+  return bill;
 }
 
 module.exports = problem5;
