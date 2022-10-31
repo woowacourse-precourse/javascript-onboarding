ee85ae47a74ef37e59eabe43d0564e0261185f08
연 <mogoo22@naver.com>
2022-10-31 15:44:17 +0900

    refactor(문제4): 정규식으로 영문자 여부를 확인하고 toUpperCase 메서드로 대소문자를 구분한다.

--- src/problem4.js
+++ src/problem4.js
@@ -1,40 +1,29 @@
-const [MIN_UPPER_ASCII, MAX_UPPER_ASCII] = [65, 90];
-const [MIN_LOWER_ASCII, MAX_LOWER_ASCII] = [97, 122];
-const isUpperCase = (string) => string.charCodeAt(0) >= MIN_UPPER_ASCII && string.charCodeAt(0) <= MAX_UPPER_ASCII;
-const isLowerCase = (string) => string.charCodeAt(0) >= MIN_LOWER_ASCII && string.charCodeAt(0) <= MAX_LOWER_ASCII;
-
-const defineStringType = (string) => {
-  if (isUpperCase(string)) return "upperCase";
-  if (isLowerCase(string)) return "lowerCase";
-  return "noAlphabet";
-};
-
-const defineASCIIRange = (stringType) => {
-  const MIN_ASCII = stringType === "upperCase" ? MIN_UPPER_ASCII : MIN_LOWER_ASCII;
-  const MAX_ASCII = stringType === "upperCase" ? MAX_UPPER_ASCII : MAX_LOWER_ASCII;
+const defineASCIIRange = (string) => {
+  const [MIN_UPPER_ASCII, MAX_UPPER_ASCII] = [65, 90];
+  const [MIN_LOWER_ASCII, MAX_LOWER_ASCII] = [97, 122];
+  const MIN_ASCII = string.toUpperCase() === string ? MIN_UPPER_ASCII : MIN_LOWER_ASCII;
+  const MAX_ASCII = string.toUpperCase() === string ? MAX_UPPER_ASCII : MAX_LOWER_ASCII;
   return [MIN_ASCII, MAX_ASCII];
 };
 
-const reverseWord = (WORD_ASCII, MIN_ASCII, MAX_ASCII) => {
-  const ASCII = MAX_ASCII - WORD_ASCII + MIN_ASCII;
-  return String.fromCharCode(ASCII);
+const reverseWord = (string) => {
+  const [MIN_ASCII, MAX_ASCII] = defineASCIIRange(string);
+  const WORD_ASCII = string.charCodeAt(0);
+  const REVERSE_ASCII = MAX_ASCII - WORD_ASCII + MIN_ASCII;
+  return String.fromCharCode(REVERSE_ASCII);
 };
 
 function problem4(word) {
   let result = "";
+  const engReg = /^[a-zA-Z]*$/;
 
-  for (const index in word) {
-    const string = word[index];
-    const stringType = defineStringType(string);
-
-    if (stringType == "noAlphabet") {
+  for (const string of word) {
+    if (!engReg.test(string)) {
       result += string;
       continue;
     }
 
-    const WORD_ASCII = word.charCodeAt(index);
-    const [MIN_ASCII, MAX_ASCII] = defineASCIIRange(stringType);
-    result += reverseWord(WORD_ASCII, MIN_ASCII, MAX_ASCII);
+    result += reverseWord(string);
   }
 
   return result;
