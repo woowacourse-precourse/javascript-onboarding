97a587b30404ccac110860044398fbe209e8016c
sein <hyuntpdls@gmail.com>
2022-10-31 16:30:30 +0900

    [문제7] feat: 함께 아는 친구 확인하기

--- src/problem7.js
+++ src/problem7.js
@@ -2,7 +2,22 @@ function problem7(user, friends, visitors) {
   var answer;
   return answer;
 }
-
+// 함께 아는 친구 체크
+function findAcquaintance(user, friends, friendsList) {
+  let findResult = {};
+  for (let i = 0; i < friends.length; i++) {
+    let allFriends = findFriends(friends[i], friendsList);
+    let validFriends = allFriends.filter((name) => name !== user);
+    for (let j = 0; j < validFriends.length; j++) {
+      if (validFriends[j] in findResult) {
+        findResult[validFriends[j]] = findResult[validFriends[j]] + 10;
+      } else {
+        findResult[validFriends[j]] = 10;
+      }
+    }
+  }
+  return findResult;
+}
 // 내 친구 체크
 function findFriends(user, friendsList) {
   let result = [];
