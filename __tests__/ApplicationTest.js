8821a6b274a0dbe35fc3d04e53e82567e74cef7f
biyamn <b1y9mn@gmail.com>
2022-10-31 17:25:53 +0900

    test: 제출을 위해 개인적으로 추가한 테스트 코드 삭제

--- __tests__/ApplicationTest.js
+++ __tests__/ApplicationTest.js
@@ -16,18 +16,6 @@ describe("problem1", () => {
   test("case3", () => {
     expect(problem1([99, 102], [211, 212])).toEqual(-1);
   });
-  test("case4", () => {
-    expect(problem1([99, 103], [211, 212])).toEqual(-1);
-  });
-  // test("case5", () => {
-  //   expect(problem1([399, 400], [211, 212])).toEqual(1);
-  // });
-  test("case6", () => {
-    expect(problem1([401, 402], [211, 212])).toEqual(-1);
-  });
-  test("case7", () => {
-    expect(problem1([99, 100], [-1, 0])).toEqual(-1);
-  });
 });
 
 describe("problem2", () => {
@@ -36,15 +24,6 @@ describe("problem2", () => {
   });
   test("case2", () => {
     expect(problem2("zyelleyz")).toEqual("");
-  });
-  test("case3", () => {
-    expect(problem2("zyellleyz")).toEqual("");
-  });
-  test("case4", () => {
-    expect(problem2("zyalllelyz")).toEqual("zyaelyz");
-  });
-  test("case4", () => {
-    expect(problem2("aabba")).toEqual("a");
   });
 });
 
@@ -60,9 +39,6 @@ describe("problem3", () => {
 describe("problem4", () => {
   test("case1", () => {
     expect(problem4("I love you")).toEqual("R olev blf");
-  });
-  test("case2", () => {
-    expect(problem4("Hello  Everyone!")).toEqual("Svool  Veviblmv!");
   });
 });
 
@@ -106,115 +82,5 @@ describe("problem7", () => {
         ["bedi", "bedi", "donut", "bedi", "shakevan"]
       )
     ).toEqual(["andole", "jun", "bedi"]);
-  });
-
-  test('case2', () => {
-    expect(
-      problem7(
-        'mrko',
-        [
-          ['mrko', 'jun'],
-          ['donut', 'jun'],
-          ['donut', 'mrko'],
-          ['shakevan', 'andole'],
-          ['shakevan', 'jun'],
-          ['shakevan', 'mrko'],
-        ],
-        ['bedi', 'bedi', 'donut', 'bedi', 'shakevan'],
-      ),
-    ).toEqual(['andole', 'bedi']);
-  });
-
-  test('case3', () => {
-    expect(
-      problem7(
-        'mrko',
-        [
-          ['mrko', 'jun'],
-          ['donut', 'jun'],
-          ['donut', 'mrko'],
-          ['shakevan', 'andole'],
-          ['jun', 'andole'],
-          ['shakevan', 'jun'],
-          ['shakevan', 'mrko'],
-        ],
-        ['bedi', 'bedi', 'donut', 'bedi', 'shakevan'],
-      ),
-    ).toEqual(['andole', 'bedi']);
-  });
-
-  test('case4', () => {
-    expect(
-      problem7(
-        'mrko',
-        [
-          ['mrko', 'jun'],
-          ['bedi', 'jun'],
-          ['bedi', 'donut'],
-          ['donut', 'jun'],
-          ['donut', 'mrko'],
-          ['shakevan', 'andole'],
-          ['jun', 'andole'],
-          ['shakevan', 'jun'],
-          ['shakevan', 'mrko'],
-        ],
-        ['donut', 'shakevan'],
-      ),
-    ).toEqual(['andole', 'bedi']);
-  });
-
-  test('case5', () => {
-    expect(
-      problem7(
-        'andole',
-        [
-          ['andole', 'jun'],
-          ['donut', 'jun'],
-          ['donut', 'shakevan'],
-          ['shakevan', 'andole'],
-          ['shakevan', 'jun'],
-          ['shakevan', 'bedi'],
-          ['anne', 'jun'],
-        ],
-        ['donut', 'mrko', 'peter', 'sam'],
-      ),
-    ).toEqual(['donut', 'anne', 'bedi', 'mrko', 'peter']);
-  });
-
-  test('case6', () => {
-    expect(
-      problem7(
-        'hello',
-        [
-          ['andole', 'jun'],
-          ['andole', 'bedi'],
-          ['jun', 'shakevan'],
-          ['jun', 'kane'],
-          ['jun', 'sam'],
-          ['bedi', 'shakevan'],
-          ['bedi', 'anne'],
-          ['bedi', 'sam'],
-          ['anne', 'mrko'],
-        ],
-        ['donut', 'anne', 'mrko', 'mrko', 'sam'],
-      ),
-    ).toEqual(['mrko', 'anne', 'donut', 'sam']);
-  });
-
-  test("case8", () => {
-    expect(
-      problem7(
-        "mrko",
-        [
-          ["donut", "andole"],
-          ["donut", "jun"],
-          ["donut", "mrko"],
-          ["shakevan", "andole"],
-          ["shakevan", "jun"],
-          ["shakevan", "mrko"],
-        ],
-        []
-      )
-    ).toEqual(["andole", "jun"]);
   });
 });
