2561b95519b18f0035b176075f7a3de1c0d9e273
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 05:40:00 +0900

    feat: [problem5] 큰 금액의 화폐부터 최대로 받을 수 있는 개수를 구하는 기능

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  const UNITS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+  var answer = UNITS.map(unit => {
+    cnt = Math.floor(money / unit);
+    money %= unit;
+    return cnt;
+  });
   return answer;
 }
 
