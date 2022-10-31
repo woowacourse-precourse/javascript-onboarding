6f47d56ad7b002988949a1beed91e3399bbb03fa
Dongkeun <hongdongk@naver.com>
2022-10-31 20:28:46 +0900

    problem6: 닉네임 두글자씩 쪼개기

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  
+  let namearray =[];
+
+  for ( let i=0; i<forms.length; i++){
+    let arr = [];
+    const [email, nickname] = forms[i];
+    for (let v = 0; v<nickname.length-1; v++){
+      arr.push(nickname.slice(v,v+2));
+    };
+    namearray.push(arr);
+  };
+
 }
 
 module.exports = problem6;
