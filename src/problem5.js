c1ecf88c10271a81801ef6555a2fe7243c35e1d1
Minju Park <project.mizzu@gmail.com>
2022-10-31 19:04:47 +0900

    Feat : listAmounts에 포함된 단위에 따라 result 도출
    - listAmounts에 있는 화폐의 단위들로 money를 나눔
    - 몫은 result에 저장, 나머지는 change에 저장
    - change가 0이 될 때 까지 반복
    - result 배열 반환

--- src/problem5.js
+++ src/problem5.js
@@ -1,25 +1,29 @@
 // [✅] 잔돈을 의미하는 change 변수 생성
 // [✅] result 배열 생성
 // [✅] 사용할 화폐의 단위를 담은 배열 생성 -> listAmounts
-// [] money를 5만원권으로 나누고 몫은 result[0]에 저장, 나머지는 change에 저장
-// [] money를 1만원권으로 나누고 몫은 result[1]에 저장, 나머지는 change에 저장
-// [] money를 5천원권으로 나누고 몫은 result[2]에 저장, 나머지는 change에 저장
-// [] money를 1천원권으로 나누고 몫은 result[3]에 저장, 나머지는 change에 저장
-// [] money를 5백원으로 나누고 몫은 result[4]에 저장, 나머지는 change에 저장
-// [] money를 1백원으로 나누고 몫은 result[5]에 저장, 나머지는 change에 저장
-// [] money를 50원으로 나누고 몫은 result[6]에 저장, 나머지는 change에 저장
-// [] money를 10원으로 나누고 몫은 result[7]에 저장, 나머지는 change에 저장
-// [] money를 1원으로 나누고 몫은 result[8]에 저장, 나머지는 change에 저장
-// [] result 배열 반환
+// [✅] money를 5만원권으로 나누고 몫은 result[0]에 저장, 나머지는 change에 저장
+// [✅] money를 1만원권으로 나누고 몫은 result[1]에 저장, 나머지는 change에 저장
+// [✅] money를 5천원권으로 나누고 몫은 result[2]에 저장, 나머지는 change에 저장
+// [✅] money를 1천원권으로 나누고 몫은 result[3]에 저장, 나머지는 change에 저장
+// [✅] money를 5백원으로 나누고 몫은 result[4]에 저장, 나머지는 change에 저장
+// [✅] money를 1백원으로 나누고 몫은 result[5]에 저장, 나머지는 change에 저장
+// [✅] money를 50원으로 나누고 몫은 result[6]에 저장, 나머지는 change에 저장
+// [✅] money를 10원으로 나누고 몫은 result[7]에 저장, 나머지는 change에 저장
+// [✅] money를 1원으로 나누고 몫은 result[8]에 저장, 나머지는 change에 저장
+// [✅] change가 0이 될 때 까지 반복
+// [✅] result 배열 반환
 
 function problem5(money) {
-  let change = 0;
+  let change = money;
   const result = [];
   const listAmounts = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  console.log(Math.floor(money / listAmounts[0]), money % listAmounts[0]);
+  for (let i = 0; i < listAmounts.length; i++) {
+    result.push(Math.floor(change / listAmounts[i]));
+    change = change % listAmounts[i];
+  }
   return result;
 }
 
-console.log(problem5(50237));
+console.log(problem5(15000));
 
 module.exports = problem5;
