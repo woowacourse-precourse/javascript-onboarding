c978a6af13f562da92923d0a97938f82cca26563
nuheajiohc <jaehun341012@gmail.com>
2022-10-31 15:15:04 +0900

    문제5 : (refactor) 변수명 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,16 @@
 function problem5(money) {
-    const answer = Classification(money);
-    return answer;
+    return Classification(money);
 }
 
 function Classification(money) {
-    const answer = [];
+    const walletArr = [];
     const kindOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
     let leftMoney = money;
-    for (let element of kindOfMoney) {
-        answer.push(Math.floor(leftMoney / element));
-        leftMoney = leftMoney % element;
+    for (let kinds of kindOfMoney) {
+        walletArr.push(Math.floor(leftMoney / kinds));
+        leftMoney = leftMoney % kinds;
     }
-    return answer;
+    return walletArr;
 }
 
 module.exports = problem5;
