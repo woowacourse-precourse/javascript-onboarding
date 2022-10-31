678e9f9ff091a9deaeaed37330e5f1838b03fa1e
sery <yunsel2@gmail.com>
2022-10-31 12:16:20 +0900

    feat(Problem7): Return array of user's friends

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,17 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendsOfUserList = getFriends(user, friends); // ["donut", "shakevan"]
+}
+
+function getFriends(user, buddies) {
+  const buddyIncludeUser = buddies.filter((buddy) => {
+    if (buddy.includes(user)) return buddy;
+  });
+
+  return eliminateUser(buddyIncludeUser, user);
+}
+
+function eliminateUser(userList, user) {
+  return [].concat(...userList).filter((person) => person !== user);
 }
 
 module.exports = problem7;
