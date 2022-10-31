aeb5ff9d21e9d400950a85a129f7ac607c9c6519
ming0830 <minsun0830@naver.com>
2022-10-31 23:22:51 +0900

    [PROBLEM4] feat : 알파벳 배열을 저장하고 알파벳 이외 문자는 그대로 변환하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,22 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alphabetList = [
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ];
+  
+  
+  let result = "";
+  const eng =/[a-zA-Z]/ ;
+  for(let i =0;i<word.length;i++){
+    if(eng.test(word[i])===false){
+      result += word[i]
+    } 
+  } return result;
+
+
 }
 
 module.exports = problem4;
