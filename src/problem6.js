d5cbc4720366543e3d651b1f4138bd76a128d155
js43o <js43o@naver.com>
2022-10-31 20:32:29 +0900

    Fix: Remove unused code from problem6.js

--- src/problem6.js
+++ src/problem6.js
@@ -3,15 +3,12 @@ function problem6(forms) {
   let words = {};
   let duplicated = new Set();
 
-  for (let [email, name] of forms) {
+  forms.forEach(([email, name]) => {
     words[email] = new Set();
     for (let i = 0; i < name.length - 1; i++) {
       words[email].add(name.slice(i, i + 2));
     }
-  }
-  for (let email in words) {
-    console.log(words[email]);
-  }
+  });
 
   for (let setKeyA in words) {
     for (let setKeyB in words) {
@@ -20,7 +17,6 @@ function problem6(forms) {
       let intersect = new Set(
         [...words[setKeyA]].filter((w) => words[setKeyB].has(w))
       );
-
       if (intersect.size > 0) {
         duplicated.add(setKeyA, setKeyB);
       }
@@ -31,15 +27,5 @@ function problem6(forms) {
 
   return answer;
 }
-
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
 
 module.exports = problem6;
