5987326508be6fa804ea4e693f4595d4adc9a5c4
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-10-31 15:18:35 +0900

    feat: Add split array

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
   var answer;
+
   return answer;
 }
+
+function deduplication(string) {
+  let answerArr = [string[0]];
+
+  for (let i = 1; i < string.length; i++) {
+    let top = answerArr[answerArr.length - 1];
+    if (top !== string[i]) {
+      answerArr.push(string[i]);
+    }
+  }
+  return answerArr;
+}
+
+// console.log(problem2("browoanoommnaon"));
+console.log(deduplication("browoanoommnaon"));
+
+//중복제거 알고리즘 구현중
 
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
+  let answer;
+
   return answer;
 }
+
+function numberArr(number) {
+  let array = [];
+  let newArray = '';
+  for (let i = 1; i <= number; i++) {
+    array.push(i.toString().split(""));
+  }
+  console.log(newArray.concat(array));
+}
+
+// console.log(problem3(13));
+console.log(numberArr(13));
 
 module.exports = problem3;
