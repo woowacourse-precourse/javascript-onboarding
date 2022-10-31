74011b06827a8de3001a27e0d2d4815049561b72
jihyun12 <kkjjhhbb@naver.com>
2022-11-01 00:03:23 +0900

    [solve]problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
-}
+  let clap = 0;
+  const regex369 = /3|6|9/g;
 
+  for (let num = 1; num <= number; num++) {
+    clap += (String(num).match(regex369) || []).length;
+  }
+  return clap;
+}
 module.exports = problem3;
