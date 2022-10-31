2ee49cfd72c64b98b935f45c11760cbe21faaf04
Ssoco <dlscjs8646@gmail.com>
2022-11-01 03:29:12 +0900

    p3: 완전탐색으로 3, 6, 9 개수 확인하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for (let i = 1; i <= number; ++i) {
+    let tmp = String(i);
+    for (let j = 0; j < tmp.length; ++j) {
+      if (tmp[j] === "3" || tmp[j] === "6" || tmp[j] === "9") ++answer;
+    }
+  }
+
   return answer;
 }
 
