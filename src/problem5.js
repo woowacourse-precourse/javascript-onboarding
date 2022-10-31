a7dea880a89c31c291ac15041a59e304e2d1e4e0
d-charlie-kim <dokkim@student.42seoul.kr>
2022-10-31 23:43:09 +0900

    Feat: <problem5> 가격대별로 나누어 계산하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,28 @@
+function calculate(money) {
+  let dividedMoney = [];
+
+  dividedMoney.push(Math.floor(money / 50000));
+  money = money % 50000;
+  dividedMoney.push(Math.floor(money / 10000));
+  money = money % 10000;
+  dividedMoney.push(Math.floor(money / 5000));
+  money = money % 5000;
+  dividedMoney.push(Math.floor(money / 1000));
+  money = money % 1000;
+  dividedMoney.push(Math.floor(money / 500));
+  money = money % 500;
+  dividedMoney.push(Math.floor(money / 100));
+  money = money % 100;
+  dividedMoney.push(Math.floor(money / 50));
+  money = money % 50;
+  dividedMoney.push(Math.floor(money / 10));
+  money = money % 10;
+  dividedMoney.push(money);
+  return dividedMoney;
+}
+
 function problem5(money) {
-  var answer;
+  let answer = calculate(money);
   return answer;
 }
 
