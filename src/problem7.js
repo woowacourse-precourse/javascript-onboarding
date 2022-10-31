16d296e0fd8e3e54cb4952339adcbce4a45c0838
d-charlie-kim <dokkim@student.42seoul.kr>
2022-11-01 06:44:19 +0900

    Feat: <problem7> 사람들 리스트 + 방문자 점수부여 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,43 @@
+function setFriendPoints(people) {
+
+}
+
+function setVisitorPoints(people, visitors) {
+  let newbie = 0;
+
+  for (let i = 0; i < visitors.length; i++) {
+    if (people.has(visitors[i]) == false)
+      people.set(visitors[i], newbie);
+    if (people.get(visitors[i]) >= 0)
+      people.set(visitors[i], people.get(visitors[i]) + 1)
+  }
+}
+
+function makeUsersList(user, friends) {
+  let people = new Map();
+  let alreadyFriend = -1;
+  let newbie = 0;
+
+  people.set(user, -2);
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] != user)
+      people.set(friends[i][0], newbie);
+    if (friends[i][1] != user)
+      people.set(friends[i][1], newbie);
+    if (friends[i][0] == user)
+      people.set(friends[i][1], alreadyFriend);
+    if (friends[i][1] != user)
+      people.set(friends[i][0], alreadyFriend);
+  }
+  return people;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  let people = makePeopleList(friends, visitors);
+
+  setFriendPoints(people);
+  setVisitorPoints(people, visitors);
+  let answer = getTopFivePeople(people);
   return answer;
 }
 
