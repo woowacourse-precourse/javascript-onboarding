96f153dbe83f80bc49d0afb7379667c68c61fafc
gsong <rmsdlf1206@gmail.com>
2022-10-31 20:39:08 +0900

    feat: problem 5 큰 단위부터 그 금액보다 크다면 계속 빼면서 result += 1 하는 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -2,5 +2,11 @@ function problem5(money) {
   let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 }
-
+function moneyBack(money, unit, index, result) {
+  while (money >= unit) {
+    money -= unit;
+    result[index] += 1;
+  }
+  return money;
+}
 module.exports = problem5;
