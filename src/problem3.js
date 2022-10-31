8dd4cd9a5ba17971af83de0de65447952d466fc1
Suyeon <msjgr2801@gmail.com>
2022-10-31 16:36:43 +0900

    Feat: solve function lists of problem3
    기능 3번 로직 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,20 @@
 /* 
   메인 로직
   다음 과정을 number가 0이 될 때까지 반복
-  1. [ ] number를 10으로 나눈 나머지가 3,6,9 중 하나면 박수 + 1
-  2. [ ] number <- number / 10
+  1. [x] number를 10으로 나눈 나머지가 3,6,9 중 하나면 박수 + 1
+  2. [x] number <- number / 10
 */
 
 
 function problem3(number) {
   let clap = 0;
+
+  while (number) {
+    if (number % 10 === 3 || number % 10 === 6 || number % 10 === 9) {
+      clap++;
+      number = number / 10;
+    }
+  }
 
   return clap;
 }
