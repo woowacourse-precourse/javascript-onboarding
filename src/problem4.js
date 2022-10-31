5008e40222c39775eb7b59bfdc9b56620713548d
Sunghyun627 <kksscc4@gmail.com>
2022-10-31 09:01:20 +0900

    feat: 해당 문자가 알파벳인지 확인하는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,7 +1,7 @@
 /*
   구현할 기능 목록
   [O] 1. 청개구리 사전을 생성 및 초기화하는 기능
-  [ ] 2. 해당 문자가 알파벳인지 확인하는 기능
+  [O] 2. 해당 문자가 알파벳인지 확인하는 기능
   [ ] 3. 해당 문자가 대문자인지 확인하는 기능
   [ ] 4. 1~3의 기능을 이용하여 주어진 word를 이용하여 변환된 문자열을 반환하는 기능
 */
@@ -22,6 +22,13 @@ function initDictionary(dictionary) {
   for (let i = firstNum; i <= lastNum; i++) {
     dictionary.set(String.fromCharCode(i), String.fromCharCode(sumNum - i));
   }
+}
+
+function isAlphabet(character) {
+  return (
+    ('A'.charCodeAt() <= character.charCodeAt() && 'Z'.charCodeAt() >= character.charCodeAt()) ||
+    ('a'.charCodeAt() <= character.charCodeAt() && 'z'.charCodeAt() >= character.charCodeAt())
+  );
 }
 
 module.exports = problem4;
