dd0ee509c8367592c6c17e6be71b7105a36d0a53
Steadystudy <sgmin206@naver.com>
2022-10-31 15:08:10 +0900

    [problem6] Feat: 같은 글자가 연속으로 포함된 닉네임의 이메일을 set에 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const set = new Set();
+
+  forms.forEach(([email, nickname],index) => {
+    for(let i = index; i <forms.length - 1; i++) {
+      for(let j = 0; j < nickname.length - 1; j++) {
+        const letters = nickname.split('').slice(j,j+2).join('');
+
+        if(forms[i+1][1].includes(letters)) {
+          set.add(email);
+          set.add(forms[i+1][0]);
+        }
+      }
+    }
+  })
 }
 
 module.exports = problem6;
