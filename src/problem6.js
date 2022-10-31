70be9919b5c3dd6429a90da220bc1f92af67dc38
chiman <rlaclghks123@naver.com>
2022-10-31 19:31:52 +0900

    feat: problem6- cutUserName 함수 생성
    username에서 2글자씩 가져오는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -1,13 +1,27 @@
 function getUserName(userInfo) {
   const userNameArr = [];
-
   userInfo.map((user) => userNameArr.push(user[1]));
-
   return userNameArr;
+}
+
+function cutUserName(userNameArr) {
+  const cutUserNameArr = [];
+
+  userNameArr.map((userName) => {
+    for (let i = 0; i < userName.length; i++) {
+      if (userName.slice(i, i + 2).length === 2) {
+        const cutName = userName.slice(i, i + 2);
+        cutUserNameArr.push(cutName);
+      }
+    }
+  });
+
+  return cutUserNameArr;
 }
 function problem6(forms) {
   var answer;
   const userNameArr = getUserName(forms);
+  const cutUserNameArr = cutUserName(userNameArr);
   return answer;
 }
 
