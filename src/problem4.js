0cb90816697f3093ce7815a56c530b215093d75e
Saans <92422813+saans808@users.noreply.github.com>
2022-10-31 16:14:58 +0900

    [p4] 배열에 넣어 아스키 코드로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,12 @@
 function problem4(word) {
   var answer;
+  let arr = [...word];
+  let newArr = [];
+
+  for (let i=0; i<arr.length; i++) {
+    newArr.push(arr[i].charCodeAt(arr[i]));
+  }  
+  
   return answer;
 }
 
