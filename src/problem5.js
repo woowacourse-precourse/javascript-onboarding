0fcd55a9adf485059ecb268b5310a934625d97c7
KUD <kimud6003@gmail.com>
2022-10-31 13:42:03 +0900

    ✨ Feature: problem5.js bankChanger - problem5.js: 환폐단위에 맞게 리스트를 만들어주는 bankChanger

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,16 @@
 function problem5(money) {
-  var answer;
+  var answer = bankChanger(money)
   return answer;
+}
+
+function bankChanger(money){
+  let result= [];
+  let moneyList = [50000,10000,5000,1000,500,100,50,10,1];
+  for(let item  of  moneyList){
+    result.push(Math.floor(money/item));
+    money = money%item;
+  }
+  return result;
 }
 
 module.exports = problem5;
