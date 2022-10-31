2c9b7279011f69ad324b23ddb7d3e29c3e7bc468
HeoJiye <heoze1031@gmail.com>
2022-10-31 13:15:58 +0900

    (problem1) docs: 기능 목록에 예외처리 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,12 +1,14 @@
 
 // < 기능 목록 >
 // 1. 책을 임의로 펼친 페이지 값을 받는다.
+//    [예외처리] element 중 0보다 작거나 같은 값이 있다.
 //    [예외처리] 첫번째 element가 짝수이다.
 //    [예외처리] 첫번째 element와 두번째 element의 차가 1이 아니다.
 // 2. 포비와 크롱의 점수를 결정한다.
 // 2. 포비와 크롱의 점수를 비교해서 return 한다.
 
 function problem1(pobi, crong) {
+
   var answer;
   return answer;
 }
