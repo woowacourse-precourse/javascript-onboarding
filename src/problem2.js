f861aa21a73e3b2b1ba4bad586d059346d876b5d
hhhminme <hororonoodle@gmail.com>
2022-11-01 00:34:28 +0900

    refactor : Clearly fix the if condition for determining the current character and the next character in problem 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,15 @@
 function problem2(cryptogram) {
-  let cryptoArray = [...cryptogram];
+  const cryptoArray = [...cryptogram];
   let cnt = 0;
   while (cnt !== cryptoArray.length - 1 && cryptoArray.length !== 0) {
     if (cryptoArray[cnt] === cryptoArray[cnt + 1]) {
       cryptoArray.splice(cnt, 2); // 현재 문자와 다음 문자가 동일할 경우 배열에서 해당 두 문자를 제거해준다.
       cnt = 0;
-    } else {
-      cnt++;
     }
+    if (cryptoArray[cnt] !== cryptoArray[cnt + 1]) cnt++;
   }
-  const answer = cryptArr.join('');
+
+  const answer = cryptoArray.join('');
   return answer;
 }
 
