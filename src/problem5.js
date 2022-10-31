f55e0861615d8995f5fe40ccf41d7a5059429571
yunho <Yunho7058@github.com>
2022-10-31 21:07:36 +0900

    problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,23 @@
 function problem5(money) {
-  var answer;
+  if (typeof money !== 'number') return '숫자를 입력해주세요.';
+  var answer = new Array(9).fill(0);
+  const list = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let count = 0;
+  let left = 0;
+  while (money) {
+    if (money - list[left] >= 0) {
+      money = money - list[left];
+      count++;
+      if (!money) {
+        answer[left] = count;
+        break;
+      }
+    } else {
+      answer[left] = count;
+      left++;
+      count = 0;
+    }
+  }
   return answer;
 }
 
