27f73a610460a4167447cff774f841bd7de3ecca
oxhwixo <hwi_030@naver.com>
2022-10-31 19:03:39 +0900

    Problem5: 입력값을 단위별로 나누어서 저장하는 기능 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,15 @@
+/** 입력값을 단위별로 나누어 저장하는 기능 **/
 function problem5(money) {
-  var answer;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0]; // 오만, 만, 오천, 천, 오백, 백, 오십, 일
+  var unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (let i = 0; i <= 9; i++) {
+    if (parseInt(money / unit[i]) > 0) {
+      answer[i] = parseInt(money / unit[i]);
+      money = parseInt(money % unit[i]);
+    }
+  }
+
   return answer;
 }
 
