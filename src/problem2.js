44e0f120fb398cdc8afa91a1556961823908dae2
JUNU <106523012+bokjunwoo@users.noreply.github.com>
2022-11-01 01:54:59 +0900

    주석추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,13 +1,16 @@
 function problem2(cryptogram) {
 
+  // 문자열을 잘라 새로운 배열에 넣는다
   let cryptogramArr = cryptogram.split('');
 
   let i = 0;
 
+  // 기존의 배열과 새로운 배열을 + 1한 값을 비교해 같은 값이 있을 경우 제거하는 while문
   while (i < cryptogramArr.length) {
-    cryptogramArr[i] == cryptogramArr[i + 1] ? (cryptogramArr.splice(i, 2), (i = 0)) : (i += 1); //false i 1증가
+    cryptogramArr[i] === cryptogramArr[i + 1] ? (cryptogramArr.splice(i, 2), (i = 0)) : (i += 1); //false i 1증가
   }
 
+  // 하나의 문자열로 반환
   return cryptogramArr.join('');
 }
 
