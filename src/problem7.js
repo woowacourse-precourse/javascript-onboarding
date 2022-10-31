e7c64174a60e14d122c808a9429b083185be647b
ukkodeveloper <ukkodeveloper@gmail.com>
2022-10-31 21:19:24 +0900

    refactor: [문제7] SNS 메소드 분리 및 변수명 정정

--- src/problem7.js
+++ src/problem7.js
@@ -15,18 +15,13 @@ function problem7(user, friends, visitors) {
     const nickNames = [user, ...friends.flat(), ...visitors];
     nickNames.forEach((nickName) => {
       if (!NICKNAME_REGEX.test(nickName)) {
-        throw new Error(
-          '모든 user의 닉네임은 1이상 30이하의 알파벳 소문자이여야 합니다.'
-        );
+        throw new Error('모든 user의 닉네임은 1이상 30이하의 알파벳 소문자이여야 합니다.');
       }
     });
   };
 
   const checkFriendsValid = (friends) => {
-    if (
-      friends.length < MIN_FREINDS_LENGTH ||
-      friends.length > MAX_FREINDS_LENGTH
-    ) {
+    if (friends.length < MIN_FREINDS_LENGTH || friends.length > MAX_FREINDS_LENGTH) {
       throw new Error('friends는 길이가 1이상 10,000 이하인 배열이여야 합니다');
     }
   };
@@ -36,50 +31,53 @@ function problem7(user, friends, visitors) {
       return;
     }
     if (visitors.length > MAX_VISITORS_LENGTH) {
-      throw new Error(
-        'visitors는 길이가 0이상 10,000 이하인 배열이여야 합니다'
-      );
+      throw new Error('visitors는 길이가 0이상 10,000 이하인 배열이여야 합니다');
     }
   };
 
-  //data structure
   class SNS {
-    constructor(usersFriendRelation, visitors) {
-      this.usersFriendRelation = usersFriendRelation;
-      this.userList = usersFriendRelation.flat().concat(visitors);
+    constructor(friends, visitors) {
+      this.friends = friends;
+      this.userList = friends.flat().concat(visitors);
       this.visitors = visitors;
     }
 
     getUserMap() {
       const userMap = new Map();
-      this.usersFriendRelation.forEach((relation) => {
-        const userId1 = relation[0];
-        const userId2 = relation[1];
-        if (userMap.get(userId1) === undefined) {
-          const userObject = new User(userId1);
-          userMap.set(userId1, userObject);
-        }
-        if (userMap.get(userId2) === undefined) {
-          const userObject = new User(userId2);
-          userMap.set(userId2, userObject);
-        }
-        const userObject1 = userMap.get(userId1);
-        const userObject2 = userMap.get(userId2);
-        userObject1.addFriend(userId2);
-        userObject2.addFriend(userId1);
-        userMap.set(userId1, userObject1);
-        userMap.set(userId2, userObject2);
+      this.friends.forEach((relation) => {
+        this.setUserMapByFriends(relation, userMap);
       });
+      this.setUserMapByVisitors(visitors, userMap);
+      return userMap;
+    }
 
+    setUserMapByFriends([userId1, userId2], userMap) {
+      if (userMap.get(userId1) === undefined) {
+        const userObject = new User(userId1);
+        userMap.set(userId1, userObject);
+      }
+      if (userMap.get(userId2) === undefined) {
+        const userObject = new User(userId2);
+        userMap.set(userId2, userObject);
+      }
+      const userObject1 = userMap.get(userId1);
+      const userObject2 = userMap.get(userId2);
+      userObject1.addFriend(userId2);
+      userObject2.addFriend(userId1);
+      userMap.set(userId1, userObject1);
+      userMap.set(userId2, userObject2);
+    }
+
+    setUserMapByVisitors(visitors, userMap) {
       visitors.forEach((visitor) => {
         if (userMap.get(visitor) === undefined) {
           const visitorObject = new User(visitor);
           userMap.set(visitor, visitorObject);
         }
       });
-      return userMap;
     }
   }
+
   class User {
     constructor(userId) {
       this.id = userId;
@@ -89,12 +87,14 @@ function problem7(user, friends, visitors) {
       this.friends.push(friend);
     }
   }
+
   class UserRecommandationSystem {
     constructor(userId, userMap) {
       this.user = userMap.get(userId);
       this.userMap = userMap;
       this.recommandationScoreBoard = new Map();
     }
+
     initBoard() {
       const scoreBoard = new Map();
       const userFriends = this.user.friends;
@@ -111,7 +111,6 @@ function problem7(user, friends, visitors) {
     }
 
     analyzeIndrectRelation() {
-      //FIXME: userFriendsList 등 기본자료 initBoard()와 중복
       const scoreBoard = this.recommandationScoreBoard;
       const candidates = [...scoreBoard.keys()].filter((candidate) => {
         return this.userMap.get(candidate).friends.length !== 0;
@@ -119,13 +118,8 @@ function problem7(user, friends, visitors) {
       candidates.forEach((candidate) => {
         const candidatesFriendList = this.userMap.get(candidate).friends;
         const userFriendList = this.user.friends;
-        const intersection = candidatesFriendList.filter((user) =>
-          userFriendList.includes(user)
-        );
-        scoreBoard.set(
-          candidate,
-          scoreBoard.get(candidate) + 10 * intersection.length
-        );
+        const intersection = candidatesFriendList.filter((user) => userFriendList.includes(user));
+        scoreBoard.set(candidate, scoreBoard.get(candidate) + 10 * intersection.length);
       });
       this.recommandationScoreBoard = scoreBoard;
       return this;
@@ -146,26 +140,19 @@ function problem7(user, friends, visitors) {
 
     representRecommandation() {
       const board = this.recommandationScoreBoard;
-      const recommandationLsit = [...board.keys()]
+      return [...board.keys()]
         .sort()
         .sort((a, b) => board.get(b) - board.get(a))
         .slice(0, 5);
-      return recommandationLsit;
     }
   }
 
-  //error handle
   checkValid(user, friends, visitors);
 
-  //data handle
   const sns = new SNS(friends, visitors);
-  snsUserMap = sns.getUserMap();
+  const snsUserMap = sns.getUserMap();
 
-  //recommandation system handle
-  const userRecommandationSystem = new UserRecommandationSystem(
-    user,
-    snsUserMap
-  );
+  const userRecommandationSystem = new UserRecommandationSystem(user, snsUserMap);
   const result = userRecommandationSystem
     .initBoard()
     .analyzeIndrectRelation()
