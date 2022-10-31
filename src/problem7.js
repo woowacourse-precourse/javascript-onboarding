05ae0bd2df25b67bd8e06ff509f0ff1ad56b4d84
jmk5315 <31719854+jmk5315@users.noreply.github.com>
2022-11-01 08:42:28 +0900

    [7번 문제] feat: nameMap 구현

--- src/problem7.js
+++ src/problem7.js
@@ -12,6 +12,16 @@ function findFriends(user, friend){
 
 function problem7(user, friends, visitors) {
   var answer;
+  let nameMap = new Map();
+
+  for(let i = 0;i < friends.length;i++){
+    if(!nameMap.has(friends[i][0])){
+      nameMap.set(friends[i][0],0);
+    }
+    if(!nameMap.has(friends[i][1])){
+      nameMap.set(friends[i][1],0);
+    }
+  }
   return answer;
 }
 
