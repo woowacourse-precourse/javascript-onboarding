1451d4351e284f0887d2d1eff347f88556a0741b
MinGyu-Kang <kagrin97@gmail.com>
2022-10-31 11:29:08 +0900

    problem5/refactor : withdrawMap객체 미들웨어를 만들어서 bankWithdraw를 가기전 wallet의 coinIndex를 자동으로 기입하게 리팩토링했습니다

--- src/problem5.js
+++ src/problem5.js
@@ -5,28 +5,58 @@ function bankWithdraw(wallet, coinIndex, money, pay) {
   return money;
 }
 
+const withdrawMap = {
+  50000(wallet, money) {
+    return bankWithdraw(wallet, 0, money, 50000);
+  },
+  10000(wallet, money) {
+    return bankWithdraw(wallet, 1, money, 10000);
+  },
+  5000(wallet, money) {
+    return bankWithdraw(wallet, 2, money, 5000);
+  },
+  1000(wallet, money) {
+    return bankWithdraw(wallet, 3, money, 1000);
+  },
+  500(wallet, money) {
+    return bankWithdraw(wallet, 4, money, 500);
+  },
+  100(wallet, money) {
+    return bankWithdraw(wallet, 5, money, 100);
+  },
+  50(wallet, money) {
+    return bankWithdraw(wallet, 6, money, 50);
+  },
+  10(wallet, money) {
+    return bankWithdraw(wallet, 7, money, 10);
+  },
+  1(wallet, money) {
+    return bankWithdraw(wallet, 8, money, 1);
+  },
+};
+
 function problem5(money) {
   const wallet = Array(9).fill(0);
 
   while (money !== 0) {
     if (money >= 50000) {
-      money = bankWithdraw(wallet, 0, money, 50000);
+      money = withdrawMap[50000](wallet, money);
     } else if (money >= 10000) {
-      money = bankWithdraw(wallet, 1, money, 10000);
+      money = withdrawMap[10000](wallet, money);
     } else if (money >= 5000) {
-      money = bankWithdraw(wallet, 2, money, 5000);
+      money = withdrawMap[5000](wallet, money);
     } else if (money >= 1000) {
-      money = bankWithdraw(wallet, 3, money, 1000);
+      money = withdrawMap[1000](wallet, money);
     } else if (money >= 500) {
-      money = bankWithdraw(wallet, 4, money, 500);
+      money = withdrawMap[500](wallet, money);
     } else if (money >= 100) {
-      money = bankWithdraw(wallet, 5, money, 100);
+      money = withdrawMap[100](wallet, money);
     } else if (money >= 50) {
-      money = bankWithdraw(wallet, 6, money, 50);
+      money = withdrawMap[50](wallet, money);
     } else if (money >= 10) {
-      money = bankWithdraw(wallet, 7, money, 10);
+      money = withdrawMap[10](wallet, money);
     } else if (money >= 1) {
-      money = bankWithdraw(wallet, 8, money, 1);
+      money = withdrawMap[1](wallet, money);
     }
   }
 
@@ -34,3 +64,5 @@ function problem5(money) {
 }
 
 module.exports = problem5;
+
+console.log(problem5(50237));
