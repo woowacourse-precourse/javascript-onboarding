6fc0eb7be794168fd85f93012e420e481ee15ceb
choieastsea <choieastsea@gmail.com>
2022-11-01 06:54:07 +0900

    1) check exception case
    Check exception case by making 'check' function

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,23 @@
 function problem1(pobi, crong) {
   var answer;
+  if (!(check(pobi) && check(crong))) {
+    //입력 조건 체크
+    answer = -1;
+  }
   return answer;
 }
 
+function check(arr) {
+  //배열이 아래의 조건들을 만족한다면 true리턴
+  return (
+    arr.length === 2 && //두페이지씩 입력
+    arr[0] + 1 === arr[1] && //책이 찢어지지 않았는지
+    arr[0] % 2 === 1 && //왼쪽이 홀수
+    arr[1] % 2 === 0 && //오른쪽이 짝수
+    arr[0] > 1 && //시작면
+    arr[1] < 400 //마지막면
+  );
+}
+
 module.exports = problem1;
+
