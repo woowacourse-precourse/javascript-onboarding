b88a4b8a5f8d03fb7b71a4387e2f5d2cce899c60
HWI <dlwognl99@naver.com>
2022-10-31 21:02:41 +0900

    문제7_feat: 친구인지 확인하고 친구들 아이디 배열을 반환하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,19 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  // user와 친구인 사용자 아이디 배열
+  const alreadyFriend = [];
+
+  friendCheck(user, friends, alreadyFriend);
+}
+
+function alreadyFriend(user, friends, alreadyFriend) {
+  // friends 원소 0또는 1자리에 원소가 user와 일치하면 1또는 0자리는 user와 친구인 사용자의 아이디 일 것이다.
+  friends.forEach((element) => {
+    if (element[0] === user) {
+      alreadyFriend.push(element[1]);
+    } else if (element[1] === user) {
+      alreadyFriend.push(element[0]);
+    }
+  });
 }
 
 module.exports = problem7;
