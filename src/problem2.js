507fcf7acf28339a36e6d6319bf1bf60188d95a2
pakxe <pigkill40@naver.com>
2022-10-31 16:50:54 +0900

    fix: [P2] 3개 이상의 중복 문자인 경우에도 올바르게 출력하도록 고침

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,18 @@
 function problem2(cryptogram) {
   const stack = new Array(cryptogram[0]);
-  let duplicatedChar = ""; //3자 이상의 중복일 경우를 위해 pop한 글자를 저장하는 변수
+  let duplicatedChar = cryptogram[0]; //3자 이상의 중복일 경우를 위해 pop한 글자를 저장하는 변수
 
   for (let i = 1; i < cryptogram.length; i++) {
-    let top = stack.length - 1;
-    if (stack[top] !== cryptogram[i]) stack.push(cryptogram[i]);
-    else if (stack[top] === cryptogram[i]) duplicatedChar = stack.pop();
+    top = stack.length - 1;
+    if (stack[top] !== cryptogram[i] && duplicatedChar !== cryptogram[i]) {
+      stack.push(cryptogram[i]);
+      duplicatedChar = "";
+    } else if (stack[top] === cryptogram[i]) duplicatedChar = stack.pop();
     else if (duplicatedChar === cryptogram[i]) continue;
   }
-
   return stack.join("");
 }
+
+console.log(problem2("zyalllelyz"));
 
 module.exports = problem2;
