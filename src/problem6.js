205a377b5b768265043948d3f60a38949183efee
배병재 <39183208+BaeByungjae@users.noreply.github.com>
2022-10-31 20:03:02 +0900

    문제6번 수정

--- src/problem6.js
+++ src/problem6.js
@@ -5,14 +5,12 @@ function problem6(forms) {
   forms.forEach((form) => {
     const user = form[1];
     userEmail[user] = form[0];
-    for (let i = 2; i <= user.length; i++) {
-      for (let j = 0; j <= user.length - i; j++) {
-        const subStr = user.substr(j, i);
-        if (subDict[subStr] && subDict[subStr] !== user) {
-          answer.push(form[0]);
-          answer.push(userEmail[subDict[subStr]]);
-        } else subDict[subStr] = user;
-      }
+    for (let j = 0; j < user.length - 1; j++) {
+      const subStr = user.substr(j, 2);
+      if (subDict[subStr] && subDict[subStr] !== user) {
+        answer.push(form[0]);
+        answer.push(userEmail[subDict[subStr]]);
+      } else subDict[subStr] = user;
     }
   });
   return [...new Set(answer)].sort();
