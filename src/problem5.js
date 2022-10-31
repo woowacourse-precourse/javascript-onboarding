e3385a52f1f5c3d084ebac31b48ddf79d5245804
yuna Kim <yuna000804@gmail.com>
2022-10-31 13:38:31 +0900

    problem5 [feat] : 금액 단위 리스트 만들기

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,12 @@
 function problem5(money) {
   var answer;
+  let moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let result = [];
+
   return answer;
 }
 
 module.exports = problem5;
+
+// 1. 금액 단위 리스트 만들기
+// 2. for 문을 돌며 금액을 단위로 나누어 몫을 리스트에 저장 후 전체 금액에서 빼줌 
