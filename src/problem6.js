7adbe00478f9fe981b04930c5c5ce21c2283d853
jungsmins1 <jungsmins00@gmail.com>
2022-10-31 16:07:27 +0900

    feat: 다른 크루들과 닉네임이 연속으로 2글자 이상 중복되는지 확인하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  
+  const formsMap = new Map(forms);
+  const nickName = Array.from(formsMap.values());
+
+  //다른 크루들과 닉네임이 연속으로 2글자 이상 중복되는지 확인하는 기능
+  const check = (name) => {
+    for (let i = 0; i < nickName.length; i++) {
+      for (let j = 0; j < nickName[i].length - 1; j++) {
+        if (name == nickName[i]) continue;
+        if (name.includes(nickName[i][j] + nickName[i][j + 1])) {
+          return true;
+        }
+      }
+    }
+  }
 }
 
-module.exports = problem6;
+problem6([ ["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"] ])
+
+// module.exports = problem6;
