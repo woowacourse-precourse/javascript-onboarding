5f41712fb4ae5d357e1e57dbd13266fe85e61015
dkile <dkile532@naver.com>
2022-11-01 02:22:09 +0900

    feat: add function of extracting user friends

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
+const extractUserFriends = (user, friends) =>
+  friends
+    .filter((friend) => friend.includes(user))
+    .flat()
+    .filter((friend) => friend !== user);
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userFriends = extractUserFriends(user, friends);
+
+  return userFriends;
 }
 
 module.exports = problem7;
