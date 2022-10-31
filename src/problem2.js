ef5a4d1b5b1de6a6c030ab667b6cc5d51579927f
JSH99 <tjgus9966@naver.com>
2022-10-31 20:50:02 +0900

    feat: 중복 문자 idx, cnt 체크

--- src/problem2.js
+++ src/problem2.js
@@ -2,7 +2,7 @@ function problem2(cryptogram) {
   var answer = cryptogram;
 
     // 1. 중복 문자 체크
-    var temp = answer[0]; 
+    var temp = answer[0];
     var bool = [];
     
     for(var i = 1; i < answer.length; i++) {
@@ -11,6 +11,18 @@ function problem2(cryptogram) {
       } else {
         bool.push(false);
         temp = answer[i];
+      }
+    }
+  
+    // 2. 중복 문자 있을 경우 idx, cnt 체크
+    var idx = 0;
+    var cnt = 1;
+
+    for(var j = 0; j < bool.length - 1; j++) {
+      if(bool[j] === true) cnt++;
+      if(bool[j] === true && bool[j + 1] === false) {
+        idx = j + 1;
+        break;
       }
     }
 
