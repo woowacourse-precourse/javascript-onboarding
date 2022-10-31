0ff0590af3d728ef2446c315a7c86144427e7106
knh6269 <knh6269@gmail.com>
2022-10-31 22:42:01 +0900

    feat : 미션3) 문자의 인덱스를 반환 후 reverse 해주는 함수

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,8 @@
-function problem4(word) {
-  var answer;
-  return answer;
-}
+ //문자의 인덱스를 반환후 reverse 해주는 함수
+ const getIndex = (letters, array) => {
+    const index = array.indexOf(letters);
+    return  array[25 - index];
+};
+
 
 module.exports = problem4;
