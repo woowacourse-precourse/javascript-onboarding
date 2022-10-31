31a7716ede6f9af91b546e66e8d4453537a89c9b
jihyun12 <kkjjhhbb@naver.com>
2022-11-01 08:12:40 +0900

    [solve]problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
-  var answer;
+  const paper = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  paper.forEach((p, index) => {
+    const converted = Math.floor(money / p);
+    answer[index] = converted;
+    money = money - converted * p;
+  });
   return answer;
 }
-
 module.exports = problem5;
