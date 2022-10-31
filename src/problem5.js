57a8c1b1ac94adf9de0d8b7f52d5979ba2fd6a4b
daal2quf <daal2quf@gmail.com>
2022-11-01 07:44:58 +0900

    코드 라인 정리

--- src/problem5.js
+++ src/problem5.js
@@ -3,17 +3,11 @@ function problem5(money) {
 
   let moneyCollection = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
 
- for(let won of moneyCollection) {
-  moenyCount.push(Math.floor(money / won))
-  money %= won
- }
-
+  for (let won of moneyCollection) {
+    moenyCount.push(Math.floor(money / won))
+    money %= won
+  }
   return moenyCount;
 }
 
-
-
 module.exports = problem5;
-
-
-
