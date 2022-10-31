67765ddcb4988379d22249b7448a0ac7b1a398ee
woo-jk <201702042@o.cnu.ac.kr>
2022-10-31 20:46:24 +0900

    refactor:[PROBLEM5] 인덱스 구하는 함수, 가장 큰 화폐 구하는 함수 병합

--- src/problem5.js
+++ src/problem5.js
@@ -1,36 +1,25 @@
-const findLargestMoneyUnit = (money) => {
-  if (money >= 50000) return 50000;
-  if (money >= 10000) return 10000;
-  if (money >= 5000) return 5000;
-  if (money >= 1000) return 1000;
-  if (money >= 500) return 500;
-  if (money >= 100) return 100;
-  if (money >= 50) return 50;
-  if (money >= 10) return 10;
-  if (money >= 1) return 1;
-};
-
-const findWalletArrayIndex = (money) => {
-  if (money === 50000) return 0;
-  if (money === 10000) return 1;
-  if (money === 5000) return 2;
-  if (money === 1000) return 3;
-  if (money === 500) return 4;
-  if (money === 100) return 5;
-  if (money === 50) return 6;
-  if (money === 10) return 7;
-  if (money === 1) return 8;
+const getLargestMoney = (money) => {
+  if (money >= 50000) return [0, 50000];
+  if (money >= 10000) return [1, 10000];
+  if (money >= 5000) return [2, 5000];
+  if (money >= 1000) return [3, 1000];
+  if (money >= 500) return [4, 500];
+  if (money >= 100) return [5, 100];
+  if (money >= 50) return [6, 50];
+  if (money >= 10) return [7, 10];
+  if (money >= 1) return [8, 1];
 };
 
 function problem5(money) {
   const wallet = Array.from({ length: 9 }, () => 0);
-  while (money > 0) {
-    const largestMoneyUnit = findLargestMoneyUnit(money);
-    const walletIndex = findWalletArrayIndex(largestMoneyUnit);
 
-    wallet[walletIndex]++;
-    money -= largestMoneyUnit;
+  while (money > 0) {
+    const [index, largestMoney] = getLargestMoney(money);
+
+    wallet[index]++;
+    money -= largestMoney;
   }
+
   return wallet;
 }
 
