6534ee3a8aacc1a81d21ca7d221dd2e690e58a8a
coolIlive <galilee155@naver.com>
2022-11-01 00:36:25 +0900

    Refactor(problem7): 변수명 수정
    userInfo->relationship/userScore->score/relationshipLoad()의 friendsName->friends/visitScoreCoutn()의 visit->visitorName/deleteInfo()의 userFriend->eachUserFriend

--- src/problem7.js
+++ src/problem7.js
@@ -1,10 +1,10 @@
-const userInfo={};
-const userScore={};
+const relationship={};
+const score={};
 
 function problem7(user, friends, visitors) {
   relationshipLoad(friends);
 
-  const userFriends=userInfo[user];
+  const userFriends=relationship[user];
   sameFriendScoreCount(userFriends);
   visitScoreCount(visitors);
   deleteInfo(user,userFriends);
@@ -15,35 +15,35 @@ function problem7(user, friends, visitors) {
   return sortTable.map((finalName)=>finalName[0]);
 };
 
-function relationshipLoad(friendsName) {
-  friendsName.forEach((name)=> {
-    userInfo[name[0]] ? userInfo[name[0]].add(name[1]) : userInfo[name[0]]=new Set([name[1]]);
-    userInfo[name[1]] ? userInfo[name[1]].add(name[0]) : userInfo[name[1]]=new Set([name[0]]);
+function relationshipLoad(friends) {
+  friends.forEach((name)=> {
+    relationship[name[0]] ? relationship[name[0]].add(name[1]) : relationship[name[0]]=new Set([name[1]]);
+    relationship[name[1]] ? relationship[name[1]].add(name[0]) : relationship[name[1]]=new Set([name[0]]);
   });
 };
 
 function sameFriendScoreCount(userFriends) {
-  Object.entries(userInfo).forEach((eachlInfo)=> {
-    userScore[eachlInfo[0]]=new Set([...eachlInfo[1]].filter(x => userFriends.has(x))).size*10
+  Object.entries(relationship).forEach((eachlInfo)=> {
+    score[eachlInfo[0]]=[...eachlInfo[1]].filter(x => userFriends.has(x)).size*10
   });
 };
 
 function visitScoreCount(visitors) {
-  visitors.forEach((visit) => {
-    userScore[visit]=(userScore[visit]|0)+1
+  visitors.forEach((visitorName) => {
+    score[visitorName]=(score[visitorName]|0)+1
   });
 };
 
 function deleteInfo(user,userFriends) {
-  delete userScore[user];
-  for (let userFriend of userFriends) {
-    delete userScore[userFriend]
+  delete score[user];
+  for (let eachUserFriend of userFriends) {
+    delete score[eachUserFriend]
   };
 };
 
 function sortFriends(sortTable) {
-  for (let member of Object.keys(userScore)) {
-    if (userScore[member]!==0) sortTable.push([member,userScore[member]]);
+  for (let member of Object.keys(score)) {
+    if (score[member]!==0) sortTable.push([member,score[member]]);
   };
 
   sortTable.sort((a,b)=> {
