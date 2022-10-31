7d8648b982910d4b4157517be1a3f312a8673d67
Dongkeun <hongdongk@naver.com>
2022-10-31 20:21:11 +0900

    problem2: 연속하는 중복 문자열 모두제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  
+  const array = cryptogram.split('');
+  let answer = [];
+
+  for(let i=0; i<array.length; i++){
+      let compare = answer[answer.length-1];
+      if(answer.length==0) answer.push(array[i]);
+      else if (compare !== array[i]) answer.push(array[i]);
+      else answer = answer.slice(0,-1);
+  };
+
+  return answer.join('');
+  
 }
 
 module.exports = problem2;
