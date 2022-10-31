5cf83c3d575fd1125ba13a47da975db7f0ad58a0
Sim <zh025700@naver.com>
2022-10-31 19:26:08 +0900

    feat: problem6 update requirement & input value in list

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,18 @@
 function problem6(forms) {
   var answer;
-  return answer;
+  let emailList = [];
+  let nickNameList = [];
+  let duplicateIdxList = [];
+  inputList(forms, emailList, nickNameList);
+
+  return duplicateIdxList.sort();
 }
+
+const inputList = (forms, emailList, nickNameList) => {
+  forms.map((e, idx) => {
+    e[0].length >= 11 && e[0].length < 20 ? emailList.push(e[0]) : null;
+    e[1].length > 0 && e[1].length < 20 ? nickNameList.push(e[1]) : null;
+  });
+};
 
 module.exports = problem6;
