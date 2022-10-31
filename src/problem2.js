9ea62356a45522f2fed4e969c9d64a54acd56954
hyejinny97 <yhj970102@gmail.com>
2022-10-31 20:49:37 +0900

    Create problem2 solution

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,32 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  // 중복 문자가 있으면 중복을 제거한 문자를 반환
+  function removeDuplicate(word) {
+    let result = ''
+    let start = 0   // 현재 확인하고 있는 character의 인덱스
+    while (start < word.length) {
+      if (word[start] === word[start + 1]) {
+        // 현재 character의 인덱스에서 연속해서 중복된 문자 찾기
+        let idx = start
+        while (word[idx + 1] === word[start]) {
+          idx += 1
+        }
+        start = idx + 1
+      } else {
+        result += word[start]
+        start += 1
+      }
+    }
+    return result
+  }
+
+  // 중복 문자가 없을 때까지 반복
+  while (true) {
+    result = removeDuplicate(cryptogram)
+    if (result === cryptogram) {
+      return result
+    }
+    cryptogram = result
+  }
 }
 
 module.exports = problem2;
