049e017e05172f2975c751ca1436e4dc50ee4a1f
Leemainsw <s2018s15@e-mirim.hs.kr>
2022-11-01 05:08:33 +0900

    FEAT : problem4 - 필요한 상수 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,9 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  // 알파벳 상수를 선언합니다.
+  const ALPHABET = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
+  const ALPHABET_REVERSE = ALPHABET.slice().reverse();
+  // 알파벳 정규식 선언
+  const reg = /^[a-zA-Z]*$/;
 }
 
 module.exports = problem4;
