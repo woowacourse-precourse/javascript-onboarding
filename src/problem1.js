b830d132e632e15cd56c03e8b4f0daa9c0563514
letsjo <hyunoh.jo@gmail.com>
2022-10-31 13:33:10 +0900

    [problem1] Simplify validationArr function for make readability

--- src/problem1.js
+++ src/problem1.js
@@ -1,44 +1,44 @@
 function problem1(pobi, crong) {
-  if(validationArr(pobi) && validationArr(crong)){  
+  if (validationArr(pobi) && validationArr(crong)) {
     return pageGameApp(pobi, crong);
   }
   return -1;
 }
 
-function validationArr(arr){
-  return arr[0]>400||arr[1]>400||arr[0]<1||arr[1]<1?false:arr[1]-arr[0] == 1?true:false;
+function validationArr(arr) {
+  return !arr.some((page) => 1 > page || page > 400) && arr[1] - arr[0] == 1 ? true : false;
 }
 
-function pageGameApp(numArrA,numArrB){
-  let maxNumA = maxValueCalculator([splitNumber(numArrA[0]),splitNumber(numArrA[1])]);
-  let maxNumB = maxValueCalculator([splitNumber(numArrB[0]),splitNumber(numArrB[1])]);
-  return maxNumA==maxNumB?0:maxNumA>maxNumB?1:2; 
+function pageGameApp(numArrA, numArrB) {
+  let maxNumA = maxValueCalculator([splitNumber(numArrA[0]), splitNumber(numArrA[1])]);
+  let maxNumB = maxValueCalculator([splitNumber(numArrB[0]), splitNumber(numArrB[1])]);
+  return maxNumA == maxNumB ? 0 : maxNumA > maxNumB ? 1 : 2;
 }
 
-function maxValueCalculator(arr){
-  return Math.max(sumArr(arr[0]),mulArr(arr[0]),sumArr(arr[1]),mulArr(arr[1]));
+function maxValueCalculator(arr) {
+  return Math.max(sumArr(arr[0]), mulArr(arr[0]), sumArr(arr[1]), mulArr(arr[1]));
 }
 
-function splitNumber(n){
-  const splitArr=[];
-  while(n>0) {
-    splitArr.unshift(n%10);
-    n = parseInt(n/10);
+function splitNumber(n) {
+  const splitArr = [];
+  while (n > 0) {
+    splitArr.unshift(n % 10);
+    n = parseInt(n / 10);
   }
   return splitArr;
 }
 
-function sumArr(arr){
+function sumArr(arr) {
   let totalSum = 0;
-  for(let i=0; i<arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     totalSum += arr[i];
   }
   return totalSum;
 }
 
-function mulArr(arr){
+function mulArr(arr) {
   let totalMul = 1;
-  for(let i=0; i<arr.length; i++){
+  for (let i = 0; i < arr.length; i++) {
     totalMul *= arr[i];
   }
   return totalMul;
