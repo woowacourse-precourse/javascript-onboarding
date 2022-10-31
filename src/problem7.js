3e4d1ad156c4b3e20a957257be950012fde10dce
Suyoung <jsy8492@gmail.com>
2022-10-31 12:34:27 +0900

    feat(problem7): 사용자와 함께 아는 친구 점수 계산 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,92 @@
+function getRecommendedFriendList({ user, friends, visitors }) {
+  const friendInfo = getFriendInfo(friends);
+  const recommendScoreInfo = getRecommendScoreInfo({
+    user,
+    friendInfo,
+    visitors,
+  });
+}
+
+function getFriendInfo(friends) {
+  const friendInfo = {};
+
+  friends.forEach(([id_A, id_B]) => {
+    addFriend({ userId: id_A, friendId: id_B }, friendInfo);
+    addFriend({ userId: id_B, friendId: id_A }, friendInfo);
+  });
+
+  return friendInfo;
+}
+
+function addFriend({ userId, friendId }, friendInfo) {
+  if (friendInfo[userId]) {
+    friendInfo[userId].push(friendId);
+  } else {
+    friendInfo[userId] = [friendId];
+  }
+}
+
+function getRecommendScoreInfo({ user, friendInfo, visitors }) {
+  const recommendScoreInfo = {};
+
+  calcRecommendScore(
+    { data: { user, friendInfo }, type: 'FRIENDS' },
+    recommendScoreInfo,
+  );
+
+  // calcRecommendScore(
+  //   { data: { user, visitors }, type: 'VISITORS' },
+  //   recommendScoreInfo,
+  // );
+
+  console.log(recommendScoreInfo);
+
+  return recommendScoreInfo;
+}
+
+function calcRecommendScore({ data, type }, recommendScoreInfo) {
+  if (type === 'FRIENDS') {
+    const { user, friendInfo } = data;
+    const userFriends = friendInfo[user];
+
+    Object.entries(friendInfo).forEach(([userId, friends]) => {
+      friends.forEach((friendId) => {
+        if (userId !== user) {
+          if (userFriends.includes(friendId)) {
+            if (recommendScoreInfo[userId]) {
+              recommendScoreInfo[userId] += 10;
+            } else {
+              recommendScoreInfo[userId] = 10;
+            }
+          }
+        }
+      });
+    });
+  }
+
+  // if (type === 'VISITORS') {
+  // }
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const answer = getRecommendedFriendList({ user, friends, visitors });
+
   return answer;
 }
+
+console.log(
+  problem7(
+    'mrko',
+    [
+      ['donut', 'andole'],
+      ['donut', 'jun'],
+      ['donut', 'mrko'],
+      ['shakevan', 'andole'],
+      ['shakevan', 'jun'],
+      ['shakevan', 'mrko'],
+    ],
+    ['bedi', 'bedi', 'donut', 'bedi', 'shakevan'],
+  ),
+);
 
 module.exports = problem7;
