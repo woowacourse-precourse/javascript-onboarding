8694ada7e60502a8c46dd24f6f95958def0ffd8d
cocoa <jwv0323@kau.kr>
2022-11-01 16:35:15 +0900

    problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,9 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let newWord = [...word].map((letter) => 
+  (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) ? String.fromCharCode(155 - letter.charCodeAt(0)) : 
+  (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122) ? String.fromCharCode(219 - letter.charCodeAt(0)) : letter);
+
+  return newWord.join('');
 }
 
 module.exports = problem4;
