ce0403bd3cf5bae2c0e04dc064a49a0485606faa
HyeJun <solar3070@naver.com>
2022-11-01 20:44:21 +0900

    feat: problem7/친구 관계를 키-값으로 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,9 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendMap = new Map();
+  friends.forEach(([f1, f2]) => {
+    friendMap.set(f1, friendMap.get(f1) ? [...friendMap.get(f1), f2] : [f2]);
+    friendMap.set(f2, friendMap.get(f2) ? [...friendMap.get(f2), f1] : [f1]);
+  });
 }
 
 module.exports = problem7;
