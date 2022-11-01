aab6bbfb0f6ec8fd77eea8fa4c63c790ceb3975e
OrangeChick <retww@naver.com>
2022-11-01 21:32:23 +0900

    feat: 🎒[Problem6] Put two consecutive letters into an array
    - 연속된 글자를 추출하여 배열에 담아주었습니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
 function problem6(forms) {
   var answer;
+
+  let twoWord = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length; j++) {
+      twoWord.push(forms[i][1].slice(j, j + 2));
+    }
+  }
+
   return answer;
 }
 
