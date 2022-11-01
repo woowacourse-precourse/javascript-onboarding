18b8010b021f7da53d3d54c67cc3ad802a13a9ab
DAIN KIM <dnkim419@gmail.com>
2022-11-01 17:45:31 +0900

    feat: solved problem7

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,37 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  let graph = {};
+  let candidate = {};
+
+  for (let [a, b] in friends) {
+    graph[a] ? graph[a].push(b) : (graph[a] = [b]);
+    graph[b] ? graph[b].push(a) : (graph[b] = [a]);
+  }
+
+  for (let i in graph[user]) {
+    for (let j in graph[i]) {
+      if (graph[user].includes(j)) continue;
+      candidate[j] ? (candidate[j] += 10) : (candidate[j] = 10);
+    }
+  }
+
+  for (let i in visitors) {
+    candidate[i] ? (candidate[i] += 1) : (candidate[i] = 1);
+  }
+
+  let [
+    first = ["", 0],
+    second = ["", 0],
+    third = ["", 0],
+    fourth = ["", 0],
+    fifth = ["", 0],
+  ] = Array.from(candidate)
+    .sort((a, b) => a[1] - b[1])
+    .reverse();
+
+  for (let [userName, score] in [first, second, third, fourth, fifth]) {
+    if (score !== 0) answer.push(userName);
+  }
   return answer;
 }
 
