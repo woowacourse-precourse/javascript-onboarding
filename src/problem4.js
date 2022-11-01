27d01a04a3f60766046d6100250c517c91b2b862
Hayeong Kim <fibonacci2246@gmail.com>
2022-11-01 19:04:17 +0900

    problem 4 solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,27 @@
+function checkCase(word) {
+  if (word.charCodeAt() >= 65 && word.charCodeAt() <= 90) {
+    return "upper";
+  } else if (word.charCodeAt() >= 97 && word.charCodeAt() <= 122) {
+    return "lower";
+  }
+}
+
 function problem4(word) {
-  var answer;
+  let answer = "";
+
+  const wordArr = word.split("");
+
+  wordArr.forEach((word) => {
+    const caseCheck = checkCase(word);
+    if (caseCheck === "upper") {
+      answer += String.fromCharCode(155 - word.charCodeAt());
+    } else if (caseCheck === "lower") {
+      answer += String.fromCharCode(219 - word.charCodeAt());
+    } else {
+      answer += word;
+    }
+  });
+
   return answer;
 }
 
