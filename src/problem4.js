6c4f34637e2d45f1e4101e2bb2978f18ad931878
7ngenious <zxvs1218@gmail.com>
2022-11-01 15:28:13 +0900

    feat: [Problem4] 1st Soltion

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
-  var answer;
+  let answer = '';
+
+  for (let i of word) {
+    const convertWord = i.charCodeAt();
+    let resultWord = '';
+    if (convertWord >= 65 && convertWord <= 90)
+      resultWord = 90 - convertWord + 65;
+    else if (convertWord >= 97 && convertWord <= 122)
+      resultWord = 122 - convertWord + 97;
+    else resultWord = convertWord;
+    answer = answer + String.fromCharCode(resultWord);
+  }
+
   return answer;
 }
 
