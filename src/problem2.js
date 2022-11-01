3f2ddcb6061a04771d823276bc09c05840316253
윤동주 <yoondj98@naver.com>
2022-11-01 21:17:38 +0900

    우아한 테크코스 프리코슥 1주차 2번 코드 주석 기입

--- src/problem2.js
+++ src/problem2.js
@@ -1,33 +1,28 @@
 function problem2(cryptogram) {
   var input = cryptogram.toString();
-  if(input.length == 1) return answer;
-  while(true){
+  if (input.length == 1) return answer;
+  while (true) {
     var answer = "";
-    var count = 0;
-    for(i=0; i<input.length; i++){
-      if(i == 0){
-        if(input[i] == input[i+1]) {
+    var count = 0; /* 문자열을 순회할 때마다 삭제되는 경우의 개수를 저장 */
+    for (i = 0; i < input.length; i++) {
+      if (i == 0) { /* 맨 앞자리의 경우 비교 */
+        if (input[i] == input[i + 1]) {
           count++;
           continue;
-        }
-        else answer = answer.concat(input[0]); 
-      }
-      else if(i == input.length-1){
-        if(input[i] == input[i-1]) {
+        } else answer = answer.concat(input[0]);
+      } else if (i == input.length - 1) { /* 맨 뒤자리의 경우 비교 */
+        if (input[i] == input[i - 1]) {
           count++;
           continue;
-        }
-        else answer = answer.concat(input[i]);
-      }
-      else{
-        if(input[i] == input[i-1] || input[i] == input[i+1]) {
+        } else answer = answer.concat(input[i]);
+      } else { /* 맨 앞 끝이 아닌 경우 비교 */
+        if (input[i] == input[i - 1] || input[i] == input[i + 1]) {
           count++;
           continue;
-        }
-        else answer = answer.concat(input[i]);
+        } else answer = answer.concat(input[i]);
       }
     }
-    if (count == 0) break;
+    if (count == 0) break; /* 변경되는게 없다는 것은 암호가 모두 해독된 것이니 해독된 값을 리턴 */
     input = answer;
   }
   return answer;
