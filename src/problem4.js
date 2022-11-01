68a5cdaa29262fd52c1aa848644aae93b46d1f4b
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-01 20:54:45 +0900

    chore :  problem4 주석 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,4 @@
+// 객체를 생성해주는 함수
 const makeObject = (str, frogStr) => {
     const obj = {};
 
@@ -13,10 +14,12 @@ function problem4(word) {
     const alphabet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
     const frog = alphabet.split('').reverse().join('');
 
+    // 대문자, 소문자 각각 객체를 만든다.
     const uppercase = makeObject(alphabet, frog);
     const lowercase = makeObject(alphabet.toLowerCase(), frog.toLowerCase());
 
     for (let x of word) {
+        // 객체 내부에 없는  단어에 대한 처리 (ex. 빈문자열, 특수문자)
         if (!uppercase.hasOwnProperty(x) && !lowercase.hasOwnProperty(x)) {
             answer += x;
         } else if (x === x.toLowerCase()) {
