24d49f06d4032343cdad3a82c761f0e850f6bc76
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 21:56:47 +0900

    feat: Add setDefault func in problem7.js
    파이썬의 setDefault와 같은 역할을 하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,17 @@
 function problem7(user, friends, visitors) {
+  const connected =  new Object();
   var answer;
   return answer;
+}
+
+function setDefault(target, key, value) {
+  if (target.hasOwnProperty(key)) {
+    target[key].add(value);
+    return target;
+  }
+  target[key] = new Set();
+  target[key].add(value);
+  return target;
 }
 
 module.exports = problem7;
