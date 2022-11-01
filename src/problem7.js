4218e5a3ff2124d2db852e37906616ab6026527e
ming0830 <minsun0830@naver.com>
2022-11-01 14:39:35 +0900

    [PROBLEM7] feat : 사용자와 함께 아는 친구 목록을 구해 10점씩 부과하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -4,13 +4,13 @@ function problem7(user, friends, visitors) {
 }
 
 //user의 친구 목록
-function getUserFriends(user,friends){
+function getUserFriends(userName,friendsList){
   let userFriends = [];
-  for(let friendSet of friends){
-    if(friendSet.includes(user)){
-    if(friendSet[0] === user){
+  for(let friendSet of friendsList){
+    if(friendSet.includes(userName)){
+    if(friendSet[0] === userName){
     userFriends.push(friendSet[1]);
-    } else if(friendSet[1] === user){
+    } else if(friendSet[1] === userName){
     userFriends.push(friendSet[0]);
     }
     }
@@ -18,12 +18,33 @@ function getUserFriends(user,friends){
   return userFriends
 }
 
-function getFrindOfFriends(friends){
+function getScore(friends){
+  // user친구의 친구 목록
+  const myFriends = getUserFriends(user,friends);
+  let  friendOfFriendsSet= [];
+  for(let friend of myFriends){ 
+    friendOfFriendsSet.push(getUserFriends(friend,friends));
+    
+  }
+  // 친구의 친구목록에서 user이름 제외
+  let friendOfFriends = [];
+  for(let ele of friendOfFriendsSet.flat()){
+    if(!ele.includes(user)) friendOfFriends.push(ele)
+  }
+  
+  // 함께 아는 친구에게 10점씩 부여
+  let score = {};
+  friendOfFriends.forEach((x)=>{
+    score[x] = (score[x] || 0)+10;
+  });
 
+  console.log(JSON.stringify(score))
+  
 }
+
 const user = "mrko";
 const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]  ;
 const visitors =  ["bedi", "bedi", "donut", "bedi", "shakevan"];
-getUserFriends(user,friends)
+getScore(friends)
 module.exports = problem7;
 
