0a3e7caa97b0360a804440475806b0da7ad4986e
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-01 16:40:21 +0900

    feat: [problem3] 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,27 @@
 function problem3(number) {
   var answer;
+  let count = 0;
+
+  for (let i = 1; i <= number; i++) {
+    const currentNum = i.toString().trim().split("").map(Number);
+
+    //3,6,9 포함 개수 count
+    for (let j = 0; j < currentNum.length; j++) {
+      if (currentNum[j] == 3) {
+        count++;
+      }
+      if (currentNum[j] == 6) {
+        count++;
+      }
+      if (currentNum[j] == 9) {
+        count++;
+      }
+    }
+    //console.log(currentNum);
+  }
+
+  console.log(count);
+  answer = count;
   return answer;
 }
 
