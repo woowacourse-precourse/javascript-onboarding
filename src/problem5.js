ffb73bc4564d29afee0a2e6043f4ed364378d53a
ekkim <eklikeable@kakao.com>
2022-11-01 15:51:17 +0900

    🎨 result 배열의 길이 하드코딩을 변수로 수정

--- src/problem5.js
+++ src/problem5.js
@@ -7,7 +7,7 @@
 
 function problem5(money) {
   const CURRENCIES = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let result = Array.from({ length: 9 }, () => 0);
+  let result = Array.from({ length: CURRENCIES.length }, () => 0);
 
   for (let i = 0; i < CURRENCIES.length; i++) {
     result[i] = Math.floor(money / CURRENCIES[i]);
