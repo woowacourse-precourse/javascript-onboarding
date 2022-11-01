ed9bf0a5fd527a04daad8ef33016556ed6980b75
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:33:48 +0900

    [problem3] feat: 반복문을 돌며 숫자를 배열로 쪼개기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,9 @@
 function problem3(number) {
   var answer;
   return answer;
+  let cnt = 0;
+  for (i = 1; i <= number; i++)
+    (i + "").split("").forEach(function (num) {
 }
 
 module.exports = problem3;
