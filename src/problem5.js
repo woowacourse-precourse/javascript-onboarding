45cb1c65603cb0e4a48408b1f9ff524026fe2a7c
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 13:08:51 +0900

    feat:[문제5] bank.forEach를 통해 변환가능한지 확인한다

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
 function problem5(money) {
   let safe = { moneyExchanged: [], change: money };
   const bank = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  bank.forEach((cashInBank) => {});
   return safe.moneyExchanged;
 }
 
