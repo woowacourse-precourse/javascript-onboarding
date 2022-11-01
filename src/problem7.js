8aaece3163869838f5e85aba68a2fdb6554f4b09
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-01 23:24:52 +0900

    fix: 문제7 점수 정렬이 적용되지 않는 문제 해결

--- src/problem7.js
+++ src/problem7.js
@@ -1,15 +1,3 @@
-function sortScore(a, b) {
-  const [nameA, scoreA] = a;
-  const [nameB, scoreB] = b;
-  if (scoreA < scoreB) {
-    return 1;
-  } else if (scoreA > scoreB) {
-    return -1;
-  } else if (scoreA === scoreB) {
-    return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
-  }
-}
-
 function problem7(user, friends, visitors) {
   let answer;
   const relationMap = new Map();
@@ -58,7 +46,15 @@ function problem7(user, friends, visitors) {
   const recommendationList = Array.from(scoreMap)
     .filter(([_, score]) => score > 0)
     .sort((a, b) => {
-      sortScore(a, b);
+      const [nameA, scoreA] = a;
+      const [nameB, scoreB] = b;
+      if (scoreA < scoreB) {
+        return 1;
+      } else if (scoreA > scoreB) {
+        return -1;
+      } else if (scoreA === scoreB) {
+        return nameA < nameB ? -1 : nameA > nameB ? 1 : 0;
+      }
     })
     .slice(0, 5);
 
