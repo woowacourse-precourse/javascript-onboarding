653eca1f483546c177fa5e7f67786bf4fa482585
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 10:39:25 +0900

    Problem3: 기능 목록 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,9 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  /*
+  기능 목록
+  1. number에 3, 6, 9가 들어가면 해당 숫자의 개수만큼 손뼉수 더하기
+  2. 전체 손뼉수 정답으로 반환
+  */
 }
 
 module.exports = problem3;
