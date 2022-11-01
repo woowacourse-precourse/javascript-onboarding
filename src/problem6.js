821baf8b1e673a0096cdbcad522bb68e4e574d69
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:51:06 +0900

    feat(prob6) : filter 통해 겹치는 form 추출

--- src/problem6.js
+++ src/problem6.js
@@ -24,6 +24,20 @@ function problem6(forms) {
   forms.forEach((v, i) => {
     setNicknameMap(nickNameMap, v[1]);
   });
+  let overlapNickname = filterOverlap(nickNameMap);
+
+  answer = forms
+    .filter((form) => {
+      for (let i = 0; i < form[1].length - 1; i++) {
+        let temp = form[1].slice(i, i + 2);
+        for (let j = 0; j < overlapNickname.length; j++) {
+          if (overlapNickname[j].includes(temp)) return true;
+        }
+      }
+      return false;
+    })
+    .map((f) => f[0]);
+
   return answer;
 }
 
