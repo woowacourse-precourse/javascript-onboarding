6c029051ced3f706d9965b1e15d66f4a7276f31f
n0eyes <doll4586633@naver.com>
2022-11-01 17:52:19 +0900

    feat: [P3] 숫자에 3,6,9가 몇 개인지 카운트하는 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -2,6 +2,13 @@ function check369(num) {
   return num === 3 || num === 6 || num === 9 ? true : false;
 }
 
+function count369(num) {
+  return num
+    .toString()
+    .split("")
+    .reduce((acc, cur) => (check369(parseInt(cur)) ? (acc += 1) : acc), 0);
+}
+
 function problem3(number) {
   var answer;
   return answer;
