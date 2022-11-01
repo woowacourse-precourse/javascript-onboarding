1b40e89caa5fb556fcccb51dbacff41e44005a74
leesoyuun <leesoyun4206@naver.com>
2022-11-01 19:26:24 +0900

    [Problem 3] 손뼉치는 횟수 구하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var clap = 0;
+  for (let i = 1; i <= number; i++) {
+      let split_nums = i.toString().split("");
+      for (let num of split_nums) {
+          if (num === "3" || num === "6" || num === "9") {
+            clap++;
+          }
+      }
+  }
+  return clap;
 }
 
 module.exports = problem3;
