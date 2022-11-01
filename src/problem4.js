269be593c737fb27e98bae062516c770f92a5780
Sangmin Lee <gink.ss00@gmail.com>
2022-11-01 16:37:15 +0900

    [problem4] feat: reverse 함수 구현
    청개구리 사전에 맞춰 입력값을 변환해준다.

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,25 @@ function isAlphabet(letter) {
   return /[a-zA-Z]/.test(letter);
 }
 
+function reverse(word) {
+  let reverseWord = "";
+
+  for (let i = 0; i < word.length; i++) {
+    if (isAlphabet(word[i])) {
+      word.charCodeAt(i) < 97
+        ? (reverseWord = reverseWord.concat(
+            String.fromCharCode(155 - word.charCodeAt(i))
+          ))
+        : (reverseWord = reverseWord.concat(
+            String.fromCharCode(219 - word.charCodeAt(i))
+          ));
+    } else {
+      reverseWord = reverseWord.concat(word[i]);
+    }
+  }
+  return reverseWord;
+}
+
 function problem4(word) {
   var answer;
   return answer;
