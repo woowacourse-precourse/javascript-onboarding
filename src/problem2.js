d5976d5c2caf494cd8c9a7cac5bc680714cfe16a
rickyshu <hoinleekk@naver.com>
2022-11-01 17:04:45 +0900

    problem2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let stack = [];
+  for (let i = 0; i < cryptogram.length; i++) {
+    let current = cryptogram[i];
+    if (stack.length === 0) {
+      stack.push(current);
+      continue;
+    } else {
+      let previous = stack[stack.length - 1];
+
+      if (previous === current) {
+        stack.pop();
+      } else {
+        stack.push(current);
+      }
+    }
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
