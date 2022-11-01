abd9b8fccef1f6e696182bc9b74af6edc74aed4d
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 15:25:43 +0900

    feat:[문제6] name을 하나씩 받아서 연속적으로 쪼갠다 단 최소길이는 2이상이다

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
+function splitNameIntoTwoOrMoreLengths(name) {
+  const arrayOfSplitNames = [];
+  for (k = 2; k <= name.length; k++) {
+    let splitName = name.split("").reduce((acc, _, i, arr) => {
+      if (arr.slice(i, i + k).join("").length >= k) {
+        acc.push(arr.slice(i, i + k).join(""));
+      }
+      return acc;
+    }, []);
+    arrayOfSplitNames.push(...splitName);
+  }
+  return arrayOfSplitNames;
+}
+
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  let emailOfPersonWithDuplicateName;
+  forms.forEach(([email, name], _, arr) => {
+    splitNameIntoTwoOrMoreLengths(name);
+  });
+  return;
 }
 
 module.exports = problem6;
