6d0d9020f98f6271b39bbf5567f7541cfa5f6bcb
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 20:45:39 +0900

    problem4 solution

--- src/problem4.js
+++ src/problem4.js
@@ -14,19 +14,12 @@ function solution(word) {
   var result = "";
   for (var i=0; i<word.length; i++) {
     var numChar = word.charAt(i).charCodeAt(0);
-    console.log("numChar: ", numChar)
     if (numChar <= 32) {
-      console.log("if Space: ", word.charAt(i))
       result = result + " ";
-      console.log("if Result: ", word.charAt(i))
     } else if (word.charAt(i) === word.charAt(i).toUpperCase()) {
-      console.log("if Upper: ", word.charAt(i));
       result = result + String.fromCharCode((90 - (numChar-65)));
-      console.log("Upper Result: ", String.fromCharCode((90 - (numChar-65))))
     } else if (word.charAt(i) === word.charAt(i).toLowerCase()) {
-      console.log("if Lower: ", word.charAt(i))
       result = result + String.fromCharCode((122 - (numChar-97)));
-      console.log("Lower Result: ", String.fromCharCode((90 - (numChar-65))))
     } 
   }
   return result;
