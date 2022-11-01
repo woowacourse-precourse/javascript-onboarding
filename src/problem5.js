a5435adafddbbdca783c4b141be3151329da6166
Minhye Kang <alsgp9898@naver.com>
2022-11-01 13:33:30 +0900

    feat(PROBLEM5): 화폐 배열 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,7 @@
-function problem5(money) {
-  var answer;
-  return answer;
-}
+function problem5(money) {}
+
+const BILL_ARRAY = Object.freeze([
+  50000, 10000, 5000, 1000, 500, 100, 50, 10, 1,
+]);
 
 module.exports = problem5;
