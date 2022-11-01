834eaf71070f96ae47a9f5933c217d4d2147123c
shinhyunsu01 <shine.master9687@gmail.com>
2022-11-01 12:59:28 +0900

    1 solve

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,31 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+	const getTopNum = (arr) => {
+		const array = arr.map((a) => String(a));
+		const firstNum = array[1];
+		const [fir, sec, thi] = firstNum;
+		const [first, second, third] = [+fir, +sec, +thi];
+
+		if (array[1] - array[0] !== 1) return -1;
+		if (!second) return firstNum;
+
+		const multiple = third ? first * second * third : first * second;
+		const plus = third ? first + second + third : first + second;
+
+		return multiple < plus ? plus : multiple;
+	};
+
+	const pobiNum = getTopNum(pobi);
+	const crongNum = getTopNum(crong);
+
+	return pobiNum === -1 || crongNum === -1
+		? -1
+		: pobiNum > crongNum
+		? 1
+		: pobiNum < crongNum
+		? 2
+		: pobiNum === crongNum
+		? 0
+		: undefined;
 }
 
 module.exports = problem1;
