61bfeade65fcb17777f6b5788b89a27a92bd64f8
HYEOK9 <leejaehyuck9@gmail.com>
2022-11-01 12:36:56 +0900

    feat:solve problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    let answer = 0;
+    for (let i = 1; i <= number; i++) {
+        for (let j = 0; j < i.toString().length; j++) {
+            let char = i.toString()[j];
+            if (char === "3" || char === "6" || char === "9") answer++;
+        }
+    }
+    return answer;
 }
 
 module.exports = problem3;
