314080cdbb8ff8ffacd49d537e452f1cea0dc7a9
dhkham <khamdong@gmail.com>
2022-11-01 16:12:25 +0900

    feat: (prob_7) add function that takes in user, friends, visitors and returns the entire name list except the user

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,23 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+// function that returns the entire name list except the user
+function getNameList(user, friends, visitors) {
+  const friendName = [];
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < friends[i].length; j++) {
+      if (friendName.indexOf(friends[i][j]) === -1 && friends[i][j] !== user) {
+        friendName.push(friends[i][j]);
+      }
+    }
+  }
+
+  for (let i = 0; i < visitors.length; i++) {
+    if (friendName.indexOf(visitors[i]) === -1) {
+      friendName.push(visitors[i]);
+    }
+  }
+
+  return friendName;
+}
