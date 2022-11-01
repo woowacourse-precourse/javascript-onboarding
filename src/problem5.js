e42f2340026bcf4e7b6ffc1eb40c68a81789783e
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-01 11:06:22 +0900

    refactor: [PROBLEM5] moneyTemp 변수 삭제

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0];
   let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10];
 
   for (let i = 0; i < unit.length; i++) {
-    let moneyTemp = money % unit[i];
-    money = moneyTemp;
-    console.log(moneyTemp === money);
+    money = money % unit[i];
   }
+
   return answer;
 }
 
