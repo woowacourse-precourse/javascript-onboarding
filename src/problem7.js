55d765e1a28ba59ae0bd337f13115b3075f8c248
Sallyvely <tpdus4159@naver.com>
2022-11-01 21:29:06 +0900

    7번 문제 추천 친구 계산하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer=[];
+  var userFriends=[];
+  let score= new Map();
+  for(let i=0; i<friends.length;i++){
+
+    if(score.has(friends[i][1])){
+      score.set(friends[i][1], score.get(friends[i][1])+10);
+    }
+    else score.set(friends[i][1], 10);
+    if(friends[i][1].includes(user)){
+      score.delete(user);
+      userFriends.push(friends[i][0]);
+    }
+  }
+
+
   return answer;
 }
-
 module.exports = problem7;
