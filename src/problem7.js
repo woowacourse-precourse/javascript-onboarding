f8386c54469488f448aa23ecfa23106ea272512c
yeeun426 <thsudkcla7@naver.com>
2022-11-01 22:44:22 +0900

    feat: 문제 7번 - 사용자와 함께 아는 친구의 수 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,20 +1,42 @@
 function problem7(user, friends, visitors) {
   var answer;
- // user의 이미 친구인 사람 찾기
- let alreadyFriends = [];
 
+ // user의 이미 친구인 경우
+ let alreadyFriends = {};
  for (i = 0; i < friends.length; i++){
    if (friends[i].includes(user)) {
      var friend = friends[i].filter(v => v !== user);
-     console.log(friend);
-
      alreadyFriends[friend] = [];
    }
  }
+ console.log(alreadyFriends);
 
- return answer;
+ // alreadyFriends와 친구인 경우 -> 10점
+ var alreadyRecommend = [];
+ var usersFriendsName = Object.keys(alreadyFriends);
+
+ for (i = 0; i < usersFriendsName.length; i++) {
+   var userFriend = usersFriendsName[i]; //userfriend = donut
+   for (j = 0; j < friends.length; j++){
+     if (friends[j].includes(userFriend) && !friends[j].includes(user)) {
+       var friend = friends[j].filter((name) => name !== userFriend);
+       alreadyFriends[userFriend].push(...friend);
+       if (alreadyRecommend[friend] !== undefined) {
+        alreadyRecommend[friend] += 10;
+        } else {
+        alreadyRecommend[friend] = 0;
+       }
+     }
+   }
+ }
+
+ // 사용자의 타임 라인에 방문한 횟수
+
+  return answer;
 
 }
+
+
 
 // module.exports = problem7;
 console.log(problem7(
