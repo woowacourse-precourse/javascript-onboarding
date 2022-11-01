481037d40511b57615f9e8fbb79ab1d20139a0be
[SW2/서울/1반] 정유진 <yearn8546@gmail.com>
2022-11-01 11:35:08 +0900

    문제7) feat: friends배열로 친구 관계를 Key:[value1, value2..] 형태로 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,30 @@
 function problem7(user, friends, visitors) {
   var answer;
+
   return answer;
 }
+
+const getFriendsRelation = (friends) => {
+  let friendsRelation = {};
+
+  friends.forEach((relation) => {
+    const friendA = relation[0];
+    const friendB = relation[1];
+
+    if (!friendsRelation[friendA]) {
+      friendsRelation[friendA] = [friendB];
+    } else {
+      friendsRelation[friendA].push(friendB);
+    }
+
+    if (!friendsRelation[friendB]) {
+      friendsRelation[friendB] = [friendA];
+    } else {
+      friendsRelation[friendB].push(friendA);
+    }
+  });
+
+  return friendsRelation;
+};
 
 module.exports = problem7;
