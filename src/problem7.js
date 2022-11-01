31133625dc4bae0c9d833afbabab569df2984f3f
ho-ji <wkdd0205@gmail.com>
2022-11-01 20:35:25 +0900

    feat: 문제7 기능 구현
    변수, 자료구조 선언

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,14 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  let cntFriends = 0;
+  let listFriendship = [];
+  listFriendship[0] = [];
+  let score = new Map();
+  score.set(user, -1);
+  let numberOfFriend = new Map();
+  numberOfFriend.set(user, cntFriends);
+  cntFriends++;
+
   return answer;
 }
 
