223bf8f3d84c2039da55bba0b1471cab7c9a9bad
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:40:30 +0900

    feat(problem2): 새로운 문자열을 다시 확인한다

--- src/problem2.js
+++ src/problem2.js
@@ -3,7 +3,12 @@ function problem2(cryptogram) {
   while (i < cryptogram.length) {
     if (i + 1 < cryptogram.length && cryptogram[i] === cryptogram[i + 1]) {
       cryptogram = cryptogram.slice(0, i) + cryptogram.slice(i + 2, cryptogram.length);
+      if (i === 0) continue;
+      i -= 1;
+    } else {
+      i++;
     }
   }
+  return cryptogram;
 }
 module.exports = problem2;
