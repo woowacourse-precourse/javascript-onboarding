2495c981ce5bf08e07682176a4a206f32f087c33
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 17:02:58 +0900

    feat : 반복문을 통하여 순차적으로 닉네임에서 추출할 수 있는 단어들을 추출하고, 중복검사를 실시하는 함수 구현.

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,31 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    var answer = new Set();
+
+    for (let i = 0; i < forms.length; i++) {
+        let nickName = forms[i][1];
+        for (let j = 2; j <= nickName.length; j++) {
+            for (let k = 0; k <= nickName.length - j; k++) {
+                let extractWord = nickName.substring(k, k + j);
+                checkDuplicate(forms, extractWord, i, answer);
+            }
+        }
+    }
+
+    return sortArray(answer);
 }
 
+function sortArray(answer) {
+    return Array.from(answer).sort();
+}
+
+function checkDuplicate(forms, checkWord, index, answer) {
+    for (let i = 0; i < forms.length; i++) {
+        if (i === index) continue;
+        else {
+            if (forms[i][1].includes(checkWord)) {
+                answer.add(forms[i][0]);
+            }
+        }
+    }
+}
 module.exports = problem6;
