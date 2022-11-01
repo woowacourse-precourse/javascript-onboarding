1c1ce368e89b7b9348761dddcf68405ad6d7aa62
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 13:35:38 +0900

    feat: Add function which turn a number into array of digits
    숫자를 인자로 받아와 각 자리수를 원소로하는 배열을 반환하는 함수를 구현하였음

--- src/problem3.js
+++ src/problem3.js
@@ -3,4 +3,14 @@ function problem3(number) {
   return answer;
 }
 
+function numIntoDigits(number) {
+  let digits = [];
+  while (number > 0) {
+    digit = number % 10;
+    digits.push(number);
+    number /= 10;
+    number = Math.floor(number);
+  }
+  return digits;
+}
 module.exports = problem3;
