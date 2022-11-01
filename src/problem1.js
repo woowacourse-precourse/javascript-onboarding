44f3166ef3d43b3d8f8db8d230403fa5e863013f
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:16:40 +0900

    feat: [problem1] 각 자리 숫자 합 구하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
   var answer;
   return answer;
+const sumOfEachDigit = (num) => {
+  let sum = 0
+  while (num > 0) {
+    let digit = num % 10
+    num = (num - digit) / 10
+    sum += digit
+  }
+  return sum
+}
 }
 
 module.exports = problem1;
