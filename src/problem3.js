358fe0dfc7854d0caa50657fe428693ebe9a7be0
sumi-0011 <selina2000@naver.com>
2022-11-01 16:31:07 +0900

    [problem3] refactoring : JSDoc 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,17 +1,25 @@
+/**
+ * @param {number} number
+ * @returns {string[]}
+ */
 const getNumberToCharList = (number) => {
   return number.toString().split("");
 };
 
-const get369Count = (arr) => {
-  return arr.filter((c) => c === "3" || c === "6" || c === "9").length;
+/**
+ * @param {number} number
+ * @returns {number} 손뼉 쳐야하는 횟수
+ */
+const get369Count = (number) => {
+  const numbers = getNumberToCharList(number);
+  return numbers.filter((c) => c === "3" || c === "6" || c === "9").length;
 };
 
 function problem3(number) {
   var answer = 0;
 
   for (let i = 1; i <= number; i++) {
-    const chars = getNumberToCharList(i);
-    answer += get369Count(chars);
+    answer += get369Count(i);
   }
 
   return answer;
