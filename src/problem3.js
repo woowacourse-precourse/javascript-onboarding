7d82dba7681a7c572b09e3c42de25af8ad1976ea
mun9769 <mun9769@naver.com>
2022-11-01 11:03:46 +0900

    Update problem3.js
    문제 조건에서 number는 10000 이하인 자연수입니다.
    1만이라는 숫자는 시간복잡도 O(n)으로 풀어도 프로그램실행시간을 지연시키는 숫자가 아닙니다.
    for문을 통해 3,6,9를 세는 방법으로 문제를 해결했습니다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,20 @@
+function countThree(num) {
+  var cnt = 0;
+  while (num > 0) {
+    var one = num % 10;
+    num = Math.floor(num / 10);
+    if (one != 0 && one % 3 == 0) {
+      cnt++;
+    }
+  }
+  return cnt;
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (var i = 1; i <= number; i++) {
+    answer += countThree(i);
+  }
   return answer;
 }
 
