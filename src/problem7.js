c679750f16d8b6074f7e25d9b53096660c865ab9
areumsheep <areumsheep@gmail.com>
2022-11-01 22:56:00 +0900

    refactor: 문제7 친구 관계인 사용자를 먼저 탐색 후 점수 추가하도록 변경

--- src/problem7.js
+++ src/problem7.js
@@ -4,31 +4,24 @@ const SCORE = {
 };
 const MAX_FRIENDS_TO_SHOW = 5;
 
-const sortByAlphabetically = (a = '', b = '') => {
-  const upperCaseA = a.toUpperCase();
-  const upperCaseB = b.toUpperCase();
-
-  if (upperCaseA > upperCaseB) return 1;
-  if (upperCaseA < upperCaseB) return -1;
-  if (upperCaseA === upperCaseB) return 0;
-};
-
 function problem7(user = '', friends = [], visitors = []) {
   const beFriends = {};
-  const alreadyFriends = new Set();
+  const alreadyFriends = getAlreadyFriends(user, friends);
 
-  for (const [alreadyFriend, beFriend] of friends) {
-    if (beFriend === user) continue;
-
-    alreadyFriends.add(alreadyFriend);
-    if (beFriends[beFriend] !== undefined) {
-      beFriends[beFriend] = beFriends[beFriend] + SCORE.ACQUAINTANCE;
-    } else beFriends[beFriend] = SCORE.ACQUAINTANCE;
+  for (const [idA, idB] of friends) {
+    if (!alreadyFriends.has(idA)) {
+      if (beFriends[idA]) beFriends[idA] += SCORE.ACQUAINTANCE;
+      else beFriends[idA] = SCORE.ACQUAINTANCE;
+      continue;
+    } else if (!alreadyFriends.has(idB)) {
+      if (beFriends[idB]) beFriends[idB] += SCORE.ACQUAINTANCE;
+      else beFriends[idB] = SCORE.ACQUAINTANCE;
+      continue;
+    }
   }
 
   for (const visitor of visitors) {
     if (alreadyFriends.has(visitor)) continue;
-
     if (beFriends[visitor]) beFriends[visitor] += SCORE.VISITED_FRIEND;
     else beFriends[visitor] = SCORE.VISITED_FRIEND;
   }
@@ -44,5 +37,27 @@ function problem7(user = '', friends = [], visitors = []) {
     .map(([name, score]) => name)
     .slice(0, MAX_FRIENDS_TO_SHOW);
 }
+
+/** 이미 친구 관계인 사용자 탐색 */
+const getAlreadyFriends = (user = '', friends = []) => {
+  const friendSet = new Set();
+  friends.forEach(([idA, idB]) => {
+    if (idA === user || idB === user) {
+      friendSet.add(idA);
+      friendSet.add(idB);
+    }
+  });
+  return friendSet;
+};
+
+/** 대소문자 관계 없이 문자열 정렬 */
+const sortByAlphabetically = (a = '', b = '') => {
+  const upperCaseA = a.toUpperCase();
+  const upperCaseB = b.toUpperCase();
+
+  if (upperCaseA > upperCaseB) return 1;
+  if (upperCaseA < upperCaseB) return -1;
+  if (upperCaseA === upperCaseB) return 0;
+};
 
 module.exports = problem7;
