612a05ac7002add99e1a55d8e12c0bb79950bd25
yunjjeongjo <two6885@gmail.com>
2022-11-01 17:29:26 +0900

    feat: problem7 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,54 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  let friendsList = new Map();
+  let score = new Map();
+
+  for (let i = 0; i < friends.length; i++) {
+    friendsList.set(
+      friends[i][0],
+      friendsList.get(friends[i][0])
+        ? friendsList.get(friends[i][0]) + "," + friends[i][1]
+        : friends[i][1]
+    );
+    friendsList.set(
+      friends[i][1],
+      friendsList.get(friends[i][1])
+        ? friendsList.get(friends[i][1]) + "," + friends[i][0]
+        : friends[i][0]
+    );
+  }
+
+  let userFriends = friendsList.get(user).split(",");
+
+  userFriends.forEach((friend) => {
+    let acquaintance = friendsList.get(friend).split(",");
+    for (let i = 0; i < acquaintance.length; i++) {
+      if (acquaintance[i] !== user) {
+        score.set(
+          acquaintance[i],
+          score.get(acquaintance[i]) ? score.get(acquaintance[i]) + 5 : 5
+        );
+      }
+    }
+  });
+
+  visitors.forEach((visitor) => {
+    if (!userFriends.includes(visitor)) {
+      score.set(visitor, score.get(visitor) ? score.get(visitor) + 1 : 1);
+    }
+  });
+
+  const sortedScore = [...score.entries()];
+  sortedScore
+    .sort((a, b) => {
+      return b[1] - a[1];
+    })
+    .forEach((arr) => {
+      if (arr[1] > 0 && answer.length < 5) {
+        answer.push(arr[0]);
+      }
+    });
+
   return answer;
 }
 
