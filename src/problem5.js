70137e767d5086d98f7a578f4ba774e7a36b8bfa
HyeJun <solar3070@naver.com>
2022-11-01 19:25:20 +0900

    feat: problem5/최대 화폐부터 돈을 나누며 몫 구하고 배열에 값 담기 돈은 해당 화폐로 나누고 난 나머지 값으로 바꾸고 이 과정을 반복

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
   const unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  return Array(9).fill(0).map((e, i) => {
+    const n = Math.floor(money / unit[i]);
+    money %= unit[i]
+    return n;
+  });
 }
 
 module.exports = problem5;
