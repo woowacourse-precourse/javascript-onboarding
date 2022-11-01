43275ca630156d9ad8064d0892d70d3ab19e86ce
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 18:58:31 +0900

    feat : stack의 개념을 이용하여 함수구현.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,36 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+    var strArray = cryptogram.split('');
+    let stack = [];
+    let prevLength;
+
+    do {
+        prevLength = strArray.length;
+        stack = [];
+        let duplicateCount = 0;
+
+        for (let i = 0; i < strArray.length; i++) {
+            let peek = stack[stack.length - 1];
+
+            if (stack.length === 0) {
+                stack.push(strArray[i]);
+                continue;
+            }
+
+            if (peek !== strArray[i]) {
+                if (duplicateCount > 0) {
+                    stack.pop();
+                    duplicateCount = 0;
+                }
+                stack.push(strArray[i]);
+            } else {
+                duplicateCount++;
+            }
+        }
+        if (duplicateCount > 0) stack.pop();
+        strArray = stack;
+    } while (prevLength !== stack.length);
+
+    return strArray.join('');
 }
 
 module.exports = problem2;
