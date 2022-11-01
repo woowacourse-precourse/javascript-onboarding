4ddce6239d209b8ae358aa74cf31c37d991b1351
EunjE <dldmswp3@naver.com>
2022-11-01 22:06:55 +0900

    solve p4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  for(var i = 0; i < word.length; i++){
+    if(word[i] >= "A" && word[i] <= "Z"){
+      answer += String.fromCharCode(155 - word.charCodeAt(i));
+    }
+    else if(word[i] >= "a" && word[i] <= "z"){
+      answer += String.fromCharCode(219 - word.charCodeAt(i));
+    }
+    else{
+      answer += word[i];
+    }
+    
+  }
   return answer;
 }
 
