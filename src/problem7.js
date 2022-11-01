5f7de153cc9845f888ec0263b8a783da3d313773
su2333066 <su2333066@naver.com>
2022-11-01 23:31:39 +0900

    problem 7 문제의미 잘못

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,31 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  let count = {};
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      const friend = friends[i].filter((item) => {
+        return item !== user;
+      });
+      count[`${friend}`] = 10;
+    }
+  }
+
+  const visitsCount = (visitors) =>
+    visitors.reduce((ac, v) => ({ ...ac, [v]: (ac[v] || 0) + 1 }), {});
+
+  const counts = visitsCount(visitors);
+
+  for (let visit in counts) {
+    if (visit in count) {
+      count[`${visit}`] += counts[visit];
+    } else {
+      count[`${visit}`] = counts[visit];
+    }
+  }
+
+  console.log(count);
+
   return answer;
 }
 
