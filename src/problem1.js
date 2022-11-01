7b0f98901f0db1fdd84b374144e562b592db39fe
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-01 23:10:06 +0900

    problem1: 기능 구현
    devidePageNumber : 페이지 넘버 배열로 만들기
    sumPageNumber : 페이지 넘버 합치기
    multiPageNumber : 페이지 넘버 곱하기
    comparePageNumber : 가장 큰 페이지 넘버 선정하기

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,61 @@
+const devidePageNumber = (page) => {
+  const pageNumberArray = page.toString().split("").map(Number);
+  return pageNumberArray;
+};
+
+const sumPageNumber = (page) => {
+  const sum = page.reduce((sumValue, currentValue) => {
+    return sumValue + currentValue;
+  });
+  return sum;
+};
+
+const multiPageNumber = (page) => {
+  const multi = page.reduce((multiValue, currentValue) => {
+    return multiValue * currentValue;
+  });
+  return multi;
+};
+
+const comparePageNumber = (page1, page2) => {
+  const maxPageValue = Math.max(
+    sumPageNumber(page1),
+    multiPageNumber(page1),
+    sumPageNumber(page2),
+    multiPageNumber(page2)
+  );
+  return maxPageValue;
+};
+
+const compareWinner = (pobi, crong) => {
+  let winner;
+  if (pobi === crong) {
+    winner = 0;
+  } else if (pobi > crong) {
+    winner = 1;
+  } else if (pobi < crong) {
+    winner = 2;
+  }
+  return winner;
+};
+
 function problem1(pobi, crong) {
-  var answer;
+  const [pobiLeft, pobiRight] = pobi;
+  const [crongLeft, crongRight] = crong;
+  let answer;
+
+  const arrPobiLeft = devidePageNumber(pobiLeft);
+  const arrPobiright = devidePageNumber(pobiRight);
+  const arrCrongLeft = devidePageNumber(crongLeft);
+  const arrCrongright = devidePageNumber(crongRight);
+
+  if (pobiLeft + 1 === pobiRight && crongLeft + 1 === crongRight) {
+    const maxPobi = comparePageNumber(arrPobiLeft, arrPobiright);
+    const maxCrong = comparePageNumber(arrCrongLeft, arrCrongright);
+    answer = compareWinner(maxPobi, maxCrong);
+  } else {
+    answer = -1;
+  }
   return answer;
 }
 
