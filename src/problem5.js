1a43881625d6f9be63db05f3af1c3130959cc7f7
rhino-ty <okko8522@gmail.com>
2022-11-01 15:04:11 +0900

    feat(problem5.js): 온보딩 문제 5번 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,19 @@
 function problem5(money) {
-  var answer;
+  // 예외 처리
+  if (money < 1) {
+    money = 1;
+  }
+  if (money > 1000000) {
+    money = 1000000;
+  }
+
+  const papers = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let answer = [];
+  papers.forEach((paper) => {
+    answer.push(Math.floor(money / paper));
+    money %= paper;
+  });
   return answer;
 }
 
