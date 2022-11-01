a785c76612361de13446edeeff465c09fc40e2ca
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-01 18:23:47 +0900

    feat: [6번 문제] 닉네임 리스트

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,10 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const nickNameList = forms.map((form) => {
+    const nickName = form[1];
+    nickNameList.push(nickName);
+  });
+
+  return nickNameList;
 }
 
 module.exports = problem6;
