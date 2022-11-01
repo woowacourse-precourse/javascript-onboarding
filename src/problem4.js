fff8a967c8477bf192624470222c1e01cfea38bc
letsjo <hyunoh.jo@gmail.com>
2022-11-01 21:18:28 +0900

    [problem4] Simplify code to be make readability

--- src/problem4.js
+++ src/problem4.js
@@ -18,24 +18,24 @@ function convertTreeFrogChar(utf16Code){
   return checkAlphabet(utf16Code)?convertChar(convertReverseCode(utf16Code)):convertChar(utf16Code);
 }
 
-function checkAlphabet(utf16Code){
-  return ( 65 <= utf16Code && utf16Code <= 90) || ( 97 <= utf16Code && utf16Code <= 122);
-}
-
-function convertChar(utf16Code){
-  return String.fromCharCode(utf16Code);
+function convertUTF16(char){
+  return char.charCodeAt(0);
 }
 
 function convertReverseCode(utf16Code){
   return checkLowerCase(utf16Code)?219-utf16Code:155-utf16Code;
 }
 
-function convertUTF16(char){
-  return char.charCodeAt(0);
+function convertChar(utf16Code){
+  return String.fromCharCode(utf16Code);
 }
 
-function convertString(charArr){
-  return charArr.join('');
+function checkAlphabet(utf16Code){
+  return ( 65 <= utf16Code && utf16Code <= 90) || ( 97 <= utf16Code && utf16Code <= 122);
+}
+
+function convertString(eachCharCollection){
+  return eachCharCollection.join('');
 }
 
 function checkLowerCase(utf16Code){
