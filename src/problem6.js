61ef8683b30fe9c6132bed78111933357286dbdd
junmi0918 <78129272+junmi0918@users.noreply.github.com>
2022-11-01 22:12:27 +0900

    6번 문제 풀이

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,34 @@
 function problem6(forms) {
   var answer;
-  return answer;
+  const doubleOB = DoubleOBJ(forms);
+  const duplEM = checkEM (forms, doubleOB);
+
+  return duplEM.sort();
+}
+
+function checkEM(forms, doubleOB) {
+  const duplEM = [];
+  forms.forEach((form) => {
+    const [email, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substring(i, i + 2);
+      if (doubleOB[testText] > 1) duplEM.push(email);
+    }
+  });
+  return duplEM;
+}
+
+function DoubleOBJ(forms) {
+  const doubleOB = {};
+  forms.forEach((form) => {
+    const [_, nickname] = form;
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const testText = nickname.substring(i, i + 2);
+      if (testText in doubleOB) doubleOB[testText] += 1;
+      else doubleOB[testText] = 1;
+    }
+  });
+  return doubleOB;
 }
 
 module.exports = problem6;
