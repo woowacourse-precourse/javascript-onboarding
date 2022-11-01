64cfa3afb54e323105a93ec4ca53aa97b720b303
shinhyunsu01 <shine.master9687@gmail.com>
2022-11-01 13:35:27 +0900

    2 solve

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+	function del(str) {
+		for (let i = 0; i < str.length; i++) {
+			str = str.replace(str[i] + str[i], "");
+		}
+		return str;
+	}
+
+	while (true) {
+		let data = del(cryptogram);
+		if (data === cryptogram || cryptogram === "") break;
+		else cryptogram = data;
+	}
+	return cryptogram;
 }
 
 module.exports = problem2;
