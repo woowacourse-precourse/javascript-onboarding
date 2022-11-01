0c7e5ab7bb133c44fe572048ba753f863a60e3ae
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 10:24:06 +0900

    Problem2: 기능 목록 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  /*
+  기능 목록
+  1. cryptogram을 한 자리씩 잘라서 dirty 배열로 변환
+  2. 첫 Cycle 먼저 구현 (처음부터 끝까지 훑으면서 중복된 문자 삭제하기)
+  3. flag 추가하여 중복된 문자가 없을 때까지 반복
+  4. dirty 배열을 합쳐서 clean 문자열로 변환 후 정답으로 반환
+  */
 }
 
 module.exports = problem2;
