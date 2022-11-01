2868863cbca2e817042257535152951360a1c7c4
1211lys <hpppower111@gmail.com>
2022-11-01 21:12:25 +0900

    ADD : 닉네임 필터링

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
-  return answer;
-}
+  let nicknames = [];
 
+  for (j in forms) {
+    for (i = 0; i < forms[j][1].length - 1; i++) {
+      nicknames.push(forms[j][1][i] + forms[j][1][1 + i]);
+    }
+  }
+
+  let getNickname = [];
+  for (i in nicknames) {
+    for (j in nicknames) {
+      if (i !== j && nicknames[i] == nicknames[j]) {
+        getNickname.push(nicknames[i]);
+      }
+    }
+  }
+
+  let getSameNicknames = new Set(getNickname);
+  let filteredNickname = [...getSameNicknames]; //중복제거
+}
 module.exports = problem6;
