f1c37355bf2adb3ab667441c65478b5eeb9f8aee
Bori-github <qhflrnfl4324@gmail.com>
2022-11-01 15:24:56 +0900

    ✨ problem7 > 사용자와 함께아는 친구 목록과 점수를 구하는 기능 및 이미 친구인 목록 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,25 @@
 function problem7(user, friends, visitors) {
   var answer;
-  return answer;
+  const knowFriendsPoint = 10;
+  const visitTimelinePoint = 1;
+
+  const avoidResult = [];
+  const friendsMap = new Map();
+  for (let i = 0; i < friends.length; i++) {
+    if (!avoidResult.includes(friends[i][0])) {
+      avoidResult.push(friends[i][0]);
+    }
+
+    if (friends[i][1] === user) continue;
+    if (friendsMap.has(friends[i][1])) {
+      friendsMap.set(
+        friends[i][1],
+        friendsMap.get(friends[i][1]) + knowFriendsPoint
+      );
+      continue;
+    }
+    friendsMap.set(friends[i][1], knowFriendsPoint);
+  }
 }
 
 module.exports = problem7;
