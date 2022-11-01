079ff6e58f072c325510067a572c4a8b3087ca1f
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 21:39:24 +0900

    [problem7] feat: 필요한 기능 check

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,10 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+// 함께 아는 친구수 count 함수
+function withFriends(friend) {}
+
+// 타임라인 방문수 count 함수
+function visitFriends(friend) {}
+
 module.exports = problem7;
