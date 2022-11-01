306957b7e5f252c826acf674bcd826c6208b2e8b
Raehyeon Jeong <raehy19@gmail.com>
2022-11-01 09:44:13 +0900

    feat(problem4) : 대문자, 소문자 알파벳인 경우 변환하는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -10,6 +10,18 @@ function isLower(charCode) {
   return charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0);
 }
 
+function upperConvert(charCode) {
+  return String.fromCharCode(
+    'A'.charCodeAt(0) + ('Z'.charCodeAt(0) - charCode),
+  );
+}
+
+function lowerConvert(charCode) {
+  return String.fromCharCode(
+    'a'.charCodeAt(0) + ('z'.charCodeAt(0) - charCode),
+  );
+}
+
 function problem4(word) {
   if (isException(word)) return;
 }
