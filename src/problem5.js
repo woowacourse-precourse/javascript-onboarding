c4fc5899a2fd98be35dc67af24255bd6dfb72f0c
pangkyu <baesense@naver.com>
2022-11-01 22:53:09 +0900

    refactor : 반복 기능 함수화
    중복되는 기능들을 합쳐 함수화로 리팩토링

--- src/problem5.js
+++ src/problem5.js
@@ -1,107 +1,25 @@
 function problem5(money) {
-  let result = [];
+  return price(money);
+}
+
+function price(money) {
+  const result = [];
   let balance = money;
-  result.push(is50000(money));
-  balance = money - is50000(money) * 50000;
+  let mount = 0;
+  const cashRange = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < cashRange.length; i++) {
+    if (money >= cashRange[i]) {
+      mount = balance / cashRange[i];
+      mount = Number.parseInt(mount);
+      balance = balance - mount * cashRange[i];
 
-  result.push(is10000(balance));
-  balance = balance - is10000(balance) * 10000;
-
-  result.push(is5000(balance));
-  balance = balance - is5000(balance) * 5000;
-
-  result.push(is1000(balance));
-  balance = balance - is1000(balance) * 1000;
-
-  result.push(is500(balance));
-  balance = balance - is500(balance) * 500;
-
-  result.push(is100(balance));
-  balance = balance - is100(balance) * 100;
-
-  result.push(is50(balance));
-  balance = balance - is50(balance) * 50;
-
-  result.push(is10(balance));
-  balance = balance - is10(balance) * 10;
-
-  result.push(is1(balance));
-  balance = balance - is1(balance) * 1;
-
+      result.push(mount);
+    } else {
+      result.push(0);
+    }
+  }
   console.log(result);
   return result;
-}
-
-function is50000(money) {
-  if (money >= 50000) {
-    let ohmanwon = money / 50000;
-    return Number.parseInt(ohmanwon);
-  }
-  return 0;
-}
-
-function is10000(money) {
-  if (money >= 10000) {
-    let manwon = money / 10000;
-    return Number.parseInt(manwon);
-  }
-  return 0;
-}
-
-function is5000(money) {
-  if (money >= 5000) {
-    let ohcheonwon = money / 5000;
-    return Number.parseInt(ohcheonwon);
-  }
-  return 0;
-}
-
-function is1000(money) {
-  if (money >= 1000) {
-    let cheonwon = money / 1000;
-    return Number.parseInt(cheonwon);
-  }
-  return 0;
-}
-
-function is500(money) {
-  if (money >= 500) {
-    let ohBaekwon = money / 500;
-    return Number.parseInt(ohBaekwon);
-  }
-  return 0;
-}
-
-function is100(money) {
-  if (money >= 100) {
-    let baekwon = money / 100;
-    return Number.parseInt(baekwon);
-  }
-  return 0;
-}
-
-function is50(money) {
-  if (money >= 50) {
-    let ohshipwon = money / 50;
-    return Number.parseInt(ohshipwon);
-  }
-  return 0;
-}
-
-function is10(money) {
-  if (money >= 10) {
-    let shipwon = money / 10;
-    return Number.parseInt(shipwon);
-  }
-  return 0;
-}
-
-function is1(money) {
-  if (money >= 1) {
-    let Illwon = money / 1;
-    return Number.parseInt(Illwon);
-  }
-  return 0;
 }
 
 function testCode() {
