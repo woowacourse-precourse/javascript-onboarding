a1c670955d9d2332ea0b1c9639149a641fecafba
Moyujin <scab12341@gmail.com>
2022-11-01 22:58:11 +0900

    7번 추천 점수 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,11 @@
 function problem7(user, friends, visitors) {
   const relations = getRealtions(friends);
   const allUsers = new Set([...Object.keys(relations), ...visitors]);
+  const userFriendList = relations[user];
+
+  const recommandScore = initRecommandScore(allUsers);
+  setFriendScore(recommandScore, relations, userFriendList, user);
+  setVisitedScore(recommandScore, visitors, userFriendList);
 }
 
 const getRealtions = (friends) => {
@@ -17,6 +22,31 @@ const getRealtions = (friends) => {
 
 const isDuplicated = (data, relations) => {
   return data in relations;
+};
+
+const initRecommandScore = (allUsers) => {
+  recommandScore = {};
+  allUsers.forEach((user) => {
+    recommandScore[user] = 0;
+  });
+
+  return recommandScore;
+};
+
+const setFriendScore = (recommandScore, relations, userFriendList, user) => {
+  userFriendList.forEach((friend) => {
+    relations[friend].forEach((relation) => {
+      if ((relation === user) | userFriendList.has(relation)) return;
+      recommandScore[relation] += 10;
+    });
+  });
+};
+
+const setVisitedScore = (recommandScore, visitors, userFriendList) => {
+  visitors.forEach((visitor) => {
+    if (userFriendList.has(visitor)) return;
+    recommandScore[visitor] += 1;
+  });
 };
 
 module.exports = problem7;
