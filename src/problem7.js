1765686f745578638d9f03bdc2e212d0a90c9c6e
oio337a <oio337a@gmail.com>
2022-11-01 20:57:58 +0900

    문제 7번 수정 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,44 +1,42 @@
 function problem7(user, friends, visitors) {
   let answer;
   let userFriends = new Set();
-  let otherFriends = new Set();
+  let others = new Set();
 
   friends.forEach((friend) => {
     userFriends.add(friend[0]);
     if (friend[1] !== user) {
-      otherFriends.add(friend[1]);
+      others.add(friend[1]);
     }
   });
   visitors.forEach((visitor) => {
     if (userFriends.has(visitor) === false) {
-      otherFriends.add(visitor);
+      others.add(visitor);
     }
   });
 
-  otherFriends = new Array(...otherFriends).sort();
+  others = new Array(...others).sort();
 
-  let otherFriendsMap = new Map();
-  otherFriends.forEach((friend) => otherFriendsMap.set(friend, 0));
+  let othersMap = new Map();
+  others.forEach((friend) => othersMap.set(friend, 0));
 
   friends.forEach((friend) => {
-    if (otherFriendsMap.has(friend[1])) {
-      otherFriendsMap.set(friend[1], otherFriendsMap.get(friend[1]) + 10);
+    if (othersMap.has(friend[1])) {
+      othersMap.set(friend[1], othersMap.get(friend[1]) + 10);
     }
   });
 
   visitors.forEach((friend) => {
-    if (otherFriendsMap.has(friend)) {
-      otherFriendsMap.set(friend, otherFriendsMap.get(friend) + 1);
+    if (othersMap.has(friend)) {
+      othersMap.set(friend, othersMap.get(friend) + 1);
     }
   });
 
-  let otherFriendsList = new Array(...otherFriendsMap).sort(
-    (x, y) => y[1] - x[1]
-  );
+  let othersList = new Array(...othersMap).sort((x, y) => y[1] - x[1]);
 
-  otherFriendsList = otherFriendsList.filter((friend) => friend[1] !== 0);
+  othersList = othersList.filter((friend) => friend[1] !== 0);
 
-  answer = otherFriendsList.map((friend) => friend[0]);
+  answer = othersList.map((friend) => friend[0]);
   answer = answer.slice(0, 5);
 
   return answer;
