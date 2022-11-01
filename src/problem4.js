a13fe75f55c5bfe3cde926dee933f984d37cce21
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 22:21:14 +0900

    feat: [PROBLEM4] 문자를 하나씩 사전에 따라 변환하여 새로운 문자열 반환

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,20 @@
+const problem4Util = require('./problem4Util');
+
 function problem4(word) {
   var answer;
+  const { changeChar } = problem4Util;
 
   try {
+    // 범위를 벗어난다면 에러를 반환한다.
     if (word.length > 1000 || word.length < 1) {
-      return -1;
+      throw new Error('word length must be between(1, 1000)');
     }
-  } catch {
+
+    answer = word
+      .split('')
+      .map((char) => changeChar(char))
+      .join('');
+  } catch (e) {
     answer = -1;
   }
   return answer;
