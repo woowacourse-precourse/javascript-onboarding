4739f3f4bdc3543e57250b35db79e0d58e59df2e
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 20:34:57 +0900

    [problem4] feat: 문자열과 ASCII 변환 후 문자 반환 changeWord 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,17 +1,35 @@
+problem4("a");
+
 function problem4(word) {
   var answer;
   // 문자열을 문자로 split
-
+  for (let i = 0; i < word.length; i++) {}
   // 변환된 문자열을 붙이기
+  changeWord("A");
 
   return answer;
 }
 
 // ASCII를 사용하여 문자 변환하는 함수
-function changeWord(word) {
+function changeWord(letter) {
+  let changeLetter = "";
+  let ascii = letter.charCodeAt(0);
+
   // 대문자 (65~90)
+  if (ascii >= 65 && ascii <= 90) {
+    changeLetter = String.fromCharCode(155 - ascii);
+  }
   // 소문자 (97~122)
+  if (ascii >= 97 && ascii <= 122) {
+    changeLetter = String.fromCharCode(219 - ascii);
+  }
+
   // 예외처리 (띄어쓰기)
+  if (letter === " ") {
+    changeLetter === " ";
+  }
+
+  return changeLetter;
 }
 
 module.exports = problem4;
