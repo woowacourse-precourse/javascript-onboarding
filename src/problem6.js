9c0d386fbf93406046636dc3629ab41c0627691a
banma1234 <banma1234@gmail.com>
2022-11-01 19:17:50 +0900

    problem_6 : 중복문자 찾는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -10,16 +10,32 @@ function errorHandler(forms) {
   else return false;
 }
 
+// 이름 쪼개는 함수
 function spliceName(forms) {
   // 이름을 담을 배열
   let nameArr = [];
 
   // 2글자씩 이름을 쪼개 새 배열에 저장
-  forms.map(([email, name]) => {
+  forms.map(([_, name]) => {
     for (i=0; i<name.length - 1; i++) {
       nameArr.push(name[i] + name[i+1]);
     }
   });
+
+  searchOverlap(nameArr);
+}
+
+// 중복문자 찾는 함수
+function searchOverlap(nameArr) {
+  let overlapArr = [];
+
+  // 중복문자 새 배열에 저장
+  nameArr.filter((item, index) => {
+    // 이름 배열에서 중복되는 문자를 새 배열에 저장
+    if (nameArr.indexOf(item) !== index)  overlapArr.push(item);
+  });
+
+  return overlapArr;
 }
 
 module.exports = problem6;
