b01a759692b28033492a849a5690fe78d8702565
sulmeee <poorooria@naver.com>
2022-11-01 14:00:12 +0900

    [problem7] feat: 친구관계 로직

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,26 @@
 function problem7(user, friends, visitors) {
   // 친구 리스트 및 점수를 담아줄 객체 선언
-  let freindsList = {}
-  let score = {}
+  let freindsList = {};
+  let score = {};
+
+  // 친구관계 관련 로직
+  for (let friend of friends) {
+    let team1 = friend[0];
+    let team2 = friend[1];
+
+    if (!(team1 in freindsList)) {
+      freindsList[team1] = [team2];
+      score[team1] = 0;
+    } else {
+      freindsList[team1].push(team2);
+    }
+    if (!(team2 in freindsList)) {
+      freindsList[team2] = [team1];
+      score[team2] = 0;
+    } else {
+      freindsList[team2].push(team1);
+    }
+  }
 }
 
 module.exports = problem7;
