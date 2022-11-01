7c1b47648b13b4af3b760ef64a9d420d6f04b228
yeongseokim <1903020@ewhain.net>
2022-11-01 20:35:34 +0900

    feat(problem1): [예외 처리] [왼쪽 페이지 번호, 오른쪽 페이지 번호]가 순서대로 들어 있지 않은 경우 -1 리턴

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,8 @@
 function problem1(pobi, crong) {
   if (pobi.length != 2) return -1
   if (crong.length != 2) return -1
+  if (pobi[1] - pobi[0] != 1) return -1
+  if (crong[1] - crong[0] != 1) return -1
 
   var answer;
 
