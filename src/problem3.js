0d11644a480e7cbebf735ff0b6e79d05d6fdc749
KPGY <yuchw8@korea.ac.kr>
2022-11-01 21:47:07 +0900

    6번문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -2,7 +2,7 @@ function problem3(number) {
   var answer =0;
 for(i=0; i<=number; i++)
 {
-  answer += (String(i).match(/[3|6|9]/g)||[]).length;
+  answer += (String(i).match(/[3|6|9]/g)||[]).length;//정규식을 이용해 3,6,9가 포함된 숫자들의 횟수만큼 asnwer에 덧샘
 }
 return answer;
 }
--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,38 @@
 function problem6(forms) {
-  var answer;
+  let name =[];
+  let nickname = [];
+  let email =[];
+  for(let i =0; i<forms.length; i++)
+{
+  name.push(forms[i][1])
+}
+
+for(let i=0; i<name.length; i++)
+{
+  for(let j=0; j<name[j].length; j++)
+{
+  nickname.push(name[i].substring(j, j+2))
+}
+
+}
+
+const duplicate = nickname.filter((element, index) => nickname.indexOf(element) !== index);
+
+for(let j=0; j<forms.length; j++)
+{
+  for(let i =0; i<duplicate.length; i++)
+{
+ if(forms[j][1].includes(duplicate[i]))
+ {
+  email.push(forms[j][0])
+ } 
+}
+
+}
+
+answer = Array.from(new Set(email)).sort();
   return answer;
 }
+
 
 module.exports = problem6;
