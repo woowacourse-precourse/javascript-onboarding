67fe36d450af14743a21b23672d184f85e60114b
terry <kdjtiger@naver.com>
2022-11-01 19:19:14 +0900

    문자열 배열로 넣고 연속된 중복 있으면 삭제 후 다시 반복 결과 값 문자열로 바꿈

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+  let stringArr = [...cryptogram];
+  let k = 0;
+  const stringArrLength = stringArr.length;
+  
+  while (k <= stringArrLength/2 ){        
+      
+    for (let i = 0; i < stringArr.length ; i++) {           
+      if (stringArr[i] == stringArr[i+1]){
+        stringArr.splice(i,2);                
+      }
+    }
+    k++;
+  }
+  let result = stringArr.join('')
+  return result;
+  }
 
 module.exports = problem2;
