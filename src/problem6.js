cdcd52485c718f2823581073ddeca46dd54ce568
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 17:06:42 +0900

    Fix: problem6 - for문 스코프 수정

--- src/problem6.js
+++ src/problem6.js
@@ -7,18 +7,18 @@ function problem6(forms) {
 
   for (let i = 0; i < forms.length; i++) {
     nickName.push(forms[i][1]);
-  }
-  for (let j = 0; j < nickName[i].length - 1; j++) {
-    nickNameTwoWords = nickName[i].substring(j, j + 2);
 
-    if (duplicationCount[nickNameTwoWords]) {
-      duplicationCount[nickNameTwoWords] += 1;
-      continue;
-    } else duplicationCount[nickNameTwoWords] = 1;
+    for (let j = 0; j < nickName[i].length - 1; j++) {
+      nickNameTwoWords = nickName[i].substring(j, j + 2);
+
+      if (duplicationCount[nickNameTwoWords]) {
+        duplicationCount[nickNameTwoWords] += 1;
+      } else duplicationCount[nickNameTwoWords] = 1;
+    }
   }
 
   for (let i = 0; i < forms.length; i++) {
-    for (let j = 0; j < nickName[i].length - 1; j++) {
+    for (let j = 0; j < nickName[j].length - 1; j++) {
       nickNameTwoWords = nickName[i].substring(j, j + 2);
 
       if (duplicationCount[nickNameTwoWords] > 1) result.add(forms[i][0]);
