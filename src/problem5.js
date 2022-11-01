0ddd28e11d80829d7b81c2b82489ed3130c2514b
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 16:22:05 +0900

    feat: Add exchangeTo50000 func in problem5.js
    금액을 인자로 받아 바꿀 수 있는 5만원 권의 개수를 반환하는 함수를 구현하였음

--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,19 @@ function problem5(money) {
   return answer;
 }
 
+function exchangeTo50000(money) {
+  const FIVEHUNDREDTHOUSAND = 500000
+  const FIFTYTHOUSAND = 50000
+  let cnt = 0
+  while (money >= FIVEHUNDREDTHOUSAND) {
+    cnt += 10
+    money -= FIVEHUNDREDTHOUSAND
+  }
+  while (money >= FIFTYTHOUSAND) {
+    cnt += 1
+    money -= FIFTYTHOUSAND
+  }
+  return cnt
+}
+
 module.exports = problem5;
