5560ddd89aa3c0075276478506969fed82772ed7
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-01 17:20:12 +0900

    문제6 refactoring

--- src/problem6.js
+++ src/problem6.js
@@ -1,29 +1,31 @@
 function problem6(forms) {
-  var answer;
-  let emailSet = new Set(); //이메일 중복 허용X
+  let emailSet = new Set();
   
-  for (var index=0; index<forms.length-1; index++){
-    let nowCrew = forms[index]; //현재 크루
-    let nowEmail = nowCrew[0];
-    let nowNickname = nowCrew[1];
-    for(var i=0; i<nowNickname.length-1; i++){
-      let nameSubstr = nowNickname.substr(i, 2); //현재 크루의 닉네임을 2음절씩 잘라 비교
-      for (var compareIndex=index+1; compareIndex<forms.length; compareIndex++){ //(index+1)~마지막에 위치한 크루와 비교
-        let compareCrew = forms[compareIndex];
-        let compareEmail = compareCrew[0];
-        let compareNickname = compareCrew[1];
+  for (let crewIndex=0; crewIndex<forms.length-1; crewIndex++){
+    const crew = forms[crewIndex];
+    const email = crew[0];
+    const nickname = crew[1];
+    
+    for(let i=0; i<nickname.length-1; i++){
+      const lengthOfSplit = 2;
+      const word = nickname.substr(i, lengthOfSplit);
+      
+      for (let anotherCrewIndex=crewIndex+1; anotherCrewIndex<forms.length; anotherCrewIndex++){
+        const anotherCrew = forms[anotherCrewIndex];
+        const comparedEmail = anotherCrew[0];
+        const comparedNickname = anotherCrew[1];
 
-        const flag = compareNickname.includes(nameSubstr); //다른 크루의 nickname에 해당 단어가 있는지 확인
+        const isIncluded = comparedNickname.includes(word);
 
-        if(flag){
-          emailSet.add(nowEmail)
-          emailSet.add(compareEmail);
-        }        
+        if(isIncluded){
+          emailSet.add(email);
+          emailSet.add(comparedEmail);
+        }
       }
     }
   }
   
-  answer = Array.from(emailSet);
+  let answer = Array.from(emailSet);
   answer.sort();
   return answer;
 }
