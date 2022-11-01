10311a67ef7b78638917e1e2eac10229ebb93201
seungki <sysh9498@gmail.com>
2022-11-01 18:15:37 +0900

    배열을 돌며 값을 넣어주고, 뒤의 문자와 연속되면 삭제하는 부분

--- src/problem2.js
+++ src/problem2.js
@@ -2,7 +2,23 @@ function problem2(cryptogram) {
   var answer;
   let temp = [];
   let arr_crypto = [...cryptogram];
-
+  for (var i = 0; ; i++) {
+    let count = 0;
+    let overall_count = 0;
+    for (var j = 0; j < arr_crypto.length; j++) {
+      temp.push(arr_crypto[j]);
+      if (arr_crypto[j] == arr_crypto[j + 1]) {
+        temp.pop();
+        count++;
+        overall_count++;
+      }
+      if (count != 0 && arr_crypto[j] != arr_crypto[j + 1]) {
+        temp.pop();
+        count = 0;
+        overall_count++;
+      }
+    }
+  }
   return answer;
 }
 
