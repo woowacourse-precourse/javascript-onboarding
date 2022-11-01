1e33d6d08ca09fad53d9398c133bf7fdda0a6c79
jihyunlee <jihyunlee.me@gmail.com>
2022-11-01 23:39:26 +0900

    feat: problem6, 문제해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      if (forms[i][1].length > 1) {
+        const nameSubstr = forms[i][1].substr(j, 2);
+        for (let k = 0; k < forms.length; k++) {
+          if (k !== i) {
+            if (forms[k][1].includes(nameSubstr) && !answer.includes(forms[k][0])) {
+              answer.push(forms[k][0]);
+            }
+          }
+        }
+      }
+    }
+  }
+  return answer.sort()
 }
 
 module.exports = problem6;
