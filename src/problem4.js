ac21b69817f22b89447303ece59135b2c430419f
emma <jhj191058@gmail.com>
2022-11-01 19:40:03 +0900

    problem4 수정 후 재 업데이트

--- src/problem4.js
+++ src/problem4.js
@@ -3,15 +3,17 @@ function problem4(word) {
 
   var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnofqrstuvwxyz';
   var re = 'ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba';
+
   var index = x => str.indexOf(x);
   var translate = x => index(x) > -1 ? re[index(x)] : x;
-  if(word == null) {
-        word = " ";
+
+  if (word == null){
+    word = "";
   }
 
   answer = word.split('').map(translate).join('');
-  
   return answer;
 }
+
 
 module.exports = problem4;
