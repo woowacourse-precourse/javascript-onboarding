e50ff2729b8e144844661f5b4df69c6b4c4ccfad
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:52:36 +0900

    feat(prob7) : user의 친구 추출 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
+function getUserFriend(user, friends) {
+  let userFriend = [];
+  for (let i = 0; i < friends.length; i++) {
+    let isFriend = friends[i].indexOf(user);
+    if (isFriend !== -1) {
+      userFriend.push(friends[i][1 - isFriend]);
+    }
+  }
+  return userFriend;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+
   return answer;
 }
 
