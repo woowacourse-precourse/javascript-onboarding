b8d02bd70332c2bd1ae20fda6e659b26610d5c41
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-01 17:55:03 +0900

    feat: [7번 문제] 모든 친구 관계를 나타내는 객체

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const friendsOb = {};
+
+  friends.map((friendRelationship) => {
+    const [A, B] = friendRelationship;
+    if (friendsOb[A]) {
+      friendsOb[A].push(B);
+    } else {
+      friendsOb[A] = [B];
+    }
+    if (friendsOb[B]) {
+      friendsOb[B].push(A);
+    } else {
+      friendsOb[B] = [A];
+    }
+  });
+
+  return friendsOb;
 }
 
 module.exports = problem7;
