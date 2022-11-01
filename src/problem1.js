cf7a2867cc8ca457b287b22ed72ba0d83794136a
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 19:23:09 +0900

    refactor:[문제1] 각 배열에서 더했을 때 최대값과 곱했을 때 최대값을 계산해준다

--- src/problem1.js
+++ src/problem1.js
@@ -10,9 +10,18 @@ const exception = (pobi, crong) => {
   if (crong.reduce((a, b) => Math.abs(a - b)) !== 1) return -1;
 };
 
+const getMaxNumber = (array, pobi) => {
+  array.forEach((page) => {
+    let plus = Object.values(String(page)).reduce((a, b) => +a + +b);
+    let multiplication = Object.values(String(page)).reduce((a, b) => +a * +b);
+  });
+};
+
 function problem1(pobi, crong) {
   pobiMax, (crongMax = []);
   if (exception(pobi, crong) === -1) return -1;
+  getMaxNumber(pobi, pobi);
+  getMaxNumber(crong, pobi);
   return answer;
 }
 
