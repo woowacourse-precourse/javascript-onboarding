6008afb03ef069364cf700c41914ef800dc47250
Minju Park <project.mizzu@gmail.com>
2022-11-01 23:07:19 +0900

    Docs : 각 문제별 의사코드 마크다운 파일 생성 & 의사코드 분리
    - js 파일에 작성되어있던 의사코드를 PROBLEM_SOLVING_PROCESS(N).md 파일로 이동

--- src/problem6.js
+++ src/problem6.js
@@ -1,18 +1,3 @@
-// [✅] forms의 배열에서 nickName을 분리한 배열을 만든다.
-// [✅] 걸러진 이메일을 담을 result 배열을 만든다.
-// [✅] nickName 배열을 순회하며 두 글자 이상의 문자가 연속적으로 포함된 닉네임을 모두 찾는다. / substring
-// - [✅] substring을 통해 (substring(index, index + 2)) keyword 추출
-// - [✅] 추출한 keyword를 바탕으로 includes(keyword[idx])를 통해 제거 대상인 이름 추출 -> duplicateNick.push()
-// [✅] duplicateNick에 존재하는 이름과 forms의 이름을 비교하여, 일치하면 result로 반환
-// [✅] result 내 이메일 목록을 오름차순 정렬한다.
-// [✅] result 내 이메일 목록에서 중복된 이메일을 제거한다. -> 닉네임 상에서 이미 중복 처리
-
-// [예외처리] -> 제한 사항에 어긋나는 경우로, forms 배열에서 제거
-// 신청한 이메일의 길이가 11자 이상 20자 미만이 아닌 경우 (email >= 11 && nick < 20)
-// 신청한 이메일이 "email.com" 도메인이 아닌 경우
-// 신청한 닉네임이 한글이 아닌 경우
-// 신청한 닉네임의 길이가 1글자 이상 20글자 미만이 아닌 경우 (nick >= 1 && nick < 20)
-
 function problem6(forms) {
   const nickName = forms.map((data) => data[1]);
   const duplicateNick = [];
--- src/problem7.js
+++ src/problem7.js
@@ -1,21 +1,3 @@
-// friends는 모든 친구 관계를 포함한 배열 -> friends를 토대로 각 유저들의 친구 관계 알아내기
-// 사용자의 친구의 "친구"를 추천해주는 프로그램으로, 이미 친구인 donut과 shakevan은 점수X
-// 친구가 아닌 유저 중 mrko의 친구와 친구가 겹칠 때 마다 -> 10점
-// mrko의 visitors -> 1점 부여
-// 예상점수 : andole 20점, jun 20점, bedi 3점, (donut 1점, shakevan 1점)
-// 최대 5명 길이의 result 반환 (오름차순)
-// 추천 점수가 0인 경우, 5명 안에 있더라도 추천하지 X
-// -
-// [✅] user "mrko"와 친구인 아이디를 friends에서 찾기 -> donut과 shakevan 찾기
-// [✅] donut과 shakevan의 친구를 찾아 friendYouMightKnow에 저장
-// [✅] friendYouMightKnow를 reduce를 사용해 friendsScore 변수에 10점씩 누적
-// [✅] visitors를 reduce를 사용해 visitorsScore 변수에 1점씩 누적
-// [✅] 점수가 높은 순대로 정렬 후, result에 5명 push
-// - [✅] friendsScore와 visitorsScore 합치기 Object.assign(target, ...sources)
-// - [✅] result의 길이가 5일 때 까지 비교, 순회 하면서 고득점자 아이디 result에 push
-// - [✅] result에 bestFriends에 존재하는 친구가 있다면 삭제, 없다면 그대로 리턴
-// - [✅] 반환 시 아이디를 알파벳 오름차순 정렬 후 반환
-
 function problem7(user, friends, visitors) {
   const result = [];
   const friendYouMightKnow = [];
