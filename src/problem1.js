cae37001b85682bb199e32b1a65fd80371bafe5b
yeongseokim <1903020@ewhain.net>
2022-11-01 20:42:11 +0900

    feat(problem1): 동점이면 0, pobi가 이기면 1, crong이 이기면 2

--- src/problem1.js
+++ src/problem1.js
@@ -32,6 +32,13 @@ function problem1(pobi, crong) {
 
   var answer;
 
+  const pobiScore = getMaxScore(pobi);
+  const crongScore = getMaxScore(crong);
+
+  if (pobiScore === crongScore) answer = 0;
+  else if (pobiScore > crongScore) answer = 1;
+  else if (pobiScore < crongScore) answer = 2;
+
   return answer;
 }
 
