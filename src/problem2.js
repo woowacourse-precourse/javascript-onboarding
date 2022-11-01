cbf44b42e5dee6d1edde479138e91efc976775f7
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:37:00 +0900

    feat(problem2): 연속하는 중복된 문자를 확인하고 제거한다

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let i = 0;
+  while (i < cryptogram.length) {
+    if (i + 1 < cryptogram.length && cryptogram[i] === cryptogram[i + 1]) {
+      cryptogram = cryptogram.slice(0, i) + cryptogram.slice(i + 2, cryptogram.length);
+    }
+  }
 }
-
 module.exports = problem2;
