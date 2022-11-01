0334620bb077545a1f124f4807224ca016d0bc03
Jinu <jinu200131@gmail.com>
2022-11-01 15:03:50 +0900

    (prob 3) 기능1 구현

--- src/problem3.js
+++ src/problem3.js
@@ -7,8 +7,20 @@
 // number => int, 1~10'000
 // return => 손뼉 총 횟수
 function problem3(number) {
-  var answer;
-  return answer;
+	var answer;
+	return answer;
+}
+
+// 1. 특정 숫자 손뼉 몇 번 산출 기능
+// return => number
+function getClap(num) {
+	var ans=0, curr;
+	while(num > 0) {
+        curr = num % 10;
+		if(curr % 3 == 0 && curr != 0) ans++;
+		num = parseInt(num / 10);
+	}
+	return ans;
 }
 
 module.exports = problem3;
