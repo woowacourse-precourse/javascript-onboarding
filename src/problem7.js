3e09e6531ed3f8ecad3b5bb88be08f1fb63e2d74
Hungeun2 <bepoohun@gmail.com>
2022-11-01 23:16:23 +0900

    Feat: problem7 Add find People function

--- src/problem7.js
+++ src/problem7.js
@@ -5,6 +5,13 @@
 // 5. 이미 user 와 친구인 사람 map에서 제거
 // 6. sort및 랭킹 생성
 
+// 회원 리스트화
+function findPeople(friends) {
+  let people = [];
+  friends.forEach((data) => people.push(...data));
+  people = [...new Set(people)];
+  return people;
+}
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
