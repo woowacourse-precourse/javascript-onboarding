1133c194eba60b297690b12e60921b5b60a65bd9
cocoa <jwv0323@kau.kr>
2022-11-01 15:56:21 +0900

    problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let decryption = [];
+  let popCrypto = "";
+
+  [...cryptogram].forEach((crypto) => {
+    (decryption[decryption.length - 1] === crypto) ? popCrypto = decryption.pop() : null;
+    (!decryption || popCrypto !== crypto) ? decryption.push(crypto) : null;
+  })
+
+  return decryption.join("");
 }
 
 module.exports = problem2;
