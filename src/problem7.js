02843bf53e8dd68d046b06c0eefa7341a7395060
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:38:56 +0900

    feat:PROBLEM7:기능 목록 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,27 @@
+/*
+ #기능 요구 사항
+사용자 아이디 user와 친구 관계를 담은 이차원 배열 friends, 
+사용자 타임 라인 방문 기록 visitors가 매개변수로 주어질 때, 
+미스터코의 친구 추천 규칙에 따라 점수가 가장 높은 순으로 정렬하여 
+최대 5명을 return 하도록 solution 메서드를 완성. 
+이때 추천 점수가 0점인 경우 추천하지 않으며, 추천 점수가 같은 경우는 이름순으로 정렬.
+
+1. user와 친구관계인 사람을 구한다.
+2.친구가 아닌 방문자를 구한다.
+3.함께아는 친구를 구한다.
+4.방문자와 함께아는 친구를 통해서 점수를 계산한다.
+5.추천 목록 정렬한다.
+
+*/
+
+
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    let answer;
+    return answer;
 }
 
 module.exports = problem7;
+
+
+
