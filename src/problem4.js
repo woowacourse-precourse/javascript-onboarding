2e17a9ebb4b0f7e1d4991503f1dd5a1d424951a0
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:26:13 +0900

    feat: [problem4] 소문자 알파벳 확인

--- src/problem4.js
+++ src/problem4.js
@@ -5,6 +5,12 @@ const isUpperAlphabet = (char) => {
   const regExp = /[A-Z]/g; // 영어
   return regExp.test(char);
 }
+
+const isLowerAlphabet = (char) => {
+  const regExp = /[a-z]/g; // 영어
+  return regExp.test(char);
+}
+
 }
 
 module.exports = problem4;
