055b95f10577a49c725a45d99ab9d1081565b642
윤동주 <yoondj98@naver.com>
2022-11-01 21:33:37 +0900

    우아한 테크코스 프리코슥 1주차 4번 코드 주석 기입

--- src/problem4.js
+++ src/problem4.js
@@ -1,14 +1,15 @@
 function problem4(word) {
   var arr_word = word.toString();
-  var answer="";
-  for(var i=0; i<arr_word.length; i++){
-    if(arr_word.charCodeAt(i)>=65 && arr_word.charCodeAt(i)<=90){
-      answer = answer.concat(String.fromCharCode(155-arr_word.charCodeAt(i)));
-    }
-    else if(arr_word.charCodeAt(i)>=97 && arr_word.charCodeAt(i)<=122){
-      answer = answer.concat(String.fromCharCode(219-arr_word.charCodeAt(i)));
-    }
-    else answer = answer.concat(arr_word[i]);
+  var answer = "";
+  /* 주어진 단어(word)에 대하여 순회하며 */
+  for (var i = 0; i < arr_word.length; i++) {
+    if (arr_word.charCodeAt(i) >= 65 && arr_word.charCodeAt(i) <= 90) {
+      /* ASCII 코드 값(A=65, Z=90) 고려하여 가우스 법칙과 같은 방식을 적용하여 반대값 구하기 */
+      answer = answer.concat(String.fromCharCode(155 - arr_word.charCodeAt(i)));
+    } else if (arr_word.charCodeAt(i) >= 97 && arr_word.charCodeAt(i) <= 122) {
+      /* ASCII 코드 값(a=97, Z=122) 고려하여 가우스 법칙과 같은 방식을 적용하여 반대값 구하기 */
+      answer = answer.concat(String.fromCharCode(219 - arr_word.charCodeAt(i)));
+    } else answer = answer.concat(arr_word[i]);
   }
   return answer;
 }
