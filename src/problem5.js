7dcc12abcfecba4ae8c7616da3d2373d0fd743b1
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 17:23:49 +0900

    refactor: Modify exchangeTo50000 func in problem5.js
    while문이 사용된 코드에서 while문을 제거하고 단순하게 계산후 return할 수 있도록 수정하였음

--- src/problem5.js
+++ src/problem5.js
@@ -30,18 +30,8 @@ function problem5(money) {
 }
 
 function exchangeTo50000(money) {
-  const FIVEHUNDREDTHOUSAND = 500000;
   const FIFTYTHOUSAND = 50000;
-  let cnt = 0;
-  while (money >= FIVEHUNDREDTHOUSAND) {
-    cnt += 10;
-    money -= FIVEHUNDREDTHOUSAND;
-  }
-  while (money >= FIFTYTHOUSAND) {
-    cnt += 1;
-    money -= FIFTYTHOUSAND;
-  }
-  return cnt;
+  return Math.floor(money/FIFTYTHOUSAND);
 }
 
 function exchangeTo10000(money) {
