c483ac18c555bd1b58650b841c61a10b61586454
OrangeChick <retww@naver.com>
2022-11-01 21:28:34 +0900

    refactor: 🔧Reduce code repetition
    - 코드의 반복을 줄이기 위해 리팩토링을 진행하였습니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,42 +1,11 @@
 function problem5(money) {
-  let answer = Array(9).fill(0);
-  let restMoney = money;
+  const answer = [];
 
-  let fiftyThousand = parseInt(restMoney / 50000);
-  answer[0] = fiftyThousand;
-  restMoney = restMoney - fiftyThousand * 50000;
-
-  let tenThousand = parseInt(restMoney / 10000);
-  answer[1] = tenThousand;
-  restMoney = restMoney - tenThousand * 10000;
-
-  let fiveThousand = parseInt(restMoney / 5000);
-  answer[2] = fiveThousand;
-  restMoney = restMoney - fiveThousand * 5000;
-
-  let oneThousand = parseInt(restMoney / 1000);
-  answer[3] = oneThousand;
-  restMoney = restMoney - oneThousand * 1000;
-
-  let fiveHundred = parseInt(restMoney / 500);
-  answer[4] = fiveHundred;
-  restMoney = restMoney - fiveHundred * 500;
-
-  let oneHundred = parseInt(restMoney / 100);
-  answer[5] = oneHundred;
-  restMoney = restMoney - oneHundred * 100;
-
-  let fifty = parseInt(restMoney / 50);
-  answer[6] = fifty;
-  restMoney = restMoney - fifty * 50;
-
-  let ten = parseInt(restMoney / 10);
-  answer[7] = ten;
-  restMoney = restMoney - ten * 10;
-
-  let one = parseInt(restMoney / 1);
-  answer[8] = one;
-  restMoney = restMoney - one * 1;
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (let i = 0; i < moneyArr.length; i++) {
+    answer.push(parseInt(money / moneyArr[i]));
+    money = money % moneyArr[i];
+  }
 
   return answer;
 }
