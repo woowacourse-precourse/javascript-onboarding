be218e544e61b177435a41f9001f234ce3eb35fe
seoyeon <wjstjdus96@naver.com>
2022-11-01 13:52:53 +0900

    feat: problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var numStr = "";
+
+  for (let i = 1; i <= number; i++) {
+    numStr = `${i + numStr}`;
+  }
+
+  return numStr.split(/3|6|9/g).length - 1;
 }
 
 module.exports = problem3;
