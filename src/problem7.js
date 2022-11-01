4f763065db81e24c16e7600893456d8c66d118e3
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 14:27:17 +0900

    [p7] 전체 유저 목록 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,23 +1,36 @@
 function findFriend(user, friends) {
   let friend = [];
 
-  friends.forEach((a1, a2) => {
-    if(a1 === user)
-      friend.push(a2);
-    if(a2 === user)
-      friend.push(a1);
+  friends.forEach((left, right) => {
+    if(left === user)
+      friend.push(right);
+    if(right === user)
+      friend.push(left);
   });
 
   return friend;
 }
 
+function users(friends) {
+  let userlist = new Set();
+  
+  friends.forEach((left, right) =>{
+    userlist.add(left);
+    userlist.add(right);
+  });
+
+  return userlist;
+}
+
 function problem7(user, friends, visitors) {
   userFriend = findFriend(user, friends);
+  userlist = users(friends);
   
   let answer = [, []];
   return answer;
 }
-//사용자와 친구관계인 사람을 구함
+//사용자와 친구관계인 유저를 구함
+//유저 목록 저장
 //친구와 친구관계인 친구에 +10점
 //방문자
 module.exports = problem7;
