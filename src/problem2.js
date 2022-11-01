a870ada7599c7bf333e058ac3f6a3fabe72175a2
sojinjang <jsojin6119@gmail.com>
2022-11-01 23:32:22 +0900

    :bug: debug(prob2): 연속되는 문자가 3개 이상일때도 제거되도록 로직 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,13 +1,20 @@
-function problem2(cryptogram) {
-  const [top, ...rest] = cryptogram.split("");
-  const temp = [top];
+function isNotDuplicated(arr, idx, word) {
+  return arr[arr.length - 1] !== word[idx];
+}
 
-  for (let nextChar of rest) {
-    if (temp.length < 1) {
-      temp.push(nextChar);
-    } else {
-      if (temp[temp.length - 1] === nextChar) temp.pop();
-      else temp.push(nextChar);
+function problem2(cryptogram) {
+  let temp = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (isNotDuplicated(temp, i, cryptogram)) temp.push(cryptogram[i]);
+    else {
+      if (temp[temp.length - 1] === cryptogram[i + 1]) i += 1;
+      if (cryptogram[i] === cryptogram[i + 1]) i += 1;
+      if (i === 2 && temp.length === 2) {
+        temp.pop();
+        break;
+      }
+      temp.pop();
     }
   }
 
