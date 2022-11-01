86ed45a2bda3ea0c7a4ea4b8ae1c1d6d5e3f0d6b
OrangeChick <retww@naver.com>
2022-11-01 21:00:53 +0900

    feat: ⛳️ Completed solution of problem 2
    - 2번 문제 풀이를 완료하였습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,16 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
-
   let splitWord = cryptogram.split("");
 
-  for (let i = 0; i < splitWord.length; i++) {
-    if (splitWord[i] === splitWord[i + 1]) {
-      splitWord.splice(i, 2);
-      result = true;
+  let result = true;
+
+  while (result) {
+    result = false;
+    for (let i = 0; i < splitWord.length; i++) {
+      if (splitWord[i] === splitWord[i + 1]) {
+        splitWord.splice(i, 2);
+        result = true;
+      }
     }
   }
 
-  return answer;
+  return splitWord.join("");
 }
 
 module.exports = problem2;
