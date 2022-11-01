2d368b98ce81afbd00987151fa8c3454c620e95d
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 20:44:12 +0900

    problem7) 함께 알고있는 친구 점수 반영

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,26 @@
+function applyKnownScore(scores, userFriends, relations, user) {
+  userFriends.forEach((uFr) => {
+    const friendKnownList = relations[uFr];
+
+    friendKnownList.forEach((fKnown) => {
+      if (fKnown === user) return;
+      if (isFriend(userFriends, fKnown)) return;
+      scores[fKnown] += 10;
+    });
+  });
+
+  return scores;
+}
+
+function applyVisitScore(scores, userFriends, visitors) {
+  visitors.forEach((visitor) => {
+    if (isFriend(userFriends, visitor)) return;
+    scores[visitor] += 1;
+  });
+
+  return scores;
+}
+
 function getRecommandScores(allUser) {
   const result = {};
 
@@ -44,11 +67,8 @@ function problem7(user, friends, visitors) {
   const userFriends = relations[user];
   const allUser = getAllUser(relations, visitors);
   const recommandScores = getRecommandScores(allUser);
-
-  visitors.forEach((visitor) => {
-    if (isFriend(userFriends, visitor)) return;
-    recommandScores[visitor] += 1;
-  });
+  applyVisitScore(recommandScores, userFriends, visitors);
+  applyKnownScore(recommandScores, userFriends, relations, user);
 
   return answer;
 }
