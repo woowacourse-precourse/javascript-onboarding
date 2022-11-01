568cc54203b0bdd314cc508a1eca6cf605c3df2c
n0eyes <doll4586633@naver.com>
2022-11-01 20:02:06 +0900

    feat: [P6] 캐시를 통해 닉네임 중복 여부를 조회 및 업데이트하는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,18 @@
+function checkAndUpdateCache(form, cache) {
+  const [email, nickname] = form;
+  const newCache = { ...cache, result: [...cache.result] };
+
+  for (let i = 0; i < nickname.length - 1; i++) {
+    const piece = nickname.slice(i, i + 2);
+
+    newCache[piece]
+      ? (newCache.result = [...newCache.result, ...newCache[piece], email])
+      : (newCache[piece] = [email]);
+  }
+
+  return newCache;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
