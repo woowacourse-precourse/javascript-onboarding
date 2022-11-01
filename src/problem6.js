733bcc3b12b9fc653202ea34eb13e1bc3c695bca
Heeseung <gmltmd0816@naver.com>
2022-11-01 15:33:20 +0900

    Solved: Problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,24 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var splittedName = {};
+
+  const isDuplicated = (word) => {
+    return splittedName[word] === undefined ? false : true;
+  }
+
+  let answer = forms.reduce((acc, [email, nickname], idx) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      let word = nickname.slice(i, i + 2);
+
+      if (isDuplicated(word)) {
+        acc.add(email).add(forms[splittedName[word]][0]);
+      } else {
+        splittedName[nickname.slice(i, i + 2)] = idx;
+      }
+    }
+    return acc;
+  }, new Set());
+
+  return Array.from(answer).sort((a, b) => a - b);
 }
 
 module.exports = problem6;
