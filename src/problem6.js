674d1790170279952c68616a2eed5d295f0723d4
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 23:08:07 +0900

    problem6 solution

--- src/problem6.js
+++ src/problem6.js
@@ -7,8 +7,52 @@ for loop으로 forms iterate
 */
 
 function problem6(forms) {
-  var answer;
+  var answer = solution(forms);
   return answer;
+}
+
+function solution(forms) {
+  let result = [forms.length];
+  let emails = [forms.length];
+  let hashMap = new Map();
+  for (var i=0; i<forms.length; i++) {
+    var name = forms[i][1];
+    //console.log("name: ", name)
+    for (var j=0; j<name.length-1; j++) {
+      let key = name.substring(j, j+2);
+      //console.log("key: ", key)
+      if(hashMap.has(key)) {
+        console.log("DFSDGDGS")
+        var email = hashMap.get(key);
+        console.log("email: ", email)
+        //console.log("forms: ", forms[i][0])
+        //console.log("email: ", email)
+        if(email != forms[i][0]) {
+          emails.push(email);
+          emails.push(forms[i][0]);
+          console.log("not in emails: ", emails.unshift(forms[i][0]))
+        }
+      } else {
+        break;
+      }
+      hashMap.set(key, forms[i][0]);
+      //console.log("hashMap: ", hashMap.get(key, forms[i][0]))
+    }
+  }
+
+  for (var i=0; i<result.length; i++) {
+    for (var j=0; j<emails.length; j++) {
+      //console.log("result: ", forms[i][0])
+      //console.log("emails: ", forms[i][0])
+      if (result[i] === emails[j]) {
+        result[i] = emails[j];
+      } else {
+        break;
+      }
+    }
+    console.log("result!!!!!: ", result.toString())
+  }
+  return result;
 }
 
 module.exports = problem6;
