7ef149c3286b0246c0cab4cb2d91b960b2f216a5
OrangeChick <retww@naver.com>
2022-11-01 21:08:10 +0900

    feat: 🎒[Problem4] Convert to tree frog letters
    - 조건에 맞게 청개구리 사전에 맞춘 글자로 변환하는 기능을 구현하였습니다.

--- src/problem4.js
+++ src/problem4.js
@@ -2,9 +2,20 @@ function problem4(word) {
   var answer;
 
   let asciiArr = [];
+  let resultArr = [];
 
   for (let i = 0; i < word.length; i++) {
     asciiArr.push(word.charCodeAt(i));
+  }
+
+  for (let i = 0; i < asciiArr.length; i++) {
+    if (asciiArr[i] === 32) {
+      resultArr.push(32);
+    } else if (asciiArr[i] >= 65 && asciiArr[i] <= 90) {
+      resultArr.push(155 - asciiArr[i]);
+    } else {
+      resultArr.push(219 - asciiArr[i]);
+    }
   }
 
   return answer;
