7bc11f4b317a7ece54f8fa9f5a05f4b6624b5ebf
loco9939 <kls9939@naver.com>
2022-11-01 12:58:25 +0900

    refactor: Modify business logic
    - moneyInPulse 변수를 제거하였습니다.
    - map 함수를 forEach 문으로 변경하였습니다.
    - parseInt()를 Math.floor()로 변경하였습니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,14 +1,15 @@
 function problem5(money) {
   if (!money || money > 1000000) throw new RangeError('매개변수는 1 이상 1,000,000 이하의 자연수여야 합니다.');
 
-  let moneyInPulse = money;
   const papers = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  
-  return papers.map((paper) => {
-    const count = parseInt(moneyInPulse / paper);
-    moneyInPulse = moneyInPulse % paper;
-    return count;
-  });
+  let paperCount = [];
+
+  papers.forEach(paper => {
+    paperCount = [...paperCount, Math.floor(money / paper)];
+    money %= paper;
+  })
+
+  return paperCount;
 }
 
 module.exports = problem5;
