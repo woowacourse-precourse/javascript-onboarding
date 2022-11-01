848f454986cd279acd9d60137b3d97e147f48988
OrangeChick <retww@naver.com>
2022-11-01 21:34:25 +0900

    feat: 🎒[Problem6] Remove single-letter words
    - 배열 요소 중 한 글자 단어를 제거하였습니다.

--- src/problem6.js
+++ src/problem6.js
@@ -2,10 +2,17 @@ function problem6(forms) {
   var answer;
 
   let twoWord = [];
+  let wordArr = [];
 
   for (let i = 0; i < forms.length; i++) {
     for (let j = 0; j < forms[i][1].length; j++) {
       twoWord.push(forms[i][1].slice(j, j + 2));
+    }
+  }
+
+  for (let i = 0; i < twoWord.length; i++) {
+    if (twoWord[i].length !== 1) {
+      wordArr.push(twoWord[i]);
     }
   }
 
