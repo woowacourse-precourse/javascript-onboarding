7f00303f227825faafa7630729d8a94d4d1232ab
Ssoco <dlscjs8646@gmail.com>
2022-11-01 23:12:48 +0900

    p7: 친구와 친구 아닌 사람들 찾기

--- src/problem7.js
+++ src/problem7.js
@@ -5,6 +5,17 @@ function problem7(user, friends, visitors) {
   // user의 친구 목록
   const userFriends = [];
   const others = [];
+
+  // user의 친구찾기
+  for (let i = 0; i < friends.length; ++i) {
+    if (friends[i].includes(user)) {
+      if (friends[i][0] === user) userFriends.push(friends[i][1]);
+      else userFriends.push(friends[i][0]);
+    } else {
+      others.push(friends[i]);
+    }
+  }
+
   return answer;
 }
 
