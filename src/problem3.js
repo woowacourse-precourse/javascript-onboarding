afd65800ecf50856b50a17c7a005248f466c4029
gogh21 <gogh.phy@gmail.com>
2022-11-01 23:04:55 +0900

    feat: implement solution for problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i=1; i < number+1; i++){
+      call_num = String(i);
+
+      for (let j = 0; j < call_num.length; j++){
+          if (call_num[j] == "3" || call_num[j] =="6" || call_num[j] == "9"){
+              answer += 1;
+          }
+      }
+  }
   return answer;
 }
+
+
 
 module.exports = problem3;
