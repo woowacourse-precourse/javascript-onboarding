77c831aaae9a646d56aa4956b743ac685a3ed933
HYEOK9 <leejaehyuck9@gmail.com>
2022-11-01 13:13:23 +0900

    feat:solve problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,37 @@
 function problem5(money) {
-  var answer;
-  return answer;
+    let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    while (money) {
+        if (50000 <= money) {
+            money -= 50000;
+            answer[0]++;
+        } else if (10000 <= money) {
+            money -= 10000;
+            answer[1]++;
+        } else if (5000 <= money) {
+            money -= 5000;
+            answer[2]++;
+        } else if (1000 <= money) {
+            money -= 1000;
+            answer[3]++;
+        } else if (500 <= money) {
+            money -= 500;
+            answer[4]++;
+        } else if (100 <= money) {
+            money -= 100;
+            answer[5]++;
+        } else if (50 <= money) {
+            money -= 50;
+            answer[6]++;
+        } else if (10 <= money) {
+            money -= 10;
+            answer[7]++;
+        } else if (1 <= money) {
+            money -= 1;
+            answer[8]++;
+        }
+    }
+    console.log(answer);
+    return answer;
 }
-
+problem5(50237);
 module.exports = problem5;
