7c643634db3c76d8f5366749d0b749d9b35bb96f
soomin <qkrtnals970804@gmail.com>
2022-11-01 17:13:01 +0900

    [problem6] 닉네임 중복 단어 이메일 받아오기 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  forms.forEach((form) => {
+    form.push(0);
+  });
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      let nickname = forms[i][1][j] + forms[i][1][j + 1];
+      for (let k = i + 1; k < forms.length; k++) {
+        if (forms[k][1].includes(nickname)) {
+          if (!forms[i][2]) {
+            answer.push(forms[i][0]);
+            forms[i][2] = 1;
+          }
+          if (!forms[k][2]) {
+            answer.push(forms[k][0]);
+            forms[k][2] = 1;
+          }
+        }
+      }
+    }
+  }
+
+  return answer.sort();
 }
 
 module.exports = problem6;
