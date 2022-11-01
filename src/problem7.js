1c7b47ce2ef1323958067f68a98cba63fd333ca5
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 23:42:02 +0900

    feats: [PROBLEM7] 각자 사람들의 친구 목록 구하기 추가(feat1)

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,41 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  // 각자 사람들의 친구 구하기
+  const getFirends = (friends) => {
+    const result = {};
+    friends.forEach((value) => {
+      const [user1, user2] = value;
+      if (!(user1 in result)) {
+        result[user1] = [user2];
+      } else {
+        result[user1].push(user2);
+      }
+
+      if (!(user2 in result)) {
+        result[user2] = [user1];
+      } else {
+        result[user2].push(user1);
+      }
+    });
+    return result;
+  };
+
+  const friendsDict = getFirends(friends);
+  console.log(friendsDict);
+
   return answer;
 }
-
+problem7(
+  'mrko',
+  [
+    ['donut', 'andole'],
+    ['donut', 'jun'],
+    ['donut', 'mrko'],
+    ['shakevan', 'andole'],
+    ['shakevan', 'jun'],
+    ['shakevan', 'mrko'],
+  ],
+  ['bedi', 'bedi', 'donut', 'bedi', 'shakevan']
+);
 module.exports = problem7;
