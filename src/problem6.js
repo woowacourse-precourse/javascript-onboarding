7294f39df2d207ca651f4eb21672830747cf42ba
Yesong <to06109@naver.com>
2022-11-01 15:34:08 +0900

    [1주차 6번] REFACTOR: 변수명 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,15 +1,20 @@
+const EMAIL = 0;
+const NICKNAME = 1;
+const DUPLICATE_NICK_LENGTH = 2;
+const DOMAIN_FIRST_INDEX = -10;
+
 function problem6(forms) {
   let answer = [];
 
   for (let form of forms) {
-    const cases = words(form[1]);
+    const cases = words(form[NICKNAME]);
     // 다른 사람들 닉네임이 이 단어 포함하는지 확인
     for (let word of cases) {
       for (let index = 0; index < forms.length; index++) {
-        if (forms[index][0] === form[0]) continue;
-        if (forms[index][1].includes(word)) {
-          answer.push(forms[index][0]);
-          answer.push(form[0]);
+        if (forms[index][EMAIL] === form[EMAIL]) continue;
+        if (forms[index][NICKNAME].includes(word)) {
+          answer.push(forms[index][EMAIL]);
+          answer.push(form[EMAIL]);
         }
       }
     }
@@ -19,8 +24,8 @@ function problem6(forms) {
 
   answer = [...set];
   answer.sort((a, b) => {
-    const a_nick = a.slice(0, -10);
-    const b_nick = b.slice(0, -10);
+    const a_nick = a.slice(0, DOMAIN_FIRST_INDEX);
+    const b_nick = b.slice(0, DOMAIN_FIRST_INDEX);
 
     if (a_nick > b_nick) return 1;
     if (a_nick < b_nick) return -1;
@@ -35,7 +40,7 @@ function words(name) {
   const wordOfCases = [];
 
   for (let index = 0; index < name.length - 1; index++) {
-    wordOfCases.push(name.substr(index, 2));
+    wordOfCases.push(name.substr(index, DUPLICATE_NICK_LENGTH));
   }
 
   return wordOfCases;
