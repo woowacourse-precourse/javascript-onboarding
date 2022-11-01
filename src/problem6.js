5d54f75ee172b3d25a8c7881527b156db27396ad
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 22:00:48 +0900

    problem6 : 중복된 문자열이 있는지 확인하는 함수
    lapover에 넣어서 반복되는 문자가 있으면 1을 반환

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,11 @@
+function lapover(a, b) {
+  for (let i = 0; i < a.length - 1; i++) {
+    const word = a.slice(i, i + 2);
+    if (b.indexOf(word) !== -1) return 1;
+  }
+  return 0;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
