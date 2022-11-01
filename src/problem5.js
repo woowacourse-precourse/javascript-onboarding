5b42a404fc412bbb1d06be8ff18ac678cafc2044
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 12:11:00 +0900

    feat(problem5): 예외사항을 적용한다

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,9 @@
 function problem5(money) {
 
+  if(money < 1 || money > 1000000) {
+    return false;
+  }
+  
   let moneyArr = [50000,10000, 5000, 1000, 500, 100, 50, 10, 1];
   let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
 
