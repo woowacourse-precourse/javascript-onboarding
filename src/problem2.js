0b34db4046d64c27934504c173779cd699bec988
meeeeju <meeju06@gmail.com>
2022-11-01 16:09:28 +0900

    221101 problem3 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,29 +1,45 @@
 function problem2(cryptogram) {
 
-  let answer;
-  
-  //문자열은 불변값이기 때문에 항상 새로운 문자열을 생성한 후 반환하므로 문자열을 배열로 바꿔주기
   let cryptogramList = Array.prototype.slice.call(cryptogram);
+  const count=cryptogramList.length/2
 
+  let current=count*2
 
-  //전 문자의 인덱스를 저장해서 현재 문자와 같을시 값 삭제해주기
-  for (let i=0;i<cryptogramList.length;i++){
-    if (i == 0){
-      prev=0;
-      continue;
-    }
-    if (cryptogramList[i]==cryptogramList[prev])
-    {
-      cryptogramList.slice(prev,2)
+  for (let j=0;j<Math.ceil(count);j++)
+  {
+      let prev= current
+      current=oneCycle(cryptogramList)
+      if (prev ==current)
+          break
   
-    }  
-      
   }
-  
+
+  var answer= cryptogramList.join("");
   return answer;
 }
 
 
+function oneCycle(cryptogramList){
+  for (let i=0;i<cryptogramList.length;i++){
+  
+      if (i == 0){
+          prev=0;
+          continue;
+      }
+      if (cryptogramList[i]==cryptogramList[prev])
+      {
+          cryptogramList.splice(prev,2)
+          console.log(cryptogramList)
+          i=i-2
+          prev=i-1
+      }
+      else{
+          prev=i
+      }
+  }
 
+  return cryptogramList.length
+
+}
 
 module.exports = problem2;
--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,70 @@
 function problem3(number) {
-  var answer;
-  return answer;
+
+
+  numList= Array.prototype.slice.call(String(number));
+  //console.log(numList.at(-1),numList[0],numList.length);
+
+  //첫번째 자릿수, 마지막 자릿수, 숫자의 길이 저장해주기
+  //const lastDigit = Number(numList.at(-1));
+  const firstDigit=Number(numList[0]);
+  const iterationNumer= Number(numList.length);
+  let totalCount=[]
+
+  //숫자가 10보다 작은 경우
+  if (number<10)
+      return less10(number);
+
+  //숫자가 10보다 큰 경우
+  for (let i=0; i<iterationNumer; i++){
+
+      if (i==0)
+      {
+          totalCount.push(3); 
+          continue; 
+      }
+
+      if (i==(iterationNumer-1))
+      {
+          //첫째자리 수보다 1을 뺀 수까지 횟수 계산해주기 
+          temp =totalCount.at(-1)*(firstDigit-1)
+          if (temp)
+              totalCount.push(temp)
+
+          smallNumber=number%(10**(i));
+          temp2= problem3(smallNumber);
+
+          if (!(firstDigit%3))
+              temp2= temp2 +(parseInt(firstDigit/3)-1)*(10**i) + (smallNumber + 1)
+          totalCount.push(temp2)
+      }
+
+      else{
+          prevCount=totalCount.at(-1)
+          temp= prevCount * 9 + 3*(10**(i))
+          totalCount.push(temp)
+      }
+  }
+
+  let sum=0
+  totalCount.forEach(element => {
+      sum+=element
+  });
+
+  
+  return sum
 }
 
+
+
+
+function less10(number){
+  if (number<3) return 0;
+  if (number<6) return 1;
+  if (number<9) return 2;
+  if (number<10) return 3;
+
+}
+
+
+console.log(problem3(33));
 module.exports = problem3;
