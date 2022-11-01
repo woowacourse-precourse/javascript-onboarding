bed81a0b31bbd7df4d717a0ce2c84997360102e6
박승훈 <tmdgns1126@naver.com>
2022-11-01 21:56:20 +0900

    [P7] Fix: 예외처리 추가 및 1차 친구 필터 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -18,10 +18,8 @@ function problem7(user, friends, visitors) {
   };
 
   // 대상 유저의 주변인들에 대한 점수 계산 함수
-  const checkFriendScore = (user, relations) => {
-    const myFriends = relations[user];
+  const checkFriendScore = (user, relations, myFriends) => {
     const scores = {};
-
     myFriends.forEach((myFriend) => {
       const theirFriends = relations[myFriend];
       theirFriends.forEach((theirFriend) => {
@@ -38,10 +36,17 @@ function problem7(user, friends, visitors) {
   };
 
   // 방문자 점수를 포함한 점수 계산 함수
-  const checkVisitors = (user, score, visitors) => {
-    const myFriends = relations[user];
+  const checkVisitors = (user, score, visitors, myFriends) => {
     visitors.forEach((visitor) => {
-      if (!myFriends.has(visitor)) {
+      if (myFriends) {
+        if (!myFriends.has(visitor)) {
+          if (score[visitor]) {
+            score[visitor] += 1;
+          } else {
+            score[visitor] = 1;
+          }
+        }
+      } else {
         if (score[visitor]) {
           score[visitor] += 1;
         } else {
@@ -62,12 +67,27 @@ function problem7(user, friends, visitors) {
     });
   };
 
+  // 유저의 1차 친구들을 걸러내는 함수
+  const filterFriends = (names, myFriends) => {
+    const ret = [];
+    for (let i = 0; i < names.length; i++) {
+      const name = names[i];
+      if (!myFriends.has(name)) {
+        ret.push(name);
+        if (ret.length === 5) break;
+      }
+    }
+    return ret;
+  };
+
   // 함수 적용 후 결과값 반환
   const relations = createRelations(friends);
-  const friendScore = checkFriendScore(user, relations);
-  const totalScores = checkVisitors(user, friendScore, visitors);
+  const myFriends = relations[user] || new Set();
+  const friendScore = checkFriendScore(user, relations, myFriends);
+  const totalScores = checkVisitors(user, friendScore, visitors, myFriends);
   const sortedResult = sortByScoreAndName(totalScores);
-  return sortedResult.slice(0, 5);
+  const result = filterFriends(sortedResult, myFriends);
+  return result;
 }
 
 module.exports = problem7;
