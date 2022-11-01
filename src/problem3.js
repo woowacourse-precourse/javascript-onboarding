73576deff0e5f5e79b773735ee4dd8d027f5d44f
yoon052 <ynj3126@hanmail.net>
2022-11-01 12:04:32 +0900

    feat: 3번 문제 해결
    [1주차] 3번 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  return getNum(number);
+}
+
+function getNum(number) {
+  var clap = 0;
+  var cNum = 1;
+
+  while(cNum <= number) { 
+    digits = cNum.toString().split('').map(Number);
+    digits.forEach(digit => {
+      if ([3, 6, 9].includes(digit))  clap += 1;
+    });
+    cNum += 1;
+  }
+  return clap;
 }
 
 module.exports = problem3;
