b8804d0e9eebf7d18c2dff7bf593b8688fd434a6
ywoo12321 <ywoo12321@gmail.com>
2022-11-01 12:46:20 +0900

    problem 4 해결방안 구상

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
-function problem4(word) {
-  var answer;
+const problem4 = (word) => {
+  let answer;
   return answer;
-}
+};
+
+/*
+| A   | B   | C   | D   | E   | F   | G   | H   | I   | J   | K   | L   | M   | N   | O   | P   | Q   | R   | S   | T   | U   | V   | W   | X   | Y   | Z   |
+| --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- | --- |
+| Z   | Y   | X   | W   | V   | U   | T   | S   | R   | Q   | P   | O   | N   | M   | L   | K   | J   | I   | H   | G   | F   | E   | D   | C   | B   | A   |
+
+*/
 
 module.exports = problem4;
