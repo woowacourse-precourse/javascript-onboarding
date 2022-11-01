f857233628024a12c1ab2cc007d6a47ba2c6a77d
oseunghyun <ohsh1477@gmail.com>
2022-11-01 09:34:42 +0900

    feat(p4): 아스키코드를 기준으로 하는 청개구리 사전 객체 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,13 @@
+// 아스키코드를 기준으로 하는 청개구리 사전 객체
+const treefrogDic = {};
+
+for (i = 0; i < 26; i++) {
+  treefrogDic[String.fromCharCode(65 + i)] = String.fromCharCode(90 - i);
+}
+for (i = 0; i < 26; i++) {
+  treefrogDic[String.fromCharCode(97 + i)] = String.fromCharCode(122 - i);
+}
+
 function problem4(word) {
   var answer;
   return answer;
