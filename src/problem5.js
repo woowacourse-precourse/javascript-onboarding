cb02e84e5b64eb4060aec1c4bfc031c9a93ceb46
leesoyuun <leesoyun4206@naver.com>
2022-11-01 20:40:43 +0900

    [Problem 5] 지갑을 가볍도록 큰 금액의 화폐를 받음

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  let change_money = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+  for (let i = 0; i < change_money.length; i++) {
+      answer.push(Math.floor(money / change_money[i]));
+      money = money % change_money[i];
+  }
   return answer;
 }
 
