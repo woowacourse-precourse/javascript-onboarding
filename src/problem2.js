4b8241c1be15023d5bd36de1583c22bb1636986a
emma <jhj191058@gmail.com>
2022-11-01 15:58:41 +0900

    upload

--- src/problem2.js
+++ src/problem2.js
@@ -1,18 +1,6 @@
 function problem2(cryptogram) {
   var answer;
 
-  for( var i = 0; i<n; i++) {
-    var j;
-
-    for( j = 0; j < i; j++) {
-      break;
-    }
-
-    if( j == 1 ) {
-      cryptogram[answer++] = cryptogram[i];
-    }
-  }
-  return cryptogram.join("").slice(cryptogram, answer);
 }
 
 
--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,7 @@
 function problem5(money) {
 
-  var arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  var answer = new Array();
-  
-  for( var i = 0; i< arr.length; i++) {
-    answer[i] = Math.floor(money/arr[i]);
-    money -= arr[i]*Math.floor(money/arr[i]);
-}
-  return answer;
+
+
 }
 
 module.exports = problem5;
