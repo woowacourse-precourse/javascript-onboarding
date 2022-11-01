00ad80e0b0b43944a5fcb2512402b5b06cb12845
Josanghyeon <BStkdgus.dev@gmail.com>
2022-11-01 22:18:50 +0900

    refactor: 함수의 단일책임 원칙 준수

--- src/problem4.js
+++ src/problem4.js
@@ -1,13 +1,21 @@
+const ABC_REGEX = /^[a-z|A-Z]+$/;
+
+// 소문자면 false 대문자면 true를 리턴하는 함수
+function strIsLower(str) {
+  return str === str.toLowerCase();
+}
+
 function problem4(word) {
-  let ans = '';
-  let ascii;
-  for (let i = 0; i < word.length; i += 1) {
-    if (word[i] === ' ') ans += word[i];
-    else {
-      ascii = word[i].charCodeAt(0);
-      ans += String.fromCharCode(ascii >= 65 && ascii <= 90 ? 155 - ascii : 219 - ascii);
-    }
+  // 청개구리 알파벳 변환 함수
+  function abcFrogConversion(str) {
+    return String.fromCharCode((strIsLower(str) ? 219 : 155) - str.charCodeAt(0));
   }
+
+  let ans = '';
+  for (let i = 0; i < word.length; i += 1) {
+    ans += ABC_REGEX.test(word[i]) ? abcFrogConversion(word[i]) : word[i];
+  }
+
   return ans;
 }
 
