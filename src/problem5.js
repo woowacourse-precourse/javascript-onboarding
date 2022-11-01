7c91996733591fd799b26579e825613256308e1a
NYeonK <skdus728@gmail.com>
2022-11-01 20:37:27 +0900

    [ feat ] 화폐의 개수를 최소화하여 출금하는 기능 구현 #PROBLEM5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,26 @@
 function problem5(money) {
-  var answer;
+  let answer = [0,0,0,0,0,0,0,0,0];
+
+  let paper = 50000; // 화폐
+  let firstNum = 5; // 화폐 단위 앞자리
+        
+  for (let i = 0; i < answer.length; i++) {
+      answer[i] = parseInt(money / paper);
+      money = money % paper;
+      
+      if (firstNum === 5) {
+        firstNum = 1;
+        paper /= 5;
+      }
+      else if (firstNum === 1 && money < 10) {
+        paper /= 10;
+      }
+      else {
+        firstNum = 5;
+        paper /= 2;
+      }
+  }
+
   return answer;
 }
 
