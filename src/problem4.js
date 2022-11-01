4be1585250fbadace8f0fb71c321b1b05c9bdd1a
n0eyes <doll4586633@naver.com>
2022-11-01 17:55:19 +0900

    feat: [P4] 입력받은 문자가 알파벳인지 판별하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,11 @@
+function isAlphabet(character) {
+  const regex = /^[a-z|A-Z]{1}/;
+
+  if (character.length > 1) return false;
+
+  return regex.test(character);
+}
+
 function problem4(word) {
   var answer;
   return answer;
