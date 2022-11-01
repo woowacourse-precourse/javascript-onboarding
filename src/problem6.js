cd618cc662aa7d2c3e44c1041b714e9a7b6d7f16
yeongseokim <1903020@ewhain.net>
2022-11-01 22:43:30 +0900

    feat(problem6): forms에서 크루원 한 명을 꺼내 닉네임을 연속된 두 글자 단위로 분해

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,20 @@
+function splitName(name) {
+  let arr = [];
+  if (name.length <= 2) return [name];
+  for (let i = 0; i < name.length - 1; i++) {
+    arr.push(name.substr(i, 2));
+  }
+  return arr
+}
+
 function problem6(forms) {
   var answer;
+
+  for (let i = 0; i < forms.length - 1; i++) {
+    let splitedName = splitName(forms[i][1]);
+
+  }
+
   return answer;
 }
 
