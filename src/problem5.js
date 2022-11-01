e668402d1f9d0c636771404f846286816fd1bcfc
ehdwns1516 <ehdwns1516@ajou.ac.kr>
2022-11-01 17:32:59 +0900

    problem5 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  const money_unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let curr = 0;
+
+  while (curr < money_unit.length) {
+    answer.push(parseInt(money / money_unit[curr]));
+    money = money % money_unit[curr];
+    curr++;
+  }
+
   return answer;
 }
 
