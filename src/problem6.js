2aeabebf7f948e718688a89d5c62252e0e9c9a15
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 19:13:28 +0900

    problem5) 입력된 이메일,닉네임의 적절성을 판단하는 함수 정의

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,15 @@
+const getMatchedStr = (regExp, str) => {
+  if (!regExp.test(str)) return '';
+  return str.match(regExp)[0];
+};
+
+const isMatched = (regExp) => (str) => str === getMatchedStr(regExp, str);
+
 function problem6(forms) {
   var answer;
+  const isProperKorean = isMatched(/[가-힣]{1,19}/);
+  const isProperEmail = isMatched(/.{1,9}@email.com$/);
+
   return answer;
 }
 
