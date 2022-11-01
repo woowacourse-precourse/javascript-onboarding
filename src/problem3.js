ed0f7e66fb72cc174baf0c59452de69eafc46202
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-01 16:59:24 +0900

    feature(problem3): getClapCount 함수 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,20 @@
 function problem3(number) {
-  var answer;
+  // 369 박수의 횟수 구하기
+  const getClapCount = (number) => {
+    let clap_count = 0;
+    // 369 정규표현식
+    const regex = /[369]/g;
+
+    // 369가 있으면 배열로 반환되고 길이를 더해줌
+    for (let i = 1; i <= number; i++) {
+      const clap = String(i).match(regex);
+      if (clap) clap_count += clap.length;
+    }
+
+    return clap_count;
+  };
+
+  let answer;
   return answer;
 }
 
