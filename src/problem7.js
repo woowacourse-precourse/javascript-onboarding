9fcfd2e5e3e958af6c62f0ec466e64b60b5a1ff4
seungki <sysh9498@gmail.com>
2022-11-01 18:30:53 +0900

    friends 배열에서 user와 친구인 사람을 뽑는 부분 구현

--- src/problem7.js
+++ src/problem7.js
@@ -11,41 +11,6 @@ function problem7(user, friends, visitors) {
       continue;
     }
   }
-  for (var i = 0; i < user_friend.length; i++) {
-    for (var j = 0; j < friends.length; j++) {
-      if (friends[j][0] == user_friend[i]) {
-        if (map.has(friends[j][1])) {
-          map.set(friends[j][1], map.get(friends[j][1]) + 10);
-          continue;
-        }
-        map.set(friends[j][1], 10);
-      } else if (friends[j][1] == user_friend[i]) {
-        if (map.has(friends[j][0])) {
-          map.set(friends[j][0], map.get(friends[j][0]) + 10);
-          continue;
-        }
-        map.set(friends[j][0], 10);
-      }
-    }
-  }
-  for (var i = 0; i < visitors.length; i++) {
-    if (map.has(visitors[i])) {
-      map.set(visitors[i], map.get(visitors[i]) + 1);
-    } else {
-      map.set(visitors[i], 1);
-    }
-  }
-  map.delete(user);
-  for (var i = 0; i < user_friend.length; i++) {
-    map.delete(user_friend[i]);
-  }
-  const arraymap = [...map];
-  arraymap.sort();
-  arraymap.sort((a, b) => b[1] - a[1]);
-  for (var i = 0; i < arraymap.length; i++) {
-    answer.push(arraymap[i][0]);
-  }
-
   return answer;
 }
 
