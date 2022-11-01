a4c9fbbadec5073ed61aa0e9265314c5825fb7da
areumsheep <areumsheep@gmail.com>
2022-11-01 21:05:44 +0900

    refactor: 문제5 예외 처리 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,9 +1,15 @@
+const MESSAGES = Object.freeze({
+  ERROR_RANGE: '금액은 1 이상 1,000,000 이하의 자연수여야 합니다.',
+});
+const MAX_MONEY_RANGE = 1000000;
 const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money = 0) {
   const result = [];
+  if (money < 1 || money > MAX_MONEY_RANGE) return MESSAGES.ERROR_RANGE;
+
   MONEY_UNIT.forEach((unit) => {
-    result.push(Math.trunc(money / unit));
+    result.push(parseInt(money / unit));
     money = money % unit;
   });
   return result;
