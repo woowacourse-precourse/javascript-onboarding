eebfb1ea4dd6ae5813aa6b11b900dabf0b3fabfe
cyberjam <jamsince1994@naver.com>
2022-11-01 13:55:29 +0900

    problem5 complete

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,24 @@
 function problem5(money) {
   var answer;
+  const result = [];
+
+  const getRemains = (money, divisor) => {
+    let quotient = parseInt(money / divisor);
+    result.push(quotient);
+    return money - quotient * divisor;
+  };
+
+  money = getRemains(money, 50000);
+  money = getRemains(money, 10000);
+  money = getRemains(money, 5000);
+  money = getRemains(money, 1000);
+  money = getRemains(money, 500);
+  money = getRemains(money, 100);
+  money = getRemains(money, 50);
+  money = getRemains(money, 10);
+  money = getRemains(money, 1);
+
+  answer = result;
   return answer;
 }
 
