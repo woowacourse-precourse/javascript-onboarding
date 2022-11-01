20cab8d02f7dd564af2676d7b66af2edb2e2dc48
dongyeopca <ckehdduq95@gmail.com>
2022-11-01 15:25:31 +0900

    fix: "Problem4 forgRule 수정" 알파벳 외의 문자는 변환하지 않도록 수정

--- src/problem4.js
+++ src/problem4.js
@@ -1,18 +1,29 @@
 function problem4(word) {
-  let answer = word.split(" ");
-  for (let i = 0; i < answer.length; i++) {
-    answer[i] = frogRule(answer[i]);
-  }
-  return answer.join(" ");
+  let answer;
+  // let answer = word.split(" ");
+  // for (let i = 0; i < answer.length; i++) {
+  //   answer[i] = frogRule(answer[i]);
+  // }
+  answer = frogRule(word);
+  return answer;
 }
 
 function frogRule(s) {
   let answer = "";
   for (const value of s) {
-    if (value === value.toUpperCase()) {
-      answer += String.fromCharCode(155 - value.charCodeAt(0));
+    if (
+      ("a".charCodeAt(0) <= value.charCodeAt(0) &&
+        value.charCodeAt(0) <= "z".charCodeAt(0)) ||
+      ("A".charCodeAt(0) <= value.charCodeAt(0) &&
+        value.charCodeAt(0) <= "Z".charCodeAt(0))
+    ) {
+      if (value === value.toUpperCase()) {
+        answer += String.fromCharCode(155 - value.charCodeAt(0));
+      } else {
+        answer += String.fromCharCode(219 - value.charCodeAt(0));
+      }
     } else {
-      answer += String.fromCharCode(219 - value.charCodeAt(0));
+      answer += value;
     }
   }
   return answer;
