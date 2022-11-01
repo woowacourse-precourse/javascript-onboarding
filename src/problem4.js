50940789deeb406d934ea0e538aac0d39486a601
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 12:21:57 +0900

    Feat: problem4 - 해당 문자열을 모두 찾으면 결과값 리턴

--- src/problem4.js
+++ src/problem4.js
@@ -6,22 +6,24 @@ function problem4(word) {
     LowerStr[String.fromCharCode(i)] = String.fromCharCode(219 - i);
     UpperStr[String.fromCharCode(i - 32)] = String.fromCharCode(219 - i - 32);
   }
-
-  const checkWords = (LowerStr, UpperStr, word) => {
-    let answer = "";
-    for (let i = 0; i < word.length; i++) {
-      if (LowerStr[word[i]]) {
-        answer += LowerStr[word[i]];
-        continue;
-      }
-      if (UpperStr[word[i]]) {
-        answer += UpperStr[word[i]];
-        continue;
-      }
-      answer += word[i];
-    }
-  };
+  checkWords(LowerStr, UpperStr, word);
 }
+
+const checkWords = (LowerStr, UpperStr, word) => {
+  let answer = "";
+  for (let i = 0; i < word.length; i++) {
+    if (LowerStr[word[i]]) {
+      answer += LowerStr[word[i]];
+      continue;
+    }
+    if (UpperStr[word[i]]) {
+      answer += UpperStr[word[i]];
+      continue;
+    }
+    answer += word[i];
+  }
+  return answer;
+};
 
 problem4("I love you");
 module.exports = problem4;
