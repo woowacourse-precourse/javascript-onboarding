f130079c5225c6c378fd49ca35ca99e7d51e4eb0
SeungWoo Kim <osabero@naver.com>
2022-11-01 23:04:11 +0900

    친구 관계를 맵으로 구현하였습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,27 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  var answer = [];
+  var friMap = new Map()
+
+  for(let i = 0; i<friends.length;i++) {
+    if(friMap.has(friends[i][0])) {
+      friMap.get(friends[i][0]).push(friends[i][1])
+    } else {
+      friMap.set(friends[i][0], new Array(friends[i][1]))
+    }
+    if(friMap.has(friends[i][1])) {
+      friMap.get(friends[i][1]).push(friends[i][0])
+    } else {
+      friMap.set(friends[i][1], new Array(friends[i][0]))
+    }
+  }
+
   
+
+ 
+
+
+
+  return answer;
 }
 
 module.exports = problem7;
