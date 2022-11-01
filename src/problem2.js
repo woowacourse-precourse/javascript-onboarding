21585949766d920ec15f2912a3590b6cf2d2c424
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 17:28:47 +0900

    [problem2] feat: while문으로 연속 중복 문자 pop/push 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -3,13 +3,22 @@ function problem2(cryptogram) {
 
   // empty stack 선언
   var stack = [];
-  let arr = [...cryptogram]; // 나머지 배열 할당
+  let arr = [...cryptogram]; // 배열로 할당
 
   // stack에 하나씩 push
-
-  // 연속하는 중복 문자 있으면 pop
-
   // 중복 없을 때까지 반복
+  while (1) {
+    stack.push(arr[0]);
+
+    for (let i = 0; i < arr.length; i++) {
+      // 연속하는 중복 문자 있으면 pop
+      if (stack[i - 1] === arr[i]) {
+        stack.pop();
+      } else {
+        stack.push(arr[i]);
+      }
+    }
+  }
 
   return answer;
 }
