26b9d52d659b33fc1e581536b5f1439e7bde43b8
Hypark2018 <hayoungpark2018@gmail.com>
2022-11-01 23:53:07 +0900

    문제 5번 커밋.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,26 @@
 function problem5(money) {
-  var answer;
+  let answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const moneyCarryUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for(let i=0; i<answer.length; i++) {
+    // const temporary = getCalculateMoney(money, moneyCarryUnit[i]);
+    // answer[i] = temporary[0];
+    // money = temporary[1];
+    [answer[i], money] = getCalculateMoney(money, moneyCarryUnit[i]);
+  }
+
   return answer;
 }
 
+// 현재 금액과 검사할 금액 
+function getCalculateMoney(restMoney, unitMoney) {
+  let unitNum = 0;
+  if(restMoney >= unitMoney) {
+    unitNum = parseInt(restMoney / unitMoney);
+    restMoney -= (unitMoney * unitNum)
+  }
+  return [unitNum, restMoney];
+}
+
 module.exports = problem5;
+
