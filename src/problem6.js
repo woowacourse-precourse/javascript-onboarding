0857c1c2c69388e050c7e243c623f4877e5d4bc3
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-01 19:06:05 +0900

    feat: [PROBLEM6] 찾은 중복된 닉네임에 해당하는 이메일을 배열에 넣어주고 중복 제거 및 정렬 후에 return

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,6 @@
 function problem6(forms) {
-  var answer;
+  var answer = [];
+  let result = [];
   let obj = new Object();
 
   for (const [e, n] of forms) {
@@ -14,13 +15,20 @@ function problem6(forms) {
       if (reg.test(`${nicknames[j]}`) && nicknames[i] !== nicknames[j]) {
         // 정규 표현식으로 찾아낸 중복된 문자가 비교대상이 되었던 닉네임에 포함 되어있는지 확인 
         // -> 중복된 문자가 순서까지 일치하는지 확인하는 과정
-        if(nicknames[i].includes(reg.exec(`${nicknames[j]}`)[0])){
-          console.log(nicknames[i], nicknames[j]);
+        if (nicknames[i].includes(reg.exec(`${nicknames[j]}`)[0])) {
+          result.push(nicknames[i], nicknames[j]);
         }
       }
     }
-
   }
+
+  for (let k = 0; k < result.length; k++) {
+    answer.push(obj[result[k]]);
+  }
+
+  // 중복 제고 & 정렬
+  answer = [...new Set(answer)];
+  answer.sort();
 
   return answer;
 }
