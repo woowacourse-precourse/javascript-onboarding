c9648982d092b625d4ec5c0ce0244f2c0c5dbced
김미진 <kimmijin@gimmijin-ui-MacBookAir.local>
2022-11-01 21:39:07 +0900

    문제 6 해결

--- src/problem6.js
+++ src/problem6.js
@@ -8,7 +8,13 @@ function problem6(forms) {
     var answer;
     let tmpforms = forms;
     let length = tmpforms.length;
+    var saveData = [];
+    let test1Array = [];
+    let test2Array = [];
+    var tmp1Array = [];
+    var tmp2Array = [];
 
+    //예외사항
     if (length < 1 || length > 10000) {
         answer = '크루원은 1명 이상 10000명 이하여야 합니다';
     }
@@ -22,9 +28,43 @@ function problem6(forms) {
         if (tmpArray2[1] != 'email.com') {
             answer = '도메인은 email.com만 가능합니다.';
         }
-
-        let tmpArray[i][0] = tmpforms[i][1];
     }
+
+    for (let i = 0; i < length; i++) {
+        tmp2Array[i] = tmpforms[i][0];
+        tmp1Array[i] = tmpforms[i][1];
+    }
+
+    var tmplength = tmp1Array.length;
+
+    for (let i = 0; i < tmplength; i++) {
+        for (let j = 0; j < tmplength; j++) {
+            if (i == j) {
+                continue;
+            }
+
+            else if (i != j) {
+                let tmp1 = tmp1Array[i];
+                let tmp2 = tmp1Array[j];
+                for (let l = 0; l < tmp1.length - 1; l++) {
+                    for (let k = 0; k < tmp2.length - 1; k++) {
+                        if (tmp1[l] == tmp2[k] && tmp1[l + 1] == tmp2[k + 1]) {
+                            saveData[i] = tmp2Array[i];
+                        }
+                    }
+                }
+            }
+        }
+    }
+
+    saveData.sort();
+    for (let i = 0; i < saveData.length; i++) {
+        if (saveData[i] == saveData[i + 1]) {
+            saveData.splice(i, 1);
+        }
+    }
+
+    answer = saveData;
 
     return answer;
 }
