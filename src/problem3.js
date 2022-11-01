d4c1f7795e303fef2310c4213c387bc348e0ed24
cyberjam <jamsince1994@naver.com>
2022-11-01 13:07:25 +0900

    problem3 complete

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
   var answer;
+
+  const numArr = number.toString().split("");
+  const tens = parseInt(numArr[0]);
+  const units = parseInt(numArr[1]);
+
+  const quotient = parseInt(number / 3);
+  if (tens % 3 === 0) {
+    answer = quotient + units + 1 - parseInt(units / 3);
+  } else {
+    answer = quotient;
+  }
   return answer;
 }
 
