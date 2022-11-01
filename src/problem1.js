56f91ef56ad5b91850eff08c4f7bb27d316b38dd
jellikim <jellikiim@gmail.com>
2022-11-01 14:18:34 +0900

    1

--- src/problem1.js
+++ src/problem1.js
@@ -1,34 +1,47 @@
-// function problem1(pobi, crong) {
-//   var answer;
-//   return answer;
-// }
-
-// module.exports = problem1;
-
 function problem1(pobi, crong) {
   function sum(n) {
-    return String(n)
-      .split("")
-      .reduce((tot, e) => tot + parseInt(e), 0);
+    let plus = 0;
+    do {
+      plus += n % 10;
+      n = Math.floor(n / 10);
+    } while (n > 0);
+    return plus;
   }
 
   function multiple(n) {
-    return String(n)
-      .split("")
-      .reduce((acc, cur) => acc * cur);
+    if (n > 100) {
+      const a = Math.floor(n / 100);
+      const b = Math.floor((n % 100) / 10);
+      const c = (n % 100) % 10;
+      return a * b * c;
+    } else if (n >= 10) {
+      const a = Math.floor(n / 10);
+      const b = n % 10;
+      return a * b;
+    } else return n;
   }
 
-  function pobiOdd(pobi) {
-    return sum(pobi[0]) >= multiple(pobi[0]) ? sum(pobi[0]) : multible(pobi[0]);
+  function array(name) {
+    return [sum(name[0]), sum(name[1]), multiple(name[0]), multiple(name[1])];
   }
 
-  function pobiEven(pobi) {
-    return sum(pobi[1]) >= multiple(pobi[1]) ? sum(pobi[1]) : multible(pobi[1]);
+  function max(name) {
+    return Math.max.apply(null, array(name));
   }
+
   var answer;
+  if (max(pobi) > max(crong)) {
+    var answer = 1;
+  } else if (max(pobi) < max(crong)) {
+    var answer = 2;
+  } else if (max(pobi) === max(crong)) {
+    var answer = 0;
+  }
+  if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) {
+    var answer = -1;
+  }
   return answer;
-  console.log(pobi[1]);
 }
 
-console.log(problem1([97, 98], [197, 198]));
+console.log(problem1([99, 102], [211, 212]));
 module.exports = problem1;
