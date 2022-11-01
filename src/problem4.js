e742fa8d98510f13a65b4b368cfec3ca2dfa1502
Bori-github <qhflrnfl4324@gmail.com>
2022-11-01 10:32:35 +0900

    ✨ problem4 > word에 따라 알파벳 반환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,74 @@
 function problem4(word) {
-  var answer;
+  let answer = '';
+  const UpprerCaseList = [
+    'Z',
+    'Y',
+    'X',
+    'W',
+    'V',
+    'U',
+    'T',
+    'S',
+    'R',
+    'Q',
+    'P',
+    'O',
+    'N',
+    'M',
+    'L',
+    'K',
+    'J',
+    'I',
+    'H',
+    'G',
+    'F',
+    'E',
+    'D',
+    'C',
+    'B',
+    'A',
+  ];
+  const LowerCaseList = [
+    'z',
+    'y',
+    'x',
+    'w',
+    'v',
+    'u',
+    't',
+    's',
+    'r',
+    'q',
+    'p',
+    'o',
+    'n',
+    'm',
+    'l',
+    'k',
+    'j',
+    'i',
+    'h',
+    'g',
+    'f',
+    'e',
+    'd',
+    'c',
+    'b',
+    'a',
+  ];
+
+  for (let i = 0; i < word.length; i++) {
+    const ASCIICode = word.charCodeAt(i);
+    if (ASCIICode === 32) {
+      answer += ' ';
+    } else if (ASCIICode < 91) {
+      const Index = ASCIICode % 65;
+      answer += UpprerCaseList[Index];
+    } else {
+      const Index = ASCIICode % 97;
+      answer += LowerCaseList[Index];
+    }
+  }
   return answer;
 }
 
