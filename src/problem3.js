d07a16cead9dce08fc114c6f8c754bef63d36ef5
LEECHANHYUNG <lsj721538@gmail.com>
2022-11-01 19:13:24 +0900

    filter 3,6,9 in array

--- src/problem3.js
+++ src/problem3.js
@@ -4,6 +4,7 @@
 */
 function problem3(number) {
   const numberArr = String([...Array(number + 1).keys()]).split("");
+  const findArr = numberArr.filter((elem) => ["3", "6", "9"].includes(elem));
 
   return findArr.length;
 }
