68df77880a0e603185d4b11a2abb2726ed1b82d3
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:32:51 +0900

    feat:PROBLEM6:닉네임 중복 여부 검사 구현

--- src/problem6.js
+++ src/problem6.js
@@ -12,4 +12,33 @@ function problem6(forms) {
   return answer;
 }
 
+/*닉네임의 중복 여부를 검사한다.*/
+function compareNickname(nickname1, nickname2) {
+
+  const EQUAL = -1;
+  const BLANK = 0;
+
+if(nickname1===nickname2){
+    return EQUAL;
+}
+
+let subString = "";
+let result="";
+
+for(let i = 0; i <nickname2.length-1; i++) {
+
+    subString = nickname2[i] + nickname2[i+1];
+
+    if(nickname1.indexOf(subString) !== -1) {
+        result = (nickname2);
+    } 
+}
+
+if(result ===''){
+    return BLANK;
+}
+
+return result;
+}
+
 module.exports = problem6;
