798eb8013e5e83873d8b647676afe73d70c4da10
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 10:57:44 +0900

    Problem3: 1부터 number까지 순회하면서 해당 숫자가 포함하는 3, 6, 9의 개수만큼 손뼉수 더하기

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,17 @@
 function problem3(number) {
   /*
   기능 목록
-  1. number에 3, 6, 9가 들어가면 해당 숫자의 개수만큼 손뼉수 더하기
+  1. 1부터 number까지 순회하면서 해당 숫자가 포함하는 3, 6, 9의 개수만큼 손뼉수 더하기
   2. 전체 손뼉수 정답으로 반환
   */
+  
+  let sum = 0;
+  for (i = 1; i <= number; i++)
+  {
+    let array = (i + '').split('');
+    let target = array.filter(v => v === '3' || v === '6' || v === '9');
+    sum += target.length;
+  }
 }
 
 module.exports = problem3;
