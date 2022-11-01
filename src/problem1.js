d041b002e7513efff4bfa71808f482e9a3dc1252
cheonyeji <ssaltuck@gmail.com>
2022-11-01 18:20:24 +0900

    - 불가능한 경우 -1을 반환하도록 케이스 추가(왼, 오른 페이지 간의 차이가 1이 아닌 경우) - 각 숫자 문자열로 변환 -> 배열로 변환하여 반복문을 통해 문제 해결

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,37 @@
+function calculMaxNum(arr) {
+  let result = [];
+
+  arr.forEach((number) => {
+    add_sum = 0;
+    multi_sum = 1;
+    [...number.toString()].forEach((n) => {
+      add_sum += Number(n);
+      multi_sum *= Number(n);
+    });
+
+    result.push(add_sum);
+    result.push(multi_sum);
+  });
+
+  return Math.max(...result);
+}
+
 function problem1(pobi, crong) {
   var answer;
+
+  p = calculMaxNum(pobi);
+  c = calculMaxNum(crong);
+  if (p > c) {
+    answer = 1;
+  } else if (p < c) {
+    answer = 2;
+  } else {
+    answer = 0;
+  }
+
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    answer = -1;
+  }
   return answer;
 }
 
