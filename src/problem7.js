3d2f4d3019bf43f386c4c68d613fbcd41a9f7cb4
Moyujin <scab12341@gmail.com>
2022-11-01 22:56:12 +0900

    7번 친구 관계 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,21 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const relations = getRealtions(friends);
 }
+
+const getRealtions = (friends) => {
+  const relations = {};
+
+  for (let [a, b] of friends) {
+    if (!isDuplicated(a, relations)) relations[a] = new Set();
+    if (!isDuplicated(b, relations)) relations[b] = new Set();
+    relations[a].add(b);
+    relations[b].add(a);
+  }
+  return relations;
+};
+
+const isDuplicated = (data, relations) => {
+  return data in relations;
+};
 
 module.exports = problem7;
