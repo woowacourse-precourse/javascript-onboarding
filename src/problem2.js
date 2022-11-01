6516688d135f4636466ef8b1e3c7ee13e0c8fe1c
imhynni <omelas329@gmail.com>
2022-11-01 13:03:18 +0900

    feat(problem2): 기능 구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
+  let answer;
+
+  const arr = cryptogram.split("");
+  const stack = [arr[0]];
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    if (stack[stack.length - 1] === arr[i]) stack.pop();
+    else stack.push(arr[i]);
+  }
+
+  answer = stack.join("");
+
   return answer;
 }
 
