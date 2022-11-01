b501fecb1fcd44febb8854cb6f7fc204375d5c7b
kimjiwon <hallu0317@gmail.com>
2022-11-01 15:37:28 +0900

    problem2 완료

--- src/problem2.js
+++ src/problem2.js
@@ -3,13 +3,16 @@ function problem2(cryptogram) {
   answer.push(cryptogram[0]);
 
   for(let i=1; i<cryptogram.length; i++) {
+    //answer가 비어있으면 cryptogram[i] push
     if(answer.length === 0) {
       answer.push(cryptogram[i]);
     }
     else {
+      //answer의 마지막 값이 cryptogram[i]의 값과 같으면 마지막 값 삭제
       if(answer[answer.length-1] === cryptogram[i]) {
         answer.pop();
       }
+      //다르다면 cryptogram[i] push
       else{
         answer.push(cryptogram[i])
       }
