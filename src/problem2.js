6c9c0290c5d945b14bd23be40acd4820490dd253
HyeJun <solar3070@naver.com>
2022-11-01 20:00:06 +0900

    feat: problem2/연속된 중복 문자 제거 1. cryptogram을 순회하며 문자를 스택에 push 2. 스택의 탑과 동일한 문자의 경우 중복이므로 중복이 시작되었음을 알리는 isOverlap 변수의 값을 1로 설정 3. 중복이 끝난 경우 스택의 탑을 pop하고 중복 체크 변수의 값을 0으로 변경

--- src/problem2.js
+++ src/problem2.js
@@ -2,7 +2,23 @@ function problem2(cryptogram) {
   cryptogram = cryptogram.split("");
 
   let isChange = 1;
-  while (isChange) {}
+  while (isChange) {
+    const chStack = [];
+    let isOverlap = 0;
+    cryptogram.forEach((ch) => {
+      if (ch === chStack[chStack.length - 1]) {
+        isOverlap = 1;
+      } else {
+        if (isOverlap) {
+          chStack.pop();
+          isOverlap = 0;
+        }
+        chStack.push(ch);
+      }
+    });
+
+    cryptogram = [...chStack];
+  }
 }
 
 module.exports = problem2;
