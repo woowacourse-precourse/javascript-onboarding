48cea82ec5a19a570094a5ef561cf243d6dfffb1
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 17:51:45 +0900

    problem3) 1~number까지 수들의 모든 자릿수 검사

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,19 @@
+function check369(num) {
+  return [3, 6, 9].includes(num % 10);
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let targetNumber = i;
+
+    while (targetNumber > 0) {
+      if (check369(targetNumber)) answer++;
+      targetNumber = Math.floor(targetNumber / 10);
+    }
+  }
+
   return answer;
 }
 
