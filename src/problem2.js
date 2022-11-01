8fbe2d517c4b4c9b6e99d297f0b85fba31481eef
seoyeon <wjstjdus96@naver.com>
2022-11-01 13:52:26 +0900

    feat: problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,10 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while (true) {
+    if (cryptogram.search(/(\w)\1+/g) == -1) break;
+    cryptogram = cryptogram.replace(/(\w)\1+/g, "");
+  }
+
+  return cryptogram;
 }
 
 module.exports = problem2;
