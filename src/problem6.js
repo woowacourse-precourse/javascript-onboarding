04e1355b7891c9f91486e5df61053d0f6ae4c3de
inthhh <glass005533@gmail.com>
2022-11-01 18:20:03 +0900

    p6_fix word length

--- src/problem6.js
+++ src/problem6.js
@@ -2,15 +2,19 @@ function problem6(forms) {
   var answer;
   let len = forms.length;
   let duplMails = new Array(len).fill(0);
+  let word;
 
   for(let i=0; i<len; ++i){
-    for(let j=0; j<len; ++j){
-      let word = forms[i][1].substr(j,2);
-      
+    
+    for(let k=0; k<forms[i][1].length; ++k){
+      word = forms[i][1].substr(k,2);
       if(word.length<2) break;
-      if(forms[j][1].indexOf(word) >= 0) {
-        duplMails[i] = 1;
-        duplMails[j] = 1;
+
+      for(let j=0; j<len; ++j){
+        if(i!=j && forms[j][1].indexOf(word) >= 0) {
+          duplMails[i] = 1;
+          duplMails[j] = 1;
+        }
       }
     }
   }
