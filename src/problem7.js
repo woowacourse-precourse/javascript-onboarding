45f8ad78fcb1089339e06886811a94412a8d1759
durumi99 <kms990615@naver.com>
2022-11-01 15:43:53 +0900

    [problem 7] user의 친구 목록을 Set 객체로 return 하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,18 @@
+// user의 친구 목록을 Set 객체로 return 하는 함수
+function findUserFriends(user, friends){
+  const set = new Set();
+  for(friend of friends){
+    if(friend[0] == user)
+      set.add(friend[1])
+    else if(friend[1] == user)
+      set.add(friend[0])
+  }
+  return set;
+}
 function problem7(user, friends, visitors) {
   var answer;
+  let userFriends = findUserFriends(user, friends);
+  
   return answer;
 }
 
