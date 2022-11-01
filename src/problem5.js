878dc95bb2cd64cf8a61f9596171b1a40daa9f87
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 21:39:02 +0900

    problem5 : current를 map함수로 돌아 몫을 answer에 저장

--- src/problem5.js
+++ src/problem5.js
@@ -2,7 +2,11 @@ function problem5(money) {
 
   const current = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   var answer = [];
-  
+  answer = current.map((cur) => {
+    const ret = Math.trunc(money / cur);
+    money %= cur;
+    return ret;
+  });
 
   return answer;
 }
