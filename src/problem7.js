8d266689bd37ce5ad70f00abf948de2a99e049b4
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 15:31:23 +0900

    [p7] 스코어보드와 친구의 친구 +10점

--- src/problem7.js
+++ src/problem7.js
@@ -11,12 +11,15 @@ function findFriend(user, friends) {
   return friend;
 }
 
-function users(friends) {
+function users(friends, user) {
   let userlist = new Set();
   
   friends.forEach(([left, right]) =>{
-    userlist.add(left);
-    userlist.add(right);
+    if(user !== left)
+      userlist.add(left);
+    
+    if(user !== right)
+      userlist.add(right);
   });
 
   return userlist;
@@ -30,11 +33,29 @@ function createScoreboard(userlist){
   return arr;
 }
 
+function friendOfFriend(friends, score, userFriend, user) {
+  friends.forEach(([left, right]) => {
+    if(userFriend.includes(left) && user !== right){
+      const idx = score.findIndex(i => i[1] === right);
+      score[idx][0] += 10;
+    }
+    if(userFriend.includes(right) && user !== left){
+      const idx = score.findIndex(i => i[1] === left); 
+      score[idx][0] += 10;
+    }
+  });
+
+  return score;
+} 
+
 function problem7(user, friends, visitors) {
   userFriend = findFriend(user, friends);
-  userlist = users(friends);
+  
+  userlist = users(friends, user);
   userlist = Array.from(userlist);
+  
   let score = createScoreboard(userlist);
+  score = friendOfFriend(friends, score, userFriend, user);
 
   return score;
 }
@@ -42,5 +63,5 @@ function problem7(user, friends, visitors) {
 //유저 목록 저장
 //스코어보드 저장
 //친구와 친구관계인 친구에 +10점
-//방문자
+//방문자 +1점
 module.exports = problem7;
