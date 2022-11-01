c5cb109d4182e60426216edd7ce9f836ffaee17d
yeongseokim <1903020@ewhain.net>
2022-11-01 22:49:16 +0900

    feat(problem6): 같은 글자가 연속적으로 포함 되는 닉네임을 작성한 지원자의 이메일 찾기

--- src/problem6.js
+++ src/problem6.js
@@ -9,10 +9,24 @@ function splitName(name) {
 
 function problem6(forms) {
   var answer;
+  let emailList = [];
+  let flag = false;
 
   for (let i = 0; i < forms.length - 1; i++) {
     let splitedName = splitName(forms[i][1]);
 
+    for (let j = i + 1; j < forms.length; j++) {
+      for (let k = 0; k < splitedName.length; k++) {
+        let target = splitedName[k];
+        if (forms[j][1].match(target)) {
+          emailList.push(forms[j][0]);
+          flag = true;
+          break;
+        }
+      }
+    }
+    if (flag) emailList.push(forms[i][0]);
+    flag = false;
   }
 
   return answer;
