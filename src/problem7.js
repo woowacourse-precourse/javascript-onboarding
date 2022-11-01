b481e06cf83667055d14f802c13e9b44b46f41be
durumi99 <kms990615@naver.com>
2022-11-01 15:46:50 +0900

    [problem 7] 추천 점수를 내림차순으로 정렬, 추천 점수가 같은 경우는 이름순으로 정렬

--- src/problem7.js
+++ src/problem7.js
@@ -44,6 +44,16 @@ function problem7(user, friends, visitors) {
     else
       recommendPoints.set(visitor, recommendPoints.get(visitor)+1)
   }
+  let sortPoints = new Map([...recommendPoints].sort(function(a, b){
+    if(a[1] == b[1]){
+      if(a[0] > b[0])
+        return 1;
+      if(a[0] < b[0])
+        return -1;
+      return 0;
+    }
+    return b[1] - a[1];
+    }));
   return answer;
 }
 
