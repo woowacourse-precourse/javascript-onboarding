cadf97dcfe9aa4f0bce32e6a0f6c6beeadd32b25
y00eunji <aine0227@naver.com>
2022-11-01 20:18:44 +0900

    feat: [problem3] count 3,6,9 in number

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,9 @@
+const getNumClapCount = (num) => {
+  const regex = /3|6|9/g;
+
+  return (num.toString().match(regex) || []).length;
+};
+
 function problem3(number) {
   var answer;
   return answer;
