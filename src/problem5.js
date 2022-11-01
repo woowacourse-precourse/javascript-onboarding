1c8f46da29f2d509ebdfcde61b62a94e0cd38952
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-01 11:14:43 +0900

    feat: [PROBLEM5] 몫을 answer 배열에 저장하여 return 한다

--- src/problem5.js
+++ src/problem5.js
@@ -3,6 +3,7 @@ function problem5(money) {
   let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
   for (let i = 0; i < unit.length; i++) {
+    answer[i] = parseInt(money / unit[i]);
     money = money % unit[i];
   }
 
