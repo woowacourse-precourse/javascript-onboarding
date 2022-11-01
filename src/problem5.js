b0d3e433cc217708064a2df3f9e3df6d5fa7dd42
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-01 11:09:22 +0900

    refactor: [PROBLEM5] unit과 answer 배열에 1원 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,6 @@
 function problem5(money) {
-  var answer = [0, 0, 0, 0, 0, 0, 0, 0];
-  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10];
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
   for (let i = 0; i < unit.length; i++) {
     money = money % unit[i];
