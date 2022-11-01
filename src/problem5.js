6d7a2d3b4bdb1589dfb29691750b6202f96a36a0
Deemou <4106243@naver.com>
2022-11-01 23:49:57 +0900

    refactor<P5>: countCurrency() 구조 변경,  currency 이름 변경
    currency -> currencyList
    countCurrency ()
    forEach -> reduce 변경

--- src/problem5.js
+++ src/problem5.js
@@ -3,14 +3,13 @@ function problem5(money) {
   return answer;
 }
 const countCurrency = (money) => {
-  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const currencyCount = [];
-  currency.forEach((item) => {
-    const count = Math.floor(money / item);
+  const currencyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  return currencyList.reduce((currencyCount, currency) => {
+    const count = Math.floor(money / currency);
     currencyCount.push(count);
-    money %= item;
-  });
-  return currencyCount;
+    money %= currency;
+    return currencyCount;
+  }, []);
 };
 
 module.exports = problem5;
