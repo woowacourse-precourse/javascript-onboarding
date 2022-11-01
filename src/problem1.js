ce7b1d7e5051932482aa57017eb2b58049b56f4f
jungsmins1 <jungsmins00@gmail.com>
2022-11-01 18:42:08 +0900

    refactor: 예외사항 기능의 리턴값 변경

--- src/problem1.js
+++ src/problem1.js
@@ -5,10 +5,10 @@ function problem1(pobi, crong) {
     const minPage = 1;
     const maxPage = 400;
   
-    if (page[0] < minPage || page[1] > maxPage) return false;
-    if (page[0] !== page[1] - 1) return false;
-    if (page.length !== 2) return false;
-    return true;
+    if (page[0] < minPage || page[1] > maxPage) return true;
+    if (page[0] !== page[1] - 1) return true;
+    if (page.length !== 2) return true;
+    return false;
   }
 
   //페이지 번호의 각 자리 숫자를 모두 더하는 기능
@@ -32,7 +32,7 @@ function problem1(pobi, crong) {
   }
 
   //4 과정에서 나온 숫자를 통해 결과 값을 구하는 기능
-  if (!exceptions(pobi) || !exceptions(crong)) return -1;
+  if (exceptions(pobi) || exceptions(crong)) return -1;
   if (maxNum(pobi) > maxNum(crong)) return 1;
   if (maxNum(pobi) < maxNum(crong)) return 2;
   if (maxNum(pobi) == maxNum(crong)) return 0;
