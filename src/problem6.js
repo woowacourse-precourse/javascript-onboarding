11cce9c84d0c5abf1fe88319535a719b9c618e7f
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:31:33 +0900

    feat: [problem6] 두 닉네임 사이 두 글자 이상 연속 중복 여부 확인

--- src/problem6.js
+++ src/problem6.js
@@ -4,4 +4,13 @@ function problem6(forms) {
   return answer;
 }
 
+const isDuplicationOfPart = (target, check) => {
+  for (let i = 1; i < target.length; i++) {
+    if (check.includes(target.slice(i - 1, i + 1))) {
+      return true
+    }
+  }
+  return false
+}
+
 module.exports = problem6;
