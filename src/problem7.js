df2aba7a7b6f9e38ca86364c5232dd82b15d041a
woodi97 <woodi.daily@gmail.com>
2022-11-01 22:45:40 +0900

    solve(): week-01 problem-07

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,65 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+const FRIENDFRIENDSCORE = 10;
+const MAXRECOMMENDATIONS = 5;
+
+let friendMap = new Map();
+let recommendScore = {};
+
+function isAwayFriend(userName, friendName){
+  if(friendMap[friendName] === undefined  && friendName !== userName) {
+    return true;
+  }
+  return false;
 }
+
+function problem7(user, friends, visitors) {
+
+  for(let i = 0; i < friends.length; i++) {
+    if(friendMap[friends[i][0]] === undefined) {
+      friendMap[friends[i][0]] = 1;
+    }
+    if(isAwayFriend(user, friends[i][1])) {
+      recommendScore[friends[i][1]] = FRIENDFRIENDSCORE;
+    }
+  }
+
+  for(let i = 0; i < visitors.length; i++) {
+    if(!friendMap[visitors[i]] && recommendScore[visitors[i]] === undefined) {
+      recommendScore[visitors[i]] = 0;
+    }
+    if(isAwayFriend(user, visitors[i])) {
+      recommendScore[visitors[i]]++;
+    }
+  }
+
+  let recommendArray = Object.entries(recommendScore);
+  // in some day, there would be minus score logic. So there is possibility that there is some negative score.
+  recommendArray = recommendArray.filter((value) => value[1] > 0);
+  recommendArray.sort((a, b) => {
+    if(a[1] === b[1]) {
+      return a[0] < b[0] ? -1 : 1;
+    }
+    return b[1] - a[1];
+  });
+
+
+  if(recommendScore.length > MAXRECOMMENDATIONS) {
+    recommendScore = recommendScore.slice(0, MAXRECOMMENDATIONS);
+  }
+
+  return recommendArray.map((value) => value[0]);
+}
+console.log(
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+))
 
 module.exports = problem7;
