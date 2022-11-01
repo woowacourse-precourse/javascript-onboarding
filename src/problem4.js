72d0f591d621ddbcc759143beec07bdff7af413c
HaroldBKim <sbookim2@gmail.com>
2022-11-01 18:27:30 +0900

    solved problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,27 @@
 function problem4(word) {
   var answer;
+  
+  const alphabet = " abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const reAlphabet =
+  " zyxwvutsrqponmlkjihgfedcbaZYXWVUTSRQPONMLKJIHGFEDCBA";
+  let alphabetArr = alphabet.split("");
+  let realphabetArr = reAlphabet.split("");
+  
+  const wordArr = word.split("");
+  let reWordArr = [];
+  
+  let indexArr = [];
+  for (let i = 0; i < wordArr.length; i++) {
+  alphabetList = alphabetArr.indexOf(wordArr[i]);
+  indexArr.push(alphabetList);
+  }
+  
+  for (let j in indexArr) {
+  reWordArr.push(realphabetArr[indexArr[j]]);
+  answer = reWordArr.join("");
+  }
+  
   return answer;
-}
-
-module.exports = problem4;
+  }
+  
+  module.exports = problem4;
