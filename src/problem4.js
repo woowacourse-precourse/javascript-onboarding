4fc63698e5df3599651947e4ee150d4b53d19561
emma <jhj191058@gmail.com>
2022-11-01 16:14:47 +0900

    problem4 기능구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
 function problem4(word) {
- 
+  var answer;
+
+  var str = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnofqrstuvwxyz';
+  var re = 'ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba';
+  var index = x => str.indexOf(x);
+  var translate = x => index(x) > -1 ? re[index(x)] : x;
+  if(word == null) {
+        word = " ";
+  }
+
+  answer = word.split('').map(translate).join('');
   
   return answer;
 }
