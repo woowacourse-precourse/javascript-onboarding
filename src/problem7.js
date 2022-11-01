a942424acd0b5554c4572c3f6798ed77a33e4e07
parkjikuk <paxkk@naver.com>
2022-11-01 22:32:18 +0900

    feat : 사용자의 친구의 친구를 구하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,11 +1,10 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const findFriends = findFriends(user, friends);
 }
 
 function findFriends(user, friends) {
-  const friendArr = [];
-  
+  let friendArr = [];
+
   friends.map((friend) => {
     if(friend.includes(user)){
       const deliteUserName = friend.filter((el) => el !== user);
@@ -13,6 +12,22 @@ function findFriends(user, friends) {
     }
   })
   return friendArr;
+}
+
+function findFriendsOfFriend(user, friends, findFriends) {
+  let friendsOfFriend = [];
+  
+  friends.map((friend) => {
+    findFriends.map((userFriend) => {
+      if(friend.includes(userFriend) && !friend.includes(user)) {
+        const friendsOfFriendArr = friend.filter((el) => el !== userFriend);
+        if (!friendsOfFriendArr.includes(friendsOfFriendArr[0])) {
+          friendsOfFriend.push(friendsOfFriendArr[0])
+        }
+      }
+    })
+  })
+  return friendsOfFriend;
 }
 
 module.exports = problem7;
