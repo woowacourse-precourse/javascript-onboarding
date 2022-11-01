53f16f4a6162c639a2f26765acebf806b65be8cc
wjdtjdgns <person3275@gmail.com>
2022-11-01 19:09:45 +0900

    각 자리 숫자를 모두 더하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+}
+
+// 각 자리 숫자를 모두 더해 그 결과를 반환하는 함수
+function digitPlus(digit) {
+  let result = digit%10;
+  digit = Math.floor(digit/10);
+  while(digit > 0) {
+    result += digit%10;
+    digit = Math.floor(digit/10);
+  }
+  return result;
 }
 
 module.exports = problem1;
