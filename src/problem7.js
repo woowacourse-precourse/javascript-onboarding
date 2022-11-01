4921c9968506ba11006cacac8ad2e7572bd0bc9a
yeongseokim <1903020@ewhain.net>
2022-11-01 22:55:49 +0900

    feat(problem7): user의 친구 찾아 userInfo에 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,15 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let userInfo = {
+    name: user,
+    friends: [],
+  };
+
+  friends.map((friend) => {
+    if (userInfo.name === friend[0]) userInfo.friends.push(friend[1]);
+    else if (userInfo.name === friend[1]) userInfo.friends.push(friend[0]);
+  });
+
   return answer;
 }
 
