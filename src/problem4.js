f694e92da5687c71e6fc07cec4cf9933facde715
cheonyeji <ssaltuck@gmail.com>
2022-11-01 19:57:19 +0900

    아스키코드로 변환하여 대소문자 구분하여 Z나 z에서 얼마나 떨어져있는지 계산

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+
+  for (var i = 0; i < word.length; i++) {
+    if (word[i] === " ") {
+      answer += word[i];
+    } else {
+      var ascii = word.charCodeAt(i);
+      // 소문자
+      if (97 <= ascii && ascii <= 122) {
+        answer += String.fromCharCode(122 - (word.charCodeAt(i) - 97));
+      }
+      // 대문자
+      else {
+        answer += String.fromCharCode(90 - (word.charCodeAt(i) - 65));
+      }
+    }
+  }
   return answer;
 }
 
