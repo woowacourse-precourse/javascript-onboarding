0e1d98c723911f4c9ad308dee6468d1ab3ae5551
jihyunlee <jihyunlee.me@gmail.com>
2022-11-01 23:38:36 +0900

    feat: problem5, 문제해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let cashArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  
+  for (let i = 0; i < cashArr.length; i++) {
+    let count = Math.trunc(money / cashArr[i]) 
+    answer[i] = count;
+    money = money - cashArr[i] * count;
+  }
   return answer;
 }
 
