bbbf44248dbbd2c96df77140f00fc689be57b1e8
imhynni <omelas329@gmail.com>
2022-11-01 13:53:14 +0900

    feat(problem5): 기능 구현 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
 function problem5(money) {
-  var answer;
+  let answer;
+
+  const UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const wallet = [];
+
+  for (let i = 0; i < UNIT.length; i++) {
+    wallet.push(parseInt(money / UNIT[i]));
+    money = money % UNIT[i];
+  }
+
+  answer = [...wallet];
+
   return answer;
 }
 
