9429ece3abd4095327e193df2140fced4685e65e
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:37:30 +0900

    [problem4] feat: 탐색한 아스키코드값으로 소문자,대문자를 구분해 변환

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,14 @@ function problem4(word) {
   answer = [];
   newWord.forEach(function (changeWord) {
     ascii = changeWord.charCodeAt(0);
+    if (ascii <= 90 && ascii >= 65) {
+      answer += String.fromCharCode(155 - ascii); //대문자일때 문자열 변환
+    } else if (ascii >= 97 && ascii <= 122) {
+      answer += String.fromCharCode(219 - ascii); //소문자일때 문자열 변환
+    } else {
+      answer += changeWord;
+    }
+  });
   return answer;
 }
 
