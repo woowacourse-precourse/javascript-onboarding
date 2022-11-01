57c6b9fa76e0a37581edb5d08da521bad32e3864
areumsheep <areumsheep@gmail.com>
2022-11-01 18:46:49 +0900

    refactor: 문제3 변수, 함수명 변경 및 예외 조건 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,15 +1,30 @@
-const includes369 = (number) => {
-  const splitNumber = `${number}`.split('');
-  return splitNumber.filter((splited) => {
-    if (splited.includes(3) || splited.includes(6) || splited.includes(9))
-      return splited;
-  }).length;
+const MESSAGES = Object.freeze({
+  ERROR_RANGE: '1 이상 10,000 이하의 자연수여야 합니다.',
+});
+const INCLUDES_NUMBERS = ['3', '6', '9'];
+
+/** 포함된 문자 개수 */
+const includesNumbersCount = (number) => {
+  const splittedArray = `${number}`.split('');
+  const includesArray = splittedArray.filter((splittedNumber) => {
+    if (INCLUDES_NUMBERS.includes(splittedNumber)) return splittedNumber;
+  });
+  return includesArray.length;
+};
+/** 파라미터 조건 확인 */
+const isValidNumber = (number = 0) => {
+  if (number < 1 || number > 10000) return true;
+  if (!Number.isInteger(number)) return true;
+
+  return false;
 };
 
 function problem3(number = 0) {
   let count = 0;
-  for (let i = 0; i <= number; i++) {
-    count += includes369(i);
+  if (isValidNumber(number)) return MESSAGES.ERROR_RANGE;
+
+  for (let i = 1; i <= number; i++) {
+    count += includesNumbersCount(i);
   }
   return count;
 }
