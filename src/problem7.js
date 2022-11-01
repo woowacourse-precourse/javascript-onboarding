2e8448c3fad9bf4c74b6730a2d879d3243061eaa
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 20:49:16 +0900

    feat[P7]: Friend Object 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+	function Friend(friendName) {
+		this.friendName = friendName;
+		this.score = 0;
+		this.visitCnt = 0;
+		this.knowingCnt = 0;
+		this.isFriend = false;
+		this.calScore = calScore;
+	}
+	function calScore() {
+		this.score = visitCnt + knowingCnt * 10;
+	}
+	let answer;
+
+	let friendsArray = [];
+
+	return answer;
 }
 
 module.exports = problem7;
