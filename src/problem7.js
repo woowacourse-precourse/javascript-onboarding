a52be95afff6c262f0bed625e2934d9aa5ba7a67
banma1234 <banma1234@gmail.com>
2022-11-01 22:09:30 +0900

    problem_7 : 친구찾기 함수

--- src/problem7.js
+++ src/problem7.js
@@ -10,4 +10,16 @@ function errorHandler(user, friends, visitors) {
   return false;
 }
 
+// 친구찾기 함수
+function findFriend(user, friends) {
+  let friendList = [];
+
+  // user가 있는 객체를 찾은 후 user의 반대편에 있는 친구의 이름을 저장한다.
+  friendList = friends.filter(([a, b]) => a == user | b == user).map(
+    ([a, b]) => (a == user ? b : a)
+  );
+
+  return friendList;
+}
+
 module.exports = problem7;
