b5737b4b3dba8bc05b920f4a168a4f73cedd74d6
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 16:56:27 +0900

    problem1) 기본 계산함수를 만들고, 조합하여 코드작성

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,46 @@
+function isWrongPage(lt, rt) {
+  if (lt % 2 !== 1 || rt % 2 !== 0) return true;
+  if (lt + 1 !== rt) return true;
+  return false;
+}
+
+function numberToArr(num) {
+  return [...num.toString()].map(Number);
+}
+
+function sum(a, b) {
+  return a + b;
+}
+
+function product(a, b) {
+  return a * b;
+}
+
+function calc(calcFn, defaultNum) {
+  return (arr) => arr.reduce(calcFn, defaultNum);
+}
+
+const sumArr = calc(sum, 0);
+const productArr = calc(product, 1);
+
+function getScore(num1, num2) {
+  if (num1 > num2) return 1;
+  if (num1 < num2) return 2;
+  if (num1 === num2) return 0;
+  return -1;
+}
+
 function problem1(pobi, crong) {
   var answer;
+  if (isWrongPage(...pobi) || isWrongPage(...crong)) return -1;
+
+  const pobiNumsArr = pobi.map(numberToArr);
+  const crongNumsArr = crong.map(numberToArr);
+
+  const pobiScore = Math.max(...pobiNumsArr.flatMap((numArr) => [sumArr(numArr), productArr(numArr)]));
+  const crongScore = Math.max(...crongNumsArr.flatMap((numArr) => [sumArr(numArr), productArr(numArr)]));
+
+  answer = getScore(pobiScore, crongScore);
   return answer;
 }
 
