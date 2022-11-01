6f046796923c532e585307a691bbb07659d9cf74
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:41:11 +0900

    feat(prob2): stack 자료구조를 이용한 연속된 문자 체크

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
+function sameCheck(a, b) {
+  if (a === b) return 1;
+  else return 0;
 }
 
+function problem2(cryptogram) {
+  var answer;
+  let stack = [];
+  let prevWord = "";
+  stack.push(cryptogram[0]);
+  for (let i = 1; i < cryptogram.length; i++) {
+    //스택 자료구조 push,pop으로
+    const char = cryptogram[i];
+    if (sameCheck(stack[stack.length - 1], char)) {
+      stack.pop();
+    } else {
+      stack.push(char);
+    }
+  }
+  answer = stack.join("");
+  return answer;
+}
 module.exports = problem2;
