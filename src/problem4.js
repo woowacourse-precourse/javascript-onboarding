f73fc61f5913d91bee9d07aa197b6aee9aa3b123
woowoni <ewoowon.2@gmail.com>
2022-11-01 23:27:02 +0900

    [문제4] 알파벳 사전 문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,28 @@
 function problem4(word) {
-  var answer;
+  let answer=[];
+  let lowerAlphabet=[];
+  let upperAlphabet=[];
+
+  for(let i = 65; i<=90; i++){
+    upperAlphabet.push(String.fromCharCode(i));
+  }
+
+  for(let i = 97; i<=122; i++){
+    lowerAlphabet.push(String.fromCharCode(i));
+  }
+
+  for(let i = 0; i<word.length; i++){
+    if(word[i] === ' '){
+      answer.push(' ');
+    }
+    if(lowerAlphabet.includes(word[i])){
+      answer.push(lowerAlphabet[lowerAlphabet.length - lowerAlphabet.indexOf(word[i]) - 1]);
+    }
+    if(upperAlphabet.includes(word[i])){
+      answer.push(upperAlphabet[upperAlphabet.length - upperAlphabet.indexOf(word[i]) - 1]);
+    }
+  }
+  answer = answer.join('');
   return answer;
 }
 
