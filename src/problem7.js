4fe75a9e2d190e5ada18bbc980b6f773a431aba6
imhynni <omelas329@gmail.com>
2022-11-01 18:11:12 +0900

    feat(problem7): user 친구들의 친구들 추천 점수 10점 더하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,54 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+  const friendsDict = {};
+  const score = {};
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] in friendsDict) {
+      friendsDict[friends[i][0]].push(friends[i][1]);
+    } else {
+      friendsDict[friends[i][0]] = [friends[i][1]];
+    }
+    if (friends[i][1] in friendsDict) {
+      friendsDict[friends[i][1]].push(friends[i][0]);
+    } else {
+      friendsDict[friends[i][1]] = [friends[i][0]];
+    }
+  }
+
+  let friendsOfFriends = [];
+
+  if (user in friendsDict) {
+    const userFriends = friendsDict[user];
+    for (let i = 0; i < userFriends.length; i++) {
+      friendsOfFriends = [...friendsOfFriends, ...friendsDict[userFriends[i]]];
+    }
+    friendsOfFriends = new Set(friendsOfFriends);
+    friendsOfFriends.delete(user);
+  }
+
+  for (const friend of friendsOfFriends) {
+    if (friend in score) {
+      score[friend] += 10;
+    } else {
+      score[friend] = 10;
+    }
+  }
+
   return answer;
 }
+
+problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+);
 
 module.exports = problem7;
