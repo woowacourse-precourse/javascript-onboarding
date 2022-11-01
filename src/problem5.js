6e02a11b48554fa5e125b3596c4b401de1e803aa
Blackraven93 <reblackraven@gmail.com>
2022-11-01 22:09:29 +0900

    Problem5[Refactor] 직관적인 rest를 사용

--- src/problem5.js
+++ src/problem5.js
@@ -3,7 +3,7 @@ function problem5(money) {
   let current = money;
 
   [50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1].forEach((unit) => {
-    answer.push(Math.floor(current / unit));
+    answer = [...answer, Math.floor(current / unit)];
     current = current % unit;
   });
 
