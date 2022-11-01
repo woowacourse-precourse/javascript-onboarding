52e1b0b7df6b1d793787716c6160dc9d37758ad8
cofla <cofla159@naver.com>
2022-11-01 20:33:47 +0900

    [p7] 문제풀이 sudo

--- src/problem7.js
+++ src/problem7.js
@@ -4,3 +4,14 @@ function problem7(user, friends, visitors) {
 }
 
 module.exports = problem7;
+
+/*
+1. friends에서 내 닉네임 포함된 배열 찾아서 내 친구 찾기
+2. 1에서 찾은 이름 포함된 배열 다시 friends에서 찾아서 내 친구의 친구 찾기
+3. 찾은 횟수*10만큼 점수 ++
+4. visitors 돌면서 방문할때마다 점수 ++
+5. 최종 점수 > 0인 사람 result에 추가
+6. 점수순 정렬
+7. 점수 같으면 이름순 정렬
+8. 5명까지만 뽑아서 return
+*/
