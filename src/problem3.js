5bd6982a35c186062c9ba5226ec778c9e80554eb
yb8350 <yb8350@naver.com>
2022-11-01 20:08:21 +0900

    Refactor: problem3-손뼉 횟수를 계산하는 코드를 함수로 변경

--- src/problem3.js
+++ src/problem3.js
@@ -1,10 +1,15 @@
 function problem3(number) {
   var answer = 0;
   for (let i = 1; i <= number; i++) {
-    let arr = String(i).match(/[369]/g);
-    if (arr) answer += arr.length;
+    answer += clapCount(i);
   }
   return answer;
+}
+
+function clapCount(num) {
+  let clapNumber = String(num).match(/[369]/g);
+  if (clapNumber) return clapNumber.length;
+  return 0;
 }
 
 module.exports = problem3;
