375ba16b957eb7a2abd18801f3dc22a8ea2b3b25
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-01 20:43:40 +0900

    refactor: [5번 문제] let => const 변경, 나머지 구하기 단축표현

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,11 @@
 const MONEY_TYPE_LIST = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
 function problem5(money) {
-  let answer = Array.from({ length: MONEY_TYPE_LIST.length }, () => 0);
+  const answer = Array.from({ length: MONEY_TYPE_LIST.length }, () => 0);
   let remainMoney = money;
   MONEY_TYPE_LIST.map((referenceMoney, i) => {
     answer[i] = Math.floor(remainMoney / referenceMoney);
-    remainMoney = remainMoney % referenceMoney;
+    remainMoney %= referenceMoney;
   });
   return answer;
 }
