ee0e478d222e78c0bbdf12ba0035167d6adc8f63
allzeroyou <ekdudd119@naver.com>
2022-11-01 17:41:44 +0900

    [feat] 문자열 알파벳 거꾸로 출력하기 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+  let splitWord = word.split("");
+  splitWord.forEach((element, index) => {
+    let ascii = word.charCodeAt();
+    if (ascii >= 65 && ascii <= 90) {
+      answer += String.fromCharCode(155 - ascii); // 대문자 65~90(155개)
+    } else if (ascii >= 97 && ascii <= 122) {
+      answer += String.fromCharCode(219 - ascii); // 소문자 97~122(219개)
+    } else {
+      answer += element;
+    }
+  });
   return answer;
 }
 
