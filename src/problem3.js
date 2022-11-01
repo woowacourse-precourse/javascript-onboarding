83f83d2a759e01a23c41636a48f480c5830d5524
yeongseokim <1903020@ewhain.net>
2022-11-01 22:29:48 +0900

    feat(problem3): 1부터 input number까지 3 or 6 or 9의 개수 세기

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,11 @@
 function problem3(number) {
   var answer = 0;
   let string;
+  const regexp = /[369]/;
+
   for (let i = 1; i <= number; i++) {
     string = i.toString();
+    answer += string.split(regexp).length - 1;
   }
   return answer;
 }
