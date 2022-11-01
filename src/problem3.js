1ed8c5e66d66431e87230ab41a0c2e09f8ddc6c0
윤동주 <yoondj98@naver.com>
2022-11-01 21:20:42 +0900

    우아한 테크코스 프리코슥 1주차 3번 코드 주석 기입

--- src/problem3.js
+++ src/problem3.js
@@ -2,7 +2,7 @@ function problem3(number) {
   var answer = 0;
   for (var i = 1; i <= number; i++) {
     var str_arr = i.toString().split("");
-    str_arr.map((k) => {
+    str_arr.map((k) => { /* 입력받는 값까지 한바퀴 돌고, 각 숫자의 자릿수들을 문자열로 돌며 각 자리에 3,6,9가 있는지 확인 */
       if (k == "3" || k == "6" || k == "9") answer++;
     });
   }
