f7fc4b5bd07ac2e04619059be5d160ed4b1be13c
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-01 11:26:29 +0900

    feat : 주어진 금액을 배열의 0번째부터 나누어 몫은 새로운 배열에 나머지는 금액에 할당

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,9 @@
 function problem5(money) {
   let price = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
+  answer.push(parseInt(money / price[i]));
+  money %= price[i];
+  return answer;
 }
 
 module.exports = problem5;
