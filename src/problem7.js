06eb03a405f68cfe0e339f4c16005ce076faa695
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 20:23:32 +0900

    problem7) getRelations 함수로 friends 배열 기반 관계객체 반환

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,28 @@
+function hasKey(obj, target) {
+  return target in obj;
+}
+
+function getRelations(friends) {
+  const relations = {};
+
+  friends.forEach(([idA, idB]) => {
+    if (!hasKey(relations, idA)) {
+      relations[idA] = new Set();
+    }
+    if (!hasKey(relations, idB)) {
+      relations[idB] = new Set();
+    }
+    relations[idA].add(idB);
+    relations[idB].add(idA);
+  });
+
+  return relations;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  const relations = getRelations(friends);
+
   return answer;
 }
 
