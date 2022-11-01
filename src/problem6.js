d2f6ac284dc8ae0f09ebb143c0da3cca9a8e443f
yunjjeongjo <two6885@gmail.com>
2022-11-01 17:29:17 +0900

    feat: problem6 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,24 @@
 function problem6(forms) {
-  var answer;
+  const answer = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      if (forms[i][1].length > 1) {
+        const nicknamePiece = forms[i][1].substr(j, 2);
+        for (let k = 0; k < forms.length; k++) {
+          if (k !== i) {
+            if (
+              forms[k][1].includes(nicknamePiece) &&
+              !answer.includes(forms[k][0])
+            ) {
+              answer.push(forms[k][0]);
+            }
+          }
+        }
+      }
+    }
+  }
+  answer.sort();
   return answer;
 }
 
