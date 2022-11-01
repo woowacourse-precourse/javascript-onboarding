1da3ad410660a8e59292d9b6367db5984826678e
sojinjang <jsojin6119@gmail.com>
2022-11-01 23:26:17 +0900

    :sparkles: feat(prob7): 기존 친구 & 친구의 친구 찾기, 점수 매기고 상위 5명만 리턴 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,55 @@
+function getFriendsWithUser(user, friends) {
+  let friendsWithUser = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) friendsWithUser.push(...friends[i]);
+  }
+  return friendsWithUser;
+}
+
+function makeNewFriends(friends, friendsWithUser) {
+  let newFriends = [];
+  friends.forEach((f) => {
+    if (f.filter((x) => !friendsWithUser.includes(x)).length === 1) {
+      newFriends.push(f.filter((x) => !friendsWithUser.includes(x)));
+    }
+  });
+  return newFriends;
+}
+
+function doCount(arr, count, score) {
+  arr.forEach((elem) => {
+    count[elem] = (count[elem] || 0) + parseInt(score);
+  });
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  let visitArr = [];
+  let count = [];
+
+  friendsWithUser = [...new Set(getFriendsWithUser(user, friends))];
+  newFriends = makeNewFriends(friends, friendsWithUser);
+
+  if (visitors.length > 0) {
+    visitArr.push(...visitors.filter((x) => !friendsWithUser.includes(x)));
+  }
+
+  newFriends.sort();
+  visitArr.sort();
+
+  doCount(newFriends, count, 10);
+  doCount(visitArr, count, 1);
+
+  let sortedCount = Object.fromEntries(
+    Object.entries(count).sort(([, a], [, b]) => b - a)
+  );
+
+  const answer = [];
+  for (let i = 0; i < 5; i++) {
+    if (!Object.keys(sortedCount)[i] !== true) {
+      answer.push(Object.keys(sortedCount)[i]);
+    }
+  }
+
   return answer;
 }
 
