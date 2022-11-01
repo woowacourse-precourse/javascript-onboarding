9672fa7a7bd2a166882d1b53901af1410633b662
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 21:00:23 +0900

    feat : getUserFriendsList함수를 통해 user의 친구 목록을 구한다. getPointMap함수를 통해 친구목록 중 같이 아는 친구들에게 점수를 부여한다

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,50 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    var answer = [];
+
+    let userFriendsList = getUserFriendsList(user, friends);
+    let pointMap = getPointMap(friends, userFriendsList, visitors);
+
+    return answer;
+}
+
+function getUserFriendsList(user, friends) {
+    let arr = [];
+    friends.map((element) => {
+        if (element.includes(user)) {
+            arr.push(element[0]);
+        }
+    });
+    return arr;
+}
+
+function getPointMap(friends, userFriendsList, visitors) {
+    let map = new Map();
+    friends.map((element) => {
+        let userFriend = element[0];
+        let recommendFriend = element[1];
+
+        if (userFriendsList.includes(userFriend) && recommendFriend !== user) {
+            if (map.get(recommendFriend) === undefined) {
+                map.set(recommendFriend, 10);
+            } else {
+                let point = map.get(recommendFriend);
+                map.set(recommendFriend, point + 10);
+            }
+        }
+    });
+
+    visitors.forEach((element) => {
+        if (!userFriendsList.includes(element)) {
+            if (map.has(element)) {
+                let point = map.get(element);
+                map.set(element, point + 1);
+            } else {
+                map.set(element, 1);
+            }
+        }
+    });
+
+    return map;
 }
 
 module.exports = problem7;
