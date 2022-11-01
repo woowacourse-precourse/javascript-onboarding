a08d02a0d43a4670b50cc98f1da4d14b688845cd
Bori-github <qhflrnfl4324@gmail.com>
2022-11-01 11:06:21 +0900

    ✨ problem5 > money에 따라 지폐, 동전이 반환되는 갯수를 배열로 나타내는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const divideArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let resultMoney = money;
+  for (let i = 0; i < answer.length; i++) {
+    const quotient = Math.floor(resultMoney / divideArr[i]);
+    answer[i] = quotient;
+    resultMoney -= quotient * divideArr[i];
+  }
+
   return answer;
 }
 
