a8cf8df8c2ab0f2d022458038d2f8848928b92cb
khkh0109 <ist200120@gmail.com>
2022-11-01 16:22:46 +0900

    feat: problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,11 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  for(let i = 0; true; i++) {
+    const regExp = /aa+|bb+|cc+|dd+|ee+|ff+|gg+|hh+|ii+|jj+|ll+|mm+|nn+|oo+|pp+|qq+|rr+|ss+|tt+|uu+|vv+|ww+|xx+|yy+|zz+/g;
+    const result = cryptogram.replace(regExp, '');
+    cryptogram = result;
+    if(regExp.test(cryptogram) === false) {
+      return cryptogram;
+    }
+  }
 }
-
 module.exports = problem2;
