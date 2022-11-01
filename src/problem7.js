61076bcd39370a360209de3ef75866733c36ea92
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-01 21:09:05 +0900

    FEAT: 7번- 방문횟수따라 1점 부여 메서드 구현

--- src/problem7.js
+++ src/problem7.js
@@ -8,7 +8,7 @@ function people(friends, visitors) {
   return setPeople;
 }
 
-// user와 친구인 사람을 찾는 함수
+// user와 친구인 사람을 찾는 함수 b
 function findFriend(user, friends) {
   let userFriend = [];
   friends.forEach((element) => {
@@ -21,6 +21,44 @@ function findFriend(user, friends) {
     }
   });
   return userFriend;
+}
+
+// user와 user의 friend를 제외한 사람 배열 b
+function realPeople(user) {
+  const friendList = [user, ...findFriend()];
+  const peopleList = [...setPeople];
+
+  let arrPeople = [];
+
+  friendList.forEach((value) => {
+    peopleList.forEach((element) => {
+      if (element !== value) {
+        arrPeople.push(element);
+      }
+    });
+  });
+  return arrPeople;
+}
+
+// 사람에 따른 점수배열
+function score() {
+  const arrScore = new Array(farPeople().length).fill(0);
+  return arrScore;
+}
+
+// visitors: 방문횟수에 따라 1점 부여하는 함수
+function grantScore(visitors, friends) {
+  const arrScore = score();
+  const arrPeople = realPeople();
+
+  arrPeople.forEach((value, index) => {
+    visitors.forEach((element => {
+      if(element == value) {
+        arrScore[index] += 1;
+      }
+    }))
+  })
+  
 }
 
 function problem7(user, friends, visitors) {
