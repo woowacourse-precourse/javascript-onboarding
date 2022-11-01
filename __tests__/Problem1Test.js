4d7439412f8485b3e93a291333c7d34d8aa02228
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 18:56:30 +0900

    test: [PROBLEM1] 자릿수 배열을 받아 2가지 계산 결과를 반환하는 함수 테스트

--- __tests__/Problem1Test.js
+++ __tests__/Problem1Test.js
@@ -1,10 +1,20 @@
 const problem1Util = require('../src/problem1Util');
 
 describe('Problem1 Util test', () => {
-  describe('getDigitFromNum', () => {
-    test('correct input', () => {
-      const digitList = problem1Util.getDigitFromNum(1234);
-      expect(digitList).toStrictEqual([1, 2, 3, 4]);
+  test('getDigitFromNum', () => {
+    const digitList = problem1Util.getDigitFromNum(1234);
+    expect(digitList).toStrictEqual([1, 2, 3, 4]);
+  });
+
+  describe('calcPageValues', () => {
+    test('not include 0', () => {
+      const digitList = problem1Util.calcPageValues([1, 2, 3]);
+      expect(digitList).toStrictEqual([6, 6]);
+    });
+
+    test('include 0', () => {
+      const digitList = problem1Util.calcPageValues([1, 0, 3]);
+      expect(digitList).toStrictEqual([4, 0]);
     });
   });
 });
