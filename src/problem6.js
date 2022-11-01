2fb9ec57e8a3a6326f576057d1d7ec5cb1e1ffc0
banma1234 <banma1234@gmail.com>
2022-11-01 20:12:07 +0900

    해당하는 계정의 이메일 걸러주는 함수

--- src/problem6.js
+++ src/problem6.js
@@ -14,15 +14,34 @@ function errorHandler(forms) {
 function spliceName(forms) {
   // 이름을 담을 배열
   let nameArr = [];
+  let overlapArr = [];
+  let result = [];
+  // 한글을 검사하기 위한 정규표현식
+  check = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
 
   // 2글자씩 이름을 쪼개 새 배열에 저장
   forms.map(([_, name]) => {
     for (i=0; i<name.length - 1; i++) {
+      // 한글 검사 후 한글이 아니면 공백처리
+      if (!check.test(name))  name = "";
+
       nameArr.push(name[i] + name[i+1]);
     }
   });
+  console.log(nameArr);
 
-  searchOverlap(nameArr);
+  overlapArr = searchOverlap(nameArr);
+  // 이름 배열중에 겹치는 문자가 있을 경우
+  for (i=0; i<overlapArr.length; i++) {
+    forms.map(([email, nickname]) => {
+      if (nickname.includes(overlapArr[i])) {
+        // 도메인이 @email.com인 경우만 result에 push
+        if (email.includes("@email.com")) result.push(email);
+      }
+    });
+  }
+
+  return result;
 }
 
 // 중복문자 찾는 함수
@@ -31,8 +50,10 @@ function searchOverlap(nameArr) {
 
   // 중복문자 새 배열에 저장
   nameArr.filter((item, index) => {
-    // 이름 배열에서 중복되는 문자를 새 배열에 저장
-    if (nameArr.indexOf(item) !== index)  overlapArr.push(item);
+    // 중복문자가 존재하며 이미 추가된 단어가 아닐 경우
+    if ((nameArr.indexOf(item) !== index) && !overlapArr.includes(item)){
+      overlapArr.push(item);
+    }
   });
 
   return overlapArr;
