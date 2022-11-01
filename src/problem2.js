1a786246dc60add2bea2d6817c5f6fdd42975fd6
oungoo <oungoo37@gmail.com>
2022-11-02 00:24:10 +0900

    [문제2] 최초 commit

--- src/problem2.js
+++ src/problem2.js
@@ -1,11 +1,27 @@
 function problem2(cryptogram) {
   var answer;
 
+  do{
+    if(cryptogram == ""){
+      return answer;
+    }
+    
+    answer = cryptogram[0];
+    var beforeLength = cryptogram.length;
+    for(var i = 1 ; i < cryptogram.length ; i++){
+      if(answer[answer.length-1] != cryptogram[i]){
+        answer += cryptogram[i];
+      }else{
+        answer = answer.substr(0, answer.length-1);
+      }
+    }
 
-
-
-
+    cryptogram = answer;
+    var afterLength = cryptogram.length;
+  }while(beforeLength != afterLength)
 
   return answer;
 }
 module.exports = problem2;
+
+//problem2("browoanoommnaon");
