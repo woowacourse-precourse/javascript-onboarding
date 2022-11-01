6b3d584310ee5d09af1a1b9e57178a05e7186e5e
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:43:32 +0900

    feat(prob3) : 3번 문제 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,18 @@
+function check369(num) {
+  let clap = 0;
+  while (num) {
+    const temp = num % 10;
+    if (temp === 3 || temp === 6 || temp === 9) clap += 1;
+    num = parseInt(num / 10);
+  }
+  return clap;
+}
+
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    answer += check369(i);
+  }
   return answer;
 }
 
