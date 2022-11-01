ff4b8ac94788a126610d2a20d165401406921046
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-01 16:43:25 +0900

    feat: [problem4] dictionary 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,36 @@
 function problem4(word) {
   var answer;
+  const arr = word.trim().split("");
+  let covertedArr = "";
+  const dic = {
+    A: "Z",
+    B: "Y",
+    C: "X",
+    D: "W",
+    E: "V",
+    F: "U",
+    G: "T",
+    H: "S",
+    I: "R",
+    J: "Q",
+    K: "P",
+    L: "O",
+    M: "N",
+    O: "L",
+    P: "K",
+    Q: "J",
+    R: "I",
+    S: "H",
+    T: "G",
+    U: "F",
+    V: "E",
+    W: "D",
+    X: "C",
+    Y: "B",
+    Z: "A",
+    " ": " ",
+  };
+ 
   return answer;
 }
-
 module.exports = problem4;
