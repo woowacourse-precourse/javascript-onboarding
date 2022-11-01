ff629b8fc5eb02feebb3114bc3103ce6e3f5d949
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-01 18:36:53 +0900

    feat: Add function of friends list(problem7)

--- src/problem7.js
+++ src/problem7.js
@@ -3,6 +3,45 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
-console.log(problem7("mrko" , [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ],["bedi", "bedi", "donut", "bedi", "shakevan"]))
+function friendsList(friends) {
+  let friendsList = new Map();
+  friends.map(([friendOne, friendTwo], idx) => {
+    friendsList.set(
+      friendOne,
+      (friendsList.get(friendOne) || []).concat(friendTwo)
+    );
+    friendsList.set(
+      friendTwo,
+      (friendsList.get(friendTwo) || []).concat(friendOne)
+    );
+  });
+
+  console.log(friendsList);
+}
+console.log(
+  friendsList([
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ])
+);
+
+// console.log(
+//   problem7(
+//     "mrko",
+//     [
+//       ["donut", "andole"],
+//       ["donut", "jun"],
+//       ["donut", "mrko"],
+//       ["shakevan", "andole"],
+//       ["shakevan", "jun"],
+//       ["shakevan", "mrko"],
+//     ],
+//     ["bedi", "bedi", "donut", "bedi", "shakevan"]
+//   )
+// );
 
 module.exports = problem7;
