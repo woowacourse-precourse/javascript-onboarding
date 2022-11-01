809a4369cb6a5eaaa4fec453442a5131f6e2bd40
dhkham <khamdong@gmail.com>
2022-11-01 15:04:05 +0900

    feat: (prob_6) add function that takes in a array of nicknames and email addresses and returns email addresses belonging to the ones with similar nicknames

--- src/problem6.js
+++ src/problem6.js
@@ -4,3 +4,16 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
+
+function getEmailList(forms, answer, i, j) {
+  for (let k = 0; k < forms[i][1].length - 1; k++) {
+    const iTestPart = forms[i][1][k] + forms[i][1][k + 1];
+    for (let l = 0; l < forms[j][1].length - 1; l++) {
+      const jTestPart = forms[j][1][l] + forms[j][1][l + 1];
+      if (iTestPart === jTestPart) {
+        answer.push(forms[i][0], forms[j][0]);
+      }
+    }
+  }
+  return answer;
+}
