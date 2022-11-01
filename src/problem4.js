79311d27376f24323b83d625a16c722710d76198
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:22:55 +0900

    feat:PROBLEM4:기능 목록 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
+/*
+ #기능 요구 사항
+엄마 말씀 word가 매개변수로 주어질 때, 
+아래 청개구리 사전을 참고해 반대로 변환하여 return 하도록 solution 메서드를 완성하라.
+EX)A->Z, B->Y, C->X ..... Z->A
+
+1. a-z, A-Z 까지의 문자를 짝에 맞게 아스키 코드를 통해서 변환한다.
+
+*/
+
 function problem4(word) {
-  var answer;
+  let answer;
   return answer;
 }
 
