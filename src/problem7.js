d12289d93581afcb830146302cdac840145e6cf7
윤동주 <yoondj98@naver.com>
2022-11-01 20:48:14 +0900

    우아한 테크코스 프리코슥 1주차 7번 기능구현 완료

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,40 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let realFriend = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user))
+      friends[i][0] == user
+        ? realFriend.push(friends[i][1])
+        : realFriend.push(friends[i][0]);
+  } //user 친구가 저장된 배열 realFriend 생성.
+
+  let friendScore = new Map();
+  for (let i = 0; i < friends.length; i++) {
+    if (!friends[i].includes(user)) {
+      if (
+        realFriend.includes(friends[i][0]) &&
+        !realFriend.includes(friends[i][1])
+      )
+        friendScore.get(friends[i][1])
+          ? friendScore.set(friends[i][1], friendScore.get(friends[i][1]) + 10)
+          : friendScore.set(friends[i][1], 10);
+      if (
+        !realFriend.includes(friends[i][0]) &&
+        realFriend.includes(friends[i][1])
+      )
+        friendScore.get(friends[i][0])
+          ? friendScore.set(friends[i][0], friendScore.get(friends[i][0]) + 10)
+          : friendScore.set(friends[i][0], 10);
+    }
+  }
+  for (let i = 0; i < visitors.length; i++) {
+    if (!realFriend.includes(visitors[i])) {
+      if (friendScore.get(visitors[i]))
+        friendScore.set(visitors[i], friendScore.get(visitors[i]) + 1);
+    else friendScore.set(visitors[i], 1);
+    }
+  }
+  let new_FriendScore = new Map([...friendScore].sort((a, b) => b[1] - a[1]));
+  var answer = Array.from(new_FriendScore.keys()).slice(0, 5);
   return answer;
 }
 
