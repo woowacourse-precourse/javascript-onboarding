de8d98e00fde37a4deb86648abebd3405f4490ac
Minhye Kang <alsgp9898@naver.com>
2022-11-01 13:35:38 +0900

    feat(PROBLEM5): 결과 값을 담을 빈 배열과 반복문 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,4 +1,8 @@
-function problem5(money) {}
+function problem5(money) {
+  const result = [];
+
+  for (let index = 0; index < BILL_ARRAY.length; index++) {}
+}
 
 const BILL_ARRAY = Object.freeze([
   50000, 10000, 5000, 1000, 500, 100, 50, 10, 1,
