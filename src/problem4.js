8e271c44b228d67c934ebef03faa1c01901b9e95
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 18:25:36 +0900

    problem4 : 알파벳으로 이루어진 단어 반대로 변경

--- src/problem4.js
+++ src/problem4.js
@@ -1,3 +1,19 @@
+function transformAlpha(english) {
+  let retAlpha = english;
+  const alphaAscii = english.charCodeAt(0);
+  const aAscii = "a".charCodeAt(0);
+  const zAscii = "z".charCodeAt(0);
+  const AAscii = "A".charCodeAt(0);
+  const ZAscii = "Z".charCodeAt(0);
+  if (aAscii <= alphaAscii && alphaAscii <= zAscii) {
+    const retAscii = aAscii + zAscii - alphaAscii;
+    retAlpha = String.fromCharCode(retAscii);
+  } else if (AAscii <= alphaAscii && alphaAscii <= ZAscii) {
+    const retAscii = AAscii + ZAscii - alphaAscii;
+    retAlpha = String.fromCharCode(retAscii);
+  }
+  return retAlpha;
+}
 function problem4(word) {
   var answer;
   return answer;
