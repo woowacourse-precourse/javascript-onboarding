44f5513cd6b68348819209c4ee5eaa9385a3f608
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 15:18:40 +0900

    feat[P4]: 대문자/소문자 구분하여 reverse하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -2,13 +2,16 @@ function problem4(word) {
 	const CODE = {
 		A_CODE: "A".charCodeAt(0),
 		Z_CODE: "Z".charCodeAt(0),
+		M_CODE: "M".charCodeAt(0),
 		a_CODE: "a".charCodeAt(0),
 		z_CODE: "z".charCodeAt(0),
+		m_CODE: "m".charCodeAt(0),
 	};
 	let answer = [];
 
 	Array.from(word).map((char) => {
-		if (isAlphabet(char)) return answer.push(reverseChar(char));
+		if (isAlphabet(char))
+			return answer.push(String.fromCharCode(reverseChar(char)));
 		else return answer.push(char);
 	});
 
@@ -23,7 +26,25 @@ function problem4(word) {
 	}
 
 	function reverseChar(char) {
-		return char;
+		let targetCode = char.charCodeAt(0);
+		if (targetCode <= CODE.Z_CODE) {
+			//대문자
+			if (targetCode <= CODE.M_CODE) {
+				//M이하
+				return targetCode + ((CODE.A_CODE - targetCode) * 2 + 25);
+			} else {
+				return targetCode + ((CODE.Z_CODE - targetCode) * 2 - 25);
+			}
+		}
+		if (targetCode >= CODE.a_CODE) {
+			//소문자
+			if (targetCode <= CODE.m_CODE) {
+				//m이하
+				return targetCode + ((CODE.a_CODE - targetCode) * 2 + 25);
+			} else {
+				return targetCode + ((CODE.z_CODE - targetCode) * 2 - 25);
+			}
+		}
 	}
 
 	return answer.join("");
