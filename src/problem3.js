5d5852ae18277a7d2ad257db2cd89d8e89213b3d
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:17:47 +0900

    feat:PROBLEM3:기능 목록 작성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
+/*
+ #기능 요구 사항
+숫자 number가 매개변수로 주어질 때, 1부터 number까지 손뼉을 몇 번 쳐야 하는지 
+횟수를 return 하도록 solution 메서드를 완성하라.
+
+1. 1부터 number까지의 3,6,9 숫자를 계산한다.
+
+*/
+
 function problem3(number) {
-  var answer;
+  let answer;
   return answer;
 }
 
