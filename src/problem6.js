043f4a9a43b1fb602f4228f597d047c7b87c2564
kwh97 <zzzang0512@naver.com>
2022-11-01 16:19:11 +0900

    feat(problem6): 필요 변수 선언 및 입력값 중에서 닉네임만 따로 추출하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  let nickNameList = [];
+  let overlapList = []; 
+  let totalList = []; 
+
+  for(let i=0; i<forms.length; i++) {
+    nickNameList.push(forms[i][1]);
+  }
 }
 
 module.exports = problem6;
