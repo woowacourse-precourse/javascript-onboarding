fb280283db09230944f2d8651d35d0fa03d301d1
jinlee <vito9270@naver.com>
2022-11-01 15:46:04 +0900

    [problem5] class 제거

--- src/problem5.js
+++ src/problem5.js
@@ -1,23 +1,13 @@
 function problem5(money) {
-  class moneyChanger {
-    constructor(money) {
-      this.money = money;
-    }
-    makeChange() {
-      const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-      const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-      let balance = this.money;
-      for (let i = 0; i < moneys.length; i++) {
-        while (balance >= moneys[i]) {
-          answer[i] = parseInt(balance / moneys[i]);
-          balance = parseInt(balance % moneys[i]);
-        }
+    const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+    const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+    for (let i = 0; i < moneys.length; i++) {
+      while (money >= moneys[i]) {
+        answer[i] = parseInt(money / moneys[i]);
+        money = parseInt(money % moneys[i]);
       }
-      return answer;
     }
-  }
-
-  return new moneyChanger(money).makeChange();
+    return answer;
 }
 
 module.exports = problem5;
