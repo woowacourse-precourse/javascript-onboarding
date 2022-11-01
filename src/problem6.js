996983f1f1b1ad4bb0a7b2832bcaf09dfa0f2dff
Sangmin Lee <gink.ss00@gmail.com>
2022-11-01 21:59:46 +0900

    [problem6] feat: findSimilarNickname 함수 구현
    다른 크루와 두 글자 이상의 연속된 문자가 중복된 닉네임을 가진 크루의 이메일을 출력해준다.

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,35 @@ function removeDuplication(emailArr) {
   return [...emailSet];
 }
 
+function findSimilarNickname(forms) {
+  if (forms.length === 1) {
+    return [];
+  }
+  let check = false;
+  let emailArr = [];
+
+  for (let base = 0; base < forms.length - 1; base++) {
+    for (let other = base + 1; other < forms.length; other++) {
+      for (let index = 0; index < forms[base][1].length - 1; index++) {
+        let twoChar = forms[base][1].substr(index, 2);
+        if (forms[other][1].includes(twoChar)) {
+          emailArr.push(forms[other][0]);
+          check = true;
+          break;
+        }
+      }
+    }
+    if (check === true) {
+      emailArr.push(forms[base][0]);
+      check = false;
+    }
+  }
+
+  emailArr = removeDuplication(emailArr);
+
+  return emailArr.sort();
+}
+
 function problem6(forms) {
   var answer;
   return answer;
