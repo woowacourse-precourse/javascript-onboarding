4b5937e5ef40c9dc796c2fbda99c92b9389e5221
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-01 11:27:01 +0900

    feat : 배열의 크기인 7만큼 반복

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,10 @@
 function problem5(money) {
   let price = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let answer = [];
-  answer.push(parseInt(money / price[i]));
-  money %= price[i];
+  for (let i = 0; i < price.length; i++) {
+    answer.push(parseInt(money / price[i]));
+    money %= price[i];
+  }
   return answer;
 }
 
