9b3aa5b5ff7ae760df745e9be8085c9107c7d3df
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 19:56:38 +0900

    [problem4] feat: 필요한 기능 check

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,13 @@
 function problem4(word) {
   var answer;
+  // 문자열을 문자로 split
+
+  // 변환된 문자열을 붙이기
+
   return answer;
 }
+
+// ASCII를 사용하여 문자 변환하는 함수
+function changeWord(word) {}
 
 module.exports = problem4;
