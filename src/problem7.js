ae1a479257404992a85c752a5b22cc5c6fe0e298
shinhyunsu01 <shine.master9687@gmail.com>
2022-11-01 17:09:38 +0900

    7 solve

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,42 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+	let answer;
+	let nearFriends = [];
+	let suggest = [];
+
+	const myfriends = friends
+		.filter((el) => el.includes(user))
+		.flat()
+		.filter((name) => name !== user);
+
+	myfriends.forEach(
+		(friend) =>
+			(nearFriends = [
+				...nearFriends,
+				...friends.filter(
+					(entry) => !entry.includes(user) && entry.includes(friend)
+				),
+			])
+	);
+
+	nearFriends.forEach((friend) => {
+		let find = suggest.findIndex((ele) => ele.name === friend[1]);
+		if (find === -1) suggest.push({ name: friend[1], point: 10 });
+	});
+	visitors.forEach((friend) => {
+		let find = suggest.findIndex((ele) => ele.name === friend);
+		if (!myfriends.includes(friend)) {
+			if (find === -1) {
+				suggest.push({ name: friend, point: 1 });
+			} else {
+				suggest[find].point += 1;
+			}
+		}
+	});
+	answer = suggest
+		.sort((a, b) => b.point - a.point)
+		.map((friend) => friend.name);
+
+	return answer;
 }
 
 module.exports = problem7;
