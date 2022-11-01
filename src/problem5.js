a244f842d901c78f9046307585efb2b73c5f0699
jincde <asdzxc@kakao.com>
2022-11-01 12:15:08 +0900

    fix : problem5 변수명 교체

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,8 @@
 function problem5(money) {
-  const moneys = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   var answer = [];
 
-  for (let temp of moneys) {
+  for (let temp of currency) {
     answer.push(Math.floor(money / temp));
     money %= temp;
   }
