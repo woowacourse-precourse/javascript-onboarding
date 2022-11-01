70b9e84804ac23caced89a8fc8a8c3416638faf5
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:14:24 +0900

    feat(problem1): 곱과 합을 비교해 가장 큰수를 구한다

--- src/problem1.js
+++ src/problem1.js
@@ -10,6 +10,12 @@ function problem1(pobi, crong) {
   function multiple(arr){
     return arr.reduce((a, b) => Number(a) * Number(b), 1);  
   }
+  
+  let pobiLeftMax = Math.max(sum(pobiLeftArr),multiple(pobiLeftArr));
+  let pobiRightMax = Math.max(sum(pobiRightArr),multiple(pobiRightArr));
+  let crongLeftMax = Math.max(sum(crongLeftArr),multiple(crongLeftArr));
+  let crongRightMax = Math.max(sum(crongRightArr),multiple(crongRightArr));
+ 
 }
 
 module.exports = problem1;
