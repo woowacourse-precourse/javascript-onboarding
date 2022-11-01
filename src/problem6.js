4716a9b175b3c6ffe1449ee9bd9991372f797007
Minhye Kang <alsgp9898@naver.com>
2022-11-01 20:03:58 +0900

    feat(PROBLEM6): 닉네임을 두글자씩 분해한 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,9 +1,20 @@
 function problem6(forms) {
   const nickNameArr = getNickNameArr(forms);
+  const splitNickName = getsplitNickName(nickNameArr);
 }
 
 const getNickNameArr = (forms) => {
   return forms.map((item) => item[1]);
+};
+
+const getsplitNickName = (nickNameArr) => {
+  const splitNickName = [];
+  for (let nickName of nickNameArr) {
+    for (let index = 0; index < nickName.length - 1; index++) {
+      splitNickName.push(nickName[index] + nickName[index + 1]);
+    }
+  }
+  return splitNickName;
 };
 
 module.exports = problem6;
