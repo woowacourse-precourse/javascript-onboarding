83d5d6689b458298dc398233870d694295c784df
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:24:08 +0900

    feat: [problem3] 각 자릿수 3, 6, 9 카운팅

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,16 @@
 function problem3(number) {
   var answer;
   return answer;
+const countMultiple3OfEachDigit = (num) => {
+  let count = 0
+  while (num > 0) {
+    let digit = num % 10
+    num = (num - digit) / 10
+    if (digit === 3 || digit === 6 || digit === 9) {
+      count += 1
+    }
+  }
+  return count
 }
 
 module.exports = problem3;
