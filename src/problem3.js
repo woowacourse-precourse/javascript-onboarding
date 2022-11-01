1d0b7fb93406eaa7cb2ada1d548b14ec02b014d9
bear109 <jokerbear109@gmail.com>
2022-11-01 18:42:14 +0900

    feat: problem3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,13 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (let i = 1; i <= number; i++) {
+    let temp = i.toString().split('');
+    if (temp.includes('3') || temp.includes('6') || temp.includes('9')) {
+      for (let j = 0; j < temp.length; j++) {
+        if (temp[j] === '3' || temp[j] === '6' || temp[j] === '9') answer += 1;
+      }
+    }
+  }
   return answer;
 }
 
