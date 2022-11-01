af69401ffc84c8e473e26e1cc86985e88e78b466
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:34:24 +0900

    [problem3] feat:3의배수가 해당배열에 있으면 카운팅

--- src/problem3.js
+++ src/problem3.js
@@ -4,6 +4,8 @@ function problem3(number) {
   let cnt = 0;
   for (i = 1; i <= number; i++)
     (i + "").split("").forEach(function (num) {
+      if (num % 3 === 0 && num !== "0") cnt += 1;
+    });
 }
 
 module.exports = problem3;
