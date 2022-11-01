3da65ed9d5beac6368f0d28ac3ac0bbdd9b50b84
hvvany <kjunhwan1998@gmail.com>
2022-11-01 21:02:40 +0900

    feat(build): solve problem_5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
 function problem5(money) {
+  const bill = [50000,10000,5000,1000,500,100,50,10,1];
   let answer = [];
-  return answer;
+  for (let i=0; i < 9; i++) {
+    answer.push(parseInt(money / bill[i]));
+    money %= bill[i];
+  }
+  return answer
 }
 
 module.exports = problem5;
