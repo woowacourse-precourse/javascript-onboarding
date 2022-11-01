9f5ca07016123690aa13425fb866e2e078a4b181
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 13:58:09 +0900

    feat[P2]: 배열의 중복된 값이 있는지 확인하고 반복하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,24 @@
 function problem2(cryptogram) {
 	let answer = [];
 	let arrayWord = [...cryptogram];
-	arrayWord.forEach((element, index) => {
-		if (element == arrayWord[index - 1]) answer.pop();
-		else answer.push(element);
-	});
+	do {
+		answer = [];
+		arrayWord.forEach((element, index) => {
+			if (element == arrayWord[index - 1]) answer.pop();
+			else answer.push(element);
+		});
+		arrayWord = [...answer];
+	} while (isDuplicated(answer));
 
-	return answer;
+	function isDuplicated(arr) {
+		let count = 0;
+		arr.forEach((element, index) => {
+			if (element == arr[index - 1]) count++;
+			else return;
+		});
+		return count > 0 ? 1 : 0;
+	}
+	return answer.join("");
 }
 
 module.exports = problem2;
