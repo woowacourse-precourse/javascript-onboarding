f3c2abb87d59896f7a634bd99cb1140613164c3f
lazypalegg <109095071+lazypalegg@users.noreply.github.com>
2022-11-01 21:02:53 +0900

    Feat: find largest num

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,22 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
-}
+function findLargest(arr) {
+  let compare = [];
+  let i = 0;
+  while (i < arr.length) {
+    let newArr = arr[i].toString().split("");
+    newArr = newArr.map(Number);
+    let numAdd = 0;
+    let numMultiply = 1;
+    numAdd = newArr.reduce((previousElement, currentElement) => previousElement + currentElement, numAdd);
+    numMultiply = newArr.reduce((previousElement, currentElement) => previousElement * currentElement, numMultiply);
+    compare.push(numAdd);
+    compare.push(numMultiply);
+    i++;
+    }
+    compare.sort((a, b) => b - a);
+    return compare[0];
+  }
+
+
+
 
 module.exports = problem1;
