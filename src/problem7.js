20a45575d31a30026cabf086f56418b26db1ab3c
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:48:23 +0900

    [problem7] feat: 생성된 사람이름에 점수를 이차원배열로 추가

--- src/problem7.js
+++ src/problem7.js
@@ -9,6 +9,10 @@ function problem7(user, friends, visitors) {
   friendName = friendName.filter(
     (item, pos) => friendName.indexOf(item) === pos
   ); //friendName 에 중복제거
+  let scoreFriend = [];
+  friendName.forEach(function (score) {
+    scoreFriend.push([0, score]);
+  }); // 점수 이름 담긴 scoreFriend + 점수는 0으로 초기화
 }
 
 module.exports = problem7;
