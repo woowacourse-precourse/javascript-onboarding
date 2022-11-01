b023d0628d7cb74c4990c9b04429b113fef0bca9
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:31:32 +0900

    feat:PROBLEM6:기능 목록 작성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
+/*
+ #기능 요구 사항
+신청받은 닉네임 중 같은 글자가 연속적으로 포함 되는 
+닉네임을 작성한 지원자의 이메일 목록을 return 하도록 solution 메서드를 완성.
+
+1. 주어진 금액을 가장 큰 단위의 화폐부터 작은 단위의 화폐로 가장 많이 포함되도록 한다.
+
+*/
+
 function problem6(forms) {
-  var answer;
+    let answer;
   return answer;
 }
 
