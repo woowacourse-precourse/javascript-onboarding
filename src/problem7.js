0aad7a6d67d0b20cd8ba94e99bba1f6c8d05580e
daal2quf <daal2quf@gmail.com>
2022-11-01 21:05:22 +0900

    변수명 읽기쉽게 변경

--- src/problem7.js
+++ src/problem7.js
@@ -2,7 +2,7 @@ function problem7(user, friends, visitors) {
   var answer;
   let userFriends = [];
   let newUserFriends = [];
-  let userFriendMap = new Map()
+  let friendScoreMap = new Map()
 
   for (let friend of friends) {
     if (friend.includes(user)) {
@@ -12,43 +12,43 @@ function problem7(user, friends, visitors) {
         userFriends.push(friend[0])
       }
     } else {
-      userFriendMap.set(friend[0], 0);
-      userFriendMap.set(friend[1], 0);
+      friendScoreMap.set(friend[0], 0);
+      friendScoreMap.set(friend[1], 0);
     }
   }
 
   for (let friend of friends) {
     userFriends.forEach((userFriend) => {
       if (friend.includes(userFriend)) {
-        const FOfF = friend.find((name) => name != user && name != userFriend)
-        if (FOfF) {
-          newUserFriends.push(FOfF)
+        const friendOfFriend = friend.find((name) => name != user && name != userFriend)
+        if (friendOfFriend) {
+          newUserFriends.push(friendOfFriend)
         }
       }
     })
   }
 
   for (let friend of userFriends) {
-    userFriendMap.delete(friend)
+    friendScoreMap.delete(friend)
   }
 
   for (let friend of newUserFriends) {
-    if (userFriendMap.has(friend)) {
-      userFriendMap.set(friend, userFriendMap.get(friend) + 10)
+    if (friendScoreMap.has(friend)) {
+      friendScoreMap.set(friend, friendScoreMap.get(friend) + 10)
     }
   }
 
   for (let visitor of visitors) {
     if (!userFriends.includes(visitor)) {
-      if (userFriendMap.has(visitor)) {
-        userFriendMap.set(visitor, userFriendMap.get(visitor) + 1)
+      if (friendScoreMap.has(visitor)) {
+        friendScoreMap.set(visitor, friendScoreMap.get(visitor) + 1)
       } else {
-        userFriendMap.set(visitor, 0)
+        friendScoreMap.set(visitor, 0)
       }
     }
   }
   
-    const RecommandationRanking = [...userFriendMap.entries()].sort(([friend1, score1], [friend2, score2]) => {
+    const RecommandationRanking = [...friendScoreMap.entries()].sort(([friend1, score1], [friend2, score2]) => {
       if (score1 === score2) {
        return friend1.localeCompare(friend2)
       } else {
