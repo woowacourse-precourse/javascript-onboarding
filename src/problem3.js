37b8844f170500164f16fb44899bda3bd0e41138
lazypalegg <109095071+lazypalegg@users.noreply.github.com>
2022-11-01 21:10:07 +0900

    Feat: 369 match test

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,18 @@
 function problem3(number) {
   if (number >= 1 && number <= 10000 && Number.isInteger(number)){
+      let answer = 0;
+      let i = 0;
       
+      while (i <= number) {
+        if (/[369]/.test(i) == true) {
+          let numberOfOccurrence = i.toString().match(/[369]/g).length;
+          answer = answer + numberOfOccurrence;
+          i++;
+        } else {
+          i++;
+        }
+      }
+      return answer;
   }
   
 }
