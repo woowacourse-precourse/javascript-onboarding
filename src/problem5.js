932de211b9c8709e1dc5b1553a49c6e981545865
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-01 23:19:36 +0900

    problem5: 기능 구현
    changeMoney: 거슬러질 수 있는 가장 큰 액수부터 돈을 거슬러 배열로 만들기 위해 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,17 @@
+const moneySizes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+const changeMoney = (money) => {
+  const changedMoneyCount = [];
+  let changingMoney = money;
+  for (const sizes of moneySizes) {
+    changedMoneyCount.push(parseInt(changingMoney / sizes));
+    changingMoney %= sizes;
+  }
+  return changedMoneyCount;
+};
+
 function problem5(money) {
-  var answer;
+  const answer = changeMoney(money);
   return answer;
 }
 
