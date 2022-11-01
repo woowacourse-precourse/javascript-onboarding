7b127e0b5db3770b8fea42cd48815da5ef029278
Jiho1996 <jhp9270@naver.com>
2022-11-01 13:36:15 +0900

    comment : isremoved 및 포인터 지정 관련 주석 추가

--- src/problem2.js
+++ src/problem2.js
@@ -2,7 +2,6 @@ function problem2(cryptogram) {
   let leftPointer = 0;
   let rightPoiner = 1;
   const cryptogramList = [...cryptogram];
-
   const moveAllPointerForward = () => {
     leftPointer += 1;
     rightPoiner += 1;
@@ -16,34 +15,42 @@ function problem2(cryptogram) {
     return leftPointer >= 0 && rightPoiner < cryptogramList.length;
   }
 
+  /**
+   * 두 포인터를 가장 앞 위치로 설정하는 함수
+   */
   const initPointer = () => {
     leftPointer = 0;
     rightPoiner = 1;
   }
+
+  /**
+   * 두 포인터가 가리키는 값이 같은지 확인해주는 함수
+   * @returns {boolean}
+   */
   const isEachPointerSameLetter = () => cryptogramList[leftPointer] === cryptogramList[rightPoiner]
   const moveRightPointerForward = () => rightPoiner ++;
   
   let isRemoved = true;
 
-  while (isRemoved){
+  while (isRemoved){ // 지운 이력이 없을 때 까지 반복
     initPointer();
-    isRemoved = false
-  while (isValidRange()){
+    isRemoved = false // 지운 이력 false로 set
+    while (isValidRange()){
     
-    if (!isEachPointerSameLetter()){
-      moveAllPointerForward();
-      continue;
-    }
-    while (isEachPointerSameLetter()){
-      moveRightPointerForward();
-    }
+      if (!isEachPointerSameLetter()){
+        moveAllPointerForward();
+        continue;
+      }
+      while (isEachPointerSameLetter()){
+        moveRightPointerForward();
+      }
 
-    cryptogramList.splice(leftPointer, rightPoiner - leftPointer);
-    isRemoved = true;
-    rightPoiner = leftPointer + 1;
-  }
+      cryptogramList.splice(leftPointer, rightPoiner - leftPointer);
+      isRemoved = true; // 지운 이력 true로 set
+      rightPoiner = leftPointer + 1; // 오른쪽 포인터 다시 왼쪽 포인터 다음으로 set
     }
-    return cryptogramList.join("");
   }
+    return cryptogramList.join("");
+}
 
 module.exports = problem2;
