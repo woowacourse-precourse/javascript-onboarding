3d66d68c916e9fb07ac0b15f8948504b62617f1b
Lim_JiSeon <dpf226@naver.com>
2022-11-01 20:48:22 +0900

    problem4 - function 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,33 @@
+/*
+기능 목록
+1. 앞의 문자와 같은지 다른지 확인하는 기능
+2. 중복 문자가 있는 문자열인지 확인하는 기능
+*/
+
+function double_check(s1, s2) {
+  if (s1 == s2) 
+    return true;
+  
+  return false;
+}
+
+function check_str(str) {
+  var chr = "";
+
+  for (let i = 0; i < str.length; i++) {
+
+    if (double_check(chr, str[i])) 
+      return true;
+    else
+      chr = str[i];
+
+  }
+  return false;
+}
+
 function problem2(cryptogram) {
   var answer;
+
   return answer;
 }
 
