85d5aaa442e5e6ae3527525bcfb86f706a93554d
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 14:44:17 +0900

    feat: [problem7] user의 친구 목록을 만드는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -11,11 +11,23 @@ function getScoreByVisitor(visitors) {
   return score;
 }
 
+function getUserFriendList(user, friends) {
+  let friendList = [];
+  for (let i = 0; i < friends.length; i++){    
+    if (friends[i][0] == user || friends[i][1] == user){
+      friendList.push(friends[i][0] == user ? friends[i][1] : friends[i][0]);
+    }
+  }
+  return friendList;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   let friendScore = {};
+  let userFriends = []
 
   friendScore = getScoreByVisitor(visitors);
+  userFriends = getUserFriendList(user, friends);
 
   return answer;
 }
