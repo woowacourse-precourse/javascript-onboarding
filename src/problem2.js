185f81899271806a99813c69f6c78cb04f665ff6
NaveOWO <pianowell96@gmail.com>
2022-11-01 15:03:38 +0900

    [ fix ] problem2_중복문자열 3개이상일 때 로직처리

--- src/problem2.js
+++ src/problem2.js
@@ -5,15 +5,23 @@
 
 function problem2(cryptogram) {
   let codeStack = [];
+  let prevLetter;
 
   codeStack.push(cryptogram[0]);
 
   for (let i = 1; i < cryptogram.length; i++) {
-    if (cryptogram[i] === codeStack[codeStack.length - 1]) {
+    if (
+      codeStack.length > 0 &&
+      cryptogram[i] === codeStack[codeStack.length - 1]
+    ) {
       codeStack.pop();
     } else {
+      if (cryptogram[i] === prevLetter) {
+        continue;
+      }
       codeStack.push(cryptogram[i]);
     }
+    prevLetter = cryptogram[i];
   }
 
   const answer = codeStack.join("");
