bd63c13fc564515b07cbdd57f2cb5f69b56fa6f6
2wndrhs <2wndrhs@gmail.com>
2022-11-01 03:31:07 -0700

    refactor(PROBLEM3): 일반 함수를 화살표 함수로 리팩토링

--- src/problem3.js
+++ src/problem3.js
@@ -1,40 +1,40 @@
 // 숫자를 각 자리 숫자로 분리하는 기능
-function splitNumbers(numbers) {
-  const splitedNumbers = numbers.map((number) =>
+const splitNumbers = (numbers) => {
+  const splittedNumbers = numbers.map((number) =>
     number
       .toString()
       .split("")
       .map((number) => +number)
   );
-  return splitedNumbers;
-}
+  return splittedNumbers;
+};
 
 // 분리한 숫자들 중에서 3, 6, 9가 얼마나 포함되어 있는지 구하는 기능
-function findNumber(source, number) {
+const findNumber = (source, number) => {
   const result = source
     .flat()
     .reduce((acc, cur) => (cur === number ? ++acc : acc), 0);
   return result;
-}
+};
 
 // 총 손뼉을 몇 번 쳐야 하는지 숫자를 모두 더하는 기능
-function addNumbers(numbers) {
+const addNumbers = (numbers) => {
   const result = numbers.reduce((acc, cur) => acc + cur, 0);
   return result;
-}
+};
 
 // solution
-function solution(number) {
+const solution = (number) => {
   const sequences = Array.from({ length: number }, (_, i) => i + 1);
-  const splitedNumbers = splitNumbers(sequences);
+  const splittedNumbers = splitNumbers(sequences);
   const numbers = [
-    findNumber(splitedNumbers, 3),
-    findNumber(splitedNumbers, 6),
-    findNumber(splitedNumbers, 9),
+    findNumber(splittedNumbers, 3),
+    findNumber(splittedNumbers, 6),
+    findNumber(splittedNumbers, 9),
   ];
   const claps = addNumbers(numbers);
   return claps;
-}
+};
 
 function problem3(number) {
   const answer = solution(number);
