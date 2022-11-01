7a64e2cc70ddae2cd13ce523d1ae1491cf84b5ca
7ngenious <zxvs1218@gmail.com>
2022-11-01 23:07:11 +0900

    comment: [Problem6] Add Solution Comment

--- src/problem6.js
+++ src/problem6.js
@@ -3,7 +3,7 @@ function problem6(forms) {
   let nameObject = new Object(); //닉네임 2글자를 키로 사용하는 객체선언
   let nickName = [];
   let nameKey = '';
-  let result = [];
+  let result = ''; //중간 결과
 
   for (let i = 0; i < forms.length; i++) {
     //입력받은 폼의 데이터 길이만큼 실행
@@ -11,6 +11,7 @@ function problem6(forms) {
     for (let j = 0; j < nickName[i].length - 1; j++) {
       nameKey = nickName[i].substring(j, j + 2); //각 닉네임에 2글자씩 잘라서 키로 만들어주기
       if (nameObject[nameKey]) {
+        //해당 키가 있을 때
         result = nameObject[nameKey];
         if (result !== forms[i][0]) {
           answer.add(result);
@@ -22,7 +23,7 @@ function problem6(forms) {
   }
 
   answer = [...answer].sort(); //결과값 오름차순 정렬
-  return answer;
+  return answer; //결과 반환
 }
 
 module.exports = problem6;
