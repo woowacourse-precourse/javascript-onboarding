e3d9673fcb45f2185e4443bd205970f76324556e
rayark1 <max980203@snu.ac.kr>
2022-11-01 15:14:02 +0900

    feat(prob5): Implement getNumberOfMoney function

--- src/problem5.js
+++ src/problem5.js
@@ -3,13 +3,24 @@
   1. 주어진 돈의 단위를 이용한 배열을 만든다.
   2. 주어진 돈을 단위로 나눈 몫을 배열에 넣는다.
   3. 주어진 돈을 단위로 나눈 나머지를 다시 주어진 돈으로 사용한다.
-  4. 주어진 돈이 0이 될 때까지 2, 3번을 반복한다.
+  4. 주어진 돈이 0이 될 때까지 2, 3번을 반복한다. (최소 단위가 1, 따라서 무조건 0이 되므로 조건 추가 X)
   5. 배열을 출력한다.
  */
 function problem5(money) {
   var answer;
   const unitOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   return answer;
+}
+
+function getNumberOfMoney(money, unitOfMoney) {
+  // 주어진 돈을 단위로 나눈 몫을 배열에 넣는 함수
+  let restOfMoney = money;
+  let numberOfMoney = [];
+  for (let i = 0; i < unitOfMoney.length; i++) {
+    numberOfMoney.push(Math.floor(restOfMoney / unitOfMoney[i]));
+    restOfMoney = restOfMoney % unitOfMoney[i];
+  }
+  return numberOfMoney;
 }
 
 module.exports = problem5;
