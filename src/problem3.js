5ed92004be87e5da0865fec535169284cd7f1d5f
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 11:17:45 +0900

    Feat: 결과값을 담는 변수와 타겟넘버 상수 생성

--- src/problem3.js
+++ src/problem3.js
@@ -1,17 +1,6 @@
 function problem3(number) {
   let answer = 0;
-
-  for (let i = 1; i <= number; i++) {
-    let count = 0;
-    let numToStr = i.toString();
-
-    for (let j = 0; j < numToStr.length; j++) {
-      if (numToStr[j] === "3" || numToStr[j] === "6" || numToStr[j] === "9")
-        count++;
-    }
-    answer += count;
-  }
-  return answer;
+  const targetNumbers = ["3", "6", "9"];
 }
-
+problem3(33);
 module.exports = problem3;
