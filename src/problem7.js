bb85994782e46acd835a54e48938536596b23724
SeungWoo Kim <osabero@naver.com>
2022-11-01 23:33:58 +0900

    방문자 점수는 기능구현했지만 친구점수는 기능구현이 안됬습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -21,7 +21,7 @@ function problem7(user, friends, visitors) {
     let friendsFri = friMap.get(myFri[i]);
     for(let j = 0; j<friendsFri.length; j++){
       if(friendsFri[j] == user) {continue}
-      if(ScreenOrientation.has(friendsFri[j])) {
+      if(value.has(friendsFri[j])) {
         let value1 = value.get(friendsFri[j])+10
         value.delete(friendsFri[j])
         value.set(friendsFri[j], value1)
@@ -30,14 +30,19 @@ function problem7(user, friends, visitors) {
       }
     }
   }
+
   
-
-
-
- 
-
-
-
+  for(let i = 0; i<visitors.length; i++) {
+    if(myFri.includes(visitors[i])) {continue}
+    if(value.has(visitors[i])){
+      let value1 = value.get(visitors[i]) + 1
+      value.delete(visitors[i])
+      value.set(visitors[i], value1)
+    } else {
+      value.set(visitors[i], 1)
+    }
+  }
+  
   return answer;
 }
 
