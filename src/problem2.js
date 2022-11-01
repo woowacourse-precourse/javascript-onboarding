a9e4b595b807b03b7b5bc947e0c84bd3d755a9fd
kimjiwon <hallu0317@gmail.com>
2022-11-01 15:27:27 +0900

    problem2 완료

--- src/problem2.js
+++ src/problem2.js
@@ -2,19 +2,19 @@ function problem2(cryptogram) {
   var answer = [];
   answer.push(cryptogram[0]);
 
-    for(let i=1; i<cryptogram.length; i++) {
-      if(answer.length === 0) {
-        answer.push(cryptogram[i]);
+  for(let i=1; i<cryptogram.length; i++) {
+    if(answer.length === 0) {
+      answer.push(cryptogram[i]);
+    }
+    else {
+      if(answer[answer.length-1] === cryptogram[i]) {
+        answer.pop();
       }
-      else {
-        if(answer[answer.length-1] === cryptogram[i]) {
-          answer.pop();
-        }
-        else{
-          answer.push(cryptogram[i])
-        }
+      else{
+        answer.push(cryptogram[i])
       }
     }
+  }
   return answer.join("");
 }
 
