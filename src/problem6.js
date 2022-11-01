5cd68f1dae15bc7e663e93a5838a9b0726ff30b7
HYEOK9 <leejaehyuck9@gmail.com>
2022-11-01 13:45:01 +0900

    feat:solve problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,26 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+    let length = forms.length;
+    let answer = [];
+    for (let i = 0; i < length - 1; i++) {
+        let name = forms[i][1];
+        let pushed = false;
+        let count = 0;
+        for (let j = i + 1; j < length; j++) {
+            let compName = forms[j][1];
+            for (let k = 0; k < name.length - 1; k++) {
+                let twoChar = name[k] + name[k + 1];
+                if (compName.includes(twoChar)) {
+                    !pushed && answer.push(forms[i][0]);
+                    answer.push(forms[j][0]);
+                    pushed = true;
+                    count == 0 ? (count += 2) : count++;
+                }
+            }
+        }
+        length -= count;
+    }
+    answer = answer.sort((a, b) => b[0].length - a[0].length);
+    return answer;
 }
 
 module.exports = problem6;
