67fd5ef36bcab45e40e3d359dd5afad5046eb6f3
allzeroyou <ekdudd119@naver.com>
2022-11-01 16:58:52 +0900

    [feat] 제한사항 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,23 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let isRestrict = false;
+
+  if (pobi.length !== 2 || crong.length !== 2) {
+    isRestrict = true;
+  } else if (
+    pobi.includes(1) ||
+    pobi.includes(400) ||
+    crong.includes(1) ||
+    crong.includes(400)
+  ) {
+    isRestrict = true;
+  } else if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    isRestrict = true;
+  }
+
+  if (isRestrict) {
+    return -1;
+  } else {
+  }
 }
 
 module.exports = problem1;
