38e25e678bd840c8e0dc6a324ab8baa99ce3a3f9
EUNA <beni1026@naver.com>
2022-11-01 17:41:54 +0900

    problem3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for(let i=1;i<=number;i++){
+    let numArr = (i+"").split("");
+    for(num of numArr){
+      if(num === '3' || num === '6' || num==='9') answer++;
+    }
+  }
   return answer;
 }
 
