31f78d7b1325014ddba85703fe8c80dab70b8528
yeonkyoung <keep.ykk@gmail.com>
2022-11-01 17:29:53 +0900

    feat: Implement problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,25 @@
 function problem2(cryptogram) {
   var answer;
+  var crytogram_tmp = cryptogram.split("");
+  var tmp = [];
+  while (true) {
+    var bef = "";
+    var aft = "";
+    tmp = crytogram_tmp.filter((value, index) => {
+      aft = index + 1 != crytogram_tmp.length ? crytogram_tmp[index + 1] : "";
+      if ((value != bef) & (value != aft)) {
+        bef = value;
+        return value;
+      }
+      bef = value;
+    });
+    if (crytogram_tmp.length == tmp.length) {
+      answer = tmp.join("");
+      break;
+    }
+    crytogram_tmp = [...tmp];
+    tmp.length = 0;
+  }
   return answer;
 }
 
