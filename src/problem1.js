4529b1a276ed5188393f90b1f310929471b3ea00
EunjE <dldmswp3@naver.com>
2022-11-01 18:43:58 +0900

    update p1 && solve p2

--- src/problem1.js
+++ src/problem1.js
@@ -2,11 +2,13 @@ function findMax(x) {
   var sum = 0;
   var mul = 1;
 
-  while (x / 10 != 0) {
+  while (Math.floor(x / 10) > 0) {
     sum += x % 10;
-    mum *= x % 10;
-    x /= 10;
+    mul *= x % 10;
+    x = Math.floor(x/10);
   }
+  sum += x;
+  mul *= x;
 
   return Math.max(sum, mul);
 }
@@ -14,6 +16,8 @@ function findMax(x) {
 
 function problem1(pobi, crong) {
   var answer;
+  if(pobi[1] !== pobi[0]+ 1) return -1;
+  if(crong[1] !== pobi[0] + 1) return -1;
 
   var maxPobi = Math.max(findMax(pobi[0]), findMax(pobi[1]));
   var maxCrong = Math.max(findMax(crong[0]), findMax(crong[1]));
--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,25 @@
 function problem2(cryptogram) {
   var answer;
+
+  while(true){
+    var len = cryptogram.length;
+    var tmp = "";
+    var prev = "";
+    for(var i = 1; i < len; i++){
+    
+      if (cryptogram[i] !== cryptogram[i - 1] && prev !== cryptogram[i-1]){
+        tmp += cryptogram[i-1];
+      }
+      else prev = cryptogram[i-1];
+
+    }
+    if (cryptogram[len - 1] != cryptogram[len - 2]) tmp += cryptogram[len - 1];
+
+    if(cryptogram === tmp) break;
+    cryptogram = tmp;
+  }
+  answer = cryptogram;
+
   return answer;
 }
 
