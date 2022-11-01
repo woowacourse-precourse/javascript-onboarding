932748b01a7c4fcc9c063276ed4160235e91a41a
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:28:43 +0900

    feat:PROBLEM5:화폐 개수 구하기 구현

--- src/problem5.js
+++ src/problem5.js
@@ -11,7 +11,22 @@
 
 function problem5(money) {
   let answer;
+  answer = solution(money);
   return answer;
+}
+
+/*주어진 금액을 가장 큰 단위의 화폐부터 작은 단위의 화폐로 가장 많이 포함되도록 한다.*/
+function solution(money) {
+    
+  const price = [50000,10000,5000,1000,500,100,50,10,1];
+  const result = new Array(9);
+
+  for(let i = 0 ; i< result.length; i++) {
+      result[i] = parseInt(money/price[i]);
+      money = money - parseInt(money/price[i]) * price[i];
+  }
+
+  return result;
 }
 
 module.exports = problem5;
