1ad8a5a6e530c40832107c5d549fa8840faa5f9c
rickyshu <hoinleekk@naver.com>
2022-11-01 17:05:46 +0900

    problme3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  const regexStr = /[369]/g;
+  for (let i = 1; i <= number; i++) {
+    answer += (i.toString().match(regexStr) || []).length;
+  }
   return answer;
 }
 
