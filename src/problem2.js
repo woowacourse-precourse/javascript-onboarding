412d26174ab047a2bf33405be3fe28953636e175
wjdtjdgns <person3275@gmail.com>
2022-11-01 19:28:26 +0900

    문자의 연속적인 부분이 있는지 확인하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  while(isContinual(cryptogram)) {
+    
+  }
+  return cryptogram;
 }
 
+// 인자로 받는 문자에 연속적인 부분이 있는지 확인하는 함수
+function isContinual(string) {
+  const strArr = [...string];
+  if(strArr.length === 0) {
+    return false;
+  }
+  for(let i = 1; i < strArr.length; i++) {
+    if(strArr[i - 1] === strArr[i]) {
+      return true;
+    }
+  }
+  return false;
+}
 module.exports = problem2;
