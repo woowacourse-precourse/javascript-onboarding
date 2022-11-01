6bbbe548fa4b5ef15b239d4a0a9f708ad218f2b3
leeharyung <lhr4884@naver.com>
2022-11-01 13:11:21 +0900

    add problem1 solution

--- src/problem1.js
+++ src/problem1.js
@@ -1,4 +1,4 @@
-const checkIsError = (pobi, crong) => {
+function checkIsError(pobi, crong) {
   // 왼쪽 페이지는 홀수 이어야한다.
   if (!pobi[0] % 2 || !crong[0] % 2) return 1;
   if (pobi[1] % 2 || crong[1] % 2) return 1;
@@ -13,26 +13,17 @@ const checkIsError = (pobi, crong) => {
   if (pobi[1] < 2 || pobi[1] > 400 || crong[1] < 2 || crong[1] > 400) return 1;
 
   return 0;
-};
+}
 
-const addDigits = (num) => {
-  let sum = 0;
-  while (num > 0) {
-    sum += num % 10;
-    num = Math.floor(num / 10);
-  }
-  return sum;
-};
+// 각 자리수 더하기
+function addDigits(num) {
+  return [...num.toString()].reduce((acc, cur) => acc + Number(cur), 0);
+}
 
-const multiplyDigits = (num) => {
-  let multiply = 1;
-  while (num > 0) {
-    multiply *= num % 10;
-    num = Math.floor(num / 10);
-  }
-  console.log(multiply);
-  return multiply;
-};
+// 각 자리수 곱하기
+function multiplyDigits(num) {
+  return [...num.toString()].reduce((acc, cur) => acc * Number(cur), 1);
+}
 
 // 포비 win 1, 크롱 win 2, 무승부 0, 에러 -1
 function problem1(pobi, crong) {
@@ -41,18 +32,14 @@ function problem1(pobi, crong) {
   // 포비와 크롱의 입력값에 오류가 있는지 확인
   if (checkIsError(pobi, crong)) return -1;
 
-  const left_pobi = pobi[0];
-  const right_pobi = pobi[1];
-
-  const left_crong = crong[0];
-  const right_crong = crong[1];
+  const [left_pobi, right_pobi] = pobi;
+  const [left_crong, right_crong] = crong;
 
   let sum_pobi = 0,
-    sum_crong = 0;
-  let multiply_pobi = 0,
-    multiply_crong = 0;
-
-  let max_pobi = 0,
+    sum_crong = 0,
+    multiply_pobi = 0,
+    multiply_crong = 0,
+    max_pobi = 0,
     max_crong = 0;
 
   sum_pobi =
