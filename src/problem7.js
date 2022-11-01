7d73c1f48f5b2062584d29ab853ade19598e2e44
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-01 19:23:47 +0900

    feat: 문제7 친구 관계 정리하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+  const relationMap = new Map();
+
+  // 친구 관계 정리
+  friends.forEach((friend) => {
+    for (let i = 0; i < friend.length; i++) {
+      if (!relationMap.has(friend[i])) {
+        relationMap.set(friend[i], []);
+      }
+      relationMap.get(friend[i]).push(i == 0 ? friend[1] : friend[0]);
+    }
+  });
+
   return answer;
 }
 
