48e5d49ae10c49618ff476e6a55442a5fa5264cd
moseung <tmdah900@naver.com>
2022-11-01 16:28:46 +0900

    ✨ problem 문제 7 기능 2 구현 friends에서 함께아는 친구를 찾아서 점수를 계산하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -10,9 +10,36 @@ function getUserFriend(friends, user) {
   return userFriend;
 }
 
+function getKnowWithFriendScore(friendsExceptUser, userFriend) {
+  let friendScore = {};
+  for (i = 0; i < friendsExceptUser.length; i++) {
+    if (friendsExceptUser[i]?.filter((x) => userFriend.includes(x))) {
+      const knowWith = friendsExceptUser[i]?.filter(
+        (x) => !userFriend.includes(x)
+      );
+      if (knowWith in friendScore) {
+        friendScore = {
+          ...friendScore,
+          [knowWith]: friendScore[knowWith] + 10,
+        };
+      } else {
+        friendScore = { ...friendScore, [knowWith]: 10 };
+      }
+    }
+  }
+  return friendScore;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   const userFriend = getUserFriend(friends, user);
+  let friendsExceptUser = friends.filter((v) => !v.includes(user));
+  let visitorsExceptUserFriends = visitors.filter(
+    (v) => !userFriend.includes(v)
+  );
+  let friendScore = {};
+
+  friendScore = getKnowWithFriendScore(friendsExceptUser, userFriend);
   return answer;
 }
 
