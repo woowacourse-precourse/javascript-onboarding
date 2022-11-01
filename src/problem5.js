ac57a5127cc94b13fbeb79d8744ea3156e0dab87
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 13:01:58 +0900

    Problem5: 기능 목록 작성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  /*
+  기능 목록
+  1. money를 상위 단위로 나눈 몫을 구하고, 그 나머지를 하위 단위로 나눈 몫을 구하는 과정을 반복
+  2. 전체 단위당 개수의 배열을 만들어 반환
+  */
 }
 
 module.exports = problem5;
