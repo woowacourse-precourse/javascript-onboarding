e0f84cacb9364db7369aad5cdba049ee1bb7c2ed
ho-ji <wkdd0205@gmail.com>
2022-11-01 20:38:23 +0900

    feat: 문제7 기능 구현
    user와 친구는 점수를 -1로, user의 친구의 친구는 점수를 10점 추가함

--- src/problem7.js
+++ src/problem7.js
@@ -25,6 +25,19 @@ function problem7(user, friends, visitors) {
     listFriendship[numberOfFriend.get(user1)].push(user2);
     listFriendship[numberOfFriend.get(user2)].push(user1);
   }
+  for (let i = 0; i < listFriendship[0].length; i++) {
+    score.set(listFriendship[0][i], -1);
+    for (let j = 0; j < listFriendship[i].length; j++) {
+      if (
+        score.has(listFriendship[i][j]) &&
+        score.get(listFriendship[i][j]) != -1
+      ) {
+        score.set(listFriendship[i][j], score.get(listFriendship[i][j]) + 10);
+      } else if (!score.has(listFriendship[i][j])) {
+        score.set(listFriendship[i][j], 10);
+      }
+    }
+  }
 
   return answer;
 }
