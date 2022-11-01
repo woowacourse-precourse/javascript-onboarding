6db009ccb0bf731aabf04d8bb53559ca1bca24d0
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:28:25 +0900

    [problem2] feat: 문자열에서 앞 뒤 문자가 같은 문자열 처리하는 함수

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
 function problem2(cryptogram) {
   var answer;
   return answer;
+  const cryptogramArray = cryptogram.split("");
+  let cryptogramString = "";
+  let waste = "";
+  cryptogramArray.forEach((code, index, cryptogramArray) => {
+    if (
+      code !== cryptogramArray[index - 1] &&
+      code !== cryptogramArray[index + 1]
+    ) {
+      cryptogramString += code;
+      return;
+    }
+    waste += code;
+  });
 }
 
 module.exports = problem2;
