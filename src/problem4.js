51544ad0bab907712afebbd677178a4d1e4560e2
rabbit-22 <chlwlal20@gmail.com>
2022-11-01 15:50:08 +0900

    problem 4 구현

--- src/problem4.js
+++ src/problem4.js
@@ -2,15 +2,22 @@ function problem4(word) {
   let answer = "";
   let reverse = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
 
-  for (let i=0; i<word.lenth; i++) {
+  for (let i=0; i<word.length; i++) {
     let char = word[i];
     if ("a" <= char.toLowerCase() && char.toLowerCase() <= "z") {
-      
+      if (char === char.toUpperCase()) {
+        answer += reverse[char.charCodeAt() - 65];
+      } else { 
+        answer += reverse[char.charCodeAt() - 97].toLowerCase();
+      }
     } else {
       answer += char;
     }
   }
   return answer;
 }
+
+var str = problem4("I love you");
+console.log(str);
 
 module.exports = problem4;
