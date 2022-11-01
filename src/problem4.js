b85e4083fa8049cbc15dbd4f62ea73caa2aa7f0d
yeonkyoung <keep.ykk@gmail.com>
2022-11-01 17:30:11 +0900

    feat: Implement problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,16 @@
 function problem4(word) {
-  var answer;
+  var answer = word.split("").reduce((accu, curr) => {
+    var tmp = " ";
+    if (curr != " ") {
+      tmp = curr.charCodeAt(0);
+      if (tmp > 96) {
+        tmp = String.fromCharCode(122 - tmp + 97);
+      } else {
+        tmp = String.fromCharCode(90 - tmp + 65);
+      }
+    }
+    return accu + tmp;
+  }, "");
   return answer;
 }
 
