67afd8bd7dda33421f4e9ca360b0fc8268bb82d9
Jinu <jinu200131@gmail.com>
2022-11-01 16:29:50 +0900

    (prob 6) 기능 1구현

--- src/problem6.js
+++ src/problem6.js
@@ -9,8 +9,22 @@
 // forms => [['email','name'], ... ]
 // return => 닉네임 중복자 ['email', ...], 오름차순 정렬, 중복 제거
 function problem6(forms) {
-  var answer;
-  return answer;
+	var answer;
+	return answer;
+}
+
+// 1. 두 문자열 두 글자 이상 문자 연속 동일 여부 검사 기능
+// nick_1, nick_2 => 닉네임
+// return => 2글자 이상 중복 부분 존재 : true, 존재X : false
+function isSim(nick_l, nick_r) {
+	var left=0, right=0;
+	for(var left=0; left<nick_l.length-1; left++) {
+		for(var right=0; right<nick_r.length-1; right++) {
+            if(nick_l[left] != nick_r[right]) continue;
+			if(nick_l[left+1] == nick_r[right+1]) return true;
+		}
+	}
+	return false;
 }
 
 module.exports = problem6;
