d621f51b16574a7fbb01d4e646f21cc875465a74
HyeJun <solar3070@naver.com>
2022-11-01 20:44:43 +0900

    feat: problem7/키-값을 순회하며 사용자와 함께 아는 친구 점수 누적 1. 만일 유저가 아무 친구도 없을 경우 점수는 0점이므로 순회하지 않음 2. 본인과 본인의 친구가 아닐 경우 10점

--- src/problem7.js
+++ src/problem7.js
@@ -4,6 +4,16 @@ function problem7(user, friends, visitors) {
     friendMap.set(f1, friendMap.get(f1) ? [...friendMap.get(f1), f2] : [f2]);
     friendMap.set(f2, friendMap.get(f2) ? [...friendMap.get(f2), f1] : [f1]);
   });
+
+  const friendList = friendMap.get(user);
+  const score = {};
+  if (friendList) {
+    for (let [k, v] of friendMap) {
+      if (k !== user && !friendList.includes(k)) {
+        score[k] = v.length * 10;
+      }
+    }
+  }
 }
 
 module.exports = problem7;
