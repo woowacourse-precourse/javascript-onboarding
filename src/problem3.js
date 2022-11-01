d1d0f173dfb2644ba0695d3c6f727cbf18b30af6
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-01 20:45:45 +0900

    feat: 문제3 - 주석 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,9 @@
 function problem3(number) {
   let result = 0;
   for (let i = 1; i <= number; i++) {
+    // 숫자 데이터를 문자 데이터로 변환
     let num = i.toString();
-    // 숫자에 3, 6, 9가 있다면 해당 개수를 result에 추가
+    // 3, 6, 9가 들어 있을 때마다 result에 1씩 더하기
     for (let j = 0; j < num.length; j++) {
       if (num[j] === "3" || num[j] === "6" || num[j] === "9") {
         result++;
