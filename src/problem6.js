73343a5381beeb386c1c7594848a0e8704795ed9
emma <jhj191058@gmail.com>
2022-11-01 16:16:07 +0900

    problem6 기능구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,21 @@
 function problem6(forms) {
 
-}
-module.exports = problem6;
+    answer=new Array();
+  
+   let checkList = [];
+   forms.map((elements) => {
+    nickName = elements[1];
+  
+    for(i =0; i<nickName.length-1; i++) {
+      checkList.push(nickName.substr(i,2))
+    }
+   })
+  
+   let arr = checkList.filter((element, index) => checkList.indexOf(element) != index);
+   arr = new Set(arr);
+   arr = [...arr]
+   return arr;
+  
+  }
+ 
+  module.exports = problem6;
