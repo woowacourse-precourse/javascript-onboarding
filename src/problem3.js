98349b7017a56a5eda1482db12d48293dcb1e8e1
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 17:57:11 +0900

    problem3) 구현을 함수로 바꿔보기

--- src/problem3.js
+++ src/problem3.js
@@ -1,16 +1,31 @@
 function check369(num) {
-  return [3, 6, 9].includes(num % 10);
+  return [3, 6, 9].includes(num);
+}
+
+function getRest(divider) {
+  return function (num) {
+    return num % divider;
+  };
+}
+
+function divide(divider) {
+  return function (num) {
+    return Math.floor(num / divider);
+  };
 }
 
 function problem3(number) {
   var answer = 0;
+  const divider = 10;
+  const getRestByDivide10 = getRest(divider);
+  const divideBy10 = divide(10);
 
   for (let i = 1; i <= number; i++) {
     let targetNumber = i;
 
     while (targetNumber > 0) {
-      if (check369(targetNumber)) answer++;
-      targetNumber = Math.floor(targetNumber / 10);
+      if (check369(getRestByDivide10(targetNumber))) answer++;
+      targetNumber = divideBy10(targetNumber);
     }
   }
 
