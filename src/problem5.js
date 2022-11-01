da134b53a76ac6367225b63ca7876845d93bf530
piporica <49173614+piporica@users.noreply.github.com>
2022-11-01 20:39:59 +0900

    problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
+MONEY_CLASS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+
 function problem5(money) {
-  var answer;
+  let left_money = money
+  let answer = []
+  MONEY_CLASS.forEach(element => {
+    let count = parseInt(left_money / element)
+    answer.push(count)
+    left_money = left_money % element 
+  });
+
   return answer;
 }
 
