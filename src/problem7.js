56eb6cae5c5e2876e47e92afa5617f357afc7e98
이여진 <jinspace3710@gmail.com>
2022-11-01 16:36:42 +0900

    feat: problem7 기능 1 - 이미 친구인 사람 추출

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,20 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
+  let alreadyFriends = findFriends(user, friends);
+
   return answer;
 }
 
+function findFriends(user, friends) {
+  let alreadyFriends = [];
+
+  for (i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      if (friends[i][0] === user) alreadyFriends.push(friends[i][1]);
+      else alreadyFriends.push(friends[i][0]);
+    }
+  }
+
+  return alreadyFriends;
+}
 module.exports = problem7;
