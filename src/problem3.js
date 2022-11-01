f92314760452ee2c16c6edce5308328e019ccf35
HYEOK9 <leejaehyuck9@gmail.com>
2022-11-01 15:45:47 +0900

    feat:problem3 제한사항 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,4 +1,5 @@
 function problem3(number) {
+    if (number < 1 || 10000 < number) return -1;
     let answer = 0;
     for (let i = 1; i <= number; i++) {
         for (let j = 0; j < i.toString().length; j++) {
