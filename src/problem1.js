86d344b4bc9d5dbc7198fdcb9c5f4f3f3f945571
Ryong-E <think8867@gmail.com>
2022-11-01 13:05:04 +0900

    style: 코드 포멧팅 미적용 부분 적용

--- src/problem1.js
+++ src/problem1.js
@@ -1,9 +1,19 @@
 function problem1(pobi, crong) {
   if (pobi[1] - pobi[0] > 1 || crong[1] - crong[0] > 1) {
     return -1;
-  } else if (pobi[0] === 1 || pobi[1] === 400 || crong[0] === 1 || crong[1] === 400) {
+  } else if (
+    pobi[0] === 1 ||
+    pobi[1] === 400 ||
+    crong[0] === 1 ||
+    crong[1] === 400
+  ) {
     return -1;
-  } else if (pobi[0] % 2 === 0 || crong[0] % 2 === 0 || pobi[1] % 2 === 1 || crong[1] % 2 === 1) {
+  } else if (
+    pobi[0] % 2 === 0 ||
+    crong[0] % 2 === 0 ||
+    pobi[1] % 2 === 1 ||
+    crong[1] % 2 === 1
+  ) {
     return -1;
   }
   const pobiScore = Math.max.apply(
--- src/problem2.js
+++ src/problem2.js
@@ -4,14 +4,14 @@ function problem2(cryptogram) {
 
 function getDecodedCryptogram(cryptogram) {
   const DUPLICATED_REGEX = /([a-z])\1+/g;
-  while (isInDuplicateWord(cryptogram,DUPLICATED_REGEX)) {
+  while (isInDuplicateWord(cryptogram, DUPLICATED_REGEX)) {
     cryptogram = cryptogram.replace(DUPLICATED_REGEX, '');
   }
 
   return cryptogram;
 }
 
-function isInDuplicateWord(cryptogram,DUPLICATED_REGEX){
-  return DUPLICATED_REGEX.test(cryptogram)
+function isInDuplicateWord(cryptogram, DUPLICATED_REGEX) {
+  return DUPLICATED_REGEX.test(cryptogram);
 }
 module.exports = problem2;
