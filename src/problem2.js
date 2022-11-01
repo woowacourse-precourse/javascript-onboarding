f7d729521690c584a65040950befc3a8876a3e4f
hjhjhj00 <dlapdlffldpald1@naver.com>
2022-11-01 23:46:12 +0900

    num.3

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,28 @@
+var cryptogram = "browoanoommnaon";
+/*
+var crypto_arr = cryptogram.split('').map(x=>parseInt(x));
+var fil = crypto_arr.filter((x,index)=>{return x !== crypto_arr[index-1];
+})
+console.log(fil);
+*/
+
 function problem2(cryptogram) {
   var answer;
+  var len = cryptogram.length;
+  var new_crypto;
+  for(var i =1; i<len; i++){
+    if(cryptogram[i]=cryptogram[i-1]){
+      if(i=1){
+        new_crypto = cryptogram.slice(2,len-2);
+      } else{
+      new_crypto = cryptogram.slice(0,i-2);
+      i=i-2;}
+    } 
+  }
+  answer = cryptogram;
   return answer;
 }
 
+
 module.exports = problem2;
+console.log(problem2(crypto));
