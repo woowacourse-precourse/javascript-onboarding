1f07ba9faa3eba37d73126a9b69fc457df3d6582
yeongseokim <1903020@ewhain.net>
2022-11-01 22:34:28 +0900

    feat(problem4): a-z 사이의 소문자 알파벳을 반전된 소문자 알파벳으로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -3,11 +3,16 @@ function problem4(word) {
   let targetWord = '';
   const askiiACode = 'A'.charCodeAt();
   const askiiZCode = 'Z'.charCodeAt();
+  const askiiaCode = 'a'.charCodeAt();
+  const askiizCode = 'z'.charCodeAt();
 
   for (let i = 0; i < word.length; i++) {
     targetWord = word[i].charCodeAt();
     if (targetWord >= askiiACode && targetWord <= askiiZCode) {
       targetWord = askiiZCode - (targetWord - askiiACode);
+    }
+    else if (targetWord >= askiiaCode && targetWord <= askiizCode) {
+      targetWord = askiizCode - (targetWord - askiiaCode);
     }
     answer += String.fromCharCode(targetWord);
   }
