7cc1f40f588da4ca3ca18448be2cb219b91d9ef5
Yoonsik-Shin <sys25800994@gmail.com>
2022-11-01 12:51:15 +0900

    problem3 solved

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    var count = 0;
+
+    for (let i = 1; i <= number; i++) {
+        for (let j of String(i)) {
+            if (j === "3" || j === "6" || j === "9") {
+                count++;
+            }
+        }
+    }
+    var answer = count;
+    return answer;
 }
 
 module.exports = problem3;
