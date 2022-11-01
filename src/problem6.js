0ad472d787c3c8160d5895b456edc36bab9ab67b
김혜진 <khj000217@naver.com>
2022-11-01 18:31:35 +0900

    feat : Q7 문제 결과만 도출

--- src/problem6.js
+++ src/problem6.js
@@ -1,9 +1,38 @@
-function restrictions(forms) {}
+function restrictions(forms) {
+  for (var n = 0; n < forms.length; n++) {
+    var email = forms[n][0];
+    var email_size = email.length;
+
+    console.log('check email', email);
+
+    if (!email.test('@email.com')) forms.shift();
+    if (11 > email_size || email_size >= 20) forms.shift();
+
+    console.log('check email forms', forms);
+  }
+
+  for (var o = 0; o < forms.length; o++) {
+    var nickname = forms[o][1];
+    var nickname_size = nickname.length;
+    var kor = /[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]/;
+    // console.log('check nickname', nickname);
+    if (1 > nickname_size || nickname_size >= 20) forms.shift();
+    for (var p = 0; p < nickname_size; p++) {
+      if (kor.test(nickname[p]) == false) forms.shift();
+    }
+    console.log('check nickname forms', forms);
+  }
+  return forms;
+}
 
 function problem6(forms) {
   var answer = [];
   var nickname = [];
   var check = [];
+
+  // 제한 사항 확인
+  // forms = restrictions(forms);
+  // console.log('check end', forms);
 
   // 닉네임만 추출
   for (var i = 0; i < forms.length; i++) {
@@ -31,6 +60,7 @@ function problem6(forms) {
     }
   }
   answer = answer.sort();
+  // console.log(answer);
 
   return answer;
 }
--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,56 @@
+function restrictions(forms) {
+  return forms;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
+  var friend_list = [];
+  var cross_friend = [];
+  var visitor = [];
+  var score = {};
+
+  // 이미 친구인 유저와 친구유저의 친구를 분리한다.
+  // user 본인은 없앤다.
+  for (var i = 0; i < friends.length; i++) {
+    friend_list.push(friends[i][0]);
+    if (friends[i][1] == user) continue;
+    cross_friend.push(friends[i][1]);
+  }
+
+  // 중복 제거
+  friend_list = Array.from(new Set(friend_list));
+  // cross_friend = Array.from(new Set(cross_friend));
+
+  // 친구의 친구 유저에게 +10점씩 준다.
+  for (var j = 0; j < cross_friend.length; j++) {
+    var name = cross_friend[j];
+    if (name in score) {
+      score[name] = score[name] + 10;
+      continue;
+    }
+    score[name] = 10;
+  }
+
+  // visitors 에서 친구 제거
+  for (var k = 0; k < visitors.length; k++) {
+    // console.log(friend_list.includes(visitors[k]));
+    if (!friend_list.includes(visitors[k])) visitor.push(visitors[k]);
+  }
+
+  // 방문자 유저에게 +1점씩 준다.
+  for (var l = 0; l < visitor.length; l++) {
+    var name = visitor[l];
+    if (name in score) {
+      score[name] = score[name] + 1;
+      continue;
+    }
+    score[name] = 1;
+  }
+
+  // console.log(score);
+
+  answer = Object.keys(score);
+  console.log(answer);
   return answer;
 }
 
