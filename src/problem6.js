731a6cd5840d41afb0ecac0cbc88f52f2cc48919
kimjiwon <hallu0317@gmail.com>
2022-11-01 15:25:04 +0900

    problem6 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var answer=[];
+
+  for(let i=0; i<forms.length; i++) {
+    for(let j=0; j<forms[i][1].length-1; j++) {
+      let str = forms[i][1].substring(j, j+2)
+      for(let k= i+1; k<forms.length; k++) {
+        if(forms[k][1].includes(str)) {
+          answer.push(forms[i][0]);
+          answer.push(forms[k][0]);
+        }
+      }
+    }
+  }
+
+  set = new Set(answer);
+  answer = [...set];
+  return answer.sort();
 }
 
 module.exports = problem6;
