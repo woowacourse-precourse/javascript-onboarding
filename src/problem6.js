af214fa16bb01dc419397af8b9118aebaf30cd6a
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 13:24:25 +0900

    feat: [problem6] 임의의 두 크루의 닉네임에서 발견된 중복 글자를 포함하는 모든 크루들을 반환하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -2,11 +2,27 @@ function getDuplicatedWords(forms, i) {
   return [...forms[i][1]].filter(word => forms[i+1][1].includes(word)).join('');
 }
 
+function checkDuplicate(forms, crews, words) {
+  forms.forEach(form => {
+    if (form[1].includes(words))
+      crews.add(form);
+  });
+  return crews;
+}
+
 function problem6(forms) {
   var answer;
 
+  let checkedWords = [];
+  let crewsWithDuplicated = new Set();
+
   for(let i=0; i < forms.length - 1; i++) {
     let duplicatedWords = getDuplicatedWords(forms, i);
+    if (duplicatedWords && !checkedWords.includes(duplicatedWords)){
+      crewsWithDuplicated = checkDuplicate(forms, crewsWithDuplicated, duplicatedWords);
+    } else {
+      checkedWords.push(duplicatedWords);
+    }
   }
 
   return answer;
