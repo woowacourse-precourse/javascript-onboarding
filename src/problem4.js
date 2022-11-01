be7ca2a4c2dda0c249da356b45caf9331dcf789d
Yeonju Jo <yjj0287@gmail.com>
2022-11-01 23:13:47 +0900

    feat: 알파벳 반대로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
-  var answer;
+  let answer = "";
+
+  for (let x of word) {
+    answer += reverseAlphabet(x.charCodeAt());
+  }
+
   return answer;
+}
+
+function reverseAlphabet(alphabet) {
+  if (alphabet >= 65 && alphabet <= 90)
+    return String.fromCharCode(65 + 90 - alphabet);
+  if (alphabet >= 97 && alphabet <= 122)
+    return String.fromCharCode(97 + 122 - alphabet);
+  return String.fromCharCode(alphabet);
 }
 
 module.exports = problem4;
