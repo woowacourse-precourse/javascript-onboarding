285aebb5f63734e966b8b62b3b0ccd13b8f5466e
jiyun1006 <jiyun4251@gmail.com>
2022-11-01 20:24:59 +0900

    🚑  디버깅위해 만들어 두었던 객체 삭제.

--- src/problem6.js
+++ src/problem6.js
@@ -62,7 +62,6 @@ const duplCheck = (word, formArr, formIdx, formLen, checkArr) => {
   return checkArr;
 }
 
-const forms = [["jm@email.com", "제이엠"], ["jason@email.com", "제이슨"], ["woniee@email.com", "워니"], ["mj@email.com", "엠제이"], ["nowm@email.com", "이제엠"]];
 problem6(forms);
 
 
