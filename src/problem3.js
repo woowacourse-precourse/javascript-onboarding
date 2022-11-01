d257d05b636a09258fa407ccc894e409302fe47a
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-01 23:48:49 +0900

    우아한테크코스 문제3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (var i = 1; i <= number; i++) {
+    var tmp = i;
+    while(tmp) {
+      if (Math.floor(tmp % 10) === 3 || Math.floor(tmp % 10) === 6 || Math.floor(tmp % 10) === 9) {
+        answer++;
+      }if (10 <= i <= number) {
+        tmp = Math.floor(tmp / 10);
+      }
+    }
+  }  
   return answer;
 }
 
