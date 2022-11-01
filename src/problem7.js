5adc3dcdb9ed28e7cef2f4f4adc1edc7635fc4c1
Ssoco <dlscjs8646@gmail.com>
2022-11-01 23:12:28 +0900

    p7: 기본 배열들과 Map, 친구 목록과 친구 아닌 목록 할당

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,10 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  const answer = [];
+  const scores = new Map();
+
+  // user의 친구 목록
+  const userFriends = [];
+  const others = [];
   return answer;
 }
 
