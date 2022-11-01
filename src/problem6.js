fff29cd3c07b151050085c1638c31870dd2f63cb
Inae Choi <inaechoi89@gmail.com>
2022-11-01 14:11:15 +0900

    refactor(p6): convert for statement to function

--- src/problem6.js
+++ src/problem6.js
@@ -1,29 +1,45 @@
 function problem6(forms) {
   var answer = [];
-  let sameNickIdx = new Set();
-  let mailList = new Set();
 
-  for (let i = 0; i < forms.length; i++) {
-    let twoChar = new Set();
-    let name = forms[i][1];
-    for (let j = 0; j < name.length - 1; j++) {
-      twoChar.add(name.substr(j, j + 2));
-    }
-    for (let j = i + 1; j < forms.length; j++) {
-      for (let item of twoChar.values()) {
-        if (forms[j][1].includes(item)) {
-          sameNickIdx.add(i);
-          sameNickIdx.add(j);
+  getSameNickIndex(forms);
+
+  function getSameNickIndex(forms) {
+    let sameNickIdx = new Set();
+    for (let i = 0; i < forms.length; i++) {
+      let twoChar = new Set();
+      let name = forms[i][1];
+      for (let j = 0; j < name.length - 1; j++) {
+        twoChar.add(name.substr(j, j + 2));
+      }
+      for (let j = i + 1; j < forms.length; j++) {
+        for (let item of twoChar.values()) {
+          if (forms[j][1].includes(item)) {
+            sameNickIdx.add(i);
+            sameNickIdx.add(j);
+          }
         }
       }
     }
+    getMailIndexList(sameNickIdx);
   }
 
-  for (let idx of sameNickIdx) {
-    mailList.add(forms[idx][0]);
+  function getMailIndexList(sameNickIdx) {
+    let mailList = new Set();
+    for (let idx of sameNickIdx) {
+      mailList.add(forms[idx][0]);
+    }
+    getMailAddressList(mailList);
   }
-  for (let mail of mailList) {
-    answer.push(mail);
+
+  function getMailAddressList(mailList) {
+    for (let mail of mailList) {
+      answer.push(mail);
+    }
+    sortMailList(answer);
+  }
+
+  function sortMailList(list) {
+    list.sort((a, b) => compareMail(a, b));
   }
 
   function compareMail(a, b) {
@@ -31,8 +47,6 @@ function problem6(forms) {
     let nickB = b.split("@")[0];
     return nickA.localeCompare(nickB);
   }
-
-  answer.sort((a, b) => compareMail(a, b));
 
   return answer;
 }
