c1de5d2c3772e5cf4cdb6afcfb8bfe057aa42e58
hvvany <kjunhwan1998@gmail.com>
2022-11-01 21:17:28 +0900

    feat(build): make friend suggest dictionary

--- src/problem7.js
+++ src/problem7.js
@@ -15,8 +15,18 @@ function problem7(user, friends, visitors) {
       }
     }
   }
+  for (let user_friend of friend_dic[user]) {
+    new_friends = friend_dic[user_friend]
+    for (let new_friend of new_friends) {
+      if (new_friend in friend_suggest) {
+        friend_suggest[new_friend] += 10
+      }
+      else {
+        friend_suggest[new_friend] = 10
+      }
+    }
+  }
 
-  
   return answer;
 }
 
