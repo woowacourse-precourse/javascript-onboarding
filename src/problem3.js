6ff6a99abd8c543b9d21f392d2b16c3137d3373b
EunjE <dldmswp3@naver.com>
2022-11-01 21:42:52 +0900

    solve p3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,27 @@
+var dp = [];
+dp.push(0);
+
+function clap(x){
+  var count = 0;
+  while (Math.floor(x / 10) > 0){
+    if((x % 10) % 3 === 0 &&  (x % 10) !== 0) count++; 
+    x = Math.floor(x / 10);
+  }
+  if ((x % 10) % 3 === 0) count++;
+  dp.push(count + dp[dp.length - 1]);
+  return;
+}
+
+
+
 function problem3(number) {
   var answer;
-  return answer;
+  
+  for(var i = 1; i <= number; i++){
+    clap(i);
+  }
+  
+  return dp[number];
 }
 
 module.exports = problem3;
