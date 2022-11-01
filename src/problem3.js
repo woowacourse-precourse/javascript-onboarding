a384a6a36e84ec580d3fe02cb06283ef2120d7f4
zktm9903 <zktm9903@naver.com>
2022-11-01 12:47:04 +0900

    problem3 주석추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,8 @@
 function problem3(number) {
   let count = 0;
 
+  // 1 ~ number 모든 수 검사
   for(let i=1;i<=number;i++){
-
     //문자열로 변환후 각 자리 수가 3, 6, 9인지 검사 
     (i+'').split('').map(digit=>{
       if(digit === '3' || digit === '6' || digit === '9')
