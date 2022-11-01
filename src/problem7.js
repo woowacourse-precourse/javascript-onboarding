b78a191d4cef685ef78b57c18fddac39ef064e5e
OrangeChick <retww@naver.com>
2022-11-01 21:42:30 +0900

    feat: 🎒[Problem7] Separate friends I know with me and putting them in an other array
    - 나와 함께 아는 친구를 분리하여 배열에 담아주었습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,17 @@
 function problem7(user, friends, visitors) {
   var answer;
+
+  let friend = [];
+  let nearFriend = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][1] === user) {
+      friend.push(friends[i][0]);
+    } else {
+      nearFriend.push(friends[i][1]);
+    }
+  }
+
   return answer;
 }
 
