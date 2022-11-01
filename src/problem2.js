83589078be3558c146218d096efd54cbf6a26bf3
jincde <asdzxc@kakao.com>
2022-11-01 21:40:06 +0900

    feat : problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
+
+  var answer = cryptogram.split("");
+
+  for (let i = 0; i < answer.length; i++) {
+
+    if (answer[i] == answer[i - 1]) {
+      let temp = 1;
+      let j = i;
+
+      while (answer[j] == answer[j - 1]) {
+        temp++;
+        j++;
+
+      }
+      answer.splice(i - 1, temp);
+      i = 0;
+    }
+  }
+  answer = answer.join("");
   return answer;
 }
-
-
-
 
 module.exports = problem2;
