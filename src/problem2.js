58408e61cd157ac8074a2b0a4afbd995f3432cca
lego37yoon <lego37yoon@outlook.com>
2022-11-01 16:06:54 +0900

    Solved problem 2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let cryptoArray = Array.from(cryptogram);
+  const arrayLength = cryptoArray.length;
+
+  for (let i = 0; i < arrayLength; i++) {
+    for (let j = 0; j < cryptoArray.length - 1; j++) {
+      console.log(cryptoArray);
+      if (cryptoArray[j] == cryptoArray[j + 1]) {
+        cryptoArray.splice(j, 2);
+      }
+    }
+  }
+
+  return cryptoArray.join('');
 }
 
 module.exports = problem2;
