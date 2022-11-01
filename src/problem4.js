c6db27657df6f607000cc9b847cd50e8701cc45a
KPGY <yuchw8@korea.ac.kr>
2022-11-01 22:07:38 +0900

    4번문제 아스키코드를 이용해 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,22 +1,22 @@
 function problem4(word) {
-let ascii="";
-let answer = "";
-for(let i=0; i<word.length; i++)//문자열 길이만큼 순회
-{
-  ascii = word.charCodeAt(i); //문자열 값을 아스키코드로 치환
-  if(65<=ascii && ascii<=90) //소문자일 경우
+  let ascii="";
+  let answer = "";
+  for(let i=0; i<word.length; i++)//문자열 길이만큼 순회
   {
-    ascii = 155-ascii
+    ascii = word.charCodeAt(i); //문자열 값을 아스키코드로 치환
+    if(65<=ascii && ascii<=90) //소문자일 경우
+    {
+      ascii = 155-ascii
+    }
+    else if(97<=ascii && ascii<=122) //대문자일 경우
+    {
+      ascii = 219-ascii
+    }
+    answer += String.fromCharCode(ascii); //다시 문자열로 변환후 정답으로 입력
   }
-  else if(97<=ascii && ascii<=122) //대문자일 경우
-  {
-    ascii = 219-ascii
+    return answer;
   }
-  answer += String.fromCharCode(ascii); //다시 문자열로 변환후 정답으로 입력
-}
-  return answer;
-}
-
-
-module.exports = problem4;
+  
+  
+  module.exports = problem4;
 
