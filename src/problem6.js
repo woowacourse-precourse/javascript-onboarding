1257460e0c7c7023854b726dff9ca27fdecb2f78
cham-min <hymn9613@gmail.com>
2022-11-01 15:47:48 +0900

    feat: 문제6 - seperateConsecutive 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -4,7 +4,13 @@ function problem6(forms) {
   let consecutiveArr = [];
 }
 
-function seperateConsecutive(nickname, uniqueArr, consecutiveArr) {}
+function seperateConsecutive(nickname, uniqueArr, consecutiveArr) {
+  for (let i = 0; i < nickname.length - 1; i++) {
+    let word = nickname.slice(i, i + 2);
+    if (uniqueArr.indexOf(word) === -1) uniqueArr.push(word);
+    else if (consecutiveArr.indexOf(word) === -1) consecutiveArr.push(word);
+  }
+}
 
 function isValid(nickname, consecutiveArr) {}
 
