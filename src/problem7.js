7e7ded31c52c420e39e8d2dc727f47ef35f95fd5
kknyapple <kknyapple@gmail.com>
2022-11-01 23:34:54 +0900

    problem7 - 친구목록 만들기

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,21 @@
+function makeFriendList(friends) {
+  let friendList = {};
+
+  friends.forEach((friend, index) => {
+    const [id_A, id_B] = friend;
+
+    friendList[id_A]
+      ? friendList[id_A].push(id_B)
+      : (friendList[id_A] = [id_B]);
+
+    friendList[id_B]
+      ? friendList[id_B].push(id_A)
+      : (friendList[id_B] = [id_A]);
+  });
+
+  return friendList;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
