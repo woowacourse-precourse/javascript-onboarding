95a50b15dc24d28a7683049aa192881c4c6e02ee
pakxe <pigkill40@naver.com>
2022-11-01 16:56:56 +0900

    refactor: [P7] 친구관계를 obj 자료형으로 바꿨으므로 .get메서드에서 [key]로 수정

--- src/problem7.js
+++ src/problem7.js
@@ -2,51 +2,45 @@ const LENGTH_RECOMMEND_FRIEND = 5;
 
 function problem7(user, friends, visitors) {
   const relationList = makeFriendsList(friends);
-  // const usersFriends = getUsersFriends(user, relationList);
-  // const friendOfFriendList = getFriendOfFriend({
-  //   user,
-  //   relationList,
-  //   usersFriends,
-  // });
-  // const scoreList = scoreFriends(friendOfFriendList);
-  // scoreVisitors(scoreList, visitors, usersFriends);
-
-  // return recommendFriends(scoreList, LENGTH_RECOMMEND_FRIEND);
-}
-// 추천 친구의 이름만 반환하는 함수
-function recommendFriends(scoreList, length) {
-  let ArrScoreList = [...scoreList]; // 배열로 만들기
-  ArrScoreList.sort(ascendingSort);
-
-  let NameList = ArrScoreList.map((score) => score[0]);
-  return NameList.splice(0, length);
-}
-
-function getUsersFriends(user, relationList) {
-  if (!relationList.has(user)) return [];
-  else return [...relationList.get(user)];
-}
-// 주어진 조건대로 정렬하는 함수
-function ascendingSort(a, b) {
-  [name1, score1] = a;
-  [name2, score2] = b;
-  if (score1 > score2) return -1;
-  if (score1 < score2) return 1;
-  if (score1 === score2) {
-    if (name1 > name2) return 1;
-    if (name1 <= name2) return -1;
-  }
-}
-
-function scoreVisitors(scoreList, visitors, usersFriends) {
-  visitors.forEach((visitor) => {
-    if (scoreList.has(visitor)) {
-      const score = scoreList.get(visitor);
-      scoreList.set(visitor, score + 1);
-    } else if (!usersFriends.includes(visitor)) {
-      scoreList.set(visitor, 1);
-    }
+  const usersFriends = relationList[user] || [];
+  const friendOfFriendList = getFriendOfFriend({
+    user,
+    relationList,
+    usersFriends,
   });
+  const scoreList = scoreFriends(friendOfFriendList);
+  scoreVisitors(scoreList, visitors, usersFriends);
+
+  return recommendFriends(scoreList, LENGTH_RECOMMEND_FRIEND);
+}
+
+function makeFriendsList(friends) {
+  const friendsList = {};
+
+  friends.forEach(([leftName, rightName]) => {
+    if (friendsList[leftName]) friendsList[leftName].push(rightName);
+    else friendsList[leftName] = [rightName];
+    if (friendsList[rightName]) friendsList[rightName].push(leftName);
+    else friendsList[rightName] = [leftName];
+  });
+
+  return friendsList;
+}
+
+function getFriendOfFriend({ user, relationList, usersFriends }) {
+  const friendsOfFriends = [];
+
+  for (let i = 0; i < usersFriends.length; i++) {
+    const friendsSet = relationList[usersFriends[i]];
+
+    friendsSet.forEach((friendOfFriend) => {
+      if (friendOfFriend !== user && !usersFriends.includes(friendOfFriend)) {
+        friendsOfFriends.push(friendOfFriend);
+      }
+    });
+  }
+
+  return friendsOfFriends;
 }
 
 function scoreFriends(friendOfFriendList) {
@@ -63,33 +57,34 @@ function scoreFriends(friendOfFriendList) {
   return scoreMap;
 }
 
-function makeFriendsList(friends) {
-  const friendsList = {};
-
-  friends.forEach(([leftName, rightName]) => {
-    if (friendsList[leftName]) friendsList[leftName].push(rightName);
-    else friendsList[leftName] = [rightName];
-    if (friendsList[rightName]) friendsList[rightName].push(leftName);
-    else friendsList[rightName] = [leftName];
+function scoreVisitors(scoreList, visitors, usersFriends) {
+  visitors.forEach((visitor) => {
+    if (scoreList.has(visitor)) {
+      const score = scoreList.get(visitor);
+      scoreList.set(visitor, score + 1);
+    } else if (!usersFriends.includes(visitor)) {
+      scoreList.set(visitor, 1);
+    }
   });
-  return friendsList;
 }
 
-// 친구의 친구가 있는 경우 배열을 반환, 없는 경우 빈 배열을 반환
-function getFriendOfFriend({ user, relationList, usersFriends }) {
-  const friendsOfFriends = [];
+function recommendFriends(scoreList, length) {
+  let ArrScoreList = [...scoreList]; // 배열로 만들기
+  ArrScoreList.sort(ascendingSort);
 
-  for (let i = 0; i < usersFriends.length; i++) {
-    const friendsSet = relationList.get(usersFriends[i]);
+  let NameList = ArrScoreList.map((score) => score[0]);
+  return NameList.splice(0, length);
+}
 
-    friendsSet.forEach((friendOfFriend) => {
-      if (friendOfFriend !== user && !usersFriends.includes(friendOfFriend)) {
-        friendsOfFriends.push(friendOfFriend);
-      }
-    });
+function ascendingSort(a, b) {
+  [name1, score1] = a;
+  [name2, score2] = b;
+  if (score1 > score2) return -1;
+  if (score1 < score2) return 1;
+  if (score1 === score2) {
+    if (name1 > name2) return 1;
+    if (name1 <= name2) return -1;
   }
-
-  return friendsOfFriends;
 }
 
 module.exports = problem7;
