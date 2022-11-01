f0f128158d7816c45dde0f73164e4396046fac74
banma70 <banma1234@gmail.com>
2022-11-01 13:58:50 +0900

    problem_4 : 예외처리 함수

--- src/problem4.js
+++ src/problem4.js
@@ -3,4 +3,14 @@ function problem4(word) {
   return answer;
 }
 
+function errorHandler(word) {
+  // 문자 쪼개기
+  var arr = (word + '').split('');
+  // 자리수가 1000을 넘을경우 1001번째 자리부터 끝까지 소거
+  if (arr.length > 1000)  arr = arr.splice(1000, arr.length - 1);
+
+  // 알파벳 판별은 아스키코드 변환 과정에서 손볼 예정
+  return arr;
+}
+
 module.exports = problem4;
