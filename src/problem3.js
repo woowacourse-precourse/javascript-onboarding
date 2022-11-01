1a1180fd537d1bec7c7d9e9c9b104df6e2e82542
ekkim <eklikeable@kakao.com>
2022-11-01 14:50:57 +0900

    🎨 주석 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,10 @@
+/**
+ * 배달이가 좋아하는 3,6,9 게임
+ * - 3, 6, 9가 들어가는 개수만큼 손뼉을 쳐야 한다
+ * @function problem3
+ * @param {number} number 1부터 number까지 게임 진행
+ * @returns {number} 손뼉을 치는 횟수
+ *  */
 function problem3(number) {
   let numbers = [];
   for (let i = 1; i <= number; i++) {
