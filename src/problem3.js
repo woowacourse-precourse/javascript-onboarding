7b9b443a9606840c4e2fb1812d19a71493acb84d
b-hyoung <youqlrqod@naver.com>
2022-11-01 23:42:40 +0900

    fix problem3

--- src/problem3.js
+++ src/problem3.js
@@ -3,16 +3,19 @@ function problem3(number) {
 
   for (let i = 1; i <= number; i++) {
     let x = i % 10;
-    let y = i / 10;
+    let ab = i / 10;
+    let y = Math.floor(ab)
     if (((x === 3 || x === 6 || x === 9)) || ((y === 3 || y === 6 || y === 9))) {
       answer++
     }
-    if (i % 10 == 0) {
+    if (((x === 3 || x === 6 || x === 9)) && ((y === 3 || y === 6 || y === 9))) {
       answer++
     }
+   
   }
   return answer;
 }
+
 
 
 module.exports = problem3;
