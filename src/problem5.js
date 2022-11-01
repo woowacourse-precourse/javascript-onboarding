05993ce28e8823881ce0b8c77d53cb80c1d5bf8d
muni <ansgml6491@naver.com>
2022-11-01 20:57:10 +0900

    [Add] Problem 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
-  return answer;
+	let answer;
+	let balane = money;
+	const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+	answer = moneyType.map((item) => {
+		const share = parseInt(balane / item);
+		balane = balane % item;
+		return share;
+	});
+
+	return answer;
 }
 
 module.exports = problem5;
