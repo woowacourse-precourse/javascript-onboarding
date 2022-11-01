8e65a99004ff812196867e28e7bb3c817cd962be
emma <jhj191058@gmail.com>
2022-11-01 19:38:51 +0900

    problem01 수정 후 재 업데이트

--- src/problem1.js
+++ src/problem1.js
@@ -1,17 +1,14 @@
 function problem1(pobi, crong) {
-
   var answer;
-    
-  
-  /**예외사항 */
-  if(1 != Math.floor(pobi[1]-pobi[0])) {
+
+  /**예외 사항 */
+  if( 1 != Math.floor(pobi[1] - pobi[0])) {
     return answer = -1;
-  }else if (1 != Math.floor(crong[1] - crong[0])) {
+  }else if ( 1 != Math.floor(crong[1] - crong [0])) {
     return answer = -1;
   }
 
   /**pobi 점수 구하기 */
-
   /**왼쪽 Page */
 
   var sumLeftP = 0;
@@ -19,7 +16,7 @@ function problem1(pobi, crong) {
   var sumRightP = 0;
   var multiRightP = 1;
   do {
-    sumLeftP += Math.floor(pobi[0]%10);
+    sumLeftP += Math.floor(pobi[0]&10);
     multiLeftP *= Math.floor(pobi[0]%10);
     pobi[0] = Math.floor(pobi[0]/10);
   }while(pobi[0] > 0);
@@ -30,16 +27,14 @@ function problem1(pobi, crong) {
     sumRightP += Math.floor(pobi[1]%10);
     multiRightP *= Math.floor(pobi[1]%10);
     pobi[1] = Math.floor(pobi[1]/10);
-  }while(pobi[1]>0);
+  }while(pobi[1] > 0);
 
   /**pobi 점수 구하기 */
   var scoreLeftPobi = ( sumLeftP > multiLeftP ) ? sumLeftP : multiLeftP;
   var scoreRightPobi = (sumRightP > multiRightP ) ? multiRightP : multiRightP;
   var fScorePobi = (scoreLeftPobi > scoreRightPobi ) ? scoreLeftPobi : scoreRightPobi;
-  
 
-  /** crong 점수 구하기  */
-
+  /** crong 점수 구하기 */
   /**왼쪽 Page */
 
   var sumLeftC;
@@ -59,16 +54,13 @@ function problem1(pobi, crong) {
     multiRightC *= Math.floor(crong[1]%10);
     crong[1] = Math.floor(crong[1]/10);
   }while(crong[1] > 0);
-  
 
-  /**crong 점수 구하기 */
+  /**crong 점수 구하기  */
   var scoreLeftCrong = (sumLeftC > multiLeftC ) ? sumLeftC : multiLeftC;
   var scoreRightCrong = (sumRightC > multiRightC ) ? multiRightC : multiRightC;
   var fScoreCrong = (scoreLeftCrong > scoreRightCrong ) ? scoreLeftCrong : scoreRightCrong;
 
-
-
-  /** pobi와 crong의 승부 펼치기*/
+  /**pobi와 crong의 승부 펼치기 */
   if(fScorePobi>fScoreCrong) {
     return answer = 1;
   } else if(fScorePobi < fScoreCrong) { 
@@ -76,9 +68,8 @@ function problem1(pobi, crong) {
   } else if (fScorePobi == fScoreCrong) {
     return answer = 0;
   } 
-  
-    
+
 }
-  
+
 
 module.exports = problem1;
