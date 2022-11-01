b901d6d52ddfc7fe4b44f320605a3e08669873fd
xownsdl0 <xownsdl0@naver.com>
2022-11-01 11:02:38 +0900

    refactor(problem1): 계산 로직 함수 분리 및 early return 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,77 +1,51 @@
 function problem1(pobi, crong) {
   var answer;
+  let pobiMax = [];
+  var crongMax = [];
 
-  var pobiLeftSum = 0;
-  var pobiLeftMultple = 1;
-  var pobiRightSum = 0;
-  var pobiRightMultple = 1;
-  var pobiMax = 0;
-
-  var crongLeftSum = 0;
-  var crongLeftMultple = 1;
-  var crongRightSum = 0;
-  var crongRightMultple = 1;
-  var crongMax = 0;
-
-  //예외처리
-  if (pobi[0] - pobi[1] === -1 && crong[0] - crong[1] === -1) {
-    //left
-    var pobiLeftNum = pobi[0];
-    pobiLeftNum = String(pobiLeftNum);
-    for (let i = 0; i < pobiLeftNum.length; i++) {
-      pobiLeftSum += parseInt(pobiLeftNum[i]);
-      pobiLeftMultple *= parseInt(pobiLeftNum[i]);
-    }
-
-    var crongLeftNum = crong[0];
-    crongLeftNum = String(crongLeftNum);
-    for (let i = 0; i < crongLeftNum.length; i++) {
-      crongLeftSum += parseInt(crongLeftNum[i]);
-      crongLeftMultple *= parseInt(crongLeftNum[i]);
-    }
-
-    //right
-    var pobiRightNum = pobi[1];
-    pobiRightNum = String(pobiRightNum);
-    for (let i = 0; i < pobiRightNum.length; i++) {
-      pobiRightSum += parseInt(pobiRightNum[i]);
-      pobiRightMultple *= parseInt(pobiRightNum[i]);
-    }
-
-    var crongRightNum = crong[1];
-    crongRightNum = String(crongRightNum);
-    for (let i = 0; i < crongRightNum.length; i++) {
-      crongRightSum += parseInt(crongRightNum[i]);
-      crongRightMultple *= parseInt(crongRightNum[i]);
-    }
-
-    var pobiValue = [
-      pobiLeftSum,
-      pobiLeftMultple,
-      pobiRightSum,
-      pobiRightMultple,
-    ];
-    var crongValue = [
-      crongLeftSum,
-      crongLeftMultple,
-      crongRightSum,
-      crongRightMultple,
-    ];
-
-    pobiMax = Math.max(...pobiValue);
-    crongMax = Math.max(...crongValue);
-
-    if (pobiMax > crongMax) {
-      answer = 1;
-    } else if (pobiMax < crongMax) {
-      answer = 2;
-    } else if (pobiMax === crongMax) {
-      answer = 0;
-    }
-  } else {
+  //예외처리 : early return
+  if (pobi[0] - pobi[1] !== -1 || crong[0] - crong[1] !== -1) {
     answer = -1;
+    return answer;
   }
+
+  // 포비와 크롱의 왼쪽, 오른쪽 페이지 합과 곱 중 가장 큰 수 구하기
+  pobi.map((num) => {
+    pobiMax.push(sum(num));
+    pobiMax.push(multiple(num));
+  });
+
+  crong.map((num) => {
+    crongMax.push(sum(num));
+    crongMax.push(multiple(num));
+  });
+
+  // 포비와 크롱의 가장 큰 수 비교 후 승자 선택
+  // if (pobiMax > crongMax) {
+  //   answer = 1;
+  // } else if (pobiMax < crongMax) {
+  //   answer = 2;
+  // } else if (pobiMax === crongMax) {
+  //   answer = 0;
+  // }
+
   return answer;
 }
+
+const sum = (num) => {
+  return String(num)
+    .split("")
+    .reduce((sum, n) => {
+      return sum + parseInt(n);
+    }, 0);
+};
+
+const multiple = (num) => {
+  return String(num)
+    .split("")
+    .reduce((mul, n) => {
+      return mul * parseInt(n);
+    }, 1);
+};
 
 module.exports = problem1;
