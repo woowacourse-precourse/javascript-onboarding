1c28c09a6f9f9d9c73679737da829f5d6da34569
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 10:28:38 +0900

    problem2 implement solution

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,22 @@
-function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
-
 /*필요한 기능
 for loop으로 각 char랑 그 다음 char 비교
 연속된 중복 문자들 삭제
 결과물 리턴
 */
+
+function problem2(cryptogram) {
+  var answer;
+  return answer;
+}
+
+function solution() {
+  for (i=0; i<1000; i++) {
+    if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
+      cryptogram.replace(charAt(i), '');
+      cryptogram.replace(charAt(i+1), '');
+    }
+  }
+  return cryptogram;
+}
 
 module.exports = problem2;
