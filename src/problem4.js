f03c9f066298cc6b9bf3a6f398ed1aeb62c31ee0
kimeodml <kimeodml@gmail.com>
2022-11-01 15:11:15 +0900

    feat: 문제4 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,16 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let ans ="";
+  for(let i = 0; i < word.length ; i++){
+    if(word[i] === ' '){
+      ans += ' ';
+      continue;
+    }else if(word[i].charCodeAt(0)<=90){
+      ans+=String.fromCharCode(155-word[i].charCodeAt(0));
+    }else {
+      ans+=String.fromCharCode(219-word[i].charCodeAt(0));
+    }
+  }
+  return ans;
 }
 
 module.exports = problem4;
