c746101ac1426bafac53b4cfd750cb7630be6ead
leeharyung <lhr4884@naver.com>
2022-11-01 13:11:26 +0900

    add problem2 solution

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,21 @@
+function solution(text) {
+  const stack = [];
+  let temp = "";
+  for (let i = 0; i < text.length; i++) {
+    if (stack.length === 0 && temp !== text[i]) stack.push(text[i]);
+    else if (stack[stack.length - 1] === text[i]) {
+      stack.pop();
+      temp = text[i];
+    } else if (temp !== text[i]) {
+      stack.push(text[i]);
+      temp = "";
+    }
+  }
+  return stack.join("");
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  return solution(cryptogram);
 }
 
 module.exports = problem2;
