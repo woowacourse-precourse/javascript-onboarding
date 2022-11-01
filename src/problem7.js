2a6925de6846e6f76de51d7141c7c3f010dfe916
osdoonhyun <skw1996@naver.com>
2022-11-01 17:15:17 +0900

    [문제7] feat: 기능1-1 user의 친구 찾는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,24 @@
+// 기능 1-1
+function findFriends(user, friends) {
+  let friendsArr = [];
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (user === friends[i][j]) {
+        friendsArr.push(friends[i]);
+      }
+    }
+  }
+  return friendsArr;
+}
+
+function problem7(user, friends, visitors) {
+  var answer;
+  friendsArr = findFriends(user, friends);
+  return answer;
+}
+
+module.exports = problem7;
+
 /* 기능 목록
 1. friends에서 user의 친구를 찾고 찾은 user 친구의 친구를 찾아 점수를 부여하는 기능
   1-1. friends에서 user가 들어간 요소를 찾고 user의 친구를 새로운 배열에 할당한다.
@@ -5,9 +26,3 @@
 2. visitors에서 사용자 아이디와 점수를 부여한 배열을 만드는 기능
 3. 점수가 높은 순으로 정렬, 추천 점수가 같다면 이름순으로 정렬하는 기능
 */
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
-
-module.exports = problem7;
