9b4d821fc34ff67232f5146b5a99218561354d7b
doka <posmane5@gmail.com>
2022-11-01 22:53:33 +0900

    7

--- src/problem7.js
+++ src/problem7.js
@@ -1,13 +1,91 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+  var answer = []
+  var myfriendSet = new Set();
+  var notMyFriend = new Set();
+  var allFriend = new Set();
+  var pointDic = {}
 
+  friends.forEach(friend=> {
+    if (friend[0] == user) {
+      myfriendSet.add(friend[1])
+    } else if (friend[1] == user) {
+      myfriendSet.add(friend[0])
+    }
+    allFriend.add(friend[0]).add(friend[1])
+  })
+  allFriend.delete(user)
+  var notMyFriend = new Set([...allFriend].filter(x => !myfriendSet.has(x)))
+  // console.log(notMyFriend)
+
+  friends.forEach(friend=> {
+    if (myfriendSet.has(friend[0])) {
+      if (notMyFriend.has(friend[1])) {
+        if (pointDic[friend[1]] === undefined) {
+          pointDic[friend[1]] = 10
+        }else {
+          pointDic[friend[1]] += 10
+        }
+
+      }
+    } else if (notMyFriend.has(friend[0])) {
+
+      if (myfriendSet.has(friend[1])) {
+        if (pointDic[friend[1]] === undefined) {
+          pointDic[friend[1]] = 10
+        }else {
+          pointDic[friend[1]] += 10
+        }
+
+    }
+
+  }})
+
+  visitors.forEach(friend=> {
+    if (myfriendSet.has(friend)) {
+      return
+    }
+    if (pointDic[friend] === undefined) {
+      pointDic[friend] = 1
+    }else {
+      pointDic[friend] += 1
+    }
+  })
+
+  var sortable = Object.entries(pointDic)
+    .sort(function(v1,v2){
+      if (v1[1] > v2[1]){
+          return -1;
+      }
+      if (v1[1] < v2[1]){
+          return 1;
+      }
+      if (v1[1] == v2[1]){
+          if (v1[0] > v2[0]){
+              return 1;
+          }
+          if (v1[0] < v2[0]){
+              return -1;
+          }
+      }
+      
+      return 0;
+  });
+  for (let i = 0; i < Math.min(sortable.length, 5); i++){
+    answer.push(sortable[i][0])
+  } 
+  // console.log(answer)
+
+
+  return answer;
+
+}
+problem7("mrko",[ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ],["bedi", "bedi", "donut", "bedi", "shakevan"])
 module.exports = problem7;
 
 // 처음 돌면서 모든사람, 내친구set 만들기 (user 인가? 맞으면 다음인덱스 혹은 이전 인덱스를 친구set에 추가)
 // 두번쨰로 돌면서 내친구X set만들기
 // 친구 : 친구들 오브젝트 만들고 set은 포함여부가 o1임
+
 // a[0] 내친구인가? 맞다면 a[1]가 내친구가 아닌가? 그러면 a[1]에게 10점추가 그외면 스킵 
 // a[0] 이 내친구 아니 라면 a[1] 이 내친구인가 보고 맞으면 a[0]에게 10점추가 그외면 스킵 (딕셔너리 생성후 밸류값 증가시키기)
 // visitors 를 돌면서 추가로 딕셔너리에있는 이름 키에 1점씩 추가
