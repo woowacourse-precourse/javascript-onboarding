0260804399416aa6fc882de6a74fc568554895e9
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-01 17:43:08 +0900

    feat : findAlreadyFriends 함수 구현
    - user와 이미 친구인 관계인 사람들을 alreadyFriendsArr에 저장후 반환

--- src/problem7.js
+++ src/problem7.js
@@ -5,5 +5,12 @@ function makeAllFriendsArr(friends) {
   let friendsArr = friends.reduce((acc, cur) => [...acc, ...cur]);
   return Array.from(new Set(friendsArr));
 }
-
+function findAlreadyFriends(user, friends) {
+  let alreadyFriendsArr = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (user === friends[i][0]) alreadyFriendsArr.push(friends[i][1]);
+    if (user === friends[i][1]) alreadyFriendsArr.push(friends[i][0]);
+  }
+  return alreadyFriendsArr;
+}
 module.exports = problem7;
