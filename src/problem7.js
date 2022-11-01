2924aa52c71dd384f8d01e7786e219bb6fdf1555
RosaDamascena <hsj990604@gmail.com>
2022-11-01 23:12:05 +0900

    solving problem7

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,22 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let answer = [];
+  let friendSet = [];
+  let linkedSet = [];
+
+  for (let i = 0; i < friends.length; i++) {
+    if (!friends[i].includes(user)) {
+      for (let j=0; j<2; j++) {
+          score.set(friends[i][j], 10);
+      }
+  }
+  else {
+      for (let j=0; j<2; j++) {
+          score.set(friends[i][j], 0);
+      }
+    }
+  }
+
+  
 }
 
 module.exports = problem7;
