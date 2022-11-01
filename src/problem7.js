41534f2ab9045d88e2c074a42cf4cf74d9d62d72
0dytpq0 <rkfnahs12e@gmail.com>
2022-11-01 22:07:12 +0900

    problem7 구현 실패..

--- src/problem7.js
+++ src/problem7.js
@@ -1,57 +1,77 @@
-
 function problem7(user, friends, visitors) {
-  let arr = [];
-  let newFriends = [];
   let ogFriends = [];
+  let newFriends = [];
   let storage = [];
   let sum = 1;
+
+  let visitorsnewFriends = [];
+  let storageForOrder = [];
 
 
   //1.기존 친구들은 누구인가?(친구들의 친구를 구하기 위한 장치)
   for (let i = 0; i < friends.length; i++) {
     if (friends[i][1] === user || friends[i][0] === user) {
-      arr.push(friends[i]);
-    }
-  }
-
-
-  //2.기존 친구의 친구들 배열 생성
-  for (let j = 0; j < arr.length; j++) {
-    for (let k = 0; k < friends.length; k++) {
-      if (arr[j][0] === friends[k][0] && arr[j][1] !== friends[k][1]) {
-        newFriends.push(friends[k][1]);
-        newFriends.sort();
+      if (friends[i][0] === user) {
+        ogFriends.push(friends[i][1]);
+      }
+      else {
+        ogFriends.push(friends[i][0]);
       }
     }
   }
+
+
+
+  //2.기존 친구의 친구들 배열 생성
+  for (let j = 0; j < ogFriends.length; j++) {
+    for (let k = 0; k < friends.length; k++) {
+      if (friends[k][0] !== user && friends[k][1] !== user) {
+        if (friends[k][1] === ogFriends[j] || friends[k][0] === ogFriends[j]) {
+          if (friends[k][0] === ogFriends[j]) {
+            newFriends.push(friends[k][1]);
+          }
+          else {
+            newFriends.push(friends[k][0]);
+          }
+        }
+      }
+    }
+  }
+  newFriends.sort();
+
+  //5. 방문자중 기존 친구가 아닌 이들 간추림.
+  for (let c of visitors) {
+    if (ogFriends.indexOf(c) === -1) {
+      if (newFriends.indexOf(c) === -1) {
+        visitorsnewFriends.push(c);
+      }
+
+    }
+  }
+  visitorsnewFriends.sort();
+
+
   //3.추천 친구
   for (let a = 0; a < newFriends.length; a++) {
     if (newFriends[a] === newFriends[a + 1]) {
       sum += 1
     } else {
+      if (visitors.indexOf(newFriends[a]) >= 0) {
+        let count = newFriends.filter(elem => newFriends[a] === elem).length;
+        storage.push(`${(sum * 10) + count} : ${newFriends[a]}`);
+        storageForOrder.push([(sum * 10) + count, newFriends[a]]);
+        sum = 1;
+        continue;
+      }
       storage.push(`${sum * 10} : ${newFriends[a]}`);
+      storageForOrder.push([(sum * 10), newFriends[a]]);
       sum = 1;
     }
 
   }
 
-  //4.기존친구들 이름배열 (기존 친구가 아닌 방문자를 구하기 위한 장치)
-  for (let q = 0; q < arr.length; q++) {
-    ogFriends.push(arr[q][0]);
-  }
 
-
-  //5. 방문자중 기존 친구가 아닌 이들 간추림.
-  let visitorsnewFriends = [];
-  for (let c of visitors) {
-    if (ogFriends.indexOf(c) === -1) {
-      visitorsnewFriends.push(c);
-    }
-  }
-  visitorsnewFriends.sort();
-  console.log(visitorsnewFriends);
-
-  //6. 방문자들을 배열에 추가
+  //4. 방문자들을 배열에 추가
   if (visitorsnewFriends[0] === visitorsnewFriends[visitorsnewFriends.length - 1]) {
     sum = visitorsnewFriends.length;
     if (sum < 10) {
@@ -74,13 +94,17 @@ function problem7(user, friends, visitors) {
       }
     }
   }
-
-  //7. 점수를 기준으로 정리해논 추천인들을 이름만 간추림.
+  storageForOrder.sort().reverse();
   storage.sort().reverse();
-  const answer = storage.map((elem) => {
+
+  //5. 점수를 기준으로 정리해논 추천인들을 이름만 간추림.
+
+  let answer = storage.map((elem) => {
     return elem.slice(5);
   })
-  //8. 점수가 같을경우 순서 바꿔주기.
+  answer = answer.slice(0, 5);
+
+  //6. 점수가 같을경우 순서 바꿔주기.
   for (let r = 0; r < storage.length - 1; r++) {
     for (let t = 0; t < storage.length; t++) {
       if (storage[r].slice(0, 2) === storage[r + 1].slice(0, 2)) {
@@ -90,8 +114,8 @@ function problem7(user, friends, visitors) {
         }
       }
     }
+
   }
   return answer;
 }
-
 module.exports = problem7;
