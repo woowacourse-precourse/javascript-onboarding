f192faa655a605e7549e8b6d239dbe8a81b8fb11
SorinKwon <ksl020700@gmail.com>
2022-11-01 20:42:21 +0900

    refactor(problem1): 변수명 수정, 함수명 수정, 코드 가독성 개선

--- src/problem1.js
+++ src/problem1.js
@@ -1,57 +1,50 @@
-function changeNumberToString(n){
-  n=n.toString();
-  return n;
+function changeNumberToString(number){
+  str=number.toString();
+  return str;
 }
 
-function plusPageNumber(n){
-  let result=0;
-  n = changeNumberToString(n);
-  for(let i=0;i<n.length;i++){
-      result+=parseInt(n[i]);
+function plusPageNumber(page_number){
+  let plus_number=0;
+  page_number = changeNumberToString(page_number);
+  for(let i=0;i<page_number.length;i++){
+    plus_number+=parseInt(page_number[i]);
   }
-  return result;
+  return plus_number;
 }
-function mutiplyPageNumber(n){
-  let result=1;
-  n = changeNumberToString(n);
-  for(let i=0;i<n.length;i++){
-      result*=parseInt(n[i]);
+function mutiplyPageNumber(page_number){
+  let multiply_number=1;
+  page_number = changeNumberToString(page_number);
+  for(let i=0;i<page_number.length;i++){
+    multiply_number*=parseInt(page_number[i]);
   }
-  return result;
+  return multiply_number;
 }
-function compareScore(a,b){
-  if(a>b){
-    return 1;
-  }
-  if(a<b){
-    return 2;
-  }
-  if(a==b){
-    return 0;
-  }
+function compareScore(pobi_max,crong_max){
+  if(pobi_max>crong_max)return 1;
+  if(pobi_max<crong_max)return 2;
+  if(pobi_max==crong_max)return 0;
 }
-function findMaxNumber(n){
-  let result;
-  for(let i=0;i<n.length;i++){
-    n[i]=Math.max(plusPageNumber(n[i]),mutiplyPageNumber(n[i]));
+function findMaxNumber(page_arr){
+  let max_number;
+  for(let i=0;i<page_arr.length;i++){
+    let plus_page = plusPageNumber(page_arr[i]);
+    let mutiply_page = mutiplyPageNumber(page_arr[i]);
+    page_arr[i]=Math.max(plus_page,mutiply_page);
   }
-  result = Math.max(...n);
-  return result;
+  max_number = Math.max(...page_arr);
+  return max_number;
 }
-function exception(n){
-  if(!(n[0]-n[1]==-1))
-    return 1;
-  
+function exception(page_arr){
+  if(!(page_arr[0]-page_arr[1]==-1))return 1;
 }
 function problem1(pobi, crong) {
-    let answer=0;
-
+    let answer;
     if((exception(pobi)||exception(crong))){
       return -1;
     }
-
-    answer=compareScore(findMaxNumber(pobi),findMaxNumber(crong));
-
+    let pobi_max_num = findMaxNumber(pobi);
+    let crong_max_num = findMaxNumber(crong);
+    answer=compareScore(pobi_max_num,crong_max_num);
     return answer;
 }
 module.exports = problem1;
