6c64b6477131bac784b9f8e0a9a0a62106178830
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 12:06:51 +0900

    feat(problem5): 금액이 큰 순서대로 개수를 변환한다

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
-  var answer;
-  return answer;
-}
 
-module.exports = problem5;
+  let moneyArr = [50000,10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let result = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+  for(i=0; i<moneyArr.length; i++) {
+
+    let count = Math.floor(money/moneyArr[i]);
+   
+    if(count >0){
+      result[i] = count;
+      money -= moneyArr[i]*count;
+    } else result[i] = 0;
+  }
+
+  return result;
+ }
+
+module.exports = problem5
