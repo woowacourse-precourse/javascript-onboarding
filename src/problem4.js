bece1f6a2b9cbfa2a0b8d03bcaac722d7393fda4
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 20:44:39 +0900

    Fix: problem4 - 테스트케이스 추가로 로직 변경

--- src/problem4.js
+++ src/problem4.js
@@ -1,16 +1,13 @@
 function problem4(word) {
-  const LowerStr = {};
-  const UpperStr = {};
+  let LowerStr = {};
+  let UpperStr = {};
+  let answer = "";
 
   for (let i = 97; i <= 122; i++) {
     LowerStr[String.fromCharCode(i)] = String.fromCharCode(219 - i);
     UpperStr[String.fromCharCode(i - 32)] = String.fromCharCode(219 - i - 32);
   }
-  checkWords(LowerStr, UpperStr, word);
-}
 
-const checkWords = (LowerStr, UpperStr, word) => {
-  let answer = "";
   for (let i = 0; i < word.length; i++) {
     if (LowerStr[word[i]]) {
       answer += LowerStr[word[i]];
@@ -19,11 +16,9 @@ const checkWords = (LowerStr, UpperStr, word) => {
     if (UpperStr[word[i]]) {
       answer += UpperStr[word[i]];
       continue;
-    }
-    answer += word[i];
+    } else answer += word[i];
   }
   return answer;
-};
+}
 
-problem4("I love you");
 module.exports = problem4;
