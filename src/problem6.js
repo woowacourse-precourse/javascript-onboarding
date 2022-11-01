29de2fcd145fefe991ec5d04d4d46ab257dc09fd
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 21:35:48 +0900

    [problem6] feat: test sample 제외 및 최종 답안 commit

--- src/problem6.js
+++ src/problem6.js
@@ -1,13 +1,13 @@
 // test sample 추가
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
+// console.log(
+//   problem6([
+//     ["jm@email.com", "제이엠"],
+//     ["jason@email.com", "제이슨"],
+//     ["woniee@email.com", "워니"],
+//     ["mj@email.com", "엠제이"],
+//     ["nowm@email.com", "이제엠"],
+//   ])
+// );
 
 function problem6(forms) {
   var answer = [];
