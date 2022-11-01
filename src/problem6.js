9490a24470e14f821ce2e86c7b200abe06fd67dd
cofla <cofla159@naver.com>
2022-11-01 20:23:19 +0900

    [p6] 콘솔 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -17,15 +17,6 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
 
 /* 
 1. forms의 루프를 돌면서
