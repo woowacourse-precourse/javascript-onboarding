964e090b6922779f632ac261a31b8be5e6b7e7d8
HyeJun <solar3070@naver.com>
2022-11-01 18:58:41 +0900

    feat: problem1/가장 큰 수 구한 뒤 본인 점수 설정

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,23 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  const pobiMax = Math.max(
+    ...pobi.map((page) =>
+      Math.max(
+        ...(page + "")
+          .split("")
+          .reduce((acc, cur) => [acc[0] + +cur, acc[1] * cur], [0, 1])
+      )
+    )
+  );
+
+  const crongMax = Math.max(
+    ...crong.map((page) =>
+      Math.max(
+        ...(page + "")
+          .split("")
+          .reduce((acc, cur) => [acc[0] + +cur, acc[1] * cur], [0, 1])
+      )
+    )
+  );
 }
 
 module.exports = problem1;
