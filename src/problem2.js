711f3c00aef6f5ef9ebd5ecd775ed191e2a1fef8
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 17:08:18 +0900

    암호를 한번 해독하는 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -4,6 +4,18 @@ function lastWord(str, idx) {
 function sameNext(str, idx) {
   return str[idx] === str[idx + 1];
 }
+function decrypt(str) {
+  let ret = "";
+  for (let i = 0; i < str.length; i++) {
+    let skip = 0;
+    while (sameNext(str, i) && !lastWord(str, i)) {
+      skip = 1;
+      i++;
+    }
+    if (!skip) ret += str[i];
+  }
+  return ret;
+}
 function problem2(cryptogram) {
   var answer;
   return answer;
