abbff419fa4cb03cef8ce4f3297c5ed9126f3215
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 17:57:18 +0900

    problem3 : 손뼉을 치는 횟수 반환 기능

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function clapCalculator(i) {
+  let clapCnt = 0;
+  for (let x of String(i)) {
+    if (x === '3' || x === '6' || x === '9') clapCnt++;
+  }
+  return clapCnt;
 }
 
+function problem3(number) {
+  let answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    let cnt = clapCalculator(i);
+    answer += cnt;
+  }
+
+  return answer;
+}
 module.exports = problem3;
