c9f9f72c3364a1519019b5388f4cbb9a86f126d9
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-01 21:08:48 +0900

    refactor(problem4): 대문자 또는 소문자를 나타내는 값을 상수로 선언

--- src/problem4.js
+++ src/problem4.js
@@ -1,24 +1,30 @@
+const Letter = Object.freeze({
+  UPPER: 1,
+  LOWER: 2,
+  NON: 0
+})
+
 function isAlpha(char) {
   const upperRegExp = /[A-Z]/;
   const lowerRegExp = /[a-z]/;
 
   if (upperRegExp.test(char)) {
-    return 1;
+    return Letter.UPPER;
   }
   if (lowerRegExp.test(char)) {
-    return 2;
+    return Letter.LOWER;
   }
-  return 0;
+  return Letter.NON;
 }
 
 function convertChar(char) {
   let newCharCode;
 
   switch (isAlpha(char)) {
-    case 1:
+    case Letter.UPPER:
       newCharCode = 'A'.charCodeAt() + 'Z'.charCodeAt() - char.charCodeAt();
       break;
-    case 2:
+    case Letter.LOWER:
       newCharCode = 'a'.charCodeAt() + 'z'.charCodeAt() - char.charCodeAt();
       break;
     default:
