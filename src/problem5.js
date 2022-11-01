a196f2394514daf3b3f2685728ac8ce28517366a
pitang <wpeffect@gmail.com>
2022-11-01 12:42:54 +0900

    5번 주석 제거 및 정리

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,15 @@
 function problem5(money) {
   let answer = [];
+  // 화폐
   const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   
   for(let i = 0; i < moneyArr.length; i++) {
+    // 나누어준 몫을 저장.
     answer[i] = Math.floor(money / moneyArr[i]);
+    // 나머지를 다시 money에 저장.
     money = (money % moneyArr[i]);
   }
   return answer;
 }
-
-// 1. 개미일꾼이랑 비슷 ! 
-// 2. 배열을 Return 해줘야함.
-// 1 : 50000, 2 : 10000, 3 : 5000, 4 : 1000, 
-// 5 : 500, 6 : 100, 7 : 50, 8 : 10, 9 : 1
 
 module.exports = problem5;
