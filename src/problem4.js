c062d3c0c3c3098e8b81c539d1d300811df59ee1
jiyun1006 <jiyun4251@gmail.com>
2022-11-01 12:50:55 +0900

    ✅  problem4 case1 해결 (feat. 대칭이되는 점을 쉽게 활용하기 위해 아스키코드를 사용.)

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,39 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answer = "";
+  const wordArr = word.trim().split(" ");
+  wordArr.forEach((el, idx) => {
+    let tmpArr = [...el];
+    let tmp = changeWord(tmpArr);
+    answer += tmp;
+  })
+  return answer.trim();
+}
+
+
+const changeWord = (wordArr) => {
+  let ansStr = "";
+  wordArr.map((el) => {
+    let asci = el.charCodeAt(0);
+    let ans = 0;
+    // 대문자는 아스키코드 65부터 90까지
+    if (asci >= 65 && asci <= 90) {
+      // 총 알파벳이 26개이므로 13개째, 14개째를 기준으로 계산을 다르게 한다.
+      if (asci <= 77) {
+        ans = 90 - (asci - 65);
+      } else {
+        ans = 65 + (90 - asci);
+      }
+      // 소문자는 아스키코드 97부터 122까지
+    } else if (asci >= 97 && asci <= 122) {
+      if (asci <= 109) {
+        ans = 122 - (asci - 97);
+      } else {
+        ans = 97 + (122 - asci);
+      }
+    }
+    ansStr = ansStr + String.fromCharCode(ans);
+  })
+  return ansStr + " ";
 }
 
 module.exports = problem4;
