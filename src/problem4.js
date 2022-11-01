ed10865933c55ed3f088358a95b0fea4a5697ef0
Sallyvely <tpdus4159@naver.com>
2022-11-01 21:21:20 +0900

    4번 문제 아스키코드를 통해 문자 변환 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer="";
+
+
+  for(let i=0; i<word.length;i++){
+    let x=word.charCodeAt(i);
+    if(x>=97 && x<=122 ){
+      answer= answer+ String.fromCharCode(219-x);
+    }
+    else if(x>=65 && x<=97){
+      answer= answer+ String.fromCharCode(155-x);
+    }
+    else answer= answer+String.fromCharCode(x);
+  }
+
   return answer;
 }
 
