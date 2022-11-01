1fe439a3a7a990c3907413a08e83b4eea6b79f85
Jiwoo Park <jiwoo9446@naver.com>
2022-11-01 20:57:04 +0900

    refactor:problme6 split module by function

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,22 @@
 function problem6(forms) {
   let map = new Map();
-  let answer = [];
+  collectSplitedNickname(forms, map);
+  let answer = selectDuplicatedNickname(forms, map);
+  answer = new Set(answer);
+  return [...answer].sort();
+}
+
+function collectSplitedNickname(forms, map) {
   forms.forEach((person) => {
     const [_, id] = person;
     for (let i = 0; i < id.length - 1; i++) {
       map.set(id.substring(i, i + 2), map.get(id.substring(i, i + 2)) + 1 || 1);
     }
   });
+}
+
+function selectDuplicatedNickname(forms, map) {
+  let selectedMail = [];
   forms.forEach((person) => {
     const [mail, id] = person;
     let dic = [];
@@ -15,13 +25,12 @@ function problem6(forms) {
     }
     for (let j = 0; j < dic.length; j++) {
       if (map.get(dic[j]) > 1) {
-        answer.push(mail);
+        selectedMail.push(mail);
         break;
       }
     }
   });
-  answer = new Set(answer);
-  return [...answer].sort();
+  return selectedMail;
 }
 
 module.exports = problem6;
