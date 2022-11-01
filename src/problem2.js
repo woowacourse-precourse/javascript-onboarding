7a823995f3385ad9f83aa7fa668d899a6b0e8e90
2wndrhs <2wndrhs@gmail.com>
2022-11-01 03:03:54 -0700

    refactor(PROBLEM2): regex를 인자로 넘기지 않도록 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,18 +1,18 @@
 // 중복되는 문자들을 반복적으로 제거하는 기능
-function deduplication(str, regex) {
+const deduplication = (str) => {
+  const regex = /([a-z])\1+/g;
   let result = str;
   while (regex.test(result)) {
     result = result.replace(regex, "");
   }
   return result;
-}
+};
 
 // solution
-function solution(cryptogram) {
-  const regex = /([a-z])\1+/g;
-  const result = deduplication(cryptogram, regex);
+const solution = (cryptogram) => {
+  const result = deduplication(cryptogram);
   return result;
-}
+};
 
 function problem2(cryptogram) {
   const answer = solution(cryptogram);
