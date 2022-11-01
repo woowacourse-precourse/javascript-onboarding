becd67d5f05424599f4a6393e59f61647593cbc4
Last-SilverLight-End <68593892+Last-SilverLight-End@users.noreply.github.com>
2022-11-01 23:50:26 +0900

    problem 7 hard

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,45 @@
 function problem7(user, friends, visitors) {
   var answer="";
+  let per =[];
+  let reco = [];
   for(let i=0; i<friends.length; i++){
+    if(friends[i][1] == user ){
+      per.push(friends[i][0]);
+    }
+    if(friends[i][0] == user ){
+      per.push(friends[i][1]);
+    }
+  }
+  for(let i=0; i<friends.length; i++){
+    for(let j=0; j<per.length; j++){
+
+      if(per[j] == friends[i][0]){
+        let flag=0;
+        for(k=0; k<reco.length; k++){
+          if(friends[i][1] == reco[k][0]){
+            reco[k][1]+=10;
+            flag=1;
+          }
+        }
+        if(flag==0)
+        reco.push([friends[i][1],10]);
+      }
+
+
+      if(per[j] == friends[i][1]){
+        let flag=0;
+        for(k=0; k<reco.length; k++){
+          if(friends[i][0] == reco[k][0]){
+            reco[k][1]+=10;
+            flag=1;
+          }
+        }
+        if(flag==0)
+        reco.push([friends[i][0],10]);
+      }
+    }
+  }
+  for(let i=0; i<visitors.length; i++){
     
   }
   return answer;
