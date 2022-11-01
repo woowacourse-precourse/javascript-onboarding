f975ecfcd5654c57e9f8b70d02342b71054adacc
kwh97 <zzzang0512@naver.com>
2022-11-01 16:42:13 +0900

    feat(problem7): user의 친구를 찾아 배열로 반환하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,10 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const userFriends = getUserFriends(user, friends);
+}
+
+function getUserFriends(user, friends) {
+  const friendsArr = friends.filter((arr) => arr.includes(user) === true);
+  return friendsArr.map(([firstName, secondName]) => firstName === user ? secondName : firstName);
 }
 
 module.exports = problem7;
