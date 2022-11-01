bc39e81302d3eea1934b36afda247d5f80b88372
Bori-github <qhflrnfl4324@gmail.com>
2022-11-01 17:51:34 +0900

    ✨ problem6 > 닉네임 중 같은 글자가 연속적으로 포함되는 크루의 이메일 목록 구하기 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,18 @@
 function problem6(forms) {
   var answer;
+
+  const emailArr = [];
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      const checkNickname = forms[i][1].slice(j, j + 2);
+      for (let k = 0; k < forms.length; k++) {
+        if (forms[k][1].includes(checkNickname) && forms[i] !== forms[k]) {
+          emailArr.push(forms[k][0]);
+        }
+      }
+    }
+  }
+
   return answer;
 }
 
