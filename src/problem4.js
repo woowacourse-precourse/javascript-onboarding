8cb1bdffcdbf0a298c7387c9805f746e7e9508f7
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:36:45 +0900

    [problem4] feat: 문자열을 쪼개 각 문자의 아스키코드값 반환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,9 @@
 function problem4(word) {
   var answer;
+  newWord = word.split("");
+  answer = [];
+  newWord.forEach(function (changeWord) {
+    ascii = changeWord.charCodeAt(0);
   return answer;
 }
 
