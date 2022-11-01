5b5c2c8a8734dbbfedc580854c5d6747146768a5
LEECHANHYUNG <lsj721538@gmail.com>
2022-11-01 23:00:25 +0900

    find users friends

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,22 @@
+/* 
+1. user의 친구를 찾는다 -> findUserFriends
+2. user의 친구의 친구를 찾는다. =>findUserFriendFriends
+3. 방문한 사람 중 user의 친구 삭제 => removeVisitorsFriends
+*/
+const findUserFriends = (user, friends) => {
+  let userFriends = [];
+  friends.map((elem) => {
+    if (elem[0] === user) {
+      userFriends.push(elem[1]);
+    }
+    if (elem[1] === user) {
+      userFriends.push(elem[0]);
+    }
+  });
+  return userFriends;
+};
 function problem7(user, friends, visitors) {
   let userFriends = [];
-  friends.map(elem=>)
   var answer;
 
   return answer;
