60eb7e2daeb4f03874bb1f41ca4083a598f093cc
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-01 23:50:02 +0900

    우아한테크코스 문제4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,39 @@
 function problem4(word) {
-  var answer;
+  var answer = [];
+  var frogword = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+  var fL = frogword.split("");
+  var fs = frogword.toLowerCase().split("");
+  for (var x of word) {
+    var num = x.charCodeAt();
+    if (num >= 97 && num <= 122) {
+      answer.push(fs[mwindex(x)]);
+    }else if (num >= 65 && num <= 95) {
+      answer.push(fL[mwindex(x)]);
+    }else {
+      answer.push(" ")
+    }
+  }
+  answer = answer.join("");
   return answer;
 }
+
+function mwindex(str) {
+  var answer = [];
+  var momword = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var sL = momword.split("");
+  var ss = momword.toLowerCase().split("");
+  for (var x of str) {
+    var num = x.charCodeAt();
+    if (num >= 65 && num <= 95) {
+      answer.push(sL.indexOf(x));
+    }else if (num >= 97 && num <= 122) {
+      answer.push(ss.indexOf(x));
+    }else {
+      answer.push(' ');
+    }
+  }
+  return answer;
+}
+
 
 module.exports = problem4;
