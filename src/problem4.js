8a272e72514045d34436f8eaf5272d04de2a22a4
Y2IN <dpdls0228@daum.net>
2022-11-01 15:53:48 +0900

    feat : 문제 제한사항 추가

--- src/problem4.js
+++ src/problem4.js
@@ -21,8 +21,20 @@ function checkAlpha(alpha) {
   return alpha;
 }
 
+function isRightWord(word) {
+  if (!(word.length > 0 && word.length < 1001)) {
+    return 0;
+  } else if (!(typeof word == "string")) {
+    return 0;
+  }
+  return 1;
+}
+
 function problem4(word) {
   let str = [];
+  if (isRightWord(word) == 0) {
+    return false;
+  }
   for (let i = 0; i < word.length; i++) {
     str += checkAlpha(word[i]);
   }
