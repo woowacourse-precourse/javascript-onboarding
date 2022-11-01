d1acf47244b7b374311fb97011cd65242bfe56ca
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 13:46:07 +0900

    [p6] 1차 탐색으로 중복 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,28 @@
+function find(forms) {
+  let tmpNick = new Set();
+  let tmpEmail = new Set();
+
+  forms.forEach((form) => {
+    const [email, nickname] = form;
+    for (let j = 1; j < nickname.length; j++) {
+      const str = nickname.substr(j - 1, 2);
+      if (tmpNick.has(str))
+        tmpEmail.add(email);
+
+      tmpNick.add(str);
+    }
+  });
+  return [tmpNick, tmpEmail];
+}
+
 function problem6(forms) {
-  var answer;
+  let nickSet = new Set();
+  let emailSet = new Set();
+
+  [nickSet, emailSet] = find(forms);
+
+  let answer = Array.from(emailSet);
+  answer.sort();
   return answer;
 }
 
