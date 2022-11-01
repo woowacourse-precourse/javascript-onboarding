f249e7ee44a271924d0213492557de5a391ab467
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:43:13 +0900

    [problem6] feat: 연속되는 문자를 저장하는 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -8,6 +8,9 @@ function problem6(forms) {
   forms.forEach(function (item) {
     nickName.push(item[1]);
   });
+  nickName.forEach(function (oneName) {
+    for (i = 0; i <= oneName.length - 1; i++) {
+      newWord = oneName[i] + oneName[i + 1];
   return answer;
 }
 
