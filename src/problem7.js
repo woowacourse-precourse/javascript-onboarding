c20ff524d23c45e7022d6789f39faae0d67377bd
JangHoonSeok <orirori512@naver.com>
2022-11-01 17:36:02 +0900

    feat: problem7. user의 친구 목록을 구하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,19 @@
 function problem7(user, friends, visitors) {
   var answer;
+  const user_friends = findFriends(user, friends);
   return answer;
+}
+
+function findFriends(user, friends) {
+  // user의 친구 찾기.
+  const user_friends = new Set();
+
+  friends.map((relation) => {
+    const [relation1, relation2] = relation;
+    if (relation2 === user) user_friends.add(relation1);
+    else if (relation1 === user) user_friends.add(relation2);
+  });
+  return [...user_friends];
 }
 
 module.exports = problem7;
