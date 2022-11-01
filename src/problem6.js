0c1f8a6bce760d7b78a48c2212c81aacce43e269
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-01 10:50:56 +0900

    refactor: [P6] 한 닉네임의 연속적인 두글자를 구하는 함수 분리
    가독성을 위한 코드 포맷팅

--- src/problem6.js
+++ src/problem6.js
@@ -1,23 +1,28 @@
 function problem6(forms) {
   let targetEmail = []
   let wordCase = {}
-  for (let form of forms) {
+  forms.forEach((form) => {
     let email = form[0]
-    let nickname = form[1]
-    let wordList = []
-    for (let i = 0; i < nickname.length - 1; i++) {
-      wordList.push(nickname.substring(i, i + 2))
-    }
-    for (let word of wordList) {
+    wordOfCrew(form).forEach((word) => {
       if (!wordCase[word]) wordCase[word] = email
       else {
         let presavedEmail = wordCase[word]
         targetEmail.push(presavedEmail)
         targetEmail.push(email)
+        wordCase[word] = email
       }
-    }
-  }
+    })
+  })
   return [...new Set(targetEmail)].sort()
 } 
+
+function wordOfCrew(form) {
+  let nickname = form[1]
+  let wordList = []
+  for (let i = 0; i < nickname.length - 1; i++) {
+    wordList.push(nickname.substring(i, i + 2))
+  }
+  return wordList
+}
 
 module.exports = problem6;
