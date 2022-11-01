392caaeb814cbdf6d221adb808dfb8347913bb48
lineJeong <line.jeong32@gmail.com>
2022-11-01 15:59:17 +0900

    problem4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,18 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  const uppercase = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  const lowercase = uppercase.toLowerCase();
+
+  for (let i = 0; i < word.length; i++) {
+    if (uppercase.includes(word[i])) {
+      answer += uppercase[25 - uppercase.indexOf(word[i])];
+    } else if (lowercase.includes(word[i])) {
+      answer += lowercase[25 - lowercase.indexOf(word[i])];
+    } else {
+      answer += word[i];
+    }
+  }
+
   return answer;
 }
 
