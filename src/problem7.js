8c558db5b83148238b81dc74ccd08b05f8875563
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:37:55 +0900

    feat: [problem7] 친구 데이터 저장
    key : user name, value: Array of user name

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,19 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let friendsDictionary = {};
+  for (const friend of friends) {
+    if (friend[0] in friendsDictionary) {
+      friendsDictionary[friend[0]].push(friend[1])
+    } else {
+      friendsDictionary[friend[0]] = [friend[1]]
+    }
+
+    if (friend[1] in friendsDictionary) {
+      friendsDictionary[friend[1]].push(friend[0])
+    } else {
+      friendsDictionary[friend[1]] = [friend[0]]
+    }
+  }
   return answer;
 }
 
