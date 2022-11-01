15ec99e8df1203de8a7bb71df6cce9fb51c94e2b
shinhyunsu01 <shine.master9687@gmail.com>
2022-11-01 13:45:43 +0900

    3 solve

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,9 @@
 function problem3(number) {
-  var answer;
-  return answer;
+	let str = "";
+	let arr = ["3", "6", "9"];
+	for (let i = 1; i <= number; i++) str += String(i);
+
+	return str.split("").filter((ele) => arr.includes(ele)).length;
 }
 
 module.exports = problem3;
