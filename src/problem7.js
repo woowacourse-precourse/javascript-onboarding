7553befea5f70d657bac2e836ee3b227427964d7
yeongseokim <1903020@ewhain.net>
2022-11-01 22:58:32 +0900

    feat(problem7): user의 친구를 친구로 갖는 만큼 점수 +10

--- src/problem7.js
+++ src/problem7.js
@@ -1,14 +1,44 @@
+function createNewCandidate(hisFriend) {
+  return {
+    friends: [hisFriend],
+    score: 0,
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   let userInfo = {
     name: user,
     friends: [],
   };
+  let recommendedCandidate = {};
 
   friends.map((friend) => {
     if (userInfo.name === friend[0]) userInfo.friends.push(friend[1]);
     else if (userInfo.name === friend[1]) userInfo.friends.push(friend[0]);
+    else {
+      if (!recommendedCandidate[friend[0]]) {
+        recommendedCandidate[friend[0]] = createNewCandidate(friend[1]);
+      }
+      else {
+        recommendedCandidate[friend[0]].friends.push(friend[1]);
+      }
+
+      if (!recommendedCandidate[friend[1]]) {
+        recommendedCandidate[friend[1]] = createNewCandidate(friend[0]);
+      }
+      else {
+        recommendedCandidate[friend[1]].friends.push(friend[0]);
+      }
+    }
   });
+
+  userInfo.friends.map((userfriend) => {
+    let friendsOfFriend = recommendedCandidate[userfriend].friends;
+    friendsOfFriend.map((friendOfFriend) => {
+      recommendedCandidate[friendOfFriend].score += 10;
+    })
+  })
 
   return answer;
 }
