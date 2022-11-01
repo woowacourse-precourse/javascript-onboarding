9dafcdebe03ae60c6785f77ce35d1d75716b84f2
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 10:45:43 +0900

    [problem1] feat: 1.Exception handling for wrong input

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,10 @@
 function problem1(pobi, crong) {
   var answer;
+  // 왼쪽 페이지 + 1 === 오른쪽 페이지가 아닐 때
+  if (pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1]) return -1;
+  // 왼쪽 페이지가 홀수가 아닐 때
+  else if (pobi[0] % 2 !== 1 || crong[0] % 2 !== 1) return -1;
+
   return answer;
 }
 
