69d04ca586044204bc838ec6d37dd8aad6321384
pakxe <pigkill40@naver.com>
2022-11-01 16:07:02 +0900

    refactor: [P3] let을 const로 변경

--- src/problem3.js
+++ src/problem3.js
@@ -5,7 +5,7 @@ function problem3(number) {
   let handsClap = 0;
 
   for (let i = 1; i <= number; i++) {
-    let num = i.toString().split(""); // 숫자를 순회할 수 있는 배열로 변경
+    const num = i.toString().split(""); // 숫자를 순회할 수 있는 배열로 변경
     handsClap += oneNumberClap(num);
   }
 
