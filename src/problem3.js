cf90ca76c9f151864368d61761480f2ee2c29325
Josanghyeon <BStkdgus.dev@gmail.com>
2022-11-01 19:13:23 +0900

    feat: problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,13 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let count = 0;
+  let degit;
+  for (let i = 3; i <= number; i += 1) {
+    for (let j = i; j > 0; j = Math.floor(j / 10)) {
+      degit = j % 10;
+      if (degit && !(degit % 3)) count += 1;
+    }
+  }
+  return count;
 }
 
 module.exports = problem3;
