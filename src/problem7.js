e3b73355ef1dfd44c5edc33270f7b49f86781e58
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 14:42:28 +0900

    feat: [problem7] 다른 사용자가 user를 방문한 횟수에 따라 점수를 부여하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,22 @@
+function getScoreByVisitor(visitors) {
+  let score = {};
+  for (let i = 0; i < visitors.length; i++){
+    if(visitors[i] in score == false){
+      score[visitors[i]] = 1;
+    }
+    else{
+      score[visitors[i]] += 1;
+    }
+  }
+  return score;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  let friendScore = {};
+
+  friendScore = getScoreByVisitor(visitors);
+
   return answer;
 }
 
