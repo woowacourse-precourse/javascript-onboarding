be5710d4aa998fe8099a6c70beffaa966e9ed7f7
Raehyeon Jeong <raehy19@gmail.com>
2022-11-01 22:14:30 +0900

    feat(problem7) : 입력값 예외 처리 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,42 @@
+function isLowerCaseOnly(word) {
+  for (let i = 0; i < word.length; ++i) {
+    if (
+      word.charCodeAt(i) < 'a'.charCodeAt(0) ||
+      word.charCodeAt(i) > 'z'.charCodeAt(0)
+    )
+      return false;
+  }
+  return true;
+}
+
+function isException(user, friends, visitors) {
+  if (
+    !(typeof user === 'string') ||
+    user.length > 30 ||
+    user.length < 1 ||
+    !Array.isArray(friends)
+  )
+    return true;
+  for (let i = 0; i < friends.length; ++i) {
+    if (
+      !Array.isArray(friends[i]) ||
+      friends[i].length !== 2 ||
+      !(typeof friends[i][0] === 'string') ||
+      friends[i][0].length > 30 ||
+      friends[i][0].length < 1 ||
+      !isLowerCaseOnly(friends[i][0]) ||
+      !(typeof friends[i][1] === 'string') ||
+      friends[i][1].length > 30 ||
+      friends[i][1].length < 1 ||
+      !isLowerCaseOnly(friends[i][1])
+    )
+      return true;
+  }
+  return false;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  if (isException(user, friends, visitors)) return;
 }
 
 module.exports = problem7;
