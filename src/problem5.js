02ac7983541089812b537608bd969745c482e307
ukkodeveloper <ukkodeveloper@gmail.com>
2022-11-01 10:33:45 +0900

    refactor: [문제5] 전역변수 설정

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,12 @@
 function problem5(money) {
-  if (money < 1 || money > 1000000) {
+  const MIN_MONEY = 1;
+  const MAX_MONEY = 1000000;
+
+  if (money < MIN_MONEY || money > MAX_MONEY) {
     throw new Error('money는 1이상 1,000,000이하여야 합니다.');
   }
 
-  const currencyKoreaDominations = [
-    50000, 10000, 5000, 1000, 500, 100, 50, 10, 1,
-  ];
+  const currencyKoreaDominations = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
   const convertMoneyToCurrencyCallback = (currency) => {
     const CurrencyCount = Math.floor(money / currency);
