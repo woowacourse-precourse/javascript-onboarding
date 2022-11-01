34d2b398b80b666dee4fd852d4882662999f88c6
n0eyes <doll4586633@naver.com>
2022-11-01 20:10:07 +0900

    feat: [P7] 유저의 직접적인 친구들을 추출하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
+function getDirectFriends(user, friends) {
+  const directFriends = [];
+
+  friends.forEach((relationship) => {
+    relationship.includes(user) &&
+      directFriends.push(relationship[1 - relationship.indexOf(user)]);
+  });
+
+  return directFriends;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  const directFriends = getDirectFriends(user, friends);
+
+  return;
 }
 
 module.exports = problem7;
