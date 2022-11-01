3c7d8db91255611d9d74aa6062f960271a978e5a
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 10:10:12 +0900

    problem1 bug fixed

--- src/problem1.js
+++ src/problem1.js
@@ -7,8 +7,9 @@ function solution(pobi, crong) {
   if (!checkValid(pobi) || !checkValid(crong)) {
     return -1;
   }
-  pobiNum = biggerNum(pobi);
-  crongNum = biggerNum(crong);
+  let pobiNum = biggerNum(pobi);
+  let crongNum = biggerNum(crong);
+  console.log("FINAL RESULT: ", pobiNum, crongNum)
   if (pobiNum == crongNum) {
     return 0;
   } else if (pobiNum > crongNum) {
@@ -18,8 +19,6 @@ function solution(pobi, crong) {
   } else {
     return -1;
   }
-
-  
 }
 
 function checkValid(array) {
@@ -27,25 +26,43 @@ function checkValid(array) {
 }
 
 function biggerNum(array) {
-  num1 = ones(array[0]);
-  num2 = tens(array[0]);
-  num3 = hundreds(array[0]);
-  num4 = ones(array[1]);
-  num5 = tens(array[1]);
-  num6 = hundreds(array[1]);
-  firstNum = compareNum(num1+num2+num3, num1*num2*num3);
-  secondNum = compareNum(num4+num5+num6, num4*num5*num6);
+  const num1 = ones(array[0]);
+  const num2 = tens(array[0]);
+  const num3 = hundreds(array[0]);
+  const num4 = ones(array[1]);
+  const num5 = tens(array[1]);
+  const num6 = hundreds(array[1]);
+  firstNum = compareNum(num1+num2+num3, multiplication(array[0],num1,num2,num3));
+  console.log("firstNum!", firstNum);
+  console.log("add all ", num1+num2+num3);
+  console.log("multi all",  multiplication(array[0],num1,num2,num3));
+  secondNum = compareNum(num4+num5+num6, multiplication(array[1],num4,num5,num6));
+  console.log("2222Num!", secondNum);
+  console.log("add all ", num4+num5+num6);
+  console.log("multi all",  multiplication(array[1],num4,num5,num6));
   result = compareNum(firstNum, secondNum);
+  console.log("resstult!", result);
+  return result;
 }
 
 function ones(num) {
   return num % 10;
 }
 function tens(num) {
-  return num / 10 % 10;
+  return Math.floor(num / 10 % 10);
 }
 function hundreds(num) {
-  return num / 100;
+  return Math.floor(num / 100);
+}
+
+function multiplication (num, x, y, z) {
+  if (num < 10) {
+    return x;
+  } else if (num < 100) {
+    return x * y;
+  } else {
+    return x * y * z;
+  }
 }
 
 function compareNum(a, b) {
