53003d627b4e83bda8f1b23f368f2bdcd5f71034
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 19:31:27 +0900

    problem6) getAllCandidateWords 함수로 조회할 모든 닉네임부분을 가져온다.

--- src/problem6.js
+++ src/problem6.js
@@ -4,11 +4,36 @@ const getMatchedStr = (regExp, str) => {
 };
 
 const isMatched = (regExp) => (str) => str === getMatchedStr(regExp, str);
+const isProperKorean = isMatched(/[가-힣]{1,19}/);
+const isProperEmail = isMatched(/.{1,9}@email.com$/);
+
+function getAllCandidateWords(nickname) {
+  const len = nickname.length;
+  const candidateWords = new Set();
+
+  for (let i = 0; i < len; i++) {
+    let nickParts = nickname[i];
+
+    for (let j = i + 1; j < len; j++) {
+      nickParts += nickname[j];
+      candidateWords.add(nickParts);
+    }
+  }
+
+  return [...candidateWords];
+}
 
 function problem6(forms) {
-  var answer;
-  const isProperKorean = isMatched(/[가-힣]{1,19}/);
-  const isProperEmail = isMatched(/.{1,9}@email.com$/);
+  var answer = [];
+  const emailMap = {};
+  const resultSet = new Set();
+
+  forms.forEach(([email, nickname]) => {
+    if (!isProperEmail(email)) return;
+    if (!isProperKorean(nickname)) return;
+
+    const candidateWords = getAllCandidateWords(nickname);
+  });
 
   return answer;
 }
