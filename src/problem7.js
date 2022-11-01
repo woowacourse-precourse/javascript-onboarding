9f2f85f86f2b08c5b5f01fbde8a2708cf2a65848
rhino-ty <okko8522@gmail.com>
2022-11-01 17:10:31 +0900

    Docs(problem7.js): 의사코드 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,12 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
-}
+/*
+ * 1. user의 친구 찾는 기능
+ * 2. user의 친구의 친구들을 찾아 +10하는 기능
+ * 3. visitors의 이름에 +1하는 기능
+ * 4. 점수 순으로 정렬 및 반환
+ *   4-1. 0점인 경우 추천 x
+ *   4-2. 점수가 같다면 sort()
+ *
+ */
+function problem7(user, friends, visitors) {}
 
 module.exports = problem7;
