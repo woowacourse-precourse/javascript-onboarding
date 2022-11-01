86bead29f05deb827f1281188cd00ad645c80ac3
dhkham <khamdong@gmail.com>
2022-11-01 16:14:35 +0900

    feat: (prob_7) add function that takes in two arrays and returns the number of mutual items in two arrays

--- src/problem7.js
+++ src/problem7.js
@@ -37,3 +37,16 @@ function getFriendNameOfUser(user, friends) {
   }
   return userFriend;
 }
+
+// count mutual item in two arrays
+function countMutualItem(arr1, arr2) {
+  let count = 0;
+  for (let i = 0; i < arr1.length; i++) {
+    for (let j = 0; j < arr2.length; j++) {
+      if (arr1[i] === arr2[j]) {
+        count++;
+      }
+    }
+  }
+  return count;
+}
