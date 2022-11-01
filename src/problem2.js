a2f3e1069eb52da14d7ef1c8e4a50c9cb74e6e59
Hannah Ahn <ahnlook90@gmail.com>
2022-11-01 13:17:40 +0900

    Resolve problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
+  let newCryptogram = '';
+
+  for (let i = 0; i <= cryptogram.length; i++) {
+    if (cryptogram[i] !== cryptogram[i+1] &&
+      cryptogram[i] !== cryptogram[i-1]) {
+        newCryptogram += cryptogram[i];
+      }
+  }
+
+  if (cryptogram !== newCryptogram) {
+    return problem2(newCryptogram);
+  }
+
+  var answer = newCryptogram;
   return answer;
 }
 
