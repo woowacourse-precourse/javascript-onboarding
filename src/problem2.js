fb881ab44b44a627ed028451dc4d7e39f304892f
xownsdl0 <xownsdl0@naver.com>
2022-11-01 15:58:12 +0900

    feat(problem2): 중복된 문자가 더 이상 없는 경우 종료 구문 추가

--- src/problem2.js
+++ src/problem2.js
@@ -4,19 +4,16 @@ function problem2(cryptogram) {
 
   for (let i = 0; i < cryptogram.length; i++) {
     //중복 되는 값이 없는 경우
-    if (setArr[setArr.length - 1] !== cryptogram[i]) {
-      setArr.push(cryptogram[i]);
-    } else {
-      //예외사항 : 중복되는 문자가 홀수일 경우
-      if (setArr[setArr.length - 1] === cryptogram[i + 1]) {
-        i += 1;
-      }
+    if (setArr[setArr.length - 1] !== cryptogram[i]) setArr.push(cryptogram[i]);
+    else {
+      // 중복되는 문자가 홀수일 경우
+      if (setArr[setArr.length - 1] === cryptogram[i + 1]) i += 1;
       // 홀수 이후에도 연속된 문자일 경우 (짝수)
-      if (cryptogram[i] === cryptogram[i + 1]) {
-        i += 1;
-      }
+      if (cryptogram[i] === cryptogram[i + 1]) i += 1;
       // 중복되는 값이 있는 경우
       setArr.pop();
+      // 중복된 문자가 하나인 경우 반복 종료
+      if (setArr.length === 1) break;
     }
   }
 
