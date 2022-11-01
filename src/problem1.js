911950df6ddb1249e96cc7e55fe626ed9a47b006
gsong <rmsdlf1206@gmail.com>
2022-11-01 20:02:41 +0900

    change: 필요없는 주석들 삭제

--- src/problem1.js
+++ src/problem1.js
@@ -52,6 +52,4 @@ function errorCheck(page) {
   )
     return false;
   return true;
-} // 왼쪽 페이지가 짝수인지 확인 필요
-
-// 입력값 타입에 따른 에러처리
+}
--- src/problem4.js
+++ src/problem4.js
@@ -7,7 +7,7 @@ function convertByAscii(word) {
   answerList = [];
   word.split("").map((letter) => {
     if (letter.charCodeAt(0) >= 65 && letter.charCodeAt(0) <= 90) {
-      answerList.push(String.fromCharCode(155 - letter.charCodeAt(0))); // 155 -> ascii A(65) + Z(90)
+      answerList.push(String.fromCharCode(155 - letter.charCodeAt(0)));
     } else if (letter.charCodeAt(0) >= 97 && letter.charCodeAt(0) <= 122) {
       answerList.push(String.fromCharCode(219 - letter.charCodeAt(0)));
     } else {
