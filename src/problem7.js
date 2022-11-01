e2564ebbf8a085bbbb6da3535fde1c994981ceec
useon <yoosk4986@naver.com>
2022-11-01 16:43:19 +0900

    feat: [problem7] user 친구를 찾는다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,15 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer = [];
+  const userFriend = new Set();
+
+  // 1. user 친구를 찾는다.
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].indexOf(user) !== -1) {
+      if (friends[i].indexOf(user) === 0) userFriend.add(friends[i][1]);
+      if (friends[i].indexOf(user) === 1) userFriend.add(friends[i][0]);
+    }
+  }
+
   return answer;
 }
 
