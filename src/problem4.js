0bec7a31333d04518420190a00145882c28ada35
김희진 <choha941108@gmail.com>
2022-11-01 16:51:50 +0900

    (4번) 가독성 증가를 위한 코드 정리 및 주석 추가

--- src/problem4.js
+++ src/problem4.js
@@ -4,6 +4,7 @@ function problem4(word) {
   //console.log("워드입력",word)
 
   let dictionary = {};
+  //딕셔너리 생성
 
   
   for ( i = 65 ; i < 91 ; i ++ ) {
@@ -25,9 +26,6 @@ function problem4(word) {
   //console.log("아스키 변환확인",new_word) 
   //입력 단어 아스키 코드 변환
 
-
-
-
   let last_word = []
   for ( l = 0 ; l < new_word.length ; l ++) {
     let chr = new_word[l];
@@ -42,6 +40,7 @@ function problem4(word) {
   //변환 단어 알파벳으로 변환
 
   answer = last_word.join("");
+  //변환 알파벳 단어로 변환
 
   return answer;
 }
