fc8b94954df86fe46aa0e942cb18423dc4e981ca
2wndrhs <2wndrhs@gmail.com>
2022-11-01 03:47:54 -0700

    refactor(PROBLEM5): 일반 함수를 화살표 함수로 리팩토링

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,5 @@
 // 돈을 각 단위의 화폐로 변환해주는 기능
-function changeMoney(money) {
+const changeMoney = (money) => {
   let restMoney = money;
   const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const result = coins.reduce((acc, cur) => {
@@ -9,19 +9,17 @@ function changeMoney(money) {
     return acc;
   }, []);
   return result;
-}
+};
 
 // solution
-function solution(money) {
+const solution = (money) => {
   const result = changeMoney(money);
   return result;
-}
+};
 
 function problem5(money) {
   const answer = solution(money);
   return answer;
 }
-
-solution(50237);
 
 module.exports = problem5;
