1db93161eb40616a88af20b395215c6b1061324d
HAEROOL <gofn080776@gmail.com>
2022-11-01 19:09:38 +0900

    feat: [problem6] - 닉네임을 두글자씩 나누어 중복된 닉네임을 판정하는 함수 작성

--- src/problem6.js
+++ src/problem6.js
@@ -4,3 +4,15 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
+
+function getDuplicateMap(form, tokenMap){
+  let email = form[0];
+  for(let x = 0;x < form[1].length - 1;x++){
+    let nameToken = form[1].slice(x, x + 2);
+    if(tokenMap.has(nameToken) && tokenMap.get(nameToken).indexOf(email) < 0){
+      tokenMap.set(nameToken, [...tokenMap.get(nameToken), email]);
+    }else{tokenMap.set(nameToken, [form[0]]);}
+  }
+  return tokenMap;
+}
+
