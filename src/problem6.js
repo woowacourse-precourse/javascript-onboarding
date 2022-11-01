2a97f02791de02c80aa6875a3aefc84e379a45d4
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-01 21:54:17 +0900

    refactor: [6번 문제] 풀이 수정으로 전체 리팩토링

--- src/problem6.js
+++ src/problem6.js
@@ -1,43 +1,56 @@
 function problem6(forms) {
-  const nickNameList = forms.map((form) => {
-    const nickName = form[1];
-    return nickName;
-  });
+  const nickNameList = getNickNameList(forms);
+  const twoLetterCount = getTwoLetterCount(nickNameList);
+  const twoLetterList = Object.keys(twoLetterCount);
+  const duplicateTwoLetter = getDuplicateTwoLetter(
+    twoLetterCount,
+    twoLetterList
+  );
+  const emailList = getDuplicateNicknameEmail(forms, duplicateTwoLetter);
+  emailList.sort();
+  return emailList;
+}
 
-  const twoLetterList = [];
+function getNickNameList(forms) {
+  const nickNameList = [];
+  forms.map((form) => {
+    const nickName = form[1];
+    nickNameList.push(nickName);
+  });
+  return nickNameList;
+}
+
+function getTwoLetterCount(nickNameList) {
+  const twoLetterCount = {};
   nickNameList.map((nickName) => {
     for (let i = 0; i < nickName.length - 1; i++) {
-      const twoLetterNickName = `${nickName[i]}${nickName[i + 1]}`;
-      if (!twoLetterList.includes(twoLetterNickName)) {
-        twoLetterList.push(twoLetterNickName);
+      const twoLetter = `${nickName[i]}${nickName[i + 1]}`;
+      twoLetterCount[twoLetter]
+        ? twoLetterCount[twoLetter]++
+        : (twoLetterCount[twoLetter] = 1);
+    }
+  });
+  return twoLetterCount;
+}
+
+function getDuplicateTwoLetter(twoLetterCount, twoLetterList) {
+  return twoLetterList.filter((twoLetter) => twoLetterCount[twoLetter] >= 2);
+}
+
+function getDuplicateNicknameEmail(forms, duplicateTwoLetter) {
+  const emailList = [];
+  forms.map((form) => {
+    const email = form[0];
+    const nickName = form[1];
+    for (let i = 0; i < nickName.length - 1; i++) {
+      const twoLetter = `${nickName[i]}${nickName[i + 1]}`;
+      if (duplicateTwoLetter.includes(twoLetter)) {
+        emailList.push(email);
+        break;
       }
     }
   });
-
-  let countList = Array.from({ length: twoLetterList.length - 1 }, () => 0);
-  nickNameList.map((nickName) => {
-    for (let i = 0; i < twoLetterList.length; i++) {
-      const twoLetter = twoLetterList[i];
-      if (nickName.includes(twoLetter)) {
-        countList[i]++;
-      }
-    }
-  });
-  countList = countList.filter((count) => count >= 2);
-
-  const result = [];
-  for (let i = 0; i < countList.length; i++) {
-    for (let j = 0; j < nickNameList.length; j++) {
-      const nickName = nickNameList[j];
-      const twoLetter = twoLetterList[i];
-      if (nickName.includes(twoLetter)) {
-        const email = forms[j][0];
-        result.push(email);
-      }
-    }
-  }
-
-  return result.sort();
+  return emailList;
 }
 
 module.exports = problem6;
