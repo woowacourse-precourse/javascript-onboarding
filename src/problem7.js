a7752c2dd700bd72fe5c733408c106321f5137b8
이여진 <jinspace3710@gmail.com>
2022-11-01 19:11:39 +0900

    feat: problem7 기능 2 - 친구 추천 목록 점수 계산

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,36 @@
 function problem7(user, friends, visitors) {
   let answer;
   let alreadyFriends = findFriends(user, friends);
+  let recommendations = [];
+
+  for (i = 0; i < friends.length; i++) {
+    let firstName = friends[i][0];
+    let secondName = friends[i][1];
+
+    if (firstName !== user && secondName !== user) {
+      if (
+        isFriend(firstName, alreadyFriends) &&
+        !isFriend(secondName, alreadyFriends)
+      ) {
+        recommendations = updateRecommendations(
+          recommendations,
+          secondName,
+          10
+        );
+      } else if (
+        !isFriend(firstName, alreadyFriends) &&
+        isFriend(secondName, alreadyFriends)
+      ) {
+        recommendations = updateRecommendations(recommendations, firstName, 10);
+      }
+    }
+  }
+
+  for (i = 0; i < visitors.length; i++) {
+    if (!isFriend(visitors[i], alreadyFriends)) {
+      recommendations = updateRecommendations(recommendations, visitors[i], 1);
+    }
+  }
 
   return answer;
 }
@@ -16,5 +46,26 @@ function findFriends(user, friends) {
   }
 
   return alreadyFriends;
+}
+
+function isFriend(name, alreadyFriends) {
+  if (alreadyFriends.includes(name)) return true;
+  else return false;
+}
+
+function updateRecommendations(recommendations, name, score) {
+  let flag = 0;
+
+  for (j = 0; j < recommendations.length; j++) {
+    if (recommendations[j][0] === name) {
+      recommendations[j][1] += score;
+      flag = 1;
+      break;
+    }
+  }
+
+  if (flag === 0) recommendations.push([name, score]);
+
+  return recommendations;
 }
 module.exports = problem7;
