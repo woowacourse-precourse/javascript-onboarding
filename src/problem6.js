be2f090153c778f019eebf778d1d954f59fdf262
cocoa <jwv0323@kau.kr>
2022-11-01 16:36:21 +0900

    problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,15 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let changeNickname = [];
+
+  forms.forEach((data, index) => {
+    for(let i = index + 1; i < forms.length; i++){
+      for(let j = 0; j < data[1].length - 1; j++){
+        (forms[i][1].search(data[1].substr(j,2)) !== -1) ? changeNickname.push(data[0], forms[i][0]) : null;
+      }
+    }
+  })
+
+  return [...new Set(changeNickname)].sort();
 }
 
 module.exports = problem6;
