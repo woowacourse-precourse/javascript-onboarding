11ac636875f40877f90cfb79f10a5d37f75e9ae1
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 12:23:49 +0900

    Problem4: 기능 목록 작성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,10 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  /*
+  기능 목록 작성
+  1. 기본 알파벳 배열과 Reverse 배열 준비 (대소문자 구분)
+  2. word를 한 자리씩 잘라 순회하면서 알파벳이면 Reverse 배열에서 변환값 추출
+  3. 최종 정답 반환
+  */
 }
 
 module.exports = problem4;
