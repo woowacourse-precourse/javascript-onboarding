a268b18694994465c083c60e4b845f3f4db12f69
yu-retel <dbdudals100@naver.com>
2022-11-01 20:29:43 +0900

    refactor: [P5] 단순 for문 고차함수로 리팩토링

--- src/problem5.js
+++ src/problem5.js
@@ -12,11 +12,11 @@ function moneyTransform(money){
   let moneyCount=0;
   const resultArr=[];
   const list = [50000,10000,5000,1000,500,100,50,10,1];
-  for(let item of list){
-    moneyCount = Math.floor(money/item); 
-    money = money - (item * Math.floor(money/item)); 
+  list.forEach((el)=>{
+    moneyCount = Math.floor(money/el); 
+    money = money - (el * Math.floor(money/el)); 
     resultArr.push(moneyCount);
-  }
+  })
   return resultArr;
 }
 
