95983bf3b8fcc4fd2314f0f4554fda54fd3ce86f
Jinu <jinu200131@gmail.com>
2022-11-01 19:06:27 +0900

    (prob 7) 요구 사항 정리

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,10 @@
+// user     => 사용자, length : 1~ 30
+// friends  => 친구 관계 배열, length : 1~ 10'000
+// visitor	=> 사용자가 방문한 사용자 이름 배열
+// return	=> 점수 내림차순, 동점 : 사전순
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+	var answer;  
+	return answer;
 }
 
 module.exports = problem7;
