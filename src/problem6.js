995818d4d7139c5914689ebebacecf1bd40568ea
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:32:23 +0900

    feat: [problem6] 각각 닉네임끼리 중복 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,14 @@
 function problem6(forms) {
   var answer;
   let isDuplicatedNickName = new Array(forms.length);
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = i + 1; j < forms.length; j++) {
+      if (isDuplicationOfPart(forms[i][1], forms[j][1])) {
+        isDuplicatedNickName[i] = 1
+        isDuplicatedNickName[j] = 1
+      }
+    }
+  }
   return answer;
 }
 
