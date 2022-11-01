adc2d4ca6d049c75e996603ef31a40e32dfefd23
webdreamer12 <dreamerdev12@gmail.com>
2022-11-01 21:54:21 +0900

    refactor: problem5.js - 구현한 코드의 불필요함을 줄이고자 반복문을 통해 리팩토링

--- src/problem5.js
+++ src/problem5.js
@@ -1,32 +1,15 @@
 function problem5(money) {
   let moneyWithdraw = money;
-  let fiftyThousandCount = parseInt(moneyWithdraw / 50000);
-  let moneyLeft = moneyWithdraw - fiftyThousandCount * 50000;
-  let tenThousandCount = parseInt(moneyLeft / 10000);
-  moneyLeft = moneyLeft - tenThousandCount * 10000;
-  let fiveThousandCount = parseInt(moneyLeft / 5000);
-  moneyLeft = moneyLeft - fiveThousandCount * 5000;
-  let thousandCount = parseInt(moneyLeft / 1000);
-  moneyLeft = moneyLeft - thousandCount * 1000;
-  let fiveHundredCount = parseInt(moneyLeft / 500);
-  moneyLeft = moneyLeft - fiveHundredCount * 500;
-  let hundredCount = parseInt(moneyLeft / 100);
-  moneyLeft = moneyLeft - hundredCount * 100;
-  let tenCount = parseInt(moneyLeft / 10);
-  moneyLeft = moneyLeft - tenCount * 10;
-  let oneCount = moneyLeft;
+  let moneyCountArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = [];
 
-  let answer = [
-    fiftyThousandCount,
-    tenThousandCount,
-    fiveThousandCount,
-    thousandCount,
-    fiveHundredCount,
-    hundredCount,
-    tenCount,
-    oneCount,
-  ];
+  for (let moneyCount of moneyCountArray) {
+    let eachCount = parseInt(moneyWithdraw / moneyCount);
+    moneyWithdraw = moneyWithdraw - eachCount * moneyCount;
+    answer.push(eachCount);
+  }
 
+  console.log(answer);
   return answer;
 }
 
