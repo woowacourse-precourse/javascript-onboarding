69037c22319d07654bf8ff7a79ccb5129c74707b
violetwhenisawu <anashape710@gmail.com>
2022-11-01 18:11:46 +0900

    Feat: user의 친구를 찾고, 친구 배열을 반환하는 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,29 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let friendsList = getUserFriends(user, friends);
+  console.log(friendsList);
+}
+
+function getUserFriends(user, friends) {
+  let friendsList = [];
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0].includes(user) || friends[i][1].includes(user)) {
+      friendsList.push(friends[i][0]);
+      friendsList.push(friends[i][1]);
+    }
+  }
+  let exceptUser = friendsList.filter((element) => element !== user);
+  return exceptUser;
 }
 
 module.exports = problem7;
+let user = "mrko";
+let friends = [
+  ["donut", "andole"],
+  ["donut", "jun"],
+  ["donut", "mrko"],
+  ["shakevan", "andole"],
+  ["shakevan", "jun"],
+  ["shakevan", "mrko"],
+];
+let visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"];
+problem7(user, friends, visitors);
