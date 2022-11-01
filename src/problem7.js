2b1ba3e8aa136d106b718449a41128f5115f157c
lazypalegg <109095071+lazypalegg@users.noreply.github.com>
2022-11-01 20:39:57 +0900

    Feat: ID length check function

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,18 @@
+function checkIdLength(arr) {
+  let i = 0;
+  let result;
+  arr = arr.flat();
+  while (i < arr.length) {
+    if (arr[i].length >= 1 && arr[i].length <= 30) {
+      i++;
+      result = true;
+    }
+  }
+  return result;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  
 }
 
 module.exports = problem7;
