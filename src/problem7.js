8ffc41609f92b298c6ea541596cd79cd25a3d59f
banma1234 <banma1234@gmail.com>
2022-11-01 21:49:47 +0900

    예외처리 함수

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,11 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function errorHandler(user, friends, visitors) {
+  if (user.length >= 30)  return true;
+  if (friends.length > 10000 | visitors > 10000)  return true;
+
+  return false;
+}
+
 module.exports = problem7;
