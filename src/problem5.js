26825fe6b5b4a9c0bb426dc0e6e740f72743339e
woodi97 <woodi.daily@gmail.com>
2022-11-01 22:02:25 +0900

    solve(): week-01 problem-05

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
+const CURRENCY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
 function problem5(money) {
-  var answer;
+  let answer;
+
+  answer = CURRENCY.map((currency) => {
+    const count = Math.floor(money / currency);
+    money -= count * currency;
+    return count;
+  });
+
   return answer;
 }
+
+console.log(problem5(50237));
 
 module.exports = problem5;
