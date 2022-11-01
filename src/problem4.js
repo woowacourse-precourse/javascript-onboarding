7ba5553ff69b55cd19f5d8bcc45b390f7c389d53
Vo9Ming <kimminj2017@gmail.com>
2022-11-01 18:25:11 +0900

    전달 받은 word에 요소하나하나 접근할 수 있도록 처리

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
-  var answer;
+  var answer = "";
+  var Aanswer = [];
+
+  var revAlp = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
+  var fUpAlp = revAlp.split("");
+  var fLoAlp = revAlp.toLowerCase().split("");
+
+  for (var int = 0; int < word.length; int++) {
+    var uniNum = word[int].charCodeAt();
+    if (uniNum > 65 && uniNum < 91) {
+    }
+  }
+
   return answer;
 }
 
 module.exports = problem4;
+console.log(problem4("I love you"));
