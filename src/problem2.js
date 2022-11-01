f9d28db3c1968feff00e7b7d80a6e6dee1d5edee
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 11:29:49 +0900

    [problem2] feat: 2.Loop for repeat check

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,17 @@ function problem2(cryptogram) {
   var answer;
   const arr = cryptogram.split("");
 
+  //
+  let check = true;
+  while (check) {
+    check = false;
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] === arr[i + 1]) {
+        arr.splice(i, 2);
+        check = true;
+      }
+    }
+  }
   return answer;
 }
 
