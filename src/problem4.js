3f63a729d5ad8abc58319a30583e3e07d22fcdbc
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 11:49:56 +0900

    Feat: problem4 - 알파벳 대소문자 전체와 뒤집은 문자를 만드는 반복문 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,11 @@
 function problem4(word) {
   const LowerStr = {};
   const UpperStr = {};
+
+  for (let i = 97; i <= 122; i++) {
+    LowerStr[String.fromCharCode(i)] = String.fromCharCode(219 - i);
+    UpperStr[String.fromCharCode(i - 32)] = String.fromCharCode(219 - i - 32);
+  }
 }
 
 problem4("I love you");
