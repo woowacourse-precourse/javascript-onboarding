40f7ef3a92f74e2f7201ca632b56f8a1fc1eb307
parkjikuk <paxkk@naver.com>
2022-11-01 23:19:12 +0900

    feat : 사용자 친구의 친구에게 점수를 부여하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,8 @@
 function problem7(user, friends, visitors) {
   const findFriends = findFriends(user, friends);
+  const friendsOfFriend = findFriendsOfFriend(user, friends, findFriends);
+  const getScoreFriendOfFriend = getScoreFriendOfFriend(friendsOfFriend);
+
 }
 
 function findFriends(user, friends) {
@@ -15,19 +18,30 @@ function findFriends(user, friends) {
 }
 
 function findFriendsOfFriend(user, friends, findFriends) {
-  let friendsOfFriend = [];
+  let friendOfFriend = [];
   
   friends.map((friend) => {
     findFriends.map((userFriend) => {
       if(friend.includes(userFriend) && !friend.includes(user)) {
         const friendsOfFriendArr = friend.filter((el) => el !== userFriend);
         if (!friendsOfFriendArr.includes(friendsOfFriendArr[0])) {
-          friendsOfFriend.push(friendsOfFriendArr[0])
+          friendOfFriend.push(friendsOfFriendArr[0])
         }
       }
     })
   })
-  return friendsOfFriend;
+  return friendOfFriend;
+}
+
+function getScoreFriendOfFriend(friendsOfFriend) {
+  let score = new Map();
+
+  friendsOfFriend.map((friendOfFriend) => {
+    score.has(friendOfFriend) 
+    ? score.set(friendOfFriend, score.get(friendOfFriend) + 10)
+    : score.set(friendOfFriend, 10);
+  })
+  return score;
 }
 
 module.exports = problem7;
