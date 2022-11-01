c70155de41661c21613c29bdd47bae8017cc675f
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 18:24:14 +0900

    feat[P6]: 각 이름마다 연속된 집합을 반환하는 함수추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,21 @@
 function problem6(forms) {
-	let answer;
+	let answer = [];
 	let emailArray = forms.map((form) => form[0]);
 	let nameArray = forms.map((form) => form[1]);
-	console.log(nameArray);
+
+	nameArray.forEach((names) => {
+		let sliceNameArray = saveNameLength2(names);
+	});
+
+	function saveNameLength2(names) {
+		let tempArray = [];
+		Array.from(names).map((char, index) => {
+			if (names[index + 1]) {
+				tempArray.push(char + names[index + 1]);
+			}
+		});
+		return tempArray;
+	}
 
 	return answer;
 }
