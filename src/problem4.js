342ea5624b665d76b87d7cfc330614faeee03f40
imhynni <omelas329@gmail.com>
2022-11-01 13:38:24 +0900

    feat(problem4): 기능 구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,28 @@
 function problem4(word) {
-  var answer;
+  let answer;
+
+  const arr = word.split("");
+  const dict = {};
+
+  let i = 97;
+  let j = 122;
+
+  while (i < 123) {
+    dict[String.fromCharCode(i)] = String.fromCharCode(j);
+    i++;
+    j--;
+  }
+
+  for (let i = 0; i < arr.length; i++) {
+    const lowerCase = arr[i].toLowerCase();
+    if (lowerCase in dict) {
+      const isUpperCase = arr[i] === arr[i].toUpperCase();
+      arr[i] = isUpperCase ? dict[lowerCase].toUpperCase() : dict[arr[i]];
+    }
+  }
+
+  answer = arr.join("");
+
   return answer;
 }
 
