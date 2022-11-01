1e356698d097122212e3f7264cac372e64e4c2ad
cjm0611 <81799492+cjm0611@users.noreply.github.com>
2022-11-01 14:15:34 +0900

    문제5 refactoring

--- src/problem5.js
+++ src/problem5.js
@@ -1,15 +1,17 @@
 function problem5(money) {
-  var answer;
-  let price = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let number = [0, 0, 0, 0, 0, 0, 0, 0, 0];
-  for (var i=0; i<price.length; i++){
-    if(parseInt(money/price[i])){
-      number[i] = parseInt(money/price[i]);
-      money -= price[i]*number[i];
+  const cashList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let cashCount = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let leftMoney = money;
+
+  for (let i=0; i<cashList.length; i++){
+    const quotient = parseInt(leftMoney/cashList[i]);
+    if(quotient){
+      cashCount[i] = quotient;
+      leftMoney -= cashList[i]*quotient;
     }
   }
-  answer = number;
-  return answer;
+
+  return cashCount;
 }
 
 module.exports = problem5;
