f019173210b074e640b60895ce0392d79bfd8a38
Jinu <jinu200131@gmail.com>
2022-11-01 16:19:10 +0900

    (prob 6) 기능 목록 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,11 @@
+/*
+기능 목록
+1. 두 문자열 두 글자 이상 문자 연속 동일 여부 검사 기능
+2. forms 배열 순회 중복자 이메일 result 배열 추가 기능
+2. result 배열 오름차순 정렬 기능
+2. result 배열 중복 제거 기능
+*/
+
 // forms => [['email','name'], ... ]
 // return => 닉네임 중복자 ['email', ...], 오름차순 정렬, 중복 제거
 function problem6(forms) {
