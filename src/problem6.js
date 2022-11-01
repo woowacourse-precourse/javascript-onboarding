410fc1f2e0065a79922fe2ceb6f3741dc111b7ec
고동현 <dhjk35@naver.com>
2022-11-01 16:38:30 +0900

    feat: 크루들의 닉네임에서 중복되는 단어들을 찾는 기능 구현 - 문제 6

--- src/problem6.js
+++ src/problem6.js
@@ -2,7 +2,7 @@
  * 기능 정의
  *
  * - [x] 조건에 맞는 이메일과 닉네임을 가진 양식인지 판별하는 기능
- * - [ ] 크루들의 닉네임에서 중복되는 단어들을 찾는 기능
+ * - [x] 크루들의 닉네임에서 중복되는 단어들을 찾는 기능
  * - [ ] 중복되는 단어를 가진 닉네임을 가진 크루들의 이메일을 찾는 기능
  * - [ ] 이메일들의 중복을 없애고 오름차순으로 정렬하는 기능
  */
@@ -18,6 +18,7 @@
  */
 function problem6(forms) {
   const validForms = validateForms(forms);
+  const duplicateKeywords = findDuplicateWords(validForms);
 
   var answer;
   return answer;
@@ -74,6 +75,67 @@ function isHanguel(string) {
   const regex = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]+$/;
 
   return regex.test(string);
+}
+
+/**
+ * 지원한 양식의 닉네임들 중 중복되는 단어들을 찾아주는 함수
+ * - 중복의 기준은 같은 글자가 연속적으로 포함되는 경우
+ * - 두 글자 이상의 문자가 연속적으로 순서에 맞추어 포함되어 있는 경우 중복으로 간주
+ *
+ * @param {string[][]} forms [이메일, 닉네임] 형식의 문자열 배열들이 담긴 배열
+ * @returns {string[]} 중복되는 글자들이 담긴 배열
+ */
+function findDuplicateWords(forms) {
+  const words = getTwoLetterWordsFromNickname(forms);
+  const wordCounts = words.reduce((acc, word) => {
+    acc[word] = (acc[word] || 0) + 1;
+    return acc;
+  }, {});
+
+  return Object.entries(wordCounts)
+    .filter(([_, value]) => value > 1)
+    .map(([key]) => key);
+}
+
+/**
+ * 닉네임으로부터 연속되는 두 글자들의 조합을 구하는 함수
+ *
+ * @param {string[][]} forms [이메일, 닉네임] 형식의 문자열 배열들이 담긴 배열
+ * @returns {string[]} 닉네임으로부터 뽑아낸 두 글자들이 담긴 배열
+ */
+function getTwoLetterWordsFromNickname(forms) {
+  return forms.reduce((words, [_, nickname]) => {
+    words.push(...getCombinations(nickname, 2));
+    return words;
+  }, []);
+}
+
+/**
+ * 기존 문자열에서 지정한 수의 글자만큼 조합을 구하는 함수
+ * - 단, 연속된 글자의 조합만을 구함
+ *
+ * @param {string[]} string 조합을 뽑아낼 문자열 배열
+ * @param {number} selectNumber 몇 글자의 단어들을 찾을 것인지
+ * @returns {string[]} 조건에 해당하는 문자열 배열
+ */
+function getCombinations(string, selectNumber) {
+  const results = [];
+
+  if (selectNumber === 1) {
+    return [...string].map((element) => element);
+  }
+
+  [...string].forEach((current, index, origin) => {
+    const rest = origin.slice(index + 1);
+    const [next, _] = getCombinations(rest, selectNumber - 1);
+
+    if (next) {
+      const attacehd = current + next;
+      results.push(attacehd);
+    }
+  });
+
+  return results;
 }
 
 module.exports = problem6;
