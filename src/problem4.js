f5edb9a409bd12cc9dad63d6284a0e0a04353aab
terry <kdjtiger@naver.com>
2022-11-01 11:59:07 +0900

    알파벳 배열을 만들고 역배열도 만든다

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,7 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const alpha = Array.from(Array(26)).map((e, i) => i + 65);
+  const alphabet = alpha.map((x) => String.fromCharCode(x));
+  const alphabetReverse = [...alphabet].reverse();
 }
 
 module.exports = problem4;
