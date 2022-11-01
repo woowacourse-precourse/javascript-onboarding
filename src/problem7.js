fd1adbcc982df6b45e77b0aee918fbc2a1658a60
n0eyes <doll4586633@naver.com>
2022-11-01 20:11:27 +0900

    feat: [P7] 친구 목록을 기반으로 점수판을 업데이트하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,22 @@
+function updateSheetByFriends(directFriends, friends, scoreSheet) {
+  const newSheet = { ...scoreSheet };
+
+  friends.forEach(([userA, userB]) => {
+    if (directFriends.includes(userA) && directFriends.includes(userB)) return;
+
+    if (directFriends.includes(userA))
+      newSheet[userB] ?? true
+        ? (newSheet[userB] = 10)
+        : (newSheet[userB] += 10);
+    else if (directFriends.includes(userB))
+      newSheet[userA] ?? true
+        ? (newSheet[userA] = 10)
+        : (newSheet[userA] += 10);
+  });
+
+  return newSheet;
+}
+
 function getDirectFriends(user, friends) {
   const directFriends = [];
 
@@ -10,8 +29,10 @@ function getDirectFriends(user, friends) {
 }
 
 function problem7(user, friends, visitors) {
+  let scoreSheet = {};
   const directFriends = getDirectFriends(user, friends);
 
+  scoreSheet = updateSheetByFriends(directFriends, friends, scoreSheet);
   return;
 }
 
