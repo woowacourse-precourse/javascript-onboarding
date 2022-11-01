0e5646d967afe189b74d8c633eed1879fe0da9f7
turtle601 <turtle601@naver.com>
2022-11-01 10:07:34 +0900

    Fix: [problem5] 문제 내 throw 구문 제거
    - process.exit()처럼 프로그램을 종료하는 throw 구문 제거
    - 대신 제한 사항에 맞지 않는 입력사항이 있을 경우 console로 정보 전달을 해줍니다

--- src/problem5.js
+++ src/problem5.js
@@ -5,16 +5,12 @@ class ErrorCase {
   }
 
   checkLimit() {
-    if (!(1 <= this._money && this._money <= 1000000)) {
-      throw new Error("input 양식에 에러가 발생했습니다!!");
-    }
+    return 1 <= this._money && this._money <= 1000000;
   }
 }
 
 class Bank {
   constructor(money) {
-    new ErrorCase(money);
-
     this._money = money;
     this._moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   }
@@ -36,6 +32,13 @@ class Bank {
 }
 
 function problem5(money) {
+  const error = new ErrorCase(money);
+
+  if (!error.checkLimit()) {
+    console.log("input 양식이 제한사항에 맞게 주어지지 않았습니다.");
+    return;
+  }
+
   const bank = new Bank(money);
   return bank.giveChange();
 }
