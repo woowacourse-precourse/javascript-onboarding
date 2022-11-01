2c1a21036b73ad1644a89c078eed273365b14375
jlee0505 <hj960322@gmail.com>
2022-11-01 22:03:14 +0900

    feat(problem7): 기능 4,5 추가

--- src/problem7.js
+++ src/problem7.js
@@ -10,15 +10,7 @@ function problem7(user, friends, visitors) {
   const obj1 = getNamesWithScores(friendsYouDoNotKnow.flat(), 10);
   const obj2 = getNamesWithScores(visitorsYoDoNotKnow, 1);
 
-  const result = new Set();
-  for (let i = 0; i < friendsYouDoNotKnow.length; i++) {
-    result.add(...friendsYouDoNotKnow[i]);
-  }
-  for (let i = 0; i < visitorsYoDoNotKnow.length; i++) {
-    result.add(visitorsYoDoNotKnow[i]);
-  }
-
-  recommendation = Array.from(result).filter((e) => Boolean(e));
+  recommendation = sortByScoreAndName(obj1, obj2);
 
   return recommendation;
 }
@@ -40,7 +32,6 @@ function getFriendsYouAlreadyKnow(user, friends) {
 
   return friendsYouAlreadyKnow;
 }
-
 function setThreeGroups(user, friends, visitors) {
   const currentFriends = getFriendsYouAlreadyKnow(user, friends);
   const friendsYouDoNotKnow = friends.map((friend) =>
@@ -52,7 +43,6 @@ function setThreeGroups(user, friends, visitors) {
 
   return { friendsYouDoNotKnow, visitorsYoDoNotKnow };
 }
-
 function getNamesWithScores(names, score) {
   const obj = {};
 
@@ -66,10 +56,36 @@ function getNamesWithScores(names, score) {
 
   return obj;
 }
+function sortByScoreAndName(obj1, obj2) {
+  const arr = [];
+  const recommendation = [];
+
+  objToArr(obj1, arr);
+  objToArr(obj2, arr);
+
+  for (let i = 0; i < arr.length; i++) {
+    recommendation.push(arr[i][0]);
+  }
+
+  return recommendation.slice(0, 5);
+}
+
+function objToArr(obj, arr) {
+  for (const [key, value] of Object.entries(obj)) {
+    arr.push([key, value]);
+    arr.sort(sortFn);
+  }
+}
+function sortFn(a, b) {
+  if (a[1] > b[1]) {
+    return -1;
+  }
+  if (a[1] === b[1]) if (a[0] < b[0]) return -1;
+  return 1;
+}
 
 module.exports = problem7;
 
-//expected: ["andole", "jun", "bedi"]
 console.log(
   problem7(
     "mrko",
