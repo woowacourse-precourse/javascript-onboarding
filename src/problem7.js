1392b7c3f99a3a31c2ccef0c5beb079cffa2621b
y00eunji <aine0227@naver.com>
2022-11-01 20:49:44 +0900

    feat: [problem7] make all people's friend list

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,21 @@
+const makeFriendsGraph = (friends) => {
+  const friendsGraph = new Map();
+
+  friends.forEach(([user1, user2]) => {
+    if (!friendsGraph.has(user1)) {
+      friendsGraph.set(user1, new Set());
+    }
+    friendsGraph.get(user1).add(user2);
+
+    if (!friendsGraph.has(user2)) {
+      friendsGraph.set(user2, new Set());
+    }
+    friendsGraph.get(user2).add(user1);
+  });
+
+  return friendsGraph;
+};
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
