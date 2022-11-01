5b1b42900928b845f6f9d473ba7d8559e8de07aa
letsjo <hyunoh.jo@gmail.com>
2022-11-01 16:10:52 +0900

    [problem7] Add function recommendFriendApp for total process management

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,12 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  return recommendFriendApp([user, friends, visitors]);
+}
+
+function recommendFriendApp([user, friendListArr, visitorListArr]) {
+  const relationshipMap = createRelationshipMap(friendListArr);
+  const visitorScoreMap = calVisitorScoreMap(visitorListArr);
+  const scoreListMap = calcScoreMap(user, relationshipMap, visitorScoreMap);
+  return sortByScoreToNameArr(scoreListMap).slice(0,5);
 }
 
 module.exports = problem7;
