bbdd11a7af4ff6e3e95eea7bdbff84f36f7aeb70
rayark1 <max980203@snu.ac.kr>
2022-11-01 17:05:20 +0900

    docs(prob7): Add functions list

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,11 @@
+/*
+  기능 목록
+  1. 주어진 친구관계를 이용해, 친구관계를 나타내는 Object를 만든다.
+  2. 친구관계를 나타내는 Object를 이용해, 친구의 친구를 찾아 점수를 부여한다.
+  3. 방문기록을 이용해, 점수를 부여한다.
+  4. 점수를 이용해, 추천 친구를 찾는다. 이 때, 0점인 친구는 제외한다.
+
+ */
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
