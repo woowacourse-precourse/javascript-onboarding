819139ad1e7aedb032aa55ebe9e04bc53c2a2f45
leeharyung <lhr4884@naver.com>
2022-11-01 13:11:32 +0900

    add problem3 solution

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,11 @@
 function problem3(number) {
-  var answer;
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    const j = [...i.toString()];
+    for (k of j) {
+      if (k === "3" || k === "6" || k === "9") answer++;
+    }
+  }
   return answer;
 }
-
 module.exports = problem3;
