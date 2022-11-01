7b277da23d22320f1467c834d1370a3944f8df79
1211lys <hpppower111@gmail.com>
2022-11-01 19:50:44 +0900

    ADD : problem3

--- src/problem3.js
+++ src/problem3.js
@@ -4,11 +4,26 @@ function problem3(number) {
   if (number < 0 || 10000 < number) {
   }
 
-  for (let i = 0; i <= number; i++) {
-    clap += getClap(i);
+  for (let i = 1; i <= number; i++) {
+    clap += getClapCount(i);
   }
 
   return clap;
+}
+
+function getClapCount(number) {
+  let count = 0;
+
+  number
+    .toString()
+    .split("")
+    .map((value) => {
+      if (["3", "6", "9"].includes(value)) {
+        count++;
+      }
+    });
+
+  return count;
 }
 
 module.exports = problem3;
