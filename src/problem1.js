8d7a7ab4fcc324201a09399b465e892149047204
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 16:52:24 +0900

    연속페이지 검증 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,10 @@
-function isValidPage(page) {
+function isValidPg(page) {
   if (page[0] === 1 || page[1] === 400) return false;
+  else return true;
+}
+
+function isContinuityPg(page) {
+  if (page[1] - page[0] !== 1) return false;
   else return true;
 }
 
@@ -7,7 +12,8 @@ function problem1(pobi, crong) {
   var answer;
   let answer;
 
-  if (!isValidPage(pobi) || !isValidPage(crong)) return -1;
+  if (!isValidPg(pobi) || !isValidPg(crong)) return -1;
+  if (!isContinuityPg(pobi) || !isContinuityPg(crong)) return -1;
 
   return answer;
 }
