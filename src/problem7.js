97dee0c5f6e25c36b8e4c1da93dcdb3abb68d32b
dim <dim@student.42seoul.kr>
2022-11-01 20:24:39 +0900

    [problem7]Feat: main structure, friendList, scoreList

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,55 @@
+function makeFriendList(friends) {
+  const friendList = {};
+  for (let i = 0; i < friends.length; i++) {
+    const [first, second] = friends[i];
+    if (friendList.hasOwnProperty(first))
+      friendList[first].push(second);
+    else
+      friendList[first] = [second];
+    if (friendList.hasOwnProperty(second))
+      friendList[second].push(first);
+    else
+      friendList[second] = [first];
+  }
+  return friendList;
+}
+
+function setScoreList(friendList) {
+  const scoreList = {};
+  for (let key in friendList) {
+    scoreList[key] = 0;
+  }
+  return scoreList;
+}
+
+function recommandFriend(user, friends, visitors) {
+  const friendList = makeFriendList(friends);
+  const scoreList = setScoreList(friendList);
+  addFriendScore(user, friendList, scoreList);
+  addVisitorScore(visitors, scoreList);
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = recommandFriend(user, friends, visitors);
   return answer;
 }
 
 module.exports = problem7;
+
+/* 
+구현
+
+객체만들어서 친구 목록 정리
+mrko : [donut, shakevan]
+donut : [andole, jun, mrko]
+andole ; [donut, shakevan]
+jun : [donut, shakevan]
+shakevan : [andole, jun, mrko]
+
+key, value로 객체 생성
+
+user 친구 목록 돌면서 친구의 친구들 +10
+visitor 돌며 +1
+user 직접적인 친구 리스트 삭제
+
+ */
