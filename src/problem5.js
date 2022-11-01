2cf0f0a3ca221cb0420c087ff0009dd9fea1cbdd
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 15:26:00 +0900

    [problem5] feat: 2.Loop for calculating the number of money

--- src/problem5.js
+++ src/problem5.js
@@ -5,6 +5,14 @@ function problem5(money) {
   // 결과값 저장을 위한 resultArr
   const resultArr = [];
 
+  // moneyArr 처음부터 끝 까지 배열 돌림 -> 오만 원권 부터 일원 동전까지 검사 가능
+  for (let i = 0; i < moneyArr.length; i++) {
+    if (money >= moneyArr[i]) {
+      resultArr.push(parseInt(money / moneyArr[i]));
+      money = money - parseInt(money / moneyArr[i]) * moneyArr[i];
+    } else resultArr.push(0);
+  }
+
   return answer;
 }
 
