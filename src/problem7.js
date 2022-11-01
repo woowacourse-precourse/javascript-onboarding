448a8f38b52993c77bc87590ed8435ad695b0626
parkjikuk <paxkk@naver.com>
2022-11-01 22:00:11 +0900

    feat : 사용자와 친구인 사람을 찾는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -3,4 +3,16 @@ function problem7(user, friends, visitors) {
   return answer;
 }
 
+function findFriends(user, friends) {
+  const friendArr = [];
+  
+  friends.map((friend) => {
+    if(friend.includes(user)){
+      const deliteUserName = friend.filter((el) => el !== user);
+      friendArr.push(deliteUserName)
+    }
+  })
+  return friendArr;
+}
+
 module.exports = problem7;
