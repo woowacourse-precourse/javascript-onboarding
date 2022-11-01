3c46d67d46b5c01d759ed425f712a0874d879293
dongyeopca <ckehdduq95@gmail.com>
2022-11-01 14:52:47 +0900

    feat: "Problem2" 기능 구현 암호 해독(decryptioin) 함수 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,37 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = cryptogram.split("");
+  let flag;
+  while (true) {
+    [answer, flag] = decryption(answer);
+    if (flag) {
+      continue;
+    } else {
+      return answer.join("");
+    }
+  }
+}
+
+function decryption(cryptogram) {
+  let checkedIndex = [];
+  let prev;
+  let result = [];
+  for (const index in cryptogram) {
+    if (prev != cryptogram[index]) {
+      prev = cryptogram[index];
+    } else {
+      checkedIndex.push(index - 1);
+      checkedIndex.push(Number(index));
+    }
+  }
+  if (checkedIndex.length) {
+    for (let i = 0; i < cryptogram.length; i++) {
+      if (!checkedIndex.includes(i)) {
+        result.push(cryptogram[i]);
+      }
+    }
+    return [result, true];
+  }
+  return [cryptogram, false];
 }
 
 module.exports = problem2;
