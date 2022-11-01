7220191f18b355465269cea3f445ff15187a1f14
youngentry <90388461+youngentry@users.noreply.github.com>
2022-11-01 17:33:14 +0900

    [문제7] <1단계> user와 친구인 배열만을 담은 myFriends 배열생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,15 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+    // 1단계) user와 친구인 배열만을 담은 myFriends 배열생성
+    const myFriends = [];
+
+    for (let i = 0; i < friends.length; i++) {
+        if (friends[i][1].includes(user)) {
+            myFriends.push(friends[i][0]);
+        }
+        if (friends[i][0].includes(user)) {
+            myFriends.push(friends[i][1]);
+        }
+    }
 }
 
 module.exports = problem7;
