1df854c3945070e297471179c953ed8f37708975
bassyu <yrkdgus@gmail.com>
2022-11-01 13:17:37 +0900

    문제1 valid 조건 추가

--- src/problem1.js
+++ src/problem1.js
@@ -1,35 +1,37 @@
 function problem1(pobi, crong) {
-  if (!valid(pobi) || !valid(crong)) {
-    return -1;
-  }
+  if (!valid(pobi) || !valid(crong)) return -1;
 
   const pobiPoint = getPoint(pobi);
   const crongPoint = getPoint(crong);
   
-  if (pobiPoint > crongPoint) {
-    return 1;
-  }
-  if (pobiPoint < crongPoint) {
-    return 2;
-  }
-  if (pobiPoint === crongPoint) {
-    return 0;
-  }
-  return -1;
+  if (pobiPoint > crongPoint) return 1;
+  if (pobiPoint < crongPoint) return 2;
+  return 0;
 }
 
 function valid([left, right]) {
-  return right - left === 1 && left % 2 === 1 && right % 2 === 0;
+  return (
+    right - left === 1 
+    && (left % 2 === 1 && right %2 === 0)
+    && (left >= 1 && right >= 2)
+    && (left <= 399 && right <= 400)
+  );
 }
 
 function getPoint([left, right]) {
-  const getArray = (number) => number.toString().split('').map(i => parseInt(i));
-  const leftArr = getArray(left);
-  const rightArr = getArray(right);
-
+  const getArray = (number) => String(number).split('').map(i => parseInt(i));
   const add = (acc, cur) => acc + cur;
   const mul = (acc, cur) => acc * cur;
-  return Math.max(leftArr.reduce(add), leftArr.reduce(mul), rightArr.reduce(add), rightArr.reduce(mul));
+
+  const leftArr = getArray(left);
+  const rightArr = getArray(right);
+  
+  return Math.max(
+    leftArr.reduce(add),
+    leftArr.reduce(mul),
+    rightArr.reduce(add),
+    rightArr.reduce(mul)
+  );
 }
 
 module.exports = problem1;
