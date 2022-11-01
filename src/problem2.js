6b95285a63c20778b912d2df3eea9909b872431c
kimeodml <kimeodml@gmail.com>
2022-11-01 15:10:50 +0900

    feat: 문제2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
+function solve(text){
+  if(text === ""){
+    return text;
+  }
+  let res = [text[0]];
+  let prev = text[0];
+  for(let i = 1; i < text.length; i++){
+    if(res[res.length-1] === text[i] || prev === text[i]){
+      res.pop()
+    }else{
+      prev = text[i]
+      res.push(text[i])
+    }
+  }
+  return res;
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let answer = solve(cryptogram);
+  return answer.join('');
 }
 
 module.exports = problem2;
