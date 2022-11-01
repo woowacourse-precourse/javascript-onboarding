ea624d4a0459b2c4335c157ef31bca5361b6d406
moseung <tmdah900@naver.com>
2022-11-01 09:19:08 +0900

    ✨ problem 6 기능1 구현 주어진 배열에서 닉네임이 겹치는 이메일을 배열에 담는 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,17 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  for (i = 0; i < forms.length; i++) {
+    for (j = 0; j < forms[i][1].length - 1; j++) {
+      let splittedWord = forms[i][1].substring(j, j + 2);
+      for (k = 0; k < forms.length; k++) {
+        if (i === k) continue;
+        if (forms[k][1].indexOf(splittedWord) !== -1) {
+          answer.push(forms[i][0]);
+          break;
+        }
+      }
+    }
+  }
   return answer;
 }
 
