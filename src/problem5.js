59f932126605783ca74fc26513b75fd8ea8f42d3
yeongseokim <1903020@ewhain.net>
2022-11-01 22:39:20 +0900

    feat(problem5): 각 몇 개의 지폐와 동전으로 변환되는지 계산

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
   var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let quotient = 0;
+  let remainder = money;
+  for (let i = 0; i < moneyType.length; i++) {
+    quotient = Math.floor(remainder / moneyType[i]);
+    if (quotient > 0) {
+      remainder -= quotient * moneyType[i];
+    }
+    answer[i] = quotient;
+  }
+
   return answer;
 }
 
