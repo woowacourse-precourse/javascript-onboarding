c3041c8763d4cce2d4930a6c18ecdb0f08f10468
HaroldBKim <sbookim2@gmail.com>
2022-11-01 18:31:12 +0900

    solved problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for (i=0; i<coins.length; i++) {
+      const count = parseInt(money / coins[i]);
+      answer[i] = count
+      money -= (coins[i] * count);
+  }
   return answer;
 }
 
