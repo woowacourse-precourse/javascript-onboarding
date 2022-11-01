2090df74c9d0d9e5e15c31473c1e6b74eefff2df
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 22:28:14 +0900

    feat: [PROBLEM5] 돈 크기에 따른 에러 반환(feat1) 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,19 @@
 function problem5(money) {
   var answer;
+
+  try {
+    if (money > 1000000 || money < 1) {
+      throw new Error('money must between(1, 1,000,000)');
+    }
+  } catch (e) {
+    answer = -1;
+  }
+
   return answer;
 }
+
+console.log(problem5(1000001));
+console.log(problem5(5555));
+console.log(problem5(0));
 
 module.exports = problem5;
