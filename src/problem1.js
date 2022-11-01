22b18bff5a7764f01046e4dd334da8d009748b8a
MunYongJang <dltk456@naver.com>
2022-11-01 18:55:18 +0900

    problem1 : 예외 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,11 @@
 function problem1(pobi, crong) {
   var answer;
 
+  //예외 처리
+  if (exceptCase(pobi) || exceptCase(crong)) {
+    return -1
+  }
+
   var pobiScore = Math.max(scoreCal(pobi[0]), scoreCal(pobi[1]));
   var crongScore = Math.max(scoreCal(crong[0]), scoreCal(crong[1]));
 }
@@ -16,6 +21,18 @@ function scoreCal(n1) {
   }
 
   return Math.max(plusVal, multiVal);
+}
+
+function exceptCase(arr) {
+  //첫 페이지나 마지막 페이지를 펼칠 경우
+  if (arr.includes(0) || arr.includes(400)) {
+    return true
+  }
+
+  //잘못된 입력이 들어올 경우 ex)[97, 102]
+  if (arr[1] - arr[0] !== 1) {
+    return true
+  }
 }
 
 module.exports = problem1;
