340a6f0d5bfcd5c298923f0092dae03622ac0985
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-01 17:07:56 +0900

    feature(problem4): replaceAlphabet 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,4 +1,79 @@
 function problem4(word) {
+  const replaceAlphabet = (word) => {
+    const lower_case = [
+      "a",
+      "b",
+      "c",
+      "d",
+      "e",
+      "f",
+      "g",
+      "h",
+      "i",
+      "j",
+      "k",
+      "l",
+      "m",
+      "n",
+      "o",
+      "p",
+      "q",
+      "r",
+      "s",
+      "t",
+      "u",
+      "v",
+      "w",
+      "x",
+      "y",
+      "z",
+    ];
+    const upper_case = [
+      "A",
+      "B",
+      "C",
+      "D",
+      "E",
+      "F",
+      "G",
+      "H",
+      "I",
+      "J",
+      "K",
+      "L",
+      "M",
+      "N",
+      "O",
+      "P",
+      "Q",
+      "R",
+      "S",
+      "T",
+      "U",
+      "V",
+      "W",
+      "X",
+      "Y",
+      "Z",
+    ];
+
+    const word_arr = word.split("");
+    return word_arr.map((char) => {
+      // 공백은 그대로 반환
+      if (char === " ") return " ";
+      // 소문자일 때
+      if (lower_case.includes(char)) {
+        const lower_case_idx = lower_case.indexOf(char);
+        return lower_case[lower_case.length - (lower_case_idx + 1)];
+      }
+      // 대문자일 때
+      else {
+        const upper_case_idx = upper_case.indexOf(char);
+        return upper_case[upper_case.length - (upper_case_idx + 1)];
+      }
+    });
+  };
+
   var answer;
   return answer;
 }
