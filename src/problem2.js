be50e276b108a39acdafd6254497ab3fe2831b62
y00eunji <aine0227@naver.com>
2022-11-01 20:08:18 +0900

    feat: [problem2] remove duplicate consecutive strings

--- src/problem2.js
+++ src/problem2.js
@@ -1,3 +1,13 @@
+const decryption = (crypto) => {
+  let regex = /(.)\1+/g;
+
+  if (!regex.test(crypto)) return false;
+
+  let decrypto = crypto.replace(regex, "");
+
+  return decrypto;
+};
+
 function problem2(cryptogram) {
   var answer;
   return answer;
