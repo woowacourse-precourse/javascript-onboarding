045e07e38e89c1d1b507457a4e1a812355fec184
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 16:32:05 +0900

    [problem1] feat: sumPage 함수 재구현

--- src/problem1.js
+++ src/problem1.js
@@ -3,13 +3,23 @@ console.log(problem1([97, 98], [197, 198]));
 
 function problem1(pobi, crong) {
   // maxNumber 비교 후 점수 return
+  answer = sumPage(pobi);
   return answer;
 }
 
 // 페이지 번호 각 자리 숫자 더하는 함수
 function sumPage(pages) {
-  let sumPages = 0;
-  sumPages = pages[0] + pages[1];
+  let firstSum = pages[0]
+    .toString()
+    .split("")
+    .reduce((x, y) => +x + +y, 0);
+
+  let secondSum = pages[1]
+    .toString()
+    .split("")
+    .reduce((x, y) => +x + +y, 0);
+
+  let sumPages = firstSum + secondSum;
 
   return sumPages;
 }
@@ -23,7 +33,11 @@ function multiPage(pages) {
 }
 
 // 각 과정 중 가장 큰 수 return 함수
-function maxNumber(leftPage, rightPage) {}
+function maxNumber(leftPage, rightPage) {
+  // leftPage에서 큰 수
+  // rightPage에서 큰 수
+  // 두 개 비교
+}
 
 // 예외처리 함수
 function checkProblem(pages) {
