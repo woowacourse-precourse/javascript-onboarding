409ee428620358f48fbe8d0b516c44f45c766881
jjongs2 <jjongs@hanyang.ac.kr>
2022-11-01 14:57:44 +0900

    feat(problem7): 주어진 친구 관계를 그래프 자료구조로 표현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,31 @@
+class UndirectedGraph {
+  constructor() {
+    this.vertices = {};
+  }
+
+  addVertex(vertex) {
+    this.vertices[vertex] = this.vertices[vertex] || [];
+  }
+
+  addEdge(vertex1, vertex2) {
+    this.vertices[vertex1].push(vertex2);
+    this.vertices[vertex2].push(vertex1);
+  }
+}
+
+function getFriendships(friends, friendships) {
+  for (const friendship of friends) {
+    friendships.addVertex(friendship[0]);
+    friendships.addVertex(friendship[1]);
+    friendships.addEdge(friendship[0], friendship[1]);
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  let friendships = new UndirectedGraph();
+
+  getFriendships(friends, friendships);
   return answer;
 }
 
