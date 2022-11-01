c88ca407498af83c5657f81b85bc8372a7a222bf
jinyong <zhaqh146@naver.com>
2022-11-01 12:57:26 +0900

    solve problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let result = 0;
+
+  for (let num = 1; num <= number; num++) {
+    let stringNum = String(num)
+
+    for (let element of stringNum) {
+      if (element != "0" && Number(element) % 3 === 0) {
+        result += 1
+      }
+    }
+  }
+
+  return result;
 }
 
 module.exports = problem3;
