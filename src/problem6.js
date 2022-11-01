3d82a275b9e234963c68e2989474e5b4779fa222
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-01 14:49:00 +0900

    feat: Add function for split nickname(problem6)

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,25 @@
 function problem6(forms) {
-  var answer;
+  let answer;
   return answer;
 }
+
+function splitNickname(forms) {
+  let nicknameArr = [];
+  for (let i = 0; i < forms.length; i++) {
+    nicknameArr.push(forms[i][1].split(""));
+  }
+
+  console.log(nicknameArr);
+}
+
+console.log(
+  splitNickname([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
 
 module.exports = problem6;
