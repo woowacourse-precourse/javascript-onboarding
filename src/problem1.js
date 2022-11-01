2dcde97a395729fec4ef92509c56d24b7a8289fb
HaroldBKim <sbookim2@gmail.com>
2022-11-01 17:17:42 +0900

    solved problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,37 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function check(pages) {
+  const [left, right] = pages;
+  if (1 <= left && left < 400 && right-left === 1) {
+    return true;
+  }
 }
 
-module.exports = problem1;
+function calculate(pages) {
+  const [left, right] = pages;
+  const sumLeftNum = String(left).split('').reduce((acc, cur) => acc + cur * 1, 0);
+  const sumRightNum = String(right).split('').reduce((acc, cur) => acc + cur * 1, 0);
+  const multiLeftNum = String(left).split('').reduce((acc, cur) => acc * cur);
+  const multiRightNum = String(right).split('').reduce((acc, cur) => acc * cur);
+
+  return Math.max(sumLeftNum, sumRightNum, multiLeftNum, multiRightNum);
+}
+
+function problme1(pobi, crong) {
+  if (!check(pobi) || !check(crong)){
+      return -1;
+  }
+  
+  const pobiScore = calculate(pobi);
+  const crongScore = calculate(crong);
+
+  if (pobiScore > crongScore) {
+      return 1
+  }
+  if (pobiScore < crongScore) {
+      return 2
+  }
+  else {
+      return 0
+  }
+}
+
+module.exports = problme1;
