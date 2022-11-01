f8a0c5f274ba4db83311fb5773ca83aebfaba56b
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 17:53:58 +0900

    [problem6] feat: 2.Append contigous letter to letters array

--- src/problem6.js
+++ src/problem6.js
@@ -5,6 +5,12 @@ function problem6(forms) {
   // 중복되는 닉네임을 가진 크루의 email 저장
   const resultArr = [];
 
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms[i][1].length - 1; j++) {
+      twoLetters.push(forms[i][1].substr(j, j + 2));
+    }
+  }
+
   return answer;
 }
 
