d45f0de75835f35567c9e601a1ad635da7f6f40f
SeungWoo Kim <osabero@naver.com>
2022-11-01 19:06:33 +0900

    일단 반복문을 중첩사용하여 닉네임을 자르고 includes를 이용하여 포함되어있는 닉네임을 찾고 아이디를 배열에 넣도록 구현하였습니다.

--- src/problem6.js
+++ src/problem6.js
@@ -1,7 +1,20 @@
 function problem6(forms) {
   var answer;
-  return answer;
+  var value = []
+  for(let i = 0;i < forms.length;i++){
+    if(forms[i][1].length == 1){continue}
+    for(let j = 0;j < forms[i][1].length - 1;j++){
+      const checkNick = forms[i][1].substr(j, 2)
+      for(let k = 0;k < forms.length;k++){
+        if (k == i){continue}
+        if (forms[k][1].includes(checkNick)) {
+          value.push(forms[k][0])
+        }}
+      }
+    }
   
+  return answer;
+
 }
 
 module.exports = problem6;
