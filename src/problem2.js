2b49e9a2c89bb9e32f99aa0bbd81f5d30cfae9b8
jeong-hyuk <jack1150@naver.com>
2022-11-01 23:02:11 +0900

    1주차문제

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = "";
+  let wordArr = cryptogram.split("");
+  let prevLength = wordArr.length;
+  let isEnd = false;
+
+  while (!isEnd) {
+    wordArr = wordArr.filter(
+      (el, index, arr) => el != arr[index - 1] && el != arr[index + 1]
+    );
+    if (prevLength === wordArr.length) isEnd = true;
+    else prevLength = wordArr.length;
+  }
+
+  for (let i = 0; i < wordArr.length; i++) {
+    answer += wordArr[i];
+  }
   return answer;
 }
 
