6233b5bb47bd752143c42191aeb822be5d59ed7b
Sallyvely <tpdus4159@naver.com>
2022-11-01 21:29:29 +0900

    7번 문제 방문자 점수  계산하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -14,6 +14,19 @@ function problem7(user, friends, visitors) {
     }
   }
 
+  for(let j=0; j<visitors.length;j++){
+    if(score.has(visitors[j])){
+      score.set(visitors[j], score.get(visitors[j])+1);
+    } else score.set(visitors[j],1);
+    for(let k=0; k<userFriends.length;k++){
+      if(score.has(userFriends[k])){
+        score.delete(visitors[j]);
+      }
+    }
+  }
+  score.forEach(function(value, key){
+    answer.push(key);
+  });
 
   return answer;
 }
