856ec4d68c397e5af9ea1b403bd895f7919803a1
soohyun_kim5 <93cherry@naver.com>
2022-11-01 19:17:08 +0900

    initial commit

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,25 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  arr = []
+  moneyType=[50000,10000,5000,1000,500,100,50,10,1]
+  arr.push(parseInt(money/50000))
+  money = money-arr[0]*50000
+  arr.push(parseInt(money/10000))
+  money = money-arr[1]*10000
+  arr.push(parseInt(money/5000))
+  money = money-arr[2]*5000
+  arr.push(parseInt(money/1000))
+  money = money-arr[3]*1000
+  arr.push(parseInt(money/500))
+  money = money-arr[4]*500
+  arr.push(parseInt(money/100))
+  money = money-arr[5]*100
+  arr.push(parseInt(money/50))
+  money = money-arr[6]*50
+  arr.push(parseInt(money/10))
+  money = money-arr[7]*10
+  arr.push(parseInt(money))
+
+  return arr
 }
 
 module.exports = problem5;
--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,20 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = new Set()
+  let length = forms.length
+
+  for (let i = 0; i < length; i++) {
+    for (let j = i + 1; j < length; j++) {
+      for (let k = 0; k < forms[i][1].length-1; k++) {
+        for (let l = 0; l < forms[j][1].length-1; l++) {
+          if (forms[i][1][k] === forms[j][1][l] && forms[i][1][k + 1] === forms[j][1][l + 1]) {
+            answer.add(forms[i][0])
+            answer.add(forms[j][0])
+          }
+        }
+      }
+    }
+  }
+  return Array.from(answer).sort();
 }
 
 module.exports = problem6;
