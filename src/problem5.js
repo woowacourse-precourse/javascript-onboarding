98143805bb76f42dcc77dcc36e3d8cf21348c71c
cham-min <hymn9613@gmail.com>
2022-11-01 13:35:12 +0900

    feat: 문제5 - 원화 갯수 결과 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = [];
+
+  for (let i = 0; i < MONEY.length; i++) {
+    answer.push(Math.floor(money / MONEY[i]));
+    money -= MONEY[i] * answer[i];
+  }
+
   return answer;
 }
 
