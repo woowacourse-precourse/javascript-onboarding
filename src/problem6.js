7395c9b2ee22e852d06056ac272b6d0b7bf08d86
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:48:23 +0900

    feat(prob6) : 반복 통해 nickname 중복에 대한 map value 수 증가

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,15 @@
+function setNicknameMap(map, nickname) {
+  for (let i = 0; i < nickname.length - 1; i++) {
+    let temp = nickname.slice(i, i + 2);
+    if (!map.has(temp)) {
+      map.set(temp, 1);
+    } else {
+      map.set(temp, map.get(temp) + 1);
+    }
+  }
+  return map;
+}
+
 function problem6(forms) {
   var answer;
   return answer;
