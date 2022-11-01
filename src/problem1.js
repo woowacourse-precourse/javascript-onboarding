755cac73c264d727c62727a2b3588f16f71307af
yiyujin <yujin94@gmail.com>
2022-11-01 21:07:50 +0900

    prob 1-5

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,61 @@
 //commit test
 function problem1(pobi, crong) {
   var answer;
+
+pArr = []
+cArr = []
+
+pCand = [];
+cCand = [];
+
+pMax = 0;
+cMax = 0;
+
+answer = 0;
+
+//0.예외처리 전체감싸기
+if(pobi[0] %2 != 0 && pobi[1]%2 == 0 && pobi[1]==pobi[0]+1){
+//무조건 아이템은 2개인 배열이니까, i < 2.
+  for (i = 0; i < 2; i++) {
+    //1. 숫자를 스트링으로 바꾼다.
+    //2. split한다
+    //3. 다시 숫자로 바꾼다.
+    digit = (pobi[i].toString().split(''))
+    pArr.push(digit.map(Number))
+  
+    digit = (crong[i].toString().split(''))
+    cArr.push(digit.map(Number))
+  }
+  
+  console.log(pArr,cArr)
+  
+  
+  for (i = 0; i < 2; i++) {
+    //아이템을 모두 더하기
+    add = pArr[i].reduce((a, b) => a + b, 0)
+    //아이템을 모두 곱하기
+    mul = pArr[i].reduce((a, b) => a * b, 1)
+    pCand.push(add,mul)
+    pMax = Math.max(...pCand)
+  
+    add = cArr[i].reduce((a, b) => a + b, 0)
+    mul = cArr[i].reduce((a, b) => a * b, 1)
+    cCand.push(add,mul)
+    cMax = Math.max(...cCand)
+  }
+  
+  //비교
+  if(pMax > cMax){
+    answer=1
+  }else if(pMax < cMax){
+    answer=2
+  }else if(pMax = cMax){
+    answer=0
+  }
+}else{
+  answer = -1;
+}
+
   return answer;
 }
 
--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,35 @@
 function problem3(number) {
   var answer;
+
+
+arr = [];
+arr2 = [];
+
+cnt = 0;
+
+// number는 1 이상 10,000 이하인 자연수이다.
+if (number >= 1 && number <= 10000) {
+  //1부터 n까지 array를 만든다.
+  arr = Array.from({ length: number }, (_, i) => i + 1)
+
+  //뭉터기 string으로 변환
+  str = arr.join('')
+
+  //1의 자리로 별개로 변환한 array
+  arr2 = Array.from(str.split(''), String);
+
+  //3,6,9를 포함하는지 체크 includes
+  for (i = 0; i < arr2.length; i++) {
+    if (arr2[i].includes('3') || arr2[i].includes('6') || arr2[i].includes('9')) {
+      cnt++;
+    }
+  }
+}
+
+answer = cnt
+
+// console.log(answer)
+
   return answer;
 }
 
