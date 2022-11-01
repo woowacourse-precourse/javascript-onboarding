36577ea654cf9cf179c9975b70a243060d86c6eb
rabbit-22 <chlwlal20@gmail.com>
2022-11-01 12:59:16 +0900

    problem 3 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,20 @@
+/* 
+기능 목록
+1. 숫자를 하나씩 잘라서 배열에 담기 
+2. 배열에 3,6,9가 들어있으면 answer 증가
+*/
+
 function problem3(number) {
-  var answer;
+  let answer = 0;
+
+  for(let i=1; i<=number;i++) {
+    let arr = String(i).split("");
+    for(let j=0; j<arr.length; j++) {
+      if(arr[j] === "3" || arr[j] === "6" || arr[j] === "9") {
+        answer++;
+      }
+    }
+  }
   return answer;
 }
 
