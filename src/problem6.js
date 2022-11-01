26505e9f23d5b21a43d9da0c09b05ef4c8803cd9
Ssoco <dlscjs8646@gmail.com>
2022-11-01 22:19:46 +0900

    p6: 연속되는 글자 체크

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,20 @@ function problem6(forms) {
 
   // 결과값
   const answer = [];
+
+  // 연속되는 글자들이 몇번 등장하는지 적어두기
+  const wordMap = new Map();
+
+  // wordMap 채우기
+  for (let i = 0; i < forms.length; ++i) {
+    for (let j = 0; j < forms[i][1].length - 1; ++j) {
+      // word 2개씩 자르기
+      const word = forms[i][1].substr(j, 2);
+      if (!wordMap.has(word)) wordMap.set(word, 1);
+      else wordMap.set(word, wordMap.get(word) + 1);
+    }
+  }
+
   return answer;
 }
 
