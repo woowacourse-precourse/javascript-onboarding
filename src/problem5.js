bee8434b31d52ab432411e9e178cd854ca1c7058
윤동주 <yoondj98@naver.com>
2022-11-01 21:36:53 +0900

    우아한 테크코스 프리코슥 1주차 5번 코드 주석 기입

--- src/problem5.js
+++ src/problem5.js
@@ -2,9 +2,9 @@ function problem5(money) {
   var money_category = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   var answer = [];
   for(var i=0; i<9; i++){
-    var quot = parseInt(parseInt(money)/parseInt(money_category[i]));
+    var quot = parseInt(parseInt(money)/parseInt(money_category[i])); /* 주어진 돈 단위로 나누어 나오는 몫(quot)이 해당 단위의 개수 */
     if(quot>0){
-      money = money%money_category[i];
+      money = money%money_category[i]; /* 나머지는 주어진 단위로 나누고 남은 돈 */
       answer.push(quot);
     }
     else answer.push(0);
