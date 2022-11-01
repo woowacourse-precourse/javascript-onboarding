e91cf4be103669b27fca866bae4184e678917ad9
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-01 20:57:25 +0900

    chore : problem6 주석 추가

--- src/problem6.js
+++ src/problem6.js
@@ -3,8 +3,9 @@ function problem6(forms) {
 
     let myMap = new Map();
     forms.forEach((form) => {
-        const [email, nickname] = form;
+        const [_, nickname] = form;
         for (let i = 0; i < nickname.length - 1; i++) {
+            // 닉네임을 두글자씩 분리
             const str = nickname.substring(i, i + 2);
             if (myMap.has(str)) myMap.set(str, myMap.get(str) + 1);
             else myMap.set(str, 1);
@@ -15,6 +16,8 @@ function problem6(forms) {
         const [email, nickname] = form;
         for (let i = 0; i < nickname.length - 1; i++) {
             const str = nickname.substring(i, i + 2);
+            // 겹치면 추가
+            // 이때 answer은 set 자료구조(중복 제거)
             if (myMap.get(str) > 1) answer.add(email);
         }
     });
