c9efda461a11134c9cb2563844512d4159167cb2
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 17:32:27 +0900

    feat:[문제7] friends배열중 나와 친구인 사람의 이름을 추출한다

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,15 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+function getFriendOfUser(user, friends) {
+  const FriendOfUser = friends.reduce((acc, friendship) => {
+    friendship.includes(user) &&
+      acc.push(...friendship.filter((friend) => friend !== user));
+    return acc;
+  }, []);
+  return FriendOfUser;
+}
+
+function problem7(user, friends) {
+  const friendOfUser = getFriendOfUser(user, friends);
+  return;
 }
 
 module.exports = problem7;
