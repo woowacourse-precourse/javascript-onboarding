50894cae6bc6907176b8479a8bdd354abc071142
kwh97 <zzzang0512@naver.com>
2022-11-01 16:20:50 +0900

    feat(problem6): 전체 연속문자 리스트와 중복된 연속문자 리스트를 얻는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -7,6 +7,25 @@ function problem6(forms) {
   for(let i=0; i<forms.length; i++) {
     nickNameList.push(forms[i][1]);
   }
+
+  for(let i=0; i<forms.length; i++) {
+    getOverlapList(nickNameList[i], totalList, overlapList);
+  }
+
+}
+
+function getOverlapList(str, totalList, overlapList) {
+  for(let i=1; i<str.length - 1; i++) {
+    let twoChar = str[i-1] + str[i];
+
+    if (totalList.includes(twoChar) === false) {
+      totalList.push(twoChar);
+    } 
+    
+    else if (overlapList.includes(twoChar) === false) {
+      overlapList.push(twoChar);
+    }
+  }
 }
 
 module.exports = problem6;
