dfa549b7beed490610e0cb0fddba06304b814d45
pitang <wpeffect@gmail.com>
2022-11-01 12:42:38 +0900

    3번 주석 제거 및 정리

--- src/problem3.js
+++ src/problem3.js
@@ -1,13 +1,13 @@
 function problem3(number) {
   let answer = 0;
 
-  // 1. [3, 6, 9] 포함된 정규식 변수 생성.
+  // [3, 6, 9] 포함된 정규식 변수 생성.
   let numberOfGame = /[3|6|9]/g;
 
   for(let i = 1; i <= number; i++) {
-    // 2. 체크해서 match 되는 숫자 저장.
+    // 체크해서 match 되는 숫자 저장.
     let numberOfMatch = (""+i).match(numberOfGame);
-    // 3. count 
+    // undefined가 아니라면 return 값에 길이만큼 더해줌. 
     if(numberOfMatch != undefined) answer += numberOfMatch.length;
   }
 
