1ebaa5295f64b9c01b7bedf2866ac3e94ee278f2
kimjiwon <hallu0317@gmail.com>
2022-11-01 15:37:44 +0900

    problem4 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,8 @@
 function problem4(word) {
   var answer = "";
-  
+
+  //청개구리사전은 A-Z, B-Y와 같이 첫값,끝값 쌍을 가진다.
+  //따라서 첫값과 끝값의 합에서 첫값이 나온다면 -끝값, 끝값이 나온다면 -첫값을 계산한다.
   for (let i = 0; i < word.length; i++) {
     if (word[i].charCodeAt() == 32) {
       answer += word[i];
