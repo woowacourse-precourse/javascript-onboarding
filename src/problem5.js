16299652a6b2e824d312b7ae30b77503c56c4546
Hayeong Kim <fibonacci2246@gmail.com>
2022-11-01 19:07:10 +0900

    problem 5 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,13 @@
 function problem5(money) {
-  var answer;
+  const moneyType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  const answer = moneyType.reduce((acc, curr) => {
+    const count = Math.floor(money / curr);
+    money = money % curr;
+    acc.push(count);
+    return acc;
+  }, []);
+
   return answer;
 }
 
