e1075941917e823e65b84510fcd6f1979f60796a
kimeodml <kimeodml@gmail.com>
2022-11-01 15:11:55 +0900

    feat: 문제6 해결

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,21 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  
+  let ans = new Set();
+  for(let i = 0;i < forms.length;i++){
+    for(let j = 0 ; j < forms.length; j++) {
+      if(i === j) continue
+      for(let start = 0; start < forms[i][1].length -2 ; start++){
+        for(let end = 2 + start; end < forms.length ; end++){
+          if(forms[j][1].indexOf(forms[i][1].substr(start,end)) !== -1){
+            ans.add(forms[j][0]);
+          }
+        }
+      }
+    }
+  }
+
+
+  return Array.from(ans).sort();
 }
 
 module.exports = problem6;
