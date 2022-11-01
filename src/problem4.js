177234605192c9a1fca411c8160c7afac0427b77
Raehyeon Jeong <raehy19@gmail.com>
2022-11-01 09:35:12 +0900

    feat(problem4) : 대문자 알파벳인지, 소문자 알파벳인지 확인하는 기능

--- src/problem4.js
+++ src/problem4.js
@@ -2,6 +2,14 @@ function isException(str) {
   return !(typeof str === 'string') || str.length > 1000 || str.length < 1;
 }
 
+function isUpper(charCode) {
+  return charCode >= 'A'.charCodeAt(0) && charCode <= 'Z'.charCodeAt(0);
+}
+
+function isLower(charCode) {
+  return charCode >= 'a'.charCodeAt(0) && charCode <= 'z'.charCodeAt(0);
+}
+
 function problem4(word) {
   if (isException(word)) return;
 }
