8914a33c8f63ed259075cff09c8aea19cc022a73
banma70 <banma1234@gmail.com>
2022-11-01 14:04:47 +0900

    problem_4 : 예외처리 함수 수정

--- src/problem4.js
+++ src/problem4.js
@@ -4,13 +4,8 @@ function problem4(word) {
 }
 
 function errorHandler(word) {
-  // 문자 쪼개기
-  var arr = (word + '').split('');
-  // 자리수가 1000을 넘을경우 1001번째 자리부터 끝까지 소거
-  if (arr.length > 1000)  arr = arr.splice(1000, arr.length - 1);
-
-  // 알파벳 판별은 아스키코드 변환 과정에서 손볼 예정
-  return arr;
+  // 1000의 자리를 넘을 경우 예외처리
+  if (word.length > 1000) return true;
 }
 
 module.exports = problem4;
