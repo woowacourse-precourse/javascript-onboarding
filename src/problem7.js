b859a6dcd5d0cbff61d6102a1b5ab358f37c7825
Ssoco <dlscjs8646@gmail.com>
2022-11-01 23:13:02 +0900

    p7: 친구 아닌 사람들에게 점수 부여하기

--- src/problem7.js
+++ src/problem7.js
@@ -16,6 +16,15 @@ function problem7(user, friends, visitors) {
     }
   }
 
+  // 아직 친구 아닌 사람들에게 점수 부여하기
+  // 혹시 내 친구를 아는 모르는 사람이라면? 점수부여!
+  others.forEach((elem) => {
+    if (userFriends.includes(elem[0]))
+      scores.set(elem[1], scores.get(elem[1]) + 10 || 10);
+    if (userFriends.includes(elem[1]))
+      scores.set(elem[0], scores.get(elem[0]) + 10 || 10);
+  });
+
   return answer;
 }
 
