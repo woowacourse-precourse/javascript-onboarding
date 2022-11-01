998379148fe5e59b47f6f40edb7ed1553d94227e
OrangeChick <retww@naver.com>
2022-11-01 19:51:57 +0900

    feat: 🎒[Problem2] Implement duplicated word removal function
    - 중복 문자를 제거하는 기능을 구현하였습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
 function problem2(cryptogram) {
   var answer;
+
+  for (let i = 0; i < splitWord.length; i++) {
+    if (splitWord[i] === splitWord[i + 1]) {
+      splitWord.splice(i, 2);
+      result = true;
+    }
+  }
+
   return answer;
 }
 
