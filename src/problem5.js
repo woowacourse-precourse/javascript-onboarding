6d30a331ba61c93916a052db92f32a29a6b79b7e
yeonkyoung <keep.ykk@gmail.com>
2022-11-01 17:30:18 +0900

    feat: Implement problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  const moneyUnit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (var i = 0; i < 9; i++) {
+    answer[i] = parseInt(money / moneyUnit[i]);
+    money = money % moneyUnit[i];
+  }
   return answer;
 }
 
