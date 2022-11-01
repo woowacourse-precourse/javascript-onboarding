eec6e9860be1253ed8a233ba8538024e19ae9cf4
loco9939 <kls9939@naver.com>
2022-11-01 12:52:17 +0900

    refactor: Create constant & Change name

--- src/problem4.js
+++ src/problem4.js
@@ -2,21 +2,27 @@ function problem4(word) {
   if (typeof word !== 'string') throw new TypeError('매개변수는 문자열 타입이여야 합니다.');
   if (!word.length || word.length > 1000) throw new RangeError('매개변수는 1 이상 1,000 이하의 문자열이여야 합니다.');
 
-  const splitWord = [...word];
-  const uppers = Array.from({length:26}, (_,i) => String.fromCharCode(65+i));
-  const lowers = Array.from({length:26}, (_,i) => String.fromCharCode(97+i));
+  const ALPHABET_LENGTH = 26;
+  const ASCII_NUMBER_CODE_UPPER_A = 65;
+  const ASCII_NUMBER_CODE_UPPER__Z = 90 ;
+  const ASCII_NUMBER_CODE_LOWER_A = 97;
+  const ASCII_NUMBER_CODE_LOWER__Z = 122;
+
+  const splitWords = [...word];
+  const uppers = Array.from({length:ALPHABET_LENGTH}, (_,i) => String.fromCharCode(ASCII_NUMBER_CODE_UPPER_A+i));
+  const lowers = Array.from({length:ALPHABET_LENGTH}, (_,i) => String.fromCharCode(ASCII_NUMBER_CODE_LOWER_A+i));
   
   const changeOppositeStr = str => {
     const codePosition = str.charCodeAt();
 
-    return (codePosition >= 65 && codePosition <= 90
-      ? [...uppers].reverse()[uppers.indexOf(str)] 
-      : codePosition >= 97 && codePosition <= 122 
-      ? [...lowers].reverse()[lowers.indexOf(str)] 
+    return (codePosition >= ASCII_NUMBER_CODE_UPPER_A && codePosition <= ASCII_NUMBER_CODE_UPPER__Z
+      ? [...uppers].reverse()[uppers.indexOf(str)]
+      : codePosition >= ASCII_NUMBER_CODE_LOWER_A && codePosition <= ASCII_NUMBER_CODE_LOWER__Z 
+      ? [...lowers].reverse()[lowers.indexOf(str)]
       : str);
   }
   
-  return splitWord.map(elem => changeOppositeStr(elem)).join('');
+  return splitWords.map(splitedWord => changeOppositeStr(splitedWord)).join('');
 }
 
 module.exports = problem4;
