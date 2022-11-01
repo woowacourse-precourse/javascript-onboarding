7cf33b95b9e77e22c62a03c1750d018a4a74770c
letsjo <hyunoh.jo@gmail.com>
2022-11-01 19:19:28 +0900

    [problem6] Rename variable and function name to be make readability

--- src/problem6.js
+++ src/problem6.js
@@ -1,42 +1,42 @@
 function problem6(forms) {
-  return duplicateCheckApp(validationArr(forms));
+  return duplicateCheckApp(checkValidation(forms));
 }
 
-function validationArr(CrewDataArr) {
-  const validationFilterArr = [];
-  const emailListArr = [];
-  CrewDataArr.map(([crewEmail,crewNickname]) => {
-    if (
-      crewEmail.length >= 11 &&
-      crewEmail.length < 20 &&
-      crewEmail.includes("@email") &&      
-      !emailListArr.includes(crewEmail) &&
-      crewNickname.length >= 2 &&
-      crewNickname.length < 20 &&
-      !/[^가-힣]/.test(crewNickname)
-    ) {
-      validationFilterArr.push([crewEmail,crewNickname]);
-      emailListArr.push(crewEmail);
+function checkValidation(forms) {
+  const validationFilter = [];
+  const emailCollection = [];
+  forms.map(([crewEmail, crewNickName]) => {
+    if (checkNickName(crewNickName) && checkEmail(crewEmail, emailCollection)) {
+      validationFilter.push([crewEmail, crewNickname]);
+      emailCollection.push(crewEmail);
     }
   });
-  return validationFilterArr;
+  return validationFilter;
 }
 
-function duplicateCheckApp(CrewDataArr){
-  const infoOfUsedTwoLetterMap = new Map();
-  const emailOfUsedNickNameSet = new Set();
-  CrewDataArr.forEach(([crewEmail,crewNickName]) => {
-    for (let i = 0; i <crewNickName.length -1; i++){
+function checkNickName(nickName) {
+  return nickName.length >= 2 && nickName.length < 20 && !/[^가-힣]/.test(nickName);
+}
+
+function checkEmail(email, emailCollection) {
+  return email.length >= 11 && email.length < 20 && email.includes("@email") && !emailCollection.includes(email);
+}
+
+function duplicateCheckApp(forms) {
+  const infoOfUsedTwoLetter = new Map();
+  const emailOfUsedNickName = new Set();
+  forms.forEach(([crewEmail, crewNickName]) => {
+    for (let i = 0; i < crewNickName.length - 1; i++) {
       let twoLetterName = crewNickName.substr(i, 2);
-      if (infoOfUsedTwoLetterMap.has(twoLetterName)){
-        emailOfUsedNickNameSet.add(crewEmail);
-        emailOfUsedNickNameSet.add(infoOfUsedTwoLetterMap.get(twoLetterName));
+      if (infoOfUsedTwoLetter.has(twoLetterName)) {
+        emailOfUsedNickName.add(crewEmail);
+        emailOfUsedNickName.add(infoOfUsedTwoLetter.get(twoLetterName));
       } else {
-        infoOfUsedTwoLetterMap.set(twoLetterName,crewEmail)
+        infoOfUsedTwoLetter.set(twoLetterName, crewEmail);
       }
     }
   });
-  return [...emailOfUsedNickNameSet].sort();
+  return [...emailOfUsedNickName].sort();
 }
 
 module.exports = problem6;
