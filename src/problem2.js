27cb733727aadb5da2fc184c59c6633a273f9dd8
sumi-0011 <selina2000@naver.com>
2022-11-01 16:31:02 +0900

    [problem2] refactoring : JSDoc 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,14 +1,24 @@
 const CONTINUOUS_CHAR_REGEX = /([a-z])\1+/g;
 
+/**
+ * 암호문 해독 : 연속하는 중복 문자들 제거
+ * @param {string} cryptogram
+ * @returns {string}
+ */
 const decryptedCryptogram = (cryptogram) => {
   return cryptogram.replace(CONTINUOUS_CHAR_REGEX, "");
 };
 
+/**
+ * @param {string} str
+ * @returns {boolean}
+ */
 const checkIsCryptogram = (str) => {
   return str.match(CONTINUOUS_CHAR_REGEX);
 };
 
 function problem2(cryptogram) {
+  /** @type {string} */
   let res = cryptogram;
 
   while (checkIsCryptogram(res)) {
