abaf07623fb7dec6377d5ef236dd119d24c962dc
yeeun426 <thsudkcla7@naver.com>
2022-11-01 22:22:25 +0900

    feat: 문제 7번 - 이미 친구인 경우

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,31 @@
 function problem7(user, friends, visitors) {
   var answer;
-  return answer;
+ // user의 이미 친구인 사람 찾기
+ let alreadyFriends = [];
+
+ for (i = 0; i < friends.length; i++){
+   if (friends[i].includes(user)) {
+     var friend = friends[i].filter(v => v !== user);
+     console.log(friend);
+
+     alreadyFriends[friend] = [];
+   }
+ }
+
+ return answer;
+
 }
 
-module.exports = problem7;
+// module.exports = problem7;
+console.log(problem7(
+  "mrko",
+  [
+    ["donut", "andole"],
+    ["donut", "jun"],
+    ["donut", "mrko"],
+    ["shakevan", "andole"],
+    ["shakevan", "jun"],
+    ["shakevan", "mrko"],
+  ],
+  ["bedi", "bedi", "donut", "bedi", "shakevan"]
+));
