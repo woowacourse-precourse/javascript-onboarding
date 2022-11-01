754eb2402df88f64102e6bda75f8cea09b705623
kwh97 <zzzang0512@naver.com>
2022-11-01 16:46:37 +0900

    feat(problem7): user 친구의 친구인 경우, 그 대상에게 10점을 부여하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,18 @@
 function problem7(user, friends, visitors) {
   const userFriends = getUserFriends(user, friends);
+  let score = {};
+
+  userFriends.forEach((name) => {
+    const recommendFriends = getUserFriends(name, friends).filter((friendName) => friendName !== user);
+    recommendFriends.forEach((name) => {
+      if(score[name]) {
+        score[name] = score[name] + 10;
+      } else {
+        score[name] = 10;
+      }
+    });
+  });
+
 }
 
 function getUserFriends(user, friends) {
