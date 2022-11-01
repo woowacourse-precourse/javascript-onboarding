146bdf637c2d94cea1ed417ba5fff710615be60c
durumi99 <kms990615@naver.com>
2022-11-01 15:44:49 +0900

    [problem 7] 함께 아는 친구의 수만큼 추가한 점수를 Map 객체로 return 하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -9,9 +9,29 @@ function findUserFriends(user, friends){
   }
   return set;
 }
+// 함께 아는 친구의 수만큼 점수를 추가하여 return 하는 함수
+function findSameFriends(user, friends, userFriends){
+  const map = new Map();
+  for(friend of friends){
+    if(friend[0] != user && userFriends.has(friend[1])){
+      if(!map.has(friend[0]))
+        map.set(friend[0], 10);
+      else
+        map.set(friend[0], map.get(friend[0])+10)
+    }
+    else if(friend[1] != user && userFriends.has(friend[0])){
+      if(!map.has(friend[1]))
+        map.set(friend[1], 10);
+      else
+        map.set(friend[1], map.get(friend[1])+10);
+    }
+  }
+  return map;
+}
 function problem7(user, friends, visitors) {
   var answer;
   let userFriends = findUserFriends(user, friends);
+  let recommendPoints = findSameFriends(user, friends, userFriends);
   
   return answer;
 }
