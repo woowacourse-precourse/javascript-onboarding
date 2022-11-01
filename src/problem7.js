bbe7ac5a6bbf942a4e6c8250a1d9a6701d699370
HyeJun <solar3070@naver.com>
2022-11-01 20:45:23 +0900

    feat: problem7/추천 점수가 가장 높은 순으로 정렬 점수가 같은 경우는 이름순으로 정렬

--- src/problem7.js
+++ src/problem7.js
@@ -19,6 +19,17 @@ function problem7(user, friends, visitors) {
       score[visitor] = (score[visitor] || 0) + 1;
     }
   });
+
+  const sortedScore = Object.entries(score).sort(
+    ([name1, score1], [name2, score2]) => {
+      if (score2 - score1 === 0) {
+        if (name2 > name1) return -1;
+        else if (name1 > name2) return 1;
+        else return 0;
+      }
+      return score2 - score1;
+    }
+  );
 }
 
 module.exports = problem7;
