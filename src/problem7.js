0b942297566914e372d297746e673cf48909e4bb
하송희 <99heesong@naver.com>
2022-11-01 20:47:53 +0900

    [문제 7] 기능추가: 입력 friends 배열에서 걸러낸 요소들의 name, score Map 정보 저장

--- src/problem7.js
+++ src/problem7.js
@@ -7,6 +7,19 @@ function problem7(user, friends, visitors) {
       })
     }
   })
+
+  const scoreMap = new Map();
+  friends.forEach((friendArr) => {
+    friendArr.forEach((name, idx) => {
+      if(name!==user && userFriends.includes(name)) {
+        const recommendedFr = friendArr[1-idx];
+        if(recommendedFr!==user) {
+          if(scoreMap.has(recommendedFr)) scoreMap.set(recommendedFr, scoreMap.get(recommendedFr)+10);
+          else scoreMap.set(recommendedFr, 10);
+        }
+      }
+    })
+  })
 }
 
 module.exports = problem7;
