5ddd26185092498f37cdf70f9374496fa2370eb7
RooT_Kim <bdd3452@gmail.com>
2022-11-01 13:04:11 +0900

    문제3번) 기능목록 수정, solution 함수 선언 및 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,8 +1,10 @@
 function problem3(number) {
-	return clap(number);
+	//1부터 number까지 루프를 돌리고 결과를 반환하는 함수 -> solution
+	//숫자에 3,6,9가 속해있는지 체크하여 박수의 횟수를 반환하는 함수 -> countClap
+	return solution(number);
 }
 
-function clap(number) {
+function solution(number) {
 	let sum = 0;
 
 	for (let i = 1; i <= number; i++) {
@@ -18,8 +20,8 @@ function countClap(number) {
 	number
 		.toString()
 		.split('')
-		.forEach((el) => {
-			count += [3, 6, 9].includes(Number(el)) ? 1 : 0;
+		.forEach((digit) => {
+			count += [3, 6, 9].includes(Number(digit)) ? 1 : 0;
 		});
 
 	return count;
