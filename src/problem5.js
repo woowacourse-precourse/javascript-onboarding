25e3944de2abe4a7ff970638e8485766be4c4465
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 16:56:34 +0900

    feat : 조건문을 통하여 주어진 기능을 만족하는 함수 구현.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,38 @@
 function problem5(money) {
-  var answer;
-  return answer;
+    var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+
+    while (true) {
+        if (money < 10) {
+            answer[8] = money;
+            break;
+        } else if (money < 50) {
+            answer[7] = Number.parseInt(money / 10);
+            money %= 10;
+        } else if (money < 100) {
+            answer[6] = Number.parseInt(money / 50);
+            money %= 50;
+        } else if (money < 500) {
+            answer[5] = Number.parseInt(money / 100);
+            money %= 100;
+        } else if (money < 1000) {
+            answer[4] = Number.parseInt(money / 500);
+            money %= 500;
+        } else if (money < 5000) {
+            answer[3] = Number.parseInt(money / 1000);
+            money %= 1000;
+        } else if (money < 10000) {
+            answer[2] = Number.parseInt(money / 5000);
+            money %= 5000;
+        } else if (money < 50000) {
+            answer[1] = Number.parseInt(money / 10000);
+            money %= 10000;
+        } else if (money > 50000) {
+            answer[0] = Number.parseInt(money / 50000);
+            money %= 50000;
+        }
+    }
+
+    return answer;
 }
 
 module.exports = problem5;
