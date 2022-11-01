239247a88c16f218a01a2d67b204c09368be1445
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-01 16:35:26 +0900

    feat: [problem2] solution 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,20 @@
 function problem2(cryptogram) {
   var answer;
+  const text = cryptogram.trim().split("");
+
+  for (let i = 0; i < text.length; ) {
+     // 암호문의 연속된 두자리가 같다면, 두 문장 모두 삭제
+  if (text[i] == text[i + 1]) {
+  text.splice(i, 1);
+  text.splice(i, 1);
+  // 처음부터 다시 검사하기 위해 변수 초기화
+  i = 0;
+  } else {
+    i++;
+  }
+  }
+  console.log(`result = ${text}`);
+  answer = text;
   return answer;
 }
 
