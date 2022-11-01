e1aaebcab4712de26f3f72a65d8c29357043237b
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:47:55 +0900

    [problem7] feat : 사람이름만 갖는 배열 생성
    friends에서 중복을 전부 제거하고 거론된 이름만을 저장하는 배열을 새로 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,14 @@
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
+  let friendName = []; //이름만 담는 리스트 friendName
+  friends.forEach(function (item) {
+    friendName.push(item[0]);
+    friendName.push(item[1]);
+  });
+  friendName = friendName.filter(
+    (item, pos) => friendName.indexOf(item) === pos
+  ); //friendName 에 중복제거
 }
 
 module.exports = problem7;
