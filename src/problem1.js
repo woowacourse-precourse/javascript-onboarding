634062f4d3371bfcef71e3657f4e1bb29e965c34
MunYongJang <dltk456@naver.com>
2022-11-01 18:45:55 +0900

    problem1 : 점수 계산 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,21 @@
 function problem1(pobi, crong) {
   var answer;
-  return answer;
+
+  var pobiScore = Math.max(scoreCal(pobi[0]), scoreCal(pobi[1]));
+  var crongScore = Math.max(scoreCal(crong[0]), scoreCal(crong[1]));
+}
+
+//점수 계산 함수
+function scoreCal(n1) {
+  var plusVal = 0;
+  var multiVal = 1;
+
+  for (var num of n1.toString()) {
+    plusVal += parseInt(num);
+    multiVal *= parseInt(num);
+  }
+
+  return Math.max(plusVal, multiVal);
 }
 
 module.exports = problem1;
