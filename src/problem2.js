71efc443906f9421586a45dfc7eac7a16545e20c
ksone02 <eksovus@gmail.com>
2022-11-01 18:07:59 +0900

    [feat] problem2: 정규 표현식으로 연속하는 중복 문자들 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
+/*
+  기능 목록
+  1. 연속하는 중복 문자들을 반복해서 삭제
+*/
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let regExp = new RegExp(/(.)\1+/);
+  while (regExp.test(cryptogram)) {
+    cryptogram = cryptogram.replace(regExp, "");
+  }
+  return cryptogram;
 }
 
 module.exports = problem2;
