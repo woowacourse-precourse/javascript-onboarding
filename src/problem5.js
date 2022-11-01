8de9bdc31bcd5c03808abf5e5eede7eb5dff38ff
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:39:46 +0900

    [problem5] feat: 제시된 값을 몫과 나머지로 변경해가며 결과반환

--- src/problem5.js
+++ src/problem5.js
@@ -3,6 +3,12 @@ function problem5(money) {
   return answer;
   currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   currencyNew = [];
+  currency.forEach(function (item) {
+    currencyNew.push(parseInt(money / item));
+    money = money % item;
+  });
+  console.log(currencyNew);
+  return currencyNew;
 }
 
 module.exports = problem5;
