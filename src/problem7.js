a2eabf07aae3aa338d9cb3fe64787f1c425acf1c
osdoonhyun <skw1996@naver.com>
2022-11-01 19:22:33 +0900

    [문제7] feat: 기능3 중복 사용자 제거 후 점수 할당한 friends, visitors 합치기 구현

--- src/problem7.js
+++ src/problem7.js
@@ -33,12 +33,26 @@ function getScore(friendArr, crew, score) {
 
   return crewScoreArr;
 }
+// 기능 3
+function joinCrew(friendsRecommendArr, visitors) {
+  const joinCrew = {};
+  for (let friend in getScore(friendsArr, friendsRecommendArr, 10)) {
+    joinCrew[friend] = getScore(friendsArr, friendsRecommendArr, 10)[friend];
+  }
+  for (let visitor in getScore(friendsArr, visitors, 1)) {
+    joinCrew[visitor] = getScore(friendsArr, visitors, 1)[visitor];
+  }
+  return joinCrew;
+}
 function problem7(user, friends, visitors) {
   var answer;
   let friendsArr = findFriends(user, friends);
   let friendsRecommendArr = friendsRecommend(user, friends, friendsArr);
-  getScore(friendsArr, friendsRecommendArr, 10);
-  getScore(friendsArr, visitors, 1);
+  answer = joinCrew(
+    getScore(friendsArr, friendsRecommendArr, 10),
+    getScore(friendsArr, visitors, 1)
+  );
+
   return answer;
 }
 
@@ -49,5 +63,5 @@ module.exports = problem7;
   1-1. friends에서 user가 들어간 요소를 찾고 user의 친구를 새로운 배열에 할당한다.
   1-2. user의 친구의 친구를 찾고 점수를 부여하여 추천 사용자 아이디와  새로운 배열을 만든다.
 2. 배열에서 중복된 사용자를 삭제하고 알맞은 점수를 할당하는 기능 
-3. 점수가 높은 순으로 정렬, 추천 점수가 같다면 이름순으로 정렬하는 기능
+3. 중복 사용자 제거 후 점수 할당한 friends와 visitors 합치는 기능
 */
