a47a7b720a25bb7dee7799ea33edcf9ba72f1e89
Sangmin Lee <gink.ss00@gmail.com>
2022-11-01 16:02:13 +0900

    [problem3] feat: count369 함수 구현
    숫자를 넣으면 3, 6, 9가 얼마나 들어있는지 출력해준다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,3 +1,16 @@
+function count369(num) {
+  const stringNum = num.toString();
+  const compareArr = ["3", "6", "9"];
+  let count = 0;
+
+  for (let i = 0; i < stringNum.length; i++) {
+    if (compareArr.includes(stringNum[i])) {
+      count++;
+    }
+  }
+  return count;
+}
+
 function problem3(number) {
   var answer;
   return answer;
