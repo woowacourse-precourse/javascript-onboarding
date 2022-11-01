9755ade8f53714c80ca59cec0c1a3c6082c350b4
Jiwoo Park <jiwoo9446@naver.com>
2022-11-01 17:17:05 +0900

    refactor: problem2 split module by function

--- src/problem2.js
+++ src/problem2.js
@@ -1,20 +1,28 @@
+function findDuplicated(str) {
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) return true;
+  }
+  return false;
+}
+
+function deleteDuplicated(str) {
+  let tmpStr = "";
+  for (let i = 0; i < str.length; i++) {
+    if (str[i] == str[i + 1]) {
+      let cnt = 0;
+      while (str[i] == str[i + cnt]) cnt++;
+      i += cnt - 1;
+    } else tmpStr += str[i];
+  }
+  return tmpStr;
+}
+
 function problem2(cryptogram) {
   let answer = cryptogram;
-  let duplicated = false;
-  while (true) {
-    duplicated = false;
-    let n = answer.length;
-    let res = "";
-    for (let i = 0; i < n; i++) {
-      if (answer[i] == answer[i + 1]) {
-        duplicated = true;
-        let cnt = 0;
-        while (answer[i] == answer[i + cnt]) cnt++;
-        i += cnt - 1;
-      } else res += answer[i];
-    }
-    answer = res;
-    if (!duplicated) break;
+  let duplicated = findDuplicated(answer);
+  while (duplicated) {
+    answer = deleteDuplicated(answer);
+    duplicated = findDuplicated(answer);
   }
   return answer;
 }
