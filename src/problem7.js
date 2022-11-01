6bcfbcf98d121e205bf795d88e4138b2412f577f
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 15:56:05 +0900

    Problem7: 기능 목록 작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  /*
+  기능 목록
+  1. user의 친구 목록 생성 (추후 친구의 친구 매핑 위해 객체로)
+  2. 추천할 타겟 목록 생성
+  3. user의 친구의 친구 찾아서 친구 목록 키에 대해 매핑 및 타겟 목록에 추가하여 점수 +10
+  4. 방문자 중 친구가 아닌 사람을 타겟에 추가 및 점수 +1
+  5. 점수 내림차순 및 점수 동일 시 사전순 정렬 로직 구현
+  6. 추천할 타겟들의 이름을 정렬 로직으로 정렬하여 반환
+  */
 }
 
 module.exports = problem7;
