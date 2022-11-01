4d38c72a10e05d8204f1adc03e4afcecd47cdc06
SorinKwon <ksl020700@gmail.com>
2022-11-01 14:49:00 +0900

    feat(problem7): user와 친구인 아이디를 탐색하는 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,17 @@
+function alreadyFriend(user, friends) {
+  let result = [];
+  for (let i = 0; i < friends.length; i++) {
+      if (user == friends[i][0]) {
+          result.push(friends[i][1]);
+      }
+      if (user == friends[i][1]) {
+          result.push(friends[i][0]);
+      }
+  }
+  return result;
+}
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
   return answer;
 }
-
 module.exports = problem7;
