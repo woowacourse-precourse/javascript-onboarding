79e66f24ece4149d7ef769c0c5eef35a38fa9cb1
NYeonK <skdus728@gmail.com>
2022-11-01 23:28:22 +0900

    [ feat ] word를 반대로 변환하는 기능 구현 #PROBLEM4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,26 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  const dictionary = [
+    'A', 'B', 'C', 'D', 'E', 'F',
+    'G', 'H', 'I', 'J', 'K', 'L',
+    'M', 'N', 'O', 'P', 'Q', 'R',
+    'S', 'T', 'U', 'V', 'W', 'X',
+    'Y', 'Z'
+  ];
+
+  const findABC = (element) => {
+    for(let i=0; i<dictionary.length; i++){
+      if(element === dictionary[i]) return dictionary[25-i];
+    }
+  }
+
+  const treeFrog = word.split('').map(el => {
+    if(el !== " ") el = findABC(el.toUpperCase());
+    return el;
+  }).join('');
+  console.log(treeFrog);
+
+  return treeFrog;
 }
+problem4("I love you");
 
 module.exports = problem4;
