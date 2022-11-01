0d50225764ffc3430c7a9f16fdab54285fd08929
Sallyvely <tpdus4159@naver.com>
2022-11-01 21:20:13 +0900

    3번 문제 369 갯수 체크하는 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,16 @@
 function problem3(number) {
   var answer;
+  let count=0;
+  for(let i=0;i<=number;i++){
+    let current=String(i);
+    for(let j=0;j<current.length;j++){
+      let x=current.charAt(j);
+      if(x=='3'||x=='6'||x=='9'){
+        count+=1;
+      }
+  }
+}
+  answer=count;
   return answer;
 }
 
