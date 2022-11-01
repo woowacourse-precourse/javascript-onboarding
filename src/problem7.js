8d27ae49727d9ca7d88ec7da261878b5e49738f3
loco9939 <kls9939@naver.com>
2022-11-01 20:03:26 +0900

    feat: Create business logic
    - 유저의 친구들을 forEach, filter 메서드로 구하였습니다.
    - 유저의 친구의 친구들을 forEach 문의 중첩과 filter 메서드로 구하였습니다.
    - forEach 메서드를 이용해 점수를 계산해주었습니다.
    - 제한 조건에 맞도록 정렬을 해주었습니다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,52 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  
+  // 유저의 친구들
+  let userFriends = []
+  friends.forEach(friend => {
+    if (friend.includes(user)) userFriends = [...userFriends, ...friend];
+  })
+  userFriends = userFriends.filter(friend => friend !== user);
+
+  // 유저 친구의 친구들 => 알 수도 있는 친구들
+  let mayKnowUsers = [];
+  friends.forEach(friend => {
+    userFriends.forEach(userFriend => {
+      if (friend.includes(userFriend)) mayKnowUsers = [...mayKnowUsers, ...friend];
+    });
+  });
+
+  mayKnowUsers = mayKnowUsers.filter(mayKnowUser => {
+    return mayKnowUser !== user && !userFriends.includes(mayKnowUser)
+  })
+
+  // 유저 점수 계산
+  const userCount = {};
+  mayKnowUsers.forEach(mayKnowUser => {
+    userCount[mayKnowUser] = (userCount[mayKnowUser] ?? 0) + 10
+  })
+
+  visitors.forEach(visitor => {
+    if (visitor === user || userFriends.includes(visitor)) return;
+    userCount[visitor] = (userCount[visitor] ?? 0) + 1;
+  });
+
+  // 점수 기준 내림차순 정렬
+  const sortFunc = (userA, userB) => {
+    const numA = userCount[userA];
+    const numB = userCount[userB];
+    
+    if (numA - numB > 0) return -1;
+    else if (numA - numB < 0) return 1;
+    else if (numA - numB === 0) {
+      return userA - userB > 0 ? -1 : 1;
+    }
+  }
+
+  const userId = Object.keys(userCount);
+
+  return userId.sort((userA,userB) => sortFunc(userA,userB));
+  
+
 }
 
 module.exports = problem7;
