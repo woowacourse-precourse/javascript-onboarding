9d44dd18e2c5b9a6e0495afc587bad541f0f40f2
BB-choi <choibobae0809@gmail.com>
2022-11-01 12:35:41 +0900

    refactor: 문제6 - 리팩토링
    - 불필요한 코드 제거
    - 변수명 변경

--- src/problem6.js
+++ src/problem6.js
@@ -11,11 +11,11 @@ const extractWord = (nickname, characterLength = 2) => {
 };
 
 const getUniqueEmails = (emails) => {
-  const uniqueEmails = [...new Set(emails)].sort();
+  const uniqueEmails = [...new Set(emails)];
 
   return uniqueEmails;
 };
-
+// 함수명 변경, 인자 수
 const countWord = ({ wordSet, wordMap, user: { email, nickname } }) => {
   for (const word of wordSet) {
     if (wordMap.has(word)) {
@@ -51,26 +51,43 @@ const createNicknameWordMap = (forms) => {
   return nicknameWordMap;
 };
 
-const getDuplicateNicknameUserInfo = (nicknameWordMap, info = "email") => {
+const getUserData = {
+  email(userInfoList) {
+    return getUniqueEmails(userInfoList);
+  },
+};
+
+const getDuplicateNicknameUserData = (nicknameWordMap, data = "email") => {
   const UNIQUE_NAME_USER_COUNT = 1;
   const result = [];
 
   for (const [, { count, users }] of nicknameWordMap) {
     if (count > UNIQUE_NAME_USER_COUNT) {
-      users.forEach((user) => result.push(user[info]));
+      users.forEach((user) => result.push(user[data]));
     }
   }
 
-  return getUniqueEmails(result);
+  return getUserData[data](result);
 };
 
 const problem6 = (forms) => {
   /* {"단어" => {count: 0, users : [{email, 닉네임}, {email,닉네임}]}} */
   const nicknameWordMap = createNicknameWordMap(forms);
   const duplicateNicknameUserEmails =
-    getDuplicateNicknameUserInfo(nicknameWordMap);
+    getDuplicateNicknameUserData(nicknameWordMap);
+  const sortedUserEmailList = duplicateNicknameUserEmails.sort();
 
-  return duplicateNicknameUserEmails;
+  return sortedUserEmailList;
 };
+
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
 
 module.exports = problem6;
