1b8e90d979eff0383541682074e652afdb3b9809
emma <jhj191058@gmail.com>
2022-11-01 19:40:36 +0900

    problem06 수정 후 재 업데이트

--- src/problem6.js
+++ src/problem6.js
@@ -1,21 +1,7 @@
 function problem6(forms) {
-
-    answer=new Array();
-  
-   let checkList = [];
-   forms.map((elements) => {
-    nickName = elements[1];
-  
-    for(i =0; i<nickName.length-1; i++) {
-      checkList.push(nickName.substr(i,2))
-    }
-   })
-  
-   let arr = checkList.filter((element, index) => checkList.indexOf(element) != index);
-   arr = new Set(arr);
-   arr = [...arr]
-   return arr;
-  
-  }
- 
-  module.exports = problem6;
+    var answer = [];
+    var arr = [];
+    return answer;
+    
+}
+module.exports = problem6;
