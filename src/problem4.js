8c0c5265f33a75d2f239f8df30f3ccce89c173c0
wjdtjdgns <person3275@gmail.com>
2022-11-01 20:26:56 +0900

    변환된 문자를 반환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  
+  const answer = changeString(word);
   return answer;
+}
+
+// 문자를 한글자씩 잘라서 convert 함수에 넣어준 뒤 변환된 문자를 반환하는 함수
+function changeString(word) {
+  let convertStr = "";
+  for(let i=0; i<word.length; i++) {
+    convertStr += convert(word.charAt(i));
+  }
+  return convertStr;
 }
 
 // 입력받은 character를 변환하여 반환하는 함수
