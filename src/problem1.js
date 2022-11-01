bc9749dfa744a7d860ca58393efb144c0453dd49
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 19:21:41 +0900

    refactor:[문제1] 예외는 전부 -1이 출력되도록 해준다

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,18 @@
-function problem1(money) {
-  var answer;
+let pobiMax = [];
+let crongMax = [];
+
+const exception = (pobi, crong) => {
+  if (pobi[0] === "" || pobi[1] === "") return -1;
+  if (crong[0] === "" || crong[1] === "") return -1;
+  if (pobi[0] % 2 === 0 || pobi[1] % 2 !== 0) return -1;
+  if (crong[0] % 2 === 0 || crong[1] % 2 !== 0) return -1;
+  if (pobi.reduce((a, b) => Math.abs(a - b)) !== 1) return -1;
+  if (crong.reduce((a, b) => Math.abs(a - b)) !== 1) return -1;
+};
+
+function problem1(pobi, crong) {
+  pobiMax, (crongMax = []);
+  if (exception(pobi, crong) === -1) return -1;
   return answer;
 }
 
