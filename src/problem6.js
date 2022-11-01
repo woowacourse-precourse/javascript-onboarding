ce543b230f667ecde00b6a50564ca96203350e39
SALT0601 <thfxm0601@gmail.com>
2022-11-01 15:23:54 +0900

    feat: problem6 완료
    3중 for문 사용. 함수를 좀 더 리팩토링 할 필요성을 느낌

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,27 @@
 function problem6(forms) {
-  var answer;
+  //1.forms의 각 닉네임을 두글자씩 빼서 자신을 제외한 나머지 닉넴과 비교
+  //2.만약 존재한다면 이메일이 answer에 있는지 한번 더 확인(중복제거위함)
+  //3.이메일이 중복되지 않는다면 answer에 추가
+  //4.오름차순으로 정렬 
+  let answer = [];
+  for(let i=0; i<forms.length;i++){
+    for(let j=0; j<forms[i][1].length-1;j++){ 
+      let two = forms[i][1].slice(j,j+2);
+      for(let k=0; k<forms.length;k++){
+        if(i===k) continue;
+        else{
+          if(forms[k][1].includes(two)){
+            if(answer.includes(forms[k][0])) continue;
+            else{
+              answer.push(forms[k][0]);
+            }
+          }
+        }
+      }
+    }
+
+  }
+  answer.sort();
   return answer;
 }
 
