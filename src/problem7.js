6b9f8e4779d1ae471fe7a342b64b74aee0e52b9c
hvvany <kjunhwan1998@gmail.com>
2022-11-01 21:16:10 +0900

    feat(build): make friend dictionary

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,22 @@
 function problem7(user, friends, visitors) {
   let answer = [];
+  let friend_dic = {};
+  let friend_suggest = {};
+  let new_friends;
+  for (let friend of friends) {
+    for (let i=0; i < 2; i++) {
+      if (friend[1-i] != user) {
+        if (friend[i] in friend_dic) {
+          friend_dic[friend[i]].push(friend[1-i])
+        }
+        else {
+          friend_dic[friend[i]] = [friend[1-i]]
+        }
+      }
+    }
+  }
+
+  
   return answer;
 }
 
