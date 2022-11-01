374fd3d996a0b201581258c99ead40dd13218f65
Tory <88531407+1two13@users.noreply.github.com>
2022-11-01 11:19:20 +0900

    refactor(problem2): answerLastWord 변수 추가

--- src/problem2.js
+++ src/problem2.js
@@ -6,15 +6,15 @@ function problem2(cryptogram) {
   let answer = "";
   // 바로 직전 문자
   let lastStr = "";
+  // answer의 마지막 문자
+  let answerLastWord = answer[answer.length - 1];
 
   for (let i = 0; i < cryptogram.length; i++) {
-    if (
-      answer[answer.length - 1] !== cryptogram[i] &&
-      lastStr !== cryptogram[i]
-    ) {
+    if (answerLastWord !== cryptogram[i] && lastStr !== cryptogram[i]) {
       answer += cryptogram[i];
-      lastStr = cryptogram[i];
     } else answer = answer.slice(0, -1);
+
+    lastStr = cryptogram[i];
   }
 
   return answer;
