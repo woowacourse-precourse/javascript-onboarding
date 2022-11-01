a20d1bac7dbd40649cc7ceb67849081ceb53b57a
yb8350 <yb8350@naver.com>
2022-11-01 21:33:33 +0900

    Refactor: problem6-같은 글자가 연속적으로 포함되었는지 판단하는 코드를 함수로 변경

--- src/problem6.js
+++ src/problem6.js
@@ -1,21 +1,25 @@
 function problem6(forms) {
   var answer = [];
 
-  let wordList = new Map();
-  let overlapSet = new Set();
+  const wordList = new Map();
+  const overlapSet = new Set();
 
   for (let i = 0; i < forms.length; i++) {
-    if (forms[i][1].length === 1) continue;
-    for (let j = 0; j < forms[i][1].length - 1; j++) {
-      let word = forms[i][1].slice(j, j + 2);
-      if (wordList.get(word) !== undefined)
-        overlapSet.add(wordList.get(word)).add(i);
-      else wordList.set(word, i);
-    }
+    const nickname = forms[i][1];
+    if (nickname.length === 1) continue;
+    overlapCheck(nickname, i, wordList, overlapSet);
   }
 
   answer = overlapEmail(forms, overlapSet);
   return answer;
+}
+
+function overlapCheck(nickname, index, wordList, overlapSet) {
+  for (let j = 0; j < nickname.length - 1; j++) {
+    const word = nickname.slice(j, j + 2);
+    if (wordList.get(word) === undefined) wordList.set(word, index);
+    else overlapSet.add(wordList.get(word)).add(index);
+  }
 }
 
 function overlapEmail(forms, overlapSet) {
