505f48f3deebb917e5411a82433108a998420452
mumuhi <whtjdus97@naver.com>
2022-11-01 23:07:54 +0900

    369 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,17 @@
 function problem3(number) {
   var answer;
+  let cnt = 0;
+  for(let i=1; i <= number; i++){
+    let ss = i;
+    while (ss != 0) {
+      if(ss % 10 == 3 || ss % 10 == 6 || ss % 10 == 9){
+        cnt++;
+      }
+      ss /= 10;
+    }
+  }
+ 
+  answer = cnt;
   return answer;
 }
 
