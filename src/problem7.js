97eb37a42fc6cadad6f976a8b29dfaf900f40571
SorinKwon <ksl020700@gmail.com>
2022-11-01 22:15:50 +0900

    refactor(problem7): 변수명 수정, 함수명 수정, 코드 가독성 개선

--- src/problem7.js
+++ src/problem7.js
@@ -1,18 +1,17 @@
 function alreadyFriend(user, friends) {
-  let result = [];
+  let user_friends = [];
   for (let i = 0; i < friends.length; i++) {
       if (user == friends[i][0]) {
-          result.push(friends[i][1]);
+          user_friends.push(friends[i][1]);
       }
       if (user == friends[i][1]) {
-          result.push(friends[i][0]);
+          user_friends.push(friends[i][0]);
       }
-  }
-  return result;
+  }return user_friends;
 }
 function shareFriend(user,friends){
-  const share_friends=[];
-  const user_friends= alreadyFriend(user,friends);
+  let share_friends=[];
+  let user_friends= alreadyFriend(user,friends);
   for(let i=0;i<user_friends.length;i++){
       for(let j=0;j<friends.length;j++){
           if(user_friends[i]==friends[j][0]&&friends[j][1]!=user){
@@ -22,52 +21,42 @@ function shareFriend(user,friends){
               share_friends.push(friends[j][0]);
           }
       }
-      
-  }
-  return share_friends;
+  }return share_friends;
 }
 function userVisit(user, friends, visitors){
-  let result=[];
+  let user_visitors=[];
   const user_friends= alreadyFriend(user,friends);
   for(let i=0;i<visitors.length;i++){
           if(!user_friends.includes(visitors[i])){
-              result.push(visitors[i]);
+            user_visitors.push(visitors[i]);
           }
-  }
-  return result;
+  }return user_visitors;
 }
-function countScore(share,visit){
+function countScore(share_user,visit_user){
   let score=[];
-  for(let i=0;i<share.length;i++){
+  for(let i=0;i<share_user.length;i++){
       for(let j=0;j<10;j++){
-          score.push(share[i]);
+          score.push(share_user[i]);
       }
   }
-  for(let i=0;i<visit.length;i++){
-          score.push(visit[i]);
-      
+  for(let i=0;i<visit_user.length;i++){
+          score.push(visit_user[i]);
   }
-  const result = score.reduce((accu,curr)=>{
-      accu.set(curr,(accu.get(curr)||0)+1);
-      return accu;
+  let user_score = score.reduce((acc,cur)=>{
+      acc.set(cur,(acc.get(cur)||0)+1);
+      return acc;
   },new Map());
-  let arr=[];
-  let sorted = new Map([...result].sort((a, b) => b[1] - a[1]));
-  for(let[key,value] of sorted){
-      arr.push(key);
-  }
-  return arr;
+  let recommend_user=[];
+  let sort = new Map([...user_score].sort((a, b) => b[1] - a[1]));
+  for(let[key,value] of sort){
+      recommend_user.push(key);
+  }return recommend_user;
 }
-
-
-
 function problem7(user, friends, visitors) {
   let answer;
-  let visit = userVisit(user, friends, visitors);
-  let share=shareFriend(user, friends, visitors);
-  answer=countScore(share,visit);
+  let visit_user = userVisit(user, friends, visitors);
+  let share_user=shareFriend(user, friends, visitors);
+  answer=countScore(share_user,visit_user);
   return answer;
 }
-
-
 module.exports = problem7;
