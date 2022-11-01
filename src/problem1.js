b5ef15b6fca270d9d0fd6e3631bf0a4fe6ba0582
wjdtjdgns <person3275@gmail.com>
2022-11-01 19:13:19 +0900

    각 자리 숫자를 모두 곱하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -12,4 +12,14 @@ function digitPlus(digit) {
   return result;
 }
 
+// 각 자리 숫자를 모두 곱해 그 결과를 반환하는 함수
+function digitMult(digit) {
+  let result = digit%10;
+  digit = Math.floor(digit/10);
+  while(digit > 0) {
+    result *= digit%10;
+    digit = Math.floor(digit/10);
+  }
+  return result;
+}
 module.exports = problem1;
