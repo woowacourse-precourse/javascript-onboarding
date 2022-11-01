35c2d2d77200190da06ad58028515bf21426037b
nuheajiohc <jaehun341012@gmail.com>
2022-11-01 11:22:21 +0900

    문제6 : (refactor) 함수 기능 세분화

--- src/problem6.js
+++ src/problem6.js
@@ -1,11 +1,10 @@
 function problem6(forms) {
-    var answer = nameOverlapCheck(forms);
-    return answer;
+    return resultSort(forms);
 }
 
-function nameOverlapCheck(forms) {
+function reduplicationCheck(forms) {
     let result = [];
-    for (let i = 0; i < forms.length - 1; i++) {
+    for (let i = 0; i < forms.length; i++) {
         for (let j = 0; j < forms[i][1].length - 1; j++) {
             for (let k = i + 1; k < forms.length; k++) {
                 if (forms[k][1].includes(forms[i][1].slice(j, j + 2))) {
@@ -15,15 +14,12 @@ function nameOverlapCheck(forms) {
             }
         }
     }
-    let resultSet = new Set(result);
+    return result;
+}
+
+function resultSort(forms) {
+    let resultSet = new Set(reduplicationCheck(forms));
     return [...resultSet].sort();
 }
-// const forms = [
-//     ["jm@email.com", "원피스"],
-//     ["jason@email.com", "제이슨"],
-//     ["woniee@email.com", "원피수"],
-//     ["mj@email.com", "엠제이"],
-//     ["nowm@email.com", "수원피"]
-// ];
-// console.log(a(forms));
+
 module.exports = problem6;
