cbcd381abdb8b48c51c1f1b739419506b6e3a7eb
kimeodml <kimeodml@gmail.com>
2022-11-01 15:11:02 +0900

    feat: 문제3 해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,10 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let cnt = 0;
+  const rgx = /3|6|9/g;
+  for(let i = 1; i <=number;i ++){
+    cnt +=(String(i).match(rgx) || "").length;
+  }
+  return cnt;
 }
 
 module.exports = problem3;
