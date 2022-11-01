6bf08d744959f6ec0e02ca2b055f8c77a8403ec1
Dahye <06robin11@gmail.com>
2022-11-01 16:23:18 +0900

    feat: get the amount of each unit which is needed to make the unit amount least

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = units.map((unit) => {
+    if (money / unit > 1) {
+      const num = Math.floor(money / unit);
+      money -= num * unit;
+      return num;
+    } else {
+      return 0;
+    }
+  });
   return answer;
 }
 
