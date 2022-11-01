27b893c3a3e04c2e0cfe68a96fbb62c5c8431de3
HYEOK9 <leejaehyuck9@gmail.com>
2022-11-01 13:04:14 +0900

    feat:solve problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,24 @@
 function problem4(word) {
-  var answer;
-  return answer;
+    let answer = word;
+    let alp = "abcdefghijklmnopqrstuvwxyz";
+
+    for (let i = 0; i < answer.length; i++) {
+        let char = answer[i];
+        if (char !== " ") {
+            answer[i].toUpperCase() === char
+                ? (char =
+                      alp[
+                          26 - alp.indexOf(char.toLowerCase()) - 1
+                      ]).toUpperCase()
+                : (char = alp[26 - alp.indexOf(char) - 1]);
+
+            answer =
+                answer.substring(0, i) +
+                char +
+                answer.substring(i + 1, answer.length);
+        }
+    }
+    return answer;
 }
 
 module.exports = problem4;
