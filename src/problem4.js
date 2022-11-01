ff5f94b2a01250aeb76aebdc30ecfc2512cb501d
RooT_Kim <bdd3452@gmail.com>
2022-11-01 13:14:56 +0900

    문제4번) 기능목록 수정, solution 함수 선언 및 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,8 +1,11 @@
 function problem4(word) {
-	return charFilter(word);
+	//알파벳인지 판별하고 결과를 반환하는 함수 -> solution
+	//대문자, 소문자를 구별하는 함수 -> alphabetFilter
+	//대문자, 소문자 별로 알파벳을 반전하는 함수 -> reverseUpper, reverseLower
+	return solution(word);
 }
 
-function charFilter(word) {
+function solution(word) {
 	let reverseWord = '';
 
 	for (let i = 0; i < word.length; i++) {
@@ -16,12 +19,12 @@ function alphabetFilter(alphabet) {
 	return /[a-z]/g.test(alphabet) ? reverseLower(alphabet.charCodeAt(0)) : reverseUpper(alphabet.charCodeAt(0));
 }
 
-function reverseLower(alphabetCode) {
-	return String.fromCharCode(alphabetCode > 109 ? 97 + (122 - alphabetCode) : 122 + (97 - alphabetCode));
-}
-
 function reverseUpper(alphabetCode) {
 	return String.fromCharCode(alphabetCode > 77 ? 65 + (90 - alphabetCode) : 90 + (65 - alphabetCode));
+}
+
+function reverseLower(alphabetCode) {
+	return String.fromCharCode(alphabetCode > 109 ? 97 + (122 - alphabetCode) : 122 + (97 - alphabetCode));
 }
 
 module.exports = problem4;
