f3483b55bf7608c8dba5496fc27bbfa221e76b26
xownsdl0 <xownsdl0@naver.com>
2022-11-01 20:20:34 +0900

    feat(problem4): 앞파벳 외의 문자 검사 조건 추가

--- src/problem4.js
+++ src/problem4.js
@@ -3,12 +3,13 @@ function problem4(word) {
   let alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
   let reverseCase = "ZYXWVUTSRQPONMLKJIHGFEDCBA";
   let replaceWord = "";
+  let checkSpc = /[~!@#$%^&*()_+|<>?:{}]/;
 
   for (let i of word) {
     // i가 공백인가.
-    if (i === " ") {
-      replaceWord += " ";
-    } else if (i === i.toUpperCase()) {
+    if (i === " ") replaceWord += " ";
+    else if (checkSpc.test(i)) replaceWord += i;
+    else if (i === i.toUpperCase()) {
       // i가 대문자인가, 청개구리 사전의 알파벳으로 치환 후 replaceWord에 담는다.
       let reChar = alphabet[reverseCase.indexOf(i)];
       replaceWord += i.replace(i, reChar);
@@ -20,7 +21,9 @@ function problem4(word) {
   }
 
   answer = replaceWord;
+  console.log(answer);
   return answer;
 }
 
+problem4("Hello  Everyone!");
 module.exports = problem4;
