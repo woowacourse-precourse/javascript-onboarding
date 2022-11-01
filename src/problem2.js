01895556fa8e8cf99ff6b027998d736583c3f7e9
hayoun <fishingest@kookmin.ac.kr>
2022-11-01 17:08:33 +0900

    [FEAT] Solve Problem2
    문제2 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,24 @@
 function problem2(cryptogram) {
   var answer;
+  while(true){
+    var status=0
+    
+    for(var i=0;i<cryptogram.length;i++){
+      if(cryptogram[i]==cryptogram[i+1]){
+        status=1
+        var text1=cryptogram.slice(0, i);
+        var text2=cryptogram.slice(i+2,cryptogram.length);
+        cryptogram=text1.concat(text2)
+        
+        break
+      }
+
+    }
+    if(status==0){
+      break
+    }
+  }
+  answer=cryptogram
   return answer;
 }
 
