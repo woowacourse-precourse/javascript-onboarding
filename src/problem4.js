8eef169c2e3f8ec0fe9836fe1155ef860b9cfe74
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 21:54:48 +0900

    feat: [PROBLEM4] 문자열 길이에 따른 에러 반환(feat1) 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,13 @@
 function problem4(word) {
   var answer;
+
+  try {
+    if (word.length > 1000 || word.length < 1) {
+      return -1;
+    }
+  } catch {
+    answer = -1;
+  }
   return answer;
 }
 
