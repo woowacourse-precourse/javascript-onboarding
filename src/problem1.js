91726d8cd60962eb01293f2d307689ddc4f3c5a5
kimeuns <dmstn2006@gmail.com>
2022-11-01 18:02:17 +0900

    문제1 refactor depth 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,47 +1,52 @@
 function problem1(pobi, crong) {
-
-  if(!checkPage(pobi) || !checkPage(crong)){return -1}
-
-  if (maxValue(pobi)>maxValue(crong)){
-    return 1
+  if (isNotPage(pobi) || isNotPage(crong)) {
+    return -1;
   }
-  if (maxValue(pobi)<maxValue(crong)){
-    return 2
+
+  if (maxValue(pobi) > maxValue(crong)) {
+    return 1;
   }
-  if(maxValue(pobi)==maxValue(crong)){
-    return 0
+  if (maxValue(pobi) < maxValue(crong)) {
+    return 2;
+  }
+  if (maxValue(pobi) == maxValue(crong)) {
+    return 0;
   }
 
   return -1;
 }
 
-function checkPage(pages){
-  if (pages[1]-pages[0] !== 1 || pages[0]%2==0 
-    || pages[0]==1 || pages[1]==400){
-      return false
+function isNotPage(pages) {
+  if (
+    pages[1] - pages[0] !== 1 ||
+    pages[0] % 2 == 0 ||
+    pages[0] == 1 ||
+    pages[1] == 400
+  ) {
+    return true;
   }
-  return true
+  return false;
 }
 
-function plusNum(page){
-  let numbers = String(page).split("")
-  const sum = numbers.reduce((sum,num)=>sum+Number(num),0)
-  return sum
+function plusNum(page) {
+  let numbers = String(page).split("");
+  return numbers.reduce((sum, num) => sum + Number(num), 0);
 }
 
-function multiNum(page){
-  let numbers = String(page).split("")
-  if(numbers.includes('0')){return 0}
-  const sum = numbers.reduce((sum,num)=>sum*Number(num),1)
-  return sum
-}
-
-function maxValue(member){
-  const resultArr = []
-  for (let page of member){
-    resultArr.push(Math.max(plusNum(page),multiNum(page)))
+function multiNum(page) {
+  let numbers = String(page).split("");
+  if (numbers.includes("0")) {
+    return 0;
   }
-  return Math.max(...resultArr)
+  return numbers.reduce((sum, num) => sum * Number(num), 1);
+}
+
+function maxValue(member) {
+  const resultArr = member.reduce((arr, page) => {
+    arr.push(Math.max(plusNum(page), multiNum(page)));
+    return arr;
+  }, []);
+  return Math.max(...resultArr);
 }
 
 module.exports = problem1;
