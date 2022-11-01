b2e2ed29f1f8c512e0ece116c8ce59ec920c0fe4
윤동주 <yoondj98@naver.com>
2022-11-01 20:47:36 +0900

    우아한 테크코스 프리코슥 1주차 5번 기능구현 완료

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,14 @@
 function problem5(money) {
-  var answer;
+  var money_category = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = [];
+  for(var i=0; i<9; i++){
+    var quot = parseInt(parseInt(money)/parseInt(money_category[i]));
+    if(quot>0){
+      money = money%money_category[i];
+      answer.push(quot);
+    }
+    else answer.push(0);
+  }
   return answer;
 }
 
