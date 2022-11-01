f8abda71462b1d381f261b4058973225a6ebab08
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 22:35:05 +0900

    problem7 : scoreMap에서 친구인 사람은 삭제하는 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -39,6 +39,13 @@ function getSortedScoreArr(scoreMap) {
   return sortedArr;
 }
 
+function removeAlreadyFriend(user, network, scoreMap) {
+  if (!network.has(user)) return;
+  for (const friend of network.get(user)) {
+    if (scoreMap.has(friend)) scoreMap.delete(friend);
+  }
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
