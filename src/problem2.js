158c56c3a63b62ad6fc2126e42a1f587ac7549d7
김희진 <choha941108@gmail.com>
2022-11-01 12:46:37 +0900

    (2번) 세개이상 반복문자열 삭제 대응

--- src/problem2.js
+++ src/problem2.js
@@ -1,15 +1,19 @@
 function problem2(cryptogram) { //단어하나를 받는다
   var answer = [] ;
   const cryptogram_add = [...cryptogram]
-  //console.log("단어 배열변경",cryptogram_add)
+  console.log("단어 배열변경",cryptogram_add)
   //단어를 알파벳 별로 배열로 쪼갠다
 
   answer.push(cryptogram_add[0])
   for (i = 1 ; i < cryptogram_add.length ; i ++) { 
-    if (cryptogram_add[i] != answer[answer.length-1]) {
-      answer.push(cryptogram_add[i])
+    const curr = cryptogram_add[i];
+    if (curr != answer[answer.length-1]) {
+      answer.push(curr)
     } else {
       answer.pop()
+      while(curr === cryptogram_add[i+1]){
+        i++;
+      }
     }
    // console.log("단어 중복값 삭제",answer) 
   }
