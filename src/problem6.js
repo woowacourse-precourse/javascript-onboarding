44bdc401a07b3f7e0aa08f5b7c5642dc57b7597a
hvvany <kjunhwan1998@gmail.com>
2022-11-01 21:09:16 +0900

    feat(build): make duplicate character set

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,22 @@
 function problem6(forms) {
   let answer = new Set();
+  let repeat = new Set();
+  let name_dic = {};
+  for (let i=0; i < forms.length; i++) {
+    let name = forms[i][1];
+    for (let j=0; j < name.length - 1; j++) {
+      if (name.length != 1) {
+        if (name.slice(j,j+2) in name_dic) {
+          name_dic[name.slice(j,j+2)].push(i);
+        }
+        else {
+          name_dic[name.slice(j,j+2)] = [i]
+        }
+      }
+    }
+  }
+
+  
   return answer;
 }
 
