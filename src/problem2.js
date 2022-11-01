1f1728fb9376ef61ef9e231a1ec30a574f2e2fb6
윤동주 <yoondj98@naver.com>
2022-11-01 20:46:32 +0900

    우아한 테크코스 프리코슥 1주차 2번 기능구현 완료

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,35 @@
 function problem2(cryptogram) {
-  var answer;
+  var input = cryptogram.toString();
+  if(input.length == 1) return answer;
+  while(true){
+    var answer = "";
+    var count = 0;
+    for(i=0; i<input.length; i++){
+      if(i == 0){
+        if(input[i] == input[i+1]) {
+          count++;
+          continue;
+        }
+        else answer = answer.concat(input[0]); 
+      }
+      else if(i == input.length-1){
+        if(input[i] == input[i-1]) {
+          count++;
+          continue;
+        }
+        else answer = answer.concat(input[i]);
+      }
+      else{
+        if(input[i] == input[i-1] || input[i] == input[i+1]) {
+          count++;
+          continue;
+        }
+        else answer = answer.concat(input[i]);
+      }
+    }
+    if (count == 0) break;
+    input = answer;
+  }
   return answer;
 }
 
