da49825841ff1c01fb0eaf53afae0ae2913a34a0
Y2IN <dpdls0228@daum.net>
2022-11-01 15:50:26 +0900

    docs : 주석 수정

--- src/problem6.js
+++ src/problem6.js
@@ -108,10 +108,3 @@ function problem6(forms) {
 }
 
 module.exports = problem6;
-problem6([
-  ["jsssm@email.com", "제이엠"],
-  ["jason@email.com", "제이슨"],
-  ["woniee@email.com", "워니"],
-  ["mj@email.com", "엠제이"],
-  ["nowm@email.com", "이제엠"],
-]);
