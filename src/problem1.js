af6c4c432f5762fe8a6bcb7644df9bc4750b4d19
김희진 <choha941108@gmail.com>
2022-11-01 16:23:12 +0900

    (1번) 오류부분 재도출

--- src/problem1.js
+++ src/problem1.js
@@ -6,12 +6,11 @@ function problem1(pobi, crong) {
   //실패 조건 검사
   if(
     (pobi[0] > 400 || pobi[1] > 400 || crong[0] > 400 || crong[1] > 400)//책은 총 400쪽 넘으면 True
-    (pobi[0] < 1 || pobi[1] < 1 || crong[0] < 1 || crong[1] < 1)
-    // || (1 == pobi[0]|| pobi[1]== 400 || 1 == crong[0] || crong[1] == 400)//시작/마지막면 이면 True
+    || (1 == pobi[0]|| pobi[1]== 400 || 1 == crong[0] || crong[1] == 400)//시작/마지막면 이면 True
     || (pobi[0]%2 != 1 || crong[0]%2 != 1)//왼쪽이 홀수 아니면 True 
     || (pobi[0] + 1 != pobi[1] || crong[0] + 1 != crong[1])//오른쪽은 왼쪽 + 1 아니면 True
   ){
-    return -1
+    return -1;
   }
 
   let pobinums = []  //포비에 받은 숫자 하나씩 분해
@@ -25,7 +24,7 @@ function problem1(pobi, crong) {
     } while (pobi[i] > 0);
     
     //console.log("포비분해",_pobinums)
-    pobinums.push(_pobinums)
+    pobinums.push(_pobinums);
 
     let _crongnums = [];
     
@@ -35,7 +34,7 @@ function problem1(pobi, crong) {
     } while (crong[i] > 0);
 
     //console.log("크롱분해",_crongnums)
-    crongnums.push(_crongnums)
+    crongnums.push(_crongnums);
   }
   //포비/크롱 분해 완료
   //console.log("포비확인",pobinums)
@@ -55,7 +54,7 @@ function problem1(pobi, crong) {
   //console.log("포비 더하기_1",pobi_plus[0])
   //console.log("포비 더하기_2",pobi_plus[1])
 
-  let crong_plus = [0,0]
+  let crong_plus = [0,0];
 
   for ( i = 0 ; i < crongnums.length ; i++) {
     for ( j = 0 ; j < crongnums[i].length ; j++) {
@@ -65,7 +64,7 @@ function problem1(pobi, crong) {
   //console.log("크롱 더하기_1",crong_plus[0])
   //console.log("크롱 더하기_2",crong_plus[1])
 
-  let pobi_multiply = [1,1]
+  let pobi_multiply = [1,1];
 
   for ( i = 0 ; i < pobinums.length ; i++) {
     for ( j = 0 ; j < pobinums[i].length ; j++) {
@@ -75,7 +74,7 @@ function problem1(pobi, crong) {
   //console.log("포비 곱하기_1",pobi_multiply[0])
   //console.log("포비 곱하기_2",pobi_multiply[1])
   
-  let crong_multiply = [1,1]
+  let crong_multiply = [1,1];
 
   for ( i = 0 ; i < crongnums.length ; i++) {
     for ( j = 0 ; j < crongnums[i].length ; j++) {
