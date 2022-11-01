11856dec71d38bdb81848cbb5a0408b49eb57782
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 12:13:44 +0900

    [problem3] feat: 1.Convert number into string

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
   var answer;
+  let cnt = 0;
+  for (let i = 1; i <= number; i++) {
+    // i를 string 으로 변환
+    let str = String(i);
+  }
+  answer = cnt;
   return answer;
 }
 
