5166fdb7ecbdafa8c342908fbd629111be4b2868
Minhye Kang <alsgp9898@naver.com>
2022-11-01 23:42:53 +0900

    feat(PROBLEM7): 함께 아는 친구에게 10점, 유저의 친구에게 0점 부여

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,24 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let friendScoreList = new Map();
+  
+
+  for (let friend of friends) {
+    if (!friend.includes(user)) {
+      for (let name of friend) {
+        friendScoreList.set(name, POINT.WITH_KNOW_FRIEND);
+      }
+    } else {
+      for (let name of friend) {
+        friendScoreList.set(name, POINT.USER_FRIEND);
+      }
+    }
+  }
 }
+
+const POINT = Object.freeze({
+  USER_FRIEND: 0,
+  WITH_KNOW_FRIEND: 10,
+  TIMELINE_VISITOR: 1,
+});
 
 module.exports = problem7;
