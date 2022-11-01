64483bd8938232094c9e273a474be3437a1d265a
Avoler0 <jys1391@gmail.com>
2022-11-01 17:42:44 +0900

    과제 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,26 @@
 function problem4(word) {
+  const lower = {
+    start:97,
+    end:122
+  }
+  const upper = {
+    start:65,
+    end:90
+  }
+
+  const momWord = word.split("")
   var answer;
+
+  momWord.forEach((element,index) => {
+    if(element === " ") return
+    if(element == element.toLowerCase()){
+      momWord[index] = String.fromCharCode(lower.start - word.charCodeAt(index) + lower.end)
+    }
+    if(element == element.toUpperCase()){
+      momWord[index] = String.fromCharCode(upper.start - word.charCodeAt(index) + upper.end)
+    }
+  });
+  answer = momWord.join("");
   return answer;
 }
 
