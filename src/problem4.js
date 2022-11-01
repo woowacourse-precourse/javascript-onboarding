99da61f3bb558d9b0ca97344c86de5621cac5d69
daal2quf <daal2quf@gmail.com>
2022-11-01 21:16:16 +0900

    변수명 알기쉽게 변경

--- src/problem4.js
+++ src/problem4.js
@@ -2,17 +2,17 @@ function problem4(word) {
   
   let mother = ['A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z']
   let frog = [...mother].sort((a, b) => b.localeCompare(a))
-  let lowerMother = mother.map((e) => e.toLowerCase())
-  let lowerFrog = frog.map((e) => e.toLowerCase())
+  let lowerCaseMother = mother.map((e) => e.toLowerCase())
+  let lowerCaseFrog = frog.map((e) => e.toLowerCase())
   let frogSay = '';
 
 const frogtranslator = (str) => {
   if (mother.includes(str)) {
     let idx = mother.indexOf(str)
     return frog[idx]
-  } else if (lowerMother.includes(str)) {
-    let idx = lowerMother.indexOf(str)
-    return lowerFrog[idx]
+  } else if (lowerCaseMother.includes(str)) {
+    let idx = lowerCaseMother.indexOf(str)
+    return lowerCaseFrog[idx]
   } else if (str === ' ')
     return str
 }
