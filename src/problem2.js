58ae847ed0cc2c641b46e7e40fe50ca555e705bc
Lim_JiSeon <dpf226@naver.com>
2022-11-01 21:02:44 +0900

    problem2 - function 추가

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,7 @@
 기능 목록
 1. 앞의 문자와 같은지 다른지 확인하는 기능
 2. 중복 문자가 있는 문자열인지 확인하는 기능
+3. 문자열에서 중복 문자를 제거하는 기능
 */
 
 function double_check(s1, s2) {
@@ -24,6 +25,21 @@ function check_str(str) {
   }
   return false;
 }
+
+function double_delete(str) {
+  var pre_char = "";
+  var new_str = "";
+
+  for (let i = 0; i < str.length; i++) {
+    if (!double_check(pre_char, str[i])) {
+      new_str += str[i];
+      pre_char = str[i];
+    }
+  }
+
+  return new_str;
+}
+
 
 function problem2(cryptogram) {
   var answer;
