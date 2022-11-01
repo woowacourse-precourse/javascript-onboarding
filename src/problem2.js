9a5bc2d10cbb6cb7bd6916a7aaf42897e96ada32
JaeJunday <jaejun@JaeJuns-MacBook-Pro.local>
2022-11-01 19:53:29 +0900

    중복된 문자열을 제거하는 기능

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,16 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+	var count = 0;
+	var remove = [];
+	while (count < cryptogram.length){
+		for (var i = 0; i < cryptogram.length - 1; i++){
+			if (cryptogram[i] === cryptogram[i + 1]){
+				remove = cryptogram.splice(i, 2);
+				i -= 2;
+			}
+		}
+		count++;
+	}
+	return cryptogram;
 }
 
 module.exports = problem2;
