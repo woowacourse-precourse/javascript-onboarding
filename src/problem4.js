be7a8ac054dca1f2f15049292ebf01805da0e0bc
woodi97 <woodi.daily@gmail.com>
2022-11-01 22:02:06 +0900

    solve(): week-01 problem-04

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,21 @@
+const REVERTED = 'ZYXWVUTSRQPONMLKJIHGFEDCBA';
+
 function problem4(word) {
-  var answer;
+  let answer = '';
+
+  for(let i=0; i<word.length; i++){
+    if(word[i] === ' '){
+      answer += ' ';
+      continue;
+    }
+    if(word[i] === word[i].toUpperCase()){
+      answer += REVERTED[word[i].charCodeAt(0) - 'A'.charCodeAt(0)];
+    }
+    else{
+      answer += REVERTED[word[i].charCodeAt(0) - 'a'.charCodeAt(0)].toLowerCase();
+    }
+  }
+
   return answer;
 }
 
