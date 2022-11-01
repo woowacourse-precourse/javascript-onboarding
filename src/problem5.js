cb3ea6f71c15807d9da701e513bb18c8b3109388
jinyong <zhaqh146@naver.com>
2022-11-01 13:44:13 +0900

    solve problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  let result = [0, 0, 0, 0, 0, 0, 0, 0, 0]
+  const moneyArray = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+  for (let i = 0; i < 9; i++) {
+    let count = parseInt(money / moneyArray[i]);
+    money %= moneyArray[i]
+    result[i] = count
+  }
+
+  return result;
 }
 
 module.exports = problem5;
