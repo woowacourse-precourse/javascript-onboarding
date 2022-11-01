b84699633a66fed071413fadab93bd97bcaa89de
yeongseokim <1903020@ewhain.net>
2022-11-01 20:36:45 +0900

    feat(problem1): 페이지 넘버의 각 자리 숫자 곱하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,14 @@
+function multDigit(num) {
+  let n = num;
+  let mult = 1;
+  while (true) {
+    mult *= n % 10;
+    n = Math.floor(n / 10);
+    if (n == 0) break;
+  }
+  return mult;
+}
+
 function problem1(pobi, crong) {
   if (pobi.length != 2) return -1
   if (crong.length != 2) return -1
