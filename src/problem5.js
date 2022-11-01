2e3c659c55d3e15cd3a0a294c3b9baeeac7155ff
y00eunji <aine0227@naver.com>
2022-11-01 20:29:02 +0900

    feat: [problem5] make new Array to count money

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,6 @@
 const getMoney = (money) => {
   const MONEY = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const countMoney = new Array(MONEY.length).fill(0);
 };
 
 function problem5(money) {
