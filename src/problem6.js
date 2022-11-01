073877a112ead3553e64feed4d5cfe58c844999d
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 16:44:51 +0900

    Feat: problem6 - 변수 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,31 +1,9 @@
 function problem6(forms) {
-  let answer = "";
-  let nickName = [];
-  let SlicedTwoWords = "";
-  let RepetitionCount = {};
-  let result = [];
-
-  for (let i = 0; i < forms.length; i++) {
-    nickName.push(forms[i][1]);
-
-    for (let j = 0; j < nickName[i].length - 1; j++) {
-      SlicedTwoWords = nickName[i].substring(j, j + 2);
-
-      if (RepetitionCount[SlicedTwoWords]) {
-        RepetitionCount[SlicedTwoWords] += 1;
-      } else RepetitionCount[SlicedTwoWords] = 1;
-    }
-  }
-
-  for (let i = 0; i < forms.length; i++) {
-    for (let j = 0; j < nickName[i].length - 1; j++) {
-      SlicedTwoWords = nickName[i].substring(j, j + 2);
-      if (RepetitionCount[SlicedTwoWords] > 1) result.push(forms[i][0]);
-    }
-  }
-  answer = result.sort();
-  console.log(answer);
-  return answer;
+  let answer = [];
+  const nickName = [];
+  let nickNameTwoWords = "";
+  let duplicationCount = {};
+  const result = new Set([]);
 }
 
 module.exports = problem6;
