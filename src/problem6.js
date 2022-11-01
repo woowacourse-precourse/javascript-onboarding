f1d23d99f65d2751eb4fe12125ed4fa70759cdd0
JangHoonSeok <orirori512@naver.com>
2022-11-01 16:56:52 +0900

    feat: problem6. 크루들의 이름을 연속된 2글자로 잘라 저장하고 카운트 하는 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,22 @@
 function problem6(forms) {
-  var answer;
+  let answer = [];
+  const overlap_dict = makeDict(forms);
+  answer = findOverlapPeople(forms, overlap_dict);
   return answer;
+}
+function makeDict(forms) {
+  // 이름을 2글자씩 잘라 Dictionary에 저장하는 함수.
+  let dict = {};
+  forms.map((form) => {
+    const [_, name] = form;
+    for (let i = 0; i < name.length - 1; i++) {
+      const temp_two_words = name[i] + name[i + 1];
+      if (!(temp_two_words in dict)) dict[temp_two_words] = 1;
+      else dict[temp_two_words] += 1;
+    }
+  });
+
+  return dict;
 }
 
 module.exports = problem6;
