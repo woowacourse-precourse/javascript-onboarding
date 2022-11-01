940d21d17de89f4622fd464d20957b94113ead83
nalsae <nalsae0207@naver.com>
2022-11-01 19:26:41 +0900

    refactor: Add a logic for validation
    - 유효성 검사를 위한 로직 추가 및 코드 리팩토링

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,15 @@
+const isValid = (pobi, crong) => {
+  const [pobiLeft, pobiRight] = pobi;
+  const [crongLeft, crongRight] = crong;
+
+  if (pobiRight - pobiLeft > 1 || crongRight - crongLeft > 1) return false;
+  if ([...pobi, ...crong].some((page) => page > 400 || page < 1)) return false;
+
+  return true;
+};
+
 const createResults = (page) => {
-  const splitPage = page.toString().split('');
+  const splitPage = page.toString().split("");
 
   return [
     splitPage.reduce((total, currentNum) => +total + +currentNum),
@@ -8,23 +18,12 @@ const createResults = (page) => {
 };
 
 const problem1 = (pobi, crong) => {
-  let answer;
-  const pobiPages = pobi;
-  const crongPages = crong;
+  if (!isValid(pobi, crong)) return -1;
 
-  if (pobiPages[1] - pobiPages[0] > 1 || crongPages[1] - crongPages[0] > 1) {
-    return -1;
-  }
+  const pobiBest = Math.max(...pobi.flatMap((page) => createResults(page)));
+  const crongBest = Math.max(...crong.flatMap((page) => createResults(page)));
 
-  const pobiResults = pobiPages.map((page) => createResults(page)).flat();
-  const crongResults = crongPages.map((page) => createResults(page)).flat();
-
-  const pobiBest = Math.max(...pobiResults);
-  const crongBest = Math.max(...crongResults);
-
-  answer = pobiBest > crongBest ? 1 : pobiBest < crongBest ? 2 : 0;
-
-  return answer;
+  return pobiBest > crongBest ? 1 : pobiBest < crongBest ? 2 : 0;
 };
 
 module.exports = problem1;
