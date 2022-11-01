1502f11fa16d69ce4db9a246264f51f7aca8e4e9
ilgon0110 <moses011010@gmail.com>
2022-11-01 15:39:30 +0900

    refactor: 연산 기능 수정(#problem6)
    연산 1-1 기능 방식 수정

--- src/problem6.js
+++ src/problem6.js
@@ -1,21 +1,4 @@
 function problem6(forms) {
-  //연산
-  //기능목록 1-1 구현
-  const isRepeat = (str1, str2) => {
-    for (let rt = 0; rt < str2.length; rt++) {
-      let cnt = 0;
-      let tmp = rt;
-      for (let lt = 0; lt < str1.length; lt++) {
-        if (str2[tmp] === str1[lt]) {
-          cnt++;
-          tmp++;
-        } else cnt = 0;
-        if (cnt === 2) return true;
-      }
-    }
-    return false;
-  };
-
   //예외처리
   const isWrong = (email, nickname) => {
     const regex = /^[ㄱ-ㅎ|가-힣]+$/;
@@ -39,20 +22,22 @@ function problem6(forms) {
   if (forms.length < 1 || forms.length > 10000) {
     throw '크루 수가 허용 범위를 벗어났습니다';
   }
-  for (let i = 0; i < forms.length; i++) {
-    if (isWrong(forms[i][0], forms[i][1]) || check[i] !== 0) continue;
-
-    for (let j = 0; j < forms.length; j++) {
-      if (i === j) continue;
-      if (isRepeat(forms[i][1], forms[j][1])) {
-        //기능목록 1-2 구현
-        result.add(forms[i][0]).add(forms[j][0]);
-        //기능목록 1-3 구현
-        check[i] = 1;
-        check[j] = 1;
+  //연산
+  //기능목록 1-1 구현
+  let twoCharList = new Map();
+  forms.forEach((v, idx) => {
+    let [email, nickname] = forms[idx];
+    if (isWrong(email, nickname)) return;
+    for (let i = 0; i < v[1].length - 1; i++) {
+      const twoChar = v[1].substring(i, i + 2);
+      if (twoCharList.has(twoChar)) {
+        twoCharList.set(twoChar, twoCharList.get(twoChar) + 1);
+      } else {
+        twoCharList.set(twoChar, 1);
       }
     }
-  }
+  });
+
   //기능목록 2-2 구현
   answer.push(...result);
   answer.sort();
