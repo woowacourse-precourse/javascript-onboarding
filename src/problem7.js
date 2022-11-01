1710740db459e70435d846214d40bb98654d7004
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-01 17:19:09 +0900

    refactor:ㅋ 코드 리펙토링

--- src/problem7.js
+++ src/problem7.js
@@ -1,66 +1,66 @@
-function problem7(user, friends, visitors) {
-  const scoreSet = new Map();
+function hasUserCheck(user, friends) {
   const excepts = new Set();
-  let score = new Set();
-  const answer = [];
-  let firstscore = 0,
-    secondscore = 0,
-    thirdscore = 0;
+  for (let friend of friends) {
+    if (friend.includes(user)) {
+      excepts.add(friend[0] === user ? friend[1] : friend[0]);
+    }
+  }
   excepts.add(user);
+  return excepts;
+}
 
-  for (let x of friends) {
-    if (x[0] === undefined || x[1] === undefined) continue;
-    if (x.includes(user)) {
-      excepts.add(x[0] === user ? x[1] : x[0]);
+function frinedsScoreCalculator(friends, hasUser, scoreMap, scoreSet) {
+  for (let user of friends) {
+    if (user[0] === undefined || user[1] === undefined) continue;
+    if (hasUser.has(user[0]) && !hasUser.has(user[1])) {
+      let score = scoreMap.has(user[1]) ? scoreMap.get(user[1]) + 10 : 10;
+      scoreMap.set(user[1], score);
+      scoreSet.add(score);
+    }
+    if (hasUser.has(user[1]) && !hasUser.has(user[0])) {
+      let score = scoreMap.has(user[0]) ? scoreMap.get(user[0]) + 10 : 10;
+      scoreMap.set(user[0], score);
+      scoreSet.add(score);
     }
   }
-
-  for (let x of friends) {
-    if (x[0] === undefined || x[1] === undefined) continue;
-
-    if (excepts.has(x[0])) {
-      secondscore = scoreSet.has(x[1]) ? scoreSet.get(x[1]) + 10 : 10;
-      scoreSet.set(x[1], secondscore);
-      score.add(secondscore);
-    }
-    if (excepts.has(x[1])) {
-      firstscore = scoreSet.has(x[0]) ? scoreSet.get(x[0]) + 10 : 10;
-      scoreSet.set(x[0], firstscore);
-      score.add(firstscore);
+}
+function visitorsScoreCalculator(visitors, hasUser, scoreMap, scoreSet) {
+  for (let user of visitors) {
+    if (user === undefined) continue;
+    if (!hasUser.has(user)) {
+      let score = scoreMap.has(user) ? scoreMap.get(user) + 1 : 1;
+      scoreMap.set(user, score);
+      scoreSet.add(score);
     }
   }
+}
 
-  for (let x of visitors) {
-    if (x === undefined) continue;
-    if (!excepts.has(x)) {
-      thirdscore = scoreSet.has(x) ? scoreSet.get(x) + 1 : 1;
-      scoreSet.set(x, thirdscore);
-      score.add(thirdscore);
+function findUser(answer, score, scoreMap) {
+  let localArray = [];
+  for (let [name, userScore] of scoreMap) {
+    if (userScore === score) {
+      localArray.push(name);
     }
   }
-
-  const sortScore = [...score].sort((a, b) => b - a);
-
-  let index = 0;
-  while (index < sortScore.length) {
-    const local = [];
-    for (let [name, score] of scoreSet) {
-      if (score === sortScore[index] && !excepts.has(name)) {
-        local.push(name);
-      }
-    }
-    local.sort();
-    for (let i = 0; i < local.length; i++) {
-      if (answer.length < 5) {
-        answer.push(local[i]);
-      }
-      if (answer.length === 5) {
-        return answer;
-      }
-    }
-    index++;
+  localArray.sort();
+  for (let i = 0; i < localArray.length; i++) {
+    if (answer.length < 5) answer.push(localArray[i]);
+    if (answer.length === 5) return answer;
   }
+  return answer;
+}
 
+function problem7(user, friends, visitors) {
+  const answer = [];
+  const scoreMap = new Map();
+  const scoreSet = new Set();
+  const hasUser = hasUserCheck(user, friends);
+  frinedsScoreCalculator(friends, hasUser, scoreMap, scoreSet);
+  visitorsScoreCalculator(visitors, hasUser, scoreMap, scoreSet);
+  const sortScore = [...scoreSet].sort((a, b) => b - a);
+  for (let score of sortScore) {
+    findUser(answer, score, scoreMap);
+  }
   return answer;
 }
 
