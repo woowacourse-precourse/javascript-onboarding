807a74c4adaf60a612cec50b7ca486168109400c
Minjee Kim <hbmj1017@naver.com>
2022-11-01 21:50:01 +0900

    problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  const bill = [50000,10000,5000,1000,500,100,50,10,1];
+  var answer=[];
+  for(let i=0; money!==0; i++) {
+    const count = money / bill[i] >= 1 ? Math.floor(money/bill[i]) : 0;
+    answer[i] = count;
+    money = count > 0 ? money -= bill[i]*count : money;
+  }
   return answer;
 }
 
