c5ef9a5d1ca0dd047e7bb69340972b23986b38e5
yb8350 <yb8350@naver.com>
2022-11-01 20:03:59 +0900

    Refactor: problem2-중복 문자 제거하는 코드를 함수로 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,27 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = cryptogram;
 
   while (true) {
-    let breakCondition = true;
-    let tempCryptogram = "";
-    for (let i = 0; i < cryptogram.length; i++) {
-      if (cryptogram[i] === cryptogram[i + 1]) {
-        breakCondition = false;
-        i++;
-      } else tempCryptogram += cryptogram[i];
-    }
-    cryptogram = tempCryptogram;
+    let [breakCondition, tempCryptogram] = overlapDelete(answer);
+    answer = tempCryptogram;
     if (breakCondition) break;
   }
 
-  answer = cryptogram;
   return answer;
+}
+
+function overlapDelete(cryptogram) {
+  let breakCondition = true;
+  let tempCryptogram = "";
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    if (cryptogram[i] === cryptogram[i + 1]) {
+      breakCondition = false;
+      i++;
+    } else tempCryptogram += cryptogram[i];
+  }
+
+  return [breakCondition, tempCryptogram];
 }
 
 module.exports = problem2;
