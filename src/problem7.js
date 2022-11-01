88c0db76c85765607a0540fd2e5c9a848064c4c9
ho-ji <wkdd0205@gmail.com>
2022-11-01 20:37:02 +0900

    feat: 문제7 기능 구현
    친구 관계를 배열에 저장함

--- src/problem7.js
+++ src/problem7.js
@@ -9,6 +9,23 @@ function problem7(user, friends, visitors) {
   numberOfFriend.set(user, cntFriends);
   cntFriends++;
 
+  for (let i = 0; i < friends.length; i++) {
+    let user1 = friends[i][0];
+    let user2 = friends[i][1];
+    if (!numberOfFriend.has(user1)) {
+      numberOfFriend.set(user1, cntFriends);
+      listFriendship[cntFriends] = [];
+      cntFriends++;
+    }
+    if (!numberOfFriend.has(user2)) {
+      numberOfFriend.set(user2, cntFriends);
+      listFriendship[cntFriends] = [];
+      cntFriends++;
+    }
+    listFriendship[numberOfFriend.get(user1)].push(user2);
+    listFriendship[numberOfFriend.get(user2)].push(user1);
+  }
+
   return answer;
 }
 
