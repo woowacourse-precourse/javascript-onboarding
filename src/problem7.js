fe332ebe6789739adbe52fd3eaf1fea17c9820d7
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-01 21:11:05 +0900

    feature(problem7): getFriends 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  const getFriends = (user, friends) => {
+    const user_friends = [];
+    for (let friend of friends) {
+      if (friend.includes(user)) user_friends.push(friend[0]);
+    }
+    return user_friends;
+  };
+
+  let answer;
   return answer;
 }
 
