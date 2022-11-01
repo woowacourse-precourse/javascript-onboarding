8078e334e5a7ad5772d0abbae289736331718d2b
ksone02 <eksovus@gmail.com>
2022-11-01 17:59:17 +0900

    [feat] problem1: 각 자리 더하고 곱해서 최댓값 찾기

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,27 @@
+/*
+  기능 목록
+  1. 예외사항 처리 
+  2. 각 자리의 수를 더하고 곱해서 가장 큰 수 구하기
+  3. 점수를 비교해서 답 도출하기
+*/
+
+function maxCalc(str) {
+  const sum = str
+    .toString()
+    .split("")
+    .reduce((initial, current) => initial + Number(current), 0);
+  const multi = str
+    .toString()
+    .split("")
+    .reduce((initial, current) => initial * Number(current), 1);
+
+  return Math.max(sum, multi);
+}
+
 function problem1(pobi, crong) {
   var answer;
+  const pobiRes = Math.max(...pobi.map((e) => maxCalc(e)));
+  const crongRes = Math.max(...crong.map((e) => maxCalc(e)));
   return answer;
 }
 
