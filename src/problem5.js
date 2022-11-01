d5094424d5ddc0fa993a6b51e9d9740ad6d40af4
jiyun1006 <jiyun4251@gmail.com>
2022-11-01 13:10:14 +0900

    ✅  problem5 case1, 2 해결 (feat. 몫과 나머지를 구하는 수식을 이용해서 해결)

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  const answer = [];
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  moneyArr.map((el) => {
+    let cnt = parseInt(money / el);
+    money %= el; 
+    answer.push(cnt);
+  })
+  console.log(answer);
   return answer;
 }
 
