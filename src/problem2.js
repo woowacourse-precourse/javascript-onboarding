01bbfc3a48d0957f54be114c41b840fa9daa4546
yeongseokim <1903020@ewhain.net>
2022-11-01 20:46:39 +0900

    feat(problem2): 문자열의 왼쪽부터 오른쪽으로 탐색하며 중복 문자 삭제

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,22 @@
+function deleteSameWord(cryptogram) {
+  let str = cryptogram;
+  let word = '';
+  let flag = false;
+  for (let i = 1; i < str.length; i++) {
+    word = str[i];
+    if (str[i - 1] == word) {
+      str = str.replace(word + word, '');
+      flag = true;
+      i--;
+    }
+  }
+  return [str, flag];
+}
+
 function problem2(cryptogram) {
   var answer;
+  answer = deleteSameWord(cryptogram);
+
   return answer;
 }
 
