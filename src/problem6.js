5edc2d69ae51f5e80655f7434628095fb2574513
MI_MO <43203911+yujeonghyeop@users.noreply.github.com>
2022-11-01 12:10:57 +0900

    problem6 input 주석 제거

--- src/problem6.js
+++ src/problem6.js
@@ -42,20 +42,4 @@ function substr(user){
   includenickname.push(temporarynickname)
 }
 var includenickname = []
-// console.log(problem6([
-//   ["jm@email.com", "제이엠"],
-//   ["jason@email.com", "제이슨"],
-//   ["woniee@email.com", "워니"],
-//   ["mj@email.com", "엠제이"],
-//   ["nowm@email.com", "이제엠"],
-//   ["jasp@email.com", "박이김"],
-//   ["jp@email.com", "김이박"],
-//   ["qpw@email.com", "김이순"],
-//   ["teemo@email.com", "티모"],
-//   ["banana@email.com", "대위티모"],
-//   ["secondJm@email.com", "제이엠"],
-//   ["apple@email.com", "땷횱봵"],
-//   ["stran@email.com", "횱봵으"],
-//   ["zebra@email.com", "두글자이상의문자가연속적으로이제맞티모"]
-// ]))
 module.exports = problem6;
