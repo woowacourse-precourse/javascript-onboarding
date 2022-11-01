534a8e55b61509054e3ba81411a0f48874cc0e60
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 18:24:31 +0900

    problem3 requirements

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
+/*필요한 기능
+for loop으로 숫자 iterate
+모든 자릿수 보면서 3,6,9 확인
+나온 만큼 count에 ++
+결과물 리턴
+*/
+
 function problem3(number) {
-  var answer;
+  var answer = solution(number);
   return answer;
 }
+
+
 
 module.exports = problem3;
