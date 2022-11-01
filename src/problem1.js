4f1526efa5a83d34305669f3eee5d20fe45f0174
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:21:00 +0900

    [problem1] feat : 책번호 합 및 곱을 구하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
   var answer;
   return answer;
+  pobiMax = makePage(pobi);
+  crongMax = makePage(crong);
+function multi(multiArray) {
+  const result = multiArray.reduce(function (multi, cur) {
+    return multi * cur;
+  }, 1);
+  return result;
+function sum(sumArray) {
+  const result = sumArray.reduce(function (sum, cur) {
+    return parseInt(sum) + parseInt(cur);
+  }, 0);
+  return result;
+}
 }
 
 module.exports = problem1;
