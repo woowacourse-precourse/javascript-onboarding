cc26401ef3d9e96e889b98ed39f23384ea799a8b
김상두 <puki4416@gmail.com>
2022-11-01 17:51:07 +0900

    problem2 : 기능 정의 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,4 @@
-// 마지막에 삭제된 값이 있으면 연속된 중복을 체크하여 삭제하는 함수 구현
-// 현재값과 스택의 최상단값을 비교하여 중복을 제거하는 함수 구현
-// 구현된 함수를 사용하여 problem2 함수 구현
+// 문자열에 중복된 값을 반복해서 삭제하는 problem2 함수 구현
 
 function checkLastDeleted(latestDeleted, currentValue) {
   if (latestDeleted !== null) {
