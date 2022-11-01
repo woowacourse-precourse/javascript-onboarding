bfa552d5903af77ddd8b41aa03061bc3bf90ff44
sulmeee <poorooria@naver.com>
2022-11-01 10:23:24 +0900

    [problem6] feat: 중복여부를 나눌 배열 선언

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
   // 결과를 담을 배열 선언
   let result = [];
+
+  // 중복되지 않은 닉네임을 담을 배열
+  let nickName = [];
+
+  // 중복되는 닉네임을 담을 배열
+  let overlapNickName = [];
 }
 
 module.exports = problem6;
