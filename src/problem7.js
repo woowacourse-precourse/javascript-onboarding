b618f6153bec056e2c97c6dd08fe9199607d37e4
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 14:18:58 +0900

    [p7] 사용자와 친구관계인 사람을 구함

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,23 @@
-function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+function findFriend(user, friends) {
+  let friend = [];
+
+  friends.forEach((a1, a2) => {
+    if(a1 === user)
+      friend.push(a2);
+    if(a2 === user)
+      friend.push(a1);
+  });
+
+  return friend;
 }
 
+function problem7(user, friends, visitors) {
+  userFriend = findFriend(user, friends);
+  
+  let answer = [, []];
+  return answer;
+}
+//사용자와 친구관계인 사람을 구함
+//친구와 친구관계인 친구에 +10점
+//방문자
 module.exports = problem7;
