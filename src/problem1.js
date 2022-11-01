e5916d176037c456ba73a4a1a23c07c437ada0ff
Jihyun Kim <ang21icjh@gmail.com>
2022-11-01 14:31:20 +0900

    feat(problem1): 자리수 덧셈 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,3 +1,15 @@
+/**
+ * @param {number} digits
+ * @returns number
+ */
+function getSumOfDigits(digits) {
+  let sum = 0;
+  for (const digit of String(digits)) {
+    sum += Number(digit);
+  }
+  return sum;
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
