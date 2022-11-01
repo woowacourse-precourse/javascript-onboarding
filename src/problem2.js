7250220b25983bb2f121c2ec5fa60e6f6e6bc7d2
yunjjeongjo <two6885@gmail.com>
2022-11-01 17:28:45 +0900

    feat: problem2 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const stack = [];
+  stack.push(cryptogram[0]);
+
+  for (let i = 1; i < cryptogram.length; i++) {
+    const currentElement = cryptogram[i];
+
+    if (stack.length === 0) {
+      stack.push(currentElement);
+    } else {
+      const lastElement = stack[stack.length - 1];
+
+      if (lastElement == currentElement) {
+        stack.pop();
+      } else {
+        stack.push(currentElement);
+      }
+    }
+  }
+  return stack.join("");
 }
 
 module.exports = problem2;
