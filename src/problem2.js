31620039f573a70e7491f12f52b674dcdb201a0d
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-01 14:19:14 +0900

    problem2: decode() done

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,30 @@
 function problem2(cryptogram) {
   var answer;
+  var cryptogramString = cryptogram;
+  var bundle = 0;
+  var flag = 1;
 
-  function decode(i) {}
+  function decode(i) {
+    if (i == cryptogramString.length - 1) return;
+    else {
+      if (cryptogramString[i] == cryptogramString[i + 1]) {
+        flag++;
+        bundle++;
+      } else {
+        if (bundle > 0) bundle = 0;
+        else answer += cryptogramString[i];
+        if (i == cryptogramString.length - 2) answer += cryptogramString[i + 1];
+      }
+      decode(i + 1);
+    }
+  }
+
+  while (cryptogramString.length > 1 && flag > 0) {
+    answer = "";
+    flag = 0;
+    decode(0);
+    cryptogramString = answer;
+  }
 
   return answer;
 }
