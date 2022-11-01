846631ab7b77e383b76015b975b1c580fd53a896
banma70 <banma1234@gmail.com>
2022-11-01 15:38:14 +0900

    problem_5 : 가장 큰 단위의 화폐 출력

--- src/problem5.js
+++ src/problem5.js
@@ -10,4 +10,23 @@ function errorHandler(num) {
   return false;
 }
 
+function ATM(money) {
+  // 지갑
+  wallet = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  let temp = 0;
+
+  while (money!=0) {
+    // 50000원 권으로 출금 가능한 경우
+    if (money >= 50000) {
+      temp = money/50000;
+      // 지갑의 0번 인덱스값 변경
+      wallet.splice(0, 1, temp);
+      // 출금한 후의 금액으로 money 초기화
+      money = money%50000;
+    }
+  }
+
+  return wallet;
+}
+
 module.exports = problem5;
