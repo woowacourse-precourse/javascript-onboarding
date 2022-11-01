0bf85263c28ce633ccc76d18eb0daf638b74c0a4
Saans <92422813+saans808@users.noreply.github.com>
2022-11-01 13:52:26 +0900

    [p6] forms를 map객체로 만들고 애름 배열 생성

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,18 @@
 function problem6(forms) {
   var answer;
+
+  let wordSlice; 
+  let obj = new Map(forms.map(([a, b]) => [a, b]));
+
+  let setName = new Set(obj.values());
+  let name = [];
+  let rep = [];
+  let ans = [];
+  // 이름 배열
+  for (let i=0; i<forms.length; i++) {
+    name.push(forms[i][1]);
+  }
+  
   return answer;
 }
 
