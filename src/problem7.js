c63ed2c8b21a7d24e09e6e49b1d9c4ff75517aef
JuhyunLim <87933367+userJu@users.noreply.github.com>
2022-11-01 23:28:47 +0900

    problem7: 기능 구현
    makeFriendsDictionary: 친구 관계를 dictionary로 설정
    pointToKnownFriendWithUser: 다른 친구와 함께 알고 있는 친구에게 10점 제공
    pointToVisitedUser: 타임 라인에 방문했던 친구에게 1점씩 제공
    sortRecommend: 최종 점수에 따라 친구를 점수가 가장 높은 순, 이름 순으로 정렬

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,63 @@
+const makeFriendsDictionary = (friends) => {
+  const dictionary = {};
+  for (const [friend1, friend2] of friends) {
+    dictionary[friend1] === undefined
+      ? (dictionary[friend1] = [friend2])
+      : dictionary[friend1].push(friend2);
+    dictionary[friend2] === undefined
+      ? (dictionary[friend2] = [friend1])
+      : dictionary[friend2].push(friend1);
+  }
+  return dictionary;
+};
+
+const pointToKnownFriendWithUser = (dictionary, userFriends) => {
+  const score = {};
+  for (const key in dictionary) {
+    dictionary[key].filter((friend) => {
+      if (userFriends.includes(friend)) {
+        score[key] === undefined ? (score[key] = 10) : (score[key] += 10);
+      }
+    });
+  }
+  return score;
+};
+
+const pointToVisitiedUser = (dictionary, visitors) => {
+  for (const visit of visitors) {
+    dictionary[visit] === undefined
+      ? (dictionary[visit] = 1)
+      : (dictionary[visit] += 1);
+  }
+  return dictionary;
+};
+
+const sortRecommend = (dictionary, user) => {
+  const sortable = [];
+  for (const key in dictionary) {
+    if (key !== user && dictionary[key] > 0) {
+      sortable.push([key, dictionary[key]]);
+    }
+  }
+  const sortedRecommend = sortable.sort((a, b) => {
+    return b[1] - a[1];
+    return b[0] - a[0];
+  });
+  return sortedRecommend;
+};
+
 function problem7(user, friends, visitors) {
-  var answer;
+  const friend = makeFriendsDictionary(friends);
+  const arleadyFirend = friend[user];
+  const friendPoint = pointToKnownFriendWithUser(friend, arleadyFirend);
+  const finalScore = pointToVisitiedUser(friendPoint, visitors);
+  let answer = [];
+
+  sortRecommend(finalScore, user).map(([name, score]) => {
+    if (!arleadyFirend.includes(name)) {
+      answer.push(name);
+    }
+  });
   return answer;
 }
 
