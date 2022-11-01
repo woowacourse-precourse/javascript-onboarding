e69095268e654c39f5b695b2f16d5dfa7e645dfa
DongWooKim97 <nanaukim@naver.com>
2022-11-01 20:24:33 +0900

    refactor(P2): 불필요한 함수 및 주석 제거, 리턴 형식 변환

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,6 @@
 function problem2(cryptogram) {
-  if (!inputValidCheck(cryptogram) && !minLengthCase(cryptogram)) {
-    return 'error'
+  if (!minLengthCase(cryptogram)) {
+    return cryptogram
   }
 
   const compareArray = [];
@@ -17,30 +17,8 @@ function problem2(cryptogram) {
   return compareArray.join('');
 }
 
-function inputValidCheck(inputValue) {
-  if (!overInRange(inputValue)) return false;
-  if (!inputTypeError(inputValue)) return false;
-  return true;
-}
-
 function minLengthCase(inputValue) {
-  // 문자열의 길이가 1일땐 , 그 문자열 그대로 return
-  if (inputValue.length !== 1) return false;
-  return inputValue;
-}
-
-function overInRange(inputValue) {
-  const minLength = 1;
-  const maxLength = 1000;
-  //  1~1000사이의 숫자일 경우 -> O / 아닐 경우 -> X
-  if (inputValue.length >= minLength && inputValue.length <= maxLength) return true
-  return false;
-}
-
-function inputTypeError(inputValue) {
-  const inputValueEnglishLength = inputValue.replace(/[a-z]/g, '').length;
-
-  if (inputValueEnglishLength === 0) return true
+  if (inputValue.length !== 1) return true;
   return false;
 }
 
