d3c224c5f2a379471aca3664ca8e69240512092b
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:51:13 +0900

    [problem7] feat: user와 친구인사람 탐색

--- src/problem7.js
+++ src/problem7.js
@@ -13,6 +13,12 @@ function problem7(user, friends, visitors) {
   friendName.forEach(function (score) {
     scoreFriend.push([0, score]);
   }); // 점수 이름 담긴 scoreFriend + 점수는 0으로 초기화
+  friends.forEach(function (discovery) {
+    if (discovery.includes(user)) {
+      const discoveryIndex = discovery.indexOf(user);
+      discovery.splice(discoveryIndex, 1);
+      userFriend = discovery + "";
+      userFriendName.push(userFriend); // user와 친구인 사람 userFriendName 안에 추가
 function friendScore( //user와 친구추가 되있는 사람의 친구들에게 10점 추가
   friendsArray,
   userFriend,
