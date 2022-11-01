bacb2067125a3de86f6cfa75398fd7a519774df7
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-01 23:47:39 +0900

    우아한테크코스 문제2

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,13 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = [];
+  var s = cryptogram;
+  for (var i = 0; i <= s.length; i++) {
+    if (s[i] === answer[answer.length - 1]) {
+      answer.pop();
+    }else {
+      answer.push(s[i]);
+    }
+  }answer = answer.join("");
   return answer;
 }
 
