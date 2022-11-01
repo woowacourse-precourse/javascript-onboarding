d0f2c4979686b83e9b156e95f1a0fe2222f44035
yeongseokim <1903020@ewhain.net>
2022-11-01 22:33:34 +0900

    feat(problem4): A-Z 사이의 대문자 알파벳을 반전된 대문자 알파벳으로 변환

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,17 @@
 function problem4(word) {
-  var answer;
+  var answer = '';
+  let targetWord = '';
+  const askiiACode = 'A'.charCodeAt();
+  const askiiZCode = 'Z'.charCodeAt();
+
+  for (let i = 0; i < word.length; i++) {
+    targetWord = word[i].charCodeAt();
+    if (targetWord >= askiiACode && targetWord <= askiiZCode) {
+      targetWord = askiiZCode - (targetWord - askiiACode);
+    }
+    answer += String.fromCharCode(targetWord);
+  }
+
   return answer;
 }
 
