9b3e45b95499a2944f045a617f14b5dc418f0f8d
mun9769 <mun9769@naver.com>
2022-11-01 11:03:20 +0900

    Update problem2.js
    stack을 활용하여 문제를 해결했습니다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,18 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  var result;
+  let stack = [];
+  for (var i = 0; i < cryptogram.length; i++) {
+    var top = stack[stack.length - 1];
+
+    if (top == cryptogram[i]) {
+      while (top == cryptogram[i + 1]) { i++; }
+      stack.pop();
+    } else {
+      stack.push(cryptogram[i]);
+    }
+  }
+  result = stack.join('');
+  return result;
 }
 
 module.exports = problem2;
