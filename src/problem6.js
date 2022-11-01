21f326d039720be1b3b76447862402a60b17f54f
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-01 16:50:40 +0900

    feat: [problem6] 변수 선언

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,14 @@
 function problem6(forms) {
   var answer;
-  return answer;
+  let arr = forms;
+  // overlap: 같은 글자가 연속적으로 포함되는 닉네임 배열
+  // nickname: forms에서 nickname만 추출한 배열
+  // email: 정답에 만족하는 닉네임의 email 배열
+  let overlap = [];
+  let nickname = [];
+  let email = [];
+
+ 
 }
 
 module.exports = problem6;
