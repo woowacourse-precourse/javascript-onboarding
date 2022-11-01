ea73ba98b94c7c7cfa3eae8f110153a86992c434
RosaDamascena <hsj990604@gmail.com>
2022-11-01 19:34:43 +0900

    modified problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,25 +1,46 @@
+function check(pageNums) {
+  const [left, right] = pageNums;
+
+  //필수 조건
+  return 1 <= left && left < 400 && left % 2 == 1 && left + 1 == right;
+}
+
+function maxNum(pageNums) {
+  //pobi와 crong의 최댓값 구하기
+  const [left, right] = pageNums;
+  const leftNums = left
+    .toString()
+    .split("")
+    .map((v) => Number(v));
+  const rightNums = right
+    .toString()
+    .split("")
+    .map((v) => Number(v));
+
+  return Math.max(
+    leftNums.reduce((acc, cur) => (acc += cur), 0),
+    leftNums.reduce((acc, cur) => (acc *= cur), 1),
+    rightNums.reduce((acc, cur) => (acc += cur), 0),
+    rightNums.reduce((acc, cur) => (acc *= cur), 1)
+  );
+}
+
 function problem1(pobi, crong) {
-
-  //책의 범위
-  
-  //Left 값 계산
-
-  //Right 값 계산
-
-  //점수 비교
+  //예외 상황
+  if (!check(pobi) || !check(crong)) {
+    return -1;
+  }
 
   //결과 출력
-  if (maxNum(pobi) > maxNum(crong)) {
+  const pobiMax = maxNum(pobi);
+  const crongMax = maxNum(crong);
+
+  if (pobiMax > crongMax) {
     return 1;
-  } else if (maxNum(pobi) < maxNum(crong)) {
+  } else if (pobiMax < crongMax) {
     return 2;
   } else {
     return 0;
-  }
-
-  //예외 상황
-  if (pobi[1] !== pobi[0]+1 || crong[1] !== crong[0]+1) {
-    return -1;
   }
 }
 
