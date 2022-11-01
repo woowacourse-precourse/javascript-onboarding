0124da344ea8213119d047b8f24b604d34bc6dfe
KUD <kimud6003@gmail.com>
2022-11-01 20:15:29 +0900

    ⚡️ HotFix: problem4.js  아즈키코드 상수 추가 - 숫자만으로 의미를 판단하기 힘들기 떄문에 상수화 - 주석 추가

--- src/problem4.js
+++ src/problem4.js
@@ -3,23 +3,23 @@ function problem4(word) {
   return answer;
 }
 
+// Change the frog's algorithm to the word
 function frogChanger(word) {
-  let wordList = word.split("");
   let result ="";
-
+  let [A_ASCII, Z_ASCII, a_ASCII, z_ASCII , AtoZ, atoz] = [65, 90, 97, 122, 155, 219];
+  let wordList = word.split("");
   for(let char of wordList){
+
     const charCode= char.charCodeAt(0);
-    if(charCode  >= 65 && charCode <= 90) {
-      result += String.fromCharCode(155-charCode);
-    } else if(charCode>= 97 && charCode <= 122) {
-      result += String.fromCharCode(219-charCode);
+    if(A_ASCII<=charCode && charCode <= Z_ASCII) {
+      result += String.fromCharCode(AtoZ-charCode);
+    } else if(a_ASCII<=charCode && charCode <= z_ASCII) {
+      result += String.fromCharCode(atoz-charCode);
     }else {
       result += char;
     }
   }
-
   return result;
-
 }
 
 module.exports = problem4;
