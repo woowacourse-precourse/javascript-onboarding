8b010f2ec8b730e0e88515968b9b823f43164c7e
Mcdonalds Trump <whdmschd2@gmail.com>
2022-11-01 18:25:06 +0900

    feat: [6번 문제] 두 글자 모음 List

--- src/problem6.js
+++ src/problem6.js
@@ -4,6 +4,16 @@ function problem6(forms) {
     nickNameList.push(nickName);
   });
 
+  const twoLetterList = [];
+  nickNameList.map((nickName) => {
+    for (let i = 0; i < nickName.length - 1; i++) {
+      const twoLetterNickName = `${nickName[i]}${nickName[i + 1]}`;
+      if (!twoLetterList.includes(twoLetterNickName)) {
+        twoLetterList.push(twoLetterNickName);
+      }
+    }
+  });
+
   return nickNameList;
 }
 
