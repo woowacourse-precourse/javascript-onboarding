8030c76992e91cffa21eb9f8c9906c3014ce2cd6
Yeonju Jo <yjj0287@gmail.com>
2022-11-01 23:03:51 +0900

    feat: problem1 예외사항 처리

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,19 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isException(pobi, crong)) return -1;
+}
+
+function isException(pobi, crong) {
+  if (pobi[0] === 1 || pobi[1] === 400 || crong[0] === 1 || crong[1] === 400)
+    return true;
+  if (pobi[0] + 1 !== pobi[1] || crong[0] + 1 !== crong[1]) return true;
+  if (
+    pobi[0] % 2 !== 1 ||
+    pobi[1] % 2 !== 0 ||
+    crong[0] % 2 !== 1 ||
+    crong[1] % 2 !== 0
+  )
+    return true;
+  return false;
 }
 
 module.exports = problem1;
