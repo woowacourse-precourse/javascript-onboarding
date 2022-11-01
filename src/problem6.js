2397b99241496e6e21265fe3650d6118f4411f77
ilgon0110 <moses011010@gmail.com>
2022-11-01 14:38:26 +0900

    feat: 예외 처리 기능 구현(#problem6)

--- src/problem6.js
+++ src/problem6.js
@@ -21,7 +21,22 @@ function problem6(forms) {
   let check = Array.from({ length: forms.length }, () => 0);
   //기능목록 2-1 구현
   let result = new Set();
+  //기능목록 3-4 구현
+  if (forms.length < 1 || forms.length > 10000) {
+    throw '크루 수가 허용 범위를 벗어났습니다';
+  }
   for (let i = 0; i < forms.length; i++) {
+    //예외처리
+    //기능목록 3-2 구현
+    const regex = /^[ㄱ-ㅎ|가-힣]+$/;
+    if (
+      !regex.test(forms[i][1]) ||
+      forms[i][1].length < 1 ||
+      forms[i][1].length >= 20
+    )
+      continue;
+    //기능목록 3-3 구현
+    if (forms[i][0].length < 11 || forms[i][0].length > 20) continue;
     //기능목록 1-3 구현
     if (check[i] !== 0) continue;
     //예외처리
