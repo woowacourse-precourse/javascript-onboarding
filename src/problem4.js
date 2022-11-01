3da68538bee43625ce8357cdc983035a4eb60d0e
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-01 12:24:16 +0900

    Problem4: 기본 알파벳 배열과 Reverse 배열 준비

--- src/problem4.js
+++ src/problem4.js
@@ -5,6 +5,11 @@ function problem4(word) {
   2. word를 한 자리씩 잘라 순회하면서 알파벳이면 Reverse 배열에서 변환값 추출
   3. 최종 정답 반환
   */
+  
+  const upperSet = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'.split('');
+  const upperReverseSet = [...upperSet].reverse();
+  const lowerSet = 'abcdefghijklmnopqrstuvwxyz'.split('');
+  const lowerReverseSet = [...lowerSet].reverse();
 }
 
 module.exports = problem4;
