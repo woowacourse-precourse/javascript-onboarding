1a00118f942535c2bbe93ea311443476f5eb2828
jlee0505 <hj960322@gmail.com>
2022-11-01 19:56:50 +0900

    feat(problem4): 기능1 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,74 +1,25 @@
-function generateAsciiChart() {
-  const asciiOfUpperCase = Array.from({ length: 26 }, (v, i) => i + 65);
-  const asciiOfLowerCase = Array.from({ length: 26 }, (v, i) => i + 97);
-
-  return { asciiOfUpperCase, asciiOfLowerCase };
-}
-
-function convertToAscii(sentence) {
-  const ascii = [];
-
-  for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " ") {
-      ascii.push(sentence.charCodeAt(i));
-    }
-  }
-
-  return ascii;
-}
-
-function convertToWord(ascii, asciiChart) {
-  const convertedWords = [];
-
-  ascii.forEach((each) => {
-    if (each >= 65 && each <= 90) {
-      convertedWords.push(handleEach(each, asciiChart.asciiOfUpperCase));
-    }
-    if (each >= 97 && each <= 122) {
-      convertedWords.push(handleEach(each, asciiChart.asciiOfLowerCase));
-    }
-  });
-
-  return convertedWords;
-}
-
-function handleEach(each, asciiChart) {
-  const reversedAsciiChart = reverseArray(asciiChart);
-  let idx = asciiChart.indexOf(each);
-  return String.fromCharCode(reversedAsciiChart[idx]);
-}
-
-function reverseArray(arr) {
-  var newArray = [];
-  for (var i = arr.length - 1; i >= 0; i--) {
-    newArray.push(arr[i]);
-  }
-  return newArray;
-}
-
-function combineWords(convertedWords, sentence) {
-  let result = [];
-
-  for (let i = 0; i < sentence.length; i++) {
-    if (sentence[i] !== " ") {
-      result.push(convertedWords.shift());
-    } else if (sentence[i] == " ") {
-      result.push(" ");
-    }
-  }
-
-  return result.join("");
-}
-
 function problem4(word) {
-  const asciiChart = generateAsciiChart();
-  const asciiFromWord = convertToAscii(word);
-  const wordFromAscii = convertToWord(asciiFromWord, asciiChart);
-  const result = combineWords(wordFromAscii, word);
+  const ascii = convertWordToAscii(word);
+  const result = convertAsciiToWord(ascii);
 
   return result;
 }
 
+function convertWordToAscii(str) {
+  const arrOfAscii = [];
+  for (let char of str) {
+    let ascii = str.charCodeAt(str.indexOf(char));
+    arrOfAscii.push(ascii);
+  }
+  return arrOfAscii;
+}
+
+function convertAsciiToWord(arrOfNum) {
+  // if (ascii < 65 || ascii > 122 || (ascii < 97 && ascii > 90)) return "";
+  // if (ascii === 32) return " ";
+  // if (ascii <= 90) return 90 - ascii + 65;
+  // if (ascii >= 97) return 122 - ascii + 97;
+}
 module.exports = problem4;
 
 //expected: "R olev blf"
