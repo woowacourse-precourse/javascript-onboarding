1165ab775615b2c2cd8d37bc7fc82b28994c92f8
ming0830 <minsun0830@naver.com>
2022-11-01 09:08:21 +0900

    [PROBLEM7] refactor : user의 친구 목록 수정
    중복되는 set과 변수 설정을 없앰.

--- src/problem7.js
+++ src/problem7.js
@@ -5,21 +5,22 @@ function problem7(user, friends, visitors) {
 
 //user의 친구 목록
 function getUserFriends(user,friends){
-  let userFriendSet = [];
+  let userFriends = [];
   for(let friendSet of friends){
     if(friendSet.includes(user)){
-      userFriendSet.push(friendSet);
+    if(friendSet[0] === user){
+    userFriends.push(friendSet[1]);
+    } else if(friendSet[1] === user){
+    userFriends.push(friendSet[0]);
+    }
     }
   } 
-  // 중복 제거 및 user 제외 목록 구현
-  const userFriend = [...new Set(userFriendSet.flat())];
-  let userFriendList = userFriend.filter((ele) => ele !== user);
-
-
-  return userFriendList
-
+  return userFriends
 }
 
+function getFrindOfFriends(friends){
+
+}
 const user = "mrko";
 const friends = [ ["donut", "andole"], ["donut", "jun"], ["donut", "mrko"], ["shakevan", "andole"], ["shakevan", "jun"], ["shakevan", "mrko"] ]  ;
 const visitors =  ["bedi", "bedi", "donut", "bedi", "shakevan"];
