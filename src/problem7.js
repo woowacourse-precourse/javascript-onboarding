729668f1472dafef7d208f38e7271cff1273f4a7
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 15:05:01 +0900

    [p7] 스코어보드 저장

--- src/problem7.js
+++ src/problem7.js
@@ -1,7 +1,7 @@
 function findFriend(user, friends) {
   let friend = [];
 
-  friends.forEach((left, right) => {
+  friends.forEach(([left, right]) => {
     if(left === user)
       friend.push(right);
     if(right === user)
@@ -14,7 +14,7 @@ function findFriend(user, friends) {
 function users(friends) {
   let userlist = new Set();
   
-  friends.forEach((left, right) =>{
+  friends.forEach(([left, right]) =>{
     userlist.add(left);
     userlist.add(right);
   });
@@ -22,15 +22,25 @@ function users(friends) {
   return userlist;
 }
 
+function createScoreboard(userlist){
+  let arr = [];
+  userlist.forEach((idx) => {
+    arr.push([0, idx]);
+  });
+  return arr;
+}
+
 function problem7(user, friends, visitors) {
   userFriend = findFriend(user, friends);
   userlist = users(friends);
-  
-  let answer = [, []];
-  return answer;
+  userlist = Array.from(userlist);
+  let score = createScoreboard(userlist);
+
+  return score;
 }
 //사용자와 친구관계인 유저를 구함
 //유저 목록 저장
+//스코어보드 저장
 //친구와 친구관계인 친구에 +10점
 //방문자
 module.exports = problem7;
