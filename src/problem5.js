66bc69be82c19e8bd2bf59fbc24bb1eee97c5477
cocoa <jwv0323@kau.kr>
2022-11-01 16:35:50 +0900

    problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const KRW = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  let wallet = KRW.map((won) => {
+    count = Math.floor(money / won)
+    money = money % won
+
+    return count;
+  })
+  return wallet;
 }
 
 module.exports = problem5;
