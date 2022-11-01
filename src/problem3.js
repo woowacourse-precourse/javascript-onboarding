a040cb7b5bde79d4ef13552cb398dff98c00e0c8
jihyunlee <jihyunlee.me@gmail.com>
2022-11-01 23:38:00 +0900

    feat: problem3, 숫자 array 만들고 3,6,9마다 카운트 / 문제해결

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,22 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  let arrange = Array.from({length: number}, (value, index) => String(index + 1))
+  for (let i = 0; i < arrange.length; i++) {
+    let splitNumArr = arrange[i].split('')
+    answer += count369(splitNumArr)
+  }
+  
   return answer;
+}
+
+function count369(arr) {
+  let count = 0;
+  arr.forEach(element => {
+    if (element === '3' || element === '6' || element === '9') {
+      count++
+    }
+  })
+  return count;
 }
 
 module.exports = problem3;
