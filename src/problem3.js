513bdf091cf1194b0e6c8767f21f2125a634e227
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:47:36 +0900

    feat(problem3): 3,6,9 유무에 따른 박수횟수를 구한다

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
-function problem3(number) {
-  var answer;
-  return answer;
+function problem3(number) {  
+  let count = 0;
+
+  for(let i=1; i<=number; i++) {
+    let arr = (i+"").split("");
+    for (j=0; j<arr.length; j++) {
+      if((Number(arr[j])=== 3) || (Number(arr[j])=== 6) || (Number(arr[j])=== 9)){
+        count++;
+      }
+    }
+  }
+  return count;
 }
 
 module.exports = problem3;
