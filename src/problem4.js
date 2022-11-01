5b785e4e8cdfc08adefc8347e984e039eed59096
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 16:53:10 +0900

    feat : 아스키코드에 해당하는 대문자,소문자를 규칙에 맞도록 map에 mapping 시켜주는 함수 구현.

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,19 @@
 function problem4(word) {
-  var answer;
-  return answer;
+    var answer = '';
+
+    var map = new Map();
+
+    let reverse = 90;
+    for (let i = 65; i <= 90; i++) {
+        map.set(String.fromCharCode(i), String.fromCharCode(reverse--));
+    }
+
+    reverse = 122;
+    for (let i = 97; i <= 122; i++) {
+        map.set(String.fromCharCode(i), String.fromCharCode(reverse--));
+    }
 }
 
 module.exports = problem4;
+
+//1. 아스키 코드에 해당되는
