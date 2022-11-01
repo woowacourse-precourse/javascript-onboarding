df9446fb99650cdfdfeddaa46d72a3306ca1dd7f
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 23:23:11 +0900

    feat : user의 친구를 구한 후, 친구에 해당하지 않은 추천친구의 점수를 구하여 정렬 한 배열을 구하는 함수 구현.

--- src/problem7.js
+++ src/problem7.js
@@ -2,7 +2,21 @@ function problem7(user, friends, visitors) {
     var answer = [];
 
     let userFriendsList = getUserFriendsList(user, friends);
-    let pointMap = getPointMap(friends, userFriendsList, visitors);
+    let pointMap = getPointMap(user, friends, visitors, userFriendsList);
+    let sortByPoint = [...pointMap.entries()].sort((prev, cur) => {
+        if (prev[1] < cur[1]) return 1;
+        if (prev[1] > cur[1]) return -1;
+        if (prev[0] > cur[0]) return 1;
+        if (prev[0] < cur[0]) return -1;
+    });
+
+    sortByPoint.some((element) => {
+        answer.push(element[0]);
+        if (answer.length === 5) {
+            return true;
+        }
+    });
+    console.log(answer);
 
     return answer;
 }
@@ -11,25 +25,39 @@ function getUserFriendsList(user, friends) {
     let arr = [];
     friends.map((element) => {
         if (element.includes(user)) {
-            arr.push(element[0]);
+            element[0] === user ? arr.push(element[1]) : arr.push(element[0]);
         }
     });
     return arr;
 }
 
-function getPointMap(friends, userFriendsList, visitors) {
+function getPointMap(user, friends, visitors, userFriendsList) {
     let map = new Map();
     friends.map((element) => {
-        let userFriend = element[0];
-        let recommendFriend = element[1];
+        if (element[0] === user || element[1] === user) return;
+        if (
+            userFriendsList.includes(element[0]) &&
+            userFriendsList.includes(element[1])
+        )
+            return;
+        if (
+            !userFriendsList.includes(element[0]) &&
+            !userFriendsList.includes(element[1])
+        )
+            return;
 
-        if (userFriendsList.includes(userFriend) && recommendFriend !== user) {
-            if (map.get(recommendFriend) === undefined) {
-                map.set(recommendFriend, 10);
-            } else {
-                let point = map.get(recommendFriend);
-                map.set(recommendFriend, point + 10);
-            }
+        let userFriend = userFriendsList.includes(element[0])
+            ? element[0]
+            : element[1];
+        let recommendFriend = userFriendsList.includes(element[0])
+            ? element[1]
+            : element[0];
+
+        if (map.get(recommendFriend) === undefined) {
+            map.set(recommendFriend, 10);
+        } else {
+            let point = map.get(recommendFriend);
+            map.set(recommendFriend, point + 10);
         }
     });
 
@@ -46,5 +74,4 @@ function getPointMap(friends, userFriendsList, visitors) {
 
     return map;
 }
-
 module.exports = problem7;
