40624775c28759af0d113221e1743face3466bda
kimeuns <dmstn2006@gmail.com>
2022-11-01 17:34:57 +0900

    문제3 refactor

--- src/problem3.js
+++ src/problem3.js
@@ -3,25 +3,14 @@ function problem3(number) {
 
   for (let i = 1;i<number+1;i++){
     let nums = String(i).split("")
-    answer+=clap(nums)
+    answer+=has369(nums)
   }
   return answer;
 }
 
-function clap(nums){
-  let answer = 0;
-  for(let num of nums){
-    has369(num)?answer+=1:null
-  }
-  return answer
-}
-
-function has369(num){
+function has369(nums){
   const arr369 = ["3", "6", "9"];
-  if(arr369.includes(num)){
-    return true
-  }
-  return false
+  return nums.filter(num=>arr369.includes(num)).length
 }
 
 module.exports = problem3;
