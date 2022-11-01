26375957e3609a13586714f6f3dbddfda9e42326
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:25:56 +0900

    feat: [problem4] 대문자 알파벳 확인

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,10 @@
 function problem4(word) {
   var answer;
   return answer;
+const isUpperAlphabet = (char) => {
+  const regExp = /[A-Z]/g; // 영어
+  return regExp.test(char);
+}
 }
 
 module.exports = problem4;
