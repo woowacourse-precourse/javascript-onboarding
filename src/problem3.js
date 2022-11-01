457a98604c2f6833a76b9f6f6eecb38c613f3f9a
ksone02 <eksovus@gmail.com>
2022-11-01 18:12:51 +0900

    [feat] problem3: number까지의 수 중 3, 6, 9가 포함되는 숫자 횟수 세기

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
+/*
+  기능 목록
+  1.  number까지의 수 중 3, 6, 9가 포함되는 숫자 횟수 세기
+*/
+
 function problem3(number) {
-  var answer;
-  return answer;
+  let slapCount = 0;
+  for (let i = 1; i <= number; i++) {
+    const numberToArray = i.toString().split("");
+    numberToArray.forEach((number) => {
+      if (number === "3" || number === "6" || number === "9") slapCount += 1;
+    });
+  }
+  return slapCount;
 }
 
 module.exports = problem3;
