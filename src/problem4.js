27cdd83ea717c74f745b14a4c959181ffd376598
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 11:48:00 +0900

    Feat: problem4 - 알파벳 대소문자 전체와 뒤집은 문자 전체를 담는 변수 생성

--- src/problem4.js
+++ src/problem4.js
@@ -1,28 +1,7 @@
 function problem4(word) {
-  let LowerStr = {};
-  let UpperStr = {};
-
-  for (let i = 97; i <= 122; i++) {
-    LowerStr[String.fromCharCode(i)] = String.fromCharCode(219 - i);
-    UpperStr[String.fromCharCode(i - 32)] = String.fromCharCode(219 - i - 32);
-  }
-  findAnswer(LowerStr, UpperStr, word);
+  const LowerStr = {};
+  const UpperStr = {};
 }
-
-const findAnswer = (LowerStr, UpperStr, word) => {
-  let answer = "";
-  for (let i = 0; i < word.length; i++) {
-    if (LowerStr[word[i]]) {
-      answer += LowerStr[word[i]];
-    } else if (UpperStr[word[i]]) {
-      answer += UpperStr[word[i]];
-    } else {
-      answer += word[i];
-    }
-  }
-  console.log(answer);
-  return answer;
-};
 
 problem4("I love you");
 module.exports = problem4;
