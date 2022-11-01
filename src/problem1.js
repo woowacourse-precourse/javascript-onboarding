31e9f8589ec4b79262df911e978a1b814c42e2e1
hvvany <kjunhwan1998@gmail.com>
2022-11-01 20:36:38 +0900

    feat(build): add big number function

--- src/problem1.js
+++ src/problem1.js
@@ -3,4 +3,19 @@ function problem1(pobi, crong) {
   return answer;
 }
 
+function big_number(page_lst) {
+  const answer = [];
+  for (let num of page_lst) {
+    let page_multiple = 1;
+    let page_sum = 0;
+    let text = num.toString();
+    for (let n of text) {
+      page_multiple *= Number(n);
+      page_sum += Number(n);
+    }
+    answer.push(page_multiple);
+    answer.push(page_sum);
+  }
+  return Math.max.apply(null,answer);
+}
 module.exports = problem1;
