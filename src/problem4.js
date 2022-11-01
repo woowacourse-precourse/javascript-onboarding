5ab5363ac59aaabd29c9a99d9761ef4a6930ebdf
jihyunlee <jihyunlee.me@gmail.com>
2022-11-01 23:39:12 +0900

    feat: problem4, 문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,23 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let frogWord = '';
+  let upperCodeA = 65;
+  let upperCodeZ = 90;
+  let lowerCodeA = 97;
+  let lowerCodeZ = 122;
+
+  let wordsArr = [...word]
+
+  wordsArr.forEach((element, index) => {
+      let codeNum = word.charCodeAt(index)
+      if (codeNum >= upperCodeA && codeNum <= upperCodeZ) { 
+        frogWord += String.fromCharCode(upperCodeZ + upperCodeA - codeNum);
+      } else if (codeNum >= lowerCodeA && codeNum <= lowerCodeZ) { 
+        frogWord += String.fromCharCode(lowerCodeZ + lowerCodeA - codeNum);   
+      } else {
+        frogWord += element;
+      }
+    })
+  return frogWord;
 }
 
 module.exports = problem4;
