4c7c64d209fe7bc79d3c566b0afcdda9f9787511
윤동주 <yoondj98@naver.com>
2022-11-01 20:47:18 +0900

    우아한 테크코스 프리코슥 1주차 4번 기능구현 완료

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,15 @@
 function problem4(word) {
-  var answer;
+  var arr_word = word.toString();
+  var answer="";
+  for(var i=0; i<arr_word.length; i++){
+    if(arr_word.charCodeAt(i)>=65 && arr_word.charCodeAt(i)<=90){
+      answer = answer.concat(String.fromCharCode(155-arr_word.charCodeAt(i)));
+    }
+    else if(arr_word.charCodeAt(i)>=97 && arr_word.charCodeAt(i)<=122){
+      answer = answer.concat(String.fromCharCode(219-arr_word.charCodeAt(i)));
+    }
+    else answer = answer.concat(arr_word[i]);
+  }
   return answer;
 }
 
