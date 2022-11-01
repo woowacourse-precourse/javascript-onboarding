b1717d7a6487a471bc8d8cb05b0138d162dca236
7ngenious <zxvs1218@gmail.com>
2022-11-01 22:21:41 +0900

    comment: [Problem3] Add Solution Comment

--- src/problem3.js
+++ src/problem3.js
@@ -1,14 +1,17 @@
 function problem3(number) {
   let answer = 0;
   for (let i = 1; i <= number; i++) {
-    let cnt = 0;
+    //주어진 숫자만큼 반복
+    let cnt = 0; //손뼉 카운트
     for (let j of String(i)) {
+      //해당 숫자를 문자열로 바꾸어 안에 개수만큼 반복
       if (j === '3' || j === '6' || j === '9') cnt++;
+      //3이나 6이나 9가 들어갈 때 카운팅
     }
-    answer += cnt;
+    answer += cnt; //결과값에 더해주기
   }
 
-  return answer;
+  return answer; //결과 반환
 }
 
 module.exports = problem3;
