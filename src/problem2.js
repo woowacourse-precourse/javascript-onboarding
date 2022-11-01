618a3cf9ef2bf956163f82ba10d3d0b028d86427
Hayeong Kim <fibonacci2246@gmail.com>
2022-11-01 18:52:08 +0900

    problem 2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,14 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  const result = cryptogram.split("").reduce((acc, curr) => {
+    if (acc[acc.length - 1] === curr) {
+      acc.pop();
+    } else {
+      acc.push(curr);
+    }
+    return acc;
+  }, []);
+
+  return result.join("");
 }
 
 module.exports = problem2;
