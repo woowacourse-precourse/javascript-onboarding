c6450c1667075494a32368bfd7cf5985beb0e8dc
seungki <sysh9498@gmail.com>
2022-11-01 18:19:27 +0900

    문자열에서 소문자를 변환하는 부분과 공백 처리하는 부분을 구현

--- src/problem4.js
+++ src/problem4.js
@@ -3,6 +3,10 @@ function problem4(word) {
   for (var i = 0; i < word.length; i++) {
     if (word[i] >= 'A' && word[i] <= 'Z') {
       answer += String.fromCharCode(155 - word[i].codePointAt(0));
+    } else if (word[i] >= 'a' && word[i] <= 'z') {
+      answer += String.fromCharCode(219 - word[i].codePointAt(0));
+    } else if (word[i] == ' ') {
+      answer += ' ';
     }
   }
   return answer;
