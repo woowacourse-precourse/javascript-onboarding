299dd38717a0b4c136e771d97db8f1cb305a3232
banma1234 <banma1234@gmail.com>
2022-11-01 18:22:40 +0900

    problem_6 : 예외처리 함수

--- src/problem6.js
+++ src/problem6.js
@@ -3,4 +3,11 @@ function problem6(forms) {
   return answer;
 }
 
+function errorHandler(forms) {
+  /* forms의 크기가 10000보다 큰 경우
+  나머지 예외처리는 실행 과정에서 처리 */
+  if (forms.length > 10000) return true;
+  else return false;
+}
+
 module.exports = problem6;
