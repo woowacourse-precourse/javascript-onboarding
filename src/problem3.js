271eb8168d624e5c5b376da60a425c439f3040a9
HaroldBKim <sbookim2@gmail.com>
2022-11-01 22:43:20 +0900

    solved problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
-function problem3(number) {
-  var answer;
+function problem3(indexber) {
+  var answer = 0;
+  for(let i = 1; i <= indexber; i++) {
+    let index = i;
+    while(index > 0) {
+      if(index % 10 === 3 || index % 10 === 6 || index % 10 === 9) {
+        answer += 1;
+      }
+      index = parseInt(index / 10);
+    }
+  }
   return answer;
 }
 
