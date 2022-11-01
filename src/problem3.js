279d2b0fcec6a571a8d35ebacd27b9a26c303020
hvvany <kjunhwan1998@gmail.com>
2022-11-01 20:57:55 +0900

    feat(build): solve problem_3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
   let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    let now = i;
+
+    while (now != 0) {
+      if (now % 10 == 3 || now % 10 == 6 || now % 10 == 9) {
+        answer += 1;
+      }
+      now = Math.floor(now / 10);
+    }
+  }
   return answer;
 }
 
