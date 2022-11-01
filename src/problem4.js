a57d222e3894604a91cd49dc522982c6f67968a9
Wooseong Kim <kimwooseong8313@gmail.com>
2022-11-01 19:09:29 +0900

    feat: solve problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,26 @@
+function check(word) {
+  if (word.charCodeAt() >= 97 && word.charCodeAt() <= 122) {
+    return "lowerCase";
+  }
+  if (word.charCodeAt() >= 65 && word.charCodeAt() <= 90) {
+    return "upperCase";
+  }
+}
+
 function problem4(word) {
-  var answer;
+  let answer = "";
+  word = [...word];
+
+  word.forEach((v) => {
+    if (check(v) === "lowerCase") {
+      answer += String.fromCharCode(219 - v.charCodeAt());
+    } else if (check(v) === "upperCase") {
+      answer += String.fromCharCode(155 - v.charCodeAt());
+    } else {
+      answer += v;
+    }
+  });
+
   return answer;
 }
 
