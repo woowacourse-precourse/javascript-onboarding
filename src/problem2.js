0e5581fb6379073585cb7c765be1569f88dac073
MunYongJang <dltk456@naver.com>
2022-11-01 19:04:54 +0900

    problem2 : 중복 문자 제거 함수

--- src/problem2.js
+++ src/problem2.js
@@ -3,4 +3,17 @@ function problem2(cryptogram) {
   return answer;
 }
 
+//중복 문자 제거 함수
+function deleteChar(str) {
+  var result = '';
+  for (var i = 0; i < str.length; i++) {
+    if (i === str.length - 1 || str[i] !== str[i + 1]) {
+      result += str[i];
+    } else {
+      i += 1;
+    }
+  }
+  return result;
+}
+
 module.exports = problem2;
