19c151968e3f84ba2512291df897a77b4fdc468b
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-11-01 21:35:22 +0900

    ♻️ problem4 리팩토링
    - 정규표현식 활용해서 불필요한 함수 제거

--- src/problem4.js
+++ src/problem4.js
@@ -3,25 +3,16 @@ const ASCII_UPPER_Z = 90;
 const ASCII_LOWER_A = 97;
 const ASCII_LOWER_Z = 122;
 
-const isAlpha = (char) => char.match(/[a-zA-Z]/i);
-
-const isUpperCase = (code) => code <= ASCII_UPPER_Z;
+const isUpperCase = (char) => char.match(/[A-Z]/);
 
 const getFrogChar = (char) => {
-  if (!isAlpha(char)) return char;
-
   const asciiCode = char.charCodeAt(0);
 
-  return isUpperCase(asciiCode)
+  return isUpperCase(char)
     ? String.fromCharCode(ASCII_UPPER_A + ASCII_UPPER_Z - asciiCode)
     : String.fromCharCode(ASCII_LOWER_A + ASCII_LOWER_Z - asciiCode);
 };
 
-const problem4 = (word) => {
-  return word
-    .split('')
-    .map((char) => getFrogChar(char))
-    .join('');
-};
+const problem4 = (word) => word.replace(/[a-zA-Z]/g, getFrogChar);
 
 module.exports = problem4;
