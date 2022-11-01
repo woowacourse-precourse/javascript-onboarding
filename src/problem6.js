0673e840d18e33c1c16951d54f9345c4a87dd943
jinyong <zhaqh146@naver.com>
2022-11-01 14:14:57 +0900

    solve problem6

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,41 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let result = [];
+  let check = new Array(forms.length).fill(0);
+  let names = new Object();
+
+  for (let i = 0; i < forms.length; i++) {
+    let nickname = forms[i][1];
+
+    if (nickname.length > 1) {
+      
+      for (let j = 0; j < nickname.length - 1; j++) {
+        let subName = nickname[j] + nickname[j + 1]
+
+        if (subName in names) {
+          names[`${subName}`].push(i);
+        } else {
+          names[`${subName}`] = [i]
+        }
+      }
+    }
+  }
+
+  for (let name in names) {
+    let duplicate = names[name]
+
+    if (duplicate.length === 1) {
+      continue
+    }
+    
+    for (let idx of duplicate) {
+      if (check[idx] == 0) {
+        result.push(forms[idx][0])
+      }
+    }
+  }
+  
+  result.sort()
+  return result;
 }
 
 module.exports = problem6;
