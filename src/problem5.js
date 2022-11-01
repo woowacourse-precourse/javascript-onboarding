20e11ad5adcbf87d20706807700de3987cd7487c
areumsheep <areumsheep@gmail.com>
2022-11-01 21:14:38 +0900

    refactor: 문제5 상수 금액 구분자 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,8 @@
 const MESSAGES = Object.freeze({
   ERROR_RANGE: '금액은 1 이상 1,000,000 이하의 자연수여야 합니다.',
 });
-const MAX_MONEY_RANGE = 1000000;
-const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+const MAX_MONEY_RANGE = 1_000_000;
+const MONEY_UNIT = [50_000, 10_000, 5_000, 1_000, 500, 100, 50, 10, 1];
 
 function problem5(money = 0) {
   const result = [];
