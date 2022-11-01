7018aa39f38d8ac1f87a3e2d40fdb60ef6e9dab7
Jiwon-Jeong99 <jjwzzy4643@gmail.com>
2022-11-01 18:56:50 +0900

    FEAT: 7번 - user와 친구인 사람을 찾는 메서드 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,30 @@
+// 사람을 중복없이 한번에 모아주는 함수(user, friend 포함)
+function people(friends, visitors) {
+  const people = [];
+  people.push(...visitors);
+  friends.forEach((element) => people.push(element[0], element[1]));
+  const setPeople = new Set(people);
+
+  return setPeople;
+}
+
+// user와 친구인 사람을 찾는 함수
+function findFriend(user, friends) {
+  let userFriend = [];
+  friends.forEach((element) => {
+    if (element.includes(user)) {
+      if (element[0] === user) {
+        userFriend.push(element[1]);
+      } else if (element[1] === user) {
+        userFriend.push(element[0]);
+      }
+    }
+  });
+  return userFriend;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  let answer;
   return answer;
 }
 
