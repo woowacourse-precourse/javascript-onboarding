df0980799a8b19fc88c8e74e30826f0830555070
Deemou <4106243@naver.com>
2022-11-01 22:36:39 +0900

    refactor<P2>: 중복제거함수 분리, top 변수 사용
    removeConsecutiveDuplicates()에서 isEqual() 분리

--- src/problem2.js
+++ src/problem2.js
@@ -8,7 +8,8 @@ const removeConsecutiveDuplicates = (text) => {
   const stack = [text[0]];
   let dupeCount = 0;
   for (let i = 1; i < text.length; i++) {
-    if (stack[stack.length - 1] == text[i]) {
+    let top = stack[stack.length - 1];
+    if (isEqual(top, text[i])) {
       dupeCount++;
       continue;
     }
@@ -16,11 +17,16 @@ const removeConsecutiveDuplicates = (text) => {
       dupeCount = 0;
       stack.pop();
     }
-    if (stack[stack.length - 1] == text[i]) stack.pop();
+    top = stack[stack.length - 1];
+    if (isEqual(top, text[i])) stack.pop();
     else stack.push(text[i]);
   }
   if (dupeCount) stack.pop();
   return stack.join("");
+};
+
+const isEqual = (a, b) => {
+  return a === b;
 };
 
 module.exports = problem2;
