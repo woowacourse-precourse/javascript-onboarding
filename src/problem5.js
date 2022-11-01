392d9f0025a3fe27cdd0aae6a47a0a5595a42837
woowoni <ewoowon.2@gmail.com>
2022-11-01 22:12:14 +0900

    [문제5] 화폐단위카운트 해결

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  let answer = [];
+  const MONEY_UNIT = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  for (const unit of MONEY_UNIT) {
+    let count = (money - (money % unit)) / unit;
+    money = money % unit;
+    answer.push(count);
+  }
   return answer;
 }
 
