9cbac4a1f162e3f4bd039e1bc34c3d409cf09690
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-01 16:13:54 +0900

    refactor: 코드 리펙토링

--- src/problem5.js
+++ src/problem5.js
@@ -1,18 +1,25 @@
+function sheetChashCheck(currentCash, cashUnit) {
+  return parseInt(currentCash / cashUnit);
+}
+function currentCashCheck(sheetCash, cashUnit) {
+  return sheetCash * cashUnit;
+}
+
 function problem5(money) {
   const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const won = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let num = 0;
-  let count = 0;
-  let local = money;
-  for (let x of won) {
-    if (local % x === local) {
-      count++;
+  let sheetCash = 0;
+  let index = 0;
+  let currentCash = money;
+  for (let cashUnit of won) {
+    if (currentCash % cashUnit === currentCash) {
+      index++;
       continue;
     }
-    num = parseInt(local / x);
-    answer[count] = num;
-    local -= num * x;
-    count++;
+    sheetCash = sheetChashCheck(currentCash, cashUnit);
+    answer[index] = sheetCash;
+    currentCash -= currentCashCheck(sheetCash, cashUnit);
+    index++;
   }
   return answer;
 }
