c169a40dc9cdb74db258dfe2c9a2e0c066fd3d46
oseunghyun <ohsh1477@gmail.com>
2022-11-01 09:48:49 +0900

    feat(p4): 단어를 청개구리 사전을 기준으로 변환해주는 기능 구현

--- src/problem4.js
+++ src/problem4.js
@@ -8,6 +8,19 @@ for (i = 0; i < 26; i++) {
   treefrogDic[String.fromCharCode(97 + i)] = String.fromCharCode(122 - i);
 }
 
+// 단어를 청개구리 사전을 기준으로 변환해주는 기능
+const convertToTreefrog = (word) => {
+  let convertedWord = [];
+  for (i = 0; i < word.length; i++) {
+    for (j = 0; j < Object.keys(treefrogDic).length; j++) {
+      if (word[i] == Object.keys(treefrogDic)[j]) {
+        convertedWord.push(Object.values(treefrogDic)[j]);
+      }
+    }
+  }
+  return convertedWord;
+};
+
 function problem4(word) {
   var answer;
   return answer;
