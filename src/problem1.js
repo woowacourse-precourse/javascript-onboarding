75f83807ecb8acae49edb8061a275fcb17f68246
OrangeChick <retww@naver.com>
2022-11-01 19:38:35 +0900

    feat: 🎒[Problem1] Create pobi and crong's score array
    - pobi와 crong의 각 점수의 배열을 만들어주었습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,20 @@
 function problem1(pobi, crong) {
   var answer;
+
+  const pobiScore = [
+    sum(pobi[0]),
+    sum(pobi[1]),
+    multiplication(pobi[0]),
+    multiplication(pobi[1]),
+  ];
+
+  const crongScore = [
+    sum(crong[0]),
+    sum(crong[1]),
+    multiplication(crong[0]),
+    multiplication(crong[1]),
+  ];
+
   return answer;
 }
 
