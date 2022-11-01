93babe31bc5b66a667fdfb9e8c5f47ca9510ec36
dongyeopca <ckehdduq95@gmail.com>
2022-11-01 15:00:24 +0900

    feat: "Problem4 기능 구현" 청개구리 법칙 함수(frogRule) 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,20 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let answer = word.split(" ");
+  for (let i = 0; i < answer.length; i++) {
+    answer[i] = frogRule(answer[i]);
+  }
+  return answer.join(" ");
 }
 
+function frogRule(s) {
+  let answer = "";
+  for (const value of s) {
+    if (value === value.toUpperCase()) {
+      answer += String.fromCharCode(155 - value.charCodeAt(0));
+    } else {
+      answer += String.fromCharCode(219 - value.charCodeAt(0));
+    }
+  }
+  return answer;
+}
 module.exports = problem4;
