b2f14cbfd033462bb436dc85243973e6df320f4a
JAESEOK PARK <jaesukpark77@naver.com>
2022-11-01 09:14:56 +0900

    feat: [6번 문제] 기능 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,20 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let answer = [];
+  let map = new Map();
+
+  forms.map(([email, korTrans]) => {
+    for (let i = 1; i < korTrans.length; i++) {
+      let cur = korTrans[i - 1] + korTrans[i];
+      map.set(cur, [...(map.get(cur) || []), email]);
+    }
+  });
+
+  for (item of map.values()) {
+    if (item.length > 1) {
+      answer = [...answer, ...item];
+    }
+  }
+  return answer.sort();
 }
 
 module.exports = problem6;
