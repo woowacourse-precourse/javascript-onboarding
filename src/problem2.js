3781d1d6cdf1fa44c2b2ff9eaefbf6300b577d26
khkh0109 <ist200120@gmail.com>
2022-11-01 17:24:45 +0900

    fix: problem2 버그 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,10 @@
 function problem2(cryptogram) {
   for(let i = 0; true; i++) {
-    const regExp = /aa+|bb+|cc+|dd+|ee+|ff+|gg+|hh+|ii+|jj+|ll+|mm+|nn+|oo+|pp+|qq+|rr+|ss+|tt+|uu+|vv+|ww+|xx+|yy+|zz+/g;
+    const regExp = /aa+|bb+|cc+|dd+|ee+|ff+|gg+|hh+|ii+|jj+|kk+|ll+|mm+|nn+|oo+|pp+|qq+|rr+|ss+|tt+|uu+|vv+|ww+|xx+|yy+|zz+/g;
     const result = cryptogram.replace(regExp, '');
     cryptogram = result;
     if(regExp.test(cryptogram) === false) {
+      console.log(cryptogram)
       return cryptogram;
     }
   }
