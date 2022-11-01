4cd99eea6c560d16d3f629caa642d8d43878b611
terry <kdjtiger@naver.com>
2022-11-01 11:51:21 +0900

    몫을 구하고 몫을 배열에 푸쉬 후 몫과 단위 곱해서 주어진 값에서 빼기

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,17 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const moneyArr = [50000,10000,5000,1000,500,100,50,10,1];
+  let newMoneyArr = [];  
+
+  for (element of moneyArr){
+    let quotient = parseInt(money/element);
+    newMoneyArr.push(quotient);
+    money -= quotient*element;
+  }
+  return newMoneyArr
 }
 
 module.exports = problem5;
+
+console.log(problem5(50237));
+console.log(problem5(15000));
+console.log(problem5(615000));
