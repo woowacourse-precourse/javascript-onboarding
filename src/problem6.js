b59357406211850251e2f377e4f56dea2121ac18
violetwhenisawu <anashape710@gmail.com>
2022-11-01 23:01:31 +0900

    Docs: problem6 함수 주석 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,12 +1,16 @@
 function problem6(forms) {
   let emailArr = [];
+
+  //forms의 요소들을 도는 반복문
   for (let i = 0; i < forms.length; i++) {
     for (let j = 0; j < forms.length - 1; j++) {
       let [name1, name2] = [forms[i][1], forms[j][1]];
       let [email1, email2] = [forms[i][0], forms[j][0]];
+
       if (name1 === name2) {
         break;
       }
+
       if (checkOverlap(name1, name2)) {
         emailArr.push(email1);
         emailArr.push(email2);
@@ -18,6 +22,7 @@ function problem6(forms) {
   return sortedArr;
 }
 
+// 모든 이름을 두글자 기준으로 중복 글자가 있는지 검사하는 함수
 function checkOverlap(name1, name2) {
   for (let k = 0; k < name1.length - 1; k++) {
     let nameTwoChar = name1.substring(k, k + 2);
@@ -28,12 +33,14 @@ function checkOverlap(name1, name2) {
   return false;
 }
 
+// 이메일의 중복을 제거하는 함수
 function deleteOverlap(arr) {
   let set = new Set(arr);
   let uniqueArr = [...set];
   return uniqueArr;
 }
 
+// 이메일을 오름차순으로 정렬하는 함수
 function sortByAscending(arr) {
   let sortedArr = arr.sort();
   return sortedArr;
