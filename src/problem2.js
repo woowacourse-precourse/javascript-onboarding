3e9b1f8e8dfd3eda0e821fb73b73789c7e16c37f
mumuhi <whtjdus97@naver.com>
2022-11-01 23:23:28 +0900

    인접한 문자열 제거

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,27 @@
 function problem2(cryptogram) {
   var answer;
+
+  let arr = cryptogram.split("")
+  let k = 0;
+  let i = 0;
+  for(i = 1; i < arr.length; i++){
+    if(arr[i-1] != arr[i]){
+      arr[k++] = arr[i-1];
+    }
+    else{
+      while(i < arr.length && arr[i-1] == arr[i]){
+        i++;
+      }
+    }
+  }
+  arr[k++] = arr[i-1];
+  
+
+  if(k != arr.length){
+    return problem2(arr.substring(0,k));
+  }
+  
+  answer = cryptogram;
   return answer;
 }
 
