1a1029869cf0315e832d4f9d20dff88c533ff3a4
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 11:24:15 +0900

    Feat: problem3 - 숫자를 1부터 number까지 문자열로 바꾸는 반복문 생성

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,9 @@
 function problem3(number) {
   let answer = 0;
   const targetNumbers = ["3", "6", "9"];
+
+  for (let num = 1; num <= number; num++) {
+    const strNum = num.toString();
+  }
 }
 module.exports = problem3;
