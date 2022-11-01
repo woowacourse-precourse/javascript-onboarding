4249c98978a46ae471900a073f041c77ccbec1aa
areumsheep <areumsheep@gmail.com>
2022-11-01 20:56:45 +0900

    refactor: 문제4 주석 및 예외 조건 추가

--- src/problem4.js
+++ src/problem4.js
@@ -1,10 +1,15 @@
-const ALPHABET_ASCII_CODE = {
+const MESSAGES = Object.freeze({
+  ERROR_RANGE: '입력된 문자는 1 이상 1,000 이하의 문자열이여야 합니다.',
+});
+const ALPHABET_ASCII_CODE = Object.freeze({
   A: 65,
   Z: 90,
   a: 97,
   z: 122,
-};
+});
 const ALPHABET_COUNT = 25;
+
+// 각 알파벳의 중간 문자 확인
 const middleUpperASCII = parseInt(
   (ALPHABET_ASCII_CODE.A + ALPHABET_ASCII_CODE.Z) / 2
 );
@@ -12,42 +17,50 @@ const middleLowerASCII = parseInt(
   (ALPHABET_ASCII_CODE.a + ALPHABET_ASCII_CODE.z) / 2
 );
 
+function problem4(word = '') {
+  const splitWords = word.split('');
+  const wordsLength = splitWords.length;
+  if (wordsLength < 1 || wordsLength > 1000) return MESSAGES.ERROR_RANGE;
+  const formattedWords = splitWords.reduce((acc, cur) => {
+    return (acc += convertByASCII(cur.charCodeAt(0)));
+  }, '');
+
+  return formattedWords;
+}
+
+/** 알파벳 시작 문자 ~ 중간 문자까지의 계산 */
 const smaller = (currentASCII = 0, ASCII_CODE = 0) => {
   return String.fromCharCode(
     currentASCII + ALPHABET_COUNT - 2 * (currentASCII - ASCII_CODE)
   );
 };
+/** 알파벳 중간 문자 ~ 끝 문자까지의 계산 */
 const bigger = (currentASCII = 0, ASCII_CODE) => {
   return String.fromCharCode(
     currentASCII - ALPHABET_COUNT + 2 * (ASCII_CODE - currentASCII)
   );
 };
-const calculateASCIICode = (currentASCII = 0) => {
-  if (
+
+/** 청개구리 규칙에 맞게 아스키코드 반환 */
+const convertByASCII = (currentASCII = 0) => {
+  const isUpperCase =
     currentASCII >= ALPHABET_ASCII_CODE.A &&
-    currentASCII <= ALPHABET_ASCII_CODE.Z
-  ) {
+    currentASCII <= ALPHABET_ASCII_CODE.Z;
+  const isLowerCase =
+    currentASCII >= ALPHABET_ASCII_CODE.a &&
+    currentASCII <= ALPHABET_ASCII_CODE.z;
+
+  if (isUpperCase) {
     return currentASCII <= middleUpperASCII
       ? smaller(currentASCII, ALPHABET_ASCII_CODE.A)
       : bigger(currentASCII, ALPHABET_ASCII_CODE.Z);
   }
-  if (
-    currentASCII >= ALPHABET_ASCII_CODE.a &&
-    currentASCII <= ALPHABET_ASCII_CODE.z
-  ) {
-    return currentASCII <= middleUpperASCII
+  if (isLowerCase) {
+    return currentASCII <= middleLowerASCII
       ? smaller(currentASCII, ALPHABET_ASCII_CODE.a)
       : bigger(currentASCII, ALPHABET_ASCII_CODE.z);
-  } else return String.fromCharCode(currentASCII);
+  }
+  return String.fromCharCode(currentASCII);
 };
-
-function problem4(word = '') {
-  const splitWords = word.split('');
-  const formattedWords = splitWords.reduce((acc, cur) => {
-    return (acc = acc + calculateASCIICode(cur.charCodeAt(0)));
-  }, '');
-
-  return formattedWords;
-}
 
 module.exports = problem4;
