12cf335e26413ff2fbd89954251783aeb08ba374
banma70 <banma1234@gmail.com>
2022-11-01 14:53:57 +0900

    problem_4 : solved

--- src/problem4.js
+++ src/problem4.js
@@ -1,11 +1,17 @@
 function problem4(word) {
+  // 글자수 예외처리
+  if(errorHandler(word))  return null;
+
   var answer;
+  // 아스키코드 변환 함수 호출
+  answer = wordToAscii(word);
+
   return answer;
 }
 
 function errorHandler(param) {
   // param이 숫자일 경우
-  if (isNum(param)) return true;
+  if (!!Number(param)) return "Error";
   // param이 문자일 경우
   else{
     // 1000의 자리를 넘을 경우 예외처리
@@ -15,6 +21,7 @@ function errorHandler(param) {
   return false;
 }
 
+// 아스키코드 변환 함수
 function wordToAscii(word) {
   // 아스키코드를 담을 변수
   result = '';
@@ -26,7 +33,7 @@ function wordToAscii(word) {
       // 아스키코드로 변환
       var temp = word.charCodeAt(i);
       // 알파벳 예외처리
-      if (!isNum(reverseAscii(temp))) {
+      if (isNaN(reverseAscii(temp))) {
         return "Error : 알파벳이 아닌 문자를 입력하셨습니다."
       }
 
@@ -36,18 +43,27 @@ function wordToAscii(word) {
       result += temp;
     }
   }
+
+  return result;
 }
 
+// 청개구리 사전 함수
 function reverseAscii(num) {
+  // 소문자의 합
   const lowerCase = 97 + 122;
+  // 대문자의 합
   const upperCase = 65 + 90;
-
+  // 변환된 값을 담을 변수;
   var result = 0;
 
   // 대문자일 경우
-  if (65 <= num <= 90) result = upperCase - num;
+  if (num >= 65 && num <= 90) {
+    result = upperCase - num;
+  }
   // 소문자일 경우
-  else if (97 <= num <= 122)  result = lowerCase - num;
+  else if (num >= 97 && num <= 122){
+    result = lowerCase - num;
+  }
   // 알파벳이 아닌 경우 예외처리
   else  return errorHandler(num);
 
