898ba561065401cf2d4244032c41a814900cf85b
mun9769 <mun9769@naver.com>
2022-11-01 11:04:07 +0900

    Update problem5.js
    cash는 화폐단위를 저장한 일차원 배열입니다.
    돈을 화폐단위로 나누어 얻은 몫을 answer에 삽입하는 for문을 돕니다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,11 @@
+const cash = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 function problem5(money) {
-  var answer;
-  return answer;
+    var answer = [];
+    for (var i = 0; i < cash.length; i++) {
+        var cnt = Math.floor(money / cash[i]); // 나눌때 floor를 잊지말아야한다.
+        money -= cnt * cash[i];
+        answer.push(cnt);
+    }
+    return answer;
 }
-
 module.exports = problem5;
