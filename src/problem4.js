93ca9f4cb78506fae9396b34edc2d8751dc35346
wjdtjdgns <person3275@gmail.com>
2022-11-01 20:20:48 +0900

    입력받은 character를 변환하여 반환하는 함수 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
 function problem4(word) {
-  var answer;
+  
   return answer;
+}
+
+// 입력받은 character를 변환하여 반환하는 함수
+function convert(str) {
+  const alphabet = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz ";
+  const reverse =  "ZYXWVUTSRQPONMLKJIHGFEDCBAzyxwvutsrqponmlkjihgfedcba ";
+  const i = alphabet.indexOf(str);
+  return reverse.charAt(i);
 }
 
 module.exports = problem4;
