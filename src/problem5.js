37440fe35ddb6ca69a0ea2a26b2cbde8acc74f0b
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 12:40:20 +0900

    Feat: problem5 - money를 큰 지폐 단위부터 나누는 반복문 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,8 @@
 function problem5(money) {
   const banknotes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let answer = [];
+  for (let i = 0; i < banknotes.length; i++) {
+    answer.push(Math.floor(money / banknotes[i]));
 }
 
 module.exports = problem5;
