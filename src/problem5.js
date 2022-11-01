af172cc348f672ef61ac45db133348d748ce54af
Sallyvely <tpdus4159@naver.com>
2022-11-01 21:21:48 +0900

    5번 문제 화폐단위를 통하여 거스름돈 개수 구하는 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,12 @@
 function problem5(money) {
-  var answer;
+  var answer=[];
+  let moneyType=[50000,10000,5000,1000,500,100,50,10,1];
+
+  for(let i=0; i<moneyType.length;i++){
+    answer.push(parseInt(money/moneyType[i]));
+    money=parseInt(money%moneyType[i]);
+  }
+  
   return answer;
 }
 
