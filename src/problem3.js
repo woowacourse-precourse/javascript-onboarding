c6e5cfb7d6b3f309dd737141c3f8927e6962c03b
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 21:29:20 +0900

    feat: [PROBLEM3] 제한 사항에 따른 에러 반환(feat1) 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,14 @@
 function problem3(number) {
   var answer;
+
+  try {
+    if (number > 10000 || number < 0) {
+      throw new Error('number must between(1, 10,000)');
+    }
+  } catch {
+    return -1;
+  }
+
   return answer;
 }
 
