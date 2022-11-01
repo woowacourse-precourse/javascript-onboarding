9a7935ec0c0257ba09d0224e1eba1932cb663624
rayark1 <max980203@snu.ac.kr>
2022-11-01 16:20:37 +0900

    docs(prob6): Add functions list

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,16 @@
+/*
+  기능 목록
+  1. nickname를 key, email를 value로 Object를 만든다.
+  2. nickname으로 list를 만든다.
+  3. 같은 글자가 연속적으로 포함되는 닉네임을 찾는다.
+    3-1. nickname을 2글자씩 자른다.
+    3-2. 자른 닉네임이 nickname list에 포함되어 있는지 확인한다.
+    3-3. 포함되어 있으면, 같은 글자가 연속적으로 포함되는 닉네임으로 간주하고 반환한다.
+  4. 닉네임을 key로 email을 value로 Object를 만든다.
+  5. email로 list를 만든다.
+  6. emailList의 중복을 제거하고 정렬한다.
+*/ 
+
 function problem6(forms) {
   var answer;
   return answer;
