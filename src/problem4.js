e448666b050beb5554971c8513e926151134b878
leesoyuun <leesoyun4206@naver.com>
2022-11-01 20:03:22 +0900

    [Problem 4] 청개구리 사전을 통한 문자 출력

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,18 @@
 function problem4(word) {
-  var answer;
-  return answer;
-}
+  let split_word = word.split("");
+    for(let i=0;i<split_word.length;i++){
+        let ASCII_num = split_word[i].charCodeAt(0);
+        if(ASCII_num==32){
+            split_word[i] = String.fromCharCode(ASCII_num);
+        }
+        else if(ASCII_num>=65 && ASCII_num<=90){
+            split_word[i] = String.fromCharCode(155-ASCII_num);
+        }
+        else{
+            split_word[i] = String.fromCharCode(219-ASCII_num);
+        }
+    }
+    return split_word.join("");
+  }
 
 module.exports = problem4;
