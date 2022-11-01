7fc8c99a0e192b7750f14ded4778f5bcbff7aa58
pakxe <pigkill40@naver.com>
2022-11-01 16:05:57 +0900

    refactor: [P1] let을 const로 변경

--- src/problem1.js
+++ src/problem1.js
@@ -3,8 +3,8 @@ const LAST_PAGE = 400;
 function problem1(pobi, crong) {
   if (!isValidPage(pobi) || !isValidPage(crong)) return -1;
 
-  let maxPobi = maxNum(pobi);
-  let maxCrong = maxNum(crong);
+  const maxPobi = maxNum(pobi);
+  const maxCrong = maxNum(crong);
   if (maxPobi > maxCrong) return 1;
   if (maxPobi < maxCrong) return 2;
   if (maxPobi === maxCrong) return 0;
@@ -21,7 +21,7 @@ function isValidPage(book) {
 
 // 2, 3번의 최대 값을 반환하는 함수
 function maxNum(book) {
-  let numList = [];
+  const numList = [];
 
   book.forEach((page) => {
     numList.push(sum(page));
@@ -34,7 +34,7 @@ function maxNum(book) {
 // 각 자리수를 더해서 반환하는 함수
 function sum(num) {
   const numToString = num.toString().split("");
-  let sum = numToString.reduce((m, n) => m + Number(n), 0);
+  const sum = numToString.reduce((m, n) => m + Number(n), 0);
 
   return sum;
 }
@@ -42,7 +42,7 @@ function sum(num) {
 // 각 자리수를 곱해서 반환하는 함수
 function mul(num) {
   const numToString = num.toString().split("");
-  let mul = numToString.reduce((m, n) => m * Number(n), 1);
+  const mul = numToString.reduce((m, n) => m * Number(n), 1);
 
   return mul;
 }
