841169c38044389958bae53df0f499b89ab7cfa3
dhkham <khamdong@gmail.com>
2022-11-01 11:45:02 +0900

    feat: (prob_2) add function that takes in a string and checks for repeating character

--- src/problem2.js
+++ src/problem2.js
@@ -2,5 +2,14 @@ function problem2(cryptogram) {
   var answer;
   return answer;
 }
-
 module.exports = problem2;
+
+// check for repetition in string
+function checkRepetition(string) {
+  for (let i = 0; i < string.length; i++) {
+    if (string[i] === string[i + 1] || string[i] === string[i - 1]) {
+      return true;
+    }
+  }
+  return false;
+}
