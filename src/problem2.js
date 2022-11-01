e81169fd51012a1fd5a09a0cf709a596574c793a
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 20:42:09 +0900

    feat:[문제2] 이전값과 현재값이 다르고 현재값과 다음값이 다르면 누산기에 알파벳넣기

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,19 @@
+function removeDuplication(splitCryptogram) {
+  let arrayWithoutDuplication = splitCryptogram.reduce(
+    (acc, alphabet, index, arr) => {
+      alphabet !== arr[index + 1] &&
+        alphabet !== arr[index - 1] &&
+        acc.push(alphabet);
+      return acc;
+    },
+    []
+  );
+  return arrayWithoutDuplication.join("");
+}
+
 function problem2(cryptogram) {
   let splitCryptogram = cryptogram.split("");
-  return;
+  return removeDuplication(splitCryptogram);
 }
 
 module.exports = problem2;
