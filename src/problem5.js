ab1544573915297e9e8a85992157ba589be2dd30
DongWooKim97 <nanaukim@naver.com>
2022-11-01 20:46:02 +0900

    refactor(P5): 예외처리 기능 삭제

--- src/problem5.js
+++ src/problem5.js
@@ -1,8 +1,4 @@
 function problem5(money) {
-  if (!checkInputValid(money)) {
-    return 'error';
-  }
-
   const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const answer = currency.map((value) => {
     let quotient = Math.floor(money / value)
@@ -14,11 +10,6 @@ function problem5(money) {
   })
 
   return answer;
-}
-
-function checkInputValid(money) {
-  if (money < 1 || money > 1000000) return false;
-  return true;
 }
 
 module.exports = problem5;
