ce8714c15d80d53f9144e35024b6f5f80fbcfce8
hahagarden <88613455+hahagarden@users.noreply.github.com>
2022-11-01 14:18:30 +0900

    problem2: function list

--- src/problem2.js
+++ src/problem2.js
@@ -1,27 +1,8 @@
 function problem2(cryptogram) {
   var answer;
-  var cryptogramString = cryptogram;
-  var bundle = 0;
-  var flag = 1;
-  function DFS(i) {
-    if (i == cryptogramString.length - 1) return;
-    if (cryptogramString[i] != cryptogramString[i + 1]) {
-      if (bundle == 0) {
-        answer += cryptogramString[i];
-      } else bundle = 0;
-      if (i == cryptogramString.length - 2) answer += cryptogramString[i + 1];
-    } else {
-      flag++;
-      bundle++;
-    }
-    DFS(i + 1);
-  }
-  while (cryptogramString.length > 1 && flag > 0) {
-    flag = 0;
-    answer = "";
-    DFS(0);
-    cryptogramString = answer;
-  }
+
+  function decode(i) {}
+
   return answer;
 }
 
