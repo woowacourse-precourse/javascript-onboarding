dac5d7688bd826c21a445bd4518d0f731bf0caf2
ming0830 <minsun0830@naver.com>
2022-11-01 17:10:59 +0900

    [PROBLEM2] refactor : 예외 사항 추가
    1. 문자열에 대문자가 속해 있으면 에러 메시지를 보낸다.
    2. 문자열이 1~1000자 이상이면 에러 메시지를 보낸다.

--- src/problem2.js
+++ src/problem2.js
@@ -1,4 +1,12 @@
 function problem2(cryptogram) {
+  // 예외 처리
+  const lowEng  = /^[a-z]+$/;
+  if(!lowEng.test(cryptogram)){
+    return "소문자만 입력 가능합니다.";
+  } else if(!(cryptogram.length >=1 && cryptogram.length <=1000)){
+    return "1~1000자의 글자만 입력 가능합니다."
+  }
+
   let result = [];
   for(let i =0;i<cryptogram.length;i++){
     const char = cryptogram[i];
@@ -9,6 +17,5 @@ function problem2(cryptogram) {
     }
   } return result.join('')
 }
-
 
 module.exports = problem2;
