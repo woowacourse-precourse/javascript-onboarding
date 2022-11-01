e9da9d6cea35eababa4a39e26c96e60f548ec3e8
ori5r2 <96720326+ori5r2@users.noreply.github.com>
2022-11-01 22:10:22 +0900

    :zap: solve problem3
    solve problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,15 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+
+  for (let i = 1; i <= number; i++) {
+    var temp = i.toString();
+    for (let j = 0; j < temp.length; j++) {
+      if (temp[j] === "3" || temp[j] === "6" || temp[j] === "9") {
+        answer += 1;
+      }
+    }
+  }
+
   return answer;
 }
 
