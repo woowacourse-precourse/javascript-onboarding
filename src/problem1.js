d968f3bd1d7ac2728dc31e7b1af4f4a644e5504a
Yeonju Jo <yjj0287@gmail.com>
2022-11-01 23:05:29 +0900

    feat: problem1 점수 계산

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,18 @@
 function problem1(pobi, crong) {
   if (isException(pobi, crong)) return -1;
+
+  const pobiLeftScore = maxScore(pobi[0]);
+  const pobiRightScore = maxScore(pobi[1]);
+  const pobiScore = Math.max(pobiLeftScore, pobiRightScore);
+
+  const crongLeftScore = maxScore(crong[0]);
+  const crongRightScore = maxScore(crong[1]);
+  const crongScore = Math.max(crongLeftScore, crongRightScore);
+
+  if (pobiScore > crongScore) return 1;
+  else if (pobiScore < crongScore) return 2;
+  else if (pobiScore === crongScore) return 0;
+  return -1;
 }
 
 function isException(pobi, crong) {
@@ -14,6 +27,20 @@ function isException(pobi, crong) {
   )
     return true;
   return false;
+}
+
+function maxScore(page) {
+  pageStr = [...String(page)];
+
+  const sum = pageStr.reduce((v, i) => {
+    return v + Number(i);
+  }, 0);
+
+  const multifly = pageStr.reduce((v, i) => {
+    return v * Number(i);
+  }, 1);
+
+  return Math.max(sum, multifly);
 }
 
 module.exports = problem1;
