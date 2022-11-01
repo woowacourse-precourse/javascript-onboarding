d273ed57849ce3db59ba6c07b4baa966e8b6ccbd
leeseooo <leeseoooo@naver.com>
2022-11-01 11:33:04 +0900

    feat: problem5 moneyList 인수로 넘겨줌

--- src/problem5.js
+++ src/problem5.js
@@ -1,11 +1,11 @@
 function problem5(money) {
   let answer = [];
   const moneyList = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  convertMoney(money, answer);
+  convertMoney(moneyList,money, answer);
   return answer;
 }
 
-function convertMoney(cur, answer) {
+function convertMoney(moneyList, cur, answer) {
   moneyList.forEach((money) => {
     answer.push(Math.floor(cur / money));
     cur %= money;
