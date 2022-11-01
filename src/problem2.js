63f4cbc3e53ba8bbb8151e994cf8caf58318cbaf
cyberjam <jamsince1994@naver.com>
2022-11-01 12:50:50 +0900

    problem2 complete

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,38 @@
 function problem2(cryptogram) {
   var answer;
+
+  let continue_flag = true;
+  let targetArr = cryptogram.toLowerCase().split("");
+
+  while (continue_flag) {
+    let result = [];
+    let suspect;
+    targetArr.push(-1);
+    targetArr.forEach((element, index) => {
+      if (element === -1) {
+        return 0;
+      }
+
+      if (element === targetArr[index + 1]) {
+        suspect = element;
+      }
+      if (element !== suspect) {
+        result.push(element);
+        suspect = "";
+      }
+    });
+    targetArr.pop();
+    console.log(`target ${targetArr}`);
+    console.log(`result ${result}`);
+    if (JSON.stringify(targetArr) === JSON.stringify(result)) {
+      continue_flag = false;
+    } else {
+      targetArr = result;
+    }
+  }
+
+  console.log(targetArr);
+  answer = targetArr.join("");
   return answer;
 }
 
