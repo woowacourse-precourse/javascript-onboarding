8e9f33ad3d502749e81f7b188b166733fcec1fdb
OrangeChick <retww@naver.com>
2022-11-01 19:33:53 +0900

    feat: 🎒[Problem1] Implement sum function
    - 각 페이지를 더하는 기능을 구현하였습니다.

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
   var answer;
+
   return answer;
+}
+
+function sum(page) {
+  let arr = page
+    .toString()
+    .split("")
+    .map((x) => parseInt(x));
+  return arr.reduce((acc, cur) => acc + cur, 0);
 }
 
 module.exports = problem1;
