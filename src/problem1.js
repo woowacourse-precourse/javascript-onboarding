7263083644fda9842e837b827537c64c91d757d3
KUD <kimud6003@gmail.com>
2022-11-01 20:16:45 +0900

    ⚡️ HotFix: problem1.js CalOfDigits추가 - 기존에 Digit함수를 개편 - 주석추가

--- src/problem1.js
+++ src/problem1.js
@@ -6,26 +6,22 @@ function problem1(pobi, crong) {
   return resultLogic(pobiScore, crongScore);
 }
 
-function sumOfDigits(num) {
-  return String(num)
-    .split('')
-    .reduce((acc, curr) => {
-      return acc + Number(curr);
-    }, 0);
+// multiply and sum each digit of the number
+function calOfDigits(num) {
+  let [sum,mult] = [0,1];
+  String(num).split('').forEach( currNum => {
+      sum += Number(currNum);
+      mult *= Number(currNum);
+    });
+  return [sum,mult];
 }
 
-function mulOfDigits(num) {
-  return String(num)
-    .split('')
-    .reduce((acc, curr) => {
-      return acc * Number(curr);
-    },1);
-}
-
+// get max score of the calOfDigits results
 function getMaxScore([leftScore, rightScore]) {
-  return Math.max(sumOfDigits(leftScore),mulOfDigits(leftScore),sumOfDigits(rightScore),mulOfDigits(rightScore));
+  return Math.max(...calOfDigits(leftScore),...calOfDigits(rightScore));
 }
 
+// return the winner
 function resultLogic(pobiScore, crongScore) {
   if (pobiScore > crongScore)  return 1
   else if(pobiScore < crongScore) return 2;
@@ -33,6 +29,7 @@ function resultLogic(pobiScore, crongScore) {
 }
 
 
+// check the error
 function errorCheck(pobi,crong) {
   if(pobi.length !== 2 || crong.length !== 2) return true;
   else if(pobi[0]+1 !== pobi[1] || crong[0]+1 !== crong[1]) return true;
