c4f3439e0c716bc170d148d1f3d16af979655036
pangkyu <baesense@naver.com>
2022-11-01 18:34:17 +0900

    feat : 원래 친구를 찾는 기능
    친구에 유저가 포함되어있는 인덱스를 찾음

--- src/problem7.js
+++ src/problem7.js
@@ -1,4 +1,16 @@
-function problem7(user, friends, visitors) {}
+function getOriginFriends(user, friends, originFriends) {
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      console.log(friends[i]);
+      // addFriendDecidedByotherSideIndex(friends[i], user, originFriends);
+    }
+  }
+}
+
+function problem7(user, friends, visitors) {
+  let originFriends = [];
+  getOriginFriends(user, friends, originFriends);
+}
 
 function testCode() {
   problem7(
