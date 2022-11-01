18053ba256e01bf3b1414e29bfaf693b98b7f259
Dahye <06robin11@gmail.com>
2022-11-01 18:19:28 +0900

    refactory: simplify by deleting if condition

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,9 @@
 function problem5(money) {
   const units = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   const answer = units.map((unit) => {
-    if (money / unit > 1) {
-      const num = Math.floor(money / unit);
-      money -= num * unit;
-      return num;
-    } else {
-      return 0;
-    }
+    const num = Math.floor(money / unit);
+    money = money % unit;
+    return num;
   });
   return answer;
 }
