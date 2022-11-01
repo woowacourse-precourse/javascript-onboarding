9d1d68361c8571f822b4e59ce5473bb20432dcec
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 21:28:23 +0900

    [problem6] feat: 같은 글자 연속 checkDouble 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -7,7 +7,24 @@ function problem6(forms) {
 }
 
 // 같은 글자(한글) 연속 check 함수
-function checkDouble(nick) {}
+function checkDouble(nick) {
+  // stack 선언
+  let nickArr = [];
+
+  for (let i = 0; i < nick.length; i++) {
+    // 두 글자씩 자르기
+    let twoSlice = nick[i].slice(i, i + 2);
+    // 자신 제외
+    let exSelf = nick.filter((v) => v !== nick[i]);
+
+    // 자른 두 글자 포함 유무 check
+    if (exSelf.some((v) => v.includes(twoSlice))) {
+      nickArr.push(twoSlice);
+    }
+  }
+
+  return nickArr.filter((v) => v !== "");
+}
 
 // 해당 이메일 return 함수
 function whatEmail(nick) {}
