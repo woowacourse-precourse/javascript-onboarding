be99a079dcd8a419a4505d38ebfa785f15819496
bkdragon0228 <rlaqjarb2067@naver.com>
2022-11-01 20:52:53 +0900

    chore : problem2 주석 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,9 @@
 function problem2(cryptogram) {
+    // 스택 (first in last out)
+    // 스택의 마지막 요소와 문자열의 요소를 비교한다.
     const stack = [];
+    // 연속된 단어가 3개 이상도 나올 수 있기 때문에 스택에서 버려지는 요소를 모아두는 배열
+    // trash의 마지막 요소와도 문자열의 요소를 비교해야한다.
     const trash = [];
 
     for (let x of cryptogram) {
