75bf9da44b939fc0e4d8dc565c2231df2c9645ae
Hchan2 <seongchan_@naver.com>
2022-11-01 12:42:15 +0900

    problem5 solved

--- src/problem5.js
+++ src/problem5.js
@@ -1,15 +1,15 @@
 function problem5(money) {
   let divideMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let count = Array(divideMoney.length).fill(0);
+  let answer = Array(divideMoney.length).fill(0);
 
-  if (money === 0) return count;
+  if (money === 0) return answer;
 
-  for (let i = 0; i < count.length; i++) {
-    count[i] = Math.floor(money / divideMoney[i]);
-    money = money - divideMoney[i] * count[i];
+  for (let i = 0; i < answer.length; i++) {
+    answer[i] = Math.floor(money / divideMoney[i]);
+    money = money - divideMoney[i] * answer[i];
   }
 
-  return count;
+  return answer;
 }
 
 module.exports = problem5;
