e065b7faed4139eacc669a63db7e3aae10fd20a6
김상두 <puki4416@gmail.com>
2022-11-01 18:00:47 +0900

    problem2 : checkLastDeleted, checkStack 함수 삭제 및 problem2 함수 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,39 +1,18 @@
 // 문자열에 중복된 값을 반복해서 삭제하는 problem2 함수 구현
 
-function checkLastDeleted(latestDeleted, currentValue) {
-  if (latestDeleted !== null) {
-    if (currentValue === latestDeleted) {
-      return true;
-    }
-  }
-  return false;
-}
-
-function checkStack(stack, currentValue) {
-  if (stack[stack.length - 1] === currentValue) {
-    stack.pop();
-    return true;
-  } else {
-    stack.push(currentValue);
-    return false;
-  }
-}
-
 function problem2(cryptogram) {
-  const stack = [];
-  let latestDeleted = null;
-  for (let i = 0; i < cryptogram.length; i++) {
-    if (checkLastDeleted(latestDeleted, cryptogram[i])) {
-      continue;
-    } else {
-      latestDeleted = null;
+  let chars = cryptogram.split("");
+  while (true) {
+    const midResult = [];
+    for (let i = 0; i < chars.length; i++) {
+      if (chars[i] !== chars[i + 1] && chars[i] !== chars[i - 1]) {
+        midResult.push(chars[i]);
+      }
     }
-
-    if (checkStack(stack, cryptogram[i])) {
-      latestDeleted = cryptogram[i];
-    }
+    if (midResult.length === chars.length) break;
+    chars = midResult;
   }
-  return stack.join("");
+  return chars.join("");
 }
 
 module.exports = problem2;
