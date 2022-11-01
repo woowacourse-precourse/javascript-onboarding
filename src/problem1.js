6ac359ffe2df6ebb82d8b00b13254249beae79c4
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-01 15:08:43 +0900

    feat: [problem1] 예외 상황 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,18 @@
 function problem1(pobi, crong) {
   var answer;
+  //예외 상황일 경우 -1을 return 
+  if (
+    pobi[0] % 2 != 1 ||
+    pobi[1] % 2 != 0 ||
+    pobi[0] + 1 != pobi[1] ||
+    crong[0] % 2 != 1 ||
+    crong[1] % 2 != 0 ||
+    crong[0] + 1 != crong[1]
+    ) {
+    answer = -1;
+    } 
+
+  
   return answer;
 }
 
