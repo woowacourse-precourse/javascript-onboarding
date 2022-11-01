b369689e15117df6e36bc86f603aafb8c5d76cd3
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:44:22 +0900

    feat(prob4) : ascii 코드를 활용한 알파벳 체크

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
+function isAlphabet(val) {
+  if ((val >= 65 && val <= 90) || (val >= 97 && val <= 122)) return 1;
+  else return 0;
+}
+
 function problem4(word) {
   var answer;
+
   return answer;
 }
+
+// console.log(problem4("I.... love you"));
 
 module.exports = problem4;
