3d3743fa3601d23fac3d94c990d2b2e9c74422a3
Minjee Kim <hbmj1017@naver.com>
2022-11-01 18:40:10 +0900

    problem 3

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,10 @@
 function problem3(number) {
-  var answer;
+  var nums = [];
+  do {
+      nums.push(number%10); 
+      number = Math.floor(number / 10) 
+  } while (number > 0)
+  var answer = nums.filter((num) => num !== 0 && num%3 === 0).length
   return answer;
 }
 
