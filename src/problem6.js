bd94c986c49f2f9975fcae975f2c242920dce3c8
김혜원 <dev.blair06@gmail.com>
2022-11-01 18:59:09 +0900

    feat: problem6 함수 구현 - forms의 이름을 두글자씩 쪼개어 저장 - 저장된 이름들 중 중복검사

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,28 @@
 function problem6(forms) {
-  var answer;
+  const checkArr = [];
+  forms.map((elem) => {
+    for (let i = 0; i < elem[1].length - 1; i++) {
+      checkArr.push(elem[1].substr(i, 2));
+    }
+  });
+
+  const duplicates = findDuplicate(checkArr);
+
+  const resultArr = [];
+
+  forms.map((elem) => {
+    if (elem[1].includes(duplicates[0])) {
+      resultArr.push(elem[0]);
+    }
+  });
+
+  const result = new Set([...resultArr]);
+  answer = [...result].sort();
   return answer;
+}
+function findDuplicate(arr) {
+  const filtered = arr.filter((item, index) => arr.indexOf(item) !== index);
+  return [...new Set(filtered)];
 }
 
 module.exports = problem6;
