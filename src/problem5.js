a12dadd3909b0f987d494417021b9d215eb0ece2
wldnd2 <jiwoong000628@gmail.com>
2022-11-01 19:01:51 +0900

    [problem5] 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
-  var answer;
+  var trans = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  for (var i = 0; i < trans.length; i++) {
+    answer[i] = Math.floor(money / trans[i]);
+    money = money % trans[i];
+  }
   return answer;
 }
 
