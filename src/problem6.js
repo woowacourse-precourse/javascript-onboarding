190ffa8d6bf44563f2017edce2843b7466b3088c
f1rstf1y9 <f1rstf1y9@naver.com>
2022-11-01 13:22:33 +0900

    feat: [problem6] 임의의 두 크루의 닉네임에서 모두 연속적으로 포함되는 글자들을 반환하는 기능

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,14 @@
+function getDuplicatedWords(forms, i) {
+  return [...forms[i][1]].filter(word => forms[i+1][1].includes(word)).join('');
+}
+
 function problem6(forms) {
   var answer;
+
+  for(let i=0; i < forms.length - 1; i++) {
+    let duplicatedWords = getDuplicatedWords(forms, i);
+  }
+
   return answer;
 }
 
