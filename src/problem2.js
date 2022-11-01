8c359e0bd4d8e49c7fb41a70b8e55ee8eea3c439
lazypalegg <109095071+lazypalegg@users.noreply.github.com>
2022-11-01 21:06:52 +0900

    Feat: restrictions

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,9 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (cryptogram.length > 0 && cryptogram.length < 1001 && typeof cryptogram === "string" && cryptogram === cryptogram.toLowerCase()) {
+          let answer = cryptogram;
+
+    return answer;
+  }
 }
 
 module.exports = problem2;
