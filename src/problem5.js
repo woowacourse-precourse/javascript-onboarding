e599c77ded166443b188834532acc036b4f96d0d
shinhyunsu01 <shine.master9687@gmail.com>
2022-11-01 14:27:13 +0900

    5 solve

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,15 @@
 function problem5(money) {
-  var answer;
-  return answer;
+	let answer = [];
+	let sortMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+	for (let i = 0; i < sortMoney.length; i++) {
+		if (i === 0) {
+			answer.push(Math.floor(money / sortMoney[i]));
+		} else {
+			answer.push(Math.floor((money % sortMoney[i - 1]) / sortMoney[i]));
+		}
+	}
+	return answer;
 }
 
 module.exports = problem5;
