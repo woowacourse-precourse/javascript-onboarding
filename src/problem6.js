a65a1b75a29e069263067a34f2c8678e9ff3980a
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 21:06:28 +0900

    feat: Implement checkingDuplicated obejct in problem6.js
    주어진 forms를 for문으로 돌면서 닉네임을 2글자씩 잘라서 key값으로, email을 원소로하는 set을 value값으로 갖는 checkingDuplicated 오브젝트를 구현하였음

--- src/problem6.js
+++ src/problem6.js
@@ -1,4 +1,21 @@
 function problem6(forms) {
+  const checkingDuplicated = {};
+  for (i=0; i<forms.length; i++) {
+    const email = forms[i][0];
+    const nickname = forms[i][1];
+    if (nickname.length == 1) {
+      continue;
+    }
+    for (j=0; j<nickname.length-1; j++) {
+      const sliced = nickname.substr(j, 2);
+      if (checkingDuplicated.hasOwnProperty(sliced)) {
+        checkingDuplicated[sliced].add(email);
+        continue;
+      }
+      checkingDuplicated[sliced] = new Set();
+      checkingDuplicated[sliced].add(email);
+    }
+  }
   var answer;
   return answer;
 }
