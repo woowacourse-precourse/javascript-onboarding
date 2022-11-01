1bd4bc7cc819b7b593a5de6d6e10cf69f7d1c50c
cho seonghoon <chsida655@gmail.com>
2022-11-01 17:34:13 +0900

    feat(prob1) : 두 수 비교 함수 추가

--- src/problem1.js
+++ src/problem1.js
@@ -8,15 +8,19 @@ function getPageSum(num) {
 
 function getPageMulti(num) {
   let result = 1;
+  num = String(num);
   for (let i = 0; i < num.length; i++) {
-    result *= num[i];
+    result *= parseInt(num[i]);
   }
   return result;
+}
+
+function compareNum(num1, num2) {
+  return Math.max(num1, num2);
 }
 
 function problem1(pobi, crong) {
   var answer;
   return answer;
 }
-
 module.exports = problem1;
