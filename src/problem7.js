f8f777a85404ca0a5da87f2008d04a8e95e74102
useon <yoosk4986@naver.com>
2022-11-01 16:49:41 +0900

    feat: [problem7] 친구목록에서 user, user 친구가 아닌 경우를 찾는다.
    찾아서 Map 객체(score)에 key로 넣고, value는 0으로 넣는다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,12 +1,22 @@
 function problem7(user, friends, visitors) {
   let answer = [];
   const userFriend = new Set();
+  let score = new Map();
 
   // 1. user 친구를 찾는다.
   for (let i = 0; i < friends.length; i++) {
     if (friends[i].indexOf(user) !== -1) {
       if (friends[i].indexOf(user) === 0) userFriend.add(friends[i][1]);
       if (friends[i].indexOf(user) === 1) userFriend.add(friends[i][0]);
+    }
+  }
+
+  // 2. 친구 목록에서 user, user 친구가 아닌 경우를 찾는다.
+  for (let i = 0; i < friends.length; i++) {
+    for (let j = 0; j < 2; j++) {
+      if (friends[i][j] !== user && !userFriend.has(friends[i][j])) {
+        score.set(friends[i][j], 0);
+      }
     }
   }
 
