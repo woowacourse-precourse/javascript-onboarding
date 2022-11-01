ae07c3e2db8afe2a814999034074ec3c9be39b79
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 12:37:05 +0900

    Feat: problem5 - 변수와 상수 생성

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,6 @@
 function problem5(money) {
   const banknotes = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
   let answer = [];
-
-  for (let i = 0; i < banknotes.length; i++) {
-    answer.push(Math.floor(money / banknotes[i]));
-    money %= banknotes[i];
-  }
-  return answer;
 }
 
 module.exports = problem5;
