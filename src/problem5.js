9ea7d5a96f0c2474b5335a40e001a6be86469dd2
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 21:38:18 +0900

    problem5 : 화폐단위로 나눈다.

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,9 @@
 function problem5(money) {
-  var answer;
+
+  const current = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = [];
+  
+
   return answer;
 }
-
 module.exports = problem5;
