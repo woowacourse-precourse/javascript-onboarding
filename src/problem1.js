1f87ec8d666482b409c49ce771477f8e70aca582
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:21:59 +0900

    [problem1] 합,곱함수를 통해 얻은 값의 최대값 반환 함수

--- src/problem1.js
+++ src/problem1.js
@@ -14,6 +14,17 @@ function sum(sumArray) {
   }, 0);
   return result;
 }
+function makePage(pocroArray) {
+  const pobiCrongMax = [];
+  pocroNumberLeft = pocroArray[0].toString().split("");
+  pocroNumberRight = pocroArray[1].toString().split("");
+  pobiCrongMax.push(sum(pocroNumberLeft));
+  pobiCrongMax.push(sum(pocroNumberRight));
+  pobiCrongMax.push(multi(pocroNumberLeft));
+  pobiCrongMax.push(multi(pocroNumberRight));
+  console.log(pobiCrongMax);
+  return Math.max(...pobiCrongMax);
+  //포비,크롱의 페이지를 각 숫자별로 배열로 만듬
 }
 
 module.exports = problem1;
