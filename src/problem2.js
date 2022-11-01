21ff8e0dc9d7d787c2a8beb51a58e80f03ab2ffd
lazypalegg <109095071+lazypalegg@users.noreply.github.com>
2022-11-01 21:07:24 +0900

    Feat: regex test

--- src/problem2.js
+++ src/problem2.js
@@ -2,6 +2,9 @@ function problem2(cryptogram) {
   if (cryptogram.length > 0 && cryptogram.length < 1001 && typeof cryptogram === "string" && cryptogram === cryptogram.toLowerCase()) {
           let answer = cryptogram;
 
+  while (/([a-z])\1/.test(answer) == true) {
+    answer = answer.replace(/([a-z])\1/, "");
+  }
     return answer;
   }
 }
