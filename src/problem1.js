155d405f3ee6b7df4f82222909948c90a02868d4
deli-ght <minjju0660@gmail.com>
2022-11-01 17:55:36 +0900

    ♻️ 문제 1 리팩토링

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,19 @@
 function problem1(pobi, crong) {
+  // 각 페이지가 연속된 페이지가 아니라면 -1 리턴
   if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) return -1
+
+  // 왼쪽 페이지가 짝수이거나 오른쪽 페이지가 홀수면 -1 리턴
+  if (
+    pobi[0] % 2 === 0 ||
+    pobi[1] % 2 === 1 ||
+    crong[0] % 2 === 0 ||
+    crong[1] % 2 === 1
+  )
+    return -1
+
+  // 페이지 번호가 1~400 사이가 아니고, 첫번째 페이지와 마지막 페이지면 -1 리턴
+  if (pobi[0] <= 2 || pobi[1] >= 399 || crong[0] <= 2 || crong[1] >= 399)
+    return -1
 
   const pobiLeftMax = Math.max(getAddValue(pobi[0]), getMutiplyValue(pobi[0]))
   const pobiRightMax = Math.max(getAddValue(pobi[1]), getMutiplyValue(pobi[1]))
@@ -29,24 +43,9 @@ function problem1(pobi, crong) {
 module.exports = problem1
 
 function getAddValue(value) {
-  let a = value
-  let b = 0
-
-  while (a / 10 > 0) {
-    b += a % 10
-    a = parseInt(a / 10)
-  }
-  return b
+  return `${value}`.split("").reduce((prev, curr) => prev + +curr, 0)
 }
 
 function getMutiplyValue(value) {
-  let a = value
-  let b = 1
-
-  while (a / 10 > 0) {
-    b = b * (a % 10)
-    a = parseInt(a / 10)
-  }
-
-  return b
+  return `${value}`.split("").reduce((prev, curr) => prev * +curr, 1)
 }
