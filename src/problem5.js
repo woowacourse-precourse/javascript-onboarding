feea42f4594642484838e7005a30965617633d11
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:28:18 +0900

    feat: [problem5] 각 화폐 단위 카운팅

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
   let answer = [];
   let money_unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1]
+
+  for (const unit of money_unit) {
+    let count = (money - (money % unit)) / unit
+    money = money % unit
+    answer.push(count)
+  }
+
   return answer;
 }
 
