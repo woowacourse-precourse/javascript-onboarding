7aa9b4165cb713b9f1054627d7aebc62043dcc72
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 19:21:39 +0900

    problem4 requirements

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
+/*필요한 기능
+for loop으로 문장 iterate
+스페이스, 대문자, 소문자 구분
+대소문자 바꿔주고 ASCII Table 보고 변환 
+결과물 리턴
+*/
+
 function problem4(word) {
-  var answer;
+  var answer = solution(word);
   return answer;
 }
+
 
 module.exports = problem4;
