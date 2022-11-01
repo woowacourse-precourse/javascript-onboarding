0a78112c316f530b4ce473ef42a761d72fefcdf2
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 18:30:48 +0900

    [problem3] feat: test sample 제외 및 최종 답안 commit

--- src/problem3.js
+++ src/problem3.js
@@ -1,17 +1,16 @@
 // test sample 추가
-console.log(problem3(36));
+// console.log(problem3(33));
 
 function problem3(number) {
-  var answer;
-  let countClap;
+  let countClap = 0;
 
   // for문으로 1부터 number까지 반복
   for (let i = 1; i <= number; i++) {
     countClap += checkClap(i);
-    console.log(i, checkClap(i));
+    //console.log(i, checkClap(i));
   }
 
-  return answer;
+  return countClap;
 }
 
 // 3, 6, 9 발견 함수
