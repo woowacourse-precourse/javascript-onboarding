09bea14d6ba07c830015901808af8996b5f6ec47
EUNA <beni1026@naver.com>
2022-11-01 17:41:48 +0900

    problem2 solved

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,26 @@
 function problem2(cryptogram) {
   var answer;
-  return answer;
+
+  let decodingStr = cryptogram;
+  while(true){
+    let stack = [];
+    let char = '';
+
+    for(const c of decodingStr){
+      let top = stack[stack.length-1];
+      if(top !== c && char !== c) stack.push(c);
+      else{
+        stack.pop();
+        char = c;
+      }
+    }
+
+    let decodedStr = stack.join("");
+    if(decodedStr === decodingStr) return decodedStr;
+    else {
+      decodingStr = stack.join("");
+    }    
+  }
 }
 
 module.exports = problem2;
