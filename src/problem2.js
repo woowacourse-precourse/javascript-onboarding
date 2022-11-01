2e08bc1f01016a3c384454cbbce5f3643a42de71
Hypark2018 <hayoungpark2018@gmail.com>
2022-11-01 23:52:44 +0900

    문제 2번.

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if(deleteRepeate(cryptogram) === null) return ""
+  return deleteRepeate(cryptogram)
+}
+
+function deleteRepeate(cryptogram) {
+  let temporaryStorage = [];
+
+  for (let i = 0; i < cryptogram.length; i++) {
+    const lastEl = temporaryStorage.slice(-1)[0];
+    const currEl = cryptogram[i];
+
+    if(lastOverlap === currEl) continue
+    if (currEl === lastEl) {
+      lastOverlap = lastEl;
+      temporaryStorage.pop();
+    }
+    else if(currEl !== lastEl) temporaryStorage.push(currEl)
+  }
+  return temporaryStorage.join('');
 }
 
 module.exports = problem2;
