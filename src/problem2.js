5a097ecb3d27f0354b5f152369c1ecf33e6221b2
Minju Park <project.mizzu@gmail.com>
2022-11-01 14:19:50 +0900

    Refactor : for문 map으로 변경
    - result의 문자들을 순회하기 위해 사용했던 for문을 map으로 리팩토링
    - 일부 주석 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,26 +1,28 @@
 // [✅] cryptogram을 split('')하여 배열로 변환
 // [✅] cryptogram를 순회하면서 특정 index의 문자가 index + 1의 문자가 일치하는지 찾기
 // [✅] 연속된 중복문자를 splice하여 cryptogram 속에서 제거
-// [] 중복문자가 없을 때 까지 위의 과정 반복 후 result.join('') 반환 / while의 종료조건을 무엇으로 해야할까...?
+// [✅] 중복문자가 없을 때 까지 위의 과정 반복 후 result.join('') 반환 / while의 종료조건을 무엇으로 해야할까...?
 // [✅] 중복 제거중인 문자를 담기 위한 deduplication 생성
-// [] result.join('')의 length가 이전 deduplication의 length와 차이가 없다면, while 종료
+// [✅] result.join('')의 length가 이전 deduplication의 length와 차이가 없다면, while 종료
 
 function problem2(cryptogram) {
   const result = cryptogram.split('');
   let deduplication = '';
   while (true) {
+    // 중복제거 이전의 문자 저장
     deduplication = result.join('');
-    for (let i = 0; i < result.length; i++) {
-      if (result[i] === result[i + 1]) {
-        result.splice(i, 2);
+    result.map((alphabet, idx) => {
+      // 현재 인덱스의 문자와 다음 인덱스의 문자의 일치여부 확인
+      if (alphabet === result[idx + 1]) {
+        // 일치 시 제거
+        result.splice(idx, 2);
       }
-    }
+    });
+    // 중복제거 후의 문자와 이전의 문자를 비교, 일치 시 반환
     if (result.join('') === deduplication) {
       return result.join('');
     }
   }
 }
-
-console.log(problem2('zyelleyz'));
 
 module.exports = problem2;
