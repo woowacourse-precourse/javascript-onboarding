88baa33c36554053ffd8a95bf9a942249223426f
kimeuns <dmstn2006@gmail.com>
2022-11-01 17:41:55 +0900

    문제2 refactor

--- src/problem2.js
+++ src/problem2.js
@@ -1,13 +1,12 @@
 function problem2(cryptogram) {
-  let answer;
-
+  
   const cryptogramArray = cryptogram.split("");
   let decryption = cryptogramArray.reduce((arr, char) => {
     arr[arr.length - 1] !== char ? arr.push(char) : arr.pop();
     return arr;
   }, []); 
 
-  answer = decryption.reduce((answer,word) => answer+word,"")
+  return  decryption.reduce((answer,word) => answer+word,"")
 }
 
 module.exports = problem2;
