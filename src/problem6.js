ef45628d727ee78d74ddbc3558a68c11ec506ec3
jlee0505 <hj960322@gmail.com>
2022-11-01 20:27:37 +0900

    refactor(problem5): console.log 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -43,14 +43,3 @@ function generateCommonSubstringList(nicknames) {
 }
 
 module.exports = problem6;
-
-//expected: ["jason@email.com", "jm@email.com", "mj@email.com"]
-console.log(
-  problem6([
-    ["jm@email.com", "제이엠"],
-    ["jason@email.com", "제이슨"],
-    ["woniee@email.com", "워니"],
-    ["mj@email.com", "엠제이"],
-    ["nowm@email.com", "이제엠"],
-  ])
-);
