56e393a627451d25582d4616383c332a359dd1a9
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-01 23:52:09 +0900

    우아한테크코스 문제 6 미완

--- src/problem6.js
+++ src/problem6.js
@@ -2,5 +2,32 @@ function problem6(forms) {
   var answer;
   return answer;
 }
+function mailName(mail) {
+  var answer;
+  var m = mail.join().split(",");
+  var name = [];
+  var ma = [];
+  for(var x of m) {
+    if(String(x).includes('@') === false) {
+      name.push(x);
+    }else {
+      ma.push(x);
+    }
+  }
+  console.log(name, ma);
+  return name;
+}
 
-module.exports = problem6;
+function vs(n) {
+  
+}
+
+//console.log(problem6(F));
+console.log(mailName([
+  ["jm@email.com", "제이엠"],
+  ["jason@email.com", "제이슨"],
+  ["woniee@email.com", "워니"],
+  ["mj@email.com", "엠제이"],
+  ["nowm@email.com", "이제엠"],
+]));
+//module.exports = problem6;
