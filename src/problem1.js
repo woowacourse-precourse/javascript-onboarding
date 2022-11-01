bcbeab6f8ca54007409127485e3907082fb15205
player397 <107818612+player397@users.noreply.github.com>
2022-11-01 23:39:43 +0900

    Update problem1.js

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,20 @@
 function problem1(pobi, crong) {
-  var answer;
+    let sum = 0;
+    let mul = 1;
+    while (num > 0) {
+        let n = num % 10;
+        sum += n;
+        mul *= n;
+        num = Math.floor(num / 10);
+    }
+    return [sum, mul];
+}
+function validation(arr) {
+    if (arr[1] - arr[0] != 1) {
+        return false;
+    }
+
+    var answer;
   return answer;
 }
 
