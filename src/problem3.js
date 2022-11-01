e5b0c0cdd2ef62e688c219011f647324fa811244
jinlee <vito9270@naver.com>
2022-11-01 15:57:41 +0900

    [problem3] class 제거

--- src/problem3.js
+++ src/problem3.js
@@ -1,21 +1,12 @@
 function problem3(number) {
-  class Game369 {
-    constructor(number) {
-      this.number = number;
-    }
-    handclap() {
-      let clap = 0;
-      const regex365 = /3|6|9/g;
-      for (let i = 1; i <= this.number; i++) {
-        clap += (String(i).match(regex365) || []).length;
-      }
-      return clap;
-    }
+  let clap = 0;
+  const regex365 = /3|6|9/g;
+  
+  for (let i = 1; i <= number; i++) {
+    clap += (String(i).match(regex365) || []).length;
   }
 
-  const answer = new Game369(number).handclap();
-
-  return answer;
+  return clap;
 }
 
 module.exports = problem3;
