229b4f6114ebed8347cd8de916f6e8c8c729e921
Junghwan Kim <junghwki@c2r5s6.42seoul.kr>
2022-11-01 11:22:32 +0900

    feat: [problem7] 전체 친구목록 함수 구현
    getUserList 함수를 이용해 전체 유저의 친구 목록 반환
    docs 수정

--- src/problem7.js
+++ src/problem7.js
@@ -2,7 +2,10 @@
 // 1. friends 배열이 1이상 10000이하 인지 검사
 // 2. visitors 배열 길이가 0이상 10000이하 인지 검사
 // 3. 사용자 아이디가 소문자인지 검사하는 함수 추가
-// 4. 친구관계 중복검사
+// 4. 전체유저 친구목록 생성
+// 5. 친구의 친구 목록 생성
+// 6. 추천 점수 계산
+// 7. 정렬후 상위 5명 반환
 
 function userValidCheck(user) {
   const userRegex = /^[a-z]{1,30}$/;
@@ -51,11 +54,44 @@ function validInputCheck(user, friends, visitors) {
   return 0;
 }
 
+function makeFriendsList(userList, firstUser, secondUser) {
+  let userFriendsList;
+
+  if (userList.has(firstUser) === false) {
+    userList.set(firstUser, []);
+  }
+  userFriendsList = userList.get(firstUser);
+  //if (userFriendsList.find((element) => element === secondUser)) {
+  //  return -1;
+  //}
+  userFriendsList.push(secondUser);
+}
+
+function getUserList(friends) {
+  let userList = new Map();
+  let leftUser;
+  let rightUser;
+
+  for (let index = 0; index < friends.length; index++) {
+    leftUser = friends[index][0];
+    rightUser = friends[index][1];
+    makeFriendsList(userList, leftUser, rightUser);
+    makeFriendsList(userList, rightUser, leftUser);
+  }
+  return userList;
+}
+
+function getResult(user, friends, visitors) {
+  const userList = getUserList(friends);
+
+  return userList;
+}
+
 function problem7(user, friends, visitors) {
   if (validInputCheck(user, friends, visitors) < 0) {
     return "Argument Error";
   }
-  var answer;
+  var answer = getResult(user, friends, visitors);
   return answer;
 }
 
