f2f08254290842fbb39369cf81fac7b57c3bc548
y00eunji <aine0227@naver.com>
2022-11-01 20:24:32 +0900

    feat: [problem4] make word reverse by frog dicrionary

--- src/problem4.js
+++ src/problem4.js
@@ -5,9 +5,24 @@ const ASCII = {
   z: "z".charCodeAt(),
 };
 
+const getReverseWord = (word) => {
+  const wordAscii = word.charCodeAt();
+
+  if (wordAscii >= ASCII.A && wordAscii <= ASCII.Z) {
+    return String.fromCharCode(ASCII.A + ASCII.Z - wordAscii);
+  }
+  if (wordAscii >= ASCII.a && wordAscii <= ASCII.z) {
+    return String.fromCharCode(ASCII.a + ASCII.z - wordAscii);
+  }
+  return word;
+};
+
 function problem4(word) {
-  var answer;
-  return answer;
+  const wordArr = word.split("");
+
+  const reversedWord = wordArr.map((word) => getReverseWord(word));
+
+  return reversedWord.join("");
 }
 
 module.exports = problem4;
