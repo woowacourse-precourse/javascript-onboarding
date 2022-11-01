03ea2154d8cb638ab64230f974fb571525c3c36c
jiseung-kang <ill0o0lli@ajou.ac.kr>
2022-11-01 21:37:24 +0900

    🐛 problem7 버그 수정
    - resultScore에 들어온 값들이 NaN인 문제 해결
    - 점수가 같을 때 이름 순으로 정렬하도록 수정

--- src/problem7.js
+++ src/problem7.js
@@ -6,27 +6,35 @@ const problem7 = (user, friends, visitors) => {
     relationShip[friendA] = relationShip[friendA]
       ? [...relationShip[friendA], friendB]
       : [friendB];
+
     relationShip[friendB] = relationShip[friendB]
       ? [...relationShip[friendB], friendA]
       : [friendA];
   });
 
   Object.keys(relationShip).forEach((person) => {
-    if (person !== user) {
-      relationScore[person] +=
-        relationShip[person].filter((someone) =>
-          relationShip[user].includes(someone)
-        ).length * 10;
-    }
+    relationScore[person] =
+      relationShip[person].filter((someone) =>
+        relationShip[user].includes(someone)
+      ).length * 10;
   });
 
-  visitors.forEach((visitor) => {
-    relationScore[visitor] = relationScore[visitor] + 1 ?? 1;
-  });
+  visitors.forEach(
+    (visitor) =>
+      (relationScore[visitor] = relationScore[visitor]
+        ? relationScore[visitor] + 1
+        : 1)
+  );
 
   return Object.keys(relationScore)
-    .filter((existed) => !relationShip[user].includes(existed))
-    .sort((a, b) => relationScore[b] - relationScore[a])
+    .filter(
+      (someone) => someone !== user && !relationShip[user].includes(someone)
+    )
+    .sort((a, b) =>
+      relationScore[b] === relationScore[a]
+        ? a.localeCompare(b)
+        : relationScore[b] - relationScore[a]
+    )
     .slice(0, 5);
 };
 
