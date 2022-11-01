88154ebfaa34160c37b682dcd9567a5dcbcaea4f
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 14:15:49 +0900

    feat[P3]: 배열생성후 각각의 digit이 3,6,9가 들어가면 count

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+	let answer = 0;
+	let numArr = Array.from({ length: number }, (_, index) => index + 1);
+	numArr.forEach((element) => {
+		element
+			.toString()
+			.split("")
+			.forEach((digit) => {
+				if (digit == 3 || digit == 6 || digit == 9) answer++;
+			});
+	});
+	return answer;
 }
 
 module.exports = problem3;
