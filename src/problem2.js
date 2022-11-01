80e5c57ac713ce4d7f0d51795ac65b9a3b1746c3
minjun06 <35772631+minjun06@users.noreply.github.com>
2022-11-01 11:24:13 +0900

    problem2 bug fixed

--- src/problem2.js
+++ src/problem2.js
@@ -5,15 +5,18 @@ for loop으로 각 char랑 그 다음 char 비교
 */
 
 function problem2(cryptogram) {
-  var answer;
+  var answer = solution(cryptogram);
   return answer;
 }
 
-function solution() {
-  for (i=0; i<1000; i++) {
-    if (cryptogram.charAt(i) == cryptogram.charAt(i+1)) {
-      cryptogram.replace(charAt(i), '');
-      cryptogram.replace(charAt(i+1), '');
+function solution(cryptogram) {
+  for (var i=0; i<cryptogram.length+2; i++) {
+    for (var j=0; j<cryptogram.length; j++) {
+      console.log(cryptogram.charAt(j), "!!!!!!!!!!!!!!!!!", cryptogram.charAt(j+1))
+      if (cryptogram.charAt(j) === cryptogram.charAt(j+1)) {
+        cryptogram = cryptogram.slice(0, j) + cryptogram.slice(j+2);
+        console.log("result: ", cryptogram);
+      }
     }
   }
   return cryptogram;
