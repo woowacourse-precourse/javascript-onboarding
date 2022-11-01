e5e903a72faa7695bbd8633825d4cf7a32ea051b
Yoo Seung Guk <dbtmd878@naver.com>
2022-11-01 17:42:54 +0900

    refactor: 코드 레픽토링

--- src/problem6.js
+++ src/problem6.js
@@ -1,24 +1,38 @@
-function problem6(forms) {
-  const arr = new Set();
-  let index = 0;
-  let state = Boolean;
-  for (let [email, nickname] of forms) {
-    state = false;
-    for (let i = 0; i < nickname.length - 1; i++) {
-      let twoChar = nickname[i] + nickname[i + 1];
-      for (let j = index + 1; j < forms.length; j++) {
-        if (forms[j][1].includes(twoChar) && !arr.has(forms[j][0])) {
-          arr.add(forms[j][0]);
-          state = true;
-        }
-      }
-    }
-    index++;
-    if (state) {
-      arr.add(email);
+function sameNameCheck(twoChar, nickname, forms, emailSet) {
+  let localState = false;
+  for (let j = 0; j < forms.length; j++) {
+    if (nickname === forms[j][1]) continue;
+    if (forms[j][1].includes(twoChar) && !emailSet.has(forms[j][0])) {
+      emailSet.add(forms[j][0]);
+      localState = true;
     }
   }
-  const answer = [...arr].sort();
+  return localState;
+}
+function nameSplit(nickname, forms, emailSet) {
+  let localState = Boolean;
+  for (let i = 0; i < nickname.length - 1; i++) {
+    let twoChar = nickname[i] + nickname[i + 1];
+    localState = sameNameCheck(twoChar, nickname, forms, emailSet);
+  }
+  return localState;
+}
+
+function sortEmail(emailArray) {
+  return emailArray.sort();
+}
+
+function problem6(forms) {
+  const EmailSet = new Set();
+
+  for (let [email, nickname] of forms) {
+    let state = nameSplit(nickname, forms, EmailSet);
+
+    if (state) {
+      EmailSet.add(email);
+    }
+  }
+  const answer = sortEmail([...EmailSet]);
   return answer;
 }
 
