7e9d9fbae095577151cadae0270e3c826e47e862
SEONGJU HAN <hsj990604@gmail.com>
2022-11-01 18:59:32 +0900

    add problems solutions

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,26 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+
+  //책의 범위
+  
+  //Left 값 계산
+
+  //Right 값 계산
+
+  //점수 비교
+
+  //결과 출력
+  if (maxNum(pobi) > maxNum(crong)) {
+    return 1;
+  } else if (maxNum(pobi) < maxNum(crong)) {
+    return 2;
+  } else {
+    return 0;
+  }
+
+  //예외 상황
+  if (pobi[1] !== pobi[0]+1 || crong[1] !== crong[0]+1) {
+    return -1;
+  }
 }
 
 module.exports = problem1;
--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,24 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
-}
+  const message = [];
+  message.push(cryptogram[0]);
+
+  //중복 문자 제거 암호문
+  for(let i = 1; i < cryptogram.length; i++) {
+    const currentElement = cryptogram[i];
+
+    if (message.length === 0) {
+      message.push(currentElement);
+    } else {
+      const lastElement = message[message.length - 1];
+
+      if (lastElement == currentElement) {
+        message.pop();
+      } else {
+        message.push(currentElement);
+      }
+    }
+  }
+  return message.join("");
+  }
 
 module.exports = problem2;
