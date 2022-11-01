9f047a25cad70d2a5862638e96b4d797fc797a24
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 21:03:12 +0900

    [problem5] feat: 각 화폐당 변환 갯수 함수 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,17 @@
 function problem5(money) {
-  var answer;
   // 화폐 단위 배열 선언
-  wonType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  let count = 0;
+  let wonType = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let countArr = [];
+  let restMoney = money;
 
   // 화폐 큰 단위 순서대로 나누고 나머지 넘기기
+  for (let i = 0; i < wonType.length; i++) {
+    countArr.push(Math.floor(restMoney / wonType[i]));
+    restMoney = restMoney % wonType[i];
+  }
 
   // 각 화폐당 변환 갯수 배열에 담기
-  return answer;
+  return countArr;
 }
 
 module.exports = problem5;
