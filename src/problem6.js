440dca6fb0b226514681107933b91080a27ce110
Minju Park <project.mizzu@gmail.com>
2022-11-01 19:51:33 +0900

    Feat : 이메일 목록 오름차순 정렬 후 반환
    - result에 있는 이메일 목록들을 sort()를 사용하여 정렬
    - 최종 result 반환

--- src/problem6.js
+++ src/problem6.js
@@ -4,8 +4,8 @@
 // - [✅] substring을 통해 (substring(index, index + 2)) keyword 추출
 // - [✅] 추출한 keyword를 바탕으로 includes(keyword[idx])를 통해 제거 대상인 이름 추출 -> duplicateNick.push()
 // [✅] duplicateNick에 존재하는 이름과 forms의 이름을 비교하여, 일치하면 result로 반환
-// [] result 내 이메일 목록을 오름차순 정렬한다.
-// [] result 내 이메일 목록에서 중복된 이메일을 제거한다.
+// [✅] result 내 이메일 목록을 오름차순 정렬한다.
+// [✅] result 내 이메일 목록에서 중복된 이메일을 제거한다. -> 닉네임 상에서 이미 중복 처리
 
 // [예외처리] -> 제한 사항에 어긋나는 경우로, forms 배열에서 제거
 // 신청한 이메일의 길이가 11자 이상 20자 미만이 아닌 경우 (email >= 11 && nick < 20)
@@ -50,17 +50,7 @@ function problem6(forms) {
       }
     });
   }
-  return result;
+  return result.sort();
 }
-
-console.log(
-  problem6([
-    ['jm@email.com', '제이엠'],
-    ['jason@email.com', '제이슨'],
-    ['woniee@email.com', '워니'],
-    ['mj@email.com', '엠제이'],
-    ['nowm@email.com', '이제엠'],
-  ])
-);
 
 module.exports = problem6;
