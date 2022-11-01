24bbce4642903f84ab6d4d80e7492d10e4b1dea0
wldnd2 <jiwoong000628@gmail.com>
2022-11-01 18:30:45 +0900

    [problem3] 기능 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,17 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var count = 0;
+  for (var i = 1; i <= number; i++) count += check_369(i);
+  return count;
+}
+
+function check_369(num) {
+  var count = 0;
+  while (true) {
+    if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) count++;
+    num = Math.floor(num / 10);
+    if (num == 0) break;
+  }
+  return count;
 }
 
 module.exports = problem3;
