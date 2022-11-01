cad1392f0fda4ae8f86930e95146752198810278
oxhwixo <hwi_030@naver.com>
2022-11-01 17:48:55 +0900

    Problem7: 친구 관계를 나타내는 객체 생성 기능 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,18 @@
+/** 친구 관계를 나타내는 객체를 생성하는 기능 **/
+function makeFriendsList(friends) {
+  let friendsObject = {};
+
+  for (let item of friends) {
+    for (let i = 0; i < 2; i++) {
+      if (item[i] in friendsObject) friendsObject[item[i]].push(item[1 - i]);
+      else friendsObject[item[i]] = [item[1 - i]];
+    }
+  }
+  return friendsObject;
+}
+
 function problem7(user, friends, visitors) {
-  var answer;
+  var answer = [];
   return answer;
 }
 
