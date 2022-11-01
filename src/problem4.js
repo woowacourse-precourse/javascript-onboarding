a68cb9b78a562ace9fe3b9c9dc78719cffe005f9
Vo9Ming <kimminj2017@gmail.com>
2022-11-01 18:28:22 +0900

    내가 전달받은 요소에 하나하나 접근할때 해당되는 알파벳에 해당하는 청개구리 알파벳을 찾기위해 얼마나 움직였는지 그 값을 찾기위해 index함수 추가

--- src/problem4.js
+++ src/problem4.js
@@ -7,12 +7,27 @@ function problem4(word) {
   var fLoAlp = revAlp.toLowerCase().split("");
 
   for (var int = 0; int < word.length; int++) {
+    console.log(word.length);
     var uniNum = word[int].charCodeAt();
-    if (uniNum > 65 && uniNum < 91) {
-    }
   }
 
   return answer;
+}
+function index(char) {
+  var result = [];
+  var Alp = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
+  var mUpAlp = Alp.split("");
+  var mLoAlp = Alp.toLowerCase().split("");
+  var index = char.charCodeAt();
+  if (index > 64 && index < 91) {
+    result.push(mUpAlp.indexOf(char));
+  } else if (index > 96 && index < 123) {
+    result.push(mLoAlp.indexOf(char));
+  } else {
+    result.push(" ");
+  }
+
+  return result;
 }
 
 module.exports = problem4;
