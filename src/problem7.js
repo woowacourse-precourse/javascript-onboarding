437aac65b22157ab520c096efc7fdbf9fc27dce2
osdoonhyun <skw1996@naver.com>
2022-11-01 17:07:35 +0900

    [문제7] 기능 목록 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,10 @@
+/* 기능 목록
+1. friends에서 user의 친구를 찾고 찾은 user 친구의 친구를 찾아 점수를 부여하는 기능
+  1-1. friends에서 user가 들어간 요소를 찾고 user의 친구를 새로운 배열에 할당한다.
+  1-2. user의 친구의 친구를 찾고 점수를 부여하여 추천 사용자 아이디와 점수가 들어간 새로운 배열을 만든다.
+2. visitors에서 사용자 아이디와 점수를 부여한 배열을 만드는 기능
+3. 점수가 높은 순으로 정렬, 추천 점수가 같다면 이름순으로 정렬하는 기능
+*/
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
