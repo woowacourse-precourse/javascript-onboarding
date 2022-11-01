94ba877214a144740093c0282b860ca2ee92680f
dongyeopca <ckehdduq95@gmail.com>
2022-11-01 15:01:50 +0900

    feat: "Problem5 기능 구현" 그리디하게 동전 배분 기능 구현

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,41 @@
 function problem5(money) {
-  var answer;
+  const answer = new Array(9).fill(0);
+  while (50000 <= money) {
+    money -= 50000;
+    answer[0] += 1;
+  }
+  while (10000 <= money) {
+    money -= 10000;
+    answer[1] += 1;
+  }
+  while (5000 <= money) {
+    money -= 5000;
+    answer[2] += 1;
+  }
+  while (1000 <= money) {
+    money -= 1000;
+    answer[3] += 1;
+  }
+  while (500 <= money) {
+    money -= 500;
+    answer[4] += 1;
+  }
+  while (100 <= money) {
+    money -= 100;
+    answer[5] += 1;
+  }
+  while (50 <= money) {
+    money -= 50;
+    answer[6] += 1;
+  }
+  while (10 <= money) {
+    money -= 10;
+    answer[7] += 1;
+  }
+  while (1 <= money) {
+    money -= 1;
+    answer[8] += 1;
+  }
   return answer;
 }
 
