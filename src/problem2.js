301c1e8d15a0cc45ffdfbc7a0d076fd79bd30baa
allzeroyou <ekdudd119@naver.com>
2022-11-01 17:27:56 +0900

    [feat] 중복 문자 삭제하는 기능 구현

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,17 @@
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  if (1 <= cryptogram <= 1000) {
+    cryptogram.toLowerCase();
+    let stack = [];
+    for (let i of cryptogram) {
+      if (stack[stack.length - 1] === i) {
+        stack.pop();
+        continue;
+      }
+      stack.push(i);
+    }
+    let answer = stack.join("");
+    return answer;
+  }
 }
 
 module.exports = problem2;
