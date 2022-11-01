7c515c70dcf2267a627d0426df320198f3ec0ae8
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 19:16:53 +0900

    feat: [PROBLEM1] 페이지 예외 사항 판단 함수

--- src/problem1Util.js
+++ src/problem1Util.js
@@ -1,5 +1,5 @@
 /**
- * 숫자를 받고, 각 자리수에 따른 숫자를 반환함.
+ * 숫자를 받고, 각 자리수에 따른 숫자를 반환
  * ex) 123 -> [1, 2, 3]
  * @param {number} num 변환할 숫자
  * @returns {array} 자리수 배열
@@ -27,7 +27,7 @@ const calcPageValues = (digitList) => {
 };
 
 /**
- * 2가지 숫자 비교 함수
+ * 2가지 숫자 비교
  * @param {number} num1 숫자(기준)
  * @param {number} num2 숫자(상대)
  * @return {number} 무승부 0, 승리 1, 패배 -1
@@ -37,4 +37,19 @@ const compareValue = (num1, num2) => {
   return num1 > num2 ? 1 : -1;
 };
 
-module.exports = { getDigitFromNum, calcPageValues, compareValue };
+/**
+ * 페이지를 받아 이것이 유효한 페이지인지 확인
+ * @param {number} leftPage
+ * @param {number} rightPage
+ * @param {number} startPage
+ * @param {number} lastPage
+ * @return {boolean} 시작 또는 끝페이지인 경우, 페이지 차이가 1이 아닌경우, 왼쪽-홀수 오른쪽-짝수가 아닌경우 inValid
+ */
+const isValidPage = (leftPage, rightPage, startPage, lastPage) => {
+  if (leftPage === startPage || rightPage === lastPage) return false;
+  if (rightPage - leftPage !== 1) return false;
+  if (!(leftPage % 2 === 1 && rightPage % 2 === 0)) return false;
+  return true;
+};
+
+module.exports = { getDigitFromNum, calcPageValues, compareValue, isValidPage };
