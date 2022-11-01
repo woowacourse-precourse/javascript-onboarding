c6058bbc85823b93c8fae7cf6b74fb5a66fffd65
sebin kwon <sbkwon98@gmail.com>
2022-11-01 21:51:20 +0900

    Solved problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,43 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer = 0;
+  if (pobi % 2 === 0) {
+    return -1
+  }
+  if (pobi[0] + 1 === pobi[1]) {
+    if (crong[0] + 1 === crong[1]) {
+      let max_pobi = 0;
+      max_pobi = big_number(pobi);
+      let max_crong = 0;
+      max_crong = big_number(crong);
+      if (max_pobi > max_crong) {
+          answer = 1;
+      }
+      else if (max_pobi < max_crong) {
+          answer = 2;
+      }
+      else {
+          answer = 0;
+      }
+      return answer;
+    }
+  }
+  answer = -1;
   return answer;
 }
 
+function big_number(page_lst) {
+  const answer = [];
+  for (let num of page_lst) {
+    let page_multiple = 1;
+    let page_sum = 0;
+    let text = num.toString();
+    for (let n of text) {
+      page_multiple *= Number(n);
+      page_sum += Number(n);
+    }
+    answer.push(page_multiple);
+    answer.push(page_sum);
+  }
+  return Math.max.apply(null,answer);
+}
 module.exports = problem1;
