1768456701af417a2a32d4ccbac621c850b06e1d
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 18:46:04 +0900

    feat: [Problem1] 각 자리수를 얻는 함수 반환 값 변경

--- src/problem1Util.js
+++ src/problem1Util.js
@@ -1,3 +1,9 @@
+/**
+ * 숫자를 받고, 각 자리수에 따른 숫자를 반환함.
+ * ex) 123 -> [1, 2, 3]
+ * @param {number} 변환할 숫자
+ * @returns {array} 자리수 배열
+ */
 const getDigitFromNum = (num) => {
   const digitList = [];
   while (num > 0) {
@@ -5,7 +11,7 @@ const getDigitFromNum = (num) => {
     num = parseInt(num / 10);
     digitList.push(rest);
   }
-  return digitList;
+  return digitList.reverse();
 };
 
 module.exports = { getDigitFromNum };
