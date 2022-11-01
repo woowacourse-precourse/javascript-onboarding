d02bc0e90ae5d98665fe69c4d5359aae38d5ec66
youngjoo <yungjoo0624@gmail.com>
2022-11-01 18:02:44 +0900

    Update : 문제 3. 기능 목록 사항 수정

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,28 @@
 function problem3(number) {
   var answer;
+
+  let count = 0;
+  let newNumber;
+  let remainder;
+
+  // 숫자를 10으로 나눈 나머지가 3, 6, 9가 있거나, 30으로 나눈 나머지가 0 일 경우 count += 1
+
+  function game_369(number, newNumber) {
+    for(let i=1; i<=number; i++) {
+        newNumber = Math.floor(i%10);
+        console.log("newNumber : " + newNumber);
+        if((newNumber == 3 || newNumber == 6 || newNumber == 9) || (i%30 == 0)) {
+          count++;
+          console.log("if문 내부 : " + newNumber, count);
+      }
+    }
+    return count;
+  }
+
+  const result = game_369(number, newNumber);
+
+  answer = result;
+  
   return answer;
 }
 
