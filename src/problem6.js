0785bf0be2509af2144f973c0024b882996a0b66
Saans <92422813+saans808@users.noreply.github.com>
2022-11-01 13:54:07 +0900

    [p6] 두 글자씩 뽑아 배열 생성, 중복 카운트 객체 생성

--- src/problem6.js
+++ src/problem6.js
@@ -12,6 +12,18 @@ function problem6(forms) {
   for (let i=0; i<forms.length; i++) {
     name.push(forms[i][1]);
   }
+
+  name.forEach(function(ele, idx) {
+    for(i=0; i<ele.length; i++) {
+        if(i==ele.length-1) break;
+            rep.push(ele.substr(i,2));   
+        }
+    })
+  // 중복 배열 카운트
+  let cntRep = {};
+  rep.forEach((x) => { 
+    cntRep[x] = (cntRep[x] || 0)+1; 
+  });
   
   return answer;
 }
