dae76deb74f40a99b9bbf94f8f94acd443cc510e
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-01 10:59:08 +0900

    feat: [PROBLEM5] money를 큰 수부터 차례로 나눈다

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
   var answer;
+  let unit = [50000, 10000, 5000, 1000, 500, 100, 50, 10];
+
+  for (let i = 0; i < unit.length; i++) {
+    let moneyTemp = money % unit[i];
+    console.log(moneyTemp);
+  }
   return answer;
 }
 
