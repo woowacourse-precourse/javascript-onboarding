8b1494d06f3fddd298c139ce3feb92f0488c1be1
hvvany <kjunhwan1998@gmail.com>
2022-11-01 20:59:29 +0900

    feat(build): solve problem_4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,34 @@
 function problem4(word) {
   let answer = "";
+  let letter = "azA";
+  const a = letter.charCodeAt(0)
+  const z = letter.charCodeAt(1)
+  const A = letter.charCodeAt(2)
+  const difference = z - a;
+  for (let i = 0; i < word.length; i++) {
+    let text = word[i];
+    if (text === " ") {
+      answer += ' ';
+    }
+    else if (isupper(text)) {
+      answer += String.fromCharCode((difference - (text.charCodeAt(0)-A))+A)
+    }
+    else {
+      answer += String.fromCharCode((difference - (text.charCodeAt(0)-a))+a)
+    }
+  }
   return answer;
+}
+
+function isupper(character) {
+  if (character == character.toLowerCase())
+  {
+    return false;
+  }
+  else
+  {
+    return true;
+  }
 }
 
 module.exports = problem4;
