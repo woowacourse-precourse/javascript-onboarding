16865e1c99edad9b55ba8efc1ae5bb3ee71108d5
jlee0505 <hj960322@gmail.com>
2022-11-01 19:19:28 +0900

    feat(problem3): 기능 3,4,5 구현

--- src/problem3.js
+++ src/problem3.js
@@ -1,9 +1,21 @@
 function problem3(number) {
   let count = 0;
+  let game = [];
 
   if (number < 3) return 1;
 
-  for (let i = 3; i <= number; i++) {}
+  for (let i = 3; i <= number; i++) {
+    game = String(i).match(/[3|6|9]/g);
+
+    if (game !== null && game.length >= 2) {
+      count += game.length - 1;
+    }
+    if (game !== null) {
+      count++;
+    }
+  }
+
+  return count;
 }
 
 module.exports = problem3;
