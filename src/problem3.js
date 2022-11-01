b387d65de263e253ebe30358439985b83d3cf691
myeongheonhong <ruddyh7076@gmail.com>
2022-11-01 16:47:25 +0900

    feat : 숫자를 문자로 더해준 후 배열로 변환하여 해당 숫자가 있을 경우 카운트 증가하는 함수 구현.

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,19 @@
 function problem3(number) {
-  var answer;
-  return answer;
+    let answer = 0;
+
+    let numToString = '';
+    for (let i = 1; i <= number; i++) {
+        numToString += i.toString();
+    }
+
+    numToString.split('').map(function (e) {
+        if (e === '3' || e === '6' || e === '9') answer++;
+    });
+
+    return answer;
 }
 
 module.exports = problem3;
+
+//1. 해당숫자까지 문자열에 더해준다
+//2. 문자열에 3,6,9가 포함된 갯수를 카운트 해준다.
