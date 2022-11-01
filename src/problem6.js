a0ce41d3b7e145e614f068aec714d7fe2b4244a4
KangYunJi <dbswl701@ajou.ac.kr>
2022-11-01 21:27:02 +0900

    feat:[문제6] forms에서 닉네임 2글자씩 끊기

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,17 @@
 function problem6(forms) {
-  var answer;
+  let answer=[];
+  let map = new Map();
+
+  // forms에서 닉네임 2글자씩 끊기
+  for(let form of forms){
+    for(let i = 0; i<form[1].length-1; i++){
+      let str = form[1].substr(i, 2); // 글자 2개씩 끊어서 str에 저장
+      if(!map.has(str)){ map.set(str, 1); } // 없으면 map에 value를 1로 한 key(str) 넣는다.
+      else{ map.set(str, map.get(str)+1); } // 있으면 해당 key의 value를 증가시킨다
+    }
+  }
+
+
   return answer;
 }
 
