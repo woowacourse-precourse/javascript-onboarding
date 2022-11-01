e33d5428f239ee6ada0cc4ba9ec0800d0985d3ef
jjjjjeongwon <jjjjjwon21@gmail.com>
2022-11-01 22:00:28 +0900

    feat: Add duplication email(problem6)

--- src/problem6.js
+++ src/problem6.js
@@ -1,44 +1,33 @@
 function problem6(forms) {
-  let answer;
-  return answer;
+  return duplicationEmailList(forms);
 }
 
-// function splitNickname(forms) {
-//   let nicknameArr = [];
-//   for (let i = 0; i < forms.length; i++) {
-//     nicknameArr.push(forms[i][1].split(""));
-//   }
-//   console.log(nicknameArr);
-// }
+function duplicationEmailList(forms) {
+  const emailCheckWord = {};
+  const emailExistCheckWord = new Set();
 
-function compareTwoWordsArr(splitNicknameArr){
-  let compareArr =[];
-  for(let i =0; i<splitNicknameArr.length-1; i++){
-  for(let j =0; j<splitNicknameArr[i].length-1; j++){
-    compareArr.push([splitNicknameArr[i][j],splitNicknameArr[i][j+1]]);
-    console.log(compareArr);
-  }
-  
+  forms.forEach(([email, nickname]) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const check = nickname.substring(i, i + 2);
+
+      if (emailCheckWord[check]) {
+        emailExistCheckWord.add(emailCheckWord[check]);
+        emailExistCheckWord.add(email);
+      } else {
+        emailCheckWord[check] = email;
+      }
+    }
+  });
+
+  return ;
 }
-  // console.log(compareArr);
-}
 
-console.log(compareTwoWordsArr([
-  [ '제', '이', '엠' ],
-  [ '제', '이', '슨' ],
-  [ '워', '니' ],
-  [ '엠', '제', '이' ],
-  [ '이', '제', '엠' ]
-]));
-
-// console.log(
-//   splitNickname([
-//     ["jm@email.com", "제이엠"],
-//     ["jason@email.com", "제이슨"],
-//     ["woniee@email.com", "워니"],
-//     ["mj@email.com", "엠제이"],
-//     ["nowm@email.com", "이제엠"],
-//   ])
-// );
-
-module.exports = problem6;
+console.log(
+  problem6([
+    ["jm@email.com", "제이엠"],
+    ["jason@email.com", "제이슨"],
+    ["woniee@email.com", "워니"],
+    ["mj@email.com", "엠제이"],
+    ["nowm@email.com", "이제엠"],
+  ])
+);
