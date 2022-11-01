7073ed020bd8a6c3c24643894bb720f599eebcc6
jkea1 <jinkyng7890@gmail.com>
2022-11-01 16:09:14 +0900

    커밋

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,5 @@
 function problem2(cryptogram) {
-  let answer = "";
+var answer = "";
   for(let i = 0; i<cryptogram.length; i++){
     if(cryptogram.indexOf(cryptogram[i]) == i) answer += cryptogram[i];
   }
