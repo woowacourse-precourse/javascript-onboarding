ab8c5caed48c6b04c6226efc394424ef51d6d3de
DAIN KIM <dnkim419@gmail.com>
2022-11-01 21:15:04 +0900

    fix: edited problem7

--- src/problem7.js
+++ src/problem7.js
@@ -3,35 +3,42 @@ function problem7(user, friends, visitors) {
   let graph = {};
   let candidate = {};
 
-  for (let [a, b] in friends) {
-    graph[a] ? graph[a].push(b) : (graph[a] = [b]);
-    graph[b] ? graph[b].push(a) : (graph[b] = [a]);
+  for (const [a, b] of friends) {
+    const has = Object.prototype.hasOwnProperty;
+    has.call(graph, a)
+      ? (graph[`${a}`] = [...graph[`${a}`], `${b}`])
+      : (graph[`${a}`] = [`${b}`]);
+    has.call(graph, b)
+      ? (graph[`${b}`] = [...graph[`${b}`], `${a}`])
+      : (graph[`${b}`] = [`${a}`]);
   }
 
-  for (let i in graph[user]) {
-    for (let j in graph[i]) {
-      if (graph[user].includes(j)) continue;
-      candidate[j] ? (candidate[j] += 10) : (candidate[j] = 10);
+  for (const friendOfUser of graph[user]) {
+    for (const friendOfFriend of graph[friendOfUser]) {
+      if (graph[user].includes(friendOfFriend)) continue;
+      if (friendOfFriend === user) continue;
+      const has = Object.prototype.hasOwnProperty;
+      has.call(candidate, friendOfFriend)
+        ? (candidate[`${friendOfFriend}`] += 10)
+        : (candidate[`${friendOfFriend}`] = 10);
     }
   }
 
-  for (let i in visitors) {
-    candidate[i] ? (candidate[i] += 1) : (candidate[i] = 1);
+  for (const visitor of visitors) {
+    if (graph[user].includes(visitor)) continue;
+    const has = Object.prototype.hasOwnProperty;
+    has.call(candidate, visitor)
+      ? (candidate[`${visitor}`] += 1)
+      : (candidate[`${visitor}`] = 1);
   }
 
-  let [
-    first = ["", 0],
-    second = ["", 0],
-    third = ["", 0],
-    fourth = ["", 0],
-    fifth = ["", 0],
-  ] = Array.from(candidate)
-    .sort((a, b) => a[1] - b[1])
-    .reverse();
-
-  for (let [userName, score] in [first, second, third, fourth, fifth]) {
-    if (score !== 0) answer.push(userName);
+  const sorted = Object.entries(candidate)
+    .sort((a, b) => b[1] - a[1])
+    .slice(0, 5);
+  for (const [userName] of sorted) {
+    answer.push(userName);
   }
+
   return answer;
 }
 
