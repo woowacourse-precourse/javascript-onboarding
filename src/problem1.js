80edfdf39ede0174198170cdac3e8d8bfe857b50
yeongseokim <1903020@ewhain.net>
2022-11-01 20:39:39 +0900

    feat(problem1): 각 자리 숫자 곱과 합 중 가장 큰 값을 반환하는 함수

--- src/problem1.js
+++ src/problem1.js
@@ -20,6 +20,10 @@ function sumDigit(num) {
   return sum;
 }
 
+function getMaxScore(arr) {
+  return Math.max(multDigit(arr[0]), multDigit(arr[1]), sumDigit(arr[0]), sumDigit(arr[1]));
+}
+
 function problem1(pobi, crong) {
   if (pobi.length != 2) return -1
   if (crong.length != 2) return -1
