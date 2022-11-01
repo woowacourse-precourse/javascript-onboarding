b12cf863ee5b058687713ae87b1d842d9d46cbb7
SeungWoo Kim <osabero@naver.com>
2022-11-01 16:21:04 +0900

    반복문을 중첩사용하여 1부터 제시된 숫자까지 완전탐색하도록 하였습니다. 솔직히 더 깔끔하고 반복문을 중첩하지않아도 될 방법이 있을것 같은데 좀더 생각해보아야할것 같습니다.

--- src/problem3.js
+++ src/problem3.js
@@ -1,7 +1,15 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  var answer = 0;
+  for(let i = 0; i<=number;i++){
+    let dummy = i.toString()
+    for(let x of dummy){
+      if(x==="3" || x==="6" || x==="9"){
+        answer++
+      }
+    }
+  }
   
+  return answer;
 }
 
 module.exports = problem3;
