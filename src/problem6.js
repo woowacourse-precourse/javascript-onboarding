48e0a2f357f08c114f70ce3c626244c168942b8f
sh981013s <sh981013s@gmail.com>
2022-11-01 10:37:50 +0900

    feat: [6] 문자열 => 두글자 문자열 변환 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,13 @@
+/**
+ * 문자열을 두글자의 문자열로 쪼갠다
+ * @param {string} nickName - 닉네임
+ * @param {number} index - index 번호
+ * @return {string}
+ */
+const getTemporaryWord = (nickName, index) => {
+  return nickName.substring(index, index + 2);
+};
+
 const problem6 = (forms) => {
   const answer = [];
   const commonNickName = {};
