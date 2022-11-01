05ccea8ee765fc722e9928be06e9fe8475afa2dd
kimseo-0 <42965120+kimseo-0@users.noreply.github.com>
2022-11-01 21:20:40 +0900

    feat: [problem1] 포비와 크롱 점수 비교

--- src/problem1.js
+++ src/problem1.js
@@ -10,6 +10,15 @@ function problem1(pobi, crong) {
   let pobiScore = Math.max(sumOfEachDigit(pobi[0]), multiplyOfEachDigit(pobi[0]), sumOfEachDigit(pobi[1]), multiplyOfEachDigit(pobi[1]))
   let crongScore = Math.max(sumOfEachDigit(crong[0]), multiplyOfEachDigit(crong[0]), sumOfEachDigit(crong[1]), multiplyOfEachDigit(crong[1]))
 
+  if (pobiScore > crongScore) {
+    return 1
+  } else if (pobiScore < crongScore) {
+    return 2
+  } else {
+    return 0
+  }
+}
+
 const sumOfEachDigit = (num) => {
   let sum = 0
   while (num > 0) {
