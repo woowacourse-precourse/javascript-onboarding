9e41222e08da495839a062505688e1d35e8d5bfb
jlee0505 <hj960322@gmail.com>
2022-11-01 20:10:33 +0900

    feat(problem4): 기능3 구현

--- src/problem4.js
+++ src/problem4.js
@@ -1,24 +1,32 @@
 function problem4(word) {
   const ascii = convertWordToAscii(word);
-  const result = convertAsciiToWord(ascii);
+  const answer = convertAsciiToWord(ascii);
 
-  return result;
+  return answer;
 }
 
 function convertWordToAscii(str) {
   const arrOfAscii = [];
+
   for (let char of str) {
     let ascii = str.charCodeAt(str.indexOf(char));
     arrOfAscii.push(ascii);
   }
+
   return arrOfAscii;
 }
 
 function convertAsciiToWord(arrOfNum) {
-  // if (ascii < 65 || ascii > 122 || (ascii < 97 && ascii > 90)) return "";
-  // if (ascii === 32) return " ";
-  // if (ascii <= 90) return 90 - ascii + 65;
-  // if (ascii >= 97) return 122 - ascii + 97;
+  let answer = "";
+
+  arrOfNum.forEach((ascii) => {
+    if (ascii === 32) answer += " ";
+    if (ascii < 65 || ascii > 122 || (ascii < 97 && ascii > 90)) return;
+    if (ascii <= 90) answer += String.fromCharCode(90 - ascii + 65);
+    if (ascii >= 97) answer += String.fromCharCode(122 - ascii + 97);
+  });
+
+  return answer;
 }
 module.exports = problem4;
 
