6ac2f62f476b0f9f7fad34f2b63cdf773fd6d48b
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 21:59:36 +0900

    feat: Implement connected object in problem7.js
    친구 관계를 표현하는 connected 오브젝트를 구현하였음

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,11 @@
 function problem7(user, friends, visitors) {
   const connected =  new Object();
+  for (relationship of friends) {
+    const friend1 = relationship[0];
+    const friend2 = relationship[1];
+    setDefault(connected, friend1, friend2);
+    setDefault(connected, friend2, friend1);
+  }
   var answer;
   return answer;
 }
