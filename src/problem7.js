e553e7b35afd11cb4581fcb292b0832c4ecfdb2f
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 22:53:48 +0900

    feat(problem7): 총 점수가 높은 순으로 친구를 추천한다

--- src/problem7.js
+++ src/problem7.js
@@ -36,7 +36,29 @@ function problem7(user, friends, visitors) {
       pointState[visitor] = 1;
     }
   });
+  
+  delete pointState[user];
 
+  friendState[user].forEach((friendOfUser) => {
+    delete pointState[friendOfUser];
+  });
+
+  Object.keys(pointState).forEach((key) => {
+    if (pointState[key] === 0) delete pointState[key];
+  });
+
+  const result = Object.entries(pointState)
+    .sort((a, b) => {
+      if (a[1] === b[1]) {
+        if (a[0] > b[0]) return 1;
+        if (a[0] < b[0]) return -1;
+        if (a[0] === b[0]) return 0;
+      }
+      return b[1] - a[1];
+    })
+    .map((el) => el[0]);
+
+  return result;
 }
 
 module.exports = problem7;
