df1ec635c8570a4e242f7cef2697e5096bb3cb8e
moseung <tmdah900@naver.com>
2022-11-01 16:27:30 +0900

    ✨ problem7 기능 1 구현 기능1 user와 친구인 목록을 만드는 함수 생성

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,18 @@
+function getUserFriend(friends, user) {
+  let userFriend = [];
+  for (i = 0; i < friends.length; i++) {
+    if (friends[i].includes(user)) {
+      friends[i].forEach((value) => {
+        if (value !== user) userFriend.push(value);
+      });
+    }
+  }
+  return userFriend;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
+  const userFriend = getUserFriend(friends, user);
   return answer;
 }
 
