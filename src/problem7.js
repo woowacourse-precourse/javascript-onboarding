f3c9c19720873089514a7c7efc7d19c79b361c62
sh981013s <sh981013s@gmail.com>
2022-11-01 19:52:07 +0900

    feat: [7] 점수 관련 상수 선언

--- src/problem7.js
+++ src/problem7.js
@@ -6,6 +6,10 @@
  */
 const problem7 = (user, friends, visitors) => {
   const answer = [];
+  // 아는 친구 점수
+  const MUTUAL_FRIEND_POINT = 10;
+  // 타임라인 방문 점수
+  const VISITOR_POINT = 1;
 
   return answer;
 };
