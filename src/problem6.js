53f7e3c41939c6e514cea7bb4543cca222cd1452
Blackraven93 <reblackraven@gmail.com>
2022-11-01 13:24:30 +0900

    Problem6[Feat] 이름들의 배열을 순회하며 정규표현식을 이용해 중복된 이름이 들어가 있다면 answer에 해당 이메일을 추가한다.

--- src/problem6.js
+++ src/problem6.js
@@ -3,10 +3,18 @@ function problem6(forms) {
 
   const studentNames = forms.map((v) => v[1]);
 
-  studentNames.forEach((word) => {
+  studentNames.forEach((word, studentIndex) => {
     [...word].forEach((alphbet, index) => {
-      if (word.length - 1 === index) return; // 다음거
+      if (word.length - 1 === index) return;
       const duplicatedName = alphbet + word[index + 1];
+
+      studentNames.forEach((v, i) => {
+        if (i === studentIndex) return;
+
+        const reg = new RegExp(duplicatedName, "gi");
+
+        if (reg.test(v)) answer.push(studentEmails[i]);
+      });
     });
   });
 }
