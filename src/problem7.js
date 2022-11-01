7749066d67fb21dd6db62220324322f4519a87df
myung-j-p <jmpack97@gmail.com>
2022-11-01 16:51:35 +0900

    문제7_ 이미 친구인 사람과 함께 아는 친구 찾고 점수 계산

--- src/problem7.js
+++ src/problem7.js
@@ -1,15 +1,53 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  const alreadyFriends = [];
+  var answer = {};
 
-  for(i in friends){
-    if(user == friends[i][0]){
-      return alreadyFriends.push(friends[i][1]);
+  function findFriends (u, f){
+    const alreadyFriends = [];
+    for(i in f){
+      if(u == f[i][0]){
+        alreadyFriends.push(f[i][1])
+      }
+      if(u == f[i][1] && alreadyFriends.indexOf(u) == -1){
+        alreadyFriends.push(f[i][0])
+      }
     }
-    if(user == friends[i][1] && alreadyFriends.indexOf(user) == -1){
-      alreadyFriends.push(friends[i][0]);
-    }
+    return alreadyFriends
   }
+  function findFriendssFriends(alreadyFriends){
+    let friendssFriends = [];
+
+    alreadyFriends.forEach(f => {
+      let r = findFriends(f,friends);
+      friendssFriends.push(...r);
+    });
+
+    return friendssFriends
+  }
+  function scoreChecker (friendssFriends){
+    //오브젝트에 키 집어넣기
+    friendssFriends.forEach(f => answer[f] = 0);
+    let score = 0;
+
+    // 키 하나씩 호출
+    Object.keys(answer).forEach(k => {
+      for(i in friendssFriends){
+        if (k == friendssFriends[i]){
+          score += 10;
+        }
+      }
+      answer[k] = score;
+      score = 0;
+    });
+  }
+
+
+  const alreadyFriends = findFriends(user,friends);
+  const friendssFriends = findFriendssFriends(alreadyFriends);
+
+  scoreChecker(friendssFriends);
+  delete answer[user]
+
+  console.log(answer);
 
   return answer;
 }
