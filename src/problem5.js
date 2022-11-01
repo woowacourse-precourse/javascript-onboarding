c94e6a744557455be478ec5cc02afb554d27e51b
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 15:23:29 +0900

    [problem5] feat: 1.Create money array & result array

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,10 @@
 function problem5(money) {
   var answer;
+  // 오만 원권부터 일원 동전까지 moneyArr에 넣음
+  const moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  // 결과값 저장을 위한 resultArr
+  const resultArr = [];
+
   return answer;
 }
 
