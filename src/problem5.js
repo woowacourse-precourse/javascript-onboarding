dec1e37c74266190979215ef4aed5633afe33958
HaroldBKim <sbookim2@gmail.com>
2022-11-01 21:21:29 +0900

    완벽하게 이해하지 못한 코드 삭제

--- src/problem5.js
+++ src/problem5.js
@@ -1,12 +1,5 @@
 function problem5(money) {
-  const coins = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  const answer = [];
-
-  for (i=0; i<coins.length; i++) {
-      const count = parseInt(money / coins[i]);
-      answer[i] = count
-      money -= (coins[i] * count);
-  }
+  var answer;
   return answer;
 }
 
--- src/problem6.js
+++ src/problem6.js
@@ -1,30 +1,6 @@
 function problem6(forms) {
-  let answer = [];
-  let dic = {};
-  let check;
-
-  for (crew of forms) {
-      for (i=0; i<crew[1].length-1; i++) {
-          check = crew[1].substring(i, i+2);
-          if (check in dic) {
-              dic[check] += 1;
-          }
-          else {
-              dic[check] = 1;
-          }
-      }
+    var answer;
+    return answer;
   }
-
-  for (crew of forms) {
-      for (j=0; j<crew[1].length-1; j++) {
-          check = crew[1].substring(j, j+2);
-          if (dic[check] > 1) {
-              answer.push(crew[0]);
-          }
-      }
-  }
-
-  return [...new Set(answer)].sort();
-}
-
-module.exports = problem6;
+  
+  module.exports = problem6;
