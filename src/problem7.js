abbc26e23f3506f9d5e55a99051e3fed1e174410
rabbitmjh <rabbitmjh@naver.com>
2022-11-01 21:52:11 +0900

    [p7] visitors 점수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -2,9 +2,9 @@ function findFriend(user, friends) {
   let friend = [];
 
   friends.forEach(([left, right]) => {
-    if(left === user)
+    if (left === user)
       friend.push(right);
-    if(right === user)
+    if (right === user)
       friend.push(left);
   });
 
@@ -13,50 +13,66 @@ function findFriend(user, friends) {
 
 function users(friends, user) {
   let userlist = new Set();
-  
-  friends.forEach(([left, right]) =>{
-    if(user !== left)
+
+  friends.forEach(([left, right]) => {
+    if (user !== left)
       userlist.add(left);
-    
-    if(user !== right)
+
+    if (user !== right)
       userlist.add(right);
   });
 
   return userlist;
 }
 
-function createScoreboard(userlist){
+function createScoreboard1(userlist) {
   let arr = [];
-  userlist.forEach((idx) => {
-    arr.push([0, idx]);
+  userlist.forEach((user) => {
+    arr.push([0, user]);
   });
   return arr;
 }
 
 function friendOfFriend(friends, score, userFriend, user) {
   friends.forEach(([left, right]) => {
-    if(userFriend.includes(left) && user !== right){
+    if (userFriend.includes(left) && user !== right) {
       const idx = score.findIndex(i => i[1] === right);
       score[idx][0] += 10;
     }
-    if(userFriend.includes(right) && user !== left){
-      const idx = score.findIndex(i => i[1] === left); 
+    if (userFriend.includes(right) && user !== left) {
+      const idx = score.findIndex(i => i[1] === left);
       score[idx][0] += 10;
     }
   });
 
   return score;
-} 
+}
+
+function visScore(score, visitors) {
+  visitors.forEach((visitor) => {
+    const check = score.find((item) => item[1] === visitor);
+    if (check) {
+      const idx = score.findIndex(i => i[1] === visitor);
+      score[idx][0]++;
+    }
+    else {
+      score.push([1, visitor]);
+    }
+  });
+  return score;
+}
 
 function problem7(user, friends, visitors) {
   userFriend = findFriend(user, friends);
-  
+
   userlist = users(friends, user);
   userlist = Array.from(userlist);
-  
-  let score = createScoreboard(userlist);
-  score = friendOfFriend(friends, score, userFriend, user);
 
+  let score = createScoreboard1(userlist);
+  score = friendOfFriend(friends, score, userFriend, user);
+  score = visScore(score, visitors);
+  
+  score.sort();
   return score;
 }
 //사용자와 친구관계인 유저를 구함
