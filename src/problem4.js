d0f7f4cca337818975257b9b034a44a0327525c1
leeharyung <lhr4884@naver.com>
2022-11-01 13:11:40 +0900

    add problem4 solution

--- src/problem4.js
+++ src/problem4.js
@@ -1,5 +1,14 @@
 function problem4(word) {
-  var answer;
+  const answer = [...word]
+    .map((i) => {
+      if (i.charCodeAt() < 91 && i.charCodeAt() > 64) {
+        return String.fromCharCode(155 - i.charCodeAt());
+      } else if (i.charCodeAt() < 123 && i.charCodeAt() > 96) {
+        return String.fromCharCode(219 - i.charCodeAt());
+      }
+      return i;
+    })
+    .join("");
   return answer;
 }
 
