c0db6edcf29e49f94a966b047b19777fbc0ebf19
jiho-bae <jiho@dgu.ac.kr>
2022-11-01 17:38:06 +0900

    problem2) 현재 crypto와 분석한 crypto가 같을 때까지 분석후 결과 반환

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,35 @@
+function isSame(a, b) {
+  return a === b;
+}
+
+function cryptAnalysis(str) {
+  const stack = [];
+  let cnt = 0;
+
+  for (let i = 0, len = str.length; i < len; i++) {
+    ++cnt;
+
+    if (str[i] === str[i + 1]) continue;
+    if (cnt === 1) {
+      stack.push(str[i]);
+    }
+    cnt = 0;
+  }
+
+  return stack.join('');
+}
+
 function problem2(cryptogram) {
-  var answer;
-  return answer;
+  let targetCrypto = cryptogram;
+  let isEnd = false;
+
+  while (!isEnd) {
+    solvedCrypto = cryptAnalysis(targetCrypto);
+    isEnd = isSame(targetCrypto, solvedCrypto);
+    targetCrypto = solvedCrypto;
+  }
+
+  return targetCrypto;
 }
 
 module.exports = problem2;
