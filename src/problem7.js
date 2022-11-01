2348241bee6dae98590c8c4c06570fdb6bcb29f1
SorinKwon <ksl020700@gmail.com>
2022-11-01 14:50:24 +0900

    feat(problem7):  함께 아는 친구를 탐색하는 기능

--- src/problem7.js
+++ src/problem7.js
@@ -10,6 +10,23 @@ function alreadyFriend(user, friends) {
   }
   return result;
 }
+function shareFriend(user,friends){
+  const share_friends=[];
+  const user_friends= alreadyFriend(user,friends);
+  for(let i=0;i<user_friends.length;i++){
+      for(let j=0;j<friends.length;j++){
+          if(user_friends[i]==friends[j][0]&&friends[j][1]!=user){
+              share_friends.push(friends[j][1]);
+          }
+          if(user_friends[i]==friends[j][1]&&friends[j][0]!=user){
+              share_friends.push(friends[j][0]);
+          }
+      }
+      
+  }
+  return share_friends;
+}
+
 function problem7(user, friends, visitors) {
   let answer;
   return answer;
