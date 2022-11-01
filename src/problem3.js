1ff4bad29d5ff5591b84519f17b0ace154df1cad
ming0830 <minsun0830@naver.com>
2022-11-01 17:15:01 +0900

    [PROBLEM3] refector : 변수명 변경
    박수 횟수를 저장하는 cnt를 clap으로 변경하여 직관적으로 변수명을 인식하기 위함

--- src/problem3.js
+++ src/problem3.js
@@ -1,15 +1,16 @@
 function problem3(number) {
-  let cnt = 0;
+  let clap = 0;
   for(let i =1;i<=number;i++){
     let strNum = String(i).split('');
     for(let num of strNum){
       if(num ==='3' || num === '6' || num === '9'){
-        cnt++
+        clap++
       }
     }
 
   }
-  return cnt
+  return clap
 }
+
 
 module.exports = problem3;
