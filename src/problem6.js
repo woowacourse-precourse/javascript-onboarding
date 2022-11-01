04336dfb7cbc940ff5dc1043949d4245d491b3df
rhino-ty <okko8522@gmail.com>
2022-11-01 16:21:00 +0900

    feat(problom6.js): 의사코드 추가

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,11 @@
-function problem6(forms) {
-  var answer;
-  return answer;
-}
+/*
+ * 1. 리턴할 빈 배열 선언
+ * 2. 변수 twoLetter에 닉네임 2개씩 자르기
+ * 3. form 순회하며 2개씩 자른 거랑 비교
+ *   3-1. form의 닉네임과 twoLetter와 하나씩 비교, twoLetter가 아마 이차원 배열이니 이중 반복문
+ *   3-2. filter로 true, 배열 추가
+ * 4. form에서 filter된 것을 인덱스 1로 추출한 다음 sort()
+ */
+function problem6(forms) {}
 
 module.exports = problem6;
