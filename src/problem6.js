56ab51438e0aa4fae0366f3e60107ac9fff7172f
sulmeee <poorooria@naver.com>
2022-11-01 10:24:27 +0900

    [problem6] feat: 중복여부를 확인하여 각 배열에 담기

--- src/problem6.js
+++ src/problem6.js
@@ -7,6 +7,15 @@ function problem6(forms) {
 
   // 중복되는 닉네임을 담을 배열
   let overlapNickName = [];
+
+  // 중복여부를 확인하여 각 배열에 담기
+  forms.forEach((form) => {
+    for (let i = 0; i < form[1].length; i++) {
+      if (nickName.includes(form[1].slice(i, i + 2)))
+        overlapNickName.push(form[1].slice(i, i + 2));
+      else nickName.push(form[1].slice(i, i + 2));
+    }
+  });
 }
 
 module.exports = problem6;
