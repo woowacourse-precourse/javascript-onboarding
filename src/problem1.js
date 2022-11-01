321a4730cfb17fe52f0cdcc5f82614e832d40f0e
kangju2000 <kangju2000@naver.com>
2022-11-02 01:15:06 +0900

    refactor: 문제 1 리팩토링 - problem1 함수의 return 값을 명확히 하기 위해 상수 선언 - pageScore 함수의 result -> scoreArr로 수정 - exceptionCheck 함수 -> isException 함수로 수정

--- src/problem1.js
+++ src/problem1.js
@@ -1,16 +1,21 @@
 function problem1(pobi, crong) {
-  if (!exceptionCheck(pobi) || !exceptionCheck(crong)) return -1;
+  const EXCEPTION = -1;
+  const POBI_WIN = 1;
+  const CRONG_WIN = 2;
+  const DRAW = 0;
+
+  if (isException(pobi) || isException(crong)) return EXCEPTION;
 
   let pobiScore = Math.max(pageScore(pobi[0]), pageScore(pobi[1]));
   let crongScore = Math.max(pageScore(crong[0]), pageScore(crong[1]));
 
-  if (pobiScore > crongScore) return 1;
-  else if (pobiScore < crongScore) return 2;
-  else return 0;
+  if (pobiScore > crongScore) return POBI_WIN;
+  else if (pobiScore < crongScore) return CRONG_WIN;
+  else return DRAW;
 }
 
 function pageScore(page) {
-  const result = page
+  let scoreArr = page
     .toString()
     .split('')
     .reduce(
@@ -22,13 +27,13 @@ function pageScore(page) {
       [0, 1]
     );
 
-  return Math.max(result[0], result[1]);
+  return Math.max(scoreArr[0], scoreArr[1]);
 }
 
-function exceptionCheck(pages) {
-  if (pages[0] <= 1 || pages[1] >= 400) return false;
-  else if (pages[1] - pages[0] !== 1) return false;
-  else return true;
+function isException(pages) {
+  if (pages[0] <= 1 || pages[1] >= 400) return true;
+  else if (pages[1] - pages[0] !== 1) return true;
+  else return false;
 }
 
 module.exports = problem1;
