f37309b8e5147f05e38870cd83d6c88ff13aa9ca
DAIN KIM <dnkim419@gmail.com>
2022-11-01 18:41:57 +0900

    fix: edited problem3

--- src/problem3.js
+++ src/problem3.js
@@ -2,9 +2,10 @@ function problem3(number) {
   var answer = 0;
 
   for (var i = 1; i <= number; ++i) {
-    while (i > 0) {
-      if (i % 10 == 3 || i % 10 == 6 || i % 10 == 9) answer += 1;
-      i /= 10;
+    let num = i;
+    while (num > 0) {
+      if (num % 10 == 3 || num % 10 == 6 || num % 10 == 9) answer += 1;
+      num = parseInt(num / 10);
     }
   }
 
