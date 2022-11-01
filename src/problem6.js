68e396edfbc55cb60464bc380bf62c6539c0e1e8
y00eunji <aine0227@naver.com>
2022-11-01 20:34:54 +0900

    feat: [problem6] check nickname is overlaped

--- src/problem6.js
+++ src/problem6.js
@@ -1,8 +1,34 @@
+const sliceName = (compareName, index) => {
+  const slicedNameSet = new Set();
+
+  for (let i = 0; i < compareName.length - 1; i++) {
+    slicedNameSet.add(compareName.slice(i, i + 2));
+  }
+
+  return [...slicedNameSet];
+};
+
+const isRepeatName = (name, validForms, index) => {
+  for (let i = 0; i < validForms.length; i++) {
+    if (i === index) continue;
+    const compareName = validForms[i][1];
+
+    const slicedName = sliceName(compareName, index);
+
+    for (let word of slicedName) {
+      if (name.includes(word)) return true;
+    }
+  }
+
+  return false;
+};
+
 const isValidNameFormat = (name) => {
   const nameRegExp = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{1,19}$/;
 
   return nameRegExp.test(name);
 };
+
 
 function problem6(forms) {
   var answer;
