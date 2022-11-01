9bd86299e2c0274fcb0096d68ae795471bcc8efa
Hungeun2 <bepoohun@gmail.com>
2022-11-01 21:01:50 +0900

    Feat: problem6 Add flow

--- src/problem6.js
+++ src/problem6.js
@@ -2,7 +2,7 @@
 // 2. 크루원들의 닉네임들 중에 곂치는 두글자 리스트화
 // 3. 멤버 객체를 순회하며 중복 닉네임이 존재 하는지 확인하고
 // 존재하면 answer 배열에 추가 check 를 true로 변경 (answer에 추가되면 다시 확인하지 않기 위해)
-
+// 4. answer sorting
 function problem6(forms) {
   var answer;
   return answer;
