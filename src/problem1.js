a9f703194ca02197e8be1d89a6557e244348f4ea
Hanav52 <wnsrl8329@gmail.com>
2022-11-01 16:42:54 +0900

    유효페이지 검증 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,14 @@
-function problem1(pobi, crong) {
-  var answer;
-  return answer;
+function isValidPage(page) {
+  if (page[0] === 1 || page[1] === 400) return false;
+  else return true;
 }
 
+function problem1(pobi, crong) {
+  var answer;
+  let answer;
+
+  if (!isValidPage(pobi) || !isValidPage(crong)) return -1;
+
+  return answer;
+}
 module.exports = problem1;
