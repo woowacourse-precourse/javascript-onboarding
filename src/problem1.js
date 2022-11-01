c7bbe1594ba84f7333505ff3d48e1fbe1a7b22fe
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:12:41 +0900

    feat(problem1): 각 자리수의 곱과 합을 구한다

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,15 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  let pobiLeftArr = (pobi[0]+'').split('');
+  let pobiRightArr = (pobi[1]+'').split('');
+  let crongLeftArr = (crong[0]+'').split('');
+  let crongRightArr = (crong[1]+'').split('');
+
+  function sum(arr){
+    return arr.reduce((a, b) => Number(a) + Number(b), 0); 
+  }
+  function multiple(arr){
+    return arr.reduce((a, b) => Number(a) * Number(b), 1);  
+  }
 }
 
 module.exports = problem1;
