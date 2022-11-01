46d2b1d474e6bc00d15268c556f2d6cb2e2f13df
imhynni <omelas329@gmail.com>
2022-11-01 14:00:13 +0900

    refactor(problem2): 변수 이름 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,12 @@
 function problem2(cryptogram) {
   let answer;
 
-  const arr = cryptogram.split("");
-  const stack = [arr[0]];
+  const cryptogramArr = cryptogram.split("");
+  const stack = [cryptogramArr[0]];
 
   for (let i = 1; i < cryptogram.length; i++) {
-    if (stack[stack.length - 1] === arr[i]) stack.pop();
-    else stack.push(arr[i]);
+    if (stack[stack.length - 1] === cryptogramArr[i]) stack.pop();
+    else stack.push(cryptogramArr[i]);
   }
 
   answer = stack.join("");
