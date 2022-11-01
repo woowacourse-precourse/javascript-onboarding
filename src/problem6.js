4cfbe9dfba237d888773987bfca71daecb74bc0a
윤동주 <yoondj98@naver.com>
2022-11-01 20:47:54 +0900

    우아한 테크코스 프리코슥 1주차 6번 기능구현 완료

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,23 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  var temp = new Set([]);
+  var warning_word = new Set([]);
+  var answer = [];
+  for (var i = 0; i < forms.length; i++) {
+    for (var j = 0; j < forms[i][1].length - 1; j++) {
+      if (temp.has(forms[i][1].substring(j, j + 2))) {
+        warning_word.add(forms[i][1].substring(j, j + 2));
+      } else temp.add(forms[i][1].substring(j, j + 2));
+    }
+  }
+  for (var i = 0; i < forms.length; i++) {
+    for (var j = 0; j < forms[i][1].length - 1; j++) {
+      if (warning_word.has(forms[i][1].substring(j, j + 2))) {
+        answer.push(forms[i][0]);
+      }
+    }
+  }
+
+  return answer.sort();
 }
 
 module.exports = problem6;
