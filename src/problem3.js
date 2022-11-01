338024f20cdb56e365f0001acbfde6c1e602eafe
AhnGeunTaek <dksrmsxor@gmail.com>
2022-11-01 11:27:26 +0900

    Feat: problem3 - 생성한 문자열을 인자로 받아 타켓 넘버를 찾는 함수 생성

--- src/problem3.js
+++ src/problem3.js
@@ -2,8 +2,15 @@ function problem3(number) {
   let answer = 0;
   const targetNumbers = ["3", "6", "9"];
 
+  const chenckNum = (strNum) => {
+    for (let i = 0; index < strNum.length; i++) {
+      if (targetNumbers.includes(strNum[i])) answer++;
+    }
+  };
+
   for (let num = 1; num <= number; num++) {
     const strNum = num.toString();
+    chenckNum(strNum);
   }
 }
 module.exports = problem3;
