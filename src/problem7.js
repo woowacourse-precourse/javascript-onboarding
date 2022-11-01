0e5675d2a80204d6123d1c7c92f655d4b21bdc56
Haram Baek <haram@Haramui-MacBookAir.local>
2022-11-01 22:09:40 +0900

    feat: [PROBLEM7] 사용자가 아는 친구를 찾아서 배열로 만든다

--- src/problem7.js
+++ src/problem7.js
@@ -1,14 +1,20 @@
 function problem7(user, friends, visitors) {
   var answer;
   let scoreObj = {};
+  let knowUser = [];
 
   for (const [a, b] of friends) {
     scoreObj[a] = 0;
     scoreObj[b] = 0;
+    if(a === user){
+      knowUser.push(b);
+    } else if(b === user){
+      knowUser.push(a);
+    }
   }
 
-  console.log(scoreObj);
-  
+  console.log(knowUser);
+
   return answer;
 }
 
