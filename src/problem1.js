ad5dd707d1d5e907b4c9ac2ce518d7b444b47ab0
allzeroyou <ekdudd119@naver.com>
2022-11-01 17:15:28 +0900

    [feat] 각 페이지 덧셈과 곱셈한 값 중 큰 수 찾기 기능 구현

--- src/problem1.js
+++ src/problem1.js
@@ -17,7 +17,39 @@ function problem1(pobi, crong) {
   if (isRestrict) {
     return -1;
   } else {
+    function checkBigger(num) {
+      let sum = 0;
+      let numStr = String(num);
+      let mul = 1;
+      for (let i = 0; i < numStr.length; i++) {
+        sum += Number(numStr[i]);
+        mul *= Number(numStr[i]);
+      }
+      return sum, mul;
+    }
+
+    function getBiggerNum(arr) {
+      let bigNum = [];
+      bigNum.push(checkBigger(arr[0]));
+      bigNum.push(checkBigger(arr[1]));
+      bigNum.sort((a, b) => b - a);
+      console.log(bigNum);
+      return bigNum[0];
+    }
+    let answer;
+
+    const [pobiPage, crongPage] = [getBiggerNum(pobi), getBiggerNum(crong)];
+
+    if (pobiPage == crongPage) {
+      answer = 0;
+    } else if (pobiPage > crongPage) {
+      answer = 1;
+    } else if (pobiPage < crongPage) {
+      answer = 2;
+    } else {
+      answer = -1;
+    }
+    return answer;
   }
 }
-
 module.exports = problem1;
