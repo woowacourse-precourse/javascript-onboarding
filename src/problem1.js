4a04f71759aa3d6cc9423d5b7f6293c4eebd0424
hvvany <kjunhwan1998@gmail.com>
2022-11-01 20:40:59 +0900

    feat(build): solve problem_1

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,24 @@
 function problem1(pobi, crong) {
   let answer = 0;
+  if (pobi[0] + 1 === pobi[1]) {
+    if (crong[0] + 1 === crong[1]) {
+      let answer_pobi = 0;
+      answer_pobi = big_number(pobi);
+      let answer_crong = 0;
+      answer_crong = big_number(crong);
+      if (answer_pobi > answer_crong) {
+          answer = 1;
+      }
+      else if (answer_pobi < answer_crong) {
+          answer = 2;
+      }
+      else {
+          answer = 0;
+      }
+      return answer;
+    }
+  }
+  answer = -1;
   return answer;
 }
 
