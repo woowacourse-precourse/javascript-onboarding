0641a7305245ae63fea8ab0cc15873b7bfab08e7
Steadystudy <sgmin206@naver.com>
2022-11-01 22:03:12 +0900

    [problem5] Refactor: 재귀함수에서 반복문으로 문제풀이 변경

--- src/problem5.js
+++ src/problem5.js
@@ -1,46 +1,15 @@
 function problem5(money) {
-  const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
+  const currency = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const answer = Array.from({length: currency.length}, () => 0);
+  let leftMoney = money;
+  let i = 0;
 
-  function recursion(change) {
-    if(change >= 50000) {
-      answer[0] += 1;
-      return recursion(change-50000)
-    }
-    if(change >= 10000) {
-      answer[1] += 1;
-      return recursion(change-10000)
-    }
-    if(change >= 5000) {
-      answer[2] += 1;
-      return recursion(change-5000)
-    }
-    if(change >= 1000) {
-      answer[3] += 1;
-      return recursion(change-1000)
-    }
-    if(change >= 500) {
-      answer[4] = answer[4] + 1;
-      return recursion(change-500)
-    }
-    if(change >= 100) {
-      answer[5] = answer[5] + 1;
-      return recursion(change-100)
-    }
-    if(change >= 50) {
-      answer[6] = answer[6] + 1;
-      return recursion(change-50)
-    }
-    if(change >= 10) {
-      answer[7] = answer[7] + 1;
-      return recursion(change-10)
-    }
-    if(change >= 1) {
-      answer[8] = answer[8] + 1;
-      return recursion(change-1)
-    }
+  while(leftMoney !== 0) {
+    const numberOfBill = Math.floor(leftMoney / currency[i]);
+    leftMoney -= currency[i] * numberOfBill;
+    answer[i] = numberOfBill;
+    i++;
   }
-
-  recursion(money);
   
   return answer;
 }
