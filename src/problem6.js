ff1754f3028dab97166db3edfb67b073c5ad6cd5
HyeJun <solar3070@naver.com>
2022-11-01 20:14:05 +0900

    feat: problem6/두 글자로 자른 닉네임과 이메일을 키-값으로 저장

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,12 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  const wordMap = new Map();
+  forms.forEach(([email, nickname]) => {
+    for (let i = 0; i < nickname.length - 1; i++) {
+      const word = nickname.slice(i, i + 2);
+      const emailList = wordMap.get(word);
+      wordMap.set(word, emailList ? [...emailList, email] : [email]);
+    }
+  });
 }
 
 module.exports = problem6;
