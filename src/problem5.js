d096899e02f59bd27ae409fcb93152d11bda2482
banma70 <banma1234@gmail.com>
2022-11-01 15:06:59 +0900

    problem_5 : 예외처리 함수

--- src/problem5.js
+++ src/problem5.js
@@ -3,4 +3,11 @@ function problem5(money) {
   return answer;
 }
 
+function errorHandler(num) {
+  // 금액이 1,000,000보다 클 경우
+  if (num > 1000000)  return true;
+
+  return false;
+}
+
 module.exports = problem5;
