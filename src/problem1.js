edfa0d862326f34671fd3d7ce8df2c6df3b61c39
Hayeong Kim <fibonacci2246@gmail.com>
2022-11-01 18:42:05 +0900

    problem 1 solved

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,40 @@
 function problem1(pobi, crong) {
-  var answer;
+  let answer = 0;
+
+  if (pobi[1] - pobi[0] !== 1 || crong[1] - crong[0] !== 1) {
+    return -1;
+  }
+
+  const [pobiScore, crongScore] = [pobi, crong].map((player) => {
+    const [first, second] = player;
+
+    const firstScore = getScore(first);
+    const secondScore = getScore(second);
+
+    return Math.max(firstScore, secondScore);
+  });
+
+  if (pobiScore > crongScore) {
+    answer = 1;
+  } else if (pobiScore < crongScore) {
+    answer = 2;
+  }
+
   return answer;
+}
+
+function getScore(num) {
+  const addedScore = num
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc + Number(cur), 0);
+
+  const multipiedScore = num
+    .toString()
+    .split("")
+    .reduce((acc, cur) => acc * Number(cur), 1);
+
+  return Math.max(addedScore, multipiedScore);
 }
 
 module.exports = problem1;
