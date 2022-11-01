95f819a3b63674c4ce31ba2fdafe81f3e0db1917
sumi-0011 <selina2000@naver.com>
2022-11-01 16:31:20 +0900

    [problem5] refactoring : JSDoc 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,9 +1,19 @@
 const BILL = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
 
+/**
+ * @param {number} unit 지폐 or 동전
+ * @param {number} money
+ * @returns {number} 변환 가능 횟수
+ */
 const getMaxUnitCnt = (unit, money) => {
   return parseInt(money / unit);
 };
 
+/**
+ * @param {number} unit 지폐 or 동전
+ * @param {number} money
+ * @returns {number} 변환 루 남은 돈
+ */
 const getUnitRest = (unit, money) => {
   return money % unit;
 };
