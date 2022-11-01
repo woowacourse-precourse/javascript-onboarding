a74f7219cfc6b3808820b5dc852b84042813f21e
KINHYEONJI <hyeonji0718@gmail.com>
2022-11-01 11:43:49 +0900

    feat : findPossibleNameArr 함수 구현
    - 닉네임에서 나타낼 수 있는 연속된 두 글자 단어들을 반환

--- src/problem6.js
+++ src/problem6.js
@@ -2,5 +2,13 @@ function problem6(forms) {
   var answer;
   return answer;
 }
+function findPossibleNameArr(nickName) {
+  let duplicatePossibleNameArr = [];
+  for (let i = 0; i < nickName.length - 1; i++) {
+    duplicatePossibleNameArr.push(nickName.substr(i, 2));
+  }
+  return duplicatePossibleNameArr;
+}
+
 
 module.exports = problem6;
