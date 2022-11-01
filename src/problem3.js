30cf1de949d147e7351ec7bf47c22d68999810bb
Dahye <06robin11@gmail.com>
2022-11-01 16:25:21 +0900

    feat: get the number of clap every 3,6,9 using regular expression

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,12 @@
 function problem3(number) {
-  var answer;
+  const clapNums = /3|6|9/g;
+  let answer = 0;
+  for (let i = 1; i <= number; i++) {
+    const exist = String(i).match(clapNums);
+    if (exist) {
+      answer += exist.length;
+    }
+  }
   return answer;
 }
 
