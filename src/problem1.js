a32d978b2ac9c3fd4874992c1ea38b78b5ee10f0
Lim_JiSeon <dpf226@naver.com>
2022-11-01 22:03:08 +0900

    problem1 - function 예외처리 구현

--- src/problem1.js
+++ src/problem1.js
@@ -7,6 +7,23 @@
 5. 가장 큰 수를 찾는 기능
 */
 
+const LEFT_PAGE = 0;
+const RIGHT_PAGE = 1;
+const START_PAGE = 1; 
+const FINISH_PAGE = 400;
+
+function exception(lst) {
+  if (lst[RIGHT_PAGE] - lst[LEFT_PAGE] != 1)
+    return -1;
+  
+  if (lst[LEFT_PAGE]%2 != 1 || lst[RIGHT_PAGE]%2 != 0)
+    return -1;
+  
+  if (lst[LEFT_PAGE] <= START_PAGE || lst[RIGHT_PAGE] >= FINISH_PAGE)
+    return -1;
+}
+
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
