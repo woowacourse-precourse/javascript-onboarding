ddf4219cd2737fb9d743f08ddf5bea6b31171a64
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-01 19:13:17 +0900

    feature(problem6): getOverlapEmail 함수 구현

--- src/problem6.js
+++ src/problem6.js
@@ -11,6 +11,26 @@ function problem6(forms) {
     });
   };
 
+  const getOverlapEmail = (nickname_list, email_list) => {
+    const slice_keyword = {};
+    const overlap_email = new Set();
+
+    for (let i = 0; i < nickname_list.length; i++) {
+      const current_nickname = nickname_list[i];
+      // 정해진 닉네임 두글자씩 slice해서 검사
+      for (let j = 0; j < current_nickname.length - 1; j++) {
+        const slice_nickname = current_nickname.substr(j, 2);
+        if (!slice_keyword[slice_nickname]) {
+          slice_keyword[slice_nickname] = email_list[i];
+        } else if (!(slice_keyword[slice_nickname] === email_list[i])) {
+          overlap_email.add(slice_keyword[slice_nickname]);
+          overlap_email.add(email_list[i]);
+        }
+      }
+    }
+    return Array.from(overlap_email).sort();
+  };
+
   let answer;
   return answer;
 }
