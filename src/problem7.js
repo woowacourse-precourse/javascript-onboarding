a9e602196d7331bd8928dbbd8141281208bb3105
sumin <csumin008255@gmail.com>
2022-11-01 20:13:10 +0900

    problem7_기능목록작성

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,17 @@
+/* 기능 목록
+* 1. friends 요소 중 user를 포함하고 있는 요소는 userFriends에 추가, friends에서 삭제
+* 2. visitors 요소 중 맵(scoreMap)에 없으면 추가 후 1점, 있으면 +1점
+* 3. firends에 userFriends 요소가 없으면 추가 후 10점, 있으면 +10점
+* 4. scoreMap에서 userFriends 요소가 있으면 삭제
+* 5. scoreMap 점수를 mapVal에 저장 후 내림차순 정렬
+* 6. mapVal 길이가 5보다 길면 자르기
+* 7. scoreMap을 이름 순으로 정렬
+* 8. mapVal 점수에 해당하는 사람을 result에 추가
+* 9. 결과출력
+* 10. 코드 정리
+*/
+
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
