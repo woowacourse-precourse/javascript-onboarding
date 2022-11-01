4611719f3f1deee2a4cd1f5f2b5fb932780addab
MJ <mj632900@gmail.com>
2022-11-01 23:07:23 +0900

    refactor : 변수명 변경

--- src/problem7.js
+++ src/problem7.js
@@ -2,9 +2,9 @@ function getUserFriend(user, friendList) {
     return friendList
         .filter((item) => item.includes(user))
         .map(([user1, user2]) => (user1 === user ? user2 : user1))
-        .reduce((acc, cur) => {
-            acc[cur] = [];
-            return acc;
+        .reduce((obj, friend) => {
+            obj[friend] = [];
+            return obj;
         }, {});
 }
 
