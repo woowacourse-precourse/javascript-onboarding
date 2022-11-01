65a1fbe6f66877d931ccc38b9388fe4a9557e125
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-01 17:00:53 +0900

    feat: 문제 7 - user의 친구의 친구를 찾아서 10점 더하기

--- src/problem7.js
+++ src/problem7.js
@@ -15,8 +15,41 @@ function problem7(user, friends, visitors) {
     return friendsList;
   }
 
+  function recommendFriends(user, friends, friend) {
+    let score = [];
+
+    // user의 친구의 친구를 찾아 10점를 더한다.
+    for (let i = 0; i < friends.length; i++) {
+      let ff = friends[i];
+      for (let j = 0; j < friend.length; j++) {
+        // user의 친구의 친구가 user, user의 친구가 아닌 경우만 처리
+        if (!ff.includes(user) && ff.includes(friend[j])) {
+          let uff = ff.filter(function (f) {
+            return f !== friend[j];
+          });
+          if (uff in score) {
+            score[uff] += SCORE_UFF;
+          } else {
+            score[uff] = SCORE_UFF;
+          }
+        }
+      }
+    }
+
+    return score;
+  }
+
+  let result = [];
+  let recommend = [];
+  const MAX_RESULT = 5;
+  const SCORE_UFF = 10;
+  const SCORE_VISITOR = 1;
   let friend = findUserFriends(user, friends);
-  console.log(friend);
+  let score = recommendFriends(user, friends, friend);
+
+  console.log(score);
+
+  return result;
 }
 
 module.exports = problem7;
