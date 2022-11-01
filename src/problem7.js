56623264e6271fcb6d5814b8dd30f44531c7504e
daal2quf <daal2quf@gmail.com>
2022-11-01 19:21:02 +0900

    유저 친구들 구하기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,40 @@
 function problem7(user, friends, visitors) {
   var answer;
+  let userFriends = [];
+  let newUserFriends = [];
+  let userFriendMap = new Map()
+
+  //유저친구들구하기
+
+  for (let friend of friends) {
+    if (friend.includes(user)) {
+      if (friend[0] === user) {
+        userFriends.push(friend[1])
+      } else {
+        userFriends.push(friend[0])
+      }
+    } else {
+      userFriendMap.set(friend[0], 0);
+      userFriendMap.set(friend[1], 0);
+    }
+  }
+
   return answer;
 }
+
+
+
+let user = "mrko";
+let friends = [
+  ["donut", "andole"],
+  ["donut", "jun"],
+  ["donut", "mrko"],
+  ["shakevan", "andole"],
+  ["shakevan", "jun"],
+  ["shakevan", "mrko"]
+]
+let visitors = ["bedi", "bedi", "donut", "bedi", "shakevan"]
+
+problem7(user, friends, visitors)
 
 module.exports = problem7;
