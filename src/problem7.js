a32428cbe7dc9ce60ffa89a93cafafe49f7224d7
Yoonkyoung Kim <100656920+Yoonkyoungme@users.noreply.github.com>
2022-11-01 16:57:36 +0900

    feat: 문제 7 - user의 친구 찾기

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,22 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  // user의 친구 찾기
+  function findUserFriends(user, friends) {
+    let friendsList = [];
+    for (let friend of friends) {
+      if (friend.includes(user)) {
+        if (friend[0] !== user) {
+          friendsList.push(friend[0]);
+        }
+        if (friend[1] !== user) {
+          friendsList.push(friend[1]);
+        }
+      }
+    }
+    return friendsList;
+  }
+
+  let friend = findUserFriends(user, friends);
+  console.log(friend);
 }
 
 module.exports = problem7;
