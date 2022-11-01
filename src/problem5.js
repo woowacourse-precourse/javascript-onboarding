15f75a327bd59d63783949a3a7aa8a00aaa51c04
Bori-github <qhflrnfl4324@gmail.com>
2022-11-01 22:22:13 +0900

    ♻️ problem5 > money가 1 이상 1,000,000 이하인 자연수가 아닌 경우 리턴값 수정

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,8 @@
 function problem5(money) {
-  if (!Number.isInteger(money) || money < 1 || money > 1000000) return;
+  // money가 1 이상 1,000,000 이하인 자연수가 아닌 경우
+  if (!Number.isInteger(money) || money < 1 || money > 1000000) {
+    return false;
+  }
 
   const answer = [0, 0, 0, 0, 0, 0, 0, 0, 0];
   const divideArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
