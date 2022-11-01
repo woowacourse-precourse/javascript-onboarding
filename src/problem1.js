9691142130af83a72e9850e06215d678d02b173f
hyozzi92 <gytjdzja@gmail.com>
2022-11-01 22:50:07 +0900

    problem1 해결!

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,72 @@
 function problem1(pobi, crong) {
-  var answer;
+  const verse = () => {
+    const toPobiValue = (pobi) => {
+      if (pobi[1] - pobi[0] !== 1) {
+        return null;
+      } else {
+        const firstNum = pobi[1];
+        const firstStr = firstNum.toString().split("");
+
+        let sum = 0;
+
+        for (let i = 0; i < firstStr.length; i++) {
+          sum = sum + parseInt(firstStr[i]);
+        }
+
+        let multiply = 1;
+
+        for (let j = 0; j < firstStr.length; j++) {
+          multiply = multiply * parseInt(firstStr[j]);
+        }
+
+        if (sum > multiply) {
+          return sum;
+        } else {
+          return multiply;
+        }
+      }
+    };
+
+    const toCrongValue = (crong) => {
+      if (crong[1] - crong[0] !== 1) {
+        return null;
+      } else {
+        const firstNum = crong[1];
+        const firstStr = firstNum.toString().split("");
+
+        let sum = 0;
+
+        for (let i = 0; i < firstStr.length; i++) {
+          sum = sum + parseInt(firstStr[i]);
+        }
+
+        let multiply = 1;
+
+        for (let j = 0; j < firstStr.length; j++) {
+          multiply = multiply * parseInt(firstStr[j]);
+        }
+        if (sum > multiply) {
+          return sum;
+        } else {
+          return multiply;
+        }
+      }
+    };
+
+    let a = toPobiValue(pobi);
+    let b = toCrongValue(crong);
+
+    if ((a == null) | (b == null)) {
+      return -1;
+    } else if (a < b) {
+      return 2;
+    } else if (a > b) {
+      return 1;
+    } else if ((a = b)) {
+      return 0;
+    }
+  };
+  var answer = verse();
   return answer;
 }
 
--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,34 @@
 function problem6(forms) {
   var answer;
+  var wordCombination = (ele) => {
+    var arr = ele.split("");
+
+    sum2 = [];
+    for (let k = 0; k <= arr.length - 1; k++) {
+      sum2.push(arr[k] + "" + arr[k + 1]);
+    }
+    return sum2.filter(function (data) {
+      return data.length < 3 && data.length > 1;
+    });
+  };
+
+  sum3 = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    for (let j = 0; j < forms.length; j++) {
+      var intersection = wordCombination(forms[i][1]).filter(
+        (x) => wordCombination(forms[j][1]).includes(x) //&&
+        ///wordCombination(forms[j][1])
+      );
+
+      if (i != j && intersection.length !== 0) {
+        sum3.push(forms[j][0]);
+      }
+    }
+  }
+  const set = new Set(sum3);
+  answer = [...set].sort();
+
   return answer;
 }
 
