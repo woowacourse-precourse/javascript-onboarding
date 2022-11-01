53b2f7597fb2db2b7899b18240561101ec6d5ae7
HaroldBKim <sbookim2@gmail.com>
2022-11-01 23:22:25 +0900

    solved problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  let typeOfMoney = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+
+  answer[0] = Math.floor(money / 50000);
+  for (let i = 1; i < 9; i++) {
+    answer[i] = Math.floor((money % typeOfMoney[i - 1]) / typeOfMoney[i]);
+  }
   return answer;
 }
 
