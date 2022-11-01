69dc23f415a1d3d958e11402852023035dedaf0f
chaehyunyun <79246447+yunchaehyun@users.noreply.github.com>
2022-11-01 17:30:33 +0900

    refactor: [problem2] Extract Function

--- src/problem2.js
+++ src/problem2.js
@@ -1,21 +1,32 @@
 function problem2(cryptogram) {
   var answer;
+
   const text = cryptogram.trim().split("");
 
-  for (let i = 0; i < text.length; ) {
-     // 암호문의 연속된 두자리가 같다면, 두 문장 모두 삭제
-  if (text[i] == text[i + 1]) {
-  text.splice(i, 1);
-  text.splice(i, 1);
-  // 처음부터 다시 검사하기 위해 변수 초기화
-  i = 0;
-  } else {
-    i++;
-  }
-  }
-  console.log(`result = ${text}`);
-  answer = text;
+  // 함수형으로 refactoring하여 재사용이 가능하도록 하고, 코드의 반복을 방지한다.
+  answer = convertText(text);
+
+  console.log(answer);
   return answer;
 }
 
+function convertText(cryptogram) {
+  let answer = "";
+
+  for (let i = 0; i < cryptogram.length; ) {
+    // 암호문의 연속된 두자리가 같다면, 두 문장 모두 삭제
+    if (cryptogram[i] == cryptogram[i + 1]) {
+      cryptogram.splice(i, 1);
+      cryptogram.splice(i, 1);
+      // i를 0으로 초기화 후 다시 검사
+      i = 0;
+    } else {
+      i++;
+    }
+  }
+
+  cryptogram.forEach((i) => (answer += i));
+
+  return answer;
+}
 module.exports = problem2;
