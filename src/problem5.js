b33fa34b6285e5ff230dc9df9e818933c3a5fc96
leeharyung <lhr4884@naver.com>
2022-11-01 13:11:46 +0900

    add problem5 solution

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,11 @@
 function problem5(money) {
-  var answer;
+  let moneyArr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  let answer = Array.from({ length: moneyArr.length }, () => 0);
+  moneyArr.map((i, index) => {
+    answer[index] = Math.floor(money / i);
+    money = money % i;
+  });
+
   return answer;
 }
 
