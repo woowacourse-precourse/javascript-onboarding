bda4d99f4b574ddda6df34da55bcb5fce85167da
Hungeun2 <bepoohun@gmail.com>
2022-11-01 23:15:13 +0900

    Feat: problem7 flow

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,10 @@
+// 1. sns 회원 리스트화
+// 2. 친구지도 (obj) 생성
+// 3. 방문자 point 계산
+// 4. 서로 아는 친구 Point 계산
+// 5. 이미 user 와 친구인 사람 map에서 제거
+// 6. sort및 랭킹 생성
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
