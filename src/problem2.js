2370384461b709f1b898f23b72a949985c960ee5
yoon052 <ynj3126@hanmail.net>
2022-11-01 12:04:09 +0900

    feat: 2번 문제 해결
    [1주차] 2번 문제 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,18 +1,30 @@
 function problem2(cryptogram) {
-  var answer =[];
-  const arr = Array.isArray(cryptogram);
-
-  answer += arr[0];
-
-  for(let i=1; i<arr.length; i++){
-    if(arr[i-1] !== arr[i]){
-      answer += arr[i];
-    }
+  var result = {
+    cryptogram : cryptogram,
+    isDecrypt : false
   }
 
-  return answer;
+  while (!result.isDecrypt) {
+    result = removeCon(result);
+  }
+
+  return result.cryptogram;
 }
 
-problem2();
+function removeCon(result) {
+  var idx = 1
+  scope = result.cryptogram.length;
+  cryptogram = result.cryptogram;
+
+  while (idx <= scope) {
+    if (cryptogram.charAt(idx) == cryptogram.charAt(idx-1)) {
+      result.cryptogram = result.cryptogram.substr(0, idx-1) + result.cryptogram.substr(idx+1, scope);
+      return result;
+    } 
+    idx += 1;
+  }
+  result.isDecrypt = true;
+  return result;
+}
 
 module.exports = problem2;
