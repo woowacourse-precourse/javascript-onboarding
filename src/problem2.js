576f7ebf81d706449c5f16a34307a393b781829f
yeong bin <2976622@naver.com>
2022-11-01 15:45:19 +0900

    fix: [problem2] 알고리즘 변경

--- src/problem2.js
+++ src/problem2.js
@@ -1,17 +1,22 @@
 /**
+ * 중복된 문자들은 삭제된 문자열 반환
+ * @param {string} str
+ * @returns {string} 중복 제거 문자열
+ */
+function getRemovedRepeatChar(str) {
+  return str.replace(/(([a-z])\2{1,})/g, '');
+}
+
+/**
  * Returns the decrypt string
  * @param {string} cryptogram
  * @returns {string}
  */
  function problem2(cryptogram) {
-  return cryptogram
-    .split("")
-    .reverse()
-    .reduce(
-      (acc, cur) =>
-        acc.includes(cur) ? acc.filter((c) => c !== cur) : [cur, ...acc],
-      []
-    )
-    .join("");
+  const tempResult = getRemovedRepeatChar(cryptogram);
+  if (cryptogram.length === tempResult.length)
+    return tempResult;
+  return problem2(tempResult);
 }
+
 module.exports = problem2;
