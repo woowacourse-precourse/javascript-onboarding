f2d421c4be4f749e1290e9ae052bb07bd143ba7c
KPGY <yuchw8@korea.ac.kr>
2022-11-01 20:04:56 +0900

    아스키코드를 이용한 4번문제 해결

--- src/problem4.js
+++ src/problem4.js
@@ -1,22 +1,22 @@
 function problem4(word) {
-  var answer;
+let ascii="";
+let answer = "";
+for(let i=0; i<word.length; i++)//문자열 길이만큼 순회
+{
+  ascii = word.charCodeAt(i); //문자열 값을 아스키코드로 치환
+  if(65<=ascii && ascii<=90) //소문자일 경우
+  {
+    ascii = 155-ascii
+  }
+  else if(97<=ascii && ascii<=122) //대문자일 경우
+  {
+    ascii = 219-ascii
+  }
+  answer += String.fromCharCode(ascii); //다시 문자열로 변환후 정답으로 입력
+}
   return answer;
 }
 
-let word ="I LOVE YOU"
-let alpa ="ABCDEFGHIJKLMNOPQRSATUVWXYZ";
-let alpaArr= Array.from(alpa);
-let beta = Array.from(alpa).reverse();
-let wordArr = Array.from(word);
-let ans =[];
-let answer =[];
-for(i=0; i<=wordArr.length; i++)
-{
-  ans.push(alpaArr.indexOf(wordArr[i]))
-}
-console.log(ans)
-for(i=0; i<=wordArr.length; i++)
-{
-  answer.push(alpaArr.find(alpaArr.indexOf ===3))
-}
+
 module.exports = problem4;
+
