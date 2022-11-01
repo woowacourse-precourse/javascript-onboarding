69532ff204b644358252492b331aed8511466af0
HaroldBKim <sbookim2@gmail.com>
2022-11-01 18:22:32 +0900

    solved problem2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,23 @@
 function problem2(cryptogram) {
   var answer;
+  const spell = cryptogram.split("");
+  
+  let i = 0;
+  
+  while (i < spell.length) {
+  if (spell[i] === spell[i + 1] && spell[i] === spell[i + 2]) {
+  spell.splice(i, 3);
+  i = -1; 
+  }
+  else if (spell[i] === spell[i + 1]) {
+  spell.splice(i, 2);
+  i = -1;
+  }
+  i++;
+  }
+  
+  answer = spell.join("");
   return answer;
-}
-
-module.exports = problem2;
+  }
+  
+  module.exports = problem2;
