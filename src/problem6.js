e7f2e530c4387ec0c73d17f33ae5992dcc4da17e
juicyorange <rlaxodud9809@khu.ac.kr>
2022-11-01 22:51:37 +0900

    feat: [PROBLEM6] 연속된 2개의 문자를 가져오는 함수 구현(feat1)

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,19 @@
 function problem6(forms) {
   var answer;
+
   return answer;
 }
+
+const getSubStringList = (name) => {
+  const result = [];
+  name.split('').map((value, idx) => {
+    if (idx < name.length - 1) {
+      result.push(name[idx] + name[idx + 1]);
+    }
+  });
+  return result;
+};
+
+console.log(getSubStringList('asd'));
 
 module.exports = problem6;
