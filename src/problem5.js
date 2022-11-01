4baef2ed059cee25577906823730b800cc73e5fa
SorinKwon <ksl020700@gmail.com>
2022-11-01 22:00:39 +0900

    refactor(problem5): 변수명 수정, 함수명 수정, 코드 가독성 개선

--- src/problem5.js
+++ src/problem5.js
@@ -1,17 +1,14 @@
-function transformMoney(n){
+function transformMoney(money){
   let money_unit = [50000,10000,5000,1000,500,100,50,10,1];
-  let result=[];
-
+  let trans_money=[];
   for(let i=0;i<money_unit.length;i++){
-      result[i]=parseInt((n/money_unit[i]));
-      n%=money_unit[i];
-  }
-  return result;
+      trans_money[i]=parseInt((money/money_unit[i]));
+      money%=money_unit[i];
+  }return trans_money;
 }
 function problem5(money) {
   let answer;
   answer=transformMoney(money);
   return answer;
-
 }
 module.exports = problem5;
