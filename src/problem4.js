662f5602c889889103564acce510042b09ac19d5
Josanghyeon <BStkdgus.dev@gmail.com>
2022-11-01 19:13:34 +0900

    feat: problem4

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  let ans = '';
+  let ascii;
+  for (let i = 0; i < word.length; i += 1) {
+    if (word[i] === ' ') ans += word[i];
+    else {
+      ascii = word[i].charCodeAt(0);
+      ans += String.fromCharCode(ascii >= 65 && ascii <= 90 ? 155 - ascii : 219 - ascii);
+    }
+  }
+  return ans;
 }
 
 module.exports = problem4;
