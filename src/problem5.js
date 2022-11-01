d2c8adb99b23d2cdcbe104024375f70d0870682a
Ryan-Dia <cjfdnjs158@naver.com>
2022-11-01 13:10:02 +0900

    feat:[문제5] 화폐(동전)으로 교환가능한지 판별해주는 exchangeCash함수 추가

--- src/problem5.js
+++ src/problem5.js
@@ -1,7 +1,17 @@
+function exchangeCash(money, cashInBank, safe) {
+  const EXCHANGE_CRITERION = 1;
+  if (money / cashInBank >= EXCHANGE_CRITERION) {
+    return;
+  }
+  if (money / cashInBank < EXCHANGE_CRITERION) return;
+}
+
 function problem5(money) {
   let safe = { moneyExchanged: [], change: money };
   const bank = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-  bank.forEach((cashInBank) => {});
+  bank.forEach((cashInBank) => {
+    exchangeCash(money, cashInBank, safe);
+  });
   return safe.moneyExchanged;
 }
 
