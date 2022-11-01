3f1bd589d2e4279cf1bd357d5c4cc57615787b56
DESKTOP-8B9G08E\Cho young jae <rnlwlswk@naver.com>
2022-11-01 23:51:02 +0900

    우아한테크코스 문제 5

--- src/problem5.js
+++ src/problem5.js
@@ -1,5 +1,16 @@
 function problem5(money) {
-  var answer;
+  var answer = [];
+  var m = money;
+  var x = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  for (var i = 0; i <= x.length -1; i++) {
+    if (Math.floor(m / Number(x[i])) !== 0) {
+      answer.push(Math.floor(m / Number(x[i])));
+      m = m % Number(x[i]);
+    }else {
+      answer.push(0);
+    }
+  }
+  console.log(answer);
   return answer;
 }
 
