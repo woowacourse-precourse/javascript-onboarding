6401cce17e4675bfa6cdeae44f2123ebdb2851e0
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 22:12:22 +0900

    feat(problem6): 중복된 닉네임을 가진 지원자를 찾는다

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,16 @@
 function problem6(forms) {
-  var answer;
-  return answer;
+  let wordList = [];
+  let answerList = [];
+
+  for (let i=0; i < forms.length; i++) {
+    for (let j=0; j < forms[i][1].length-1; j++) {
+      if (wordList.includes(forms[i][1].substring(j, j+2))) {
+        answerList.push(forms[i][0])
+        answerList.push(forms[j][0])
+      }
+      wordList.push(forms[i][1].substring(j, j+2))
+    }
+  }
 }
 
 module.exports = problem6;
