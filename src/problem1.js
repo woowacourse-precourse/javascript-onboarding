3020a637af128527a50ca6a86d3a5875490bcf2a
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:16:06 +0900

    feat(problem1): 자신의 점수를 설정한다

--- src/problem1.js
+++ src/problem1.js
@@ -10,11 +10,20 @@ function problem1(pobi, crong) {
   function multiple(arr){
     return arr.reduce((a, b) => Number(a) * Number(b), 1);  
   }
-  
+
   let pobiLeftMax = Math.max(sum(pobiLeftArr),multiple(pobiLeftArr));
   let pobiRightMax = Math.max(sum(pobiRightArr),multiple(pobiRightArr));
   let crongLeftMax = Math.max(sum(crongLeftArr),multiple(crongLeftArr));
   let crongRightMax = Math.max(sum(crongRightArr),multiple(crongRightArr));
+  
+  function makeScore(leftMax, rightMax){
+    if(leftMax>rightMax){
+      return leftMax;
+    } return rightMax;
+  }
+
+ let pobiScore = makeScore(pobiLeftMax,pobiRightMax);
+ let crongScore = makeScore(crongLeftMax,crongRightMax);
  
 }
 
