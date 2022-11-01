4e2d6797d76add619f0421eac3f4d48368edb45f
energy1-914 <dlawldnjs5@daum.net>
2022-11-01 23:02:31 +0900

    feat: 문제 6번 기능구현

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,32 @@
 function problem6(forms) {
-  var answer;
+  let commonWords = {};
+  let twoWords = [];
+  let nicknames = [];
+  let standardNickname = '';
+  const len = forms.length;
+  let answer = [];
+  for (i=0; i<len; i++) {
+    nicknames.push(forms[i][1]);
+    }
+  for (i=0; i<len; i++){
+    standardNickname = nicknames[i]
+    for (j=0; j< standardNickname.length-1; j++){
+        twoWords.push(standardNickname.substr(j,2));
+        nicknames.forEach(x => { 
+          if (x.includes(twoWords) && x !==standardNickname)
+          commonWords[twoWords] = 1;
+        })
+        twoWords = []; 
+    }
+  }
+  let num = Object.keys(commonWords).length;
+  for(i=0;i<num;i++){
+    for (j=0;j<len;j++){
+      if(nicknames[j].includes(Object.keys(commonWords)[0])){
+        answer.push(forms[j][0]);
+      }
+    }  
+  }
   return answer;
 }
 
