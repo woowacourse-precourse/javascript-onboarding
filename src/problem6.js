4987ebc848329c972594675f6e61d1e4b66f57ff
이여진 <jinspace3710@gmail.com>
2022-11-01 14:43:35 +0900

    feat: problem6 기능 1 - 닉네임 중 두 연속 글자 추출 후 포함 여부 확인

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,19 @@
 function problem6(forms) {
-  var answer;
+  let answer;
+
+  for (i = 0; i < forms.length; i++) {
+    let flag = 0;
+
+    for (j = 0; j < forms[i][1].length - 1; j++) {
+      let word = forms[i][1].substr(j, 2);
+
+      for (k = i + 1; k < forms.length; k++) {
+        if (forms[k][1].includes(word)) {
+        }
+      }
+    }
+  }
+
   return answer;
 }
 
