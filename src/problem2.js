29dc5b0ea6d235622bbb1967b04fe03dd2773c7b
minseok1109 <park485201@naver.com>
2022-11-01 10:02:13 +0900

    refactor: [problem2] 중복된 문자가 2개이상인 경우 해결

--- src/problem2.js
+++ src/problem2.js
@@ -1,8 +1,17 @@
 function problem2(cryptogram) {
   let stack = [];
+  let duplicated = "";
   for (let i of cryptogram) {
-    if (stack[stack.length - 1] === i) stack.pop();
-    else stack.push(i);
+    if (duplicated === i) {
+      continue;
+    } else if (stack[stack.length - 1] === i) {
+      duplicated = stack.pop();
+    } else {
+      stack.push(i);
+    }
+    if (duplicated !== i) {
+      duplicated = "";
+    }
   }
   return stack.join("");
 }
