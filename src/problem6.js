eb4236f800dd2421668ce2de3027e0499a011b39
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 21:10:35 +0900

    [problem6] feat: 필요한 기능 check

--- src/problem6.js
+++ src/problem6.js
@@ -1,6 +1,15 @@
 function problem6(forms) {
   var answer;
+
+  // 이메일 오름차순 및 중복 제거
+
   return answer;
 }
+
+// 같은 글자(한글) 연속 check 함수
+function checkDouble(nick) {}
+
+// 해당 이메일 return 함수
+function whatEmail(nick) {}
 
 module.exports = problem6;
