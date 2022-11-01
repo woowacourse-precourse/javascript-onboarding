4ff6e558f9d40492772d470a33c12e7ccf9406c1
Sanggil Han <tkdrlf95@naver.com>
2022-11-01 15:33:38 +0900

    feat: Add encoding function in problem4.js
    ascii코드를 받아서 A-Z를 Z-A, a-z를 z-a로 변환하는 함수를 구현하였음

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,14 @@
 function problem4(word) {
-  var answer;
+  let answer = '';
   return answer;
 }
 
+function Encoding(ascii) {
+  if (ascii >= 65 && ascii <= 90) {
+    return String.fromCharCode(90-(ascii-65));
+  }
+  if (ascii >= 97 && ascii <= 122) {
+    return String.fromCharCode(122-(ascii-97));
+  }
+}
 module.exports = problem4;
