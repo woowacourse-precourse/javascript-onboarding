89473638beb75aa200e34e56b09c1c8dacd8da6e
n0eyes <doll4586633@naver.com>
2022-11-01 17:43:44 +0900

    feat: [P1] 페이지의 각 숫자들의 곱을 구하는 함수 구현

--- src/problem1.js
+++ src/problem1.js
@@ -5,6 +5,13 @@ function getResultOfAdd(page) {
     .reduce((prev, cur) => (prev += parseInt(cur)), 0);
 }
 
+function getResultOfMul(page) {
+  return page
+    .toString()
+    .split("")
+    .reduce((prev, cur) => (prev *= parseInt(cur)), 1);
+}
+
 function problem1(pobi, crong) {
   var answer;
   return answer;
