2f9b03c043954f8e32d7cc0bdf542b1deef18cf1
Sangmin Lee <gink.ss00@gmail.com>
2022-11-01 22:01:36 +0900

    [problem7] feat: findUserFriends 함수 구현
    user와 친구 관계인 배열을 출력해준다.

--- src/problem7.js
+++ src/problem7.js
@@ -1,3 +1,16 @@
+function findUserFriends(user, friends) {
+  const userFriends = [user];
+
+  for (let i = 0; i < friends.length; i++) {
+    if (friends[i][0] === user) {
+      userFriends.push(friends[i][1]);
+    } else if (friends[i][1] === user) {
+      userFriends.push(friends[i][0]);
+    }
+  }
+  return userFriends;
+}
+
 function problem7(user, friends, visitors) {
   var answer;
   return answer;
