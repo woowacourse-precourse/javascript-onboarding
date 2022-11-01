9250bf6be15dcd89e14981c98ff9082e7fb3145e
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 15:35:39 +0900

    feat[P5]: bank의 배열로 money를 나누기 & 몫을 answer에 배열로 저장

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,13 @@
 function problem5(money) {
-  var answer;
-  return answer;
+	const bank = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+	let answer = [];
+	bank.forEach((element) => {
+		let quotient = parseInt(money / element);
+		answer.push(quotient);
+		money -= quotient * element;
+	});
+
+	return answer;
 }
 
 module.exports = problem5;
