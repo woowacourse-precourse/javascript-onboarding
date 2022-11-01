d71444839da60c7952dd51170606b465cf6f4c04
SeongOh Park <qkrtjddh1212@naver.com>
2022-11-01 14:48:09 +0900

    feat[P4]: ASCII CODE 선언 & reduce함수로 각각의 char에 접근

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,17 @@
 function problem4(word) {
-  var answer;
-  return answer;
+	const CODE = {
+		A_CODE: "A".charCodeAt(0),
+		Z_CODE: "Z".charCodeAt(0),
+		a_CODE: "a".charCodeAt(0),
+		z_CODE: "z".charCodeAt(0),
+	};
+	let answer;
+
+	answer = Array.from(word).reduce((prev, curr) => {
+		console.log(prev, curr);
+		return prev + curr;
+	});
+	return answer;
 }
 
 module.exports = problem4;
