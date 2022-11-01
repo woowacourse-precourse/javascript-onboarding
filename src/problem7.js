53500b3906af38bec1df030f117068c8ea6ca30d
Kim jinwoo <49385170+Sunny-jinn@users.noreply.github.com>
2022-11-01 19:43:09 +0900

    [problem7] feat: 1.Create score array & friends array

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
+  // 추천친구 결과 result
+  var answer = [];
+  // 추천 친구와 점수 포함하는 object를 담는 array
+  const scoreArr = [];
+  // 추천 친구 이름 담는 배열
+  const nameArr = [];
+  // 현재 친구인 user 이름 담는 array
+  const friendsArr = [];
+
   return answer;
 }
 
