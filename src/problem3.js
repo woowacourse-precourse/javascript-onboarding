e26389ae25cebcb18707591a8a4e1e40e95ba8eb
HaroldBKim <sbookim2@gmail.com>
2022-11-01 18:24:32 +0900

    solved problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,15 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  
+  for (let i = 1; i <= number; i++) {
+  numArr = String(i).split("");
+  
+  const divArr = numArr.map((x) => Number(x) > 0 && Number(x) % 3);
+  
+  const clap = divArr.filter((x) => x === 0).length;
+  answer += clap;
+  }
   return answer;
-}
-
-module.exports = problem3;
+  }
+  
+  module.exports = problem3;
