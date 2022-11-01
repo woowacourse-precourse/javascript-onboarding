2a46bcd89170333a2ab1996f6862842966685f76
1211lys <hpppower111@gmail.com>
2022-11-01 22:18:37 +0900

    ADD : 친구 목록 필터링

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,13 @@
 function problem7(user, friends, visitors) {
-  var answer;
-  return answer;
+  let getFriends = [];
+
+  for (i in friends) {
+    if (friends[i][0] == user) {
+      getFriends.push(friends[i][1]);
+    } else if (friends[i][1] == user) {
+      getFriends.push(friends[i][0]);
+    }
+  }
 }
 
 module.exports = problem7;
