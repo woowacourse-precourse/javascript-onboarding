469782eeecd8182d501dea74838b8b2bcd7d5fdf
hyunjoebrother <hyunjoe31@gmail.com>
2022-11-01 18:01:31 +0900

    [problem3] feat: 필요한 기능 check

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,12 @@
 function problem3(number) {
   var answer;
+  let countClap;
+
+  // for문으로 1부터 number까지 반복
   return answer;
 }
+
+// 3, 6, 9 발견 함수
+function checkClap(num) {}
 
 module.exports = problem3;
