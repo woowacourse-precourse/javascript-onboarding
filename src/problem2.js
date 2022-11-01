f24f888d0e0ba2dac272624e74853bc26fcdf8c8
hyunjin <leehj0110@kakao.com>
2022-11-01 11:06:58 +0900

    refactor: problem2 스택사용해 리팩터링

--- src/problem2.js
+++ src/problem2.js
@@ -1,33 +1,15 @@
 function problem2(cryptogram) {
-  if (cryptogram.length === 1) return cryptogram
+  const stack = []
 
-  while (1) {
-    const duplicatedSection = []
-    let left = 0,
-      right = 0
+  let lastPoppedChar = ""
 
-    while (left <= cryptogram.length && right <= cryptogram.length) {
-      if (left === right) right++
-      else if (cryptogram[left] !== cryptogram[right]) left = right
-      else if (cryptogram[left] === cryptogram[right]) {
-        duplicatedSection.push(left)
-        duplicatedSection.push(right)
-        right += 1
-        left = right
-      }
-    }
-
-    if (duplicatedSection.length === 0) return cryptogram
-
-    let tempCryptogram = ""
-
-    for (let i = 0; i < cryptogram.length; i++) {
-      if (!duplicatedSection.includes(i)) tempCryptogram += cryptogram[i]
-    }
-
-    cryptogram = tempCryptogram
-  }
-  return cryptogram
+  Array.from(cryptogram).forEach((char) => {
+    if (stack[stack.length - 1] === char) lastPoppedChar = stack.pop()
+    else if (stack.length === 0 && lastPoppedChar !== char) stack.push(char)
+    else if (lastPoppedChar !== char) stack.push(char)
+  })
+  console.log(stack.join(""))
+  return stack.join("")
 }
 
 module.exports = problem2
