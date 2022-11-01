82181016528656724298d703558b64d0ad3cf0ee
Dahye <06robin11@gmail.com>
2022-11-01 18:16:49 +0900

    feat:get book game result;

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,35 @@
+function getHigherNumber(nums) {
+  const heigherNums = nums.map((num) => {
+    const sum = String(num)
+      .split("")
+      .reduce((prev, next) => +prev + +next, 0);
+    const mul = String(num)
+      .split("")
+      .reduce((prev, next) => +prev * +next, 1);
+    return Math.max(sum, mul);
+  });
+  return Math.max(...heigherNums);
+}
+
+function isError(page1, page2) {
+  return page1 + 1 !== page2;
+}
+
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if (isError(pobi[0], pobi[1]) || isError(crong[0], crong[1])) {
+    return -1;
+  }
+
+  const pobiNum = getHigherNumber(pobi);
+  const crongNum = getHigherNumber(crong);
+
+  if (pobiNum > crongNum) {
+    return 1;
+  } else if (pobiNum < crongNum) {
+    return 2;
+  } else if (pobiNum === crongNum) {
+    return 0;
+  }
 }
 
 module.exports = problem1;
