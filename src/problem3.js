61af17681cfacfca87ec2ba6f85eafbab855c9e4
1211lys <hpppower111@gmail.com>
2022-11-01 19:50:13 +0900

    ADD : problem3 제한사항 추가

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,14 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let clap = 0;
+
+  if (number < 0 || 10000 < number) {
+  }
+
+  for (let i = 0; i <= number; i++) {
+    clap += getClap(i);
+  }
+
+  return clap;
 }
 
 module.exports = problem3;
