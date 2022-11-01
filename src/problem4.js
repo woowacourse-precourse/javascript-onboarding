0f8542554f2adb9dcb9cae12157e2f3efdc1919e
SALT0601 <thfxm0601@gmail.com>
2022-11-01 16:04:09 +0900

    refactor: problem 4 예외처리
    알파벳 외의 문자는 변환하지 않기 위해 예외 처리 추가

--- src/problem4.js
+++ src/problem4.js
@@ -6,13 +6,15 @@ function problem4(word) {
     let wordAscii = wordArr.charCodeAt(i);
     if(wordAscii === 32){ //32는 아스키 코드로 공백이므로 공백일 경우 " "
       answer += " ";
-    }else{ //공백이 아닐 경우 
+    }else if(wordAscii>=65 && wordAscii<=90 || wordAscii>=97 && wordAscii<=122){ //공백이 아닐 경우 
       let newWord = String.fromCharCode(155 - wordAscii);//반대의 글자 아스키코드를 합하면 155라서 155에서 빼줌
       if( word.charCodeAt(i)>=65 && word.charCodeAt(i)<=90 ){//입력된 값이 대문자라면
         answer += newWord //대문자 그대로 합쳐줌
       }else{
         answer += newWord.toLowerCase(); //대문자가 아니라면 소문자로 치환해서 합쳐줌
       }
+    }else{//알파벳 이외의 문자일 경우 변환 안함
+      answer += String.fromCharCode(wordAscii);
     }
   }
 
