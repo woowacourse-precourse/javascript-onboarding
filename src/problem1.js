5227860bf94b591e2e30d0fa73cd114885fd22f6
terry <kdjtiger@naver.com>
2022-11-01 19:07:22 +0900

    예외사항 추가 (왼쪽이 짝수거나 왼쪽 오른쪽의 차이가 1이 넘을 때  -1 리턴)

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,18 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+  if ( !isValid(pobi) || !isValid(crong) ) {
+    return -1;
+  }
+
+}
+function isValid(arr) {
+  let left = arr[0];
+  let right = arr[1];
+
+  if ( left % 2 == 0) {
+    return false;
+  }
+
+  return right - left == 1;
 }
 
 module.exports = problem1;
