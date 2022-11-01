5c4b2158003d59918f7eea174211c23a07bccf10
parkjikuk <paxkk@naver.com>
2022-11-01 23:56:47 +0900

    feat : 함수 이름 수정 및 방문자 점수 부여하는 함수 추가

--- src/problem7.js
+++ src/problem7.js
@@ -1,14 +1,9 @@
-function problem7(user, friends, visitors) {
-  const findFriends = findFriends(user, friends);
-  const friendsOfFriend = findFriendsOfFriend(user, friends, findFriends);
-  const getScoreFriendOfFriend = getScoreFriendOfFriend(friendsOfFriend);
-
-}
+const friendArr = [];
+const friendOfFriendArr = [];
+let score = new Map();
 
 function findFriends(user, friends) {
-  let friendArr = [];
-
-  friends.map((friend) => {
+    friends.map((friend) => {
     if(friend.includes(user)){
       const deliteUserName = friend.filter((el) => el !== user);
       friendArr.push(deliteUserName)
@@ -17,31 +12,54 @@ function findFriends(user, friends) {
   return friendArr;
 }
 
-function findFriendsOfFriend(user, friends, findFriends) {
-  let friendOfFriend = [];
-  
+
+function findFriendsOfFriend(user, friends, friendArr) {
   friends.map((friend) => {
-    findFriends.map((userFriend) => {
+    friendArr.map((userFriend) => {
       if(friend.includes(userFriend) && !friend.includes(user)) {
         const friendsOfFriendArr = friend.filter((el) => el !== userFriend);
         if (!friendsOfFriendArr.includes(friendsOfFriendArr[0])) {
-          friendOfFriend.push(friendsOfFriendArr[0])
+          friendOfFriendArr.push(friendsOfFriendArr[0])
         }
       }
     })
   })
-  return friendOfFriend;
+  return friendOfFriendArr;
 }
 
-function getScoreFriendOfFriend(friendsOfFriend) {
-  let score = new Map();
 
-  friendsOfFriend.map((friendOfFriend) => {
+function getScoreFriendOfFriend(friendOfFriendArr) {
+  friendOfFriendArr.map((friendOfFriend) => {
     score.has(friendOfFriend) 
     ? score.set(friendOfFriend, score.get(friendOfFriend) + 10)
     : score.set(friendOfFriend, 10);
   })
   return score;
+}
+
+
+function getScoreVisitors(visitors) {
+  visitors.map((visitor) => {
+    if (!friendArr.includes(visitor)) {
+      score.has(visitor)
+      ? score.set(visitor, score.get(visitor) + 1)
+      : score.set(visitor, 1);
+    }
+  })
+  return score;
+}
+
+
+function problem7(user, friends, visitors) {
+  findFriends(user, friends);
+  findFriendsOfFriend(user, friends, friendArr);
+  getScoreFriendOfFriend(friendOfFriendArr);
+  getScoreVisitors(visitors);
+
+  let scoreArr = [...score].sort((a, b) => b[1] - a[1]);
+  let answer = scoreArr.slice(0, 5).map((score) => score[0]);
+
+  return answer;
 }
 
 module.exports = problem7;
