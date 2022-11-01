f4e348274fd339cb701dce78d042ccbad31105db
Suhyeon Lee <suhyeonlee.dev@gmail.com>
2022-11-01 19:40:16 +0900

    feat: 문제7 user와 함께 아는 친구 점수 계산하는 기능 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,6 +1,8 @@
 function problem7(user, friends, visitors) {
   let answer;
   const relationMap = new Map();
+  const scoreMap = new Map();
+  let userFriend;
 
   // 친구 관계 정리
   friends.forEach((friend) => {
@@ -9,6 +11,21 @@ function problem7(user, friends, visitors) {
         relationMap.set(friend[i], []);
       }
       relationMap.get(friend[i]).push(i == 0 ? friend[1] : friend[0]);
+    }
+  });
+
+  userFriend = relationMap.get(user);
+
+  // user와 함께 아는 친구 점수 계산
+  userFriend.forEach((friend) => {
+    const acquaintance = relationMap.get(friend);
+    for (let i = 0; i < acquaintance.length; i++) {
+      if (acquaintance[i] !== user) {
+        if (!scoreMap.has(acquaintance[i])) {
+          scoreMap.set(acquaintance[i], 0);
+        }
+        scoreMap.set(acquaintance[i], scoreMap.get(acquaintance[i]) + 10);
+      }
     }
   });
 
