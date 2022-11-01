2b2e375a1f841dd8423ab310b358efab888ce174
y00eunji <aine0227@naver.com>
2022-11-01 20:33:02 +0900

    feat: [problem6] check nickname is valid

--- src/problem6.js
+++ src/problem6.js
@@ -1,3 +1,9 @@
+const isValidNameFormat = (name) => {
+  const nameRegExp = /^[ㄱ-ㅎ|ㅏ-ㅣ|가-힣]{1,19}$/;
+
+  return nameRegExp.test(name);
+};
+
 function problem6(forms) {
   var answer;
   return answer;
