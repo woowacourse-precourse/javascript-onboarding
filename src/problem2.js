353e9d39e6aeffd44257914078ad0f60995b9c00
Lena <lny970116@gmail.com>
2022-11-01 23:09:10 +0900

    problem 2 fix

--- src/problem2.js
+++ src/problem2.js
@@ -1,9 +1,10 @@
 function problem2(cryptogram) {
-  var answer;
+  var answer = [];
   answer.push(cryptogram[0]);
 
   for (let i = 1; i < cryptogram.length; i++) {
-    answer[answer.length - 1] === cryptogram[i] ? answer.pop() : answer.push(cryptogram[i]);}
+    answer[answer.length - 1] === cryptogram[i] ? answer.pop() : answer.push(cryptogram[i]);
+  }
 
   return answer.join("");
 }
