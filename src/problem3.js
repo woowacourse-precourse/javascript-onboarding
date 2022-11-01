1da649318c8065d1aa7ab766fe9950443d47e7ad
cybersyn2 <quandonestpetit@outlook.com>
2022-11-01 15:14:04 +0900

    draft of problem3

--- src/problem3.js
+++ src/problem3.js
@@ -1,6 +1,22 @@
 function problem3(number) {
-  var answer;
-  return answer;
+  let res = 0;
+  for (let i = 0; i <= number; i++) {
+    res += _counter(i);
+  }
+  return res;
+}
+
+function _counter(number) {
+  let remainder = 0;
+  let cnt = 0;
+  while (number > 0) {
+    remainder = number % 10;
+    if (remainder == 3 || remainder == 6 || remainder == 9) {
+      cnt += 1;
+    }
+    number = Math.trunc(number / 10);
+  }
+  return cnt;
 }
 
 module.exports = problem3;
