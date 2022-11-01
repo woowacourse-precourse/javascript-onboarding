fe8df0eab3ec55ed6074f4a74706264c0673f2dd
soomin <qkrtnals970804@gmail.com>
2022-11-01 18:30:11 +0900

    [problem7] 친구 관계 찾기 기능

--- src/problem7.js
+++ src/problem7.js
@@ -1,5 +1,15 @@
+const findRelation = (user, friends) => {
+  let friendRelation = [];
+  friends.forEach((friend) => {
+    if (friend.includes(user)) {
+      friendRelation.push(friend);
+    }
+  })
+  return friendRelation;
+}
 function problem7(user, friends, visitors) {
-  var answer;
+  const friendRelation = findRelation(user, friends);
+  let answer;
   return answer;
 }
 
