ec64152767c44812b362174b82a2bc32094ab147
jungsmins1 <jungsmins00@gmail.com>
2022-11-01 19:08:56 +0900

    feat: 예외처리 부분 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,12 +1,15 @@
 function problem2(cryptogram) {
   let arr = [];
+  let overlap = ""
 
   //문자열을 반복하면서 연속하는 중복 문자를 찾고 삭제하는 기능
   for (let i = 0; i < cryptogram.length; i++) {
-    if (arr[arr.length - 1] !== cryptogram[i]) {
+    if (arr[arr.length - 1] !== cryptogram[i] &&
+        cryptogram[i] !== overlap) {
       arr.push(cryptogram[i])
+      overlap = "";
     } else if (arr[arr.length - 1] === cryptogram[i]) {
-      arr.pop()
+      overlap = arr.pop();
     }
   }
   return arr.join("")
