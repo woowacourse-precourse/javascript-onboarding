927f648bf5e7f481395498c5a1d24cefcb2c9e8d
heejaykong <heejay.dev@gmail.com>
2022-11-01 19:49:31 +0900

    feat: 매개변수 pobi와 crong를 받는 기능(w/예외처리) 구현

--- src/problem1.js
+++ src/problem1.js
@@ -1,5 +1,44 @@
+function isIntegerParsable(n) {
+  return !Number.isNaN(parseInt(n)) && Number.isSafeInteger(Number(n));
+};
+
+function isProperInput(pages) {
+  const FIRST_PAGE_NUMBER = 1;
+  const LAST_PAGE_NUMBER = 400;
+  const LEFT_PAGE = 0;
+  const RIGHT_PAGE = 1;
+
+  if(!isIntegerParsable(pages[LEFT_PAGE]) || !isIntegerParsable(pages[RIGHT_PAGE])) {
+    return false;
+  }
+
+  const leftPageNumber = parseInt(pages[LEFT_PAGE]);
+  const rightPageNumber = parseInt(pages[RIGHT_PAGE]);
+
+  if (leftPageNumber === FIRST_PAGE_NUMBER) {
+    return false;
+  }
+
+  if (rightPageNumber === LAST_PAGE_NUMBER) {
+    return false;
+  }
+
+  if (leftPageNumber !== rightPageNumber - 1) {
+    return false;
+  }
+
+  return true;
+}
+
 function problem1(pobi, crong) {
-  var answer;
+  let answer;
+
+  const CODE_EXCEPTION = -1;
+
+  if (!isProperInput(pobi) || !isProperInput(crong)) {
+    return CODE_EXCEPTION;
+  }
+
   return answer;
 }
 
