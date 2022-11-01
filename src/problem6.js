96df2cdf79a3a7faa5aafa30bb13beaf9aa9d9e7
7ngenious <zxvs1218@gmail.com>
2022-11-01 21:56:43 +0900

    feat: [Problem6] 1st Soltion

--- src/problem6.js
+++ src/problem6.js
@@ -1,5 +1,26 @@
 function problem6(forms) {
-  var answer;
+  let answer = new Set();
+  let noneDuplicated = [];
+  let nickName = [];
+  let nameKey = [];
+  let result = [];
+
+  for (let i = 0; i < forms.length; i++) {
+    nickName.push(forms[i][1]);
+    for (let j = 0; j < nickName[i].length - 1; j++) {
+      nameKey = nickName[i].substring(j, j + 2);
+      if (noneDuplicated[nameKey]) {
+        result = noneDuplicated[nameKey];
+        if (result !== forms[i][0]) {
+          answer.add(result);
+          answer.add(forms[i][0]);
+        }
+      }
+      noneDuplicated[nameKey] = forms[i][0];
+    }
+  }
+
+  answer = [...answer].sort();
   return answer;
 }
 
