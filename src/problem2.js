094b12207e75eb4d06e5ae79b7091ecc74691687
Sangwon Lee <tkdtm2010@ajou.ac.kr>
2022-11-01 09:17:12 +0900

    기능 함수화 처리 및 버그 수정

--- src/problem2.js
+++ src/problem2.js
@@ -1,23 +1,39 @@
 function problem2(cryptogram) {
   var answer;
   let cryptoAry = cryptogram.split("");
-  let count;
 
-  for (let i = 0; i < cryptoAry.length; i++) {
-    if (cryptoAry[i] == cryptoAry[i + 1]) {
-      count = i + 1;
-      while (cryptoAry[count] === cryptoAry[count + 1]) {
-        count++;
+  function scan(array) {
+    let count = 1;
+    let temp;
+
+    for (let i = 0; i < array.length; i++, count = 1) {
+      temp = array[i];
+      if (temp === array[i + 1]) {
+        for (let j = i + 1; temp === array[j]; j++) {
+          count++;
+        }
+        array.splice(i, count);
+        i--;
       }
-      cryptoAry.splice(i, count - i + 1);
-      i = 0;
     }
-    if (cryptoAry.length == 2 && cryptoAry[0] === cryptoAry[1]) {
-      cryptoAry = [];
-    }
+    return array;
   }
-  
-  answer = cryptoAry.join("");
+
+  let tempAry = scan(cryptoAry);
+
+  while (true) {
+    if (tempAry === scan(tempAry)) {
+      tempAry == scan(tempAry);
+      break;
+    }
+    tempAry = scan(tempAry);
+  }
+  tempAry = scan(tempAry);
+
+
+
+  answer = tempAry.join("");
+  console.log(answer);
   return answer;
 }
 
