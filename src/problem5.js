0c2f9714827be4799d27004b00b9f4f287e3b100
emma <jhj191058@gmail.com>
2022-11-01 19:40:18 +0900

    problem5 수정 후 재 업데이트

--- src/problem5.js
+++ src/problem5.js
@@ -1,13 +1,13 @@
 function problem5(money) {
+  var arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  var answer = new Array();
 
-    var arr = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
-    var answer = new Array();
-    
-    for( var i = 0; i< arr.length; i++) {
-      answer[i] = Math.floor(money/arr[i]);
-      money -= arr[i]*Math.floor(money/arr[i]);
+  for ( var i = 0; i < arr.length; i++) {
+    answer[i] = Math.floor(money/arr[i]);
+    money -= arr[i]*Math.floor(money/arr[i]);
   }
-    return answer;
-  }
-  
-  module.exports = problem5;
+
+  return answer;
+}
+
+module.exports = problem5;
