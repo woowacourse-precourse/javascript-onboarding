66bafc78ee89972ebb8384689d225b39f38334e0
dldnjswns31 <dldnjswns32@gmail.com>
2022-11-01 21:12:32 +0900

    feature(problem7): getRelationScore 함수 구현

--- src/problem7.js
+++ src/problem7.js
@@ -1,10 +1,27 @@
 function problem7(user, friends, visitors) {
+  // friends 배열에서 user의 친구만 배열로 생성
   const getFriends = (user, friends) => {
     const user_friends = [];
     for (let friend of friends) {
       if (friend.includes(user)) user_friends.push(friend[0]);
     }
     return user_friends;
+  };
+
+  // user의 친구의 친구에게 추천 점수 부여
+  const getRelationScore = (friends, user_friends, recommend_score) => {
+    friends.forEach((item) => {
+      const my_friend = item[0];
+      const relation_friend = item[1];
+      if (user_friends.includes(my_friend) && item[1] !== user) {
+        if (relation_friend in recommend_score) {
+          recommend_score[relation_friend] += 10;
+        } else {
+          recommend_score[relation_friend] = 10;
+        }
+      }
+    });
+    return recommend_score;
   };
 
   let answer;
