b2cc4cd4bf123e9cefa7e1ec70720fe54e264a88
1211lys <hpppower111@gmail.com>
2022-11-01 20:50:15 +0900

    ADD : problem5

--- src/problem5.js
+++ src/problem5.js
@@ -1,6 +1,14 @@
 function problem5(money) {
-  var answer;
-  return answer;
+  const UNIT_OF_MONEYS = [50000, 10000, 5000, 1000, 500, 100, 50, 10, 1];
+  const result = [];
+
+  for (let i = 0; i < 9; i++) {
+    result.push(parseInt(money / UNIT_OF_MONEYS[i]));
+
+    money = money % UNIT_OF_MONEYS[i];
+  }
+
+  return result;
 }
 
 module.exports = problem5;
