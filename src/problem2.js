02e8b676129fc62bda71fe20c838832a963ce75e
recomplete <wantop@jbnu.ac.kr>
2022-11-01 22:14:12 +0900

    feat:PROBLEM2:기능 목록 작성

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,14 @@
+/*
+ #기능 요구 사항
+임의의 문자열 cryptogram이 매개변수로 주어질 때, 
+연속하는 중복 문자들을 삭제한 결과를 return 하도록 solution 메서드를 완성하라.
+
+1. 연속하는 중복 문자들을 재귀적으로 삭제한다. 
+
+*/
+
 function problem2(cryptogram) {
-  var answer;
+  let answer;
   return answer;
 }
 
