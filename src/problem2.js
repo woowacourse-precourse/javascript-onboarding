66483909a198e1dbf5929d46c1689e82cfef6361
Monte <monte6198@gmail.com>
2022-11-01 16:50:39 +0900

    feat(problem2.js): 변수 선언

--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,8 @@
 function problem2(cryptogram) {
   var answer;
+  let cryptoArray = cryptogram.split(""); //암호문을 문자 배열로 쪼갠다.
+  let decrypt = []; //암호문 해독 문자열(반복 문자를 제거한 결과 배열)
+
   return answer;
 }
 
