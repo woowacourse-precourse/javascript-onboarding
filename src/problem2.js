663f054658620df88d738abf5389898f6bf7299e
Tory <88531407+1two13@users.noreply.github.com>
2022-11-01 11:22:38 +0900

    refactor(problem2): answerLastWord 변수 위치 변경

--- src/problem2.js
+++ src/problem2.js
@@ -7,10 +7,12 @@ function problem2(cryptogram) {
   // 바로 직전 문자
   let lastStr = "";
   // answer의 마지막 문자
-  let answerLastWord = answer[answer.length - 1];
 
   for (let i = 0; i < cryptogram.length; i++) {
-    if (answerLastWord !== cryptogram[i] && lastStr !== cryptogram[i]) {
+    // answer의 마지막 문자
+    let answerLastWord = answer[answer.length - 1];
+
+    if (answerLastWord !== cryptogram[i] && cryptogram[i] !== lastStr) {
       answer += cryptogram[i];
     } else answer = answer.slice(0, -1);
 
