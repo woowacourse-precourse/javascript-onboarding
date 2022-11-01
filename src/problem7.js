8eff033d4764b272e5cd32eaed5e2c970e5e8191
jongwonp <jw032876@gmail.com>
2022-11-01 20:57:04 +0900

    problem7 예외처리 추가

--- src/problem7.js
+++ src/problem7.js
@@ -17,8 +17,13 @@ function problem7(user, friends, visitors) {
     usersFriend.has(visitor) ? false : true
   );
 
-  // user가 들어간 친구관계는 삭제 (이후에 friends 배열을 사용하기 쉽도록 하기 위해서 user가 들어간 부분은 제거함)
-  friends = friends.filter((friend) => (friend.includes(user) ? false : true));
+  // user가 들어간 친구관계는 삭제, 양쪽 다 user와 친구상태인 경우도 삭제 (이후에 friends 배열을 사용하기 쉽도록 하기 위해서 user가 들어간 부분은 제거함)
+  friends = friends.filter((friend) => {
+    if (usersFriend.has(friend[0]) && usersFriend.has(friend[1])) {
+      return false;
+    }
+    return friend.includes(user) ? false : true;
+  });
 
   // 점수를 내는 두 함수가 반환하는 객체를 합치기
   const totalScore = { ...scoreByVisiting(visitors) };
@@ -34,7 +39,14 @@ function problem7(user, friends, visitors) {
 
   // 점수 순, abc 순 으로 정렬
   const scoreArr = Object.entries(totalScore).slice();
-  scoreArr.sort((a, b) => (a[1] > b[1] ? -1 : 1));
+
+  scoreArr.sort((a, b) => {
+    if (a[1] > b[1]) return -1;
+    else if (a[1] === b[1]) {
+      if (a[0] > b[0]) return 1;
+      else return -1;
+    } else return 1;
+  });
 
   // 최대 5명 까지 return
   for (let i = 0; i < 5; i++) {
