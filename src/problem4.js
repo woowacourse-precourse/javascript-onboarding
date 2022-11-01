681c04f3fb65c41b902773e2ae59dc8b611243e6
MINJOO-KIM <gkminju2000@gmail.com>
2022-11-01 11:56:35 +0900

    feat(problem4): 소문자와 대문자를 반대로 변환한다

--- src/problem4.js
+++ src/problem4.js
@@ -1,6 +1,15 @@
 function problem4(word) {
-  var answer;
-  return answer;
+  
+  let arr = word.split("");
+  
+  for(let i=0; i < arr.length; i++) {
+    if(arr[i].charCodeAt()>="A".charCodeAt() && arr[i].charCodeAt()<="Z".charCodeAt()){
+      arr[i] = String.fromCodePoint("A".charCodeAt()+"Z".charCodeAt()-arr[i].charCodeAt());
+    }else if(arr[i].charCodeAt()>="a".charCodeAt() && arr[i].charCodeAt()<="z".charCodeAt()){
+      arr[i] = String.fromCodePoint("a".charCodeAt()+"z".charCodeAt()-arr[i].charCodeAt());
+    }
+  }
+  
 }
 
 module.exports = problem4;
