3d3398f9f7b1b6aa8112f55dadbf5e264dc8bf20
hvvany <kjunhwan1998@gmail.com>
2022-11-01 20:53:47 +0900

    feat(build): solve problem_2

--- src/problem2.js
+++ src/problem2.js
@@ -1,6 +1,30 @@
 function problem2(cryptogram) {
-  let answer = ''
-  return answer;
+  let lst = [...cryptogram];
+  while (true) {
+    let cnt = 0;
+    let l = lst.length
+    for (let i = 0; i < l-1; i++) {
+      cnt += 1;
+      if (lst[i] === lst[i+1]) {
+        if (i+2 < l) {
+          let lst2 = lst.slice(0,i);
+          let lst3 = lst.slice(i+2);
+          lst = lst2.concat(lst3);
+        }
+        else {
+          lst = lst.slice(0,i);
+        }
+        break
+      }
+    }
+    if (cnt === l-1) {
+      let answer = ''
+      for (let i = 0; i < lst.length; i++){
+        answer += lst[i];
+      }
+      return answer
+    }
+  }
 }
 
 module.exports = problem2;
