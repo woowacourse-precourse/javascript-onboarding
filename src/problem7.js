feabdb110f03beb4d4fd6b60183eb0a00b086767
HoberMin <102784200+HoberMin@users.noreply.github.com>
2022-11-01 21:49:37 +0900

    [problem7] feat: visitors에게 추가 1점
    해당 리스트에 이름이 없다면 추가로 1점을 갖고 생성함

--- src/problem7.js
+++ src/problem7.js
@@ -13,6 +13,19 @@ function problem7(user, friends, visitors) {
   friendName.forEach(function (score) {
     scoreFriend.push([0, score]);
   }); // 점수 이름 담긴 scoreFriend + 점수는 0으로 초기화
+function visitorScore(visitorsArray, friendNameArray, scoreFriendArray) {
+  //scoreFriend 배열에 친구명단을 조사해 점수를 부여하는 함수
+  visitorsArray.forEach(function (visitor) {
+    if (friendNameArray.includes(visitor)) {
+      const friendIndex = friendNameArray.indexOf(visitor);
+      scoreFriendArray[friendIndex][0] += 1;
+    } else {
+      //방문자명단에는 있는데 점수,사람 배열에 없으면 1의값을 갖고 push
+      friendNameArray.push(visitor);
+      scoreFriendArray.push([1, visitor]);
+    }
+  }); //visitor에게 점수 부여
+  return scoreFriendArray;
 }
 
 module.exports = problem7;
