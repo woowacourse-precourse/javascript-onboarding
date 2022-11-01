f90c23cfdf9a6a2b9d79ed292bf6feff24c45ff4
윤동주 <yoondj98@naver.com>
2022-11-01 20:46:58 +0900

    우아한 테크코스 프리코슥 1주차 3번 기능구현 완료

--- src/problem3.js
+++ src/problem3.js
@@ -1,5 +1,11 @@
 function problem3(number) {
-  var answer;
+  var answer = 0;
+  for (var i = 1; i <= number; i++) {
+    var str_arr = i.toString().split("");
+    str_arr.map((k) => {
+      if (k == "3" || k == "6" || k == "9") answer++;
+    });
+  }
   return answer;
 }
 
