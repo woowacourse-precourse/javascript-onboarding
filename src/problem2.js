9753dff723a1aa49909074e201cf574ee8fa99f8
2pandi <coder.2pandi@gmail.com>
2022-11-01 19:45:52 +0900

    :bulb: comment: Add comment in problem2
    문제 2번 풀이 과정 주석 추가

--- src/problem2.js
+++ src/problem2.js
@@ -1,7 +1,12 @@
 function problem2(cryptogram) {
+  // 변수 result에 전달인자 복사
   let result = cryptogram;
+
+  // result의 글자수 만큼 반복하여
   for (let i = 0; i < result.length; i++) {
+    // i번 째 글자와 i + 1번 째 글자가 중복 문자인 경우
     if (result[i] === result[i + 1])
+      // 해당 두 문자열을 result에서 제외시킨다.
       return problem2(result.substring(0, i) + result.substring(i + 2));
   }
   return result;
