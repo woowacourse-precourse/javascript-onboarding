ab024845d80d01969eaf67e470a6f71bff1c9277
TFVO11 <cosmoyj@naver.com>
2022-11-02 21:16:12 +0900

    solving problem1

--- src/problem1.js
+++ src/problem1.js
@@ -1,6 +1,69 @@
 function problem1(pobi, crong) {
-  var answer;
-  return answer;
+    var sum = 0;
+    var mul = 0;
+
+    const sumSolution = n => {
+
+
+        do {
+            sum += n % 10;
+            n = Math.floor(n / 10);
+        } while (n > 10);
+
+        return (sum + n);
+    };
+
+    const multiplySolution = n => {
+
+
+        do {
+            mul *= n % 10;
+            n = Math.floor(n / 10);
+        } while (n > 10);
+
+        return (mul * n);
+    };
+
+    const numberPobi = () => {
+        var sumPobiFirst = sumSolution(pobi[0]);
+        var sumPobiSecond = sumSolution(pobi[1]);
+        var mulPobiFirst = multiplySolution(pobi[0]);
+        var mulPobiSecond = multiplySolution(pobi[1]);
+
+        var arrayPobi = [sumPobiFirst, sumPobiSecond, mulPobiFirst, mulPobiSecond];
+        var maxPobi = Math.max(arrayPobi);
+
+        return maxPobi;
+    };
+
+    const numberCrong = () => {
+        var sumCrongFirst = sumSolution(crong[0]);
+        var sumCrongSecond = sumSolution(crong[1]);
+        var mulCrongFirst = multiplySolution(crong[0]);
+        var mulCrongSecond = multiplySolution(crong[1]);
+
+        var arrayCrong = [sumCrongFirst, sumCrongSecond, mulCrongFirst, mulCrongSecond];
+
+        var maxCrong = Math.max(arrayCrong);
+
+        return maxCrong;
+    };
+
+    let answer = 0;
+    if (numberPobi() < numberCrong()) {
+        answer = 2
+    };
+    if (numberPobi() > numberCrong()) {
+        answer = 1
+    };
+    if (numberPobi() == numberCrong()) {
+        answer = 0
+    };
+    if (pobi[1] > pobi[0] + 1 || crong[1] > crong[0] + 1) {
+        answer = -1
+    };
+
+    return answer;
 }
 
 module.exports = problem1;
--- src/problem2.js
+++ src/problem2.js
@@ -1,5 +1,19 @@
 function problem2(cryptogram) {
-  var answer;
+  var str = cryptogram;
+
+
+  function removeDuplicates(splitStr) {
+    var arr1 = Array.from(splitStr)
+
+    for (i = 0; i < arr1.length + 1; i++) {
+      var arr2 = arr1.filter(e => e !== e)
+    };
+    return arr2;
+
+  }
+  e.match(/[A-z]{2}/);
+
+  var answer = removeDuplicates(str);
   return answer;
 }
 
