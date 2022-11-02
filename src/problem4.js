3898a9d8ef24eb4cd565997df694985a7612f173
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-02 21:07:50 +0900

    style: [P4] 변수명 변경, 함수 정의 순서 변경

--- src/problem4.js
+++ src/problem4.js
@@ -1,23 +1,24 @@
-function problem4(word) {
-  const wordChar = word.split('')
-  const changeChar = (char) => {
-    if (/^[a-z]*$/.test(char)) char = reverseChar(char)
-    if (/^[A-Z]*$/.test(char)) char = reverseChar(char.toLowerCase()).toUpperCase()
-    return char
-  }
-  const changedWord = wordChar.map((char) => changeChar(char)).join("")
-  return changedWord
-}
-
 function reverseChar(char) {
   let alphabet = 'abcdefghijklmnopqrstuvwxyz'.split('')
   let frogDic = new Object()
   
-  for (let i = 0; i < alphabet.length; i++) {
-    let key = alphabet[i]
-    frogDic[key] = alphabet[alphabet.length - 1 - i]
+  for (let alphabetIndex = 0; alphabetIndex < alphabet.length; alphabetIndex++) {
+    let key = alphabet[alphabetIndex]
+    frogDic[key] = alphabet[alphabet.length - 1 - alphabetIndex]
   }
   return frogDic[char]
+}
+
+function changeChar(char) {
+  if (/^[a-z]*$/.test(char)) char = reverseChar(char)
+  if (/^[A-Z]*$/.test(char)) char = reverseChar(char.toLowerCase()).toUpperCase()
+  return char
+}
+
+function problem4(word) {
+  const wordChar = word.split('')
+  const changedWord = wordChar.map((char) => changeChar(char)).join("")
+  return changedWord
 }
 
 module.exports = problem4;
