cd99f2dfbd476366f6d914397730863234489c5d
youzysu <111998760+youzysu@users.noreply.github.com>
2022-11-02 21:20:36 +0900

    style, perf: [P6] 변수명 변경, 불필요한 작업 삭제

--- src/problem6.js
+++ src/problem6.js
@@ -9,7 +9,6 @@ function problem6(forms) {
         let presavedEmail = wordCase[word]
         targetEmail.push(presavedEmail)
         targetEmail.push(email)
-        wordCase[word] = email
       }
     })
   })
@@ -19,8 +18,8 @@ function problem6(forms) {
 function wordOfCrew(form) {
   let nickname = form[1]
   let wordList = []
-  for (let i = 0; i < nickname.length - 1; i++) {
-    wordList.push(nickname.substring(i, i + 2))
+  for (let char = 0; char < nickname.length - 1; char++) {
+    wordList.push(nickname.substring(char, char + 2))
   }
   return [...new Set(wordList)]
 }
