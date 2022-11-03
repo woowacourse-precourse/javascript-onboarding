22f8f4f000e38edfae373b51fcd8e33d4cafcf12
Hypark2018 <hayoungpark2018@gmail.com>
2022-11-03 09:28:54 +0900

    💡 refactor: 코드 간소화, 로직 수정, 함수 기능 단위에 맞게 리팩토링

--- src/problem7.js
+++ src/problem7.js
@@ -1,14 +1,5 @@
 function problem7(user, friends, visitors) {
-  let scoreList = {};
-  let friendList = scoreFriend(friends, scoreList);
-
-  if (friendList[user]) {
-    friendList[user].forEach((rel) => {
-      friendList[rel].forEach((one) => {
-        scoreList[one] += 10;
-      });
-    });
-  }
+  let [friendList, scoreList] = scoreFriend(friends, user);
   scoreList = scoreVisitor(visitors, scoreList);
 
   let answer = Object.keys(scoreList).filter((name) => {
@@ -25,20 +16,30 @@ function problem7(user, friends, visitors) {
 
 function scoreVisitor(visitors, scoreList) {
   visitors.forEach((visitor) => {
-      scoreList[visitor] ? scoreList[visitor] = 1 : scoreList[visitor] += 1;
+      scoreList[visitor] ? scoreList[visitor] += 1 : scoreList[visitor] = 1;
   });
   return scoreList;
 }
 
-function scoreFriend(friends, scoreList) {
+function scoreFriend(friends, user) {
   const friendList = {};
+  const scoreList = {};
+  
   friends.forEach((arr, i) => {
       scoreList[arr[0]] = 0;
       scoreList[arr[1]] = 0;
       friendList[arr[0]] ? (friendList[arr[0]].push(arr[1])) : friendList[arr[0]] = [arr[1]];
       friendList[arr[1]] ? (friendList[arr[1]].push(arr[0])) : friendList[arr[1]] = [arr[0]];
     })
-  return friendList;
+
+  if (friendList[user]) {
+    friendList[user].forEach((rel) => {
+      friendList[rel].forEach((one) => {
+        scoreList[one] += 10;
+      });
+    });
+  }
+  return [friendList, scoreList];
 }
 
 function sortAnswer(a, b) {
