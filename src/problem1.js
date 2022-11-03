6389a0e4b424beb6060ba4c8c5c18046e4ef80f6
areumsheep <areumsheep@gmail.com>
2022-11-04 03:52:22 +0900

    refactor: 문제1 불필요한 조건문 삭제

--- src/problem1.js
+++ src/problem1.js
@@ -27,16 +27,15 @@ const isInvalidPage = (page = []) => {
   const [left, right] = page;
 
   if (left % 2 !== 1) return true;
-  if (right % 2 !== 0) return true;
+
+  // 연속된 페이지
+  if (left + 1 !== right) return true;
 
   if (left <= 1 || left >= 400) return true;
   if (right <= 1 || right >= 400) return true;
 
   // 페이지 형식
   if (page.length > 2) return true;
-
-  // 연속된 페이지
-  if (left + 1 !== right) return true;
   return false;
 };
 
