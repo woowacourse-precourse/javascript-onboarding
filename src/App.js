c79863e0651a0e4e9ba7efb2be1eb57dd067273f
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-08 21:44:51 +0900

    Feat: 랜덤 컴퓨터 정답 생성

--- src/App.js
+++ src/App.js
@@ -9,6 +9,17 @@ class App {
 	}
 	start() {
 		this.Console.print('숫자 야구 게임을 시작합니다.');
+		this.computerAnswer = this.getComputerAnswer();
+	}
+	getComputerAnswer() {
+		const computer = [];
+		while (computer.length < 3) {
+			const number = this.Random.pickNumberInRange(1, 9);
+			if (!computer.includes(number)) {
+				computer.push(number);
+			}
+		}
+		return computer.join('');
 	}
 }
 
