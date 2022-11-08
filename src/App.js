5fc105d0eab23821747fb8b003dfdd6058550c93
sookyeongyeom <sookyeong.dev@gmail.com>
2022-11-08 21:55:21 +0900

    Feat: 다음 스텝 유효성 검증

--- src/App.js
+++ src/App.js
@@ -42,6 +42,7 @@ class App {
 	getNextStep() {
 		this.Console.readLine('게임을 새로 시작하려면 1, 종료하려면 2를 입력하세요.\n', (nextStep) => {
 			this.nextStep = nextStep;
+			this.validateNextStep();
 		});
 	}
 	validatePlayerAnswer() {
@@ -52,6 +53,9 @@ class App {
 			if (isNaN(v)) throw new Error('Invalid Player Answer');
 			if (!Number(v)) throw new Error('Invalid Player Answer');
 		});
+	}
+	validateNextStep() {
+		if (!(this.nextStep === '1' || this.nextStep === '2')) throw new Error('Invalid Next Step');
 	}
 	getComputerAnswer() {
 		const computer = [];
