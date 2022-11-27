1ce51f8b6ee7fee1da97df38b75d92cd3b95265a
soohyun <bmy1320@gmail.com>
2022-11-27 15:16:39 +0900

    refactor(problem4): 문제4 리팩터링

--- src/problem4.js
+++ src/problem4.js
@@ -1,23 +1,31 @@
-function problem4(word) {
-  const [ASCII_A, ASCII_Z] = [65, 90];
-  const [ASCII_a, ASCII_z] = [97, 122];
-
-  function frogDict(num) {
-    let frogDictKey = 0;
-    if (num >= ASCII_A && num <= ASCII_Z)
-      frogDictKey = ASCII_Z - (num - ASCII_A);
-    else if (num >= ASCII_a && num <= ASCII_z)
-      frogDictKey = ASCII_z - (num - ASCII_a);
-
-    return String.fromCharCode(frogDictKey);
+class FrogWord {
+  constructor(word) {
+    this.word = word;
   }
 
-  let result = "";
-  word.split("").forEach((str) => {
-    result += str !== " " ? frogDict(str.charCodeAt(0)) : " ";
-  });
+  getWord() {
+    const letters = [];
+    this.word.split("").forEach((letter) => {
+      letters.push(this.translate(letter));
+    });
 
-  return result;
+    return letters.join("");
+  }
+
+  translate(letter) {
+    const [ASCII_A, ASCII_Z, ASCII_a, ASCII_z] = [65, 90, 97, 122];
+    const key = letter.charCodeAt();
+    if (key >= ASCII_A && key <= ASCII_Z)
+      return String.fromCharCode(ASCII_Z - (key - ASCII_A));
+    else if (key >= ASCII_a && key <= ASCII_z)
+      return String.fromCharCode(ASCII_z - (key - ASCII_a));
+    return String.fromCharCode(key);
+  }
+}
+
+function problem4(word) {
+  const frogWord = new FrogWord(word);
+  return frogWord.getWord();
 }
 
 module.exports = problem4;
